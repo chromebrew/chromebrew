e268072345f04b6f16f919453a7e3a023b2d8344
Michał Siwek <mike21@aol.pl>
2013-09-12 18:30:31 +0200

    Temporarily remove the license file

---
+++ packages/ruby.rb
@@ -0,0 +1,11 @@
+require 'package'
+
+class Ruby < Package
+  version '2.0.0p247'
+  binary_url ({
+    i686: "https://dl.dropboxusercontent.com/s/tufbuqcn80ubypx/ruby-2.0.0p247-chromeos-i686.tar.gz?token_hash=AAGu_5wuqZe2eDOkfL5oh4esQ8HAZJIvbMG7GplnQrSa3g&dl=1"
+  })
+  binary_sha1 ({
+    i686: "49eeba5d542e4c3e6aa3686f215485e0946fb99a"
+  })
+end
