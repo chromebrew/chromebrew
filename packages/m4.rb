6a04923966ea9f65ec77528430edc418d82b1a49
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-11-17 12:37:01 -0500

    ncurses, ncurses_so, autoconf, automake, clisp, diffutils, ffcall, libsigsegv, m4

---
+++ packages/m4.rb
@@ -0,0 +1,16 @@
+require 'package'
+
+class M4 < Package
+  version '1.4.17'
+  source_url 'ftp://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.xz'
+  source_sha1 '74ad71fa100ec8c13bc715082757eb9ab1e4bbb0'
+
+  def self.build
+    system "./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -1,13 +1,19 @@
 require 'package'
 
 class Ncurses < Package
-  version '5.9-chromeos1'
-  binary_url ({
-    i686: 'https://dl.dropboxusercontent.com/s/aule5impm7ek644/ncurses-5.9-chromeos-i686.tar.gz?token_hash=AAH6Z34Dd17OeG_to_cD7z_1bc95AObrDrfU2mHGhZ-3bg&dl=1',
-    x86_64: 'https://dl.dropboxusercontent.com/s/py8kz22dql0li8x/ncurses-5.9-chromeos1-chromeos-x86_64.tar.gz?token_hash=AAFMbTmK7iW16QidxzsQ7Y2CI96B33MpF9-9GBpqbOH4uw&dl=1'
-  })
-  binary_sha1 ({
-    i686: 'a46463be1b8f7cd3af8309e49fd6bc60cf0e5341',
-    x86_64: '691c4819c98a26da69136da04b57bed38aad775f'
-  })
+  version '5.9'
+  source_url 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz'
+  source_sha1 '3e042e5f2c7223bffdaac9646a533b8c758b65b5'
+
+  depends_on "libsigsegv"
+  depends_on "diffutils"
+
+  def self.build
+    system "./configure --without-debug --prefix=/usr/local"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
 end
