b4bcf4221b81d69a9b2432f8677ebd9c9beec4a7
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-11-17 17:10:07 -0500

    zsh works

--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -5,11 +5,10 @@ class Ncurses < Package
   source_url 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz'
   source_sha1 '3e042e5f2c7223bffdaac9646a533b8c758b65b5'
 
-  depends_on "libsigsegv"
   depends_on "diffutils"
 
   def self.build
-    system "./configure --without-debug --prefix=/usr/local"
+    system "./configure CFLAGS=\" -fPIC\" --without-debug --prefix=/usr/local"
     system "make"
   end
 
--- packages/zsh.rb
+++ packages/zsh.rb
@@ -1,18 +1,14 @@
 require 'package'
 
 class Zsh < Package
-  version '5.0.2'
-  source_url 'http://downloads.sourceforge.net/project/zsh/zsh/5.0.2/zsh-5.0.2.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fzsh%2Ffiles%2F&ts=1384135329&use_mirror=heanet'
-  source_sha1 '9f55ecaaae7cdc1495f91237ba2ec087777a4ad9'
-  binary_url ({
-    x86_64: 'https://dl.dropboxusercontent.com/s/0m8ms5b97t0r0s8/zsh-5.0.2-chromeos-x86_64.tar.gz?dl=1&token_hash=AAHiOSakPQiU2zhohXT1dVZyl6zphvESvADcrhyMomniAA'
-  })
-  binary_sha1 ({
-    x86_64: 'b9f87032e60cd04ce7e709a94f1cf5a8a71d0090'
-  })
+  version '5.0.7'
+  source_url "http://softlayer-dal.dl.sourceforge.net/project/zsh/zsh/5.0.7/zsh-5.0.7.tar.gz"
+  source_sha1 "a77519d3a6c251c69b1f4924cacdac17cc8e6a9d"
+
+  depends_on "ncurses"
 
   def self.build
-    system "./configure"
+    system "./configure --prefix=/usr/local"
     system "make"
   end
 
