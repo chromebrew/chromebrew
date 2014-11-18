d8244688d2a78cfd690ac65480422fbc0da448e4
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-11-18 09:18:32 -0500

    rename link

--- packages/jdk8.rb
+++ packages/jdk8.rb
@@ -3,7 +3,7 @@ require 'package'
 class Jdk8 < Package
   version '8.0.25'
   binary_url ({
-    x86_64: "https://www.dropbox.com/s/jgmiqjpripy7sf9/jdk1.8.0_25_package.tar.gz?dl=0"
+    x86_64: "https://www.dropbox.com/s/2zohwl033i6rol2/jdk1.8.0_25_x86_64.tar.gz?dl=0"
   })
   binary_sha1 ({
     x86_64: "601ab66006ade8bbac8dad465cf71aefe5266744"
