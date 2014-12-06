c15430aecbeac08d51faf8d4262a163f4e8be302
Jake Waksbaum <jake.waksbaum@gmail.com>
2014-12-06 00:11:36 -0500

    patchelf

---
+++ packages/patchelf.rb
@@ -0,0 +1,16 @@
+require 'package'
+
+class Patchelf < Package
+  version '0.8'
+  source_url 'http://nixos.org/releases/patchelf/patchelf-0.8/patchelf-0.8.tar.bz2'
+  source_sha1 'd0645e9cee6f8e583ae927311c7ce88d29f416fc'
+
+  def self.build
+    system "./configure", "prefix=/usr/local"
+    system "make"
+  end
+
+  def self.install
+    system "make", "PREFIX=#{CREW_DEST_DIR}/usr/local", "install"
+  end
+end
