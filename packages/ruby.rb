9d39bd9f19da5ebfe38148339ddc2dac14296f9d
Michał Siwek <mike21@aol.pl>
2015-01-10 13:12:36 +0100

    Fix ruby x86_64 binary link

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -4,7 +4,7 @@ class Ruby < Package
   version '2.0.0p247-chromeos1'
   binary_url ({
     i686: "https://dl.dropboxusercontent.com/s/tufbuqcn80ubypx/ruby-2.0.0p247-chromeos-i686.tar.gz?token_hash=AAGu_5wuqZe2eDOkfL5oh4esQ8HAZJIvbMG7GplnQrSa3g&dl=1",
-    x86_64: "https://dl.dropboxusercontent.com/s/bwb315aluunam6b/ruby-2.0.0-p247-chromeos1-chromeos-x86_86.tar.gz?token_hash=AAGYQhHhEpgAHCKtVVonVlXIJKZOWzq3arNsirxcRJQgLA&dl=1"
+    x86_64: "https://www.dropbox.com/s/x3jt0z5i1r4afyv/ruby-2.0.0p247-chromeos-x86_64.tar.gz?dl=1"
   })
   binary_sha1 ({
     i686: "49eeba5d542e4c3e6aa3686f215485e0946fb99a",
