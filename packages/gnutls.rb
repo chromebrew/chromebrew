69beebd85fca23d335759944559b4007450aa1d9
Tit Petric <black@scene-si.org>
2015-05-19 22:19:35 +0200

    gnutls import

---
+++ packages/gnutls.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Gnutls < Package
+	version '3.3.15'
+	source_url 'ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-3.3.15.tar.xz'
+	source_sha1 'd7f66b0aeaf48ff8621cc1913230635ef672f0a4'
+
+	depends_on 'buildessential'
+	depends_on 'nettle'
+
+	def self.build
+		system "./configure"
+		system "make"
+	end
+
+	def self.install
+		system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+	end
+end
