1be20ac0d775afecec5de8aa0acbfc4023fcaa4b
Tivs <tivoli@nurfed.com>
2015-06-08 18:27:42 -0400

    Update to version 2.2.1 and remove symlinks

--- packages/iojs.rb
+++ packages/iojs.rb
@@ -1,17 +1,13 @@
 require 'package'
 
 class Iojs < Package
-  version '2.1.0'
-  source_url 'https://iojs.org/dist/v2.1.0/iojs-v2.1.0.tar.gz'
-  source_sha1 '3236518f6e16334d0ed4c40b075625a0bb04086d'
+  version '2.2.1'
+  source_url 'https://iojs.org/dist/v2.2.1/iojs-v2.2.1.tar.gz'
+  source_sha1 '2b4a43cc1333526cbc5ac2e30d765a4d9c26427c'
 
   depends_on 'buildessential'
-  depends_on 'python27'
 
   def self.build
-    # These are needed for iojs to install
-    system "sudo ln -s /usr/local/bin/gcc /usr/local/bin/cc"
-
     system "./configure"
     system "make"
   end
