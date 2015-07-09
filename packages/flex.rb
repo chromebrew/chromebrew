8b40753479895ac243715c08f20a5af84793af3b
system_user <randre@gmail.com>
2015-07-09 10:17:59 -0700

    Added recipes for flex and bc

---
+++ packages/flex.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Flex < Package
+  version '2.5.39'
+  source_url 'http://fossies.org/linux/misc/flex-2.5.39.tar.gz'
+  source_sha1 '40866373afd3ab88a7f7fe26c9b27b035aa1f2a9'
+
+  depends_on 'm4'
+
+  def self.build
+    system "./configure --libdir=/usr/local/lib64/ CC=\"gcc -m64\" CFLAGS=\" -fPIC\""
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
