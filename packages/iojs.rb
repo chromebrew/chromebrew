f8c3f2c8abb745d924dc09c487316ac85742676a
Jonathan 'Tivs' Rainey <tivoli@nurfed.com>
2015-07-16 13:32:21 -0400

    Update iojs to v2.3.4 and fix issue skycocker/chromebrew#86 add python dependency and cc needs to be symlinked

--- packages/iojs.rb
+++ packages/iojs.rb
@@ -1,13 +1,17 @@
 require 'package'
 
 class Iojs < Package
-  version '2.2.1'
-  source_url 'https://iojs.org/dist/v2.2.1/iojs-v2.2.1.tar.gz'
-  source_sha1 '2b4a43cc1333526cbc5ac2e30d765a4d9c26427c'
+  version '2.3.4'
+  source_url 'https://iojs.org/dist/v2.3.4/iojs-v2.3.4.tar.gz'
+  source_sha1 '97639c34ebb951a4655ce8b903fa38a3f0135396'
 
   depends_on 'buildessential'
+  depends_on 'python27'
 
   def self.build
+    # These are needed for iojs to install
+    system "sudo ln -sf /usr/local/bin/gcc /usr/local/bin/cc"
+
     system "./configure"
     system "make"
   end
