2139902a1afcd5a8f1e3869fd9b2eb9f6649e041
Tit Petric <black@scene-si.org>
2015-07-26 10:06:16 +0200

    added missing dependency, can't install pkg-config without buildessential

--- packages/pkgconfig.rb
+++ packages/pkgconfig.rb
@@ -5,6 +5,8 @@ class Pkgconfig < Package
   source_url 'http://pkgconfig.freedesktop.org/releases/pkg-config-0.28.tar.gz'
   source_sha1 '71853779b12f958777bffcb8ca6d849b4d3bed46'
 
+  depends_on 'buildessential'
+
   def self.build
       system "./configure --with-internal-glib --libdir=/usr/local/lib64/ CC=\"gcc -m64\" CFLAGS=\" -fPIC\""
       system "make"
