b3e38d626ed379bdeeb2d6f2ff9b12d34b67c0b0
Jonathan 'Tivs' Rainey <tivoli@nurfed.com>
2015-08-14 16:22:40 -0400

    Upgrade postgres to v9.4.4 and add pg commands to PATH

--- packages/postgres.rb
+++ packages/postgres.rb
@@ -1,12 +1,12 @@
 require 'package'
 
 class Postgres < Package
-  version '9.3.2'
-  source_url 'https://ftp.postgresql.org/pub/source/v9.3.2/postgresql-9.3.2.tar.bz2'
-  source_sha1 'e1438f6cc8d387703dd1cca201316603266bd83a'
+  version '9.4.4'
+  source_url 'https://ftp.postgresql.org/pub/source/v9.4.4/postgresql-9.4.4.tar.bz2'
+  source_sha1 'e295fee0f1bace740b2db1eaa64ac060e277d5a7'
 
   depends_on 'buildessential'
- 
+
   def self.build
     system "./configure"
     system "make"
@@ -19,6 +19,7 @@ class Postgres < Package
     puts "------------------"
     puts "Installation success!"
     puts "To get started, you need to initialize a database directory"
+    puts "'echo \"export PATH=/usr/local/pgsql/bin:$PATH\" >> ~/.bashrc'"
     puts "'sudo chown chronos:chronos /usr/local/pgsql/data'"
     puts "'sudo chmod 700 /usr/local/pgsql/data'"
     puts "'initdb -D /usr/local/pgsql/data'"
