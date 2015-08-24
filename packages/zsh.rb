2c7cea0ce1bf3b4f9665aaaa58637b3366381d78
Michał Siwek <mike21@aol.pl>
2015-08-24 14:22:06 +0200

    Update zsh source url because of recent 404s
    Resolves #97

--- packages/zsh.rb
+++ packages/zsh.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Zsh < Package
   version '5.0.7'
-  source_url "http://softlayer-dal.dl.sourceforge.net/project/zsh/zsh/5.0.7/zsh-5.0.7.tar.gz"
+  source_url "http://sourceforge.net/projects/zsh/files/zsh/5.0.7/zsh-5.0.7.tar.gz/download"
   source_sha1 "a77519d3a6c251c69b1f4924cacdac17cc8e6a9d"
 
   depends_on "ncurses"
