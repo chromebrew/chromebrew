70e174889fe8bf435c14a2bfdbc59118a60d0918
Jonathan 'Tivs' Rainey <tivoli@nurfed.com>
2015-09-11 18:01:52 -0400

    Fixes skycocker/chromebrew#86, skycocker/chromebrew#84 Update nodejs and iojs to latest versions and set configure to use python2.7

--- packages/iojs.rb
+++ packages/iojs.rb
@@ -1,15 +1,15 @@
 require 'package'
 
 class Iojs < Package
-  version '3.0.0'
-  source_url 'https://iojs.org/dist/v3.0.0/iojs-v3.0.0.tar.gz'
-  source_sha1 'd5de35b35300b8602ebf6be06ebc0f97de4961f4'
+  version '3.3.0'
+  source_url 'https://iojs.org/dist/v3.3.0/iojs-v3.3.0.tar.gz'
+  source_sha1 '17c199be74fdcd3f44b70386fb903dc7fbd7402a'
 
   depends_on 'buildessential'
   depends_on 'python27'
 
   def self.build
-    system "CC='gcc' ./configure"
+    system "CC='gcc' python2.7 ./configure"
     system "make"
   end
 
--- packages/nodejs.rb
+++ packages/nodejs.rb
@@ -1,18 +1,15 @@
 require 'package'
 
 class Nodejs < Package
-  version '0.12.0'
-  source_url 'http://nodejs.org/dist/v0.12.0/node-v0.12.0.tar.gz'
-  source_sha1 'd08810034d170c19759cb38bfc9442ab6b0ebc7a'
+  version '4.0.0'
+  source_url 'https://nodejs.org/dist/v4.0.0/node-v4.0.0.tar.gz'
+  source_sha1 'd4791cb483492aea74574f49eee9d6949ecbc759'
 
   depends_on 'buildessential'
   depends_on 'python27'
 
   def self.build
-    # These are needed for node to install
-    system "sudo ln -s /usr/local/bin/gcc /usr/local/bin/cc"
-
-    system "./configure"
+    system "CC='gcc' python2.7 ./configure"
     system "make"
   end
 
