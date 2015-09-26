3d0318852350354b0f22104500b71dba891b3181
Michał Siwek <mike21@aol.pl>
2015-09-27 01:52:33 +0200

    Fix indentation of some package.rb files

--- packages/gnutls.rb
+++ packages/gnutls.rb
@@ -1,20 +1,20 @@
 require 'package'
 
 class Gnutls < Package
-	version '3.3.15'
-	source_url 'ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-3.3.15.tar.xz'
-	source_sha1 'd7f66b0aeaf48ff8621cc1913230635ef672f0a4'
+  version '3.3.15'
+  source_url 'ftp://ftp.gnutls.org/gcrypt/gnutls/v3.3/gnutls-3.3.15.tar.xz'
+  source_sha1 'd7f66b0aeaf48ff8621cc1913230635ef672f0a4'
 
-	depends_on 'buildessential'
-	depends_on 'nettle'
-	depends_on 'pkgconfig'
+  depends_on 'buildessential'
+  depends_on 'nettle'
+  depends_on 'pkgconfig'
 
-	def self.build
-		system "./configure"
-		system "make"
-	end
+  def self.build
+    system "./configure"
+    system "make"
+  end
 
-	def self.install
-		system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-	end
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
 end
--- packages/nettle.rb
+++ packages/nettle.rb
@@ -1,19 +1,19 @@
 require 'package'
 
 class Nettle < Package
-	version '2.7.1'
-	source_url 'https://ftp.gnu.org/gnu/nettle/nettle-2.7.1.tar.gz'
-	source_sha1 'e7477df5f66e650c4c4738ec8e01c2efdb5d1211'
+  version '2.7.1'
+  source_url 'https://ftp.gnu.org/gnu/nettle/nettle-2.7.1.tar.gz'
+  source_sha1 'e7477df5f66e650c4c4738ec8e01c2efdb5d1211'
 
-	depends_on 'buildessential'
-	depends_on 'm4'
+  depends_on 'buildessential'
+  depends_on 'm4'
 
-	def self.build
-		system "./configure --libdir=/usr/local/lib"
-		system "make"
-	end
+  def self.build
+    system "./configure --libdir=/usr/local/lib"
+    system "make"
+  end
 
-	def self.install
-		system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-	end
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
 end
