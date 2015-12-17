d22e4c9e70086c87a7dee03ec66e8b44e2e5d5ee
Jonathan 'Tivs' Rainey <tivoli@nurfed.com>
2015-12-17 10:21:05 -0500

    Update ruby to version 2.2.4

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -1,16 +1,19 @@
 require 'package'
 
 class Ruby < Package
-  version '2.0.0p247-chromeos1'
-  binary_url ({
-    i686: "https://dl.dropboxusercontent.com/s/tufbuqcn80ubypx/ruby-2.0.0p247-chromeos-i686.tar.gz?token_hash=AAGu_5wuqZe2eDOkfL5oh4esQ8HAZJIvbMG7GplnQrSa3g&dl=1",
-    x86_64: "https://www.dropbox.com/s/x3jt0z5i1r4afyv/ruby-2.0.0p247-chromeos-x86_64.tar.gz?dl=1"
-  })
-  binary_sha1 ({
-    i686: "49eeba5d542e4c3e6aa3686f215485e0946fb99a",
-    x86_64: "ec4d9c9d1a993ca03d0dee594c79ab1945e5b88d"
-  })
+  version '2.2.4'
+  source_url 'https://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.4.tar.gz'
+  source_sha1 '818e5e157f76d4912ba3a7c7b4fc5156105e83c3'
 
   depends_on 'readline'
   depends_on 'zlibpkg'
+
+  def self.build
+    system "CC='gcc' ./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
 end
