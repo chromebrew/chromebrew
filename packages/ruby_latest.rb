a32c6ff417f79494ab08dcf622593cd598c62067
Jonathan 'Tivs' Rainey <tivoli@nurfed.com>
2016-01-07 11:57:00 -0500

    Fix ruby latest naming and filename

--- packages/ruby_latest.rb
+++ packages/ruby_latest.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class RubyLatest < Package
+class Ruby_latest < Package
   version '2.2.4'
   source_url 'https://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.4.tar.gz'
   source_sha1 '818e5e157f76d4912ba3a7c7b4fc5156105e83c3'
