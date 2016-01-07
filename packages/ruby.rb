d6e4f6a1de97e7cb53feeca419280c33b170241c
Damian Montero <photoca@hotmail.com>
2016-01-07 12:24:05 -0500

    Fixed Sha1 signature for Ruby Package x64 package

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -3,7 +3,7 @@ require 'package'
 class Ruby < Package
   version '2.0.0p247-chromeos1'
   binary_url ({
-    i686: "https://dl.dropboxusercontent.com/s/tufbuqcn80ubypx/ruby-2.0.0p247-chromeos-i686.tar.gz?token_hash=AAGu_5wuqZe2eDOkfL5oh4esQ8HAZJIvbMG7GplnQrSa3g&dl=1",
+    i686: "https://dl.dropboxusercontent.com/s/tufbuqcn80ubypx/ruby-2.0.0p247-chromeos-i686.tar.gz&dl=1",
     x86_64: "https://www.dropbox.com/s/x3jt0z5i1r4afyv/ruby-2.0.0p247-chromeos-x86_64.tar.gz?dl=1"
   })
   binary_sha1 ({
