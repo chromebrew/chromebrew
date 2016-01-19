5aee56a854a1e3e20a63767a108dc1a203a17e0c
Damian Montero <photoca@hotmail.com>
2016-01-19 10:43:07 -0500

    Fixed JDK8 download url

--- packages/jdk8.rb
+++ packages/jdk8.rb
@@ -4,7 +4,7 @@ class Jdk8 < Package
   version '8.0.25'
   binary_url ({
     i686: "https://www.dropbox.com/s/d1omw087ilkh5oq/jdk1.8.0_25_i686.tar.gz?dl=0",
-    x86_64: "https://www.dropbox.com/s/my0ztzdngrjbsfh/jdk1.8.0_65_x86_64.tar.gz?dl=0"
+    x86_64: "https://www.dropbox.com/s/ykw2jt797cpghfy/jdk1.8.0_65_x86_64.tar.gz?dl=0"
   })
   binary_sha1 ({
     i686: "2f6fabc6e7b86fa2f21d19f9617d2641c5862a30",
