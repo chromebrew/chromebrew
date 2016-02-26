571d481bec84a1c17c3857bc25cacee25f13f6be
Richard Fisher <richardfsr@gmail.com>
2016-02-26 21:58:01 +0800

    add pkg-config dependency

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -4,6 +4,8 @@ class Imagemagick < Package
   version '6.9.3-5'
   source_url 'http://www.imagemagick.org/download/releases/ImageMagick-6.9.3-5.tar.xz'
   source_sha1 'aa42c3a08eb807a4da9c117f8640f9239617ce76'
+  
+  depends_on 'pkgconfig'
 
   def self.build
     system "./configure --libdir=/usr/local/lib#{SHORTARCH}/ CC=\"gcc -m#{SHORTARCH}\" CFLAGS=\" -fPIC\" --without-python"
