cf2a338e3fe7343430fddd9491b5d79133b1efe5
Richard Fisher <richardfsr@gmail.com>
2016-02-26 21:37:21 +0800

    fix class name

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class ImageMagick < Package
+class Imagemagick < Package
   version '6.9.3-0'
   source_url 'http://www.imagemagick.org/download/releases/ImageMagick-6.9.3-0.tar.xz'
   source_sha1 '2ba0656eb03d72d8a44e741ead524e8c34097418c0bb5487a5c4f4fe5eca9656'
