7b1af2f7bd2b86fc72c7a185bded7b359873b774
Richard Fisher <richardfsr@gmail.com>
2016-02-26 21:35:59 +0800

    add imagemagick package

---
+++ packages/imagemagick.rb
@@ -0,0 +1,16 @@
+require 'package'
+
+class ImageMagick < Package
+  version '6.9.3-0'
+  source_url 'http://www.imagemagick.org/download/releases/ImageMagick-6.9.3-0.tar.xz'
+  source_sha1 '2ba0656eb03d72d8a44e741ead524e8c34097418c0bb5487a5c4f4fe5eca9656'
+
+  def self.build
+    system "./configure --libdir=/usr/local/lib#{SHORTARCH}/ CC=\"gcc -m#{SHORTARCH}\" CFLAGS=\" -fPIC\" --without-python"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
