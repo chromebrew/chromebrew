a6851ae558ad3146780809574f6760b16375d9db
Richard Fisher <richardfsr@gmail.com>
2016-03-08 18:32:11 +0800

    add openssh this is necessary as it needs to be built against the version of openssl included in chromebrew otherwise you get an openssl version missmatch when running ssh after installing the crew openssl package

---
+++ packages/openssh.rb
@@ -0,0 +1,15 @@
+require 'package'
+
+class Openssh < Package
+  version '7.2p1'
+  source_url 'http://mirror.internode.on.net/pub/OpenBSD/OpenSSH/portable/openssh-7.2p1.tar.gz'
+  source_sha1 'd30a6fd472199ab5838a7668c0c5fd885fb8d371'
+
+  def self.build
+    system "CC=\"gcc -m#{SHORTARCH}\" CFLAGS=\" -fPIC\" make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
