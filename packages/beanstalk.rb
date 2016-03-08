532ea87fa9887d1bd300a9e1fa5ed33bcce4cbd4
Richard Fisher <richardfsr@gmail.com>
2016-03-08 18:11:20 +0800

    add beanstalkd

---
+++ packages/beanstalk.rb
@@ -0,0 +1,15 @@
+require 'package'
+
+class Beanstalk < Package
+  version '1.10'
+  source_url 'https://github.com/kr/beanstalkd/archive/v1.10.tar.gz'
+  source_sha1 'bfc0ccf99e15b15eac03ec1d8a57a3aaff143237'
+
+  def self.build
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
