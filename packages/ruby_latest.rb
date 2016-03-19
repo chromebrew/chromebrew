7205857b77c6b660c89b3a00d4f84d84c0dbe553
Richard Fisher <richardfsr@gmail.com>
2016-03-19 12:07:05 +0800

    require openssl when building ruby otherwise ruby won't be able to create secure connections

--- packages/ruby_latest.rb
+++ packages/ruby_latest.rb
@@ -5,11 +5,12 @@ class Ruby_latest < Package
   source_url 'https://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.4.tar.gz'
   source_sha1 '818e5e157f76d4912ba3a7c7b4fc5156105e83c3'
 
+  depends_on 'openssl'
   depends_on 'readline'
   depends_on 'zlibpkg'
 
   def self.build
-    system "./configure"
+    system "./configure --with-openssl-dir=#{CREW_PREFIX}/ssl"
     system "CC=\"gcc -m#{SHORTARCH}\" CFLAGS=\" -fPIC\" make"
   end
 
