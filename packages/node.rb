3c35a37cf657252c2897ae0660fb7ef16e5d7e84
Damian Montero <photoca@hotmail.com>
2016-05-27 10:44:00 -0400

    Fixing Node permissions for installing -g global packages

--- packages/node.rb
+++ packages/node.rb
@@ -15,5 +15,7 @@ class Node < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    #Fix Permissiongs for -g installs of node packages
+    system "sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}"
   end
 end
