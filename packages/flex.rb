af6f6049f1bb9cbcbe160b3c75acc843d138fa82
Bruno Fosados <bruno.fosados@gmail.com>
2016-06-01 17:46:07 -0500

    Fix source_url and update sha1 sum of new file

--- packages/flex.rb
+++ packages/flex.rb
@@ -2,8 +2,8 @@ require 'package'
 
 class Flex < Package
   version '2.5.39'
-  source_url 'http://fossies.org/linux/misc/flex-2.5.39.tar.gz'
-  source_sha1 '40866373afd3ab88a7f7fe26c9b27b035aa1f2a9'
+  source_url 'http://pkgs.fedoraproject.org/repo/pkgs/flex/flex-2.5.39.tar.bz2/77d44c6bb8c0705e0017ab9a84a1502b/flex-2.5.39.tar.bz2'
+  source_sha1 '267794c709d5c50f2dcb48ff5d8dcbbfe40f953e'
 
   depends_on 'm4'
 
