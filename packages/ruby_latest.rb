328fcbb7345cd9402635b41e418b314b00deb665
Brendan Morris <morris.brendan.o@gmail.com>
2016-07-08 13:49:21 -0400

    Updated to latest stable version 2.3.1

--- packages/ruby_latest.rb
+++ packages/ruby_latest.rb
@@ -1,9 +1,9 @@
 require 'package'
 
 class Ruby_latest < Package
-  version '2.2.4'
-  source_url 'https://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.4.tar.gz'
-  source_sha1 '818e5e157f76d4912ba3a7c7b4fc5156105e83c3'
+  version '2.3.1'
+  source_url 'https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz'
+  source_sha256 'b87c738cb2032bf4920fef8e3864dc5cf8eae9d89d8d523ce0236945c5797dcd'
 
   depends_on 'readline'
   depends_on 'zlibpkg'
