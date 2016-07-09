3c944f9ac57ce1ccc5b2e134af728319ab64d379
Brendan Morris <morris.brendan.o@gmail.com>
2016-07-08 22:06:28 -0400

    Update ruby_latest.rb

--- packages/ruby_latest.rb
+++ packages/ruby_latest.rb
@@ -3,7 +3,7 @@ require 'package'
 class Ruby_latest < Package
   version '2.3.1'
   source_url 'https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz'
-  source_sha256 'b87c738cb2032bf4920fef8e3864dc5cf8eae9d89d8d523ce0236945c5797dcd'
+  source_sha2 'b87c738cb2032bf4920fef8e3864dc5cf8eae9d89d8d523ce0236945c5797dcd'
 
   depends_on 'readline'
   depends_on 'zlibpkg'
