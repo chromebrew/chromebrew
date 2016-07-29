ef1efd3a61be1a46c05184d08f9a9ed883464793
Damian Montero <thedamian@users.noreply.github.com>
2016-07-29 13:07:05 -0400

    Update node_current.rb

--- packages/node_current.rb
+++ packages/node_current.rb
@@ -1,14 +1,12 @@
 require 'package'
 
 class Node_current < Package
-  version '6.3.1'
-  source_url 'https://nodejs.org/dist/v6.3.1/node-v6.3.1.tar.xz'
-  source_sha1 '7021da27637f0e099598f0a7e8ddaf07e8bb3a69'
+  version '6.2.2'
+  source_url 'https://nodejs.org/dist/v6.2.2/node-v6.2.2.tar.xz'
+  source_sha1 '5014800813fa3682b8053637518b37a119c26e93'
 
   depends_on 'buildessential'
   depends_on 'python27'
-
-  depends_on 'openssl_devel'
 
   def self.build
     system "CC='gcc' python2.7 ./configure --without-snapshot"
@@ -18,7 +16,6 @@ class Node_current < Package
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
     #Fix Permissiongs for -g installs of node packages
-    system "echo 'Now to get npm -g working you should type this'" 
-    system "echo 'sudo chown -R chronos /usr/local/lib/node_modules'"
+    system "sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}"
   end
 end
