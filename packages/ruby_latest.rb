42479968c34e1f4f8aa375e04cbf036b199a99fc
system_user <photoca@hotmail.com>
2016-07-29 13:29:42 -0400

    cleaning this pull request

---
+++ packages/ruby_latest.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Ruby_latest < Package
+  version '2.2.4'
+  source_url 'https://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.4.tar.gz'
+  source_sha1 '818e5e157f76d4912ba3a7c7b4fc5156105e83c3'
+
+  depends_on 'readline'
+  depends_on 'zlibpkg'
+
+  def self.build
+    system "CC='gcc' ./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
