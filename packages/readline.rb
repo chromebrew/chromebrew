58279643264bb90ae387072b7b3326d375a75737
system_user <photoca@hotmail.com>
2016-07-29 12:55:20 -0400

    New branch wit just node updates

--- packages/readline.rb
+++ packages/readline.rb
@@ -1,14 +1,19 @@
 require 'package'
 
 class Readline < Package
-  version '6.3'
-  source_url 'http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz'
-  source_sha1 '017b92dc7fd4e636a2b5c9265a77ccc05798c9e1'
+  version '6.2'
 
-  depends_on 'buildessential'
+  binary_url({
+    x86_64: "https://dl.dropboxusercontent.com/s/v8xva6e4u3tvkaw/readline-6.2-chromeos-x86_64.tar.gz?token_hash=AAHOv1kLiv7xzDkYEZCSB_3ShK0n_7B4m2dNGHPnn2fZIg&dl=1"
+  })
+  binary_sha1({
+    x86_64: '621c2538b452be33cca507110ccfd4144c61edd2'
+  })
+  
+  depends_on 'buildessential' #until binary for i686 provided
 
   def self.build
-    system "CC='gcc' ./configure"
+    system "./configure"
     system "make"
   end
 
