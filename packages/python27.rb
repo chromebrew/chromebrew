76386a171f7671c2cfb530cd42a56981c1b24d1c
system_user <photoca@hotmail.com>
2016-07-29 13:23:58 -0400

    had deleted python27 by mistake

---
+++ packages/python27.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Python27 < Package
+  version '2.7.10'
+  source_url 'https://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz' # software source tarball url
+  source_sha1 '49089f1f6ab445dc8ace97beeb276095c4c8039b'                  # source tarball sha1 sum
+
+  depends_on 'bz2'
+  depends_on 'ncurses'
+
+  def self.build                                                  # self.build contains commands needed to build the software from source
+    system "./configure --prefix=/usr/local CPPFLAGS=\"-I/usr/local/include -I/usr/local/include/ncurses\" LDFLAGS=\"-L/usr/local/lib\" CFLAGS=\" -fPIC\""
+    system "make"                                                 # ordered chronologically
+  end
+
+  def self.install                                                # self.install contains commands needed to install the software on the target system
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"          # remember to include DESTDIR set to CREW_DEST_DIR - needed to keep track of changes made to system
+  end
+end
