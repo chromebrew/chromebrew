cdfb7c9e5959f801d9ac79459b956dd041f7462d
0x5c <winhelp32.exe@gmail.com>
2016-10-09 03:21:32 -0400

    Added package file-5.28

---
+++ packages/file.rb
@@ -0,0 +1,16 @@
+require 'package'
+
+class file < Package
+  version '5.28'
+  source_url 'ftp://ftp.astron.com/pub/file/file-5.28.tar.gz'
+  source_sha1 '1b789a93bea54da3119d98986468cd90b1e571bf'
+  
+  def self.build
+    system "./configure --prefix=/usr/local"
+    system "make"
+  end
+  
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
