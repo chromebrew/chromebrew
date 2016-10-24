7718b96cb061e40ed02083b8cafbc09b85be192e
Karthik Sandeep Vempati <zelectrokarztm@gmail.com>
2016-10-24 08:11:16 +0530

    Update and rename PythonBeta.rb to Python3Beta.rb

--- packages/Python3Beta.rb
+++ packages/Python3Beta.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class PythonBeta < Package
+class Python3Beta < Package
   version '3.6.0b2'
   source_url 'https://www.python.org/ftp/python/3.6.0/Python-3.6.0b2.tgz'     # software source tarball url
   source_sha1 'e2ec7f5ae442fc0e918C55d28868a14bc662c078'          	# source tarball sha1 sum
@@ -14,4 +14,3 @@ class PythonBeta < Package
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"          # remember to include DESTDIR set to CREW_DEST_DIR - needed to keep track of changes made to system
   end         
 end
-Contact GitHub API Training Shop Blog About
