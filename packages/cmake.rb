2338f0a4a2bb1e4a54e8cb470f9ddf124b56a3f1
system_user <photoca@hotmail.com>
2016-11-25 00:28:54 -0500

    adding cmake, libunwind, lldb, llvm and pkg-config to make dot.net CORE work

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -1,12 +1,15 @@
 require 'package'
 
 class Cmake < Package
-  version '3.0.2'
-  source_url 'http://www.cmake.org/files/v3.0/cmake-3.0.2.tar.gz'
-  source_sha1 '379472e3578902a1d6f8b68a9987773151d6f21a'
+  version '3.6.1'
+  source_url 'https://cmake.org/files/v3.6/cmake-3.6.1.tar.gz'
+  source_sha1 'a37785b3f256a31ee21a047569bc74a8f57067bb'
+
+  depends_on 'buildessential'
+  depends_on 'openssl_devel'
 
   def self.build
-    system "./configure --prefix=/usr/local"
+    system "./bootstrap"
     system "make"
   end
 
---
+++ packages/libunwind.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Libunwind < Package
+  version '1.1'
+  source_url 'http://download.savannah.gnu.org/releases/libunwind/libunwind-1.1.tar.gz'
+  source_sha1 '7c93a6ae35afff4d0efd143b62a9fd235423964f'
+
+  depends_on 'buildessential'
+  depends_on 'openssl_devel'
+
+  def self.build
+    system "CC='gcc' ./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
