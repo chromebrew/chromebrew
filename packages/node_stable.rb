bd01bc2b2894c59087345cf2eadc736bbbc95a58
Ben Fayers <bfayers@users.noreply.github.com>
2016-12-10 07:48:54 +0000

    Fix SHA1 Signature.

--- packages/node_stable.rb
+++ packages/node_stable.rb
@@ -3,7 +3,7 @@ require 'package'
 class Node_stable < Package
   version '6.2.2'
   source_url 'https://nodejs.org/dist/v6.2.2/node-v6.2.2.tar.xz'
-  source_sha1 '5014800813FA3682B8053637518B37A119C26E93'
+  source_sha1 '5014800813fa3682b8053637518b37a119c26e93'
 
   depends_on 'buildessential'
   depends_on 'python27'
