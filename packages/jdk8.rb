d10e7aa5f2769ab3adfef324d706ba8e2bd9469f
Ben Fayers <bfayers@users.noreply.github.com>
2016-12-12 00:35:52 +0000

    Change URLs because name change.

--- packages/jdk8.rb
+++ packages/jdk8.rb
@@ -3,8 +3,8 @@ require 'package'
 class Jdk8 < Package
   version '1.8.0-112'
   binary_url ({
-    i686: "https://dl.dropboxusercontent.com/u/14799278/crew/jdk1.8.0_112-i686.tar.gz",
-    x86_64: "https://dl.dropboxusercontent.com/u/14799278/crew/jdk1.8.0_112-x86_64.tar.gz"
+    i686: "https://dl.dropboxusercontent.com/u/14799278/crew/jdk8u112-i686.tar.gz",
+    x86_64: "https://dl.dropboxusercontent.com/u/14799278/crew/jdk8u112-x86_64.tar.gz"
   })
   binary_sha1 ({
     i686: "65de377470bdd42e4f3e158b16917166ebe47a02",
