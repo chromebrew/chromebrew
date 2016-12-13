4bf1eedfa637855ee717df381bcac89cac58d8c8
Damian Montero <thedamian@users.noreply.github.com>
2016-12-12 22:27:32 -0500

    openssl_devel requirement for the missing SSL in python

--- packages/python27.rb
+++ packages/python27.rb
@@ -2,11 +2,12 @@ require 'package'
 
 class Python27 < Package
   version '2.7.12'
-  source_url 'https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz' # software source tarball url
-  source_sha1 '1e80b781eacc6b7e243bd277e5002426aa56d0f1'                  # source tarball sha1 sum
+  source_url 'https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tar.xz' # software source tarball url
+  source_sha1 '05360b8ade117b35e266b2004a7f1f11250c6dcd'                     # source tarball sha1 sum
 
   depends_on 'bz2'
   depends_on 'ncurses'
+  depends_on 'openssl_devel'
 
   def self.build                                                  # self.build contains commands needed to build the software from source
     system "./configure --prefix=/usr/local CPPFLAGS=\"-I/usr/local/include -I/usr/local/include/ncurses\" LDFLAGS=\"-L/usr/local/lib\" CFLAGS=\" -fPIC\""
