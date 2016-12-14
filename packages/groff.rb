2a693270dc28cd91f0b0a9cbf9836928a1199662
lyxell <alyxell@kth.se>
2016-12-14 12:23:53 +0000

    Add mandb and dependecies

---
+++ packages/groff.rb
@@ -0,0 +1,16 @@
+require 'package'
+ 
+class Groff < Package
+  version '1.22.3'
+  source_url 'http://ftp.gnu.org/gnu/groff/groff-1.22.3.tar.gz'
+  source_sha1 '61a6808ea1ef715df9fa8e9b424e1f6b9fa8c091'
+  
+  def self.build
+    system './configure'
+    system 'make'
+  end
+  
+  def self.install
+    system 'make', 'install'
+  end
+end
---
+++ packages/libpipeline.rb
@@ -0,0 +1,16 @@
+require 'package'
+ 
+class Libpipeline < Package
+  version '1.4.1'
+  source_url 'https://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.4.1.tar.gz'
+  source_sha1 'b31cc955f22b1aa4545dc8d00ddbde831936594f'
+  
+  def self.build
+    system './configure'
+    system 'make'
+  end
+  
+  def self.install
+    system 'make install'
+  end
+end
