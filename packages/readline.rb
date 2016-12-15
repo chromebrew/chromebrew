eb65f95771927c941778465174ee46ad1ddd034d
Damian Montero <thedamian@users.noreply.github.com>
2016-12-14 23:11:37 -0500

    readline to 6.3 - also build from source

--- packages/readline.rb
+++ packages/readline.rb
@@ -1,22 +1,14 @@
 require 'package'
 
 class Readline < Package
-  version '6.2'
+  version '6.3'
+  source_url 'http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz'
+  source_sha1 '017b92dc7fd4e636a2b5c9265a77ccc05798c9e1'
 
-  # arm has 6.3 readline library in system, so leave it as is
-  binary_url({
-    armv7l: "https://dl.dropboxusercontent.com/s/kut6emhlda9pbc9/dummy-1.0.0-chromeos-armv7l.tar.gz",
-    x86_64: "https://dl.dropboxusercontent.com/s/v8xva6e4u3tvkaw/readline-6.2-chromeos-x86_64.tar.gz?token_hash=AAHOv1kLiv7xzDkYEZCSB_3ShK0n_7B4m2dNGHPnn2fZIg&dl=1"
-  })
-  binary_sha1({
-    armv7l: "049db60338a74d798e72afabe05097f3a4c4f7cd",
-    x86_64: '621c2538b452be33cca507110ccfd4144c61edd2'
-  })
-  
-  depends_on 'buildessential' #until binary for i686 provided
+  depends_on 'buildessential'
 
   def self.build
-    system "./configure"
+    system "CC='gcc' ./configure"
     system "make"
   end
 
