8c924d23c99039c43e04d2407e9ecd810da3579f
Damian Montero <photoca@hotmail.com>
2016-12-16 09:57:31 -0500

    bringing back the arm binary

--- packages/readline.rb
+++ packages/readline.rb
@@ -1,20 +1,22 @@
 require 'package'
 
 class Readline < Package
-  version '6.2'
+  version '6.3'
 
-  binary_url({
-    x86_64: "https://dl.dropboxusercontent.com/s/v8xva6e4u3tvkaw/readline-6.2-chromeos-x86_64.tar.gz?token_hash=AAHOv1kLiv7xzDkYEZCSB_3ShK0n_7B4m2dNGHPnn2fZIg&dl=1"
-  })
-  binary_sha1({
-    x86_64: '621c2538b452be33cca507110ccfd4144c61edd2'
-  })
+  binary_url({		
+     armv7l: "https://dl.dropboxusercontent.com/s/kut6emhlda9pbc9/dummy-1.0.0-chromeos-armv7l.tar.gz",		
+     x86_64: "http://ftp.gnu.org/gnu/readline/readline-6.3.tar.gz"		
+   })		
+   binary_sha1({		
+     armv7l: "049db60338a74d798e72afabe05097f3a4c4f7cd",		
+     x86_64: '017b92dc7fd4e636a2b5c9265a77ccc05798c9e1'		
+   })
   
   depends_on 'buildessential' #until binary for i686 provided
 
   def self.build
     system "./configure"
-    system "make"
+    system "CC='gcc' make"
   end
 
   def self.install
