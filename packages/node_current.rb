2e80a6be9867ff3701a40f4da3f251f4e287e62c
Damian Montero <thedamian@users.noreply.github.com>
2016-12-15 22:52:19 -0500

    having the user run a line we cannot

--- packages/node_current.rb
+++ packages/node_current.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Node_current < Package
-  version '6.2.2'
+  version '6.9.2'
   source_url 'https://nodejs.org/dist/v6.9.2/node-v6.9.2.tar.gz'
   source_sha1 '6b33b9fac28785b8317dfa5f2402aff2df3e2d5a'
 
@@ -16,6 +16,11 @@ class Node_current < Package
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
     #Fix Permissiongs for -g installs of node packages
-    system "sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}"
+    echo "************************************************************"
+    echo "Now run:"
+    echo "sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}"
+    echo "To be able to do"
+    echo "npm install -g"
+    echo "without being SUDO"
   end
 end
