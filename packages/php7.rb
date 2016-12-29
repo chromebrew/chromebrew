3186c90a7f87af30c0558e8f21ebc041d4a58311
Ed Reel <edreel@gmail.com>
2016-12-28 23:45:25 -0600

    Remove nginx dependency and change to version 7.1.0

--- packages/php7.rb
+++ packages/php7.rb
@@ -1,13 +1,12 @@
 require 'package'
 
 class Php7 < Package
-  version '7.0.14'
-  source_url 'http://php.net/get/php-7.0.14.tar.gz/from/this/mirror' # software source tarball url
-  source_sha1 '7d098167c63446011e87b1d1578f2f3ac0bab01a'  # source tarball sha1 sum
-  
-  depends_on 'nginx'
+  version '7.1.0'
+  source_url 'http://php.net/distributions/php-7.1.0.tar.xz' # software source tarball url
+  source_sha1 'c74c920256b9c6873bae696fbb0ec14a02dc8495'  # source tarball sha1 sum
+
   depends_on 'libxml2'
-  
+
   def self.build                                                  # self.build contains commands needed to build the software from source
     system "./configure"
     system "make"                                                 # ordered chronologically
