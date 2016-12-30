7644cc0468f92acaf85179e99e2c62f42eac3c4e
Casey Strouse <casey.strouse@gmail.com>
2016-12-30 07:56:15 -0700

    Fixing class name to match filename

--- packages/netcat.rb
+++ packages/netcat.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class Nc < Package
+class Netcat < Package
   version '0.7.1'
   source_url 'http://downloads.sourceforge.net/project/netcat/netcat/0.7.1/netcat-0.7.1.tar.gz'
   source_sha1 'b5cbc52a7ceed2fd5c4f5081f5747130b2d0fe01'
