df1e31e971cc0c634c10927da22233b320f9cf08
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-01-04 03:11:07 +0900

    Increment version number

--- packages/mono.rb
+++ packages/mono.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Mono < Package
-  version '4.4.0.148'
+  version '4.4.0.148-1'
   source_url 'http://download.mono-project.com/sources/mono/mono-4.4.0.148.tar.bz2'
   source_sha1 '8da7726b7c09df97856b55eda062356666928d35'
 
