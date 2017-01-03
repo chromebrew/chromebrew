83bfe0bbe1d7ed8324363c7b427dc0630cc13b41
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-01-04 02:05:18 +0900

    Increments version number

--- packages/libffi.rb
+++ packages/libffi.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Libffi < Package
-  version '3.0.13'
+  version '3.0.13-1'
   source_url 'ftp://sourceware.org/pub/libffi/libffi-3.0.13.tar.gz'
   source_sha1 'f5230890dc0be42fb5c58fbf793da253155de106'
 
