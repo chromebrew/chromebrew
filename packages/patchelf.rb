5dcdedc192649706591934bdba2be395877ffd32
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-01-06 01:30:40 +0900

    Fix CREW_DEST_DIR usage again

--- packages/patchelf.rb
+++ packages/patchelf.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Patchelf < Package
-  version '0.8-1'
+  version '0.8-2'
   source_url 'http://nixos.org/releases/patchelf/patchelf-0.8/patchelf-0.8.tar.bz2'
   source_sha1 'd0645e9cee6f8e583ae927311c7ce88d29f416fc'
 
@@ -11,6 +11,6 @@ class Patchelf < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}/usr/local", "install"
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
 end
