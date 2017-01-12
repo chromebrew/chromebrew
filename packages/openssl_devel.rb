506d57e4d030f6a5ce539e356ff127bee2abaad8
Damian Montero <thedamian@users.noreply.github.com>
2017-01-11 19:33:40 -0500

    Update openssl_devel.rb

--- packages/openssl_devel.rb
+++ packages/openssl_devel.rb
@@ -5,14 +5,10 @@ require 'package'
 # place holder here.  PH needs to have source_url unfortunately.
 
 class Openssl_devel < Package
+  version 'removed'
 
-  version '1.0.2j'
-
-#  chromeos wget can't do proper ssl negotiation with this server
-#  source_url 'https://www.openssl.org/source/openssl-1.0.2j.tar.gz'
-#  so use their ftp server.  
-  source_url 'ftp://ftp.openssl.org/source/openssl-1.0.2j.tar.gz'
-  source_sha1 'bdfbdb416942f666865fa48fe13c2d0e588df54f'
+  source_url 'ftp://ftp.openssl.org/source/old/1.0.2/openssl-1.0.2f.tar.gz'
+  source_sha1 '2047c592a6e5a42bd37970bdb4a931428110a927'
 
   def self.build
   end
