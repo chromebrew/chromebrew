119eae004a48f4418aa7d010ab8a0c04d11dbde3
Damian Montero <photoca@hotmail.com>
2017-01-13 10:36:20 -0500

    Node to 6.9.4 and node_current to 7.4.0, Node_stable deleted

--- packages/node_current.rb
+++ packages/node_current.rb
@@ -1,9 +1,9 @@
 require 'package'
 
 class Node_current < Package
-  version '6.9.2'
-  source_url 'https://nodejs.org/dist/v6.9.2/node-v6.9.2.tar.gz'
-  source_sha1 '6b33b9fac28785b8317dfa5f2402aff2df3e2d5a'
+  version '7.4.0'
+  source_url 'https://nodejs.org/dist/v7.4.0/node-v7.4.0.tar.gz'
+  source_sha1 'e31d4d67a6f39ccce2db66dcfa0e6c11851c1375'
 
   depends_on 'buildessential'
   depends_on 'python27'
