5db956e4ae79fc25e0c72292d2de39e768c66d9a
Ben Fayers <bfayers@users.noreply.github.com>
2017-01-21 08:57:02 +0000

    IF installing over an old install this makes it actually work.
    Otherwise you get errors like "Read error at byte 692/690 ((null)). Retrying."

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ echo '}' >> device.json
 #download git and its dependencies .rb package files
 cd $CREW_PACKAGES_PATH
 for file in git zlibpkg libssh2 perl curl expat gettext python readline ruby buildessential gcc binutils make mpc mpfr gmp glibc linuxheaders; do
-  wget -N -c $URL/packages/$file.rb
+  wget -O$file.rb $URL/packages/$file.rb
 done
 
 #install readline for ruby
