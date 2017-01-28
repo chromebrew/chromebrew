1cb9e7442c4b790d62d1e35a13bddde686a50145
Ed Reel <edreel@gmail.com>
2017-01-27 21:23:00 -0600

    Rename glib2 dependency to glib

--- packages/sshfs.rb
+++ packages/sshfs.rb
@@ -5,7 +5,7 @@ class Sshfs < Package
   source_url 'https://github.com/libfuse/sshfs/releases/download/sshfs_2.8/sshfs-2.8.tar.gz'
   source_sha1 '2b792aa5b3a45e0c3fe65c44bd9da8f64a690830'
 
-  depends_on 'glib2'
+  depends_on 'glib'
   depends_on 'fuse'
 
   def self.build
