009846ad2e99ebe09bbf7bd99d32d489ce8023d6
Casey Strouse <casey.strouse@gmail.com>
2017-01-30 05:04:29 -0700

    Update diffutils to 3.5
    This is a general bugfix/maintenance release. The makefile modifications
    are no longer necessary and have been removed.
    
    Tested as working on Samsung XE50013-K01US.
    
    All tests passing.
    https://gist.github.com/cstrouse/58a6eb6c5c93231db5b4e113497d7d53

--- packages/diffutils.rb
+++ packages/diffutils.rb
@@ -1,16 +1,13 @@
 require 'package'
 
 class Diffutils < Package
-  version '3.3'
-  source_url 'ftp://ftp.gnu.org/gnu/diffutils/diffutils-3.3.tar.xz'
-  source_sha1 '6463cce7d3eb73489996baefd0e4425928ecd61e'
+  version '3.5'
+  source_url 'ftp://ftp.gnu.org/gnu/diffutils/diffutils-3.5.tar.xz'
+  source_sha1 '1169cce8eaaf7290dc087d65db7ed75de0dceb93'
 
   depends_on "libsigsegv"
 
   def self.build
-    system "sed -i -e '/gets is a/d' lib/stdio.in.h"  # fixes an error, credit to http://www.linuxfromscratch.org/lfs/view/7.3/chapter05/diffutils.html
-    system "sed -i 's:= @mkdir_p@:= /bin/mkdir -p:' po/Makefile.in.in" # http://www.linuxfromscratch.org/lfs/view/development/chapter06/diffutils.html
-
     system "./configure --prefix=/usr/local"
     system "make"
   end
