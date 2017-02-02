c1e5a8f4be57de60d94e6904d6e0ce1e4a56a354
Ed Reel <edreel@gmail.com>
2017-02-01 20:42:27 -0600

    Add GeoIP Legacy C API

---
+++ packages/geoip.rb
@@ -0,0 +1,16 @@
+require 'package'
+
+class Geoip < Package
+  version '1.6.9'
+  source_url 'https://github.com/maxmind/geoip-api-c/releases/download/v1.6.9/GeoIP-1.6.9.tar.gz'
+  source_sha1 'ac0deb2309c14d5763e82fa4139de1f3193ab6b1'
+
+  def self.build
+    system "./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
