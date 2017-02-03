54e140456a731986d54bf7bddf96df89b5f6f38e
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-03 20:25:28 +0900

    Add ncursesw and remove --enable-widec option from ncurses

--- packages/ncursesw.rb
+++ packages/ncursesw.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class Ncurses < Package
+class Ncursesw < Package
   version '6.0'
   source_url 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz'
   source_sha1 'acd606135a5124905da770803c05f1f20dd3b21c'
@@ -13,7 +13,8 @@ class Ncurses < Package
 	    '--without-debug ' \
 	    '--prefix=/usr/local ' \
 	    '--with-shared ' \
-	    '--with-cxx-shared '
+	    '--with-cxx-shared ' \
+	    '--enable-widec'
     system "make"
   end
 
