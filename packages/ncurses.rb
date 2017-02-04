4181fb9c90103aa494fba7a417d54eddcef2a743
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-04 16:15:52 +0900

    increment version number of ncurses in order to force to recompile it

--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Ncurses < Package
-  version '6.0'
+  version '6.0-1'
   source_url 'ftp://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.0.tar.gz'
   source_sha1 'acd606135a5124905da770803c05f1f20dd3b21c'
 
