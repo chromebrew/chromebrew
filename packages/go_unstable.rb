4508d02d3d27ac513ac6da7e0b48a6e1ca22655a
Ben Fayers <bfayers@users.noreply.github.com>
2017-02-06 16:35:42 +0000

    change to recommended way of installing with existing go.

--- packages/go_unstable.rb
+++ packages/go_unstable.rb
@@ -2,37 +2,11 @@ require 'package'
 
 class Go_unstable < Package
   version '1.8rc3'
-  source_url 'https://storage.googleapis.com/golang/go1.8rc3.src.tar.gz'
-  source_sha1 '7f01a411de18ebe8d078ae81b093926d9d7475f0'
   
-  # go is required to build versions of go > 1.4
-  if `which go` == ''
-    depends_on 'go_bootstrap'
-  end
-
-  def self.build
-    FileUtils.cd('src') do
-      # install with go_bootstrap if go is not in the path
-      if `which go` == ''
-        system "GOROOT_BOOTSTRAP=/usr/local/lib/go_bootstrap/go \
-                TMPDIR=/usr/local/tmp \
-                ./make.bash"
-      else
-        system "TMPDIR=/usr/local/tmp ./make.bash"
-      end
-    end
-  end
-
+  is_fake
+  
+  depends_on 'go'
   def self.install
-    dest = "#{CREW_DEST_DIR}/usr/local/lib/"
-    system "mkdir", "-p", dest
-    FileUtils.mv Dir.pwd, dest
-
-    puts "--------"
-    puts "Installed Go for #{ARCH} in /usr/local/lib/go"
-    puts "Make sure to set go environment variables."
-    puts "Minimal:"
-    puts "\texport GOROOT=/usr/local/lib/go"
-    puts "\texport PATH=$PATH:$GOROOT/bin"
+    system "go get golang.org/x/build/version/go1.8rc3"
   end
 end
