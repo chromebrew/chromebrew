0e7c8ecf4c9d8f24d0df1bca552ce7aa10124f2b
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-11 21:34:58 +0900

    Update flex to 2.6.4 and change to strip binaries

--- packages/flex.rb
+++ packages/flex.rb
@@ -1,19 +1,24 @@
 require 'package'
 
 class Flex < Package
-  version '2.6.3'
-  source_url 'https://fossies.org/linux/misc/flex-2.6.3.tar.gz'
-  source_sha1 '8008eb0e4ccc634c334ac1d9e04716be2ba527a9'
+  version '2.6.4'
+  source_url 'https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz'
+  source_sha1 'fafece095a0d9890ebd618adb1f242d8908076e1'
 
   depends_on 'm4'
-  depends_on 'bison'
+  depends_on 'bison' => :build
 
   def self.build
-    system "./configure CFLAGS=\" -fPIC\""
+    system "./configure", "--with-pic", "--disable-static", "--enable-shared"
     system "make"
+    system "find . -name '*.so' -print | xargs strip -S"
   end
 
   def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install-strip"
+  end
+
+  def self.check
+    system "make", "check"
   end
 end
