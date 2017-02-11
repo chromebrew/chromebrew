ad9575529cf64ca7fdcc33a1ded4fde85a3f9b39
Casey Strouse <casey.strouse@gmail.com>
2017-02-11 15:19:34 -0700

    Add xe (0.6.1) package
    xe is a new tool for constructing command lines from file listings or
    arguments, which includes the best features of xargs(1) and apply(1).
    
    Tested on Samsung XE50013-K01US (x86_64).

---
+++ packages/xe.rb
@@ -0,0 +1,15 @@
+require 'package'
+
+class Xe < Package
+  version '0.6.1'
+  source_url 'https://github.com/chneukirchen/xe/archive/v0.6.1.tar.gz'
+  source_sha1 '76c0a31c4d646314c3e92753a2fec3ae7cedad1d'
+
+  def self.build
+    system "make", "PREFIX=/usr/local"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install" 
+  end
+end
