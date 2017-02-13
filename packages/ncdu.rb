bc4088761e7a2ec5e8aade82875c54a2917105f0
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-14 01:22:57 +0900

    Correct dependency

--- packages/ncdu.rb
+++ packages/ncdu.rb
@@ -5,7 +5,7 @@ class Ncdu < Package
   source_url 'https://dev.yorhel.nl/download/ncdu-1.12.tar.gz'
   source_sha1 'b79b1c44784f334dca74d89a49f49274f14cfeef'
 
-  depends_on "ncurses_so"
+  depends_on "ncurses"
 
   def self.build
     system "./configure --prefix=/usr/local CPPFLAGS=-I/usr/local/include/ncurses"
