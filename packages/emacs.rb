9614d4fec6c3197c5e969847ae21db878a04455c
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-18 13:05:36 +0900

    Add :build to emacs.rb dependencies

--- packages/emacs.rb
+++ packages/emacs.rb
@@ -7,11 +7,11 @@ class Emacs < Package
   source_url 'ftp://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.xz'
   source_sha1 '983e457971e3e3c8964d039c113033f98132b8a8'
 
-  depends_on "zlibpkg"
-  depends_on "diffutils"
-  depends_on "m4"
-  depends_on "autoconf"
-  depends_on "automake"
+  depends_on "zlibpkg" => :build
+  depends_on "diffutils" => :build
+  depends_on "m4" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def self.build
     system "./configure --prefix=/usr/local --without-x --without-makeinfo --without-selinux"
