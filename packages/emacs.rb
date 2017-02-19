81801e1f655dcd2c03061a4b41fe0381100cb8d1
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-02-19 08:18:20 -0800

    Add `zlibpkg` dep to Emacs.
    It fails to compile otherwise.

--- packages/emacs.rb
+++ packages/emacs.rb
@@ -5,6 +5,7 @@ class Emacs < Package
   source_url 'ftp://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.xz'
   source_sha1 '983e457971e3e3c8964d039c113033f98132b8a8'
 
+  depends_on "zlibpkg"
   depends_on "diffutils"
   depends_on "m4"
   depends_on "autoconf"
