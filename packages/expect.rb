1e11204966eceb9fa5ed6970f63dc156559857b0
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-25 14:51:40 +0900

    Change expect.rb to strip binary and library. Also add check method.

--- packages/expect.rb
+++ packages/expect.rb
@@ -3,7 +3,7 @@ require 'package'
 class Expect < Package
   description 'Expect is a tool for automating interactive applications such as telnet, ftp, passwd, fsck, rlogin, tip, etc.'
   homepage 'http://expect.sourceforge.net/'
-  version '5.45'
+  version '5.45-1'
   source_url 'http://prdownloads.sourceforge.net/expect/expect5.45.tar.gz'
   source_sha1 'e634992cab35b7c6931e1f21fbb8f74d464bd496'
 
@@ -12,9 +12,15 @@ class Expect < Package
   def self.build
     system "./configure", "--prefix=/usr/local"
     system "make"
+    system "find . -name '*.so' -print | xargs strip -S"
+    system "strip expect"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make test"
   end
 end
