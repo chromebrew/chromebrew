1f114b7524984b95a901cfa514e76ff06afbb4fc
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-02-25 10:07:37 +0900

    Change libffi.rb to not create static library Correct dependencies

--- packages/libffi.rb
+++ packages/libffi.rb
@@ -1,18 +1,20 @@
 require 'package'
 
 class Libffi < Package
-  version '3.2.1'
+  version '3.2.1-1'
   source_url 'ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz'
   source_sha1 '280c265b789e041c02e5c97815793dfc283fb1e6'
 
-  depends_on 'autoconf'
-  depends_on 'automake'
-
   def self.build
-    system "./configure", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--enable-shared", "--disable-static", "--with-pic", "--disable-debug", "--disable-dependency-tracking"
+    system "make"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    # system "make check"         # DejaGNU required
   end
 end
