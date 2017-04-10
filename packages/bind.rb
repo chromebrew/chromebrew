47086343fbdc8bb805c51346a7f60d343b5167ed
ntchambers <nchambers@lightspeedsystems.com>
2017-04-10 11:23:37 -0500

    Added bind version 9.10.4

---
+++ packages/bind.rb
@@ -0,0 +1,16 @@
+require 'package'
+
+class Bind < Package
+  version '9.10.4'
+  source_url 'https://www.isc.org/downloads/file/bind-9-10-4-p6/'
+  source_sha1 'c08bef47136b3b88844a4c3b8a6227445fca6f40'
+
+  def self.build
+    system "BUILD_CC=gcc ./configure --host=x86_64 --with-randomdev=no --with-ecdsa=yes --with-gost=yes --prefix=/usr/local"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
