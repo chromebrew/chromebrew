045778624d0140d9431aa72647a94095ea2e83a1
Ben Fayers <bfayers@users.noreply.github.com>
2017-04-10 14:13:37 +0100

    Fix urls as dropbox been updated

--- packages/jdk8.rb
+++ packages/jdk8.rb
@@ -3,9 +3,9 @@ require 'package'
 class Jdk8 < Package
   version '8u112'
   binary_url ({
-    i686: "https://dl.dropboxusercontent.com/u/14799278/crew/jdk8u112-i686.tar.gz",
-    x86_64: "https://dl.dropboxusercontent.com/u/14799278/crew/jdk8u112-x86_64.tar.gz",
-    armv7l: "https://www.dropbox.com/s/bfu14nhbeoi8tdo/jdk8u111-armv7l.tar.gz?dl=1"
+    i686: "https://www.dropbox.com/s/0c9yratmcf5fdpq/jdk8u112-i686.tar.gz",
+    x86_64: "https://www.dropbox.com/s/ojtv2phawxzg6wr/jdk8u112-x86_64.tar.gz",
+    armv7l: "https://www.dropbox.com/s/vcejuitboafaxib/jdk8u22-armv7l.tar.gz"
   })
   binary_sha1 ({
     i686: "65de377470bdd42e4f3e158b16917166ebe47a02",
