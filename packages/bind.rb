d5abc2df536d2dc9cd2a80201981401b3a8f68fc
Nicholas Chambers <nchambers@lightspeedsystems.com>
2017-04-10 19:25:25 -0700

    Update bind.rb
    fixed build options

--- packages/bind.rb
+++ packages/bind.rb
@@ -5,8 +5,13 @@ class Bind < Package
   source_url 'https://www.isc.org/downloads/file/bind-9-10-4-p6/'
   source_sha1 'c08bef47136b3b88844a4c3b8a6227445fca6f40'
 
+  depends_on "buildessential"
+  depends_on "openssl"
+  depends_on "libcap"
+  depends_on "readline"
+
   def self.build
-    system "BUILD_CC=gcc ./configure --host=x86_64 --with-randomdev=no --with-ecdsa=yes --with-gost=yes --prefix=/usr/local"
+    system "BUILD_CC=gcc ./configure --with-randomdev=no --with-ecdsa=yes --with-gost=yes --prefix=/usr/local"
     system "make"
   end
 
