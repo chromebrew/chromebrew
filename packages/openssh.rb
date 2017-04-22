b0606f4e55ce9d7de770cec848533f7f54e1a0a6
nchambers <nchambers@lightspeedsystems.com>
2017-04-22 12:39:12 -0500

    Added openssh and strace

---
+++ packages/openssh.rb
@@ -0,0 +1,21 @@
+require 'package'
+
+class Openssh < Package
+  version '7.5'
+  source_url 'https://github.com/openssh/openssh-portable/archive/V_7_5_P1.tar.gz'
+  source_sha1 '3c5d829be2d30ed80feef708231ec223cd37b264'
+
+  depends_on 'buildessential'
+  depends_on 'autoconf'
+
+  def self.build
+    system "autoconf"
+    system "autoheader"
+    system "./configure --prefix=/usr/local"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
