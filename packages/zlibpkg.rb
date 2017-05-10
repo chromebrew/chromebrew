8f54dab6279ac923c477e5bee8078474642d3978
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-05-10 22:11:40 +0900

    Change zlibpkg.rb to strip debug symbol of shared library

--- packages/zlibpkg.rb
+++ packages/zlibpkg.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Zlibpkg < Package
-  version '1.2.11'
+  version '1.2.11-1'
   source_url 'http://www.zlib.net/zlib-1.2.11.tar.gz'
   source_sha1 'e6d119755acdf9104d7ba236b1242696940ed6dd'
 
@@ -12,8 +12,12 @@ class Zlibpkg < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+
     # remove static library since there is no configuration option to not create it.
     system "rm", "#{CREW_DEST_DIR}/usr/local/lib/libz.a"
+
+    # strip library
+    system "strip -S #{CREW_DEST_DIR}/usr/local/lib/libz.so.*"
   end
 
   def self.check
