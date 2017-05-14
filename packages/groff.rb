685f13fc4694ccf9725e169589944d3442620137
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-05-14 16:01:21 +0900

    Change groff.rb to strip binaries

--- packages/groff.rb
+++ packages/groff.rb
@@ -1,12 +1,12 @@
 require 'package'
  
 class Groff < Package
-  version '1.22.3'
+  version '1.22.3-1'
   source_url 'http://ftp.gnu.org/gnu/groff/groff-1.22.3.tar.gz'
   source_sha1 '61a6808ea1ef715df9fa8e9b424e1f6b9fa8c091'
   
   def self.build
-    system './configure'
+    system 'INSTALL_PROGRAM=\'${INSTALL} -s\' ./configure'
 
     # force to compile in sequential since groff Makefile doesn't work in parallel
     system 'make', '-j1'
@@ -14,5 +14,9 @@ class Groff < Package
   
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make", "check"
   end
 end
