bd4e3c7d8b3152a9ba07f22e72feb5b54e8b5054
Nicolas Bougues <nbougues@users.noreply.github.com>
2017-05-14 01:24:36 -0700

    Update make.rb
    The configure script needs limits.h in the preprocessor test, which is part of linuxheaders

--- packages/make.rb
+++ packages/make.rb
@@ -5,6 +5,8 @@ class Make < Package
   source_url 'ftp://ftp.gnu.org/gnu/make/make-4.2.tar.bz2'
   source_sha1 'd78b84a219b4c16593544f541dff7eb765ce3d74'
 
+  depends_on 'linuxheaders'
+
   def self.build
     system "./configure"
     system "./build.sh"
