3112fd7dd3fb7fdb2d68c1f54cd44a53a75d3000
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-05-14 18:08:22 +0900

    Change sqlite.rb to strip binary and libraries

--- packages/sqlite.rb
+++ packages/sqlite.rb
@@ -1,16 +1,17 @@
 require 'package'
  
 class Sqlite < Package
-  version '3.18.0'
+  version '3.18.0-1'
   source_url 'https://www.sqlite.org/2017/sqlite-autoconf-3180000.tar.gz'
   source_sha1 '74559194e1dd9b9d577cac001c0e9d370856671b'
 
   def self.build
     system "./configure", "--disable-static", "--enable-shared", "--with-pic"
     system "make"
+    system "find . -name 'lib*.so.*' -print | xargs strip -S"
   end
   
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install-strip"
   end
 end
