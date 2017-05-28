82cd6fcc969effff2eb71d3665c513bd8120b6e2
Ed Reel <edreel@gmail.com>
2017-05-28 01:19:26 -0500

    Add patchutils package

---
+++ packages/patchutils.rb
@@ -0,0 +1,20 @@
+require 'package'
+
+class Patchutils < Package
+  version '0.3.4'
+  source_url 'http://cyberelk.net/tim/data/patchutils/stable/patchutils-0.3.4.tar.xz'
+  source_sha1 'b1d91eb1e2213450eae666a4701b3f917625dea3'
+
+  def self.build
+    system './configure --prefix=/usr/local'
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make", "check"
+  end
+end
