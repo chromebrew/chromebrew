d2f970832dbfe482fa532872978826869d8c8352
Jan Baudisch <dev@baudisch.xyz>
2017-06-02 15:28:56 +0200

    Fix wrong URL

--- packages/yarn.rb
+++ packages/yarn.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Yarn < Package
   version '0.25.4'
-  source_url 'https://github.com/yarnpkg/yarn/releases/download/v0.25.3/yarn-v0.25.3.tar.gz'
+  source_url 'https://github.com/yarnpkg/yarn/releases/download/v0.25.4/yarn-v0.25.4.tar.gz'
   source_sha1 '94b08478cf06652a337aef5742cf32472c767924'
 
   depends_on 'node'
