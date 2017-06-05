9c3c4748543ef22faeb7825facb66ad4a02ad212
Ed Reel <edreel@gmail.com>
2017-06-04 22:40:54 -0500

    Fix libx264 package version

--- packages/libx264.rb
+++ packages/libx264.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Libx264 < Package
-  version '20170528-2245-stable'
-  source_url 'http://ftp.videolan.org/pub/videolan/x264/snapshots/last_stable_x264.tar.bz2'
+  version '20170604-2245-stable'
+  source_url 'http://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20170604-2245-stable.tar.bz2'
   source_sha1 '8003044b45010b1b4b40ac3dd8be98d80c888ece'
 
   depends_on 'yasm'
