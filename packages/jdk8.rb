c7d6b7fdb43a7a7ddb1b0d4b5343faf60a8afc78
Damian Montero <photoca@hotmail.com>
2017-06-13 04:19:17 -0400

    JDK8 upgraded to 8u131 (for x86 and 64x ONLY), arm still at u22, but we are using official oracle URLs now

--- packages/jdk8.rb
+++ packages/jdk8.rb
@@ -3,16 +3,16 @@ require 'package'
 class Jdk8 < Package
   description 'The JDK is a development environment for building applications, applets, and components using the Java programming language.'
   homepage 'http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html'
-  version '8u111'
+  version '8u131'
   binary_url ({
-    i686: "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=216422",
-    x86_64: "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=216424",
+    i686: "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220303_d54c1d3a095b4ff2b6607d096fa80163",
+    x86_64: "http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220305_d54c1d3a095b4ff2b6607d096fa80163",
     armv7l: "https://www.dropbox.com/s/vcejuitboafaxib/jdk8u22-armv7l.tar.gz",
     aarch64: "https://www.dropbox.com/s/vcejuitboafaxib/jdk8u22-armv7l.tar.gz"
   })
   binary_sha1 ({
-    i686:    '37556a34403cb6d76c6e51471627c9e07727ea2d',
-    x86_64:  'd09fa2b77b6cf05ba835377a00ef495e6deebe1b',
+    i686:    '20d1a77783a01a592d9bcf3597256caee2ccfce0',
+    x86_64:  '9303d3a8e8fc2d2eda014887fd92a6f5883f0170',
     aarch64: '913adb900bf0d9d42452a4591c1a9093076ed4b6',
     armv7l:  '913adb900bf0d9d42452a4591c1a9093076ed4b6'
   })
