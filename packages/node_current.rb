2252ad539187346539733fd3834c23beaa3bc8cd
Damian Montero <photoca@hotmail.com>
2017-06-13 04:02:57 -0400

    Node_current to 8.1.0

--- packages/node_current.rb
+++ packages/node_current.rb
@@ -1,28 +1,29 @@
 require 'package'
 
-class Node_current < Package
-  description 'As an asynchronous event driven JavaScript runtime, Node current is designed to build scalable network applications.'
+class Node < Package
+  description 'As an asynchronous event driven JavaScript runtime, Node is designed to build scalable network applications.'
   homepage 'https://nodejs.org/en/'
-  version '7.4.0'
-  source_url 'https://nodejs.org/dist/v7.4.0/node-v7.4.0.tar.xz'
-  source_sha1 'e31d4d67a6f39ccce2db66dcfa0e6c11851c1375'
+  version '8.1.0'
+  source_url 'https://nodejs.org/dist/v8.1.0/node-v8.1.0.tar.xz'
+  source_sha1 '13ed221ed55577beac5203c3c8bfec61af3cad97'
 
   depends_on 'buildessential'
   depends_on 'python27'
 
   def self.build
-    system "CC='gcc' python2.7 ./configure --without-snapshot"
+    system "CC='gcc -m64' python2.7 ./configure --without-snapshot"
     system "make"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
     #Fix Permissiongs for -g installs of node packages
-    puts  "************************************************************"
-    puts "Now run:"
-    puts "sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}"
-    puts "To be able to do"
-    puts "npm install -g"
-    puts "without being SUDO"
+    #puts  "************************************************************"
+    #puts "Now run:"
+    #puts "sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}"
+    #puts "To be able to do"
+    #puts "npm install -g"
+    #puts "without being SUDO"
+    system "sudo chown -R $(whoami) $(npm config get prefix)/{lib/node_modules,bin,share}"
   end
 end
