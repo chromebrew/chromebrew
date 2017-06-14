522e3e0074113423ecdf1a7a68fb0a42ce4eb05a
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-06-14 20:41:46 +0900

    Change openssl.rb to compile it from source on aarch64 correctly.

--- packages/openssl.rb
+++ packages/openssl.rb
@@ -4,27 +4,21 @@ class Openssl < Package
   description 'OpenSSL is an open source project that provides a robust, commercial-grade, and full-featured toolkit for the Transport Layer Security (TLS) and Secure Sockets Layer (SSL) protocols.'
   homepage 'https://www.openssl.org/'
   version '1.0.2l'
-
   source_url 'https://github.com/openssl/openssl/archive/OpenSSL_1_0_2l.tar.gz'
   source_sha256 'a3d3a7c03c90ba370405b2d12791598addfcafb1a77ef483c02a317a56c08485'
-  binary_url ({
-    aarch64: 'https://github.com/jam7/chromebrew/releases/download/binaries/openssl-1.0.2l-chromeos-armv7l.tar.xz',
-    armv7l:  'https://github.com/jam7/chromebrew/releases/download/binaries/openssl-1.0.2l-chromeos-armv7l.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '4af16174aa6a9f565a5895fedea89daf0c8fb66b8f26b0c8416f5456aa440ea5',
-    armv7l:  '4af16174aa6a9f565a5895fedea89daf0c8fb66b8f26b0c8416f5456aa440ea5',
-  })
 
   depends_on 'perl' => :build
   depends_on 'zlibpkg' => :build
 
   def self.build
     options="shared zlib-dynamic"
-    if `uname -m`.strip == 'aarch64'
-      options = options + " no-asm"
+    case `uname -m`.strip
+    when "aarch64"
+      # Specify armv7 since aarch64 uses armv7 as its user land.
+      system "./Configure --prefix=/usr/local --openssldir=/etc/ssl #{options} linux-armv4 -march=armv7-a"
+    else
+      system "./config --prefix=/usr/local --openssldir=/etc/ssl #{options}"
     end
-    system "./config --prefix=/usr/local --openssldir=/etc/ssl #{options}"
     system "make"
   end
 
