91f76a9097b57d8f6bc6fb6a4011512f71a1d55e
Casey Strouse <casey.strouse@gmail.com>
2017-06-16 14:08:43 -0700

    Update valgrind from 3.12.0 -> 3.13.0
    This is a general maintenance and bugfix release.

--- packages/valgrind.rb
+++ packages/valgrind.rb
@@ -3,9 +3,9 @@ require 'package'
 class Valgrind < Package
   description 'Valgrind is an instrumentation framework for building dynamic analysis tools.'
   homepage 'http://valgrind.org/'
-  version '3.12.0'
-  source_url 'http://valgrind.org/downloads/valgrind-3.12.0.tar.bz2'
-  source_sha1 '7a6878bf998c60d1e377a4f22ebece8d9305bda4'
+  version '3.13.0'
+  source_url 'ftp://sourceware.org/pub/valgrind/valgrind-3.13.0.tar.bz2'
+  source_sha1 'ddf13e22dd0ee688bd533fc66b94cf88f75fad86'
 
   def self.build
     system './configure'
