db3498bb27bf8f1213586198fa320a3b223cf9c6
Ed Reel <edreel@gmail.com>
2017-06-19 14:37:29 -0500

    Add gawk dependency

--- packages/byobu.rb
+++ packages/byobu.rb
@@ -7,6 +7,8 @@ class Byobu < Package
   source_url 'https://launchpadlibrarian.net/322131788/byobu_5.119.orig.tar.gz'
   source_sha1 '1d8d07da4c94c4adeb662a7f8b33fe02482d9839'
 
+  depends_on 'gawk'
+
   def self.build
     system './configure'
     system 'make'
