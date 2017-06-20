a4b74880490a7b8b6a68757e904939240e27d847
Casey Strouse <casey.strouse@gmail.com>
2017-06-20 12:34:06 -0700

    Update nano from 2.7.1 to 2.8.4
    This is a general maintenance and bugfix release which also includes several
    enhancements to usability.

--- packages/nano.rb
+++ packages/nano.rb
@@ -3,9 +3,9 @@ require 'package'
 class Nano < Package
   description 'Nano\'s ANOther editor, an enhanced free Pico clone.'
   homepage 'https://www.nano-editor.org/'
-  version '2.7.1'
-  source_url 'ftp://ftp.gnu.org/pub/gnu/nano/nano-2.7.1.tar.gz'
-  source_sha1 '5ad089d08e68dc7977f51403de6d7872686647af'
+  version '2.8.4'
+  source_url 'https://nano-editor.org/dist/v2.8/nano-2.8.4.tar.xz'
+  source_sha1 '83ac3285e50690205011a822eadb42709c0fb2f3'
 
   depends_on 'buildessential'
   depends_on 'ncurses' 
