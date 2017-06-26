87bd751fe4bfde8cfee3a2ae6b6d8258cbf4416a
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-06-26 21:20:07 +0900

    Change groff.rb dependency since it uses perl at run-time

--- packages/groff.rb
+++ packages/groff.rb
@@ -7,6 +7,8 @@ class Groff < Package
   source_url 'http://ftp.gnu.org/gnu/groff/groff-1.22.3.tar.gz'
   source_sha256 '3a48a9d6c97750bfbd535feeb5be0111db6406ddb7bb79fc680809cda6d828a5'
 
+  depends_on 'perl'
+
   def self.build
     system 'INSTALL_PROGRAM=\'${INSTALL} -s\' ./configure'
 
