bb516275a85bbbfb31a9abbaaa09b230d95e149c
Ed Reel <edreel@gmail.com>
2017-06-27 21:09:03 -0500

    Add datamash package

---
+++ packages/datamash.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Datamash < Package
+  description 'GNU Datamash is a command-line program which performs basic numeric,textual and statistical operations on input textual data files.'
+  homepage 'http://savannah.gnu.org/projects/datamash'
+  version '1.1.1'
+  source_url 'http://ftp.gnu.org/gnu/datamash/datamash-1.1.1.tar.gz'
+  source_sha1 '12edeb94ee2461a5d55c7094b81fa4a2429aeb50'
+  #source_sha256 '420819b3d7372ee3ce704add847cff7d08c4f8176c1d48735d4a632410bb801b'
+
+  def self.build
+    system './configure'
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
