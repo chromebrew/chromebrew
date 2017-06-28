48e45cb51a68c413edf401f9f288a13b3c87d375
Ed Reel <edreel@gmail.com>
2017-06-27 20:23:37 -0500

    Add aspell dictionaries

---
+++ packages/aspell_en.rb
@@ -0,0 +1,21 @@
+require 'package'
+
+class Aspell_en < Package
+  description 'English Aspell Dictionary'
+  homepage 'ftp://ftp.gnu.org/gnu/aspell/dict/0index.html'
+  version '2017.01.22-0'
+  source_url 'ftp://ftp.gnu.org/gnu/aspell/dict/en/aspell6-en-2017.01.22-0.tar.bz2'
+  source_sha1 '05f5123d7c043446c385d4fb77fa3d796f2356ab'
+  #source_sha256 '93c73fae3eab5ea3ca6db3cea8770715a820f1b7d6ea2b932dd66a17f8fd55e1'
+
+  depends_on 'aspell'
+
+  def self.build
+    system './configure'
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
---
+++ packages/aspell_es.rb
@@ -0,0 +1,21 @@
+require 'package'
+
+class Aspell_es < Package
+  description 'Spanish Aspell Dictionary'
+  homepage 'ftp://ftp.gnu.org/gnu/aspell/dict/0index.html'
+  version '1.11-2'
+  source_url 'ftp://ftp.gnu.org/gnu/aspell/dict/es/aspell6-es-1.11-2.tar.bz2'
+  source_sha1 '18acfa4bc08433e920bb015b158e43643e5125cf'
+  #source_sha256 'ad367fa1e7069c72eb7ae37e4d39c30a44d32a6aa73cedccbd0d06a69018afcc'
+
+  depends_on 'aspell'
+
+  def self.build
+    system './configure'
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
