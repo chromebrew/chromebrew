5bd8061baf4ad34342fa90993e5ce909c9cb18a1
Ed Reel <edreel@gmail.com>
2017-06-29 12:17:58 -0500

    Change from sha1 to sha256

--- packages/lcms.rb
+++ packages/lcms.rb
@@ -5,7 +5,7 @@ class Lcms < Package
   homepage 'http://www.littlecms.com/'
   version '2.8'
   source_url 'https://downloads.sourceforge.net/project/lcms/lcms/2.8/lcms2-2.8.tar.gz'
-  source_sha1 'e9535ec4a572b8fc7a1c405c35e6f4dc97714197'
+  source_sha256 '66d02b229d2ea9474e62c2b6cd6720fde946155cd1d0d2bffdab829790a0fb22'
 
   def self.build
     system './configure'
