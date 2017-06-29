c533bea1ca9524d34600b5db46e977310ad26a99
Ed Reel <edreel@gmail.com>
2017-06-29 12:28:28 -0500

    Change from sha1 to sha256

--- packages/healthcheck.rb
+++ packages/healthcheck.rb
@@ -5,8 +5,7 @@ class Healthcheck < Package
   homepage 'https://github.com/SimplyLinuxFAQ/health-check-script'
   version '1cace5'
   source_url 'https://github.com/SimplyLinuxFAQ/health-check-script/archive/1cace54e871c69cd4bbbaeeb3b9e314d7f575b06.tar.gz'
-  source_sha1 '203a5e8f0378c656a0d7f6b9e87d5cf217b18395'
-  #source_sha256 '4f40995614da68da7422e2ba8e01815c569e3c1a268122a5e13a007899092630'
+  source_sha256 '4f40995614da68da7422e2ba8e01815c569e3c1a268122a5e13a007899092630'
 
   depends_on 'sysstat'
 
