d090989f79df49eeea508695eda83d826a6d0e79
Ed Reel <edreel@gmail.com>
2017-07-02 23:36:40 -0500

    Change source_url to use https as requested by @lyxell

--- packages/datamash.rb
+++ packages/datamash.rb
@@ -4,7 +4,7 @@ class Datamash < Package
   description 'GNU Datamash is a command-line program which performs basic numeric,textual and statistical operations on input textual data files.'
   homepage 'http://savannah.gnu.org/projects/datamash'
   version '1.1.1'
-  source_url 'http://ftpmirror.gnu.org/datamash/datamash-1.1.1.tar.gz'
+  source_url 'https://ftpmirror.gnu.org/datamash/datamash-1.1.1.tar.gz'
   source_sha256 '420819b3d7372ee3ce704add847cff7d08c4f8176c1d48735d4a632410bb801b'
 
   def self.build
