2fa37f40890f5b30b331f63c929ee8bd854e6ef1
Ed Reel <edreel@gmail.com>
2017-07-02 23:32:52 -0500

    Change source_url to use https as requested by @lyxell

--- packages/shtool.rb
+++ packages/shtool.rb
@@ -4,7 +4,7 @@ class Shtool < Package
   description 'GNU shtool is a compilation of small but very stable and portable shell scripts into a single shell tool.'
   homepage 'http://savannah.gnu.org/projects/shtool'
   version '2.0.8'
-  source_url 'http://ftpmirror.gnu.org/shtool/shtool-2.0.8.tar.gz'
+  source_url 'https://ftpmirror.gnu.org/shtool/shtool-2.0.8.tar.gz'
   source_sha256 '1298a549416d12af239e9f4e787e6e6509210afb49d5cf28eb6ec4015046ae19'
 
   def self.build
