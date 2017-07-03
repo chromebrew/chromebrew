8abfb9a577f3690e8e2fbd9ab497006a1e2706b4
Ed Reel <edreel@gmail.com>
2017-07-02 23:39:13 -0500

    Change source_url to use https as requested by @lyxell

--- packages/coreutils.rb
+++ packages/coreutils.rb
@@ -4,7 +4,7 @@ class Coreutils < Package
   description 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.'
   homepage 'http://www.gnu.org/software/coreutils/coreutils.html'
   version '8.27'
-  source_url 'http://ftpmirror.gnu.org/coreutils/coreutils-8.27.tar.xz'
+  source_url 'https://ftpmirror.gnu.org/coreutils/coreutils-8.27.tar.xz'
   source_sha256 '8891d349ee87b9ff7870f52b6d9312a9db672d2439d289bc57084771ca21656b'
 
   def self.build
