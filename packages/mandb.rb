2cceb2785d6f07a0838b2318a0b8f68a28c92134
Ed Reel <edreel@gmail.com>
2017-07-04 10:00:27 -0500

    Fix mandb to get apropos working

--- packages/mandb.rb
+++ packages/mandb.rb
@@ -24,12 +24,30 @@ class Mandb < Package
   end
 
   def self.install
+    system "mkdir -p #{CREW_DEST_DIR}/usr/local/cache/man"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' include/manconfig.h.in"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' src/manp.c"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' src/tests/mandb-7"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' src/man_db.conf.in"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' init/systemd/man-db.conf"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' manual/db.me"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' manual/files.me"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' man/man1/whatis.man1"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' man/man1/apropos.man1"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' man/man1/man.man1"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' man/man8/accessdb.man8"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' man/man8/mandb.man8"
+    system "sed -i 's,/usr/share/man,/usr/local/share/man,g' tools/chconfig"
+    system "sed -i 's,/var/cache/man,/usr/local/cache/man,g' tools/chconfig"
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
     puts ""
-    puts "You will have to change the default PAGER environment variable to be able to use mandb:"
-    puts "echo \"export PAGER=/usr/local/bin/less\" >> ~/.bashrc && . ~/.bashrc"
+    puts "You will have to change the default PAGER environment variable to be able to use mandb:".lightblue
+    puts "echo \"export PAGER=/usr/local/bin/less\" >> ~/.bashrc && . ~/.bashrc".lightblue
     puts ""
-    puts "You will also have to set the MANPATH environment variable:"
-    puts "echo \"export MANPATH=/usr/local/man:$MANPATH\" >> ~/.bashrc && . ~/.bashrc"
+    puts "You will also have to set the MANPATH environment variable:".lightblue
+    puts "echo \"export MANPATH=/usr/local/man:$MANPATH\" >> ~/.bashrc && . ~/.bashrc".lightblue
+    puts ""
+    puts "To create the man databases and get apropos working, type 'mandb -c'.".lightblue
+    puts ""
   end
 end
