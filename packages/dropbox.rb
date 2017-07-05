0cf18cb76fe4c362e8e748ba3ffb99e62cdda120
Ed Reel <edreel@gmail.com>
2017-07-04 23:20:57 -0500

    Update dropbox from 28.4.14 to 29.4.20

--- packages/dropbox.rb
+++ packages/dropbox.rb
@@ -3,14 +3,14 @@ require 'package'
 class Dropbox < Package
   description 'Dropbox simplifies the way you create, share and collaborate. Bring your photos, docs, and videos anywhere and keep your files safe.'
   homepage 'https://www.dropbox.com/'
-  version '28.4.14'
+  version '29.4.20'
   case ARCH
   when 'i686'
-    source_url 'https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86-28.4.14.tar.gz'
-    source_sha256 '8ab28710d819ad2f790a7f5d5132a58d28ea9751f2f04f234bc1f9f9979c5c83'
+    source_url 'https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86-29.4.20.tar.gz'
+    source_sha256 'd3d51259a4dae434c10f9c352aa2a3f75a98ffbfb04dfab8c182cff7e3b5ed7d'
   when 'x86_64'
-    source_url 'https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86_64-28.4.14.tar.gz'
-    source_sha256 'a2385e48ab8ea8403f5c51c297105019a90cb58773bf383c6205bd3586a7ee0a'
+    source_url 'https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86_64-29.4.20.tar.gz'
+    source_sha256 '82321e3955b8d6dc329fe1fa9f4eb17a88e1beeac9eae8874318574310461671'
   end
 
   depends_on 'python' unless File.exists? '/usr/local/bin/python'
@@ -18,11 +18,11 @@ class Dropbox < Package
   def self.install
     system "wget https://linux.dropbox.com/packages/dropbox.py"
     system "sed -i 's,~/.dropbox-dist,/usr/local/bin,g' dropbox.py"
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
-    system "cp -r .dropbox-dist/. #{CREW_DEST_DIR}/usr/local/bin"
     system "echo '#!/bin/bash' > dropbox"
     system "echo 'python /usr/local/bin/dropbox.py \$1' >> dropbox"
     system "chmod +x dropbox"
+    system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
+    system "cp -r .dropbox-dist/. #{CREW_DEST_DIR}/usr/local/bin"
     system "cp dropbox.py #{CREW_DEST_DIR}/usr/local/bin"
     system "cp dropbox #{CREW_DEST_DIR}/usr/local/bin"
   end
