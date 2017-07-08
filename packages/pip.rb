3d7908f0d0fa16046d31ad45e97b16e4955059ba
Ed Reel <edreel@gmail.com>
2017-07-07 22:11:14 -0500

    Abort install if pip is already installed

--- packages/pip.rb
+++ packages/pip.rb
@@ -7,13 +7,12 @@ class Pip < Package
   source_url 'https://github.com/pypa/pip/archive/9.0.1.tar.gz'
   source_sha256 'd03fabbc4fbf2fbfc2f97307960aef2b3ca4c880ecda993dcc35957e33d7cd76'
 
-  # Don't overwrite the currently installed version.
-  abort 'pip is already installed.'.lightgreen if File.exists? '/usr/local/bin/pip'
-
-  # Install python if not found.
+  # Install python only if not found.
   depends_on 'python' unless File.exists? '/usr/local/bin/python'
 
   def self.install
+    # Don't overwrite the currently installed version.
+    abort 'Package pip is already installed, skipping...'.lightgreen if File.exists? '/usr/local/bin/pip'
     system "wget https://bootstrap.pypa.io/get-pip.py"
     system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
     system "python get-pip.py -I -b #{CREW_DEST_DIR}/usr/local"
