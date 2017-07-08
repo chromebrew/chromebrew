56aace04e0043511a129ef77e71d7227366bd582
Ed Reel <edreel@gmail.com>
2017-07-07 22:04:20 -0500

    Add condition to only install if not found

--- packages/pip.rb
+++ packages/pip.rb
@@ -7,12 +7,16 @@ class Pip < Package
   source_url 'https://github.com/pypa/pip/archive/9.0.1.tar.gz'
   source_sha256 'd03fabbc4fbf2fbfc2f97307960aef2b3ca4c880ecda993dcc35957e33d7cd76'
 
+  # Don't overwrite the currently installed version.
+  abort 'pip is already installed.'.lightgreen if File.exists? '/usr/local/bin/pip'
+
+  # Install python if not found.
   depends_on 'python' unless File.exists? '/usr/local/bin/python'
 
   def self.install
     system "wget https://bootstrap.pypa.io/get-pip.py"
     system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
     system "python get-pip.py -I -b #{CREW_DEST_DIR}/usr/local"
-    system "cp /usr/local/bin/pip* #{CREW_DEST_DIR}/usr/local/bin"
+    system "cp /usr/local/bin/pip #{CREW_DEST_DIR}/usr/local/bin"
   end
 end
