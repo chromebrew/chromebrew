b7f17bfa755a6cf43cd09b91a53e7db280e0efc7
Ed Reel <edreel@gmail.com>
2017-07-07 21:48:08 -0500

    Add pip package

--- packages/pip.rb
+++ packages/pip.rb
@@ -5,7 +5,7 @@ class Pip < Package
   homepage 'https://pip.pypa.io/'
   version '9.0.1'
   source_url 'https://github.com/pypa/pip/archive/9.0.1.tar.gz'
-  source_sha1 '39b5840f6a9e6888d56a78beb447a064aeaf1592'
+  source_sha256 'd03fabbc4fbf2fbfc2f97307960aef2b3ca4c880ecda993dcc35957e33d7cd76'
 
   depends_on 'python' unless File.exists? '/usr/local/bin/python'
 
