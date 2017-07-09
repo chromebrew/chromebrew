468ae35f631c6d00437b9b8d81c28e9da8cd90a1
Ed Reel <edreel@gmail.com>
2017-07-09 17:48:40 -0500

    Correct homepage for cbase package

--- packages/cbase.rb
+++ packages/cbase.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Cbase < Package
   description 'cbase is a C library of useful functions that simplify systems software development on System V UNIX.'
-  homepage 'http://www.hyperrealm.com/main.php?s=misctools'
+  homepage 'http://www.hyperrealm.com/main.php?s=cbase'
   version '1.3.7'
   source_url 'http://www.hyperrealm.com/cbase/cbase-1.3.7.tar.gz'
   source_sha256 'c4d155686ac2e9d1480319de311967fadad745a6ab6971d53d495d9a9e52dc47'
