6ecd92568ac03f064a6b0cffe1c30c92cd802eb2
Ed Reel <edreel@gmail.com>
2017-07-10 07:30:57 -0500

    Change parser line to exclude only the ruby package in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 IFS=' '
-packages="$(crew search | grep '(i)' | cut -d':' -f1 | cut -d')' -f2 | sed 's,\[0m,,g' | tr -dc '[:print:]' | sed 's, ruby,,')"
+packages="$(crew search | grep '(i)' | cut -d':' -f1 | cut -d')' -f2 | sed 's,\[0m,,g' | tr -dc '[:print:]' | sed 's, ruby , ,')"
 for package in $packages; do
   crew remove $package
 done
