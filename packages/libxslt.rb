3327e051b812900d1dc8e6d9f78f041219fb31a4
Ed Reel <edreel@gmail.com>
2017-07-16 00:00:22 -0500

    Add dependency to libxslt

--- packages/libxslt.rb
+++ packages/libxslt.rb
@@ -7,6 +7,8 @@ class Libxslt < Package
   source_url 'http://xmlsoft.org/sources/libxslt-1.1.28.tar.gz'
   source_sha256 '5fc7151a57b89c03d7b825df5a0fae0a8d5f05674c0e7cf2937ecec4d54a028c'
 
+  depends_on 'libxml2'
+
   def self.build
     system "./configure CFLAGS=\" -fPIC\" --without-python"
     system "make"
