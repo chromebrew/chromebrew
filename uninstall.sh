067f3d3b3d3258f116dc5f5b30708160e2585c58
Ed Reel <edreel@gmail.com>
2017-07-16 01:46:20 -0500

    Add more cleanup lines

--- uninstall.sh
+++ uninstall.sh
@@ -2,17 +2,23 @@
 packages="$(crew search | grep '(i)' | cut -d':' -f1 | cut -d')' -f2 | sed 's,\[0m,,g' | tr -dc '[:print:]' | sed 's, ruby , ,')"
 for package in $packages; do
   crew remove $package
-  rm -rf /usr/local/db/$package
-  rm -rf /usr/local/etc/$package
-  rm -rf /usr/local/lib/$package
-  rm -rf /usr/local/lib64/$package
-  rm -rf /usr/local/libexec/$package
-  rm -rf /usr/local/opt/$package
-  rm -rf /usr/local/share/$package
-  rm -rf /usr/local/$package
+  /bin/rm -rf /usr/local/db/$package
+  /bin/rm -rf /usr/local/etc/$package
+  /bin/rm -rf /usr/local/lib/$package
+  /bin/rm -rf /usr/local/lib64/$package
+  /bin/rm -rf /usr/local/libexec/$package
+  /bin/rm -rf /usr/local/opt/$package
+  /bin/rm -rf /usr/local/share/$package
+  /bin/rm -rf /usr/local/$package
 done
 crew remove ruby
-rm -rf /usr/local/lib/node_modules
-rm -rf /usr/local/lib/ruby
-rm -rf /usr/local/lib/crew
-rm -f /usr/local/bin/crew
+/bin/rm -rf /usr/local/lib/node_modules
+/bin/rm -rf /usr/local/lib/perl*
+/bin/rm -rf /usr/local/include/python*
+/bin/rm -rf /usr/local/lib/python*
+/bin/rm -rf /usr/local/lib64/python*
+/bin/rm -rf /usr/local/include/ruby*
+/bin/rm -rf /usr/local/lib/ruby*
+/bin/rm -rf /usr/local/lib64/ruby*
+/bin/rm -rf /usr/local/lib/crew
+/bin/rm -f /usr/local/bin/crew
