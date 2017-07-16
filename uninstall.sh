a601936ac2ab85f7f0daa964dfdab0809a87a32f
Ed Reel <edreel@gmail.com>
2017-07-15 23:57:18 -0500

    Add extra cleanup lines

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-IFS=' '
 packages="$(crew search | grep '(i)' | cut -d':' -f1 | cut -d')' -f2 | sed 's,\[0m,,g' | tr -dc '[:print:]' | sed 's, ruby , ,')"
 for package in $packages; do
   crew remove $package
+  rm -rf /usr/local/db/$package
+  rm -rf /usr/local/etc/$package
+  rm -rf /usr/local/lib/$package
+  rm -rf /usr/local/lib64/$package
+  rm -rf /usr/local/libexec/$package
+  rm -rf /usr/local/opt/$package
+  rm -rf /usr/local/share/$package
+  rm -rf /usr/local/$package
 done
 crew remove ruby
+rm -rf /usr/local/lib/node_modules
+rm -rf /usr/local/lib/ruby
+rm -rf /usr/local/lib/crew
+rm -f /usr/local/bin/crew
