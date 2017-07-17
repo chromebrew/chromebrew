679ea3a4ae3ea12d3673c7d2aa73f26c7e20f400
Ed Reel <edreel@gmail.com>
2017-07-17 09:38:34 -0500

    Add dmidecode package

---
+++ packages/dmidecode.rb
@@ -0,0 +1,21 @@
+require 'package'
+
+class Dmidecode < Package
+  description "Dmidecode reports information about your system's hardware as described in your system BIOS according to the SMBIOS/DMI standard (see a sample output)."
+  homepage 'http://www.nongnu.org/dmidecode/'
+  version '3.1'
+  source_url 'http://download.savannah.gnu.org/releases/dmidecode/dmidecode-3.1.tar.xz'
+  source_sha256 'd766ce9b25548c59b1e7e930505b4cad9a7bb0b904a1a391fbb604d529781ac0'
+
+  def self.build
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    puts ""
+    puts "To complete the installation, execute the following:".lightblue
+    puts "echo 'export PATH=$PATH:/usr/local/sbin' >> ~/.bashrc && source ~/.bashrc".lightblue
+    puts ""
+  end
+end
