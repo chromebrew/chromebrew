0e50a7ad1c8660737a6ddf8c64e907c1c68cc0f0
Ed Reel <edreel@gmail.com>
2017-07-18 23:17:56 -0500

    Update automake from 1.15 to 1.15.1

--- packages/automake.rb
+++ packages/automake.rb
@@ -3,9 +3,9 @@ require 'package'
 class Automake < Package
   description 'Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.'
   homepage 'http://www.gnu.org/software/automake/'
-  version '1.15'
-  source_url 'ftp://ftp.gnu.org/gnu/automake/automake-1.15.tar.xz'
-  source_sha256 '9908c75aabd49d13661d6dcb1bc382252d22cc77bf733a2d55e87f2aa2db8636'
+  version '1.15.1'
+  source_url 'https://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.xz'
+  source_sha256 'af6ba39142220687c500f79b4aa2f181d9b24e4f8d8ec497cea4ba26c64bedaf'
 
   depends_on 'autoconf'
 
