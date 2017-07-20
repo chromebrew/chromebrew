a5443c6c1e8ad553ea8b9ac63f1304d61e2bd032
Ed Reel <edreel@gmail.com>
2017-07-19 22:39:44 -0500

    Update subversion from 1.9.5 to 1.9.6

--- packages/subversion.rb
+++ packages/subversion.rb
@@ -3,9 +3,9 @@ require 'package'
 class Subversion < Package
   description 'Subversion is an open source version control system.'
   homepage 'https://subversion.apache.org/'
-  version '1.9.5'
-  source_url 'http://apache.mirrors.ionfish.org/subversion/subversion-1.9.5.tar.gz'
-  source_sha256 '280ba586c5d51d7b976b65d22d5e8e42f3908ed1c968d71120dcf534ce857a83'
+  version '1.9.6'
+  source_url 'http://apache.mirrors.ionfish.org/subversion/subversion-1.9.6.tar.bz2'
+  source_sha256 'dbcbc51fb634082f009121f2cb64350ce32146612787ffb0f7ced351aacaae19'
 
   depends_on 'aprutil'
   depends_on 'sqlite'
