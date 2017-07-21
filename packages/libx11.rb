d49f77c31ac0ef682db77896af7610d0e9dbd6ec
Jan Baudisch <dev@baudisch.xyz>
2017-07-20 22:34:12 -0600

    Add libx11

---
+++ packages/libx11.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Libx11 < Package
+  description 'C interface to the X window system'
+  homepage 'https://x.org'
+  version '1.6.5'
+  source_url 'https://www.x.org/archive/individual/lib/libX11-1.6.5.tar.gz'
+  source_sha256 '3abce972ba62620611fab5b404dafb852da3da54e7c287831c30863011d28fb3'
+
+  def self.build
+    system "./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
