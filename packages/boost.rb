3f7db7d3108ba284e56f8a4e3d55b81ab649f5b0
Ed Reel <edreel@gmail.com>
2017-07-21 21:31:03 -0500

    Make sure boost is uninstallable

--- packages/boost.rb
+++ packages/boost.rb
@@ -8,10 +8,12 @@ class Boost < Package
   source_sha256 '7bcc5caace97baa948931d712ea5f37038dbb1c5d89b43ad4def4ed7cb683332'
 
   def self.build
-    system './bootstrap.sh --prefix=/usr/local'
+    system './bootstrap.sh'
   end
 
   def self.install
-    system './b2 -a'
+    system "./b2 -a install"
+    system "mkdir -p #{CREW_DEST_DIR}/usr/local/lib"
+    system "cp /usr/local/lib/libboost_* #{CREW_DEST_DIR}/usr/local/lib"
   end
 end
