993b5f0f6913fb74da6c6cbcf8374ea8d7d87463
Jan Baudisch <dev@baudisch.xyz>
2017-07-22 19:47:28 -0700

    Update libxcb dependencies

--- packages/libxcb.rb
+++ packages/libxcb.rb
@@ -8,6 +8,8 @@ class Libxcb < Package
   source_sha256 '092f147149d8a6410647a848378aaae749304d5b73e028ccb8306aa8a9e26f06'
 
   depends_on 'xcb_proto'
+  depends_on 'libxau'
+  depends_on 'pthread_stubs'
 
   def self.build
     system "./configure"
