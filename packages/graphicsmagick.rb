aeeda42829cf77322799b4662a14f5602afc6f33
Ed Reel <edreel@gmail.com>
2017-07-23 10:13:30 -0500

    Update graphicsmagick from 1.3.23 to 1.3.26

--- packages/graphicsmagick.rb
+++ packages/graphicsmagick.rb
@@ -3,15 +3,15 @@ require 'package'
 class Graphicsmagick < Package
   description 'GraphicsMagick is the swiss army knife of image processing.'
   homepage 'http://www.graphicsmagick.org/'
-  version '1.3.23'
-  source_url 'http://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.23/GraphicsMagick-1.3.23.tar.gz'
-  source_sha256 'cb320e009173c66927041a675755fad454b8aadf1da2c6fd1d65eac298c556db'
+  version '1.3.26'
+  source_url 'https://downloads.sourceforge.net/project/graphicsmagick/graphicsmagick/1.3.26/GraphicsMagick-1.3.26.tar.xz'
+  source_sha256 'fba015f3d5e5d5f17e57db663f1aa9d338e7b62f1d415b85d13ee366927e5f88'
 
   depends_on 'buildessential'
 
   def self.build
-    system "./configure"
-    system "make"
+    system './configure'
+    system 'make'
   end
 
   def self.install
