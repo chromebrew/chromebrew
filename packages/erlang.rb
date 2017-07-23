378a1e32ee9cc4cd5d7ce26a6a3ae1522cab02e3
Ed Reel <edreel@gmail.com>
2017-07-22 22:55:09 -0500

    Update erlang from 19.2 20.0

--- packages/erlang.rb
+++ packages/erlang.rb
@@ -3,9 +3,9 @@ require 'package'
 class Erlang < Package
   description 'Erlang is a programming language used to build massively scalable soft real-time systems with requirements on high availability.'
   homepage 'http://www.erlang.org/'
-  version '19.2'
-  source_url 'http://www.erlang.org/download/otp_src_19.2.tar.gz'
-  source_sha256 'a016b3ef5dac1e532972617b2715ef187ecb616f7cd7ddcfe0f1d502f5d24870'
+  version '20.0'
+  source_url 'http://erlang.org/download/otp_src_20.0.tar.gz'
+  source_sha256 'fe80e1e14a2772901be717694bb30ac4e9a07eee0cc7a28988724cbd21476811'
 
   def self.build
     system 'export ERL_OTP=`pwd`'
