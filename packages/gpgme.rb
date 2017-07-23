1b1bf8bc5d99b4c8194712a43af4d61b9f5fced3
Ed Reel <edreel@gmail.com>
2017-07-23 10:08:38 -0500

    Update gpgme from 1.8.0 to 1.9.0

--- packages/gpgme.rb
+++ packages/gpgme.rb
@@ -3,12 +3,12 @@ require 'package'
 class Gpgme < Package
   description 'GnuPG Made Easy (GPGME) is a library designed to make access to GnuPG easier for applications.'
   homepage 'https://www.gnupg.org/related_software/gpgme/index.html'
-  version '1.8.0'
-  source_url 'https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.8.0.tar.bz2'
-  source_sha256 '596097257c2ce22e747741f8ff3d7e24f6e26231fa198a41b2a072e62d1e5d33'
+  version '1.9.0'
+  source_url 'https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.9.0.tar.bz2'
+  source_sha256 '1b29fedb8bfad775e70eafac5b0590621683b2d9869db994568e6401f4034ceb'
 
-  depends_on "libgpgerror"
-  depends_on "libassuan"
+  depends_on 'libgpgerror'
+  depends_on 'libassuan'
 
   def self.build
     system "./configure --prefix=/usr/local"
