c62459a8ff0490c9bcd3137733d23b7764c23e44
Ed Reel <edreel@gmail.com>
2017-07-22 23:58:00 -0500

    Update freetype from 2.7.1 to 2.8

--- packages/freetype.rb
+++ packages/freetype.rb
@@ -3,9 +3,9 @@ require 'package'
 class Freetype < Package
   description 'FreeType is a freely available software library to render fonts.'
   homepage 'https://www.freetype.org/'
-  version '2.7.1'
-  source_url 'http://download.savannah.gnu.org/releases/freetype/freetype-2.7.1.tar.gz'
-  source_sha256 '162ef25aa64480b1189cdb261228e6c5c44f212aac4b4621e28cf2157efb59f5'
+  version '2.8'
+  source_url 'http://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.bz2'
+  source_sha256 'a3c603ed84c3c2495f9c9331fe6bba3bb0ee65e06ec331e0a0fb52158291b40b'
 
   def self.build
       system "./configure CFLAGS=\" -fPIC\""
