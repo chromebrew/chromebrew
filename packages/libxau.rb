40db30cc490970f5e3662986c00cf84f83b425db
Jan Baudisch <dev@baudisch.xyz>
2017-07-22 19:50:10 -0700

    Update libxau dependencies

--- packages/libxau.rb
+++ packages/libxau.rb
@@ -6,7 +6,9 @@ class Libxau < Package
   version '1.0.8'
   source_url 'https://www.x.org/archive/individual/lib/libXau-1.0.8.tar.gz'
   source_sha256 'c343b4ef66d66a6b3e0e27aa46b37ad5cab0f11a5c565eafb4a1c7590bc71d7b'
-
+  
+  depends_on 'xproto'
+  
   def self.build
     system "./configure"
     system "make"
