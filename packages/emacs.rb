e5ed48891cd1ac942530d9e7f9db9ff91e05c514
Ed Reel <edreel@gmail.com>
2017-07-22 22:31:56 -0500

    Update emacs from 25.1 to 25.2

--- packages/emacs.rb
+++ packages/emacs.rb
@@ -3,19 +3,23 @@ require 'package'
 class Emacs < Package
   description 'An extensible, customizable, free/libre text editor - and more.'
   homepage 'http://www.gnu.org/software/emacs/'
-  version '25.1'
-  source_url 'ftp://ftp.gnu.org/gnu/emacs/emacs-25.1.tar.xz'
-  source_sha256 '19f2798ee3bc26c95dca3303e7ab141e7ad65d6ea2b6945eeba4dbea7df48f33'
+  version '25.2'
+  source_url 'https://ftp.gnu.org/gnu/emacs/emacs-25.2.tar.xz'
+  source_sha256 '59b55194c9979987c5e9f1a1a4ab5406714e80ffcfd415cc6b9222413bc073fa'
 
-  depends_on "zlibpkg" => :build
-  depends_on "diffutils" => :build
-  depends_on "m4" => :build
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on 'zlibpkg' => :build
+  depends_on 'diffutils' => :build
+  depends_on 'm4' => :build
+  depends_on 'autoconf' => :build
+  depends_on 'automake' => :build
 
   def self.build
-    system "./configure --prefix=/usr/local --without-x --without-makeinfo --without-selinux"
-    system "make"
+    system './configure \
+            --with-x=no \
+            --localstatedir=/usr/local/share \
+            --without-makeinfo \
+            --without-selinux'
+    system 'make'
   end
 
   def self.install
