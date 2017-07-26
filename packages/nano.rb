b6348a7a1eb01d73a8b585b757e9fbc00faf0153
Jan Baudisch <dev@baudisch.xyz>
2017-07-25 21:49:31 -0700

    Update nano

--- packages/nano.rb
+++ packages/nano.rb
@@ -3,9 +3,9 @@ require 'package'
 class Nano < Package
   description 'Nano\'s ANOther editor, an enhanced free Pico clone.'
   homepage 'https://www.nano-editor.org/'
-  version '2.8.4'
-  source_url 'https://nano-editor.org/dist/v2.8/nano-2.8.4.tar.xz'
-  source_sha256 'c7cf264f0f3e4af43ecdbc4ec72c3b1e831c69a1a5f6512d5b0c109e6bac7b11'
+  version '2.8.6'
+  source_url 'https://www.nano-editor.org/dist/v2.8/nano-2.8.6.tar.xz'
+  source_sha256 '9a46962a3ae59db922467a095217ed23280b42d80640f932f3a59ba2a6a85776'
 
   depends_on 'buildessential'
   depends_on 'ncurses'
