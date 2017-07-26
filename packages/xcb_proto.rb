7524e06a8c8bdf27f8b275ada757e59a371157bd
Jan Baudisch <dev@baudisch.xyz>
2017-07-25 22:48:18 -0700

    Add python dependency to xcbproto

--- packages/xcb_proto.rb
+++ packages/xcb_proto.rb
@@ -7,6 +7,8 @@ class Xcb_proto < Package
   source_url 'https://www.x.org/archive/individual/xcb/xcb-proto-1.12.tar.gz'
   source_sha256 'cfa49e65dd390233d560ce4476575e4b76e505a0e0bacdfb5ba6f8d0af53fd59'
 
+  depends_on 'python'
+  
   def self.build
     system "./configure"
     system "make"
