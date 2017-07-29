ed60aae69a48541a78d166884ef5442a89dcaf5e
Ed Reel <edreel@gmail.com>
2017-07-29 10:25:24 -0500

    Update harfbuzz from 1.4.5 to 1.4.7

--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -3,9 +3,9 @@ require 'package'
 class Harfbuzz < Package
   description 'HarfBuzz is an OpenType text shaping engine.'
   homepage 'https://www.freedesktop.org/wiki/Software/HarfBuzz/'
-  version '1.1.5'
-  source_url 'https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.4.5.tar.bz2'
-  source_sha256 'd0e05438165884f21658154c709075feaf98c93ee5c694b951533ac425a9a711'
+  version '1.4.7'
+  source_url 'https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-1.4.7.tar.bz2'
+  source_sha256 'b85f6627425d54f32118308e053b939c63a388de9bf455b3830f68cad406bc6d'
 
   def self.build
     system "./configure"
