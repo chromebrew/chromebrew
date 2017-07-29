47d3bac884acb29b5eb52ccaaa54a418cf28d73f
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-07-29 13:04:42 +0900

    Add source compile method for arm

--- packages/stack.rb
+++ packages/stack.rb
@@ -12,10 +12,20 @@ class Stack < Package
   when 'x86_64'
     source_url 'https://github.com/commercialhaskell/stack/releases/download/v1.4.0/stack-1.4.0-linux-x86_64.tar.gz'
     source_sha256 '618a309d763432a2cca654bc29249a77c7de096c693a28b84dd3656470269f5a'
+  else
+    # dummy file
+    source_url 'https://github.com/commercialhaskell/stack/releases/download/v1.4.0/stack-1.4.0-linux-x86_64.tar.gz'
+    source_sha256 '618a309d763432a2cca654bc29249a77c7de096c693a28b84dd3656470269f5a'
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
-    system "cp stack #{CREW_DEST_DIR}/usr/local/bin"
+    system "mkdir -p #{CREW_DEST_DIR}#{CREW_PREFIX}/bin"
+    case ARCH
+    when 'i686', 'x86_64'
+      system "cp stack #{CREW_DEST_DIR}#{CREW_PREFIX}/bin"
+    else
+      # create stack from scratch as described at https://docs.haskellstack.org/en/stable/README/
+      system "curl -sSL https://get.haskellstack.org/ | sh -s - -d #{CREW_DEST_DIR}#{CREW_PREFIX}/bin"
+    end
   end
 end
