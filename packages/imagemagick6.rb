080cc3e5a051a68a676a1aed39b5ba43b57dce08
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-07-30 07:26:44 +0900

    Change imagemagick6.rb to update it to 6.9.9-4

--- packages/imagemagick6.rb
+++ packages/imagemagick6.rb
@@ -3,9 +3,9 @@ require 'package'
 class Imagemagick6 < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '6.9.8.10'
-  source_url 'https://www.imagemagick.org/download/ImageMagick-6.9.8-10.tar.xz'
-  source_sha256 '8fc268f6e1bc514b41620e0f3f6c5dd33bfc5169db679e9a5c0455c6edd11810'
+  version '6.9.9-4'
+  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-6.9.9-4.tar.xz'
+  source_sha256 'beb6545d8914005ed4e1cc6b718cab57b4f6d26d01361792e6ad75050a44f4c1'
 
   depends_on 'pkgconfig'
   depends_on "libjpeg"
