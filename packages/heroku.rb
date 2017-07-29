4a8394a9c46c98b753a576642b3ae284655aff50
Ed Reel <edreel@gmail.com>
2017-07-29 11:19:04 -0500

    Update heroku from 4.28.3 to 6.13.5

--- packages/heroku.rb
+++ packages/heroku.rb
@@ -3,15 +3,26 @@ require 'package'
 class Heroku < Package
   description 'The Heroku Command Line Interface (CLI), formerly known as the Heroku Toolbelt, is a tool for creating and managing Heroku apps from the command line / shell of various operating systems.'
   homepage 'https://devcenter.heroku.com/articles/heroku-cli'
-  version '4.28.3'
-  binary_url ({
-    armv7l: "https://drive.google.com/uc?export=download&id=0ByCixsDmZPzxOHFIMzQyNHNFUWc",
-    i686: "https://drive.google.com/uc?export=download&id=0ByCixsDmZPzxd3NULTRkMWlHQTA",
-    x86_64: "https://drive.google.com/uc?export=download&id=0ByCixsDmZPzxLURkMktpREpDZk0"
-  })
-  binary_sha256 ({
-    armv7l: "b7fab53d3c8cfd42d41c1b07db069c9fd5f7261fcea48fd99114981cf1a293d7",
-    i686: "1e73fbff3a10c0c95dd1aa1cf68952a95a330634fa410765962df0396315a42c",
-    x86_64: "9259cd8b5d7aaeb9172142b5956ad767a3d4f1bcf126e51a7f4a8c055e53c068"
-  })
+  version '6.13.5'
+  source_url 'https://github.com/heroku/cli/archive/v6.13.5.tar.gz'
+  source_sha256 '769943a2e272f9b9332d85cfd5d4f49a7a3070a06b81e6da1cb33b4723a85607'
+
+  def self.install
+    case ARCH
+    when 'i686'
+      system 'wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x86.tar.gz -O heroku-cli.tar.gz'
+    when 'x86_64'
+      system 'wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku-cli.tar.gz'
+    else
+      abort 'Architecture not supported.'.lightred
+    end
+    system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
+    system "mkdir -p #{CREW_DEST_DIR}/usr/local/lib"
+    system "tar xvf heroku-cli.tar.gz"
+    system "mv heroku-cli-v* heroku-cli-linux-#{ARCH}"
+    system "mv heroku-cli-linux-#{ARCH} #{CREW_DEST_DIR}/usr/local/lib"
+    FileUtils.cd("#{CREW_DEST_DIR}/usr/local/bin") do
+      system "ln -s /usr/local/lib/heroku-cli-linux-#{ARCH}/bin/heroku heroku"
+    end
+  end
 end
