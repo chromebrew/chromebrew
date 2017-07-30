f9baba7543251d79426855297f9050e1b796474a
Ed Reel <edreel@gmail.com>
2017-07-30 11:30:21 -0500

    Update lha from 1.14i-ac20040929 to 1.14i-ac20050924p1

--- packages/lha.rb
+++ packages/lha.rb
@@ -3,9 +3,9 @@ require 'package'
 class Lha < Package
   description 'LHa for UNIX is a console-based utility that uncompresses and compresses .lha packages.'
   homepage 'http://freecode.com/projects/lhaforunix'
-  version '1.14i-ac20040929'
-  source_url 'http://prdownloads.sourceforge.jp/lha/11617/lha-1.14i-ac20040929.tar.gz'
-  source_sha256 '6da392c63768c931c038cab7cd9939a2b05d390bff6922cc45faeed7b7530b8b'
+  version '1.14i-ac20050924p1'
+  source_url 'http://prdownloads.sourceforge.jp/lha/22231/lha-1.14i-ac20050924p1.tar.gz'
+  source_sha256 'b5261e9f98538816aa9e64791f23cb83f1632ecda61f02e54b6749e9ca5e9ee4'
 
   def self.build
     system './configure'
