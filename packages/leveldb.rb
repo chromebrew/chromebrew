db129c057675863a0560e323c3f940e35f4e1f80
Ed Reel <edreel@gmail.com>
2017-07-30 11:20:05 -0500

    Update leveldb from 1.19 to 1.20

--- packages/leveldb.rb
+++ packages/leveldb.rb
@@ -3,9 +3,9 @@ require 'package'
 class Leveldb < Package
   description 'LevelDB is a fast key-value storage library written at Google that provides an ordered mapping from string keys to string values.'
   homepage 'https://leveldb.googlecode.com/'
-  version '1.19.0'
-  source_url 'https://github.com/google/leveldb/archive/v1.19.tar.gz'
-  source_sha256 '7d7a14ae825e66aabeb156c1c3fae9f9a76d640ef6b40ede74cc73da937e5202'
+  version '1.20'
+  source_url 'https://github.com/google/leveldb/archive/v1.20.tar.gz'
+  source_sha256 'f5abe8b5b209c2f36560b75f32ce61412f39a2922f7045ae764a2c23335b6664'
 
   def self.build
     system "make"
@@ -16,7 +16,7 @@ class Leveldb < Package
     system "mkdir", "-p", "#{CREW_DEST_DIR}/usr/local/lib"
     system "cp", "-R", "include/leveldb", "#{CREW_DEST_DIR}/usr/local/include"
     system "cp", "out-static/libleveldb.a", "#{CREW_DEST_DIR}/usr/local/lib"
-    system "cp", "out-shared/libleveldb.so.1.19", "#{CREW_DEST_DIR}/usr/local/lib"
+    system "cp", "out-shared/libleveldb.so.1.20", "#{CREW_DEST_DIR}/usr/local/lib"
     system "cp", "-P", "out-shared/libleveldb.so.1", "#{CREW_DEST_DIR}/usr/local/lib"
     system "cp", "-P", "out-shared/libleveldb.so", "#{CREW_DEST_DIR}/usr/local/lib"
   end
