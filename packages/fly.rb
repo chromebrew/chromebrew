48b02ff8fc80cdf25e3fa1cf9f936460db82bf00
Ed Reel <edreel@gmail.com>
2017-07-31 00:29:39 -0500

    Add fly package

---
+++ packages/fly.rb
@@ -0,0 +1,20 @@
+require 'package'
+
+class Fly < Package
+  description 'fly is a C program that creates PNG, JPEG or GIF images on the fly from CGI and other programs.'
+  homepage 'http://martin.gleeson.com/fly/index.html'
+  version '1.6.5'
+  source_url 'ftp://ftp.unimelb.edu.au/pub/www/tools/unix/fly/fly-1.6.5.tar.gz'
+  source_sha256 '8411915a9acd9a3d86896db5a53ac6bd4e35b44106c0f180cc0f96a6f02a62c9'
+
+  depends_on 'libgd'
+
+  def self.build
+    system 'make'
+  end
+
+  def self.install
+    system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
+    system "cp fly #{CREW_DEST_DIR}/usr/local/bin"
+  end
+end
