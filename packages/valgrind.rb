872db86be1d593fd653c2d33460ad4bb79164fb0
Yan Couto <yancouto@gmail.com>
2017-08-02 14:10:46 -0300

    dependency and version fix for valgrind

--- packages/valgrind.rb
+++ packages/valgrind.rb
@@ -3,9 +3,11 @@ require 'package'
 class Valgrind < Package
   description 'Valgrind is an instrumentation framework for building dynamic analysis tools.'
   homepage 'http://valgrind.org/'
-  version '3.13.0'
+  version '3.13.0-1'
   source_url 'ftp://sourceware.org/pub/valgrind/valgrind-3.13.0.tar.bz2'
   source_sha256 'd76680ef03f00cd5e970bbdcd4e57fb1f6df7d2e2c071635ef2be74790190c3b'
+
+  depends_on 'patch' => :build
 
   def self.build
     system 'patch -p0 << EOF
