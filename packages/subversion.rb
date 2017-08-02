e7804464b77794ca9640030318287f2b89503292
Ed Reel <edreel@gmail.com>
2017-08-02 00:01:56 -0500

    Bump version so 'crew upgrade' works

--- packages/subversion.rb
+++ packages/subversion.rb
@@ -3,7 +3,7 @@ require 'package'
 class Subversion < Package
   description 'Subversion is an open source version control system.'
   homepage 'https://subversion.apache.org/'
-  version '1.9.6'
+  version '1.9.6-1'
   source_url 'http://apache.mirrors.ionfish.org/subversion/subversion-1.9.6.tar.bz2'
   source_sha256 'dbcbc51fb634082f009121f2cb64350ce32146612787ffb0f7ced351aacaae19'
 
