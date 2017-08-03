bcc3c5333ac23a11d0898f024af672b7a19d8d37
Ed Reel <edreel@gmail.com>
2017-08-02 22:21:08 -0500

    Change heroku to build from yarn

--- packages/heroku.rb
+++ packages/heroku.rb
@@ -7,22 +7,19 @@ class Heroku < Package
   source_url 'https://github.com/heroku/cli/archive/v6.13.5.tar.gz'
   source_sha256 '769943a2e272f9b9332d85cfd5d4f49a7a3070a06b81e6da1cb33b4723a85607'
 
+  depends_on 'yarn'
+
+  def self.build
+    system '. ~/.bashrc && nvm install stable'
+    system 'yarn install'
+  end
+
   def self.install
-    case ARCH
-    when 'i686'
-      system 'wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x86.tar.gz -O heroku-cli.tar.gz'
-    when 'x86_64'
-      system 'wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku-cli.tar.gz'
-    else
-      abort 'Architecture not supported.'.lightred
-    end
     system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/lib"
-    system "tar xvf heroku-cli.tar.gz"
-    system "mv heroku-cli-v* heroku-cli-linux-#{ARCH}"
-    system "mv heroku-cli-linux-#{ARCH} #{CREW_DEST_DIR}/usr/local/lib"
+    system "mkdir -p #{CREW_DEST_DIR}/usr/local/share/heroku"
+    system "cp -r . #{CREW_DEST_DIR}/usr/local/share/heroku"
     FileUtils.cd("#{CREW_DEST_DIR}/usr/local/bin") do
-      system "ln -s /usr/local/lib/heroku-cli-linux-#{ARCH}/bin/heroku heroku"
+      system "ln -s /usr/local/share/heroku/bin/run heroku"
     end
   end
 end
