a09c6b30268eb7707e9c89348c4422e22fb0c874
Ed Reel <edreel@gmail.com>
2017-08-02 23:48:52 -0500

    Bump version from 6.13.5 to 6.13.6

--- packages/heroku.rb
+++ packages/heroku.rb
@@ -3,9 +3,9 @@ require 'package'
 class Heroku < Package
   description 'The Heroku Command Line Interface (CLI), formerly known as the Heroku Toolbelt, is a tool for creating and managing Heroku apps from the command line / shell of various operating systems.'
   homepage 'https://devcenter.heroku.com/articles/heroku-cli'
-  version '6.13.5'
-  source_url 'https://github.com/heroku/cli/archive/v6.13.5.tar.gz'
-  source_sha256 '769943a2e272f9b9332d85cfd5d4f49a7a3070a06b81e6da1cb33b4723a85607'
+  version '6.13.6'
+  source_url 'https://github.com/heroku/cli/archive/v6.13.6.tar.gz'
+  source_sha256 '395c7e22d16b1fce2d5ed4ac1e022dbd3c9aa8fdbc172d9dadf86ba9c1ad6978'
 
   depends_on 'nvm'
   depends_on 'yarn'
