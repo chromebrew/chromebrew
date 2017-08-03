8fe385e51c280965438fe83cd6e0912f90a12a0f
Ed Reel <edreel@gmail.com>
2017-08-02 20:39:37 -0500

    Change source_url and add dependencies

--- packages/httpd.rb
+++ packages/httpd.rb
@@ -4,20 +4,21 @@ class Httpd < Package
   description 'The Apache HTTP Server Project is an effort to develop and maintain an open-source HTTP server for modern operating systems including UNIX and Windows.'
   homepage 'http://httpd.apache.org/'
   version '2.4.27'
-  source_url 'https://github.com/apache/httpd/archive/2.4.27.tar.gz'
-  source_sha256 '20d6a9258d408077b822d223da7893a905b185fbca518f4399445a68ad971b21'
+  source_url 'http://www-us.apache.org/dist/httpd/httpd-2.4.27.tar.gz'
+  source_sha256 '346dd3d016ae5d7101016e68805150bdce9040a8d246c289aa70e68a7cd86b66'
 
-  depends_on 'subversion'
+  depends_on 'apr'
+  depends_on 'aprutil'
+  depends_on 'libtool'
+  depends_on 'pcre'
 
   def self.build
-    system 'svn co https://svn.apache.org/repos/asf/apr/apr/branches/1.5.x srclib/apr'
-    system 'svn co https://svn.apache.org/repos/asf/apr/apr-util/branches/1.5.x srclib/apr-util'
-    system './buildconf'
-    system './configure --with-included-apr'
+    system './configure'
     system 'make'
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "libtool --finish #{CREW_DEST_DIR}#{CREW_PREFIX}/apache2/modules"
   end
 end
