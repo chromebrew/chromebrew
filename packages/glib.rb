41dc447a3a42f9b18b1656cdb03e231599ca2dfa
Ed Reel <edreel@gmail.com>
2017-08-05 14:10:57 -0500

    Fix to make glib visibile

--- packages/glib.rb
+++ packages/glib.rb
@@ -17,5 +17,12 @@ class Glib < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    FileUtils.cd("#{CREW_DEST_DIR}#{CREW_PREFIX}/include") do
+      system "cp -r glib-2.0/* ."
+      system "cp #{CREW_DEST_DIR}#{CREW_PREFIX}/lib/glib-2.0/include/glibconfig.h ."
+      system "rm -rf #{CREW_DEST_DIR}#{CREW_PREFIX}/lib/glib-2.0/"
+      system "rm -rf #{CREW_DEST_DIR}#{CREW_PREFIX}/lib/gio/"
+      system "rm -rf glib-2.0/"
+    end
   end
 end
