93fdd0e8ff0e9f5775ad129e1cdcdd8ed62b396c
Ed Reel <edreel@gmail.com>
2017-08-05 00:07:31 -0500

    Update mapserver from 7.0.4 to 7.0.6

--- packages/mapserver.rb
+++ packages/mapserver.rb
@@ -3,19 +3,39 @@ require 'package'
 class Mapserver < Package
   description 'MapServer is an Open Source platform for publishing spatial data and interactive mapping applications to the web.'
   homepage 'http://mapserver.org/'
-  version '7.0.4'
-  source_url 'http://download.osgeo.org/mapserver/mapserver-7.0.4.tar.gz'
-  source_sha256 'c91d40da5cc242b20ca7e29c41bd9eb23097b98a77908b1d708e9708f6f6cf69'
+  version '7.0.6'
+  source_url 'http://download.osgeo.org/mapserver/mapserver-7.0.6.tar.gz'
+  source_sha256 'dcbebd62976deef1490b084d8f6a0b2f2a1a25407efb6e058390025375539507'
 
   depends_on 'cmake'
-  depends_on 'freetype'
   depends_on 'cairo'
   depends_on 'curl'
+  depends_on 'fribidi'
+  depends_on 'gdal'
+  depends_on 'geos'
+  depends_on 'harfbuzz'
+  depends_on 'libjpeg'
+  depends_on 'proj4'
 
   def self.build
     system "mkdir build"
     Dir.chdir "build" do
-      system "cmake -DWITH_THREAD_SAFETY=1 -DWITH_HARFBUZZ=0 -DWITH_FRIBIDI=0 -DWITH_FCGI=0 -DWITH_POSTGIS=0 -DWITH_GIF=0 -DWITH_CLIENT_WFS=1 -DWITH_CLIENT_WMS=1 -DWITH_CURL=1 -DCMAKE_C_FLAGS=\" -fPIC\" -DCMAKE_INSTALL_PREFIX=/usr/local .. > ../configure.out.txt"
+      system "cmake \
+              -DWITH_CLIENT_WFS=1 \
+              -DWITH_CLIENT_WMS=1 \
+              -DWITH_CURL=1 \
+              -DWITH_FCGI=0 \
+              -DWITH_FRIBIDI=1 \
+              -DWITH_GDAL=1 \
+              -DWITH_GIF=0 \
+              -DWITH_HARFBUZZ=1 \
+              -DWITH_OGR=1 \
+              -DWITH_POSTGIS=0 \
+              -DWITH_PROJ=1 \
+              -DWITH_THREAD_SAFETY=1 \
+              -DWITH_WCS=1 \
+              -DCMAKE_C_FLAGS=\" -fPIC\" \
+              -DCMAKE_INSTALL_PREFIX=/usr/local .. > ../configure.out.txt"
       system "make"
     end
   end
