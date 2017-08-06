249a7fbac6be6c076d2e1d988d972c602ed21d95
Ed Reel <edreel@gmail.com>
2017-08-06 09:20:53 -0500

    Add libxslt dependency

--- packages/mutt.rb
+++ packages/mutt.rb
@@ -7,7 +7,10 @@ class Mutt < Package
   source_url 'ftp://ftp.mutt.org/pub/mutt/mutt-1.8.3.tar.gz'
   source_sha256 '9b81746d67ffeca5ea44f60893b70dc93c86d4bc10187d4dd360185e4d42ed42'
 
+  depends_on 'libxslt'
+
   def self.build
+    system 'mkdir /usr/local/mail'
     system './configure --with-mailpath=/usr/local/mail --prefix=/usr/local'
     system 'make'
   end
