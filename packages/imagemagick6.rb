a9d5514d84e9e7aab4c527d7d6f9ef52da32d832
Vince Wadhwani <vince@econify.com>
2017-08-06 18:38:31 -0400

    update imagemagick6 also adds missing dependency.

--- packages/imagemagick6.rb
+++ packages/imagemagick6.rb
@@ -3,15 +3,16 @@ require 'package'
 class Imagemagick6 < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '6.9.9-4'
-  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-6.9.9-4.tar.xz'
-  source_sha256 'beb6545d8914005ed4e1cc6b718cab57b4f6d26d01361792e6ad75050a44f4c1'
+  version '6.9.9-5'
+  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-6.9.9-5.tar.xz'
+  source_sha256 'aa6444ece8d29b87fb4f25e997cd0b4fb48774206cea9490c6028babdee7d056'
 
   depends_on 'pkgconfig'
   depends_on "libjpeg"
   depends_on "libpng"
   depends_on "libtiff"
   depends_on "freetype"
+  depends_on "zlibpkg"
 
   def self.build
     system "./configure CFLAGS=\" -fPIC\" --without-python"
