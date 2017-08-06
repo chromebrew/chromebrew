58fca7853718c9fff1a51fac30b4ccf2f1f2afc3
Ed Reel <edreel@gmail.com>
2017-08-05 22:59:54 -0500

    Update mosh from 1.3.0 to 1.3.2

--- packages/mosh.rb
+++ packages/mosh.rb
@@ -3,9 +3,9 @@ require 'package'
 class Mosh < Package
   description 'Remote terminal application that allows roaming, supports intermittent connectivity, and provides intelligent local echo and line editing of user keystrokes.'
   homepage 'https://mosh.org/'
-  version '1.3.0'
-  source_url 'https://mosh.org/mosh-1.3.0.tar.gz'
-  source_sha256 '320e12f461e55d71566597976bd9440ba6c5265fa68fbf614c6f1c8401f93376'
+  version '1.3.2'
+  source_url 'https://mosh.org/mosh-1.3.2.tar.gz'
+  source_sha256 'da600573dfa827d88ce114e0fed30210689381bbdcff543c931e4d6a2e851216'
 
   depends_on 'protobuf'
 
