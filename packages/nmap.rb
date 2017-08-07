72ed61076ac0723d376f6e45af0e2e3164b409dc
Ed Reel <edreel@gmail.com>
2017-08-06 23:58:15 -0500

    Update nmap from 7.50 to 7.60

--- packages/nmap.rb
+++ packages/nmap.rb
@@ -3,9 +3,9 @@ require 'package'
 class Nmap < Package
   description 'Nmap (\'Network Mapper\') is a free and open source (license) utility for network discovery and security auditing.'
   homepage 'https://nmap.org/'
-  version '7.50'
-  source_url 'https://nmap.org/dist/nmap-7.50.tgz'
-  source_sha256 '40febe4a4e4b583aabcdd8cfceb6ae0f366dbb2fede96e4a529340bdb6d24776'
+  version '7.60'
+  source_url 'https://nmap.org/dist/nmap-7.60.tar.bz2'
+  source_sha256 'a8796ecc4fa6c38aad6139d9515dc8113023a82e9d787e5a5fb5fa1b05516f21'
 
   depends_on 'buildessential'
 
