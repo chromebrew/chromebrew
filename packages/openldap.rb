60c2542a0342cb3db10b89b5706dd6beecacb896
Ed Reel <edreel@gmail.com>
2017-08-07 22:34:37 -0500

    Update openldap from 2.4.44 to 2.4.45

--- packages/openldap.rb
+++ packages/openldap.rb
@@ -3,9 +3,9 @@ require 'package'
 class Openldap < Package
   description 'OpenLDAP Software is an open source implementation of the Lightweight Directory Access Protocol.'
   homepage 'http://www.openldap.org/'
-  version '2.4.44'
-  source_url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.44.tgz'
-  source_sha256 'd7de6bf3c67009c95525dde3a0212cc110d0a70b92af2af8e3ee800e81b88400'
+  version '2.4.45'
+  source_url 'ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.45.tgz'
+  source_sha256 'cdd6cffdebcd95161a73305ec13fc7a78e9707b46ca9f84fb897cd5626df3824'
 
   def self.build
     system "./configure --disable-slapd --prefix=/usr/local"
