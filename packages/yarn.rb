d6f393e54db7419efba9f2ab4e6b60bf14b390f7
Ed Reel <edreel@gmail.com>
2017-08-10 00:34:38 -0500

    Remove nodebrew dependency

--- packages/yarn.rb
+++ packages/yarn.rb
@@ -1,7 +1,5 @@
 require 'package'
 
-NODE_VERSION = `node -v 2> /dev/null`
-
 class Yarn < Package
   description 'Yarn is a new package manager for JavaScript and an alternative to npm.'
   homepage 'https://yarnpkg.com/en/'
@@ -9,10 +7,7 @@ class Yarn < Package
   source_url 'https://nightly.yarnpkg.com/yarn-v1.0.0-20170808.1058.tar.gz'
   source_sha256 '7056c5ee12aa8d014d59b377be2e494ce49a6059e2678d8f79f1fc9904eb008b'
 
-  depends_on 'nodebrew' unless "#{NODE_VERSION}" != ""
-
   def self.install
-    system "nodebrew install-binary latest && nodebrew use latest" unless "#{NODE_VERSION}" != ""
     system "mkdir -p #{CREW_DEST_DIR}/usr/local"
     system "cp -r lib/ #{CREW_DEST_DIR}/usr/local"
     system "cp -r bin/ #{CREW_DEST_DIR}/usr/local"
