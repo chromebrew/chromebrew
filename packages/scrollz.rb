f8d4939d5b6c0cc3d448de0144f614b810d59488
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-12 11:46:15 +0900

    Change scrollz to disable to use termcap since it causes link errors.

--- packages/scrollz.rb
+++ packages/scrollz.rb
@@ -3,7 +3,7 @@ require 'package'
 class Scrollz < Package
   description 'ScrollZ is an advanced IRC client based on ircII.'
   homepage 'http://www.scrollz.info/'
-  version '2.3'
+  version '2.3-1'
   source_url 'http://www.scrollz.info/download/ScrollZ-2.3.tar.gz'
   source_sha256 '22535bcc54ad752107ab181775d90d9cf1b37648f500d627f428388a9d3710e6'
 
@@ -11,7 +11,8 @@ class Scrollz < Package
   depends_on 'ncurses'
 
   def self.build
-    system "CFLAGS=-I/usr/local/include/ncurses ./configure"
+    # Force to disable using termcap since it causes link errors.
+    system "CFLAGS=-I/usr/local/include/ncurses ./configure ac_cv_header_termcap_h=no ac_cv_lib_termcap_tgetent=no"
     system "make"
   end
 
