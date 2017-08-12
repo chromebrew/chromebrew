97e4a4a24787b0875429a160ba611169c011e5af
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-12 11:09:53 +0900

    Change libmp3lame to make it compilable on i686. This modification is required for only i686 using gcc-4.x. It will be possible to remove this line once we start using gcc-5.x.

--- packages/libmp3lame.rb
+++ packages/libmp3lame.rb
@@ -8,7 +8,15 @@ class Libmp3lame < Package
   source_sha256 '24346b4158e4af3bd9f2e194bb23eb473c75fb7377011523353196b19b9a23ff'
 
   def self.build
-    system "./configure"
+    case ARCH
+    when 'i686'
+      # gcc-4.x for i686 has problem, so forcing to use -msse here.
+      # see https://mail-index.netbsd.org/pkgsrc-users/2015/04/18/msg021393.html
+      # and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=56145 for details.
+      system 'CFLAGS="-O3 -fomit-frame-pointer -ffast-math -maccumulate-outgoing-args -msse" ./configure'
+    else
+      system "./configure"
+    end
     system "make"
   end
 
