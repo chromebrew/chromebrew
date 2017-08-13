8be98be0633f50f108f6b45d4a85c5814d27e117
Ed Reel <edreel@gmail.com>
2017-08-12 22:56:03 -0500

    Update dropbox from 30.4.22 to 32.4.23

--- packages/dropbox.rb
+++ packages/dropbox.rb
@@ -1,31 +1,34 @@
 require 'package'
 
 class Dropbox < Package
-  description 'Dropbox simplifies the way you create, share and collaborate. Bring your photos, docs, and videos anywhere and keep your files safe.'
+  description 'Dropbox simplifies the way you create, share and collaborate.  Bring your photos, docs, and videos anywhere and keep your files safe.'
   homepage 'https://www.dropbox.com/'
-  version '30.4.22'
+  version '32.4.23'
   case ARCH
   when 'i686'
-    source_url 'https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86-30.4.22.tar.gz'
-    source_sha256 '4b41ab4fae4b02a0428cbafccb45ecde82b2b3b774127826f1945f660e0ed47f'
+    source_url "https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86-32.4.23.tar.gz"
+    source_sha256 'd7e130f2872fb2d141f8d2f892f7a2c29b95ccd3620398c21ea53dee878dc075'
   when 'x86_64'
-    source_url 'https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86_64-30.4.22.tar.gz'
-    source_sha256 '1928e49d91a5c039f4903c5bc12be032eb499fcc8352b6eb2a33b40b8d4c8961'
+    source_url "https://clientupdates.dropboxstatic.com/dbx-releng/client/dropbox-lnx.x86_64-32.4.23.tar.gz"
+    source_sha256 'a18dca750e72e0604b9798bfe5e0b9b7a2b5ed43116ab96166a298ae3c1b5086'
   else
-    puts 'Unable to install dropboxd.  Supported architectures include x86 and x86_64 only.'.lightred
+    puts 'Unable to install dropboxd.  Supported architectures include i686 and x86_64 only.'.lightred
   end
 
   depends_on 'python27' unless File.exists? '/usr/local/bin/python'
 
-  def self.install
+  def self.build
     system "wget https://linux.dropbox.com/packages/dropbox.py"
-    system "sed -i 's,~/.dropbox-dist,/usr/local/bin,g' dropbox.py"
+    system "sed -i 's,~/.dropbox-dist,#{CREW_PREFIX}/bin,g' dropbox.py"
     system "echo '#!/bin/bash' > dropbox"
-    system "echo 'python /usr/local/bin/dropbox.py \$1' >> dropbox"
+    system "echo 'python #{CREW_PREFIX}/bin/dropbox.py \$1' >> dropbox"
     system "chmod +x dropbox"
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
-    system "cp -r .dropbox-dist/. #{CREW_DEST_DIR}/usr/local/bin"
-    system "cp dropbox.py #{CREW_DEST_DIR}/usr/local/bin"
-    system "cp dropbox #{CREW_DEST_DIR}/usr/local/bin"
+  end
+
+  def self.install
+    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
+    system "cp -r .dropbox-dist/* #{CREW_DEST_PREFIX}/bin"
+    system "cp dropbox.py #{CREW_DEST_PREFIX}/bin"
+    system "cp dropbox #{CREW_DEST_PREFIX}/bin"
   end
 end
