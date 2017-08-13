f522f382ba42a2504591894ebffe9156e330a3f3
Jan Baudisch <dev@baudisch.xyz>
2017-08-13 15:34:45 +0200

    Add libcap_ng

---
+++ packages/libcap_ng.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Libcap_ng < Package
+  description 'The libcap-ng library is intended to make programming with posix capabilities much easier than the traditional libcap library.'
+  homepage 'https://people.redhat.com/sgrubb/libcap-ng'
+  version '0.7.8'
+  source_url 'https://people.redhat.com/sgrubb/libcap-ng/libcap-ng-0.7.8.tar.gz'
+  source_sha256 'c21af997445cd4107a55d386f955c5ea6f6e96ead693e9151277c0ab5f97d05f'
+
+  def self.build
+    system "./configure"
+    system "make"
+  end
+
+  def self.install
+ 	  system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
