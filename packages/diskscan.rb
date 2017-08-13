5e03f42f595bcc14bea9b1c9b3507c2eecc3533e
Yan Couto <yancouto@gmail.com>
2017-08-13 12:10:29 -0300

    fixing diskscan and imagemagick

--- packages/diskscan.rb
+++ packages/diskscan.rb
@@ -7,7 +7,7 @@ class Diskscan < Package
   source_url 'https://github.com/baruch/diskscan/archive/0.19.tar.gz'
   source_sha256 '92a7298af99043e1e584e4343040b6574b9229f44c122e1cbcb90ba478d928d1'
 
-  depends_on 'cmake'
+  depends_on 'cmake' => :build
   depends_on 'termcap'
 
   def self.build
@@ -17,9 +17,5 @@ class Diskscan < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-  end
-
-  def self.check
-    system "make", "check"
   end
 end
