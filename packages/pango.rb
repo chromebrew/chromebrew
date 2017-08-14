d4cfbd99d50daf31280587c12bccf96f4d32a8b7
Ed Reel <edreel@gmail.com>
2017-08-14 00:00:41 -0500

    Update pango from 1.40.0 to 1.40.9

--- packages/pango.rb
+++ packages/pango.rb
@@ -3,12 +3,10 @@ require 'package'
 class Pango < Package
   description 'Pango is a library for laying out and rendering of text, with an emphasis on internationalization.'
   homepage 'http://www.pango.org/'
-  version '1.40.0'
-  source_url 'https://ftp.gnome.org/pub/gnome/sources/pango/1.40/pango-1.40.0.tar.xz'
-  source_sha256 'da17985df314cb07d066ab5424f59c21ce973ece05b7de4df04d798ec8511c8b'
+  version '1.40.9'
+  source_url 'https://ftp.gnome.org/pub/gnome/sources/pango/1.40/pango-1.40.9.tar.xz'
+  source_sha256 '9faea6535312fe4436b93047cf7a04af544eb52a079179bd3a33821aacce7e16'
 
-  depends_on 'freetype'
-  depends_on 'fontconfig'
   depends_on 'harfbuzz'
   depends_on 'cairo'
 
