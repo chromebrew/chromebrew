24d2648ed0cf75113bd53efb5c19c0f81e08a974
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-14 20:41:04 +0900

    Add individual package files to install perl modules. Also modify texinfo.rb to use those perl modules.

--- packages/texinfo.rb
+++ packages/texinfo.rb
@@ -3,18 +3,18 @@ require 'package'
 class Texinfo < Package
   description 'Texinfo is the official documentation format of the GNU project.'
   homepage 'https://www.gnu.org/software/texinfo/'
-  version '6.4'
+  version '6.4-1'
   source_url 'http://ftpmirror.gnu.org/texinfo/texinfo-6.4.tar.xz'
   source_sha256 '6ae2e61d87c6310f9af7c6f2426bd0470f251d1a6deb61fba83a3b3baff32c3a'
 
   depends_on 'gettext' => :build
   depends_on 'perl'
+  depends_on 'perl_locale_messages'
+  depends_on 'perl_text_unidecode'
+  depends_on 'perl_unicode_eastasianwidth'
   depends_on 'ncurses'
 
   def self.build
-    # installing necessary perl modules
-    system 'cpan', 'install', 'CPAN', 'Locale::Messages', 'Text::Unidecode', 'Unicode::EastAsianWidth'
-
     # configure and make
     system './configure',
         '--with-external-Text-Unidecode',
