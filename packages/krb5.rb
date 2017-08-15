d853175c200591acecf841d70219fd00201350ab
Jan Baudisch <dev@baudisch.xyz>
2017-08-15 17:49:36 +0200

    Add kerberos (krb5)

---
+++ packages/krb5.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Krb5 < Package
+  description 'Kerberos is a network authentication protocol.'
+  homepage 'https://web.mit.edu/kerberos'
+  version '1.15.1'
+  source_url 'http://web.mit.edu/kerberos/dist/krb5/1.15/krb5-1.15.1.tar.gz'
+  source_sha256 '437c8831ddd5fde2a993fef425dedb48468109bb3d3261ef838295045a89eb45'
+
+  def self.build
+    Dir.chdir "src" do
+ 	    system "./configure --without-system-verto"
+    	system "make"
+    end
+  end
+
+  def self.install
+    Dir.chdir "src" do
+ 	    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    end
+  end
+end
