978b9d6af35ac5f13fc2ee6f2862ff420caf02cc
Jan Baudisch <dev@baudisch.xyz>
2017-08-15 18:01:38 +0200

    Add libtirpc

---
+++ packages/libtirpc.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Libtirpc < Package
+  description 'Libtirpc is a port of Suns Transport-Independent RPC library to Linux.'
+  homepage 'https://sourceforge.net/projects/libtirpc'
+  version '1.0.2'
+  source_url 'http://downloads.sourceforge.net/project/libtirpc/libtirpc/1.0.2/libtirpc-1.0.2.tar.bz2'
+  source_sha256 '723c5ce92706cbb601a8db09110df1b4b69391643158f20ff587e20e7c5f90f5'
+
+  def self.build
+    system "./configure --prefix=/usr/local"
+    system "make"
+  end
+
+  def self.install
+ 	  system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "mkdir #{CREW_DEST_DIR}/usr/local/include/rpc"
+    system "mv #{CREW_DEST_DIR}/usr/local/include/tirpc/rpc/* #{CREW_DEST_DIR}/usr/local/include/rpc/"
+    
+    system "curl -Lo #{CREW_DEST_DIR}/usr/local/include/netconfig.h https://ftp.netbsd.org/pub/NetBSD/NetBSD-current/src/include/netconfig.h"
+  end
+end
