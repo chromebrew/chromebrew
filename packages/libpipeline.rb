ae38d364716831dac8f12d8dbbe6c1f908466ce3
Casey Strouse <casey.strouse@gmail.com>
2017-08-17 11:07:33 -0700

    Put wrong sha version
    sha1 -> sha256

--- packages/libpipeline.rb
+++ packages/libpipeline.rb
@@ -5,7 +5,7 @@ class Libpipeline < Package
   homepage 'http://libpipeline.nongnu.org/'
   version '1.4.2'
   source_url 'https://download.savannah.gnu.org/releases/libpipeline/libpipeline-1.4.2.tar.gz'
-  source_sha1 '1967691198434f7bc78ffaa2ef6556ee60b0ca4a'
+  source_sha256 'fef1fc9aa40ce8796f18cd1aecd888a9484a9556c8b0f8d07c863578277679be'
   
   def self.build
     system './configure', '--disable-static', '--enable-shared', '--with-pic'
