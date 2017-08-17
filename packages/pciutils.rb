29c56c9ef00df1a620aaf1af20ce611282e3aa34
Casey Strouse <casey.strouse@gmail.com>
2017-08-17 11:27:36 -0700

    Update pciutils from 3.5.2 to 3.5.5
    General bugfix release. Tested as working on XE500C13-K01US.

--- packages/pciutils.rb
+++ packages/pciutils.rb
@@ -3,9 +3,9 @@ require 'package'
 class Pciutils < Package
   description 'The PCI Utilities are a collection of programs for inspecting and manipulating configuration of PCI devices, all based on a common portable library libpci which offers access to the PCI configuration space on a variety of operating systems.'
   homepage 'http://mj.ucw.cz/sw/pciutils/'
-  version '3.5.2'
-  source_url 'https://www.kernel.org/pub/software/utils/pciutils/pciutils-3.5.2.tar.xz'
-  source_sha256 '3a99141a9f40528d0a0035665a06dc37ddb1ae341658e51b50a76ecf86235efc'
+  version '3.5.5'
+  source_url 'https://www.kernel.org/pub/software/utils/pciutils/pciutils-3.5.5.tar.xz'
+  source_sha256 '1d62f8fa192f90e61c35a6fc15ff3cb9a7a792f782407acc42ef67817c5939f5'
 
   def self.build
     system "make", "PREFIX=/usr/local", "SHARED=yes"
