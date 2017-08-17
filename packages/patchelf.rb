7a6790512b744d08124f53bc76db312505413829
Casey Strouse <casey.strouse@gmail.com>
2017-08-17 11:22:01 -0700

    Update patchelf from 0.8 to 0.9
    General bugfix release. Tested as working on XE500C13-K02US.

--- packages/patchelf.rb
+++ packages/patchelf.rb
@@ -3,9 +3,9 @@ require 'package'
 class Patchelf < Package
   description 'PatchELF is a small utility to modify the dynamic linker and RPATH of ELF executables.'
   homepage 'http://nixos.org/patchelf.html'
-  version '0.8-2'
-  source_url 'http://nixos.org/releases/patchelf/patchelf-0.8/patchelf-0.8.tar.bz2'
-  source_sha256 'c99f84d124347340c36707089ec8f70530abd56e7827c54d506eb4cc097a17e7'
+  version '0.9'
+  source_url 'http://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2'
+  source_sha256 'a0f65c1ba148890e9f2f7823f4bedf7ecad5417772f64f994004f59a39014f83'
 
   def self.build
     system "./configure", "prefix=/usr/local"
