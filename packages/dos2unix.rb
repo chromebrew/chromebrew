1f5332f4ec2634a3edc57e39acfd33e37e31f6ea
Yan Couto <yancouto@gmail.com>
2017-08-17 22:55:42 -0300

    clisp and dos2unix dependency fix

--- packages/dos2unix.rb
+++ packages/dos2unix.rb
@@ -7,6 +7,8 @@ class Dos2unix < Package
   source_url 'https://downloads.sourceforge.net/project/dos2unix/dos2unix/7.3.5/dos2unix-7.3.5.tar.gz'
   source_sha256 'a72caa2fb5cb739403315472fe522eda41aabab2a02ad6f5589639330af262e5'
 
+  depends_on 'gettext' => :build
+
   def self.build
     system 'make'
   end
