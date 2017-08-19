accbfe821436233551973b77fc9cfbe49bf2649a
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-19 11:56:21 +0900

    Change pacparser.rb to install files through CREW_DEST_DIR. Also Add -j1 flag to make to avoid race condition.

--- packages/pacparser.rb
+++ packages/pacparser.rb
@@ -3,15 +3,15 @@ require 'package'
 class Pacparser < Package
   description 'pacparser is a library to parse proxy auto-config (PAC) files.'
   homepage 'http://pacparser.manugarg.com/'
-  version '1.3.7'
+  version '1.3.7-1'
   source_url 'https://github.com/pacparser/pacparser/archive/1.3.7.tar.gz'
   source_sha256 '575c5d8096b4c842b2af852bbb8bcfde96170b28b49f33249dbe2057a8beea13'
 
   def self.build
-    system "make -C src"
+    system "make -j1 -C src"
   end
 
   def self.install
-    system "PREFIX=/usr/local make -C src install"
+    system "DESTDIR=#{CREW_DEST_DIR} PREFIX=/usr/local make -C src install"
   end
 end
