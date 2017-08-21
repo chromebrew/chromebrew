d7003d844402108e3a8ad3ed79b3a4dc69c0d5ac
Ed Reel <edreel@gmail.com>
2017-08-20 20:17:43 -0500

    Add recode package

---
+++ packages/recode.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Recode < Package
+  description 'Charset converter tool and library'
+  homepage 'https://github.com/pinard/Recode'
+  version '2d7092'
+  source_url 'https://github.com/pinard/Recode/archive/2d7092a9999194fc0e9449717a8048c8d8e26c18.tar.gz'
+  source_sha256 'd7de37aa4793ec00b042a0315f142281a1dc480f6f6d7cb2cd2568e58c4f2e87'
+
+  def self.build
+    system './configure'
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
