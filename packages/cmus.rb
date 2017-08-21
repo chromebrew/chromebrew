a27ab8cb9a583d6dca26bb6f0ae7a51e6325016e
Ed Reel <edreel@gmail.com>
2017-08-20 23:22:51 -0500

    Add cmus package

---
+++ packages/cmus.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Cmus < Package
+  description 'cmus is a small, fast and powerful console music player for Unix-like operating systems.'
+  homepage 'https://cmus.github.io/'
+  version '2.7.1'
+  source_url 'https://github.com/cmus/cmus/archive/v2.7.1.tar.gz'
+  source_sha256 '8179a7a843d257ddb585f4c65599844bc0e516fe85e97f6f87a7ceade4eb5165'
+
+  def self.build
+    system "sed -i 's,/usr/include,#{CREW_PREFIX}/include,g' configure"
+    system "./configure"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
