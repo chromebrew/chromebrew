d839ea4c84e08a190bf67a92890c54943fd18d90
Ed Reel <edreel@gmail.com>
2017-08-23 13:37:42 -0500

    Update glib to configure libdir

--- packages/glib.rb
+++ packages/glib.rb
@@ -3,7 +3,7 @@ require 'package'
 class Glib < Package
   description 'GLib provides the core application building blocks for libraries and applications written in C.'
   homepage 'https://developer.gnome.org/glib/'
-  version '2.40.2'
+  version '2.40.2-1'
   source_url 'https://ftp.gnome.org/pub/gnome/sources/glib/2.40/glib-2.40.2.tar.xz'
   source_sha256 'e8ff8af2950897e805408480c454c415d1eade4e670ec5fb507f5e5853726c7a'
 
@@ -26,7 +26,7 @@ class Glib < Package
   depends_on 'python27'
 
   def self.build
-    system "./configure"
+    system "./configure --libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
