352efadad7675de1c57edcb73900f14d8a9740c6
Ed Reel <edreel@gmail.com>
2017-08-23 22:28:57 -0500

    Add multitail package

---
+++ packages/multitail.rb
@@ -0,0 +1,13 @@
+require 'package'
+
+class Multitail < Package
+  description 'MultiTail allows you to monitor logfiles and command output in multiple windows in a terminal, colorize, filter and merge.'
+  homepage 'https://www.vanheusden.com/multitail/'
+  version '6.4.2'
+  source_url 'https://www.vanheusden.com/multitail/multitail-6.4.2.tgz'
+  source_sha256 'af1d5458a78ad3b747c5eeb135b19bdca281ce414cefdc6ea0cff6d913caa1fd'
+
+  def self.install
+    system "make", "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
