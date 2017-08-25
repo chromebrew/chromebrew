ed8d7bed2dbc09df24d8b70f8c2db3a533d9ee1a
Ed Reel <edreel@gmail.com>
2017-08-24 20:17:14 -0500

    Add ncursesw dependency

--- packages/multitail.rb
+++ packages/multitail.rb
@@ -7,6 +7,8 @@ class Multitail < Package
   source_url 'https://www.vanheusden.com/multitail/multitail-6.4.2.tgz'
   source_sha256 'af1d5458a78ad3b747c5eeb135b19bdca281ce414cefdc6ea0cff6d913caa1fd'
 
+  depends_on 'ncursesw'
+
   def self.install
     system "make", "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
