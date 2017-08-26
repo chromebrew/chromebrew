7d3d81de4e59ed6d2400f29227488f228def99c0
Yan Couto <yancouto@gmail.com>
2017-08-26 19:28:37 -0300

    Adding ncursesw dependency to aspell

--- packages/aspell.rb
+++ packages/aspell.rb
@@ -3,7 +3,7 @@ require 'package'
 class Aspell < Package
   description 'GNU Aspell is a Free and Open Source spell checker designed to eventually replace Ispell.'
   homepage 'http://aspell.net/'
-  version '0.60.7-rc1-1'
+  version '0.60.7-rc1-2'
   source_url 'ftp://alpha.gnu.org/gnu/aspell/aspell-0.60.7-rc1.tar.gz'
   source_sha256 '86b5662f24316142f70c5890787bdc5596625ca3604dfe85926ee61f27f2365e'
 
@@ -21,6 +21,7 @@ class Aspell < Package
   })
 
   depends_on 'ruby' unless File.exists? '/usr/local/bin/ruby'
+  depends_on 'ncursesw'
 
   def self.build
     system "./configure --libdir=#{CREW_LIB_PREFIX}"
