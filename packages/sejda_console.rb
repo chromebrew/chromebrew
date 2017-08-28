23ca519e6bd9507e7211185c794d0cbe0b62c8b8
Ed Reel <edreel@gmail.com>
2017-08-27 23:42:59 -0500

    Remove binary stuff so @jam7 can do his multiple architecture compile

--- packages/sejda_console.rb
+++ packages/sejda_console.rb
@@ -8,16 +8,8 @@ class Sejda_console < Package
   source_sha256 '09379d7f89d6b373a39d3e642a377650f0660849e8a934e2b94169df51dd7c13'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.22-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.22-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.22-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.22-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '7d9ae694a3df31802075f15af16567bc581935a7450033e03d99e4aed0b5612a',
-     armv7l: '7d9ae694a3df31802075f15af16567bc581935a7450033e03d99e4aed0b5612a',
-       i686: 'f70cdb95ad0fc91feaf82951f33c5ede10fac0d394bed7a61724e9e70fa21a6f',
-     x86_64: '91e9dc1e77208544b9a9ba60f7e633bc7c3cd5b289e463a67675f968307610b8',
   })
 
   depends_on 'help2man'
