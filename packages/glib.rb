66a3039beaa69ccfc25fb89a3c0db7061bd16313
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-28 14:10:21 +0900

    Add few more pre-built binaries.

--- packages/glib.rb
+++ packages/glib.rb
@@ -8,8 +8,16 @@ class Glib < Package
   source_sha256 'e8ff8af2950897e805408480c454c415d1eade4e670ec5fb507f5e5853726c7a'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.40.2-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.40.2-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.40.2-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.40.2-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '2dac85fcc4af6d049cee58cee774c644f3c7374b53344c62e62f6de90d99784e',
+     armv7l: '2dac85fcc4af6d049cee58cee774c644f3c7374b53344c62e62f6de90d99784e',
+       i686: '60e5ff58390596269e5690219afbc37f4c7a3f0808244e8b438ffa787c3deb3c',
+     x86_64: '3e14b78664198a520486af445ef0ed5a4650d7bbf22bb6e5d1f16c13c3fc7e15',
   })
 
   depends_on 'libffi'
--- packages/sfk.rb
+++ packages/sfk.rb
@@ -8,8 +8,16 @@ class Sfk < Package
   source_sha256 '1c53d4d9d05af752546c8341a718bf64be99b62491ff91db02ef100e2f93bfc3'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/sfk-1.8.7-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/sfk-1.8.7-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/sfk-1.8.7-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/sfk-1.8.7-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '1666f3bd5271973be9ce535e6cc1a71a831ba3d479ba2a08b7c587d2142ef360',
+     armv7l: '1666f3bd5271973be9ce535e6cc1a71a831ba3d479ba2a08b7c587d2142ef360',
+       i686: 'e2366c801933f920eef6058c559883be2ab31470f314c0fc0bdd40187014331c',
+     x86_64: '86c8b0fc3953d74b5225e16aec90dfb8e5c6f794f4d02ffb731e8ab8469c97e6',
   })
 
   def self.build
