5ea2914d5145fa68ccbf772b1eee85ba1d8ded66
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-08-29 07:39:03 +0900

    Add pre-built binary files for recently added packages

--- packages/gdal.rb
+++ packages/gdal.rb
@@ -8,8 +8,16 @@ class Gdal < Package
   source_sha256 '927098d54083ac919a497f787b835b099e9a194f2e5444dbff901f7426b86066'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '4c384905671538d0eb26a9e29a8dc7d3b2cf5e3e429282c637a138e11c5d75d2',
+     armv7l: '4c384905671538d0eb26a9e29a8dc7d3b2cf5e3e429282c637a138e11c5d75d2',
+       i686: '8f6a1b7c965c3e58eda805e91c8ecc3e7f145aff766d34f15c2186ff4c3c22fe',
+     x86_64: 'b23583b8becbb229b1f6b958c4ab04374ddfdae0d22c506739bea3af25fdf9a0',
   })
 
   depends_on 'python27'
--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -8,8 +8,16 @@ class Imagemagick < Package
   source_sha256 '5a9889c87dc351cf4b57ca3ae418c9cdcefc77aaa9a3b16044ae8a4eecf3aeed'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '037a93341e12a029a09dc9c4a0549b18cec7082da027fc5d218d9dcd9231e88e',
+     armv7l: '037a93341e12a029a09dc9c4a0549b18cec7082da027fc5d218d9dcd9231e88e',
+       i686: 'e5c8de20e68f5b608c7e0ae243096f01ce82fbe35c7a6464f4479eeb4254da43',
+     x86_64: '1f409d7ce9007e3274685030b4f50743abb5ddb5fe7eab6c72e52960917bfa5f',
   })
 
   depends_on 'buildessential' => :build
