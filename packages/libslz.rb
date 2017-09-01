47cb8ebe97ceecf640de724402b4177b7c1a54fd
Yan Couto <yancouto@gmail.com>
2017-08-31 21:39:07 -0300

    Updating and fixing libszl

--- packages/libslz.rb
+++ packages/libslz.rb
@@ -3,9 +3,9 @@ require 'package'
 class Libslz < Package
   description 'SLZ is a fast and memory-less stream compressor which produces an output that can be decompressed with zlib or gzip.'
   homepage 'https://github.com/haproxy/libslz'
-  version '1.1.0'
-  source_url 'http://git.1wt.eu/web?p=libslz.git;a=snapshot;h=afa04ae1f976957cf36287cc5370998d0559bc63;sf=tbz2'
-  source_sha256 '45527de53bca42faad9ad4078893c5e9a633e15c86eb9b06218d5b97e12f2096'
+  version '1.1.0-1'
+  source_url 'http://git.1wt.eu/web?p=libslz.git;a=snapshot;h=f65544ca6a0d628b1db2d76069cc3be493543d38;sf=tbz2'
+  source_sha256 '985cb527a4d1c7a93e9eb19d79f97513eb57cfe6e112b727976855c8144ffccc'
 
   binary_url ({
   })
@@ -13,7 +13,7 @@ class Libslz < Package
   })
 
   def self.build
-    system "make PREFIX=/usr/local"
+    system "make PREFIX=#{CREW_PREFIX}"
   end
 
   def self.install
