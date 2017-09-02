a19b24a402643007f74aea32b46677bd949efad2
Yan Couto <yancouto@gmail.com>
2017-09-01 21:29:50 -0300

    Switching libslz to stable version
    Also changed the homepage to a more relevant one.

--- packages/libslz.rb
+++ packages/libslz.rb
@@ -2,10 +2,10 @@ require 'package'
 
 class Libslz < Package
   description 'SLZ is a fast and memory-less stream compressor which produces an output that can be decompressed with zlib or gzip.'
-  homepage 'https://github.com/haproxy/libslz'
+  homepage 'http://www.libslz.org/'
   version '1.1.0-1'
-  source_url 'http://git.1wt.eu/web?p=libslz.git;a=snapshot;h=f65544ca6a0d628b1db2d76069cc3be493543d38;sf=tbz2'
-  source_sha256 '985cb527a4d1c7a93e9eb19d79f97513eb57cfe6e112b727976855c8144ffccc'
+  source_url 'http://git.1wt.eu/web?p=libslz.git;a=snapshot;h=afa04ae1f976957cf36287cc5370998d0559bc63;sf=tbz2'
+  source_sha256 'cf6b2c00066890d3c7f40ef00e6620784b9dc4662e9a3b35f2059997037be728'
 
   binary_url ({
   })
