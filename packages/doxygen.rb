17ba29c4fd1aac69bdb8da490e90775a1665d5ee
Ed Reel <edreel@gmail.com>
2017-09-03 01:16:13 -0500

    Change to compile from source

--- packages/doxygen.rb
+++ packages/doxygen.rb
@@ -4,23 +4,23 @@ class Doxygen < Package
   description 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources'
   homepage 'http://www.stack.nl/~dimitri/doxygen/'
   version '1.8.13'
-  source_url 'https://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.13.linux.bin.tar.gz'
-  source_sha256 '1b3ceb3708c5099d51341cd4ac63893f05c736388f12fb99958081fc832a3b3e'
+  source_url 'https://github.com/doxygen/doxygen/archive/Release_1_8_13.zip'
+  source_sha256 '1a1f67106c7e7642b79884f02faaf753d2e42be1ebac04083c173224123783b5'
 
   binary_url ({
   })
   binary_sha256 ({
   })
 
-  depends_on 'help2man'
+  depends_on 'cmake'
+  depends_on 'unzip'
 
   def self.build
-    system "help2man -N bin/doxygen > doxygen.1"
+    system "cmake ."
+    system "make"
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}/man/man1"
-    system "cp doxygen.1 #{CREW_DEST_PREFIX}/man/man1"
-    system "cp -r bin/ #{CREW_DEST_PREFIX}"
+    system "make", "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
 end
