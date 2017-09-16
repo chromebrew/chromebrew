18be8ba3ad765acce44756abf39e2c81a4ea2814
Jan Baudisch <dev@baudisch.xyz>
2017-09-16 13:10:02 +0200

    Update lvm2

--- packages/lvm2.rb
+++ packages/lvm2.rb
@@ -3,12 +3,14 @@ require 'package'
 class Lvm2 < Package
   description 'LVM2 refers to the userspace toolset that provide logical volume management facilities on linux.'
   homepage 'https://sourceware.org/lvm2'
-  version '2.02.173'
-  source_url 'ftp://sources.redhat.com/pub/lvm2/LVM2.2.02.173.tgz'
-  source_sha256 'ceb9168c7e009ef487f96a1fe969b23cbb07d920ffb71769affdbdf30fea8d64'
+  version '2.02.174'
+  source_url 'ftp://sources.redhat.com/pub/lvm2/releases/LVM2.2.02.174.tgz'
+  source_sha256 'fb6ecec0408cb14ac4923c87cb0a356481122d7d0ae3fee6e94d2edb9d170a8b'
+
+  depends_on 'readline'
 
   def self.build
-    system "./configure --disable-selinux --prefix=/usr/local"
+    system "./configure --disable-selinux --prefix=#{CREW_PREFIX}"
     system "make"
   end
 
