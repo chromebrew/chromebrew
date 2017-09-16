14ca17c87abeddc790937978030d580ffb45c294
Jan Baudisch <dev@baudisch.xyz>
2017-09-16 22:19:12 +0200

    Update libpstat.rb

--- packages/libpstat.rb
+++ packages/libpstat.rb
@@ -3,9 +3,9 @@ require 'package'
 class Libpstat < Package
   description 'Library for getting information about running processes'
   homepage 'https://github.com/jcnelson/libpstat'
-  version '1.0.0'
-  source_url 'https://github.com/flyingP0tat0/libpstat/archive/1.0.0.tar.gz'
-  source_sha256 '97c07e1338e0159a6e9d411939d696282c1cbb8134555dbf11d5b38e8c174e07'
+  version '030f37'
+  source_url 'https://github.com/jcnelson/libpstat/archive/030f37a9d22ef3b8be839d487c97759e6126c30f.tar.gz'
+  source_sha256 '71e5d3f190fe045b38f31dfa9b5962fb6ea4099f46b25981b411122569ff6b30'
 
   def self.build
     system "make"
