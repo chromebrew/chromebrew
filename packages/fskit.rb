546644691801d0e1835ed4f8a32b0b404f63bcb6
Jan Baudisch <dev@baudisch.xyz>
2017-09-16 22:15:32 +0200

    Correct fskit version

--- packages/fskit.rb
+++ packages/fskit.rb
@@ -3,7 +3,7 @@ require 'package'
 class Fskit < Package
   description 'Filesystem utility library and SDK'
   homepage 'https://github.com/jcnelson/fskit'
-  version '1.0.0'
+  version '2feb33'
   source_url 'https://github.com/jcnelson/fskit/archive/2feb33a84932980e4ed9691242537771dd4e80f4.tar.gz'
   source_sha256 'c3f0506eb5d9345e5ea91086ebb2b2afcee889eed55792715c8ab5e523e02031'
 
