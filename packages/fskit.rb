ef49c171c213e884147856847496aa486f8a8364
Jan Baudisch <dev@baudisch.xyz>
2017-09-16 13:08:47 +0200

    Update fskit

--- packages/fskit.rb
+++ packages/fskit.rb
@@ -7,6 +7,8 @@ class Fskit < Package
   source_url 'https://github.com/flyingP0tat0/fskit/archive/1.0.0.tar.gz'
   source_sha256 '3a1d49d433870fa27e498ab25e49280ce788f401cdd0afb27f29d99ba0e117c2'
 
+  depends_on 'attr'
+
   def self.build
     system "make"
   end
