e54455361f6f43a8736ece1f32af14c1f70c012f
Kazushi (Jam) Marukawa <jam@pobox.com>
2017-09-19 21:16:11 +0900

    Change fontconfig.rb to prepare for pre-built binary files update.

--- packages/fontconfig.rb
+++ packages/fontconfig.rb
@@ -3,15 +3,11 @@ require 'package'
 class Fontconfig < Package
   description 'Fontconfig is a library for configuring and customizing font access.'
   homepage 'https://www.freedesktop.org/software/fontconfig/front.html'
-  version '2.12.4'
+  version '2.12.4-1'
   source_url 'https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.12.4.tar.bz2'
   source_sha256 '668293fcc4b3c59765cdee5cee05941091c0879edcc24dfec5455ef83912e45c'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/fontconfig-2.12.4-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/fontconfig-2.12.4-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/fontconfig-2.12.4-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/fontconfig-2.12.4-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
     aarch64: 'e9019600344fb674b24c24b78599cb9ad45f5b33cfd3d3e31c2a3b8c87895f67',
