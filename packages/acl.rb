8faeaa65d5874e18928eb75589319f8d58258e07
Ed Reel <edreel@gmail.com>
2017-09-26 00:07:31 -0500

    Change source location url to https in acl.rb

--- packages/acl.rb
+++ packages/acl.rb
@@ -4,7 +4,7 @@ class Acl < Package
   description 'Commands for Manipulating POSIX Access Control Lists.'
   homepage 'http://savannah.nongnu.org/projects/acl'
   version '2.2.52-1'
-  source_url 'http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz'
+  source_url 'https://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz'
   source_sha256 '179074bb0580c06c4b4137be4c5a92a701583277967acdb5546043c7874e0d23'
 
   binary_url ({
