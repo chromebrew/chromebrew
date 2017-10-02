3bdc86c8a71267126f5c907df5c4fdf8633d5ece
Casey Strouse <casey.strouse@gmail.com>
2017-10-02 12:06:53 -0700

    Update cmake from 3.9.1 to 3.9.3
    This is a general bugfix and maintenance release.
    
    Tested as working on XE500C13-K01US.

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,21 +3,13 @@ require 'package'
 class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  version '3.9.1'
-  source_url 'https://cmake.org/files/v3.9/cmake-3.9.1.tar.gz'
-  source_sha256 'd768ee83d217f91bb597b3ca2ac663da7a8603c97e1f1a5184bc01e0ad2b12bb'
+  version '3.9.3'
+  source_url 'https://cmake.org/files/v3.9/cmake-3.9.3.tar.gz'
+  source_sha256 '8eaf75e1e932159aae98ab5e7491499545554be62a08cbcbc7c75c84b999f28a'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.9.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.9.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.9.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.9.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '0735ec6920a6598a066e9cddca144eb3e6d3d56313d766d19ce0f91eafa3f90a',
-     armv7l: '0735ec6920a6598a066e9cddca144eb3e6d3d56313d766d19ce0f91eafa3f90a',
-       i686: '3ed6f69859386916cd001cd3d68330b361133883a7f65146b08080b238efcb7f',
-     x86_64: '2ddaca97b6f8e4973d7fe1631652734ac414e47b6e221ac126de61c0f5e12455',
   })
 
   depends_on 'buildessential'
@@ -25,8 +17,8 @@ class Cmake < Package
   depends_on 'ncurses'
 
   def self.build
-    if Dir.exist? '/usr/local/include/ncursesw'
-      system 'sed -i "51s,$,\n  set(CURSES_INCLUDE_PATH /usr/local/include/ncursesw)," Modules/FindCurses.cmake'
+    if Dir.exist? "#{CREW_PREFIX}/include/ncursesw"
+      system 'sed -i "51s,$,\n  set(CURSES_INCLUDE_PATH ' + "#{CREW_PREFIX}/include/ncursesw" + ')," Modules/FindCurses.cmake'
     end
     system "./bootstrap"
     system "make"
