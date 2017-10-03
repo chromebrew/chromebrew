5f4d6c67b4beb1ea2140bcf27132dcaef9c31c78
Casey Strouse <casey.strouse@gmail.com>
2017-10-03 15:37:55 -0700

    Add brotli package
    Brotli is a generic-purpose lossless compression algorithm from Google.
    
    Tested as working on XE500C13-K01US.

---
+++ packages/brotli.rb
@@ -0,0 +1,31 @@
+require 'package'
+
+class Brotli < Package
+  description 'Brotli compression format '
+  homepage 'https://github.com/google/brotli'
+  version '1.0.1'
+  source_url 'https://github.com/google/brotli/archive/v1.0.1.tar.gz'
+  source_sha256 '6870f9c2c63ef58d7da36e5212a3e1358427572f6ac5a8b5a73a815cf3e0c4a6'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'cmake' => :build
+
+  def self.build
+    system 'mkdir out'
+    Dir.chdir 'out' do
+      system "cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} -DCMAKE_INSTALL_LIBDIR=#{CREW_LIB_PREFIX}"
+      system "cmake --build . --config Release --target install"
+    end
+  end
+
+  def self.install
+    #system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    Dir.chdir 'out' do
+      system "cmake -DCMAKE_INSTALL_PREFIX=#{CREW_DEST_PREFIX} -DCMAKE_INSTALL_LIBDIR=#{CREW_LIB_PREFIX} -P cmake_install.cmake"
+    end
+  end
+end
