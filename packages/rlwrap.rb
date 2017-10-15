1497bf68431cfc6acf19329ed861c12d3088465b
Ed Reel <edreel@gmail.com>
2017-10-15 15:35:35 -0500

    Add rlwrap package

---
+++ packages/rlwrap.rb
@@ -0,0 +1,26 @@
+require 'package'
+
+class Rlwrap < Package
+  description 'A readline wrapper'
+  homepage 'https://github.com/hanslub42/rlwrap'
+  version '0.43'
+  source_url 'https://github.com/hanslub42/rlwrap/archive/v0.43.tar.gz'
+  source_sha256 '29e5a850fbe4753f353b0734e46ec0da043621bdcf7b52a89b77517f3941aade'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'autoconf'
+
+  def self.build
+    system "autoreconf --install"
+    system "./configure --prefix=#{CREW_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
