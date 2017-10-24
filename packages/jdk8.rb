83ea96d67c322fdef575facc3b57df3b0aed8b03
Ed Reel <edreel@gmail.com>
2017-10-23 23:32:02 -0500

    Change urls from http to https

--- packages/jdk8.rb
+++ packages/jdk8.rb
@@ -15,7 +15,7 @@ class Jdk8 < Package
   def self.install
     case ARCH
     when 'aarch64'
-      system 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" --no-check-certificate http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-arm64-vfp-hflt.tar.gz'
+      system 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" --no-check-certificate https://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-arm64-vfp-hflt.tar.gz'
       abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('jdk-8u152-linux-arm64-vfp-hflt.tar.gz') ) == '732609dd090a7d20e09b48b90f8f447497879bb6da1b786a9dd1ea6e91ea178f'
       system 'tar xvf jdk-8u152-linux-arm64-vfp-hflt.tar.gz'
     when 'armv7l'
@@ -23,11 +23,11 @@ class Jdk8 < Package
       abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('jdk-8u152-linux-arm32-vfp-hflt.tar.gz') ) == '35ab532355c72310c4c7add2b7c7f9d1eb0e045cf59d3fd69ee08fa6a9e610f0'
       system 'tar xvf jdk-8u152-linux-arm32-vfp-hflt.tar.gz'
     when 'i686'
-      system 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" --no-check-certificate http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-i586.tar.gz'
+      system 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" --no-check-certificate https://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-i586.tar.gz'
       abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('jdk-8u152-linux-i586.tar.gz') ) == 'fc60b121d4b194fa70084fd0ca0d69e8b78e5db01a071efe200e90eed83c4c3e'
       system 'tar xvf jdk-8u152-linux-i586.tar.gz'
     when 'x86_64'
-      system 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" --no-check-certificate http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz'
+      system 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" --no-check-certificate https://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz'
       abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('jdk-8u152-linux-x64.tar.gz') ) == '218b3b340c3f6d05d940b817d0270dfe0cfd657a636bad074dcabe0c111961bf'
       system 'tar xvf jdk-8u152-linux-x64.tar.gz'
     end
