7a5978b2e8f758172481e456f9136b08e0654c45
Alan Hardman <alanaktion@gmail.com>
2017-10-27 14:31:10 -0600

    Updating source to use GitLab mirror
    This is more likely to stay up longer than my personal Gogs mirror.

--- packages/elinks.rb
+++ packages/elinks.rb
@@ -3,10 +3,10 @@ require 'package'
 class Elinks < Package
   description 'Full-Featured Text WWW Browser'
   homepage 'http://elinks.or.cz/'
-  version 'f86be65971'
+  version 'f86be659'
 
-  source_url 'https://git.phpizza.com/alan/elinks/archive/f86be659718c0cd0a67f88b42f07044c23d0d028.tar.gz'
-  source_sha256 'ce416fdd57b23442275b5d74c80e0ac5793b66ec0f8ed07500c64eb647339838'
+  source_url 'https://gitlab.com/alanaktion/elinks-mirror/repository/f86be659718c0cd0a67f88b42f07044c23d0d028/archive.tar.gz'
+  source_sha256 'c19a342a5a6716b5213a4c539a149711491136d8b12daba7846bc2225c5fb309'
 
   depends_on 'bz2'
   depends_on 'lzip'
