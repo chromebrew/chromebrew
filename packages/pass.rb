5331592fc54136611376e43c2825016537ca4c4d
Alan Hardman <alanaktion@gmail.com>
2017-10-31 13:25:45 -0600

    Adding tree dependency to pass
    The pass program requires tree to list the stored passwords correctly.

--- packages/pass.rb
+++ packages/pass.rb
@@ -8,6 +8,7 @@ class Pass < Package
   source_sha256 'f6d2199593398aaefeaa55e21daddfb7f1073e9e096af6d887126141e99d9869'
 
   depends_on 'gnupg'
+  depends_on 'tree'
 
   def self.install
     system  "make", "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", "install"
