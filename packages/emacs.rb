fbb521ddc7503efc7523a3d6424b84b7738bab08
Ed Reel <edreel@gmail.com>
2017-11-01 12:13:34 -0500

    Update emacs from 25.2 to 25.3

--- packages/emacs.rb
+++ packages/emacs.rb
@@ -3,9 +3,9 @@ require 'package'
 class Emacs < Package
   description 'An extensible, customizable, free/libre text editor - and more.'
   homepage 'http://www.gnu.org/software/emacs/'
-  version '25.2'
-  source_url 'https://ftp.gnu.org/gnu/emacs/emacs-25.2.tar.xz'
-  source_sha256 '59b55194c9979987c5e9f1a1a4ab5406714e80ffcfd415cc6b9222413bc073fa'
+  version '25.3'
+  source_url 'https://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.xz'
+  source_sha256 '253ac5e7075e594549b83fd9ec116a9dc37294d415e2f21f8ee109829307c00b'
 
   depends_on 'zlibpkg' => :build
   depends_on 'diffutils' => :build
