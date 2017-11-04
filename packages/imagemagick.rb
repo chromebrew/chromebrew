197044fff728cc84504a6614bf08ec83c401bd79
Casey Strouse <casey.strouse@gmail.com>
2017-11-03 21:04:23 -0700

    Update imagemagick from 7.0.6-10 7.0.7-9
    This is a bugfix and maintenance release.
    
    Tested as working on Samsung Chromebook Plus (aarch64).

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -3,27 +3,17 @@ require 'package'
 class Imagemagick < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '7.0.6-10'
-  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.6-10.tar.xz'
-  source_sha256 '5a9889c87dc351cf4b57ca3ae418c9cdcefc77aaa9a3b16044ae8a4eecf3aeed'
+  version '7.0.7-9'
+  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-9.tar.xz'
+  source_sha256 'dc0c7418e20d9f113b3c3ae6903ed89d00a95465bd3f5e15832f3b06593a085c'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.6-10-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '037a93341e12a029a09dc9c4a0549b18cec7082da027fc5d218d9dcd9231e88e',
-     armv7l: '037a93341e12a029a09dc9c4a0549b18cec7082da027fc5d218d9dcd9231e88e',
-       i686: 'e5c8de20e68f5b608c7e0ae243096f01ce82fbe35c7a6464f4479eeb4254da43',
-     x86_64: '1f409d7ce9007e3274685030b4f50743abb5ddb5fe7eab6c72e52960917bfa5f',
   })
 
-  depends_on 'buildessential' => :build
-
   def self.build
-    system "./configure CFLAGS=\" -fPIC\" --without-python"
+    system "./configure CFLAGS=\" -fPIC\" --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} --without-python --disable-dependency-tracking"
     system "make"
   end
 
