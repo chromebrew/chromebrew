54b42fa30a82cfdd21d678763dc4d56258d337cb
Casey Strouse <casey.strouse@gmail.com>
2017-11-04 15:07:59 -0700

    Update units from 2.15 to 2.16
    This is a bugfix and maintenance release.
    
    Tested as working on Samsung Chromebook Plus (aarch64).

--- packages/units.rb
+++ packages/units.rb
@@ -3,9 +3,9 @@ require 'package'
 class Units < Package
   description 'GNU Units converts quantities expressed in various systems of measurement to their equivalents in other systems of measurement.'
   homepage 'https://www.gnu.org/software/units/units.html'
-  version '2.15'
-  source_url 'https://ftp.gnu.org/gnu/units/units-2.15.tar.gz'
-  source_sha256 '25d6334fafc801e15b3b0388e207e1efc5d4ae88af6dc7b17d3913de6441f47c'
+  version '2.16'
+  source_url 'https://ftp.gnu.org/gnu/units/units-2.16.tar.gz'
+  source_sha256 'dcf3f78482a13b150fc6bf0f8d611510816a3424bc2b26229b85b1cadeb81686'
 
   def self.build
     system './configure',
