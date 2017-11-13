e27cf33862b9311b7cbd32f6c7972770ef4ab35c
Ed Reel <edreel@gmail.com>
2017-11-13 08:53:49 -0600

    Update chromebrew_scripts from 1.0.0 to 1.1.0

--- packages/chromebrew_scripts.rb
+++ packages/chromebrew_scripts.rb
@@ -3,9 +3,9 @@ require 'package'
 class Chromebrew_scripts < Package
   description 'Useful bash utility scripts for Chrome/Chromium OS systems.'
   homepage 'https://github.com/uberhacker/chromebrew-scripts'
-  version '1.0.0'
-  source_url 'https://github.com/uberhacker/chromebrew-scripts/archive/1.0.0.tar.gz'
-  source_sha256 '97afac04020e9d82e7026113f8040ad609fb4887da202ded129bd7f65b996341'
+  version '1.1.0'
+  source_url 'https://github.com/uberhacker/chromebrew-scripts/archive/1.1.0.tar.gz'
+  source_sha256 'acf8f89ef3b8a04fc33bb5a94188687d0fdd2dd7b05102ad442a91019cab20fb'
 
   binary_url ({
   })
@@ -13,9 +13,9 @@ class Chromebrew_scripts < Package
   })
 
   def self.install
+    system "install -Dm755 chromebrew #{CREW_DEST_PREFIX}/bin/chromebrew"
     system "install -Dm755 powerwash #{CREW_DEST_PREFIX}/bin/powerwash"
     system "install -Dm755 rootfs #{CREW_DEST_PREFIX}/bin/rootfs"
     system "install -Dm755 rw #{CREW_DEST_PREFIX}/bin/rw"
-    system "install -Dm755 uninstall #{CREW_DEST_PREFIX}/bin/uninstall"
   end
 end
