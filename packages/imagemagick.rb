7608806c244cb687d2bde1fd65bc3f52cb6963a4
rumpaar1 <rumpaar1@kvc.school.nz>
2017-11-14 18:59:43 +1300

    Update imagemagick.rb

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -3,7 +3,7 @@ require 'package'
 class Imagemagick < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '7.0.7-9'
+  version '7.0.7-11'
   source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-11.tar.xz'
   source_sha256 '7993942d64c138f6c3e9d4bce6d8c13f747128bafe5c2295dcb45d91d1ff21e3'
 
