2787294dd42d13eb5417b51429d064154af848f9
hdt <p4ngb0y@gmail.com>
2017-11-14 09:23:30 -0600

    add proxychains package

---
+++ packages/proxychains.rb
@@ -0,0 +1,17 @@
+require 'package'
+
+class Proxychains < Package
+  description 'a tool that forces any TCP connection made by any given application to follow through proxy like TOR or any other SOCKS4, SOCKS5 or HTTP(S) proxy.'
+  homepage 'https://github.com/haad/proxychains'
+  version '4.2.0'
+  source_url 'https://github.com/haad/proxychains/archive/proxychains-4.2.0.tar.gz'
+  source_sha256 '225284e5553fb062d09ed425d2815387eda9c1c0d6e2bc24ea95393a71601619'
+  def self.build
+    system "./configure", "--prefix=#{CREW_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
