17765e5dd5d778c1ccfb76f83df060e8986280bc
Ed Reel <edreel@gmail.com>
2017-11-18 21:10:33 -0600

    Update libtasn1 from 4.10-2 to 4.12

--- packages/libtasn1.rb
+++ packages/libtasn1.rb
@@ -3,25 +3,16 @@ require 'package'
 class Libtasn1 < Package
   description 'Libtasn1 is the ASN.1 library used by GnuTLS, GNU Shishi and some other packages.'
   homepage 'https://www.gnu.org/software/libtasn1/'
-  version '4.10-2'
-  source_url 'http://ftpmirror.gnu.org/libtasn1/libtasn1-4.10.tar.gz'
-  source_sha256 '681a4d9a0d259f2125713f2e5766c5809f151b3a1392fd91390f780b4b8f5a02'
+  version '4.12'
+  source_url 'https://ftpmirror.gnu.org/libtasn1/libtasn1-4.12.tar.gz'
+  source_sha256 '6753da2e621257f33f5b051cc114d417e5206a0818fe0b1ecfd6153f70934753'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libtasn1-4.10-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libtasn1-4.10-2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libtasn1-4.10-2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libtasn1-4.10-2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '2d0af019e1690fea1f7b277a008783713f4d3535f54bc04fb6cd785606006f66',
-     armv7l: '2d0af019e1690fea1f7b277a008783713f4d3535f54bc04fb6cd785606006f66',
-       i686: '3fb18df6adf7689a405a45b009a9f496250fa462ce27db5ab778edd725149416',
-     x86_64: '99e35599977b8cff5e69b6733b09c40f7a3cbf260699c3e427987ad8e841d6a2',
   })
 
   # bison, diff, cmp are required at compile-time
-  depends_on 'buildessential' => :build
   depends_on 'bison' => :build
   depends_on 'diffutils' => :build
 
