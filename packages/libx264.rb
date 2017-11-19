cd12f5754e4fbb4a2755850f5a1bb455d5bcacf2
Ed Reel <edreel@gmail.com>
2017-11-18 21:18:24 -0600

    Update libx264 from 20170604-2245-stable to 20171118-2245-stable

--- packages/libx264.rb
+++ packages/libx264.rb
@@ -3,27 +3,21 @@ require 'package'
 class Libx264 < Package
   description 'x264 is a free software library and application for encoding video streams into the H.264/MPEG-4 AVC compression format.'
   homepage 'http://www.videolan.org/developers/x264.html'
-  version '20170604-2245-stable'
-  source_url 'http://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20170604-2245-stable.tar.bz2'
-  source_sha256 '42fa7cc6af0af36e4a8286ce724e6d20de0967d9017640883e955b4b6d3690ac'
+  version '20171118-2245-stable'
+  source_url 'http://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20171118-2245-stable.tar.bz2'
+  source_sha256 '22f29bbeba7087d9a92e0a50837e22c5729ba46b6c03392f149cae6ca3fa01ad'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20170604-2245-stable-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20170604-2245-stable-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20170604-2245-stable-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20170604-2245-stable-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '4aa17ddc683247df0a313a3731fa50c9695fe92f755d024368c79991a2b832ae',
-     armv7l: '4aa17ddc683247df0a313a3731fa50c9695fe92f755d024368c79991a2b832ae',
-       i686: '68fa4d06c473ce45499b4ce9d57e4adf85a53ba3d42464a9449d7feea4682c05',
-     x86_64: '74c95e2d82fd09cdab5fe9688285a76018563ade0dafca83900858fe14b5430d',
   })
 
-  depends_on 'yasm'
+  depends_on 'yasm' => :build
 
   def self.build
     system "./configure \
+            --prefix=#{CREW_PREFIX} \
+            --libdir=#{CREW_LIB_PREFIX} \
             --enable-shared \
             --enable-static"
     system "make"
