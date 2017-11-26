23a7e1fae0b5f5fe49fc4f13977c90e8c6a5b009
Casey Strouse <casey.strouse@gmail.com>
2017-11-25 17:38:41 -0700

    Add irssi package
    irssi is a console IRC client with perl scripting support.
    
    Tested as working on Samsung Chromebook Plus (aarch64).

---
+++ packages/irssi.rb
@@ -0,0 +1,30 @@
+require 'package'
+
+class Irssi < Package
+  description 'Modular text mode IRC client with Perl scripting'
+  homepage 'https://irssi.org/'
+  version '1.0.5'
+  source_url 'https://github.com/irssi/irssi/releases/download/1.0.5/irssi-1.0.5.tar.xz'
+  source_sha256 'c2556427e12eb06cabfed40839ac6f57eb8b1aa6365fab6dfcd331b7a04bb914'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'ncurses'
+  depends_on 'openssl'
+  depends_on 'perl'
+
+  def self.build
+    system './configure',
+	     "--prefix=#{CREW_PREFIX}",
+	     "--libdir=#{CREW_LIB_PREFIX}",
+	     "--sysconfdir=#{CREW_PREFIX}/etc"
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+end
