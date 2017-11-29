c00d3b54f53b27fae6d3c2e9208f8f84721c44a2
Ed Reel <edreel@gmail.com>
2017-11-29 01:06:15 -0600

    Add dependencies to pdfgrep

--- packages/pdfgrep.rb
+++ packages/pdfgrep.rb
@@ -3,7 +3,7 @@ require 'package'
 class Pdfgrep < Package
   description 'a commandline utility to search text in PDF files'
   homepage 'https://pdfgrep.org/'
-  version '2.0.1'
+  version '2.0.1-1'
   source_url 'https://pdfgrep.org/download/pdfgrep-2.0.1.tar.gz'
   source_sha256 '0370d744b3072d47383dbed2cb9c8b0b64b83c084da5a8961f8d4bc7669e941e'
 
@@ -12,6 +12,8 @@ class Pdfgrep < Package
   binary_sha256 ({
   })
 
+  depends_on 'libgcrypt'
+  depends_on 'pcre'
   depends_on 'poppler'
 
   def self.build
