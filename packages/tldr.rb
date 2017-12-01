0d7302a7a48e72d5bdda668cbf4ef15ca485223c
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2017-11-30 19:47:00 -0500

    Update tldr.rb

--- packages/tldr.rb
+++ packages/tldr.rb
@@ -1,16 +1,13 @@
 require 'package'
   
 class Tldr < Package
-    description 'A collection of simplified and community-driven man pages.'
-    homepage 'http://tldr.sh/'
-    version '93cfb27'
-    source_url 'https://raw.githubusercontent.com/raylee/tldr/93cfb278d2c56c3518a39ba3e01cbfc40423cc3b/tldr'
-    source_sha256 '58cb21c28946d51db01b080853dc589773bb1b1e62f3a53306bc1f46cabcb66d'
+  description 'A collection of simplified and community-driven man pages.'
+  homepage 'http://tldr.sh/'
+  version '93cfb27'
+  source_url 'https://github.com/raylee/tldr/archive/93cfb278d2c56c3518a39ba3e01cbfc40423cc3b.tar.gz'
+  source_sha256 'd565c6bdc594b87bf204fa35b3059011931a3cb59b935812c8a8a5fbe1fbd5e5'
 
-
-   def self.install
-       system "curl -o tldr https://raw.githubusercontent.com/raylee/tldr/93cfb278d2c56c3518a39ba3e01cbfc40423cc3b/tldr"
-       abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('tldr') ) == '58cb21c28946d51db01b080853dc589773bb1b1e62f3a53306bc1f46cabcb66d'
-       system "install -Dm755 tldr #{CREW_DEST_PREFIX}/bin/tldr"
-   end
+  def self.install
+    system "install -Dm755 tldr #{CREW_DEST_PREFIX}/bin/tldr"
+  end
 end
