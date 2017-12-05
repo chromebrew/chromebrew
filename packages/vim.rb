fdf38e485a4be2a922df6a88862c6dda2eed1f78
Ed Reel <edreel@gmail.com>
2017-12-05 00:37:04 -0600

    Update vim from 8.0-1 to 8.0.586

--- packages/vim.rb
+++ packages/vim.rb
@@ -3,23 +3,16 @@ require 'package'
 class Vim < Package
   description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient.'
   homepage 'http://www.vim.org/'
-  version '8.0-1'
-  source_url 'ftp://ftp.vim.org/pub/vim/unix/vim-8.0.tar.bz2'
+  version '8.0.586'
+  source_url 'ftp://ftp.vim.org/pub/vim/unix/vim-8.0.586.tar.bz2'
   source_sha256 '08bd0d1dd30ece3cb9905ccd48b82b2f81c861696377508021265177dc153a61'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '00b27877b9f8df26396f172dba2761b85c1aa3ffd9332c7df3de7d9578e2a993',
-     armv7l: '00b27877b9f8df26396f172dba2761b85c1aa3ffd9332c7df3de7d9578e2a993',
-       i686: '8eb41d4ef310e5fde6416f6e205806ba8615df9b38a84e0c3d1fa9362451bc6e',
-     x86_64: 'dd869d3d0a499ea0a931a8370b72fc69b4a37b89b6d8e389861294d72b0290c1',
   })
 
+  depends_on 'compressdoc' => :build
   depends_on 'ncurses'
   # vim uses shared library of following languages, so need them isntalled at run-time
   depends_on 'perl'
@@ -27,16 +20,31 @@ class Vim < Package
   depends_on 'ruby'
 
   def self.build
-    system './configure --prefix=/usr/local --enable-gui=no --with-features=huge --without-x --disable-nls --enable-multibyte  --with-tlib=ncurses --enable-perlinterp --enable-pythoninterp  --enable-rubyinterp --with-ruby-command=/usr/local/bin/ruby --disable-selinux'
+    system "./configure \
+            --prefix=#{CREW_PREFIX} \
+            --enable-gui=no \
+            --with-features=huge \
+            --without-x \
+            --disable-nls \
+            --enable-multibyte \
+            --with-tlib=ncurses \
+            --enable-perlinterp \
+            --enable-pythoninterp \
+            --enable-rubyinterp \
+            --with-ruby-command=#{CREW_PREFIX}/bin/ruby \
+            --disable-selinux"
     system "make"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "compressdoc --gzip -9 #{CREW_DEST_PREFIX}/share/man/man1"
+  end
 
-    puts "\nMake sure to put your .vim directory in a subdirectory of /usr/local so it has execute permissions"
-    puts "You can then symlink to your home directory so vim can see it"
-    puts "\tln -s /usr/local/vim ~/.vim"
-    puts "\tln -s ~/.vim/vimrc ~/.vimrc\n\n"
+  def self.postinstall
+    puts "\nMake sure to put your .vim directory in a subdirectory of #{CREW_PREFIX} so it has execute permissions.".lightblue
+    puts "You can then symlink to your home directory so vim can see it as follows:".lightblue
+    puts "ln -s #{CREW_PREFIX}/vim ~/.vim".lightblue
+    puts "ln -s ~/.vim/vimrc ~/.vimrc\n".lightblue
   end
 end
