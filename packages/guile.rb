2a9eae7fb85dd421a4c4edfc86abd432bf754bbe
Ed Reel <edreel@gmail.com>
2017-12-06 10:08:15 -0600

    Add pre-built binaries for guile, less and vim

--- packages/guile.rb
+++ packages/guile.rb
@@ -8,8 +8,16 @@ class Guile < Package
   source_sha256 '8353a8849cd7aa77be66af04bd6bf7a6207440d2f8722e46672232bb9f0a4086'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/guile-2.2.3-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/guile-2.2.3-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/guile-2.2.3-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/guile-2.2.3-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '418eb676b562f77b80e3dae974f35159c0f844284c8e98f02d6763ff08924d4e',
+     armv7l: '418eb676b562f77b80e3dae974f35159c0f844284c8e98f02d6763ff08924d4e',
+       i686: '18c2f42f3c60a6474854b89065c7d2de518693a1727a5dfd793fd76313a78ece',
+     x86_64: '11a28901148adef29d90c96b6b28e2ff0caee7479588683cf3a919b9cea825e9',
   })
 
   depends_on 'diffutils' => :build
--- packages/less.rb
+++ packages/less.rb
@@ -8,8 +8,16 @@ class Less < Package
   source_sha256 'f3dc8455cb0b2b66e0c6b816c00197a71bf6d1787078adeee0bcf2aea4b12706'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/less-487-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/less-487-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/less-487-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/less-487-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'b8121181edca892d7d552375e0943af58bd2e4b203466d85e560e41a570e2638',
+     armv7l: 'b8121181edca892d7d552375e0943af58bd2e4b203466d85e560e41a570e2638',
+       i686: '01484da1a6be5af400f1b6f238cd13f84865e359d18eaae8f30edf71e302325f',
+     x86_64: 'e3b116acea3eff0dfb87c38b37751bf561268c1b9305b4ef91be8eb3cc5a939e',
   })
 
   depends_on 'compressdoc' => :build
