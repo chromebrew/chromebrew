c06242e5c9c72e889686ee132c6bbdc86be7bf06
skiman6010 <8137212+skiman6010@users.noreply.github.com>
2017-12-13 12:11:47 -0500

    Update Heroku.rb

--- packages/heroku.rb
+++ packages/heroku.rb
@@ -3,23 +3,11 @@ require 'package'
 class Heroku < Package
   description 'The Heroku Command Line Interface (CLI), formerly known as the Heroku Toolbelt, is a tool for creating and managing Heroku apps from the command line / shell of various operating systems.'
   homepage 'https://devcenter.heroku.com/articles/heroku-cli'
-  version '6.13.9'
-  source_url 'https://github.com/heroku/cli/archive/v6.13.9.tar.gz'
-  source_sha256 '76699029d86fc10326f8cf39b0c0a3b1f322dee39bd7c96fa81e34e52ff454f9'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.13.9-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.13.9-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.13.9-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.13.9-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '73f9f8288e4c8767dd3d6158ab86b1384c734367b639c18d4fce971c133cb31d',
-     armv7l: '73f9f8288e4c8767dd3d6158ab86b1384c734367b639c18d4fce971c133cb31d',
-       i686: '787f77bde4831a1e82ec9777dfe5a16c4837f07a04f74fd5f51637288c469f1d',
-     x86_64: '7b78ab9be7f1c12c2d2b3d11957e749729089a2bc1b8342f4fc1253ecfe229e6',
-  })
-
+  version '6.14.42'
+  source_url 'https://github.com/heroku/cli/archive/v6.14.42.tar.gz'
+  source_sha256 'cf129c61622be965384a66355a702c7919d6bc2e45b60969c6e9f9fa7342988e'
+ 
+  
   depends_on 'yarn'
 
   def self.build
@@ -27,9 +15,10 @@ class Heroku < Package
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_DIR}#{CREW_LIB_PREFIX}/heroku"
-    system "cp -r . #{CREW_DEST_DIR}#{CREW_LIB_PREFIX}/heroku"
-    system "mkdir -p #{CREW_DEST_DIR}#{CREW_PREFIX}/bin"
-    system "ln -s #{CREW_LIB_PREFIX}/heroku/bin/run #{CREW_DEST_DIR}#{CREW_PREFIX}/bin/heroku"
+	system "mkdir -p #{CREW_DEST_PREFIX}/share/heroku"
+    system "cp -r . #{CREW_DEST_PREFIX}/share/heroku"
+    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
+    system "ln -s #{CREW_PREFIX}/share/heroku/bin/run #{CREW_DEST_PREFIX}/bin/heroku"
+  
   end
 end
