29ed2837f2f556fc462cc351235e932e845624d2
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2017-12-15 22:13:11 -0500

    Update miniupnpc.rb

--- packages/miniupnpc.rb
+++ packages/miniupnpc.rb
@@ -3,22 +3,9 @@ require 'package'
 class Miniupnpc < Package
   description 'UPnP IGD client lightweight library'
   homepage 'http://miniupnp.free.fr/'
-  version '2.0'
-  source_url 'http://miniupnp.free.fr/files/download.php?file=miniupnpc-2.0.tar.gz'
-  source_sha256 'd434ceb8986efbe199c5ca53f90ed53eab290b1e6d0530b717eb6fa49d61f93b'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/miniupnpc-2.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/miniupnpc-2.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/miniupnpc-2.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/miniupnpc-2.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '4d2540a3b8465f16d54a74aa8fb3aee103f28a24e6e0800049c19f4ac116619a',
-     armv7l: '4d2540a3b8465f16d54a74aa8fb3aee103f28a24e6e0800049c19f4ac116619a',
-       i686: '061732c34b114a957b2eede570e0aee3b468e3c94600fc52902aec04a1ddc7cd',
-     x86_64: '6092ef9f6538f1403c904c349535cd2bddedfd27fe8e7e7b39627c3518b8bf44',
-  })
+  version '2.0.20171212'
+  source_url 'http://miniupnp.free.fr/files/download.php?file=miniupnpc-2.0.20171212.tar.gz'
+  source_sha256 '6eda4744fe26a3c7fb626e3044e9a88af145f88d29a8c2a41ca8384a25482e0d'
 
   def self.build
     system "sed -i '139s,/usr,,' Makefile"
