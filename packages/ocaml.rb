64318702dc2f9c166abcf5d723ac875629dd55b3
Ed Reel <edreel@gmail.com>
2017-12-16 10:20:25 -0600

    Add pre-built binaries for expect, miniupnpc, ocaml, ruby and scons

--- packages/ocaml.rb
+++ packages/ocaml.rb
@@ -7,6 +7,19 @@ class Ocaml < Package
   source_url 'https://caml.inria.fr/pub/distrib/ocaml-4.06/ocaml-4.06.0.tar.xz'
   source_sha256 '1236b5f91e1c075086d69e2d40cfab21e048b9fe38e902f707815bebbc20c5b7'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ocaml-4.06.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ocaml-4.06.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ocaml-4.06.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ocaml-4.06.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '66f2bf801661c70eb4779f8075da2454b13f4b71cb3efe8680e487c54d539bb2',
+     armv7l: '66f2bf801661c70eb4779f8075da2454b13f4b71cb3efe8680e487c54d539bb2',
+       i686: 'c5942aa70696eee4c242f27a85751c7823f556eb36804f1171fde1ea64f53ba2',
+     x86_64: '3855090573fb7a460c65f2c2705d8f73cf011e79f925a30508397c9ae2376a6f',
+  })
+
   depends_on 'buildessential' => :build
   depends_on 'gdbm'
 
--- packages/scons.rb
+++ packages/scons.rb
@@ -7,6 +7,19 @@ class Scons < Package
   source_url 'https://prdownloads.sourceforge.net/scons/scons-3.0.1.tar.gz'
   source_sha256 '24475e38d39c19683bc88054524df018fe6949d70fbd4c69e298d39a0269f173'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/scons-3.0.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/scons-3.0.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/scons-3.0.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/scons-3.0.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '15e77d81c7c8ea494594896659d552e88d7d71daea7d8fb88b59d582277d33fa',
+     armv7l: '15e77d81c7c8ea494594896659d552e88d7d71daea7d8fb88b59d582277d33fa',
+       i686: '8c7249140fcecd97daf1cdaa7bf3df76281d89ff20697b9990abe6b8e3905484',
+     x86_64: 'c1196e05cda0e471b61d6948cb02dd74a32e8c68794901bf63d72a6e7a0d135e',
+  })
+
   depends_on 'python27'
 
   def self.build
