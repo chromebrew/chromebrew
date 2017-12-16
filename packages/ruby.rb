45df1bc33da9ada7276f1d493d19e176d31aef0b
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2017-12-15 22:26:44 -0500

    Update ruby.rb

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -3,22 +3,9 @@ require 'package'
 class Ruby < Package
   description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
   homepage 'https://www.ruby-lang.org/en/'
-  version '2.4.1'
-  source_url 'https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.1.tar.xz'
-  source_sha256 '4fc8a9992de3e90191de369270ea4b6c1b171b7941743614cc50822ddc1fe654'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.4.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.4.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.4.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.4.1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '7bbdd4234a0db3ca0a9b368513b05d23e4e7c347506f89647bd71892f3d85ed3',
-     armv7l: '7bbdd4234a0db3ca0a9b368513b05d23e4e7c347506f89647bd71892f3d85ed3',
-       i686: 'e52ad60357b2dd198b701eb11caf3ee8b0c3d61ad4c428233476a43a3ec9612f',
-     x86_64: 'c6660e493c4f7caa5c2e677841eec754eaeb88b76352d59d80764fb3f3445638',
-  })
+  version '2.4.2'
+  source_url 'https://cache.ruby-lang.org/pub/ruby/ruby-2.4.2.tar.xz'
+  source_sha256 '748a8980d30141bd1a4124e11745bb105b436fb1890826e0d2b9ea31af27f735'
 
   depends_on 'readline'
   depends_on 'ncurses'
@@ -27,7 +14,7 @@ class Ruby < Package
   # at run-time, system's gmp, openssl, readline and zlibpkg are possible to use
 
   def self.build
-    system "CC='gcc' ./configure --libdir=#{CREW_LIB_PREFIX} --enable-shared"
+    system "CC='gcc' ./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} --enable-shared"
     system "make"
   end
 
