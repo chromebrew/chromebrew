1b960c1647475574321b4051ec934fad98616b32
Ed Reel <edreel@gmail.com>
2017-12-20 22:25:20 -0600

    Add pre-built binaries for dbus, libuv, qemu and r packages
    Replace local binary test file

--- packages/r.rb
+++ packages/r.rb
@@ -7,6 +7,19 @@ class R < Package
   source_url 'https://cran.r-project.org/src/base/R-3/R-3.4.3.tar.gz'
   source_sha256 '7a3cb831de5b4151e1f890113ed207527b7d4b16df9ec6b35e0964170007f426'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.4.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.4.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.4.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.4.3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '49a5b66353b19c4078daa3081819348f391949dfaea74b25a4a08c15fe6b0d58',
+     armv7l: '49a5b66353b19c4078daa3081819348f391949dfaea74b25a4a08c15fe6b0d58',
+       i686: 'b3ef1c5ee0c69f8da1dda0292299df1ffc6692c63198a87fe7add6a8bcecd84c',
+     x86_64: '997022827cf2709da1c76ab9ffbefcec6f5be21df2a72b1c33c2a797a77dcead',
+  })
+
   # depends_on 'gfortran'       # require gfortran enabled gcc
   depends_on 'pcre'             # need to use pcre not pcre2
   depends_on 'zlibpkg'
