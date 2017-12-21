237dda24adf351acff769be31d516684411e2400
Ed Reel <edreel@gmail.com>
2017-12-21 10:37:57 -0600

    Add pre-built binaries for chicken, cmake and lynis packages

--- packages/chicken.rb
+++ packages/chicken.rb
@@ -7,6 +7,19 @@ class Chicken < Package
   source_url 'https://code.call-cc.org/releases/4.13.0/chicken-4.13.0.tar.gz'
   source_sha256 'add549619a31363d6608b39e0cf0e68b9d5e6ff2a719b5691ddeba57229c6c43'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-4.13.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-4.13.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-4.13.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-4.13.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'bfc9280f5d2c3e079f7bdc1819e71243c6464e0e855878bbcb8f05f4f033ae20',
+     armv7l: 'bfc9280f5d2c3e079f7bdc1819e71243c6464e0e855878bbcb8f05f4f033ae20',
+       i686: '431b8e207f33561da047c373f38904c54aa8680d258c2b4788f327461243e776',
+     x86_64: 'fff1ca9617ebf651e31f552fb68c916d38a0278521f188956cdf2d284e7f5d61',
+  })
+
   def self.build
     system "make", "PLATFORM=linux"
   end
--- packages/cmake.rb
+++ packages/cmake.rb
@@ -7,6 +7,19 @@ class Cmake < Package
   source_url 'https://cmake.org/files/v3.10/cmake-3.10.1.tar.gz'
   source_sha256 '7be36ee24b0f5928251b644d29f5ff268330a916944ef4a75e23ba01e7573284'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.10.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.10.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.10.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.10.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'bbdc47f0ee67e500c6cab439b9443dc0d21f2d472709b6cb9232490a8d7e9254',
+     armv7l: 'bbdc47f0ee67e500c6cab439b9443dc0d21f2d472709b6cb9232490a8d7e9254',
+       i686: '47a1dd635316052f383a36949a49decb0ca6ba57d40d5f118c0361e5dacf52ce',
+     x86_64: '77c23449a2a8e4a9f98069b330d2e186a5845bba78a4899d7a84840b378c3b7f',
+  })
+
   depends_on 'buildessential'
   depends_on 'openssl'
   depends_on 'ncurses'
