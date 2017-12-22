c693ed120759f10332c00f13b471d67dfbf03fc9
Ed Reel <edreel@gmail.com>
2017-12-22 14:10:24 -0600

    Add pre-built binaries for gdal, jansson, libuuid, sejda_console and vim packages

--- packages/gdal.rb
+++ packages/gdal.rb
@@ -8,8 +8,16 @@ class Gdal < Package
   source_sha256 'a328d63d476b3653f5a25b5f7971e87a15cdf8860ab0729d4b1157ba988b8d0b'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gdal-2.2.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '87fc7a9a09710e991981b46d4507384e57580e118885ba1fe0f392fb39ae5b8c',
+     armv7l: '87fc7a9a09710e991981b46d4507384e57580e118885ba1fe0f392fb39ae5b8c',
+       i686: 'c3926343dd3c528b21ae50f3b30e5347c2bd0530227fd28b9503cde3539820f7',
+     x86_64: 'f7a43568512f9426a7951b90006507e4d7080cb61d68fa152a3723c3e0c4e90e',
   })
 
   depends_on 'python27'
--- packages/sejda_console.rb
+++ packages/sejda_console.rb
@@ -6,6 +6,19 @@ class Sejda_console < Package
   version '3.2.43'
   source_url 'https://github.com/torakiki/sejda/releases/download/v3.2.43/sejda-console-3.2.43-bin.zip'
   source_sha256 '0d1c5e9e5857332cc50a881e6ab6f2051ebd8ae04f8421f6865c38b9f14dd67c'
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.43-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.43-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.43-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/sejda_console-3.2.43-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '28de2bf7e8b6b5edd1534334729e88c1c2f7611debc8ad443c80660b4659d420',
+     armv7l: '28de2bf7e8b6b5edd1534334729e88c1c2f7611debc8ad443c80660b4659d420',
+       i686: 'ef5a2804b143f13123f83a5f1e2741f3e20d11f3fcdb9bca455239009b7aab48',
+     x86_64: 'd462f3d37b2f0ecb9657a20f983e70e13ff261bd75046d260c6dc99d1171c632',
+  })
   
   depends_on 'help2man' => :build
   depends_on 'jdk8'
