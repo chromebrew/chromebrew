e6799969bf0d117fa378c937a54c9a6f35894d8b
Ed Reel <edreel@gmail.com>
2017-12-24 01:30:37 -0600

    Add pre-built binaries for aalib, dos2unix, expat and libpipeline packages

--- packages/expat.rb
+++ packages/expat.rb
@@ -7,6 +7,19 @@ class Expat < Package
   source_url 'https://downloads.sourceforge.net/project/expat/expat/2.2.5/expat-2.2.5.tar.bz2'
   source_sha256 'd9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.5-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.5-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.5-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.5-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'd4fa088b346329e2cf8fd1d11bc56fd5c6a8eac4053d39656cc5345d7734471b',
+     armv7l: 'd4fa088b346329e2cf8fd1d11bc56fd5c6a8eac4053d39656cc5345d7734471b',
+       i686: '8af1d7c0f458360eed69132acaaf22008db74f2f602b8aafd75203f168bf76d6',
+     x86_64: '164b4f7de9bc956d13514b7fe66f9e7c3745fc6988b25e53e754325d40070fab',
+  })
+
   def self.build
     system "./configure", "--libdir=#{CREW_LIB_PREFIX}", "--enable-shared", "--disable-static", "--with-pic"
     system "make"
--- packages/libpipeline.rb
+++ packages/libpipeline.rb
@@ -8,6 +8,19 @@ class Libpipeline < Package
   source_url 'https://mirror.csclub.uwaterloo.ca/nongnu/libpipeline/libpipeline-1.5.0.tar.gz'
   source_sha256 '0d72e12e4f2afff67fd7b9df0a24d7ba42b5a7c9211ac5b3dcccc5cd8b286f2b'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libpipeline-1.5.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libpipeline-1.5.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libpipeline-1.5.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libpipeline-1.5.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'b22c978f504de0e8356772bdb6c775a6d4b9760cb5422ab540a70940d63e8cab',
+     armv7l: 'b22c978f504de0e8356772bdb6c775a6d4b9760cb5422ab540a70940d63e8cab',
+       i686: '5ab87ee758833b15d55b680fee6742f27f26e25394feaeeca5d1c2d6e069b1d5',
+     x86_64: '9cefcb70b634fa7d1ff081794712ca4e1448e0dbfd57d0be411bbef2a149bd78',
+  })
+
   def self.build
     system './configure', "--libdir=#{CREW_LIB_PREFIX}", '--disable-static', '--enable-shared', '--with-pic'
     system 'make'
