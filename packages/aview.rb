2457dd712f717ca8bb3944f42e78103eb5ecf285
Ed Reel <edreel@gmail.com>
2017-12-30 22:15:54 -0600

    Add pre-built binaries for aspell_en, aview, fswatch, iptraf and rtmpdump packages

--- packages/aview.rb
+++ packages/aview.rb
@@ -8,8 +8,16 @@ class Aview < Package
   source_sha256 '42d61c4194e8b9b69a881fdde698c83cb27d7eda59e08b300e73aaa34474ec99'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/aview-1.3.0rc1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/aview-1.3.0rc1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/aview-1.3.0rc1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/aview-1.3.0rc1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'b7dac7b7bb1df20b2f3c5f4b3072d7c687d00187a7f65e25e7d754b658215bcd',
+     armv7l: 'b7dac7b7bb1df20b2f3c5f4b3072d7c687d00187a7f65e25e7d754b658215bcd',
+       i686: '4646662cc91ca8049a577bb0a0ceb2545645250c6bc05a124fd2c8c2eb536c05',
+     x86_64: '70d29a20c0ade541d6f0aba440412d844a4815f8c0317a00d7c13005e1232395',
   })
 
   depends_on 'aalib'
--- packages/rtmpdump.rb
+++ packages/rtmpdump.rb
@@ -8,10 +8,19 @@ class Rtmpdump < Package
   source_sha256 'dba4d4d2e1c7de6884b01d98194b83cab6784669089fa3c919152087a3a38fd2'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-fa8646dae-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-fa8646dae-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-fa8646dae-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-fa8646dae-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '04a7e360b0290d82402addc6432d6b4d3c551aafb99ab3902bf30df6de2bdeae',
+     armv7l: '04a7e360b0290d82402addc6432d6b4d3c551aafb99ab3902bf30df6de2bdeae',
+       i686: '2298fa49696d409dbfe0bd159d4199b1219d2da4a1746fb5c1644f2f0a94a5d7',
+     x86_64: '163c47847d3cfcfe9178995c96511e7642991a1bb567f90d33f26e36e85d16e6',
   })
 
+  depends_on 'openssl'
   depends_on 'zlibpkg'
 
   def self.build
