6e0087ec508154b008d3e216cb47e76824540036
Don Harper <duck@duckland.org>
2017-12-30 22:51:51 -0600

    libassuan.so : added dep on libgpgerror

--- packages/libassuan.rb
+++ packages/libassuan.rb
@@ -7,6 +7,8 @@ class Libassuan < Package
   source_url 'https://www.gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.1.tar.bz2'
   source_sha256 '47f96c37b4f2aac289f0bc1bacfa8bd8b4b209a488d3d15e2229cb6cc9b26449'
 
+  depends_on 'libgpgerror'
+
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system 'make'
