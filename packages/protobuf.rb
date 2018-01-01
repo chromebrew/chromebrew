e9aaaa18c11191ac19534c1a04e8d8e321c3baf6
Don Harper <duck@duckland.org>
2018-01-01 13:42:34 -0600

    protobuf - update to 3.5.1

--- packages/protobuf.rb
+++ packages/protobuf.rb
@@ -3,9 +3,9 @@ require 'package'
 class Protobuf < Package
   description 'Protocol buffers are a language-neutral, platform-neutral extensible mechanism for serializing structured data.'
   homepage 'https://developers.google.com/protocol-buffers/'
-  version '3.3.0-2'
-  source_url 'https://github.com/google/protobuf/archive/v3.3.0.tar.gz'
-  source_sha256 '9a36bc1265fa83b8e818714c0d4f08b8cec97a1910de0754a321b11e66eb76de'
+  version '3.5.1'
+  source_url 'https://github.com/google/protobuf/archive/v3.5.1.tar.gz'
+  source_sha256 '826425182ee43990731217b917c5c3ea7190cfda141af4869e6d4ad9085a740f'
 
   binary_url ({
   })
