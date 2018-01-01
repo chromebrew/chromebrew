4c944e30e5f9201718bf205fbcbaaf54c19c73ef
Don Harper <duck@duckland.org>
2018-01-01 13:28:30 -0600

    manpages - update to 4.14

--- packages/manpages.rb
+++ packages/manpages.rb
@@ -3,9 +3,9 @@ require 'package'
 class Manpages < Package
   description 'The Linux man-pages project documents the Linux kernel and C library interfaces that are employed by user-space programs.'
   homepage 'https://www.kernel.org/doc/man-pages/'
-  version '4.13'
-  source_url 'https://www.kernel.org/pub/linux/docs/man-pages/man-pages-4.13.tar.xz'
-  source_sha256 'd5c005c5b653248ab6680560de00ea8572ff39e48a57bd5be1468d986a0631bf'
+  version '4.14'
+  source_url 'https://www.kernel.org/pub/linux/docs/man-pages/man-pages-4.14.tar.xz'
+  source_sha256 '3052b87898c313c089848a913e5cf44a0565cc4d21d94119ef6586d971f5c971'
 
   binary_url ({
   })
