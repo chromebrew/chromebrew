8237fe1ba14bb6ef4c76b8bf18462293e48c1f6b
Don Harper <duck@duckland.org>
2018-01-01 13:53:29 -0600

    xe - update to 0.11

--- packages/xe.rb
+++ packages/xe.rb
@@ -3,9 +3,9 @@ require 'package'
 class Xe < Package
   description 'simple xargs and apply replacement.'
   homepage 'https://github.com/chneukirchen/xe/'
-  version '0.10'
-  source_url 'https://github.com/chneukirchen/xe/archive/v0.10.tar.gz'
-  source_sha256 '8993cea06b2c664195df2a6124d0386d1bce7c27eb6ecbf968866bfd05cb9d7a'
+  version '0.11'
+  source_url 'https://github.com/chneukirchen/xe/archive/v0.11.tar.gz'
+  source_sha256 '4087d40be2db3df81a836f797e1fed17d6ac1c788dcf0fd6a904f2d2178a6f1a'
 
   binary_url ({
   })
