c7af9929bca7d05fb063995909a951741c311438
Don Harper <duck@duckland.org>
2018-01-01 13:08:27 -0600

    gzsize - update to 1.2

--- packages/gzsize.rb
+++ packages/gzsize.rb
@@ -3,21 +3,13 @@ require 'package'
 class Gzsize < Package
   description 'Print the uncompressed size of a GZipped file.'
   homepage 'https://bfontaine.github.io/gzsize/'
-  version '0.1.1'
-  source_url 'https://github.com/bfontaine/gzsize/archive/0.1.1.tar.gz'
-  source_sha256 'ffb9cc1e5ed10443b1bcf2f711787bc7f69eee27ed83b48f2ccf9d80e39554dd'
+  version '1.2'
+  source_url 'https://github.com/bfontaine/gzsize/archive/0.1.2.tar.gz'
+  source_sha256 '32e20cb2e570f16bc30e62c4fda406ede23b23dfd313fc82528173827487b170'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gzsize-0.1.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gzsize-0.1.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gzsize-0.1.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gzsize-0.1.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'd4a24b086e172b771b389d5ce421877e5c3763dc66731cbbfa3bfe2fc40b66c6',
-     armv7l: 'd4a24b086e172b771b389d5ce421877e5c3763dc66731cbbfa3bfe2fc40b66c6',
-       i686: 'e8bbc0b2342a40b5c6cb231d61425a78e2804df614bd34bfe37ffc0cb2659c84',
-     x86_64: '5da9324de5a8799dad4cc5126033eefbed40aeb5c2e5616aede325010f500b91',
   })
 
   def self.build
