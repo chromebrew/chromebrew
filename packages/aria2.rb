6d7663316235c81a77941659ac00b913145d0404
Don Harper <duck@duckland.org>
2018-01-01 14:01:15 -0600

    aria2 - switch to download the .xz

--- packages/aria2.rb
+++ packages/aria2.rb
@@ -4,8 +4,8 @@ class Aria2 < Package
   description 'aria2 is a lightweight multi-protocol & multi-source, cross platform download utility operated in command-line. It supports HTTP/HTTPS, FTP, SFTP, BitTorrent and Metalink.'
   homepage 'https://aria2.github.io/'
   version '1.33.1'
-  source_url 'https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.bz2'
-  source_sha256 '32e8dc76da0c630da18d6a656afb117fef8f2f71d6f38e326a6e09fa113e2109'
+  source_url 'https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.xz'
+  source_sha256 '2539e4844f55a1f1f5c46ad42744335266053a69162e964d9a2d80a362c75e1b'
 
   binary_url ({
   })
