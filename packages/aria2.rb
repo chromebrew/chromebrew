5268c8fc4402ba46475c6e1a0f07d4dd54f1aa39
Don Harper <duck@duckland.org>
2018-01-01 12:23:32 -0600

    aria2 - update to 1.33.1

--- packages/aria2.rb
+++ packages/aria2.rb
@@ -3,9 +3,9 @@ require 'package'
 class Aria2 < Package
   description 'aria2 is a lightweight multi-protocol & multi-source, cross platform download utility operated in command-line. It supports HTTP/HTTPS, FTP, SFTP, BitTorrent and Metalink.'
   homepage 'https://aria2.github.io/'
-  version '1.33.0'
-  source_url 'https://github.com/aria2/aria2/releases/download/release-1.33.0/aria2-1.33.0.tar.xz'
-  source_sha256 '996e3fc2fd07ce2dd517e20a1f79b8b3dbaa5c7e27953b5fc19dae38f3874b8c'
+  version '1.33.1'
+  source_url 'https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.bz2'
+  source_sha256 '32e8dc76da0c630da18d6a656afb117fef8f2f71d6f38e326a6e09fa113e2109'
 
   binary_url ({
   })
