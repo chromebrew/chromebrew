453a7c9764305597634b9d15fa951fa91112027c
Don Harper <duck@duckland.org>
2018-01-01 13:29:19 -0600

    memcached - update to 1.5.4

--- packages/memcached.rb
+++ packages/memcached.rb
@@ -3,9 +3,9 @@ require 'package'
 class Memcached < Package
   description 'Memcached is an in-memory key-value store for small chunks of arbitrary data (strings, objects) from results of database calls, API calls, or page rendering.'
   homepage 'https://memcached.org/'
-  version '1.5.3'
-  source_url 'https://memcached.org/files/memcached-1.5.3.tar.gz'
-  source_sha256 '258cc3ddb7613685465acfd0215f827220a3bbdd167fd2c080632105b2d2f3ce'
+  version '1.5.4'
+  source_url 'https://www.memcached.org/files/memcached-1.5.4.tar.gz'
+  source_sha256 'e0c3cfa89fa4c2ffd8aa45df7825c6d1a2423ac89ab1a7c4f42bb9803f7403d4'
 
   binary_url ({
   })
