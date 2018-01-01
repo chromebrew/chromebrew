c6cd7d97e49b3735d0950de9455eacc5b750997c
Don Harper <duck@duckland.org>
2018-01-01 14:03:12 -0600

    datamash - switch download URL back to gnu.org

--- packages/datamash.rb
+++ packages/datamash.rb
@@ -4,7 +4,7 @@ class Datamash < Package
   description 'GNU Datamash is a command-line program which performs basic numeric,textual and statistical operations on input textual data files.'
   homepage 'http://savannah.gnu.org/projects/datamash'
   version '1.2'
-  source_url 'https://mirror.sergal.org/gnu/datamash/datamash-1.2.tar.gz'
+  source_url 'https://ftpmirror.gnu.org/datamash/datamash-1.2.tar.gz'
   source_sha256 'e8d46fb22ccc77e5380f26cde622a733f363d388b04a2c22e7fb6de0e9d85996'
 
   binary_url ({
