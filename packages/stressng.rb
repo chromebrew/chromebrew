01e3bd436aa1a5b667c1afd51a1c921e58881d89
Don Harper <duck@duckland.org>
2018-01-01 13:48:22 -0600

    stressng - update to 0.09.07

--- packages/stressng.rb
+++ packages/stressng.rb
@@ -3,9 +3,9 @@ require 'package'
 class Stressng < Package
   description 'stress-ng will stress test a computer system in various selectable ways.'
   homepage 'http://kernel.ubuntu.com/~cking/stress-ng/'
-  version '0.09.02'
-  source_url 'http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.09.02.tar.gz'
-  source_sha256 '9fffd8e8157ee969dfe99d1a5b310ff3337b1dbecd276ccaa8c30c1cc14392fd'
+  version '0.09.07'
+  source_url 'http://kernel.ubuntu.com/~cking/tarballs/stress-ng/stress-ng-0.09.07.tar.xz'
+  source_sha256 'e4dc07e127e23de7c55f69687ef5f55ee718c386a45ba53f3560e01819a3205e'
 
   binary_url ({
   })
