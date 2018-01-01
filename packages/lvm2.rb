e90c53fc4a71453f4415adab502d2ea918262339
Don Harper <duck@duckland.org>
2018-01-01 13:26:51 -0600

    lvm2 - update to 2.02.177

--- packages/lvm2.rb
+++ packages/lvm2.rb
@@ -3,9 +3,9 @@ require 'package'
 class Lvm2 < Package
   description 'LVM2 refers to the userspace toolset that provide logical volume management facilities on linux.'
   homepage 'https://sourceware.org/lvm2'
-  version '2.02.176'
-  source_url 'ftp://sources.redhat.com/pub/lvm2/releases/LVM2.2.02.176.tgz'
-  source_sha256 'dece83628c2c78a267a126ce6046d506582185ae5d40fb8d59b56547fccea473'
+  version '2.02.177'
+  source_url 'ftp://sources.redhat.com/pub/lvm2/releases/LVM2.2.02.177.tgz'
+  source_sha256 '4025a23ec9b15c2cb7486d151c29dc953b75efc4d452cfe9dbbc7c0fac8e80f2'
 
   depends_on 'readline'
 
