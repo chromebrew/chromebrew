c485997d81fdb6b6846f375c4ff5fa2e4250967d
Don Harper <duck@duckland.org>
2018-01-01 13:47:26 -0600

    strace - update to 4.20

--- packages/strace.rb
+++ packages/strace.rb
@@ -3,9 +3,9 @@ require 'package'
 class Strace < Package
   description 'strace is a diagnostic, debugging and instructional userspace utility for Linux.'
   homepage 'https://strace.io/'
-  version '4.19'
-  source_url 'https://downloads.sourceforge.net/project/strace/strace/4.19/strace-4.19.tar.xz'
-  source_sha256 '7c93ebc6c29280f47c24a0eb86873a99ccb2cac6512c60a60ba4ef99ab807281'
+  version '4.20'
+  source_url 'https://downloads.sourceforge.net/project/strace/strace/4.20/strace-4.20.tar.xz'
+  source_sha256 '5bf3148dd17306a42566f7da17368fdd781afa147db05ea63a4ca2b50f58c523'
 
   binary_url ({
   })
