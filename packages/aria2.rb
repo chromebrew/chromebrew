baabfb83d495381bda43f5ba235910c296f91e59
Ed Reel <edreel@gmail.com>
2018-01-01 21:39:54 -0600

    Add pre-built binaries for aria2, ascii, asciidoc, avocado, bdwgc, datamash, whois and wput
    Update versions based on changes
    
    Bump versions to detect changes
    
    Change permissions on whois.conf

--- packages/aria2.rb
+++ packages/aria2.rb
@@ -3,21 +3,21 @@ require 'package'
 class Aria2 < Package
   description 'aria2 is a lightweight multi-protocol & multi-source, cross platform download utility operated in command-line. It supports HTTP/HTTPS, FTP, SFTP, BitTorrent and Metalink.'
   homepage 'https://aria2.github.io/'
-  version '1.33.1'
+  version '1.33.1-1'
   source_url 'https://github.com/aria2/aria2/releases/download/release-1.33.1/aria2-1.33.1.tar.xz'
   source_sha256 '2539e4844f55a1f1f5c46ad42744335266053a69162e964d9a2d80a362c75e1b'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/aria2-1.33.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'afa65990c71c2db8c8af57e2bf52345db576929995f310335224249f642f749d',
-     armv7l: 'afa65990c71c2db8c8af57e2bf52345db576929995f310335224249f642f749d',
-       i686: 'aec5932f39758c78c098d25d07d115a2f2ad655083651c554263be538fffd29a',
-     x86_64: '8457b44396e6ef4f76c3e386389ba742a08280bdea9a9376da1795ef07f28c2e',
+    aarch64: '25987f8b48bb8063ee39ae3a260aec1db1917974c6622dd10628cd7ca919a803',
+     armv7l: '25987f8b48bb8063ee39ae3a260aec1db1917974c6622dd10628cd7ca919a803',
+       i686: 'a1693909b4ae5ddd9f7d0c56b118b49cb4ab85f60965b2c67a16bee055eee7de',
+     x86_64: '94053db3425eb109f7831f9db293c439d4c0b0e17e70f40a3a3a5514a48b2b9a',
   })
 
   depends_on 'c_ares'
--- packages/whois.rb
+++ packages/whois.rb
@@ -3,13 +3,21 @@ require 'package'
 class Whois < Package
   description 'Intelligent WHOIS client'
   homepage 'https://github.com/rfc1036/whois'
-  version '5.2.20'
+  version '5.2.20-1'
   source_url 'https://github.com/rfc1036/whois/archive/v5.2.20.tar.gz'
   source_sha256 '1812b9c64a41d8ed70507bb1161a18a0a7b2f29ba5b442ca7828a5acb1e44c7e'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/whois-5.2.20-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/whois-5.2.20-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/whois-5.2.20-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/whois-5.2.20-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '0f540befde8dc78202a0c7c30e73b15bf7e3db6bf108e762e231a58bf7f2f8eb',
+     armv7l: '0f540befde8dc78202a0c7c30e73b15bf7e3db6bf108e762e231a58bf7f2f8eb',
+       i686: '543cd5034dce8928a35d03c90d9f1357b98321b4135c93f3b0bd1c92e51edd64',
+     x86_64: 'b8125e4f3ab2652c45069d0e56ea7eb9118f7555e4b1deafb057f43ac42b691b',
   })
 
   depends_on 'gettext'
@@ -24,13 +32,12 @@ class Whois < Package
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/bin"
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/man/man1"
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/man/man5"
-    system "cp mkpasswd #{CREW_DEST_DIR}/usr/local/bin"
-    system "cp whois #{CREW_DEST_DIR}/usr/local/bin"
-    system "cp mkpasswd.1 #{CREW_DEST_DIR}/usr/local/man/man1"
-    system "cp whois.1 #{CREW_DEST_DIR}/usr/local/man/man1"
-    system "cp whois.conf.5 #{CREW_DEST_DIR}/usr/local/man/man5"
+    system "gzip -9 mkpasswd.1 whois.1 whois.conf.5"
+    system "install -Dm755 mkpasswd #{CREW_DEST_PREFIX}/bin/mkpasswd"
+    system "install -Dm755 whois #{CREW_DEST_PREFIX}/bin/whois"
+    system "install -Dm644 whois.conf #{CREW_DEST_PREFIX}/etc/whois.conf"
+    system "install -Dm644 mkpasswd.1.gz #{CREW_DEST_PREFIX}/man/man1/mkpasswd.1.gz"
+    system "install -Dm644 whois.1.gz #{CREW_DEST_PREFIX}/man/man1/whois.1.gz"
+    system "install -Dm644 whois.conf.5.gz #{CREW_DEST_PREFIX}/man/man5/whois.conf.5.gz"
   end
 end
