64e27cda88936fca769e44857a78497fe76b5b2c
Ed Reel <edreel@gmail.com>
2018-01-05 12:37:03 -0600

    Add pre-built binaries for siege, snooze, tpp and vim

--- packages/siege.rb
+++ packages/siege.rb
@@ -8,8 +8,16 @@ class Siege < Package
   source_sha256 '8f7dcf18bd722bb9cc92bc3ea4b59836b4a2f8d8f01d4a94c8d181f56d91ea6f'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/siege-4.0.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/siege-4.0.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/siege-4.0.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/siege-4.0.4-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '626dab09c5fe3682c416e379c3f3e3115f46658a0b1094786a3932afcc3286df',
+     armv7l: '626dab09c5fe3682c416e379c3f3e3115f46658a0b1094786a3932afcc3286df',
+       i686: '35b04a8f7c6a5fa298f617c9f6a3a1f387a0b2580504e493b6001a5bce96a6ef',
+     x86_64: '3b56b8491da7d50beaa5b8cad9c481c99fabe8e7f04368a93025af8fd0619e8f',
   })
 
   depends_on 'compressdoc' => :build
--- packages/vim.rb
+++ packages/vim.rb
@@ -3,7 +3,7 @@ require 'package'
 class Vim < Package
   description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient.'
   homepage 'http://www.vim.org/'
-  version '8.0'
+  version '8.0-1'
   source_url 'ftp://ftp.vim.org/pub/vim/unix/vim-8.0.tar.bz2'
   source_sha256 '08bd0d1dd30ece3cb9905ccd48b82b2f81c861696377508021265177dc153a61'
 
@@ -14,10 +14,10 @@ class Vim < Package
      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '3fe52510d8b3d3d6d39dd43d4404720da714ca215a1688afa72af90e1ec413df',
-     armv7l: '3fe52510d8b3d3d6d39dd43d4404720da714ca215a1688afa72af90e1ec413df',
-       i686: '64ab42391041be65a77464d48cd71edc46e161d88d25ef3cb12b5dd9d1b47219',
-     x86_64: '0e55111657d14d950b97e4b6d14b96ccf5ab42afd0e5e73387e2d6adcf897b0e',
+    aarch64: '86d5b6b889110adab6bb3bfb34aa9579c37f26efcb35b1c4523558fdedb7dace',
+     armv7l: '86d5b6b889110adab6bb3bfb34aa9579c37f26efcb35b1c4523558fdedb7dace',
+       i686: 'cea1f34f7e1456fd9725d18e943f3eee1735e82fe1ec2a35bf3197971436750f',
+     x86_64: '3bfd0d28f2a05d9d551f449e9a5b662a906c0a7799c8263cac05567dcb2b9cc4',
   })
 
   depends_on 'compressdoc' => :build
