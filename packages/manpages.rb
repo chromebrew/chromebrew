c2230ff1ef9c736db9e4b642a70c82f6ee256646
Ed Reel <edreel@gmail.com>
2018-01-12 23:47:03 -0600

    Update manpages from 4.13 to 4.14

--- packages/manpages.rb
+++ packages/manpages.rb
@@ -3,21 +3,13 @@ require 'package'
 class Manpages < Package
   description 'The Linux man-pages project documents the Linux kernel and C library interfaces that are employed by user-space programs.'
   homepage 'https://www.kernel.org/doc/man-pages/'
-  version '4.13'
-  source_url 'https://www.kernel.org/pub/linux/docs/man-pages/man-pages-4.13.tar.xz'
-  source_sha256 'd5c005c5b653248ab6680560de00ea8572ff39e48a57bd5be1468d986a0631bf'
+  version '4.14'
+  source_url 'https://www.kernel.org/pub/linux/docs/man-pages/man-pages-4.14.tar.xz'
+  source_sha256 '3052b87898c313c089848a913e5cf44a0565cc4d21d94119ef6586d971f5c971'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/manpages-4.13-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/manpages-4.13-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/manpages-4.13-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/manpages-4.13-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '97ddc239d22e2d9651d531d16872cfbcbc4f1fb9a9cc98379ce3141c2f088872',
-     armv7l: '97ddc239d22e2d9651d531d16872cfbcbc4f1fb9a9cc98379ce3141c2f088872',
-       i686: '744a2625e56519fc7d5a9bb9d110431f59fa400eebaad162659dc48c0ed9cfd9',
-     x86_64: '9d617f18a4cdfb42b45a2b475e36b82dc693244065e021387aa5c222395877ac',
   })
 
   depends_on 'mandb'
