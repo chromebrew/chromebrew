6baab93ed1b251576ab7a23c9b5fa9890df568b7
Ed Reel <edreel@gmail.com>
2018-01-13 16:54:02 -0600

    Update harfbuzz from 1.4.8 to 1.7.4

--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -3,21 +3,13 @@ require 'package'
 class Harfbuzz < Package
   description 'HarfBuzz is an OpenType text shaping engine.'
   homepage 'https://www.freedesktop.org/wiki/Software/HarfBuzz/'
-  version '1.4.8'
-  source_url 'https://github.com/behdad/harfbuzz/releases/download/1.4.8/harfbuzz-1.4.8.tar.bz2'
-  source_sha256 'ccec4930ff0bb2d0c40aee203075447954b64a8c2695202413cc5e428c907131'
+  version '1.7.4'
+  source_url 'https://github.com/harfbuzz/harfbuzz/releases/download/1.7.4/harfbuzz-1.7.4.tar.bz2'
+  source_sha256 'b5d6ac8415f97f3540d73f3f91c41c5c10f8a4d76350f11a7184062aae88ac0b'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/harfbuzz-1.4.8-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'ff18bdc0196bbf720b9a39512ddacfbdd78a1bec1d0fa5bedb6ab9d306de0eba',
-     armv7l: 'ff18bdc0196bbf720b9a39512ddacfbdd78a1bec1d0fa5bedb6ab9d306de0eba',
-       i686: 'ba39ab3fbddef9cf32a60ea03aed0e0ada4d6126e565e30054e2fffa285f6864',
-     x86_64: 'ed686080f0d20252a0e82d72eb01a261629ac2fe9a31840e2afcfd8589829dbc',
   })
 
   depends_on 'glib'
@@ -25,7 +17,7 @@ class Harfbuzz < Package
   depends_on 'freetype'
 
   def self.build
-    system "./configure"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
