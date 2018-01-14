066cb61595ad8b4a8c566df9b7ecc59dc0fe7f89
Ed Reel <edreel@gmail.com>
2018-01-13 19:08:32 -0600

    Replace imagemagick with multiple version install options

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -3,29 +3,59 @@ require 'package'
 class Imagemagick < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '7.0.7-15'
-  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-15.tar.xz'
-  source_sha256 '9809be6bab0c5258721a5c5bbe8bccf61357b506024f8514d94e9efcd575a69d'
+  version '6.9.9-33-7.0.7-21'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.7-15-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.7-15-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.7-15-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/imagemagick-7.0.7-15-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '702365fbf31a26bbd0e395ba1d3fae2cbb9112ae8e5a8fd114bcb00bc0c6b7b6',
-     armv7l: '702365fbf31a26bbd0e395ba1d3fae2cbb9112ae8e5a8fd114bcb00bc0c6b7b6',
-       i686: '386b84c9a0cc1b93171a89a086ef15b6f553c61d930da5087879b7c4d89b670d',
-     x86_64: 'bd307f8ad7b79e89016679bf90da61159d25479ab5ce8bcbae0218f97387c0ed',
-  })
+  if ARGV[0] == 'install'
+    imver = `stream -version 2> /dev/null | head -1 | cut -d' ' -f3`.chomp
+    abort "ImageMagick version #{imver} already installed.".lightgreen unless "#{imver}" == ""
+    puts
+    puts "Enter the ImageMagick version to install:"
+    puts "6 = ImageMagick 6.9.9-33"
+    puts "7 = ImageMagick 7.0.7-21"
+    puts "0 = Cancel"
+
+    while version = STDIN.gets.chomp
+      case version
+      when '6'
+        source_url 'https://www.imagemagick.org/download/releases/ImageMagick-6.9.9-33.tar.xz'
+        source_sha256 '996adc81c91ebb589c0483b88ce4ddd76e91e0acee4858f71c9d024bda717869'
+        binary_url ({
+        })
+        binary_sha256 ({
+        })
+        break
+      when '7'
+        source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-21.tar.xz'
+        source_sha256 '3b2783a845279f76a4250f2c3caf04af22766e2d5f084bde589ebd73de6a0019'
+        binary_url ({
+        })
+        binary_sha256 ({
+        })
+        break
+      when '0'
+        abort
+        break
+      else
+        puts "Please select from one of the options or enter 0 to cancel."
+      end
+    end
+  end
+
+  depends_on 'lcms'
+  depends_on 'lzma'
+  depends_on 'libjpeg'
+  depends_on 'libpng'
+  depends_on 'libtiff'
+  depends_on 'freetype'
+  depends_on 'pango'
+  depends_on 'zlibpkg'
 
   def self.build
     system './configure',
            "--prefix=#{CREW_PREFIX}",
            "--libdir=#{CREW_LIB_PREFIX}",
-           '--without-python',
-           '--disable-dependency-tracking'
+           '--disable-dependency-tracking',
+           '--without-x'
     system 'make'
   end
 
