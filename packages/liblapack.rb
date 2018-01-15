7304fabacd170229fafc12ea716a9c53d79cf26b
lovedheart <lovedheart@163.com>
2018-01-15 12:05:44 -0800

    Create liblapack.rb
    LAPACK is a linear algebra package.

---
+++ packages/liblapack.rb
@@ -0,0 +1,26 @@
+require 'package'
+
+class Liblapack < Package                 # The first character of the class name must be upper case
+  description 'Lapack is a linear algebra package.'
+  homepage 'http://www.netlib.org/lapack/#_lapack_version_3_8_0'
+  version '3.8.0'
+  source_url 'http://www.netlib.org/lapack/lapack-3.8.0.tar.gz'
+  source_sha256 'deb22cc4a6120bff72621155a9917f485f96ef8319ac074a7afbc68aab88bcf6'   # Use the command 
+"sha256sum"
+
+  depends_on 'cmake' => :build
+
+  def self.build
+    Dir.mkdir 'build'
+    Dir.chdir 'build' do
+      system "cmake .."   # recommended for default cmake setting
+      system "make"
+    end
+  end
+
+  def self.install
+    Dir.chdir 'build' do
+      system "make install" # recommended for default installation directory
+    end
+  end  
+end
