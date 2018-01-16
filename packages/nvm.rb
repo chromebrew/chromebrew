826e910b9015a8d2c62ade87c95509ec24cdf093
Ed Reel <edreel@gmail.com>
2018-01-15 22:25:49 -0600

    Update nvm from 0.33.2 to 0.33.8
    Update postinstall message
    
    Simplify install

--- packages/nvm.rb
+++ packages/nvm.rb
@@ -3,23 +3,26 @@ require 'package'
 class Nvm < Package
   description 'Node Version Manager - Simple bash script to manage multiple active node.js versions.'
   homepage 'https://github.com/creationix/nvm'
-  version '0.33.2'
-  source_url 'https://github.com/creationix/nvm/archive/v0.33.2.tar.gz'
-  source_sha256 '2ad455a0752d5bae49ecff38a8a7778cc734c2d0ece9942dfdd164c2f01e80da'
+  version '0.33.8'
+  source_url 'https://github.com/creationix/nvm/archive/v0.33.8.tar.gz'
+  source_sha256 '59429f4bf3da7c2b7bcac06c488054dd774ae6962bdcefe249015d3590704b0b'
 
   def self.install
-    system "sed -i 's,\$HOME/.nvm,/usr/local/share/nvm,g' install.sh"
-    system "NVM_DIR=/usr/local/share/nvm && bash install.sh"
-    system "rm -rf /usr/local/share/nvm/.git*"
-    system "rm -rf /usr/local/share/nvm/test"
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local/share"
-    system "cp -r /usr/local/share/nvm #{CREW_DEST_DIR}/usr/local/share"
-    puts ""
+    system "sed -i 's,\$HOME/.nvm,#{CREW_DEST_PREFIX}/share/nvm,g' install.sh"
+    system "NVM_DIR=#{CREW_DEST_PREFIX}/share/nvm && bash install.sh"
+    system "rm -rf #{CREW_DEST_PREFIX}/share/nvm/.git*"
+    system "rm -rf #{CREW_DEST_PREFIX}/share/nvm/test"
+  end
+
+  def self.postinstall
+    puts
     puts "To complete the installation, execute:".lightblue
     puts "source ~/.bashrc".lightblue
-    puts ""
-    puts "To uninstall, in addition to 'crew remove nvm' you will".lightblue
-    puts "also need to delete any lines with NVM_DIR in ~/.bashrc.".lightblue
-    puts ""
+    puts
+    puts "To uninstall, execute the following:".lightblue
+    puts "crew remove nvm".lightblue
+    puts "rm -rf #{CREW_PREFIX}/share/nvm".lightblue
+    puts "Delete any lines with NVM_DIR in ~/.bashrc.".lightblue
+    puts
   end
 end
