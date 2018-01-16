c3b338e128d21078cfa0b9dec15f0cb8ae93f656
lovedheart <lovedheart@163.com>
2018-01-16 20:17:41 +0100

    Update liblapack.rb

--- packages/liblapack.rb
+++ packages/liblapack.rb
@@ -5,8 +5,7 @@ class Liblapack < Package                 # The first character of the class nam
   homepage 'http://www.netlib.org/lapack/#_lapack_version_3_8_0'
   version '3.8.0'
   source_url 'http://www.netlib.org/lapack/lapack-3.8.0.tar.gz'
-  source_sha256 'deb22cc4a6120bff72621155a9917f485f96ef8319ac074a7afbc68aab88bcf6'   # Use the command 
-"sha256sum"
+  source_sha256 'deb22cc4a6120bff72621155a9917f485f96ef8319ac074a7afbc68aab88bcf6'   # Use the command "sha256sum"
 
   depends_on 'cmake' => :build
 
