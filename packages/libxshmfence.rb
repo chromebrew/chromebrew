78d3a764adcf6a951129bd1c06c0e6c14cb9dd70
lovedheart <lovedheart@163.com>
2018-01-17 01:02:07 +0100

    Update libxshmfence.rb

--- packages/libxshmfence.rb
+++ packages/libxshmfence.rb
@@ -1,10 +1,10 @@
 require 'package'
 
 class Libxshmfence < Package
-  description 'libxshmfence: A library that exposes a event API on top of Linux futexes'
+  description 'A library that exposes a event API on top of Linux futexes'
   homepage 'http://t2sde.org/packages/libxshmfence.html'
   version '1.32'
-  source_url 'http://xorg.freedesktop.org/releases/individual/lib/libxshmfence-1.2.tar.bz2'
+  source_url 'https://xorg.freedesktop.org/releases/individual/lib/libxshmfence-1.2.tar.bz2'
   source_sha256 'd21b2d1fd78c1efbe1f2c16dae1cb23f8fd231dcf891465b8debe636a9054b0c'
 
   def self.build
