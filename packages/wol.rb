457ed1f86f60a916b4c77f53352682ec80b4b782
Yoav Shai <gergesh@gmail.com>
2018-01-24 16:47:29 +0200

    Fixed configure and manpage compression

--- packages/wol.rb
+++ packages/wol.rb
@@ -3,28 +3,20 @@ require 'package'
 class Wol < Package
   description 'Wake up hardware that is Magic Packet compliant'
   homepage 'http://ahh.sourceforge.net/wol/'
-  version '0.7.1-r1'
+  version '0.7.1'
   source_url 'https://downloads.sourceforge.net/ahh/wol-0.7.1.tar.gz'
   source_sha256 'e0086c9b9811df2bdf763ec9016dfb1bcb7dba9fa6d7858725b0929069a12622'
 
   depends_on 'compressdoc' => :build
 
   def self.build
-     system"./configure \
-            --prefix=#{CREW_PREFIX} \
-            --enable-gui=no \
-            --with-features=huge \
-            --without-x \
-            --disable-nls \
-            --enable-multibyte \
-            --with-tlib=ncurses \
-            --enable-perlinterp \
-            --disable-selinux"
+    system "./configure", "--prefix=#{CREW_PREFIX}", "--mandir #{CREW_PREFIX}/share/man"
     system "make"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "MANDIR=#{CREW_DEST_PREFIX}/share/man/man1", "install"
+    system "compressdoc --gzip -9 #{CREW_DEST_PREFIX}/share/man/man1"
   end
 
 end
