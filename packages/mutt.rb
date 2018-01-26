2691ce91dd82673247b3c65edccdff3c49e23ac8
Ed Reel <edreel@gmail.com>
2018-01-25 22:31:56 -0600

    Add pre-built binaries for fish, go, libcyrussasl, mutt and wol

--- packages/mutt.rb
+++ packages/mutt.rb
@@ -8,8 +8,16 @@ class Mutt < Package
   source_sha256 'a2e152a352bbf02d222d54074199d9c53821c19f700c4cb85f78fa85faed7896'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mutt-1.9.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mutt-1.9.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mutt-1.9.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mutt-1.9.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '0acaa6fa2863afa8da56a61ba99d365e2bdd73dc56e9801ee9b6e9338d33e2a2',
+     armv7l: '0acaa6fa2863afa8da56a61ba99d365e2bdd73dc56e9801ee9b6e9338d33e2a2',
+       i686: '97e12bc88aff256ea46c6389c69a5cf2623908c423ca43f08d6a905c0cb50391',
+     x86_64: 'e8ca9e36db89e76471118d9620df06e9f6eac43c20eefd01810a98ca80149583',
   })
 
   depends_on 'gdbm'
