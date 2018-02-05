d9e5aa8949a1ed7c86e4a7cf931208122657ebc7
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2018-02-05 18:45:30 -0500

    Update gdb.rb

--- packages/gdb.rb
+++ packages/gdb.rb
@@ -3,9 +3,9 @@ require 'package'
 class Gdb < Package
   description 'GDB, the GNU Project debugger, allows you to see what is going on \'inside\' another program while it executes -- or what another program was doing at the moment it crashed.'
   homepage 'https://www.gnu.org/software/gdb/'
-  version '8.0.1'
-  source_url 'https://ftp.gnu.org/gnu/gdb/gdb-8.0.1.tar.xz'
-  source_sha256 '3dbd5f93e36ba2815ad0efab030dcd0c7b211d7b353a40a53f4c02d7d56295e3'
+  version '8.1'
+  source_url 'https://ftpmirror.gnu.org/gdb/gdb-8.1.tar.xz'
+  source_sha256 'af61a0263858e69c5dce51eab26662ff3d2ad9aa68da9583e8143b5426be4b34'
 
   binary_url ({
   })
