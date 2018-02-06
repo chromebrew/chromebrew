0f7ee0ab01fed792233937a08cae20d8fc445f69
lovedheart <lovedheart@163.com>
2018-02-06 19:11:30 +0100

    Update libxi.rb

--- packages/libxi.rb
+++ packages/libxi.rb
@@ -7,7 +7,7 @@ class Libxi < Package
   source_url 'https://github.com/mirror/libXi/archive/libXi-1.7.9.tar.gz'
   source_sha256 'e3bc48654d4c21ac37592e8b41c87a5de73872a243e7b0fb39ebd565be5b943d'
 
-
+  depends_on 'automake' => :build
   depends_on 'libx11'
 
   def self.build
