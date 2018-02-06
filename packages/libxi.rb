20ea201a17540da952b661dc075f0625f9f097ff
lovedheart <lovedheart@163.com>
2018-02-06 18:20:07 +0100

    Create libxi.rb
    Dependency for building gtk

---
+++ packages/libxi.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Libxi < Package
+  description 'X.org libXi Client library for XInput'
+  homepage 'https://x.org'
+  version '1.7.9'
+  source_url 'https://github.com/mirror/libXi/archive/libXi-1.7.9.tar.gz'
+  source_sha256 'e3bc48654d4c21ac37592e8b41c87a5de73872a243e7b0fb39ebd565be5b943d'
+
+
+  depends_on 'libx11'
+
+  def self.build
+    system "./autogen.sh"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
