34f7af1b67a8b58298294dfd060b9163ecf1f6f0
lovedheart <lovedheart@163.com>
2018-02-06 19:09:01 +0100

    Update at_spi2_atk.rb

--- packages/at_spi2_atk.rb
+++ packages/at_spi2_atk.rb
@@ -7,7 +7,7 @@ class At_spi2_atk < Package
   source_url 'https://github.com/GNOME/at-spi2-atk/archive/AT_SPI2_ATK_2_26_1.tar.gz'
   source_sha256 '45fc52f794c785d54d6e2689f51ade043c96ea0263f6113c1ce26df1ef26af5b'
   
-  depends_on 'automake'
+  depends_on 'automake' => :build
   depends_on 'at_spi2_core'  
 
   def self.build
