7ca4b797ed72f97c09c4a7987739ae41198f00ef
lovedheart <lovedheart@163.com>
2018-02-06 11:13:07 +0100

    Create gdk_pixbuf.rb
    Dependency for building gtk

---
+++ packages/gdk_pixbuf.rb
@@ -0,0 +1,23 @@
+require 'package'
+
+class Gdk_pixbuf < Package
+  description 'GdkPixbuf is a library for image loading and manipulation.'
+  homepage 'https://developer.gnome.org/gdk-pixbuf'
+  version '2.36.7'
+  source_url 'https://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/2.36/gdk-pixbuf-2.36.7.tar.xz'
+  source_sha256 '1b6e5eef09d98f05f383014ecd3503e25dfb03d7e5b5f5904e5a65b049a6a4d8'
+
+  depends_on 'pango'
+  depends_on 'gobject_introspection'
+  depends_on 'libtiff'
+  depends_on 'libjpeg'
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
