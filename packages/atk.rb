beff37072be47d43ab3961be71224d4be11ef8db
lovedheart <lovedheart@163.com>
2018-02-07 01:40:02 +0100

    Update atk.rb
    Fix dependencies and installation error

--- packages/atk.rb
+++ packages/atk.rb
@@ -7,6 +7,10 @@ class Atk < Package
   source_url 'https://download.gnome.org/sources/atk/2.26/atk-2.26.1.tar.xz'
   source_sha256 'ef00ff6b83851dddc8db38b4d9faeffb99572ba150b0664ee02e46f015ea97cb'
 
+  depends_on 'gobject_introspection'
+  depends_on 'diffutils' => :build
+  depends_on 'perl' => :build
+  depends_on 'python27' => :build
 
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
@@ -14,6 +18,8 @@ class Atk < Package
   end
 
   def self.install
+    system "pip install six"  # ensure correction installation
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "pip uninstall --yes six"
   end
 end
