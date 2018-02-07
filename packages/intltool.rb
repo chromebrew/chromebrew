2e3f8d3e39935bf2c03f1559b1c8e673e7b35b4d
lovedheart <lovedheart@163.com>
2018-02-07 23:29:37 +0100

    Update intltool.rb
    Bump version

--- packages/intltool.rb
+++ packages/intltool.rb
@@ -3,7 +3,7 @@ require 'package'
 class Intltool < Package
   description 'intltool is a set of tools to centralize translation of many different file formats using GNU gettext-compatible PO files.'
   homepage 'https://freedesktop.org/wiki/Software/intltool/'
-  version '0.51.0'
+  version '0.51.0-1'
   source_url 'https://launchpad.net/intltool/trunk/0.51.0/+download/intltool-0.51.0.tar.gz'
   source_sha256 '67c74d94196b153b774ab9f89b2fa6c6ba79352407037c8c14d5aeb334e959cd'
 
