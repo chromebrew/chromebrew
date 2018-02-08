4701c7ce607581e6aff0fbe9b2257cecca33eebe
lovedheart <lovedheart@163.com>
2018-02-08 15:09:00 +0100

    Update buildessential.rb
    Add more automake, libtool and intltool into buildessential

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,7 +3,7 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage ''
-  version '1.0'
+  version '1.0-1'
 
   is_fake
 
@@ -11,4 +11,9 @@ class Buildessential < Package
   depends_on 'linuxheaders'
   depends_on 'make'
   depends_on 'pkgconfig'
+  
+  depends_on 'automake' #
+  depends_on 'libtool'  #
+  depends_on 'intltool' # These three are mostly required for "./autogen.sh"
+  
 end
