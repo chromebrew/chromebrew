eb6dee0dda037448a3a1f5f1eb96fd0f74a9a146
lovedheart <lovedheart@163.com>
2018-02-09 22:57:37 +0100

    Create perl_text_wrapi18n.rb (#1811)
    * Create perl_text_wrapi18n.rb
    
    * Update perl_text_wrapi18n.rb

---
+++ packages/perl_text_wrapi18n.rb
@@ -0,0 +1,32 @@
+require 'package'
+
+class Perl_text_wrapi18n < Package
+  description 'Text::WrapI18N - Line wrapping module with support for multibyte, fullwidth, and combining characters and languages without whitespaces between words.'
+  homepage 'https://metacpan.org/pod/Text::WrapI18N'
+  version '0.06'
+  source_url 'https://cpan.metacpan.org/authors/id/K/KU/KUBOTA/Text-WrapI18N-0.06.tar.gz'
+  source_sha256 '4bd29a17f0c2c792d12c1005b3c276f2ab0fae39c00859ae1741d7941846a488'
+
+  depends_on 'perl'
+
+  def self.build
+  end
+
+  def self.install
+    # install files to build directory
+    system 'cpanm', '-l', "build", '--self-contained', '.'
+
+    # install lib
+    libdir = `perl -e 'require Config; print $Config::Config{'"'installsitelib'"'};'`
+    system "mkdir -p #{CREW_DEST_DIR}#{libdir}"
+    system "(cd build/lib/perl5; tar cf - .) | (cd #{CREW_DEST_DIR}#{libdir}; tar xfp -)"
+
+    # install man
+    mandir = "#{CREW_PREFIX}/share/man"
+    system "mkdir -p #{CREW_DEST_DIR}#{mandir}"
+    system "(cd build/man; tar cf - .) | (cd #{CREW_DEST_DIR}#{mandir}; tar xfp -)"
+  end
+
+  def self.check
+  end
+end
