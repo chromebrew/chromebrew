3d35a7ae0197c0ba3e3d2b2243160e668a26d6d8
lovedheart <lovedheart@163.com>
2018-02-09 21:12:52 +0100

    Update graphviz.rb
    Fix it

--- packages/graphviz.rb
+++ packages/graphviz.rb
@@ -4,8 +4,8 @@ class Graphviz < Package
   description 'Graphviz is open source graph visualization software.'
   homepage 'https://www.graphviz.org/'
   version '2.40.1'
-  source_url 'https://graphviz.gitlab.io/pub/graphviz/stable/SOURCES/graphviz.tar.gz'
-  source_sha256 'ca5218fade0204d59947126c38439f432853543b0818d9d728c589dfe7f3a421'
+  source_url 'https://gitlab.com/graphviz/graphviz/repository/67cd2e5121379a38e0801cc05cce5033f8a2a609/archive.tar.bz2'
+  source_sha256 '8dddc80b4194b17c1a5bc1cc7b5e001e9a3ec27272287dc16f278c736a29a9b9'
 
   depends_on 'fontconfig'
   depends_on 'pango'
@@ -15,6 +15,7 @@ class Graphviz < Package
   depends_on 'poppler'
 
   def self.build
+    system "./autogen.sh"
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
