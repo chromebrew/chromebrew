8b6116fadf657898dddcc5b3e823022d1764214e
lovedheart <lovedheart@163.com>
2018-02-09 20:01:12 +0100

    Update libsigcplucpluc.rb
    Correct information

--- packages/libsigcplucpluc.rb
+++ packages/libsigcplucpluc.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Libsigcplusplus < Package
-  description 'http://libsigc.sourceforge.net/'
-  homepage 'libsigc++ implements a typesafe callback system for standard C++.'
+  description 'libsigc++ implements a typesafe callback system for standard C++.'
+  homepage 'http://libsigc.sourceforge.net/'
   version '2.10.0'
   source_url 'http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.10/libsigc++-2.10.0.tar.xz'
   source_sha256 'f843d6346260bfcb4426259e314512b99e296e8ca241d771d21ac64f28298d81'
