6ba4eee1049d410522be51912bee7a05c798ee64
lovedheart <lovedheart@163.com>
2018-02-09 19:17:18 +0100

    Update libxrender.rb

--- packages/libxrender.rb
+++ packages/libxrender.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Libxrender < Package
   description 'X Rendering Extension client library.'
-  homepage 'www.x.org'
+  homepage 'http://www.x.org'
   version '0.9.10'
   source_url 'https://www.x.org/releases/individual/lib/libXrender-0.9.10.tar.gz'
   source_sha256 '770527cce42500790433df84ec3521e8bf095dfe5079454a92236494ab296adf'
