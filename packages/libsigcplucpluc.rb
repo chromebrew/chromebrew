85dd24cc1a7c6860595d3bd1f5f4e9d77d61d35c
lovedheart <lovedheart@163.com>
2018-02-09 20:45:32 +0100

    Create libsigcplucpluc.rb (#1804)
    * Create libsigcplucpluc.rb
    
    * Update libsigcplucpluc.rb
    
    Correct information

---
+++ packages/libsigcplucpluc.rb
@@ -0,0 +1,20 @@
+require 'package'
+
+class Libsigcplusplus < Package
+  description 'libsigc++ implements a typesafe callback system for standard C++.'
+  homepage 'http://libsigc.sourceforge.net/'
+  version '2.10.0'
+  source_url 'http://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.10/libsigc++-2.10.0.tar.xz'
+  source_sha256 'f843d6346260bfcb4426259e314512b99e296e8ca241d771d21ac64f28298d81'
+
+  depends_on 'pkgconfig' => :build
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
