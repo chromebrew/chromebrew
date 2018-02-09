ed8be4c22b5757443d12dbc976eb6a29fe14c460
lovedheart <lovedheart@163.com>
2018-02-09 13:51:44 +0100

    Update xcb_util_renderutil.rb

--- packages/xcb_util_renderutil.rb
+++ packages/xcb_util_renderutil.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Xcb_util_renderutil < Package
   description 'The xcb-util-renderutil package provides additional extensions to the XCB library'
-  homepage 'http://www.linuxfromscratch.org/blfs/view/cvs/x/xcb-util-renderutil.html'
+  homepage 'https://xcb.freedesktop.org/'
   version '0.3.9'
   source_url 'https://xcb.freedesktop.org/dist/xcb-util-renderutil-0.3.9.tar.bz2'
   source_sha256 'c6e97e48fb1286d6394dddb1c1732f00227c70bd1bedb7d1acabefdd340bea5b'
