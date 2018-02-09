ba19e43097d04c158aa4ce038a2b65f5eb20ee92
lovedheart <lovedheart@163.com>
2018-02-09 19:09:02 +0100

    Create libxrender.rb
    Graphviz needs this library to compile.

---
+++ packages/libxrender.rb
@@ -0,0 +1,20 @@
+require 'package'
+
+class Libxrender < Package
+  description 'X Rendering Extension client library.'
+  homepage 'www.x.org'
+  version '0.9.10'
+  source_url 'https://www.x.org/releases/individual/lib/libXrender-0.9.10.tar.gz'
+  source_sha256 '770527cce42500790433df84ec3521e8bf095dfe5079454a92236494ab296adf'
+
+  depends_on 'pkgconfig' => :build
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
