ac89a3c1a6e18ae426b596ab11038264775f3aa9
lovedheart <lovedheart@163.com>
2018-02-09 21:51:21 +0100

    Create perl_locale_gettext.rb

---
+++ packages/perl_locale_gettext.rb
@@ -0,0 +1,33 @@
+
+require 'package'
+
+class Perl_locale_gettext < Package
+  description 'Locale::gettext - message handling functions'
+  homepage 'https://metacpan.org/pod/Locale::gettext'
+  version '1.07'
+  source_url 'https://cpan.metacpan.org/authors/id/P/PV/PVANDRY/gettext-1.07.tar.gz'
+  source_sha256 '909d47954697e7c04218f972915b787bd1244d75e3bd01620bc167d5bbc49c15'
+
+  depends_on 'perl'
+
+  def self.build
+  end
+
+  def self.install
+    # install files to build directory
+    system 'cpanm', '-l', "build", '--self-contained', '.'
+
+    # install lib
+    libdir = `perl -e 'require Config; print $Config::Config{'"'installsitelib'"'};'`
+    system "mkdir -p #{CREW_DEST_DIR}#{libdir}"
+    system "(cd build/lib/perl5; tar cf - .) | (cd #{CREW_DEST_DIR}#{libdir}; tar xfp -)"
+
+    # install man
+    mandir = "#{CREW_PREFIX}/share/man"
+    system "mkdir -p #{CREW_DEST_DIR}#{mandir}"
+    system "(cd build/man; tar cf - .) | (cd #{CREW_DEST_DIR}#{mandir}; tar xfp -)"
+  end
+
+  def self.check
+  end
+end
