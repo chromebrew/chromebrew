69bd3bff83c3654f91171614e6905c8d91f0ce5c
lovedheart <lovedheart@163.com>
2018-02-10 21:00:19 +0100

    Update buildessential.rb

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -11,4 +11,20 @@ class Buildessential < Package
   depends_on 'linuxheaders'
   depends_on 'make'
   depends_on 'pkgconfig'
+
+  # typically required library to compile source code using "./autogen.sh"
+  depends_on 'automake' 
+  depends_on 'libtool'  
+  depends_on 'intltool' 
+  depends_on 'patch'
+  depends_on 'diffutils'
+  depends_on 'bison'
+  depends_on 'flex'
+  depends_on 'util_macros'  
+  
+  # xorg protocols headers
+  depends_on 'xorg_proto'
+  
+  
+  
 end
