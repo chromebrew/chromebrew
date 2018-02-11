61233edbf93bca873fb8bbd97664f2c160580500
Thomas Blaschke <tblaschke@users.noreply.github.com>
2018-02-11 17:15:58 +0100

    Update jsonc.rb
    Fix to build and install Jsonc to /usr/lib64/

--- packages/jsonc.rb
+++ packages/jsonc.rb
@@ -7,6 +7,8 @@ class Jsonc < Package
   source_url 'https://s3.amazonaws.com/json-c_releases/releases/json-c-0.12.1-nodoc.tar.gz'
   source_sha256 '5a617da9aade997938197ef0f8aabd7f97b670c216dc173977e1d56eef9e1291'
 
+  depends_on "autoconf" => :build
+  
   binary_url ({
     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/jsonc-0.12.1-nodoc-chromeos-armv7l.tar.xz',
      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/jsonc-0.12.1-nodoc-chromeos-armv7l.tar.xz',
@@ -19,9 +21,9 @@ class Jsonc < Package
        i686: 'c6f53630a47e62238fdc973e6b9ea7c0a0b0346cb320a9ea19d5e72b30e7c6d0',
      x86_64: 'fdd9d9e5d263fef32972f841cc92e77be61a6df5d175e8ae4033c0df62737ba9',
   })
-
+  
   def self.build
-    system "./configure --prefix=/usr/local"
+    system "./configure", "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
