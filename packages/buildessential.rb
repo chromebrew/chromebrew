93108b0d8f5bf5e485c90162196b0191641b125c
lovedheart <lovedheart@163.com>
2018-02-12 01:23:54 +0100

    Update buildessential.rb

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -22,11 +22,15 @@ class Buildessential < Package
   depends_on 'flex'
   depends_on 'util_macros'  
   depends_on 'gettext'
+  depends_on 'wget'     # in some case, some patches might be required and can be downloaded using wget
  
   # xorg protocols headers
-  depends_on 'xorg_proto'
+  #depends_on 'xorg_proto'
  
   # maybe meson build system ?   
   # depends_on 'meson'        ## With python binary fixed, this chould be included here.
+  
+  # perl module build ?
+  # depends_on 'perl_module_build'
   
 end
