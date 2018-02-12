20b924bd5b2b357e31804f49de2fa423706e9b0b
lovedheart <lovedheart@163.com>
2018-02-12 04:58:57 +0100

    Update xorg libraries list (#1868)

--- packages/xorg_lib.rb
+++ packages/xorg_lib.rb
@@ -3,14 +3,30 @@ require 'package'
 class Xorg_lib < Package
   description 'A collection of xorg libraries.'
   homepage ''
-  version '0.1'  
+  version '0.1-0'  
   
   is_fake
  
+  depends_on 'libxtrans'
+  depends_on 'libx11'
+  depends_on 'libxext'
+  depends_on 'libfontenc'
+  depends_on 'libxinerama'
+  depends_on 'libxdamage'
+  depends_on 'libxtst'
+  depends_on 'libpciaccess'
+  depends_on 'libxkbfile'
+  depends_on 'libxshmfence'
+  depends_on 'libxi'
+  depends_on 'libxcursor'
+  depends_on 'libxrender'
+  depends_on 'libxfixes'
+  
   # new
   depends_on 'libdmx'
   depends_on 'libfs'
   depends_on 'libice'
+  depends_on 'libsm'
   depends_on 'libxaw'
   depends_on 'libxfont2'
   depends_on 'libxft'
@@ -23,5 +39,7 @@ class Xorg_lib < Package
   depends_on 'libxvmc'
   depends_on 'libxxf86dga'
   depends_on 'libxxf86vm'
+  depends_on 'libxcomposite'
+  depends_on 'libxscrnsaver'
   
 end
