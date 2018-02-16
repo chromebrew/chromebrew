a7b20523e259189f3e16d55a2b0bd52463715c47
Ed Reel <edreel@gmail.com>
2018-02-16 01:21:56 -0600

    Add chromebeer package

---
+++ packages/chromebeer.rb
@@ -0,0 +1,26 @@
+require 'package'
+
+class Chromebeer < Package
+  description 'An opinionated bundle of "essential" Chromebrew packages.'
+  homepage ''
+  version '1.0'
+
+  is_fake
+
+  depends_on 'ascii'
+  depends_on 'bz2'
+  depends_on 'curl'
+  depends_on 'filecmd'
+  depends_on 'gawk'
+  depends_on 'gzsize'
+  depends_on 'lshw'
+  depends_on 'mediainfo'
+  depends_on 'mlocate'
+  depends_on 'psmisc'
+  depends_on 'rsync'
+  depends_on 'sysstat'
+  depends_on 'tree'
+  depends_on 'unzip'
+  depends_on 'zip'
+  depends_on 'manpages'
+end
