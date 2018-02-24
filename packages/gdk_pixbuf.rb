623d50de6a6f3344835741df319acb1daebd6157
lovedheart <lovedheart@163.com>
2018-02-24 22:22:38 +0100

    Update gdk_pixbuf.rb
    Add x11 support and missing files caused by installation using a "DESTDIR" method

--- packages/gdk_pixbuf.rb
+++ packages/gdk_pixbuf.rb
@@ -3,21 +3,13 @@ require 'package'
 class Gdk_pixbuf < Package
   description 'GdkPixbuf is a library for image loading and manipulation.'
   homepage 'https://developer.gnome.org/gdk-pixbuf'
-  version '2.36.7'
+  version '2.36.7-0'
   source_url 'https://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/2.36/gdk-pixbuf-2.36.7.tar.xz'
   source_sha256 '1b6e5eef09d98f05f383014ecd3503e25dfb03d7e5b5f5904e5a65b049a6a4d8'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '5e1b4efd43d724cbf2a525e193cb104ec09f45007c8d99cd0c113060013d5d36',
-     armv7l: '5e1b4efd43d724cbf2a525e193cb104ec09f45007c8d99cd0c113060013d5d36',
-       i686: 'f51aece1494cdf21d63c609dc5be2055acb8afd5051ed7b149c286fc06e95a56',
-     x86_64: '7eab51523a55ebfcd8d87edf83554efbf04078eb8cfeafc1bb269081ce01f426',
   })
 
   depends_on 'pango'
@@ -27,7 +19,7 @@ class Gdk_pixbuf < Package
   depends_on 'python27'
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} --with-x11"
     system "make"
   end
 
@@ -35,5 +27,10 @@ class Gdk_pixbuf < Package
     system "pip install six" # for installation process
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
     system "pip uninstall --yes six"
+  end
+  
+  def self.postinstall
+    # If the package is installed using a "DESTDIR" method, an important file was not installed and should be copied or generated.
+    system "sudo gdk-pixbuf-query-loaders --update-cache"
   end
 end
