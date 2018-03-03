97601099d86276733690eb115fc3f21c064c76b3
Ed Reel <edreel@gmail.com>
2018-03-02 21:19:52 -0600

    Update imagemagick packages

--- packages/imagemagick.rb
+++ packages/imagemagick.rb
@@ -3,30 +3,30 @@ require 'package'
 class Imagemagick < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '6.9.9-34-7.0.7-22'
+  version '6.9.9-36-7.0.7-24'
 
   if ARGV[0] == 'install'
     imver = `stream -version 2> /dev/null | head -1 | cut -d' ' -f3`.chomp
     abort "ImageMagick version #{imver} already installed.".lightgreen unless "#{imver}" == ""
     puts
     puts "Enter the ImageMagick version to install:"
-    puts "6 = ImageMagick 6.9.9-34"
-    puts "7 = ImageMagick 7.0.7-22"
+    puts "6 = ImageMagick 6.9.9-36"
+    puts "7 = ImageMagick 7.0.7-24"
     puts "0 = Cancel"
 
     while version = STDIN.gets.chomp
       case version
       when '6'
-        source_url 'https://www.imagemagick.org/download/releases/ImageMagick-6.9.9-34.tar.xz'
-        source_sha256 'b37d8ea71a367f0d2267e8441b470cc6f2eaac0ce94837dcc87b011c55bf212c'
+        source_url 'https://www.imagemagick.org/download/releases/ImageMagick-6.9.9-36.tar.xz'
+        source_sha256 'a86dc03ef8981e2dec83657fce3b4976347a537d785cc8e08af85af22a1b1bda'
         binary_url ({
         })
         binary_sha256 ({
         })
         break
       when '7'
-        source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-22.tar.xz'
-        source_sha256 '49de9e08ea255a1f939158d85d50dfa29285bccbdcb7fee0fe4309061d438489'
+        source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-24.tar.xz'
+        source_sha256 '2f83f8a1b7725e9d96a6f4ddf8dd2e70d44bc039bdc32f056805ab4d0f7485fb'
         binary_url ({
         })
         binary_sha256 ({
@@ -41,15 +41,18 @@ class Imagemagick < Package
     end
   end
 
+  depends_on 'freetype'
+  depends_on 'ghostscript'
   depends_on 'lcms'
   depends_on 'lzma'
   depends_on 'libjpeg'
   depends_on 'libpng'
   depends_on 'libtiff'
   depends_on 'libwebp'
-  depends_on 'freetype'
+  depends_on 'msttcorefonts'
   depends_on 'openjpeg'
   depends_on 'pango'
+  depends_on 'perl'
   depends_on 'python27'
   depends_on 'zlibpkg'
 
@@ -58,7 +61,6 @@ class Imagemagick < Package
            "--prefix=#{CREW_PREFIX}",
            "--libdir=#{CREW_LIB_PREFIX}",
            '--disable-dependency-tracking',
-           '--with-python',
            '--without-x'
     system 'make'
   end
--- packages/imagemagick7.rb
+++ packages/imagemagick7.rb
@@ -3,24 +3,27 @@ require 'package'
 class Imagemagick7 < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '7.0.7-22'
-  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-22.tar.xz'
-  source_sha256 '49de9e08ea255a1f939158d85d50dfa29285bccbdcb7fee0fe4309061d438489'
+  version '7.0.7-24'
+  source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-24.tar.xz'
+  source_sha256 '2f83f8a1b7725e9d96a6f4ddf8dd2e70d44bc039bdc32f056805ab4d0f7485fb'
 
   binary_url ({
   })
   binary_sha256 ({
   })
 
+  depends_on 'freetype'
+  depends_on 'ghostscript'
   depends_on 'lcms'
   depends_on 'lzma'
   depends_on 'libjpeg'
   depends_on 'libpng'
   depends_on 'libtiff'
   depends_on 'libwebp'
-  depends_on 'freetype'
+  depends_on 'msttcorefonts'
   depends_on 'openjpeg'
   depends_on 'pango'
+  depends_on 'perl'
   depends_on 'python27'
   depends_on 'zlibpkg'
 
@@ -29,7 +32,6 @@ class Imagemagick7 < Package
            "--prefix=#{CREW_PREFIX}",
            "--libdir=#{CREW_LIB_PREFIX}",
            '--disable-dependency-tracking',
-           '--with-python',
            '--without-x'
     system 'make'
   end
