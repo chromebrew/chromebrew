fe517ae4e326aa8dbc0f382962af7be6d773ad05
lovedheart <lovedheart@163.com>
2018-03-09 21:02:42 +0100

    Create libevdev.rb (#1940)
    dependency of weston

---
+++ packages/libevdev.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Libevdev < Package
+  description 'libevdev is a wrapper library for evdev devices.'
+  homepage 'https://www.freedesktop.org/wiki/Software/libevdev'
+  version '1.5.9'
+  source_url 'https://www.freedesktop.org/software/libevdev/libevdev-1.5.9.tar.xz'
+  source_sha256 'e1663751443bed9d3e76a4fe2caf6fa866a79705d91cacad815c04e706198a75'
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
