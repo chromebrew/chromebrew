c72c421acc1aaf4c396d3235fe8606cb63fda466
lovedheart <lovedheart@163.com>
2018-03-10 12:30:45 +0100

    Update doxygen.rb

--- packages/doxygen.rb
+++ packages/doxygen.rb
@@ -8,16 +8,10 @@ class Doxygen < Package
   source_sha256 '18bc3790b4d5f4d57cb8ee0a77dd63a52518f3f70d7fdff868a7ce7961a6edc3'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-x86_64.tar.xz',
+
   })
   binary_sha256 ({
-    aarch64: 'a787a8e520e656ff86284638cb2750a0a96ba457f435b92e79492b1d44a57f19',
-     armv7l: 'a787a8e520e656ff86284638cb2750a0a96ba457f435b92e79492b1d44a57f19',
-       i686: 'd93402dc56f6758a68cf9d13f297322e62b758c5b77378064e52c44ced929dd5',
-     x86_64: '6373172fab94d0834508c943249aefd8bc364347f67a8c4a0aa15b9d05ef33b4',
+    
   })
 
   depends_on 'cmake' => :build
