31f2076fc46e1775e3fc8253d69c9bf2a6720cb0
lovedheart <lovedheart@163.com>
2018-03-10 18:45:24 +0100

    Upgrade wayland_protocols to 1.13 (#1954)
    * Upgrade wayland_protocols to 1.13
    
    Required for weston build
    
    * Update wayland_protocols.rb
    
    * Update wayland_protocols.rb

--- packages/wayland_protocols.rb
+++ packages/wayland_protocols.rb
@@ -3,27 +3,21 @@ require 'package'
 class Wayland_protocols < Package
   description 'Wayland is a protocol for a compositor to talk to its clients'
   homepage 'https://wayland.freedesktop.org'
-  version '1.9'
-  source_url 'https://wayland.freedesktop.org/releases/wayland-protocols-1.9.tar.xz'
-  source_sha256 '666b72de30ca3b70c2b54ccc9e8114cb520e76db224c816b5e23501099174f75'
+  version '1.13'
+  source_url 'https://wayland.freedesktop.org/releases/wayland-protocols-1.13.tar.xz'
+  source_sha256 '0758bc8008d5332f431b2a84fea7de64d971ce270ed208206a098ff2ebc68f38'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.9-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.9-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.9-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.9-chromeos-x86_64.tar.xz',
+
   })
   binary_sha256 ({
-    aarch64: 'b1c7bf17d30ff2335dafdbd2c57bb8769bfe54f8a2342d70af06fbf0a2adc51f',
-     armv7l: 'b1c7bf17d30ff2335dafdbd2c57bb8769bfe54f8a2342d70af06fbf0a2adc51f',
-       i686: '11d26d2ca2b5e960b71e44573ebeba6c601e3e9e3d0e4614972c66cd8316145e',
-     x86_64: '5c1f8913e2ff2ee59dc8080b8c076383caf693890b20bbf08113d08f7fb6199e',
+
   })
 
   depends_on 'libwayland'
   
   def self.build
-    system "./configure"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
