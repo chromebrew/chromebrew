fadda245fab296532bcf85c3df30206a5b7386c4
lovedheart <lovedheart@163.com>
2018-03-10 12:30:18 +0100

    Update doxygen.rb

--- packages/doxygen.rb
+++ packages/doxygen.rb
@@ -3,7 +3,7 @@ require 'package'
 class Doxygen < Package
   description 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources'
   homepage 'http://www.stack.nl/~dimitri/doxygen/'
-  version '1.8.14'
+  version '1.8.14-0'
   source_url 'https://github.com/doxygen/doxygen/archive/Release_1_8_14.tar.gz'
   source_sha256 '18bc3790b4d5f4d57cb8ee0a77dd63a52518f3f70d7fdff868a7ce7961a6edc3'
 
