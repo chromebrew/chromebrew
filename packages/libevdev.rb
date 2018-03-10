d457a5186821e1bcfb191e7d67230e6385626d4a
lovedheart <lovedheart@163.com>
2018-03-10 13:15:39 +0100

    Fix dependencies for libevdev (#1945)
    * Fix dependencies for libevdev
    
    * Update libevdev.rb

--- packages/libevdev.rb
+++ packages/libevdev.rb
@@ -3,10 +3,13 @@ require 'package'
 class Libevdev < Package
   description 'libevdev is a wrapper library for evdev devices.'
   homepage 'https://www.freedesktop.org/wiki/Software/libevdev'
-  version '1.5.9'
+  version '1.5.9-0'
   source_url 'https://www.freedesktop.org/software/libevdev/libevdev-1.5.9.tar.xz'
   source_sha256 'e1663751443bed9d3e76a4fe2caf6fa866a79705d91cacad815c04e706198a75'
 
+  depends_on 'doxygen'
+  depends_on 'python27'
+  
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
