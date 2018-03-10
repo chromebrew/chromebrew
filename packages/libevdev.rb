2123dfc6c45a6c1bc016d27bdf5c7fe73f249518
lovedheart <lovedheart@163.com>
2018-03-10 12:06:32 +0100

    Fix dependencies for libevdev

--- packages/libevdev.rb
+++ packages/libevdev.rb
@@ -7,6 +7,9 @@ class Libevdev < Package
   source_url 'https://www.freedesktop.org/software/libevdev/libevdev-1.5.9.tar.xz'
   source_sha256 'e1663751443bed9d3e76a4fe2caf6fa866a79705d91cacad815c04e706198a75'
 
+  depends_on 'doxygen'
+  depends_on 'python27'
+  
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
