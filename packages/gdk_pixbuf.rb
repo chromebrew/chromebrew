9cfe468c3053f4a444cc19fe83cbd071b3294c27
Ed Reel <edreel@gmail.com>
2018-03-10 14:05:50 -0600

    Add pre-built binaries for packages g-w

--- packages/gdk_pixbuf.rb
+++ packages/gdk_pixbuf.rb
@@ -8,9 +8,17 @@ class Gdk_pixbuf < Package
   source_sha256 '1b6e5eef09d98f05f383014ecd3503e25dfb03d7e5b5f5904e5a65b049a6a4d8'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.36.7-1-chromeos-x86_64.tar.xz',
 
   })
   binary_sha256 ({
+    aarch64: '3baf4efbf49e731ae15e0f3650dccbf47e1128fc735b3f1dc0b82f49faaac926',
+     armv7l: '3baf4efbf49e731ae15e0f3650dccbf47e1128fc735b3f1dc0b82f49faaac926',
+       i686: 'b70469069a781d819aedb94f5c6e1a829815b76aef27926129d43bf20fe97042',
+     x86_64: '044b281179d0d781757bd131fc181169e74bf3018f4de51fa8550449ea96f19e',
 
   })
 
