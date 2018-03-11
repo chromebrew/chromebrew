ae365254cb5e5d99f8749a99a81260b4010ba54f
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-11 06:52:07 +0100

    Add files via upload

---
+++ packages/libcroco.rb
@@ -0,0 +1,30 @@
+require 'package'
+
+class Libcroco < Package
+  description 'Generic Cascading Style Sheet (CSS) parsing and manipulation toolkit. https://git.gnome.org/browse/libcroco/'
+  homepage 'http://www.linuxfromscratch.org/blfs/view/svn/general/libcroco.html'
+  version '0.6.12'
+  source_url 'http://ftp.gnome.org/pub/gnome/sources/libcroco/0.6/libcroco-0.6.12.tar.xz'
+  source_sha256 'ddc4b5546c9fb4280a5017e2707fbd4839034ed1aba5b7d4372212f34f84f860'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+  
+  depends_on 'libxml2'
+  depends_on 'gtk_doc'
+
+  def self.build
+    system "sh autogen.sh"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "pip install six"
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "pip uninstall --yes six"
+  end
+  
+end
