473313cd151e9c971203879bcf0ce475c85ba507
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-11 06:54:22 +0100

    Update gnome_common.rb

--- packages/gnome_common.rb
+++ packages/gnome_common.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Gnome_common < Package
-  description 'Common files for development of Gnome packages. https://git.gnome.org/browse/gnome-common'
-  homepage 'https://github.com/GNOME/gnome-common/releases'
+  description 'Common files for development of Gnome packages.'
+  homepage 'https://git.gnome.org/browse/gnome-common'
   version '3.18.0'
   source_url 'https://github.com/GNOME/gnome-common/archive/3.18.0.tar.gz'
   source_sha256 '8407fd8786a44c9ce47987de0906d9266492195df9251a089afaa06cc65c72d8'
