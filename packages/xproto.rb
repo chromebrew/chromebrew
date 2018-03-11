b5a263604ea5955d7d95736ac445b3f7376e2860
lovedheart <lovedheart@163.com>
2018-03-11 22:21:27 +0100

    Update xproto.rb

--- packages/xproto.rb
+++ packages/xproto.rb
@@ -3,7 +3,7 @@ require 'package'
 class Xproto < Package
   description 'The protocols for the X window system provide extended functionality for communication between a X client and the server.'
   homepage 'https://x.org'
-  version '7.0.3-0'
+  version '7.0.31-0'
   source_url 'https://www.x.org/archive/individual/proto/xproto-7.0.31.tar.gz'
   source_sha256 '6d755eaae27b45c5cc75529a12855fed5de5969b367ed05003944cf901ed43c7'
 
