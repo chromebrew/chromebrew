065d24d30e7f7236b99ee7732096c9dca1b85b62
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-11 07:04:21 +0100

    Update cairo.rb

--- packages/cairo.rb
+++ packages/cairo.rb
@@ -3,22 +3,9 @@ require 'package'
 class Cairo < Package
   description 'Cairo is a 2D graphics library with support for multiple output devices.'
   homepage 'https://www.cairographics.org'
-  version '1.14.12-1'
-  source_url 'https://www.cairographics.org/releases/cairo-1.14.12.tar.xz'
-  source_sha256 '8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.12-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.12-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.12-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cairo-1.14.12-1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '39ed1c001a51c6041b8f6e8454cc9cf2563e9d992f124d5266ca384e4932bc92',
-     armv7l: '39ed1c001a51c6041b8f6e8454cc9cf2563e9d992f124d5266ca384e4932bc92',
-       i686: '6e37fb090daa853dd24857eddfdad7f613a48efadd203f6fb147758fb68b1b33',
-     x86_64: 'b36c865cef3deb8c3c52dd5b0ac51a3a9811a12ce91fde4196d6a2cb289f3faa',
-  })
+  version '1.15.10'
+  source_url 'http://cairographics.org/snapshots/cairo-1.15.10.tar.xz'
+  source_sha256 '62ca226134cf2f1fd114bea06f8b374eb37f35d8e22487eaa54d5e9428958392'
 
   depends_on 'libpng'
   depends_on 'pixman'
