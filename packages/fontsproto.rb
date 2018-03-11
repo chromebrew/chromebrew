cc7716d71f017ed302a12b31037c4ad3c138f434
lovedheart <lovedheart@163.com>
2018-03-11 21:48:20 +0100

    Update fontsproto.rb

--- packages/fontsproto.rb
+++ packages/fontsproto.rb
@@ -3,25 +3,19 @@ require 'package'
 class Fontsproto < Package
   description 'The protocols for the X window system provide extended functionality for communication between a X client and the server.'
   homepage 'https://x.org'
-  version '2.1.3'
+  version '2.1.3-0'
   source_url 'https://www.x.org/archive/individual/proto/fontsproto-2.1.3.tar.gz'
   source_sha256 '72c44e63044b2b66f6fa112921621ecc20c71193982de4f198d9a29cda385c5e'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/fontsproto-2.1.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/fontsproto-2.1.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/fontsproto-2.1.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/fontsproto-2.1.3-chromeos-x86_64.tar.xz',
+
   })
   binary_sha256 ({
-    aarch64: 'a29922ecf12e3153d24edefa17f8db27e0067b92fc605ea9c90f5e41c7327be2',
-     armv7l: 'a29922ecf12e3153d24edefa17f8db27e0067b92fc605ea9c90f5e41c7327be2',
-       i686: '6ffb63f9690efc704a890cdb37c6b1f32c19dbcd728edd9e7b7dca88ba61e633',
-     x86_64: '17ab2f6e3163dcfcc7a442120edf94fa73443ebc89bd23fcb450511925cb3b87',
+
   })
 
   def self.build
-    system "./configure"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
   end
 
   def self.install
