ed0715cc1d972e3ccc7033ed1c5641c086f3cdc1
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 01:01:21 +0100

    Update libiconv.rb

--- packages/libiconv.rb
+++ packages/libiconv.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Libiconv < Package
-  description 'GNU charset conversion library for libc which does not implement it. https://www.gnu.org/software/libiconv/'
-  homepage 'https://ftp.gnu.org/pub/gnu/libiconv/'
+  description 'GNU charset conversion library for libc which does not implement it.'
+  homepage 'https://www.gnu.org/software/libiconv/'
   version '1.15'
   source_url 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz'
   source_sha256 'ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178'
