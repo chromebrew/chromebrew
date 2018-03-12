8eacd828803f3ff00c1b2c1acc91c7aa6f055fd8
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 19:59:28 +0100

    Add files via upload

---
+++ libgee.rb
@@ -0,0 +1,23 @@
+require 'package'
+
+class Libgee < Package
+  description 'GObject-based interfaces and classes for commonly used data structures. https://wiki.gnome.org/Projects/Libgee'
+  homepage 'https://wiki.gnome.org/Projects/Libgee/Release'
+  version '0.18.0'
+  source_url 'https://download.gnome.org/sources/libgee/0.18/libgee-0.18.0.tar.xz'
+  source_sha256 '4ad99ef937d071b4883c061df40bfe233f7649d50c354cf81235f180b4244399'
+
+  depends_on 'glib'
+  depends_on 'gobject_introspection'
+  depends_on 'vala'
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install" # the steps required to install the package
+  end
+  
+end
