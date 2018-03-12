34bf7de428f03d734689946153694d37d91de10a
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 19:45:53 +0100

    Add files via upload

---
+++ packages/dbus_glib.rb
@@ -0,0 +1,20 @@
+require 'package'
+
+class Dbus_glib < Package
+  description 'D-Bus bindings for glib. https://dbus.freedesktop.org/'
+  homepage 'https://dbus.freedesktop.org/releases/dbus/'
+  version '1.13.2'
+  source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.2.tar.gz'
+  source_sha256 '945deb349a7e2999184827c17351c1bf93c6395b9c3ade0c91cad42cb93435b1'
+
+  def self.build
+    system "sh autogen.sh"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install" # the steps required to install the package
+  end
+  
+end
