410bdeeaf2311c8f97a54b9abe33374b15960462
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 20:03:09 +0100

    Add files via upload

---
+++ packages/glib_networking.rb
@@ -0,0 +1,23 @@
+require 'package'
+
+class Glib_networking < Package
+  description 'Network-related giomodules for glib. https://git.gnome.org/browse/glib-networking/'
+  homepage 'https://download.gnome.org/sources/glib-networking/'
+  version '2.54.1'
+  source_url 'https://download.gnome.org/sources/glib-networking/2.54/glib-networking-2.54.1.tar.xz'
+  source_sha256 'eaa787b653015a0de31c928e9a17eb57b4ce23c8cf6f277afaec0d685335012f'
+
+  depends_on 'glib'
+  depends_on 'meson'
+  depends_on 'gsettings_desktop_schemas'
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install" # the steps required to install the package
+  end
+  
+end
