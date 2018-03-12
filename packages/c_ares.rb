1e263da9678f104e3caec28e8c7333f5511cf642
lovedheart <lovedheart@163.com>
2018-03-12 10:15:58 -0700

    [c_ares] Fix libdir
    #1981

--- packages/c_ares.rb
+++ packages/c_ares.rb
@@ -8,20 +8,14 @@ class C_ares < Package
   source_sha256 '03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/c_ares-1.13.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/c_ares-1.13.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/c_ares-1.13.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/c_ares-1.13.0-chromeos-x86_64.tar.xz',
+
   })
   binary_sha256 ({
-    aarch64: '750a615e4facc29c6bd14287bdd33b898e41ffaec0d58e572bd6b5f0091d7235',
-     armv7l: '750a615e4facc29c6bd14287bdd33b898e41ffaec0d58e572bd6b5f0091d7235',
-       i686: '33846c33b585cedb0f79614f0a7605417027317e1ba11b9817bd8399b310eea2',
-     x86_64: '2bbe81d4a4ca4c5ed396625f7b93ba876144fedec201d3f2093eecf718635c1e',
+
   })
 
   def self.build
-    system "./configure"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
