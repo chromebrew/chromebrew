c2b9e363d56478cc7d7579c2aa8b80efdd278827
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 01:02:13 +0100

    Update mate_calc.rb

--- packages/mate_calc.rb
+++ packages/mate_calc.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Mate_calc < Package
-  description 'Calculator for MATE. https://mate-desktop.org'
-  homepage 'https://pub.mate-desktop.org/releases/'
+  description 'Calculator for MATE.'
+  homepage 'https://mate-desktop.org'
   version '1.20'
   source_url 'https://pub.mate-desktop.org/releases/1.20/mate-calc-1.20.0.tar.xz'
   source_sha256 'e3b075f7d0a02fb72adbf78678685888dec9402cd0ae6b04200d32009651950d'
