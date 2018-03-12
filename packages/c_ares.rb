ce8bb8a21778b9feee4260076dd0deb108ae2bb8
lovedheart <lovedheart@163.com>
2018-03-12 14:40:49 -0700

    [c_ares] upgrade to 1.14.0 (#2055)

--- packages/c_ares.rb
+++ packages/c_ares.rb
@@ -3,16 +3,10 @@ require 'package'
 class C_ares < Package
   description 'c-ares is a C library for asynchronous DNS requests (including name resolves).'
   homepage 'https://c-ares.haxx.se/'
-  version '1.13.0-0'
-  source_url 'https://c-ares.haxx.se/download/c-ares-1.13.0.tar.gz'
-  source_sha256 '03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7'
+  version '1.14.0'
+  source_url 'https://c-ares.haxx.se/download/c-ares-1.14.0.tar.gz'
+  source_sha256 '45d3c1fd29263ceec2afc8ff9cd06d5f8f889636eb4e80ce3cc7f0eaf7aadc6e'
 
-  binary_url ({
-
-  })
-  binary_sha256 ({
-
-  })
 
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
