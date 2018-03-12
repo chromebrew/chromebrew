f52ca91ff5555c51821e20d21ecff741158910f3
lovedheart <lovedheart@163.com>
2018-03-12 21:11:37 +0100

    Update c_ares.rb

--- packages/c_ares.rb
+++ packages/c_ares.rb
@@ -3,7 +3,7 @@ require 'package'
 class C_ares < Package
   description 'c-ares is a C library for asynchronous DNS requests (including name resolves).'
   homepage 'https://c-ares.haxx.se/'
-  version '1.13.0'
+  version '1.13.0-0'
   source_url 'https://c-ares.haxx.se/download/c-ares-1.13.0.tar.gz'
   source_sha256 '03f708f1b14a26ab26c38abd51137640cb444d3ec72380b21b20f1a8d2861da7'
 
