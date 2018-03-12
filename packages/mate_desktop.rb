9f606917fb35b89d51c2bb9494db23013ae778e1
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 01:03:02 +0100

    Update mate_desktop.rb

--- packages/mate_desktop.rb
+++ packages/mate_desktop.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Mate_desktop < Package
-  description 'Libraries for the MATE desktop that are not part of the UI. https://mate-desktop.org/'
-  homepage 'https://pub.mate-desktop.org/releases/'
+  description 'Libraries for the MATE desktop that are not part of the UI.'
+  homepage 'https://mate-desktop.org'
   version '1.20'
   source_url 'https://pub.mate-desktop.org/releases/1.20/mate-desktop-1.20.0.tar.xz'
   source_sha256 '36d0722750ce30ba8d4b8b735bef6ad1b1b4c8f9f51257e36ec419b9b438a178'
