40e8ebe877f1a43a43e03bb41e2329c0765be101
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 20:15:44 +0100

    Update xfce4_dev_tools.rb

--- packages/xfce4_dev_tools.rb
+++ packages/xfce4_dev_tools.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Xfce4_dev_tools < Package
-  description 'A set of scripts and m4/autoconf macros that ease build system maintenance. https://www.xfce.org/'
-  homepage 'https://sourceforge.net/projects/gentoo/files/gentoo/'
+  description 'A set of scripts and m4/autoconf macros that ease build system maintenance.'
+  homepage 'https://www.xfce.org/'
   version '4.12.0'
   source_url 'https://github.com/xfce-mirror/xfce4-dev-tools/archive/xfce4-dev-tools-4.12.0.tar.gz'
   source_sha256 '50eee04bc13fedfa8f4aa4e6e329285569d3fe8b27cf1584f431ce279b59ba0b'
