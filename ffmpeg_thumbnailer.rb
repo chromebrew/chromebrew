d2c0e8f584b711561c505ed6201fe061ad9c9f54
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 19:48:01 +0100

    Add files via upload

---
+++ ffmpeg_thumbnailer.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Ffmpeg_thumbnailer < Package
+  description 'Lightweight video thumbnailer. https://github.com/dirkvdb/ffmpegthumbnailer'
+  homepage 'https://github.com/dirkvdb/ffmpegthumbnailer/releases'
+  version '2.2.0'
+  source_url 'https://github.com/dirkvdb/ffmpegthumbnailer/archive/2.2.0.tar.gz'
+  source_sha256 '8bc74755aa960531acdea8fa19ad7e1b7c806c28a068306a8c7150d278241a8f'
+
+  def self.build
+    system "cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_GIO=ON -DENABLE_THUMBNAILER=ON ."
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install" # the steps required to install the package
+  end
+  
+end
