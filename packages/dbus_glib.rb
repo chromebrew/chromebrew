1f075ede21ab91b240b2e6335422919a9d8cf7e9
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 20:17:44 +0100

    Update dbus_glib.rb

--- packages/dbus_glib.rb
+++ packages/dbus_glib.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Dbus_glib < Package
-  description 'D-Bus bindings for glib. https://dbus.freedesktop.org/'
-  homepage 'https://dbus.freedesktop.org/releases/dbus/'
+  description 'D-Bus bindings for glib.'
+  homepage 'https://dbus.freedesktop.org'
   version '1.13.2'
   source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.2.tar.gz'
   source_sha256 '945deb349a7e2999184827c17351c1bf93c6395b9c3ade0c91cad42cb93435b1'
