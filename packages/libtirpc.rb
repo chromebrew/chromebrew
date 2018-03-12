a395ca70e2920b4f9b8fb3be47e5e88281d8cf8d
lovedheart <lovedheart@163.com>
2018-03-12 09:24:40 -0700

    [libtirpc] fix libdir
    #1981

--- packages/libtirpc.rb
+++ packages/libtirpc.rb
@@ -8,22 +8,16 @@ class Libtirpc < Package
   source_sha256 '723c5ce92706cbb601a8db09110df1b4b69391643158f20ff587e20e7c5f90f5'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libtirpc-1.0.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libtirpc-1.0.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libtirpc-1.0.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libtirpc-1.0.2-chromeos-x86_64.tar.xz',
+
   })
   binary_sha256 ({
-    aarch64: '0de6310280dc7845637976ea042bc9bc608fab9569e6fed8a429db12015a0737',
-     armv7l: '0de6310280dc7845637976ea042bc9bc608fab9569e6fed8a429db12015a0737',
-       i686: '11eaa0ef23ed9fd214a4c3aa87fd4a642bf00a6d597bf177185a29e1d52749ca',
-     x86_64: '3465e356fbdb29c63383062a58e62e8f9dd8a297a3c1706f82f846636d5dc303',
+
   })
 
   depends_on 'krb5'
   
   def self.build
-    system "./configure --prefix=/usr/local"
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
