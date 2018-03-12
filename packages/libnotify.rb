de0477613c37344b104be8004cf4038ef9eb9c93
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 01:01:49 +0100

    Update libnotify.rb

--- packages/libnotify.rb
+++ packages/libnotify.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Libnotify < Package
-  description 'A library for sending desktop notifications. https://git.gnome.org/browse/libnotify'
-  homepage 'https://github.com/GNOME/libnotify/releases'
+  description 'A library for sending desktop notifications.'
+  homepage 'https://git.gnome.org/browse/libnotify'
   version '0.7.7'
   source_url 'https://github.com/GNOME/libnotify/archive/0.7.7.tar.gz'
   source_sha256 '8ff216938a47df591b454ad062465f6d91d7a83496a1ba8eb755df8952cf48f2'
