2d8e0da4dc16024fb5dd996b51a8fca1629bfb24
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-12 01:02:39 +0100

    Update mate_common.rb

--- packages/mate_common.rb
+++ packages/mate_common.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Mate_common < Package
-  description 'Common files for development of MATE packages. https://mate-desktop.org'
-  homepage 'https://pub.mate-desktop.org/releases/'
+  description 'Common files for development of MATE packages.'
+  homepage 'https://mate-desktop.org'
   version '1.20'
   source_url 'https://pub.mate-desktop.org/releases/1.20/mate-common-1.20.0.tar.xz'
   source_sha256 '616d9c319ee892f05494570fb0f7316c10f17a1f8d15d0a9a6ae38c320161a41'
