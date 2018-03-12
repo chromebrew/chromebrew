ca5525154ea867b04c5542f60b4e592755a60b8b
lovedheart <lovedheart@163.com>
2018-03-12 21:26:31 +0100

    Update dbus.rb

--- packages/dbus.rb
+++ packages/dbus.rb
@@ -7,12 +7,7 @@ class Dbus < Package
   source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.2.tar.gz'
   source_sha256 '945deb349a7e2999184827c17351c1bf93c6395b9c3ade0c91cad42cb93435b1'
 
-  binary_url ({
 
-  })
-  binary_sha256 ({
-
-  })
 
   depends_on 'expat'
 
@@ -20,7 +15,6 @@ class Dbus < Package
     system "./configure \
     --prefix=#{CREW_PREFIX} \
     --libdir=#{CREW_LIB_PREFIX} \
-    --localstatedir=#{CREW_DEST_PREFIX}/var \
     --disable-static \
     --disable-doxygen-docs \
     --disable-xml-docs"
