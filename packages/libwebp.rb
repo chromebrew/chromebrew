b85b2fc5e8891061553e2ab30b45b3b19614ee45
lovedheart <lovedheart@163.com>
2018-03-13 19:44:25 +0100

    Update libwebp.rb

--- packages/libwebp.rb
+++ packages/libwebp.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libwebp < Package
   description 'WebP is a modern image format that provides superior lossless and lossy compression for images on the web.'
   homepage 'https://developers.google.com/speed/webp/'
-  version '0.6.0-0'
+  version '0.6.1'
   source_url 'https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-0.6.1.tar.gz'
   source_sha256 '06503c782d9f151baa325591c3579c68ed700ffc62d4f5a32feead0ff017d8ab'
 
