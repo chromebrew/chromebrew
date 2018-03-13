5257d9098033acf0ae762f1733e9c1bfa4e339e3
lovedheart <lovedheart@163.com>
2018-03-13 19:35:33 +0100

    Update libseccomp.rb

--- packages/libseccomp.rb
+++ packages/libseccomp.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libseccomp < Package
   description 'The libseccomp library provides an easy to use, platform independent, interface to the Linux Kernel\'s syscall filtering mechanism.'
   homepage 'https://github.com/seccomp/libseccomp'
-  version '2.3.3-0'
+  version '2.3.3'
   source_url 'https://github.com/seccomp/libseccomp/archive/v2.3.3.tar.gz'
   source_sha256 '5a52495207f00d1254707f11226e17c16ec53f5038d65bbabf1892873fa2fe5b'
 
