217f3fa09fbd97a8a2bb78deb77b4c9703ed2128
lovedheart <lovedheart@163.com>
2018-03-13 19:48:31 +0100

    Update libxvid.rb

--- packages/libxvid.rb
+++ packages/libxvid.rb
@@ -3,9 +3,9 @@ require 'package'
 class Libxvid < Package
   description 'The free video codec that is strong in compression and quality.'
   homepage 'https://www.xvid.com/'
-  version '1.3.4-0'
-  source_url 'http://downloads.xvid.org/downloads/xvidcore-1.3.4.tar.gz'
-  source_sha256 '4e9fd62728885855bc5007fe1be58df42e5e274497591fec37249e1052ae316f'
+  version '1.3.5'
+  source_url 'https://downloads.xvid.com/downloads/xvidcore-1.3.5.tar.bz2'
+  source_sha256 '7c20f279f9d8e89042e85465d2bcb1b3130ceb1ecec33d5448c4589d78f010b4'
 
 
   depends_on 'yasm'
