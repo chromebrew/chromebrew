ec7dee901b08d1f334e0eb0b1df6901c225b359d
Ed Reel <edreel@gmail.com>
2018-03-12 19:33:43 -0500

    Add pre-built binaries for packages c-l

--- packages/krb5.rb
+++ packages/krb5.rb
@@ -8,11 +8,19 @@ class Krb5 < Package
   source_sha256 'faeb125f83b0fb4cdb2f99f088140631bb47d975982de0956d18c85842969e08'
 
   binary_url ({
-
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/krb5-1.16-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/krb5-1.16-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/krb5-1.16-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/krb5-1.16-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-
+    aarch64: 'bcb8897583e93562cc1fc0c16fdb712dfa464d833c6aac83a668201763d6deeb',
+     armv7l: 'bcb8897583e93562cc1fc0c16fdb712dfa464d833c6aac83a668201763d6deeb',
+       i686: '15ce6e5b300eb7b050c3495b99aaa91c07166144783a20d9278457215bf22c52',
+     x86_64: 'fb5fb9ad6e393104a31c1ef29208840720b796555fa9f7ccd0bbd0412e44495e',
   })
+
+  depends_on 'gawk'
 
   def self.build
     Dir.chdir "src" do
--- packages/libunwind.rb
+++ packages/libunwind.rb
@@ -7,6 +7,18 @@ class Libunwind < Package
   source_url 'http://download.savannah.gnu.org/releases/libunwind/libunwind-1.2.1.tar.gz'
   source_sha256 '3f3ecb90e28cbe53fba7a4a27ccce7aad188d3210bb1964a923a731a27a75acb'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'ad248bc364a305c449d684ba65649f0c952a0e9dcf7742411f7918346ba6ed20',
+     armv7l: 'ad248bc364a305c449d684ba65649f0c952a0e9dcf7742411f7918346ba6ed20',
+       i686: '5a5730f583590483fbac1c4a64970db358449767d2858ad01d13f1f33f3f4d86',
+     x86_64: 'd439b332a4a18ee2a49d0877409c71f28d49f539e31aec87feb6247ad4c22e41',
+  })
 
   depends_on 'buildessential' => :build
   depends_on 'openssl' => :build
