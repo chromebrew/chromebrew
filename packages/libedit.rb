dfef7e5c531897ce0a43d3ff9aed12f1ca47d837
lovedheart <lovedheart@163.com>
2018-03-13 19:16:15 +0100

    Update libedit.rb

--- packages/libedit.rb
+++ packages/libedit.rb
@@ -4,14 +4,14 @@ class Libedit < Package
   description 'This is an autotool and libtoolized port of the NetBSD Editline library (libedit).'
   homepage 'http://thrysoee.dk/editline/'
   version '3.1'
-  source_url 'http://thrysoee.dk/editline/libedit-20170329-3.1.tar.gz'
+  source_url 'https://thrysoee.dk/editline/libedit-20170329-3.1.tar.gz'
   source_sha256 '91f2d90fbd2a048ff6dad7131d9a39e690fd8a8fd982a353f1333dd4017dd4be'
 
 
   def self.build
-	system "./configure  \
-			    --prefix=#{CREW_PREFIX} \
-			    --libdir=#{CREW_LIB_PREFIX} \ 
+    system "./configure  \
+	    --prefix=#{CREW_PREFIX} \
+	    --libdir=#{CREW_LIB_PREFIX} \ 
             CPPFLAGS=\"-I/usr/local/include/ncurses\""
     system "make"
   end
