e0c38a3dcb42db3ede011408170174cbe5d286fd
lovedheart <lovedheart@163.com>
2018-03-13 19:16:55 +0100

    Update libedit.rb

--- packages/libedit.rb
+++ packages/libedit.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libedit < Package
   description 'This is an autotool and libtoolized port of the NetBSD Editline library (libedit).'
   homepage 'http://thrysoee.dk/editline/'
-  version '3.1'
+  version '3.1-0'
   source_url 'https://thrysoee.dk/editline/libedit-20170329-3.1.tar.gz'
   source_sha256 '91f2d90fbd2a048ff6dad7131d9a39e690fd8a8fd982a353f1333dd4017dd4be'
 
