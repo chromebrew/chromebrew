59a4d48e22b3a6f82ae7a553474dba173e88f5a6
lovedheart <lovedheart@163.com>
2018-03-13 19:52:01 +0100

    Update libopus.rb

--- packages/libopus.rb
+++ packages/libopus.rb
@@ -3,16 +3,16 @@ require 'package'
 class Libopus < Package
   description 'Opus is a totally open, royalty-free, highly versatile audio codec.'
   homepage 'http://opus-codec.org/'
-  version '1.2'
-  source_url 'https://archive.mozilla.org/pub/opus/opus-1.2.tar.gz'
-  source_sha256 '77db45a87b51578fbc49555ef1b10926179861d854eb2613207dc79d9ec0a9a9'
+  version '1.2.1'
+  source_url 'https://archive.mozilla.org/pub/opus/opus-1.2.1.tar.gz'
+  source_sha256 'cfafd339ccd9c5ef8d6ab15d7e1a412c054bf4cb4ecbbbcc78c12ef2def70732'
 
 
 
   def self.build
-	system "./configure  \
-			--prefix=#{CREW_PREFIX} \
-			--libdir=#{CREW_LIB_PREFIX} \
+    system "./configure  \
+	    --prefix=#{CREW_PREFIX} \
+	    --libdir=#{CREW_LIB_PREFIX} \
             --enable-shared \
             --enable-static"
     system "make"
