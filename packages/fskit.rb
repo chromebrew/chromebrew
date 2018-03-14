21600416477fc148d4364891f8652dca969f5f25
lovedheart <lovedheart@163.com>
2018-03-14 19:58:52 +0100

    Update fskit.rb

--- packages/fskit.rb
+++ packages/fskit.rb
@@ -3,9 +3,9 @@ require 'package'
 class Fskit < Package
   description 'Filesystem utility library and SDK'
   homepage 'https://github.com/jcnelson/fskit'
-  version '2feb33-1'
-  source_url 'https://github.com/jcnelson/fskit/archive/2feb33a84932980e4ed9691242537771dd4e80f4.tar.gz'
-  source_sha256 'c3f0506eb5d9345e5ea91086ebb2b2afcee889eed55792715c8ab5e523e02031'
+    version 'dd9a8'
+  source_url 'https://github.com/jcnelson/fskit/archive/dd9a8dc844b81afc63e5067397786a32ecb66008.tar.gz'
+  source_sha256 'fe682890ebab9226d65fc6ebfb8b3619c0d5a93e3161787cea9d01ad23d3a83a'
 
 
   depends_on 'attr'
@@ -14,8 +14,8 @@ class Fskit < Package
   def self.patch
     # for x86_64 architecture, build library should be modified to #{CREW_PREFIX}/lib64
     if ARCH == "x86_64"
-       puts "Patch build file for x86_64 ..."
-      `sed -i -- 's/lib/lib64/g' buildconf.mk`  # replace lib by lib64 
+       puts "Patch build file for x86_64 ..." 
+        `sed -i -- 's/lib/lib64/g' buildconf.mk` # replace lib by lib64
     end
   end
     
