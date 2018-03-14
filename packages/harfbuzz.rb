33f1e0b51ab8dab7c4d0f0623946a4e4811bcd10
lovedheart <lovedheart@163.com>
2018-03-14 12:20:01 -0700

    Update harfbuzz.rb (#2084)

--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -3,14 +3,14 @@ require 'package'
 class Harfbuzz < Package
   description 'HarfBuzz is an OpenType text shaping engine.'
   homepage 'https://www.freedesktop.org/wiki/Software/HarfBuzz/'
-  version '1.7.6'
+  version '1.7.6-0'
   source_url 'https://github.com/harfbuzz/harfbuzz/releases/download/1.7.6/harfbuzz-1.7.6.tar.bz2'
   source_sha256 'da7bed39134826cd51e57c29f1dfbe342ccedb4f4773b1c951ff05ff383e2e9b'
 
 
   depends_on 'glib'
   depends_on 'icu4c'
-  depends_on 'freetype'
+  depends_on 'freetype_sub'
 
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
