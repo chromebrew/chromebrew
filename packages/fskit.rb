d41c998fc48392f804a7546c8b8745757abdfd9f
lovedheart <lovedheart@163.com>
2018-03-14 11:10:26 -0700

    [fskit] fix libdir
    #1981

--- packages/fskit.rb
+++ packages/fskit.rb
@@ -3,25 +3,22 @@ require 'package'
 class Fskit < Package
   description 'Filesystem utility library and SDK'
   homepage 'https://github.com/jcnelson/fskit'
-  version '2feb33'
+  version '2feb33-1'
   source_url 'https://github.com/jcnelson/fskit/archive/2feb33a84932980e4ed9691242537771dd4e80f4.tar.gz'
   source_sha256 'c3f0506eb5d9345e5ea91086ebb2b2afcee889eed55792715c8ab5e523e02031'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/fskit-2feb33-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/fskit-2feb33-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/fskit-2feb33-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/fskit-2feb33-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'df4308ee69d9e77c1c48e435f0d3173442b2155af4ca12614ad4b8a290def107',
-     armv7l: 'df4308ee69d9e77c1c48e435f0d3173442b2155af4ca12614ad4b8a290def107',
-       i686: '2a77491b23249a084335e3ff5ac9ed16bac64972be617dfbf6c195104c24c061',
-     x86_64: '3544dae79049282a668fa02af3c59907ca78c3ca06be83dac4dda2491ae93651',
-  })
 
   depends_on 'attr'
 
+    
+  def self.patch
+    # for x86_64 architecture, build library should be modified to #{CREW_PREFIX}/lib64
+    if ARCH == "x86_64"
+       puts "Patch build file for x86_64 ..."
+      `sed -i -- 's/lib/lib64/g' buildconf.mk`  # replace lib by lib64 
+    end
+  end
+    
   def self.build
     system "make"
   end
