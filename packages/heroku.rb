7721f16f12fbe03fc3961e0ea059819c4f85ef0e
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2018-03-14 15:10:50 -0400

    Update heroku.rb

--- packages/heroku.rb
+++ packages/heroku.rb
@@ -3,22 +3,9 @@ require 'package'
 class Heroku < Package
   description 'The Heroku Command Line Interface (CLI), formerly known as the Heroku Toolbelt, is a tool for creating and managing Heroku apps from the command line / shell of various operating systems.'
   homepage 'https://devcenter.heroku.com/articles/heroku-cli'
-  version '6.15.22'
-  source_url 'https://github.com/heroku/cli/archive/v6.15.22.tar.gz'
-  source_sha256 '51f2f82bff5c4dd10a02c6cc177139a91a4b0caa5c85597aafc371caa0da763a'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.15.22-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.15.22-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.15.22-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-6.15.22-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '72fea4ba83d5d8d84dcf4ef4046c30d633ee18fe750ff9e0fde8763aa735157a',
-     armv7l: '72fea4ba83d5d8d84dcf4ef4046c30d633ee18fe750ff9e0fde8763aa735157a',
-       i686: 'f5b6490e7a6919a5f42d32f0361797021bd4e1e4b8d0bc2f017a222c71e35ff9',
-     x86_64: '10e993f7f72df5c41b19ef8af2c65ec81f5bbd107483d014b05c73b53e66aada',
-  })
+  version '6.15.31'
+  source_url 'https://github.com/heroku/cli/archive/v6.15.31.tar.gz'
+  source_sha256 '940c8083b90f95c5400df37dd0de19e423bd665e729c3d8e03b4c1ed34852d95'
   
   depends_on 'yarn'
 
