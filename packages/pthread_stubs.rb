18aac927244b08f20fb59e4c4e5bf5808a63950d
lovedheart <lovedheart@163.com>
2018-03-17 12:44:04 -0700

    Update pthread_stubs.rb

--- packages/pthread_stubs.rb
+++ packages/pthread_stubs.rb
@@ -7,8 +7,6 @@ class Pthread_stubs < Package
   source_url 'https://xcb.freedesktop.org/dist/libpthread-stubs-0.3.tar.bz2'
   source_sha256 '35b6d54e3cc6f3ba28061da81af64b9a92b7b757319098172488a660e3d87299'
 
-
-
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
