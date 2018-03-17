c3dd9683d2ad930ece527c1f1e0ae44ca5766c25
pgdagenais <pgdagenais@gmail.com>
2018-03-16 23:09:54 -0400

    Change source_url to https

--- packages/ansible.rb
+++ packages/ansible.rb
@@ -4,7 +4,7 @@ class Ansible < Package
   description 'Ansible is a radically simple IT automation engine that automates cloud provisioning, configuration management, application deployment, intra-service orchestration, and many other IT needs.'
   homepage 'https://www.ansible.com/'
   version '2.4.3'
-  source_url 'http://releases.ansible.com/ansible/ansible-2.4.3.0.tar.gz'
+  source_url 'https://releases.ansible.com/ansible/ansible-2.4.3.0.tar.gz'
   source_sha256 '0e98b3a56928d03979d5f8e7ae5d8e326939111b298729b03f00b3ad8f998a3d'
 
   depends_on 'python27'
