4f29f1de5ce6d68092b0ddefe17eafdc764f2022
lovedheart <lovedheart@163.com>
2018-03-19 16:05:59 -0700

    Update gusb.rb (#2117)
    Fix dependency

--- packages/gusb.rb
+++ packages/gusb.rb
@@ -3,16 +3,13 @@ require 'package'
 class Gusb < Package
   description 'GUsb is a GObject wrapper for libusb1'
   homepage 'https://www.openhub.net/p/gusb'
-  version '0.2.11'
+  version '0.2.11-0'
   source_url 'https://github.com/hughsie/libgusb/archive/gusb_0_2_11.tar.gz'
   source_sha256 '090eb605e75f8a5b0b3df7ff29d96dd51730850ac89417378d4a8d39fab13702'
 
-  depends_on 'automake' => :build
-  depends_on 'glib'
   depends_on 'gtk_doc'
   depends_on 'libusb'
-  depends_on 'libtool' => :build
-  depends_on 'intltool' => :build
+  depends_on 'gobject_introspection'
 
   def self.build
     system "./autogen.sh"
