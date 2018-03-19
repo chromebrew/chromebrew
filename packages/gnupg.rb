fd86b4699e5e7b05f5318d9358fc63eef76219ab
Ed Reel <edreel@gmail.com>
2018-03-19 00:52:45 -0500

    Add pre-built binaries for packages e-x

--- packages/gnupg.rb
+++ packages/gnupg.rb
@@ -8,8 +8,16 @@ class Gnupg < Package
   source_sha256 '3fa189a32d4fb62147874eb1389047c267d9ba088f57ab521cb0df46f08aef57'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gnupg-2.2.5-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gnupg-2.2.5-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gnupg-2.2.5-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gnupg-2.2.5-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '433aa56f0b8e547d14e65f0aa007fa8dcfbd2b9656052c5e232ef6b8fc269245',
+     armv7l: '433aa56f0b8e547d14e65f0aa007fa8dcfbd2b9656052c5e232ef6b8fc269245',
+       i686: '47ba8dab458b2a33f9b402bd4441ada0aae486b59e48e2658bf1537a95be58f1',
+     x86_64: '26bb165f909b7482f680bd5bba57c76210543afb75c6be62c495a2f237c2e3c9',
   })
 
   depends_on 'automake' => :build
--- packages/gtk3.rb
+++ packages/gtk3.rb
@@ -8,10 +8,16 @@ class Gtk3 < Package
   source_sha256 '61eef0d320e541976e2dfe445729f12b5ade53050ee9de6184235cb60cd4b967'
 
   binary_url ({
-
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gtk3-3.22.26-0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gtk3-3.22.26-0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gtk3-3.22.26-0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gtk3-3.22.26-0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-
+    aarch64: 'd28da624022f0b307f9ed465c8080822384fd0f487b0737e93e40d7fa6c66ca8',
+     armv7l: 'd28da624022f0b307f9ed465c8080822384fd0f487b0737e93e40d7fa6c66ca8',
+       i686: '19c2cd6f3f3f5dac6bf26b378fed28e090aa96277e386f9dc3ada77fdd8cbc93',
+     x86_64: 'e07a18dfb302dbfbf2e5c081873f48ac8d32757238fc4f9ef7060eff307ff90a',
   })
 
   depends_on 'gdk_pixbuf'
@@ -19,7 +25,7 @@ class Gtk3 < Package
   depends_on 'graphene'
   depends_on 'libxkbcommon'
   depends_on 'at_spi2_atk'
-  
+
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
