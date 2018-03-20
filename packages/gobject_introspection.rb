7f3a8095e2cb11bb767fe45abe9dadc81fe7153a
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 12:58:15 +0100

    Update gobject_introspection.rb

--- packages/gobject_introspection.rb
+++ packages/gobject_introspection.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Gobject_introspection < Package
-  description 'GObject introspection is a middleware layer between C libraries (using GObject) and language bindings. https://wiki.gnome.org/action/show/Projects/GObjectIntrospection'
-  homepage 'https://github.com/GNOME/gobject-introspection/releases'
+  description 'GObject introspection is a middleware layer between C libraries (using GObject) and language bindings.'
+  homepage 'https://wiki.gnome.org/action/show/Projects/GObjectIntrospection'
   version '1.56.0'
   source_url 'https://github.com/GNOME/gobject-introspection/archive/1.56.0.tar.gz'
   source_sha256 'dbf222ad56bc0f5a3b6cfc7be07b4b7c9a7e3a65761d5969b9fc800327c88312'
