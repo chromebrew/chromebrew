9f48ef0ddc6caadf388a0adcdea0f3d2a6e2b6d3
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 13:05:00 +0100

    Update libtool.rb

--- packages/libtool.rb
+++ packages/libtool.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Libtool < Package
-  description 'GNU libtool is a generic library support script. Libtool hides the complexity of using shared libraries behind a consistent, portable interface. https://www.gnu.org/software/libtool/'
-  homepage 'http://ftp.igh.cnrs.fr/pub/gnu/libtool/'
+  description 'GNU libtool is a generic library support script. Libtool hides the complexity of using shared libraries behind a consistent, portable interface.'
+  homepage 'https://www.gnu.org/software/libtool/'
   version '2.4.6'
   source_url 'http://ftp.igh.cnrs.fr/pub/gnu/libtool/libtool-2.4.6.tar.xz'
   source_sha256 '7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f'
