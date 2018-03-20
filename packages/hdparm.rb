0abb6433ee3d917a07e23ac42aa56efbdc9ad7e8
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 13:03:41 +0100

    Update hdparm.rb

--- packages/hdparm.rb
+++ packages/hdparm.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Hdparm < Package
-  description 'hdparm is a GNU/Linux shell utility for viewing and manipulating various IDE drive and driver parameters. https://directory.fsf.org/wiki/Hdparm'
-  homepage 'https://sourceforge.net/projects/hdparm/files/hdparm/'
+  description 'hdparm is a GNU/Linux shell utility for viewing and manipulating various IDE drive and driver parameters.'
+  homepage 'https://directory.fsf.org/wiki/Hdparm'
   version '9.55'
   source_url 'https://sourceforge.net/projects/hdparm/files/hdparm/hdparm-9.55.tar.gz'
   source_sha256 'b5c76b732483df36bf08001a209b95b45558016615f935008e5ea91a7fde6dc7'
