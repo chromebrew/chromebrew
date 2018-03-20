7d15d3d6d664d070c36075672d3af1b0a4dab8a9
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 12:49:49 +0100

    Update binutils.rb

--- packages/binutils.rb
+++ packages/binutils.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Binutils < Package
-  description 'The GNU Binutils are a collection of binary tools. http://www.gnu.org/software/binutils/'
-  homepage 'https://ftp.gnu.org/gnu/binutils/'
+  description 'The GNU Binutils are a collection of binary tools.'
+  homepage 'http://www.gnu.org/software/binutils/'
   version '2.30'
   source_url 'https://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.xz'
   source_sha256 '6e46b8aeae2f727a36f0bd9505e405768a72218f1796f0d09757d45209871ae6'
