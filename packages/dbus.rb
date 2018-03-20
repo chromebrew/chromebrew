ff54dee8eb6676aca057c68203a8bd52b0b5a01a
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 12:54:26 +0100

    Update dbus.rb

--- packages/dbus.rb
+++ packages/dbus.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Dbus < Package
-  description 'D-Bus is a message bus system, a simple way for applications to talk to one another. https://www.freedesktop.org/wiki/Software/dbus/'
-  homepage 'https://dbus.freedesktop.org/releases/dbus/'
+  description 'D-Bus is a message bus system, a simple way for applications to talk to one another.'
+  homepage 'https://www.freedesktop.org/wiki/Software/dbus/'
   version '1.13.2'
   source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.2.tar.gz'
   source_sha256 '945deb349a7e2999184827c17351c1bf93c6395b9c3ade0c91cad42cb93435b1'
