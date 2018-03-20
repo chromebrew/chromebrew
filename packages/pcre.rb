4b481a1a9b1e4ebd4b465bdba5774452f73fbc63
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 13:07:22 +0100

    Update pcre.rb

--- packages/pcre.rb
+++ packages/pcre.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Pcre < Package
-  description 'The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5. http://pcre.org/'
-  homepage 'https://ftp.pcre.org/pub/pcre/'
+  description 'The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5.'
+  homepage 'http://pcre.org/'
   version '8.41'
   source_url 'https://ftp.pcre.org/pub/pcre/pcre-8.41.tar.bz2'
   source_sha256 'e62c7eac5ae7c0e7286db61ff82912e1c0b7a0c13706616e94a7dd729321b530'
