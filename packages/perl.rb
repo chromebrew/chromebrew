0331de3599ec87a705681ea142333fe498c3bee3
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 13:09:40 +0100

    Update perl.rb

--- packages/perl.rb
+++ packages/perl.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Perl < Package
-  description 'Perl 5 is a highly capable, feature-rich programming language with over 29 years of development. https://www.perl.org/'
-  homepage 'https://www.perl.org/get.html'
+  description 'Perl 5 is a highly capable, feature-rich programming language with over 29 years of development.'
+  homepage 'https://www.perl.org/'
   version '5.26.1'
   source_url 'http://www.cpan.org/src/5.0/perl-5.26.1.tar.gz'
   source_sha256 'e763aa485e8dc1a70483dbe6d615986bbf32b977f38016480d68c99237e701dd'
