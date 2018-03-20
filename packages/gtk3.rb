823f59c9c9dce3223319ab803f62398b9c328228
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 13:00:33 +0100

    Update gtk3.rb

--- packages/gtk3.rb
+++ packages/gtk3.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Gtk3 < Package
-  description 'Gtk3 is a cross-platform widget toolkit for creating graphical user interfaces. https://developer.gnome.org/gtk3/3.0/'
-  homepage 'https://download.gnome.org/sources/gtk+/'
+  description 'Gtk3 is a cross-platform widget toolkit for creating graphical user interfaces.'
+  homepage 'https://developer.gnome.org/gtk3/3.0/'
   version '3.22.29'
   source_url 'https://download.gnome.org/sources/gtk+/3.22/gtk+-3.22.29.tar.xz'
   source_sha256 'a07d64b939fcc034a066b7723fdf9b24e92c9cfb6a8497593f3471fe56fbbbf8'
