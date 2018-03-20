c99c18fd83ba082a3a94db7e3dfd602821c9df19
Ed Reel <edreel@gmail.com>
2018-03-20 12:19:23 -0500

    Update core packages (#2130)
    * Update core packages
    
    Add pre-built binaries
    
    Use source with higher compression for wget
    
    * Fix source for wget

--- packages/libsigsegv.rb
+++ packages/libsigsegv.rb
@@ -3,30 +3,38 @@ require 'package'
 class Libsigsegv < Package
   description 'GNU libsigsegv is a library for handling page faults in user mode.'
   homepage 'https://www.gnu.org/software/libsigsegv/'
-  version '2.11'
-  source_url 'ftp://ftp.gnu.org/gnu/libsigsegv/libsigsegv-2.11.tar.gz'
-  source_sha256 'dd7c2eb2ef6c47189406d562c1dc0f96f2fc808036834d596075d58377e37a18'
+  version '2.12'
+  source_url 'https://ftpmirror.gnu.org/libsigsegv/libsigsegv-2.12.tar.gz'
+  source_sha256 '3ae1af359eebaa4ffc5896a1aee3568c052c99879316a1ab57f8fe1789c390b6'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.11-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.11-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.11-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.11-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.12-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.12-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.12-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigsegv-2.12-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '848d1532b32bd8833013093ae05f42bc7289d12aafcd1b29152598deefcd546c',
-     armv7l: '848d1532b32bd8833013093ae05f42bc7289d12aafcd1b29152598deefcd546c',
-       i686: '4f2ed1ee3c5827881d8fbf0e0fe59fa8a9d4f581e5adc03a93ff97e8032a25f4',
-     x86_64: 'a6e4750facffe105650502313c2622368e0923919adeb38865e8875b88288565',
+    aarch64: 'e95892871d5cfd7164b3056e87461fd852d1a224ca0a23f0dd73a98e71a83217',
+     armv7l: 'e95892871d5cfd7164b3056e87461fd852d1a224ca0a23f0dd73a98e71a83217',
+       i686: '626159d654d90139bfb1301323aea5c28b6ad37f895824323471eb7911a5ce4f',
+     x86_64: '02097e964faa7116a1e4701f322da97375d8df1a0928cadc05e86b838fe9fef3',
   })
 
   def self.build
-    system "./configure", "--libdir=#{CREW_LIB_PREFIX}", "--enable-shared", "--disable-static", "--with-pic"
-    system "make"
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           '--enable-shared',
+           '--disable-static',
+           '--with-pic'
+    system 'make'
   end
 
   def self.install
-    system "make check"
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make", "check"
   end
 end
