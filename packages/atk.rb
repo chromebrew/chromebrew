7aad3910cbaee7ab7de9adb1c15b30527d438b11
beliefless dogmasless <34506558+beliefless-dogmasless@users.noreply.github.com>
2018-03-20 12:48:59 +0100

    Update atk.rb

--- packages/atk.rb
+++ packages/atk.rb
@@ -1,8 +1,8 @@
 require 'package'
 
 class Atk < Package
-  description 'GTK+ & GNOME Accessibility Toolkit. https://wiki.gnome.org/Accessibility'
-  homepage 'https://download.gnome.org/sources/atk/'
+  description 'GTK+ & GNOME Accessibility Toolkit.'
+  homepage 'https://wiki.gnome.org/Accessibility'
   version '2.28.1'
   source_url 'https://download.gnome.org/sources/atk/2.28/atk-2.28.1.tar.xz'
   source_sha256 'cd3a1ea6ecc268a2497f0cd018e970860de24a6d42086919d6bf6c8e8d53f4fc'
