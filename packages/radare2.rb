a020cf8368fedc4fd571c41b4f8ae24019199a92
Ed Reel <edreel@gmail.com>
2018-03-24 14:27:42 -0500

    Add pre-built binaries for packages i-s

--- packages/radare2.rb
+++ packages/radare2.rb
@@ -8,8 +8,16 @@ class Radare2 < Package
   source_sha256 'e2edef4d70c7bbbb47d04002ce9d384eb2fc9c0cd4cbfde77cda8c10cae9ff24'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/radare2-2.4.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/radare2-2.4.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/radare2-2.4.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/radare2-2.4.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '7f1625aa3bbeea13de76bf122eab70130df32da119585cb0e46cea523f3e07ab',
+     armv7l: '7f1625aa3bbeea13de76bf122eab70130df32da119585cb0e46cea523f3e07ab',
+       i686: '81f78367ade45bc48632a1cfe4ebaebd9d6c549abfe3ff26a7d32116b79329ee',
+     x86_64: 'fcde3ae52954bf875ecbc650d0e0392ef80175f3df4c7b7f560aeb0535ed1969',
   })
 
   depends_on 'openssl'
