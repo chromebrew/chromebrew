1322aa29243c3d6b5223b585f4f53ef3daaeef47
Jeff Mcneill <jeff@mcneill.io>
2018-03-24 14:22:07 +0700

    Update imagemagick7.rb
    I've tested this checksum, previously was reading digest.rdf incorrectly.

--- packages/imagemagick7.rb
+++ packages/imagemagick7.rb
@@ -5,7 +5,7 @@ class Imagemagick7 < Package
   homepage 'http://www.imagemagick.org/script/index.php'
   version '7.0.7-27'
   source_url 'https://www.imagemagick.org/download/releases/ImageMagick-7.0.7-27.tar.xz'
-  source_sha256 '56130c16790d8d38c3a1888cc07074d5b50ffb718243e0c56e4c88839edbf931'
+  source_sha256 '543776f09d69e3ca29b1b83a9c0223185ba26bec673593840b4d7face6ea253b'
 
   binary_url ({
   })
