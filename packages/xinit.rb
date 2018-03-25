8552208f4b43cafdf5f2dec202a5fa89f199dea7
lovedheart <lovedheart@163.com>
2018-03-25 23:41:46 +0200

    Update xinit.rb

--- packages/xinit.rb
+++ packages/xinit.rb
@@ -4,7 +4,7 @@ class Xinit < Package
   description 'The xinit package contains a usable script to start the xserver'
   homepage 'https://www.x.org'
   version '1.4.0'
-  source_url 'ftp://ftp.x.org/pub/individual/app/xinit-1.4.0.tar.bz2'
+  source_url 'https://x.org/pub/individual/app/xinit-1.4.0.tar.bz2'
   source_sha256 '230835eef2f5978a1e1344928168119373f6df1d0a32c09515e545721ee582ef'
 
   depends_on 'xorg_lib'
