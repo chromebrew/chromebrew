f429d6895353e1d979ce8392cad584d857d525b8
lovedheart <lovedheart@163.com>
2018-03-25 23:31:59 +0200

    Update gnome_themes_standard.rb

--- packages/gnome_themes_standard.rb
+++ packages/gnome_themes_standard.rb
@@ -3,19 +3,16 @@ require 'package'
 class Gnome_themes_standard < Package
   description 'Standard Themes for GNOME Applications.'
   homepage 'https://git.gnome.org/browse/gnome-themes-standard/'
-  version '3.27.92'
+  version '3.27.92-0'
   source_url 'https://github.com/GNOME/gnome-themes-extra/archive/3.27.92.tar.gz'
   source_sha256 'c2c2b02e6826d51de4e6e53afffbdb0ec301ef776df91944574fdc42b558cecb'
 
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
   
   depends_on 'gtk3'
   depends_on 'gtk2'
   depends_on 'librsvg'
   depends_on 'gdk_pixbuf'
+  depends_on 'gnome_common'
 
   def self.build
     system "sh autogen.sh"
@@ -24,7 +21,7 @@ class Gnome_themes_standard < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install" # the steps required to install the package
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
   
 end
