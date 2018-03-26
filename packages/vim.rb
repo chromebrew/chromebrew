103236f3ba98f3550f460b625064b28e1acaf174
Ed Reel <edreel@gmail.com>
2018-03-26 17:30:22 -0500

    Update vim from 8.0-1 to 8.0-2

--- packages/vim.rb
+++ packages/vim.rb
@@ -3,21 +3,21 @@ require 'package'
 class Vim < Package
   description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient.'
   homepage 'http://www.vim.org/'
-  version '8.0-1'
+  version '8.0-2'
   source_url 'ftp://ftp.vim.org/pub/vim/unix/vim-8.0.tar.bz2'
   source_sha256 '08bd0d1dd30ece3cb9905ccd48b82b2f81c861696377508021265177dc153a61'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '86d5b6b889110adab6bb3bfb34aa9579c37f26efcb35b1c4523558fdedb7dace',
-     armv7l: '86d5b6b889110adab6bb3bfb34aa9579c37f26efcb35b1c4523558fdedb7dace',
-       i686: 'cea1f34f7e1456fd9725d18e943f3eee1735e82fe1ec2a35bf3197971436750f',
-     x86_64: '3bfd0d28f2a05d9d551f449e9a5b662a906c0a7799c8263cac05567dcb2b9cc4',
+    aarch64: '41c5462bc6a85758c826a3238d1e639beff5aeaad4cad6c279a8916b6d3ae21a',
+     armv7l: '41c5462bc6a85758c826a3238d1e639beff5aeaad4cad6c279a8916b6d3ae21a',
+       i686: 'c248117c8aba83f20c362999da8ec4b7c7b73aa74e6e4251a0dc114320809df9',
+     x86_64: 'c0b2c46297ce1b5d01c42352675e26c980c4f51e1816263150c4c0e99f1899be',
   })
 
   depends_on 'compressdoc' => :build
@@ -40,6 +40,7 @@ class Vim < Package
             --enable-pythoninterp \
             --enable-rubyinterp \
             --with-ruby-command=#{CREW_PREFIX}/bin/ruby \
+            --with-vimdiff \
             --disable-selinux"
     system "make"
   end
