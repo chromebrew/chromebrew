138a168176ada105bcb2373c08cbafc8c956b60e
Ed Reel <edreel@gmail.com>
2018-03-25 19:57:40 -0500

    Add pre-built binaries for gnome_themes_standard, ncdc, sshrc, tack and xinit

--- packages/ncdc.rb
+++ packages/ncdc.rb
@@ -8,8 +8,16 @@ class Ncdc < Package
   source_sha256 '8a998857df6289b6bd44287fc06f705b662098189f2a8fe95b1a5fbc703b9631'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ncdc-1.20-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ncdc-1.20-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ncdc-1.20-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ncdc-1.20-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '29c39214a120bd002a78b118a53daa085530e837a6b67f1cf10bbaa7d1991524',
+     armv7l: '29c39214a120bd002a78b118a53daa085530e837a6b67f1cf10bbaa7d1991524',
+       i686: 'f28d8e26a29f6c2d555f591e5b3552aa1231fc843f52b827950ab67293443f2f',
+     x86_64: '7d72e59960fa27fef99a6f45c0fc9fc86b62931851c811d192e60d1376829452',
   })
 
   depends_on 'ncursesw'
