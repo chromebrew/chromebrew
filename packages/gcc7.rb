bff375540373464ca069126a36ab0976518e8c1e
Nick Meyer <nick.j.meyer@gmail.com>
2018-04-01 18:54:17 -0400

    Update dependencies and remove x86_64 specifics in gcc7.

--- packages/gcc7.rb
+++ packages/gcc7.rb
@@ -10,28 +10,31 @@ class Gcc7 < Package
   source_sha256 'd8cb199f0d98bd83e3dac645126722e44137c24cbe5e87e1113dd4270f7530c6'
 
   depends_on 'buildessential'
-  depends_on 'binutils'
-  depends_on 'gmp'
-  depends_on 'mpfr'
-  depends_on 'mpc'
-  depends_on 'isl'
-  depends_on 'cloog'
-  depends_on 'glibc'
   depends_on 'gawk'
+  depends_on 'unzip' => :build
 
   def self.build
     system "mkdir", "build"
     Dir.chdir "build" do
-      ENV["LIBRARY_PATH"] = "/usr/local/lib64"
-      system "../configure", "-v", "--build=x86_64-linux-gnu", "--host=x86_64-linux-gnu", "--target=x86_64-linux-gnu", "--libdir=#{CREW_LIB_PREFIX}", "--prefix=#{CREW_PREFIX}", "--enable-checking=release", "--enable-languages=c,c++,fortran", "--disable-multilib", "--disable-libmpx", "--program-suffix=-7.3"
+      ENV["LIBRARY_PATH"] = "#{CREW_LIB_PREFIX}"
+      system "../configure",
+             "-v",
+             "--libdir=#{CREW_LIB_PREFIX}",
+             "--prefix=#{CREW_PREFIX}",
+             "--enable-checking=release",
+             "--enable-languages=c,c++,fortran",
+             "--disable-multilib",
+             "--disable-libmpx",
+             "--program-suffix=-7.3"
       system "make"
     end
   end
 
   def self.install
     Dir.chdir "build" do
-      ENV["LIBRARY_PATH"] = "/usr/local/lib64"
-      system "make", "install-strip"
+      ENV["LIBRARY_PATH"] = "#{CREW_LIB_PREFIX}"
+      system "make",
+             "install-strip"
     end
   end
 end
