f8a6d5c779a46aabc912d54eb1c55bb4ee9ccab0
lovedheart <lovedheart@163.com>
2018-04-02 08:41:27 -0700

    [doxygen] minor change

--- packages/doxygen.rb
+++ packages/doxygen.rb
@@ -1,36 +1,42 @@
 require 'package'
 
 class Doxygen < Package
-  description 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources'
+  description 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources, but it also supports other popular programming languages such as C, Objective-C, C#, PHP, Java, Python, IDL (Corba, Microsoft, and UNO/OpenOffice flavors), Fortran, VHDL, Tcl, and to some extent D.'
   homepage 'http://www.stack.nl/~dimitri/doxygen/'
-  version '1.8.14-0'
+  version '1.8.14'
   source_url 'https://github.com/doxygen/doxygen/archive/Release_1_8_14.tar.gz'
   source_sha256 '18bc3790b4d5f4d57cb8ee0a77dd63a52518f3f70d7fdff868a7ce7961a6edc3'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-0-chromeos-x86_64.tar.xz',
-
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/doxygen-1.8.14-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '7b9c085e0b949dc392fbef741fffb1990ca5c4e4da9574e70bc78f404c4f6823',
-     armv7l: '7b9c085e0b949dc392fbef741fffb1990ca5c4e4da9574e70bc78f404c4f6823',
-       i686: 'ffdefe84883879da73bba23ea6aed9cf652efa868b384910b0830afcd5028b15',
-     x86_64: 'd6627ee0caad73c73f622d49c4a2702680d66bc231ef1d42acf9f61fa0949644',
-    
+    aarch64: 'a4e9e8bbade37410c9d055995070a50208e39bcec4ad4f00a180dc901c2fa5e0',
+     armv7l: 'a4e9e8bbade37410c9d055995070a50208e39bcec4ad4f00a180dc901c2fa5e0',
+       i686: '7e117bc6d09452cb8b271357dcdefb3484aed607de6b0cde60d5085b2d1e12d1',
+     x86_64: '90eb469a28b5f7d058d90a93b21589a239edcc29059d397585e59f44d786f17d',
   })
 
   depends_on 'cmake' => :build
   depends_on 'python27' => :build
 
   def self.build
-    system "cmake ."
-    system "make"
+    system "mkdir -p build"
+    Dir.chdir("build") do
+      system "cmake",
+             "-DCMAKE_INSTALL_PREFIX:PATH=#{CREW_PREFIX}",
+             "-DCMAKE_LIBRARY_PATH=#{CREW_LIB_PREFIX}",
+             ".."
+      system "make"
+    end
   end
 
   def self.install
-    system "make", "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    Dir.chdir("build") do
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    end
   end
 end
