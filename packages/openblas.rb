9309f4e16ba46a448e5e7f38229b942e6f703723
Nick Meyer <nick.j.meyer@gmail.com>
2018-04-04 19:38:22 -0400

    Fix openblas package.

--- packages/openblas.rb
+++ packages/openblas.rb
@@ -3,13 +3,12 @@ require 'package'
 class Openblas < Package
   description 'OpenBLAS is an optimized BLAS library'
   homepage 'http://www.openblas.net/'
-  version '7.3.0'
+  version '0.2.20'
 
   source_url 'https://github.com/xianyi/OpenBLAS/archive/v0.2.20.zip'
 
   source_sha256 'bb5499049cf60b07274740a4ddd756daa0fe2c817d981d7fe7e5898dcf411fdc'
 
-  depends_on 'gcc' => :build
   depends_on 'unzip' => :build
 
   def self.build
@@ -20,6 +19,7 @@ class Openblas < Package
     system "make",
            "DESTDIR=#{CREW_DEST_DIR}",
            "PREFIX=#{CREW_PREFIX}",
+           "LIBDIR=#{CREW_LIB_PREFIX}",
            "install"
   end
 end
