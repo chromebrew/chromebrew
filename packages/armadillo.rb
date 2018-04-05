5f181fcfcee3bcef0a9efe92f4c36fda0b8b70ac
Nicholas J Meyer <nickjmeyer@users.noreply.github.com>
2018-04-04 21:53:15 -0400

    Fix armadillo package (#2197)
    * Fix armadillo package
    
    * Remove LIBDIR from armadillo install.

--- packages/armadillo.rb
+++ packages/armadillo.rb
@@ -8,17 +8,20 @@ class Armadillo < Package
   source_url 'https://downloads.sourceforge.net/project/arma/armadillo-8.400.0.tar.xz'
   source_sha256 '5cb6bc2f457a9d6a0758cfb15c418d48289909daccd79d0e428452029285dd9b'
 
+  depends_on 'cmake' => :build
   depends_on 'openblas'
 
   def self.build
     system "./configure",
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
+           "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
+           "-DCMAKE_INSTALL_LIBDIR=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "make",
+           "DESTDIR=#{CREW_DEST_DIR}",
+           "install"
   end
 
 end
