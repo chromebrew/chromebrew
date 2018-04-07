9e0af0a89365ed91d0af6ff32ac0d43d5d840a52
Ed Reel <edreel@gmail.com>
2018-04-07 18:11:21 -0500

    Update php packages

--- packages/php5.rb
+++ packages/php5.rb
@@ -3,42 +3,66 @@ require 'package'
 class Php5 < Package
   description 'PHP is a popular general-purpose scripting language that is especially suited to web development.'
   homepage 'http://www.php.net/'
-  version '5.6.34'
-  source_url 'http://php.net/distributions/php-5.6.34.tar.xz'
-  source_sha256 '21453be3a045204cd2717543ef42771324f411f40962ecda4fe841930a933128'
+  version '5.6.35'
+  source_url 'http://php.net/distributions/php-5.6.35.tar.xz'
+  source_sha256 '9985cb64cb8224c289effff5b34f670d14f838175f76daea0507d643eec650d2'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.34-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.34-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.34-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.34-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.35-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.35-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.35-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.35-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '21f7c51091347184fa94f298d229e7cd60892e4a5286d65b03443c1847105741',
-     armv7l: '21f7c51091347184fa94f298d229e7cd60892e4a5286d65b03443c1847105741',
-       i686: 'e0964018f1b9e26c0c3979ab062c6d7d4b10a90971a7c8f7d434ab11847fb21f',
-     x86_64: '987c1b2a993ad2da290e356bc9d853f8be7ea401b9c4d8925c7e4c294174df1c',
+    aarch64: 'd3effb35a61f47e79ae459b36b49ee40bfc2a30626cee8bf73f5614965413cdd',
+     armv7l: 'd3effb35a61f47e79ae459b36b49ee40bfc2a30626cee8bf73f5614965413cdd',
+       i686: '96430c77f94a022f8b94734af36614368f26d3b2bab8bb4f240c3b908e729e52',
+     x86_64: '0772f8d82fd27a2d3bae83d27929950cc80db59d2e0f70d8db04f131f6cad753',
   })
 
-  depends_on 'pkgconfig'
-  depends_on 'zlibpkg'
+  depends_on 'libgcrypt'
   depends_on 'libpng'
-  depends_on 'libxml2'
   depends_on 'libxslt'
-  depends_on 'openssl'
   depends_on 'curl'
   depends_on 'pcre'
   depends_on 'readline'
 
   def self.build
+    libdir = 'lib'
+    libdir = 'lib64' if ARCH == 'x86_64'
+    system "sed -i 's,;pid = run/php-fpm.pid,pid = #{CREW_PREFIX}/tmp/run/php-fpm.pid,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,;error_log = log/php-fpm.log,error_log = #{CREW_PREFIX}/log/php-fpm.log,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,include=@php_fpm_sysconfdir@/php-fpm.d,include=#{CREW_PREFIX}/etc/php-fpm.d,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,^user,;user,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,^group,;group,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,^user,;user,' sapi/fpm/www.conf.in"
+    system "sed -i 's,^group,;group,' sapi/fpm/www.conf.in"
+    system "sed -i 's,@sbindir@,#{CREW_PREFIX}/bin,' sapi/fpm/init.d.php-fpm.in"
+    system "sed -i 's,@sysconfdir@,#{CREW_PREFIX}/etc,' sapi/fpm/init.d.php-fpm.in"
+    system "sed -i 's,@localstatedir@,#{CREW_PREFIX}/tmp,' sapi/fpm/init.d.php-fpm.in"
+    # Set some sane defaults
+    system "sed -i 's,post_max_size = 8M,post_max_size = 128M,' php.ini-development"
+    system "sed -i 's,upload_max_filesize = 2M,upload_max_filesize = 128M,' php.ini-development"
+    system "sed -i 's,;opcache.enable=0,opcache.enable=1,' php.ini-development"
     system "./configure \
       --prefix=#{CREW_PREFIX} \
+      --docdir=#{CREW_PREFIX}/doc \
+      --infodir=#{CREW_PREFIX}/info \
       --libdir=#{CREW_LIB_PREFIX} \
+      --localstatedir=#{CREW_PREFIX}/tmp \
+      --mandir=#{CREW_PREFIX}/man \
+      --sbindir=#{CREW_PREFIX}/bin \
+      --with-config-file-path=#{CREW_PREFIX}/etc \
+      --with-libdir=#{libdir} \
+      --enable-fpm \
+      --enable-mbstring \
+      --enable-opcache \
       --with-curl \
       --with-gd \
       --with-xsl \
-      --enable-mbstring \
+      --with-mysqli \
       --with-openssl \
+      --with-pdo-mysql \
       --with-pcre-regex \
       --with-readline \
       --with-zlib"
@@ -46,11 +70,36 @@ class Php5 < Package
   end
 
   def self.install
+    system "mkdir -p #{CREW_DEST_PREFIX}/log"
+    system "mkdir -p #{CREW_DEST_PREFIX}/tmp/run"
     system "make", "INSTALL_ROOT=#{CREW_DEST_DIR}", "install"
+    system "install -Dm644 php.ini-development #{CREW_DEST_PREFIX}/etc/php.ini"
+    system "install -Dm755 sapi/fpm/init.d.php-fpm.in #{CREW_DEST_PREFIX}/etc/init.d/php-fpm"
+    system "install -Dm644 sapi/fpm/php-fpm.conf.in #{CREW_DEST_PREFIX}/etc/php-fpm.conf"
+    system "install -Dm644 sapi/fpm/www.conf.in #{CREW_DEST_PREFIX}/etc/php-fpm.d/www.conf"
+    system "ln -s #{CREW_PREFIX}/etc/init.d/php-fpm #{CREW_DEST_PREFIX}/bin/php5-fpm"
 
     # clean up some files created under #{CREW_DEST_DIR}. check http://pear.php.net/bugs/bug.php?id=20383 for more details
     system "mv", "#{CREW_DEST_DIR}/.depdb", "#{CREW_DEST_LIB_PREFIX}/php"
     system "mv", "#{CREW_DEST_DIR}/.depdblock", "#{CREW_DEST_LIB_PREFIX}/php"
     system "rm", "-rf", "#{CREW_DEST_DIR}/.channels", "#{CREW_DEST_DIR}/.filemap", "#{CREW_DEST_DIR}/.lock", "#{CREW_DEST_DIR}/.registry"
+  end
+
+  def self.postinstall
+    puts
+    puts "To start the php-fpm service, execute:".lightblue
+    puts "php5-fpm start".lightblue
+    puts
+    puts "To stop the php-fpm service, execute:".lightblue
+    puts "php5-fpm stop".lightblue
+    puts
+    puts "To restart the php-fpm service, execute:".lightblue
+    puts "php5-fpm restart".lightblue
+    puts
+    puts "To start php-fpm on login, execute the following:".lightblue
+    puts "echo 'if [ -f #{CREW_PREFIX}/bin/php5-fpm ]; then' >> ~/.bashrc".lightblue
+    puts "echo '  #{CREW_PREFIX}/bin/php5-fpm start' >> ~/.bashrc".lightblue
+    puts "echo 'fi' >> ~/.bashrc".lightblue
+    puts "source ~/.bashrc".lightblue
   end
 end
--- packages/php7.rb
+++ packages/php7.rb
@@ -3,35 +3,45 @@ require 'package'
 class Php7 < Package
   description 'PHP is a popular general-purpose scripting language that is especially suited to web development.'
   homepage 'http://www.php.net/'
-  version '7.2.3'
-  source_url 'https://php.net/distributions/php-7.2.3.tar.xz'
-  source_sha256 'b3a94f1b562f413c0b96f54bc309706d83b29ac65d9b172bc7ed9fb40a5e651f'
+  version '7.2.4'
+  source_url 'https://php.net/distributions/php-7.2.4.tar.xz'
+  source_sha256 '7916b1bd148ddfd46d7f8f9a517d4b09cd8a8ad9248734e7c8dd91ef17057a88'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.2.4-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '076113ce15e8e5f0e2176924d296a9b936fa6e269e2b6d9e9ca2b2b3493f5267',
-     armv7l: '076113ce15e8e5f0e2176924d296a9b936fa6e269e2b6d9e9ca2b2b3493f5267',
-       i686: 'c8ee47aee521aa13748e0d0a701fd67c8a4aae98a3a8b05aa4fdf01e3b0eb9c4',
-     x86_64: 'd38bc3cd1ddbc24c9803aac7d30dfdf1620e67f83b3feeb142ec4bdb35deb825',
+    aarch64: '3cf53ef772dada3e75f9582d67648b5136668a9a86bda5ed506c555103c597e3',
+     armv7l: '3cf53ef772dada3e75f9582d67648b5136668a9a86bda5ed506c555103c597e3',
+       i686: '2a9969194dba7b3c03fa13519d136402a2b67f54742af4cae365b70bdbee8300',
+     x86_64: '07832256ee0f311534432a5cf074e179a4f1928627af868c63b16b5a4e23dc1d',
   })
 
-  depends_on 'pkgconfig'
-  depends_on 'zlibpkg'
   depends_on 'libgcrypt'
   depends_on 'libpng'
-  depends_on 'libxml2'
   depends_on 'libxslt'
-  depends_on 'openssl'
   depends_on 'curl'
   depends_on 'pcre'
   depends_on 'readline'
 
   def self.build
+    libdir = 'lib'
+    libdir = 'lib64' if ARCH == 'x86_64'
+    system "sed -i 's,;pid = run/php-fpm.pid,pid = #{CREW_PREFIX}/tmp/run/php-fpm.pid,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,;error_log = log/php-fpm.log,error_log = #{CREW_PREFIX}/log/php-fpm.log,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,include=@php_fpm_sysconfdir@/php-fpm.d,include=#{CREW_PREFIX}/etc/php-fpm.d,' sapi/fpm/php-fpm.conf.in"
+    system "sed -i 's,^user,;user,' sapi/fpm/www.conf.in"
+    system "sed -i 's,^group,;group,' sapi/fpm/www.conf.in"
+    system "sed -i 's,@sbindir@,#{CREW_PREFIX}/bin,' sapi/fpm/init.d.php-fpm.in"
+    system "sed -i 's,@sysconfdir@,#{CREW_PREFIX}/etc,' sapi/fpm/init.d.php-fpm.in"
+    system "sed -i 's,@localstatedir@,#{CREW_PREFIX}/tmp,' sapi/fpm/init.d.php-fpm.in"
+    # Set some sane defaults
+    system "sed -i 's,post_max_size = 8M,post_max_size = 128M,' php.ini-development"
+    system "sed -i 's,upload_max_filesize = 2M,upload_max_filesize = 128M,' php.ini-development"
+    system "sed -i 's,;opcache.enable=0,opcache.enable=1,' php.ini-development"
     system "./configure \
       --prefix=#{CREW_PREFIX} \
       --docdir=#{CREW_PREFIX}/doc \
@@ -39,11 +49,18 @@ class Php7 < Package
       --libdir=#{CREW_LIB_PREFIX} \
       --localstatedir=#{CREW_PREFIX}/tmp \
       --mandir=#{CREW_PREFIX}/man \
+      --sbindir=#{CREW_PREFIX}/bin \
+      --with-config-file-path=#{CREW_PREFIX}/etc \
+      --with-libdir=#{libdir} \
+      --enable-fpm \
+      --enable-mbstring \
+      --enable-opcache \
       --with-curl \
       --with-gd \
       --with-xsl \
-      --enable-mbstring \
+      --with-mysqli \
       --with-openssl \
+      --with-pdo-mysql \
       --with-pcre-regex \
       --with-readline \
       --with-zlib"
@@ -51,11 +68,36 @@ class Php7 < Package
   end
 
   def self.install
+    system "mkdir -p #{CREW_DEST_PREFIX}/log"
+    system "mkdir -p #{CREW_DEST_PREFIX}/tmp/run"
     system "make", "INSTALL_ROOT=#{CREW_DEST_DIR}", "install"
+    system "install -Dm644 php.ini-development #{CREW_DEST_PREFIX}/etc/php.ini"
+    system "install -Dm755 sapi/fpm/init.d.php-fpm.in #{CREW_DEST_PREFIX}/etc/init.d/php-fpm"
+    system "install -Dm644 sapi/fpm/php-fpm.conf.in #{CREW_DEST_PREFIX}/etc/php-fpm.conf"
+    system "install -Dm644 sapi/fpm/www.conf.in #{CREW_DEST_PREFIX}/etc/php-fpm.d/www.conf"
+    system "ln -s #{CREW_PREFIX}/etc/init.d/php-fpm #{CREW_DEST_PREFIX}/bin/php7-fpm"
 
     # clean up some files created under #{CREW_DEST_DIR}. check http://pear.php.net/bugs/bug.php?id=20383 for more details
     system "mv", "#{CREW_DEST_DIR}/.depdb", "#{CREW_DEST_LIB_PREFIX}/php"
     system "mv", "#{CREW_DEST_DIR}/.depdblock", "#{CREW_DEST_LIB_PREFIX}/php"
     system "rm", "-rf", "#{CREW_DEST_DIR}/.channels", "#{CREW_DEST_DIR}/.filemap", "#{CREW_DEST_DIR}/.lock", "#{CREW_DEST_DIR}/.registry"
+  end
+
+  def self.postinstall
+    puts
+    puts "To start the php-fpm service, execute:".lightblue
+    puts "php7-fpm start".lightblue
+    puts
+    puts "To stop the php-fpm service, execute:".lightblue
+    puts "php7-fpm stop".lightblue
+    puts
+    puts "To restart the php-fpm service, execute:".lightblue
+    puts "php7-fpm restart".lightblue
+    puts
+    puts "To start php-fpm on login, execute the following:".lightblue
+    puts "echo 'if [ -f #{CREW_PREFIX}/bin/php7-fpm ]; then' >> ~/.bashrc".lightblue
+    puts "echo '  #{CREW_PREFIX}/bin/php7-fpm start' >> ~/.bashrc".lightblue
+    puts "echo 'fi' >> ~/.bashrc".lightblue
+    puts "source ~/.bashrc".lightblue
   end
 end
