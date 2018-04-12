753ad7549fc235af8d012079b78fd3a6eddb3319
lovedheart <lovedheart@163.com>
2018-04-12 14:11:25 -0700

    [pkgconfig] Recompile binaries
    Reconfigure system-include-path and system-library-path for different architectures.

--- packages/pkgconfig.rb
+++ packages/pkgconfig.rb
@@ -3,34 +3,48 @@ require 'package'
 class Pkgconfig < Package
   description 'pkg-config is a helper tool used when compiling applications and libraries.'
   homepage 'https://www.freedesktop.org/wiki/Software/pkg-config/'
-  version '0.29.2'
+  version '0.29.2-0'
   source_url 'http://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz'
   source_sha256 '6fc69c01688c9458a57eb9a1664c9aba372ccda420a02bf4429fe610e7e7d591'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pkgconfig-0.29.2-0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'b07f5bc401a82f0c91a15cf8918fa3ba19233fa4d5098117220bdb27f60b0ddb',
-     armv7l: 'b07f5bc401a82f0c91a15cf8918fa3ba19233fa4d5098117220bdb27f60b0ddb',
-       i686: '32b54dd0baa0518164fc44021244415b7657673e8a704f0bdd137e3a403c2285',
-     x86_64: '0002d91658f4c0789de2831b716fc4025ffa661c3de68d7308d72bddc553d426',
+    aarch64: '53a0d80b00c745762a25eb9622fdd96f1f2e3a7ff86ca13bec15cdc6c943845c',
+     armv7l: '53a0d80b00c745762a25eb9622fdd96f1f2e3a7ff86ca13bec15cdc6c943845c',
+       i686: 'bb54749f9ca7f6cf50adc8e52ecf2dc48af3eb1e5bc841e65ecab55142020741',
+     x86_64: 'd5084005544be49e828269473c0acd43eb7bb46b7080926c3a9cddd0926c028b',
   })
-
-  # It is not possible to write buildessential here since it causes dependency loop.
-  #   depends_on 'buildessential'
-  # Write dependency to gcc make linuxheaders instead.
-  depends_on 'gcc'
-  depends_on 'linuxheaders'
-  depends_on 'make'
+  
+  depends_on 'python27' => :build
 
   def self.build
-      # check lib64 on any architectures since it is not a problem to not exist lib64 directory.
-      system "./configure", "--with-internal-glib", "--with-pc-path=/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig:/usr/local/share/pkgconfig"
-      system "make"
+    case ARCH
+    when "x86_64"
+      system "./configure",
+             "--with-internal-glib",
+             "--with-pc-path=#{CREW_PREFIX}/lib/pkgconfig:#{CREW_PREFIX}/lib64/pkgconfig:#{CREW_PREFIX}/share/pkgconfig",
+             "--with-system-include-path=/usr/include:#{CREW_PREFIX}/include",
+             "--with-system-library-path=#{CREW_PREFIX}/lib:#{CREW_PREFIX}/lib64",
+             "--disable-static",
+             "--enable-shared",
+             "--disable-host-tool"
+    when "i686","armv7l","aarch64"
+      system "./configure",
+             "--with-internal-glib",
+             "--with-pc-path=#{CREW_PREFIX}/lib/pkgconfig:#{CREW_PREFIX}/share/pkgconfig",
+             "--with-system-include-path=/usr/include:#{CREW_PREFIX}/include",
+             "--with-system-library-path=#{CREW_PREFIX}/lib",
+             "--disable-static",
+             "--enable-shared",
+             "--disable-host-tool"
+    end
+    system "make"
+
   end
 
   def self.install
