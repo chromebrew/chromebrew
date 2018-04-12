de76054af357ca7899c1ac2c638587e19f62ac3e
Ed Reel <edreel@gmail.com>
2018-04-12 02:25:35 -0500

    Update core packages a-u

--- packages/gawk.rb
+++ packages/gawk.rb
@@ -3,21 +3,21 @@ require 'package'
 class Gawk < Package
   description 'The gawk utility interprets a special-purpose programming language that makes it possible to handle simple data-reformatting jobs with just a few lines of code.'
   homepage 'https://www.gnu.org/software/gawk/'
-  version '4.2.0'
-  source_url 'https://ftp.gnu.org/gnu/gawk/gawk-4.2.0.tar.xz'
-  source_sha256 'd4f3cd31c001fd0ed52832d4fbfbdfeaa38ad541c182f80ff8fdf87324a6a9f2'
+  version '4.2.1'
+  source_url 'https://ftpmirror.gnu.org/gawk/gawk-4.2.1.tar.gz'
+  source_sha256 '2b23d51503b2df9a41aa6fddc6002ad7ebf2a386ac19dc1b6be0dd48b0acf6db'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '8fd6f5aef0be9a63f263c014a87116fffec221492a88fdaa8e73368a4ba65cc7',
-     armv7l: '8fd6f5aef0be9a63f263c014a87116fffec221492a88fdaa8e73368a4ba65cc7',
-       i686: 'd59963a75fee7fe41194ec51f3f800d3276577772152cead45f505cbaaed2ec3',
-     x86_64: '24f82a456dc1322f65116b54ce83a9e657a7272639b4579d021094e19eb073c3',
+    aarch64: '8a9bcb6324601c08e562c2a05b9986a4124922a666474ac9419aa144f21dda63',
+     armv7l: '8a9bcb6324601c08e562c2a05b9986a4124922a666474ac9419aa144f21dda63',
+       i686: '30522a5f3d71d4c8189d7ff2b10e09bcf0209e87f5b92ac0ecde62324c7776ff',
+     x86_64: '64faab8c251933f07df9b6dbfacad9983dafdb831168e6dc912a38cfdae4af41',
   })
 
   depends_on 'libsigsegv'
--- packages/mpc.rb
+++ packages/mpc.rb
@@ -3,17 +3,32 @@ require 'package'
 class Mpc < Package
   description 'Gnu Mpc is a C library for the arithmetic of complex numbers with arbitrarily high precision and correct rounding of the result.'
   homepage 'http://www.multiprecision.org/'
-  version '1.0.3-2'
+  version '1.1.0'
+  source_url 'https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz'
+  source_sha256 '6985c538143c1208dcb1ac42cedad6ff52e267b47e5f970183a3e75125b43c2e'
+
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew-cross/mpc-1.0.3-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew-cross/mpc-1.0.3-2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew-cross/mpc-1.0.3-2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew-cross/mpc-1.0.3-2-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mpc-1.1.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mpc-1.1.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mpc-1.1.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mpc-1.1.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '49795b457852098b34964c768bc4f5fa852c2c40e72f4e856437301f36edb3fd',
-     armv7l: '49795b457852098b34964c768bc4f5fa852c2c40e72f4e856437301f36edb3fd',
-       i686: 'efec8e5ab8dac926546dabda0ff8b32fe30d85ff2a7c1ab44bdb3d58ad49d19b',
-     x86_64: 'b9b26abf315efce1b826f58e70795493d7ed503652486bc3766ac54dfa5d0a35',
+    aarch64: '93e218f15597b7fcbd6291986597e4c391e521d4f71ac19d815267cffcdeaf07',
+     armv7l: '93e218f15597b7fcbd6291986597e4c391e521d4f71ac19d815267cffcdeaf07',
+       i686: 'c952ab3fc51d56c6b0140534627888769f3a1bde43a645b279b9ef1010c7080f',
+     x86_64: '1fe81b949e466649174009019f96fbc09ea5161dfc99f0d5e462da196b5b64a1',
   })
+
+  def self.build
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           '--disable-maintainer-mode'
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
 end
