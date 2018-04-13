ad7694ec7376d6f76d69ad987f1b784da6974a08
lovedheart <lovedheart@163.com>
2018-04-13 12:36:49 -0700

    [libnotify] fix dependencies
    Fix #2121

--- packages/libnotify.rb
+++ packages/libnotify.rb
@@ -7,19 +7,16 @@ class Libnotify < Package
   source_url 'https://github.com/GNOME/libnotify/archive/0.7.7.tar.gz'
   source_sha256 '8ff216938a47df591b454ad062465f6d91d7a83496a1ba8eb755df8952cf48f2'
 
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
-  
-  depends_on 'gdk_pixbuf'
-  depends_on 'gobject_introspection'
   depends_on 'gtk_doc'
   depends_on 'gtk3'
+  depends_on 'gnome_common'
 
   def self.build
-    system "sh autogen.sh"
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "./autogen.sh"
+    system "./configure",
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           "--disable-static"
     system "make"
   end
 
