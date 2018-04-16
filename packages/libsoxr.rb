729bcdaa57fe66a393617f58793a45d3500160fb
Ed Reel <edreel@gmail.com>
2018-04-16 10:42:49 -0500

    Update source_url of libsoxr package
    Add pre-built binaries

--- packages/libsoxr.rb
+++ packages/libsoxr.rb
@@ -3,9 +3,22 @@ require 'package'
 class Libsoxr < Package
   description 'High quality, one-dimensional sample-rate conversion library.'
   homepage 'https://sourceforge.net/projects/soxr/'
-  version '0.1.3'
-  source_url 'https://sourceforge.net/projects/soxr/files/soxr-0.1.3-Source.tar.xz'
+  version '0.1.3-1'
+  source_url 'http://downloads.sourceforge.net/project/soxr/soxr-0.1.3-Source.tar.xz'
   source_sha256 'b111c15fdc8c029989330ff559184198c161100a59312f5dc19ddeb9b5a15889'
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libsoxr-0.1.3-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libsoxr-0.1.3-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libsoxr-0.1.3-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libsoxr-0.1.3-1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'e4f9d134c15ee05ceb89a70f0b660dca0425ea4365d5c1f1fb288f55e314064c',
+     armv7l: 'e4f9d134c15ee05ceb89a70f0b660dca0425ea4365d5c1f1fb288f55e314064c',
+       i686: '37088510fc2195e185475bcaa0ffa778feae8e8157188047328d93164b446fce',
+     x86_64: '4a850aee3eedd80b601cb1d5bb06b6b06844a3a09423a22f264b7437fbf5eee1',
+  })
 
   def self.build
     if ARCH == "x86_64"
