7b31bc10068a1bb2366971adc57bd9c23b5ca05e
Ed Reel <edreel@gmail.com>
2018-04-20 00:32:49 -0500

    Update avocado from 57.0-1 to 60.0

--- packages/avocado.rb
+++ packages/avocado.rb
@@ -3,36 +3,36 @@ require 'package'
 class Avocado < Package
   description 'Avocado is a next generation testing framework inspired by Autotest and modern development tools such as git.'
   homepage 'http://avocado-framework.github.io/'
-  version '57.0-1'
-  source_url 'https://github.com/avocado-framework/avocado/archive/57.0.tar.gz'
-  source_sha256 'c49fdf0946eed445fd8397354db3491869389ed4578ba477d447f105c99f15e4'
+  version '60.0'
+  source_url 'https://github.com/avocado-framework/avocado/archive/60.0.tar.gz'
+  source_sha256 'cadafe363a315a1dd3b332f66407da68748bcbe534153e20c59d588daa83a4ce'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-57.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-57.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-57.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-57.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-60.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-60.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-60.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-60.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '7145a4b0c550c49abd7045d9964f49b35491fa17828339a5ee709daeebafd913',
-     armv7l: '7145a4b0c550c49abd7045d9964f49b35491fa17828339a5ee709daeebafd913',
-       i686: 'bc5a0fa2e7c18c382d69ad7be7f141074729c12e9723a7ce49a36b8586af937b',
-     x86_64: 'c742ec98b24cf17d6709edecfa028a9ebedf80863fff6d075ef78e2dd4c1a632',
+    aarch64: 'f434f34711084c9e425b6c6ea9f6a3412274cb41b2ab59027bc3919c811a2c7a',
+     armv7l: 'f434f34711084c9e425b6c6ea9f6a3412274cb41b2ab59027bc3919c811a2c7a',
+       i686: 'f8284b6aa7b57571759724904979a0052ff42abd0009b2b785c03440ad365437',
+     x86_64: 'c8db96fe48bdade9f974931f6eac557f7e2d65292a335449b4fdbc6c47b4ee53',
   })
 
   depends_on 'python27' unless File.exists? "#{CREW_PREFIX}/bin/pip"
   depends_on 'xzutils'
 
   def self.install
-    system "pip install avocado-framework --root #{CREW_DEST_PREFIX} --prefix #{CREW_DEST_PREFIX}"
-    system "pip install -r https://raw.githubusercontent.com/avocado-framework/avocado/master/requirements.txt --root #{CREW_DEST_PREFIX} --prefix #{CREW_DEST_PREFIX}"
-    system "mv #{CREW_DEST_PREFIX}/usr/libexec #{CREW_DEST_PREFIX}"
-    system "mv #{CREW_DEST_PREFIX}/usr/share #{CREW_DEST_PREFIX}"
-    system "mv #{CREW_DEST_PREFIX}#{CREW_DEST_PREFIX}/* #{CREW_DEST_PREFIX}"
-    system "rm -rf #{CREW_DEST_PREFIX}#{CREW_PREFIX}/tmp"
+    system "pip install avocado-framework -r requirements.txt --root #{CREW_DEST_DIR} --prefix #{CREW_PREFIX}"
     system "mkdir -p $HOME/.config/avocado"
     system "ln -sf #{CREW_PREFIX}/etc/avocado/avocado.conf $HOME/.config/avocado/avocado.conf"
     system "mkdir -p #{CREW_DEST_DIR}/$HOME/.config/avocado"
     system "ln -sf #{CREW_PREFIX}/etc/avocado/avocado.conf #{CREW_DEST_DIR}/$HOME/.config/avocado/avocado.conf"
+  end
+
+  def self.postinstall
+    # For some reason, avocado is not executable.
+    system "chmod +x #{CREW_PREFIX}/bin/avocado"
   end
 end
