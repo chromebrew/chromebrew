08f7fecbc3d6aac524a6fd14d06ee9d554f46317
Ed Reel <edreel@gmail.com>
2018-04-20 23:43:47 -0500

    Update aws from 1.14.32 to 1.15.6
    Add pre-built binaries

--- packages/aws.rb
+++ packages/aws.rb
@@ -3,25 +3,25 @@ require 'package'
 class Aws < Package
   description 'The AWS CLI is an open source tool built on top of the AWS SDK for Python (Boto) that provides commands for interacting with AWS services.'
   homepage 'https://aws.amazon.com/documentation/cli/'
-  version '1.14.32'
-  source_url 'https://github.com/aws/aws-cli/archive/1.14.32.tar.gz'
-  source_sha256 'de84cef870b78198168404af27c7ab511dc27b0c28e1be6e4ed77fd8dbd4b78c'
+  version '1.15.6'
+  source_url 'https://github.com/aws/aws-cli/archive/1.15.6.tar.gz'
+  source_sha256 '8dbc72ba35802579c99a18a0821932c15cd44e3456aaab986f3e68824e4722b6'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.14.32-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.14.32-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.14.32-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.14.32-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.15.6-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.15.6-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.15.6-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/aws-1.15.6-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '70d4d81009a0948e2d8069df794c1b52c7864f6918dd78146e30a1ff55c3bb2f',
-     armv7l: '70d4d81009a0948e2d8069df794c1b52c7864f6918dd78146e30a1ff55c3bb2f',
-       i686: '703001f046ae3ef9e8dfab13e968ae11ece54d737916110dcd7c87ffb25a5fb5',
-     x86_64: 'd79bce821399bb5edd1716bbca98b0d9fe83b77e3766859aad820d4b3ef9132d',
+    aarch64: '4b94294abde4f05a2be26b748036400c456322c303ba274c5f3dcc08be879a05',
+     armv7l: '4b94294abde4f05a2be26b748036400c456322c303ba274c5f3dcc08be879a05',
+       i686: '78c0ad96c3d70e0d0124fd458a78a4221477fe659dd3966d3ab14cbd4b9f7fd2',
+     x86_64: 'f8a8b52a993aefe3f601badaba08a676ca53486e83484feda732bc3916636c1b',
   })
 
   depends_on 'python27' unless File.exists? "#{CREW_PREFIX}/bin/python"
-  depends_on 'unzip'
+  depends_on 'unzip' => :build
 
   def self.install
     system "wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip"
