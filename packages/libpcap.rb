b11907fca016c75c33be3ec2fa61e1e7a6e13422
Ed Reel <edreel@gmail.com>
2018-04-24 00:03:13 -0500

    Add pre-built binaries for aarch64/armv7l

--- packages/libpcap.rb
+++ packages/libpcap.rb
@@ -8,16 +8,16 @@ class Libpcap < Package
   source_sha256 '673dbc69fdc3f5a86fb5759ab19899039a8e5e6c631749e48dcd9c6f0c83541e'
 
   binary_url ({
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libpcap-1.8.1-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libpcap-1.8.1-0-chromeos-x86_64.tar.xz',
     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libpcap-1.8.1-0-chromeos-armv7l.tar.xz',
      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libpcap-1.8.1-0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libpcap-1.8.1-0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libpcap-1.8.1-0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '71b746ae9ee9b4240ae99577ff92fc5ce7f637f5191379787842be68baf0a7e2',
+     armv7l: '71b746ae9ee9b4240ae99577ff92fc5ce7f637f5191379787842be68baf0a7e2',
        i686: '6b0bdf1e1e244d3eac8ade1a2ba5f4442af97567a785638b6fc7619a06381465',
      x86_64: 'f6b91e914ff739c6b17160da29566af9746b4c38ad7ff24394b43594d62eb8b6',
-    aarch64: 'not',
-     armv7l: 'not',
   })
 
   def self.build
