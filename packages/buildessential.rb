5e48c514613c84c75a237ca602afe1baabcb7a96
lovedheart <lovedheart@163.com>
2018-04-24 07:07:42 +0200

    [buildessential] update gcc version to 7.3.0 (#2277)
    * [buildessential] update gcc version to 7.3.0
    
    * upgrade gcc to 7.3.0, please remove gcc first

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,11 +3,11 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage ''
-  version '1.0-3'
+  version '1.1'
 
   is_fake
 
-  depends_on 'gcc'
+  depends_on 'gcc7'
   depends_on 'linuxheaders'
   depends_on 'make'
   depends_on 'pkgconfig'
