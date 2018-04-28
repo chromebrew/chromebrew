224ef1f93e462bddaed2ca24f6a52b6fff4ebc80
Ed Reel <edreel@gmail.com>
2018-04-28 15:12:47 -0500

    Update multitail from 6.3 to 6.4.2-1
    Add pre-built binaries

--- packages/multitail.rb
+++ packages/multitail.rb
@@ -3,21 +3,21 @@ require 'package'
 class Multitail < Package
   description 'MultiTail allows you to monitor logfiles and command output in multiple windows in a terminal, colorize, filter and merge.'
   homepage 'https://www.vanheusden.com/multitail/'
-  version '6.4.3'
-  source_url 'https://github.com/flok99/multitail/archive/v6.3.tar.gz'
-  source_sha256 'bbc478e562b66e87f1b7191864702314de33ecf6ce08155b6a955bd25242a4f8'
+  version '6.4.2-1'
+  source_url 'https://www.vanheusden.com/multitail/multitail-6.4.2.tgz'
+  source_sha256 'af1d5458a78ad3b747c5eeb135b19bdca281ce414cefdc6ea0cff6d913caa1fd'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.2-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.2-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.2-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/multitail-6.4.2-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'd77ae3e6dd7e70ae0e96e8fa8df966379d239a27095329af54dc43c813f675c2',
-     armv7l: 'd77ae3e6dd7e70ae0e96e8fa8df966379d239a27095329af54dc43c813f675c2',
-       i686: '5d91c88fc441f62ba9a6ba8fcc61a2c94c927fe1b4b0d12a8cb9d9665ec108ee',
-     x86_64: '8a9f738f63365e9902482077ff6ae70272e57313595b75a75414feb2339f8c00',
+    aarch64: 'e82af7ce211b54d03d0938a27af1c9471c857440e81a88cd4b5188dd63913a27',
+     armv7l: 'e82af7ce211b54d03d0938a27af1c9471c857440e81a88cd4b5188dd63913a27',
+       i686: '553c789667253e18f656dfec111eaa05c9b95e69ee06ebcfec0488655ac00434',
+     x86_64: '2059e4345609cbd3c45b40b5f741dc294e26f58f44e52b424f85c07ff3375ec7',
   })
 
   depends_on 'ncurses'
