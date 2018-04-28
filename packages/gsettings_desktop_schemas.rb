ed26e6cc4951d98eb07bae8a2c7daf3f3b7e9a43
Ed Reel <edreel@gmail.com>
2018-04-28 00:09:36 -0500

    Add pre-built binaries for packages a-y

--- packages/gsettings_desktop_schemas.rb
+++ packages/gsettings_desktop_schemas.rb
@@ -7,6 +7,19 @@ class Gsettings_desktop_schemas < Package
   source_url 'https://github.com/GNOME/gsettings-desktop-schemas/archive/3.28.0.tar.gz'
   source_sha256 '7e816d73a6a206c470adaf94a5a713e7f7d1b50f97615d017d19dfdd1e23394d'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gsettings_desktop_schemas-3.28.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gsettings_desktop_schemas-3.28.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gsettings_desktop_schemas-3.28.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gsettings_desktop_schemas-3.28.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'de624dd7458ac173f505dca408ca47c02a78ab71aecdcd2246b7779f823f763e',
+     armv7l: 'de624dd7458ac173f505dca408ca47c02a78ab71aecdcd2246b7779f823f763e',
+       i686: '31c292efcca1f8aa253be2d85a5686d0c0e59e793dfd8505c9e71b661bcc5593',
+     x86_64: 'ff3bcf36058e651fe7662433fb961d9c1dc1f8db5ab17c11788ee4c3c26c99f2',
+  })
+
   depends_on 'gnome_common'
   depends_on 'glib'
   depends_on 'gobject_introspection'
--- packages/libproxy.rb
+++ packages/libproxy.rb
@@ -8,11 +8,17 @@ class Libproxy < Package
   source_sha256 '654db464120c9534654590b6683c7fa3887b3dad0ca1c4cd412af24fbfca6d4f'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libproxy-0.4.15-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libproxy-0.4.15-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libproxy-0.4.15-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libproxy-0.4.15-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'f0df6d6fbe49b9783ebe03bf922cb917714358dc4380c44dfa9b5bbdc747d831',
+     armv7l: 'f0df6d6fbe49b9783ebe03bf922cb917714358dc4380c44dfa9b5bbdc747d831',
+       i686: '00c146655e1bf4c437974ad59549b900924ac93f7de277477c196c56c44b7c73',
+     x86_64: '896073cbf37b750da1856e37c58111f4c1350196ddb6fef5ce7799197d7a9f27',
   })
-
-  depends_on 'cmake' => :build
 
   def self.build
     system './autogen.sh'
