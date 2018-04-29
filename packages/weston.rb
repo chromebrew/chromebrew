0aa50ad5a6ebc9eedec802744580fd52092dd0d0
lovedheart <lovedheart@163.com>
2018-04-29 21:56:10 +0200

    [weston] upgrade to 4.0.0 (#2299)

--- packages/weston.rb
+++ packages/weston.rb
@@ -3,22 +3,9 @@ require 'package'
 class Weston < Package
   description 'Weston is the reference implementation of a Wayland compositor, and a useful compositor in its own right.'
   homepage 'http://wayland.freedesktop.org'
-  version '3.0.91'
-  source_url 'https://github.com/wayland-project/weston/archive/3.0.91.tar.gz'
-  source_sha256 'a27eecd9dd9a18131292d33eba37f35369ede5f3b0688154244e3238b27f854a'
-
-  binary_url ({
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/weston-3.0.91-chromeos-x86_64.tar.xz',
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/weston-3.0.91-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/weston-3.0.91-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/weston-3.0.91-chromeos-i686.tar.xz',
-  })
-  binary_sha256 ({
-     x86_64: '6ce064ae0d5b4282858d436d24e902f2f690d2bb7f1acca576013c201c4b9c42',
-    aarch64: '07fd4523ff123b53271ac100d5b2bee8edd40a1e11026cbfc19ab2671179f7f4',
-     armv7l: '07fd4523ff123b53271ac100d5b2bee8edd40a1e11026cbfc19ab2671179f7f4',
-       i686: '1bf7ac234043bfad8877e0eea7f58ca20487d1464a865e2ab12e877404ebe8e1',
-  })
+  version '4.0.0'
+  source_url 'https://github.com/wayland-project/weston/archive/4.0.0.tar.gz'
+  source_sha256 '46b0178cd37b0bf7471c9af12c847c7b8728699ecd5c04ce79be77ea12c98512'
 
   depends_on 'harfbuzz'
   depends_on 'libxcursor'
@@ -31,14 +18,16 @@ class Weston < Package
     
   depends_on 'pango'
   depends_on 'dbus'
+  
+  depends_on 'libxxf86vm'
 
-    
   def self.build
-    system "./autogen.sh"  
-    system "./configure \
-            --prefix=#{CREW_PREFIX} \
-            --libdir=#{CREW_LIB_PREFIX} \
-            --disable-weston-launch"    
+    system "./autogen.sh"
+    system "./configure",
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           "--disable-weston-launch",
+           "--enable-demo-clients-install"
     system "make"
   end
 
@@ -46,3 +35,4 @@ class Weston < Package
     system "make DESTDIR=#{CREW_DEST_DIR} install"
   end
 end
+
