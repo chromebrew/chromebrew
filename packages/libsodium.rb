647cc2771147852ee696e07cc1e3a0aff533bd56
Daniel Baldwin <danielcbaldwin@gmail.com>
2018-05-04 20:49:16 +0000

    Add libsodium package. A modern, portable, easy to use crypto library https://libsodium.org

---
+++ packages/libsodium.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Libsodium < Package
+  description 'A modern, portable, easy to use crypto library'
+  homepage 'https://libsodium.org'
+  version '1.0.16'
+  source_url 'https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz'
+  source_sha256 'eeadc7e1e1bcef09680fb4837d448fbdf57224978f865ac1c16745868fbd0533'
+
+  def self.build
+    system "./configure", "--prefix=#{CREW_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make", "check"
+  end
+end
