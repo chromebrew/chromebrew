b634066dc5eb129afbd00e2a92fc62a0f4daaf5d
Daniel Baldwin <danielcbaldwin@gmail.com>
2018-05-04 15:02:09 -0600

    Added libdir as per @uberhacker

--- packages/libsodium.rb
+++ packages/libsodium.rb
@@ -8,7 +8,7 @@ class Libsodium < Package
   source_sha256 'eeadc7e1e1bcef09680fb4837d448fbdf57224978f865ac1c16745868fbd0533'
 
   def self.build
-    system "./configure", "--prefix=#{CREW_PREFIX}"
+    system "./configure", "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
 
