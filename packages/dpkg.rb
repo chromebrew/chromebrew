845196de9e760a348236312bc4f594750df64e8c
James Larrowe <larrowe.semaj11@gmail.com>
2018-05-12 16:17:40 +0000

    Too many changes to be listed in a commit. See for yourself [here.](https://github.com/JL2210/chromebrew/tree/dpkg/packages/dpkg.rb)

--- packages/dpkg.rb
+++ packages/dpkg.rb
@@ -24,7 +24,14 @@ class Dpkg < Package
       system "make install DESTDIR=#{CREW_DEST_DIR}"
       system "mkdir -p #{CREW_DEST_PREFIX}/usr/"
       system "ln -s #{CREW_PREFIX} #{CREW_DEST_PREFIX}/usr/local"
-      system "touch #{CREW_DEST_PREFIX}/var/lib/dpkg/status"
+      system "echo >> /usr/local/var/lib/dpkg/status"
     end
+  end
+    def self.postinstall
+    puts "You may need to run" 
+    puts "'sudo chromeos-firmwareupdate --mode=todev',"
+    puts "followed by 'sudo /usr/share/vboot/bin/make_dev_ssd.sh --remove_rootfs_verification',"
+    puts "follow the instructions, and restart your Chromebook before installing packages."
+    puts "However, this isn't necessary if you aren't going to use dpkg -i."
   end
 end
