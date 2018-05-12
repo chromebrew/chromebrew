9b2cc9bad49c1842f11c046299b884157af1149c
James Larrowe <larrowe.semaj11@gmail.com>
2018-05-12 14:17:12 +0000

    Fixed file format

--- packages/dpkg.rb
+++ packages/dpkg.rb
@@ -7,21 +7,23 @@ class Dpkg < Package
   source_url 'https://salsa.debian.org/dpkg-team/dpkg/-/archive/1.19.0.5/dpkg-1.19.0.5.tar.gz'
   source_sha256 'd38308afcd5d7896bbd1f946875b90f9d8510a8a96b44e4f14e781285e5d9641'
 
-    depends_on 'icu4c' => :build
+  depends_on 'icu4c' => :build
 
-    def self.build
-          system "git clone https://salsa.debian.org/dpkg-team/dpkg.git"
-  Dir.chdir ("dpkg")
+  def self.build
+    system "git clone https://salsa.debian.org/dpkg-team/dpkg.git"
+    Dir.chdir ("dpkg")
       system "git checkout 1.19.0.5"
       system "autoreconf -i -f"
       system "./configure --libdir=#{CREW_LIB_PREFIX} --prefix=#{CREW_PREFIX}"
       system "make"
+    end
   end
 
   def self.install
-      Dir.chdir ("dpkg")
+    Dir.chdir ("dpkg")
       system "make install DESTDIR=#{CREW_DEST_DIR}"
       system "mkdir -p #{CREW_DEST_PREFIX}/usr/"
       system "ln -s #{CREW_PREFIX} #{CREW_DEST_PREFIX}/usr/local"
+    end
   end
 end
