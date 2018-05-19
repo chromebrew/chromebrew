9a88bbed528cbd0920dfdf6e12daa8582d868e60
James Larrowe <larrowe.semaj11@gmail.com>
2018-05-19 15:01:59 +0000

    Added fakeroot package

---
+++ packages/fakeroot.rb
@@ -0,0 +1,21 @@
+require 'package'
+
+class Fakeroot < Package
+  description 'Run a command in an environment faking root privileges for file manipulation.'
+  homepage 'https://wiki.debian.org/FakeRoot'
+  version '1.22'
+  source_url 'http://ftp.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.22.orig.tar.bz2'
+  source_sha256 'bd806a4a1e641203eb3d4571a10089e8a038c10ec7e492fa1e061b03ae3ec6fe'
+
+  depends_on 'libcap' => :build
+  
+  def self.build
+    system "./bootstrap"
+    system "./configure", "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_DIR}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
