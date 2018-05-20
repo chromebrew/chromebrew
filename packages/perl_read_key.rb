aba48453a06f11bc09c86eb5c851fca57c197a24
James Larrowe <larrowe.semaj11@gmail.com>
2018-05-20 16:15:35 +0000

    Added package perl_read_key

---
+++ packages/perl_read_key.rb
@@ -0,0 +1,19 @@
+require 'package'
+
+class Perl_read_key < Package
+  description 'Character mode terminal access for Perl Term::ReadKey'
+  homepage 'https://metacpan.org/source/JSTOWE/TermReadKey-2.37/'
+  version '2.37'
+  source_url 'https://github.com/jonathanstowe/TermReadKey/archive/v2.37.tar.gz'
+  source_sha256 '0fa4fb2f8145e3fb2c2129ad28d55be175abcc258f239ba8ddc2cd83790aa8fb'
+
+  def self.build
+    system "perl", "Makefile.PL"
+    system "make"
+    system "perl", "ReadKey_pm.PL"
+  end
+
+  def self.install
+    system "make", "install"
+  end
+end
