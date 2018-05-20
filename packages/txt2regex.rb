9a8131399daea1a27ecf67d856c3542c0bd5be73
Ed Reel <edreel@gmail.com>
2018-05-20 06:18:24 +0000

    Add txt2regex package

---
+++ packages/txt2regex.rb
@@ -0,0 +1,13 @@
+require 'package'
+
+class Txt2regex < Package
+  description 'Regex wizard for the terminal, written in Bash'
+  homepage 'http://aurelio.net/projects/txt2regex/'
+  version '0.8'
+  source_url 'https://github.com/aureliojargas/txt2regex/archive/v0.8.tar.gz'
+  source_sha256 '31a83516db6c88c07a3d5440136d4fe3928090f0d8b59cb127ca611d6bb9dda9'
+
+  def self.install
+    system "install -Dm755 txt2regex.sh #{CREW_DEST_PREFIX}/bin/txt2regex"
+  end
+end
