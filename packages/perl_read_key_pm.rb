589e9880dde301e86010e40d74b6d53703b42a04
James Larrowe <larrowe.semaj11@gmail.com>
2018-05-21 20:44:07 +0000

    Added another package, perl_read_key_pm, that incorporates PerlReadkey_pm.PL

--- packages/perl_read_key_pm.rb
+++ packages/perl_read_key_pm.rb
@@ -1,23 +1,23 @@
 require 'package'
 
-class Perl_read_key < Package
-  description 'Dependency, fixes perl_read_key_pm'
+class Perl_read_key_pm < Package
+  description 'Character mode terminal access for Perl Term::ReadKey'
   homepage 'https://metacpan.org/source/JSTOWE/TermReadKey-2.37/'
   version '2.37'
   source_url 'https://github.com/jonathanstowe/TermReadKey/archive/v2.37.tar.gz'
   source_sha256 '0fa4fb2f8145e3fb2c2129ad28d55be175abcc258f239ba8ddc2cd83790aa8fb'
 
   def self.build
+    system "crew", "install", "perl_read_key"
+    system "perl", "Makefile.PL"
+    system "make", "uninstall"
+    system "perl", "ReadKey_pm.PL"
     system "perl", "Makefile.PL"
     system "make"
+    system "crew", "remove", "perl_read_key"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
-
-  def self.postinstall
-    puts "Please install perl_read_key_pm after installing this package,"
-    puts "This just fills in dependencies so it can compile correctly."
-    puts "If you are currently installing perl_read_key_pm, you are OK."
 end
