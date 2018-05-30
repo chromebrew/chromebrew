2c4dbcb8bebdaf3da4c0021fb7de57ec76cd5749
James Larrowe <larrowe.semaj11@gmail.com>
2018-05-30 12:52:41 -0500

    Removed version bump for glibc headers

--- packages/glibc_headers.rb
+++ packages/glibc_headers.rb
@@ -3,7 +3,7 @@ require 'package'
 class Glibc_headers < Package
   description 'GNU C Library Headers'
   homepage 'https://www.gnu.org/software/libc/'
-  version '2.27-0'
+  version '2.27'
   source_url 'https://ftpmirror.gnu.org/glibc/glibc-2.27.tar.xz'
   source_sha256 '5172de54318ec0b7f2735e5a91d908afe1c9ca291fec16b5374d9faadfc1fc72'
 
