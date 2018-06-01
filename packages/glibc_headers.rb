56ee1ed0915698bee3fd11523ce759f419c2f938
James Larrowe <larrowe.semaj11@gmail.com>
2018-06-01 17:04:36 -0500

    Added strings/endian.h and strings/sys to include

--- packages/glibc_headers.rb
+++ packages/glibc_headers.rb
@@ -15,6 +15,7 @@ class Glibc_headers < Package
     system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/posix"
     system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/wcsmbs"
     system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/time"
+    system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/string"
     system "cp", "-r", "include", "#{CREW_DEST_PREFIX}"
     system "cp", "-r", "bits", "#{CREW_DEST_PREFIX}/include/"
     system "cp", "-r", "stdlib/errno.h", "#{CREW_DEST_PREFIX}/include/stdlib/"
@@ -25,5 +26,6 @@ class Glibc_headers < Package
     system "cp", "-r", "posix/sys", "#{CREW_DEST_PREFIX}/include/posix/"
     system "cp", "-r", "time/bits", "#{CREW_DEST_PREFIX}/include/time/"
     system "cp", "-r", "time/sys", "#{CREW_DEST_PREFIX}/include/time/"
+    system "cp", "-r", "string/endian.h", "#{CREW_DEST_PREFIX}/include/string/"
   end
 end
