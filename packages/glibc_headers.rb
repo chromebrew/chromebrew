ce11a7224832335f93b0f2f183216eda91eb1f05
James Larrowe <larrowe.semaj11@gmail.com>
2018-06-02 18:57:28 -0400

    Changed version for glibc_headersto 2.24 for compatibility with older versions of glibc

--- packages/glibc_headers.rb
+++ packages/glibc_headers.rb
@@ -3,9 +3,9 @@ require 'package'
 class Glibc_headers < Package
   description 'GNU C Library Headers'
   homepage 'https://www.gnu.org/software/libc/'
-  version '2.27'
-  source_url 'https://ftpmirror.gnu.org/glibc/glibc-2.27.tar.xz'
-  source_sha256 '5172de54318ec0b7f2735e5a91d908afe1c9ca291fec16b5374d9faadfc1fc72'
+  version '2.24'
+  source_url 'https://ftpmirror.gnu.org/glibc/glibc-2.24.tar.xz'
+  source_sha256 '99d4a3e8efd144d71488e478f62587578c0f4e1fa0b4eed47ee3d4975ebeb5d3'
 
   depends_on 'libc6'
 
