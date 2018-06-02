1fcfab7ba0186c31fa549eb3031446e26aa1439e
James Larrowe <larrowe.semaj11@gmail.com>
2018-06-01 19:21:16 -0500

    Added more header files

--- packages/glibc_headers.rb
+++ packages/glibc_headers.rb
@@ -17,6 +17,9 @@ class Glibc_headers < Package
     system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/time"
     system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/string"
     system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/locale"
+    system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/intl"
+    system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/ctype"
+    system "mkdir", "-p", "#{CREW_DEST_PREFIX}/include/signal"
     system "cp", "-r", "include", "#{CREW_DEST_PREFIX}"
     system "cp", "-r", "bits", "#{CREW_DEST_PREFIX}/include/"
     system "cp", "-r", "stdlib/errno.h", "#{CREW_DEST_PREFIX}/include/stdlib/"
@@ -32,8 +35,13 @@ class Glibc_headers < Package
     system "cp", "-r", "string/endian.h", "#{CREW_DEST_PREFIX}/include/string/"
     system "cp", "-r", "string/bits", "#{CREW_DEST_PREFIX}/include/string/"
     system "cp", "-r", "locale/bits", "#{CREW_DEST_PREFIX}/include/locale/"
+    system "cp", "-r", "locale/locale.h", "#{CREW_DEST_PREFIX}/include/locale/"
     system "cp", "-r", "sysdeps/generic/string_private.h", "#{CREW_DEST_PREFIX}/include/"
     system "cp", "-r", "string/string.h", "#{CREW_DEST_PREFIX}/include/string/"
     system "cp", "-r", "string/strings.h", "#{CREW_DEST_PREFIX}/include/string/"
+    system "cp", "-r", "intl/libintl.h", "#{CREW_DEST_PREFIX}/include/intl/"
+    system "cp", "-r", "ctype/ctype.h", "#{CREW_DEST_PREFIX}/include/ctype/"
+    system "cp", "-r", "signal/signal.h", "#{CREW_DEST_PREFIX}/include/signal/"
+    system "cp", "-r", "signal/bits", "#{CREW_DEST_PREFIX}/include/signal/"
   end
 end
