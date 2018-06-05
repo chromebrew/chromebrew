b259c571a61598eb1aa660368f509a74d9732c24
James Larrowe <larrowe.semaj11@gmail.com>
2018-06-05 17:32:58 -0500

    Created glibc_headers package with headers from glibc223

--- packages/glibc_headers.rb
+++ packages/glibc_headers.rb
@@ -4,17 +4,16 @@ class Glibc_headers < Package
   description 'GNU C Library Headers'
   homepage 'https://www.gnu.org/software/libc/'
   version '2.23'
-  source_url 'https://ftpmirror.gnu.org/glibc/glibc-2.24.tar.xz'
-  source_sha256 '99d4a3e8efd144d71488e478f62587578c0f4e1fa0b4eed47ee3d4975ebeb5d3'
-
-  depends_on 'libc6'
+  source_url 'https://github.com/skycocker/chromebrew/files/2059646/bashrc.txt'
+  source_sha256 '17346974f26e49b1261fb35ef51261fbcc9043844f63eb736ec3c2f2e0453ec2'
 
   def self.build
     system "crew", "download", "glibc223"
-    system "tar", "-xf", "glibc223-2.23-3-chromeos-x86_64.tar.xz", "-C", "."
+    system "tar", "-xf", "#{CREW_BREW_DIR}/glibc223-2.23-3-chromeos-x86_64.tar.xz", "-C", "."
   end
 
   def self.install
+    system "mkdir", "-p", "#{CREW_DEST_PREFIX}"
     Dir.chdir ("usr/local") do
       system "cp", "-r", "include", "#{CREW_DEST_PREFIX}/"
     end
