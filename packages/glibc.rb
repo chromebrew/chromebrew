66a10c6d8e9e7fd9c499d0cd4d55cf7c737a2c4a
Ed Reel <edreel@gmail.com>
2018-06-21 09:21:53 -0500

    Update for release 67 and above

--- packages/glibc.rb
+++ packages/glibc.rb
@@ -3,35 +3,17 @@ require 'package'
 class Glibc < Package
   description 'The GNU C Library project provides the core libraries for GNU/Linux systems.'
   homepage 'https://www.gnu.org/software/libc/'
-  version '1.1'
+  version '1.2'
 
   is_fake
 
-  if (File.exist? "/lib/libc-2.23.so") || (File.exist? "/lib64/libc-2.23.so")
-    depends_on 'glibc223'
-    # Check previous version
-    if File.exist? CREW_CONFIG_PATH + "meta/glibc219.filelist"
-      conflict_solve = "`crew remove glibc219`"
+  if CHROMEOS_RELEASE <= '66'
+    if (File.exist? '/lib/libc-2.23.so') || (File.exist? '/lib64/libc-2.23.so')
+      # Check previous version
+      if File.exist? CREW_CONFIG_PATH + 'meta/glibc219.filelist'
+        system 'crew remove glibc219'
+      end
+      depends_on 'glibc223'
     end
-  else
-    depends_on 'glibc219'
-  end
-
-  # Check old glibc
-  if File.exist? CREW_CONFIG_PATH + "meta/glibc.filelist"
-    if File.exist? CREW_CONFIG_PATH + "meta/glibc223.filelist"
-      # Already installed old glibc and glibc223, so need to remove both once
-      conflict_solve = "`crew remove glibc223 glibc`"
-    else
-      # Already installed old glibc, so need it once
-      conflict_solve = "`crew remove glibc`"
-    end
-  end
-
-  if conflict_solve
-    puts
-    puts "In order to install/upgrade glibc, please execute #{conflict_solve} first".lightblue
-    puts
-    exit 1
   end
 end
