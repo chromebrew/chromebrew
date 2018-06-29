b2560a9441a7f36180a28d32336f9e68874bf4ca
Lazar Doric <doriclazar@gmail.com>
2018-06-29 13:35:02 -0500

    Incremented vim version, and removed the binaries.

--- packages/vim.rb
+++ packages/vim.rb
@@ -3,22 +3,10 @@ require 'package'
 class Vim < Package
   description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient.'
   homepage 'http://www.vim.org/'
-  version '8.0-2'
+  version '8.0-3'
   source_url 'ftp://ftp.vim.org/pub/vim/unix/vim-8.0.tar.bz2'
   source_sha256 '08bd0d1dd30ece3cb9905ccd48b82b2f81c861696377508021265177dc153a61'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/vim-8.0-2-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '41c5462bc6a85758c826a3238d1e639beff5aeaad4cad6c279a8916b6d3ae21a',
-     armv7l: '41c5462bc6a85758c826a3238d1e639beff5aeaad4cad6c279a8916b6d3ae21a',
-       i686: 'c248117c8aba83f20c362999da8ec4b7c7b73aa74e6e4251a0dc114320809df9',
-     x86_64: 'c0b2c46297ce1b5d01c42352675e26c980c4f51e1816263150c4c0e99f1899be',
-  })
   depends_on 'compressdoc' => :build
   depends_on 'ncurses'
   # vim uses shared library of following languages, so need them isntalled at run-time
