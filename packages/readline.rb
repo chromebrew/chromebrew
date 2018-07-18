056e9252f6aaee22b7071f20bdfb660c194a0bb1
Ed Reel <edreel@gmail.com>
2018-07-18 11:55:34 -0700

    Clean up spacing and remove dependencies
    Symlink libhistory.so
    
    Add pre-built binaries

--- packages/readline.rb
+++ packages/readline.rb
@@ -8,16 +8,17 @@ class Readline < Package
   source_sha256 '56ba6071b9462f980c5a72ab0023893b65ba6debb4eeb475d7a563dc65cafd43'
 
   binary_url ({
-
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-6.3p8-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/readline-6.3p8-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/readline-6.3p8-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-6.3p8-1-chromeos-x86_64.tar.xz',
   })
-
   binary_sha256 ({
-
+    aarch64: 'bb0ad30615c12e9559fcc7158a87d76cda4f216b4ccd79e0434064cd071e197c',
+     armv7l: 'bb0ad30615c12e9559fcc7158a87d76cda4f216b4ccd79e0434064cd071e197c',
+       i686: 'a3015ee22758606e1bb85e4add62335bb6ce36da2cb99805da051609b04843c6',
+     x86_64: '77227d45ffa89f861ddbc7dba8433c0a8401333ab527d11c0c809f81de53d567',
   })
-
-  depends_on 'buildessential' => :build
-  depends_on 'patch' => :build
-  depends_on 'ncurses'
 
   def self.build
     system "wget -r -N -nd --no-parent ftp://ftp.gnu.org/gnu/readline/readline-6.3-patches/readline63-001 -P readline-6.3-patches"
@@ -37,9 +38,12 @@ class Readline < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-   FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}"
+    FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}"
     system "ln", "-sf", "/#{ARCH_LIB}/libreadline.so.6.3", "#{CREW_DEST_LIB_PREFIX}/libreadline.so.6"
     system "ln", "-sf", "/#{ARCH_LIB}/libreadline.so.6.3", "#{CREW_DEST_LIB_PREFIX}/libreadline.so.6.3"
     system "ln", "-sf", "/#{ARCH_LIB}/libreadline.so.6.3", "#{CREW_DEST_LIB_PREFIX}/libreadline.so"
+    system "ln", "-sf", "/#{ARCH_LIB}/libhistory.so.6.3", "#{CREW_DEST_LIB_PREFIX}/libhistory.so.6"
+    system "ln", "-sf", "/#{ARCH_LIB}/libhistory.so.6.3", "#{CREW_DEST_LIB_PREFIX}/libhistory.so.6.3"
+    system "ln", "-sf", "/#{ARCH_LIB}/libhistory.so.6.3", "#{CREW_DEST_LIB_PREFIX}/libhistory.so"
   end
 end
