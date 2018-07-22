23091676138072af2f78c14368d25458e3e22c89
James Larrowe <larrowe.semaj11@gmail.com>
2018-07-21 21:19:43 -0400

    Changed install.sh to a static version of README.md

--- packages/crew_profile.rb
+++ packages/crew_profile.rb
@@ -4,8 +4,8 @@ class Crew_profile < Package
   description 'A profile for Chromebrew, to be used instead of ~/.*rc'
   homepage 'https://github.com/skycocker/chromebrew'
   version '0.4.3'
-  source_url 'https://raw.githubusercontent.com/skycocker/chromebrew/master/install.sh'
-  source_sha256 '6c061009df229624485279e8a076f94b61218b8dc67478d49a6b563692e92aef'
+  source_url 'https://github.com/skycocker/chromebrew/raw/511ee9cb3138e113df74ddf0dee057ef5d9a46fd/README.md'
+  source_sha256 '27b201cec82d903a1856972e6d7ff1ac58a67c761d729ecd7fd14f24fa9d9901'
 
   def self.postinstall
     puts
