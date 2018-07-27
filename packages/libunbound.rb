b2a58089aa6124eb183bf1f1c594dd10e8ddfba5
Ed Reel <edreel@gmail.com>
2018-07-26 21:59:19 -0700

    Update libunbound from 1.7.0 to 1.7.3
    Add pre-built binaries

--- packages/libunbound.rb
+++ packages/libunbound.rb
@@ -3,27 +3,22 @@ require 'package'
 class Libunbound < Package
   description 'Unbound is a validating, recursive, and caching DNS resolver.'
   homepage 'https://www.unbound.net/'
-  version '1.7.0'
-  source_url 'https://www.unbound.net/downloads/unbound-1.7.0.tar.gz'
-  source_sha256 '94dd9071fb13d8ccd122a3ac67c4524a3324d0e771fc7a8a7c49af8abfb926a2'
+  version '1.7.3'
+  source_url 'https://www.unbound.net/downloads/unbound-1.7.3.tar.gz'
+  source_sha256 'c11de115d928a6b48b2165e0214402a7a7da313cd479203a7ce7a8b62cba602d'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '4444835bc92d3e88e94ad6f1e9599f4c60a2843d206d2a2b15a31460816f76d5',
-     armv7l: '4444835bc92d3e88e94ad6f1e9599f4c60a2843d206d2a2b15a31460816f76d5',
-       i686: '3a2daf9725731c9aa89b3fd495e8fc8e87ff66a0906d8cd5c2ac120860f3e63b',
-     x86_64: '54e53fa1bb5a673611048619d031cf57ef2dc259b4265ade626b661b71a91864',
+    aarch64: '27f917465295015394aed113d07edde320ce977185c55883a875fccfb269d47b',
+     armv7l: '27f917465295015394aed113d07edde320ce977185c55883a875fccfb269d47b',
+       i686: 'e5765e59f11ee3aa5ada3a9571607cdaef2522de2986c74e895063e6e04353c9',
+     x86_64: '3cd8a50cc5ffd19a56d086e5990590eac27f32e78099bf75288902078f487e9f',
   })
-
-  depends_on 'flex' => :build
-  depends_on 'bison' => :build
-  depends_on 'gawk' => :build
-  depends_on 'expat'
 
   def self.build
     system './configure',
