793a76bcd349dcae90dfbadec503a197230df92a
Ed Reel <edreel@gmail.com>
2018-07-30 07:57:01 -0500

    Add pre-built binaries for libglade

--- packages/libglade.rb
+++ packages/libglade.rb
@@ -8,8 +8,16 @@ class Libglade < Package
   source_sha256 '64361e7647839d36ed8336d992fd210d3e8139882269bed47dc4674980165dec'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libglade-2.6.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libglade-2.6.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libglade-2.6.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libglade-2.6.4-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '9f211b42969c6d791e8d61f13aad15251e3991c0270c251810eda6c5fbdb550e',
+     armv7l: '9f211b42969c6d791e8d61f13aad15251e3991c0270c251810eda6c5fbdb550e',
+       i686: '13920b24507ee60dc5f4c8501dd89305a05797b030afe25af6d3771595b83e56',
+     x86_64: '2749ac0d67edb91b1f59355fba8dd045c8dbc44ede3fc619304853b2ac4799cf',
   })
 
   depends_on 'gtk2'
