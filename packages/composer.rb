2a2db6171ff7322b32cb26059c6ac55e6f2b9e15
Ed Reel <edreel@gmail.com>
2018-08-05 12:02:09 -0500

    Update composer from 1.6.5 to 1.7.0 (#2510)

--- packages/composer.rb
+++ packages/composer.rb
@@ -3,29 +3,43 @@ require 'package'
 class Composer < Package
   description 'Dependency Manager for PHP'
   homepage 'https://getcomposer.org/'
-  version '1.6.5'
-  source_url 'https://github.com/composer/composer/archive/1.6.5.tar.gz'
-  source_sha256 'da4f81a53f845ed0ac4d77fb6d57e3644002b660f4c31cdd14a5a9c340db9776'
+  version '1.7.0'
+  source_url 'https://github.com/composer/composer/archive/1.7.0.tar.gz'
+  source_sha256 '08fd8538872f59c666d4c0a5596e8aff65ddb76900f615e1480d1e8803563adc'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/composer-1.6.5-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/composer-1.6.5-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/composer-1.6.5-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/composer-1.6.5-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '13b02d5d54d773cc03d6f94392151d5957364b76350a8e991a1a3e5762a45c16',
-     armv7l: '13b02d5d54d773cc03d6f94392151d5957364b76350a8e991a1a3e5762a45c16',
-       i686: '0451a78d65547a48f3056ae78ffb8d6452f05ca6acaaf91f75462ee91949e640',
-     x86_64: 'd010d91133dd3097ed323f79a3314d110ce04791c3f3c9e31660b7306aa38c31',
   })
 
   depends_on 'php7' unless File.exists? "#{CREW_PREFIX}/bin/php"
+  depends_on 'xdg_base'
+
+  def self.preinstall
+    if Dir.exists? "$HOME/.config"
+      # Save any existing configuration
+      system "cp -r $HOME/.config #{CREW_PREFIX}"
+    else
+      # Remove the symlink, if it exists
+      system "rm -f $HOME/.config"
+    end
+  end
 
   def self.install
     system "php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\""
     abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA384.hexdigest( File.read('composer-setup.php') ) == '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061' 
     system "mkdir -p #{CREW_DEST_PREFIX}/bin"
-    system "php composer-setup.php --install-dir=#{CREW_DEST_PREFIX}/bin --filename=composer --version=1.6.5"
+    system "php composer-setup.php --install-dir=#{CREW_DEST_PREFIX}/bin --filename=composer --version=1.7.0"
+    system "mkdir -p #{CREW_DEST_PREFIX}/.config"
+    system "cp -r $HOME/.config/composer #{CREW_DEST_PREFIX}/.config"
+    system "rm -rf $HOME/.config"
+    system "ln -s #{CREW_PREFIX}/.config $HOME/.config"
+  end
+
+  def self.postinstall
+    puts
+    puts "To finish the installation, execute the following:".lightblue
+    puts "echo 'export PATH=\$HOME/.config/composer/vendor/bin:\$PATH' >> ~/.bashrc && . ~/.bashrc".lightblue
+    puts
   end
 end
