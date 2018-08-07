19731049563d4feaa880dfa98c14b4b2a8fdf7cd
James Larrowe <larrowe.semaj11@gmail.com>
2018-08-07 19:45:12 -0400

    Added Xterm and Xwayland. The X server now works. (#2518)
    * Added Xterm and Xwayland. The X server now works.
    
    * Bumped Xwayland version.
    
    * Switched from ftp to https in source_url

--- packages/xwayland.rb
+++ packages/xwayland.rb
@@ -3,7 +3,7 @@ require 'package'
 class Xwayland < Package
   description 'X server configured to (only) work with weston'
   homepage 'https://x.org'
-  version '1.19.3'
+  version '1.19.3-1'
   source_url 'https://www.x.org/releases/individual/xserver/xorg-server-1.19.3.tar.gz'
   source_sha256 '8f93b98f1ac9fbd87515bfe329a069b48bbec98e5329584ab5fbf759a0953b8d'
 
@@ -31,6 +31,7 @@ class Xwayland < Package
   depends_on 'nettle'
   depends_on 'libtirpc'
   depends_on 'pixman'
+  depends_on 'xinit'
 
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --disable-docs --disable-devel-docs --enable-xwayland --disable-xorg --disable-xvfb --disable-xnest --disable-xquartz --disable-xwin"
@@ -39,5 +40,6 @@ class Xwayland < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "ln", "-s", "#{CREW_PREFIX}/bin/Xwayland", "#{CREW_DEST_PREFIX}/bin/X"
   end
 end
