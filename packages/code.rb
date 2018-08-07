cdaeb096aed593d420683fc5c18db10d9b3d738a
James Larrowe <larrowe.semaj11@gmail.com>
2018-08-07 19:39:21 -0400

    Added VS code, works on X86_64 and I686

---
+++ packages/code.rb
@@ -0,0 +1,56 @@
+require 'package'
+
+class Code < Package
+  description 'Visual Studio Code is a source code editor developed by Microsoft for Windows, Linux and macOS.'
+  homepage 'https://code.visualstudio.com/docs/?dv=linux64'
+  version '1.25'
+  case ARCH
+    when 'x86_64'
+      source_url 'https://go.microsoft.com/fwlink/?LinkID=620884'
+      source_sha256 '5856bbebf38aa05d584da4722869bbe507cf123f69f7ffab5f1532d73dbf3438'
+    when 'i686'
+      source_url 'https://go.microsoft.com/fwlink/?LinkID=620885'
+      source_sha256 'af6adc2e2500e50bfebe7ee7b97d661b6e774a590136bf5f89334132a5b292e2'
+    else
+      
+   end
+
+  depends_on 'xwayland'
+  depends_on 'xinit'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'xinit'
+  
+  def self.build
+    nil
+  end
+
+  def self.install
+    case ARCH
+      when 'x86_64', 'i686'
+        system "mkdir", "-p", "#{CREW_DEST_PREFIX}/lib/code"
+        system "mkdir", "-p", "#{CREW_DEST_PREFIX}/bin"
+        system "cp", "-rpa", ".", "#{CREW_DEST_PREFIX}/lib/code/"
+        system "echo '#!/bin/bash' > #{CREW_DEST_PREFIX}/bin/code"
+        system "echo >> #{CREW_DEST_PREFIX}/bin/code"
+        system "echo '#{CREW_PREFIX}/lib/code/bin/code' >> #{CREW_DEST_PREFIX}/bin/code"
+        system "chmod a+x #{CREW_DEST_PREFIX}/bin/code"
+      else
+        
+    end
+  end
+
+  def self.postinstall
+    puts
+    puts "To run VS code, run this:"
+    puts "xinit"
+    puts "When in the X server, run this:"
+    puts "code"
+    puts "Happy coding!"
+    puts
+  end
+end
