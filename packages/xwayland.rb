b72330c877fecfd6539e3da3ac70b0ad33dda0f8
Ed Reel <edreel@gmail.com>
2018-08-08 08:44:16 -0500

    Update xwayland from 1.19.3-2 to 1.20.1
    Add pre-built binaries

--- packages/xwayland.rb
+++ packages/xwayland.rb
@@ -3,13 +3,21 @@ require 'package'
 class Xwayland < Package
   description 'X server configured to (only) work with weston'
   homepage 'https://x.org'
-  version '1.19.3-2'
-  source_url 'https://www.x.org/releases/individual/xserver/xorg-server-1.19.3.tar.gz'
-  source_sha256 '8f93b98f1ac9fbd87515bfe329a069b48bbec98e5329584ab5fbf759a0953b8d'
+  version '1.20.1'
+  source_url 'https://www.x.org/releases/individual/xserver/xorg-server-1.20.1.tar.bz2'
+  source_sha256 '59c99fe86fe75b8164c6567bfc6e982aecc2e4a51e6fbac1b842d5d00549e918'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xwayland-1.20.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xwayland-1.20.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xwayland-1.20.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xwayland-1.20.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '18a1711d3dc2383ea5ccb515916db9dbe7c1418c8c603da1a4cdd54b3ae3e3e8',
+     armv7l: '18a1711d3dc2383ea5ccb515916db9dbe7c1418c8c603da1a4cdd54b3ae3e3e8',
+       i686: 'b7ce2f3ba702cd71a03aef6d36c1e739773d8c234db8a25a1e40734f6ad18fd5',
+     x86_64: 'b4bad45b9c4a9a7bd34d61c8b31e5a621133ed7cde17b85668c33b4632477f25',
   })
 
   depends_on 'libepoxy'
@@ -26,6 +34,8 @@ class Xwayland < Package
   depends_on 'xinit'
   depends_on 'libxkbcommon'
   depends_on 'libunwind'
+  depends_on 'font_util'
+  depends_on 'glproto'
 
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --disable-docs --disable-devel-docs --enable-xwayland --disable-xorg --disable-xvfb --disable-xnest --disable-xquartz --disable-xwin"
