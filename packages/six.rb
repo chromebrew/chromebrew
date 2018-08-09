3321730291672c0bfc53e6ab9e20723f8d5ecf3c
James Larrowe <larrowe.semaj11@gmail.com>
2018-08-09 13:09:44 -0400

    Now builds six from source

--- packages/six.rb
+++ packages/six.rb
@@ -4,8 +4,8 @@ class Six < Package
   description 'Six is a Python 2 and 3 compatibility library.'
   homepage 'https://github.com/benjaminp/six'
   version '1.11.0'
-  source_url 'https://github.com/skycocker/chromebrew/raw/cc4f47007b6fe702e03de28347e20d3a0bb692f6/install.sh'
-  source_sha256 'bf141178609cfbb130b611b0d38c28f07360fde829100ef6a047db19fe1fb593'
+  source_url 'https://github.com/benjaminp/six/archive/1.11.0.tar.gz'
+  source_sha256 '927dc6fcfccd4e32e1ce161a20bf8cda39d8c9d5f7a845774486907178f69bd4'
 
   binary_url ({
   })
@@ -15,8 +15,13 @@ class Six < Package
   depends_on 'python3'
   depends_on 'python27'
 
+  def self.build
+    system "python2.7 setup.py build"
+    system "python3 setup.py build"
+  end
+
   def self.install
-    system "pip install six --upgrade --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
-    system "pip3 install six --upgrade --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
+    system "python2.7 setup.py install --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
+    system "python3 setup.py install --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
   end
 end
