5b84fe2ef422602d0bee810b911258d643922bee
James Larrowe <larrowe.semaj11@gmail.com>
2018-08-10 16:53:13 -0400

    Add vscode again (#2536)
    * Trying again, without gtk_doc dependency on dbus_glib
    
    * Re-added six, taking a minimal approach to fixing it this time.

---
+++ packages/libgconf.rb
@@ -0,0 +1,26 @@
+require 'package'
+
+class Libgconf < Package
+  description 'GConf is a system for storing application preferences.'
+  homepage 'https://projects-old.gnome.org/gconf/'
+  version '3.2.6'
+  source_url 'https://ftp.gnome.org/pub/GNOME/sources/GConf/3.2/GConf-3.2.6.tar.xz'
+  source_sha256 '1912b91803ab09a5eed34d364bf09fe3a2a9c96751fde03a4e0cfa51a04d784c'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'dbus_glib'
+  depends_on 'gtk3'
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} --disable-orbit"
+    system "make"
+  end
+
+  def self.install
+    system "make install DESTDIR=#{CREW_DEST_DIR}"
+  end
+end
