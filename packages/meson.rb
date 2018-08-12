9432e316bf75cb9587b8216bf177504ed7570ff6
Ed Reel <edreel@gmail.com>
2018-08-12 15:29:21 -0500

    Update meson from 0.44.0-1 to 0.47.1
    Add pre-built binaries

--- packages/meson.rb
+++ packages/meson.rb
@@ -3,23 +3,23 @@ require 'package'
 class Meson < Package
   description 'The Meson Build System'
   homepage 'http://mesonbuild.com/'
-  version '0.44.0-1'
-  source_url 'https://github.com/mesonbuild/meson/releases/download/0.44.0/meson-0.44.0.tar.gz'
-  source_sha256 '50f9b12b77272ef6ab064d26b7e06667f07fa9f931e6a20942bba2216ba4281b'
+  version '0.47.1'
+  source_url 'https://github.com/mesonbuild/meson/releases/download/0.47.1/meson-0.47.1.tar.gz'
+  source_sha256 'd673de79f7bab064190a5ea06140eaa8415efb386d0121ba549f6d66c555ada6'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.44.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.44.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.44.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.44.0-1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.47.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.47.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.47.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.47.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '0fd514b6ea8d80a48a3e374175e1624a465b699d1b0bb9b15c447b49397b7d38',
-     armv7l: '0fd514b6ea8d80a48a3e374175e1624a465b699d1b0bb9b15c447b49397b7d38',
-       i686: '39995c658f15fa4b8d432a8f3a66e660d5fdebbf964262fa07c5b4cd182f2709',
-     x86_64: '19c473a9f38e549ba03cde3208da6e39f09ffa381be5ff32db9acc8e30e7c6b4',
+    aarch64: '70044685fed7440fbce8f433cf3d8912545e1d99f176cc374a61010dee9d6da8',
+     armv7l: '70044685fed7440fbce8f433cf3d8912545e1d99f176cc374a61010dee9d6da8',
+       i686: '0894590fed44551d2e56f4ce419306b115c0f00387a87a78ce34bf5a77d6292b',
+     x86_64: '41499c5d9e9d27413fac18ab977e5011abf22baeb8738c42ae98a187645f2ea1',
   })
-  
+
   depends_on 'ninja'
 
   def self.install
