bf074f1aad3772728148fcd56f713b020c2bea9b
Chris Thurber <8137212+skiman6010@users.noreply.github.com>
2018-08-18 21:57:56 -0400

    Updated wine.rb (#2561)
    * Updated wine and freetype

--- packages/freetype.rb
+++ packages/freetype.rb
@@ -3,21 +3,13 @@ require 'package'
 class Freetype < Package
   description 'FreeType is a freely available software library to render fonts.'
   homepage 'https://www.freetype.org/'
-  version '2.9-0'
-  source_url 'http://download.savannah.gnu.org/releases/freetype/freetype-2.9.tar.bz2'
-  source_sha256 'e6ffba3c8cef93f557d1f767d7bc3dee860ac7a3aaff588a521e081bc36f4c8a'
+  version '2.9.1'
+  source_url 'https://download.savannah.gnu.org/releases/freetype/freetype-2.9.1.tar.gz'
+  source_sha256 'ec391504e55498adceb30baceebd147a6e963f636eb617424bcfc47a169898ce'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.9-0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.9-0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.9-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.9-0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '7fd752cc084ee8a101030e682b0dbce39551049fddbf547762b6f8e60450e131',
-     armv7l: '7fd752cc084ee8a101030e682b0dbce39551049fddbf547762b6f8e60450e131',
-       i686: '1c2688c3070e37d61db309a6310a5a613aa89c419a89cbc79d9055b56c1c82fc',
-     x86_64: '1490d08a9b385ef3abc323c914aaf218f0b59961885e3cda64278d971ace71c9',
   })
 
   depends_on 'expat'
@@ -26,6 +18,7 @@ class Freetype < Package
   depends_on 'harfbuzz'
 
   def self.build
+	system "sed -i 's,/usr/include/freetype2,#{CREW_PREFIX}/include/freetype2,g' configure"
     system "./configure \
             CFLAGS=\" -fPIC\" \
             --prefix=#{CREW_PREFIX} \
