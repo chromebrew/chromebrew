4dc0cc5c0b69d087db5cda50bd93bac623da76a4
superloach <superloach42@gmail.com>
2018-08-22 18:54:22 -0400

    remove core dependencies

--- packages/libexiv2.rb
+++ packages/libexiv2.rb
@@ -7,11 +7,6 @@ class Libexiv2 < Package
   source_url 'http://www.exiv2.org/builds/exiv2-0.26-trunk.tar.gz'
   source_sha256 'c75e3c4a0811bf700d92c82319373b7a825a2331c12b8b37d41eb58e4f18eafb'
 
-  depends_on 'zlibpkg'
-  depends_on 'gettext'
-  depends_on 'libiconv'
-  depends_on 'expat'
-
   def self.build
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
