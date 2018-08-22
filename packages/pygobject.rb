2604cd21967eb124fc89132d5d13b59997d5c8a5
superloach <superloach42@gmail.com>
2018-08-22 17:08:14 -0400

    Add PyGObject

---
+++ packages/pygobject.rb
@@ -0,0 +1,25 @@
+require 'package'
+
+class Pygobject < Package
+  description 'PyGObject is a Python package which provides bindings for GObject based libraries such as GTK+, GStreamer, WebKitGTK+, GLib, GIO and many more.'
+  homepage 'https://pygobject.readthedocs.io/en/latest/'
+  version '3.28.3'
+  source_url 'https://files.pythonhosted.org/packages/e0/e8/1e4f21800015a9ca153969e85fc29f7962f8f82fc5dbc1ecbdeb9dc54c75/PyGObject-3.28.3.tar.gz'
+  source_sha256 '250fb669b6ac64eba034cc4404fcbcc993717b1f77c29dff29f8c9202da20d55'
+
+  depends_on 'glib'
+  depends_on 'gobject_introspection'
+  depends_on 'libffi'
+  depends_on 'pycairo'
+  depends_on 'pkgconfig'
+
+  def self.build
+    system "python2.7 setup.py build"
+    system "python3 setup.py build"
+  end
+
+  def self.install
+    system "python2.7 setup.py install --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
+    system "python3 setup.py install --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
+  end
+end
