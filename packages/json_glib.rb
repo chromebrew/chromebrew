d3dea51c4c74139614d55eda2db4be0f88421153
superloach <superloach42@gmail.com>
2018-08-22 01:40:17 -0400

    Fixed per inline comments

--- packages/json_glib.rb
+++ packages/json_glib.rb
@@ -7,10 +7,7 @@ class Json_glib < Package
   source_url 'https://download.gnome.org/sources/json-glib/1.4/json-glib-1.4.2.tar.xz'
   source_sha256 '2d7709a44749c7318599a6829322e081915bdc73f5be5045882ed120bb686dc8'
 
-  depends_on 'python3'
-  depends_on 'ninja'
   depends_on 'meson'
-  depends_on 'pkgconfig'
   depends_on 'gtk_doc'
   depends_on 'glib'
   depends_on 'gobject_introspection'
@@ -18,9 +15,12 @@ class Json_glib < Package
   def self.build
     system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} _build"
     system "ninja -C _build"
-    system "meson test -C _build"
   end
 
+  def self.check
+    system "meson test -C _build"
+  end
+  
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} ninja -C _build install"
   end
