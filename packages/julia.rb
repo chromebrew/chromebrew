4fd2f6fe5d1fa063b45c1c7d70374fdc01d6cd39
Ed Reel <edreel@gmail.com>
2018-08-26 16:23:53 -0500

    Symlink library files to #{CREW_LIB_PREFIX}

--- packages/julia.rb
+++ packages/julia.rb
@@ -3,7 +3,7 @@ require 'package'
 class Julia < Package
   description 'Julia is a flexible dynamic language, appropriate for scientific and numerical computing'
   homepage 'https://julialang.org/'
-  version '1.0.0'
+  version '1.0.0-1'
   case ARCH
   when 'x86_64'
     source_url 'https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.0-linux-x86_64.tar.gz'
@@ -24,8 +24,12 @@ class Julia < Package
   })
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}"
-	system "cp -r . #{CREW_DEST_PREFIX}"
-	system "mv #{CREW_DEST_PREFIX}/lib #{CREW_DEST_PREFIX}/lib64" if ARCH == 'x86_64'
+    FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}"
+    system "cp -r . #{CREW_DEST_PREFIX}"
+    if ARCH == 'x86_64'
+      FileUtils.cd "#{CREW_DEST_PREFIX}/lib" do
+        system "find . -type f -exec ln -s #{CREW_PREFIX}/lib/{} #{CREW_DEST_LIB_PREFIX}/{} \\;"
+      end
+    end
   end
 end
