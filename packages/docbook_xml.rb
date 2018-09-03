46042e45b4935140c96b01df469d4fb4b2d8d34f
Ed Reel <edreel@gmail.com>
2018-09-02 20:48:06 -0500

    Update docbook packages and dependencies
    Add pre-built binaries

--- packages/docbook_xml.rb
+++ packages/docbook_xml.rb
@@ -10,17 +10,25 @@ class Docbook_xml < Package
   source_sha256 '23068a94ea6fd484b004c5a73ec36a66aa47ea8f0d6b62cc1695931f5c143464'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/docbook_xml-4.3-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/docbook_xml-4.3-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/docbook_xml-4.3-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/docbook_xml-4.3-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'd3008b147f80217982def39e17e04ab9dd32774932322685ae54e5310c70787b',
+     armv7l: 'd3008b147f80217982def39e17e04ab9dd32774932322685ae54e5310c70787b',
+       i686: '2650849c66d4838eb91c679dcd18792583e848a11073b8f60fdc2f5c9f81a7dd',
+     x86_64: '2479e13c8b04434b51a07a96d8500a753042e08ed68dac8b692a267b44372001',
   })
 
   depends_on 'docbook'
+  depends_on 'sgml_common'
 
   def self.install
 
-    xml_dtd = 'xml-dtd-4.3'
-    xml_version = 'xml/4.3'
-    vxml = 'V4.3'
+    xml_version = '4.3'
+    xml_dtd = "xml-dtd-#{xml_version}"
 
     system "install -v -d -m755 #{CREW_DEST_PREFIX}/share/xml/docbook/#{xml_dtd}"
     system "install -v -d -m755 #{CREW_DEST_PREFIX}/etc/xml"
@@ -28,11 +36,11 @@ class Docbook_xml < Package
     system "rm -f #{CREW_PREFIX}/etc/xml/docbook && \
                 xmlcatalog --noout --create #{CREW_DEST_PREFIX}/etc/xml/docbook && \
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//DTD DocBook XML #{vxml}//EN' \
-                'http://www.oasis-open.org/docbook/#{xml_version}/docbookx.dtd' \
+                '-//OASIS//DTD DocBook XML V#{xml_version}//EN' \
+                'http://www.oasis-open.org/docbook/xml/#{xml_version}/docbookx.dtd' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//DTD DocBook XML CALS Table Model #{vxml}//EN' \
+                '-//OASIS//DTD DocBook XML CALS Table Model V#{xml_version}//EN' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/calstblx.dtd' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
@@ -40,35 +48,35 @@ class Docbook_xml < Package
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/soextblx.dtd' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//ELEMENTS DocBook XML Information Pool #{vxml}//EN' \
+                '-//OASIS//ELEMENTS DocBook XML Information Pool V#{xml_version}//EN' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/dbpoolx.mod' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//ELEMENTS DocBook XML Document Hierarchy #{vxml}//EN' \
+                '-//OASIS//ELEMENTS DocBook XML Document Hierarchy V#{xml_version}//EN' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/dbhierx.mod' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//ELEMENTS DocBook XML HTML Tables #{vxml}//EN' \
+                '-//OASIS//ELEMENTS DocBook XML HTML Tables V#{xml_version}//EN' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/htmltblx.mod' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//ENTITIES DocBook XML Notations #{vxml}//EN' \
+                '-//OASIS//ENTITIES DocBook XML Notations V#{xml_version}//EN' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/dbnotnx.mod' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//ENTITIES DocBook XML Character Entities #{vxml}//EN' \
+                '-//OASIS//ENTITIES DocBook XML Character Entities V#{xml_version}//EN' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/dbcentx.mod' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'public' \
-                '-//OASIS//ENTITIES DocBook XML Additional General Entities #{vxml}//EN' \
+                '-//OASIS//ENTITIES DocBook XML Additional General Entities V#{xml_version}//EN' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}/dbgenent.mod' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'rewriteSystem' \
-                'http://www.oasis-open.org/docbook/#{xml_version}' \
+                'http://www.oasis-open.org/docbook/xml/#{xml_version}' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook &&
             xmlcatalog --noout --add 'rewriteURI' \
-                'http://www.oasis-open.org/docbook/#{xml_version}' \
+                'http://www.oasis-open.org/docbook/xml/#{xml_version}' \
                 'file://#{CREW_PREFIX}/share/xml/docbook/#{xml_dtd}' \
                 #{CREW_DEST_PREFIX}/etc/xml/docbook"
     
---
+++ packages/sgml_common.rb
@@ -0,0 +1,54 @@
+require 'package'
+
+# from LFS: http://www.linuxfromscratch.org/blfs/view/cvs/pst/sgml-common.html
+
+class Sgml_common < Package
+  description 'The sgml-common package gathers very basic stuff necessary to work with SGML and XML.'
+  homepage 'https://sourceware.org/docbook-tools/'
+  version '0.6.3'
+  source_url 'https://sourceware.org/ftp/docbook-tools/new-trials/SOURCES/sgml-common-0.6.3.tgz'
+  source_sha256 '7dc418c1d361123ffc5e45d61f1b97257940a8eb35d0bfbbc493381cc5b1f959'
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/sgml_common-0.6.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/sgml_common-0.6.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/sgml_common-0.6.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/sgml_common-0.6.3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '18ff17ac8af52e6e6790564c49155d5c2e8e05583d514744af1e30502d4a79b8',
+     armv7l: '18ff17ac8af52e6e6790564c49155d5c2e8e05583d514744af1e30502d4a79b8',
+       i686: '8bae935790db5a8b6aa63cc76e9c8c568681a7bf66e505f3c2be872f5e713338',
+     x86_64: '48fcb41950ca59883d56b7285608121150c6a2c4969579847467606610d7c0a8',
+  })
+
+  def self.patch
+    system 'wget http://www.linuxfromscratch.org/patches/blfs/svn/sgml-common-0.6.3-manpage-1.patch'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('sgml-common-0.6.3-manpage-1.patch') ) == '50d93af704a0869faf8fedf5d23b1a9f90fff6499f4c11cf4addb5e460b7b58b'
+    system 'patch -Np1 -i sgml-common-0.6.3-manpage-1.patch && autoreconf -f -i'
+  end
+
+  def self.build
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--sysconfdir=#{CREW_PREFIX}/etc"
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/sgml"
+    system "touch #{CREW_DEST_PREFIX}/etc/sgml/sgml-ent.cat"
+    system "touch #{CREW_DEST_PREFIX}/etc/sgml/sgml-docbook.cat"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/sgml/sgml-iso-entities-8879.1986"
+    system "touch #{CREW_DEST_PREFIX}/share/sgml/sgml-iso-entities-8879.1986/catalog"
+    system "sed -i 's,/etc/sgml,#{CREW_PREFIX}/etc/sgml,g' #{CREW_DEST_PREFIX}/bin/install-catalog"
+  end
+
+  def self.postinstall
+    system "install-catalog --add #{CREW_PREFIX}/etc/sgml/sgml-ent.cat \
+            #{CREW_PREFIX}/share/sgml/sgml-iso-entities-8879.1986/catalog &&
+            install-catalog --add #{CREW_PREFIX}/etc/sgml/sgml-docbook.cat \
+            #{CREW_PREFIX}/etc/sgml/sgml-ent.cat"
+  end
+end
