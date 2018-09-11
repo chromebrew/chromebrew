0472baed607db34bd69e5f9fbd5db7f7d4ba15aa
Lorenzo Martinico <lorenzo.martinico@nexedi.com>
2018-09-11 16:50:20 +0200

    Add shill wrapper trick to overcome race condition

--- packages/re6stnet.rb
+++ packages/re6stnet.rb
@@ -3,24 +3,26 @@ class Re6stnet < Package
   description 'Resilient, Scalable, IPv6 Network'
   homepage 'https://re6st.nexedi.com/'
   version '0.509'
-  source_url 'https://lab.nexedi.com/nexedi/chromebrew-buildout-re6st/repository/archive.tar.bz2?ref=5cb542c266b398fd2a36d16c9f8cb1fb9fc1d567'
-  source_sha256 '6872b0275ff11d264241e098133b2f490a04273dfb1db5bf91547ceb0c320763'
+  source_url 'https://lab.nexedi.com/nexedi/chromebrew-buildout-re6st/repository/archive.tar.bz2?ref=1bc16d97519af4329b01514634e648db0a7be333'
+  source_sha256 '02e90c41ed7511216b7ab283d8b7893882a3349d7c383e7b983561299972a9ce'
 
   binary_url ({
     x86_64: 'https://softinst105421.host.vifib.net/public/re6stnet-0.509-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    x86_64: '506d8e7f959bbe05c6e68672b5e488ecfc4e87a3782aafe73a4621b34a6cf0c8',
+    x86_64: 'b9b8cc48e5a7a1fa202a89c8807d4aa63a8db79353ab6dc992608b608130a52b',
   })
 
   depends_on 'python27' => :build
   depends_on 'filecmd' => :build
   depends_on 'libiconv' => :build
+  depends_on 'glibc'
 
   @re6st_dir = 	"#{CREW_PREFIX}/opt/re6st"
 
   def self.build
     system "mkdir -p #{@re6st_dir}/bin && cp buildout.cfg #{@re6st_dir} && cp bin/grandenet #{@re6st_dir}/bin"
+    system "gcc shill_wrapper.c -o #{@re6st_dir}/bin/shill_wrapper -ldl -Wall -fPIC -pie"
     system "SSL_CERT_DIR=/etc/ssl/certs ./bin/buildout buildout:directory=#{@re6st_dir}"
   end
 
