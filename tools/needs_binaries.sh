ae0f0b1fd1172eb7863f2a35d6392499ecb31c64
Ed Reel <edreel@gmail.com>
2018-09-12 22:38:06 -0500

    Add more exclusions for armv7l/aarch64

--- tools/needs_binaries.sh
+++ tools/needs_binaries.sh
@@ -9,8 +9,8 @@ exclusions="antlr4.rb asciinema.rb autosetup.rb cabal.rb code.rb composer.rb cre
 exclusions+=" docx2txt.rb dpkg.rb dr.rb far.rb fpc.rb freedos.rb ghc.rb julia.rb kr.rb libtinfo.rb lsb_release.rb"
 exclusions+=" mysqltuner.rb nodebrew.rb nvm.rb oc.rb spark.rb stack.rb thefuck.rb txt2regex.rb uwsgi.rb xdg_base.rb yarn.rb"
 if [[ "$arch" == "aarch64" || "$arch" == "armv7l" ]]; then
-  exclusions+=" cbase.rb cf.rb clisp.rb dmidecode.rb dropbox.rb freebasic.rb google_cloud_sdk.rb lldb.rb mesa.rb"
-  exclusions+=" miniconda3.rb misctools.rb oci.rb qt.rb wkhtmltox.rb"
+  exclusions+=" az.rb cbase.rb cf.rb clisp.rb dmidecode.rb dropbox.rb freebasic.rb google_cloud_sdk.rb lldb.rb mesa.rb"
+  exclusions+=" miniconda3.rb misctools.rb oci.rb qt.rb wkhtmltox.rb xorg_intel_driver.rb xorg_vmmouse_driver.rb"
 fi
 packages=$(grep -L "$arch:" *.rb)
 for p in $packages; do
