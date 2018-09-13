f1a5b99b2f16fc0415f8af9151a9ca0d9b01558c
Ed Reel <edreel@gmail.com>
2018-09-12 23:07:18 -0500

    Add pre-built binaries for packages f-x

--- packages/perl_read_key.rb
+++ packages/perl_read_key.rb
@@ -7,6 +7,19 @@ class Perl_read_key < Package
   source_url 'https://github.com/jonathanstowe/TermReadKey/archive/v2.37.tar.gz'
   source_sha256 '0fa4fb2f8145e3fb2c2129ad28d55be175abcc258f239ba8ddc2cd83790aa8fb'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/perl_read_key-2.37-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/perl_read_key-2.37-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/perl_read_key-2.37-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/perl_read_key-2.37-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '4e9628ee71561b0b6b63a5aeffd31d0b128b0cefa989ce5b16a956122aa1f387',
+     armv7l: '4e9628ee71561b0b6b63a5aeffd31d0b128b0cefa989ce5b16a956122aa1f387',
+       i686: '476c3fb90d238d61040f0a5cdd5c8ffc0ad676327f635fbc851a6b4788b4c149',
+     x86_64: '966e8c16894a81003dd29af9c5ceb69d5e2a509c56a8701ff680fe4a2f21868d',
+  })
+
   def self.build
     system "perl", "Makefile.PL"
     system "make"
--- packages/perl_time_hires.rb
+++ packages/perl_time_hires.rb
@@ -7,6 +7,19 @@ class Perl_time_hires < Package
   source_url 'https://cpan.metacpan.org/authors/id/J/JH/JHI/Time-HiRes-1.9758.tar.gz'
   source_sha256 '5bfa145bc11e70a8e337543b1084a293743a690691b568493455dedf58f34b1e'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/perl_time_hires-1.9758-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/perl_time_hires-1.9758-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/perl_time_hires-1.9758-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/perl_time_hires-1.9758-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '40c061bbf460cd2f617e94802ed91d3c7a97fc142e4c649ef72160d868e68a94',
+     armv7l: '40c061bbf460cd2f617e94802ed91d3c7a97fc142e4c649ef72160d868e68a94',
+       i686: 'dd6a1fcab145b5e92ccdec417e61c66beb0f303e0cbc98a5b4ff45cdb0261d09',
+     x86_64: '79dfe890436e0e110ae7af0e3905f1aea79b684c63cc751140638eb71e62170a',
+  })
+
   def self.build
     system "perl", "Makefile.PL"
     system "make"
