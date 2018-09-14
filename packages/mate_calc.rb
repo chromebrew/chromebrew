4e57a5846b8f0fe8a0f70bb2050b9d758310e459
Ed Reel <edreel@gmail.com>
2018-09-13 23:45:04 -0500

    Update mate_calc from 1.20 to 1.21
    Add dependencies
    
    Add pre-built binaries

--- packages/mate_calc.rb
+++ packages/mate_calc.rb
@@ -3,14 +3,26 @@ require 'package'
 class Mate_calc < Package
   description 'Calculator for MATE.'
   homepage 'https://mate-desktop.org'
-  version '1.20'
-  source_url 'https://pub.mate-desktop.org/releases/1.20/mate-calc-1.20.0.tar.xz'
-  source_sha256 'e3b075f7d0a02fb72adbf78678685888dec9402cd0ae6b04200d32009651950d'
+  version '1.21'
+  source_url 'https://pub.mate-desktop.org/releases/1.21/mate-calc-1.21.0.tar.xz'
+  source_sha256 'c6b2c4d832f67d5f3d1293cb143e428365077e3cdfe4fc4bb80b3fce5362b51e'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mate_calc-1.21-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mate_calc-1.21-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mate_calc-1.21-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mate_calc-1.21-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '311ade356f6259ca68ebf2318b7dfec9053e51899c3bb033b91e091479d587f6',
+     armv7l: '311ade356f6259ca68ebf2318b7dfec9053e51899c3bb033b91e091479d587f6',
+       i686: '1e3ce84febcc23812533a02f4d49b3ed4bb7b4a9a97f50cd220313ee9d701884',
+     x86_64: 'c5848a45335a4bcbb9e4066c3e90875a82b3ebc1486fe1279ded5babbddd3148',
   })
+
+  depends_on 'gtk3'
+  depends_on 'mate_common'
+  depends_on 'yelp_tools'
 
   def self.build
     system "sh autogen.sh"
