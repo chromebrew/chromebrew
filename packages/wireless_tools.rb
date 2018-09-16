d1b997156c2006c5036c0bdde87525018f1a794e
Ed Reel <edreel@gmail.com>
2018-09-16 17:04:48 -0500

    Update fakeroot url_source
    Add pre-built binaries for fakeroot, nano and wireless_tools

--- packages/wireless_tools.rb
+++ packages/wireless_tools.rb
@@ -8,8 +8,16 @@ class Wireless_tools < Package
   source_sha256 '69c5face9ac9d3273042436408a9a057d3416a814253dedeaaef210fcbc42d40'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wireless_tools-2.9-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wireless_tools-2.9-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/wireless_tools-2.9-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wireless_tools-2.9-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'df298e3972dea2f49eec0009fcd651196da3c63430fc1f4e7e0773d6f1a2d4bf',
+     armv7l: 'df298e3972dea2f49eec0009fcd651196da3c63430fc1f4e7e0773d6f1a2d4bf',
+       i686: 'ac6e1d412978402488d60a192aa41c8ca2ed6f4bacf6d231c8e398f9c9ec6d3f',
+     x86_64: '23de1b7f6dd37177fbd6a3c5ac8917c7c61db74ce774606cbb3f5efe4da03e88',
   })
 
   def self.build
