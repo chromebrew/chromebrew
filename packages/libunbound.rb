d27acab696c32279701fe5f405be8ab3024dfa6c
Ed Reel <edreel@gmail.com>
2018-09-16 23:20:03 -0500

    Update libunbound from 1.7.3 to 1.8.0
    Add pre-built binaries

--- packages/libunbound.rb
+++ packages/libunbound.rb
@@ -3,21 +3,21 @@ require 'package'
 class Libunbound < Package
   description 'Unbound is a validating, recursive, and caching DNS resolver.'
   homepage 'https://www.unbound.net/'
-  version '1.7.3'
-  source_url 'https://www.unbound.net/downloads/unbound-1.7.3.tar.gz'
-  source_sha256 'c11de115d928a6b48b2165e0214402a7a7da313cd479203a7ce7a8b62cba602d'
+  version '1.8.0'
+  source_url 'https://www.unbound.net/downloads/unbound-1.8.0.tar.gz'
+  source_sha256 '78f79d6d3b643fdcd74a14fc76542250da886c82f82bc55b51e189663d61b83f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.7.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.8.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.8.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.8.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libunbound-1.8.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '27f917465295015394aed113d07edde320ce977185c55883a875fccfb269d47b',
-     armv7l: '27f917465295015394aed113d07edde320ce977185c55883a875fccfb269d47b',
-       i686: 'e5765e59f11ee3aa5ada3a9571607cdaef2522de2986c74e895063e6e04353c9',
-     x86_64: '3cd8a50cc5ffd19a56d086e5990590eac27f32e78099bf75288902078f487e9f',
+    aarch64: 'be4733b2311bad10f4dac8f3032b525fade63c4829c5e4f474f93e7411f54df6',
+     armv7l: 'be4733b2311bad10f4dac8f3032b525fade63c4829c5e4f474f93e7411f54df6',
+       i686: 'a0652536ce6348433d476d06993f942df1b0c103f5bcdfef8eb70b1d07c87362',
+     x86_64: '8668a925a7f46f477106661f561a2f6085cf23e389e26f86a23a46c0db064f12',
   })
 
   def self.build
