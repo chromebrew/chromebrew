95a0aef0cf95c6138936f6633209e71342b61b7a
Ed Reel <edreel@gmail.com>
2018-09-18 22:27:41 -0500

    Update gnutls to fix libunbound error

--- packages/gnutls.rb
+++ packages/gnutls.rb
@@ -3,21 +3,21 @@ require 'package'
 class Gnutls < Package
   description 'GnuTLS is a secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them.'
   homepage 'http://gnutls.org/'
-  version '3.6.3'
+  version '3.6.3-1'
   source_url 'https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.3.tar.xz'
   source_sha256 'ed642b66a4ecf4851ab2d809cd1475c297b6201d8e8bd14b4d1c08b53ffca993'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.3-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '2534f0e4e6ccfd51f536d8941ee366250a520233dec514df0a7069eee8c9cea4',
-     armv7l: '2534f0e4e6ccfd51f536d8941ee366250a520233dec514df0a7069eee8c9cea4',
-       i686: '94f26d89d7cd24fbadafa539ccdb60ef2cc7ddb5bacccfdafdea367bd3cb5bca',
-     x86_64: 'f4973935b8bfde5225b3d58a90a0f64d0676303f0eb8f2c7cb8d8aff3f62e51a',
+    aarch64: '7b8f451dcc136eafc149196998494173200d4c0e6e44484c6cdedfaccb1e32fe',
+     armv7l: '7b8f451dcc136eafc149196998494173200d4c0e6e44484c6cdedfaccb1e32fe',
+       i686: 'b23d185954cb6e28ff20918a54ce573700686e25193d2921acb18dd017d629ba',
+     x86_64: '70a7b1753ab61e09249f599bbad21e16e8eff71848787c8ad4e77c90582a6f1b',
   })
 
   depends_on 'zlibpkg'
