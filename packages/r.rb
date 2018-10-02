1a0f70b87d1402e0507121892b0f0f42adfe9aae
Ed Reel <edreel@gmail.com>
2018-10-02 11:39:13 -0500

    Add pre-built binaries for acl, heroku and r packages

--- packages/r.rb
+++ packages/r.rb
@@ -8,26 +8,31 @@ class R < Package
   source_sha256 '0463bff5eea0f3d93fa071f79c18d0993878fd4f2e18ae6cf22c1639d11457ed'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.5.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.5.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.5.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/r-3.5.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '983840999a1c53a65cee42ba0a0fcef1cb4c61833d8de078beae5ba83300c908',
+     armv7l: '983840999a1c53a65cee42ba0a0fcef1cb4c61833d8de078beae5ba83300c908',
+       i686: 'd6e8403d9001e8412ad3c17c9422816ab594c1eb2b2576ad0dbb49c27af02cf9',
+     x86_64: 'bd46cf97defc2d200e1bf94c0a157040a5f86cc56ffc99e85b6abe5e875f9e50',
   })
 
   # depends_on 'gfortran'       # require gfortran enabled gcc
   depends_on 'pcre'             # need to use pcre not pcre2
-  depends_on 'zlibpkg'
   depends_on 'xzutils'
   depends_on 'bz2'
   depends_on 'curl'
-  depends_on 'openssl'
   depends_on 'readline7'
-  depends_on 'icu4c'
 
   def self.build
     system './configure',
       "--prefix=#{CREW_PREFIX}",
       "--libdir=#{CREW_LIB_PREFIX}",
       '--with-x=no', # X is not available
-	  '--enable-R-shlib'
+      '--enable-R-shlib'
     system 'make'
   end
 
