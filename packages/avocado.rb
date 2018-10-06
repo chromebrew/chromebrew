fbdcfd129c0c1f0cf6d9e7d10f20ad936e3eb0b6
Ed Reel <edreel@gmail.com>
2018-10-06 00:26:17 -0500

    Add pre-built binaries for attr, avocado and go

--- packages/avocado.rb
+++ packages/avocado.rb
@@ -8,8 +8,16 @@ class Avocado < Package
   source_sha256 'bf1434d61754f389771dc38beb66a62ee6cca95f5224b17eb2a4ca2f7abf22ab'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-64.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-64.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-64.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/avocado-64.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'a2659851c10f6b1d57bf55c6e15f035183a256951422004bff486eb1ec338222',
+     armv7l: 'a2659851c10f6b1d57bf55c6e15f035183a256951422004bff486eb1ec338222',
+       i686: 'd7c93778bdc3f7eeffca15e38fa2208f5dac6a2bce16aa6caee97aebf0dce375',
+     x86_64: '223c4eccb8c646453f633ebb6b6c8b23bf52535ba4895999a029db7c92329ca3',
   })
 
   depends_on 'xdg_base'
