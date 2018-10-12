6373a87cd17192fd7cc5359290bb6fe3363d23b4
Ed Reel <edreel@gmail.com>
2018-10-12 16:54:40 -0500

    Update google_cloud_sdk from 198.0.0 to 220.0.0

--- packages/google_cloud_sdk.rb
+++ packages/google_cloud_sdk.rb
@@ -3,35 +3,29 @@ require 'package'
 class Google_cloud_sdk < Package
   description 'Command-line interface for Google Cloud Platform products and services'
   homepage 'https://cloud.google.com/sdk/'
-  version '198.0.0'
+  version '220.0.0'
 
   case ARCH
   when 'i686'
-    source_url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-198.0.0-linux-x86.tar.gz'
-    source_sha256 'd7f0296356808b96026a62ca20c7c97fbcfe634fdf1d31d7964c31a2b2b94463'
+    source_url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-220.0.0-linux-x86.tar.gz'
+    source_sha256 'd347562e5a076330f96506285c01e9c9f6e594524c54e02842da7e63bdfe264d'
   when 'x86_64'
-    source_url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-198.0.0-linux-x86_64.tar.gz'
-    source_sha256 '37223616327ec82e33d0b07b77f6d641e4c031a786ea945f21e67f78fc67d1c1'
-  else
-    puts 'Unable to install google_cloud_sdk.  Supported architectures include i686 and x86_64 only.'.lightred
+    source_url 'https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-220.0.0-linux-x86_64.tar.gz'
+    source_sha256 'a2205e35b11136004d52d47774762fbec9145bf0bda74ca506f52b71452c570e'
   end
 
   binary_url ({
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/google_cloud_sdk-198.0.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/google_cloud_sdk-198.0.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-       i686: '9308ccb875080829dcfe76b7f822552471ab01ef68e0a8209138ac14237e1b0c',
-     x86_64: 'a767bc817d26e619f2f492abb4f17ed85ee1c8f61bbc8c5317577c0664cc9eca',
   })
 
-  case ARCH
-  when 'i686', 'x86_64'
-    depends_on 'python27' unless File.exists? "#{CREW_PREFIX}/bin/python"
-  end
+  depends_on 'python27' unless File.exists? "#{CREW_PREFIX}/bin/python"
+  depends_on 'xdg_base'
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}/share/google_cloud_sdk"
+    FileUtils.mkdir_p "#{ENV['HOME']}/.config/gcloud"
+    FileUtils.mkdir_p "#{CREW_DEST_HOME}/.config/gcloud"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/google_cloud_sdk"
     system "cp -r . #{CREW_DEST_PREFIX}/share/google_cloud_sdk"
     FileUtils.cd("#{CREW_DEST_PREFIX}/share/google_cloud_sdk") do
       system "./install.sh \
@@ -43,7 +37,9 @@ class Google_cloud_sdk < Package
     FileUtils.cd("#{CREW_DEST_PREFIX}/share/google_cloud_sdk/bin") do
       system "find . -type f -exec ln -s #{CREW_PREFIX}/share/google_cloud_sdk/bin/{} #{CREW_DEST_PREFIX}/bin \\;"
     end
-    system "sed -i 's,#{CREW_DEST_DIR},,g' $HOME/.bashrc"
+    system "sed -i 's,#{CREW_DEST_DIR},,g' #{ENV['HOME']}/.bashrc"
+    system "touch #{CREW_DEST_HOME}/.config/gcloud/config_sentinel"
+    system "touch #{CREW_DEST_HOME}/.config/gcloud/gce"
   end
 
   def self.postinstall
