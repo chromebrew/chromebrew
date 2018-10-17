d7eabe6c234608231ce5b17bd8cefd126857a9de
James Larrowe <larrowe.semaj11@gmail.com>
2018-10-17 17:07:56 -0400

    Use better source URL and description.

--- packages/sysfsutils.rb
+++ packages/sysfsutils.rb
@@ -1,10 +1,10 @@
 require 'package'
 
 class Sysfsutils < Package
-  description 'Libsysfs provides a stable programming interface to sysfs and eases querying system devices and their attributes.'
-  homepage 'https://github.com/Distrotech/sysfsutils'
+  description 'These are a set of utilites built upon sysfs, a new virtual filesystem in Linux kernel versions 2.5+ that exposes a system\'s device tree. The current version of sysfsutils includes libsysfs and systool.'
+  homepage 'http://linux-diag.sourceforge.net/Sysfsutils.html'
   version '2.1.0'
-  source_url 'http://http.debian.net/debian/pool/main/s/sysfsutils/sysfsutils_2.1.0+repack.orig.tar.gz'
+  source_url 'https://sourceforge.net/projects/linux-diag/files/sysfsutils/2.1.0/sysfsutils-2.1.0.tar.gz'
   source_sha256 'e865de2c1f559fff0d3fc936e660c0efaf7afe662064f2fb97ccad1ec28d208a'
 
   binary_url ({
