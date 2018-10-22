3cb47f348fec6d969fd23de797d08b919705d6ac
Ed Reel <edreel@gmail.com>
2018-10-21 22:50:19 -0500

    Update alsa_lib and alsa_plugins from 1.1.5 to 1.1.7
    Add pre-built binaries

--- packages/alsa_lib.rb
+++ packages/alsa_lib.rb
@@ -3,21 +3,21 @@ require 'package'
 class Alsa_lib < Package
   description 'The Advanced Linux Sound Architecture (ALSA) provides audio and MIDI functionality to the Linux operating system.'
   homepage 'https://www.alsa-project.org/main/index.php/Main_Page'
-  version '1.1.5'
-  source_url 'ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.1.5.tar.bz2'
-  source_sha256 'f4f68ad3c6da36b0b5241ac3c798a7a71e0e97d51f972e9f723b3f20a9650ae6'
+  version '1.1.7'
+  source_url 'ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.1.7.tar.bz2'
+  source_sha256 '9d6000b882a3b2df56300521225d69717be6741b71269e488bb20a20783bdc09'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.5-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.5-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.5-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.5-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.7-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.7-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.7-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.1.7-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '10b105f4ca993f0d24035fe6bdcec6e8637c768be301757d4040e6dd166d235c',
-     armv7l: '10b105f4ca993f0d24035fe6bdcec6e8637c768be301757d4040e6dd166d235c',
-       i686: '4be56a32b5149e53aa05e15a87a50da8d3b8a25eafbabe2fd3ea436ba2fa0df5',
-     x86_64: '82a8be3966832edbcd42306d5d3920fe3bbcb1cdcb594e375f6f8f4335030abf',
+    aarch64: '4b382a9eaed1d08d1c7ba1906a4b155476c691c9ee1d8f9852af655a933b3c59',
+     armv7l: '4b382a9eaed1d08d1c7ba1906a4b155476c691c9ee1d8f9852af655a933b3c59',
+       i686: 'cb22d0641935cc10dcf4d01838e2eed3a8e8f690b1ccb0741da87336fa29a43b',
+     x86_64: '0e5b6e000aedc2ffc9264eb62bf0afb03e06a86a7803c6105489758b14e822f5',
   })
 
   def self.build
