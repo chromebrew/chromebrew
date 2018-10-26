55f1f4ca0bd244793b97b57ec517558465577584
Terra Branford <superloach42@gmail.com>
2018-10-25 22:53:57 -0400

    Update pipes_sh.rb

--- packages/pipes_sh.rb
+++ packages/pipes_sh.rb
@@ -10,6 +10,6 @@ class Pipes_sh < Package
   depends_on 'ncurses'
 
   def self.install
-    system "make", "PREFIX=#{CREW_DEST_DIR}#{CREW_PREFIX}", "install"
+    system "make DESTDIR=#{CREW_DEST_DIR} PREFIX=#{CREW_PREFIX} install"
   end
 end
