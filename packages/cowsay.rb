2b2ec8ed70dfbc1344532128a7315bcf6751d269
Terra Branford <superloach42@gmail.com>
2018-10-26 09:33:20 -0400

    MOO
    Homepage and prefix

--- packages/cowsay.rb
+++ packages/cowsay.rb
@@ -2,12 +2,12 @@ require 'package'
 
 class Cowsay < Package
   description 'cowsay is a program that generates ASCII pictures of a cow with a message.'
-  homepage 'https://en.wikipedia.org/wiki/Cowsay'
+  homepage 'https://github.com/tnalpgge/rank-amateur-cowsay/'
   version '3.04'
   source_url 'https://github.com/tnalpgge/rank-amateur-cowsay/archive/cowsay-3.04.tar.gz'
   source_sha256 'd8b871332cfc1f0b6c16832ecca413ca0ac14d58626491a6733829e3d655878b'
 
   def self.install
-    system "sh install.sh #{CREW_PREFIX}"
+    system "sh install.sh #{CREW_DEST_PREFIX}"
   end
 end
