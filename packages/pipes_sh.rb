0c2deae657f8a2129efb96b6922600fcf5bd8ea5
Terra Branford <superloach42@gmail.com>
2018-10-25 22:55:47 -0400

    pipes.sh and pipesX.sh (#2790)
    * Create pipes_sh.rb
    
    * Create pipesx_sh.rb
    
    * Update pipes_sh.rb
    
    * Update pipesx_sh.rb

---
+++ packages/pipes_sh.rb
@@ -0,0 +1,15 @@
+require 'package'
+
+class Pipes_sh < Package
+  description 'Animated pipes terminal screensaver'
+  homepage 'https://pipeseroni.github.io/'
+  version '1.3.0'
+  source_url 'https://github.com/pipeseroni/pipes.sh/archive/v1.3.0.tar.gz'
+  source_sha256 '532976dd8dc2d98330c45a8bcb6d7dc19e0b0e30bba8872dcce352361655a426'
+
+  depends_on 'ncurses'
+
+  def self.install
+    system "make DESTDIR=#{CREW_DEST_DIR} PREFIX=#{CREW_PREFIX} install"
+  end
+end
