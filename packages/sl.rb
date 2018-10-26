d23c6147330ed57c8dd58a00c3da79aeabcb78b5
Terra Branford <superloach42@gmail.com>
2018-10-26 09:54:18 -0400

    Terminal fun! (cowsay, fortune, sl) (#2791)
    * Cowsay!
    
    Cows, because why the hell not.
    
    * Fortune!
    
    * Fortunes for fortune!
    
    * Lolcats!!!1!11!!
    
    ~meow~
    
    * TRAINS
    
    I really hope nobody's trying to `crew install ls`.
    
    * Delete lolcat.rb
    
    They can just use gem to install this.
    
    * MOO
    
    Homepage and prefix
    
    * fortune uwu
    
    change to use `sed` and `install`
    
    * steam locomotive lol
    
    switch to `sed` and `install`

---
+++ packages/sl.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Sl < Package
+  description 'Steam Locomotive runs across your terminal when you type "sl" as you meant to type "ls".'
+  homepage 'https://github.com/mtoyoda/sl'
+  version '5.02'
+  source_url 'https://github.com/mtoyoda/sl/archive/5.02.tar.gz'
+  source_sha256 '1e5996757f879c81f202a18ad8e982195cf51c41727d3fea4af01fdcbbb5563a'
+
+  def self.build
+    system "sed -i 's,curses.h,#{CREW_PREFIX}/include/ncurses/curses.h,' sl.c"
+    system "make"
+  end
+
+  def self.install
+    system "install -Dm755 sl #{CREW_DEST_PREFIX}/bin/"
+  end
+end
