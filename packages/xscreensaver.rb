10c7641bde7bde042cbaf2a4024fc52d2e5df413
Terra Branford <superloach42@gmail.com>
2018-10-26 15:29:25 -0400

    XScreenSaver (#2795)
    * XScreenSaver
    
    Let's hope Jamie Zawinski doesn't have too much of a grudge against ChromeOS 🤣
    Also, none of the GLX screensavers work.
    
    * move sommelier to end
    
    * remove redundant libglu

---
+++ packages/xscreensaver.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Xscreensaver < Package
+  description 'XScreenSaver is the standard screen saver collection shipped on most Linux and Unix systems running the X11 Window System.'
+  homepage 'https://www.jwz.org/xscreensaver/download.html'
+  version '5.40'
+  source_url 'https://www.jwz.org/xscreensaver/xscreensaver-5.40.tar.gz'
+  source_sha256 '30a0908d4164cf780ef034f87ba884316296b308af2484261ccde86be0c95ae0'
+
+  depends_on 'glfw'
+  depends_on 'freeglut'
+  depends_on 'sommelier'
+
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX}"
+    system "make"
+  end
+
+  def self.install
+    system "make DESTDIR=#{CREW_DEST_DIR} PREFIX=#{CREW_PREFIX} install"
+  end
+end
