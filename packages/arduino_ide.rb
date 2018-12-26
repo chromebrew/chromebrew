edce4ea76b39d56eefd72ef03e7e9da92fc72e78
James Larrowe <larrowe.semaj11@gmail.com>
2018-12-26 16:41:28 -0500

    Update Arduino IDE (#2996)
    Use #{CREW_DEST_PREFIX}/bin instead of #{CREW_PREFIX}/bin and take into acount @uberhacker's suggestions
    
    Remove 2 assignment operations
    
    Move variable definitions to the top of the file

--- packages/arduino_ide.rb
+++ packages/arduino_ide.rb
@@ -3,11 +3,23 @@ ENV["CREW_NOT_STRIP"] = "true"
 require 'package'
 
 class Arduino_ide < Package
+
+  @version = "1.8.8"
+
+  case ARCH
+  when 'x86_64'
+    @platform = 'linux64'
+  when 'i686'
+    @platform = 'linux32'
+  when 'armv7l', 'aarch64'
+    @platform = 'linuxarm'
+  end
+
   description 'Arduino is an open-source physical computing platform based on a simple I/O board and a development environment that implements the Processing/Wiring language.'
   homepage 'https://www.arduino.cc/'
-  version '1.8.7'
-  source_url 'https://github.com/arduino/Arduino/releases/download/1.8.7/arduino-1.8.7.tar.xz'
-  source_sha256 'f9d684554c7bd55065d9163cc6819acf17dc8826f9ccb27437fb22401b29326e'
+  version '1.8.8'
+  source_url "https://github.com/arduino/Arduino/releases/download/#{@version}/arduino-#{@version}.tar.xz"
+  source_sha256 '05bbed313f12801ed37c5a38d642ec142ed8e7ccb34dd77815b45e663f1c0c7a'
 
   binary_url ({
   })
@@ -20,46 +32,31 @@ class Arduino_ide < Package
   depends_on 'sommelier'
 
   def self.build
-    case ARCH
-    when 'x86_64'
-      platform = 'linux64'
-    when 'i686'
-      platform = 'linux32'
-    when 'armv7l', 'aarch64'
-      platform = 'linuxarm'
-    end
     Dir.chdir("build") do
       system "env",
              "JAVA_HOME=#{CREW_PREFIX}/share/jdk8",
              "ant",
              "-Djava.net.preferIPv4Stack=true",
-             "-Dversion=#{version}",
-             "-Dplatform=#{platform}",
+             "-Dversion=#{@version}",
+             "-Dplatform=#{@platform}",
              "clean",
              "dist"
       system "echo '#!/bin/bash' > arduino"
       system "echo >> arduino"
       system "echo 'echo \"Enabling Arduino write access...\"' >> arduino"
-      system "echo 'sudo  chmod o+w /dev/ttyACM*' >> arduino"
-      system "echo 'sommelier -X --x-display=\$DISPLAY --scale=\$SCALE #{CREW_PREFIX}/share/arduino-#{version}/arduino \"$@\"' >> arduino"
+      system "echo 'sudo chmod o+rw /dev/ttyACM*' >> arduino"
+      system "echo '#{CREW_PREFIX}/share/arduino-#{@version}/arduino \"$@\"' >> arduino"
       system "echo 'echo \"Disabling Arduino write access...\"' >> arduino"
-      system "echo 'sudo chmod o-w /dev/ttyACM*' >> arduino"
+      system "echo 'sudo chmod o-rw /dev/ttyACM*' >> arduino"
     end
   end
 
   def self.install
-    case ARCH
-    when 'x86_64'
-      platform = 'linux64'
-    when 'i686'
-      platform = 'linux32'
-    when 'armv7l', 'aarch64'
-      platform = 'linuxarm'
-    end
     Dir.chdir("build") do
       FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
-      system "tar", "xpvf", "linux/arduino-#{version}-#{platform}.tar.xz", "-C", "#{CREW_DEST_PREFIX}/share/"
+      system "tar", "xpf", "linux/arduino-#{@version}-#{@platform}.tar.xz", "-C", "#{CREW_DEST_PREFIX}/share/"
       system "install", "-Dm755", "arduino", "#{CREW_DEST_PREFIX}/bin/arduino"
+      system "ln", "-s", "../share/arduino-#{@version}/arduino-builder", "#{CREW_DEST_PREFIX}/bin"
     end
   end
 end
