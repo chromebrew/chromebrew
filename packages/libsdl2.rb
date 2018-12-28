865b48fa43394a38f2950235a29ea3dccbdb4da1
Ed Reel <edreel@gmail.com>
2018-12-27 23:54:39 -0600

    Add pre-built binaries for eudev, libsdl, libsdl2 and xterm

--- packages/libsdl2.rb
+++ packages/libsdl2.rb
@@ -8,8 +8,16 @@ class Libsdl2 < Package
   source_sha256 '255186dc676ecd0c1dbf10ec8a2cc5d6869b5079d8a38194c2aecdff54b324b1'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libsdl2-2.0.9-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libsdl2-2.0.9-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libsdl2-2.0.9-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libsdl2-2.0.9-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '8d317956cc1a196d8f5d1a84993abda2c04091afabf71ade3229f9077082b99f',
+     armv7l: '8d317956cc1a196d8f5d1a84993abda2c04091afabf71ade3229f9077082b99f',
+       i686: 'd1a410a9aa771feb91da30f7cab69a38aa88b8cb3a18a16653c30f4b4f9ae6da',
+     x86_64: '4a7cf8e0c57bb8485eb1cade392ca919aff3e71b811464adf6a51e1f98488a00',
   })
 
   depends_on 'xwayland'
--- packages/xterm.rb
+++ packages/xterm.rb
@@ -8,8 +8,16 @@ class Xterm < Package
   source_sha256 '63a07dfa2b5d501a9e1da0e05772bff7ad431ac2065127c77eebe392ef33cb44'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'b0c2289b1f2a2927d3bd133059705407e6418bc595a59aca522eaf9bd92e9e00',
+     armv7l: 'b0c2289b1f2a2927d3bd133059705407e6418bc595a59aca522eaf9bd92e9e00',
+       i686: '0803e8b84a900c5344bc2aa4f2586e8795bf6feb517bc7b75dae69c9438d5eef',
+     x86_64: 'c93930b998ed21cccde94dc81c2aa5e0f91d986d9f193c4c61ca2714d10a8956',
   })
 
   depends_on 'pcre'
