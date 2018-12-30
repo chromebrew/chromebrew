6dac5afc71892802e309331dd7a64efdeda0a37c
James Larrowe <larrowe.semaj11@gmail.com>
2018-12-30 10:31:31 -0500

    Update libdrm package
    Add libdrm intel target
    
    Add libdrm amdgpu target

--- packages/libdrm.rb
+++ packages/libdrm.rb
@@ -3,46 +3,38 @@ require 'package'
 class Libdrm < Package
   description 'Cross-driver middleware for DRI protocol.'
   homepage 'https://dri.freedesktop.org'
-  version '2.4.91'
-  source_url 'https://dri.freedesktop.org/libdrm/libdrm-2.4.91.tar.bz2'
-  source_sha256 '634a0ed0cc1eff06f48674b1da81aafa661a9f001e7a4f43dde81076886dc800'
+  version '2.4.96'
+  source_url 'https://dri.freedesktop.org/libdrm/libdrm-2.4.96.tar.bz2'
+  source_sha256 '0d561acf7bb4cc59dc82415100e6c1a44860e8c380e00f9592923e3cd08db393'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.91-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.91-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.91-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.91-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '4489dc1f2a45a7a3c0b5d5245672b29b134b108208aadcc7d684b3a8547202a7',
-     armv7l: '4489dc1f2a45a7a3c0b5d5245672b29b134b108208aadcc7d684b3a8547202a7',
-       i686: 'b96e268dfc9a04bf9482c216110c18a75a643dd0ef21e95b3367c8f42b6caf45',
-     x86_64: '4bfe97f39a465077ecb2d24d9a6da5cf1f2bba1a4dcabb93fc7bea34cdf95db7',
   })
 
   depends_on 'libpciaccess'
-  depends_on 'xorg_lib' => :build
+  depends_on 'xorg_lib'
   depends_on 'eudev'
+  depends_on 'libxslt'
 
   def self.build
-    case ARCH
-      when 'x86_64', 'i686'
-        system "./configure \
-                --prefix=#{CREW_PREFIX} \
-                --libdir=#{CREW_LIB_PREFIX} \
-                --enable-vmwgfx \
-                --enable-nouveau \
-                --enable-udev"  
-      when 'aarch64', 'armv7l'
-         system "./configure \
-                --prefix=#{CREW_PREFIX} \
-                --libdir=#{CREW_LIB_PREFIX} \
-                --enable-nouveau \
-                --enable-tegra-experimental-api \
-                --enable-exynos-experimental-api \
-                --enable-vc4 \
-                --enable-udev"
-      end
+    system "./configure",
+           "--enable-etnaviv-experimental-api",
+           "--enable-exynos-experimental-api",
+           "--enable-tegra-experimental-api",
+           "--enable-omap-experimental-api",
+           "--enable-install-test-programs",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           "--prefix=#{CREW_PREFIX}",
+           "--enable-freedreno-kgsl",
+           "--enable-freedreno",
+           "--enable-manpages",
+           "--enable-nouveau",
+           "--enable-amdgpu",
+           "--enable-vmwgfx",
+           "--enable-intel",
+           "--enable-udev",
+           "--enable-vc4"  
     system "make"
   end
 
