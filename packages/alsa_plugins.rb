e5f0af60c52d6a40a2943d031dd6ab43283ea726
Ed Reel <edreel@gmail.com>
2019-01-01 13:55:47 -0600

    Fix alsa_plugins library files location
    Add pre-built binaries

--- packages/alsa_plugins.rb
+++ packages/alsa_plugins.rb
@@ -3,21 +3,21 @@ require 'package'
 class Alsa_plugins < Package
   description 'alsa-plugins contains plugins for various ALSA needs (e.g. Jack).'
   homepage 'https://www.alsa-project.org/main/index.php/Main_Page'
-  version '1.1.7-1'
+  version '1.1.7-2'
   source_url 'ftp://ftp.alsa-project.org/pub/plugins/alsa-plugins-1.1.7.tar.bz2'
   source_sha256 'a74b405ab6d9e346e6908a853d5e7631cc61038d9b265bc7f37fab16e827da47'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_plugins-1.1.7-2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '5a0c67e36abf9d2dfc3d7da7b53c7ed14734e8545fc44fb77073bfbc0f8592fa',
-     armv7l: '5a0c67e36abf9d2dfc3d7da7b53c7ed14734e8545fc44fb77073bfbc0f8592fa',
-       i686: '7cb449d186acc168056cbfaf63b4c4942abc14475ce9d5f2b1fd6671f7226b3d',
-     x86_64: '00d5ec8f220e64b9f4197b4897326192c963da616541d4787723a3969f2a129a',
+    aarch64: '9b287b4d7c2252ce45f7ac08fe8b123fd14c538c83f03fedf9a9af6cc3c14c49',
+     armv7l: '9b287b4d7c2252ce45f7ac08fe8b123fd14c538c83f03fedf9a9af6cc3c14c49',
+       i686: 'e8358822e27b9825cb8d115d197514ca4af5a97e4273b8266a0b9b64bbc2865a',
+     x86_64: 'b8356d2d759e7d5079e36840f200dfde01c786a53b6f7f7ab2355734afe7acaa',
   })
 
   depends_on 'alsa_lib'
@@ -30,10 +30,8 @@ class Alsa_plugins < Package
            "--libdir=#{CREW_LIB_PREFIX}",
            '--disable-maintainer-mode'
     system "for f in \$(find . -name Makefile); do
-sed -i 's,/etc/alsa,#{CREW_PREFIX}/etc/alsa,g' \$f
-sed -i 's,/usr/local/lib,#{CREW_LIB_PREFIX},g' \$f
-sed -i 's,/usr/local,#{CREW_PREFIX},g' \$f
-done"
+            sed -i 's,/etc/alsa,#{CREW_PREFIX}/etc/alsa,g' \$f
+            sed -i 's,/usr/local,#{CREW_PREFIX},g' \$f; done"
     system 'make'
   end
 
