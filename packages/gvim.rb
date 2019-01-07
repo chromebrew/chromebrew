4671a305f3366db4ca301ccab5ee8d8f0078be85
Ed Reel <edreel@gmail.com>
2019-01-07 11:08:54 -0600

    Install Mako via pip3
    Reduce i686 drivers
    
    Add glproto dependency to xorg_server
    
    Bump mesa version
    
    Add pre-built binaries
    
    Add twm, xclock and xterm dependencies to xinit

--- packages/gvim.rb
+++ packages/gvim.rb
@@ -8,8 +8,16 @@ class Gvim < Package
   source_sha256 '7e6ad44dbb8fda0aca91c22fa0dcaed2d845cf00c26d6d3df3bfaa38c9da222a'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.0648-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.0648-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.0648-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.0648-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'c45090038eea4923238023236849b34366da1e6b9a50db3dc6ef8b409c4c075d',
+     armv7l: 'c45090038eea4923238023236849b34366da1e6b9a50db3dc6ef8b409c4c075d',
+       i686: 'a7e9a2c05d77784b6da46ee40b7bbc8365a0ea4840b1979e05abe4cbc69aa1c6',
+     x86_64: '7e6c36e7602a62d615899cdc3230aacea3a08c908f1927ba2f44ff31bd09fe4f',
   })
 
   depends_on 'python27' => :build
