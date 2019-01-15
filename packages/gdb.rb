b181df1ca7f94bdba638fd8a6673ef340aecebf7
Ed Reel <edreel@gmail.com>
2019-01-14 22:22:18 -0600

    Add pre-built binaries for gdb, lzo, lzop and xhost

--- packages/gdb.rb
+++ packages/gdb.rb
@@ -8,8 +8,16 @@ class Gdb < Package
   source_sha256 '0a6a432907a03c5c8eaad3c3cffd50c00a40c3a5e3c4039440624bae703f2202'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gdb-8.2.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gdb-8.2.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gdb-8.2.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gdb-8.2.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '9bd4500d8af36782c5e047e15aef059fc0562e0f1c7acc391692d6287d88276a',
+     armv7l: '9bd4500d8af36782c5e047e15aef059fc0562e0f1c7acc391692d6287d88276a',
+       i686: '2078a79d604628c270576be8fb96e8ccaa087ef207e3f6d14356c606898027e8',
+     x86_64: '95fe8fac708e2c28b4c8c8453bfbd7d85d5fe795d80ceaf86ff255d574b2c5b1',
   })
 
   depends_on 'texinfo'
