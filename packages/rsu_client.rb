074f70fee375c1278f2cd670d6504b1e5946c6bc
Terra Branford <superloach42@gmail.com>
2019-01-23 17:46:20 -0500

    Much better sed-ing technique

--- packages/rsu_client.rb
+++ packages/rsu_client.rb
@@ -9,54 +9,18 @@ class Rsu_client < Package
 
   depends_on 'jdk8'
   depends_on 'p7zip'
-  depends_on 'perl'
   depends_on 'wxwidgets'
   depends_on 'xdg_utils'
   depends_on 'sommelier'
 
   def self.patch
     Dir.chdir 'runescape' do
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./changelog.txt"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./install-desktop-icons"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu-settings"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/bin/rsu-query-unix"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/API/client/launch/launcher.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/Perl5lib/XML/RSSLite.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/modules/rsu/java/jre.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/modules/rsu/mains.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/modules/rsu/nvidia/optimus.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/modules/updater/download/sysdload.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/modules/updater/download/wxsysdload.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/framework/modules/updater/gui/zenity.pm"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./rsu/rsu-query"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./runescape"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./templates/packaging/usr/games/rsu-settings"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./templates/packaging/usr/games/runescape"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./templates/packaging/usr/games/update-runescape-client"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./templates/packaging/usr/local/bin/xdg-open"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./templates/protocol/install-jagex-protocol-linux"
-      system "sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' ./updater"
-
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./changelog.txt"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./install-desktop-icons"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu-settings"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/bin/rsu-query-unix"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/API/client/launch/launcher.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/Perl5lib/XML/RSSLite.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/modules/rsu/java/jre.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/modules/rsu/mains.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/modules/rsu/nvidia/optimus.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/modules/updater/download/sysdload.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/modules/updater/download/wxsysdload.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/framework/modules/updater/gui/zenity.pm"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./rsu/rsu-query"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./runescape"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./templates/packaging/usr/games/rsu-settings"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./templates/packaging/usr/games/runescape"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./templates/packaging/usr/games/update-runescape-client"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./templates/packaging/usr/local/bin/xdg-open"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./templates/protocol/install-jagex-protocol-linux"
-      system "sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' ./updater"
+      system "for f in \$(grep -r /usr/bin .|egrep -v '^Binary'|egrep -o '^[^:]+');do
+                sed -i 's,#{CREW_PREFIX},/usr,gw /dev/stdout' $f
+              done"
+      system "for f in \$(grep -r /usr/bin .|egrep -v '^Binary'|egrep -o '^[^:]+');do
+                sed -i 's,/usr,#{CREW_PREFIX},gw /dev/stdout' $f
+              done"
     end
   end
 
