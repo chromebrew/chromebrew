a6adc870bd5f28ba8207f3bd9ab59c5031493e7a
Terra Branford <superloach42@gmail.com>
2019-01-23 08:42:29 -0500

    More redundant dependencies

--- packages/freerdp.rb
+++ packages/freerdp.rb
@@ -7,12 +7,9 @@ class Freerdp < Package
   source_url 'https://github.com/FreeRDP/FreeRDP/archive/2.0.0-rc4.tar.gz'
   source_sha256 '3406f3bfab63f81c1533029a5bf73949ff60f22f6e155c5a08005b8b8afe6d49'
 
-  depends_on 'alsa_lib'
   depends_on 'ffmpeg'
   depends_on 'gst_plugins_base'
   depends_on 'cups'
-  depends_on 'pulseaudio'
-  depends_on 'cmake' => :build
   depends_on 'xmlto'
   depends_on 'ninja'
   depends_on 'sommelier'
