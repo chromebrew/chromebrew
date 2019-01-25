0df163ccc3ff36b756be419de5b928fe7b2ef752
Terra Brown <superloach42@gmail.com>
2019-01-25 09:46:13 -0500

    Fix bz2 and openssl manpages. (#3117)
    * Fix bz2 manpages.
    
    * Fix openssl manpages.
    
    * Bump openssl version.
    
    * Change mkdir from system to Ruby.
    
    * Go back to proper bump.
    
    * Switch to Ruby mkdir in bz2.

--- packages/openssl.rb
+++ packages/openssl.rb
@@ -3,22 +3,9 @@ require 'package'
 class Openssl < Package
   description 'OpenSSL is an open source project that provides a robust, commercial-grade, and full-featured toolkit for the Transport Layer Security (TLS) and Secure Sockets Layer (SSL) protocols.'
   homepage 'https://www.openssl.org/'
-  version '1.0.2p'
+  version '1.0.2p-1'
   source_url 'https://github.com/openssl/openssl/archive/OpenSSL_1_0_2p.tar.gz'
   source_sha256 '95ca65a25bdd41e127e5f4054539e8532a46be602b43b44af7c7100172e7cd50'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/openssl-1.0.2p-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/openssl-1.0.2p-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/openssl-1.0.2p-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/openssl-1.0.2p-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'e7d69e95badd39e35c3d607386319e03a5db364ddd2be6a359a21661a764b587',
-     armv7l: 'e7d69e95badd39e35c3d607386319e03a5db364ddd2be6a359a21661a764b587',
-       i686: '587cc2a7d1eca05eaf9abc3c8e468be2264eb167a87d5a63c12bc46617696196',
-     x86_64: '1e23e83b900b3a472dca44a6d88c7860119904f39cfde09a81f8d6caae01f42b',
-  })
 
   depends_on 'bc' => :build             # required for `make test`
 
@@ -52,8 +39,9 @@ class Openssl < Package
     system "make", "INSTALL_PREFIX=#{CREW_DEST_DIR}", "install"
     system "find #{CREW_DEST_PREFIX} -name 'lib*.a' -print | xargs rm"
 
-    # move man to #{CREW_PREFIX}/man
-    system "mv", "#{CREW_DEST_DIR}/etc/ssl/man", "#{CREW_DEST_PREFIX}/man"
+    # move man to #{CREW_PREFIX}/share/man
+    FileUtils.mkdir_p("#{CREW_DEST_PREFIX}/share")
+    system "mv", "#{CREW_DEST_DIR}/etc/ssl/man", "#{CREW_DEST_PREFIX}/share/man"
 
     # remove all files under /etc/ssl (use system's /etc/ssl as is)
     system "rm", "-rf", "#{CREW_DEST_DIR}/etc"
