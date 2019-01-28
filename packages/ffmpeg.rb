94c90be872d6232c1e130bdac3cd917c71a9a6d5
Ed Reel <edreel@gmail.com>
2019-01-28 12:06:58 -0600

    Add pre-built binaires for packages a-p (#3125)

--- packages/ffmpeg.rb
+++ packages/ffmpeg.rb
@@ -8,8 +8,16 @@ class Ffmpeg < Package
   source_sha256 'a38ec4d026efb58506a99ad5cd23d5a9793b4bf415f2c4c2e9c1bb444acd1994'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.1-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.1-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.1-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.1-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '07ce108d022f4dd0f42ba99f4ef1739555d15905d3feabc5c62149d752b5871f',
+     armv7l: '07ce108d022f4dd0f42ba99f4ef1739555d15905d3feabc5c62149d752b5871f',
+       i686: '30bd2fd8d5909cc8799a42ca4f8733a06f63e3fc01dcdef44f8f03bcce91f05e',
+     x86_64: '3720e339c273fd6d8913220e08a1c1ba8d25d056803747c8f077818c78d8b287',
   })
 
   depends_on 'libopencoreamr'
@@ -68,7 +76,7 @@ class Ffmpeg < Package
            '--enable-frei0r',
            '--enable-gnutls',
            '--enable-libass',
-           "--arch=#{ARCH}',
+           "--arch=#{ARCH}",
            '--enable-gray',
            '--enable-gpl'
   end
