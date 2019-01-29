a975e3a41460666466b73eeb71932d8a0c8df002
Ed Reel <edreel@gmail.com>
2019-01-29 10:32:39 -0600

    Add gcc8 package (#3127)
    Remove gcc package
    
    Update gcc7 from 7.3.0-3 to 7.4.0
    
    Change dependency to gcc8 in buildessential
    
    Update install.sh to use gcc8
    
    Replace xz package with xzutils
    
    Update postinstall message in install.sh
    
    Add pre-built binaries for gcc7 and gcc8

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,11 +3,11 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage ''
-  version '1.3'
+  version '1.4'
 
   is_fake
 
-  depends_on 'gcc7'
+  depends_on 'gcc8'
   depends_on 'linuxheaders'
   depends_on 'make'
   depends_on 'pkgconfig'
