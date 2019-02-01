aa972652afbe1c4e01de6948716e2acf34217b7a
Ed Reel <edreel@gmail.com>
2019-02-01 07:52:48 -0600

    Add #{version} variable
    Remove ulimit (already set at unlimited by default)

--- packages/gcc8.rb
+++ packages/gcc8.rb
@@ -53,7 +53,7 @@ class Gcc8 < Package
                  '--enable-languages=all',
                  '--enable-threads=posix',
                  '--with-tune=cortex-a15',
-                 '--program-suffix=-8.2',
+                 '--program-suffix=-#{version}',
                  '--with-arch=armv7-a',
                  '--with-float=hard',
                  '--disable-libmpx',
@@ -70,7 +70,7 @@ class Gcc8 < Package
                  "--prefix=#{CREW_PREFIX}",
                  '--enable-languages=all',
                  '--enable-threads=posix',
-                 '--program-suffix=-8.2',
+                 '--program-suffix=-#{version}',
                  '--with-arch-64=x86-64',
                  '--with-arch-32=i686',
                  '--disable-multilib',
@@ -85,7 +85,6 @@ class Gcc8 < Package
   # preserve for check, skip check for current version
   def self.check
     Dir.chdir('build') do
-      system 'ulimit -s 32768'
       system 'make -k check'
       system '../contrib/test_summary'
     end
@@ -105,25 +104,25 @@ class Gcc8 < Package
 
       # Make symbolic links
       FileUtils.ln_s('gcc', "#{CREW_DEST_PREFIX}/bin/cc")
-      FileUtils.ln_s('gcc-8.2', "#{CREW_DEST_PREFIX}/bin/gcc")
-      FileUtils.ln_s('c++-8.2', "#{CREW_DEST_PREFIX}/bin/c++")
-      FileUtils.ln_s('g++-8.2', "#{CREW_DEST_PREFIX}/bin/g++")
-      FileUtils.ln_s('cpp-8.2', "#{CREW_DEST_PREFIX}/bin/cpp")
-      FileUtils.ln_s('gcc-ar-8.2', "#{CREW_DEST_PREFIX}/bin/gcc-ar")
-      FileUtils.ln_s('gcc-nm-8.2', "#{CREW_DEST_PREFIX}/bin/gcc-nm")
-      FileUtils.ln_s('gcc-ranlib-8.2', "#{CREW_DEST_PREFIX}/bin/gcc-ranlib")
-      FileUtils.ln_s('gcov-8.2', "#{CREW_DEST_PREFIX}/bin/gcov")
-      FileUtils.ln_s('gcov-dump-8.2', "#{CREW_DEST_PREFIX}/bin/gcov-dump")
-      FileUtils.ln_s('gcov-tool-8.2', "#{CREW_DEST_PREFIX}/bin/gcov-tool")
-      FileUtils.ln_s('gfortran-8.2', "#{CREW_DEST_PREFIX}/bin/gfortran")
+      FileUtils.ln_s("gcc-#{version}", "#{CREW_DEST_PREFIX}/bin/gcc")
+      FileUtils.ln_s("c++-#{version}", "#{CREW_DEST_PREFIX}/bin/c++")
+      FileUtils.ln_s("g++-#{version}", "#{CREW_DEST_PREFIX}/bin/g++")
+      FileUtils.ln_s("cpp-#{version}", "#{CREW_DEST_PREFIX}/bin/cpp")
+      FileUtils.ln_s("gcc-ar-#{version}", "#{CREW_DEST_PREFIX}/bin/gcc-ar")
+      FileUtils.ln_s("gcc-nm-#{version}", "#{CREW_DEST_PREFIX}/bin/gcc-nm")
+      FileUtils.ln_s("gcc-ranlib-#{version}", "#{CREW_DEST_PREFIX}/bin/gcc-ranlib")
+      FileUtils.ln_s("gcov-#{version}", "#{CREW_DEST_PREFIX}/bin/gcov")
+      FileUtils.ln_s("gcov-dump-#{version}", "#{CREW_DEST_PREFIX}/bin/gcov-dump")
+      FileUtils.ln_s("gcov-tool-#{version}", "#{CREW_DEST_PREFIX}/bin/gcov-tool")
+      FileUtils.ln_s("gfortran-#{version}", "#{CREW_DEST_PREFIX}/bin/gfortran")
 
-      FileUtils.ln_s("$(gcc -dumpmachine)-c++-8.2", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-c++")
-      FileUtils.ln_s("$(gcc -dumpmachine)-g++-8.2", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-g++")
-      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-8.2", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc")
-      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-ar-8.2", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc-ar")
-      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-nm-8.2", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc-nm")
-      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-ranlib-8.2", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc-ranlib")
-      FileUtils.ln_s("$(gcc -dumpmachine)-gfortran-8.2", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gfortran")
+      FileUtils.ln_s("$(gcc -dumpmachine)-c++-#{version}", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-c++")
+      FileUtils.ln_s("$(gcc -dumpmachine)-g++-#{version}", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-g++")
+      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-#{version}", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc")
+      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-ar-#{version}", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc-ar")
+      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-nm-#{version}", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc-nm")
+      FileUtils.ln_s("$(gcc -dumpmachine)-gcc-ranlib-#{version}", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gcc-ranlib")
+      FileUtils.ln_s("$(gcc -dumpmachine)-gfortran-#{version}", "#{CREW_DEST_PREFIX}/bin/$(gcc -dumpmachine)-gfortran")
     end
   end
 end
