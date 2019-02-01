81dcee3c11a2420853a920e14ddfadbd41199abc
Ed Reel <edreel@gmail.com>
2019-02-01 08:00:08 -0600

    Double quote all #{version} lines
    Remove pre-built binaries

--- packages/gcc8.rb
+++ packages/gcc8.rb
@@ -8,16 +8,8 @@ class Gcc8 < Package
   source_sha256 '196c3c04ba2613f893283977e6011b2345d1cd1af9abeac58e916b1aab3e0080'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gcc8-8.2.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gcc8-8.2.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gcc8-8.2.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gcc8-8.2.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '536323fc5dd35e53665a19e65c636f288153efe4cdf31617c32103c64593b285',
-     armv7l: '536323fc5dd35e53665a19e65c636f288153efe4cdf31617c32103c64593b285',
-       i686: 'e37ca8d49198beaa9ba582fc25fa598ab3cd5ed7e46f96a03176fb9002cc6f7b',
-     x86_64: '6d982f3c35acd3a738e176a3f3b8c4ea093c7be86882153a7711841cf0c80737',
   })
 
   depends_on 'dejagnu' => :build # for test
@@ -53,7 +45,7 @@ class Gcc8 < Package
                  '--enable-languages=all',
                  '--enable-threads=posix',
                  '--with-tune=cortex-a15',
-                 '--program-suffix=-#{version}',
+                 "--program-suffix=-#{version}",
                  '--with-arch=armv7-a',
                  '--with-float=hard',
                  '--disable-libmpx',
@@ -70,7 +62,7 @@ class Gcc8 < Package
                  "--prefix=#{CREW_PREFIX}",
                  '--enable-languages=all',
                  '--enable-threads=posix',
-                 '--program-suffix=-#{version}',
+                 "--program-suffix=-#{version}",
                  '--with-arch-64=x86-64',
                  '--with-arch-32=i686',
                  '--disable-multilib',
