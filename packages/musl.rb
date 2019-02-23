67b17e81c209a8936b4e5b9bedf4a6c8520884e0
Ed Reel <edreel@gmail.com>
2019-02-22 23:18:34 -0600

    Add pre-built binaries for musl

--- packages/musl.rb
+++ packages/musl.rb
@@ -8,8 +8,16 @@ class Musl < Package
   source_sha256 'c742b66f6f49c9e5f52f64d8b79fecb5a0f6e0203fca176c70ca20f6be285f44'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/musl-1.1.21-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/musl-1.1.21-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/musl-1.1.21-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/musl-1.1.21-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '4ecca2c16ee80892028f740da1e18141c0dd5a618723d48e9386c8dd3b8baecf',
+     armv7l: '4ecca2c16ee80892028f740da1e18141c0dd5a618723d48e9386c8dd3b8baecf',
+       i686: '40af8e0b75a42f599f5de5002240bf43eb03df134a6356a7c22cd5e92dc94984',
+     x86_64: 'f205355013debc9b33ca9fffd46c5dda61eb74998f6ef6c310c4e2805619f83f',
   })
 
   def self.build
