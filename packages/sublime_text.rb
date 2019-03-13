ad7fc24453c2d37093271b94dc2bc15e17ad6f92
Chris Thurber <skiman6010@gmail.com>
2019-03-13 11:45:47 -0400

    Update Sublime Text to 3.2 (#3215)

--- packages/sublime_text.rb
+++ packages/sublime_text.rb
@@ -3,14 +3,14 @@ require 'package'
 class Sublime_text < Package
   description 'A sophisticated text editor for code, markup and prose.'
   homepage 'https://www.sublimetext.com/'
-  version '3.1.1'
+  version '3.2'
   case ARCH
   when 'x86_64'		  
-    source_url 'https://download.sublimetext.com/sublime_text_3_build_3176_x64.tar.bz2'
-    source_sha256 '74f17c1aec4ddec9d4d4c39f5aec0414a4755d407a05efa571e8892e0b9cf732'
+    source_url 'https://download.sublimetext.com/sublime_text_3_build_3200_x64.tar.bz2'
+    source_sha256 'b9d5297c65bd14a8ba3394e74dbd9a5013b3fed5a5a2f8f5120d2cec40614fbd'
   when 'i686'
-    source_url 'https://download.sublimetext.com/sublime_text_3_build_3176_x32.tar.bz2'
-    source_sha256 '43e49dfdbea61cc0a1b6ebcdf0ff522d8531e6998e05f4756ba4218446f85a21'
+    source_url 'https://download.sublimetext.com/sublime_text_3_build_3200_x32.tar.bz2'
+    source_sha256 'f94772aa6313f228a930ae85573bb18411e96c60c3c14027e9c27c14e5aa7906'
   end
   
   binary_url ({
