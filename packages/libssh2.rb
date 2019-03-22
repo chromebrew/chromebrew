211e63b31e89c9c95b20aac78d94ff818979de5c
James Larrowe <larrowe.semaj11@gmail.com>
2019-03-22 10:53:18 -0400

    Update libssh2 package (#3151)
    * Update libssh2 package to 1.8.1
    
    * Add pre-built binaries for libssh2
    
    * Update binaries for libssh2 in install.sh

--- packages/libssh2.rb
+++ packages/libssh2.rb
@@ -3,42 +3,40 @@ require 'package'
 class Libssh2 < Package
   description 'libssh2 is a client-side C library implementing the SSH2 protocol.'
   homepage 'https://www.libssh2.org/'
-  version '1.8.0'
-  source_url 'https://www.libssh2.org/download/libssh2-1.8.0.tar.gz'
-  source_sha256 '39f34e2f6835f4b992cafe8625073a88e5a28ba78f83e8099610a7b3af4676d4'
+  version '1.8.1'
+  source_url 'https://www.libssh2.org/download/libssh2-1.8.1.tar.gz'
+  source_sha256 '40b517f35b1bb869d0075b15125c7a015557f53a5a3a6a8bffb89b69fd70f159'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.8.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '6fa84296583273dd9e749a2c54cb1cf688a7dab032e2528de5944a4d9777f037',
-     armv7l: '6fa84296583273dd9e749a2c54cb1cf688a7dab032e2528de5944a4d9777f037',
-       i686: '771b2d30a49dd691db8456f773da404753d368f3c31d03c682c552ea0b5eb65e',
-     x86_64: '6e026450389021c6267a9cc79b8722d15f48e2f8d812d5212501f686b4368e3c',
+    aarch64: 'd8fae766d77818ef95ae8a993bda0c03730d773cfb8392ceaaf6c8697206409c',
+     armv7l: 'd8fae766d77818ef95ae8a993bda0c03730d773cfb8392ceaaf6c8697206409c',
+       i686: 'ccdea94d6dcc0bdd27b364cd99f53e9aba6f90f0b8272a5f3856bf2a9a32beb6',
+     x86_64: '1025b413f30c5ac27bfa340e41cb437bf19311e72b95030f64020e25be4cda31',
   })
 
-  depends_on 'openssl' => :build
-  depends_on 'zlibpkg' => :build
+  def self.patch
+    system 'sed', '-i', '/TESTS =/s, mansyntax.sh,,g', 'tests/Makefile.in'
+  end
 
   def self.build
-    system "./configure", "--libdir=#{CREW_LIB_PREFIX}", "--with-zlib", "--with-openssl", "--with-pic", "--disable-static"
-    system "make"
-    system "find . -name '*.so.*' -print | xargs strip -S"
+    system './configure',
+           '--with-libz',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
-    # Ignore mansyntax.sh test which checks man page syntax errors.
-    # It causes 'col: Invalid or incomplete multibyte or wide character'
-    # error at hyphenations.  It is mandb's problem.
-    system "sed -i tests/Makefile -e '/TESTS =/s/mansyntax.sh//'"
-
-    system "make", "check"
+    system 'make', 'check'
   end
 end
