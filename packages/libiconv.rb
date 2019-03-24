1cf0e6ae6bd7b883147dbe88ec746ddf04f4ac18
Ed Reel <edreel@gmail.com>
2019-03-24 12:55:37 -0500

    Add pre-built binaries

--- packages/libiconv.rb
+++ packages/libiconv.rb
@@ -8,10 +8,18 @@ class Libiconv < Package
   source_sha256 'ccf536620a45458d26ba83887a983b96827001e92a13847b45e4925cc8913178'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.15-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.15-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.15-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.15-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'cfa05a46d37d49479047f205e3d4ab2a8c11f953ed91e11465a1195ff2d95d01',
+     armv7l: 'cfa05a46d37d49479047f205e3d4ab2a8c11f953ed91e11465a1195ff2d95d01',
+       i686: 'b080cd1e667dd9efba1a4392c7c177f5b8292f1b6fc29e862634b7ad41d29ab5',
+     x86_64: '7ac97b03fff5d1befecb26ac471daa239c2c23ab1bc774a5366e6c46d1bb9ad3',
   })
-  
+
   def self.build
     system './configure',
            '--enable-shared',
