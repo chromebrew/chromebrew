c21c088c403b4db6a296db7983342fe7ead0bad9
Ed Reel <edreel@gmail.com>
2019-03-25 00:37:27 -0500

    Add pre-built binaries for bdwgc, libatomic_ops, neofetch and tinycc packages

--- packages/bdwgc.rb
+++ packages/bdwgc.rb
@@ -8,8 +8,16 @@ class Bdwgc < Package
   source_sha256 '436a0ddc67b1ac0b0405b61a9675bca9e075c8156f4debd1d06f3a56c7cd289d'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bdwgc-8.0.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bdwgc-8.0.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bdwgc-8.0.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bdwgc-8.0.4-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '876731fcfc8923fb084dceaed37bb98c6a74db13b526b38a3b3b6fa8dd5393c9',
+     armv7l: '876731fcfc8923fb084dceaed37bb98c6a74db13b526b38a3b3b6fa8dd5393c9',
+       i686: 'be933d4e732ba200b3c3c0bea9c163552cabe118d157d02b713e21dbc896eac2',
+     x86_64: '3195a44840152f6e67ca5f8f27118186af4422d7ab145e0276aa0af4dfd84378',
   })
 
   depends_on 'libatomic_ops'
