e16536ed19be22a7bd66dc250664018d411eaab2
Ed Reel <edreel@gmail.com>
2019-04-08 09:45:33 -0500

    Update yarn from 1.13.0 to 1.15.2 (#3240)

--- packages/yarn.rb
+++ packages/yarn.rb
@@ -3,9 +3,9 @@ require 'package'
 class Yarn < Package
   description 'Yarn is a new package manager for JavaScript and an alternative to npm.'
   homepage 'https://yarnpkg.com/en/'
-  version '1.13.0'
-  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.13.0/yarn-v1.13.0.tar.gz'
-  source_sha256 '125d40ebf621ebb08e3f66a618bd2cc5cd77fa317a312900a1ab4360ed38bf14'
+  version '1.15.2'
+  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.15.2/yarn-v1.15.2.tar.gz'
+  source_sha256 'c4feca9ba5d6bf1e820e8828609d3de733edf0e4722d17ed7ce493ed39f61abd'
 
   binary_url ({
   })
