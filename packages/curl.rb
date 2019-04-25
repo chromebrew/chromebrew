b5ad8cdf06d162f974253df10d5e6cf9fa95cbcb
Ed Reel <edreel@gmail.com>
2019-04-25 10:00:38 -0500

    Add pre-built binaries for libnghttp2 and curl (#3255)

--- packages/curl.rb
+++ packages/curl.rb
@@ -8,16 +8,16 @@ class Curl < Package
   source_sha256 '9252332a7f871ce37bfa7f78bdd0a0e3924d8187cc27cb57c76c9474a7168fb3'
 
   binary_url ({
-    aarch64: '',
-     armv7l: '',
-       i686: '',
-     x86_64: '',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.64.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.64.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.64.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.64.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '',
-     armv7l: '',
-       i686: '',
-     x86_64: '',
+    aarch64: '19860f4f84b100e306940aebd25c389e64fd87941b02daae842211b5f9ca4123',
+     armv7l: '19860f4f84b100e306940aebd25c389e64fd87941b02daae842211b5f9ca4123',
+       i686: '1545311084cc3db7252883fc80cb5eb12a0c519dae91cdd96f5a449fa2c278e5',
+     x86_64: 'b00a4605990253654ca1a57359d043c614a8e28acab6981c344ff2b45c5ff3ea',
   })
 
   depends_on 'groff' => :build
