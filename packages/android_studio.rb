1f18507e339ba7c4bb9c1b2b065493684860a21a
Ed Reel <edreel@gmail.com>
2019-05-13 11:35:54 -0500

    Update android_studio from 3.3.1.0 to 3.4.0.18 (#3267)

--- packages/android_studio.rb
+++ packages/android_studio.rb
@@ -3,19 +3,17 @@ require 'package'
 class Android_studio < Package
   description 'Android Studio is the official IDE for Android development.'
   homepage 'https://developer.android.com/studio'
-  version '3.3.1.0'
-  source_url 'https://dl.google.com/dl/android/studio/ide-zips/3.3.1.0/android-studio-ide-182.5264788-linux.zip'
-  source_sha256 '82603100f772baf5b6b54532375ada756b4b2dc11854cabae267cd8bd8c4f039'
+  version '3.4.0.18'
+  source_url 'https://dl.google.com/dl/android/studio/ide-zips/3.4.0.18/android-studio-ide-183.5452501-linux.tar.gz'
+  source_sha256 'ad2bd4be87a55cfaeee6f28d40d925691314c11862b303d411f9776b76fa1c45'
 
   depends_on 'jdk8'
   depends_on 'xdg_base'
   depends_on 'sommelier'
 
-  abort 'Only x86_64 architecture is supported.'.lightred unless ARGV[0] == 'search' || ARCH == 'x86_64'
-
   if ARGV[0] == 'install' || ARGV[0] == 'upgrade' then
-    free_space = `echo $(($(stat -f --format="%a*%S" .)))`.chomp
-    abort 'Not enough free disk space.  You need at least 6 GB to install.'.lightred if free_space < '6442450944'
+    free_space = `echo $(($(stat -f --format="%a*%S" .)))`.chomp.to_i
+    abort 'Not enough free disk space.  You need at least 6 GB to install.'.lightred if free_space < 6442450944
   end
 
   def self.install
@@ -25,11 +23,11 @@ class Android_studio < Package
     FileUtils.cd(CREW_DEST_PREFIX + '/bin') do
       FileUtils.ln_s(CREW_PREFIX + '/share/android-studio/bin/studio.sh', 'studio')
     end
-    FileUtils.mkdir_p(CREW_DEST_PREFIX + '/.config/.AndroidStudio3.3')
+    FileUtils.mkdir_p(CREW_DEST_PREFIX + '/.config/.AndroidStudio3.4')
     FileUtils.mkdir_p(CREW_DEST_PREFIX + '/.config/Android')
     FileUtils.mkdir_p(CREW_DEST_HOME)
     FileUtils.cd(CREW_DEST_HOME) do
-      FileUtils.ln_sf(CREW_PREFIX + '/.config/.AndroidStudio3.3/', '.AndroidStudio3.3')
+      FileUtils.ln_sf(CREW_PREFIX + '/.config/.AndroidStudio3.4/', '.AndroidStudio3.4')
       FileUtils.ln_sf(CREW_PREFIX + '/.config/Android/', 'Android')
     end
   end
