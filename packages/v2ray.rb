7f6e771cdd8c22e1a46bc430cba675dbeb5fa466
dx0eu <0xdanger@protonmail.com>
2019-05-17 02:53:48 +0000

    v2ray v4.18.0

--- packages/v2ray.rb
+++ packages/v2ray.rb
@@ -4,26 +4,32 @@ class V2ray < Package
   description 'A platform for building proxies to bypass network restrictions.'
   homepage 'https://www.v2ray.com/'
   version 'v4.18.0'
-  source_url 'https://github.com/v2ray/v2ray-core/archive/v4.18.0.zip'
-  source_sha256 '95068cbae73a3c11d332f82241d3431e203babb0e24afb9237c8e6df17f1f6dd'
 
-  binary_url ({
-    aarch64: 'https://github.com/dx0eu/v2rew/releases/download/v4.18.0/v2ray-chromeos-aarch64.tar.xz',
-    armv7l: 'https://github.com/dx0eu/v2rew/releases/download/v4.18.0/v2ray-chromeos-armv7l.tar.xz',
-      i686: 'https://github.com/dx0eu/v2rew/releases/download/v4.18.0/v2ray-chromeos-i686.tar.xz',
-    x86_64: 'https://github.com/dx0eu/v2rew/releases/download/v4.18.0/v2ray-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'ef0da5acfe0512d9b8d21d10b93ed7e5ef3d490df77f884d8f13af33f20c4879',
-    armv7l: 'dcab9210822e614ee497444ca44fcadfc9c3db084ba3566e09bdd8ac13286466',
-      i686: '6f1fb32a74732f2c8271897aa92f00dea941bdc1998e9a5b44d578e62c0cdc1e',
-    x86_64: '7bc56c99362d2dfa846c067d9a842e67c0912eb89c5708f9929b6c9866eea91d',
-  })
+  case ARCH
+  when 'aarch64', 'armv7l'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.18.0/v2ray-linux-arm.zip'
+    source_sha256 '9bb0b299c071cf3131dfa6eef822841d6b3fad4f9877a040f70e44e9f7f7d848'
+  when 'i686'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.18.0/v2ray-linux-32.zip'
+    source_sha256 'dc9876be5c9631fc23c95d934abb049639e67aa6731fa2b7ba7f160441c44a7d'
+  when 'x86_64'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.18.0/v2ray-linux-64.zip'
+    source_sha256 'e3b518795c76b26b6efdc837ebdf01a401b0a153c9758e928bd1cd4df35631ed'
+  end
+
+  def self.install
+    FileUtils.mkdir_p(CREW_DEST_PREFIX + '/share/v2ray')
+    FileUtils.cp_r('.', CREW_DEST_PREFIX + '/share/v2ray')
+    FileUtils.mkdir_p(CREW_DEST_PREFIX + '/bin')
+    FileUtils.cd(CREW_DEST_PREFIX + '/bin') do
+      FileUtils.ln_s(CREW_PREFIX + '/share/v2ray/v2ray', 'v2ray')
+      FileUtils.ln_s(CREW_PREFIX + '/share/v2ray/v2ctl', 'v2ctl')
+    end
+  end
 
   def self.postinstall
     FileUtils.chmod('u=x,go=x', CREW_PREFIX + '/share/v2ray/v2ray')
     FileUtils.chmod('u=x,go=x', CREW_PREFIX + '/share/v2ray/v2ctl')
-
     puts
     puts 'To start using v2ray, type `v2ray`.'.lightblue
     puts
