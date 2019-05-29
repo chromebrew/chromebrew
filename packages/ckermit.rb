31bdc714436aba6c53f9d300cce0b16f86bbe9e8
Lloyd Huang - chromebook <lloyd.tw@gmail.com>
2019-05-29 21:20:12 +0800

    for short description. Change CREW_DEST_DIR to CREW_DEST_PREFIX

--- packages/ckermit.rb
+++ packages/ckermit.rb
@@ -1,34 +1,20 @@
 require 'package'
 
 class Ckermit < Package
-  description 'C-Kermit is a combined serial and network communication software package offering a consistent, medium-independent, secure cross-platform approach to connection establishment, terminal sessions, file transfer, character-set translation, and automation of communication tasks.'
+  description 'C-Kermit is a combined serial and network communication software package.'
   homepage 'http://www.kermitproject.org/ck90.html'
-  version '211'
+  version '8.0.211'
   source_url 'https://fossies.org/linux/privat/old/cku211.tar.gz'
   source_sha256 '0503518ef8f225d06749f7923ae84521bc358daf353222c75c009cd040dc5e26'
-
-  binary_url ({
-    aarch64: '',
-     armv7l: '',
-       i686: '',
-     x86_64: '',
-  })
-  binary_sha256 ({
-    aarch64: '',
-     armv7l: '',
-       i686: '',
-     x86_64: '',
-  })
 
   depends_on 'ncurses'
 
   def self.build
     system 'make KFLAGS="-DCK_NCURSES -I/usr/local/include/ncurses" LNKFLAGS="-lresolv -lcrypt" linux'
-
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_DIR}/usr/local"
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system "mkdir -p #{CREW_DEST_PREFIX}/usr/local"
+    system 'make', "DESTDIR=#{CREW_DEST_PREFIX}", 'install'
   end
 end
