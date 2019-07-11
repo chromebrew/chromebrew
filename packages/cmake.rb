f77ab842ea4b595068a5247d006622994a325054
Ed Reel <edreel@gmail.com>
2019-07-11 10:13:57 -0500

    Update cmake from 3.14.3 to 3.14.5 (#3361)
    Add pre-built binaries

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,21 +3,21 @@ require 'package'
 class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  version '3.14.3'
-  source_url 'https://cmake.org/files/v3.14/cmake-3.14.3.tar.gz'
-  source_sha256 '215d0b64e81307182b29b63e562edf30b3875b834efdad09b3fcb5a7d2f4b632'
+  version '3.14.5'
+  source_url 'https://cmake.org/files/v3.14/cmake-3.14.5.tar.gz'
+  source_sha256 '505ae49ebe3c63c595fa5f814975d8b72848447ee13b6613b0f8b96ebda18c06'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.5-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.5-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.5-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.14.5-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '53ff9ffbb8894a4fa0951125223001085f3854058c83485baa99305b37a40f10',
-     armv7l: '53ff9ffbb8894a4fa0951125223001085f3854058c83485baa99305b37a40f10',
-       i686: '720e5aeafd1435966efe5979a1a88025540c792a71227473a351be61aabd8362',
-     x86_64: '68bb956136478169cc74d43e59a3c055d9e565c6f0b93a1fcbf7e56f3912d580',
+    aarch64: 'd07a9fe9e229760fb27ef66b4c32021293e6b47dbf26f24220c264b23c3f4014',
+     armv7l: 'd07a9fe9e229760fb27ef66b4c32021293e6b47dbf26f24220c264b23c3f4014',
+       i686: '76c3548ddcc06811e74a4811c40fa2e2b03ed16be4ab374a9797e350f13fe38d',
+     x86_64: 'ee3c221fb737141c8ca18418732d9b8ef7a0f0729b4990161690e88555b10412',
   })
 
   def self.build
@@ -29,6 +29,6 @@ class Cmake < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
