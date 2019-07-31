ea85c10e79176e7a16569f5d26ca2d896d275f27
James Larrowe <larrowe.semaj11@gmail.com>
2019-07-31 15:07:15 -0400

    Update HAproxy package to 2.0.3 (#3384)

--- packages/haproxy.rb
+++ packages/haproxy.rb
@@ -2,28 +2,18 @@ require 'package'
 
 class Haproxy < Package
   description 'The Reliable, High Performance TCP/HTTP Load Balancer.'
-  homepage 'http://www.haproxy.org/'
-  version '1.9.8'
-  source_url 'http://www.haproxy.org/download/1.9/src/haproxy-1.9.8.tar.gz'
-  source_sha256 '2d9a3300dbd871bc35b743a83caaf50fecfbf06290610231ca2d334fd04c2aee'
+  homepage 'https://www.haproxy.org/'
+  version '2.0.3'
+  source_url 'https://www.haproxy.org/download/2.0/src/haproxy-2.0.3.tar.gz'
+  source_sha256 'aac1ff3e5079997985b6560f46bf265447d0cd841f11c4d77f15942c9fe4b770'
 
-  binary_url ({
-      i686: 'https://dl.bintray.com/chromebrew/chromebrew/haproxy-1.9.8-chromeos-i686.tar.xz',
-    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/haproxy-1.9.8-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-      i686: '895f482eb4f01aa8b846f253064280c6b42b886add6432b65e228b07593d8508',
-    x86_64: 'a3db1613a3c363a4f09de71efcecbc70fc08efff257f1dd74fd6863c429d59a3',
-  })
-
-  depends_on 'openssl'
-  depends_on 'pcre'
+  depends_on 'pcre2'
 
   def self.build
-    system "make TARGET=linux26 USE_OPENSSL=1 USE_PCRE=1 USE_ZLIB=1"
+    system 'make', 'TARGET=linux-glibc', 'USE_OPENSSL=1', 'USE_PCRE2_JIT=1', 'USE_ZLIB=1', 'DEBUG=-s'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
