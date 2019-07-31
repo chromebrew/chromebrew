f5b092ce0623aeec9a9a9e47277fb530ef3c7f50
Ed Reel <edreel@gmail.com>
2019-07-31 10:12:23 -0500

    Add pre-built binaries for cpustat and xxhash (#3383)

--- packages/cpustat.rb
+++ packages/cpustat.rb
@@ -8,8 +8,16 @@ class Cpustat < Package
   source_sha256 'fe48369cf031ecf5edd77f585c66b66d3c6e71a0933230502e1a665fcc86aa8b'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cpustat-0.02.09-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cpustat-0.02.09-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cpustat-0.02.09-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cpustat-0.02.09-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '14b4aa6110904b31c0259f3bc6aeba5efeca97b8c8d95a6342fd1bbf2b81f426',
+     armv7l: '14b4aa6110904b31c0259f3bc6aeba5efeca97b8c8d95a6342fd1bbf2b81f426',
+       i686: '7eab479cc88afe2a1336f5a0c091bcf44751a561c911f77fce86e3b29abe2e59',
+     x86_64: 'aa9a721eaa7cd8c54306e744cf40d40db278742a9542a69956b68d3c7f42fdae',
   })
 
   def self.build
--- packages/xxhash.rb
+++ packages/xxhash.rb
@@ -8,8 +8,16 @@ class Xxhash < Package
   source_sha256 'b34792646d5e19964bb7bba24f06cb13aecaac623ab91a54da08aa19d3686d7e'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xxhash-0.7.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xxhash-0.7.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xxhash-0.7.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xxhash-0.7.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '9773069f3ec4801c0a77b6e44f9cf32f2a3de8d64e4bb328cf9bcfd311f07b59',
+     armv7l: '9773069f3ec4801c0a77b6e44f9cf32f2a3de8d64e4bb328cf9bcfd311f07b59',
+       i686: '6ab164331e6031f36742b34e20d58935d86bb427e7790c1fcdf3d86b59df8a84',
+     x86_64: '96c0b3684f2e9579764d583c5a0a66d7284d6d586d3fdba03ac3907162142ff2',
   })
 
   def self.build
