920ae703f9ade4aabcb0d51cd459090c0d8c4565
Ed Reel <edreel@gmail.com>
2019-07-31 23:24:26 -0500

    Add pre-built binaries for haproxy

--- packages/haproxy.rb
+++ packages/haproxy.rb
@@ -7,6 +7,19 @@ class Haproxy < Package
   source_url 'https://www.haproxy.org/download/2.0/src/haproxy-2.0.3.tar.gz'
   source_sha256 'aac1ff3e5079997985b6560f46bf265447d0cd841f11c4d77f15942c9fe4b770'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/haproxy-2.0.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/haproxy-2.0.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/haproxy-2.0.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/haproxy-2.0.3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'a39b9aed16ef1c7847900c7e4d35deaf7fa3bb704f95a15544db010798b276c0',
+     armv7l: 'a39b9aed16ef1c7847900c7e4d35deaf7fa3bb704f95a15544db010798b276c0',
+       i686: '44b553dcc063d51b090a51c67c2755a5d6412215cc8c8804348d783fe1195cac',
+     x86_64: '4de3e8213ea510996e81039fbd6dd9e5d4b15de6728cded2a0a12763a56d5f2a',
+  })
+
   depends_on 'pcre2'
 
   def self.build
