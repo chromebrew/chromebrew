a765ce69c49f2eaa555385b1f4fb1484cc5e54f2
Ed Reel <edreel@gmail.com>
2019-08-03 08:21:44 -0500

    Add scientific computing libraries
    Add the following:
      arpack_ng.rb
      hdf5.rb
      openmpi.rb
      superlu.rb
    
    Rename liblapack.rb to lapack.rb and update
    
    Update armadillo from 8.400.0 to 9.600.5
    
    Add pre-built binaries

---
+++ packages/arpack_ng.rb
@@ -0,0 +1,66 @@
+require 'package'
+
+class Arpack_ng < Package
+  description 'Collection of Fortran77 subroutines designed to solve large scale eigenvalue problems.'
+  homepage 'https://github.com/opencollab/arpack-ng'
+  version '3.7.0'
+  source_url 'https://github.com/opencollab/arpack-ng/archive/3.7.0.tar.gz'
+  source_sha256 '972e3fc3cd0b9d6b5a737c9bf6fd07515c0d6549319d4ffb06970e64fa3cc2d6'
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/arpack_ng-3.7.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/arpack_ng-3.7.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/arpack_ng-3.7.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/arpack_ng-3.7.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '8ac867cd31ef8c507039105de1e6467807c30a75f1752ab43650d82348b558d0',
+     armv7l: '8ac867cd31ef8c507039105de1e6467807c30a75f1752ab43650d82348b558d0',
+       i686: '8c2096851e7868c456220480b315d8ba84b5c188d8bf193cf8ce8abea6b20eb4',
+     x86_64: '83b1d11cbc66d977dc2287f8273a39527bd33c302b81baaa84172c110b6aeed1',
+  })
+
+  depends_on 'lapack'
+  depends_on 'openblas'
+  depends_on 'openmpi'
+  depends_on 'ld_default' => :build
+
+  def self.patch
+    # Fix Unknown CMake command "check_symbol_exists".
+    # See https://techoverflow.net/2019/06/20/how-to-fix-unknown-cmake-command-check_symbol_exists/.
+    system "sed -i '1iinclude(CheckSymbolExists)' CMakeLists.txt"
+  end
+
+  def self.build
+    Dir.mkdir 'build'
+    Dir.chdir 'build' do
+      suffix = ''
+      interface = 'OFF'
+      if ARCH == 'x86_64'
+        suffix = '64'
+        interface = 'ON'
+      end
+      # Use the gold linker. 
+      old_ld = `ld_default g`.chomp
+      system 'cmake',
+             '-DCMAKE_BUILD_TYPE=Release',
+             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
+             "-DBLAS_LIBRARIES=#{CREW_LIB_PREFIX}/libopenblas.so",
+             "-DINTERFACE64=#{interface}",
+             "-DLIBSUFFIX=#{suffix}",
+             '-DBUILD_SHARED_LIBS=ON',
+             '-DEXAMPLES=ON',
+             '-DICB=ON',
+             '-DMPI=ON',
+             '..'
+      system 'make'
+      system 'ld_default', "#{old_ld}"
+    end
+  end
+
+  def self.install
+    Dir.chdir 'build' do
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    end
+  end
+end
---
+++ packages/superlu.rb
@@ -0,0 +1,47 @@
+require 'package'
+
+class Superlu < Package
+  description 'SuperLU is a general purpose library for the direct solution of large, sparse, nonsymmetric systems of linear equations.'
+  homepage 'https://portal.nersc.gov/project/sparse/superlu/'
+  version '5.2.1'
+  source_url 'https://github.com/xiaoyeli/superlu/archive/v5.2.1.tar.gz'
+  source_sha256 '77582501dedef295eb74e4dc9433e2816d2d8be211eae307379c13d93c65bc71'
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/superlu-5.2.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/superlu-5.2.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/superlu-5.2.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/superlu-5.2.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'ba7c045b4aefe809e33de9fe35476cca3ed62af1e38ec162c5e199d36d8181e1',
+     armv7l: 'ba7c045b4aefe809e33de9fe35476cca3ed62af1e38ec162c5e199d36d8181e1',
+       i686: '5f72a5326b985427af744b7de4d379141bb71acf3a0611726ff14fc8832019b5',
+     x86_64: '349e683c9d47ecd626543ed10fd2a76561eeb122ecfff7e45d67798db53c5d2c',
+  })
+
+  def self.build
+    Dir.mkdir 'build'
+    Dir.chdir 'build' do
+      system 'cmake',
+             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
+             "-DCMAKE_INSTALL_LIBDIR=#{ARCH_LIB}",
+             '-DCMAKE_BUILD_TYPE=Release',
+             '-DBUILD_SHARED_LIBS=ON',
+             '..'
+      system 'make'
+    end
+  end
+
+  def self.check
+    Dir.chdir 'build' do
+      system 'make', 'test'
+    end
+  end
+
+  def self.install
+    Dir.chdir 'build' do
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    end
+  end
+end
