67daca516f4ae767e55e5f08ed3d9d8b1f21886e
Ed Reel <edreel@gmail.com>
2019-08-04 20:12:49 -0500

    Update tk from 8.6.8 to 8.6.9
    Add libx11 dependency
    
    Change dependency from libx11 to xorg_lib
    
    Add pre-built binaries

--- packages/tk.rb
+++ packages/tk.rb
@@ -3,23 +3,24 @@ require 'package'
 class Tk < Package
   description 'Tk is a graphical user interface toolkit that takes developing desktop applications to a higher level than conventional approaches.'
   homepage 'http://www.tcl.tk/'
-  version '8.6.8'
-  source_url 'https://prdownloads.sourceforge.net/tcl/tk8.6.8-src.tar.gz'
-  source_sha256 '49e7bca08dde95195a27f594f7c850b088be357a7c7096e44e1158c7a5fd7b33'
+  version '8.6.9'
+  source_url 'https://downloads.sourceforge.net/tcl/tk8.6.9-src.tar.gz'
+  source_sha256 'd3f9161e8ba0f107fe8d4df1f6d3a14c30cc3512dfc12a795daa367a27660dac'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.8-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.8-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.8-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.8-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.9-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.9-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.9-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.9-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '0c12b3457d4a6ea994c11b659d9e88058de96bdc1202eaed31db1e1975d0e871',
-     armv7l: '0c12b3457d4a6ea994c11b659d9e88058de96bdc1202eaed31db1e1975d0e871',
-       i686: '854b3f056b6f1b85f2ab65cd2fbed9d568bef018cdc9d887ec157467f3835f51',
-     x86_64: '0d036e369efa5c3199ebeaf9ae71e5dea15b982fa3d885fbb6b8dcd19cfdf3e3',
+    aarch64: '79f0e0905a1809f177b9ab3048168acb45491b58e9460ec1254f307397239b67',
+     armv7l: '79f0e0905a1809f177b9ab3048168acb45491b58e9460ec1254f307397239b67',
+       i686: '18708ac8f18cde32ef3787ec7a3ce1721a1243dbfb73b231b3b71995afe4d0de',
+     x86_64: 'd6e901e0e1f86d87d4f97e88a73380e98fdf73bdec4c27832c4f3feb6af953a9',
   })
 
+  depends_on 'xorg_lib'
   depends_on 'tcl'
 
   def self.build
@@ -28,6 +29,7 @@ class Tk < Package
         system './configure',
                "--prefix=#{CREW_PREFIX}",
                "--libdir=#{CREW_LIB_PREFIX}",
+               "--mandir=#{CREW_PREFIX}/share/man",
                "--with-tcl=#{CREW_LIB_PREFIX}",
                '--enable-threads',
                '--enable-64bit'
@@ -35,6 +37,7 @@ class Tk < Package
         system './configure',
                "--prefix=#{CREW_PREFIX}",
                "--libdir=#{CREW_LIB_PREFIX}",
+               "--mandir=#{CREW_PREFIX}/share/man",
                "--with-tcl=#{CREW_LIB_PREFIX}",
                '--enable-threads',
                '--disable-64bit'
@@ -46,6 +49,7 @@ class Tk < Package
   def self.install
     FileUtils.chdir('unix') do
       system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+      FileUtils.ln_s "#{CREW_PREFIX}/bin/wish8.6", "#{CREW_DEST_PREFIX}/bin/wish"
     end
   end
 end
