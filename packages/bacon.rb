928956e9ab5b5379fb5c94b09b593001a50f2022
Ed Reel <edreel@gmail.com>
2019-08-06 12:43:20 -0500

    Add pre-built binaries for bacon and gn (#3400)

--- packages/bacon.rb
+++ packages/bacon.rb
@@ -8,8 +8,16 @@ class Bacon < Package
   source_sha256 'ad1da72c92f8956b18cab9bd7cc505eb7f5515217f02ae47545328743195591f'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bacon-3.9.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bacon-3.9.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bacon-3.9.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bacon-3.9.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'cca03b628bbec3fdf951b933a7b20a037ab23b716ff70e6c2aa895f4fdb0e140',
+     armv7l: 'cca03b628bbec3fdf951b933a7b20a037ab23b716ff70e6c2aa895f4fdb0e140',
+       i686: 'de727e4f192b1b493fdc7cf5cee8d44d3c1270233647069f0d1e5f12e3bef9c0',
+     x86_64: '884601d8d402237c5827af5aebde04ad1b44ab82b6acd6d1b05f763495858689',
   })
 
   def self.build
