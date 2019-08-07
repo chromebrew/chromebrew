0966ba7dbbdb82bb507feb0cb6c24995767daced
Casey Strouse <casey.strouse@gmail.com>
2019-08-06 21:30:51 -0700

    Add faultstat package (#3403)
    * Add faultstat package
    
    Faultstat measures page fault activity and swap utilization of processes. Tested on ARM.
    
    * Change MANPATH to use crew default.

---
+++ packages/faultstat.rb
@@ -0,0 +1,23 @@
+require 'package'
+
+class Faultstat < Package
+  description 'Faultstat measures page fault activity and swap utilization of processes'
+  homepage 'https://kernel.ubuntu.com/~cking/faultstat/'
+  version '0.01.01'
+  source_url 'https://kernel.ubuntu.com/~cking/tarballs/faultstat/faultstat-0.01.01.tar.xz'
+  source_sha256 '81218818fe7498411797289bdd0967e82665d2065407be8b5335eaf2959b8991'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  def self.build
+    system "CPPFLAGS=-I#{CREW_PREFIX}/include/ncurses make" 
+  end
+
+  def self.install
+    system "install -Dm755 faultstat #{CREW_DEST_PREFIX}/bin/faultstat"
+    system "install -Dm644 faultstat.8 #{CREW_DEST_PREFIX}/share/man/man8/faultstat.8"
+  end
+end
