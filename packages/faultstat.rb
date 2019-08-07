43ec6527f316675123fea99ec7826dd2c1e9f4bb
Casey Strouse <casey.strouse@gmail.com>
2019-08-06 21:10:25 -0700

    Change MANPATH to use crew default.

--- packages/faultstat.rb
+++ packages/faultstat.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Faultstat < Package
   description 'Faultstat measures page fault activity and swap utilization of processes'
-  homepage 'http://kernel.ubuntu.com/~cking/faultstat/'
+  homepage 'https://kernel.ubuntu.com/~cking/faultstat/'
   version '0.01.01'
   source_url 'https://kernel.ubuntu.com/~cking/tarballs/faultstat/faultstat-0.01.01.tar.xz'
   source_sha256 '81218818fe7498411797289bdd0967e82665d2065407be8b5335eaf2959b8991'
@@ -18,6 +18,6 @@ class Faultstat < Package
 
   def self.install
     system "install -Dm755 faultstat #{CREW_DEST_PREFIX}/bin/faultstat"
-    system "install -Dm644 faultstat.8 #{CREW_DEST_PREFIX}/man/man8/faultstat.8"
+    system "install -Dm644 faultstat.8 #{CREW_DEST_PREFIX}/share/man/man8/faultstat.8"
   end
 end
