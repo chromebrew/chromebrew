793dc1807a62f004463b595038197f2393a48e61
Ed Reel <edreel@gmail.com>
2019-08-07 23:47:19 -0500

    Add pre-built binares for a-f packages

--- packages/chicken.rb
+++ packages/chicken.rb
@@ -8,8 +8,16 @@ class Chicken < Package
   source_sha256 '5c1101a8d8faabfd500ad69101e0c7c8bd826c68970f89c270640470e7b84b4b'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-5.1.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-5.1.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-5.1.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/chicken-5.1.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '508dcf75c52f3d316a9479e593654a6db5fd8cfa54e874e673750f446a0ed8c4',
+     armv7l: '508dcf75c52f3d316a9479e593654a6db5fd8cfa54e874e673750f446a0ed8c4',
+       i686: '608ec4490b87f1b125d0577a2fc91bbc8a2d4031a4340307f7a8194ff73b2aa0',
+     x86_64: '43218a8fdc1f7118dd93b3955da341c76b1689d1c29615eccb378beb450ae176',
   })
 
   depends_on 'filecmd'
--- packages/docutils.rb
+++ packages/docutils.rb
@@ -8,8 +8,16 @@ class Docutils < Package
   source_sha256 'c35e87e985f70106f6f97e050f3bed990641e0e104566134b9cd23849a460e96'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/docutils-0.15-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/docutils-0.15-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/docutils-0.15-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/docutils-0.15-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'b5e6ebde642f897d08eed30a521daaea456ac179d36a27b002205ab82ddb2a42',
+     armv7l: 'b5e6ebde642f897d08eed30a521daaea456ac179d36a27b002205ab82ddb2a42',
+       i686: 'e074c6b0893088d27811967dc9f3d9e7321d9a7704f1e468af1564261862cdd6',
+     x86_64: '08863068ca607c70717d9a7054ecae077ce8466fe9b06d4e3504b2e7561ee7b7',
   })
 
   depends_on 'python3'
