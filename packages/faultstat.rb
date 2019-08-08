85429e3b555fdeb45d7dbec8ebe5fe814901ea9b
Ed Reel <edreel@gmail.com>
2019-08-08 09:11:33 -0500

    Change man path for eventstat (#3408)
    Add pre-built binaries for d-s packages

--- packages/faultstat.rb
+++ packages/faultstat.rb
@@ -8,8 +8,16 @@ class Faultstat < Package
   source_sha256 '81218818fe7498411797289bdd0967e82665d2065407be8b5335eaf2959b8991'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/faultstat-0.01.01-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/faultstat-0.01.01-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/faultstat-0.01.01-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/faultstat-0.01.01-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '4e77ced9098ba0ceefd750295c6dfa4bd032136d62dd5ca8b80252e9307206c5',
+     armv7l: '4e77ced9098ba0ceefd750295c6dfa4bd032136d62dd5ca8b80252e9307206c5',
+       i686: 'd06fb76f7101afbdf7faddb3d071462c5636b2f5c14d70361d844d926fc33655',
+     x86_64: '6a2eb859919e7ad7e5c6b58541cf04f8c7ab42dd7f03112ddfda6c27198a9053',
   })
 
   def self.build
