b41dfea1ac103b17d7f9dc1be1da1be17c4c0dac
Ed Reel <edreel@gmail.com>
2019-08-19 09:52:26 -0500

    Update yarn from 1.16.0 to 1.17.3 (#3428)

--- packages/yarn.rb
+++ packages/yarn.rb
@@ -3,16 +3,16 @@ require 'package'
 class Yarn < Package
   description 'Yarn is a new package manager for JavaScript and an alternative to npm.'
   homepage 'https://yarnpkg.com/en/'
-  version '1.16.0'
-  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.16.0/yarn-v1.16.0.tar.gz'
-  source_sha256 'df202627d9a70cf09ef2fb11cb298cb619db1b958590959d6f6e571b50656029'
+  version '1.17.3'
+  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.17.3/yarn-v1.17.3.tar.gz'
+  source_sha256 'e3835194409f1b3afa1c62ca82f561f1c29d26580c9e220c36866317e043c6f3'
 
   binary_url ({
   })
   binary_sha256 ({
   })
 
-  node_version=`node -v 2> /dev/null`
+  node_version=`node -v 2> /dev/null`.chomp
   depends_on 'nodebrew' unless "#{node_version}" != ""
 
   def self.install
