4fef09aa919a45996193d25c1477ed01024d358f
Ed Reel <edreel@gmail.com>
2019-08-19 09:52:40 -0500

    Update composer from 1.8.6 to 1.9.0 (#3429)
    Update SHA384.hexdigest for composer-setup.php

--- packages/composer.rb
+++ packages/composer.rb
@@ -3,14 +3,9 @@ require 'package'
 class Composer < Package
   description 'Dependency Manager for PHP'
   homepage 'https://getcomposer.org/'
-  version '1.8.6'
-  source_url 'https://github.com/composer/composer/archive/1.8.6.tar.gz'
-  source_sha256 'abd42546a0af0c4daf78b49925dc9855b06a4ed726db6c2a43161e3b5dc12436'
-
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
+  version '1.9.0'
+  source_url 'https://github.com/composer/composer/archive/1.9.0.tar.gz'
+  source_sha256 'e7667275b73aa087af9a41e681dfb1c998e3c80bb032574b4ddf4756bb2a0d3b'
 
   depends_on 'php7' unless File.exists? "#{CREW_PREFIX}/bin/php"
   depends_on 'xdg_base'
@@ -27,7 +22,7 @@ class Composer < Package
 
   def self.install
     system "php -r \"copy('https://getcomposer.org/installer', 'composer-setup.php');\""
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA384.hexdigest( File.read('composer-setup.php') ) == '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA384.hexdigest( File.read('composer-setup.php') ) == 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1'
     system "mkdir -p #{CREW_DEST_PREFIX}/bin"
     system "php composer-setup.php --install-dir=#{CREW_DEST_PREFIX}/bin --filename=composer --version=#{version}"
     system "mkdir -p #{CREW_DEST_PREFIX}/.config"
