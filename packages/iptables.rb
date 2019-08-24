c367bc4dcf7f119f8841e80dbc9393615da607c5
Ed Reel <edreel@gmail.com>
2019-08-24 14:22:10 -0500

    Add pre-built binaries for iptables

--- packages/iptables.rb
+++ packages/iptables.rb
@@ -8,8 +8,16 @@ class Iptables < Package
   source_sha256 'a23cac034181206b4545f4e7e730e76e08b5f3dd78771ba9645a6756de9cdd80'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/iptables-1.8.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/iptables-1.8.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/iptables-1.8.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/iptables-1.8.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '18c2cdd028a017bd6e7fa02ebc1f41158fa3793873bcd33a3054d746a7655be1',
+     armv7l: '18c2cdd028a017bd6e7fa02ebc1f41158fa3793873bcd33a3054d746a7655be1',
+       i686: '7d19db312bd7d3ebad6e90049f03b656add0abf7f55f299a2243316136a2e130',
+     x86_64: 'fbef75acb3252c55f30712ac902662a1c2968932e167df82173ba64cd333e45e',
   })
 
   def self.build
