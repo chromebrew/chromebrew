30075206d8b2de356e3bff18ba5d7e43813595ac
Ed Reel <edreel@gmail.com>
2019-08-27 10:07:21 -0500

    Change source_url for emacs and gemacs (#3440)
    Add pre-built binaries for emacs, gemacs and libspng

--- packages/gemacs.rb
+++ packages/gemacs.rb
@@ -4,14 +4,20 @@ class Gemacs < Package
   description 'An extensible, customizable, free/libre text editor - and more.'
   homepage 'http://www.gnu.org/software/emacs/'
   version '26.2'
-  source_url 'https://ftp.gnu.org/gnu/emacs/emacs-26.2.tar.xz'
+  source_url 'https://ftpmirror.gnu.org/emacs/emacs-26.2.tar.xz'
   source_sha256 '151ce69dbe5b809d4492ffae4a4b153b2778459de6deb26f35691e1281a9c58e'
 
   binary_url ({
-
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-
+    aarch64: 'cf400dc1a499f64c25c5fd08cb954305f62bba1f65bfaddeea799205d59fe506',
+     armv7l: 'cf400dc1a499f64c25c5fd08cb954305f62bba1f65bfaddeea799205d59fe506',
+       i686: 'd0b38c6c09d92c73dd0c02aff134b364ef1a05ff2e530a04b83e53aa62fed95c',
+     x86_64: '120a8438b8ab8fda338f38151a2846528ba41a06923e294ed43b2c090c86a113',
   })
 
   depends_on 'emacs'
@@ -24,6 +30,7 @@ class Gemacs < Package
     system "./configure \
             --prefix=#{CREW_PREFIX} \
             --localstatedir=#{CREW_PREFIX}/share \
+            --without-selinux \
             --with-x=yes \
             --with-x-toolkit=gtk3 \
             --with-gif=yes \
--- packages/libspng.rb
+++ packages/libspng.rb
@@ -8,8 +8,16 @@ class Libspng < Package
   source_sha256 '220a653802559943ae43fd48f03ba6ff3935a5243766d9ee5ff905240d4399a7'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libspng-0.5.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libspng-0.5.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libspng-0.5.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libspng-0.5.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '269f399358a4f7f3d9f8756f3e437b1c9c055d3b3632e802a28a3b0673553e39',
+     armv7l: '269f399358a4f7f3d9f8756f3e437b1c9c055d3b3632e802a28a3b0673553e39',
+       i686: '0abff0a70f0ae1e4b66185248857aa2485b6d29ec2265d53a2df5cfbacab2b22',
+     x86_64: '4ffaad2bcca5bfca6034670ddbbc6bf6cea078bb587488e511225e75d9901067',
   })
 
   def self.build
@@ -24,6 +32,7 @@ class Libspng < Package
   def self.install
     Dir.chdir('build') do
       system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+      FileUtils.mv "#{CREW_DEST_PREFIX}/lib/libspng.so", "#{CREW_DEST_LIB_PREFIX}/libspng.so" if ARCH == 'x86_64'
     end
   end
 end
