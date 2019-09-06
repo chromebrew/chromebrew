50c344f374192c0d42b855c914397dc8c48001bd
Ed Reel <edreel@gmail.com>
2019-09-06 10:44:12 -0500

    Update strace to use libdw instead of libunwind (#3449)
    Add patch for strace-graph
    
    Add pre-built binaries

--- packages/wl_clipboard.rb
+++ packages/wl_clipboard.rb
@@ -9,8 +9,16 @@ class Wl_clipboard < Package
   source_sha256 '3c7815986bb43c49912e1b64fb447bc31a80aa1326471dd575123ed6cd86e6be'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wl_clipboard-c0109-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wl_clipboard-c0109-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/wl_clipboard-c0109-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wl_clipboard-c0109-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'd47f4caf0c77409dc90c3783b24b2033dd6b69388a492096b1048468f388e289',
+     armv7l: 'd47f4caf0c77409dc90c3783b24b2033dd6b69388a492096b1048468f388e289',
+       i686: '7f4ae7de3f2124c5439118e8fe463009eec9f096abd6e40ed3a364af30c1134e',
+     x86_64: '410bb77a3471d12c9babc4466486b7e2785f954fb2399bc5fbf51a555ce361db',
   })
 
   depends_on 'wayland_protocols' # xdg-shell support, depends on wayland
