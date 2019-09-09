6d9ca3f04cd12f1b35a9771a524909d92309bb9f
Ed Reel <edreel@gmail.com>
2019-09-09 12:05:35 -0500

    Update libunwind from 1.2.1-0 to 1.3.1 (#3452)
    Add pre-built binaries

--- packages/libunwind.rb
+++ packages/libunwind.rb
@@ -2,34 +2,35 @@ require 'package'
 
 class Libunwind < Package
   description 'libunwind is a portable and efficient C programming interface (API) to determine the call-chain of a program.'
-  homepage 'http://www.nongnu.org/libunwind/'
-  version '1.2.1-0'
-  source_url 'http://download.savannah.gnu.org/releases/libunwind/libunwind-1.2.1.tar.gz'
-  source_sha256 '3f3ecb90e28cbe53fba7a4a27ccce7aad188d3210bb1964a923a731a27a75acb'
+  homepage 'https://www.nongnu.org/libunwind/'
+  version '1.3.1'
+  source_url 'https://download.savannah.gnu.org/releases/libunwind/libunwind-1.3.1.tar.gz'
+  source_sha256 '43997a3939b6ccdf2f669b50fdb8a4d3205374728c2923ddc2354c65260214f8'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.2.1-0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.3.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.3.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.3.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libunwind-1.3.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'ad248bc364a305c449d684ba65649f0c952a0e9dcf7742411f7918346ba6ed20',
-     armv7l: 'ad248bc364a305c449d684ba65649f0c952a0e9dcf7742411f7918346ba6ed20',
-       i686: '5a5730f583590483fbac1c4a64970db358449767d2858ad01d13f1f33f3f4d86',
-     x86_64: 'd439b332a4a18ee2a49d0877409c71f28d49f539e31aec87feb6247ad4c22e41',
+    aarch64: 'f156d2f13cd1cb5f62ab67e5f97f3866bfbb1ee5dd36830635ac89d1fea8b8d1',
+     armv7l: 'f156d2f13cd1cb5f62ab67e5f97f3866bfbb1ee5dd36830635ac89d1fea8b8d1',
+       i686: '0eeee073903fbe10cdc818e9da21caad07daff07b10ae9a1f7513277a42e5bc2',
+     x86_64: '5b7de2da3534ee138cd258ed3202eca570cc77ef584353c011c6111fd6b0b024',
   })
 
-  depends_on 'buildessential' => :build
-  depends_on 'openssl' => :build
   depends_on 'xzutils' => :build
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           '--enable-ptrace'
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
