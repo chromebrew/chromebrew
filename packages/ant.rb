4aa20934fc06d40bbd8d9c677b8b0d6e2941785f
Casey Strouse <casey.strouse@gmail.com>
2019-09-20 10:54:38 -0700

    Update ant from 1.10.5 to 1.10.7

--- packages/ant.rb
+++ packages/ant.rb
@@ -3,21 +3,13 @@ require 'package'
 class Ant < Package
   description 'Apache Ant is a Java library and command-line tool whose mission is to drive processes described in build files as targets and extension points dependent upon each other.'
   homepage 'http://ant.apache.org/'
-  version '1.10.5'
-  source_url 'https://www.apache.org/dist/ant/source/apache-ant-1.10.5-src.tar.xz'
-  source_sha256 'ec7c704e22a2b0167e1ff35cc3d4198267e6489f142aa3e9da1b7fe6e39ff53d'
+  version '1.10.7'
+  source_url 'https://www.apache.org/dist/ant/source/apache-ant-1.10.7-src.tar.xz'
+  source_sha256 'c8d68b396d9e44b49668bafe0c82f8c89497915254b5395d73d6f6e41d7a0e25'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ant-1.10.5-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ant-1.10.5-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ant-1.10.5-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ant-1.10.5-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '80c3117cc17432043a67a73fcc9119a08c5ca4c26c530f6f181a10da03659a70',
-     armv7l: '80c3117cc17432043a67a73fcc9119a08c5ca4c26c530f6f181a10da03659a70',
-       i686: 'a6fb4227c591d591d61d2f61df39bcf989bcfdb6281554da7a051d392c7594f0',
-     x86_64: 'df3d7a6d91af51068b9fbcbec1851f3a22a10edca341a6791262fced0a7deb36',
   })
 
   depends_on 'jdk8'
