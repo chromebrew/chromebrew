1338101b3c6a1d0b1fe00b1680812c9e6c35bafc
Ed Reel <edreel@gmail.com>
2019-09-20 17:55:56 -0500

    Add pre-built binaries for ansible, ant, aria2, aws and libcyrussasl

--- packages/libcyrussasl.rb
+++ packages/libcyrussasl.rb
@@ -8,8 +8,16 @@ class Libcyrussasl < Package
   source_sha256 '26866b1549b00ffd020f188a43c258017fa1c382b3ddadd8201536f72efb05d5'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libcyrussasl-2.1.27-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libcyrussasl-2.1.27-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libcyrussasl-2.1.27-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libcyrussasl-2.1.27-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '281ac03fa4841ff2bd2395e960a9803791fac3ccfa0bf70ec7b8f79088c25817',
+     armv7l: '281ac03fa4841ff2bd2395e960a9803791fac3ccfa0bf70ec7b8f79088c25817',
+       i686: 'dbce3fbbf6efedd4c9266c14163022ca9a1da22386f55eeffe6074c341a5bac0',
+     x86_64: '46241ea5cf5e0181649e91edb183128669d5ab5bc13a938493ba1158a5189743',
   })
 
   depends_on 'diffutils' => :build
