204dabafc5c8a5a168db68af40847880dc5951ee
Ed Reel <edreel@gmail.com>
2019-09-23 23:31:13 -0500

    Add pre-built binaries for packages c-u

--- packages/tig.rb
+++ packages/tig.rb
@@ -8,8 +8,16 @@ class Tig < Package
   source_sha256 'b6b6aa183e571224d0e1fab3ec482542c1a97fa7a85b26352dc31dbafe8558b8'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.4.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.4.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.4.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.4.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '712274329587938cdb648fba374338f12f78145a0a69e2285f30a3ac0f2b0eb2',
+     armv7l: '712274329587938cdb648fba374338f12f78145a0a69e2285f30a3ac0f2b0eb2',
+       i686: '70ed711fb304a329f256e5802983110587e9f3e7c6378a12b73d5a0a6d5f5d36',
+     x86_64: '16aa53a2087362bfacb86aada31eccd4614b347b7b3202849c0ce790efc0cfcb',
   })
 
   depends_on 'readline'
--- packages/util_linux.rb
+++ packages/util_linux.rb
@@ -8,8 +8,16 @@ class Util_linux < Package
   source_sha256 '743f9d0c7252b6db246b659c1e1ce0bd45d8d4508b4dfa427bbb4a3e9b9f62b5'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/util_linux-2.34-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/util_linux-2.34-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/util_linux-2.34-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/util_linux-2.34-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '806848160db052933921a25eebb0772ff258f5e87ac58b3d932fce0a5acada82',
+     armv7l: '806848160db052933921a25eebb0772ff258f5e87ac58b3d932fce0a5acada82',
+       i686: 'f900818913f0a3f0396db7713ad99093af6fea2fd5a4d3e3131ac2ed60e845cd',
+     x86_64: 'f3bbcda8e4c87a20fc3fb6f12766af439bd30f931bd6e2a3f450cbdbed3bc084',
   })
 
   depends_on 'python27'
