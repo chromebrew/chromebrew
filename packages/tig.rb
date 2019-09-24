b9147713a5ef047a54d3e2a88e5b9be2ad46c8c1
Casey Strouse <casey.strouse@gmail.com>
2019-09-23 18:38:21 -0700

    Update tig from 2.3.0 to 2.4.1 (#3496)

--- packages/tig.rb
+++ packages/tig.rb
@@ -2,22 +2,14 @@ require 'package'
 
 class Tig < Package
   description 'Tig is an ncurses-based text-mode interface for git.'
-  homepage 'http://jonas.nitro.dk/tig/'
-  version '2.3.0-1'
-  source_url 'https://github.com/jonas/tig/releases/download/tig-2.3.0/tig-2.3.0.tar.gz'
-  source_sha256 '686f0386927904dc6410f0b1a712cb8bd7fff3303f688d7dc43162f4ad16c0ed'
+  homepage 'https://jonas.github.io/tig/'
+  version '2.4.1'
+  source_url 'https://github.com/jonas/tig/releases/download/tig-2.4.1/tig-2.4.1.tar.gz'
+  source_sha256 'b6b6aa183e571224d0e1fab3ec482542c1a97fa7a85b26352dc31dbafe8558b8'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.3.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.3.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.3.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/tig-2.3.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '73b43ae17f23d942b30a90e4092b315dc5d0b44679d1242f2423c4c7d2cd3561',
-     armv7l: '73b43ae17f23d942b30a90e4092b315dc5d0b44679d1242f2423c4c7d2cd3561',
-       i686: '3476982a7d2055274c9fd99b0cf87815e0623329b3bfa45d78b2ba44329d61b9',
-     x86_64: '59a21ceda7f12ef29719ba7387eeb883220c39ad5b2db660a5b9d4d290554214',
   })
 
   depends_on 'readline'
