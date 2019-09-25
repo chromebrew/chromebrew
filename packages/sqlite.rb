5590c9e3e2c5c6c240f7c5e6a2a68ab36c0c70c0
Casey Strouse <casey.strouse@gmail.com>
2019-09-25 12:07:51 -0700

    Update sqlite from 3.28.0 to 3.29.0

--- packages/sqlite.rb
+++ packages/sqlite.rb
@@ -3,21 +3,13 @@ require 'package'
 class Sqlite < Package
   description 'SQLite is a self-contained, high-reliability, embedded, full-featured, public-domain, SQL database engine.'
   homepage 'http://www.sqlite.org/'
-  version '3.28.0'
-  source_url 'https://sqlite.org/2019/sqlite-autoconf-3280000.tar.gz'
-  source_sha256 'd61b5286f062adfce5125eaf544d495300656908e61fca143517afcc0a89b7c3'
+  version '3.29.0'
+  source_url 'https://sqlite.org/2019/sqlite-autoconf-3290000.tar.gz'
+  source_sha256 '8e7c1e2950b5b04c5944a981cb31fffbf9d2ddda939d536838ebc854481afd5b'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.28.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.28.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.28.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.28.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '81c476300ae1c045be072eb305163305faeb48c932d4c411d8f28373d1aad70a',
-     armv7l: '81c476300ae1c045be072eb305163305faeb48c932d4c411d8f28373d1aad70a',
-       i686: '41ff3ba7559641c2fbce5b387bc9ab85b5bb2095401813244c7bcaef0dc536be',
-     x86_64: '13bc85cf0cfa0d52de43ff3381a14ffafe70e27447abbfe68a1324fcb0dc4611',
   })
 
   depends_on 'libedit'
