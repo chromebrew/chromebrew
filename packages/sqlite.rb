51bca323b9f75abae6e50f1caf909a37041c0df7
Ed Reel <edreel@gmail.com>
2019-09-26 00:15:45 -0500

    Add pre-built binaries for bacon, musl, nmap and sqlite

--- packages/sqlite.rb
+++ packages/sqlite.rb
@@ -8,8 +8,16 @@ class Sqlite < Package
   source_sha256 '8e7c1e2950b5b04c5944a981cb31fffbf9d2ddda939d536838ebc854481afd5b'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.29.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.29.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.29.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/sqlite-3.29.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '9d9adb320dfdaaed8cf896f4338b0a78e75f0aa0d01d2c0480ff2c18dc4ed66f',
+     armv7l: '9d9adb320dfdaaed8cf896f4338b0a78e75f0aa0d01d2c0480ff2c18dc4ed66f',
+       i686: '18e1aaaa527230edc785c7637e8088fb8f10bf6aee8911ace8bd1d350e727ed7',
+     x86_64: '71b432b2d4c2a030f2739754998ebeca05942625b93ef8ba96c4d00957feaf58',
   })
 
   depends_on 'libedit'
