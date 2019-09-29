9b70d1364d4604c45c02935c43d349fe453b47b1
Ed Reel <edreel@gmail.com>
2019-09-29 18:49:52 -0500

    Add CREW_BUILD constant

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.2.4'
+CREW_VERSION = '1.2.5'
 
 ARCH = `uname -m`.strip
 ARCH_LIB = if ARCH == 'x86_64' then 'lib64' else 'lib' end
@@ -45,3 +45,5 @@ CREW_NOT_STRIP = ENV["CREW_NOT_STRIP"]
 USER = `whoami`.chomp
 
 CHROMEOS_RELEASE = `grep CHROMEOS_RELEASE_CHROME_MILESTONE= /etc/lsb-release | cut -d'=' -f2`.chomp
+
+CREW_BUILD = `gcc -dumpmachine`
