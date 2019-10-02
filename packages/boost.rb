513c3321d4cea28b604dfb8a0575fa56dad59a01
Ed Reel <edreel@gmail.com>
2019-10-01 23:12:12 -0500

    Add pre-built binaries for boost (#3515)

--- packages/boost.rb
+++ packages/boost.rb
@@ -8,8 +8,16 @@ class Boost < Package
   source_sha256 'd73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'e0d2c1c299cff64f8d7f8de806cc8bfb2dce0be992d037cf17967bf4e9795e46',
+     armv7l: 'e0d2c1c299cff64f8d7f8de806cc8bfb2dce0be992d037cf17967bf4e9795e46',
+       i686: 'f000df2868e43bcab73d09d6fc7b161ef8b4ae433809dec3d8943b375aa36982',
+     x86_64: '7d028e7fab5e95a2cbc74eb8b86de0d77619f652c83e687b4ae0e491baed3e5d',
   })
 
   def self.build
