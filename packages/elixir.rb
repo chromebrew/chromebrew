b5bb5e6f9463828f65ace495f446cdcb32cbc514
Ed Reel <edreel@gmail.com>
2019-10-04 23:56:59 -0500

    Update elixir from 1.9.1 to 1.9.1-1

--- packages/elixir.rb
+++ packages/elixir.rb
@@ -3,7 +3,7 @@ require 'package'
 class Elixir < Package
   description 'Elixir is a dynamic, functional language designed for building scalable and maintainable applications.'
   homepage 'https://elixir-lang.org/'
-  version '1.9.1'
+  version '1.9.1-1'
   source_url 'https://github.com/elixir-lang/elixir/releases/download/v1.9.1/Precompiled.zip'
   source_sha256 '17e43cb1627a0fa5625bc370cec0964ad6471ef242f1549e316db73c0d94fcc6'
 
@@ -16,10 +16,18 @@ class Elixir < Package
   depends_on 'unzip' => :build
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
+    # Remove unused files
+    FileUtils.rm 'man/common'
+    FileUtils.rm 'bin/mix.ps1'
+    FileUtils.rm 'bin/.DS_Store'
+    FileUtils.rm Dir.glob('bin/*.bat')
+    FileUtils.rm Dir.glob('man/*.1.in')
+    # Prepare destination directories
     FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}"
-    system "cp -r bin/ #{CREW_DEST_PREFIX}"
-    system "cp -r lib/* #{CREW_DEST_LIB_PREFIX}"
-    system "cp -r man/ #{CREW_DEST_PREFIX}/share"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/man/man1"
+    # Add relevant files
+    FileUtils.cp_r 'bin/', "#{CREW_DEST_PREFIX}"
+    FileUtils.cp_r Dir.glob('lib/mix/lib/*'), "#{CREW_DEST_LIB_PREFIX}"
+    FileUtils.cp_r Dir.glob('man/*'), "#{CREW_DEST_PREFIX}/share/man/man1"
   end
 end
