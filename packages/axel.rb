96886926c404a2b8974e0f6fe8480b347387f48d
Casey Strouse <casey.strouse@gmail.com>
2019-10-17 14:42:52 -0700

    Update axel from 2.16.1 to 2.17.6
    Tested on ARM.

--- packages/axel.rb
+++ packages/axel.rb
@@ -3,21 +3,13 @@ require 'package'
 class Axel < Package
   description 'Light command line download accelerator for Linux and Unix'
   homepage 'https://github.com/axel-download-accelerator/axel'
-  version '2.16.1'
-  source_url 'https://github.com/axel-download-accelerator/axel/releases/download/v2.16.1/axel-2.16.1.tar.xz'
-  source_sha256 'd34094eb63eaebc989f96eabdda091dcfb28e840bb8d274a5f216775941f59e4'
+  version '2.17.6'
+  source_url 'https://github.com/axel-download-accelerator/axel/releases/download/v2.17.6/axel-2.17.6.tar.xz'
+  source_sha256 '24ab549021bdfca01ad5e8e95b706869dd30fe9ab1043da4cbb9dff89edc267d'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/axel-2.16.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/axel-2.16.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/axel-2.16.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/axel-2.16.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '823600341cf335d42a3e23465d693925e1b03af01aa974f52e3ca66659c0e91b',
-     armv7l: '823600341cf335d42a3e23465d693925e1b03af01aa974f52e3ca66659c0e91b',
-       i686: '2f3807501995381c3c60beff7eb30bc007337a7f28fc0fd244aea9a3a046fc72',
-     x86_64: '842d5fee9613e55a6efc696739814a623d77b16494d2bf7c6d473aa5b686530c',
   })
 
   def self.build
@@ -26,6 +18,6 @@ class Axel < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
