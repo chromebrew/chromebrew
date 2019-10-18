45dc5e8fce66ec7b8970c9fe3f162a663dc65c83
Casey Strouse <casey.strouse@gmail.com>
2019-10-17 22:46:20 -0700

    Update argon2 from 20171227 to 20190702 (#3544)
    Tested on ARM.

--- packages/argon2.rb
+++ packages/argon2.rb
@@ -3,21 +3,13 @@ require 'package'
 class Argon2 < Package
   description 'The password hash Argon2, winner of PHC'
   homepage 'https://github.com/P-H-C/phc-winner-argon2'
-  version '20171227'
-  source_url 'https://github.com/P-H-C/phc-winner-argon2/archive/20171227.tar.gz'
-  source_sha256 'eaea0172c1f4ee4550d1b6c9ce01aab8d1ab66b4207776aa67991eb5872fdcd8'
+  version '20190702'
+  source_url 'https://github.com/P-H-C/phc-winner-argon2/archive/20190702.tar.gz'
+  source_sha256 'daf972a89577f8772602bf2eb38b6a3dd3d922bf5724d45e7f9589b5e830442c'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/argon2-20171227-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/argon2-20171227-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/argon2-20171227-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/argon2-20171227-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '6688f94b68d0541be8c03cbae3a5cd904d0a15c2fb84f9d2d8b0ffd1d7bfb643',
-     armv7l: '6688f94b68d0541be8c03cbae3a5cd904d0a15c2fb84f9d2d8b0ffd1d7bfb643',
-       i686: '545249fd01da7b62a00a4c2ecfb09b99423d98030f68111ae9f0e2b30cbad8ce',
-     x86_64: '700fbeddc09b599c7525bb16487f3e73ece490e6e3ad92919baf3888af65464d',
   })
 
   def self.build
