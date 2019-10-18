89c93886c54443231016f5a15edaf5276558f1bc
Casey Strouse <casey.strouse@gmail.com>
2019-10-17 22:45:24 -0700

    Update bash_completion from 2.8 to 2.9 (#3546)
    Tested on ARM.

--- packages/bash_completion.rb
+++ packages/bash_completion.rb
@@ -3,21 +3,13 @@ require 'package'
 class Bash_completion < Package
   description 'Programmable completion functions for bash'
   homepage 'https://github.com/scop/bash-completion'
-  version '2.8'
-  source_url 'https://github.com/scop/bash-completion/archive/2.8.tar.gz'
-  source_sha256 'c3f1bacac64bc69e3dfb9b1ef79ddbf755aac441b452845531950f1643bed25f'
+  version '2.9'
+  source_url 'https://github.com/scop/bash-completion/archive/2.9.tar.gz'
+  source_sha256 'fddd9e6739c972eea95d1f0e0e84fa9a17a5935ba21eab37308b27580cc4d790'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bash_completion-2.8-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bash_completion-2.8-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bash_completion-2.8-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bash_completion-2.8-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'de8e10c9a5d3bf2be05d6fac39e10db2ab1d39c6c9d6b3f3acdec71b6d9f9f04',
-     armv7l: 'de8e10c9a5d3bf2be05d6fac39e10db2ab1d39c6c9d6b3f3acdec71b6d9f9f04',
-       i686: 'ee87f97c9a5ccec83d6f618fdc09c5af0ec93a095d717ff3fbc8b24137bf264a',
-     x86_64: '9efbe3b29ab29977df9c6f1cce34fb06677590bd2a4c44ad6da5b3d49b5f7d9c',
   })
 
   def self.build
