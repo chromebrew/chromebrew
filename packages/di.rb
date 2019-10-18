f8ff630059c76465a7fad543c6f4ffc09c570e96
Casey Strouse <casey.strouse@gmail.com>
2019-10-17 22:39:41 -0700

    Update di from 4.46 to 4.47.1 (#3554)
    Tested on ARM.

--- packages/di.rb
+++ packages/di.rb
@@ -3,21 +3,13 @@ require 'package'
 class Di < Package
   description '\'di\' is a disk information utility, displaying everything (and more) that your \'df\' command does.'
   homepage 'http://gentoo.com/di/'
-  version '4.46'
-  source_url 'https://gentoo.com/di/di-4.46.tar.gz'
-  source_sha256 '3cc77280618d5e7b61eeedd3f25bb8521a6de5420793d73e217ce2c83d8e5333'
+  version '4.47.1'
+  source_url 'https://gentoo.com/di/di-4.47.1.tar.gz'
+  source_sha256 'eea8ad94197d9f11790afea0924d8bf29ec001c32eb6209e81c4e13766a2abad'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/di-4.46-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/di-4.46-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/di-4.46-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/di-4.46-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'd21f892ca57b54da560cdaa3509e25ca658ec4643281cdbde5a25a1375c4ffbb',
-     armv7l: 'd21f892ca57b54da560cdaa3509e25ca658ec4643281cdbde5a25a1375c4ffbb',
-       i686: 'f35449a564d83e81c51823609efb3ba8f9a132f3f300e3581ac0191d1eef2c16',
-     x86_64: '4125910072a4aa818af4016bcf95589e0943d5186093ea87352ffe3db895e483',
   })
 
   def self.build
