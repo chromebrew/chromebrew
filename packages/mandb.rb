f2bbf388e7ff80b5b1487ed2c5bffcf0240f870c
Ed Reel <edreel@gmail.com>
2019-10-26 01:21:02 -0500

    Update mandb from 2.8.6.1 to 2.9.0
    Restore missing dependencies
    
    Update postinstall to execute mandb -c
    
    Add pre-built binaries

--- packages/mandb.rb
+++ packages/mandb.rb
@@ -1,31 +1,35 @@
 require 'package'
 
 class Mandb < Package
-  description 'mandb is used to initialise or manually update index database caches that are usually maintained by man.'
-  homepage 'http://savannah.nongnu.org/projects/man-db'
-  version '2.8.6.1'
-  source_url 'https://download.savannah.gnu.org/releases/man-db/man-db-2.8.6.1.tar.xz'
-  source_sha256 '2c77a2bb9dbf06f72417a6f63063e204888a22a5020ff77334d304d55f813a28'
+  description 'mandb is used to initialize or manually update index database caches that are usually maintained by man.'
+  homepage 'http://man-db.nongnu.org/'
+  version '2.9.0'
+  source_url 'http://download.savannah.gnu.org/releases/man-db/man-db-2.9.0.tar.xz'
+  source_sha256 '5d4aacd9e8876d6a3203a889860c3524c293c38f04111a3350deab8a6cd3e261'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.8.6.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.8.6.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.8.6.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.8.6.1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '4ab54543270e692f94ac50235b1518824aa1bdb09a45745991848e9c30dc6469',
-     armv7l: '4ab54543270e692f94ac50235b1518824aa1bdb09a45745991848e9c30dc6469',
-       i686: 'c8378e0ca6622c336bf3e8cfab8a85aff1f49a498008221a06be74dc5509311d',
-     x86_64: '5d830ca395acb5b7ab78fef4e44669bc1c5066196d139bcfcabd91c5b51055a8',
+    aarch64: '7545fe308e8b643c6c289650770392ff7499f08cf2be63f98b753c990c1d0b9b',
+     armv7l: '7545fe308e8b643c6c289650770392ff7499f08cf2be63f98b753c990c1d0b9b',
+       i686: 'e400afbddd2f8564e4cdee38ae17447d632c7fb9bdbd35c53b0ed653830c373d',
+     x86_64: '3e898c4771564163d80eb207acbeec8ec04c8c7de25ae572c9a4de163cfa28af',
   })
+
+  depends_on 'gdbm'
+  depends_on 'groff'
+  depends_on 'libpipeline'
 
   def self.build
     system './configure',
       "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}",
       "--with-systemdtmpfilesdir=#{CREW_PREFIX}/etc/tmpfiles.d", # we can't write to /usr/lib/tmpfiles.d
       '--disable-cache-owner',                                   # we can't create the user 'man'
-      "--with-pager=#{CREW_PREFIX}/bin/less"                     # the pager is not at the default location
+      "--with-pager=#{CREW_PREFIX}/bin/most"                     # the pager is not at the default location
     system 'make'
   end
 
@@ -34,7 +38,6 @@ class Mandb < Package
     [
       'include/manconfig.h.in',
       'src/manp.c',
-      'src/tests/mandb-7',
       'src/man_db.conf.in',
       'manual/db.me',
       'manual/files.me',
@@ -52,8 +55,6 @@ class Mandb < Package
   end
 
   def self.postinstall
-    puts
-    puts "To create the man databases and get apropos working, type 'mandb -c'.".lightblue
-    puts
+    system 'mandb -c'
   end
 end
