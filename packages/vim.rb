0902702ec0acdc91e39862ef3ab49b076335c3bf
Vincent GIRARD <vincent@heliosphere.fr>
2019-10-27 13:15:06 +0100

    Remove python27 dependency since python27 ends its life in 01/2020

--- packages/vim.rb
+++ packages/vim.rb
@@ -17,7 +17,6 @@ class Vim < Package
   binary_sha256 ({
   })
 
-  depends_on 'python27' => :build
   depends_on 'python3' => :build
   depends_on 'vim_runtime'
 
@@ -42,7 +41,6 @@ class Vim < Package
               "--enable-cscope",
               "--enable-fontset",
               "--enable-perlinterp=dynamic",
-              "--enable-pythoninterp=dynamic",
               "--enable-python3interp=dynamic",
               "--enable-rubyinterp=dynamic",
               "--disable-selinux"
