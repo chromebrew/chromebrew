60ba049e4bda1c7236d1772021448f4a6b32e412
Vincent GIRARD <vincent@heliosphere.fr>
2019-10-27 20:17:38 +0100

    Upgrade gvim and vim_runtime packages to 8.1.2225

--- packages/gvim.rb
+++ packages/gvim.rb
@@ -3,24 +3,15 @@ require 'package'
 class Gvim < Package
   description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient. (with advanced features, such as a GUI)'
   homepage 'http://www.vim.org/'
-  version '8.1.1915'
-  source_url 'https://github.com/vim/vim/archive/v8.1.1915.tar.gz'
-  source_sha256 '508bcffd340497d6279fb2d5aa98ff43190cf7983a87fe4838fb780446f900a9'
+  version '8.1.2225'
+  source_url 'https://github.com/vim/vim/archive/v8.1.2225.tar.gz'
+  source_sha256 'a88cf5fb07701d92ea79a3cc8f6e020e6b9bf07c0dfa72f0dde8a65f87e30f2d'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.1915-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.1915-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.1915-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gvim-8.1.1915-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'b31573015a9c31dd256555db8d75634e40cdfe79f3141540558887c6fe29b851',
-     armv7l: 'b31573015a9c31dd256555db8d75634e40cdfe79f3141540558887c6fe29b851',
-       i686: '655cf5e0076da4b7d52d7ecfe87f02b5b2ba48d1b5366652ebd08fa0288b546d',
-     x86_64: '07be8e948923b2c2b81acd1ee343da7c4f221825ee3dc14bea488149cc5fef18',
   })
 
-  depends_on 'python27' => :build
   depends_on 'python3' => :build
   depends_on 'vim_runtime'
   depends_on 'gtk3'
@@ -47,7 +38,6 @@ class Gvim < Package
               "--enable-cscope",
               "--enable-fontset",
               "--enable-perlinterp=dynamic",
-              "--enable-pythoninterp=dynamic",
               "--enable-python3interp=dynamic",
               "--enable-rubyinterp=dynamic",
               "--disable-selinux"
