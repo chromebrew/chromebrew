a8ce5a739abc5fdf06b911d4283a70ccd0fd26b5
Ed Reel <edreel@gmail.com>
2019-10-28 09:59:46 -0500

    Update core_packages.txt and needs_binaries.sh (#3579)

--- tools/needs_binaries.sh
+++ tools/needs_binaries.sh
@@ -5,16 +5,21 @@ if [ ! -d ../packages ]; then
 fi
 cd ../packages
 arch=$(uname -m)
-exclusions='antlr4.rb asciinema.rb autosetup.rb broadway.rb cabal.rb checkinstall.rb composer.rb cowsay.rb crew_profile.rb cros_resize.rb'
-exclusions+=' docx2txt.rb dr.rb far.rb fortune.rb fortunes.rb fpc.rb freedos.rb ghc.rb google_cloud_sdk.rb gradle.rb julia.rb komodo.rb'
-exclusions+=' kr.rb ld_default.rb libtinfo.rb lsb_release.rb mysqltuner.rb nodebrew.rb nvm.rb oc.rb pipes_sh.rb pipesx_sh.rb pngcheck.rb'
-exclusions+=' qt.rb sl.rb spark.rb stack.rb sublime_merge.rb sublime_text.rb thefuck.rb tinycore.rb txt2regex.rb uwsgi.rb xdg_base.rb yarn.rb'
-exclusions+=' neofetch.rb perl_gcstring_linebreak.rb perl_io_socket_ssl.rb perl_locale_gettext.rb perl_locale_messages.rb perl_module_build.rb'
-exclusions+=' perl_read_key.rb perl_sgmls.rb perl_term_ansicolor.rb perl_text_charwidth.rb perl_text_unidecode.rb perl_text_wrapi18n.rb'
-exclusions+=' perl_time_hires.rb perl_unicode_eastasianwidth.rb perl_xml_parser.rb perl_xml_sax_parserfactory.rb perl_xml_simple.rb'
-exclusions+=' elixir.rb leiningen.rb tkdiff.rb v2ray.rb'
+exclusions='android_studio.rb antlr4.rb asciinema.rb autosetup.rb broadway.rb cabal.rb checkinstall.rb codium.rb composer.rb cowsay.rb'
+exclusions+=' crew_profile.rb cros_resize.rb docx2txt.rb dr.rb elixir.rb far.rb fortune.rb fortunes.rb fpc.rb freedos.rb gcc_tools.rb'
+exclusions+=' ghc.rb gittools.rb google_cloud_sdk.rb gradle.rb hugo.rb julia.rb komodo.rb kr.rb ld_default.rb leiningen.rb libtinfo.rb'
+exclusions+=' lsb_release.rb mysqltuner.rb nconvert.rb neofetch.rb ngrok.rb nodebrew.rb nvm.rb oc.rb perl_gcstring_linebreak.rb'
+exclusions+=' perl_io_socket_ssl.rb perl_locale_gettext.rb perl_locale_messages.rb perl_module_build.rb perl_read_key.rb perl_sgmls.rb'
+exclusions+=' perl_term_ansicolor.rb perl_text_charwidth.rb perl_text_unidecode.rb perl_text_wrapi18n.rb perl_time_hires.rb'
+exclusions+=' perl_unicode_eastasianwidth.rb perl_xml_parser.rb perl_xml_sax_parserfactory.rb perl_xml_simple.rb pipes_sh.rb'
+exclusions+=' pipesx_sh.rb pngcheck.rb qtcreator.rb sl.rb spark.rb stack.rb sublime_merge.rb sublime_text.rb thefuck.rb tinycore.rb'
+exclusions+=' tkdiff.rb txt2regex.rb uwsgi.rb v2ray.rb xdg_base.rb yarn.rb'
 if [[ "${arch}" == 'aarch64' || "${arch}" == 'armv7l' ]]; then
-  exclusions+=' az.rb cf.rb clisp.rb dropbox.rb freebasic.rb miniconda3.rb misctools.rb oci.rb wkhtmltox.rb xorg_intel_driver.rb xorg_vmmouse_driver.rb'
+  exclusions+=' az.rb cf.rb clisp.rb dropbox.rb fakeroot_ng.rb freebasic.rb miniconda3.rb misctools.rb oci.rb weston.rb wkhtmltox.rb'
+  exclusions+=' xorg_intel_driver.rb xorg_vmmouse_driver.rb'
+fi
+if [[ "${arch}" == 'i686' ]]; then
+  exclusions+=' fakeroot_ng.rb'
 fi
 packages=$(grep -L "${arch}:" *.rb)
 for p in ${packages}; do
