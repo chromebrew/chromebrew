fbc9b4ae151ff5fce724b22e244c93c4470decde
vincowl <vince512@free.fr>
2019-10-29 22:35:00 +0100

    Upgrade Hunspell to 1.7.0 and add french dictionary (#3595)
    * Upgrade Hunspell to 1.7.0 and add french dictionary
    
    * Simplify this package after review's first steps
    
    * Split dictionaries installation in new packages
    
    * Correct the Hunspell_fr package name
    
    * Correction of dictionaries folder
    
    * Extract the english module and solve bugs
    
    * Simplify postinstall step
    
    * Remove unnecessary files
    
    * Clean packages and add spanish
    
    * Clean packages and add spanish
    
    * Add a menu to define the language to install by default with hunspell
    
    * Remove unnessary lines and correct bugs
    
    * Add a hunspell_base package to ease maintenance and default language choice
    
    * remove unused dependency

---
+++ packages/hunspell_base.rb
@@ -0,0 +1,34 @@
+require 'package'
+
+class Hunspell_base < Package
+  description 'Hunspell is a spell checker and morphological analyzer library'
+  homepage 'http://hunspell.github.io/'
+  version '1.7.0'
+  source_url 'https://github.com/hunspell/hunspell/archive/v1.7.0.tar.gz'
+  source_sha256 'bb27b86eb910a8285407cf3ca33b62643a02798cf2eef468c0a74f6c3ee6bc8a'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'readline'
+  
+  def self.build
+    system 'autoreconf -vfi'
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--include=#{CREW_PREFIX}/include",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           '--with-ui',
+           '--with-readline'
+    system "sed -i 's,/usr/share,#{CREW_PREFIX}/share,g' man/hunspell.1"
+    system "sed -i 's,/usr/share,#{CREW_PREFIX}/share,g' src/tools/hunspell.cxx"
+    system "sed -i 's,ncurses.h,#{CREW_PREFIX}/include/ncursesw/ncurses.h,' src/tools/hunspell.cxx"
+    system 'make'
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+end
---
+++ packages/hunspell_es_any.rb
@@ -0,0 +1,35 @@
+require 'package'
+
+class Hunspell_es_any < Package
+  description 'Hunspell Spanish dictionary'
+  homepage 'http://hunspell.github.io/'
+  version '1.7.0'
+  source_url 'https://github.com/hunspell/hunspell/archive/v1.7.0.tar.gz'
+  source_sha256 'bb27b86eb910a8285407cf3ca33b62643a02798cf2eef468c0a74f6c3ee6bc8a'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'hunspell_base'
+
+  def self.install
+    system 'git clone -b libreoffice-6-1 --depth 1 git://anongit.freedesktop.org/libreoffice/dictionaries.git'
+    system "install -Dm644 dictionaries/es/es_ANY.aff #{CREW_DEST_HOME}/Library/Spelling/es_ANY.aff"
+    system "install -Dm644 dictionaries/es/es_ANY.dic #{CREW_DEST_HOME}/Library/Spelling/es_ANY.dic"
+  end
+
+  def self.postinstall
+    system 'git clone -b libreoffice-6-1 --depth 1 git://anongit.freedesktop.org/libreoffice/dictionaries.git'
+    FileUtils.cp 'dictionaries/es/es_ANY.aff', "#{HOME}/Library/Spelling/es_ANY.aff"
+    FileUtils.cp 'dictionaries/es/es_ANY.dic', "#{HOME}/Library/Spelling/es_ANY.dic"
+    FileUtils.rm_rf 'dictionaries/'
+    puts
+    puts "To update the dictionaries periodically, execute 'crew postinstall hunspell_es_any'".lightblue
+    puts
+    puts "To complete the installation, execute the following:".lightblue
+    puts "echo 'export DICTIONARY=es_ANY' >> ~/.bashrc && source ~/.bashrc".lightblue
+    puts
+  end
+end
