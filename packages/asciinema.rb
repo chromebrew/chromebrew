04d62ce9f7a2213b582fe89145f23e6f050b2573
vincowl <vince512@free.fr>
2019-10-29 22:35:42 +0100

    Upgrade asciinema to 2.0.2 (#3615)

--- packages/asciinema.rb
+++ packages/asciinema.rb
@@ -3,21 +3,13 @@ require 'package'
 class Asciinema < Package
   description 'Terminal session recorder'
   homepage 'https://asciinema.org/'
-  version '2.0.1-2'
-  source_url 'https://github.com/asciinema/asciinema/archive/v2.0.1.tar.gz'
-  source_sha256 '7087b247dae36d04821197bc14ebd4248049592b299c9878d8953c025ac802e4'
+  version '2.0.2'
+  source_url 'https://github.com/asciinema/asciinema/archive/v2.0.2.tar.gz'
+  source_sha256 '2578a1b5611e5375771ef6582a6533ef8d40cdbed1ba1c87786fd23af625ab68'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/asciinema-2.0.1-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/asciinema-2.0.1-2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/asciinema-2.0.1-2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/asciinema-2.0.1-2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '6b5ea0249de2ca8b44e974316799eba90b7190c43720ba2b9625cec134085318',
-     armv7l: '6b5ea0249de2ca8b44e974316799eba90b7190c43720ba2b9625cec134085318',
-       i686: '699f103391c9d5925dcc2b85ffa4d15348249e0ca033234c904bcec2ebd3c16c',
-     x86_64: '550a06b4dbdd3662852bc78036fc632e2407f5a47d582489b3d539f11b7ac259',
   })
 
   depends_on 'xdg_base'
