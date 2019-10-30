67632fa0a1954702e077cdf032c1bbeee3c8e6e7
Vincent GIRARD <vincent@heliosphere.fr>
2019-10-30 01:35:31 +0100

    correction of the do  88 wnloaded package url. Get i686 and x86_64 archs

--- packages/cabal.rb
+++ packages/cabal.rb
@@ -4,8 +4,14 @@ class Cabal < Package
   description 'Common Architecture for Building Applications and Libraries'
   homepage 'https://www.haskell.org/cabal/'
   version '2.4.1.0'
-  source_url 'https://www.haskell.org/cabal/release/cabal-install-2.4.1.0/cabal-install-2.4.1.0.tar.gz'
-  source_sha256 '69bcb2b54a064982412e1587c3c5c1b4fada3344b41b568aab25730034cb21ad'
+  case ARCH
+  when 'i686'
+    source_url 'hhttps://downloads.haskell.org/~cabal/cabal-install-2.4.1.0/cabal-install-2.4.1.0-i386-unknown-linux.tar.xz'
+    source_sha256 'b2da736cc27609442b10f77fc1a687aba603a7a33045b722dbf1a0066fade198'
+  when 'x86_64'
+    source_url 'https://downloads.haskell.org/~cabal/cabal-install-2.4.1.0/cabal-install-2.4.1.0-x86_64-unknown-linux.tar.xz'
+    source_sha256 '6136c189ffccaa39916f9cb5788f757166444a2d0c473b987856a79ecbf0c714'
+  end
   
   depends_on 'ghc'
 
