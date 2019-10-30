38352c19792b1d1524ab2f1eb867b7cd63918d7a
vincowl <vince512@free.fr>
2019-10-30 04:00:29 +0100

    Upgrade cabal to 2.4.1.0 (#3617)
    * Upgrade cabal to 2.4.1.0
    
    * correction of the do
     88 wnloaded package url. Get i686 and x86_64 archs

--- packages/cabal.rb
+++ packages/cabal.rb
@@ -3,16 +3,16 @@ require 'package'
 class Cabal < Package
   description 'Common Architecture for Building Applications and Libraries'
   homepage 'https://www.haskell.org/cabal/'
-  version '2.2.0.0'
+  version '2.4.1.0'
   case ARCH
   when 'i686'
-    source_url 'https://www.haskell.org/cabal/release/cabal-install-2.2.0.0/cabal-install-2.2.0.0-i386-unknown-linux.tar.gz'
-    source_sha256 '20cc07bc7c813fac3e88bb308c972e3a28ade30dd32783d7a74cdadc74c5079e'
+    source_url 'hhttps://downloads.haskell.org/~cabal/cabal-install-2.4.1.0/cabal-install-2.4.1.0-i386-unknown-linux.tar.xz'
+    source_sha256 'b2da736cc27609442b10f77fc1a687aba603a7a33045b722dbf1a0066fade198'
   when 'x86_64'
-    source_url 'https://www.haskell.org/cabal/release/cabal-install-2.2.0.0/cabal-install-2.2.0.0-x86_64-unknown-linux.tar.gz'
-    source_sha256 '35c14663496cd18bf33cdcf4441a77eb169b0a93cfd0b79081c6816ada53f05c'
+    source_url 'https://downloads.haskell.org/~cabal/cabal-install-2.4.1.0/cabal-install-2.4.1.0-x86_64-unknown-linux.tar.xz'
+    source_sha256 '6136c189ffccaa39916f9cb5788f757166444a2d0c473b987856a79ecbf0c714'
   end
-
+  
   depends_on 'ghc'
 
   def self.install
