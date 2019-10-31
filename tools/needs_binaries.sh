3d9d66a1e9f949051308a6c71b0819cf7074f717
Ed Reel <edreel@gmail.com>
2019-10-31 00:57:57 -0500

    Update packages.yaml and needs_binaries.sh in tools

--- tools/needs_binaries.sh
+++ tools/needs_binaries.sh
@@ -7,8 +7,8 @@ cd ../packages
 arch=$(uname -m)
 exclusions='android_studio.rb antlr4.rb asciinema.rb autosetup.rb broadway.rb cabal.rb checkinstall.rb codium.rb composer.rb cowsay.rb'
 exclusions+=' crew_profile.rb cros_resize.rb docx2txt.rb dr.rb elixir.rb far.rb fortune.rb fortunes.rb fpc.rb freedos.rb gcc_tools.rb'
-exclusions+=' ghc.rb gittools.rb google_cloud_sdk.rb gradle.rb hugo.rb julia.rb komodo.rb kr.rb ld_default.rb leiningen.rb libtinfo.rb'
-exclusions+=' lsb_release.rb mysqltuner.rb nconvert.rb neofetch.rb ngrok.rb nodebrew.rb nvm.rb oc.rb perl_gcstring_linebreak.rb'
+exclusions+=' ghc.rb gittools.rb google_cloud_sdk.rb gradle.rb hugo.rb hunspell.rb julia.rb komodo.rb kr.rb ld_default.rb leiningen.rb'
+exclusions+=' libtinfo.rb lsb_release.rb mysqltuner.rb nconvert.rb neofetch.rb ngrok.rb nodebrew.rb nvm.rb oc.rb packer.rb perl_gcstring_linebreak.rb'
 exclusions+=' perl_io_socket_ssl.rb perl_locale_gettext.rb perl_locale_messages.rb perl_module_build.rb perl_read_key.rb perl_sgmls.rb'
 exclusions+=' perl_term_ansicolor.rb perl_text_charwidth.rb perl_text_unidecode.rb perl_text_wrapi18n.rb perl_time_hires.rb'
 exclusions+=' perl_unicode_eastasianwidth.rb perl_xml_parser.rb perl_xml_sax_parserfactory.rb perl_xml_simple.rb pipes_sh.rb'
