918a189f5be3d31dcdc4e09ce6b31d164fdb75dd
Ed Reel <edreel@gmail.com>
2019-10-31 12:07:38 -0500

    Update dbus from 1.13.4 to 1.13.12 (#3584)
    Add pre-built binaries

--- packages/dbus.rb
+++ packages/dbus.rb
@@ -3,36 +3,35 @@ require 'package'
 class Dbus < Package
   description 'D-Bus is a message bus system, a simple way for applications to talk to one another.'
   homepage 'https://www.freedesktop.org/wiki/Software/dbus/'
-  version '1.13.4'
-  source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.4.tar.gz'
-  source_sha256 '8a8f0b986ac6214da9707da521bea9f49f09610083c71fdc8eddf8b4c54f384b'
+  version '1.13.12'
+  source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.12.tar.xz'
+  source_sha256 '7588649b56dd257c6a5f85a8c45aa2dfdf9e99f4de3983710f452081ca43eca6'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.4-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.4-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.4-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.4-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'dee478f7df2d50ddb8eac1637a7d359e5d1ba1e7d81d091b31ac3f7463ebb11b',
-     armv7l: 'dee478f7df2d50ddb8eac1637a7d359e5d1ba1e7d81d091b31ac3f7463ebb11b',
-       i686: '60b9dd5fc3b754dad79f83ffec8bfeb6e7cf0263b477f12ee505b6db0d03e72e',
-     x86_64: '2c4049f13366a42f07a397a62fbbacf19ffb779a853f2367743a65e2f7013b8e',
+    aarch64: 'bc3711b6214181b732f6c98024718845f326258a8c4e681b58b2e8436998909b',
+     armv7l: 'bc3711b6214181b732f6c98024718845f326258a8c4e681b58b2e8436998909b',
+       i686: '4a208ad455645a8ebfbc06b53d7aa3e9a149169afb18f8232645287bb61a2865',
+     x86_64: '9e24a9f1c2a389491c5dd06dc59ca7748479d786a7f0f4ce3247cdd982813756',
   })
 
   depends_on 'expat'
 
   def self.build
-    system "./configure \
-    --prefix=#{CREW_PREFIX} \
-    --libdir=#{CREW_LIB_PREFIX} \
-    --disable-static \
-    --disable-doxygen-docs \
-    --disable-xml-docs"
-    system "make"
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           '--disable-doxygen-docs',
+           '--disable-xml-docs'
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
