e4208b711b7471f5e6d709612c20a92ca3d9299a
Ed Reel <edreel@gmail.com>
2019-11-01 00:31:53 -0500

    Add dependencies to libewf
    Fix library path in libewf
    
    Update newlisp version
    
    Add pre-built binaries for itstool, libewf, lz4 and newlisp

--- packages/itstool.rb
+++ packages/itstool.rb
@@ -8,8 +8,16 @@ class Itstool < Package
   source_sha256 'bda0b08e9a1db885c9d7d1545535e9814dd8931d5b8dd5ab4a47bd769d0130c6'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/itstool-2.0.6-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/itstool-2.0.6-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/itstool-2.0.6-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/itstool-2.0.6-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '35a0d41b76930ad6ac3111cc6f0b3b9a5defc0e0889953ecb0c20279d65322b1',
+     armv7l: '35a0d41b76930ad6ac3111cc6f0b3b9a5defc0e0889953ecb0c20279d65322b1',
+       i686: 'f29590049adb39b313121007c280bfa25a13db9f04a35b0760dcff86bce6a828',
+     x86_64: '14f2395c59768e563a6e13bd5115108391ed498fbe637b7865306c6a59759bc7',
   })
 
   def self.build
--- packages/lz4.rb
+++ packages/lz4.rb
@@ -8,8 +8,16 @@ class Lz4 < Package
   source_sha256 '658ba6191fa44c92280d4aa2c271b0f4fbc0e34d249578dd05e50e76d0e5efcc'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/lz4-1.9.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/lz4-1.9.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/lz4-1.9.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/lz4-1.9.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '103784c0d0f91d022a0d6487550d87b8cefd0452d168cf53e794b44d458a8142',
+     armv7l: '103784c0d0f91d022a0d6487550d87b8cefd0452d168cf53e794b44d458a8142',
+       i686: 'b18dd62ffc90a8c5db2f95c9fc8ca130027077f00ed24ff812eec16f0b09979d',
+     x86_64: '6042be5675f4a8d7deb51f595bb97fcd4932be5ffcd8ffbc3d2e8c9de03d55ba',
   })
 
   def self.build
