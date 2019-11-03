bfc3cbcd56b692fed9749611eaca90fb47e6fd2c
Casey Strouse <casey.strouse@gmail.com>
2019-11-03 14:18:58 -0700

    Change download source back to GitHub release rather than archive

--- packages/libcheck.rb
+++ packages/libcheck.rb
@@ -4,8 +4,8 @@ class Libcheck < Package
   description 'A unit testing framework for C'
   homepage 'https://github.com/libcheck/check'
   version '0.13.0'
-  source_url 'https://github.com/libcheck/check/archive/0.13.0.tar.gz'
-  source_sha256 'c95d5e0419f768c100ec6d91c7b0a05985f07cc05b93c3c68c3efd48d3655fce'
+  source_url 'https://github.com/libcheck/check/releases/download/0.13.0/check-0.13.0.tar.gz'
+  source_sha256 'c4336b31447acc7e3266854f73ec188cdb15554d0edd44739631da174a569909'
 
   binary_url ({
   })
