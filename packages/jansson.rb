f70b8e5fd27905753646c191bc7171ccd2e7bb16
Casey Strouse <casey.strouse@gmail.com>
2019-11-03 14:22:07 -0700

    Update jansson from 2.11 to 2.12 (#3634)
    Tested on ARM.

--- packages/jansson.rb
+++ packages/jansson.rb
@@ -3,21 +3,13 @@ require 'package'
 class Jansson < Package
   description 'Jansson is a C library for encoding, decoding and manipulating JSON data.'
   homepage 'http://www.digip.org/jansson/'
-  version '2.11'
-  source_url 'https://github.com/akheron/jansson/archive/v2.11.tar.gz'
-  source_sha256 '6ff0eab3a8baf64d21cae25f88a0311fb282006eb992080722a9099469c32881'
+  version '2.12'
+  source_url 'https://github.com/akheron/jansson/archive/v2.12.tar.gz'
+  source_sha256 '76260d30e9bbd0ef392798525e8cd7fe59a6450c54ca6135672e3cd6a1642941'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.11-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.11-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.11-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.11-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'c2cdc0bf928cc44d8e8b99dce354f838fef6008fd3a65f0fc1171077c3c4238d',
-     armv7l: 'c2cdc0bf928cc44d8e8b99dce354f838fef6008fd3a65f0fc1171077c3c4238d',
-       i686: '318d31c65a9c07a25a1984959eed6e7d88ea2b9f35b4722209ad4c2363d772cf',
-     x86_64: 'e71aeaec54d731aa0524cc33e88329713a0d072d6e5f4bb8e76bd7bdc634bd6c',
   })
 
   def self.build
@@ -27,6 +19,6 @@ class Jansson < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
