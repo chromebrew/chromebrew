ac464321fd0d547beb0271386a86846f08077d7a
Ed Reel <edreel@gmail.com>
2019-11-05 09:22:25 -0600

    Update packages a-r (#3648)
    - Add self.patch section to chibi_scheme
    
    - Change home from http to https for gemacs
    
    - Update arduino_ide from 1.8.9 to 1.8.10
    
    - Update mcelog from 162 to 165
    
    - Update rsu_client from 4.3.7 to 4.3.8
    
    - Add pre-built binaries

--- packages/mcelog.rb
+++ packages/mcelog.rb
@@ -3,13 +3,21 @@ require 'package'
 class Mcelog < Package
   description 'logs and accounts machine checks (in particular memory, IO, and CPU hardware errors) on modern x86 Linux systems.'
   homepage 'https://www.mcelog.org/'
-  version '162'
-  source_url 'https://git.kernel.org/pub/scm/utils/cpu/mce/mcelog.git/snapshot/mcelog-162.tar.gz'
-  source_sha256 '875e98572e86240ea319ab1f69ee6d744eb8b73ac5d700e474f6410d0f52d3fc'
+  version '165'
+  source_url 'https://git.kernel.org/pub/scm/utils/cpu/mce/mcelog.git/snapshot/mcelog-165.tar.gz'
+  source_sha256 'a18fdef9cfe2dfaefa09087c616c376a301dc87b1fa14a37476d97370962c668'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mcelog-165-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mcelog-165-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mcelog-165-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mcelog-165-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '5d9cf717ea6e8f1008b061468130164e14794588d1bf5610d01563cf48182c9b',
+     armv7l: '5d9cf717ea6e8f1008b061468130164e14794588d1bf5610d01563cf48182c9b',
+       i686: '9d5f57770c2189457f402992ef0fa08ccf26d9db677ec725177589ec92b32118',
+     x86_64: 'cbe973fe9210ca821f3bda8e2f0f98249ab8fa4043fcf30fdb7ef5a1c234362b',
   })
 
   def self.patch
@@ -22,6 +30,6 @@ class Mcelog < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
