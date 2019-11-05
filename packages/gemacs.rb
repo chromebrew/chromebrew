bf1eb6d8a3054dd0f30730f60bbe599e4dbc350e
Ed Reel <edreel@gmail.com>
2019-11-05 09:22:25 -0600

    Update packages a-r (#3648)
    - Add self.patch section to chibi_scheme
    
    - Change home from http to https for gemacs
    
    - Update arduino_ide from 1.8.9 to 1.8.10
    
    - Update mcelog from 162 to 165
    
    - Update rsu_client from 4.3.7 to 4.3.8
    
    - Add pre-built binaries

--- packages/gemacs.rb
+++ packages/gemacs.rb
@@ -2,14 +2,22 @@ require 'package'
 
 class Gemacs < Package
   description 'An extensible, customizable, free/libre text editor - and more.'
-  homepage 'http://www.gnu.org/software/emacs/'
+  homepage 'https://www.gnu.org/software/emacs/'
   version '26.3'
   source_url 'https://ftpmirror.gnu.org/emacs/emacs-26.3.tar.xz'
   source_sha256 '4d90e6751ad8967822c6e092db07466b9d383ef1653feb2f95c93e7de66d3485'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '45bae4cffee76691bf89478032d4ac68faf331b263696b6236b3cdcaa9ae8eee',
+     armv7l: '45bae4cffee76691bf89478032d4ac68faf331b263696b6236b3cdcaa9ae8eee',
+       i686: '2980639d04f3d911773b3a97f0a99e604eb38f5155beabafb3d02cb033ce3a93',
+     x86_64: '3e3becfa06271d8b9a13aacdbc0a9f3aeade8ddfa7991f1a24c4f6b8bdc30a89',
   })
 
   depends_on 'emacs'
