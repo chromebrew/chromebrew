ea1d3ad6be359ae4ccabcb5400ef2497eacc3837
vincowl <vince512@free.fr>
2019-11-09 16:43:38 +0100

    Upgrade to 0.4.3 (#3650)

--- packages/neovim.rb
+++ packages/neovim.rb
@@ -3,19 +3,13 @@ require 'package'
 class Neovim < Package
   description 'Neovim is a refactor, and sometimes redactor, in the tradition of Vim (which itself derives from Stevie).'
   homepage 'https://neovim.io/'
-  version '0.4.2'
-  source_url 'https://github.com/neovim/neovim/archive/v0.4.2.tar.gz'
-  source_sha256 '9f874d3d2a74f33b931db62adebe28f8d2ec116270d1e13998b58a73348b6e56'
+  version '0.4.3'
+  source_url 'https://github.com/neovim/neovim/archive/v0.4.3.tar.gz'
+  source_sha256 '91a0b5d32204a821bf414690e6b48cf69224d1961d37158c2b383f6a6cf854d2'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/neovim-0.4.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/neovim-0.4.2-chromeos-armv7l.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/neovim-0.4.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '4f34e7ec984b6e5e9ea0c71780b475157b7d158b4d4887e92d96f12531a6e4a2',
-     armv7l: '4f34e7ec984b6e5e9ea0c71780b475157b7d158b4d4887e92d96f12531a6e4a2',
-     x86_64: 'e5a5461efeab2b27240dbaa7908ceb874982d1094fafd01d5d75b0af1df7118e',
   })
 
   depends_on 'meson' => :build
