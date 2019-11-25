836a7fcf8817f53431b47848f281145ceb346799
Ed Reel <edreel@gmail.com>
2019-11-25 08:20:00 -0600

    Update php packages (#3663)
    * Update php packages
    
    Remove binaries from php.rb and separate versions into packages.
    
    Remove php7 package
    
    * Update php packages
    
    Remove binaries from php.rb and separate versions into packages
    
    Remove php7 package
    
    Set php package to is_fake
    
    * Add pre-built binaries
    
    * Add check for previous versions to php5

--- packages/php70.rb
+++ packages/php70.rb
@@ -1,11 +1,11 @@
 require 'package'
 
-class Php5 < Package
+class Php70 < Package
   description 'PHP is a popular general-purpose scripting language that is especially suited to web development.'
   homepage 'http://www.php.net/'
-  version '5.6.40-1'
-  source_url 'http://php.net/distributions/php-5.6.40.tar.xz'
-  source_sha256 '1369a51eee3995d7fbd1c5342e5cc917760e276d561595b6052b21ace2656d1c'
+  version '7.0.33'
+  source_url 'https://php.net/distributions/php-7.0.33.tar.xz'
+  source_sha256 'ab8c5be6e32b1f8d032909dedaaaa4bbb1a209e519abb01a52ce3914f9a13d96'
 
   if ARGV[0] == 'install'
     phpver = `php -v 2> /dev/null | head -1 | cut -d' ' -f2`.chomp
@@ -13,38 +13,35 @@ class Php5 < Package
   end
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.40-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.40-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.40-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/php5-5.6.40-1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.0.33-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.0.33-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.0.33-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/php7-7.0.33-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'c70edbf5ed87c65852ebae34aa83273e810b6727e630b7416d2ee68d56722398',
-     armv7l: 'c70edbf5ed87c65852ebae34aa83273e810b6727e630b7416d2ee68d56722398',
-       i686: '58320b4a5e73dbc5d028bc0969f28fd606e7aa6b2367aa2cf8b61fc043ce8740',
-     x86_64: '5e7f5461af3627f086a84bc62a26de7cdc907493244fbecd65be02c6ad9869bb',
+    aarch64: '4b1a64041ce564014c9318ec4697796c783caac0689489c21f90d41d338dc753',
+     armv7l: '4b1a64041ce564014c9318ec4697796c783caac0689489c21f90d41d338dc753',
+       i686: 'adce0459926b85882ee5d2dd5801a4d062eec3f63abf893a12da9a30a8b4d738',
+     x86_64: 'a55e3ee6889347fe5065ccb2d16fd19e783c612661a8985f0040e1a5443b5368',
   })
 
   depends_on 'libgcrypt'
-  depends_on 'libpng'
+  depends_on 'libwebp'
   depends_on 'libxslt'
   depends_on 'libzip'
   depends_on 'curl'
   depends_on 'exif'
   depends_on 'freetype'
   depends_on 'pcre'
+  depends_on 're2c'
   depends_on 'tidy'
   depends_on 'unixodbc'
 
   def self.patch
-    # Fix for tidy
-    system "sed -i 's,buffio.h,tidybuffio.h,' ext/tidy/tidy.c"
     # Configuration
     system "sed -i 's,;pid = run/php-fpm.pid,pid = #{CREW_PREFIX}/tmp/run/php-fpm.pid,' sapi/fpm/php-fpm.conf.in"
     system "sed -i 's,;error_log = log/php-fpm.log,error_log = #{CREW_PREFIX}/log/php-fpm.log,' sapi/fpm/php-fpm.conf.in"
     system "sed -i 's,include=@php_fpm_sysconfdir@/php-fpm.d,include=#{CREW_PREFIX}/etc/php-fpm.d,' sapi/fpm/php-fpm.conf.in"
-    system "sed -i 's,^user,;user,' sapi/fpm/php-fpm.conf.in"
-    system "sed -i 's,^group,;group,' sapi/fpm/php-fpm.conf.in"
     system "sed -i 's,^user,;user,' sapi/fpm/www.conf.in"
     system "sed -i 's,^group,;group,' sapi/fpm/www.conf.in"
     system "sed -i 's,@sbindir@,#{CREW_PREFIX}/bin,' sapi/fpm/init.d.php-fpm.in"
@@ -54,6 +51,11 @@ class Php5 < Package
     system "sed -i 's,post_max_size = 8M,post_max_size = 128M,' php.ini-development"
     system "sed -i 's,upload_max_filesize = 2M,upload_max_filesize = 128M,' php.ini-development"
     system "sed -i 's,;opcache.enable=0,opcache.enable=1,' php.ini-development"
+    # Fix cc: error: ext/standard/.libs/type.o: No such file or directory
+    #system "sed -i '98303d' configure"
+    #system "sed -i '98295,98296d' configure"
+    # Fix /usr/bin/file: No such file or directory
+    system 'filefix'
   end
 
   def self.build
@@ -63,20 +65,26 @@ class Php5 < Package
            "--infodir=#{CREW_PREFIX}/info",
            "--libdir=#{CREW_LIB_PREFIX}",
            "--localstatedir=#{CREW_PREFIX}/tmp",
-           "--mandir=#{CREW_PREFIX}/man",
+           "--mandir=#{CREW_PREFIX}/share/man",
            "--sbindir=#{CREW_PREFIX}/bin",
            "--with-config-file-path=#{CREW_PREFIX}/etc",
            "--with-libdir=#{ARCH_LIB}",
            "--with-freetype-dir=#{CREW_PREFIX}/include/freetype2/freetype",
+           "--with-pcre-regex=#{CREW_LIB_PREFIX}",
+           "--with-jpeg-dir=#{CREW_LIB_PREFIX}",
+           "--with-kerberos=#{CREW_LIB_PREFIX}",
+           "--with-png-dir=#{CREW_LIB_PREFIX}",
+           "--with-webp-dir=#{CREW_LIB_PREFIX}",
+           "--with-xpm-dir=#{CREW_LIB_PREFIX}",
            '--enable-exif',
            '--enable-fpm',
            '--enable-ftp',
            '--enable-mbstring',
            '--enable-opcache',
            '--enable-pcntl',
-           '--enable-sockets',
            '--enable-shared',
            '--enable-shmop',
+           '--enable-sockets',
            '--enable-zip',
            '--with-bz2',
            '--with-curl',
@@ -86,15 +94,19 @@ class Php5 < Package
            '--with-libzip',
            '--with-mysqli',
            '--with-openssl',
+           '--with-pcre-regex',
            '--with-pdo-mysql',
            '--with-pear',
-           '--with-pcre-regex',
            '--with-readline',
            '--with-tidy',
            '--with-unixODBC',
            '--with-xsl',
            '--with-zlib'
     system 'make'
+  end
+
+  def self.check
+    #system 'make', 'test'
   end
 
   def self.install
@@ -105,7 +117,7 @@ class Php5 < Package
     system "install -Dm755 sapi/fpm/init.d.php-fpm.in #{CREW_DEST_PREFIX}/etc/init.d/php-fpm"
     system "install -Dm644 sapi/fpm/php-fpm.conf.in #{CREW_DEST_PREFIX}/etc/php-fpm.conf"
     system "install -Dm644 sapi/fpm/www.conf.in #{CREW_DEST_PREFIX}/etc/php-fpm.d/www.conf"
-    system "ln -s #{CREW_PREFIX}/etc/init.d/php-fpm #{CREW_DEST_PREFIX}/bin/php5-fpm"
+    system "ln -s #{CREW_PREFIX}/etc/init.d/php-fpm #{CREW_DEST_PREFIX}/bin/php7-fpm"
 
     # clean up some files created under #{CREW_DEST_DIR}. check http://pear.php.net/bugs/bug.php?id=20383 for more details
     system "mv", "#{CREW_DEST_DIR}/.depdb", "#{CREW_DEST_LIB_PREFIX}/php"
@@ -116,17 +128,17 @@ class Php5 < Package
   def self.postinstall
     puts
     puts "To start the php-fpm service, execute:".lightblue
-    puts "php5-fpm start".lightblue
+    puts "php7-fpm start".lightblue
     puts
     puts "To stop the php-fpm service, execute:".lightblue
-    puts "php5-fpm stop".lightblue
+    puts "php7-fpm stop".lightblue
     puts
     puts "To restart the php-fpm service, execute:".lightblue
-    puts "php5-fpm restart".lightblue
+    puts "php7-fpm restart".lightblue
     puts
     puts "To start php-fpm on login, execute the following:".lightblue
-    puts "echo 'if [ -f #{CREW_PREFIX}/bin/php5-fpm ]; then' >> ~/.bashrc".lightblue
-    puts "echo '  #{CREW_PREFIX}/bin/php5-fpm start' >> ~/.bashrc".lightblue
+    puts "echo 'if [ -f #{CREW_PREFIX}/bin/php7-fpm ]; then' >> ~/.bashrc".lightblue
+    puts "echo '  #{CREW_PREFIX}/bin/php7-fpm start' >> ~/.bashrc".lightblue
     puts "echo 'fi' >> ~/.bashrc".lightblue
     puts "source ~/.bashrc".lightblue
   end
