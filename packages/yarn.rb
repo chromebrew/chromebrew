c1f45327ceaae06f689123b590934f0db3480375
Ed Reel <edreel@gmail.com>
2019-12-02 05:49:34 -0600

    Update yarn from 1.19.1 to 1.21.0 (#3679)

--- packages/yarn.rb
+++ packages/yarn.rb
@@ -3,9 +3,9 @@ require 'package'
 class Yarn < Package
   description 'Yarn is a new package manager for JavaScript and an alternative to npm.'
   homepage 'https://yarnpkg.com/en/'
-  version '1.19.1'
-  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.19.1/yarn-v1.19.1.tar.gz'
-  source_sha256 '34293da6266f2aae9690d59c2d764056053ff7eebc56b80b8df05010c3da9343'
+  version '1.21.0'
+  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.21.0/yarn-v1.21.0.tar.gz'
+  source_sha256 'dd17d4e5bc560aa28140038a31fa50603ef76b710fee44e5ec5efbea7ad24c61'
 
   binary_url ({
   })
@@ -16,8 +16,8 @@ class Yarn < Package
   depends_on 'nodebrew' unless "#{node_version}" != ""
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}"
-    system "cp -r lib/ #{CREW_DEST_PREFIX}"
-    system "cp -r bin/ #{CREW_DEST_PREFIX}"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}"
+    FileUtils.cp_r 'lib/', "#{CREW_DEST_PREFIX}"
+    FileUtils.cp_r 'bin/', "#{CREW_DEST_PREFIX}"
   end
 end
