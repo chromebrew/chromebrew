bc619a53a46e5218194710bd25f5ac95d037fe8f
Ed Reel <edreel@gmail.com>
2019-12-02 05:49:47 -0600

    Add php74 package (#3680)
    Update php package with new 7.4 option
    
    Add oniguruma dependency
    
    Add pre-built binaries

--- packages/php.rb
+++ packages/php.rb
@@ -3,7 +3,7 @@ require 'package'
 class Php < Package
   description 'PHP is a popular general-purpose scripting language that is especially suited to web development.'
   homepage 'http://www.php.net/'
-  version '5.6.40-7.3.12'
+  version '5.6.40-7.4.0'
 
   is_fake
 
@@ -17,6 +17,7 @@ class Php < Package
     puts "  7.1 = PHP 7.1.33"
     puts "  7.2 = PHP 7.2.25"
     puts "  7.3 = PHP 7.3.12"
+    puts "  7.4 = PHP 7.4.0"
     puts "    0 = Cancel"
 
     while version = STDIN.gets.chomp
@@ -35,6 +36,9 @@ class Php < Package
         break
       when '7.3'
         depends_on 'php73'
+        break
+      when '7.4'
+        depends_on 'php74'
         break
       when '0'
         abort
