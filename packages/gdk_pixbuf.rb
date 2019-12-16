5740f03711e5b0417b5bbe578885c7a173adf19b
Ed Reel <edreel@gmail.com>
2019-12-16 13:00:51 -0600

    Rebuild gdk_pixbuf to update paths (#3697)
    Add pre-built binaries

--- packages/gdk_pixbuf.rb
+++ packages/gdk_pixbuf.rb
@@ -3,21 +3,21 @@ require 'package'
 class Gdk_pixbuf < Package
   description 'GdkPixbuf is a library for image loading and manipulation.'
   homepage 'https://developer.gnome.org/gdk-pixbuf'
-  version '2.40.0'
+  version '2.40.0-1'
   source_url 'https://ftp.gnome.org/pub/gnome/sources/gdk-pixbuf/2.40/gdk-pixbuf-2.40.0.tar.xz'
   source_sha256 '1582595099537ca8ff3b99c6804350b4c058bb8ad67411bbaae024ee7cead4e6'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-1-chromeos-armv7l.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-1-chromeos-x86_64.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gdk_pixbuf-2.40.0-1-chromeos-i686.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '27c5d0f1f700078e2e1ec3497332468b433d654921c429f1babafcdd5dce8dd4',
-     armv7l: '27c5d0f1f700078e2e1ec3497332468b433d654921c429f1babafcdd5dce8dd4',
-       i686: '0fe14285a8dba7328f45d719a7f4b57112a561a4daffca3631d7b49fcba0efc3',
-     x86_64: '04f4f25490ad360d585b34ca81b47ebc25d1f072a4062199d5ba090d13e89bb4',
+    aarch64: '1ee4c25a59579f11c12b3ef0f2b466710268164e4810c879861aa65a4185edfb',
+     armv7l: '1ee4c25a59579f11c12b3ef0f2b466710268164e4810c879861aa65a4185edfb',
+     x86_64: 'b8ab8af77ef2b41003487ea06f17ffa8e3828af62b646aa634b8a092160df1ea',
+       i686: 'e041085afe17c2a853ef5057e44c05e3e3b0e4b943557f1d60e59b574ee52bf3',
   })
 
   depends_on 'meson' => :build
@@ -33,9 +33,9 @@ class Gdk_pixbuf < Package
     Dir.mkdir 'build'
     Dir.chdir 'build' do
       system 'meson',
-             "--prefix=#{CREW_DEST_PREFIX}",
-             "--libdir=#{CREW_DEST_LIB_PREFIX}",
-             "--mandir=#{CREW_DEST_PREFIX}/share/man",
+             "--prefix=#{CREW_PREFIX}",
+             "--libdir=#{CREW_LIB_PREFIX}",
+             "--mandir=#{CREW_PREFIX}/share/man",
              '-Dinstalled_tests=false',
              '-Dbuildtype=release',
              '-Drelocatable=true',
@@ -48,7 +48,7 @@ class Gdk_pixbuf < Package
 
   def self.install
     Dir.chdir 'build' do
-      system 'ninja install'
+      system "DESTDIR=#{CREW_DEST_DIR} ninja install"
     end
     loader_dir = "#{CREW_DEST_LIB_PREFIX}/gdk-pixbuf-2.0/2.10.0/loaders"
     FileUtils.mkdir_p "#{loader_dir}"
