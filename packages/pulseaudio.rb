15fce57a1f60ab402d6e5011fab6dabcb41372ac
Ed Reel <edreel@gmail.com>
2019-12-16 19:34:18 -0600

    Rebuild pulseaudio and add dependencies
    Move from read-only to a writable directory
    
    Add pre-built binaries

--- packages/pulseaudio.rb
+++ packages/pulseaudio.rb
@@ -3,21 +3,21 @@ require 'package'
 class Pulseaudio < Package
   description 'PulseAudio is a sound system for POSIX OSes, meaning that it is a proxy for your sound applications.'
   homepage 'https://www.freedesktop.org/wiki/Software/PulseAudio/'
-  version '13.0'
+  version '13.0-1'
   source_url 'https://freedesktop.org/software/pulseaudio/releases/pulseaudio-13.0.tar.xz'
   source_sha256 '961b23ca1acfd28f2bc87414c27bb40e12436efcf2158d29721b1e89f3f28057'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'aa7b56bcd26e70751a4cbc8dcfb9f5c6e875787d2000845d17915e93af873d1f',
-     armv7l: 'aa7b56bcd26e70751a4cbc8dcfb9f5c6e875787d2000845d17915e93af873d1f',
-       i686: '8da3c531177e998b0460f9955721531bb54c1c6a94497f7ce4ecdbd4c3ac1dfd',
-     x86_64: 'a6cb37e2bbf484eae8c3752bbc26c9092210a6401b5a86ccef5d0f683193f6c5',
+    aarch64: 'b716830733de485cc75e7cacdc67a75210b76aa9efde63ca60c4c16bea22d6fe',
+     armv7l: 'b716830733de485cc75e7cacdc67a75210b76aa9efde63ca60c4c16bea22d6fe',
+       i686: '9dd90bb5103bea3aa384dbb3223e39ed64db0aeef98bae4455bc3ec915c3b21b',
+     x86_64: '0cddede21a0f1c4d4f637eed06e808058f76533681f9d0161f788a9951cef68e',
   })
 
   depends_on 'gsettings_desktop_schemas'
@@ -33,6 +33,8 @@ class Pulseaudio < Package
   depends_on 'eudev'
   depends_on 'gtk3'
   depends_on 'dbus'
+  depends_on 'gdbm'
+  depends_on 'tdb'
 
   def self.build
     system './configure',
@@ -43,6 +45,7 @@ class Pulseaudio < Package
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    FileUtils.mv "#{CREW_DEST_DIR}/lib", "#{CREW_DEST_PREFIX}/libexec"
   end
 
   def self.check
