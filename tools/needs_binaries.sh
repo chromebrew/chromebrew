44ed92339ede81439111249c967c7a7dceac550c
Ed Reel <edreel@gmail.com>
2019-12-17 00:38:38 -0600

    Update dart from 1.24.3 to 2.7.0
    Add dart to the list of exclusions for binaries

--- tools/needs_binaries.sh
+++ tools/needs_binaries.sh
@@ -6,7 +6,7 @@ fi
 cd ../packages
 arch=$(uname -m)
 exclusions='android_studio.rb antlr4.rb asciinema.rb autosetup.rb broadway.rb cabal.rb checkinstall.rb codium.rb composer.rb cowsay.rb'
-exclusions+=' crew_profile.rb cros_resize.rb docx2txt.rb dr.rb elixir.rb far.rb fortune.rb fortunes.rb fpc.rb freedos.rb gcc_tools.rb'
+exclusions+=' crew_profile.rb cros_resize.rb dart.rb docx2txt.rb dr.rb elixir.rb far.rb fortune.rb fortunes.rb fpc.rb freedos.rb gcc_tools.rb'
 exclusions+=' ghc.rb gittools.rb google_cloud_sdk.rb gradle.rb hugo.rb hunspell.rb julia.rb komodo.rb kr.rb ld_default.rb leiningen.rb'
 exclusions+=' libtinfo.rb lsb_release.rb mysqltuner.rb nconvert.rb neofetch.rb ngrok.rb nodebrew.rb nvm.rb oc.rb packer.rb perl_gcstring_linebreak.rb'
 exclusions+=' perl_io_socket_ssl.rb perl_locale_gettext.rb perl_locale_messages.rb perl_module_build.rb perl_read_key.rb perl_sgmls.rb'
