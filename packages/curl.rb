85d3b7a25550487e15c3f7e46afceea34aef2558
Casey Strouse <casey.strouse@gmail.com>
2019-12-26 02:27:41 -0700

    Update curl from 7.66.0 to 7.67.0

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,21 +3,13 @@ require 'package'
 class Curl < Package
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.haxx.se/'
-  version '7.66.0-1'
-  source_url 'https://curl.haxx.se/download/curl-7.66.0.tar.xz'
-  source_sha256 'dbb48088193016d079b97c5c3efde8efa56ada2ebf336e8a97d04eb8e2ed98c1'
+  version '7.67.0'
+  source_url 'https://curl.haxx.se/download/curl-7.67.0.tar.xz'
+  source_sha256 'f5d2e7320379338c3952dcc7566a140abb49edb575f9f99272455785c40e536c'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.66.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.66.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.66.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.66.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'd3fa586deef7030c2bdd3c3e477391c276b34276d5dddbf755cf4785a56ec482',
-     armv7l: 'd3fa586deef7030c2bdd3c3e477391c276b34276d5dddbf755cf4785a56ec482',
-       i686: '76dfc54131f9ad33fdc6fba5ffc74eabd730322ba88cfdf10ded79b304c1ce22',
-     x86_64: 'bee317b81814d931317a7539cd86678e28b3de46508aa09ad7a5aa648ba761ff',
   })
 
   depends_on 'groff' => :build
