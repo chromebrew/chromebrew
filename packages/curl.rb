29d7e46206f99acae157a19e3d4288245060fcd7
Ed Reel <edreel@gmail.com>
2019-12-27 10:08:28 -0600

    Add pre-built binaries for curl, postgres and python3 (#3719)

--- packages/curl.rb
+++ packages/curl.rb
@@ -8,8 +8,16 @@ class Curl < Package
   source_sha256 'f5d2e7320379338c3952dcc7566a140abb49edb575f9f99272455785c40e536c'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '5ae1098c3ab701d280fcef2266fd55d89c7c2b258a2a54141ad01abdaa394f54',
+     armv7l: '5ae1098c3ab701d280fcef2266fd55d89c7c2b258a2a54141ad01abdaa394f54',
+       i686: '497a670dbe39877b48916f3545d6a75b855624635e6c93806bd47c9280d3e490',
+     x86_64: '1c12008aa3b2c72084b2bae2c8e4c3246dc8358e25222c159ddf24ec0776ae5c',
   })
 
   depends_on 'groff' => :build
