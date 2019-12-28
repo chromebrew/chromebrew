e0ab3f5bd8796b9292b179a913ed3b03a070f80a
Vincent GIRARD <vincent@heliosphere.fr>
2019-12-28 16:26:03 +0100

    [jsonc] Remove binaries

--- packages/jsonc.rb
+++ packages/jsonc.rb
@@ -8,16 +8,8 @@ class Jsonc < Package
   source_sha256 '5d867baeb7f540abe8f3265ac18ed7a24f91fe3c5f4fd99ac3caba0708511b90'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/jsonc-0.13.1-20180305-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/jsonc-0.13.1-20180305-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/jsonc-0.13.1-20180305-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/jsonc-0.13.1-20180305-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '185e75bf352feb18166712f66783de119d304276f8af67f528d8ce2353d51103',
-     armv7l: '185e75bf352feb18166712f66783de119d304276f8af67f528d8ce2353d51103',
-       i686: 'e3333ce47bf5694de1dd4c4290938b4fd0be77dbb816afa299d1cf6304e4ed6c',
-     x86_64: 'e8ca8ecbb00bc9e7cf7949d84cbaf1efdc53883002da46e3f48733e871617304',
   })
 
 
