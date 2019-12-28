71baad83fb287d122418c51922b7146f0f490d4f
vincowl <vince512@free.fr>
2019-12-28 01:19:12 +0100

    [codium] Update to 1.41.1 (#3723)

--- packages/codium.rb
+++ packages/codium.rb
@@ -3,9 +3,9 @@ require 'package'
 class Codium < Package
   description 'VSCodium is Open Source Software Binaries of VSCode with a community-driven default configuration.'
   homepage 'https://vscodium.com/'
-  version '1.41.0'
-  source_url 'https://github.com/VSCodium/vscodium/releases/download/1.41.0/VSCodium-linux-x64-1.41.0.tar.gz'
-  source_sha256 'f56ca012fb45cf95c1d2fcff9eea088ad03614ac1c3ea664f8cdff5f53505dda'
+  version '1.41.1'
+  source_url 'https://github.com/VSCodium/vscodium/releases/download/1.41.1/VSCodium-linux-x64-1.41.1.tar.gz'
+  source_sha256 '3d50cedad289730301ddbb6cc5f754753e3fc58b0c812da5390b2871c0639c58'
 
   binary_url ({
   })
