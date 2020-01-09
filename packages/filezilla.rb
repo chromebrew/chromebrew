918dca51918e162cb2a132d99a391b1a5e9f1323
vincowl <vince512@free.fr>
2020-01-09 15:22:11 +0100

    [filezilla] Update to 3.46.3 (#3762)

--- packages/filezilla.rb
+++ packages/filezilla.rb
@@ -3,21 +3,13 @@ require 'package'
 class Filezilla < Package
   description 'FileZilla Client is a free FTP solution.'
   homepage 'https://filezilla-project.org/'
-  version '3.43.0'
-  source_url 'https://download.filezilla-project.org/client/FileZilla_3.43.0_src.tar.bz2'
-  source_sha256 '6fe652c360682f860abe2dc2e196bddca6260242cefe6fe779d42c327c01258e'
+  version '3.46.3'
+  source_url 'https://download.filezilla-project.org/client/FileZilla_3.46.3_src.tar.bz2'
+  source_sha256 'f1c5bff0a99c39447a5ea9fdfcc16cc408876ef52fa8d5e7bf041ea0717a7395'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.43.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.43.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.43.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.43.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'fdef2404efb7251ba6c9b3675d6f7364b57272cc8c431e8de69761224bae5f9d',
-     armv7l: 'fdef2404efb7251ba6c9b3675d6f7364b57272cc8c431e8de69761224bae5f9d',
-       i686: '652990bad986fc083e5d9137bf9df6bba7479ca8ec255a9250c34680b5676610',
-     x86_64: '3fe771c921d4738d0c82d33bfd0aeea1405009ce0ea7d126603801f40abec1ee',
   })
 
   depends_on 'dbus'
