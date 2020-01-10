8f130308f1cfb0e846e2796d389f2bd012569ffe
Ed Reel <edreel@gmail.com>
2020-01-09 19:39:07 -0600

    Add pre-built binaries for filezilla

--- packages/filezilla.rb
+++ packages/filezilla.rb
@@ -8,8 +8,16 @@ class Filezilla < Package
   source_sha256 'f1c5bff0a99c39447a5ea9fdfcc16cc408876ef52fa8d5e7bf041ea0717a7395'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.46.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.46.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.46.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.46.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '47a8469458b831385a5120538a967ffe947d4e072c9075f19d0c9f15e4a2ccab',
+     armv7l: '47a8469458b831385a5120538a967ffe947d4e072c9075f19d0c9f15e4a2ccab',
+       i686: '2c1aa841ff739162478e8bef80097229f1fe66437db2fd41f6f517070c35116f',
+     x86_64: '8f3c9199aa0dadec311daba0c0e1c0a06d407376a882da4e4468278b5d806b08',
   })
 
   depends_on 'dbus'
