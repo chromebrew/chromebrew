9b8d0fff68c837e6280e3c4c90071b3d464b2a50
Ed Reel <edreel@gmail.com>
2020-01-21 09:48:13 -0600

    Update meson from 0.52.0 to 0.53.0 (#3775)
    Add pre-built binaries

--- packages/meson.rb
+++ packages/meson.rb
@@ -3,21 +3,21 @@ require 'package'
 class Meson < Package
   description 'The Meson Build System'
   homepage 'http://mesonbuild.com/'
-  version '0.52.0'
-  source_url 'https://github.com/mesonbuild/meson/releases/download/0.52.0/meson-0.52.0.tar.gz'
-  source_sha256 'd60f75f0dedcc4fd249dbc7519d6f3ce6df490033d276ef1cf27453ef4938d32'
+  version '0.53.0'
+  source_url 'https://github.com/mesonbuild/meson/releases/download/0.53.0/meson-0.53.0.tar.gz'
+  source_sha256 '035e75993ab6fa6c9ebf902b835c64cf397a763eb8e65c9bb6e1cc9730a9d3f6'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.52.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.52.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.52.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.52.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '92400a58c43f1d1c729fea0b548bea093a439110729b7dabf7fe56c6b816d721',
-     armv7l: '92400a58c43f1d1c729fea0b548bea093a439110729b7dabf7fe56c6b816d721',
-       i686: 'a83a4e4b1a2b516141e5fe744e9b4f57ec823dd9561e2ff22e558015adc4e3d3',
-     x86_64: '504f7a9829eb50e497c4aba9b2115909144b4e36909e3648453ad96686cce60d',
+    aarch64: 'cce1f631fe48be9a287ffdfe18fb0c841e880af98d5c486c07dee5dbf4722182',
+     armv7l: 'cce1f631fe48be9a287ffdfe18fb0c841e880af98d5c486c07dee5dbf4722182',
+       i686: '5dad566b1bd6d142c78b53cecf4c06068c2b6c0a1901ec7aeb6bff55b11a84e4',
+     x86_64: '5246ffe7d4b2b2901a2c9dd21c6516634314d247c8a2dc09d3a0a3c516485923',
   })
 
   depends_on 'ninja'
