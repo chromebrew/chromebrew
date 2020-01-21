630e8d76d6fd5f82bcc2b5a0efb9922bbbc3aa18
Ed Reel <edreel@gmail.com>
2020-01-21 09:48:37 -0600

    Update make from 4.2.1 to 4.3 (#3777)
    Add pre-built binaries

--- packages/make.rb
+++ packages/make.rb
@@ -3,34 +3,36 @@ require 'package'
 class Make < Package
   description 'GNU Make is a tool which controls the generation of executables and other non-source files of a program from the program\'s source files.'
   homepage 'https://www.gnu.org/software/make/'
-  version '4.2.1'
-  source_url 'https://ftp.gnu.org/gnu/make/make-4.2.1.tar.bz2'
-  source_sha256 'd6e262bf3601b42d2b1e4ef8310029e1dcf20083c5446b4b7aa67081fdffc589'
+  version '4.3'
+  source_url 'https://ftpmirror.gnu.org/make/make-4.3.tar.lz'
+  source_sha256 'de1a441c4edf952521db30bfca80baae86a0ff1acd0a00402999344f04c45e82'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.2.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.2.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.2.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.2.1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/make-4.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'bbfd8a8ab6ab5caae56b259ebede8ebf269efbb76999eab8809aa86256b3403e',
-     armv7l: 'bbfd8a8ab6ab5caae56b259ebede8ebf269efbb76999eab8809aa86256b3403e',
-       i686: '979bc4e45fb8367c68cdccfa13b2d5f920fd7543c287963cc072fd1bc0682163',
-     x86_64: 'ebab3261fbd37c88586894e3f79c1eedfbc3d24d999996bc5abb974a8a07980a',
+    aarch64: '88a00eb5088f6a6b15c510f75040f7932b467e3f8306166052f24b56b4cd9405',
+     armv7l: '88a00eb5088f6a6b15c510f75040f7932b467e3f8306166052f24b56b4cd9405',
+       i686: '1138cb670502f23d7e0acf189b59982f045715693c143c982f96f4c77eca365b',
+     x86_64: '26dee9755f910ebbc5e8742fc40937dc4b5900ded4a415b26aa282def3821d9d',
   })
 
   def self.build
-    system "./configure"
-    system "./build.sh"
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           '--enable-cross-guesses=conservative'
+    system './build.sh'
   end
 
   def self.install
-    system "./make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system './make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
     # Give it several tries since output-sync fails rarely
-    system "./make check || ./make check || ./make check"
+    system './make check || ./make check || ./make check'
   end
 end
