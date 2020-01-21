94b9c30d2251f831e88441a2f38feeb78a3f704e
Ed Reel <edreel@gmail.com>
2020-01-21 09:48:25 -0600

    Update gawk from 4.2.1 to 5.0.1 (#3776)
    Add pre-built binaries

--- packages/gawk.rb
+++ packages/gawk.rb
@@ -3,21 +3,21 @@ require 'package'
 class Gawk < Package
   description 'The gawk utility interprets a special-purpose programming language that makes it possible to handle simple data-reformatting jobs with just a few lines of code.'
   homepage 'https://www.gnu.org/software/gawk/'
-  version '4.2.1'
-  source_url 'https://ftpmirror.gnu.org/gawk/gawk-4.2.1.tar.gz'
-  source_sha256 '2b23d51503b2df9a41aa6fddc6002ad7ebf2a386ac19dc1b6be0dd48b0acf6db'
+  version '5.0.1'
+  source_url 'https://ftpmirror.gnu.org/gawk/gawk-5.0.1.tar.xz'
+  source_sha256 '8e4e86f04ed789648b66f757329743a0d6dfb5294c3b91b756a474f1ce05a794'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-4.2.1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-5.0.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-5.0.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-5.0.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gawk-5.0.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '8a9bcb6324601c08e562c2a05b9986a4124922a666474ac9419aa144f21dda63',
-     armv7l: '8a9bcb6324601c08e562c2a05b9986a4124922a666474ac9419aa144f21dda63',
-       i686: '30522a5f3d71d4c8189d7ff2b10e09bcf0209e87f5b92ac0ecde62324c7776ff',
-     x86_64: '64faab8c251933f07df9b6dbfacad9983dafdb831168e6dc912a38cfdae4af41',
+    aarch64: '6d0fe32829c7f60400a11c727a01db6d2dcd6997f5180188fd0ff04f75f82758',
+     armv7l: '6d0fe32829c7f60400a11c727a01db6d2dcd6997f5180188fd0ff04f75f82758',
+       i686: 'a4d55290bf0bab116a6b8740b652d023dc66b6c86cb18a889decce7b257363c1',
+     x86_64: 'cec79d580d52eca31927087cc1346fff08028edb3f9161106ca87719dd6b2350',
   })
 
   depends_on 'libsigsegv'
@@ -32,7 +32,7 @@ class Gawk < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
