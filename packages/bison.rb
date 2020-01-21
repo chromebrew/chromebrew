be370069e78bd487c649d6c08e70b6e8093593fd
Ed Reel <edreel@gmail.com>
2020-01-21 09:47:05 -0600

    Update bison from 3.4.2 to 3.5 (#3772)
    Add pre-built binaries

--- packages/bison.rb
+++ packages/bison.rb
@@ -3,21 +3,21 @@ require 'package'
 class Bison < Package
   description 'Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser employing LALR(1) parser tables.'
   homepage 'http://www.gnu.org/software/bison/'
-  version '3.4.2'
-  source_url 'https://ftpmirror.gnu.org/gnu/bison/bison-3.4.2.tar.xz'
-  source_sha256 '27d05534699735dc69e86add5b808d6cb35900ad3fd63fa82e3eb644336abfa0'
+  version '3.5'
+  source_url 'https://ftpmirror.gnu.org/gnu/bison/bison-3.5.tar.xz'
+  source_sha256 '55e4a023b1b4ad19095a5f8279f0dc048fa29f970759cea83224a6d5e7a3a641'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.4.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.4.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.4.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.4.2-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '5e075d35935386c9f2e05c520ed10b2c0d5d453337f5d4cd6a663dcaaa675250',
-     armv7l: '5e075d35935386c9f2e05c520ed10b2c0d5d453337f5d4cd6a663dcaaa675250',
-       i686: '6a114d5b271549af936db512fabd910ae2329fcfd2373a83ce47f8c2232e3aae',
-     x86_64: 'd020a319652c3c70c3530c35b6fc9aba85d25be25fe40c160cb8cc8a87112a31',
+    aarch64: '1a81b867d451925f47e43af13c6b270cae838511d5ae88cdbcdc6b1e95e04489',
+     armv7l: '1a81b867d451925f47e43af13c6b270cae838511d5ae88cdbcdc6b1e95e04489',
+       i686: 'af29f98f0888cb0d00e53edcacd4084a36e2f81bd119655d4e7a96eb9637b4f5',
+     x86_64: 'ac8aa34ea566a028fa22d85b51cbda6fb0436a4608646b6b479707e8826a5009',
   })
 
   def self.build
