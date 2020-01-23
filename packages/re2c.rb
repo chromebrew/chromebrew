aba4ccdfd4be617fe3a38bff74c317f0782211ee
Ed Reel <edreel@gmail.com>
2020-01-23 00:36:32 -0600

    Update re2c from 1.1.1 to 1.3
    Add pre-built binaries

--- packages/re2c.rb
+++ packages/re2c.rb
@@ -3,21 +3,21 @@ require 'package'
 class Re2c < Package
   description 're2c is a free and open-source lexer generator for C and C++.'
   homepage 'http://re2c.org/'
-  version '1.1.1'
-  source_url 'https://github.com/skvadrik/re2c/releases/download/1.1.1/re2c-1.1.1.tar.gz'
-  source_sha256 '856597337ea00b24ce91f549f79e6eece1b92ba5f8b63292cad66c14ac7451cf'
+  version '1.3'
+  source_url 'https://github.com/skvadrik/re2c/releases/download/1.3/re2c-1.3.tar.xz'
+  source_sha256 'f37f25ff760e90088e7d03d1232002c2c2672646d5844fdf8e0d51a5cd75a503'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.1.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.1.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.1.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.1.1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/re2c-1.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'e0afdfb419024f81bf62dd338ba6e80ef9a04abc83b8b4c1fd956ca6beed0902',
-     armv7l: 'e0afdfb419024f81bf62dd338ba6e80ef9a04abc83b8b4c1fd956ca6beed0902',
-       i686: '159d7224aac0c6649afb531f17d420a51a459ba1354ec91f6f9e1ca5f5d6f31c',
-     x86_64: 'f1d91e37777b18324aea0e6dcad7927c50aed93daa91be6118a7760eebfa5b7a',
+    aarch64: '1ee30785ba83b9ed00258fa126f18a14e687cd4902a20f10b9bf1ce322f4692f',
+     armv7l: '1ee30785ba83b9ed00258fa126f18a14e687cd4902a20f10b9bf1ce322f4692f',
+       i686: '0fefae6d421dbe3166a1cefd28473264423d158ee52b41b3aec3cfb7ccfb5526',
+     x86_64: 'a4639d6224bf8537e25703f03161a3f849e05160d40282b274b91611a674b069',
   })
 
   def self.build
@@ -31,6 +31,6 @@ class Re2c < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
