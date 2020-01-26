0fadcbf41db46df20722733ce92d0dbc77e435dd
Ed Reel <edreel@gmail.com>
2020-01-25 20:05:36 -0600

    Update wing from 7.1.3.0 to 7.2.0.1

--- packages/wing.rb
+++ packages/wing.rb
@@ -3,15 +3,18 @@ require 'package'
 class Wing < Package
   description 'Wing Personal is a free Python IDE designed for students and hobbyists.'
   homepage 'https://wingware.com/'
-  version '7.1.3.0'
-  source_url 'https://wingware.com/pub/wing-personal/7.1.3.0/wing-personal-7.1.3.0-linux-x64.tar.bz2'
-  source_sha256 '17d0b9152a25fac3eda9d053e6cd284a118ece478b1a87c8afcebb60f71f6864'
+  version '7.2.0.1'
+  case ARCH
+  when 'x86_64'
+    source_url 'https://wingware.com/pub/wing-personal/7.2.0.1/wing-personal-7.2.0.1-linux-x64.tar.bz2'
+    source_sha256 '75a74725c8f7b8f4b00f5f24a259bfdc7b2ebcd84fa0a68e6ad2664e028ed54b'
+  end
 
   binary_url ({
-    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wing-7.1.3.0-chromeos-x86_64.tar.xz',
+    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wing-7.2.0.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    x86_64: 'f17dedf46eecb804986894058ea5012572847823c90715fc120481f2e35b2d79',
+    x86_64: '144dab3ac72b4f020a74f36e87eca46299939547c63106667be97a748d62b771',
   })
 
   depends_on 'python27'
@@ -22,16 +25,15 @@ class Wing < Package
   end
 
   def self.install
-    abort "#{ARCH} architecture is not supported.".lightred unless ARCH == 'x86_64'
     # The install is interactive, so remember to prefix directories with #{CREW_PREFIX} when prompted.
     system './wing-install.py'
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
     FileUtils.mkdir_p "#{CREW_DEST_HOME}/.wingpersonal7"
     system "touch #{CREW_DEST_HOME}/.wingpersonal7/ide.log"
-    FileUtils.cp "#{CREW_PREFIX}/bin/wing-personal7.1", "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.cp_r "#{CREW_PREFIX}/share/wing-personal7.1", "#{CREW_DEST_PREFIX}/share"
-    FileUtils.ln_s "#{CREW_PREFIX}/share/wing-personal7.1/wing-personal", "#{CREW_DEST_PREFIX}/bin/wing"
+    FileUtils.cp "#{CREW_PREFIX}/bin/wing-personal7.2", "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.cp_r "#{CREW_PREFIX}/share/wing-personal7.2", "#{CREW_DEST_PREFIX}/share"
+    FileUtils.ln_s "#{CREW_PREFIX}/share/wing-personal7.2/wing-personal", "#{CREW_DEST_PREFIX}/bin/wing"
   end
 
   def self.postinstall
