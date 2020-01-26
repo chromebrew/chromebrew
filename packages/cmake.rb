ebf591b2c67b51fedb1b744a932324f387714a39
Ed Reel <edreel@gmail.com>
2020-01-25 21:26:14 -0600

    Add pre-built binaries

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -8,8 +8,16 @@ class Cmake < Package
   source_sha256 'e54f16df9b53dac30fd626415833a6e75b0e47915393843da1825b096ee60668'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'ad08eeacaf54d48e0e926667bde7138225f0559e24c31d894547bf2e5369ab01',
+     armv7l: 'ad08eeacaf54d48e0e926667bde7138225f0559e24c31d894547bf2e5369ab01',
+       i686: '5902f1320df14c8140669fc01c042463e3592691958793650f220601dfab6c22',
+     x86_64: 'c00f519bdd2cc7c9553f28c3af690320ab81cacf8b95644bfd1a4021114e9ba3',
   })
 
   def self.patch
