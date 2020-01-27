3252eacfb1accf361cb75fa265aa05364aa72ac7
Ed Reel <edreel@gmail.com>
2020-01-27 10:25:27 -0600

    Update cmake from 3.16.0 to 3.16.3 (#3788)
    * Update cmake from 3.16.0 to 3.16.3
    
    * Add pre-built binaries

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,21 +3,21 @@ require 'package'
 class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  version '3.16.0'
-  source_url 'https://cmake.org/files/v3.16/cmake-3.16.0.tar.gz'
-  source_sha256 '6da56556c63cab6e9a3e1656e8763ed4a841ac9859fefb63cbe79472e67e8c5f'
+  version '3.16.3'
+  source_url 'https://cmake.org/files/v3.16/cmake-3.16.3.tar.gz'
+  source_sha256 'e54f16df9b53dac30fd626415833a6e75b0e47915393843da1825b096ee60668'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.16.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '9cb171530fbf62c483fbbb6311da02f77718b05f7a62ea79eac8238edd189516',
-     armv7l: '9cb171530fbf62c483fbbb6311da02f77718b05f7a62ea79eac8238edd189516',
-       i686: '03b373b0891dcd883b6f1d397785366b8e94d33a6fa88f4e591780d8d5bb8fb0',
-     x86_64: '5fa7550151e4861114f93d968e7ddd30f7494767953c79ed0ea291fe87845437',
+    aarch64: 'ad08eeacaf54d48e0e926667bde7138225f0559e24c31d894547bf2e5369ab01',
+     armv7l: 'ad08eeacaf54d48e0e926667bde7138225f0559e24c31d894547bf2e5369ab01',
+       i686: '5902f1320df14c8140669fc01c042463e3592691958793650f220601dfab6c22',
+     x86_64: 'c00f519bdd2cc7c9553f28c3af690320ab81cacf8b95644bfd1a4021114e9ba3',
   })
 
   def self.patch
