854748682aa28e17ef6f56c03bfb9fa4315e1519
Ed Reel <edreel@gmail.com>
2020-01-26 23:37:59 -0600

    Update pycharm from 2019.3.1 to 2019.3.2

--- packages/pycharm.rb
+++ packages/pycharm.rb
@@ -3,14 +3,9 @@ require 'package'
 class Pycharm < Package
   description 'The Python IDE for Professional Developers'
   homepage 'https://www.jetbrains.com/pycharm/'
-  version '2019.3.1'
-  source_url 'https://download.jetbrains.com/python/pycharm-community-anaconda-2019.3.1.tar.gz'
-  source_sha256 '05562eb56ecf8cfd798e6561db9567a4cafefa8b8e5411d7043e9a65fef60eb9'
-
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
+  version '2019.3.2'
+  source_url 'https://download.jetbrains.com/python/pycharm-community-2019.3.2.tar.gz'
+  source_sha256 '2262e37adbf00a65c9d672c2fa338aa97e4a17a853ffa915da0f4b83b5c4bf19'
 
   depends_on 'jdk8'
   depends_on 'xdg_base'
