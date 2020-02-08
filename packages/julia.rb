8269aaf755e1ca909af67ce457d3d2ee45bb523d
Ed Reel <edreel@gmail.com>
2020-02-08 13:52:17 -0600

    Update julia from 1.0.4 to 1.3.1

--- packages/julia.rb
+++ packages/julia.rb
@@ -3,23 +3,18 @@ require 'package'
 class Julia < Package
   description 'Julia is a flexible dynamic language, appropriate for scientific and numerical computing'
   homepage 'https://julialang.org/'
-  version '1.0.4'
+  version '1.3.1'
   case ARCH
-  when 'x86_64'
-    source_url 'https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.4-linux-x86_64.tar.gz'
-    source_sha256 '9ffbcf7f4a111e13415954caccdd1ce90b5c835cee9f62d6ac708f5b752c87dd'
+  when 'aarch64', 'armv7l'
+   source_url 'https://julialang-s3.julialang.org/bin/linux/armv7l/1.3/julia-1.3.1-linux-armv7l.tar.gz'
+   source_sha256 '965c8fab2214f8ce1b3d449d088561a6de61be42543b48c3bbadaed5b02bf824'
   when 'i686'
-    source_url 'https://julialang-s3.julialang.org/bin/linux/x86/1.0/julia-1.0.4-linux-i686.tar.gz'
-    source_sha256 'e2ce5fa564242c2dbb836f9493166ce6eaaec8f46db9861b4cdad047497dd4c4'
-  when 'armv7l', 'aarch64'
-   source_url 'https://julialang-s3.julialang.org/bin/linux/armv7l/1.0/julia-1.0.4-linux-armv7l.tar.gz'
-   source_sha256 '2e742f4ddf5ac21779b6943ef210d73dc02f1c6de23836b352336a3dcbb18ae6'
+    source_url 'https://julialang-s3.julialang.org/bin/linux/x86/1.3/julia-1.3.1-linux-i686.tar.gz'
+    source_sha256 '2cef14e892ac317707b39d2afd9ad57a39fb77445ffb7c461a341a4cdf34141a'
+  when 'x86_64'
+    source_url 'https://julialang-s3.julialang.org/bin/linux/x64/1.3/julia-1.3.1-linux-x86_64.tar.gz'
+    source_sha256 'faa707c8343780a6fe5eaf13490355e8190acf8e2c189b9e7ecbddb0fa2643ad'
   end
-
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}/julia"
