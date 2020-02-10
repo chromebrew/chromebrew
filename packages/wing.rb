f6e1ca44a433db24905d6352c851e0d057369604
Ed Reel <edreel@gmail.com>
2020-02-10 11:04:07 -0600

    Update wing from 7.2.0.1 to 7.2.1.0 (#3809)
    * Update wing from 7.2.0.1 to 7.2.1.0
    
    * Add pre-built binary

--- packages/wing.rb
+++ packages/wing.rb
@@ -3,18 +3,18 @@ require 'package'
 class Wing < Package
   description 'Wing Personal is a free Python IDE designed for students and hobbyists.'
   homepage 'https://wingware.com/'
-  version '7.2.0.1'
+  version '7.2.1.0'
   case ARCH
   when 'x86_64'
-    source_url 'https://wingware.com/pub/wing-personal/7.2.0.1/wing-personal-7.2.0.1-linux-x64.tar.bz2'
-    source_sha256 '75a74725c8f7b8f4b00f5f24a259bfdc7b2ebcd84fa0a68e6ad2664e028ed54b'
+    source_url 'https://wingware.com/pub/wing-personal/7.2.1.0/wing-personal-7.2.1.0-linux-x64.tar.bz2'
+    source_sha256 'e42a8269a08c8bff6a91d021dcc11de8ab0b007a5a267bdeb870e7369f155064'
   end
 
   binary_url ({
-    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wing-7.2.0.1-chromeos-x86_64.tar.xz',
+    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wing-7.2.1.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    x86_64: '144dab3ac72b4f020a74f36e87eca46299939547c63106667be97a748d62b771',
+    x86_64: '8be03ea9bcfa884c4f4a324190ecf6cbefd7b82389e264b6a35e318f30ec7ed3',
   })
 
   depends_on 'python27'
