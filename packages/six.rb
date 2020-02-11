fc40dc049326e5f020e736b2807115f032725036
Ed Reel <edreel@gmail.com>
2020-02-10 22:12:11 -0600

    Update six from 1.12.0 to 1.14.0
    Add pre-built binaries

--- packages/six.rb
+++ packages/six.rb
@@ -3,21 +3,21 @@ require 'package'
 class Six < Package
   description 'Six is a Python 2 and 3 compatibility library.'
   homepage 'https://github.com/benjaminp/six'
-  version '1.12.0'
-  source_url 'https://github.com/benjaminp/six/archive/1.12.0.tar.gz'
-  source_sha256 '0ce7aef70d066b8dda6425c670d00c25579c3daad8108b3e3d41bef26003c852'
+  version '1.14.0'
+  source_url 'https://github.com/benjaminp/six/archive/1.14.0.tar.gz'
+  source_sha256 '6efff7289d1d369f0a25180433aba83ec2584e489e90f115b52ba69e4816cfb4'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.12.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.12.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.12.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.12.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.14.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.14.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.14.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/six-1.14.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'ba69ece2f777f5c2d7446f8f40c075e03ec4cfde838bd88cb7a7af5ecc77102e',
-     armv7l: 'ba69ece2f777f5c2d7446f8f40c075e03ec4cfde838bd88cb7a7af5ecc77102e',
-       i686: '7bc2e16af9e81fe89fda678fd770808fcd022f62dc9719a515b6dfaa54d93f55',
-     x86_64: '206dea928935e759ef3f37193595b32e1e2eef16f4be877ec41fe50cb6c67d2a',
+    aarch64: '194646b4fb1a0ed11492d21a45bc09d91ab7da78cbc8a6ad2f0c3ebe723692ba',
+     armv7l: '194646b4fb1a0ed11492d21a45bc09d91ab7da78cbc8a6ad2f0c3ebe723692ba',
+       i686: '51c17725b9925874af0fa7bcd86afbc930b5023ab353efb3754c69d7e7d52d4c',
+     x86_64: '649ca8a551af2fd558bc889cfa0df05ff2e2b853ea76786b61d5d8a765adcd02',
   })
 
   depends_on 'python3'
