e8fa4a6087d73d9425e554112946d75dfdd5380c
daemonspudguy <jacob.a.tice@gmail.com>
2020-02-28 20:14:27 -0500

    Update alpine.rb

--- packages/alpine.rb
+++ packages/alpine.rb
@@ -8,7 +8,14 @@ class Alpine < Package                 # The first character of the class name m
   source_sha256 '849567c1b6f71fde3aaa1c97cf0577b12a525d9e22c0ea47797c4bf1cd2bbfdb'   # Use the command "sha256sum"
 
   def self.build                   # the steps required to build the package
-    system "./configure", "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}", "--with-ssl-dir=/etc/ssl", "--with-ssl-include-dir=/usr/local/include", "--with-ssl-lib-dir=#{CREW_LIB_PREFIX}", "--disable-nls", "--with-system-pinerc=#{CREW_PROFILE}/etc/alpine.d/pine.conf", "--with-sysytem-fixed-pinerc=#{CREW_PREFIX}/etc/alpine/pine.conf.fixed"
+    system "./configure",
+    "--prefix=#{CREW_PREFIX}",
+    "--libdir=#{CREW_LIB_PREFIX}",
+    "--with-ssl-dir=/etc/ssl",
+    "--with-ssl-include-dir=/usr/local/include",
+    "--with-ssl-lib-dir=#{CREW_LIB_PREFIX}", "--disable-nls",
+    "--with-system-pinerc=#{CREW_PREFIX}/etc/alpine.d/pine.conf",
+    "--with-system-fixed-pinerc=#{CREW_PREFIX}/etc/alpine/pine.conf.fixed"
     system "make"
   end
 
