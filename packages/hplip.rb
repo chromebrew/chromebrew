cdd37f38bd751e74fed6b737ae23ca88089c236f
Casey Strouse <casey.strouse@gmail.com>
2020-03-01 13:53:43 -0700

    [hplip] Add missing dependencies and update to 3.20.2 (#3853)
    * Add missing dependencies and update to 3.20.2
    
    Tested on x86_64. Source builds on ARM still don't work.
    
    * Change libjpeg dep to use alternate package

--- packages/hplip.rb
+++ packages/hplip.rb
@@ -3,17 +3,24 @@ require 'package'
 class Hplip < Package
   description 'Print, Scan and Fax Drivers for Linux'
   homepage 'https://developers.hp.com/hp-linux-imaging-and-printing/'
-  version '3.18.7'
-  source_url 'https://sourceforge.net/projects/hplip/files/hplip/3.18.7/hplip-3.18.7.tar.gz'
-  source_sha256 '55b879e1d6d0d88c32d79486b748a21759cab404036d883be4ea9fcb55bfeb1d'
+  version '3.20.2'
+  source_url 'https://sourceforge.net/projects/hplip/files/hplip/3.20.2/hplip-3.20.2.tar.gz'
+  source_sha256 '90c49d74688b4d745a739a6db9bf8dbdfa134c24e921e31909bffe9d84f471c2'
 
   binary_url ({
   })
   binary_sha256 ({
   })
 
-  depends_on 'libjpeg'
   depends_on 'cups'
+  depends_on 'dbus'
+  depends_on 'libjpeg_turbo'
+  depends_on 'sane_backends'
+
+  def self.preinstall
+    system "curl -Ls -o config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'"
+    system "curl -Ls -o config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'"
+  end
 
   def self.build
     system "sed -i 's,/usr/bin/perl,#{CREW_PREFIX}/bin/perl,g' hpijs-drv"
