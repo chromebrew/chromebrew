2a559d915593cd48184af0d64145923e2633a871
Ed Reel <edreel@gmail.com>
2020-03-02 18:53:23 -0600

    Add pre-built binaries for hplip, hunspell_base, qrencode and testdisk

--- packages/hunspell_base.rb
+++ packages/hunspell_base.rb
@@ -8,8 +8,16 @@ class Hunspell_base < Package
   source_sha256 'bb27b86eb910a8285407cf3ca33b62643a02798cf2eef468c0a74f6c3ee6bc8a'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/hunspell_base-1.7.0-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/hunspell_base-1.7.0-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/hunspell_base-1.7.0-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/hunspell_base-1.7.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'e3e77aa133961b491fee9c7b139d3ae40f04a407faff775cf76a204a947a7b62',
+     armv7l: 'e3e77aa133961b491fee9c7b139d3ae40f04a407faff775cf76a204a947a7b62',
+       i686: 'd4cdc61aac3ab02d3234146364cb1c747e0c070a219197f6990f1d61877b3b13',
+     x86_64: '9f7a88d1e448094147b004a80b62ec5e9cf25b5e93ccbb363a2dddb3461ed8a2',
   })
 
   depends_on 'readline'
