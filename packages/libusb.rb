5050da73bcafebb30751a97834df7e40562888ed
Ed Reel <edreel@gmail.com>
2020-03-07 09:39:20 -0600

    Add pre-built binaries for libusb (#3866)

--- packages/libusb.rb
+++ packages/libusb.rb
@@ -8,8 +8,16 @@ class Libusb < Package
   source_sha256 'db11c06e958a82dac52cf3c65cb4dd2c3f339c8a988665110e0d24d19312ad8d'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libusb-1.0.23-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libusb-1.0.23-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libusb-1.0.23-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libusb-1.0.23-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '25b18e0f7bec888d717dd466c8fbbd0fe67e6bce54d052dcc52d9a004179fd0f',
+     armv7l: '25b18e0f7bec888d717dd466c8fbbd0fe67e6bce54d052dcc52d9a004179fd0f',
+       i686: 'eb3b3152a972b4849e28328a2d7d170ab7c795375def2173f77bf2a024a3dde9',
+     x86_64: 'b59a07eb8cfbb17d6d3ba7586e360b037f4ff7f96630ea4cf9ebc8a295ae7bd6',
   })
 
   depends_on 'eudev'
