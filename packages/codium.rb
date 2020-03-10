f9858b2c4489cb34a808bc2990e8103acdbb6e67
Ed Reel <edreel@gmail.com>
2020-03-10 07:40:58 -0500

    Update codium from 1.42.1 to 1.43.0

--- packages/codium.rb
+++ packages/codium.rb
@@ -3,15 +3,15 @@ require 'package'
 class Codium < Package
   description 'VSCodium is Open Source Software Binaries of VSCode with a community-driven default configuration.'
   homepage 'https://vscodium.com/'
-  version '1.42.1'
+  version '1.43.0'
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://github.com/VSCodium/vscodium/releases/download/1.42.1/VSCodium-linux-arm-1.42.1.tar.gz'
-    source_sha256 '4406e67781b543b2d1b8a25b038a675d94bd778c1d95356ca4abbcfe2758ea21'
+    source_url 'https://github.com/VSCodium/vscodium/releases/download/1.43.0/VSCodium-linux-arm-1.43.0.tar.gz'
+    source_sha256 '8c7d4cb7c75285d028ec1edcaeee2a25b1a945064308abd0dbed6a9bf91888a6'
     @arch = 'arm'
   when 'x86_64'
-    source_url 'https://github.com/VSCodium/vscodium/releases/download/1.42.1/VSCodium-linux-x64-1.42.1.tar.gz'
-    source_sha256 'be05377a2a88a3167e3959cf312c55a404314be30df96d7df543e87b761e4cdd'
+    source_url 'https://github.com/VSCodium/vscodium/releases/download/1.43.0/VSCodium-linux-x64-1.43.0.tar.gz'
+    source_sha256 '6ae5c32f0968d93570a7acf3262cb44fb2dd5dba6c6e6133bb1433daa3c23a8d'
     @arch = 'x64'
   end
 
@@ -31,7 +31,7 @@ class Codium < Package
   def self.postinstall
     puts
     puts 'Congratulations! You have installed VSCodium on Chrome OS!'.lightgreen
-    puts 'Now, please type \'codium\' to execute.'.lightgreen
+    puts 'Type \'codium\' to get started.'.lightgreen
     puts 'Happy coding!'.lightgreen
     puts
   end
