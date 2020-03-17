a9e7c07d9a75b776f6751891bfc8d494143716c4
Ed Reel <edreel@gmail.com>
2020-03-16 22:36:51 -0500

    Update xorg_server from 1.20.3 to 1.20.7 (#3888)
    Add pre-built binaries

--- packages/xorg_server.rb
+++ packages/xorg_server.rb
@@ -3,21 +3,21 @@ require 'package'
 class Xorg_server < Package
   description 'The Xorg Server is the core of the X Window system.'
   homepage 'https://www.x.org'
-  version '1.20.3'
-  source_url 'https://www.x.org/releases/individual/xserver/xorg-server-1.20.3.tar.bz2'
-  source_sha256 '1b3ce466c12cacbe2252b3ad5b0ed561972eef9d09e75900d65fb1e21f9201de'
+  version '1.20.7'
+  source_url 'https://www.x.org/releases/individual/xserver/xorg-server-1.20.7.tar.bz2'
+  source_sha256 'bd5986f010f34f5b3d6bc99fe395ecb1e0dead15a26807e0c832701809a06ea1'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.7-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.7-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.7-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_server-1.20.7-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '5603c3ad8211c77b9af761cb9ca78493b7a9a4f877197e619235e2892ffba8cc',
-     armv7l: '5603c3ad8211c77b9af761cb9ca78493b7a9a4f877197e619235e2892ffba8cc',
-       i686: 'cbec00ee50d01ccb2cdae94c2e62a254c6687a8f282ca7af24b625825dfb2958',
-     x86_64: 'daeac28c7d97ef2eaff50439cbb83299cf9df6c27a3ec816ffe55b2680b0053b',
+    aarch64: '311a12cae0c1f03dd40c3605168c0c8928d40fdd2bb6cf42e5d20a6c8a97b6c1',
+     armv7l: '311a12cae0c1f03dd40c3605168c0c8928d40fdd2bb6cf42e5d20a6c8a97b6c1',
+       i686: '83adbeae700a2f36a5d238094579dd08efb33cc26e63e05d7f60d417d0448810',
+     x86_64: '9ef9acf6f82c2815880438a323c1acf5625f1111963fba714c5c3d11697f4959',
   })
 
   depends_on 'pixman'
@@ -35,22 +35,22 @@ class Xorg_server < Package
   depends_on 'glproto'
 
   def self.build
-    system "./configure",
-             "--prefix=#{CREW_PREFIX}",
-             "--libdir=#{CREW_LIB_PREFIX}",
-             "--enable-xfree86-utils",
-             "--enable-xf86vidmode",
-             "--enable-glamor",
-             "--enable-xorg",
-             "--enable-xwayland",
-             "--enable-xvfb",
-             "--enable-xnest",
-             "--disable-systemd-logind"
-    system "make"
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           '--enable-xfree86-utils',
+           '--enable-xf86vidmode',
+           '--enable-glamor',
+           '--enable-xorg',
+           '--enable-xwayland',
+           '--enable-xvfb',
+           '--enable-xnest',
+           '--disable-systemd-logind'
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-    system "ln", "-sf", "Xwayland", "#{CREW_DEST_PREFIX}/bin/X"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    FileUtils.ln_sf 'Xwayland', "#{CREW_DEST_PREFIX}/bin/X"
   end
 end
