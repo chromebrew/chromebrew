a9e346144f7359c5c7ee3686677f7f63a33a26ba
Ed Reel <edreel@gmail.com>
2020-03-16 22:37:13 -0500

    Update qtsvg and qtx11extras packages (#3890)
    Add pre-built binaries

--- packages/qtsvg.rb
+++ packages/qtsvg.rb
@@ -3,21 +3,21 @@ require 'package'
 class Qtsvg < Package
   description 'Provides classes for displaying the contents of SVG files.'
   homepage 'https://www.qt.io/'
-  version '5.12.3'
+  version '5.12.3-1'
   source_url 'http://download.qt.io/official_releases/qt/5.12/5.12.3/submodules/qtsvg-everywhere-src-5.12.3.tar.xz'
   source_sha256 'f666438dbf6816b7534e539b95e3fa4405f11d7e2e2bbcde34f2db5ae0f27dc2'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qtsvg-5.12.3-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '8d7b4568bca5a401a53d177f4a32bf28cb8865028fe72569508015b02a356e6a',
-     armv7l: '8d7b4568bca5a401a53d177f4a32bf28cb8865028fe72569508015b02a356e6a',
-       i686: '980a79436c094a1e057d05e44d3e49ddea30af0917b29e9576bd340c4e699a04',
-     x86_64: 'a5ec7c11247ab0c42061049ef313994c4a4ece6ac2bb6bceb1298e12114d6a8b',
+    aarch64: 'c89fb9e2a3031c2375dfb0fe46ab0b25850cd7c54fbe8ac147fdbbe1c572930d',
+     armv7l: 'c89fb9e2a3031c2375dfb0fe46ab0b25850cd7c54fbe8ac147fdbbe1c572930d',
+       i686: '808c77d05d0ebdf489bb3c99dd6ac89c52349e05df101781ca7fd4d97aa02688',
+     x86_64: 'e775106f346d65f528a55fa473cd45c3862bbb903a54268f0d8a754ef6574f5a',
   })
 
   depends_on 'qtbase'
@@ -34,5 +34,6 @@ class Qtsvg < Package
     FileUtils.cp_r 'include', "#{CREW_DEST_PREFIX}/share/Qt-5"
     FileUtils.cp_r 'mkspecs', "#{CREW_DEST_PREFIX}/share/Qt-5"
     FileUtils.cp_r 'plugins', "#{CREW_DEST_PREFIX}/share/Qt-5"
+    FileUtils.cp_r 'src', "#{CREW_DEST_PREFIX}/share/Qt-5"
   end
 end
--- packages/qtx11extras.rb
+++ packages/qtx11extras.rb
@@ -3,21 +3,21 @@ require 'package'
 class Qtx11extras < Package
   description 'Provides classes for developing for the X11 platform.'
   homepage 'https://www.qt.io/'
-  version '5.12.3'
+  version '5.12.3-1'
   source_url 'http://download.qt.io/official_releases/qt/5.12/5.12.3/submodules/qtx11extras-everywhere-src-5.12.3.tar.xz'
   source_sha256 '85e3ae5177970c2d8656226d7535d0dff5764c100e55a79a59161d80754ba613'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qtx11extras-5.12.3-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'f88300f9d8d06dd02a44570832881dcc79e40a41e41a3e418122512eacce6f99',
-     armv7l: 'f88300f9d8d06dd02a44570832881dcc79e40a41e41a3e418122512eacce6f99',
-       i686: 'a2151e5240dafae30e73e7b2998b7b4c216478d780e4534540f48f817f5ee3a4',
-     x86_64: 'd0f65e142afaad622feb7fc8f25c04f3aed698677634115106ad4be3a3f75671',
+    aarch64: '730db647ee53c29484da0ee0bfdf9ffd5f239c5155dbe1664b9d776c9bffd55b',
+     armv7l: '730db647ee53c29484da0ee0bfdf9ffd5f239c5155dbe1664b9d776c9bffd55b',
+       i686: 'ad8cd860ab1b1846973e490b76bb015a8dd5ad13f874a9c8ae45f634db244011',
+     x86_64: '0eafd80bc09b178f93b0553352098a281ad90f88323121becb0b98842bc980a2',
   })
 
   depends_on 'qtbase'
@@ -32,5 +32,6 @@ class Qtx11extras < Package
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/Qt-5"
     FileUtils.cp_r 'include', "#{CREW_DEST_PREFIX}/share/Qt-5"
     FileUtils.cp_r 'mkspecs', "#{CREW_DEST_PREFIX}/share/Qt-5"
+    FileUtils.cp_r 'src', "#{CREW_DEST_PREFIX}/share/Qt-5"
   end
 end
