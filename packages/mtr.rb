73f0282c0b8187daa6024d2760107b3dc763fc74
Ed Reel <edreel@gmail.com>
2020-03-22 17:58:41 -0500

    Add postinstall section
    Reorganize build options
    
    Add pre-built binaries

--- packages/mtr.rb
+++ packages/mtr.rb
@@ -8,27 +8,35 @@ class Mtr < Package
   source_sha256 '229c673d637bd7dbb96471623785a47e85da0b1944978200c949994c1e6af10d'
 
   binary_url ({
-    aarch64: '',
-     armv7l: '',
-     x86_64: '',
-       i686: ''
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mtr-0.93-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mtr-0.93-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mtr-0.93-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mtr-0.93-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '',
-     armv7l: '',
-     x86_64: '',
-       i686: '',
+    aarch64: '10827000fd244c4a001077b013ed89d9ee156297764a59b763938e2e261d1965',
+     armv7l: '10827000fd244c4a001077b013ed89d9ee156297764a59b763938e2e261d1965',
+       i686: '6d42197636f5bc79bd63e879a4e50fa4d13c72e552d8393ac1893b2ae10bf226',
+     x86_64: '7ec1e4e20d17fa583089f1ce681c4dd373fdf9683eb36a7d14ede883a2fca0fc',
   })
 
-  depends_on 'ncurses'
-
   def self.build
-    system "./configure", "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}", "--without-gtk",
-        "LDFLAGS=-Wl,-rpath,#{CREW_LIB_PREFIX}"
-    system "make"
+    system './configure',
+           '--without-gtk',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           "LDFLAGS=-Wl,-rpath,#{CREW_LIB_PREFIX}"
+    system 'make'
   end
 
   def self.install
-    system "sudo", "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'sudo', 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system 'sudo', 'chown', '-R', "#{USER}:#{USER}", "#{CREW_DEST_DIR}"
+  end
+
+  def self.postinstall
+    puts
+    puts "To run, execute 'sudo mtr <domain>'".lightblue
+    puts
   end
 end
