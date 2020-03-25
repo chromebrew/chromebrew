fa874450ec8dc33910b94dc8b4137460602e91bb
Vincent GIRARD <vincent@heliosphere.fr>
2020-03-25 14:26:10 +0100

    [libxml2] Update to 2.9.9

--- packages/libxml2.rb
+++ packages/libxml2.rb
@@ -3,21 +3,13 @@ require 'package'
 class Libxml2 < Package
   description 'Libxml2 is the XML C parser and toolkit developed for the Gnome project.'
   homepage 'http://xmlsoft.org/'
-  version '2.9.8-2'
-  source_url 'https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.8/libxml2-v2.9.8.tar.bz2'
-  source_sha256 '75f60530c4ff9717930ea28abcf3dfc4b213ac7617884f37637d069b01032b8a'
+  version '2.9.9'
+  source_url 'https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.9/libxml2-v2.9.9.tar.bz2'
+  source_sha256 'd598e907b5f3efa992b65094f7113d9d8cc87238f32e4e1ddf8beff01b60a653'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.8-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.8-2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.8-2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.8-2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '51646006a23ab2adcf59c464c0358b4cde2c78707d25fc736cfcabb16e37bb27',
-     armv7l: '51646006a23ab2adcf59c464c0358b4cde2c78707d25fc736cfcabb16e37bb27',
-       i686: '11b78a2921229406fcff2e8421df0a58d27ec97c7bfcc89f908583f46dafd71c',
-     x86_64: '2b1ecd272734fd960d79d75187313e581911971466ad99f360e24a5fc0e19e6a',
   })
  
   depends_on 'zlibpkg'
