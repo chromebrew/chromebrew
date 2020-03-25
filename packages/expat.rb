701d2a65637ce3feb66abf2db03450a363851178
Vincent GIRARD <vincent@heliosphere.fr>
2020-03-25 21:46:17 +0100

    [expat] Update to 2.2.9

--- packages/expat.rb
+++ packages/expat.rb
@@ -3,21 +3,13 @@ require 'package'
 class Expat < Package
   description 'James Clark\'s Expat XML parser library in C.'
   homepage 'https://sourceforge.net/projects/expat/'
-  version '2.2.6'
-  source_url 'https://prdownloads.sourceforge.net/project/expat/expat/2.2.6/expat-2.2.6.tar.bz2'
-  source_sha256 '17b43c2716d521369f82fc2dc70f359860e90fa440bea65b3b85f0b246ea81f2'
+  version '2.2.9'
+  source_url 'https://prdownloads.sourceforge.net/project/expat/expat/2.2.9/expat-2.2.9.tar.bz2'
+  source_sha256 'f1063084dc4302a427dabcca499c8312b3a32a29b7d2506653ecc8f950a9a237'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.6-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.6-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.6-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/expat-2.2.6-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '419210356fe54f15bf2e8c51e436ccffe2e240b6cfb44a12b9c999561611e418',
-     armv7l: '419210356fe54f15bf2e8c51e436ccffe2e240b6cfb44a12b9c999561611e418',
-       i686: 'bb902406002d8cee6c803dd18a567658d02e60c1fbb0dfa94a5d31a076fe2348',
-     x86_64: '9e37b6a5b6ec892b3da12fbd984685e375b3641ed53074da98bd43a7faf75161',
   })
 
   def self.build
