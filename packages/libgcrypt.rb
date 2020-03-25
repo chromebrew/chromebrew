5c459b7b2d31072e88039e8822c4012be50de4e5
Ed Reel <edreel@gmail.com>
2020-03-25 10:11:43 -0500

    Add pre-built binaries for packages d-x

--- packages/libgcrypt.rb
+++ packages/libgcrypt.rb
@@ -8,8 +8,16 @@ class Libgcrypt < Package
   source_sha256 '3b4a2a94cb637eff5bdebbcaf46f4d95c4f25206f459809339cdada0eb577ac3'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libgcrypt-1.8.5-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libgcrypt-1.8.5-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libgcrypt-1.8.5-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libgcrypt-1.8.5-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '94018f0c7542235ef7fba73a4fbcd67e415f1c5f9cc6116fa2075638c472448c',
+     armv7l: '94018f0c7542235ef7fba73a4fbcd67e415f1c5f9cc6116fa2075638c472448c',
+       i686: '7c7a857f964a2af40a45ae63d1241e40f03bbf917e609febd222e095db22c80e',
+     x86_64: '6c0cbecc94b4cd9615844e45733a0ef92f197ec6ab7a41d1733a19d168db3a3f',
   })
 
   depends_on 'libgpgerror'
--- packages/libxml2_python.rb
+++ packages/libxml2_python.rb
@@ -8,8 +8,16 @@ class Libxml2_python < Package
   source_sha256 '94fb70890143e3c6549f265cee93ec064c80a84c42ad0f23e85ee1fd6540a871'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '05b9616edf2809cea4bfaa2a0f9b21e8455871e04f9a76f08c5472e25ab655c6',
+     armv7l: '05b9616edf2809cea4bfaa2a0f9b21e8455871e04f9a76f08c5472e25ab655c6',
+       i686: '9676158d3071ecd96bd3fc3cb0619dd137efc39b23a8c1d0c4d38990d58223ad',
+     x86_64: '54261947d04449bdeb3af3fd9c60b450fc8237defdee0076d23122786eb05934',
   })
 
   depends_on 'python27'
