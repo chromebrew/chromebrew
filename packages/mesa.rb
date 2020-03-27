e59fef408a11c50461b868f2aa44d3c57372bcd6
Vincent GIRARD <vincent@heliosphere.fr>
2020-03-27 14:25:27 +0100

    [mesa] Remove comments

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -26,17 +26,6 @@ class Mesa < Package
     system "pip3 uninstall -y Mako MarkupSafe || :"
     system "pip3 install --prefix \"#{CREW_PREFIX}\" --root \"#{CREW_DEST_DIR}\" Mako==1.0.7"
     system "pip3 install --prefix \"#{CREW_PREFIX}\" Mako==1.0.7"
-#    case ARCH
-#    when 'i686'
-#      gallium_drivers = 'i915,nouveau,pl111,svga,swrast,vc4,virgl'
-#      dri_drivers = 'i915,i965,nouveau,radeon,r200,swrast'
-#    when 'x86_64'
-#      gallium_drivers = 'i915,nouveau,r300,r600,radeonsi,pl111,svga,swrast,swr,vc4,virgl'
-#      dri_drivers = 'i915,i965,nouveau,radeon,r200,swrast'
-#    when 'aarch64', 'armv7l'
-#      gallium_drivers = 'nouveau,r300,freedreno,pl111,swrast,tegra,vc4,virgl'
-#      dri_drivers = 'nouveau,radeon,r200,swrast'
-#    end
     system "meson",
       "--prefix=#{CREW_PREFIX}",
       "--libdir=#{CREW_LIB_PREFIX}",
@@ -56,32 +45,10 @@ class Mesa < Package
       "-Dglx=auto",
       "-Dllvm=true",
       "builddir"
-#    system "./configure",
-#           "--prefix=#{CREW_PREFIX}",
-#           "--libdir=#{CREW_LIB_PREFIX}",
-#           "--enable-shared-glapi",
-#           "--with-gallium-drivers=#{gallium_drivers}",
-#           "--with-dri-drivers=#{dri_drivers}",
-#           "--enable-osmesa",
-#           "--enable-opengl",
-#           "--enable-egl",
-#           "--enable-xvmc",
-#           "--disable-asm",
-#           "--enable-nine",
-#           "--enable-gles1",
-#           "--enable-gles2",
-#           "--with-platforms=x11,drm,wayland",
-#           "--enable-gbm",
-#           "--enable-xa",
-#           "--enable-glx",
-#           "--enable-dri",
-#           "--enable-llvm"
-#    system "make"
     system "ninja -C builddir"
   end
 
   def self.install
-#    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
     system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
 
   end
