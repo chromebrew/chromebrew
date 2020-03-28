af77dc71bb84e0432f3a24e522366a42d27c067e
Vincent GIRARD <vincent@heliosphere.fr>
2020-03-28 03:04:48 +0100

    [mesa_utils] Set a more stable url

--- packages/mesa_utils.rb
+++ packages/mesa_utils.rb
@@ -4,8 +4,8 @@ class Mesa_utils< Package
   description 'Open-source implementation of the OpenGL specification - Provides tools such as glxinfo, glxgears'
   homepage 'https://www.mesa3d.org'
   version '8.4.0'
-  source_url 'ftp://ftp.freedesktop.org/pub/mesa/demos/mesa-demos-8.4.0.tar.bz2'
-  source_sha256 '01e99c94a0184e63e796728af89bfac559795fb2a0d6f506fa900455ca5fff7d'
+  source_url 'https://gitlab.freedesktop.org/mesa/demos/-/archive/mesa-demos-8.4.0/demos-mesa-demos-8.4.0.tar.bz2'
+  source_sha256 '84338ce936fa110232a62bcd36c7c2c8710c4a280e9a81c4d10d06f6c2506b7d'
 
   binary_url ({
   })
@@ -16,6 +16,7 @@ class Mesa_utils< Package
   depends_on 'glew'
 
   def self.build
+    system "./autogen.sh"
     system "./configure",
       "--prefix=#{CREW_PREFIX}",
       "--libdir=#{CREW_LIB_PREFIX}"
