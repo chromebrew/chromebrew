134deabce5bfc7f7e0e27ae6fc7031ae85f6ffd5
vincowl <vince512@free.fr>
2020-03-28 07:15:14 +0100

    [libxkbfile] Update to 1.1.0 (#3964)
    Co-authored-by: Vincent GIRARD <vincent@heliosphere.fr>

--- packages/libxkbfile.rb
+++ packages/libxkbfile.rb
@@ -3,21 +3,13 @@ require 'package'
 class Libxkbfile < Package
   description 'library for the X window system'
   homepage 'https://x.org'
-  version '1.0.9-0'
-  source_url 'https://www.x.org/archive/individual/lib/libxkbfile-1.0.9.tar.gz'
-  source_sha256 '95df50570f38e720fb79976f603761ae6eff761613eb56f258c3cb6bab4fd5e3'
+  version '1.1.0'
+  source_url 'https://www.x.org/archive/individual/lib/libxkbfile-1.1.0.tar.bz2'
+  source_sha256 '758dbdaa20add2db4902df0b1b7c936564b7376c02a0acd1f2a331bd334b38c7'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libxkbfile-1.0.9-0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libxkbfile-1.0.9-0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libxkbfile-1.0.9-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libxkbfile-1.0.9-0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '0da685fe99dcbbe5f69daa717ea1938d6bf6fab3149cbc50cefbd14e68e3963a',
-     armv7l: '0da685fe99dcbbe5f69daa717ea1938d6bf6fab3149cbc50cefbd14e68e3963a',
-       i686: '7be642dffe2807fc53b4b3b1dde506251ad42a2d24c94b3271db58c61a607711',
-     x86_64: '4fd10398c3b8d444dc540aa574e437401597f442f8e2837f2a2eacabc08d83ba',
   })
 
   def self.build
