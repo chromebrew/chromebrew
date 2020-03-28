856d58585b0622b896e2851114acc029b21a28ea
vincowl <vince512@free.fr>
2020-03-28 18:27:38 +0100

    [xkeyboard_config] Update to 2.29 (#3957)
    * [xkeyboard_config] Update to 2.29
    
    * [xkeyboard_config] Modify sed command
    
    Co-authored-by: Vincent GIRARD <vincent@heliosphere.fr>

--- packages/xkeyboard_config.rb
+++ packages/xkeyboard_config.rb
@@ -4,21 +4,13 @@ require 'package'
 class Xkeyboard_config < Package
   description 'The non-arch keyboard configuration database for X Window.'
   homepage 'http://www.freedesktop.org/wiki/Software/XKeyboardConfig'
-  version '2.21'
-  source_url 'https://www.x.org/releases/individual/data/xkeyboard-config/xkeyboard-config-2.21.tar.bz2'
-  source_sha256 '30c17049fae129fc14875656da9aa3099e3031d6ce0ee1d77aae190fd9edcec5'
+  version '2.29'
+  source_url 'https://www.x.org/releases/individual/data/xkeyboard-config/xkeyboard-config-2.29.tar.bz2'
+  source_sha256 '1d4175278bf06000683656763a8b1d3282c61a314b6db41260c8efe92d621802'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xkeyboard_config-2.21-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xkeyboard_config-2.21-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xkeyboard_config-2.21-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xkeyboard_config-2.21-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'f60a526d9c26835976d5186115b2bd340772d3e9efd7beae47424703c48da974',
-     armv7l: 'f60a526d9c26835976d5186115b2bd340772d3e9efd7beae47424703c48da974',
-       i686: '75204643000e91af66ab288e1e7740193d7c1d7ced2a9d8a9f3164a428664242',
-     x86_64: '8bdf41c09086fb7209ebb87072b5ef8268176f5051e99f60aa0375046585f776',
   })
 
   depends_on "util_macros" => :build
@@ -27,6 +19,7 @@ class Xkeyboard_config < Package
   depends_on "gettext" => :build
 
   def self.build
+    system "sed -i 's,/usr/bin/python3,#{CREW_PREFIX}/bin/python3,' ./rules/compat/map-variants.py"i
     system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
     system "make"
   end
