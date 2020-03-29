4eb7e5ef6f294151d705b7443d49287ef3e987f2
Ed Reel <edreel@gmail.com>
2020-03-29 17:11:53 -0500

    Add pre-built binaries for bison

--- packages/bison.rb
+++ packages/bison.rb
@@ -8,8 +8,16 @@ class Bison < Package
   source_sha256 '2bf85b5f88a5f2fa8069aed2a2dfc3a9f8d15a97e59c713e3906e5fdd982a7c4'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '1ff70ed7db74cadbb20d0e17a6d631bd256caf3549c0932c347eba3edf1f2a53',
+     armv7l: '1ff70ed7db74cadbb20d0e17a6d631bd256caf3549c0932c347eba3edf1f2a53',
+       i686: '091fc0b760316ca632cce4240b1fa910c1528a953d1328d5edb5e05239a8e875',
+     x86_64: '1e810c55fec98830af9089b773150efdc5a1d9a9380bf885b4ae6bf8261111d9',
   })
 
   def self.build
