43df9896467af38a972d42fc2dc1b4f454679fec
daemonspudguy <jacob.a.tice@gmail.com>
2020-04-09 17:09:09 -0400

    Update freebasic

--- packages/freebasic.rb
+++ packages/freebasic.rb
@@ -3,24 +3,15 @@ require 'package'
 class Freebasic < Package
   description 'FreeBASIC is a free/open source (GPL), BASIC compiler for Microsoft Windows, DOS and Linux.'
   homepage 'https://www.freebasic.net/'
-  version '1.05.0-2'
+  version '1.07.1'
   case ARCH
   when 'i686'
-    source_url 'https://downloads.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.05.0-linux-x86.tar.gz'
-    source_sha256 'df20d3d11637d6f6173017dadf666d673133087189f8ecdb9756136f175e91b9'
+    source_url 'https://downloads.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.07.1-linux-x86.tar.gz'
+    source_sha256 'a5b8f31e38a214d63a3d8e0b1ac6608f6a304891e40a3c0755940cba2f8401f1'
   when 'x86_64'
-    source_url 'https://downloads.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.05.0-linux-x86_64.tar.gz'
-    source_sha256 '26d184061e2a55c7ee9c12213ac4bf062556ecec1bacf84037233a41aef6c74f'
+    source_url 'https://downloads.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.07.1-linux-x86_64.tar.gz'
+    source_sha256 'd5034e81201760de7fd7bc563d8c33213a9530da3d15d4907e52d47278a00243'
   end
-
-  binary_url ({
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/freebasic-1.05.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/freebasic-1.05.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-       i686: '78d66a743264f37f605eaaf83fcbe612b52588928b200d89fe9227430dbf2dd8',
-     x86_64: '3d289b59687e4a0fdba3272b290b3b638f1f2cf625ef14873e4e33e2acdd12aa',
-  })
 
   depends_on 'libtinfo'
 
