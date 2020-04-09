d63b3cf6ceee6d4f8e42440b1d4f7724a2e3d687
Ed Reel <edreel@gmail.com>
2020-04-09 10:34:11 -0500

    Update glew from 2.1.0 to 2.1.0-1
    Add pre-built binaries

--- packages/glew.rb
+++ packages/glew.rb
@@ -3,21 +3,21 @@ require 'package'
 class Glew < Package
   description 'GLEW provides efficient run-time mechanisms for determining which OpenGL extensions are supported on the target platform'
   homepage 'http://glew.sourceforge.net/'
-  version '2.1.0'
+  version '2.1.0-1'
   source_url 'https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0.tgz'
   source_sha256 '04de91e7e6763039bc11940095cd9c7f880baba82196a7765f727ac05a993c95'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glew-2.1.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '66d50f1b19c6a53fc3b30940df863e4471d9ee641310eef1965f69748d54fa98',
-     armv7l: '66d50f1b19c6a53fc3b30940df863e4471d9ee641310eef1965f69748d54fa98',
-       i686: '6cdeed1dbca32d7ca46cda937c0d5487ad2d5c2ef96916339093f838e0d0ba9e',
-     x86_64: '021f5b6b7669a7719e5b46716ce3aac75c4fd1922ec4d2f112a318867665e5c8',
+    aarch64: 'd80923b6a32ec1b38df81dcd7860516d140c57157e3dea23a37d8f874c1cbbe3',
+     armv7l: 'd80923b6a32ec1b38df81dcd7860516d140c57157e3dea23a37d8f874c1cbbe3',
+       i686: '9dbd934bdf44c5109f9e52931801c2c0fb0b7806f146d4ead90a129bb31ebac1',
+     x86_64: '0496b31639402805881fa3312aaf8bea3b4239734b38935bbc53a10d48bcc840',
   })
 
   depends_on 'mesa'
@@ -34,7 +34,7 @@ class Glew < Package
 
   def self.install
     Dir.chdir 'build' do
-      system 'make', "GLEW_DEST=#{CREW_DEST_DIR}", 'install'
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     end
   end
 end
