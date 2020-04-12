51a472712c3b93e7cd5df8d119b2c01ec41522ef
Ed Reel <edreel@gmail.com>
2020-04-12 10:24:54 -0500

    Update firefox from 73.0.1 to 75.0 (#4003)

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,14 +3,14 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '73.0.1'
+  version '75.0'
   case ARCH
   when 'i686'
-    source_url 'https://archive.mozilla.org/pub/firefox/releases/73.0.1/linux-i686/en-US/firefox-73.0.1.tar.bz2'
-    source_sha256 '0cf4ed94739979f5c7fce0a94152911079722cc0118edab9a8f6a573bdf06c3a'
+    source_url 'https://archive.mozilla.org/pub/firefox/releases/75.0/linux-i686/en-US/firefox-75.0.tar.bz2'
+    source_sha256 '1ea6efedd36293239a66a91c502ef7b24cccb8fdbde1f72763706f07f99fb057'
   when 'x86_64'
-    source_url 'https://archive.mozilla.org/pub/firefox/releases/73.0.1/linux-x86_64/en-US/firefox-73.0.1.tar.bz2'
-    source_sha256 'd5a2c93844763b2e7f7f555eab239b71442cd87205c40a8ad287c38208a2a513'
+    source_url 'https://archive.mozilla.org/pub/firefox/releases/75.0/linux-x86_64/en-US/firefox-75.0.tar.bz2'
+    source_sha256 'c5d9700381c4ad1bde4282735593d5726d9869e3db69ac2e0c24e40597ca4aa6'
   end
 
   depends_on 'gtk3'
@@ -18,13 +18,13 @@ class Firefox < Package
 
   def self.build
     system "echo '#!/bin/bash' > firefox.sh"
-    system "echo 'cd #{CREW_PREFIX}/share/firefox' >> firefox.sh"
+    system "echo 'cd #{CREW_PREFIX}/firefox' >> firefox.sh"
     system "echo './firefox \"\$@\"' >> firefox.sh"
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/firefox"
-    FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/share/firefox"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/firefox"
+    FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/firefox"
     system "install -Dm755 firefox.sh #{CREW_DEST_PREFIX}/bin/firefox"
   end
 end
