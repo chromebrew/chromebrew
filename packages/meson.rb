0e213a5a7bb54c3e1ec6ba751d832fdd0e37787c
Ed Reel <edreel@gmail.com>
2020-04-17 08:49:03 -0700

    Update meson from 0.53.0 to 0.54.0 and ninja from 1.9.0 to 1.10.0 (#4010)
    Add setuptools dependency to meson
    
    Add pre-built binaries

--- packages/meson.rb
+++ packages/meson.rb
@@ -3,24 +3,25 @@ require 'package'
 class Meson < Package
   description 'The Meson Build System'
   homepage 'http://mesonbuild.com/'
-  version '0.53.0'
-  source_url 'https://github.com/mesonbuild/meson/releases/download/0.53.0/meson-0.53.0.tar.gz'
-  source_sha256 '035e75993ab6fa6c9ebf902b835c64cf397a763eb8e65c9bb6e1cc9730a9d3f6'
+  version '0.54.0'
+  source_url 'https://github.com/mesonbuild/meson/releases/download/0.54.0/meson-0.54.0.tar.gz'
+  source_sha256 'dde5726d778112acbd4a67bb3633ab2ee75d33d1e879a6283a7b4a44c3363c27'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.53.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.54.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.54.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.54.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.54.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'cce1f631fe48be9a287ffdfe18fb0c841e880af98d5c486c07dee5dbf4722182',
-     armv7l: 'cce1f631fe48be9a287ffdfe18fb0c841e880af98d5c486c07dee5dbf4722182',
-       i686: '5dad566b1bd6d142c78b53cecf4c06068c2b6c0a1901ec7aeb6bff55b11a84e4',
-     x86_64: '5246ffe7d4b2b2901a2c9dd21c6516634314d247c8a2dc09d3a0a3c516485923',
+    aarch64: 'e7e4adb627f7496ae4a302f112f25ef3485adbeae59c8fc1e518ff6f4580c96c',
+     armv7l: 'e7e4adb627f7496ae4a302f112f25ef3485adbeae59c8fc1e518ff6f4580c96c',
+       i686: '1a2afdd2aff04d1aff1130ec3f96c0ae41d06c1567d01cd73cc19b749e8cf4ec',
+     x86_64: 'c78da3c1d4d5ea1704f05079bb405fa113ac610ee59ffd87f585601e4113068f',
   })
 
   depends_on 'ninja'
+  depends_on 'setuptools'
 
   def self.install
     system "python3 setup.py install --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
--- packages/ninja.rb
+++ packages/ninja.rb
@@ -3,33 +3,28 @@ require 'package'
 class Ninja < Package
   description 'a small build system with a focus on speed'
   homepage 'https://ninja-build.org'
-  version '1.9.0'
-  source_url 'https://github.com/ninja-build/ninja/archive/v1.9.0.tar.gz'
-  source_sha256 '5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9'
+  version '1.10.0'
+  source_url 'https://github.com/ninja-build/ninja/archive/v1.10.0.tar.gz'
+  source_sha256 '3810318b08489435f8efc19c05525e80a993af5a55baa0dfeae0465a9d45f99f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.9.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.9.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.9.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.9.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.10.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.10.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.10.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ninja-1.10.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'b9f89afdefe5747802c7ccd5184775c2a18f8f66f380cf68213c3c86833f0822',
-     armv7l: 'b9f89afdefe5747802c7ccd5184775c2a18f8f66f380cf68213c3c86833f0822',
-       i686: 'c1a6a31fec8fd4185ab6c7e478f7cc935c3f836c7e399f2a74725946e9d23d98',
-     x86_64: 'a1b8db9d11211ece355c2d4d7a1fecffb719d0d47b1c5c48276a2cb22f505f62',
+    aarch64: '5e2ef70ad213e86a4a32e48bb4783face0bff6f3b079080d3d668bb35ba83088',
+     armv7l: '5e2ef70ad213e86a4a32e48bb4783face0bff6f3b079080d3d668bb35ba83088',
+       i686: 'ec2996217bdf7c57d0528a8ba0eefabda15c17e59f958daebc3fe1eda6a8a9bc',
+     x86_64: 'e74f2b1d374edd0ff0877939b3afb8fa80240774bf3c9126278e5a965c7a5d0b',
   })
 
-  depends_on 'python3'
-  depends_on 're2c'
-  depends_on 'unzip'
-
   def self.build
-    system "python3 configure.py --bootstrap"
+    system 'python3 configure.py --bootstrap'
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
-    system "cp ninja #{CREW_DEST_PREFIX}/bin/"
+    system "install -Dm755 ninja #{CREW_DEST_PREFIX}/bin/ninja"
   end
 end
