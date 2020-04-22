bfcaa92c944be58adcda38cc7bd8062d2c423e67
Ed Reel <edreel@gmail.com>
2020-04-21 17:41:14 -0700

    Update freetype packages from 2.10 to 2.10.1 (#4028)
    Install docwriter
    
    Add pre-built binaries

--- packages/freetype.rb
+++ packages/freetype.rb
@@ -3,21 +3,21 @@ require 'package'
 class Freetype < Package
   description 'FreeType is a freely available software library to render fonts.'
   homepage 'https://www.freetype.org/'
-  version '2.10'
-  source_url 'https://namesdir.com/mirrors/nongnu/freetype/freetype-2.10.0.tar.gz'
-  source_sha256 '955e17244e9b38adb0c98df66abb50467312e6bb70eac07e49ce6bd1a20e809a'
+  version '2.10.1'
+  source_url 'https://download.savannah.gnu.org/releases/freetype/freetype-2.10.1.tar.xz'
+  source_sha256 '16dbfa488a21fe827dc27eaf708f42f7aa3bb997d745d31a19781628c36ba26f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/freetype-2.10.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '3cdfc1eae6ee52f0d3c046e2a3bc01d8383d830ea48cb2f84db94caced32b2a9',
-     armv7l: '3cdfc1eae6ee52f0d3c046e2a3bc01d8383d830ea48cb2f84db94caced32b2a9',
-       i686: 'b3a195210ba9bc95a863956c69218b99bdcac6a276b27648ff043d5e7bf64b39',
-     x86_64: '0c761037a91633e1371974ced873e59c1f847ead4416f338119761be1bbe21c7',
+    aarch64: '227d47ee4d7b5cb50cbe4be8cec8be7125ebd28d96e649972aa8ba2e947a5fa2',
+     armv7l: '227d47ee4d7b5cb50cbe4be8cec8be7125ebd28d96e649972aa8ba2e947a5fa2',
+       i686: 'de195bd3d95afa2ba8e0ef7bf137d957037d41b356ee482f6b270e6cf0780780',
+     x86_64: 'ccadfa7f9242639930dd5ae0fd9ac1609901ffefb33842c4dbd63f729f853a74',
   })
 
   depends_on 'expat'
@@ -26,16 +26,15 @@ class Freetype < Package
   depends_on 'harfbuzz'
 
   def self.build
-	system "sed -i 's,/usr/include/freetype2,#{CREW_PREFIX}/include/freetype2,g' configure"
-    system "./configure \
-            CFLAGS=\" -fPIC\" \
-            --prefix=#{CREW_PREFIX} \
-            --libdir=#{CREW_LIB_PREFIX} \
-            --with-harfbuzz"
-    system "make"
+    system 'pip3 install docwriter'
+    system "sed -i 's,/usr/include/freetype2,#{CREW_PREFIX}/include/freetype2,g' configure"
+    system "./configure CFLAGS=' -fPIC' #{CREW_OPTIONS} --enable-freetype-config --with-harfbuzz"
+    system 'make'
+    system 'pip3 uninstall docwriter -y'
+    system "pip3 install docwriter --root #{CREW_DEST_DIR} --prefix #{CREW_PREFIX}"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
