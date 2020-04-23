58857835e191b31def3977518ecd691fcaa8abee
Ed Reel <edreel@gmail.com>
2020-04-22 23:35:37 -0500

    Update curl from 7.67.0 to 7.69.1
    Add pre-built binaries

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,21 +3,21 @@ require 'package'
 class Curl < Package
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.haxx.se/'
-  version '7.67.0'
-  source_url 'https://curl.haxx.se/download/curl-7.67.0.tar.xz'
-  source_sha256 'f5d2e7320379338c3952dcc7566a140abb49edb575f9f99272455785c40e536c'
+  version '7.69.1'
+  source_url 'https://curl.haxx.se/download/curl-7.69.1.tar.xz'
+  source_sha256 '03c7d5e6697f7b7e40ada1b2256e565a555657398e6c1fcfa4cb251ccd819d4f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.67.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.69.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.69.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.69.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.69.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '5ae1098c3ab701d280fcef2266fd55d89c7c2b258a2a54141ad01abdaa394f54',
-     armv7l: '5ae1098c3ab701d280fcef2266fd55d89c7c2b258a2a54141ad01abdaa394f54',
-       i686: '497a670dbe39877b48916f3545d6a75b855624635e6c93806bd47c9280d3e490',
-     x86_64: '1c12008aa3b2c72084b2bae2c8e4c3246dc8358e25222c159ddf24ec0776ae5c',
+    aarch64: '991776840afbdf259a7d0e2c04669cfdbc6732f99c1f967541ede6be8d4b3623',
+     armv7l: '991776840afbdf259a7d0e2c04669cfdbc6732f99c1f967541ede6be8d4b3623',
+       i686: '62ed095da0b4344ef7e6d73d474d76899a5d8c3c62b8b337159d2707e5b494b3',
+     x86_64: 'ad171822283ad474aa1235ce385541c66e08f299441d6e0d718b2272c5e56cbf',
   })
 
   depends_on 'groff' => :build
@@ -30,6 +30,7 @@ class Curl < Package
 
   def self.build
     ENV['LIBRARY_PATH'] = CREW_LIB_PREFIX
+    ENV['TMPDIR'] = "#{CREW_PREFIX}/tmp"
     system './configure',
       "--prefix=#{CREW_PREFIX}",
       "--libdir=#{CREW_LIB_PREFIX}",
