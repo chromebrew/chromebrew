1bda0d56e49a78fd00037945ea65b9aa17ea9910
Ed Reel <edreel@gmail.com>
2020-04-28 20:35:09 -0500

    Update ruby from 2.5.3 to 2.7.1
    Add pre-built binaries
    
    Add libressl to install.sh
    
    Update install.sh with pre-built binaries

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -3,21 +3,21 @@ require 'package'
 class Ruby < Package
   description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
   homepage 'https://www.ruby-lang.org/en/'
-  version '2.5.3'
-  source_url 'https://cache.ruby-lang.org/pub/ruby/2.5/ruby-2.5.3.tar.gz'
-  source_sha256 '9828d03852c37c20fa333a0264f2490f07338576734d910ee3fd538c9520846c'
+  version '2.7.1'
+  source_url 'https://cache.ruby-lang.org/pub/ruby/2.7/ruby-2.7.1.tar.xz'
+  source_sha256 'b224f9844646cc92765df8288a46838511c1cec5b550d8874bd4686a904fcee7'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.5.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.5.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.5.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.5.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.7.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.7.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.7.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ruby-2.7.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '5e485a0320b298e1f5c4ff50d98c6fe6d06ad9a38d9119d580a8b469418e1e6a',
-     armv7l: '5e485a0320b298e1f5c4ff50d98c6fe6d06ad9a38d9119d580a8b469418e1e6a',
-       i686: '6f4a5b96c31ef5ee4f09ac15da4c7a4a9d838ed5233038136ead1e155d17f342',
-     x86_64: '352b78fc883cf8936136991fda9ca5d49e90b2951626158d6af8ef4b58d67f97',
+    aarch64: '8808b9bc9b55206781ea706008f405a12083af15839ff69d96434674004baee8',
+     armv7l: '8808b9bc9b55206781ea706008f405a12083af15839ff69d96434674004baee8',
+       i686: '5544e722fb76733df5fb3734db4bed818668b11fbb01f9484b1c85f78fd442c6',
+     x86_64: 'bcce410dc64861dcb57c7e72e5eedc3df79ca94d6e0d0ee32e0d14d1fd3712c0',
   })
 
   depends_on 'readline'
@@ -27,18 +27,15 @@ class Ruby < Package
   # at run-time, system's gmp, openssl, readline and zlibpkg are possible to use
 
   def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--enable-shared'
+    system "./configure #{CREW_OPTIONS} --enable-shared"
     system 'make'
-  end
-
-  def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
   end
 
   def self.check
     system "TMPDIR=#{CREW_PREFIX}/tmp make check"
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
