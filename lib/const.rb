a06aecf0cd1e49435894a9b59e397f5e2fefc395
Ed Reel <edreel@gmail.com>
2020-05-06 18:48:38 -0500

    Add CREW_MAN_PREFIX and CREW_DEST_MAN_PREFIX constants

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.3.6'
+CREW_VERSION = '1.3.7'
 
 ARCH = `uname -m`.strip
 ARCH_LIB = if ARCH == 'x86_64' then 'lib64' else 'lib' end
@@ -13,6 +13,7 @@ else
   @pkg.build_from_source = true
 end
 CREW_LIB_PREFIX = CREW_PREFIX + '/' + ARCH_LIB
+CREW_MAN_PREFIX = CREW_PREFIX + '/share/man'
 
 CREW_LIB_PATH = CREW_PREFIX + '/lib/crew/'
 CREW_CONFIG_PATH = CREW_PREFIX + '/etc/crew/'
@@ -20,6 +21,7 @@ CREW_BREW_DIR = CREW_PREFIX + '/tmp/crew/'
 CREW_DEST_DIR = CREW_BREW_DIR + 'dest'
 CREW_DEST_PREFIX = CREW_DEST_DIR + CREW_PREFIX
 CREW_DEST_LIB_PREFIX = CREW_DEST_DIR + CREW_LIB_PREFIX
+CREW_DEST_MAN_PREFIX = CREW_DEST_DIR + CREW_MAN_PREFIX
 
 if ENV['CREW_PREFIX'].to_s == ''
   HOME = ENV['HOME']
@@ -54,4 +56,4 @@ when 'i686'
 when 'x86_64'
   CREW_BUILD = 'x86_64-cros-linux-gnu'
 end
-CREW_OPTIONS = "--prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} --mandir=#{CREW_PREFIX}/share/man --build=#{CREW_BUILD} --host=#{CREW_BUILD} --target=#{CREW_BUILD}"
+CREW_OPTIONS = "--prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} --mandir=#{CREW_MAN_PREFIX} --build=#{CREW_BUILD} --host=#{CREW_BUILD} --target=#{CREW_BUILD}"
