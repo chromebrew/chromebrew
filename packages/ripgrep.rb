56edda9a708c2101003c14ca78d3f7284f0f62cf
Vincent GIRARD <vincent@heliosphere.fr>
2020-05-07 22:57:22 +0200

    [ripgrep] Update to 12.0.1

--- packages/ripgrep.rb
+++ packages/ripgrep.rb
@@ -3,21 +3,13 @@ require 'package'
 class Ripgrep < Package
   description 'ripgrep recursively searches directories for a regex pattern'
   homepage 'https://github.com/BurntSushi/ripgrep'
-  version '0.8.1'
-  source_url 'https://github.com/BurntSushi/ripgrep/archive/0.8.1.tar.gz'
-  source_sha256 '7035379fce0c1e32552e8ee528b92c3d01b8d3935ea31d26c51a73287be74bb3'
+  version '12.0.1'
+  source_url 'https://github.com/BurntSushi/ripgrep/archive/12.0.1.tar.gz'
+  source_sha256 '5be34aa77a36ac9d8f1297a0d97069e4653e03f61c67d192cee32944cd2b6329'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ripgrep-0.8.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ripgrep-0.8.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ripgrep-0.8.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ripgrep-0.8.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'b7722213294547ddb9b404fc6af87744a8e3bca8418d104286ea537656d53453',
-     armv7l: 'b7722213294547ddb9b404fc6af87744a8e3bca8418d104286ea537656d53453',
-       i686: '772e4c4e1fa7352c813f2a032110ff4a54a311224b81e8003eaf407627ed81a9',
-     x86_64: 'b1a981fa69a4622adb75167173afeed506b95ac685f8ea6dd82718b33547ef70',
   })
 
   depends_on 'rust'
