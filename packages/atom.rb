2e08c53d1265538f7330736122557adcf7d2debf
Ed Reel <edreel@gmail.com>
2020-05-07 12:02:24 -0500

    Update atom from 1.43.0 to 1.45.0 (#4049)

--- packages/atom.rb
+++ packages/atom.rb
@@ -3,7 +3,7 @@ require 'package'
 class Atom < Package
   description 'The hackable text editor'
   homepage 'https://atom.io/'
-  version '1.43.0'
+  version '1.45.0'
   case ARCH
   when 'x86_64'
     source_url 'file:///dev/null'
@@ -11,10 +11,10 @@ class Atom < Package
   end
 
   binary_url ({
-    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/atom-1.43.0-chromeos-x86_64.tar.xz',
+    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/atom-1.45.0-chromeos-x86_64.tar.xz'
   })
   binary_sha256 ({
-    x86_64: '435736336fa7d108a318b11ebe92fc7266abf0b14ee59fd8d2890a722483cb01',
+    x86_64: 'f8f6bbc3420cea88be0021f0b6e1630a9aec4a4d9a802ac069cbce67f04aca6a'
   })
 
   depends_on 'alien' => :build
@@ -25,7 +25,7 @@ class Atom < Package
 
   def self.build
     system "wget https://github.com/atom/atom/releases/download/v#{version}/atom-amd64.deb"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('atom-amd64.deb') ) == 'ce3af452301b6ad45ee0abf340e497f8778f6fae114be9e455a305045ab3a037'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('atom-amd64.deb') ) == '04a80193cd3ded01c3a89d0e7b0a030b4f5f5c5e2f461a327e006ea8daf3f5e1'
     system "alien -tc atom-amd64.deb"
     system "tar xvf atom-#{version}.tgz"
   end
