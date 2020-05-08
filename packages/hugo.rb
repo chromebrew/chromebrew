ff5d9fcdda9f38e7e3627abaa7273a538ad4eca4
Vincent GIRARD <vincent@heliosphere.fr>
2020-05-08 02:19:10 +0200

    [hugo] Update to 0.70.0

--- packages/hugo.rb
+++ packages/hugo.rb
@@ -3,18 +3,18 @@ require 'package'
 class Hugo < Package
   description 'Hugo is one of the most popular open-source static site generators.'
   homepage 'https://gohugo.io'
-  version '0.69.0'
+  version '0.70.0'
 
   case ARCH
   when 'aarch64','armv7l'
-    source_url 'https://github.com/gohugoio/hugo/releases/download/v0.69.0/hugo_0.69.0_Linux-ARM.tar.gz'
-    source_sha256 '3c8b5159f2072bb44ac8fb8f5f75d7e9d8fad26aea079c81da3b026f94fb5266'
+    source_url 'https://github.com/gohugoio/hugo/releases/download/v0.70.0/hugo_0.70.0_Linux-ARM.tar.gz'
+    source_sha256 '7891baefe1845ea8ff03ca2f0589305b526a60f4ee4acbf556f4c180d8e8859f'
   when 'i686'
-    source_url 'https://github.com/gohugoio/hugo/releases/download/v0.69.0/hugo_0.69.0_Linux-32bit.tar.gz'
-    source_sha256 '31dd00e1e35944e3ba7a019b453771dd1e0c389773aea00c48a83aa1d59bf866'
+    source_url 'https://github.com/gohugoio/hugo/releases/download/v0.70.0/hugo_0.70.0_Linux-32bit.tar.gz'
+    source_sha256 '5c7c530ab9be86ca90a3f5d5a583ac55d7b2e7a59471188c0eb58113e1476ffd'
   when 'x86_64'
-    source_url 'https://github.com/gohugoio/hugo/releases/download/v0.69.0/hugo_0.69.0_Linux-64bit.tar.gz'
-    source_sha256 '4a11fbce51cfabaa26a244d75e84ff16c7f9fd47c315e6f11f3f39612d82ba46'
+    source_url 'https://github.com/gohugoio/hugo/releases/download/v0.70.0/hugo_0.70.0_Linux-64bit.tar.gz'
+    source_sha256 'd0e2e1f8d2f69c1b378e34daaf9f91997cf777d3151da4d8aa6bd7b5703f9446'
   end
 
   binary_url ({
