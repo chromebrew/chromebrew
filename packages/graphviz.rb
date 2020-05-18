093323dbe8daa8f5483579e356899ca0ca8fa1fb
Ed Reel <edreel@gmail.com>
2020-05-18 01:04:01 -0500

    Add pre-built binaries for graphviz

--- packages/graphviz.rb
+++ packages/graphviz.rb
@@ -8,8 +8,16 @@ class Graphviz < Package
   source_sha256 'eaff61ce05bf8196c596097bab6310fa6e7e96ee908a199a4bc83fbd5e7082d6'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '3208ec940c22105f3f138933ef6924e31c9dd86cbd9cd90e6be6bb95378eea04',
+     armv7l: '3208ec940c22105f3f138933ef6924e31c9dd86cbd9cd90e6be6bb95378eea04',
+       i686: '4ec896b8ec6dea9115c4adece31aea21fbf8e6dda4851465e17aef301d556106',
+     x86_64: '4065b2252f78f92802cfa2c7293ed6636d7cb71f33fbba9145e7c686d73b3f0d',
   })
 
   depends_on 'automake' => :build
@@ -22,12 +30,23 @@ class Graphviz < Package
   depends_on 'expat'
 
   def self.build
-    system "./autogen.sh"
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} LDFLAGS='-L#{CREW_LIB_PREFIX}' --with-libgd=yes --with-gdincludedir=#{CREW_PREFIX}/include --with-gdlibdir=#{CREW_LIB_PREFIX} --enable-tcl=no --enable-lua=no --enable-perl=no --enable-io=no --with-libexpat=yes"
-    system "make"
+    system './autogen.sh'
+    system './configure',
+           "--prefix=#{CREW_PREFIX}",
+           "--libdir=#{CREW_LIB_PREFIX}",
+           "LDFLAGS='-L#{CREW_LIB_PREFIX}'",
+           "--with-gdlibdir=#{CREW_LIB_PREFIX}",
+           "--with-gdincludedir=#{CREW_PREFIX}/include",
+           '--enable-io=no',
+           '--enable-tcl=no',
+           '--enable-lua=no',
+           '--enable-perl=no',
+           '--with-libgd=yes',
+           '--with-libexpat=yes'
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
