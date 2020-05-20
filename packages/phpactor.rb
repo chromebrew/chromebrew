0856792b303abef67be5bde3c1b4f57a38dd53d5
Vincent GIRARD <vincent@heliosphere.fr>
2020-05-20 18:55:46 +0200

    handle crew search funky outputs

--- packages/phpactor.rb
+++ packages/phpactor.rb
@@ -17,12 +17,14 @@ class Phpactor < Package
   phpMinimalVersion = "7.3"
   phpDefaultVersion = "7.4"
   phpver = `php -v 2> /dev/null | head -1 | cut -d' ' -f2`.chomp
-  if "#{phpver}" == ""
-    depends_on "php"+"#{phpDefaultVersion}".split(".").take(2).join
-  elsif Gem::Version.new("#{phpver}") >= Gem::Version.new("#{phpMinimalVersion}")
-    depends_on "php"+"#{phpver}".split(".").take(2).join
-  else
-    abort "Minimum version of php73 required".lightred 
+  if (ARGV[0] == 'install' || ARGV[0] == 'reinstall' || ARGV[0] == 'upgrade') && ARGV[1] == 'phpactor'
+    if "#{phpver}" == ""
+      depends_on "php"+"#{phpDefaultVersion}".split(".").take(2).join
+    elsif Gem::Version.new("#{phpver}") >= Gem::Version.new("#{phpMinimalVersion}")
+      depends_on "php"+"#{phpver}".split(".").take(2).join
+    else
+      abort "Minimum version of php73 required".lightred 
+    end
   end
 
 
