788ead911b2f5ba033ef6de4cc90f3532dd4f336
Ed Reel <edreel@gmail.com>
2020-05-28 08:25:30 -0500

    Add pre-built binaries for github_cli, ripgrep and wxwidgets
    Add postinstall sections to github_cli and ripgrep

--- packages/github_cli.rb
+++ packages/github_cli.rb
@@ -8,8 +8,16 @@ class Github_cli < Package
   source_sha256 '318295e5a662f785662751f1e2cd4b1f613ec3aced1c4e7f1755d27922dbfdbf'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-0.9.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-0.9.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-0.9.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-0.9.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'e1ac503938d742d34c46ee3c35e0ce1a0f43732c95a20826908633bedc50e625',
+     armv7l: 'e1ac503938d742d34c46ee3c35e0ce1a0f43732c95a20826908633bedc50e625',
+       i686: '6162d4893573836ace9915244dbf565342dcc41ac6369bc8dfa20f970e1cbc93',
+     x86_64: '624f3bf1941d4d1b097c3506af48dbf3fd16d1055057730896a742006e5dc55d',
   })
 
   depends_on 'go' => :build
@@ -20,5 +28,11 @@ class Github_cli < Package
 
   def self.install
     system "install -Dm755 bin/gh #{CREW_DEST_PREFIX}/bin/gh"
+  end
+
+  def self.postinstall
+    puts
+    puts "Type 'gh' to get started.".lightblue
+    puts
   end
 end
--- packages/ripgrep.rb
+++ packages/ripgrep.rb
@@ -8,8 +8,14 @@ class Ripgrep < Package
   source_sha256 'ca2d11dd7b7346734d47ad8073468e9c409fbe85842a608d372b8d4fb36be291'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ripgrep-12.1.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ripgrep-12.1.0-chromeos-armv7l.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ripgrep-12.1.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '3a258a81559b077d04cf3d9912731da26d2f8398538e9a9faa3fb3e0a15aa6cd',
+     armv7l: '3a258a81559b077d04cf3d9912731da26d2f8398538e9a9faa3fb3e0a15aa6cd',
+     x86_64: '1e1b614726be5ad7de8e35921d8ea73c0fd72f4cd3d245b5dae78e356055e17b',
   })
 
   depends_on 'rust'
@@ -27,10 +33,17 @@ class Ripgrep < Package
   end
 
   def self.check
+    ENV['TMPDIR'] = "#{CREW_PREFIX}/tmp"
     system 'cargo test --all'
   end
 
   def self.install
     system "install -Dm755 target/release/rg #{CREW_DEST_PREFIX}/bin/rg"
+  end
+
+  def self.postinstall
+    puts
+    puts "Type 'rg' to get started.".lightblue
+    puts
   end
 end
