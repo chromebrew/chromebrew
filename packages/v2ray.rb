1e418a2efd275df6de74d2f30ea1b19bf41653f3
dx0eu <0xdanger@protonmail.com>
2020-05-31 13:52:44 +0000

    Update v2ray to v4.23.2

--- packages/v2ray.rb
+++ packages/v2ray.rb
@@ -3,18 +3,18 @@ require 'package'
 class V2ray < Package
   description 'A platform for building proxies to bypass network restrictions.'
   homepage 'https://www.v2ray.com/'
-  version 'V4.23.1'
+  version 'v4.23.2'
 
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://github.com/v2ray/v2ray-core/releases/download/V4.23.1/v2ray-linux-arm.zip'
-    source_sha256 '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.23.2/v2ray-linux-arm.zip'
+    source_sha256 '6755c3f35e488e796ecf8d4765be0434057212aae13cd2041a1f275dffd59220'
   when 'i686'
-    source_url 'https://github.com/v2ray/v2ray-core/releases/download/V4.23.1/v2ray-linux-32.zip'
-    source_sha256 '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.23.2/v2ray-linux-32.zip'
+    source_sha256 '55ee0eeaefff9fa42703ecf62ff472610079c4e1e9f8ce1bc62da67559a99507'
   when 'x86_64'
-    source_url 'https://github.com/v2ray/v2ray-core/releases/download/V4.23.1/v2ray-linux-64.zip'
-    source_sha256 '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.23.2/v2ray-linux-64.zip'
+    source_sha256 '791b2974f2f8373844fc3f1e9d93534ebb35b7dfb2ee041de5d6ab9d33f06dd4'
   end
 
   def self.install
