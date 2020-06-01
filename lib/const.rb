f3cf508d3c8539b725df3539df8aa33fb7a90081
Ed Reel <edreel@gmail.com>
2020-06-01 13:10:29 -0500

    Bump crew version

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.4.0'
+CREW_VERSION = '1.4.1'
 
 ARCH = `uname -m`.strip
 ARCH_LIB = if ARCH == 'x86_64' then 'lib64' else 'lib' end
