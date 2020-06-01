de919e0b6d11bcbcdcbe4b208732aac473264d84
Ed Reel <edreel@gmail.com>
2020-06-01 07:16:19 -0500

    Update libxml2 from 2.9.9 to 2.9.10
    Add pre-built binaries for libxml2
    
    Update libxml2_python from 2.9.9 to 2.9.10
    
    Add pre-built binaries for libxml2_python

--- packages/libxml2_python.rb
+++ packages/libxml2_python.rb
@@ -3,25 +3,22 @@ require 'package'
 class Libxml2_python < Package
   description 'Libxml2 (python module) is the XML C parser and toolkit developed for the Gnome project.'
   homepage 'http://xmlsoft.org/'
-  version '2.9.9'
-  source_url 'ftp://xmlsoft.org/libxml2/libxml2-2.9.9.tar.gz'
-  source_sha256 '94fb70890143e3c6549f265cee93ec064c80a84c42ad0f23e85ee1fd6540a871'
+  version '2.9.10'
+  source_url 'ftp://xmlsoft.org/libxml2/libxml2-2.9.10.tar.gz'
+  source_sha256 'aafee193ffb8fe0c82d4afef6ef91972cbaf5feea100edc2f262750611b4be1f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.9-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.10-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.10-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.10-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2_python-2.9.10-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '05b9616edf2809cea4bfaa2a0f9b21e8455871e04f9a76f08c5472e25ab655c6',
-     armv7l: '05b9616edf2809cea4bfaa2a0f9b21e8455871e04f9a76f08c5472e25ab655c6',
-       i686: '9676158d3071ecd96bd3fc3cb0619dd137efc39b23a8c1d0c4d38990d58223ad',
-     x86_64: '54261947d04449bdeb3af3fd9c60b450fc8237defdee0076d23122786eb05934',
+    aarch64: 'e4e0785991911d9a84fc6023345fb5bf1195b14ecff7ab32e2f5fc7e03301569',
+     armv7l: 'e4e0785991911d9a84fc6023345fb5bf1195b14ecff7ab32e2f5fc7e03301569',
+       i686: 'cd4f6ebe11023d88de1e9809fbfd4f1e85d4513cb34450dd9d67fa2f55d96f45',
+     x86_64: 'a4e7d5428a898c545a2f97d41454d711224341b93782cc7e1a435c442b952ade',
   })
-
-  depends_on 'python27'
-  depends_on 'libxml2'
 
   def self.build
     Dir.chdir("python") do
@@ -31,8 +28,7 @@ class Libxml2_python < Package
 
   def self.install
     Dir.chdir("python") do
-      system "python2 setup.py install --root=#{CREW_DEST_DIR}"
+      system "python2 setup.py install --prefix=#{CREW_PREFIX} --root=#{CREW_DEST_DIR}"
     end
   end
-
 end
