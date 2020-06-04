cc509c1b5395c266b4cfeefd1786dd2d52428914
Ed Reel <edreel@gmail.com>
2020-06-03 23:53:47 -0500

    Update brave from 81.1.10.36 to 1.11.48

--- packages/brave.rb
+++ packages/brave.rb
@@ -3,16 +3,15 @@ require 'package'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '81.1.10.36'
+  version '1.11.48'
   case ARCH
   when 'x86_64'
-    source_url 'https://github.com/brave/brave-browser/releases/download/v1.10.36/brave-v1.10.36-linux-x64.zip'
-    source_sha256 'b8e5de4ffd454b171a10e9be260523d414b32e45c05524b2bd8b4b9a96640251'
+    source_url 'https://github.com/brave/brave-browser/releases/download/v1.11.48/brave-v1.11.48-linux-x64.zip'
+    source_sha256 'c1780ec5c4b41e13b2225fd1ed0c7e35af9e3200bf954a91b91c1f0a99cca5d5'
+    depends_on 'gtk3'
+    depends_on 'xdg_base'
+    depends_on 'sommelier'
   end
-
-  depends_on 'gtk3'
-  depends_on 'xdg_base'
-  depends_on 'sommelier'
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
