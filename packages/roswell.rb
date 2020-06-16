76b94ec1ff8cc468c0be106fe092f7979d3a8356
Lloyd Huang - chromebook <lloyd.tw@gmail.com>
2020-06-16 16:39:51 +0800

    add roswell package

---
+++ packages/roswell.rb
@@ -0,0 +1,33 @@
+require 'package'
+
+class Roswell < Package
+  description 'A lisp installer and launcher for major environment.'
+  homepage 'https://github.com/roswell/roswell'
+  version '20.01.14.104'
+  compatibility 'all'
+  source_url 'https://github.com/roswell/roswell/archive/v20.01.14.104.tar.gz'
+  source_sha256 '798b96fdcb8c89445b36692b31570bb99882d83719d6310d969ccfcb2a35a1d4'
+
+  binary_url ({
+    aarch64: '',
+     armv7l: '',
+       i686: '',
+     x86_64: '',
+  })
+  binary_sha256 ({
+    aarch64: '',
+     armv7l: '',
+       i686: '',
+     x86_64: '',
+  })
+
+  def self.build
+    system 'sh bootstrap'
+    system './configure', "--prefix=#{CREW_PREFIX}"
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+end
