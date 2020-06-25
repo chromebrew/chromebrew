d1d7ee7d0355d4f0000f4e714041721b16a71360
Ed Reel <edreel@gmail.com>
2020-06-25 15:29:44 -0500

    Update atom from 1.47.0 to 1.48.0 (#4130)

--- packages/atom.rb
+++ packages/atom.rb
@@ -3,7 +3,7 @@ require 'package'
 class Atom < Package
   description 'The hackable text editor'
   homepage 'https://atom.io/'
-  version '1.47.0'
+  version '1.48.0'
   compatibility 'x86_64'
   case ARCH
   when 'x86_64'
@@ -17,15 +17,15 @@ class Atom < Package
   end
 
   binary_url ({
-    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/atom-1.47.0-chromeos-x86_64.tar.xz',
+    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/atom-1.48.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    x86_64: 'd0a7efc1177865775a27f80a16ec9b8b3110cfa2695d2e1cae4b3173844c912a',
+    x86_64: '0ec7fd1c4aa6c2451c02f6c294e37063b84b1bb97e921c9dee65e99c64ee19e5',
   })
 
   def self.build
     system "wget https://github.com/atom/atom/releases/download/v#{version}/atom-amd64.deb"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('atom-amd64.deb') ) == '500117c2da4b6a2a0d1342559777c921e577aec216ecad2c04b5d800b7d460b3'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('atom-amd64.deb') ) == '41a717bb3749a77f1b55f9262417c5dc8cf9e22d99b5951a85d238ea575f2399'
     system "alien -tc atom-amd64.deb"
     system "tar xvf atom-#{version}.tgz"
   end
