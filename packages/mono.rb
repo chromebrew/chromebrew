9a1f40b07cda3a43b20bdf7f0beb48b8ee9467aa
Ed Reel <edreel@gmail.com>
2020-06-25 15:31:17 -0500

    Update mono from 5.10.1.47 to 6.8.0.123 (#4139)
    Add pre-built binaries

--- packages/mono.rb
+++ packages/mono.rb
@@ -3,36 +3,32 @@ require 'package'
 class Mono < Package
   description 'Mono is a software platform designed to allow developers to easily create cross platform applications part of the .NET Foundation.'
   homepage 'http://www.mono-project.com/'
-  version '5.10.1.47'
+  version '6.8.0.123'
   compatibility 'all'
-  source_url 'https://download.mono-project.com/sources/mono/mono-5.10.1.47.tar.bz2'
-  source_sha256 '90c237b5288f95f6fdab4ace1e36ab64a6369e2c9fddd462d604fd788e2545da'
+  source_url 'https://download.mono-project.com/sources/mono/mono-6.8.0.123.tar.xz'
+  source_sha256 'e2e42d36e19f083fc0d82f6c02f7db80611d69767112af353df2f279744a2ac5'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-5.10.1.47-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mono-5.10.1.47-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mono-5.10.1.47-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-5.10.1.47-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '2d1486554face0afdc68af43869136966674d929d3904296475971a2efc7fff3',
-     armv7l: '2d1486554face0afdc68af43869136966674d929d3904296475971a2efc7fff3',
-       i686: 'dee7c875844a58f2711fc890fc8a51c6defaf5cbaa57889dd7c64d73976ee9e3',
-     x86_64: '32aae35fa5144e6d4d96c79d054491f9783083574e6390c92bd0d6ac29cde1c8',
+    aarch64: 'e149fae5b52de42513f903eebcf3dc3a9690d055554a2a699522225207628874',
+     armv7l: 'e149fae5b52de42513f903eebcf3dc3a9690d055554a2a699522225207628874',
+       i686: '4c05f5517336de0eab70ca765a6d26ccd96ea2e11d654ad556d8f6953ad684d1',
+     x86_64: 'd0ec517b0bc3eafd6a8b0fb22fbf53d97152ce3ade5920a95153c392b7dbae1e',
   })
 
   def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-dependency-tracking',
-           '--disable-maintainer-mode',
-           '--disable-silent-rules',
-           '--enable-nls=no'
+    system "./configure #{CREW_OPTIONS} --disable-dependency-tracking --disable-maintainer-mode --disable-silent-rules --enable-nls=no"
     system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    FileUtils.ln_sf "#{CREW_PREFIX}/bin/#{CREW_BUILD}-mono-sgen", "#{CREW_DEST_PREFIX}/bin/mono"
+    FileUtils.ln_sf "#{CREW_MAN_PREFIX}/man1/#{CREW_BUILD}-mono.1.gz", "#{CREW_DEST_MAN_PREFIX}/man1/mono.1.gz"
   end
 end
