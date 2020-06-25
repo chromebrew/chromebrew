6d92b8d54a9d8c48b05a0e9b4718130d56a77299
Ed Reel <edreel@gmail.com>
2020-06-25 15:30:54 -0500

    Update freeglut from 3.0.0 to 3.2.1 (#4138)
    Add pre-built binaries

--- packages/freeglut.rb
+++ packages/freeglut.rb
@@ -3,22 +3,22 @@ require 'package'
 class Freeglut < Package
   description 'FreeGLUT is a free-software/open-source alternative to the OpenGL Utility Toolkit (GLUT) library.'
   homepage 'http://freeglut.sourceforge.net/'
-  version '3.0.0'
+  version '3.2.1'
   compatibility 'all'
-  source_url 'https://prdownloads.sourceforge.net/project/freeglut/freeglut/3.0.0/freeglut-3.0.0.tar.gz'
-  source_sha256 '2a43be8515b01ea82bcfa17d29ae0d40bd128342f0930cd1f375f1ff999f76a2'
+  source_url 'https://downloads.sourceforge.net/project/freeglut/freeglut/3.2.1/freeglut-3.2.1.tar.gz'
+  source_sha256 'd4000e02102acaf259998c870e25214739d1f16f67f99cb35e4f46841399da68'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.0.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.0.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.0.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.0.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.2.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.2.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.2.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/freeglut-3.2.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '8a42e5a1693a57cb3ae0159e9c1e2a64dcf248077902b893dcd39aefd4100cf3',
-     armv7l: '8a42e5a1693a57cb3ae0159e9c1e2a64dcf248077902b893dcd39aefd4100cf3',
-       i686: 'cd7ac27ead67b3660bb18c037830a0e80bc3d87938c78eda15baa5ba682aef6e',
-     x86_64: '00f1c07741619855037ed004f9ab6a5662fdbc500cb8180513fd39f289d668ea',
+    aarch64: '49c22c36b1c02bd9ea8b439e05ce83d25ff2b0bec96da91366f57500abaafc57',
+     armv7l: '49c22c36b1c02bd9ea8b439e05ce83d25ff2b0bec96da91366f57500abaafc57',
+       i686: '7df49b8ae79cbb1db19a01ba656c652d04a19a236f749ef1246190e8c1b04a68',
+     x86_64: 'f834844fb8a0afb528d602513bef51306afcba74f8361707f28d052d8e28d4d7',
   })
 
   depends_on 'libglu'
@@ -29,6 +29,6 @@ class Freeglut < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
