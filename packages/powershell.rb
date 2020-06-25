7ff0ca69018fd8270e2eed36855b6afab31c4c36
Ed Reel <edreel@gmail.com>
2020-06-25 15:29:27 -0500

    Update powershell from 7.0.0 to 7.0.2 (#4129)

--- packages/powershell.rb
+++ packages/powershell.rb
@@ -3,15 +3,15 @@ require 'package'
 class Powershell < Package
   description 'Powershell is a cross-platform, task-based command-line shell and scripting language that helps rapidly automate tasks that manage operating systems and processes'
   homepage 'https://docs.microsoft.com/en-us/powershell/'
-  version '7.0.0'
+  version '7.0.2'
   compatibility 'aarch64,armv7l,x86_64'
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/powershell-7.0.0-linux-arm32.tar.gz'
-    source_sha256 'cbb6acbb40f73ccf83920f4729dc86ebf3566bb9a01ca68e0698d33fdbe6d7b0'
+    source_url 'https://github.com/PowerShell/PowerShell/releases/download/v7.0.2/powershell-7.0.2-linux-arm32.tar.gz'
+    source_sha256 '3f884c8eb97cbf28402d5ee5c64c50c556c87779f08a7eb01062c5b7567f9246'
   when 'x86_64'
-    source_url 'https://github.com/PowerShell/PowerShell/releases/download/v7.0.0/powershell-7.0.0-linux-x64.tar.gz'
-    source_sha256 '3e80a662a5defb283185961330c4a44d5d5179f9eb2a2ee74ac7e1d3caf16b2c'
+    source_url 'https://github.com/PowerShell/PowerShell/releases/download/v7.0.2/powershell-7.0.2-linux-x64.tar.gz'
+    source_sha256 'e7e458a32c51c6ce43647b8a0eb7fbd9da1b67850c18e389f0eef7c7109891fb'
   end
 
   case ARCH
@@ -22,7 +22,7 @@ class Powershell < Package
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/powershell"
-    FileUtils.cp_r Dir.glob('.'), "#{CREW_DEST_PREFIX}/share/powershell"
+    FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/share/powershell"
     FileUtils.ln_s "#{CREW_PREFIX}/share/powershell/pwsh", "#{CREW_DEST_PREFIX}/bin/pwsh"
   end
 
