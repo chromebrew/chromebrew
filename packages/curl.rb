551b2d230fa7a96c7e6a11d413b2185379e777d3
Ed Reel <edreel@gmail.com>
2020-07-12 10:45:29 -0500

    Update curl from 7.70.0 to 7.71.1

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,22 +3,22 @@ require 'package'
 class Curl < Package
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.haxx.se/'
-  version '7.70.0'
+  version '7.71.1'
   compatibility 'all'
-  source_url 'https://curl.haxx.se/download/curl-7.70.0.tar.xz'
-  source_sha256 '032f43f2674008c761af19bf536374128c16241fb234699a55f9fb603fcfbae7'
+  source_url 'https://curl.haxx.se/download/curl-7.71.1.tar.xz'
+  source_sha256 '40f83eda27cdbeb25cd4da48cefb639af1b9395d6026d2da1825bf059239658c'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.70.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.70.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.70.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.70.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '267a2e42282fe8d75d2ea5f2b7a85d06a34b002a7f2ee6e5f473a1082bf6b79a',
-     armv7l: '267a2e42282fe8d75d2ea5f2b7a85d06a34b002a7f2ee6e5f473a1082bf6b79a',
-       i686: '9c1341459a7febb56423108c4a60f0d6c7ea0d616c96fc36b4fbc2e01fe8ff76',
-     x86_64: '8a66e93101ff728ef712d05080cfbb028a57f344fe7c4548c01efa2c6ef1d955',
+    aarch64: '58505d5a912a20dcc1b09472513e83bcff81fa0aa5492824a928d8aad1a6f588',
+     armv7l: '58505d5a912a20dcc1b09472513e83bcff81fa0aa5492824a928d8aad1a6f588',
+       i686: '545849d0ab122536a49978919b4ba1455a3f3af68fff85ebe3ad603111e93e56',
+     x86_64: 'c31dd8206122e7f66211824098ae9c3005cb3847178d76c24af3b7393d2f7e1f',
   })
 
   depends_on 'groff' => :build
