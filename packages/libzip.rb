ed0dfc9b9d1a7e7381525d39513580fa3b9bdeb8
Ed Reel <edreel@gmail.com>
2020-07-29 16:23:20 -0500

    Update libzip from 1.5.1 to 1.7.3 (#4186)

--- packages/libzip.rb
+++ packages/libzip.rb
@@ -3,22 +3,22 @@ require 'package'
 class Libzip < Package
   description 'libzip is a C library for reading, creating, and modifying zip archives.'
   homepage 'https://libzip.org/'
-  version '1.5.1'
+  version '1.7.3'
   compatibility 'all'
-  source_url 'https://libzip.org/download/libzip-1.5.1.tar.xz'
-  source_sha256 '04ea35b6956c7b3453f1ed3f3fe40e3ddae1f43931089124579e8384e79ed372'
+  source_url 'https://libzip.org/download/libzip-1.7.3.tar.xz'
+  source_sha256 'a60473ffdb7b4260c08bfa19c2ccea0438edac11193c3afbbb1f17fbcf6c6132'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.5.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.5.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.5.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.5.1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.7.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.7.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.7.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libzip-1.7.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'd219497c9fb7451c26c93dd9379808fbbd936317e765be63beafb8d91db2a84d',
-     armv7l: 'd219497c9fb7451c26c93dd9379808fbbd936317e765be63beafb8d91db2a84d',
-       i686: 'd286172ecf333bed71e0bd1c7713cbbefb5a5434e2592ee5e67cd881bccb5784',
-     x86_64: '4d1fa9fad684b7e4aa92885e247e963ac7ad1a1c85fd7a35cec728491a2e72cc',
+    aarch64: 'a8cc96f1befee6a3d4967aa7fa41e691b1cfd564405286214fc9a12ae6ff19e6',
+     armv7l: 'a8cc96f1befee6a3d4967aa7fa41e691b1cfd564405286214fc9a12ae6ff19e6',
+       i686: 'aa4656edce164335ee023fb7af32b135a3ecc733c2a353550a9e325602ac79f6',
+     x86_64: 'bdf0fe0f8dbaa87a70b678cc5aa3730ae7c8b4fc684ab8c499e947b999f7bb6b',
   })
 
   depends_on 'bz2'
@@ -29,7 +29,8 @@ class Libzip < Package
       system 'cmake',
              "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
              "-DCMAKE_INSTALL_LIBDIR=#{ARCH_LIB}",
-             '-DCMAKE_BUILD_TYPE=Release ..'
+             '-DCMAKE_BUILD_TYPE=Release',
+             '..'
       system 'make'
     end
   end
