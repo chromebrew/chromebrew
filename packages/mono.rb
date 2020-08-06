45fbb190f11e1211912b8ca26e663cbab1b454f3
Ed Reel <edreel@gmail.com>
2020-08-06 15:33:11 -0500

    Add symlinks to mono package binaries (#4190)

--- packages/mono.rb
+++ packages/mono.rb
@@ -3,22 +3,22 @@ require 'package'
 class Mono < Package
   description 'Mono is a software platform designed to allow developers to easily create cross platform applications part of the .NET Foundation.'
   homepage 'http://www.mono-project.com/'
-  version '6.8.0.123'
+  version '6.8.0.123-1'
   compatibility 'all'
   source_url 'https://download.mono-project.com/sources/mono/mono-6.8.0.123.tar.xz'
   source_sha256 'e2e42d36e19f083fc0d82f6c02f7db80611d69767112af353df2f279744a2ac5'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mono-6.8.0.123-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'e149fae5b52de42513f903eebcf3dc3a9690d055554a2a699522225207628874',
-     armv7l: 'e149fae5b52de42513f903eebcf3dc3a9690d055554a2a699522225207628874',
-       i686: '4c05f5517336de0eab70ca765a6d26ccd96ea2e11d654ad556d8f6953ad684d1',
-     x86_64: 'd0ec517b0bc3eafd6a8b0fb22fbf53d97152ce3ade5920a95153c392b7dbae1e',
+    aarch64: '8e62055873099e3f943dd33789274c3db2d5436ddec704c72c8cf758d24f6ee5',
+     armv7l: '8e62055873099e3f943dd33789274c3db2d5436ddec704c72c8cf758d24f6ee5',
+       i686: 'd0442cd9a809387cf79cb6a605018244704ccd25333fcc33f1e8440fc0c52a04',
+     x86_64: '46077f0cefcfc389006e730755ec9832a7624d67b9285b27410655cd65b55083',
   })
 
   def self.build
@@ -28,7 +28,15 @@ class Mono < Package
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    FileUtils.ln_sf "#{CREW_PREFIX}/bin/#{CREW_BUILD}-mono-sgen", "#{CREW_DEST_PREFIX}/bin/mono"
-    FileUtils.ln_sf "#{CREW_MAN_PREFIX}/man1/#{CREW_BUILD}-mono.1.gz", "#{CREW_DEST_MAN_PREFIX}/man1/mono.1.gz"
+    Dir.chdir "#{CREW_DEST_PREFIX}/bin" do
+      system "for f in \$(ls #{CREW_BUILD}-*); do g=\$(echo \$f | sed 's,#{CREW_BUILD}-,,'); ln -sf \$f \$g; done"
+      FileUtils.ln_sf "#{CREW_BUILD}-mono-sgen", 'mono'
+    end
+    Dir.chdir "#{CREW_DEST_MAN_PREFIX}/man1" do
+      system "for f in \$(ls #{CREW_BUILD}-*); do g=\$(echo \$f | sed 's,#{CREW_BUILD}-,,'); ln -sf \$f \$g; done"
+    end
+    Dir.chdir "#{CREW_DEST_MAN_PREFIX}/man5" do
+      system "for f in \$(ls #{CREW_BUILD}-*); do g=\$(echo \$f | sed 's,#{CREW_BUILD}-,,'); ln -sf \$f \$g; done"
+    end
   end
 end
