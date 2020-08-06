e7a7f9cba5ef8efd6209602e544dd846261d6064
vincowl <vince512@free.fr>
2020-08-06 05:23:50 +0200

    [neovim] Update to 0.4.4 (#4193)
    Co-authored-by: Vincent GIRARD <vincent@heliosphere.fr>

--- packages/neovim.rb
+++ packages/neovim.rb
@@ -3,23 +3,17 @@ require 'package'
 class Neovim < Package
   description 'Neovim is a refactor, and sometimes redactor, in the tradition of Vim (which itself derives from Stevie).'
   homepage 'https://neovim.io/'
-  version '0.4.3'
+  version '0.4.4'
   compatibility 'aarch64,armv7l,x86_64'
   case ARCH
   when 'aarch64', 'armv7l', 'x86_64'
-    source_url 'https://github.com/neovim/neovim/archive/v0.4.3.tar.gz'
-    source_sha256 '91a0b5d32204a821bf414690e6b48cf69224d1961d37158c2b383f6a6cf854d2'
+    source_url 'https://github.com/neovim/neovim/archive/v0.4.4.tar.gz'
+    source_sha256 '2f76aac59363677f37592e853ab2c06151cca8830d4b3fe4675b4a52d41fc42c'
   end
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/neovim-0.4.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/neovim-0.4.3-chromeos-armv7l.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/neovim-0.4.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '3a984f1e22e7a401e81b3acf0af68c19f42fe9bd6c9c3496f80f27b7fac38bfb',
-     armv7l: '3a984f1e22e7a401e81b3acf0af68c19f42fe9bd6c9c3496f80f27b7fac38bfb',
-     x86_64: 'a0164cb3b46076bb0723b90fec6234dbb478b4067d657aa096cc765a91888464',
   })
 
   def self.patch
