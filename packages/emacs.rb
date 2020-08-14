94d4de3dc4076486f82b42f45c75e7542cb0e01e
Ed Reel <edreel@gmail.com>
2020-08-13 22:38:38 -0500

    Add pre-built binaries for emacs and gemacs (#4206)

--- packages/emacs.rb
+++ packages/emacs.rb
@@ -9,8 +9,16 @@ class Emacs < Package
   source_sha256 '4a4c128f915fc937d61edfc273c98106711b540c9be3cd5d2e2b9b5b2f172e41'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-27.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-27.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-27.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-27.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '2a4c0e724b6d5d05ae99418cad0557dc6e72c04e66dbccc041971bd3d954f408',
+     armv7l: '2a4c0e724b6d5d05ae99418cad0557dc6e72c04e66dbccc041971bd3d954f408',
+       i686: 'e428b1bcf011ce6a81a057b7b757436385ad11c5e3b34d5508d2ed3411b27047',
+     x86_64: 'ee87e75116c30bdfcab5c92dff5f7413c6b7bdc1fe813727dce16f21f2b0ec61',
   })
 
   depends_on 'lcms'
--- packages/gemacs.rb
+++ packages/gemacs.rb
@@ -9,8 +9,16 @@ class Gemacs < Package
   source_sha256 '4a4c128f915fc937d61edfc273c98106711b540c9be3cd5d2e2b9b5b2f172e41'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-27.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-27.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-27.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-27.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '303510f76a0b25adbf43299521ff13ca40dfb5442e907084b06865515eb56b6e',
+     armv7l: '303510f76a0b25adbf43299521ff13ca40dfb5442e907084b06865515eb56b6e',
+       i686: 'c5e322757cc41505365d52f48a1668c1fb44d28ded3ac75687c6f7e96bc38c8e',
+     x86_64: 'e6ab6fb5a2aa094a2589a805e2539b0fc2a1dbddbb88d203e2c536dcf903aba8',
   })
 
   depends_on 'giflib'
