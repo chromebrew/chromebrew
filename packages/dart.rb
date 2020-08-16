e437ec71f9c442793a64061bd74b3cb029ba0612
Casey Strouse <casey.strouse@gmail.com>
2020-08-16 05:22:29 -0700

    Update dart from 2.7.0 to 2.9.1 (#4215)
    Tested on x86_64.

--- packages/dart.rb
+++ packages/dart.rb
@@ -3,18 +3,18 @@ require 'package'
 class Dart < Package
   description 'The Dart SDK is a set of tools and libraries for the Dart programming language.  You can find information about Dart online at dartlang.org.'
   homepage 'https://www.dartlang.org'
-  version '2.7.0'
+  version '2.9.1'
   compatibility 'all'
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/2.7.0/sdk/dartsdk-linux-arm-release.zip'
-    source_sha256 '2270ae2d3e467c539dcc6358312bba949f2614f7da78225e7a1ba5b57981ca0c'
+    source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/2.9.1/sdk/dartsdk-linux-arm-release.zip'
+    source_sha256 'e393c05136571bf7c4879a2bfe207a768be449a3d0bcc8a7ff700b01c4330109'
   when 'i686'
-    source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/2.7.0/sdk/dartsdk-linux-ia32-release.zip'
-    source_sha256 'a503731077c332fbde70c06b602efc5024d59e7331f08dba087d2d8bbf4e6c23'
+    source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/2.9.1/sdk/dartsdk-linux-ia32-release.zip'
+    source_sha256 '8492ec94a07c6100f1a097864668e4b9063f3b1ed9f05c01dee2d59158ae6a04'
   when 'x86_64'
-    source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/2.7.0/sdk/dartsdk-linux-x64-release.zip'
-    source_sha256 '65844622eb095be903d057d78af4826bfc204d8ea156f77a14b954520f019827'
+    source_url 'https://storage.googleapis.com/dart-archive/channels/stable/release/2.9.1/sdk/dartsdk-linux-x64-release.zip'
+    source_sha256 '8e092105482e3e1be90af44962e3b7973a20cd8f71aa9f6e07199174a58e0eed'
   end
 
   binary_url ({
