2b75fe91562d873eb8521737582467a01c5f9d20
JL2210 <larrowe.semaj11@gmail.com>
2020-08-16 10:12:30 -0400

    Update tilp2.rb (#4054)
    Inline patch; not sure if this works or not, please test, and, if applicable, fix.

--- packages/tilp2.rb
+++ packages/tilp2.rb
@@ -26,9 +26,41 @@ class Tilp2 < Package
   depends_on 'sommelier'
 
   def self.patch
-    system "wget https://github.com/JL2210/patches/raw/04eeee244059b41e0f0f4bf60f38fc4b6cb1e480/tilp2-1.18-autoconf.patch"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('tilp2-1.18-autoconf.patch') ) == 'dedba5bbd1ceb9cf819ea62c156377b6e4eb4b167f33e2c4c8823c2684fa352b'
-    system 'patch -Np1 -i tilp2-1.18-autoconf.patch'
+    system 'cat << _EOF > tilp2.patch
+diff -ur a/configure.ac b/configure.ac
+--- a/configure.ac	2016-01-15 15:28:52.000000000 -0500
++++ b/configure.ac	2018-09-16 13:15:59.476538177 -0400
+@@ -131,7 +131,6 @@
+ fi
+ if test "x$kde" = "xyes"; then
+         AC_PROG_CXX
+-        AC_PATH_KDE
+         AC_DEFINE(WITH_KDE, 1, [Use KDE support])
+ fi
+ AM_CONDITIONAL(USE_KDE, test "x$kde" = "xyes")
+diff -ur a/src/Makefile.am b/src/Makefile.am
+--- a/src/Makefile.am	2016-03-14 15:55:57.000000000 -0400
++++ b/src/Makefile.am	2018-09-16 13:17:01.062539362 -0400
+@@ -9,16 +9,13 @@
+ tilp_CPPFLAGS = -I$(top_srcdir)/intl \
+ 	@TICABLES_CFLAGS@ @TIFILES_CFLAGS@ @TICALCS_CFLAGS@ @TICONV_CFLAGS@ \
+ 	@GLIB_CFLAGS@ @GTK_CFLAGS@ \
+-	@KDE_INCLUDES@ @QT_INCLUDES@ \
+ 	-DSHARE_DIR=\"$(pkgdatadir)\" \
+ 	-DLOCALEDIR=\"$(datadir)/locale\" \
+ 	-DSYSCONFDIR=\"$(sysconfdir)\" \
+ 	-DGTK_DISABLE_DEPRECATED
+ tilp_LDFLAGS = -export-dynamic
+ tilp_LDADD = @TICABLES_LIBS@ @TIFILES_LIBS@ @TICALCS_LIBS@ @TICONV_LIBS@ \
+-	@GLIB_LIBS@ @GTK_LIBS@ \
+-	@LIB_KDECORE@ @LIB_KDEUI@ @LIB_KIO@ @LIB_QT@ @KDE_LDFLAGS@ \
+-	@QT_LDFLAGS@ @X_LDFLAGS@ @LIBZ@
++	@GLIB_LIBS@ @GTK_LIBS@ @LIBZ@
+ tilp_SOURCES = *.h \
+ 	tilp_calcs.c tilp_cmdline.c tilp_config.c tilp_error.c \
+ 	tilp_files.c tilp_gif.c tilp_main.c \
+_EOF'
+    system "patch -Np1 -i tilp2.patch"
   end
 
   def self.build
