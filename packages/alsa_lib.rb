0b399bfede6bd795e0918037a404bfe90bb7485e
vincowl <vince512@free.fr>
2020-08-18 01:33:19 +0200

    [alsa-lib] Update to 1.2.3.2 (#4222)
    * [alsa-lib] Update to 1.2.3.2
    
    Co-authored-by: Vincent GIRARD <vincent@heliosphere.fr>

--- packages/alsa_lib.rb
+++ packages/alsa_lib.rb
@@ -3,22 +3,14 @@ require 'package'
 class Alsa_lib < Package
   description 'The Advanced Linux Sound Architecture (ALSA) provides audio and MIDI functionality to the Linux operating system.'
   homepage 'https://www.alsa-project.org/main/index.php/Main_Page'
-  version '1.2.2'
+  version '1.2.3.2'
   compatibility 'all'
-  source_url 'ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.2.2.tar.bz2'
-  source_sha256 'd8e853d8805574777bbe40937812ad1419c9ea7210e176f0def3e6ed255ab3ec'
+  source_url 'ftp://ftp.alsa-project.org/pub/lib/alsa-lib-1.2.3.2.tar.bz2'
+  source_sha256 'e81fc5b7afcaee8c9fd7f64a1e3043e88d62e9ad2c4cff55f578df6b0a9abe15'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.2.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.2.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.2.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/alsa_lib-1.2.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'f20836e7392c095c013f60d797f4baafeb45cbca294a8c18f65f3e4b6254f177',
-     armv7l: 'f20836e7392c095c013f60d797f4baafeb45cbca294a8c18f65f3e4b6254f177',
-       i686: 'fb5eac7b44ff2281f11c6ca0868111d082d52c426bf233b096db43966fc77411',
-     x86_64: '69c2ad1f9f67af8573f63f79c2e44072d521aad1497ebf1197c668d22fddfec4',
   })
 
   depends_on 'python3'
