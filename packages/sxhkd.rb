c6695ad3e3abd46de06015c5cccd96668cd8f15d
ThatGeekyWeeb <thatgeekyweeb@gmail.com>
2020-08-17 22:21:33 -0400

    New Package: sxhkd - Hotkey Deamon (#4225)

---
+++ packages/sxhkd.rb
@@ -0,0 +1,21 @@
+require 'package'
+    
+class Sxhkd < Package
+    description 'Simple X HotKey Daemon'
+    homepage "https://github.com/baskerville/sxhkd"
+    version '0.6.2'
+    compatibility 'all'
+    source_url "#{homepage}/archive/#{version}.tar.gz"
+    source_sha256 '1edc8b1a8b3631c10ba9cb9df1181830dacbbdf77adb558e31d5dd2029637386'
+    
+    depends_on 'xcb_util_keysyms'
+    depends_on 'xcb_util'
+    depends_on 'libxcb'
+    
+    def self.build
+        system "make PREFIX=#{CREW_PREFIX}"
+    end
+    def self.install
+        system "make install"
+    end
+end
