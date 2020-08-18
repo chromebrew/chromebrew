43dcc5a6c58fa6ab743ea540f17f4c9d8ebf5556
ThatGeekyWeeb <thatgeekyweeb@gmail.com>
2020-08-18 16:04:33 -0400

    New Package: pavucontrol - Packaged with templator

--- packages/glibmm.rb
+++ packages/glibmm.rb
@@ -1,42 +1,21 @@
 require 'package'
 
 class Glibmm < Package
-  description 'Glibmm package is a set of C++ bindings for GLib'
-  homepage 'https://www.gtkmm.org/en/'
-  version '2.56.0-0'
-  compatibility 'all'
-  source_url 'https://ftp.gnome.org/pub/gnome/sources/glibmm/2.56/glibmm-2.56.0.tar.xz'
-  source_sha256 '6e74fcba0d245451c58fc8a196e9d103789bc510e1eee1a9b1e816c5209e79a9'
+  description "C++ bindings for GLib"
+  homepage "https://www.gtkmm.org"
+  version "2.64.2"
+  compatibility "all"
+  source_url "https://ftp.gnome.org/pub/GNOME/sources/glibmm/2.64/glibmm-2.64.2.tar.xz"
+  source_sha256 "a75282e58d556d9b2bb44262b6f5fb76c824ac46a25a06f527108bec86b8d4ec"
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.56.0-0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.56.0-0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.56.0-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.56.0-0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '734ea5bce78ffef9ad52a2d7503b55b2bb47cbe743142c371ac68cb543df4343',
-     armv7l: '734ea5bce78ffef9ad52a2d7503b55b2bb47cbe743142c371ac68cb543df4343',
-       i686: 'c88a048055070b1c14c296f49eb20333a6ffe4a8e4c37fdab06990fd798b6b87',
-     x86_64: '3c30713258dd4972d7118024d44e09f87e1eeea466dfc4b72cb5bb8ea19347bc',
-  })
-
-  depends_on 'glib'
-  depends_on 'libsigcplusplus' # sigc++-2.0
+  depends_on "libsigcplusplus"
   depends_on 'mm_common' => :build
 
   def self.build
-    # fix the documents directory name
-    system "sed -e '/^libdocdir =/ s/$(book_name)/glibmm-2.56.0/' \
-    -i docs/Makefile.in"
-    system "./configure",
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+      system "./configure #{CREW_OPTIONS} "
+      system "make -j#{CREW_NPROC}"
   end
-
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
- end
-
+      system "make install DESTDIR=#{CREW_DEST_DIR}"
+  end
 end
--- packages/gtkmm3.rb
+++ packages/gtkmm3.rb
@@ -3,38 +3,21 @@ require 'package'
 class Gtkmm3 < Package
   description 'The Gtkmm3 package provides a C++ interface to GTK+ 3.'
   homepage 'https://www.gtkmm.org/'
-  version '3.22.2'
+  version '3.24.1'
   compatibility 'all'
-  source_url 'https://ftp.gnome.org/pub/gnome/sources/gtkmm/3.22/gtkmm-3.22.2.tar.xz'
-  source_sha256 '91afd98a31519536f5f397c2d79696e3d53143b80b75778521ca7b48cb280090'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gtkmm3-3.22.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gtkmm3-3.22.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gtkmm3-3.22.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gtkmm3-3.22.2-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '01538cda99b9b67168aa6d723a06b80d8ebd4bf0bc3b218f793f70868ee2a8da',
-     armv7l: '01538cda99b9b67168aa6d723a06b80d8ebd4bf0bc3b218f793f70868ee2a8da',
-       i686: '3a89b7fd9f1b2c7bd863070144d3d84d0e99797eac3f8dfd06f7e88441ea0541',
-     x86_64: '53bab1cbca694187e64084186991068557f6126064a0ca21f96508d55c9fba88',
-  })
+  source_url 'https://ftp.gnome.org/pub/gnome/sources/gtkmm/3.24/gtkmm-3.24.1.tar.xz'
+  source_sha256 'ddfe42ed2458a20a34de252854bcf4b52d3f0c671c045f56b42aa27c7542d2fd'
 
   depends_on 'atkmm'
   depends_on 'gtk3'
   depends_on 'pangomm'
 
   def self.build
-    # fix the documents directory name
-    system "sed -e '/^libdocdir =/ s/$(book_name)/gtkmm-3.22.2/' \
-    -i docs/Makefile.in"
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+    system "./configure #{CREW_OPTIONS}"
+    system "make -j#{CREW_NPROC}"
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
- end
-
+  end
 end
