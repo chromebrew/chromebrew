c57fd56e1e9865f5610c8ebba2ccf1fc066937a9
Ed Reel <edreel@gmail.com>
2020-08-23 01:51:32 -0500

    Add pre-built binaries for garcon, libwnck, xfce4_panel and thunar

--- packages/garcon.rb
+++ packages/garcon.rb
@@ -8,13 +8,28 @@ class Garcon < Package
   source_url 'https://archive.xfce.org/src/xfce/garcon/0.7/garcon-0.7.0.tar.bz2'
   source_sha256 '82c3b61b508011642b09e6fb01b1d3f22c4e4de0fc54a9244327d0ddb66b2423'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/garcon-0.7.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/garcon-0.7.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/garcon-0.7.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/garcon-0.7.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'b0259935006ca1549ba4bca6349047ccb730a4f6e0ae3bea71bb1ef84a46c032',
+     armv7l: 'b0259935006ca1549ba4bca6349047ccb730a4f6e0ae3bea71bb1ef84a46c032',
+       i686: 'cdbab26e79d8b12d9bdae3cdfbf02dd95488667748a79497c16484fd7b0b9fe6',
+     x86_64: '4d4f409b680b69c1f0d45e8d49e99f559c53ce20bc4551e11b05a260933949bf',
+  })
+
+  depends_on 'gtk2'
   depends_on 'libxfce4ui'
 
   def self.build
-      system "./configure #{CREW_OPTIONS} --disable-static"
-      system "make -j#{CREW_NPROC}"
+    system "./configure #{CREW_OPTIONS} --disable-static --enable-gtk2 --enable-libxfce4ui"
+    system "make -j#{CREW_NPROC}"
   end
+
   def self.install
-      system "make install DESTDIR=#{CREW_DEST_DIR}"
+    system "make install DESTDIR=#{CREW_DEST_DIR}"
   end
 end
--- packages/xfce4_panel.rb
+++ packages/xfce4_panel.rb
@@ -8,6 +8,19 @@ class Xfce4_panel < Package
   source_url 'https://archive.xfce.org/src/xfce/xfce4-panel/4.14/xfce4-panel-4.14.4.tar.bz2'
   source_sha256 '8e5ea79412ba84cfada897ff309cbe2cd4aca16b9bd4f93df060229528576fd5'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_panel-4.14.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_panel-4.14.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_panel-4.14.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_panel-4.14.4-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '283544affaec2a778d61b6f93a3b1c0b5dc3c59569cef42522d38e888afeb725',
+     armv7l: '283544affaec2a778d61b6f93a3b1c0b5dc3c59569cef42522d38e888afeb725',
+       i686: 'fbc48b16e7c35e54096170b94cf1db6a662886bbd43937974f0df20812adb8e3',
+     x86_64: 'e75c8e2317baef70266e4b4c8643d4d704c4fa082104290bbae97178392f870f',
+  })
+
   depends_on 'libwnck'
   depends_on 'libxfce4ui'
   depends_on 'xfconf'
@@ -16,10 +29,11 @@ class Xfce4_panel < Package
   depends_on 'gtk3'
 
   def self.build
-      system "./configure #{CREW_OPTIONS} --disable-static --enable-gio-unix --enable-gtk3"
-      system "make -j#{CREW_NPROC}"
+    system "./configure #{CREW_OPTIONS} --disable-static --enable-gio-unix --enable-gtk2"
+    system "make -j#{CREW_NPROC}"
   end
+
   def self.install
-      system "make install DESTDIR=#{CREW_DEST_DIR}"
+    system "make install DESTDIR=#{CREW_DEST_DIR}"
   end
 end
