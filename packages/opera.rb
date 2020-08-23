4f60b8d24bb0f2850fcdc7b22c5d0a067a9ddc5e
Ed Reel <edreel@gmail.com>
2020-08-23 12:09:02 -0500

    Update opera from 69.0.3686.36 to 70.0.3728.95

--- packages/opera.rb
+++ packages/opera.rb
@@ -3,7 +3,7 @@ require 'package'
 class Opera < Package
   description "Opera isn't just a browser. It's an awesome browser."
   homepage 'https://www.opera.com/'
-  version '69.0.3686.36'
+  version '70.0.3728.95'
   compatibility 'x86_64'
   case ARCH
   when 'x86_64'
@@ -15,15 +15,15 @@ class Opera < Package
   end
 
   binary_url ({
-    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/opera-68.0.3618.63-chromeos-x86_64.tar.xz',
+    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/opera-70.0.3728.95-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    x86_64: '37db703800136f9ae5f356f3e4fa4d6b60b0dfc3bf87f58d0655b3a1d24ea018',
+    x86_64: '33f8f82a7d4b2ecf603b7571a7fb5339cc0bb6d9a329f3a4d766cce6f0b1bda4',
   })
 
   def self.build
     system "wget https://get.geo.opera.com/pub/opera/desktop/#{version}/linux/opera-stable_#{version}_amd64.deb"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("opera-stable_#{version}_amd64.deb") ) == 'aa7fcd532feae92fca4e347b0a19f6a9363d7bd0735d9fea0e0f95a7aa6e9029'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("opera-stable_#{version}_amd64.deb") ) == '98d4bd5fe806773ff7e7eb7eded13e0ed440952821784d3a3e0a21a26deb8a77'
     system "alien -tc opera-stable_#{version}_amd64.deb"
     system "tar xvf opera-stable-#{version}.tgz"
   end
