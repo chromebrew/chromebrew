46202dfcf2dae0ea45554da573ff3661a19b5978
Ed Reel <edreel@gmail.com>
2020-08-24 12:03:54 -0500

    Add pre-built binaries e-x (#4235)
    Add missing dependencies

--- packages/libxfce4ui.rb
+++ packages/libxfce4ui.rb
@@ -2,22 +2,36 @@ require 'package'
 
 class Libxfce4ui < Package
   description 'Replacement of the old libxfcegui4 library'
-  homepage "https://xfce.org/"
+  homepage 'https://xfce.org/'
   version '4.15.3'
   compatibility 'all'
-  source_url "https://archive.xfce.org/src/xfce/libxfce4ui/4.15/libxfce4ui-4.15.3.tar.bz2"
+  source_url 'https://archive.xfce.org/src/xfce/libxfce4ui/4.15/libxfce4ui-4.15.3.tar.bz2'
   source_sha256 'ce89419720da0fa84a3bb46bc447564c5800057c026c272ae0b016918c0a9307'
-    
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libxfce4ui-4.15.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libxfce4ui-4.15.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libxfce4ui-4.15.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libxfce4ui-4.15.3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '54ae66c12fc66405c48554cc4d9e439e84a65da3cf438f907febbc15f76eadb7',
+     armv7l: '54ae66c12fc66405c48554cc4d9e439e84a65da3cf438f907febbc15f76eadb7',
+       i686: '91273a8ee09968a855c910684b7b5d4c388f6c53f19c7965b5265fc456a22d92',
+     x86_64: 'c41174c81ff48917e9e89f99d970ff716261247ab0a8cfd0ae17ebfe85e69cde',
+  })
+
   depends_on 'gtk3'
   depends_on 'gtk2'
   depends_on 'pygtk' # For gtk+
   depends_on 'xfconf'
-    
+
   def self.build
-      system "./configure #{CREW_OPTIONS}"
-      system "make -j#{CREW_NPROC}"
- end
- def self.install
-      system "make install DESTDIR=#{CREW_DEST_DIR}"
+    system "./configure #{CREW_OPTIONS}"
+    system "make -j#{CREW_NPROC}"
+  end
+
+  def self.install
+    system "make install DESTDIR=#{CREW_DEST_DIR}"
   end
 end
--- packages/xfce4_dev_tools.rb
+++ packages/xfce4_dev_tools.rb
@@ -2,19 +2,33 @@ require 'package'
 
 class Xfce4_dev_tools < Package
   description 'Xfce4 development tools'
-  homepage "https://xfce.org/"
+  homepage 'https://xfce.org/'
   version '4.14.0'
   compatibility 'all'
-  source_url "https://archive.xfce.org/src/xfce/xfce4-dev-tools/4.14/xfce4-dev-tools-4.14.0.tar.bz2"
+  source_url 'https://archive.xfce.org/src/xfce/xfce4-dev-tools/4.14/xfce4-dev-tools-4.14.0.tar.bz2'
   source_sha256 '2c9eb8e0fe23e47dc31411a93b683fd1b7a49140e9163f0aab9e94a3d8a0b5fd'
-    
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_dev_tools-4.14.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_dev_tools-4.14.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_dev_tools-4.14.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_dev_tools-4.14.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '8b1ceccfed1a4bdb34dbf9ed35c246cca5035754ae01185db88261cd5ed62dd4',
+     armv7l: '8b1ceccfed1a4bdb34dbf9ed35c246cca5035754ae01185db88261cd5ed62dd4',
+       i686: 'fe79c7ae95dc08eb4a32168d8b0ff5fcd203cd91d7b12d894a94f826d84dd425',
+     x86_64: '24341f2465119626d4625ba9926f7d63f7e6a95bba6f18e01706025855399c9e',
+  })
+
   depends_on 'gtk_doc'
 
   def self.build
-      system "./configure #{CREW_OPTIONS}"
-      system "make -j#{CREW_NPROC}"
+    system "./configure #{CREW_OPTIONS}"
+    system "make -j#{CREW_NPROC}"
   end
+
   def self.install
-      system "make install DESTDIR=#{CREW_DEST_DIR}"
+    system "make install DESTDIR=#{CREW_DEST_DIR}"
   end
 end
