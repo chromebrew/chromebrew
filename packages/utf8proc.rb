4303d4fb687cb11414fe11547268e04a75233ecc
Ed Reel <edreel@gmail.com>
2020-08-31 00:05:04 -0500

    Update utf8proc from 2.1.0-1 to 2.5.0

--- packages/utf8proc.rb
+++ packages/utf8proc.rb
@@ -3,22 +3,22 @@ require 'package'
 class Utf8proc < Package
   description 'a clean C library for processing UTF-8 Unicode data: normalization, case-folding, graphemes, and more'
   homepage 'https://julialang.org/utf8proc/'
-  version '2.1.0-1'
+  version '2.5.0'
   compatibility 'all'
-  source_url 'https://github.com/JuliaLang/utf8proc/archive/v2.1.0.tar.gz'
-  source_sha256 '25ccc5619ac400f9eaf24aafaae5b625afa1961e140b8dd6072b393267833260'
+  source_url 'https://github.com/JuliaStrings/utf8proc/archive/v2.5.0.tar.gz'
+  source_sha256 'd4e8dfc898cfd062493cb7f42d95d70ccdd3a4cd4d90bec0c71b47cca688f1be'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.1.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.1.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.1.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.1.0-1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.5.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.5.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.5.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/utf8proc-2.5.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'c64f1656e7dc1b7c3395661dacfb0f9d4b043e94ff07e0baf0ee8cc04fd8f757',
-     armv7l: 'c64f1656e7dc1b7c3395661dacfb0f9d4b043e94ff07e0baf0ee8cc04fd8f757',
-       i686: '9bbc7e68deb6dbc1888fd1ac8300485114a288264baca54babc0adcb0cee5eba',
-     x86_64: '837e33dad2cc4db985980978e4ffdebe7316e1f6aacf5308d00e78e65c00e627',
+    aarch64: '9ebbd94d6221173beb34f0c80f62d38d557d87ceceac29d9ee7cfd96f6ccbfb2',
+     armv7l: '9ebbd94d6221173beb34f0c80f62d38d557d87ceceac29d9ee7cfd96f6ccbfb2',
+       i686: '5a8ba1b61b76ce238665c679c00ea25d35815e0bc66cdce0d506227314d90cb9',
+     x86_64: '93e528e8c0e6dbce7a76f0120ceca9de33ad168718893a3a1e16d28514617813',
   })
 
   def self.build
@@ -26,7 +26,12 @@ class Utf8proc < Package
   end
 
   def self.install
-    system "make", "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}",
-           "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    case ARCH
+    when 'x86_64'
+      Dir.chdir "#{CREW_DEST_PREFIX}" do
+        FileUtils.mv 'lib/', 'lib64/'
+      end
+    end
   end
 end
