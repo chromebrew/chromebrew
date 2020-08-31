157261e2cb5ecc546efa1399b4af33d80ac7b63d
Ed Reel <edreel@gmail.com>
2020-08-31 00:35:34 -0500

    Add netsurf binaries and dependencies

--- packages/libdom.rb
+++ packages/libdom.rb
@@ -8,15 +8,35 @@ class Libdom < Package
   source_url 'https://download.netsurf-browser.org/libs/releases/libdom-0.4.1-src.tar.gz'
   source_sha256 '98ee072471e55e208c9878e515c410ad462ca36f87b4afcbecad023f3a2cef4a'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libdom-0.4.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libdom-0.4.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libdom-0.4.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libdom-0.4.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '7fa6e8636c79092311db986438b9234bd2c5ec9a8d66081ad13c87f49048ac70',
+     armv7l: '7fa6e8636c79092311db986438b9234bd2c5ec9a8d66081ad13c87f49048ac70',
+       i686: '4a646fac2a67a2129096e873c04adeed05a95cf55c08f7a24d30ee70c86fe48c',
+     x86_64: 'adb310321694c02e68faf7f4ba97e54903b5c6e132a7a29564ec590bab9269c0',
+  })
+
   depends_on 'expat'
   depends_on 'libparserutils'
   depends_on 'libwapcaplet'
   depends_on 'libhubbub'
 
   def self.build
-   system "make -j#{CREW_NPROC} PREFIX=#{CREW_PREFIX}"
+    system "make PREFIX=#{CREW_PREFIX} COMPONENT_TYPE=lib-shared"
   end
+
   def self.install
-   system "make -j#{CREW_NPROC} DESTDIR=#{CREW_DEST_DIR} install PREFIX=#{CREW_PREFIX}"
+    system "make install PREFIX=#{CREW_PREFIX} COMPONENT_TYPE=lib-shared DESTDIR=#{CREW_DEST_DIR}"
+    case ARCH
+    when 'x86_64'
+      Dir.chdir CREW_DEST_PREFIX do
+        FileUtils.mv 'lib/', 'lib64/'
+      end
+    end
   end
 end
--- packages/libnsutils.rb
+++ packages/libnsutils.rb
@@ -8,10 +8,30 @@ class Libnsutils < Package
   source_url 'https://download.netsurf-browser.org/libs/releases/libnsutils-0.1.0-src.tar.gz'
   source_sha256 '790c6516344abe82f6289d656392e9ccebe475b20cc7e1e2d843011385f7aef0'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libnsutils-0.1.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libnsutils-0.1.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libnsutils-0.1.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libnsutils-0.1.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'ef293ef4867289c24fee1e31a382d6d0091f5a69e3a1f7c02c1d3dba8cf09d5c',
+     armv7l: 'ef293ef4867289c24fee1e31a382d6d0091f5a69e3a1f7c02c1d3dba8cf09d5c',
+       i686: 'f02f7469190685bb06d1c0aed2378195363183db3a62cd6c3cb37d7f032364d6',
+     x86_64: '503ce72a465a355da07ebdd96d6e3b6116d80e79e1cb4bd9fc9c0c928951c124',
+  })
+
   def self.build
-   system "make -j#{CREW_NPROC} PREFIX=#{CREW_PREFIX}"
+    system "make PREFIX=#{CREW_PREFIX} COMPONENT_TYPE=lib-shared"
   end
+
   def self.install
-   system "make -j#{CREW_NPROC} DESTDIR=#{CREW_DEST_DIR} install PREFIX=#{CREW_PREFIX}"
+    system "make install PREFIX=#{CREW_PREFIX} COMPONENT_TYPE=lib-shared DESTDIR=#{CREW_DEST_DIR}"
+    case ARCH
+    when 'x86_64'
+      Dir.chdir CREW_DEST_PREFIX do
+        FileUtils.mv 'lib/', 'lib64/'
+      end
+    end
   end
 end
