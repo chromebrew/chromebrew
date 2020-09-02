dbe563b8f19087fc9c6a01c3de9aeed42ba126cc
Vincent GIRARD <vincent@heliosphere.fr>
2020-09-02 15:26:00 +0200

    [poppler] Update to 20.08.0

--- packages/poppler.rb
+++ packages/poppler.rb
@@ -3,22 +3,14 @@ require 'package'
 class Poppler < Package
   description 'Poppler is a PDF rendering library based on the xpdf-3.0 code base.'
   homepage 'https://poppler.freedesktop.org/'
-  version '0.86.1'
+  version '20.08.0'
   compatibility 'all'
-  source_url 'https://poppler.freedesktop.org/poppler-0.86.1.tar.xz'
-  source_sha256 'af630a277c8e194c31339c5446241834aed6ed3d4b4dc7080311e51c66257f6c'
+  source_url 'https://poppler.freedesktop.org/poppler-20.08.0.tar.xz'
+  source_sha256 'ae65fef04bbf63259a6352e7b620719115d4fb97f5079b0b8b00a8eb0c86eca5'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/poppler-0.86.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/poppler-0.86.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/poppler-0.86.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/poppler-0.86.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'd7a3f4fbf1f47c73958e64b3ec1009865fe291f651de0dd6c5c25283a3bcc77d',
-     armv7l: 'd7a3f4fbf1f47c73958e64b3ec1009865fe291f651de0dd6c5c25283a3bcc77d',
-       i686: '1a79d3f532ba1d3d26da035676516cd7c1a184a3f703438ea615fa6d498416d4',
-     x86_64: '214addecd68af303a0e158f5e0ccd4910aea8a0073340adc9098d1307a56bc36',
   })
 
   depends_on 'boost'
