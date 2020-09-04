ad326dcc55464cd42b541325f2296a6e4156c51d
Ed Reel <edreel@gmail.com>
2020-09-03 22:03:07 -0500

    Add pre-built binaries for elogind and shadow

--- packages/elogind.rb
+++ packages/elogind.rb
@@ -8,6 +8,19 @@ class Elogind < Package
   source_url 'https://github.com/elogind/elogind/archive/v243.4.tar.gz'
   source_sha256 'f1098745863138e6270ea22e78a39baef9a0356b48246c5a53b34211992dc7db'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'cb9cee0ff417657040c5c02ebe618eaf972e5cb374de641db7e66e8754673fc3',
+     armv7l: 'cb9cee0ff417657040c5c02ebe618eaf972e5cb374de641db7e66e8754673fc3',
+       i686: '9dbcdc416612a3cc32d71c8120b3c38d6d37b8f75011b4a8cfe04003fe2eb737',
+     x86_64: 'e76b69346288beef9a595b90b9f7dd30424339b9c4e906e928ff4bd8cb5f82fa',
+  })
+
   depends_on 'eudev'
   depends_on 'libcap'
   depends_on 'libseccomp'
@@ -20,11 +33,12 @@ class Elogind < Package
   depends_on 'glib'
 
   def self.build
-    system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} _build -Dcgroup-controller=elogind -Dman=false \ 
--Ddefault-hierarchy=legacy -Ddefault-kill-user-processes=false -Dhalt-path=/sbin/halt -Drootlibexecdir=#{CREW_PREFIX}/libexec/elogind -Dreboot-path=/sbin/reboot"
+    system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} -Dcgroup-controller=elogind \
+-Dman=false -Ddefault-hierarchy=legacy -Ddefault-kill-user-processes=false -Dhalt-path=/sbin/halt \
+-Drootlibexecdir=#{CREW_PREFIX}/libexec/elogind -Dreboot-path=/sbin/reboot _build"
     system 'ninja -v -C _build'
   end
-  
+
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} ninja -C _build install"
   end
--- packages/shadow.rb
+++ packages/shadow.rb
@@ -8,14 +8,29 @@ class Shadow < Package
   source_url 'https://github.com/shadow-maint/shadow/releases/download/4.8.1/shadow-4.8.1.tar.xz'
   source_sha256 'a3ad4630bdc41372f02a647278a8c3514844295d36eefe68ece6c3a641c1ae62'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/shadow-4.8.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/shadow-4.8.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/shadow-4.8.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/shadow-4.8.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '52d073915c219d4402907e47a32e89c1f61585c36d4cb608fcb6bcb03ed4f44e',
+     armv7l: '52d073915c219d4402907e47a32e89c1f61585c36d4cb608fcb6bcb03ed4f44e',
+       i686: 'e7559741bb901a45e9d98fd989750d6df0501611e81fa56af83014016df7e29a',
+     x86_64: '562849baf46b40c5e015c369ae77cc2f92266309acbc2b0cc1e87c0f0fa7a72b',
+  })
+
   depends_on 'openpam'
 
   def self.build
-      system "./configure #{CREW_OPTIONS} --bindir=#{CREW_PREFIX}/bin --sbindir=#{CREW_PREFIX}/bin --enable-shared \ 
---disable-static --with-libpam --without-selinux --with-acl --with-attr --without-su --disable-nls --enable-subordinate-ids --disable-account-tools-setuid"
-      system "make"
+    system "./configure #{CREW_OPTIONS} --bindir=#{CREW_PREFIX}/bin --sbindir=#{CREW_PREFIX}/bin \
+--enable-shared --disable-static --with-libpam --without-selinux --with-acl --with-attr --without-su \
+--disable-nls --enable-subordinate-ids --disable-account-tools-setuid"
+    system 'make'
   end
+
   def self.install
-      system "make install DESTDIR=#{CREW_DEST_DIR}"
+    system "make install DESTDIR=#{CREW_DEST_DIR}"
   end
 end
