c829689ad06f3b7c7d222a0d61ad4aac95d4e0f1
Vincent GIRARD <vincent@heliosphere.fr>
2020-09-07 16:16:41 +0200

    Update packages

--- packages/imagemagick7.rb
+++ packages/imagemagick7.rb
@@ -3,12 +3,12 @@ require 'package'
 class Imagemagick7 < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  version '7.0.10-28'
+  version '7.0.10-29'
   compatibility 'aarch64,armv7l,x86_64'
   case ARCH
   when 'aarch64', 'armv7l', 'x86_64'
-    source_url 'https://imagemagick.org/download/ImageMagick-7.0.10-28.tar.xz'
-    source_sha256 '6a6b32f04fa508f198eb0e3677d95729121796cbe156add05d3eb8070c7569b0'
+    source_url 'https://imagemagick.org/download/ImageMagick-7.0.10-29.tar.xz'
+    source_sha256 '0d2b0da5bcc4b4de82d25dc641a51c36bc59b73c847e0407468373a4bb989779'
     depends_on 'flif'
     depends_on 'freeimage'
     depends_on 'freetype'
@@ -45,23 +45,25 @@ class Imagemagick7 < Package
   end
 
   def self.build
-    FileUtils.ln_s "#{CREW_PREFIX}/include/locale.h", "#{CREW_PREFIX}/include/xlocale.h" 
-    system "CFLAGS=' -I#{CREW_PREFIX}/include/gdk-pixbuf-2.0 -I#{CREW_PREFIX}/include/c++/v1/support/xlocale' \
-           ./configure \
-           --prefix=#{CREW_PREFIX} \
-           --libdir=#{CREW_LIB_PREFIX} \
-           --disable-dependency-tracking \
-           --with-windows-font-dir=#{CREW_PREFIX}/share/fonts/truetype/msttcorefonts \
-           --with-jemalloc \
-           --with-modules \
-           --enable-hdri \
-           --with-perl \
-           --with-rsvg \
-           --with-x"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/include"
+    FileUtils.ln_s "#{CREW_PREFIX}/include/locale.h", "#{CREW_PREFIX}/include/xlocale.h", force: true
+    system './configure',
+           "CFLAGS=' -I#{CREW_PREFIX}/include/gdk-pixbuf-2.0 -I#{CREW_PREFIX}/include/c++/v1/support/xlocale'",
+           "#{CREW_OPTIONS}",
+           "--with-windows-font-dir=#{CREW_PREFIX}/share/fonts/truetype/msttcorefonts",
+           '--disable-dependency-tracking',
+           '--with-jemalloc',
+           '--with-modules',
+           '--enable-hdri',
+           '--with-perl',
+           '--with-rsvg',
+           '--with-x'
     system 'make'
   end
 
   def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/include"
+    FileUtils.ln_s "#{CREW_PREFIX}/include/locale.h", "#{CREW_PREFIX}/include/xlocale.h", force: true
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     FileUtils.ln_s "#{CREW_LIB_PREFIX}/libMagickWand-7.Q16HDRI.so.6", "#{CREW_DEST_LIB_PREFIX}/libMagickWand-7.Q16.so.6"
     FileUtils.ln_s "#{CREW_LIB_PREFIX}/libMagickCore-7.Q16HDRI.so.6", "#{CREW_DEST_LIB_PREFIX}/libMagickCore-7.Q16.so.6"
