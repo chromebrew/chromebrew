5e62d00c0732aafb5dfeb62be3500c7759d69d68
Ed Reel <edreel@gmail.com>
2020-09-07 22:59:04 -0500

    Add pre-built binaries for docbook_xsl, gemacs and picom

--- packages/gemacs.rb
+++ packages/gemacs.rb
@@ -9,8 +9,14 @@ class Gemacs < Package
   source_sha256 '4a4c128f915fc937d61edfc273c98106711b540c9be3cd5d2e2b9b5b2f172e41'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-27.1-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-27.1-1-chromeos-armv7l.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-27.1-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '5d238f878cd80fa41dc83a3abc4a956466c08b336c2e01025a1bc2eb5532be72',
+     armv7l: '5d238f878cd80fa41dc83a3abc4a956466c08b336c2e01025a1bc2eb5532be72',
+     x86_64: '427c6f1c7143c449f7d2e53d82c82d191e87fed49d2e921916fc7aef2584ac0d',
   })
 
   depends_on 'giflib'
