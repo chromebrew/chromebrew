1cf6b8d240057ed1359c54300f623c5ee5882ae8
Ed Reel <edreel@gmail.com>
2020-09-10 23:49:24 -0500

    Add pre-built binaries for enlightenment and dependencies

--- packages/enlightenment.rb
+++ packages/enlightenment.rb
@@ -8,6 +8,19 @@ class Enlightenment < Package
   source_url 'https://download.enlightenment.org/rel/apps/enlightenment/enlightenment-0.24.1.tar.xz'
   source_sha256 'aee2b6178c918d71ebe661129f4008d773e70e5784651dadbcf56eec0a6d4a09'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/enlightenment-0.24.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/enlightenment-0.24.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/enlightenment-0.24.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/enlightenment-0.24.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '128eacd09df52d7aafb76a53387591660f12a0e7d5b52ee20f0c0d15940f624b',
+     armv7l: '128eacd09df52d7aafb76a53387591660f12a0e7d5b52ee20f0c0d15940f624b',
+       i686: 'fe5bce7195a74539cfbb4391bdf65fcd57b9970c018d40fa47a73953311174af',
+     x86_64: 'caff7dce9419868c7c3b23729d08ac7774163a16c0698dfcf8c2260c9ec36de6',
+  })
+
   depends_on 'libefl'
   depends_on 'openpam'
   depends_on 'xkeyboard_config'
@@ -19,7 +32,7 @@ class Enlightenment < Package
     system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} _build -Dbluez4=false -Dsystemd=false"
     system 'ninja -v -C _build'
   end
-  
+
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} ninja -C _build install"
   end
--- packages/terminology.rb
+++ packages/terminology.rb
@@ -8,6 +8,19 @@ class Terminology < Package
   source_url 'https://download.enlightenment.org/rel/apps/terminology/terminology-1.8.0.tar.xz'
   source_sha256 'c6f5b003412f25507277702cabe1a11d7190971343c1d6030aa7d3fe5b45765f'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/terminology-1.8.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/terminology-1.8.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/terminology-1.8.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/terminology-1.8.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'be229db30e4c8c423bd89adb118e1e3d7958fe718ca5ecc149bfc395939b0523',
+     armv7l: 'be229db30e4c8c423bd89adb118e1e3d7958fe718ca5ecc149bfc395939b0523',
+       i686: 'd9d03f10e5522feec35fbb4ea2c789eb805a483403ceafacfc0ab6dcda3c36c6',
+     x86_64: '8d975f5df0ff60a90073b858141b042c0b5abe034b84c862b116e4ce0e767fdb',
+  })
+
   depends_on 'desktop_file_utilities'
   depends_on 'xdg_utils'
   depends_on 'libefl'
@@ -17,7 +30,7 @@ class Terminology < Package
     system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} _build"
     system 'ninja -v -C _build'
   end
-  
+
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} ninja -C _build install"
   end
