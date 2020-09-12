b843de6a196d5c9f37cc7c1d37b4fa2cc6c05fdd
vincowl <vincent@heliosphere.fr>
2020-09-13 01:53:47 +0200

    [sass] Add new package (#4307)
    * [sass] Add new package
    
    * Correction to make it work as sass ruby's gem
    
    * update sass steps

---
+++ packages/sass.rb
@@ -0,0 +1,30 @@
+require 'package'
+
+class Sass < Package
+  description 'A Dart implementation of Sass. Sass makes CSS fun again.'
+  homepage 'https://sass-lang.com/'
+  version '1.26.10'
+  compatibility 'all'
+  source_url 'https://github.com/sass/dart-sass/archive/1.26.10.tar.gz'
+  source_sha256 '9131be72e3eb6d32265e354d6da9f9162d9b28ed5b4910f03744c31509be894f'
+
+  binary_url ({
+  })
+  binary_sha256 ({
+  })
+
+  depends_on 'dart'
+
+  def self.build
+    system "echo '#!/bin/bash' >> sass" 
+    system "echo 'dart #{CREW_PREFIX}/share/dart-sass/bin/sass.dart $@' >> sass"
+  end
+
+  def self.install
+    system 'pub get'
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/dart-sass"
+    FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/share/dart-sass"
+    system "install -Dm755 sass #{CREW_DEST_PREFIX}/bin/sass"
+    FileUtils.ln_sf "#{CREW_PREFIX}/bin/sass", "#{CREW_PREFIX}/bin/scss"
+  end
+end
