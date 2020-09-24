bd0f2f488e7e1ca9dfcf7404eaac7a15fe8ce933
Ed Reel <edreel@gmail.com>
2020-09-24 13:50:27 -0500

    Update libtiff from 4.0.9 to 4.1.0 (#4336)

--- packages/libtiff.rb
+++ packages/libtiff.rb
@@ -3,39 +3,32 @@ require 'package'
 class Libtiff < Package
   description 'LibTIFF provides support for the Tag Image File Format (TIFF), a widely used format for storing image data.'
   homepage 'http://www.libtiff.org/'
-  version '4.0.9'
+  version '4.1.0'
   compatibility 'all'
-  source_url 'ftp://download.osgeo.org/libtiff/tiff-4.0.9.tar.gz'
-  source_sha256 '6e7bdeec2c310734e734d19aae3a71ebe37a4d842e0e23dbb1b8921c0026cfcd'
+  source_url 'https://download.osgeo.org/libtiff/tiff-4.1.0.tar.gz'
+  source_sha256 '5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.0.9-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.0.9-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.0.9-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.0.9-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '66ae653bdf59f79bfc0456f0f132ca052d9625fc796d65c4ff18f42c30f3884d',
-     armv7l: '66ae653bdf59f79bfc0456f0f132ca052d9625fc796d65c4ff18f42c30f3884d',
-       i686: '2eaa114ff1d6010921631b5b498f11e0d49fe1c11986c09a8826f832c7b321e0',
-     x86_64: '256279df77d0772be848deb674f865b1c2359c018562ceb8bb6cbd06f8f28a2e',
+    aarch64: '130c8ef969b06b24f0fd67b4b28b8932a7c96f5326e502d760efaaf4bf7bc850',
+     armv7l: '130c8ef969b06b24f0fd67b4b28b8932a7c96f5326e502d760efaaf4bf7bc850',
+       i686: '596e5034c8bd7cbecd28d6aa00aa0919b97a1cdc27325b4260015047d761a5cf',
+     x86_64: 'c25ec510ec23d166ab0edfa20984b1df4325b82d7e72405d483d173480586910',
   })
 
   def self.build
-    system "./configure \
-            CFLAGS=\" -fPIC\" \
-            --prefix=#{CREW_PREFIX} \
-            --libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+    system "./configure CFLAGS=' -fPIC' #{CREW_OPTIONS}"
+    system 'make'
   end
 
-  depends_on 'compressdoc' => :build
   depends_on 'libjpeg'
-  depends_on 'zlibpkg'
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-    system "compressdoc --gzip -9 #{CREW_DEST_PREFIX}/share/man/man1"
-    system "compressdoc --gzip -9 #{CREW_DEST_PREFIX}/share/man/man3"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
