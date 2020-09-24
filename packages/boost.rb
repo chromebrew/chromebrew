74e28641f6051a79e10fc4efab3fad7f8a8967e2
Ed Reel <edreel@gmail.com>
2020-09-24 13:48:14 -0500

    Update boost from 1.71.0 to 1.74.0 (#4327)

--- packages/boost.rb
+++ packages/boost.rb
@@ -3,22 +3,22 @@ require 'package'
 class Boost < Package
   description 'Boost provides free peer-reviewed portable C++ source libraries.'
   homepage 'https://www.boost.org/'
-  version '1.71.0'
+  version '1.74.0'
   compatibility 'all'
-  source_url 'https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2'
-  source_sha256 'd73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee'
+  source_url 'https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.bz2'
+  source_sha256 '83bfc1507731a0906e387fc28b7ef5417d591429e51e788417fe9ff025e116b1'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.71.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.74.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.74.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.74.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/boost-1.74.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'e0d2c1c299cff64f8d7f8de806cc8bfb2dce0be992d037cf17967bf4e9795e46',
-     armv7l: 'e0d2c1c299cff64f8d7f8de806cc8bfb2dce0be992d037cf17967bf4e9795e46',
-       i686: 'f000df2868e43bcab73d09d6fc7b161ef8b4ae433809dec3d8943b375aa36982',
-     x86_64: '7d028e7fab5e95a2cbc74eb8b86de0d77619f652c83e687b4ae0e491baed3e5d',
+    aarch64: '3ae8405849a0a624682f230a8f4d8135c4eca3657246bf5dd8fdbb79461bce1d',
+     armv7l: '3ae8405849a0a624682f230a8f4d8135c4eca3657246bf5dd8fdbb79461bce1d',
+       i686: 'de792b84f1a2629a58a1bd9e4169271e1d856ba3cf7247ea67283dd2a14c7652',
+     x86_64: 'e1e1faca0d6458b83e2338dd9786f16846607a2a30d41ebc6a2fc3545bc972ff',
   })
 
   def self.build
