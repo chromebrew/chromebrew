f924f214655034e4a4448332f0b4e0fd174b73a8
Chris Thurber <skiman6010@gmail.com>
2020-10-02 17:19:16 -0400

    Qemu 4.2.0 update (#4358)

--- packages/qemu.rb
+++ packages/qemu.rb
@@ -3,22 +3,16 @@ require 'package'
 class Qemu < Package
   description 'QEMU is a generic and open source machine emulator and virtualizer.'
   homepage 'http://www.qemu.org/'
-  version '3.1.0'
+  version '4.2.0'
   compatibility 'all'
-  source_url 'https://download.qemu.org/qemu-3.1.0.tar.xz'
-  source_sha256 '6a0508df079a0a33c2487ca936a56c12122f105b8a96a44374704bef6c69abfc'
+  source_url 'https://download.qemu.org/qemu-4.2.0.tar.xz'
+  source_sha256 'd3481d4108ce211a053ef15be69af1bdd9dde1510fda80d92be0f6c3e98768f0'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qemu-3.1.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qemu-3.1.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qemu-3.1.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qemu-3.1.0-chromeos-x86_64.tar.xz',
+
   })
   binary_sha256 ({
-    aarch64: '23aa474b3f63e9f5825a77323b4bf7b5b710022e055ea97c524fa3d32cfc36cb',
-     armv7l: '23aa474b3f63e9f5825a77323b4bf7b5b710022e055ea97c524fa3d32cfc36cb',
-       i686: '600b31eaeb4a55c8fe73a1a8e2645ad44bfa166c34893a24383acd76f25c1579',
-     x86_64: 'fe691d318fb71461e3ce85a733165bea3d56749bf4fa572185dd086c9d7e97bd',
+
   })
 
   depends_on 'bz2'
