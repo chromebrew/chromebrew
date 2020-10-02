433cf3627ea9177fe393641fcd360f2c5b92e158
Ed Reel <edreel@gmail.com>
2020-10-02 00:12:06 -0500

    Add pre-built binaries for brotli and libmbedtls (#4353)

--- packages/brotli.rb
+++ packages/brotli.rb
@@ -9,8 +9,16 @@ class Brotli < Package
   source_sha256 'a0bfaf49d8d35262ef1d1e617486b061f47c634721c345051bf8d9fb806f3bb9'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/brotli-1.0.8-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/brotli-1.0.8-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/brotli-1.0.8-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/brotli-1.0.8-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '636a5bb46059311e280f1828aa032e2d2bad83905b124549159b73e279856688',
+     armv7l: '636a5bb46059311e280f1828aa032e2d2bad83905b124549159b73e279856688',
+       i686: 'c16396d2d9a4a000b360fd6f096e4f0053abafb7bf1f8c84766527ea7a4b074f',
+     x86_64: '921fc1be57c195176d500f82bf96a52566824e62036c329ced629a0177c7c9d1',
   })
 
   def self.build
