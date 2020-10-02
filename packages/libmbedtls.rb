ae11566461d3e1d898ce43cff17e1087c2c037b0
Ed Reel <edreel@gmail.com>
2020-10-02 00:12:06 -0500

    Add pre-built binaries for brotli and libmbedtls (#4353)

--- packages/libmbedtls.rb
+++ packages/libmbedtls.rb
@@ -8,6 +8,19 @@ class Libmbedtls < Package
   source_url 'https://github.com/ARMmbed/mbedtls/archive/v2.16.8.tar.gz'
   source_sha256 'fe9e3b15c3375943bdfebbbb20dd6b4f1147b3b5d926248bd835d73247407430'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libmbedtls-2.16.8-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libmbedtls-2.16.8-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libmbedtls-2.16.8-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libmbedtls-2.16.8-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'fa1207538c87a278987bd49aa789d9a064745eb17af6e0bca986cab290f7c8e4',
+     armv7l: 'fa1207538c87a278987bd49aa789d9a064745eb17af6e0bca986cab290f7c8e4',
+       i686: '82f4fe07be9599125bbce078e3ff8a5c3f2c96691270abc60cf97fed79879e45',
+     x86_64: '58e98d4edf8ff733228ff2499e7faf33bfc1d90bc6c834e8b1b78b7c369b85f1',
+  })
+
   def self.build
     Dir.mkdir 'build'
     Dir.chdir 'build' do
