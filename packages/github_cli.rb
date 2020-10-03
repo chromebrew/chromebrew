7c1873964234938dcc0196a4999a1e73f82e140a
Ed Reel <edreel@gmail.com>
2020-10-03 01:19:06 -0500

    Add pre-built binaries for github_cli and heroku

--- packages/github_cli.rb
+++ packages/github_cli.rb
@@ -9,9 +9,17 @@ class Github_cli < Package
   source_sha256 'e3d1c341829f5b885dce9aa2bf4bc84db48072752250f6fdb2d62903caf07cfb'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-1.0.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-1.0.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-1.0.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/github_cli-1.0.0-chromeos-x86_64.tar.xz',
 
   })
   binary_sha256 ({
+    aarch64: 'f105f30044c96869f9f2c199a6907d7518ce69a6e7dc8de3059f2c2e1716d1e2',
+     armv7l: 'f105f30044c96869f9f2c199a6907d7518ce69a6e7dc8de3059f2c2e1716d1e2',
+       i686: 'b28ff38226112dcdb6c37b46fe8c84bbf28d709ae6d68ba102d2de4b72cf4a42',
+     x86_64: '4949a7f29a571f59c8262f7b007a02cc238900d9999f10ad2941975fc2e3878b',
 
   })
 
--- packages/heroku.rb
+++ packages/heroku.rb
@@ -9,8 +9,14 @@ class Heroku < Package
   source_sha256 '565e2a42a5095da43466465f29455ea646b235742f16b667d58da0a312c0fe8a'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-7.44.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-7.44.0-chromeos-armv7l.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/heroku-7.44.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '60c419195c5a9c40b9bccc18d9da7d50a122e6609784a85c3fd5ea9f882bcf26',
+     armv7l: '60c419195c5a9c40b9bccc18d9da7d50a122e6609784a85c3fd5ea9f882bcf26',
+     x86_64: 'fa9b538dd683248d3e892f8a74c16c2a124dc189e4b9b0b18c17e57897b93096',
   })
 
   depends_on 'yarn' => :build
