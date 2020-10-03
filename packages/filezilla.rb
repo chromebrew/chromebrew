ff8cf95448f0b3c4172deb59383831994f2c58c4
vincowl <vincent@heliosphere.fr>
2020-10-03 20:25:07 +0200

    [filezilla] Update to 3.50.0 (#4363)

--- packages/filezilla.rb
+++ packages/filezilla.rb
@@ -3,22 +3,14 @@ require 'package'
 class Filezilla < Package
   description 'FileZilla Client is a free FTP solution.'
   homepage 'https://filezilla-project.org/'
-  version '3.49.1'
+  version '3.50.0'
   compatibility 'all'
-  source_url 'https://download.filezilla-project.org/client/FileZilla_3.49.1_src.tar.bz2'
-  source_sha256 '4ecc8c0cf1a790d17ba1daa3abdeeb999809548b20a804172ae0f80dd8e5b3b6'
+  source_url 'https://download.filezilla-project.org/client/FileZilla_3.50.0_src.tar.bz2'
+  source_sha256 'e0db87269ca5208aad14a02415337b4f9efe3c49c2d4d14e66e921c78a135c10'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.49.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.49.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.49.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.49.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'ed0be66719af2b0c5464df895c42145685e7de12210c2a8a946d2fcc76cc7d61',
-     armv7l: 'ed0be66719af2b0c5464df895c42145685e7de12210c2a8a946d2fcc76cc7d61',
-       i686: 'e0186c7d4b3668eea3b2beac65830a9787ed20ed2498bd72e140fb360bc7a801',
-     x86_64: '74c0a8de788b4ea7dab339d8d856ba12ce3745a2b989067df0fbe3f0011d222c',
   })
 
   depends_on 'dbus'
--- packages/libfilezilla.rb
+++ packages/libfilezilla.rb
@@ -3,22 +3,14 @@ require 'package'
 class Libfilezilla < Package
   description 'libfilezilla is a small and modern C++ library, offering some basic functionality to build high-performing, platform-independent programs.'
   homepage 'https://lib.filezilla-project.org/'
-  version '0.23.0'
+  version '0.24.1'
   compatibility 'all'
-  source_url 'https://download.filezilla-project.org/libfilezilla/libfilezilla-0.23.0.tar.bz2'
-  source_sha256 'adeac127fbd1714b22c828cdd27fc6c92e09fd1abcc96684bf30535fb8226852'
+  source_url 'https://download.filezilla-project.org/libfilezilla/libfilezilla-0.24.1.tar.bz2'
+  source_sha256 'fdd5b4ee1916af7b1d402e7dd467f05df7624bb65fb95a0875a03712ecc2d6fd'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libfilezilla-0.23.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libfilezilla-0.23.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libfilezilla-0.23.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libfilezilla-0.23.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'ca803dcb4dbc4b6d946c1e3b0110b9a2e11b5bf82d55f2fa9cdea40622927c22',
-     armv7l: 'ca803dcb4dbc4b6d946c1e3b0110b9a2e11b5bf82d55f2fa9cdea40622927c22',
-       i686: '56e280e263f0187c21b6f4f834790867fb37fef36e93adedeab5dbc3c6762b79',
-     x86_64: '1215012524a037640996fc6d72936dbfcb7411f5cdedd289b6c9baf807a5c617',
   })
 
   def self.build
