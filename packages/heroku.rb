ddc7af0595d9663f5aecb5032d7b58fd7d289868
Ed Reel <edreel@gmail.com>
2020-10-03 00:34:42 -0500

    Remove i686 support for heroku

--- packages/heroku.rb
+++ packages/heroku.rb
@@ -4,18 +4,16 @@ class Heroku < Package
   description 'The Heroku Command Line Interface (CLI), formerly known as the Heroku Toolbelt, is a tool for creating and managing Heroku apps from the command line / shell of various operating systems.'
   homepage 'https://devcenter.heroku.com/articles/heroku-cli'
   version '7.44.0'
-  compatibility 'all'
+  compatibility 'aarch64,armv7l,x86_64'
   source_url 'https://github.com/heroku/cli/archive/v7.44.0.tar.gz'
   source_sha256 '565e2a42a5095da43466465f29455ea646b235742f16b667d58da0a312c0fe8a'
 
   binary_url ({
-
   })
   binary_sha256 ({
-
   })
 
-  depends_on 'yarn'
+  depends_on 'yarn' => :build
 
   def self.build
     system 'yarn install'
