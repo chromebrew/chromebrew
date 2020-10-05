fd4a0ee9f2ba38240dac725898d0d0845ebdda7b
Vincent GIRARD <vincent@heliosphere.fr>
2020-10-05 16:11:21 +0200

    nftables and libnftnl missing compatibility field

--- packages/libnftnl.rb
+++ packages/libnftnl.rb
@@ -3,7 +3,8 @@ require 'package'
 class Libnftnl < Package
   description 'libnftnl is a userspace library providing a low-level netlink programming interface (API) to the in-kernel nf_tables subsystem.'
   homepage 'https://netfilter.org/projects/libnftnl/'
-  version '1.1.7'
+  compatibility 'all'
+  version '1.1.7-1'
   source_url 'https://netfilter.org/projects/libnftnl/files/libnftnl-1.1.7.tar.bz2'
   source_sha256 '20dbc13f11004aea2c9e479cfb90359cb11fe3446c3140811c18e4ec1648ed8f'
 
