accb6f18ade731316fcf6507ac996bc4e48a4f00
satmandu <satadru@umich.edu>
2020-10-04 23:55:25 -0400

    Curl update to 7.72

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,23 +3,11 @@ require 'package'
 class Curl < Package
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.haxx.se/'
-  version '7.71.1'
+  version '7.72.0'
   compatibility 'all'
-  source_url 'https://curl.haxx.se/download/curl-7.71.1.tar.xz'
-  source_sha256 '40f83eda27cdbeb25cd4da48cefb639af1b9395d6026d2da1825bf059239658c'
+  source_url 'https://curl.haxx.se/download/curl-7.72.0.tar.xz'
+  source_sha256 '0ded0808c4d85f2ee0db86980ae610cc9d165e9ca9da466196cc73c346513713'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.71.1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '58505d5a912a20dcc1b09472513e83bcff81fa0aa5492824a928d8aad1a6f588',
-     armv7l: '58505d5a912a20dcc1b09472513e83bcff81fa0aa5492824a928d8aad1a6f588',
-       i686: '545849d0ab122536a49978919b4ba1455a3f3af68fff85ebe3ad603111e93e56',
-     x86_64: 'c31dd8206122e7f66211824098ae9c3005cb3847178d76c24af3b7393d2f7e1f',
-  })
 
   depends_on 'groff' => :build
   depends_on 'brotli'
