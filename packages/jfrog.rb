dd9c0c1379476f40580b8b4e496a690a32b2c160
masseyzachery57 <55512924+masseyzachery57@users.noreply.github.com>
2020-10-08 10:47:35 -0400

    Update jfrog.rb (#4399)
    updated version and hash, needs binaries

--- packages/jfrog.rb
+++ packages/jfrog.rb
@@ -3,22 +3,16 @@ require 'package'
 class Jfrog < Package
   description 'JFrog CLI is a compact and smart client that provides a simple interface that automates access to Artifactory, Bintray and Mission Control through their respective REST APIs.'
   homepage 'https://github.com/JFrogDev/jfrog-cli-go'
-  version '1.23.1'
+  version '1.39.5'
   compatibility 'all'
-  source_url 'https://github.com/jfrog/jfrog-cli-go/archive/1.23.1.tar.gz'
-  source_sha256 'a5200552acdf55592a6972900f2f658cb25dd6508793bc52fb3386a310a75414'
+  source_url 'https://github.com/jfrog/jfrog-cli-go/archive/1.39.5.tar.gz'
+  source_sha256 '75a0f9bfd3170d03c542b62442761822ff2e4a7a279406d9b2ee5eeee888813f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/jfrog-1.23.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/jfrog-1.23.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/jfrog-1.23.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/jfrog-1.23.1-chromeos-x86_64.tar.xz',
+    
   })
   binary_sha256 ({
-    aarch64: '2a34fcd0e785f273a6e877e0f157636b58a0677b1785f5a976450c31b4cf3ac5',
-     armv7l: '2a34fcd0e785f273a6e877e0f157636b58a0677b1785f5a976450c31b4cf3ac5',
-       i686: '4f56688d44178682c7b0edf56a92ccad324ae771a84a562ae019e65f46f4096b',
-     x86_64: '7c5bb6f662dc8ea1ae59ad19926d2eab9590ccefd12b131cbc65a5081dbb1ad3',
+    
   })
 
   depends_on 'go' => :build
