221b68b88cff85084e6be78233a3c0b846b5f134
masseyzachery57 <55512924+masseyzachery57@users.noreply.github.com>
2020-10-08 10:44:53 -0400

    Update lynis.rb (#4395)
    updated version and hash, needs binaries

--- packages/lynis.rb
+++ packages/lynis.rb
@@ -3,22 +3,16 @@ require 'package'
 class Lynis < Package
   description 'Lynis is a security auditing tool for UNIX derivatives like Linux, macOS, BSD, Solaris, AIX, and others.'
   homepage 'https://cisofy.com/download/lynis/'
-  version '2.5.7'
+  version '3.0.0'
   compatibility 'all'
-  source_url 'https://cisofy.com/files/lynis-2.5.7.tar.gz'
-  source_sha256 '95a6b8249b729d6c431377015c53724d3d267b74c2c9e5596a4d1c59c15df64c'
+  source_url 'https://cisofy.com/files/lynis-3.0.0.tar.gz'
+  source_sha256 '7da40f8c52d43b19eb9dc66aaece9c756c2ec581609ffce1a2c510cd99af38e1'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/lynis-2.5.7-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/lynis-2.5.7-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/lynis-2.5.7-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/lynis-2.5.7-chromeos-x86_64.tar.xz',
+    
   })
   binary_sha256 ({
-    aarch64: '2e12dd40c61fff75b1c32c6d3e9dee10458183ba39537578c049e5dff3ed67cc',
-     armv7l: '2e12dd40c61fff75b1c32c6d3e9dee10458183ba39537578c049e5dff3ed67cc',
-       i686: 'aab3f5715cfa6c9b944b1ce88ef3d4603e84c9bc24d19bd6e126b892eb862246',
-     x86_64: '4c728a9fd76868b3ea84e04cd7ae0ad88d68a59d09d6374cff36557a8a87061b',
+    
   })
 
   def self.install
