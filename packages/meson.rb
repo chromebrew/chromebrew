36ce3f867af5936f1fcea6077486e555117fee93
Ed Reel <edreel@gmail.com>
2020-10-08 12:14:17 -0500

    Update core packages cmake, curl, git, libressl, meson, python3 and ruby (#4392)
    Add curl and dependencies to install.sh
    
    Add new packages to core_packages.txt

--- packages/meson.rb
+++ packages/meson.rb
@@ -8,6 +8,19 @@ class Meson < Package
   source_url 'https://github.com/mesonbuild/meson/archive/0.55.3.tar.gz'
   source_sha256 '2b276df50c5b13ccdbfb14d3333141e9e7985aca31b60400b3f3e0be2ee6897e'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.55.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.55.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.55.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.55.3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'dc4075b1285709e2a3701d906a5096e7c6e721f0f1998713bec433676f5671d0',
+     armv7l: 'dc4075b1285709e2a3701d906a5096e7c6e721f0f1998713bec433676f5671d0',
+       i686: 'ca7b2fb537278a2083c536fd0208edab55adba18b90884dd47c55449c05e6250',
+     x86_64: '79921f5b27571e342c7414e34f15dff0e7a610277ace4242043f50410a363d95',
+  })
+
   depends_on 'ninja'
   depends_on 'setuptools'
 
