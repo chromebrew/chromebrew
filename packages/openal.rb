1956f6971f8d7e76986b62227b3590898e55abce
masseyzachery57 <55512924+masseyzachery57@users.noreply.github.com>
2020-10-08 10:43:34 -0400

    Update openal.rb (#4393)
    updated version and hash, needs binary

--- packages/openal.rb
+++ packages/openal.rb
@@ -3,22 +3,16 @@ require 'package'
 class Openal < Package
   description 'OpenAL Soft is a software implementation of the cross-platform OpenAL 3D audio API appropriate for use with gaming applications and many other types of audio applications.'
   homepage 'https://openal-soft.org/'
-  version '1.20.0'
+  version '1.20.1'
   compatibility 'all'
-  source_url 'https://github.com/kcat/openal-soft/archive/openal-soft-1.20.0.tar.gz'
-  source_sha256 'ea1582337ce0344754cc6742b9a5da248fb971fb6fbcc592f843f765423d9265'
+  source_url 'https://github.com/kcat/openal-soft/archive/openal-soft-1.20.1.tar.gz'
+  source_sha256 'c32d10473457a8b545aab50070fe84be2b5b041e1f2099012777ee6be0057c13'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/openal-1.20.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/openal-1.20.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/openal-1.20.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/openal-1.20.0-chromeos-x86_64.tar.xz',
+    
   })
   binary_sha256 ({
-    aarch64: 'eb94add03a88623b6915da3b531fea73b947c4caf49d89582a90737c6282ea53',
-     armv7l: 'eb94add03a88623b6915da3b531fea73b947c4caf49d89582a90737c6282ea53',
-       i686: 'e75fb5996dedacf7125def831f5ba8483de171d0b7061993acb6032508de94a1',
-     x86_64: '2bd3bff607f873b741093e459e80f0fe83a039fbcfbe4f45f7fd16891a46e571',
+   
   })
 
   def self.build
