2099729013d062ce08cf4af713fc2566ce25f3d7
Ed Reel <edreel@gmail.com>
2020-10-12 00:06:49 -0500

    Update yarn from 1.22.5 to 1.22.10

--- packages/yarn.rb
+++ packages/yarn.rb
@@ -3,10 +3,10 @@ require 'package'
 class Yarn < Package
   description 'Yarn is a new package manager for JavaScript and an alternative to npm.'
   homepage 'https://yarnpkg.com/en/'
-  version '1.22.5'
+  version '1.22.10'
   compatibility 'all'
-  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.22.5/yarn-v1.22.5.tar.gz'
-  source_sha256 'c664fb4692e4dfea750a37a533780834b40198c00cef4bbc5e8c14abab2ac141'
+  source_url 'https://github.com/yarnpkg/yarn/releases/download/v1.22.10/yarn-v1.22.10.tar.gz'
+  source_sha256 '7e433d4a77e2c79e6a7ae4866782608a8e8bcad3ec6783580577c59538381a6e'
 
   node_version=`node -v 2> /dev/null`.chomp
   depends_on 'nodebrew' unless "#{node_version}" != ""
