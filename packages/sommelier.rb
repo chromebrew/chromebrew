3b95499e8ddb284e081e2b19823f30ca2afa1296
satmandu <satadru@umich.edu>
2020-10-19 10:51:03 -0400

    Add more documentation

--- packages/sommelier.rb
+++ packages/sommelier.rb
@@ -5,6 +5,7 @@ class Sommelier < Package
   homepage 'https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/vm_tools/sommelier/'
   version '2237607'
   compatibility 'all'
+  # Later version has workaround for debian issue we don't need to bother with.
   #source_url 'https://chromium-review.googlesource.com/changes/chromiumos%2Fplatform2~2333733/revisions/31/patch?zip&path=%2FCOMMIT_MSG'
   source_url 'https://chromium-review.googlesource.com/changes/chromiumos%2Fplatform2~2237607/revisions/10/patch?zip&path=%2FCOMMIT_MSG'
   source_sha256 '0dda9635fe947383e0a4abc573a289acbd81e07115dbb1aaafcc7f3b85a35d6a'
@@ -13,18 +14,18 @@ class Sommelier < Package
 
   depends_on 'mesa'
   depends_on 'xkbcomp'
-  depends_on 'xorg_server'
+  depends_on 'xorg_server' # Miminal xwayland package also works.
   depends_on 'psmisc'
   depends_on 'xsetroot'
   depends_on 'llvm' => ':build'
   depends_on 'meson' => ':build'
 
   def self.build
-  # Using method here to download single file from googlesource in lieu of 
-  # checking out the ChromeOS kernel tree submodule:
-  # https://chromium-review.googlesource.com/c/chromiumos/platform2/+/2237607
+
   system 'curl https://chromium.googlesource.com/chromiumos/platform2/+archive/be4e16feb380360cabbb5d6199a09592ecaf4a42.tar.gz | tar mzx --warning=no-timestamp'
   Dir.chdir ("vm_tools/sommelier") do
+      # Using method here to download single file from googlesource in lieu of 
+      # checking out the ChromeOS kernel tree submodule:
       system 'curl "https://chromium.googlesource.com/chromiumos/third_party/kernel/+/refs/heads/master/include/uapi/linux/virtwl.h?format=TEXT" | base64 --decode > virtwl.h'
       system "sed -i '/virtwl.h/d' sommelier.c"
       system "sed -i '/linux-dmabuf-unstable-v1-client-protocol.h/a #include \"virtwl.h\"' sommelier.c"
@@ -62,11 +63,13 @@ class Sommelier < Package
 				  system "echo 'MESA_LOADER_DRIVER_OVERRIDE=i965' >> .sommelier.env"
 		  end
 	      system "echo '#!/bin/bash' > sommelierd"
-		  # As per https://www.reddit.com/r/chromeos/comments/8r5pvh/crouton_sommelier_openjdk_and_oracle_sql/e0pfknx/
-		  # One needs a second sommelier instance for wayland clients since at some point wl-drm was not implemented
-		  # in ChromeOS's wayland compositor. But the following isn't working, so disable for now.
+              # As per https://www.reddit.com/r/chromeos/comments/8r5pvh/crouton_sommelier_openjdk_and_oracle_sql/e0pfknx/
+              # One needs a second sommelier instance for wayland clients since at some point wl-drm was not implemented
+	      # in ChromeOS's wayland compositor. But the following isn't working, so disable for now.
               system "echo '#sommelier --master --peer-cmd-prefix=/#{ARCH_LIB}/ld-linux-x86-64.so.2 --drm-device=/dev/dri/renderD128 --shm-driver=noop --data-driver=noop --display=wayland-0 --socket=wayland-1 --virtwl-device=/dev/null > /dev/null 2>&1 &' >> sommelierd"
+	      
 	      system "echo 'sommelier -X --x-display=\$DISPLAY --scale=\$SCALE --glamor --drm-device=/dev/dri/renderD128 --virtwl-device=/dev/null --shm-driver=noop --data-driver=noop --display=wayland-0 --xwayland-path=/usr/local/bin/Xwayland --peer-cmd-prefix=/#{ARCH_LIB}/ld-linux-x86-64.so.2 --no-exit-with-child /bin/sh -c \"#{CREW_PREFIX}/etc/sommelierrc\" &>/dev/null' >> sommelierd"
+	      
 	      system "echo '#!/bin/bash' > initsommelier"
 	      system "echo 'SOMM=\$(pidof sommelier 2> /dev/null)' >> initsommelier"
 	      system "echo 'if [ -z \"\$SOMM\" ]; then' >> initsommelier"
@@ -106,8 +109,8 @@ class Sommelier < Package
 	      system "install -Dm755 initsommelier #{CREW_DEST_PREFIX}/bin/initsommelier"
 	      system "install -Dm755 stopsommelier #{CREW_DEST_PREFIX}/bin/stopsommelier"
 	      system "install -Dm644 .sommelier.env #{CREW_DEST_HOME}/.sommelier.env"
-          system "install -Dm755 sommelierrc #{CREW_DEST_PREFIX}/etc/sommelierrc"
-	    end
+              system "install -Dm755 sommelierrc #{CREW_DEST_PREFIX}/etc/sommelierrc"
+      end
     end
   end
 
