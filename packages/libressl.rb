00c0b1db6d554292263df430340d6c77d978a94b
Satadru Pramanik <satadru@gmail.com>
2020-10-20 09:53:36 -0400

    Fix older version symlinks

--- packages/libressl.rb
+++ packages/libressl.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libressl < Package
   description 'LibreSSL is a version of the TLS/crypto stack forked from OpenSSL in 2014, with goals of modernizing the codebase, improving security, and applying best practice development processes.'
   homepage 'https://www.libressl.org/'
-  version '3.1.4'
+  version '3.2.2'
   compatibility 'all'
   source_url 'https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.2.2.tar.gz'
   source_sha256 'a9d1e1d030b8bcc67bf6428b8c0fff14a5602e2236257b9e3d77acaf12e2a7a1'
@@ -24,9 +24,15 @@ class Libressl < Package
     FileUtils.rm_rf "#{CREW_DEST_DIR}/etc/ssl"
 
     # add symlink to libcrypto.so.1.0.0
-    FileUtils.ln_s "#{CREW_LIB_PREFIX}/libcrypto.so.46.0.1", "#{CREW_DEST_LIB_PREFIX}/libcrypto.so.1.0.0"
+    FileUtils.ln_s "#{CREW_LIB_PREFIX}/libcrypto.so.1.0.0", "#{CREW_DEST_LIB_PREFIX}/libcrypto.so.45"
+
+    # add symlink to libcrypto.so.1.0.0
+    #FileUtils.ln_s "#{CREW_LIB_PREFIX}/libcrypto.so.1.0.0", "#{CREW_DEST_LIB_PREFIX}/libcrypto.so.46"
 
     # add symlink to libssl.so.1.0.0
-    FileUtils.ln_s "#{CREW_LIB_PREFIX}/libssl.so.48.0.1", "#{CREW_DEST_LIB_PREFIX}/libssl.so.1.0.0"
+    FileUtils.ln_s "#{CREW_LIB_PREFIX}/libssl.so.1.0.0", "#{CREW_DEST_LIB_PREFIX}/libssl.so.47"
+
+    # add symlink to libssl.so.1.0.0
+    #FileUtils.ln_s "#{CREW_LIB_PREFIX}/libssl.so.1.0.0", "#{CREW_DEST_LIB_PREFIX}/libssl.so.48"
   end
 end
