ca51bec5b168225bae89c2c10fa85ce6bcec927d
Ed Reel <edreel@gmail.com>
2020-10-21 08:18:04 -0500

    Update graphviz from 2.44.0-1 to 2.44.1

--- packages/graphviz.rb
+++ packages/graphviz.rb
@@ -3,22 +3,22 @@ require 'package'
 class Graphviz < Package
   description 'Graphviz is open source graph visualization software.'
   homepage 'https://www.graphviz.org/'
-  version '2.44.0-1'
+  version '2.44.1'
   compatibility 'all'
-  source_url 'https://gitlab.com/graphviz/graphviz/-/archive/2.44.0/graphviz-2.44.0.tar.bz2'
-  source_sha256 'eaff61ce05bf8196c596097bab6310fa6e7e96ee908a199a4bc83fbd5e7082d6'
+  source_url 'https://gitlab.com/graphviz/graphviz/-/archive/2.44.1/graphviz-2.44.1.tar.bz2'
+  source_sha256 '0f8f3fbeaddd474e0a270dc9bb0e247a1ae4284ae35125af4adceffae5c7ae9b'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.0-1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/graphviz-2.44.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '31608ae44ed6971f1db667f6632884d3339cb6ca23726d37054d6530b5f62a74',
-     armv7l: '31608ae44ed6971f1db667f6632884d3339cb6ca23726d37054d6530b5f62a74',
-       i686: 'ced8535b2bf48eb70560d7a504919ec7cd961eabd4a1cad74693bdf9fac230da',
-     x86_64: '010ce4c07b742c9ee8f59abb3672efb69f524a07315169495849f56f70e91608',
+    aarch64: 'da98580f38a3061a10b951562da06f0802dc7524e7d17bbc1b33f57f6913a441',
+     armv7l: 'da98580f38a3061a10b951562da06f0802dc7524e7d17bbc1b33f57f6913a441',
+       i686: '3178c12127d2256d6859975726039c1d81ae3a53f07346dd6d2b1d457889e3e4',
+     x86_64: '42842a1c3fc77efe32ee82cea9edd29c5024019053103347a4d4ff9da5d6fd2f',
   })
 
   depends_on 'libgd'
@@ -41,12 +41,8 @@ class Graphviz < Package
     Dir.chdir 'build' do
       system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     end
-    case ARCH
-    when 'x86_64'
-      Dir.mkdir CREW_DEST_LIB_PREFIX
-      Dir.chdir CREW_DEST_PREFIX do
-        FileUtils.mv Dir.glob('lib/*'), 'lib64/'
-      end
+    Dir.chdir CREW_DEST_PREFIX do
+      FileUtils.mv 'lib', 'lib64' if ARCH == 'x86_64'
     end
   end
 end
