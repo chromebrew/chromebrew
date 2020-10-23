6b40110808b1fbb5b0757ca7a09e7fcc2428ff1d
satmandu <satadru@umich.edu>
2020-10-23 12:26:52 -0400

    Libxrandr update to 1.5.2, use clang linker (#4457)
    * update to 1.5.2, use clang linker
    
    * Use CREW_OPTIONS

--- packages/libxrandr.rb
+++ packages/libxrandr.rb
@@ -3,16 +3,15 @@ require 'package'
 class Libxrandr < Package
   description 'X.org X Resize, Rotate and Reflection extension library'
   homepage 'http://www.x.org'
-  version '1.5.2-0'
+  version '1.5.2-1'
   compatibility 'all'
   source_url 'https://www.x.org/archive/individual/lib/libXrandr-1.5.2.tar.gz'
   source_sha256 '3f10813ab355e7a09f17e147d61b0ce090d898a5ea5b5519acd0ef68675dcf8e'
 
-
-  depends_on 'libxrender'
   depends_on 'libx11'
   depends_on 'libxext'
-  depends_on 'llvm' => ':build'
+  depends_on 'libxrender'
+  depends_on 'llvm' => :build
 
   def self.build
     ENV['CFLAGS'] = "-fuse-ld=lld"
