2c1fdfabf450031ee5e2d648952ebd1bb28cf77d
satmandu <satadru@umich.edu>
2020-10-23 21:34:01 -0400

    Fix MESON_OPTIONS breakage (#4498)

--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -3,7 +3,7 @@ require 'package'
 class Harfbuzz < Package
   description 'HarfBuzz is an OpenType text shaping engine.'
   homepage 'https://www.freedesktop.org/wiki/Software/HarfBuzz/'
-  version '2.7.2'
+  version '2.7.2-1'
   compatibility 'all'
   source_url 'https://github.com/harfbuzz/harfbuzz/releases/download/2.7.2/harfbuzz-2.7.2.tar.xz'
   source_sha256 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce'
@@ -21,12 +21,18 @@ class Harfbuzz < Package
     ENV['CFLAGS'] = "-fuse-ld=lld"
     ENV['CXXFLAGS'] = "-fuse-ld=lld"
     system "meson",
-      #{CREW_MESON_OPTIONS},
       "-Dintrospection=enabled",
       "-Dbenchmark=disabled",
       "-Dtests=disabled",
       "-Dgraphite=enabled",
       "-Ddocs=disabled",
+      "-Dprefix=#{CREW_PREFIX}",
+      "-Dlibdir=#{CREW_LIB_PREFIX}",
+      "-DLIB_INSTALL_DIR=#{CREW_LIB_PREFIX}",
+      "-Dmandir=#{CREW_MAN_PREFIX}",
+      "-DSYSCONFDIR=#{CREW_PREFIX}/etc",
+      "-Ddatadir=#{CREW_LIB_PREFIX}",
+      '-Dbuildtype=release',
       "builddir"
     system "meson compile -C builddir"
   end
