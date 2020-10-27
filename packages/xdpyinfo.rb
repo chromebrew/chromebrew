3ca1b44328faadc4b3e26dab3dd389482929c0ff
satmandu <satadru@umich.edu>
2020-10-27 09:23:31 -0400

    clean-up version, indents

--- packages/xdpyinfo.rb
+++ packages/xdpyinfo.rb
@@ -5,7 +5,7 @@ require 'package'
 
 class Xorg_xdpyinfo < Package
   description 'Display information utility for X'
-  version '1.3.2-4'
+  version '1.3.2'
   compatibility 'all'
   source_url 'https://xorg.freedesktop.org/archive/individual/app/xdpyinfo-1.3.2.tar.bz2'
   source_sha256 '30238ed915619e06ceb41721e5f747d67320555cc38d459e954839c189ccaf51'
@@ -19,12 +19,12 @@ class Xorg_xdpyinfo < Package
   depends_on 'libxinerama'
   depends_on 'xorg_proto' 
 
-        def self.build
-                system "./configure #{CREW_OPTIONS}"
-                system 'make'
-        end
-        
-        def self.install
-                system "make DESTDIR=#{CREW_DEST_DIR} install"
-        end
+  def self.build
+    system "./configure #{CREW_OPTIONS}"
+    system 'make'
+  end
+  
+  def self.install
+    system "make DESTDIR=#{CREW_DEST_DIR} install"
+  end
 end
