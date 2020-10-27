6599ce0a6fc9291a17aa267112c3a65f56ca37d2
satmandu <satadru@umich.edu>
2020-10-27 09:40:08 -0400

    Add peg (#4530)
    * Add peg
    
    * Force use of PREFIX
    
    * use suggested sed command

---
+++ packages/peg.rb
@@ -0,0 +1,19 @@
+# Adapted from Arch Linux peg PKGBUILD at:
+# https://github.com/archlinux/svntogit-community/raw/packages/peg/trunk/PKGBUILD
+
+require 'package'
+
+class Peg < Package
+  description 'recursive-descent parser generators for C'
+  version '0.1.18-2'
+  source_url 'http://piumarta.com/software/peg/peg-0.1.18.tar.gz'
+  source_sha256 '20193bdd673fc7487a38937e297fff08aa73751b633a086ac28c3b34890f9084'
+
+  def self.build
+    system "sed -i 's,PREFIX = /usr/local,PREFIX = #{CREW_PREFIX},g' Makefile"
+    system "make"
+  end
+  def self.install
+    system "make ROOT=#{CREW_DEST_DIR} install"
+  end
+end
