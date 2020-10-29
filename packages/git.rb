6e0735a64b0b3ea053ef65c32dd0d9bb91655271
Ed Reel <edreel@gmail.com>
2020-10-29 17:53:53 -0500

    Update curl, git, libiconv, libressl and rtmpdump packages (#4532)
    Add pre-built binaries
    
    Update install.sh
    
    Remove absolute path from curl in crew

--- packages/git.rb
+++ packages/git.rb
@@ -3,22 +3,22 @@ require 'package'
 class Git < Package
   description 'Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.'
   homepage 'https://git-scm.com/'
-  version '2.28.0-1'
+  version '2.29.1'
   compatibility 'all'
-  source_url 'https://github.com/git/git/archive/v2.28.0.tar.gz'
-  source_sha256 '02016d16dbce553699db5c9c04f6d13a3f50727c652061b7eb97a828d045e534'
+  source_url 'https://github.com/git/git/archive/v2.29.1.tar.gz'
+  source_sha256 'ba2c36747abd2f07ed8ba61481cbb2290188f7a784d5d6ba2a052b56b139dd9f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.28.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.28.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.28.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.28.0-1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.29.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.29.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.29.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.29.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'f3a111e4f5ab2484b4eea1c32b68089256a7535b6ee9c495b45e776d3108c5e5',
-     armv7l: 'f3a111e4f5ab2484b4eea1c32b68089256a7535b6ee9c495b45e776d3108c5e5',
-       i686: '62fa336fd068611c6b3bd0c7be3723dd71aada453470554d3aa63f84bae5377e',
-     x86_64: '33cc1be6a9dca5a8baacc85a9a083949ea650bea37f45c13f14ee879f8afe73d',
+    aarch64: '34d1b4b333195d6aaf5856665dbd9a8b56152603e93fd85f36a1b9c69de8852a',
+     armv7l: '34d1b4b333195d6aaf5856665dbd9a8b56152603e93fd85f36a1b9c69de8852a',
+       i686: '0246035512a9cd81206672ccb996b97a1252050e9473340278c324988ca90b6d',
+     x86_64: 'b850298e9dbbbcc9777d8fff1c95bcf701f26124780fa9976c6dd4381bcc204a',
   })
 
   depends_on 'curl' => :build
@@ -27,7 +27,7 @@ class Git < Package
 
   def self.build
     system 'autoreconf -i'
-    system "./configure --without-tcltk #{CREW_OPTIONS} --with-perl=#{CREW_PREFIX}/bin/perl --with-python=#{CREW_PREFIX}/bin/python3 \
+    system "./configure --with-openssl=#{CREW_PREFIX}/etc/ssl --without-tcltk #{CREW_OPTIONS} --with-perl=#{CREW_PREFIX}/bin/perl --with-python=#{CREW_PREFIX}/bin/python3 \
 --with-gitconfig=#{CREW_PREFIX}/etc/gitconfig --with-gitattributes=#{CREW_PREFIX}/etc/gitattributes"
     system 'make'
   end
--- packages/libiconv.rb
+++ packages/libiconv.rb
@@ -3,22 +3,22 @@ require 'package'
 class Libiconv < Package
   description 'GNU charset conversion library for libc which does not implement it.'
   homepage 'https://www.gnu.org/software/libiconv/'
-  version '1.16'
+  version '1.16-1'
   compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/libiconv/libiconv-1.16.tar.gz'
   source_sha256 'e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libiconv-1.16-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'daee13f0eef96fb2d65a06d129f702cacbf8c70747d7f0dd2d27a9df977372b7',
-     armv7l: 'daee13f0eef96fb2d65a06d129f702cacbf8c70747d7f0dd2d27a9df977372b7',
-       i686: 'a7afa407d80bc94038711a636f10cf9af7fae7fce5b68220f4d9c51eca44849e',
-     x86_64: 'cc29b28830c4bc496b2ef495e9dd43d96e596f879d02d7176222575bb83b5088',
+    aarch64: '6746c57dfa263dd4a4e32a3246f0ca97bee9c15075fd84f5dfbe4cf872ddcc2e',
+     armv7l: '6746c57dfa263dd4a4e32a3246f0ca97bee9c15075fd84f5dfbe4cf872ddcc2e',
+       i686: 'f43a3150b8427b719e0a810426490b84bd4c9b2c9dddc0141ac10b543029dbeb',
+     x86_64: '449564c5ca12cc7f78241c3d9df8476b0f449fdc3930f01f7c3a6506ae3eb1a2',
   })
 
   def self.build
