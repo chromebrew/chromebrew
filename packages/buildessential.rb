c4b262c1d319766753a0f47169d5c7f82ab1f356
Ed Reel <edreel@gmail.com>
2020-10-29 18:25:54 -0500

    Add missing libpsl and zstd dependencies
    Update constants in const.rb and crew
    
    Install order matters
    
    Add zstd to core_packages.txt
    
    Update buildessential package
    
    Update git sha256 hash

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,12 +3,12 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage ''
-  version '1.6'
+  version '1.7'
   compatibility 'all'
 
   is_fake
 
-  depends_on 'gcc8'
+  depends_on 'gcc10'
   depends_on 'gcc_tools'
   depends_on 'linuxheaders'
   depends_on 'make'
