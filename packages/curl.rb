7f1ae24c3761640ee7373d118523937bf29cf6b6
Ed Reel <edreel@gmail.com>
2020-10-29 17:53:53 -0500

    Update curl, git, libiconv, libressl and rtmpdump packages (#4532)
    Add pre-built binaries
    
    Update install.sh
    
    Remove absolute path from curl in crew

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,22 +3,22 @@ require 'package'
 class Curl < Package
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.haxx.se/'
-  version '7.72.0-1'
+  version '7.73.0'
   compatibility 'all'
-  source_url 'https://curl.haxx.se/download/curl-7.72.0.tar.xz'
-  source_sha256 '0ded0808c4d85f2ee0db86980ae610cc9d165e9ca9da466196cc73c346513713'
+  source_url 'https://curl.haxx.se/download/curl-7.73.0.tar.xz'
+  source_sha256 '7c4c7ca4ea88abe00fea4740dcf81075c031b1d0bb23aff2d5efde20a3c2408a'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.72.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.72.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.72.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.72.0-1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.73.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.73.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.73.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.73.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'bf7579c54c6e06479f615d1abf87adf89fc39f3d2c404e2ab2cb95070ace6098',
-     armv7l: 'bf7579c54c6e06479f615d1abf87adf89fc39f3d2c404e2ab2cb95070ace6098',
-       i686: '6fead78266081c9f909b0b5f5f58b0ad70c4acfc0d1fe691c9556b718aefe5f9',
-     x86_64: '99097864dbb9b36941387cde22ccfff3ce6e3f5e8f79f7975dfedb9c875f6a7e',
+    aarch64: '0f75d8a211f66cf27c0f9bfe35b10761f21c6f6d71665d10d5511c1c72017c58',
+     armv7l: '0f75d8a211f66cf27c0f9bfe35b10761f21c6f6d71665d10d5511c1c72017c58',
+       i686: '3109622744ec710f5f7d37678f108f382f481a7297fbecc01902fb5933a0cfa1',
+     x86_64: 'ab8fc3bdc5f2495067b979105b21fc684952beb463a356051fb5736d70f5e1da',
   })
 
   depends_on 'groff' => :build
@@ -33,34 +33,23 @@ class Curl < Package
   depends_on 'libunbound'
   depends_on 'openldap'
   depends_on 'rtmpdump'
+  depends_on 'zstd'
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system 'cmake',
-             "-DCMAKE_INSTALL_MANDIR=#{CREW_MAN_PREFIX}",
-             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
-             "-DCMAKE_INSTALL_LIBDIR=#{ARCH_LIB}",
-             '-DCMAKE_BUILD_TYPE=Release',
-             '-DCMAKE_USE_OPENLDAP=ON',
-             '-DCMAKE_USE_MBEDTLS=ON',
-             '-DCURL_BROTLI=ON',
-             '-DUSE_NGHTTP2=ON',
-             '-ENABLE_ARES=ON',
-             '..'
-      system 'make'
-    end
+    system "./configure #{CREW_OPTIONS} --disable-maintainer-mode --enable-ares --with-ldap-lib=#{CREW_LIB_PREFIX}/libldap.so --with-lber-lib=#{CREW_LIB_PREFIX}/liblber.so"
+    system 'make'
   end
 
   def self.check
-    Dir.chdir 'build' do
-      #system 'ctest'
-    end
+    # Python package impacket needed for testing.
+    # 1094 tests out of 1097 reported OK: 99% on 10/25/2020
+    # The 3 tests that failed were FTP, SMB and GOPHER.
+    #system 'pip install impacket'
+    #system 'make', 'check'
+    #system 'pip uninstall -y impacket'
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
