ef7bb7d3a13700bde33116d44cd24588f2e4993f
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-11-08 15:36:28 +0800

    Fix bugs and and add xorg_xdpyinfo.rb

--- packages/openbox.rb
+++ packages/openbox.rb
@@ -1,12 +1,12 @@
-#require 'package'
+require 'package'
 
-#class Openbox < Package                 # The first character of the class name must be upper case
+class Openbox < Package
   description 'Openbox is a highly configurable, next generation window manager with extensive standards support.'
   homepage 'http://openbox.org/'
   version '3.6.1'
   compatibility 'all'
   source_url 'http://openbox.org/dist/openbox/openbox-3.6.1.tar.xz'
-  source_sha256 'abe75855cc5616554ffd47134ad15291fe37ebbebf1a80b69cbde9d670f0e26d'   # Use the command "sha256sum"
+  source_sha256 'abe75855cc5616554ffd47134ad15291fe37ebbebf1a80b69cbde9d670f0e26d'
 
   depends_on 'libev'
   depends_on 'startup_notification'
@@ -22,92 +22,53 @@
   depends_on 'libsm'
   depends_on 'pango' => :build
   depends_on 'libx11' => :build
-  # depends_on 'adwaita_icon_theme'
-  # depends_on 'xorg_xdpyinfo'
+  #depends_on 'adwaita_icon_theme'
+  depends_on 'xorg_xdpyinfo'
   depends_on 'sommelier'
 
   ENV['CFLAGS'] = "-lX11 -lXau"
-  def self.build                   # the steps required to build the package
+  def self.build
     system "./configure #{CREW_OPTIONS}"
     system "make"
   end
 
-  def self.install                 # the steps required to install the package
+  def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-    # For Chrome OS/Fyde OS with android arc container support
-    # Use XServer XSDL
     system "echo '#!/bin/bash' > startopenbox"
-    system "echo 'echo Starting XServer XSDL' >> startopenbox"
-    system "echo 'echo am start x.org.server/.MainActivity | sudo android-sh' >> startopenbox"
-    
-    ## Remove the comment if the xorg_xdpyinfo and 'adwaita_icon_theme' package is ready
-    # system "cat <EOF>> startopenbox
-    ## Wait for XSDL X server
-    #MAX_ATTEMPTS=120 # About 60 seconds
-    #COUNT=0
-    #stopsommelier
-    #echo -n \"Waiting for X server to be ready...\"
-    #while ! xdpyinfo -display ${DISPLAY} >/dev/null 2>&1; do
-    #  echo -n \".\"
-    #  sleep 0.50s
-    #  export DISPLAY=100.115.92.2:0
-    #  COUNT=$(( COUNT + 1 ))
-    #  if [ \"${COUNT}\" -ge \"${MAX_ATTEMPTS}\" ]; then
-    #    echo \"  Gave up waiting for X server on ${DISPLAY}\"
-    #    exit 1
-    #  fi
-    #done
-    #echo \"  Done - X server is ready!\"
-    #EOF"
-    system "echo 'sleep 7' >> startopenbox"
+    system "cat <<'EOF'> startopenbox
+stopsommelier
+echo am start x.org.server/.MainActivity | sudo android-sh
+echo Starting XServer XSDL
+# Wait for XSDL X server
+MAX_ATTEMPTS=120 # About 60 seconds
+COUNT=0
+echo -n \"Waiting for X server to be ready...\"
+while ! xdpyinfo -display ${DISPLAY} >/dev/null 2>&1; do
+  echo -n \".\"
+  sleep 0.50s
+  COUNT=$(( COUNT + 1 ))
+  if [ \"${COUNT}\" -ge \"${MAX_ATTEMPTS}\" ]; then
+    echo \"  Gave up waiting for X server on ${DISPLAY}\"
+    exit 1
+  fi
+done
+echo \"  Done - X server is ready!\"
+EOF"
     system "echo 'export DISPLAY=100.115.92.2:0' >> startopenbox"
-    system "echo 'openbox \"$@\"' >> startopenbox"
+    system "echo 'openbox \"$@\" &' >> startopenbox"
+    system "echo 'echo XServer closed or logged out session' >> startopenbox"
     system "echo 'startsommelier' >> startopenbox"
+    system "echo 'export DISPLAY=:0' >> startopenbox"
     # For Chrome OS/Chromium OS/Cloudready without android arc container support
     # Use 'xinit'
-    system "echo #!/bin/bash"
-    system "echo \"if [[ cat /etc/lsb-release | grep ARC != "" ]]; then echo 'Your OS have Android Support :)'; else if [[ whereis xinit | grep /usr/local/bin/xinit != /usr/local/bin/xinit ]]; then echo 'To use this package, You need to install `xinit` package'; else echo 'pkill twm && openbox &' >> /usr/local/etc/X11/xinit/xinitrc; fi\" >> cloudready.sh "
-    system "cloudready.sh && rm cloudready.sh"
-    ## For i686 user
-    # system "echo 'The unstable method, feel free to test it:)'"
-    ## Download ARChon Runtime for Chrome
-    # puts "Downloading ARChon Android Runtime for Chrome...".lightblue
-    # system "wget http://archon.vf.io/ARChon-v2.1.0-x86_32.zip"
-    ## Download XServer Extension for ARChon Runtime
-    # puts "Downloading XServer Extension for Chrome...".lightblue
-    # system "wget https://github.com/supechicken666/X-Server-Extension-for-Chrome/raw/main/au.com.darkside.XServer.zip"
-    # system "echo '#!/bin/bash' > startopenbox_arc-extension"
-    # system "cat <EOF>> startopenbox_arc-extension
-    ## Wait for X server extension
-    # MAX_ATTEMPTS=120 # About 60 seconds
-    # COUNT=0
-    # echo Please open `au.com.darkside.XServer` extension from app drawer
-    # stopsommelier
-    # echo -n \"Waiting for X server to be ready...\"
-    # while ! xdpyinfo -display ${DISPLAY} >/dev/null 2>&1; do
-    #  echo -n \".\"
-    #  sleep 0.50s
-    #  export DISPLAY=127.0.0.1:0
-    #  COUNT=$(( COUNT + 1 ))
-    #  if [ \"${COUNT}\" -ge \"${MAX_ATTEMPTS}\" ]; then
-    #    echo \"  Gave up waiting for X server on ${DISPLAY}\"
-    #    exit 1
-    #  fi
-    #done
-    #echo \"  Done - X server is ready!\"
-    #xclock &
-    #xterm &
-    #openbox &
-    #EOF"
-    # system "unzip *.zip && mv */ ~/Downloads"
-    # puts "Install the two unpack extension to chrome with entension developer mode on".lightblue  
-    # system "echo \"if [[ $(uname -m) = i686 ]]; then 'install -Dm755 startopenbox_arc-extension #{CREW_DEST_PREFIX}/bin/startopenbox'; else 'install -Dm755 startopenbox #{CREW_DEST_PREFIX}/bin/startopenbox'; fi\" > install.sh "
-    # system "install.sh"
-    # system "rm install.sh"
-    system "install -Dm755 startopenbox #{CREW_DEST_PREFIX}/bin/startopenbox'"
+    system "echo '#!/bin/bash' > cloudready.sh"
+    system "temp=\"echo 'pkill twm && openbox &' >> /usr/local/etc/X11/xinit/xinitrc\" && echo \"if [[ '$(cat /etc/lsb-release | grep CHROMEOS_ARC_ANDROID_SDK_VERSION)' = '' ]]; then crew install xinit && $temp ; fi\" >> cloudready.sh"
+    system "/bin/bash cloudready.sh && rm cloudready.sh"
+    system "install -Dm755 startopenbox #{CREW_DEST_PREFIX}/bin/startopenbox"
+    puts "Your cpu architecture is `uname -m`".lightgreen
   end
-    
-  def self.check                   # the steps required to check if the package was built ok
+
+  def self.check
     system "make", "check"
   end
   def self.postinstall
@@ -117,7 +78,6 @@
       puts "Use 'startopenbox' instead of 'openbox' to execute this package".lightblue
       puts "For Cloudready/Chromium OS user:".lightblue
       puts "Use 'xinit' instead of 'openbox' to execute this package".lightblue
-      # puts "For legacy processor user, use `startopenbox`".lightblue
       puts "For legacy processor user, set up XServer XSDL on phone".lightblue
   end
 end
---
+++ packages/xorg_xdpyinfo.rb
@@ -0,0 +1,26 @@
+require 'package'
+
+class Xorg_xdpyinfo < Package
+  description 'Display information utility for X'
+  version '1.3.2-4'
+  compatibility 'all'
+  source_url 'https://xorg.freedesktop.org/archive/individual/app/xdpyinfo-1.3.2.tar.bz2'
+  source_sha256 '30238ed915619e06ceb41721e5f747d67320555cc38d459e954839c189ccaf51'
+
+  depends_on 'libx11'
+  depends_on 'libxext'
+  depends_on 'libxtst'
+  depends_on 'libxxf86vm'
+  depends_on 'libxrender'
+  depends_on 'libxcomposite'
+  depends_on 'libxinerama'
+  depends_on 'xorg_proto' 
+
+  def self.build
+    system "./configure #{CREW_OPTIONS}"
+    system 'make'
+  end
+  def self.install
+    system "make DESTDIR=#{CREW_DEST_DIR} install"
+  end
+end
