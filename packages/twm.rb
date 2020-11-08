89a0cfcb0dba08156a65359d7f2d12ebe07b78f5
supechicken <73414985+supechicken@users.noreply.github.com>
2020-11-08 16:40:20 +0800

    Update `xclock` `twm` `xterm` `powershell`

--- packages/twm.rb
+++ packages/twm.rb
@@ -3,23 +3,11 @@ require 'package'
 class Twm < Package
   description 'The twm package contains a very minimal window manager.'
   homepage 'http://www.linuxfromscratch.org/blfs/view/svn/x/twm.html'
-  version '1.0.10'
+  version '1.0.11'
   compatibility 'all'
-  source_url 'https://www.x.org/pub/individual/app/twm-1.0.10.tar.bz2'
-  source_sha256 '6449eadca16ce0f0d900605b0cc020b95f40619261b7beccfb46bcc1836445d7'
+  source_url 'https://www.x.org/pub/individual/app/twm-1.0.11.tar.xz'
+  source_sha256 'f4e8a842dec410e79741f25ed4dfac09df080835d0bba2e3983b5914569b68c7'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/twm-1.0.10-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/twm-1.0.10-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/twm-1.0.10-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/twm-1.0.10-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'e5b66bf042c287bf844da9a0ac2cff8c0af7d679ddcb83bea15b4b9a580534bf',
-     armv7l: 'e5b66bf042c287bf844da9a0ac2cff8c0af7d679ddcb83bea15b4b9a580534bf',
-       i686: '3abf216bc7c9a2e8f8f43ea263c46fe6c7e366d37e6195ac256c0123de5ce37c',
-     x86_64: '2db77d9f08c7c9090e7bc663b00f4c052442addd01fd614220603ec555e16af9',
-  })
 
   depends_on 'xorg_server'
 
--- packages/xterm.rb
+++ packages/xterm.rb
@@ -3,46 +3,37 @@ require 'package'
 class Xterm < Package
   description 'The xterm program is a terminal emulator for the X Window System.'
   homepage 'https://invisible-island.net/xterm/'
-  version '341-1'
+  version '361'
   compatibility 'all'
-  source_url "https://invisible-mirror.net/archives/xterm/xterm-341.tgz"
-  source_sha256 '63a07dfa2b5d501a9e1da0e05772bff7ad431ac2065127c77eebe392ef33cb44'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xterm-341-1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'b0c2289b1f2a2927d3bd133059705407e6418bc595a59aca522eaf9bd92e9e00',
-     armv7l: 'b0c2289b1f2a2927d3bd133059705407e6418bc595a59aca522eaf9bd92e9e00',
-       i686: '0803e8b84a900c5344bc2aa4f2586e8795bf6feb517bc7b75dae69c9438d5eef',
-     x86_64: 'c93930b998ed21cccde94dc81c2aa5e0f91d986d9f193c4c61ca2714d10a8956',
-  })
-
+  source_url "https://invisible-mirror.net/archives/xterm/xterm-361.tgz"
+  source_sha256 '85610f20d5e47205cc1b6876f7a4da28d6ae051bd8eac0b932e92c37a73a623f'
   depends_on 'pcre'
   depends_on 'sommelier'
 
   def self.build
-    system './configure',
-           '--with-x',
-           '--with-pcre',
-           '--with-xinerama',
-           '--enable-dabbrev',
-           '--enable-toolbar',
-           '--with-pkg-config',
-           '--enable-exec-xterm',
-           '--enable-16bit-chars',
-           '--enable-dec-locator',
-           '--enable-double-buffer',
-           '--enable-readline-mouse',
-           '--enable-regis-graphics',
-           '--enable-sixel-graphics',
+    system "TEMP=$PATH"
+    system "export PATH=/bin:/sbin:/usr/local/bin" # The /usr/bin need to remove from PATH as bash will point 'tic' to /usr/bin/tic 
+    system "./configure",
+           "--with-x",
+           "--with-pcre",
+           "--with-xinerama",
+           "--enable-dabbrev",
+           "--enable-toolbar",
+           "--with-pkg-config",
+           "--enable-exec-xterm",
+           "--enable-16bit-chars",
+           "--enable-dec-locator",
+           "--enable-double-buffer",
+           "--enable-readline-mouse",
+           "--enable-regis-graphics",
+           "--enable-sixel-graphics",
            "--prefix=#{CREW_PREFIX}",
            "--with-xpm=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
+           "--libdir=#{CREW_LIB_PREFIX}",
+           "--with-ticlib"
+    system "make"
+    system "export PATH=$TEMP"
+    system "export TEMP=''"
   end
 
   def self.install
