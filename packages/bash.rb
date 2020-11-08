ea1a496683915ae190466828b5fedf2b3233222b
supechicken <73414985+supechicken@users.noreply.github.com>
2020-11-09 00:00:05 +0800

    Use https and mirror

--- packages/bash.rb
+++ packages/bash.rb
@@ -2,9 +2,9 @@ require 'package'
 
 class Bash < Package
   description 'The Bourne Again SHell'
-  homepage 'http://www.gnu.org/software/bash/'
+  homepage 'https://www.gnu.org/software/bash/'
   version '5.1-rc2'
-  source_url 'http://ftp.gnu.org/gnu/bash/bash-5.1-rc2.tar.gz'
+  source_url 'https://ftpmirror.gnu.org/bash/bash-5.1-rc2.tar.gz'
   source_sha256 'f3274290260e2c74a2ec61606cc932cc0e093028d55c779cf2c7907bdb60abc0'
 
   def self.build
