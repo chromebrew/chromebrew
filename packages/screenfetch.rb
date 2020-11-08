3fe0e43f646257a659fd982a4a7e680a525ea898
supechicken <73414985+supechicken@users.noreply.github.com>
2020-11-08 18:20:14 +0800

    Update `skype` and `screenfetch`
    Fixes #4564

--- packages/screenfetch.rb
+++ packages/screenfetch.rb
@@ -3,31 +3,23 @@ require 'package'
 class Screenfetch < Package
   description 'Fetches system/theme information in terminal for Linux desktop screenshots.'
   homepage 'https://github.com/KittyKatt/screenFetch'
-  version '3.9.0'
+  version '3.9.1'
   compatibility 'all'
-  source_url 'https://github.com/KittyKatt/screenFetch/archive/v3.9.0.tar.gz'
-  source_sha256 'd6df4ef7763f9761d818c878465d78ef701b71002a50d4f150f65a31cc1bea37'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/screenfetch-3.9.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/screenfetch-3.9.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/screenfetch-3.9.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/screenfetch-3.9.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '1a4f6205d1b5404524cd23242ebe476fb11074b72974908bd4e7250a35a3209b',
-     armv7l: '1a4f6205d1b5404524cd23242ebe476fb11074b72974908bd4e7250a35a3209b',
-       i686: 'f59b9a43ca298cdddfaba8fc7be3e0c4f2b172ff4c27e32e2879cafe0621cfd6',
-     x86_64: 'f91a00be5514342fb734eb55ef5448f0da5c7d31538e04721f114c94625bfa15',
-  })
+  source_url 'file:///dev/null'
+  source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
   depends_on 'bc'
 
   def self.build
+    system "curl https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch-dev -#o screenfetch-dev"
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("screenfetch-dev") ) == '8841eabb5aa7c319f2b1f87dbac75a07f425b67888154a8779976319603732d0'
+    system "curl https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch.1 -#o screenfetch.1"
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("screenfetch.1") ) == '77ee19b1dc5c91e80d5d0d8e983b4c39928d58b0411b5f603fd7b26b765b6023'
+    puts "Archive downloaded".lightgreen
   end
 
   def self.install
-    system "install -D screenfetch-dev #{CREW_DEST_PREFIX}/bin/screenfetch"
+    system "install -Dm755 screenfetch-dev #{CREW_DEST_PREFIX}/bin/screenfetch"
     system "install -D screenfetch.1 #{CREW_DEST_PREFIX}/man/man1/screenfetch.1"
   end
 end
