245c7fc1869943bebaf783ba5bf5496de3acb6e5
satmandu <satadru@umich.edu>
2020-11-08 15:31:34 -0500

    bump version

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,7 +3,7 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage ''
-  version '1.7'
+  version '1.8'
   compatibility 'all'
 
   is_fake
