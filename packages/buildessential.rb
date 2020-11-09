af2275886b0cf1f52130694d4534d277fdf7aa9d
supechicken <73414985+supechicken@users.noreply.github.com>
2020-11-09 15:01:50 +0800

    Add `bash` to `buildessential` as a dependency

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,11 +3,12 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage ''
-  version '1.7'
+  version '1.8'
   compatibility 'all'
 
   is_fake
 
+  depends_on 'bash'
   depends_on 'gcc10'
   depends_on 'gcc_tools'
   depends_on 'linuxheaders'
