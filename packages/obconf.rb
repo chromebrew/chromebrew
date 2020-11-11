ceb104d8cf2d4f4a2cfe298444f78df01ba9c862
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-11-11 14:32:29 +0800

    Remove `gtk2` dependency

--- packages/obconf.rb
+++ packages/obconf.rb
@@ -8,7 +8,6 @@ class Obconf < Package
   source_url 'http://openbox.org/dist/obconf/obconf-2.0.4.tar.gz'
   source_sha256 '71a3e5f4ee246a27421ba85044f09d449f8de22680944ece9c471cd46a9356b9' 
 
-  depends_on 'gtk2'
   depends_on 'libglade'
   depends_on 'openbox'
 
