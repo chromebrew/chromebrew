fdd1dd6b08e761c6e6fa590edbb5c4836c2a9cc1
Ed Reel <edreel@gmail.com>
2020-11-12 16:56:00 -0600

    Update vim packages and dependencies

--- packages/libssh2.rb
+++ packages/libssh2.rb
@@ -3,22 +3,22 @@ require 'package'
 class Libssh2 < Package
   description 'libssh2 is a client-side C library implementing the SSH2 protocol.'
   homepage 'https://www.libssh2.org/'
-  version '1.9.0'
+  version '1.9.0-1'
   compatibility 'all'
   source_url 'https://www.libssh2.org/download/libssh2-1.9.0.tar.gz'
   source_sha256 'd5fb8bd563305fd1074dda90bd053fb2d29fc4bce048d182f96eaa466dfadafd'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libssh2-1.9.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '497e146e4d769aa85b0d547889784d7552e2aadbbaa448df5bbe9515cd94c182',
-     armv7l: '497e146e4d769aa85b0d547889784d7552e2aadbbaa448df5bbe9515cd94c182',
-       i686: 'dd2172bf82e3ffc1cfd06463108a36de55ba5e4bc2eb283852b54a737e087fe4',
-     x86_64: 'd02623da747ecc95acdba69a056220ef579b01dae82a0cbfec2aa96c2ea8e914',
+    aarch64: 'da6c1206dbf05ca327afb69311f4938ec09c2d2552edc1f12b78fa5048550bff',
+     armv7l: 'da6c1206dbf05ca327afb69311f4938ec09c2d2552edc1f12b78fa5048550bff',
+       i686: 'aaf97c46b8e2f4fdd5b603e27735898d1aa166d20ee79387b0040f6160c12ba6',
+     x86_64: '658605c62a5444a0665b4ef72f3d705512c4ca72a58505e40fbe1718db3ed7bd',
   })
 
   def self.patch
@@ -33,11 +33,11 @@ class Libssh2 < Package
     system 'make'
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
   def self.check
     system 'make', 'check'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
