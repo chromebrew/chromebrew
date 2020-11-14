5ef84cde9436b52f1f83bf88220543c315a99e9a
satmandu <satadru@umich.edu>
2020-11-13 20:37:19 -0500

    Add weston.ini and use xdg_base depend

--- packages/weston.rb
+++ packages/weston.rb
@@ -21,6 +21,7 @@ class Weston < Package
     depends_on 'dbus'
     depends_on 'libxxf86vm'
     depends_on 'llvm' => :build
+    depends_on 'xdg_base'
   end
 
 
@@ -30,22 +31,21 @@ class Weston < Package
     system "meson #{CREW_MESON_OPTIONS} -Dshell-ivi=false -Dremoting=false -Dbackend-default=wayland -Dbackend-drm=false -Dpipewire=false -Dcolor-management-colord=false -Dcolor-management-lcms=false -Dbackend-rdp=false -Dlauncher-logind=false -Dweston-launch=false -Dsystemd=false -Dxwayland-path=#{CREW_PREFIX}/bin/Xwayland build"
     system "meson configure build"
     system 'ninja -C build'
+    system "cat <<'EOF'> weston.ini
+[core]
+'xwayland=true
+
+[terminal]
+font=Cousine
+EOF"
   end
 
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} ninja install -C build"
+    system "install -Dm644 weston.ini #{CREW_DEST_HOME}/.config/weston.ini"
   end
   
   def self.postinstall
-    puts
-    puts "To complete the installation, execute the following:".lightblue
-    puts "mkdir -p ~/.config".lightblue
-    puts "echo '[core]' >> ~/.config/weston.ini".lightblue
-    puts "echo 'xwayland=true' >> ~/.config/weston.ini".lightblue
-    puts "echo '[terminal]' >> ~/.config/weston.ini".lightblue
-    # Cousine is the defaut ChromeOS monospace font.
-    puts "echo 'font=Cousine' >> ~/.config/weston.ini".lightblue
-    puts
     puts "To run weston with xwayland try something like this:".lightred
     puts "export WAYLAND_DISPLAY=wayland-1".lightred
     puts "WAYLAND_DISPLAY=wayland-0 weston -Swayland-1 --xwayland".lightred
