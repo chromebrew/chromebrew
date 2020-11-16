da984f930f7259135506110bfd806b07d8992d6b
uberhacker <edreel@gmail.com>
2020-11-16 03:08:07 +0000

    Update composer from 2.0.4 to 2.0.7

--- packages/composer.rb
+++ packages/composer.rb
@@ -3,10 +3,10 @@ require 'package'
 class Composer < Package
   description 'Dependency Manager for PHP'
   homepage 'https://getcomposer.org/'
-  version '2.0.4'
+  version '2.0.7'
   compatibility 'all'
-  source_url 'https://github.com/composer/composer/archive/2.0.4.tar.gz'
-  source_sha256 '0d699731fe6b3b31a3a2a86032459f08449dbe095fe57749536da922d32294c2'
+  source_url 'https://github.com/composer/composer/archive/2.0.7.tar.gz'
+  source_sha256 '204215dccc01fea015de5c8506100a078c821fe31afb3eee5aa93c5841f83f4d'
 
   depends_on 'php74' unless File.exists? "#{CREW_PREFIX}/bin/php"
   depends_on 'xdg_base'
@@ -23,7 +23,7 @@ class Composer < Package
 
   def self.install
     system "curl -Ls -o composer https://github.com/composer/composer/releases/download/#{version}/composer.phar"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('composer') ) == 'c3b2bc477429c923c69f7f9b137e06b2a93c6a1e192d40ffad1741ee5d54760d'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('composer') ) == '0a060e8caf1d5cde55c4562fd68369b08cf231b8fd30da9c8f994b111557ebe9'
     system "install -Dm755 composer #{CREW_DEST_PREFIX}/bin/composer"
     FileUtils.ln_sf "#{CREW_PREFIX}/.config", "#{HOME}/.config"
   end
