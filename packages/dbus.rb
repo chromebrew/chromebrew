b2addb6bb813b2f3f77b0f29a850942d422f07db
vincowl <vincent@heliosphere.fr>
2020-11-17 16:42:06 +0100

    [dbus] Update to 1.13.18 (#4585)

--- packages/dbus.rb
+++ packages/dbus.rb
@@ -3,22 +3,14 @@ require 'package'
 class Dbus < Package
   description 'D-Bus is a message bus system, a simple way for applications to talk to one another.'
   homepage 'https://www.freedesktop.org/wiki/Software/dbus/'
-  version '1.13.12'
+  version '1.13.18'
   compatibility 'all'
-  source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.12.tar.xz'
-  source_sha256 '7588649b56dd257c6a5f85a8c45aa2dfdf9e99f4de3983710f452081ca43eca6'
+  source_url 'https://dbus.freedesktop.org/releases/dbus/dbus-1.13.18.tar.xz'
+  source_sha256 '8078f5c25e34ab907ce06905d969dc8ef0ccbec367e1e1707c7ecf8460f4254e'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.12-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'bc3711b6214181b732f6c98024718845f326258a8c4e681b58b2e8436998909b',
-     armv7l: 'bc3711b6214181b732f6c98024718845f326258a8c4e681b58b2e8436998909b',
-       i686: '4a208ad455645a8ebfbc06b53d7aa3e9a149169afb18f8232645287bb61a2865',
-     x86_64: '9e24a9f1c2a389491c5dd06dc59ca7748479d786a7f0f4ce3247cdd982813756',
   })
 
   depends_on 'expat'
