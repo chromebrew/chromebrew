646c15a098c2778592d5d1710cddf41ecc313d54
Ed Reel <edreel@gmail.com>
2020-11-21 00:17:59 -0600

    Add pre-built binaries for packages d-p

--- packages/dbus.rb
+++ packages/dbus.rb
@@ -9,8 +9,16 @@ class Dbus < Package
   source_sha256 '8078f5c25e34ab907ce06905d969dc8ef0ccbec367e1e1707c7ecf8460f4254e'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.18-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.18-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.18-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/dbus-1.13.18-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: '11c36243a83f99f7a2f44194ed1e8691fc5a4dd98238a067e8fa8caf53364842',
+     armv7l: '11c36243a83f99f7a2f44194ed1e8691fc5a4dd98238a067e8fa8caf53364842',
+       i686: 'cfa8bf8ea807e3b6762a041419ffd39ce6c4888a430debb0b6a4c9aa55a63489',
+     x86_64: 'c777f379f48d88844a5f9eb1f9e38174df8645c6e76845ab0fc8f1ee58034c52',
   })
 
   depends_on 'expat'
--- packages/openbox.rb
+++ packages/openbox.rb
@@ -8,6 +8,19 @@ class Openbox < Package
   source_url 'http://openbox.org/dist/openbox/openbox-3.6.1.tar.xz'
   source_sha256 'abe75855cc5616554ffd47134ad15291fe37ebbebf1a80b69cbde9d670f0e26d'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/openbox-3.6.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/openbox-3.6.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/openbox-3.6.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/openbox-3.6.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '0bf8f99523d3194bd04c4ae49dc6b1f01b93852d141117aae56a0c7a8f946201',
+     armv7l: '0bf8f99523d3194bd04c4ae49dc6b1f01b93852d141117aae56a0c7a8f946201',
+       i686: 'e58f0787ecef84e8f87389c9aca8b5a35e0846a0a6b8223c01cf4ea447ac6ac6',
+     x86_64: 'f6f6b3a0fcab357ce8f7dce834e119dd87ad7770412e83890c141dfb7979e59e',
+  })
+
   depends_on 'libev'
   depends_on 'startup_notification'
   depends_on 'xcb_util_cursor'
@@ -34,11 +47,11 @@ class Openbox < Package
   ENV['CFLAGS'] = "-lX11 -lXau"
   def self.build
     system "./configure #{CREW_OPTIONS}"
-    system "make"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     system "cat <<'EOF'> startopenbox
 #!/bin/bash
 stopsommelier
@@ -48,12 +61,12 @@ echo Starting XServer XSDL
 MAX_ATTEMPTS=120 # About 60 seconds
 COUNT=0
 echo -n \"Waiting for X server to be ready...\"
-while ! xdpyinfo -display ${DISPLAY} >/dev/null 2>&1; do
+while ! xdpyinfo -display \${DISPLAY} >/dev/null 2>&1; do
   echo -n \".\"
   sleep 0.50s
-  COUNT=$(( COUNT + 1 ))
-  if [ \"${COUNT}\" -ge \"${MAX_ATTEMPTS}\" ]; then
-    echo \"  Gave up waiting for X server on ${DISPLAY}\"
+  COUNT=\$(( COUNT + 1 ))
+  if [ \"\${COUNT}\" -ge \"\${MAX_ATTEMPTS}\" ]; then
+    echo \"  Gave up waiting for X server on \${DISPLAY}\"
     exit 1
   fi
 done
@@ -67,22 +80,25 @@ EOF"
     # For Chrome OS/Chromium OS/Cloudready without android arc container support
     # Use 'xinit'
     system "install -Dm755 startopenbox #{CREW_DEST_PREFIX}/bin/startopenbox"
-    puts "Your cpu architecture is `uname -m`".lightgreen
+    puts "Your cpu architecture is #{ARCH}".lightgreen
   end
 
   def self.check
-    system "make", "check"
+    #system 'make', 'check'
   end
+
   def self.postinstall
-      system "echo '#!/bin/bash' > cloudready.sh"
-      system "temp=\"echo 'pkill twm && openbox &' >> /usr/local/etc/X11/xinit/xinitrc\" && echo \"if [[ '$(cat /etc/lsb-release | grep CHROMEOS_ARC_ANDROID_SDK_VERSION)' = '' ]]; then crew install xinit && $temp ; fi\" >> cloudready.sh"
-      system "bash cloudready.sh"
-      puts
-      puts "For Chrome OS/Fyde OS user:".lightblue
-      puts "To use this package, you need to download XServer XSDL from Google Play Store".lightblue
-      puts "Use 'startopenbox' instead of 'openbox' to execute this package".lightblue
-      puts "For Cloudready/Chromium OS user:".lightblue
-      puts "Use 'xinit' instead of 'openbox' to execute this package".lightblue
-      puts "For legacy processor user, set up XServer XSDL on phone".lightblue
+    system "echo '#!/bin/bash' > cloudready.sh"
+    system "temp=\"echo 'pkill twm && openbox &' >> #{CREW_PREFIX}/etc/X11/xinit/xinitrc\" && echo \"if [[ '$(cat /etc/lsb-release | grep CHROMEOS_ARC_ANDROID_SDK_VERSION)' = '' ]]; then crew install xinit && \$temp ; fi\" >> cloudready.sh"
+    system "bash cloudready.sh"
+    puts
+    puts "For Chrome OS/Fyde OS user:".lightblue
+    puts "To use this package, you need to download XServer XSDL from Google Play Store".lightblue
+    puts "Use 'startopenbox' instead of 'openbox' to execute this package".lightblue
+    puts
+    puts "For Cloudready/Chromium OS user:".lightblue
+    puts "Use 'xinit' instead of 'openbox' to execute this package".lightblue
+    puts "For legacy processor user, set up XServer XSDL on phone".lightblue
+    puts
   end
 end
