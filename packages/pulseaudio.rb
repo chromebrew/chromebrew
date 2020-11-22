4873fe97cce46b90b51245f796a4541fdef4ee76
Ed Reel <edreel@gmail.com>
2020-11-21 23:28:13 -0600

    Add pre-built binaries for packages d-p (#4590)

--- packages/pulseaudio.rb
+++ packages/pulseaudio.rb
@@ -9,8 +9,16 @@ class Pulseaudio < Package
   source_sha256 '2572543a7686c699654c40ec3945a598e6af50ff703f60de0446b7fbcd16ef01'
 
   binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
+    aarch64: 'ed785bbf45af02e5a96ed947740f1f5b055bdee39670f1006d15d6fd96876f24',
+     armv7l: 'ed785bbf45af02e5a96ed947740f1f5b055bdee39670f1006d15d6fd96876f24',
+       i686: '0e19e15a116395b9aa52977c3cd99c9e735d70365146435d54bdd9e300df7e3c',
+     x86_64: 'f346f11c323de6019f1f1189db6398d12671db5b493b68c18b1617e177faff68',
   })
 
   depends_on 'gsettings_desktop_schemas'
