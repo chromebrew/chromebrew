a541f75876c040afc67338d64f6e09a31c92b22a
Ed Reel <edreel@gmail.com>
2020-11-21 23:28:13 -0600

    Add pre-built binaries for packages d-p (#4590)

--- packages/obconf.rb
+++ packages/obconf.rb
@@ -8,6 +8,19 @@ class Obconf < Package
   source_url 'http://openbox.org/dist/obconf/obconf-2.0.4.tar.gz'
   source_sha256 '71a3e5f4ee246a27421ba85044f09d449f8de22680944ece9c471cd46a9356b9' 
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/obconf-2.0.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/obconf-2.0.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/obconf-2.0.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/obconf-2.0.4-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'e11f0fbb1d393185c455d03faef83ee21e4166afbb4e0676dd0b176a6113b9a3',
+     armv7l: 'e11f0fbb1d393185c455d03faef83ee21e4166afbb4e0676dd0b176a6113b9a3',
+       i686: 'ef68ba89facc43bdd81029ff643414d418a9ce101fa0c7023dda1c615b111972',
+     x86_64: '269b7e861ced2a76d203881329de5ce2bddf2b48588a13d341b01e78872844fd',
+  })
+
   depends_on 'libglade'
   depends_on 'openbox'
 
