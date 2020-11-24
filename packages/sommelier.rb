2a580df4ad635f6811667e3c5413c6a61999b774
satmandu <satadru@umich.edu>
2020-11-24 16:46:00 -0500

    Sommelier & Xwayland (#4450)
    * Update to sommelier from platform2 with graphics hardware acceleration support
    
    * Cleanups of ARCH variables & formatting
    
    * Add more documentation
    
    * cleanup default drivers and paths
    
    * Shared Memory Drivers w/o crostini need to be noop
    
    * Cleanup formatting and fix library paths for non-x86_64 archs
    
    * Refactored with hashcheck, & new const
    
    * s/armv11/armv7l/
    
    * depends cleanup
    
    * Update to current c++ version of sommelier
    
    Also add startsommelier & restartsommelier scripts as per 
    https://github.com/skycocker/chromebrew/pull/4517
    
    * Add xdpyinfo as dep as part of .bashrc script
    
    * Use more flags used in crostini
    
    * Display should be a variable
    
    * refactor sommelier variables to not source scripts
    
    As pe https://chromium.googlesource.com/chromiumos/docs/+/master/security/noexec_shell_scripts.md
    
    * needs xauth; log to /tmp/sommelier.log
    
    --sd-notify=READY=1 does not work.
    
    * adjust sommelier run time flags & env file documentation
    
    * Refactored scripts
    
    Also now have both sommelier instances running correctly,
    for both wayland & xwayland.
    
    * eliminate need to use sed on source files
    
    Just put virtwl.h in build/linux!
    
    * wayland sommelier instance needs display setting
    
    * Have all X instances run through sommelier proxied wayland
    
    * cleanups of env var code
    
    * Use "-L" with curl
    
    * eliminate variable in .sommelier.env
    
    * conditional dependency to allow for xwayland package
    
    * Changed logfile location
    
    * edit postinstall text
    
    * Add xwayland tweaked for sommelier
    
    * Add xwayland dependency.
    
    (This can be removed if a later version of xorg_server incorporates the xwayland changes.)
    
    * remove xauth flag, more extensive xserver cleanup
    
    * use XDG_RUNTIME_DIR in cleanup
    
    * Fix variable name
    
    * fix array variable assignment in .sommelier.env
    
    * Adjust sommelier_accelerators variable
    
    * escape line continue \
    
    * Don't export temporary variables
    
    * Refactor scripts to indepedently start/stop sommelier instances
    
    * Make suggested changes, fix gcc >9.x error.
    
    * clean up patch infrastructure
    
    * change download semantics as requested
    
    * Fix missing "d"
    
    * make checksommelierwayland fn more robust

--- packages/sommelier.rb
+++ packages/sommelier.rb
@@ -2,106 +2,261 @@ require 'package'
 
 class Sommelier < Package
   description 'Sommelier works by redirecting X11 and Wayland programs to the built-in ChromeOS wayland server.'
-  homepage 'https://chromium.googlesource.com/chromiumos/containers/sommelier'
-  version '1382ce084cc407'
+  homepage 'https://chromium.googlesource.com/chromiumos/platform2/+/HEAD/vm_tools/sommelier/'
+  version '20201025'
   compatibility 'all'
-  source_url 'https://chromium.googlesource.com/chromiumos/containers/sommelier/+/0.20/README?format=TEXT'
-  source_sha256 'b58d799b16d20abf92369fe0749c73f7398996f0afa9933517051778a8bb16c3'
-
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
+  source_url 'https://chromium-review.googlesource.com/changes/chromiumos%2Fplatform2~2476815/revisions/5/patch?zip&path=%2FCOMMIT_MSG'
+  source_sha256 'd1850e1d4a1e1ec873b9e4add7a881e981f6c0bc17dfd2a1b85efd7df6dd84b4'
 
   depends_on 'mesa'
   depends_on 'xkbcomp'
-  depends_on 'xorg_server'
+  depends_on 'xorg_server' unless File.exists? "#{CREW_PREFIX}/bin/Xwayland.elf"
+  depends_on 'xwayland'
   depends_on 'psmisc'
+  depends_on 'vim' # Until there is a standalone package providing xxd
+  depends_on 'xdpyinfo'
+  depends_on 'xauth'
   depends_on 'xsetroot'
+  depends_on 'llvm' => :build
 
+  case ARCH
+   when 'armv7l', 'aarch64'
+      PEER_CMD_PREFIX='/lib/ld-linux-armhf.so.3'
+   when 'i686'
+      PEER_CMD_PREFIX='/lib/ld-linux-i686.so.2'
+   when 'x86_64'
+      PEER_CMD_PREFIX='/lib64/ld-linux-x86-64.so.2'
+  end
+    
   def self.build
-    system 'git clone https://chromium.googlesource.com/chromiumos/containers/sommelier'
-    Dir.chdir ("sommelier") do
-      system "git reset --hard 1382ce084cc40790340d672e8b62ec47733cb860"
-      system "sed -i 's,/lib/,/#{ARCH_LIB}/,g' Makefile"
-      system "sed -i 's,-I.,-I. -I#{CREW_PREFIX}/include/pixman-1,g' Makefile"
-      system "make PREFIX=#{CREW_PREFIX} SYSCONFDIR=#{CREW_PREFIX}/etc"
-      system "echo 'GDK_BACKEND=x11' > .sommelier.env"
-      system "echo 'CLUTTER_BACKEND=wayland' >> .sommelier.env"
-      system "echo 'XDG_RUNTIME_DIR=/var/run/chrome' >> .sommelier.env"
-      system "echo 'WAYLAND_DISPLAY=wayland-0' >> .sommelier.env"
-      system "echo 'DISPLAY=:0' >> .sommelier.env"
-      system "echo 'SCALE=1' >> .sommelier.env"
-      system "echo '#!/bin/bash' > sommelierd"
-      system "echo 'sommelier -X --x-display=\$DISPLAY --scale=\$SCALE --no-exit-with-child /bin/sh -c \"#{CREW_PREFIX}/etc/sommelierrc\"' >> sommelierd"
-      system "echo '#!/bin/bash' > initsommelier"
-      system "echo 'SOMM=\$(pidof sommelier 2> /dev/null)' >> initsommelier"
-      system "echo 'if [ -z \"\$SOMM\" ]; then' >> initsommelier"
-      system "echo '  [ -f #{CREW_PREFIX}/bin/stopbroadway ] && stopbroadway' >> initsommelier"
-      system "echo '  #{CREW_PREFIX}/sbin/sommelierd &' >> initsommelier"
-      system "echo '  sleep 3' >> initsommelier"
-      system "echo 'fi' >> initsommelier"
-      system "echo 'SOMM=\$(pidof sommelier 2> /dev/null)' >> initsommelier"
-      system "echo 'if [ ! -z \"\$SOMM\" ]; then' >> initsommelier"
-      system "echo '  echo \"sommelier process \$SOMM is running\"' >> initsommelier"
-      system "echo 'else' >> initsommelier"
-      system "echo '  echo \"sommelier failed to start\"' >> initsommelier"
-      system "echo '  exit 1' >> initsommelier"
-      system "echo 'fi' >> initsommelier"
-      system "echo '#!/bin/bash' > stopsommelier"
-      system "echo 'SOMM=\$(pidof sommelier 2> /dev/null)' >> stopsommelier"
-      system "echo 'if [ ! -z \"\$SOMM\" ]; then' >> stopsommelier"
-      system "echo '  killall -g sommelier' >> stopsommelier"
-      system "echo '  if pgrep \"Xwayland\"; then killall Xwayland; fi'"
-      system "echo '  sleep 3' >> stopsommelier"
-      system "echo 'fi' >> stopsommelier"
-      system "echo 'SOMM=\$(pidof sommelier 2> /dev/null)' >> stopsommelier"
-      system "echo 'if [ -z \"\$SOMM\" ]; then' >> stopsommelier"
-      system "echo '  echo \"sommelier process stopped\"' >> stopsommelier"
-      system "echo 'else' >> stopsommelier"
-      system "echo '  echo \"sommelier process \$SOMM is running\"' >> stopsommelier"
-      system "echo '  exit 1' >> stopsommelier"
-      system "echo 'fi' >> stopsommelier"
-      system "echo '#!/bin/bash' > startsommelier"
-      system "echo 'set -a && source ~/.sommelier.env && set +a && initsommelier' >> startsommelier"
+    # There is no good way to checksum the googlesource tgz file, as they appear to be generated on the fly
+    # and checksums vary with each download.
+    system 'curl -L https://chromium.googlesource.com/chromiumos/platform2/+archive/c6f85a3472584df37fcf2d3d99474081f29256cd.tar.gz | tar mzx --warning=no-timestamp'
+    Dir.chdir ("vm_tools/sommelier") do
+    
+    # Google's sommelier expects to find virtwl.h in their kernel source includes, but we may not have
+    # set of kernel headers which match, so we just download virtwl.h and then patch the sommelier source
+    # to look for the file locally.
+    ######################### Download virtwl.h from Chromium 5.4 kernel tree ###########################################
+    url_virtwl = "https://chromium.googlesource.com/chromiumos/third_party/kernel/+/5d641a7b7b64664230d2fd2aa1e74dd792b8b7bf/include/uapi/linux/virtwl.h?format=TEXT"
+    uri_virtwl = URI.parse url_virtwl
+    filename_virtwl = 'virtwl.h_base64'
+    sha256sum_virtwl = 'a8215f4946ccf30cbd61fcf2ecc4edfe6d05bffeee0bacadd910455274955446'
+    
+    puts "Downloading virtwl".yellow
+    system('curl', '-s', '-C', '-', '--insecure', '-L', '-#', url_virtwl, '-o', filename_virtwl)
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless
+      Digest::SHA256.hexdigest( File.read( filename_virtwl ) ) == sha256sum_virtwl
+    puts "virtwl base64 downloaded".lightgreen
+    FileUtils.mkdir_p 'build/linux'
+    system 'base64 --decode virtwl.h_base64 > build/linux/virtwl.h'   
+    
+    # Patch to avoid error with GCC > 9.x
+    # ../sommelier.cc:3238:10: warning: ‘char* strncpy(char*, const char*, size_t)’ specified bound 108 equals destination size [-Wstringop-truncation]
+    system "sed -i 's/sizeof(addr.sun_path))/sizeof(addr.sun_path) - 1)/' sommelier.cc"
+    
+    # lld is needed so libraries linked to system libraries (e.g. libgbm.so) can be linked against, since those are required for graphics acceleration.
+    ENV['CFLAGS'] = "-fuse-ld=lld"
+    ENV['CXXFLAGS'] = "-fuse-ld=lld"
+    
+    system "meson #{CREW_MESON_OPTIONS} -Dxwayland_path=#{CREW_PREFIX}/bin/Xwayland \
+    -Dxwayland_gl_driver_path=/usr/#{ARCH_LIB}/dri -Ddefault_library=both \
+    -Dxwayland_shm_driver=noop -Dshm_driver=noop -Dvirtwl_device=/dev/null \
+    -Dpeer_cmd_prefix=\"#{CREW_PREFIX}#{PEER_CMD_PREFIX}\" build"
+    system "meson configure build"
+    system "ninja -C build"
+    
+    Dir.chdir ("build") do
+      system 'curl -L "https://chromium.googlesource.com/chromiumos/containers/sommelier/+/refs/heads/master/sommelierrc?format=TEXT" | base64 --decode > sommelierrc'
+      
+      system "cat <<'EOF'> .sommelier.env
+#!/bin/bash
+shopt -os allexport
+CLUTTER_BACKEND=wayland
+DISPLAY=:0
+GDK_BACKEND=x11
+SCALE=0.5
+SOMMELIER_ACCELERATORS=\"Super_L,<Alt>bracketleft,<Alt>bracketright\"
+WAYLAND_DISPLAY=wayland-1
+XDG_RUNTIME_DIR=/var/run/chrome
+shopt -ou allexport
+UNAME_ARCH=$(uname -m)
+if [[ \"$UNAME_ARCH\" == 'x86_64' ]] || [[ \"$UNAME_ARCH\" == 'i686' ]]
+then
+  declare -a VERTEMP
+  TEMP1=$(uname -r)
+  VERTEMP=(${TEMP1//[.-]/ })
+  VERSION=$(((${VERTEMP[0]} * 1000000)\\
+        + (${VERTEMP[1]} * 10000)\\
+        + ${VERTEMP[2]}))
+  if [[ \"$VERSION\" -lt '4160000' ]]
+  then
+      export MESA_LOADER_DRIVER_OVERRIDE=i965
+  fi
+fi
+EOF"     
+      
+      #Create local startup and shutdown scripts
+      
+      # sommelier_sh 
+      # This file via:
+      # crostini: /opt/google/cros-containers/bin/sommelier
+      # https://source.chromium.org/chromium/chromium/src/+/master:third_party/chromite/third_party/lddtree.py;drc=46da9a8dfce28c96765dc7d061f0c6d7a52e7352;l=146
+      system "cat <<'EOF'> sommelier_sh
+#!/bin/bash
+if base=$(readlink \"$0\" 2>/dev/null); then
+  case $base in
+  /*) base=$(readlink -f \"$0\" 2>/dev/null);; # if $0 is abspath symlink, make symlink fully resolved.
+  *)  base=$(dirname \"$0\")/\"${base}\";;
+  esac
+else
+  case $0 in
+  /*) base=$0;;
+  *)  base=${PWD:-`pwd`}/$0;;
+  esac
+fi
+basedir=${base%/*}
+# TODO(crbug/1003841): Remove LD_ARGV0 once
+# ld.so supports forwarding the binary name.
+LD_ARGV0=\"$0\" LD_ARGV0_REL=\"../bin/sommelier\" exec   \"${basedir}/..#{PEER_CMD_PREFIX}\"   --library-path \"${basedir}/../#{ARCH_LIB}\"   --inhibit-rpath ''   \"${base}.elf\"   \"$@\"
+EOF"
+
+      # sommelierd
+      system "cat <<'EOF'> sommelierd
+#!/bin/bash
+set -a && source ~/.sommelier.env && set +a
+mkdir -p #{CREW_PREFIX}/var/{log,run}
+checksommelierwayland () {
+  /sbin/ss --unix -a -p | grep \"\\b\$(cat #{CREW_PREFIX}/var/run/sommelier-wayland.pid)\" | grep wayland &>/dev/null
+}
+checksommelierxwayland () {
+  xdpyinfo -display \$DISPLAY &>/dev/null
+}
+# As per https://www.reddit.com/r/chromeos/comments/8r5pvh/crouton_sommelier_openjdk_and_oracle_sql/e0pfknx/
+# One needs a second sommelier instance for wayland clients since at some point wl-drm was not implemented
+# in ChromeOS's wayland compositor.
+if ! checksommelierwayland ; then
+pkill -F #{CREW_PREFIX}/var/run/sommelier-wayland.pid &>/dev/null
+sudo rm \${XDG_RUNTIME_DIR}/wayland-1*
+sommelier --master --peer-cmd-prefix=\"#{CREW_PREFIX}#{PEER_CMD_PREFIX}\" --drm-device=/dev/dri/renderD128 --shm-driver=noop --data-driver=noop --display=wayland-0 --socket=wayland-1 --virtwl-device=/dev/null > #{CREW_PREFIX}/var/log/sommelier.log 2>&1 &
+echo \$! >#{CREW_PREFIX}/var/run/sommelier-wayland.pid
+fi
+if ! checksommelierxwayland; then
+pkill -F #{CREW_PREFIX}/var/run/sommelier-xwayland.pid &>/dev/null
+[[ ! -d /tmp/.X11-unix ]] && mkdir /tmp/.X11-unix
+sudo chmod -R 1777 /tmp/.X11-unix
+sudo chown root:root /tmp/.X11-unix
+DISPLAY=\"\${DISPLAY//:}\"
+DISPLAY=\"\${DISPLAY:0:2}\"
+sudo rm /tmp/.X11-unix/X\"\${DISPLAY}\"
+sommelier -X --x-display=:\$DISPLAY  --scale=\$SCALE --glamor --drm-device=/dev/dri/renderD128 --virtwl-device=/dev/null --shm-driver=noop --data-driver=noop --display=wayland-0 --xwayland-path=/usr/local/bin/Xwayland --peer-cmd-prefix=\"#{CREW_PREFIX}#{PEER_CMD_PREFIX}\" --no-exit-with-child /bin/sh -c \"touch ~/.Xauthority; xauth -f ~/.Xauthority add :$DISPLAY . $(xxd -l 16 -p /dev/urandom); . #{CREW_PREFIX}/etc/sommelierrc\" &>>#{CREW_PREFIX}/var/log/sommelier.log
+echo \$! >#{CREW_PREFIX}/var/run/sommelier-xwayland.pid
+xhost +si:localuser:root &>/dev/null
+fi
+EOF"
+
+      # startsommelier
+       system "cat <<'EOF'> startsommelier
+#!/bin/bash
+set -a && source ~/.sommelier.env &>/dev/null && set +a
+checksommelierwayland () {
+  /sbin/ss --unix -a -p | grep \"\\b\$(cat #{CREW_PREFIX}/var/run/sommelier-wayland.pid)\" | grep wayland &>/dev/null
+}
+checksommelierxwayland () {
+  xdpyinfo -display \$DISPLAY &>/dev/null
+}
+if ! checksommelierwayland || ! checksommelierxwayland ; then
+  [ -f  #{CREW_PREFIX}/bin/stopbroadway ] && stopbroadway
+  #{CREW_PREFIX}/sbin/sommelierd > /dev/null 2>&1 &
+fi
+wait=3
+until checksommelierwayland && checksommelierxwayland
+do
+  [[ \"$wait\" -le \"0\" ]] && break
+  (( wait = wait - 1 ))
+  sleep 1
+done
+SOMMWPIDS=\$(pgrep -f \"sommelier.elf --master\" 2> /dev/null)
+SOMMWPROCS=\$(pgrep -fa \"sommelier.elf --master\" 2> /dev/null)
+SOMMXPIDS=\$(pgrep -f \"sommelier.elf -X\" 2> /dev/null)
+SOMMXPROCS=\$(pgrep -fa \"sommelier.elf -X\" 2> /dev/null)
+if checksommelierwayland && checksommelierxwayland ; then
+  echo -e \"sommelier processes running: \$(echo \$SOMMWPIDS \$SOMMXPIDS)\"
+else
+  echo \"some sommelier processes failed to start\"
+  echo -e \"sommelier processes running: \$SOMMWPROCS \\n \$SOMMXPROCS\"
+  exit 1
+fi
+EOF"
+
+      # stopsommelier
+      system "cat <<'EOF'> stopsommelier
+#!/bin/bash
+SOMM=\$(pgrep -fc sommelier.elf 2> /dev/null)
+if [[ \"$SOMM\" -gt \"0\" ]]; then
+  pkill -f sommelier.elf &>/dev/null
+  pkill -F #{CREW_PREFIX}/var/run/sommelier-wayland.pid &>/dev/null
+  pkill -F #{CREW_PREFIX}/var/run/sommelier-xwayland.pid &>/dev/null
+else
+  exit 0
+fi
+if [[ \"$(pgrep -fc sommelier.elf 2> /dev/null)\" -gt \"0\" ]]; then
+  echo \"sommelier failed to stop\"
+  exit 1
+else
+  echo \"sommelier stopped\"
+fi
+EOF"
+
+      # restartsommelier
+      system "echo '#!/bin/bash' > restartsommelier"
+      system "echo 'stopsommelier && startsommelier' >> restartsommelier"
+      end
     end
   end
-
+    
   def self.install
-    Dir.chdir ("sommelier") do
-      system "make", "PREFIX=#{CREW_PREFIX}", "SYSCONFDIR=#{CREW_PREFIX}/etc", "DESTDIR=#{CREW_DEST_DIR}", "install"
-      system "install -Dm755 sommelierd #{CREW_DEST_PREFIX}/sbin/sommelierd"
-      system "install -Dm755 initsommelier #{CREW_DEST_PREFIX}/bin/initsommelier"
-      system "install -Dm755 stopsommelier #{CREW_DEST_PREFIX}/bin/stopsommelier"
-      system "install -Dm755 startsommelier #{CREW_DEST_PREFIX}/bin/startsommelier"
-      system "install -Dm644 .sommelier.env #{CREW_DEST_HOME}/.sommelier.env"
+    Dir.chdir ("vm_tools/sommelier") do
+      system "DESTDIR=#{CREW_DEST_DIR} ninja -C build install"
+      Dir.chdir ("build") do
+        FileUtils.mv "#{CREW_DEST_PREFIX}/bin/sommelier", "#{CREW_DEST_PREFIX}/bin/sommelier.elf"
+        system "install -Dm755 sommelier_sh #{CREW_DEST_PREFIX}/bin/sommelier"
+        system "install -Dm755 sommelierd #{CREW_DEST_PREFIX}/sbin/sommelierd"
+        system "install -Dm755 startsommelier #{CREW_DEST_PREFIX}/bin/startsommelier"
+        FileUtils.ln_sf 'startsommelier', "#{CREW_DEST_PREFIX}/bin/initsommelier"
+        system "install -Dm755 stopsommelier #{CREW_DEST_PREFIX}/bin/stopsommelier"
+        system "install -Dm755 restartsommelier #{CREW_DEST_PREFIX}/bin/restartsommelier"
+        system "install -Dm755 sommelierrc #{CREW_DEST_PREFIX}/etc/sommelierrc"
+        system "install -Dm644 .sommelier.env #{CREW_DEST_HOME}/.sommelier.env"
+
+      end
     end
   end
-
+  
   def self.postinstall
     puts
     puts "To complete the installation, execute the following:".lightblue
     puts "echo '# Sommelier environment variables + daemon' >> ~/.bashrc".lightblue
     puts "echo '# See https://github.com/dnschneid/crouton/wiki/Sommelier-(A-more-native-alternative-to-xiwi)' >> ~/.bashrc".lightblue
-    puts "echo 'if [ ! -d /tmp/.X11-unix ]; then' >> ~/.bashrc".lightblue
-    puts "echo 'mkdir /tmp/.X11-unix' >> ~/.bashrc".lightblue
-    puts "echo 'fi' >> ~/.bashrc".lightblue
-    puts "echo 'sudo chmod -R 1777 /tmp/.X11-unix' >> ~/.bashrc".lightblue
-    puts "echo 'sudo chown root:root /tmp/.X11-unix' >> ~/.bashrc".lightblue
+    puts "echo 'set -a && source ~/.sommelier.env && set +a' >> ~/.bashrc".lightblue
     puts "echo 'startsommelier' >> ~/.bashrc".lightblue
     puts "source ~/.bashrc".lightblue
     puts
+    puts "To adjust environment variables, edit ~/.sommelier.env".lightblue
+    puts "e.g. You may need to adjust the SCALE environment variable to".lightblue
+    puts "get the correct screen size.".lightblue
+    puts
     puts "To start the sommelier daemon, run 'startsommelier'".lightblue
     puts "To stop the sommelier daemon, run 'stopsommelier'".lightblue
+    puts "To restart the sommelier daemon, run 'restartsommelier'".lightblue
     puts
-    puts "To adjust environment variables, edit ~/.sommelier.env".lightblue
+    puts "Please be aware that gui applications may not work without the".orange
+    puts "sommelier daemon running.".orange
     puts
-    puts "You may need to adjust the SCALE environment variable to get the correct screen size.".lightblue
+    puts "The sommelier daemon may also have to be restarted with".orange
+    puts "'restartsommelier'after waking your device.".orange
     puts
-    puts "Please be aware that gui applications may not work without the sommelier daemon running.".orange
-    puts
-    puts "If you are upgrading from an earlier version of sommelier, run".orange
-    puts "'stopsommelier && startsommelier' or logout and login again.".orange
-    puts
+    puts "(If you are upgrading from an earlier version of sommelier,".orange
+    puts "also run 'restartsommelier'.)".orange
   end
 end
---
+++ packages/xwayland.rb
@@ -0,0 +1,141 @@
+require 'package'
+
+class Xwayland < Package
+  description 'X server configured to work with weston or sommelier'
+  homepage 'https://x.org'
+  version '1.20.9-1'
+  # Using xorg ToT due to large number of recent xwayland commits.
+  source_url 'https://github.com/freedesktop/xorg-xserver/archive/d18dcecbe08a9ff22e43f12b6b7679a6ef1a6eb0.zip'
+  source_sha256 '091edf47059adfa09242906db97e1d9d44dc6557efde4af861a7055975fa55ce'
+
+  
+  depends_on 'libepoxy'
+  depends_on 'xorg_proto'
+  depends_on 'libxtrans'
+  depends_on 'libxkbfile'
+  depends_on 'wayland'
+  depends_on 'eudev'
+  depends_on 'libxfont'
+  depends_on 'libbsd'
+  depends_on 'nettle'
+  depends_on 'libtirpc'
+  depends_on 'pixman'
+  depends_on 'xinit'
+  depends_on 'libxkbcommon'
+  depends_on 'libunwind'
+  depends_on 'font_util'
+  depends_on 'xorg_lib'
+  depends_on 'libtirpc'
+  depends_on 'font_util'
+  depends_on 'libbsd'
+  depends_on 'dbus'
+  depends_on 'lzma' => :build
+  depends_on 'xkbcomp'
+  depends_on 'glproto'
+  depends_on 'mesa'
+
+  # Patches are from Google xwayland overlay at https://source.chromium.org/chromiumos/chromiumos/codesearch/+/master:src/third_party/chromiumos-overlay/x11-base/xwayland/files/
+  def self.patch
+    url_patch1 = "https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/067ac4b5060c16e6687a97cbb4bcdbaf5a0b5639/x11-base/xwayland/files/0001-Eliminate-conflict-with-X11-Xlib.h-with-khronos-eglp.patch?format=TEXT"
+    uri_patch1 = URI.parse url_patch1
+    filename_patch1 = 'patch1_base64'
+    sha256sum_patch1 = '16f5d5d1986daf07b4e0f01eb0e68b53d8ef301d169f3cc9493158c8b4987f16'
+
+    puts "Downloading patch1".yellow
+    system('curl', '-s', '-C', '-', '--insecure', '-L', '-#', url_patch1, '-o', filename_patch1)
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless
+      Digest::SHA256.hexdigest( File.read( filename_patch1 ) ) == sha256sum_patch1
+    puts "patch1 archive downloaded".lightgreen
+    system 'base64 --decode patch1_base64 > patch1'
+    system 'patch -p 1 < patch1'
+
+    url_patch2 = "https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/067ac4b5060c16e6687a97cbb4bcdbaf5a0b5639/x11-base/xwayland/files/0001-xwayland-Fall-back-to-gbm_bo_create-if-no-modifiers-.patch?format=TEXT"
+    uri_patch2 = URI.parse url_patch2
+    filename_patch2 = 'patch2_base64'
+    sha256sum_patch2 = 'd44bfca0e26ca06892b37b12faca2a6756cdc2455aac36ac7607bfa188856b35'
+
+    puts "Downloading patch2".yellow
+    system('curl', '-s', '-C', '-', '--insecure', '-L', '-#', url_patch2, '-o', filename_patch2)
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless
+      Digest::SHA256.hexdigest( File.read( filename_patch2 ) ) == sha256sum_patch2
+    puts "patch2 archive downloaded".lightgreen
+    system 'base64 --decode patch2_base64 > patch2'
+    system 'patch -p 1 < patch2'
+
+    
+    url_patch3 = "https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/067ac4b5060c16e6687a97cbb4bcdbaf5a0b5639/x11-base/xwayland/files/0001-xwayland-sysmacros.patch?format=TEXT"
+    uri_patch3 = URI.parse url_patch3
+    filename_patch3 = 'patch3_base64'
+    sha256sum_patch3 = 'b66eb66cc8023ab2170db91e4d6038dca99b814addfcb0977d30479a2e514ab8'
+
+    puts "Downloading patch3".yellow
+    system('curl', '-s', '-C', '-', '--insecure', '-L', '-#', url_patch3, '-o', filename_patch3)
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless
+      Digest::SHA256.hexdigest( File.read( filename_patch3 ) ) == sha256sum_patch3
+    puts "patch3 archive downloaded".lightgreen
+    system 'base64 --decode patch3_base64 > patch3'
+    system 'patch -F 10 --force -p 1 < patch3'
+  
+  end
+
+  case ARCH
+   when 'armv7l', 'aarch64'
+      PEER_CMD_PREFIX='/lib/ld-linux-armhf.so.3'
+   when 'i686'
+      PEER_CMD_PREFIX='/lib/ld-linux-i686.so.2'
+   when 'x86_64'
+      PEER_CMD_PREFIX='/lib64/ld-linux-x86-64.so.2'
+  end
+
+  def self.build
+      ENV['CFLAGS'] = "-fuse-ld=lld"
+      ENV['CXXFLAGS'] = "-fuse-ld=lld"
+      system "meson setup build"
+      system "meson configure #{CREW_MESON_OPTIONS} \
+              -Dipv6=true \
+              -Dxvfb=true \
+              -Dxnest=true \
+              -Dxcsecurity=true \
+              -Dxorg=true \
+              -Dxephyr=false \
+              -Dxwayland=true \
+              -Dglamor=true \
+              -Dudev=true \
+              -Ddocs=false \
+              -Dxwin=false \
+              -Dsystemd_logind=false \
+              -Dlibunwind=true \
+              -Dint10=false \
+              -Dlog_dir=#{CREW_PREFIX}/var/log \
+              build"
+      system "meson configure build"
+      system "ninja -C build"
+
+      system "cat <<'EOF'> Xwayland_sh
+#!/bin/bash
+if base=$(readlink \"$0\" 2>/dev/null); then
+  case $base in
+  /*) base=$(readlink -f \"$0\" 2>/dev/null);; # if $0 is abspath symlink, make symlink fully resolved.
+  *)  base=$(dirname \"$0\")/\"${base}\";;
+  esac
+else
+  case $0 in
+  /*) base=$0;;
+  *)  base=${PWD:-`pwd`}/$0;;
+  esac
+fi
+basedir=${base%/*}
+# TODO(crbug/1003841): Remove LD_ARGV0 once
+# ld.so supports forwarding the binary name.
+LD_ARGV0=\"$0\" LD_ARGV0_REL=\"../bin/Xwayland.sh\" exec   \"${basedir}/..#{PEER_CMD_PREFIX}\"   --library-path \"${basedir}/../#{ARCH_LIB}\"   --inhibit-rpath ''   \"${base}.elf\"   \"$@\"
+EOF"
+
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C build install"
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/Xwayland", "#{CREW_DEST_PREFIX}/bin/Xwayland.elf"
+    system "install -Dm755 Xwayland_sh #{CREW_DEST_PREFIX}/bin/Xwayland"
+    FileUtils.ln_sf "#{CREW_DEST_PREFIX}/bin/Xwayland", "#{CREW_DEST_PREFIX}/bin/X"
+    end
+end
