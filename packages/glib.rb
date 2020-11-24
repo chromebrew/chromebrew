01052f511b30a043cae30ab9b08191a21469764a
satmandu <satadru@umich.edu>
2020-11-24 16:38:38 -0500

    Fix iconv issue, don't use dev version (#4605)

--- packages/glib.rb
+++ packages/glib.rb
@@ -3,19 +3,17 @@ require 'package'
 class Glib < Package
   description 'GLib provides the core application building blocks for libraries and applications written in C.'
   homepage 'https://developer.gnome.org/glib'
-  version '2.67.0'
+  version '2.66.3'
   compatibility 'all'
-  source_url 'https://download.gnome.org/sources/glib/2.67/glib-2.67.0.tar.xz'
-  source_sha256 '0b15e57ab6c2bb90ced4e24a1b0d8d6e9a13af8a70266751aa3a45baffeed7c1'
+  source_url 'https://download.gnome.org/sources/glib/2.66/glib-2.66.3.tar.xz'
+  source_sha256 '79f31365a99cb1cc9db028625635d1438890702acde9e2802eae0acebcf7b5b1'
 
 
   depends_on 'util_linux'
   depends_on 'six'
-  
-  ENV['LDFLAGS'] = "-liconv"
 
   def self.build
-    system "meson #{CREW_MESON_OPTIONS} -Dinternal_pcre=true builddir"
+    system "meson #{CREW_MESON_OPTIONS} -Dinternal_pcre=true -Diconv=external builddir"
     system "ninja -C builddir"
   end
 
