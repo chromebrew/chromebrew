cfb3e03e2e0f1a0ee2ae922300cf6c83ef5f6f4e
supechicken <73414985+supechicken@users.noreply.github.com>
2020-11-25 23:58:38 +0800

    Update screenfetch.rb

--- packages/screenfetch.rb
+++ packages/screenfetch.rb
@@ -5,17 +5,12 @@ class Screenfetch < Package
   homepage 'https://github.com/KittyKatt/screenFetch'
   version '3.9.1'
   compatibility 'all'
-  source_url 'file:///dev/null'
-  source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  source_url 'https://github.com/KittyKatt/screenFetch/archive/v3.9.1.tar.gz'
+  source_sha256 'aa97dcd2a8576ae18de6c16c19744aae1573a3da7541af4b98a91930a30a3178'
 
   depends_on 'bc'
 
   def self.build
-    system "curl https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch-dev -#o screenfetch-dev"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("screenfetch-dev") ) == '8841eabb5aa7c319f2b1f87dbac75a07f425b67888154a8779976319603732d0'
-    system "curl https://raw.githubusercontent.com/KittyKatt/screenFetch/master/screenfetch.1 -#o screenfetch.1"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("screenfetch.1") ) == '77ee19b1dc5c91e80d5d0d8e983b4c39928d58b0411b5f603fd7b26b765b6023'
-    puts "Archive downloaded".lightgreen
   end
 
   def self.install
