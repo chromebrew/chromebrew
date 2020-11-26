72796f411d74563a24e3dd424a0ad81a0639c6f1
satmandu <satadru@umich.edu>
2020-11-26 16:41:08 -0500

    Bump version

--- packages/glib.rb
+++ packages/glib.rb
@@ -3,7 +3,7 @@ require 'package'
 class Glib < Package
   description 'GLib provides the core application building blocks for libraries and applications written in C.'
   homepage 'https://developer.gnome.org/glib'
-  version '2.66.3'
+  version '2.66.3-1'
   compatibility 'all'
   source_url 'https://download.gnome.org/sources/glib/2.66/glib-2.66.3.tar.xz'
   source_sha256 '79f31365a99cb1cc9db028625635d1438890702acde9e2802eae0acebcf7b5b1'
