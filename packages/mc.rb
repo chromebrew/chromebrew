a8e39a8e8e0ade10e36ba70085aa9be876e35e6e
supechicken <73414985+supechicken@users.noreply.github.com>
2020-11-28 00:21:25 +0800

    Remove dependencies duplicate with core packages

--- packages/mc.rb
+++ packages/mc.rb
@@ -8,12 +8,8 @@ class Mc < Package
   source_url 'https://github.com/MidnightCommander/mc/archive/4.8.25.tar.gz'
   source_sha256 '8a4aa1556a528fbe9140c47aa3d0665155187468a17c3b80a824de5fcbbebae1'
 
-  depends_on 'automake' => :build
-  depends_on 'libtool' => :build
   depends_on 'glib' => :build
-  depends_on 'libssh2'
   depends_on 'aspell'
-  depends_on 'slang'
     
 ENV['LIBS'] = "-ldl -lssl -lcrypto"
 
