2996ffe52204d4ebf5414c4f4badbdd8ae5e5e10
satmandu <satadru@umich.edu>
2020-11-27 14:37:23 -0500

    gcr -> 3.38, remove pygtk dep (#4647)

--- packages/gcr.rb
+++ packages/gcr.rb
@@ -3,23 +3,11 @@ require 'package'
 class Gcr < Package
   description 'GNOME crypto package'
   homepage 'https://www.gnome.org'
-  version '3.36.0'
+  version '3.38.0'
   compatibility 'aarch64,armv7l,x86_64'
-  source_url 'https://ftp.gnome.org/pub/GNOME/sources/gcr/3.36/gcr-3.36.0.tar.xz'
-  source_sha256 'aaf9bed017a2263c6145c89a1a84178f9f40f238426463e4ae486694ef5f6601'
+  source_url 'https://download.gnome.org/sources/gcr/3.38/gcr-3.38.0.tar.xz'
+  source_sha256 'a64cc7b65757fc2cd16de1708d132a16d05cd1f62c6eba436d56fe45d4ba27e1'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gcr-3.36.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gcr-3.36.0-chromeos-armv7l.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gcr-3.36.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'e0830fd1ebb83ec32587d1c299565262c0af0b1d8ea893f3d92572b3b44d85df',
-     armv7l: 'e0830fd1ebb83ec32587d1c299565262c0af0b1d8ea893f3d92572b3b44d85df',
-     x86_64: '9417383cdb622492605252348a7b966041fbfea37d52d8467f9c8328e6d2050c',
-  })
-
-  depends_on 'pygtk'
   depends_on 'libgcrypt'
   depends_on 'libxslt'
   depends_on 'desktop_file_utilities'
@@ -31,11 +19,12 @@ class Gcr < Package
   depends_on 'vala' => :build
 
   def self.build
-    system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} _build -Dgtk_doc=false" # Due to issues with gtk_doc it has been disabled; I will revist this when gtk_doc is fixed
-    system 'ninja -v -C _build'
+    system "meson #{CREW_MESON_OPTIONS} build -Dgtk_doc=false"
+    system 'meson configure build'
+    system 'ninja -v -C build'
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C _build install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C build install"
   end
 end
