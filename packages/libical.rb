226c9ffa580f7ae14494852ed36e3ad5ef4fe5e4
satmandu <satadru@umich.edu>
2020-11-28 13:52:13 -0500

    update formatting, remove doxygen from deps

--- packages/libical.rb
+++ packages/libical.rb
@@ -8,21 +8,20 @@ class Libical < Package
   source_sha256 '5fdf39b0ea654589bf2e9aa88ef1b0206c18aa6987f5a4299db3748e8b1eefd6'
 
   depends_on 'glib'
-  depends_on 'icu'
+  depends_on 'icu4c'
   depends_on 'gtk_doc' => ':build'
-  depends_on 'doxygen' => ':build'
   depends_on 'vala' => ':build'
   depends_on 'gobject_introspection' => ':build'
 
-def self.build
-    Dir.mkdir 'build'
-      Dir.chdir 'build' do
-      system "cmake #{CREW_CMAKE_OPTIONS} .. -G Ninja \
-      -DGOBJECT_INTROSPECTION=true \
-      -DICAL_GLIB_VAPI=true \
-      -DICAL_BUILD_DOCS=false \
-      -DLIBICAL_BUILD_TESTING=false"
-      end
+  def self.build
+  Dir.mkdir 'build'
+    Dir.chdir 'build' do
+    system "cmake #{CREW_CMAKE_OPTIONS} .. -G Ninja \
+    -DGOBJECT_INTROSPECTION=true \
+    -DICAL_GLIB_VAPI=true \
+    -DICAL_BUILD_DOCS=false \
+    -DLIBICAL_BUILD_TESTING=false"
+    end
   system "ninja -C build"
   end
   
