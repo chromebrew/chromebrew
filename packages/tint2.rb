9afe2936e6090d19033bea33e3252068f84c11a2
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-11-28 15:42:31 +0800

    Add compatibility

--- packages/tint2.rb
+++ packages/tint2.rb
@@ -4,6 +4,7 @@ class Tint2 < Package
   description 'A lightweight panel/taskbar for Linux and BSD'
   homepage 'https://gitlab.com/o9000/tint2'
   version '16.7'
+  compatibility 'all'
   source_url 'https://gitlab.com/o9000/tint2/-/archive/v16.7/tint2-v16.7.tar.gz'
   source_sha256 '74962c1dd111be9955319a133a790b24fe5a8ef0315a1bcdabc58c1157c1fb26'
     
