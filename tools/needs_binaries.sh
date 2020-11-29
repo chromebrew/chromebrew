12bdcca7b96e0f292f1e87ca8d2826e20866bc0e
Ed Reel <edreel@gmail.com>
2020-11-28 23:02:12 -0600

    Add more package exclusions to needs_binaries.sh (#4664)

--- tools/needs_binaries.sh
+++ tools/needs_binaries.sh
@@ -12,18 +12,17 @@ exclusions+=' komodo.rb kotlin.rb kr.rb ld_default.rb leiningen.rb libtinfo.rb l
 exclusions+=' ngrok.rb nodebrew.rb nvm.rb oc.rb packer.rb papirus_icon_theme.rb perl_gcstring_linebreak.rb perl_io_socket_ssl.rb perl_locale_gettext.rb'
 exclusions+=' perl_locale_messages.rb perl_module_build.rb perl_read_key.rb perl_sgmls.rb perl_term_ansicolor.rb perl_text_charwidth.rb perl_text_unidecode.rb'
 exclusions+=' perl_text_wrapi18n.rb perl_time_hires.rb perl_unicode_eastasianwidth.rb perl_xml_parser.rb perl_xml_sax_parserfactory.rb'
-exclusions+=' perl_xml_simple.rb pipes_sh.rb pipesx_sh.rb platformsh.rb pngcheck.rb powershell.rb pup.rb pycharm.rb qtcreator.rb sl.rb spark.rb squeak.rb'
-exclusions+=' stack.rb sublime_merge.rb sublime_text.rb symfony.rb terminus.rb terraform.rb thefuck.rb tinycore.rb tkdiff.rb txt2regex.rb uthash.rb uwsgi.rb'
+exclusions+=' perl_xml_simple.rb pipes_sh.rb pipesx_sh.rb platformsh.rb pngcheck.rb powershell.rb pup.rb pycharm.rb qtcreator.rb sl.rb sommelier.rb spark.rb'
+exclusions+=' squeak.rb stack.rb sublime_merge.rb sublime_text.rb symfony.rb terminus.rb terraform.rb thefuck.rb tinycore.rb tkdiff.rb txt2regex.rb uthash.rb uwsgi.rb'
 exclusions+=' v2ray.rb wp_cli.rb xdg_base.rb xz_java.rb yarn.rb'
 if [[ "${arch}" == 'aarch64' || "${arch}" == 'armv7l' ]]; then
-  exclusions+=' atom.rb az.rb cf.rb brackets.rb clisp.rb dropbox.rb fakeroot_ng.rb ghostscript.rb gitkraken.rb handbrake.rb hplip.rb miniconda3.rb'
-  exclusions+=' misctools.rb oci.rb nethack4.rb obs.rb opera.rb sbcl.rb skype.rb weston.rb wing.rb wkhtmltox.rb xorg_intel_driver.rb'
-  exclusions+=' xorg_vmmouse_driver.rb'
+  exclusions+=' atom.rb az.rb cf.rb brackets.rb clisp.rb dropbox.rb fakeroot_ng.rb ghostscript.rb gitkraken.rb handbrake.rb hplip.rb jdk11.rb miniconda3.rb'
+  exclusions+=' misctools.rb oci.rb nethack4.rb obs.rb opera.rb sbcl.rb skype.rb weston.rb wing.rb wkhtmltox.rb xorg_intel_driver.rb xorg_vmmouse_driver.rb'
 fi
 if [[ "${arch}" == 'i686' ]]; then
-  exclusions+=' aqemu.rb atom.rb codelite.rb cras.rb dia.rb exa.rb fakeroot_ng.rb fzf.rb gcr.rb geany.rb gemacs.rb gimp.rb gitkraken.rb gnome_keyring.rb gtk_engines_adwaita.rb'
-  exclusions+=' handbrake.rb imagemagick6.rb imagemagick7.rb libgnome_keyring.rb librespot.rb libvncserver.rb neovim.rb opera.rb poppler.rb ripgrep.rb skype.rb tcpflow.rb'
-  exclusions+=' wing.rb xorg_intel_driver.rb zathura.rb'
+  exclusions+=' aqemu.rb atom.rb codelite.rb cras.rb dia.rb exa.rb fakeroot_ng.rb fzf.rb gcr.rb geany.rb geany_plugins.rb gemacs.rb gimp.rb gitkraken.rb gnome_keyring.rb'
+  exclusions+=' gtk_engines_adwaita.rb handbrake.rb heroku.rb imagemagick6.rb imagemagick7.rb jdk11.rb libgnome_keyring.rb librespot.rb libvncserver.rb neovim.rb opera.rb'
+  exclusions+=' pipewire.rb poppler.rb qtdeclarative.rb qtfm.rb qttools.rb ripgrep.rb skype.rb tcpflow.rb vivaldi.rb wing.rb xorg_intel_driver.rb zathura.rb'
 fi
 packages=$(grep -L "${arch}:" *.rb)
 for p in ${packages}; do
