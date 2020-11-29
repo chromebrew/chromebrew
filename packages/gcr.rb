e34ffcb4dbb1c8ffd404ec850c3fd79a1421f38c
Ed Reel <edreel@gmail.com>
2020-11-29 13:50:49 -0600

    Add pre-built binaries for gcr and libical

--- packages/gcr.rb
+++ packages/gcr.rb
@@ -5,20 +5,33 @@ class Gcr < Package
   homepage 'https://www.gnome.org'
   version '3.38.0'
   compatibility 'aarch64,armv7l,x86_64'
-  source_url 'https://download.gnome.org/sources/gcr/3.38/gcr-3.38.0.tar.xz'
-  source_sha256 'a64cc7b65757fc2cd16de1708d132a16d05cd1f62c6eba436d56fe45d4ba27e1'
+  case ARCH
+  when 'aarch64', 'armv7l', 'x86_64'
+    source_url 'https://download.gnome.org/sources/gcr/3.38/gcr-3.38.0.tar.xz'
+    source_sha256 'a64cc7b65757fc2cd16de1708d132a16d05cd1f62c6eba436d56fe45d4ba27e1'
+    depends_on 'libgcrypt'
+    depends_on 'libxslt'
+    depends_on 'desktop_file_utilities'
+    depends_on 'hicolor_icon_theme'
+    depends_on 'gnupg'
+    depends_on 'glib'
+    depends_on 'gnupg'
+    depends_on 'libxslt'
+    depends_on 'vala' => :build
+    depends_on 'gtk3'
+    depends_on 'graphite'
+  end
 
-  depends_on 'libgcrypt'
-  depends_on 'libxslt'
-  depends_on 'desktop_file_utilities'
-  depends_on 'hicolor_icon_theme'
-  depends_on 'gnupg'
-  depends_on 'glib'
-  depends_on 'gnupg'
-  depends_on 'libxslt'
-  depends_on 'vala' => :build
-  depends_on 'gtk3'
-  depends_on 'graphite'
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gcr-3.38.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gcr-3.38.0-chromeos-armv7l.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gcr-3.38.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'de66b71446151441ac30d62cf29e92009a999370dda3e4ad5b08ecdb0441eda6',
+     armv7l: 'de66b71446151441ac30d62cf29e92009a999370dda3e4ad5b08ecdb0441eda6',
+     x86_64: '3690fa506949ac8b4ee9ebe189d751086f945d782993ef80777fe6ac0d6ccb9f',
+  })
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} build -Dgtk_doc=false"
