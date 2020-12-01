d7ac99936c705ecead94a7ddcd33c8765fa13caa
satmandu <satadru@umich.edu>
2020-12-01 15:37:03 -0500

    libxml update for ICU 68

--- packages/libxml2.rb
+++ packages/libxml2.rb
@@ -3,26 +3,30 @@ require 'package'
 class Libxml2 < Package
   description 'Libxml2 is the XML C parser and toolkit developed for the Gnome project.'
   homepage 'http://xmlsoft.org/'
-  version '2.9.10'
+  version '2.9.10-1'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.10/libxml2-v2.9.10.tar.bz2'
   source_sha256 '5f1cc19c849cccabb983881cf1ebf833f42db5d7b8afba7a7763a2ac3101715c'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.10-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.10-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.10-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libxml2-2.9.10-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '6c7da2f6074adc4022ade9e72a070037657a1c708150404c5b9c7d07634c5390',
-     armv7l: '6c7da2f6074adc4022ade9e72a070037657a1c708150404c5b9c7d07634c5390',
-       i686: '9baf9a41ad01e2b6d09f09c9a65a74b37ac839e53ce00c0ebf048bc1d1d7b01f',
-     x86_64: '4c1305434bbac822e6d08234d3058e94bca0ab95591ca25a0ccc8675cd16c48e',
-  })
+
 
   depends_on 'zlibpkg'
 
+  def self.patch
+    # Patch to work with icu .68.1
+    url_patch1 = "https://gitlab.gnome.org/GNOME/libxml2/-/commit/b516ed189eb440e909f36baca1557b98e4d9ffd7.patch"
+    uri_patch1 = URI.parse url_patch1
+    filename_patch1 = 'patch1'
+    sha256sum_patch1 = 'f02a435761f26ff664041d49f9d05924dc627bf103c7f542feee891f69aa84a2'
+
+    puts "Downloading patch1".yellow
+    system('curl', '-s', '--insecure', '-L', '-#', url_patch1, '-o', filename_patch1)
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless
+      Digest::SHA256.hexdigest( File.read( filename_patch1 ) ) == sha256sum_patch1
+    puts "patch1 downloaded".lightgreen
+    system 'patch -p 1 < patch1'
+  end
+    
   def self.build
     system './autogen.sh'
     # libxml2-python built in another package (libxml2_python)
