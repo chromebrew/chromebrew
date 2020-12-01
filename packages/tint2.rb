4f0b4ec9f0771366d33bd8ef320df773ac5057a8
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2020-12-01 22:58:06 +0800

    New Packages: Tint2, Nitrogen (#4655)
    * Add tint2.rb nitrogen.rb
    
    * Unhardcode prefix
    
    * Add compatibility
    
    * Update tint2.rb

---
+++ packages/tint2.rb
@@ -0,0 +1,30 @@
+require 'package'
+
+class Tint2 < Package
+  description 'A lightweight panel/taskbar for Linux and BSD'
+  homepage 'https://gitlab.com/o9000/tint2'
+  version '16.7'
+  compatibility 'all'
+  source_url 'https://gitlab.com/o9000/tint2/-/archive/v16.7/tint2-v16.7.tar.gz'
+  source_sha256 '74962c1dd111be9955319a133a790b24fe5a8ef0315a1bcdabc58c1157c1fb26'
+    
+  depends_on 'imlib2'
+  depends_on 'gdk_pixbuf'
+    
+  ENV['CFLAGS'] = "-lX11 -lXau"
+
+
+  def self.build
+   Dir.mkdir 'build'
+   Dir.chdir 'build' do
+     system "cmake .. -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} -DCMAKE_LIBRARY_PATH=#{CREW_LIB_PREFIX}"
+     system 'make'
+   end
+  end
+
+  def self.install
+     Dir.chdir 'build' do
+      system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+     end
+  end
+end
