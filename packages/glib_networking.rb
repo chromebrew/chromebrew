411dcc3b2f13aceb82d3785cc45400eecbe90167
satmandu <satadru@umich.edu>
2020-12-04 00:40:46 -0500

    glib_networking -> 2.66.0 (#4696)
    * glib_networking -> 2.72

--- packages/glib_networking.rb
+++ packages/glib_networking.rb
@@ -3,31 +3,20 @@ require 'package'
 class Glib_networking < Package
   description 'Network extensions for GLib'
   homepage 'https://github.com/GNOME/glib-networking'
-  version '2.57.3'
+  version '2.66.0'
   compatibility 'all'
-  source_url 'https://github.com/GNOME/glib-networking/archive/2.57.3.tar.gz'
-  source_sha256 '7e45263dc9ece73e6370db4f61ccbac14a37f68d66f487417bc319e5f948a82d'
+  source_url 'https://github.com/GNOME/glib-networking/archive/2.66.0.tar.gz'
+  source_sha256 '186a670c00525d62aa160bc3e492d9efd2f59c540c50477982eb732ed62ee96c'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.57.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.57.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.57.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.57.3-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'f2a9ca1aa951dc280c405252f7a0d52fa61ff6f0d4452e2c91ee5e46d8d1221b',
-     armv7l: 'f2a9ca1aa951dc280c405252f7a0d52fa61ff6f0d4452e2c91ee5e46d8d1221b',
-       i686: 'e0e0e3327b782cb8a3e9126c7fa3264bdb5e006c30597da66438735087fa3e16',
-     x86_64: '1cc6f98c79144848a68189470e195518348a997673a01474ec41f0cc63b72134',
-  })
 
   depends_on 'libproxy'
   depends_on 'gsettings_desktop_schemas'
   depends_on 'meson' => :build
 
   def self.build
-    system 'meson builddir'
-    system 'ninja -v -C builddir'
+    system "meson #{CREW_MESON_OPTIONS} -Dstatic_modules=true builddir"
+    system "meson configure builddir"
+    system "ninja -C builddir"
   end
 
   def self.install
