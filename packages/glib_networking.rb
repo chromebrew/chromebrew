e9c04c9633821049a1ff1051623264f7782d6d62
Ed Reel <edreel@gmail.com>
2020-12-05 16:50:33 -0600

    Add pre-built binaries for packages b-x (#4708)

--- packages/glib_networking.rb
+++ packages/glib_networking.rb
@@ -8,6 +8,18 @@ class Glib_networking < Package
   source_url 'https://github.com/GNOME/glib-networking/archive/2.66.0.tar.gz'
   source_sha256 '186a670c00525d62aa160bc3e492d9efd2f59c540c50477982eb732ed62ee96c'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.66.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.66.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.66.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glib_networking-2.66.0-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '5ec999a6e4353ea7d38d018e29f6e3afb66203d8336135edcd773028715e0955',
+     armv7l: '5ec999a6e4353ea7d38d018e29f6e3afb66203d8336135edcd773028715e0955',
+       i686: '9336de8df82c434274c2b7ccdb206dc4bd3603ec1e9e8f97b85071bf97524131',
+     x86_64: 'fd74a47da0dce066856b4105673187db2e411d032e59ea8a1f3df646209ccb32',
+  })
 
   depends_on 'libproxy'
   depends_on 'gsettings_desktop_schemas'
@@ -15,8 +27,8 @@ class Glib_networking < Package
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} -Dstatic_modules=true builddir"
-    system "meson configure builddir"
-    system "ninja -C builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
   end
 
   def self.install
--- packages/mc.rb
+++ packages/mc.rb
@@ -8,11 +8,23 @@ class Mc < Package
   source_url 'https://github.com/MidnightCommander/mc/archive/4.8.25.tar.gz'
   source_sha256 '8a4aa1556a528fbe9140c47aa3d0665155187468a17c3b80a824de5fcbbebae1'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mc-4.8.25-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mc-4.8.25-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mc-4.8.25-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mc-4.8.25-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '5b2e335ad9f3fff1ee7f64f79c3bc0b4b15fd51a6974fa6f11254a99683b887d',
+     armv7l: '5b2e335ad9f3fff1ee7f64f79c3bc0b4b15fd51a6974fa6f11254a99683b887d',
+       i686: 'be1e4b7953d540ef610db499db67c55354889c4d305ca96a9dd2a7b90300f3d9',
+     x86_64: 'be58cc2c96f8cc3c5caddaab9488aa727cc48353e11f9784e7535852886c066c',
+  })
+
   depends_on 'glib' => :build
   depends_on 'aspell'
-    
-ENV['LIBS'] = "-ldl -lssl -lcrypto"
 
+  ENV['LIBS'] = '-ldl -lssl -lcrypto'
   def self.build
     system './autogen.sh'
     system "./configure #{CREW_OPTIONS}"
@@ -20,6 +32,6 @@ ENV['LIBS'] = "-ldl -lssl -lcrypto"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
