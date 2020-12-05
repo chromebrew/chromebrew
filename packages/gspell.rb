f6dc0b11b05cac09f9dcb824cd59c23010d3faf8
Ed Reel <edreel@gmail.com>
2020-12-05 15:24:52 -0600

    Add pre-built binaries for packages g-x

--- packages/gspell.rb
+++ packages/gspell.rb
@@ -7,6 +7,19 @@ class Gspell < Package
   source_url 'https://download.gnome.org/sources/gspell/1.9/gspell-1.9.1.tar.xz'
   source_sha256 'dcbb769dfdde8e3c0a8ed3102ce7e661abbf7ddf85df08b29915e92cd723abdd'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gspell-1.9.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gspell-1.9.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gspell-1.9.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gspell-1.9.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'b804ca54e3ff7f007d078c1a3f329b67131f7e4fe2fb72d835c2d93762093c7b',
+     armv7l: 'b804ca54e3ff7f007d078c1a3f329b67131f7e4fe2fb72d835c2d93762093c7b',
+       i686: 'b05c30504de2f207fca5f4ff92fff5294db9d1479b0404028b5345c972cea38e',
+     x86_64: 'f7c41e8c5b900aa694b002f5134b89d9c9757e7e5f56c060b7f308fb146dd5dc',
+  })
+
   depends_on 'gtk3'
   depends_on 'enchant'
   depends_on 'libxml2'
