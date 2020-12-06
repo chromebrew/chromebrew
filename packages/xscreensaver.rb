bc4cf6f06dcdf3216ba79dbb9c47b5629cdadb43
Ed Reel <edreel@gmail.com>
2020-12-06 09:22:43 -0600

    Update xscreensaver and glfw dependency
    - Update xscreensaver from 5.40 to 5.44
    
    - Update glfw from 3.2.1-1 to 3.3.2

--- packages/xscreensaver.rb
+++ packages/xscreensaver.rb
@@ -3,10 +3,23 @@ require 'package'
 class Xscreensaver < Package
   description 'XScreenSaver is the standard screen saver collection shipped on most Linux and Unix systems running the X11 Window System.'
   homepage 'https://www.jwz.org/xscreensaver/download.html'
-  version '5.40'
+  version '5.44'
   compatibility 'all'
-  source_url 'https://www.jwz.org/xscreensaver/xscreensaver-5.40.tar.gz'
-  source_sha256 '30a0908d4164cf780ef034f87ba884316296b308af2484261ccde86be0c95ae0'
+  source_url 'https://www.jwz.org/xscreensaver/xscreensaver-5.44.tar.gz'
+  source_sha256 '73d8089cfc7d7363b5dac99b5b01dffb3429d0a855e6af16ce9a4b7777017b95'
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xscreensaver-5.44-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xscreensaver-5.44-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xscreensaver-5.44-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xscreensaver-5.44-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '6e6fe321404454f174492ed9b116ed339b7b3053c86d6af7b463d266b4558feb',
+     armv7l: '6e6fe321404454f174492ed9b116ed339b7b3053c86d6af7b463d266b4558feb',
+       i686: '601c1c6c14e128d6ec5bd69949cd7f25e50c7cd29b57cc2faf075ae0c36e0f60',
+     x86_64: '9f318d5f089884acfcef0ccfd0147535a44b38957f94338ff2f97e1cec27f3c0',
+  })
 
   depends_on 'glfw'
   depends_on 'freeglut'
