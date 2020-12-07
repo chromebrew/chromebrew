112026bd7dd1a48065a195e5a06596de78d1ad74
supechicken <73414985+supechicken@users.noreply.github.com>
2020-12-08 00:19:16 +0800

    Update libtinfo.rb

--- packages/libtinfo.rb
+++ packages/libtinfo.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libtinfo < Package
   description 'Missing ncurses library reference.'
   homepage 'https://www.gnu.org/software/ncurses/'
-  version '0.22-1'
+  version '6.2'
   compatibility 'all'
   source_url 'file:///dev/null'
   source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
@@ -18,8 +18,8 @@ class Libtinfo < Package
   def self.install
     # See https://forums.opensuse.org/showthread.php/446927-missing-library-libtinfo-so-5.
     # See also http://www.linuxforums.org/forum/installation/6251-libtinfo-so-5-a.html.
-    FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}"
-    FileUtils.ln_sf "#{CREW_LIB_PREFIX}/libncurses.so.6 #{CREW_DEST_LIB_PREFIX}/libtinfo.so.5"
-    FileUtils.ln_sf "#{CREW_LIB_PREFIX}/libncurses.so.6 #{CREW_DEST_LIB_PREFIX}/libtinfo.so.6"
+    FileUtils.mkdir_p #{CREW_DEST_LIB_PREFIX}
+    FileUtils.ln_sf '#{CREW_LIB_PREFIX}/libncurses.so.6', '#{CREW_DEST_LIB_PREFIX}/libtinfo.so.5'
+    FileUtils.ln_sf '#{CREW_LIB_PREFIX}/libncurses.so.6', '#{CREW_DEST_LIB_PREFIX}/libtinfo.so.6'
   end
 end
