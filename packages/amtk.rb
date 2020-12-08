8d176ee76dd21fd770a7fa8dc8214fa1d9f16aff
Ed Reel <edreel@gmail.com>
2020-12-07 20:49:26 -0600

    Add pre-built binaries for amtk, mesa and weston (#4725)

--- packages/amtk.rb
+++ packages/amtk.rb
@@ -8,6 +8,19 @@ class Amtk < Package
   source_url 'https://github.com/GNOME/amtk/archive/33ec171156ebc49d6dad568e6ba39470edb272e1.zip'
   source_sha256 'fc9bad18bbd5421da52e0548f9e91eda180539bb568d1e7116ca65f4f73f4b67'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/amtk-5.2.0-33ec-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/amtk-5.2.0-33ec-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/amtk-5.2.0-33ec-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/amtk-5.2.0-33ec-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '4605dcc0d4505562e1201ec13471d4afa2ad3be9b1a9685190bd3b9f595cb3cf',
+     armv7l: '4605dcc0d4505562e1201ec13471d4afa2ad3be9b1a9685190bd3b9f595cb3cf',
+       i686: '9a358303f926ef725cf4532df5812304024915af13e90f0bfc12d8f58f77fbad',
+     x86_64: 'f15f58b2e32128bcafffc180fddbce945c70be6bb12241bbf0207cac69a3572e',
+  })
+
   depends_on 'gtk3'
   depends_on 'gobject_introspection' => ':build'
   depends_on 'gtk_doc' => ':build'
