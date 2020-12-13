998d594285b6516c678fc71fcb4205e942bac150
Ed Reel <edreel@gmail.com>
2020-12-13 00:34:57 -0600

    Update core packages b-s

--- packages/libffi.rb
+++ packages/libffi.rb
@@ -3,31 +3,31 @@ require 'package'
 class Libffi < Package
   description 'The libffi library provides a portable, high level programming interface to various calling conventions.'
   homepage 'https://sourceware.org/libffi/'
-  version '3.2.1-2'
+  version '3.3'
   compatibility 'all'
-  source_url 'ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz'
-  source_sha256 'd06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37'
+  source_url 'https://sourceware.org/pub/libffi/libffi-3.3.tar.gz'
+  source_sha256 '72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.2.1-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.2.1-2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.2.1-2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.2.1-2-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libffi-3.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '2af9d89eeb28be4422b2a75218414de25e8d1e7c09efa9fb37660fbd6b6969bd',
-     armv7l: '2af9d89eeb28be4422b2a75218414de25e8d1e7c09efa9fb37660fbd6b6969bd',
-       i686: '32d5df74da5e018709c7c0ca7cf17504face5b951af7ed1c8267980a8ac20d6e',
-     x86_64: '92d528749d53e5140e510864458a4ff46521b2c17c2311f05077707413965e9e',
+    aarch64: '22a2ec5efcbcf5b587f34f35489ee0976f5a1ed256f47a4ac87a240f804c0d02',
+     armv7l: '22a2ec5efcbcf5b587f34f35489ee0976f5a1ed256f47a4ac87a240f804c0d02',
+       i686: '8041e3401d2b9721a2dd87d31aff2b1b5380f7946c092d9a3c18f573ca6422f8',
+     x86_64: 'aecc094eb0e5b961106ee3d4086f120a30555cb82b3b8263107d9cab4e6c278f',
   })
 
   def self.build
-    system "./configure", "--libdir=#{CREW_LIB_PREFIX}", "--enable-shared", "--disable-static", "--with-pic", "--disable-debug", "--disable-dependency-tracking"
-    system "make"
+    system "./configure #{CREW_OPTIONS} --enable-shared --disable-static --with-pic --disable-debug --disable-dependency-tracking"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -3,67 +3,71 @@ require 'package'
 class Ncurses < Package
   description 'The ncurses (new curses) library is a free software emulation of curses in System V Release 4.0 (SVr4), and more.'
   homepage 'https://www.gnu.org/software/ncurses/'
-  version '6.2'
+  version '6.2-20201205'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/ncurses/ncurses-6.2.tar.gz'
-  source_sha256 '30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d'
+  source_url 'https://github.com/mirror/ncurses/archive/42259b594b5dabd37fe2bc294051d2db82e873a2.zip'
+  source_sha256 '782bd5e77fb795f505d6ffd3e443da1cd0ffd6ebb36588a3b7e81e7da34bf340'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-20201205-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-20201205-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-20201205-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ncurses-6.2-20201205-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'e040d9c065046e107be2712904f0452720724254cc36aa7f04056246967b41b5',
-     armv7l: 'e040d9c065046e107be2712904f0452720724254cc36aa7f04056246967b41b5',
-       i686: '715c277f836a8f8f6c40736c667f63913d6e8ee005b4eb258a7a156a6b6db95a',
-     x86_64: '7ac9b423de77550b4ac2fc82ce4d42fc694352d3911918ba265ca2472d595f38',
+    aarch64: '780763658f32364cdc24bef4fb8efcb14d2beba989c7c0a894bf73f3e4fccc9b',
+     armv7l: '780763658f32364cdc24bef4fb8efcb14d2beba989c7c0a894bf73f3e4fccc9b',
+       i686: '3119323613fefd0c8c4d6f0ec6d55d2ae3ec6390aeb19eb01ca47bff4903322f',
+     x86_64: 'd342c29d70e7bb4405555954ee376ff72782b419025b673ee750fe7d0c9efa1f',
   })
 
   def self.build
     # build libncursesw
-    system "mkdir -p ncursesw_build"
-    Dir.chdir("ncursesw_build") do
-      system "../configure",
+    FileUtils.mkdir 'ncursesw_build'
+    Dir.chdir 'ncursesw_build' do
+      system '../configure',
              "--prefix=#{CREW_PREFIX}",
              "--libdir=#{CREW_LIB_PREFIX}",
-             "--without-normal",
-             "--with-shared",
-             "--with-cxx-shared",
-             "--without-debug",
-             "--enable-pc-files",
+             '--with-shared',
+             '--with-cxx-shared',
+             '--without-debug',
+             '--enable-pc-files',
              "--with-pkg-config-libdir=#{CREW_LIB_PREFIX}/pkgconfig",
-             "--enable-widec"
-      system "make"
+             '--enable-widec',
+             '--without-tests',
+             '--with-termlib'
+      system 'make'
     end
 
     # build libncurses
-    system "mkdir -p ncurses_build"
-    Dir.chdir("ncurses_build") do
-      system "../configure",
+    FileUtils.mkdir 'ncurses_build'
+    Dir.chdir 'ncurses_build' do
+      system '../configure',
              "--prefix=#{CREW_PREFIX}",
              "--libdir=#{CREW_LIB_PREFIX}",
-             "--without-normal",
-             "--with-shared",
-             "--with-cxx-shared",
-             "--without-debug",
-             "--enable-pc-files",
+             '--with-shared',
+             '--with-cxx-shared',
+             '--without-debug',
+             '--enable-pc-files',
              "--with-pkg-config-libdir=#{CREW_LIB_PREFIX}/pkgconfig",
-             "--disable-db-install",
-             "--without-manpages",
-             "--without-progs",
-             "--without-tack"
-      system "make"
+             '--disable-db-install',
+             '--without-manpages',
+             '--without-progs',
+             '--without-tack',
+             '--without-tests',
+             '--with-termlib'
+      system 'make'
     end
   end
 
   def self.install
-    Dir.chdir("ncursesw_build") do
-      system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    Dir.chdir 'ncursesw_build' do
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+      FileUtils.ln_sf "#{CREW_LIB_PREFIX}/libtinfow.so.6", "#{CREW_DEST_LIB_PREFIX}/libtinfow.so.5"
     end
-    Dir.chdir("ncurses_build") do
-      system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    Dir.chdir 'ncurses_build' do
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+      FileUtils.ln_sf "#{CREW_LIB_PREFIX}/libtinfo.so.6", "#{CREW_DEST_LIB_PREFIX}/libtinfo.so.5"
     end
   end
 end
