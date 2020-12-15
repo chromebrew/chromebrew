e8275bce37478b49561e3b489551bd7ff322d07c
Casey Strouse <casey.strouse@gmail.com>
2020-12-14 17:28:12 -0700

    Update youtubedl from 2019.10.29 to 2020.12.14 (#4752)
    Tested on x86_64.

--- packages/youtubedl.rb
+++ packages/youtubedl.rb
@@ -3,22 +3,14 @@ require 'package'
 class Youtubedl < Package
   description 'Command-line program to download videos from YouTube.com and other video sites'
   homepage 'http://rg3.github.io/youtube-dl/'
-  version '2019.10.29'
+  version '2020.12.14'
   compatibility 'all'
-  source_url 'https://github.com/ytdl-org/youtube-dl/archive/2019.10.29.tar.gz'
-  source_sha256 'ac2ee960587c069efeda0322f3363ffe4b7b1fd5ad8cb7c35848eae11e767e75'
+  source_url 'https://github.com/ytdl-org/youtube-dl/archive/2020.12.14.tar.gz'
+  source_sha256 '34f5f817bf82bb2f768bb3cf2460e7341e3df3cf8901ed00aebb9b97a308c980'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/youtubedl-2019.10.29-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/youtubedl-2019.10.29-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/youtubedl-2019.10.29-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/youtubedl-2019.10.29-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '72e9c612666916c70df7808ca57164d44723a6bcbf1f1b18e655d5b871cfd2f5',
-     armv7l: '72e9c612666916c70df7808ca57164d44723a6bcbf1f1b18e655d5b871cfd2f5',
-       i686: '3983aa6b74d962316c445224cd6ac0aa3d8756d6351a7c5ab5f9a4222d9dd0ca',
-     x86_64: 'f82524ea792a08abc14c17837cdbf82a05d004829413aee1f2e70bf7cce75acb',
   })
 
   def self.install
