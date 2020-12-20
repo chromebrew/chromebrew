603e5ef974884f40ed451053c38ec4efb513fc7f
satmandu <satadru@umich.edu>
2020-12-20 00:09:02 -0500

    uninstall before install solves upgrade (#4782)

--- packages/pygments.rb
+++ packages/pygments.rb
@@ -3,12 +3,13 @@ require 'package'
 class Pygments < Package
   description 'Python Syntax Highlighter'
   homepage 'https://pygments.org/'
-  version '2.7.3-1'
+  version '2.7.3-2'
   compatibility 'all'
   source_url 'file:///dev/null'
   source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
   def self.install
-    system "pip3 install pygments --no-warn-script-location --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
+    system "pip3 uninstall -y pygments"
+    system "pip3 install --upgrade --no-warn-script-location pygments --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
   end
 end
