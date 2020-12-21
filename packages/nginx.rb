7c7b6bb8f88de0592b5ad09bc6740d1ea2c86750
Ed Reel <edreel@gmail.com>
2020-12-20 23:07:52 -0600

    Update nginx from 1.19.3 to 1.19.6

--- packages/nginx.rb
+++ packages/nginx.rb
@@ -3,22 +3,22 @@ require 'package'
 class Nginx < Package
   description 'nginx [engine x] is an HTTP and reverse proxy server, a mail proxy server, and a generic TCP/UDP proxy server, originally written by Igor Sysoev.'
   homepage 'http://nginx.org/'
-  version '1.19.3'
+  version '1.19.6'
   compatibility 'all'
-  source_url 'https://nginx.org/download/nginx-1.19.3.tar.gz'
-  source_sha256 '91e5b74fa17879d2463294e93ad8f6ffc066696ae32ad0478ffe15ba0e9e8df0'
+  source_url 'https://nginx.org/download/nginx-1.19.6.tar.gz'
+  source_sha256 'b11195a02b1d3285ddf2987e02c6b6d28df41bb1b1dd25f33542848ef4fc33b5'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.3-chromeos-x86_64.tar.xz',
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.6-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.6-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.6-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/nginx-1.19.6-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'aca948f9c2d22727e0e41fd6f81ac2e2f3775df9a683855dafd2ec664c0f2eb2',
-     armv7l: 'aca948f9c2d22727e0e41fd6f81ac2e2f3775df9a683855dafd2ec664c0f2eb2',
-       i686: '9dc198322df7652fb39474066516a783f57990c75939fcd906854be05d4ad9dd',
-     x86_64: 'c3798879f505b6173e4964105b741a7f5fdf85243562ad70d3b99a405ed1498f',
+    aarch64: '12c999e173d529e09e73651be3da52e10c330292bb77b5f1091754470622f32c',
+     armv7l: '12c999e173d529e09e73651be3da52e10c330292bb77b5f1091754470622f32c',
+       i686: '5655869f1c9bc543734be534fa1528957a031a2946c0a01847fdf582a680001e',
+     x86_64: '82fd389650190b29f398b9430e34d92441c0071d39be8a5fa327b8035aa8e7a2',
   })
 
   depends_on 'pcre'
@@ -78,9 +78,21 @@ class Nginx < Package
     puts "echo 'fi' >> ~/.bashrc".lightblue
     puts "source ~/.bashrc".lightblue
     puts
-    puts "To completely remove nginx, perform the following:".lightblue
-    puts "crew remove nginx".lightblue
-    puts "sudo rm -rf #{CREW_PREFIX}/share/nginx".lightblue
-    puts
+  end
+
+  def self.remove
+    if Dir.exists? "#{CREW_PREFIX}/share/nginx"
+      puts
+      print "Would you like to remove #{CREW_PREFIX}/share/nginx? [y/N] "
+      response = STDIN.getc
+      case response
+      when "y", "Y"
+        FileUtils.rm_rf "#{CREW_PREFIX}/share/nginx"
+        puts "#{CREW_PREFIX}/share/nginx removed.".lightred
+      else
+        puts "#{CREW_PREFIX}/share/nginx saved.".lightgreen
+      end
+      puts
+    end
   end
 end
