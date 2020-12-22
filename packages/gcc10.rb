7107399d5d9eaedf4317fc9c2f3b537a79e36361
satmandu <satadru@umich.edu>
2020-12-22 15:30:05 -0500

    gcc10 which compiles with gcc 10.

--- packages/gcc10.rb
+++ packages/gcc10.rb
@@ -3,23 +3,26 @@ require 'package'
 class Gcc10 < Package
   description 'The GNU Compiler Collection includes front ends for C, C++, Objective-C, Fortran, Ada, and Go.'
   homepage 'https://www.gnu.org/software/gcc/'
-  version '10.2.0'
+  version '10.2.1-07fe'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gcc/gcc-10.2.0/gcc-10.2.0.tar.xz'
-  source_sha256 'b8dd4368bb9c7f0b98188317ee0254dd8cc99d1e3a18d0ff146c855fe16c1d8c'
+  source_url 'https://github.com/gcc-mirror/gcc/archive/07fed10b19e1d88ad111fd27b4181e53d1aa6778.zip'
+  source_sha256 '455b11e3e82118d8b026cb1a8cb0e7b8331f1f9d76551c60e6792d488b4f1f12'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gcc10-10.2.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gcc10-10.2.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gcc10-10.2.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gcc10-10.2.0-chromeos-x86_64.tar.xz',
+     aarch64: 'file:///usr/local/tmp/packages/gcc10-10.2.1-07fe-chromeos-armv7l.tar.xz',
+      armv7l: 'file:///usr/local/tmp/packages/gcc10-10.2.1-07fe-chromeos-armv7l.tar.xz',
+        i686: 'file:///usr/local/tmp/packages/gcc10-10.2.1-07fe-chromeos-i686.tar.xz',
+      x86_64: 'file:///usr/local/tmp/packages/gcc10-10.2.1-07fe-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '832a12c3db18537775d174c4188cf4bc14aeed72b243a1099e8f1715f6575dbc',
-     armv7l: '832a12c3db18537775d174c4188cf4bc14aeed72b243a1099e8f1715f6575dbc',
-       i686: 'c319f3a643f23b409fef6baffa91eea8afc43936d95ca467f0f26945cde6e5cf',
-     x86_64: 'fc3b15e4b499548131389f38b67cc4ab35e32a4fe9d21f96b108c7a3d20598de',
+     aarch64: 'e5405c03363f2e1feff695da2c29320665698b41877bfc32bdb670b330d7099b',
+      armv7l: 'e5405c03363f2e1feff695da2c29320665698b41877bfc32bdb670b330d7099b',
+        i686: '8562773de8265d440a6b4de987676959dfd5c46a7153bb0c1cb24776ba500c03',
+      x86_64: '40fe0391e7cb070e1f3fe7d140dcefdcb64cd6f3063784ae93aa309cce479bc4',
   })
+
+
+
 
   depends_on 'unzip' => :build
   depends_on 'gawk' => :build
@@ -36,12 +39,17 @@ class Gcc10 < Package
   depends_on 'glibc'
   depends_on 'zstd'
 
+  
+  
   def self.preinstall
-    gccver = `gcc -v 2>&1 | tail -1 | cut -d' ' -f3`.chomp
-    abort "GCC version #{gccver} already installed.".lightgreen unless "#{gccver}" == "No" || "#{gccver}" == "not" || "#{gccver}" == "gcc:" || "#{gccver}" == "#{version}"
+    installed_gccver = `gcc -v 2>&1 | tail -1 | cut -d' ' -f3`.chomp
+    gcc_version = version.split("-")[0]
+    abort "GCC version #{installed_gccver} already installed.".lightgreen unless "#{installed_gccver}" == "No" || "#{installed_gccver}" == "not" || "#{installed_gccver}" == "gcc:" || "#{installed_gccver}" == "#{gcc_version}"
   end
 
   def self.build
+    gcc_version = version.split("-")[0]
+    
     # previous compile issue
     # /usr/local/bin/ld: cannot find crti.o: No such file or directory
     # /usr/local/bin/ld: cannot find /usr/lib64/libc_nonshared.a
@@ -58,7 +66,7 @@ class Gcc10 < Package
     sed -i '77a #ifndef PATH_MAX' libsanitizer/asan/asan_linux.cpp)"
     Dir.chdir("objdir") do
       case ARCH
-      when 'armv7l', 'aarch64'
+      when 'armv7l', 'armv8l', 'aarch64'
         system "../configure",
         "--prefix=#{CREW_PREFIX}",
         "--libdir=#{CREW_LIB_PREFIX}",
@@ -74,7 +82,7 @@ class Gcc10 < Package
         "--disable-libmpx",
         "--enable-static",
         "--enable-shared",
-        "--program-suffix=-#{version}",
+        "--program-suffix=-#{gcc_version}",
         "--with-arch=armv7-a",
         "--with-tune=cortex-a15",
         "--with-fpu=neon",
@@ -95,7 +103,7 @@ class Gcc10 < Package
         "--disable-libmpx",
         "--enable-static",
         "--enable-shared",
-        "--program-suffix=-#{version}",
+        "--program-suffix=-#{gcc_version}",
         "--with-arch-64=x86-64"
       when 'i686'
         system "../configure",
@@ -113,7 +121,7 @@ class Gcc10 < Package
         "--disable-libmpx",
         "--enable-static",
         "--enable-shared",
-        "--program-suffix=-#{version}",
+        "--program-suffix=-#{gcc_version}",
         "--with-arch-32=i686"
       end
       system 'make'
@@ -132,8 +140,11 @@ class Gcc10 < Package
     Dir.chdir("objdir") do
       system "make", "DESTDIR=#{CREW_DEST_DIR}", "install-strip"
 
-      gcc_arch = `gcc -dumpmachine`.chomp
-      gcc_dir = "gcc/#{gcc_arch}/#{version}"
+      gcc_arch = `gcc/xgcc -dumpmachine`.chomp
+      gcc_version = version.split("-")[0]
+      #gcc_version = `cat ../gcc/BASE-VER`
+      #gcc_version = `gcc/xgcc -dumpversion`.chomp
+      gcc_dir = "gcc/#{gcc_arch}/#{gcc_version}"
       gcc_libdir = "#{CREW_LIB_PREFIX}/#{gcc_dir}"
 
       # http://www.linuxfromscratch.org/lfs/view/development/chapter06/gcc.html#contents-gcc
@@ -146,41 +157,41 @@ class Gcc10 < Package
       system "install -v -dm755 #{CREW_DEST_LIB_PREFIX}/bfd-plugins"
 
       # Add a compatibility symlink to enable building programs with Link Time Optimization (LTO)
-      system "ln -sfv #{CREW_PREFIX}/libexec/#{gcc_dir}/liblto_plugin.so #{CREW_DEST_LIB_PREFIX}/bfd-plugins/"
+      system "ln -rsfv #{CREW_DEST_PREFIX}/libexec/#{gcc_dir}/liblto_plugin.so #{CREW_DEST_LIB_PREFIX}/bfd-plugins/"
 
       # Make symbolic links
       Dir.chdir "#{CREW_DEST_LIB_PREFIX}/#{gcc_dir}" do
         system "find . -type f -maxdepth 1 -exec ln -sv #{gcc_libdir}/{} #{CREW_DEST_LIB_PREFIX}/{} \\;"
       end
 
-      system "ln -sv #{CREW_PREFIX}/bin/gcc-#{version} #{CREW_DEST_PREFIX}/bin/cc"
-      system "ln -sv #{CREW_PREFIX}/bin/gcc-#{version} #{CREW_DEST_PREFIX}/bin/gcc"
-      system "ln -sv #{CREW_PREFIX}/bin/c++-#{version} #{CREW_DEST_PREFIX}/bin/c++"
-      system "ln -sv #{CREW_PREFIX}/bin/g++-#{version} #{CREW_DEST_PREFIX}/bin/g++"
-      system "ln -sv #{CREW_PREFIX}/bin/cpp-#{version} #{CREW_DEST_PREFIX}/bin/cpp"
-      system "ln -sv #{CREW_PREFIX}/bin/gcc-ar-#{version} #{CREW_DEST_PREFIX}/bin/gcc-ar"
-      system "ln -sv #{CREW_PREFIX}/bin/gcc-nm-#{version} #{CREW_DEST_PREFIX}/bin/gcc-nm"
-      system "ln -sv #{CREW_PREFIX}/bin/gcc-ranlib-#{version} #{CREW_DEST_PREFIX}/bin/gcc-ranlib"
-      system "ln -sv #{CREW_PREFIX}/bin/gcov-#{version} #{CREW_DEST_PREFIX}/bin/gcov"
-      system "ln -sv #{CREW_PREFIX}/bin/gcov-dump-#{version} #{CREW_DEST_PREFIX}/bin/gcov-dump"
-      system "ln -sv #{CREW_PREFIX}/bin/gcov-tool-#{version} #{CREW_DEST_PREFIX}/bin/gcov-tool"
-      system "ln -sv #{CREW_PREFIX}/bin/gfortran-#{version} #{CREW_DEST_PREFIX}/bin/gfortran"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcc-#{gcc_version} #{CREW_DEST_PREFIX}/bin/cc"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcc-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gcc"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/c++-#{gcc_version} #{CREW_DEST_PREFIX}/bin/c++"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/g++-#{gcc_version} #{CREW_DEST_PREFIX}/bin/g++"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/cpp-#{gcc_version} #{CREW_DEST_PREFIX}/bin/cpp"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcc-ar-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gcc-ar"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcc-nm-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gcc-nm"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcc-ranlib-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gcc-ranlib"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcov-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gcov"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcov-dump-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gcov-dump"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gcov-tool-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gcov-tool"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/gfortran-#{gcc_version} #{CREW_DEST_PREFIX}/bin/gfortran"
 
-      system "ln -sv #{CREW_PREFIX}/bin/#{gcc_arch}-c++-#{version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-c++"
-      system "ln -sv #{CREW_PREFIX}/bin/#{gcc_arch}-g++-#{version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-g++"
-      system "ln -sv #{CREW_PREFIX}/bin/#{gcc_arch}-gcc-#{version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc"
-      system "ln -sv #{CREW_PREFIX}/bin/#{gcc_arch}-gcc-ar-#{version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-ar"
-      system "ln -sv #{CREW_PREFIX}/bin/#{gcc_arch}-gcc-nm-#{version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-nm"
-      system "ln -sv #{CREW_PREFIX}/bin/#{gcc_arch}-gcc-ranlib-#{version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-ranlib"
-      system "ln -sv #{CREW_PREFIX}/bin/#{gcc_arch}-gfortran-#{version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gfortran"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-c++-#{gcc_version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-c++"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-g++-#{gcc_version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-g++"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-#{gcc_version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-ar-#{gcc_version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-ar"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-nm-#{gcc_version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-nm"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-ranlib-#{gcc_version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gcc-ranlib"
+      system "ln -rsv #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gfortran-#{gcc_version} #{CREW_DEST_PREFIX}/bin/#{gcc_arch}-gfortran"
 
-      system "ln -sv #{CREW_PREFIX}/share/man/man1/cpp-#{version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/cpp.1.gz"
-      system "ln -sv #{CREW_PREFIX}/share/man/man1/g++-#{version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/g++.1.gz"
-      system "ln -sv #{CREW_PREFIX}/share/man/man1/gcc-#{version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcc.1.gz"
-      system "ln -sv #{CREW_PREFIX}/share/man/man1/gcov-#{version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcov.1.gz"
-      system "ln -sv #{CREW_PREFIX}/share/man/man1/gcov-dump-#{version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcov-dump.1.gz"
-      system "ln -sv #{CREW_PREFIX}/share/man/man1/gcov-tool-#{version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcov-tool.1.gz"
-      system "ln -sv #{CREW_PREFIX}/share/man/man1/gfortran-#{version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gfortran.1.gz"
+      system "ln -rsv #{CREW_DEST_PREFIX}/share/man/man1/cpp-#{gcc_version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/cpp.1.gz"
+      system "ln -rsv #{CREW_DEST_PREFIX}/share/man/man1/g++-#{gcc_version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/g++.1.gz"
+      system "ln -rsv #{CREW_DEST_PREFIX}/share/man/man1/gcc-#{gcc_version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcc.1.gz"
+      system "ln -rsv #{CREW_DEST_PREFIX}/share/man/man1/gcov-#{gcc_version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcov.1.gz"
+      system "ln -rsv #{CREW_DEST_PREFIX}/share/man/man1/gcov-dump-#{gcc_version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcov-dump.1.gz"
+      system "ln -rsv #{CREW_DEST_PREFIX}/share/man/man1/gcov-tool-#{gcc_version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gcov-tool.1.gz"
+      system "ln -rsv #{CREW_DEST_PREFIX}/share/man/man1/gfortran-#{gcc_version}.1.gz #{CREW_DEST_PREFIX}/share/man/man1/gfortran.1.gz"
     end
   end
 end
