8b4f36d1c514a6c147bb8faa47395c78ef843dbe
satmandu <satadru@umich.edu>
2020-12-23 17:11:45 -0500

    Update upfrog.sh (#4806)

--- tools/upfrog.sh
+++ tools/upfrog.sh
@@ -17,7 +17,7 @@
 #   4. Install jfrog:
 #      $ crew install jfrog
 #   5. Configure jfrog:
-#      $ jfrog bt c --user <bintray_username> --key <api_key> --licences GPL-3.0
+#      $ jfrog bt c --user <bintray_username> --key <api_key> --licenses GPL-3.0
 #      See https://www.jfrog.com/confluence/display/CLI/CLI+for+JFrog+Bintray#CLIforJFrogBintray-Configuration
 #
 # Note: OSS free account has limitation of the number of REST API uses by a day.
