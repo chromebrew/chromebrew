79c2bbafb9384d78c409e6d544e4e11c9d79bdb0
satmandu <satadru@umich.edu>
2020-12-24 02:09:43 -0500

    i686 fix

--- packages/p11kit.rb
+++ packages/p11kit.rb
@@ -3,7 +3,7 @@ require 'package'
 class P11kit < Package
   description "Provides a standard configuration setup for installing PKCS#11 modules in such a way that they're discoverable."
   homepage 'https://p11-glue.freedesktop.org/p11-kit.html'
-  version '0.23.22'
+  version '0.23.22-1'
   compatibility 'all'
   source_url 'https://github.com/p11-glue/p11-kit/archive/0.23.22.tar.gz'
   source_sha256 'e4bf1e374b3c0950a724cf367bd52201519f4b7025c8e9902a274123cc232560'
@@ -16,6 +16,7 @@ class P11kit < Package
     system "meson setup #{CREW_MESON_OPTIONS} \
       build"
     system "meson configure build"
+    system "sed -i s,/usr/bin/asn1Parser,/usr/local/bin/asn1Parser,g build/build.ninja" 
     system "ninja -C build"
   end
   
