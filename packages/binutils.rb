9c2523f3bb5e1bba893dfec7647fb5ffbab850c2
satmandu <satadru@umich.edu>
2020-12-28 13:46:22 -0500

    Add binaries

--- packages/binutils.rb
+++ packages/binutils.rb
@@ -8,7 +8,18 @@ class Binutils < Package
   source_url 'https://ftpmirror.gnu.org/binutils/binutils-2.35.1.tar.xz'
   source_sha256 '3ced91db9bf01182b7e420eab68039f2083aed0a214c0424e257eae3ddee8607'
 
-
+  binary_url ({
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/binutils-2.35.1-1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/binutils-2.35.1-1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/binutils-2.35.1-1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/binutils-2.35.1-1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+     aarch64: 'ccadf54533ef07715dfe17e4dec9639a0c6e1c34e1df780f936ede7d7233db27',
+      armv7l: 'ccadf54533ef07715dfe17e4dec9639a0c6e1c34e1df780f936ede7d7233db27',
+        i686: '5fda35d6de84ae32d29d7369a36b4e8220a1304ed978d76b9fdcc5bc17961565',
+      x86_64: '20fd8355151fd24dab6a82123a1d04c7a9dcb94cb2841df2795c6bc21a1107df',
+  })
 
   depends_on 'filecmd'
   depends_on 'texinfo'
@@ -28,7 +39,6 @@ class Binutils < Package
               --enable-64-bit-bfd \
               --enable-lto \
               --enable-vtable-verify \
-              --disable-werror \
               --enable-install-libiberty"
       system 'make'
     end
