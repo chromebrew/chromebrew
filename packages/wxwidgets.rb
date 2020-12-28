b6a80fcfa6d011c104bb4a240bfee3e2cc4efef9
satmandu <satadru@umich.edu>
2020-12-28 14:54:38 -0500

    Ugly but working

--- packages/wxwidgets.rb
+++ packages/wxwidgets.rb
@@ -5,21 +5,23 @@ class Wxwidgets < Package
   homepage 'https://www.wxwidgets.org/'
   version '3.0.5.1'
   compatibility 'all'
-  source_url 'https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.5.1/wxWidgets-3.0.5.1.tar.bz2'
-  source_sha256 '440f6e73cf5afb2cbf9af10cec8da6cdd3d3998d527598a53db87099524ac807'
+  source_url 'https://github.com/wxWidgets/wxWidgets/archive/v3.0.5.1.tar.gz'
+  source_sha256 'bae4d9f289e33a05fb8553fcc580564d30efe6a882ff08e3d4e09ef01f5f6578'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wxwidgets-3.0.5.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '9bc9ccfe7c61b34880f86340c35114f2cadd9a1cee6e7941cb345c9919a2c33d',
-     armv7l: '9bc9ccfe7c61b34880f86340c35114f2cadd9a1cee6e7941cb345c9919a2c33d',
-       i686: 'dd1e04c0d67446afcb3c3083d4e2c77ed4d1227d08434b4e512dab4bda0c4993',
-     x86_64: 'f7c04b355523fc215d30e16671ae2e3654fbf13112acc753d0797e3ef862d2b4',
+     aarch64: '440d312b479a90ffd73e84c396b9d54a76a01f8049adb820535236845b8b2acb',
+      armv7l: '440d312b479a90ffd73e84c396b9d54a76a01f8049adb820535236845b8b2acb',
+        i686: 'aed2e2b7577f820dcb2f6eddaa39b6b602d47249b5dc8f5d0b2b18c6093b9c30',
+      x86_64: '3e3df3560a084dfe8e100728ae77ebcb68548186e05d7c787d162e015d71245c',
   })
+
+
 
   depends_on 'gst_plugins_base'
   depends_on 'libnotify'
@@ -30,8 +32,40 @@ class Wxwidgets < Package
   depends_on 'gtk3'
 
   def self.build
-    system "./configure #{CREW_OPTIONS} --with-gtk"
-    system 'make'
+    # Keeps an abicheck error from derailing compile on multiple versions of wxwidgets, including 3.1
+    # This may be useful when moving to the next 3.1 based stable version, so keep it in.
+    system "cat <<'EOF'> make-abicheck-non-fatal.patch 
+diff -up wxGTK-2.8.12/src/common/appbase.cpp.abicheck wxGTK-2.8.12/src/common/appbase.cpp
+--- wxGTK-2.8.12/src/common/appbase.cpp.abicheck	2015-03-12 17:15:18.000000000 +0100
++++ wxGTK-2.8.12/src/common/appbase.cpp	2015-03-12 17:15:57.000000000 +0100
+@@ -424,10 +424,7 @@ bool wxAppConsole::CheckBuildOptions(con
+         msg.Printf(_T(\"Mismatch between the program and library build versions detected.\nThe library used %s,\nand %s used %s.\"),
+                    lib.c_str(), progName.c_str(), prog.c_str());
+ 
+-        wxLogFatalError(msg.c_str());
+-
+-        // normally wxLogFatalError doesn't return
+-        return false;
++        wxLogWarning(msg.c_str());
+     }
+ #undef wxCMP
+EOF"
+    system "patch -Np1 -i make-abicheck-non-fatal.patch || true"
+    system "unset CC CXX CXXFLAGS CFLAGS && \
+      export LIBRARY_PATH=#{CREW_LIB_PREFIX} && \
+      export LD_LIBRARY_PATH=#{CREW_LIB_PREFIX} && \
+      export CC=gcc && \
+      export CXX=g++ && \
+      export CFLAGS= && \
+      ./configure #{CREW_OPTIONS} \
+      --with-gtk=3 --with-opengl --enable-unicode \
+      --enable-graphics_ctx --enable-mediactrl --enable-webview --with-regex=builtin \
+      --with-libpng=sys \
+      --with-libjpeg=sys \
+      --with-libtiff=sys \
+      --without-gnomevfs \
+      --disable-precomp-headers"
+   system 'make'
   end
 
   def self.install
