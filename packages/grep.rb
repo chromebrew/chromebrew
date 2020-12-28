0df8c2c4602d91e083c74e4a851215e4c4dc82b6
satmandu <satadru@umich.edu>
2020-12-27 20:01:37 -0500

    add bintray links

--- packages/grep.rb
+++ packages/grep.rb
@@ -10,10 +10,10 @@ class Grep < Package
   source_sha256 '667e15e8afe189e93f9f21a7cd3a7b3f776202f417330b248c2ad4f997d9373e'
 
   binary_url ({
-     aarch64: 'file:///usr/local/tmp/packages/grep-3.6-chromeos-armv7l.tar.xz',
-      armv7l: 'file:///usr/local/tmp/packages/grep-3.6-chromeos-armv7l.tar.xz',
-        i686: 'file:///usr/local/tmp/packages/grep-3.6-chromeos-i686.tar.xz',
-      x86_64: 'file:///usr/local/tmp/packages/grep-3.6-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/grep-3.6-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/grep-3.6-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/grep-3.6-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/grep-3.6-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
      aarch64: '87ebec000c54b032f1e2720b03a30dfb13f14dc3a2a68a4c943daae64dc5ee34',
