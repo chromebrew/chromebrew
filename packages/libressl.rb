abe3e8e5166ba10905ff1e5a4cc6fe5edba19c40
satmandu <satadru@umich.edu>
2020-12-28 14:27:05 -0500

    Add binaries

--- packages/libressl.rb
+++ packages/libressl.rb
@@ -8,9 +8,20 @@ class Libressl < Package
   source_url 'https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/libressl-3.2.3.tar.gz'
   source_sha256 '412dc2baa739228c7779e93eb07cd645d5c964d2f2d837a9fd56db7498463d73'
 
+  binary_url ({
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libressl-3.2.3-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libressl-3.2.3-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/libressl-3.2.3-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libressl-3.2.3-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+     aarch64: '59a8e3c3cf6bd97210f04882c2b0ce94566311f360536d1174d7e04e8f9884ab',
+      armv7l: '59a8e3c3cf6bd97210f04882c2b0ce94566311f360536d1174d7e04e8f9884ab',
+        i686: 'd6962728e73894df3382ad17035b838f213f246edb6dc4aa54522c3bc5c59dd8',
+      x86_64: 'be499bd46626d26ed6c333dc1a74df423537053e6afbf2a14e542c5ceec796ef',
+  })
 
   def self.build
-    system "./configure --help"
     system "./configure #{CREW_OPTIONS} --with-openssldir=#{CREW_PREFIX}/etc/ssl"
     system 'make'
   end
