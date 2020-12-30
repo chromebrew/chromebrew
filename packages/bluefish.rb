078a856c03c2733b99d5067c640851f5a49a3fa2
supechicken <73414985+supechicken@users.noreply.github.com>
2020-12-31 01:22:53 +0800

    Update bluefish.rb

--- packages/bluefish.rb
+++ packages/bluefish.rb
@@ -8,19 +8,6 @@ class Bluefish < Package
   source_url 'https://www.bennewitz.com/bluefish/stable/source/bluefish-2.2.10.tar.bz2'
   source_sha256 'afeca12b693bb58e30c2199e6a21cc06da02d88e0ac9d08b7a231a9c8e7c3eb2'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.10-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.10-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.10-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.10-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'ea47d2b5ecded9b79975d1ea7f2df86e441e9ac85e4fc6c331f1fdf3869498ae',
-     armv7l: 'ea47d2b5ecded9b79975d1ea7f2df86e441e9ac85e4fc6c331f1fdf3869498ae',
-       i686: '405127fefd5539f779fe4810aa302371f5919ac1a02017686049a7cb7ab91759',
-     x86_64: '7403531e5aef2c5d7afe05433fa4d6c3b5b74797a3b4e81ef4ebd20c32de228d',
-  })
-
   depends_on 'enchant'
   depends_on 'gtk3'
   depends_on 'gnome_icon_theme'
