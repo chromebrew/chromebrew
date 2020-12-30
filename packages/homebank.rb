65704f14a7ec12bc0b45597e7c32ec1c902a7c62
supechicken <73414985+supechicken@users.noreply.github.com>
2020-12-31 00:57:11 +0800

    Update homebank.rb

--- packages/homebank.rb
+++ packages/homebank.rb
@@ -25,7 +25,9 @@ class Homebank < Package
   depends_on 'libsoup'
   depends_on 'gtk3'
   depends_on 'xdg_base'
-  depends_on 'sommelier'
+  depends_on 'wayland_protocols'
+  depends_on 'mesa'
+  depends_on 'xcb_util'
 
   def self.build
     system './configure',
@@ -36,5 +38,13 @@ class Homebank < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    FileUtils.mv '#{CREW_DEST_PREFIX}/bin/homebank', '#{CREW_DEST_PREFIX}/bin/hb_orig'
+    system "cat <<'EOF'> homebank
+WAYLAND_DISPLAY=wayland-0
+GDK_BACKEND=wayland
+DISPLAY=
+#{CREW_PREFIX}/bin/hb_orig $@
+EOF"
+    system "install -Dm755 homebank #{CREW_DEST_PREFIX}/bin/homebank"
   end
 end
