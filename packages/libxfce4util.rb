8762fbd5a4863b13ba3f237f8c6730081eb3d86a
supechicken <73414985+supechicken@users.noreply.github.com>
2020-12-31 18:37:33 +0800

    Update libxfce4util.rb

--- packages/libxfce4util.rb
+++ packages/libxfce4util.rb
@@ -5,7 +5,7 @@ class Libxfce4util < Package
   homepage 'https://xfce.org/'
   version '4.16.0'
   compatibility 'all'
-  source_url "https://archive.xfce.org/src/xfce/libxfce4util/4.16/libxfce4util-4.16.0.tar.bz2"
+  source_url 'https://archive.xfce.org/src/xfce/libxfce4util/4.16/libxfce4util-4.16.0.tar.bz2'
   source_sha256 '60598d745d1fc81ff5ad3cecc3a8d1b85990dd22023e7743f55abd87d8b55b83'
 
   depends_on 'gobject_introspection'
