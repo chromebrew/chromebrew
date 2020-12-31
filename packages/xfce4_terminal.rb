cc0417526f7eded25da047157dfd62f6b691074d
supechicken <73414985+supechicken@users.noreply.github.com>
2020-12-31 18:40:16 +0800

    Add files via upload

--- packages/xfce4_terminal.rb
+++ packages/xfce4_terminal.rb
@@ -3,23 +3,10 @@ require 'package'
 class Xfce4_terminal < Package
   description 'Modern terminal emulator primarly for the Xfce desktop environment'
   homepage 'https://xfce.org/'
-  version '0.8.9'
+  version '0.8.10'
   compatibility 'all'
-  source_url 'https://archive.xfce.org/src/apps/xfce4-terminal/0.8/xfce4-terminal-0.8.9.tar.bz2'
-  source_sha256 '247683a51a964cfaa6b1e92030afe9f782efebfcb550a464170b53eb94216795'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_terminal-0.8.9-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_terminal-0.8.9-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_terminal-0.8.9-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xfce4_terminal-0.8.9-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'd37498733609e4d3f3f15850338c4784a4eb78a004e7e25acf77931926e10bfc',
-     armv7l: 'd37498733609e4d3f3f15850338c4784a4eb78a004e7e25acf77931926e10bfc',
-       i686: '9840a888716501c16051c3d5bd1e2589041842e5ce04752e5ad4ccca1040d220',
-     x86_64: '8de24a4111db8171a242bf6a0365c9cb491fdcd472909fef82dce15a11f51d20',
-  })
+  source_url "https://archive.xfce.org/src/apps/xfce4-terminal/0.8/xfce4-terminal-#{version}.tar.bz2"
+  source_sha256 '7a3337c198e01262a0412384823185753ac8a0345be1d6776a7e9bbbcbf33dc7'
 
   depends_on 'desktop_file_utilities'
   depends_on 'gtkvte'
