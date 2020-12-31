53ca73f6c66bff8f518e96a7316a831cd44b110d
supechicken <73414985+supechicken@users.noreply.github.com>
2020-12-31 17:27:05 +0800

    Update filezilla.rb

--- packages/filezilla.rb
+++ packages/filezilla.rb
@@ -8,19 +8,6 @@ class Filezilla < Package
   source_url 'https://download.filezilla-project.org/client/FileZilla_3.51.0_src.tar.bz2'
   source_sha256 '82be1c4a4f51bb32a599d0b4cc3a24127ba948c0af0f957233cc6a13ea3b6c4c'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.51.0-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.51.0-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.51.0-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/filezilla-3.51.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-     aarch64: '09d27595590ea5d0e3a31b943a34962c8ec9314e8c3628472cb69f0c98bb3a25',
-      armv7l: '09d27595590ea5d0e3a31b943a34962c8ec9314e8c3628472cb69f0c98bb3a25',
-        i686: '731e2488ce0d5024b3f2ae8874dce80e2826d5aa6aefbb92b808e1ac08fafab9',
-      x86_64: 'a2bcd8daeca485ef2695b1eac1490cebb9cfa1d740d20e20ecacffb869451403',
-  })
-
   depends_on 'dbus'
   depends_on 'gnome_icon_theme'
   depends_on 'hicolor_icon_theme'
@@ -30,7 +17,9 @@ class Filezilla < Package
   depends_on 'wxwidgets'
   depends_on 'xdg_utils'
   depends_on 'libwebp'
-  depends_on 'sommelier'
+  depends_on 'wayland_protocols'
+  depends_on 'mesa'
+  depends_on 'xcb_util'
   
 
   def self.patch
@@ -46,5 +35,13 @@ class Filezilla < Package
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    FileUtils.mv '#{CREW_DEST_PREFIX}/bin/filezilla', '#{CREW_DEST_PREFIX}/bin/filezilla_orig'
+    system "cat <<'EOF'> filezilla
+WAYLAND_DISPLAY=wayland-0
+GDK_BACKEND=wayland
+DISPLAY=
+#{CREW_PREFIX}/bin/filezilla_orig $@
+EOF"
+    system "install -Dm755 filezilla #{CREW_DEST_PREFIX}/bin/filezilla"
   end
 end
