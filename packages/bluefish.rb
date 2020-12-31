8816ec5c0507cb1dd319cb90183a5db335b2fced
supechicken <73414985+supechicken@users.noreply.github.com>
2020-12-31 13:19:57 +0800

    Update bluefish.rb

--- packages/bluefish.rb
+++ packages/bluefish.rb
@@ -3,10 +3,10 @@ require 'package'
 class Bluefish < Package
   description 'Bluefish is a powerful editor targeted towards programmers and webdevelopers'
   homepage 'http://bluefish.openoffice.nl/index.html'
-  version '2.2.10'
+  version '2.2.12'
   compatibility 'all'
-  source_url 'https://www.bennewitz.com/bluefish/stable/source/bluefish-2.2.10.tar.bz2'
-  source_sha256 'afeca12b693bb58e30c2199e6a21cc06da02d88e0ac9d08b7a231a9c8e7c3eb2'
+  source_url "https://www.bennewitz.com/bluefish/stable/source/bluefish-#{version}.tar.gz"
+  source_sha256 '948fc2921f0a67a7ce811220093a3b3dfc8021a6e3005f549373cd3402ee0f26'
 
   depends_on 'enchant'
   depends_on 'gtk3'
