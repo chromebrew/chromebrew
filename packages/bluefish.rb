319676c4f8153ceff73da77db31e668410a5d4e6
Ed Reel <edreel@gmail.com>
2021-01-01 14:17:23 -0600

    Add pre-built binaries for bluefish and homebank

--- packages/bluefish.rb
+++ packages/bluefish.rb
@@ -8,6 +8,19 @@ class Bluefish < Package
   source_url 'https://www.bennewitz.com/bluefish/stable/source/bluefish-2.2.12.tar.gz'
   source_sha256 '948fc2921f0a67a7ce811220093a3b3dfc8021a6e3005f549373cd3402ee0f26'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.12-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.12-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.12-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bluefish-2.2.12-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '6e769339832399c45018d072a2c906c00025b16c1b4a3acd8c5ea58dafa86b3c',
+     armv7l: '6e769339832399c45018d072a2c906c00025b16c1b4a3acd8c5ea58dafa86b3c',
+       i686: '7f706a40177c68674b55fe19f25dbe91100fb08525b0fbe69767137e7db247ff',
+     x86_64: '5ecc4aae82c14977ac46419fedbe4f298ca9e4e979d9ccca3c641dd6b298721c',
+  })
+
   depends_on 'enchant'
   depends_on 'gtk3'
   depends_on 'gnome_icon_theme'
