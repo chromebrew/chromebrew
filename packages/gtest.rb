fcd8f6a5b12061b42d80e319e8d19cb9f252d7fa
satmandu <satadru@umich.edu>
2021-01-04 11:57:13 -0500

    fix URLS

--- packages/gtest.rb
+++ packages/gtest.rb
@@ -8,10 +8,10 @@ class Gtest < Package
   source_sha256 '763e20249e76417bed7ebc44aa85fedf5fbac6f9fb6d30bddb628ab07ebf04f5'
 
   binary_url ({
-     aarch64: 'file:///usr/local/tmp/packages/gtest-1.10.0-389c-chromeos-armv7l.tar.xz',
-      armv7l: 'file:///usr/local/tmp/packages/gtest-1.10.0-389c-chromeos-armv7l.tar.xz',
-        i686: 'file:///usr/local/tmp/packages/gtest-1.10.0-389c-chromeos-i686.tar.xz',
-      x86_64: 'file:///usr/local/tmp/packages/gtest-1.10.0-389c-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gtest-1.10.0-389c-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gtest-1.10.0-389c-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/gtest-1.10.0-389c-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gtest-1.10.0-389c-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
      aarch64: '4c1dde1b6562fca74a35cd29f627d57ce06e6fb77b1873c34be7e75200ce5826',
@@ -19,6 +19,7 @@ class Gtest < Package
         i686: '111db3afcc8f26d2a9e2d04221c99a8514d786da64fdc06349ea3665eaa3ea86',
       x86_64: '560e28bcf87d89c9c076f0d094c8cfb461392e68679b7d833038a18c4d3c2fa2',
   })
+
 
   case ARCH
   when 'aarch64','armv7l'
