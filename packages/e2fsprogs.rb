f3228fccc2ba40b3ed170f974fda421f314e4284
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-13 01:30:13 -0500

    Added `compatibility 'all'`
    as requested by uberhacker

--- packages/e2fsprogs.rb
+++ packages/e2fsprogs.rb
@@ -4,6 +4,7 @@ class E2fsprogs < Package
   description 'e2fsprogs are ext2/3/4 file system utilities.'
   homepage 'http://e2fsprogs.sourceforge.net/'
   version '1.45.6'
+  compatibility 'all'
   source_url 'https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v1.45.6/e2fsprogs-1.45.6.tar.xz'
   source_sha256 'ffa7ae6954395abdc50d0f8605d8be84736465afc53b8938ef473fcf7ff44256'
 
