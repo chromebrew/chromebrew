2a23e29ee613b9e5cad6b269f0c7229c17610265
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-13 01:32:39 -0500

    [New Package] dosfstools

---
+++ packages/dosfstools.rb
@@ -0,0 +1,25 @@
+require 'package'
+
+class Dosfstools < Package
+  description 'dosfstools consists of the programs mkfs.fat, fsck.fat and fatlabel to create, check and label file systems of the FAT family.'
+  homepage 'https://github.com/dosfstools/dosfstools'
+  compatibility 'all'
+  version '4.1'
+  source_url 'https://github.com/dosfstools/dosfstools/releases/download/v4.1/dosfstools-4.1.tar.xz'
+  source_sha256 'e6b2aca70ccc3fe3687365009dd94a2e18e82b688ed4e260e04b7412471cc173'
+
+  depends_on 'vim' => :check #Requires `xxd` to check, otherwise all checks are skipped and test exits 0.
+
+  def self.build
+    system "./configure #{CREW_OPTIONS}"
+    system "make"
+  end
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+  end
+
+  def self.check
+    system "make", "check"
+  end
+end
