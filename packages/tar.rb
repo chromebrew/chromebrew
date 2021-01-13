4c3c86040ad60765deca060c6f3784b33e602b1a
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-13 11:11:47 -0500

    Change source to better source
    Also verified that checksum is still correct, it is.

--- packages/tar.rb
+++ packages/tar.rb
@@ -5,7 +5,7 @@ class Tar < Package
   homepage 'https://www.gnu.org/software/tar/'
   version '1.33'
   compatibility 'all'
-  source_url 'https://ftp.gnu.org/gnu/tar/tar-1.33.tar.xz'
+  source_url 'https://ftpmirror.gnu.org/tar/tar-1.33.tar.xz'
   source_sha256 '66a8344b1dc83a411d311bd1547e0176e56cc311f28ee94a30f84dafb3d9067e'
 
   def self.build
