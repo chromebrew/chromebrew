d9361df42b8f9da4245451e93a2e587e287e67d8
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-14 05:18:47 -0500

    [New Package] Perl YAML::Tiny (#4913)
    * [New Package] Perl YAML::Tiny
    
    * Correct version and remove self.build

---
+++ packages/perl_yaml_tiny.rb
@@ -0,0 +1,29 @@
+require 'package'
+
+class Perl_yaml_tiny < Package
+  description 'YAML::Tiny - Read/Write YAML files with as little code as possible'
+  homepage 'https://metacpan.org/pod/YAML::Tiny'
+  version '1.73'
+  compatibility 'all'
+  source_url 'https://cpan.metacpan.org/authors/id/E/ET/ETHER/YAML-Tiny-1.73.tar.gz'
+  source_sha256 'bc315fa12e8f1e3ee5e2f430d90b708a5dc7e47c867dba8dce3a6b8fbe257744'
+
+  depends_on 'perl'
+  
+  def self.install
+    # install files to build directory
+    system 'cpanm', '-l', 'build', '--self-contained', '--force', '.'
+
+    # install lib
+    libdir = CREW_DEST_DIR + `perl -e 'require Config; print $Config::Config{'"'installsitelib'"'};'`
+    FileUtils.mkdir_p libdir
+    system "(cd build/lib/perl5; tar cf - .) | (cd #{libdir}; tar xfp -)"
+
+    # install man
+    FileUtils.mkdir_p CREW_DEST_MAN_PREFIX
+    system "(cd build/man; tar cf - .) | (cd #{CREW_DEST_MAN_PREFIX}; tar xfp -)"
+  end
+
+  def self.check
+  end
+end
