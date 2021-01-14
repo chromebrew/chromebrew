914a98b23b68c5570edfcb8db545c6273ee3fe32
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-13 20:36:30 -0500

    [New Package] Perl Term::ReadKey

---
+++ packages/perl_term_readkey.rb
@@ -0,0 +1,32 @@
+require 'package'
+
+class Perl_term_readkey < Package
+  description 'Term::ReadKey - A perl module for simple terminal control'
+  homepage 'https://metacpan.org/pod/Term::ReadKey'
+  version '1.63'
+  compatibility 'all'
+  source_url 'https://cpan.metacpan.org/authors/id/J/JS/JSTOWE/TermReadKey-2.38.tar.gz'
+  source_sha256 '5a645878dc570ac33661581fbb090ff24ebce17d43ea53fd22e105a856a47290'
+
+  depends_on 'perl'
+
+  def self.build
+  end
+
+  def self.install
+    # install files to build directory
+    system 'cpanm', '-l', 'build', '--self-contained', '--force', '.'
+
+    # install lib
+    libdir = CREW_DEST_DIR + `perl -e 'require Config; print $Config::Config{'"'installsitelib'"'};'`
+    FileUtils.mkdir_p libdir
+    system "(cd build/lib/perl5; tar cf - .) | (cd #{libdir}; tar xfp -)"
+
+    # install man
+    FileUtils.mkdir_p CREW_DEST_MAN_PREFIX
+    system "(cd build/man; tar cf - .) | (cd #{CREW_DEST_MAN_PREFIX}; tar xfp -)"
+  end
+
+  def self.check
+  end
+end
