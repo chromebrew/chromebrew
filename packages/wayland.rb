7ae7d5a5aba1679abd25790d68754a712ed85783
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-15 01:20:24 +0800

    Update wayland.rb

--- packages/wayland.rb
+++ packages/wayland.rb
@@ -3,7 +3,7 @@ require 'package'
 class Wayland < Package
   description 'Wayland is intended as a simpler replacement for X, easier to develop and maintain.'
   homepage 'https://wayland.freedesktop.org'
-  version '1.18.0'
+  version '1.18.0-2'
   compatibility 'all'
   source_url 'https://wayland.freedesktop.org/releases/wayland-1.18.0.tar.xz'
   source_sha256 '4675a79f091020817a98fd0484e7208c8762242266967f55a67776936c2e294d'
