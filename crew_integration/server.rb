66bbfdaca0a5679bb1f741eea3d3b307d33b2cab
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-14 14:36:54 +0800

    Update server.rb

--- crew_integration/server.rb
+++ crew_integration/server.rb
@@ -6,13 +6,7 @@ EM.run {
      EM::WebSocket.run(:host => "0.0.0.0", :port => 25500) do |ws|
          ws.onopen { |handshake| }
          ws.onmessage { |message|
-             case message
-             when 'terminal'
-                 system "sudo mount -io bind #{PWA_PREFIX}/crosh.sh #{CROSH}"
-                 ws.send 'Ready'
-             else
-                 system "#{message} &"
-             end
+             system "#{message} &"
          }
 end
          }
