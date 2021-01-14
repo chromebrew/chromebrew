3b9afdbefd3dba14db46fa72417094e8ec2cd41b
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-14 14:32:28 -0500

    Theoretically support all architectures, correct spelling mistake

--- packages/busybox.rb
+++ packages/busybox.rb
@@ -3,7 +3,7 @@ require 'package'
 class Busybox < Package
   description 'BusyBox is a software suite that provides several Unix utilities in a single executable file'
   homepage 'https://busybox.net'
-  compatibility 'x86_64'
+  compatibility 'all'
   version '1.33.0'
   source_url 'https://www.busybox.net/downloads/busybox-1.33.0.tar.bz2'
   source_sha256 'd568681c91a85edc6710770cebc1e80e042ad74d305b5c2e6d57a5f3de3b8fbd'
@@ -22,7 +22,7 @@ class Busybox < Package
     
   def self.install
     system "install -Dm 755 0_lib/busybox #{CREW_DEST_PREFIX}/bin/busybox"
-    system "install -Dm 755 0_lib/libbusybox.so.1.33.0 #{CREW_DEST_PREFIX}/lib64/libbusybux.so.1.33.0"
+    system "install -Dm 755 0_lib/libbusybox.so.1.33.0 #{CREW_DEST_LIB_PREFIX}/libbusybox.so.1.33.0"
   end
 
   def self.check
