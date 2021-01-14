7e3fe432d8015057bb19a028eeb0b273ebce7638
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-14 04:52:03 -0500

    Correct version

--- packages/perl_term_readkey.rb
+++ packages/perl_term_readkey.rb
@@ -3,16 +3,13 @@ require 'package'
 class Perl_term_readkey < Package
   description 'Term::ReadKey - A perl module for simple terminal control'
   homepage 'https://metacpan.org/pod/Term::ReadKey'
-  version '1.63'
+  version '2.38'
   compatibility 'all'
   source_url 'https://cpan.metacpan.org/authors/id/J/JS/JSTOWE/TermReadKey-2.38.tar.gz'
   source_sha256 '5a645878dc570ac33661581fbb090ff24ebce17d43ea53fd22e105a856a47290'
 
   depends_on 'perl'
-
-  def self.build
-  end
-
+  
   def self.install
     # install files to build directory
     system 'cpanm', '-l', 'build', '--self-contained', '--force', '.'
