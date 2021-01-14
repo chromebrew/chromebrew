87b57b3e9c192ed2c7baa1c0ba098c63d2165eb8
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-15 01:18:48 +0800

    Update wayland_protocols.rb

--- packages/wayland_protocols.rb
+++ packages/wayland_protocols.rb
@@ -3,7 +3,7 @@ require 'package'
 class Wayland_protocols < Package
   description 'Wayland is a protocol for a compositor to talk to its clients.'
   homepage 'https://wayland.freedesktop.org/'
-  version '1.20'
+  version '1.20-1'
   compatibility 'all'
   source_url 'https://wayland.freedesktop.org/releases/wayland-protocols-1.20.tar.xz'
   source_sha256 '9782b7a1a863d82d7c92478497d13c758f52e7da4f197aa16443f73de77e4de7'
