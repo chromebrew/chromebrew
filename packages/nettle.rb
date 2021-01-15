8db4919b3b96933db8e75c4847db523978037d7f
satmandu <satadru@umich.edu>
2021-01-14 21:46:37 -0500

    Nettle -> 3.7 & using openssl

--- packages/nettle.rb
+++ packages/nettle.rb
@@ -3,29 +3,30 @@ require 'package'
 class Nettle < Package
   description 'Nettle is a cryptographic library that is designed to fit easily in more or less any context: In crypto toolkits for object-oriented languages (C++, Python, Pike, ...), in applications like LSH or GNUPG, or even in kernel space.'
   homepage 'http://www.lysator.liu.se/~nisse/nettle/'
-  version '3.5.1'
+  @_ver = '3.7'
+  version @_ver
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/nettle/nettle-3.5.1.tar.gz'
-  source_sha256 '75cca1998761b02e16f2db56da52992aef622bf55a3b45ec538bc2eedadc9419'
+  source_url "https://ftpmirror.gnu.org/nettle/nettle-#{@_ver}.tar.gz"
+  source_sha256 'f001f64eb444bf13dd91bceccbc20acbc60c4311d6e2b20878452eb9a9cec75a'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.5.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.5.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.5.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.5.1-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.7-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.7-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.7-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/nettle-3.7-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'a9605d17f44b2ca3f0aaad799a8f3100a803bc45bda5048a36012f9e0b7595dd',
-     armv7l: 'a9605d17f44b2ca3f0aaad799a8f3100a803bc45bda5048a36012f9e0b7595dd',
-       i686: '499d5354c73f68eba5fc2378ca81bec08ddd3e45860206f325d64f2d58c2ba5a',
-     x86_64: '682ba5bd37d140b8dd70f3ce591253cef32fec7652e9ea42c7a5e754519fa13a',
+     aarch64: 'ac62f7cd94b7047e87d413a8d6e338ebaeadbac613458b0a46290b9332ac647f',
+      armv7l: 'ac62f7cd94b7047e87d413a8d6e338ebaeadbac613458b0a46290b9332ac647f',
+        i686: '21eb9162bced72c824cdfa392bcb4b783ca6c7a5a7487134258027c8662c484f',
+      x86_64: '2ea16fff4808cfa8105c8a7f1f6eab83157979aeb2b0cce06da8f8b6ab3bdd02',
   })
 
   depends_on 'm4'
   depends_on 'openssl'
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
