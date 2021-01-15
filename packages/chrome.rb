7e1e921b5d14393fed13abf957876ac9c545267e
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-16 01:47:44 +0800

    New Package: chrome

---
+++ packages/chrome.rb
@@ -0,0 +1,40 @@
+require 'package'
+
+class Chrome < Package
+  description 'Google Chrome is a fast, easy to use, and secure web browser.'
+  homepage 'https://www.google.com/chrome'
+  @_ver = '87.0.4280.141'
+  version @_ver
+
+  case ARCH
+  when 'x86_64'
+    source_url "https://dl.google.com/linux/deb/pool/main/g/google-chrome-stable/google-chrome-stable_#{@_ver}-1_amd64.deb"
+    source_sha256 'b7edb7cd5c166bf3c0a1d245baa5924e242c3b81b97090468bec778f41f40373'
+    depends_on 'alien' => :build
+    depends_on 'nspr'
+    depends_on 'cairo'
+    depends_on 'gtk3'
+    depends_on 'expat'
+    depends_on 'cras'
+    depends_on 'sommelier'
+  end
+
+  def self.build
+    @_deb = "google-chrome-stable_#{@_ver}-1_amd64.deb"
+    FileUtils.mv "../#{@_deb}", '.'
+    system "alien -tc #{@_deb}"
+    system "tar xvf google-chrome-stable-#{@_ver}.tgz > /dev/null"
+  end
+
+  def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.cp_r 'opt/', CREW_PREFIX
+    FileUtils.mv 'usr/share', CREW_DEST_PREFIX
+    FileUtils.mv 'etc', CREW_DEST_PREFIX
+    FileUtils.ln_s "#{CREW_PREFIX}/opt/google/chrome/google-chrome", "#{CREW_DEST_PREFIX}/bin/google-chrome"
+  end
+    
+  def self.remove
+    FileUtils.rm_rf "#{CREW_PREFIX}/opt/google/chrome"
+  end
+end
