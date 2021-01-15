1a5998a371caa17f69c6694d5b5b304d267d615c
satmandu <satadru@umich.edu>
2021-01-15 12:47:08 -0500

    coreutils -> 8.32

--- packages/coreutils.rb
+++ packages/coreutils.rb
@@ -3,34 +3,36 @@ require 'package'
 class Coreutils < Package
   description 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.'
   homepage 'http://www.gnu.org/software/coreutils/coreutils.html'
-  version '8.29'
+  @_ver = '8.32'
+  version @_ver
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gnu/coreutils/coreutils-8.29.tar.xz'
-  source_sha256 '92d0fa1c311cacefa89853bdb53c62f4110cdfda3820346b59cbd098f40f955e'
+  source_url "https://ftpmirror.gnu.org/gnu/coreutils/coreutils-#{@_ver}.tar.xz"
+  source_sha256 '4458d8de7849df44ccab15e16b1548b285224dbba5f08fac070c1c0e0bcc4cfa'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.29-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.29-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.29-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.29-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.32-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.32-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.32-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/coreutils-8.32-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'cc4f7ac351c1de16e6dd195c795b3505f05fc981bae46d0ab7cfbc7009ee920d',
-     armv7l: 'cc4f7ac351c1de16e6dd195c795b3505f05fc981bae46d0ab7cfbc7009ee920d',
-       i686: 'c55c8f37354f348741f387fe0de01f664dfe20ad89e8ae93b27d66553375add9',
-     x86_64: '26c43836ce9581f9daeddc1b50f88a88d5c1f8417cd545ee417729f94ad59621',
+     aarch64: 'dad8c4a7b6fe38114c099b0e1c7c7130a1f9bddfc80c8d56df978b3bb68e7250',
+      armv7l: 'dad8c4a7b6fe38114c099b0e1c7c7130a1f9bddfc80c8d56df978b3bb68e7250',
+        i686: 'e4b0c9478c2ffb15c7be9aa7219f7e4af334dd260363a497efdd6f5f8d2dfd9e',
+      x86_64: 'd4eb950bb753789b837f9238fbd28cc8ea4e5e84f63a27b92ed676cb5fb418c4',
   })
 
+
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX}"
+    system "env CFLAGS='-flto' ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.cd("#{CREW_DEST_PREFIX}/bin") do
       system "echo '#!/bin/bash' > arch"
-      system "echo '#{CREW_PREFIX}/bin/uname -m' >> arch"
+      system "echo '#{ARCH}' >> arch"
       system 'chmod +x arch'
     end
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
