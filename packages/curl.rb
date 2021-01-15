8649531661398922def5ee1a502f56c299d302b4
satmandu <satadru@umich.edu>
2021-01-14 21:49:28 -0500

    curl recompile with openssl & lto

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,22 +3,23 @@ require 'package'
 class Curl < Package
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.se/'
-  version '7.74.0'
+  @_ver = "7.74.0"
+  version @_ver + '-1'
   compatibility 'all'
-  source_url 'https://curl.se/download/curl-7.74.0.tar.xz'
+  source_url "https://curl.se/download/curl-#{@_ver}.tar.xz"
   source_sha256 '999d5f2c403cf6e25d58319fdd596611e455dd195208746bc6e6d197a77e878b'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/curl-7.74.0-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '6c817cc8f2043982148a08713fd5439efc26e80f44788788b58bde6d5a86186c',
-     armv7l: '6c817cc8f2043982148a08713fd5439efc26e80f44788788b58bde6d5a86186c',
-       i686: '77b6af733566a22e9eb1187470190b9c48e2480dd13589efc452da589530630d',
-     x86_64: '137ee2aadee4e1fad6a4f80b361bdbbb7181baea32cc0b8326aa693a1d3916d4',
+     aarch64: 'f54a7ff2c3521535af5873b061d26514005e3e080e5b444888e25dce95981201',
+      armv7l: 'f54a7ff2c3521535af5873b061d26514005e3e080e5b444888e25dce95981201',
+        i686: 'ba7b7abeda8592135d2ea851f906636716b1f1d577fdcc75ad68bd6f0b67e5e0',
+      x86_64: '7696a5014d09eaff0864ee36fe4a7f8032c99e7eeb56baf0e04a87427e0114c6',
   })
 
   depends_on 'groff' => :build
@@ -35,7 +36,12 @@ class Curl < Package
   depends_on 'zstd'
 
   def self.build
-    system "./configure #{CREW_OPTIONS} --disable-maintainer-mode --enable-ares --with-ldap-lib=#{CREW_LIB_PREFIX}/libldap.so --with-lber-lib=#{CREW_LIB_PREFIX}/liblber.so"
+    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' \
+    ./configure #{CREW_OPTIONS} \
+    --disable-maintainer-mode \
+    --enable-ares \
+    --with-ldap-lib=#{CREW_LIB_PREFIX}/libldap.so \
+    --with-lber-lib=#{CREW_LIB_PREFIX}/liblber.so"
     system 'make'
   end
 
