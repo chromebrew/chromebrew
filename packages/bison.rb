247d616257bfed8a26e37d0654f56d0c58340cce
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-18 13:09:13 +0800

    Update bison.rb

--- packages/bison.rb
+++ packages/bison.rb
@@ -3,9 +3,10 @@ require 'package'
 class Bison < Package
   description 'Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser employing LALR(1) parser tables.'
   homepage 'https://www.gnu.org/software/bison/'
-  version '3.7.4'
+  @_ver = '3.7.4'
+  version @_ver
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gnu/bison/bison-3.7.4.tar.xz'
+  source_url "https://ftpmirror.gnu.org/gnu/bison/bison-#{@_ver}.tar.xz"
   source_sha256 'a3b5813f48a11e540ef26f46e4d288c0c25c7907d9879ae50e430ec49f63c010'
 
   def self.build
