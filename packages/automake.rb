cc4ccd169390a6a27acdbcd886264a68756e4114
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-18 13:07:45 +0800

    Update automake.rb

--- packages/automake.rb
+++ packages/automake.rb
@@ -3,9 +3,10 @@ require 'package'
 class Automake < Package
   description 'Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.'
   homepage 'https://www.gnu.org/software/automake/'
-  version '1.16.3'
+  @_ver = '1.16.3'
+  version @_ver
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/automake/automake-1.16.3.tar.gz'
+  source_url "https://ftpmirror.gnu.org/automake/automake-#{@_ver}.tar.gz"
   source_sha256 'ce010788b51f64511a1e9bb2a1ec626037c6d0e7ede32c1c103611b9d3cba65f'
 
   depends_on 'autoconf'
