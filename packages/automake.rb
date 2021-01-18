872e976b1a54df58d61eb0dab28ada0217bcdc31
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-18 12:50:57 +0800

    Update automake.rb

--- packages/automake.rb
+++ packages/automake.rb
@@ -2,29 +2,16 @@ require 'package'
 
 class Automake < Package
   description 'Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.'
-  homepage 'http://www.gnu.org/software/automake/'
-  version '1.16.2'
+  homepage 'https://www.gnu.org/software/automake/'
+  version '1.16.3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/automake/automake-1.16.2.tar.xz'
-  source_sha256 'ccc459de3d710e066ab9e12d2f119bd164a08c9341ca24ba22c9adaa179eedd0'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/automake-1.16.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/automake-1.16.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/automake-1.16.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/automake-1.16.2-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '3f318d4d4873a656e805ee7a60f9d442922f6e2f0efffd7c6639f43f98d727a3',
-     armv7l: '3f318d4d4873a656e805ee7a60f9d442922f6e2f0efffd7c6639f43f98d727a3',
-       i686: '4df8a089bfeef4610ad7a1e6eb303fbd8e49182252522dc2a2ddac4aa173c648',
-     x86_64: 'b24ad3589d0ea2ad97f213157ccca85a965d268cf128dabf9e5d11243d77babc',
-  })
+  source_url 'https://ftpmirror.gnu.org/automake/automake-1.16.3.tar.gz'
+  source_sha256 'ce010788b51f64511a1e9bb2a1ec626037c6d0e7ede32c1c103611b9d3cba65f'
 
   depends_on 'autoconf'
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX}"
+    system "./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
