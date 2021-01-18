86a37e13f5dce3d744c0378718ec7287fce4410e
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-18 10:45:45 -0500

    add changes
    Add abort if gettext is installed conflict and libdir to self.install

--- packages/gettext_tiny.rb
+++ packages/gettext_tiny.rb
@@ -8,11 +8,15 @@ class Gettext_tiny < Package
   source_url 'http://ftp.barfooze.de/pub/sabotage/tarballs/gettext-tiny-0.3.2.tar.xz'
   source_sha256 'a9a72cfa21853f7d249592a3c6f6d36f5117028e24573d092f9184ab72bbe187'
 
+  def self.preinstall
+    abort "gettext already installed. gettext_tiny conflics with gettext.".lightred if File.exist? "#{CREW_PREFIX}/bin/msgcomm"
+  end
+
   def self.build
     system "make", "LIBINTL=NONE"
   end
 
   def self.install
-    system "make", "LIBINTL=NONE", "DESTDIR=#{CREW_DEST_DIR}", "prefix=/usr/local", "install"
+    system "make", "LIBINTL=NONE", "DESTDIR=#{CREW_DEST_DIR}", "prefix=#{CREW_PREFIX}", "libdir=#{CREW_LIB_PREFIX}", "install"
   end
 end
