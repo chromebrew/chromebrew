6a72c2d204d2ceeba4040216baa2890bc1dcd67f
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-18 12:41:48 +0800

    Update bison.rb

--- packages/bison.rb
+++ packages/bison.rb
@@ -6,7 +6,7 @@ class Bison < Package
   version '3.7.4'
   compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/gnu/bison/bison-3.7.4.tar.xz'
-  source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  source_sha256 'a3b5813f48a11e540ef26f46e4d288c0c25c7907d9879ae50e430ec49f63c010'
 
   def self.build
     system "./configure #{CREW_OPTIONS}"
