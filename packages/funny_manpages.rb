f41e4e7683b2d9c1d84677d3149d630065f83a49
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-01-19 00:01:46 -0500

    [New Package] funny-manpages

---
+++ packages/funny_manpages.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Funny_manpages < Package
+  description 'funny unix manpages'
+  homepage 'https://github.com/ltworf/funny-manpages'
+  version '2.3'
+  compatibility 'all'
+  source_url 'https://github.com/ltworf/funny-manpages/releases/download/2.3/funny-manpages_2.3.orig.tar.gz'
+  source_sha256 'b3459487abf375d0925494f1c015103e2f3198977b68640bb29bc2c3a8f89c83'
+
+  depends_on 'mandb'
+
+  def self.install
+    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "mkdir #{CREW_DEST_PREFIX} -p"
+    system "mv #{CREW_DEST_DIR}/usr/share #{CREW_DEST_PREFIX}/ -v"
+  end
+end
