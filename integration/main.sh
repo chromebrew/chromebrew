b33b7a7e5348ec98d9698aa799ab10bbd7469efc
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-20 01:28:32 +0800

    Update main.sh

--- integration/main.sh
+++ integration/main.sh
@@ -10,9 +10,9 @@ EXTENSION_PREFIX=${HOME}/MyFiles/.extension
 PWA_PREFIX=${CREW_LIB_PREFIX}/pwa
 SERVER=${PWA_PREFIX}/server.rb
 SENDER=${PWA_PREFIX}/send.rb
-appicon_path=${EXTENSION_PREFIX}/icon
-extension_id=id_id
-help='
+APPICON_PATH=${EXTENSION_PREFIX}/icon
+ID=id_id
+HELP='
   -s                  Start shortcut server
   -n (App Name)       Make a new shortcut
   -h                  Show this message
@@ -28,24 +28,24 @@ case ${1} in
        pkill ruby
        #######################################
        # icon
-       cmd=${2}
-       desktop_file=$( find ${CREW_PREFIX} -name ${2}.desktop | sed -n '1p' )
-       if [[ ${desktop_file} != '' ]]; then
-         appname=$(grep 'Name=' ${desktop_file} | cut -d'=' -f2 | sed -n '1p' )
+       CMD=${2}
+       DESKTOP_FILE=$( find ${CREW_PREFIX} -name ${2}.desktop | sed -n '1p' )
+       if [[ ${DESKTOP_FILE} != '' ]]; then
+         APPNAME=$(grep 'Name=' ${DESKTOP_FILE} | cut -d'=' -f2 | sed -n '1p' )
        else
-         appname=${2^}
+         APPNAME=${2^}
        fi
-       if [ -f "${appicon_path}/${appname}.png" ]; then
-         echo -e "${GREEN}Found an icon for ${appname^}, using it.${RESET}"
+       if [ -f "${APPICON_PATH}/${APPNAME}.png" ]; then
+         echo -e "${GREEN}Found an icon for ${APPNAME^}, using it.${RESET}"
        else
-         icon () { ls -1 ${CREW_PREFIX}/share/pixmaps/ | grep ${cmd}; }
+         icon () { ls -1 ${CREW_PREFIX}/share/pixmaps/ | grep ${CMD}; }
          if [[ $(icon) != '' ]]; then
-           num=$(icon | wc -l)
-           if [[ $num = 1 ]]; then
-             echo -e "${GREEN}Found an preinstalled icon for ${appname^}, using it.${RESET}"
-             convert ${CREW_PREFIX}/share/pixmaps/$(icon) ${appicon_path}/${2}.png
+           NUM=$(icon | wc -l)
+           if [[ $NUM = 1 ]]; then
+             echo -e "${GREEN}Found an preinstalled icon for ${APPNAME^}, using it.${RESET}"
+             convert ${CREW_PREFIX}/share/pixmaps/$(icon) ${APPICON_PATH}/${2}.png
            else
-             echo -e "${BLUE}${num} icons were found for ${appname^}, here is the path of them${RESET}"
+             echo -e "${BLUE}${num} icons were found for ${APPNAME^}, here is the path of them${RESET}"
              icon
              read -r -p "Which icon do you want to use (Enter the path): " icon_path
              echo -e "${RESET}"
@@ -54,13 +54,13 @@ case ${1} in
            echo -e "${YELLOW}${2^} does not provide any icon :/ Using default Chromebrew icon.${RESET}"
          fi
        fi
-       if [[ ${icon_path} = '' ]]; then
-           icon_path="${appicon_path}/${2}.png"
+       if [[ ${ICON_PATH} = '' ]]; then
+           icon_path="${APPICON_PATH}/${2}.png"
        fi
-       convert ${icon_path} -resize 1024x1024 ${appicon_path}/${2}.png
+       convert ${ICON_PATH} -resize 1024x1024 ${APPICON_PATH}/${2}.png
        echo -e "${GREEN}Shortcut for ${2^} deployed!${RESET}"
        pkill ruby
-       ruby ${PWA_PREFIX}/sender.rb "chrome-extension://${extension_id}/main.html?cmd=${2}&friendly_name=${appname}"
+       ruby ${PWA_PREFIX}/sender.rb "chrome-extension://${ID}/main.html?cmd=${2}&friendly_name=${APPNAME}"
        sleep 5
        exec ruby ${SERVER} &
        ;;
@@ -79,11 +79,11 @@ case ${1} in
        ;;
   -i)
        pkill ruby
-       ruby ${PWA_PREFIX}/sender.rb "chrome-extension://${extension_id}/main.html?cmd=terminal&friendly_name=Terminal"
+       ruby ${PWA_PREFIX}/sender.rb "chrome-extension://${ID}/main.html?cmd=terminal&friendly_name=Terminal"
        sleep 5
        exec ruby ${SERVER} &
        ;;
   *)
-       echo -e "${BLUE}${help}${RESET}"
+       echo -e "${BLUE}${HELP}${RESET}"
        ;;
 esac
