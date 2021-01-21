bc3f3d10a2e3ae0911f073334e773373161ece60
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-21 23:43:42 +0800

    Bison => 3.7.4 (#4935)
    * Update bison.rb

--- packages/bison.rb
+++ packages/bison.rb
@@ -2,27 +2,28 @@ require 'package'
 
 class Bison < Package
   description 'Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser employing LALR(1) parser tables.'
-  homepage 'http://www.gnu.org/software/bison/'
-  version '3.5.3'
+  homepage 'https://www.gnu.org/software/bison/'
+  @_ver = '3.7.4'
+  version @_ver
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gnu/bison/bison-3.5.3.tar.xz'
-  source_sha256 '2bf85b5f88a5f2fa8069aed2a2dfc3a9f8d15a97e59c713e3906e5fdd982a7c4'
-
+  source_url "https://ftpmirror.gnu.org/gnu/bison/bison-#{@_ver}.tar.xz"
+  source_sha256 'a3b5813f48a11e540ef26f46e4d288c0c25c7907d9879ae50e430ec49f63c010'
+  
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.5.3-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.7.4-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.7.4-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.7.4-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/bison-3.7.4-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '1ff70ed7db74cadbb20d0e17a6d631bd256caf3549c0932c347eba3edf1f2a53',
-     armv7l: '1ff70ed7db74cadbb20d0e17a6d631bd256caf3549c0932c347eba3edf1f2a53',
-       i686: '091fc0b760316ca632cce4240b1fa910c1528a953d1328d5edb5e05239a8e875',
-     x86_64: '1e810c55fec98830af9089b773150efdc5a1d9a9380bf885b4ae6bf8261111d9',
+     aarch64: 'ba8edbb5a88d16aedbd7c37dc247bd4a4fec5390063dc6220b1275a0bdcfb44c',
+      armv7l: 'ba8edbb5a88d16aedbd7c37dc247bd4a4fec5390063dc6220b1275a0bdcfb44c',
+        i686: '5e3db2010be0f04fefe4d4db3c265e4c5c96e7d9b36748fbd221fb71c4405ff5',
+      x86_64: '9ae17d6c8ae82bbf4ff2d6e2cdca52f91c21a8498b972f1012629697e6d8ee5d',
   })
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
