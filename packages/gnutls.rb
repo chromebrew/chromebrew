ea4e0164a1620184aa472c42a0f838e746539b8f
satmandu <satadru@umich.edu>
2021-01-21 18:59:01 -0500

    Packages updated to work with OpenSSL (#4922)
    * Nettle -> 3.7 & using openssl
    
    * gnutls -> 3.7.0 & use openssl
    
    * curl recompile with openssl & lto
    
    * git -> 2.30.0 & using openssl and LTO
    
    * xwayland compiled with openssl & dependencies
    
    * sommelier recompiled with llvm11, lto, openssl, xwayland
    
    * Update for openssl
    
    * fix transposition error
    
    * reintroduce spacing

--- packages/gnutls.rb
+++ packages/gnutls.rb
@@ -3,22 +3,22 @@ require 'package'
 class Gnutls < Package
   description 'GnuTLS is a secure communications library implementing the SSL, TLS and DTLS protocols and technologies around them.'
   homepage 'http://gnutls.org/'
-  version '3.6.12'
+  version '3.7.0'
   compatibility 'all'
-  source_url 'https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.12.tar.xz'
-  source_sha256 'bfacf16e342949ffd977a9232556092c47164bd26e166736cf3459a870506c4b'
+  source_url 'https://www.gnupg.org/ftp/gcrypt/gnutls/v3.7/gnutls-3.7.0.tar.xz'
+  source_sha256 '49e2a22691d252c9f24a9829b293a8f359095bc5a818351f05f1c0a5188a1df8'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.12-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.12-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.12-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.6.12-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.7.0-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.7.0-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.7.0-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gnutls-3.7.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'ef7aac519396162958750c83622781f8f9e4c30cee06b911271a8ae3d8cb44a4',
-     armv7l: 'ef7aac519396162958750c83622781f8f9e4c30cee06b911271a8ae3d8cb44a4',
-       i686: 'd403e4200b18dfafba3c46bdf3c95e3e59088499b8fae5e22085f65b8a99fc49',
-     x86_64: '261a3766026d916bdb4a68fd1b55a9409e52ac06e2949310371bf9a13dea3751',
+     aarch64: '4e7f7e7314732e4716817f541e408a76489a53a9bb3b98e0c4072bb290c50cf5',
+      armv7l: '4e7f7e7314732e4716817f541e408a76489a53a9bb3b98e0c4072bb290c50cf5',
+        i686: 'c3ebd7f90ca13365f208afd7384f6979de959c38fd7cc2ea780b4f951e743b36',
+      x86_64: '74ede0950a89488bf91cc9e144f7318d8e26b4a522d50db557f182cef66ced39',
   })
 
   depends_on 'zlibpkg'
@@ -33,10 +33,13 @@ class Gnutls < Package
   depends_on 'libidn2'
 
   def self.build
-    system "./configure", "--enable-shared", "--disable-static", "--with-pic",
-      "--with-system-priority-file=#{CREW_PREFIX}/etc/gnutls/default-priorities",
-      "--with-unbound-root-key-file=#{CREW_PREFIX}/etc/unbound/root.key",
-      "--libdir=#{CREW_LIB_PREFIX}"
+    system "./configure --help"
+    system "env CFLAGS='-flto=1' CXXFLAGS='-flto=1' \
+    ./configure #{CREW_OPTIONS} \
+    --enable-shared \
+    --with-pic \
+    --with-system-priority-file=#{CREW_PREFIX}/etc/gnutls/default-priorities \
+    --with-unbound-root-key-file=#{CREW_PREFIX}/etc/unbound/root.key"
     system "make"
   end
 
