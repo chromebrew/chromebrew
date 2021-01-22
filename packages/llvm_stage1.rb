c07bcf66761d59708fe7146c31866cf1113b4b30
satmandu <satadru@umich.edu>
2021-01-22 13:25:09 -0500

    llvm -> 11.1.0 (#4775)
    * llvm -> 11.0.1 (rc2)
    
    * add ARM logic
    
    * Add AMDGPU logic since mesa complains otherwise
    
    * build all targets (otherwise mesa build complains)
    
    * rubify
    
    * Update llvm.rb
    
    * cleanup
    
    * sync w/ WIP build.
    
    * Update llvm.rb
    
    * sync to currently compiling build
    
    * use binutils branch
    
    * Current testing package file
    
    * Working on x86_64!
    
    * This allows compilation with llvm installed.
    
    * add lto-fat-objects and also use parallel lto
    
    * Update llvm.rb
    
    * Add llvm_stage1
    
    * Don't build AMDGPU target for i686, reduces file size below 250Mb
    
    * cleanup
    
    * s/Tenacious D Joke//
    
    * remove unused constants
    
    * set LLVM_DEFAULT_TARGET_TRIPLE to CREW_BUILD
    
    * Use CREW_BUILD
    
    * Reflect true version
    
    * remove binutils, rubyize
    
    * rubyize

--- packages/llvm_stage1.rb
+++ packages/llvm_stage1.rb
@@ -1,7 +1,7 @@
 require 'package'
 
-class Llvm < Package
-  description 'The LLVM Project is a collection of modular and reusable compiler and toolchain technologies. The optional packages clang, lld, lldb, polly, compiler-rt, libcxx, libcxxabi, and openmp are included.'
+class Llvm_stage1 < Package
+  description 'THIS IS NOT THE LLVM PACKAGE. This is only a Limited Stage 1 compile of LLVM.'
   homepage 'http://llvm.org/'
   @_ver = '11.1.0-rc1'
   version @_ver
@@ -10,16 +10,16 @@ class Llvm < Package
   source_sha256 'e610297041129a5c5b24355a988c99c7452ee7105ee2355334a8c521b988eb3c'
 
   binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/llvm-11.1.0-rc1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/llvm-11.1.0-rc1-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/llvm-11.1.0-rc1-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/llvm-11.1.0-rc1-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/llvm_stage1-11.1.0-rc1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/llvm_stage1-11.1.0-rc1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/llvm_stage1-11.1.0-rc1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/llvm_stage1-11.1.0-rc1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-     aarch64: '5ecb72e60483b15c7588446a7ac424d64cdd3f21ca0fa5ffea4737a25dedca89',
-      armv7l: '5ecb72e60483b15c7588446a7ac424d64cdd3f21ca0fa5ffea4737a25dedca89',
-        i686: '5595ca097aa454bd54b8a336d467ee266f8afd4bd8100d1a3831e3face9b4f43',
-      x86_64: '031f6f3510ebbc5a1270fac07934d7efa9126c8e89f98695505b1376d6d233a5',
+     aarch64: '863dd7cae43abef88dfe25c60e0dda1f8d59756f8c8fbb111989a3669dca0a16',
+      armv7l: '863dd7cae43abef88dfe25c60e0dda1f8d59756f8c8fbb111989a3669dca0a16',
+        i686: '84a5a2a381af8c789fcb895d1ae62dc1932b3ad1ad423e8351e37aa9be5e2490',
+      x86_64: 'ddb9e88444729174b0c6f4de883910ce78e8fa489e0831acdf92daaf4561e9af',
   })
   
   # llvm_stage1 is compiled with gcc, without -flto
@@ -32,50 +32,28 @@ class Llvm < Package
   depends_on 'swig'
   depends_on 'pygments' => :build
   depends_on 'ccache' => :build
-  depends_on 'llvm_stage1' => :build
-  depends_on 'binutils' => :build
 
   case ARCH
   when 'aarch64','armv7l'
     #LLVM_TARGETS_TO_BUILD = 'ARM;AArch64;AMDGPU'
-    LLVM_TARGETS_TO_BUILD = 'all'
-    @ARCH_C_FLAGS = "-fPIC -march=armv7-a -mfloat-abi=hard -ccc-gcc-name #{LLVM_DEFAULT_TARGET_TRIPLE}"
-    @ARCH_CXX_FLAGS = "-fPIC -march=armv7-a -mfloat-abi=hard -ccc-gcc-name #{LLVM_DEFAULT_TARGET_TRIPLE}"
-    @ARCH_LDFLAGS=''
-  when 'i686'
-    LLVM_TARGETS_TO_BUILD = 'X86'
-    # Because ld.lld: error: undefined symbol: __atomic_store
-    # Polly demands fPIC
-    @ARCH_C_FLAGS = '-latomic -fPIC'
-    @ARCH_CXX_FLAGS = '-latomic -fPIC'
-    # Because getting this error:
-    # ld.lld: error: relocation R_386_PC32 cannot be used against symbol isl_map_fix_si; recompile with -fPIC
-    # So as per https://github.com/openssl/openssl/issues/11305#issuecomment-602003528
-    @ARCH_LDFLAGS='-Wl,-znotext'
-  when 'x86_64'
+    @ARCH_C_FLAGS = '-march=armv7-a -mfloat-abi=hard'
+    @ARCH_CXX_FLAGS = '-march=armv7-a -mfloat-abi=hard'
+    LLVM_PROJECTS_TO_BUILD = 'clang;clang-tools-extra;libcxx;libcxxabi;lld'
+  when 'i686','x86_64'
     #LLVM_TARGETS_TO_BUILD = 'X86;AMDGPU'
-    LLVM_TARGETS_TO_BUILD = 'all'
     @ARCH_C_FLAGS = '-fPIC'
     @ARCH_CXX_FLAGS = '-fPIC'
-    @ARCH_LDFLAGS=''
+    LLVM_PROJECTS_TO_BUILD = 'clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lld'
   end
-  @ARCH_C_LTO_FLAGS = "#{@ARCH_C_FLAGS} -flto=thin -fuse-ld=lld"
-  @ARCH_CXX_LTO_FLAGS = "#{@ARCH_CXX_FLAGS} -flto=thin -fuse-ld=lld"
-  LLVM_PROJECTS_TO_BUILD = 'clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lldb;compiler-rt;lld;polly;openmp'
   
-  # Using Targets 'all' for non-i686 because otherwise mesa complains.
+  @ARCH_C_LTO_FLAGS = "#{@ARCH_C_FLAGS} -flto"
+  @ARCH_CXX_LTO_FLAGS = "#{@ARCH_CXX_FLAGS} -flto"
+  
+  # Using Targets 'all' because otherwise mesa complains.
   # This may be patched upstream as per 
   # https://reviews.llvm.org/rG1de56d6d13c083c996dfd44a32041dacae037d66
-  #LLVM_TARGETS_TO_BUILD = 'all'
-
-  def self.patch
-    # This keeps system libraries from being linked in as dependencies on a build host image, such as in docker.
-    # Uncomment the rest of this section if you need to.
-    #system "sudo rm /lib#{CREW_LIB_SUFFIX}/libncurses.so.5 || true"
-    #system "sudo rm /usr/lib#{CREW_LIB_SUFFIX}/libform.so || true"
-    #system "sudo ln -s #{CREW_LIB_PREFIX}/libncurses.so.6 /lib#{CREW_LIB_SUFFIX}/libncurses.so.5 || true"
-    #system "sudo ln -s #{CREW_LIB_PREFIX}/libform.so /usr/lib#{CREW_LIB_SUFFIX}/libform.so || true"
-  end
+  LLVM_TARGETS_TO_BUILD = 'all'
+  LLVM_VERSION = version.split("-")[0]
 
   def self.build
     ############################################################
@@ -86,13 +64,6 @@ class Llvm < Package
     puts "Setting compile to use python3".lightgreen
     ############################################################
     system "grep -rl '#!.*python' | xargs sed -i '1s/python$/python3/'"
-    
-    ############################################################
-    # puts "Downloading binutils #{BINUTILS_BRANCH} src to enable gold plugin build".lightgreen
-    ############################################################
-    # system "git config --global advice.detachedHead false"
-    # As per https://github.com/SVF-tools/SVF/wiki/Install-LLVM-Gold-Plugin-on-Ubuntu
-    # system "git clone --depth 1 --branch #{BINUTILS_BRANCH} git://sourceware.org/git/binutils-gdb.git binutils"
     
     Dir.mkdir 'builddir'
     Dir.chdir 'builddir' do
@@ -108,31 +79,23 @@ cxx_sys=#{CREW_PREFIX}/include/c++/\${version}
 cxx_inc=#{CREW_PREFIX}/include/c++/\${version}/\${machine}
 gnuc_lib=#{CREW_LIB_PREFIX}/gcc/\${machine}/\${version}
 clang++ -fPIC  -rtlib=compiler-rt -stdlib=libc++ -cxx-isystem \${cxx_sys} -I \${cxx_inc} -B \${gnuc_lib} -L \${gnuc_lib} \"\$@\"' > clc++"
-        system "env PATH=#{CREW_LIB_PREFIX}/ccache/bin:#{CREW_PREFIX}/bin:/usr/bin:/bin LD=ld.lld \
+        system "env PATH=#{CREW_LIB_PREFIX}/ccache/bin:#{CREW_PREFIX}/bin:/usr/bin:/bin FC= \
             cmake -G Ninja \
-            -DLLVM_ENABLE_LTO=Thin \
-            -DCMAKE_C_COMPILER=$(which clang) \
-            -DCMAKE_CXX_COMPILER=$(which clang++) \
             -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} \
             -DLLVM_DEFAULT_TARGET_TRIPLE=#{CREW_BUILD} \
-            -DCMAKE_C_COMPILER_TARGET=#{CREW_BUILD} \
-            -DCMAKE_ASM_COMPILER_TARGET=#{CREW_BUILD} \
             -DLLVM_TARGETS_TO_BUILD=\'#{LLVM_TARGETS_TO_BUILD}' \
             -DCMAKE_BUILD_TYPE=Release \
             -DLLVM_LIBDIR_SUFFIX='#{CREW_LIB_SUFFIX}' \
-            -DCMAKE_LINKER=$(which ld.lld) \
-            -D_CMAKE_TOOLCHAIN_PREFIX=llvm- \
+            -DCMAKE_LINKER=$(which ld.gold) \
+            -D_CMAKE_TOOLCHAIN_PREFIX=gcc- \
             -DLLVM_BUILD_LLVM_DYLIB=ON \
             -DLLVM_LINK_LLVM_DYLIB=ON \
-            -DLLVM_CCACHE_BUILD=ON \
             -DLLVM_ENABLE_FFI=ON \
             -DLLVM_ENABLE_RTTI=ON \
-            -DCMAKE_C_FLAGS='#{@ARCH_C_LTO_FLAGS}' \
-            -DCMAKE_CXX_FLAGS='#{@ARCH_CXX_LTO_FLAGS}' \
-            -DCMAKE_EXE_LINKER_FLAGS='#{@ARCH_LDFLAGS}' \
+            -DCMAKE_C_FLAGS='#{@ARCH_C_FLAGS}' \
+            -DCMAKE_CXX_FLAGS='#{@ARCH_CXX_FLAGS}' \
             -DLLVM_PARALLEL_LINK_JOBS=1 \
             -DPYTHON_EXECUTABLE=$(which python3) \
-            -DLLVM_INSTALL_UTILS=ON \
             -DLLVM_BINUTILS_INCDIR='#{CREW_PREFIX}/include' \
             -DLLVM_OPTIMIZED_TABLEGEN=ON \
             -DLLVM_ENABLE_TERMINFO=ON \
@@ -147,7 +110,7 @@ clang++ -fPIC  -rtlib=compiler-rt -stdlib=libc++ -cxx-isystem \${cxx_sys} -I \${
             -DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON \
             -Wno-dev \
             ../llvm"
-      system 'ninja'
+      system 'ninja -j1'
     end
   end
 
@@ -160,7 +123,7 @@ clang++ -fPIC  -rtlib=compiler-rt -stdlib=libc++ -cxx-isystem \${cxx_sys} -I \${
       FileUtils.ln_s "lib#{CREW_LIB_SUFFIX}/LLVMgold.so", "#{CREW_DEST_LIB_PREFIX}/bfd-plugins/"
     end
   end
-
+  
   def self.check
     Dir.chdir("builddir") do
       #system "ninja check-llvm || true"
