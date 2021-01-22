cfd4d14f86a490f39dab7dcaf3c607dc04c45e5f
satmandu <satadru@umich.edu>
2021-01-22 13:47:29 -0500

    At spi2 updates (#4969)
    * at_spi2_core -> 2.39.1
    
    * at_spi2_atk -> 2.38.0

--- packages/at_spi2_atk.rb
+++ packages/at_spi2_atk.rb
@@ -1,24 +1,24 @@
 require 'package'
-
+  
 class At_spi2_atk < Package
   description 'D-Bus AT-SPI'
   homepage 'http://www.freedesktop.org/'
-  version '2.26.1'
+  version '2.38.0'
   compatibility 'all'
-  source_url 'https://github.com/GNOME/at-spi2-atk/archive/AT_SPI2_ATK_2_26_1.tar.gz'
-  source_sha256 '45fc52f794c785d54d6e2689f51ade043c96ea0263f6113c1ce26df1ef26af5b'
+  source_url 'https://github.com/GNOME/at-spi2-atk/archive/AT_SPI2_ATK_2_38_0.tar.gz'
+  source_sha256 '95f10c80834d3811938153199da671967ee3c8f378883ed3f6ddeee1d316d3e4'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.26.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.26.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.26.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.26.1-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.38.0-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.38.0-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.38.0-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/at_spi2_atk-2.38.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '4e72e7b89903b2361b55065beefa639904ae880741fbc64b1914ee9fd596a1a8',
-     armv7l: '4e72e7b89903b2361b55065beefa639904ae880741fbc64b1914ee9fd596a1a8',
-       i686: '165854bff7a99fcfaf7a807e41eaa6d889c10998250f0c706dcb9eee3d198aba',
-     x86_64: '01c66589188a81499b7ac4424f937f3adbe7265bb1c55d8d0d8746df8d302061',
+     aarch64: '097744ccba6da2013771b9da22a887e64cd6fd0df0b39bc1576e9131e1ff30a9',
+      armv7l: '097744ccba6da2013771b9da22a887e64cd6fd0df0b39bc1576e9131e1ff30a9',
+        i686: '12aba0b16a694f3e79e7fa562bfd7510c27d8d4864a5b57f9caed48e7801a890',
+      x86_64: 'ce793ff57e4c0ba75b76e8f12934741f6cb26b4d02cc9080e647c28170b1f627',
   })
 
   depends_on 'automake' => :build
@@ -26,12 +26,12 @@ class At_spi2_atk < Package
   depends_on 'atk'
 
   def self.build
-    system "./autogen.sh"
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+    system "meson #{CREW_MESON_LTO_OPTIONS} builddir"
+    system "meson configure builddir"
+    system "ninja -C builddir"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
