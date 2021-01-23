33ba6a93b62318b7e6eb54f0a317aa6b82322f8b
satmandu <satadru@umich.edu>
2021-01-23 13:04:01 -0500

    FIX crew error on reinstall, Add llvm to base install & use llvm-strip instead of strip (#4975)
    * Have crew use llvm-strip if available
    
    * Add llvm to base install
    
    * Fix "no implicit conversion of String into Array (TypeError)" error
    
    * uprev

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.5.7'
+CREW_VERSION = '1.5.8'
 
 ARCH_ACTUAL = `uname -m`.strip
 # This helps with virtualized builds on aarch64 machines
