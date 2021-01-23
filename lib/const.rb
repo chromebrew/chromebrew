20e7c68790e3bed07ade5e0e4832c2f03266f6eb
satmandu <satadru@umich.edu>
2021-01-23 17:32:58 -0500

    uprev

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.5.9'
+CREW_VERSION = '1.5.10'
 
 ARCH_ACTUAL = `uname -m`.strip
 # This helps with virtualized builds on aarch64 machines
