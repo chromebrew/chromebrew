a7b46a97a62e20e88ad23639edd1d6fb740558ab
satmandu <satadru@umich.edu>
2021-01-24 15:52:53 -0500

    Wayland & Wayland protocols binaries (#4987)
    * wayland binaries
    
    * add binaries

--- packages/wayland.rb
+++ packages/wayland.rb
@@ -8,6 +8,19 @@ class Wayland < Package
   source_url 'https://wayland.freedesktop.org/releases/wayland-1.18.0.tar.xz'
   source_sha256 '4675a79f091020817a98fd0484e7208c8762242266967f55a67776936c2e294d'
 
+  binary_url ({
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wayland-1.18.0-2-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wayland-1.18.0-2-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/wayland-1.18.0-2-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wayland-1.18.0-2-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+     aarch64: 'f5686b065f2395724aadbd5c27d953aa9a93035f35fd71b315fcddf96c89507a',
+      armv7l: 'f5686b065f2395724aadbd5c27d953aa9a93035f35fd71b315fcddf96c89507a',
+        i686: '1abcde23427ad7fffc9773cca1695790ec08e7a0ee2b98c4601f395a808acac4',
+      x86_64: '41abcb4d0bf10e5b09b8cecdcfe8685379d91b07e541ed54654477c8fbb807fb',
+  })
+
   depends_on 'expat'
   depends_on 'libpng'
   depends_on 'libffi'
--- packages/wayland_protocols.rb
+++ packages/wayland_protocols.rb
@@ -8,6 +8,19 @@ class Wayland_protocols < Package
   source_url 'https://wayland.freedesktop.org/releases/wayland-protocols-1.20.tar.xz'
   source_sha256 '9782b7a1a863d82d7c92478497d13c758f52e7da4f197aa16443f73de77e4de7'
 
+  binary_url ({
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.20-1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.20-1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.20-1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wayland_protocols-1.20-1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+     aarch64: '1decd76dce195360e3b2879b7a1ee63efefc64b437faa98e9ff5c833758730d2',
+      armv7l: '1decd76dce195360e3b2879b7a1ee63efefc64b437faa98e9ff5c833758730d2',
+        i686: '6ff5c74feef6541c342f7393fb2df70e4ec121f4b94d55f254bfa28c6e3e3dc0',
+      x86_64: 'a88f0f3c10c6108c6b9b584a1c74998d8a49ea70cb022f6350d3df129f120a72',
+  })
+
   depends_on 'wayland'
 
   def self.build
