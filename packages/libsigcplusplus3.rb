ea4e45313045e11711590b55ea6b336ac38183b2
satmandu <satadru@umich.edu>
2021-01-26 10:58:03 -0500

    Update to 3.0.6

--- packages/libsigcplusplus3.rb
+++ packages/libsigcplusplus3.rb
@@ -2,35 +2,35 @@ require 'package'
 
 class Libsigcplusplus3 < Package
   description 'libsigc++ implements a typesafe callback system for standard C++.'
-  homepage 'http://libsigc.sourceforge.net/'
-  version '2.99.11'
+  homepage 'https://github.com/libsigcplusplus/libsigcplusplus/'
+  @_ver = '3.0.6'
+  version @_ver
   compatibility 'all'
-  source_url 'https://ftp.gnome.org/pub/GNOME/sources/libsigc++/2.99/libsigc++-2.99.11.tar.xz'
-  source_sha256 '177fb08df33da71780eef2ce4c5991a2b3b6d07d4ad1efbf6ad38f7964e4bb55'
+  source_url "https://github.com/libsigcplusplus/libsigcplusplus/archive/#{@_ver}.tar.gz"
+  source_sha256 '25ff9bf59c28e185c3901963f11bbdac58ce866281c65c870145b119e59a0836'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-2.99.11-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-2.99.11-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-2.99.11-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-2.99.11-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-3.0.6-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-3.0.6-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-3.0.6-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libsigcplusplus3-3.0.6-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '37cc07ba4469b7975214cf2bdb8fed2665962353318888386907f96f08dc9f95',
-     armv7l: '37cc07ba4469b7975214cf2bdb8fed2665962353318888386907f96f08dc9f95',
-       i686: '86a3fed7a036dbcfed62bfd4b83e903d3cc78f241a793546f07af00fc3855d2b',
-     x86_64: 'e0fb98d028040eb6567a153c1314bd51fb044d4f7cf1c2cfaf4cb4d672a31130',
+     aarch64: '30e2d139bec3e807e96ebee3047ec91fb080def2c0d45ca27aa2ba383e8fb9d6',
+      armv7l: '30e2d139bec3e807e96ebee3047ec91fb080def2c0d45ca27aa2ba383e8fb9d6',
+        i686: '497f109d9d2c97bb13ce0df424cb8ec6eb7fa46e34b96e585b9831bb2d75f69c',
+      x86_64: 'f5c843178b73298dc190a48f3dabdf5626121aeb436ba05b26a481160b909bb7',
   })
-
+  
   def self.build
-    system './configure',
-           "CXX_FLAGS=' --std=c++14'",
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-maintainer-mode'
-    system 'make'
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    -Dbuild-examples=false \
+    builddir"
+    system "meson configure builddir"
+    system "ninja -C builddir"
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
