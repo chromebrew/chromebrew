0c32e3801c042cc4f01adcbad5c2ce2e117f288e
satmandu <satadru@umich.edu>
2021-01-26 00:43:38 -0500

    yelp_xsl to 3.38.2

--- packages/yelp_xsl.rb
+++ packages/yelp_xsl.rb
@@ -5,28 +5,29 @@ class Yelp_xsl < Package
   description 'yelp-xsl is a collection of programs and data files to help you build, maintain, and distribute documentation'
   homepage 'https://github.com/GNOME/yelp-xsl'
   compatibility 'all'
-  version '3.27.90'
-  source_url 'https://github.com/GNOME/yelp-xsl/archive/3.27.90.tar.gz'
-  source_sha256 '26df38777769031fbe5fa641e5656d69ebea81098f7627ab677f59dd91f523d6'
+  version '3.38.2'
+  source_url 'https://github.com/GNOME/yelp-xsl/archive/3.38.2.tar.gz'
+  source_sha256 'd9e9cc02fc7bb442601515c68fccc4bfbee69860166ca318be9e6b37525943ad'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.27.90-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.27.90-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.27.90-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.27.90-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.38.2-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.38.2-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.38.2-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-3.38.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '7bc40594bafb4b9ba4fc4f52a0563e70655f4ba8cf30bcb7acd8a80d7d01c2ca',
-     armv7l: '7bc40594bafb4b9ba4fc4f52a0563e70655f4ba8cf30bcb7acd8a80d7d01c2ca',
-       i686: '1e2c1fa0da42c8ff8909b89c18720f235ffcd9f1baa5f5ed581aac8ea30068b9',
-     x86_64: 'c3be835828187766de18110d03f8daed3a7700cdd6aada7e1334042e6a8d0143',
+     aarch64: '3dfe0bdcddbbdc0bd1cd85890919bdec6962d24894fe39db49686c5655d61674',
+      armv7l: '3dfe0bdcddbbdc0bd1cd85890919bdec6962d24894fe39db49686c5655d61674',
+        i686: 'f3a4d2c7155e43b8a43e8188686abfa3e5a195e6479325f5172fa2192ef84977',
+      x86_64: '652e4331e8a136bf3f492618c372063ae86b9feddb407f1edca24fec4362027b',
   })
 
   depends_on 'itstool'
 
   def self.build
     system './autogen.sh'
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
+      ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
