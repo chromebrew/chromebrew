4c4f9f3064852d9f7511553ff0dd4ccdc199c56e
satmandu <satadru@umich.edu>
2021-01-26 17:47:00 -0500

    Fixes compatibility with openssl (#5019)

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,22 +3,23 @@ require 'package'
 class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  version '3.19.2'
+  @_ver = '3.19.3'
+  version @_ver
   compatibility 'all'
-  source_url 'https://github.com/Kitware/CMake/releases/download/v3.19.2/cmake-3.19.2.tar.gz'
-  source_sha256 'e3e0fd3b23b7fb13e1a856581078e0776ffa2df4e9d3164039c36d3315e0c7f0'
+  source_url "https://github.com/Kitware/CMake/releases/download/v#{@_ver}/cmake-#{@_ver}.tar.gz"
+  source_sha256 '3faca7c131494a1e34d66e9f8972ff5369e48d419ea8ceaa3dc15b4c11367732'
 
   binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.2-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.2-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.2-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.2-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.3-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.3-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.3-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/cmake-3.19.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-     aarch64: 'ba16050036e58a4b3c5fa23865608e850eb9de865e9f5a6fac163c84adeca261',
-      armv7l: 'ba16050036e58a4b3c5fa23865608e850eb9de865e9f5a6fac163c84adeca261',
-        i686: 'd7d39b576dd92448b9c1440d9c35e4c4284513cb72f3576af1e282f0c5c58f22',
-      x86_64: '6c0737c9d2cea24bfb4bbbb7ff0fa6139488d6f19866fd799fe5b89adaf36f7a',
+     aarch64: 'f7c3464b04d34ba0e80a9dc00c8a79c7755dfa1002530c9ed3eba52babacf85d',
+      armv7l: 'f7c3464b04d34ba0e80a9dc00c8a79c7755dfa1002530c9ed3eba52babacf85d',
+        i686: 'ee8e96b017d69a9235f65cc72b64451dfefe903a53455aceffacdabe3111f6da',
+      x86_64: '6d6f640ac421c0e93086368b829587a6439e555b6926b4ac337f381cbb65a8bf',
   })
 
   depends_on 'llvm' => :build
@@ -30,7 +31,8 @@ class Cmake < Package
   end
 
   def self.build
-    system "./bootstrap --prefix=#{CREW_PREFIX}"
+    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
+      ./bootstrap --prefix=#{CREW_PREFIX}"
     system 'make'
   end
 
