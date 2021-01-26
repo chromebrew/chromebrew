ab3c429738962a4d5f7dd6f136a58d973388da60
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-26 22:34:11 +0800

    Fix symlinks in Cloudready (#5011)
    * Update install.sh
    
    Co-authored-by: supechicken666 <68530202+supechicken666@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -42,8 +42,8 @@ esac
 sudo chown -R "$(id -u)":"$(id -g)" "${CREW_PREFIX}"
 # Delete 'var' symlink on Cloudready platform
 if [[ $(grep neverware /etc/lsb-release) != "" ]]; then
-  sudo unlink /usr/local/var
-  sudo unlink /usr/local/local
+  [ -L /usr/local/var ] && sudo rm -f /usr/local/var
+  [ -L /usr/local/local ] && sudo rm -f /usr/local/local
 fi
 
 # prepare directories
