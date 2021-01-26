6a7641c79a3f860fed397b70e208afffadef6e31
satmandu <satadru@umich.edu>
2021-01-26 10:58:40 -0500

    glibmm to 2.64.5

--- packages/glibmm.rb
+++ packages/glibmm.rb
@@ -3,33 +3,39 @@ require 'package'
 class Glibmm < Package
   description 'C++ bindings for GLib'
   homepage 'https://www.gtkmm.org'
-  version '2.64.2'
+  version '2.64.5'
   compatibility 'all'
-  source_url 'https://ftp.gnome.org/pub/GNOME/sources/glibmm/2.64/glibmm-2.64.2.tar.xz'
-  source_sha256 'a75282e58d556d9b2bb44262b6f5fb76c824ac46a25a06f527108bec86b8d4ec'
+  source_url 'https://ftp.gnome.org/pub/GNOME/sources/glibmm/2.64/glibmm-2.64.5.tar.xz'
+  source_sha256 '508fc86e2c9141198aa16c225b16fd6b911917c0d3817602652844d0973ea386'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.2-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.5-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.5-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.5-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glibmm-2.64.5-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '72bb27894e3eb810f9cb188a4b80c286213900f01b310ab39b6a68370e9dda3a',
-     armv7l: '72bb27894e3eb810f9cb188a4b80c286213900f01b310ab39b6a68370e9dda3a',
-       i686: '96cfa5d2030370e2dd75ac11dd6389eef53751dab7a66f20abd5f53eaa7ffa48',
-     x86_64: '97c66ad7fb93bf2715de8cfda6ce6afeb0f83d5235602cd7ba3d4cc42f02c6eb',
+     aarch64: '08d942541e0df0a351f6d748051cc7deb18c4aa21c7950d9b166780ad86f2327',
+      armv7l: '08d942541e0df0a351f6d748051cc7deb18c4aa21c7950d9b166780ad86f2327',
+        i686: 'f0be0860b956388e55cc8d6d5cfb67f6ff547e653a06570ea0aa72e0e7121913',
+      x86_64: '2f050d4abd5cda7efa15e8df5dedc713bc18eb60141982c6b322b21d742ea233',
   })
 
   depends_on 'libsigcplusplus'
   depends_on 'mm_common' => :build
 
   def self.build
-    system "./configure #{CREW_OPTIONS} "
-    system "make -j#{CREW_NPROC}"
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    --default-library=both \
+    -Dbuild-documentation=false \
+    -Dbuild-demos=false \
+    -Dbuild-tests=false \
+    builddir"
+    system "meson configure builddir"
+    system "ninja -C builddir"
   end
 
   def self.install
-    system "make install DESTDIR=#{CREW_DEST_DIR}"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
