f3361669ff4302fae858b766840d5a0bda9f985d
satmandu <satadru@umich.edu>
2021-01-26 11:14:49 -0500

    fix man install

--- packages/exfatprogs.rb
+++ packages/exfatprogs.rb
@@ -10,18 +10,18 @@ class Exfatprogs < Package
   source_sha256 '3f755d35785a74138348b3a22dfcda5afc8a69e66a6a0c79be48225e6ca321ce'
 
   binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/exfatprogs-1.0.4-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/exfatprogs-1.0.4-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/exfatprogs-1.0.4-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/exfatprogs-1.0.4-chromeos-x86_64.tar.xz',
+     aarch64: 'file:///usr/local/tmp/packages/exfatprogs-1.0.4-chromeos-armv7l.tar.xz',
+      armv7l: 'file:///usr/local/tmp/packages/exfatprogs-1.0.4-chromeos-armv7l.tar.xz',
+        i686: 'file:///usr/local/tmp/packages/exfatprogs-1.0.4-chromeos-i686.tar.xz',
+      x86_64: 'file:///usr/local/tmp/packages/exfatprogs-1.0.4-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-     aarch64: '2f40e8d171a80311e0014353e33795df9fa60fd11927a0a1f389e44277d31fa7',
-      armv7l: '2f40e8d171a80311e0014353e33795df9fa60fd11927a0a1f389e44277d31fa7',
-        i686: '5ad356044174e945904cdafef8a35cc73c9e88ba51a4680354adadcbfa6cde8c',
-      x86_64: '8638c0b4e22145481c3d9431ac5d9534c9f41595736662cde8423ce4a685b856',
+     aarch64: '0fb0f5a16c09f11c912016a73d0002e252737106a3c2c31fd838b5057bee02b8',
+      armv7l: '0fb0f5a16c09f11c912016a73d0002e252737106a3c2c31fd838b5057bee02b8',
+        i686: 'ef85f1bae5f9079f7664509977c3dc9383c8427bc4109ca67162eb463d10048f',
+      x86_64: '68184ee070d40c97c079fe6b5974e924ee373173b8c802d0c3f166d7277d70e7',
   })
-  
+
   def self.build
     system './autogen.sh'
     system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
@@ -31,6 +31,6 @@ class Exfatprogs < Package
   
   def self.install
     system "make DESTDIR=#{CREW_DEST_DIR} install"
-    system "install -Dm644 -t #{CREW_DEST_DIR}/usr/share/man/man8 */*.8"
+    system "install -Dm644 -t #{CREW_DEST_PREFIX}/share/man/man8 */*.8"
   end
 end
