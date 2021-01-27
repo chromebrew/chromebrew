6f3854f2b4a4d9084357c649fe5a65aec98f049c
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-01-27 20:26:02 +0800

    Update gparted.rb

--- packages/gparted.rb
+++ packages/gparted.rb
@@ -9,19 +9,6 @@ class Gparted < Package
   source_url "https://downloads.sourceforge.net/project/gparted/gparted/gparted-#{@_ver}/gparted-#{@_ver}.tar.gz"
   source_sha256 '6c90715d254d7a7ec0208b29007b64160dd9fb7df4c4aa7f8ec2c9d23114c719'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-     aarch64: '8258baa778feb98e34f766ee8ce822d96eee1fc970313b7992874f0911dcf874',
-      armv7l: '8258baa778feb98e34f766ee8ce822d96eee1fc970313b7992874f0911dcf874',
-        i686: '0ec95233c92cf8abdfe830b959a45fd24aa8ba6f05b6776f2420e93af3a5e65c',
-      x86_64: '17f7af323b09b3b6de020b59ce2e28707e66514bc8505095c37ef9a792a53071',
-  })
-
   depends_on 'parted'
   depends_on 'gtkmm3'
   depends_on 'intltool' => ':build'
