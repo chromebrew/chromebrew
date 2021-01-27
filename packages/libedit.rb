1bc0049787fc90f8f384001349819d7589444b3a
satmandu <satadru@umich.edu>
2021-01-27 17:34:34 -0500

    Libedit -> 20191231 3.1 (#5035)
    * Update libedit to 20191231-3.1
    
    * Update libedit in install.sh

--- packages/libedit.rb
+++ packages/libedit.rb
@@ -3,29 +3,29 @@ require 'package'
 class Libedit < Package
   description 'This is an autotool and libtoolized port of the NetBSD Editline library (libedit).'
   homepage 'http://thrysoee.dk/editline/'
-  version '3.1-0'
+  version '20191231-3.1'
   compatibility 'all'
-  source_url 'https://thrysoee.dk/editline/libedit-20170329-3.1.tar.gz'
-  source_sha256 '91f2d90fbd2a048ff6dad7131d9a39e690fd8a8fd982a353f1333dd4017dd4be'
+  source_url 'https://thrysoee.dk/editline/libedit-20191231-3.1.tar.gz'
+  source_sha256 'dbb82cb7e116a5f8025d35ef5b4f7d4a3cdd0a3909a146a39112095a2d229071'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-3.1-0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-3.1-0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-3.1-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-3.1-0-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-20191231-3.1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-20191231-3.1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-20191231-3.1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libedit-20191231-3.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '90f9db2636bdcae5cb4c8d6a0d020398b13c6290164c27776507689bc5fc85be',
-     armv7l: '90f9db2636bdcae5cb4c8d6a0d020398b13c6290164c27776507689bc5fc85be',
-       i686: '0590ee8aa7923a29332aa436ff42b909cf968c7ab50121df43539d968e3c496f',
-     x86_64: '6f6ed8dd83aad5895b5e3927c673d66c5d676a345f3bc6913e94392a74db929a',
+     aarch64: '610986dcb014c2079b2b355628ee6f7b867076974cf751b891a926f0a0d9d5b8',
+      armv7l: '610986dcb014c2079b2b355628ee6f7b867076974cf751b891a926f0a0d9d5b8',
+        i686: 'dc3d211fd649803d0dd89c3f82163a8bf67eeebaea9629b14bd9e570ba545933',
+      x86_64: '834915020f3fd98b0e3ca8609ae0b915b47ca77ea5877fa7b758a85571401e8e',
   })
 
   def self.build
-    system "./configure \
-            --prefix=#{CREW_PREFIX} \
-            --libdir=#{CREW_LIB_PREFIX} \
-            CPPFLAGS=\"-I#{CREW_PREFIX}/include/ncurses\""
+    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto \"-I#{CREW_PREFIX}/include/ncurses\"' LDFLAGS='-flto=auto' \
+      ./configure \
+      #{CREW_OPTIONS} \
+      CPPFLAGS=\"-I#{CREW_PREFIX}/include/ncurses\""
     system "make"
   end
 
