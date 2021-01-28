fe21969d8e768cbdf89b74dbccde390616f027e1
supechicken <73414985+supechicken@users.noreply.github.com>
2021-01-29 00:50:03 +0800

    [gparted] Add xhost dependency (#5022)
    * gparted
    
    * Update gparted.rb
    
    Co-authored-by: supechicken666 <68530202+supechicken666@users.noreply.github.com>

--- packages/gparted.rb
+++ packages/gparted.rb
@@ -4,23 +4,10 @@ class Gparted < Package
   description 'A Partition Magic clone, frontend to GNU Parted'
   homepage 'https://gparted.org/'
   @_ver = '1.2.0'
-  version @_ver
+  version @_ver + '-1'
   compatibility 'all'
   source_url "https://downloads.sourceforge.net/project/gparted/gparted/gparted-#{@_ver}/gparted-#{@_ver}.tar.gz"
   source_sha256 '6c90715d254d7a7ec0208b29007b64160dd9fb7df4c4aa7f8ec2c9d23114c719'
-
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gparted-1.2.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-     aarch64: '8258baa778feb98e34f766ee8ce822d96eee1fc970313b7992874f0911dcf874',
-      armv7l: '8258baa778feb98e34f766ee8ce822d96eee1fc970313b7992874f0911dcf874',
-        i686: '0ec95233c92cf8abdfe830b959a45fd24aa8ba6f05b6776f2420e93af3a5e65c',
-      x86_64: '17f7af323b09b3b6de020b59ce2e28707e66514bc8505095c37ef9a792a53071',
-  })
 
   depends_on 'parted'
   depends_on 'gtkmm3'
@@ -29,10 +16,13 @@ class Gparted < Package
   depends_on 'yelp_tools' => ':build'
   depends_on 'xfsprogs'
   depends_on 'exfatprogs'
+  depends_on 'f2fs_tools'
   depends_on 'gpart'
   depends_on 'mtools'
   depends_on 'libsigcplusplus'
   depends_on 'librsvg'
+  depends_on 'xhost'
+  depends_on 'sommelier'
 
   def self.build
     system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
@@ -42,15 +32,16 @@ class Gparted < Package
       --enable-libparted-dmraid \
       --enable-xhost-root"
     system "make"
+    system "cat <<'EOF'> gparted_
+#!/bin/sh
+xhost si:localuser:root
+sudo -E LD_LIBRARY_PATH=#{CREW_LIB_PREFIX} gparted.elf
+EOF"
   end
 
   def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
-  end
-  
-  def self.postinstall
-    puts "To run gparted, execute the following:".orange
-    puts "xhost si:localuser:root".lightblue
-    puts "sudo gparted".lightblue
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/gparted", "#{CREW_DEST_PREFIX}/bin/gparted.elf"
+    FileUtils.install 'gparted_', "#{CREW_DEST_PREFIX}/bin/gparted", mode: 0755
   end
 end
