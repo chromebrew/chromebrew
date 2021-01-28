3eee21a0e1f3acc5f21b238579d125d8c9500aa3
satmandu <satadru@umich.edu>
2021-01-28 16:41:13 -0500

    Update libwpe.rb

--- packages/libwpe.rb
+++ packages/libwpe.rb
@@ -1,43 +1,37 @@
 require 'package'
 
 class Libwpe < Package
-  description 'General-purpose library specifically developed for the WPE-flavored port of WebKit.'
-  homepage 'https://github.com/WebPlatformForEmbedded/libwpe'
-  version '1.6.0'
+  description 'General-purpose library for WPE WebKit'
+  homepage 'https://wpewebkit.org'
+  @_ver = '1.8.0'
+  version @_ver
   compatibility 'all'
-  source_url 'https://github.com/WebPlatformForEmbedded/libwpe/releases/download/1.6.0/libwpe-1.6.0.tar.xz'
-  source_sha256 '3587c6b8a807f4bb76b268ba74ca82c6b395b90235db41ad8252224456193c90'
+  source_url "https://github.com/WebPlatformForEmbedded/libwpe/releases/download/#{@_ver}/libwpe-#{@_ver}.tar.xz"
+  source_sha256 'a6f00a7d091cbd4db57fe7ee3b4c12c6350921d654ed79812800a26c888481d2'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.6.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.6.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.6.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.6.0-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.8.0-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.8.0-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.8.0-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libwpe-1.8.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '83424c5f20ccc168be9bcf8dfdebb38486b6bac3984575f9c021b71c93feee10',
-     armv7l: '83424c5f20ccc168be9bcf8dfdebb38486b6bac3984575f9c021b71c93feee10',
-       i686: '597a643c854cc2a6fba391d47adacb4f9d1d77dd1445981c03aedc6818903756',
-     x86_64: '9c258f03e8f7861cbf990b6585687b7f3a150782854abccf1bc2768e14c26991',
+     aarch64: '2ac2dfad4b4ada47f50eb01de8e4f48629de8f4d88a7808173c0164f8cfc7978',
+      armv7l: '2ac2dfad4b4ada47f50eb01de8e4f48629de8f4d88a7808173c0164f8cfc7978',
+        i686: 'c6ea7fb4281d48faf071f6a701e372863046ebfca86d3c9bb644f405e2192bf7',
+      x86_64: 'fb76a33b0eaed0ebb0ed586ee019a12d6f5a77e2b3b1e99ccc3ecd9884e43b20',
   })
 
   depends_on 'libxkbcommon'
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system 'cmake',
-             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
-             "-DCMAKE_INSTALL_LIBDIR=#{ARCH_LIB}",
-             '-DCMAKE_BUILD_TYPE=Release',
-             '..'
-      system 'make'
-    end
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    builddir"
+    system "meson configure builddir"
+    system "ninja -C builddir"
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
