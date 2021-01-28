7d4003cc725d834628683a3084a4e13038b22900
satmandu <satadru@umich.edu>
2021-01-28 16:42:36 -0500

    Update wpebackend_fdo.rb

--- packages/wpebackend_fdo.rb
+++ packages/wpebackend_fdo.rb
@@ -1,44 +1,41 @@
 require 'package'
 
 class Wpebackend_fdo < Package
-  description 'General-purpose library specifically developed for the WPE-flavored port of WebKit.'
-  homepage 'https://github.com/Igalia/WPEBackend-fdo'
-  version '1.6.1'
+  description 'Freedesktop.org backend for WPE WebKit'
+  homepage 'https://wpewebkit.org'
+  @_ver = '1.8.0'
+  version @_ver
   compatibility 'all'
-  source_url 'https://github.com/Igalia/WPEBackend-fdo/releases/download/1.6.1/wpebackend-fdo-1.6.1.tar.xz'
-  source_sha256 '740eee3327acfb462b8460519a219e30dc0a870326e88e2ddc4fe2c8de20b1c9'
+  source_url "https://github.com/Igalia/WPEBackend-fdo/releases/download/#{@_ver}/wpebackend-fdo-#{@_ver}.tar.xz"
+  source_sha256 '9652a99c75fe1c6eab0585b6395f4e104b2427e4d1f42969f1f77df29920d253'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.6.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.6.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.6.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.6.1-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.8.0-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.8.0-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.8.0-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wpebackend_fdo-1.8.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'b4d88634ce83f63a2ff51952f0810de5b006b7040a963d45d3365b91a5fe6e24',
-     armv7l: 'b4d88634ce83f63a2ff51952f0810de5b006b7040a963d45d3365b91a5fe6e24',
-       i686: '4b2fac0a52908ab4024a4c9c2b9bce413a86dcee20f9a7ef1f8fe858e4558f69',
-     x86_64: '4793cf26d8d027dfb038d8e6ec16280e962c70f7472caebab690b85960d71eba',
+     aarch64: '2eb8dbb4be7ec82b3b48c5396759f447374b206ef3d498d49899a86b066b60d0',
+      armv7l: '2eb8dbb4be7ec82b3b48c5396759f447374b206ef3d498d49899a86b066b60d0',
+        i686: '77e5834339fe892f5f4eca9f642b28a5e7f72e5c1ecaafff8b5acea570543e90',
+      x86_64: 'a6ebf24575dc975a1615508cd259542ea3379529dae274ae800e64b5166cf5f6',
   })
 
   depends_on 'libwpe'
   depends_on 'wayland'
+  depends_on 'libepoxy'
+  depends_on 'mesa' => ':build'
+  depends_on 'wayland_protocols' => ':build'
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system 'cmake',
-             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
-             "-DCMAKE_INSTALL_LIBDIR=#{ARCH_LIB}",
-             '-DCMAKE_BUILD_TYPE=Release',
-             '..'
-      system 'make'
-    end
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    builddir"
+    system "meson configure builddir"
+    system "ninja -C builddir"
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
