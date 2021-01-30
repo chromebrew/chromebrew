a79b73ad0f38cc8fcf06789554c1dba760a1ab61
satmandu <satadru@umich.edu>
2021-01-30 12:22:32 -0500

    rebuild w/ openssl

--- packages/qtbase.rb
+++ packages/qtbase.rb
@@ -3,26 +3,27 @@ require 'package'
 class Qtbase < Package
   description 'Qt Base (Core, Gui, Widgets, Network, ...)'
   homepage 'https://code.qt.io/cgit/qt/qtbase'
-  version '5.15.2'
+  @_ver = '5.15.2'
+  version @_ver + '-1'
   compatibility 'all'
   source_url 'file:///dev/null'
   source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qtbase-5.15.2-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'b528840c58650096eccde40d60b8d3283badbd14ecd251969512e6158611147b',
-     armv7l: 'b528840c58650096eccde40d60b8d3283badbd14ecd251969512e6158611147b',
-       i686: '4054ed51b94ad9a9ec21cd89f0ee147aaecff572c6c2218bf8018ae2be7b4de1',
-     x86_64: '6f01b89c997bd5649a46103f6208fb521e3cf60ea8643adc66acbf6d758e97a0',
+     aarch64: 'a3e863cc19101742af4d808cb552b954d0c8581febd8015865087ea36da0fa56',
+      armv7l: 'a3e863cc19101742af4d808cb552b954d0c8581febd8015865087ea36da0fa56',
+        i686: '34eedd58aac9f5a803af9e2641a645badbf6b96e501eae1aa495a271bb53037a',
+      x86_64: '0d26af5842faa70ad3f1c7aa822c8e4f7c4c6f42535f1f24042f1f6e1e73df4d',
   })
 
   depends_on 'alsa_plugins'
-  depends_on 'cups'
+  depends_on 'cups'a
   depends_on 'ffmpeg'
   depends_on 'fontconfig'
   depends_on 'freetds'
@@ -38,7 +39,7 @@ class Qtbase < Package
   depends_on 'protobuf'
 
   def self.build
-    system "git clone --branch v#{version} --depth 1 git://code.qt.io/qt/qtbase.git"
+    system "git clone --branch v#{@_ver} --depth 1 git://code.qt.io/qt/qtbase.git"
     Dir.chdir 'qtbase' do
       system './configure',
         "--prefix=#{CREW_PREFIX}/share/Qt-5",
@@ -54,8 +55,7 @@ class Qtbase < Package
         '-system-zlib',
         '-system-libpng',
         '-system-libjpeg',
-        '-system-freetype',
-        '-no-openssl'
+        '-system-freetype'
       system 'make'
     end
   end
