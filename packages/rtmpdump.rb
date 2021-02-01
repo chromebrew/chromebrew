a4beccdd5f960a4c8520f845d14d98d11850f23d
satmandu <satadru@umich.edu>
2021-02-01 01:13:53 -0500

    rebuilt w/o LibreSSL

--- packages/rtmpdump.rb
+++ packages/rtmpdump.rb
@@ -3,28 +3,33 @@ require 'package'
 class Rtmpdump < Package
   description 'rtmpdump is a toolkit for RTMP streams.'
   homepage 'https://rtmpdump.mplayerhq.hu/'
-  version 'c5f04a58f'
+  version 'c5f04a58f-1'
   compatibility 'all'
   source_url 'https://git.ffmpeg.org/gitweb/rtmpdump.git/snapshot/c5f04a58fc2aeea6296ca7c44ee4734c18401aa3.tar.gz'
   source_sha256 'fd8c21263d93fbde8bee8aa6c5f6a657789674bb0f9e74f050651504d5f43b46'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/rtmpdump-c5f04a58f-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '9e8f50f34f570d8bd6fb161fd96dc4c3384259e07eeebf03cc975b4eb9d21306',
-     armv7l: '9e8f50f34f570d8bd6fb161fd96dc4c3384259e07eeebf03cc975b4eb9d21306',
-       i686: '3f28349cce85d7cc73208f7bd921543baea29dd1994e068ecf3597119b761a18',
-     x86_64: '015f35c6556f9fa4100093e0cb44581ad0725a25391b301ce8972e5a090c8cec',
+     aarch64: '47e67bbdb93c72136630dfc35c37ddd7d37c242fbd3446f67ff40b52376fa3e6',
+      armv7l: '47e67bbdb93c72136630dfc35c37ddd7d37c242fbd3446f67ff40b52376fa3e6',
+        i686: '109fb6fa70409f8ba274fa452c2e04dc1ff3ba740f618525c188139c1ddce363',
+      x86_64: '0212408c6faad92b5909d77188d8c27e68ce955f61dcb5597603303e31e601ce',
   })
-
-  def self.build
+  
+  def self.patch
     system "sed -i 's,prefix=/usr/local,prefix=#{CREW_PREFIX},' Makefile"
     system "sed -i 's,prefix=/usr/local,prefix=#{CREW_PREFIX},' librtmp/Makefile"
     system "sed -i 's,libdir=\$(prefix)/lib,libdir=#{CREW_LIB_PREFIX},' librtmp/Makefile"
+    # OpenSSL builds are broken.
+    system "sed -e 's/^CRYPTO=OPENSSL/#CRYPTO=OPENSSL/' -e 's/#CRYPTO=GNUTLS/CRYPTO=GNUTLS/' -i Makefile -i librtmp/Makefile"
+  end
+ 
+  def self.build
     system 'make'
   end
 
