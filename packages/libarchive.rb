6a0b3a27a60f480a51eb0623b0f02758691531f7
satmandu <satadru@umich.edu>
2021-02-03 20:21:15 -0500

    Packages rebuilt not to use libiconv (#5080)
    * libpsl -> 0.21.1
    
    * removed libiconv dep
    
    * libical -> 3.0.9
    
    * rebuild w/o libiconv
    
    * Update git.rb
    
    * rebuild w/o libiconv, using flto
    
    * rebuild w/o libiconv

--- packages/libarchive.rb
+++ packages/libarchive.rb
@@ -4,29 +4,32 @@ class Libarchive < Package
   description 'Multi-format archive and compression library.'
   homepage 'https://www.libarchive.org/'
   @_ver = '3.5.1'
-  version @_ver
+  version "#{@_ver}-1"
   compatibility 'all'
   source_url "https://www.libarchive.org/downloads/libarchive-#{@_ver}.tar.xz"
   source_sha256 '0e17d3a8d0b206018693b27f08029b598f6ef03600c2b5d10c94ce58692e299b'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libarchive-3.5.1-1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: '1f88febf96c404466b7e86849b291ecd8e3086021b6fd3fb6b4887f3935e24d8',
-      armv7l: '1f88febf96c404466b7e86849b291ecd8e3086021b6fd3fb6b4887f3935e24d8',
-        i686: '457630efbf3cb6af9a88cd3229407e416ec478b78c176bd3735c40f1edbfd192',
-      x86_64: 'f70e487934dfd505812e01ae2d4cf2c7fc7077cab847240b9f0d840f7d360f55',
+  binary_sha256({
+    aarch64: 'd58404d09d291373169e80b4012ffb5c51f30760503769808b7a9e98111cfb68',
+     armv7l: 'd58404d09d291373169e80b4012ffb5c51f30760503769808b7a9e98111cfb68',
+       i686: 'b7634129d40fe7beed9185ce73ed78730c8255485d59d4316d67267a0b56c206',
+     x86_64: '53a2d351d07063157a3ed33a8cf711af401b7adee4c72f4fa5772637e4823fa3'
   })
 
   depends_on 'lz4'
   depends_on 'xzutils' => :build
 
   def self.build
+    raise StandardError, 'Please remove libiconv before building.' if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
+
     system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
+      LDFLAGS='-flto=auto' \
       ./configure #{CREW_OPTIONS}"
     system 'make'
   end
--- packages/libical.rb
+++ packages/libical.rb
@@ -2,22 +2,22 @@ require 'package'
 
 class Libical < Package
   description 'An open source reference implementation of the icalendar data type and serialization format'
-  version '3.0.8.99' # 3.0.8 doesn't compile.
+  version '3.0.9'
   compatibility 'all'
-  source_url 'https://github.com/libical/libical/archive/07dc84fb0d5e50fb493005e10fa41930111da48f.zip'
-  source_sha256 '5fdf39b0ea654589bf2e9aa88ef1b0206c18aa6987f5a4299db3748e8b1eefd6'
+  source_url 'https://github.com/libical/libical/releases/download/v3.0.9/libical-3.0.9.tar.gz'
+  source_sha256 'bd26d98b7fcb2eb0cd5461747bbb02024ebe38e293ca53a7dfdcb2505265a728'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.8.99-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.8.99-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.8.99-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.8.99-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.9-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.9-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.9-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libical-3.0.9-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'ff814e2b53f6c707a029f74895c90cde0b4bf6b8156dfbc03ecb9f49d34dc919',
-     armv7l: 'ff814e2b53f6c707a029f74895c90cde0b4bf6b8156dfbc03ecb9f49d34dc919',
-       i686: '1a9abe4217cf37eefd0e09ced2203651dd283157072efb850b7bdcb69ebdee39',
-     x86_64: 'f21f8c0273497d6906a087cfad00d0ab51f588121d0f0564a391364caabc9781',
+  binary_sha256({
+    aarch64: '7db2461fee6be357f39abd270cfad5f20aa2e0d40f65a3310f1d2cc755c50e5e',
+     armv7l: '7db2461fee6be357f39abd270cfad5f20aa2e0d40f65a3310f1d2cc755c50e5e',
+       i686: '15749b91f620d4f108c6c73ce3a23075621a13728348d401ae499b1db047b747',
+     x86_64: 'd3ebd14fbd5b34f1c0347dfb21e6b278a6a7131fb9367a1be0e53d23cac755ac'
   })
 
   depends_on 'glib'
@@ -27,18 +27,20 @@ class Libical < Package
   depends_on 'gobject_introspection' => ':build'
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system "cmake #{CREW_CMAKE_OPTIONS} .. -G Ninja \
-        -DGOBJECT_INTROSPECTION=true \
-        -DICAL_GLIB_VAPI=true \
-        -DICAL_BUILD_DOCS=false \
-        -DLIBICAL_BUILD_TESTING=false"
+    Dir.mkdir 'builddir'
+    Dir.chdir 'builddir' do
+      system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
+      LDFLAGS='-flto=auto' \
+      cmake #{CREW_CMAKE_OPTIONS} .. -G Ninja \
+      -DGOBJECT_INTROSPECTION=true \
+      -DICAL_GLIB_VAPI=true \
+      -DICAL_BUILD_DOCS=false \
+      -DLIBICAL_BUILD_TESTING=false"
     end
-    system "ninja -C build"
+    system 'ninja -C builddir'
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C build install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
