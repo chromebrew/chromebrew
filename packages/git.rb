f8ba658c818cf11e7048eb1369cd78c64db47210
satmandu <satadru@umich.edu>
2021-02-03 20:21:15 -0500

    Packages rebuilt not to use libiconv (#5080)
    * libpsl -> 0.21.1
    
    * removed libiconv dep
    
    * libical -> 3.0.9
    
    * rebuild w/o libiconv
    
    * Update git.rb
    
    * rebuild w/o libiconv, using flto
    
    * rebuild w/o libiconv

--- packages/git.rb
+++ packages/git.rb
@@ -4,33 +4,37 @@ class Git < Package
   description 'Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.'
   homepage 'https://git-scm.com/'
   @_ver = '2.30.0'
-  version @_ver + '-1'
+  version "#{@_ver}-2"
   compatibility 'all'
   source_url "https://github.com/git/git/archive/v#{@_ver}.tar.gz"
   source_sha256 '8db4edd1a0a74ebf4b78aed3f9e25c8f2a7db3c00b1aaee94d1e9834fae24e61'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-1-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-1-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/git-2.30.0-2-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: '4386bc951eaa7311b8c828fb76b030410f63686c9c86f6288b2a1025594166f1',
-      armv7l: '4386bc951eaa7311b8c828fb76b030410f63686c9c86f6288b2a1025594166f1',
-        i686: '6dd0fc056dd3cdca76596603c1a161724e8f81d68c50bf18f25877e4df8dc9e4',
-      x86_64: 'ae2a5001e272fdf4a1f212ed31f38b62e2fd20205f8ab0c77ab7226b8a94b6ac',
+  binary_sha256({
+    aarch64: 'a11bad34ddd17924b226026eccabf449381cf6b069715b914ab7bd66b3190a36',
+     armv7l: 'a11bad34ddd17924b226026eccabf449381cf6b069715b914ab7bd66b3190a36',
+       i686: 'd09ad3f842c1fefba128e28252d383b69bc890627e7759abeb1d75b624bf4d84',
+     x86_64: '3a63be93d3bbc46811a7f8b81a8e4b08c03ff82facf36419030ac214b60e4e6b'
   })
 
   depends_on 'curl' => :build
   depends_on 'python3' => :build
 
   def self.build
+    raise StandardError, 'Please remove libiconv before building.' if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
+
     system 'autoreconf -i'
     system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' \
+    LDFLAGS='-flto=auto' \
     ./configure \
+    #{CREW_OPTIONS} \
     --with-openssl=#{CREW_PREFIX}/etc/ssl \
-    --without-tcltk #{CREW_OPTIONS} \
+    --without-tcltk \
     --with-perl=#{CREW_PREFIX}/bin/perl \
     --with-python=#{CREW_PREFIX}/bin/python3 \
     --with-gitconfig=#{CREW_PREFIX}/etc/gitconfig \
@@ -46,15 +50,15 @@ class Git < Package
 
   def self.postinstall
     puts
-    puts "Git completion support is available for the following shells:".lightblue
+    puts 'Git completion support is available for the following shells:'.lightblue
     system "ls #{CREW_PREFIX}/share/git-completion"
     puts
-    puts "To add git completion for bash, execute the following:".lightblue
+    puts 'To add git completion for bash, execute the following:'.lightblue
     puts "echo '# git completion' >> ~/.bashrc".lightblue
     puts "echo 'if [ -f #{CREW_PREFIX}/share/git-completion/git-completion.bash ]; then' >> ~/.bashrc".lightblue
     puts "echo '  source #{CREW_PREFIX}/share/git-completion/git-completion.bash' >> ~/.bashrc".lightblue
     puts "echo 'fi' >> ~/.bashrc".lightblue
-    puts "source ~/.bashrc".lightblue
+    puts 'source ~/.bashrc'.lightblue
     puts
   end
 end
--- packages/libpsl.rb
+++ packages/libpsl.rb
@@ -3,30 +3,34 @@ require 'package'
 class Libpsl < Package
   description 'C library for the Public Suffix List'
   homepage 'https://github.com/rockdaboot/libpsl'
-  version '0.20.2'
+  @_ver = '0.21.1'
+  version @_ver
   compatibility 'all'
-  source_url 'https://github.com/rockdaboot/libpsl/releases/download/libpsl-0.20.2/libpsl-0.20.2.tar.gz'
-  source_sha256 'f8fd0aeb66252dfcc638f14d9be1e2362fdaf2ca86bde0444ff4d5cc961b560f'
+  source_url "https://github.com/rockdaboot/libpsl/releases/download/#{@_ver}/libpsl-#{@_ver}.tar.lz"
+  source_sha256 '644375d557bb3b84c485df2dae98ee388fe1e11fb75230004e4b8623b3b833a9'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.20.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.20.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.20.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.20.2-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.21.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.21.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.21.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libpsl-0.21.1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'f6dab3749e5e195e417775c03870738aef36454f5124e59708c70a006b417b9c',
-     armv7l: 'f6dab3749e5e195e417775c03870738aef36454f5124e59708c70a006b417b9c',
-       i686: '51e81940423ac5bf3de3a931f697d1b0962a74be326f7cf0685f0328d97b7dab',
-     x86_64: 'f7f422c26f3d5b3cf690a6f9db6245f32ec7700d1db8e2b33eac212ca00c422e',
+  binary_sha256({
+    aarch64: '9b8376e9c1b5e65363e0c4a7ad61721930e07dbaa032c76455723186d6268d47',
+     armv7l: '9b8376e9c1b5e65363e0c4a7ad61721930e07dbaa032c76455723186d6268d47',
+       i686: 'c5d298373d73a26ecd8559254f00286f14e129a09dbe13f94252356a332fc172',
+     x86_64: 'e8c7a5c5f46be4ff1158d5d643a3efa14fcf008f4f6af906703612b907c9e725'
   })
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    -Ddocs=disabled \
+    builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
