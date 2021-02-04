ed82d39f862b6176ffa4c3a1a0b3fff17f94b2df
satmandu <satadru@umich.edu>
2021-02-03 20:15:46 -0500

    rebuild w/o libiconv

--- packages/gettext.rb
+++ packages/gettext.rb
@@ -3,22 +3,23 @@ require 'package'
 class Gettext < Package
   description 'GNU gettext utilities are a set of tools that provides a framework to help other GNU packages produce multi-lingual messages.'
   homepage 'https://www.gnu.org/software/gettext/'
-  version '0.21'
+  @_ver = '0.21'
+  version "#{@ver}-1"
   compatibility 'all'
-  source_url 'https://gnu.freemirror.org/gnu/gettext/gettext-0.21.tar.lz'
+  source_url "https://gnu.freemirror.org/gnu/gettext/gettext-#{@_ver}.tar.lz"
   source_sha256 '435b546e3880ab767c967c0731b20629a0cb0ba620e6bac2f590f401c10ebb45'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gettext-0.21-1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '6489c458f5f3ad46d60415fd5097bac4ed5d1c739500b2ca90262a75cb05b17e',
-     armv7l: '6489c458f5f3ad46d60415fd5097bac4ed5d1c739500b2ca90262a75cb05b17e',
-       i686: '178c97cca996dbb982deb8ab6ce7faea9c1ae4795e8f716a1a92565b5c7b52c6',
-     x86_64: 'c51bf8877623c0c797b6a3eaf36a60693a45a044371601d065f8e904f8eadd8c',
+  binary_sha256({
+    aarch64: '57ae562920cf6caefb4e6e764a171a8a78f18e5767668e42e4722cc63612cfa7',
+     armv7l: '57ae562920cf6caefb4e6e764a171a8a78f18e5767668e42e4722cc63612cfa7',
+       i686: '520172814ee0312877b6a0fb0d5d9f4e22473cf5bb9272facf93bf4d5597d1c0',
+     x86_64: 'de490d13276f5b9a48e007b57c08c64aac21730b1091be453c1373a0e1c26c52'
   })
 
   depends_on 'ncurses'
@@ -27,12 +28,18 @@ class Gettext < Package
   depends_on 'jdk8' => :build
 
   def self.build
-    system "./configure #{CREW_OPTIONS} --disable-static --enable-shared --with-pic"
+    raise StandardError, 'Please remove libiconv before building.' if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
+
+    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto'  LDFLAGS='-flto=auto'\
+    ./configure #{CREW_OPTIONS} \
+    --disable-static \
+    --enable-shared \
+    --with-pic"
     system 'make'
   end
 
   def self.check
-    #system 'make', 'check'
+    # system 'make', 'check'
   end
 
   def self.install
