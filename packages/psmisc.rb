39c322021a483ac8b346f8b86b5888c37ba4bf7a
satmandu <satadru@umich.edu>
2021-02-03 19:36:44 -0500

    rubocop changes

--- packages/psmisc.rb
+++ packages/psmisc.rb
@@ -12,23 +12,24 @@ class Psmisc < Package
   source_sha256 'fe530b0a29902f8660481248fc19f6994927282b4fe0cd992121016144b95fa6'
 
   binary_url({
-               aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-armv7l.tar.xz',
-               armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-armv7l.tar.xz',
-               i686: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-i686.tar.xz',
-               x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-x86_64.tar.xz'
-             })
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/psmisc-23.3-chromeos-x86_64.tar.xz'
+  })
   binary_sha256({
-                  aarch64: 'd951db8224299af61b2ca8fb78d64f93667f52df0901928927d8c743216a629f',
-                  armv7l: 'd951db8224299af61b2ca8fb78d64f93667f52df0901928927d8c743216a629f',
-                  i686: '00181538fdcdb8922a07ff68f046a1d5333866f76bb241ae628d369179a931c2',
-                  x86_64: '61880a1987c8295f18c44c8472a953179510318082ed92ac1ade306dbda96bd1'
-                })
+    aarch64: 'd951db8224299af61b2ca8fb78d64f93667f52df0901928927d8c743216a629f',
+     armv7l: 'd951db8224299af61b2ca8fb78d64f93667f52df0901928927d8c743216a629f',
+       i686: '00181538fdcdb8922a07ff68f046a1d5333866f76bb241ae628d369179a931c2',
+     x86_64: '61880a1987c8295f18c44c8472a953179510318082ed92ac1ade306dbda96bd1'
+  })
 
   def self.build
     system './autogen.sh'
     system './configure --help'
     system "env CFLAGS='-pipe -flto=auto -I#{CREW_PREFIX}/include/ncurses -fno-stack-protector' \
-      CXXFLAGS='-pipe -flto=auto' LDFLAGS='-flto=auto -I#{CREW_PREFIX}/include/ncurses' \
+      CXXFLAGS='-pipe -flto=auto' \
+      LDFLAGS='-flto=auto -I#{CREW_PREFIX}/include/ncurses' \
       ./configure #{CREW_OPTIONS}"
     system 'make'
   end
