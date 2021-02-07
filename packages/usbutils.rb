06595398621dda60a339fdbfae56c388d8dfe2ca
satmandu <satadru@umich.edu>
2021-02-06 19:48:46 -0500

    usbutils -> 013 (#5085)
    * usbutils -> 013
    
    * use #{CREW_PREFIX}

--- packages/usbutils.rb
+++ packages/usbutils.rb
@@ -3,31 +3,31 @@ require 'package'
 class Usbutils < Package
   description 'Tools for examining usb devices'
   homepage 'http://linux-usb.sourceforge.net/'
-  version '012'
+  @_ver = '013'
+  version @_ver
   compatibility 'all'
-  source_url 'https://www.kernel.org/pub/linux/utils/usb/usbutils/usbutils-012.tar.xz'
-  source_sha256 '88634625f91840bc1993d2731cc081ee8d3b13d56069a95bdd6ac6ef0e063e46'
+  source_url "https://mirrors.kernel.org/pub/linux/utils/usb/usbutils/usbutils-#{@_ver}.tar.xz"
+  source_sha256 '9e23494fcc78b7a80ee29a07dd179c95ae2f71509c35728dbbabc2d1cca41338'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-012-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-012-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-012-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-012-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-013-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-013-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-013-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/usbutils-013-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '84309375b4d635563a2bdc47e55c5a95841d18a937fcb3af646b51f5e1af5b53',
-     armv7l: '84309375b4d635563a2bdc47e55c5a95841d18a937fcb3af646b51f5e1af5b53',
-       i686: 'a174fbc8d94512af2591505a790226ec4230ea2236afe06f1ed119a33982b7c9',
-     x86_64: '483f60e4d8dd5f44a2fabe7840e560c1d5eb67ae4eddca08f209dc8b30b9d4bc',
+  binary_sha256({
+    aarch64: '07502628efc250a37a113baaf9d24c23f524ee3f7a9a5f5693a4454f3259d4e5',
+     armv7l: '07502628efc250a37a113baaf9d24c23f524ee3f7a9a5f5693a4454f3259d4e5',
+       i686: 'ea0051d91206aab125e7cdd7c142123fdd0b43c06f9438dc10023060f8463ad4',
+     x86_64: '09ebfe03787624a97ccf79e1e746ad3177b4ca4d991fa8ed2aa5de8227349d76'
   })
 
   depends_on 'libusb'
 
   def self.build
     system './autogen.sh'
-    system "./configure \
-      --prefix=#{CREW_PREFIX} \
-      --libdir=#{CREW_LIB_PREFIX}"
+    system "env CFLAGS='-flto=auto -ltinfo' CXXFLAGS='-flto=auto' LDFLAGS='-flto=auto' \
+    ./configure #{CREW_OPTIONS}"
   end
 
   def self.install
@@ -40,8 +40,8 @@ class Usbutils < Package
     puts "It's recommended that you setup a cron job to update this file regularly.".lightblue
     puts 'You can install a cron package by executing `crew install cronie`'.lightblue
     puts
-    puts 'Add a cron job with something like the following:' .lightblue
+    puts 'Add a cron job with something like the following:'.lightblue
     puts '# Update usb.ids at 6pm daily.'.lightblue
-    puts '0 18 * * * /usr/local/bin/crew postinstall usbutils >/dev/null 2>&1'.lightblue
+    puts "0 18 * * * #{CREW_PREFIX}/bin/crew postinstall usbutils >/dev/null 2>&1".lightblue
   end
 end
