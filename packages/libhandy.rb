58e21a31d2bee7c526c32b5ce0ee17bb39108501
satmandu <satadru@umich.edu>
2021-02-08 14:45:26 -0500

    libhandy -> 1.0.3 (#5094)

--- packages/libhandy.rb
+++ packages/libhandy.rb
@@ -3,24 +3,24 @@ require 'package'
 class Libhandy < Package
   description 'The aim of the handy library is to help with developing UI for mobile devices using GTK/GNOME.'
   homepage 'https://gitlab.gnome.org/GNOME/libhandy/'
-  version '1.0.2'
+  @_ver = '1.0.3'
+  version @_ver
   compatibility 'all'
-  source_url 'https://gitlab.gnome.org/GNOME/libhandy/-/archive/1.0.2/libhandy-1.0.2.tar.bz2'
-  source_sha256 '5b1ceebf80a82bb56804370d88481b8a4affe5d33f13a7fd06905135cceaaf88'
+  source_url "https://gitlab.gnome.org/GNOME/libhandy/-/archive/#{@_ver}/libhandy-#{@_ver}.tar.bz2"
+  source_sha256 '8cdeb88deaf13efbc0da73306bf8f8d14b6e5b108807a4f7b43d6a7c6e3a158d'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.2-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.3-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.3-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.3-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libhandy-1.0.3-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'ccb7edee4b44c0e0d466722194e2f5b331e5893c2442f27ccbbe86cf0eea004a',
-     armv7l: 'ccb7edee4b44c0e0d466722194e2f5b331e5893c2442f27ccbbe86cf0eea004a',
-       i686: 'ab4a4302ff0ad48c3edaedfe37cc930389dc3984ba9ab74de1e79636d52bc9c7',
-     x86_64: 'c865b663caf4b2ae0da32a59f0a719711ed5f4af9d231e5317f921c4cdeb5693',
+     aarch64: '4e5c2b68cdf5884bf233086a4cb8258c39d7c4f3abc84e262b9dc485292eff1a',
+      armv7l: '4e5c2b68cdf5884bf233086a4cb8258c39d7c4f3abc84e262b9dc485292eff1a',
+        i686: 'fabc2b197a1b8c6c1560a0ba3dd4f6a1839715e38f7079a11bd6169dbf54f38b',
+      x86_64: 'b2b09e3a0ded79ce0ab151c489d95549c3b8c508ee485d8fdfdb4f4e0edcfb29',
   })
-
 
   depends_on 'vala'
 
@@ -29,11 +29,12 @@ class Libhandy < Package
   end
 
   def self.build
-    system "meson #{CREW_MESON_OPTIONS} build"
-    system 'ninja -v -C build'
+    system "meson #{CREW_MESON_LTO_OPTIONS} builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C build install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
