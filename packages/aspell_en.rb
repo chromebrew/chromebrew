93759f3d21d1a4f8af12c21fc8ebabf3b46a5253
satmandu <satadru@umich.edu>
2021-02-08 16:56:11 -0500

    Spelling package updates (#5098)
    * aspell_en -> 2020.12.07-0
    
    * enchant -> 2.2.15

--- packages/aspell_en.rb
+++ packages/aspell_en.rb
@@ -3,32 +3,33 @@ require 'package'
 class Aspell_en < Package
   description 'English Aspell Dictionary'
   homepage 'https://ftpmirror.gnu.org/aspell/dict/0index.html'
-  version '2019.10.06-0'
+  version '2020.12.07-0'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/aspell/dict/en/aspell6-en-2019.10.06-0.tar.bz2'
-  source_sha256 '24334b4daac6890a679084f4089e1ce7edbe33c442ace776fa693d8e334f51fd'
+  source_url 'https://gnu.askapache.com/aspell/dict/en/aspell6-en-2020.12.07-0.tar.bz2'
+  source_sha256 '4c8f734a28a088b88bb6481fcf972d0b2c3dc8da944f7673283ce487eac49fb3'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2019.10.06-0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2019.10.06-0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2019.10.06-0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2019.10.06-0-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2020.12.07-0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2020.12.07-0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2020.12.07-0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/aspell_en-2020.12.07-0-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'a738f11f46ba13405bd69ed228494415df6dd43c82639df3dee97a923bf6f43a',
-     armv7l: 'a738f11f46ba13405bd69ed228494415df6dd43c82639df3dee97a923bf6f43a',
-       i686: 'f0d6533f73c87c5a6dc8cd0c5e2db3cee262f208c10ec4aa7bb9af1f7b78a2ad',
-     x86_64: 'd346224eada4650b7bacc1146e3664800f2728287e184b16a6c95f46ba2cf805',
+  binary_sha256({
+    aarch64: 'a3f897c049eb4c7b6caf9c1d08c894b6f9541681a0ce9cd2b664226a1574d69d',
+     armv7l: 'a3f897c049eb4c7b6caf9c1d08c894b6f9541681a0ce9cd2b664226a1574d69d',
+       i686: 'ec4684e5665cf4ef98c90ac28567e2d5a6833c9468263486eb81e3fd2d1e27ca',
+     x86_64: '2b39dd619b454a9fcf4bad9b09b4a60163744209dc9dbad4c87c0971bca363f1'
   })
 
   depends_on 'aspell'
 
   def self.build
-    system './configure'
+    system "env CFLAGS='-flto=auto -ltinfo' CXXFLAGS='-flto=auto' \
+        LDFLAGS='-flto=auto' ./configure"
     system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
--- packages/enchant.rb
+++ packages/enchant.rb
@@ -3,22 +3,23 @@ require 'package'
 class Enchant < Package
   description 'Enchant is a library (and command-line program) that wraps a number of different spelling libraries and programs with a consistent interface.'
   homepage 'https://abiword.github.io/enchant/'
-  version '2.2.13'
+  @_ver = '2.2.15'
+  version @_ver
   compatibility 'all'
-  source_url 'https://github.com/AbiWord/enchant/archive/v2.2.13.tar.gz'
-  source_sha256 '783d2e847ee0a49b0552f1c5584a1c93b8ba7e93d4bd95887fc4644fb10b8563'
+  source_url "https://github.com/AbiWord/enchant/archive/v#{@_ver}.tar.gz"
+  source_sha256 '85295934102a4ab94f209cbc7c956affcb2834e7a5fb2101e2db436365e2922d'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.13-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.13-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.13-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.13-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.15-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.15-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.15-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/enchant-2.2.15-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '78082be8cb3c5f1f6c37a80d1f1cfdc634db65b01f14199aeee7fe7702536a81',
-     armv7l: '78082be8cb3c5f1f6c37a80d1f1cfdc634db65b01f14199aeee7fe7702536a81',
-       i686: 'e3984a9b8462d07b3c811650c7fac072936172d7a695099db3162bff664698da',
-     x86_64: 'eb517cf71ab7e0d54cae2027ffc59c4ece70427d2e7764fc60f45c3cb8c9b02f',
+  binary_sha256({
+    aarch64: '1d13b3581ec9223b127f91b29bd5838b44ed61cfee4eb6d2d58a1e3f945f0a14',
+     armv7l: '1d13b3581ec9223b127f91b29bd5838b44ed61cfee4eb6d2d58a1e3f945f0a14',
+       i686: 'cb657dc849d0133429a5fa8d2de803f15b098e1d05a865d538f6917c10a3234a',
+     x86_64: '975d538cf6b7faf44237230c1e61c9ad51e344bded85cca9527ad4c148bccc0c'
   })
 
   depends_on 'aspell_en'
@@ -27,7 +28,11 @@ class Enchant < Package
 
   def self.build
     system './bootstrap'
-    system "./configure #{CREW_OPTIONS} --with-hunspell --with-aspell"
+    system "env CFLAGS='-flto=auto -ltinfo' CXXFLAGS='-flto=auto' \
+        LDFLAGS='-flto=auto' \
+        ./configure #{CREW_OPTIONS} \
+        --with-hunspell \
+        --with-aspell"
     system 'make'
   end
 
