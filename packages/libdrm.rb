527e9a5fbf855a7a6561c1eecc2af2b6dfeaf105
satmandu <satadru@umich.edu>
2021-02-08 14:44:53 -0500

    libdrm -> 2.4.104 (#5095)

--- packages/libdrm.rb
+++ packages/libdrm.rb
@@ -3,22 +3,23 @@ require 'package'
 class Libdrm < Package
   description 'Cross-driver middleware for DRI protocol.'
   homepage 'https://dri.freedesktop.org'
-  version '2.4.102'
+  @_ver = '2.4.104'
+  version @_ver
   compatibility 'all'
-  source_url 'https://dri.freedesktop.org/libdrm/libdrm-2.4.102.tar.xz'
-  source_sha256 '8bcbf9336c28e393d76c1f16d7e79e394a7fce8a2e929d52d3ad7ad8525ba05b'
+  source_url "https://dri.freedesktop.org/libdrm/libdrm-#{@_ver}.tar.xz"
+  source_sha256 'd66ad8b5c2441015ac1333e40137bb803c3bde3612ff040286fcc12158ea1bcb'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.102-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.102-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.102-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.102-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.104-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.104-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.104-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libdrm-2.4.104-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'd02b90125434ef5bd9f1bd86514929edc09a08089792cafdfaf2b678d3dc2ed5',
-     armv7l: 'd02b90125434ef5bd9f1bd86514929edc09a08089792cafdfaf2b678d3dc2ed5',
-       i686: '9d0cd25cdc0a2193ef30b1549d6d2979c94dd941a95c7a07f5ed84413da89cbe',
-     x86_64: '5f67dd18f9bf9653addbf2989b9179fa9c3d55fe855a3b1681243239a32987c4',
+     aarch64: 'd92b2cf767f17517a2e90e610749b38d878f126f237bafa027c5f019bd26ab78',
+      armv7l: 'd92b2cf767f17517a2e90e610749b38d878f126f237bafa027c5f019bd26ab78',
+        i686: 'ffe58ea6bed51afe09957f581c1490643090b8d6b79c6ae0467d0e7d682afa94',
+      x86_64: '74ebadab71db82425c8a823e03f9da0f8f44eb692e448c83f8b7d12b69bb76b6',
   })
 
   depends_on 'libpciaccess'
@@ -27,27 +28,25 @@ class Libdrm < Package
   depends_on 'libxslt'
 
   def self.build
-    system "meson", "setup",
-           "-Dfreedreno-kgsl=true",
-           "-Damdgpu=true",
-           "-Dradeon=true",
-           "-Dnouveau=true",
-           "-Dintel=true",
-           "-Dvmwgfx=true",
-           "-Dvc4=true",
-           "-Dfreedreno=true",
-           "-Detnaviv=true",
-           "-Dlibkms=true",
-           "-Dexynos=true",
-           "-Dudev=true",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           "--prefix=#{CREW_PREFIX}",
-           "builddir/"
-
-    system "ninja -C builddir/"
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+      -Dfreedreno-kgsl=true \
+      -Damdgpu=true \
+      -Dradeon=true \
+      -Dnouveau=true \
+      -Dintel=true \
+      -Dvmwgfx=true \
+      -Dvc4=true \
+      -Dfreedreno=true \
+      -Detnaviv=true \
+      -Dlibkms=true \
+      -Dexynos=true \
+      -Dudev=true \
+      builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
   end
 
   def self.install
-    system "cd builddir && DESTDIR=#{CREW_DEST_DIR} meson install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
