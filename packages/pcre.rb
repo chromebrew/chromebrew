d0310ee491297c65988bdafcc4a71f74486df1d8
Kevin Perkins <kjperkin@users.noreply.github.com>
2021-02-09 12:26:47 -0800

    [New Package] SWI-Prolog (#5102)
    * [readline]: Link against ncurses, build with multibyte support (Fixes #5099, #5101)
    
    * [pcre]: Build with --enable-unicode-properties (Fixes #5100)
    
    * [New Package] SWI-prolog (stable) v8.2.4
    
    * Updated with binaries
    
    * Updated with binaries
    
    * Binaries...
    
    * fix compatibility
    
    * adjust depends
    
    Co-authored-by: satmandu <satadru@gmail.com>

--- packages/pcre.rb
+++ packages/pcre.rb
@@ -3,39 +3,43 @@ require 'package'
 class Pcre < Package
   description 'The PCRE library is a set of functions that implement regular expression pattern matching using the same syntax and semantics as Perl 5.'
   homepage 'http://pcre.org/'
-  version '8.44-1'
+  @_ver = '8.44'
+  version "#{@_ver}-1"
   compatibility 'all'
-  source_url 'https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.bz2'
+  source_url "https://ftp.pcre.org/pub/pcre/pcre-#{@_ver}.tar.bz2"
   source_sha256 '19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d'
 
-  binary_url ({
+  binary_url({
     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pcre-8.44-1-chromeos-armv7l.tar.xz',
      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pcre-8.44-1-chromeos-armv7l.tar.xz',
        i686: 'https://dl.bintray.com/chromebrew/chromebrew/pcre-8.44-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pcre-8.44-1-chromeos-x86_64.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pcre-8.44-1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'b6475dc3ff52f0c471d320a7c7d7a17ad015f2f08b46f4fc6805d80efb80ec10',
-     armv7l: 'b6475dc3ff52f0c471d320a7c7d7a17ad015f2f08b46f4fc6805d80efb80ec10',
-       i686: 'abb6faf10ed437b2fdef5bd6da661a2327d2ffa79dfbdf7f3bc12713da1370cf',
-     x86_64: '4f2e3865d910f15b113c59cb69ce87dedbc236a8f655304ffb3fe8ceb89ab64a',
+  binary_sha256({
+    aarch64: '6d54befc6735b9d85928c14d1f03519d25514aed4c1a1f3d12114d3585f535f3',
+     armv7l: '6d54befc6735b9d85928c14d1f03519d25514aed4c1a1f3d12114d3585f535f3',
+       i686: '092fe524f7e00bc4e19b6a5cf01e1677f9d7900c8e105adcd086cd9670c7c558',
+     x86_64: '4c62ce6b8ae532c7b0885973b50363034c2a84b281baa5542149226a38016cbe'
   })
 
   def self.build
-    system "./configure #{CREW_OPTIONS} \
-  --enable-shared \
-  --disable-static \
-  --with-pic \
-  --enable-utf \
-  --enable-pcre16 \
-  --enable-pcre32 \
-  --enable-jit \
-  --enable-pcretest-libreadline"
+    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' \
+      LDFLAGS='-flto=auto' \
+      ./configure #{CREW_OPTIONS} \
+      --enable-shared \
+      --with-pic \
+      --enable-unicode-properties \
+      --enable-pcre16 \
+      --enable-pcre32 \
+      --enable-jit \
+      --enable-pcretest-libreadline"
     system 'make'
   end
 
   def self.check
-#    system 'make', 'check'
+    # `make check` fails because of an issue with locale.
+    # All but one test passes.
+    # system 'make', 'check'
   end
 
   def self.install
