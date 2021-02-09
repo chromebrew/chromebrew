81ff662cdaaad9fd79c8a92cb6ce5f6cce45a36c
satmandu <satadru@gmail.com>
2021-02-09 12:15:30 -0500

    Updated with binaries

--- packages/readline.rb
+++ packages/readline.rb
@@ -3,30 +3,37 @@ require 'package'
 class Readline < Package
   description 'The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in.'
   homepage 'http://cnswww.cns.cwru.edu/php/chet/readline/rltop.html'
-  version '8.1-1'
+  @_ver = '8.1'
+  version "#{@_ver}-1"
   compatibility 'all'
-  source_url 'https://gnu.askapache.com/readline/readline-8.1.tar.gz'
+  source_url "https://gnu.askapache.com/readline/readline-#{@_ver}.tar.gz"
   source_sha256 'f8ceb4ee131e3232226a17f51b164afc46cd0b9e6cef344be87c65962cb82b02'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: '4c2e3ab6488be4b58a28975f4e9f2fa7fb8649402cf4268c15c708bce9667f20',
-      armv7l: '4c2e3ab6488be4b58a28975f4e9f2fa7fb8649402cf4268c15c708bce9667f20',
-        i686: '9033816a26e04eb5c846ecb18f7354b1bbabf9cf5b8710e8843868dfee9d8a80',
-      x86_64: 'ddd7cf950d05c8492834b60ff0dbb124f07158c671784eb78118e0fa71691d1b',
+  binary_sha256({
+    aarch64: '59a46fc3edcb1a6dd5039aee1e7dda31289e87f60c8d260f8f883da11880425e',
+     armv7l: '59a46fc3edcb1a6dd5039aee1e7dda31289e87f60c8d260f8f883da11880425e',
+       i686: '5ceea6d03304fdbd8cd1a1a999831bafe46938f458128c0bc5df1a6819114201',
+     x86_64: '3d60b7fd04b57c4b57b34d8af12ab7b7dbf368842fc8b19b09df7698e9623292'
   })
 
   def self.build
-    system "./configure #{CREW_OPTIONS} CFLAGS='-flto=auto -g0' --with-curses --enable-multibyte"
-    system 'make', "SHLIB_LIBS=-Wl,--as-needed -Wl,-ltinfow -Wl,--no-as-needed"
+    system "env CFLAGS='-flto=auto -g0' \
+      CXXFLAGS='-flto=auto' \
+      LDFLAGS='-flto=auto' \
+      ./configure \
+      #{CREW_OPTIONS} \
+      --with-curses \
+      --enable-multibyte"
+    system 'make', 'SHLIB_LIBS=-Wl,--as-needed -Wl,-ltinfow -Wl,--no-as-needed'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
