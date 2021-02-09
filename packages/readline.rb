313ad51c3549fe08441b455b44c1aeabd90499fb
Kevin Perkins <kjperkin@users.noreply.github.com>
2021-02-09 01:38:50 -0800

    [readline]: Link against ncurses, build with multibyte support (Fixes #5099, #5101)

--- packages/readline.rb
+++ packages/readline.rb
@@ -3,16 +3,16 @@ require 'package'
 class Readline < Package
   description 'The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in.'
   homepage 'http://cnswww.cns.cwru.edu/php/chet/readline/rltop.html'
-  version '8.1'
+  version '8.1-1'
   compatibility 'all'
   source_url 'https://gnu.askapache.com/readline/readline-8.1.tar.gz'
   source_sha256 'f8ceb4ee131e3232226a17f51b164afc46cd0b9e6cef344be87c65962cb82b02'
 
   binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/readline-8.1-1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
      aarch64: '4c2e3ab6488be4b58a28975f4e9f2fa7fb8649402cf4268c15c708bce9667f20',
@@ -21,12 +21,9 @@ class Readline < Package
       x86_64: 'ddd7cf950d05c8492834b60ff0dbb124f07158c671784eb78118e0fa71691d1b',
   })
 
-
-
   def self.build
-    system "./configure #{CREW_OPTIONS} \
-    --with-curses"
-    system 'make'
+    system "./configure #{CREW_OPTIONS} CFLAGS='-flto=auto -g0' --with-curses --enable-multibyte"
+    system 'make', "SHLIB_LIBS=-Wl,--as-needed -Wl,-ltinfow -Wl,--no-as-needed"
   end
 
   def self.install
