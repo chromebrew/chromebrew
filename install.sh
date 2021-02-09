34e546f17c9def613ec3c6177a75654a274e6fb9
satmandu <satadru@umich.edu>
2021-02-09 11:28:29 -0500

    Don't do a recursive chown on all of /usr/local

--- install.sh
+++ install.sh
@@ -39,7 +39,9 @@ case "${ARCH}" in
 esac
 
 # This will allow things to work without sudo
-sudo chown -R "$(id -u)":"$(id -g)" "${CREW_PREFIX}"
+crew_folders="bin cache doc docbook etc include lib lib64 libexec man sbin share tmp var"
+for folder in $crew_folders ; do sudo chown -R "$(id -u)":"$(id -g)" "${CREW_PREFIX}"/"$folder" ; done
+sudo chown "$(id -u)":"$(id -g)" "${CREW_PREFIX}"
 # Delete 'var' symlink on Cloudready platform
 if [[ $(grep neverware /etc/lsb-release) != "" ]]; then
   [ -L /usr/local/var ] && sudo rm -f /usr/local/var
