1a9a61ae1d9edd89fa710d0a8a664a53121742fe
supechicken <73414985+supechicken@users.noreply.github.com>
2021-02-11 16:40:35 +0800

    Add missing dependencies

--- packages/launcher.rb
+++ packages/launcher.rb
@@ -6,8 +6,12 @@ class Launcher < Package
   version '5.0'
   source_url 'https://github.com/supechicken/chromebrew/raw/master/launcher/launcher.rb'
   source_sha256 '117615ee5f2cf5096c8fef274cfa3cc94c2161b5b23e080ff199081c18b5c0a7'
-    
+  
+  depends_on 'libyaml'
+  depends_on 'imagemagick7'
+
   def self.install
+    system('gem', 'install', 'em-websocket')
     FileUtils.mkdir_p(CREW_DEST_PREFIX + '/bin')
     FileUtils.install('../launcher.rb', CREW_DEST_PREFIX + '/bin/launcher', mode: 0755)
   end
