0b1807017a2630bd886d1575c291f75e533ced26
supechicken <73414985+supechicken@users.noreply.github.com>
2021-02-11 14:08:11 +0800

    Delete integration/extension directory

--- integration/extension/icon/README.md
+++
@@ -1,1 +0,0 @@
-# Icons for Chromebrew GUI Apps
--- integration/extension/js/crosh.js
+++
@@ -1,9 +0,0 @@
-var NEWTERM = 'chrome-untrusted://crosh/',
-    OLDTERM = 'chrome-extension://nkoccljplnhpfnfiajclkommnmllphnl/html/crosh.html';
-chrome.runtime.sendMessage('nkoccljplnhpfnfiajclkommnmllphnl', null, null, (response) => {
-    if (chrome.runtime.lastError) {
-        chrome.tabs.update({url: NEWTERM})
-    } else {
-        chrome.tabs.update({url: OLDTERM})
-    }
-})
