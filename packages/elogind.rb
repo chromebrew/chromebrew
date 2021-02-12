30b590306672ddfc63a0157a6367682368b7c3b9
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-11 22:01:39 -0500

    [Fix Package] elogind (#4949)
    * [Fix Package] elogind

--- packages/elogind.rb
+++ packages/elogind.rb
@@ -1,26 +1,13 @@
 require 'package'
 
 class Elogind < Package
-  description 'Standalone logind fork'
+  description 'Standalone systemd-logind fork'
   homepage 'https://github.com/elogind/elogind'
   version '243.4'
   compatibility 'all'
   source_url 'https://github.com/elogind/elogind/archive/v243.4.tar.gz'
   source_sha256 'f1098745863138e6270ea22e78a39baef9a0356b48246c5a53b34211992dc7db'
-
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-243.4-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'cb9cee0ff417657040c5c02ebe618eaf972e5cb374de641db7e66e8754673fc3',
-     armv7l: 'cb9cee0ff417657040c5c02ebe618eaf972e5cb374de641db7e66e8754673fc3',
-       i686: '9dbcdc416612a3cc32d71c8120b3c38d6d37b8f75011b4a8cfe04003fe2eb737',
-     x86_64: 'e76b69346288beef9a595b90b9f7dd30424339b9c4e906e928ff4bd8cb5f82fa',
-  })
-
+  
   depends_on 'eudev'
   depends_on 'libcap'
   depends_on 'libseccomp'
@@ -33,13 +20,23 @@ class Elogind < Package
   depends_on 'glib'
 
   def self.build
-    system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} -Dcgroup-controller=elogind \
--Dman=false -Ddefault-hierarchy=legacy -Ddefault-kill-user-processes=false -Dhalt-path=/sbin/halt \
--Drootlibexecdir=#{CREW_PREFIX}/libexec/elogind -Dreboot-path=/sbin/reboot _build"
-    system 'ninja -v -C _build'
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+            -Drootprefix=#{CREW_PREFIX} \
+            --strip \
+            -Dcgroup-controller=elogind \
+            -Dman=false \
+            -Ddefault-hierarchy=legacy \
+            -Ddefault-kill-user-processes=false \
+            -Dhalt-path=/sbin/halt \
+            -Drootlibdir=#{CREW_PREFIX}/#{ARCH_LIB} \
+            -Drootlibexecdir=#{CREW_PREFIX}/libexec/elogind \
+            -Drootbindir=#{CREW_PREFIX}/bin \
+            -Dreboot-path=/sbin/reboot \
+            builddir"
+    system "ninja -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C _build install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
