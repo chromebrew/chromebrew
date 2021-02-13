a675d4cd9653de174adcca24e541f57998a8db64
supechicken <73414985+supechicken@users.noreply.github.com>
2021-02-13 15:09:12 +0800

    Add files via upload

--- launcher/launcher.rb
+++ launcher/launcher.rb
@@ -1,16 +1,54 @@
 #!/usr/bin/env ruby
 
 # use chromebrew variable
-require 'CREW_PREFIX_/lib/crew/lib/const'
-require 'CREW_PREFIX_/lib/crew/lib/color'
+require "CREW_PREFIX_/lib/crew/lib/const".gsub('CREW_PREFIX_', '/usr/local')
+require "CREW_PREFIX_/lib/crew/lib/color".gsub('CREW_PREFIX_', '/usr/local')
 require 'em-websocket'
 require 'fileutils'
 
+# for multi-language support in .desktop file
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+LANG = ENV['LANG'] || ''
+
+# variables
 PID = CREW_PREFIX + '/tmp/server.pid'
 LOG = CREW_PREFIX + '/tmp/server.log'
-PWA_PREFIX = CREW_PREFIX + '/lib/pwa'
+PWA_PREFIX = CREW_PREFIX + '/lib/pwa/'
 PORT = 25500
-PATH = "#{CREW_PREFIX}/share/pixmaps " + "/usr/local/share/icons " + "#{HOME}/.local/ " + PWA_PREFIX
+
+ICON_TYPE = [
+  '96x96/',
+  '128x128/',
+  '192x192/',
+  '256x256/',
+  '512x512/'
+]
+
+ICON_PATH = [ 
+  "#{CREW_PREFIX}/share/icons/",
+  "#{HOME}/.local/",
+  PWA_PREFIX 
+]
+
+ICON_EXTENSION = [
+  '.png',
+  '.xpm'
+]
+
+ICON_PROVIDER = [
+  'gnome/',
+  'Adwaita/',
+  'hicolor/'
+]
+
+DESKTOP_PATH = [ 
+  "#{CREW_PREFIX}/share/applications/", 
+  "#{HOME}/.local/share/applications/",
+  PWA_PREFIX
+]
+
+# help message and template
 
 HELP = <<EOF
 launcher: Add GUI applications shortcut to Chrome/Chromium OS launcher
@@ -39,7 +77,7 @@ JSON = <<EOF
     }
   },
   "name": "NAME",
-  "version": "1.0",
+  "version": "VER",
   "description": "INFO",
   "icons": { "1024": "icon.png" },
   "minimum_chrome_version": "56.0"
@@ -51,9 +89,11 @@ You're almost done, three more steps:
 
 Go to chrome://extensions in Chrome
 Ensure that the 'Developer mode' checkbox in the top right-hand corner is checked.
-Click on Load Unpacked and select the '<CMD>.launcher' folder.".lightblue
+Click on Load Unpacked and select the '<CMD>.launcher' folder.
+".lightblue
 
 # kill pid of the previous server in the log file (if exist)
+
 def stop_server
   begin
     Process.kill 'KILL', File.read(PID).to_i if File.exist?(PID)
@@ -65,34 +105,59 @@ end
 def desktop_icon_name_finder (app)
   # find out the info, name and icon of the command
     
-  @desktop_path = [ "#{CREW_PREFIX}/share/applications/", "#{HOME}/.local/share/applications/" ]
-    
   # abort if a desktop file not found for the command
-  unless File.exists? @desktop_path[0] + "#{app}.desktop" or File.exists? @desktop_path[1] + "#{app}.desktop"
-    puts "Cannot find the .desktop file for #{app}, using default Chromebrew icon... 
-(If you want to use customize icon, put the icon file to #{CREW_PREFIX}/lib/pwa/ and name it to '#{app}.png')".yellow
-    FileUtils.mkdir_p PWA_PREFIX
-    system('curl', '-L#', 'https://github.com/supechicken/chromebrew/raw/master/images/brew546.png', '-o', PWA_PREFIX + '/brew.png')
-    @icon = PWA_PREFIX + '/brew.png'
-    @icon = PWA_PREFIX + "/#{app}.png" if File.exist?(PWA_PREFIX + "/#{app}.png")
+  
+  DESKTOP_PATH.each do |desktop|
+    desktop_path = desktop + app + '.desktop'
+    @desktop = desktop_path if File.exist?(desktop_path)
+  end
+    
+  # set @desktop to nil if no file found in PATH
+  
+  @desktop ||= nil
+  
+  unless @desktop
     @name = app.capitalize
   else
-    for var in @desktop_path
-      p var
-      @desktop = "#{var}/#{app}.desktop" if File.exist?("#{var}/#{app}.desktop")
-    end
+    desktop = File.read(@desktop).split("\n\n", 2)[0]
+    icon_name = desktop.match(/^Icon=(.*?)\n/)[0].partition('=')[2].tr("\n", '')
     
-    @icon = File.readlines(@desktop).select { |line| line =~ /^Icon/ }[0].partition('=')[2].tr("\n", '')
-    @icon = `find #{PATH} -iname #{@icon}.png -o -iname #{@icon}.xpm 2> /dev/null |\
-             sed -e 's:/usr/local/share/icons/hicolor/::g' | sort -n | sed -e 's:^:/usr/local/share/icons/hicolor/:g' |\
-             tail -n1`.tr("\n", '')
-  
+    ICON_PATH.each {|icon| ICON_TYPE.each {|size| ICON_PROVIDER.each {|provider| ICON_EXTENSION.each {|extname|
+      xpm = CREW_PREFIX + '/share/pixmaps/' + icon_name + extname
 
-    @name = File.readlines(@desktop).select { |line| line =~ /^Name/ }[0].partition('=')[2].tr("\n", '')
-    @info = File.readlines(@desktop).select { |line| line =~ /^Comment/ }[0].partition('=')[2].tr("\n", '')
+      @icon ||= xpm if File.exist?(xpm)
+        
+      icon_path = icon + provider + size + 'apps/' + icon_name + extname
+      @icon = icon_path if File.exist?(icon_path)
+    }}}}
+
+    # if localized name (language) not found, use default
+    @name = (desktop.match(/^Name\[#{LANG}\]=(.*?)\n/)[0].partition('=')[2]) rescue desktop.match(/^Name=(.*?)\n/)[0].partition('=')[2]
+    @info = (desktop.match(/^Comment\[#{LANG}\]=(.*?)\n/)[0].partition('=')[2]) rescue desktop.match(/^Comment=(.*?)\n/)[0].partition('=')[2]   
   end
-  @info = `crew search -d #{app} | head -1` unless @info
+      
+  unless @icon
+    puts "Cannot find an icon for #{app}, using default Chromebrew icon... 
+(If you want to use customize icon, put the icon file to #{PWA_PREFIX} and name it to '#{app}.png')".yellow
+
+    FileUtils.mkdir_p PWA_PREFIX
+    system('curl', '-L#', 'https://github.com/supechicken/chromebrew/raw/master/images/brew546.png', '-o', PWA_PREFIX + '/brew.png')
+    @icon = PWA_PREFIX + 'brew.png'
+    @icon = PWA_PREFIX + "#{app}.png" if File.exist?(PWA_PREFIX + "#{app}.png")
+  end
+   
+  unless @info
+    @info = File.readlines(CREW_LIB_PATH + 'packages/' + app + '.rb').select { |line| line =~ /description /}[0].tr("'", '"').scan(/"(.*?)"/)[0][0]
+  end
+  @ver = `#{app} --version 2> /dev/null | head -1 | grep -o '[.0-9]'`.tr("\n", '')
+      
+  puts 'Name: ' + @name
+  puts 'Version: ' + @ver
+  puts 'Icon: ' + @icon
+  puts 'Info: ' + @info
 end
+
+
 
 def new (app)
   # abort when command not found
@@ -104,13 +169,17 @@ def new (app)
   @working_on_dir = "#{HOME}/MyFiles/#{app}.launcher/"
   FileUtils.mkdir_p(@working_on_dir)
   
+  # convert icon to .png and resize
   system('convert', @icon, '-resize', '1024x1024', @working_on_dir + 'icon.png')
     
-  File.write @working_on_dir + 'manifest.json', JSON.gsub('NAME', @name).gsub('INFO', @info)
+  File.write @working_on_dir + 'manifest.json', JSON.gsub('NAME', @name).gsub('INFO', @info).gsub('VER', @ver)
   File.write @working_on_dir + 'background.js', JS.gsub('CMD', app)
   
   puts POST_INSTALL.gsub('<CMD>', app).lightblue
 end
+
+
+
 
 def start_server
     
@@ -152,10 +221,15 @@ def start_server
     }
   end
 end
+
+
+
     
 case ARGV[0]
 when 'start'
   start_server
+when 'test'
+  desktop_icon_name_finder('opera')
 when 'new'
   if ARGV[1] then
     new ARGV[1]
