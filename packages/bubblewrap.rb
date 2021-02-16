465e0428fc11fef912e91a1c45da109ca724e132
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-16 15:22:14 -0500

    Bump version

--- packages/bubblewrap.rb
+++ packages/bubblewrap.rb
@@ -3,7 +3,7 @@ require 'package'
 class Bubblewrap < Package
   description 'bubblewrap works by creating a new, completely empty, mount namespace'
   homepage 'https://github.com/containers/bubblewrap'
-  version '0.4.1'
+  version '0.4.1_1'
   compatibility 'all'
   source_url 'https://github.com/containers/bubblewrap/releases/download/v0.4.1/bubblewrap-0.4.1.tar.xz'
   source_sha256 'b9c69b9b1c61a608f34325c8e1a495229bacf6e4a07cbb0c80cf7a814d7ccc03'
