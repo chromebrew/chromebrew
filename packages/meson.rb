894a314d59977f4b39c2c27511dea000019fdd26
satmandu <satadru@umich.edu>
2021-02-17 16:53:38 -0500

    meson-> 0.57 (#5151)

--- packages/meson.rb
+++ packages/meson.rb
@@ -3,26 +3,24 @@ require 'package'
 class Meson < Package
   description 'The Meson Build System'
   homepage 'http://mesonbuild.com/'
-  version '0.56.2'
+  @_ver = '0.57.0'
+  version @_ver
   compatibility 'all'
-  source_url 'https://github.com/mesonbuild/meson/archive/0.56.2.tar.gz'
-  source_sha256 'aaae961c3413033789248ffe6762589e80b6cf487c334d0b808e31a32c48f35f'
+  source_url "https://github.com/mesonbuild/meson/archive/#{@_ver}.tar.gz"
+  source_sha256 'fd26a27c1a509240c668ebd29d280649d9239cf8684ead51d5cb499d1e1188bd'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.56.2-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.56.2-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.56.2-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.56.2-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.57.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.57.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.57.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/meson-0.57.0-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: 'c463376e6d041ec860ea6ca4a02d852260ca289805e3822442173fc007fb9e43',
-      armv7l: 'c463376e6d041ec860ea6ca4a02d852260ca289805e3822442173fc007fb9e43',
-        i686: '78bb992b3336d189ee5f73fc9b19697b630974913d906d2d86ef71dd963736f4',
-      x86_64: 'c44b86828e222b236133d4e27c0437616bf9bb1503b3acfcd48ca7b655cdf50d',
+  binary_sha256({
+    aarch64: '956b43e32f94d860cf43773a7a80634f0e7c09febc150518cb6419265b5d7e33',
+     armv7l: '956b43e32f94d860cf43773a7a80634f0e7c09febc150518cb6419265b5d7e33',
+       i686: '1b7d8082b8930f302c43e07404b13accabcdf29f0c3bc9e5abba07876a1e8b57',
+     x86_64: 'cafa9cb7cbd2473e1f22abe2f93b8f7ffe9816834a62b4e4810ec21565153571'
   })
-
-  depends_on 'ninja'
-  depends_on 'setuptools'
 
   def self.install
     system "python3 setup.py install --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
