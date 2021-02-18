c5a52062c51f4795c3fc1e6ce54757297c24561c
satmandu <satadru@gmail.com>
2021-02-17 20:48:10 -0500

    current working build
    - no sound
    - The X11 DISPLAY variable isn't getting through.
    - This is the sort of command that works after installing: flatpak run --socket=wayland org.videolan.VLC

--- packages/flatpak.rb
+++ packages/flatpak.rb
@@ -7,19 +7,19 @@ class Flatpak < Package
   version @_ver
   compatibility 'all'
   source_url "https://github.com/flatpak/flatpak/releases/download/#{@_ver}/flatpak-#{@_ver}.tar.xz"
-  source_sha256 'c1354f42bf3b5d51aeb4028c9b62fd4ffc673ef2ff6e583c17777f5dafdbdcb7'
+  source_sha256 'c70215792b7cbece83c489dab86adc9bfaf9b140c506affe2a48c92afa3d69b7'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/flatpak-1.10.1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: 'c7973f796b3b3b5d3bca1135055eee116589d94f00bedcf7b2784353125c8ddc',
-      armv7l: 'c7973f796b3b3b5d3bca1135055eee116589d94f00bedcf7b2784353125c8ddc',
-        i686: '2527dd47eef699215e6cfebdb2e5133b936cfc38bdd95a570c6ba8c2df724484',
-      x86_64: 'e068889544d79080b12af9bdea43b96b239c54dcc5b48eb511c538e1dd835c26',
+  binary_sha256({
+    aarch64: '4347861920c16c6ae610d08574a7000a57310bb70a60dd15840accc636a96167',
+     armv7l: '4347861920c16c6ae610d08574a7000a57310bb70a60dd15840accc636a96167',
+       i686: 'ba6a92d2607c511c6c78f006c77216e820c2c776017a849991a539d199c95bf7',
+     x86_64: 'e115b1d3b779a67402e9ff00b8c92b9acd885ef39fcc710f8e0bd02b40b21925'
   })
 
   depends_on 'bubblewrap'
@@ -32,10 +32,16 @@ class Flatpak < Package
 
   def self.build
     system 'env NOCONFIGURE=1 ./autogen.sh'
-    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto'  LDFLAGS='-flto=auto' \
+    system 'filefix'
+    system "env BWRAP=#{CREW_PREFIX}/bin/bwrap CFLAGS='-flto=auto' CXXFLAGS='-flto=auto'  LDFLAGS='-flto=auto' \
       ./configure #{CREW_OPTIONS} \
       --with-system-install-dir=#{CREW_PREFIX}/var/lib/flatpak \
+      --enable-sudo \
+      --with-priv-mode=setuid \
+      --enable-san \
       --without-systemd \
+      --with-system-fonts-dir=#{CREW_PREFIX}/share/fonts:/usr/share/fonts \
+      --with-system-font-cache-dirs=/usr/share/cache/fontconfig:#{CREW_PREFIX}/cache/fontconfig \
       --disable-documentation \
       --disable-maintainer-mode \
       --with-system-bubblewrap"
