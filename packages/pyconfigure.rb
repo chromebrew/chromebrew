cf8d7a6757b78e392c89d805dfa4cd1efcbc18d3
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-20 00:07:33 -0500

    Update pyconfigure.rb

--- packages/pyconfigure.rb
+++ packages/pyconfigure.rb
@@ -3,12 +3,10 @@ require 'package'
 class Pyconfigure < Package
   description 'GNU pyconfigure provides developers with file templates for implementing standard configure scripts and Makefile recipes for their Python packages.'
   homepage 'https://www.gnu.org/software/pyconfigure/'
-  version '3.1.1'
+  version '0.2.3'
   compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/pyconfigure/pyconfigure-0.2.3.tar.gz'
   source_sha256 '37438ee1ae0aad7692188c7d01218cb407a1c89f3efdb5d360311b7ade4ab14f'
-  
-  depends_on 'python2_7'
 
   def self.build
     system "./configure #{CREW_OPTIONS}"
