e5406960a61f25d1c1ce98c411e1c13c82989d91
Ed Reel <edreel@gmail.com>
2021-02-20 01:02:51 -0600

    Update composer from 2.0.8 to 2.0.9

--- packages/composer.rb
+++ packages/composer.rb
@@ -3,10 +3,10 @@ require 'package'
 class Composer < Package
   description 'Dependency Manager for PHP'
   homepage 'https://getcomposer.org/'
-  version '2.0.8'
+  version '2.0.9'
   compatibility 'all'
-  source_url 'https://github.com/composer/composer/archive/2.0.8.tar.gz'
-  source_sha256 '009f1d8ee3d5050a78b3732a947d4a10c2718dfbb83da52c3fcf71ff2d7dd465'
+  source_url "https://github.com/composer/composer/archive/#{version}.tar.gz"
+  source_sha256 '2334cbd4236c7a30afc871cb045a19e4941b2f9bdecb943180d10b3a040d1e8b'
 
   depends_on 'php74' unless File.exists? "#{CREW_PREFIX}/bin/php"
   depends_on 'xdg_base'
@@ -22,8 +22,8 @@ class Composer < Package
   end
 
   def self.install
-    system 'curl -Ls -o composer https://github.com/composer/composer/releases/download/2.0.8/composer.phar'
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('composer') ) == '2021f0d52b446e0efe3c548cc058ab5671fa38cdbcf814e7911c7e9d71d61538'
+    system "curl -Ls -o composer https://github.com/composer/composer/releases/download/#{version}/composer.phar"
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('composer') ) == '8e91344a5ca2fc0fb583c50f195a1f36918908561c4ea3d6f01a4ef01c3b8560'
     system "install -Dm755 composer #{CREW_DEST_PREFIX}/bin/composer"
   end
 
