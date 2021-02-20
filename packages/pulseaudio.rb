d205dfa38bd184ed68dcfa9fb83b43f94fdd05be
satmandu <satadru@umich.edu>
2021-02-19 21:37:51 -0500

    PulseAudio 14.2 + pavucontrol  (#5171)
    * pulseaudio -> 14.2, start fixing config
    
    * rebuild for pulseaudio 14.2

--- packages/pulseaudio.rb
+++ packages/pulseaudio.rb
@@ -3,22 +3,23 @@ require 'package'
 class Pulseaudio < Package
   description 'PulseAudio is a sound system for POSIX OSes, meaning that it is a proxy for your sound applications.'
   homepage 'https://www.freedesktop.org/wiki/Software/PulseAudio/'
-  version '13.99.3'
+  @_ver = '14.2'
+  version @_ver
   compatibility 'all'
-  source_url 'https://freedesktop.org/software/pulseaudio/releases/pulseaudio-13.99.3.tar.gz'
-  source_sha256 '2572543a7686c699654c40ec3945a598e6af50ff703f60de0446b7fbcd16ef01'
+  source_url "https://freedesktop.org/software/pulseaudio/releases/pulseaudio-#{@_ver}.tar.xz"
+  source_sha256 '75d3f7742c1ae449049a4c88900e454b8b350ecaa8c544f3488a2562a9ff66f1'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-13.99.3-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'ed785bbf45af02e5a96ed947740f1f5b055bdee39670f1006d15d6fd96876f24',
-     armv7l: 'ed785bbf45af02e5a96ed947740f1f5b055bdee39670f1006d15d6fd96876f24',
-       i686: '0e19e15a116395b9aa52977c3cd99c9e735d70365146435d54bdd9e300df7e3c',
-     x86_64: 'f346f11c323de6019f1f1189db6398d12671db5b493b68c18b1617e177faff68',
+  binary_sha256({
+    aarch64: '5229ace51bc615012d3ad2d0e00a587344d036f911819a6c3929c693e94eec9c',
+     armv7l: '5229ace51bc615012d3ad2d0e00a587344d036f911819a6c3929c693e94eec9c',
+       i686: 'cd9fd9ba81591a09bfd175d461918a8724c5edfa6059abf545f78d24385a3677',
+     x86_64: 'c9aff79692b3beb4d210f3ad3eef87ec92dd64480e476a0cf1808c2a7ef3a0c3'
   })
 
   depends_on 'gsettings_desktop_schemas'
@@ -34,19 +35,24 @@ class Pulseaudio < Package
   depends_on 'eudev'
   depends_on 'gtk3'
   depends_on 'dbus'
-  depends_on 'gdbm'
   depends_on 'tdb'
+  depends_on 'check' => :build
 
   def self.build
-    system "./configure #{CREW_OPTIONS}"
-    system "make -j#{CREW_NPROC}"
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    --default-library=both \
+    -Dsystem_user=chronos \
+    -Dsystem_group=cras \
+    -Daccess_group=cras \
+    -Dbluez5=false \
+    -Dalsa=enabled \
+    -Dudevrulesdir=#{CREW_PREFIX}/libexec/rules.d \
+    builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
   end
 
   def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
