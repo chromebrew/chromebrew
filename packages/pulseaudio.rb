9467a5205692fd91151f2a9abc2efec9e57b736c
satmandu <satadru@umich.edu>
2021-02-20 15:46:15 -0500

    working configuration which allows pulseaudio to be loaded in flatpak

--- packages/pulseaudio.rb
+++ packages/pulseaudio.rb
@@ -4,22 +4,22 @@ class Pulseaudio < Package
   description 'PulseAudio is a sound system for POSIX OSes, meaning that it is a proxy for your sound applications.'
   homepage 'https://www.freedesktop.org/wiki/Software/PulseAudio/'
   @_ver = '14.2'
-  version @_ver
+  version "#{@_ver}-1"
   compatibility 'all'
   source_url "https://freedesktop.org/software/pulseaudio/releases/pulseaudio-#{@_ver}.tar.xz"
   source_sha256 '75d3f7742c1ae449049a4c88900e454b8b350ecaa8c544f3488a2562a9ff66f1'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/pulseaudio-14.2-1-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '5229ace51bc615012d3ad2d0e00a587344d036f911819a6c3929c693e94eec9c',
-     armv7l: '5229ace51bc615012d3ad2d0e00a587344d036f911819a6c3929c693e94eec9c',
-       i686: 'cd9fd9ba81591a09bfd175d461918a8724c5edfa6059abf545f78d24385a3677',
-     x86_64: 'c9aff79692b3beb4d210f3ad3eef87ec92dd64480e476a0cf1808c2a7ef3a0c3'
+    aarch64: '9f10feb9805320b1c710d93a78a706d0ecc9c8a2b06e82008369b938c7fa6ea7',
+     armv7l: '9f10feb9805320b1c710d93a78a706d0ecc9c8a2b06e82008369b938c7fa6ea7',
+       i686: '816c9dae258d6c3f10fd72bbf621253891cb01915746460d8b806784fe6647c0',
+     x86_64: '89be113223767b5c7af0b6f064efbb65338aac39208018b489152e1654185a33'
   })
 
   depends_on 'gsettings_desktop_schemas'
@@ -36,23 +36,95 @@ class Pulseaudio < Package
   depends_on 'gtk3'
   depends_on 'dbus'
   depends_on 'tdb'
+  depends_on 'cras'
+  depends_on 'orc'
+  depends_on 'jack'
+  depends_on 'avahi'
+  depends_on 'gstreamer'
+  depends_on 'gst_plugins_base'
+  depends_on 'gst_plugins_good'
+  depends_on 'gst_plugins_bad'
+  depends_on 'gst_plugins_bad'
   depends_on 'check' => :build
+  depends_on 'webrtc_audio_processing'
 
   def self.build
     system "meson #{CREW_MESON_LTO_OPTIONS} \
     --default-library=both \
+    -Dcpp_args='-I#{CREW_PREFIX}/include' \
     -Dsystem_user=chronos \
     -Dsystem_group=cras \
     -Daccess_group=cras \
     -Dbluez5=false \
     -Dalsa=enabled \
+    -Dgstreamer=enabled \
+    -Dtests=true \
     -Dudevrulesdir=#{CREW_PREFIX}/libexec/rules.d \
+    -Dalsadatadir=#{CREW_PREFIX}/share/alsa-card-profile \
     builddir"
     system 'meson configure builddir'
     system 'ninja -C builddir'
   end
 
+  def self.check
+    # Only test failure is on armv7l:
+    # 39/50 thread-test                                                                                                   FAIL             4.02s   exit status 1
+    # >>> MALLOC_PERTURB_=232 MAKE_CHECK=1 /usr/local/tmp/crew/pulseaudio-14.2.tar.xz.dir/pulseaudio-14.2/builddir/src/tests/thread-test
+    # ――――――――――――――――――――――――――――――――――――― ✀  ―――――――――――――――――――――――――――――――――――――
+    # stdout:
+    # Running suite(s): Thread
+    # 0%: Checks: 1, Failures: 0, Errors: 1
+    # ../src/tests/thread-test.c:108:E:thread:thread_test:0: (after this point) Test timeout expired
+    # stderr:
+    # loop-init
+    # once!
+    system 'ninja -C builddir test || true'
+  end
+
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    @pulseaudio_daemon_conf = <<~PAUDIO_DAEMON_CONF_HEREDOC
+      # Replace these with the proper values
+      exit-idle-time = 10 # Exit as soon as unneeded
+      flat-volumes = yes # Prevent messing with the master volume
+    PAUDIO_DAEMON_CONF_HEREDOC
+    IO.write("#{CREW_DEST_PREFIX}/etc/pulse/daemon.conf", @pulseaudio_daemon_conf, perm: 0o555)
+    @pulseaudio_client_conf = <<~PAUDIO_CLIENT_CONF_HEREDOC
+      # Replace these with the proper values
+
+      # Applications that uses PulseAudio *directly* will spawn it,
+      # use it, and pulse will exit itself when done because of the
+      # exit-idle-time setting in daemon.conf
+      autospawn = yes
+    PAUDIO_CLIENT_CONF_HEREDOC
+    IO.write("#{CREW_DEST_PREFIX}/etc/pulse/client.conf", @pulseaudio_client_conf, perm: 0o555)
+    @pulseaudio_default_pa = <<~PAUDIO_DEFAULT_PA_HEREDOC
+      # Replace the *entire* content of this file with these few lines and
+      # read the comments
+
+      .fail
+          # Set tsched=0 here if you experience glitchy playback. This will
+          # revert back to interrupt-based scheduling and should fix it.
+          #
+          # Replace the device= part if you want pulse to use a specific device
+          # such as "dmix" and "dsnoop" so it doesn't lock an hw: device.
+
+          # INPUT/RECORD
+          load-module module-alsa-source device="default" tsched=1
+
+          # OUTPUT/PLAYBACK
+          load-module module-alsa-sink device="default" tsched=1
+
+          # Accept clients -- very important
+          load-module module-native-protocol-unix
+
+      .nofail
+      .ifexists module-x11-publish.so
+          # Publish to X11 so the clients know how to connect to Pulse. Will
+          # clear itself on unload.
+          load-module module-x11-publish
+      .endif
+    PAUDIO_DEFAULT_PA_HEREDOC
+    IO.write("#{CREW_DEST_PREFIX}/etc/pulse/default.pa", @pulseaudio_default_pa, perm: 0o555)
   end
 end
