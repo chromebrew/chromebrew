91215afc1609a5a9b0cbe14143acc9b658acfa56
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-02-20 11:51:06 +0800

    Update stack.rb

--- packages/stack.rb
+++ packages/stack.rb
@@ -3,22 +3,25 @@ require 'package'
 class Stack < Package
   description 'The Haskell Tool Stack - Stack is a cross-platform program for developing Haskell projects. It is aimed at Haskellers both new and experienced.'
   homepage 'https://docs.haskellstack.org/en/stable/README/'
-  version '2.3.1'
-  compatibility 'all'
-
-  case ARCH
-  when 'x86_64'
-    source_url 'https://github.com/commercialhaskell/stack/releases/download/v2.3.1/stack-2.3.1-linux-x86_64.tar.gz'
-    source_sha256 'b753cd21d446aea16a221326ec686e3acdf1b146c714a77b5d27fd855475554d'
-  end
+  @_ver = '2.5.1'
+  version @_ver
+  compatibility 'x86_64'
+  source_url "https://github.com/commercialhaskell/stack/releases/download/v#{@_ver}/stack-#{@_ver}-linux-x86_64.tar.gz"
+  source_sha256 `curl -Ls https://github.com/commercialhaskell/stack/releases/download/v#{@_ver}/stack-#{@_ver}-linux-x86_64.tar.gz.sha256 |\
+                 tail -n1 | cut -d ' ' -f1`.tr("\n", '') if (ARGV[0] == 'install' or ARGV[0] == 'reinstall')
 
   def self.install
     FileUtils.mkdir_p CREW_DEST_HOME
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/.stack"
-    system "install -Dm755 stack #{CREW_DEST_PREFIX}/bin/stack"
-    system "echo 'local-bin-path: #{CREW_PREFIX}/bin' > #{CREW_DEST_PREFIX}/.stack/config.yaml"
-    system "echo 'local-programs-path: #{CREW_PREFIX}/share/stack' >> #{CREW_DEST_PREFIX}/.stack/config.yaml"
     FileUtils.ln_s "#{CREW_PREFIX}/.stack", "#{CREW_DEST_HOME}/.stack"
+    FileUtils.install 'stack', "#{CREW_DEST_PREFIX}/bin/stack", mode: 0755
+    
+    @config = <<~EOF
+      local-bin-path: #{CREW_PREFIX}/bin
+      local-programs-path: #{CREW_PREFIX}/share/stack
+    EOF
+       
+    File.write("#{CREW_DEST_PREFIX}/.stack/config.yaml", @config)
   end
 
   def self.postinstall
@@ -28,10 +31,10 @@ class Stack < Package
     puts "echo 'eval \"\$(stack --bash-completion-script stack)\"' >> ~/.bashrc".lightblue
     puts "source ~/.bashrc".lightblue
     puts
-    puts "To completely uninstall stack, execute the following:".lightblue
-    puts "crew remove stack".lightblue
-    puts "rm -rf #{CREW_PREFIX}/share/stack".lightblue
-    puts "rm -rf ~/.stack".lightblue
-    puts
+  end
+  
+  def self.remove
+    FileUtils.rm_rf "#{CREW_PREFIX}/share/stack"
+    FileUtils.rm_rf "#{HOME}/.stack"
   end
 end
