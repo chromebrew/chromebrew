2f9b35a6fd3a5958c3cdc684903294836b8fb1e6
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-21 14:21:06 -0500

    [New Package] libdeflate

---
+++ packages/libdeflate.rb
@@ -0,0 +1,18 @@
+require 'package'
+
+class Libdeflate < Package
+  description 'Heavily optimized library for DEFLATE compression and decompression'
+  homepage 'https://github.com/ebiggers/libdeflate/'
+  version '1.7'
+  compatibility 'all'
+  source_url 'https://github.com/ebiggers/libdeflate/archive/v1.7.tar.gz'
+  source_sha256 'a5e6a0a9ab69f40f0f59332106532ca76918977a974e7004977a9498e3f11350'
+  
+  def self.build
+    system 'make', "PREFIX=#{CREW_PREFIX}"
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+end
