4ca0ccb58483f438e40a5447a3e77e51ee3a94cb
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-21 14:34:59 -0500

    make prefix and libdir work

--- packages/libdeflate.rb
+++ packages/libdeflate.rb
@@ -8,8 +8,13 @@ class Libdeflate < Package
   source_url 'https://github.com/ebiggers/libdeflate/archive/v1.7.tar.gz'
   source_sha256 'a5e6a0a9ab69f40f0f59332106532ca76918977a974e7004977a9498e3f11350'
   
+  def self.patch
+    system "sed -i 's:PREFIX ?= /usr/local:PREFIX ?= #{CREW_PREFIX}:g' Makefile"
+    system "sed -i 's:LIBDIR ?= $(PREFIX)/lib:LIBDIR ?= #{CREW_LIB_PREFIX}:g' Makefile"
+  end
+  
   def self.build
-    system 'make', "PREFIX=#{CREW_PREFIX}"
+    system 'make'
   end
 
   def self.install
