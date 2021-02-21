0f8265f22a5b5bb03854f966c3192c03abc5f69f
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-21 16:42:19 -0500

    [Fix Package] libgdiplus (#5223)
    * [Fix Package] libgdiplus
    
    * Bump version

--- packages/libgdiplus.rb
+++ packages/libgdiplus.rb
@@ -3,21 +3,26 @@ require 'package'
 class Libgdiplus < Package
   description 'libgdiplus is the mono library that provides a GDI+-compatible API on non-windows operating systems.'
   homepage 'https://www.mono-project.com/docs/gui/libgdiplus/'
-  version '6.0.5'
+  version '6.0.5-1'
   compatibility 'all'
-  source_url 'https://github.com/mono/libgdiplus/archive/6.0.5.tar.gz'
-  source_sha256 '1fd034f4b636214cc24e94c563cd10b3f3444d9f0660927b60e63fd4131d97fa'
+  source_url 'https://download.mono-project.com/sources/libgdiplus/libgdiplus-6.0.5.tar.gz'
+  source_sha256 'b81e4e5cc3e4831b2945de08bef26eb1bdcd795aeaf8f971b221c51213a025ef'
 
+  depends_on 'pcre'
   depends_on 'glib'
   depends_on 'cairo'
   depends_on 'graphite'
   depends_on 'libexif'
   depends_on 'libtiff'
+  depends_on 'libpng'
+  depends_on 'libwebp'
+  depends_on 'imake' => :build
 
   def self.build
-    system "env NOCONFIGURE=1 ./autogen.sh"
     system "./configure #{CREW_OPTIONS} \
-            --disable-static"
+            --with-libexif \
+            --with-libtiff \
+            --with-x11"
     system "make"
   end
 
