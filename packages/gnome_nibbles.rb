fc5af8e968e223413e8d52d9b6737cb675fb47a3
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-02-22 01:35:44 +0800

    Add files via upload

---
+++ packages/gnome_nibbles.rb
@@ -0,0 +1,47 @@
+require 'package'
+
+class Gnome_nibbles < Package
+  description 'snake game, up to four players'
+  homepage 'https://wiki.gnome.org/Apps/Nibbles'
+  @_app = 'gnome-nibbles'
+  @_ver = '3.38'
+  @_patch = '.2'
+  @_fullver = @_ver + @_patch
+  version @_fullver
+  source_url "https://download.gnome.org/sources/#{@_app}/#{@_ver}/#{@_app}-#{@_fullver}.tar.xz"
+  source_sha256 `curl -Ls https://download.gnome.org/sources/#{@_app}/#{@_ver}/#{@_app}-#{@_fullver}.sha256sum |\
+                 tail -n1 | cut -d ' ' -f1`.chomp
+
+  depends_on 'clutter_gtk'
+  depends_on 'gsound'
+  depends_on 'librsvg'
+  depends_on 'libgnome_games_support'
+
+  def self.build
+    system "env CFLAGS='-fuse-ld=lld' CXXFLAGS='-fuse-ld=lld' \
+            meson #{CREW_MESON_LTO_OPTIONS} builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    @_wrapper = <<~EOF
+      #!/bin/sh
+      app="#{@_app}"
+      
+      WAYLAND_DISPLAY=wayland-0
+      GDK_BACKEND=wayland
+      CLUTTER_BACKEND=wayland
+      exec ${app}.elf "$@"
+    EOF
+    File.write(@_app, @_wrapper)
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/#{@_app}", "#{CREW_DEST_PREFIX}/bin/#{@_app}.elf"
+    FileUtils.install @_app, "#{CREW_DEST_PREFIX}/bin/#{@_app}", mode: 0755
+  end
+
+  def self.postinstall
+    system 'gdk-pixbuf-query-loaders --update-cache'
+    system "glib-compile-schemas #{CREW_PREFIX}/share/glib-2.0/schemas"
+  end
+end
