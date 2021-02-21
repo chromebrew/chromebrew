ea1e7eea8cd3bfe45c2be630abf78380c0c091fa
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-21 16:37:45 -0500

    Bump version

--- packages/libgdiplus.rb
+++ packages/libgdiplus.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libgdiplus < Package
   description 'libgdiplus is the mono library that provides a GDI+-compatible API on non-windows operating systems.'
   homepage 'https://www.mono-project.com/docs/gui/libgdiplus/'
-  version '6.0.5'
+  version '6.0.5-1'
   compatibility 'all'
   source_url 'https://download.mono-project.com/sources/libgdiplus/libgdiplus-6.0.5.tar.gz'
   source_sha256 'b81e4e5cc3e4831b2945de08bef26eb1bdcd795aeaf8f971b221c51213a025ef'
