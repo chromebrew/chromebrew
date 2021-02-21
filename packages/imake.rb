227a9e4148f69cde4eaec32ae2f87678651a7953
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-21 16:38:21 -0500

    [Fix Package] imake (#5222)

--- packages/imake.rb
+++ packages/imake.rb
@@ -8,6 +8,9 @@ class Imake < Package
   source_url 'https://www.x.org/releases/individual/util/imake-1.0.8.tar.gz'
   source_sha256 '8178a09bfef33ad5f61cb5cb62283df7d3a5682f014507d2e7cfd922485a5c00'
 
+  depends_on 'xorg_cf_files'
+  depends_on 'gccmakedep'
+
   def self.build
     system "./configure #{CREW_OPTIONS} \
             --sysconfdir=#{CREW_PREFIX}/etc"
