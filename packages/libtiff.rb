d3fe76af3b8fadb702ef82984a1cd6ce252b657f
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-21 16:39:24 -0500

    [Update Package] libtiff (#5220)

--- packages/libtiff.rb
+++ packages/libtiff.rb
@@ -3,30 +3,32 @@ require 'package'
 class Libtiff < Package
   description 'LibTIFF provides support for the Tag Image File Format (TIFF), a widely used format for storing image data.'
   homepage 'http://www.libtiff.org/'
-  version '4.1.0'
+  version '4.2.0'
   compatibility 'all'
-  source_url 'https://download.osgeo.org/libtiff/tiff-4.1.0.tar.gz'
-  source_sha256 '5d29f32517dadb6dbcd1255ea5bbc93a2b54b94fbf83653b4d65c7d6775b8634'
+  source_url 'https://download.osgeo.org/libtiff/tiff-4.2.0.tar.gz'
+  source_sha256 'eb0484e568ead8fa23b513e9b0041df7e327f4ee2d22db5a533929dfc19633cb'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libtiff-4.1.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '130c8ef969b06b24f0fd67b4b28b8932a7c96f5326e502d760efaaf4bf7bc850',
-     armv7l: '130c8ef969b06b24f0fd67b4b28b8932a7c96f5326e502d760efaaf4bf7bc850',
-       i686: '596e5034c8bd7cbecd28d6aa00aa0919b97a1cdc27325b4260015047d761a5cf',
-     x86_64: 'c25ec510ec23d166ab0edfa20984b1df4325b82d7e72405d483d173480586910',
-  })
-
+  depends_on 'libx11'
+  depends_on 'libjpeg'
+  depends_on 'libwebp'
+  depends_on 'libdeflate'
+  depends_on 'imake' => :build
+  
   def self.build
-    system "./configure CFLAGS=' -fPIC' #{CREW_OPTIONS}"
+    system "env NOCONFIGURE=1 ./autogen.sh"
+    system "./configure #{CREW_OPTIONS} \
+            --with-x \
+            --enable-zlib \
+            --enable-mdi \
+            --enable-libdeflate \
+            --enable-pixarlog \
+            --enable-jpeg \
+            --enable-lzma \
+            --enable-zstd \
+            --enable-webp \
+            --enable-cxx"
     system 'make'
   end
-
-  depends_on 'libjpeg'
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
