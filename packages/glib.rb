631f9e23932ac0d87ba774461ea0f4d16d4a1902
satmandu <satadru@umich.edu>
2021-02-21 18:14:41 -0500

    Glib 2.67.4 + gtk3 binary add (#5230)
    * glib -> 2.67.4
    
    * gtk3 add binaries, lint

--- packages/glib.rb
+++ packages/glib.rb
@@ -3,22 +3,22 @@ require 'package'
 class Glib < Package
   description 'GLib provides the core application building blocks for libraries and applications written in C.'
   homepage 'https://developer.gnome.org/glib'
-  version '2.67.3'
+  version '2.67.4'
   compatibility 'all'
-  source_url 'https://download.gnome.org/sources/glib/2.67/glib-2.67.3.tar.xz'
-  source_sha256 'b8e5f7397bc399d8134f3cdab7323b2210dd202510cf62a517e00dcca3488b78'
+  source_url 'https://download.gnome.org/sources/glib/2.67/glib-2.67.4.tar.xz'
+  source_sha256 '8d87b962032dadfcae8df62d248aa91fed2c7a43faf2c6d8b9107eb6c50e5b14'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.3-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/glib-2.67.4-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '9d38ac3744e0ce1477d804a4404a841df8df298df1d639206f16830e0c5e9f20',
-     armv7l: '9d38ac3744e0ce1477d804a4404a841df8df298df1d639206f16830e0c5e9f20',
-       i686: '100eec0254ae2ffc34d3903da688f740bad38fec9ac3e443a2bb9c75d76337f3',
-     x86_64: 'f31c6a72ee5495d2121983ee475475f45b03918e7f01f5115c4047ff216c7b82'
+    aarch64: '7d15e68503124e82d4ad52755dda9ff290572c84d17fc7d56d6eebdb4e261ae3',
+     armv7l: '7d15e68503124e82d4ad52755dda9ff290572c84d17fc7d56d6eebdb4e261ae3',
+       i686: '9031f7b19185505460005ea15589a6e52f7cc5ace50760b9154772d1c8951244',
+     x86_64: '1d2e776143a13ea6b2e6b12709f72f4be628e55323dd39cea69ba0a7e92c43ca'
   })
 
   depends_on 'shared_mime_info'
