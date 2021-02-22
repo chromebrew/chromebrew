e7d077519b3a750abdcb9a90b2a6cc05d81242ff
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-02-22 10:40:44 +0800

    Update gsound.rb

--- packages/gsound.rb
+++ packages/gsound.rb
@@ -6,11 +6,10 @@ class Gsound < Package
   @_app = 'gsound'
   @_ver = '1.0'
   @_patch = '.2'
-  @_fullver = @_ver + @_patch
-  version @_fullver
-  source_url "https://download.gnome.org/sources/#{@_app}/#{@_ver}/#{@_app}-#{@_fullver}.tar.xz"
-  source_sha256 `curl -Ls https://download.gnome.org/sources/#{@_app}/#{@_ver}/#{@_app}-#{@_fullver}.sha256sum |\
-                 tail -n1 | cut -d ' ' -f1`.chomp
+  @_url = "https://download.gnome.org/sources/#{@_app}/#{@_ver}/#{@_app}-#{@_ver}#{@_patch}"
+  version @_ver + @_patch
+  source_url "#{@_url}.tar.xz"
+  source_sha256 `curl -Ls #{@_url}.sha256sum | tail -n1 | cut -d ' ' -f1`.chomp
   
   depends_on 'gobject_introspection'
   depends_on 'libcanberra'
