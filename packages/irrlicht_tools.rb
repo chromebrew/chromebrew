9989f2ccfb05154a34de96396de27557c8e8d589
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-23 18:32:19 -0500

    [New Package] irrlicht_tools

---
+++ packages/irrlicht_tools.rb
@@ -0,0 +1,58 @@
+require 'package'
+
+class Irrlicht_tools < Package
+  description 'An open source realtime 3D engine written in C++ — Tools'
+  homepage 'http://irrlicht.sourceforge.net/'
+  version '1.8.4'
+  compatibility 'all'
+  source_url 'https://downloads.sourceforge.net/irrlicht/irrlicht-1.8.4.zip'
+  source_sha256 'f42b280bc608e545b820206fe2a999c55f290de5c7509a02bdbeeccc1bf9e433'
+  
+  depends_on 'irrlicht'
+  depends_on 'dos2unix' => :build
+  
+  def self.patch
+    system "curl -#LO https://dev.gentoo.org/~mgorny/dist/irrlicht-1.8.4-patchset.tar.bz2"
+    @sha256sums = <<~EOF
+      03b6a5a8a98191f8efe6f7ccd5c957bbf2c4bf15ac6cdc987655792c8baff454  irrlicht-1.8.4-patchset.tar.bz2
+    EOF
+    IO.write("sha256sums", @sha256sums)
+    system "sha256sum -c sha256sums"
+    system "sed -i 's:\.\./\.\./media:../media:g' $(grep -rl '\.\./\.\./media' examples)"
+    system "sed -i 's/\r$//' source/Irrlicht/COSOperator.cpp"
+    system "dos2unix include/IrrCompileConfig.h"
+    system "tar xvf irrlicht-1.8.4-patchset.tar.bz2"
+    system "for patch in irrlicht-1.8.4-patchset/*; do patch -p 1 -i ${patch}; done"
+    system "sed -i 's:-I/usr/include/freetype2/:-I#{CREW_PREFIX}/include/freetype2:g' $(find -name Makefile)"
+    system "sed -i 's:-I/usr/X11R6/include:-I#{CREW_PREFIX}/X11R6/include:g' $(find -name Makefile)"
+    system "sed -i 's:-L/usr/X11R6/lib$(LIBSELECT):-L/usr/local/X11R6/lib$(LIBSELECT):g' $(find -name Makefile)"
+    system "sed -i 's:-I/usr/include:-I#{CREW_PREFIX}/include:g' $(find -name Makefile)"
+  end
+  
+  def self.build
+    Dir.chdir 'tools/FileToHeader' do
+      system 'make'
+    end
+    Dir.chdir 'tools/GUIEditor' do
+      system 'make'
+    end
+    Dir.chdir 'tools/IrrFontTool/newFontTool' do
+      system 'make'
+    end
+    Dir.chdir 'tools/MeshConverter' do
+      system 'make'
+    end
+  end
+
+  def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/libexec/irrlicht"
+    Dir.chdir 'bin' do
+      FileUtils.cp Dir.glob("Linux/*"), '.'
+      FileUtils.rm_r Dir.glob("Win*")
+      FileUtils.rm_r "MacOSX"
+      FileUtils.rm_r "Linux"
+      FileUtils.rm "readme.txt"
+      FileUtils.cp Dir.glob("./*"), "#{CREW_DEST_PREFIX}/libexec/irrlicht/"
+    end
+  end
+end
