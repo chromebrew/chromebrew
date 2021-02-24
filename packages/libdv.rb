1291591d4521604455ca8955b7693237255a3dd8
satmandu <satadru@umich.edu>
2021-02-24 13:07:17 -0500

    libdv: new crew_options rebuild (#5271)
    * new creq_options rebuild
    
    * Update libdv.rb

--- packages/libdv.rb
+++ packages/libdv.rb
@@ -3,25 +3,26 @@ require 'package'
 class Libdv < Package
   description 'The Quasar DV codec libdv is a software codec for DV video'
   homepage 'http://libdv.sourceforge.net/'
-  version '1.0.0'
+  version '1.0.0-1'
   compatibility 'all'
   source_url 'https://downloads.sourceforge.net/libdv/libdv-1.0.0.tar.gz'
   source_sha256 'a305734033a9c25541a59e8dd1c254409953269ea7c710c39e540bd8853389ba'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libdv-1.0.0-1-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'f86306f6a491fce2a0b18005e8c28aeac074e441fef41d43f60abecd9a09b2e0',
-     armv7l: 'f86306f6a491fce2a0b18005e8c28aeac074e441fef41d43f60abecd9a09b2e0',
-       i686: '3f55b05602d407a1649ea5771114634fe8b08496f854471050496434ce2807da',
-     x86_64: 'd62fb6a0fd56d1b7c056f5cbb9c774c769a435660c0d50b4fa024dddc4f3689d'
+    aarch64: '74afbcfd3c2ae2209ca1d65b08590504f467fa319a686363e58f5f47c3dbec60',
+     armv7l: '74afbcfd3c2ae2209ca1d65b08590504f467fa319a686363e58f5f47c3dbec60',
+       i686: '28bde5081c65a611ca446c0ed3bcd66ab7dcf296511ba4b617c64a129019ca48',
+     x86_64: '626912b6bf0fd3cbc315b913b5b80b0d707573c9a44e69680b6fb53213b14cfc'
   })
 
   depends_on 'popt'
+  depends_on 'libxv'
 
   def self.build
     system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto'  LDFLAGS='-flto=auto' \
