10a5aeeb33ffcaa346ffb837e0f7ac68976a1142
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-02-23 21:27:34 -0500

    [New Package] civetweb (#5266)
    * [New Package] civetweb

---
+++ packages/civetweb.rb
@@ -0,0 +1,34 @@
+require 'package'
+
+class Civetweb < Package
+  description 'Embedded C/C++ web server'
+  homepage 'https://github.com/civetweb/civetweb/'
+  version '1.13'
+  compatibility 'all'
+  source_url 'https://github.com/civetweb/civetweb/archive/v1.13.tar.gz'
+  source_sha256 'a7ccc76c2f1b5f4e8d855eb328ed542f8fe3b882a6da868781799a98f4acdedc'
+  
+  def self.build
+    Dir.mkdir "builddir"
+    Dir.chdir "builddir" do
+      system "cmake -G 'Ninja' #{CREW_CMAKE_OPTIONS} \
+		          -DBUILD_TESTING=OFF \
+		          -DBUILD_SHARED_LIBS=ON \
+		          -DCIVETWEB_BUILD_TESTING=OFF \
+		          -DCIVETWEB_ENABLE_LUA=OFF \
+		          -DCIVETWEB_ENABLE_DUKTAPE=OFF \
+		          -DCIVETWEB_ENABLE_CXX=ON \
+		          -DCIVETWEB_ENABLE_SERVER_EXECUTABLE=ON \
+		          -DCIVETWEB_ENABLE_SSL=ON .."
+      system "ninja"
+    end
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+  end
+
+  def self.check
+    system "ninja -C builddir test"
+  end
+end
