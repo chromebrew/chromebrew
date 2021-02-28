5c9d20def73b43113e6ad8595b4ae3f30e2b239b
satmandu <satadru@umich.edu>
2021-02-28 14:40:11 -0500

    harcode default ca_cert location in curl, wget (#5299)
    * Add ca_cert location to build
    
    * add ca_certificates location

--- packages/wget.rb
+++ packages/wget.rb
@@ -4,22 +4,22 @@ class Wget < Package
   description 'GNU Wget is a free software package for retrieving files using HTTP, HTTPS, FTP and FTPS.'
   homepage 'https://www.gnu.org/software/wget/'
   @_ver = '1.21.1'
-  version @_ver
+  version "#{@_ver}-1"
   compatibility 'all'
   source_url "https://ftpmirror.gnu.org/wget/wget-#{@_ver}.tar.lz"
   source_sha256 'db9bbe5347e6faa06fc78805eeb808b268979455ead9003a608569c9d4fc90ad'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wget-1.21.1-1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: 'c365e53c36d6927b761ed7c1293f2506e289d13694fa5de9882087e5444cf185',
-      armv7l: 'c365e53c36d6927b761ed7c1293f2506e289d13694fa5de9882087e5444cf185',
-        i686: '399d68710cf8bb0924cad818f85bb38c2ba3d68d2a3dff494f21e9727a7069fe',
-      x86_64: '583bd6f6a1f1aacbd0faafcd5d9ffa6405825ea2c0d41aa7bd2e134a8a67f6c4',
+  binary_sha256({
+    aarch64: '407cb0b92209fe7c9f967a9b2b68134ba747bbbbd622328a89353bad524739c4',
+     armv7l: '407cb0b92209fe7c9f967a9b2b68134ba747bbbbd622328a89353bad524739c4',
+       i686: '342544c5ac5b6634edc3323c94a94d16a3cd8e5c7d4d695e5481e51d0608dc52',
+     x86_64: 'bd779500c54435989731f1f40918a7bd58065f6da5ea562f332412fa88dc833d'
   })
 
   depends_on 'gnutls'
@@ -27,17 +27,27 @@ class Wget < Package
   depends_on 'libxdmcp'
   depends_on 'libmetalink'
   depends_on 'pcre2'
-
+  depends_on 'ca_certificates'
+  
   def self.build
-    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' ./configure #{CREW_OPTIONS} --sysconfdir=#{CREW_PREFIX}/etc"
+    raise StandardError, 'Please remove libiconv before building.' if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
+
+    system "env CFLAGS='-pipe -flto=auto' \
+      CXXFLAGS='-pipe -flto=auto' \
+      LDFLAGS='-flto=auto' \
+      ./configure #{CREW_OPTIONS} \
+      --sysconfdir=#{CREW_PREFIX}/etc\
+      --with-metalink \
+      --without-libiconv-prefix"
     system 'make'
   end
 
   def self.check
-    #system 'make', 'check'
+    # system 'make', 'check'
   end
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system "echo \"ca_directory=#{CREW_PREFIX}/etc/ssl/certs\" >> #{CREW_DEST_PREFIX}/etc/wgetrc"
   end
 end
