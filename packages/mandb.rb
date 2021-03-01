11d77689a767905739836fab747d98cbdcebdbf0
satmandu <satadru@umich.edu>
2021-03-01 15:01:10 -0500

    Update mandb.rb

--- packages/mandb.rb
+++ packages/mandb.rb
@@ -3,22 +3,23 @@ require 'package'
 class Mandb < Package
   description 'mandb is used to initialize or manually update index database caches that are usually maintained by man.'
   homepage 'http://man-db.nongnu.org/'
-  version '2.9.3'
+  @_ver = '2.9.4'
+  version @_ver
   compatibility 'all'
-  source_url 'https://download.savannah.gnu.org/releases/man-db/man-db-2.9.3.tar.xz'
-  source_sha256 'fa5aa11ab0692daf737e76947f45669225db310b2801a5911bceb7551c5597b8'
+  source_url "https://download.savannah.gnu.org/releases/man-db/man-db-#{@_ver}.tar.xz"
+  source_sha256 'b66c99edfad16ad928c889f87cf76380263c1609323c280b3a9e6963fdb16756'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.3-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mandb-2.9.4-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '8bad5f96c6b1259cf8c13e72759bf13ddbd74cb76d3ea03a330b9b3b2da7bce6',
-     armv7l: '8bad5f96c6b1259cf8c13e72759bf13ddbd74cb76d3ea03a330b9b3b2da7bce6',
-       i686: '336d1b5cd1b6d0abfafd0b9c12e925ccd3e239acca9c55b09835fe9ada917736',
-     x86_64: 'e6023a153a255916cda39d0598c8cfa7b38345b6c69d5bf61babd54fec66f624',
+  binary_sha256({
+    aarch64: 'ccd36d83dc2dcb04d003a79fd503273ebd06a77d4da618f7033d537fda537d4e',
+     armv7l: 'ccd36d83dc2dcb04d003a79fd503273ebd06a77d4da618f7033d537fda537d4e',
+       i686: '97a79a235a9ab3c3f077ddf462a79d9d17ff3ff6cd35145321d790253775387f',
+     x86_64: '30939c206bd1adc66a33a8157d749b288a3726a995a8eb318c057104807c138d'
   })
 
   depends_on 'gdbm'
@@ -26,11 +27,24 @@ class Mandb < Package
   depends_on 'libpipeline'
 
   def self.build
-    system './configure',
-      "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}",
-      "--with-systemdtmpfilesdir=#{CREW_PREFIX}/etc/tmpfiles.d", # we can't write to /usr/lib/tmpfiles.d
-      '--disable-cache-owner',                                   # we can't create the user 'man'
-      "--with-pager=#{CREW_PREFIX}/bin/most"                     # the pager is not at the default location
+    raise StandardError, 'Please remove libiconv before building.' if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
+
+    # we can't write to /usr/lib/tmpfiles.d
+    # we can't create the user 'man'
+    # the pager is not at the default location
+    system './configure --help'
+    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
+      LDFLAGS='-flto=auto' \
+      ./configure \
+      #{CREW_OPTIONS} \
+      --with-systemdtmpfilesdir=#{CREW_PREFIX}/etc/tmpfiles.d \
+      --disable-cache-owner \
+      --disable-setuid \
+      --enable-automatic-create \
+      --enable-static \
+      --without-libiconv-prefix \
+      --disable-rpath \
+      --with-pager=#{CREW_PREFIX}/bin/most"
     system 'make'
   end
 
@@ -48,14 +62,23 @@ class Mandb < Package
       'man/man8/accessdb.man8',
       'man/man8/mandb.man8',
       'tools/chconfig'
-    ].each { |file|
+    ].each do |file|
       system "sed -i 's,/var/cache/man,#{CREW_PREFIX}/cache/man,g' #{file}"
-    }
+    end
     system "sed -i 's,/usr/share/man,#{CREW_PREFIX}/share/man,g' tools/chconfig"
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system "sed -i 's,/usr/share/man,#{CREW_PREFIX}/share/man,g' #{CREW_DEST_PREFIX}/etc/man_db.conf"
+    system "sed -i 's,/usr/man,#{CREW_PREFIX}/share/man,g' #{CREW_DEST_PREFIX}/etc/man_db.conf"
   end
 
   def self.postinstall
-    system 'mandb -c'
+    system "env MANPATH=#{CREW_MAN_PREFIX} mandb -psc"
+    pager_in_bashrc = `grep -c "PAGER" ~/.bashrc || true`
+    unless pager_in_bashrc.to_i.positive?
+      puts 'Putting PAGER=most in ~/.bashrc'.lightblue
+      system "echo 'export PAGER=most' >> ~/.bashrc"
+      puts 'To complete the installation, execute the following:'.orange
+      puts 'source ~/.bashrc'.orange
+    end
   end
 end
