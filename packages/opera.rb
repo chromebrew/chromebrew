c8148028bb3ca9079a7d0cf9069b606c3d45ba36
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-03-02 00:03:02 +0800

    Update opera.rb

--- packages/opera.rb
+++ packages/opera.rb
@@ -30,16 +30,14 @@ class Opera < Package
     ENV['CREW_NOT_STRIP'] = '1'
     
     # since opera put the executable to library, we need to link it to CREW_PREFIX
-    # remove old symlink which link to /usr/lib/x86_64-linux-gnu/opera/opera
-    FileUtils.rm_rf 'bin/opera'
-    FileUtils.ln_s "#{CREW_LIB_PREFIX}/opera/opera", 'bin/opera'
+    FileUtils.ln_sf "#{CREW_LIB_PREFIX}/opera/opera", 'bin/opera'
       
     # place all stuff to lib64 instead of lib
-    FileUtils.mv 'lib/x86_64-linux-gnu/', 'lib64/'
+    FileUtils.mv 'lib/x86_64-linux-gnu/', 'share/'
     FileUtils.rm_rf 'lib/'
     
     FileUtils.mkdir_p CREW_DEST_PREFIX
     FileUtils.mv Dir.glob('*'), CREW_DEST_PREFIX
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/opera", "#{CREW_DEST_PREFIX}/bin/x-www-browser" unless File.exist?("#{CREW_PREFIX}/bin/x-www-browser")
+    FileUtils.ln_s "#{CREW_PREFIX}/share/opera/opera", "#{CREW_DEST_PREFIX}/bin/x-www-browser" unless File.exist?("#{CREW_PREFIX}/bin/x-www-browser")
   end
 end
