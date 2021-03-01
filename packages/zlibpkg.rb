f89f36983dd4ac8165d62943889ce1e24dcd8a86
satmandu <satadru@umich.edu>
2021-03-01 17:34:51 -0500

    Bind9 -> 9.17.10 & Binutils 2.36.1 , Libuv 1.39.0, zlibpkg lto (#5308)
    * rebuild w/ lto
    
    * libuv -> 1.39.0
    
    * bind -> 9.17.10
    
    * binutils -> 2.36.1

--- packages/zlibpkg.rb
+++ packages/zlibpkg.rb
@@ -3,29 +3,31 @@ require 'package'
 class Zlibpkg < Package
   description 'zlib is a massively spiffy yet delicately unobtrusive compression library.'
   homepage 'http://www.zlib.net/'
-  version '1.2.11-3'
+  @_ver = '1.2.11'
+  version "#{@_ver}-4"
   compatibility 'all'
-  source_url 'http://www.zlib.net/zlib-1.2.11.tar.gz'
+  source_url "http://www.zlib.net/zlib-#{@_ver}.tar.gz"
   source_sha256 'c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-3-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-3-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-3-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-3-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/zlibpkg-1.2.11-4-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: '32adce95047eab53e31ee93794255159cc839ef5cdac1da46ed0ff5a15812df4',
-      armv7l: '32adce95047eab53e31ee93794255159cc839ef5cdac1da46ed0ff5a15812df4',
-        i686: 'd74959077aadf86030e2acd969923d23fa042d67bf5f1fe1cc91af09816f70e6',
-      x86_64: '8fab32aaf18449efe4140af1cd2af1abaf7543314524508b888d7ef4019d581f',
+  binary_sha256({
+    aarch64: '4b9c8b7ae88784f0c2744f268cc31afa1bb467f2ea964a7cb601509f12fceee7',
+     armv7l: '4b9c8b7ae88784f0c2744f268cc31afa1bb467f2ea964a7cb601509f12fceee7',
+       i686: 'fdd119fa2214635decf1a926a762cffc6e76a9ee4986d3a601c2afee05b150b8',
+     x86_64: '16c5a9c26c5ba526ae48dfaf277bb31c471bde9982747035f3e7594f4da710e8'
   })
-
 
   def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
+    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
+      LDFLAGS='-flto=auto' \
+      ./configure \
+      --prefix=#{CREW_PREFIX} \
+      --libdir=#{CREW_LIB_PREFIX}"
     system 'make'
   end
 
