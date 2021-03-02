7bd8c435a10a6879c4b6bf74ade7b5fd5bb0ce3d
satmandu <satadru@umich.edu>
2021-03-02 15:45:24 -0500

    Fix location of files which go in dbus-1/system.d directory in multiple package (#5311)
    * Update libqmi.rb
    
    * Update libmbim.rb
    
    * Update geoclue.rb
    
    * Update avahi.rb
    
    * Update pulseaudio.rb
    
    * Update flatpak.rb
    
    * use specific working commit for git checkout

--- packages/geoclue.rb
+++ packages/geoclue.rb
@@ -3,22 +3,23 @@ require 'package'
 class Geoclue < Package
   description 'Modular geoinformation service built on the D-Bus messaging system'
   homepage 'https://www.freedesktop.org/wiki/Software/GeoClue/'
-  version '2.5.7'
+  @_ver = '2.5.7'
+  version "#{@_ver}-1"
   compatibility 'all'
-  source_url 'https://gitlab.freedesktop.org/geoclue/geoclue/-/archive/2.5.7/geoclue-2.5.7.tar.bz2'
+  source_url "https://gitlab.freedesktop.org/geoclue/geoclue/-/archive/#{@_ver}/geoclue-#{@_ver}.tar.bz2"
   source_sha256 '6cc7dbe4177b4e7f3532f7fe42262049789a3cd6c55afe60a3564d7394119c27'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/geoclue-2.5.7-1-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'ca4c050657f094f10ccf46f4cba13d309d0757830c4be084e8190d72e3ca8949',
-     armv7l: 'ca4c050657f094f10ccf46f4cba13d309d0757830c4be084e8190d72e3ca8949',
-       i686: 'cb9adf521670add9b3d4d0e4491daa10965ab2638f0b5da89ea81a8df0870688',
-     x86_64: '85a1879573e68dea80192fade6cdd52934ea0b2c02c22e959ec3a88b029f144b'
+    aarch64: '8208222a9240c0d90afaac07a4111ad27e5b02f9ea3ba55211278440f065dad2',
+     armv7l: '8208222a9240c0d90afaac07a4111ad27e5b02f9ea3ba55211278440f065dad2',
+       i686: '17377ae86e1205882121acc6fd1beee159ff0a4d090b2081dfa6582d73248ba2',
+     x86_64: '214403eb81cd51b173365be83577e79b3f30266941f54f8ceec7659565d3cba8'
   })
 
   depends_on 'libsoup'
@@ -33,9 +34,10 @@ class Geoclue < Package
   def self.build
     system "meson #{CREW_MESON_LTO_OPTIONS} \
       -Dsystemd=disabled \
-      -D3g-source=false \
+      -Ddbus-sys-dir=#{CREW_PREFIX}/share/dbus-1 \
+      -D3g-source=true \
       -Dcdma-source=false \
-      -Dmodem-gps-source=false \
+      -Dmodem-gps-source=true \
       builddir"
     system 'meson configure builddir'
     system 'ninja -C builddir'
