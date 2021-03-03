10eec789160fc72f49e024db4d1bff2e953de478
satmandu <satadru@umich.edu>
2021-03-03 16:06:36 -0500

    Update nasm.rb

--- packages/nasm.rb
+++ packages/nasm.rb
@@ -3,30 +3,34 @@ require 'package'
 class Nasm < Package
   description 'The Netwide Assembler'
   homepage 'https://www.nasm.us/'
-  version '2.14.02'
+  @_ver = '2.15.05'
+  version @_ver
   compatibility 'all'
-  source_url 'https://www.nasm.us/pub/nasm/releasebuilds/2.14.02/nasm-2.14.02.tar.xz'
-  source_sha256 'e24ade3e928f7253aa8c14aa44726d1edf3f98643f87c9d72ec1df44b26be8f5'
+  source_url 'https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.xz'
+  source_sha256 '3caf6729c1073bf96629b57cee31eeb54f4f8129b01902c73428836550b30a3f'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.14.02-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.14.02-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.14.02-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.14.02-chromeos-x86_64.tar.xz',
+     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.15.05-chromeos-armv7l.tar.xz',
+      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.15.05-chromeos-armv7l.tar.xz',
+        i686: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.15.05-chromeos-i686.tar.xz',
+      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/nasm-2.15.05-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: 'f63ec3e4cbfa87aaccf94c33c608324bfca004f798502485c1b04994f73a47ae',
-     armv7l: 'f63ec3e4cbfa87aaccf94c33c608324bfca004f798502485c1b04994f73a47ae',
-       i686: '86bb6fa809e9b44a6677da1de4bc1efbcad9fd22450f092ef146f524c7c51eca',
-     x86_64: 'd7ad212d782c300a90293b2cbd389c8b343e2f6c9a36422f2e1ce8fca75e3988',
+     aarch64: '341d485bb248ed27fa1deaf10d361ab13eb9ac8332298cb45d3c2199e01aca25',
+      armv7l: '341d485bb248ed27fa1deaf10d361ab13eb9ac8332298cb45d3c2199e01aca25',
+        i686: 'e78c9d471c116b2ced338a800b84a01346e964b377b0fd66f260ef517da8e801',
+      x86_64: '920e5d3591a66153b45d68c3e67bca8ef74cfc6cf3d71e2c4c57c5624a2b5111',
   })
 
   def self.build
-    system "./configure", "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}"
+    system "env CFLAGS='-flto=auto' \
+      CXXFLAGS='-flto=auto' LDFLAGS='-flto=auto' \
+      ./configure \
+      #{CREW_OPTIONS}"
     system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
