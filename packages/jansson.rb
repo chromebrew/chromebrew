19aafc2c2adbf1762bd7f04038db67eef6144024
satmandu <satadru@umich.edu>
2021-03-03 11:41:10 -0500

    Add Network Manager Prereqs (#5313)
    * Create libnewt.rb
    
    * Update jansson.rb
    
    * Create libxcrypt.rb
    
    * Create gpm.rb
    
    * Create mobile_broadband_provider_info.rb
    
    * Update libnewt.rb
    
    * Update mobile_broadband_provider_info.rb
    
    * Update gpm.rb
    
    * remove gpm.sh

--- packages/jansson.rb
+++ packages/jansson.rb
@@ -3,29 +3,30 @@ require 'package'
 class Jansson < Package
   description 'Jansson is a C library for encoding, decoding and manipulating JSON data.'
   homepage 'http://www.digip.org/jansson/'
-  version '2.12'
+  @_ver = '2.13.1'
+  version @_ver
   compatibility 'all'
-  source_url 'https://github.com/akheron/jansson/archive/v2.12.tar.gz'
-  source_sha256 '76260d30e9bbd0ef392798525e8cd7fe59a6450c54ca6135672e3cd6a1642941'
+  source_url "https://github.com/akheron/jansson/archive/v#{@_ver}.tar.gz"
+  source_sha256 'f22901582138e3203959c9257cf83eba9929ac41d7be4a42557213a22ebcc7a0'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.12-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.12-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.12-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.12-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.13.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.13.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.13.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/jansson-2.13.1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '1268da5b1e8fb780fa300e2435992978ad3ca1dca671fcb157591795b219cee3',
-     armv7l: '1268da5b1e8fb780fa300e2435992978ad3ca1dca671fcb157591795b219cee3',
-       i686: '1a5b9a9099cb64e5b097090bc17a0ae3b225c66815dd06f3db0a32d4adedac03',
-     x86_64: '3cb0754c45a3997f7c41cc0d941964c4ec55f12e28792712ececc2d5f41a42e4',
+  binary_sha256({
+    aarch64: '934cd7813f9a0c8ad07c967f0d65de05a545d697cc10c6a270c09e6dbbdf30df',
+     armv7l: '934cd7813f9a0c8ad07c967f0d65de05a545d697cc10c6a270c09e6dbbdf30df',
+       i686: '3a4c91fbfd2b20b52174e9dfb28b16d1ed22767af66379711e1da77ac9ec9e86',
+     x86_64: '224290a4b9c5e4361d7a41171745ec02211281582af9a6f7aa0b7de47a06b7de'
   })
 
   def self.build
     system 'autoreconf -i'
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
+    system "env CFLAGS='-flto=auto' \
+      CXXFLAGS='-flto=auto' LDFLAGS='-flto=auto' \
+      ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
