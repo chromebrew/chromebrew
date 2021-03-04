632c5648145fb2d7fae98e2fcc54ab5e5710d271
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-03-04 19:38:41 +0800

    Update torbrowser.rb

--- packages/torbrowser.rb
+++ packages/torbrowser.rb
@@ -9,15 +9,15 @@ class Torbrowser < Package
 
   case ARCH
   when 'x86_64'
-    @_url = "https://www.torproject.org/dist/torbrowser/#{@_ver}/"
+    @_url = "https://www.torproject.org/dist/torbrowser/#{@_ver}"
     @_name = "tor-browser-linux64-#{@_ver}_en-US.tar.xz"
-    source_url @_url + @_name
-    source_sha256 `curl -Ls #{@_url}/sha256sums-signed-build.txt | grep '#{@_name}' | cut -d' ' -f1`.chomp
+    source_url "#{@_url}/#{@_name}"
+    source_sha256 `curl -Ls '#{@_url}/sha256sums-signed-build.txt' | grep '#{@_name}' | cut -d' ' -f1`.chomp
     depends_on 'gtk3'
     depends_on 'sommelier'
   end
 
-  def self.patch
+  def self.build
     @tor = <<~EOF
       #!/bin/sh -e
       cd #{CREW_PREFIX}/share/
@@ -29,14 +29,32 @@ class Torbrowser < Package
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
-    FileUtils.mv 'Browser/', "#{CREW_DEST_PREFIX}/share/Browser"
+    FileUtils.mv 'Browser/', "#{CREW_DEST_PREFIX}/share/"
     FileUtils.mv 'start-tor-browser.desktop', "#{CREW_DEST_PREFIX}/share"
     FileUtils.install 'tor', "#{CREW_DEST_PREFIX}/bin/tor", mode: 0755
   end
 
   def self.postinstall
     puts
+    puts 'Set Tor as your default browser? [Y/n]: '
+    case STDIN.gets.chomp
+    when "\n", 'Y', 'y', 'yes'
+      Dir.chdir("#{CREW_PREFIX}/bin") do
+        FileUtils.ln_sf 'tor', 'x-www-browser'
+      end
+    else
+      puts 'No change have been made.'
+      puts
+    end
+    
+    puts
     puts "Type 'tor' to get started.".lightblue
     puts
+  end
+  
+  def self.remove
+    Dir.chdir("#{CREW_PREFIX}/bin") do
+      FileUtils.rm_rf 'x-www-browser' if File.realpath('x-www-browser') == "#{CREW_PREFIX}/bin/tor"
+    end    
   end
 end
