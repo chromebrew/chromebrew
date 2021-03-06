9700988262b8bc20c862d14792e0ff9316cda220
satmandu <satadru@gmail.com>
2021-03-05 19:52:29 -0500

    Update gnome_shell.rb

--- packages/gnome_shell.rb
+++ packages/gnome_shell.rb
@@ -8,7 +8,7 @@ class Gnome_shell < Package
   homepage 'https://wiki.gnome.org/Projects/GnomeShell'
   version '40.beta'
   compatibility 'x86_64 , aarch64, armv7l'
-  source_url 'https://github.com/GNOME/gnome-shell/archive/40.beta.tar.gz'
+  source_url "https://github.com/GNOME/gnome-shell/archive/#{version}.tar.gz"
   source_sha256 'fd929eba43ca52c1f620460be02de0c116ea7ba792e504329eb85c83e5800ada'
 
   binary_url({
