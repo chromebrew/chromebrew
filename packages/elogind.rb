b9a666420709b274be189a5c295e9d59d0d10b28
satmandu <satadru@umich.edu>
2021-03-06 10:33:08 -0500

    Add updated gnome packages, and unstackguarded packages (#5330)
    * Add updated packages
    
    * Remove merged patch
    
    * Remove old compat symlinks
    
    * remove i686 detritus
    
    * Update gnome_session.rb
    
    * Update gnome_shell.rb
    
    * Update gnome_shell.rb
    
    * Update gnome_tweaks.rb
    
    * Update mutter.rb
    
    * more package tweaks
    
    * fix sha256 hash
    
    * only this url works for building version 1.0.32

--- packages/elogind.rb
+++ packages/elogind.rb
@@ -4,27 +4,26 @@ class Elogind < Package
   description 'Standalone systemd-logind fork'
   homepage 'https://github.com/elogind/elogind'
   @_ver = '246.10'
-  version @_ver
+  version "#{@_ver}-1"
   compatibility 'all'
   source_url "https://github.com/elogind/elogind/archive/v#{@_ver}.tar.gz"
-  source_sha256 '9a6285c077f3345c69fc21d1902226966ef6dc6e40d0eba259ee882c5b766eca'
+  source_sha256 'c490dc158c8f5bca8d00ecfcc7ad5af24d1c7b9e59990a0b3b1323996221a922'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/elogind-246.10-1-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '757fc7dc87b8699b51abdbc3b5b7e7dc505cc3bf1934aabe356d7bfff6712d2d',
-     armv7l: '757fc7dc87b8699b51abdbc3b5b7e7dc505cc3bf1934aabe356d7bfff6712d2d',
-       i686: '8e1c7f42275654fe1a3010c8631be0b5095f6bdcad3020f5ed4b3dd68289eee7',
-     x86_64: '186e7ec40bded8ae9624b8dd3ff90880cd6d0de98016413745d2f672a43cbfa7'
+    aarch64: '7bf79ff8159f5d90f36c8680f70bb4ad8b36276c0600e4aac664ce79b8808b6e',
+     armv7l: '7bf79ff8159f5d90f36c8680f70bb4ad8b36276c0600e4aac664ce79b8808b6e',
+       i686: '2a4c82de76b21a2fd0ea1e2b53c96ea5e90650d012fe5509e48f837258771648',
+     x86_64: 'a12c145e41d5f8691731bf29a3d832f7974fea3e887677b649dbc43ea5888a9a'
   })
 
   depends_on 'eudev'
   depends_on 'libcap'
-  depends_on 'libseccomp'
   depends_on 'linux_pam' # For _pam_macros.h
   depends_on 'dbus'
   depends_on 'docbook_xsl'
@@ -36,19 +35,23 @@ class Elogind < Package
 
   def self.build
     system "meson #{CREW_MESON_LTO_OPTIONS} \
-            -Drootprefix=#{CREW_PREFIX} \
-            -Dsysconfdir=#{CREW_PREFIX}/etc \
-            --strip \
-            -Dcgroup-controller=elogind \
-            -Dman=true \
-            -Ddefault-hierarchy=legacy \
-            -Ddefault-kill-user-processes=false \
-            -Dhalt-path=/sbin/halt \
-            -Drootlibdir=#{CREW_PREFIX}/#{ARCH_LIB} \
-            -Drootlibexecdir=#{CREW_PREFIX}/libexec/elogind \
-            -Drootbindir=#{CREW_PREFIX}/bin \
-            -Dreboot-path=/sbin/reboot \
-            builddir"
+      -Dc_args='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
+      -Dc_link_args='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
+      -Dcpp_args='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
+      -Dcpp_link_args='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
+      -Drootprefix=#{CREW_PREFIX} \
+      -Dsysconfdir=#{CREW_PREFIX}/etc \
+      --strip \
+      -Dcgroup-controller=elogind \
+      -Dman=true \
+      -Ddefault-hierarchy=legacy \
+      -Ddefault-kill-user-processes=false \
+      -Dhalt-path=/sbin/halt \
+      -Drootlibdir=#{CREW_PREFIX}/#{ARCH_LIB} \
+      -Drootlibexecdir=#{CREW_PREFIX}/libexec/elogind \
+      -Drootbindir=#{CREW_PREFIX}/bin \
+      -Dreboot-path=/sbin/reboot \
+      builddir"
     system 'ninja -C builddir'
   end
 
--- packages/tk.rb
+++ packages/tk.rb
@@ -4,22 +4,23 @@ class Tk < Package
   description 'Tk is a graphical user interface toolkit that takes developing desktop applications to a higher level than conventional approaches.'
   homepage 'http://www.tcl.tk/'
   @_ver = '8.6.11'
-  version @_ver
+  @_ver_prelastdot = @_ver.rpartition('.')[0]
+  version "#{@_ver}-1"
   compatibility 'all'
   source_url "https://downloads.sourceforge.net/project/tcl/Tcl/#{@_ver}/tk#{@_ver}-src.tar.gz"
   source_sha256 '5228a8187a7f70fa0791ef0f975270f068ba9557f57456f51eb02d9d4ea31282'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/tk-8.6.11-1-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'a8866147a504c56cd834f71e078d6ca080f52873e673cbc55c7a7939991c6d3b',
-     armv7l: 'a8866147a504c56cd834f71e078d6ca080f52873e673cbc55c7a7939991c6d3b',
-       i686: '8a4afe834eaf7fb966714513becbde55e10125e1c3018e2db61aabb5565526fe',
-     x86_64: 'dac36fa0385f8958676b71ee2dc10278fa7e5ebf7aa6ae3a3e04ea1b85fa9443'
+    aarch64: '45f352f6979be6011f5f1d4a8ee6caef842ae2d8579f2d3ae69bf361a41e320d',
+     armv7l: '45f352f6979be6011f5f1d4a8ee6caef842ae2d8579f2d3ae69bf361a41e320d',
+       i686: '13d38ebfee7bec6dc28f0785fed724d459b22e3dbf8e8f2c7483c05ee75e0c25',
+     x86_64: '05b1753d45c1820ec8f51e28fe523376efaf04589b93bcefd38fc83752587390'
   })
 
   depends_on 'xorg_lib'
@@ -28,18 +29,18 @@ class Tk < Package
   def self.build
     FileUtils.chdir('unix') do
       if ARCH == 'x86_64'
-        system "env CFLAGS='-flto=auto -pipe -fuse-ld=gold' \
-          CXXFLAGS='-flto=auto -pipe -fuse-ld=gold' \
-          LDFLAGS='-flto=auto' \
+        system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
           ./configure \
           #{CREW_OPTIONS} \
           --with-tcl=#{CREW_LIB_PREFIX} \
           --enable-threads \
           --enable-64bit"
       else
-        system "env CFLAGS='-flto=auto -pipe -fuse-ld=gold' \
-          CXXFLAGS='-flto=auto -pipe -fuse-ld=gold' \
-          LDFLAGS='-flto=auto' \
+        system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
           ./configure \
           #{CREW_OPTIONS} \
           --with-tcl=#{CREW_LIB_PREFIX} \
@@ -53,7 +54,7 @@ class Tk < Package
   def self.install
     FileUtils.chdir('unix') do
       system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-      FileUtils.ln_s "#{CREW_PREFIX}/bin/wish8.6", "#{CREW_DEST_PREFIX}/bin/wish"
+      FileUtils.ln_s "#{CREW_PREFIX}/bin/wish#{@_ver_prelastdot}", "#{CREW_DEST_PREFIX}/bin/wish"
     end
   end
 end
