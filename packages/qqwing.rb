9fbd2269fc442927c7d7e8f90e9f889bfdaa5541
supechicken <73414985+supechicken@users.noreply.github.com>
2021-03-08 22:28:24 +0800

    Update qqwing.rb

--- packages/qqwing.rb
+++ packages/qqwing.rb
@@ -3,10 +3,10 @@ require 'package'
 class Qqwing < Package
   description 'QQwing is a Sudoku puzzle generator and solver.'
   homepage 'https://qqwing.com/'
-  @_ver = '1.3.4'
-  version @_ver
-  source_url "https://qqwing.com/qqwing-#{@_ver}.tar.gz"
-  source_sha256 'dc5d61b4470862b190d437f8143d4090639c164d34461c4caa5c081c5c91e6cc'
+  version '1.3.4'
+  compatibility 'all'
+  source_url 'https://qqwing.com/qqwing-1.3.4.tar.gz'
+  source_sha256 '1753736c31feea0085f5cfac33143743204f8a7e66b81ccd17e249ecafba802f'
 
   def self.build
     system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto'  LDFLAGS='-flto=auto' \
