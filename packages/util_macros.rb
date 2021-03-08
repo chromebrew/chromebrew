5c4286bc5834aa30ac6f032082a1a0bf3b6f00c0
satmandu <satadru@gmail.com>
2021-03-08 16:57:21 -0500

    Update util_macros.rb

--- packages/util_macros.rb
+++ packages/util_macros.rb
@@ -10,16 +10,16 @@ class Util_macros < Package
   source_sha256 '0f812e6e9d2786ba8f54b960ee563c0663ddbe2434bf24ff193f5feab1f31971'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_util_macros-1.19.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_util_macros-1.19.3-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_util_macros-1.19.3-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/xorg_util_macros-1.19.3-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/util_macros-1.19.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/util_macros-1.19.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/util_macros-1.19.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/util_macros-1.19.3-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '5ff671e17592e9c7d771aa0c80674aa64c457b6bff87a9553de198d4664e8ee5',
-     armv7l: '5ff671e17592e9c7d771aa0c80674aa64c457b6bff87a9553de198d4664e8ee5',
-       i686: 'e9feda16b4fd408253af38a2f2687db4fbde3b8a5a24125a317d859c389a75c9',
-     x86_64: '22cae36ecdc1686f1f8f82c4d7d7de442b0f0eac70c549a72a9ec73f50dd01c5'
+    aarch64: '42bfc95875cfcee649d1a66cfea163f64d3426cbc07b37c05af920a0d5e54cf9',
+     armv7l: '42bfc95875cfcee649d1a66cfea163f64d3426cbc07b37c05af920a0d5e54cf9',
+       i686: 'ab596326234a9a8be37027980f9c63abda60705a85ae2edfc6e891a862f74519',
+     x86_64: 'c2fe3d08599cb0a2b4a0a23b5647f2e8e9959073c0d817157814d05be49ddc83'
   })
 
   def self.build
