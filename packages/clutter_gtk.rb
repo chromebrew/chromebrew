46aea203acb97c60fb2e3934f8f31dad79c0a6a0
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-03-08 10:19:55 +0800

    Update clutter_gtk.rb

--- packages/clutter_gtk.rb
+++ packages/clutter_gtk.rb
@@ -4,9 +4,10 @@ class Clutter_gtk < Package
   description 'The Clutter Gtk package is a library providing facilities to integrate Clutter into GTK+ applications.'
   homepage 'https://wiki.gnome.org/Projects/Clutter'
   compatibility 'all'
+  @_app = name.tr('_', '-')
   @_fullver = '1.8.4'
   @_mainver = @_fullver.rpartition('.')[0]
-  @_url = "https://download.gnome.org/sources/#{name}/#{@_mainver}/#{name}-#{@_fullver}"
+  @_url = "https://download.gnome.org/sources/#{@_app}/#{@_mainver}/#{@_app}-#{@_fullver}"
   version @_fullver
   source_url "#{@_url}.tar.xz"
   source_sha256 `curl -Ls #{@_url}.sha256sum | tail -n1 | cut -d ' ' -f1`.chomp
