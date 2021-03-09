a2f99e256fd30caea649554b32aebf0fa64ddbd9
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-03-09 13:25:56 +0800

    Update ffmpeg.rb

--- packages/ffmpeg.rb
+++ packages/ffmpeg.rb
@@ -3,11 +3,23 @@ require 'package'
 class Ffmpeg < Package
   description 'A complete, cross-platform solution to record, convert and stream audio and video.'
   homepage 'https://ffmpeg.org/'
-  @_ver = '4.3.2'
-  version @_ver
+  version '4.2.1'
   compatibility 'all'
-  source_url "https://ffmpeg.org/releases/ffmpeg-#{@_ver}.tar.xz"
-  source_sha256 '46e4e64f1dd0233cbc0934b9f1c0da676008cad34725113fb7f802cfa84ccddb'
+  source_url 'https://ffmpeg.org/releases/ffmpeg-4.2.1.tar.xz'
+  source_sha256 'cec7c87e9b60d174509e263ac4011b522385fd0775292e1670ecc1180c9bb6d4'
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.2.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.2.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.2.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/ffmpeg-4.2.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '55b40e675db77e1e807d189438c82f3fc1113c737de8c49deeccb1416c931885',
+     armv7l: '55b40e675db77e1e807d189438c82f3fc1113c737de8c49deeccb1416c931885',
+       i686: 'f73db0e5b238ee23dad1ef7d9c3183df4659f97cb5a5b57d21726b5575d8f488',
+     x86_64: 'da2c292468285d298b2fe589f7789d894b582aa88b4eb28aa81713a5c9a59d27',
+  })
 
   depends_on 'ld_default' => :build
   depends_on 'libopencoreamr'
@@ -50,58 +62,61 @@ class Ffmpeg < Package
   depends_on 'zvbi'
 
   def self.build
-    system "env TMPDIR=#{CREW_BREW_DIR} \
-            CFLAGS='-pipe -flto=auto -fuse-ld=gold' \
-            CXXFLAGS='-pipe -flto=auto -fuse-ld=gold' \
-            LDFLAGS='-flto=auto' \
-            ./configure \
-            #{CREW_OPTIONS.sub(/--build=.*/, '')} \
-            --enable-libopencore-amrnb \
-            --enable-libopencore-amrwb \
-            --enable-libvo-amrwbenc \
-            --enable-libfontconfig \
-            --enable-chromaprint \
-            --enable-libopenjpeg \
-            --enable-libfreetype \
-            --enable-fontconfig \
-            --enable-libfdk-aac \
-            --enable-libfribidi \
-            --enable-libmp3lame \
-            --enable-libvidstab \
-            --enable-libwavpack \
-            --enable-libdc1394  \
-            --enable-libtheora  \
-            --enable-libvorbis  \
-            --enable-libdav1d   \
-            --enable-version3   \
-            --enable-libpulse   \
-            --enable-libspeex   \
-            --enable-libopus    \
-            --enable-librtmp    \
-            --enable-libsoxr    \
-            --enable-libwebp    \
-            --enable-libx264    \
-            --enable-libx265    \
-            --enable-libxml2    \
-            --enable-libxvid    \
-            --enable-libzvbi    \
-            --enable-nonfree    \
-            --enable-libdrm     \
-            --enable-libssh     \
-            --enable-libvpx     \
-            --enable-libzmq     \
-            --enable-openal     \
-            --enable-shared     \
-            --disable-debug     \
-            --enable-frei0r     \
-            --enable-gnutls     \
-            --enable-libass     \
-            --arch=#{ARCH}      \
-            --enable-gray       \
-            --enable-gpl"
+    system 'env',
+           "TMPDIR=#{CREW_BREW_DIR}",
+           './configure',
+           "--libdir=#{CREW_LIB_PREFIX}",
+           '--enable-libopencore-amrnb',
+           '--enable-libopencore-amrwb',
+           "--prefix=#{CREW_PREFIX}",
+           '--enable-libvo-amrwbenc',
+           '--enable-libfontconfig',
+           '--enable-chromaprint',
+           '--enable-libopenjpeg',
+           '--enable-libfreetype',
+           '--enable-fontconfig',
+           '--enable-libfdk-aac',
+           '--enable-libfribidi',
+           '--enable-libmp3lame',
+           '--enable-libvidstab',
+           '--enable-libwavpack',
+           '--enable-libdc1394',
+           '--enable-libtheora',
+           '--enable-libvorbis',
+           '--enable-libdav1d',
+           '--enable-version3',
+           '--enable-libpulse',
+           '--enable-libspeex',
+           '--enable-libopus',
+           '--enable-librtmp',
+           '--enable-libsoxr',
+           '--enable-libwebp',
+           '--enable-libx264',
+           '--enable-libx265',
+           '--enable-libxml2',
+           '--enable-libxvid',
+           '--enable-libzvbi',
+           '--enable-nonfree',
+           '--enable-libdrm',
+           '--enable-libssh',
+           '--enable-libvpx',
+           '--enable-libzmq',
+           '--enable-openal',
+           '--enable-shared',
+           '--disable-debug',
+           '--enable-frei0r',
+           '--enable-gnutls',
+           '--enable-libass',
+           "--arch=#{ARCH}",
+           '--enable-gray',
+           '--enable-gpl'
   end
 
   def self.install
+    # Use the gold linker.
+    old_ld = `ld_default g`.chomp
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    # Restore the original linker.
+    system 'ld_default', "#{old_ld}"
   end
 end
