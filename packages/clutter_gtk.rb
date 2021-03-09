30655af5341369dacadf108d6dc4c2020b524ef6
Ed Reel <edreel@gmail.com>
2021-03-09 10:29:02 -0600

    Add pre-built binaries for packages b-r (#5375)

--- packages/clutter_gtk.rb
+++ packages/clutter_gtk.rb
@@ -11,6 +11,19 @@ class Clutter_gtk < Package
   version @_fullver
   source_url "#{@_url}.tar.xz"
   source_sha256 `curl -Ls #{@_url}.sha256sum | tail -n1 | cut -d ' ' -f1`.chomp
+
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/clutter_gtk-1.8.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/clutter_gtk-1.8.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/clutter_gtk-1.8.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/clutter_gtk-1.8.4-chromeos-x86_64.tar.xz'
+  })
+  binary_sha256 ({
+    aarch64: '7f759c259b71f86c1e13ec5ffb8e0add079b5dfa4cddae06d81700db6834ac7d',
+     armv7l: '7f759c259b71f86c1e13ec5ffb8e0add079b5dfa4cddae06d81700db6834ac7d',
+       i686: '499108e196161dffd762aff54ffa8bbd338863c9d9cc8c2af54fd5728d11cb7d',
+     x86_64: '3b5b60ce00a67815def67ff992aac8caf89fb4ee0ef68d7a51c857ce30a3461e'
+  })
   
   depends_on 'gtk3'
   depends_on 'libgee'
--- packages/qqwing.rb
+++ packages/qqwing.rb
@@ -8,6 +8,19 @@ class Qqwing < Package
   source_url 'https://qqwing.com/qqwing-1.3.4.tar.gz'
   source_sha256 '1753736c31feea0085f5cfac33143743204f8a7e66b81ccd17e249ecafba802f'
 
+  binary_url ({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qqwing-1.3.4-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qqwing-1.3.4-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qqwing-1.3.4-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qqwing-1.3.4-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'b06f588f35e0e02728cc058ed736a97653c61b0a7dd93f151e301ca2e03bf0a3',
+     armv7l: 'b06f588f35e0e02728cc058ed736a97653c61b0a7dd93f151e301ca2e03bf0a3',
+       i686: '44111882030a083db1f5f4062124092f69cc2b67094a57c6266f7cd0d5a24c8b',
+     x86_64: '2272662e36f739b18a3a24c33e3213cb6a5fbe34b92049154f831f0407f58f21',
+  })
+
   def self.build
     system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto'  LDFLAGS='-flto=auto' \
             ./configure #{CREW_OPTIONS}"
