5406881c6573ed3646bca6bbad83715aab980e4f
Ed Reel <edreel@gmail.com>
2021-03-10 15:33:05 -0600

    Fix conditional in libva_intel_driver_hybrid (#5394)

--- packages/libva_intel_driver_hybrid.rb
+++ packages/libva_intel_driver_hybrid.rb
@@ -4,7 +4,8 @@ class Libva_intel_driver_hybrid < Package
   description 'VA-API implementation for Intel G45 and HD Graphics family'
   version '2.4.1'
   compatibility 'i686 x86_64'
-  if ARCH == 'i686', 'x86_64'
+  case ARCH
+  when 'i686', 'x86_64'
     source_url 'https://github.com/intel/intel-vaapi-driver/archive/2.4.1.tar.gz'
     source_sha256 '03cd7e16acc94f828b6e7f3087863d8ca06e99ffa3385588005b1984bdd56157'
     depends_on 'igt_gpu_tools'
