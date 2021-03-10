0774da2ad8a51dd45743fce5b91bf5c4e8096850
satmandu <satadru@gmail.com>
2021-03-10 00:30:11 -0500

    Update libx264.rb

--- packages/libx264.rb
+++ packages/libx264.rb
@@ -3,36 +3,44 @@ require 'package'
 class Libx264 < Package
   description 'x264 is a free software library and application for encoding video streams into the H.264/MPEG-4 AVC compression format.'
   homepage 'http://www.videolan.org/developers/x264.html'
-  version '20171118-2245-stable'
+  version 'b86a'
   compatibility 'all'
-  source_url 'http://ftp.videolan.org/pub/videolan/x264/snapshots/x264-snapshot-20171118-2245-stable.tar.bz2'
-  source_sha256 '22f29bbeba7087d9a92e0a50837e22c5729ba46b6c03392f149cae6ca3fa01ad'
+  source_url 'https://code.videolan.org/videolan/x264/-/archive/b86ae3c66f51ac9eab5ab7ad09a9d62e67961b8a/x264-b86ae3c66f51ac9eab5ab7ad09a9d62e67961b8a.tar.bz2'
+  source_sha256 '6d4eacd1e735f1670a84f56802023fce9a68f19a6186440281fff7757810297d'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20171118-2245-stable-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20171118-2245-stable-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20171118-2245-stable-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-20171118-2245-stable-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-b86a-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-b86a-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-b86a-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libx264-b86a-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'a0790759e5686aa91e43b64ab0f5c64ee9dfc6b01e62af3dfb1a095976130939',
-     armv7l: 'a0790759e5686aa91e43b64ab0f5c64ee9dfc6b01e62af3dfb1a095976130939',
-       i686: '59488d66a03c91546359f616898a96c362113aa6af939dd9344d0c10cc2e39c0',
-     x86_64: '32b6a7a5f9557b066e633beeb3f3ff748dd90a2bef14ead31e292f984e5d226c',
+  binary_sha256({
+    aarch64: 'e212794830955a9f109c01000eae365580dc61568db24a4ae412f9f518932f3a',
+     armv7l: 'e212794830955a9f109c01000eae365580dc61568db24a4ae412f9f518932f3a',
+       i686: '57005b8eb98bbc08ddca26fcecce43ea58e7f489f493d89367512bdc0ffbbb30',
+     x86_64: '280d34bcb68b0ddf87e747b7f21bb63c9a881f3de6bb91ab6fca6a9d191a16a5'
   })
 
-  depends_on 'yasm' => :build
+  depends_on 'nasm' => :build
+  depends_on 'l_smash'
+
+  @archflags = ARCH == 'aarch64' || ARCH == 'armv7l' ? '-march=armv7-a -mfpu=neon' : ''
 
   def self.build
-    system "./configure \
-            --prefix=#{CREW_PREFIX} \
-            --libdir=#{CREW_LIB_PREFIX} \
-            --enable-shared \
-            --enable-static"
-    system "make"
+    system '[ -x configure ] || ./autogen.sh'
+    system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto #{@archflags}' \
+      CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+      ./configure #{CREW_OPTIONS} \
+      --enable-shared \
+      --enable-static \
+      --enable-pic \
+      --enable-lto \
+      --disable-avs"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system "make DESTDIR=#{CREW_DEST_DIR} install"
   end
 end
