c135fe69c981dfe78476b08ce10c097ddb49f34f
supechicken <supechicken666@gmail.com>
2021-03-11 10:18:16 +0800

    Update libva_utils.rb

--- packages/libva_utils.rb
+++ packages/libva_utils.rb
@@ -7,7 +7,7 @@ class Libva_utils < Package
   version @_ver
   compatibility 'all'
   source_url "https://github.com/intel/libva-utils/archive/#{@_ver}.tar.gz"
-  source_sha256 '33f06929faa395f55ec816432679219c56d70850bf465c848f0418e8a4f0352b'
+  source_sha256 'cbb7f9f6eae21d772e31b67bc8c311be6e35fe9c65e63acc57f9b16d72bf8dc0'
 
   depends_on 'libva'
 
