bd83d97130653d5d6639f54f47ba15a713f42e50
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-03-12 12:51:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,12 +15,10 @@ CREW_BREW_DIR="${CREW_PREFIX}/tmp/crew/"
 CREW_DEST_DIR="${CREW_BREW_DIR}/dest"
 CREW_PACKAGES_PATH="${CREW_LIB_PATH}/packages"
 
-EARLY_PACKAGES="gcc10 llvm brotli c_ares libcyrussasl libiconv libidn2 \
-libmetalink libnghttp2 libpsl libssh2 libtirpc libunistring openldap \
-rtmpdump zstd ncurses ca_certificates ruby libffi openssl nettle krb5 \
-p11kit libtasn1 gnutls curl git icu4c libedit"
+EARLY_PACKAGES=(gcc10 libiconv libssh2 ncurses ruby openssl git)
 
 ARCH="$(uname -m)"
+BREW=🍺
 
 GREEN='\e[1;32m';
 RED='\e[1;31m';
@@ -28,10 +26,47 @@ BLUE='\e[1;34m';
 YELLOW='\e[1;33m';
 RESET='\e[0m'
 
-if [ "${EUID}" == "0" ]; then
-  echo -e "${RED}Chromebrew should not be installed or run as root.${RESET}"
-  exit 1;
-fi
+function puts() {
+  reset='\e[0m'
+  
+  case ${2} in
+  lightred)
+    colorize='\e[1;31m';;
+  lightgreen)
+    colorize='\e[1;32m';;
+  yellow)
+    colorize='\e[1;33m';;
+  orange)
+    colorize='\e[0;33m';;
+  lightblue)
+    colorize='\e[1;34m';;
+  cyan)
+    colorize='\e[0;36m';;
+  *)
+    colorize=${2};;
+  esac
+  
+  case ${3} in
+  cover)
+    colorize=${colorize}'\e[1A\e[K';;
+  covercurrent)
+    colorize=${colorize}'\r';;
+  printf);;
+  *)
+    reset=${reset}'\n';;
+  esac
+  
+  printf "${colorize}${1}${reset}"
+}
+
+function rootchecker() {
+  if [ ${EUID} == 0 ]; then
+    puts 'Chromebrew should not be installed or run as root.' yellow
+    puts "Trying to switch to user ${USER}..." yellow
+    su chronos - <<< "$(curl -L# ${URL}/install.sh)" ||\
+      (puts "Failed to switch to user chronos or installation failed" lightred; exit 1)
+  fi
+} && rootchecker
 
 case "${ARCH}" in
 "i686"|"x86_64"|"armv7l"|"aarch64")
@@ -39,7 +74,7 @@ case "${ARCH}" in
   [ "${ARCH}" == "x86_64" ] && LIB_SUFFIX='64'
   ;;
 *)
-  echo -e "${RED}Your device is not supported by Chromebrew yet :/${RESET}"
+  puts 'Your device is not supported by Chromebrew yet :/' lightred
   exit 1;;
 esac
 
@@ -77,20 +112,18 @@ case "${ARCH}" in
   if ! type "xz" > /dev/null; then
     temp_url='https://github.com/snailium/chrome-cross/releases/download/v1.8.1/xz-5.2.3-chromeos-armv7l.tar.gz'
     temp_sha256='4dc9f086ee7613ab0145ec0ed5ac804c80c620c92f515cb62bae8d3c508cbfe7'
-    urls[k]="$temp_url"
-    sha256s[k]="$temp_sha256"
-    k=$((k+1))
+    urls+=("$temp_url")
+    sha256s+=("$temp_sha256")
   fi
   ;;
 esac
 
-for package in $EARLY_PACKAGES; do
+for package in ${EARLY_PACKAGES[@]}; do
   pkgfile="$(curl -Lsf "${URL}"/packages/"$package".rb)"
   temp_url="$(echo "$pkgfile" | grep -m 3 "$ARCH": | head -n 1 | cut -d\' -f2 | tr -d \' | tr -d \" | sed 's/,//g')"
   temp_sha256="$(echo "$pkgfile" | grep -m 3 "$ARCH": | tail -n 1 | cut -d\' -f2 | tr -d \' | tr -d \" | sed 's/,//g')"
-  urls[k]="$temp_url"
-  sha256s[k]="$temp_sha256"
-  k=$((k+1))
+  urls+=("$temp_url")
+  sha256s+=("$temp_sha256")
 done
 
 # functions to maintain packages
@@ -98,16 +131,18 @@ function download_check () {
     cd "${CREW_BREW_DIR}"
 
     #download
-    echo -e "${BLUE}Downloading ${1}...${RESET}"
-    curl '-#' -C - -L --ssl "${2}" -o "${3}"
+    puts "=> Downloading ${1}..." lightblue
+    puts "=> ${BREW} ${2}" cyan
+    $CURL '-#' -C - -L --ssl "${2}" -o "${3}"
+    puts ' └─> Archive download' lightgreen cover
 
     #verify
-    echo -e "${BLUE}Verifying ${1}...${RESET}"
-    echo -e "${GREEN}$(echo "${4}" "${3}" | sha256sum -c -)${RESET}"
+    loading "$(puts "=> Verifying ${1}...  " lightblue)"
+    puts    "\n └─> $(echo ${4} ${3} | sha256sum -c)" lightgreen
     case "${?}" in
     0) ;;
     *)
-      echo -e "${RED}Verification failed, something may be wrong with the download.${RESET}"
+      puts 'Verification failed, something may be wrong with the download.' lightred
       exit 1;;
     esac
 }
@@ -119,41 +154,45 @@ function extract_install () {
     cd "${CREW_DEST_DIR}"
 
     #extract and install
-    echo "Extracting ${1} (this may take a while)..."
+    puts "=> Extracting ${1} (this may take a while)...  " lightblue
+    start=$(date +%s)
     tar xpf ../"${2}"
-    echo "Installing ${1} (this may take a while)..."
+
+    puts "=> Installing ${1} (this may take a while)...  " lightblue
     tar cpf - ./*/* | (cd /; tar xp --keep-directory-symlink -f -)
     mv ./dlist "${CREW_CONFIG_PATH}/meta/${1}.directorylist"
     mv ./filelist "${CREW_CONFIG_PATH}/meta/${1}.filelist"
+    puts " └─> Installed in $(($(date +%s)-${start})) seconds" orange
 }
 
 function update_device_json () {
   cd "${CREW_CONFIG_PATH}"
 
   if grep '"name": "'"${1}"'"' device.json > /dev/null; then
-    echo "Updating version number of ${1} in device.json..."
+    puts "=> Updating version number of ${1} in device.json..."
     sed -i device.json -e '/"name": "'"${1}"'"/N;//s/"version": ".*"/"version": "'"${2}"'"/'
   elif grep '^    }$' device.json > /dev/null; then
-    echo "Adding new information on ${1} to device.json..."
+    puts "=> Adding new information on ${1} to device.json..."
     sed -i device.json -e '/^    }$/s/$/,\
     {\
       "name": "'"${1}"'",\
       "version": "'"${2}"'"\
     }/'
   else
-    echo "Adding new information on ${1} to device.json..."
+    puts "=> Adding new information on ${1} to device.json..."
     sed -i device.json -e '/^  "installed_packages": \[$/s/$/\
     {\
       "name": "'"${1}"'",\
       "version": "'"${2}"'"\
     }/'
   fi
+  puts
 }
 
 # create the device.json file if it doesn't exist
 cd "${CREW_CONFIG_PATH}"
 if [ ! -f device.json ]; then
-  echo -e "${YELLOW}Creating new device.json...${RESET}"
+  puts 'Creating new device.json...' yellow
   echo '{' > device.json
   echo '  "architecture": "'"${ARCH}"'",' >> device.json
   echo '  "installed_packages": [' >> device.json
@@ -162,16 +201,14 @@ if [ ! -f device.json ]; then
 fi
 
 # extract, install and register packages
-for i in $(seq 0 $((${#urls[@]} - 1))); do
-  url="${urls["${i}"]}"
-  sha256="${sha256s["${i}"]}"
+for i in ${!urls[@]}; do
   tarfile="$(basename ${url})"
   name="${tarfile%%-*}"   # extract string before first '-'
   rest="${tarfile#*-}"    # extract string after first '-'
   version="$(echo ${rest} | sed -e 's/-chromeos.*$//')"
                         # extract string between first '-' and "-chromeos"
 
-  download_check "${name}" "${url}" "${tarfile}" "${sha256}"
+  download_check "${name}" "${urls[${i}]}" "${tarfile}" "${sha256s[${i}]}"
   extract_install "${name}" "${tarfile}"
   update_device_json "${name}" "${version}"
 done
@@ -201,19 +238,19 @@ yes | crew install buildessential less most
 
 echo
 if [[ "${CREW_PREFIX}" != "/usr/local" ]]; then
-  echo -e "${YELLOW}
+  puts "
 Since you have installed Chromebrew in a directory other than '/usr/local',
 you need to run these commands to complete your installation:
-${RESET}"
+" yellow
 
-  echo -e "${BLUE}
+  puts "
 echo 'export CREW_PREFIX=${CREW_PREFIX}' >> ~/.bashrc
 echo 'export PATH=\"\${CREW_PREFIX}/bin:\${CREW_PREFIX}/sbin:\${PATH}\"' >> ~/.bashrc
 echo 'export LD_LIBRARY_PATH=${CREW_PREFIX}/lib${LIB_SUFFIX}' >> ~/.bashrc
 source ~/.bashrc
-${RESET}"
+" lightblue
 fi
-echo -e "${BLUE}
+puts "
 To set the default PAGER environment variable to use less:
 echo \"export PAGER='less'\" >> ~/.bashrc && . ~/.bashrc
 
@@ -235,5 +272,5 @@ echo \"export EDITOR='nano'\" >> ~/.bashrc && . ~/.bashrc
 
 To set 'vim' as the default editor, execute:
 echo \"export EDITOR='vim'\" >> ~/.bashrc && . ~/.bashrc
-${RESET}"
-echo -e "${GREEN}Chromebrew installed successfully and package lists updated.${RESET}"
+" lightblue
+puts 'Chromebrew installed successfully and package lists updated.' lightgreen
