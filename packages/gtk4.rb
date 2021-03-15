8fc1d737540698913e985d926681eb4904ae79ce
Satadru Pramanik <satadru@gmail.com>
2021-03-15 14:32:17 -0400

    gtk3 & gtk4 update

--- packages/gtk4.rb
+++ packages/gtk4.rb
@@ -3,22 +3,25 @@ require 'package'
 class Gtk4 < Package
   description 'GTK+ is a multi-platform toolkit for creating graphical user interfaces.'
   homepage 'https://developer.gnome.org/gtk4/'
-  version '4.1.1'
+  @_ver = '4.1.2'
+  @_ver_prelastdot = @_ver.rpartition('.')[0]
+  version @_ver
   compatibility 'all'
-  source_url 'https://download.gnome.org/sources/gtk/4.1/gtk-4.1.1.tar.xz'
-  source_sha256 'f7e1789f6c637b091cffb17de08bd9c3986543282eecdff0750dd04f1673b737'
+  source_url "https://gitlab.gnome.org/GNOME/gtk/-/archive/#{@_ver}/gtk-#{@_ver}.tar.bz2"
+  # source_url "https://download.gnome.org/sources/gtk/#{@_ver_prelastdot}/gtk-#{@_ver}.tar.xz"
+  source_sha256 '64b042592ba48c63535a47ed087d161290620e1de9e577ff89ea3afabf1a4edb'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gtk4-4.1.2-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'dcce3aeb6954b2010a867f2bf84b11995d6d0433a1ece96313ee36d27af4819c',
-     armv7l: 'dcce3aeb6954b2010a867f2bf84b11995d6d0433a1ece96313ee36d27af4819c',
-       i686: '182fa221219f0a2b25b5ebe902e86dde0ed09e3e741bb1e52acdd120e8bcb308',
-     x86_64: '81fbb2bd61ff6c1f0729f686e9c1a21f39c24bfaf8d2bd0a92adb929e3f32475'
+    aarch64: 'f04e726a8c2a71d04c1a274a67b0727aa6ae318b40d9325f20546ca29064e37e',
+     armv7l: 'f04e726a8c2a71d04c1a274a67b0727aa6ae318b40d9325f20546ca29064e37e',
+       i686: '690f527ac11a4af784b299159350323e726a5be18b3443fabdfc41f83dcd10a4',
+     x86_64: '3d715f42be186839b936929deaec180a3bf13f3fb2f7e89dc21421d6b19988fa'
   })
 
   depends_on 'cups'
@@ -70,16 +73,18 @@ class Gtk4 < Package
     system "glib-compile-schemas #{CREW_PREFIX}/share/glib-2.0/schemas"
     # update mime database
     system "update-mime-database #{CREW_PREFIX}/share/mime"
-    unless File.exist?("#{HOME}/.config/gtk-4.0/settings.ini")
+    @xdg_config_home = ENV['XDG_CONFIG_HOME']
+    @gtk4settings = <<~GTK4_CONFIG_HEREDOC
+      [Settings]
+      gtk-icon-theme-name = Adwaita
+      gtk-theme-name = Adwaita
+      gtk-font-name = Arimo 10
+    GTK4_CONFIG_HEREDOC
+    unless File.exist?("#{@xdg_config_home}/gtk-4.0/settings.ini")
       puts
-      puts 'Adding basic gtk4 settings to ~/.config/gtk-4.0/settings.ini'.lightblue
-      FileUtils.mkdir_p "#{HOME}/.config/gtk-4.0"
-      system "cat << 'EOF' > #{HOME}/.config/gtk-4.0/settings.ini
-[Settings]
-gtk-icon-theme-name = Adwaita
-gtk-theme-name = Adwaita
-gtk-font-name = Arimo 10
-EOF"
+      puts 'Adding basic gtk4 settings to XDG_CONFIG_HOME/gtk-4.0/settings.ini'.lightblue
+      FileUtils.mkdir_p "#{@xdg_config_home}/gtk-4.0"
+      File.write("#{@xdg_config_home}/gtk-4.0/settings.ini", @gtk4settings)
     end
   end
 end
