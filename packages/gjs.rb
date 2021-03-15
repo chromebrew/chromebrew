be47818ceaa57ca9abf4240e17fe44bf6062bd04
satmandu <satadru@umich.edu>
2021-03-14 23:50:37 -0400

    gjs => 1.67.3 (#5420)

--- packages/gjs.rb
+++ packages/gjs.rb
@@ -2,22 +2,24 @@ require 'package'
 
 class Gjs < Package
   description 'Javascript Bindings for GNOME'
-  version '1.67.2'
+  @_ver = '1.67.3'
+  @_ver_prelastdot = @_ver.rpartition('.')[0]
+  version @_ver
   compatibility 'all'
-  source_url 'https://download.gnome.org/sources/gjs/1.67/gjs-1.67.2.tar.xz'
-  source_sha256 '4d9a4b8580a6871239e227338e3bc54f7d119057306fec56a364d90da69e1ce6'
+  source_url "https://download.gnome.org/sources/gjs/#{@_ver_prelastdot}/gjs-#{@_ver}.tar.xz"
+  source_sha256 '12df0c0ff2dd4c944ad27477ee8053e1363c4ad499542686bba21e06d38c6733'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.2-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gjs-1.67.3-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'db807d23b09a122d9ffd02699cc301f4b6aa5668076d610c45cf4c6f1bf981c6',
-     armv7l: 'db807d23b09a122d9ffd02699cc301f4b6aa5668076d610c45cf4c6f1bf981c6',
-       i686: '8377ad2a8e958df5fa9182f0d0a7138e22ca82a814c9bc4ba265d5edf55a55cb',
-     x86_64: 'e5d3274313176202aee2dbf0fc2d2c4277be1bde67f93d5e9b10c0d3fac20dd5'
+    aarch64: '6ee8b11391aee0245536d1d702b765739b20310df841ca4395fce2d9b07eb532',
+     armv7l: '6ee8b11391aee0245536d1d702b765739b20310df841ca4395fce2d9b07eb532',
+       i686: 'f8eeea3dd66b93420d99942db1dc355c8b0f36d0201e77ca239186e8de113051',
+     x86_64: '0bcdfb345b073565735fbd8b3e76a5cd6d9694590cbd10b217619207b91a3f88'
   })
 
   depends_on 'cairo'
