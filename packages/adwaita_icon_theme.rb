cf69fa0d060b55b580a5aaa858d5b6a925eb1adc
satmandu <satadru@gmail.com>
2021-03-15 17:47:10 -0400

    Update adwaita_icon_theme.rb

--- packages/adwaita_icon_theme.rb
+++ packages/adwaita_icon_theme.rb
@@ -4,17 +4,21 @@ class Adwaita_icon_theme < Package
   description 'Theme consisting of a set of icons for GTK+'
   homepage 'https://gitlab.gnome.org/GNOME/adwaita-icon-theme'
   version '3.38.0-c5be'
-  compatibility 'x86_64'
+  compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/archive/c5bed6840332a7539b175feb4ec7ecf957fb1399/adwaita-icon-theme-c5bed6840332a7539b175feb4ec7ecf957fb1399.tar.bz2'
   source_sha256 '54d67549cb7b295dd649460a22af9d2f436dd73386068eb20ce888f6f4bee0fb'
 
   binary_url({
-    i686: 'https://dl.bintray.com/chromebrew/chromebrew/adwaita_icon_theme-3.38.0-c5be-chromeos-i686.tar.xz',
-  x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/adwaita_icon_theme-3.38.0-c5be-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/adwaita_icon_theme-3.38.0-c5be-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/adwaita_icon_theme-3.38.0-c5be-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/adwaita_icon_theme-3.38.0-c5be-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/adwaita_icon_theme-3.38.0-c5be-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    i686: 'f5e0d629d2b99eb18cfa2a6d991a33e0efb4efe206b6510d884bf8d96ee3d9af',
-  x86_64: 'bd03ca46fc6310ea449362b95e0032e800fdda4f86252e02f64502407cd29adc'
+    aarch64: 'c2d4e312434a6ee7ece0a044d16e36a2b582e54c1a4bbad246bc7e6f1c327271',
+     armv7l: 'c2d4e312434a6ee7ece0a044d16e36a2b582e54c1a4bbad246bc7e6f1c327271',
+       i686: 'f5e0d629d2b99eb18cfa2a6d991a33e0efb4efe206b6510d884bf8d96ee3d9af',
+     x86_64: 'bd03ca46fc6310ea449362b95e0032e800fdda4f86252e02f64502407cd29adc'
   })
 
   depends_on 'cantarell_fonts'
