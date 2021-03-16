0772184916c84b1b666530188a793434b8964430
Satadru Pramanik <satadru@gmail.com>
2021-03-16 07:56:26 -0400

    Gnome 40.rc builds

--- packages/gobject_introspection.rb
+++ packages/gobject_introspection.rb
@@ -3,24 +3,23 @@ require 'package'
 class Gobject_introspection < Package
   description 'GObject introspection is a middleware layer between C libraries (using GObject) and language bindings.'
   homepage 'https://wiki.gnome.org/action/show/Projects/GObjectIntrospection'
-  version '1.66.1-3'
+  version '1.67.1'
   compatibility 'all'
   source_url 'https://download.gnome.org/sources/gobject-introspection/1.66/gobject-introspection-1.66.1.tar.xz'
   source_sha256 'dd44a55ee5f426ea22b6b89624708f9e8d53f5cc94e5485c15c87cb30e06161d'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.66.1-3-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.66.1-3-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.66.1-3-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.66.1-3-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.67.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.67.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.67.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.67.1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: 'e4e2c7666bb7887a4af92b83140abfc6f68f9113bc3ce28b93cbf1760abcbe76',
-      armv7l: 'e4e2c7666bb7887a4af92b83140abfc6f68f9113bc3ce28b93cbf1760abcbe76',
-        i686: 'd5b61a8cbb54d82302cd45515cc20fd795887aa5209cb787c76bb626d034659d',
-      x86_64: '726f495ff2e741e7066badf37da5decb29fef37886bebdd2ac5ea4ec2beb54b2',
+  binary_sha256({
+    aarch64: '4da5e9d81d858f5d62f3923656c0ede85615ec6a013553d68c94b6108e0642fb',
+     armv7l: '4da5e9d81d858f5d62f3923656c0ede85615ec6a013553d68c94b6108e0642fb',
+       i686: '7e6e6507aaf64cd48f9f5a99b5d0c9aa983e5c05dccaf29c2437e497e9509ae8',
+     x86_64: '72db415a3796c08d212a0dbd9a1c32c2c1318ac379189b6411df383eac8f8724'
   })
-
 
   depends_on 'glib'
 
--- packages/gtksourceview.rb
+++ packages/gtksourceview.rb
@@ -3,23 +3,23 @@ require 'package'
 class Gtksourceview < Package
   description 'Source code editing widget'
   homepage 'https://wiki.gnome.org/Projects/GtkSourceView'
-  @_ver = '4.8.0'
+  @_ver = '4.8.1'
   version @_ver
   compatibility 'all'
   source_url "https://github.com/GNOME/gtksourceview/archive/#{@_ver}.tar.gz"
-  source_sha256 'dade8ff4ee6e0574af518cddbcde752739e73d346178baf4381e4d9d422b420a'
+  source_sha256 'cbfde42161662525805be4a38102b085db5e991fe1263171a28a78c7da253760'
 
-  binary_url ({
-     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.0-chromeos-armv7l.tar.xz',
-      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.0-chromeos-armv7l.tar.xz',
-        i686: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.0-chromeos-i686.tar.xz',
-      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.0-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gtksourceview-4.8.1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-     aarch64: '8f730dc093a598c27fcff1967eb22e1ca0a9e57cf352d00c28570224decd5be6',
-      armv7l: '8f730dc093a598c27fcff1967eb22e1ca0a9e57cf352d00c28570224decd5be6',
-        i686: 'c43453cd39857420d5d0f3fc7b14e54e3a9231bb546f4a65a530bd25d93e39af',
-      x86_64: 'dec3b93f94494360f8d1011c595f1dec98c5359a28dc557a5da1014acf4c2e71',
+  binary_sha256({
+    aarch64: '8847fb32a4b6b814dbe1f58cdc0487ab75c781daffca97ee1b965da1786b498b',
+     armv7l: '8847fb32a4b6b814dbe1f58cdc0487ab75c781daffca97ee1b965da1786b498b',
+       i686: '409401c64604dfd3f827cef4345c53415c3ff1c7af5f19b720ed5fb5bb3bdd11',
+     x86_64: '8839be6b737b4555bddb5b6a9aaba47da81b7432c672b6b3b5c7ccf7a7a80ddb'
   })
 
   depends_on 'setuptools' => :build
@@ -35,8 +35,8 @@ class Gtksourceview < Package
     -Dc_args='-fno-stack-protector -flto=thin -fuse-ld=lld' \
     -Dc_link_args='-fno-stack-protector -flto=thin -fuse-ld=lld' \
     builddir"
-    system "meson configure builddir"
-    system "ninja -C builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
   end
 
   def self.install
