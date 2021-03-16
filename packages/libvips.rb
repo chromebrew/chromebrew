196291ca1cb3682357593b90dd6bd453098958ae
Satadru Pramanik <satadru@gmail.com>
2021-03-16 19:52:48 -0400

    lib64 fixes and deps

--- packages/libvips.rb
+++ packages/libvips.rb
@@ -3,33 +3,38 @@ require 'package'
 class Libvips < Package
   description 'A fast image processing library with low memory needs'
   homepage 'https://libvips.github.io/libvips/'
-  @_ver = '8.10.6-beta'
+  @_ver = '8.10.6-beta2'
   version @_ver
   compatibility 'all'
   source_url "https://github.com/libvips/libvips/archive/v#{@_ver}.tar.gz"
-  source_sha256 '975371c3650dbfedbde012b6573034338b0bb8f03d5df8d031abb80c3b4c9014'
+  source_sha256 'b2412f580ba83129d55e57a73c7c4fdb53e60a39c48910acc5f0d80518deb7a5'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libvips-8.10.6-beta2-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '64f9cad9ad9287b1c57086adec379640676a4a456b3e753d9e7797fe0ee92ec7',
-     armv7l: '64f9cad9ad9287b1c57086adec379640676a4a456b3e753d9e7797fe0ee92ec7',
-       i686: '504779b4a009c269dc19dc4f3bb0ad60512dc53da38bfbba3507c959540e3d37',
-     x86_64: '3d719741999b3e75ab54788b99d0468217b539ec9e79d1f672a71f14002a6c96'
+    aarch64: '2d95674bfc951f232315ea0e93311d5982b9975c76f7f8d092f8d3fcfe6810b6',
+     armv7l: '2d95674bfc951f232315ea0e93311d5982b9975c76f7f8d092f8d3fcfe6810b6',
+       i686: '4bf07840cb30a398201160d0004530b909c48c691d21eb4bc35b2a4081c86f27',
+     x86_64: '3d2244043e9fd70c3a7429e98c825243cadac0e613a3c5443ee3a243721d8815'
   })
 
   depends_on 'cfitsio'
   depends_on 'fftw'
-  depends_on 'imagemagick'
+  depends_on 'imagemagick7'
+  depends_on 'lcms'
   depends_on 'libexif'
   depends_on 'libgsf'
   depends_on 'libheif'
   depends_on 'libimagequant'
   depends_on 'librsvg'
+  depends_on 'libtiff'
+  depends_on 'libwebp'
+  depends_on 'orc'
+  depends_on 'poppler'
 
   def self.build
     system 'NOCONFIGURE=1 ./autogen.sh'
@@ -37,7 +42,7 @@ class Libvips < Package
     system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
       CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
       LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      ./configure"
+      ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
--- packages/qttools.rb
+++ packages/qttools.rb
@@ -3,34 +3,35 @@ require 'package'
 class Qttools < Package
   description 'Qt Tools'
   homepage 'https://github.com/qt/qttools'
-  version '5.15.1'
-  compatibility 'aarch64,armv7l,x86_64'
-  source_url 'http://download.qt.io/official_releases/qt/5.15/5.15.1/submodules/qttools-everywhere-src-5.15.1.tar.xz'
-  source_sha256 'c98ee5f0f980bf68cbf0c94d62434816a92441733de50bd9adbe9b9055f03498'
+  version '5.15.2'
+  compatibility 'all'
+  source_url 'http://download.qt.io/official_releases/qt/5.15/5.15.2/submodules/qttools-everywhere-src-5.15.2.tar.xz'
+  source_sha256 'c189d0ce1ff7c739db9a3ace52ac3e24cb8fd6dbf234e49f075249b38f43c1cc'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qttools-5.15.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qttools-5.15.1-chromeos-armv7l.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qttools-5.15.1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/qttools-5.15.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/qttools-5.15.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/qttools-5.15.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/qttools-5.15.2-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'b800130b6db80652fc3c9bde2008b8fa64b1e5bab5dfe717c10b8fb04d686b94',
-     armv7l: 'b800130b6db80652fc3c9bde2008b8fa64b1e5bab5dfe717c10b8fb04d686b94',
-     x86_64: 'ca2818363c965077ae2eaf6430369f5a37f5288dd229bbf352072c2a8adffa03',
+  binary_sha256({
+    aarch64: '0d40cb342b8b31427bc976979dbc4c5bfcd8995e5bcc1e74a2a91cf4d84c2265',
+     armv7l: '0d40cb342b8b31427bc976979dbc4c5bfcd8995e5bcc1e74a2a91cf4d84c2265',
+       i686: '1f94a4b40c8f94d47b13445fd9dfb7abb50694cf0876d83e71287f59f36caeca',
+     x86_64: '4fe6c1fcd3b4bdfbdface915f9c7d71b6c73deadbb5bb20cb5d89af8d6d9d2d2'
   })
 
   depends_on 'qtbase'
   depends_on 'libtinfo'
-  depends_on 'llvm'
-  depends_on 'zstd'
-  depends_on 'sommelier'
 
   def self.build
-    system 'qmake && make'
+    # Note: This MAY NOT COMPILE with llvm installed on x86. Install dependencies,
+    # disable depends, and uninstall llvm before attempting compile.
+    system 'qmake -Wall -early QMAKE_CFLAGS=-flto -early QMAKE_CXXFLAGS=-flto && make'
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}"
+    FileUtils.mkdir_p CREW_DEST_LIB_PREFIX.to_s
     system "cp -a lib/* #{CREW_DEST_LIB_PREFIX}"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/Qt-5"
     FileUtils.cp_r 'bin', "#{CREW_DEST_PREFIX}/share/Qt-5"
