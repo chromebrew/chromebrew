3fa2e4d0fb6ebae411336df8deebe00c5720eacd
Satadru Pramanik <satadru@gmail.com>
2021-03-16 08:08:42 -0400

    Gnome 40.rc gobject_introspection

--- packages/gobject_introspection.rb
+++ packages/gobject_introspection.rb
@@ -3,10 +3,11 @@ require 'package'
 class Gobject_introspection < Package
   description 'GObject introspection is a middleware layer between C libraries (using GObject) and language bindings.'
   homepage 'https://wiki.gnome.org/action/show/Projects/GObjectIntrospection'
-  version '1.67.1'
+  @_ver = '1.67.1'
+  version @_ver
   compatibility 'all'
-  source_url 'https://download.gnome.org/sources/gobject-introspection/1.66/gobject-introspection-1.66.1.tar.xz'
-  source_sha256 'dd44a55ee5f426ea22b6b89624708f9e8d53f5cc94e5485c15c87cb30e06161d'
+  source_url "https://gitlab.gnome.org/GNOME/gobject-introspection/-/archive/#{@_ver}/gobject-introspection-#{@_ver}.tar.bz2"
+  source_sha256 '4cd840eb3a9b5f6cddda96f79e9f186b1da4b93343a0c82e0ab185beccb25a6b'
 
   binary_url({
     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.67.1-chromeos-armv7l.tar.xz',
@@ -15,10 +16,10 @@ class Gobject_introspection < Package
      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gobject_introspection-1.67.1-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '4da5e9d81d858f5d62f3923656c0ede85615ec6a013553d68c94b6108e0642fb',
-     armv7l: '4da5e9d81d858f5d62f3923656c0ede85615ec6a013553d68c94b6108e0642fb',
-       i686: '7e6e6507aaf64cd48f9f5a99b5d0c9aa983e5c05dccaf29c2437e497e9509ae8',
-     x86_64: '72db415a3796c08d212a0dbd9a1c32c2c1318ac379189b6411df383eac8f8724'
+    aarch64: '07f5085fba9f5d6ef6ad5be8f19d977b27a4ba869819f92fd52d230c6b68cfb4',
+     armv7l: '07f5085fba9f5d6ef6ad5be8f19d977b27a4ba869819f92fd52d230c6b68cfb4',
+       i686: '277e24570f9dccdc644e5398ca92538af71cbedcf000b61eb702f4563071ebe2',
+     x86_64: '845bce3214bddc6008d384f31efb210578aa97e4b50dff9017aa12b54f8a2963'
   })
 
   depends_on 'glib'
