b49394645388f7f94781d1dd691b0262974b0ee5
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-03-21 19:31:06 -0400

    Fix problems from crew-license-framework (#5459)
    * Fix antlr4
    
    * fix binclock
    
    * fix libva
    
    * Fix putsing of license

--- packages/antlr4.rb
+++ packages/antlr4.rb
@@ -4,7 +4,7 @@ class Antlr4 < Package
   description 'ANTLR (ANother Tool for Language Recognition) is a powerful parser generator for reading, processing, executing, or translating structured text or binary files.'
   homepage 'https://www.antlr.org/'
   version '4.7.1'
-  licnese 'BSD'
+  license 'BSD'
   compatibility 'all'
   source_url 'https://raw.githubusercontent.com/antlr/antlr4/4.7.1/README.md'
   source_sha256 '70a58ea4c4f5ed23306313782bc13f36c3529d9a990e95ab273d5deed9286d4f'
@@ -17,7 +17,7 @@ class Antlr4 < Package
   depends_on 'jdk8'
 
   def self.install
-    system 'curl -#LO' https://www.antlr.org/download/antlr-4.7.1-complete.jar'
+    system 'curl -#LO https://www.antlr.org/download/antlr-4.7.1-complete.jar'
     abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('antlr-4.7.1-complete.jar') ) == 'f41dce7441d523baf9769cb7756a00f27a4b67e55aacab44525541f62d7f6688'
     system "install -Dm644 antlr-4.7.1-complete.jar #{CREW_DEST_LIB_PREFIX}/antlr-4.7.1-complete.jar"
   end
