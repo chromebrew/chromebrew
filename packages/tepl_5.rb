14a6b4426bc5e8d906632141831ca9773a449a6f
Satadru Pramanik <satadru@gmail.com>
2021-03-23 12:00:45 -0400

    new Gnome 40.0 builds, split tepl into _5 & _6 packages

---
+++ packages/tepl_5.rb
@@ -0,0 +1,43 @@
+require 'package'
+
+class Tepl_5 < Package
+  description 'Library that eases the development of GtkSourceView-based text editors and IDEs'
+  homepage 'https://wiki.gnome.org/Projects/Tepl'
+  version '5.0.1'
+  license 'LGPL-2.1+'
+  compatibility 'all'
+  source_url 'https://github.com/GNOME/tepl/archive/5.0.1.tar.gz'
+  source_sha256 '2dda3ed2bd16742f6d0fc6d602448eaa2e40b9c967b88599add2338d6fa590e7'
+
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/tepl_5-5.0.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/tepl_5-5.0.1-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/tepl_5-5.0.1-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/tepl_5-5.0.1-chromeos-x86_64.tar.xz'
+  })
+  binary_sha256({
+    aarch64: '1c6bda4f08ad49244dd0e368858e43de9323adf54ab34d387754d0982a8af710',
+     armv7l: '1c6bda4f08ad49244dd0e368858e43de9323adf54ab34d387754d0982a8af710',
+       i686: '380fcc1fba9345a966726985dd7232d5bf1326bf62597f43b2bd194a40ce4a91',
+     x86_64: 'd41156324372ef2d95d056a4b58d10f177bd9c1cf176f5c5baafdc81b55d2c1a'
+  })
+
+  depends_on 'amtk'
+  depends_on 'cairo'
+  depends_on 'glib'
+  depends_on 'gtk3'
+  depends_on 'gtksourceview'
+  depends_on 'gobject_introspection' => ':build'
+  depends_on 'gtk_doc' => ':build'
+  depends_on 'vala' => ':build'
+
+  def self.build
+    system "meson #{CREW_MESON_LTO_OPTIONS} builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+  end
+end
--- packages/yelp_xsl.rb
+++ packages/yelp_xsl.rb
@@ -3,30 +3,30 @@ require 'package'
 class Yelp_xsl < Package
   description 'yelp-xsl is a collection of programs and data files to help you build, maintain, and distribute documentation'
   homepage 'https://github.com/GNOME/yelp-xsl'
-  version '40.rc'
+  version '40.0'
+  compatibility 'all'
   license 'GPL-2+, LGPL-2.1+, MIT and FDL-1.1+'
   license 'GPL-2+ or freedist and GPL-2+'
-  compatibility 'all'
-  source_url 'https://github.com/GNOME/yelp-xsl/archive/40.rc.tar.gz'
-  source_sha256 'f158b5c899b32267dee796146deb8c2831272aa66bbe3cc103031b2ddd182fc8'
+  source_url "https://gitlab.gnome.org/GNOME/yelp-xsl/-/archive/#{version}/yelp-xsl-#{version}.tar.bz2"
+  source_sha256 '3938ae88880cdcc3bbd20db25a05bac03cd1324d60c2940ed85e56259725e7f7'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.rc-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.rc-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.rc-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.rc-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/yelp_xsl-40.0-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '945153ad9d4bea36d348411382dfbd5a6d492c491e17afb7f059901c6805d486',
-     armv7l: '945153ad9d4bea36d348411382dfbd5a6d492c491e17afb7f059901c6805d486',
-       i686: 'b5c6b4ac0658eac4717204b3c0a99d947a884a470b34b04251bb8214ce831547',
-     x86_64: '5f03c4a1a5606b16cfc8bdd4d421b01b222fe6368c04c9837eaeb9671598aed9'
+    aarch64: 'd98fe7c598a54bee90907294bca5ab24732ecbe3f2badc0953693e88007e49ba',
+     armv7l: 'd98fe7c598a54bee90907294bca5ab24732ecbe3f2badc0953693e88007e49ba',
+       i686: 'abe1c586c597739747c831fbf5a7ee7fe6658d519250d0404ce3bbfda2509c1f',
+     x86_64: 'b23d398de5907823b21dc15f0eba4ec6d6da33d9e42c399c48d8e36de4188d2d'
   })
 
   depends_on 'itstool'
 
   def self.build
-    system './autogen.sh'
+    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
     system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
       LDFLAGS='-flto=auto' \
       ./configure #{CREW_OPTIONS}"
