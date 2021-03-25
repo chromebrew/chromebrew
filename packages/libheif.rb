2111d727bc3aa59e5d0d41acda1b658c237c6ee8
Satadru Pramanik <satadru@gmail.com>
2021-03-24 20:06:35 -0400

    add i686 libheif and ffmpeg rebuild

--- packages/libheif.rb
+++ packages/libheif.rb
@@ -6,18 +6,20 @@ class Libheif < Package
   @_ver = '1.11.0'
   version "#{@_ver}-2"
   license 'GPL-3'
-  compatibility 'x86_64 aarch64 armv7l'
+  compatibility 'all'
   source_url "https://github.com/strukturag/libheif/releases/download/v#{@_ver}/libheif-#{@_ver}.tar.gz"
   source_sha256 'c550938f56ff6dac83702251a143f87cb3a6c71a50d8723955290832d9960913'
 
   binary_url({
     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libheif-1.11.0-2-chromeos-armv7l.tar.xz',
      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libheif-1.11.0-2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libheif-1.11.0-2-chromeos-i686.tar.xz',
      x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libheif-1.11.0-2-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
     aarch64: '741663915e39e3f343c5d084423597484477eb49dc719f1395c62f84501ba8d9',
      armv7l: '741663915e39e3f343c5d084423597484477eb49dc719f1395c62f84501ba8d9',
+       i686: '73ad610bbde6b2adb6ac5171efb9078e26ca9a252aece71688e5bd85b0478f86',
      x86_64: '00cdd26729e9af1d85ca26edd0094195cab10c53bb07158a892293ef94e033af'
   })
 
