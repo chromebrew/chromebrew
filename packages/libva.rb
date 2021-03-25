6a00b18ae4cdc39733d215fafb0ab1b61243f4b0
satmandu <satadru@umich.edu>
2021-03-25 16:04:21 -0400

    libcap, mesa, libva update (#5490)
    * libcap mesa libva updates
    
    * Edited depends to mark non-runtime deps as build deps

--- packages/libva.rb
+++ packages/libva.rb
@@ -1,26 +1,26 @@
 require 'package'
 
 class Libva < Package
-  description 'Libva is an implementation for VA-API (VIdeo Acceleration API)'
+  description 'Libva is an implementation for VA-API (Video Acceleration API)'
   homepage 'https://01.org/linuxmedia'
-  @_ver = '2.10.0'
-  version "#{@_ver}-2"
+  @_ver = '2.11.0'
+  version @_ver
   license 'MIT'
   compatibility 'all'
-  source_url "https://github.com/intel/libva/releases/download/#{@_ver}/libva-#{@_ver}.tar.bz2"
-  source_sha256 'fa81e35b50d9818fce5ec9eeeeff08a24a8864ceeb9a5c8e7ae4446eacfc0236'
+  source_url "https://github.com/intel/libva/archive/refs/tags/#{@_ver}.tar.gz"
+  source_sha256 'ee2bd79bad5e2404143f089360685f5da63a32dd551b54ccd61d2d49c041178a'
 
   binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.10.0-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.10.0-2-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.10.0-2-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.10.0-2-chromeos-x86_64.tar.xz'
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.11.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.11.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.11.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/libva-2.11.0-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '9e988606a1aa5f6334be989638016bf13f12f53436175e3852ac4d353084cb15',
-     armv7l: '9e988606a1aa5f6334be989638016bf13f12f53436175e3852ac4d353084cb15',
-       i686: 'c2224c241da00d19a01e5d47aeff24da900bde1818e99c9bc2f9a9178f4e9aa7',
-     x86_64: '833e6a04798cdd78665b3be09beb9d4f3874748f77b720fc324d3c121acfa8f3'
+    aarch64: '4af41d8c10b335f1b00bbfa9c4afcea07bbb0b7d00acd3a18a0c007592302a4a',
+     armv7l: '4af41d8c10b335f1b00bbfa9c4afcea07bbb0b7d00acd3a18a0c007592302a4a',
+       i686: '49938688385785511cca5cdd3d5354a0e1a227538fd7a1dfec4588dbd9af90b5',
+     x86_64: 'fcdd6886cfacef1a4e3e31448bf469f78233993a1376c1c4255314d7355c1eed'
   })
 
   depends_on 'libdrm'
