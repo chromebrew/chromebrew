f80177de0dac3828a5766c3964ec9d506a6ec30a
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-03-29 17:56:04 -0400

    touch ups

--- packages/bmon.rb
+++ packages/bmon.rb
@@ -3,14 +3,14 @@ require 'package'
 class Bmon < Package
   description 'bmon is a commandline bandwidth monitor and rate estimator.'
   homepage 'https://www.infradead.org/~tgr/bmon/'
-  version '2.2.3.dfsg.1'
-  license 'BSD-2'
+  version '4.0'
+  license 'BSD-2 and MIT'
   compatibility 'all'
   source_url "https://github.com/tgraf/bmon/releases/download/v4.0/bmon-4.0.tar.gz"
   source_sha256 '02fdc312b8ceeb5786b28bf905f54328f414040ff42f45c83007f24b76cc9f7a'
-  
+
   depends 'libnl3'
-  
+
   def self.build
     system "autoreconf -fiv"
     system "./configure #{CREW_OPTIONS} \
--- packages/speedometer.rb
+++ packages/speedometer.rb
@@ -8,15 +8,15 @@ class Speedometer < Package
   compatibility 'all'
   source_url 'https://httpredir.debian.org/debian/pool/main/s/speedometer/speedometer_2.8-3_all.deb'
   source_sha256 '63e676c96a9df953b4f7f631f1c1d21fedd71d23d159b53e60662c6bb4c19437'
-  
+
   depends_on 'psutil'
   depends_on 'six'
   depends_on 'nuitka' => :build
-  
+
   def self.patch
     system "sed -i 's:#!/usr/bin/python3:#!#{CREW_PREFIX}/bin/python3:' bin/speedometer"
   end
-  
+
   def self.build
     FileUtils.mv "bin/speedometer", "bin/speedometer.py"
     system "python3 -m nuitka --show-progress \
@@ -27,11 +27,11 @@ class Speedometer < Package
             -o bin/speedometer"
     system "gunzip share/man/man1/speedometer.1.gz"
   end
-  
+
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin/"
-    FileUtils.mkdir_p "#{CREW_MAN_PREFIX}/man1/"
+    FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1/"
     FileUtils.cp "bin/speedometer", "#{CREW_DEST_PREFIX}/bin/"
-    FileUtils.cp "share/man/man1/speedometer.1", "#{CREW_MAN_PREFIX}/man1/"
+    FileUtils.cp "share/man/man1/speedometer.1", "#{CREW_DEST_MAN_PREFIX}/man1/"
   end
 end
