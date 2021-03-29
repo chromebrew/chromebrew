22e20e491aa9256fa30eca745545f5b60ca9a20c
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-03-29 18:16:36 -0400

    share/man => man_prefix 2

--- packages/wallstreet.rb
+++ packages/wallstreet.rb
@@ -10,21 +10,21 @@ class Wallstreet < Package
   source_sha256 '793ef1f022b376e131c75e05ff1b55a010c0f4193225bb79018855cb9ab89acb'
 
   # Wallstreet doesn't need binaries
-  
+
   depends_on 'byobu'
   depends_on 'libcaca'
   depends_on 'newsboat'
   depends_on 'ticker'
   depends_on 'wget'
   depends_on 'w3m'
-  
+
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/lib"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/man/man1"
+    FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1"
     FileUtils.cp "./bin/wallstreet", "#{CREW_DEST_PREFIX}/bin/"
     FileUtils.cp_r "./lib/wallstreet/", "#{CREW_DEST_PREFIX}/lib/"
     FileUtils.cp_r "./share/wallstreet/", "#{CREW_DEST_PREFIX}/share/"
-    FileUtils.cp "./share/man/man1/wallstreet.1", "#{CREW_DEST_PREFIX}/share/man/man1/"
+    FileUtils.cp "./share/man/man1/wallstreet.1", "#{CREW_DEST_MAN_PREFIX}/man1/"
   end
 end
