aa1f996ef267ab569b9ee2b9382a6d9db81773fd
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-03-30 12:56:18 -0400

    broken depends line (#5504)

--- packages/bmon.rb
+++ packages/bmon.rb
@@ -9,7 +9,7 @@ class Bmon < Package
   source_url "https://github.com/tgraf/bmon/releases/download/v4.0/bmon-4.0.tar.gz"
   source_sha256 '02fdc312b8ceeb5786b28bf905f54328f414040ff42f45c83007f24b76cc9f7a'
 
-  depends 'libnl3'
+  depends_on 'libnl3'
 
   def self.build
     system "autoreconf -fiv"
