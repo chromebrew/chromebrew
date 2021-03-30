ad1a2198dd5e93ec3ae066390394ba5e58e1c611
satmandu <satadru@umich.edu>
2021-03-30 18:27:57 -0400

    nautilus and deps updates (#5505)
    * nautilus and deps updates
    
    * add license
    
    * Update exempi.rb
    
    * add license
    
    * add license
    
    * add license back
    
    * license added
    
    * cleanup
    
    * Update libimobiledevice.rb
    
    * add license
    
    * add license
    
    * add license
    
    * Remove duplicate deps.
    
    * rebuild
    
    * remove extra quoting
    
    * rebuild
    
    * fix quoted build variables
    
    * Add non-core runtime deps to package files
    
    * remove core deps

---
+++ packages/lmdb.rb
@@ -0,0 +1,59 @@
+# Adapted from Arch Linux lmdb PKGBUILD at:
+# https://github.com/archlinux/svntogit-packages/raw/packages/lmdb/trunk/PKGBUILD
+
+require 'package'
+
+class Lmdb < Package
+  description 'Symas Lightning Memory-Mapped Database'
+  homepage 'https://symas.com/mdb'
+  version '0.9.29'
+  license 'OpenLDAP Public License'
+  compatibility 'all'
+  source_url "https://git.openldap.org/openldap/openldap/-/archive/LMDB_#{version}/openldap-LMDB_#{version}.tar.gz"
+  source_sha256 'd4c668167a2d703ef91db733b4069b8b74dbc374405855be6626b45e2a7e2dd3'
+
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/lmdb-0.9.29-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/lmdb-0.9.29-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/lmdb-0.9.29-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/lmdb-0.9.29-chromeos-x86_64.tar.xz'
+  })
+  binary_sha256({
+    aarch64: '0459de4035dccce3ab4e8ca3b7ee061447d530fbb526e97c20c143e7688dc7a7',
+     armv7l: '0459de4035dccce3ab4e8ca3b7ee061447d530fbb526e97c20c143e7688dc7a7',
+       i686: '8489cc379f9fd20c638b753c8218750f496ef10364897cef6ef9c7121fd1e127',
+     x86_64: '8209022816c95b209a720ba22897064a86f7e6a44006b148c20681a1eba658eb'
+  })
+
+  def self.patch
+    system "sed -i 's,libdir = $(exec_prefix)/lib,libdir = $(exec_prefix)/lib#{CREW_LIB_SUFFIX},g' libraries/liblmdb/Makefile"
+    system "sed -i 's,prefix	= /usr/local,prefix	= #{CREW_PREFIX},g' libraries/liblmdb/Makefile"
+    system "sed -i 's,OPT = -O2 -g,OPT = -O2 -g -flto,g' libraries/liblmdb/Makefile"
+  end
+
+  def self.build
+    Dir.chdir 'libraries/liblmdb' do
+      system "make prefix=#{CREW_PREFIX}"
+    end
+    @lmdb_pc = <<~LMDB_PC_EOF
+      prefix=#{CREW_PREFIX}
+      libdir=#{CREW_LIB_PREFIX}
+      includedir=\${prefix}/include
+
+      Name: liblmdb
+      Description: Lightning Memory-Mapped Database
+      URL: https://symas.com/products/lightning-memory-mapped-database/
+      Version: #{version}
+      Libs: -L\${libdir} -llmdb
+      Cflags: -I\${includedir}
+    LMDB_PC_EOF
+  end
+
+  def self.install
+    Dir.chdir 'libraries/liblmdb' do
+      system "make DESTDIR=#{CREW_DEST_DIR} install"
+    end
+    FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}/pkgconfig/"
+    IO.write("#{CREW_DEST_LIB_PREFIX}/pkgconfig/lmdb.pc", @lmdb_pc)
+  end
+end
---
+++ packages/nautilus.rb
@@ -0,0 +1,82 @@
+# Adapted from Arch Linux nautilus PKGBUILD at:
+# https://github.com/archlinux/svntogit-packages/raw/packages/nautilus/trunk/PKGBUILD
+
+require 'package'
+
+class Nautilus < Package
+  description 'Default file manager for GNOME'
+  homepage 'https://wiki.gnome.org/Apps/Files'
+  @_ver = '40.0'
+  version @_ver
+  license 'GPLv3'
+  compatibility 'all'
+  source_url "https://gitlab.gnome.org/GNOME/nautilus/-/archive/#{@_ver}/nautilus-#{@_ver}.tar.bz2"
+  source_sha256 '9bcb93c5ce56dbe1cd2b2d0808c21a5e37cc1d098ee037b7da75c0a4a59e84e7'
+
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/nautilus-40.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/nautilus-40.0-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/nautilus-40.0-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/nautilus-40.0-chromeos-x86_64.tar.xz'
+  })
+  binary_sha256({
+    aarch64: '7343e4245acf8b1bce3c8ad9208a6d3981edc0bb7997071758c7223399334c65',
+     armv7l: '7343e4245acf8b1bce3c8ad9208a6d3981edc0bb7997071758c7223399334c65',
+       i686: '286c26c5c8b89a8a52ff7ee8f55b516207447471868b311cf8c206722555158b',
+     x86_64: 'bae97c6259459844e5cf945160e162734edbc66ce50eb3965e1c55b9932a6564'
+  })
+
+  depends_on 'appstream_glib' => :build
+  depends_on 'atk'
+  depends_on 'cairo'
+  depends_on 'dconf'
+  depends_on 'gdk_pixbuf'
+  depends_on 'gexiv2'
+  depends_on 'glib'
+  depends_on 'gnome_autoar'
+  depends_on 'gnome_desktop'
+  depends_on 'gobject_introspection' => :build
+  depends_on 'gst_plugins_base'
+  depends_on 'gstreamer'
+  depends_on 'gtk3'
+  depends_on 'gtk_doc' => :build
+  depends_on 'gvfs'
+  depends_on 'libhandy'
+  depends_on 'libportal'
+  depends_on 'pango'
+  depends_on 'tracker3'
+  depends_on 'tracker3_miners'
+
+  def self.patch
+    # Source has libgnome-volume-control repo as submodule
+    @git_dir = 'subprojects/libgd'
+    @git_hash = 'c7c7ff4e05d3fe82854219091cf116cce6b19de0'
+    @git_url = 'https://gitlab.gnome.org/GNOME/libgd.git'
+    FileUtils.rm_rf(@git_dir)
+    FileUtils.mkdir_p(@git_dir)
+    Dir.chdir @git_dir do
+      system 'git init'
+      system "git remote add origin #{@git_url}"
+      system "git fetch --depth 1 origin #{@git_hash}"
+      system 'git checkout FETCH_HEAD'
+    end
+  end
+
+  def self.build
+    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    -Ddocs=false \
+    -Dpackagekit=false \
+    -Dnetworkmanager=false \
+    builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+  end
+  
+  def self.postinstall
+    system "glib-compile-schemas #{CREW_PREFIX}/share/glib-2.0/schemas/"
+  end
+end
