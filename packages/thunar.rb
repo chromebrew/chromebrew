172505caa6a20d79ca1a46515dcd23289ea37246
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-04-01 14:28:05 +0800

    Update thunar.rb

--- packages/thunar.rb
+++ packages/thunar.rb
@@ -3,9 +3,10 @@ require 'package'
 class Thunar < Package
   description 'Thunar File Manager'
   homepage 'https://docs.xfce.org/xfce/thunar/Start'
-  version '4.16.1'
+  @_ver = '4.16.1'
+  version @_ver + '-1'
   compatibility 'all'
-  source_url "https://archive.xfce.org/src/xfce/thunar/4.16/thunar-#{version}.tar.bz2"
+  source_url "https://archive.xfce.org/src/xfce/thunar/4.16/thunar-#{@_ver}.tar.bz2"
   source_sha256 'da2d17d2cb69eb33768690b714cc232ed367cbd71eb9543aaa2a805d05dc3ce1'
 
   binary_url ({
@@ -29,23 +30,22 @@ class Thunar < Package
   depends_on 'desktop_file_utilities'
   depends_on 'hicolor_icon_theme'
   depends_on 'xdg_base'
-  depends_on 'wayland_protocols'
-  depends_on 'mesa'
+  depends_on 'gui_setup'
 
   def self.build
     system "./configure #{CREW_OPTIONS} --enable-gio-unix --enable-gudev --enable-exif --enable-pcre --disable-static --enable-notifications"
-    system "make -j#{CREW_NPROC}"
+    system 'make'
   end
 
   def self.install
-    system "make install DESTDIR=#{CREW_DEST_DIR}"
-    system "cat <<'EOF'> Thunar
-WAYLAND_DISPLAY=wayland-0
-GDK_BACKEND=wayland
-DISPLAY=
-#{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar $@
-EOF"
-    system "install -Dm755 Thunar #{CREW_DEST_PREFIX}/bin/Thunar"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    @file <<~EOF
+      WAYLAND_DISPLAY=${WAYLAND_DISPLAY:-wayland-0}
+      GDK_BACKEND=${GDK_BACKEND:-wayland}
+      exec #{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar "$@"
+    EOF
+    File.write("#{CREW_DEST_PREFIX}/bin/Thunar", @file)
+    File.chmod(0755, "#{CREW_DEST_PREFIX}/bin/Thunar")
     # FileUtils.ln_sf "#{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar", "#{CREW_DEST_PREFIX}/bin/Thunar"
   end
 end
