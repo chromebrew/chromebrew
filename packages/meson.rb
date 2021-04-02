3c8d53dd4f575979e60bd03f06329402041b6506
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-02 07:15:19 -0400

    Undouble dependencies

--- packages/meson.rb
+++ packages/meson.rb
@@ -10,7 +10,7 @@ class Meson < Package
   source_url "https://github.com/mesonbuild/meson/archive/#{@_ver}.tar.gz"
   source_sha256 '0c043c9b5350e9087cd4f6becf6c0d10b1d618ca3f919e0dcca2cdf342360d5d'
 
-  depends_on 'ninja'
+  depends_on 'samu'
   depends_on 'ninja'
 
   binary_url({
