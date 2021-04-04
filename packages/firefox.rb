fd3a31bf4b826ab86bf926ab2182c419e337c732
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-04-05 00:10:45 +0800

    Update firefox.rb

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,35 +3,46 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '85.0.2'
-  compatibility 'i686,x86_64'
+  compatibility 'all'
+  
+  version '87.0' if ARCH =~ /^(i686|x86_64)/
+  
   case ARCH
   when 'i686'
     source_url "https://archive.mozilla.org/pub/firefox/releases/#{version}/linux-i686/en-US/firefox-#{version}.tar.bz2"
-    source_sha256 '83ace0384a14e383e65716ef9305d72e39832b855ebecf2035e6267b4c9a97a5'
+    source_sha256 '9127aee106dd9f09fac0c3cb89c5d75553384da4ec9be5943b60a5f55f31fccc'
   when 'x86_64'
     source_url "https://archive.mozilla.org/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
-    source_sha256 '98763f4b1526811967d71e1bbb9552a9a3fd877321ecb497083b9e313b528c31'
+    source_sha256 '3c9207bee0a998634c4fd12293acfae207d16508749ad405bf1e8717d06acf02'
+  when 'armv7l', 'aarch64'
+    @_arm = true
+    version '87.0-2' # follow debian patch
+    source_url "https://http.us.debian.org/debian/pool/main/f/firefox/firefox_#{version}_armhf.deb"
+    source_sha256 'bd4c13a8cf824c9c9c4f5dbb7ab3d0c1dda3bb5ddc99c53632509f835814e4c1'
   end
 
-  case ARCH
-  when 'i686', 'x86_64'
-    depends_on 'gtk3'
-    depends_on 'apulse'
-    depends_on 'sommelier'
-  end
+  depends_on 'gtk3'
+  depends_on 'apulse'
+  depends_on 'sommelier'
 
-# To get sound working, used : https://codelab.wordpress.com/2017/12/11/firefox-drops-alsa-apulse-to-the-rescue/
-
-  def self.build
-    system "echo '#!/bin/bash' > firefox.sh"
-    system "echo 'cd #{CREW_PREFIX}/firefox' >> firefox.sh"
-    system "echo 'apulse ./firefox \"\$@\"' >> firefox.sh"
+  def self.patch    
+    @_wrapper = <<~EOF
+      #!/bin/sh
+      # To get sound working, used : https://codelab.wordpress.com/2017/12/11/firefox-drops-alsa-apulse-to-the-rescue/
+      
+      exec apulse #{CREW_LIB_PREFIX}/firefox/firefox "$@"
+      EOF
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/firefox"
-    FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/firefox"
-    system "install -Dm755 firefox.sh #{CREW_DEST_PREFIX}/bin/firefox"
+    if @_arm
+      FileUtils.mv(Dir['./*'], CREW_DEST_PREFIX)
+    else
+      # follow official armhf deb, install to lib instead of share
+      FileUtils.mkdir_p [CREW_DEST_LIB_PREFIX, "#{CREW_DEST_PREFIX}/bin/"]
+      FileUtils.mv('.', "#{CREW_DEST_LIB_PREFIX}/firefox")
+    end
+    File.write("#{CREW_DEST_PREFIX}/bin/firefox", @_wrapper)
+    File.chmod(0755, "#{CREW_DEST_PREFIX}/bin/firefox")
   end
 end
