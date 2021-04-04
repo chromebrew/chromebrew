271beceed88e5724158585d0575b9f228b13fe84
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-04-04 18:48:51 +0800

    Update gui_setup.rb

--- packages/gui_setup.rb
+++ packages/gui_setup.rb
@@ -12,7 +12,7 @@ class Gui_setup < Package
   def self.preflight
     # exo wayland compositor is required for sommelier
     abort 'Graphical environment is not supported in your Chrome OS version'.lightred \
-      unless ( begin; File.stat('/var/run/chrome/wayland-').socket?; rescue Errno::ENOENT; false; end )
+      unless ( begin; File.stat('/var/run/chrome/wayland-0').socket?; rescue Errno::ENOENT; false; end )
 
     puts <<~EOT
       Select display server:
