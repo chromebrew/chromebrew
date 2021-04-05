4da2805907aa42f05ea869bef19385ec324e6019
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-04-05 17:13:21 +0800

    fix glibc version (#8)
    * fix glibc version
    
    * Update firefox.rb
    
    * Update firefox.rb

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -4,60 +4,49 @@ class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
   
-  # follow debian patch
-  version '87.0-2'
+  # follow ubuntu patch
+  @_ver = '87.0+build3-0ubuntu0.16.04.2'
+  version @_ver.split('+', 2).first
   license 'MPL-2.0, GPL-2 and LGPL-2.1'
   compatibility 'all'
 
+  # get update on http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu/pool/main/f/firefox/
+  
   case ARCH
   when 'i686'
     @_arch = 'i386'
-    source_sha256 '5b755e6e1c7c6f508507860b96fdd758c80218940ff6bf67b5508feb6860133e'
+    source_sha256 'b338a2c287e7e40bfcc13f93c42d26f1bd78615cec9022e7bcc00a870e1603cf'
   when 'x86_64'
     @_arch = 'amd64'
-    source_sha256 '6c59098a23a46ccf376e8f608d04c1765e10e265fa5086e8a622591a415e007e'
+    source_sha256 '74b13e650ff9f68fb8fc1fd1acf870a5c28f1b22c6900436bdc6019d8207c901'
   when 'armv7l', 'aarch64'
     @_arch = 'armhf'
-    source_sha256 'bd4c13a8cf824c9c9c4f5dbb7ab3d0c1dda3bb5ddc99c53632509f835814e4c1'
+    source_sha256 '6b9e10346af4df382e01a7ae86f6f66b1e8f6838f263da31c7cfaf445e991677'
   end
   
-  source_url "http://ftp.us.debian.org/debian/pool/main/f/firefox/firefox_#{version}_#{@_arch}.deb"
+  source_url "http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu/pool/main/f/firefox/firefox_#{@_ver}_#{@_arch}.deb"
 
-  depends_on 'atk'
-  depends_on 'cairo'
   depends_on 'dbus'
   depends_on 'dbus_glib'
-  depends_on 'fontconfig'
-  depends_on 'freetype'
-  depends_on 'gdk_pixbuf'
-  depends_on 'glib'
   depends_on 'gtk2'
   depends_on 'gtk3'
-  depends_on 'libx11'
-  depends_on 'libxcb'
-  depends_on 'libxcomposite'
-  depends_on 'libxcursor'
-  depends_on 'libxdamage'
-  depends_on 'libxext'
-  depends_on 'libxfixes'
-  depends_on 'libxi'
-  depends_on 'libxrender'
-  depends_on 'libxt'
-  depends_on 'pango'
   depends_on 'pulseaudio'
   depends_on 'sommelier'
 
-  def self.patch   
-    @_wrapper = <<~EOF
-      #!/bin/sh
-      # To get sound working, used : https://codelab.wordpress.com/2017/12/11/firefox-drops-alsa-apulse-to-the-rescue/
-      
-      exec apulse #{CREW_PREFIX}/lib/firefox/firefox "$@"
-    EOF
+  def self.patch
+    system('sed', '-i', "s:MOZ_LIBDIR=/usr/lib/firefox:MOZ_LIBDIR=#{CREW_PREFIX}/lib/firefox:", './usr/lib/firefox/firefox.sh')
     
-    FileUtils.rm('./usr/bin/firefox')
-    File.write('./usr/bin/firefox', @_wrapper)
-    File.chmod(0755, './usr/bin/firefox')
+    @_about = <<~EOF
+      [Global]
+      id=cros
+      version=1.0
+      about=Mozilla Firefox for Chrome OS, adapted from Ubuntu
+
+      [Preferences]
+      app.distributor="chromebrew"
+      app.distributor.channel="cros"
+    EOF
+    File.write('./usr/lib/firefox/distribution/distribution.ini', @_about)
   end
 
   def self.install
