4174b54e73b4ac18f973492c93a5cf29ecb543bb
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-04-05 11:47:47 +0800

    Update firefox.rb

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,23 +3,25 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '87.0' if ARCH =~ /^(i686|x86_64)/
+  
+  # follow debian patch
+  version '87.0-2'
   license 'MPL-2.0, GPL-2 and LGPL-2.1'
   compatibility 'all'
 
   case ARCH
   when 'i686'
-    source_url "https://archive.mozilla.org/pub/firefox/releases/#{version}/linux-i686/en-US/firefox-#{version}.tar.bz2"
-    source_sha256 '9127aee106dd9f09fac0c3cb89c5d75553384da4ec9be5943b60a5f55f31fccc'
+    @_arch = 'i386'
+    source_sha256 '5b755e6e1c7c6f508507860b96fdd758c80218940ff6bf67b5508feb6860133e'
   when 'x86_64'
-    source_url "https://archive.mozilla.org/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
-    source_sha256 '3c9207bee0a998634c4fd12293acfae207d16508749ad405bf1e8717d06acf02'
+    @_arch = 'amd64'
+    source_sha256 '6c59098a23a46ccf376e8f608d04c1765e10e265fa5086e8a622591a415e007e'
   when 'armv7l', 'aarch64'
-    @_arm = true
-    version '87.0-2' # follow debian patch
-    source_url "http://http.us.debian.org/debian/pool/main/f/firefox/firefox_#{version}_armhf.deb"
+    @_arch = 'armhf'
     source_sha256 'bd4c13a8cf824c9c9c4f5dbb7ab3d0c1dda3bb5ddc99c53632509f835814e4c1'
   end
+  
+  source_url "http://ftp.us.debian.org/debian/pool/main/f/firefox/firefox_#{version}_#{@_arch}.deb"
 
   depends_on 'atk'
   depends_on 'cairo'
@@ -50,18 +52,15 @@ class Firefox < Package
       #!/bin/sh
       # To get sound working, used : https://codelab.wordpress.com/2017/12/11/firefox-drops-alsa-apulse-to-the-rescue/
       
-      exec apulse #{CREW_LIB_PREFIX}/firefox/firefox "$@"
+      exec apulse #{CREW_PREFIX}/lib/firefox/firefox "$@"
     EOF
   end
 
   def self.install
-    if @_arm
-      FileUtils.mv(Dir['./*'], CREW_DEST_PREFIX)
-    else
-      # follow official armhf deb, install to lib instead of share
-      FileUtils.mkdir_p ["#{CREW_DEST_LIB_PREFIX}/firefox/", "#{CREW_DEST_PREFIX}/bin/"]
-      FileUtils.mv(Dir['./*'], "#{CREW_DEST_LIB_PREFIX}/firefox/")
-    end
+    FileUtils.mkdir_p(CREW_DEST_PREFIX)
+    FileUtils.mv('./etc/', CREW_DEST_PREFIX)
+    FileUtils.mv(Dir['./usr/*'], CREW_DEST_PREFIX)
+    
     File.write("#{CREW_DEST_PREFIX}/bin/firefox", @_wrapper)
     File.chmod(0755, "#{CREW_DEST_PREFIX}/bin/firefox")
   end
