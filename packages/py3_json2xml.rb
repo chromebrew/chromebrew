e763957c317608280f4fa0bb9c5fef6949f1f716
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 11:52:03 -0400

    fix json2xml

--- packages/py3_json2xml.rb
+++ packages/py3_json2xml.rb
@@ -1,6 +1,6 @@
 require 'package'
 
-class Json2xml < Package
+class Py3_json2xml < Package
   description 'json to xml converter'
   homepage 'https://github.com/vinitkumar/json2xml'
   version '2.1.0-1'
@@ -9,21 +9,14 @@ class Json2xml < Package
   source_url 'https://github.com/vinitkumar/json2xml/archive/v2.1.0.tar.gz'
   source_sha256 '61c7c848f7dc74d09ecc10badaa69902859483ea4e3b13148a83173af564aeea'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/json2xml-2.1.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/json2xml-2.1.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/json2xml-2.1.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/json2xml-2.1.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'a7456faac3802bfffa003a845d7f7f12869e02796542b97b0539940ca6222061',
-     armv7l: 'a7456faac3802bfffa003a845d7f7f12869e02796542b97b0539940ca6222061',
-       i686: 'ed8b21bbcc5b6bc8d92eb5e36ff827de365e21a70c30537cf955dfb096833f8b',
-     x86_64: 'cea573afae8c6f6439610041160f11aef73988eac87708d875b5be0cbef77043',
-  })
-
   depends_on 'libxslt'
-  depends_on 'setuptools'
+  depends_on 'py3_dicttoxml'
+  depends_on 'py3_xmltodict'
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "CFLAGS=-I#{CREW_PREFIX}/include/libxml2 python3 setup.py build #{CREW_SETUP_PY3_BUILD_OPTIONS}"
+  end
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
@@ -37,6 +30,6 @@ class Json2xml < Package
       system "echo 'fi' >> json2xml"
       system "chmod +x json2xml"
     end
-    system "CFLAGS=-I#{CREW_PREFIX}/include/libxml2 pip3 install . --root #{CREW_DEST_DIR} --prefix #{CREW_PREFIX}"
+    system "CFLAGS=-I#{CREW_PREFIX}/include/libxml2 python3 setup.py install #{CREW_SETUP_PY_INSTALL_OPTIONS}"
   end
 end
