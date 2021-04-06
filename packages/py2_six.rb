e4f3d5541b07d32a055ce32886388912403c095e
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 14:13:02 -0400

    use py2_setuptools to build py2_six

--- packages/py2_six.rb
+++ packages/py2_six.rb
@@ -10,7 +10,7 @@ class Py2_six < Package
   source_sha256 '36252a752837b72f60def78bc408a05c21d22fe00a34a7dc3409d96b2c6e20c8'
 
   depends_on 'python27'
-  depends_on 'py3_setuptools' => :build
+  depends_on 'py2_setuptools' => :build
 
   def self.build
     system "python2 setup.py build #{CREW_SETUP_PY2_BUILD_OPTIONS}"
