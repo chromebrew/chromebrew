3373a9be0e7ca573751f137f95fdf9e41649281e
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 11:52:28 -0400

    fix mesa

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -3,25 +3,12 @@ require 'package'
 class Mesa < Package
   description 'Open-source implementation of the OpenGL specification'
   homepage 'https://www.mesa3d.org'
-  @_ver = '21.0.1'
+  @_ver = '21.0.1-1'
   version @_ver
   license 'MIT'
   compatibility 'all'
   source_url "https://mesa.freedesktop.org/archive/mesa-#{@_ver}.tar.xz"
   source_sha256 '379fc984459394f2ab2d84049efdc3a659869dc1328ce72ef0598506611712bb'
-
-  binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.1-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    aarch64: '281a8b7b6cc4009fc39020151176e56a8f2f695c02c86fd04b81887484183a38',
-     armv7l: '281a8b7b6cc4009fc39020151176e56a8f2f695c02c86fd04b81887484183a38',
-       i686: 'd18cebf293137b55ba21b7daf8b015f11e787e34592b180bc3b904a8311798f9',
-     x86_64: 'f82724f2890c1a8a6ceb768826cbe17301580286d47d365bbc425943af22cd6c'
-  })
 
   depends_on 'elfutils'
   depends_on 'glslang' => :build
@@ -40,6 +27,7 @@ class Mesa < Package
   depends_on 'libxvmc'
   depends_on 'libxxf86vm'
   depends_on 'lm_sensors'
+  depends_on 'py3_mako'
   depends_on 'valgrind' => :build
   depends_on 'vulkan_headers' => :build
   depends_on 'vulkan_icd_loader' => :build
@@ -55,9 +43,6 @@ class Mesa < Package
       @vk = 'auto'
       @galliumdrivers = 'auto'
     end
-    system 'pip3 uninstall -y Mako MarkupSafe || :'
-    system "pip3 install --no-warn-script-location --prefix \"#{CREW_PREFIX}\" --root \"#{CREW_DEST_DIR}\" Mako"
-    system "pip3 install --prefix \"#{CREW_PREFIX}\" Mako"
     system "meson #{CREW_MESON_LTO_OPTIONS} \
     -Db_asneeded=false \
     -Dvulkan-drivers=#{@vk} \
