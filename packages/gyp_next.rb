a8d46ab03537ae63d5db4f77287e2887c085e45c
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 11:39:04 -0400

    fix gyp_next

--- packages/gyp_next.rb
+++ packages/gyp_next.rb
@@ -1,29 +1,19 @@
 require 'package'
 
 class Gyp_next < Package
-  description 'GYP can Generate Your Projects.'
+  description 'GYP can Generate Your Projects'
   homepage 'https://github.com/nodejs/gyp-next'
-  version '0.8.0'
+  version '0.8.1'
   license 'BSD'
   compatibility 'all'
-  source_url 'file:///dev/null'
-  source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  source_url 'https://github.com/nodejs/gyp-next/archive/v0.8.1.tar.gz'
+  source_sha256 '2387f884799b561bc8e4e982e66b8a825147aa17d2d08c42473f08f8152e245a'
 
-  binary_url({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gyp_next-0.8.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gyp_next-0.8.0-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gyp_next-0.8.0-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gyp_next-0.8.0-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    aarch64: '6f9637bbb731df38ebe855839e40e7c390127fc391e37428acf9ab978e73607b',
-     armv7l: '6f9637bbb731df38ebe855839e40e7c390127fc391e37428acf9ab978e73607b',
-       i686: '9662dcd813ad9d11c600d92f00cadc549ea787533c228b34bcf1b28e5cb9d180',
-     x86_64: '4e94350d25225bc3ccc8a2b32d710ccef16ea2ce637969774ccccc8b85d8edd9'
-  })
+  def self.build
+    system "python3 setup.py build #{CREW_SETUP_PY3_BUILD_OPTIONS}"
+  end
 
   def self.install
-    system 'pip3 uninstall -y gyp-next'
-    system "pip3 install --upgrade --no-warn-script-location gyp-next --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
+    system "python3 setup.py install #{CREW_SETUP_PY_INSTALL_OPTIONS}"
   end
 end
