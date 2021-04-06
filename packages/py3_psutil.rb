e527f4b8e098877301f34d03068405fe4d31f607
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 11:34:04 -0400

    fix psutil

--- packages/py3_psutil.rb
+++ packages/py3_psutil.rb
@@ -1,15 +1,21 @@
 require 'package'
 
-class Psutil < Package
+class Py3_psutil < Package
   description 'psutil (process and system utilities) is a cross-platform library for retrieving information on running processes and system utilization in Python.'
   homepage 'https://github.com/giampaolo/psutil'
-  version '5.8.0'
+  version '5.8.0-1'
   license 'BSD-3'
   compatibility 'all'
-  source_url 'https://github.com/giampaolo/psutil/archive/refs/tags/release-5.8.0.tar.gz'
+  source_url 'https://github.com/giampaolo/psutil/archive/release-5.8.0.tar.gz'
   source_sha256 'c015248da36109ffaa15f46fce8c0003f678cfaa408090f466c80318885e7abf'
 
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{CREW_SETUP_PY3_BUILD_OPTIONS}"
+  end
+
   def self.install
-    system "pip install --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR} -I psutil==#{version} --no-warn-script-location"
+    system "python3 setup.py install #{CREW_SETUP_PY_INSTALL_OPTIONS}"
   end
 end
