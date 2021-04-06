d94dabafdc591fd256436f9a928489b30bc7f01b
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 11:42:30 -0400

    add mako

---
+++ packages/py3_mako.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Py3_mako < Package
+  description 'Mako is a template library written in Python.'
+  homepage 'https://www.makotemplates.org/'
+  version '1.1.4'
+  license 'MIT'
+  compatibility 'all'
+  source_url 'https://github.com/sqlalchemy/mako/archive/rel_1_1_4.tar.gz'
+  source_sha256 '21a46722e5b54a0e02c6d09e3b353f025975b7da7a4a4f25d6813d84be452168'
+
+  depends_on 'py3_markupsafe'
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{CREW_SETUP_PY3_BUILD_OPTIONS}"
+  end
+
+  def self.install
+    system "python3 setup.py install #{CREW_SETUP_PY_INSTALL_OPTIONS}"
+  end
+end
