8ec8186bdc2e4e3fc5b61bdee43e9500b37f2143
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 11:58:13 -0400

    fix urlwatch

--- packages/urlwatch.rb
+++ packages/urlwatch.rb
@@ -1,29 +1,29 @@
 require 'package'
 
 class Urlwatch < Package
-  description 'A tool for monitoring webpages for updates'
+  description 'urlwatch helps you watch changes in webpages.'
   homepage 'https://thp.io/2008/urlwatch/'
-  version '2.7-1'
+  version '2.22'
   license 'BSD'
   compatibility 'all'
-  source_url 'https://github.com/thp/urlwatch/archive/2.7.tar.gz'
-  source_sha256 '0a7b46d161501463dda99a1d82daf00237e2627616beaa6ccbf04d6c70c68ded'
+  source_url 'https://github.com/thp/urlwatch/archive/2.22.tar.gz'
+  source_sha256 '951148da670777541a4698c9e6cf46d20c15d4982815bbac17dfb2af64ce9e06'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/urlwatch-2.7-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/urlwatch-2.7-1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/urlwatch-2.7-1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/urlwatch-2.7-1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'daab4655856fab857ec59c3c946aceaba592a74e2e655473ec5189bf2370326e',
-     armv7l: 'daab4655856fab857ec59c3c946aceaba592a74e2e655473ec5189bf2370326e',
-       i686: 'a19bcb80f178fffd27170d30ebda7f4b113f59ed4249a1c3ca36100e6a589072',
-     x86_64: '7c41c7dddfcc0d6ed8563019c047a1c940fe33b1b920b6071a3be8e432367c53',
-  })
+  depends_on 'py3_pyyaml'
+  depends_on 'py3_minidb'
+  depends_on 'py3_requests'
+  depends_on 'py3_keyring'
+  depends_on 'py3_appdirs'
+  depends_on 'py3_chump'
+  depends_on 'py3_pushbullet_py'
+  depends_on 'py3_pycodestyle'
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{CREW_SETUP_PY3_BUILD_OPTIONS}"
+  end
 
   def self.install
-    system "python3 -m pip install pyyaml minidb requests keyring appdirs chump pushbullet.py pycodestyle --root #{CREW_DEST_DIR}"
-    system "pip3 install urlwatch --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
+    system "python3 setup.py install #{CREW_SETUP_PY_INSTALL_OPTIONS}"
   end
 end
