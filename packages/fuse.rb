5d755eda6073694dab14b9731b96c266ef9e80be
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 12:00:28 -0400

    fix fuse

--- packages/fuse.rb
+++ packages/fuse.rb
@@ -9,6 +9,8 @@ class Fuse < Package
   source_url "https://github.com/libfuse/libfuse/releases/download/fuse-#{version}/fuse-#{version}.tar.xz"
   source_sha256 '736e8d1ce65c09cb435fbbb500d53dc75f4d6e93bd325d22adc890951cf56337'
 
+  depends_on 'py3_pytest' => :build
+  
   binary_url({
     aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/fuse-3.10.2-chromeos-armv7l.tar.xz',
      armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/fuse-3.10.2-chromeos-armv7l.tar.xz',
@@ -23,7 +25,6 @@ class Fuse < Package
   })
 
   def self.build
-    system "pip install --upgrade --no-warn-script-location pytest --prefix #{CREW_PREFIX}"
     system "meson #{CREW_MESON_LTO_OPTIONS} \
     -Ddisable-mtab=true \
     -Dudevrulesdir=#{CREW_PREFIX}/etc/udev/rules.d/ \
@@ -39,6 +40,5 @@ class Fuse < Package
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/init.d/"
     FileUtils.mv "#{CREW_DEST_DIR}/etc/init.d/fuse3", "#{CREW_DEST_PREFIX}/etc/init.d/fuse3"
     FileUtils.mv "#{CREW_DEST_PREFIX}/sbin/mount.fuse3", "#{CREW_DEST_PREFIX}/bin/mount.fuse3"
-    system 'pip uninstall -y pytest'
   end
 end
