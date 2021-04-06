040573a8b906657c9928371a7392289cde9907af
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 11:45:43 -0400

    fix thef*ck

--- packages/thefuck.rb
+++ packages/thefuck.rb
@@ -1,19 +1,30 @@
 require 'package'
 
 class Thefuck < Package
-  description 'Magnificent app which corrects your previous console command.'
-  homepage 'https://github.com/nvbn/thefuck'
-  version '3.26'
+  description 'TheF*ck is a magnificent app which corrects your previous console command.'
+  homepage 'https://github.com/nvbn/thefuck/'
+  version '3.30'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://raw.githubusercontent.com/nvbn/thefuck/3.26/README.md'
-  source_sha256 '105e10e37c4c1430fe6bc3a48c9508254048721825ae14856d713de61c95bc66'
+  source_url 'https://github.com/nvbn/thefuck/archive/3.30.tar.gz'
+  source_sha256 'dd2e7bc94e9453beabbafc10c4d62c67601ea705250103a67ababcdad2991dd3'
+
+  depends_on 'py3_colorama'
+  depends_on 'py3_decorator'
+  depends_on 'py3_psutil'
+  depends_on 'py3_pyte'
+  depends_on 'py3_six'
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{CREW_SETUP_PY3_BUILD_OPTIONS}"
+  end
 
   def self.install
-    system "pip3 install thefuck --root #{CREW_DEST_DIR} --prefix #{CREW_PREFIX} --no-warn-script-location"
+    system "python3 setup.py install #{CREW_SETUP_PY_INSTALL_OPTIONS}"
   end
 
   def self.postinstall
-    system 'fuck && fuck'
+    system 'fuck'
   end
 end
