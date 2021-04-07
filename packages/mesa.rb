015ad9dd2806b99c889a8ff20d51359943c18d93
satmandu <satadru@gmail.com>
2021-04-07 14:37:55 -0400

    Update Mesa to 21.0.2

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -3,12 +3,12 @@ require 'package'
 class Mesa < Package
   description 'Open-source implementation of the OpenGL specification'
   homepage 'https://www.mesa3d.org'
-  @_ver = '21.0.1-1'
+  @_ver = '21.0.2'
   version @_ver
   license 'MIT'
   compatibility 'all'
   source_url "https://mesa.freedesktop.org/archive/mesa-#{@_ver}.tar.xz"
-  source_sha256 '379fc984459394f2ab2d84049efdc3a659869dc1328ce72ef0598506611712bb'
+  source_sha256 '46c1dc5bb54a372dee43ec3c067229c299187d5bdadf1402756bbf66a6df5b88'
 
   depends_on 'elfutils'
   depends_on 'glslang' => :build
