558fe7867960e025eaa5377acb82b86d205c4b5b
satmandu <satadru@gmail.com>
2021-04-07 16:20:02 -0400

    mesa binaries

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -10,6 +10,19 @@ class Mesa < Package
   source_url "https://mesa.freedesktop.org/archive/mesa-#{@_ver}.tar.xz"
   source_sha256 '46c1dc5bb54a372dee43ec3c067229c299187d5bdadf1402756bbf66a6df5b88'
 
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/mesa-21.0.2-chromeos-x86_64.tar.xz'
+  })
+  binary_sha256({
+    aarch64: '535d0993748c3331486a2a48f380898244d223f355306374fd7761260871731a',
+     armv7l: '535d0993748c3331486a2a48f380898244d223f355306374fd7761260871731a',
+       i686: 'dd315ca82272212afa69914bc1f4a676fd0d5f1e35f7e11c1264c569afed360d',
+     x86_64: '0d69570884f25ca4ea75635c05d60652148397d51afecedc92cf3dcee6955361'
+  })
+  
   depends_on 'elfutils'
   depends_on 'glslang' => :build
   depends_on 'libdrm'
