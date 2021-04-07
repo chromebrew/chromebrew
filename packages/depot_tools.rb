13a5a5de599cb40a929223f70f6fcfca154dbb5d
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-06 22:28:22 -0400

    fix depot tools

--- packages/depot_tools.rb
+++ packages/depot_tools.rb
@@ -10,28 +10,27 @@ class Depot_tools < Package
   source_url 'file:///dev/null'
   source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
-  binary_url({
+  binary_url ({
   })
-  binary_sha256({
+  binary_sha256 ({
   })
 
   depends_on 'xdg_base'
 
   def self.install
-    FileUtils.rm_rf 'depot_tools'
-    system 'git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git --depth 10 depot_tools'
+    system "git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git --depth 10"
     Dir.chdir 'depot_tools' do
       system "git checkout #{@_ver}"
-      FileUtils.rm_rf 'man/src'
+      FileUtils.rm_rf 'man/src/'
       FileUtils.rm_rf Dir.glob('.git*')
-      system 'find -name \'*.bat\' -exec rm {} +'
-      FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/depot_tools"
-      FileUtils.mkdir_p CREW_DEST_MAN_PREFIX
-      FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/doc/depot_tools"
-      FileUtils.cp_r Dir.glob('man/html/*'), "#{CREW_DEST_PREFIX}/share/doc/depot_tools"
-      FileUtils.rm_rf 'man/src'
-      FileUtils.mv 'man/', CREW_DEST_MAN_PREFIX
-      FileUtils.mv '.', "#{CREW_DEST_PREFIX}/share/depot_tools"
+      system 'find -name \'*.bat\' -delete'
+      FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/depot_tools/"
+      FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/"
+      FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/doc/depot_tools/"
+      FileUtils.mv Dir.glob('man/html/*.html'), "#{CREW_DEST_PREFIX}/share/doc/depot_tools/"
+      FileUtils.mv Dir.glob('man/*'), "#{CREW_DEST_MAN_PREFIX}"
+      FileUtils.rm_r 'man/'
+      FileUtils.mv Dir.glob('*'), "#{CREW_DEST_PREFIX}/share/depot_tools"
       FileUtils.mkdir_p "#{CREW_DEST_HOME}/.config/.vpython-root"
       FileUtils.mkdir_p "#{CREW_DEST_HOME}/.config/.vpython_cipd_cache"
       FileUtils.ln_s "#{HOME}/.config/.vpython-root/", "#{CREW_DEST_HOME}/.vpython-root"
@@ -41,7 +40,7 @@ class Depot_tools < Package
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
     @depot_tools_env = <<~DEPOT_TOOLS_EOF
       # Add depot-tools to path
-      PATH="\$PATH:#{CREW_PREFIX}/share/depot_tools"
+      PATH="$PATH:#{CREW_PREFIX}/share/depot_tools"
     DEPOT_TOOLS_EOF
     IO.write("#{CREW_DEST_PREFIX}/etc/env.d/depot_tools", @depot_tools_env)
   end
