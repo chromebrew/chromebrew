ff8fe2a4bc74dc2cee2c8a9adadc0c7cf679dd94
supechicken <supechicken666@gmail.com>
2021-04-10 02:58:35 -0700

    Update xdg_base.rb

--- packages/xdg_base.rb
+++ packages/xdg_base.rb
@@ -8,6 +8,8 @@ class Xdg_base < Package
   compatibility 'all'
   source_url 'file:///dev/null'
   source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
+  
+  depends_on 'unix_home'
 
   def self.preinstall
     # Save any previous configuration, if it exists.
@@ -22,30 +24,28 @@ class Xdg_base < Package
     end
   end
 
+  def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
+    @xdgbaseenv = <<~XDGBASEEOF
+      set -a
+      # Chromebrew's XDG configuration
+      # See https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+      # XDG Base Directory Specification Environment Variables
+      # All variables will export automatically
+      XDG_CACHE_HOME=#{@HOME}/.cache
+      XDG_CONFIG_DIRS=#{CREW_PREFIX}/etc/xdg
+      XDG_CONFIG_HOME=#{@HOME}/.config
+      XDG_DATA_DIRS=#{CREW_PREFIX}/share
+      XDG_DATA_HOME=#{@HOME}/.local/share
+      XDG_RUNTIME_DIR=/var/run/chrome
+      set +a
+    XDGBASEEOF
+    IO.write("#{CREW_DEST_PREFIX}/etc/env.d/xdg_base", @xdgbaseenv)
+  end
+
   def self.postinstall
     FileUtils.mkdir_p "#{CREW_PREFIX}/.config/.local/share"
     FileUtils.mkdir_p "#{CREW_PREFIX}/.cache"
     FileUtils.mkdir_p "#{CREW_PREFIX}/etc/xdg"
-    @_str = "XDG_CONFIG_HOME=#{CREW_PREFIX}/.config"
-    if `grep -c '#{@_str}' #{HOME}/.bashrc`.to_i.zero?
-      puts 'Putting XDG Environment Variables in ~/.bashrc'.lightblue
-      system "echo '# See https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html' >> ~/.bashrc"
-      system "echo '# XDG Base Directory Specification Environment Variables' >> ~/.bashrc"
-      system "sed -i '/XDG_CACHE_HOME=/d' ~/.bashrc"
-      system "sed -i '/XDG_CONFIG_DIRS/d' ~/.bashrc"
-      system "sed -i '/XDG_CONFIG_HOME/d' ~/.bashrc"
-      system "sed -i '/XDG_DATA_DIRS/d' ~/.bashrc"
-      system "sed -i '/XDG_DATA_HOME/d' ~/.bashrc"
-      system "sed -i '/XDG_RUNTIME_DIR/d' ~/.bashrc"
-      system "echo 'export XDG_CACHE_HOME=#{CREW_PREFIX}/.cache' >> ~/.bashrc"
-      system "echo 'export XDG_CONFIG_DIRS=#{CREW_PREFIX}/etc/xdg' >> ~/.bashrc"
-      system "echo 'export XDG_CONFIG_HOME=#{CREW_PREFIX}/.config' >> ~/.bashrc"
-      system "echo 'export XDG_DATA_DIRS=#{CREW_PREFIX}/share' >> ~/.bashrc"
-      system "echo 'export XDG_DATA_HOME=#{CREW_PREFIX}/.config/.local/share' >> ~/.bashrc"
-      system "echo 'export XDG_RUNTIME_DIR=/var/run/chrome' >> ~/.bashrc"
-      puts 'To complete the installation, execute the following:'.orange
-      puts 'source ~/.bashrc'.orange
-      puts 'or reboot'.red
-    end
   end
 end
