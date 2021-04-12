a9a174359f0c0c1d11cb4436e17c23033d6bf039
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-12 03:33:20 -0400

    install.sh: add crew_profile_base to LATE_PACKAGES

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ EARLY_PACKAGES="gcc10 brotli c_ares libcyrussasl libidn2 libmetalink libnghttp2 
 libtirpc libunistring openldap rtmpdump zstd ncurses ca_certificates ruby libffi openssl \
 nettle krb5 p11kit libtasn1 gnutls curl git icu4c"
 
-LATE_PACKAGES="less most manpages filecmd mawk readline perl pcre pcre2 python27 python3 \
+LATE_PACKAGES="crew_profile_base less most manpages filecmd mawk readline perl pcre pcre2 python27 python3 \
 sed bz2 lz4 lzip unzip xzutils zip"
 
 ARCH="$(uname -m)"
