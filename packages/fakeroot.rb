6b317047d05a5db53fa182d47799f4b2a59cd5cc
satmandu <satadru@umich.edu>
2021-04-12 17:26:52 -0400

    fakeroot 1.25.3 (#5607)

--- packages/fakeroot.rb
+++ packages/fakeroot.rb
@@ -3,34 +3,43 @@ require 'package'
 class Fakeroot < Package
   description 'Run a command in an environment faking root privileges for file manipulation.'
   homepage 'https://wiki.debian.org/FakeRoot'
-  version '1.23'
+  version '1.25.3'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://httpredir.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.23.orig.tar.xz'
-  source_sha256 '009cd6696a931562cf1c212bb57ca441a4a2d45cd32c3190a35c7ae98506f4f6'
+  source_url 'https://httpredir.debian.org/debian/pool/main/f/fakeroot/fakeroot_1.25.3.orig.tar.gz'
+  source_sha256 '8e903683357f7f5bcc31b879fd743391ad47691d4be33d24a76be3b6c21e956c'
 
-  binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.23-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.23-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.23-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.23-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.25.3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.25.3-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.25.3-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/fakeroot-1.25.3-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '2ed598167ed99897379481674aee1637a41197747518eee2f4c062852066b5e8',
-     armv7l: '2ed598167ed99897379481674aee1637a41197747518eee2f4c062852066b5e8',
-       i686: '0295d42fdb5703320ccd2d329092b9a62133e6da160fa7880f11a8f46a635fd0',
-     x86_64: '8d7f3cb9052e264d43aa8c37eda7b7da2e3710f7f29061ada7fbca6c90b1f814',
+  binary_sha256({
+    aarch64: '92c44951d47177c59c5afcb0f58a9628e469f4f29586e85ace47eb84bbfddbe7',
+     armv7l: '92c44951d47177c59c5afcb0f58a9628e469f4f29586e85ace47eb84bbfddbe7',
+       i686: 'b689d26d69b8760b6a083d4119c8240aa26a6d4cf7efb70fd547901badd29ad2',
+     x86_64: 'df77a7108f6dfc836eda895fed8c7b84274828af41d0e5e4eaa1d1ca0ba0cf9d'
   })
 
   depends_on 'libcap'
 
+  def self.patch
+    # Needed since build fails when building other language
+    # manpages.
+    system "sed -i '/SUBDIRS/d' doc/Makefile.am"
+  end
+
   def self.build
     system './bootstrap'
-    system "./configure", "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}"
+    system "env CFLAGS='-flto=auto -fuse-ld=gold' \
+      CXXFLAGS='-pipe -flto=auto -fuse-ld=gold' \
+      LDFLAGS='-flto=auto' \
+      ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
