3db87e2819df50cc4d2acf507a071c3a27189145
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-04-13 20:27:14 -0400

    packages/dash.rb: fix homepage and binaries

--- packages/dash.rb
+++ packages/dash.rb
@@ -2,25 +2,25 @@ require 'package'
 
 class Dash < Package
   description 'The Debian Almquist Shell (dash) is a POSIX-compliant shell derived from ash that executes scripts faster than bash and has fewer library dependencies.'
-  homepage 'https://salsa.debian.org/debian/dash'
+  homepage 'https://invisible-island.net/mawk/'
   version '0.5.11.1'
   license 'BSD'
   compatibility 'all'
   source_url 'https://salsa.debian.org/debian/dash.git'
   git_tag 'upstream/0.5.11.2'
 
-#  binary_url ({
-#    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/dash-0.5.11.1-chromeos-armv7l.tar.xz',
-#     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/dash-0.5.11.1-chromeos-armv7l.tar.xz',
-#       i686: 'https://dl.bintray.com/chromebrew/chromebrew/dash-0.5.11.1-chromeos-i686.tar.xz',
-#     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/dash-0.5.11.1-chromeos-x86_64.tar.xz',
-#  })
-#  binary_sha256 ({
-#    aarch64: '1b44e3da6bfc8c4e10752b14da464d8a04b748025de69423f944fac8035dbb96',
-#     armv7l: '1b44e3da6bfc8c4e10752b14da464d8a04b748025de69423f944fac8035dbb96',
-#       i686: 'ac582d141f234881f09860bc400688b0462d92431a5ef739c59687e09cdb9299',
-#     x86_64: 'fc193b1d169b2341cd99e329552740656706ae3e22867733eac7d2f7873053b8',
-#  })
+  binary_url ({
+    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/dash-0.5.11.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/dash-0.5.11.1-chromeos-armv7l.tar.xz',
+       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/dash-0.5.11.1-chromeos-i686.tar.xz',
+     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/dash-0.5.11.1-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: '1b44e3da6bfc8c4e10752b14da464d8a04b748025de69423f944fac8035dbb96',
+     armv7l: '1b44e3da6bfc8c4e10752b14da464d8a04b748025de69423f944fac8035dbb96',
+       i686: 'ac582d141f234881f09860bc400688b0462d92431a5ef739c59687e09cdb9299',
+     x86_64: 'fc193b1d169b2341cd99e329552740656706ae3e22867733eac7d2f7873053b8',
+  })
 
   depends_on 'libedit'
 
