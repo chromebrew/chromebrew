76924b889c7fbcb7545bbbcfd116a7c913daf9d7
Satadru Pramanik <satadru@gmail.com>
2021-04-15 17:31:34 -0400

    Revert ncurses change. Let's put that in its own dedicated PR.

--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -1,44 +1,66 @@
 require 'package'
 
 class Ncurses < Package
-  description 'The ncurses (new curses) library is a free software emulation of curses in System V Release 4.0 (SVr4), and more. — Wide character'
+  description 'The ncurses (new curses) library is a free software emulation of curses in System V Release 4.0 (SVr4), and more.'
   homepage 'https://www.gnu.org/software/ncurses/'
-  @_ver = 6.2
-  version "#{@_ver}-1"
+  version '6.2-20210206-1'
   license 'MIT'
   compatibility 'all'
-  source_url "https://ftpmirror.gnu.org/ncurses/ncurses-#{@_ver}.tar.gz"
-  source_sha256 '30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d'
+  source_url 'https://github.com/mirror/ncurses/archive/b724cdc89cf31757ab43262ecefe5242b0edc450.zip'
+  source_sha256 'e5e83965329c85d8d28f9a35a71ab785fce015f42f491e7dcafbb9f36ad1eaea'
 
   binary_url({
-    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/ncurses-6.2-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/ncurses-6.2-1-chromeos-armv7l.tar.xz',
-       i686: 'https://downloads.sourceforge.net/project/chromebrew/ncurses-6.2-1-chromeos-i686.tar.xz',
-     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/ncurses-6.2-1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/ncurses-6.2-20210206-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/ncurses-6.2-20210206-1-chromeos-armv7l.tar.xz',
+       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/ncurses-6.2-20210206-1-chromeos-i686.tar.xz',
+     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/ncurses-6.2-20210206-1-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '8f07ad615e960adea4a458cde9b4129bc6b3bf387a4d410f7c343e73cebdde32',
-     armv7l: '8f07ad615e960adea4a458cde9b4129bc6b3bf387a4d410f7c343e73cebdde32',
-       i686: '704f171007a30c8db3e4ac6ffba11b5f2ad69a388f8cf2b0726f2e2178c809bb',
-     x86_64: '508ebaa8dad47d2eb84e499c7d86f96227dbe556e32f71f0cb6744903acf9d6d'
+    aarch64: 'f87816a803fdbb6403ecb03ea4faec04da8c3a829a1455a2c9e0a0686818ebb6',
+     armv7l: 'f87816a803fdbb6403ecb03ea4faec04da8c3a829a1455a2c9e0a0686818ebb6',
+       i686: 'ad1fb3a7d832d4fcf4e695cd5f5f7e023581c2d0529a2bf9729acd2cd96e44cd',
+     x86_64: 'd81416072e4e4454b585495db8b7e0767867c851e512afd4655fa8174d244c02'
   })
 
   def self.build
     # build libncursesw
     FileUtils.mkdir 'ncursesw_build'
     Dir.chdir 'ncursesw_build' do
-      system "env #{CREW_ENV_OPTIONS} \
-        ../configure #{CREW_OPTIONS} \
-          --program-prefix='' \
-          --program-suffix='' \
-          --with-shared \
-          --with-cxx-shared \
-          --without-debug \
-          --enable-pc-files \
-          --with-pkg-config-libdir=#{CREW_LIB_PREFIX}/pkgconfig \
-          --enable-widec \
-          --without-tests \
-          --with-termlib"
+      system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' \
+        LDFLAGS='-flto=auto' ../configure \
+        #{CREW_OPTIONS} \
+        --program-prefix='' \
+        --program-suffix='' \
+        --with-shared \
+        --with-cxx-shared \
+        --without-debug \
+        --enable-pc-files \
+        --with-pkg-config-libdir=#{CREW_LIB_PREFIX}/pkgconfig \
+        --enable-widec \
+        --without-tests \
+        --with-termlib"
+      system 'make'
+    end
+
+    # build libncurses
+    FileUtils.mkdir 'ncurses_build'
+    Dir.chdir 'ncurses_build' do
+      system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' \
+        LDFLAGS='-flto=auto' ../configure \
+         #{CREW_OPTIONS} \
+         --program-prefix='' \
+         --program-suffix='' \
+         --with-shared \
+         --with-cxx-shared \
+         --without-debug \
+         --enable-pc-files \
+         --with-pkg-config-libdir=#{CREW_LIB_PREFIX}/pkgconfig \
+         --disable-db-install \
+         --without-manpages \
+         --without-progs \
+         --without-tack \
+         --without-tests \
+         --with-termlib"
       system 'make'
     end
   end
@@ -47,13 +69,8 @@ class Ncurses < Package
     Dir.chdir 'ncursesw_build' do
       system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     end
-    # Make symlinks for non-wide software
-    Dir.chdir CREW_DEST_LIB_PREFIX.to_s do
-      Dir.glob('*w.so*').each do |f|
-        @basefile_nowide = f.gsub('w.so', '.so')
-        puts "Symlinking #{f} to #{@basefile_nowide}"
-        FileUtils.ln_sf f, @basefile_nowide
-      end
+    Dir.chdir 'ncurses_build' do
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     end
   end
 end
