3f3b089da40302378b3a3d52a0259754a6e33bf6
supechicken <supechicken666@gmail.com>
2021-04-18 12:24:36 +0800

    Update thunar.rb

--- packages/thunar.rb
+++ packages/thunar.rb
@@ -3,10 +3,11 @@ require 'package'
 class Thunar < Package
   description 'Thunar File Manager'
   homepage 'https://docs.xfce.org/xfce/thunar/Start'
-  version '4.16.1'
+  @_ver = '4.17.1'
+  version @_ver
   license 'GPL-2+ and LGPL-2+'
   compatibility 'all'
-  source_url "https://archive.xfce.org/src/xfce/thunar/4.16/thunar-#{version}.tar.bz2"
+  source_url "https://archive.xfce.org/src/xfce/thunar/#{@_ver.rpartition('.')[0]}/thunar-#{@_ver}.tar.bz2"
   source_sha256 'da2d17d2cb69eb33768690b714cc232ed367cbd71eb9543aaa2a805d05dc3ce1'
 
   binary_url ({
@@ -30,23 +31,23 @@ class Thunar < Package
   depends_on 'desktop_file_utilities'
   depends_on 'hicolor_icon_theme'
   depends_on 'xdg_base'
-  depends_on 'wayland_protocols'
-  depends_on 'mesa'
+  depends_on 'wayland'
 
   def self.build
-    system "./configure #{CREW_OPTIONS} --enable-gio-unix --enable-gudev --enable-exif --enable-pcre --disable-static --enable-notifications"
-    system "make -j#{CREW_NPROC}"
+    system <<~CONFIGURE
+      #{CREW_ENV_OPTIONS} \
+      ./configure #{CREW_OPTIONS} \
+      --enable-gio-unix \
+      --enable-gudev \
+      --enable-exif \
+      --enable-pcre \
+      --disable-static \
+      --enable-notifications
+    CONFIGURE
+    system 'make'
   end
 
   def self.install
-    system "make install DESTDIR=#{CREW_DEST_DIR}"
-    system "cat <<'EOF'> Thunar
-WAYLAND_DISPLAY=wayland-0
-GDK_BACKEND=wayland
-DISPLAY=
-#{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar $@
-EOF"
-    system "install -Dm755 Thunar #{CREW_DEST_PREFIX}/bin/Thunar"
-    # FileUtils.ln_sf "#{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar", "#{CREW_DEST_PREFIX}/bin/Thunar"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
