6b197388affb01a7a6f86fc57e953e0b4c292c04
supechicken <supechicken666@gmail.com>
2021-04-18 22:58:01 +0800

    Add files via upload (#5659)

--- packages/vte.rb
+++ packages/vte.rb
@@ -3,24 +3,12 @@ require 'package'
 class Vte < Package
   description 'Virtual Terminal Emulator widget for use with GTK'
   homepage 'https://wiki.gnome.org/Apps/Terminal/VTE'
-  version '0.63.91'
+  @_ver = '0.64.0'
+  version @_ver
   license 'LGPL-2+ and GPL-3+'
   compatibility 'all'
-  source_url 'https://download.gnome.org/sources/vte/0.63/vte-0.63.91.tar.xz'
-  source_sha256 '2a6f58470148d2a16bac387da12525d061e5984b68fc1ff8d068d10d4f1716ab'
-
-  binary_url({
-    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/vte-0.63.91-chromeos-armv7l.tar.xz',
-     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/vte-0.63.91-chromeos-armv7l.tar.xz',
-       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/vte-0.63.91-chromeos-i686.tar.xz',
-     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/vte-0.63.91-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    aarch64: '4028b67195f646d4a0e9fef0c3655fe6196339a78c9a9a8ec9e01855d76f9f8d',
-     armv7l: '4028b67195f646d4a0e9fef0c3655fe6196339a78c9a9a8ec9e01855d76f9f8d',
-       i686: '5a89dfee8af6d95ec42c14d308c760bc0f65bbe3653e1ce0a29a13455a681d6b',
-     x86_64: '0d51c8ac7090fb7b427bd963d71ad2b6f47cb459cae447548760973c60dbf7a6'
-  })
+  source_url "https://download.gnome.org/sources/vte/#{@_ver.rpartition('.')[0]}/vte-#{@_ver}.tar.xz"
+  source_sha256 'c0c60b8dc343167437c86d984b0cf134df86034180ed70513f683006ada3ec41'
 
   depends_on 'gobject_introspection' => :build
   depends_on 'fribidi'
@@ -38,19 +26,18 @@ class Vte < Package
   end
 
   def self.build
-    system "meson \
-      #{CREW_MESON_FNO_LTO_OPTIONS} \
-      -Dc_args='-fuse-ld=lld -pipe -fno-stack-protector' \
-      -Dc_link_args='-fuse-ld=lld -pipe -fno-stack-protector' \
-      -Dcpp_args='-fuse-ld=lld -pipe -fno-stack-protector' \
-      -Dcpp_link_args='-fuse-ld=lld -pipe -fno-stack-protector' \
+    system <<~CONFIGURE
+      meson \
+      #{CREW_MESON_FNO_LTO_OPTIONS.gsub('-fno-lto', '-fno-lto -fno-stack-protector')} \
       -D_systemd=false \
       -Dfribidi=true \
       -Dgtk3=true \
       -Dgtk4=true \
       -Dgir=false \
       -Dvapi=false \
-      builddir"
+      builddir
+    CONFIGURE
+
     system 'meson configure builddir'
     system 'ninja -C builddir'
   end
