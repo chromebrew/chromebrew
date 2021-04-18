91d137d619186d0c85aa8ed20469f2899e149e86
supechicken <supechicken666@gmail.com>
2021-04-18 13:55:38 +0800

    Update clutter_gtk.rb

--- packages/clutter_gtk.rb
+++ packages/clutter_gtk.rb
@@ -17,10 +17,10 @@ class Clutter_gtk < Package
   def self.build
     system "meson #{CREW_MESON_FNO_LTO_OPTIONS} builddir"
     system 'meson', 'configure', 'builddir'
-    system 'ninja', '-C builddir'
+    system 'ninja', '-C', 'builddir'
   end
 
   def self.install
-    system 'ninja', "DESTDIR=#{CREW_DEST_DIR}", '-C builddir', 'install'
+    system 'ninja', "DESTDIR=#{CREW_DEST_DIR}", '-C', 'builddir', 'install'
   end
 end
