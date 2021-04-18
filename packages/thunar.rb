9a55f4ba7e4da6172f7da7281b8ba2ca04545692
supechicken666 <68530202+supechicken666@users.noreply.github.com>
2021-04-18 12:13:57 +0800

    Update thunar.rb

--- packages/thunar.rb
+++ packages/thunar.rb
@@ -3,18 +3,17 @@ require 'package'
 class Thunar < Package
   description 'Thunar File Manager'
   homepage 'https://docs.xfce.org/xfce/thunar/Start'
-  @_ver = '4.16.1'
-  version @_ver + '-1'
+  version '4.16.1'
   license 'GPL-2+ and LGPL-2+'
   compatibility 'all'
-  source_url "https://archive.xfce.org/src/xfce/thunar/4.16/thunar-#{@_ver}.tar.bz2"
+  source_url "https://archive.xfce.org/src/xfce/thunar/4.16/thunar-#{version}.tar.bz2"
   source_sha256 'da2d17d2cb69eb33768690b714cc232ed367cbd71eb9543aaa2a805d05dc3ce1'
 
   binary_url ({
-    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/thunar-4.16.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/thunar-4.16.1-chromeos-armv7l.tar.xz',
-       i686: 'https://dl.bintray.com/chromebrew/chromebrew/thunar-4.16.1-chromeos-i686.tar.xz',
-     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/thunar-4.16.1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/thunar-4.16.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/thunar-4.16.1-chromeos-armv7l.tar.xz',
+       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/thunar-4.16.1-chromeos-i686.tar.xz',
+     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/thunar-4.16.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
     aarch64: 'f72859ef9d34c9f53b027e3ff4f939d53dfd02e2e14fa1ccba7272857dff6846',
@@ -31,29 +30,23 @@ class Thunar < Package
   depends_on 'desktop_file_utilities'
   depends_on 'hicolor_icon_theme'
   depends_on 'xdg_base'
-  depends_on 'gui_setup'
+  depends_on 'wayland_protocols'
+  depends_on 'mesa'
 
   def self.build
-    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' LDFLAGS='-flto=auto' \
-            ./configure #{CREW_OPTIONS} \
-              --enable-gio-unix \
-              --enable-gudev \
-              --enable-exif \
-              --enable-pcre \
-              --disable-static \
-              --enable-notifications"
-    system 'make'
+    system "./configure #{CREW_OPTIONS} --enable-gio-unix --enable-gudev --enable-exif --enable-pcre --disable-static --enable-notifications"
+    system "make -j#{CREW_NPROC}"
   end
 
   def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    @file <<~EOF
-      WAYLAND_DISPLAY=${WAYLAND_DISPLAY:-wayland-0}
-      GDK_BACKEND=${GDK_BACKEND:-wayland}
-      exec #{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar "$@"
-    EOF
-    File.write("#{CREW_DEST_PREFIX}/bin/Thunar", @file)
-    File.chmod(0755, "#{CREW_DEST_PREFIX}/bin/Thunar")
+    system "make install DESTDIR=#{CREW_DEST_DIR}"
+    system "cat <<'EOF'> Thunar
+WAYLAND_DISPLAY=wayland-0
+GDK_BACKEND=wayland
+DISPLAY=
+#{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar $@
+EOF"
+    system "install -Dm755 Thunar #{CREW_DEST_PREFIX}/bin/Thunar"
     # FileUtils.ln_sf "#{CREW_PREFIX}/bin/#{CREW_BUILD}-thunar", "#{CREW_DEST_PREFIX}/bin/Thunar"
   end
 end
