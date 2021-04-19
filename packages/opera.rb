137a3a099fecd02abdaec56f1449c400aced9edf
Ed Reel <edreel@gmail.com>
2021-04-18 20:34:19 -0500

    Update postinstall and remove sections

--- packages/opera.rb
+++ packages/opera.rb
@@ -20,10 +20,11 @@ class Opera < Package
     # llvm-strip doesn't works with opera
     ENV['CREW_NOT_STRIP'] = '1'
 
-    # since opera put the executable to library, we need to link it to CREW_PREFIX
-    FileUtils.ln_sf "#{CREW_LIB_PREFIX}/opera/opera", 'bin/opera'
+    # Since opera puts the executable in a location that is not in the path,
+    # we need to link it to bin directory.
+    FileUtils.ln_sf "#{CREW_PREFIX}/share/x86_64-linux-gnu/opera/opera", 'bin/opera'
 
-    # place all stuff to lib64 instead of lib
+    # Move lib subfolder to the share directory.
     FileUtils.mv 'lib/x86_64-linux-gnu/', 'share/'
     FileUtils.rm_rf 'lib/'
 
@@ -33,21 +34,25 @@ class Opera < Package
 
   def self.postinstall
     puts
-    puts 'Set Opera as your default browser? [Y/n]: '
-    case STDIN.gets.chomp
-    when "\n", 'Y', 'y', 'yes'
+    print 'Set Opera as your default browser? [Y/n]: '
+    case STDIN.getc
+    when "\n", 'Y', 'y'
       Dir.chdir("#{CREW_PREFIX}/bin") do
         FileUtils.ln_sf "#{CREW_LIB_PREFIX}/opera/opera", 'x-www-browser'
       end
+      puts 'Opera is now your default browser.'.lightgreen
     else
-      puts 'No change has been made.'
-      puts
+      puts 'No change has been made.'.orange
     end
+    puts "\nType 'opera' to get started.\n".lightblue
   end
 
   def self.remove
     Dir.chdir("#{CREW_PREFIX}/bin") do
-      FileUtils.rm 'x-www-browser' if File.symlink?('x-www-browser') && File.realpath('x-www-browser') == "#{CREW_LIB_PREFIX}/opera/opera"
+      if File.exist?('x-www-browser') and File.symlink?('x-www-browser') and \
+        File.realpath('x-www-browser') == "#{CREW_PREFIX}/share/x86_64-linux-gnu/opera/opera"
+        FileUtils.rm 'x-www-browser'
+      end
     end
   end
 end
