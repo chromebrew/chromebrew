44628fdd428a94cc6722412253107c114057a9e8
satmandu <satadru@umich.edu>
2021-04-18 20:14:40 -0400

    wayland binaries (#5663)

--- packages/wayland.rb
+++ packages/wayland.rb
@@ -4,16 +4,27 @@ class Wayland < Package
   description 'Wayland is intended as a simpler replacement for X, easier to develop and maintain.'
   homepage 'https://wayland.freedesktop.org'
   @_ver = '1.19.0'
-  version @_ver + '-1'
+  version "#{@_ver}-1"
   license 'MIT'
   compatibility 'all'
   source_url "https://wayland.freedesktop.org/releases/wayland-#{@_ver}.tar.xz"
   source_sha256 'baccd902300d354581cd5ad3cc49daa4921d55fb416a5883e218750fef166d15'
 
+  binary_url({
+    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/wayland-1.19.0-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/wayland-1.19.0-1-chromeos-armv7l.tar.xz',
+       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/wayland-1.19.0-1-chromeos-i686.tar.xz',
+     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/wayland-1.19.0-1-chromeos-x86_64.tar.xz'
+  })
+  binary_sha256({
+    aarch64: 'afde6d47410b47f681cf945f3fee881715d5d67c78e055353011413498fda590',
+     armv7l: 'afde6d47410b47f681cf945f3fee881715d5d67c78e055353011413498fda590',
+       i686: '460606fc3edcac28c61a684ef1a22fad67ff569d3fdb1f9d7414e4a8dbcbc923',
+     x86_64: 'd57e02b79a9e6d875b0496597bab4d219fc257df08bafb0be7dee39cbbd7c597'
+  })
+
   depends_on 'expat'
   depends_on 'libpng'
-  depends_on 'libffi'
-  depends_on 'libxml2'
   depends_on 'libxslt'
 
   def self.build
@@ -28,12 +39,12 @@ class Wayland < Package
       GDK_BACKEND=wayland
       set +a
     EOF
-    
+
     system "meson #{CREW_MESON_FNO_LTO_OPTIONS} \
     -Ddocumentation=false \
     builddir"
-    system "meson configure builddir"
-    system "ninja -C builddir"
+    system 'meson configure builddir'
+    system 'ninja -C builddir'
   end
 
   def self.install
