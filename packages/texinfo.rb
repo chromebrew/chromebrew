5a874b3c4f09176b4e2a948fb2ae7dd850a1fda4
Satadru Pramanik <satadru@gmail.com>
2021-04-19 23:41:14 -0400

    texinfo rebuild

--- packages/texinfo.rb
+++ packages/texinfo.rb
@@ -3,39 +3,35 @@ require 'package'
 class Texinfo < Package
   description 'Texinfo is the official documentation format of the GNU project.'
   homepage 'https://www.gnu.org/software/texinfo/'
-  version '6.7'
+  version '6.7-1'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/texinfo/texinfo-6.7.tar.xz'
   source_sha256 '988403c1542d15ad044600b909997ba3079b10e03224c61188117f3676b02caa'
 
-  binary_url ({
-    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/texinfo-6.7-chromeos-armv7l.tar.xz',
-     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/texinfo-6.7-chromeos-armv7l.tar.xz',
-       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/texinfo-6.7-chromeos-i686.tar.xz',
-     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/texinfo-6.7-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/texinfo-6.7-1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/texinfo-6.7-1-chromeos-armv7l.tar.xz',
+       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/texinfo-6.7-1-chromeos-i686.tar.xz',
+     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/texinfo-6.7-1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: 'c01bcef457daa115be3bb4a7dbbf36263b491f500217e226b5ffbcb8777d706d',
-     armv7l: 'c01bcef457daa115be3bb4a7dbbf36263b491f500217e226b5ffbcb8777d706d',
-       i686: '14d8e01b60692cba6eae6de7c7fd6bb2cf9187b22bc51f3d45d5c6340cb47619',
-     x86_64: '1f77c47871a7096cb013e696ebb0a7ce5cb9f6b5d68dd48862b5e6b605ecf4bb',
+  binary_sha256({
+    aarch64: '005b37aa4e36dea62555ce6ca36426f37ebc66d9f8ecba23cb8c960b3fc7c092',
+     armv7l: '005b37aa4e36dea62555ce6ca36426f37ebc66d9f8ecba23cb8c960b3fc7c092',
+       i686: '24bf126b3bba3a6dff27bdd869e0c6e3263eb9b04c39003ecc7bc8730230e816',
+     x86_64: '4e7a16cb4f7d93a7c949c7ff10b03fa2e27b9db4372e5c138b0f10561fafa292'
   })
 
-  depends_on 'gettext' => :build
-  depends_on 'perl'
   depends_on 'perl_locale_messages'
   depends_on 'perl_text_unidecode'
   depends_on 'perl_unicode_eastasianwidth'
-  depends_on 'ncurses'
 
   def self.build
     # configure and make
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--with-external-Text-Unidecode',
-           '--with-external-Unicode-EastAsianWidth'
+    system "env #{CREW_ENV_OPTIONS} \
+      ./configure #{CREW_OPTIONS} \
+      --with-external-Text-Unidecode \
+      --with-external-Unicode-EastAsianWidth"
     system 'make'
   end
 
