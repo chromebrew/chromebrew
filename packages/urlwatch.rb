c07cd139ee8f48cf01d99051a4e34e3ddf1cae11
Ed Reel <edreel@gmail.com>
2021-04-22 06:58:38 -0500

    Update github binary_urls for packages a-x (#5679)

--- packages/urlwatch.rb
+++ packages/urlwatch.rb
@@ -10,16 +10,16 @@ class Urlwatch < Package
   source_sha256 'b61997ec6229b2cb22b7121d0b666da91e524e212d126f55cd939d230daa5887'
 
   binary_url({
-    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/urlwatch-2.23-chromeos-armv7l.tar.xz',
-     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/urlwatch-2.23-chromeos-armv7l.tar.xz',
-       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/urlwatch-2.23-chromeos-i686.tar.xz',
-     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/urlwatch-2.23-chromeos-x86_64.tar.xz'
+    aarch64: 'https://github.com/chromebrew/binaries/raw/main/armv7l/urlwatch-2.23-chromeos-armv7l.tar.xz',
+     armv7l: 'https://github.com/chromebrew/binaries/raw/main/armv7l/urlwatch-2.23-chromeos-armv7l.tar.xz',
+       i686: 'https://github.com/chromebrew/binaries/raw/main/i686/urlwatch-2.23-chromeos-i686.tar.xz',
+     x86_64: 'https://github.com/chromebrew/binaries/raw/main/x86_64/urlwatch-2.23-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '8b23e4d28741b7d0287be81957a599fad546b048fd66b8a4b7e71a53a27c678e',
-     armv7l: '8b23e4d28741b7d0287be81957a599fad546b048fd66b8a4b7e71a53a27c678e',
-       i686: '5f4be490ff9b44052563617a9085ab4e9c861cbe33f059626b6e00a5d321b549',
-     x86_64: '48f21d5770116a5fb650b6d3c7c860172653ddfbb0209485ee250422f69e259e'
+    aarch64: '1f17b72ee478889438aa358a966b649523793f1c5c97fd19b4eaddaa5f75f7ed',
+     armv7l: '1f17b72ee478889438aa358a966b649523793f1c5c97fd19b4eaddaa5f75f7ed',
+       i686: '66a0c448752ed7a144e6e73967fb600d5f2784aab59b49279650a156d4cb8b92',
+     x86_64: '9d2629ab5d1172e48eb97bd074c5156a6fafe75d1f11a698a85a6cc84a87934c'
   })
 
   def self.install
