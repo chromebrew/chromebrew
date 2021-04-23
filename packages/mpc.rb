eaf52d74760c70cf56ef6d6a3d240e90cc5418ec
satmandu <satadru@umich.edu>
2021-04-23 12:54:52 -0400

    Update gcc prereqs: gmp, mpc, mpfr (#5682)
    * Update gcc prereqs: gmp, mpc, mpfr
    
    * Use specific commit for patches
    
    * Update mpfr.rb

--- packages/mpc.rb
+++ packages/mpc.rb
@@ -3,34 +3,36 @@ require 'package'
 class Mpc < Package
   description 'Gnu Mpc is a C library for the arithmetic of complex numbers with arbitrarily high precision and correct rounding of the result.'
   homepage 'http://www.multiprecision.org/'
-  version '1.1.0'
+  version '1.2.1'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/mpc/mpc-1.1.0.tar.gz'
-  source_sha256 '6985c538143c1208dcb1ac42cedad6ff52e267b47e5f970183a3e75125b43c2e'
+  source_url 'https://ftpmirror.gnu.org/mpc/mpc-1.2.1.tar.gz'
+  source_sha256 '17503d2c395dfcf106b622dc142683c1199431d095367c6aacba6eec30340459'
 
-  binary_url ({
-    aarch64: 'https://github.com/chromebrew/binaries/raw/main/armv7l/mpc-1.1.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://github.com/chromebrew/binaries/raw/main/armv7l/mpc-1.1.0-chromeos-armv7l.tar.xz',
-       i686: 'https://github.com/chromebrew/binaries/raw/main/i686/mpc-1.1.0-chromeos-i686.tar.xz',
-     x86_64: 'https://github.com/chromebrew/binaries/raw/main/x86_64/mpc-1.1.0-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://github.com/chromebrew/binaries/raw/main/armv7l/mpc-1.2.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://github.com/chromebrew/binaries/raw/main/armv7l/mpc-1.2.1-chromeos-armv7l.tar.xz',
+       i686: 'https://github.com/chromebrew/binaries/raw/main/i686/mpc-1.2.1-chromeos-i686.tar.xz',
+     x86_64: 'https://github.com/chromebrew/binaries/raw/main/x86_64/mpc-1.2.1-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '93e218f15597b7fcbd6291986597e4c391e521d4f71ac19d815267cffcdeaf07',
-     armv7l: '93e218f15597b7fcbd6291986597e4c391e521d4f71ac19d815267cffcdeaf07',
-       i686: 'c952ab3fc51d56c6b0140534627888769f3a1bde43a645b279b9ef1010c7080f',
-     x86_64: '1fe81b949e466649174009019f96fbc09ea5161dfc99f0d5e462da196b5b64a1',
+  binary_sha256({
+    aarch64: '51f7b0de3567634f42cb4e563520243cd6c9eca00b8efb1add25fbae97b286ce',
+     armv7l: '51f7b0de3567634f42cb4e563520243cd6c9eca00b8efb1add25fbae97b286ce',
+       i686: '63b012cdf4a7c8e1e948ceff325ea6dd476e2a4330462d1ce4b2778b51529ca9',
+     x86_64: '57276bec912dabfed4d65d5edb3e18788c168056727057ee0c91aa8484eebdba'
   })
+
+  depends_on 'mpfr' => :build
 
   def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-maintainer-mode'
+    system "./configure \
+      #{CREW_OPTIONS} \
+      --disable-maintainer-mode \
+      --enable-shared"
     system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
