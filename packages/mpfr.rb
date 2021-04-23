426765c45edf8ffe861697c377e8e215258cbfa6
Satadru Pramanik <satadru@gmail.com>
2021-04-23 12:19:23 -0400

    Update gcc prereqs: gmp, mpc, mpfr

--- packages/mpfr.rb
+++ packages/mpfr.rb
@@ -3,41 +3,50 @@ require 'package'
 class Mpfr < Package
   description 'The MPFR library is a C library for multiple-precision floating-point computations with correct rounding.'
   homepage 'http://www.mpfr.org/'
-  version '4.0.1'
+  version '4.1.0'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'http://www.mpfr.org/mpfr-current/mpfr-4.0.1.tar.xz'
-  source_sha256 '67874a60826303ee2fb6affc6dc0ddd3e749e9bfcb4c8655e3953d0458a6e16e'
+  source_url 'http://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.xz'
+  source_sha256 '0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f'
 
-  binary_url ({
-    aarch64: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/mpfr-4.0.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://downloads.sourceforge.net/project/chromebrew/armv7l/mpfr-4.0.1-chromeos-armv7l.tar.xz',
-       i686: 'https://downloads.sourceforge.net/project/chromebrew/i686/mpfr-4.0.1-chromeos-i686.tar.xz',
-     x86_64: 'https://downloads.sourceforge.net/project/chromebrew/x86_64/mpfr-4.0.1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://github.com/chromebrew/binaries/raw/main/armv7l/mpfr-4.1.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://github.com/chromebrew/binaries/raw/main/armv7l/mpfr-4.1.0-chromeos-armv7l.tar.xz',
+       i686: 'https://github.com/chromebrew/binaries/raw/main/i686/mpfr-4.1.0-chromeos-i686.tar.xz',
+     x86_64: 'https://github.com/chromebrew/binaries/raw/main/x86_64/mpfr-4.1.0-chromeos-x86_64.tar.xz'
   })
-  binary_sha256 ({
-    aarch64: '95fb95f4a32a0c91e48c3dee207710614eb44d1e98f3e43d7d439c5be7c1e5eb',
-     armv7l: '95fb95f4a32a0c91e48c3dee207710614eb44d1e98f3e43d7d439c5be7c1e5eb',
-       i686: '1f2598510252794e1eb9c98a132ddc7ad394c2c369cdfa4a690f50a2c981026f',
-     x86_64: 'e5d43065f3f87eabf33debd0c9789988f980d68a0f1a91d6782b3f97916c0680',
+  binary_sha256({
+    aarch64: '02fc4dce2dfade4e7cb7ca539ddcf8142c1cb8839c9a1f83e5362e8f00f7a1e8',
+     armv7l: '02fc4dce2dfade4e7cb7ca539ddcf8142c1cb8839c9a1f83e5362e8f00f7a1e8',
+       i686: '2950b94cbcbcb11b0bb34a6e31d7e6e9982eb74d37edd73a17a3700c2b6abe41',
+     x86_64: 'a7e0a584a6d54b852d4869ef3ba8f9fdb076f48d6d3f2cb1f46842bc8975583a'
   })
+
+  depends_on 'autoconf_archive' => :build
+
+  def self.patch
+    puts 'Applying current rolling patchset. See https://www.mpfr.org/mpfr-current/#bugs'
+    puts 'for more information.'
+    system 'curl -Ls https://www.mpfr.org/mpfr-current/allpatches | patch -NZp1 --binary'
+  end
 
   def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-static',
-           '--enable-shared'
+    system 'filefix'
+    system 'autoreconf -fiv'
+    system "./configure \
+      #{CREW_OPTIONS} \
+      --enable-shared"
     system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     system "mkdir -p #{CREW_DEST_LIB_PREFIX}"
     system "ln -s #{CREW_LIB_PREFIX}/libmpfr.so.6 #{CREW_DEST_LIB_PREFIX}/libmpfr.so.4"
   end
 
   def self.check
-    system "make", "check"
+    # Check only works after an install on x86_64
+    system 'PWD=$(pwd) ; LD_LIBRARY_PATH=$pwd/src/.libs:$LD_LIBRARY_PATH make check'
   end
 end
