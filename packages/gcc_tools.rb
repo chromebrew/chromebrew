b11eaa4ff05b6d16013b0f59d748eadaf28a8a92
Satadru Pramanik <satadru@gmail.com>
2021-04-28 10:45:48 -0400

    Update gcc_tools with new libssp & gcc11

--- packages/gcc_tools.rb
+++ packages/gcc_tools.rb
@@ -3,7 +3,7 @@ require 'package'
 class Gcc_tools < Package
   description 'Tools for working with gcc packages'
   homepage 'https://github.com/skycocker/chromebrew'
-  version '1.1'
+  version '1.2'
   license 'GPL-3+'
   compatibility 'all'
   source_url 'file:///dev/null'
@@ -41,19 +41,19 @@ function download_check () {
 }
 
 if [ ! -f "${CREW_PREFIX}/lib${LIB_SUFFIX}/libssp.so.0" ]; then
-  # prepare gcc8 url and sha256
+  # prepare gcc11.1 derived libssp url and sha256
   case "${ARCH}" in
   "aarch64"|"armv7l")
-    url="https://dl.bintray.com/chromebrew/chromebrew/gcc8-8.3.0-chromeos-armv7l.tar.xz"
-    sha256="fbd8a589befb3d10400af6e4975d02a6940bab4907628f8fc0d6913ea89f70ae"
+    url="https://gitlab.com/api/v4/projects/26210301/packages/generic/libssp/11.1.0_armv7l/libssp-11.1.0-chromeos-armv7l.tar.xz"
+    sha256="21d6872debb576b442a0a62ab7d0ae4bc765db83bf43cdb8e11c28f56e51d456"
     ;;
   "i686")
-    url="https://dl.bintray.com/chromebrew/chromebrew/gcc8-8.3.0-chromeos-i686.tar.xz"
-    sha256="4d625e92969fde16c1a92601740e22c07331f8056452edf7cf0bf2c1c88aa152"
+    url="https://gitlab.com/api/v4/projects/26210301/packages/generic/libssp/11.1.0_i686/libssp-11.1.0-chromeos-i686.tar.xz"
+    sha256="5b02bf769ec3754f4b0c937e5420f13bf285bc81bdc9c68fbc6f63c56562682b"
     ;;
   "x86_64")
-    url="https://dl.bintray.com/chromebrew/chromebrew/gcc8-8.3.0-chromeos-x86_64.tar.xz"
-    sha256="ae8c8c33e4090f7fdbd39b2364754dcfc5f6bdd9a74062fde3eeb6272562f48b"
+    url="https://gitlab.com/api/v4/projects/26210301/packages/generic/libssp/11.1.0_x86_64/libssp-11.1.0-chromeos-x86_64.tar.xz"
+    sha256="356cd568080dd0e724081ec7f020d0b21e091d5552d175746f830c48830bd8b9"
     ;;
   esac
   tarfile="$(basename ${url})"
@@ -67,16 +67,16 @@ if [ ! -f "${CREW_PREFIX}/bin/ruby" ]; then
   # prepare ruby url and sha256
   case "${ARCH}" in
   "aarch64"|"armv7l")
-    url="https://dl.bintray.com/chromebrew/chromebrew/ruby-2.7.2-chromeos-armv7l.tar.xz"
-    sha256="a435e6bf7965e1a82e8842e5ea66bdd670ec9b627d785bd720d3d2652fc89f6d"
+    url="https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.0.1-1_armv7l/ruby-3.0.1-1-chromeos-armv7l.tar.xz"
+    sha256="03f443d63cca0fb640ec4d11f3b1306a9719e775f8c706c732f9231e41e66afd"
     ;;
   "i686")
-    url="https://dl.bintray.com/chromebrew/chromebrew/ruby-2.7.2-chromeos-i686.tar.xz"
-    sha256="81865864d3ba93b6cbd5dc8e1b6cb51bd2ebe854f6c01e282c1b73f379fb7caf"
+    url="https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.0.1-1_i686/ruby-3.0.1-1-chromeos-i686.tar.xz"
+    sha256="dc388589df39c39e9c494e761ae2ca950a9b4f0573aa0d96508959787c050dcd"
     ;;
   "x86_64")
-    url="https://dl.bintray.com/chromebrew/chromebrew/ruby-2.7.2-chromeos-x86_64.tar.xz"
-    sha256="658808516b7a2e58f8102fd131e765aaa79f2a7c906d0330b7e883fbdc12d1a9"
+    url="https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.0.1-1_x86_64/ruby-3.0.1-1-chromeos-x86_64.tar.xz"
+    sha256="4f86c98eaae1e708bf66f1c939a5cd07d5764b9b64ca827e6f7d5f61021bcb7e"
     ;;
   esac
   tarfile="$(basename ${url})"
@@ -108,7 +108,7 @@ else
       echo "Enter the GCC version to install:"
       echo " 7 = GCC 7.4.0"
       echo " 8 = GCC 8.3.0"
-      echo "10 = GCC 10.2.0"
+      echo "11 = GCC 11.1.0"
       echo " 0 = Cancel"
       read version
     done
@@ -119,11 +119,11 @@ else
       case $v in
         7)
           echo " 8 = GCC 8.3.0"
-          echo "10 = GCC 10.2.0"
+          echo "11 = GCC 11.1.0"
           ;;
         8)
           echo " 7 = GCC 7.4.0"
-          echo "10 = GCC 10.2.0"
+          echo "11 = GCC 11.1.0"
           ;;
         10)
           echo " 7 = GCC 7.4.0"
