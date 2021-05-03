a796f84c12fe9bcf70990b8a68ef416d2331c580
satmandu <satadru@umich.edu>
2021-05-02 22:22:00 -0400

    allow verbose crew list installed (#5720)
    * allow verbose crew list installed
    
    * uprev crew
    
    * Add verbose option to crew help for crew list

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.9.1'
+CREW_VERSION = '1.9.2'
 
 ARCH_ACTUAL = `uname -m`.strip
 # This helps with virtualized builds on aarch64 machines
