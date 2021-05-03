2bbc43e054f4e3dc554b2ca9292c793db99c6cac
Satadru Pramanik <satadru@gmail.com>
2021-05-03 16:28:36 -0400

    work crew modifications

--- packages/perl_xml_parser.rb
+++ packages/perl_xml_parser.rb
@@ -29,7 +29,7 @@ class Perl_xml_parser < Package
     system "(cd build/man; tar cf - .) | (cd #{CREW_DEST_MAN_PREFIX}; tar xfp -)"
 
     # conflicts with other perl module installs
-    system "find #{CREW_DEST_DIR} -name .packlist -o -name perllocal.pod -delete"
+    system "find #{CREW_DEST_DIR} -type f -name .packlist -o -name perllocal.pod -delete"
   end
 
   def self.check
