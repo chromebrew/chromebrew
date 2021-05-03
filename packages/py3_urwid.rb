08699437e532951ab384ab1ffc4e37765ba0dab0
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-05-03 12:10:38 -0400

    choose

---
+++ packages/py3_urwid.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Py3_urwid < Package
+  description 'Urwid is a full-featured console user interface library.'
+  homepage 'http://urwid.org/'
+  @_ver = '2.1.2'
+  version @_ver
+  license 'LGPL-2.1'
+  compatibility 'all'
+  source_url 'https://github.com/urwid/urwid.git'
+  git_hashtag 'release-' + @_ver
+
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+  end
+
+  def self.install
+    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+  end
+end
