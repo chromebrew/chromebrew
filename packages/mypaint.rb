35d71c0c8a183e8a28f4542fb153137360806ccd
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-05-04 13:06:25 -0400

    mypaint

--- packages/mypaint.rb
+++ packages/mypaint.rb
@@ -3,24 +3,11 @@ require 'package'
 class Mypaint < Package
   description 'MyPaint is a simple drawing and painting program'
   homepage 'http://mypaint.org/'
-  version '1.2.1'
+  version '1.2.1-1'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/mypaint/mypaint/releases/download/v1.2.1/mypaint-1.2.1.tar.xz'
   source_sha256 'ed2349382f6a0c583d7defae06427e97ac7d14acb77b9e2c0fc7171ddd96d3c7'
-
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mypaint/1.2.1_armv7l/mypaint-1.2.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mypaint/1.2.1_armv7l/mypaint-1.2.1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mypaint/1.2.1_i686/mypaint-1.2.1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mypaint/1.2.1_x86_64/mypaint-1.2.1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '34fa83b9be46e7bb03e914abe51abe7e17da0d7bf6967c8ff2c77413f8edcab2',
-     armv7l: '34fa83b9be46e7bb03e914abe51abe7e17da0d7bf6967c8ff2c77413f8edcab2',
-       i686: '03fb94655a4fe7876ec7f9c3b86adc7e95cd43cc4263a505b5541649f4407d66',
-     x86_64: 'a32e94eb92824d65544c35d09ef5d3e9d69b5ce504a820a46bd2ea48471cd125',
-  })
 
   depends_on 'scons' => :build
   depends_on 'mypaint_brushes'
@@ -28,9 +15,9 @@ class Mypaint < Package
   depends_on 'librsvg'
   depends_on 'xdg_base'
   depends_on 'sommelier'
+  depends_on 'py3_numpy'
 
   def self.build
-    system 'pip install numpy'
     system "echo '#!/bin/bash' > mypaint.sh"
     system "echo 'GDK_BACKEND=x11' >> mypaint.sh"
     system "echo 'gdk-pixbuf-query-loaders --update-cache' >> mypaint.sh"
@@ -40,8 +27,6 @@ class Mypaint < Package
 
   def self.install
     system "scons", "prefix=#{CREW_DEST_PREFIX}", "install"
-    system 'pip uninstall numpy -y'
-    system "pip install numpy --prefix #{CREW_PREFIX} --root #{CREW_DEST_DIR}"
     FileUtils.mkdir_p "#{CREW_DEST_HOME}/.config/mypaint"
     FileUtils.touch "#{CREW_DEST_HOME}/.config/mypaint/accelmap.conf"
     FileUtils.touch "#{CREW_DEST_HOME}/.config/mypaint/settings.json"
