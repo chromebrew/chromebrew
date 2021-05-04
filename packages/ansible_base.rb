dcb746d9427796ac33389e9e04c79576fce2a82c
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-05-04 17:13:23 -0400

    avocado-framework

--- packages/ansible_base.rb
+++ packages/ansible_base.rb
@@ -9,6 +9,7 @@ class Ansible_base < Package
   source_url 'https://files.pythonhosted.org/packages/2f/7c/36119f0695fe48fd5eeb33a78c11137642abf1f67b477f31b9772da5a85d/ansible-base-2.10.9.tar.gz'
   source_sha256 '04635d3e08fc29358c76b8e7f1e9db0ce443fb09ce30b2acc6cacaad165f2151'
 
+  depends_on 'xdg_base'
   depends_on 'py3_cryptography'
   depends_on 'py3_pyyaml'
   depends_on 'py3_packaging'
---
+++ packages/avocado_framework.rb
@@ -0,0 +1,24 @@
+require 'package'
+
+class Avocado_framework < Package
+  description 'Avocado is a next generation testing framework inspired by Autotest and modern development tools such as git.'
+  homepage 'https://avocado-framework.github.io/'
+  @_ver = '87.0'
+  version @_ver
+  license 'GPL-2 and GPL-2+'
+  compatibility 'all'
+  source_url 'https://github.com/avocado-framework/avocado.git'
+  git_hashtag @_ver
+
+  depends_on 'xdg_base'
+  depends_on 'xzutils'
+  depends_on 'py3_setuptools'
+
+  def self.build
+    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+  end
+
+  def self.install
+    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+  end
+end
