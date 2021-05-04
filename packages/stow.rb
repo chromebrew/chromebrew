c6e7ed311a0a3d0039291567c9cac653dd0518a4
satmandu <satadru@gmail.com>
2021-05-03 20:34:01 -0400

    compiled with perl 5.3.1

--- packages/stow.rb
+++ packages/stow.rb
@@ -9,10 +9,23 @@ class Stow < Package
   source_url 'https://ftpmirror.gnu.org/stow/stow-2.3.1.tar.gz'
   source_sha256 '09d5d99671b78537fd9b2c0b39a5e9761a7a0e979f6fdb7eabfa58ee45f03d4b'
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/stow/2.3.1_armv7l/stow-2.3.1-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/stow/2.3.1_armv7l/stow-2.3.1-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/stow/2.3.1_i686/stow-2.3.1-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/stow/2.3.1_x86_64/stow-2.3.1-chromeos-x86_64.tar.xz'
+  })
+  binary_sha256({
+    aarch64: '503b01aab3beaf36bd629ab7b8aee2d98e29b780bbc20becdb2e1d0cacdfe150',
+     armv7l: '503b01aab3beaf36bd629ab7b8aee2d98e29b780bbc20becdb2e1d0cacdfe150',
+       i686: '5a38f30a09b1815beda68dd24ae3ea298183eb40b23b6c455cc990314159d17f',
+     x86_64: 'bd129677fb5214d53531b7fde7afd8bc22160e04d55fd05b3c9c1dc8b0c21867'
+  })
+
   def self.prebuild
     system 'cpan App::cpanminus'
   end
-  
+
   def self.build
     system 'cpanm Test::Output'
     system "./configure #{CREW_OPTIONS} \
