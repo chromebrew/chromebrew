09d83d36b495ae516088212ba3d7db607a3cc3d4
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-05-08 15:12:21 -0400

    sphinx
    Signed-off-by: saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>

---
+++ packages/py3_alabaster.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Py3_alabaster < Package
+  description 'Alabaster is a configurable sidebar-enabled Sphinx theme.'
+  homepage 'https://alabaster.readthedocs.io/'
+  @_ver = '0.7.12'
+  version @_ver
+  license 'BSD' # License is BSD-style, might be BSD-3?
+  compatibility 'all'
+  source_url 'https://github.com/bitprophet/alabaster.git'
+  git_hashtag @_ver
+
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+  end
+
+  def self.install
+    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+  end
+end
