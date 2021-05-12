b8168992f4e1df747872279615caf966d1241946
Satadru Pramanik <satadru@gmail.com>
2021-05-12 19:55:08 -0400

    oniguruma, lua, jq, nmap updates

--- packages/nmap.rb
+++ packages/nmap.rb
@@ -1,35 +1,62 @@
+# Adapted from Arch Linux nmap PKGBUILD at:
+# https://github.com/archlinux/svntogit-packages/raw/packages/nmap/trunk/PKGBUILD
+
 require 'package'
 
 class Nmap < Package
-  description 'Nmap (\'Network Mapper\') is a free and open source (license) utility for network discovery and security auditing.'
+  description 'Utility for network discovery and security auditing'
   homepage 'https://nmap.org/'
-  version '7.80'
-  license 'NPSL'
+  version '7.91'
+  license 'GPL2'
   compatibility 'all'
-  source_url 'https://nmap.org/dist/nmap-7.80.tar.bz2'
-  source_sha256 'fcfa5a0e42099e12e4bf7a68ebe6fde05553383a682e816a7ec9256ab4773faa'
+  source_url 'https://nmap.org/dist/nmap-7.91.tar.bz2'
+  source_sha256 '18cc4b5070511c51eb243cdd2b0b30ff9b2c4dc4544c6312f75ce3a67a593300'
 
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_armv7l/nmap-7.80-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_armv7l/nmap-7.80-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_i686/nmap-7.80-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_x86_64/nmap-7.80-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.91_armv7l/nmap-7.91-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.91_armv7l/nmap-7.91-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.91_i686/nmap-7.91-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.91_x86_64/nmap-7.91-chromeos-x86_64.tpxz'
   })
-  binary_sha256 ({
-    aarch64: '05f6f7b8303f7249e0ce26650f13bd051d0d5665d321be0ff59f6ce51cff57fe',
-     armv7l: '05f6f7b8303f7249e0ce26650f13bd051d0d5665d321be0ff59f6ce51cff57fe',
-       i686: 'f40786b4937b7d3a6b6ddc67afb4a004ba577f865d9d922fa0982ebb871b1451',
-     x86_64: '624e3f64733244d3f987b5dfbd72a52acef314fa481857cc1ad4f92d621d2f3a',
+  binary_sha256({
+    aarch64: 'b4a00869eb4406a56b541d6f0acc93ac9ca94aa9e6aa89b9f8dea098abc23447',
+     armv7l: 'b4a00869eb4406a56b541d6f0acc93ac9ca94aa9e6aa89b9f8dea098abc23447',
+       i686: '1d02de32a0e266752fefab260e59d7dbfea5fcd3ac57f2dcc87116fe3e1aef00',
+     x86_64: '047f51c3a169ea6d76b45d18fa86c5f6eada542ceca4c02a9ff6e5fafcfb5201'
   })
+
+  depends_on 'libpcap'
+  depends_on 'libssh2'
 
   def self.build
-    #fixup "/usr/bin/file" -> "#{CREW_PREFIX}/bin/file" in the configure scripts
-    system "sed -i s#/usr/bin/file##{CREW_DEST_PREFIX}/bin/file#g libdnet-stripped/configure"
-    system "./configure --with-pcap=linux --without-zenmap --prefix=#{CREW_PREFIX}"
+    # ensure we build devendored deps"
+    @deps = %w[libpcap libpcre macosx mwin32 libssh2 libz]
+    @deps.each do |dep|
+      FileUtils.rm_rf dep if Dir.exist?(dep)
+    end
+    system 'autoreconf -fiv'
+    system 'filefix'
+    system "env #{CREW_ENV_OPTIONS} \
+    ./configure \
+    #{CREW_OPTIONS} \
+    --with-libpcap=#{CREW_PREFIX} \
+    --with-libpcre=#{CREW_PREFIX} \
+    --with-zlib=#{CREW_PREFIX} \
+    --with-libssh2=#{CREW_PREFIX} \
+    --with-liblua=included \
+    --without-ndiff"
     system 'make'
+    @zenmap_ = <<~'ZENMAP_EOF'
+      #!/bin/sh
+      xhost si:localuser:root
+      sudo -E LD_LIBRARY_PATH=#{CREW_LIB_PREFIX} zenmap.elf
+    ZENMAP_EOF
+    IO.write('zenmap_', @zenmap_)
   end
 
   def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system "make DESTDIR=#{CREW_DEST_DIR} install"
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/zenmap", "#{CREW_DEST_PREFIX}/bin/zenmap.elf"
+    FileUtils.install 'zenmap_', "#{CREW_DEST_PREFIX}/bin/zenmap", mode: 0o755
   end
 end
