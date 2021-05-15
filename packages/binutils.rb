7d395d2a34c16a98b56793e5eda4c28aa3ff2a6b
Satadru Pramanik <satadru@gmail.com>
2021-05-15 08:56:27 -0400

    Combine binutils and gdb, add source_highlight

--- packages/binutils.rb
+++ packages/binutils.rb
@@ -1,35 +1,45 @@
 require 'package'
 
 class Binutils < Package
-  description 'The GNU Binutils are a collection of binary tools.'
+  description 'The GNU Binutils are a collection of binary tools. This also provides the GNU Debugger.'
   homepage 'https://www.gnu.org/software/binutils/'
-  @_ver = '2.36.1'
-  version "#{@_ver}-2"
+  version '10.2'
   license 'GPL-3+'
   compatibility 'all'
-  source_url "https://ftpmirror.gnu.org/binutils/binutils-#{@_ver}.tar.xz"
-  source_sha256 'e81d9edf373f193af428a0f256674aea62a9d74dfe93f65192d4eae030b0f3b0'
+  source_url 'git://sourceware.org/git/binutils-gdb.git'
+  git_hashtag 'ce35d7163e779b1321058b22f005c70ce1524b25'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.36.1-2_armv7l/binutils-2.36.1-2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.36.1-2_armv7l/binutils-2.36.1-2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.36.1-2_i686/binutils-2.36.1-2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.36.1-2_x86_64/binutils-2.36.1-2-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/10.2_armv7l/binutils-10.2-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/10.2_armv7l/binutils-10.2-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/10.2_i686/binutils-10.2-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/10.2_x86_64/binutils-10.2-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '46564bc989d33c3518f7afbb54f95e4b614f735ddcbe783114ba397ff283e408',
-     armv7l: '46564bc989d33c3518f7afbb54f95e4b614f735ddcbe783114ba397ff283e408',
-       i686: 'e9ea43c0c5f9f98422f83b8f1732104c78b55f53572e7353657944fe9dc6298a',
-     x86_64: '784c5e9bfd884c411708ae3c2ee1c852022f411794ff109fb7d37e91f124037b'
+    aarch64: 'd40a81b55e9801eb330ca732f9a41899ac411f31c207407d89f1abe128d38645',
+     armv7l: 'd40a81b55e9801eb330ca732f9a41899ac411f31c207407d89f1abe128d38645',
+       i686: 'ef7cbd28404ab6cf05c6c3545fba364f032c0c0fe2346806f8762a60f8f23194',
+     x86_64: '3524c1cad62b2aac6e2fa98f33c215bbdb6cc8e4d7b1a03412e5f77720983798'
   })
 
-  depends_on 'zlibpkg' # R
-  depends_on 'glibc' # R
+  depends_on 'boost' => :build # R (gdb only)
   depends_on 'elfutils' # R
+  depends_on 'expat' => :build # R (gdb only)
   depends_on 'flex' # R
+  depends_on 'glibc' # R
+  depends_on 'gmp' # R
+  depends_on 'libx11' => :build
+  depends_on 'mpfr' # R
+  depends_on 'ncurses' => :build # R (gdb only)
+  depends_on 'python3' # R
+  depends_on 'readline' => :build # R (gdb only)
+  depends_on 'source_highlight' => :build # R (gdb only)
+  depends_on 'xzutils' # R
+  depends_on 'zlibpkg' # R
 
   def self.patch
     system 'filefix'
+    system "sed -i 's%#include <term.h>%#include <ncursesw/term.h>%g' gdb/gdb_curses.h"
     system "sed -i 's,scriptdir = \$(tooldir)/lib,scriptdir = \$(tooldir)/#{ARCH_LIB},g' ld/Makefile.am"
     Dir.chdir 'ld' do
       system 'aclocal && automake'
@@ -39,25 +49,38 @@ class Binutils < Package
   def self.build
     Dir.mkdir 'build'
     Dir.chdir 'build' do
-      system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
-        LDFLAGS='-flto=auto' \
-        ../configure #{CREW_OPTIONS} \
-        --disable-bootstrap \
+      system "env #{CREW_ENV_OPTIONS} \
+      CPPFLAGS='-I#{CREW_PREFIX}/include/ncursesw  -lncursesw' \
+        ../configure \
+        #{CREW_OPTIONS} \
         --disable-maintainer-mode \
+        --disable-nls \
         --enable-64-bit-bfd \
         --enable-gold \
+        --enable-host-shared \
         --enable-install-libiberty \
         --enable-ld=default \
         --enable-lto \
         --enable-plugins \
         --enable-relro \
         --enable-shared \
+        --enable-sim \
+        --enable-source-highlight \
         --enable-threads \
+        --enable-tui \
         --enable-vtable-verify \
-        --with-pic \
+        --with-curses \
         --with-lib-path=#{CREW_LIB_PREFIX} \
-        --with-system-zlib"
+        --with-lzma \
+        --with-pic \
+        --with-pkgversion=Chromebrew \
+        --with-python=python3 \
+        --with-system-gdbinit=#{CREW_PREFIX}/etc/gdb/gdbinit \
+        --with-system-readline \
+        --with-system-zlib \
+        --with-x"
       system 'make configure-host'
+      system 'make'
       system "make tooldir=#{CREW_PREFIX}"
     end
   end
@@ -70,8 +93,16 @@ class Binutils < Package
 
   def self.install
     Dir.chdir 'build' do
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
       system 'make', "DESTDIR=#{CREW_DEST_DIR}", "prefix=#{CREW_PREFIX}",
              "tooldir=#{CREW_PREFIX}", 'install'
     end
+  end
+
+  def self.postinstall
+    puts
+    puts 'To use gdb, please install the gdb runtime dependencies with:'.lightblue
+    puts 'crew install gdb'.lightblue
+    puts
   end
 end
--- packages/gdb.rb
+++ packages/gdb.rb
@@ -1,51 +1,28 @@
 require 'package'
 
 class Gdb < Package
-  description 'GDB, the GNU Project debugger, allows you to see what is going on \'inside\' another program while it executes -- or what another program was doing at the moment it crashed.'
+  description 'The GNU Debugger'
   homepage 'https://www.gnu.org/software/gdb/'
-  version '8.3'
-  license 'GPL-2 and LGPL-2'
+  version '10.2'
   compatibility 'all'
-  source_url 'http://ftpmirror.gnu.org/gdb/gdb-8.3.tar.xz'
-  source_sha256 '802f7ee309dcc547d65a68d61ebd6526762d26c3051f52caebe2189ac1ffd72e'
+  license 'GPL3'
 
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/8.3_armv7l/gdb-8.3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/8.3_armv7l/gdb-8.3-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/8.3_i686/gdb-8.3-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/8.3_x86_64/gdb-8.3-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'cb31458449dbe3901b104a0e0629e9ef0cd219ea2783db736360d0176c321ca9',
-     armv7l: 'cb31458449dbe3901b104a0e0629e9ef0cd219ea2783db736360d0176c321ca9',
-       i686: '84bd95e06d5515647135ab5ce1f1c6a2956d5df8f324837dc26705fc28bcd07c',
-     x86_64: 'afb397d5022e72ea19e7d87fff5167b798f5063f59b1174dd56337837fdc7dea',
-  })
+  depends_on 'binutils' # L (gdb is in binutils)
+  depends_on 'boost' # R (gdb only)
+  depends_on 'elfutils' # R
+  depends_on 'expat' # R (gdb only)
+  depends_on 'flex' # R
+  depends_on 'glibc' # R
+  depends_on 'gmp' # R
+  depends_on 'libx11' => :build
+  depends_on 'mpfr' # R
+  depends_on 'ncurses' # R (gdb only)
+  depends_on 'python3' # R
+  depends_on 'readline' # R (gdb only)
+  depends_on 'source_highlight' # R (gdb only)
+  depends_on 'xzutils' # R
+  depends_on 'zlibpkg' # R
 
-  depends_on 'libx11'
-  depends_on 'six'
+  is_fake
 
-  def self.build
-    system './configure',
-           '--with-x',
-           '--with-lzma',
-           '--enable-sim',
-           '--with-expat',
-           '--enable-tui',
-           '--with-python',
-           '--with-curses',
-           '--enable-shared',
-           '--with-system-zlib',
-           '--enable-64-bit-bfd',
-           '--enable-host-shared',
-           '--with-system-readline',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--with-pkgversion=Chromebrew'
-    system "make"
-  end
-
-  def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-  end
 end
