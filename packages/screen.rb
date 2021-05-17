48b289367151d2332215d0173d71d03069709a43
Satadru Pramanik <satadru@gmail.com>
2021-05-17 16:01:40 -0400

    add binaries

--- packages/screen.rb
+++ packages/screen.rb
@@ -3,30 +3,30 @@ require 'package'
 class Screen < Package
   description 'Screen is a full-screen window manager that multiplexes a physical terminal between several processes, typically interactive shells.'
   homepage 'https://www.gnu.org/software/screen/'
-  version '4.6.2'
+  version '4.8.0'
   license 'public-domain'
   compatibility 'all'
-  source_url 'https://ftp.gnu.org/gnu/screen/screen-4.6.2.tar.gz'
-  source_sha256 '1b6922520e6a0ce5e28768d620b0f640a6631397f95ccb043b70b91bb503fa3a'
+  source_url 'https://ftp.gnu.org/gnu/screen/screen-4.8.0.tar.gz'
+  source_sha256 '6e11b13d8489925fde25dfb0935bf6ed71f9eb47eff233a181e078fde5655aa1'
 
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.6.2_armv7l/screen-4.6.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.6.2_armv7l/screen-4.6.2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.6.2_i686/screen-4.6.2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.6.2_x86_64/screen-4.6.2-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.8.0_armv7l/screen-4.8.0-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.8.0_armv7l/screen-4.8.0-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.8.0_i686/screen-4.8.0-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/screen/4.8.0_x86_64/screen-4.8.0-chromeos-x86_64.tpxz'
   })
-  binary_sha256 ({
-    aarch64: 'f8d903c06175118730ea3d44ed59f6d6fbd94c7f5c6ed190e8e7ca42bb3cfc76',
-     armv7l: 'f8d903c06175118730ea3d44ed59f6d6fbd94c7f5c6ed190e8e7ca42bb3cfc76',
-       i686: '43e1b9056ee3d67b7b3f45ef52c381a62340bcaf97748a7ebc8b92adfed10026',
-     x86_64: '27ec590cfff20b1429f86baca0d42a6e3819a4978d13155b5ec31db04a374b5b',
+  binary_sha256({
+    aarch64: '9326008594f4e219f65995c8ee60e2aa5053b507d6b03c0561f2090ecae35f4e',
+     armv7l: '9326008594f4e219f65995c8ee60e2aa5053b507d6b03c0561f2090ecae35f4e',
+       i686: 'c7cf733507523e8e57c5b290c9b131a08d0c9a824b5ccc68a578e448afa152ca',
+     x86_64: '4ca4a6c4da3801b368b2cdc924310e304fb378ece773e15e7ca714c7ad0c94b3'
   })
-
-  depends_on 'ncurses'
 
   def self.build
-    system './configure',
-      "--prefix=#{CREW_PREFIX}"
+    system "env #{CREW_ENV_OPTIONS} \
+      ./configure \
+      #{CREW_OPTIONS} \
+      --enable-colors256"
     system 'make'
   end
 
