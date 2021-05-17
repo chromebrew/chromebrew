22a26082858a2515ea633d844d40653e66419fd8
satmandu <satadru@umich.edu>
2021-05-17 16:27:27 -0400

    add binaries (#5792)

--- packages/wayland_protocols.rb
+++ packages/wayland_protocols.rb
@@ -3,23 +3,23 @@ require 'package'
 class Wayland_protocols < Package
   description 'Wayland is a protocol for a compositor to talk to its clients.'
   homepage 'https://wayland.freedesktop.org/'
-  version '1.20-1'
+  version '1.21'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://wayland.freedesktop.org/releases/wayland-protocols-1.20.tar.xz'
-  source_sha256 '9782b7a1a863d82d7c92478497d13c758f52e7da4f197aa16443f73de77e4de7'
+  source_url 'https://wayland.freedesktop.org/releases/wayland-protocols-1.21.tar.xz'
+  source_sha256 'b99945842d8be18817c26ee77dafa157883af89268e15f4a5a1a1ff3ffa4cde5'
 
-  binary_url ({
-     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.20-1_armv7l/wayland_protocols-1.20-1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.20-1_armv7l/wayland_protocols-1.20-1-chromeos-armv7l.tar.xz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.20-1_i686/wayland_protocols-1.20-1-chromeos-i686.tar.xz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.20-1_x86_64/wayland_protocols-1.20-1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.21_armv7l/wayland_protocols-1.21-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.21_armv7l/wayland_protocols-1.21-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.21_i686/wayland_protocols-1.21-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.21_x86_64/wayland_protocols-1.21-chromeos-x86_64.tpxz'
   })
-  binary_sha256 ({
-     aarch64: '1decd76dce195360e3b2879b7a1ee63efefc64b437faa98e9ff5c833758730d2',
-      armv7l: '1decd76dce195360e3b2879b7a1ee63efefc64b437faa98e9ff5c833758730d2',
-        i686: '6ff5c74feef6541c342f7393fb2df70e4ec121f4b94d55f254bfa28c6e3e3dc0',
-      x86_64: 'a88f0f3c10c6108c6b9b584a1c74998d8a49ea70cb022f6350d3df129f120a72',
+  binary_sha256({
+    aarch64: '487266d1e54fd7094f0b75184bbea84044304dbbe4d64c9214069ff6cfee4472',
+     armv7l: '487266d1e54fd7094f0b75184bbea84044304dbbe4d64c9214069ff6cfee4472',
+       i686: '79e0dc6d2724fdc3ca0724a5d04649adf2e88136e3fc1b5a4ee2f4001117cac2',
+     x86_64: 'fd5283cabe38f85f245a5dcbb6b946594fefa64a5d0777a5554ee534f985ee56'
   })
 
   depends_on 'wayland'
