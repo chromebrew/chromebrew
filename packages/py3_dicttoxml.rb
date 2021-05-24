14835c7e35fae3f696b9ac700d5991e0c7300df9
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-05-23 20:24:47 -0400

    json2xml

---
+++ packages/py3_dicttoxml.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Py3_dicttoxml < Package
+  description 'DicttoXML converts Python dictionaries into XML strings.'
+  homepage 'https://github.com/quandyfactory/dicttoxml/'
+  @_ver = '1.3.1'
+  version @_ver
+  license 'MIT'
+  compatibility 'all'
+  source_url 'https://github.com/quandyfactory/dicttoxml.git'
+  git_hashtag 'v' + @_ver
+
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+  end
+
+  def self.install
+    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+  end
+end
