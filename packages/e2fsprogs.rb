3e8ad05c7f7fa46f1752418a76f8624df64ddb30
Ed Reel <edreel@gmail.com>
2021-06-20 12:51:20 -0500

    Update e2fsprogs and krb5 packages
    Build libcom_err library in krb5
    
    Add krb5 dependency to e2fsprogs

--- packages/e2fsprogs.rb
+++ packages/e2fsprogs.rb
@@ -4,24 +4,26 @@ class E2fsprogs < Package
   description 'e2fsprogs are ext2/3/4 file system utilities.'
   homepage 'http://e2fsprogs.sourceforge.net/'
   @_ver = '1.46.2'
-  version @_ver
+  version @_ver + '-1'
   license 'GPL-2 and BSD'
   compatibility 'all'
   source_url "https://mirrors.edge.kernel.org/pub/linux/kernel/people/tytso/e2fsprogs/v#{@_ver}/e2fsprogs-#{@_ver}.tar.xz"
   source_sha256 '23aa093295c94e71ef1be490c4004871c5b01d216a8cb4d111fa6c0aac354168'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2_armv7l/e2fsprogs-1.46.2-chromeos-armv7l.tpxz',
-    armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2_armv7l/e2fsprogs-1.46.2-chromeos-armv7l.tpxz',
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2_i686/e2fsprogs-1.46.2-chromeos-i686.tpxz',
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2_x86_64/e2fsprogs-1.46.2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2-1_armv7l/e2fsprogs-1.46.2-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2-1_armv7l/e2fsprogs-1.46.2-1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2-1_i686/e2fsprogs-1.46.2-1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/e2fsprogs/1.46.2-1_x86_64/e2fsprogs-1.46.2-1-chromeos-x86_64.tpxz',
   })
   binary_sha256({
-    aarch64: '63e83f7a4d01967863b2c92c09c72d2f0897ec247a82c72daa7d9b54a3740a64',
-    armv7l: '63e83f7a4d01967863b2c92c09c72d2f0897ec247a82c72daa7d9b54a3740a64',
-    i686: '1665ca1ac6d19c2505e5ea1730cc5b0d60d5eaf336b4fa4e3dc7986dfb188b53',
-    x86_64: '28da7a270ca155feaa91b2cba1c15050cb5e91d1b90283f7f973ad083dc86ff2'
+    aarch64: '51e6911d87664439b3a2b17f78a379c3a9de7c54e2703caa461a79017bc2e152',
+     armv7l: '51e6911d87664439b3a2b17f78a379c3a9de7c54e2703caa461a79017bc2e152',
+       i686: 'c26758bca2c3c6cf18668b43f5a4e37f512d5260a90ad7364faee81a1e5bd951',
+     x86_64: 'b418edf3a80a54edb3602f67b2a6f1f71b166bfac0e30edfd72829e7e48cbaaa',
   })
+
+  depends_on 'krb5'
 
   def self.build
     system "env #{CREW_ENV_OPTIONS} \
@@ -42,5 +44,12 @@ class E2fsprogs < Package
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install-libs'
+    # Avoid conflict with files in krb5.
+    FileUtils.rm_f Dir["#{CREW_DEST_LIB_PREFIX}/libcom_err.*"]
+    FileUtils.rm_f "#{CREW_DEST_PREFIX}/bin/compile_et"
+    FileUtils.rm_f "#{CREW_DEST_PREFIX}/include/com_err.h"
+    FileUtils.rm_f "#{CREW_DEST_MAN_PREFIX}/man1/compile_et.1"
+    FileUtils.rm_f "#{CREW_DEST_PREFIX}/share/et/et_c.awk"
+    FileUtils.rm_f "#{CREW_DEST_PREFIX}/share/et/et_h.awk"
   end
 end
--- packages/krb5.rb
+++ packages/krb5.rb
@@ -3,29 +3,29 @@ require 'package'
 class Krb5 < Package
   description 'Kerberos is a network authentication protocol.'
   homepage 'https://web.mit.edu/kerberos'
-  version '1.19.1'
+  @_ver = '1.19.1'
+  version @_ver + '-1'
   license 'openafs-krb5-a, BSD, MIT, OPENLDAP, BSD-2, HPND, BSD-4, ISC, RSA, CC-BY-SA-3.0 and BSD-2 or GPL-2+ )'
   compatibility 'all'
   source_url 'https://web.mit.edu/kerberos/dist/krb5/1.19/krb5-1.19.1.tar.gz'
   source_sha256 'fa16f87eb7e3ec3586143c800d7eaff98b5e0dcdf0772af7d98612e49dbeb20b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_armv7l/krb5-1.19.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_armv7l/krb5-1.19.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_i686/krb5-1.19.1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_x86_64/krb5-1.19.1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1-1_armv7l/krb5-1.19.1-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1-1_armv7l/krb5-1.19.1-1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1-1_i686/krb5-1.19.1-1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1-1_x86_64/krb5-1.19.1-1-chromeos-x86_64.tpxz',
   })
   binary_sha256({
-    aarch64: 'ed77e5d8274ca8cd6acf53edca8d86479aaa67be18d69927424fa3b0572e970e',
-     armv7l: 'ed77e5d8274ca8cd6acf53edca8d86479aaa67be18d69927424fa3b0572e970e',
-       i686: 'ab646a5876870bbd762229dbad705f6826eaf34bd2f3838172916ffcc5067a3a',
-     x86_64: 'dde095df31c2a2b8925d0d9323a9e447b07238cec09ae0e5a43191d0a7b2c94d'
+    aarch64: '3580a0f77f18a4c76b1218596ff8b774d325036fe7c5c9c54c898665300f123e',
+     armv7l: '3580a0f77f18a4c76b1218596ff8b774d325036fe7c5c9c54c898665300f123e',
+       i686: '212f2bea3b6ec825b609703e0385e2a80de7c56b6c1cca4e6680a60dc8808fb9',
+     x86_64: '2801e5e94ff094d3bfa86992042c57ee6bc15c13ce4d63b08c4b93c264114a25',
   })
 
   depends_on 'ccache' => :build
-  depends_on 'e2fsprogs'
 
-  @k5libs = %w[libgssapi_krb5.a libgssrpc.a libk5crypto.a
+  @k5libs = %w[libgssapi_krb5.a libgssrpc.a libk5crypto.a libcom_err.a
                libkadm5clnt_mit.a libkadm5clnt.a libkadm5srv_mit.a libkadm5srv.a
                libkdb5.a libkrad.a libkrb5.a libkrb5support.a libverto.a]
 
@@ -43,8 +43,8 @@ class Krb5 < Package
       --localstatedir=#{CREW_PREFIX}/var/krb5kdc \
       --disable-shared \
       --enable-static \
-      --with-system-et \
-      --with-system-ss \
+      --without-system-et \
+      --without-system-ss \
       --without-system-verto"
       system "env PATH=#{@path} \
       make -j#{CREW_NPROC}"
@@ -61,8 +61,8 @@ class Krb5 < Package
       ./configure #{CREW_OPTIONS} \
       --localstatedir=#{CREW_PREFIX}/var/krb5kdc \
       --enable-shared \
-      --with-system-et \
-      --with-system-ss \
+      --without-system-et \
+      --without-system-ss \
       --without-system-verto"
       system "env PATH=#{@path} \
       make -j#{CREW_NPROC}"
