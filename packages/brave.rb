c78c92b223a95fa64fa396027a9dae06d520c889
Ed Reel <edreel@gmail.com>
2021-06-28 10:34:49 -0500

    Update brave from 1.26.11 to 1.28.33 (#5904)

--- packages/brave.rb
+++ packages/brave.rb
@@ -3,11 +3,11 @@ require 'package'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '1.26.21'
+  version '1.28.33'
   license 'MPL-2'
   compatibility 'x86_64'
   source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-nightly-#{version}-linux-amd64.zip"
-  source_sha256 'd43665349e0575a38ec7109744b8c85c0dd54e9f775ffd8ee35016ef11b17fba'
+  source_sha256 'd0d9e55f0815561091f6606d89ab5d44d8a6a9fe9ef462be9aca87a35b0b25ab'
 
   depends_on 'gtk3'
   depends_on 'xdg_base'
