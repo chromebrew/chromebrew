2aeb242376a2688415ae3582bab69814fd340f6a
Ed Reel <edreel@gmail.com>
2021-06-28 00:20:04 -0500

    Update kotlin from 1.4.0 to 1.5.20

--- packages/kotlin.rb
+++ packages/kotlin.rb
@@ -3,25 +3,20 @@ require 'package'
 class Kotlin < Package
   description 'Statically typed programming language for modern multiplatform applications'
   homepage 'https://kotlinlang.org/'
-  version '1.4.0'
+  version '1.5.20'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://github.com/JetBrains/kotlin/releases/download/v1.4.0/kotlin-compiler-1.4.0.zip'
-  source_sha256 '590391d13b3c65ba52cba470f56efd5b14e2b1f5b9459f63aa12eb38ef52f161'
-
-  binary_url ({
-  })
-  binary_sha256 ({
-  })
+  source_url 'https://github.com/JetBrains/kotlin/releases/download/v1.5.20/kotlin-compiler-1.5.20.zip'
+  source_sha256 'edf34263ddaabd48f7ec59661e4c0d1dc868462fd3a1ea323083d0e3e83a8a8b'
 
   depends_on 'jdk8'
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
-    system "mkdir -p #{CREW_DEST_PREFIX}/share/kotlin"
-    system "rm -f bin/*.bat"
-    system "cp -r . #{CREW_DEST_PREFIX}/share/kotlin"
-    FileUtils.cd("#{CREW_DEST_PREFIX}/share/kotlin/bin") do
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/kotlin"
+    FileUtils.rm_f Dir['bin/*.bat']
+    FileUtils.mv Dir['.'] "#{CREW_DEST_PREFIX}/share/kotlin"
+    FileUtils.cd "#{CREW_DEST_PREFIX}/share/kotlin/bin" do
       system "find . -type f -exec ln -s #{CREW_PREFIX}/share/kotlin/bin/{} #{CREW_DEST_PREFIX}/bin \\;"
     end
   end
