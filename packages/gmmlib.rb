dcd7d10a0fcde0c8b6dbf447c89b447d87a0f419
satmandu <satadru@umich.edu>
2021-06-30 22:35:47 -0400

    libva gmmlib intel_media_driver updates (#5917)

--- packages/gmmlib.rb
+++ packages/gmmlib.rb
@@ -3,36 +3,26 @@ require 'package'
 class Gmmlib < Package
   description 'The Intel(R) Graphics Memory Management Library provides device specific and buffer management for the Intel(R) Graphics Compute Runtime for OpenCL(TM) and the Intel(R) Media Driver for VAAPI.'
   homepage 'https://github.com/intel/gmmlib/'
-  @_ver = '21.1.1'
+  @_ver = '21.2.1'
   version @_ver
   license 'MIT'
-  compatibility 'i686 x86_64'
-  source_url "https://github.com/intel/gmmlib/archive/intel-gmmlib-#{@_ver}.tar.gz"
-  source_sha256 'b996f09264e05ebca0dc275ea32791ba22769fe04147592371b4a61cdf508763'
+  compatibility 'x86_64'
+  source_url 'https://github.com/intel/gmmlib.git'
+  git_hashtag "intel-gmmlib-#{@_ver}"
 
   binary_url({
-      i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmmlib/21.1.1_i686/gmmlib-21.1.1-chromeos-i686.tar.xz',
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmmlib/21.1.1_x86_64/gmmlib-21.1.1-chromeos-x86_64.tar.xz'
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmmlib/21.2.1_x86_64/gmmlib-21.2.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-      i686: 'c71cb96bf16648e0818daca7913e32a71a67a117ffaf1379f7e4970f075d5454',
-    x86_64: '8cc86525354adbcc15e2e9ba8b59444178ec2037f72f9d29763286ef23dd1973'
+    x86_64: '99d879841a2c3d0d9d02438ae37720bec7b02f68ba177b52992b0fac6092bf1d'
   })
 
   depends_on 'libva'
 
-  def self.patch
-    system "find . -type f -exec sed -e 's,LD_LIBRARY_PATH=,LD_LIBRARY_PATH=#{CREW_LIB_PREFIX}:,g' \
-            -e 's,-fstack-protector,-fno-stack-protector,g' -i {} +"
-  end
-
   def self.build
     FileUtils.mkdir('builddir')
     Dir.chdir('builddir') do
-      system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
-            CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
-            LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-            cmake -G Ninja \
+      system "cmake -G Ninja \
             #{CREW_CMAKE_OPTIONS} \
             -DRUN_TEST_SUITE=OFF \
             .."
--- packages/intel_media_driver.rb
+++ packages/intel_media_driver.rb
@@ -3,18 +3,18 @@ require 'package'
 class Intel_media_driver < Package
   description 'The Intel(R) Media Driver for VAAPI is a new VA-API (Video Acceleration API) user mode driver supporting hardware accelerated decoding, encoding, and video post processing for GEN based graphics hardware.'
   homepage 'https://github.com/intel/media-driver'
-  @_ver = '21.1.3'
+  @_ver = '21.2.3'
   version @_ver
   license 'BSD-3, and MIT'
   compatibility 'x86_64'
-  source_url "https://github.com/intel/media-driver/archive/intel-media-#{@_ver}.tar.gz"
-  source_sha256 '219ce6b08a84bdce311160dc694d866249fd4e390391c2ac7be55f13a2fb928c'
+  source_url 'https://github.com/intel/media-driver.git'
+  git_hashtag "intel-media-#{@_ver}"
 
   binary_url({
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/intel_media_driver/21.1.3_x86_64/intel_media_driver-21.1.3-chromeos-x86_64.tar.xz'
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/intel_media_driver/21.2.3_x86_64/intel_media_driver-21.2.3-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    x86_64: 'cb0a13759d9694716c928640384ca59c3ac044a3379e0cc4160f156347e4d158'
+    x86_64: '9bf56121437ac94df50cd1e9e6d2ac74802d0b72d901a0019c47d6ef68af353a'
   })
 
   depends_on 'gmmlib'
@@ -27,16 +27,14 @@ class Intel_media_driver < Package
   def self.patch
     system "find . -type f -exec sed -e 's,LD_LIBRARY_PATH=,LD_LIBRARY_PATH=#{CREW_LIB_PREFIX}:,g' \
             -e 's,-fstack-protector-all,-fno-stack-protector,g' \
+            -e 's,-Werror=return-type,-Werror=return-type -Wno-error=maybe-uninitialized,g' \
             -e 's,-fstack-protector,-fno-stack-protector,g' -i {} +"
   end
 
   def self.build
     FileUtils.mkdir('builddir')
     Dir.chdir('builddir') do
-      system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
-            CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto -fuse-ld=gold' \
-            LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-            cmake #{CREW_CMAKE_OPTIONS} ../ -G Ninja"
+      system "cmake #{CREW_CMAKE_OPTIONS.sub('-pipe', '-pipe -Wno-error')} ../ -G Ninja"
     end
     system 'ninja -C builddir'
   end
