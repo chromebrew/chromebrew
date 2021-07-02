18e57e110b8cb78906266754b06b1979e59949ce
Ed Reel <edreel@gmail.com>
2021-07-02 04:25:54 -0500

    Rebuild ctorrent binaries (#5920)

--- packages/ctorrent.rb
+++ packages/ctorrent.rb
@@ -3,36 +3,35 @@ require 'package'
 class Ctorrent < Package
   description 'CTorrent is a BitTorrent client implemented in C++ to be lightweight and quick.'
   homepage 'http://www.rahul.net/dholmes/ctorrent/'
-  version '3.3.2'
+  @_ver = '3.3.2'
+  version @_ver + '-1'
   license 'GPL-2'
   compatibility 'all'
   source_url 'http://www.rahul.net/dholmes/ctorrent/ctorrent-dnh3.3.2.tar.gz'
   source_sha256 'c87366c91475931f75b924119580abd06a7b3cb3f00fef47346552cab1e24863'
 
   binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2_armv7l/ctorrent-3.3.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2_armv7l/ctorrent-3.3.2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2_i686/ctorrent-3.3.2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2_x86_64/ctorrent-3.3.2-chromeos-x86_64.tar.xz',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2-1_armv7l/ctorrent-3.3.2-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2-1_armv7l/ctorrent-3.3.2-1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2-1_i686/ctorrent-3.3.2-1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ctorrent/3.3.2-1_x86_64/ctorrent-3.3.2-1-chromeos-x86_64.tpxz',
   })
   binary_sha256 ({
-    aarch64: '32f8e0727420efc33e670f025c1608fe4c8aec5cbf660a24fde1366dd8a85ef1',
-     armv7l: '32f8e0727420efc33e670f025c1608fe4c8aec5cbf660a24fde1366dd8a85ef1',
-       i686: '18e5029cb1032bf6a623197c06a5f023d1a3af53e421be77ab8950be52a5bda3',
-     x86_64: '012252e1976bd0580c34f697cff7338b53d4989b6f52a02b5171807981aa1ec4',
+    aarch64: '4f56ef37eccd39f4a2b410feaacd1d2904d789af45e72b3a7bdab3cda30d764e',
+     armv7l: '4f56ef37eccd39f4a2b410feaacd1d2904d789af45e72b3a7bdab3cda30d764e',
+       i686: '9fcf5a83b561500c4ec44dfbcedc0dc3fd51343221ae6a65d8496cb9a8f8b750',
+     x86_64: '74fa2be2160fb2ffa7fe9fa2479d52636be246db4da7955518eed5fc1b7c2959',
   })
 
-  depends_on 'openssl' => :build
-
   def self.build
-    system "./configure"
-  end
-
-  def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system './configure'
   end
 
   def self.check
-    system "make", "check"
+    system 'make', 'check'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
