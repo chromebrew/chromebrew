d20067a1c63a28fd3a950097e54b7a2dd1a96508
supechicken <supechicken666@gmail.com>
2021-07-07 21:31:50 +0800

    Fix .deb unarchive algorithm (#5931)
    * Add files via upload
    
    * Add exception switch back
    
    * Bump version

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.11.5'
+CREW_VERSION = '1.11.6'
 
 ARCH_ACTUAL = `uname -m`.strip
 # This helps with virtualized builds on aarch64 machines
