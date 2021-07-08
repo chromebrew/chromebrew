34c6b74300523301668275788b3f8714cb68dac2
Ed Reel <edreel@gmail.com>
2021-07-07 19:27:46 -0500

    Update glib from 2.68.3 to 2.69.0

--- packages/glib.rb
+++ packages/glib.rb
@@ -3,7 +3,7 @@ require 'package'
 class Glib < Package
   description 'GLib provides the core application building blocks for libraries and applications written in C.'
   homepage 'https://developer.gnome.org/glib'
-  @_ver = '2.68.3'
+  @_ver = '2.69.0'
   @_ver_prelastdot = @_ver.rpartition('.')[0]
   version @_ver
   license 'LGPL-2.1'
@@ -12,16 +12,16 @@ class Glib < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.68.3_armv7l/glib-2.68.3-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.68.3_armv7l/glib-2.68.3-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.68.3_i686/glib-2.68.3-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.68.3_x86_64/glib-2.68.3-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.69.0_armv7l/glib-2.69.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.69.0_armv7l/glib-2.69.0-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.69.0_i686/glib-2.69.0-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.69.0_x86_64/glib-2.69.0-chromeos-x86_64.tar.xz',
   })
   binary_sha256({
-    aarch64: '640d0eecf23f8772e38ac490b9c491eb59899d9a1dc50b346e499877363fc629',
-     armv7l: '640d0eecf23f8772e38ac490b9c491eb59899d9a1dc50b346e499877363fc629',
-       i686: '95880703d43a11a6df0d22b7ad1f94272690f1ada158d69506cf9629ac929877',
-     x86_64: 'b0e7d349295d9ba8b01347f0198ab327345f5b8e2c8ad3fab6b6ee584fe03971'
+    aarch64: '573261af071950bce6f0ed2022cff39ed9fe341a0fc623fda18dd12a246b1392',
+     armv7l: '573261af071950bce6f0ed2022cff39ed9fe341a0fc623fda18dd12a246b1392',
+       i686: '60f54d5bac8c668f5b932e021b17391560353e6ae8391fed00a6af740620eea5',
+     x86_64: 'f2f4cce78c7cedfa0ab73438c83f6aed840b5bdd92b8d0dcb7dea3ef81bd785f',
   })
 
   depends_on 'pygments' => :build
