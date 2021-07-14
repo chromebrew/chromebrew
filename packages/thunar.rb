0c949aca8d9f254590955b71c08695be97bf5ee4
Ed Reel <edreel@gmail.com>
2021-07-14 16:49:47 -0500

    Update thunar from 4.17.1 to 4.17.3 (#5964)

--- packages/thunar.rb
+++ packages/thunar.rb
@@ -3,12 +3,23 @@ require 'package'
 class Thunar < Package
   description 'Thunar File Manager'
   homepage 'https://docs.xfce.org/xfce/thunar/Start'
-  @_ver = '4.17.1'
+  @_ver = '4.17.3'
   version @_ver
   license 'GPL-2+ and LGPL-2+'
-  compatibility 'all'
+  compatibility 'aarch64,armv7l,x86_64'
   source_url "https://archive.xfce.org/src/xfce/thunar/#{@_ver.rpartition('.')[0]}/thunar-#{@_ver}.tar.bz2"
-  source_sha256 '33f99656d5f01b8ec6c3faa9a9f645c264cd460029ad08d96ba76925c43f2571'
+  source_sha256 '4580913d6c88003dbffc7e6d98a843ca0ae0fd1c5fa7b1e49fef565f33c7bea7'
+
+  binary_url ({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/thunar/4.17.3_armv7l/thunar-4.17.3-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/thunar/4.17.3_armv7l/thunar-4.17.3-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/thunar/4.17.3_x86_64/thunar-4.17.3-chromeos-x86_64.tpxz',
+  })
+  binary_sha256 ({
+    aarch64: '4cd11110afd9b2b55ce2c167a494e95fd49e5b44ecece194e2561ea5e9b93c19',
+     armv7l: '4cd11110afd9b2b55ce2c167a494e95fd49e5b44ecece194e2561ea5e9b93c19',
+     x86_64: 'f567c97ed456b27374b465283081605bb70d6b4a7ff84851580c45891f02945c',
+  })
 
   depends_on 'exo'
   depends_on 'libexif'
