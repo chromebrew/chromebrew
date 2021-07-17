a1506c60d70e418f12f64f01afd5a9274c564131
Satadru Pramanik <satadru@gmail.com>
2021-07-16 21:19:54 -0400

    Graphics & Gnome updates

--- packages/gnome_settings_daemon.rb
+++ packages/gnome_settings_daemon.rb
@@ -3,24 +3,22 @@ require 'package'
 class Gnome_settings_daemon < Package
   description 'GNOME Settings Daemon'
   homepage 'https://gitlab.gnome.org/GNOME/gnome-settings-daemon'
-  @_ver = '40.0'
+  @_ver = '40.0.1'
   version @_ver
   license 'GPL-2+ and LGPL-2+'
-  compatibility 'all'
-  source_url "https://gitlab.gnome.org/GNOME/gnome-settings-daemon/-/archive/#{@_ver}/gnome-settings-daemon-#{@_ver}.tar.bz2"
-  source_sha256 'ea6351b9f82c507e431cea15a69e7bb0574b8003618c48ff8c07e04969516e7f'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://gitlab.gnome.org/GNOME/gnome-settings-daemon.git'
+  git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_settings_daemon/40.0_armv7l/gnome_settings_daemon-40.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_settings_daemon/40.0_armv7l/gnome_settings_daemon-40.0-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_settings_daemon/40.0_i686/gnome_settings_daemon-40.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_settings_daemon/40.0_x86_64/gnome_settings_daemon-40.0-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_settings_daemon/40.0.1_armv7l/gnome_settings_daemon-40.0.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_settings_daemon/40.0.1_armv7l/gnome_settings_daemon-40.0.1-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_settings_daemon/40.0.1_x86_64/gnome_settings_daemon-40.0.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: 'ae054caa58411bbf1f00729e748aaf0974913bff99ff77d24797f3814e4c5387',
-     armv7l: 'ae054caa58411bbf1f00729e748aaf0974913bff99ff77d24797f3814e4c5387',
-       i686: '90a5725eb84e76c5a23bd80fb670a569e63315a6ae8a599ef92cf8ec499f3aec',
-     x86_64: '294b8c533a037a62c14971e49d909b5a48c62b7717734bdc3b0ab20ee981be11'
+    aarch64: '13be0d7f914c132310b6a5ccd7cfb9584269dac858a3cc61a46fcd63966cf425',
+     armv7l: '13be0d7f914c132310b6a5ccd7cfb9584269dac858a3cc61a46fcd63966cf425',
+     x86_64: '26646a35e2335da43548bcade06a0f1e609ee8eacfe1815c2ac6c07a2187355d'
   })
 
   depends_on 'dconf'
@@ -40,20 +38,20 @@ class Gnome_settings_daemon < Package
   depends_on 'geoclue'
   depends_on 'gcr'
 
-  def self.patch
-    # Source has libgnome-volume-control repo as submodule
-    @git_dir = 'subprojects/gvc'
-    @git_hash = '7a621180b46421e356b33972e3446775a504139c'
-    @git_url = 'https://gitlab.gnome.org/GNOME/libgnome-volume-control.git'
-    FileUtils.rm_rf(@git_dir)
-    FileUtils.mkdir_p(@git_dir)
-    Dir.chdir @git_dir do
-      system 'git init'
-      system "git remote add origin #{@git_url}"
-      system "git fetch --depth 1 origin #{@git_hash}"
-      system 'git checkout FETCH_HEAD'
-    end
-  end
+  # def self.patch
+  ## Source has libgnome-volume-control repo as submodule
+  # @git_dir = 'subprojects/gvc'
+  # @git_hash = '7a621180b46421e356b33972e3446775a504139c'
+  # @git_url = 'https://gitlab.gnome.org/GNOME/libgnome-volume-control.git'
+  # FileUtils.rm_rf(@git_dir)
+  # FileUtils.mkdir_p(@git_dir)
+  # Dir.chdir @git_dir do
+  # system 'git init'
+  # system "git remote add origin #{@git_url}"
+  # system "git fetch --depth 1 origin #{@git_hash}"
+  # system 'git checkout FETCH_HEAD'
+  # end
+  # end
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
