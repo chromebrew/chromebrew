fdad36b4754bab2f562b84493f7a9c5173dedcda
Ed Reel <edreel@gmail.com>
2021-07-18 00:18:50 -0500

    Update putty from 0.70 to 0.76

--- packages/putty.rb
+++ packages/putty.rb
@@ -3,27 +3,27 @@ require 'package'
 class Putty < Package
   description 'Free Telnet, SSH, and Rlogin clients plus a terminal emulator'
   homepage 'https://www.chiark.greenend.org.uk/~sgtatham/putty/'
-  version '0.70'
+  version '0.76'
   license 'MIT'
-  compatibility 'all'
-  source_url 'https://the.earth.li/~sgtatham/putty/latest/putty-0.70.tar.gz'
-  source_sha256 'bb8aa49d6e96c5a8e18a057f3150a1695ed99a24eef699e783651d1f24e7b0be'
+  compatibility 'aarch64,armv7l,x86_64'
+  source_url 'https://the.earth.li/~sgtatham/putty/latest/putty-0.76.tar.gz'
+  source_sha256 '547cd97a8daa87ef71037fab0773bceb54a8abccb2f825a49ef8eba5e045713f'
 
   binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/putty/0.70_armv7l/putty-0.70-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/putty/0.70_armv7l/putty-0.70-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/putty/0.70_i686/putty-0.70-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/putty/0.70_x86_64/putty-0.70-chromeos-x86_64.tar.xz',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/putty/0.76_armv7l/putty-0.76-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/putty/0.76_armv7l/putty-0.76-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/putty/0.76_x86_64/putty-0.76-chromeos-x86_64.tpxz',
   })
   binary_sha256 ({
-    aarch64: '5c12ee34ea177bc3b03e4684100246cc0d5a3553a68622c88b1aeff3cea141d9',
-     armv7l: '5c12ee34ea177bc3b03e4684100246cc0d5a3553a68622c88b1aeff3cea141d9',
-       i686: '49b86933fe7a26d4c5cd211ef117a21a7d5262b99973d1cacd8fa8e563504370',
-     x86_64: 'f52110b94240f1cf02eace40fd962d58084fb8ce13970387a658548fbf56a366',
+    aarch64: '1c8444fbab905ffbe099078db6165da8056a1f4cfcd7ad038bcd9bd0a00c4d2c',
+     armv7l: '1c8444fbab905ffbe099078db6165da8056a1f4cfcd7ad038bcd9bd0a00c4d2c',
+     x86_64: '6c0e2c9e868099bdcd4657a380b269e1ee97fc10020cce67cacf32a5b0616a37',
   })
 
+  depends_on 'gtk3'
+
   def self.build
-    system './configure --without-gtk'
+    system "./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
