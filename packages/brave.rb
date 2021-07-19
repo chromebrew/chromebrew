8443712d5b1655659d4863005b833b544d8fbf7b
Ed Reel <edreel@gmail.com>
2021-07-19 18:27:18 -0500

    Revert brave from nightly 1.28.33 to pre-release 1.27.106 (#5971)

--- packages/brave.rb
+++ packages/brave.rb
@@ -3,17 +3,19 @@ require 'package'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '1.28.33'
+  version '1.27.106'
   license 'MPL-2'
   compatibility 'x86_64'
-  source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-nightly-#{version}-linux-amd64.zip"
-  source_sha256 'd0d9e55f0815561091f6606d89ab5d44d8a6a9fe9ef462be9aca87a35b0b25ab'
+  source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-#{version}-linux-amd64.zip"
+  source_sha256 'f90c1b9bd4d775a0931ce143b7fc9e0a2f1299e63128a912dd5beef265c5f59e'
 
   depends_on 'gtk3'
+  depends_on 'libcom_err'
   depends_on 'xdg_base'
   depends_on 'sommelier' # The hardware acceleration with Wayland is broken, so use x11
 
   def self.install
+    ENV['CREW_SHRINK_ARCHIVE'] = '0'
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/brave"
     FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/share/brave"
