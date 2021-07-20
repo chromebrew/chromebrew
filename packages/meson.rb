764e19857815bb6d7a6e361a06a9917e9e393c61
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-20 12:45:42 -0700

    update meson

--- packages/meson.rb
+++ packages/meson.rb
@@ -3,7 +3,7 @@ require 'package'
 class Meson < Package
   description 'Meson is an open source build system meant to be both extremely fast and user friendly.'
   homepage 'https://mesonbuild.com/'
-  @_ver = '0.58.0'
+  @_ver = '0.58.1'
   version @_ver
   license 'Apache-2.0'
   compatibility 'all'
