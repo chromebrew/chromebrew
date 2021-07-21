8633f04d2b5d7a647697fbfc40e36d3033b86cf4
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-21 09:49:32 -0700

    py3_libxml2

---
+++ packages/py3_libxml2.rb
@@ -0,0 +1,28 @@
+require 'package'
+
+class Py3_libxml2 < Package
+  description 'Libxml2-python provides access to libxml2 and libxslt in Python.'
+  homepage 'https://gitlab.gnome.org/GNOME/libxml2/'
+  @_ver = '2.9.12'
+  version @_ver
+  license 'MIT'
+  compatibility 'all'
+  source_url 'https://gitlab.gnome.org/GNOME/libxml2.git'
+  git_hashtag 'v' + @_ver
+
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system 'autoreconf -fiv'
+    system "./configure #{CREW_OPTIONS}"
+    Dir.chdir('python') do
+      system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+    end
+  end
+
+  def self.install
+    Dir.chdir('python') do
+      system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+    end
+  end
+end
