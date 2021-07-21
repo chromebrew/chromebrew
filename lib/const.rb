ab6355d542d63951fa908ad83a45ec4ac3619a60
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-21 07:53:05 -0700

    update lib/const.rb

--- lib/const.rb
+++ lib/const.rb
@@ -102,3 +102,6 @@ PY3_SETUP_BUILD_OPTIONS = "--executable=#{CREW_PREFIX}/bin/python3"
 PY2_SETUP_BUILD_OPTIONS = "--executable=#{CREW_PREFIX}/bin/python2"
 PY_SETUP_INSTALL_OPTIONS_NO_SVEM = "--root=#{CREW_DEST_DIR} --prefix=#{CREW_PREFIX} -O2 --compile"
 PY_SETUP_INSTALL_OPTIONS = "#{PY_SETUP_INSTALL_OPTIONS_NO_SVEM} --single-version-externally-managed"
+
+CREW_FIRST_PACKAGES = %w[curl git pixz shared_mime_info]
+CREW_LAST_PACKAGES = %w[ghc mandb gtk3 gtk4 sommelier]
