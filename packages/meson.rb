748f4f5a983ecffcd9784403a259d3cd7998fed5
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-22 13:00:21 -0700

    packages/meson.rb: compile better

--- packages/meson.rb
+++ packages/meson.rb
@@ -15,18 +15,16 @@ class Meson < Package
   depends_on 'py3_setuptools' => :build
 
   def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
-
-  def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
-    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/meson", "#{CREW_DEST_PREFIX}/bin/meson.py"
     system "python3 -m nuitka --show-progress \
             --show-scons \
             --follow-imports \
             --prefer-source-code \
-            #{CREW_DEST_PREFIX}/bin/meson.py \
-            -o #{CREW_DEST_PREFIX}/bin/meson"
-    FileUtils.rm "#{CREW_DEST_PREFIX}/bin/meson.py"
+            meson.py \
+            -o meson"
+  end
+
+  def self.install
+    FileUtils.mkdir "#{CREW_DEST_PREFIX}/bin/"
+    FileUtils.cp "meson", "#{CREW_DEST_PREFIX}/bin/meson"
   end
 end
