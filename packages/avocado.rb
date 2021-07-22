49189cf12735b12f2ba40371a1a46f7983770064
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-22 10:09:43 -0700

    packages/avocado.rb: create dummy package

---
+++ packages/avocado.rb
@@ -0,0 +1,14 @@
+require 'package'
+
+class Avocado < Package
+  description 'A dummy package for avocado_framework.'
+  homepage 'https://avocado-framework.github.io/'
+  @_ver = '87.0'
+  version @_ver
+  license 'GPL-2 and GPL-2+'
+  compatibility 'all'
+  is_fake
+
+  depends_on 'avocado_framework'
+
+end
