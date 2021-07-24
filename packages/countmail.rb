7cfd836522b9cd0c0dbdd147c284e091db1c6f20
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:41:30 -0700

    countmail

---
+++ packages/countmail.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Countmail < Bsdgame
+  description 'Informs the user of their loneliness or lack of friends. Perfect for a pandemic.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
