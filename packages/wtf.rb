a146f0878ad0fbe477fecc7311d15d3843a9048c
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:45:18 -0700

    wtf

---
+++ packages/wtf.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Wtf < Bsdgame
+  description 'Look up abbreviations to find out wtf they stand for.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
