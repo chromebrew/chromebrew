3bb8fec6d3c095c324f1a0c7e5b9ac9f971b81f1
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:41:56 -0700

    gomoku

---
+++ packages/gomoku.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Gomoku < Bsdgame
+  description 'A Connect-5 version of tic-tac-toe.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
