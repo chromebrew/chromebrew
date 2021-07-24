d1a51cad568c980bf93a5a5686bec86fec92f784
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:43:03 -0700

    phantasia

---
+++ packages/phantasia.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Phantasia < Bsdgame
+  description 'Phantasia is a multi-player multi-terminal fantasy game.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
