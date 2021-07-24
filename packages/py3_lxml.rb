799b2a8b60f19790c5ffde4312b47eef79d7f7e1
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 14:43:10 -0700

    packages/py3_lxml.rb: add py3_cython as builddep

--- packages/py3_lxml.rb
+++ packages/py3_lxml.rb
@@ -10,6 +10,7 @@ class Py3_lxml < Package
   source_url 'https://github.com/lxml/lxml.git'
   git_hashtag 'lxml-' + @_ver
 
+  depends_on 'py3_cython' => :build
   depends_on 'py3_setuptools' => :build
 
   def self.build
