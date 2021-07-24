b31c8ebfaa126e4115fa7c7c91a6e82ab2dca2aa
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:43:34 -0700

    primes

---
+++ packages/primes.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Primes < Bsdgame
+  description 'Primes generates prime numbers up to 2^64-1'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
