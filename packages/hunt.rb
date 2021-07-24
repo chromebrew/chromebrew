b5f3d6d4f25e8dc1fd19360b9f5f29f38e77f198
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:42:25 -0700

    hunt

---
+++ packages/hunt.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Hunt < Bsdgame
+  description 'Hunt is a multi-player multi-terminal game.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
