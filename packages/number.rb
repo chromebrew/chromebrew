efa3e52d612cb01fe005a29dff8170d838b3617f
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:42:54 -0700

    number

---
+++ packages/number.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Number < Bsdgame
+  description 'Number converts Arabic numerals to English.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
