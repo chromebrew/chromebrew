1a96de4a0dffe4154b2bba696f64260d8bf144f9
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:38:45 -0700

    adventure

---
+++ packages/adventure.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Adventure < Bsdgame
+  description 'Colossal Cave Adventure is a text adventure game, developed between 1975 and 1977 by Will Crowther and Don Woods for the PDP-10 mainframe.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
