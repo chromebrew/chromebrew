699f29cf0e1e5d062e6c9d90b5f1761f96bbce86
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-24 15:41:02 -0700

    caesar

---
+++ packages/caesar.rb
@@ -0,0 +1,14 @@
+require 'package'
+require 'packages/libbsdgames2'
+
+class Caesar < Bsdgame
+  description 'Caesar performs rotated-alphabet cryptography, aka caesar cyphers.'
+  homepage $_bsdgame_homepage
+  version $_bsdgame_version
+  license 'BSD'
+  compatibility 'all'
+  source_url $_bsdgame_source_url
+  git_hashtag $_bsdgame_git_hashtag
+
+  @_bsdgame = self.name.to_s.downcase
+end
