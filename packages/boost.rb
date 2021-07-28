966aed3ba5df82c14844ac593b629f0736ab52e6
Ed Reel <edreel@gmail.com>
2021-07-28 01:38:58 -0500

    Add pre-built binaries for boost (#5998)

--- packages/boost.rb
+++ packages/boost.rb
@@ -10,6 +10,19 @@ class Boost < Package
   source_url "https://boostorg.jfrog.io/artifactory/main/release/#{@_ver}/source/boost_#{@_ver.gsub('.','_')}.tar.bz2"
   source_sha256 'f0397ba6e982c4450f27bf32a2a83292aba035b827a5623a14636ea583318c41'
 
+  binary_url ({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/boost/1.76.0_armv7l/boost-1.76.0-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/boost/1.76.0_armv7l/boost-1.76.0-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/boost/1.76.0_i686/boost-1.76.0-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/boost/1.76.0_x86_64/boost-1.76.0-chromeos-x86_64.tpxz',
+  })
+  binary_sha256 ({
+    aarch64: '3faf1c0322845086c77e4ef173beee27ec5fa6c780727c3e6eef85584ee8bc40',
+     armv7l: '3faf1c0322845086c77e4ef173beee27ec5fa6c780727c3e6eef85584ee8bc40',
+       i686: 'f5f924e7ccaead8a1db56fff2963233d36e5466dc34fcbf7d9527ee00c117aeb',
+     x86_64: 'b64b49911d39f51b9460e3ae4ba76f6239b5b2aec473a8376780330ea68b2277',
+  })
+
   def self.build
     system './bootstrap.sh'
   end
