bdce202d165bd50bd58b4cfb9d0b98565bb9f442
Ed Reel <edreel@gmail.com>
2021-07-29 06:56:38 -0500

    Revert fontconfig, freetype and harfbuzz packages (#6004)

--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -3,37 +3,37 @@ require 'package'
 class Harfbuzz < Package
   description 'HarfBuzz is an OpenType text shaping engine.'
   homepage 'https://www.freedesktop.org/wiki/Software/HarfBuzz/'
-  @_ver = '2.8.2'
+  @_ver = '2.8.1'
   version @_ver
   license 'Old-MIT, ISC and icu'
-  compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://github.com/harfbuzz/harfbuzz.git'
-  git_hashtag @_ver
+  compatibility 'all'
+  source_url "https://github.com/harfbuzz/harfbuzz/archive/#{@_ver}.tar.gz"
+  source_sha256 'b3f17394c5bccee456172b2b30ddec0bb87e9c5df38b4559a973d14ccd04509d'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/2.8.2_armv7l/harfbuzz-2.8.2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/2.8.2_armv7l/harfbuzz-2.8.2-chromeos-armv7l.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/2.8.2_x86_64/harfbuzz-2.8.2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/2.8.1_armv7l/harfbuzz-2.8.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/2.8.1_armv7l/harfbuzz-2.8.1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/2.8.1_i686/harfbuzz-2.8.1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/2.8.1_x86_64/harfbuzz-2.8.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: 'b5161044e1976ac2ed7fcc59c7ed1a1539530c6695cb84f1efdd33b1c7b55a97',
-     armv7l: 'b5161044e1976ac2ed7fcc59c7ed1a1539530c6695cb84f1efdd33b1c7b55a97',
-     x86_64: 'ab4f370493d031b21c1fbe58575096eb3616c84d824bbed5c7dfe9e3bdfd05f0'
+    aarch64: 'f39e2aba4445d71c63a6602650590d8c5463a5c093b3ec26933e2963dc354dcc',
+     armv7l: 'f39e2aba4445d71c63a6602650590d8c5463a5c093b3ec26933e2963dc354dcc',
+       i686: '25e7c49b04e81fa61db4385d4033b6b2c633bdfe5ef2a5ab3606c970ad4b307d',
+     x86_64: '801f46cbab0369087004aa3ce7952fc4d7754a85a0cb3c4b0648ce647fc87604'
   })
 
   depends_on 'cairo' => :build
-  depends_on 'chafa' => :build
   depends_on 'glib' => :build
   depends_on 'gobject_introspection'
   depends_on 'ragel' => :build
-  depends_on 'freetype_sub' => :build
+  depends_on 'freetype_sub'
   depends_on 'six' => :build
   depends_on 'graphite' => :build
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
     --default-library=both \
-    -Dfreetype=enabled \
     -Dintrospection=enabled \
     -Dbenchmark=disabled \
     -Dtests=disabled \
