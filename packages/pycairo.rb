66c0270c956893c681ab32b5effb8c7f94876146
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-07-29 10:10:06 -0700

    add dummy packages

---
+++ packages/pycairo.rb
@@ -0,0 +1,12 @@
+require 'package'
+
+class Pycairo < Package
+  description 'Dummy package for py3_pycairo.'
+  homepage 'https://cairographics.org/pycairo/'
+  @_ver = '1'
+  license 'LGPL-2.1 or MPL-1.1'
+  compatibility 'all'
+  is_fake
+
+  depends_on 'py3_pycairo'
+end
