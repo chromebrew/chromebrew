ff39bbcc4cc151f1ac45e0a0662d0c1582c257aa
Satadru Pramanik <satadru@gmail.com>
2021-07-29 16:40:04 -0400

    stack binaries

--- packages/stack.rb
+++ packages/stack.rb
@@ -6,15 +6,18 @@ class Stack < Package
   @_ver = '2.5.1'
   version @_ver
   license 'BSD'
-
   compatibility 'all'
   source_url "https://github.com/commercialhaskell/stack/releases/download/v#{@_ver}/stack-#{@_ver}-linux-x86_64.tar.gz"
   source_sha256 'c83b6c93d6541c0bce2175085a04062020f4160a86116e20f3b343b562f2d1e8'
 
   binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/stack/2.5.1_armv7l/stack-2.5.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/stack/2.5.1_armv7l/stack-2.5.1-chromeos-armv7l.tpxz',
     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/stack/2.5.1_x86_64/stack-2.5.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
+    aarch64: 'c133843b51770fcd93dfa4f8328cccbf06f4523ea9ebc9764d812ff50d527054',
+     armv7l: 'c133843b51770fcd93dfa4f8328cccbf06f4523ea9ebc9764d812ff50d527054',
     x86_64: '23722336f49adc7c685e8ca936928b2f3d53fd4f826ce4603062d9bd1f409b2d'
   })
 
