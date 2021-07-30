9dc5ae342016523ec03b846647bca8807fc4056a
Ed Reel <edreel@gmail.com>
2021-07-30 11:08:43 -0500

    Fix no implicit conversion of Float into String in crew line 525 (#6016)

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.15.0'
+CREW_VERSION = '1.15.1'
 
 ARCH_ACTUAL = `uname -m`.strip
 # This helps with virtualized builds on aarch64 machines
