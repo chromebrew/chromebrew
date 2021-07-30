81a68da73d8983d6ef62fc8266ac36989efa0f6f
K.Noah Howerton <noah@chromakey.io>
2021-07-30 04:46:31 -0700

    adding libCBOR for libfido2  and openssh support for fido and u2f keys

---
+++ packages/libcbor.rb
@@ -0,0 +1,31 @@
+require 'package'
+
+class LibCBOR < Package
+  description 'CBOR Protocol implementation for C and others'
+  homepage 'https://github.com/PJK/libcbor'
+  version '0.8.0'
+  license 'MIT'
+  source_url 'https://github.com/PJK/libcbor/archive/0.8.0.tar.gz'
+  source_sha256 '618097166ea4a54499646998ccaa949a5816e6a665cf1d6df383690895217c8b'
+
+  depends_on 'cmake' => :build
+
+  def self.build
+    Dir.mkdir 'build'
+    Dir.chdir 'build' do
+      system 'cmake',
+             '-DWITH_EXAMPLES=OFF',
+             '-DBUILD_SHARED_LIBS=ON',
+             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
+             '..'
+      system "make"
+    end
+  end
+
+  def self.install
+    Dir.chdir 'build' do
+      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    end
+  end
+
+end
