62ea8d7f51098aab563167aa068ebe5627c0f421
system_user <noah@chromakey.io>
2021-07-31 01:46:44 -0700

    the package (name) and checksum have changed 3 times today!

--- packages/libcbor.rb
+++ packages/libcbor.rb
@@ -6,7 +6,7 @@ class Libcbor < Package
   version '0.8.0'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/PJK/libcbor/archive/0.8.0.tar.gz'
+  source_url 'https://github.com/PJK/libcbor/archive/refs/tags/v0.8.0.tar.gz'
   source_sha256 '618097166ea4a54499646998ccaa949a5816e6a665cf1d6df383690895217c8b'
 
   depends_on 'cmake' => :build
