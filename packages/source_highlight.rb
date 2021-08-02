c9ba1d0d6b4ca424b30baae984f600397ee365a3
satmandu <satadru@umich.edu>
2021-08-02 02:19:11 -0400

    source_highlight rebuild with new boost (#6017)

--- packages/source_highlight.rb
+++ packages/source_highlight.rb
@@ -6,29 +6,28 @@ require 'package'
 class Source_highlight < Package
   description 'Convert source code to syntax highlighted document'
   homepage 'https://www.gnu.org/software/src-highlite/'
-  version '3.1.9-9049'
+  version '3.1.9-9049-1'
   license 'GPL'
   compatibility 'all'
   source_url 'https://git.savannah.gnu.org/git/src-highlite.git'
   git_hashtag '904949c9026cb772dc93fbe0947a252ef47127f4'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049_armv7l/source_highlight-3.1.9-9049-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049_armv7l/source_highlight-3.1.9-9049-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049_i686/source_highlight-3.1.9-9049-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049_x86_64/source_highlight-3.1.9-9049-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049-1_armv7l/source_highlight-3.1.9-9049-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049-1_armv7l/source_highlight-3.1.9-9049-1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049-1_i686/source_highlight-3.1.9-9049-1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/source_highlight/3.1.9-9049-1_x86_64/source_highlight-3.1.9-9049-1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: 'd684918851e16cda44873f6f9d70f9c7c1d65c93689851de6cbc9a6024311986',
-     armv7l: 'd684918851e16cda44873f6f9d70f9c7c1d65c93689851de6cbc9a6024311986',
-       i686: 'a177f2214443d14111a75242e97b1f3b45d2d5dcfc23a5336215a358b6f80a9a',
-     x86_64: '1e0ea17e28e39601fb97771dfa75f13dc9a5b9a201381f0470ce3a7f5d2d12e1'
+    aarch64: '5c5660558f3a902622714f0c5562aa49b4e2c4ebcf54867b25205656c23d3251',
+     armv7l: '5c5660558f3a902622714f0c5562aa49b4e2c4ebcf54867b25205656c23d3251',
+       i686: '1032b43c246c0e8d95392437112d49f48ce2d5d52f0be5d42944459b0067506b',
+     x86_64: '89797072609793b821f66c9069e96bdb8c02db74545968728a681de6c59d16ed'
   })
 
   depends_on 'boost' # R
   depends_on 'ctags' => :build
   depends_on 'texinfo' => :build
-
 
   def self.build
     system 'NOCONFIGURE=1 autoreconf -fiv'
