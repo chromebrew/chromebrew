ed390e64d7b8e75c5e6b1c9dd367ea69e4cae42f
supechicken <supechicken666@gmail.com>
2021-08-03 14:39:37 +0800

    Update crew_url_opener.rb (#5)
    * Update crew_url_opener.rb
    
    * Update crew_url_opener.rb
    
    * Update crew_url_opener.rb

--- packages/crew_url_opener.rb
+++ packages/crew_url_opener.rb
@@ -7,36 +7,13 @@ class Crew_url_opener < Package
   license 'GPL-3'
   compatibility 'all'
   source_url 'SKIP'
-  source_sha256 'SKIP'
-  
-  def self.install
-    @script = <<SCRIPT
+
+  def self.build
+    @_script = <<SCRIPT
 #!/bin/bash
 
-case ${1} in
-https://|http://)
-  exec dbus-send \
-    --system \
-    --type=method_call \
-    --print-reply \
-    --dest=org.chromium.UrlHandlerService \
-    /org/chromium/UrlHandlerService \
-    org.chromium.UrlHandlerServiceInterface.OpenUrl \
-    "string:${1}"
-;;
--h|--help)
-  cat <<EOF
-open-url: Open an URL in Chrome OS browser.
-
-Usage:
-  open-url <option> [URL]
-  (URL must start with 'http://' or 'https://')
-
-  -h --help      Show this message
-EOF
-
-;;
-*)
+case "${1}" in
+ftp://*)
   cat <<EOF
 Error: only support URL start with 'http://' or 'https://'
 
@@ -45,12 +22,51 @@ EOF
 
   exit 1
 ;;
+-h|--help)
+  cat <<EOF
+open-url: Open an URL in Chrome OS browser.
+
+Usage:
+  open-url <option> [URL]
+
+  -h --help      Show this message
+EOF
+
+;;
+*)
+  exec dbus-send \
+    --system \
+    --type=method_call \
+    --print-reply \
+    --dest=org.chromium.UrlHandlerService \
+    /org/chromium/UrlHandlerService \
+    org.chromium.UrlHandlerServiceInterface.OpenUrl \
+    "string:http://${1/http*:\\/\\//}"
+    # use http:// for compatibility
+;;
 esac
 SCRIPT
 
   end
 
+  def self.install
+    @_file = "#{CREW_DEST_PREFIX}/bin/url-opener"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin/"
+    File.write @_file, @_script
+    File.chmod 0755, @_file
+  end
+
   def self.postinstall
+    print 'Set Chrome OS browser as default? [Y/n]: '
+
+    case STDIN.getc
+    when "\n", 'Y', 'y'
+      FileUtils.ln_s 'url-opener', "#{CREW_DEST_PREFIX}/bin/x-www-browser"
+      puts 'Chrome OS browser is now default.'.lightgreen
+    else
+      puts 'No change has been made.'.orange
+    end
+
     puts "Type 'url-opener' for usage"
   end
 end
