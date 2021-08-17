bb30c87a68627b861e47a290b64d614b700fabbd
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2021-08-17 03:15:54 -0700

    libidn2 2.3.1 => 2.3.2 (#6057)
    Add armv7l, x86_64 binaries
    
    Add i686 binary

--- packages/libidn2.rb
+++ packages/libidn2.rb
@@ -3,27 +3,28 @@ require 'package'
 class Libidn2 < Package
   description 'GNU Libidn is a fully documented implementation of the Stringprep, Punycode and IDNA 2003 specifications.'
   homepage 'https://www.gnu.org/software/libidn/'
-  version '2.3.1-1'
+  @_ver = '2.3.2'
+  version @_ver
   license 'GPL-2+ and LGPL-3+'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/libidn/libidn2-2.3.1.tar.gz'
-  source_sha256 '8af684943836b8b53965d5f5b6714ef13c26c91eaa36ce7d242e3d21f5d40f2d'
+  source_url "https://ftpmirror.gnu.org/libidn/libidn2-#{@_ver}.tar.gz"
+  source_sha256 '76940cd4e778e8093579a9d195b25fff5e936e9dc6242068528b437a76764f91'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.1-1_armv7l/libidn2-2.3.1-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.1-1_armv7l/libidn2-2.3.1-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.1-1_i686/libidn2-2.3.1-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.1-1_x86_64/libidn2-2.3.1-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.2_armv7l/libidn2-2.3.2-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.2_armv7l/libidn2-2.3.2-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.2_i686/libidn2-2.3.2-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.2_x86_64/libidn2-2.3.2-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: 'b9c6d94688c48b1eae57d2c62e41c15bb4c7942eae4683bc39699f09865d0ad7',
-     armv7l: 'b9c6d94688c48b1eae57d2c62e41c15bb4c7942eae4683bc39699f09865d0ad7',
-       i686: '985b7de2215322d8b7becb9a7673e26d70db39e497b9a2d3af1f6304f6bb2d15',
-     x86_64: '4fd2287da572b21f87a2d63c5c6c933169fa69735075447d8aa83c244a236c37'
+    aarch64: '6c335d7c611cc1d054c22d2b658aaa060a7bfd682528bac46dc404ffff14c818',
+     armv7l: '6c335d7c611cc1d054c22d2b658aaa060a7bfd682528bac46dc404ffff14c818',
+       i686: '7ce581270eeba81b5f3ec0d7f0e7826b95248a54e960608cc72777beb9f31098',
+     x86_64: '740ae2266e95b4d1974b80165967ec15efd0a7810a649fd7408bdb2964f06de7'
   })
 
   def self.build
-    system "./configure #{CREW_OPTIONS}"
+    system "env #{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
