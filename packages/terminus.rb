3b147a8565403436938f59ba4440a7689962ba5c
Ed Reel <edreel@gmail.com>
2021-08-26 01:17:41 -0500

    Update terminus from 2.5.0 to 2.6.1 (#6106)

--- packages/terminus.rb
+++ packages/terminus.rb
@@ -3,17 +3,17 @@ require 'package'
 class Terminus < Package
   description 'The Pantheon CLI — a standalone utility for performing operations on the Pantheon Platform'
   homepage 'https://github.com/pantheon-systems/terminus'
-  version '2.5.0'
+  version '2.6.1'
   license 'MIT'
   compatibility 'all'
-  source_url "https://raw.githubusercontent.com/pantheon-systems/terminus/#{version}/README.md"
-  source_sha256 '299d011cbd856ba92d22d309efbdb01a3b93bd85eb727512c2bf83eda08dc329'
+  source_url 'SKIP'
 
   depends_on 'php74' unless File.exists? "#{CREW_PREFIX}/bin/php"
 
   def self.install
     system "curl -#LO https://github.com/pantheon-systems/terminus/releases/download/#{version}/terminus.phar"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('terminus.phar') ) == '71da25da6dc1813d9e3ff129964466e9a14687c2e5ea4e52267f02f28fcc3e27'
-    system "install -Dm755 terminus.phar #{CREW_DEST_PREFIX}/bin/terminus"
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('terminus.phar') ) == '97254f4aa4fb8fc8a900a2e22ab449d95d8ca7387c3916e041a1ea946a49d34b'
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.install 'terminus.phar', "#{CREW_DEST_PREFIX}/bin/terminus", mode: 0o755
   end
 end
