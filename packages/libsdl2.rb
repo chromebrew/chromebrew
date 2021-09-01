86a2af4b349da6ca2162184a2a7f4f6b3b12deed
Satadru Pramanik <satadru@gmail.com>
2021-09-01 10:33:09 -0400

    libsdl2 -> 2.0.16

--- packages/libsdl2.rb
+++ packages/libsdl2.rb
@@ -3,24 +3,22 @@ require 'package'
 class Libsdl2 < Package
   description 'Simple DirectMedia Layer is a cross-platform development library designed to provide low level access to audio, keyboard, mouse, joystick, and graphics hardware via OpenGL and Direct3D.'
   homepage 'http://www.libsdl.org'
-  @_ver = '2.0.14'
+  @_ver = '2.0.16'
   version @_ver
   license 'ZLIB'
   compatibility 'all'
   source_url "https://www.libsdl.org/release/SDL2-#{@_ver}.tar.gz"
-  source_sha256 'd8215b571a581be1332d2106f8036fcb03d12a70bae01e20f424976d275432bc'
+  source_sha256 '65be9ff6004034b5b2ce9927b5a4db1814930f169c4b2dae0a1e4697075f287b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.14_armv7l/libsdl2-2.0.14-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.14_armv7l/libsdl2-2.0.14-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.14_i686/libsdl2-2.0.14-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.14_x86_64/libsdl2-2.0.14-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.16_armv7l/libsdl2-2.0.16-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.16_armv7l/libsdl2-2.0.16-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.16_x86_64/libsdl2-2.0.16-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '824f086a8e1fbe7fc90e9d715f402bfe77cf4800574be2bc8404cc3d1b73803f',
-     armv7l: '824f086a8e1fbe7fc90e9d715f402bfe77cf4800574be2bc8404cc3d1b73803f',
-       i686: '7952f95341bbac68414e6d2edacda191061e6dc203e41350e11e116feb313ff9',
-     x86_64: 'bdf479e4bb1f8898bd59ffc09fdffb4861a49294e849bfa49ef207fa38a66061'
+    aarch64: '05cbfc5d3efb7e42009001fea4ade1932f2f273caba7090ab11e68887286ecbd',
+     armv7l: '05cbfc5d3efb7e42009001fea4ade1932f2f273caba7090ab11e68887286ecbd',
+     x86_64: '5f0d468a5fdf3c5ac02eb3e369af10b975d6e66340e3002289e8c9acd237ef33'
   })
 
   depends_on 'xorg_server'
@@ -29,9 +27,11 @@ class Libsdl2 < Package
   depends_on 'nasm' => :build
 
   def self.build
+    system 'filefix'
     system "env #{CREW_ENV_OPTIONS} \
       ./configure \
       #{CREW_OPTIONS}"
+    system 'make'
   end
 
   def self.install
