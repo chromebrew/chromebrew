9a5f6a52ab95415836ec1f724034af6b881337aa
Satadru Pramanik <satadru@umich.edu>
2021-09-10 19:04:07 -0400

    crew: improve 'Attempting to compress' output (#6152)
    * crew: improve 'Attempting to compress' output
    
    * bump version

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.16.5'
+CREW_VERSION = '1.16.6'
 
 ARCH_ACTUAL = `uname -m`.strip
 # This helps with virtualized builds on aarch64 machines
