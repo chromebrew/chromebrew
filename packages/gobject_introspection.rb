c2175b41cf17195e660db86d1e07818a30606788
Ed Reel <edreel@gmail.com>
2021-09-10 07:35:32 +0000

    Add i686 binary

--- packages/gobject_introspection.rb
+++ packages/gobject_introspection.rb
@@ -13,11 +13,13 @@ class Gobject_introspection < Package
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.69.0_armv7l/gobject_introspection-1.69.0-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.69.0_armv7l/gobject_introspection-1.69.0-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.69.0_i686/gobject_introspection-1.69.0-chromeos-i686.tar.xz',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.69.0_x86_64/gobject_introspection-1.69.0-chromeos-x86_64.tpxz'
   })
   binary_sha256({
     aarch64: 'f07dcf8fa48c6fcd1bc08cf03dad0ee605c2dd78c939221971ae2e8b8950f7f3',
      armv7l: 'f07dcf8fa48c6fcd1bc08cf03dad0ee605c2dd78c939221971ae2e8b8950f7f3',
+       i686: '7f7e4d8121131dbe3ee06006dce70e24fa84198aaea0d13d7f3f4e9d65af1af4',
      x86_64: '2c3a734c15234a2d9fc7891b80ab65e6d11e7cca4275256b173a1d85fecc5c8c'
   })
 
