bfcaf68ae73ddf6979d1c94dcecec5cd40e8e131
Ed Reel <edreel@gmail.com>
2021-09-11 15:54:12 -0500

    Add pre-built binaries

--- packages/wget.rb
+++ packages/wget.rb
@@ -9,6 +9,19 @@ class Wget < Package
   source_url 'https://ftpmirror.gnu.org/gnu/wget/wget-1.21.2.tar.gz'
   source_sha256 'e6d4c76be82c676dd7e8c61a29b2ac8510ae108a810b5d1d18fc9a1d2c9a2497'
 
+  binary_url ({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wget/1.21.2_armv7l/wget-1.21.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wget/1.21.2_armv7l/wget-1.21.2-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wget/1.21.2_i686/wget-1.21.2-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wget/1.21.2_x86_64/wget-1.21.2-chromeos-x86_64.tar.xz',
+  })
+  binary_sha256 ({
+    aarch64: 'c2179f28acacdff15012005c0949712366045e1a40dc0682f8aa5b1758cffef6',
+     armv7l: 'c2179f28acacdff15012005c0949712366045e1a40dc0682f8aa5b1758cffef6',
+       i686: 'a80b25da86146cd54a35c534705e4dc2ccd5839f17e5c85f5ab3626018091dee',
+     x86_64: '792bbb12e594477ab1857749dab3f878edaa736307b100c235b987ce25bc144c',
+  })
+
   depends_on 'gnutls'
   depends_on 'libpsl'
   depends_on 'libxdmcp'
