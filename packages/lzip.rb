5211c3bed589e80bed80e704f7d9c110039466c3
supechicken <supechicken666@gmail.com>
2021-09-11 14:03:32 +0800

    lzip => 1.22

--- packages/lzip.rb
+++ packages/lzip.rb
@@ -3,36 +3,23 @@ require 'package'
 class Lzip < Package
   description 'Lzip is a lossless data compressor with a user interface similar to the one of gzip or bzip2.'
   homepage 'https://www.nongnu.org/lzip/lzip.html'
-  version '1.21'
+  version '1.22'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://download.savannah.gnu.org/releases/lzip/lzip-1.21.tar.lz'
-  source_sha256 '68c703c7b5198b3fd7a0c3f20011e9a8c938b8dec14824c4c873922fdb01719f'
-
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lzip/1.21_armv7l/lzip-1.21-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lzip/1.21_armv7l/lzip-1.21-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lzip/1.21_i686/lzip-1.21-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lzip/1.21_x86_64/lzip-1.21-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '09f94e775a908f77177ee4657940e62e8a223b50210d66b581b4ee823416cac9',
-     armv7l: '09f94e775a908f77177ee4657940e62e8a223b50210d66b581b4ee823416cac9',
-       i686: '6ec82131cdda0a15c8b78bc5602fdfa84b52336d76b4fd1c70d710e99b9ed3cc',
-     x86_64: '3e79f5606033b8dde418900d6957d06b606a9bffb240eb67e7166e38dcdfe699',
-  })
+  source_url 'https://download.savannah.gnu.org/releases/lzip/lzip-1.22.tar.gz'
+  source_sha256 'c3342d42e67139c165b8b128d033b5c96893a13ac5f25933190315214e87a948'
 
   def self.build
-    system "./configure",
-           "--prefix=#{CREW_PREFIX}"
-    system "make"
+    # configure: WARNING: unrecognized option: '--libdir=/usr/local/lib64' 
+    system "#{CREW_ENV_OPTIONS} ./configure --prefix=#{CREW_PREFIX}"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
-    system "make", "check"
+    system 'make', 'check'
   end
 end
