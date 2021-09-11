e70257371462626dec8569702b5f5afa19dfa11b
supechicken <supechicken666@gmail.com>
2021-09-11 14:10:21 +0800

    m4 => 1.4.19

--- packages/m4.rb
+++ packages/m4.rb
@@ -3,37 +3,24 @@ require 'package'
 class M4 < Package
   description 'GNU M4 is an implementation of the traditional Unix macro processor.'
   homepage 'https://www.gnu.org/software/m4/'
-  version '1.4.18'
+  version '1.4.19'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/m4/m4-1.4.18.tar.xz'
-  source_sha256 'f2c1e86ca0a404ff281631bdc8377638992744b175afb806e25871a24a934e07'
-
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/m4/1.4.18_armv7l/m4-1.4.18-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/m4/1.4.18_armv7l/m4-1.4.18-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/m4/1.4.18_i686/m4-1.4.18-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/m4/1.4.18_x86_64/m4-1.4.18-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '57b2c4f5bc8ef5cc680e97e878d79382f3cfd2b1e54679cae269bf968cd72316',
-     armv7l: '57b2c4f5bc8ef5cc680e97e878d79382f3cfd2b1e54679cae269bf968cd72316',
-       i686: 'd67fafe955ee4cdabad4b17df7a402409fc3a8fffe27671d350073ac5312688e',
-     x86_64: 'd364c72f7cf138bf120aa18a5a0b1e4500b50f79bc927f435c468cbd4bf4cc9b',
-  })
+  source_url 'https://ftpmirror.gnu.org/gnu/m4/m4-1.4.19.tar.xz'
+  source_sha256 '63aede5c6d33b6d9b13511cd0be2cac046f2e70fd0a07aa9573a04a82783af96'
 
   depends_on 'libsigsegv'
 
   def self.build
-    system "./configure"
-    system "make"
+    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
-    system "make check"
+    system 'make', 'check'
   end
 end
