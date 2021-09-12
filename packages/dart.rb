a1612cb8869fee70ecd4b316fd835311cf903d60
Ed Reel <edreel@gmail.com>
2021-09-12 13:12:21 -0500

    Update dart from 2.13.4 to 2.14.1

--- packages/dart.rb
+++ packages/dart.rb
@@ -3,20 +3,20 @@ require 'package'
 class Dart < Package
   description 'The Dart SDK is a set of tools and libraries for the Dart programming language.  You can find information about Dart online at dartlang.org.'
   homepage 'https://dart.dev/'
-  @_ver = '2.13.4'
+  @_ver = '2.14.1'
   version @_ver
   license 'BSD-3'
   compatibility 'all'
   case ARCH
   when 'aarch64', 'armv7l'
     source_url "https://storage.googleapis.com/dart-archive/channels/stable/release/#{@_ver}/sdk/dartsdk-linux-arm-release.zip"
-    source_sha256 'f7733f30e44345237a817bf9104fee1e20820a5796162770b964adcaf705711d'
+    source_sha256 'ce209720ff628c737067be8e55c86a3cf0257931107ad46142680c1d59dfb4b6'
   when 'i686'
     source_url "https://storage.googleapis.com/dart-archive/channels/stable/release/#{@_ver}/sdk/dartsdk-linux-ia32-release.zip"
-    source_sha256 '3116ac10532ec954d0dd31b99cb562279109909ba818dbe081b1c2059a8f50b8'
+    source_sha256 '132d57b773fb81a2376490d8d3885e96d1ac18b87a4adadaf80eddcadf4ef22b'
   when 'x86_64'
     source_url "https://storage.googleapis.com/dart-archive/channels/stable/release/#{@_ver}/sdk/dartsdk-linux-x64-release.zip"
-    source_sha256 '633a9aa4812b725ff587e2bbf16cd5839224cfe05dcd536e1a74804e80fdb4cd'
+    source_sha256 'a615aec227431a6b15dd77c63904e505ba6b2882a48d4efad8fb20efe868a740'
   end
 
   def self.install
@@ -24,7 +24,7 @@ class Dart < Package
     FileUtils.mkdir_p CREW_DEST_LIB_PREFIX
     FileUtils.cp_r 'bin/', CREW_DEST_PREFIX
     FileUtils.cp_r 'include/', CREW_DEST_PREFIX
-    FileUtils.cp_r Dir.glob('lib/*'), CREW_DEST_LIB_PREFIX
+    FileUtils.cp_r Dir['lib/*'], CREW_DEST_LIB_PREFIX
     FileUtils.cp 'version', CREW_DEST_PREFIX # This stops 'pub get' from throwing errors
   end
 end
