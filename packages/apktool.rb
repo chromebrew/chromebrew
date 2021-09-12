dd632f02998df06f11423b460926bf7ee37f0a28
Ed Reel <edreel@gmail.com>
2021-09-12 11:49:49 -0500

    Update apktool from 2.5.0 to 2.6.0 (#6164)

--- packages/apktool.rb
+++ packages/apktool.rb
@@ -3,7 +3,7 @@ require 'package'
 class Apktool < Package
   description 'A tool for reverse engineering Android apk files'
   homepage 'https://ibotpeaches.github.io/Apktool/'
-  version '2.5.0'
+  version '2.6.0'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
@@ -12,9 +12,9 @@ class Apktool < Package
 
   def self.install
     system "curl -L#O https://raw.githubusercontent.com/iBotPeaches/Apktool/v#{version}/scripts/linux/apktool"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('apktool') ) == 'e9a49351b4df65d532e7c3cef27144c7e0e1c2daad1e367e65b2acef6715b863'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('apktool') ) == '84797978cbaf5f6cfee926ca55a5db710448b43a3e957567bd3e24feab41201e'
     system "curl -L#O https://github.com/iBotPeaches/Apktool/releases/download/v#{version}/apktool_#{version}.jar"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("apktool_#{version}.jar") ) == 'b392d7cb99b592e9c5acc3c06f1b0f180edde96c66b86b3d6932b7c0c4079fe4'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read("apktool_#{version}.jar") ) == 'f750a3cd2c1f942f27f5f7fd5d17eada3bdaff0a6643f49db847e842579fdda5'
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install "apktool_#{version}.jar", "#{CREW_DEST_PREFIX}/bin/apktool.jar", mode: 0o644
     FileUtils.install 'apktool', "#{CREW_DEST_PREFIX}/bin/apktool", mode: 0o755
