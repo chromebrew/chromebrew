8d1e3584b2f83b33013ba4b742e81d2deb426675
Ed Reel <edreel@gmail.com>
2021-09-13 01:32:53 -0500

    Update freebasic from 1.07.1-1 to 1.08.1 (#6174)
    Update libtinfo from 6.2-1 to 5.9
    
    Add pre-built binaries for libtinfo
    
    Add postinstall section to freebasic

--- packages/freebasic.rb
+++ packages/freebasic.rb
@@ -3,16 +3,19 @@ require 'package'
 class Freebasic < Package
   description 'FreeBASIC is a free and open source, BASIC compiler for Microsoft Windows, DOS and Linux.'
   homepage 'https://www.freebasic.net/'
-  version '1.07.1-1'
+  version '1.08.1'
   license 'LGPL-2 and GPL-2'
-  compatibility 'i686,x86_64'
+  compatibility 'all'
   case ARCH
+  when 'aarch64', 'armv7l'
+    source_url 'https://downloads.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/FreeBASIC-1.08.1-linux-arm.tar.xz'
+    source_sha256 'e321b8bec63446c9891db4df2d2e4d08552f3ca030def416a208f851367bee8c'
   when 'i686'
-    source_url 'https://downloads.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.07.1-linux-x86.tar.gz' # 404 not found
-    source_sha256 'a5b8f31e38a214d63a3d8e0b1ac6608f6a304891e40a3c0755940cba2f8401f1'
+    source_url 'https://downloads.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/FreeBASIC-1.08.1-linux-x86.tar.xz'
+    source_sha256 '8f11af664858e29d0798e19aa895c0439f14a9660eba04c7fe4c872c98132524'
   when 'x86_64'
-    source_url 'https://downloads.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC-1.07.1-linux-x86_64.tar.gz' # 404 not found
-    source_sha256 'd5034e81201760de7fd7bc563d8c33213a9530da3d15d4907e52d47278a00243'
+    source_url 'https://downloads.sourceforge.net/project/fbc/FreeBASIC-1.08.1/Binaries-Linux/FreeBASIC-1.08.1-linux-x86_64.tar.xz'
+    source_sha256 'f32213b2efb0be2799995cab0de429ce862ae2f9b36af4e236948985c20583c7'
   end
 
   depends_on 'libtinfo'
@@ -21,5 +24,9 @@ class Freebasic < Package
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
     system "./install.sh -i #{CREW_DEST_PREFIX}"
     FileUtils.mv "#{CREW_DEST_PREFIX}/man", "#{CREW_DEST_PREFIX}/share"
+  end
+
+  def self.postinstall
+    puts "\nType 'fbc' to get started.\n".lightblue
   end
 end
