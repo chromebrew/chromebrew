c4e6118deedc3d0013ef2970f5aec12ad52abbe7
Ed Reel <edreel@gmail.com>
2021-09-13 01:36:00 -0500

    Update brave from 1.29.77 to 1.29.79 (#6168)

--- packages/brave.rb
+++ packages/brave.rb
@@ -3,11 +3,11 @@ require 'package'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '1.29.77'
+  version '1.29.79'
   license 'MPL-2'
   compatibility 'x86_64'
   source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-#{version}-linux-amd64.zip"
-  source_sha256 '4e8cdeb839659d5cca3ddfd4811ec51539e018ae3510693502cedab22b663459'
+  source_sha256 'ae79857fae7fa91edc91efa61d3ffd98f183c6a772ba2654f8faabcd8aa3ad59'
 
   depends_on 'gtk3'
   depends_on 'libcom_err'
