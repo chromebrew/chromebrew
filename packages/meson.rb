41b998149f80febbff8e1a772654495975c1e639
Satadru Pramanik <satadru@umich.edu>
2021-09-14 00:30:03 -0400

    Meson -> 0.59.1 (#6185)
    * Meson -> 0.59.1
    
    * Add i686 binary
    
    Co-authored-by: Ed Reel <edreel@gmail.com>

--- packages/meson.rb
+++ packages/meson.rb
@@ -3,7 +3,7 @@ require 'package'
 class Meson < Package
   description 'Meson is an open source build system meant to be both extremely fast and user friendly.'
   homepage 'https://mesonbuild.com/'
-  @_ver = '0.59.0'
+  @_ver = '0.59.1'
   version @_ver
   license 'Apache-2.0'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Meson < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.0_armv7l/meson-0.59.0-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.0_armv7l/meson-0.59.0-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.0_i686/meson-0.59.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.0_x86_64/meson-0.59.0-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.1_armv7l/meson-0.59.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.1_armv7l/meson-0.59.1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.1_i686/meson-0.59.1-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.59.1_x86_64/meson-0.59.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '4cdb34e921d39f0a7439543a241941d869fc4c7390e2a5c33d0cd6485996bbc6',
-     armv7l: '4cdb34e921d39f0a7439543a241941d869fc4c7390e2a5c33d0cd6485996bbc6',
-       i686: 'd91ab13dcce6d3f112a9275a160baea1ec5663fb215fe717d6543fa723b69d90',
-     x86_64: '9358e0c8dc83a744fc2ceb24119d918dc5007334d9670cdf523e15e4b083d29d'
+    aarch64: 'b476fa2a9fb9b7936b2677fe54497d6c8b9f05447303ea4c3ba93fcf58d1914b',
+     armv7l: 'b476fa2a9fb9b7936b2677fe54497d6c8b9f05447303ea4c3ba93fcf58d1914b',
+       i686: '8851b08384d500adc273b10ece731f3e93ac11199734d2d26fca32b0a2fc2337',
+     x86_64: 'ae500b01a538d74935049b5701d5efdfb99aa08628147b7228b0359a5a592d79'
   })
 
   depends_on 'ninja'
