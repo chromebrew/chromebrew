84a91c2934f5e18f9a3914dfe6d619759b7e5029
Ed Reel <edreel@gmail.com>
2021-09-14 05:22:19 +0000

    Add i686 binary

--- packages/autoconf213.rb
+++ packages/autoconf213.rb
@@ -12,11 +12,13 @@ class Autoconf213 < Package
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf213/2.13-1_armv7l/autoconf213-2.13-1-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf213/2.13-1_armv7l/autoconf213-2.13-1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf213/2.13-1_i686/autoconf213-2.13-1-chromeos-i686.tar.xz',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf213/2.13-1_x86_64/autoconf213-2.13-1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
     aarch64: 'fe089c6ec2d91a455bc5cd38bd73a653e0b80728bf873d0589ed57f0c7ab04be',
      armv7l: 'fe089c6ec2d91a455bc5cd38bd73a653e0b80728bf873d0589ed57f0c7ab04be',
+       i686: '1b6ba4eac9c9547458438cb4924a4640300221ed30bcad9c3765a55d35e7a744',
      x86_64: '98f744a5a7ae84583997e57ec03dcc6ce1f962b6316bd6d4c54a6c3ebaad5a36'
   })
 
