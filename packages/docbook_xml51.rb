2c7d9e10eebcf5aada68886035bcf3da8fda667f
supechicken <supechicken666@gmail.com>
2021-09-16 00:11:52 +0800

    Update docbook_xml51.rb

--- packages/docbook_xml51.rb
+++ packages/docbook_xml51.rb
@@ -4,7 +4,7 @@ class Docbook_xml51 < Package
   description 'A widely used XML scheme for writing documentation and help'
   homepage 'https://www.oasis-open.org/docbook/'
   @_ver = '5.1'
-  version "#{@_ver}-2"
+  version "#{@_ver}-3"
   license 'MIT'
   compatibility 'all'
   source_url "https://docbook.org/xml/#{@_ver}/docbook-v#{@_ver}-os.zip"
@@ -25,45 +25,30 @@ class Docbook_xml51 < Package
 
   depends_on 'docbook_xml'
   depends_on 'xmlcatmgr'
-  depends_on 'bash'
 
   def self.install
     system "xmlcatalog --noout --create docbook-#{@_ver}.xml"
-    # RNG
-    system "xmlcatalog --noout --add 'uri' \
-      'http://www.oasis-open.org/docbook/xml/#{@_ver}/rng/docbook.rng' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rng' docbook-#{@_ver}.xml"
-    system "xmlcatalog --noout --add 'uri' \
-      'http://docbook.org/xml/#{@_ver}/rng/docbook.rng' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rng' docbook-#{@_ver}.xml"
-    # RNG+XInclude
-    system "xmlcatalog --noout --add 'uri' \
-      'http://www.oasis-open.org/docbook/xml/#{@_ver}/rng/docbookxi.rng' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rng' docbook-#{@_ver}.xml"
-    system "xmlcatalog --noout --add 'uri' \
-      'http://docbook.org/xml/#{@_ver}/rng/docbookxi.rng' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rng' docbook-#{@_ver}.xml"
-    # RNC
-    system "xmlcatalog --noout --add 'uri' \
-      'http://www.oasis-open.org/docbook/xml/#{@_ver}/rnc/docbook.rnc' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rnc' docbook-#{@_ver}.xml"
-    system "xmlcatalog --noout --add 'uri' \
-      'http://docbook.org/xml/#{@_ver}/rng/docbook.rnc' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rnc' docbook-#{@_ver}.xml"
-    # RNC+XInclude
-    system "xmlcatalog --noout --add 'uri' \
-      'http://www.oasis-open.org/docbook/xml/#{@_ver}/rnc/docbookxi.rnc' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rnc' docbook-#{@_ver}.xml"
-    system "xmlcatalog --noout --add 'uri' \
-      'http://docbook.org/xml/#{@_ver}/rng/docbookxi.rnc' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rnc' docbook-#{@_ver}.xml"
-    # Schematron
-    system "xmlcatalog --noout --add 'uri' \
-      'http://www.oasis-open.org/docbook/xml/#{@_ver}/sch/docbook.sch' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/sch/#{@_ver}/docbook.sch' docbook-#{@_ver}.xml"
-    system "xmlcatalog --noout --add 'uri' \
-      'http://docbook.org/xml/#{@_ver}/sch/docbook.sch' \
-      'file://#{CREW_PREFIX}/share/xml/docbook/schema/sch/#{@_ver}/docbook.sch' docbook-#{@_ver}.xml"
+    
+    { 
+      # RNG
+      "http://www.oasis-open.org/docbook/xml/#{@_ver}/rng/docbook.rng" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rng",
+      "http://docbook.org/xml/#{@_ver}/rng/docbook.rng" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rng",
+      # RNG+XInclude
+      "http://www.oasis-open.org/docbook/xml/#{@_ver}/rng/docbookxi.rng" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rng",
+      "http://docbook.org/xml/#{@_ver}/rng/docbookxi.rng" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rng",
+      # RNC
+      "http://www.oasis-open.org/docbook/xml/#{@_ver}/rnc/docbook.rnc" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rnc",
+      "http://docbook.org/xml/#{@_ver}/rng/docbook.rnc" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbook.rnc",
+      # RNC+XInclude
+      "http://www.oasis-open.org/docbook/xml/#{@_ver}/rnc/docbookxi.rnc" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rnc",
+      "http://docbook.org/xml/#{@_ver}/rng/docbookxi.rnc" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/rng/#{@_ver}/docbookxi.rnc",
+      # Schematron
+      "http://www.oasis-open.org/docbook/xml/#{@_ver}/sch/docbook.sch" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/sch/#{@_ver}/docbook.sch",
+      "http://docbook.org/xml/#{@_ver}/sch/docbook.sch" => "file://#{CREW_PREFIX}/share/xml/docbook/schema/sch/#{@_ver}/docbook.sch"
+    }.each_pair do |k, v|
+      system 'xmlcatalog', '--noout', '--add', 'uri', k, v, "docbook-#{@_ver}.xml"
+    end
+
     # Build XML catalog files for each Schema
     @ADDFILES_SH = <<~ADDFILES_HEREDOC
       for s in schemas/rng schemas/sch; do
@@ -108,20 +93,21 @@ class Docbook_xml51 < Package
       done
     ADDFILES_HEREDOC
     IO.write('add_files.sh', @ADDFILES_SH, perm: 0o755)
-    system "#{CREW_PREFIX}/bin/bash -x ./add_files.sh | true"
+    system "bash -x ./add_files.sh | true"
 
-    system "#{CREW_PREFIX}/bin/bash -c \"for type in rng sch ; do
-      mkdir -p #{CREW_DEST_PREFIX}/share/xml/docbook/schema/\\\${type}/#{@_ver}
-      install -m644 schemas/\\\${type}/* #{CREW_DEST_PREFIX}/share/xml/docbook/schema/\\\${type}/#{@_ver}
-    done\""
+    ['rng', 'sch'].each do |type|
+      FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/xml/docbook/schema/#{type}/#{@_ver}/"
+      FileUtils.cp Dir["schemas/#{type}/*"], "#{CREW_DEST_PREFIX}/share/xml/docbook/schema/#{type}/#{@_ver}/"
+    end
+
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    system "install -m755 tools/db4-entities.pl #{CREW_DEST_PREFIX}/bin"
+    FileUtils.install mode: 0o755, 'tools/db4-entities.pl', "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/xml/docbook/stylesheet/docbook5"
-    system "install -m644 tools/db4-upgrade.xsl #{CREW_DEST_PREFIX}/share/xml/docbook/stylesheet/docbook5/"
+    FileUtils.install 'tools/db4-upgrade.xsl', "#{CREW_DEST_PREFIX}/share/xml/docbook/stylesheet/docbook5/"
 
     # catalog configuration
-    system "mkdir -p #{CREW_DEST_PREFIX}/etc/xml"
-    system "install -m644 docbook-#{@_ver}.xml  #{CREW_DEST_PREFIX}/etc/xml/docbook-#{@_ver}.xml"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/xml"
+    FileUtils.install mode: 0o644, "docbook-#{@_ver}.xml", "#{CREW_DEST_PREFIX}/etc/xml/docbook-#{@_ver}.xml"
   end
 
   def self.preinstall
@@ -147,21 +133,12 @@ class Docbook_xml51 < Package
   end
 
   def self.postinstall
-    system "xmlcatalog --noout --add \"delegatePublic\" \
-        \"-//OASIS//ENTITIES DocBook XML\" \
-        \"file://#{CREW_PREFIX}/etc/xml/docbook-xml\" \
-        '#{CREW_PREFIX}/etc/xml/catalog'"
-    system "xmlcatalog --noout --add \"delegatePublic\" \
-        \"-//OASIS//DTD DocBook XML\" \
-        \"file://#{CREW_PREFIX}/etc/xml/docbook-xml\" \
-        '#{CREW_PREFIX}/etc/xml/catalog'"
-    system "xmlcatalog --noout --add \"delegateSystem\" \
-        \"http://www.oasis-open.org/docbook/\" \
-        \"file://#{CREW_PREFIX}/etc/xml/docbook-xml\" \
-        '#{CREW_PREFIX}/etc/xml/catalog'"
-    system "xmlcatalog --noout --add \"delegateURI\" \
-        \"http://www.oasis-open.org/docbook/\" \
-        \"file://#{CREW_PREFIX}/etc/xml/docbook-xml\" \
-        '#{CREW_PREFIX}/etc/xml/catalog'"
+    { 'delegatePublic' => '-//OASIS//ENTITIES DocBook XML',
+      'delegatePublic' => '-//OASIS//ENTITIES DocBook XML',
+      'delegateSystem' => 'http://www.oasis-open.org/docbook/',
+      'delegateURI' => 'http://www.oasis-open.org/docbook/'
+    }.each_pair do |k, v|
+      system 'xmlcatalog', '--noout', '--add', k, v, "file://#{CREW_PREFIX}/etc/xml/docbook-xml", "#{CREW_PREFIX}/etc/xml/catalog"
+    end
   end
 end
