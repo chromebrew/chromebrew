b98cda6d8ac7d2e93869ef0dcbe609fdcd3a5c74
Satadru Pramanik <satadru@umich.edu>
2021-09-24 10:38:31 -0400

    elfutils update and binutils rebuild (#6227)
    * elfutils update and binutils rebuild
    
    * rebuild armv7l, with upx disabled for install

--- packages/binutils.rb
+++ packages/binutils.rb
@@ -4,23 +4,23 @@ class Binutils < Package
   description 'The GNU Binutils are a collection of binary tools.'
   homepage 'https://www.gnu.org/software/binutils/'
   @_ver = '2.37'
-  version @_ver
+  version "#{@_ver}-1"
   license 'GPL-3+'
   compatibility 'all'
   source_url "https://ftpmirror.gnu.org/binutils/binutils-#{@_ver}.tar.xz"
   source_sha256 '820d9724f020a3e69cb337893a0b63c2db161dadcb0e06fc11dc29eb1e84a32c'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37_armv7l/binutils-2.37-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37_armv7l/binutils-2.37-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37_i686/binutils-2.37-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37_x86_64/binutils-2.37-chromeos-x86_64.tpxz'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37-1_i686/binutils-2.37-1-chromeos-i686.tar.xz',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37-1_armv7l/binutils-2.37-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37-1_armv7l/binutils-2.37-1-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/binutils/2.37-1_x86_64/binutils-2.37-1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '0acc9160cb2b55fbbd289c25da3b8f0684e02efb6ab16b7aaff2f0fcb6fb8cef',
-     armv7l: '0acc9160cb2b55fbbd289c25da3b8f0684e02efb6ab16b7aaff2f0fcb6fb8cef',
-       i686: '9735a35796db89f36df9ff4cd615e0d030aec2be566b8e3ab4ec8f011a89e1eb',
-     x86_64: '6648a5f4b57cc1b93166920d48ff9ff2318e9eb834984d8511eaa2a4d992da83'
+       i686: '9bd786abf5e42b06e3ecd51ffdd63348fbf54f18fd6aeec8aafcebc20f231404',
+    aarch64: '2b0dccac7104572bd7e050ba191fc5eb355019351cba9d07293bb53450b4ff84',
+     armv7l: '2b0dccac7104572bd7e050ba191fc5eb355019351cba9d07293bb53450b4ff84',
+     x86_64: 'd9e0347631803d94648495ba9f2b16836f6f637046ac4c757a725c47f57a2c7a'
   })
 
   def self.prebuild
@@ -38,8 +38,7 @@ class Binutils < Package
   def self.build
     Dir.mkdir 'build'
     Dir.chdir 'build' do
-      system "#{CREW_ENV_OPTIONS} \
-        ../configure #{CREW_OPTIONS} \
+      system "../configure #{CREW_OPTIONS} \
         --disable-bootstrap \
         --disable-maintainer-mode \
         --enable-64-bit-bfd \
@@ -67,6 +66,13 @@ class Binutils < Package
   end
 
   def self.install
+    if ARCH == 'armv7l'
+      ENV['CREW_SHRINK_ARCHIVE'] = '0'
+      warn_level = $VERBOSE
+      $VERBOSE = nil
+      load "#{CREW_LIB_PATH}lib/const.rb"
+      $VERBOSE = warn_level
+    end
     Dir.chdir 'build' do
       system 'make', "DESTDIR=#{CREW_DEST_DIR}", "prefix=#{CREW_PREFIX}",
              "tooldir=#{CREW_PREFIX}", 'install'
