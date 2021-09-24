e3b52a19d2a68faad29c64d31f6828f1f08a5b9c
Satadru Pramanik <satadru@umich.edu>
2021-09-24 03:27:39 -0400

    Gnome 41 updates (#6233)

--- packages/libadwaita.rb
+++ packages/libadwaita.rb
@@ -3,23 +3,23 @@ require 'package'
 class Libadwaita < Package
   description 'Library of GNOME-specific UI patterns, replacing libhandy for GTK4'
   homepage 'https://gitlab.gnome.org/GNOME/libadwaita/'
-  version '1.1.0-aab6'
+  version '1.0.0-alpha.2'
   license 'LGPL-2.1+'
   compatibility 'all'
-  source_url 'https://gitlab.gnome.org/GNOME/libadwaita/-/archive/aab6c89993dbf9231e0aeec9d31ce35c52ea04a2/libadwaita-aab6c89993dbf9231e0aeec9d31ce35c52ea04a2.tar.bz2'
-  source_sha256 'af4e34b811c18f2e42f76764c33f835b63110f8a1d471156befcd500d062daab'
+  source_url 'https://gitlab.gnome.org/GNOME/libadwaita.git'
+  git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.1.0-aab6_armv7l/libadwaita-1.1.0-aab6-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.1.0-aab6_armv7l/libadwaita-1.1.0-aab6-chromeos-armv7l.tar.xz',
        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.1.0-aab6_i686/libadwaita-1.1.0-aab6-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.1.0-aab6_x86_64/libadwaita-1.1.0-aab6-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.0.0-alpha.2_armv7l/libadwaita-1.0.0-alpha.2-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.0.0-alpha.2_armv7l/libadwaita-1.0.0-alpha.2-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.0.0-alpha.2_x86_64/libadwaita-1.0.0-alpha.2-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '50e69b5c70875b5d938f9d578fd786eed3ce1de4ca4ab0d788eac24dcf63788c',
-     armv7l: '50e69b5c70875b5d938f9d578fd786eed3ce1de4ca4ab0d788eac24dcf63788c',
        i686: 'd68dd1fb68393dbeed3f7537a81d5812cc3cdff659dc74dfd07592a1cacad297',
-     x86_64: 'c6e0ebffb33a5c57fb9dfcd94e66e9c4c2a7c354427405ab424c9f205a97e4aa'
+    aarch64: '64fa0c4373eb849fbb139e8fd1fcd2f9f6b64f4ccf373ef17c58a0130a77ae54',
+     armv7l: '64fa0c4373eb849fbb139e8fd1fcd2f9f6b64f4ccf373ef17c58a0130a77ae54',
+     x86_64: '39da23ece24e4872fc0519d499e9f6306d6dbb6e5de11a7220f18e8f9b7c95e3'
   })
 
   depends_on 'cairo'
@@ -31,10 +31,6 @@ class Libadwaita < Package
   depends_on 'libjpeg'
   depends_on 'pango'
   depends_on 'vala' => :build
-
-  def self.patch
-    system "sed -i 's/-fstack-protector-strong/-flto/g' meson.build"
-  end
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
