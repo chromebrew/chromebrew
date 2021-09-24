c19545d15502290e9b17dffb1ad14b0101b09c45
Satadru Pramanik <satadru@umich.edu>
2021-09-24 03:27:59 -0400

    gedit dependency updates for updated libicu (#6231)
    * gspell rebuild
    
    * icu compat updates: tepl_5, tepl_6

--- packages/gspell.rb
+++ packages/gspell.rb
@@ -3,23 +3,21 @@ require 'package'
 class Gspell < Package
   description 'a flexible API to implement the spell checking in a GTK+ application'
   homepage 'https://wiki.gnome.org/Projects/gspell'
-  version '1.9.1-1'
+  version '1.9.1-2'
   license 'LGPL-2.1+'
   compatibility 'all'
   source_url 'https://download.gnome.org/sources/gspell/1.9/gspell-1.9.1.tar.xz'
   source_sha256 'dcbb769dfdde8e3c0a8ed3102ce7e661abbf7ddf85df08b29915e92cd723abdd'
 
-  binary_url ({
-     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gspell/1.9.1-1_armv7l/gspell-1.9.1-1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gspell/1.9.1-1_armv7l/gspell-1.9.1-1-chromeos-armv7l.tar.xz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gspell/1.9.1-1_i686/gspell-1.9.1-1-chromeos-i686.tar.xz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gspell/1.9.1-1_x86_64/gspell-1.9.1-1-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gspell/1.9.1-2_armv7l/gspell-1.9.1-2-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gspell/1.9.1-2_armv7l/gspell-1.9.1-2-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gspell/1.9.1-2_x86_64/gspell-1.9.1-2-chromeos-x86_64.tpxz'
   })
-  binary_sha256 ({
-     aarch64: '26c16ee849e3c09cd67f6dd951fd917959d362d195d9e52d3efca4e1b6875328',
-      armv7l: '26c16ee849e3c09cd67f6dd951fd917959d362d195d9e52d3efca4e1b6875328',
-        i686: '6c1dc99a8c02e8e507950149823969c89100c96ca692603d534d4ed5b0a5f7b5',
-      x86_64: 'ed611db95e4986659250615fce945abd25aadb908f819de371182525673b99e2',
+  binary_sha256({
+    aarch64: 'b3d710b9a07b831b1d7b771bd563367e5ebf40adb880238824c4a906ba3118bb',
+     armv7l: 'b3d710b9a07b831b1d7b771bd563367e5ebf40adb880238824c4a906ba3118bb',
+     x86_64: 'e5f3df2f932455279ab3cd4ac3828684644524e39e4a53c6b54de62c30e2ad2b'
   })
 
   depends_on 'gtk3'
@@ -37,16 +35,15 @@ class Gspell < Package
   ENV['XML_CATALOG_FILES'] = "#{CREW_PREFIX}/etc/xml/catalog"
 
   def self.patch
-    # Fixes ./configure: /usr/bin/file: No such file or directory
     system 'filefix'
   end
 
   def self.build
-    system "CFLAGS=-fuse-ld=lld \
-    CXXFLAGS=-fuse-ld=lld \
-    ./configure  #{CREW_OPTIONS} --enable-gtk-doc-html=no"
-    system "make"
+    system "#{CREW_ENV_OPTIONS} \
+      ./configure  #{CREW_OPTIONS} --enable-gtk-doc-html=no"
+    system 'make'
   end
+
   def self.install
     system "make DESTDIR=#{CREW_DEST_DIR} install"
   end
