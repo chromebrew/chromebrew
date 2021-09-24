ef321efd13380dabaee697c598f295625c1441ae
Satadru Pramanik <satadru@gmail.com>
2021-09-24 17:15:07 -0400

    update i686 build

--- packages/gdb.rb
+++ packages/gdb.rb
@@ -13,11 +13,13 @@ class Gdb < Package
   source_sha256 'cccfcc407b20d343fb320d4a9a2110776dd3165118ffd41f4b1b162340333f94'
 
   binary_url({
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/11.1_i686/gdb-11.1-chromeos-i686.tar.xz',
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/11.1_armv7l/gdb-11.1-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/11.1_armv7l/gdb-11.1-chromeos-armv7l.tpxz',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/11.1_x86_64/gdb-11.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
+       i686: '5ed42d4d144048f46d888079719431d5031f39f60f392dd690d72fe52233e19f',
     aarch64: 'd790322bdc75abf93546a0845c55562e9ef358edd5f80883c4847b630bda6c27',
      armv7l: 'd790322bdc75abf93546a0845c55562e9ef358edd5f80883c4847b630bda6c27',
      x86_64: 'f3d0bb232d38629b0c25e12d29a5098a7d6fb01abcc65e914401b313b5d0bd68'
