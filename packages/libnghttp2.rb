11363a50fd411afeda773c0bb0acbdabdd663518
Satadru Pramanik <satadru@umich.edu>
2021-09-26 22:45:16 -0400

    libnghttp2 -> 1.45.1, libcurl -> 7.79.1 (#6252)
    * libnghttp2 -> 1.45.1, libcurl -> 7.79.1
    
    * add aarch64 line
    
    * adjust build options and remove libevent
    
    * Add i686 binaries
    
    Co-authored-by: Ed Reel <edreel@gmail.com>

--- packages/libnghttp2.rb
+++ packages/libnghttp2.rb
@@ -3,37 +3,35 @@ require 'package'
 class Libnghttp2 < Package
   description 'library implementing HTTP/2 protocol'
   homepage 'https://nghttp2.org/'
-  @_ver = '1.44.0'
+  @_ver = '1.45.1'
   version @_ver
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/nghttp2/nghttp2.git'
-  git_hashtag 'v' + @_ver
+  git_hashtag "v#{@_ver}"
 
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.44.0_armv7l/libnghttp2-1.44.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.44.0_armv7l/libnghttp2-1.44.0-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.44.0_i686/libnghttp2-1.44.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.44.0_x86_64/libnghttp2-1.44.0-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.45.1_armv7l/libnghttp2-1.45.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.45.1_armv7l/libnghttp2-1.45.1-chromeos-armv7l.tpxz'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.45.1_i686/libnghttp2-1.45.1-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.45.1_x86_64/libnghttp2-1.45.1-chromeos-x86_64.tpxz',
   })
-  binary_sha256 ({
-    aarch64: '7409b33d59d3a48db51830a3a1522668a285641a20636078d9fea29d37435ca1',
-     armv7l: '7409b33d59d3a48db51830a3a1522668a285641a20636078d9fea29d37435ca1',
-       i686: '0f55cc49ebc2542942d6aff73efb876e89da22494892f28c310611ddb0849aff',
-     x86_64: '1db2fa466e7c434dab211a202a85518f1607d0a9311d8ad7606f5cbd9a55635b',
+  binary_sha256({
+    aarch64: 'a53af4ca732b343d976f729e809dff883159482b6338dfcb1541e9d20da469db',
+     armv7l: 'a53af4ca732b343d976f729e809dff883159482b6338dfcb1541e9d20da469db'
+       i686: '209c4b802cc37ceb4e508e54a403fea7ab23a84c9ec317d8bf497723e32969c4',
+     x86_64: 'f1d7369aee4be665d89a1cc582802fa1cc9d0b7803bd5df91cf2eb8b62a79241',
   })
 
   depends_on 'jansson'
   depends_on 'jemalloc'
-  depends_on 'libevent' => :build
   depends_on 'libev' => :build
   depends_on 'py3_cython' => :build
 
   def self.build
-    Dir.mkdir 'builddir'
+    FileUtils.mkdir 'builddir'
     Dir.chdir 'builddir' do
       system "cmake -G Ninja #{CREW_CMAKE_OPTIONS} \
-        -DENABLE_EXAMPLES=OFF \
         -DENABLE_SHARED_LIB=ON \
         -DENABLE_STATIC_LIB=ON .."
       system 'samu'
