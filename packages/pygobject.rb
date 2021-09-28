0db723425f652c8e97477dfef995783b19a74006
Ed Reel <edreel@gmail.com>
2021-09-28 05:16:13 +0000

    Add i686 binary

--- packages/pygobject.rb
+++ packages/pygobject.rb
@@ -13,13 +13,13 @@ class Pygobject < Package
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0_armv7l/pygobject-3.42.0-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0_armv7l/pygobject-3.42.0-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0_i686/pygobject-3.42.0-chromeos-i686.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0_i686/pygobject-3.42.0-chromeos-i686.tar.xz',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0_x86_64/pygobject-3.42.0-chromeos-x86_64.tpxz'
   })
   binary_sha256({
     aarch64: '3078465f299ca21b754eddacf8f3d10e5674cb93773288539010803a7024c339',
      armv7l: '3078465f299ca21b754eddacf8f3d10e5674cb93773288539010803a7024c339',
-       i686: '9ddfd5b69486b7af2e07bb455136d60a412764d0e02839ca74baefe81ed55243',
+       i686: 'b4a3f0c1db1c86bcba8f36a213af0031ec749218d7facf8c77f20c76f1cf9fb2',
      x86_64: '119c35a1e20179f9e1010e6b713209539e060d3fbd8c0f560aaa6e8d55499b00'
   })
 
