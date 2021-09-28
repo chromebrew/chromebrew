eb9f4049103eb8f790093312c3e667b8a41fecc4
Satadru Pramanik <satadru@umich.edu>
2021-09-28 01:25:59 -0400

    Update OpenSSL perl modules (#6034)
    * Update OpenSSL perl modules
    
    * update i686 binaries

--- packages/perl_io_socket_ssl.rb
+++ packages/perl_io_socket_ssl.rb
@@ -3,24 +3,27 @@ require 'package'
 class Perl_io_socket_ssl < Package
   description 'IO::Socket::SSL - SSL sockets with IO::Socket interface'
   homepage 'https://metacpan.org/pod/IO::Socket::SSL'
-  version '2.068-1'
+  version '2.071'
   license 'GPL-1+ or Artistic'
   compatibility 'all'
-  source_url 'https://cpan.metacpan.org/authors/id/S/SU/SULLR/IO-Socket-SSL-2.068.tar.gz'
-  source_sha256 '4420fc0056f1827b4dd1245eacca0da56e2182b4ef6fc078f107dc43c3fb8ff9'
+  source_url 'https://cpan.metacpan.org/authors/id/S/SU/SULLR/IO-Socket-SSL-2.071.tar.gz'
+  source_sha256 '40da40948ecc9c787ed39c95715872679eebfd54243721174993a2003e32ab0a'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.068-1_armv7l/perl_io_socket_ssl-2.068-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.068-1_armv7l/perl_io_socket_ssl-2.068-1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.068-1_i686/perl_io_socket_ssl-2.068-1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.068-1_x86_64/perl_io_socket_ssl-2.068-1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.071_armv7l/perl_io_socket_ssl-2.071-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.071_armv7l/perl_io_socket_ssl-2.071-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.071_i686/perl_io_socket_ssl-2.071-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_io_socket_ssl/2.071_x86_64/perl_io_socket_ssl-2.071-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '8c2230feb2b7205349703e5b5b5b10e9f21f3ce079e5b6036728c33ee91a1ff6',
-     armv7l: '8c2230feb2b7205349703e5b5b5b10e9f21f3ce079e5b6036728c33ee91a1ff6',
-       i686: '24df6cac573463073b723da4b9b1a65380e2cdd67a053aabb9e61491410960c7',
-     x86_64: 'd753d89a94d4a9538bf576040e00b3216b1746a9930e091a60366f2627978fc4'
+    aarch64: '9fc1e546ec21478fb4ed274e0a06bcbf62b1cd255e55f27368548f925ced9a5a',
+     armv7l: '9fc1e546ec21478fb4ed274e0a06bcbf62b1cd255e55f27368548f925ced9a5a',
+       i686: 'a6d658776f94b42cf15d63e53c7a85ce11e7624842a16f6c077560d050db2a5b',
+     x86_64: 'edb13970364eda495488af15f78716f4959fa907645574667ef9dddf5e3281cf'
   })
+
+  depends_on 'perl_net_ssleay'
+  depends_on 'perl_mozilla_ca'
 
   def self.build
     system "yes | perl Makefile.PL PREFIX=#{CREW_PREFIX} DESTDIR=#{CREW_DEST_DIR}"
