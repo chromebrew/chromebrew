ca234c193c808183f50618d8660e849d5cf74f64
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2021-10-01 16:36:15 +0000

    pipdeptree

---
+++ packages/py3_pipdeptree.rb
@@ -0,0 +1,23 @@
+require 'package'
+
+class Py3_pipdeptree < Package
+  description 'Displays a dependency tree of the installed Python packages.'
+  homepage 'https://github.com/naiquevin/pipdeptree/'
+  @_ver = '2.1.0'
+  version @_ver
+  license 'MIT'
+  compatibility 'all'
+  source_url 'https://github.com/naiquevin/pipdeptree.git'
+  git_hashtag @_ver
+
+  depends_on 'py3_pip'
+  depends_on 'py3_setuptools' => :build
+
+  def self.build
+    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+  end
+
+  def self.install
+    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+  end
+end
