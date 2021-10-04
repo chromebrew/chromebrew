f206518a25108ba6ee86fcf1cc0b8509da5836d9
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2021-10-04 17:20:18 +0000

    Findutils: don't conflict with mlocate

--- packages/findutils.rb
+++ packages/findutils.rb
@@ -3,24 +3,11 @@ require 'package'
 class Findutils < Package
   description 'the basic directory searching utilities of the GNU operating system'
   homepage 'https://www.gnu.org/software/findutils/'
-  version '4.8.0'
+  version '4.8.0-1'
   license 'GPL-3+'
   compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/findutils/findutils-4.8.0.tar.xz'
   source_sha256 '57127b7e97d91282c6ace556378d5455a9509898297e46e10443016ea1387164'
-
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_armv7l/findutils-4.8.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_armv7l/findutils-4.8.0-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_i686/findutils-4.8.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_x86_64/findutils-4.8.0-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'e20da96c2bc854e84a1f0f7f6f6876d5d5a36c8add7ebf3d8df94b64299ab25c',
-     armv7l: 'e20da96c2bc854e84a1f0f7f6f6876d5d5a36c8add7ebf3d8df94b64299ab25c',
-       i686: '3246911390cb99054dc438bf67f01bb91453363fb95e2c10a5cf9170d965d1f3',
-     x86_64: '98a700b356ab5b021e51116cdbc80c86ee36168e8583bdd8e49a205938c18562',
-  })
 
   depends_on 'python3'
 
@@ -33,6 +20,12 @@ class Findutils < Package
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    
+    # Don't conflict with mlocate.
+    FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man1/locate.1", "#{CREW_DEST_MAN_PREFIX}/man1/locate.findutils.1"
+    FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man1/updatedb.1", "#{CREW_DEST_MAN_PREFIX}/man1/updatedb.findutils.1"
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/locate", "#{CREW_DEST_PREFIX}/bin/locate.findutils"
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/updatedb", "#{CREW_DEST_PREFIX}/bin/updatedb.findutils"
   end
 
   def self.check
