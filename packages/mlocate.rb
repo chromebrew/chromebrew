8b8dcedb32e8e714d8e387d612a464e0b1b8b1e5
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2021-10-04 17:21:38 +0000

    mlocate: don't conflict with findutils

--- packages/mlocate.rb
+++ packages/mlocate.rb
@@ -2,35 +2,39 @@ require 'package'
 
 class Mlocate < Package
   description 'mlocate is a locate/updatedb implementation.'
-  homepage 'https://pagure.io/mlocate'
-  version '0.26-1'
+  homepage 'https://pagure.io/mlocate/'
+  version '0.26-2'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://releases.pagure.org/mlocate/mlocate-0.26.tar.xz'
   source_sha256 '3063df79fe198fb9618e180c54baf3105b33d88fe602ff2d8570aaf944f1263e'
 
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mlocate/0.26-1_armv7l/mlocate-0.26-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mlocate/0.26-1_armv7l/mlocate-0.26-1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mlocate/0.26-1_i686/mlocate-0.26-1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mlocate/0.26-1_x86_64/mlocate-0.26-1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: '69e02f44a498cd38a8f4bc2e3410f934506426d670664c402a09bfb34d51418c',
-     armv7l: '69e02f44a498cd38a8f4bc2e3410f934506426d670664c402a09bfb34d51418c',
-       i686: '74acf4453947b3570f68c43e485cb8e9f2da688e4a50336d593bbba1b6ebd2ee',
-     x86_64: 'f943fe00ad4666f5a9c7bf12d710cd8f2e136a33ad94568863b92dd45526d7eb',
-  })
+  def self.patch
+      @updatedb_wrapper = <<~EOF
+      #!/bin/bash -e
+      if [ "${EUID}" = 0 ]; then
+        echo "updatedb should not be run as root on ChromeOS."
+        exit 1
+      else
+        #{CREW_PREFIX}/bin/updatedb.real "${@}"
+      fi
+    EOF
+    IO.write("updatedb.sh", @updatedb_wrapper)
+  end
 
   def self.build
-    system "mkdir -p #{CREW_PREFIX}/db/mlocate"
-    system "sed -i 's,\$(localstatedir)/,#{CREW_PREFIX}/db/,g' Makefile.*"
-    system "sed -i \"s/groupname = mlocate/groupname = #{USER}/g\" Makefile.*"
-    system "./configure"
-    system "make"
+    system "sed -i 's:groupname = mlocate:groupname = #{USER}:g' Makefile.*"
+    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
+    system 'make'
   end
 
   def self.install
     system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/updatedb", "#{CREW_DEST_PREFIX}/bin/updatedb.real"
+    FileUtils.install "updatedb.sh", "#{CREW_DEST_PREFIX}/bin/updatedb", mode: 0755
+  end
+  
+  def self.check
+    system 'make', 'check'
   end
 end
