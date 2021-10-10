24dbaff7e1beea9b070d28f5ab47d228fb776678
supechicken <supechicken666@gmail.com>
2021-10-10 23:24:32 +0800

    Bump version to 1.1

--- packages/crew_launcher.rb
+++ packages/crew_launcher.rb
@@ -3,11 +3,11 @@ require 'package'
 class Crew_launcher < Package
   description 'Add Chromebrew app to launcher'
   homepage 'https://github.com/skycocker/chromebrew'
-  version '1.0'
+  version '1.1'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://github.com/supechicken/crew-launcher.git'
-  git_hashtag '1.0'
+  git_hashtag '1.1'
   
   depends_on 'graphicsmagick'
 
