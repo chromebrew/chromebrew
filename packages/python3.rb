1795f8874fada8f764e4bbe1da558e42522c9f8e
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 11:33:34 -0700

    upgrade python to 3.10

--- packages/python3.rb
+++ packages/python3.rb
@@ -3,25 +3,12 @@ require 'package'
 class Python3 < Package
   description 'Python is a programming language that lets you work quickly and integrate systems more effectively.'
   homepage 'https://www.python.org/'
-  @_ver = '3.9.7'
+  @_ver = '3.10.0'
   version @_ver
   license 'PSF-2.0'
   compatibility 'all'
   source_url "https://www.python.org/ftp/python/#{@_ver}/Python-#{@_ver}.tar.xz"
-  source_sha256 'f8145616e68c00041d1a6399b76387390388f8359581abc24432bb969b5e3c57'
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/python3/3.9.7_armv7l/python3-3.9.7-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/python3/3.9.7_armv7l/python3-3.9.7-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/python3/3.9.7_i686/python3-3.9.7-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/python3/3.9.7_x86_64/python3-3.9.7-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '7c0e20f9fd73300052c3b267dac8717a67e61bd5d82c9c55345948e3f18d99d9',
-     armv7l: '7c0e20f9fd73300052c3b267dac8717a67e61bd5d82c9c55345948e3f18d99d9',
-       i686: 'ac791613a84ee15ee70248671b91b548cec15f3fd69e11313e2037b880c2c42c',
-     x86_64: '53433704eddfa9f05fe60f9cc03df1a2154dcfb23f01979c74b1d6a16e1ecbf3'
-  })
+  source_sha256 '5a99f8e7a6a11a7b98b4e75e0d1303d3832cada5534068f69c7b6222a7b1b002'
 
   depends_on 'autoconf_archive' => :build
   depends_on 'krb5' => :build
@@ -83,14 +70,14 @@ class Python3 < Package
         LDFLAGS='#{@ldflags} -lpthread' \
       ../configure #{CREW_OPTIONS} \
         --with-computed-gotos \
-        --with-loadable-sqlite-extensions \
+        --enable-loadable-sqlite-extensions \
         --without-ensurepip \
         --enable-optimizations \
         --with-lto \
         --with-platlibdir='lib#{CREW_LIB_SUFFIX}' \
         --with-system-ffi \
         --with-system-expat \
-        --with-system-libmpdev \
+        --with-system-libmpdec \
         --enable-shared"
       system 'make'
     end
