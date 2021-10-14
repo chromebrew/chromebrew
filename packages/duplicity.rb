461a6fd713f8841751604e9b9f914ef7e2bb2092
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:23:37 -0700

    python3.10ize duplicity

--- packages/duplicity.rb
+++ packages/duplicity.rb
@@ -6,19 +6,8 @@ class Duplicity < Package
   version '0.8.19'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://files.pythonhosted.org/packages/9f/ae/4eb8869a12a531f453ef1d64a0d53dfa6a674895f6222b347f8c96a7a88b/duplicity-0.8.19.tar.gz'
-  source_sha256 'd5dda2918058d0e47bf7a1299cfc9b51cc2f0546920fc3cfcf7ae06fc76d68ba'
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/duplicity/0.8.19_armv7l/duplicity-0.8.19-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/duplicity/0.8.19_armv7l/duplicity-0.8.19-chromeos-armv7l.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/duplicity/0.8.19_x86_64/duplicity-0.8.19-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '69cffac0a6b444047e073e8d8aff2dcd0f3e362da626ea0cb2824530963aa684',
-     armv7l: '69cffac0a6b444047e073e8d8aff2dcd0f3e362da626ea0cb2824530963aa684',
-     x86_64: 'a472aa7109a205f91dd16a4e3604bf73ddd7ef5e257eddfd7c63a39ba18eebbc'
-  })
+  source_url 'https://files.pythonhosted.org/packages/84/65/5ca97dade5527b6a93757e88455c53b0d7002322f9d47d848c35902ef431/duplicity-0.8.20.tar.gz'
+  source_sha256 '488af2ecadb059214074f2b3ac51bf9d7de55a800e37ccc2f1075cd0a74940e3'
 
   depends_on 'librsync'
   depends_on 'gnupg'
