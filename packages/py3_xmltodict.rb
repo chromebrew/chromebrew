b24d6bcc05e674ab774009908cb84761c48096ed
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:26 -0700

    python3.10ize py3_xmltodict

--- packages/py3_xmltodict.rb
+++ packages/py3_xmltodict.rb
@@ -4,24 +4,11 @@ class Py3_xmltodict < Package
   description 'XMLtoDict makes working with XML feel like you are working with JSON.'
   homepage 'https://github.com/martinblech/xmltodict/'
   @_ver = '0.12.0'
-  version @_ver
+  version @_ver + '-1'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/martinblech/xmltodict.git'
   git_hashtag "v#{@_ver}"
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_xmltodict/0.12.0_armv7l/py3_xmltodict-0.12.0-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_xmltodict/0.12.0_armv7l/py3_xmltodict-0.12.0-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_xmltodict/0.12.0_i686/py3_xmltodict-0.12.0-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_xmltodict/0.12.0_x86_64/py3_xmltodict-0.12.0-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: 'd2f1f67dd415be8c6ea04832d0c92a5241d2db02f33b036c89d4c04b587fb17e',
-     armv7l: 'd2f1f67dd415be8c6ea04832d0c92a5241d2db02f33b036c89d4c04b587fb17e',
-       i686: '59801b6583e036eb706d57b0b71e6ae7719310e665d435cffef7f65f084ecab7',
-     x86_64: 'a71384afcde283e696cfce1754884f6b347da546ad58523929cc5600c75a5b5c'
-  })
 
   depends_on 'py3_setuptools' => :build
 
