b2dd5120049545d55b99fbf59bfe4ca3cc34ad39
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:03 -0700

    python3.10ize py3_oci

--- packages/py3_oci.rb
+++ packages/py3_oci.rb
@@ -3,23 +3,12 @@ require 'package'
 class Py3_oci < Package
   description 'Oracle Cloud Infrastructure Python SDK'
   homepage 'https://oracle-cloud-infrastructure-python-sdk.readthedocs.io/'
-  @_ver = '2.38.1'
+  @_ver = '2.47.1'
   version @_ver
   license 'UPL-1.0 or Apache-2.0'
   compatibility 'all'
   source_url 'https://github.com/oracle/oci-python-sdk.git'
   git_hashtag "v#{@_ver}"
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_oci/2.38.1_armv7l/py3_oci-2.38.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_oci/2.38.1_armv7l/py3_oci-2.38.1-chromeos-armv7l.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_oci/2.38.1_x86_64/py3_oci-2.38.1-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '0412d58af1bf04b702d03c3121ffd53eaf3b2143c88d4ba40336c2f340a210da',
-     armv7l: '0412d58af1bf04b702d03c3121ffd53eaf3b2143c88d4ba40336c2f340a210da',
-     x86_64: 'd8405fcf598555bd2e060fd2549ac055b09d086a6d4f96abda982a9b0d48697e'
-  })
 
   depends_on 'py3_dateutil'
   depends_on 'py3_configparser'
