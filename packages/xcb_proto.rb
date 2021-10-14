7d95be483068893b8b7cc372509740ea6842f8af
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:30 -0700

    python3.10ize xcb_proto

--- packages/xcb_proto.rb
+++ packages/xcb_proto.rb
@@ -3,30 +3,15 @@ require 'package'
 class Xcb_proto < Package
   description 'The protocols for the X window system provide extended functionality for communication between a X client and the server.'
   homepage 'https://xcb.freedesktop.org'
-  version '1.14.1'
+  version '1.14.1-1'
   license 'MIT-with-advertising'
   compatibility 'all'
-  source_url 'https://x.org/releases/individual/proto/xcb-proto-1.14.tar.xz'
-  source_sha256 '186a3ceb26f9b4a015f5a44dcc814c93033a5fc39684f36f1ecc79834416a605'
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xcb_proto/1.14.1_armv7l/xcb_proto-1.14.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xcb_proto/1.14.1_armv7l/xcb_proto-1.14.1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xcb_proto/1.14.1_i686/xcb_proto-1.14.1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xcb_proto/1.14.1_x86_64/xcb_proto-1.14.1-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    aarch64: '44acc3a13eef02910837f702f2375cfe9b4ec84b29b21901dde05a20bb03cd5f',
-     armv7l: '44acc3a13eef02910837f702f2375cfe9b4ec84b29b21901dde05a20bb03cd5f',
-       i686: '43bd2fe4d681f2a13deef0eeb5b1e22850dc53c836613a18fe73f1c77267a03a',
-     x86_64: '6f7ccb0abc93a0b8156993ab46aa096bc087ae1c82eac33ef8924d7d69f3027f'
-  })
+  source_url 'https://x.org/releases/individual/proto/xcb-proto-1.14.1.tar.xz'
+  source_sha256 'f04add9a972ac334ea11d9d7eb4fc7f8883835da3e4859c9afa971efdf57fcc3'
 
   def self.build
-    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' \
-        LDFLAGS='-flto=auto' \
-        ./autogen.sh \
-        #{CREW_OPTIONS}"
+    system 'NOCONFIGURE=1 ./autogen.sh'
+    system "#{CREW_ENV_OPTIONS} ./autogen.sh #{CREW_OPTIONS}"
     system 'make'
   end
 
