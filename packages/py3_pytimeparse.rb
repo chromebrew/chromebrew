4d7dc2e74042cfd20442a4732d70e0cf70d74072
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:12 -0700

    python3.10ize py3_pytimeparse

--- packages/py3_pytimeparse.rb
+++ packages/py3_pytimeparse.rb
@@ -4,24 +4,11 @@ class Py3_pytimeparse < Package
   description 'Pytimeparse is a small Python module to parse various kinds of time expressions. '
   homepage 'https://github.com/wroberts/pytimeparse/'
   @_ver = '1.1.8'
-  version @_ver
+  version @_ver + '-1'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/wroberts/pytimeparse.git'
   git_hashtag @_ver
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytimeparse/1.1.8_armv7l/py3_pytimeparse-1.1.8-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytimeparse/1.1.8_armv7l/py3_pytimeparse-1.1.8-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytimeparse/1.1.8_i686/py3_pytimeparse-1.1.8-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytimeparse/1.1.8_x86_64/py3_pytimeparse-1.1.8-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '0d51a50a382749a31a884549ac6914bf9c43fc6937eb5c330c083b050fed74f8',
-     armv7l: '0d51a50a382749a31a884549ac6914bf9c43fc6937eb5c330c083b050fed74f8',
-       i686: 'dab4724d8ef6d17b01092e58a7f8d79f8fcecfd0bdfdbdca95c1e97fbe272423',
-     x86_64: 'fcfab1dc5a048c57fd8a465fc1f3df01bb74e938f10a627c7c327f3eb3dc280f'
-  })
 
   depends_on 'py3_setuptools' => :build
 
