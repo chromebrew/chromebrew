549bcff23426f1e0e798a88a9da7ae7c1cfc9340
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:14 -0700

    python3.10ize py3_retrying

--- packages/py3_retrying.rb
+++ packages/py3_retrying.rb
@@ -4,24 +4,11 @@ class Py3_retrying < Package
   description 'Retrying simplifies the task of adding retry behavior to just about anything.'
   homepage 'https://github.com/rholder/retrying/'
   @_ver = '1.3.3'
-  version @_ver
+  version @_ver + '-1'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'https://github.com/rholder/retrying.git'
   git_hashtag "v#{@_ver}"
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_retrying/1.3.3_armv7l/py3_retrying-1.3.3-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_retrying/1.3.3_armv7l/py3_retrying-1.3.3-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_retrying/1.3.3_i686/py3_retrying-1.3.3-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_retrying/1.3.3_x86_64/py3_retrying-1.3.3-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: 'b740f5334db1044b55f4edc3876f7f88b631584abde4cb57f68f0a29285e4820',
-     armv7l: 'b740f5334db1044b55f4edc3876f7f88b631584abde4cb57f68f0a29285e4820',
-       i686: 'd0dcfc77dc868dda22e863de9d140384cb53fe010b9e67da9a13a217c6365e85',
-     x86_64: 'b96a36c91d9c3a54ec595ac9133cf62d45f03c6057a785cca907cae6006c55b7'
-  })
 
   depends_on 'py3_six'
   depends_on 'py3_setuptools' => :build
