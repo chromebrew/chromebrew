5ac191da8b76cc396474f79bd875988a2d97dc73
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:04 -0700

    python3.10ize py3_openpyxl

--- packages/py3_openpyxl.rb
+++ packages/py3_openpyxl.rb
@@ -3,23 +3,12 @@ require 'package'
 class Py3_openpyxl < Package
   description 'Openpyxl is a Python library to read/write Excel 2010 xlsx/xlsm files.'
   homepage 'https://openpyxl.readthedocs.io/'
-  @_ver = '3.0.7'
+  @_ver = '3.0.9'
   version @_ver
   license 'MIT'
   compatibility 'all'
-  source_url 'https://files.pythonhosted.org/packages/f1/7d/fb475cd963bd9d244f95a90c98f518f5c834fefe749f25f9f479ca2d8a51/openpyxl-3.0.7.tar.gz'
-  source_sha256 '6456a3b472e1ef0facb1129f3c6ef00713cebf62e736cd7a75bcc3247432f251'
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_openpyxl/3.0.7_armv7l/py3_openpyxl-3.0.7-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_openpyxl/3.0.7_armv7l/py3_openpyxl-3.0.7-chromeos-armv7l.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_openpyxl/3.0.7_x86_64/py3_openpyxl-3.0.7-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '67f3562fdfa8fc22850b8cb128678f4bf508704e5b2eea838e29952084b74202',
-     armv7l: '67f3562fdfa8fc22850b8cb128678f4bf508704e5b2eea838e29952084b74202',
-     x86_64: '7fc82dee675d67baafa0458367c844c5493bf9c167ef229e40d940fc55420703'
-  })
+  source_url 'https://files.pythonhosted.org/packages/9e/19/c45fb7a40cd46e03e36d60d1db26a50a795fa0b6b8a2a8094f4ac0c71ae5/openpyxl-3.0.9.tar.gz'
+  source_sha256 '40f568b9829bf9e446acfffce30250ac1fa39035124d55fc024025c41481c90f'
 
   depends_on 'py3_et_xmlfile'
   depends_on 'py3_setuptools' => :build
