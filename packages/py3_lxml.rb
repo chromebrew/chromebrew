1f7a45f70467918a8c9ef723cf8897dcb2643173
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:00 -0700

    python3.10ize py3_lxml

--- packages/py3_lxml.rb
+++ packages/py3_lxml.rb
@@ -4,22 +4,13 @@ class Py3_lxml < Package
   description 'LXML is a Python library for processing XML and HTML.'
   homepage 'https://lxml.de/'
   @_ver = '4.6.3'
-  version @_ver
+  version @_ver + '-1'
   license 'BSD-3'
   compatibility 'all'
-  source_url 'https://github.com/lxml/lxml.git'
-  git_hashtag "lxml-#{@_ver}"
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_lxml/4.6.3_armv7l/py3_lxml-4.6.3-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_lxml/4.6.3_armv7l/py3_lxml-4.6.3-chromeos-armv7l.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_lxml/4.6.3_x86_64/py3_lxml-4.6.3-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '78988749162202d2da062a5c9b718d27f9cb0290d18cf3526eeba8affa489657',
-     armv7l: '78988749162202d2da062a5c9b718d27f9cb0290d18cf3526eeba8affa489657',
-     x86_64: 'fa0232ee0bd1d4a171f37f15e0e608e3fd27bc9306c30d195ac83639cfa5feb6'
-  })
+  #source_url 'https://github.com/lxml/lxml.git'
+  #git_hashtag "lxml-#{@_ver}"
+  source_url 'https://files.pythonhosted.org/packages/e5/21/a2e4517e3d216f0051687eea3d3317557bde68736f038a3b105ac3809247/lxml-4.6.3.tar.gz'
+  source_sha256 '39b78571b3b30645ac77b95f7c69d1bffc4cf8c3b157c435a34da72e78c82468'
 
   depends_on 'py3_cython' => :build
   depends_on 'py3_setuptools' => :build
