10b96fb01b12592ff1dd400db94cfd562d438ad8
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:06 -0700

    python3.10ize py3_pipdeptree

--- packages/py3_pipdeptree.rb
+++ packages/py3_pipdeptree.rb
@@ -3,7 +3,7 @@ require 'package'
 class Py3_pipdeptree < Package
   description 'Displays a dependency tree of the installed Python packages.'
   homepage 'https://github.com/naiquevin/pipdeptree/'
-  @_ver = '2.1.0'
+  @_ver = '2.2.0'
   version @_ver
   license 'MIT'
   compatibility 'all'
