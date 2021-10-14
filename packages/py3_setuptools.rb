7b651848bbc434e6b44806816c706c365941a2d4
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-13 19:24:15 -0700

    python3.10ize py3_setuptools

--- packages/py3_setuptools.rb
+++ packages/py3_setuptools.rb
@@ -10,6 +10,8 @@ class Py3_setuptools < Package
   source_url "https://files.pythonhosted.org/packages/1e/5c/3d7b3d91a86d71faf5038c5d259ed36b5d05b7804648e2c43251d574a6e6/setuptools-58.2.0.tar.gz"
   source_sha256 '2c55bdb85d5bb460bd2e3b12052b677879cffcf46c0c688f2e5bf51d36001145'
 
+  depends_on 'py3_packaging'
+
   def self.build
     system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
   end
