72a4b349c676bb425fd90611487b3c9b4cddb2d6
Satadru Pramanik <satadru@gmail.com>
2021-10-15 16:39:08 -0400

    cairo libffi rebuild

--- packages/cairo.rb
+++ packages/cairo.rb
@@ -3,23 +3,21 @@ require 'package'
 class Cairo < Package
   description 'Cairo is a 2D graphics library with support for multiple output devices.'
   homepage 'https://www.cairographics.org'
-  version '1.17.4'
+  version '1.17.4-1'
   license 'LGPL-2.1 or MPL-1.1'
   compatibility 'all'
   source_url 'https://cairographics.org/snapshots/cairo-1.17.4.tar.xz'
   source_sha256 '74b24c1ed436bbe87499179a3b27c43f4143b8676d8ad237a6fa787401959705'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4_armv7l/cairo-1.17.4-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4_armv7l/cairo-1.17.4-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4_i686/cairo-1.17.4-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4_x86_64/cairo-1.17.4-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4-1_armv7l/cairo-1.17.4-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4-1_armv7l/cairo-1.17.4-1-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4-1_x86_64/cairo-1.17.4-1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '9ff5df66de2bf89670852e030379a6c1873dfc2e154e86fe0992ee6ebebe4be9',
-     armv7l: '9ff5df66de2bf89670852e030379a6c1873dfc2e154e86fe0992ee6ebebe4be9',
-       i686: 'f0fb7bee8a0311b73b1a8b5a877b9a276d4bce929a267dd2be9542437cbc12bb',
-     x86_64: 'b12d21a683f06db69052f2d927039f6e893aecdc993de1b384af9267d25ff93e'
+    aarch64: '03c6f9c849edf2736de57db8801aea7660d2250d14ed55417bf538ec26ec9787',
+     armv7l: '03c6f9c849edf2736de57db8801aea7660d2250d14ed55417bf538ec26ec9787',
+     x86_64: '281d060106d371f9723bffb6bbc4caa57ec3a991cce740170fa1267a602a051d'
   })
 
   depends_on 'fontconfig'
