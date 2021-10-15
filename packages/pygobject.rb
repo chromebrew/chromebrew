be765f4f6ed04dc8f3ca982e5d335439ad0a6988
Satadru Pramanik <satadru@gmail.com>
2021-10-15 16:59:59 -0400

    Add pygobject and wayland binary with version bump

--- packages/pygobject.rb
+++ packages/pygobject.rb
@@ -4,13 +4,21 @@ class Pygobject < Package
   description 'PyGObject is a Python package which provides bindings for GObject based libraries such as GTK+, GStreamer, WebKitGTK+, GLib, GIO and many more.'
   homepage 'https://pygobject.readthedocs.io/'
   @_ver = '3.42.0'
-  version @_ver + '-1'
+  version "#{@_ver}-1"
   license 'LGPL-2.1+'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/pygobject.git'
   git_hashtag @_ver
 
+  binary_url({
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0-1_x86_64/pygobject-3.42.0-1-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    x86_64: '440ec12a89f102c6421a551584a4a5a11938e9dca4b2aa27f146b1d770a8db18'
+  })
+
   depends_on 'glib'
+  depends_on 'wayland'
   depends_on 'gobject_introspection'
   depends_on 'py3_pycairo'
   depends_on 'py3_setuptools' => :build
