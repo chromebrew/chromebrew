ec49ee3126d765354b6b99ee4a40040e9d29a390
Satadru Pramanik <satadru@gmail.com>
2021-10-15 17:11:26 -0400

    Add py3_libxml2

--- packages/py3_libxml2.rb
+++ packages/py3_libxml2.rb
@@ -4,11 +4,22 @@ class Py3_libxml2 < Package
   description 'Libxml2-python provides access to libxml2 and libxslt in Python.'
   homepage 'https://gitlab.gnome.org/GNOME/libxml2/'
   @_ver = '2.9.12'
-  version @_ver + '-1'
+  version "#{@_ver}-1"
   license 'MIT'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/libxml2.git'
   git_hashtag "v#{@_ver}"
+
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.9.12-1_armv7l/py3_libxml2-2.9.12-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.9.12-1_armv7l/py3_libxml2-2.9.12-1-chromeos-armv7l.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.9.12-1_x86_64/py3_libxml2-2.9.12-1-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    aarch64: 'd2390c711d8f261e1dbd5c193796bc18b47c9b27728730ddccb478302fc9f478',
+     armv7l: 'd2390c711d8f261e1dbd5c193796bc18b47c9b27728730ddccb478302fc9f478',
+     x86_64: '110b87135cf67d05d3eb2291db13b2146f15ef9d33096903e61534a5ffcc78b6'
+  })
 
   depends_on 'libxml2'
   depends_on 'libxslt'
