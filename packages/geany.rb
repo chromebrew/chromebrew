56848b6ff6010fb9340f4ca9073e7dfc649c2d15
Ed Reel <edreel@gmail.com>
2021-10-15 07:53:54 +0000

    Add i686 binary

--- packages/geany.rb
+++ packages/geany.rb
@@ -12,11 +12,13 @@ class Geany < Package
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany/1.38.0_armv7l/geany-1.38.0-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany/1.38.0_armv7l/geany-1.38.0-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany/1.38.0_i686/geany-1.38.0-chromeos-i686.tar.xz',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany/1.38.0_x86_64/geany-1.38.0-chromeos-x86_64.tpxz'
   })
   binary_sha256({
     aarch64: '5171072c5f06a83470f05b717f4920670e096401476bd75da529f3ae6c2a2eed',
      armv7l: '5171072c5f06a83470f05b717f4920670e096401476bd75da529f3ae6c2a2eed',
+       i686: 'ff04fdbe592f3de51138d98f1c1886fa1535625dae9caa8bd8cb147c8e29774a',
      x86_64: 'af37717e4484a5fbed7755961cbacebbe8e7e4abfd352ac26d01a33a2b989aba'
   })
 
