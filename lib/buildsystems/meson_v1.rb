3dad93d3269161bff2a98542755bb396fc3bad21
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-10-16 16:59:41 -0700

    Add meson function and example

---
+++ lib/buildsystems/meson_v1.rb
@@ -0,0 +1,37 @@
+require 'package'
+
+class Meson_v1 < Package
+  @_buildsystems_meson_version = '1.0.0'
+
+  def self.meson_options (options = nil)
+    if options
+      @meson_options = options
+    end
+    @meson_options
+  end
+
+  def self.check? (bool)
+    if bool == true
+      @check = true
+    else
+      @check = false
+    end
+    @check
+  end
+
+  def self.build
+    system "meson #{CREW_MESON_OPTIONS} \
+            #{@meson_options} builddir"
+    system 'samu -C builddir'
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+  end
+
+  if @check == true
+    def self.check
+      system 'samu -C builddir test'
+    end
+  end
+end
--- packages/xorg_proto.rb
+++ packages/xorg_proto.rb
@@ -1,38 +1,13 @@
-require 'package'
+require 'buildsystems/meson_v1'
 
-class Xorg_proto < Package
+class Xorg_proto < Meson_v1
   description 'The xorgproto package provides the header files required to build the X Window system, and to allow other applications to build against the installed X Window system.'
   homepage 'https://www.x.org/'
-  version '2020.1-1'
+  version '2021.5'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2020.1.tar.bz2'
-  source_sha256 '54a153f139035a376c075845dd058049177212da94d7a9707cf9468367b699d2'
+  source_url 'https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2021.5.tar.bz2'
+  source_sha256 'aa2f663b8dbd632960b24f7477aa07d901210057f6ab1a1db5158732569ca015'
 
-  binary_url ({
-     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_proto/2020.1-1_armv7l/xorg_proto-2020.1-1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_proto/2020.1-1_armv7l/xorg_proto-2020.1-1-chromeos-armv7l.tar.xz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_proto/2020.1-1_i686/xorg_proto-2020.1-1-chromeos-i686.tar.xz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_proto/2020.1-1_x86_64/xorg_proto-2020.1-1-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-     aarch64: '3de61490afe9c9b45ad23abf007109b942e196aed3299f90da7ff6429400b687',
-      armv7l: '3de61490afe9c9b45ad23abf007109b942e196aed3299f90da7ff6429400b687',
-        i686: '521ca4cca07304bf9c18e050a98ad9fde912b1ecc6c5cf13aa223b969c617d09',
-      x86_64: '1ab0f6074972bafa33e78e945958b70c7886c0abff146bc8a020bfc499c7e14f',
-  })
-
-  depends_on 'llvm' => :build
-
-  def self.build
-    system "meson #{CREW_MESON_FNO_LTO_OPTIONS} \
-      -Dc_args='-fuse-ld=lld' \
-      builddir"
-    system "meson configure builddir"
-    system "ninja -C builddir"
-  end
-
-  def self.install
-   system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
+  check? true
 end
