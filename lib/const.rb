f76626ed924a43ae4feea8ea67b7dfe5ba93b3af
Satadru Pramanik <satadru@umich.edu>
2021-10-18 11:38:34 -0400

    Switch upx to just use lzma to speed up builds (#6313)

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.17.4'
+CREW_VERSION = '1.17.5'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
