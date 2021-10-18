10aac24bc2bcbe478eee759e1dc06fedc66ad9b0
Satadru Pramanik <satadru@gmail.com>
2021-10-18 15:02:16 -0400

    add gjs

--- packages/gjs.rb
+++ packages/gjs.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Gjs < Package
   description 'Javascript Bindings for GNOME'
-  @_ver = '1.68.1'
+  @_ver = '1.70.0'
   @_ver_prelastdot = @_ver.rpartition('.')[0]
   version @_ver
   license 'MIT and MPL-1.1, LGPL-2+ or GPL-2+'
@@ -11,16 +11,10 @@ class Gjs < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gjs/1.68.1_armv7l/gjs-1.68.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gjs/1.68.1_armv7l/gjs-1.68.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gjs/1.68.1_i686/gjs-1.68.1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gjs/1.68.1_x86_64/gjs-1.68.1-chromeos-x86_64.tpxz'
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gjs/1.70.0_x86_64/gjs-1.70.0-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: 'a44d907e21468900d3ac6e0ea326dc051e431dd2eedf3ea8f3fc1603b5b93d70',
-     armv7l: 'a44d907e21468900d3ac6e0ea326dc051e431dd2eedf3ea8f3fc1603b5b93d70',
-       i686: 'd1c8db6ed575dd5f471fc3c91e8c7f5f34e448ba8eca2c3aa8802eaf923f3ff3',
-     x86_64: '0d0b81db80db509870491850ce86bc0b7b0aaca8407eb8d7c09e47797aa908ba'
+    x86_64: '2431c3e3f578dd8570349140729630d7916d49f9cf0b127097cad2e0fdaa1fe5'
   })
 
   depends_on 'cairo' # R
