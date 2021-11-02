6b09eff14f77b97c50675c01c7a753f8b670229e
Satadru Pramanik <satadru@gmail.com>
2021-11-02 19:06:49 -0400

    glib -> 2.70.1

--- packages/glib.rb
+++ packages/glib.rb
@@ -3,25 +3,25 @@ require 'package'
 class Glib < Package
   description 'GLib provides the core application building blocks for libraries and applications written in C.'
   homepage 'https://developer.gnome.org/glib'
-  @_ver = '2.70.0'
+  @_ver = '2.70.1'
   @_ver_prelastdot = @_ver.rpartition('.')[0]
-  version "#{@_ver}-1"
+  version @_ver
   license 'LGPL-2.1'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/glib.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.0-1_armv7l/glib-2.70.0-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.0-1_armv7l/glib-2.70.0-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.0-1_i686/glib-2.70.0-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.0-1_x86_64/glib-2.70.0-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.1_armv7l/glib-2.70.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.1_armv7l/glib-2.70.1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.1_i686/glib-2.70.1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glib/2.70.1_x86_64/glib-2.70.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '1ee370c35e2e73717605624b05642d84f1956520a7e5a20c5c33d6f000e51c90',
-     armv7l: '1ee370c35e2e73717605624b05642d84f1956520a7e5a20c5c33d6f000e51c90',
-       i686: '76277283eefa7f66e342eceeae14b1d5111dd38ed24c42f1fa19db285654db36',
-     x86_64: 'f387960239452bc78ae156eeec65d81d7435ca0d8620c135836b2a4ec339ceae'
+    aarch64: '35ff6b5b193f8cdee2860fa3a4ea0e4d1883ed8b4aa8b92133668b00e81befdb',
+     armv7l: '35ff6b5b193f8cdee2860fa3a4ea0e4d1883ed8b4aa8b92133668b00e81befdb',
+       i686: '36dc278935817052f3c82942f09e1acd5d3235249b45362d035fb4c98c480b9d',
+     x86_64: '7592825461594bd7884ea2f245cd877bda9958c49f4c00441f3b3ce774eb5de4'
   })
 
   depends_on 'shared_mime_info' # L
