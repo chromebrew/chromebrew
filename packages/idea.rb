414e3ad3089021ff753efb2354e26b6c57d5b6e3
Ed Reel <edreel@gmail.com>
2021-11-06 15:44:39 -0500

    Idea 2021.1.2 => 2021.2.3

--- packages/idea.rb
+++ packages/idea.rb
@@ -3,10 +3,10 @@ require 'package'
 class Idea < Package
   description 'Capable and Ergonomic IDE for JVM'
   homepage 'https://www.jetbrains.com/idea/'
-  version '2021.1.2'
+  version '2021.2.3'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://download.jetbrains.com/idea/ideaIC-2021.1.2.tar.gz'
+  source_url 'https://download.jetbrains.com/idea/ideaIC-2021.2.3.tar.gz'
   source_sha256 'e2517d79b39581f1548ca4119cb2fa478505cf73203d97b4f3292f05ae71250e'
 
   depends_on 'jdk8'
@@ -14,14 +14,14 @@ class Idea < Package
   depends_on 'sommelier'
 
   def self.install
-    # Fix java.io.IOException: Cannot run program "/home/chronos/user/.IdeaIC2019.3/system/tmp/ij1055598732.tmp": error=13, Permission denied
+    # Fix java.io.IOException: Cannot run program "/home/chronos/user/.IdeaIC2021.2/system/tmp/ij1055598732.tmp": error=13, Permission denied
     FileUtils.mkdir_p "#{CREW_DEST_HOME}"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/.config/.IdeaIC2021.1"
-    system "touch #{CREW_DEST_PREFIX}/.config/.IdeaIC2021.1/test"
-    FileUtils.ln_s "#{CREW_PREFIX}/.config/.IdeaIC2021.1", "#{CREW_DEST_HOME}/.IdeaIC2021.1"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/.config/.IdeaIC2021.2"
+    system "touch #{CREW_DEST_PREFIX}/.config/.IdeaIC2021.2/test"
+    FileUtils.ln_s "#{CREW_PREFIX}/.config/.IdeaIC2021.2", "#{CREW_DEST_HOME}/.IdeaIC2021.2"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/Idea"
-    FileUtils.mv Dir.glob('*'), "#{CREW_DEST_PREFIX}/share/Idea"
+    FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/Idea"
     FileUtils.ln_s "#{CREW_PREFIX}/share/Idea/bin/idea.sh", "#{CREW_DEST_PREFIX}/bin/idea"
   end
 
@@ -32,7 +32,7 @@ class Idea < Package
   def self.remove
     print "Would you like to remove the config directories? [y/N] "
     response = STDIN.getc
-    config_dirs = ["#{CREW_PREFIX}/.config/.IdeaIC2021.1", "#{HOME}/.IdeaIC2021.1"]
+    config_dirs = ["#{CREW_PREFIX}/.config/.IdeaIC2021.2", "#{HOME}/.IdeaIC2021.2"]
     config_dirs.each { |config_dir|
       if Dir.exist? config_dir
         case response
