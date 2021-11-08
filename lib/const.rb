a4c4a0981c95906076a80f110e1b3e90014080b5
Satadru Pramanik <satadru@gmail.com>
2021-11-07 23:24:49 -0500

    bump version

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.17.9'
+CREW_VERSION = '1.18.0'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
