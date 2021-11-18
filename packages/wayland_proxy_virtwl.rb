b68951deb7f27b181427f577d67fc8f95cfbcaa5
Satadru Pramanik <satadru@gmail.com>
2021-11-18 16:57:07 -0500

    Add wayland_proxy_virtwl

---
+++ packages/wayland_proxy_virtwl.rb
@@ -0,0 +1,39 @@
+# Adapted from Arch Linux opam PKGBUILD at:
+# https://github.com/archlinux/svntogit-community/raw/packages/opam/trunk/PKGBUILD
+
+require 'package'
+
+class Wayland_proxy_virtwl < Package
+  description 'Proxy Wayland connections across the VM boundary'
+  homepage 'https://github.com/talex5/wayland-proxy-virtwl'
+  @_ver = '5b576d5cad76de6ed7513cc85748efd78ca366cf'
+  version @_ver
+  compatibility 'all'
+  source_url 'https://github.com/talex5/wayland-proxy-virtwl.git'
+  git_hashtag @_ver
+
+  binary_url({
+    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_i686/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-i686.tpxz',
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_x86_64/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    i686: 'eeaae9cf7237f0941ea8810089d71f75c0fa79388f45762be33d1bcde60c7ab8',
+  x86_64: 'dc6ec360d6f58b84f41cb2b97ba0c7355307d3f1b1d249e82cc0467b70cd54bf'
+  })
+
+  depends_on 'ocaml'
+  depends_on 'opam'
+
+  def self.build
+  end
+
+  def self.install
+    ENV['OPAMROOT'] = "#{CREW_PREFIX}/opam"
+    ENV['CREW_FHS_NONCOMPLIANCE_ONLY_ADVISORY'] = '1'
+    warn_level = $VERBOSE
+    $VERBOSE = nil
+    load "#{CREW_LIB_PATH}lib/const.rb"
+    $VERBOSE = warn_level
+    system "OPAMROOT=#{CREW_PREFIX}/opam opam install . --root=#{CREW_PREFIX}/opam --destdir=#{CREW_DEST_PREFIX}/opam -y"
+  end
+end
