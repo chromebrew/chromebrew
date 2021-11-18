8fe7bb006369d339cea06c6afb40020ce619120a
Satadru Pramanik <satadru@gmail.com>
2021-11-18 11:41:51 -0500

    Harfbuzz -> 3.1.1

--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -3,24 +3,24 @@ require 'package'
 class Harfbuzz < Package
   description 'HarfBuzz is an OpenType text shaping engine.'
   homepage 'https://www.freedesktop.org/wiki/Software/HarfBuzz/'
-  @_ver = '3.0.0'
-  version "#{@_ver}-1"
+  @_ver = '3.1.1'
+  version @_ver
   license 'Old-MIT, ISC and icu'
   compatibility 'all'
   source_url 'https://github.com/harfbuzz/harfbuzz.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.0.0-1_armv7l/harfbuzz-3.0.0-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.0.0-1_armv7l/harfbuzz-3.0.0-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.0.0-1_i686/harfbuzz-3.0.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.0.0-1_x86_64/harfbuzz-3.0.0-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.1.1_armv7l/harfbuzz-3.1.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.1.1_armv7l/harfbuzz-3.1.1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.1.1_i686/harfbuzz-3.1.1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/3.1.1_x86_64/harfbuzz-3.1.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '1b8e95215f5147d297389f81831f27d33eb10d3bc8ef31e6efa4588b477d9d04',
-     armv7l: '1b8e95215f5147d297389f81831f27d33eb10d3bc8ef31e6efa4588b477d9d04',
-       i686: 'f60099106878fd712bb16cba741509333f67bab1761c23ace579a37cb7feb1fc',
-     x86_64: 'cd44acac570c86213ccb02a4da7ff4c2f60c26e12ca7e531e8210334a08166c2'
+    aarch64: '36fa3b23aa11ec3aee2cebef48edf3fc5b82b5c74bb56ec4889717c6981bde62',
+     armv7l: '36fa3b23aa11ec3aee2cebef48edf3fc5b82b5c74bb56ec4889717c6981bde62',
+       i686: 'd1419bdd44a8b7673a78b20c98fe5d454b464f59124e196af09f5e5e12d88959',
+     x86_64: '06a8f9366cec6772f2a9d2ab3da03402f46e1fa768a9b60bc3fd0d5ac0c0e167'
   })
 
   depends_on 'cairo' => :build
@@ -38,7 +38,7 @@ class Harfbuzz < Package
     -Dintrospection=enabled \
     -Dbenchmark=disabled \
     -Dtests=disabled \
-    -Dgraphite=enabled \
+    -Dgraphite2=enabled \
     -Dfreetype=enabled \
     -Dragel_subproject=true \
     -Ddocs=disabled \
