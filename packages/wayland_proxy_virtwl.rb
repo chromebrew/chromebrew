075d9992dc924abec5af87d24249499d51d32aff
Satadru Pramanik <satadru@gmail.com>
2021-11-18 19:02:24 -0500

    fix formatting

--- packages/wayland_proxy_virtwl.rb
+++ packages/wayland_proxy_virtwl.rb
@@ -15,21 +15,20 @@ class Wayland_proxy_virtwl < Package
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_armv7l/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_armv7l/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-armv7l.tpxz',
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_i686/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-i686.tpxz',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_x86_64/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-x86_64.tpxz'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_i686/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_proxy_virtwl/5b576d5cad76de6ed7513cc85748efd78ca366cf_x86_64/wayland_proxy_virtwl-5b576d5cad76de6ed7513cc85748efd78ca366cf-chromeos-x86_64.tpxz'
   })
   binary_sha256({
     aarch64: '9cd15da6fb9278ec06a85e8db9192691cb9785e07806a3e02aa1d2e64056aef2',
      armv7l: '9cd15da6fb9278ec06a85e8db9192691cb9785e07806a3e02aa1d2e64056aef2',
-    i686: 'eeaae9cf7237f0941ea8810089d71f75c0fa79388f45762be33d1bcde60c7ab8',
-  x86_64: 'dc6ec360d6f58b84f41cb2b97ba0c7355307d3f1b1d249e82cc0467b70cd54bf'
+       i686: 'eeaae9cf7237f0941ea8810089d71f75c0fa79388f45762be33d1bcde60c7ab8',
+     x86_64: 'dc6ec360d6f58b84f41cb2b97ba0c7355307d3f1b1d249e82cc0467b70cd54bf'
   })
 
   depends_on 'ocaml' => :build
   depends_on 'opam' => :build
 
-  def self.build
-  end
+  def self.build; end
 
   def self.install
     ENV['OPAMROOT'] = "#{CREW_PREFIX}/opam"
