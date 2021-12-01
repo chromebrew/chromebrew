dc1268df0a55a06cd77fc048c98836f41aeb2426
Satadru Pramanik <satadru@gmail.com>
2021-12-01 13:36:35 -0500

    Add binaries and dependencies for xrdb

--- packages/xrdb.rb
+++ packages/xrdb.rb
@@ -10,6 +10,21 @@ class Xrdb < Package
   source_url 'https://gitlab.freedesktop.org/xorg/app/xrdb.git'
   git_hashtag "xrdb-#{@_ver}"
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xrdb/1.2.1_armv7l/xrdb-1.2.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xrdb/1.2.1_armv7l/xrdb-1.2.1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xrdb/1.2.1_i686/xrdb-1.2.1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xrdb/1.2.1_x86_64/xrdb-1.2.1-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    aarch64: 'fd449dbef069f5f608d0e5a5d6b7da6104a4e7284b5a1a5e22fdb6d7a21ebc65',
+     armv7l: 'fd449dbef069f5f608d0e5a5d6b7da6104a4e7284b5a1a5e22fdb6d7a21ebc65',
+       i686: 'f1eae836a2c769f2b383cc3c21d2d2e6307f32019a66ebce33ace94755ac3151',
+     x86_64: 'f7f4003249dba8c91b80cf4c462506a6ca7e11deb3891fca79df735925c36ca4'
+  })
+
+  depends_on 'libxmu'
+
   def self.build
     system 'NOCONFIGURE=1 ./autogen.sh'
     system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
