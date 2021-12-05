5c8c80b1fadd6e3e6c0ac1b50a41024a006b0074
Ed Reel <edreel@gmail.com>
2021-12-05 14:12:07 -0600

    Nconvert 6.88 => 7.39 (#6449)

--- packages/nconvert.rb
+++ packages/nconvert.rb
@@ -3,19 +3,20 @@ require 'package'
 class Nconvert < Package
   description 'Command Line Batch utility for images'
   homepage 'https://www.xnview.com/en/nconvert/'
-  version '6.88'
+  version '7.39'
   license 'NConvert-EULA'
   compatibility 'i686,x86_64'
-  case ARCH
-  when 'x86_64'
-    source_url 'https://download.xnview.com/NConvert-linux64.tgz'
-    source_sha256 '852c9cfefb92ea49670c8bc8fc4364297556066026b773321fe6b13718c57467'
-  when 'i686'
-    source_url 'https://download.xnview.com/NConvert-linux.tgz'
-    source_sha256 '10f576609a982c9d7f4a519c9a49899609b63cea65d1a38b0c6f0c2fda2eb8c1'
-  end
+  source_url ({
+      i686: 'https://download.xnview.com/NConvert-linux.tgz',
+    x86_64: 'https://download.xnview.com/NConvert-linux64.tgz',
+  })
+  source_sha256 ({
+      i686: '5e8364bdc1fe61d2c37871e0591ddc2048ccf0cd8041846b433ef04f10280cba',
+    x86_64: '10f576609a982c9d7f4a519c9a49899609b63cea65d1a38b0c6f0c2fda2eb8c1',
+  })
 
   def self.install
-    system "install -Dm755 nconvert #{CREW_DEST_PREFIX}/bin/nconvert"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.install 'nconvert', "#{CREW_DEST_PREFIX}/bin/nconvert", mode: 0o755
   end
 end
