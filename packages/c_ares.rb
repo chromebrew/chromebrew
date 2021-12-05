f18dcead1c6e5ad8512c4875fa79ad27611e5bfe
Satadru Pramanik <satadru@gmail.com>
2021-12-04 20:24:17 -0500

    pre-rebuild of wireshark which depends upon libffi

--- packages/c_ares.rb
+++ packages/c_ares.rb
@@ -3,26 +3,28 @@ require 'package'
 class C_ares < Package
   description 'c-ares is a C library for asynchronous DNS requests (including name resolves).'
   homepage 'https://c-ares.haxx.se/'
-  version '1.17.1'
+  version '1.18.1'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://c-ares.haxx.se/download/c-ares-1.17.1.tar.gz'
-  source_sha256 'd73dd0f6de824afd407ce10750ea081af47eba52b8a6cb307d220131ad93fc40'
+  source_url 'https://github.com/c-ares/c-ares.git'
+  git_hashtag "cares-#{version.gsub('.', '_')}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.17.1_armv7l/c_ares-1.17.1-chromeos-armv7l.tpxz',
-    armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.17.1_armv7l/c_ares-1.17.1-chromeos-armv7l.tpxz',
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.17.1_i686/c_ares-1.17.1-chromeos-i686.tpxz',
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.17.1_x86_64/c_ares-1.17.1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.18.1_armv7l/c_ares-1.18.1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.18.1_armv7l/c_ares-1.18.1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.18.1_i686/c_ares-1.18.1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.18.1_x86_64/c_ares-1.18.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '7d3542bd270aa9ebf5f2b08273f547978290f5febc5142ff8f7fed363efc122e',
-    armv7l: '7d3542bd270aa9ebf5f2b08273f547978290f5febc5142ff8f7fed363efc122e',
-    i686: 'bb4a3bd9eaeff1e09b68f83514ca9a2a64414445bd040536cfe48c147a745641',
-    x86_64: '2ab4479e0290444e44b842072c86c75e3a7a68c326d987be18f2cad01c19ea46'
+    aarch64: '54077bfe2f8fbf4ea028c4b3917d39f3bc65397fd0b59d1ace6af28c15765102',
+     armv7l: '54077bfe2f8fbf4ea028c4b3917d39f3bc65397fd0b59d1ace6af28c15765102',
+       i686: '1533c2e1dcd9d88ba63786743498450b7e9ff7e04598a4f62a2852f10f628a18',
+     x86_64: 'f13b09f5627f12c3333268391cf9b7f2648429ebbbd196c9bf707b5ba7a8cc08'
   })
 
   def self.build
+    system 'autoreconf -fiv'
+    system 'filefix'
     system "./configure #{CREW_OPTIONS} #{CREW_ENV_OPTIONS}"
     system 'make'
   end
