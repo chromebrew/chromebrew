9afeeef6ad979557b43b2797743d85e123675e27
Satadru Pramanik <satadru@gmail.com>
2021-12-05 23:06:24 -0500

    update cairo and add i686 build

--- packages/cairo.rb
+++ packages/cairo.rb
@@ -3,23 +3,23 @@ require 'package'
 class Cairo < Package
   description 'Cairo is a 2D graphics library with support for multiple output devices.'
   homepage 'https://www.cairographics.org'
-  version '1.17.4-1'
+  version '1.17.5-a047'
   license 'LGPL-2.1 or MPL-1.1'
   compatibility 'all'
-  source_url 'https://cairographics.org/snapshots/cairo-1.17.4.tar.xz'
-  source_sha256 '74b24c1ed436bbe87499179a3b27c43f4143b8676d8ad237a6fa787401959705'
+  source_url 'https://gitlab.freedesktop.org/cairo/cairo.git'
+  git_hashtag 'a04786b9330109ce54bf7f65c7068281419cec6a'
 
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4-1_armv7l/cairo-1.17.4-1-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4-1_armv7l/cairo-1.17.4-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4-1_i686/cairo-1.17.4-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.4-1_x86_64/cairo-1.17.4-1-chromeos-x86_64.tpxz'
+    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.5-a047_i686/cairo-1.17.5-a047-chromeos-i686.tpxz',
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cairo/1.17.5-a047_x86_64/cairo-1.17.5-a047-chromeos-x86_64.tpxz'
   })
   binary_sha256({
     aarch64: 'd565cf9531e2248dc81df24bf667fb6798d72e92b3db77c86087137b3f63dd03',
      armv7l: 'd565cf9531e2248dc81df24bf667fb6798d72e92b3db77c86087137b3f63dd03',
-       i686: '070df0379e6f08ed51a8bee21c9d0929a584050b442e856dd17092d0e8c6e75e',
-     x86_64: '85538c1aac1a7a4cc4061a2b9d94e4dd04f51d9991e102025f145c936b169147'
+    i686: 'cc91b1482cf26d5eb23b88225f644f364e721c5fabe213a132da88444c13ade4',
+  x86_64: '9d24a1127d2d7103a0be565bf773c8f49952524c53b8a17c356ea30b213b911c'
   })
 
   depends_on 'fontconfig'
