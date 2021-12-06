1d1ab448b9d20266807ac2858ed31cb3d264870a
Ed Reel <edreel@gmail.com>
2021-12-05 22:36:45 -0600

    Packer 1.4.4 => 1.7.8

--- packages/packer.rb
+++ packages/packer.rb
@@ -1,32 +1,31 @@
-ENV['CREW_NOT_STRIP'] = 'true'
-
 require 'package'
 
 class Packer < Package
   description 'Packer is an open source tool for creating identical machine images for multiple platforms from a single source configuration.'
   homepage 'https://www.packer.io/'
-  version '1.4.4'
+  version '1.7.8'
   license 'Apache-2.0, BSD-2, BSD-4, MIT, MPL-2.0 and unicode'
   compatibility 'all'
-
-  case ARCH
-  when 'aarch64'
-    source_url 'https://releases.hashicorp.com/packer/1.4.4/packer_1.4.4_linux_arm64.zip'
-    source_sha256 'c6930cf5afdeb99df3ed5c9eeeef89fbcb3a1a71a9e17ebba16c873405ab72cd'
-  when 'armv7l'
-    source_url 'https://releases.hashicorp.com/packer/1.4.4/packer_1.4.4_linux_arm.zip'
-    source_sha256 '0cac826f983172aa836da65f76aa535fe7eacdece0510832ccfe3b51cb8dfe47'
-  when 'i686'
-    source_url 'https://releases.hashicorp.com/packer/1.4.4/packer_1.4.4_linux_386.zip'
-    source_sha256 'a1a3773305453c81c48a5f5c0d2df91d64a44c29063e3d5dccd6be77fef3bcea'
-  when 'x86_64'
-    source_url 'https://releases.hashicorp.com/packer/1.4.4/packer_1.4.4_linux_amd64.zip'
-    source_sha256 'b4dc37877a0fd00fc72ebda98977c2133be9ba6b26bcdd13b1b14a369e508948'
-  end
+  source_url ({
+    aarch64: 'https://releases.hashicorp.com/packer/1.7.8/packer_1.7.8_linux_arm.zip',
+     armv7l: 'https://releases.hashicorp.com/packer/1.7.8/packer_1.7.8_linux_arm.zip',
+       i686: 'https://releases.hashicorp.com/packer/1.7.8/packer_1.7.8_linux_386.zip',
+     x86_64: 'https://releases.hashicorp.com/packer/1.7.8/packer_1.7.8_linux_amd64.zip',
+  })
+  source_sha256 ({
+    aarch64: 'e9fce8a6ccaae3d7aecad55d06366730f215285bb4572acd308a09837ee4517f',
+     armv7l: 'e9fce8a6ccaae3d7aecad55d06366730f215285bb4572acd308a09837ee4517f',
+       i686: '46e49526e3900ee56c738c7a3415112a82af67f3627cc46cd85ee1b2316e4ffe',
+     x86_64: '8a94b84542d21b8785847f4cccc8a6da4c7be5e16d4b1a2d0a5f7ec5532faec0',
+  })
 
   def self.install
-    system "install -Dm755 packer #{CREW_DEST_PREFIX}/bin/packer"
+    ENV['CREW_NOT_STRIP'] = '1'
+    warn_level = $VERBOSE
+    $VERBOSE = nil
+    load "#{CREW_LIB_PATH}lib/const.rb"
+    $VERBOSE = warn_level
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.install 'packer', "#{CREW_DEST_PREFIX}/bin/packer", mode: 0o755
   end
 end
-
-ENV['CREW_NOT_STRIP'] = ''
