dbc212416d2029134513f1ef75b77dbda3ce9afc
Satadru Pramanik <satadru@gmail.com>
2021-12-08 17:25:46 -0500

    use cmake for c_ares

--- packages/c_ares.rb
+++ packages/c_ares.rb
@@ -16,20 +16,32 @@ class C_ares < Package
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/c_ares/1.18.1_x86_64/c_ares-1.18.1-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '05983b6d59990fed22205a11394c7674eb9f757c46490238919158a505498c22',
-     armv7l: '05983b6d59990fed22205a11394c7674eb9f757c46490238919158a505498c22',
-       i686: 'd482232284da08305ccd177c21a280851f95b61c3e8ce4d769b60c3d92d6c78c',
-     x86_64: '6dbb3a2793c6253a67a946af561c1a5cf8cf19bbd271ff4254ddbca747acb267'
+    aarch64: 'd2116cf4dc3432f0c14f9c9667e42b10c912c82b95269ab45afe9af5271d6df5',
+     armv7l: 'd2116cf4dc3432f0c14f9c9667e42b10c912c82b95269ab45afe9af5271d6df5',
+       i686: '1241aa581fae4395c242a79372098d34f640b33c5db9d8e114ad4aa87fc239c2',
+     x86_64: '9b7be17a55637cdcb7b956147b15269e0513ca954e298e679afd08ad8b4b17b3'
   })
 
+  def self.patch
+    system "sed -i 's/1.18.0/#{version}/g' CMakeLists.txt"
+  end
+
   def self.build
-    system 'autoreconf -fiv'
-    system 'filefix'
-    system "./configure #{CREW_OPTIONS} #{CREW_ENV_OPTIONS}"
-    system 'make'
+    FileUtils.mkdir('builddir')
+    Dir.chdir('builddir') do
+      system "cmake #{CREW_CMAKE_OPTIONS} \
+      -DCARES_STATIC=ON \
+      -DCARES_STATIC_PIC=ON \
+      -DCARES_BUILD_TESTS=OFF \
+      -DCARES_BUILD_TOOLS=ON \
+      -DCARES_SHARED=ON \
+      -DCMAKE_INTERPROCEDURAL_OPTIMIZATION=ON \
+      ../ -G Ninja"
+    end
+    system 'samu -C builddir'
   end
 
   def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
   end
 end
