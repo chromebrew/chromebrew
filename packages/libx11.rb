b19f8d16e2587dce83ba1e93dd1478dae76b38db
Satadru Pramanik <satadru@gmail.com>
2021-12-07 22:27:08 -0500

    add libx11 binaries

--- packages/libx11.rb
+++ packages/libx11.rb
@@ -10,12 +10,16 @@ class Libx11 < Package
   git_hashtag "libX11-#{version}"
 
   binary_url({
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libx11/1.7.2_i686/libx11-1.7.2-chromeos-i686.tpxz',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libx11/1.7.2_x86_64/libx11-1.7.2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libx11/1.7.2_armv7l/libx11-1.7.2-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libx11/1.7.2_armv7l/libx11-1.7.2-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libx11/1.7.2_i686/libx11-1.7.2-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libx11/1.7.2_x86_64/libx11-1.7.2-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    i686: '75c5a9329341c60e26bfd7983c89975b0f2d0f81261966a20ed0ec8735cc17f3',
-  x86_64: '329c60bc8ff483f079024654d81bcf932c54b79d7dd0314606a3f0d1b436be94'
+    aarch64: 'e066f82ad7b8578f672c472d8e132683426ebfc7707b036aa13891027b63fe8d',
+     armv7l: 'e066f82ad7b8578f672c472d8e132683426ebfc7707b036aa13891027b63fe8d',
+       i686: 'ad26fb5dbad41b0d1d8d3550fca22c48d3ca395bf5bb0440af5f6b701e1371df',
+     x86_64: '329c60bc8ff483f079024654d81bcf932c54b79d7dd0314606a3f0d1b436be94'
   })
 
   depends_on 'llvm' => :build
