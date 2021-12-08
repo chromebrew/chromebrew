8690c00ad224e420633116b121d6cb9763c393fe
Satadru Pramanik <satadru@gmail.com>
2021-12-08 17:37:30 -0500

    update xorg_cf_files

--- packages/xorg_cf_files.rb
+++ packages/xorg_cf_files.rb
@@ -3,23 +3,23 @@ require 'package'
 class Xorg_cf_files < Package
   description 'X.org cf files for use with imake builds.'
   homepage 'https://x.org'
-  version '1.0.6-1'
+  version '1.0.7'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://www.x.org/releases/individual/util/xorg-cf-files-1.0.6.tar.gz'
-  source_sha256 '6d56094e5d1a6c7d7a9576ac3a0fc2c042344509ea900d59f4b23df668b96c7a'
+  source_url 'https://www.x.org/releases/individual/util/xorg-cf-files-1.0.7.tar.gz'
+  source_sha256 'a49478ba0c2138bc53de38979cd2dee073b6fd6728597c552d266a707747f472'
 
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_cf_files/1.0.7_armv7l/xorg_cf_files-1.0.7-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_cf_files/1.0.7_armv7l/xorg_cf_files-1.0.7-chromeos-armv7l.tpxz',
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_cf_files/1.0.6-1_i686/xorg_cf_files-1.0.6-1-chromeos-i686.tpxz',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_cf_files/1.0.6-1_x86_64/xorg_cf_files-1.0.6-1-chromeos-x86_64.tpxz'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_cf_files/1.0.7_i686/xorg_cf_files-1.0.7-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xorg_cf_files/1.0.7_x86_64/xorg_cf_files-1.0.7-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '33f293b55cd416e2bd7be8658f1c1a19187d99e8e043ccb1aa42d5c8284fc8d0',
-     armv7l: '33f293b55cd416e2bd7be8658f1c1a19187d99e8e043ccb1aa42d5c8284fc8d0',
-    i686: 'd0360fd8c1db9eac92973e4a311fcb804476a5c581deb11e5b1cbe41af0a42b2',
-  x86_64: '8c8286394588d83387cb89afd5f687e84fd2e61a2d23d088176e60dc3d869b25'
+    aarch64: '9fa482169d3e253107419a1f05cb8e6d3e3c2f58846133c2b0f827cde7161565',
+     armv7l: '9fa482169d3e253107419a1f05cb8e6d3e3c2f58846133c2b0f827cde7161565',
+       i686: '265ed1b208b8a304e741a34c96160858df338269f3cbeb56fce312b5d3f101f2',
+     x86_64: 'a5547a9bff2657d49b928e1979b1abf2aa0c6de2740b1d710fb20f6f3013b7d8'
   })
 
   depends_on 'font_util'
