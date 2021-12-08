12bc94a09fed388fcecdf659b21bd7c37093e753
Ed Reel <edreel@gmail.com>
2021-12-08 10:21:53 -0600

    Update sublime packages (#6463)
    sublime_merge Build 1119 => Build 2063
    
    sublime_text 3.2.2 => Build 4121

--- packages/sublime_merge.rb
+++ packages/sublime_merge.rb
@@ -3,11 +3,11 @@ require 'package'
 class Sublime_merge < Package
   description 'A snappy UI, three-way merge tool,side-by-side diffs, syntax highlighting, and more.'
   homepage 'https://www.sublimemerge.com/'
-  version 'Build 1119'
-  license '' # Can't find license
+  version 'Build 2063'
+  license 'Copyright Sublime HQ Pty Ltd' # Shareware for evaluation purposes only.  Source code is not available.
   compatibility 'x86_64'
-  source_url 'https://download.sublimetext.com/sublime_merge_build_1119_x64.tar.xz'
-  source_sha256 '64b06320c0300923ade0901493cfd78e7f28598324f99d4c3609b3c85c5f1cea'
+  source_url 'https://download.sublimetext.com/sublime_merge_build_2063_x64.tar.xz'
+  source_sha256 '97abf170e210de442a3732e47f73dbb94dc3a437cb874b570a5fcb9c9b02b769'
 
   depends_on 'sommelier'
 
