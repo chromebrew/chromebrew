940f1ef3fd8418a2b2f06f391e9e46c47f712256
Satadru Pramanik <satadru@gmail.com>
2021-12-21 17:29:56 -0500

    Add i686 binary for polkit

--- packages/polkit.rb
+++ packages/polkit.rb
@@ -13,12 +13,14 @@ class Polkit < Package
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/polkit/0.120-7a99b_armv7l/polkit-0.120-7a99b-chromeos-armv7l.tpxz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/polkit/0.120-7a99b_armv7l/polkit-0.120-7a99b-chromeos-armv7l.tpxz',
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/polkit/0.120-7a99b_x86_64/polkit-0.120-7a99b-chromeos-x86_64.tpxz'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/polkit/0.120-7a99b_i686/polkit-0.120-7a99b-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/polkit/0.120-7a99b_x86_64/polkit-0.120-7a99b-chromeos-x86_64.tpxz'
   })
   binary_sha256({
     aarch64: '41b4b9c4dc68b2f1960f571f09632422187fb3d726fccf90fe48e48539564e4c',
      armv7l: '41b4b9c4dc68b2f1960f571f09632422187fb3d726fccf90fe48e48539564e4c',
-    x86_64: 'ced0c15bbae3c494e287a217268fd5cb62e133d71a347b6d8224392f96c2d43f'
+       i686: 'dc377359a5b4c77cd0fa429ff11134614b5c6766cb55dfbf85b4a3bcb29ba77c',
+     x86_64: 'ced0c15bbae3c494e287a217268fd5cb62e133d71a347b6d8224392f96c2d43f'
   })
 
   depends_on 'js91'
