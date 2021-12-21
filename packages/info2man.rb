1b296b9169840f0a63a93df1a00d7aec017309f7
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-12-20 22:10:26 -0800

    use perl_version variable, remove newline in perl code, fix info2man shebang

--- packages/info2man.rb
+++ packages/info2man.rb
@@ -4,6 +4,7 @@ class Info2man < Package
   description 'Convert GNU info files to POD or man pages.'
   homepage 'https://www.cskk.ezoshosting.com/cs/css/info2pod.html'
   @_ver = '1.1-10'
+  @_perl_version = %x[echo "printf '%vd', $^V;" | perl].to_s
   version @_ver
   license 'unknown'
   compatibility 'all'
@@ -12,15 +13,16 @@ class Info2man < Package
 
   def self.patch
     system "sed -i 's:#!/usr/bin/perl:#!#{CREW_PREFIX}/bin/perl:g' 0README.txt info2pod"
+    system "sed -i 's:#!/bin/sh:#!/usr/bin/env sh:g' info2man"
   end
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin/"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/lib/perl5/site_perl/#{%x[echo "printf '%vd\n', $^V;" | perl].chomp}/"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/lib/perl5/site_perl/#{@_perl_version}/"
     FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1/"
     FileUtils.install 'info2man', "#{CREW_DEST_PREFIX}/bin/info2man", mode: 0o755
     FileUtils.install 'info2pod', "#{CREW_DEST_PREFIX}/bin/info2pod", mode: 0o755
-    FileUtils.cp_r 'cs', "#{CREW_DEST_PREFIX}/lib/perl5/site_perl/#{%x[echo "printf '%vd\n', $^V;" | perl].chomp}/"
+    FileUtils.cp_r 'cs', "#{CREW_DEST_PREFIX}/lib/perl5/site_perl/#{@_perl_version}/"
     FileUtils.install 'debian/info2man.1', "#{CREW_DEST_MAN_PREFIX}/man1/info2man.1"
     FileUtils.install 'debian/info2pod.1', "#{CREW_DEST_MAN_PREFIX}/man1/info2pod.1"
   end
