cc078d84e5878f6a7f4cc4204fd3095a5c3fc4cd
Satadru Pramanik <satadru@gmail.com>
2021-12-21 19:36:43 -0500

    opam -> 2.1.2

--- packages/opam.rb
+++ packages/opam.rb
@@ -6,23 +6,23 @@ require 'package'
 class Opam < Package
   description 'OCaml package manager'
   homepage 'https://opam.ocaml.org/'
-  @_ver = '2.1.1'
-  version "#{@_ver}-2"
+  @_ver = '2.1.2'
+  version @_ver
   compatibility 'all'
   source_url 'https://github.com/ocaml/opam.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1-2_armv7l/opam-2.1.1-2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1-2_armv7l/opam-2.1.1-2-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1-2_i686/opam-2.1.1-2-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1-2_x86_64/opam-2.1.1-2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_armv7l/opam-2.1.2-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_armv7l/opam-2.1.2-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_i686/opam-2.1.2-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_x86_64/opam-2.1.2-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: 'ef542a60a56abda5549b9e0a07e76898315dd07149bda008f0498b5244ec79b8',
-     armv7l: 'ef542a60a56abda5549b9e0a07e76898315dd07149bda008f0498b5244ec79b8',
-       i686: '380e81c2ce976bf42ccfc18050598c50bf282b157a269f4dd179d0410ab2dbb9',
-     x86_64: 'cb95bae566579fcf5d1952afe41c89d40932a10458e90c00a0f812ae9ef0f43f'
+    aarch64: 'dc177f2bb38e12fd6ce7c697117a661edc6ad67ca9b7249c7661c43f8e218a52',
+     armv7l: 'dc177f2bb38e12fd6ce7c697117a661edc6ad67ca9b7249c7661c43f8e218a52',
+       i686: '57f36dfc182d5719229f8ef883be8e878160ab116bb8c004a8ce3bdf5a802dd1',
+     x86_64: '612c10ea82c335d79c6a06e5abf9da93e873f3419574a7a93a17fa284c913daf'
   })
 
   depends_on 'bubblewrap'
