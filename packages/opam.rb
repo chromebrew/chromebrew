ad876761c33b8ef10f7ea059d0aafdbbaa111ac0
Satadru Pramanik <satadru@umich.edu>
2021-12-23 01:24:21 -0500

    Add wayland_proxy_virtwl (a sommelier alternative) (#6423)
    * wayland_proxy_virtwl working build
    
    * Adjust sample options in postinstall
    
    * fix patching
    
    * Add new binaries
    
    * Adjust example usge suggestion
    
    * refresh armv7l binaries
    
    * remove .to_s
    
    * Test upstream changes
    
    * testing
    
    * switch to upstream patched commit
    
    * opam -> 2.1.2
    
    * new binaries
    
    * requested changes

--- packages/opam.rb
+++ packages/opam.rb
@@ -6,71 +6,68 @@ require 'package'
 class Opam < Package
   description 'OCaml package manager'
   homepage 'https://opam.ocaml.org/'
-  @_ver = '2.1.1'
+  @_ver = '2.1.2'
   version @_ver
   compatibility 'all'
   source_url 'https://github.com/ocaml/opam.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1_armv7l/opam-2.1.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1_armv7l/opam-2.1.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1_i686/opam-2.1.1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.1_x86_64/opam-2.1.1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_armv7l/opam-2.1.2-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_armv7l/opam-2.1.2-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_i686/opam-2.1.2-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opam/2.1.2_x86_64/opam-2.1.2-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '53d3b4e436c1a70f5772523f0af5454aa54ad7dfb777d50fe7495d31160b7462',
-     armv7l: '53d3b4e436c1a70f5772523f0af5454aa54ad7dfb777d50fe7495d31160b7462',
-       i686: '4d8e3979729315249b0a9dabf795108c91d16618dd5f38285dc758eabf3896d1',
-     x86_64: 'f0cced8d7504be9b5bbd2f98d346e28b9cd18cd8b02985722643854a5cd5d51f'
+    aarch64: '596b3928ed6ebf82b865314ff0601f40f6e2bd8d9d2a0296ab6f45da1afc29cf',
+     armv7l: '596b3928ed6ebf82b865314ff0601f40f6e2bd8d9d2a0296ab6f45da1afc29cf',
+       i686: 'e21ee0b4989a8d18cc6ea6a0da59c8e214abe3de96104636b6d323195a69f393',
+     x86_64: 'bb2a382e59f36eb109eaaa0e471f0e9bc8f74823703fa9e9e0b0e11dfed6d9e7'
   })
 
   depends_on 'bubblewrap'
   depends_on 'ocaml'
   depends_on 'rsync'
 
+  @OPAMROOT = "#{CREW_PREFIX}/share/opam"
+
   def self.build
     system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
     system "make lib-ext all -j1 \
       OCAMLC='ocamlc -unsafe-string' \
       OCAMLOPT='ocamlopt -unsafe-string'"
-    @bashd_opam = <<~EOF
-      export OPAMROOT=#{CREW_PREFIX}/opam
-      eval \$(opam env --root=#{CREW_PREFIX}/opam --switch=default)
-      test -r #{CREW_PREFIX}/opam/opam-init/init.sh && . #{CREW_PREFIX}/opam/opam-init/init.sh > /dev/null 2> /dev/null || true
-    EOF
+    @bashd_opam = <<~OPAMEOF
+      export OPAMROOT=#{@OPAMROOT}
+      eval \$(opam env --root=#{@OPAMROOT} --switch=default)
+      test -r #{@OPAMROOT}/opam-init/init.sh && . #{@OPAMROOT}/opam-init/init.sh &> /dev/null || true
+    OPAMEOF
   end
 
   def self.install
-    ENV['CREW_FHS_NONCOMPLIANCE_ONLY_ADVISORY'] = '1'
-    warn_level = $VERBOSE
-    $VERBOSE = nil
-    load "#{CREW_LIB_PATH}lib/const.rb"
-    $VERBOSE = warn_level
     system "make DESTDIR=#{CREW_DEST_DIR} install"
     FileUtils.mkdir_p %W[#{CREW_DEST_PREFIX}/etc/bash.d]
     File.write("#{CREW_DEST_PREFIX}/etc/bash.d/opam", @bashd_opam)
   end
 
   def self.postinstall
-    system "opam init --root=#{CREW_PREFIX}/opam -y \
-            && eval \$(opam env --root=#{CREW_PREFIX}/opam --switch=default) \
-            && opam option --global depext=false --root=#{CREW_PREFIX}/opam -y"
+    system "opam init --root=#{@OPAMROOT} -y \
+            && eval \$(opam env --root=#{@OPAMROOT} --switch=default) \
+            && opam option --global depext=false --root=#{@OPAMROOT} -y"
   end
 
   def self.remove
-    if Dir.exist? "#{CREW_PREFIX}/opam"
-      puts
-      print "Would you like to remove #{CREW_PREFIX}/opam? [y/N] "
-      response = $stdin.getc
-      case response
-      when 'y', 'Y'
-        FileUtils.rm_rf "#{CREW_PREFIX}/opam"
-        puts "#{CREW_PREFIX}/opam removed.".lightred
-      else
-        puts "#{CREW_PREFIX}/opam saved.".lightgreen
-      end
-      puts
+    return unless Dir.exist? @OPAMROOT
+
+    puts
+    print "Would you like to remove #{@OPAMROOT}? [y/N] "
+    response = $stdin.getc
+    case response
+    when 'y', 'Y'
+      FileUtils.rm_rf @OPAMROOT
+      puts "#{@OPAMROOT} removed.".lightred
+    else
+      puts "#{@OPAMROOT} saved.".lightgreen
     end
+    puts
   end
 end
