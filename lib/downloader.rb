8fc7b84aaca6a07ae94b672990c07de5b13b43e7
supechicken <supechicken666@gmail.com>
2021-12-24 11:53:50 +0800

    Add a downloader based on the `net/http` library (#6491)
    * Use net http (#3)
    
    * Add user agent header
    
    * Update crew
    
    * Update downloader.rb
    
    * Update const.rb
    
    * Fix const.rb crash.
    
    Co-authored-by: Satadru Pramanik <satadru@umich.edu>
    Co-authored-by: Satadru Pramanik <satadru@gmail.com>

---
+++ lib/downloader.rb
@@ -0,0 +1,78 @@
+require 'io/console'
+require 'net/http'
+require 'uri'
+require_relative 'const'
+require_relative 'color'
+require_relative 'convert_size'
+
+def setTermSize
+  # setTermSize: set progress bar size based on terminal width
+  # get terminal window size
+  @termH, @termW = IO.console.winsize
+  # space for progress bar after minus the reserved space for showing
+  # the file size and progress percentage
+  @progBarW = @termW - 17
+  return true
+end
+
+def downloader (url, filename = File.basename(url), retry_count = 0, verbose = false)
+  setTermSize
+  # reset width settings after terminal resized
+  trap('WINCH') { setTermSize }
+  uri = URI(url)
+
+  Net::HTTP.start(uri.host, uri.port, {
+      max_retries: 3,
+      use_ssl: uri.scheme.eql?('https'),
+      ca_file: SSL_CERT_FILE,
+      ca_path: SSL_CERT_DIR
+  }) do |http|
+    http.request( Net::HTTP::Get.new(uri) ) do |response|
+      case
+      when response.is_a?(Net::HTTPOK)
+      when response.is_a?(Net::HTTPRedirection) # follow HTTP redirection
+        puts <<~EOT if verbose
+          * Follow HTTP redirection: #{response['Location']}
+          *
+        EOT
+        return downloader(response['Location'], filename, retry_count, verbose)
+      else
+        abort "Download failed with error #{response.code}: #{response.msg}".lightred
+      end
+
+      # get target file size (should be returned by the server)
+      file_size = response['Content-Length'].to_i
+      downloaded_size = 0
+
+      if verbose
+        puts <<~EOT
+          * Connected to #{uri.host} port #{uri.port}
+          * HTTPS: #{uri.scheme.eql?('https')}
+          *
+        EOT
+
+        response.to_hash.each_pair do |k, v|
+          puts "> #{k}: #{v}"
+        end
+        puts
+      end
+
+      File.open(filename, 'wb') do |io|
+        response.read_body do |chunk|
+          downloaded_size += chunk.size
+          if file_size.positive?
+            # calculate downloading progress percentage with the given file size
+            percentage = (downloaded_size.to_f / file_size.to_f) * 100
+            # show progress bar, file size and progress percentage
+            printf "\r[%-#{@progBarW}s] %9.9s %3d%%",
+                   '#' * ( @progBarW * (percentage / 100) ).to_i,
+                   human_size(file_size),
+                   percentage
+          end
+          io.write(chunk)
+        end
+      end
+      puts
+    end
+  end
+end
