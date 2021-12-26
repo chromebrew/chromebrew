fff686a3112eccbe8654aa3a583b852d2efbfabd
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2021-12-26 11:26:15 -0800

    update some python packages (#6473)
    * update py3_cryptography
    
    * update py3_cython
    
    * update py3_nuitka
    
    * update py3_packaging
    
    * update py3_pip
    
    * update py3_setuptools
    
    * update py3_setuptools_rust
    
    * add binaries + py3_typing_extensions
    
    * nuitka => 0.6.18.5, and use pypi tarball
    
    * Add py3_nuitka
    
    Co-authored-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_cryptography.rb
+++ packages/py3_cryptography.rb
@@ -3,7 +3,7 @@ require 'package'
 class Py3_cryptography < Package
   description 'Cryptography provides cryptographic recipes and primitives to Python developers.'
   homepage 'https://cryptography.io/'
-  @_ver = '35.0.0'
+  @_ver = '36.0.0'
   version @_ver
   license 'MIT'
   compatibility 'all'
@@ -11,20 +11,21 @@ class Py3_cryptography < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/35.0.0_armv7l/py3_cryptography-35.0.0-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/35.0.0_armv7l/py3_cryptography-35.0.0-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/35.0.0_i686/py3_cryptography-35.0.0-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/35.0.0_x86_64/py3_cryptography-35.0.0-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/36.0.0_armv7l/py3_cryptography-36.0.0-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/36.0.0_armv7l/py3_cryptography-36.0.0-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/36.0.0_i686/py3_cryptography-36.0.0-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cryptography/36.0.0_x86_64/py3_cryptography-36.0.0-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '565e29e043e7fb83c7cd02c619b2611620689959e1c83ab27a7827b943382f40',
-     armv7l: '565e29e043e7fb83c7cd02c619b2611620689959e1c83ab27a7827b943382f40',
-       i686: 'a3bd7ca74b310b4581658eb9ee97c6358f78fb2e21305be74f43cd38014889c9',
-     x86_64: 'b4980330c51dfcfc6f1d8fb9cdd15bed2081db54331db70c805bf5cd5ab88a96'
+    aarch64: '9671703e41b0eba9780b2b1dbd4fa53c5509119bd06fed1b9f320169945ebdb7',
+     armv7l: '9671703e41b0eba9780b2b1dbd4fa53c5509119bd06fed1b9f320169945ebdb7',
+       i686: '0add4c30b9b356e7db03071eb02c5151c5978b31239e11879b53f18d49c5392f',
+     x86_64: '888da2eea6ed932f686569af840e3bbdbfd24ca4e164f783c78a5180c191a536'
   })
 
   depends_on 'py3_cffi'
   depends_on 'py3_six'
+  depends_on 'py3_typing_extensions'
   depends_on 'py3_setuptools_rust' => :build
   depends_on 'py3_setuptools' => :build
   depends_on 'rust' => :build
--- packages/py3_nuitka.rb
+++ packages/py3_nuitka.rb
@@ -3,24 +3,24 @@ require 'package'
 class Py3_nuitka < Package
   description 'Nuitka is a Python to binary compiler written in Python. You feed it your Python app, it does a lot of clever things, and spits out an executable or extension module.'
   homepage 'https://nuitka.net/'
-  @_ver = '0.6.17.2'
+  @_ver = '0.6.18.5'
   version @_ver
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://github.com/Nuitka/Nuitka.git'
-  git_hashtag @_ver
+  source_url 'https://files.pythonhosted.org/packages/78/b3/e8b6f9d728454a1be16bcc4921e5ea55804587d89e8f8637c89d33ae11da/Nuitka-0.6.18.5.tar.gz'
+  source_sha256 'a9c7b49d0bd6ae73fe299772b0f2ae95e6d5d9a20de128932bcc5b92826f0ea7'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.17.2_armv7l/py3_nuitka-0.6.17.2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.17.2_armv7l/py3_nuitka-0.6.17.2-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.17.2_i686/py3_nuitka-0.6.17.2-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.17.2_x86_64/py3_nuitka-0.6.17.2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.18.5_armv7l/py3_nuitka-0.6.18.5-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.18.5_armv7l/py3_nuitka-0.6.18.5-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.18.5_i686/py3_nuitka-0.6.18.5-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_nuitka/0.6.18.5_x86_64/py3_nuitka-0.6.18.5-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: 'e7b1cf49bba389bb9d4022e33f142dce6cb8238640ab66215e5f87320a2efc24',
-     armv7l: 'e7b1cf49bba389bb9d4022e33f142dce6cb8238640ab66215e5f87320a2efc24',
-       i686: 'ac254fd32d7b2cb3e258e8a5abcc0b3424de67f90cc6802f393042108f2e0d06',
-     x86_64: 'd9f3030fa1f7bea0b9daac1dd2ae6fd2e7cd4cd469ee30ecd70f167c705206c7'
+    aarch64: '28fcd09039c0d12758806a8d8083ec410ce73c6382581906cf4934fb6f4ebff4',
+     armv7l: '28fcd09039c0d12758806a8d8083ec410ce73c6382581906cf4934fb6f4ebff4',
+       i686: 'bd1f33a43e05eb874706a1b90bfc80dcdd085442cf6c6414bd6c25f7b71e9faa',
+     x86_64: '26a84ef600a85bed53bb9f5cf8eb6202c8686389908061588f08fa52f9b6737a'
   })
 
   depends_on 'py3_setuptools' => :build
