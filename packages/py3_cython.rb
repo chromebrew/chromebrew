a6573496ce3e25a980936228c3d84acf8fe8bfda
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2021-12-26 11:26:15 -0800

    update some python packages (#6473)
    * update py3_cryptography
    
    * update py3_cython
    
    * update py3_nuitka
    
    * update py3_packaging
    
    * update py3_pip
    
    * update py3_setuptools
    
    * update py3_setuptools_rust
    
    * add binaries + py3_typing_extensions
    
    * nuitka => 0.6.18.5, and use pypi tarball
    
    * Add py3_nuitka
    
    Co-authored-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_cython.rb
+++ packages/py3_cython.rb
@@ -3,7 +3,7 @@ require 'package'
 class Py3_cython < Package
   description 'The Cython language makes writing C extensions for the Python language as easy as Python itself.'
   homepage 'https://cython.org/'
-  @_ver = '0.29.24'
+  @_ver = '0.29.25'
   version @_ver
   license 'Apache-2.0'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Py3_cython < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.24_armv7l/py3_cython-0.29.24-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.24_armv7l/py3_cython-0.29.24-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.24_i686/py3_cython-0.29.24-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.24_x86_64/py3_cython-0.29.24-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.25_armv7l/py3_cython-0.29.25-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.25_armv7l/py3_cython-0.29.25-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.25_i686/py3_cython-0.29.25-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_cython/0.29.25_x86_64/py3_cython-0.29.25-chromeos-x86_64.tpxz'
   })
   binary_sha256({
-    aarch64: '015010c91705dda3e05a05ffc0f5ee62b460e67d5dc58c4b50e7412cd5d528b4',
-     armv7l: '015010c91705dda3e05a05ffc0f5ee62b460e67d5dc58c4b50e7412cd5d528b4',
-       i686: 'f738a39df78d2b74cc58b4d25a6f66b857328a1887035bdaaccc1e78ab131a63',
-     x86_64: 'a15ac94aa1a7da63465a28d2c91f9c1494a08762894d9b1092e9b86cfa89466b'
+    aarch64: '42ff5324e9f2f70b0ee4445902ee9f2045dca5fe92dfa99012415472dfd18548',
+     armv7l: '42ff5324e9f2f70b0ee4445902ee9f2045dca5fe92dfa99012415472dfd18548',
+       i686: '14cdcdcf993e6e4302582ac8285d00e4f4b216ccad741d66b3def5381e23f859',
+     x86_64: '1e8c3908182c4bfefc6df5cc5ab5594432952cc2b43af24ab46cb8f93f57b09d'
   })
 
   depends_on 'py3_setuptools' => :build
