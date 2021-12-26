59e6f1918d172a5ad45e31164e8fdaeb501a4a26
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2021-12-25 19:26:32 -0800

    gnupg => 2.3.4

--- packages/gnupg.rb
+++ packages/gnupg.rb
@@ -3,24 +3,11 @@ require 'package'
 class Gnupg < Package
   description 'GnuPG is a complete and free implementation of the OpenPGP standard as defined by RFC4880 (also known as PGP).'
   homepage 'https://gnupg.org/'
-  version '2.2.7'
+  version '2.3.4'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.7.tar.bz2'
-  source_sha256 'd95b361ee6ef7eff86af40c8c72bf9313736ac9f7010d6604d78bf83818e976e'
-
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnupg/2.2.7_armv7l/gnupg-2.2.7-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnupg/2.2.7_armv7l/gnupg-2.2.7-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnupg/2.2.7_i686/gnupg-2.2.7-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnupg/2.2.7_x86_64/gnupg-2.2.7-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-    aarch64: 'e356625feaef9e93d4d0946635db862df13c5f555636b38c5504042fa4ff3c23',
-     armv7l: 'e356625feaef9e93d4d0946635db862df13c5f555636b38c5504042fa4ff3c23',
-       i686: '32060d1d69ac5b44ad7f7fe7e6a2744404f972b26b28ba2999e70c12f9c20dc7',
-     x86_64: 'eaa2a33957ce00de10e0655b326ba1d2d9512b749e1d81cbf205c4c6e064a729',
-  })
+  source_url 'https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.4.tar.bz2'
+  source_sha256 'f3468ecafb1d7f9ad7b51fd1db7aebf17ceb89d2efa8a05cf2f39b4d405402ae'
 
   depends_on 'bz2'
   depends_on 'libassuan'
@@ -29,13 +16,20 @@ class Gnupg < Package
   depends_on 'pinentry'
 
   def self.build
-    system './configure',
-      "--prefix=#{CREW_PREFIX}",
-      "--libdir=#{CREW_LIB_PREFIX}"
+    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS} \
+              --enable-all-tests \
+              --with-capabilities \
+              --with-zlib \
+              --with-bzip2 \
+              --with-readline"
     system 'make'
   end
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+
+  def self.check
+    system 'make', 'check'
   end
 end
