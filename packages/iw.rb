6274b8267bf102c3fd8d84d8fc3289f40161bf54
Satadru Pramanik <satadru@gmail.com>
2021-12-28 09:59:24 -0500

    add binaries

--- packages/iw.rb
+++ packages/iw.rb
@@ -8,7 +8,20 @@ class Iw < Package
   license 'ISC'
   compatibility 'all'
   source_url 'https://git.kernel.org/pub/scm/linux/kernel/git/jberg/iw.git'
-  git_hashtag 'v' + @_ver
+  git_hashtag "v#{@_ver}"
+
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/iw/5.16_armv7l/iw-5.16-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/iw/5.16_armv7l/iw-5.16-chromeos-armv7l.tpxz',
+    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/iw/5.16_i686/iw-5.16-chromeos-i686.tpxz',
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/iw/5.16_x86_64/iw-5.16-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    aarch64: '65fc4e003fb7dd5fc962ffbc6d50dd7aec0afae7dbfb8301bdf30a9855bb63bf',
+     armv7l: '65fc4e003fb7dd5fc962ffbc6d50dd7aec0afae7dbfb8301bdf30a9855bb63bf',
+    i686: '7710c556d73687e599354e0932479e2f63b50317f016813d3735dbe17998a9f9',
+  x86_64: '8aa9b00ac829c5f9ba048ec175311c7b453dfc432d63a681d393666a630ad932'
+  })
 
   depends_on 'libnl3'
 
@@ -17,14 +30,14 @@ class Iw < Package
   end
 
   def self.build
-    system "make"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
-    system "make", "check"
+    system 'make', 'check'
   end
 end
