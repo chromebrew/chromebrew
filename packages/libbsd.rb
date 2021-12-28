a82230fdd842a9765422b0db0e87abb5642605af
Satadru Pramanik <satadru@gmail.com>
2021-12-28 15:51:43 -0500

    add binaries

--- packages/libbsd.rb
+++ packages/libbsd.rb
@@ -10,6 +10,19 @@ class Libbsd < Package
   source_url 'https://git.hadrons.org/git/libbsd.git'
   git_hashtag @_ver
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libbsd/0.11.3_armv7l/libbsd-0.11.3-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libbsd/0.11.3_armv7l/libbsd-0.11.3-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libbsd/0.11.3_i686/libbsd-0.11.3-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libbsd/0.11.3_x86_64/libbsd-0.11.3-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    aarch64: '391cf3f07f66a315e7da44504dc3e713c93a541b92e94e87a0383382f674113d',
+     armv7l: '391cf3f07f66a315e7da44504dc3e713c93a541b92e94e87a0383382f674113d',
+       i686: 'ae08dd18ad0d73e21166745bf5736f4248a42b224d3f0a94736c3172cbfb7712',
+     x86_64: '55da730b6d52c086cb934546543475e298851c1060c5838bede71826631c566f'
+  })
+
   depends_on 'libmd'
 
   def self.build
@@ -17,14 +30,14 @@ class Libbsd < Package
     system 'autoupdate'
     system 'autoreconf -fiv'
     system "#{CREW_ENV_FNO_LTO_OPTIONS} ./configure #{CREW_OPTIONS}"
-    system "make"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
-    system "make", "check"
+    system 'make', 'check'
   end
 end
--- packages/libmd.rb
+++ packages/libmd.rb
@@ -10,18 +10,31 @@ class Libmd < Package
   source_url 'https://git.hadrons.org/git/libmd.git'
   git_hashtag @_ver
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmd/1.0.4_armv7l/libmd-1.0.4-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmd/1.0.4_armv7l/libmd-1.0.4-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmd/1.0.4_i686/libmd-1.0.4-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmd/1.0.4_x86_64/libmd-1.0.4-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    aarch64: 'd7f7fb355eeffac5bed6e547812810e56b722663262e8059048e3e66da1610b4',
+     armv7l: 'd7f7fb355eeffac5bed6e547812810e56b722663262e8059048e3e66da1610b4',
+       i686: 'd07ff3785775f604b95d0bfec9e3fefe054dc8b9131da983c93033ae4b5042c1',
+     x86_64: '672b5ccf0a9b2188476b6c00940f93c2f8615c55694871e399076a6b7527cdba'
+  })
+
   def self.build
     system 'autoupdate'
     system 'autoreconf -fiv'
     system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
-    system "make"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
-    system "make", "check"
+    system 'make', 'check'
   end
 end
