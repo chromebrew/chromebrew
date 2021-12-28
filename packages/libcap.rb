0847e3d8c29af63f9c44f8e9e79d19d65a7ae490
Satadru Pramanik <satadru@gmail.com>
2021-12-28 16:25:25 -0500

    requested changes

--- packages/libcap.rb
+++ packages/libcap.rb
@@ -5,14 +5,13 @@ class Libcap < Package
   homepage 'https://directory.fsf.org/wiki/Libcap/'
   @_ver = '2.62'
   version @_ver
-  license 'GPL-2 or BSD'
   compatibility 'all'
-  source_url "https://git.kernel.org/pub/scm/libs/libcap/libcap.git"
-  git_hashtag 'libcap-' + @_ver
+  license 'GPL-2 or BSD'
+  source_url 'https://git.kernel.org/pub/scm/libs/libcap/libcap.git'
+  git_hashtag "libcap-#{@_ver}"
 
   depends_on 'gperf' => :build
   depends_on 'linux_pam'
-  depends_on 'patchelf' => :build
 
   def self.build
     # add includes option to make it work with gperf-3.1
@@ -24,7 +23,7 @@ class Libcap < Package
     # set exec_prefix
     system "sed -i 's,^exec_prefix=,exec_prefix=\$(prefix),g' Make.Rules"
     # use system libdir
-    system "sed -i 's,^lib_prefix=$(exec_prefix),lib_prefix=#{CREW_LIB_PREFIX},g' Make.Rules"
+    system "sed -i 's,^lib_prefix=\$(exec_prefix),lib_prefix=#{CREW_LIB_PREFIX},g' Make.Rules"
     # http://git.yoctoproject.org/cgit.cgi/poky/plain/meta/recipes-support/libcap/files/0001-ensure-the-XATTR_NAME_CAPS-is-defined-when-it-is-use.patch
     system 'sed -i "s,^\#ifdef VFS_CAP_U32,\#if defined (VFS_CAP_U32) \&\& defined (XATTR_NAME_CAPS),g" libcap/cap_file.c'
     system "#{CREW_ENV_OPTIONS} make"
@@ -32,7 +31,6 @@ class Libcap < Package
 
   def self.install
     system 'make', 'RAISE_SETFCAP=no', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    #system "patchelf --set-rpath #{CREW_LIB_PREFIX} #{CREW_DEST_PREFIX}/sbin/capsh"
   end
 
   def self.check
