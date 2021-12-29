94bfb9a99695bd9c868a89b7cf65d7e9f6b5187b
Ed Reel <edreel@gmail.com>
2021-12-29 06:14:45 -0600

    Revert #6495 (#6537)

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.20.1'
+CREW_VERSION = '1.20.0'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
