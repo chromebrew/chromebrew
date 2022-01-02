3ca3a047358ed76d7864e09bcb5e099db6294b49
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-01-01 18:14:52 -0800

    build fd from the source (#6477)
    * build fd from the source
    
    * fix finding fd.bash
    
    * Add binaries
    
    Co-authored-by: Satadru Pramanik <satadru@gmail.com>

--- packages/fd.rb
+++ packages/fd.rb
@@ -1,42 +1,47 @@
 require 'package'
 
 class Fd < Package
-  description "A simple, fast and user-friendly alternative to 'find'"
-  homepage 'https://github.com/sharkdp/fd'
-  version '8.3.0'
-  license 'Apache License Version 2.0'
+  description "A simple, fast and user-friendly alternative to 'find'."
+  homepage 'https://github.com/sharkdp/fd/'
+  @_ver = '8.3.0'
+  version "#{@_ver}-1"
+  license 'Apache-2.0 and MIT'
   compatibility 'all'
-  source_url ({
-    aarch64: 'https://github.com/sharkdp/fd/releases/download/v8.3.0/fd-v8.3.0-arm-unknown-linux-musleabihf.tar.gz',
-     armv7l: 'https://github.com/sharkdp/fd/releases/download/v8.3.0/fd-v8.3.0-arm-unknown-linux-musleabihf.tar.gz',
-       i686: 'https://github.com/sharkdp/fd/releases/download/v8.3.0/fd-v8.3.0-i686-unknown-linux-musl.tar.gz',
-     x86_64: 'https://github.com/sharkdp/fd/releases/download/v8.3.0/fd-v8.3.0-x86_64-unknown-linux-musl.tar.gz',
+  source_url 'https://github.com/sharkdp/fd.git'
+  git_hashtag "v#{@_ver}"
+
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fd/8.3.0-1_armv7l/fd-8.3.0-1-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fd/8.3.0-1_armv7l/fd-8.3.0-1-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fd/8.3.0-1_i686/fd-8.3.0-1-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fd/8.3.0-1_x86_64/fd-8.3.0-1-chromeos-x86_64.tpxz'
   })
-  source_sha256 ({
-    aarch64: '14646b148e3cfbdaae667b32cf5eea48cf11e6ddc3dc0d845177c13053a316ff',
-     armv7l: '14646b148e3cfbdaae667b32cf5eea48cf11e6ddc3dc0d845177c13053a316ff',
-       i686: '279038bf1a9703934e3bddf2fa359af1445d3bbe4ab80e758ab5c58d96b0dd0e',
-     x86_64: 'ff36894153f352a29ecf0efd97adcd3ac788053801726861c6db4bdec1aa8461',
+  binary_sha256({
+    aarch64: '1325765ea06369318274a4c9432b22cff9a6714b15afd7c6d61ab833166852f3',
+     armv7l: '1325765ea06369318274a4c9432b22cff9a6714b15afd7c6d61ab833166852f3',
+       i686: '7c39bc4ee7d48d65baea2e20a5573ba48a285c00a6e1816809a04749f657a15a',
+     x86_64: '076f8e237125bcd488bdcf72a4f6a6f3e0786de795fb19444ec30c288f421965'
   })
 
+  depends_on 'rust' => :build
+
   def self.build
-    File.write('fd.bash', "source #{CREW_PREFIX}/share/fd/autocomplete/fd.bash-completion")
+    system 'cargo build --release'
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/fd"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/bash.d"
-    FileUtils.mv 'autocomplete', "#{CREW_DEST_PREFIX}/share/fd"
-    FileUtils.install 'fd', "#{CREW_DEST_PREFIX}/bin/fd", mode: 0o755
-    FileUtils.install 'fd.1', "#{CREW_DEST_MAN_PREFIX}/man1/fd.1", mode: 0o644
-    FileUtils.install 'fd.bash', "#{CREW_DEST_PREFIX}/etc/bash.d/fd.bash", mode: 0o644
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin/"
+    FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1/"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/bash-completion/completions/"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/zsh/site-functions/"
+    FileUtils.install 'target/release/fd', "#{CREW_DEST_PREFIX}/bin/fd", mode: 0o755
+    FileUtils.install `find . -name 'fd.bash' -type f`.to_s.chomp,
+                      "#{CREW_DEST_PREFIX}/share/bash-completion/completions/fd"
+    FileUtils.install 'doc/fd.1', "#{CREW_DEST_MAN_PREFIX}/man1/fd.1"
+    FileUtils.install 'contrib/completion/_fd', "#{CREW_DEST_PREFIX}/share/zsh/site-functions/_fd"
   end
 
-  def self.postinstall
-    puts "\nTo enable bash completion, execute the following:".lightblue
-    puts "source ~/.bashrc".lightblue
-    puts "\nType 'fd -h' to get started.\n".lightblue
+  def self.check
+    system 'cargo test --release'
   end
 end
