293a75fc6c328c06487b5f701876528614f4c7bc
Satadru Pramanik <satadru@gmail.com>
2022-01-04 12:01:42 -0500

    crew: Make renaming .la files optional during builds

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.20.4'
+CREW_VERSION = '1.20.5'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
