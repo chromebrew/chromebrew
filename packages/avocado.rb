0461a69da95b010fdbde742c15a7c1e0bf4eb554
Ed Reel <edreel@gmail.com>
2022-01-09 08:40:55 -0600

    Avocado 91.0 => 94.0 (#6574)

--- packages/avocado.rb
+++ packages/avocado.rb
@@ -3,7 +3,7 @@ require 'package'
 class Avocado < Package
   description 'A dummy package for avocado_framework.'
   homepage 'https://avocado-framework.github.io/'
-  @_ver = '91.0'
+  @_ver = '94.0'
   version @_ver
   license 'GPL-2 and GPL-2+'
   compatibility 'all'
