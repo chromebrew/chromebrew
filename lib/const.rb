7656bf1642280798323d3912c0d7df564ab6db9d
Satadru Pramanik <satadru@umich.edu>
2022-01-10 09:27:44 -0500

    crew: show env hash expanded when showing Kernel.system errors (#6577)

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.21.2'
+CREW_VERSION = '1.21.3'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
