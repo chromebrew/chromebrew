9569113e14e33e53ef174a200ece2274bd6cb8a0
supechicken <supechicken666@gmail.com>
2022-01-12 01:32:38 +0800

    Update docbook_xsl_nons.rb

--- packages/docbook_xsl_nons.rb
+++ packages/docbook_xsl_nons.rb
@@ -38,6 +38,7 @@ class Docbook_xsl_nons < Package
     ENV['XML_CATALOG_FILES'] = "#{CREW_DEST_PREFIX}/etc/xml/catalog"
     @pkgroot = "#{CREW_DEST_PREFIX}/share/xml/docbook/xsl-stylesheets-#{@_ver}-nons"
     @ADDFILES_SH = <<~ADDFILES_HEREDOC
+      #!/usr/bin/env bash -ex
       install -Dt @pkgroot -m644 VERSION{,.xsl}
       (
         shopt -s nullglob  # ignore missing files
@@ -51,7 +52,8 @@ class Docbook_xsl_nons < Package
         done
       )
     ADDFILES_HEREDOC
-    system 'bash ./add_files.sh || true'
+    IO.write('add_files.sh', @ADDFILES_SH, perm: 0o755)
+    system './add_files.sh || true'
   end
 
   def self.preinstall
