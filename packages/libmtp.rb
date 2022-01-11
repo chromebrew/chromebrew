f3ce392b59d8e0da74d6ea3694027a44acc827d9
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2022-01-11 12:56:49 -0800

    libmtp

---
+++ packages/libmtp.rb
@@ -0,0 +1,35 @@
+require 'package'
+
+class Libmtp < Package
+  description 'libmtp is a library to access MTP (Media Transfer Protocol) Devices.'
+  homepage 'https://github.com/libmtp/libmtp/'
+  @_ver = '1.1.19'
+  version @_ver
+  license 'LGPL-2.1'
+  compatibility 'all'
+  source_url 'https://github.com/libmtp/libmtp.git'
+  git_hashtag 'v' + @_ver
+
+  depends_on 'libusb'
+  depends_on 'eudev'
+
+  def self.patch
+    system "sed -e 's:read IN:IN=y:' \
+                -e 's:wget -O:curl -#Lo:g' \
+                -i autogen.sh"
+  end
+
+  def self.build
+    system 'NOCONFIGURE=1 ./autogen.sh'
+    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+
+  def self.check
+    system 'make', 'check'
+  end
+end
