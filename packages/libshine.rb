0c31a760504861bc19a72d019ccf12811a59a516
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2022-01-11 12:41:03 -0800

    libshine

---
+++ packages/libshine.rb
@@ -0,0 +1,26 @@
+require 'package'
+
+class Libshine < Package
+  description 'shine is a rapid fixed-point MP3 encoder.'
+  homepage 'https://github.com/toots/shine/'
+  @_ver = '3.1.1'
+  version @_ver
+  license 'GPL-2'
+  compatibility 'all'
+  source_url 'https://github.com/toots/shine.git'
+  git_hashtag @_ver
+
+  def self.build
+    system 'autoreconf -fiv'
+    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+
+  def self.check
+    system 'make', 'check'
+  end
+end
