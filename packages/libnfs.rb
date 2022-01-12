d117776ff2387783c5e4f400f0378af8b24f688b
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2022-01-12 14:33:40 -0800

    add binaries

--- packages/libnfs.rb
+++ packages/libnfs.rb
@@ -4,11 +4,24 @@ class Libnfs < Package
   description 'client library for accessing NFS shares'
   homepage 'https://github.com/sahlberg/libnfs'
   @_ver = '4.0.0'
-  version @_ver + '-1'
-  license 'GPL-3, LGPL-2.1 and BSD'
+  version "#{@_ver}-1"
   compatibility 'all'
-  source_url "https://github.com/sahlberg/libnfs.git"
-  git_hashtag 'libnfs-' + @_ver
+  license 'GPL-3, LGPL-2.1 and BSD'
+  source_url 'https://github.com/sahlberg/libnfs.git'
+  git_hashtag "libnfs-#{@_ver}"
+
+  binary_url ({
+     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnfs/4.0.0-1_armv7l/libnfs-4.0.0-1-chromeos-armv7l.tpxz',
+      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnfs/4.0.0-1_armv7l/libnfs-4.0.0-1-chromeos-armv7l.tpxz',
+        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnfs/4.0.0-1_i686/libnfs-4.0.0-1-chromeos-i686.tpxz',
+      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnfs/4.0.0-1_x86_64/libnfs-4.0.0-1-chromeos-x86_64.tpxz',
+  })
+  binary_sha256 ({
+     aarch64: '93c8332ed7a11e62196a451206a1bb01f5c8d1bba0c860fdf08a6ee52638748f',
+      armv7l: '93c8332ed7a11e62196a451206a1bb01f5c8d1bba0c860fdf08a6ee52638748f',
+        i686: 'd838e8f83938d1621884496bcc226b33c61b5d20a1c45496d98733cde4c2505c',
+      x86_64: '8ff8bdbe19ee2c2bafe7ca410729eb30fc1195de1c3cab48867375752ad59299',
+  })
 
   def self.build
     system 'autoreconf -fiv'
