00ba2cc6ffcc47ab3429e6e0521808f1c8096747
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-01-15 10:28:55 -0800

    telegram => 3.4.3 (#6625)

--- packages/telegram.rb
+++ packages/telegram.rb
@@ -3,11 +3,11 @@ require 'package'
 class Telegram < Package
   description "Telegram is a messaging app with a focus on speed and security, it's super-fast, simple and free."
   homepage 'https://telegram.org/'
-  version '3.3.0'
+  version '3.4.3'
   license 'BSD, LGPL-2+ and GPL-3-with-openssl-exception'
   compatibility 'x86_64'
-  source_url 'https://github.com/telegramdesktop/tdesktop/releases/download/v3.3.0/tsetup.3.3.0.tar.xz'
-  source_sha256 '9e340b983c0cd1ede2f8dea4d596e20606334381bb4cb0a803a3019900016287'
+  source_url 'https://github.com/telegramdesktop/tdesktop/releases/download/v3.4.3/tsetup.3.4.3.tar.xz'
+  source_sha256 'c6ff9ad17e9337e080f0477a974ff7ca7d41680680758344fbd5300c5bb1623b'
 
   def self.build
     telegram = <<~EOF
