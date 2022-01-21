8132c60dbc1e30a8f23c72cd1ae4573eccf6cc01
supechicken <supechicken666@gmail.com>
2022-01-22 00:20:28 +0800

    install.sh: Add rsync as a core package (#6621)
    * Update crew
    
    * Update crew
    
    * Bump version
    
    * Update crew
    
    * Update crew
    
    * Update crew
    
    * Update crew
    
    * Update crew
    
    * Update install.sh
    
    * Update install.sh
    
    * Update install.sh
    
    * Update crew
    
    * Update install.sh
    
    * Update crew
    
    * Update install.sh
    
    * Update crew

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.21.7'
+CREW_VERSION = '1.21.8'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
