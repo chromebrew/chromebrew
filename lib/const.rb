f7b2d911a18d5c78fe611345944c38706de6f3a6
supechicken <supechicken666@gmail.com>
2022-01-22 16:50:18 +0800

    crew: Replace `binutils/ar` with a new library (#6642)
    * Create deb_utils.rb
    
    * Update crew
    
    * Update core.rb
    
    * Update const.rb
    
    * Update deb_utils.rb
    
    * Wrap as a module
    
    Co-authored-by: Ed Reel <edreel@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.21.8'
+CREW_VERSION = '1.22.0'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
