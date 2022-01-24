c6bc5db752cf5daab03631b7a8cb503ad6fa3c83
Ed Reel <edreel@gmail.com>
2022-01-23 21:52:36 -0600

    Brave 1.34.80 => 1.34.81

--- packages/brave.rb
+++ packages/brave.rb
@@ -3,11 +3,11 @@ require 'package'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '1.34.80'
+  version '1.34.81'
   license 'MPL-2'
   compatibility 'x86_64'
   source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-#{version}-linux-amd64.zip"
-  source_sha256 '81a0c4be851951f319c17068626a5667d9f89e450ba72f0cbc2dd5e1bfa98e6d'
+  source_sha256 'fcc92e7f3372937b2a0547124bf40d8fb95138f1bfe83612918aa2124dabf9b0'
 
   depends_on 'gtk3'
   depends_on 'libcom_err'
