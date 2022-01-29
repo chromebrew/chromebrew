586771bfcdc286bc0e9a98861321a7e1c3382462
Ed Reel <edreel@gmail.com>
2022-01-29 10:09:47 -0600

    Platformsh 3.71.0 => 3.76.0 (#6672)

--- packages/platformsh.rb
+++ packages/platformsh.rb
@@ -3,7 +3,7 @@ require 'package'
 class Platformsh < Package
   description 'The unified tool for managing your Platform.sh services from the command line.'
   homepage 'https://docs.platform.sh/overview/cli.html'
-  version '3.71.0'
+  version '3.76.0'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
@@ -12,7 +12,7 @@ class Platformsh < Package
 
   def self.install
     system "curl -#LO https://github.com/platformsh/platformsh-cli/releases/download/v#{version}/platform.phar"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('platform.phar') ) == '05caf428a7bc4c9bb841e6310f3d6d3c07599bf6f128e4d28dc8f36429d1f759'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('platform.phar') ) == '424335d9545178eba21e0e46b139432735f064212e328b72c815975a17d58e95'
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'platform.phar', "#{CREW_DEST_PREFIX}/bin/platform", mode: 0o755
   end
