298d19051de2c5e12d0ff7224ea448f459d96875
Ed Reel <edreel@gmail.com>
2022-01-29 01:02:23 -0600

    Terminus 3.0.1 => 3.0.4

--- packages/terminus.rb
+++ packages/terminus.rb
@@ -3,7 +3,7 @@ require 'package'
 class Terminus < Package
   description 'The Pantheon CLI — a standalone utility for performing operations on the Pantheon Platform'
   homepage 'https://github.com/pantheon-systems/terminus'
-  version '3.0.1'
+  version '3.0.4'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
@@ -12,7 +12,7 @@ class Terminus < Package
 
   def self.install
     system "curl -#LO https://github.com/pantheon-systems/terminus/releases/download/#{version}/terminus.phar"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('terminus.phar') ) == 'b3a444bcb5870cb9352f7708aa4e4212b4113ec6662b13cd123a6b0f2f958f40'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('terminus.phar') ) == 'c8622c69379952c4b782b2a94fb9e92d8e3e26af73fdeec9c379c83255d8c0da'
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'terminus.phar', "#{CREW_DEST_PREFIX}/bin/terminus", mode: 0o755
   end
