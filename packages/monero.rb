59b92e2c7ca265e7019a06673c9c53ed76a412ef
Ed Reel <edreel@gmail.com>
2022-01-29 11:07:30 -0600

    Monero 0.17.2.3 => 0.17.3.0 (#6683)

--- packages/monero.rb
+++ packages/monero.rb
@@ -3,19 +3,19 @@ require 'package'
 class Monero < Package
   description 'Private, decentralized cryptocurrency that keeps your finances confidential and secure.'
   homepage 'https://www.getmonero.org/'
-  version '0.17.2.3'
+  version '0.17.3.0'
   license 'The Cryptonote developers,The Boolberry developers,MIT'
   compatibility 'all'
   case ARCH
   when 'aarch64', 'armv7l'
-   source_url 'https://downloads.getmonero.org/cli/monero-linux-armv7-v0.17.2.3.tar.bz2'
-   source_sha256 '9100704b7d98d7bffbf7f72804c0c048f5e60f1dc9f6d4830c633dda3687c419'
+   source_url 'https://downloads.getmonero.org/cli/monero-linux-armv7-v0.17.3.0.tar.bz2'
+   source_sha256 'da49d85ce2d52fc07846c58d0c58d6412f454f9d389bfa31eab9c1d49a1a13ed'
   when 'i686'
-    source_url 'https://downloads.getmonero.org/cli/monero-linux-x86-v0.17.2.3.tar.bz2'
-    source_sha256 'a93788591bd56753369a3f3171fbefb09387758c1e3a219e9265473031f0a35d'
+    source_url 'https://downloads.getmonero.org/cli/monero-linux-x86-v0.17.3.0.tar.bz2'
+    source_sha256 '586b9967d848eb31dd66bdb6d828bd3a640098434595a5933374d129b76958eb'
   when 'x86_64'
-    source_url 'https://downloads.getmonero.org/cli/monero-linux-x64-v0.17.2.3.tar.bz2'
-    source_sha256 '8069012ad5e7b35f79e35e6ca71c2424efc54b61f6f93238b182981ba83f2311'
+    source_url 'https://downloads.getmonero.org/cli/monero-linux-x64-v0.17.3.0.tar.bz2'
+    source_sha256 'ac18ce3d1189410a5c175984827d5d601974733303411f6142296d647f6582ce'
   end
 
   def self.install
