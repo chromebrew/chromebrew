5888cf263ee0b70c6d0d28cc7c472b2ea6fd0ee8
Ed Reel <edreel@gmail.com>
2022-01-29 11:06:53 -0600

    Mutagen 0.12.0 => 0.13.0 (#6682)

--- packages/mutagen.rb
+++ packages/mutagen.rb
@@ -3,19 +3,19 @@ require 'package'
 class Mutagen < Package
   description 'Mutagen provides real-time file synchronization and flexible network forwarding, extending the reach of your existing development tools to cloud-based containers and infrastructure.'
   homepage 'https://mutagen.io/'
-  version '0.12.0'
+  version '0.13.0'
   license 'GPL-2'
   compatibility 'all'
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://github.com/mutagen-io/mutagen/releases/download/v0.12.0/mutagen_linux_arm_v0.12.0.tar.gz'
-    source_sha256 '2705d665e7e66555b577260d32b11cca2f3a252f3f90dc27e2982f8af907a18f'
+    source_url 'https://github.com/mutagen-io/mutagen/releases/download/v0.13.0/mutagen_linux_arm_v0.13.0.tar.gz'
+    source_sha256 'be09d6cfe04ca6c9607914f7ec509608148a4209afe22d00be9b2c23b0d930d9'
   when 'i686'
-    source_url 'https://github.com/mutagen-io/mutagen/releases/download/v0.12.0/mutagen_linux_386_v0.12.0.tar.gz'
-    source_sha256 'c41175729cdb5c7f7280ccb27d56b0992e26e20fb1b7cefe9a7a6d2f7f623d4a'
+    source_url 'https://github.com/mutagen-io/mutagen/releases/download/v0.13.0/mutagen_linux_386_v0.13.0.tar.gz'
+    source_sha256 'a05c14dfeacbf56976983c4d513f4cd91f55e6e35fea86a4f3a44a1e2ed8808d'
   when 'x86_64'
-    source_url 'https://github.com/mutagen-io/mutagen/releases/download/v0.12.0/mutagen_linux_amd64_v0.12.0.tar.gz'
-    source_sha256 '7a35891c0667105d43ce36b1ee406b3af21802ba5cd402cac47fcf5eff1ecad9'
+    source_url 'https://github.com/mutagen-io/mutagen/releases/download/v0.13.0/mutagen_linux_amd64_v0.13.0.tar.gz'
+    source_sha256 'bd4c009e4bbce76bf13823d8734b7ed951c1623026faedecb63f0f77aceefde1'
   end
 
   def self.install
@@ -28,13 +28,12 @@ class Mutagen < Package
       arch = 'amd64'
     end
     system "tar xvf mutagen-agents.tar.gz linux_#{arch}"
-    system "install -Dm755 mutagen #{CREW_DEST_PREFIX}/bin/mutagen"
-    system "install -Dm755 linux_#{arch} #{CREW_DEST_PREFIX}/bin/mutagen-agent"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.install 'mutagen', "#{CREW_DEST_PREFIX}/bin/mutagen", mode: 0o755
+    FileUtils.install "linux_#{arch}", "#{CREW_DEST_PREFIX}/bin/mutagen-agent", mode: 0o755
   end
 
   def self.postinstall
-    puts
-    puts "Type 'mutagen' to get started.".lightblue
-    puts
+    puts "\nType 'mutagen' to get started.\n".lightblue
   end
 end
