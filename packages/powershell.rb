2d5f9eac20caeb15f64e8d265d6f722cf964e1f3
Ed Reel <edreel@gmail.com>
2022-01-30 01:51:07 -0600

    Powershell 7.2.0 => 7.2.1

--- packages/powershell.rb
+++ packages/powershell.rb
@@ -3,18 +3,18 @@ require 'package'
 class Powershell < Package
   description 'Powershell is a cross-platform, task-based command-line shell and scripting language that helps rapidly automate tasks that manage operating systems and processes'
   homepage 'https://docs.microsoft.com/en-us/powershell/'
-  version '7.2.0'
+  version '7.2.1'
   license 'MIT'
   compatibility 'aarch64,armv7l,x86_64'
   source_url ({
-    aarch64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-arm32.tar.gz',
-     armv7l: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-arm32.tar.gz',
-     x86_64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.0/powershell-7.2.0-linux-x64.tar.gz',
+    aarch64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-arm32.tar.gz',
+     armv7l: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-arm32.tar.gz',
+     x86_64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-x64.tar.gz',
   })
   source_sha256 ({
-    aarch64: 'bb1e2ca5ff230721ed3d2579714a7cfe12ea27b7b27b22ae222b3515d9038b8c',
-     armv7l: 'bb1e2ca5ff230721ed3d2579714a7cfe12ea27b7b27b22ae222b3515d9038b8c',
-     x86_64: '15b67d0571d9891a10e541a1462c906084ab148614dd83c6bbddf90046a815a8',
+    aarch64: 'd40ca2c8e05dd8b6169d7f35683432c671a3c58f35e46deaf1d776d9a78b38fe',
+     armv7l: 'd40ca2c8e05dd8b6169d7f35683432c671a3c58f35e46deaf1d776d9a78b38fe',
+     x86_64: '337d9864799ad09b46d261071b9f835f69f078814409bc2681f4cc2857b6bda5',
   })
 
   depends_on 'xdg_base'
