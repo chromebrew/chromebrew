e005da772c2c769a86c3856a08ca1cd76462fd48
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-01-31 18:36:13 -0800

    add opencl binaries

--- packages/opencl_headers.rb
+++ packages/opencl_headers.rb
@@ -10,6 +10,19 @@ class Opencl_headers < Package
   source_url 'https://github.com/KhronosGroup/OpenCL-Headers.git'
   git_hashtag "v#{@_ver}"
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_headers/2022.01.04_armv7l/opencl_headers-2022.01.04-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_headers/2022.01.04_armv7l/opencl_headers-2022.01.04-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_headers/2022.01.04_i686/opencl_headers-2022.01.04-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_headers/2022.01.04_x86_64/opencl_headers-2022.01.04-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    aarch64: 'f0b12ec6ee2767ea73ed54471e561265056d4fb77487b5b4c87200fd19498623',
+     armv7l: 'f0b12ec6ee2767ea73ed54471e561265056d4fb77487b5b4c87200fd19498623',
+       i686: 'df5da693a74ad37afb6994e6d63aa7286b851204962add65f89ab67bad15d724',
+     x86_64: '61327f01cbe562dcebccf9dc2355e09bfb34f560b51512bb55e9085d7045686b'
+  })
+
   def self.build
     Dir.mkdir 'builddir'
     Dir.chdir 'builddir' do
--- packages/opencl_icd_loader.rb
+++ packages/opencl_icd_loader.rb
@@ -10,6 +10,19 @@ class Opencl_icd_loader < Package
   source_url 'https://github.com/KhronosGroup/OpenCL-ICD-Loader.git'
   git_hashtag "v#{@_ver}"
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_icd_loader/2022.01.04_armv7l/opencl_icd_loader-2022.01.04-chromeos-armv7l.tpxz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_icd_loader/2022.01.04_armv7l/opencl_icd_loader-2022.01.04-chromeos-armv7l.tpxz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_icd_loader/2022.01.04_i686/opencl_icd_loader-2022.01.04-chromeos-i686.tpxz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/opencl_icd_loader/2022.01.04_x86_64/opencl_icd_loader-2022.01.04-chromeos-x86_64.tpxz'
+  })
+  binary_sha256({
+    aarch64: '060686f444eb9a9484b305d05eeb919fb20460c724eaecc6997ca0fe5158ded1',
+     armv7l: '060686f444eb9a9484b305d05eeb919fb20460c724eaecc6997ca0fe5158ded1',
+       i686: '53e3402e0df4c65cb9d6169ffbd96508888b1419d6626ea648f8e7e292dd70a0',
+     x86_64: 'e2d9de8e96da760c71576e88a0af5132b052abf070b8c698aac6e2e2fe5929b9'
+  })
+
   depends_on 'opencl_headers' => :build
 
   def self.build
