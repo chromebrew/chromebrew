bd27691addc3966bca7ded49d0905b039f83e350
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-02-01 21:38:27 -0800

    ruby => 3.1.0

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -2,25 +2,12 @@ require 'package'
 
 class Ruby < Package
   description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
-  homepage 'https://www.ruby-lang.org/en/'
-  version '3.0.3'
+  homepage 'https://www.ruby-lang.org/'
+  version '3.1.0'
   license 'Ruby-BSD and BSD-2'
   compatibility 'all'
-  source_url 'https://cache.ruby-lang.org/pub/ruby/3.0/ruby-3.0.3.tar.gz'
-  source_sha256 '3586861cb2df56970287f0fd83f274bd92058872d830d15570b36def7f1a92ac'
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.0.3_armv7l/ruby-3.0.3-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.0.3_armv7l/ruby-3.0.3-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.0.3_i686/ruby-3.0.3-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.0.3_x86_64/ruby-3.0.3-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '2ca59bc65bfcdc4fcf7c2626c76b99a5cf9647620818534e129f81eff02ee041',
-     armv7l: '2ca59bc65bfcdc4fcf7c2626c76b99a5cf9647620818534e129f81eff02ee041',
-       i686: '13db0c964a9a23f0db69fea818c9f37910c20170e75eb2e334ed17b6fb0e9f74',
-     x86_64: '048b39185f15fa3626c45ab2e9505cedabf33baff853bd57cee5c387b298a44c'
-  })
+  source_url 'https://cache.ruby-lang.org/pub/ruby/3.1/ruby-3.1.0.tar.xz'
+  source_sha256 '1a0e0b69b9b062b6299ff1f6c6d77b66aff3995f63d1d8b8771e7a113ec472e2'
 
   depends_on 'ca_certificates'
   depends_on 'libyaml' # This is needed to install gems
