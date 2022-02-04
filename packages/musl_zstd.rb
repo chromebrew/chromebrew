ff37bae2eb8d7cacfa71da877e14ed533b299e7b
Satadru Pramanik <satadru@gmail.com>
2022-02-03 20:03:52 -0500

    crew: use zstd (#6703)
    * Initial .zst support
    
    * initial zstd changes
    
    * reverse tar version condition
    
    * Change extension to .tar.zst
    
    * Add verbosity, use nice for zstd
    
    * add tar to core
    
    * Add tar to buildessential
    
    * Add tar to bootstrap packages
    
    * Add musl tar with zstd support
    
    * add error for old tar on system
    
    * Add zst support to gl.sh, add update of rust in zst package format
    
    * Add check for zstd before decompressing .zst files
    
    * change new tar dependency to i686 only
    
    * add more musl fixes
    
    * add musl paths to tar cmd
    
    * Add musl_zstd update with libc
    
    * add musl path to install path
    
    * add zst to strip_dir
    
    * Add spaces to gl.sh
    
    * remove extraneous no_zstd

--- packages/musl_zstd.rb
+++ packages/musl_zstd.rb
@@ -3,7 +3,7 @@ require 'package'
 class Musl_zstd < Package
   description 'Zstandard - Fast real-time compression algorithm'
   homepage 'http://www.zstd.net'
-  @_ver = '1.5.1'
+  @_ver = '1.5.2'
   version @_ver
   license 'BSD or GPL-2'
   compatibility 'all'
@@ -11,38 +11,46 @@ class Musl_zstd < Package
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.1_armv7l/musl_zstd-1.5.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.1_armv7l/musl_zstd-1.5.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.1_i686/musl_zstd-1.5.1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.1_x86_64/musl_zstd-1.5.1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.2_armv7l/musl_zstd-1.5.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.2_armv7l/musl_zstd-1.5.2-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.2_i686/musl_zstd-1.5.2-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_zstd/1.5.2_x86_64/musl_zstd-1.5.2-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'd87582aa6dbd85e81c90317acd30b30b6300e4c63c01fe5570b37b631338e26e',
-     armv7l: 'd87582aa6dbd85e81c90317acd30b30b6300e4c63c01fe5570b37b631338e26e',
-       i686: '88af0514d1a2a6d93df12c0566a675175012ed7906b8bb27e7bf50d4d1ec13ae',
-     x86_64: 'bd7ef06d006489657649d1e3a2d999dde9b4e2d7788ca9b6b967a7ccecc75b9c'
+    aarch64: '3cef255a8af0fa12e19e7d91f9b14e4d3c57fd73415f651d5dfc1d78a2d573a9',
+     armv7l: '3cef255a8af0fa12e19e7d91f9b14e4d3c57fd73415f651d5dfc1d78a2d573a9',
+       i686: '3f0adecdbc55e1c2114ff1013d21da3c11c468a28e5a28dfb54620ba54ea4f36',
+     x86_64: '5a96064ca69ee5db65b6961f795a239f83ac5cbca16acea2b59a6f7472d54eb3'
   })
 
   depends_on 'musl_native_toolchain' => :build
 
   is_static
+  is_musl
+  no_zstd
+  patchelf
+  conflicts_ok # copies in libc.so from musl
 
   def self.build
-    load "#{CREW_LIB_PATH}lib/musl.rb"
     FileUtils.mkdir('build/cmake/builddir')
     Dir.chdir('build/cmake/builddir') do
       system "#{MUSL_CMAKE_OPTIONS.gsub('-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE',
                                         '-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=OFF')} \
       -DZSTD_BUILD_STATIC=ON \
       -DZSTD_BUILD_SHARED=OFF \
-      -DZSTD_BUILD_PROGRAMS=OFF \
+      -DZSTD_BUILD_PROGRAMS=ON \
+      -DZSTD_PROGRAMS_LINK_SHARED=OFF \
+      -DPROGRAMS_ZSTD_LINK_TARGET=libzstd_static \
       ../ -G Ninja"
     end
-    system "PATH=#{CREW_MUSL_PREFIX}/bin:#{CREW_MUSL_PREFIX}/#{ARCH}-linux-musl#{@abi}/bin:#{ENV['PATH']} \
+    system "#{MUSL_ENV_OPTIONS} \
       samu -C build/cmake/builddir -j#{CREW_NPROC}"
   end
 
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} samu -C build/cmake/builddir install"
+    FileUtils.mkdir_p "#{CREW_DEST_MUSL_PREFIX}/lib"
+    FileUtils.cp "#{CREW_MUSL_PREFIX}/lib/libc.so", "#{CREW_DEST_MUSL_PREFIX}/lib/"
+    FileUtils.rm_f Dir.glob("#{CREW_DEST_MUSL_PREFIX}/share/man/")
   end
 end
