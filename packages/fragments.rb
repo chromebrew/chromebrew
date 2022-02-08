0ae54e878ad25582d8a6ecfd442594e16623f981
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-02-08 13:52:48 -0800

    new package fragments

---
+++ packages/fragments.rb
@@ -0,0 +1,33 @@
+require 'package'
+
+class Fragments < Package
+  description 'Fragments is an easy to use BitTorrent client for the GNOME desktop environment.'
+  homepage 'https://gitlab.gnome.org/World/Fragments'
+  @_ver = '2.0.1'
+  version @_ver
+  license 'GPL-3'
+  compatibility 'all'
+  source_url 'https://gitlab.gnome.org/World/Fragments.git'
+  git_hashtag @_ver
+
+  depends_on 'dbus'
+  depends_on 'glib'
+  depends_on 'gtk4'
+  depends_on 'pango'
+  depends_on 'libadwaita'
+  depends_on 'rust' => :build
+
+  def self.build
+    system "meson #{CREW_MESON_OPTIONS} builddir"
+    system 'meson configure builddir'
+    system 'samu -C builddir || samu -C builddir || samu -C builddir' # Known bug where transmission-client crate fails to build the first time and succeeds the second or third.
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+  end
+
+  def self.check
+    system 'samu -C builddir test'
+  end
+end
