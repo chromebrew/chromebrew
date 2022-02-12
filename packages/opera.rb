498ebb41ffb9c9992ee6f94ba6dc8da37cddc75e
Ed Reel <edreel@gmail.com>
2022-02-12 15:46:11 -0600

    Opera 83.0.4254.27 => 83.0.4254.54

--- packages/opera.rb
+++ packages/opera.rb
@@ -3,11 +3,11 @@ require 'package'
 class Opera < Package
   description "Opera is a multi-platform web browser based on Chromium and developed by Opera Software."
   homepage 'https://www.opera.com/'
-  version '83.0.4254.27'
+  version '83.0.4254.54'
   license 'OPERA-2018'
   compatibility 'x86_64'
   source_url "https://get.opera.com/pub/opera/desktop/#{version}/linux/opera-stable_#{version}_amd64.deb"
-  source_sha256 '8a841eddcc6327670d3329129be63287075c59698aece03e38a0bad44e3929a7'
+  source_sha256 '92ff741667a71a901b372c3fa6ec046d1fef54d1d5d9de54757dda98c4dc23b9'
 
   depends_on 'gtk3'
   depends_on 'gsettings_desktop_schemas'
