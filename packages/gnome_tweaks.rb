0bade35f210b24370a16c1a2c4aa9e08012179ff
Satadru Pramanik <satadru@gmail.com>
2022-02-16 23:16:44 -0500

    gnome_tweaks rebuild for python3.10

--- packages/gnome_tweaks.rb
+++ packages/gnome_tweaks.rb
@@ -2,22 +2,23 @@ require 'package'
 
 class Gnome_tweaks < Package
   description 'Graphical interface for advanced GNOME 3 settings Tweak Tool'
-  @_ver = '40.0'
+  @_ver = '40.8-43f5bd'
   version @_ver
   license 'GPL-3+ and CC0-1.0'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/gnome-tweaks.git'
-  git_hashtag @_ver
+  git_branch 'gnome-40'
+  git_hashtag '43f5bd133289a90012726f2a5796a92547e7d24a'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_tweaks/40.0_armv7l/gnome_tweaks-40.0-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_tweaks/40.0_armv7l/gnome_tweaks-40.0-chromeos-armv7l.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_tweaks/40.0_x86_64/gnome_tweaks-40.0-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_tweaks/40.8-43f5bd_armv7l/gnome_tweaks-40.8-43f5bd-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_tweaks/40.8-43f5bd_armv7l/gnome_tweaks-40.8-43f5bd-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_tweaks/40.8-43f5bd_x86_64/gnome_tweaks-40.8-43f5bd-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ebec11a3439e8cd1817f871c2f2015ebce581a483a1926cc5dcbf42c55ae1145',
-     armv7l: 'ebec11a3439e8cd1817f871c2f2015ebce581a483a1926cc5dcbf42c55ae1145',
-     x86_64: 'a002b3463df0c990e3cab84ae1a9748eddaa8d50e9952b04d593cd99373578f9'
+    aarch64: 'de82ca077fc6ff11f34bf3a7df1b62cb7fb47a2efd4fce85194164a31537643c',
+     armv7l: 'de82ca077fc6ff11f34bf3a7df1b62cb7fb47a2efd4fce85194164a31537643c',
+     x86_64: '00f4110c8ca385922da7060d0d14ec3552a2e6a626c80d3817f2f640305925cd'
   })
 
   depends_on 'gnome_settings_daemon'
