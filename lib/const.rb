ba88ca6e55b52d8d8b85d2966838bff1fc2acc07
supechicken <supechicken666@gmail.com>
2022-02-17 23:43:52 +0800

    crew: Allow package name starts with digits (#6748)
    * Update crew
    
    * Update const.rb
    
    Co-authored-by: Ed Reel <edreel@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.22.9'
+CREW_VERSION = '1.22.10'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
