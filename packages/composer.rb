153f0bb118a3522753d0734f754ad64c28129eb0
Ed Reel <edreel@gmail.com>
2022-02-20 12:06:33 -0600

    Composer 2.2.5 => 2.2.6 (#6762)

--- packages/composer.rb
+++ packages/composer.rb
@@ -3,7 +3,7 @@ require 'package'
 class Composer < Package
   description 'Dependency Manager for PHP'
   homepage 'https://getcomposer.org/'
-  version '2.2.5'
+  version '2.2.6'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
@@ -23,7 +23,7 @@ class Composer < Package
 
   def self.install
     system "curl -Ls -o composer https://github.com/composer/composer/releases/download/#{version}/composer.phar"
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('composer') ) == '81ef304a70c957d6f05a7659f03b00eb50df6155195f51118459b2e49c96c3f3'
+    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('composer') ) == '1d58486b891e59e9e064c0d54bb38538f74d6014f75481542c69ad84d4e97704'
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'composer', "#{CREW_DEST_PREFIX}/bin/composer", mode: 0o755
   end
