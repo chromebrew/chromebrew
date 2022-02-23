262d2962ab4df589d233b732e173e25a076e03de
Ed Reel <edreel@gmail.com>
2022-02-23 01:03:02 -0600

    Julia 1.6.4 => 1.7.2

--- packages/julia.rb
+++ packages/julia.rb
@@ -3,20 +3,20 @@ require 'package'
 class Julia < Package
   description 'Julia is a flexible dynamic language, appropriate for scientific and numerical computing'
   homepage 'https://julialang.org/'
-  version '1.6.4'
+  version '1.7.2'
   license 'MIT'
   compatibility 'all'
   source_url ({
-    aarch64: 'https://julialang-s3.julialang.org/bin/linux/armv7l/1.6/julia-1.6.4-linux-armv7l.tar.gz',
-     armv7l: 'https://julialang-s3.julialang.org/bin/linux/armv7l/1.6/julia-1.6.4-linux-armv7l.tar.gz',
-       i686: 'https://julialang-s3.julialang.org/bin/linux/x86/1.6/julia-1.6.4-linux-i686.tar.gz',
-     x86_64: 'https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.4-linux-x86_64.tar.gz',
+    aarch64: 'https://julialang-s3.julialang.org/bin/linux/armv7l/1.7/julia-1.7.2-linux-armv7l.tar.gz',
+     armv7l: 'https://julialang-s3.julialang.org/bin/linux/armv7l/1.7/julia-1.7.2-linux-armv7l.tar.gz',
+       i686: 'https://julialang-s3.julialang.org/bin/linux/x86/1.7/julia-1.7.2-linux-i686.tar.gz',
+     x86_64: 'https://julialang-s3.julialang.org/bin/linux/x64/1.7/julia-1.7.2-linux-x86_64.tar.gz',
   })
   source_sha256 ({
-    aarch64: '9ad3f6bd71eb6840d4cef1569855da20c0b4931a2bdf77703a64df672b0702a1',
-     armv7l: '9ad3f6bd71eb6840d4cef1569855da20c0b4931a2bdf77703a64df672b0702a1',
-       i686: '9d43d642174cf22cf0fde18dc2578c84f357d2c619b9d846d3a6da4192ba48cf',
-     x86_64: '52244ae47697e8073dfbc9d1251b0422f0dbd1fbe1a41da4b9f7ddf0506b2132',
+    aarch64: '837c93adf75a3e981eacf35f24f1d95cd2e4d9c490b84befd3865b2d558b730c',
+     armv7l: '837c93adf75a3e981eacf35f24f1d95cd2e4d9c490b84befd3865b2d558b730c',
+       i686: 'f5f8e6cbaf0acd473a5e13c23c80f0672207eb86408178fec221f4f7fb88f6d1',
+     x86_64: 'a75244724f3b2de0e7249c861fbf64078257c16fb4203be78f1cf4dd5973ba95',
   })
 
   def self.install
