38d9ef40af34e2d6fb2ad32216d11163346abc9d
Ed Reel <edreel@gmail.com>
2022-02-23 02:43:11 -0600

    Hugo 0.89.4 => 0.92.2 (#6770)

--- packages/hugo.rb
+++ packages/hugo.rb
@@ -3,20 +3,20 @@ require 'package'
 class Hugo < Package
   description 'Hugo is one of the most popular open-source static site generators.'
   homepage 'https://gohugo.io'
-  version '0.89.4'
+  version '0.92.2'
   license 'Apache-2.0, Unlicense, BSD, BSD-2 and MPL-2.0'
   compatibility 'all'
   source_url ({
-    aarch64: 'https://github.com/gohugoio/hugo/releases/download/v0.89.4/hugo_0.89.4_Linux-ARM.tar.gz',
-     armv7l: 'https://github.com/gohugoio/hugo/releases/download/v0.89.4/hugo_0.89.4_Linux-ARM.tar.gz',
-       i686: 'https://github.com/gohugoio/hugo/releases/download/v0.89.4/hugo_0.89.4_Linux-32bit.tar.gz',
-     x86_64: 'https://github.com/gohugoio/hugo/releases/download/v0.89.4/hugo_extended_0.89.4_Linux-64bit.tar.gz',
+    aarch64: 'https://github.com/gohugoio/hugo/releases/download/v0.92.2/hugo_0.92.2_Linux-ARM.tar.gz',
+     armv7l: 'https://github.com/gohugoio/hugo/releases/download/v0.92.2/hugo_0.92.2_Linux-ARM.tar.gz',
+       i686: 'https://github.com/gohugoio/hugo/releases/download/v0.92.2/hugo_0.92.2_Linux-32bit.tar.gz',
+     x86_64: 'https://github.com/gohugoio/hugo/releases/download/v0.92.2/hugo_extended_0.92.2_Linux-64bit.tar.gz',
   })
   source_sha256 ({
-    aarch64: 'a8781ab0e61aa71c9084a23b908644296bb3390eda9cf5be78d2d225b5f0cc56',
-     armv7l: 'a8781ab0e61aa71c9084a23b908644296bb3390eda9cf5be78d2d225b5f0cc56',
-       i686: '1731fe5e9e7375d7c2d4f275d763957952734c4b52ed886d989b1112c3ff3a45',
-     x86_64: '97743ff4026eb1f0f6ceeea91c5f8236b4833ecbe36370e91dfea247da56072e',
+    aarch64: '8a5b176768fb27d13bf665cf51ffd2d8fb71f2f382a887ab130c7ef927707fd5',
+     armv7l: '8a5b176768fb27d13bf665cf51ffd2d8fb71f2f382a887ab130c7ef927707fd5',
+       i686: '2f65f968b3288e0119035b49688bac52fd46707c95e635dcfc5a442e8b907f36',
+     x86_64: 'e493b819c77b4f3af6b5ac4635c49311adc24a4ada109bd87452fb8bf5bef9ca',
   })
 
   def self.install
