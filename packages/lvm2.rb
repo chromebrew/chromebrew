f389bcb5c3b6e89de844ee31634106a01c2df456
Zopolis4 <creatorsmithmdt@gmail.com>
2022-02-25 13:59:12 +1100

    Update LVM2 (Bump version and add deventd)

--- packages/lvm2.rb
+++ packages/lvm2.rb
@@ -3,25 +3,12 @@ require 'package'
 class Lvm2 < Package
   description 'LVM2 refers to the userspace toolset that provide logical volume management facilities on linux.'
   homepage 'https://sourceware.org/lvm2'
-  @_ver = '2.03.11'
+  @_ver = '2.03.15'
   version @_ver
   license 'GPL-2'
   compatibility 'all'
   source_url "https://www.sourceware.org/pub/lvm2/releases/LVM2.#{@_ver}.tgz"
-  source_sha256 '842c4510d4653990927d4518a5bf2743126a37531671a05842cdaf8d54bb9dd4'
-
-  binary_url ({
-     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lvm2/2.03.11_armv7l/lvm2-2.03.11-chromeos-armv7l.tar.xz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lvm2/2.03.11_armv7l/lvm2-2.03.11-chromeos-armv7l.tar.xz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lvm2/2.03.11_i686/lvm2-2.03.11-chromeos-i686.tar.xz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/lvm2/2.03.11_x86_64/lvm2-2.03.11-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-     aarch64: 'a60be47b7f7011234587ebba6e28adb36d27138b4bb182aa88da8af7fc6a6079',
-      armv7l: 'a60be47b7f7011234587ebba6e28adb36d27138b4bb182aa88da8af7fc6a6079',
-        i686: '62fbf27d3a75a5bf043c02b24ecf4f55f428a14192a59289bb97fd14cd073d78',
-      x86_64: 'd1b98a8b2c8ca75d78a4fd744a2d1aad44b0c23f102b588f8b5e7a7ed17d42c0',
-  })
+  source_sha256 '935283a51ee17abd752a545a0ed1cf4edc993359265bc9e562edf81500edc99e'
 
   depends_on 'libaio'
 
@@ -30,6 +17,10 @@ class Lvm2 < Package
       ./configure \
       #{CREW_OPTIONS} \
       --disable-selinux \
+      --enable-cmdlib \
+      --enable-dmeventd \
+      --enable-pkgconfig \
+      --with-symvers=no \
       --with-confdir=#{CREW_PREFIX}/etc"
     system "make"
   end
