53a2c9925903bb921bd70536b08b11b2919bec79
Ed Reel <edreel@gmail.com>
2022-02-26 20:42:48 -0600

    Sublime_text 4121 => 4126 (#6787)

--- packages/sublime_text.rb
+++ packages/sublime_text.rb
@@ -3,11 +3,11 @@ require 'package'
 class Sublime_text < Package
   description 'A sophisticated text editor for code, markup and prose.'
   homepage 'https://www.sublimetext.com/'
-  version 'Build 4121'
+  version 'Build 4126'
   license 'Copyright Sublime HQ Pty Ltd' # Shareware for evaluation purposes only.  Source code is not available.
   compatibility 'x86_64'
-  source_url 'https://download.sublimetext.com/sublime_text_build_4121_x64.tar.xz'
-  source_sha256 '084fcf79457c320d59da1f0e58c85a7a63956bde41d64db01ec86df2554fc5e6'
+  source_url 'https://download.sublimetext.com/sublime_text_build_4126_x64.tar.xz'
+  source_sha256 '5c64e534cce0032e54d3c7028e8d6b3bdef28f3fd28a26244a360a2ce75450a1'
 
   depends_on 'gtk2'
   depends_on 'sommelier'
@@ -20,9 +20,7 @@ class Sublime_text < Package
   end
 
   def self.postinstall
-    puts
-    puts 'Congratulations! You have installed Sublime Text on Chrome OS!'.lightgreen
-    puts 'Now, please run \'sublime_text\' to start Sublime Text.'.lightgreen
-    puts
+    puts "\nCongratulations! You have installed Sublime Text on Chrome OS!".lightgreen
+    puts "Now, please run 'sublime_text' to get started.\n".lightgreen
   end
 end
