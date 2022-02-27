c4221d3019937644ec1063988110bba2d6b18a8b
Ed Reel <edreel@gmail.com>
2022-02-26 20:42:18 -0600

    V2ray v4.24.2 => v4.28.2 (#6781)

--- packages/v2ray.rb
+++ packages/v2ray.rb
@@ -3,25 +3,27 @@ require 'package'
 class V2ray < Package
   description 'A platform for building proxies to bypass network restrictions.'
   homepage 'https://www.v2ray.com/'
-  version 'v4.24.2'
+  version 'v4.28.2'
   license 'MIT'
   compatibility 'all'
 
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.24.2/v2ray-linux-arm.zip'
-    source_sha256 'd6c93961c0c192097cadadc73e3a4b2cc6fff722851af9c073ea2612f996a3b7'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.28.2/v2ray-linux-arm32-v7a.zip'
+    source_sha256 '9090cfc97418719e66467f296bfc2297776da14d3af127d23aa3bb196d7707e8'
   when 'i686'
-    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.24.2/v2ray-linux-32.zip'
-    source_sha256 '7a72f5dc9ee9b38fc9a827b555150714da19905d27d88f23b1ae3adb411a0ece'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.28.2/v2ray-linux-32.zip'
+    source_sha256 '8b31f08f6e25609792f1d1ad6a4d9e35e4089a19ac85b30bd70b95bd48b8e9e3'
   when 'x86_64'
-    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.24.2/v2ray-linux-64.zip'
-    source_sha256 '2d763daae301869bdb1e8cc5ef851bd5f8a3eefd2cd31c400b7830c36398d903'
+    source_url 'https://github.com/v2ray/v2ray-core/releases/download/v4.28.2/v2ray-linux-64.zip'
+    source_sha256 'e50e6818a743f842e1bd464f81ed3e42896f68f461718d9ba0f8d75107355c11'
   end
 
   def self.install
     FileUtils.mkdir_p(CREW_DEST_PREFIX + '/share/v2ray')
     FileUtils.cp_r('.', CREW_DEST_PREFIX + '/share/v2ray')
+    FileUtils.chmod('u=x', CREW_DEST_PREFIX + '/share/v2ray/v2ray')
+    FileUtils.chmod('u=x', CREW_DEST_PREFIX + '/share/v2ray/v2ctl')
     FileUtils.mkdir_p(CREW_DEST_PREFIX + '/bin')
     FileUtils.cd(CREW_DEST_PREFIX + '/bin') do
       FileUtils.ln_s(CREW_PREFIX + '/share/v2ray/v2ray', 'v2ray')
@@ -30,8 +32,6 @@ class V2ray < Package
   end
 
   def self.postinstall
-    FileUtils.chmod('u=x,go=x', CREW_PREFIX + '/share/v2ray/v2ray')
-    FileUtils.chmod('u=x,go=x', CREW_PREFIX + '/share/v2ray/v2ctl')
     puts
     puts 'To start using v2ray, type `v2ray`.'.lightblue
     puts
