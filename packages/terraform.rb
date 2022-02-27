d841f217c09219e78469b72644ae6e741b2d343d
Ed Reel <edreel@gmail.com>
2022-02-26 20:43:52 -0600

    Terraform 1.1.0 => 1.1.6 (#6785)

--- packages/terraform.rb
+++ packages/terraform.rb
@@ -3,7 +3,7 @@ require 'package'
 class Terraform < Package
   description 'Terraform is a tool for building, changing, and combining infrastructure safely and efficiently.'
   homepage 'https://www.terraform.io/'
-  version '1.1.0'
+  version '1.1.6'
   license 'Apache-2.0, BSD-2, BSD-4, ECL-2.0, imagemagick, ISC, JSON, MIT, MIT-with-advertising, MPL-2.0 and unicode'
   compatibility 'all'
   source_url ({
@@ -13,10 +13,10 @@ class Terraform < Package
      x86_64: "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip",
   })
   source_sha256 ({
-    aarch64: '28f90802515d2ef8468eafbe4b0125fe812ab383d7707429adb2c8f9ff8aab7d',
-     armv7l: '28f90802515d2ef8468eafbe4b0125fe812ab383d7707429adb2c8f9ff8aab7d',
-       i686: '16225f17149505a42751599acd8e12d01690324f680e7fa2099afed581876916',
-     x86_64: 'a92df4a151d390144040de5d18351301e597d3fae3679a814ea57554f6aa9b24',
+    aarch64: '2b977ed400747f03374262f171d3deba710f303a0be2daafe9ae2fd65ed851f3',
+     armv7l: '2b977ed400747f03374262f171d3deba710f303a0be2daafe9ae2fd65ed851f3',
+       i686: 'e5a8bcbe048c4c3f510106d434208ceab22c627b314007145a2d0ec1c3086825',
+     x86_64: '3e330ce4c8c0434cdd79fe04ed6f6e28e72db44c47ae50d01c342c8a2b05d331',
   })
 
   def self.install
