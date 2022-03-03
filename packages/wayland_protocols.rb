f71f32d7ad6ecf84dffad28637ade37afb6c2d45
Satadru Pramanik <satadru@gmail.com>
2022-03-03 13:20:11 -0500

    wayland_protocols -> 1.25

--- packages/wayland_protocols.rb
+++ packages/wayland_protocols.rb
@@ -3,23 +3,23 @@ require 'package'
 class Wayland_protocols < Package
   description 'Wayland is a protocol for a compositor to talk to its clients.'
   homepage 'https://wayland.freedesktop.org/'
-  version '1.24'
+  version '1.25'
   license 'MIT'
   compatibility 'all'
   source_url 'https://gitlab.freedesktop.org/wayland/wayland-protocols.git'
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.24_armv7l/wayland_protocols-1.24-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.24_armv7l/wayland_protocols-1.24-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.24_i686/wayland_protocols-1.24-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.24_x86_64/wayland_protocols-1.24-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.25_armv7l/wayland_protocols-1.25-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.25_armv7l/wayland_protocols-1.25-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.25_i686/wayland_protocols-1.25-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wayland_protocols/1.25_x86_64/wayland_protocols-1.25-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'c633f35563e10ddb018f0d546f5d3a3414d60981f477fee9e66c638018e4fe2a',
-     armv7l: 'c633f35563e10ddb018f0d546f5d3a3414d60981f477fee9e66c638018e4fe2a',
-       i686: '024424651a90ae99a88c7f7f5fef9b207b1c74e67574f3e86fb09b9d2a051723',
-     x86_64: '985f6edc42f6a1bded7c38563ca45cfa2fa51d9d1128dfa4445998604062db7a'
+    aarch64: '2b7f7735462285555096b5200b63323318b6da02ebf36e1da32320bba202b72f',
+     armv7l: '2b7f7735462285555096b5200b63323318b6da02ebf36e1da32320bba202b72f',
+       i686: '48758272211b03ee657e4cafaf7e71301da90623723cb02676fe5313e810f6c4',
+     x86_64: '5d21d13aa6c092e9287091071411f732cac0353175bab2ba4f5f8b8999762be6'
   })
 
   depends_on 'wayland'
