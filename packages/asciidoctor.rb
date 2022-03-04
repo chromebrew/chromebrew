232ae228121f89afd2d0727618833a5c85ddd63f
Satadru Pramanik <satadru@gmail.com>
2022-03-04 00:31:04 -0500

    asciidoctor => 2.0.17 (#6821)

--- packages/asciidoctor.rb
+++ packages/asciidoctor.rb
@@ -3,22 +3,22 @@ require 'package'
 class Asciidoctor < Package
   description 'A fast text processor & publishing toolchain for converting AsciiDoc to HTML5, DocBook & more.'
   homepage 'https://asciidoctor.org/'
-  version '2.0.16'
+  version '2.0.17'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.16_armv7l/asciidoctor-2.0.16-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.16_armv7l/asciidoctor-2.0.16-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.16_i686/asciidoctor-2.0.16-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.16_x86_64/asciidoctor-2.0.16-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.17_armv7l/asciidoctor-2.0.17-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.17_armv7l/asciidoctor-2.0.17-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.17_i686/asciidoctor-2.0.17-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/asciidoctor/2.0.17_x86_64/asciidoctor-2.0.17-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '523871f0cc68641c49cb2731f0b02c75c7968ed94020f8088a171b8279461601',
-     armv7l: '523871f0cc68641c49cb2731f0b02c75c7968ed94020f8088a171b8279461601',
-       i686: 'a6429f928ec85357e1f18a6ededb251ca27b9a73e87d3ae92f5622ece769b83b',
-     x86_64: '20289399b9d9ec21db5fab0a21e76456f4b3349e05a7e5a7c60f65cb19931635'
+    aarch64: 'b947ceb099358cca321dbdb483e058efd41391c729be866acc40f672b1b82910',
+     armv7l: 'b947ceb099358cca321dbdb483e058efd41391c729be866acc40f672b1b82910',
+       i686: '890a1d900911ad20b4e87808c38ed59d5a88b4bb6343abecca0cb31b85ef8f27',
+     x86_64: '8f6581e50b2ccf4074488575f439caee9e83dcb1dbf24778e2e3ef0b271f1c55'
   })
 
   def self.install
