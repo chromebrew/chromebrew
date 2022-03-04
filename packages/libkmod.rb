7aff51a44632e003da8ca4595722ebfbf9229adb
Satadru Pramanik <satadru@gmail.com>
2022-03-03 20:15:58 -0500

    clear out old binaries

--- packages/libkmod.rb
+++ packages/libkmod.rb
@@ -9,19 +9,6 @@ class Libkmod < Package
   source_url "https://git.kernel.org/pub/scm/utils/kernel/kmod/kmod.git"
   git_hashtag "v#{version}"
 
-  binary_url ({
-     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libkmod/28_armv7l/libkmod-28-chromeos-armv7l.tar.xz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libkmod/28_armv7l/libkmod-28-chromeos-armv7l.tar.xz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libkmod/28_i686/libkmod-28-chromeos-i686.tar.xz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libkmod/28_x86_64/libkmod-28-chromeos-x86_64.tar.xz',
-  })
-  binary_sha256 ({
-     aarch64: '57c5726cd378bf3e1458cdfebead52b9279557af8229550d5f91f0a3e04d4f10',
-      armv7l: '57c5726cd378bf3e1458cdfebead52b9279557af8229550d5f91f0a3e04d4f10',
-        i686: '95c805d62289d8751682d4bb1c8046deeae68958e0f06f6863e30eb74e941864',
-      x86_64: '258578c242a793bfda78e9dc98a9e029a8788e9971a809d0c6e42c2abe10b317',
-  })
-
   depends_on 'py3_cython'
   depends_on 'xzutils'
 
