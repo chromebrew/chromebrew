ab76f505071f00ea22311203dbd69dda2a1c8dc7
Satadru Pramanik <satadru@gmail.com>
2022-03-10 14:58:23 -0500

    fix fetchtags, start gtk3 update

--- packages/gtk3.rb
+++ packages/gtk3.rb
@@ -3,7 +3,7 @@ require 'package'
 class Gtk3 < Package
   description 'GTK+ is a multi-platform toolkit for creating graphical user interfaces.'
   homepage 'https://developer.gnome.org/gtk3/3.0/'
-  @_ver = '3.24.31'
+  @_ver = '3.24.33'
   @_ver_prelastdot = @_ver.rpartition('.')[0]
   version @_ver
   license 'LGPL-2.1'
@@ -11,51 +11,33 @@ class Gtk3 < Package
   source_url 'https://gitlab.gnome.org/GNOME/gtk.git'
   git_hashtag @_ver
 
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.31_armv7l/gtk3-3.24.31-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.31_armv7l/gtk3-3.24.31-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.31_i686/gtk3-3.24.31-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.31_x86_64/gtk3-3.24.31-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '7df7bcce97f9ceca4bb6819b2306f3d5b0470a7b3a789d10a35b2e9071f386de',
-     armv7l: '7df7bcce97f9ceca4bb6819b2306f3d5b0470a7b3a789d10a35b2e9071f386de',
-       i686: '6fdcaf10062f4c1a8880c3d3eb897cacb27193f5817346041b4048608f362f74',
-     x86_64: '6273b79151dc38fa8a240100e75b72a3cba6a868611f3c25fe7862f98751a106'
-  })
-
   # L = Logical Dependency, R = Runtime Dependency
-  depends_on 'docbook' => :build
-  depends_on 'ghostscript' => :build
-  depends_on 'gobject_introspection' => :build
-  depends_on 'iso_codes' => :build
-  depends_on 'libsass' => :build
-  depends_on 'libspectre' => :build
-  depends_on 'mesa' => :build
-  depends_on 'valgrind' => :build
-  depends_on 'graphene' => :build # Do we need this?
-  depends_on 'graphite' => :build # Do we need this?
-  depends_on 'libdeflate' => :build # Do we need this?
-  depends_on 'libjpeg' => :build # Do we need this?
-  depends_on 'py3_six' => :build # Do we need this?
   depends_on 'adwaita_icon_theme' # L
-  depends_on 'cantarell_fonts' # L
-  depends_on 'gnome_icon_theme' # L
-  depends_on 'hicolor_icon_theme' # L
-  depends_on 'shared_mime_info' # L
-  depends_on 'xdg_base' # L
   depends_on 'atk' # R
   depends_on 'at_spi2_atk' # R
   depends_on 'cairo' # R
+  depends_on 'cantarell_fonts' # L
   depends_on 'cups' # R
+  depends_on 'docbook' => :build
   depends_on 'fontconfig' # R
   depends_on 'freetype' # R
   depends_on 'fribidi' # R
   depends_on 'gdk_pixbuf' # R
+  depends_on 'ghostscript' => :build
   depends_on 'glib' # R
+  depends_on 'gnome_icon_theme' # L
+  depends_on 'gobject_introspection' => :build
+  depends_on 'graphene' => :build # Do we need this?
+  depends_on 'graphite' => :build # Do we need this?
   depends_on 'harfbuzz' # R
+  depends_on 'hicolor_icon_theme' # L
+  depends_on 'iso_codes' => :build
   depends_on 'json_glib' # R
+  depends_on 'libdeflate' => :build # Do we need this?
   depends_on 'libepoxy' # R
+  depends_on 'libjpeg' => :build # Do we need this?
+  depends_on 'libsass' => :build
+  depends_on 'libspectre' => :build
   depends_on 'libx11' # R
   depends_on 'libxcomposite' # R
   depends_on 'libxcursor' # R
@@ -66,9 +48,14 @@ class Gtk3 < Package
   depends_on 'libxi' # R
   depends_on 'libxkbcommon' # R
   depends_on 'libxrandr' # R
+  depends_on 'mesa' => :build
   depends_on 'pango' # R
+  depends_on 'py3_six' => :build # Do we need this?
   depends_on 'rest' # R
+  depends_on 'shared_mime_info' # L
+  depends_on 'valgrind' => :build
   depends_on 'wayland' # R
+  depends_on 'xdg_base' # L
 
   def self.patch
     # Use locally build subprojects
