0fe43ffa8afb13e3aee063df910dcab2391934f2
supechicken <supechicken666@gmail.com>
2022-03-11 02:07:16 +0800

    crew: Rewrite dependency resolver algorithm (#6840)
    * Rewrite dependency resolver algorithm
    
    * Add color for build dependencies, show satisfied dependencies
    
    * Update help message
    
    * Fix typo

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.22.10'
+CREW_VERSION = '1.23.0'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
