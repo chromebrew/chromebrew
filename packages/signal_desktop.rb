8c487937d5877cd6d698ea18678ab7df9b8b9bf7
supechicken <supechicken666@gmail.com>
2022-03-11 21:58:19 +0800

    signal_desktop.rb: use source_url

--- packages/signal_desktop.rb
+++ packages/signal_desktop.rb
@@ -6,21 +6,13 @@ class Signal_desktop < Package
   version '5.34.0'
   license 'AGPL-3.0'
   compatibility 'x86_64'
-  source_url 'SKIP'
+  source_url "http://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_#{version}_amd64.deb"
+  source_sha256 'b94e1626d77dab0ae31e0b0ad280e0fe909fff995f361a0c0c477f9475272f09'
 
   depends_on 'alien' => :build
   depends_on 'at_spi2_atk'
   depends_on 'gtk3'
   depends_on 'sommelier'
-
-  def self.build
-    system "curl -L#O http://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_#{version}_amd64.deb"
-    unless Digest::SHA256.hexdigest( File.read("signal-desktop_#{version}_amd64.deb") ) == 'b94e1626d77dab0ae31e0b0ad280e0fe909fff995f361a0c0c477f9475272f09'
-      abort 'Checksum mismatch. :/ Try again.'.lightred
-    end
-    system "alien -tc signal-desktop_#{version}_amd64.deb"
-    system "tar xvf signal-desktop-#{version}.tgz"
-  end
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
