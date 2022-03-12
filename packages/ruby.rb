93aa6fd0a5d87de19ca980aab87271bad66f73de
Satadru Pramanik <satadru@gmail.com>
2022-03-11 21:44:51 -0500

    Ruby -> 3.1.1 (#6850)

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -3,55 +3,45 @@ require 'package'
 class Ruby < Package
   description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
   homepage 'https://www.ruby-lang.org/en/'
-  version '3.1.0'
+  version '3.1.1'
   license 'Ruby-BSD and BSD-2'
   compatibility 'all'
   source_url 'https://github.com/ruby/ruby.git'
-  git_hashtag "v#{version.gsub('.', '_')}"
+  git_hashtag "v#{version.tr('.', '_')}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.0_armv7l/ruby-3.1.0-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.0_armv7l/ruby-3.1.0-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.0_i686/ruby-3.1.0-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.0_x86_64/ruby-3.1.0-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.1_armv7l/ruby-3.1.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.1_armv7l/ruby-3.1.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.1_i686/ruby-3.1.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.1_x86_64/ruby-3.1.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '584d70c21a759b5a83a7ca0315020f0be8132229687b797cf029f29c1a8bccc5',
-     armv7l: '584d70c21a759b5a83a7ca0315020f0be8132229687b797cf029f29c1a8bccc5',
-       i686: 'd249ad97ab91d80d256f169233f9e927192624b293a99519c1f2198ceb22d8be',
-     x86_64: 'c560190cdc8f41ffb0c90a80652f152fe6ca8ff993940bf845aa98481e866100'
+    aarch64: '3d929442ae74fbbeef4697c2fd16ce7b942847a91cfffada61d8cf42fe23b54c',
+     armv7l: '3d929442ae74fbbeef4697c2fd16ce7b942847a91cfffada61d8cf42fe23b54c',
+       i686: 'fe5e320182698789dcadf8afdf18c9e3e249e77e3717c1bb6a8cf82419247982',
+     x86_64: '6ffc2c0304526000b3ebefb1f653b27f1e449dd9dc5e0a6a2ed762aaca85398d'
   })
-
-  source_sha256 '1a0e0b69b9b062b6299ff1f6c6d77b66aff3995f63d1d8b8771e7a113ec472e2'
 
   depends_on 'ca_certificates'
   depends_on 'libyaml' # This is needed to install gems
   # at run-time, system's gmp, openssl, readline and zlibpkg are possible to use
-
-  def self.patch
-    case ARCH
-    when 'i686'
-      system "sed -i 's,mingw*|emscripten*,mingw*|emscripten*|linux*,g' configure.ac"
-      system "sed -i 's/-fstack-protector-strong//g' configure.ac"
-      system "sed -i 's/-fstack-protector/-fno-stack-protector/g' configure.ac"
-    end
-  end
+  no_env_options
 
   def self.build
-    # The download from ruby-lang.org doesn't need autoconf run,
-    # but the download from github does.
+    # mold is not working with the ruby build for both i686 and x86_64
+    @crew_linker = 'gold'
     system '[ -x configure ] || autoreconf -fiv'
-    system "CC='gcc -fno-stack-protector' \
-    CXX='g++ -fno-stack-protector' \
+    system "LD=ld.#{@crew_linker} \
     RUBY_TRY_CFLAGS='stack_protector=no' \
     RUBY_TRY_LDFLAGS='stack_protector=no' \
-    CFLAGS='-ltinfow -flto=auto' CXXFLAGS='-flto=auto' \
-    LDFLAGS='-flto=auto' \
-    optflags='-flto=auto' \
+    CFLAGS='-flto -fuse-ld=#{@crew_linker} #{CREW_LINKER_FLAGS}' \
+    CXXFLAGS='-flto -fuse-ld=#{@crew_linker} #{CREW_LINKER_FLAGS}' \
+    LDFLAGS='-flto' \
+    optflags='-flto' \
     ./configure #{CREW_OPTIONS} \
     --enable-shared \
     --disable-fortify-source"
-    system 'make'
+    system "LD=ld.#{@crew_linker} make"
   end
 
   def self.check
