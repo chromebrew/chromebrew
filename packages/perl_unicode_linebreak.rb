b43683677a3d8e9910dbd9d40b52618ee45ed814
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-03-16 08:43:14 -0700

    update Unicode::Linebreak

---
+++ packages/perl_unicode_linebreak.rb
@@ -0,0 +1,24 @@
+require 'package'
+
+class Perl_unicode_linebreak < Package
+  description 'Unicode::LineBreak - UAX #14 Unicode Line Breaking Algorithm'
+  homepage 'http://search.cpan.org/~nezumi/Unicode-LineBreak-2018.003/lib/Unicode/LineBreak.pod'
+  version '2019.001-2'
+  license 'GPL-1+ or Artistic'
+  compatibility 'all'
+  source_url 'https://github.com/hatukanezumi/Unicode-LineBreak.git'
+  git_hashtag 'Unicode-LineBreak-2019.001'
+
+  def self.prebuild
+    system 'perl', 'Makefile.PL'
+    system "sed -i 's,/usr/local,#{CREW_PREFIX},g' Makefile"
+  end
+
+  def self.build
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+end
