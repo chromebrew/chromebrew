d4708fba8b142bb9ef571e09e5dc770a2bfe442c
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-03-16 11:03:11 -0700

    File::Tail correct spelling

--- packages/perl_file_tail.rb
+++ packages/perl_file_tail.rb
@@ -1,7 +1,7 @@
 require 'package'
 
 class Perl_file_tail < Package
-  description 'File::Tail - Perl extension for reading from continously updated files'
+  description 'File::Tail - Perl extension for reading from continuously updated files'
   homepage 'https://metacpan.org/pod/File::Tail'
   version '1.3-2'
   license 'GPL-1+ or Artistic'
