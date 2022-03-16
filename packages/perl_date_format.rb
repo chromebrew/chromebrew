f568020ca3b5a4f552e701ceb23b492fa585cb8b
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-03-16 08:48:03 -0700

    update Date::Format

--- packages/perl_date_format.rb
+++ packages/perl_date_format.rb
@@ -9,21 +9,12 @@ class Perl_date_format < Package
   source_url 'https://cpan.metacpan.org/authors/id/A/AT/ATOOMIC/TimeDate-2.33.tar.gz'
   source_sha256 'c0b69c4b039de6f501b0d9f13ec58c86b040c1f7e9b27ef249651c143d605eb2'
 
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_date_format/2.33-1_armv7l/perl_date_format-2.33-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_date_format/2.33-1_armv7l/perl_date_format-2.33-1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_date_format/2.33-1_i686/perl_date_format-2.33-1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_date_format/2.33-1_x86_64/perl_date_format-2.33-1-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    aarch64: '58c1886e79ef37627314f0ffa838d11190e45de6ff17e02de21af1d129bff4b3',
-     armv7l: '58c1886e79ef37627314f0ffa838d11190e45de6ff17e02de21af1d129bff4b3',
-       i686: 'f81f2743e65366196693cf781d30ec7b0a0c9387cee583b6789245f81eb2ffeb',
-     x86_64: '2998c9fed1b23f2b438c4320dae495129b3f7067e770cce38983a868088f1db5'
-  })
+  def self.prebuild
+    system 'perl', 'Makefile.PL'
+    system "sed -i 's,/usr/local,#{CREW_PREFIX},g' Makefile"
+  end
 
   def self.build
-    system 'perl', 'Makefile.PL'
     system 'make'
   end
 
@@ -31,6 +22,6 @@ class Perl_date_format < Package
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
 
     # Conflict with perl_xml_parser package.
-    FileUtils.rm "#{CREW_PREFIX}/lib/perl5/site_perl/5.32.1/Date/Language/Amharic.pm"
+    #FileUtils.rm "#{CREW_PREFIX}/lib/perl5/site_perl/5.32.1/Date/Language/Amharic.pm"
   end
 end
