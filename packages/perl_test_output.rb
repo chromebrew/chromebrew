4af9357e558cf1ca214581d7cc830a8b8d833169
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-03-16 08:44:31 -0700

    add Test::Output

---
+++ packages/perl_test_output.rb
@@ -0,0 +1,24 @@
+require 'package'
+
+class Perl_yaml_tiny < Package
+  description 'Test::Output - Utilities to test STDOUT and STDERR messages.'
+  homepage 'https://metacpan.org/pod/Test::Output'
+  version '1.033'
+  license 'GPL-1+ or Artistic'
+  compatibility 'all'
+  source_url 'https://cpan.metacpan.org/authors/id/B/BD/BDFOY/Test-Output-1.033.tar.gz'
+  source_sha256 'f6a8482740b075fad22aaf4d987d38ef927c6d2b452d4ae0d0bd8f779830556e'
+
+  def self.prebuild
+    system 'perl', 'Makefile.PL'
+    system "sed -i 's,/usr/local,#{CREW_PREFIX},g' Makefile"
+  end
+
+  def self.build
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+end
