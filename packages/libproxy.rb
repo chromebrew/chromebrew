3e569d71f90c9b453219d981c12e39bdcf84103d
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-03-17 10:34:08 -0700

    binaries (x86_64, i686) & rubocop edits

--- packages/libproxy.rb
+++ packages/libproxy.rb
@@ -10,6 +10,15 @@ class Libproxy < Package
   source_url 'https://github.com/libproxy/libproxy.git'
   git_hashtag @_ver
 
+  binary_url({
+    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libproxy/0.4.17_i686/libproxy-0.4.17-chromeos-i686.tar.zst',
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libproxy/0.4.17_x86_64/libproxy-0.4.17-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    i686: '43aede3ba83cc73fb0cbe8040e8e0c36db419bee4932550eae31cf004351af07',
+  x86_64: '4b2a329496d51d6ef1c97bb30cf5530af784800c2df634abbae428f6cca4a04c'
+  })
+
   depends_on 'dbus'
   depends_on 'vala' => :build
   depends_on 'glib'
@@ -18,7 +27,7 @@ class Libproxy < Package
   def self.build
     Dir.mkdir 'builddir'
     Dir.chdir 'builddir' do
-      #system "cmake -G Ninja #{CREW_CMAKE_OPTIONS} .."
+      # system "cmake -G Ninja #{CREW_CMAKE_OPTIONS} .."
       system "cmake -G 'Unix Makefiles' #{CREW_CMAKE_OPTIONS} \
               -DLIBEXEC_INSTALL_DIR=#{CREW_LIB_PREFIX} \
               -DLIB_INSTALL_DIR=#{CREW_LIB_PREFIX} \
@@ -41,16 +50,16 @@ class Libproxy < Package
               -Wno-dev .."
       system 'make'
     end
-    #system 'samu -C builddir'
+    # system 'samu -C builddir'
   end
 
   def self.install
-    #system "samu -C builddir install"
+    # system "samu -C builddir install"
     Dir.chdir 'builddir' do system "make DESTDIR=#{CREW_DEST_DIR} install" end
   end
 
   def self.check
-    #system "samu -C builddir test"
-    Dir.chdir 'builddir' do system "make test" end
+    # system "samu -C builddir test"
+    Dir.chdir 'builddir' do system 'make test' end
   end
 end
