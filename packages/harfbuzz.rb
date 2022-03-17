6b4b02f18bba92e3052189a8b4e61c26edf2f840
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-03-17 12:38:17 -0700

    unbundle libpng from harfbuzz

--- packages/harfbuzz.rb
+++ packages/harfbuzz.rb
@@ -4,24 +4,11 @@ class Harfbuzz < Package
   description 'HarfBuzz is an OpenType text shaping engine.'
   homepage 'https://www.freedesktop.org/wiki/Software/HarfBuzz/'
   @_ver = '4.0.0'
-  version "#{@_ver}-1"
+  version "#{@_ver}-2"
   license 'Old-MIT, ISC and icu'
   compatibility 'all'
   source_url 'https://github.com/harfbuzz/harfbuzz.git'
   git_hashtag @_ver
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/4.0.0-1_armv7l/harfbuzz-4.0.0-1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/4.0.0-1_armv7l/harfbuzz-4.0.0-1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/4.0.0-1_i686/harfbuzz-4.0.0-1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/harfbuzz/4.0.0-1_x86_64/harfbuzz-4.0.0-1-chromeos-x86_64.tar.zst'
-  })
-  binary_sha256({
-    aarch64: '45c23425ee8a671df2f26dd8db51962b5a9de734042ae54431a6504339ffc049',
-     armv7l: '45c23425ee8a671df2f26dd8db51962b5a9de734042ae54431a6504339ffc049',
-       i686: 'fe23c57f63f0fd9d03cd8679076d0aa6aeb0db9880efd1099814385fe53c878f',
-     x86_64: 'c9ad1d2c137cfa65efcfbc855fc6b2a616130653d18ff730b53ece6676513a44'
-  })
 
   # provides libpng, freetype (sans harfbuzz), and ragel
   # depends_on 'cairo' => :build (cairo is only needed for tests and tools)
@@ -34,6 +21,7 @@ class Harfbuzz < Package
   depends_on 'graphite'
   depends_on 'icu4c'
   depends_on 'libffi'
+  depends_on 'libpng'
   depends_on 'pcre'
   depends_on 'py3_six' => :build
   depends_on 'zlibpkg'
--- packages/libpng.rb
+++ packages/libpng.rb
@@ -1,13 +1,29 @@
 require 'package'
 
 class Libpng < Package
-  description 'libpng is the official PNG reference library. Now bundled with harfbuzz.'
+  description 'libpng is the official PNG reference library, with APNG support packaged in.'
   homepage 'http://libpng.org/pub/png/libpng.html'
-  version "1.0"
+  version '1.6.37'
   license 'libpng2'
   compatibility 'all'
+  source_url 'https://sourceforge.net/projects/libpng/files/libpng16/1.6.37/libpng-1.6.37.tar.xz'
+  source_sha256 '505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca'
 
-  depends_on 'harfbuzz'
-  is_fake
+  def self.patch
+    downloader 'https://sourceforge.net/projects/libpng-apng/files/libpng16/1.6.37/libpng-1.6.37-apng.patch.gz'
+    system 'gunzip -c libpng-1.6.37-apng.patch.gz | patch -Np1'
+  end
 
+  def self.build
+    system "./configure #{CREW_OPTIONS}"
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+
+  def self.check
+    system 'make', 'check'
+  end
 end
