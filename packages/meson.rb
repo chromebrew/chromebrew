f957c2c36304ebbed7390684c4fcf9773af5ae28
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-03-18 12:40:05 -0700

    meson => 0.61.3 (#6872)
    * meson => 0.61.3
    
    * meson binaries x86_64 i686
    
    * Add arm binaries
    
    Co-authored-by: Ed Reel <edreel@gmail.com>

--- packages/meson.rb
+++ packages/meson.rb
@@ -3,7 +3,7 @@ require 'package'
 class Meson < Package
   description 'Meson is an open source build system meant to be both extremely fast and user friendly.'
   homepage 'https://mesonbuild.com/'
-  @_ver = '0.61.1'
+  @_ver = '0.61.3'
   version @_ver
   license 'Apache-2.0'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Meson < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.1_armv7l/meson-0.61.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.1_armv7l/meson-0.61.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.1_i686/meson-0.61.1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.1_x86_64/meson-0.61.1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_armv7l/meson-0.61.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_armv7l/meson-0.61.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_i686/meson-0.61.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_x86_64/meson-0.61.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '89471f377e91165418858f9dfb13e524e816f878ea3944636af042c9ddb42ffb',
-     armv7l: '89471f377e91165418858f9dfb13e524e816f878ea3944636af042c9ddb42ffb',
-       i686: 'b941660b357c7c215ae2da568f74184106acf942ba5e3452058f6c1b57e53637',
-     x86_64: 'a17eb696f9f1ef5c6d03143f0f8c7ad2ee0b7454604660f302c738402e0f8197'
+    aarch64: '6eaaf072f96c0e188081cb326e364881de6c565f4bd6e6e4a715443a6347cf30',
+     armv7l: '6eaaf072f96c0e188081cb326e364881de6c565f4bd6e6e4a715443a6347cf30',
+       i686: '18788f704936ea4780d23bf8c62cc92c285061f8234319f9cda94183aaf2f524',
+     x86_64: '4e6a6c90dd4bd7383c63d8b8e4c4bbddac0bf01b11cfcb371f991c2554ece7d6'
   })
 
   depends_on 'ninja'
