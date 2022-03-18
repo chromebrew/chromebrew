deeb301d2385dc5fc008aa8f3c9c7c66bcedbefb
Ed Reel <edreel@gmail.com>
2022-03-18 19:38:53 +0000

    Add arm binaries

--- packages/meson.rb
+++ packages/meson.rb
@@ -11,12 +11,16 @@ class Meson < Package
   git_hashtag @_ver
 
   binary_url({
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_i686/meson-0.61.3-chromeos-i686.tar.zst',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_x86_64/meson-0.61.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_armv7l/meson-0.61.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_armv7l/meson-0.61.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_i686/meson-0.61.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/meson/0.61.3_x86_64/meson-0.61.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    i686: '18788f704936ea4780d23bf8c62cc92c285061f8234319f9cda94183aaf2f524',
-  x86_64: '4e6a6c90dd4bd7383c63d8b8e4c4bbddac0bf01b11cfcb371f991c2554ece7d6'
+    aarch64: '6eaaf072f96c0e188081cb326e364881de6c565f4bd6e6e4a715443a6347cf30',
+     armv7l: '6eaaf072f96c0e188081cb326e364881de6c565f4bd6e6e4a715443a6347cf30',
+       i686: '18788f704936ea4780d23bf8c62cc92c285061f8234319f9cda94183aaf2f524',
+     x86_64: '4e6a6c90dd4bd7383c63d8b8e4c4bbddac0bf01b11cfcb371f991c2554ece7d6'
   })
 
   depends_on 'ninja'
