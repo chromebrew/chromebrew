e1f20ad1591c93f217974602d7e6bbe9a0b8ad62
Ed Reel <edreel@gmail.com>
2022-03-19 21:52:15 +0000

    Update vim_runtime, vim and gvim 8.2.3892 => 8.2.4594

--- packages/vim.rb
+++ packages/vim.rb
@@ -3,7 +3,7 @@ require 'package'
 class Vim < Package
   description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient.'
   homepage 'http://www.vim.org/'
-  @_ver = '8.2.3892'
+  @_ver = '8.2.4594'
   version @_ver
   license 'GPL-2'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Vim < Package
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.3892_armv7l/vim-8.2.3892-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.3892_armv7l/vim-8.2.3892-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.3892_i686/vim-8.2.3892-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.3892_x86_64/vim-8.2.3892-chromeos-x86_64.tar.xz',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.4594_armv7l/vim-8.2.4594-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.4594_armv7l/vim-8.2.4594-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.4594_i686/vim-8.2.4594-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vim/8.2.4594_x86_64/vim-8.2.4594-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e06450c39c01019d88335bd6b0f4a8929126e72207c07f060a6c42224f0d235c',
-     armv7l: 'e06450c39c01019d88335bd6b0f4a8929126e72207c07f060a6c42224f0d235c',
-       i686: '092ff95aa22452f3f5d8b27d6e637504cfd545c95d736549523d92d0b9bcdb8f',
-     x86_64: '96b12b7ea83046639241bae0d9e52b60b38b3d202c6b5f143ebbb86ac780952c',
+    aarch64: '67807f3bb5fa193a8997e484b01f7e187a183e6060e602240a26065b4f96f623',
+     armv7l: '67807f3bb5fa193a8997e484b01f7e187a183e6060e602240a26065b4f96f623',
+       i686: '156f8c4cc69a4cdf79ef6fc2d2abaad0bcbf76241d9a16a3f60a3a2340be3d2f',
+     x86_64: '69ea16d8dbdb6f2ea932d766c3222b1a870e0344e5d44dbd84fb16dbbb5f78cb'
   })
 
   depends_on 'vim_runtime'
