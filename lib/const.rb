48b3fc14d4f228c2f102763561611ebd6bb4d31d
supechicken <supechicken666@gmail.com>
2022-03-20 23:59:27 +0800

    Correct version

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.23.3-1'
+CREW_VERSION = '1.23.4'
 
 ARCH_ACTUAL = `uname -m`.chomp
 # This helps with virtualized builds on aarch64 machines
