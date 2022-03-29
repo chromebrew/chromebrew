287ac6cf1a7dc433a027a014ddd9e65355bf255e
Satadru Pramanik <satadru@gmail.com>
2022-03-29 12:05:38 -0700

    Ffmpeg rebuild (#6913)
    * ffmpeg rebuilds
    
    * add ffmpeg binaries

--- packages/libva.rb
+++ packages/libva.rb
@@ -4,23 +4,23 @@ class Libva < Package
   description 'Libva is an implementation for VA-API (Video Acceleration API)'
   homepage 'https://01.org/linuxmedia'
   @_ver = '2.14.0'
-  version @_ver
+  version "#{@_ver}-2"
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/intel/libva.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0_armv7l/libva-2.14.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0_armv7l/libva-2.14.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0_i686/libva-2.14.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0_x86_64/libva-2.14.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_armv7l/libva-2.14.0-2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_armv7l/libva-2.14.0-2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_i686/libva-2.14.0-2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_x86_64/libva-2.14.0-2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '1177ea4f0e3d8218a917070ba4943a8850ffb48c92be88c53373a77c35063c08',
-     armv7l: '1177ea4f0e3d8218a917070ba4943a8850ffb48c92be88c53373a77c35063c08',
-       i686: 'e77eafe870dde013f5868eabebdd1ea0980ae642c28b11d5d4ee8b8f60c0fbfe',
-     x86_64: '4f330f77b5cdec5d11bcce6436c601db00c3def6fe07ebaaebd163068eb566ed'
+    aarch64: 'fccd32adece0312102e7294dc451ca4c8abb267abaf4726470da491891423101',
+     armv7l: 'fccd32adece0312102e7294dc451ca4c8abb267abaf4726470da491891423101',
+       i686: '98c2d3379fe1b513f4c1f8edfdc7424e84d940408141d63a065c29a92503e162',
+     x86_64: '7baea1dbbeb4fac85e384403151c0b8baefb366c8d7c982bac63c6ef15990fde'
   })
 
   depends_on 'libdrm'
@@ -42,5 +42,9 @@ class Libva < Package
 
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+  end
+
+  def self.check
+    system 'ninja -C builddir test'
   end
 end
--- packages/zvbi.rb
+++ packages/zvbi.rb
@@ -3,29 +3,36 @@ require 'package'
 class Zvbi < Package
   description 'The Zapping VBI library, in short ZVBI, provides functions to capture and decode VBI data.'
   homepage 'http://zapping.sourceforge.net/ZVBI/'
-  version '0.2.35-1'
-  compatibility 'all'
+  version '0.2.35-2'
   license 'GPL-2 and LGPL-2'
+  compatibility 'all'
   source_url 'https://downloads.sourceforge.net/project/zapping/zvbi/0.2.35/zvbi-0.2.35.tar.bz2'
   source_sha256 'fc883c34111a487c4a783f91b1b2bb5610d8d8e58dcba80c7ab31e67e4765318'
 
-  binary_url ({
-     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-1_armv7l/zvbi-0.2.35-1-chromeos-armv7l.tpxz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-1_armv7l/zvbi-0.2.35-1-chromeos-armv7l.tpxz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-1_i686/zvbi-0.2.35-1-chromeos-i686.tpxz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-1_x86_64/zvbi-0.2.35-1-chromeos-x86_64.tpxz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-2_armv7l/zvbi-0.2.35-2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-2_armv7l/zvbi-0.2.35-2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-2_i686/zvbi-0.2.35-2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zvbi/0.2.35-2_x86_64/zvbi-0.2.35-2-chromeos-x86_64.tar.zst'
   })
-  binary_sha256 ({
-     aarch64: '41b4539eea8ae06c8ba26874c084cbda7fb2f241e6529bcb2e22906fb81f6ce7',
-      armv7l: '41b4539eea8ae06c8ba26874c084cbda7fb2f241e6529bcb2e22906fb81f6ce7',
-        i686: '2957624655028f18bb32a4ddc5a19b6a114daa756fb4ccdfb61fb44e163bde22',
-      x86_64: 'a7ae368f0e5f3d010a44928d0536f809aaca88b28ffe5a037d4e2d31349c9603',
+  binary_sha256({
+    aarch64: '8540c8bf13aa2b9e8df2e1652d11d54c3eee1cc94aa8f282d9f1fa9d72fbf61f',
+     armv7l: '8540c8bf13aa2b9e8df2e1652d11d54c3eee1cc94aa8f282d9f1fa9d72fbf61f',
+       i686: 'bc08d5d03f44f8db298e76fa5dd25328d06e6d88914ea1944092d4dfdc1eb2c2',
+     x86_64: '1646789cf7bae04f328d2000d5a40829798f9aa6a0955cd2b7ce41aa84a56a1d'
   })
 
-  depends_on 'libpng'
+  depends_on 'harfbuzz'
+
+  def self.patch
+    system 'filefix'
+    # png.h path isn't found for build properly
+    @png_h_path = `crew whatprovides png.h | grep "png.h$" | awk '{print $2}'`.chomp.gsub("#{CREW_PREFIX}/include/", '')
+    system "sed -i 's,png.h,#{@png_h_path},g' src/exp-gfx.c"
+  end
 
   def self.build
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
+    system "./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
