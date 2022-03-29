0afceec9e24561b8f97815923fc00edeec3241b9
Satadru Pramanik <satadru@gmail.com>
2022-03-29 13:00:21 -0400

    ffmpeg rebuilds

--- packages/tesseract.rb
+++ packages/tesseract.rb
@@ -3,28 +3,29 @@ require 'package'
 class Tesseract < Package
   description 'A neural net (LSTM) based OCR engine which is focused on line recognition & an older OCR engine which recognizes character patterns.'
   homepage 'https://github.com/tesseract-ocr/tesseract'
-  @_ver = '4.1.1'
-  version "#{@_ver}-2"
+  @_ver = '5.1.0'
+  version @_ver
   license 'Apache-2.0'
   compatibility 'all'
-  source_url "https://github.com/tesseract-ocr/tesseract/archive/#{@_ver}.tar.gz"
-  source_sha256 '2a66ff0d8595bff8f04032165e6c936389b1e5727c3ce5a27b3e059d218db1cb'
+  source_url 'https://github.com/tesseract-ocr/tesseract.git'
+  git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/4.1.1-2_armv7l/tesseract-4.1.1-2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/4.1.1-2_armv7l/tesseract-4.1.1-2-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/4.1.1-2_i686/tesseract-4.1.1-2-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/4.1.1-2_x86_64/tesseract-4.1.1-2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/5.1.0_armv7l/tesseract-5.1.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/5.1.0_armv7l/tesseract-5.1.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/5.1.0_i686/tesseract-5.1.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tesseract/5.1.0_x86_64/tesseract-5.1.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'dd19e48f5511792037333cb2b6945c9dd37cc87233d815932cdbf8a43bb30d01',
-     armv7l: 'dd19e48f5511792037333cb2b6945c9dd37cc87233d815932cdbf8a43bb30d01',
-       i686: 'eb365e5d43659b1ea285aaf81db2dc9c520fb00248212dae571571ec0d5a3d60',
-     x86_64: '611623a2484006193d0a9506cc277f5ffb39a6c8de9229663c6d81da984d2b45'
+    aarch64: '866199b054c0f0b73da596873330d161512a8c78be8f73157732aee7186887d3',
+     armv7l: '866199b054c0f0b73da596873330d161512a8c78be8f73157732aee7186887d3',
+       i686: 'aa036d11efbddd231af55bede9ac2a6cb3c97729727dbd4f1c572888f0e4f030',
+     x86_64: '769d295e22bd108230cceab03b1ea3bc0daf88c6140166883e671339955f7566'
   })
 
   depends_on 'asciidoc' => :build
   depends_on 'cairo'
+  depends_on 'docbook_xsl' => :build
   depends_on 'fontconfig'
   depends_on 'giflib'
   depends_on 'glib'
@@ -32,17 +33,18 @@ class Tesseract < Package
   depends_on 'libarchive'
   depends_on 'libcurl'
   depends_on 'libjpeg'
-  depends_on 'libpng'
+  depends_on 'harfbuzz'
   depends_on 'libtiff'
   depends_on 'pango'
+  git_fetchtags
 
   def self.build
     system '[ -x configure ] || ./autogen.sh'
     system 'filefix'
-    system "[ -f Makefile ] || env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -fno-math-errno -flto=auto' \
-      CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -fno-math-errno -flto=auto' \
-      LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      ./configure #{CREW_OPTIONS}"
+    system "[ -f Makefile ] || #{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
+    # XML_CATALOG_FILES=#{CREW_PREFIX}/etc/xml/catalog does not get set
+    # in the Makefile without this, which results in errors at install
+    system "find . -name 'Makefile' -exec sed -i 's,XML_CATALOG_FILES = ,XML_CATALOG_FILES = #{CREW_PREFIX}/etc/xml/catalog,g' {} +"
     system 'make'
     system 'make training'
   end
@@ -51,6 +53,6 @@ class Tesseract < Package
     system "make DESTDIR=#{CREW_DEST_DIR} install"
     system "make DESTDIR=#{CREW_DEST_DIR} training-install"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/tessdata"
-    system "curl -Ls https://github.com/tesseract-ocr/tessdata/raw/4767ea922bcc460e70b87b1d303ebdfed0897da8/eng.traineddata -o #{CREW_DEST_PREFIX}/share/tessdata/osd.traineddata"
+    system "curl -Ls https://github.com/tesseract-ocr/tessdata/blob/c2b2e0df86272ce11be323f23f96cf656565ed41/eng.traineddata -o #{CREW_DEST_PREFIX}/share/tessdata/osd.traineddata"
   end
 end
