c249d8417f54dd0b86c79b2f848f429f1bb82846
Satadru Pramanik <satadru@gmail.com>
2022-03-29 19:41:23 -0700

    Cmake -> 3.23.0 (#6916)

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,7 +3,7 @@ require 'package'
 class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  @_ver = '3.22.1'
+  @_ver = '3.23.0'
   version @_ver
   license 'CMake'
   compatibility 'all'
@@ -11,17 +11,18 @@ class Cmake < Package
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.22.1_armv7l/cmake-3.22.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.22.1_armv7l/cmake-3.22.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.22.1_i686/cmake-3.22.1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.22.1_x86_64/cmake-3.22.1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.23.0_armv7l/cmake-3.23.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.23.0_armv7l/cmake-3.23.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.23.0_i686/cmake-3.23.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.23.0_x86_64/cmake-3.23.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '679a9c4eeed318606315c9a51eeb231472eb60adb062e1749c5f0b49021008f5',
-     armv7l: '679a9c4eeed318606315c9a51eeb231472eb60adb062e1749c5f0b49021008f5',
-       i686: '9b6726536bff3d0f9fb6b7d19f86f63b5d76780171c64143d03d2f7d131c8a70',
-     x86_64: '0d9a7d101d419c6589e86237236545e20f25a1d74e6999f256ae6f7a188896ba'
+    aarch64: '1e31576926ad32bb574f57d06d26eaf1f49fe578802b8ba672aeaa731ed3dd9e',
+     armv7l: '1e31576926ad32bb574f57d06d26eaf1f49fe578802b8ba672aeaa731ed3dd9e',
+       i686: '0cdbc190fd23f9c35007ae83ebe4c3105aca907b646116ed5383eeb947c48945',
+     x86_64: 'fb97af30472cb4b23944b9c5d1247282edebc2a4debaaef25caf3ddc6cae58e7'
   })
+
 
   depends_on 'expat'
   depends_on 'jsoncpp'
@@ -36,32 +37,32 @@ class Cmake < Package
   depends_on 'libuv'
   depends_on 'llvm' => :build
 
-  def self.patch
-    if Dir.exist? "#{CREW_PREFIX}/include/ncursesw"
-      system "sed -i \"51s,$,\\n  set(CURSES_INCLUDE_PATH #{CREW_PREFIX}/include/ncursesw),\" Modules/FindCurses.cmake"
-    end
-  end
-
   def self.build
     Dir.mkdir 'builddir'
     Dir.chdir 'builddir' do
-      system "#{CREW_ENV_OPTIONS} cmake \
+      # mold linker breaks the build on i686, so just disable for this
+      # build
+      system "cmake \
           -G Ninja \
-          #{CREW_CMAKE_OPTIONS} \
+          #{CREW_CMAKE_OPTIONS.gsub('mold', 'gold')} \
           -DCMAKE_USE_SYSTEM_LIBRARIES=ON \
           -DBUILD_QtDialog=NO \
           .."
     end
-    system 'samu -C builddir'
+    system 'ninja -C builddir'
   end
 
-  # Only the BundleUtiliities test fails here.
+  # Failed tests:
+  # BundleUtilities
+  # GeneratorExpression
+  # RunCMake.CMakeRelease
+  # RunCMake.file-GET_RUNTIME_DEPENDENCIES
   # def self.check
-  #  system "samu -C builddir test"
+  #   system 'ninja -C builddir test || true'
   # end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
     FileUtils.mv "#{CREW_DEST_PREFIX}/doc/", "#{CREW_DEST_PREFIX}/share/"
   end
 end
