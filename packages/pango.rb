87edd0760731403041b6f262e87f050addae43ed
Satadru Pramanik <satadru@gmail.com>
2022-03-30 12:46:29 -0400

    add binaries for fragments prereqs (#6922)

--- packages/pango.rb
+++ packages/pango.rb
@@ -2,8 +2,8 @@ require 'package'
 
 class Pango < Package
   description 'Pango is a library for laying out and rendering of text, with an emphasis on internationalization.'
-  homepage 'http://www.pango.org/'
-  @_ver = '1.50.5'
+  homepage 'https://pango.gnome.org/'
+  @_ver = '1.50.6'
   version @_ver
   license 'LGPL-2+ and FTL'
   compatibility 'all'
@@ -11,17 +11,18 @@ class Pango < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.5_armv7l/pango-1.50.5-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.5_armv7l/pango-1.50.5-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.5_i686/pango-1.50.5-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.5_x86_64/pango-1.50.5-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.6_armv7l/pango-1.50.6-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.6_armv7l/pango-1.50.6-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.6_i686/pango-1.50.6-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.6_x86_64/pango-1.50.6-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'f8b38d2b96f4da443d316f4fd45137324f51c91394c1e9c2a1849b1e04213ad2',
-     armv7l: 'f8b38d2b96f4da443d316f4fd45137324f51c91394c1e9c2a1849b1e04213ad2',
-       i686: '6e6c1b69695ba150cbeb20b72a57fc9de5f585a7d46432c2a1bc8c72d91af0a3',
-     x86_64: 'e0ac8c3dd0b95e6db046d2c3e68536d903a8ba8671dc01e1914852912e3ea082'
+    aarch64: '0bfbe4cd5abae49df9607a31fa0bb63beea7fa035de915bc63ede502830fe0ab',
+     armv7l: '0bfbe4cd5abae49df9607a31fa0bb63beea7fa035de915bc63ede502830fe0ab',
+       i686: '9822afa1329ee5fe532965dc5f381abf6bdaae1c7c3670f36f000a568097beb3',
+     x86_64: '23e71f4700068b9123fdcc7fe5d6750602a077c4c7d0d9954c72411050bf6778'
   })
+
 
   depends_on 'cairo'
   depends_on 'fontconfig'
@@ -31,9 +32,11 @@ class Pango < Package
   depends_on 'gobject_introspection' # add this package to build gtk+, avoid compilation error
   depends_on 'harfbuzz'
   depends_on 'libx11'
+  depends_on 'libxdmcp'
   depends_on 'libxft'
   depends_on 'libxrender'
   depends_on 'py3_six'
+  depends_on 'xorg_proto' => :build
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
@@ -41,12 +44,19 @@ class Pango < Package
     -Dcairo=enabled \
     -Dfreetype=enabled \
     -Dfontconfig=enabled \
-    builddir"
+    -Dlibthai=disabled \
+    -Dgtk_doc=false builddir"
+    # gtk-doc is set to false because I (@saltedcofii) am not
+    # convinced anyone is reading them; it's accessable online anyway
     system 'meson configure builddir'
-    system 'ninja -C builddir'
+    system 'samu -C builddir'
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+  end
+
+  def self.check
+    system 'samu -C builddir test || true'
   end
 end
