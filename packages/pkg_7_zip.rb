9ee2761f5369f1949043496bb2cbb6b6af59a58e
Satadru Pramanik <satadru@gmail.com>
2022-03-30 11:32:20 -0400

    fix armv7l build and add binaries

--- packages/pkg_7_zip.rb
+++ packages/pkg_7_zip.rb
@@ -9,23 +9,36 @@ class Pkg_7_zip < Package
   source_url 'https://www.7-zip.org/a/7z2107-src.tar.xz'
   source_sha256 '213d594407cb8efcba36610b152ca4921eda14163310b43903d13e68313e1e39'
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pkg_7_zip/21.07_armv7l/pkg_7_zip-21.07-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pkg_7_zip/21.07_armv7l/pkg_7_zip-21.07-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pkg_7_zip/21.07_i686/pkg_7_zip-21.07-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pkg_7_zip/21.07_x86_64/pkg_7_zip-21.07-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    aarch64: '83e2070e6eefb32e53260f7d029d3ad7d72fb1c6c64890cd24d0574ffc5e33e1',
+     armv7l: '83e2070e6eefb32e53260f7d029d3ad7d72fb1c6c64890cd24d0574ffc5e33e1',
+       i686: '3b7bb7e973d6c8ffa4a95e4f763050f04bd20f098976a1fc098d43386e794cea',
+     x86_64: 'c3679390e45a5f1cc8cb45f2bab8c7412685505ddf1cbae693233dd1f39d6734'
+  })
+
   no_env_options
 
   # needed for compiling the faster asm binary in x86/x64
-  depends_on 'asmc' => :build if %W[i686 x86_64].include?(ARCH)
+  depends_on 'asmc' => :build if %w[i686 x86_64].include?(ARCH)
 
-  case ARCH
-  when 'x86_64'
-    @_makefile = 'cmpl_gcc_x64.mak'
-  when 'i686'
-    @_makefile = 'cmpl_gcc_x86.mak'
-  else
-    @_makefile = 'cmpl_gcc.mak'
-  end
+  @_makefile = case ARCH
+               when 'x86_64'
+                 'cmpl_gcc_x64.mak'
+               when 'i686'
+                 'cmpl_gcc_x86.mak'
+               else
+                 'cmpl_gcc.mak'
+               end
 
   @_bundles = {
-       'Alone2' => "#{CREW_DEST_PREFIX}/bin/7zz",
-    'Format7zF' => "#{CREW_DEST_LIB_PREFIX}/7z.so"
+    'Alone2' => "#{CREW_DEST_PREFIX}/bin/7zz",
+ 'Format7zF' => "#{CREW_DEST_LIB_PREFIX}/7z.so"
   }
 
   @_bundle_dir = 'CPP/7zip/Bundles'
@@ -35,13 +48,13 @@ class Pkg_7_zip < Package
   end
 
   def self.build
-    @_bundles.each_pair do |bundle, bin_dest|
+    @_bundles.each_pair do |bundle, _bin_dest|
       system 'make', '-f', "../../#{@_makefile}", chdir: "#{@_bundle_dir}/#{bundle}"
     end
   end
 
   def self.install
-    FileUtils.mkdir_p [ "#{CREW_DEST_PREFIX}/bin", CREW_DEST_LIB_PREFIX ]
+    FileUtils.mkdir_p ["#{CREW_DEST_PREFIX}/bin", CREW_DEST_LIB_PREFIX]
 
     @_bundles.each_pair do |bundle, bin_dest|
       bin = File.basename(bin_dest)
