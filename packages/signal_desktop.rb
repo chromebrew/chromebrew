fc503d5532b056cb0d7d108b78ebc55e4e16ab11
Ed Reel <edreel@gmail.com>
2022-03-31 09:46:30 -0500

    Signal_desktop 5.34.0 => 5.37.0 (#6927)
    * Signal_desktop 5.34.0 => 5.37.0
    
    * Add no_compile_needed
    
    Co-authored-by: Satadru Pramanik <satadru@gmail.com>

--- packages/signal_desktop.rb
+++ packages/signal_desktop.rb
@@ -3,11 +3,13 @@ require 'package'
 class Signal_desktop < Package
   description 'Private Messenger for Windows, Mac, and Linux'
   homepage 'https://signal.org/'
-  version '5.34.0'
+  version '5.37.0'
   license 'AGPL-3.0'
   compatibility 'x86_64'
   source_url "http://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_#{version}_amd64.deb"
-  source_sha256 'b94e1626d77dab0ae31e0b0ad280e0fe909fff995f361a0c0c477f9475272f09'
+  source_sha256 '867452ffb0993e4d5b6c18e91cb032c0a56edaeee3820de9e7fa7111d933309f'
+
+  no_compile_needed
 
   depends_on 'at_spi2_atk'
   depends_on 'gtk3'
