09ef556c63652b8a5d19dfc5330a6c1bbeb2d0c1
Satadru Pramanik <satadru@gmail.com>
2022-03-31 21:17:58 -0400

    refactor to use gem installer

--- packages/ruby_rubocop.rb
+++ packages/ruby_rubocop.rb
@@ -8,8 +8,9 @@ class Ruby_rubocop < Package
   homepage 'https://rubocop.org'
   version '1.26.1'
   compatibility 'all'
-  source_url 'https://github.com/rubocop/rubocop.git'
-  git_hashtag "v#{version}"
+  source_url 'SKIP'
+  # source_url 'https://github.com/rubocop/rubocop.git'
+  # git_hashtag "v#{version}"
 
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby_rubocop/1.26.1_armv7l/ruby_rubocop-1.26.1-chromeos-armv7l.tar.zst',
@@ -21,7 +22,7 @@ class Ruby_rubocop < Package
     aarch64: '751d173e446bf5e478b5cf75d0d2bcf3ca67e8a0fbb602667c4e3d224e938c5c',
      armv7l: '751d173e446bf5e478b5cf75d0d2bcf3ca67e8a0fbb602667c4e3d224e938c5c',
        i686: '3ba763b890c2bb0a39cd07567bc248b7dc4b12242537e817ef1650b4fb2ce6bc',
-     x86_64: '43962c9f3309ab26545e693ee1bfa04af0198141305a73939428d86e30086397'
+     x86_64: '59942e42753e15dafe07c9e522fa0e01483e5e2ca66b7c54a347bef8e58d4b8c'
   })
 
   no_fhs
@@ -34,30 +35,42 @@ class Ruby_rubocop < Package
   @xdg_config_home = "#{CREW_PREFIX}/.config" if @xdg_config_home.to_s.empty?
 
   def self.build
-    system 'bundle install'
-    system 'rake build'
+    # system 'bundle install'
+    # system 'rake build'
   end
 
   def self.install
-    system "gem install --build=#{CREW_DEST_DIR} pkg/rubocop-#{version}.gem"
+    # system "gem install --build=#{CREW_DEST_DIR} pkg/rubocop-#{version}.gem"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/.config/rubocop"
-    system "curl -Ls https://github.com/satmandu/chromebrew/raw/rubocop_yaml/.rubocop.yml > #{CREW_DEST_PREFIX}/.config/rubocop/config.yml"
+
+    # Remove following line after merge of this PR.
+    system "curl -Lsf https://github.com/satmandu/chromebrew/raw/rubocop_yaml/.rubocop.yml -o #{CREW_DEST_PREFIX}/.config/rubocop/config.yml"
     # Uncomment after merge
-    # system "curl -Ls https://github.com/chromebrew/chromebrew/raw/master/.rubocop.yml > #{CREW_DEST_PREFIX}/.config/rubocop/config.yml"
+    # system "curl -Lsf https://github.com/skycocker/chromebrew/raw/master/.rubocop.yml -o #{CREW_DEST_PREFIX}/.config/rubocop/config.yml"
   end
 
   def self.postinstall
-    # system 'gem install -N rubocop --conservative'
+    @gem_name = self.name.sub('ruby_','')
+    system "gem install -N #{@gem_name} --conservative"
+
     puts "Installing Chromebrew rubocop config file at #{@xdg_config_home}/rubocop/config.yml".lightblue
     puts 'This can be overridden by a ~/.rubocop.yml'.lightblue
     FileUtils.mkdir_p "#{@xdg_config_home}/rubocop"
-    system "curl -Ls https://github.com/satmandu/chromebrew/raw/rubocop_yaml/.rubocop.yml > #{@xdg_config_home}/rubocop/config.yml"
+    # Remove following line after merge of this PR.
+    system "curl -Lsf https://github.com/satmandu/chromebrew/raw/rubocop_yaml/.rubocop.yml -o #{@xdg_config_home}/rubocop/config.yml"
     # Uncomment after merge
-    # system "curl -Ls https://github.com/skycocker/chromebrew/raw/master/.rubocop.yml > #{@xdg_config_home}/rubocop/config.yml"
+    # system "curl -Lsf https://github.com/skycocker/chromebrew/raw/master/.rubocop.yml -o #{@xdg_config_home}/rubocop/config.yml"
   end
 
   def self.remove
-    # system 'gem uninstall -x rubocop'
+    @gem_name = self.name.sub('ruby_','')
+    @gems_deps = `gem dependency ^#{@gem_name}\$ | awk '{print \$1}'`.chomp
+    @gems = @gems_deps.split("\n").drop(1).append("#{@gem_name}")
+    @gems.delete('bundler')
+    @gems.each do |gem|
+      system "gem uninstall -Dx --force --abort-on-dependent #{gem} || true"
+    end
+
     FileUtils.rm_f "#{@xdg_config_home}/rubocop/config.yml"
   end
 end
