b57b497bf503d9d1258d77c52061f6a9da6f22b0
Satadru Pramanik <satadru@gmail.com>
2022-04-18 00:13:00 -0400

    Patchelf -> 0.14.5, Update patchelf fxn in crew, rebuild util-linux (#6991)
    * adjust patchelf veto files
    
    * patchelf -> 0.14.5
    
    * add util__linux rebuild
    
    * remove ldconfig
    
    * bump crew version

--- packages/patchelf.rb
+++ packages/patchelf.rb
@@ -3,28 +3,30 @@ require 'package'
 class Patchelf < Package
   description 'PatchELF is a small utility to modify the dynamic linker and RPATH of ELF executables.'
   homepage 'http://nixos.org/patchelf.html'
-  version '0.14.3'
+  version '0.14.5'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://github.com/NixOS/patchelf.git'
-  git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.3_armv7l/patchelf-0.14.3-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.3_armv7l/patchelf-0.14.3-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.3_i686/patchelf-0.14.3-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.3_x86_64/patchelf-0.14.3-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_armv7l/patchelf-0.14.5-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_armv7l/patchelf-0.14.5-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_i686/patchelf-0.14.5-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_x86_64/patchelf-0.14.5-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '76cf82fea05e417739a96bd96b0468be749d4dc2ed7edcfff73ee48fd2ed1130',
-     armv7l: '76cf82fea05e417739a96bd96b0468be749d4dc2ed7edcfff73ee48fd2ed1130',
-       i686: '41389fd3670f713ee2b085560071155ff99c453fc80c222fc26354ab6a840c04',
-     x86_64: '0f15080a932a654e0861f45145e9e76927f07db9cf3a032ad837426d95bb6a68'
+    aarch64: '81227e2a9b1de212fb041bc7dffd6282f844182e4c39ea6fb6e50084561929f0',
+     armv7l: '81227e2a9b1de212fb041bc7dffd6282f844182e4c39ea6fb6e50084561929f0',
+       i686: 'db817f6eff4c97d07b1849c634dc82087ad584e486d154fd2a0bc02072ec923f',
+     x86_64: '9bc6661f27094ce4033eadc302c042d923b9c0a544f959d1359cfc767971a9b6'
   })
+
+  git_hashtag version
+  no_env_options
 
   def self.build
     system './bootstrap.sh'
-    system "LDFLAGS='-flto=auto -static' ./configure #{CREW_OPTIONS}"
+    system "CFLAGS='-fuse-ld=gold' LDFLAGS='-flto=auto -static' ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
