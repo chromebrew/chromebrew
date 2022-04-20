9e4e74b8a7197e323da5146b250c7ace46aea2be
Satadru Pramanik <satadru@gmail.com>
2022-04-20 15:54:57 -0400

    Gparted -> 1.40 and X deps updates (#7001)

--- packages/libsm.rb
+++ packages/libsm.rb
@@ -3,34 +3,35 @@ require 'package'
 class Libsm < Package
   description 'X.org X Session Management Library'
   homepage 'http://www.x.org'
-  version '1.2.2'
+  version '1.2.3'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://www.x.org/archive/individual/lib/libSM-1.2.2.tar.gz'
-  source_sha256 '14bb7c669ce2b8ff712fbdbf48120e3742a77edcd5e025d6b3325ed30cf120f4'
+  source_url 'https://www.x.org/archive/individual/lib/libSM-1.2.3.tar.gz'
+  source_sha256 '1e92408417cb6c6c477a8a6104291001a40b3bb56a4a60608fdd9cd2c5a0f320'
 
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.2_armv7l/libsm-1.2.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.2_armv7l/libsm-1.2.2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.2_i686/libsm-1.2.2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.2_x86_64/libsm-1.2.2-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.3_armv7l/libsm-1.2.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.3_armv7l/libsm-1.2.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.3_i686/libsm-1.2.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsm/1.2.3_x86_64/libsm-1.2.3-chromeos-x86_64.tar.zst'
   })
-  binary_sha256 ({
-    aarch64: '37e41534cb72c0816c7836f5e042183bc03062367b09d2da556b355be4cd541f',
-     armv7l: '37e41534cb72c0816c7836f5e042183bc03062367b09d2da556b355be4cd541f',
-       i686: '43c197b6a44e70314c6c6593e719e35b011ac20776ebc9a2114b4af06e6448a0',
-     x86_64: '9516fc81d7106f5f611bfd0f47fc4d44bf3562cddf9c2f83a18a50e3960e9386',
+  binary_sha256({
+    aarch64: '7d6702a3ba19eae8a974a69614c262f50ae7c6f7b1b514bd52591131192bb086',
+     armv7l: '7d6702a3ba19eae8a974a69614c262f50ae7c6f7b1b514bd52591131192bb086',
+       i686: '8ecb14d2b98abcb482d222fabcb04e036525ae02e29556ee0f5ec6632403a7b6',
+     x86_64: 'f994171ce6971904dbba890e78e2a95db68c99c0509730e62cb53f6ef71ce88a'
   })
 
   depends_on 'libice'
   depends_on 'libx11'
+  patchelf
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+    system "./configure #{CREW_OPTIONS}"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
--- packages/libxmu.rb
+++ packages/libxmu.rb
@@ -3,36 +3,37 @@ require 'package'
 class Libxmu < Package
   description 'X.org X interface library for miscellaneous utilities not part of the Xlib standard'
   homepage 'https://www.x.org'
-  version '1.1.2'
+  version '1.1.3'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://www.x.org/archive/individual/lib/libXmu-1.1.2.tar.gz'
-  source_sha256 'e5fd4bacef068f9509b8226017205040e38d3fba8d2de55037200e7176c13dba'
+  source_url 'https://www.x.org/archive/individual/lib/libXmu-1.1.3.tar.gz'
+  source_sha256 '5bd9d4ed1ceaac9ea023d86bf1c1632cd3b172dce4a193a72a94e1d9df87a62e'
 
-  binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.2_armv7l/libxmu-1.1.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.2_armv7l/libxmu-1.1.2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.2_i686/libxmu-1.1.2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.2_x86_64/libxmu-1.1.2-chromeos-x86_64.tar.xz',
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.3_armv7l/libxmu-1.1.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.3_armv7l/libxmu-1.1.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.3_i686/libxmu-1.1.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmu/1.1.3_x86_64/libxmu-1.1.3-chromeos-x86_64.tar.zst'
   })
-  binary_sha256 ({
-    aarch64: 'b0e35fa7604228c0bc7e556a3425e928a90f9f00707126c6007c235b9162c75c',
-     armv7l: 'b0e35fa7604228c0bc7e556a3425e928a90f9f00707126c6007c235b9162c75c',
-       i686: '6780b8d82aa0b43aae9af514546741dcde6758c32ecbafdf0b181aeb74600b47',
-     x86_64: '66b75c1f3488973f36f4520fa283f860bdebdea608716a082776e8364a092d4f',
+  binary_sha256({
+    aarch64: '13b7e9be2a0605f57496e57a4dbf928ed6ea1cfb07e682a7b47348572bda714c',
+     armv7l: '13b7e9be2a0605f57496e57a4dbf928ed6ea1cfb07e682a7b47348572bda714c',
+       i686: 'a28796da18c49538711e5f69ea5fdd0c9a040c15acb66b5e5199ac64d273fdc9',
+     x86_64: 'c69a0f76d5622da115b48cb108c46e3bd2d8c862c6582b77842b022f62defc40'
   })
 
   depends_on 'libxt'
   depends_on 'libxext'
   depends_on 'util_macros'
   depends_on 'libx11'
+  patchelf
 
   def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system "make"
+    system "./configure #{CREW_OPTIONS}"
+    system 'make'
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
