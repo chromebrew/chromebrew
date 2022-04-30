d8502b485ac6af875986a32621a8e90db4b3b0ec
Satadru Pramanik <satadru@gmail.com>
2022-04-29 21:30:31 -0400

    pax_utils -> 1.3.4 and updated deps, add gnulib_git

--- packages/libcap.rb
+++ packages/libcap.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libcap < Package
   description 'Libcap implements the user-space interfaces to the POSIX 1003.1e capabilities available in Linux kernels.'
   homepage 'https://directory.fsf.org/wiki/Libcap/'
-  @_ver = '2.63'
+  @_ver = '2.64'
   version @_ver
   license 'GPL-2 or BSD'
   compatibility 'all'
@@ -11,22 +11,23 @@ class Libcap < Package
   git_hashtag "libcap-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.63_armv7l/libcap-2.63-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.63_armv7l/libcap-2.63-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.63_i686/libcap-2.63-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.63_x86_64/libcap-2.63-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.64_armv7l/libcap-2.64-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.64_armv7l/libcap-2.64-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.64_i686/libcap-2.64-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcap/2.64_x86_64/libcap-2.64-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b090422c28aa929a2a585c73739eda6c8c953379ecb0b90f1f596ad5e37706de',
-     armv7l: 'b090422c28aa929a2a585c73739eda6c8c953379ecb0b90f1f596ad5e37706de',
-       i686: '235e7deaec2896dd3db2af3b851699c1fd4dd680b6e2e02d91b895d54ca6d01b',
-     x86_64: '2e306e64bf4f170ad232ab0a64de65a5c4dbbb877ce6f5d7e7ebaa57c39acdde'
+    aarch64: '03701822e8ae9cfee9ea023b377499797b037372c204f27c755bf68a01dddd4c',
+     armv7l: '03701822e8ae9cfee9ea023b377499797b037372c204f27c755bf68a01dddd4c',
+       i686: '4032d0a74b1b6a23da3d97643f14c830d7499c7b3d8677715bb369e77054a655',
+     x86_64: 'cc6593f845dfc3adc63cfd5dadaf23117c646ae5d37442961380fe0f82295fe3'
   })
 
+  depends_on 'glibc' # R
   depends_on 'gperf' => :build
   depends_on 'linux_pam'
 
-  def self.build
+  def self.patch
     # add includes option to make it work with gperf-3.1
     system 'sed -e "/gperf --/s/gperf --/gperf --includes --/" -e "/gperf --/s/cap_lookup_name(/cap_dummy(/" -i libcap/Makefile'
     # change the path to ld
@@ -36,9 +37,15 @@ class Libcap < Package
     # set exec_prefix
     system "sed -i 's,^exec_prefix=,exec_prefix=\$(prefix),g' Make.Rules"
     # use system libdir
-    system "sed -i 's,^lib_prefix=\$(exec_prefix),lib_prefix=#{CREW_LIB_PREFIX},g' Make.Rules"
+    case ARCH
+    when 'armv7l', 'aarch64'
+      system "sed -i 's,^lib_prefix=\$(exec_prefix),lib_prefix=#{CREW_LIB_PREFIX},g' Make.Rules"
+    end
     # http://git.yoctoproject.org/cgit.cgi/poky/plain/meta/recipes-support/libcap/files/0001-ensure-the-XATTR_NAME_CAPS-is-defined-when-it-is-use.patch
     system 'sed -i "s,^\#ifdef VFS_CAP_U32,\#if defined (VFS_CAP_U32) \&\& defined (XATTR_NAME_CAPS),g" libcap/cap_file.c'
+  end
+
+  def self.build
     system "#{CREW_ENV_OPTIONS} make"
   end
 
