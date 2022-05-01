16a5c8ba20b2081adade468846f5286487567524
Satadru Pramanik <satadru@gmail.com>
2022-05-01 05:22:37 -0400

    wxwidgets rebuild + update

--- packages/wxwidgets31.rb
+++ packages/wxwidgets31.rb
@@ -1,32 +1,31 @@
 require 'package'
 
-class Wxwidgets < Package
+class Wxwidgets31 < Package
   description 'wxWidgets is a C++ library that lets developers create applications for Windows, macOS, Linux and other platforms with a single code base.'
   homepage 'https://www.wxwidgets.org/'
-  @_ver = '3.0.5.1'
-  version "#{@_ver}-4"
-  license 'GPL-2'
+  @_ver = '3.1.6'
+  version @_ver
   compatibility 'aarch64,armv7l,x86_64'
   source_url 'https://github.com/wxWidgets/wxWidgets.git'
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wxwidgets/3.0.5.1-4_armv7l/wxwidgets-3.0.5.1-4-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wxwidgets/3.0.5.1-4_armv7l/wxwidgets-3.0.5.1-4-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wxwidgets/3.0.5.1-4_x86_64/wxwidgets-3.0.5.1-4-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wxwidgets31/3.1.6_armv7l/wxwidgets31-3.1.6-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wxwidgets31/3.1.6_armv7l/wxwidgets31-3.1.6-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/wxwidgets31/3.1.6_x86_64/wxwidgets31-3.1.6-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '18ea442358ab44fd12c3f48dc190d616aafea4284a95a1681edacf6ecb5a1e8b',
-     armv7l: '18ea442358ab44fd12c3f48dc190d616aafea4284a95a1681edacf6ecb5a1e8b',
-     x86_64: '13d720a56907dd10e11bdc39b84a8689da18f8f36fe6a1b27bcd1b4a988c7e7e'
+    aarch64: 'ec00c1f8cefea8fe50d8926bf1f4a077b2adebc35005aa929c0a2c5bb6334cf7',
+     armv7l: 'ec00c1f8cefea8fe50d8926bf1f4a077b2adebc35005aa929c0a2c5bb6334cf7',
+     x86_64: 'e1b739eb3e7253fa5741226fae1d1fb9de9fcd7f75e3fd93f7bedfb0f17ed4eb'
   })
 
   depends_on 'atk' # R
-  depends_on 'fontconfig'
   depends_on 'gdk_pixbuf' # R
   depends_on 'glib' # R
   depends_on 'gstreamer' # R
-  depends_on 'gtk3' # R
+  depends_on 'gtk3'
+  depends_on 'gvfs'
   depends_on 'harfbuzz' # R
   depends_on 'libglu' # R
   depends_on 'libjpeg' # R
@@ -38,7 +37,7 @@ class Wxwidgets < Package
   depends_on 'libtiff' # R
   depends_on 'libx11' # R
   depends_on 'libxxf86vm' # R
-  depends_on 'mesa' # R
+  depends_on 'mesa'
   depends_on 'pango' # R
 
   def self.patch
@@ -51,7 +50,7 @@ class Wxwidgets < Package
       @@ -424,10 +424,7 @@ bool wxAppConsole::CheckBuildOptions(con
                msg.Printf(_T("Mismatch between the program and library build versions detected.\nThe library used %s,\nand %s used %s."),
                           lib.c_str(), progName.c_str(), prog.c_str());
-
+       
       -        wxLogFatalError(msg.c_str());
       -
       -        // normally wxLogFatalError doesn't return
@@ -65,7 +64,9 @@ class Wxwidgets < Package
   end
 
   def self.build
-    system "./configure #{CREW_OPTIONS} \
+    system "env #{CREW_ENV_OPTIONS} \
+      LDFLAGS='-flto=auto' \
+      ./configure #{CREW_OPTIONS} \
       --with-gtk=3 \
       --with-opengl \
       --enable-unicode \
@@ -84,7 +85,7 @@ class Wxwidgets < Package
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     Dir.chdir "#{CREW_DEST_PREFIX}/bin" do
-      FileUtils.ln_sf "#{CREW_LIB_PREFIX}/wx/config/gtk3-unicode-3.0", 'wx-config'
+      FileUtils.ln_sf "#{CREW_LIB_PREFIX}/wx/config/gtk3-unicode-3.1", 'wx-config'
     end
   end
 end
