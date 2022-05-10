d5ca8f36996e6f67bf7ec334e6f716ad087dddb6
Satadru Pramanik <satadru@gmail.com>
2022-05-10 19:42:25 -0400

    openssl rebuilds, add gcc12 to install.sh (#7048)
    * openssl rebuilds, add gcc12 to install.sh
    
    * add armv7l to popt gcc deps

--- packages/popt.rb
+++ packages/popt.rb
@@ -3,24 +3,27 @@ require 'package'
 class Popt < Package
   description 'Library for parsing command line options'
   homepage 'https://directory.fsf.org/wiki/Popt'
-  version '78ebda3'
+  version '1.18-7182e46'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/rpm-software-management/popt.git'
-  git_hashtag '78ebda342ca9bec60e4dd5f1d5b720dade3ab4b2'
+  git_hashtag '7182e4618ad5a0186145fc2aa4a98c2229afdfa8'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/78ebda3_armv7l/popt-78ebda3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/78ebda3_armv7l/popt-78ebda3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/78ebda3_i686/popt-78ebda3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/78ebda3_x86_64/popt-78ebda3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/1.18-7182e46_armv7l/popt-1.18-7182e46-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/1.18-7182e46_armv7l/popt-1.18-7182e46-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/1.18-7182e46_i686/popt-1.18-7182e46-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/popt/1.18-7182e46_x86_64/popt-1.18-7182e46-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '2ba47ce5e9cf8493c82d371da80dc4e5a0e0d7bc0cf36996781b089d01e3b8d1',
-     armv7l: '2ba47ce5e9cf8493c82d371da80dc4e5a0e0d7bc0cf36996781b089d01e3b8d1',
-       i686: '611be7ffbadab77c9da3492f13fbc8860522ec01adefbfbb462fb9c1afc76530',
-     x86_64: '591ff491561a48dc2a993b718a554dc303fab823929dc622963d6144db6e5fe1'
+    aarch64: '5bc51a9e367fac643a4c39acf250d8989c606c3d49c5ee41a662911acfc6863d',
+     armv7l: '5bc51a9e367fac643a4c39acf250d8989c606c3d49c5ee41a662911acfc6863d',
+       i686: '3ef4682d25a75e2d3464a5479744d9bd57473e9e303d1161fa4a2945752d3eb5',
+     x86_64: '93a2f1c02bda8ceee0c4cec070eab58bb997ba1a80e5cd0c37d4052464da4ce2'
   })
+
+  depends_on 'glibc' # R
+  depends_on 'gcc12' unless ARCH == 'x86_64' # R
 
   def self.build
     system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
