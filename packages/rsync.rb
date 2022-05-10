03e0b5e63c84a72ba7c1cf8e050cbb7fd38cb9dc
Satadru Pramanik <satadru@gmail.com>
2022-05-10 18:36:50 -0400

    openssl rebuilds, add gcc12 to install.sh

--- packages/rsync.rb
+++ packages/rsync.rb
@@ -3,30 +3,34 @@ require 'package'
 class Rsync < Package
   description 'rsync is an open source utility that provides fast incremental file transfer.'
   homepage 'https://rsync.samba.org/'
-  @_ver = '3.2.3'
-  version "#{@_ver}-2"
+  @_ver = '3.2.4'
+  version @_ver.to_s
   license 'GPL-3'
   compatibility 'all'
   source_url "http://rsync.samba.org/ftp/rsync/src/rsync-#{@_ver}.tar.gz"
-  source_sha256 'becc3c504ceea499f4167a260040ccf4d9f2ef9499ad5683c179a697146ce50e'
+  source_sha256 '6f761838d08052b0b6579cf7f6737d93e47f01f4da04c5d24d3447b7f2a5fad1'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.3-2_armv7l/rsync-3.2.3-2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.3-2_armv7l/rsync-3.2.3-2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.3-2_i686/rsync-3.2.3-2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.3-2_x86_64/rsync-3.2.3-2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.4_armv7l/rsync-3.2.4-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.4_armv7l/rsync-3.2.4-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.4_i686/rsync-3.2.4-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.4_x86_64/rsync-3.2.4-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ba288d6e49f3bef27932e810d58206b1c3c8c60bfb5ef3168bbded428ee89a0b',
-     armv7l: 'ba288d6e49f3bef27932e810d58206b1c3c8c60bfb5ef3168bbded428ee89a0b',
-       i686: '6eb5c8f79b5e29ad461f833738db4251bfbadb037ab2ea8030ed3516e709c1a4',
-     x86_64: '7f8006dc3358106d7f949b8a344746cc7b445c03bb9640238b1d48322f5c07ae'
+    aarch64: 'a2a22a25d50fe400ddb6cd525abbfea05f82fb36e2da0dd6550402b8a0529327',
+     armv7l: 'a2a22a25d50fe400ddb6cd525abbfea05f82fb36e2da0dd6550402b8a0529327',
+       i686: 'f78152b34b3ccfb798f890ac78a8174f03bf27fe750fb747576e5274f3f3045f',
+     x86_64: '78d02794bbea0bfc750a4f216eb855b43179b22d0c0366119544692c0881b160'
   })
 
-  depends_on 'xxhash'
-  depends_on 'lz4'
-  depends_on 'popt'
-  depends_on 'zstd'
+  depends_on 'acl' # R
+  depends_on 'attr' # R
+  depends_on 'glibc' # R
+  depends_on 'lz4' # R
+  depends_on 'openssl' # R
+  depends_on 'popt' # R
+  depends_on 'xxhash' # R
+  depends_on 'zstd' # R
 
   def self.build
     system "./configure #{CREW_OPTIONS} --disable-maintainer-mode"
