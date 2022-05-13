8db0d55df0962a088217528ac6ff23753021500f
Satadru Pramanik <satadru@gmail.com>
2022-05-12 23:10:38 -0400

    rebuild of musl_openssl with rand-seed, and also rebuild git, musl_curl (#7059)

--- packages/musl_openssl.rb
+++ packages/musl_openssl.rb
@@ -4,23 +4,23 @@ class Musl_openssl < Package
   description 'The Open Source toolkit for Secure Sockets Layer and Transport Layer Security'
   homepage 'https://www.openssl.org'
   @_ver = '3.0.3'
-  version @_ver
+  version "#{@_ver}-1"
   license 'openssl'
   compatibility 'all'
   source_url "https://www.openssl.org/source/openssl-#{@_ver}.tar.gz"
   source_sha256 'ee0078adcef1de5f003c62c80cc96527721609c6f3bb42b7795df31f8b558c0b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3_armv7l/musl_openssl-3.0.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3_armv7l/musl_openssl-3.0.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3_i686/musl_openssl-3.0.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3_x86_64/musl_openssl-3.0.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3-1_armv7l/musl_openssl-3.0.3-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3-1_armv7l/musl_openssl-3.0.3-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3-1_i686/musl_openssl-3.0.3-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/musl_openssl/3.0.3-1_x86_64/musl_openssl-3.0.3-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b5e5e2ee3f997a5c4b0ed556c9910152620cdb6077a35cdc3618ffbc3ae19783',
-     armv7l: 'b5e5e2ee3f997a5c4b0ed556c9910152620cdb6077a35cdc3618ffbc3ae19783',
-       i686: 'd05edf1e5a84af558c55b8370164dcb7b00c9b3ef05491102c0d4ae7a716e768',
-     x86_64: 'bc294a522e698f72afba02f6b1e1b757fca513c0d082cbfef6096b89cd122bd4'
+    aarch64: '4fef0865c1fbe05a53c9f5b780dcaa057d9a9392850f0f49cd5ad4e61c557bb5',
+     armv7l: '4fef0865c1fbe05a53c9f5b780dcaa057d9a9392850f0f49cd5ad4e61c557bb5',
+       i686: '5be37951d05b99439b34f8964435f9b60a9b35a8b2ee2aae7ae617b6ab70f47f',
+     x86_64: '2359bf9d949a73617bae01b42368af122541f2464d56964acd47ccb31201f8c3'
   })
 
   depends_on 'musl_native_toolchain' => :build
@@ -33,13 +33,19 @@ class Musl_openssl < Package
   is_static
 
   def self.build
+    # rand-seed is needed to keep git from breaking with an error about
+    # insufficient randomness being available.
     case ARCH
     when 'aarch64', 'armv7l'
       @openssl_configure_target = 'linux-generic32'
+      # rdcpu breaks armv7l builds with OpenSSL 3.0.3
+      @rand_seed = 'os,getrandom'
     when 'i686'
       @openssl_configure_target = 'linux-elf'
+      @rand_seed = 'os,getrandom,rdcpu'
     when 'x86_64'
       @openssl_configure_target = 'linux-x86_64'
+      @rand_seed = 'os,getrandom,rdcpu'
     end
     # Use debian build options to work around problem building on armv7l.
     # Disable cast because it breaks i686 builds.
@@ -58,6 +64,7 @@ class Musl_openssl < Package
         no-ssl3-method \
         no-tests \
         no-zlib \
+        --with-rand-seed=#{@rand_seed} \
         -static --static \
         -Wl,-rpath=#{CREW_MUSL_PREFIX}/lib -Wl,--enable-new-dtags \
         -Wl,-Bsymbolic \
