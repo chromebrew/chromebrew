62a54b0ae96e0f438ed8a83accab626b50fca37f
Ed Reel <edreel@gmail.com>
2022-05-28 17:33:11 -0500

    Edge 101.0.1210.39 => 101.0.1210.53

--- packages/edge.rb
+++ packages/edge.rb
@@ -3,12 +3,12 @@ require 'package'
 class Edge < Package
   description 'Microsoft Edge is the fast and secure browser'
   homepage 'https://www.microsoft.com/en-us/edge'
-  @_ver = '101.0.1210.39'
+  @_ver = '101.0.1210.53'
   version @_ver
   compatibility 'x86_64'
   license 'MIT'
-  source_url 'https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_101.0.1210.39-1_amd64.deb'
-  source_sha256 'e05cc175822661efea467a54b47046f30551af0607d8cbfa57635b08fdaba778'
+  source_url 'https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_101.0.1210.53-1_amd64.deb'
+  source_sha256 '8c9370c66c408986c9a69a5ca85d2eb42821babf3590d6bc3d516d5e31b5ba6f'
 
   no_compile_needed
 
