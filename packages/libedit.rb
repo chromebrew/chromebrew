bffd50543ed4b0c6cadb6569ca388d7d5693979c
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-05-30 15:31:47 -0700

    libedit => 20210910-3.1 (#7098)

--- packages/libedit.rb
+++ packages/libedit.rb
@@ -1,32 +1,30 @@
 require 'package'
 
 class Libedit < Package
-  description 'This is an autotool and libtoolized port of the NetBSD Editline library (libedit).'
-  homepage 'http://thrysoee.dk/editline/'
-  version '20210522-3.1'
-  license 'BSD-2'
+  description 'An autotooled and libtoolized port of the NetBSD Editline library (libedit).'
+  homepage 'https://thrysoee.dk/editline/'
+  version '20210910-3.1'
   compatibility 'all'
-  source_url 'https://thrysoee.dk/editline/libedit-20210522-3.1.tar.gz'
-  source_sha256 '0220bc2047e927c0c1984ef5f7b4eb2a9469a5b7bf12ba573ca3b23ca02bbb6f'
+  license 'BSD-2'
+  source_url 'https://thrysoee.dk/editline/libedit-20210910-3.1.tar.gz'
+  source_sha256 '6792a6a992050762edcca28ff3318cdb7de37dccf7bc30db59fcd7017eed13c5'
 
   binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210522-3.1_armv7l/libedit-20210522-3.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210522-3.1_armv7l/libedit-20210522-3.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210522-3.1_i686/libedit-20210522-3.1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210522-3.1_x86_64/libedit-20210522-3.1-chromeos-x86_64.tar.xz',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210910-3.1_armv7l/libedit-20210910-3.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210910-3.1_armv7l/libedit-20210910-3.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210910-3.1_i686/libedit-20210910-3.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libedit/20210910-3.1_x86_64/libedit-20210910-3.1-chromeos-x86_64.tar.zst',
   })
   binary_sha256 ({
-    aarch64: '21b75fb838a66f7459299bfe2e19905ad51473592d8a3a270b1cec5c7611fa60',
-     armv7l: '21b75fb838a66f7459299bfe2e19905ad51473592d8a3a270b1cec5c7611fa60',
-       i686: '2d42b3a52e8db660b5959f164a436595feada9559cd0635e11ccda26d51ab9e2',
-     x86_64: '5ab911b8abe6d07c2a6c88e5305597001d8de5bd61c799d318db4147a7e52cfa',
+    aarch64: '2b68bcfe634ef21dec0ff29215e1de3f0cd506041ccae30d6eea93fb946870e9',
+     armv7l: '2b68bcfe634ef21dec0ff29215e1de3f0cd506041ccae30d6eea93fb946870e9',
+       i686: '5397384d810b157b11ab640c2144cf35f0999039daf1fa9dfc7bea8ad84af6f1',
+     x86_64: '41ec1b9f504bc593668bf88cde1eb85fcfd621397d7ec17c29ec686bc2238b5b',
   })
 
   def self.build
-    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto \"-I#{CREW_PREFIX}/include/ncurses\"' LDFLAGS='-flto=auto' \
-      ./configure \
-      #{CREW_OPTIONS} \
-      CPPFLAGS=\"-I#{CREW_PREFIX}/include/ncursesw\""
+    system "./configure #{CREW_OPTIONS} \
+            CPPFLAGS=\"-I#{CREW_PREFIX}/include/ncursesw\""
     system 'make'
   end
 
@@ -34,5 +32,9 @@ class Libedit < Package
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     # Remove conflict with readline
     FileUtils.rm_f "#{CREW_DEST_MAN_PREFIX}/man3/history.3"
+  end
+
+  def self.check
+    system 'make', 'check'
   end
 end
