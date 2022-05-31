4f4be21fc8a7fac1cfa2d89c70cf1c7396f4a1ac
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-05-31 15:40:31 -0700

    x86_64 and i686 binaries

--- packages/cpio.rb
+++ packages/cpio.rb
@@ -9,13 +9,22 @@ class Cpio < Package
   source_url 'https://ftpmirror.gnu.org/cpio/cpio-2.13.tar.bz2'
   source_sha256 'eab5bdc5ae1df285c59f2a4f140a98fc33678a0bf61bdba67d9436ae26b46f6d'
 
+  binary_url({
+    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_i686/cpio-2.13-chromeos-i686.tar.zst',
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_x86_64/cpio-2.13-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    i686: 'af635bbef1f4b56a2ad457e97f0a4794e3b854784f4e86375fe3dea6905e0407',
+  x86_64: '11265e9d8eb7760d0d9e98e80860056793243e622cdf82d4ed2a2ef51722f8ee'
+  })
+
   def self.patch
     # these patches allow -flto to work
-    _url = "https://github.com/alpinelinux/aports/raw/dfc99097d6372098d36f33a1c80961d3639293f2/community/cpio"
-    downloader "#{_url}/CVE-2021-38185.patch", "a92b9b3367a90cb66e08674df9b62dfacafa85885bfe5b16de0517e1694a2502"
-    downloader "#{_url}/CVE-2021-38185-2.patch", "2a2b3564d3910dfa7b0f595af6a69ed5a664443e786fc770a9a51d453b7af6d1"
-    downloader "#{_url}/CVE-2021-38185-3.patch", "3b337b844cc37043cb49b115fcf0efa0bc6a042eee7ef7a074c74b56d3978156"
-    downloader "#{_url}/gcc-10.patch", "68181f05736e9165493320f5213f76410c496a04ca03a3a7da3eb6f8d0c4e4c1"
+    _url = 'https://github.com/alpinelinux/aports/raw/dfc99097d6372098d36f33a1c80961d3639293f2/community/cpio'
+    downloader "#{_url}/CVE-2021-38185.patch", 'a92b9b3367a90cb66e08674df9b62dfacafa85885bfe5b16de0517e1694a2502'
+    downloader "#{_url}/CVE-2021-38185-2.patch", '2a2b3564d3910dfa7b0f595af6a69ed5a664443e786fc770a9a51d453b7af6d1'
+    downloader "#{_url}/CVE-2021-38185-3.patch", '3b337b844cc37043cb49b115fcf0efa0bc6a042eee7ef7a074c74b56d3978156'
+    downloader "#{_url}/gcc-10.patch", '68181f05736e9165493320f5213f76410c496a04ca03a3a7da3eb6f8d0c4e4c1'
     Dir['CVE-2021-38185.patch', 'CVE-2021-38185-2.patch', 'CVE-2021-38185-3.patch', 'gcc-10.patch'].each do |filename|
       system "patch -Np1 -i #{filename}"
     end
@@ -28,7 +37,7 @@ class Cpio < Package
   end
 
   def self.install
-    system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 
   def self.check
