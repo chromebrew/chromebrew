9e1202707e35bf26686503a6d3c1d347275ab347
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-05-30 17:51:18 -0700

    upgrade py3_setuptools_rust

--- packages/py3_setuptools_rust.rb
+++ packages/py3_setuptools_rust.rb
@@ -3,25 +3,12 @@ require 'package'
 class Py3_setuptools_rust < Package
   description 'Setuptools-rust provides rust support for setuptools.'
   homepage 'https://github.com/PyO3/setuptools-rust/'
-  @_ver = '1.1.2'
+  @_ver = '1.3.0'
   version @_ver
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/PyO3/setuptools-rust.git'
   git_hashtag "v#{@_ver}"
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_setuptools_rust/1.1.2_armv7l/py3_setuptools_rust-1.1.2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_setuptools_rust/1.1.2_armv7l/py3_setuptools_rust-1.1.2-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_setuptools_rust/1.1.2_i686/py3_setuptools_rust-1.1.2-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_setuptools_rust/1.1.2_x86_64/py3_setuptools_rust-1.1.2-chromeos-x86_64.tpxz'
-  })
-  binary_sha256({
-    aarch64: '04099645c42222d96d437fcc19fe0de20d69827f369c9097f784aa51a9948ba5',
-     armv7l: '04099645c42222d96d437fcc19fe0de20d69827f369c9097f784aa51a9948ba5',
-       i686: 'd44f823d537a817ad9b71aad7bb48b2dc0446bbcf7b7507f0ca26e6aaa56f083',
-     x86_64: '721e1f3a97bfe023d6912abf20eb64bde96f76b587af245abad06481f2250e13'
-  })
 
   depends_on 'rust'
   depends_on 'py3_toml'
@@ -29,10 +16,10 @@ class Py3_setuptools_rust < Package
   depends_on 'py3_setuptools'
 
   def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+    system "python3 -m build #{PY3_BUILD_OPTIONS}"
   end
 
   def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+    system "python3 -m installer #{PY3_INSTALLER_OPTIONS}"
   end
 end
