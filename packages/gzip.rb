49115d215138eac9c9b7656e7b11ab1894c18beb
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-05-30 23:40:04 -0700

    add x86_64 binaries (i686 broken)

--- packages/gzip.rb
+++ packages/gzip.rb
@@ -9,6 +9,13 @@ class Gzip < Package
   source_url 'https://ftpmirror.gnu.org/gzip/gzip-1.12.tar.xz'
   source_sha256 'ce5e03e519f637e1f814011ace35c4f87b33c0bbabeec35baf5fbd3479e91956'
 
+  binary_url({
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.12_x86_64/gzip-1.12-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    x86_64: 'bd27a2c304c111cd4983416745ff83c210f15773f2dc5fd864369d7617ef904a'
+  })
+
   def self.build
     system "./configure #{CREW_OPTIONS} \
             --enable-threads=posix"
