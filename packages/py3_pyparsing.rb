084858f146ce4417af8d7905e067e73f987ab3e1
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-05-31 00:42:48 -0700

    new package py3_build (#7100)
    new package py3_installer
    
    new package py3_pep517
    
    new package py3_tomli
    
    add new buildsystem to buildessential
    
    add constants to const.rb for new buildsystem
    
    new package py3_flit_core
    
    new package py3_flit
    
    add py3_flit_core to buildessential
    
    new package py3_tomli_w
    
    add x86_64 and i686 binaries
    
    fix flit-core and tomli-w binaries
    
    bump crew version
    
    update pyparsing and packaging
    
    oops py3_packaging depends on py3_pyparsing (add x86_64 and i686 binaries)
    
    py3_pyparsing does not depend on itself
    
    add arm binaries

--- packages/py3_pyparsing.rb
+++ packages/py3_pyparsing.rb
@@ -3,33 +3,31 @@ require 'package'
 class Py3_pyparsing < Package
   description 'The pyparsing module is an alternative approach to creating and executing simple grammars, vs. the traditional lex/yacc approach, or the use of regular expressions.'
   homepage 'https://github.com/pyparsing/pyparsing/'
-  @_ver = '2.4.7'
-  version "#{@_ver}-1"
+  @_ver = '3.0.9'
+  version @_ver
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/pyparsing/pyparsing.git'
   git_hashtag "pyparsing_#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/2.4.7-1_armv7l/py3_pyparsing-2.4.7-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/2.4.7-1_armv7l/py3_pyparsing-2.4.7-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/2.4.7-1_i686/py3_pyparsing-2.4.7-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/2.4.7-1_x86_64/py3_pyparsing-2.4.7-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9_armv7l/py3_pyparsing-3.0.9-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9_armv7l/py3_pyparsing-3.0.9-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9_i686/py3_pyparsing-3.0.9-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9_x86_64/py3_pyparsing-3.0.9-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '5c84dec013f9409d5ddad79f5c4034c10541926dde48d5848d946284b2aacb12',
-     armv7l: '5c84dec013f9409d5ddad79f5c4034c10541926dde48d5848d946284b2aacb12',
-       i686: 'c084beab43308489cdc1f2814776354e5d2b94c680e7c75ceccf330945747659',
-     x86_64: '10addc1059f3399f556b9ee9b6b2206abf8c0131ec74ec853465032274acbce9'
+    aarch64: '260491f085b29afd3337a19ed3b6799f209e8ef902a0dc378524617518b50432',
+     armv7l: '260491f085b29afd3337a19ed3b6799f209e8ef902a0dc378524617518b50432',
+       i686: 'c0b44b7b246de089e25f55dbea208910a4489565af2f0d694435fdd49aad647a',
+     x86_64: 'a198eb924014106ccb4577cd3bb4d55be6123c303eb4a445535d2cde488ea694'
   })
 
-  depends_on 'py3_setuptools' => :build
-
   def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
+    system "python3 -m build #{PY3_BUILD_OPTIONS}"
   end
 
   def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+    system "python3 -m installer #{PY3_INSTALLER_OPTIONS}"
   end
 end
