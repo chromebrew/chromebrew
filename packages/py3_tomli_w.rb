1b26c828cbf2cfd3445e71a25a8ceda0def3779e
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-06-01 10:55:30 -0700

    add missed binaries

--- packages/py3_tomli_w.rb
+++ packages/py3_tomli_w.rb
@@ -4,11 +4,20 @@ class Py3_tomli_w < Package
   description "Tomli-w is a lil' TOML writer."
   homepage 'https://github.com/hukkin/tomli-w/'
   @_ver = '1.0.0'
-  version @_ver + '-1'
+  version "#{@_ver}-1"
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/hukkin/tomli-w.git'
   git_hashtag @_ver
+
+  binary_url({
+    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_tomli_w/1.0.0-1_i686/py3_tomli_w-1.0.0-1-chromeos-i686.tar.zst',
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_tomli_w/1.0.0-1_x86_64/py3_tomli_w-1.0.0-1-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    i686: 'b3b83ad54211417662308008f19f8851eaafd0b14b2978f139937f5f6894a8a0',
+  x86_64: '26ab14b98ab7fc0742e864b84c54f176bebe1e78423856bf8cd7a3a5d9d832b7'
+  })
 
   def self.build
     system "SETUPTOOLS_SCM_PRETEND_VERSION=#{@_ver} python3 -m build #{PY3_BUILD_OPTIONS}"
