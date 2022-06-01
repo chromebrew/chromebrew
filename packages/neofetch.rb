59901071327336ad396c7eac89e0d6267ae6e0e6
Satadru Pramanik <satadru@gmail.com>
2022-05-31 20:44:26 -0400

    add binaries

--- packages/neofetch.rb
+++ packages/neofetch.rb
@@ -11,11 +11,15 @@ class Neofetch < Package
 
   binary_url({
     i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/neofetch/7.1.0-ccd5_i686/neofetch-7.1.0-ccd5-chromeos-i686.tar.zst',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/neofetch/7.1.0-ccd5_x86_64/neofetch-7.1.0-ccd5-chromeos-x86_64.tar.zst'
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/neofetch/7.1.0-ccd5_x86_64/neofetch-7.1.0-ccd5-chromeos-x86_64.tar.zst',
+ aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/neofetch/7.1.0-ccd5_armv7l/neofetch-7.1.0-ccd5-chromeos-armv7l.tar.zst',
+  armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/neofetch/7.1.0-ccd5_armv7l/neofetch-7.1.0-ccd5-chromeos-armv7l.tar.zst'
   })
   binary_sha256({
-    i686: 'd3135a635671605f5747e47779626b9185e1d0a3fe7ef8166b689bd13196b9c6',
-  x86_64: '30ab2f9afee217da47cdd8daaab26b5a204288c6a8efab5e19627c96ea273e76'
+    i686: '5a35006bcec52e81d2ae60de414207fee83828f15e48962337f4613b963dd72a',
+  x86_64: 'fc648c0dd5701a96143247dc190e4474823ecb9c687a0de02274c822746ca6b7',
+ aarch64: 'c0957fdd5975660a493075dd6636c91ba55a29e1fc59760e51c9691d0d49ea5f',
+  armv7l: 'c0957fdd5975660a493075dd6636c91ba55a29e1fc59760e51c9691d0d49ea5f'
   })
 
   def self.install
