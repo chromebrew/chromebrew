29a95a9c7a094d6d8f18629152cfaa7ce12af7b9
Satadru Pramanik <satadru@gmail.com>
2022-05-31 21:29:54 -0400

    add binaries

--- packages/cpio.rb
+++ packages/cpio.rb
@@ -4,18 +4,22 @@ class Cpio < Package
   description 'GNU cpio copies files into or out of a cpio or tar archive. The archive can be another file on the disk, a magnetic tape, or a pipe.'
   homepage 'https://www.gnu.org/software/cpio/'
   version '2.13'
-  license 'GPL-3'
   compatibility 'all'
+  license 'GPL-3'
   source_url 'https://ftpmirror.gnu.org/cpio/cpio-2.13.tar.bz2'
   source_sha256 'eab5bdc5ae1df285c59f2a4f140a98fc33678a0bf61bdba67d9436ae26b46f6d'
 
   binary_url({
     i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_i686/cpio-2.13-chromeos-i686.tar.zst',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_x86_64/cpio-2.13-chromeos-x86_64.tar.zst'
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_x86_64/cpio-2.13-chromeos-x86_64.tar.zst',
+ aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_armv7l/cpio-2.13-chromeos-armv7l.tar.zst',
+  armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_armv7l/cpio-2.13-chromeos-armv7l.tar.zst'
   })
   binary_sha256({
     i686: 'af635bbef1f4b56a2ad457e97f0a4794e3b854784f4e86375fe3dea6905e0407',
-  x86_64: '11265e9d8eb7760d0d9e98e80860056793243e622cdf82d4ed2a2ef51722f8ee'
+  x86_64: '11265e9d8eb7760d0d9e98e80860056793243e622cdf82d4ed2a2ef51722f8ee',
+ aarch64: '61e781fe2ff468ae371bc7058f0d970a299d763012bb285895826b099ace958a',
+  armv7l: '61e781fe2ff468ae371bc7058f0d970a299d763012bb285895826b099ace958a'
   })
 
   def self.patch
