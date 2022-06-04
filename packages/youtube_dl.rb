d688fb1a2a94948df07fe30afdbf017d3f3eb80f
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2022-06-04 08:24:10 -0700

    youtube-dl -> 2021.12.17 (#7114)
    * youtube-dl -> 2021.12.17
    * bump version in youtubedl.rb
    * add binaries
    
    Co-authored-by: Ed Reel <edreel@gmail.com>

--- packages/youtube_dl.rb
+++ packages/youtube_dl.rb
@@ -3,24 +3,24 @@ require 'package'
 class Youtube_dl < Package
   description 'Command-line program to download videos from YouTube.com and other video sites'
   homepage 'https://youtube-dl.org/'
-  @_ver = '2021.06.06'
-  version "#{@_ver}-1"
+  @_ver = '2021.12.17'
+  version @_ver
   license 'public-domain'
   compatibility 'all'
   source_url 'https://github.com/ytdl-org/youtube-dl.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.06.06-1_armv7l/youtube_dl-2021.06.06-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.06.06-1_armv7l/youtube_dl-2021.06.06-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.06.06-1_i686/youtube_dl-2021.06.06-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.06.06-1_x86_64/youtube_dl-2021.06.06-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.12.17_armv7l/youtube_dl-2021.12.17-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.12.17_armv7l/youtube_dl-2021.12.17-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.12.17_i686/youtube_dl-2021.12.17-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/youtube_dl/2021.12.17_x86_64/youtube_dl-2021.12.17-chromeos-x86_64.tar.zst',
   })
   binary_sha256({
-    aarch64: '4ed1f09c314bdc024df635dc785bba85933cd7781e6d6b6afbc1d4aef9123671',
-     armv7l: '4ed1f09c314bdc024df635dc785bba85933cd7781e6d6b6afbc1d4aef9123671',
-       i686: '0a22a05e40e851363b4f72614785db19a19a66d638ad04b937abc15f3e7081c6',
-     x86_64: 'f2d75d96cce08dff64f1fb9aeef7a5190b7c3bdcac8facd45d1b938eba57ab22'
+    aarch64: '67493026c59e65899994e63542d9c032c2ab85d443b57cca9933280e81d7f6a3',
+     armv7l: '67493026c59e65899994e63542d9c032c2ab85d443b57cca9933280e81d7f6a3',
+       i686: '4097a2d204d79d05e2b8acba94c13c5cbce0e3e0391f4a7454ca83b03e9d3a1e',
+     x86_64: '8c1848f320b4f82d25eafaba316eb0858a33318dde94dbaa6fdf8c1feaa6d41f',
   })
 
   def self.build
