f1914917c1be660398d077f8d011f8ab29ea28e4
Satadru Pramanik <satadru@gmail.com>
2022-06-10 12:50:53 -0400

    imagemagick 7, ffmpeg, openjpeg updates (#7137)

--- packages/imagemagick7.rb
+++ packages/imagemagick7.rb
@@ -3,7 +3,7 @@ require 'package'
 class Imagemagick7 < Package
   description 'Use ImageMagick to create, edit, compose, or convert bitmap images.'
   homepage 'http://www.imagemagick.org/script/index.php'
-  @_ver = '7.1.0-19'
+  @_ver = '7.1.0-37'
   version @_ver
   license 'imagemagick'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Imagemagick7 < Package
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-19_armv7l/imagemagick7-7.1.0-19-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-19_armv7l/imagemagick7-7.1.0-19-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-19_i686/imagemagick7-7.1.0-19-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-19_x86_64/imagemagick7-7.1.0-19-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-37_armv7l/imagemagick7-7.1.0-37-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-37_armv7l/imagemagick7-7.1.0-37-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-37_i686/imagemagick7-7.1.0-37-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imagemagick7/7.1.0-37_x86_64/imagemagick7-7.1.0-37-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e4ab61bc827580630719cdcd95036bff97375f7626deaea8a22e2cb8ffdde9a1',
-     armv7l: 'e4ab61bc827580630719cdcd95036bff97375f7626deaea8a22e2cb8ffdde9a1',
-       i686: 'f112987ecd6c6c5be4632ae7f3b953a30d2a4ad1ebeeb92d51d006a300a887cb',
-     x86_64: 'f3f60b31f682ce39b3fc56bb7a3f44d5fd0fcb87c37e0951080fc57dbc852757'
+    aarch64: '734c1dde282b695f90ad9a7af940a088ff91eefcb30189fbcb0b89d5eb14bd3a',
+     armv7l: '734c1dde282b695f90ad9a7af940a088ff91eefcb30189fbcb0b89d5eb14bd3a',
+       i686: '1f0490156c755f3328b6881641251292332ca527bd5fdfa757bf3190f1982dc8',
+     x86_64: '7f9e553d8b73ba2339a800d8668f80d9518879dbf3c2296e55e14f4ebd190edb'
   })
 
   depends_on 'flif'
