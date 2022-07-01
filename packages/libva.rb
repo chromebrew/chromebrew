1511713e3bbf4da783dcff06371d05a00208e5a7
Satadru Pramanik <satadru@gmail.com>
2022-07-01 16:06:52 -0400

    libva -> 2.15

--- packages/libva.rb
+++ packages/libva.rb
@@ -3,24 +3,24 @@ require 'package'
 class Libva < Package
   description 'Libva is an implementation for VA-API (Video Acceleration API)'
   homepage 'https://01.org/linuxmedia'
-  @_ver = '2.14.0'
-  version "#{@_ver}-2"
+  @_ver = '2.15.0'
+  version @_ver.to_s
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/intel/libva.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_armv7l/libva-2.14.0-2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_armv7l/libva-2.14.0-2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_i686/libva-2.14.0-2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.14.0-2_x86_64/libva-2.14.0-2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_armv7l/libva-2.15.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_armv7l/libva-2.15.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_i686/libva-2.15.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_x86_64/libva-2.15.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'fccd32adece0312102e7294dc451ca4c8abb267abaf4726470da491891423101',
-     armv7l: 'fccd32adece0312102e7294dc451ca4c8abb267abaf4726470da491891423101',
-       i686: '98c2d3379fe1b513f4c1f8edfdc7424e84d940408141d63a065c29a92503e162',
-     x86_64: '7baea1dbbeb4fac85e384403151c0b8baefb366c8d7c982bac63c6ef15990fde'
+    aarch64: '201b781e9ecae81f6d7b978f9314ca321f6dbe78829467f84f5f38461597b371',
+     armv7l: '201b781e9ecae81f6d7b978f9314ca321f6dbe78829467f84f5f38461597b371',
+       i686: 'db83ae08bb404bfec867ba159a30b1d9bca57d4fe17f22332965da0ce4aab8da',
+     x86_64: '660fde88cdf68687ae52aca11d88ecc90424a943d7dd16ab5c572143637007a4'
   })
 
   depends_on 'libdrm'
@@ -33,8 +33,6 @@ class Libva < Package
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
     --default-library=both \
-    -Db_lto=true \
-    -Db_pie=true \
     builddir"
     system 'meson configure builddir'
     system 'ninja -C builddir'
