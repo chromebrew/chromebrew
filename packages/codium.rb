f8ba2525234b75a6b1522db47192dbebb8cf820a
Ed Reel <edreel@gmail.com>
2022-07-18 10:51:49 -0500

    Codium 1.68.1 => 1.69.1 (#7208)

--- packages/codium.rb
+++ packages/codium.rb
@@ -3,17 +3,17 @@ require 'package'
 class Codium < Package
   description 'VSCodium is Open Source Software Binaries of VSCode with a community-driven default configuration.'
   homepage 'https://vscodium.com/'
-  version '1.68.1'
+  version '1.69.1'
   license 'MIT'
   compatibility 'aarch64,armv7l,x86_64'
   case ARCH
   when 'aarch64', 'armv7l'
     source_url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium-linux-armhf-#{version}.tar.gz"
-    source_sha256 '6205e2379e2280f5704226b973925c496d7d64bb7a5ed77897415c5cff8b7b33'
+    source_sha256 '1952087a5b0d8a673817cddc36d7d8d2465b4f47afd0694fdbf4b6420a38f46f'
     @arch = 'arm'
   when 'x86_64'
     source_url "https://github.com/VSCodium/vscodium/releases/download/#{version}/VSCodium-linux-x64-#{version}.tar.gz"
-    source_sha256 'c578cb5c90ea338fb728ecef5818f4de5d47f1b05742c137b2583e9d9c23a6bf'
+    source_sha256 'd84f835e73830c2ae6ac9ca5b7fc945ff991fa00a889e98e212c962e23cc585b'
     @arch = 'x64'
   end
 
