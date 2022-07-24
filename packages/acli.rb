ff462fffa8341603d09998ed733e7b2abd8c95fc
Ed Reel <edreel@gmail.com>
2022-07-24 11:25:05 -0500

    Acli 1.30.1 => 2.1.0

--- packages/acli.rb
+++ packages/acli.rb
@@ -3,7 +3,7 @@ require 'package'
 class Acli < Package
   description 'Acquia CLI - The official command-line tool for interacting with the Drupal Cloud Platform and services.'
   homepage 'https://github.com/acquia/cli'
-  version '1.30.1'
+  version '2.1.0'
   license 'GPL-2.0'
   compatibility 'all'
   source_url 'SKIP'
@@ -13,14 +13,14 @@ class Acli < Package
   def self.preflight
     major = `php -v 2> /dev/null | head -1 | cut -d' ' -f2 | cut -d'.' -f1`
     minor = `php -v 2> /dev/null | head -1 | cut -d' ' -f2 | cut -d'.' -f2`
-    unless major.empty? or minor.empty? or (major.to_i >= 7 and minor.to_i >= 3)
-      abort "acli requires php >= 7.3. php #{major.chomp}.#{minor.chomp} does not meet the minimum requirement.".lightred
+    unless major.empty? or minor.empty? or (major.to_i >= 8 and minor.to_i >= 0)
+      abort "acli requires php >= 8.0. php #{major.chomp}.#{minor.chomp} does not meet the minimum requirement.".lightred
     end
   end
 
   def self.install
     downloader "https://github.com/acquia/cli/releases/download/#{version}/acli.phar",
-               'f6286b12bca73aee6e835557123c33acc816e4a99b994dd2faa47a52505d757f'
+               'e96a36b62f36682b7c9a85fa75fe7d43e0f3525d88cc670a105c8bdb13566649'
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'acli.phar', "#{CREW_DEST_PREFIX}/bin/acli", mode: 0o755
   end
