7bb85d5b6354ef06d9aa845faa797e86b64bc61a
Satadru Pramanik <satadru@gmail.com>
2022-08-17 19:08:13 -0400

    Add Crosvm, minijail, update vulkan_headers, vulkan_icd_loader, virglrenderer, libdrm (#7284)
    * add crosvm
    
    * update binary
    
    * requested changes
    
    * fix typo
    
    * Initial add of more crosvm deps
    
    * add more binaries
    
    * more binaries
    
    * add minijail to packages.yaml
    
    * add aarch64 conditional
    
    * fix git_hashtag locations
    
    * add updated libdrm

---
+++ packages/crosvm.rb
@@ -0,0 +1,43 @@
+# Adapted from Arch Linux crosvm-git PKGBUILD at:
+# https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=crosvm-git
+
+require 'package'
+
+class Crosvm < Package
+  description 'The Chrome OS Virtual Machine Monitor'
+  homepage 'https://chromium.googlesource.com/crosvm/crosvm'
+  version '379dd2d'
+  license 'custom:chromiumos'
+  compatibility 'x86_64'
+  source_url 'https://chromium.googlesource.com/crosvm/crosvm.git'
+  git_hashtag '379dd2dfecf1a0c06adf0f6e257a5ebc75374cb8'
+
+  binary_url({
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/crosvm/379dd2d_x86_64/crosvm-379dd2d-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    x86_64: '0292c2ccfc2261c756aeb82e48a6cac14efdf4fff2483950eacc05cd040451d4'
+  })
+
+  depends_on 'libcap'
+  depends_on 'dbus'
+  depends_on 'dtc'
+  depends_on 'protobuf'
+  depends_on 'rust' => :build
+  depends_on 'virglrenderer'
+  depends_on 'wayland_protocols' => :build
+
+  def self.build
+    @pwd = Dir.pwd
+    FileUtils.mkdir 'build_bin'
+    FileUtils.ln_s "#{CREW_PREFIX}/bin/gcc", 'build_bin/arm-linux-gnueabihf-gcc' if (ARCH == 'aarch64') || (ARCH == 'armv7l')
+    system "PATH=#{@pwd}/build_bin:$PATH cargo build --release --features=virgl_renderer"
+  end
+
+  def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin/"
+    FileUtils.install 'target/release/crosvm', "#{CREW_DEST_PREFIX}/bin/crosvm", mode: 0o755
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/policy/crosvm/"
+    FileUtils.cp_r Dir["seccomp/#{ARCH}/*"], "#{CREW_DEST_PREFIX}/share/policy/crosvm/" if ARCH == 'x86_64'
+  end
+end
--- packages/libdrm.rb
+++ packages/libdrm.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libdrm < Package
   description 'Cross-driver middleware for DRI protocol.'
   homepage 'https://dri.freedesktop.org'
-  @_ver = '2.4.110'
+  @_ver = '2.4.112'
   version @_ver
   license 'MIT'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Libdrm < Package
   git_hashtag "libdrm-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.110_armv7l/libdrm-2.4.110-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.110_armv7l/libdrm-2.4.110-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.110_i686/libdrm-2.4.110-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.110_x86_64/libdrm-2.4.110-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.112_armv7l/libdrm-2.4.112-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.112_armv7l/libdrm-2.4.112-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.112_i686/libdrm-2.4.112-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.112_x86_64/libdrm-2.4.112-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '17358ed1f87950f483410a3079757d77c7d62f8893571b61e4b93a24e0f50b9a',
-     armv7l: '17358ed1f87950f483410a3079757d77c7d62f8893571b61e4b93a24e0f50b9a',
-       i686: 'b9dff811d3f6a7fc04e2abb259d1e3f1f5486b1609d21e2cd11f5d31435f472e',
-     x86_64: '64ff40876627b172d3d3a92d641e38bffebb4363a52cb1dbc281f09afa0863a1'
+    aarch64: '80aaba199fd6f8c788e07cb1dc1d77b77318d810d7d7593a24d7d422d927b798',
+     armv7l: '80aaba199fd6f8c788e07cb1dc1d77b77318d810d7d7593a24d7d422d927b798',
+       i686: '280ee9624f654a35e16f77a053488e13d9b1f2e7f51b1c060abb286636c76749',
+     x86_64: '6f0aca30b18bd101dcc1e38d888e2c734b260291a9d2d19076dd540919ed0cdd'
   })
 
   depends_on 'libpciaccess' # R
@@ -39,7 +39,6 @@ class Libdrm < Package
       -Dvc4=true \
       -Dfreedreno=true \
       -Detnaviv=true \
-      -Dlibkms=true \
       -Dexynos=true \
       -Dudev=true \
       builddir"
