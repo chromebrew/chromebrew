a3ddfd895b0e5f82a23c61cc46f10f69b5176362
Satadru Pramanik <satadru@gmail.com>
2022-08-18 12:53:42 -0400

    updates for gobject_instrospection, gdk_pixbuf, py3_docutils (#7288)

--- packages/gobject_introspection.rb
+++ packages/gobject_introspection.rb
@@ -3,30 +3,30 @@ require 'package'
 class Gobject_introspection < Package
   description 'GObject introspection is a middleware layer between C libraries (using GObject) and language bindings.'
   homepage 'https://wiki.gnome.org/action/show/Projects/GObjectIntrospection'
-  @_ver = '1.70.0'
-  version "#{@_ver}-1"
+  @_ver = '1.73.0'
+  version @_ver.to_s
   license 'LGPL-2+ and GPL-2+'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/gobject-introspection.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.70.0-1_armv7l/gobject_introspection-1.70.0-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.70.0-1_armv7l/gobject_introspection-1.70.0-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.70.0-1_i686/gobject_introspection-1.70.0-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.70.0-1_x86_64/gobject_introspection-1.70.0-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.73.0_armv7l/gobject_introspection-1.73.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.73.0_armv7l/gobject_introspection-1.73.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.73.0_i686/gobject_introspection-1.73.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gobject_introspection/1.73.0_x86_64/gobject_introspection-1.73.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '5eb28e864dbaead1f78ef2caeb00c57d42ced3d9c044e2b30179a3fd529b3f30',
-     armv7l: '5eb28e864dbaead1f78ef2caeb00c57d42ced3d9c044e2b30179a3fd529b3f30',
-       i686: '70f5c4b09aa0ad77d820b00463d4ea7fec479e4c2773f3a879d6437fc30c19ec',
-     x86_64: 'b44fc8463ff340776c2038da369277fda23d7c1d5fe95f138a666b0204817995'
+    aarch64: '7314c1f2b339c804ae2a2f6d6796bdc0e7dcc100ef6996e0da1f1543dc95c008',
+     armv7l: '7314c1f2b339c804ae2a2f6d6796bdc0e7dcc100ef6996e0da1f1543dc95c008',
+       i686: '51d59a3021eebc098d69cbff7e895743bf66c0f04b180fa78af3273e418b8249',
+     x86_64: '8ae83a1eee02a8f95c6d95be86ac44420eb4008a03df1bcce3fd13677d103cda'
   })
 
   depends_on 'glib'
 
   def self.build
-    system "#{CREW_ENV_OPTIONS} meson #{CREW_MESON_OPTIONS} \
+    system "meson #{CREW_MESON_OPTIONS} \
       builddir"
     system 'meson configure builddir'
     system 'ninja -C builddir'
--- packages/py3_docutils.rb
+++ packages/py3_docutils.rb
@@ -3,24 +3,24 @@ require 'package'
 class Py3_docutils < Package
   description 'Docutils is an open-source text processing system for processing plaintext documentation into useful formats, such as HTML, LaTeX, man-pages, open-document or XML.'
   homepage 'http://docutils.sourceforge.net/'
-  @_ver = '0.17.1'
-  version "#{@_ver}-1"
+  @_ver = '0.19'
+  version @_ver.to_s
   license 'BSD-2, GPL-3 and public-domain'
   compatibility 'all'
-  source_url 'https://files.pythonhosted.org/packages/4c/17/559b4d020f4b46e0287a2eddf2d8ebf76318fd3bd495f1625414b052fdc9/docutils-0.17.1.tar.gz'
-  source_sha256 '686577d2e4c32380bb50cbb22f575ed742d58168cee37e99117a854bcd88f125'
+  source_url 'https://files.pythonhosted.org/packages/6b/5c/330ea8d383eb2ce973df34d1239b3b21e91cd8c865d21ff82902d952f91f/docutils-0.19.tar.gz'
+  source_sha256 '33995a6753c30b7f577febfc2c50411fec6aac7f7ffeb7c4cfe5991072dcf9e6'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.17.1-1_armv7l/py3_docutils-0.17.1-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.17.1-1_armv7l/py3_docutils-0.17.1-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.17.1-1_i686/py3_docutils-0.17.1-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.17.1-1_x86_64/py3_docutils-0.17.1-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.19_armv7l/py3_docutils-0.19-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.19_armv7l/py3_docutils-0.19-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.19_i686/py3_docutils-0.19-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_docutils/0.19_x86_64/py3_docutils-0.19-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '89a62ed3b047061fe765329171a91e76d7e113006136fbbb25311f6ce1692309',
-     armv7l: '89a62ed3b047061fe765329171a91e76d7e113006136fbbb25311f6ce1692309',
-       i686: 'a906df40046622e6b6e13220da7b03a5adcf677127d9da6ebe1bba3734aa683b',
-     x86_64: '5db96f75f85e79f9f69fed41f4c5739996626d1a288fbd35ef9a22c10d169b75'
+    aarch64: 'e5105f54a9ced471b418123b1a9f8132bb872ea8341b089f2ba2804edbe14d2a',
+     armv7l: 'e5105f54a9ced471b418123b1a9f8132bb872ea8341b089f2ba2804edbe14d2a',
+       i686: '6132c1d239cd79e5d8a249bff6e0fa3976910d930aaaa1211a3318e0b507722b',
+     x86_64: 'd3c4af2bcf3e87c14aca985ada99faaa3af70ad6f7068bff0becf603c97fd82e'
   })
 
   depends_on 'py3_setuptools' => :build
@@ -31,5 +31,7 @@ class Py3_docutils < Package
 
   def self.install
     system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+    # Needed for gdk_pixbuf build.
+    FileUtils.ln "#{CREW_DEST_PREFIX}/bin/rst2man.py", "#{CREW_DEST_PREFIX}/bin/rst2man"
   end
 end
