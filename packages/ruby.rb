889c19e4203a76c4caf9085c651c43bfb0a9a907
Satadru Pramanik <satadru@gmail.com>
2022-08-19 23:26:00 -0400

    ruby: repackage as tar.xz

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -3,24 +3,23 @@ require 'package'
 class Ruby < Package
   description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
   homepage 'https://www.ruby-lang.org/en/'
-  @_ver = '3.1.2'
-  version "#{@_ver}-2"
+  version = '3.1.2-3'
   license 'Ruby-BSD and BSD-2'
   compatibility 'all'
   source_url 'https://github.com/ruby/ruby.git'
-  git_hashtag "v#{@_ver.tr('.', '_')}"
+  git_hashtag 'v3_1_2'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-2_armv7l/ruby-3.1.2-2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-2_armv7l/ruby-3.1.2-2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-2_i686/ruby-3.1.2-2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-2_x86_64/ruby-3.1.2-2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_armv7l/ruby-3.1.2-3-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_armv7l/ruby-3.1.2-3-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_i686/ruby-3.1.2-3-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_x86_64/ruby-3.1.2-3-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: '6a7f6815e3c945afa520415d82caed3a277af294d323713ff3d3afb2e9201374',
-     armv7l: '6a7f6815e3c945afa520415d82caed3a277af294d323713ff3d3afb2e9201374',
-       i686: '8c1c82df986e4038b1c6e4a17d1807542e336c0517269f5398351dc896ab2343',
-     x86_64: '70bae7cde10fadde59f6b2ab5c2017fdfa390385820a4154df49533527b6e3ff'
+    aarch64: 'a466a4eed626562b831ece35d10502f3b0816ba81ac7ff3b4c7a04051aa17856',
+     armv7l: 'a466a4eed626562b831ece35d10502f3b0816ba81ac7ff3b4c7a04051aa17856',
+       i686: '52fb0865ecb00ce46a1f5e58bf39413aaa32683f4dd547d316ac83a1c54a7946',
+     x86_64: 'b18f6c4ad3165cc35f25d6089276a7680a4e34bd9dd1ff3fbcd150ba17a8ef7a'
   })
 
   depends_on 'zlibpkg' # R
@@ -36,16 +35,17 @@ class Ruby < Package
   # at run-time, system's gmp, openssl, readline and zlibpkg can be used
 
   no_patchelf
+  no_zstd
 
   def self.build
     system '[ -x configure ] || autoreconf -fiv'
     system "RUBY_TRY_CFLAGS='stack_protector=no' \
       RUBY_TRY_LDFLAGS='stack_protector=no' \
-      optflags='-flto' \
+      optflags='-flto -fuse-ld=mold' \
       ./configure #{CREW_OPTIONS} \
       --enable-shared \
       --disable-fortify-source"
-    system "make"
+    system 'make'
   end
 
   def self.check
