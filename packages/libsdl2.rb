77fd24281a6cbb0fa743d4914c6f51186f0412e5
Satadru Pramanik <satadru@gmail.com>
2022-08-22 11:19:13 -0400

    libsdl2 -> 2.24.0 (#7318)
    * libsdl2 -> 2.24.0
    
    * fix header order

--- packages/libsdl2.rb
+++ packages/libsdl2.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libsdl2 < Package
   description 'Simple DirectMedia Layer is a cross-platform development library designed to provide low level access to audio, keyboard, mouse, joystick, and graphics hardware via OpenGL and Direct3D.'
   homepage 'http://www.libsdl.org'
-  @_ver = '2.0.22'
+  @_ver = '2.24.0'
   version @_ver
   license 'ZLIB'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Libsdl2 < Package
   git_hashtag "release-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.22_armv7l/libsdl2-2.0.22-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.22_armv7l/libsdl2-2.0.22-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.22_i686/libsdl2-2.0.22-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.0.22_x86_64/libsdl2-2.0.22-chromeos-x86_64.tar.zst'
+    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.24.0_i686/libsdl2-2.24.0-chromeos-i686.tar.zst',
+  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.24.0_x86_64/libsdl2-2.24.0-chromeos-x86_64.tar.zst',
+ aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.24.0_armv7l/libsdl2-2.24.0-chromeos-armv7l.tar.zst',
+  armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libsdl2/2.24.0_armv7l/libsdl2-2.24.0-chromeos-armv7l.tar.zst'
   })
   binary_sha256({
-    aarch64: 'f4da387182a750ffd0217770492f2726e98b5bf10610e73cdf19264b1c133509',
-     armv7l: 'f4da387182a750ffd0217770492f2726e98b5bf10610e73cdf19264b1c133509',
-       i686: '53ee765821d142fe2b0c9f8af8dd1529466977a4229964c0c2c7c22c94ec00e8',
-     x86_64: '9d6abfe61dae7446d32c9500d9f8764dff8f1a89af5029bbbf4b83acf7b582d1'
+    i686: '1bd6297e4988229ce177a37f49985356610fc30d6f853a0b3bd8375ddbf67b61',
+  x86_64: '9a93f85dc4c8889b48c32ed9755367e67df2bceb0c200a59bc25f8d85a51d83f',
+ aarch64: '359f785c46faa39300fcdca8a52f9b35a3cbc143cf02d88d0a7aacb0b7df068e',
+  armv7l: '359f785c46faa39300fcdca8a52f9b35a3cbc143cf02d88d0a7aacb0b7df068e'
   })
 
   depends_on 'xorg_server'
