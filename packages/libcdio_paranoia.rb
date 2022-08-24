c2fe92445d1b38f852553f1d9537d10af465fcc0
Satadru Pramanik <satadru@gmail.com>
2022-08-23 23:40:32 -0400

    Mpv -> 0.34.1 and fix deps (#7333)
    * fix mpv deps
    
    * mpv -> 0.34.1
    
    * adjust indents on mpv

--- packages/libcdio_paranoia.rb
+++ packages/libcdio_paranoia.rb
@@ -6,33 +6,30 @@ require 'package'
 class Libcdio_paranoia < Package
   description 'CD paranoia libraries from libcdio'
   homepage 'https://www.gnu.org/software/libcdio/'
-  version '10.2-2.0.1'
+  version '10.2-2.0.1-1'
   license 'GPLv3'
   compatibility 'all'
   source_url 'https://github.com/rocky/libcdio-paranoia/archive/release-10.2+2.0.1.tar.gz'
   source_sha256 '7a4e257c85f3f84129cca55cd097c397364c7a6f79b9701bbc593b13bd59eb95'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1_armv7l/libcdio_paranoia-10.2-2.0.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1_armv7l/libcdio_paranoia-10.2-2.0.1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1_i686/libcdio_paranoia-10.2-2.0.1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1_x86_64/libcdio_paranoia-10.2-2.0.1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1-1_armv7l/libcdio_paranoia-10.2-2.0.1-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1-1_armv7l/libcdio_paranoia-10.2-2.0.1-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1-1_i686/libcdio_paranoia-10.2-2.0.1-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libcdio_paranoia/10.2-2.0.1-1_x86_64/libcdio_paranoia-10.2-2.0.1-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '8ba30c99931a347744767a197b49cb14cb3459e7997928bb4377acc2082afeaf',
-     armv7l: '8ba30c99931a347744767a197b49cb14cb3459e7997928bb4377acc2082afeaf',
-       i686: '9fde7bf09aa6023ca6128088ec9f1e1be2df36b4a2d7b596f20d448a11124044',
-     x86_64: '6b324873fd7c441d6aaedb03463c19267d1352cf73af95d872808ff5de4ee35b'
+    aarch64: '4098fbea318ee03de6ccecbf8bae3a1075ab9f42fc5a7fe6a02f2016aba2ab4d',
+     armv7l: '4098fbea318ee03de6ccecbf8bae3a1075ab9f42fc5a7fe6a02f2016aba2ab4d',
+       i686: '825a103125f4c22eec50202f9ca8da9d5a287f54e8bbc6da24202da53bc5ca90',
+     x86_64: 'e5f50c96babd9bdcc74f92de9ea9ac6a58372c858ae79c40292f6726a44d76ea'
   })
 
   depends_on 'libcdio'
 
   def self.build
     system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
-    system "env CFLAGS='-flto=auto' \
-      CXXFLAGS='-pipe -flto=auto' \
-      LDFLAGS='-flto=auto' \
-      ./configure #{CREW_OPTIONS} \
+    system " ./configure #{CREW_OPTIONS} \
       --enable-cpp-progs \
       --disable-example-progs"
     system 'make'
