26e99ed8d8bb91eff961df63d9f640f4647d0fbf
Satadru Pramanik <satadru@gmail.com>
2022-08-23 21:11:44 -0400

    remmina, freerdp, vte updates

--- packages/vte.rb
+++ packages/vte.rb
@@ -3,7 +3,7 @@ require 'package'
 class Vte < Package
   description 'Virtual Terminal Emulator widget for use with GTK'
   homepage 'https://wiki.gnome.org/Apps/Terminal/VTE'
-  @_ver = '0.67.90'
+  @_ver = '0.69.92'
   version @_ver
   license 'LGPL-2+ and GPL-3+'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Vte < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.67.90_armv7l/vte-0.67.90-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.67.90_armv7l/vte-0.67.90-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.67.90_i686/vte-0.67.90-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.67.90_x86_64/vte-0.67.90-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.69.92_armv7l/vte-0.69.92-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.69.92_armv7l/vte-0.69.92-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.69.92_i686/vte-0.69.92-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vte/0.69.92_x86_64/vte-0.69.92-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'dcfdd849cf41e7eebdbf4898736f3a4a4110bce26bdfb46fb8b2e2ed6abca976',
-     armv7l: 'dcfdd849cf41e7eebdbf4898736f3a4a4110bce26bdfb46fb8b2e2ed6abca976',
-       i686: 'df376361939b8573ab907b9a2be807c1086bf37b9979829153d8f73194ca3fa4',
-     x86_64: '646cc0968db68d6db74c294a134ad05a1b3b04af0661416b0f19997e6492867b'
+    aarch64: 'c7b015afc564f5ca0f50328ca1ae8f1e27bbc8d9f5d389feb783f9d36986e9d7',
+     armv7l: 'c7b015afc564f5ca0f50328ca1ae8f1e27bbc8d9f5d389feb783f9d36986e9d7',
+       i686: '9f6840aeb382b7bfcdf089f2f50aa3edfe9993c6c657bc6e9c87b9256f0d27f8',
+     x86_64: 'e3958e79e715ca4b03fbb25c03bb728db4c0c7f92e97b57dba62410c18979546'
   })
 
   depends_on 'gobject_introspection' => :build
