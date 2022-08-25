03734526a013de263b715debe7c2f817942d385d
chronos <no-reply@github.com>
2022-08-25 04:29:59 +0000

    Add openjdk8 package

---
+++ packages/openjdk8.rb
@@ -0,0 +1,43 @@
+require 'package'
+
+class Openjdk8 < Package
+  description 'The JDK is a development environment for building applications, applets, and components using the Java programming language.'
+  homepage 'https://openjdk.org/'
+  version '1.8.0_342'
+  license 'GPL-2'
+  compatibility 'all'
+  source_url({
+    aarch64: 'https://cdn.azul.com/zulu-embedded/bin/zulu8.64.0.15-ca-jdk8.0.342-linux_aarch32hf.tar.gz',
+     armv7l: 'https://cdn.azul.com/zulu-embedded/bin/zulu8.64.0.15-ca-jdk8.0.342-linux_aarch32hf.tar.gz',
+       i686: 'https://cdn.azul.com/zulu/bin/zulu8.64.0.19-ca-jdk8.0.345-linux_i686.tar.gz',
+     x86_64: 'https://cdn.azul.com/zulu/bin/zulu8.64.0.19-ca-jdk8.0.345-linux_x64.tar.gz'
+  })
+  source_sha256({
+    aarch64: 'ef9b8e9592fce03170af6ba536a7c48c3f81276fa28ec7aa25c476829a11a473',
+     armv7l: 'ef9b8e9592fce03170af6ba536a7c48c3f81276fa28ec7aa25c476829a11a473',
+       i686: '87ee29b0dd1d16e98b12ec55f5351d4001ba30d686fa5fccb67ca04f6db94496',
+     x86_64: '1f99233f6e9cc92567316f2757c48b1bf4bb5624d40afa56ec263cfebdd887f9'
+  })
+
+  no_compile_needed
+  no_fhs
+
+  def self.preflight
+    jdkver = `java -version 2>&1 | head -1 | cut -d'"' -f2`.chomp
+    unless jdkver.empty? || jdkver.include?('No such file or directory') || jdkver.include?('not found')
+      majver = `java -version 2>&1 | head -1 | cut -d'"' -f2 | cut -d'.' -f1`.chomp
+      majver = '8' if majver == '1'
+      puts "Package jdk#{majver} already installed.".lightgreen
+      abort "Enter `crew remove jdk#{majver} && crew install openjdk8` to install this version."
+    end
+  end
+
+  def self.install
+    FileUtils.mkdir_p CREW_DEST_MAN_PREFIX
+    FileUtils.cp_r 'bin/', CREW_DEST_PREFIX
+    FileUtils.cp_r 'include/', CREW_DEST_PREFIX
+    FileUtils.cp_r 'jre/', CREW_DEST_PREFIX
+    FileUtils.cp_r 'lib/', CREW_DEST_PREFIX
+    FileUtils.cp_r Dir['man/*'], CREW_DEST_MAN_PREFIX
+  end
+end
