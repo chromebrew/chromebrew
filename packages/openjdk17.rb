a178088d900dca0426fb566d16da75efcc559325
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-08-25 09:59:05 -0500

    I forgot Google doesn't properly support armv8 yet

--- packages/openjdk17.rb
+++ packages/openjdk17.rb
@@ -7,13 +7,13 @@ class Openjdk17 < Package
   license 'GPL-2'
   compatibility 'all'
   source_url({
-    aarch64: 'https://cdn.azul.com/zulu/bin/zulu17.36.17-ca-jdk17.0.4.1-linux_aarch64.tar.gz',
+    aarch64: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.36.19-ca-jdk17.0.4.1-linux_aarch32hf.tar.gz',
      armv7l: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.36.19-ca-jdk17.0.4.1-linux_aarch32hf.tar.gz',
        i686: 'https://cdn.azul.com/zulu/bin/zulu17.36.19-ca-jdk17.0.4.1-linux_i686.tar.gz',
      x86_64: 'https://cdn.azul.com/zulu/bin/zulu17.36.17-ca-jdk17.0.4.1-linux_x64.tar.gz'
   })
   source_sha256({
-    aarch64: '8e8b7fa243a152b79527651a47a255f4db91c7886b32de14fb2787a2a6aa51dc',
+    aarch64: '93228b96f904a53495fc30d338316c488da908f54368dc0d2643255b4ebd6cef',
      armv7l: '93228b96f904a53495fc30d338316c488da908f54368dc0d2643255b4ebd6cef',
        i686: '96a3c6e70ce5eff76cc54fa67c53c6f657229f00abbaed59c15da3f7f85829af',
      x86_64: 'b3a1e74047ab3209eac8c22f991436c6c4cead3d77892d3a67a056d848aae26a'
