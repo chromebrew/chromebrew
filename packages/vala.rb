5281d613df4c49e64ff90e5f5d4e4e2706762d58
Satadru Pramanik <satadru@gmail.com>
2022-09-22 19:33:00 -0400

    update cantarell_fonts and glib

--- packages/vala.rb
+++ packages/vala.rb
@@ -16,10 +16,10 @@ class Vala < Package
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vala/0.56.3_x86_64/vala-0.56.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '86d5a6f17c1221eb24c2802342c2645074c895311991f2a4a2deb9436b220cc0',
-     armv7l: '86d5a6f17c1221eb24c2802342c2645074c895311991f2a4a2deb9436b220cc0',
-       i686: '3b902fb63708c219f8e5cede9b24fee5641899611b294ee4e391f578489fa41c',
-     x86_64: '3edff660a6693a9245e7e9fa2bd4a4cf890b993f0ed3b3b4fef42484fba34b2d'
+    aarch64: '80704831036697f5d42a8e2a9127d19ced5c093d63ac22cf491a7f34f6cd6b1c',
+     armv7l: '80704831036697f5d42a8e2a9127d19ced5c093d63ac22cf491a7f34f6cd6b1c',
+       i686: 'e813314e321e7f2b4cfe01aa058e9d9f554581af0c4ace8487a3082127b939da',
+     x86_64: '7aa8e4200d9a8553d0055ecb4bcbc1621bb1d0ae9e16b4cd456233e1360d7058'
   })
 
   depends_on 'autoconf_archive' => :build
