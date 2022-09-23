58d5fd32971e9c38d22754c58911ef254b9a10df
Satadru Pramanik <satadru@gmail.com>
2022-09-22 20:13:06 -0400

    pygobject -> 3.42.2

--- packages/pygobject.rb
+++ packages/pygobject.rb
@@ -3,24 +3,24 @@ require 'package'
 class Pygobject < Package
   description 'PyGObject is a Python package which provides bindings for GObject based libraries such as GTK+, GStreamer, WebKitGTK+, GLib, GIO and many more.'
   homepage 'https://pygobject.readthedocs.io/'
-  @_ver = '3.42.0'
-  version "#{@_ver}-1"
+  @_ver = '3.42.2'
+  version @_ver.to_s
   license 'LGPL-2.1+'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/pygobject.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0-1_armv7l/pygobject-3.42.0-1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0-1_armv7l/pygobject-3.42.0-1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0-1_i686/pygobject-3.42.0-1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.0-1_x86_64/pygobject-3.42.0-1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.2_armv7l/pygobject-3.42.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.2_armv7l/pygobject-3.42.2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.2_i686/pygobject-3.42.2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pygobject/3.42.2_x86_64/pygobject-3.42.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '8d3c72f92d272438ada7781dc3c7e2830e92c5567b37000770a6a567398e4aee',
-     armv7l: '8d3c72f92d272438ada7781dc3c7e2830e92c5567b37000770a6a567398e4aee',
-       i686: '3a24ec575fc98956236aa0ade2ace4eb7cee0c62b78c1abe4bd3924c4a12f16d',
-     x86_64: '301494929ef6856b8278cbedaf77a2d6b73746826576671708dfbb4c866e84fd'
+    aarch64: '0f0bd117ff285b72db15d479ce21676e7931a87e20430d94f4f5c1b4bacf3c4a',
+     armv7l: '0f0bd117ff285b72db15d479ce21676e7931a87e20430d94f4f5c1b4bacf3c4a',
+       i686: '819f11aa7a9230417860c66fc39d348ffdf4a103939b5143ed64fd32e442997f',
+     x86_64: 'd51378dfb0c2b7124a178d15a318a2184851f08ba1421ed40d8ed674f839c07c'
   })
 
   depends_on 'glib'
