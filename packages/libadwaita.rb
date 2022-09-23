249d885aa3855dcf91f0ee3594d9b8da16333f1b
Satadru Pramanik <satadru@gmail.com>
2022-09-23 19:59:56 -0400

    libadwaita -> 1.2.0

--- packages/libadwaita.rb
+++ packages/libadwaita.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libadwaita < Package
   description 'Library of GNOME-specific UI patterns, replacing libhandy for GTK4'
   homepage 'https://gitlab.gnome.org/GNOME/libadwaita/'
-  @_ver = '1.0.3'
+  @_ver = '1.2.0'
   version @_ver
   license 'LGPL-2.1+'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Libadwaita < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.0.3_armv7l/libadwaita-1.0.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.0.3_armv7l/libadwaita-1.0.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.0.3_i686/libadwaita-1.0.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.0.3_x86_64/libadwaita-1.0.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.2.0_armv7l/libadwaita-1.2.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.2.0_armv7l/libadwaita-1.2.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.2.0_i686/libadwaita-1.2.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libadwaita/1.2.0_x86_64/libadwaita-1.2.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '009e5ef5d3adcfde46a4b7e671f707680c46d9817c031732eec13aee1c0d170f',
-     armv7l: '009e5ef5d3adcfde46a4b7e671f707680c46d9817c031732eec13aee1c0d170f',
-       i686: '3e7139dea2d70fbc714594070bfa6316024413200815bafb1d860b5d99c3c291',
-     x86_64: 'd828e672cbf543de90112d024cf2db87ac2d38f68ccbab2d970f01059189b057'
+    aarch64: '8fafff9cafac5e7b44668c80192c392cde6837894a084d5f7ea21bf183d048d8',
+     armv7l: '8fafff9cafac5e7b44668c80192c392cde6837894a084d5f7ea21bf183d048d8',
+       i686: '32d94aab8c421bcc1375528a16d1a643e9879b163ec3903d25527ee377e74c77',
+     x86_64: '02445f3204282fdb1c151acdfd042be5f2dc85b07e5fb06a38cfa8d7ee37137b'
   })
 
   depends_on 'cairo'
@@ -40,7 +40,7 @@ class Libadwaita < Package
             -Dintrospection=enabled \
             -Dgtk_doc=false builddir"
     system 'meson configure builddir'
-    system 'samu -C builddir'
+    system 'mold -run samu -C builddir'
   end
 
   def self.install
