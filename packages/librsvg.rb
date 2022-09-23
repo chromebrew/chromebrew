06be1224e51de7efb502ec0e9372256f4637398f
Satadru Pramanik <satadru@gmail.com>
2022-09-22 21:29:50 -0400

    librsvg -> 2.55.1

--- packages/librsvg.rb
+++ packages/librsvg.rb
@@ -3,23 +3,23 @@ require 'package'
 class Librsvg < Package
   description 'SVG library for GNOME'
   homepage 'https://wiki.gnome.org/Projects/LibRsvg'
-  version '2.54.4'
+  version '2.55.1'
   license 'LGPL-2+'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/librsvg.git'
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.54.4_armv7l/librsvg-2.54.4-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.54.4_armv7l/librsvg-2.54.4-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.54.4_i686/librsvg-2.54.4-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.54.4_x86_64/librsvg-2.54.4-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.55.1_armv7l/librsvg-2.55.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.55.1_armv7l/librsvg-2.55.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.55.1_i686/librsvg-2.55.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/librsvg/2.55.1_x86_64/librsvg-2.55.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '58760d1112269cead225097aaac939ea5be45e2b10616bb8dff6ffbe421a68df',
-     armv7l: '58760d1112269cead225097aaac939ea5be45e2b10616bb8dff6ffbe421a68df',
-       i686: '01233045873ee26b638876acd32697ca2b9c5d80327c6aff04d6bdd840831527',
-     x86_64: 'c8d079c44fe9017d16ec1b32aecd0c6e00e6143f02bd0a124fb9eb683d29a5a8'
+    aarch64: '951f7eef7aa09ff792a1b42a004f69098cd92a327d18146106eaea970d233968',
+     armv7l: '951f7eef7aa09ff792a1b42a004f69098cd92a327d18146106eaea970d233968',
+       i686: '9b21a71b73784056ec3143f78c87974a9f06415bc4f48595c7ceb8aa404549e0',
+     x86_64: '6963f6819d9e73f78ffff35ae39c48650dad22071decaf74d57fc14db1806f80'
   })
 
   depends_on 'fontconfig'
