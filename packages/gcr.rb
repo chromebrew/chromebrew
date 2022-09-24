6a676a20005618b5489b8e30c1be094e03c70228
Satadru Pramanik <satadru@gmail.com>
2022-09-23 23:28:02 -0400

    gcr -> 3.92.0

--- packages/gcr.rb
+++ packages/gcr.rb
@@ -3,24 +3,24 @@ require 'package'
 class Gcr < Package
   description 'GNOME crypto package'
   homepage 'https://www.gnome.org'
-  @_ver = '3.40.0'
-  version "#{@_ver}-1"
+  @_ver = '3.92.0'
+  version @_ver.to_s
   license 'GPL-2+ and LGPL-2+'
   compatibility 'all'
   source_url "https://gitlab.gnome.org/GNOME/gcr/-/archive/#{@_ver}/gcr-#{@_ver}.tar.bz2"
-  source_sha256 '659a49bac1c713a743894845c82ef53ccc7326dcce1879b1af0ab502ec10e7ab'
+  source_sha256 '94db38a729bc3725e3ab64589222f574dbba9b30bf52cbcb7266a38f67a0e55b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.40.0-1_armv7l/gcr-3.40.0-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.40.0-1_armv7l/gcr-3.40.0-1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.40.0-1_i686/gcr-3.40.0-1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.40.0-1_x86_64/gcr-3.40.0-1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_armv7l/gcr-3.92.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_armv7l/gcr-3.92.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_i686/gcr-3.92.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_x86_64/gcr-3.92.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e1062d5322f4dd084436459fe26e76af0a6118e3a1b0f88329dacae0617c0d96',
-     armv7l: 'e1062d5322f4dd084436459fe26e76af0a6118e3a1b0f88329dacae0617c0d96',
-       i686: 'd927f0e2bdcfa4542d68d4ddc9371b67776f2724b19bd0847029dee6d9751244',
-     x86_64: 'a7a7c8ecd1472f17269e33624ba2e91ca4dd699613f833ce9b566889a81ab45f'
+    aarch64: 'e89038cc2cb54d472aac9b431881284a8844bbbbca49f0ad63b6c1480abf9a29',
+     armv7l: 'e89038cc2cb54d472aac9b431881284a8844bbbbca49f0ad63b6c1480abf9a29',
+       i686: 'eb3972c1901413040c8d0e9abaad63b219358c0d63d1484acd85cb29b0352f9b',
+     x86_64: '28c34c8d44abea29b4c067dc36f9740cb4013a90f9c9a4bbc0c73850d386f837'
   })
 
   depends_on 'cairo'
@@ -33,9 +33,12 @@ class Gcr < Package
   depends_on 'hicolor_icon_theme'
   depends_on 'libgcrypt'
   depends_on 'libjpeg'
+  depends_on 'libsecret'
   depends_on 'libxslt'
   depends_on 'pango'
   depends_on 'vala' => :build
+  depends_on 'vulkan_headers' => :build
+  gnome
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
