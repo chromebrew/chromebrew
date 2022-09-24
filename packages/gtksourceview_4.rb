90add2cf068cceb9c4b60dd4723dd1e80141d18c
Satadru Pramanik <satadru@gmail.com>
2022-09-23 20:09:31 -0400

    gtksourceview_4 -> 4.8.3

--- packages/gtksourceview_4.rb
+++ packages/gtksourceview_4.rb
@@ -3,22 +3,24 @@ require 'package'
 class Gtksourceview_4 < Package
   description 'Source code editing widget'
   homepage 'https://wiki.gnome.org/Projects/GtkSourceView'
-  @_ver = '4.8.1'
+  @_ver = '4.8.3'
   version @_ver
   license 'LGPL-2.1+'
-  compatibility 'x86_64 aarch64 armv7l'
-  source_url "https://gitlab.gnome.org/GNOME/gtksourceview/-/archive/#{@_ver}/gtksourceview-#{@_ver}.tar.bz2"
-  source_sha256 'ec16a6a61d643e278d3a02828257a59d802d4ae740247bf03fdebf10d9401635'
+  compatibility 'all'
+  source_url 'https://gitlab.gnome.org/GNOME/gtksourceview.git'
+  git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtksourceview_4/4.8.1_armv7l/gtksourceview_4-4.8.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtksourceview_4/4.8.1_armv7l/gtksourceview_4-4.8.1-chromeos-armv7l.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtksourceview_4/4.8.1_x86_64/gtksourceview_4-4.8.1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtksourceview_4/4.8.3_armv7l/gtksourceview_4-4.8.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtksourceview_4/4.8.3_armv7l/gtksourceview_4-4.8.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtksourceview_4/4.8.3_i686/gtksourceview_4-4.8.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtksourceview_4/4.8.3_x86_64/gtksourceview_4-4.8.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b6e5a158eaf5853eadcc5fcf3057c84b60800113358e3db76b1c9b716faec11e',
-     armv7l: 'b6e5a158eaf5853eadcc5fcf3057c84b60800113358e3db76b1c9b716faec11e',
-     x86_64: '68b3cea57e7cd0edf6712b9ad3b8c9c1e3b2b7723d7a1cc070178b04db597d82'
+    aarch64: '664f99be9dcaf2caba948f7ff4f06b9b1280bc78b0c95608f3e8d2cdfab65edd',
+     armv7l: '664f99be9dcaf2caba948f7ff4f06b9b1280bc78b0c95608f3e8d2cdfab65edd',
+       i686: '02daf0ff8c5a8fa2b86fad1a1ad71dfeff43c5fc81467cedfa874f849737b6a5',
+     x86_64: 'a9c2b5d14d3b1603ed0400edd45aa2ca907d1bdf413c85623152788ed342d494'
   })
 
   depends_on 'atk'
@@ -35,6 +37,7 @@ class Gtksourceview_4 < Package
   depends_on 'libsoup'
   depends_on 'pango'
   depends_on 'vala'
+  depends_on 'vulkan_headers' => :build
   depends_on 'vulkan_icd_loader'
 
   def self.patch
@@ -46,10 +49,10 @@ class Gtksourceview_4 < Package
     -Db_asneeded=false \
     builddir"
     system 'meson configure builddir'
-    system 'ninja -C builddir'
+    system 'mold -run samu -C builddir'
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
   end
 end
