a8d313a16c3a57fc7a46a95e639f347edd51c71e
Satadru Pramanik <satadru@gmail.com>
2022-09-25 14:12:37 -0400

    suggested rework by @supechicken

--- packages/hunspell.rb
+++ packages/hunspell.rb
@@ -1,36 +1,65 @@
 require 'package'
-require 'timeout'
 
 class Hunspell < Package
   description 'Hunspell is a spell checker and morphological analyzer library'
-  homepage 'http://hunspell.github.io/'
+  homepage 'https://hunspell.github.io'
   version '1.7.0'
   license 'MPL-1.1, GPL-2 and LGPL-2.1'
   compatibility 'all'
-  source_url 'https://github.com/hunspell/hunspell/archive/v1.7.0.tar.gz'
-  source_sha256 'bb27b86eb910a8285407cf3ca33b62643a02798cf2eef468c0a74f6c3ee6bc8a'
 
   is_fake
 
-  if ARGV[0] == 'install'
-    begin
-      @timeout = 10
-      @user_input = Timeout.timeout(@timeout) do
-        puts
-        puts 'Enter your preferred language:'
-        puts "(The default, American English, will be selected in #{@timeout} seconds if there is no input.)"
-        puts '1 = American English'
-        puts '2 = Français'
-        puts '3 = Español'
-        puts '0 = Cancel'
-        $stdin.getc.chomp.to_i
+  if ARGV.include?('install') # fix when 'install' isn't in ARGV[0] (like `crew -k install`)
+    @timeout = 10
+
+    puts <<~EOT
+
+      Enter your preferred language:
+      (The default, American English, will be selected in #{@timeout} seconds if there is no input.)
+
+        1 = American English
+        2 = Français
+        3 = Español
+        0 = Cancel
+
+    EOT
+
+    if STDIN.isatty # check if stdin is a terminal
+      @stdin_thread = Thread.new do
+        @user_input = STDIN.getc.to_i
+
+        Thread.kill(@countdown_thread) # stop countdown when user input
       end
-    rescue Timeout::Error
-      @user_input = 1
+
+      @countdown_thread = Thread.new do
+        (0..@timeout).each do |i|
+          remaining_time = @timeout - i
+          print "\r#{remaining_time} second(s) left: "
+
+          if remaining_time.zero?
+            Thread.kill(@stdin_thread) # stop reading from terminal if timeout
+          else
+            sleep 1
+          end
+        end
+      end
+
+      @stdin_thread.join
+      @countdown_thread.join
     end
-    depends_on 'hunspell_en_us' if @user_input == 1
-    depends_on 'hunspell_fr_fr' if @user_input == 2
-    depends_on 'hunspell_es_any' if @user_input == 3
-    abort if @user_input.zero?
+
+    case @user_input
+    when 0
+      abort "\nCancelled.".lightred
+    when 1
+      depends_on 'hunspell_en_us'
+    when 2
+      depends_on 'hunspell_fr_fr'
+    when 3
+      depends_on 'hunspell_es_any'
+    else
+      warn "\nSelected 'American English' by default.".yellow
+      depends_on 'hunspell_en_us'
+    end
   end
 end
