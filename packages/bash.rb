7c5f247cf80e052517bcb528955454ca03caa636
Satadru Pramanik <satadru@gmail.com>
2022-09-27 13:55:45 -0400

    bash -> 5.2 (#7432)

--- packages/bash.rb
+++ packages/bash.rb
@@ -3,54 +3,55 @@ require 'package'
 class Bash < Package
   description 'The GNU Bourne Again SHell is a Bourne-compatible shell with useful csh and ksh features.'
   homepage 'https://www.gnu.org/software/bash/'
-  version '5.1.16'
+  version '5.2'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/bash/bash-5.1.16.tar.gz'
-  source_sha256 '5bac17218d3911834520dad13cd1f85ab944e1c09ae1aba55906be1f8192f558'
+  source_url 'https://ftpmirror.gnu.org/bash/bash-5.2.tar.gz'
+  source_sha256 'a139c166df7ff4471c5e0733051642ee5556c1cc8a4a78f145583c5c81ab32fb'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.1.16_armv7l/bash-5.1.16-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.1.16_armv7l/bash-5.1.16-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.1.16_i686/bash-5.1.16-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.1.16_x86_64/bash-5.1.16-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_armv7l/bash-5.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_armv7l/bash-5.2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_i686/bash-5.2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_x86_64/bash-5.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e81f33f36736aeba1d48ddffcc626dc0c9732cea529283e7c0abc80964c45641',
-     armv7l: 'e81f33f36736aeba1d48ddffcc626dc0c9732cea529283e7c0abc80964c45641',
-       i686: '0b1d3b6f3a6ca41e28bddf1838edd26fdacc20aa7f8ab4085665931247db7660',
-     x86_64: 'd92e0f2a8b44e93c6d6869a074039b74f7a4295583b8497637f058de56a28d32'
+    aarch64: 'e6bfc3b8744f2eca36531b74ff9b0c12c6f0c6cfb2ac767095cadc93c00223e4',
+     armv7l: 'e6bfc3b8744f2eca36531b74ff9b0c12c6f0c6cfb2ac767095cadc93c00223e4',
+       i686: '1e267103627dc708ccbd7ca767fede6e2dce0f972ab0bb1b42059045836ceb9f',
+     x86_64: 'b3c92bb0d63a5dbbea804123932faec289e9e560b8f2224a34c958f7fd9d87c3'
   })
 
-  depends_on 'mimalloc' => :build if ARCH == 'i686'
-  no_env_options
-
   def self.build
-    case ARCH
-    when 'i686'
-      @configure_flags = '--without-bash-malloc'
-      # Use mimalloc since it is better than an OLD malloc from an old
-      # glibc
-      @bash_env_options = CREW_ENV_OPTIONS.gsub('LDFLAGS="',
-                                                "LDFLAGS=\"#{CREW_LIB_PREFIX}/libmimalloc.a ")
-    when 'aarch64', 'armv7l', 'x86_64'
-      @configure_flags = '--with-bash-malloc'
-      @bash_env_options = CREW_ENV_OPTIONS
-    end
-    puts @bash_env_options.lightblue
-    system <<~BUILD
-      #{@bash_env_options} ./configure #{CREW_OPTIONS} #{@configure_flags} \
-        --with-curses --enable-readline \
-        --enable-mem-scramble --enable-usg-echo-default \
-        --enable-single-help-strings --enable-select \
-        --enable-restricted --enable-progcomp --enable-process-substitution \
-        --enable-net-redirections --enable-multibyte --enable-job-control \
-        --enable-history --enable-help-builtin --enable-dparen-arithmetic \
-        --enable-directory-stack --enable-coprocesses --enable-cond-regexp \
-        --enable-cond-command --enable-command-timing --enable-casemod-expansions \
-        --enable-casemod-attributes --enable-brace-expansion --enable-bang-history \
-        --enable-array-variables --enable-arith-for-command --enable-alias
-    BUILD
+    system "./configure #{CREW_OPTIONS} \
+        --enable-alias\
+        --enable-arith-for-command \
+        --enable-array-variables \
+        --enable-bang-history \
+        --enable-brace-expansion \
+        --enable-casemod-attributes \
+        --enable-casemod-expansions \
+        --enable-command-timing \
+        --enable-cond-command \
+        --enable-cond-regexp \
+        --enable-coprocesses \
+        --enable-directory-stack \
+        --enable-dparen-arithmetic \
+        --enable-help-builtin \
+        --enable-history \
+        --enable-job-control \
+        --enable-mem-scramble \
+        --enable-multibyte \
+        --enable-net-redirections \
+        --enable-process-substitution \
+        --enable-progcomp \
+        --enable-readline \
+        --enable-restricted \
+        --enable-select \
+        --enable-single-help-strings \
+        --enable-usg-echo-default \
+        --with-bash-malloc \
+        --with-curses"
 
     system 'make'
   end
