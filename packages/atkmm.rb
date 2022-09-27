fee713d6b06d2296cf9ab5779eb82191a38cd853
Satadru Pramanik <satadru@gmail.com>
2022-09-27 15:27:07 -0400

    update atkmm yelp_xsl yelp_tools

--- packages/atkmm.rb
+++ packages/atkmm.rb
@@ -3,34 +3,33 @@ require 'package'
 class Atkmm < Package
   description 'Atkmm is the official C++ interface for the ATK accessibility toolkit library.'
   homepage 'https://www.gtkmm.org/'
-  version '2.28.1'
+  version '2.28.3'
   license 'LGPL-2.1+'
   compatibility 'all'
-  source_url 'https://ftp.gnome.org/pub/gnome/sources/atkmm/2.28/atkmm-2.28.1.tar.xz'
-  source_sha256 '116876604770641a450e39c1f50302884848ce9cc48d43c5dc8e8efc31f31bad'
+  source_url 'https://ftp.gnome.org/pub/gnome/sources/atkmm/2.28/atkmm-2.28.3.tar.xz'
+  source_sha256 '7c2088b486a909be8da2b18304e56c5f90884d1343c8da7367ea5cd3258b9969'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.1_armv7l/atkmm-2.28.1-chromeos-armv7l.tar.xz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.1_armv7l/atkmm-2.28.1-chromeos-armv7l.tar.xz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.1_i686/atkmm-2.28.1-chromeos-i686.tar.xz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.1_x86_64/atkmm-2.28.1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.3_armv7l/atkmm-2.28.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.3_armv7l/atkmm-2.28.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.3_i686/atkmm-2.28.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atkmm/2.28.3_x86_64/atkmm-2.28.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '0f959c7e95bee71b46cb1b90902d0e7556d0914e56e1a4e7654c95166307dd8d',
-      armv7l: '0f959c7e95bee71b46cb1b90902d0e7556d0914e56e1a4e7654c95166307dd8d',
-        i686: '1b400ecf33d5dcbceaffee8608a06c155fcd57d118262569136552c1f11c96bc',
-      x86_64: '2aee40b687e3c021894d9d98719655de5bc703f5d397b4ee2f452accc45915cd'
+    aarch64: 'f0e1d2d64a82204014132c2218ca8909d940d89929eb538ea18d530433d27883',
+     armv7l: 'f0e1d2d64a82204014132c2218ca8909d940d89929eb538ea18d530433d27883',
+       i686: 'aac296de2f47c8fd9dc3cd1bfcf3e1d059ff5665549802e603664862d54d8f77',
+     x86_64: 'ceee59dcefe0aa9fe7eac7a6af0b4057d9301661aceb87a62741e6a097b11b32'
   })
 
   depends_on 'atk'
   depends_on 'glibmm'
+  gnome
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
     --default-library=both \
     -Dbuild-documentation=false \
-    -Dbuild-demos=false \
-    -Dbuild-tests=false \
     builddir"
     system 'meson configure builddir'
     system 'ninja -C builddir'
--- packages/yelp_tools.rb
+++ packages/yelp_tools.rb
@@ -3,23 +3,23 @@ require 'package'
 class Yelp_tools < Package
   description 'yelp-tools is a collection of scripts and build utilities to help create, manage, and publish documentation for Yelp and the web'
   homepage 'https://github.com/GNOME/yelp-tools'
-  version '40.0'
+  version '42.0'
   license 'GPL-2+ or freedist and GPL-2+'
   compatibility 'all'
   source_url "https://gitlab.gnome.org/GNOME/yelp-tools/-/archive/#{version}/yelp-tools-#{version}.tar.bz2"
-  source_sha256 'bada2afb5160066aef39e11f90eb5377f0bb161aa2b4dcd744c381e3c2ff77ce'
+  source_sha256 'c71a7b10adb91712887c96541ce6eedf7c6f68d0cd1fa67bce0b27c46c3b2b05'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/40.0_armv7l/yelp_tools-40.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/40.0_armv7l/yelp_tools-40.0-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/40.0_i686/yelp_tools-40.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/40.0_x86_64/yelp_tools-40.0-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_armv7l/yelp_tools-42.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_armv7l/yelp_tools-42.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_i686/yelp_tools-42.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_x86_64/yelp_tools-42.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '1317a7aa14db9fd3f3794ece39708c35320b2098a3265dfaad0b7d1e418b0319',
-     armv7l: '1317a7aa14db9fd3f3794ece39708c35320b2098a3265dfaad0b7d1e418b0319',
-       i686: '86378c2ee8f256700d241c977cf676c45607f0733bcdd811fa138ddc7114e834',
-     x86_64: '2c039b5bbc4aacf02f8e6892ce52e2cc334144c769b44ea2251429f2d91fc677'
+    aarch64: '346bc52c050fc19c822aea860ee73d017a738ee74f8dbbdf3710ec0f52480fae',
+     armv7l: '346bc52c050fc19c822aea860ee73d017a738ee74f8dbbdf3710ec0f52480fae',
+       i686: '1534b7273cf516f371b11203ac7b2cda478754d26b155a295ae18f264f714cdf',
+     x86_64: '67191036eaff541ae39d3da6f9d1b4b70c45944bcbac429d4a9e949b586ad4d1'
   })
 
   depends_on 'yelp_xsl'
@@ -27,7 +27,7 @@ class Yelp_tools < Package
   depends_on 'py3_lxml'
 
   def self.build
-    system "meson #{CREW_MESON_LTO_OPTIONS} \
+    system "meson #{CREW_MESON_OPTIONS} \
     builddir"
     system 'meson configure builddir'
     system 'samu -C builddir'
