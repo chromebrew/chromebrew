d57aa38ec55173ff96ed3c514b30585acab162b3
Satadru Pramanik <satadru@gmail.com>
2022-09-29 17:35:52 -0400

    rebuilds of re2c, ninja with mold 1.5.1

--- packages/ninja.rb
+++ packages/ninja.rb
@@ -3,40 +3,31 @@ require 'package'
 class Ninja < Package
   description 'a small build system with a focus on speed'
   homepage 'https://ninja-build.org'
-  version '1.11.1'
+  version '1.11.1-1'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/ninja-build/ninja/archive/v1.11.1.tar.gz'
-  source_sha256 '31747ae633213f1eda3842686f83c2aa1412e0f5691d1c14dbbcc67fe7400cea'
+  source_url 'https://github.com/ninja-build/ninja.git'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1_armv7l/ninja-1.11.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1_armv7l/ninja-1.11.1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1_i686/ninja-1.11.1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1_x86_64/ninja-1.11.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1-1_armv7l/ninja-1.11.1-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1-1_armv7l/ninja-1.11.1-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1-1_i686/ninja-1.11.1-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ninja/1.11.1-1_x86_64/ninja-1.11.1-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ff439c9f733c638cbfb37602ec31775ab8102f927e7c26b57b3bedd2a30c725d',
-     armv7l: 'ff439c9f733c638cbfb37602ec31775ab8102f927e7c26b57b3bedd2a30c725d',
-       i686: 'ffa46259484a0a86c92010b3cc62867fb2cff0862c9e2ce17723d8318b9ce635',
-     x86_64: '96cad814e005ffb14a47a01bb7bf61971cc1890439e879444f04bf88f5083886'
+    aarch64: 'e3e130403a43980cc2ef0a03adb9156a6dbd31b5718efb445681a2191dbc2b2a',
+     armv7l: 'e3e130403a43980cc2ef0a03adb9156a6dbd31b5718efb445681a2191dbc2b2a',
+       i686: '190004a05d176d485eb3f0919fef7baac98d5d78f571160022558aabdec1d6bf',
+     x86_64: '6ed13fed4d9f7332ce42b48e1da9411f0b9928ec9effb1d30817e342f5ef45d8'
   })
 
+  git_hashtag 'v1.11.1'
+
   depends_on 're2c' => :build
-  depends_on 'samurai' => :build
 
   def self.build
     Dir.mkdir 'builddir'
     Dir.chdir 'builddir' do
-      # Handle issue with re2c: error: cannot rename or write temporary file
-      # ninja-1.11.1/builddir/lexer.cc to output file ninja-1.11.1/builddir/lexer.cc
-      # https://github.com/skvadrik/re2c/issues/417
-      if ARCH == 'x86_64'
-        @files = %w[lexer.cc depfile_parser.cc]
-        @files.each do |file|
-          FileUtils.touch file
-        end
-      end
       system "mold -run cmake \
           #{CREW_CMAKE_OPTIONS} \
           -Wdev \
--- packages/re2c.rb
+++ packages/re2c.rb
@@ -3,23 +3,23 @@ require 'package'
 class Re2c < Package
   description 're2c is a free and open-source lexer generator for C and C++.'
   homepage 'http://re2c.org/'
-  version '3.0'
+  version '3.0-1'
   license 'public-domain'
   compatibility 'all'
   source_url 'https://github.com/skvadrik/re2c/releases/download/3.0/re2c-3.0.tar.lz'
   source_sha256 '39757db2defc21a6713560cdff75895fb30f02af22a3ac8066fd9ba84f16a1b3'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0_armv7l/re2c-3.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0_armv7l/re2c-3.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0_i686/re2c-3.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0_x86_64/re2c-3.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0-1_armv7l/re2c-3.0-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0-1_armv7l/re2c-3.0-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0-1_i686/re2c-3.0-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2c/3.0-1_x86_64/re2c-3.0-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '2d26bed05dd891ca9111f32fefb93e731ff075091e73780cac8978d87860185e',
-     armv7l: '2d26bed05dd891ca9111f32fefb93e731ff075091e73780cac8978d87860185e',
-       i686: '66382a147c0f35c6b66f80b1ff11191cd46a40f5134a0a740a1073c09b27242f',
-     x86_64: 'b84773fba0c9890b2128edc28925f3b4d827ed34f56f58204f6e2cba65d7bf3d'
+    aarch64: 'e7bd792f3befad5c69622bf54d20c77f029b40ca40a3a0f223e380b79b9d6fd9',
+     armv7l: 'e7bd792f3befad5c69622bf54d20c77f029b40ca40a3a0f223e380b79b9d6fd9',
+       i686: '1ab8a1859f894f9e1d8581007cd62f8095fee0b43e60e954a824c8fd70e270f6',
+     x86_64: '2deeb6677004c222983ed9a2841f4e14e45010d8fefd0cc06f6b6d3fe623c7db'
   })
 
   def self.build
