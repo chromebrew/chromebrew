abb8e0475d269960658248e95852244678a59f0d
Satadru Pramanik <satadru@gmail.com>
2022-09-29 20:43:10 -0400

    rebuilds of libssh libnghttp2 libidn2 libpsl libcurl

--- packages/libidn2.rb
+++ packages/libidn2.rb
@@ -4,23 +4,23 @@ class Libidn2 < Package
   description 'GNU Libidn is a fully documented implementation of the Stringprep, Punycode and IDNA 2003 specifications.'
   homepage 'https://www.gnu.org/software/libidn/'
   @_ver = '2.3.3'
-  version @_ver
+  version "#{@_ver}-1"
   license 'GPL-2+ and LGPL-3+'
   compatibility 'all'
   source_url "https://ftpmirror.gnu.org/libidn/libidn2-#{@_ver}.tar.gz"
   source_sha256 'f3ac987522c00d33d44b323cae424e2cffcb4c63c6aa6cd1376edacbf1c36eb0'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3_armv7l/libidn2-2.3.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3_armv7l/libidn2-2.3.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3_i686/libidn2-2.3.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3_x86_64/libidn2-2.3.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3-1_armv7l/libidn2-2.3.3-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3-1_armv7l/libidn2-2.3.3-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3-1_i686/libidn2-2.3.3-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libidn2/2.3.3-1_x86_64/libidn2-2.3.3-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '506044ffa75da19775112ad71b62c1531b9b272304ece0d250891b7df73dd298',
-     armv7l: '506044ffa75da19775112ad71b62c1531b9b272304ece0d250891b7df73dd298',
-       i686: 'f8b784f50e9cb56f73f78520a0712e6dceba66316a86571f3aa4165daab02a4f',
-     x86_64: '7c5c25b5028ec8c1a4e8ae7b525959b1130e4118e10233238ce96adf51938809'
+    aarch64: '1f1a4300e5141ff670b61c24ce33eea2455801af61916fa325866acd47648a2f',
+     armv7l: '1f1a4300e5141ff670b61c24ce33eea2455801af61916fa325866acd47648a2f',
+       i686: '4cb850ff4d430533d513e5811b8af44c4081161c9d5c5714e7c1fe7b2f1434d7',
+     x86_64: 'ea4b9348c401c8655f0a77fc92149887b26fb7cad7f6ce5628488fa72d8223d6'
   })
 
   def self.build
--- packages/libpsl.rb
+++ packages/libpsl.rb
@@ -4,23 +4,23 @@ class Libpsl < Package
   description 'C library for the Public Suffix List'
   homepage 'https://github.com/rockdaboot/libpsl'
   @_ver = '0.21.1'
-  version "#{@_ver}-2"
+  version "#{@_ver}-3"
   license 'MIT'
   compatibility 'all'
   source_url "https://github.com/rockdaboot/libpsl/releases/download/#{@_ver}/libpsl-#{@_ver}.tar.lz"
   source_sha256 '644375d557bb3b84c485df2dae98ee388fe1e11fb75230004e4b8623b3b833a9'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-2_armv7l/libpsl-0.21.1-2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-2_armv7l/libpsl-0.21.1-2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-2_i686/libpsl-0.21.1-2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-2_x86_64/libpsl-0.21.1-2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-3_armv7l/libpsl-0.21.1-3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-3_armv7l/libpsl-0.21.1-3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-3_i686/libpsl-0.21.1-3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpsl/0.21.1-3_x86_64/libpsl-0.21.1-3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'd3ceed9e1804466bc69c4850385353b2cc149c3c2e391726afef1791af4934ef',
-     armv7l: 'd3ceed9e1804466bc69c4850385353b2cc149c3c2e391726afef1791af4934ef',
-       i686: '8d06b3c1b028b6abab13f0c77d1e36903781d20e68b80709b09f2fd0fa0ac945',
-     x86_64: '36a20069519346782dad64a36cdb1628de2dcbf01320399ee6c088c18fd86b3f'
+    aarch64: 'f35e2a5509eece1e4117aaa558ca3d50bd3e5015bc1781ade88ed173210142fc',
+     armv7l: 'f35e2a5509eece1e4117aaa558ca3d50bd3e5015bc1781ade88ed173210142fc',
+       i686: 'ee55698080997b2d65c26dc3fccf1fdde660d76c52fdd3ed8498064a88b91a71',
+     x86_64: '65be38748baca2f4bf9cf689b9894ce62dc37d909e2545a27a9612ec9fb8d419'
   })
 
   def self.build
