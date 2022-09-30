81d32dff530bf0beac581014d85625c159414404
Satadru Pramanik <satadru@gmail.com>
2022-09-30 07:08:06 -0400

    cmake rebuild

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -4,23 +4,23 @@ class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
   @_ver = '3.24.2'
-  version @_ver
+  version "#{@_ver}-1"
   license 'CMake'
   compatibility 'all'
   source_url 'https://github.com/Kitware/CMake.git'
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2_armv7l/cmake-3.24.2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2_armv7l/cmake-3.24.2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2_i686/cmake-3.24.2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2_x86_64/cmake-3.24.2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2-1_armv7l/cmake-3.24.2-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2-1_armv7l/cmake-3.24.2-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2-1_i686/cmake-3.24.2-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.24.2-1_x86_64/cmake-3.24.2-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '14b515ebc3b2fdf607975d38e193d50b61a2a3163edff069bfa266346e09bfec',
-     armv7l: '14b515ebc3b2fdf607975d38e193d50b61a2a3163edff069bfa266346e09bfec',
-       i686: '7ba3147e7d93c10f59d71c510fd49742dd2574dd8cc794ac1aede4506942e758',
-     x86_64: '017673d3425483e0e33af04c6ecb59f1a6713047a1ef9a23cb98fbbb9019d6c8'
+    aarch64: '7c553287271609f9f2153b79879495f65f64ad5c8f7f146b66beebc168de9e6a',
+     armv7l: '7c553287271609f9f2153b79879495f65f64ad5c8f7f146b66beebc168de9e6a',
+       i686: 'bcaa0e282dba32c33bd6cec8678cae917e1b740289835c4d90f45bf56e17bbd7',
+     x86_64: '8c82b522e826f18d25189ced89a99e86ee660d965feb0af2b9176ed9f1c5b6e6'
   })
 
   depends_on 'expat'
