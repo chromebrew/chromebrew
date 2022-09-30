f72f02e159489b39124f9f9644f68a9fb31664a7
Satadru Pramanik <satadru@gmail.com>
2022-09-29 21:24:01 -0400

    add bash rebuild

--- packages/bash.rb
+++ packages/bash.rb
@@ -3,23 +3,23 @@ require 'package'
 class Bash < Package
   description 'The GNU Bourne Again SHell is a Bourne-compatible shell with useful csh and ksh features.'
   homepage 'https://www.gnu.org/software/bash/'
-  version '5.2'
+  version '5.2-1'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/bash/bash-5.2.tar.gz'
   source_sha256 'a139c166df7ff4471c5e0733051642ee5556c1cc8a4a78f145583c5c81ab32fb'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_armv7l/bash-5.2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_armv7l/bash-5.2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_i686/bash-5.2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2_x86_64/bash-5.2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2-1_armv7l/bash-5.2-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2-1_armv7l/bash-5.2-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2-1_i686/bash-5.2-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bash/5.2-1_x86_64/bash-5.2-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e6bfc3b8744f2eca36531b74ff9b0c12c6f0c6cfb2ac767095cadc93c00223e4',
-     armv7l: 'e6bfc3b8744f2eca36531b74ff9b0c12c6f0c6cfb2ac767095cadc93c00223e4',
-       i686: '1e267103627dc708ccbd7ca767fede6e2dce0f972ab0bb1b42059045836ceb9f',
-     x86_64: 'b3c92bb0d63a5dbbea804123932faec289e9e560b8f2224a34c958f7fd9d87c3'
+    aarch64: '06b11b54021d3aed9765959a7bb11d062c1fa5df454ce617dffdc0be9e9bab8f',
+     armv7l: '06b11b54021d3aed9765959a7bb11d062c1fa5df454ce617dffdc0be9e9bab8f',
+       i686: '30949988b5b75a3db5e0d33385a470e434b0b4cc584c837f9380cd5af4c489c8',
+     x86_64: '897db36a9bc15c296d519a77af77d0dee125f17a72dc923031a11910c3afd880'
   })
 
   def self.build
