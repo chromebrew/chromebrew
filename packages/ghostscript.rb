3adb6793ef5fa778b6a3f9d1e969284cfc9e9625
Satadru Pramanik <satadru@gmail.com>
2022-10-01 23:39:31 -0400

    initial ghostscript 10

--- packages/ghostscript.rb
+++ packages/ghostscript.rb
@@ -3,23 +3,23 @@ require 'package'
 class Ghostscript < Package
   description 'Interpreter for the PostScript language'
   homepage 'https://www.ghostscript.com/'
-  version '9.54'
+  version '10.0.0'
   license 'AGPL-3+'
   compatibility 'all'
-  source_url 'https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs9540/ghostpdl-9.54.0.tar.xz'
-  source_sha256 'ecbaa2e79e6d82cab8d0fbdfd899aa4fc68ffb43f6901d547d33ca7008fe1871'
+  source_url 'https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs1000/ghostpdl-10.0.0.tar.xz'
+  source_sha256 '8f2b7941f60df694b4f5c029b739007f7c4e0d43858471ae481e319a967d5d8b'
 
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ghostscript/9.54_armv7l/ghostscript-9.54-chromeos-armv7l.tar.xz',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ghostscript/9.54_armv7l/ghostscript-9.54-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ghostscript/9.54_i686/ghostscript-9.54-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ghostscript/9.54_x86_64/ghostscript-9.54-chromeos-x86_64.tar.xz'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ghostscript/10.0.0_i686/ghostscript-10.0.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ghostscript/10.0.0_x86_64/ghostscript-10.0.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
     aarch64: 'a2fee20357654918f2deac0d7af356c84898e282d008906ede6b424fd95fd549',
      armv7l: 'a2fee20357654918f2deac0d7af356c84898e282d008906ede6b424fd95fd549',
-       i686: 'f68277f2d042311835bf70828dcae6ce281d5cb376e4d525d63e1858012ab30d',
-     x86_64: '92ac6123594ea3fe04474e7654785a599f65b57884f5931b71bfeb63100b0741'
+       i686: '91dfb5bab16d31c3418c57cae464f0b2377f0e19d0057cec751ef8b14f50b9aa',
+     x86_64: 'f2652c512f3ea6c1d484cb3ce7d72879570cce5fd52c95f679d2e449b4084dc5'
   })
 
   depends_on 'atk'
@@ -44,12 +44,12 @@ class Ghostscript < Package
   depends_on 'openjpeg'
   depends_on 'pango'
 
-  case ARCH
-  when 'armv7l', 'aarch64'
-    @ltoflags = ''
-  when 'x86_64', 'i686'
-    @ltoflags = '-flto'
-  end
+  # case ARCH
+  # when 'armv7l', 'aarch64'
+  # @ltoflags = ''
+  # when 'x86_64', 'i686'
+  # @ltoflags = '-flto'
+  # end
 
   def self.patch
     system 'rm -r cups/libs expat ijs jpeg lcms2mt libpng openjpeg tiff zlib'
@@ -58,9 +58,7 @@ class Ghostscript < Package
   def self.build
     system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
     system 'filefix'
-    system "env CFLAGS='-pipe -fuse-ld=gold #{@ltoflags}' \
-      CXXFLAGS='-pipe -fuse-ld=gold #{@ltoflags}' \
-      ./configure #{CREW_OPTIONS} \
+    system "./configure #{CREW_OPTIONS} \
       --disable-compile-inits \
       --enable-dynamic \
       --enable-fontconfig \
