4345f3a0cf3e1941a708741eccde2928a88f9abb
Satadru Pramanik <satadru@gmail.com>
2022-10-02 10:59:12 -0400

    update emacs gstreamer

--- packages/gstreamer.rb
+++ packages/gstreamer.rb
@@ -4,23 +4,23 @@ class Gstreamer < Package
   description 'GStreamer is a library for constructing graphs of media-handling components.'
   homepage 'https://gstreamer.freedesktop.org/'
   @_ver = '1.20.3'
-  version @_ver
+  version "#{@_ver}-1"
   license 'LGPL-2+'
   compatibility 'all'
   source_url 'https://gitlab.freedesktop.org/gstreamer/gstreamer.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3_armv7l/gstreamer-1.20.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3_armv7l/gstreamer-1.20.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3_i686/gstreamer-1.20.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3_x86_64/gstreamer-1.20.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3-1_armv7l/gstreamer-1.20.3-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3-1_armv7l/gstreamer-1.20.3-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3-1_i686/gstreamer-1.20.3-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gstreamer/1.20.3-1_x86_64/gstreamer-1.20.3-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a2c884c6f7db53d0a56d0483e10026181d0a8fdd6e1fcd9aa03ab03dca95b28a',
-     armv7l: 'a2c884c6f7db53d0a56d0483e10026181d0a8fdd6e1fcd9aa03ab03dca95b28a',
-       i686: '98748c1022ef0945d4b270975cd4ad42c12754d83beacbae8e6badd041d60e37',
-     x86_64: 'ed5e8eb2d867cb0426ee2d8d9d54ad3cfd7dcc832dedae0845833f2b68fb5cfe'
+    aarch64: 'e841406957a963846da6a63864001c8165b7afa83abcc73ec1a86b939a3c9462',
+     armv7l: 'e841406957a963846da6a63864001c8165b7afa83abcc73ec1a86b939a3c9462',
+       i686: 'f2ac4f825c4b078617ab15ca590dfe1dbd4fb644c87ad984726cb28f70117a18',
+     x86_64: '23752e3b1a1cb4feb2c3c0a7286833ef2d49598ab615ef93f0874b535e4d1e06'
   })
 
   depends_on 'elfutils'
@@ -28,6 +28,7 @@ class Gstreamer < Package
   depends_on 'gsl'
   depends_on 'gtk3'
   depends_on 'libcap'
+  depends_on 'libjpeg'
   depends_on 'libunwind'
 
   conflicts_ok # conflicts with orc, gst_plugins_{base,bad}
