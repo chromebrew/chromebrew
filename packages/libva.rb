0e46887972a9f5ee84d29f3d8deabdfff3371804
Satadru Pramanik <satadru@gmail.com>
2022-10-03 12:04:48 -0400

    libva -> 2.16.0

--- packages/libva.rb
+++ packages/libva.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libva < Package
   description 'Libva is an implementation for VA-API (Video Acceleration API)'
   homepage 'https://01.org/linuxmedia'
-  @_ver = '2.15.0'
+  @_ver = '2.16.0'
   version @_ver.to_s
   license 'MIT'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Libva < Package
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_armv7l/libva-2.15.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_armv7l/libva-2.15.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_i686/libva-2.15.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libva/2.15.0_x86_64/libva-2.15.0-chromeos-x86_64.tar.zst'
+    aarch64: 'file:///usr/local/tmp/packages/libva-2.16.0-chromeos-armv7l.tar.zst',
+     armv7l: 'file:///usr/local/tmp/packages/libva-2.16.0-chromeos-armv7l.tar.zst',
+       i686: 'file:///usr/local/tmp/packages/libva-2.16.0-chromeos-i686.tar.zst',
+     x86_64: 'file:///usr/local/tmp/packages/libva-2.16.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '201b781e9ecae81f6d7b978f9314ca321f6dbe78829467f84f5f38461597b371',
-     armv7l: '201b781e9ecae81f6d7b978f9314ca321f6dbe78829467f84f5f38461597b371',
-       i686: 'db83ae08bb404bfec867ba159a30b1d9bca57d4fe17f22332965da0ce4aab8da',
-     x86_64: '660fde88cdf68687ae52aca11d88ecc90424a943d7dd16ab5c572143637007a4'
+    aarch64: 'ac2a55f303a50fc14f80f0308cdef28cee672d08a435e806b27f4845b4a16402',
+     armv7l: 'ac2a55f303a50fc14f80f0308cdef28cee672d08a435e806b27f4845b4a16402',
+       i686: '080be345a0938b687dea51acae964e89f3b5b34169283a6bd6a9f55d9b839356',
+     x86_64: '320022e94dc9beac537279085f66d4caa270e8af9fe4eb0455fbaa11b48c5258'
   })
 
   depends_on 'libdrm'
