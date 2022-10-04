e4e032e575959715ff8b2e4cb26e12e857efc9b3
Satadru Pramanik <satadru@gmail.com>
2022-10-04 17:22:53 -0400

    gcr rebuild and git update

--- packages/gcr.rb
+++ packages/gcr.rb
@@ -4,23 +4,23 @@ class Gcr < Package
   description 'GNOME crypto package'
   homepage 'https://www.gnome.org'
   @_ver = '3.92.0'
-  version @_ver.to_s
+  version "#{@_ver}-1"
   license 'GPL-2+ and LGPL-2+'
   compatibility 'all'
   source_url "https://gitlab.gnome.org/GNOME/gcr/-/archive/#{@_ver}/gcr-#{@_ver}.tar.bz2"
   source_sha256 '94db38a729bc3725e3ab64589222f574dbba9b30bf52cbcb7266a38f67a0e55b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_armv7l/gcr-3.92.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_armv7l/gcr-3.92.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_i686/gcr-3.92.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0_x86_64/gcr-3.92.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0-1_armv7l/gcr-3.92.0-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0-1_armv7l/gcr-3.92.0-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0-1_i686/gcr-3.92.0-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcr/3.92.0-1_x86_64/gcr-3.92.0-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e89038cc2cb54d472aac9b431881284a8844bbbbca49f0ad63b6c1480abf9a29',
-     armv7l: 'e89038cc2cb54d472aac9b431881284a8844bbbbca49f0ad63b6c1480abf9a29',
-       i686: 'eb3972c1901413040c8d0e9abaad63b219358c0d63d1484acd85cb29b0352f9b',
-     x86_64: '28c34c8d44abea29b4c067dc36f9740cb4013a90f9c9a4bbc0c73850d386f837'
+    aarch64: '0617bb385b1e183456bfcf68d95e14c5a031b0ce11c654ee9efd75e9185d21b4',
+     armv7l: '0617bb385b1e183456bfcf68d95e14c5a031b0ce11c654ee9efd75e9185d21b4',
+       i686: 'f7e78aa0f37cb98ad07d88a32dc993d027de9be7369193f4e4eb9e2a9cc4fcc3',
+     x86_64: '11faa5ee0fead928462f8dd0489f33da216cc0fec4bf544956ed94e4c58ab589'
   })
 
   depends_on 'cairo'
