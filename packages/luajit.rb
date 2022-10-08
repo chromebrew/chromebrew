3c1fc0b8238046d6916db94b5ef14e21078e4aae
Satadru Pramanik <satadru@gmail.com>
2022-10-07 21:08:29 -0400

    update luajit

--- packages/luajit.rb
+++ packages/luajit.rb
@@ -3,23 +3,23 @@ require 'package'
 class Luajit < Package
   description 'LuaJIT is a Just-In-Time Compiler (JIT) for the Lua programming language.'
   homepage 'https://github.com/openresty/luajit2'
-  version '2.1-20220411'
+  version '2.1-20220915'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/openresty/luajit2.git'
-  git_hashtag "v#{version}"
+  source_url "https://github.com/openresty/luajit2/archive/refs/tags/v#{version}.tar.gz"
+  source_sha256 'a6104d4fa342d864ae00cb3313b07091af02dc61294b1d8fce1e17779998b497'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220411_armv7l/luajit-2.1-20220411-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220411_armv7l/luajit-2.1-20220411-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220411_i686/luajit-2.1-20220411-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220411_x86_64/luajit-2.1-20220411-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220915_armv7l/luajit-2.1-20220915-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220915_armv7l/luajit-2.1-20220915-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220915_i686/luajit-2.1-20220915-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit/2.1-20220915_x86_64/luajit-2.1-20220915-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '18fbd6916f9f75db3b5939997af0ef447102f49439941852c044e079d82bb611',
-     armv7l: '18fbd6916f9f75db3b5939997af0ef447102f49439941852c044e079d82bb611',
-       i686: '747299199b1011f83b77c0e142052811bb0f4736a1ec834062ff6f1c36338951',
-     x86_64: 'ad76e59f1079539a74159d81563ed7d23f5e333661d63a46cc3f6508d8fd8c5e'
+    aarch64: '8ece5b7a8d617e0b6f717ac3d7f999f75ae2fac85707d0f7f5da815aaffe15cd',
+     armv7l: '8ece5b7a8d617e0b6f717ac3d7f999f75ae2fac85707d0f7f5da815aaffe15cd',
+       i686: '945cb7a5d311ed4b124709ea30a3843b1997929a95c27322950fcb8b71a1fae0',
+     x86_64: '8f446341a8e379d65d820709d70ba85c1cbb47f4dcbcf3f0f9e200fd60d5c565'
   })
 
   def self.build
