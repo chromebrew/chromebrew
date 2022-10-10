6e17bbb60ee00f62ea6efeb3487665a7eac2e516
Satadru Pramanik <satadru@gmail.com>
2022-10-09 20:32:45 -0400

    revert to older luajit_lgi

--- packages/luajit_lgi.rb
+++ packages/luajit_lgi.rb
@@ -6,23 +6,23 @@ require 'package'
 class Luajit_lgi < Package
   description 'Lua bindings for gnome/gobject using gobject-introspection library'
   homepage 'https://github.com/pavouk/lgi'
-  version '0.9.2-34fe0e2'
+  version '0.9.2'
   license 'custom:MIT'
   compatibility 'all'
-  source_url 'https://github.com/lgi-devs/lgi/archive/34fe0e2470429be11fc7268a9391ee715b3377e0.zip'
-  source_sha256 'f0076a2652c0f0057e9f62d3ea2edb51cf5680844162d93912c47461c66d3243'
+  source_url 'https://github.com/lgi-devs/lgi/archive/refs/tags/0.9.2.tar.gz'
+  source_sha256 'cfc4105482b4730b3a40097c9d9e7e35c46df2fb255370bdeb2f45a886548c4f'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit_lgi/0.9.2-34fe0e2_armv7l/luajit_lgi-0.9.2-34fe0e2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit_lgi/0.9.2-34fe0e2_armv7l/luajit_lgi-0.9.2-34fe0e2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit_lgi/0.9.2-34fe0e2_i686/luajit_lgi-0.9.2-34fe0e2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/luajit_lgi/0.9.2-34fe0e2_x86_64/luajit_lgi-0.9.2-34fe0e2-chromeos-x86_64.tar.zst'
+    aarch64: 'file:///usr/local/tmp/packages/luajit_lgi-0.9.2-chromeos-armv7l.tar.zst',
+     armv7l: 'file:///usr/local/tmp/packages/luajit_lgi-0.9.2-chromeos-armv7l.tar.zst',
+       i686: 'file:///usr/local/tmp/packages/luajit_lgi-0.9.2-chromeos-i686.tar.zst',
+     x86_64: 'file:///usr/local/tmp/packages/luajit_lgi-0.9.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b9c28200a459766c0688765b240402bae057313117e3d4ef232bff6dc8cbaef3',
-     armv7l: 'b9c28200a459766c0688765b240402bae057313117e3d4ef232bff6dc8cbaef3',
-       i686: '8925c8bd0e256cd1f57041b320f22868f60d670b066f4c477376bb123766b037',
-     x86_64: '810db53f6eedff005c6dc67cb047a4e900c9c3515f8bf710235016b7c526774f'
+    aarch64: 'e19655e9a947cf484f57a92c528fe58b830c53ebac0ef271e5a623c42c906982',
+     armv7l: 'e19655e9a947cf484f57a92c528fe58b830c53ebac0ef271e5a623c42c906982',
+       i686: 'ca6206e7ec462dd8d71caa64ec23cda3f6ac3a856f6deccbfb058f3244e06d12',
+     x86_64: 'fb5ba8a94d24d9e129ad435304102571bfc785e753dfa34e88e4addd5869fbde'
   })
 
   depends_on 'cairo'
@@ -33,22 +33,29 @@ class Luajit_lgi < Package
   depends_on 'luajit'
 
   def self.build
-    system "meson #{CREW_MESON_OPTIONS} \
-      -Dlua-pc=luajit \
-      -Dlua-bin=#{CREW_PREFIX}/bin/luajit \
-      -Dtests=false \
-      builddir"
-    system 'meson configure builddir'
-    system 'mold -run samu -C builddir'
+    @lua_cflags = `pkg-config --cflags luajit`.chomp
+    @lua_include = `pkg-config --cflags-only-I luajit | sed 's/-I//'`.chomp
+    system "make LUA_INCDIR=#{@lua_include} \
+       LUA_CFLAGS=#{@lua_cflags}"
+    # system "meson #{CREW_MESON_OPTIONS} \
+    #-Dlua-pc=luajit \
+    #-Dlua-bin=#{CREW_PREFIX}/bin/luajit \
+    #-Dtests=false \
+    # builddir"
+    # system 'meson configure builddir'
+    # system 'mold -run samu -C builddir'
   end
 
   def self.install
     @lua_version = `luajit -e "print(_VERSION)" | awk '{print $2}'`.chomp
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/lib/lua/#{@lua_version}/lgi"
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
-    return unless ARCH == 'x86_64'
+    system "make \
+    LUA_LIBDIR=#{CREW_PREFIX}/lib/lua/#{@lua_version} \
+    LUA_SHAREDIR=#{CREW_PREFIX}/share/lua/#{@lua_version} \
+    DESTDIR=#{CREW_DEST_DIR} install"
+    # return unless ARCH == 'x86_64'
 
-    FileUtils.mv "#{CREW_DEST_LIB_PREFIX}/lua/#{@lua_version}/lgi/corelgilua51.so",
-                 "#{CREW_DEST_PREFIX}/lib/lua/#{@lua_version}/lgi/corelgilua51.so"
+    # FileUtils.mv "#{CREW_DEST_LIB_PREFIX}/lua/#{@lua_version}/lgi/corelgilua51.so",
+    #             "#{CREW_DEST_PREFIX}/lib/lua/#{@lua_version}/lgi/corelgilua51.so"
   end
 end
