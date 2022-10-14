7900db110afdb5adbb35bf557964731e5a49ba27
Ed Reel <edreel@gmail.com>
2022-10-14 17:52:48 -0500

    Composer 2.4.1 => 2.4.3

--- packages/composer.rb
+++ packages/composer.rb
@@ -3,11 +3,11 @@ require 'package'
 class Composer < Package
   description 'Dependency Manager for PHP'
   homepage 'https://getcomposer.org/'
-  version '2.4.1'
+  version '2.4.3'
   license 'MIT'
   compatibility 'all'
   source_url "https://github.com/composer/composer/releases/download/#{version}/composer.phar"
-  source_sha256 'ea8cf6308ec76ff9645c3818841a7588096b9dc2767345fbd4bd492dd8a6dca6'
+  source_sha256 '26d72f2790502bc9b22209e1cec1e0e43d33b368606ad227d327cccb388b609a'
 
   depends_on 'php74' unless File.exist? "#{CREW_PREFIX}/bin/php"
   depends_on 'xdg_base'
@@ -28,7 +28,7 @@ class Composer < Package
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d"
     FileUtils.install 'composer.phar', "#{CREW_DEST_PREFIX}/bin/composer", mode: 0o755
-    system "echo 'export PATH=\$HOME/.config/composer/vendor/bin:\$PATH' > #{CREW_DEST_PREFIX}/etc/env.d/composer"
+    system "echo 'export PATH=\$HOME/.config/composer/vendor/bin:\$PATH' > #{CREW_DEST_PREFIX}/etc/env.d/10-composer"
   end
 
   def self.postinstall
