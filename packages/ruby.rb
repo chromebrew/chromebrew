4ef6801a1f65bc88fd8baa94a9cce1af6c5b17b1
Satadru Pramanik <satadru@gmail.com>
2022-10-15 00:44:44 -0400

    Add ruby debug package, rebuild ruby from 3.1 stable branch (#7490)
    * Add ruby debug package
    
    * bump core version
    
    * add ruby rebuild
    
    * suggested changes
    
    * change gemrc options
    
    * change grep string
    
    * build newer ruby 3.1
    
    * fix postinstall
    
    * revert name.delete changes
    
    * lint changes

--- packages/ruby.rb
+++ packages/ruby.rb
@@ -3,23 +3,23 @@ require 'package'
 class Ruby < Package
   description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
   homepage 'https://www.ruby-lang.org/en/'
-  version '3.1.2-3'
+  version '3.1-994b505'
   license 'Ruby-BSD and BSD-2'
   compatibility 'all'
-  source_url 'https://github.com/ruby/ruby.git'
-  git_hashtag 'v3_1_2'
+  source_url 'https://github.com/ruby/ruby/archive/994b505ffb0bf9eb795525199b47697412a98abb.zip'
+  source_sha256 '247155fd6978dffea5f1f25e7a77d1fe3c29c224ad24e15384c49e7e0aa71761'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_armv7l/ruby-3.1.2-3-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_armv7l/ruby-3.1.2-3-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_i686/ruby-3.1.2-3-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1.2-3_x86_64/ruby-3.1.2-3-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1-994b505_armv7l/ruby-3.1-994b505-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1-994b505_armv7l/ruby-3.1-994b505-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1-994b505_i686/ruby-3.1-994b505-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ruby/3.1-994b505_x86_64/ruby-3.1-994b505-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'a466a4eed626562b831ece35d10502f3b0816ba81ac7ff3b4c7a04051aa17856',
-     armv7l: 'a466a4eed626562b831ece35d10502f3b0816ba81ac7ff3b4c7a04051aa17856',
-       i686: '52fb0865ecb00ce46a1f5e58bf39413aaa32683f4dd547d316ac83a1c54a7946',
-     x86_64: 'b18f6c4ad3165cc35f25d6089276a7680a4e34bd9dd1ff3fbcd150ba17a8ef7a'
+    aarch64: 'ffa3f5aea7408599aab2d074876b30484bd845abf79dfffef8646fd8c38b7292',
+     armv7l: 'ffa3f5aea7408599aab2d074876b30484bd845abf79dfffef8646fd8c38b7292',
+       i686: '78180a2bbfbaadbbb0cfa8a6f3d9c3fe741da922dbf0242b53a4f6676d11e6d5',
+     x86_64: 'ae3cf478808d30321e87db6433f38d0886cf38adef123eba0e0a826b6c6367f1'
   })
 
   depends_on 'zlibpkg' # R
@@ -33,6 +33,7 @@ class Ruby < Package
   depends_on 'readline' # R
   depends_on 'ca_certificates'
   depends_on 'libyaml' # This is needed to install gems
+
   # at run-time, system's gmp, openssl, readline and zlibpkg can be used
 
   no_patchelf
@@ -42,7 +43,7 @@ class Ruby < Package
     system '[ -x configure ] || autoreconf -fiv'
     system "RUBY_TRY_CFLAGS='stack_protector=no' \
       RUBY_TRY_LDFLAGS='stack_protector=no' \
-      optflags='-flto -fuse-ld=mold' \
+      optflags='-flto -fuse-ld=#{CREW_LINKER}' \
       ./configure #{CREW_OPTIONS} \
       --enable-shared \
       --disable-fortify-source"
@@ -61,7 +62,14 @@ class Ruby < Package
 
   def self.postinstall
     puts 'Updating ruby gems. This may take a while...'
-    File.write("#{HOME}/.gemrc", "gem: --no-document\n", mode: 'a') if (File.exist?("#{HOME}/.gemrc") && !Kernel.system("grep -q \"gem: --no-document\" #{HOME}/.gemrc")) || !File.exist?("#{HOME}/.gemrc")
+    unless Kernel.system("grep -q \"no-document\" #{HOME}/.gemrc")
+      File.write("#{HOME}/.gemrc", "gem: --no-document\n",
+                 mode: 'a')
+    end
+    unless Kernel.system("grep -q \"gempath\" #{HOME}/.gemrc")
+      File.write("#{HOME}/.gemrc", "gempath: #{CREW_LIB_PREFIX}/ruby/gems/3.1.0\n",
+                 mode: 'a')
+    end
     silent = @opt_verbose ? '' : '--silent'
     system "gem update #{silent} -N --system"
   end
