93244381d06aa88bbec4a68f426a73245803d543
Satadru Pramanik <satadru@gmail.com>
2022-10-15 23:05:16 -0400

    libgxps rebuild

--- packages/libgxps.rb
+++ packages/libgxps.rb
@@ -7,23 +7,23 @@ class Libgxps < Package
   description 'XPS Documents library'
   homepage 'https://wiki.gnome.org/Projects/libgxps'
   @_ver = '0.3.2'
-  version "#{@_ver}-1"
+  version "#{@_ver}-2"
   license 'GPL2'
   compatibility 'all'
   source_url "https://gitlab.gnome.org/GNOME/libgxps/-/archive/#{@_ver}/libgxps-#{@_ver}.tar.bz2"
   source_sha256 'ffbc656e801233a64ea5b6ab5532691e8b91f72cce3ea957615fe8523696d42a'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-1_armv7l/libgxps-0.3.2-1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-1_armv7l/libgxps-0.3.2-1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-1_i686/libgxps-0.3.2-1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-1_x86_64/libgxps-0.3.2-1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-2_armv7l/libgxps-0.3.2-2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-2_armv7l/libgxps-0.3.2-2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-2_i686/libgxps-0.3.2-2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgxps/0.3.2-2_x86_64/libgxps-0.3.2-2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b66e2b7334eec0ddc7061deaeb7d36b48a24bd2b2cb8f9343cacc7788bc7dc3d',
-     armv7l: 'b66e2b7334eec0ddc7061deaeb7d36b48a24bd2b2cb8f9343cacc7788bc7dc3d',
-       i686: '6c1cac1aaa4f0eec41867f335c93b6a5fe39aaa009b318bec14c6c7a8d2592a4',
-     x86_64: '9cbdfadd27b0fad97bbf2c9dd76a0788e738733cc789ca94b74087b1d1fa97df'
+    aarch64: '1461554706deed2492e3b07ce9bca2d2902086e7a540b1a27b757b9168424b52',
+     armv7l: '1461554706deed2492e3b07ce9bca2d2902086e7a540b1a27b757b9168424b52',
+       i686: 'b0b4ea7a91286c6737e693bbd1523a3241cf499287a7553d0354f74ee3beceb9',
+     x86_64: '370e17e06126bda39c3d9ef3e44d1ee57f31bdf42198dd281a257db0afb827d9'
   })
 
   depends_on 'cairo'
@@ -42,6 +42,7 @@ class Libgxps < Package
   depends_on 'openjpeg'
   depends_on 'glibc' # R
   depends_on 'harfbuzz' # R
+  depends_on 'gcc' # R
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
