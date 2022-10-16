6c43fa0e714edc69ec431bed8479270633f4d6c1
supechicken <supechicken666@gmail.com>
2022-10-16 12:50:16 +0800

    jdk: Fetch version from packages

--- packages/jdk.rb
+++ packages/jdk.rb
@@ -11,30 +11,37 @@ class Jdk < Package
 
   def self.preflight
     if ARGV.include?('install')
-      if File.exist?("#{CREW_PREFIX}/bin/java")
-        jdkver_str = `#{CREW_PREFIX}/bin/java -version 2>&1`
-        is_openjdk = jdkver_str.include?('openjdk')
-        jdkver     = jdkver_str[/version "(.+?)"/, 1]
-        majver     = jdkver.split('.')[0]
-        majver     = '8' if majver == '1'
-        pkg_suffix = is_openjdk ? 'openjdk' : 'jdk'
+      jdk_exec = File.join(CREW_PREFIX, 'bin', 'java')
 
-        puts "Package #{pkg_suffix}#{majver} already installed.".lightgreen
-        warn "Run `crew remove #{pkg_suffix}#{majver} && crew install jdk` to install a different version.".yellow
-        return false
+      if File.exist?(jdk_exec)
+        jdk_ver_str   = `#{jdk_exec} -version 2>&1`
+        jdk_ver       = jdk_ver_str[/version "(.+?)"/, 1]
+        jdk_major_ver = jdk_ver.match?(/^1.8/) ? '8' : jdk_ver.partition('.')[0]
+
+        is_openjdk   = jdk_ver_str.include?('openjdk')
+        pkg_branding = is_openjdk ? 'OpenJDK' : 'Oracle JDK'
+        pkg_prefix   = is_openjdk ? 'openjdk' : 'jdk'
+
+        abort <<~EOT.yellow
+
+          #{pkg_branding} #{jdk_ver} installed.
+
+          Run "crew remove #{pkg_prefix}#{jdk_major_ver}; crew install #{name}" to install another version of JDK
+        EOT
       end
 
-      jdk_latest_ver = %w[8 11 17 18].to_h do |jdk_majver|
+      # full version number extracted from jdk*.rb recipes
+      jdk_ver = %i[8 11 17 18].to_h do |jdk_majver|
         pkg = Package.load_package("#{CREW_PACKAGES_PATH}/jdk#{jdk_majver}.rb")
 
         [jdk_majver, pkg.version]
       end
 
       options = [
-        { value: 'jdk8',  description: 'Oracle JDK 8'  },
-        { value: 'jdk11', description: 'Oracle JDK 11' },
-        { value: 'jdk17', description: 'Oracle JDK 17' },
-        { value: 'jdk18', description: 'Oracle JDK 18' }
+        { value: 'jdk8',  description: "Oracle JDK #{jdk_ver[:8]}"  },
+        { value: 'jdk11', description: "Oracle JDK #{jdk_ver[:11]}" },
+        { value: 'jdk17', description: "Oracle JDK #{jdk_ver[:17]}" },
+        { value: 'jdk18', description: "Oracle JDK #{jdk_ver[:18]}" }
       ]
 
       depends_on Selector.new(options).show_prompt
