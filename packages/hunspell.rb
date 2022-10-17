aa99cf2638cc3f0c35210b1b08e19bba25c64a07
Satadru Pramanik <satadru@gmail.com>
2022-10-16 23:25:38 -0400

    fix hunspell

---
+++ packages/hunspell.rb
@@ -0,0 +1,23 @@
+require 'package'
+
+class Hunspell < Package
+  description 'Hunspell is a spell checker and morphological analyzer library'
+  homepage 'https://hunspell.github.io/'
+  version '1.x'
+  license 'MPL-1.1, GPL-2 and LGPL-2.1'
+  compatibility 'all'
+
+  is_fake
+
+  def self.preflight
+    if ARGV.include?('install')
+      options = [
+        { value: 'hunspell_en_us',  description: 'American English' },
+        { value: 'hunspell_fr_fr',  description: 'Français'         },
+        { value: 'hunspell_es_any', description: 'Español'          }
+      ]
+
+      depends_on Selector.new(options).show_prompt
+    end
+  end
+end
