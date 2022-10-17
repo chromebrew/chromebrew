714277a2000d8fb18b43e3a5dba55835be7a3f66
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2022-10-17 12:22:52 +0300

    add perl cpan buildsystem

---
+++ lib/buildsystems/perl_cpan/v1.rb
@@ -0,0 +1,31 @@
+require 'package'
+
+class Perl_cpan_v1 < Package
+  @_buildsystems_perl_cpan_version = '1.0.0'
+
+  def self.check? (bool = true)
+    @check = bool
+    return @check
+  end
+
+  depends_on 'perl'
+
+  def self.prebuild
+    system 'perl', 'Makefile.PL'
+    system "sed -i 's,/usr/local,#{CREW_PREFIX},g' Makefile"
+  end
+
+  def self.build
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+
+  if @check
+    def self.check
+      system 'make', 'test'
+    end
+  end
+end
