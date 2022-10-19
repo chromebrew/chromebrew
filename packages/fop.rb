7f3cb623952a25f9d732520146a6ce0810b865e2
Satadru Pramanik <satadru@gmail.com>
2022-10-19 11:29:12 -0400

    fop: Fix JAVA_HOME (#7515)

--- packages/fop.rb
+++ packages/fop.rb
@@ -3,23 +3,23 @@ require 'package'
 class Fop < Package
   description 'Apache FOP (Formatting Objects Processor) is a print formatter driven by XSL formatting objects (XSL-FO) and an output independent formatter.'
   homepage 'https://xmlgraphics.apache.org/fop/'
-  version '2.7'
+  version '2.7-1'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'https://downloads.apache.org/xmlgraphics/fop/binaries/fop-2.7-bin.tar.gz'
   source_sha256 'ec75d6135f55f57b275f8332e069f8817990fdc7f63b1f5c0cb9da5609aa3074'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7_armv7l/fop-2.7-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7_armv7l/fop-2.7-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7_i686/fop-2.7-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7_x86_64/fop-2.7-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7-1_armv7l/fop-2.7-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7-1_armv7l/fop-2.7-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7-1_i686/fop-2.7-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fop/2.7-1_x86_64/fop-2.7-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '3b9f80cc072b9f23a54a06f8f83ded64d1c1a4c79ad1f887c79b0ecb5b25da74',
-     armv7l: '3b9f80cc072b9f23a54a06f8f83ded64d1c1a4c79ad1f887c79b0ecb5b25da74',
-       i686: '525ad8aea3c8c5512a85fb82c01a9569a3bbc858a9b1ca0369d9c430c814ccb4',
-     x86_64: '5e36714fdc3db30e7f9df579d47a19bdff8da481ac2665bf4449458f34344629'
+    aarch64: '592a35d04bcca3ab8762e8ba03ce7f57c10a93f03efecad58f3f9304c3eaa79a',
+     armv7l: '592a35d04bcca3ab8762e8ba03ce7f57c10a93f03efecad58f3f9304c3eaa79a',
+       i686: 'c6c96e9a3b66a448dc1c30b8cbe64022e2d6cb6a078f879d824ffe143839e56b',
+     x86_64: '537463270af9f987c2604b98f46e17bb4b1249a7203e055531822574c7562450'
   })
 
   depends_on 'openjdk8'
@@ -39,7 +39,7 @@ class Fop < Package
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
     @fopenv = <<~FOPEOF
       # Fop configuration
-      export JAVA_HOME=#{CREW_LIB_PREFIX}/jdk8
+      export JAVA_HOME=#{CREW_PREFIX}/jre
     FOPEOF
     File.write("#{CREW_DEST_PREFIX}/etc/env.d/fop", @fopenv)
   end
