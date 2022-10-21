16db19e742132ef1f533d21cd5989c42d94715ea
Satadru Pramanik <satadru@gmail.com>
2022-10-21 14:28:27 -0400

    update gemacs

--- packages/gemacs.rb
+++ packages/gemacs.rb
@@ -10,46 +10,41 @@ class Gemacs < Package
   source_sha256 'ee21182233ef3232dc97b486af2d86e14042dbb65bbc535df562c3a858232488'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.0.50.1-01b0_armv7l/gemacs-28.0.50.1-01b0-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.0.50.1-01b0_armv7l/gemacs-28.0.50.1-01b0-chromeos-armv7l.tpxz',
        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.2_i686/gemacs-28.2-chromeos-i686.tar.zst',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.2_x86_64/gemacs-28.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '8ba0433113a23361cf961d372c8e7db79206c5baf3046255bebec0acd1d73452',
-     armv7l: '8ba0433113a23361cf961d372c8e7db79206c5baf3046255bebec0acd1d73452',
        i686: 'd3c1c2133ca735512de8b77069093f78110a41377962080a69271d5052d18a46',
      x86_64: 'ed1a8eacde7a8a17be9fb11a2bcdf83d3ce03ba3d78ebbb71884f808f515debd'
   })
 
-  depends_on 'alsa_lib'
-  depends_on 'cairo'
-  depends_on 'freetype' => :build
-  depends_on 'giflib'
-  depends_on 'gpm'
-  depends_on 'gtk3'
-  depends_on 'harfbuzz'
-  depends_on 'jansson'
-  depends_on 'libjpeg'
-  depends_on 'libotf'
-  depends_on 'libpng'
-  depends_on 'librsvg'
-  depends_on 'libtiff'
-  depends_on 'texinfo'
-  depends_on 'webkit2gtk'
   depends_on 'acl' # R
+  depends_on 'alsa_lib' # R
   depends_on 'atk' # R
+  depends_on 'at_spi2_core' # R
+  depends_on 'cairo'
   depends_on 'dbus' # R
+  depends_on 'freetype' # R
   depends_on 'gcc' # R
   depends_on 'gdk_pixbuf' # R
-  depends_on 'glib' # R
+  depends_on 'giflib' # R
   depends_on 'glibc' # R
+  depends_on 'glib' # R
   depends_on 'gmp' # R
   depends_on 'gnutls' # R
+  depends_on 'gpm' # R
+  depends_on 'gtk3' # R
+  depends_on 'harfbuzz' # R
+  depends_on 'jansson' # R
   depends_on 'lcms' # R
   depends_on 'libice' # R
+  depends_on 'libjpeg' # R
+  depends_on 'libotf' # R
+  depends_on 'libpng' # R
+  depends_on 'librsvg' # R
   depends_on 'libsm' # R
   depends_on 'libsoup2' # R
+  depends_on 'libtiff' # R
   depends_on 'libx11' # R
   depends_on 'libxcb' # R
   depends_on 'libxcomposite' # R
@@ -62,7 +57,8 @@ class Gemacs < Package
   depends_on 'libxrender' # R
   depends_on 'ncurses' # R
   depends_on 'pango' # R
-  depends_on 'webkit2gtk_4' # R
+  depends_on 'texinfo'
+  depends_on 'webkit2gtk'
   depends_on 'zlibpkg' # R
 
   def self.build
