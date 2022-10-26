9af884956354a29792387012906742ffba28a70e
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2022-10-26 20:20:06 +0300

    libgit2 => 1.5.0

--- packages/libgit2.rb
+++ packages/libgit2.rb
@@ -3,37 +3,26 @@ require 'package'
 class Libgit2 < Package
   description 'A portable, pure C implementation of the Git core methods'
   homepage 'https://libgit2.org/'
-  @_ver = '1.4.2'
+  @_ver = '1.5.0'
   version @_ver
   license 'GPL-2-with-linking-exception'
   compatibility 'all'
   source_url 'https://github.com/libgit2/libgit2.git'
   git_hashtag "v#{@_ver}"
 
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgit2/1.4.2_armv7l/libgit2-1.4.2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgit2/1.4.2_armv7l/libgit2-1.4.2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgit2/1.4.2_i686/libgit2-1.4.2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libgit2/1.4.2_x86_64/libgit2-1.4.2-chromeos-x86_64.tar.zst'
-  })
-  binary_sha256({
-    aarch64: '8f893a29c3c3cf66a868fe6a02fe60f20c854d1b1b0dc5585550df52947679fb',
-     armv7l: '8f893a29c3c3cf66a868fe6a02fe60f20c854d1b1b0dc5585550df52947679fb',
-       i686: '328bb033fc1eaf16bac59a5b84264c7d138bc2d40750c6af78419e1c59ef286f',
-     x86_64: 'f9db9f7f7cf14110f58447f7fada36b6f6b137a5b11fcf1eadc9218fc2b89e7e'
-  })
-
-  depends_on 'python3'
-  depends_on 'libssh2'
+  depends_on 'glibc' # R
+  depends_on 'libssh2' # R
+  depends_on 'openssl' # R
+  depends_on 'pcre' # R
+  depends_on 'python3' # L
+  depends_on 'zlibpkg' # R
 
   def self.build
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "cmake -G Ninja #{CREW_CMAKE_OPTIONS} \
+    system "cmake -B builddir -G Ninja #{CREW_CMAKE_OPTIONS} \
               -DUSE_SSH=ON \
-              -Wno-dev .."
-      system 'samu'
-    end
+              -DUSE_BUNDLED_ZLIB=OFF \
+              -Wno-dev"
+    system 'samu -C builddir'
   end
 
   def self.install
@@ -42,6 +31,6 @@ class Libgit2 < Package
 
   def self.check
     # Tests #3 and #8 fail in containers
-    # system 'samu -C builddir test'
+    system 'samu -C builddir test || true'
   end
 end
