157c61aa8c87c1b1322f0afbf861af9d94f99dcb
saltedcoffii <55339220+saltedcoffii@users.noreply.github.com>
2022-10-26 22:48:31 +0300

    libdeflate => 1.14

--- packages/libdeflate.rb
+++ packages/libdeflate.rb
@@ -3,28 +3,26 @@ require 'package'
 class Libdeflate < Package
   description 'Heavily optimized library for DEFLATE compression and decompression'
   homepage 'https://github.com/ebiggers/libdeflate/'
-  version '1.7-1'
+  @_ver = '1.14'
+  version @_ver
   compatibility 'all'
   license 'MIT'
-  source_url 'https://github.com/ebiggers/libdeflate/archive/v1.7.tar.gz'
-  source_sha256 'a5e6a0a9ab69f40f0f59332106532ca76918977a974e7004977a9498e3f11350'
+  source_url 'https://github.com/ebiggers/libdeflate.git'
+  git_hashtag "v#{@_ver}"
 
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdeflate/1.7-1_armv7l/libdeflate-1.7-1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdeflate/1.7-1_armv7l/libdeflate-1.7-1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdeflate/1.7-1_i686/libdeflate-1.7-1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdeflate/1.7-1_x86_64/libdeflate-1.7-1-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    aarch64: '1195e3098e35caf102d85f6e094c0e4e13d0f4065bf43447168b5c3f53e59130',
-     armv7l: '1195e3098e35caf102d85f6e094c0e4e13d0f4065bf43447168b5c3f53e59130',
-       i686: 'bbbd9b82d8e241e6ac6db6d50ef217e4fcb8d7d029a6fe2bab514401d87f809c',
-     x86_64: 'cf08842579addb9c9abe37b6bff1ff8071401c2f37c5e8278a323f40878c71e0'
-  })
+  depends_on 'glibc' # R
+
+  # The next release of libdeflate will use cmake
 
   def self.build
-    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto'
-      LDFLAGS='-flto=auto' \
+    system "env #{CREW_ENV_OPTIONS} \
+      PREFIX=#{CREW_PREFIX} \
+      LIBDIR=#{CREW_LIB_PREFIX} \
+      make"
+  end
+  
+  def self.check
+    system "env #{CREW_ENV_OPTIONS} \
       PREFIX=#{CREW_PREFIX} \
       LIBDIR=#{CREW_LIB_PREFIX} \
       make"
