d5299cdc9e354848d7b7c49b7ec7585dfefcc045
Satadru Pramanik <satadru@gmail.com>
2022-10-27 13:21:57 -0400

    add postinstall to gnome_console

--- packages/gnome_console.rb
+++ packages/gnome_console.rb
@@ -54,4 +54,8 @@ class Gnome_console < Package
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
   end
+
+  def self.postinstall
+    system "glib-compile-schemas #{CREW_PREFIX}/share/glib-2.0/schemas"
+  end
 end
