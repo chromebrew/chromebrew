45e75ba163f70c294de29752a613b7969ce7dd16
Satadru Pramanik <satadru@gmail.com>
2022-11-01 18:10:38 -0400

    Some updates

--- packages/py3_libxml2.rb
+++ packages/py3_libxml2.rb
@@ -3,28 +3,31 @@ require 'package'
 class Py3_libxml2 < Package
   description 'Libxml2-python provides access to libxml2 and libxslt in Python.'
   homepage 'https://gitlab.gnome.org/GNOME/libxml2/'
-  version '2.10.2'
+  @_ver = '2.10.3'
+  version "#{@_ver}-py3.10"
   license 'MIT'
   compatibility 'all'
-  source_url 'https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.10.2/libxml2-v2.10.2.tar.bz2'
-  source_sha256 'd50e8a55b2797501929d3411b81d5d37ec44e9a4aa58eae9052572977c632d7a'
+  source_url 'https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.10.3/libxml2-v2.10.3.tar.bz2'
+  source_sha256 '302bbb86400b8505bebfbf7b3d1986e9aa05073198979f258eed4be481ff5f83'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.2_armv7l/py3_libxml2-2.10.2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.2_armv7l/py3_libxml2-2.10.2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.2_i686/py3_libxml2-2.10.2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.2_x86_64/py3_libxml2-2.10.2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_armv7l/py3_libxml2-2.10.3-py3.10-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_armv7l/py3_libxml2-2.10.3-py3.10-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_i686/py3_libxml2-2.10.3-py3.10-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_x86_64/py3_libxml2-2.10.3-py3.10-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '198aec6c86c106f45a2d945032ec232a5650a7a5f2630da11ad9d11eb40a7592',
-     armv7l: '198aec6c86c106f45a2d945032ec232a5650a7a5f2630da11ad9d11eb40a7592',
-       i686: '16f08ba727a13268c8a0a4516a9c81f1bcc9291a81d8a4c1e5cdeb2aa6473a98',
-     x86_64: 'd5578c3e0aac30896b6f198e9b93988e055b5deca57dbaed7c9fda12b60aaa80'
+    aarch64: '78aa7d4890fe6890dfa4d61d285a193ba57d4856db0bd31037c8f0ec563debcd',
+     armv7l: '78aa7d4890fe6890dfa4d61d285a193ba57d4856db0bd31037c8f0ec563debcd',
+       i686: '58b5d28a139656758a015d580943a8a6a049e31526d21e09e57aee8358100ee3',
+     x86_64: 'e66f9d4928223c33cbd1ae060f39da55b87ff1a4cb4ded49272650b97481c4cd'
   })
 
   depends_on 'libxml2'
   depends_on 'libxslt'
   depends_on 'py3_setuptools' => :build
+  depends_on 'glibc' # R
+  depends_on 'zlibpkg' # R
 
   def self.build
     system 'autoreconf -fiv'
--- packages/yelp_tools.rb
+++ packages/yelp_tools.rb
@@ -3,28 +3,32 @@ require 'package'
 class Yelp_tools < Package
   description 'yelp-tools is a collection of scripts and build utilities to help create, manage, and publish documentation for Yelp and the web'
   homepage 'https://github.com/GNOME/yelp-tools'
-  version '42.0'
+  version '42.1'
   license 'GPL-2+ or freedist and GPL-2+'
   compatibility 'all'
   source_url "https://gitlab.gnome.org/GNOME/yelp-tools/-/archive/#{version}/yelp-tools-#{version}.tar.bz2"
-  source_sha256 'c71a7b10adb91712887c96541ce6eedf7c6f68d0cd1fa67bce0b27c46c3b2b05'
+  source_sha256 '40aadae0c6f0af41f90bcaba532d20b5b401476d64b2650aa419ef6264c00ecd'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_armv7l/yelp_tools-42.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_armv7l/yelp_tools-42.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_i686/yelp_tools-42.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.0_x86_64/yelp_tools-42.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.1_armv7l/yelp_tools-42.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.1_armv7l/yelp_tools-42.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.1_i686/yelp_tools-42.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/yelp_tools/42.1_x86_64/yelp_tools-42.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'bdf45b19f8c95e000cb32ca96f0b17946177487a1294f5812cb200e3a4d94a58',
-     armv7l: 'bdf45b19f8c95e000cb32ca96f0b17946177487a1294f5812cb200e3a4d94a58',
-       i686: '8431a64a6b7790d78fad98baf9cbddcf129cebe130f337a111f62310b147a7da',
-     x86_64: 'e513576b06f6d6d2a027e120e3dcfb3774fd9222e47d69a4749f0ed3db67b9f8'
+    aarch64: '70792bf80c6bb3f66c959acdad595a2ff08fef8de990e41b700f74d0ae6a931e',
+     armv7l: '70792bf80c6bb3f66c959acdad595a2ff08fef8de990e41b700f74d0ae6a931e',
+       i686: 'eee77db471affeb7110d6c4cff18a1d4ea624d58803420a5654a4f3442fa2d07',
+     x86_64: 'dec4d9bac63673a7fa572d7006d2f139caa4d10da8c1b965e36719135755a99d'
   })
 
   depends_on 'yelp_xsl'
   depends_on 'libxslt'
   depends_on 'py3_lxml'
+
+  def self.patch
+    system "sed -i 's,/usr/bin/python3,#{CREW_PREFIX}/bin/python3,g' tools/*.in"
+  end
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
