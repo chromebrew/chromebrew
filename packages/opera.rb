1856220b4089a074930a34a3d0527b464adb0ebd
Ed Reel <edreel@gmail.com>
2022-11-03 12:37:12 -0500

    Opera 92.0.4561.33 => 92.0.4561.43

--- packages/opera.rb
+++ packages/opera.rb
@@ -3,14 +3,14 @@ require 'package'
 class Opera < Package
   description 'Opera is a multi-platform web browser based on Chromium and developed by Opera Software.'
   homepage 'https://www.opera.com/'
-  version '92.0.4561.33'
+  version '92.0.4561.43'
   license 'OPERA-2018'
   compatibility 'x86_64'
 
   # faster apt mirror, but only works when downloading latest version of opera
   # source_url "https://deb.opera.com/opera/pool/non-free/o/opera-stable/opera-stable_#{version}_amd64.deb"
   source_url "https://get.opera.com/pub/opera/desktop/#{version}/linux/opera-stable_#{version}_amd64.deb"
-  source_sha256 '3dc81278f8cd679849c74c7af7416d7373b78bfd5ac5075dd4f7b2db304c703f'
+  source_sha256 '9320e4acb67ba3b2f7d4e9acfa24cbc1ae722a3991974f5df808425484a0822f'
 
   depends_on 'gtk3'
   depends_on 'gsettings_desktop_schemas'
