0aef2e48ecfae31e44c7358524959bb22dbbcf5a
Satadru Pramanik <satadru@gmail.com>
2022-11-03 13:56:50 -0400

    Webkit2gtk{_4,_4_1} Builds added for armv7l, i686 (#7584)
    * add armv7l binaries for webkit2gtk_4
    
    * add armv7l binaries for webkit2gtk_4_1
    
    * add gemacs builds
    
    * add armv7l build
    
    * adjust compatibility
    
    * suggested changes
    
    * suggested changes

--- packages/gemacs.rb
+++ packages/gemacs.rb
@@ -5,15 +5,19 @@ class Gemacs < Package
   homepage 'https://www.gnu.org/software/emacs/'
   version '28.2'
   license 'GPL-3+, FDL-1.3+, BSD, HPND, MIT, W3C, unicode, PSF-2'
-  compatibility 'x86_64 i686'
+  compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/emacs/emacs-28.2.tar.xz'
   source_sha256 'ee21182233ef3232dc97b486af2d86e14042dbb65bbc535df562c3a858232488'
 
   binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.2_armv7l/gemacs-28.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.2_armv7l/gemacs-28.2-chromeos-armv7l.tar.zst',
        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.2_i686/gemacs-28.2-chromeos-i686.tar.zst',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gemacs/28.2_x86_64/gemacs-28.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
+    aarch64: 'ee40e23f622347d65983dc172acf7727238f963e3ac12ad7456c5409dc6bf323',
+     armv7l: 'ee40e23f622347d65983dc172acf7727238f963e3ac12ad7456c5409dc6bf323',
        i686: 'd3c1c2133ca735512de8b77069093f78110a41377962080a69271d5052d18a46',
      x86_64: 'ed1a8eacde7a8a17be9fb11a2bcdf83d3ce03ba3d78ebbb71884f808f515debd'
   })
@@ -58,7 +62,7 @@ class Gemacs < Package
   depends_on 'ncurses' # R
   depends_on 'pango' # R
   depends_on 'texinfo'
-  depends_on 'webkit2gtk'
+  depends_on 'webkit2gtk_4'
   depends_on 'zlibpkg' # R
 
   def self.build
