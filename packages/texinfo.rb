6300dd47af35bbf688c159aadc71702c5bad4b5b
Satadru Pramanik <satadru@gmail.com>
2022-11-08 13:24:46 -0500

    texinfo -> 7.0

--- packages/texinfo.rb
+++ packages/texinfo.rb
@@ -3,23 +3,23 @@ require 'package'
 class Texinfo < Package
   description 'Texinfo is the official documentation format of the GNU project.'
   homepage 'https://www.gnu.org/software/texinfo/'
-  version '6.8-1'
+  version '7.0'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/texinfo/texinfo-6.8.tar.xz'
-  source_sha256 '8eb753ed28bca21f8f56c1a180362aed789229bd62fff58bf8368e9beb59fec4'
+  source_url 'http://ftpmirror.gnu.org/texinfo/texinfo-7.0.tar.xz'
+  source_sha256 '20744b82531ce7a04d8cee34b07143ad59777612c3695d5855f29fba40fbe3e0'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/6.8-1_armv7l/texinfo-6.8-1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/6.8-1_armv7l/texinfo-6.8-1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/6.8-1_i686/texinfo-6.8-1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/6.8-1_x86_64/texinfo-6.8-1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0_armv7l/texinfo-7.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0_armv7l/texinfo-7.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0_i686/texinfo-7.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0_x86_64/texinfo-7.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b74885ef4051db9e1706e77f7ec58747ad1f4de3901c08f28831b07fd7f0f6b4',
-     armv7l: 'b74885ef4051db9e1706e77f7ec58747ad1f4de3901c08f28831b07fd7f0f6b4',
-       i686: '1a199141340ffa01dcb0287e590ad5844f70ce1dc9b5df9459df486aec420ce0',
-     x86_64: '6a6c3c50dfbd0fbc32552308d45ba48cf44e55fd7c29563aabbaefa9b3726bfe'
+    aarch64: 'b2a728d4411d0f6413b4799db055b2a86d34de21b4a3fab0b63816a75e5a1c0f',
+     armv7l: 'b2a728d4411d0f6413b4799db055b2a86d34de21b4a3fab0b63816a75e5a1c0f',
+       i686: '5d2bc0b29e4066dfb002a618089cefe8c438635e45e9cce523fd3da6691c18a8',
+     x86_64: '2c5c0166ad926b0fbafed07ae06b44635bba753fdc569108d6f07dbfb314a7ed'
   })
 
   depends_on 'glibc' # R
@@ -27,6 +27,7 @@ class Texinfo < Package
   depends_on 'perl_locale_messages'
   depends_on 'perl_text_unidecode'
   depends_on 'perl_unicode_eastasianwidth'
+  depends_on 'ncurses' # R
 
   def self.build
     # configure and make
