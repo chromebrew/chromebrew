77b53d4698e406fef2661dba3ffc964331109976
Satadru Pramanik <satadru@gmail.com>
2022-11-10 12:35:50 -0500

    libdrm -> 2.4.114

--- packages/libdrm.rb
+++ packages/libdrm.rb
@@ -3,7 +3,7 @@ require 'package'
 class Libdrm < Package
   description 'Cross-driver middleware for DRI protocol.'
   homepage 'https://dri.freedesktop.org'
-  @_ver = '2.4.113'
+  @_ver = '2.4.114'
   version @_ver
   license 'MIT'
   compatibility 'all'
@@ -11,23 +11,24 @@ class Libdrm < Package
   git_hashtag "libdrm-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.113_armv7l/libdrm-2.4.113-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.113_armv7l/libdrm-2.4.113-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.113_i686/libdrm-2.4.113-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.113_x86_64/libdrm-2.4.113-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.114_armv7l/libdrm-2.4.114-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.114_armv7l/libdrm-2.4.114-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.114_i686/libdrm-2.4.114-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libdrm/2.4.114_x86_64/libdrm-2.4.114-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'd89807cb6e368b1bb03f8351a3c699463fe81cb0533fc2d54ec0d917f41974c8',
-     armv7l: 'd89807cb6e368b1bb03f8351a3c699463fe81cb0533fc2d54ec0d917f41974c8',
-       i686: '41e96c881ac6e9dd40b500095d1e8bc060477cb5c6b2662661ae5d4797fc78cc',
-     x86_64: '950a8cbf4110cc3b9e10127aa09be07e8d9828e115d6eb4ec18ec23d81818f77'
+    aarch64: '22204bf614205ee547091bea152cfa7a94f623ba87bf9e2b549b5e4177fd9d8c',
+     armv7l: '22204bf614205ee547091bea152cfa7a94f623ba87bf9e2b549b5e4177fd9d8c',
+       i686: '154326bf8f1d4d42c87550580a8295f490807e463ef74ff9f56d36a64efc358e',
+     x86_64: 'd22e18ca912b0ec5ce698a03eec86f45ded30c55a04265c975468328ef3204ef'
   })
 
+  depends_on 'cairo' # R
   depends_on 'libpciaccess' # R
-  depends_on 'xorg_lib' => :build
   depends_on 'eudev' => :build
   depends_on 'libxslt' => :build
   depends_on 'glibc' # R
+  depends_on 'gcc' # R
 
   def self.build
     system "meson #{CREW_MESON_OPTIONS} \
