e386e87c6e9e92e9b707d191a032b7ffb3881c9d
Satadru Pramanik <satadru@gmail.com>
2022-11-14 18:48:52 -0500

    cleanup libglu

--- packages/libglu.rb
+++ packages/libglu.rb
@@ -10,21 +10,20 @@ class Libglu < Package
   git_hashtag "glu-#{version}"
 
   binary_url({
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_i686/libglu-9.0.2-chromeos-i686.tar.zst',
- aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_armv7l/libglu-9.0.2-chromeos-armv7l.tar.zst',
-  armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_armv7l/libglu-9.0.2-chromeos-armv7l.tar.zst',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_x86_64/libglu-9.0.2-chromeos-x86_64.tar.zst'
+      i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_i686/libglu-9.0.2-chromeos-i686.tar.zst',
+   aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_armv7l/libglu-9.0.2-chromeos-armv7l.tar.zst',
+    armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_armv7l/libglu-9.0.2-chromeos-armv7l.tar.zst',
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libglu/9.0.2_x86_64/libglu-9.0.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    i686: '1019df9d060a8deffe29d83c5f7bc3ae36aad7ce5aa0922ef992630cb5a54b9c',
- aarch64: 'ac1d9cc34fd08c5fcff392ba91b28e58e99bf86b9d1575be073404516d297898',
-  armv7l: 'ac1d9cc34fd08c5fcff392ba91b28e58e99bf86b9d1575be073404516d297898',
-  x86_64: '55358e0044362fd6a9a4c1abf11f6d5757cd8cc757d96787da850e3b107ef4d1'
+      i686: '1019df9d060a8deffe29d83c5f7bc3ae36aad7ce5aa0922ef992630cb5a54b9c',
+   aarch64: 'ac1d9cc34fd08c5fcff392ba91b28e58e99bf86b9d1575be073404516d297898',
+    armv7l: 'ac1d9cc34fd08c5fcff392ba91b28e58e99bf86b9d1575be073404516d297898',
+    x86_64: '55358e0044362fd6a9a4c1abf11f6d5757cd8cc757d96787da850e3b107ef4d1'
   })
 
   depends_on 'gcc' # R
   depends_on 'glibc' # R
-  depends_on 'libglvnd' => :build
   depends_on 'mesa' # R
   depends_on 'libglvnd' # R
 
