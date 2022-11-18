00ac7578029955d83c9fc2b1e09cd070c09122e5
Satadru Pramanik <satadru@gmail.com>
2022-11-17 23:19:15 -0500

    less -> 608

--- packages/less.rb
+++ packages/less.rb
@@ -3,27 +3,28 @@ require 'package'
 class Less < Package
   description 'GNU less is a program similar to more, but which allows backward movement in the file as well as forward movement.'
   homepage 'https://www.gnu.org/software/less/'
-  version '590'
+  version '608'
   license 'GPL-3 or BSD-2'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gnu/less/less-590.tar.gz'
-  source_sha256 '6aadf54be8bf57d0e2999a3c5d67b1de63808bb90deb8f77b028eafae3a08e10'
+  source_url 'https://ftpmirror.gnu.org/gnu/less/less-608.tar.gz'
+  source_sha256 'a69abe2e0a126777e021d3b73aa3222e1b261f10e64624d41ec079685a6ac209'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/590_armv7l/less-590-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/590_armv7l/less-590-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/590_i686/less-590-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/590_x86_64/less-590-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/608_armv7l/less-608-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/608_armv7l/less-608-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/608_i686/less-608-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/less/608_x86_64/less-608-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '56244b1b648ddee1801596a1a6b07da06d1749cc1c222c9fb35e0c660764817e',
-     armv7l: '56244b1b648ddee1801596a1a6b07da06d1749cc1c222c9fb35e0c660764817e',
-       i686: '7e471501cba155fdff2734e56c631bb4c3d3d8c375533fda34876daf931d6766',
-     x86_64: 'fe15b66e0ad8e0be7c3695ad601019ac5abd610e8680ab5b07b7f289510b87c2'
+    aarch64: '5a945772c06719f52ac9d8435a64611d53c29714e7f9c34fc342c38c1912afda',
+     armv7l: '5a945772c06719f52ac9d8435a64611d53c29714e7f9c34fc342c38c1912afda',
+       i686: 'ed9e9b0d5edd476252dc909aa9753e81d0aa4f8167d624ac6d1d6956d18d75ea',
+     x86_64: 'd19353c6ea34e7da8554eab1ec322b655bc581232bc48ecff28fb852420265b1'
   })
 
   depends_on 'glibc' # R
   depends_on 'ncurses' # R
+  depends_on 'gcc' # R
 
   def self.build
     system "./configure #{CREW_OPTIONS} --with-regex=posix"
