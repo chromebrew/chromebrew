41b4c3a2ac66099cdc2415d2d66f811017e5206c
Satadru Pramanik <satadru@gmail.com>
2022-11-22 20:01:58 -0500

    more binaries

--- packages/qtmultimedia.rb
+++ packages/qtmultimedia.rb
@@ -3,40 +3,43 @@ require 'package'
 class Qtmultimedia < Package
   description 'Qt Multimedia'
   homepage 'https://www.qt.io/'
-  version '5.15.2'
+  version '5.15.7-f3dc189'
   license 'FDL, GPL-2, GPL-3, GPL-3-with-qt-exception, LGPL-2.1 and LGPL-3'
   compatibility 'all'
-  source_url 'https://download.qt.io/official_releases/qt/5.15/5.15.2/submodules/qtmultimedia-everywhere-src-5.15.2.tar.xz'
-  source_sha256 '0c3758810e5131aabcf76e4965e4c18b8911af54d9edd9305d2a8278d8346df5'
+  source_url 'https://invent.kde.org/qt/qt/qtmultimedia.git'
+  git_hashtag 'f3dc18968c437c993886d3bfb668b62344a10860'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.2_armv7l/qtmultimedia-5.15.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.2_armv7l/qtmultimedia-5.15.2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.2_i686/qtmultimedia-5.15.2-chromeos-i686.tar.xz',
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.2_x86_64/qtmultimedia-5.15.2-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.7-f3dc189_armv7l/qtmultimedia-5.15.7-f3dc189-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.7-f3dc189_armv7l/qtmultimedia-5.15.7-f3dc189-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.7-f3dc189_i686/qtmultimedia-5.15.7-f3dc189-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtmultimedia/5.15.7-f3dc189_x86_64/qtmultimedia-5.15.7-f3dc189-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '01549aaf37a1c85ced9a7a8f796c9d085cf3a7f4f6197abf1af5afad24a72dfc',
-     armv7l: '01549aaf37a1c85ced9a7a8f796c9d085cf3a7f4f6197abf1af5afad24a72dfc',
-       i686: '47def7e00a23da3a6c138232a2a790a2dac7cbab0041dc57cae4dce06995f5da',
-    x86_64: '0dbcb8550581fe3677df9ba03bed052a7dc1e513ddd83c7c40878684211b3379'
+    aarch64: 'dd009ac11c9191d766972aad5263bc6d775e1e3fd57eeff0603acba7542250c1',
+     armv7l: 'dd009ac11c9191d766972aad5263bc6d775e1e3fd57eeff0603acba7542250c1',
+       i686: 'e6d551db835d7e9d9e897995f4fe03c58c9ec78cd2f4bbed9933048df7d1d188',
+     x86_64: 'cf26c441370cb378f2f19ab01dfd58eea25b63152f1da17ecd305f1e764171a7'
   })
 
-  depends_on 'qtbase' => :build
+  depends_on 'qtbase' # R
+  depends_on 'qtdeclarative' # R
+  depends_on 'gstreamer' => :build
+  depends_on 'alsa_lib' # R
+  depends_on 'gcc' # R
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
+  depends_on 'gstreamer' # R
+  depends_on 'libglvnd' # R
+  depends_on 'openal' # R
+  depends_on 'pulseaudio' # R
 
   def self.build
-    system 'qmake && make'
+    system 'qmake'
+    system 'make'
   end
 
   def self.install
-    ENV['CREW_CONFLICTS_ONLY_ADVISORY'] = '1'
-    reload_constants
-    FileUtils.mkdir_p CREW_DEST_LIB_PREFIX
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/Qt-5/plugins/mediaservice"
-    FileUtils.cp_r Dir['lib/*'], CREW_DEST_LIB_PREFIX
-    FileUtils.cp_r 'include', "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.cp_r 'plugins', "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.cp_r 'src', "#{CREW_DEST_PREFIX}/share/Qt-5"
+    system "make INSTALL_ROOT=#{CREW_DEST_DIR} install"
   end
 end
--- packages/qttools.rb
+++ packages/qttools.rb
@@ -3,49 +3,40 @@ require 'package'
 class Qttools < Package
   description 'Qt Tools'
   homepage 'https://github.com/qt/qttools'
-  version '5.15.2'
+  version '5.15.7-c4750dd'
   license 'FDL, GPL-2, GPL-3, GPL-3-with-qt-exception and LGPL-3'
   compatibility 'all'
-  source_url 'https://download.qt.io/official_releases/qt/5.15/5.15.2/submodules/qttools-everywhere-src-5.15.2.tar.xz'
-  source_sha256 'c189d0ce1ff7c739db9a3ace52ac3e24cb8fd6dbf234e49f075249b38f43c1cc'
+  source_url 'https://invent.kde.org/qt/qt/qttools.git'
+  git_hashtag 'c4750dd02070ce246ff98cc5d137193825043912'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.2_armv7l/qttools-5.15.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.2_armv7l/qttools-5.15.2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.2_i686/qttools-5.15.2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.2_x86_64/qttools-5.15.2-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.7-c4750dd_armv7l/qttools-5.15.7-c4750dd-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.7-c4750dd_armv7l/qttools-5.15.7-c4750dd-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.7-c4750dd_i686/qttools-5.15.7-c4750dd-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qttools/5.15.7-c4750dd_x86_64/qttools-5.15.7-c4750dd-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '0d40cb342b8b31427bc976979dbc4c5bfcd8995e5bcc1e74a2a91cf4d84c2265',
-     armv7l: '0d40cb342b8b31427bc976979dbc4c5bfcd8995e5bcc1e74a2a91cf4d84c2265',
-       i686: '1f94a4b40c8f94d47b13445fd9dfb7abb50694cf0876d83e71287f59f36caeca',
-     x86_64: '4fe6c1fcd3b4bdfbdface915f9c7d71b6c73deadbb5bb20cb5d89af8d6d9d2d2'
+    aarch64: '2aec1354a1b2bf672a0c91c5e06e80378a77faccd3f275b1039a5857aa9462ec',
+     armv7l: '2aec1354a1b2bf672a0c91c5e06e80378a77faccd3f275b1039a5857aa9462ec',
+       i686: 'f601314a281f7f64bd23f5603f2702cad362bfdd32f7d969415bc1b2107fc899',
+     x86_64: '0d7b2e4341b4cceca8ab22710f89e933c5bf35d784a030d67e314b4d8879529e'
   })
 
   depends_on 'gcc' # R
   depends_on 'glibc' # R
   depends_on 'mesa' # R
-  depends_on 'qtbase'
+  depends_on 'qtbase' # R
   depends_on 'libtinfo'
+  depends_on 'libglvnd' # R
+  depends_on 'llvm' # R
+  depends_on 'qtdeclarative' # R
 
   def self.build
-    # NOTE: This MAY NOT COMPILE with llvm installed on x86. Install dependencies,
-    # disable depends, and uninstall llvm before attempting compile.
-    system 'qmake -Wall -early QMAKE_CFLAGS=-flto -early QMAKE_CXXFLAGS=-flto && make'
+    system 'qmake'
+    system 'make'
   end
 
   def self.install
-    FileUtils.mkdir_p CREW_DEST_LIB_PREFIX.to_s
-    system "cp -a lib/* #{CREW_DEST_LIB_PREFIX}"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.cp_r 'bin', "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.cp_r 'examples', "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.cp_r 'include', "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.cp_r 'mkspecs', "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.cp_r 'src/linguist/phrasebooks', "#{CREW_DEST_PREFIX}/share/Qt-5"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    Dir.chdir "#{CREW_DEST_PREFIX}/share/Qt-5/bin" do
-      system "find . -type f -exec ln -s #{CREW_PREFIX}/share/Qt-5/bin/{} #{CREW_DEST_PREFIX}/bin/{} \\;"
-    end
+    system "make INSTALL_ROOT=#{CREW_DEST_DIR} install"
   end
 end
