9ca1fe667d34422fed8ecd35ac5dbbaef6d756d2
Satadru Pramanik <satadru@gmail.com>
2022-11-29 22:06:45 -0500

    waypipe => 0.8.4

--- packages/waypipe.rb
+++ packages/waypipe.rb
@@ -3,23 +3,23 @@ require 'package'
 class Waypipe < Package
   description 'A proxy for Wayland protocol applications. WARNING: different versions are incompatible'
   homepage 'https://gitlab.freedesktop.org/mstoeckl/waypipe'
-  version '0.8.0'
+  version '0.8.4'
   license 'MIT'
   compatibility 'all'
   source_url 'https://gitlab.freedesktop.org/mstoeckl/waypipe.git'
   git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.0_armv7l/waypipe-0.8.0-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.0_armv7l/waypipe-0.8.0-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.0_i686/waypipe-0.8.0-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.0_x86_64/waypipe-0.8.0-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.4_armv7l/waypipe-0.8.4-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.4_armv7l/waypipe-0.8.4-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.4_i686/waypipe-0.8.4-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/waypipe/0.8.4_x86_64/waypipe-0.8.4-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '7ed1843341ca708c08c6ec854ece411d0dc4a2e37af472e8c11a75651c5beecc',
-     armv7l: '7ed1843341ca708c08c6ec854ece411d0dc4a2e37af472e8c11a75651c5beecc',
-       i686: '0c994fb5538080d055b0116fa26b65538e9b4f237b7be268b877df1c006ae408',
-     x86_64: '96d33e96782fe12fb707271c66de01d3e5948a7c9d9fc59963bae8f33f19bf9f'
+    aarch64: 'e188ef809cb986bd1550de0a434c92df6458b360e13b423864704de05114c7fa',
+     armv7l: 'e188ef809cb986bd1550de0a434c92df6458b360e13b423864704de05114c7fa',
+       i686: 'db973f5765ec2b971f4618a1e17115383c28fd6d97b51b6cb95f4387551769cb',
+     x86_64: 'ce9eceed169b2707a12e55ca318466611f29d18c2f7705a13df33f80fe8c00e0'
   })
 
   depends_on 'speexdsp' => :build
@@ -27,14 +27,13 @@ class Waypipe < Package
   depends_on 'libdrm' => :build
   depends_on 'libva' # R
   depends_on 'mesa' # R
-
-  def self.patch
-    system "sed -i '/#include \"util.h\"/a #include  <linux/version.h>' src/dmabuf.c"
-    system "sed -i 's#defined(__FreeBSD__)#defined(__FreeBSD__) || LINUX_VERSION_CODE < KERNEL_VERSION(4, 6, 0)#g' src/dmabuf.c"
-  end
+  depends_on 'glibc' # R
+  depends_on 'lz4' # R
+  depends_on 'zstd' # R
+  depends_on 'gcc' # R
 
   def self.build
-    system "meson #{CREW_MESON_OPTIONS} \
+    system "CC=clang LD=mold meson setup #{CREW_MESON_OPTIONS.gsub('-ffat-lto-objects', '').gsub('-fuse-ld=mold', '')} \
       builddir"
     system 'meson configure builddir'
     system 'ninja -C builddir'
