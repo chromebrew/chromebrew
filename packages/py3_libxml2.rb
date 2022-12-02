76ca8cb1482e5bdc65f65b23609feda808072015
Satadru Pramanik <satadru@gmail.com>
2022-12-01 20:13:53 -0500

    update py3_libxml2.rb py3_pyelftools.rb py3_codespell.rb

--- packages/py3_libxml2.rb
+++ packages/py3_libxml2.rb
@@ -11,16 +11,16 @@ class Py3_libxml2 < Package
   source_sha256 '302bbb86400b8505bebfbf7b3d1986e9aa05073198979f258eed4be481ff5f83'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_armv7l/py3_libxml2-2.10.3-py3.10-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_armv7l/py3_libxml2-2.10.3-py3.10-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_i686/py3_libxml2-2.10.3-py3.10-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.10_x86_64/py3_libxml2-2.10.3-py3.10-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.11_armv7l/py3_libxml2-2.10.3-py3.11-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.11_armv7l/py3_libxml2-2.10.3-py3.11-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.11_i686/py3_libxml2-2.10.3-py3.11-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_libxml2/2.10.3-py3.11_x86_64/py3_libxml2-2.10.3-py3.11-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '78aa7d4890fe6890dfa4d61d285a193ba57d4856db0bd31037c8f0ec563debcd',
-     armv7l: '78aa7d4890fe6890dfa4d61d285a193ba57d4856db0bd31037c8f0ec563debcd',
-       i686: '58b5d28a139656758a015d580943a8a6a049e31526d21e09e57aee8358100ee3',
-     x86_64: 'e66f9d4928223c33cbd1ae060f39da55b87ff1a4cb4ded49272650b97481c4cd'
+    aarch64: '09be87f83073941a30c4653c4e364fe919d5a23417d14e84942a97ce0e484fde',
+     armv7l: '09be87f83073941a30c4653c4e364fe919d5a23417d14e84942a97ce0e484fde',
+       i686: '008663242f5709f9b04cd1f3b444a7b2c48862dd032da0399dcbd7b1ccad738a',
+     x86_64: '993ad6b79abe840dbc544f6ee4c7b25e9632101c42071a3dfb24039a65cc3dae'
   })
 
   depends_on 'libxml2'
--- packages/py3_pyelftools.rb
+++ packages/py3_pyelftools.rb
@@ -11,16 +11,16 @@ class Py3_pyelftools < Package
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.28_armv7l/py3_pyelftools-0.28-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.28_armv7l/py3_pyelftools-0.28-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.28_i686/py3_pyelftools-0.28-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.28_x86_64/py3_pyelftools-0.28-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.29-py3.11_armv7l/py3_pyelftools-0.29-py3.11-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.29-py3.11_armv7l/py3_pyelftools-0.29-py3.11-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.29-py3.11_i686/py3_pyelftools-0.29-py3.11-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyelftools/0.29-py3.11_x86_64/py3_pyelftools-0.29-py3.11-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '969669befcac95f56ea469f66cced8d37728d89c92b87c6e11fa9b12943d0482',
-     armv7l: '969669befcac95f56ea469f66cced8d37728d89c92b87c6e11fa9b12943d0482',
-       i686: 'c6112ede652cbcb1f46efcdab9c5d66e73ab9483b9615a2d50ef2a00ba7bc735',
-     x86_64: 'a43b98cb137c5c8093a497a39ae63d79d9c2ad0ef4ec08391392e099a962f5a4'
+    aarch64: '507c5cb7f67912c4f58751e1f2355bba66db769347413914b172bb1597a7894c',
+     armv7l: '507c5cb7f67912c4f58751e1f2355bba66db769347413914b172bb1597a7894c',
+       i686: '4832ba057d09781193e13c422f6ebe2cc4559b9fc610bfdeb5bf8d81a7e8ca8d',
+     x86_64: '5bbf7eb6f58583ba570a591b953c411acd7fb31d2263edec7c72b7793ad104ca'
   })
 
   depends_on 'glibc' # R
