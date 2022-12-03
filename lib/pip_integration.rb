331b436fad70be402dc35b0180870148089d88ef
supechicken <supechicken666@gmail.com>
2022-12-04 03:01:18 +0800

    crew: Redirect all `py3_*` packages to `pip`

---
+++ lib/pip_integration.rb
@@ -0,0 +1,54 @@
+require 'json'
+require_relative 'color'
+
+class Pip
+  def self.get_info(pkgName)
+    pkgInfo = JSON.parse(`curl -LSs https://pypi.org/pypi/#{pkgName}/json`, symbolize_names: true)
+
+    {
+      description: pkgInfo[:info][:description],
+      homepage: pkgInfo[:info][:home_page],
+      license: pkgInfo[:info][:license],
+      version: pkgInfo[:info][:version]
+    }.transform_values(&:chomp)
+  end
+
+  def self.update_installed_list
+    @installed = `pip list`.lines(chomp: true)[2..-1].map(&:split).to_h
+  end
+
+  def self.installed_list
+    @installed || update_installed_list
+
+    return @installed
+  end
+
+  def self.install(pkgName)
+    @installed || update_installed_list
+
+    if @installed.has_key?(pkgName)
+      warn "Package py3_#{pkgName} already installed, skipping...".lightgreen
+      return false
+    else
+      system 'pip', 'install', pkgName, exception: true
+      update_installed_list
+    end
+  end
+
+  def self.remove(pkgName)
+    @installed || update_installed_list
+
+    if @installed.has_key?(pkgName)
+      system 'pip', 'uninstall', '--yes', pkgName, exception: true
+      update_installed_list
+    else
+      warn "Package py3_#{pkgName} isn't installed.".lightred
+      return false
+    end
+  end
+
+  def self.reinstall(pkgName)
+    remove pkgName
+    install pkgName
+  end
+end
