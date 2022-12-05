8732ccc4fb9b584710c3c2ead30a3d7e4b3660a7
Satadru Pramanik <satadru@gmail.com>
2022-12-05 18:52:11 -0500

    multiple x package updates (#7717)

--- packages/libxdamage.rb
+++ packages/libxdamage.rb
@@ -3,30 +3,36 @@ require 'package'
 class Libxdamage < Package
   description 'library for the X window system'
   homepage 'https://x.org'
-  @_ver = '1.1.5'
-  version @_ver
+  version '1.1.6'
   license 'MIT'
   compatibility 'all'
-  source_url "https://www.x.org/archive/individual/lib/libXdamage-#{@_ver}.tar.gz"
-  source_sha256 '630ec53abb8c2d6dac5cd9f06c1f73ffb4a3167f8118fdebd77afd639dbc2019'
+  source_url 'https://gitlab.freedesktop.org/xorg/lib/libxdamage.git'
+  git_hashtag "libXdamage-#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.5_armv7l/libxdamage-1.1.5-chromeos-armv7l.tar.xz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.5_armv7l/libxdamage-1.1.5-chromeos-armv7l.tar.xz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.5_i686/libxdamage-1.1.5-chromeos-i686.tar.xz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.5_x86_64/libxdamage-1.1.5-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.6_armv7l/libxdamage-1.1.6-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.6_armv7l/libxdamage-1.1.6-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.6_i686/libxdamage-1.1.6-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxdamage/1.1.6_x86_64/libxdamage-1.1.6-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '1732f53c0dadda7fbc3a17b9628930e3f5a5c22200cc0d8e6020eca7f410264b',
-      armv7l: '1732f53c0dadda7fbc3a17b9628930e3f5a5c22200cc0d8e6020eca7f410264b',
-        i686: '26fa5709ae5558b5513d7b0044b12a73e2ab023d7df570276c5e7a75b630afef',
-      x86_64: 'fc6ecb1acb475d6dbe7c75028b3c251218e5a75dc5431b5e5e549f56bc5b5cde'
+    aarch64: '1acd0ec73124faf374844894ebbaa792e985272284c5ff544c93499c3b6d8e2f',
+     armv7l: '1acd0ec73124faf374844894ebbaa792e985272284c5ff544c93499c3b6d8e2f',
+       i686: 'af9d5094cd0eb74f4555c75c9133b34f4954b44dcd04101004355373148f42d4',
+     x86_64: '122712b7c3630e9d849fa134d94201437da15ff1cda107d9932cc3a39d08d6c4'
   })
 
-  depends_on 'libxfixes'
-  depends_on 'libx11'
+  depends_on 'glibc' # R
+  depends_on 'libbsd' # R
+  depends_on 'libmd' # R
+  depends_on 'libx11' # R
+  depends_on 'libxau' # R
+  depends_on 'libxcb' # R
+  depends_on 'libxdmcp' # R
+  depends_on 'libxfixes' # R
 
   def self.build
+    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
     system "./configure #{CREW_OPTIONS}"
     system 'make'
   end
