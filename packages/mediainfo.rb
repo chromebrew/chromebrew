21b7e7b3ed43bcc541052892814ccdee67fe3857
Ed Reel <edreel@gmail.com>
2022-12-17 00:02:07 -0600

    Mediainfo 17.12 => 22.09

--- packages/mediainfo.rb
+++ packages/mediainfo.rb
@@ -3,27 +3,31 @@ require 'package'
 class Mediainfo < Package
   description 'MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files.'
   homepage 'http://mediaarea.net/en/MediaInfo'
-  version '17.12'
+  version '22.09'
   license 'BSD-2'
   compatibility 'all'
-  source_url 'http://mediaarea.net/download/binary/mediainfo/17.12/MediaInfo_CLI_17.12_GNU_FromSource.tar.bz2'
-  source_sha256 '0f23ccc9a78b24104dc387691e1df484ed75350ee77277982cd170cca59750b0'
+  source_url 'https://mediaarea.net/download/binary/mediainfo/22.09/MediaInfo_CLI_22.09_GNU_FromSource.tar.bz2'
+  source_sha256 'cbeb09e63d77e10d74ff5ee3c4955f7ff848796f506d64cbed3d6e828632d52b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/17.12_armv7l/mediainfo-17.12-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/17.12_armv7l/mediainfo-17.12-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/17.12_i686/mediainfo-17.12-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/17.12_x86_64/mediainfo-17.12-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/22.09_armv7l/mediainfo-22.09-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/22.09_armv7l/mediainfo-22.09-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/22.09_i686/mediainfo-22.09-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mediainfo/22.09_x86_64/mediainfo-22.09-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a963c89216f20971448bc34dc69b868d9bb3aa106d9cd6cb0eb5884ebd9e08b3',
-     armv7l: 'a963c89216f20971448bc34dc69b868d9bb3aa106d9cd6cb0eb5884ebd9e08b3',
-       i686: '700b4c95aa8d9693638eec5202bdaf2975840000ff0325ae48fbda770a867e76',
-     x86_64: '29566ace5b5966c57ec206e2d17ca32592b116b968e65565ae6c1e4510daae46'
+    aarch64: 'c9f8c08e3b443beea717e7a709b799930f3f68188e17c232b952bc1e4920b955',
+     armv7l: 'c9f8c08e3b443beea717e7a709b799930f3f68188e17c232b952bc1e4920b955',
+       i686: '5e3d9d17a62739aebb3ea66a43641dabd62aaaa3fbd17a67fd4efbc21e98271e',
+     x86_64: '235aae12f22bc6dee1e57b3871780eeecb950f87cd89c4a49dfddd6131ddaa44'
   })
 
+  def self.patch
+    system 'filefix'
+  end
+
   def self.build
-    system "./CLI_Compile.sh --prefix=#{CREW_PREFIX}"
+    system "./CLI_Compile.sh #{CREW_OPTIONS}"
   end
 
   def self.install
