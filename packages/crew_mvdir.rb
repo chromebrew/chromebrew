906942f4123b2d453c036e905d8d95623fd13327
supechicken <supechicken666@gmail.com>
2022-12-17 19:40:55 +0800

    crew: Move files instead of copying+deleting

---
+++ packages/crew_mvdir.rb
@@ -0,0 +1,21 @@
+require 'package'
+
+class Crew_mvdir < Package
+  description 'Alternative for "mv dir1/* dir2", with ability to override conflict files in destination'
+  homepage 'https://chromebrew.github.io'
+  version '0.1'
+  compatibility 'all'
+  license 'GPL-3'
+
+  source_url 'https://github.com/chromebrew/chromebrew/raw/master/src/mvdir.c'
+  source_sha256 'SKIP'
+
+  def self.build
+    system "mold -run cc #{CREW_COMMON_FLAGS} mvdir.c crew-mvdir"
+  end
+
+  def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.install 'crew-mvdir', "#{CREW_DEST_PREFIX}/bin/crew-mvdir", mode: 0o755
+  end
+end
