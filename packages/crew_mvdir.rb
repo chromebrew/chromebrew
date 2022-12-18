0b99ee21c02063caa5657cbd69aef924c6a8c28d
supechicken <supechicken666@gmail.com>
2022-12-18 13:01:12 +0800

    Switch to chromebrew repo

--- packages/crew_mvdir.rb
+++ packages/crew_mvdir.rb
@@ -7,8 +7,8 @@ class Crew_mvdir < Package
   compatibility 'all'
   license 'GPL-3'
 
-  source_url 'https://github.com/supechicken/chromebrew/raw/use_rename/src/mvdir.c'
-  source_sha256 'SKIP'
+  source_url 'https://github.com/chromebrew/chromebrew/raw/master/src/mvdir.c' # we cannot get the commit before #7751 merged
+  source_sha256 '0b7e3eae6d13c3e46e9ebd0c31eae5551e5c7640d2accaa1b5f855c8dedb8a21'
 
   no_shrink # upx: CantPackException: bad DT_GNU_HASH (1+ max_bucket)=0x1 < symbias=0x14
 
