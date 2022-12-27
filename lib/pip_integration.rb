5a28548e86d415c3094e4f64fa3451f4f2a01ff9
supechicken <supechicken666@gmail.com>
2022-12-27 23:57:42 +0800

    Add update and upgrade support

--- lib/pip_integration.rb
+++ lib/pip_integration.rb
@@ -2,43 +2,83 @@ require 'json'
 require_relative 'color'
 
 class Pip
-  def self.get_info(pkgName)
-    pkgInfo = JSON.parse(`curl -LSs https://pypi.org/pypi/#{pkgName}/json`, symbolize_names: true)
+  def self.get_pkg_info(pkgName, prefer_online: false)
+    is_installed = installed_list.key?(pkgName)
 
-    {
-      description: pkgInfo[:info][:description],
-      homepage: pkgInfo[:info][:home_page],
-      license: pkgInfo[:info][:license],
-      version: pkgInfo[:info][:version]
-    }.transform_values(&:chomp)
+    if prefer_online || !is_installed
+      pkgInfo = JSON.parse(`curl -LSs https://pypi.org/pypi/#{pkgName}/json`, symbolize_names: true)
+
+      info = {
+        description: pkgInfo[:info][:description],
+        homepage: pkgInfo[:info][:home_page],
+        license: pkgInfo[:info][:license],
+        version: pkgInfo[:info][:version]
+      }.transform_values(&:chomp)
+    else
+      pkgInfo   = `pip show -f #{pkgName}`.scan(/^[[:blank:]]*(.+?): (.+)$/).to_h
+      pkgPrefix = pkgInfo['Location']
+
+      filelist = pkgInfo[pkgInfo.index('Files:')..].map do |path|
+        File.expand_path(path, pkgPrefix)
+      end
+
+      info = {
+        description: pkgInfo['Summary'],
+        homepage: pkgInfo['Home-page'],
+        license: pkgInfo['License'],
+        version: pkgInfo['Version'],
+        filelist:
+      }
+    end
   end
 
   def self.update_installed_list
-    @installed = `pip list`.lines(chomp: true)[2..].map(&:split).to_h
+    @installed = `pip freeze`.scan(/^(.+?)==(.+)$/).to_h
   end
 
-  def self.installed_list
-    @installed || update_installed_list
-
-    return @installed
+  def self.update_upgradable_list
+    @upgradable = `pip list --outdated`.lines(chomp: true)[2..].map do |pkg|
+      pkgName, currentVer, latestVer, _ = pkg.split(/\s+/, 4)
+      [pkgName, [currentVer, latestVer]]
+    end.to_h
   end
 
-  def self.install(pkgName)
-    @installed || update_installed_list
+  def self.installed_list = (@installed || update_installed_list)
+  def self.upgradable_list = (@upgradable_list || update_upgradable_list)
 
-    if @installed.key?(pkgName)
+  def self.check_update
+    upgradable_list.each_pair do |pkgName, (currentVer, latestVer)|
+      puts "#{pkgName} could be updated from #{currentVer} to #{latestVer}"
+    end
+
+    return upgradable_list.size
+  end
+
+  def self.upgrade(pkgName = nil)
+    if pkgName
+      install pkgName, '--upgrade'
+    else
+      upgradable_list.each_pair do |pkgName, (currentVer, latestVer)|
+        install "#{pkgName}==#{latestVer}", '--upgrade'
+      end
+    end
+    update_installed_list
+    update_upgradable_list
+  end
+
+  def self.install(pkgName, version = nil, *opts)
+    if installed_list.key?(pkgName)
       warn "Package py3_#{pkgName} already installed, skipping...".lightgreen
       return false
     else
-      system 'pip', 'install', pkgName, exception: true
+      pkgName = version ? "#{pkgName}==#{version}" : pkgName
+      system 'pip', 'install', pkgName, *opts, exception: true
       update_installed_list
     end
   end
 
   def self.remove(pkgName)
-    @installed || update_installed_list
-
-    if @installed.key?(pkgName)
+    if installed_list.key?(pkgName)
       system 'pip', 'uninstall', '--yes', pkgName, exception: true
       update_installed_list
     else
@@ -50,5 +90,11 @@ class Pip
   def self.reinstall(pkgName)
     remove pkgName
     install pkgName
+  end
+
+  def self.get_filelist(pkgName)
+    abort unless installed_list.key?(pkgName)
+
+    return get_pkg_info(pkgName)[:filelist]
   end
 end
