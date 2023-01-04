3682034dd5ba91229457571cc17eb6e460a1c2e3
Satadru Pramanik <satadru@gmail.com>
2023-01-04 17:26:17 -0500

    vim,gvim,xxd => 9.x

--- packages/gvim.rb
+++ packages/gvim.rb
@@ -3,27 +3,43 @@ require 'package'
 class Gvim < Package
   description 'Vim is a highly configurable text editor built to make creating and changing any kind of text very efficient. (with advanced features, such as a GUI)'
   homepage 'http://www.vim.org/'
-  @_ver = '8.2.4594'
+  @_ver = '9.0.1145'
   version @_ver
   license 'GPL-2'
-  compatibility 'aarch64,armv7l,x86_64'
+  compatibility 'all'
   source_url 'https://github.com/vim/vim.git'
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gvim/8.2.4594_armv7l/gvim-8.2.4594-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gvim/8.2.4594_armv7l/gvim-8.2.4594-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gvim/8.2.4594_x86_64/gvim-8.2.4594-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gvim/9.0.1145_armv7l/gvim-9.0.1145-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gvim/9.0.1145_armv7l/gvim-9.0.1145-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gvim/9.0.1145_i686/gvim-9.0.1145-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gvim/9.0.1145_x86_64/gvim-9.0.1145-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a778b458c9111ccedf0c84b2cdd472c52bf80c9e52e40f6c22f22f2ccf27d50a',
-     armv7l: 'a778b458c9111ccedf0c84b2cdd472c52bf80c9e52e40f6c22f22f2ccf27d50a',
-     x86_64: '0b238d45425755746cf68dd398f33ca5e22345a3fcab50dfb472bf7fe574f402'
+    aarch64: 'd8edb31697fd1e5f229f4e6cd508fbc63dc8a6c6eb48cdf38f4e5f058b9a2aa4',
+     armv7l: 'd8edb31697fd1e5f229f4e6cd508fbc63dc8a6c6eb48cdf38f4e5f058b9a2aa4',
+       i686: '61bc60db3c6ec2c9ecc3f6805880858f35be16037846cd4cd3d16608c6c35d86',
+     x86_64: '0e83d2ea39503b2e2214a800a9c6008b81f27fde3d24318ec05c5ec04f4a5e75'
   })
 
   depends_on 'vim_runtime'
   depends_on 'gtk3'
   depends_on 'sommelier'
+  depends_on 'acl' # R
+  depends_on 'gcc' # R
+  depends_on 'gdk_pixbuf' # R
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
+  depends_on 'gpm' # R
+  depends_on 'gtk2' # R
+  depends_on 'libice' # R
+  depends_on 'libsm' # R
+  depends_on 'libsodium' # R
+  depends_on 'libx11' # R
+  depends_on 'libxt' # R
+  depends_on 'ncurses' # R
+  depends_on 'pango' # R
 
   def self.preflight
     vim = `which #{CREW_PREFIX}/bin/vim 2> /dev/null`.chomp
@@ -31,7 +47,6 @@ class Gvim < Package
   end
 
   def self.patch
-    abort('Please remove libiconv before building.') if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
     # set the system-wide vimrc path
     FileUtils.cd('src') do
       system 'sed', '-i', "s|^.*#define SYS_VIMRC_FILE.*$|#define SYS_VIMRC_FILE \"#{CREW_PREFIX}/etc/vimrc\"|",
@@ -43,10 +58,7 @@ class Gvim < Package
 
   def self.build
     system '[ -x configure ] || autoreconf -fvi'
-    system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      ./configure \
+    system "./configure \
       #{CREW_OPTIONS} \
       --localstatedir=#{CREW_PREFIX}/var/lib/vim \
       --with-features=huge \
@@ -86,11 +98,40 @@ class Gvim < Package
 
   def self.postinstall
     puts
-    puts "The config files are located in #{CREW_PREFIX}/etc".lightblue
-    puts 'User-specific configuration should go in ~/.gvimrc'.lightblue
+    puts "The config files are located in #{CREW_PREFIX}/etc.".lightblue
+    puts 'User-specific configuration should go in ~/.vimrc.'.lightblue
     puts
     puts 'If you are upgrading from an earlier version, edit ~/.bashrc'.orange
     puts "and remove the 'export VIMRUNTIME' and 'export LC_ALL=C' lines.".orange
     puts
+    # Set vim to be the default vi if there is no vi or if a default
+    # vi does not exist.
+    @crew_vi = File.file?("#{CREW_PREFIX}/bin/vi")
+    @system_vi = File.file?('/usr/bin/vi')
+    @create_vi_symlink = true if !@system_vi && !@crew_vi
+    @create_vi_symlink_ask = true if @crew_vi || @system_vi
+    if @create_vi_symlink_ask
+      print "\nWould you like to set vim to be the default vi [y/N] "
+      case $stdin.getc
+      when 'y', 'Y'
+        @create_vi_symlink = true
+      else
+        @create_vi_symlink = false
+        puts 'Default vi left unchanged.'.lightgreen
+      end
+    end
+    return unless @create_vi_symlink
+
+    FileUtils.ln_sf "#{CREW_PREFIX}/bin/vim", "#{CREW_PREFIX}/bin/vi"
+    puts 'Default vi set to vim.'.lightgreen
+  end
+
+  def self.remove
+    # Remove vi symlink if it is to vim.
+    unless File.symlink?("#{CREW_PREFIX}/bin/vi") && (File.readlink("#{CREW_PREFIX}/bin/vi") == "#{CREW_PREFIX}/bin/vim")
+      return
+    end
+
+    FileUtils.rm "#{CREW_PREFIX}/bin/vi"
   end
 end
