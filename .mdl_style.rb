ea06c1ee598087a2655f807b6d839d0caa5ae14e
supechicken <supechicken666@gmail.com>
2023-01-07 01:28:54 +0800

    [readme]: Revert #7795, disable some rules in linter (#7811)
    * readme: Revert and various improvements
    
    * Make github happy
    
    * Update README.md
    
    * Update images
    
    * Disable `MD002`
    
    * Make linter happy

--- .mdl_style.rb
+++ .mdl_style.rb
@@ -1,11 +1,10 @@
 all
-# Ignore 80 character line length limit.
-exclude_rule 'MD013'
-# Ignore horizontal rule style.
-exclude_rule 'MD035'
-# Ignore emphasis used instead of a header.
-exclude_rule 'MD036'
-# Ignore first line in file should be a top level header.
-exclude_rule 'MD041'
-# Ignore code block style.
-exclude_rule 'MD046'
+rule 'MD003', style: :atx
+
+exclude_rule 'MD002' # We use HTML <h1> tag as the top level header
+exclude_rule 'MD013' # Ignore 80 character line length limit.
+exclude_rule 'MD033' # We need HTML to center logo, which is not possible in pure Markdown
+exclude_rule 'MD035' # Ignore horizontal rule style.
+exclude_rule 'MD036' # Ignore emphasis used instead of a header.
+exclude_rule 'MD041' # Ignore first line in file should be a top level header.
+exclude_rule 'MD046' # Ignore code block style.
