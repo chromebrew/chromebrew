4032fb384887e8dea736c5462efa8d20719ebd9c
Satadru Pramanik <satadru@gmail.com>
2023-01-17 16:41:32 -0500

    intel video updates

--- packages/intel_media_driver.rb
+++ packages/intel_media_driver.rb
@@ -3,24 +3,24 @@ require 'package'
 class Intel_media_driver < Package
   description 'The Intel(R) Media Driver for VAAPI is a new VA-API (Video Acceleration API) user mode driver supporting hardware accelerated decoding, encoding, and video post processing for GEN based graphics hardware.'
   homepage 'https://github.com/intel/media-driver'
-  @_ver = '22.6.0-c7132d8'
+  @_ver = '22.6.6'
   version @_ver
   license 'BSD-3, and MIT'
   compatibility 'x86_64'
   source_url 'https://github.com/intel/media-driver.git'
-  git_hashtag 'c7132d869bed4d9748b789ed4aeb06cc49a311d5'
+  git_hashtag "intel-media-#{@_ver}"
 
   binary_url({
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/intel_media_driver/22.6.0-c7132d8_x86_64/intel_media_driver-22.6.0-c7132d8-chromeos-x86_64.tar.zst'
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/intel_media_driver/22.6.6_x86_64/intel_media_driver-22.6.6-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    x86_64: 'c8fbadb3eb1e5bec3ff67430d3de69037913e3b2605b02b985b73a1d43c36f17'
+    x86_64: '415c7263be5e0743e023f3271b1d2e0823bc645678e877965fd30fc6383ea573'
   })
 
   depends_on 'gcc' # R
   depends_on 'glibc' # R
-  depends_on 'gmmlib'
-  depends_on 'libva'
+  depends_on 'gmmlib' # R
+  depends_on 'libva' # R
 
   #  def self.preflight
   #    abort 'Not an Intel processor, aborting.'.lightred unless CREW_IS_INTEL
