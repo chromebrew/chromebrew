2165998b67c9c6f56ab4c8d9d73057fa7eff5d53
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-01-25 16:12:09 -0500

    add binaries

--- packages/exa.rb
+++ packages/exa.rb
@@ -10,10 +10,25 @@ class Exa < Package
   source_url 'https://github.com/ogham/exa.git'
   git_hashtag "v#{@_ver}"
 
-  depends_on 'libgit2'
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exa/0.10.1-1_armv7l/exa-0.10.1-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exa/0.10.1-1_armv7l/exa-0.10.1-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exa/0.10.1-1_i686/exa-0.10.1-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exa/0.10.1-1_x86_64/exa-0.10.1-1-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    aarch64: '322a9e66be6310b50ec98fd9810a65ff5ab55f04a9a885e03c81095dbd2ac87d',
+     armv7l: '322a9e66be6310b50ec98fd9810a65ff5ab55f04a9a885e03c81095dbd2ac87d',
+       i686: 'b3970d579330b011380867cfdcf55e2ec8fab7bab63d1b6b86a024b2f2fae0d4',
+     x86_64: '6664ea74960837d09a049303ed36b3b766e40a85c652da2ec293279d87f9460f'
+  })
+
+  depends_on 'gcc' # R
+  depends_on 'glibc' # R
+  depends_on 'libgit2' # R
   depends_on 'moreutils' => :build # for the sponge command
-  depends_on 'rust' => :build
   depends_on 'pandoc' => :build
+  depends_on 'rust' => :build
 
   def self.patch
     # Cargo.lock is outdated
@@ -43,9 +58,11 @@ class Exa < Package
     FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1/"
     FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man5/"
     FileUtils.install 'target/release/exa', "#{CREW_DEST_PREFIX}/bin/exa", mode: 0o755
-    FileUtils.install 'completions/completions.bash', "#{CREW_DEST_PREFIX}/share/bash-completion/completions/exa", mode: 0o644
+    FileUtils.install 'completions/completions.bash', "#{CREW_DEST_PREFIX}/share/bash-completion/completions/exa",
+      mode: 0o644
     FileUtils.install 'completions/completions.zsh', "#{CREW_DEST_PREFIX}/share/zsh/site-functions/_exa", mode: 0o644
-    FileUtils.install 'completions/completions.fish', "#{CREW_DEST_PREFIX}/share/fish/vendor_completions.d/exa.fish", mode: 0o644
+    FileUtils.install 'completions/completions.fish', "#{CREW_DEST_PREFIX}/share/fish/vendor_completions.d/exa.fish",
+      mode: 0o644
     FileUtils.install 'exa.1', "#{CREW_DEST_MAN_PREFIX}/man1/exa.1", mode: 0o644
     FileUtils.install 'exa_colors.5', "#{CREW_DEST_MAN_PREFIX}/man5/exa_colors.5", mode: 0o644
   end
