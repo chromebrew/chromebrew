40cbf34baa5056d43c5e9209d917be1e68e2e1b8
Satadru Pramanik <satadru@gmail.com>
2023-01-25 12:44:57 -0500

    Add binaries

--- packages/libarchive.rb
+++ packages/libarchive.rb
@@ -10,14 +10,30 @@ class Libarchive < Package
   source_url "https://www.libarchive.org/downloads/libarchive-#{@_ver}.tar.xz"
   source_sha256 '9e2c1b80d5fbe59b61308fdfab6c79b5021d7ff4ff2489fb12daf0a96a83551d'
 
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libarchive/3.6.2_armv7l/libarchive-3.6.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libarchive/3.6.2_armv7l/libarchive-3.6.2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libarchive/3.6.2_i686/libarchive-3.6.2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libarchive/3.6.2_x86_64/libarchive-3.6.2-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    aarch64: 'ea79c0fb02a8fcedaecb62560c31229a9e75d2c9c610d098a5b8f6edb6259b53',
+     armv7l: 'ea79c0fb02a8fcedaecb62560c31229a9e75d2c9c610d098a5b8f6edb6259b53',
+       i686: 'facc3060ac2b1fbc32adfca9c4603261d54044788d48dcb91521ca12c6c37192',
+     x86_64: 'b47fba0e0591e1e05c38d3cf74c44d49ba9ff8a4bb2bd6a02bdfa61d9c098802'
+  })
+
   depends_on 'acl' # R
+  depends_on 'attr' # R
   depends_on 'bz2' # R
   depends_on 'glibc' # R
+  depends_on 'icu4c' # R
   depends_on 'libxml2' # R
   depends_on 'lz4' # R
   depends_on 'openssl' # R
   depends_on 'xzutils' # R
   depends_on 'zlibpkg' # R
+  depends_on 'zstd' # R
 
   def self.build
     raise StandardError, 'Please remove libiconv before building.' if File.exist?("#{CREW_LIB_PREFIX}/libcharset.so")
