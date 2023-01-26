4f6155486424fbfc0e771eb9aa34de9c437ab553
Satadru Pramanik <satadru@gmail.com>
2023-01-25 21:05:16 -0500

    update krb5, libcyrussasl

--- packages/krb5.rb
+++ packages/krb5.rb
@@ -3,27 +3,28 @@ require 'package'
 class Krb5 < Package
   description 'Kerberos is a network authentication protocol.'
   homepage 'https://web.mit.edu/kerberos'
-  version '1.19.1'
+  version '1.20.1'
   license 'openafs-krb5-a, BSD, MIT, OPENLDAP, BSD-2, HPND, BSD-4, ISC, RSA, CC-BY-SA-3.0 and BSD-2 or GPL-2+ )'
   compatibility 'all'
-  source_url 'https://web.mit.edu/kerberos/dist/krb5/1.19/krb5-1.19.1.tar.gz'
-  source_sha256 'fa16f87eb7e3ec3586143c800d7eaff98b5e0dcdf0772af7d98612e49dbeb20b'
+  source_url 'https://web.mit.edu/kerberos/dist/krb5/1.20/krb5-1.20.1.tar.gz'
+  source_sha256 '704aed49b19eb5a7178b34b2873620ec299db08752d6a8574f95d41879ab8851'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_armv7l/krb5-1.19.1-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_armv7l/krb5-1.19.1-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_i686/krb5-1.19.1-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.19.1_x86_64/krb5-1.19.1-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.20.1_armv7l/krb5-1.20.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.20.1_armv7l/krb5-1.20.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.20.1_i686/krb5-1.20.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/krb5/1.20.1_x86_64/krb5-1.20.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ed77e5d8274ca8cd6acf53edca8d86479aaa67be18d69927424fa3b0572e970e',
-     armv7l: 'ed77e5d8274ca8cd6acf53edca8d86479aaa67be18d69927424fa3b0572e970e',
-       i686: 'ab646a5876870bbd762229dbad705f6826eaf34bd2f3838172916ffcc5067a3a',
-     x86_64: 'dde095df31c2a2b8925d0d9323a9e447b07238cec09ae0e5a43191d0a7b2c94d'
+    aarch64: '9c02f47739672ffb168d41bc654e77ab6a3b07c3fbe7f4f32d2e4be279ee89ad',
+     armv7l: '9c02f47739672ffb168d41bc654e77ab6a3b07c3fbe7f4f32d2e4be279ee89ad',
+       i686: 'f41117ab29c09af9a50c2d064d6bbd0e7915c720e427869d714550b74c1ad0de',
+     x86_64: '0dc50e8a13a6843f5ba79b62a3bcf303b6f9fbb3db9d241f65c5f07286dd1d50'
   })
 
-  # depends_on 'ccache' => :build
+  depends_on 'ccache' => :build
   depends_on 'e2fsprogs' # R
+  depends_on 'gcc' # R
   depends_on 'glibc' # R
   depends_on 'openssl' # R
 
