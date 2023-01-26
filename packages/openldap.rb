4b32b3e32f4fcdd24c84c1d6769cfb99d3c5c738
Satadru Pramanik <satadru@gmail.com>
2023-01-25 22:42:56 -0500

    openldap => 2.6.3

--- packages/openldap.rb
+++ packages/openldap.rb
@@ -3,30 +3,31 @@ require 'package'
 class Openldap < Package
   description 'OpenLDAP Software is an open source implementation of the Lightweight Directory Access Protocol.'
   homepage 'https://www.openldap.org/'
-  @_ver = '2.6.1'
+  @_ver = '2.6.3'
   version @_ver
   license 'OpenLDAP and GPL-2'
   compatibility 'all'
   source_url "https://openldap.org/software/download/OpenLDAP/openldap-release/openldap-#{@_ver}.tgz"
-  source_sha256 '9d576ea6962d7db8a2e2808574e8c257c15aef55f403a1fb5a0faf35de70e6f3'
+  source_sha256 'd2a2a1d71df3d77396b1c16ad7502e674df446e06072b0e5a4e941c3d06c0d46'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.1_armv7l/openldap-2.6.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.1_armv7l/openldap-2.6.1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.1_i686/openldap-2.6.1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.1_x86_64/openldap-2.6.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.3_armv7l/openldap-2.6.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.3_armv7l/openldap-2.6.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.3_i686/openldap-2.6.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openldap/2.6.3_x86_64/openldap-2.6.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '6b3a8709e772c271933bfe76cc79aa7c6a73319358c48857dc4d3a53d1cabd7d',
-     armv7l: '6b3a8709e772c271933bfe76cc79aa7c6a73319358c48857dc4d3a53d1cabd7d',
-       i686: '25415a5c55504a205f7c1768d5e12b5c7278c70e56f3bd77c3e5d103f8da6517',
-     x86_64: '4549a1d764ad469b8bffaba66bb291d68ba7bd20deeb8b8b286bc65f825d71a9'
+    aarch64: 'e33440e146ed109636f46a3f5cf05db4798e6b16905f0335fdffd7a9f2f0f135',
+     armv7l: 'e33440e146ed109636f46a3f5cf05db4798e6b16905f0335fdffd7a9f2f0f135',
+       i686: '2a63e953489dc70695b841afca334218754cc883a018ef68b08f6861247cbef9',
+     x86_64: '2a2aa9b72b4a9c6f5503e388372d1a6af4b3b389ae5f9bc506df6d0afd1b0fdb'
   })
 
+  depends_on 'e2fsprogs' => :build
+  depends_on 'gcc' # R
   depends_on 'glibc' # R
+  depends_on 'krb5' => :build
   depends_on 'libcyrussasl' # R
-  depends_on 'krb5' # R
-  depends_on 'e2fsprogs' # R
   depends_on 'openssl' # R
   depends_on 'util_linux' # R
 
