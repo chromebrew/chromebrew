aebb3d9be8c201c1a625e9fe4e6e328c56f2f8f1
Ed Reel <edreel@gmail.com>
2023-01-29 12:01:59 -0600

    Update filezilla packages (#7879)
    filezilla 3.61.0 => 3.63.1
    
    libfilezilla 0.39.1 => 0.41.0

--- packages/libfilezilla.rb
+++ packages/libfilezilla.rb
@@ -3,21 +3,21 @@ require 'package'
 class Libfilezilla < Package
   description 'libfilezilla is a small and modern C++ library, offering some basic functionality to build high-performing, platform-independent programs.'
   homepage 'https://lib.filezilla-project.org/'
-  version '0.39.1'
+  version '0.41.0'
   license 'GPL-2+'
   compatibility 'aarch64,armv7l,x86_64'
-  source_url 'https://download.filezilla-project.org/libfilezilla/libfilezilla-0.39.1.tar.bz2'
-  source_sha256 'f3d6c0df28f33f52cf253cb03fc51579752ae4daef2aac7345a6bd7de623a6c5'
+  source_url 'https://download.filezilla-project.org/libfilezilla/libfilezilla-0.41.0.tar.bz2'
+  source_sha256 'ac2a1d0d828ea600787cea1e7ee50d21f0d3bd9152cece6187b8afc900622aa0'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libfilezilla/0.39.1_armv7l/libfilezilla-0.39.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libfilezilla/0.39.1_armv7l/libfilezilla-0.39.1-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libfilezilla/0.39.1_x86_64/libfilezilla-0.39.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libfilezilla/0.41.0_armv7l/libfilezilla-0.41.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libfilezilla/0.41.0_armv7l/libfilezilla-0.41.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libfilezilla/0.41.0_x86_64/libfilezilla-0.41.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '250d912aef70be101d50da5d50240dc6f7064f6e6898248fccd51776fad6807e',
-     armv7l: '250d912aef70be101d50da5d50240dc6f7064f6e6898248fccd51776fad6807e',
-     x86_64: '31de2e21979648f016118fe198e2a12991922f8dd62bad88f629d51d71d8f6cd'
+    aarch64: 'd150480edb6fbe9b676b0b31cc3b660bb0fb762a37049ddbef5974a4b378866a',
+     armv7l: 'd150480edb6fbe9b676b0b31cc3b660bb0fb762a37049ddbef5974a4b378866a',
+     x86_64: '62f9f92b33fcfd2b8ccd8f889f8c8e3f6a25475aafbdef3caa6bb0747e0582d7'
   })
 
   depends_on 'openmp'
