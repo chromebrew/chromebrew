618e7a80bfe52c199e22d19c49ee96c41039f44a
Ed Reel <edreel@gmail.com>
2023-02-05 13:54:15 -0600

    Wp_cli 2.7.0 => 2.7.1

--- packages/wp_cli.rb
+++ packages/wp_cli.rb
@@ -3,11 +3,11 @@ require 'package'
 class Wp_cli < Package
   description 'The command line interface for WordPress'
   homepage 'https://wp-cli.org/'
-  version '2.7.0'
+  version '2.7.1'
   license 'LGPL-3'
   compatibility 'all'
   source_url "https://github.com/wp-cli/wp-cli/releases/download/v#{version}/wp-cli-#{version}.phar"
-  source_sha256 '4445500eaf741b1c127c60ed2a8c54e1c4c8f54cd56aaae842d38bf459d282fa'
+  source_sha256 'bbf096bccc6b1f3f1437e75e3254f0dcda879e924bbea403dff3cfb251d4e468'
 
   depends_on 'php74' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
