664383b052b2be5000c7457a9aeb5850bf3dc7c6
Satadru Pramanik <satadru@gmail.com>
2023-02-08 17:52:41 -0500

    add ncompress binaries

--- packages/ncompress.rb
+++ packages/ncompress.rb
@@ -13,16 +13,16 @@ class Ncompress < Package
   source_sha256 '96ec931d06ab827fccad377839bfb91955274568392ddecf809e443443aead46'
 
   binary_url({
-     aarch64: 'file:///usr/local/tmp/packages/ncompress-5.0-chromeos-armv7l.tar.zst',
-      armv7l: 'file:///usr/local/tmp/packages/ncompress-5.0-chromeos-armv7l.tar.zst',
-        i686: 'file:///usr/local/tmp/packages/ncompress-5.0-chromeos-i686.tar.zst',
-      x86_64: 'file:///usr/local/tmp/packages/ncompress-5.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncompress/5.0_armv7l/ncompress-5.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncompress/5.0_armv7l/ncompress-5.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncompress/5.0_i686/ncompress-5.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncompress/5.0_x86_64/ncompress-5.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-     aarch64: 'a3e3069054eccfc3011cc705f49123bb08fdb5de39e2848749a584be0a406d35',
-      armv7l: 'a3e3069054eccfc3011cc705f49123bb08fdb5de39e2848749a584be0a406d35',
-        i686: 'a0e95bd06aa844286d5b4a92ef5d65073d152a82ab63384a3dcd3b3c4f1d0ad7',
-      x86_64: 'a84ba675a89c4184c1e2495b0a016f964ff75f2b0f2a977552cf7fd94be136b4'
+    aarch64: 'a3e3069054eccfc3011cc705f49123bb08fdb5de39e2848749a584be0a406d35',
+     armv7l: 'a3e3069054eccfc3011cc705f49123bb08fdb5de39e2848749a584be0a406d35',
+       i686: 'a0e95bd06aa844286d5b4a92ef5d65073d152a82ab63384a3dcd3b3c4f1d0ad7',
+     x86_64: 'a84ba675a89c4184c1e2495b0a016f964ff75f2b0f2a977552cf7fd94be136b4'
   })
 
   depends_on 'glibc'
