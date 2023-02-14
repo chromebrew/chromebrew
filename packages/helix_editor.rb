1f31b20002c5bea936dcd9294a036d16f17e55d4
ncdm-stldr <ncdm.stldr@gmail.com>
2023-02-14 22:54:08 +0100

    improvements related to the PR review

--- packages/helix_editor.rb
+++ packages/helix_editor.rb
@@ -1,24 +1,18 @@
 require 'package'
 
 class Helix_editor < Package                 # The first character of the class name must be upper case
-  description 'A vim like editor with LSP support'
+  description 'A vim inspired editor with LSP support'
   homepage 'https://helix-editor.com/'
   version '22.12'
   license 'MPL-2.0' # license of source
   compatibility 'all'
   source_url 'https://github.com/helix-editor/helix/archive/refs/tags/22.12.tar.gz'
   source_sha256 'edae8af46401b45c3e71c38b4fa99f931c4458127978ccd1b29aaae79331d972'   # Use the command "sha256sum"
-  #source_url 'file:///usr/local/home/helix/deleteme.tar.gz'
-  #source_sha256 'c3a0e022a7bcd6b139ae7abf1e0dae33c8f9ec82dc680076d9e5ea6593b85bc6'
 
   depends_on 'rust' => :build
 
-  # TODO is it the appropriate location?
-  # By default, helix expects runtime to be in ~/.config or XDG_CONFIG_HOME.
-  # The first location is not appropriate because helix will not be installed in ~/
-  # With the second location crew complaints that it is not  FHS3 compliant.
-  @runtime_dest_dir_suffix = "/var/lib/helix"
-
+  @no_fhs = true
+  
   def self.build
    puts "Building. This may be long."
    system "cargo build \
@@ -33,34 +27,20 @@ class Helix_editor < Package                 # The first character of the class 
     FileUtils.mkdir_p("#{helix_executable_dest_dir}")
     FileUtils.cp("./target/release/hx", "#{helix_executable_dest_dir}")
     # Copy runtime dir
-    FileUtils.mkdir_p("#{CREW_DEST_PREFIX}#{@runtime_dest_dir_suffix}")
-    FileUtils.cp_r("./runtime", "#{CREW_DEST_PREFIX}#{@runtime_dest_dir_suffix}")
-    # Set env var for runtime dir
-    env_dir = "#{CREW_DEST_PREFIX}/etc/env.d"
-    FileUtils.mkdir_p "#{env_dir}"
-    File.write("#{env_dir}/helix",
-               "export HELIX_RUNTIME=#{CREW_PREFIX}#{@runtime_dest_dir_suffix}/runtime")
-    # system "source #{env_dir}/helix"
+    helix_runtime_dest_dir = "#{CREW_DEST_DIR}#{ENV['XDG_CONFIG_HOME']}/helix"
+    FileUtils.mkdir_p("#{helix_runtime_dest_dir}")
+    FileUtils.cp_r("./runtime", "#{helix_runtime_dest_dir}")
   end
 
   def self.check
-    # # TODO (verify that this check works, and if it fails when the system language
-    # #  is not english)
-    # helix_health = capture(:stdout) { system("hx --health") }
-    # return !helix_health.contains('Runtime directory does not exist')
-    return true
-  end
-
-  def self.remove
-    # Some files in the directory are write protected...
-    FileUtils.rm_rf("#{CREW_PREFIX}#{@runtime_dest_dir_suffix}")
-    # TODO if a local ~/.config/helix folder is found, ask user if he wishes to remove it
+    # TODO: check seems to be never called
+    # system "hx --version"
+    # helix_health_output = capture(:stdout) { system("hx --health") }
+    # return helix_health_output.include?("#{CREW_PREFIX}#{ENV['XDG_CONFIG_HOME']}/helix")
   end
 
   def self.postinstall
     puts <<~EOT2.orange
-      Restart a new shell for helix environnement variables to be set. 
-      
       Use the 'hx' command to start helix.
     
       Use 'hx --health' to see if helix detects its runtime and to see which LSP 
