6098d2675f9e1ca035a702479239b87df3164ad8
Satadru Pramanik <satadru@gmail.com>
2023-02-16 16:52:35 -0500

    add grive binaries

--- packages/grive.rb
+++ packages/grive.rb
@@ -3,42 +3,43 @@ require 'package'
 class Grive < Package
   description 'Google Drive client with support for new Drive REST API and partial sync'
   homepage 'https://github.com/vitalif/grive2'
-  version '0.5.2-e6fcc63'
+  version '0.5.3-648ff8e'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/vitalif/grive2.git'
-  git_hashtag 'e6fcc637f8d51126312f12d0c0a568046c4f95de'
+  git_hashtag '648ff8eea1a3c7cac8bfba283f75717bf54c67eb'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.2-e6fcc63_armv7l/grive-0.5.2-e6fcc63-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.2-e6fcc63_armv7l/grive-0.5.2-e6fcc63-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.2-e6fcc63_i686/grive-0.5.2-e6fcc63-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.2-e6fcc63_x86_64/grive-0.5.2-e6fcc63-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.3-648ff8e_armv7l/grive-0.5.3-648ff8e-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.3-648ff8e_armv7l/grive-0.5.3-648ff8e-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.3-648ff8e_i686/grive-0.5.3-648ff8e-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grive/0.5.3-648ff8e_x86_64/grive-0.5.3-648ff8e-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '2a2c5dae9efd74c5b0497813c895f5bac5c91df8b1b7a9a31ac196ccaf09dd13',
-     armv7l: '2a2c5dae9efd74c5b0497813c895f5bac5c91df8b1b7a9a31ac196ccaf09dd13',
-       i686: '35301f6fe1a3d097341c1ef9b881b2f5dab461b3fd24c5859ddacb4a79b69b0a',
-     x86_64: '6aecea77e9fd0150ac18f2df6444b12cc95e411897800d462d89a1a5f6c8aeae'
+    aarch64: 'f20f7a15dbdc514af123edc914c2ca63b2f5327b1161d5aa981e182654608b4a',
+     armv7l: 'f20f7a15dbdc514af123edc914c2ca63b2f5327b1161d5aa981e182654608b4a',
+       i686: '7f263c2999dccd40fff6955bf6601a37bfcc1ce7c403ccd210de23151119cafd',
+     x86_64: '85e04905425f8e2ab8b6a9843032af15a781aa2c5446e12fbd4444b36c18f5ea'
   })
 
-  depends_on 'yajl'
-  depends_on 'libcurl'
-  depends_on 'libgcrypt'
-  depends_on 'boost'
-  depends_on 'expat'
+  depends_on 'binutils' # R
+  depends_on 'boost' # R
+  depends_on 'cppunit' => :build
+  depends_on 'expat' => :build
+  depends_on 'gcc' # R
+  depends_on 'glibc' # R
+  depends_on 'libcurl' # R
+  depends_on 'libgcrypt' # R
+  depends_on 'libgpgerror' # R
+  depends_on 'yajl' # R
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system "cmake #{CREW_CMAKE_OPTIONS} .."
-      system 'make'
-    end
+    system "cmake -B builddir #{CREW_CMAKE_OPTIONS} \
+            -G Ninja"
+    system 'samu -C builddir'
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
   end
 end
