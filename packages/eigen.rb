2dd639fdbec4694588eb28f2f97e940af69c5b8d
Satadru Pramanik <satadru@gmail.com>
2023-02-16 16:17:46 -0500

    add eigen binaries

--- packages/eigen.rb
+++ packages/eigen.rb
@@ -3,44 +3,37 @@ require 'package'
 class Eigen < Package
   description 'Eigen is a C++ template library for linear algebra: matrices, vectors, numerical solvers, and related algorithms.'
   homepage 'http://eigen.tuxfamily.org/'
-  version '3.3.7'
+  version '3.4.0'
   license 'MPL-2.0'
   compatibility 'all'
-  source_url 'https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.bz2'
-  source_sha256 '685adf14bd8e9c015b78097c1dc22f2f01343756f196acdc76a678e1ae352e11'
+  source_url 'https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.bz2'
+  source_sha256 'b4c198460eba6f28d34894e3a5710998818515104d6e74e5cc331ce31e46e626'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.3.7_armv7l/eigen-3.3.7-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.3.7_armv7l/eigen-3.3.7-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.3.7_i686/eigen-3.3.7-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.3.7_x86_64/eigen-3.3.7-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.4.0_armv7l/eigen-3.4.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.4.0_armv7l/eigen-3.4.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.4.0_i686/eigen-3.4.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eigen/3.4.0_x86_64/eigen-3.4.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'd5cf4005c822890e8d6a3a57fd186151dfd36d0fef513200a2e81fa767ddb64e',
-     armv7l: 'd5cf4005c822890e8d6a3a57fd186151dfd36d0fef513200a2e81fa767ddb64e',
-       i686: '11f717b454e479d2f91ad0410aa403fa68c904fab37a8f30499254f3e780964a',
-     x86_64: '4ae4ecb6aed2407f0e4adf84116b91b40e830eaed6d2ef028825a26e89d8bc72'
+    aarch64: '4e98dce7b77978aff8f6991edcc18577e79728f71cda1753a5b3fa75165e141b',
+     armv7l: '4e98dce7b77978aff8f6991edcc18577e79728f71cda1753a5b3fa75165e141b',
+       i686: 'ba97ef42530781794fab541b020104184c188d56c4e13c2de0924b835bd539cb',
+     x86_64: 'b9275bcb88a8f123b1568f23f723f2340b955fb5323845e0891b98596e8813cf'
   })
 
-  depends_on 'boost'
-  depends_on 'fftw'
-  depends_on 'mesa'
-  depends_on 'superlu'
+  depends_on 'boost' => :build
+  depends_on 'fftw' => :build
+  depends_on 'mesa' => :build
+  depends_on 'superlu' => :build
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system 'cmake',
-             '-DCMAKE_BUILD_TYPE=Release',
-             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
-             '..'
-      system 'make'
-    end
+    system "cmake -B builddir #{CREW_CMAKE_OPTIONS} \
+            -G Ninja"
+    system 'samu -C builddir'
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
   end
 end
