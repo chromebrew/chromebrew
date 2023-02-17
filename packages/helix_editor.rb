078bdab28a6bfa9dbbe64fc0bb86cb53c2fc2877
ncdm-stldr <ncdm.stldr@gmail.com>
2023-02-18 00:53:03 +0100

    write short command on single line

--- packages/helix_editor.rb
+++ packages/helix_editor.rb
@@ -20,10 +20,7 @@ class Helix_editor < Package
 
   def self.build
     puts 'Building. This may be long.'
-    system 'cargo build \
-      --release \
-      --locked \
-     '
+    system 'cargo build --release --locked'
   end
 
   def self.install
