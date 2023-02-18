5ac82d27dc37af2e792787a8f937f8892c8bf091
ncdm-stldr <ncdm.stldr@gmail.com>
2023-02-18 01:28:17 +0100

    fixing invalid argument

--- packages/helix_editor.rb
+++ packages/helix_editor.rb
@@ -49,7 +49,7 @@ class Helix_editor < Package
     command_status = system 'hx --version', exception: false
     puts 'Warning: hx is not in path'.lightred unless command_status == true
     # Check if helix can find its runtime path
-    command_output = `hx --health`, exception: false
+    command_output = `hx --health`
     puts 'Warning: helix cannot find its runtime dir'.lightred unless command_output.include? @helix_runtime_dir
 
     puts <<~EOT2.lightblue
