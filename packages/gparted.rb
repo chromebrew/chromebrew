4150f9c7447589f1444ffe5ae4a53d58d72f35b4
Ed Reel <edreel@gmail.com>
2023-02-22 20:00:13 -0600

    Gparted 1.4.0 => 1.5.0

--- packages/gparted.rb
+++ packages/gparted.rb
@@ -3,24 +3,22 @@ require 'package'
 class Gparted < Package
   description 'A Partition Magic clone, frontend to GNU Parted'
   homepage 'https://gparted.org/'
-  @_ver = '1.4.0'
+  @_ver = '1.5.0'
   version @_ver
   license 'GPL-2+ and FDL-1.2+'
-  compatibility 'all'
+  compatibility 'aarch64,armv7l,x86_64'
   source_url "https://downloads.sourceforge.net/project/gparted/gparted/gparted-#{@_ver}/gparted-#{@_ver}.tar.gz"
-  source_sha256 'e5293a792e53fdbeba29c4a834113cd9603d0d639330da931a468bf3687887be'
+  source_sha256 '3c95ea26a944083ff1d9b17639b1e2ad9758df225dc751ff407b2a6aa092a8de'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gparted/1.4.0_armv7l/gparted-1.4.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gparted/1.4.0_armv7l/gparted-1.4.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gparted/1.4.0_i686/gparted-1.4.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gparted/1.4.0_x86_64/gparted-1.4.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gparted/1.5.0_armv7l/gparted-1.5.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gparted/1.5.0_armv7l/gparted-1.5.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gparted/1.5.0_x86_64/gparted-1.5.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '789aedda96b2cd62cc48460b638af8f94b3cb2773ca6bc0a7d009ad109d94f4f',
-     armv7l: '789aedda96b2cd62cc48460b638af8f94b3cb2773ca6bc0a7d009ad109d94f4f',
-       i686: '59716f3f302de5e7134c51a97329083a0f325c59f5e796a94f79825e2e0601de',
-     x86_64: '4d74f091eb9ce93c896f18784bbe715c0f300f2a15d04c9e7fb659651932a216'
+    aarch64: 'f4365811ad6f720bd9a3faf834634f5776cdd3921f093e63f014a675cc31b371',
+     armv7l: 'f4365811ad6f720bd9a3faf834634f5776cdd3921f093e63f014a675cc31b371',
+     x86_64: '8a3230702da1f1f1f780bdc24085fc35f5f2a70404ad9fd0b41a19946486607f'
   })
 
   depends_on 'parted'
@@ -37,7 +35,10 @@ class Gparted < Package
   depends_on 'librsvg'
   depends_on 'xhost'
   depends_on 'sommelier'
-  patchelf
+
+  def self.patch
+    system 'filefix' # Fix ./configure: line 7001: /usr/bin/file: No such file or directory
+  end
 
   def self.build
     system "./configure #{CREW_OPTIONS} \
@@ -46,16 +47,17 @@ class Gparted < Package
       --enable-libparted-dmraid \
       --enable-xhost-root"
     system 'make'
-    system "cat <<'EOF'> gparted_
-#!/bin/sh
-xhost si:localuser:root
-sudo -E LD_LIBRARY_PATH=#{CREW_LIB_PREFIX} gparted.elf
-EOF"
+    gparted_sh = <<~EOF
+      #!/bin/sh
+      xhost si:localuser:root
+      sudo -E LD_LIBRARY_PATH=#{CREW_LIB_PREFIX} #{CREW_PREFIX}/bin/gparted.elf
+    EOF
+    File.write('gparted.sh', gparted_sh)
   end
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
     FileUtils.mv "#{CREW_DEST_PREFIX}/bin/gparted", "#{CREW_DEST_PREFIX}/bin/gparted.elf"
-    FileUtils.install 'gparted_', "#{CREW_DEST_PREFIX}/bin/gparted", mode: 0o755
+    FileUtils.install 'gparted.sh', "#{CREW_DEST_PREFIX}/bin/gparted", mode: 0o755
   end
 end
