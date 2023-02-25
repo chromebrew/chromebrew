7a67dd541a96f1832828561c84ead17772e6b2f3
supechicken <supechicken666@gmail.com>
2023-02-25 03:16:56 +0000

    Update package file

--- packages/crew_mvdir.rb
+++ packages/crew_mvdir.rb
@@ -7,17 +7,16 @@ class Crew_mvdir < Package
   compatibility 'all'
   license 'GPL-3'
 
-  source_url 'https://github.com/chromebrew/chromebrew/raw/master/src/mvdir.c' # we cannot get the commit before #7751 merged
-  source_sha256 '0b7e3eae6d13c3e46e9ebd0c31eae5551e5c7640d2accaa1b5f855c8dedb8a21'
+  source_url 'https://github.com/chromebrew/crew-mvdir.git'
+  git_hashtag 'ca2bec1fcf76394387a880f1745e57393b7a63c2'
 
-  no_shrink # upx: CantPackException: bad DT_GNU_HASH (1+ max_bucket)=0x1 < symbias=0x14
+  #no_shrink # upx: CantPackException: bad DT_GNU_HASH (1+ max_bucket)=0x1 < symbias=0x14
 
   def self.build
-    system "mold -run cc #{CREW_COMMON_FLAGS} mvdir.c -o crew-mvdir"
+    system "mold -run cc -static #{CREW_COMMON_FLAGS} mvdir.c -o crew-mvdir"
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'crew-mvdir', "#{CREW_DEST_PREFIX}/bin/crew-mvdir", mode: 0o755
   end
 end
