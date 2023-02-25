d63c2431d07d755e53b04052973507fab2407631
Ed Reel <edreel@gmail.com>
2023-02-25 12:02:43 -0600

    Networkmanager 1.30.2 => 1.42.2 (#7994)

--- packages/networkmanager.rb
+++ packages/networkmanager.rb
@@ -3,24 +3,22 @@ require 'package'
 class Networkmanager < Package
   description 'Network connection manager and user applications'
   homepage 'https://wiki.gnome.org/Projects/NetworkManager'
-  @_ver = '1.30.2'
+  @_ver = '1.42.2'
   version @_ver
   license 'GPL-2+ and LGPL-2.1+'
-  compatibility 'all'
+  compatibility 'aarch64,armv7l,x86_64'
   source_url "https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/archive/#{@_ver}/NetworkManager#{@_ver}.tar.bz2"
-  source_sha256 '9fab930be75a2dc4fcfdacdaf9a870580d2969b64aec019dbfc63f4cab491485'
+  source_sha256 '939a27acb0520efc4af3bb7a7dac1e85c34a4b8bbec2f59e2247baf93ac81403'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/networkmanager/1.30.2_armv7l/networkmanager-1.30.2-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/networkmanager/1.30.2_armv7l/networkmanager-1.30.2-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/networkmanager/1.30.2_i686/networkmanager-1.30.2-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/networkmanager/1.30.2_x86_64/networkmanager-1.30.2-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/networkmanager/1.42.2_armv7l/networkmanager-1.42.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/networkmanager/1.42.2_armv7l/networkmanager-1.42.2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/networkmanager/1.42.2_x86_64/networkmanager-1.42.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '69bf115544cf9735cbbddb8b24d3b10991b3ee430856f8d8ac36b26e4e192f13',
-     armv7l: '69bf115544cf9735cbbddb8b24d3b10991b3ee430856f8d8ac36b26e4e192f13',
-       i686: '11c10ff9926abbe5ec47a0015d4922d7d33216dd7b2afd3777fca4d43f966c15',
-     x86_64: 'b0be77ef463fe109b9b55bfe39997a1611f3243fcf3451da370e358088e8c943'
+    aarch64: 'cbabb7283021aca5027e28dc36e957464c14ae7496eb098bb4a6fe0a43712f9c',
+     armv7l: 'cbabb7283021aca5027e28dc36e957464c14ae7496eb098bb4a6fe0a43712f9c',
+     x86_64: '5a48bb9db96022e23cbdb2e76e20a7c009c3700d7cfa208552b3aec2e1b62259'
   })
 
   depends_on 'gobject_introspection'
@@ -36,95 +34,6 @@ class Networkmanager < Package
   depends_on 'libnewt'
   depends_on 'mobile_broadband_provider_info'
   # depends_on 'ccache' => :build
-
-  def self.patch
-    # Patch fixes meson dependency race condition
-    # from https://gitlab.freedesktop.org/NetworkManager/NetworkManager/-/issues/675#note_824519
-    @order_patch = <<~ORDER_PATCH_EOF
-      diff --git i/clients/tui/newt/meson.build w/clients/tui/newt/meson.build
-      index 0c89c0f68155..cbe9d5544995 100644
-      --- i/clients/tui/newt/meson.build
-      +++ w/clients/tui/newt/meson.build
-      @@ -26,6 +26,7 @@ libnmt_newt = static_library(
-         ),
-         dependencies: [
-           libnm_nm_default_dep,
-      +    libnm_dep,
-           newt_dep,
-         ],
-         c_args: [
-    ORDER_PATCH_EOF
-    File.write('order.patch', @order_patch)
-    # This should not be needed in versions newer than 1.30.0 as it is patched
-    # in the main branch.
-    system 'patch -p1 -i order.patch' unless Gem::Version.new(@_ver) > Gem::Version.new('1.30.0')
-    case ARCH
-    when 'i686'
-      # Older kernel headers break wifi usage on kernel 3.18, so
-      # since we disable wifi, just patch around this missing define.
-      system "sed -i 's/NL80211_STA_INFO_BEACON_SIGNAL_AVG/NL80211_STA_INFO_SIGNAL_AVG/g' src/core/platform/wifi/nm-wifi-utils-nl80211.c"
-      # Add in other missing defines as needed.
-      @kernel_missing = <<~KERNEL_MISSING_EOF
-        enum {
-        IFLA_BR_AGEING_TIME,
-        IFLA_BR_STP_STATE,
-        IFLA_BR_PRIORITY,
-        IFLA_BR_VLAN_FILTERING,
-        IFLA_BR_VLAN_PROTOCOL,
-        IFLA_BR_GROUP_FWD_MASK,
-        IFLA_BR_ROOT_ID,
-        IFLA_BR_BRIDGE_ID,
-        IFLA_BR_ROOT_PORT,
-        IFLA_BR_ROOT_PATH_COST,
-        IFLA_BR_TOPOLOGY_CHANGE,
-        IFLA_BR_TOPOLOGY_CHANGE_DETECTED,
-        IFLA_BR_HELLO_TIMER,
-        IFLA_BR_TCN_TIMER,
-        IFLA_BR_TOPOLOGY_CHANGE_TIMER,
-        IFLA_BR_GC_TIMER,
-        IFLA_BR_GROUP_ADDR,
-        IFLA_BR_FDB_FLUSH,
-        IFLA_BR_MCAST_ROUTER,
-        IFLA_BR_MCAST_SNOOPING,
-        IFLA_BR_MCAST_QUERY_USE_IFADDR,
-        IFLA_BR_MCAST_QUERIER,
-        IFLA_BR_MCAST_HASH_ELASTICITY,
-        IFLA_BR_MCAST_HASH_MAX,
-        IFLA_BR_MCAST_LAST_MEMBER_CNT,
-        IFLA_BR_MCAST_STARTUP_QUERY_CNT,
-        IFLA_BR_MCAST_LAST_MEMBER_INTVL,
-        IFLA_BR_MCAST_MEMBERSHIP_INTVL,
-        IFLA_BR_MCAST_QUERIER_INTVL,
-        IFLA_BR_MCAST_QUERY_INTVL,
-        IFLA_BR_MCAST_QUERY_RESPONSE_INTVL,
-        IFLA_BR_MCAST_STARTUP_QUERY_INTVL,
-        IFLA_BR_NF_CALL_IPTABLES,
-        IFLA_BR_NF_CALL_IP6TABLES,
-        IFLA_BR_NF_CALL_ARPTABLES,
-        IFLA_BR_VLAN_DEFAULT_PVID,
-        IFLA_BR_PAD,
-        IFLA_BR_VLAN_STATS_ENABLED,
-        IFLA_BR_MCAST_STATS_ENABLED,
-        IFLA_BR_MCAST_IGMP_VERSION,
-        IFLA_BR_MCAST_MLD_VERSION,
-        IFLA_BR_VLAN_STATS_PER_PORT,
-        };
-      KERNEL_MISSING_EOF
-      File.write('kernel_missing_defs.txt', @kernel_missing)
-      system "sed -i '/nm-udev-utils.h/ r kernel_missing_defs.txt' src/core/platform/nm-linux-platform.c"
-    end
-  end
-
-  case ARCH
-  when 'aarch64', 'armv7l', 'x86_64'
-    @ebpf = 'true'
-    @wifi = 'true'
-    @wext = 'true'
-  when 'i686'
-    @ebpf = 'false'
-    @wifi = 'false'
-    @wext = 'false'
-  end
 
   def self.build
     system "meson setup #{CREW_MESON_OPTIONS} \
@@ -145,9 +54,9 @@ class Networkmanager < Package
       -Dnm_cloud_setup=true \
       -Dbluez5_dun=false \
       -Dlibaudit=no \
-      -Debpf=#{@ebpf} \
-      -Dwifi=#{@wifi} \
-      -Dwext=#{@wifi} \
+      -Debpf=true \
+      -Dwifi=true \
+      -Dwext=true \
       -Dconfig_plugins_default=keyfile \
       -Dnetconfig=no \
       -Dconfig_dns_rc_manager_default=symlink \
