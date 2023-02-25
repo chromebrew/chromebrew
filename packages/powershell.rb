b5d1c8000383c1fb925ccab14126ed4c7f6a5496
supechicken <supechicken666@gmail.com>
2023-02-25 13:25:35 +0000

    powershell => 7.3.3 (#7993)
    * powershell => 7.3.3
    
    * Add no_compile_needed flag

--- packages/powershell.rb
+++ packages/powershell.rb
@@ -3,25 +3,27 @@ require 'package'
 class Powershell < Package
   description 'Powershell is a cross-platform, task-based command-line shell and scripting language that helps rapidly automate tasks that manage operating systems and processes'
   homepage 'https://docs.microsoft.com/en-us/powershell/'
-  version '7.2.1'
+  version '7.3.3'
   license 'MIT'
   compatibility 'aarch64,armv7l,x86_64'
+
   source_url({
-    aarch64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-arm32.tar.gz',
-     armv7l: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-arm32.tar.gz',
-     x86_64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.2.1/powershell-7.2.1-linux-x64.tar.gz'
+    aarch64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/powershell-7.3.3-linux-arm32.tar.gz',
+     armv7l: 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/powershell-7.3.3-linux-arm32.tar.gz',
+     x86_64: 'https://github.com/PowerShell/PowerShell/releases/download/v7.3.3/powershell-7.3.3-linux-x64.tar.gz'
   })
   source_sha256({
-    aarch64: 'd40ca2c8e05dd8b6169d7f35683432c671a3c58f35e46deaf1d776d9a78b38fe',
-     armv7l: 'd40ca2c8e05dd8b6169d7f35683432c671a3c58f35e46deaf1d776d9a78b38fe',
-     x86_64: '337d9864799ad09b46d261071b9f835f69f078814409bc2681f4cc2857b6bda5'
+    aarch64: '8f10ade3bd12a21fba51bcaaecb12b79cb11092725ac0f7c7e6947c053053766',
+     armv7l: '8f10ade3bd12a21fba51bcaaecb12b79cb11092725ac0f7c7e6947c053053766',
+     x86_64: '478320f62c9bec5765414b82b4086310777bf95ab2fc2310b2db6cbc253b40b4'
   })
 
   depends_on 'xdg_base'
 
+  no_compile_needed
+
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/powershell"
+    FileUtils.mkdir_p %W[#{CREW_DEST_PREFIX}/bin #{CREW_DEST_PREFIX}/share/powershell]
     FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/share/powershell"
     FileUtils.ln_s "#{CREW_PREFIX}/share/powershell/pwsh", "#{CREW_DEST_PREFIX}/bin/pwsh"
   end
