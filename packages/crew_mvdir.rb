819bb2185ba5afd174afa80d062f2ba251248d59
supechicken <supechicken666@gmail.com>
2023-02-25 03:22:13 +0000

    Re-add `mkdir`

--- packages/crew_mvdir.rb
+++ packages/crew_mvdir.rb
@@ -10,13 +10,12 @@ class Crew_mvdir < Package
   source_url 'https://github.com/chromebrew/crew-mvdir.git'
   git_hashtag 'ca2bec1fcf76394387a880f1745e57393b7a63c2'
 
-  #no_shrink # upx: CantPackException: bad DT_GNU_HASH (1+ max_bucket)=0x1 < symbias=0x14
-
   def self.build
     system "mold -run cc -static #{CREW_COMMON_FLAGS} crew-mvdir.c -o crew-mvdir"
   end
 
   def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'crew-mvdir', "#{CREW_DEST_PREFIX}/bin/crew-mvdir", mode: 0o755
   end
 end
