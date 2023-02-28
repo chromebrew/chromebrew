f244e4c66a7fe3532571c64fd4cb98b1d086c10c
supechicken <supechicken666@gmail.com>
2023-03-01 04:15:15 +0800

    Update downloader.rb

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -125,17 +125,15 @@ def http_downloader(uri, filename = File.basename(url), verbose = false)
         warn "\n"
       end
 
-      begin
-        progress_bar_thread = progress_bar.show # print progress bar
+      progress_bar_thread = progress_bar.show # print progress bar
 
-        # read file chunks from server, write it to filesystem
-        File.open(filename, 'wb') do |io|
-          response.read_body do |chunk|
-            downloaded_size += chunk.size # record downloaded size, used for showing progress bar
-            progress_bar.set_downloaded_size(downloaded_size, invalid_size_error: false) if file_size.positive?
+      # read file chunks from server, write it to filesystem
+      File.open(filename, 'wb') do |io|
+        response.read_body do |chunk|
+          downloaded_size += chunk.size # record downloaded size, used for showing progress bar
+          progress_bar.set_downloaded_size(downloaded_size, invalid_size_error: false) if file_size.positive?
 
-            io.write(chunk) # write to file
-          end
+          io.write(chunk) # write to file
         end
       ensure
         # stop progress bar, wait for it to terminate
