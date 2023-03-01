9100460ca699ea5a896ebc8e368d53920551d4e5
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-03-01 13:31:24 -0500

    patchelf => 0.17.2 (#8020)

--- packages/patchelf.rb
+++ packages/patchelf.rb
@@ -3,31 +3,40 @@ require 'package'
 class Patchelf < Package
   description 'PatchELF is a small utility to modify the dynamic linker and RPATH of ELF executables.'
   homepage 'http://nixos.org/patchelf.html'
-  version '0.14.5'
+  version '0.17.2'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://github.com/NixOS/patchelf.git'
+  git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_armv7l/patchelf-0.14.5-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_armv7l/patchelf-0.14.5-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_i686/patchelf-0.14.5-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.14.5_x86_64/patchelf-0.14.5-chromeos-x86_64.tar.zst'
+     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.17.2_armv7l/patchelf-0.17.2-chromeos-armv7l.tar.zst',
+      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.17.2_armv7l/patchelf-0.17.2-chromeos-armv7l.tar.zst',
+        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.17.2_i686/patchelf-0.17.2-chromeos-i686.tar.zst',
+      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/patchelf/0.17.2_x86_64/patchelf-0.17.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '81227e2a9b1de212fb041bc7dffd6282f844182e4c39ea6fb6e50084561929f0',
-     armv7l: '81227e2a9b1de212fb041bc7dffd6282f844182e4c39ea6fb6e50084561929f0',
-       i686: 'db817f6eff4c97d07b1849c634dc82087ad584e486d154fd2a0bc02072ec923f',
-     x86_64: '9bc6661f27094ce4033eadc302c042d923b9c0a544f959d1359cfc767971a9b6'
+     aarch64: 'd3b21500b8727dc9372dd42f21c24e346634cb4aa972fbc4653832c08cb9e64d',
+      armv7l: 'd3b21500b8727dc9372dd42f21c24e346634cb4aa972fbc4653832c08cb9e64d',
+        i686: '6175e787dee232ce9120e86dbb5b578c2662959f92ff8e51efb2efdd8d52d915',
+      x86_64: 'ce1fc5eb4f3ba8bf99fab4f4c3b4c9ca2cd2a93c504c0ac0928318b8cdafaeab'
   })
 
-  git_hashtag version
   no_env_options
 
   def self.build
     system './bootstrap.sh'
-    system "CFLAGS='-fuse-ld=gold' LDFLAGS='-flto=auto -static' ./configure #{CREW_OPTIONS}"
+    system "LDFLAGS='-flto=auto -static' ./configure #{CREW_OPTIONS}"
     system 'make'
+  end
+
+  def self.check
+    system "sed -i 's/-flto=auto -static//g' Makefile"
+    system "sed -i 's/-flto=auto -static//g' tests/Makefile"
+    Dir.chdir('tests') do
+      system 'make clean'
+    end
+    system 'make', 'check'
   end
 
   def self.install
