de544719d51d96cd3fbb6593b6b985ebbbffa6cd
Satadru Pramanik <satadru@gmail.com>
2023-03-06 15:30:52 -0500

    update libxft, pango packages

--- packages/libxft.rb
+++ packages/libxft.rb
@@ -3,30 +3,15 @@ require 'package'
 class Libxft < Package
   description 'X.org X FreeType interface library'
   homepage 'https://www.x.org'
-  version '2.3.6'
+  version '2.3.7'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://www.x.org/archive/individual/lib/libXft-2.3.6.tar.xz'
-  source_sha256 '60a6e7319fc938bbb8d098c9bcc86031cc2327b5d086d3335fc5c76323c03022'
-
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxft/2.3.6_armv7l/libxft-2.3.6-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxft/2.3.6_armv7l/libxft-2.3.6-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxft/2.3.6_i686/libxft-2.3.6-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxft/2.3.6_x86_64/libxft-2.3.6-chromeos-x86_64.tar.zst'
-  })
-  binary_sha256({
-    aarch64: '1b3530350b911564546426102b9e98649f8368ee24e84349211e6de8934eb7f9',
-     armv7l: '1b3530350b911564546426102b9e98649f8368ee24e84349211e6de8934eb7f9',
-       i686: '122cf78151a5e1d00ea1dd3a8194818bbcb8faa8d9a223da0d33f62334a5b7fe',
-     x86_64: 'bafef43f2fc7087875145273472f333873cfdd6324bbdf286c603ebf76de0768'
-  })
+  source_url 'https://www.x.org/archive/individual/lib/libXft-2.3.7.tar.xz'
+  source_sha256 '79f0b37c45007381c371a790c2754644ad955166dbf2a48e3625032e9bdd4f71'
 
   depends_on 'libxrender'
   depends_on 'libx11'
-  depends_on 'fontconfig'
   depends_on 'util_macros'
-  depends_on 'freetype' # R
   depends_on 'glibc' # R
   depends_on 'harfbuzz' # R
 
--- packages/pango.rb
+++ packages/pango.rb
@@ -3,29 +3,14 @@ require 'package'
 class Pango < Package
   description 'Pango is a library for laying out and rendering of text, with an emphasis on internationalization.'
   homepage 'https://pango.gnome.org/'
-  @_ver = '1.50.10'
+  @_ver = '1.50.14'
   version @_ver
   license 'LGPL-2+ and FTL'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/pango.git'
   git_hashtag @_ver
 
-  binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.10_armv7l/pango-1.50.10-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.10_armv7l/pango-1.50.10-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.10_i686/pango-1.50.10-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/pango/1.50.10_x86_64/pango-1.50.10-chromeos-x86_64.tar.zst'
-  })
-  binary_sha256({
-    aarch64: '53ef271e640894edfe28064d33001b940e2b696519f58b587bf7abe605a8b2f9',
-     armv7l: '53ef271e640894edfe28064d33001b940e2b696519f58b587bf7abe605a8b2f9',
-       i686: 'd84a0a3efe1f1aefb67d8a88f962db89475e3fc581952734920c2f1b3ecb0ed6',
-     x86_64: 'aea1ea8fdc7d53166a36e983bf092e0a41cb8b7a9133548bc88a964d5219f3bf'
-  })
-
   depends_on 'cairo'
-  depends_on 'fontconfig'
-  depends_on 'freetype'
   depends_on 'fribidi' # Gets built inside install automatically.
   depends_on 'glib'
   depends_on 'gobject_introspection' # add this package to build gtk+, avoid compilation error
@@ -34,9 +19,10 @@ class Pango < Package
   depends_on 'libxdmcp'
   depends_on 'libxft'
   depends_on 'libxrender'
-  depends_on 'py3_six'
   depends_on 'xorg_proto' => :build
   depends_on 'glibc' # R
+  depends_on 'fontconfig' # R
+  depends_on 'freetype' # R
 
   def self.build
     system "meson setup #{CREW_MESON_OPTIONS} \
@@ -49,14 +35,14 @@ class Pango < Package
     # gtk-doc is set to false because I (@saltedcofii) am not
     # convinced anyone is reading them; it's accessable online anyway
     system 'meson configure builddir'
-    system 'mold -run samu -C builddir'
+    system "mold -run #{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 
   def self.check
-    system 'samu -C builddir test || true'
+    system "#{CREW_NINJA} -C builddir test || true"
   end
 end
