f674495d2a80a0ce851d062d513534ac71667a12
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-03-07 23:09:41 -0500

    gtk3 => 3.24.37 (#8051)
    * gtk3 => 3.24.37
    
    * Make sommelier postinstall happen last
    
    * update binaries

--- packages/gtk3.rb
+++ packages/gtk3.rb
@@ -3,25 +3,22 @@ require 'package'
 class Gtk3 < Package
   description 'GTK+ is a multi-platform toolkit for creating graphical user interfaces.'
   homepage 'https://developer.gnome.org/gtk3/3.0/'
-  @_ver = '3.24.34-80d8970'
-  @_ver_prelastdot = @_ver.rpartition('.')[0]
+  @_ver = '3.24.37'
   version @_ver
   license 'LGPL-2.1'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/gtk.git'
-  git_hashtag '80d8970072650c7715c62bef933f8d332525c6c0'
+  git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.34-80d8970_armv7l/gtk3-3.24.34-80d8970-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.34-80d8970_armv7l/gtk3-3.24.34-80d8970-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.34-80d8970_i686/gtk3-3.24.34-80d8970-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.34-80d8970_x86_64/gtk3-3.24.34-80d8970-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.37_armv7l/gtk3-3.24.37-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.37_armv7l/gtk3-3.24.37-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk3/3.24.37_x86_64/gtk3-3.24.37-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '817fabb61fc657cd6521da1daf75b59c4e05c3bb789cf2f288891f24e51cf6c4',
-     armv7l: '817fabb61fc657cd6521da1daf75b59c4e05c3bb789cf2f288891f24e51cf6c4',
-       i686: '559f507efe16ef35f24fb9ca36fc01ad903de6c86039d298a7570e0f4149ac39',
-     x86_64: '1abe7f4880f0c4380413bf39cfea75621c47907047016210d620d988b95c5dc0'
+    aarch64: '3dbf8f2b97f1ea7e0b58bfd07a95be8d8288674a92e084f26688b9e8233d96bc',
+     armv7l: '3dbf8f2b97f1ea7e0b58bfd07a95be8d8288674a92e084f26688b9e8233d96bc',
+     x86_64: 'ef6decef4d5d47c29a99777af6fc20ac9f9fcc3490a0974e512569cd3f75dc9c'
   })
 
   # L = Logical Dependency, R = Runtime Dependency
@@ -34,8 +31,10 @@ class Gtk3 < Package
   depends_on 'fontconfig' # R
   depends_on 'freetype' # R
   depends_on 'fribidi' # R
+  depends_on 'gcc' # R
   depends_on 'gdk_pixbuf' # R
   depends_on 'ghostscript' => :build
+  depends_on 'glibc' # R
   depends_on 'glib' # R
   depends_on 'gnome_icon_theme' # L
   depends_on 'gobject_introspection' => :build
@@ -44,10 +43,11 @@ class Gtk3 < Package
   depends_on 'harfbuzz' # R
   depends_on 'hicolor_icon_theme' # L
   depends_on 'iso_codes' => :build
-  depends_on 'json_glib' # R
+  depends_on 'json_glib' => :build
   depends_on 'libdeflate' => :build # Do we need this?
   depends_on 'libepoxy' # R
   depends_on 'libjpeg' => :build # Do we need this?
+  depends_on 'librsvg' => :build
   depends_on 'libsass' => :build
   depends_on 'libspectre' => :build
   depends_on 'libx11' # R
@@ -62,14 +62,15 @@ class Gtk3 < Package
   depends_on 'libxrandr' # R
   depends_on 'mesa' => :build
   depends_on 'pango' # R
-  depends_on 'rest' # R
+  depends_on 'rest' => :build
   depends_on 'shared_mime_info' # L
   depends_on 'sommelier' # L
   depends_on 'valgrind' => :build
   depends_on 'wayland' # R
   depends_on 'xdg_base' # L
-  depends_on 'glibc' # R
+
   gnome
+  no_fhs
 
   def self.patch
     # Use locally build subprojects
@@ -87,7 +88,7 @@ class Gtk3 < Package
       -Dgtk_doc=false \
       builddir"
     system 'meson configure builddir'
-    system 'ninja -C builddir'
+    system "#{CREW_NINJA} -C builddir"
     @gtk3settings = <<~GTK3_CONFIG_HEREDOC
       [Settings]
       gtk-icon-theme-name = Adwaita
@@ -99,9 +100,9 @@ class Gtk3 < Package
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     system "sed -i 's,null,,g'  #{CREW_DEST_LIB_PREFIX}/pkgconfig/gtk*.pc"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/gtk-3.0"
-    File.write("#{CREW_DEST_PREFIX}/etc/gtk-3.0/settings.ini", @gtk3settings)
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/.config/gtk-3.0"
+    File.write("#{CREW_DEST_PREFIX}/.config/gtk-3.0/settings.ini", @gtk3settings)
   end
 end
