ed55862770f09274c3c9e7ac9ee61882e36fe97c
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-03-15 10:49:14 -0400

    cmake => 3.26.0 (#8070)

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,7 +3,7 @@ require 'package'
 class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  @_ver = '3.25.3'
+  @_ver = '3.26.0'
   version @_ver
   license 'CMake'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Cmake < Package
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.25.3_armv7l/cmake-3.25.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.25.3_armv7l/cmake-3.25.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.25.3_i686/cmake-3.25.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.25.3_x86_64/cmake-3.25.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.0_armv7l/cmake-3.26.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.0_armv7l/cmake-3.26.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.0_i686/cmake-3.26.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.0_x86_64/cmake-3.26.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '4911ff7281c642c3fdb810743ebaaa16ba47d2f001c97557070da05b20359b3b',
-     armv7l: '4911ff7281c642c3fdb810743ebaaa16ba47d2f001c97557070da05b20359b3b',
-       i686: '3c9347271d0626df4cda13c8f5b3fa238db3a9d7d3668b99a5e74d5b1dc38e3a',
-     x86_64: '50ada85d5eb8b571bfb3ede09e5bb9fc83669346bfb447568d704705d27ed080'
+    aarch64: 'c733cd92e32108da4b329d3bfc7a8915a6d6e192ad70bc104700a469ad775b0e',
+     armv7l: 'c733cd92e32108da4b329d3bfc7a8915a6d6e192ad70bc104700a469ad775b0e',
+       i686: '8cb8e78bd8e1fdab7f38dcd2761b4d0a00c3731ea9a7ba0d07894c7a7888f579',
+     x86_64: 'ae046518c25ab673477f56066998d9e85ad8d1a28b8de243177af86f9087c398'
   })
 
   depends_on 'expat'
@@ -40,29 +40,25 @@ class Cmake < Package
   depends_on 'ncurses' # R
 
   def self.build
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "mold -run cmake \
+    system "mold -run cmake -B builddir \
           -G Ninja \
           #{CREW_CMAKE_OPTIONS} \
           -DCMAKE_USE_SYSTEM_LIBRARIES=ON \
-          -DBUILD_QtDialog=NO \
-          .."
-    end
-    system 'mold -run ninja -C builddir'
+          -DBUILD_QtDialog=NO"
+    system "mold -run #{CREW_NINJA} -C builddir"
   end
 
   # Failed tests:
   # BundleUtilities (armv7l,x86_64)
-  # CTest.UpdateGIT (x86_64)
-  # CustomCommand (armv7l,x86_64)
+  # BootstrapTest (armv7l,i686,x86_64)
+  # CustomCommand (armv7l)
   # RunCMake.CMakeRelease (armv7l,i686,x86_64)
   def self.check
-    system 'ninja -C builddir test || true'
+    system "#{CREW_NINJA} -C builddir test || true"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     FileUtils.mv "#{CREW_DEST_PREFIX}/doc/", "#{CREW_DEST_PREFIX}/share/"
   end
 end
