392ff65661f01f08225f10f30da8761e20e972f8
Satadru Pramanik <satadru@gmail.com>
2023-03-15 15:52:12 -0400

    lint

--- lib/const.rb
+++ lib/const.rb
@@ -139,7 +139,7 @@ CHROMEOS_RELEASE = if File.exist?('/etc/lsb-release')
                    end
 
 # If CREW_DISABLE_MVDIR environment variable exists and is equal to 1 use rsync/tar to install files in lieu of crew-mvdir.
-CREW_DISABLE_MVDIR = ENV['CREW_DISABLE_MVDIR'] != "0" ? true : false
+CREW_DISABLE_MVDIR = ENV['CREW_DISABLE_MVDIR'] != '0'
 
 # If CREW_USE_CURL environment variable exists use curl in lieu of net/http.
 CREW_USE_CURL = ENV['CREW_USE_CURL'].eql?('1')
