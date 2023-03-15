a56db8ee4b8680a5a55fb04679f2c7a9e34b9923
Satadru Pramanik <satadru@gmail.com>
2023-03-14 20:13:18 -0400

    Jellyfin Media Player => 1.9.0 (plus update shaderc dep)

--- packages/jellyfin_media_player.rb
+++ packages/jellyfin_media_player.rb
@@ -6,22 +6,22 @@ require 'package'
 class Jellyfin_media_player < Package
   description 'Jellyfin Desktop Client'
   homepage 'https://github.com/jellyfin/jellyfin-media-player'
-  version '1.7.1'
+  version '1.9.0'
   license 'GPL'
-  compatibility 'all'
-  source_url 'https://github.com/jellyfin/jellyfin-media-player/archive/refs/tags/v1.7.1.tar.gz'
-  source_sha256 '5139cf7128673c053fcc5db7158ee696ccfd7623e133a21c9a2377b66a37efb3'
+  compatibility 'x86_64'
+  source_url 'https://github.com/jellyfin/jellyfin-media-player/archive/refs/tags/v1.9.0.tar.gz'
+  source_sha256 '366aac5a355d9dd435037442e0fff091a85019ea8b27ce6db3f957c8dd54d1ca'
 
   binary_url({
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/jellyfin_media_player/1.7.1_x86_64/jellyfin_media_player-1.7.1-chromeos-x86_64.tar.zst'
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/jellyfin_media_player/1.9.0_x86_64/jellyfin_media_player-1.9.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    x86_64: '065fe6baa1172b33554a73a50ab62d2a4c8f5885a56486a7329333307673be34'
+     x86_64: '04f33ba8a02ba9dde7c4a2605fa243d1a569d1f9daadf43c9818bdd9aa853d64'
   })
 
   depends_on 'gcc' # R
   depends_on 'glibc' # R
-  depends_on 'libcec' unless ARCH == 'i686'
+  depends_on 'libcec' # R
   depends_on 'libglvnd' # R
   depends_on 'libice' # R
   depends_on 'libsdl2' # R
@@ -30,37 +30,34 @@ class Jellyfin_media_player < Package
   depends_on 'libxext' # R
   depends_on 'libxrandr' # R
   depends_on 'mpv' # R
-  depends_on 'p8_platform' unless ARCH == 'i686'
+  depends_on 'p8_platform' # R
   depends_on 'protobuf' => :build
   depends_on 'qtbase' # R
   depends_on 'qtdeclarative' # R
   depends_on 'qtlocation' # R
-  depends_on 'qtquickcontrols'
+  depends_on 'qtquickcontrols' => :build
   depends_on 'qtwayland' # R
   depends_on 'qtwebchannel' # R
   depends_on 'qtwebengine' # R
   depends_on 'qtx11extras' # R
+  depends_on 'shaderc' # L
   depends_on 'sommelier' # L
 
   def self.build
     system './download_webclient.sh'
-    FileUtils.mkdir_p 'build'
-    Dir.chdir 'build' do
-      system "cmake -G Ninja \
-        #{CREW_CMAKE_OPTIONS} \
-        -DLINUX_X11POWER=ON \
-        -DQTROOT=./qt \
-        -DCMAKE_SKIP_RPATH=1 \
-        -Wno-dev \
-        .."
-    end
-    system 'ninja -C build'
+    system "cmake -B builddir -G Ninja \
+      #{CREW_CMAKE_OPTIONS} \
+      -DLINUX_X11POWER=ON \
+      -DQTROOT=./qt \
+      -DCMAKE_SKIP_RPATH=1 \
+      -Wno-dev"
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C build install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d"
-    File.write "#{CREW_DEST_PREFIX}/etc/env.d/10-jellyfinmediaplayer", <<~'JELLYFIN_ENVD_EOF'
+    File.write "#{CREW_DEST_PREFIX}/etc/env.d/10-jellyfinmediaplayer", <<~JELLYFIN_ENVD_EOF
       # QT_QPA_PLATFORM=wayland is unaccelerated and unusable.
       alias jellyfinmediaplayer="QT_QPA_PLATFORM=eglfs jellyfinmediaplayer"
     JELLYFIN_ENVD_EOF
--- packages/shaderc.rb
+++ packages/shaderc.rb
@@ -5,49 +5,42 @@ require 'package'
 
 class Shaderc < Package
   description 'Collection of tools, libraries and tests for shader compilation'
-  version '2020.5'
+  version '2023.2'
   license 'Apache'
   compatibility 'all'
-  source_url "https://github.com/google/shaderc/archive/refs/tags/v#{version}.tar.gz"
-  source_sha256 'e96d8cb208b796cecb9e6cce437c7d1116343158ef3ea26277eb13b62cf56834'
+  source_url 'https://github.com/google/shaderc.git'
+  git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2020.5_armv7l/shaderc-2020.5-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2020.5_armv7l/shaderc-2020.5-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2020.5_i686/shaderc-2020.5-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2020.5_x86_64/shaderc-2020.5-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2023.2_armv7l/shaderc-2023.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2023.2_armv7l/shaderc-2023.2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2023.2_i686/shaderc-2023.2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shaderc/2023.2_x86_64/shaderc-2023.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '74e0264a8277b173f33e04e84a31a118173c2f4c5013ddd9f68f51a4a38b0965',
-     armv7l: '74e0264a8277b173f33e04e84a31a118173c2f4c5013ddd9f68f51a4a38b0965',
-       i686: '061996cc65ec0c60bd26a09b26e5d949504d2c6718eae3531949b4ccf9d2da3f',
-     x86_64: '9e32e0db031138d5ead88e9e6a99f1a14d1d07e6c89999f0d336d105462b1306'
+    aarch64: '82dae8a8ea92e6ddeee954db3ed57c61dc8f6fbd5cad222e1a1a656525b85be0',
+     armv7l: '82dae8a8ea92e6ddeee954db3ed57c61dc8f6fbd5cad222e1a1a656525b85be0',
+       i686: '894583bffe1d861a0bb56b9a7f7ad04769d296b7e4433abe404c6661359b0129',
+     x86_64: '35cb29eb703b562c83a749d9427c10e80a47cc734ade0d233f1163ac6b5c9fff'
   })
 
   depends_on 'ruby_asciidoctor' => :build
+  depends_on 'gcc' # R
+  depends_on 'glibc' # R
 
   def self.build
     system './utils/git-sync-deps'
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "env CFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      CXXFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      LDFLAGS='-fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      cmake \
-        -G Ninja \
+    system "cmake -B builddir -G Ninja \
         #{CREW_CMAKE_OPTIONS} \
         -DSPIRV_WERROR=Off \
         -DBUILD_SHARED_LIBS=ON \
         -DSHADERC_SKIP_TESTS=ON \
         -DSHADERC_ENABLE_EXAMPLES=OFF \
-        -DCMAKE_CXX_FLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-        -Wno-dev \
-        .."
-    end
-    system 'ninja -C builddir'
+        -Wno-dev"
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
