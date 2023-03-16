4c8a31bee759299a22a93fca1dd454804a020b83
Satadru Pramanik <satadru@gmail.com>
2023-03-16 11:33:11 -0400

    transmission => 4.0.2

--- packages/transmission.rb
+++ packages/transmission.rb
@@ -3,57 +3,61 @@ require 'package'
 class Transmission < Package
   description 'A fast, easy, and free BitTorrent client.'
   homepage 'https://transmissionbt.com/'
-  version '4.0.1'
+  version '4.0.2'
   license 'GPL-2, GPL-3 or Transmission-OpenSSL-exception, GPL-2 and MIT'
-  compatibility 'aarch64,armv7l,x86_64'
-  source_url 'https://github.com/transmission/transmission/releases/download/4.0.1/transmission-4.0.1.tar.xz'
-  source_sha256 '8fc5aef23638c983406f6a3ee9918369e4cdc84e3228bd2fb3d01dd55cdad900'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://github.com/transmission/transmission/releases/download/4.0.2/transmission-4.0.2.tar.xz'
+  source_sha256 '39bf7a104a722805a9dc089cdaaffe33bf90b82230a7ea7f340cae59f00a2ee8'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/transmission/4.0.1_armv7l/transmission-4.0.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/transmission/4.0.1_armv7l/transmission-4.0.1-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/transmission/4.0.1_x86_64/transmission-4.0.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/transmission/4.0.2_armv7l/transmission-4.0.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/transmission/4.0.2_armv7l/transmission-4.0.2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/transmission/4.0.2_x86_64/transmission-4.0.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a24a9d056da0fa9285b8e09c03176fbf7e088632075b7ac94211fb1aa56d825b',
-     armv7l: 'a24a9d056da0fa9285b8e09c03176fbf7e088632075b7ac94211fb1aa56d825b',
-     x86_64: 'e0892a64138e17054f34b4acdda32f37aee610eed50ad3a7f4da12d71b0bae46'
+    aarch64: 'cf3fe4b1eee0677f3fbfcc275dc18eebcb9379c46c66e1f1edf0c067a92f9150',
+     armv7l: 'cf3fe4b1eee0677f3fbfcc275dc18eebcb9379c46c66e1f1edf0c067a92f9150',
+     x86_64: '250988507ff257b2815543dd9caafc7c1b3b7f3ab02aab275e7cef0f1bf088a6'
   })
 
-  depends_on 'at_spi2_core'
-  depends_on 'cairo'
-  depends_on 'freetype' => :build
-  depends_on 'gdk_pixbuf'
-  depends_on 'glib'
-  depends_on 'glibmm'
-  depends_on 'gnome_icon_theme'
-  depends_on 'gtk4'
-  depends_on 'gtkmm4'
-  depends_on 'harfbuzz'
-  depends_on 'hicolor_icon_theme'
-  depends_on 'libevent'
-  depends_on 'libutp'
-  depends_on 'miniupnpc'
-  depends_on 'pango'
+  depends_on 'at_spi2_core' => :build
+  depends_on 'cairomm_1_16' # R
+  depends_on 'gcc' # R
+  depends_on 'gdk_pixbuf' # R
+  depends_on 'glibc' # R
+  depends_on 'glibmm_2_68' # R
+  depends_on 'glib' # R
+  depends_on 'gnome_icon_theme' # L
+  depends_on 'graphene' # R
+  depends_on 'gtk4' # R
+  depends_on 'gtkmm4' # R
+  depends_on 'harfbuzz' # R
+  depends_on 'hicolor_icon_theme' # L
+  depends_on 'libcurl' # R
+  depends_on 'libdeflate' # R
+  depends_on 'libevent' # R
+  depends_on 'libpsl' # R
+  depends_on 'libsigcplusplus3' # R
+  depends_on 'libutp' # L
+  depends_on 'miniupnpc' # R
+  depends_on 'openssl' # R
+  depends_on 'pangomm_2_48' # R
   depends_on 'shared_mime_info'
-  depends_on 'vulkan_headers'
-  depends_on 'xdg_base'
-  depends_on 'sommelier'
+  depends_on 'vulkan_headers' => :build
+  depends_on 'vulkan_icd_loader' # R
+  depends_on 'xdg_base' # L
+  depends_on 'pango' # R
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system "cmake \
-        #{CREW_CMAKE_OPTIONS} \
-        -DENABLE_CLI=ON \
-        -DENABLE_GTK=ON \
-        -DENABLE_UTILS=ON \
-        -DINSTALL_LIB=ON \
-        -DINSTALL_DOC=ON \
-        -DUSE_GTK_VERSION=4 \
-        .."
-      system 'make'
-    end
+    system "cmake -B builddir -G Ninja \
+      #{CREW_CMAKE_OPTIONS} \
+      -DENABLE_CLI=ON \
+      -DENABLE_GTK=ON \
+      -DENABLE_UTILS=ON \
+      -DINSTALL_LIB=ON \
+      -DINSTALL_DOC=ON \
+      -DUSE_GTK_VERSION=4"
+    system "#{CREW_NINJA} -C builddir"
     starttransmission = <<~EOF
       #!/bin/bash
       TRAN=$(pidof transmission-daemon 2> /dev/null)
@@ -96,9 +100,7 @@ class Transmission < Package
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/bash.d"
     FileUtils.install 'starttransmission', "#{CREW_DEST_PREFIX}/bin", mode: 0o755
