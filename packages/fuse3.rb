25881ffa7c2a26052890d5271a2c58879dc4a1a9
Satadru Pramanik <satadru@gmail.com>
2023-03-20 16:29:28 -0400

    update fuse3, gvfs

--- packages/fuse3.rb
+++ packages/fuse3.rb
@@ -3,7 +3,7 @@ require 'package'
 class Fuse3 < Package
   description 'The reference implementation of the Linux FUSE (Filesystem in Userspace) interface.'
   homepage 'https://github.com/libfuse/libfuse/'
-  @_ver = '3.13.0'
+  @_ver = '3.14.0'
   version @_ver
   license 'GPL-2+'
   compatibility 'all'
@@ -11,16 +11,16 @@ class Fuse3 < Package
   git_hashtag "fuse-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.13.0_armv7l/fuse3-3.13.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.13.0_armv7l/fuse3-3.13.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.13.0_i686/fuse3-3.13.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.13.0_x86_64/fuse3-3.13.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.14.0_armv7l/fuse3-3.14.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.14.0_armv7l/fuse3-3.14.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.14.0_i686/fuse3-3.14.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fuse3/3.14.0_x86_64/fuse3-3.14.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '4c1ab96deec6a400b49fdd66b6fc987d91448e55c195dc99e4fe73f581e0472c',
-     armv7l: '4c1ab96deec6a400b49fdd66b6fc987d91448e55c195dc99e4fe73f581e0472c',
-       i686: '764d0748f621b8662af616b2a0f660a2af0af7af0eded78abbafa1811e72bb44',
-     x86_64: '39d22e7a3161362766214d68bd78fb53ae61d4edf9a5c03f422f2a00c8065710'
+    aarch64: '5f4b9c7ae2b8f94e0e9e745beb28d44ab2eacc0780eafb39f5bbfc6698498736',
+     armv7l: '5f4b9c7ae2b8f94e0e9e745beb28d44ab2eacc0780eafb39f5bbfc6698498736',
+       i686: '7a123c98ec3ca9d47692119ed47aa573ebc10e553edfac86f38e258357bd2d7b',
+     x86_64: '342b9a27abb38fff56ddc6f99635849d1eba12ff241ad8514125a418f470b61b'
   })
 
   depends_on 'py3_pytest' => :build
@@ -35,11 +35,11 @@ class Fuse3 < Package
       -Duseroot=false \
       builddir"
     system 'meson configure builddir'
-    system 'samu -C builddir'
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/sbin"
     Dir.chdir "#{CREW_DEST_PREFIX}/sbin" do
       FileUtils.ln_s '../bin/fusermount3', 'fusermount3'
