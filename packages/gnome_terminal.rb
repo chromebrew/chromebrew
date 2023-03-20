4f343d2af68ea3d57d6ca61e019314b187640718
Satadru Pramanik <satadru@gmail.com>
2023-03-20 17:45:49 -0400

    update gnome_terminal

--- packages/gnome_terminal.rb
+++ packages/gnome_terminal.rb
@@ -3,30 +3,28 @@ require 'package'
 class Gnome_terminal < Package
   description 'The GNOME Terminal Emulator'
   homepage 'https://wiki.gnome.org/Apps/Terminal'
-  @_ver = '3.46.8'
+  @_ver = '3.48.0'
   version @_ver
   license 'GPL-3+'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/gnome-terminal.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_terminal/3.46.8_armv7l/gnome_terminal-3.46.8-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_terminal/3.46.8_armv7l/gnome_terminal-3.46.8-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_terminal/3.46.8_i686/gnome_terminal-3.46.8-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_terminal/3.46.8_x86_64/gnome_terminal-3.46.8-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_terminal/3.48.0_armv7l/gnome_terminal-3.48.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_terminal/3.48.0_armv7l/gnome_terminal-3.48.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gnome_terminal/3.48.0_x86_64/gnome_terminal-3.48.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '38a6da5d7cf8f4a00f72f6b70341db0dd4ca123724cc41e77d3a01c1a6f45a16',
-     armv7l: '38a6da5d7cf8f4a00f72f6b70341db0dd4ca123724cc41e77d3a01c1a6f45a16',
-       i686: '70ea134e60c8bb05e627eaf86bb12183553dec7df7c8b3268dd179f0cdb7b8e2',
-     x86_64: '1f7e3dd155ef327748ec1efabcd2380c5782d6a358f082f3f11b56043bb1a95b'
+    aarch64: '0f2aaae2cb3b2bcfcc00df7e03213a079a74b3cd9e1f6c26507fde0ef2a209b3',
+     armv7l: '0f2aaae2cb3b2bcfcc00df7e03213a079a74b3cd9e1f6c26507fde0ef2a209b3',
+     x86_64: '5b1844c860a4b5b05da151525d045b22b8668c9150fac8c854e538d46604182e'
   })
 
   depends_on 'adobe_source_code_pro_fonts' # (Needed for monospace fonts)
   depends_on 'at_spi2_core' # R
-  depends_on 'dconf' => :build
   depends_on 'dbus' # L
+  depends_on 'dconf' => :build
   depends_on 'desktop_file_utilities' => :build
   depends_on 'gcc' # R
   depends_on 'glibc' # R
