561908b400ad45eea205a22d09ecb87a573479b7
Satadru Pramanik <satadru@gmail.com>
2023-03-20 10:29:45 -0400

    update tracker3

--- packages/tracker3.rb
+++ packages/tracker3.rb
@@ -6,40 +6,42 @@ require 'package'
 class Tracker3 < Package
   description 'Desktop-neutral user information store, search tool and indexer'
   homepage 'https://wiki.gnome.org/Projects/Tracker'
-  version '3.4.0.rc'
+  version '3.5.0'
   license 'GPLv2+'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/tracker.git'
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.4.0.rc_armv7l/tracker3-3.4.0.rc-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.4.0.rc_armv7l/tracker3-3.4.0.rc-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.4.0.rc_i686/tracker3-3.4.0.rc-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.4.0.rc_x86_64/tracker3-3.4.0.rc-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.5.0_armv7l/tracker3-3.5.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.5.0_armv7l/tracker3-3.5.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.5.0_i686/tracker3-3.5.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3/3.5.0_x86_64/tracker3-3.5.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'abdf19bdb2bd2744dd67cb08f1518981e9b7480fb9b9c8a5e0ba839fee599473',
-     armv7l: 'abdf19bdb2bd2744dd67cb08f1518981e9b7480fb9b9c8a5e0ba839fee599473',
-       i686: '24b33cf2d548e0248033de66875e7325f255b1d466437030fbda8832457b5d7b',
-     x86_64: 'e23b3ea2372e41898ee5a8e5eb38aaee70b3d19511513c8af74ca643603e2490'
+    aarch64: '3a8b373d7264681df1abd2cab31df0b4b55b2b094ecf3418d4a4daefca2b1e5b',
+     armv7l: '3a8b373d7264681df1abd2cab31df0b4b55b2b094ecf3418d4a4daefca2b1e5b',
+       i686: '63eac259d495420d057634e1eba55c6ab3d1e4c63e25cfc37b75a224e0b78c2b',
+     x86_64: '24773226fab94d1f5fc9f9da4fa8e347d790717be6177451696bb700105aaf38'
   })
 
-  depends_on 'asciidoc'
-  depends_on 'docbook_xml'
-  depends_on 'glib'
+  depends_on 'asciidoc' => :build
+  depends_on 'docbook_xml' => :build
+  depends_on 'gcc' # R
   depends_on 'glibc' # R
+  depends_on 'glib' # R
   depends_on 'gobject_introspection' => :build
   depends_on 'gtk_doc' => :build
-  depends_on 'json_glib'
-  depends_on 'libsoup'
-  depends_on 'libsoup2'
-  depends_on 'libstemmer'
-  depends_on 'util_linux'
-  depends_on 'vala' => :build
   depends_on 'icu4c' # R
+  depends_on 'json_glib' # R
+  depends_on 'libsoup2' # R
+  depends_on 'libsoup' # R
+  depends_on 'libstemmer' # R
   depends_on 'libxml2' # R
+  depends_on 'pygobject' => :build
   depends_on 'sqlite' # R
+  depends_on 'util_linux' => :build
+  depends_on 'vala' => :build
 
   def self.build
     system "meson setup #{CREW_MESON_OPTIONS} \
@@ -49,10 +51,10 @@ class Tracker3 < Package
       -Dsystemd_user_services=false \
       builddir"
     system 'meson configure builddir'
-    system 'ninja -C builddir'
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
--- packages/tracker3_miners.rb
+++ packages/tracker3_miners.rb
@@ -6,23 +6,21 @@ require 'package'
 class Tracker3_miners < Package
   description 'Collection of data extractors for Tracker/Nepomuk'
   homepage 'https://wiki.gnome.org/Projects/Tracker'
-  version '3.4.0.rc'
+  version '3.5.0'
   license 'GPLv2+'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url "https://gitlab.gnome.org/GNOME/tracker-miners/-/archive/#{version}/tracker-miners-#{version}.tar.bz2"
-  source_sha256 '85dd84e76803d2df0c7cab8ee82c32ce0349f9784039571604dc4def00dfa85d'
+  source_sha256 '6bc573140e379ef1fd9040adcda234fd8da6586622878a67f0f4ec75bd2cc170'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3_miners/3.4.0.rc_armv7l/tracker3_miners-3.4.0.rc-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3_miners/3.4.0.rc_armv7l/tracker3_miners-3.4.0.rc-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3_miners/3.4.0.rc_i686/tracker3_miners-3.4.0.rc-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3_miners/3.4.0.rc_x86_64/tracker3_miners-3.4.0.rc-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3_miners/3.5.0_armv7l/tracker3_miners-3.5.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3_miners/3.5.0_armv7l/tracker3_miners-3.5.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tracker3_miners/3.5.0_x86_64/tracker3_miners-3.5.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '6b156051e5c37973ebe6d36e3cf9cea32823837c708918395a8be139101847bc',
-     armv7l: '6b156051e5c37973ebe6d36e3cf9cea32823837c708918395a8be139101847bc',
-       i686: '52db658c8903f7ad1d2586627438326265353e54ea6d7e956aba07e2e5a277d5',
-     x86_64: 'e7799b7a989501ce2fa23203becdae5a174747c4f6ca563d0e2c7e50646b1dfa'
+    aarch64: '44885f8b09a3d7469b7fdee754ce9893dd1103680d6e83bc0134180ee74844d6',
+     armv7l: '44885f8b09a3d7469b7fdee754ce9893dd1103680d6e83bc0134180ee74844d6',
+     x86_64: '5b1961229a1ef4284241dbc6b2f9ca1487f6daf841f0a027d3f24b3abf781213'
   })
 
   depends_on 'asciidoc' => :build
@@ -31,33 +29,33 @@ class Tracker3_miners < Package
   depends_on 'giflib' # R
   depends_on 'glibc' # R
   depends_on 'glib' # R
-  depends_on 'glibc' # R
   depends_on 'gstreamer' # R
   depends_on 'icu4c' # R
   depends_on 'libcue' => :build
   depends_on 'libexif' # R
   depends_on 'libgrss' # R
   depends_on 'libgsf' # R
-  depends_on 'libjpeg'
+  depends_on 'libjpeg' # R
   depends_on 'libpng' # R
   depends_on 'libseccomp' # R
   depends_on 'libtiff' # R
   depends_on 'libxml2' # R
   depends_on 'tracker3' # R
-  depends_on 'vala' => :build
   depends_on 'util_linux' # R
+  depends_on 'vala' => :build
 
   def self.build
     system "meson setup #{CREW_MESON_OPTIONS} \
       -Ddbus_services_dir=#{CREW_PREFIX}/share/dbus-1/services/ \
+      -Dbattery_detection=none \
       -Dman=false \
       -Dsystemd_user_services=false \
       builddir"
     system 'meson configure builddir'
-    system 'ninja -C builddir'
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
