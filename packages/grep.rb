cfa7c4b3e73a437c0da45cf24f5c03c11ec18783
Ed Reel <edreel@gmail.com>
2023-03-23 17:02:55 -0500

    Grep 3.9 => 3.10

--- packages/grep.rb
+++ packages/grep.rb
@@ -6,23 +6,23 @@ require 'package'
 class Grep < Package
   description 'A string search utility'
   homepage 'https://www.gnu.org/software/grep/'
-  version '3.9'
+  version '3.10'
   license 'GPL-3+'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/grep/grep-3.9.tar.xz'
-  source_sha256 'abcd11409ee23d4caf35feb422e53bbac867014cfeed313bb5f488aca170b599'
+  source_url 'https://ftpmirror.gnu.org/grep/grep-3.10.tar.xz'
+  source_sha256 '24efa5b595fb5a7100879b51b8868a0bb87a71c183d02c4c602633b88af6855b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.9_armv7l/grep-3.9-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.9_armv7l/grep-3.9-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.9_i686/grep-3.9-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.9_x86_64/grep-3.9-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.10_armv7l/grep-3.10-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.10_armv7l/grep-3.10-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.10_i686/grep-3.10-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/grep/3.10_x86_64/grep-3.10-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a6154af6e4abc36435d339cd91b826b9c8f535f022ed8e69e3319f691f300905',
-     armv7l: 'a6154af6e4abc36435d339cd91b826b9c8f535f022ed8e69e3319f691f300905',
-       i686: 'beb06a03aa72c063905589811b3e7c917d149bb476703c9012e56e965076cede',
-     x86_64: 'a2162b32e3d4042f82583edeccfcdbb2aad61790c54e7169f758b45b179b42d9'
+    aarch64: '1c09b2c36f6ac56d0e5bbab737e4d5d24ef45c21e2f9b9ca581137c80d6209d9',
+     armv7l: '1c09b2c36f6ac56d0e5bbab737e4d5d24ef45c21e2f9b9ca581137c80d6209d9',
+       i686: '345ac10320793cccad6d5b817cb15e1853c135f4235dc532b66d2972fba9a698',
+     x86_64: '44ab3681e82fcfd16e97894538a84473ad5669d323db0277d81aaf7ccf49cf8a'
   })
 
   depends_on 'pcre'
