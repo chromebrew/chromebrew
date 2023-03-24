9f9c952a2de26f5891977878960108b03eea67e1
Satadru Pramanik <satadru@gmail.com>
2023-03-24 18:29:04 -0400

    lint

--- packages/balena_etcher.rb
+++ packages/balena_etcher.rb
@@ -9,11 +9,11 @@ class Balena_etcher < Package
   compatibility 'x86_64'
 
   source_url({
-    x86_64: "https://github.com/balena-io/etcher/releases/download/v#{@_ver}/balenaEtcher-#{@_ver}-x64.AppImage",
+    x86_64: "https://github.com/balena-io/etcher/releases/download/v#{@_ver}/balenaEtcher-#{@_ver}-x64.AppImage"
   })
 
   source_sha256({
-    x86_64: 'a21e64e6709c8741a5283d915c4acfa89643a901ceb9e7589d7a50b29a74f4d9',
+    x86_64: 'a21e64e6709c8741a5283d915c4acfa89643a901ceb9e7589d7a50b29a74f4d9'
   })
 
   no_compile_needed
@@ -32,18 +32,17 @@ class Balena_etcher < Package
   depends_on 'sommelier'
 
   def self.build
-    etcher = <<~EOF
+    @etcher = <<~EOF
       #!/bin/bash
       xhost si:localuser:root
       cd #{CREW_PREFIX}/share/balena-etcher
       sudo -E LD_LIBRARY_PATH=#{CREW_LIB_PREFIX} ./AppRun "$@"
     EOF
-    File.write('etcher.sh', etcher)
+    File.write('etcher.sh', @etcher)
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/balena-etcher"
+    FileUtils.mkdir_p %W[#{CREW_DEST_PREFIX}/bin #{CREW_DEST_PREFIX}/share/balena-etcher]
     FileUtils.install 'etcher.sh', "#{CREW_DEST_PREFIX}/bin/etcher", mode: 0o755
     FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/balena-etcher"
   end
