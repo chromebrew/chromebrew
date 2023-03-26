7ff9a4b43e4c9b8d1da2d4f9eb0b61d0e1211969
Ed Reel <edreel@gmail.com>
2023-03-26 12:06:54 -0500

    Symfony 4.28.1 => 5.5.2

--- packages/symfony.rb
+++ packages/symfony.rb
@@ -3,36 +3,28 @@ require 'package'
 class Symfony < Package
   description 'Symfony is a set of PHP Components, a Web Application framework'
   homepage 'https://symfony.com/'
-  version '4.28.1'
+  version '5.5.2'
   license 'Symfony-cli-EULA'
   compatibility 'all'
-  source_url 'SKIP'
+  source_url({
+    aarch64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_armv6.tar.gz',
+     armv7l: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_armv6.tar.gz',
+       i686: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_386.tar.gz',
+     x86_64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_amd64.tar.gz'
+  })
+  source_sha256({
+    aarch64: '987def27a646ef0f10b47f08149308f635db52dfe74d665d6298daf8d2d8c11c',
+     armv7l: '987def27a646ef0f10b47f08149308f635db52dfe74d665d6298daf8d2d8c11c',
+       i686: '986d9008af590ead3902a5a490c2183e34284c08cd4d57ad55fc1e82f7c2f05d',
+     x86_64: 'ff2966767d14e8c23df9efa16ec13a0b509c30733f354cc1291a14bd3f7d9983'
+  })
 
-  depends_on 'php74' unless File.exist? "#{CREW_PREFIX}/bin/php"
+  depends_on 'php81' unless File.exist? "#{CREW_PREFIX}/bin/php"
+
+  no_compile_needed
 
   def self.install
-    case ARCH
-    when 'aarch64', 'armv7l'
-      arch = 'arm'
-      sha256 = '01a872cdfd33db4216e73e6c9a56ef4c63799d1feb119e57c51141aca121b85c'
-    when 'i686'
-      arch = '386'
-      sha256 = 'c51c2be1b1f82c572777fd5fc2bb55e553c58309f7ffc7b14a1aace02e2359e8'
-    when 'x86_64'
-      arch = 'amd64'
-      sha256 = '66c2daf21e3acbdda8d826a0484b02e59255401d54027cdbe2605406f77933a8'
-    end
-    symfony_file = "symfony_linux_#{arch}"
-    downloader "https://github.com/symfony/cli/releases/download/v#{version}/#{symfony_file}", sha256
-
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.install symfony_file, "#{CREW_DEST_PREFIX}/bin/symfony", mode: 0o755
-  end
-
-  def self.postinstall
-    puts
-    puts 'Installing shell auto-completion...'.lightblue
-    system 'symfony self:shell-setup --silent'
-    puts
+    FileUtils.install 'symfony', "#{CREW_DEST_PREFIX}/bin/symfony", mode: 0o755
   end
 end
