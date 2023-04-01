d9d470f81ca6ccedbd25ea292221c1fcc316dd28
Ed Reel <edreel@gmail.com>
2023-04-01 10:06:09 -0500

    Rsync 3.2.5 => 3.2.7 (#8136)

--- packages/rsync.rb
+++ packages/rsync.rb
@@ -3,23 +3,23 @@ require 'package'
 class Rsync < Package
   description 'rsync is an open source utility that provides fast incremental file transfer.'
   homepage 'https://rsync.samba.org/'
-  version '3.2.5'
+  version '3.2.7'
   license 'GPL-3'
   compatibility 'all'
-  source_url "http://rsync.samba.org/ftp/rsync/src/rsync-#{@_ver}.tar.gz"
-  source_sha256 '2ac4d21635cdf791867bc377c35ca6dda7f50d919a58be45057fd51600c69aba'
+  source_url 'https://rsync.samba.org/ftp/rsync/src/rsync-3.2.7.tar.gz'
+  source_sha256 '4e7d9d3f6ed10878c58c5fb724a67dacf4b6aac7340b13e488fb2dc41346f2bb'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.5_armv7l/rsync-3.2.5-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.5_armv7l/rsync-3.2.5-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.5_i686/rsync-3.2.5-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.5_x86_64/rsync-3.2.5-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.7_armv7l/rsync-3.2.7-chromeos-armv7l.tar.xz',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.7_armv7l/rsync-3.2.7-chromeos-armv7l.tar.xz',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.7_i686/rsync-3.2.7-chromeos-i686.tar.xz',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/rsync/3.2.7_x86_64/rsync-3.2.7-chromeos-x86_64.tar.xz'
   })
   binary_sha256({
-    aarch64: 'a2b1a6ed4614b1fd9bb41914669bb30783d70ad5719c13760ed0fbce9be0aae3',
-     armv7l: 'a2b1a6ed4614b1fd9bb41914669bb30783d70ad5719c13760ed0fbce9be0aae3',
-       i686: 'f026e547aeeb952b75df5e710f64e6cb54a7ad6a845f3443dc601d93b1bed31e',
-     x86_64: '02db82ac18ab11be2e081cb1c909d93694eeb78e7b5d198287b1b872095912c0'
+    aarch64: 'c3d7f3a8422896e5c0695d9b31415631fecac77402448297e461c39fc0207a7d',
+     armv7l: 'c3d7f3a8422896e5c0695d9b31415631fecac77402448297e461c39fc0207a7d',
+       i686: 'b45b006511bb54f04504a2b38983948cf9423f80484c8d39df3713b945c77cd8',
+     x86_64: '1186347920dbad663f280a059bda3bbc1bf5dabc4a6f5dcc4befa20e9a01265e'
   })
 
   depends_on 'acl' # R
@@ -31,6 +31,7 @@ class Rsync < Package
   depends_on 'py3_cmarkgfm' => :build
   depends_on 'xxhash' # R
   depends_on 'zstd' # R
+
   no_patchelf
   no_zstd
 
