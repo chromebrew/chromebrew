431a5bf5e22c9fbe15d21d2ab369721ffcba4c66
Ed Reel <edreel@gmail.com>
2023-04-06 11:55:46 -0500

    Signal_desktop 6.11.0 => 6.13.0

--- packages/signal_desktop.rb
+++ packages/signal_desktop.rb
@@ -3,11 +3,11 @@ require 'package'
 class Signal_desktop < Package
   description 'Private Messenger for Windows, Mac, and Linux'
   homepage 'https://signal.org/'
-  version '6.11.0'
+  version '6.13.0'
   license 'AGPL-3.0'
   compatibility 'x86_64'
-  source_url 'https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_6.11.0_amd64.deb'
-  source_sha256 '94e7395b75d8116c1327dfd4d9c3131fddf542fd9eeddfbdbe89a72bd0e3d52d'
+  source_url 'https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_6.13.0_amd64.deb'
+  source_sha256 '62758fadb0b9a7f6388137ca8dc04d3fceb0ac3d37d51b4425bde042dd46a94f'
 
   no_compile_needed
 
