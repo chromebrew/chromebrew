fa7ef416139e4a4dd7fa49c525312048e24c5791
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-04-06 15:05:27 -0400

    Refresh geany & geany plugins (#8159)

--- packages/geany_plugins.rb
+++ packages/geany_plugins.rb
@@ -3,44 +3,49 @@ require 'package'
 class Geany_plugins < Package
   description 'Geany plugins.'
   homepage 'https://www.geany.org/'
-  version 'a76f728'
+  version '1.38-3ac62e2'
   license 'GPL-2'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/geany/geany-plugins.git'
-  git_hashtag 'a76f728f52fe7d277f1c226a255c87a2cf6d8fe8'
+  git_hashtag '3ac62e2c81cba1fe59d7fae1db8ea910367cd589'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany_plugins/a76f728_armv7l/geany_plugins-a76f728-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany_plugins/a76f728_armv7l/geany_plugins-a76f728-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany_plugins/a76f728_i686/geany_plugins-a76f728-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany_plugins/a76f728_x86_64/geany_plugins-a76f728-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany_plugins/1.38-3ac62e2_armv7l/geany_plugins-1.38-3ac62e2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany_plugins/1.38-3ac62e2_armv7l/geany_plugins-1.38-3ac62e2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/geany_plugins/1.38-3ac62e2_x86_64/geany_plugins-1.38-3ac62e2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a0a8ec98c39432edcf7d17369b410e48878a45ac58206cf2e5cfd6da13946893',
-     armv7l: 'a0a8ec98c39432edcf7d17369b410e48878a45ac58206cf2e5cfd6da13946893',
-       i686: 'cb4786fcb33463aacee5c377fb5c9e2cb864408323fef68610c311c5c28ef4d9',
-     x86_64: '410488ee35fefbd1e502ead3a67fde697ac48ee8a8ecd6de1217951db382e08a'
+    aarch64: 'e43377ce7d8d200eb58350772e7d2132edbe84d9d10390a1ba95ae04d037cd40',
+     armv7l: 'e43377ce7d8d200eb58350772e7d2132edbe84d9d10390a1ba95ae04d037cd40',
+     x86_64: '4aa9fbe81d1d637979a6b112c4f3eedf23170fa4f78f2aed3f7a7a17d3625ef2'
   })
 
+  depends_on 'aspell' # R
   depends_on 'at_spi2_core' # R
-  depends_on 'cairo' # R
+  depends_on 'cairo' => :build
   depends_on 'enchant' # R
+  depends_on 'gcc' # R
   depends_on 'gdk_pixbuf' # R
   depends_on 'geany' # R
+  depends_on 'glibc' # R
   depends_on 'glib' # R
   depends_on 'gtk3' # R
   depends_on 'harfbuzz' # R
-  depends_on 'libgit2' => :build
+  depends_on 'icu4c' # R
+  depends_on 'libgit2' # R
   depends_on 'libsoup2' # R
+  depends_on 'libxml2' # R
+  depends_on 'ncurses' # R
   depends_on 'pango' # R
   depends_on 'valgrind' => :build
   depends_on 'vte' # R
   depends_on 'webkit2gtk_4' # R
+  depends_on 'zlibpkg' # R
 
   def self.build
     system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
     system 'filefix'
-    system "./configure #{CREW_OPTIONS}"
+    system "mold -run ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
