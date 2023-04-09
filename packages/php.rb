b867377b61462a1832047878367579ba33dd4e19
chronos <noreply@github.com>
2023-04-09 00:49:27 +0000

    Update php to determine versions programmatically
    Fix linter issues

--- packages/php.rb
+++ packages/php.rb
@@ -1,9 +1,27 @@
 require 'package'
+Dir["#{CREW_PACKAGES_PATH}/php*.rb"].each do |phpFile|
+  next unless phpFile =~ /php(\d+).rb/
+
+  phpName = File.basename(phpFile, '.rb')
+  require_relative phpName
+end
 
 class Php < Package
   description 'PHP is a popular general-purpose scripting language that is especially suited to web development.'
   homepage 'http://www.php.net/'
-  version '5.6.40-8.2.4'
+  key = 1
+  @phpVersions = []
+  Dir["#{CREW_PACKAGES_PATH}/php*.rb"].each do |phpFile|
+    next unless phpFile =~ /php(\d+).rb/
+
+    phpName = File.basename(phpFile, '.rb')
+    phpVer = Object.const_get(phpName.capitalize).version.to_s.gsub(/-(\d+)/, '')
+    @phpVersions.push [key, phpName, phpVer]
+    @minPhpVer = phpVer if key == 1
+    @maxPhpVer = phpVer
+    key += 1
+  end
+  version "#{@minPhpVer}-#{@maxPhpVer}"
   license 'PHP-3.01'
   compatibility 'all'
 
@@ -16,49 +34,21 @@ class Php < Package
       puts "Php#{major}#{minor} already installed.".lightgreen
       abort "Enter `crew remove php#{major}#{minor} && crew install php` to install a different version."
     end
-    puts
-    puts 'Select version:'
-    puts '5.6 = PHP 5.6.40'
-    puts '7.1 = PHP 7.1.33'
-    puts '7.2 = PHP 7.2.34'
-    puts '7.3 = PHP 7.3.33'
-    puts '7.4 = PHP 7.4.33'
-    puts '8.0 = PHP 8.0.28'
-    puts '8.1 = PHP 8.1.17'
-    puts '8.2 = PHP 8.2.4'
-    puts '  0 = Cancel'
+    puts "\nSelect version:"
+    @phpVersions.each do |phpVer|
+      puts "#{phpVer[0]} = PHP #{phpVer[2]}"
+    end
+    puts '0 = Cancel'
 
-    while version = $stdin.gets.chomp.downcase
+    while version = $stdin.gets.chomp.downcase.to_i
+      version = 0 if version.negative? || version > @phpVersions.length
+      key = version - 1
       case version
-      when '5.6'
-        depends_on 'php5'
+      when @phpVersions[key][0]
+        depends_on @phpVersions[key][1]
         break
-      when '7.1'
-        depends_on 'php71'
-        break
-      when '7.2'
-        depends_on 'php72'
-        break
-      when '7.3'
-        depends_on 'php73'
-        break
-      when '7.4'
-        depends_on 'php74'
-        break
-      when '8.0'
-        depends_on 'php80'
-        break
-      when '8.1'
-        depends_on 'php81'
-        break
-      when '8.2'
-        depends_on 'php82'
-        break
-      when '0'
+      when 0
         abort
-        break
-      else
-        puts "\nPlease select from one of the options or enter 0 to cancel."
       end
     end
   end
