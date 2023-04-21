d951443d35e955dd02c4dc261f1b6dab0f9cda10
Ed Reel <edreel@gmail.com>
2023-04-20 21:44:49 -0500

    Fltk 1.3.5 => 1.3.8

--- packages/fltk.rb
+++ packages/fltk.rb
@@ -3,42 +3,35 @@ require 'package'
 class Fltk < Package
   description 'Fast Light Toolkit or FLTK (pronounced "fulltick") is a cross-platform C++ GUI toolkit'
   homepage 'https://www.fltk.org/'
-  version '1.3.5'
+  version '1.3.8'
   license 'FLTK and LGPL-2'
-  compatibility 'all'
-  source_url 'https://www.fltk.org/pub/fltk/1.3.5/fltk-1.3.5-source.tar.bz2'
-  source_sha256 '2933c72400f9e7c0f4c3a81a1ce98bc9582b2a3edc44b8597ccd26e240e32c3c'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://www.fltk.org/pub/fltk/1.3.8/fltk-1.3.8-source.tar.bz2'
+  source_sha256 '1d9832409fc08c908c64ab40ad9fd8b0e8af5d203651eb9690b810f41e2f412d'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.5_armv7l/fltk-1.3.5-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.5_armv7l/fltk-1.3.5-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.5_i686/fltk-1.3.5-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.5_x86_64/fltk-1.3.5-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.8_armv7l/fltk-1.3.8-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.8_armv7l/fltk-1.3.8-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.8_x86_64/fltk-1.3.8-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '98d7e8f6846f357be4b7600b016fe4f1800c8e7e08d1846227c674fa7e0a7a89',
-     armv7l: '98d7e8f6846f357be4b7600b016fe4f1800c8e7e08d1846227c674fa7e0a7a89',
-       i686: 'ee29eb618e5f09f1486e56061c5b82d1a331dba26eb0e0329020641261d6ac92',
-     x86_64: '95fabef98ee5b2fc9c7ba32ea8f62bf0193322b791f8048d4ee003d76b7c8e83'
+    aarch64: '0eb0447c12dc146a402b31c0b829d8b9bb74c148454a2b5bda1b7546291062ef',
+     armv7l: '0eb0447c12dc146a402b31c0b829d8b9bb74c148454a2b5bda1b7546291062ef',
+     x86_64: 'b6ae65a5a86c7ea727358a0333a261d4f67bfbfc1f51b68c81e099b1f0d2b614'
   })
 
   depends_on 'sommelier'
 
   def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system 'cmake',
-             '-DCMAKE_BUILD_TYPE=Release',
-             "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
-             '-DOPTION_BUILD_EXAMPLES=OFF',
-             '..'
-      system 'make'
-    end
+    system "cmake -B builddir \
+      #{CREW_CMAKE_OPTIONS} \
+      -DOPTION_BUILD_EXAMPLES=OFF \
+      -DOPTION_BUILD_SHARED_LIBS=ON \
+      -G Ninja"
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
