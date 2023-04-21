331c6a1b3f53ad5e9a9a1377d98501829f6f2065
Satadru Pramanik <satadru@gmail.com>
2023-04-21 07:58:02 -0400

    qemu => 8.0.0

--- packages/qemu.rb
+++ packages/qemu.rb
@@ -3,27 +3,28 @@ require 'package'
 class Qemu < Package
   description 'QEMU is a generic and open source machine emulator and virtualizer.'
   homepage 'http://www.qemu.org/'
-  @_ver = '7.1.0'
+  @_ver = '8.0.0'
   version @_ver
-  compatibility 'armv7l aarch64 x86_64'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url "https://download.qemu.org/qemu-#{@_ver}.tar.xz"
-  source_sha256 'a0634e536bded57cf38ec8a751adb124b89c776fe0846f21ab6c6728f1cbbbe6'
+  source_sha256 'bb60f0341531181d6cc3969dd19a013d0427a87f918193970d9adb91131e56d0'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qemu/7.1.0_armv7l/qemu-7.1.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qemu/7.1.0_armv7l/qemu-7.1.0-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qemu/7.1.0_x86_64/qemu-7.1.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qemu/8.0.0_armv7l/qemu-8.0.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qemu/8.0.0_armv7l/qemu-8.0.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qemu/8.0.0_x86_64/qemu-8.0.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'c9e03b09775ade72b65341ae26365258935abd463fa64d1d22a4942b18bccd10',
-     armv7l: 'c9e03b09775ade72b65341ae26365258935abd463fa64d1d22a4942b18bccd10',
-     x86_64: '8fdece512eb79eb1426df761cd594be642f109b34ed8fc38336503eef5133104'
+    aarch64: '87470f40e0fceb744c76b1588abb906f8f94f952f7ff0364f94ec0e20e5a0053',
+     armv7l: '87470f40e0fceb744c76b1588abb906f8f94f952f7ff0364f94ec0e20e5a0053',
+     x86_64: 'cdf730735f4156e2f59218b0fc9e5690d7f72f6dc78b09d41bfbfc8c054b2083'
   })
 
   depends_on 'alsa_lib' # R
   depends_on 'at_spi2_core' # R
   depends_on 'cairo' # R
   depends_on 'eudev' # R
+  depends_on 'elfutils' # R
   depends_on 'fontconfig' # R
   depends_on 'gdk_pixbuf' # R
   depends_on 'glib' # R
@@ -60,6 +61,7 @@ class Qemu < Package
   depends_on 'libseccomp' # R
   depends_on 'libssh' # R
   depends_on 'ncurses' # R
+  depends_on 'vte' # R
   depends_on 'zlibpkg' # R
   depends_on 'zstd' # R
 
@@ -67,14 +69,14 @@ class Qemu < Package
     # Avoid linux/usbdevice_fs.h:88:9: error: unknown type name ‘u8’ error
     FileUtils.mkdir_p 'linux'
     FileUtils.cp "#{CREW_PREFIX}/include/linux/usbdevice_fs.h", 'linux/usbdevice_fs.h'
-    system "sed -i 's,^\\\([[:blank:]]*\\\)u8,\\1__u8,g' linux/usbdevice_fs.h"
+    system "sed -i 's,^\\([[:blank:]]*\\)u8,\\1__u8,g' linux/usbdevice_fs.h"
     system "sed -i 's,<linux/usbdevice_fs.h>,\"linux/usbdevice_fs.h\",g' hw/usb/host-libusb.c"
   end
 
   def self.build
     FileUtils.mkdir_p 'build'
     Dir.chdir 'build' do
-      system "../configure #{CREW_OPTIONS.sub(/--target.*/, '')} \
+      system "mold -run ../configure #{CREW_OPTIONS.sub(/--target.*/, '')} \
         --enable-lto"
       system 'make || make -j1'
     end
