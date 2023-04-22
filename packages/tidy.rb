d06daff95839d8d049626f04e04838e0cb509932
Ed Reel <edreel@gmail.com>
2023-04-22 11:12:07 -0500

    Tidy 5.6.0 => 5.8.0 (#8211)
    * Tidy 5.6.0 => 5.8.0
    
    * Rebuild tidy, just with current formatting.
    
    ---------
    
    Co-authored-by: chronos <noreply@github.com>
    Co-authored-by: Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>

--- packages/tidy.rb
+++ packages/tidy.rb
@@ -3,40 +3,35 @@ require 'package'
 class Tidy < Package
   description 'The granddaddy of HTML tools, with support for modern standards'
   homepage 'http://www.html-tidy.org/'
-  version '5.6.0'
+  version '5.8.0-1'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/htacg/tidy-html5/archive/5.6.0.tar.gz'
-  source_sha256 '08a63bba3d9e7618d1570b4ecd6a7daa83c8e18a41c82455b6308bc11fe34958'
+  source_url 'https://github.com/htacg/tidy-html5/archive/5.8.0.tar.gz'
+  source_sha256 '59c86d5b2e452f63c5cdb29c866a12a4c55b1741d7025cf2f3ce0cde99b0660e'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_armv7l/tidy-5.6.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_armv7l/tidy-5.6.0-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_i686/tidy-5.6.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_x86_64/tidy-5.6.0-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0-1_armv7l/tidy-5.8.0-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0-1_armv7l/tidy-5.8.0-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0-1_i686/tidy-5.8.0-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0-1_x86_64/tidy-5.8.0-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'cff1c1a6c12ac5503db6d5d38b674069df305dfdf5abe327af57f4fe10559b07',
-     armv7l: 'cff1c1a6c12ac5503db6d5d38b674069df305dfdf5abe327af57f4fe10559b07',
-       i686: '34c25a0930741ae084808c6008d9103efcdbc7d0f2e4fade64f6d4c9d239cdd2',
-     x86_64: 'd132e0477d2273b7b9a5077b5389b0325f3dfeaed7516a632fa14620ab32e95e'
+    aarch64: '968686c5c89e00f8fdb99f5840430bd028360726404558b0686ac4e018f2a00d',
+     armv7l: '968686c5c89e00f8fdb99f5840430bd028360726404558b0686ac4e018f2a00d',
+       i686: '911e08b1a7e63ca787429839ace0c6e573e6ef60ce248e66c093da0911c2a9f4',
+     x86_64: '59f1d096f5c745ae606f37e136aac3aa4b0ae4337efa4ee7415ffed9f946d0ff'
   })
 
+  depends_on 'glibc' # R
+
   def self.build
-    Dir.chdir 'build/cmake' do
-      case ARCH
-      when 'x86_64'
-        system "cmake ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} -DLIB_SUFFIX=64"
-      else
-        system "cmake ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}"
-      end
-      system 'make'
-    end
+    system "cmake -B builddir #{CREW_CMAKE_LIBSUFFIX_OPTIONS} \
+      -DBUILD_TAB2SPACE=ON \
+      -G Ninja"
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    Dir.chdir 'build/cmake' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
