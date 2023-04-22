ffd4240909097ade6ad0b089ccf6d3c1789f67a8
chronos <noreply@github.com>
2023-04-22 06:25:00 +0000

    Tidy 5.6.0 => 5.8.0

--- packages/tidy.rb
+++ packages/tidy.rb
@@ -3,40 +3,34 @@ require 'package'
 class Tidy < Package
   description 'The granddaddy of HTML tools, with support for modern standards'
   homepage 'http://www.html-tidy.org/'
-  version '5.6.0'
+  version '5.8.0'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/htacg/tidy-html5/archive/5.6.0.tar.gz'
-  source_sha256 '08a63bba3d9e7618d1570b4ecd6a7daa83c8e18a41c82455b6308bc11fe34958'
+  source_url 'https://github.com/htacg/tidy-html5/archive/5.8.0.tar.gz'
+  source_sha256 '59c86d5b2e452f63c5cdb29c866a12a4c55b1741d7025cf2f3ce0cde99b0660e'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_armv7l/tidy-5.6.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_armv7l/tidy-5.6.0-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_i686/tidy-5.6.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.6.0_x86_64/tidy-5.6.0-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0_armv7l/tidy-5.8.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0_armv7l/tidy-5.8.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0_i686/tidy-5.8.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tidy/5.8.0_x86_64/tidy-5.8.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'cff1c1a6c12ac5503db6d5d38b674069df305dfdf5abe327af57f4fe10559b07',
-     armv7l: 'cff1c1a6c12ac5503db6d5d38b674069df305dfdf5abe327af57f4fe10559b07',
-       i686: '34c25a0930741ae084808c6008d9103efcdbc7d0f2e4fade64f6d4c9d239cdd2',
-     x86_64: 'd132e0477d2273b7b9a5077b5389b0325f3dfeaed7516a632fa14620ab32e95e'
+    aarch64: '3865806933d4fb0d2a72777f4f3f498a44e02b7fde5d3a67900d0d9497e080b5',
+     armv7l: '3865806933d4fb0d2a72777f4f3f498a44e02b7fde5d3a67900d0d9497e080b5',
+       i686: '1b8c20aa52b283b5a51e1c17fe29edbe785378958b10192a67289eb5b96726b2',
+     x86_64: 'd9830bb0e92f4f1d8cf8412b44a40697551f3b50ab76997fd1681b170ac00ae3'
   })
 
   def self.build
-    Dir.chdir 'build/cmake' do
-      case ARCH
-      when 'x86_64'
-        system "cmake ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} -DLIB_SUFFIX=64"
-      else
-        system "cmake ../.. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}"
-      end
-      system 'make'
-    end
+    system "cmake -B build/cmake \
+      #{CREW_CMAKE_OPTIONS} \
+      -DLIB_SUFFIX=#{CREW_LIB_SUFFIX} \
+      -DBUILD_TAB2SPACE=ON"
+    system 'make -C build/cmake'
   end
 
   def self.install
-    Dir.chdir 'build/cmake' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
+    system "DESTDIR=#{CREW_DEST_DIR} make -C build/cmake install"
   end
 end
