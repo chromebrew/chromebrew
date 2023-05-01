42577880c35cfc7d6ce97f07caec570e85aa8daf
Ed Reel <edreel@gmail.com>
2023-05-01 02:07:14 -0500

    Filezilla 3.63.2.1 => 3.64.0
    Libfilezilla 0.41.1 => 0.42.2
    
    Convert File.write to FileUtils.install in filezilla.rb
    
    Install file instead of variable

--- packages/filezilla.rb
+++ packages/filezilla.rb
@@ -3,21 +3,21 @@ require 'package'
 class Filezilla < Package
   description 'FileZilla Client is a free FTP solution.'
   homepage 'https://filezilla-project.org/'
-  version '3.63.2.1'
+  version '3.64.0'
   license 'GPL-2'
   compatibility 'aarch64,armv7l,x86_64'
-  source_url 'https://download.filezilla-project.org/client/FileZilla_3.63.2.1_src.tar.bz2'
-  source_sha256 '6dc14c6f7f4de7e44c8429b32f7f9edbbf10732d21dd75d8974ae44007b7b79d'
+  source_url 'https://download.filezilla-project.org/client/FileZilla_3.64.0_src.tar.xz'
+  source_sha256 '813d184d54984cf0fb27a455423089070e273b2ef8a70cb613e741170edf479a'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/filezilla/3.63.2.1_armv7l/filezilla-3.63.2.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/filezilla/3.63.2.1_armv7l/filezilla-3.63.2.1-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/filezilla/3.63.2.1_x86_64/filezilla-3.63.2.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/filezilla/3.64.0_armv7l/filezilla-3.64.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/filezilla/3.64.0_armv7l/filezilla-3.64.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/filezilla/3.64.0_x86_64/filezilla-3.64.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '0f2797a486f08daf0beea70493e3cb66466d7a908c9531002269cb754ad6f287',
-     armv7l: '0f2797a486f08daf0beea70493e3cb66466d7a908c9531002269cb754ad6f287',
-     x86_64: 'fc5cf25b9cfdae4b0c82ff0ab9fd7c4de90e9f7303e9f091b5d6ec35513d6504'
+    aarch64: '50e6374e6ec43e176f47940c281ec5f6ea089ec44aede3ef1636d6c343d92ad7',
+     armv7l: '50e6374e6ec43e176f47940c281ec5f6ea089ec44aede3ef1636d6c343d92ad7',
+     x86_64: '56402c6e620466310b85376fd5f6d432ab6aaa93d0a1a335185a588e0e181481'
   })
 
   depends_on 'dbus'
@@ -44,11 +44,10 @@ class Filezilla < Package
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
-    filezilla = <<~'EOF'
+    File.write 'filezilla', <<~EOF
       alias filezilla="WAYLAND_DISPLAY=wayland-0 DISPLAY='' GDK_BACKEND=wayland filezilla"
     EOF
-    File.write("#{CREW_DEST_PREFIX}/etc/env.d/10-filezilla", filezilla)
+    FileUtils.install 'filezilla', "#{CREW_DEST_PREFIX}/etc/env.d/10-filezilla", mode: 0o644
   end
 
   def self.postinstall
