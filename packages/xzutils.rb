98a43de0c0fa314be43099fd70c1d838b5431aff
Ed Reel <edreel@gmail.com>
2023-05-05 02:31:14 -0500

    Xzutils 5.4.2 => 5.4.3

--- packages/xzutils.rb
+++ packages/xzutils.rb
@@ -3,23 +3,23 @@ require 'package'
 class Xzutils < Package
   description 'XZ Utils is free general-purpose data compression software with a high compression ratio.'
   homepage 'http://tukaani.org/xz/'
-  version '5.4.2'
+  version '5.4.3'
   license 'public-domain, LGPL-2.1+ and GPL-2+'
   compatibility 'all'
-  source_url 'https://tukaani.org/xz/xz-5.4.2.tar.gz'
-  source_sha256 '87947679abcf77cc509d8d1b474218fd16b72281e2797360e909deaee1ac9d05'
+  source_url 'https://tukaani.org/xz/xz-5.4.3.tar.gz'
+  source_sha256 '1c382e0bc2e4e0af58398a903dd62fff7e510171d2de47a1ebe06d1528e9b7e9'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.2_armv7l/xzutils-5.4.2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.2_armv7l/xzutils-5.4.2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.2_i686/xzutils-5.4.2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.2_x86_64/xzutils-5.4.2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_armv7l/xzutils-5.4.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_armv7l/xzutils-5.4.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_i686/xzutils-5.4.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_x86_64/xzutils-5.4.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '7641b6c7b1795b5ceb9fc69a07709383af08447d19b72282431edc8f25d83148',
-     armv7l: '7641b6c7b1795b5ceb9fc69a07709383af08447d19b72282431edc8f25d83148',
-       i686: '88e98f1c8524bf5cd6080b05b5bf9f1d7c38224abea6c216783288aebf7266d3',
-     x86_64: '0ec4d59acf5bab9c6ce3a2bef1fe0721bcf9eb8df2103805eb5c2ed860b6ddf1'
+    aarch64: '423d4266d49d82aceb6341b5d2bce7d3416ff06fb1f026f4acab0584338913c9',
+     armv7l: '423d4266d49d82aceb6341b5d2bce7d3416ff06fb1f026f4acab0584338913c9',
+       i686: '82b65794ce833407142d7183d6ec497f69ba7399df5582b5335d30270c57585c',
+     x86_64: '60ffcc7daaa04574fb5624b0be54c5db39e52da3c786dc4ed2d4dc628211ab92'
   })
 
   depends_on 'autoconf_archive' => :build
