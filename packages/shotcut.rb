4d9c1f98fdfc7e0bdb780506ffebfb9ffb6f4b58
Satadru Pramanik <satadru@gmail.com>
2023-05-15 13:02:14 -0400

    update shotcut

--- packages/shotcut.rb
+++ packages/shotcut.rb
@@ -3,46 +3,125 @@ require 'package'
 class Shotcut < Package
   description 'Shotcut is a free, open source, cross-platform video editor.'
   homepage 'https://www.shotcut.org/'
-  version '20.11.25'
+  version '23.05.14'
   license 'GPL-3+'
   compatibility 'x86_64'
-  source_url 'https://github.com/mltframework/shotcut/releases/download/v20.11.25/shotcut-linux-x86_64-201125.txz'
-  source_sha256 'fa45d26a1e1e6af82e4f600a7a98b020e1af14f9c176595567aacbb3c9f21093'
+  source_url 'https://github.com/mltframework/shotcut/releases/download/v23.05.14/shotcut-linux-x86_64-230514.txz'
+  source_sha256 '38e1f32e0211043feae7b2a075caad978f2897c24502ab880b5bef30e6539695'
 
   binary_url({
-    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shotcut/20.11.25_x86_64/shotcut-20.11.25-chromeos-x86_64.tar.xz'
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/shotcut/23.05.14_x86_64/shotcut-23.05.14-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    x86_64: 'c252f03d9b50f5533d86640734d2327ee6e364df79d3505e890da587d1b32efc'
+    x86_64: '30f56af633399525b4f97ebd286de279524fa62966133c5889715acfc439362a'
   })
 
+  depends_on 'acl' # R
+  depends_on 'alsa_lib' # R
+  depends_on 'bz2' # R
+  depends_on 'dav1d' # R
+  depends_on 'dbus' # R
+  depends_on 'elfutils' # R
+  depends_on 'eudev' # R
+  depends_on 'expat' # R
+  depends_on 'fftw' # R
+  depends_on 'flac' # R
+  depends_on 'freetype' # R
+  depends_on 'gcc' # R
+  depends_on 'gdk_pixbuf' # R
+  depends_on 'glibc' # R
+  depends_on 'glib' # R
   depends_on 'gtk3'
-  depends_on 'jack1'
-  depends_on 'ffmpeg'
-  depends_on 'sommelier'
+  depends_on 'harfbuzz' # R
+  depends_on 'jack' # R
+  depends_on 'libaom' # R
+  depends_on 'libarchive' # R
+  depends_on 'libbsd' # R
+  depends_on 'libdb' # R
+  depends_on 'libdrm' # R
+  depends_on 'libepoxy' # R
+  depends_on 'libgcrypt' # R
+  depends_on 'libglvnd' # R
+  depends_on 'libgpgerror' # R
+  depends_on 'libice' # R
+  depends_on 'libinput' # R
+  depends_on 'libmp3lame' # R
+  depends_on 'libogg' # R
+  depends_on 'libpciaccess' # R
+  depends_on 'libpng' # R
+  depends_on 'libsamplerate' # R
+  depends_on 'libsdl2' # R
+  depends_on 'libsm' # R
+  depends_on 'libsndfile' # R
+  depends_on 'libtheora' # R
+  depends_on 'libva' # R
+  depends_on 'libvorbis' # R
+  depends_on 'libwebp' # R
+  depends_on 'libx11' # R
+  depends_on 'libx264' # R
+  depends_on 'libxcb' # R
+  depends_on 'libxcursor' # R
+  depends_on 'libxext' # R
+  depends_on 'libxfixes' # R
+  depends_on 'libxinerama' # R
+  depends_on 'libxi' # R
+  depends_on 'libxkbcommon' # R
+  depends_on 'libxml2' # R
+  depends_on 'libxrandr' # R
+  depends_on 'libxrender' # R
+  depends_on 'libxshmfence' # R
+  depends_on 'libxss' # R
+  depends_on 'libxv' # R
+  depends_on 'libxxf86vm' # R
+  depends_on 'lz4' # R
+  depends_on 'mtdev' # R
+  depends_on 'ncurses' # R
+  depends_on 'openssl' # R
+  depends_on 'opus' # R
+  depends_on 'pango' # R
+  depends_on 'pcre2' # R
+  depends_on 'pipewire' # R
+  depends_on 'pulseaudio' # R
+  depends_on 'readline' # R
+  depends_on 'rubberband' # R
+  depends_on 'sommelier' # L
+  depends_on 'sqlite' # R
+  depends_on 'tcpwrappers' # R
+  depends_on 'util_linux' # R
+  depends_on 'vmaf' # R
+  depends_on 'wayland' # R
+  depends_on 'xcb_util' # R
+  depends_on 'xzutils' # R
+  depends_on 'zimg' # R
+  depends_on 'zlibpkg' # R
+  depends_on 'zstd' # R
+
+  def self.preflight
+    abort 'Shotcut requires glibc > 2.29.' if Gem::Version.new(LIBC_VERSION.to_s) < Gem::Version.new('2.29')
+  end
 
   def self.build
     system "cat << 'EOF' > shotcut
 #!/bin/sh
 # Set up environment
 # Run this instead of trying to run bin/shotcut. It runs shotcut with the correct environment.
-CURRENT_DIR=\$(readlink -f \"\$0\")
+CURRENT_DIR=$(readlink -f \"$0\")
 INSTALL_DIR=#{CREW_PREFIX}/share/shotcut
-export LD_LIBRARY_PATH=\"\$INSTALL_DIR/lib\":\$LD_LIBRARY_PATH
-export MLT_REPOSITORY=\"\$INSTALL_DIR/lib/mlt\"
-export MLT_DATA=\"\$INSTALL_DIR/share/mlt\"
-export MLT_PROFILES_PATH=\"\$INSTALL_DIR/share/mlt/profiles\"
-export MLT_MOVIT_PATH=\"\$INSTALL_DIR/share/movit\"
-export FREI0R_PATH=\"\$INSTALL_DIR/lib/frei0r-1\"
+export LD_LIBRARY_PATH=\"$INSTALL_DIR/lib\":$LD_LIBRARY_PATH
+export MLT_REPOSITORY=\"$INSTALL_DIR/lib/mlt\"
+export MLT_DATA=\"$INSTALL_DIR/share/mlt\"
+export MLT_PROFILES_PATH=\"$INSTALL_DIR/share/mlt/profiles\"
+export MLT_MOVIT_PATH=\"$INSTALL_DIR/share/movit\"
+export FREI0R_PATH=\"$INSTALL_DIR/lib/frei0r-1\"
 # Temporarily ignore user and default path because csladspa bug is crashing with
 # LADSPA_PATH set, and Shotcut only needs the supplied SWH plugins.
-# export LADSPA_PATH=\"\$LADSPA_PATH:/usr/local/lib/ladspa:/usr/lib/ladspa:/usr/lib64/ladspa:\$INSTALL_DIR/lib/ladspa\"
-export LADSPA_PATH=\"\$INSTALL_DIR/lib/ladspa\"
-export LIBVA_DRIVERS_PATH=\"\$INSTALL_DIR/lib/va\"
-cd \"\$INSTALL_DIR\"
+# export LADSPA_PATH=\"$LADSPA_PATH:/usr/local/lib/ladspa:/usr/lib/ladspa:/usr/lib64/ladspa:$INSTALL_DIR/lib/ladspa\"
+export LADSPA_PATH=\"$INSTALL_DIR/lib/ladspa\"
+export LIBVA_DRIVERS_PATH=\"$INSTALL_DIR/lib/va\"
+cd \"$INSTALL_DIR\"
 export QT_PLUGIN_PATH=\"lib/qt5\"
 export QML2_IMPORT_PATH=\"lib/qml\"
-bin/shotcut \"\$@\"
+bin/shotcut \"$@\"
 EOF"
   end
 
@@ -53,7 +132,7 @@ EOF"
       FileUtils.mv 'lib', "#{CREW_DEST_PREFIX}/share/shotcut"
       FileUtils.mv 'share', "#{CREW_DEST_PREFIX}/share/shotcut"
     end
-    system "install -Dm755 shotcut #{CREW_DEST_PREFIX}/bin/shotcut"
-    system "install -Dm644 Shotcut.desktop #{CREW_DEST_PREFIX}/share/applications/Shotcut.desktop"
+    FileUtils.install 'shotcut', "#{CREW_DEST_PREFIX}/bin/shotcut", mode: 0o755
+    FileUtils.install 'Shotcut.desktop', "#{CREW_DEST_PREFIX}/share/applications/Shotcut.desktop", mode: 0o644
   end
 end
