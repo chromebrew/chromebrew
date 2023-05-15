6c93ef685764f357fc7185a98b085ad115f39faa
Satadru Pramanik <satadru@gmail.com>
2023-05-15 17:58:22 -0400

    update gegl

--- packages/gegl.rb
+++ packages/gegl.rb
@@ -3,21 +3,21 @@ require 'package'
 class Gegl < Package
   description 'GEGL (Generic Graphics Library) is a data flow based image processing framework, providing floating point processing and non-destructive image processing capabilities to GNU Image Manipulation Program and other projects.'
   homepage 'http://gegl.org/'
-  version '0.4.42'
+  version '0.4.44'
   license 'GPL-3+ and LGPL-3'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://download.gimp.org/pub/gegl/0.4/gegl-0.4.42.tar.xz'
-  source_sha256 'aba83a0cbaa6c56edc29ea22f2e8172950a53b96daa51592083d59222bdde02d'
+  source_url 'https://download.gimp.org/pub/gegl/0.4/gegl-0.4.44.tar.xz'
+  source_sha256 '0a4cdb41635e406a0849cd0d3f03caf7d97cab8aa13d28707d532d0089d56126'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gegl/0.4.42_armv7l/gegl-0.4.42-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gegl/0.4.42_armv7l/gegl-0.4.42-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gegl/0.4.42_x86_64/gegl-0.4.42-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gegl/0.4.44_armv7l/gegl-0.4.44-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gegl/0.4.44_armv7l/gegl-0.4.44-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gegl/0.4.44_x86_64/gegl-0.4.44-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b0aba71d5f37ffc560c39786b193d7a32f122d62fe612a88539d3ed6e13102ae',
-     armv7l: 'b0aba71d5f37ffc560c39786b193d7a32f122d62fe612a88539d3ed6e13102ae',
-     x86_64: '17e36b43e2d5217b7df42f83c013a91c32df492d5b719665e9aada083e9a9173'
+    aarch64: '4dd98c8698998748677622d0ffe3c034993e805ebd3f0e04f87cf6b862c7af9f',
+     armv7l: '4dd98c8698998748677622d0ffe3c034993e805ebd3f0e04f87cf6b862c7af9f',
+     x86_64: '27b61ace2d2a769c9c8fe78a8221c5372c07e2a80685503fb31351e465003eaf'
   })
 
   depends_on 'asciidoc' => :build
@@ -50,12 +50,12 @@ class Gegl < Package
   depends_on 'zlibpkg' # R
 
   def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} \
+    system "mold -run meson setup #{CREW_MESON_OPTIONS} \
       -Dlibjpeg=enabled \
       -Dlibpng=enabled \
     builddir"
     system 'meson configure builddir'
-    system "mold -run #{CREW_NINJA} -C builddir"
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
