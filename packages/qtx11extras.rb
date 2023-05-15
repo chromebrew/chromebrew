c1e3aceaf7586d5b6d6e2f35ffcb75cc9c6171bf
Satadru Pramanik <satadru@gmail.com>
2023-05-15 16:47:31 -0400

    update qtwayland and qtx11extras

--- packages/qtx11extras.rb
+++ packages/qtx11extras.rb
@@ -3,23 +3,21 @@ require 'package'
 class Qtx11extras < Package
   description 'Provides classes for developing for the X11 platform.'
   homepage 'https://www.qt.io/'
-  version '5.15.7-9134cdb'
+  version '5.15.9-8bba77a'
   license 'FDL, GPL-2, GPL-3, GPL-3-with-qt-exception and LGPL-3'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://invent.kde.org/qt/qt/qtx11extras.git'
-  git_hashtag '9134cdba9386e408ce2ffe515ca0c3f6f6c66685'
+  git_hashtag '8bba77a558049727d1bc88736dd03d1b5c41cead'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtx11extras/5.15.7-9134cdb_armv7l/qtx11extras-5.15.7-9134cdb-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtx11extras/5.15.7-9134cdb_armv7l/qtx11extras-5.15.7-9134cdb-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtx11extras/5.15.7-9134cdb_i686/qtx11extras-5.15.7-9134cdb-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtx11extras/5.15.7-9134cdb_x86_64/qtx11extras-5.15.7-9134cdb-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtx11extras/5.15.9-8bba77a_armv7l/qtx11extras-5.15.9-8bba77a-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtx11extras/5.15.9-8bba77a_armv7l/qtx11extras-5.15.9-8bba77a-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qtx11extras/5.15.9-8bba77a_x86_64/qtx11extras-5.15.9-8bba77a-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '02e27baed54441a37170ed009702fe21a32da8bee1c9bebfc0248bdc5dd9daf0',
-     armv7l: '02e27baed54441a37170ed009702fe21a32da8bee1c9bebfc0248bdc5dd9daf0',
-       i686: 'b2baac1b14d2ddaf0fbe481f9e7a1afd9dd40dffadbe8d0aecfc4ba1969c9cf1',
-     x86_64: '40c58bbed435f536cf50cb13f51625a0e2c7d72213769069e745282f94683d23'
+    aarch64: 'f38d4a7b21e22ab58b571fb48fda4a2175a6a9426c4bec45d69aecf540dece17',
+     armv7l: 'f38d4a7b21e22ab58b571fb48fda4a2175a6a9426c4bec45d69aecf540dece17',
+     x86_64: '225bf24774fd718bcbc89c0b681a56c05d65eeb054662f05dc52ae44e6f1aae2'
   })
 
   depends_on 'gcc' # R
