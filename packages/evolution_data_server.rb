c9cf71708a8d8416df7902dc3386622d4ba18bbe
Satadru Pramanik <satadru@gmail.com>
2023-05-19 21:03:52 -0400

    add evolution_data_server build

--- packages/evolution_data_server.rb
+++ packages/evolution_data_server.rb
@@ -2,24 +2,18 @@ require 'package'
 
 class Evolution_data_server < Package
   description 'Centralized access to appointments and contacts'
-  @_ver = '3.46.0'
+  @_ver = '3.48.1'
   version @_ver
   license 'LGPL-2 or LGPL-3, BSD and Sleepycat'
-  compatibility 'all'
+  compatibility 'x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/evolution-data-server.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/evolution_data_server/3.46.0_armv7l/evolution_data_server-3.46.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/evolution_data_server/3.46.0_armv7l/evolution_data_server-3.46.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/evolution_data_server/3.46.0_i686/evolution_data_server-3.46.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/evolution_data_server/3.46.0_x86_64/evolution_data_server-3.46.0-chromeos-x86_64.tar.zst'
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/evolution_data_server/3.48.1_x86_64/evolution_data_server-3.48.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '856ab38434b324eecacdfdb2981dcbc2e67769680621370e3da09c1d0979d8b4',
-     armv7l: '856ab38434b324eecacdfdb2981dcbc2e67769680621370e3da09c1d0979d8b4',
-       i686: '303248f7eb4e050cbf846caaf9f23b04c19df4fa624ee90db6c799f891175271',
-     x86_64: '57189bbc3c4fa355db5d3e64dc89521f91e7366eadb251b993bd963ec573d1d3'
+    x86_64: '59465eb6457d3f2a01fa24983d0a99226d22145674bd6a5c38244b12569d617f'
   })
 
   depends_on 'at_spi2_core' # R
@@ -50,15 +44,14 @@ class Evolution_data_server < Package
   depends_on 'graphene' # R
   depends_on 'gtk4' # R
   depends_on 'json_glib' # R
+  depends_on 'vulkan_headers' => :build
   depends_on 'vulkan_icd_loader' # R
   depends_on 'webkit2gtk_4_1' # R
-  depends_on 'webkit2gtk_5' # R
+  depends_on 'webkitgtk_6' # R
 
   def self.build
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "LIBRARY_PATH=#{CREW_LIB_PREFIX} \
-      cmake #{CREW_CMAKE_LIBSUFFIX_OPTIONS} .. -G Ninja \
+    system "LIBRARY_PATH=#{CREW_LIB_PREFIX} \
+      cmake #{CREW_CMAKE_LIBSUFFIX_OPTIONS} -B builddir -G Ninja \
       -DCMAKE_VERBOSE_MAKEFILE=ON \
       -DENABLE_CANBERRA=OFF \
       -DENABLE_EXAMPLES=OFF \
@@ -73,11 +66,10 @@ class Evolution_data_server < Package
       -DWITH_NSS_INCLUDES=#{CREW_PREFIX}/include/nss \
       -DWITH_OPENLDAP=OFF \
       -DWITH_PHONENUMBER=OFF"
-    end
-    system 'ninja -C builddir'
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
