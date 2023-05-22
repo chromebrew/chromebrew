fba3acbafadabd1033d680f5551d66edc9bfa1d9
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-05-22 19:46:14 -0400

    Add Icecream (#8291)
    * update libcap_ng
    
    * remove intentional linter failure
    
    * fixup docbook2x
    
    * add ice cream

--- packages/docbook2x.rb
+++ packages/docbook2x.rb
@@ -3,33 +3,40 @@ require 'package'
 class Docbook2x < Package
   description 'docbook2X is a software package that converts DocBook documents into the traditional Unix man page format and the GNU Texinfo format.'
   homepage 'http://docbook2x.sourceforge.net/'
-  version '0.8.8'
+  version '0.8.8-1'
   license 'MIT'
   compatibility 'all'
   source_url 'https://downloads.sourceforge.net/project/docbook2x/docbook2x/0.8.8/docbook2X-0.8.8.tar.gz'
   source_sha256 '4077757d367a9d1b1427e8d5dfc3c49d993e90deabc6df23d05cfe9cd2fcdc45'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8_armv7l/docbook2x-0.8.8-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8_armv7l/docbook2x-0.8.8-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8_i686/docbook2x-0.8.8-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8_x86_64/docbook2x-0.8.8-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8-1_armv7l/docbook2x-0.8.8-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8-1_armv7l/docbook2x-0.8.8-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8-1_i686/docbook2x-0.8.8-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/docbook2x/0.8.8-1_x86_64/docbook2x-0.8.8-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '908f22bd15b5d8cd5d4ebc09f9ee7cd2f7bbbf83a0b359c899983c83a0437a2e',
-     armv7l: '908f22bd15b5d8cd5d4ebc09f9ee7cd2f7bbbf83a0b359c899983c83a0437a2e',
-       i686: '06a3b6ea1ada68b8bf1b8b2b7f2d2112bff3c26da0f6900c4b4bc8aeda20b8c9',
-     x86_64: 'dca6f49d1bae407301a334edbde810845fa2aa6e2000b1cd1d658aac681cbdbe'
+    aarch64: '17f43a04fd80fb126cd8c7fd62163897be3edb24ed82026a1109fd614c52a585',
+     armv7l: '17f43a04fd80fb126cd8c7fd62163897be3edb24ed82026a1109fd614c52a585',
+       i686: '43dfe469d361f695e655035bc30e4e92273b43e6f5b0ec8ccff872815810dc90',
+     x86_64: 'ceaba3863b07df670a310e6e54e2b9469bddd43f824a6e455811ee244cceb72f'
   })
 
-  depends_on 'perl_xml_sax_parserfactory'
+  depends_on 'perl_xml_sax_parserfactory' # R
+  depends_on 'glibc' # R
 
   def self.build
-    system './configure'
+    system "./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system "sed -i 's,http://docbook2x.sf.net/latest,#{CREW_PREFIX}/share/docbook2X,g' \
+      #{CREW_DEST_PREFIX}/bin/db2x_xsltproc"
+    Dir["#{CREW_DEST_PREFIX}/bin/*"].each do |bin|
+      system "sed -i 's,${prefix},#{CREW_PREFIX},g' \
+        #{bin}"
+    end
   end
 end
---
+++ packages/icecream.rb
@@ -0,0 +1,137 @@
+# Adapted from Arch Linux icecream PKGBUILD at:
+# https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=icecream
+
+require 'package'
+
+class Icecream < Package
+  description 'takes compile jobs from your build and distributes it to remote machines allowing a parallel build on several machines.'
+  homepage 'https://github.com/icecc/icecream'
+  version '1.4'
+  license 'GPL2'
+  compatibility 'all'
+  source_url 'https://github.com/icecc/icecream/archive/refs/tags/1.4.tar.gz'
+  source_sha256 '249dcf74f0fc477ff9735ff0bdcdfaa4c257a864c4db5255d8b25c9f4fd20b6b'
+
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/icecream/1.4_armv7l/icecream-1.4-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/icecream/1.4_armv7l/icecream-1.4-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/icecream/1.4_i686/icecream-1.4-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/icecream/1.4_x86_64/icecream-1.4-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    aarch64: '1e9f3f7b04cc40f8ed6a01baf6cd8bee2ec8079b3e35a701146d1ee7185ca706',
+     armv7l: '1e9f3f7b04cc40f8ed6a01baf6cd8bee2ec8079b3e35a701146d1ee7185ca706',
+       i686: '489103234caa73c4f9c9dd999c8b54cf12a4d69c89423337375e21caba242eac',
+     x86_64: '7560a473bb913b928f84d207dac8d5d24bc0ba6dcfc1529dcb6f43165a1eb5fd'
+  })
+
+  depends_on 'acl' # R
+  depends_on 'attr' # R
+  depends_on 'bz2' # R
+  depends_on 'docbook2x' => :build
+  depends_on 'gcc' # R
+  depends_on 'glibc' # R
+  depends_on 'icu4c' # R
+  depends_on 'libarchive' # R
+  depends_on 'libcap_ng' # R
+  depends_on 'libxml2' # R
+  depends_on 'llvm' => :build
+  depends_on 'lz4' # R
+  depends_on 'lzo' # R
+  depends_on 'openssl' # R
+  depends_on 'perl_xml_namespacesupport' => :build
+  depends_on 'perl_xml_sax_base' => :build
+  depends_on 'perl_xml_sax' => :build
+  depends_on 'xzutils' # R
+  depends_on 'zlibpkg' # R
+  depends_on 'zstd' # R
+
+  def self.patch
+    downloader 'https://aur.archlinux.org/cgit/aur.git/plain/icecream.conf?h=icecream',
+               '4c1b993761600955a2be53e945a698638ef861a513258956d8aac0e0f8df3dfe', 'icecream.conf'
+    system "sed -i 's,/var,#{CREW_PREFIX}/var,g' icecream.conf"
+  end
+
+  def self.build
+    system './autogen.sh'
+    system "./configure --prefix=#{CREW_LIB_PREFIX}/icecream \
+      --enable-shared \
+      --disable-static \
+      --enable-clang-wrappers \
+      --enable-clang-rewrite-includes \
+      --mandir=#{CREW_MAN_PREFIX} \
+      --with-libcap_ng"
+    system 'make'
+
+    File.write 'ld-icecream.conf', <<~LD_ICECREAM_HEREDOC
+      #{CREW_LIB_PREFIX}/icecream/lib
+    LD_ICECREAM_HEREDOC
+
+    File.write 'icecream-schedulerd', <<~ICECREAM_SCHEDULERD_HEREDOC
+      #!/bin/sh
+
+      . #{CREW_PREFIX}/etc/icecream.conf
+
+      netname=
+      if test -n "$ICECREAM_NETNAME"; then
+          netname="-n $ICECREAM_NETNAME"
+      fi
+      logfile=""
+      if test -n "$ICECREAM_LOG_FILE"; then
+          touch $ICECREAM_LOG_FILE
+          logfile="-l $ICECREAM_LOG_FILE"
+      fi
+
+      #{CREW_LIB_PREFIX}/icecream/sbin/icecc-scheduler ${netname} ${logfile} &>/dev/null
+    ICECREAM_SCHEDULERD_HEREDOC
+
+    File.write 'icecreamd', <<~ICECREAMD_HEREDOC
+      #!/bin/bash
+      . #{CREW_PREFIX}/etc/icecream.conf
+
+      if test -n "${ICECREAM_NETNAME}"; then
+          netname="-n ${ICECREAM_NETNAME}"
+      fi
+      logfile=""
+      if test -n "${ICECREAM_LOG_FILE}"; then
+          touch ${ICECREAM_LOG_FILE}
+          chown icecream:icecream ${ICECREAM_LOG_FILE}
+          logfile="-l ${ICECREAM_LOG_FILE}"
+      fi
+      nice=""
+      if test -n "${ICECREAM_NICE_LEVEL}"; then
+          nice="--nice ${ICECREAM_NICE_LEVEL}"
+      fi
+      scheduler=""
+      if test -n "${ICECREAM_SCHEDULER_HOST}"; then
+          scheduler="-s ${ICECREAM_SCHEDULER_HOST}"
+      fi
+      noremote=""
+      if test "${ICECREAM_ALLOW_REMOTE}" = "no" 2> /dev/null; then
+          noremote="--no-remote"
+      fi
+      maxjobs=""
+      if test -n "${ICECREAM_MAX_JOBS}"; then
+          if test "${ICECREAM_MAX_JOBS}" -eq 0 2> /dev/null; then
+            maxjobs="-m 1"
+            noremote="--no-remote"
+          else
+            maxjobs="-m ${ICECREAM_MAX_JOBS}"
+          fi
+      fi
+
+      #{CREW_LIB_PREFIX}/icecream/sbin/iceccd -u icecream ${logfile} ${nice} ${scheduler} ${netname} -b "${ICECREAM_BASEDIR}" ${maxjobs} ${noremote}  &>/dev/null
+    ICECREAMD_HEREDOC
+  end
+
+  def self.install
+    system "make DESTDIR=#{CREW_DEST_DIR} install"
+    FileUtils.install 'icecream.conf', "#{CREW_DEST_PREFIX}/etc/icecream.conf", mode: 0o644
+    FileUtils.install 'icecreamd', "#{CREW_DEST_LIB_PREFIX}/icecream/icecreamd", mode: 0o755
+    FileUtils.install 'icecream-schedulerd', "#{CREW_DEST_LIB_PREFIX}/icecream/icecream-schedulerd", mode: 0o755
+    FileUtils.install 'ld-icecream.conf', "#{CREW_DEST_PREFIX}/etc/ld.so.conf.d/icecream.conf", mode: 0o644
+    FileUtils.install "#{CREW_DEST_LIB_PREFIX}/icecream/lib/pkgconfig/icecc.pc",
+                      "#{CREW_DEST_LIB_PREFIX}/pkgconfig/icecc.pc", mode: 0o644
+    FileUtils.rm_rf "#{CREW_DEST_LIB_PREFIX}/icecream/lib/pkgconfig"
+  end
+end
