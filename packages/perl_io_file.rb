31d8d76fa3bd51437ad87be2e303ff70c9758c59
Satadru Pramanik <satadru@gmail.com>
2023-05-21 23:10:28 -0400

    fix conflict

--- packages/perl_io_file.rb
+++ packages/perl_io_file.rb
@@ -4,12 +4,13 @@ class Perl_io_file < Package
   description 'Supply object methods for filehandles'
   homepage 'https://metacpan.org/pod/IO::File'
   version '1.51-perl5.36'
-  license 'GPL-1+ or Artistic'
   compatibility 'all'
+  license 'GPL-1+ or Artistic'
   source_url 'https://cpan.metacpan.org/authors/id/T/TO/TODDR/IO-1.51.tar.gz'
   source_sha256 '5493ea55998728cd2b7ecb8234c58fb5d5df27098d0f07addca22444d7616ce0'
 
   depends_on 'perl_file_temp' => :build
+  depends_on 'glibc' # R
 
   no_compile_needed
 
@@ -24,5 +25,25 @@ class Perl_io_file < Package
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    @perl_conflicts = %w[
+      File::Temp
+      IO::Pipe
+      IO::Poll
+      IO::Seekable
+      IO::Select
+      IO::Socket
+      IO::Socket::INET
+      IO::Socket::UNIX
+      IO
+      IO::Dir
+      IO::File
+      IO::Handle
+    ]
+    @perl_conflicts.each do |conflict|
+      if File.exist?("#{CREW_DEST_MAN_PREFIX}/man3/#{conflict}.3")
+        FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man3/#{conflict}.3",
+                     "#{CREW_DEST_MAN_PREFIX}/man3/#{conflict}-#{name}.3"
+      end
+    end
   end
 end
