e484b943646e1325385cef5e1d7b46ec8e79d50a
Satadru Pramanik <satadru@gmail.com>
2023-05-21 22:11:10 -0400

    updates

--- packages/perl_text_unidecode.rb
+++ packages/perl_text_unidecode.rb
@@ -10,16 +10,16 @@ class Perl_text_unidecode < Package
   source_sha256 '6c24f14ddc1d20e26161c207b73ca184eed2ef57f08b5fb2ee196e6e2e88b1c6'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-3_armv7l/perl_text_unidecode-1.30-3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-3_armv7l/perl_text_unidecode-1.30-3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-3_i686/perl_text_unidecode-1.30-3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-3_x86_64/perl_text_unidecode-1.30-3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-perl-5.36_armv7l/perl_text_unidecode-1.30-perl-5.36-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-perl-5.36_armv7l/perl_text_unidecode-1.30-perl-5.36-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-perl-5.36_i686/perl_text_unidecode-1.30-perl-5.36-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_text_unidecode/1.30-perl-5.36_x86_64/perl_text_unidecode-1.30-perl-5.36-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ccbe0cfc4c936ec4a7b56c08cea0eb00fd3624ab25d51bf8b1da66fea6d9cf11',
-     armv7l: 'ccbe0cfc4c936ec4a7b56c08cea0eb00fd3624ab25d51bf8b1da66fea6d9cf11',
-       i686: '8bed41f94818af7023c983bdf2e0974a1c7465056b22d1e20e6c9761dfa764af',
-     x86_64: '8580a3912f3760c45fbb0fb0a8b9d5faf2e963dcd699e64bfb52a7be300bc5f0'
+    aarch64: 'ecfa8e0ed890718428d85991af35a66d697183921e17700142822155f3920df0',
+     armv7l: 'ecfa8e0ed890718428d85991af35a66d697183921e17700142822155f3920df0',
+       i686: '67d8385f2ea0c3be3e7096be54caca940758cc91d18bfb564876bdaac95b8c62',
+     x86_64: '1801ecff3570deca2fe22ecc47364a36c4a5a03765cea70e9c8820f8f925d4be'
   })
 
   def self.prebuild
--- packages/perl_unicode_eastasianwidth.rb
+++ packages/perl_unicode_eastasianwidth.rb
@@ -10,16 +10,16 @@ class Perl_unicode_eastasianwidth < Package
   source_sha256 '2a5bfd926c4fe5f77e6137da2c31ac2545282ae5fec6e9af0fdd403555a90ff4'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-3_armv7l/perl_unicode_eastasianwidth-12.0-3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-3_armv7l/perl_unicode_eastasianwidth-12.0-3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-3_i686/perl_unicode_eastasianwidth-12.0-3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-3_x86_64/perl_unicode_eastasianwidth-12.0-3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-perl5.36_armv7l/perl_unicode_eastasianwidth-12.0-perl5.36-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-perl5.36_armv7l/perl_unicode_eastasianwidth-12.0-perl5.36-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-perl5.36_i686/perl_unicode_eastasianwidth-12.0-perl5.36-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_unicode_eastasianwidth/12.0-perl5.36_x86_64/perl_unicode_eastasianwidth-12.0-perl5.36-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '3b835e5f74b86606da08cf160c10a39746b259ef16cddcd7841406d0c65f3e40',
-     armv7l: '3b835e5f74b86606da08cf160c10a39746b259ef16cddcd7841406d0c65f3e40',
-       i686: 'e20824f9b8574b51cc6765297686d426ac0cc294db24d587cdb55ab1fbf95dff',
-     x86_64: 'd53287ba65526e878280aeec684193f34dc30738d1c99f80ff503f6d4c01b151'
+    aarch64: 'fffb5ae93e047955c1d23884d57096d2ddc89421a3618b6e01eec821a8932aa4',
+     armv7l: 'fffb5ae93e047955c1d23884d57096d2ddc89421a3618b6e01eec821a8932aa4',
+       i686: 'de7609f68c9554ae6f24089862d5adc56d7024608888170431a98d3d8724876d',
+     x86_64: 'e2e72c7e9624ba4f97190fbfa038dd3c7dbb1636ab2c31e56ed8af0c86d78746'
   })
 
   def self.prebuild
