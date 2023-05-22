d80eb8be4caa596f115d46782a9289c6852963ab
Satadru Pramanik <satadru@gmail.com>
2023-05-21 22:59:40 -0400

    fix conflict

--- packages/perl_file_spec.rb
+++ packages/perl_file_spec.rb
@@ -4,10 +4,12 @@ class Perl_file_spec < Package
   description 'Portably perform operations on file names'
   homepage 'https://metacpan.org/pod/File::Spec'
   version '3.75-perl5.36'
-  license 'GPL-1+ or Artistic'
   compatibility 'all'
+  license 'GPL-1+ or Artistic'
   source_url 'https://cpan.metacpan.org/authors/id/X/XS/XSAWYERX/PathTools-3.75.tar.gz'
   source_sha256 'a558503aa6b1f8c727c0073339081a77888606aa701ada1ad62dd9d8c3f945a2'
+
+  depends_on 'glibc' # R
 
   no_compile_needed
 
@@ -22,5 +24,21 @@ class Perl_file_spec < Package
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    @perl_conflicts = %w[
+      Cwd
+      File::Spec
+      File::Spec::AmigaOS
+      File::Spec::Cygwin
+      File::Spec::Epoc
+      File::Spec::Functions
+      File::Spec::Mac
+      File::Spec::OS2
+      File::Spec::Unix
+      File::Spec::VMS
+      File::Spec::Win32
+    ]
+    @perl_conflicts.each do |conflict|
+      FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man3/#{conflict}.3", "#{CREW_DEST_MAN_PREFIX}/man3/#{conflict}-#{name}.3"
+    end
   end
 end
