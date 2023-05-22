82bbb41dc9f4e8d48edca86b002b9c84794d331c
Satadru Pramanik <satadru@gmail.com>
2023-05-21 22:07:51 -0400

    update perl_glib

--- packages/perl_glib.rb
+++ packages/perl_glib.rb
@@ -10,18 +10,20 @@ class Perl_glib < Package
   source_sha256 '7316a0c1e7cc5cb3db7211214f45d7bdc2354365a680ac4bd3ac8bf06d1cb500'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293_armv7l/perl_glib-1.3293-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293_armv7l/perl_glib-1.3293-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293_i686/perl_glib-1.3293-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293_x86_64/perl_glib-1.3293-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293-perl5.36_armv7l/perl_glib-1.3293-perl5.36-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293-perl5.36_armv7l/perl_glib-1.3293-perl5.36-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293-perl5.36_i686/perl_glib-1.3293-perl5.36-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_glib/1.3293-perl5.36_x86_64/perl_glib-1.3293-perl5.36-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'c761c75189294107f3ceddb7daf065fbca3cc3d4ff3e189e514e43b0c53e0abd',
-     armv7l: 'c761c75189294107f3ceddb7daf065fbca3cc3d4ff3e189e514e43b0c53e0abd',
-       i686: 'dffe05fe906e39a96c4b8b007a2b7239ec7144ef16a5900c774fdac3fa7072dd',
-     x86_64: '2e15f22ae728ee3fb06025f4ad53f77b69023da297023a7500d32b80b92dfe30'
+    aarch64: '0cf6846578f858302924f7cf2d63b475dbe86ea93dc8869a96e4823f31b40321',
+     armv7l: '0cf6846578f858302924f7cf2d63b475dbe86ea93dc8869a96e4823f31b40321',
+       i686: 'd07fcde84f93a3e4f6f325d4515b6482342f7d8fb84abadd121571ec2a21a2c5',
+     x86_64: '207db96d819ac87be27180c54a44794246dca912ac95a6964f6d8c7af27981bb'
   })
 
+  depends_on 'glibc' # R
+  depends_on 'glib' # R
   depends_on 'perl_extutils_depends' => :build
   depends_on 'perl_extutils_pkgconfig' => :build
 
--- packages/perl_gtk3.rb
+++ packages/perl_gtk3.rb
@@ -5,7 +5,7 @@ class Perl_gtk3 < Package
   homepage 'https://metacpan.org/pod/Gtk3'
   version '0.038-perl5.36'
   license 'GPL 2.1+'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://cpan.metacpan.org/authors/id/X/XA/XAOC/Gtk3-0.038.tar.gz'
   source_sha256 '70dc4bf2aa74981c79e15fd298d998e05a92eba4811f1ad5c9f1f4de37737acc'
 
