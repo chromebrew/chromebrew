3875b57906a0e53706e855385a2f983736b95d29
Satadru Pramanik <satadru@gmail.com>
2023-05-21 22:27:36 -0400

    update linebreak

--- packages/perl_gcstring_linebreak.rb
+++ packages/perl_gcstring_linebreak.rb
@@ -1,9 +1,10 @@
 require 'package'
+require_relative 'perl_unicode_linebreak'
 
 class Perl_gcstring_linebreak < Package
   description 'UAX 14 Unicode Line Breaking Algorithm - Perl binding Unicode::LineBreak Unicode::GCString'
   homepage 'http://search.cpan.org/~nezumi/Unicode-LineBreak-2018.003/lib/Unicode/LineBreak.pod'
-  version '2019.001-2'
+  version Perl_unicode_linebreak.version.to_s
   license 'GPL-1+ or Artistic'
   compatibility 'all'
 
