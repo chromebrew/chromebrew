978a087c7b706985c3e28967d014ea06df613ffe
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-05-23 19:55:28 -0400

    Docker (#7828)
    * Docker WIP
    
    * bump version to 23.0.0-rc.3
    
    * Update docker to 23.0.1
    
    * update runc
    
    * update containers_common
    
    * update containerd
    
    * update docker
    
    * add docker preflight
    
    * update docker preflight message

--- packages/runc.rb
+++ packages/runc.rb
@@ -6,23 +6,21 @@ require 'package'
 class Runc < Package
   description 'CLI tool for managing OCI compliant containers'
   homepage 'https://runc.io/'
-  version '1.1.4'
+  version '1.1.7'
   license 'Apache'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/opencontainers/runc.git'
   git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/runc/1.1.4_armv7l/runc-1.1.4-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/runc/1.1.4_armv7l/runc-1.1.4-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/runc/1.1.4_i686/runc-1.1.4-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/runc/1.1.4_x86_64/runc-1.1.4-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/runc/1.1.7_armv7l/runc-1.1.7-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/runc/1.1.7_armv7l/runc-1.1.7-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/runc/1.1.7_x86_64/runc-1.1.7-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b096c698a50db38a576e30bc84c1a589c72384972bf70df01e91be76b19291c5',
-     armv7l: 'b096c698a50db38a576e30bc84c1a589c72384972bf70df01e91be76b19291c5',
-       i686: '25956d2f26c123d9ddb1fd14ef1866913ea558ae2ea23c7932757e9024b2c060',
-     x86_64: '9e16ffb288b01c5d8245bcd21bb83e0aa8e3159c91546314e4189636831d71ea'
+    aarch64: '9b26302b3cd4803d516c069901471f81ba5a043bc4e9873a87b3992f618484c8',
+     armv7l: '9b26302b3cd4803d516c069901471f81ba5a043bc4e9873a87b3992f618484c8',
+     x86_64: '8302c85fcfabe3766323a583c65b578c8af6c0ada28e44f247d82b1c9d4dcbb4'
   })
 
   depends_on 'libseccomp' # R
@@ -41,8 +39,6 @@ class Runc < Package
   end
 
   def self.install
-    FileUtils.mkdir_p %W[#{CREW_DEST_PREFIX}/bin/ #{CREW_DEST_MAN_PREFIX}/man8
-                         #{CREW_DEST_PREFIX}/share/bash-completion/completions]
     FileUtils.install 'runc', "#{CREW_DEST_PREFIX}/bin/runc", mode: 0o755
     FileUtils.install 'contrib/completions/bash/runc',
                       "#{CREW_DEST_PREFIX}/share/bash-completion/completions/runc", mode: 0o644
