5811aed814ec65e09239ec4572a4b4842fb1aa09
Satadru Pramanik <satadru@gmail.com>
2023-05-23 13:54:39 -0400

    update libheif, libsm

--- packages/libheif.rb
+++ packages/libheif.rb
@@ -3,24 +3,22 @@ require 'package'
 class Libheif < Package
   description 'libheif is a ISO/IEC 23008-12:2017 HEIF file format decoder and encoder.'
   homepage 'https://github.com/strukturag/libheif'
-  @_ver = '1.15.1'
+  @_ver = '1.16.2'
   version @_ver
   license 'GPL-3'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/strukturag/libheif.git'
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libheif/1.15.1_armv7l/libheif-1.15.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libheif/1.15.1_armv7l/libheif-1.15.1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libheif/1.15.1_i686/libheif-1.15.1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libheif/1.15.1_x86_64/libheif-1.15.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libheif/1.16.2_armv7l/libheif-1.16.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libheif/1.16.2_armv7l/libheif-1.16.2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libheif/1.16.2_x86_64/libheif-1.16.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ba384b24e42ff1198b62cf6d58d90f0e10c758654d2a2efd3172e983997b88e0',
-     armv7l: 'ba384b24e42ff1198b62cf6d58d90f0e10c758654d2a2efd3172e983997b88e0',
-       i686: 'f605879bcb28e7d13b95f2116f180950e5f6889cba5c37172b3505e3deac2bcf',
-     x86_64: 'd017e2cbfdc71a68b7ee56f0ec473ad26c3ed330abb3ecb3d0864aed315cd539'
+    aarch64: '4f8c58cd76e80fb068037384b9df97d32fe2053ccc8694548a6826118ea08d43',
+     armv7l: '4f8c58cd76e80fb068037384b9df97d32fe2053ccc8694548a6826118ea08d43',
+     x86_64: '77b620b3b7bf687577ab36fd88098aaa85ffe930fa71a7a342776b4d555f8f24'
   })
 
   depends_on 'dav1d' # R
@@ -33,10 +31,11 @@ class Libheif < Package
   depends_on 'libjpeg' # R
   depends_on 'libpng' # R
   depends_on 'libtiff' # L
+  depends_on 'libwebp' # R
   depends_on 'libx265' # R
   depends_on 'rav1e' # R
-  depends_on 'zlibpkg' # R
   depends_on 'svt_av1' # R
+  depends_on 'zlibpkg' # R
 
   def self.build
     system "cmake -B builddir \
