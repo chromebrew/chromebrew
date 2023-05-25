a695c8f901fab2360b20f694f031bd16ef64db4f
Satadru Pramanik <satadru@gmail.com>
2023-05-25 15:19:48 -0400

    update libpng

--- packages/libpng.rb
+++ packages/libpng.rb
@@ -3,40 +3,45 @@ require 'package'
 class Libpng < Package
   description 'libpng is the official PNG reference library.'
   homepage 'http://libpng.org/pub/png/libpng.html'
-  version '1.6.37'
+  version '1.6.39'
   license 'libpng2'
   compatibility 'all'
-  source_url 'https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.xz'
-  source_sha256 '505e70834d35383537b6491e7ae8641f1a4bed1876dbfe361201fc80868d88ca'
+  source_url 'https://git.code.sf.net/p/libpng/code.git'
+  git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.37_armv7l/libpng-1.6.37-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.37_armv7l/libpng-1.6.37-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.37_i686/libpng-1.6.37-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.37_x86_64/libpng-1.6.37-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.39_armv7l/libpng-1.6.39-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.39_armv7l/libpng-1.6.39-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.39_i686/libpng-1.6.39-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpng/1.6.39_x86_64/libpng-1.6.39-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a38812b2665ff8c46b6f7b1398bc9a77875f3db199e71c5181e5db195063fbbb',
-     armv7l: 'a38812b2665ff8c46b6f7b1398bc9a77875f3db199e71c5181e5db195063fbbb',
-       i686: '2cc3c4a73a0e33e8d35ea6c9da5443b24cb6f2250475d9cc63016d438968fa4b',
-     x86_64: '30be8b66be155cdd233dcc7b638ba2cd173e347165f088c71b1d9dab928cca95'
+    aarch64: '931e483e593bede53cc971755a9d8e6dbaa023d05017855a1aa0673640882ef9',
+     armv7l: '931e483e593bede53cc971755a9d8e6dbaa023d05017855a1aa0673640882ef9',
+       i686: '8380cd281ec792f8819621b0f27dc52a1c4d466ca44e2ac5a046220b3267dd1c',
+     x86_64: '6813487ba62c178ca6fab45a9c2a851d48ea57f4cdd3b85be184be647195503a'
   })
 
   depends_on 'zlibpkg'
-
-  def self.patch
-    # Fix /usr/bin/file: No such file or directory
-    system 'filefix'
-  end
+  depends_on 'glibc' # R
 
   def self.build
-    system "./configure #{CREW_OPTIONS} \
-            --disable-dependency-tracking \
-            --disable-maintainer-mode"
-    system 'make'
+    system "cmake \
+      -B builddir -G Ninja \
+      #{CREW_CMAKE_OPTIONS.gsub('-mfpu=vfpv3-d16', '-mfpu=neon-fp16')} \
+      -DPNG_STATIC=OFF \
+      -Wno-dev"
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
+  end
+
+  def self.postinstall
+    return unless File.exist?("#{CREW_PREFIX}/bin/gdk-pixbuf-query-loaders")
+
+    system 'gdk-pixbuf-query-loaders',
+           '--update-cache'
   end
 end
