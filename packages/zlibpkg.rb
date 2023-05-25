f4a82abbd4f328f5790db0b88da7a50e48ef9098
Satadru Pramanik <satadru@gmail.com>
2023-05-25 15:21:42 -0400

    rebuild zlibpkg

--- packages/zlibpkg.rb
+++ packages/zlibpkg.rb
@@ -4,7 +4,7 @@ class Zlibpkg < Package
   description 'zlib is a massively spiffy yet delicately unobtrusive compression library.'
   homepage 'https://www.zlib.net/'
   @_ver = '1.2.13'
-  version @_ver
+  version "#{@_ver}-1"
   # When upgrading zlibpkg, be sure to upgrade minizip in tandem.
   # The following breaks the installer script.
   # puts "#{self} version differs from Minizip version #{Minizip.version}".orange if @_ver != Minizip.version
@@ -14,16 +14,16 @@ class Zlibpkg < Package
   source_sha256 'b3a24de97a8fdbc835b9833169501030b8977031bcb54b3b3ac13740f846ab30'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13_armv7l/zlibpkg-1.2.13-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13_armv7l/zlibpkg-1.2.13-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13_i686/zlibpkg-1.2.13-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13_x86_64/zlibpkg-1.2.13-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13-1_armv7l/zlibpkg-1.2.13-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13-1_armv7l/zlibpkg-1.2.13-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13-1_i686/zlibpkg-1.2.13-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.2.13-1_x86_64/zlibpkg-1.2.13-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a8b06cf54f2cf380588f65aad72287dba2e79e89a9ff13da64e2f54e0e79dd2c',
-     armv7l: 'a8b06cf54f2cf380588f65aad72287dba2e79e89a9ff13da64e2f54e0e79dd2c',
-       i686: '3b5f806293a0a82dc89198ced41a72b81d4e2e6d0cd5542d64471d92e877ebc6',
-     x86_64: '535eb1176d122ebd9bbae77d9e6a3c0cd055d20ec4908ad2aa47971badc3e527'
+    aarch64: '4d1da86d9682dcf90538cce22f4650b7c2a35dff251d613783b446023eb7d211',
+     armv7l: '4d1da86d9682dcf90538cce22f4650b7c2a35dff251d613783b446023eb7d211',
+       i686: 'e772f09b58ac0ea37e6845da3aefcb217631de37daeaa9863d7f96d74c3dc45b',
+     x86_64: 'a8a3748e34f5a19bf2696cfef5a95642dd138996ae06acda06a1527a0a793dd7'
   })
 
   depends_on 'glibc' # R
@@ -44,5 +44,7 @@ class Zlibpkg < Package
 
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+    # Remove static library.
+    FileUtils.rm "#{CREW_DEST_LIB_PREFIX}/libz.a"
   end
 end
