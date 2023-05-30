65c67e70d922b4c2d2f1eaf63e68bd998a25f562
Ed Reel <edreel@gmail.com>
2023-05-30 02:55:52 -0500

    Nautilus 44.0 => 44.2

--- packages/nautilus.rb
+++ packages/nautilus.rb
@@ -6,22 +6,21 @@ require 'package'
 class Nautilus < Package
   description 'Default file manager for GNOME'
   homepage 'https://wiki.gnome.org/Apps/Files'
-  @_ver = '44.0'
-  version @_ver
+  version '44.2'
   license 'GPLv3'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/nautilus.git'
-  git_hashtag @_ver
+  git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nautilus/44.0_armv7l/nautilus-44.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nautilus/44.0_armv7l/nautilus-44.0-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nautilus/44.0_x86_64/nautilus-44.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nautilus/44.2_armv7l/nautilus-44.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nautilus/44.2_armv7l/nautilus-44.2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nautilus/44.2_x86_64/nautilus-44.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'fb635cde6d3b7b4df435c5ab226f8519ace60edd03930628dfb7ee4fd0d05e85',
-     armv7l: 'fb635cde6d3b7b4df435c5ab226f8519ace60edd03930628dfb7ee4fd0d05e85',
-     x86_64: '327eef2ac9bc742575bedb967c7c49e3e19335f910815a93c0755843c38abe63'
+    aarch64: '7aa9ac926a916451288e17fa7eff600afb79530c9eade1bbf7724049ad5a9059',
+     armv7l: '7aa9ac926a916451288e17fa7eff600afb79530c9eade1bbf7724049ad5a9059',
+     x86_64: '8bad0c1b95bd31109c9df5811aeeda5a15d18e2b2756523136b9e072fca53af0'
   })
 
   depends_on 'appstream_glib' => :build
@@ -53,6 +52,7 @@ class Nautilus < Package
   depends_on 'pango' # R
   depends_on 'tracker3_miners'
   depends_on 'tracker3' # R
+  depends_on 'vulkan_headers' => :build
   depends_on 'vulkan_icd_loader' # R
 
   gnome
