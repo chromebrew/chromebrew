8cc05fad97e71ba374a0d5dce339dbbe1d4c8f38
Ed Reel <edreel@gmail.com>
2023-05-31 09:21:11 -0500

    Github_cli 2.29.0 => 2.30.0 (#8320)

--- packages/github_cli.rb
+++ packages/github_cli.rb
@@ -3,20 +3,20 @@ require 'package'
 class Github_cli < Package
   description 'Official Github CLI tool'
   homepage 'https://cli.github.com/'
-  version '2.29.0'
+  version '2.30.0'
   license 'MIT'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/cli/cli/releases/download/v2.29.0/gh_2.29.0_linux_armv6.tar.gz',
-     armv7l: 'https://github.com/cli/cli/releases/download/v2.29.0/gh_2.29.0_linux_armv6.tar.gz',
-       i686: 'https://github.com/cli/cli/releases/download/v2.29.0/gh_2.29.0_linux_386.tar.gz',
-     x86_64: 'https://github.com/cli/cli/releases/download/v2.29.0/gh_2.29.0_linux_amd64.tar.gz'
+    aarch64: 'https://github.com/cli/cli/releases/download/v2.30.0/gh_2.30.0_linux_armv6.tar.gz',
+     armv7l: 'https://github.com/cli/cli/releases/download/v2.30.0/gh_2.30.0_linux_armv6.tar.gz',
+       i686: 'https://github.com/cli/cli/releases/download/v2.30.0/gh_2.30.0_linux_386.tar.gz',
+     x86_64: 'https://github.com/cli/cli/releases/download/v2.30.0/gh_2.30.0_linux_amd64.tar.gz'
   })
   source_sha256({
-    aarch64: '193bc95e024a3e53a3d3850fc2e619ddc9ca1bf3de384abb0f15e68ff1aaefbd',
-     armv7l: '193bc95e024a3e53a3d3850fc2e619ddc9ca1bf3de384abb0f15e68ff1aaefbd',
-       i686: '99c8b66f18b1d5e71950f2e04f8e41f466316179f838816d73c4c94fc632a7a7',
-     x86_64: '9fe05f43a11a7bf8eacf731422452d1997e6708d4160ef0efcb13c103320390e'
+    aarch64: '472424e5739fb626921423f04cc30aed14605eb019ec860104351baf1a7307d4',
+     armv7l: '472424e5739fb626921423f04cc30aed14605eb019ec860104351baf1a7307d4',
+       i686: 'b44c30f3662c27f60d64d420f4c8fe62d5670592de894faa245405c710a07e1c',
+     x86_64: '5aee45bd42a27f5be309373c326e45cbcc7f04591b1798581a3094af767225b7'
   })
 
   no_compile_needed
