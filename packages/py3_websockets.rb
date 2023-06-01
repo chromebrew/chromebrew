9b76febec292a87cc1c510951848f4fe8e57a04b
Satadru Pramanik <satadru@gmail.com>
2023-06-01 13:02:10 -0400

    modify py3 install setups...

--- packages/py3_websockets.rb
+++ packages/py3_websockets.rb
@@ -12,22 +12,17 @@ class Py3_websockets < Package
 
   depends_on 'python3' # L
   depends_on 'py3_pip' # L
-  no_compile_needed
 
-  def self.postinstall
-    puts "Installing #{name.gsub('Py3_', '')} python module. This may take a while...".lightblue
-    system "pip install #{name.gsub('Py3_', '')}", exception: false
-    @pip_files = `pip show -f #{name.gsub('Py3_', '')}`.chomp
+  def self.install
+    puts "Installing #{name.gsub('py3_', '')} python module. This may take a while...".lightblue
+    system "python -s -m pip install #{name.gsub('py3_', '')}", exception: false
+    @pip_files = `python -s -m pip show -f #{name.gsub('py3_', '')}`.chomp
     @pip_files_base = @pip_files[/(?<=Location: ).*/, 0].concat('/')
     @pip_files_lines = @pip_files[/(?<=Files:\n)[\W|\w]*/, 0].split
-    File.open("#{CREW_META_PATH}#{name.downcase}.filelist", 'w+') do |f|
-      @pip_files_lines.each do |pip_file|
-        f.puts File.expand_path("#{@pip_files_base}#{pip_file}")
-      end
+    @pip_files_lines.each do |pip_file|
+      @pip_path = File.expand_path("#{@pip_files_base}#{pip_file}")
+      @destpath = "#{CREW_DEST_DIR.chomp("/")}#{@pip_path}"
+      FileUtils.install @pip_path, @destpath
     end
-  end
-
-  def self.remove
-    system "pip uninstall #{name.gsub('Py3_', '')} -y", exception: false
   end
 end
