66735b6d3bb8e70784a8195bc329ff10a4b6d8d3
Cassandra Watergate <55339220+saltedcoffii@users.noreply.github.com>
2023-06-02 04:13:05 +0000

    new package yt-dlp (#7523)
    * py3_cffi => 1.15.1
    
    * new package brotlicffi => 1.0.9.2
    
    * remove empty extra file
    
    * yt_dlp.rb: fix misspelled dependency
    
    * yt_dlp requires pandoc to build documentation
    
    * FHS compliance, screwy makefile
    
    * fix manpage moving
    
    * make manfolder variable more obviously a variable (ruby called as method)
    
    * manfolder is STRING VARIABLE not a method
    
    * thanks @supechicken and @uberhacker
    
    * fix description and homepage
    
    * making completions is unnecessary: its done during install
    
    * update packages, use pip, merge master
    
    * use standard install for py3_cffi
    
    * modify py3 install setups...
    
    * lint
    
    * update packages.yaml
    
    * add py3_cffi binaries
    
    * add source_url skip
    
    * add filelists
    
    ---------
    
    Co-authored-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_certifi.rb
+++ packages/py3_certifi.rb
@@ -3,33 +3,38 @@ require 'package'
 class Py3_certifi < Package
   description 'Certifi provides Mozilla\'s CA Bundle.'
   homepage 'https://certifi.io/' # 403 Forbidden on 2021.05.08, still 403 Forbidden on 2021.10.13
-  @_ver = '2021.10.08'
+  @_ver = '2023.05.07'
   version "#{@_ver}-py3.11"
   license 'MPL-2.0'
   compatibility 'all'
-  source_url 'https://github.com/certifi/python-certifi.git'
-  git_hashtag @_ver
+  source_url 'SKIP'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2021.10.08-py3.11_armv7l/py3_certifi-2021.10.08-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2021.10.08-py3.11_armv7l/py3_certifi-2021.10.08-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2021.10.08-py3.11_i686/py3_certifi-2021.10.08-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2021.10.08-py3.11_x86_64/py3_certifi-2021.10.08-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2023.05.07-py3.11_armv7l/py3_certifi-2023.05.07-py3.11-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2023.05.07-py3.11_armv7l/py3_certifi-2023.05.07-py3.11-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2023.05.07-py3.11_i686/py3_certifi-2023.05.07-py3.11-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_certifi/2023.05.07-py3.11_x86_64/py3_certifi-2023.05.07-py3.11-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'abca6245b393cc3068268c77540d9bab205c7c124e0b6cda6fa55e7dc570f495',
-     armv7l: 'abca6245b393cc3068268c77540d9bab205c7c124e0b6cda6fa55e7dc570f495',
-       i686: 'fcb25b8e3aba29b08363ef4bd61f309df750d798ab461d20c7cd548122b2ac62',
-     x86_64: 'b7c9747a94f8fab4b51d967291d99bc9b41533ef2db9ba3124c97aadd5ec5d6a'
+    aarch64: '357f8e30de7b9d2da3f7e4b851e36a7958f9b25723c7814a46db8be7f960814c',
+     armv7l: '357f8e30de7b9d2da3f7e4b851e36a7958f9b25723c7814a46db8be7f960814c',
+       i686: '6883f510a4bfc7622440b0b912f631e766555d2fa6304747e73f2a1a42ca1739',
+     x86_64: '8a9eebf9bca1c0d4302a1296ecd1fdf32b4021769dd88952fdee368e83d24558'
   })
 
-  depends_on 'python3' => :build
-
-  def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
+  depends_on 'python3' # L
+  depends_on 'py3_pip' # L
 
   def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
+    puts "Installing #{name.gsub('py3_', '')} python module. This may take a while...".lightblue
+    system "python -s -m pip install #{name.gsub('py3_', '')}", exception: false
+    @pip_files = `python -s -m pip show -f #{name.gsub('py3_', '')}`.chomp
+    @pip_files_base = @pip_files[/(?<=Location: ).*/, 0].concat('/')
+    @pip_files_lines = @pip_files[/(?<=Files:\n)[\W|\w]*/, 0].split
+    @pip_files_lines.each do |pip_file|
+      @pip_path = File.expand_path("#{@pip_files_base}#{pip_file}")
+      @destpath = "#{CREW_DEST_DIR.chomp('/')}#{@pip_path}"
+      FileUtils.install @pip_path, @destpath
+    end
   end
 end
