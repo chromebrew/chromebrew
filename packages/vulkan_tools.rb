dc230b35ad4c993fded77506a454731bc834991d
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-06-09 15:58:46 -0400

    Mesa => 23.1.2 (#8361)
    * Skype 8.87.0.403 => 8.87.0.406 (#7292)
    
    * update vulkan packages
    
    * mesa => 23.1.2
    
    * cleanup vulkan filelists
    
    * fixup deps
    
    ---------
    
    Co-authored-by: Ed Reel <edreel@gmail.com>

--- packages/vulkan_tools.rb
+++ packages/vulkan_tools.rb
@@ -6,25 +6,24 @@ require 'package'
 class Vulkan_tools < Package
   description 'Vulkan Utilities and Tools'
   homepage 'https://www.khronos.org/vulkan/'
-  version '1.3.240'
+  version '1.3.251'
   license 'custom'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/KhronosGroup/Vulkan-Tools.git'
   git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_tools/1.3.240_armv7l/vulkan_tools-1.3.240-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_tools/1.3.240_armv7l/vulkan_tools-1.3.240-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_tools/1.3.240_i686/vulkan_tools-1.3.240-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_tools/1.3.240_x86_64/vulkan_tools-1.3.240-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_tools/1.3.251_armv7l/vulkan_tools-1.3.251-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_tools/1.3.251_armv7l/vulkan_tools-1.3.251-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/vulkan_tools/1.3.251_x86_64/vulkan_tools-1.3.251-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b3f3b0490b311b16d6d589369679ee46df7df92e9b09af1da06242a6fec1e32a',
-     armv7l: 'b3f3b0490b311b16d6d589369679ee46df7df92e9b09af1da06242a6fec1e32a',
-       i686: '414ecb4752b785f2328a9a52ba922554404cbc7e2dd0711b0c3b70cc24ad352c',
-     x86_64: '1d42540b628e8ebcd42a83cbfe10673f203e99e80248f7b58731c679d9c86eec'
+    aarch64: '170c874b3cc837e84154d248952b6cc83fc6a46a84bfec5096d8630f7b8f70b8',
+     armv7l: '170c874b3cc837e84154d248952b6cc83fc6a46a84bfec5096d8630f7b8f70b8',
+     x86_64: '61db4831ab580e0c5e6f1672a9fe252fc2aa82294e7c26358d52323173046d7c'
   })
 
+  depends_on 'gcc_dev' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'glslang' => :build
@@ -41,10 +40,7 @@ class Vulkan_tools < Package
 
   def self.build
     system 'scripts/update_deps.py'
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "env #{CREW_ENV_OPTIONS} \
-        cmake -G Ninja \
+    system "cmake -G Ninja -B builddir \
         #{CREW_CMAKE_OPTIONS} \
         -DVULKAN_HEADERS_INSTALL_DIR=#{CREW_PREFIX} \
         -DCMAKE_INSTALL_SYSCONFDIR=#{CREW_PREFIX}/etc \
@@ -55,13 +51,8 @@ class Vulkan_tools < Package
         -DBUILD_WSI_WAYLAND_SUPPORT=On \
         -DBUILD_CUBE=ON \
         -DBUILD_VULKANINFO=ON \
-        -DBUILD_ICD=OFF \
-        .."
-    end
-    Dir.mkdir 'builddir-wayland'
-    Dir.chdir 'builddir-wayland' do
-      system "env #{CREW_ENV_OPTIONS} \
-        cmake -G Ninja \
+        -DBUILD_ICD=OFF"
+    system "cmake -G Ninja -B builddir-wayland \
         #{CREW_CMAKE_OPTIONS} \
         -DVULKAN_HEADERS_INSTALL_DIR=#{CREW_PREFIX} \
         -DCMAKE_INSTALL_SYSCONFDIR=#{CREW_PREFIX}/etc \
@@ -73,15 +64,13 @@ class Vulkan_tools < Package
         -DBUILD_CUBE=ON \
         -DCUBE_WSI_SELECTION=WAYLAND \
         -DBUILD_VULKANINFO=OFF \
-        -DBUILD_ICD=OFF \
-        .."
-    end
-    system 'samu -C builddir'
-    system 'samu -C builddir-wayland'
+        -DBUILD_ICD=OFF"
+    system "#{CREW_NINJA} -C builddir"
+    system "#{CREW_NINJA} -C builddir-wayland"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     FileUtils.install 'builddir-wayland/cube/vkcube-wayland', "#{CREW_DEST_PREFIX}/bin/vkcube-wayland", mode: 0o755
   end
 end
