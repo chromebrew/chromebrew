ad2ce58e1b3a249d6207d7ebc6606d7e83c818bb
Zopolis4 <creatorsmithmdt@gmail.com>
2023-06-09 18:55:43 +1000

    Remove leftover bintray url from wkhtmltox

--- packages/wkhtmltox.rb
+++ packages/wkhtmltox.rb
@@ -15,15 +15,6 @@ class Wkhtmltox < Package
     x86_64: '049b2cdec9a8254f0ef8ac273afaf54f7e25459a273e27189591edc7d7cf29db'
   })
 
-  binary_url({
-    i686: 'https://dl.bintray.com/chromebrew/chromebrew/wkhtmltox-0.12.4-1-chromeos-i686.tar.xz',
-    x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/wkhtmltox-0.12.4-1-chromeos-x86_64.tar.xz'
-  })
-  binary_sha256({
-    i686: '6b8fea9c8369203b53d57d3b1f3f22789379a72122d4fda19980aa0622e59717',
-    x86_64: '51b6abcd0d037008c01b043b75c0679f10e105808aacb69be6e7d313c842516c'
-  })
-
   def self.install
     FileUtils.mkdir_p CREW_DEST_PREFIX.to_s
     FileUtils.mkdir_p CREW_DEST_LIB_PREFIX.to_s
