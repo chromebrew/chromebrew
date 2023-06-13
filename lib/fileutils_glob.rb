59f0c63e9cf0c52575e2ce88c4d62e597dc0deee
supechicken <supechicken666@gmail.com>
2023-06-13 23:55:00 +0800

    Fix bugs

--- lib/fileutils_glob.rb
+++ lib/fileutils_glob.rb
@@ -3,18 +3,20 @@ require 'fileutils'
 
 module FileUtils
   class << self
-    def glob_handler(first_arg, *args, **opts)
+    def glob_handler(*args, **opts)
+      path_index = __callee__.eql?(:install) ? 1 : 0
+
       # replace ~ to home directory
-      first_arg[0] = Dir.home if first_arg[0] == '~'
+      args[path_index][0] = Dir.home if first_arg[path_index][0] == '~'
+
+      # parse glob
+      args[path_index] = Dir[args[path_index]]
 
       # call original method with resolved glob
-      send("orig_#{__callee__}", Dir[first_arg], *args, **opts)
+      send("orig_#{__callee__}", *args, **opts)
     end
 
-    %w[
-      chmod chmod_R chown chown_R cp cp_r
-      install ln ln_s ln_sf mv rm rm_f rm_rf rmdir
-    ].each do |method|
+    %w[chmod chmod_R cp cp_r install ln ln_s ln_sf mv rm rm_f rm_rf rmdir].each do |method|
       # rename existing methods to orig_#{method_name}
       alias_method "orig_#{method}".to_sym, method.to_sym
       remove_method method.to_sym
