978de079571bb8e39509ab61e1f6408d08d88fe7
Satadru Pramanik <satadru@gmail.com>
2023-06-14 15:39:42 -0400

    add more libs to gcc_lib

--- lib/const.rb
+++ lib/const.rb
@@ -1,6 +1,6 @@
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.34.3'
+CREW_VERSION = '1.34.4'
 
 # kernel architecture
 KERN_ARCH = `uname -m`.chomp
--- packages/gcc_lib.rb
+++ packages/gcc_lib.rb
@@ -2,26 +2,27 @@ require 'package'
 require_relative 'gcc_build'
 
 class Gcc_lib < Package
-  description 'Libgcc'
+  description 'GCC shared libs except libgccjit'
   homepage Gcc_build.homepage
-  version '13.1.0' # Do not use @_ver here, it will break the installer.
+  version '13.1.0-1' # Do not use @_ver here, it will break the installer.
   license Gcc_build.license
-  # When upgrading gcc_build, be sure to upgrade gcc_lib and gcc_dev in tandem.
-  puts "#{self} version differs from gcc version #{Gcc_build.version}".orange if version != Gcc_build.version.to_s
+  # When upgrading gcc_build, be sure to upgrade gcc_lib, gcc_dev, and libssp in tandem.
+  puts "#{self} version differs from gcc version #{Gcc_build.version}".orange if version.to_s.gsub(/-.*/,
+                                                                                                   '') != Gcc_build.version.to_s
   compatibility 'all'
   source_url 'SKIP'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0_armv7l/gcc_lib-13.1.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0_armv7l/gcc_lib-13.1.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0_i686/gcc_lib-13.1.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0_x86_64/gcc_lib-13.1.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0-1_armv7l/gcc_lib-13.1.0-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0-1_armv7l/gcc_lib-13.1.0-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0-1_i686/gcc_lib-13.1.0-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gcc_lib/13.1.0-1_x86_64/gcc_lib-13.1.0-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '43e835c2e924a80bf0fd87a853a3f5692e3db97e6368a820f08da6065086e540',
-     armv7l: '43e835c2e924a80bf0fd87a853a3f5692e3db97e6368a820f08da6065086e540',
-       i686: '563c75cd3dba893bf6231f84e328fe368ec4307b599ad6bff72a8d34cd2e9251',
-     x86_64: '98e1240095dd7dbfb3539f5c6d1450cb01a9674733b7490a5f32eff9c8458d86'
+    aarch64: '3c5c72d2955468c9aa44014ad3a880a3c988f5e9da174225634965fdae1a26c5',
+     armv7l: '3c5c72d2955468c9aa44014ad3a880a3c988f5e9da174225634965fdae1a26c5',
+       i686: '395ef2e6e37c5d3e2745d83f9c7aadc90cb11387a0099578ab98f6265d90c791',
+     x86_64: '30fc5cd851e8785798dc5ddaf1f0fdd905e0f1abd0d3dd581cf0b12770c591e6'
   })
 
   depends_on 'gcc_build' => :build
@@ -34,7 +35,7 @@ class Gcc_lib < Package
     @filelist = File.readlines(@filelist_path, chomp: true).sort
 
     @filelist.each do |filename|
-      next unless filename.include?('.so') && filename.include?('libgcc_s')
+      next unless filename.include?('.so') && !filename.include?('libgccjit')
 
       @destpath = "#{CREW_DEST_DIR.chomp('/')}#{filename}"
       @filename_target = File.realpath(filename)
