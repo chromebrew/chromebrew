4919fbdedcb32bfe95fa0cdbc369fe60fbcc7a4d
Satadru Pramanik <satadru@gmail.com>
2023-06-21 18:40:34 -0400

    update bind to latest stable series release

--- packages/bind.rb
+++ packages/bind.rb
@@ -3,23 +3,23 @@ require 'package'
 class Bind < Package
   description 'BIND is open source software that enables you to publish your Domain Name System (DNS) information on the Internet, and to resolve DNS queries for your users.'
   homepage 'https://www.isc.org/downloads/bind/'
-  version '9.19.9'
+  version '9.18.16'
   license 'Apache-2.0, BSD, BSD-2, GPL-2, HPND, ISC and MPL-2.0'
-  compatibility 'all'
+  compatibility 'x86_64'
   source_url 'https://gitlab.isc.org/isc-projects/bind9.git'
-  git_hashtag "v#{version.gsub('.', '_')}"
+  git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.19.9_armv7l/bind-9.19.9-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.19.9_armv7l/bind-9.19.9-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.19.9_i686/bind-9.19.9-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.19.9_x86_64/bind-9.19.9-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.18.16_armv7l/bind-9.18.16-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.18.16_armv7l/bind-9.18.16-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.18.16_i686/bind-9.18.16-chromeos-i686.tar.zst',
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bind/9.18.16_x86_64/bind-9.18.16-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '9cccc756b0f853e4ab80b2b20d809480c63407ae1342e79d3b40eb4c1a8d2fd2',
-     armv7l: '9cccc756b0f853e4ab80b2b20d809480c63407ae1342e79d3b40eb4c1a8d2fd2',
-       i686: '1cb7e48d387667fa5c7d51ebb391f50a5408142214b62eaa928592c5cb524e3b',
-     x86_64: '02811689db43260e353d0c8d1ff9437d1401e59bf41d27a8fd49e52499fb4100'
+    aarch64: 'e558664b7580684c06fbb746c531e5337ab760e6bc0891e5a8d23567b0410c9d',
+     armv7l: 'e558664b7580684c06fbb746c531e5337ab760e6bc0891e5a8d23567b0410c9d',
+       i686: 'c89e967767e8e94c759e659382e1d1d9e95abea58abdc91f7838e9c4db83cac6',
+    x86_64: 'c05311d3614493bdeab6c4c09d4e26c72265e10368891fa591d5d0325b3ceecc'
   })
 
   depends_on 'e2fsprogs' # R
@@ -32,6 +32,7 @@ class Bind < Package
   depends_on 'libedit' # R
   depends_on 'libidn2' # R
   depends_on 'libnghttp2' # R
+  depends_on 'liburcu' => :build
   depends_on 'libunistring' # R
   depends_on 'libuv' # R
   depends_on 'libxml2' # R
