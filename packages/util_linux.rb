fdf6a14815cb788370ab6ccd1fe7ddb4b170a432
Ed Reel <edreel@gmail.com>
2023-06-28 21:23:42 -0500

    Util_linux 2.38.1 => 2.39.1 (#8450)
    Co-authored-by: chronos <noreply@github.com>

--- packages/util_linux.rb
+++ packages/util_linux.rb
@@ -3,23 +3,23 @@ require 'package'
 class Util_linux < Package
   description 'essential linux tools'
   homepage 'https://www.kernel.org/pub/linux/utils/util-linux/'
-  version '2.38.1'
+  version '2.39.1-py3.11'
   license 'GPL-2, LGPL-2.1, BSD-4, MIT and public-domain'
   compatibility 'all'
-  source_url 'https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.38/util-linux-2.38.1.tar.xz'
-  source_sha256 '60492a19b44e6cf9a3ddff68325b333b8b52b6c59ce3ebd6a0ecaa4c5117e84f'
+  source_url 'https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.39/util-linux-2.39.1.tar.xz'
+  source_sha256 '890ae8ff810247bd19e274df76e8371d202cda01ad277681b0ea88eeaa00286b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.38.1_armv7l/util_linux-2.38.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.38.1_armv7l/util_linux-2.38.1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.38.1_i686/util_linux-2.38.1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.38.1_x86_64/util_linux-2.38.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.39.1_armv7l/util_linux-2.39.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.39.1_armv7l/util_linux-2.39.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.39.1_i686/util_linux-2.39.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/util_linux/2.39.1_x86_64/util_linux-2.39.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '43eb0bf0f2f25a6de16b6da2117488a2af3271f19a4f2eb8cbb3d24a35c98b14',
-     armv7l: '43eb0bf0f2f25a6de16b6da2117488a2af3271f19a4f2eb8cbb3d24a35c98b14',
-       i686: '7cf66b87e3d4441096fed54c8a4b5b9414eb49530bb1938af07757fac3baae02',
-     x86_64: 'ffbdb18c1d22d32605c2a8033f795ee97dc8a0adaf7a1f4e6c83a810c36becfe'
+    aarch64: '4ce6e57e3f5fab54f18764edc9edfd059faf94b45b8e43930aa5f6f879761794',
+     armv7l: '4ce6e57e3f5fab54f18764edc9edfd059faf94b45b8e43930aa5f6f879761794',
+       i686: '9b84a3f15cfa13ef60ae498d0cf9cf1d1ca04bef988da28973367a7b313104f8',
+     x86_64: 'eae35dd81597ed8506bec34aaa7b5050c26e55325110ca4b0567299896f623eb'
   })
 
   depends_on 'bz2' # R
@@ -37,6 +37,11 @@ class Util_linux < Package
 
   patchelf
   no_env_options
+
+  def self.patch
+    # Fix sys-utils/setarch.c:90:7: error: PER_LINUX_FDPIC undeclared here
+    system "sed -i 's,PER_LINUX_FDPIC,PER_LINUX_32BIT,' sys-utils/setarch.c" if ARCH == 'i686'
+  end
 
   def self.build
     system "./configure #{CREW_OPTIONS} \
