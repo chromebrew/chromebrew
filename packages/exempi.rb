80d23e792aaafdc0584b0ede04c6941ecb3e8ebe
Satadru Pramanik <satadru@gmail.com>
2023-07-10 17:41:58 -0400

    update exempi, rebuild libvisio

--- packages/exempi.rb
+++ packages/exempi.rb
@@ -1,28 +1,28 @@
 # Adapted from Arch Linux exempi PKGBUILD at:
 # https://github.com/archlinux/svntogit-packages/raw/packages/exempi/trunk/PKGBUILD
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Exempi < Package
+class Exempi < Autotools
   description 'A library to parse XMP metadata'
   homepage 'https://libopenraw.freedesktop.org/wiki/Exempi'
-  version '2.6.3'
+  version '2.6.4'
   license 'BSD'
   compatibility 'all'
   source_url "https://gitlab.freedesktop.org/libopenraw/exempi/-/archive/#{version}/exempi-#{version}.tar.bz2"
-  source_sha256 'e79995bb3c5319293e3f2abfc9da83a9ee5a83102724336599d535d874509632'
+  source_sha256 '8d34b3860192f6d2ac017537884b099b13a492ed4884130c65da5173d5162590'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.3_armv7l/exempi-2.6.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.3_armv7l/exempi-2.6.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.3_i686/exempi-2.6.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.3_x86_64/exempi-2.6.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.4_armv7l/exempi-2.6.4-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.4_armv7l/exempi-2.6.4-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.4_i686/exempi-2.6.4-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/exempi/2.6.4_x86_64/exempi-2.6.4-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '60ae93ee0ae5c7edd00d3206dff793d5255436d37eb5bf118243bf7142393ac4',
-     armv7l: '60ae93ee0ae5c7edd00d3206dff793d5255436d37eb5bf118243bf7142393ac4',
-       i686: '5febec27428020406e8d6dd85abd5058cd2e72b47ee92dc28be7ea4997d6a29e',
-     x86_64: '17a8c579b9a51f254476675b3d2bcdda6370fae697bf25da191f569321f7cd4e'
+    aarch64: 'f9ed9074c94cbf4bc9888698b3d953e5d1174c810c09701e336eaf0555449766',
+     armv7l: 'f9ed9074c94cbf4bc9888698b3d953e5d1174c810c09701e336eaf0555449766',
+       i686: '3a1b5f6f68ec1742e71a1113a9034b47d57255782e7e81a51287d23916fcb735',
+     x86_64: 'e3651643073233be72ced1c0d4644ac7577d04b8fecd4d69be694af3adcdf950'
   })
 
   depends_on 'autoconf_archive' => :build
@@ -31,14 +31,4 @@ class Exempi < Package
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'zlibpkg' # R
-
-  def self.build
-    system 'NOCONFIGURE=1 ./autogen.sh'
-    system "./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
-  end
 end
--- packages/libvisio.rb
+++ packages/libvisio.rb
@@ -6,23 +6,23 @@ require 'buildsystems/autotools'
 class Libvisio < Autotools
   description 'Library providing ability to interpret and import visio diagrams'
   homepage 'https://wiki.documentfoundation.org/DLP/Libraries/libvisio'
-  version '0.1.7-1'
+  version '0.1.7-2'
   license 'LGPL'
   compatibility 'all'
   source_url 'https://dev-www.libreoffice.org/src/libvisio/libvisio-0.1.7.tar.xz'
   source_sha256 '8faf8df870cb27b09a787a1959d6c646faa44d0d8ab151883df408b7166bea4c'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-1_armv7l/libvisio-0.1.7-1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-1_armv7l/libvisio-0.1.7-1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-1_i686/libvisio-0.1.7-1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-1_x86_64/libvisio-0.1.7-1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-2_armv7l/libvisio-0.1.7-2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-2_armv7l/libvisio-0.1.7-2-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-2_i686/libvisio-0.1.7-2-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libvisio/0.1.7-2_x86_64/libvisio-0.1.7-2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'cf522b359e4a8c916843accd9ad20b11ea9f5a5d02b109cbada695c484b0695c',
-     armv7l: 'cf522b359e4a8c916843accd9ad20b11ea9f5a5d02b109cbada695c484b0695c',
-       i686: '6af5f6358d7782795645ebc2c2ab132cd8c34d3619acf3f4ade4a5fc19063f64',
-     x86_64: '7efd8c88816c31d43bd7b8597c92058ff81d585b12c30fe22927f62ff2ec4d48'
+    aarch64: '881a0996ae47fc7f1402972814298d56a8f1d5e5d94b8c65f3d02a0a120d23d3',
+     armv7l: '881a0996ae47fc7f1402972814298d56a8f1d5e5d94b8c65f3d02a0a120d23d3',
+       i686: '51dc3a8c3c3e003423abcf95e2fe1306ef47e75e2311ed96aa747833e02c1ea3',
+     x86_64: '576f1a0274289b8ec2c4204af563e71e238824c2d03a2ff521077c90be6f7cb8'
   })
 
   depends_on 'boost' => :build
