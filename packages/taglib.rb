4d6496c30a1c4d9ea518c538e1de5bd07d22397a
Satadru Pramanik <satadru@gmail.com>
2023-07-10 16:12:44 -0400

    update taglib

--- packages/taglib.rb
+++ packages/taglib.rb
@@ -3,35 +3,35 @@ require 'package'
 class Taglib < Package
   description 'TagLib is a library for reading and editing the meta-data of several popular audio formats.'
   homepage 'https://taglib.org'
-  version '1.13'
+  version '1.13.1'
   license 'LGPL-2.1 and MPL-1.1'
   compatibility 'all'
-  source_url 'https://taglib.org/releases/taglib-1.13.tar.gz'
-  source_sha256 '58f08b4db3dc31ed152c04896ee9172d22052bc7ef12888028c01d8b1d60ade0'
+  source_url 'https://taglib.org/releases/taglib-1.13.1.tar.gz'
+  source_sha256 'c8da2b10f1bfec2cd7dbfcd33f4a2338db0765d851a50583d410bacf055cfd0b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13_armv7l/taglib-1.13-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13_armv7l/taglib-1.13-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13_i686/taglib-1.13-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13_x86_64/taglib-1.13-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13.1_armv7l/taglib-1.13.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13.1_armv7l/taglib-1.13.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13.1_i686/taglib-1.13.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/taglib/1.13.1_x86_64/taglib-1.13.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '551c4a220b3986ecaaedf9465aa24b210726ed716574af83d903c46764fa756b',
-     armv7l: '551c4a220b3986ecaaedf9465aa24b210726ed716574af83d903c46764fa756b',
-       i686: 'e7b54f8f92fb9cd7eb797a4eb7717157a9734894f0e85aef808fcbf73ee4a38a',
-     x86_64: 'b91534c1c2edac5560ac303cb4cf8c0c12257dad4c7a44a8e54fbbfd33bffcae'
+    aarch64: 'dcfa97bc61b0aa4878a96cfa82177614bc9bc1095cadf265f9065ba43a3e1f64',
+     armv7l: 'dcfa97bc61b0aa4878a96cfa82177614bc9bc1095cadf265f9065ba43a3e1f64',
+       i686: '474adc96ebfec82a8a458f871520c1efbc6c9e6b6ccb2285ea59fe76cd1bdacd',
+     x86_64: '07297b6925fd56cf8623cc4af997d5dbe548628d1330fd94494eed422d4e4a65'
   })
 
   depends_on 'boost' => :build
   depends_on 'cppunit' => :build
 
   def self.build
-    system "cmake -B builddir #{CREW_CMAKE_OPTIONS} \
+    system "mold -run cmake -B builddir #{CREW_CMAKE_OPTIONS} \
             -G Ninja"
-    system 'samu -C builddir'
+    system "#{CREW_NINJA} -C builddir || ( sed -i 's/-fno-lto//g' builddir/taglib/build.ninja && #{CREW_NINJA} -C builddir)"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
