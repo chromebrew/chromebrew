d4c02f9ac33e8db7f99f0600479bdb8076770265
Satadru Pramanik <satadru@gmail.com>
2023-07-10 19:48:49 -0400

    update tcpflow, ledger

--- packages/tcpflow.rb
+++ packages/tcpflow.rb
@@ -3,29 +3,29 @@ require 'package'
 class Tcpflow < Package
   description 'TCP/IP packet demultiplexer'
   homepage 'https://github.com/simsong/tcpflow'
-  version '1.6.1'
+  version '1.6.1-b1479db'
   license 'GPL-3'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/simsong/tcpflow.git'
-  git_hashtag "tcpflow-#{version}"
+  git_hashtag 'b1479db14b1604e00d35c2d39566c54e8b1785d0'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tcpflow/1.6.1_armv7l/tcpflow-1.6.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tcpflow/1.6.1_armv7l/tcpflow-1.6.1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tcpflow/1.6.1_i686/tcpflow-1.6.1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tcpflow/1.6.1_x86_64/tcpflow-1.6.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tcpflow/1.6.1-b1479db_armv7l/tcpflow-1.6.1-b1479db-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tcpflow/1.6.1-b1479db_armv7l/tcpflow-1.6.1-b1479db-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tcpflow/1.6.1-b1479db_x86_64/tcpflow-1.6.1-b1479db-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '46718d8c909cae4a175ec7fc95e04e560407d615c66134ff1f903427712a03c9',
-     armv7l: '46718d8c909cae4a175ec7fc95e04e560407d615c66134ff1f903427712a03c9',
-       i686: '747f148abc6a655671ab6e24503572e03c5ed0d53e702cb2b2abbeb4b7170aa2',
-     x86_64: '4ad32e11746afa35888f1878c503694349b7ff9979b485922b26037f88f3bd65'
+    aarch64: '13f7de4e37ae24a3969a87b8748e01fc6a5becd4f5c28c74fafcf9eaf3be4afb',
+     armv7l: '13f7de4e37ae24a3969a87b8748e01fc6a5becd4f5c28c74fafcf9eaf3be4afb',
+     x86_64: '21d9e46128c7c1d7767cf79c53448c7e989e308fcefa579f97f04debb35069de'
   })
 
   depends_on 'boost' => :build
   depends_on 'bz2' # R
   depends_on 'cairo' => :build
+  depends_on 'cairo' # R
   depends_on 'expat' # R
+  depends_on 'fontconfig' # R
   depends_on 'freetype' # R
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
