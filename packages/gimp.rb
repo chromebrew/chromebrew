8cc6f3dd7ae9fe61bc152ac15c076c5706df4cea
Satadru Pramanik <satadru@gmail.com>
2023-07-10 22:52:02 -0400

    update gimp

--- packages/gimp.rb
+++ packages/gimp.rb
@@ -3,23 +3,21 @@ require 'package'
 class Gimp < Package
   description 'GIMP is a cross-platform image editor available for GNU/Linux, OS X, Windows and more operating systems.'
   homepage 'https://www.gimp.org/'
-  version '2.99.14'
+  version '2.99.16'
   license 'GPL-3 and LGPL-3'
-  compatibility 'all'
-  source_url 'https://download.gimp.org/gimp/v2.99/gimp-2.99.14.tar.xz'
-  source_sha256 '313a205475d1ff03c5c4d9602f09f5c975ba6c1c79d8843e2396f9fe2abdf7a8'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://download.gimp.org/gimp/v2.99/gimp-2.99.16.tar.xz'
+  source_sha256 '6b4496edee447339f923276755247eadb64ec40d8aec241d06b62d1a6eb6508d'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gimp/2.99.14_armv7l/gimp-2.99.14-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gimp/2.99.14_armv7l/gimp-2.99.14-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gimp/2.99.14_i686/gimp-2.99.14-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gimp/2.99.14_x86_64/gimp-2.99.14-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gimp/2.99.16_armv7l/gimp-2.99.16-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gimp/2.99.16_armv7l/gimp-2.99.16-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gimp/2.99.16_x86_64/gimp-2.99.16-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a1e0ae2ca1ed77cb11d746745fa4103231c7fd61959d745f2c803dcc5de29e8d',
-     armv7l: 'a1e0ae2ca1ed77cb11d746745fa4103231c7fd61959d745f2c803dcc5de29e8d',
-       i686: '0508068761b35033e80f25866f54d835a63f967da7eec4547b32f0f213d272d0',
-     x86_64: '40972b5410efa1d614286ea762d2dfcf969e346f893b172000d4e06784dbca22'
+    aarch64: 'cc06da044ebc5be47a1b757a4dfdf85c8c4433cd75b1333d89468664bc3e55bc',
+     armv7l: 'cc06da044ebc5be47a1b757a4dfdf85c8c4433cd75b1333d89468664bc3e55bc',
+     x86_64: '2548cb28a3f325f0941fb6229acded8bcc010363cfc8331be6ab3f17c4134496'
   })
 
   depends_on 'aalib' # R
@@ -32,8 +30,8 @@ class Gimp < Package
   depends_on 'ffmpeg' => :build
   depends_on 'fontconfig' # R
   depends_on 'freetype' # R
-  depends_on 'gcc_lib' # R
   depends_on 'gcc_dev' # R
+  depends_on 'gcc_lib' # R
   depends_on 'gdk_pixbuf' # R
   depends_on 'gegl' # R
   depends_on 'gexiv2' # R
@@ -42,6 +40,7 @@ class Gimp < Package
   depends_on 'glibc' # R
   depends_on 'glib_networking'
   depends_on 'glib' # R
+  depends_on 'gobject_introspection' # R
   depends_on 'gtk3' # R
   depends_on 'harfbuzz' # R
   depends_on 'ilmbase' # R
@@ -52,7 +51,7 @@ class Gimp < Package
   depends_on 'libavif' => :build
   depends_on 'libexif' => :build
   depends_on 'libgudev' # R
-  depends_on 'libheif' => :build
+  depends_on 'libheif' # R
   depends_on 'libice' => :build
   depends_on 'libjpeg' # R
   depends_on 'libjxl' # R
@@ -80,10 +79,11 @@ class Gimp < Package
   depends_on 'pango' # R
   depends_on 'poppler_data'
   depends_on 'poppler' # R
-  depends_on 'pygobject' # L
   depends_on 'py3_pycairo' # L
+  depends_on 'pygobject' # L
   depends_on 'pygtk' => :build
   depends_on 'shared_mime_info' => :build
+  depends_on 'vala' => :build
   depends_on 'xdg_base' => :build
   depends_on 'xzutils' # R
   depends_on 'zlibpkg' # R
@@ -91,15 +91,15 @@ class Gimp < Package
   gnome
 
   def self.build
-    system "meson \
+    system "meson setup \
       #{CREW_MESON_OPTIONS} \
       -Dbug-report-url=https://github.com/chromebrew/chromebrew/issues \
       builddir"
     system 'meson configure builddir'
-    system 'ninja -C builddir'
+    system "#{CREW_NINJA} -C builddir"
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
   end
 end
