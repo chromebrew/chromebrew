138eaa3bed6e1ebf5a193763c22bc0b08e9f3b63
Satadru Pramanik <satadru@gmail.com>
2023-07-10 21:02:47 -0400

    update gdb, rebuild libtorrent

--- packages/gdb.rb
+++ packages/gdb.rb
@@ -6,25 +6,26 @@ require 'package'
 class Gdb < Package
   description 'The GNU Debugger'
   homepage 'https://www.gnu.org/software/gdb/'
-  version '12.1-a4418a9-py3.11'
+  version '13.2-py3.11'
   license 'GPL3'
   compatibility 'all'
-  source_url 'https://github.com/bminor/binutils-gdb.git'
-  git_hashtag 'a4418a9c6f99fd31c51698b1f6a6f8dbc1b81b6f'
+  source_url 'https://ftpmirror.gnu.org/gnu/gdb/gdb-13.2.tar.xz'
+  source_sha256 'fd5bebb7be1833abdb6e023c2f498a354498281df9d05523d8915babeb893f0a'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/12.1-a4418a9-py3.11_armv7l/gdb-12.1-a4418a9-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/12.1-a4418a9-py3.11_armv7l/gdb-12.1-a4418a9-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/12.1-a4418a9-py3.11_i686/gdb-12.1-a4418a9-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/12.1-a4418a9-py3.11_x86_64/gdb-12.1-a4418a9-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/13.2-py3.11_armv7l/gdb-13.2-py3.11-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/13.2-py3.11_armv7l/gdb-13.2-py3.11-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/13.2-py3.11_i686/gdb-13.2-py3.11-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gdb/13.2-py3.11_x86_64/gdb-13.2-py3.11-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'cdacd167edef7d56254298d1169551996b2910d1dcc97111fbb0740307f60154',
-     armv7l: 'cdacd167edef7d56254298d1169551996b2910d1dcc97111fbb0740307f60154',
-       i686: '87a1644e64b00b86aeed1dd00e6081314129a4658f9609fc2b9aaff06d388809',
-     x86_64: 'a88eabe0cceb8bd0d87d6c54dfb70922e7d810af4629e49cb91b7bf99f7f70d6'
+    aarch64: 'dcd1c83d33b476f72819f88309957e4885222257286af38d6278bf435ac3b112',
+     armv7l: 'dcd1c83d33b476f72819f88309957e4885222257286af38d6278bf435ac3b112',
+       i686: 'f443297dca5d7e247d6e423b3cfc8bab778ddca02de58fec7ddc36aeed762d45',
+     x86_64: '5e55256a5cea3c1f4954900e3b59c99f150d22ebd67b3c499a75a896017dedaf'
   })
 
+  depends_on 'binutils' # R
   depends_on 'boost' # R
   depends_on 'elfutils' # R
   depends_on 'expat' # R
@@ -39,44 +40,7 @@ class Gdb < Package
   depends_on 'xxhash' # R
   depends_on 'xzutils' # R
   depends_on 'zlibpkg' # R
-
-  def self.patch
-    @readline8patch = <<~READLINE8_PATCH_EOF
-      commit 1add37b567a7dee39d99f37b37802034c3fce9c4
-      Author: Andreas Schwab <schwab@linux-m68k.org>
-      Date:   Sun Mar 20 14:01:54 2022 +0100
-
-          Add support for readline 8.2
-
-          In readline 8.2 the type of rl_completer_word_break_characters changed to
-          include const.
-
-      diff --git a/gdb/completer.c b/gdb/completer.c
-      index d3900ae2014..a51c16ac7f8 100644
-      --- a/gdb/completer.c
-      +++ b/gdb/completer.c
-      @@ -36,7 +36,7 @@
-          calling a hook instead so we eliminate the CLI dependency.  */
-       #include "gdbcmd.h"
-
-      -/* Needed for rl_completer_word_break_characters() and for
-      +/* Needed for rl_completer_word_break_characters and for
-          rl_filename_completion_function.  */
-       #include "readline/readline.h"
-
-      @@ -2011,7 +2011,7 @@ gdb_completion_word_break_characters_throw ()
-             rl_basic_quote_characters = NULL;
-           }
-
-      -  return rl_completer_word_break_characters;
-      +  return (char *) rl_completer_word_break_characters;
-       }
-
-       char *
-    READLINE8_PATCH_EOF
-    File.write('upstream-fix-build-readline8.patch', @readline8patch)
-    system 'patch -Np1 -i upstream-fix-build-readline8.patch'
-  end
+  depends_on 'zstd' # R
 
   def self.build
     FileUtils.mkdir_p 'build'
