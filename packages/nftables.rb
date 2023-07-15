fb7ba93e1da565ca0ee7d03ec2db5e11fb92aa6d
Ed Reel <edreel@gmail.com>
2023-07-15 10:13:04 -0500

    Nftables 1.0.7 => 1.0.8 (#8477)
    Libnftnl 1.2.5 => 1.2.6

--- packages/nftables.rb
+++ packages/nftables.rb
@@ -6,21 +6,21 @@ require 'package'
 class Nftables < Package
   description 'Netfilter tables userspace tools'
   homepage 'https://netfilter.org/projects/nftables/'
-  version '1.0.7'
+  version '1.0.8'
   license 'GPL2'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://netfilter.org/projects/nftables/files/nftables-1.0.7.tar.xz'
-  source_sha256 'c12ac941fff9adaedf17367d5ce213789b98a0d314277bc22b3d71e10891f412'
+  source_url 'https://netfilter.org/projects/nftables/files/nftables-1.0.8.tar.xz'
+  source_sha256 '9373740de41a82dbc98818e0a46a073faeb8a8d0689fa4fa1a74399c32bf3d50'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.7_armv7l/nftables-1.0.7-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.7_armv7l/nftables-1.0.7-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.7_x86_64/nftables-1.0.7-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.8_armv7l/nftables-1.0.8-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.8_armv7l/nftables-1.0.8-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.8_x86_64/nftables-1.0.8-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'f3f13ae9973a69553bfc0105a4437f5ff049331f1323a1839e5e7a522f6d00f8',
-     armv7l: 'f3f13ae9973a69553bfc0105a4437f5ff049331f1323a1839e5e7a522f6d00f8',
-     x86_64: 'ea7b3b864f346788e228516ee5a170ec37ad5f798e977df045b46673995704cb'
+    aarch64: 'b1151ae07c75b41f9263acd4c0197c7ec0dd6bf3666d4f214aef90cdd1af23e1',
+     armv7l: 'b1151ae07c75b41f9263acd4c0197c7ec0dd6bf3666d4f214aef90cdd1af23e1',
+     x86_64: '87087b6ac01cd72c6293701453b8bae99795bbcf68cfd874de91936363048e40'
   })
 
   depends_on 'asciidoc' => :build
