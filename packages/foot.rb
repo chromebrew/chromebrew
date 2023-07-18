bfb2cd9553c2e7a64c08001f6c1b554a31ecdb04
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-07-17 20:22:55 -0400

    foot => 0.15.0 (#8482)

--- packages/foot.rb
+++ packages/foot.rb
@@ -1,25 +1,25 @@
 # Adapted from Arch Linux foot PKGBUILD at:
 # https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=foot
 
-require 'package'
+require 'buildsystems/meson'
 
-class Foot < Package
+class Foot < Meson
   description 'Wayland terminal emulator - fast, lightweight and minimalistic'
   homepage 'https://codeberg.org/dnkl/foot'
-  version '1.13.1'
-  compatibility 'aarch64, armv7l, x86_64'
+  version '1.15.0'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://codeberg.org/dnkl/foot.git'
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/foot/1.13.1_armv7l/foot-1.13.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/foot/1.13.1_armv7l/foot-1.13.1-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/foot/1.13.1_x86_64/foot-1.13.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/foot/1.15.0_armv7l/foot-1.15.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/foot/1.15.0_armv7l/foot-1.15.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/foot/1.15.0_x86_64/foot-1.15.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'a1c2d52499b3c842c1a9c01dfb14024cf435f37d88d53f4bff1a2741cee89e02',
-     armv7l: 'a1c2d52499b3c842c1a9c01dfb14024cf435f37d88d53f4bff1a2741cee89e02',
-     x86_64: '7586ac28caae7ffd3cb38e700f9455582d7a2a5e47e8e6bd7c78eb2c6a32c0de'
+    aarch64: '600c9b3fefa7b6416f87c39a95afb3bf06075760a2e1ad6e9e84dfe1a46bc609',
+     armv7l: '600c9b3fefa7b6416f87c39a95afb3bf06075760a2e1ad6e9e84dfe1a46bc609',
+     x86_64: '4baccb81bb940da9f73b2610dd98777524e1085422709fa7ac3ae193cf65291c'
   })
 
   depends_on 'libxkbcommon'
@@ -35,21 +35,9 @@ class Foot < Package
   depends_on 'glibc' # R
   depends_on 'harfbuzz' # R
   depends_on 'gcc_lib' # R
+  depends_on 'glibc_lib' # R
 
   def self.preflight
     abort 'Foot requires glibc > 2.28.' if Gem::Version.new(LIBC_VERSION.to_s) < Gem::Version.new('2.28')
-  end
-
-  def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} \
-      -Dterminfo=disabled \
-      -Dthemes=false \
-      builddir"
-    system 'meson configure builddir'
-    system 'ninja -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
   end
 end
