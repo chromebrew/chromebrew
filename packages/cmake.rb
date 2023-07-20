a771cea2658ede6ad4737825a16ee855d094fa94
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-07-20 11:48:21 -0400

    CMake => 3.27.0 (#8488)
    * add cppdap
    
    * rebuild cppdap
    
    * cmake => 3.27.0

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,40 +3,41 @@ require 'package'
 class Cmake < Package
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  version '3.26.3'
+  version '3.27.0'
   license 'CMake'
   compatibility 'all'
   source_url 'https://github.com/Kitware/CMake.git'
   git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.3_armv7l/cmake-3.26.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.3_armv7l/cmake-3.26.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.3_i686/cmake-3.26.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.26.3_x86_64/cmake-3.26.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.0_armv7l/cmake-3.27.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.0_armv7l/cmake-3.27.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.0_i686/cmake-3.27.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.0_x86_64/cmake-3.27.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'd34f102d0a7a780cbb62c36fad3be03e36cb645b4d9a3dbcf08f887bfc2b0f53',
-     armv7l: 'd34f102d0a7a780cbb62c36fad3be03e36cb645b4d9a3dbcf08f887bfc2b0f53',
-       i686: 'dbdb8eb039fce12920255b85229f979cfb33b5d848b3810afe9ee97a8b8ca7dc',
-     x86_64: '7606cedd3efc463333b9a9360d98570bd65acbf32b9bc0f65feccf4e871eac8d'
+    aarch64: '0e0b6f694a674e714acd4660f4509a708b4147de4801b9649571312f713428c3',
+     armv7l: '0e0b6f694a674e714acd4660f4509a708b4147de4801b9649571312f713428c3',
+       i686: '314e49283d7af18689b353fd3fba87403ede99452b105dbedc33939ba534f6f8',
+     x86_64: '91f171e6d2b834d693d127d761e53099f1417dd1bc56f4ef9055c00350757494'
   })
 
-  depends_on 'bz2'
-  depends_on 'curl'
-  depends_on 'expat'
+  depends_on 'bz2' => :build
+  depends_on 'cppdap' # R
+  depends_on 'curl' # R
+  depends_on 'expat' # R
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
-  depends_on 'jsoncpp'
-  depends_on 'libarchive'
-  depends_on 'libnghttp2'
-  depends_on 'librhash'
-  depends_on 'libuv'
+  depends_on 'jsoncpp' # R
+  depends_on 'libarchive' # R
+  depends_on 'libnghttp2' => :build
+  depends_on 'librhash' # R
+  depends_on 'libuv' # R
   depends_on 'llvm16_lib' => :build
   depends_on 'ncurses' # R
-  depends_on 'xzutils'
-  depends_on 'zlibpkg'
-  depends_on 'zstd'
+  depends_on 'xzutils' => :build
+  depends_on 'zlibpkg' # R
+  depends_on 'zstd' => :buils
 
   def self.build
     system "mold -run cmake -B builddir \
@@ -50,7 +51,8 @@ class Cmake < Package
   # Failed tests:
   # BundleUtilities (armv7l,x86_64)
   # BootstrapTest (armv7l,i686,x86_64)
-  # CustomCommand (armv7l)
+  # CMakeLib.testDebuggerNamedPipe-Project (armv7l,i686,x86_64)
+  # CMakeLib.testDebuggerNamedPipe-Script (armv7l,i686,x86_64)
   # RunCMake.CMakeRelease (armv7l,i686,x86_64)
   def self.check
     system "#{CREW_NINJA} -C builddir test || true"
