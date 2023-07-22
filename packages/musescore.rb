a982c481d9bc6db71599ca2c8d0a3bdfbc65e88c
Franceschinie <94822523+Franceschinie@users.noreply.github.com>
2023-07-22 20:07:50 +0200

    Update musescore.rb

--- packages/musescore.rb
+++ packages/musescore.rb
@@ -6,19 +6,28 @@ class Musescore < Package
   version '4.0.2'
   license 'GPL-3'
   compatibility 'aarch64, x86_64'
-  source_url 'https://dl.flathub.org/repo/appstream/org.musescore.MuseScore.flatpakref'
-  source_sha256 'e38b3a4c4dbe9027fc6c8e5e922c66c593b097b05e5daa1e2cfb021716931174'
+  source_url 'https://musescore.org/en/download/musescore-x86_64.AppImage'
+  source_sha256 'f9a54ad7caf8d2173d67e972ffa68b750bf149f34f4b07d59cbc96dc2c663c79'
  
-  depends_on 'flatpak'
   depends_on 'crew-laucher'
-
+  depends_on 'p7zip'
+  depends_on 'sommelier'
+  depends_on 'qtbase'
+ 
   def self.preflight
   free_space = `echo $(($(stat -f --format="%a*%S" #{CREW_PREFIX})))`.chomp.to_i
   abort 'Not enough free disk space.  You need at least 317 MB to install.'.lightred if free_space < 317000000
   def self.install
-  system("flatpak install ./org.musescore.MuseScore.flatpakref -y")
+  system("7z x musescore-x86_64.AppImage")
+  FileUtils.mv 'bin' ,"#{CREW_DEST_PREFIX}"
+  FileUtils.mv 'share' ,"#{CREW_DEST_PREFIX}"
+  FileUtils.mv 'include' ,"#{CREW_DEST_PREFIX}"
+  FileUtils.mv 'lib' ,"#{CREW_DEST_PREFIX}"
   end
-
   def self.postinstall
+   FileUtils.mv 'plugins/*/*.so' ,"#{CREW_DEST_PREFIX}/lib64"
+   FileUtils.mv
+   system("crew-launcher start")
+   system("crew-launcher add #{CREW_DEST_PREFIX}/share/applications/org.musescore.MuseScore4portable.desktop")
   end
 end
