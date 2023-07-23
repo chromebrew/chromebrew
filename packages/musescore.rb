a0a1949d75446e31117aefed1d1ba39f5f0e0582
Franceschinie <94822523+Franceschinie@users.noreply.github.com>
2023-07-23 20:47:11 +0200

    Update musescore.rb

--- packages/musescore.rb
+++ packages/musescore.rb
@@ -6,7 +6,7 @@ class Musescore < Package
   version '4.1.0'
   license 'GPL-3'
   compatibility 'aarch64, x86_64'
-  source_url 'https://github.com/musescore/MuseScore/releases/download/v4.1.0/MuseScore-4.1.0.231921359-x86_64.AppImage'
+  source_url 'https://raw.githubusercontent.com/Franceschinie/musescore/main/install.sh'
   source_sha256 'f9a54ad7caf8d2173d67e972ffa68b750bf149f34f4b07d59cbc96dc2c663c79'
  
   depends_on 'p7zip'
@@ -20,13 +20,9 @@ class Musescore < Package
    abort 'Not enough free disk space.  You need at least 317 MB to install.'.lightred if free_space < 317000000
   end
   def self.install
-  FileUtils.mv 'squashfs-root/bin' ,"#{CREW_DEST_PREFIX}"
-  FileUtils.mv 'squashfs-root/share' ,"#{CREW_DEST_PREFIX}"
-  FileUtils.mv 'squashfs-root/include' ,"#{CREW_DEST_PREFIX}"
-  FileUtils.mv 'squashfs-root/lib' ,"#{CREW_DEST_PREFIX}"
+system("sudo bash ./install.sh")
   end
   def self.postinstall
-   FileUtils.mv 'squashfs-root/plugins/*/*.so' ,"#{CREW_DEST_PREFIX}/lib64"
   end 
  def self.remove
   end
