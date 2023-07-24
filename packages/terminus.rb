530f0ebf14ef984cf238e36f130a7b39912375bd
Ed Reel <edreel@gmail.com>
2023-07-24 16:34:57 -0500

    Terminus 3.1.4 => 3.2.1 (#8509)

--- packages/terminus.rb
+++ packages/terminus.rb
@@ -3,16 +3,15 @@ require 'package'
 class Terminus < Package
   description 'The Pantheon CLI — a standalone utility for performing operations on the Pantheon Platform'
   homepage 'https://github.com/pantheon-systems/terminus'
-  version '3.1.4'
+  version '3.2.1'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/pantheon-systems/terminus/releases/download/3.1.4/terminus.phar'
-  source_sha256 'a3cd9881eaa563fe4cf1c481f0a94da4f575f486faba9fbffeb063fbaab1dc7b'
+  source_url 'https://github.com/pantheon-systems/terminus/releases/download/3.2.1/terminus.phar'
+  source_sha256 '9a955d196a1bd63717cc2d6a64170c5d80baa2565fd062a832f2de36e2a82b74'
 
-  depends_on 'php74' unless File.exist? "#{CREW_PREFIX}/bin/php"
+  depends_on 'php81' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'terminus.phar', "#{CREW_DEST_PREFIX}/bin/terminus", mode: 0o755
   end
 end
