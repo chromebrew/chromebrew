df989a1bad52d125465ce6d1957cc5c33658fdde
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2023-07-25 04:54:11 +1000

    Fix no_lto detection in meson and cmake buildsystems (#8486)
    * Fix no_lto detection in meson and cmake buildsystems
    
    * bump version
    
    ---------
    
    Co-authored-by: Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>

--- lib/buildsystems/meson.rb
+++ lib/buildsystems/meson.rb
@@ -11,9 +11,9 @@ class Meson < Package
 
   def self.build
     puts "Additional meson_options being used: #{@meson_options.nil? || @meson_options.empty? ? '<no meson_options>' : @meson_options}".orange
-    @crew_meson_options = no_lto ? CREW_MESON_FNO_LTO_OPTIONS : CREW_MESON_OPTIONS
-    @mold_linker_prefix_cmd = CREW_LINKER == 'mold' ? 'mold -run ' : ''
-    system "#{@mold_linker_prefix_cmd}meson setup #{@crew_meson_options} #{@meson_options} builddir"
+    @crew_meson_options = @no_lto ? CREW_MESON_FNO_LTO_OPTIONS : CREW_MESON_OPTIONS
+    @mold_linker_prefix_cmd = CREW_LINKER == 'mold' ? 'mold -run' : ''
+    system "#{@mold_linker_prefix_cmd} meson setup #{@crew_meson_options} #{@meson_options} builddir"
     system 'meson configure builddir'
     system "#{CREW_NINJA} -C builddir"
   end
