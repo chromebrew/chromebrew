3fd15a959dcb9213d2baf4908fa0501bf00189ed
Satadru Pramanik <satadru@gmail.com>
2023-07-25 19:07:52 -0400

    openssh => 9.3p2

--- packages/openssh.rb
+++ packages/openssh.rb
@@ -3,23 +3,23 @@ require 'package'
 class Openssh < Package
   description 'OpenSSH is the premier connectivity tool for remote login with the SSH protocol.'
   homepage 'https://www.openssh.com/'
-  version '9.3'
+  version '9.3p2'
   license 'BSD and GPL-2'
   compatibility 'all'
   source_url 'https://github.com/openssh/openssh-portable.git'
-  git_hashtag 'V_9_3_P1'
+  git_hashtag 'V_9_3_P2'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3_armv7l/openssh-9.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3_armv7l/openssh-9.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3_i686/openssh-9.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3_x86_64/openssh-9.3-chromeos-x86_64.tar.zst'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_i686/openssh-9.3p2-chromeos-i686.tar.zst',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_armv7l/openssh-9.3p2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_armv7l/openssh-9.3p2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_x86_64/openssh-9.3p2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '1a63e381cdc66ab8a90f5ab09e330c82049ae9c2d9f1214a60bd2f7674d59b30',
-     armv7l: '1a63e381cdc66ab8a90f5ab09e330c82049ae9c2d9f1214a60bd2f7674d59b30',
-       i686: 'b034291d0bdb5df894bf94c388c091084c74fbec744914377b3525cb533d3b4a',
-     x86_64: '13016d5e28942c0c225ba914a23ed4a604d5f55c426cf49f51dd24c5f7e18e92'
+       i686: '6ef7930647d75f8df8146c252f08ea3a588c9b5ec30459374f7b6ad69ab57b82',
+    aarch64: 'd270fd8923e239bf8122fd026d7469830893043c19afc538ce1b415c02aa3e12',
+     armv7l: 'd270fd8923e239bf8122fd026d7469830893043c19afc538ce1b415c02aa3e12',
+     x86_64: '14121876580f0c5dfcc5ca3a4d8db27cf86f734b22ea0edd9ca9c5daaab8b8c6'
   })
 
   depends_on 'gcc_lib' # R
