d2728648a29050d6a4b9faeb9d1d34773704947d
Ed Reel <edreel@gmail.com>
2023-08-03 05:59:06 -0500

    Xzutils 5.4.3 => 5.4.4 (#8532)

--- packages/xzutils.rb
+++ packages/xzutils.rb
@@ -1,45 +1,31 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Xzutils < Package
+class Xzutils < Autotools
   description 'XZ Utils is free general-purpose data compression software with a high compression ratio.'
   homepage 'http://tukaani.org/xz/'
-  version '5.4.3'
+  version '5.4.4'
   license 'public-domain, LGPL-2.1+ and GPL-2+'
   compatibility 'all'
-  source_url 'https://tukaani.org/xz/xz-5.4.3.tar.gz'
-  source_sha256 '1c382e0bc2e4e0af58398a903dd62fff7e510171d2de47a1ebe06d1528e9b7e9'
+  source_url 'https://tukaani.org/xz/xz-5.4.4.tar.gz'
+  source_sha256 'aae39544e254cfd27e942d35a048d592959bd7a79f9a624afb0498bb5613bdf8'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_armv7l/xzutils-5.4.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_armv7l/xzutils-5.4.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_i686/xzutils-5.4.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.3_x86_64/xzutils-5.4.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.4_armv7l/xzutils-5.4.4-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.4_armv7l/xzutils-5.4.4-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.4_i686/xzutils-5.4.4-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xzutils/5.4.4_x86_64/xzutils-5.4.4-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '423d4266d49d82aceb6341b5d2bce7d3416ff06fb1f026f4acab0584338913c9',
-     armv7l: '423d4266d49d82aceb6341b5d2bce7d3416ff06fb1f026f4acab0584338913c9',
-       i686: '82b65794ce833407142d7183d6ec497f69ba7399df5582b5335d30270c57585c',
-     x86_64: '60ffcc7daaa04574fb5624b0be54c5db39e52da3c786dc4ed2d4dc628211ab92'
+    aarch64: 'c47ede32e4586414aae59b89610925108b343c9f372ba774975c0f32d34b1cd7',
+     armv7l: 'c47ede32e4586414aae59b89610925108b343c9f372ba774975c0f32d34b1cd7',
+       i686: '35f444be4703e8fccf53fd194943ae3d50497bf4eda676461d942c6be1e06ca9',
+     x86_64: 'e43ed33f516fcba977a417c11f00b32fd481a580aedef32347e723e99191d114'
   })
 
   depends_on 'autoconf_archive' => :build
   depends_on 'glibc' # R
 
-  def self.build
-    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh --no-po4a'
-    system "#{CREW_ENV_OPTIONS} \
-      ./configure \
-      #{CREW_OPTIONS} \
-      --disable-docs \
-      --enable-shared"
-    system 'make'
-  end
+  run_tests
 
-  def self.check
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options << '--enable-shared'
 end
