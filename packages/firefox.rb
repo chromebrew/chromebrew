1a22cbd4988cee4ef3a9cdf26c427bfc8f0f3e62
Ed Reel <edreel@gmail.com>
2023-08-06 16:16:40 -0500

    Firefox 116.0 => 116.0.1

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,7 +3,7 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '116.0'
+  version '116.0.1'
   license 'MPL-2.0, GPL-2 and LGPL-2.1'
   compatibility 'x86_64'
 
@@ -11,7 +11,7 @@ class Firefox < Package
     x86_64: "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
   })
   source_sha256({
-    x86_64: 'c689747d1dfa2b562f665ead7a82a5b399eb8f75b21aab11762f1093d90fea6c'
+    x86_64: '2c85a25a0a201f41babd7a321a2c339f3fcf51e4cab2a94f98bc4b25e6672c6e'
   })
 
   depends_on 'at_spi2_core'
