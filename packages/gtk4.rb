1230aa1a24963a4f3a735b24033907b047561f79
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-08-06 23:43:07 -0400

    gtk4 => 4.12.0 (#8545)

--- packages/gtk4.rb
+++ packages/gtk4.rb
@@ -3,21 +3,21 @@ require 'package'
 class Gtk4 < Package
   description 'GTK+ is a multi-platform toolkit for creating graphical user interfaces.'
   homepage 'https://developer.gnome.org/gtk4/'
-  version '4.11.4'
+  version '4.12.0'
   license 'LGPL-2.1'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/gtk.git'
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk4/4.11.4_armv7l/gtk4-4.11.4-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk4/4.11.4_armv7l/gtk4-4.11.4-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk4/4.11.4_x86_64/gtk4-4.11.4-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk4/4.12.0_armv7l/gtk4-4.12.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk4/4.12.0_armv7l/gtk4-4.12.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gtk4/4.12.0_x86_64/gtk4-4.12.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '6916b897d6ee04dc402edc81d2128ed7e6ac8634b4c82bc31c935410b12a1d01',
-     armv7l: '6916b897d6ee04dc402edc81d2128ed7e6ac8634b4c82bc31c935410b12a1d01',
-     x86_64: '09ff3f57d25ffb0689df850da2afd838993f72cbed191f502bdd1fa6f6601f31'
+    aarch64: '8c1c783109f01b89ea70771b941aa4a94f15abe6543ea93a34dc17ad69d87471',
+     armv7l: '8c1c783109f01b89ea70771b941aa4a94f15abe6543ea93a34dc17ad69d87471',
+     x86_64: '1074c93ce9094703d1f953b4268eaaf5ccc56e0a71336239e7e8b38a9b927c82'
   })
 
   # L = Logical Dependency, R = Runtime Dependency
@@ -65,6 +65,7 @@ class Gtk4 < Package
   depends_on 'py3_gi_docgen' => :build
   depends_on 'py3_pygments' => :build
   depends_on 'sassc' => :build
+  depends_on 'shaderc' => :build
   depends_on 'shared_mime_info' # L
   depends_on 'sommelier' # L
   depends_on 'valgrind' => :build
