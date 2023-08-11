4919dbc75ab4b28de5c8c09109036cd844e63760
Ed Reel <edreel@gmail.com>
2023-08-10 20:19:29 -0500

    Openssh 9.3p2 => 9.4p1

--- packages/openssh.rb
+++ packages/openssh.rb
@@ -1,25 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Openssh < Package
+class Openssh < Autotools
   description 'OpenSSH is the premier connectivity tool for remote login with the SSH protocol.'
   homepage 'https://www.openssh.com/'
-  version '9.3p2'
+  version '9.4p1'
   license 'BSD and GPL-2'
   compatibility 'all'
   source_url 'https://github.com/openssh/openssh-portable.git'
-  git_hashtag 'V_9_3_P2'
+  git_hashtag 'V_9_4_P1'
 
   binary_url({
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_i686/openssh-9.3p2-chromeos-i686.tar.zst',
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_armv7l/openssh-9.3p2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_armv7l/openssh-9.3p2-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.3p2_x86_64/openssh-9.3p2-chromeos-x86_64.tar.zst'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_i686/openssh-9.4p1-chromeos-i686.tar.zst',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_armv7l/openssh-9.4p1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_armv7l/openssh-9.4p1-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_x86_64/openssh-9.4p1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-       i686: '6ef7930647d75f8df8146c252f08ea3a588c9b5ec30459374f7b6ad69ab57b82',
-    aarch64: 'd270fd8923e239bf8122fd026d7469830893043c19afc538ce1b415c02aa3e12',
-     armv7l: 'd270fd8923e239bf8122fd026d7469830893043c19afc538ce1b415c02aa3e12',
-     x86_64: '14121876580f0c5dfcc5ca3a4d8db27cf86f734b22ea0edd9ca9c5daaab8b8c6'
+       i686: '52afc47da42cb9781d834ac1a2ba5d80bcb59d477814a8e712507c0d4f2777dc',
+    aarch64: '5b2b1f607dc197dacf61f2c18ee94d7945b04bfffef2a5d0d6f13e9accf75e33',
+     armv7l: '5b2b1f607dc197dacf61f2c18ee94d7945b04bfffef2a5d0d6f13e9accf75e33',
+     x86_64: '559fd33ad76b3077a8bb7073406a80bbc8cbf89eafa1a77b6e1d70902d6f04fb'
   })
 
   depends_on 'gcc_lib' # R
@@ -28,15 +28,5 @@ class Openssh < Package
   depends_on 'openssl' # R
   depends_on 'zlibpkg' # R
 
-  def self.build
-    system 'autoreconf -fiv'
-    system 'autoheader'
-    # hardening breaks build with "retpolineplt: unknown -z option" error.
-    system "./configure #{CREW_OPTIONS} --without-hardening"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options '--without-hardening'
 end
