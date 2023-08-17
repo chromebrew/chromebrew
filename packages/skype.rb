607706fcee5923783b2c13d2cb36e92155dd21cb
chronos <noreply@gitlab.com>
2023-08-17 21:00:06 +0000

    Skype 8.101.0.212 => 8.102.0.211

--- packages/skype.rb
+++ packages/skype.rb
@@ -3,11 +3,11 @@ require 'package'
 class Skype < Package
   description 'Skype is a telecommunications application that specializes in providing video chat and voice calls between devices'
   homepage 'https://www.skype.com/'
-  version '8.101.0.212'
+  version '8.102.0.211'
   license 'Skype-TOS'
   compatibility 'x86_64'
-  source_url 'https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_8.101.0.212_amd64.deb'
-  source_sha256 'ed48a6bf36a39ba3490d54d55c3054a093bd4344d170cf62aa6ebd334cec8c7c'
+  source_url 'https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_8.102.0.211_amd64.deb'
+  source_sha256 '662cd02248d4cb2bfdaad6bbd1069f22f132780799d61edc62d3dea91599b5f3'
 
   no_compile_needed
   no_shrink
