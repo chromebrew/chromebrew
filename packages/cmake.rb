d5b6bd050e33f8f5e5a50ba7e0bc8da85d654b1c
Satadru Pramanik <satadru@gmail.com>
2023-08-18 16:10:57 -0400

    cmake => 3.27.3

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -5,18 +5,20 @@ class Cmake < Package
   homepage 'https://cmake.org/'
   version '3.27.3'
   license 'CMake'
-  compatibility 'x86_64 aarch64 armv7l'
+  compatibility 'all'
   source_url 'https://github.com/Kitware/CMake.git'
   git_hashtag "v#{version}"
 
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.3_armv7l/cmake-3.27.3-chromeos-armv7l.tar.zst',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.3_armv7l/cmake-3.27.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.3_i686/cmake-3.27.3-chromeos-i686.tar.zst',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cmake/3.27.3_x86_64/cmake-3.27.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
     aarch64: '28c962368f6d549b09c2b5176c06096d30120ebeac0ded7bc96157c35d838ecf',
      armv7l: '28c962368f6d549b09c2b5176c06096d30120ebeac0ded7bc96157c35d838ecf',
+       i686: '3cc6da57c1fe994b208f39052b167ba362e62e6eafeda5ef019c0b755c28fb99',
      x86_64: 'ee6fd4ae03757c08dc27b409d359d07c3c035339755f2f484ab22207a7b2fe9b'
   })
 
