61ff9b116d5126dd4ca25840f9c5e5554b94dd82
Ed Reel <edreel@gmail.com>
2023-08-20 02:25:07 -0500

    Minizip 1.2.13-1 => 1.3

--- packages/minizip.rb
+++ packages/minizip.rb
@@ -4,8 +4,7 @@ require_relative 'zlibpkg'
 class Minizip < Package
   description 'Minizip is a simple package to zip/unzip files, from zlib.'
   homepage Zlibpkg.homepage
-  @_ver = '1.2.13'
-  version "#{@_ver}-1"
+  version '1.3'
   # When upgrading minizip, be sure to upgrade zlibpkg in tandem.
   puts "#{self} version differs from Zlibpkg version #{Zlibpkg.version}".orange if version != Zlibpkg.version
   license Zlibpkg.license
@@ -14,16 +13,16 @@ class Minizip < Package
   source_sha256 Zlibpkg.source_sha256
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.2.13_armv7l/minizip-1.2.13-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.2.13_armv7l/minizip-1.2.13-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.2.13_i686/minizip-1.2.13-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.2.13_x86_64/minizip-1.2.13-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.3_armv7l/minizip-1.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.3_armv7l/minizip-1.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.3_i686/minizip-1.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/minizip/1.3_x86_64/minizip-1.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '7fd4d60ceddcc16766bf6cfa5249221da656d1a5420ed8c9d89b5105d4981e92',
-     armv7l: '7fd4d60ceddcc16766bf6cfa5249221da656d1a5420ed8c9d89b5105d4981e92',
-       i686: '691d10c6dfdd5eaed1cfcf711752618493b59d613947f405b9633ee0edf3fe37',
-     x86_64: '74eea7645907e49beb344a748a3eec59684063c68070cd759898f259966550b7'
+    aarch64: '9a789d1f8f2119e3f4e79ca6a7311a5df8c524c0690f7e15319691c6a60fc3ff',
+     armv7l: '9a789d1f8f2119e3f4e79ca6a7311a5df8c524c0690f7e15319691c6a60fc3ff',
+       i686: '194f8f1a9f7ac94b202926b3b5da3780d734f3586167750cb725f5d3a5f194f0',
+     x86_64: '19b5affc8be85b051f70039b4007088a23d00cea7bead6c4512b89134a493177'
   })
 
   depends_on 'zlibpkg' # R
