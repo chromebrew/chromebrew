0199c734564ef46eb1655acaca470551af7da702
Ed Reel <edreel@gmail.com>
2023-08-20 15:04:46 -0500

    Gzip 1.12 => 1.13 (#8577)

--- packages/gzip.rb
+++ packages/gzip.rb
@@ -1,70 +1,26 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Gzip < Package
+class Gzip < Autotools
   description 'GNU Gzip is a popular data compression program originally written by Jean-loup Gailly for the GNU project.'
   homepage 'https://www.gnu.org/software/gzip/'
-  version '1.12'
+  version '1.13'
   compatibility 'all'
   license 'GPL-3'
-  source_url 'https://ftpmirror.gnu.org/gzip/gzip-1.12.tar.xz'
-  source_sha256 'ce5e03e519f637e1f814011ace35c4f87b33c0bbabeec35baf5fbd3479e91956'
+  source_url 'https://ftpmirror.gnu.org/gzip/gzip-1.13.tar.xz'
+  source_sha256 '7454eb6935db17c6655576c2e1b0fabefd38b4d0936e0f87f48cd062ce91a057'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.12_armv7l/gzip-1.12-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.12_armv7l/gzip-1.12-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.12_x86_64/gzip-1.12-chromeos-x86_64.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.12_i686/gzip-1.12-chromeos-i686.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.13_armv7l/gzip-1.13-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.13_armv7l/gzip-1.13-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.13_x86_64/gzip-1.13-chromeos-x86_64.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gzip/1.13_i686/gzip-1.13-chromeos-i686.tar.zst'
   })
   binary_sha256({
-    aarch64: '2a699b3eba6ff714a780e8c2c32131c842f2960d2eb04de280268c54d111aed1',
-     armv7l: '2a699b3eba6ff714a780e8c2c32131c842f2960d2eb04de280268c54d111aed1',
-     x86_64: 'bd27a2c304c111cd4983416745ff83c210f15773f2dc5fd864369d7617ef904a',
-       i686: '9beef9bcb1bbd34f2c476e1412854a72181104b4d7db79ffec815818b93a8e81'
+    aarch64: '787cab0140058f4c0ce09a56b95df269540b39738fcb8483351d1586b00a840f',
+     armv7l: '787cab0140058f4c0ce09a56b95df269540b39738fcb8483351d1586b00a840f',
+     x86_64: 'c2e8d1af7bcb50c90a0aa5168d4af9b9cfb7286e904d0e0da807eb3b95d02e54',
+       i686: '87cdfa0d7f9719e17ca2e90578fab3326c0ab5f5fd55ee8ba9181db38bd37243'
   })
 
-  def self.patch
-    return unless ARCH == 'i686'
-
-    # Patch from https://git.savannah.gnu.org/cgit/gnulib.git/diff/?id=84863a1c4dc8cca8fb0f6f670f67779cdd2d543b
-    @gnulibpatch = <<~'PATCH_EOF'
-      diff --git a/lib/string.in.h b/lib/string.in.h
-      index b6840fa..33160b2 100644
-      --- a/lib/string.in.h
-      +++ b/lib/string.in.h
-      @@ -583,7 +583,7 @@ _GL_FUNCDECL_RPL (strndup, char *,
-                         _GL_ATTRIBUTE_MALLOC _GL_ATTRIBUTE_DEALLOC_FREE);
-       _GL_CXXALIAS_RPL (strndup, char *, (char const *__s, size_t __n));
-       # else
-      -#  if !@HAVE_DECL_STRNDUP@ || __GNUC__ >= 11
-      +#  if !@HAVE_DECL_STRNDUP@ || (__GNUC__ >= 11 && !defined strndup)
-       _GL_FUNCDECL_SYS (strndup, char *,
-                         (char const *__s, size_t __n)
-                         _GL_ARG_NONNULL ((1))
-      @@ -593,7 +593,7 @@ _GL_CXXALIAS_SYS (strndup, char *, (char const *__s, size_t __n));
-       # endif
-       _GL_CXXALIASWARN (strndup);
-       #else
-      -# if __GNUC__ >= 11
-      +# if __GNUC__ >= 11 && !defined strndup
-       /* For -Wmismatched-dealloc: Associate strndup with free or rpl_free.  */
-       _GL_FUNCDECL_SYS (strndup, char *,
-                         (char const *__s, size_t __n)
-    PATCH_EOF
-    File.write('gnulib.patch', @gnulibpatch)
-    system 'patch -p 1 -i gnulib.patch'
-  end
-
-  def self.build
-    system "./configure #{CREW_OPTIONS} \
-            --enable-threads=posix"
-    system "mold -run make -j #{CREW_NPROC}"
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  run_tests
 end
