160242e5c7049bd8c97749ce4008f0c9214c4d53
chronos <noreply@gitlab.com>
2023-08-23 16:24:52 +0000

    Chrome 116.0.5845.96-1 => 116.0.5845.110-1

--- packages/chrome.rb
+++ packages/chrome.rb
@@ -3,20 +3,20 @@ require 'package'
 class Chrome < Package
   description 'Google Chrome is a fast, easy to use, and secure web browser.'
   homepage 'https://www.google.com/chrome'
-  version '116.0.5845.96-1'
+  version '116.0.5845.110-1'
   license 'google-chrome'
   compatibility 'x86_64'
-  source_url 'https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_116.0.5845.96-1_amd64.deb'
-  source_sha256 'b429124b27f9b5ff3a63229823af0d4200020698eb58b75027897ba5b5e327eb'
+  source_url 'https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_116.0.5845.110-1_amd64.deb'
+  source_sha256 'd31e7ddda507e465a09752b2f82581f707dffcde319af2948f3c40f57df51822'
 
   depends_on 'nspr'
   depends_on 'cairo'
   depends_on 'gtk3'
   depends_on 'expat'
   depends_on 'cras'
-  depends_on 'sommelier'
 
   no_compile_needed
+  no_shrink
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
@@ -26,5 +26,9 @@ class Chrome < Package
 
     FileUtils.ln_s '../share/chrome/google-chrome', "#{CREW_DEST_PREFIX}/bin/google-chrome-stable"
     FileUtils.ln_s '../share/chrome/google-chrome', "#{CREW_DEST_PREFIX}/bin/google-chrome"
+  end
+
+  def self.postinstall
+    puts "\nType 'google-chrome' to get started.\n".lightblue
   end
 end
