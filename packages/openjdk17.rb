68a7e6a0d1e7028f4e9c5ca9662d47c56b9e895d
Ed Reel <edreel@gmail.com>
2023-09-02 07:17:34 -0500

    Openjdk17 17.0.7 => 17.0.8 (#8610)
    Co-authored-by: chronos <noreply@gitlab.com>

--- packages/openjdk17.rb
+++ packages/openjdk17.rb
@@ -3,23 +3,24 @@ require 'package'
 class Openjdk17 < Package
   description 'The JDK is a development environment for building applications, applets, and components using the Java programming language.'
   homepage 'https://openjdk.org/'
-  version '17.0.7'
+  version '17.0.8'
   license 'GPL-2'
   compatibility 'all'
   source_url({
-    aarch64: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.42.19-ca-jdk17.0.7-linux_aarch32hf.tar.gz',
-     armv7l: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.42.19-ca-jdk17.0.7-linux_aarch32hf.tar.gz',
-       i686: 'https://cdn.azul.com/zulu/bin/zulu17.42.19-ca-jdk17.0.7-linux_i686.tar.gz',
-     x86_64: 'https://cdn.azul.com/zulu/bin/zulu17.42.19-ca-jdk17.0.7-linux_x64.tar.gz'
+    aarch64: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.44.15-ca-jdk17.0.8-linux_aarch32hf.tar.gz',
+     armv7l: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.44.15-ca-jdk17.0.8-linux_aarch32hf.tar.gz',
+       i686: 'https://cdn.azul.com/zulu/bin/zulu17.44.15-ca-jdk17.0.8-linux_i686.tar.gz',
+     x86_64: 'https://cdn.azul.com/zulu/bin/zulu17.44.15-ca-jdk17.0.8-linux_x64.tar.gz'
   })
   source_sha256({
-    aarch64: '25f48e8d4a339f7346eb7884fa0c040911c86113b361a0c6787d28627cae09c2',
-     armv7l: '25f48e8d4a339f7346eb7884fa0c040911c86113b361a0c6787d28627cae09c2',
-       i686: '53a66b711d828deae801870143b00be2cf4563ce283d393b08b7b96a846dabd8',
-     x86_64: '28861f8292eab43290109c33e1ba7ff3776c44d043e7c8462d6c9702bf9fffe0'
+    aarch64: '4feb1f6cba26c66160e03284b876071ee9d27954df2e2be51def935afc30d8d9',
+     armv7l: '4feb1f6cba26c66160e03284b876071ee9d27954df2e2be51def935afc30d8d9',
+       i686: 'bc84c25f53f6961c5649f1fd363e944d48113ead07020ba90a2b3d53cea1b5eb',
+     x86_64: '423d1f40e0cbe873f357cebb95b19a59503948ee3c91639ecbc8e64df6a6f5fc'
   })
 
   no_compile_needed
+  no_shrink
 
   def self.preflight
     jdkver = `java -version 2>&1`[/version "(.*?)"/, 1].to_s
