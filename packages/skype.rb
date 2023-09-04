4fa8230a31720c4e9146cc76e31dd7233d31a5a2
Ed Reel <edreel@gmail.com>
2023-09-04 12:24:30 -0500

    Skype 8.102.0.211 => 8.103.0.208 (#8612)
    Co-authored-by: chronos <noreply@gitlab.com>

--- packages/skype.rb
+++ packages/skype.rb
@@ -3,11 +3,11 @@ require 'package'
 class Skype < Package
   description 'Skype is a telecommunications application that specializes in providing video chat and voice calls between devices'
   homepage 'https://www.skype.com/'
-  version '8.102.0.211'
+  version '8.103.0.208'
   license 'Skype-TOS'
   compatibility 'x86_64'
-  source_url 'https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_8.102.0.211_amd64.deb'
-  source_sha256 '662cd02248d4cb2bfdaad6bbd1069f22f132780799d61edc62d3dea91599b5f3'
+  source_url 'https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_8.103.0.208_amd64.deb'
+  source_sha256 'c728163ba42c6f4df711ed0a24a81816bfd80dc89bf4641479358bbc7d56c51f'
 
   no_compile_needed
   no_shrink
