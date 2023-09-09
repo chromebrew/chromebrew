04ccc39db7563d6e3b96e8e70ad5a28c004cc32e
Ed Reel <edreel@gmail.com>
2023-09-09 18:01:57 -0500

    Bat 0.22.1 => 0.23.0

--- packages/bat.rb
+++ packages/bat.rb
@@ -3,28 +3,26 @@ require 'package'
 class Bat < Package
   description 'Clone of cat with syntax highlighting and Git integration'
   homepage 'https://github.com/sharkdp/bat'
-  version '0.22.1'
+  version '0.23.0'
   license 'Apache-2.0, MIT'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-v0.22.1-arm-unknown-linux-gnueabihf.tar.gz',
-     armv7l: 'https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-v0.22.1-arm-unknown-linux-gnueabihf.tar.gz',
-       i686: 'https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-v0.22.1-i686-unknown-linux-gnu.tar.gz',
-     x86_64: 'https://github.com/sharkdp/bat/releases/download/v0.22.1/bat-v0.22.1-x86_64-unknown-linux-gnu.tar.gz'
+    aarch64: 'https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-arm-unknown-linux-gnueabihf.tar.gz',
+     armv7l: 'https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-arm-unknown-linux-gnueabihf.tar.gz',
+       i686: 'https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-i686-unknown-linux-gnu.tar.gz',
+     x86_64: 'https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz'
   })
   source_sha256({
-    aarch64: '1e390f9af728142afb1e454965b4722ce35165dcc7f8d9ca1e902deb9b3f8ff2',
-     armv7l: '1e390f9af728142afb1e454965b4722ce35165dcc7f8d9ca1e902deb9b3f8ff2',
-       i686: 'dc57b012470cfa9e7f4f0aa1c491c779f23a10bb504f59b798234aab5d33e790',
-     x86_64: 'eb81510a3d14d3a15353cb9c5edb2e037fbce07ee8e986c8c63c94ad55368acf'
+    aarch64: 'badbcf12c9aa4b03cc9c0df8a9c21141172981f55bfff28a0cb8ea58bea1eb41',
+     armv7l: 'badbcf12c9aa4b03cc9c0df8a9c21141172981f55bfff28a0cb8ea58bea1eb41',
+       i686: 'e2391d17fafb94a61fb04c85c9b00ef77d2282bdc4c45dc15ed1c8bd52643133',
+     x86_64: '901c7a0f2e54ea6ecd33f56a13b14ded265a24393421b0c99ad9f4563a8be4a1'
   })
 
   no_compile_needed
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}/man1"
-    FileUtils.install 'bat', "#{CREW_DEST_PREFIX}/bin", mode: 0o755
-    FileUtils.install 'bat.1', "#{CREW_DEST_MAN_PREFIX}/man1", mode: 0o755
+    FileUtils.install 'bat', "#{CREW_DEST_PREFIX}/bin/bat", mode: 0o755
+    FileUtils.install 'bat.1', "#{CREW_DEST_MAN_PREFIX}/man1/bat.1", mode: 0o644
   end
 end
