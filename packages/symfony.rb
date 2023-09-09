ee0ff1ab0f7ef46e49b70b7b157339cd86700c36
Ed Reel <edreel@gmail.com>
2023-09-09 17:29:07 -0500

    Symfony 5.5.2 => 5.5.8 (#8629)

--- packages/symfony.rb
+++ packages/symfony.rb
@@ -3,20 +3,20 @@ require 'package'
 class Symfony < Package
   description 'Symfony is a set of PHP Components, a Web Application framework'
   homepage 'https://symfony.com/'
-  version '5.5.2'
+  version '5.5.8'
   license 'AGPL-3.0'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_armv6.tar.gz',
-     armv7l: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_armv6.tar.gz',
-       i686: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_386.tar.gz',
-     x86_64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.2/symfony-cli_linux_amd64.tar.gz'
+    aarch64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.8/symfony-cli_linux_armv6.tar.gz',
+     armv7l: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.8/symfony-cli_linux_armv6.tar.gz',
+       i686: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.8/symfony-cli_linux_386.tar.gz',
+     x86_64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.5.8/symfony-cli_linux_amd64.tar.gz'
   })
   source_sha256({
-    aarch64: '987def27a646ef0f10b47f08149308f635db52dfe74d665d6298daf8d2d8c11c',
-     armv7l: '987def27a646ef0f10b47f08149308f635db52dfe74d665d6298daf8d2d8c11c',
-       i686: '986d9008af590ead3902a5a490c2183e34284c08cd4d57ad55fc1e82f7c2f05d',
-     x86_64: 'ff2966767d14e8c23df9efa16ec13a0b509c30733f354cc1291a14bd3f7d9983'
+    aarch64: '43b1a745ae5d7cf669bfa4791e9018fcda4bbf891179ff3ff60616063bceb329',
+     armv7l: '43b1a745ae5d7cf669bfa4791e9018fcda4bbf891179ff3ff60616063bceb329',
+       i686: '35105197c72aefdc9e713e4f9d5ca7d4de011fdc34044447af18c87bce3bea0b',
+     x86_64: '627e3f6a5da42c47744ed8ea8090cb76ef91f9fc93db8c64f46c508305c34a0a'
   })
 
   depends_on 'php81' unless File.exist? "#{CREW_PREFIX}/bin/php"
@@ -24,7 +24,6 @@ class Symfony < Package
   no_compile_needed
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'symfony', "#{CREW_DEST_PREFIX}/bin/symfony", mode: 0o755
   end
 end
