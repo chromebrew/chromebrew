004a8f260c80cfee250a83794d4e9c79e78e3f41
Ed Reel <edreel@gmail.com>
2023-09-09 17:16:55 -0500

    Wp_cli 2.7.1 => 2.8.1

--- packages/wp_cli.rb
+++ packages/wp_cli.rb
@@ -3,16 +3,15 @@ require 'package'
 class Wp_cli < Package
   description 'The command line interface for WordPress'
   homepage 'https://wp-cli.org/'
-  version '2.7.1'
+  version '2.8.1'
   license 'LGPL-3'
   compatibility 'all'
   source_url "https://github.com/wp-cli/wp-cli/releases/download/v#{version}/wp-cli-#{version}.phar"
-  source_sha256 'bbf096bccc6b1f3f1437e75e3254f0dcda879e924bbea403dff3cfb251d4e468'
+  source_sha256 '8503cd579480d0cb237b4bef35e0c3da11c2ab872a1bc8f26d2da0ca0729b6a7'
 
-  depends_on 'php74' unless File.exist? "#{CREW_PREFIX}/bin/php"
+  depends_on 'php81' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install "wp-cli-#{version}.phar", "#{CREW_DEST_PREFIX}/bin/wp", mode: 0o755
   end
 end
