83733b8262d1e61fa439f1d211e5900fd6d0e126
Ed Reel <edreel@gmail.com>
2023-09-11 15:03:49 -0500

    Mutt 2.2.11 => 2.2.12

--- packages/mutt.rb
+++ packages/mutt.rb
@@ -3,23 +3,23 @@ require 'buildsystems/autotools'
 class Mutt < Autotools
   description 'Mutt is a small but very powerful text-based mail client for Unix operating systems.'
   homepage 'http://mutt.org/'
-  version '2.2.11'
+  version '2.2.12'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'ftp://ftp.mutt.org/pub/mutt/mutt-2.2.11.tar.gz'
-  source_sha256 '12325cf66d5ff8ac4bd87fac8db52c869de52dd278fc301cfd57d5a1f9f465cc'
+  source_url 'ftp://ftp.mutt.org/pub/mutt/mutt-2.2.12.tar.gz'
+  source_sha256 '043af312f64b8e56f7fd0bf77f84a205d4c498030bd9586457665c47bb18ce38'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.11_armv7l/mutt-2.2.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.11_armv7l/mutt-2.2.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.11_i686/mutt-2.2.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.11_x86_64/mutt-2.2.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.12_armv7l/mutt-2.2.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.12_armv7l/mutt-2.2.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.12_i686/mutt-2.2.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mutt/2.2.12_x86_64/mutt-2.2.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '98bc06c612dcc678fc32265f832e749660413a316b1f8c319ade106cef04011e',
-     armv7l: '98bc06c612dcc678fc32265f832e749660413a316b1f8c319ade106cef04011e',
-       i686: 'ae547bc244cbd265563341f9a93dc221ffc552ec6538544167be22b2e842f105',
-     x86_64: '31e4c665796270c5a24bc6697ce01e71f5fd8f355ad4fb66eb27c200573b9377'
+    aarch64: 'd896cb0d3a3735d12951ba29b6f98b8f3cc32c4a29f6c9e4a26186f0ace42b71',
+     armv7l: 'd896cb0d3a3735d12951ba29b6f98b8f3cc32c4a29f6c9e4a26186f0ace42b71',
+       i686: '6ca743089425ed3d40bac6e3267d04ba3916244010b8ad9be50a720b01a88e4f',
+     x86_64: '1e877319f01ee8343036dfcd11bf4ba71a0f7ded74a6f3f9c585ae4dd5a1f5e8'
   })
 
   depends_on 'libcyrussasl'
