1869ab27fd5ee12e9ac9c9602e8d0e0017b532d9
Ed Reel <edreel@gmail.com>
2023-09-12 09:53:46 -0500

    Mysqltuner 1.8.3 => 2.2.12 (#8642)
    Co-authored-by: chronos <noreply@gitlab.com>

--- packages/mysqltuner.rb
+++ packages/mysqltuner.rb
@@ -3,21 +3,23 @@ require 'package'
 class Mysqltuner < Package
   description 'MySQLTuner is a script written in Perl that allows you to review a MySQL installation quickly and make adjustments to increase performance and stability.'
   homepage 'https://github.com/major/MySQLTuner-perl'
-  version '1.8.3'
+  version '2.2.12'
   license 'GPL-3+'
   compatibility 'all'
-  source_url 'https://github.com/major/MySQLTuner-perl/archive/1.8.3.tar.gz'
-  source_sha256 '9b8b1dff03550f03c659d2b850cb287d784f0d79f193c1d8a969516cc44738cb'
+  source_url 'https://github.com/major/MySQLTuner-perl/archive/v2.2.12.tar.gz'
+  source_sha256 '51d7f569ec2fde5ffbe38731e79c8b113899bbf5ea200fc407c622a359b2cca4'
 
   depends_on 'perl'
 
+  no_compile_needed
+
   def self.patch
+    system "sed -i 's,2\.2\.9,#{version},g' mysqltuner.pl"
     system "sed -i 's,/usr/bin/env perl,#{CREW_PREFIX}/bin/perl,' mysqltuner.pl"
   end
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/mysqltuner"
     FileUtils.install 'mysqltuner.pl', "#{CREW_DEST_PREFIX}/share/mysqltuner/mysqltuner.pl", mode: 0o755
     FileUtils.install 'basic_passwords.txt', "#{CREW_DEST_PREFIX}/share/mysqltuner/basic_passwords.txt", mode: 0o644
     FileUtils.install 'vulnerabilities.csv', "#{CREW_DEST_PREFIX}/share/mysqltuner/vulnerabilities.csv", mode: 0o644
