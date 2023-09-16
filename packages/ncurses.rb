c998a2471fc969b374663c0874138c0e13c11110
Satadru Pramanik <satadru@gmail.com>
2023-09-15 22:55:45 -0400

    update ncurses

--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -3,28 +3,27 @@ require 'package'
 class Ncurses < Package
   description 'The ncurses (new curses) library is a free software emulation of curses in System V Release 4.0 (SVr4), and more. — Wide character'
   homepage 'https://www.gnu.org/software/ncurses/'
-  version '6.4'
+  version '6.4-20230909'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/mirror/ncurses.git'
-  git_hashtag "v#{version}"
+  source_url 'https://invisible-island.net/archives/ncurses/current/ncurses-6.4-20230909.tgz'
+  source_sha256 'f859a4646974231a099a9339bbd66146dd4746a0dcbb802913c73335f8c750bb'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4_armv7l/ncurses-6.4-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4_armv7l/ncurses-6.4-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4_i686/ncurses-6.4-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4_x86_64/ncurses-6.4-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_armv7l/ncurses-6.4-20230909-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_armv7l/ncurses-6.4-20230909-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_i686/ncurses-6.4-20230909-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_x86_64/ncurses-6.4-20230909-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b0a5264b54ebb2c794dd160bf80c15a53e7bf643729ffe6d52a7cee14a38ec17',
-     armv7l: 'b0a5264b54ebb2c794dd160bf80c15a53e7bf643729ffe6d52a7cee14a38ec17',
-       i686: 'c528b561ca5f8e2add713471bc7c08febb56f207f661047b86efb1566905919f',
-     x86_64: '8deeac491093d2413eb512daf8c0a98839e3e144ff282d94ec9566c59bd98aba'
+    aarch64: '5698eaefd29e5e1526baef7f8eb84d2c1a8e286df43b9b17d659eb8edc65b0e6',
+     armv7l: '5698eaefd29e5e1526baef7f8eb84d2c1a8e286df43b9b17d659eb8edc65b0e6',
+       i686: 'e5210cbf69eb819c659106ce34196df8a287fdbec55bdb2f6b4d4a4b402408d3',
+     x86_64: 'abe7e5d619073319c8e5f8963f6b64619a64a0e3c6cdf5eb9ee79776a1d21283'
   })
 
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
-  no_patchelf
 
   def self.build
     # build libncurses
