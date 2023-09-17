39138b610f7cce9a06bfa817ec1fc8abc7b395c2
Ed Reel <edreel@gmail.com>
2023-09-17 11:39:57 -0500

    Brave 1.58.124 => 1.58.127 (#8665)
    Co-authored-by: chronos <noreply@gitlab.com>

--- packages/brave.rb
+++ packages/brave.rb
@@ -3,14 +3,14 @@ require 'package'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '1.58.124'
+  version '1.58.127'
   license 'MPL-2'
   compatibility 'x86_64'
   source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-#{version}-linux-amd64.zip"
-  source_sha256 'de3fefb33da491afa27e2c1f910025e9cc653b15e6f9579a356edc31528ff27f'
+  source_sha256 'e6654dc36299d4c2cf131f7dfe54b426c75e17ff3a947a4d41492fe2489d958d'
 
   no_compile_needed
-  no_strip
+  no_shrink
 
   depends_on 'gtk3'
   depends_on 'libcom_err'
