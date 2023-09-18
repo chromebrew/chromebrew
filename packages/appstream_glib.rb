889bb478b41e5d0f8058e7e1df7b7865eba826da
chronos <noreply@gitlab.com>
2023-09-18 03:01:25 +0000

    Appstream_glib 0.7.18-dbd6 => 0.8.2

--- packages/appstream_glib.rb
+++ packages/appstream_glib.rb
@@ -1,25 +1,23 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Appstream_glib < Package
+class Appstream_glib < Meson
   description 'Objects and methods for reading and writing AppStream metadata'
   homepage 'https://people.freedesktop.org/~hughsient/appstream-glib/'
-  version '0.7.18-dbd6'
+  version '0.8.2'
   license 'LGPL-2.1+'
-  compatibility 'all'
-  source_url 'https://github.com/hughsie/appstream-glib/archive/dbd62f6e054aab8ad8fd36a81023a247fdd543d8.zip'
-  source_sha256 'e6532620fde20df56d121f692ab24a28a4389b2aa37d5199d676eda37baf1fd4'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://github.com/hughsie/appstream-glib.git'
+  git_hashtag 'appstream_glib_0_8_2'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/appstream_glib/0.7.18-dbd6_armv7l/appstream_glib-0.7.18-dbd6-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/appstream_glib/0.7.18-dbd6_armv7l/appstream_glib-0.7.18-dbd6-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/appstream_glib/0.7.18-dbd6_i686/appstream_glib-0.7.18-dbd6-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/appstream_glib/0.7.18-dbd6_x86_64/appstream_glib-0.7.18-dbd6-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/appstream_glib/0.8.2_armv7l/appstream_glib-0.8.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/appstream_glib/0.8.2_armv7l/appstream_glib-0.8.2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/appstream_glib/0.8.2_x86_64/appstream_glib-0.8.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e917ad02dce4cfcf29f82a6f5a6af91bb1d26493aefeec0a14ecaf71c274821c',
-     armv7l: 'e917ad02dce4cfcf29f82a6f5a6af91bb1d26493aefeec0a14ecaf71c274821c',
-       i686: 'eb36d8dc0e3543d23b6c9a0ec4f05b246f9b6552882a6c387e79a05eaaea72af',
-     x86_64: 'bde94624a342e2db10151ec317e0d719a99638e76385be0b42f4b691f19949d5'
+    aarch64: '2a591cfad5df70455b2ff90f915ae057f8e5efc1e95e22c29bd5a78c9406e48e',
+     armv7l: '2a591cfad5df70455b2ff90f915ae057f8e5efc1e95e22c29bd5a78c9406e48e',
+     x86_64: '2a10e4da59cadb05dadd12364062fea9050883cb550bd8ca14c4b985fda87f3c'
   })
 
   depends_on 'cairo'
@@ -44,19 +42,5 @@ class Appstream_glib < Package
   depends_on 'pango'
   depends_on 'util_linux'
 
-  def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} \
-      -Dtests=false \
-      -Dsysprof=disabled \
-      -Dintrospection=true \
-      -Drpm=false \
-      builddir"
-    system 'meson configure builddir'
-    system "sed -i 's#-R#-Wl,-rpath=#g' builddir/build.ninja"
-    system 'ninja -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
+  meson_options '-Dintrospection=true -Drpm=false'
 end
