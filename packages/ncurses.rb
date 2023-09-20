cde71d30544c61614c381c1e615892abc0cf040d
Satadru Pramanik <satadru@gmail.com>
2023-09-20 18:31:55 -0400

    rebuild ncurses, adjust url

--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -3,23 +3,23 @@ require 'package'
 class Ncurses < Package
   description 'The ncurses (new curses) library is a free software emulation of curses in System V Release 4.0 (SVr4), and more. — Wide character'
   homepage 'https://www.gnu.org/software/ncurses/'
-  version '6.4-20230909'
+  version '6.4-20230918'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://invisible-island.net/archives/ncurses/current/ncurses-6.4-20230909.tgz'
-  source_sha256 'f859a4646974231a099a9339bbd66146dd4746a0dcbb802913c73335f8c750bb'
+  source_url 'https://github.com/ThomasDickey/ncurses-snapshots.git'
+  git_hashtag 'v6_4_20230918'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_armv7l/ncurses-6.4-20230909-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_armv7l/ncurses-6.4-20230909-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_i686/ncurses-6.4-20230909-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230909_x86_64/ncurses-6.4-20230909-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230918_armv7l/ncurses-6.4-20230918-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230918_armv7l/ncurses-6.4-20230918-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230918_i686/ncurses-6.4-20230918-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6.4-20230918_x86_64/ncurses-6.4-20230918-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '5698eaefd29e5e1526baef7f8eb84d2c1a8e286df43b9b17d659eb8edc65b0e6',
-     armv7l: '5698eaefd29e5e1526baef7f8eb84d2c1a8e286df43b9b17d659eb8edc65b0e6',
-       i686: 'e5210cbf69eb819c659106ce34196df8a287fdbec55bdb2f6b4d4a4b402408d3',
-     x86_64: 'abe7e5d619073319c8e5f8963f6b64619a64a0e3c6cdf5eb9ee79776a1d21283'
+    aarch64: '40794c56e1a88fb6750ab78bdeecba24f5574b84b80e61180870a4cfd7345d12',
+     armv7l: '40794c56e1a88fb6750ab78bdeecba24f5574b84b80e61180870a4cfd7345d12',
+       i686: '20f5040f1102665a0f9f9b7781911432acbc5a88cc4e459c6bf4a02fda0d5d82',
+     x86_64: '39d022c4b62f897785491c79964273cd1b0020792412211fcf95b0860979d4f8'
   })
 
   depends_on 'gcc_lib' # R
