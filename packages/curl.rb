514022807a334c81a0d1053dc2b28596bfe2b6c2
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-09-20 19:08:35 -0400

    curl => 8.3.0 (#8677)

--- packages/curl.rb
+++ packages/curl.rb
@@ -1,25 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Curl < Package
+class Curl < Autotools
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.se/'
-  version '8.2.1'
+  version '8.3.0'
   license 'curl'
   compatibility 'all'
-  source_url 'https://curl.se/download/curl-8.2.1.tar.xz'
-  source_sha256 'dd322f6bd0a20e6cebdfd388f69e98c3d183bed792cf4713c8a7ef498cba4894'
+  source_url 'https://curl.se/download/curl-8.3.0.tar.xz'
+  source_sha256 '376d627767d6c4f05105ab6d497b0d9aba7111770dd9d995225478209c37ea63'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.2.1_armv7l/curl-8.2.1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.2.1_armv7l/curl-8.2.1-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.2.1_i686/curl-8.2.1-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.2.1_x86_64/curl-8.2.1-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.3.0_armv7l/curl-8.3.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.3.0_armv7l/curl-8.3.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.3.0_i686/curl-8.3.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.3.0_x86_64/curl-8.3.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '5367dd8f8b4a18217e8aabd731232398519a985629256d51e3cc86d36671c995',
-     armv7l: '5367dd8f8b4a18217e8aabd731232398519a985629256d51e3cc86d36671c995',
-       i686: '8ee2d0983014c888263c19e55e4a3019202fe147f697ec617173fe944b632c57',
-     x86_64: '86d80829dd1b7124ebaa3c457c5157d9a2dbf282714b9273c537985e6f4a2aad'
+    aarch64: '12c66e1342454bdc628ee2f4858131e4bc0b66eec398331c4879a4ef14021c5b',
+     armv7l: '12c66e1342454bdc628ee2f4858131e4bc0b66eec398331c4879a4ef14021c5b',
+       i686: '301128e5ef2625f07485dd9f2a328469c77afc837416027c4a50c1c608d97f1c',
+     x86_64: '74bfc11a9086d27128209330d17c96c5695d2e7872375900d831ff037fef90ab'
   })
 
   depends_on 'brotli' # R
@@ -40,11 +40,7 @@ class Curl < Package
   depends_on 'zlibpkg' # R
   depends_on 'zstd' # R
 
-  def self.build
-    system '[ -x configure ] || autoreconf -fvi'
-    system 'filefix'
-    system "mold -run ./configure #{CREW_OPTIONS} \
-      --disable-maintainer-mode \
+  configure_options "--disable-maintainer-mode \
       --enable-ares \
       --enable-ipv6 \
       --enable-ldap \
@@ -56,10 +52,4 @@ class Curl < Package
       --with-openssl \
       --without-gnutls \
       --without-librtmp"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
