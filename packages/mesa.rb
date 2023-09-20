74c46a1d7441a708961183fc31b947673fd86f64
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-09-20 17:44:25 -0400

    mesa and sommelier updated and built against llvm17 (#8674)

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -3,22 +3,22 @@ require 'package'
 class Mesa < Package
   description 'Open-source implementation of the OpenGL specification'
   homepage 'https://www.mesa3d.org'
-  @_ver = '23.1.7'
-  version "#{@_ver}-llvm16"
+  @_ver = '23.2.0-rc3'
+  version "#{@_ver}-llvm17"
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.freedesktop.org/mesa/mesa.git'
   git_hashtag "mesa-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.1.7-llvm16_armv7l/mesa-23.1.7-llvm16-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.1.7-llvm16_armv7l/mesa-23.1.7-llvm16-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.1.7-llvm16_x86_64/mesa-23.1.7-llvm16-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.2.0-rc3-llvm17_armv7l/mesa-23.2.0-rc3-llvm17-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.2.0-rc3-llvm17_armv7l/mesa-23.2.0-rc3-llvm17-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.2.0-rc3-llvm17_x86_64/mesa-23.2.0-rc3-llvm17-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '7e9f3ca82a711837db8844ae5b6e4a9ed54f6e15a1dccfe9dc90e8b5ddbdd0af',
-     armv7l: '7e9f3ca82a711837db8844ae5b6e4a9ed54f6e15a1dccfe9dc90e8b5ddbdd0af',
-     x86_64: '616f5b654eb6ee910dd94317e0a600e94538fc3992f7d5cfd9bfbe62714ce5f4'
+    aarch64: 'b460334f6f91834f0ec42115c186b952c2a177b9ff5908f04a1997df12bf20d8',
+     armv7l: 'b460334f6f91834f0ec42115c186b952c2a177b9ff5908f04a1997df12bf20d8',
+     x86_64: '7daba007a3f08650aec99a3d7f3e233c2428aa1a64c63c2fdd7d92c5bbf32186'
   })
 
   depends_on 'elfutils' # R
@@ -46,8 +46,8 @@ class Mesa < Package
   depends_on 'libxvmc' # R
   depends_on 'libxv' # R
   depends_on 'libxxf86vm' # R
-  depends_on 'llvm16_dev' => :build
-  depends_on 'llvm16_lib' # R
+  depends_on 'llvm17_dev' => :build
+  depends_on 'llvm17_lib' # R
   depends_on 'lm_sensors' # R
   depends_on 'py3_mako'
   depends_on 'valgrind' => :build
