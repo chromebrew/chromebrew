4956bcf6f7638042b64962f2a5e58895cf556305
Ed Reel <edreel@gmail.com>
2023-09-25 09:29:26 -0500

    Coreutils 9.2 => 9.4

--- packages/coreutils.rb
+++ packages/coreutils.rb
@@ -1,29 +1,28 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Coreutils < Package
+class Coreutils < Autotools
   description 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.'
   homepage 'https://www.gnu.org/software/coreutils/coreutils.html'
-  version '9.2'
+  version '9.4'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gnu/coreutils/coreutils-9.2.tar.xz'
-  source_sha256 '6885ff47b9cdb211de47d368c17853f406daaf98b148aaecdf10de29cc04b0b3'
+  source_url 'https://ftpmirror.gnu.org/coreutils/coreutils-9.4.tar.xz'
+  source_sha256 'ea613a4cf44612326e917201bbbcdfbd301de21ffc3b59b6e5c07e040b275e52'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.2_armv7l/coreutils-9.2-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.2_armv7l/coreutils-9.2-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.2_i686/coreutils-9.2-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.2_x86_64/coreutils-9.2-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.4_armv7l/coreutils-9.4-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.4_armv7l/coreutils-9.4-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.4_i686/coreutils-9.4-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/coreutils/9.4_x86_64/coreutils-9.4-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '39540783dbce98b48122fdba187de6a14622fdeec7bc6ceffe399e280379ec4f',
-     armv7l: '39540783dbce98b48122fdba187de6a14622fdeec7bc6ceffe399e280379ec4f',
-       i686: '0745b0227a7f39f3958403cb116974a11189dccc6953a23e2919768e83f769ee',
-     x86_64: 'c6c2bd27e42c071f17bf7499618a6c4defc24ec2b72aebc3c6ccb0e864bf3948'
+    aarch64: 'e3227e245094451d3dbc2f05aeef64b56149340b54da864c613ab124d2e5e413',
+     armv7l: 'e3227e245094451d3dbc2f05aeef64b56149340b54da864c613ab124d2e5e413',
+       i686: '2b432eb59bd0e97bee40786c054c9be4863f95727bc7ed6ffd98c57de686ec14',
+     x86_64: '40c8e491fe8d37583cffd67323cf4a0304ca95d29289db00b5dee126b115c5cf'
   })
 
   depends_on 'libcap' # R
-  no_patchelf
 
   def self.preflight
     %w[uutils_coreutils].each do |cutils|
@@ -36,7 +35,9 @@ class Coreutils < Package
   end
 
   def self.build
-    system "./configure #{CREW_OPTIONS}"
+    year2038 = ARCH == 'x86_64' ? '' : ' --disable-year2038'
+    options = CREW_OPTIONS + year2038
+    system "./configure #{options}"
     system 'make'
     arch = <<~EOF
       #!/bin/bash
@@ -54,4 +55,7 @@ class Coreutils < Package
       FileUtils.rm %w[kill uptime]
     end
   end
+
+  # FAIL: tests/tail/inotify-dir-recreate.sh
+  # run_tests
 end
