ed940673bf2247a29397671a2de8587c8b819f51
Ed Reel <edreel@gmail.com>
2023-09-27 03:53:24 -0500

    Glibmm_2_68 2.75.0 => 2.78.0

--- packages/glibmm_2_68.rb
+++ packages/glibmm_2_68.rb
@@ -3,28 +3,27 @@ require 'package'
 class Glibmm_2_68 < Package
   description 'C++ bindings for GLib api version 2.68'
   homepage 'https://www.gtkmm.org'
-  version '2.75.0'
+  version '2.78.0'
   license 'LGPL-2.1+'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/glibmm.git'
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glibmm_2_68/2.75.0_armv7l/glibmm_2_68-2.75.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glibmm_2_68/2.75.0_armv7l/glibmm_2_68-2.75.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glibmm_2_68/2.75.0_i686/glibmm_2_68-2.75.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glibmm_2_68/2.75.0_x86_64/glibmm_2_68-2.75.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glibmm_2_68/2.78.0_armv7l/glibmm_2_68-2.78.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glibmm_2_68/2.78.0_armv7l/glibmm_2_68-2.78.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/glibmm_2_68/2.78.0_x86_64/glibmm_2_68-2.78.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '6e6dea95b0de055316df3584217217ac6e71f1433f5fd94ce686a556e1f7cdcd',
-     armv7l: '6e6dea95b0de055316df3584217217ac6e71f1433f5fd94ce686a556e1f7cdcd',
-       i686: '2d8765e47873841ff2dd2dfa5b23dc8a7add20624978698223416e1e3e368860',
-     x86_64: 'a50f35cdfec10d52ce52c8054310aca215bb2089da825245992672a1a12dec66'
+    aarch64: 'd101b082fdbd28e16fed1ad66a3042a58f4d4d5575e5a5b9de59552ac8402964',
+     armv7l: 'd101b082fdbd28e16fed1ad66a3042a58f4d4d5575e5a5b9de59552ac8402964',
+     x86_64: '115a613e6a6f7dcc3faa0f4bbc5eecadb068951a3c9e1bf0a9f90102702db5a0'
   })
 
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'graphviz' # R
   depends_on 'libsigcplusplus3' # R
   depends_on 'mm_common' => :build
 
