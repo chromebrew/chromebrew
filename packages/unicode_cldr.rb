02d2224c09f8b27ce7b25f85d2fcef4f45d20887
Ed Reel <edreel@gmail.com>
2023-10-01 16:33:59 -0500

    Unicode_cldr 43.0 => 43.1 (#8722)

--- packages/unicode_cldr.rb
+++ packages/unicode_cldr.rb
@@ -3,17 +3,16 @@ require 'package'
 class Unicode_cldr < Package
   description 'Unicode Common Locale Data Repository'
   homepage 'http://cldr.unicode.org/'
-  version '43.0'
+  version '43.1'
   license 'unicode'
   compatibility 'all'
-  source_url "https://unicode.org/Public/cldr/#{version.rpartition('.')[0]}/cldr-common-#{version}.zip"
-  source_sha256 '7800dadb6a11e06ba1475f8c2830aa87e0749ed441c953d8deea60b4baeef592'
+  source_url 'https://github.com/unicode-org/cldr.git'
+  git_hashtag 'release-43-1'
 
   no_compile_needed
 
   def self.install
-    FileUtils.mkdir_p("#{CREW_DEST_PREFIX}/share/unicode/cldr/common")
-    FileUtils.cp_r('common/.', "#{CREW_DEST_PREFIX}/share/unicode/cldr/common")
-    FileUtils.chmod_R(0o755, "#{CREW_DEST_PREFIX}/share/unicode/cldr/common")
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/unicode/cldr"
+    FileUtils.cp_r 'common/', "#{CREW_DEST_PREFIX}/share/unicode/cldr"
   end
 end
