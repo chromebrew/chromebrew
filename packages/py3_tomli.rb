5cd8fdead4bee1375187666736828fe7e3602523
Satadru Pramanik <satadru@gmail.com>
2023-10-03 17:14:17 -0400

    fix package versioning

--- packages/py3_tomli.rb
+++ packages/py3_tomli.rb
@@ -17,10 +17,10 @@ class Py3_tomli < Python
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_tomli/2.0.1-py3.12_x86_64/py3_tomli-2.0.1-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '61dbfafd949c4991a945a9fcce2b949f0ed325fedb2a350248b10283dba204d0',
-     armv7l: '61dbfafd949c4991a945a9fcce2b949f0ed325fedb2a350248b10283dba204d0',
-       i686: '360bbb2634e35761dfde2b39ef206d494b026528dd426957ecba714537ba4a26',
-     x86_64: 'd7da94aa98fcd43e3c4ffabe53cad959bac1c75f4b7a7f73880a53ec65e4d3cc'
+    aarch64: '35692904b29b433587a5e52cb4f128624d9df8ede7020777860b9296917d62ff',
+     armv7l: '35692904b29b433587a5e52cb4f128624d9df8ede7020777860b9296917d62ff',
+       i686: 'c8e83f1e16cd0e1c2b0e322361963b40e6ba32bfaf59aa342557768bee5fe4a1',
+     x86_64: '09bcd13b118d79e12eb097525d90555fb7180883db0c6cb91bf0264f8bc0b09b'
   })
 
   depends_on 'python3'
