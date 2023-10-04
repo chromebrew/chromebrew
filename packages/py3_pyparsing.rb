673fa2bee8d24c62a12193e391cb65df254437f8
Satadru Pramanik <satadru@gmail.com>
2023-10-04 08:07:16 -0400

    update py3_packaging, py3_pyparsing

--- packages/py3_pyparsing.rb
+++ packages/py3_pyparsing.rb
@@ -8,19 +8,19 @@ class Py3_pyparsing < Python
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/pyparsing/pyparsing.git'
-  git_hashtag "pyparsing_#{@_ver}"
+  git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9-py3.12_armv7l/py3_pyparsing-3.0.9-py3.12-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9-py3.12_armv7l/py3_pyparsing-3.0.9-py3.12-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9-py3.12_i686/py3_pyparsing-3.0.9-py3.12-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.0.9-py3.12_x86_64/py3_pyparsing-3.0.9-py3.12-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.1.1-py3.12_armv7l/py3_pyparsing-3.1.1-py3.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.1.1-py3.12_armv7l/py3_pyparsing-3.1.1-py3.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.1.1-py3.12_i686/py3_pyparsing-3.1.1-py3.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pyparsing/3.1.1-py3.12_x86_64/py3_pyparsing-3.1.1-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '1aefcf303baf937046b8567543c738aad4d91261674ebdc16154a8355890de8c',
-     armv7l: '1aefcf303baf937046b8567543c738aad4d91261674ebdc16154a8355890de8c',
-       i686: '12a1f316d390f4e5eba4d721e23ffe46badb2142127395a8f8650dbaee3f65d6',
-     x86_64: 'b886898c0dc40e497b6e2b56e467c9247ab811d0ff9dc68bdd57637c23436f74'
+    aarch64: 'fae7cd1b45e3f9f03c5184eb96435cb0493bb09059a2601cebf956f7adea097f',
+     armv7l: 'fae7cd1b45e3f9f03c5184eb96435cb0493bb09059a2601cebf956f7adea097f',
+       i686: '08ecb776d1fc561f00dcc8aae2cc7296a21f2fcdef6085d593b9f3031a99bbbf',
+     x86_64: '5f02a7f3f7b6b857a162914563d26e6878bb35eef0a2ae2bb2373d4508b1be43'
   })
 
   depends_on 'python3'
