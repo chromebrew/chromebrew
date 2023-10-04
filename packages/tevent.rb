06127aea72e77af549743d1540f01aec74559703
Satadru Pramanik <satadru@gmail.com>
2023-10-04 09:30:04 -0400

    update talloc, tevent

--- packages/tevent.rb
+++ packages/tevent.rb
@@ -6,24 +6,24 @@ require 'package'
 class Tevent < Package
   description 'Event system based on the talloc memory management library'
   homepage 'https://tevent.samba.org/'
-  @_ver = '0.14.1'
-  version "#{@_ver}-py3.11"
+  @_ver = '0.15.0'
+  version "#{@_ver}-py3.12"
   license 'LGPL'
   compatibility 'all'
   source_url "https://samba.org/ftp/tevent/tevent-#{@_ver}.tar.gz"
-  source_sha256 'ef85fcaa80ffd2351036ba4b347630fef2a1ac3da964a7f1820466bad03cd00d'
+  source_sha256 '662a9f277281bcf506b6bc0a0baa03e44a62229516ee34bcc703a0b82aa46905'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.14.1-py3.11_armv7l/tevent-0.14.1-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.14.1-py3.11_armv7l/tevent-0.14.1-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.14.1-py3.11_i686/tevent-0.14.1-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.14.1-py3.11_x86_64/tevent-0.14.1-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.15.0-py3.12_armv7l/tevent-0.15.0-py3.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.15.0-py3.12_armv7l/tevent-0.15.0-py3.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.15.0-py3.12_i686/tevent-0.15.0-py3.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/tevent/0.15.0-py3.12_x86_64/tevent-0.15.0-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '345bde4110b93f90d8d71109dd28e599dd23bb665c652ed3e6d743c7980b6cca',
-     armv7l: '345bde4110b93f90d8d71109dd28e599dd23bb665c652ed3e6d743c7980b6cca',
-       i686: 'e7483ab95644fb71d5fea6a4b97813b7d30edc470295bcfcdb6754b7734e5413',
-     x86_64: '9670ae6cdf285b38902cf8b552698266d514b1ed4627d6e1df2bc0358ee11b73'
+    aarch64: '4c53a7e86a60c982855809c9bca2cbaf4802b1cc8d7919fae6b71e3e4d6deaa7',
+     armv7l: '4c53a7e86a60c982855809c9bca2cbaf4802b1cc8d7919fae6b71e3e4d6deaa7',
+       i686: '9f0ed4a472cbb1acdb356bf23b9354cb5f22a9a66cf91be6669808fb3acff081',
+     x86_64: 'fe16ba3edd13137222598a9565f4f3ae6e231b80868bdfcc262805c84183b412'
   })
 
   depends_on 'cmocka' => :build
