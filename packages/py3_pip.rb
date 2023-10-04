1c488101a031a7e2c6101d9e4edce1394bfb17b2
Satadru Pramanik <satadru@gmail.com>
2023-10-03 21:15:12 -0400

    update py3_pip

--- packages/py3_pip.rb
+++ packages/py3_pip.rb
@@ -1,40 +1,29 @@
-require 'package'
+require 'buildsystems/python'
 
-class Py3_pip < Package
+class Py3_pip < Python
   description 'Pip is the python package manager from the Python Packaging Authority.'
   homepage 'https://pip.pypa.io/'
-  @_ver = '22.3.1'
-  version "#{@_ver}-py3.11"
+  @_ver = '23.2.1'
+  version "#{@_ver}-py3.12"
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/pypa/pip.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/22.3.1-py3.11_armv7l/py3_pip-22.3.1-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/22.3.1-py3.11_armv7l/py3_pip-22.3.1-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/22.3.1-py3.11_i686/py3_pip-22.3.1-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/22.3.1-py3.11_x86_64/py3_pip-22.3.1-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/23.2.1-py3.12_armv7l/py3_pip-23.2.1-py3.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/23.2.1-py3.12_armv7l/py3_pip-23.2.1-py3.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/23.2.1-py3.12_i686/py3_pip-23.2.1-py3.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pip/23.2.1-py3.12_x86_64/py3_pip-23.2.1-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '023cc76bd338a1d14b6ed45d55e8fe6b05454145abe941cc0fdec3d0bbf5ffe4',
-     armv7l: '023cc76bd338a1d14b6ed45d55e8fe6b05454145abe941cc0fdec3d0bbf5ffe4',
-       i686: '3826d8987e023d5afa98cb322a6f920fcbbc37c83d82ec4e048b28c7fd16cdaf',
-     x86_64: 'af52f8111904a49684b37251cc65c8d5dc9235238ecf49aa56a27fb95bd1c994'
+    aarch64: '158b83094f5520b638bee16c5599bd0135382b58b43fa4b3a83c53ed9121856e',
+     armv7l: '158b83094f5520b638bee16c5599bd0135382b58b43fa4b3a83c53ed9121856e',
+       i686: 'a0a695312be7d34ced2c4087a39257cff87b37f9994c46e44f395d4bc2a0957b',
+     x86_64: 'd159d50bae5227068e20403c6b08069303a5afcb725a99ec325238719de2e9dc'
   })
 
-  depends_on 'python3', '< 3.11.0'
+  depends_on 'python3', '< 3.13.0'
 
   conflicts_ok
-
-  def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
-
-  def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
-
-    # Make pip3 the default pip
-    # FileUtils.ln_s "pip3", "#{CREW_DEST_PREFIX}/bin/pip" # This automatically happens
-  end
 end
