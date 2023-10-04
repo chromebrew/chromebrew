a4d20d19b925ac0f65ede11f6010fe77b736f8ff
Satadru Pramanik <satadru@gmail.com>
2023-10-03 21:45:21 -0400

    py3_websockets using pip buildsystem

--- packages/py3_websockets.rb
+++ packages/py3_websockets.rb
@@ -10,16 +10,16 @@ class Py3_websockets < Pip
   source_url 'SKIP'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.11_armv7l/py3_websockets-11.0.3-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.11_armv7l/py3_websockets-11.0.3-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.11_i686/py3_websockets-11.0.3-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.11_x86_64/py3_websockets-11.0.3-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.12_armv7l/py3_websockets-11.0.3-py3.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.12_armv7l/py3_websockets-11.0.3-py3.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.12_i686/py3_websockets-11.0.3-py3.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_websockets/11.0.3-py3.12_x86_64/py3_websockets-11.0.3-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e99b09d9084b0e4ed82014c51dd88e59253b748223ccf0e68b20279a750470fc',
-     armv7l: 'e99b09d9084b0e4ed82014c51dd88e59253b748223ccf0e68b20279a750470fc',
-       i686: 'a6d2a87d2bcaecb6d3a625e15b5b7f51f8102e2bbfebdbb27fc9e4294e0a9d8e',
-     x86_64: 'ae8ee5d284f2b2d1fd43d153b3b4f423ed46a5eb94c93292f4922940dc30767c'
+    aarch64: 'ee9f174e40937fd731bb66f496554cd740571f6de6b19f29e285e0f397c586f0',
+     armv7l: 'ee9f174e40937fd731bb66f496554cd740571f6de6b19f29e285e0f397c586f0',
+       i686: 'e639477f5f0044f5764dceee7aac74d1902128046b47957930348d01761815cd',
+     x86_64: 'e7f49a05a22346ee8230dd26a26a77c25bf9fd15a71bf1b03ea231e81006432d'
   })
 
   depends_on 'python3' # L
