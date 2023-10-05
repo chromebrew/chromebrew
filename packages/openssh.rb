a00cd8d538e9ca9475d336efc26325cd07820d7e
Ed Reel <edreel@gmail.com>
2023-10-04 23:56:16 -0500

    Openssh 9.4p1 => 9.5p1

--- packages/openssh.rb
+++ packages/openssh.rb
@@ -3,23 +3,23 @@ require 'buildsystems/autotools'
 class Openssh < Autotools
   description 'OpenSSH is the premier connectivity tool for remote login with the SSH protocol.'
   homepage 'https://www.openssh.com/'
-  version '9.4p1'
+  version '9.5p1'
   license 'BSD and GPL-2'
   compatibility 'all'
   source_url 'https://github.com/openssh/openssh-portable.git'
-  git_hashtag 'V_9_4_P1'
+  git_hashtag 'V_9_5_P1'
 
   binary_url({
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_i686/openssh-9.4p1-chromeos-i686.tar.zst',
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_armv7l/openssh-9.4p1-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_armv7l/openssh-9.4p1-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.4p1_x86_64/openssh-9.4p1-chromeos-x86_64.tar.zst'
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.5p1_i686/openssh-9.5p1-chromeos-i686.tar.zst',
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.5p1_armv7l/openssh-9.5p1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.5p1_armv7l/openssh-9.5p1-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/openssh/9.5p1_x86_64/openssh-9.5p1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-       i686: '52afc47da42cb9781d834ac1a2ba5d80bcb59d477814a8e712507c0d4f2777dc',
-    aarch64: '5b2b1f607dc197dacf61f2c18ee94d7945b04bfffef2a5d0d6f13e9accf75e33',
-     armv7l: '5b2b1f607dc197dacf61f2c18ee94d7945b04bfffef2a5d0d6f13e9accf75e33',
-     x86_64: '559fd33ad76b3077a8bb7073406a80bbc8cbf89eafa1a77b6e1d70902d6f04fb'
+       i686: 'bdb0ed63cf5def0fcddce9353cc1685af9adf3a7cc989197b8321a64f80fb129',
+    aarch64: 'd78d84daeeb728cc8ca5df99777b5b84ff14ffb46c04b58e0a673f3bc8ba91ef',
+     armv7l: 'd78d84daeeb728cc8ca5df99777b5b84ff14ffb46c04b58e0a673f3bc8ba91ef',
+     x86_64: 'b2ba793087a80bd9e24b4571be43eeb3e65bdeae75a0fa687cabd8fef8552278'
   })
 
   depends_on 'gcc_lib' # R
