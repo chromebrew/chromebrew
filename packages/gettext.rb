26d1ebbf3fd4fc5fea0c14f9f5014f6e7658078f
Ed Reel <edreel@gmail.com>
2023-10-04 21:36:41 -0500

    Gettext 0.22 => 0.22.3 (#8738)

--- packages/gettext.rb
+++ packages/gettext.rb
@@ -1,25 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Gettext < Package
+class Gettext < Autotools
   description 'GNU gettext utilities are a set of tools that provides a framework to help other GNU packages produce multi-lingual messages.'
   homepage 'https://www.gnu.org/software/gettext/'
-  version '0.22'
+  version '0.22.3'
   license 'GPL-3+ and LGPL-2.1+'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gettext/gettext-0.22.tar.lz'
-  source_sha256 '24e2669b23abf26f9afdbbc3055edeb33879fb36454de80e8e4aadb3dbbe0b96'
+  source_url 'https://ftpmirror.gnu.org/gettext/gettext-0.22.3.tar.lz'
+  source_sha256 '109da96c68c3df67ef6dadac23fe9f4e9ba3dc07a9844cb10b08a612cb9e6f71'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22_armv7l/gettext-0.22-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22_armv7l/gettext-0.22-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22_i686/gettext-0.22-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22_x86_64/gettext-0.22-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22.3_armv7l/gettext-0.22.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22.3_armv7l/gettext-0.22.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22.3_i686/gettext-0.22.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gettext/0.22.3_x86_64/gettext-0.22.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '3808f2fdd880d2f8bc33179773ca14c3bf67d8e90faecd176d561bc88d35f8b4',
-     armv7l: '3808f2fdd880d2f8bc33179773ca14c3bf67d8e90faecd176d561bc88d35f8b4',
-       i686: '41286e5a6b2fe8d6c59589a963d161cc8e18c261c85f2d61c82fca94a5c8ecdd',
-     x86_64: '83325934f7695541f53f9ec84e5e5ffa35a3cdbf1b7607d4e17532f6567812fb'
+    aarch64: 'd31ef5b9149219196b389e505d999b698bf124b88c850deb294e743607bc448f',
+     armv7l: 'd31ef5b9149219196b389e505d999b698bf124b88c850deb294e743607bc448f',
+       i686: '429bfd53a81afabf131c4150e5261929231541b39b31ef47c929674f57dd42fe',
+     x86_64: 'c32933f4644304b0d793315876335663b6c5b6b9a850cfb74778186f3a8cd1b4'
   })
 
   depends_on 'acl' # R
@@ -34,23 +34,8 @@ class Gettext < Package
 
   no_env_options
 
-  def self.build
-    system '[ -x configure ] || ./autogen.sh'
-    system 'filefix'
-    system "./configure #{CREW_OPTIONS} \
-      --disable-static \
-      --enable-shared \
-      --with-pic \
-      --without-included-gettext"
-    system 'make'
-  end
-
-  def self.check
-    # FAIL: test-u8-conv-from-enc
-    # system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options '--disable-static \
+    --enable-shared \
+    --with-pic \
+    --without-included-gettext'
 end
