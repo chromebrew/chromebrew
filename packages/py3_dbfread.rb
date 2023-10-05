70b3d3deb3e6251fcde2dba22e0c10b44f8c088a
Satadru Pramanik <satadru@gmail.com>
2023-10-05 11:17:37 -0400

    py3_dbfread.rb  py3_agate_dfb.rb py3_sqlalchemy.rb py3_agate_sql.rb

--- packages/py3_dbfread.rb
+++ packages/py3_dbfread.rb
@@ -1,35 +1,27 @@
-require 'package'
+require 'buildsystems/python'
 
-class Py3_dbfread < Package
+class Py3_dbfread < Python
   description 'DBFread reads DBF Files with Python.'
   homepage 'https://dbfread.readthedocs.io/'
   @_ver = '2.0.7'
-  version "#{@_ver}-py3.11"
+  version "#{@_ver}-py3.12"
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/olemb/dbfread.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.11_armv7l/py3_dbfread-2.0.7-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.11_armv7l/py3_dbfread-2.0.7-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.11_i686/py3_dbfread-2.0.7-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.11_x86_64/py3_dbfread-2.0.7-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.12_armv7l/py3_dbfread-2.0.7-py3.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.12_armv7l/py3_dbfread-2.0.7-py3.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.12_i686/py3_dbfread-2.0.7-py3.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_dbfread/2.0.7-py3.12_x86_64/py3_dbfread-2.0.7-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b65ebb3937c60bf909fd6b352f17139eb778cb8be33a194be129913f46b5ec66',
-     armv7l: 'b65ebb3937c60bf909fd6b352f17139eb778cb8be33a194be129913f46b5ec66',
-       i686: '00d1994ab6b9d78860a79346a25769df7ac4724d97a0dc2b05a195daa6756891',
-     x86_64: '8cbbe7ccf0407c72ce9838d4dec3cef7374de1db139312c977ad6a05deb5d39e'
+    aarch64: '93cfff2250b28c5ebabbcb9f95197f2869e1769aa9e385a810a57c71a9945f55',
+     armv7l: '93cfff2250b28c5ebabbcb9f95197f2869e1769aa9e385a810a57c71a9945f55',
+       i686: 'bf5940ad975c45a48914ac09ba0677317555154bc153fda751ccce569c135ee8',
+     x86_64: '1a9ae024122fdc0df2e2973ff78e9ddc7c78d0cea43c9e29ffe48732ef5a5bb7'
   })
 
   depends_on 'python3' => :build
-
-  def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
-
-  def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
-  end
 end
