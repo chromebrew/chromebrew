21349f06d68479f021078da4a898f35f19e1fbeb
Satadru Pramanik <satadru@gmail.com>
2023-10-05 09:14:29 -0400

    rebuild py3_build

--- packages/py3_build.rb
+++ packages/py3_build.rb
@@ -4,27 +4,28 @@ class Py3_build < Package
   description 'Python build is a simple, correct PEP 517 build frontend.'
   homepage 'https://pypa-build.readthedocs.io/'
   @_ver = '0.9.0'
-  version "#{@_ver}-py3.11"
+  version "#{@_ver}-1-py3.11"
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/pypa/build.git'
   git_hashtag @_ver
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-py3.11_armv7l/py3_build-0.9.0-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-py3.11_armv7l/py3_build-0.9.0-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-py3.11_i686/py3_build-0.9.0-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-py3.11_x86_64/py3_build-0.9.0-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-1-py3.11_armv7l/py3_build-0.9.0-1-py3.11-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-1-py3.11_armv7l/py3_build-0.9.0-1-py3.11-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-1-py3.11_i686/py3_build-0.9.0-1-py3.11-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_build/0.9.0-1-py3.11_x86_64/py3_build-0.9.0-1-py3.11-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '9b0448006c3e4f6453bfcae30472f50a4e68848d6ae322dae9b5335c4274cb71',
-     armv7l: '9b0448006c3e4f6453bfcae30472f50a4e68848d6ae322dae9b5335c4274cb71',
-       i686: '9048410e94aef4feb6950fa7574c0082c1c39a030aa79bd99ae731a58c01c1df',
-     x86_64: 'd98e8bf6a856a84306eb4c29a6858307625cbc0f4eaa4a4344e5d9506406e5e3'
+    aarch64: 'a9c9fcf17177e0cacf41eb187891ca499122eacd8324ddeb65ce9f71647116bf',
+     armv7l: 'a9c9fcf17177e0cacf41eb187891ca499122eacd8324ddeb65ce9f71647116bf',
+       i686: 'fdc59807e7f84c345889f3afb67a1f452950d8bd873323b3b9f3f1ec329931d6',
+     x86_64: '021cc30ea7a0f270d330f8c08d1f70024be19349d874aff6541c88c332edd847'
   })
 
   depends_on 'python3'
   depends_on 'py3_packaging'
+  depends_on 'py3_pep517' => :build
   depends_on 'py3_pyproject_hooks'
   depends_on 'py3_tomli'
 
