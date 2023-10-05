9c5a128d5915166c9249a19eee617e5ed4ec00fc
Satadru Pramanik <satadru@gmail.com>
2023-10-04 20:41:32 -0400

    update py3_pytz

--- packages/py3_pytz.rb
+++ packages/py3_pytz.rb
@@ -1,35 +1,26 @@
-require 'package'
+require 'buildsystems/pip'
 
-class Py3_pytz < Package
+class Py3_pytz < Pip
   description 'pytz brings the Olson tz database into Python.'
   homepage 'https://pythonhosted.org/pytz/'
-  @_ver = '2021.3'
-  version "#{@_ver}-py3.11"
+  @_ver = '2023.3.post1'
+  version "#{@_ver}-py3.12"
   license 'MIT'
   compatibility 'all'
-  source_url 'https://files.pythonhosted.org/packages/e3/8e/1cde9d002f48a940b9d9d38820aaf444b229450c0854bdf15305ce4a3d1a/pytz-2021.3.tar.gz'
-  source_sha256 'acad2d8b20a1af07d4e4c9d2e9285c5ed9104354062f275f3fcd88dcef4f1326'
+  source_url 'SKIP'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2021.3-py3.11_armv7l/py3_pytz-2021.3-py3.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2021.3-py3.11_armv7l/py3_pytz-2021.3-py3.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2021.3-py3.11_i686/py3_pytz-2021.3-py3.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2021.3-py3.11_x86_64/py3_pytz-2021.3-py3.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2023.3.post1-py3.12_armv7l/py3_pytz-2023.3.post1-py3.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2023.3.post1-py3.12_armv7l/py3_pytz-2023.3.post1-py3.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2023.3.post1-py3.12_i686/py3_pytz-2023.3.post1-py3.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_pytz/2023.3.post1-py3.12_x86_64/py3_pytz-2023.3.post1-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '17f1e90d1c7af248b5bce82030551e245ed7d467d8f1a145549708b6c2022225',
-     armv7l: '17f1e90d1c7af248b5bce82030551e245ed7d467d8f1a145549708b6c2022225',
-       i686: 'aff25a04682350a37c6074a875a8e049f39dbbb9c87978bc0920a2e26138ead4',
-     x86_64: '07ca710fd0b7f336691b89343e65b820424f2e9d457b4d975deb49d5f7113052'
+    aarch64: '2abf8c4ba0c64ee5182a52097af744c00f8beec18fefa2ad70f28a66b1025628',
+     armv7l: '2abf8c4ba0c64ee5182a52097af744c00f8beec18fefa2ad70f28a66b1025628',
+       i686: '2d59ea0aff59fc0376da4f1cdda1e385475b373bd38cd2650a8e58c632460fa3',
+     x86_64: '16fd6926904599b119f9ec14268b31af02fc898cc111404a26aaf127658ffcb3'
   })
 
   depends_on 'python3' => :build
-
-  def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
-
-  def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
-  end
 end
