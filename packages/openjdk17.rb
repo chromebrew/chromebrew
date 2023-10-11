ba8ed9fe4a4edfd3bcd4a156cc54d1f95507a310
Ed Reel <edreel@gmail.com>
2023-10-11 13:00:54 -0500

    Openjdk17 17.0.8 => 17.0.8.1 (#8769)

--- packages/openjdk17.rb
+++ packages/openjdk17.rb
@@ -3,20 +3,21 @@ require 'package'
 class Openjdk17 < Package
   description 'The JDK is a development environment for building applications, applets, and components using the Java programming language.'
   homepage 'https://openjdk.org/'
-  version '17.0.8'
+  version '17.0.8.1'
   license 'GPL-2'
   compatibility 'all'
+  # Visit https://www.azul.com/downloads/?package=jdk#zulu to download the binaries.
   source_url({
-    aarch64: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.44.15-ca-jdk17.0.8-linux_aarch32hf.tar.gz',
-     armv7l: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.44.15-ca-jdk17.0.8-linux_aarch32hf.tar.gz',
-       i686: 'https://cdn.azul.com/zulu/bin/zulu17.44.15-ca-jdk17.0.8-linux_i686.tar.gz',
-     x86_64: 'https://cdn.azul.com/zulu/bin/zulu17.44.15-ca-jdk17.0.8-linux_x64.tar.gz'
+    aarch64: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.44.53-ca-jdk17.0.8.1-linux_aarch32hf.tar.gz',
+     armv7l: 'https://cdn.azul.com/zulu-embedded/bin/zulu17.44.53-ca-jdk17.0.8.1-linux_aarch32hf.tar.gz',
+       i686: 'https://cdn.azul.com/zulu/bin/zulu17.44.53-ca-jdk17.0.8.1-linux_i686.tar.gz',
+     x86_64: 'https://cdn.azul.com/zulu/bin/zulu17.44.53-ca-jdk17.0.8.1-linux_x64.tar.gz'
   })
   source_sha256({
-    aarch64: '4feb1f6cba26c66160e03284b876071ee9d27954df2e2be51def935afc30d8d9',
-     armv7l: '4feb1f6cba26c66160e03284b876071ee9d27954df2e2be51def935afc30d8d9',
-       i686: 'bc84c25f53f6961c5649f1fd363e944d48113ead07020ba90a2b3d53cea1b5eb',
-     x86_64: '423d1f40e0cbe873f357cebb95b19a59503948ee3c91639ecbc8e64df6a6f5fc'
+    aarch64: 'c5aab3b2b80527247846bc15c4eb3cb826619ce28b4b052cfb6e0d45b2f884a3',
+     armv7l: 'c5aab3b2b80527247846bc15c4eb3cb826619ce28b4b052cfb6e0d45b2f884a3',
+       i686: '987b821c995867f27024da363f5cb9a52d860100f27f838187dd47c1824d1141',
+     x86_64: 'b9482f2304a1a68a614dfacddcf29569a72f0fac32e6c74f83dc1b9a157b8340'
   })
 
   no_compile_needed
@@ -39,11 +40,23 @@ class Openjdk17 < Package
   def self.install
     FileUtils.mkdir_p CREW_DEST_MAN_PREFIX
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/openjdk17"
-    FileUtils.mv 'bin/', CREW_DEST_PREFIX
-    FileUtils.mv 'conf/', "#{CREW_DEST_PREFIX}/share/openjdk17"
-    FileUtils.mv 'jmods/', "#{CREW_DEST_PREFIX}/share/openjdk17"
-    FileUtils.mv 'include/', CREW_DEST_PREFIX
-    FileUtils.mv 'lib/', CREW_DEST_PREFIX
-    FileUtils.mv Dir['man/*'], CREW_DEST_MAN_PREFIX
+    FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/openjdk17/"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    Dir["#{CREW_DEST_PREFIX}/share/openjdk17/bin/*"].each do |binfile|
+      @basename = File.basename(binfile)
+      FileUtils.ln_s "#{CREW_PREFIX}/share/openjdk17/bin/#{@basename}", "#{CREW_DEST_PREFIX}/bin/#{@basename}"
+    end
+    FileUtils.mv Dir["#{CREW_DEST_PREFIX}/share/openjdk17/man/*"], CREW_DEST_MAN_PREFIX
+    # Add environment variable.
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
+    javaenv = <<~EOF
+      # Java configuration
+      JAVA_HOME=#{CREW_PREFIX}
+    EOF
+    File.write("#{CREW_DEST_PREFIX}/etc/env.d/10-openjdk17", javaenv)
+  end
+
+  def self.postinstall
+    puts "\nType 'source ~/.bashrc' to finish the installation.\n".lightblue
   end
 end
