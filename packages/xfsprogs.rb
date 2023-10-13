09060413512271410cca7e9714a1775813b483e4
Ed Reel <edreel@gmail.com>
2023-10-13 13:33:52 -0500

    Xfsprogs 6.4.0 => 6.5.0 (#8777)

--- packages/xfsprogs.rb
+++ packages/xfsprogs.rb
@@ -3,23 +3,23 @@ require 'package'
 class Xfsprogs < Package
   description 'XFS filesystem utilities'
   homepage 'https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git/'
-  version '6.4.0'
+  version '6.5.0'
   license 'LGPL-2.1'
   compatibility 'all'
   source_url 'https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git'
   git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.4.0_armv7l/xfsprogs-6.4.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.4.0_armv7l/xfsprogs-6.4.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.4.0_i686/xfsprogs-6.4.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.4.0_x86_64/xfsprogs-6.4.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.5.0_armv7l/xfsprogs-6.5.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.5.0_armv7l/xfsprogs-6.5.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.5.0_i686/xfsprogs-6.5.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xfsprogs/6.5.0_x86_64/xfsprogs-6.5.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ffab65dd189d4b258f87241d6e4708767b1af11379238da165f10c07a74e5409',
-     armv7l: 'ffab65dd189d4b258f87241d6e4708767b1af11379238da165f10c07a74e5409',
-       i686: '480f11957cd44fdbd191c266a4fba5161b3c314428ec4494dcc033aff47c60f1',
-     x86_64: '44b56d9763e64846fdf65660813e0cf0d6cab677b1f21b09abc873f26a261f7f'
+    aarch64: '11346e490774fd09e12b234d71a487ed3ac3686d57814b0ef90cfb33d0b6c145',
+     armv7l: '11346e490774fd09e12b234d71a487ed3ac3686d57814b0ef90cfb33d0b6c145',
+       i686: '8dc50a04c13a40f2d6bc663af487b3003bc27f0800be2d39847658f18894b66c',
+     x86_64: '9c6c7e554f35bb08a13e2d1ff31b032f6da0080238f1db00c2871584e6b80c37'
   })
 
   depends_on 'gcc_lib' # R
