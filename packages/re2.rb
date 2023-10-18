b00f674cf20dccdaf9116f5b7838036e8e0e20db
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-10-18 15:12:08 -0400

    update abseil, re2 (#8797)

--- packages/re2.rb
+++ packages/re2.rb
@@ -1,34 +1,30 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Re2 < Package
+class Re2 < CMake
   description 'RE2 is a fast, safe, thread-friendly alternative to backtracking regular expression engines like those used in PCRE, Perl, and Python.'
   homepage 'https://github.com/google/re2/'
-  version '2021-02-02'
+  version '2023-09-01'
   license 'BSD'
   compatibility 'all'
-
   source_url "https://github.com/google/re2/archive/#{version}.tar.gz"
-  source_sha256 '1396ab50c06c1a8885fb68bf49a5ecfd989163015fd96699a180d6414937f33f'
+  source_sha256 '5bb6875ae1cd1e9fedde98018c346db7260655f86fdb8837e3075103acd3649b'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2021-02-02_armv7l/re2-2021-02-02-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2021-02-02_armv7l/re2-2021-02-02-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2021-02-02_i686/re2-2021-02-02-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2021-02-02_x86_64/re2-2021-02-02-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2023-09-01_armv7l/re2-2023-09-01-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2023-09-01_armv7l/re2-2023-09-01-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2023-09-01_i686/re2-2023-09-01-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/re2/2023-09-01_x86_64/re2-2023-09-01-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ae5d5606c670ae8b7a1d6faae048683e2a68a49594e3955ba422924d0e236b6a',
-     armv7l: 'ae5d5606c670ae8b7a1d6faae048683e2a68a49594e3955ba422924d0e236b6a',
-       i686: '0405d43ceba95bc74cd1d301d7fc32441599f22686fcf05850376fb32044bfff',
-     x86_64: '669255b395f866889afadd99b31c34a94f7c6ede80e1292a42c6126cedf90315'
+    aarch64: '2115d0d431c3d4c511a90c91b315cf7ba23f0a148fa9120d5f40a213c9ab5d79',
+     armv7l: '2115d0d431c3d4c511a90c91b315cf7ba23f0a148fa9120d5f40a213c9ab5d79',
+       i686: '2c0aff02b699216184d0370703fb9effcfce0a225541525ddab5aee942e2c585',
+     x86_64: '14368932ce32c61012519b7bac70d4c9ba75a2bdcb33b33b277458b671197032'
   })
 
-  def self.build
-    # cmake version doesn't provide libre2 library, use make instead
-    system 'make'
-  end
+  depends_on 'abseil_cpp' # R
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
 
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} #{CREW_OPTIONS.gsub('--', '')} install"
-  end
+  cmake_options '-DBUILD_SHARED_LIBS=ON'
 end
