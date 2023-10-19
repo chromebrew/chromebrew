fe168ec121124fa5e6b16fe0cca0b7b3dd7226cc
Satadru Pramanik <satadru@gmail.com>
2023-10-19 13:21:30 -0400

    add binaries for qt5_declarative

--- packages/qt5_declarative.rb
+++ packages/qt5_declarative.rb
@@ -5,12 +5,16 @@ class Qt5_declarative < Package
   homepage 'https://www.qt.io/'
   version '5.15.11-1b0e366'
   license 'FDL, GPL-2, GPL-3, GPL-3-with-qt-exception and LGPL-3'
-  compatibility 'x86_64'
+  compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://invent.kde.org/qt/qt/qtdeclarative.git'
   git_hashtag '1b0e366092bcfae0392592c3b7891f0e47af1018' # from kde/5.15 branch
 
-  binary_url({})
-  binary_sha256({})
+  binary_url({
+    x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qt5_declarative/5.15.11-1b0e366_x86_64/qt5_declarative-5.15.11-1b0e366-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    x86_64: 'efb6a6eb5bce1b11dab4d35c5a8855efef2d432b06f24e97fce7e697654add90'
+  })
 
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
@@ -19,7 +23,7 @@ class Qt5_declarative < Package
 
   def self.build
     system 'qmake'
-    system 'make || make'
+    system 'make'
   end
 
   def self.install
