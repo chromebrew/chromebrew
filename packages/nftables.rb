b2ef044a17d0a7123471e6e9518f406eefacb0a1
Ed Reel <edreel@gmail.com>
2023-10-19 12:06:28 -0500

    Nftables 1.0.8 => 1.0.9 (#8805)

--- packages/nftables.rb
+++ packages/nftables.rb
@@ -1,26 +1,26 @@
 # Adapted from Arch Linux nftables PKGBUILD at:
 # https://github.com/archlinux/svntogit-packages/raw/packages/nftables/trunk/PKGBUILD
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Nftables < Package
+class Nftables < Autotools
   description 'Netfilter tables userspace tools'
   homepage 'https://netfilter.org/projects/nftables/'
-  version '1.0.8'
+  version '1.0.9'
   license 'GPL2'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://netfilter.org/projects/nftables/files/nftables-1.0.8.tar.xz'
-  source_sha256 '9373740de41a82dbc98818e0a46a073faeb8a8d0689fa4fa1a74399c32bf3d50'
+  source_url 'https://netfilter.org/projects/nftables/files/nftables-1.0.9.tar.xz'
+  source_sha256 'a3c304cd9ba061239ee0474f9afb938a9bb99d89b960246f66f0c3a0a85e14cd'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.8_armv7l/nftables-1.0.8-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.8_armv7l/nftables-1.0.8-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.8_x86_64/nftables-1.0.8-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.9_armv7l/nftables-1.0.9-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.9_armv7l/nftables-1.0.9-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nftables/1.0.9_x86_64/nftables-1.0.9-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b1151ae07c75b41f9263acd4c0197c7ec0dd6bf3666d4f214aef90cdd1af23e1',
-     armv7l: 'b1151ae07c75b41f9263acd4c0197c7ec0dd6bf3666d4f214aef90cdd1af23e1',
-     x86_64: '87087b6ac01cd72c6293701453b8bae99795bbcf68cfd874de91936363048e40'
+    aarch64: 'bbed65ebad95e0ee11ec0ab2a1b17ecd8e33140505441f9c2d95a86ef360225b',
+     armv7l: 'bbed65ebad95e0ee11ec0ab2a1b17ecd8e33140505441f9c2d95a86ef360225b',
+     x86_64: '370e9eb2fe2c1bf7ee577623e8c8aa29d659336eb17ebbe3217c9b9379b5464e'
   })
 
   depends_on 'asciidoc' => :build
@@ -30,20 +30,11 @@ class Nftables < Package
   depends_on 'libnftnl' # R
   depends_on 'readline' # R
 
-  def self.build
-    system 'autoreconf -fiv'
-    system "./configure \
-           --prefix=#{CREW_PREFIX} \
-           --libdir=#{CREW_LIB_PREFIX} \
-           --sysconfdir=#{CREW_PREFIX}/etc \
-           --with-json \
-           --with-cli=readline \
-           --with-mini-gmp \
-           --disable-debug"
-    system 'make'
-  end
+  configure_options "--sysconfdir=#{CREW_PREFIX}/etc \
+    --with-json \
+    --with-cli=readline \
+    --with-mini-gmp \
+    --disable-debug"
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  run_tests
 end
