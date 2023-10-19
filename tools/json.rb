3e46dddef53afa91687b57d0afd77bcd99c92a58
Zopolis4 <creatorsmithmdt@gmail.com>
2023-10-19 21:08:25 +1100

    Minor improvements to Repology workflow and backend

--- tools/json.rb
+++ tools/json.rb
@@ -10,7 +10,8 @@ output = Array.new
 
 Dir.glob('../packages/*.rb').each do |filename|
   pkg = Package.load_package(filename)
-  output << {name: File.basename(filename, '.rb'), description: pkg.description, homepage: pkg.homepage, version: pkg.version, license: pkg.license, compatibility: pkg.compatibility}
+  next if pkg.is_fake?
+  output << {name: File.basename(filename, '.rb').gsub("_","-"), description: pkg.description, homepage: pkg.homepage, version: pkg.version, license: pkg.license, compatibility: pkg.compatibility}
 end
 
 File.write('repology.json', JSON.generate(output))
