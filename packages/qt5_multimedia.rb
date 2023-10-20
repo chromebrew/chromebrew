31b59c6bdde36c5b61fe9e531949ae564fd2764c
Satadru Pramanik <satadru@gmail.com>
2023-10-20 17:11:01 -0400

    add more qt5 binaries

--- packages/qt5_multimedia.rb
+++ packages/qt5_multimedia.rb
@@ -9,8 +9,16 @@ class Qt5_multimedia < Package
   source_url 'https://invent.kde.org/qt/qt/qtmultimedia.git'
   git_hashtag 'f587b18db4abd68cb6d4d77fbcec1b94c38d2a51' # from kde/5.15 branch
 
-  binary_url({})
-  binary_sha256({})
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qt5_multimedia/5.15.11-f587b18_armv7l/qt5_multimedia-5.15.11-f587b18-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qt5_multimedia/5.15.11-f587b18_armv7l/qt5_multimedia-5.15.11-f587b18-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qt5_multimedia/5.15.11-f587b18_x86_64/qt5_multimedia-5.15.11-f587b18-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    aarch64: '682f5598df01442a3bde7ff5324bc3d43d1742e18cd24564fd4f26f51ee86a85',
+     armv7l: '682f5598df01442a3bde7ff5324bc3d43d1742e18cd24564fd4f26f51ee86a85',
+     x86_64: 'a2e7a9911878a22ed43e58840366b9300a69f264bb98253c34d5f8a33841dce2'
+  })
 
   depends_on 'alsa_lib' # R
   depends_on 'gcc_lib' # R
--- packages/qt5_serialport.rb
+++ packages/qt5_serialport.rb
@@ -9,10 +9,21 @@ class Qt5_serialport < Package
   source_url 'https://invent.kde.org/qt/qt/qtserialport.git'
   git_hashtag '3380465d5d4977326616c5e57789a81681be650e' # from kde/5.15 branch
 
-  binary_url({})
-  binary_sha256({})
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qt5_serialport/5.15.11-3380465_armv7l/qt5_serialport-5.15.11-3380465-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qt5_serialport/5.15.11-3380465_armv7l/qt5_serialport-5.15.11-3380465-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qt5_serialport/5.15.11-3380465_x86_64/qt5_serialport-5.15.11-3380465-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    aarch64: 'eb2c1d0a8d43064df1d9fcde90fa5391a4623b6ccd6c59eac009ddc951af6d9e',
+     armv7l: 'eb2c1d0a8d43064df1d9fcde90fa5391a4623b6ccd6c59eac009ddc951af6d9e',
+     x86_64: 'f0044c04788d6de615d606a812a133b9ed7e6e7361ae1e8a3ddbce7a066e7216'
+  })
 
-  depends_on 'qt5_base'
+  depends_on 'eudev' # R
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'qt5_base' # R
 
   def self.build
     system 'qmake'
