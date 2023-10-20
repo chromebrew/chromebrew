43297ab1b2552483844e660a271af3b5f0f47d52
Ed Reel <edreel@gmail.com>
2023-10-20 14:17:10 -0500

    Texinfo 7.0.3-perl5.38 => 7.1-perl5.38

--- packages/texinfo.rb
+++ packages/texinfo.rb
@@ -1,25 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Texinfo < Package
+class Texinfo < Autotools
   description 'Texinfo is the official documentation format of the GNU project.'
   homepage 'https://www.gnu.org/software/texinfo/'
-  version '7.0.3-perl5.38'
+  version '7.1-perl5.38'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/texinfo/texinfo-7.0.3.tar.xz'
-  source_sha256 '74b420d09d7f528e84f97aa330f0dd69a98a6053e7a4e01767eed115038807bf'
+  source_url 'https://ftpmirror.gnu.org/texinfo/texinfo-7.1.tar.xz'
+  source_sha256 'deeec9f19f159e046fdf8ad22231981806dac332cc372f1c763504ad82b30953'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0.3-perl5.38_armv7l/texinfo-7.0.3-perl5.38-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0.3-perl5.38_armv7l/texinfo-7.0.3-perl5.38-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0.3-perl5.38_i686/texinfo-7.0.3-perl5.38-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.0.3-perl5.38_x86_64/texinfo-7.0.3-perl5.38-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.1-perl5.38_armv7l/texinfo-7.1-perl5.38-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.1-perl5.38_armv7l/texinfo-7.1-perl5.38-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.1-perl5.38_i686/texinfo-7.1-perl5.38-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/texinfo/7.1-perl5.38_x86_64/texinfo-7.1-perl5.38-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '1a335d6c6c27b1427c8b373f72f577bac1c26087575e3c82040f331249648672',
-     armv7l: '1a335d6c6c27b1427c8b373f72f577bac1c26087575e3c82040f331249648672',
-       i686: '89d51b99fb05f96fc867ba354d4b872c49b5d17b321eb4c49b474a86439c421f',
-     x86_64: 'aea090d1b6511ec19e85b9e258c3d63eb361a43755f9b9886a4bf95f8fe4e062'
+    aarch64: '1993d33be6ade8fc000d23e2b5e0b91bb41da63916e7ceb31698c536bbf3f3f9',
+     armv7l: '1993d33be6ade8fc000d23e2b5e0b91bb41da63916e7ceb31698c536bbf3f3f9',
+       i686: '63815ca8dbeb9708a7e2bf1c4e611d726b567832c54f1946bbd7d2275b972066',
+     x86_64: '45efc72577b1c1a40a91d6a665085dcd6eb9c32a9e023c6d9ca7d5439b0cce96'
   })
 
   depends_on 'glibc_lib' # R
@@ -30,18 +30,8 @@ class Texinfo < Package
   depends_on 'perl_text_unidecode' # L
   depends_on 'perl_unicode_eastasianwidth' # L
 
-  def self.build
-    system "mold -run ./configure #{CREW_OPTIONS} \
-      --with-external-Text-Unidecode \
-      --with-external-Unicode-EastAsianWidth"
-    system 'make'
-  end
+  configure_options '--with-external-Text-Unidecode \
+    --with-external-Unicode-EastAsianWidth'
 
-  def self.check
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  run_tests
 end
