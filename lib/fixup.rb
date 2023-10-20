97268ff85d5ddc4b259282ab566718246489f78f
Satadru Pramanik <satadru@gmail.com>
2023-10-19 20:56:34 -0400

    add more verbose comment

--- lib/fixup.rb
+++ lib/fixup.rb
@@ -5,6 +5,7 @@ FileUtils.rm_rf "#{HOME}/.cache/crewcache/manifest"
 
 # Fix missing $PATH not added in install.sh
 unless system "grep -q '$PATH' #{CREW_PREFIX}/etc/env.d/path"
+  puts 'Fixing path env.d file...'.orange
   File.write "#{CREW_PREFIX}/etc/env.d/path", <<~ENVD_PATH_EOF
     ## Inserted by Chromebrew version #{CREW_VERSION}
     PATH=#{CREW_PREFIX}/bin:#{CREW_PREFIX}/sbin:#{CREW_PREFIX}/share/musl/bin:\\\$PATH
