efc48b385449b0d5eb9285942870322a65b328ea
Satadru Pramanik <satadru@gmail.com>
2023-10-23 15:54:15 -0400

    eudev => 3.2.14

--- packages/eudev.rb
+++ packages/eudev.rb
@@ -1,25 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Eudev < Package
+class Eudev < Autotools
   description 'Gentoo standalone udev'
   homepage 'https://wiki.gentoo.org/wiki/Project:Eudev'
-  version '3.2.11'
+  version '3.2.14'
   license 'LGPL-2.1, MIT and GPL-2'
   compatibility 'all'
   source_url 'https://github.com/gentoo/eudev.git'
   git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.11_armv7l/eudev-3.2.11-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.11_armv7l/eudev-3.2.11-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.11_i686/eudev-3.2.11-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.11_x86_64/eudev-3.2.11-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.14_armv7l/eudev-3.2.14-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.14_armv7l/eudev-3.2.14-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.14_i686/eudev-3.2.14-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/eudev/3.2.14_x86_64/eudev-3.2.14-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '23041d6ccc617f40fe78e3de297295366f0d4c5fbc8091a2656256f5a7f51c1a',
-     armv7l: '23041d6ccc617f40fe78e3de297295366f0d4c5fbc8091a2656256f5a7f51c1a',
-       i686: '7c3412817cb76d6277a2cbd16314192fd55e1e230ab02caa76743c591012f1ee',
-     x86_64: 'b7c99add75188caaa8cd4f12ebcb06b2e575e2c8f2a8103405aa4b27107d308e'
+    aarch64: 'f533cfaf65406ef7e16ddbdef10a0cb60068da3fb875874d1957811b706c9a86',
+     armv7l: 'f533cfaf65406ef7e16ddbdef10a0cb60068da3fb875874d1957811b706c9a86',
+       i686: '5667ea76c8eb854037e62d8cb06f59e1e0e318c88777849e2ec1ec2ce4c92526',
+     x86_64: 'eaba1d39e5bf6fa886ef72e5d1aacf3236a76476c647da0e6587162bf0fe41f6'
   })
 
   depends_on 'acl' # R
@@ -33,16 +33,6 @@ class Eudev < Package
     system 'sed -i s,/usr/bin/xsltproc,xsltproc,g man/make.sh'
   end
 
-  def self.build
-    system 'NOCONFIGURE=1 ./autogen.sh'
-    system "./configure \
-              #{CREW_OPTIONS} \
-              --enable-hwdb \
-              --enable-rule-generator"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options '--enable-hwdb \
+              --enable-rule-generator'
 end
