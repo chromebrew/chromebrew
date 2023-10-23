0c28fc91ad1fb9f7b69685133ddebe786ff1e0c0
Satadru Pramanik <satadru@gmail.com>
2023-10-23 12:36:14 -0400

    gmp => 6.3.0

--- packages/gmp.rb
+++ packages/gmp.rb
@@ -1,46 +1,30 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Gmp < Package
+class Gmp < Autotools
   description 'GMP is a free library for arbitrary precision arithmetic, operating on signed integers, rational numbers, and floating-point numbers.'
   homepage 'https://gmplib.org/'
-  version '6.2.1' # Do not use @_ver here, it will break the installer.
+  version '6.3.0' # Do not use @_ver here, it will break the installer.
   license 'LGPL-3+ and GPL-2+'
   compatibility 'all'
-  source_url 'https://gmplib.org/download/gmp/gmp-6.2.1.tar.lz'
-  source_sha256 '2c7f4f0d370801b2849c48c9ef3f59553b5f1d3791d070cffb04599f9fc67b41'
+  source_url 'https://gmplib.org/download/gmp/gmp-6.3.0.tar.zst'
+  source_sha256 '7f55d73f472c29cc14d7fa985f3654d48829415b649eaf61646b7b33f2a80e27'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.2.1_armv7l/gmp-6.2.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.2.1_armv7l/gmp-6.2.1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.2.1_i686/gmp-6.2.1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.2.1_x86_64/gmp-6.2.1-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.3.0_armv7l/gmp-6.3.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.3.0_armv7l/gmp-6.3.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.3.0_i686/gmp-6.3.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/gmp/6.3.0_x86_64/gmp-6.3.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'c9a07f1b3654f087dff322a5a0d5cbe98fba1e7c5b483621702655a0c879c152',
-     armv7l: 'c9a07f1b3654f087dff322a5a0d5cbe98fba1e7c5b483621702655a0c879c152',
-       i686: '2428a10b21a6b6bfaca1d2bd85b5c2adcd05aa3f48b6e9712d96ff84fa525478',
-     x86_64: '50847bd14c11de841c7c74696e2ff4503253bbaacd38e9ffb31814d6ccfb76f3'
+    aarch64: '32a4e78e10e6d20a42764a205e2334001d4bfec896dccac705313da8cd327fdf',
+     armv7l: '32a4e78e10e6d20a42764a205e2334001d4bfec896dccac705313da8cd327fdf',
+       i686: 'd165a4a8b2cdd49592ae3b88ff2709f1e6ce99f227f6fefcba86c1563f9e6ad4',
+     x86_64: 'd4240bcc61b5901ab86c15b28e320c72ae470bfc20d415471d20cb90b2456753'
   })
 
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
-  no_patchelf
 
-  def self.build
-    system 'filefix'
-    system "./configure \
-      #{CREW_OPTIONS} \
-      --disable-maintainer-mode \
-      --enable-cxx"
-    system 'make'
-  end
-
-  def self.check
-    # Run tests on armv7l after install
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options '--disable-maintainer-mode \
+      --enable-cxx'
 end
