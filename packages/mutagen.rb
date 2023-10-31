042b7b45501aed508b1d114d0650369e70d7700f
Ed Reel <edreel@gmail.com>
2023-10-31 01:49:27 -0500

    Mutagen 0.15.1 => 0.17.2

--- packages/mutagen.rb
+++ packages/mutagen.rb
@@ -3,20 +3,20 @@ require 'package'
 class Mutagen < Package
   description 'Mutagen provides real-time file synchronization and flexible network forwarding, extending the reach of your existing development tools to cloud-based containers and infrastructure.'
   homepage 'https://mutagen.io/'
-  version '0.15.1'
+  version '0.17.2'
   license 'GPL-2'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/mutagen-io/mutagen/releases/download/v0.15.1/mutagen_linux_arm_v0.15.1.tar.gz',
-     armv7l: 'https://github.com/mutagen-io/mutagen/releases/download/v0.15.1/mutagen_linux_arm_v0.15.1.tar.gz',
-       i686: 'https://github.com/mutagen-io/mutagen/releases/download/v0.15.1/mutagen_linux_386_v0.15.1.tar.gz',
-     x86_64: 'https://github.com/mutagen-io/mutagen/releases/download/v0.15.1/mutagen_linux_amd64_v0.15.1.tar.gz'
+    aarch64: 'https://github.com/mutagen-io/mutagen/releases/download/v0.17.2/mutagen_linux_arm_v0.17.2.tar.gz',
+     armv7l: 'https://github.com/mutagen-io/mutagen/releases/download/v0.17.2/mutagen_linux_arm_v0.17.2.tar.gz',
+       i686: 'https://github.com/mutagen-io/mutagen/releases/download/v0.17.2/mutagen_linux_386_v0.17.2.tar.gz',
+     x86_64: 'https://github.com/mutagen-io/mutagen/releases/download/v0.17.2/mutagen_linux_amd64_v0.17.2.tar.gz'
   })
   source_sha256({
-    aarch64: '74e5dd5aba9129e41b788a4cca46b102f61bef11a22f63a3b7c7e73c85b8d8bb',
-     armv7l: '74e5dd5aba9129e41b788a4cca46b102f61bef11a22f63a3b7c7e73c85b8d8bb',
-       i686: 'f02be5655607541fd264d5678a7ae8e6f34c6de05b4f24c3b20bf2945c11cbc4',
-     x86_64: 'e88afa1657291697245f54abab4eda84531e785bb945e2d92dfc2aab830f7120'
+    aarch64: 'fc6475d5c0a275491a7a5228d03b41b7fb2a46dc054395c4b8003af9bd49e3a1',
+     armv7l: 'fc6475d5c0a275491a7a5228d03b41b7fb2a46dc054395c4b8003af9bd49e3a1',
+       i686: 'e9657d23a2b1ea7cb753fdbbb5c82063efceec1619bd516f4014018aa1e96bd6',
+     x86_64: '85d967462ade994d37fc8d4906579d33b6fbb1744bab4a7cceb0c12424fb0f1d'
   })
 
   def self.install
@@ -29,7 +29,6 @@ class Mutagen < Package
       arch = 'amd64'
     end
     system "tar xvf mutagen-agents.tar.gz linux_#{arch}"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'mutagen', "#{CREW_DEST_PREFIX}/bin/mutagen", mode: 0o755
     FileUtils.install "linux_#{arch}", "#{CREW_DEST_PREFIX}/bin/mutagen-agent", mode: 0o755
   end
