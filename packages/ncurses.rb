df9878cf11b4ae8cbafe238e9063bd6be817d740
Satadru Pramanik <satadru@gmail.com>
2023-10-31 11:17:27 -0400

    rebuild again

--- packages/ncurses.rb
+++ packages/ncurses.rb
@@ -19,10 +19,10 @@ class Ncurses < Package
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/ncurses/6_4_20231028_x86_64/ncurses-6_4_20231028-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '56e665459d09ab3cb87d301d81e6d3f3909fc5e3b4b2963bae513cc25bcdd2f4',
-     armv7l: '56e665459d09ab3cb87d301d81e6d3f3909fc5e3b4b2963bae513cc25bcdd2f4',
-       i686: 'de430fd09e0b7567f9a45524dd9b461cce4d92f9916047501278b9c1be4b79ba',
-     x86_64: '943d53afd17826534d2447c7a52c1dd8a1b30afcfbe3613a7689a5775d457b39'
+    aarch64: '3835d468d96412d0641f317c15856269d01a2cc9bc92704d014e418f2de35dc7',
+     armv7l: '3835d468d96412d0641f317c15856269d01a2cc9bc92704d014e418f2de35dc7',
+       i686: 'eec49654265f5e37a95f7f252f8b8dc73b40834935e6f212de30d1cdab63ce6d',
+     x86_64: 'ec3b1a804dd73c7177e649712cf3932ffadc993d92f191db47ec1e67a1ac7284'
   })
 
   depends_on 'gcc_lib' # R
@@ -75,6 +75,8 @@ class Ncurses < Package
       FileUtils.install 'lib/libncursesw.so', "#{CREW_DEST_LIB_PREFIX}/lib#{lib}.so", mode: 0o755
       FileUtils.install 'misc/ncursesw.pc', "#{CREW_DEST_LIB_PREFIX}/pkgconfig/#{lib}.pc", mode: 0o644
     end
+    FileUtils.ln_sf "#{CREW_PREFIX}/bin/ncursesw6-config", "#{CREW_DEST_PREFIX}/bin/ncurses6-config"
+    FileUtils.ln_sf "#{CREW_PREFIX}/include/ncursesw", "#{CREW_DEST_PREFIX}/include/ncurses"
     # Remove conflicts with dvtm package.
     Dir.chdir "#{CREW_DEST_PREFIX}/share/terminfo/d" do
       FileUtils.rm %w[dvtm dvtm-256color]
