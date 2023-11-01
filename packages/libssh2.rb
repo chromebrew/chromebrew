9c4a1a33cfd5f7ef7617c560309f78a836fc46c9
Satadru Pramanik <satadru@gmail.com>
2023-11-01 16:20:09 -0400

    update libssh2

--- packages/libssh2.rb
+++ packages/libssh2.rb
@@ -1,38 +1,30 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libssh2 < Package
+class Libssh2 < Autotools
   description 'libssh2 is a client-side C library implementing the SSH2 protocol.'
   homepage 'https://www.libssh2.org/'
-  version '1.10.0'
-  compatibility 'all'
+  version '1.11.0'
   license 'BSD'
-  source_url 'https://www.libssh2.org/download/libssh2-1.10.0.tar.gz'
-  source_sha256 '2d64e90f3ded394b91d3a2e774ca203a4179f69aebee03003e5a6fa621e41d51'
+  compatibility 'all'
+  source_url 'https://www.libssh2.org/download/libssh2-1.11.0.tar.gz'
+  source_sha256 '3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.10.0_armv7l/libssh2-1.10.0-chromeos-armv7l.tpxz',
-      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.10.0_armv7l/libssh2-1.10.0-chromeos-armv7l.tpxz',
-        i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.10.0_i686/libssh2-1.10.0-chromeos-i686.tpxz',
-      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.10.0_x86_64/libssh2-1.10.0-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.11.0_armv7l/libssh2-1.11.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.11.0_armv7l/libssh2-1.11.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.11.0_i686/libssh2-1.11.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libssh2/1.11.0_x86_64/libssh2-1.11.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'c31d6ee0243ce8b131d559c996a4d5d856a9ad4dfe412eb3b99af364d71157f5',
-      armv7l: 'c31d6ee0243ce8b131d559c996a4d5d856a9ad4dfe412eb3b99af364d71157f5',
-        i686: '6b65af8a14f9d2dfdcd8ee4006bd1c9ae35932108f0d9536394ce668c870cc30',
-      x86_64: 'fc86a278cfc296e49716bc02e25119504be4ee451e5d12da9d9941e1a3bc2eb9'
+    aarch64: 'a7fa2cb89d69242f3a961563e0d3a2de115e5b2a1a2314669c93506105f723a8',
+     armv7l: 'a7fa2cb89d69242f3a961563e0d3a2de115e5b2a1a2314669c93506105f723a8',
+       i686: '278b8c6b59c297009e6e964672f0b73166a0f7636401f0f365ed38f736b5a629',
+     x86_64: 'f1b86b658c3fb8dfc91f89d6800eafc0662e43d6692c68bf56ae6b1f4f655e62'
   })
 
-  def self.patch
-    system "sed -i 's:/TESTS =/s:mansyntax.sh:g' tests/Makefile.in"
-  end
+  depends_on 'glibc' # R
+  depends_on 'openssl' # R
+  depends_on 'zlibpkg' # R
 
-  def self.build
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS} \
-              --with-libz"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options '--with-libz'
 end
