98dae9c0f228866a84d439369551a4142301f459
Satadru Pramanik <satadru@gmail.com>
2023-10-31 22:01:20 -0400

    adjust lint rule

--- packages/ruby_rubocop.rb
+++ packages/ruby_rubocop.rb
@@ -6,10 +6,10 @@ require 'package'
 class Ruby_rubocop < Package
   description 'A Ruby static code analyzer and formatter'
   homepage 'https://rubocop.org'
-  version '1.56.4-ruby-3.2'
+  version '1.57.2-ruby-3.2'
   compatibility 'all'
   source_url 'https://github.com/chromebrew/chromebrew/raw/master/.rubocop.yml'
-  source_sha256 'ef0331bf6c4b032233f587b3fd74b688d9c1bd6cce32390dfca977bc92ef4968'
+  source_sha256 'b72214b7d7569ccdb34a43f7abbdf43cf4331f04838c79b82648d86606d96825'
 
   depends_on 'libyaml'
   depends_on 'ruby'
