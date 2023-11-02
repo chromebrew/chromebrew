7dc3f8edb7e635140a1b5b9f9a1ee84d140d1f22
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-11-02 12:28:21 -0400

    Inetutils => 2.4 (#8871)
    * fixup iputils filelists, add alive preflight message
    
    * inetutils => 2.4

--- packages/inetutils.rb
+++ packages/inetutils.rb
@@ -1,35 +1,37 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Inetutils < Package
+class Inetutils < Autotools
   description 'The Inetutils package contains programs for basic networking. Such as dnsdomainname, ftp, hostname, ifconfig, ping, ping6, talk, telnet, tftp, traceroute'
   homepage 'https://www.gnu.org/software/inetutils/'
-  version '2.2'
+  version '2.4'
   license 'GPL-3'
   compatibility 'all'
   source_url "https://ftpmirror.gnu.org/inetutils/inetutils-#{version}.tar.xz"
-  source_sha256 'd547f69172df73afef691a0f7886280fd781acea28def4ff4b4b212086a89d80'
+  source_sha256 '1789d6b1b1a57dfe2a7ab7b533ee9f5dfd9cbf5b59bb1bb3c2612ed08d0f68b2'
 
   binary_url({
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.2_i686/inetutils-2.2-chromeos-i686.tar.xz',
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.2_armv7l/inetutils-2.2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.2_armv7l/inetutils-2.2-chromeos-armv7l.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.2_x86_64/inetutils-2.2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.4_armv7l/inetutils-2.4-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.4_armv7l/inetutils-2.4-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.4_i686/inetutils-2.4-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/inetutils/2.4_x86_64/inetutils-2.4-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    i686: 'ee0f7e82efe8fee69a27bda30a6d42d2a616bcd5d9be1682cec43136f70f9b71',
-    aarch64: '413181b35983017b30a53f08de729ffb7f4dd54a2fdbb6b6d0d4dc7fa4333c76',
-     armv7l: '413181b35983017b30a53f08de729ffb7f4dd54a2fdbb6b6d0d4dc7fa4333c76',
-     x86_64: '4b6d96a0e618da13d6225d3f14f22739e6c199e944820dacbf99175ec88982df'
+    aarch64: 'f01833856fc5199d6b116fe6c5a1ef446a0063cbaaf61b031910e29b785898c4',
+     armv7l: 'f01833856fc5199d6b116fe6c5a1ef446a0063cbaaf61b031910e29b785898c4',
+       i686: '135835e0aba58d421bf1a6fd6067c777429be7a862d0d3d0f341788bfe2fbb2d',
+     x86_64: 'e982be9a2ecc72bb21acc17f6edcd666789c36086a998f4da2691ced76251b95'
   })
 
-  depends_on 'linux_pam'
-  depends_on 'patchelf' => :build
-  depends_on 'libcap'
+  depends_on 'e2fsprogs' # R
+  depends_on 'glibc' # R
+  depends_on 'krb5' # R
+  depends_on 'libcap' # R
+  depends_on 'linux_pam' # R
+  depends_on 'ncurses' # R
+  depends_on 'readline' # R
 
-  def self.build
-    # logger conflicts with util_linux
-    system "env #{CREW_ENV_OPTIONS.sub("CFLAGS='", "CFLAGS='-lpthread -ltinfo ")} \
-      LIBRARY_PATH=#{CREW_LIB_PREFIX} ./configure #{CREW_OPTIONS} \
+  pre_configure_options "CC='gcc -lncursesw -lpthread -ltinfow' CXX='g++ -lncursesw -lpthread -ltinfow'"
+  configure_options "--with-ncurses-include-dir=#{CREW_PREFIX}/include/ncursesw \
       --with-krb5=#{CREW_PREFIX} \
       --disable-rpath \
       --with-wrap \
@@ -40,15 +42,9 @@ class Inetutils < Package
       --enable-authentication \
       --disable-servers \
       --disable-logger"
-    system 'make'
-  end
 
   def self.install
     system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-
-    system "patchelf --set-rpath #{CREW_LIB_PREFIX} #{CREW_DEST_PREFIX}/bin/ping"
-    system "patchelf --set-rpath #{CREW_LIB_PREFIX} #{CREW_DEST_PREFIX}/bin/ping6"
-    system "patchelf --set-rpath #{CREW_LIB_PREFIX} #{CREW_DEST_PREFIX}/bin/traceroute"
     FileUtils.install "#{CREW_DEST_PREFIX}/bin/ping", "#{CREW_DEST_PREFIX}/bin/ping.elf", mode: 0o755
     FileUtils.install "#{CREW_DEST_PREFIX}/bin/ping6", "#{CREW_DEST_PREFIX}/bin/ping6.elf", mode: 0o755
     FileUtils.install "#{CREW_DEST_PREFIX}/bin/traceroute", "#{CREW_DEST_PREFIX}/bin/traceroute.elf", mode: 0o755
@@ -56,21 +52,21 @@ class Inetutils < Package
       #!/bin/bash
       sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
           --keep=1 --user=nobody --addamb=cap_net_raw -- \\
-          -c "#{CREW_PREFIX}/bin/ping.elf \$@"
+          -c "#{CREW_PREFIX}/bin/ping.elf $@"
     PING_HEREDOC
     File.write("#{CREW_DEST_PREFIX}/bin/ping", @PING_SH, perm: 0o755)
     @PING6_SH = <<~PING6_HEREDOC
       #!/bin/bash
       sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
           --keep=1 --user=nobody --addamb=cap_net_raw -- \\
-          -c "#{CREW_PREFIX}/bin/ping6.elf \$@"
+          -c "#{CREW_PREFIX}/bin/ping6.elf $@"
     PING6_HEREDOC
     File.write("#{CREW_DEST_PREFIX}/bin/ping6", @PING6_SH, perm: 0o755)
     @TRACEROUTE_SH = <<~TRACEROUTE_HEREDOC
       #!/bin/bash
       sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
           --keep=1 --user=nobody --addamb=cap_net_raw -- \\
-          -c "#{CREW_PREFIX}/bin/traceroute.elf \$@"
+          -c "#{CREW_PREFIX}/bin/traceroute.elf $@"
     TRACEROUTE_HEREDOC
     File.write("#{CREW_DEST_PREFIX}/bin/traceroute", @TRACEROUTE_SH, perm: 0o755)
   end
