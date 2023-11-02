adef67f4c8a8479ec73d47924b8e0bf0564f2faa
Ed Reel <edreel@gmail.com>
2023-11-02 12:22:27 -0500

    Skype 8.106.0.212 => 8.107.0.215 (#8874)

--- packages/skype.rb
+++ packages/skype.rb
@@ -3,11 +3,11 @@ require 'package'
 class Skype < Package
   description 'Skype is a telecommunications application that specializes in providing video chat and voice calls between devices'
   homepage 'https://www.skype.com/'
-  version '8.106.0.212'
+  version '8.107.0.215'
   license 'Skype-TOS'
   compatibility 'x86_64'
-  source_url 'https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_8.106.0.212_amd64.deb'
-  source_sha256 '4e5aa10a3e67c8bf12128dcce9a84f2d83930eb1231efc6dbb5a85491f0bb643'
+  source_url 'https://repo.skype.com/deb/pool/main/s/skypeforlinux/skypeforlinux_8.107.0.215_amd64.deb'
+  source_sha256 'ec5291abada05fc3607ed6fd7dab59687889972fbaab4e2062112ec7b72dce72'
 
   no_compile_needed
   no_shrink
