5661d6027640a59bbc93953a4a7936ab1ecdfd0b
Ed Reel <edreel@gmail.com>
2023-11-05 17:52:59 -0600

    Update zathura packages and dependency
    Girara 0.3.5 => 0.4.0
    
    Zathura 0.4.7 => 0.5.2
    
    Zathura_poppler_pdf 0.3.0 => 0.3.1

--- packages/zathura.rb
+++ packages/zathura.rb
@@ -1,37 +1,31 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Zathura < Package
+class Zathura < Meson
   description 'zathura is a highly customizable and functional PDF document viewer'
   homepage 'https://pwmt.org/projects/zathura/'
-  version '0.4.7'
+  version '0.5.2'
   license 'ZLIB'
-  compatibility 'aarch64,armv7l,x86_64'
-  source_url 'https://pwmt.org/projects/zathura/download/zathura-0.4.7.tar.xz'
-  source_sha256 'e012dbfe2b981b826553a9af8420d42a9c5d3387fbe14a5399ce94a2d374a1e7'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://pwmt.org/projects/zathura/download/zathura-0.5.2.tar.xz'
+  source_sha256 'c64ba7cb9facf2b1499b9dc929b6736c72c69f8062eed4f2940556c852256194'
 
   depends_on 'appstream_glib'
   depends_on 'desktop_file_utilities'
   depends_on 'girara'
+  depends_on 'librsvg'
   depends_on 'libseccomp'
   depends_on 'zathura_poppler_pdf'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zathura/0.4.7_armv7l/zathura-0.4.7-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zathura/0.4.7_armv7l/zathura-0.4.7-chromeos-armv7l.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zathura/0.4.7_x86_64/zathura-0.4.7-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zathura/0.5.2_armv7l/zathura-0.5.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zathura/0.5.2_armv7l/zathura-0.5.2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zathura/0.5.2_x86_64/zathura-0.5.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '401655bbb1a8f83cbcd158e67370abc105a292027f8b35ccbe6af04755b33d61',
-     armv7l: '401655bbb1a8f83cbcd158e67370abc105a292027f8b35ccbe6af04755b33d61',
-     x86_64: '7b56a87ff85d05ebcd9c8ea7cad130f861d9f3f6deaad0e8934ccc24ab4f87f1'
+    aarch64: '3cfbee5522f48b4f99d62d0c4f559320881012812b0f879e0592edf9b3fb608a',
+     armv7l: '3cfbee5522f48b4f99d62d0c4f559320881012812b0f879e0592edf9b3fb608a',
+     x86_64: 'a402bed68aff3ac037973935760317a4db6716049abe75bd456067798e190a46'
   })
 
-  def self.build
-    system "meson --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX} -Dmanpages=disabled -Dsynctex=disabled builddir"
-    system 'ninja -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
+  meson_options '-Dmanpages=disabled -Dsynctex=disabled'
 end
