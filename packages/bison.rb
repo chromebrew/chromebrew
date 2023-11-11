fe9a19946ecae0967ed4e79c77ec41af69d5107f
Satadru Pramanik <satadru@gmail.com>
2023-11-11 17:00:36 -0500

    rebuild bison,sed; update cpio

--- packages/bison.rb
+++ packages/bison.rb
@@ -1,40 +1,28 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Bison < Package
+class Bison < Autotools
   description 'Bison is a general-purpose parser generator that converts an annotated context-free grammar into a deterministic LR or generalized LR (GLR) parser employing LALR(1) parser tables.'
   homepage 'https://www.gnu.org/software/bison/'
-  version '3.8.2'
+  @_ver = '3.8.2'
+  version "#{@_ver}-1"
   license 'GPL-2'
   compatibility 'all'
-  source_url "https://ftpmirror.gnu.org/bison/bison-#{version}.tar.lz"
+  source_url "https://ftpmirror.gnu.org/bison/bison-#{@_ver}.tar.lz"
   source_sha256 'fdf98bfe82abb04a34d4356753f7748dbbd2ef1221b1f202852a2b5ce0f78534'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2_armv7l/bison-3.8.2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2_armv7l/bison-3.8.2-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2_i686/bison-3.8.2-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2_x86_64/bison-3.8.2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2-1_armv7l/bison-3.8.2-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2-1_armv7l/bison-3.8.2-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2-1_i686/bison-3.8.2-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/bison/3.8.2-1_x86_64/bison-3.8.2-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '8f1ac3143e216a054c56a4dcb23d58078c7dbccdc67f86b1d4a8169fb4f0d43b',
-     armv7l: '8f1ac3143e216a054c56a4dcb23d58078c7dbccdc67f86b1d4a8169fb4f0d43b',
-       i686: '026e8c897e6f8c5bc1d30d17f0f5612d5d0b99e8e5cf77e16cecaf646716a755',
-     x86_64: 'e671fd14e25757fd0fc45d65443ff55d13df428e34e80fc2f642d86aa5a9df14'
+    aarch64: '5c7748f4fd47bd81bad984cb278df1561407b43ea3601d2c49ccaa752119dae4',
+     armv7l: '5c7748f4fd47bd81bad984cb278df1561407b43ea3601d2c49ccaa752119dae4',
+       i686: '8b8120f98e4707695a337d4ac0b21fc0bc26e1a1ee734e4b191420988601d576',
+     x86_64: '4476aa49cf42b79a2fed31e77a7d1df7e11aebb3bf269d036c2666d2ab7e238a'
   })
 
-  depends_on 'gettext'
-
-  def self.build
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    puts 'Notice: Bison checks take a very long time.'.yellow
-    system 'make', 'check'
-  end
+  depends_on 'gettext' # R
+  depends_on 'glibc' # R
 end
--- packages/cpio.rb
+++ packages/cpio.rb
@@ -1,50 +1,28 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Cpio < Package
+class Cpio < Autotools
   description 'GNU cpio copies files into or out of a cpio or tar archive. The archive can be another file on the disk, a magnetic tape, or a pipe.'
   homepage 'https://www.gnu.org/software/cpio/'
-  version '2.13'
-  compatibility 'all'
+  version '2.14'
   license 'GPL-3'
-  source_url 'https://ftpmirror.gnu.org/cpio/cpio-2.13.tar.bz2'
-  source_sha256 'eab5bdc5ae1df285c59f2a4f140a98fc33678a0bf61bdba67d9436ae26b46f6d'
+  compatibility 'all'
+  source_url 'https://ftpmirror.gnu.org/cpio/cpio-2.14.tar.bz2'
+  source_sha256 'fcdc15d60f7267a6fc7efcd6b9db7b6c8966c4f2fbbb964c24d41336fd3f2c12'
 
   binary_url({
-    i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_i686/cpio-2.13-chromeos-i686.tar.zst',
-  x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_x86_64/cpio-2.13-chromeos-x86_64.tar.zst',
- aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_armv7l/cpio-2.13-chromeos-armv7l.tar.zst',
-  armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.13_armv7l/cpio-2.13-chromeos-armv7l.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.14_armv7l/cpio-2.14-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.14_armv7l/cpio-2.14-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.14_i686/cpio-2.14-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/cpio/2.14_x86_64/cpio-2.14-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    i686: 'af635bbef1f4b56a2ad457e97f0a4794e3b854784f4e86375fe3dea6905e0407',
-  x86_64: '11265e9d8eb7760d0d9e98e80860056793243e622cdf82d4ed2a2ef51722f8ee',
- aarch64: '61e781fe2ff468ae371bc7058f0d970a299d763012bb285895826b099ace958a',
-  armv7l: '61e781fe2ff468ae371bc7058f0d970a299d763012bb285895826b099ace958a'
+    aarch64: 'b3bca5b31c4e1b45e2f843658464c9fcbdb8eefeec7c5f4284f2be2011b3d969',
+     armv7l: 'b3bca5b31c4e1b45e2f843658464c9fcbdb8eefeec7c5f4284f2be2011b3d969',
+       i686: 'ca491cfb7ce43d1121d3682da98335d26b73a64eb3fc54af638b54137d814ea7',
+     x86_64: '7801234185c3afa64b2b27e1a2ebb5da842d065a9d5f57e4b5c803f529805978'
   })
 
-  def self.patch
-    # these patches allow -flto to work
-    _url = 'https://github.com/alpinelinux/aports/raw/dfc99097d6372098d36f33a1c80961d3639293f2/community/cpio'
-    downloader "#{_url}/CVE-2021-38185.patch", 'a92b9b3367a90cb66e08674df9b62dfacafa85885bfe5b16de0517e1694a2502'
-    downloader "#{_url}/CVE-2021-38185-2.patch", '2a2b3564d3910dfa7b0f595af6a69ed5a664443e786fc770a9a51d453b7af6d1'
-    downloader "#{_url}/CVE-2021-38185-3.patch", '3b337b844cc37043cb49b115fcf0efa0bc6a042eee7ef7a074c74b56d3978156'
-    downloader "#{_url}/gcc-10.patch", '68181f05736e9165493320f5213f76410c496a04ca03a3a7da3eb6f8d0c4e4c1'
-    Dir['CVE-2021-38185.patch', 'CVE-2021-38185-2.patch', 'CVE-2021-38185-3.patch', 'gcc-10.patch'].each do |filename|
-      system "patch -Np1 -i #{filename}"
-    end
-  end
+  depends_on 'glibc' # R
 
-  def self.build
-    system "./configure #{CREW_OPTIONS} \
-              --enable-mt" # magnetic tape, not manifest tool
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  configure_options '--enable-mt' # magnetic tape, not manifest tool
 end
