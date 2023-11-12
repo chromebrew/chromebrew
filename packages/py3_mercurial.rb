adad6d6a8be72e50d1a9e1469e8fd94988c11fae
Satadru Pramanik <satadru@gmail.com>
2023-11-12 12:38:57 -0500

    add new binaries

--- packages/py3_mercurial.rb
+++ packages/py3_mercurial.rb
@@ -1,36 +1,28 @@
-require 'package'
+require 'buildsystems/pip'
 
-class Mercurial < Package
+class Py3_mercurial < Pip
   description 'Mercurial is a free, distributed source control management tool. It efficiently handles projects of any size and offers an easy and intuitive interface.'
   homepage 'https://www.mercurial-scm.org/'
-  version '5.9.2'
+  version '6.5.3'
   license 'GPL-2+'
   compatibility 'all'
-  source_url "https://www.mercurial-scm.org/release/mercurial-#{version}.tar.gz"
-  source_sha256 '1edad93096f64d5cae55b9550bb835ac73840c7406861c3cf4e14c3b443bec54'
+  source_url 'SKIP'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mercurial/5.9.2_armv7l/mercurial-5.9.2-chromeos-armv7l.tpxz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mercurial/5.9.2_armv7l/mercurial-5.9.2-chromeos-armv7l.tpxz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mercurial/5.9.2_i686/mercurial-5.9.2-chromeos-i686.tpxz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mercurial/5.9.2_x86_64/mercurial-5.9.2-chromeos-x86_64.tpxz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_mercurial/6.5.3_armv7l/py3_mercurial-6.5.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_mercurial/6.5.3_armv7l/py3_mercurial-6.5.3-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_mercurial/6.5.3_i686/py3_mercurial-6.5.3-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/py3_mercurial/6.5.3_x86_64/py3_mercurial-6.5.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '144612b4e724584a23ebc84bc456e253090931492a377d4502894f8b2f1b719f',
-     armv7l: '144612b4e724584a23ebc84bc456e253090931492a377d4502894f8b2f1b719f',
-       i686: 'bc14bb98c624fecbe3ad9f02135b9159bb36b2c79f89ff3ed2c1e381e1bc7c03',
-     x86_64: '429d53235c70be2a251c2324982222b4836ed4fe15d912d86b577836655157ce'
+    aarch64: 'ad9fdf69371c58c8b4a3a9a557ab5eaac6a67853ae9f5dc5425ebd823fa1e41b',
+     armv7l: 'ad9fdf69371c58c8b4a3a9a557ab5eaac6a67853ae9f5dc5425ebd823fa1e41b',
+       i686: '391928a642b2d0f201569e9b8f08bbcc1554befe83506148a06db4dd6a935930',
+     x86_64: '651fa4cdc65f2bcec0517701b32088a50edb942cd18d136b7829eb403a6f5efe'
   })
 
-  depends_on 'py3_docutils'
+  depends_on 'glibc' # R
+  depends_on 'py3_docutils' => :build
+  depends_on 'python3' # L
   depends_on 'texinfo' => :build
-  depends_on 'python3' => :build
-
-  def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
-
-  def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
-  end
 end
