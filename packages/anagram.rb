1bf8109b5fcfd2817615851e3e839c3f0c3213ea
Satadru Pramanik <satadru@gmail.com>
2023-11-12 18:54:28 -0500

    rebuild anagram, update libgudev & upower

--- packages/anagram.rb
+++ packages/anagram.rb
@@ -3,52 +3,46 @@ require 'package'
 class Anagram < Package
   description 'finds anagrams or permutations of words in the target phrase'
   homepage 'https://www.fourmilab.ch/anagram/'
-  version '1.5'
+  version '1.5-1'
   license 'public-domain'
   compatibility 'all'
   source_url 'https://www.fourmilab.ch/anagram/anagram-1.5.tar.gz'
   source_sha256 '62eca59318782e889118a0e130d454e1c397aedd99fc59b2194393bf0eff5348'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5_armv7l/anagram-1.5-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5_armv7l/anagram-1.5-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5_i686/anagram-1.5-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5_x86_64/anagram-1.5-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5-1_armv7l/anagram-1.5-1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5-1_armv7l/anagram-1.5-1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5-1_i686/anagram-1.5-1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/anagram/1.5-1_x86_64/anagram-1.5-1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '86cba09c62c088c62405e95f844312efeb9b376ea05af25c54b8bae33834405d',
-     armv7l: '86cba09c62c088c62405e95f844312efeb9b376ea05af25c54b8bae33834405d',
-       i686: 'd71541b068c8a1348c6982cf4a25695789f8c97102b9a97962cc1c6573f9d294',
-     x86_64: '261cccedc4ccfd4dfb5351bed6444ab2183800bfe5bd3f34f5332fe966e42c43'
+    aarch64: '60eb2350a7133d51a49e92ff958136e1b9dc81c4dd8ef7b75df3f0bd4907dcf0',
+     armv7l: '60eb2350a7133d51a49e92ff958136e1b9dc81c4dd8ef7b75df3f0bd4907dcf0',
+       i686: 'e9079eddb55e0f0c9a660c761394355ef5b8d755bdd457ca2c591388a0e8f10a',
+     x86_64: '5e02d9bf17529e1db392e014b193f528b3c2fd990e758be3f023478b56ac9975'
   })
 
+  depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
 
   def self.build
     system "./configure #{CREW_OPTIONS}"
     system 'make'
 
-    @_anagram_wrapper = <<~EOF
+    @_anagram_wrapper = <<~ANAGRAM_WRAPPER_EOF
       #!/bin/bash -e
 
       exec #{CREW_PREFIX}/share/anagram/bin/anagram \
         --dictionary #{CREW_PREFIX}/share/anagram/crossword.txt \
         --bindict #{CREW_PREFIX}/share/anagram/wordlist.bin "${@}"
-    EOF
+    ANAGRAM_WRAPPER_EOF
+    File.write 'anagram_wrapper', @_anagram_wrapper, perm: 0o755
   end
 
   def self.install
-    FileUtils.mkdir_p %W[
-      #{CREW_DEST_PREFIX}/bin
-      #{CREW_DEST_PREFIX}/share/anagram/bin
-      #{CREW_DEST_MAN_PREFIX}/man1
-    ]
-
-    system "gzip -c -9 anagram.1 > #{CREW_DEST_MAN_PREFIX}/man1/anagram.1.gz"
-
-    File.write "#{CREW_DEST_PREFIX}/bin/anagram", @_anagram_wrapper, perm: 0o755
-
     FileUtils.install 'anagram', "#{CREW_DEST_PREFIX}/share/anagram/bin", mode: 0o755
+    FileUtils.install 'anagram_wrapper', "#{CREW_DEST_PREFIX}/bin/anagram", mode: 0o755
+    FileUtils.install 'anagram.1', "#{CREW_DEST_MAN_PREFIX}/man1/anagram.1", mode: 0o644
     FileUtils.install 'crossword.txt', "#{CREW_DEST_PREFIX}/share/anagram", mode: 0o644
     FileUtils.install 'wordlist.bin', "#{CREW_DEST_PREFIX}/share/anagram", mode: 0o644
   end
--- packages/upower.rb
+++ packages/upower.rb
@@ -1,50 +1,38 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Upower < Package
+class Upower < Meson
   description 'Abstraction for enumerating power devices, listening to device events and querying history and statistics'
   homepage 'https://upower.freedesktop.org'
-  version '1.90.0'
+  version '1.90.2'
   license 'GPL-2'
-  compatibility 'all'
-  source_url 'https://gitlab.freedesktop.org/upower/upower/-/archive/v1.90.0/upower-v1.90.0.tar.bz2'
-  source_sha256 'eafa0a367dc0417390984eeb81e9d1a2b449999ba6ad20ed39bf270d0b4e5e77'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://gitlab.freedesktop.org/upower/upower.git'
+  git_hashtag "v#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/upower/1.90.0_armv7l/upower-1.90.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/upower/1.90.0_armv7l/upower-1.90.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/upower/1.90.0_i686/upower-1.90.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/upower/1.90.0_x86_64/upower-1.90.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/upower/1.90.2_armv7l/upower-1.90.2-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/upower/1.90.2_armv7l/upower-1.90.2-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/upower/1.90.2_x86_64/upower-1.90.2-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '2468840113835a829c7b94744958ff8fe11dcdbaa1dedafb5c924e91da4331cf',
-     armv7l: '2468840113835a829c7b94744958ff8fe11dcdbaa1dedafb5c924e91da4331cf',
-       i686: '302a4a5efeaecee4f0d01a45666ce0e049556b2f590f44af0631576842233622',
-     x86_64: 'f289eb3beb36fbcf5dd26a57207b0b04062132d75e9a1feda7a50e86a801866f'
+    aarch64: '4b7a470ec75af657fd8ca4cac58f6adf568756348a7c1b4112c1521fefa3cbcf',
+     armv7l: '4b7a470ec75af657fd8ca4cac58f6adf568756348a7c1b4112c1521fefa3cbcf',
+     x86_64: 'cfe67815bc2225f8189e8f2b426b61258a081c6b4a3869e52c6e991bece42b72'
   })
 
-  depends_on 'libusb'
-  depends_on 'libgudev' # R
   depends_on 'docbook_xsl' => :build
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'glib' # R
   depends_on 'gobject_introspection' => :build
   depends_on 'gtk_doc' => :build
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
+  depends_on 'libgudev' # R
+  depends_on 'libusb' => :build
   depends_on 'py3_pygments' => :build
-  depends_on 'gcc_lib' # R
 
-  def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} \
-      -Dos_backend=linux \
+  meson_options "-Dos_backend=linux \
       -Dsystemdsystemunitdir=no \
       -Dhistorydir=#{CREW_PREFIX}/var \
       -Dudevrulesdir=#{CREW_PREFIX}/etc/udev \
-      -Dudevhwdbdir=#{CREW_PREFIX}/etc/udev \
-      builddir"
-    system 'meson configure --no-pager builddir'
-    system 'ninja -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
+      -Dudevhwdbdir=#{CREW_PREFIX}/etc/udev"
 end
