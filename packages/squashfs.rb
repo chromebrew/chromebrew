990ba7ad5f78a7d6fe53d276c436760d1c37a535
Satadru Pramanik <satadru@gmail.com>
2023-11-14 22:11:48 -0500

    rebuild using 'make install'

--- packages/squashfs.rb
+++ packages/squashfs.rb
@@ -16,10 +16,10 @@ class Squashfs < Package
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/squashfs/4.6.1_x86_64/squashfs-4.6.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'fa146db645ee4d4cf3b10f9a7e6425c182fef04505f289378a456e7aba7a373a',
-     armv7l: 'fa146db645ee4d4cf3b10f9a7e6425c182fef04505f289378a456e7aba7a373a',
-       i686: '51f3a8423f782ce0ee19ede8834deac84aa4161e321df87a397b2b92185395c8',
-     x86_64: '861359978b79ed8c8e6d8869b0a7b6c68ead651a627014f45bd9db65c5d58ec6'
+    aarch64: 'da924cdb4c4f856b7b9b869e60496b10096132beb2e57592e8f015c1486c123c',
+     armv7l: 'da924cdb4c4f856b7b9b869e60496b10096132beb2e57592e8f015c1486c123c',
+       i686: 'fbe58537fd0a167f61b60c8c6ea23517c46816ae9e30fdace0f65c72a4887add',
+     x86_64: '14309b3d8544314236aacd248f48cb781d28d232b23228646c481e2619223974'
   })
 
   depends_on 'compressdoc' => :build
@@ -32,23 +32,26 @@ class Squashfs < Package
   depends_on 'zlibpkg' # R
   depends_on 'zstd' # R
 
-  def self.build
+  def self.patch
     FileUtils.cd('squashfs-tools') do
       system "sed -i '5iLZ4_SUPPORT = 1' Makefile"
       system "sed -i '6iLZO_SUPPORT = 1' Makefile"
       system "sed -i '7iXZ_SUPPORT = 1' Makefile"
       system "sed -i '8iZSTD_SUPPORT = 1' Makefile"
+      system "sed -i 's,INSTALL_PREFIX = /usr/local,INSTALL_PREFIX = #{CREW_DEST_PREFIX},g' Makefile"
+      system "sed -i 's,INSTALL_MANPAGES_DIR = $(INSTALL_PREFIX)/man/man1,INSTALL_MANPAGES_DIR = #{CREW_DEST_MAN_PREFIX}man1,g' Makefile"
+    end
+  end
+
+  def self.build
+    FileUtils.cd('squashfs-tools') do
       system 'make'
     end
   end
 
   def self.install
     FileUtils.cd('squashfs-tools') do
-      FileUtils.install 'mksquashfs',  "#{CREW_DEST_PREFIX}/bin/mksquashfs", mode: 0o755
-      FileUtils.install 'unsquashfs',  "#{CREW_DEST_PREFIX}/bin/unsquashfs", mode: 0o755
-      FileUtils.mkdir_p "#{CREW_DEST_MAN_PREFIX}man1"
-      system "help2man ./mksquashfs -N --no-discard-stderr > #{CREW_DEST_MAN_PREFIX}man1/mksquashfs.1"
-      system "help2man ./unsquashfs -N --no-discard-stderr > #{CREW_DEST_MAN_PREFIX}man1/unsquashfs.1"
+      system 'make install'
     end
   end
 end
