e40a883c22e63f2921e97d260c6203cdba9ca000
Satadru Pramanik <satadru@gmail.com>
2023-11-16 22:45:03 -0500

    build i686 binaries for gobject_intropection, libxmlb; rebuild flatpak using git subprojects

--- packages/flatpak.rb
+++ packages/flatpak.rb
@@ -15,14 +15,14 @@ class Flatpak < Meson
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/flatpak/1.15.6_x86_64/flatpak-1.15.6-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'c908904e24c3344b10a62a47c69d5e2ed238daec026016ae4e8a1797be64e503',
-     armv7l: 'c908904e24c3344b10a62a47c69d5e2ed238daec026016ae4e8a1797be64e503',
-     x86_64: '4b7afe615bf9032dbd8fffb63302081ff6e1f4ec570bc50549c78d252fc1f611'
+    aarch64: '313ff8ecaf84a2a42d24bde5999d6ec455dda6694d734973cb4c226b510241d9',
+     armv7l: '313ff8ecaf84a2a42d24bde5999d6ec455dda6694d734973cb4c226b510241d9',
+     x86_64: '205159d142b8fb7c9a65ac75309d4391889af4afc2dbd56e79da692b18c93e32'
   })
 
   depends_on 'appstream_glib' => :build
   depends_on 'appstream' # R
-  depends_on 'bubblewrap' => :build
+  depends_on 'bubblewrap' # L
   depends_on 'curl' # R
   depends_on 'dconf' # R
   depends_on 'fuse3' # R
@@ -49,52 +49,6 @@ class Flatpak < Meson
   depends_on 'xdg_dbus_proxy' => :build
   depends_on 'xmlto' => :build
   depends_on 'zstd' # R
-
-  def self.patch
-    # Use fuse3
-    # system "sed -i 's:PKG_CHECK_MODULES(FUSE, fuse >= 2.9.2):PKG_CHECK_MODULES(FUSE, fuse3 >= 3.1.1):' configure.ac"
-    # system "sed -i 's:#define FUSE_USE_VERSION 26:#define FUSE_USE_VERSION 31:' revokefs/main.c"
-
-    # Source has libglnx repo as submodule
-    @git_dir = 'libglnx'
-    @git_hash = 'aff1eea9b6fa62a1ef05c8fa0936c69a31a2bf4e'
-    @git_url = 'https://gitlab.gnome.org/GNOME/libglnx.git'
-    FileUtils.rm_rf(@git_dir)
-    FileUtils.mkdir_p(@git_dir)
-    Dir.chdir @git_dir do
-      system 'git init'
-      system 'git config --local advice.detachedHead false'
-      system "git remote add origin #{@git_url}"
-      system "git fetch --depth 1 origin #{@git_hash}"
-      system 'git checkout FETCH_HEAD'
-    end
-    # Source has bubblewrap repo as submodule
-    @git_dir = 'bubblewrap'
-    @git_hash = 'v0.8.0'
-    @git_url = 'https://github.com/containers/bubblewrap.git'
-    FileUtils.rm_rf(@git_dir)
-    FileUtils.mkdir_p(@git_dir)
-    Dir.chdir @git_dir do
-      system 'git init'
-      system 'git config --local advice.detachedHead false'
-      system "git remote add origin #{@git_url}"
-      system "git fetch --depth 1 origin #{@git_hash}"
-      system 'git checkout FETCH_HEAD'
-    end
-    # Source has variant-schema-compiler repo as submodule
-    @git_dir = 'variant-schema-compiler'
-    @git_hash = 'cfc356c38edfcf73c8ac240d215479b950f91b1f'
-    @git_url = 'https://gitlab.gnome.org/alexl/variant-schema-compiler.git'
-    FileUtils.rm_rf(@git_dir)
-    FileUtils.mkdir_p(@git_dir)
-    Dir.chdir @git_dir do
-      system 'git init'
-      system 'git config --local advice.detachedHead false'
-      system "git remote add origin #{@git_url}"
-      system "git fetch --depth 1 origin #{@git_hash}"
-      system 'git checkout FETCH_HEAD'
-    end
-  end
 
   meson_options "-Dsystem_install_dir=#{CREW_PREFIX}/var/lib/flatpak \
               -Dsystemd=disabled \
--- packages/libxmlb.rb
+++ packages/libxmlb.rb
@@ -8,18 +8,20 @@ class Libxmlb < Meson
   homepage 'https://github.com/hughsie/libxmlb'
   version '0.3.14'
   license 'LGPL'
-  compatibility 'x86_64 aarch64 armv7l'
+  compatibility 'all'
   source_url 'https://github.com/hughsie/libxmlb.git'
   git_hashtag version
 
   binary_url({
     aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmlb/0.3.14_armv7l/libxmlb-0.3.14-chromeos-armv7l.tar.zst',
      armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmlb/0.3.14_armv7l/libxmlb-0.3.14-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmlb/0.3.14_i686/libxmlb-0.3.14-chromeos-i686.tar.zst',
      x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libxmlb/0.3.14_x86_64/libxmlb-0.3.14-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
     aarch64: 'c63785e43de4d1761d560dfb928a87811cdfacabca418a69cc85e3478db50dca',
      armv7l: 'c63785e43de4d1761d560dfb928a87811cdfacabca418a69cc85e3478db50dca',
+       i686: 'e61e0fc258310c2e294ecc12f5028129a855ba6e68f3cbaceeaa61cb457a8f9e',
      x86_64: '2c94f36118889be247c3dcbf2e871dac303770aef92b04c3b1b0d9237c25d2fb'
   })
 
