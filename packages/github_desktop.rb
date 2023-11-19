953a07212f119e453b1bfb5c4572ba67fe63cb58
Ed Reel <edreel@gmail.com>
2023-11-19 10:31:53 -0600

    Github_desktop 2.9.6-RC => 3.3.3-RC2

--- packages/github_desktop.rb
+++ packages/github_desktop.rb
@@ -3,20 +3,29 @@ require 'package'
 class Github_desktop < Package
   description 'GitHub Desktop is an open source Electron-based GitHub app'
   homepage 'https://desktop.github.com/'
-  version '2.9.6-RC'
+  version '3.3.3-RC2'
   license 'MIT'
-  compatibility 'x86_64'
-  source_url 'https://github.com/shiftkey/desktop/releases/download/release-2.9.6-linux1/GitHubDesktop-linux-2.9.6-linux1.AppImage'
-  source_sha256 'e5187e7c5a9ad1fa3c110c1ec60c9e7f75e0792c3670907741243f7cbea831b0'
-
-  binary_url({})
-  binary_sha256({})
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url({
+    aarch64: 'https://github.com/shiftkey/desktop/releases/download/release-3.3.3-linux2/GitHubDesktop-linux-armv7l-3.3.3-linux2.AppImage',
+     armv7l: 'https://github.com/shiftkey/desktop/releases/download/release-3.3.3-linux2/GitHubDesktop-linux-armv7l-3.3.3-linux2.AppImage',
+     x86_64: 'https://github.com/shiftkey/desktop/releases/download/release-3.3.3-linux2/GitHubDesktop-linux-x86_64-3.3.3-linux2.AppImage'
+  })
+  source_sha256({
+    aarch64: '7080fd4b4e371e56b5bc49a0d66fe888f81884260283f181c0e33377810613b4',
+     armv7l: '7080fd4b4e371e56b5bc49a0d66fe888f81884260283f181c0e33377810613b4',
+     x86_64: 'c8b71fef885e903ff569273853c2848230a990e602d053b196443bdc3c7645a0'
+  })
 
   depends_on 'at_spi2_core'
   depends_on 'gdk_pixbuf'
   depends_on 'libcom_err'
   depends_on 'xdg_base'
-  depends_on 'sommelier'
+  depends_on 'gtk3'
+
+  no_compile_needed
+  no_shrink
+  no_strip # Fixes Syntax error: redirection unexpected (expecting ")")
 
   def self.build
     gd = <<~EOF
