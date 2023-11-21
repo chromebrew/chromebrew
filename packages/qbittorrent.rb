c64b2964e443d01930d514ba2db4a810c6f35ebc
Ed Reel <edreel@gmail.com>
2023-11-21 10:18:12 -0600

    Qbittorrent 4.6.0 => 4.6.1 (#8972)

--- packages/qbittorrent.rb
+++ packages/qbittorrent.rb
@@ -3,21 +3,21 @@ require 'buildsystems/cmake'
 class Qbittorrent < CMake
   description 'Open-source software alternative to µTorrent.'
   homepage 'https://www.qbittorrent.org/'
-  version '4.6.0'
+  version '4.6.1'
   license 'GPL-2'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.6.0/qbittorrent-4.6.0.tar.xz'
-  source_sha256 '27a5954dccd3948dc69dd59d24ee8f4f7427bdc09359fb30cf05c786b370d3be'
+  source_url 'https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.6.1/qbittorrent-4.6.1.tar.xz'
+  source_sha256 '63223387cab9936a4feae21597b3631f20ba33f33fa73ef3f35d07d63cae3643'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qbittorrent/4.6.0_armv7l/qbittorrent-4.6.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qbittorrent/4.6.0_armv7l/qbittorrent-4.6.0-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qbittorrent/4.6.0_x86_64/qbittorrent-4.6.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qbittorrent/4.6.1_armv7l/qbittorrent-4.6.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qbittorrent/4.6.1_armv7l/qbittorrent-4.6.1-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/qbittorrent/4.6.1_x86_64/qbittorrent-4.6.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'bcfcab1d82ce71eadd51d6f15a3146fd6d0d95f0a0d2519dcb6609e1fef4f67c',
-     armv7l: 'bcfcab1d82ce71eadd51d6f15a3146fd6d0d95f0a0d2519dcb6609e1fef4f67c',
-     x86_64: '80bc158761f5beac01aa02f567382a04edaa0cada2fd1e1375fbd9101f702ab8'
+    aarch64: '94b250197ea05707dd327eb023c287a5909ee15e97ec9cb263022be7a86508a5',
+     armv7l: '94b250197ea05707dd327eb023c287a5909ee15e97ec9cb263022be7a86508a5',
+     x86_64: '5820c1260786d38c765000b5edda7ba593c233b96b9360352cb6e66117b163ff'
   })
 
   depends_on 'cmake' => :build
