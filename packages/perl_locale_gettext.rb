c583f2eca31f85c560e69316e01ca88bbbccd906
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-11-26 21:39:23 -0500

    Add perl buildsystem (#8994)
    * Add perl buildsystem
    
    * avoid conflict between perl and buildsystem
    
    * build perl_xml_parser
    
    * rebuild remaining perl packages with files in 5.38.0 dir
    
    * bump version
    
    * suggested changes
    
    * suggested changes

--- packages/perl_locale_gettext.rb
+++ packages/perl_locale_gettext.rb
@@ -1,39 +1,26 @@
-require 'package'
+require 'buildsystems/perl'
 
-class Perl_locale_gettext < Package
+class Perl_locale_gettext < PERL
   description 'Locale::gettext - message handling functions'
   homepage 'https://search.cpan.org/dist/Locale-gettext/'
   license 'GPL-1+ or Artistic'
-  version '1.07-perl5.38'
+  version '1.07-perl5.38.1'
   compatibility 'all'
   source_url 'https://cpan.metacpan.org/authors/id/P/PV/PVANDRY/gettext-1.07.tar.gz'
   source_sha256 '909d47954697e7c04218f972915b787bd1244d75e3bd01620bc167d5bbc49c15'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38_armv7l/perl_locale_gettext-1.07-perl5.38-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38_armv7l/perl_locale_gettext-1.07-perl5.38-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38_i686/perl_locale_gettext-1.07-perl5.38-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38_x86_64/perl_locale_gettext-1.07-perl5.38-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38.1_armv7l/perl_locale_gettext-1.07-perl5.38.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38.1_armv7l/perl_locale_gettext-1.07-perl5.38.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38.1_i686/perl_locale_gettext-1.07-perl5.38.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_locale_gettext/1.07-perl5.38.1_x86_64/perl_locale_gettext-1.07-perl5.38.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '236293fc1eca2efd01b9f159763db91fe2ce32d934590cac86892cf1ff5699b1',
-     armv7l: '236293fc1eca2efd01b9f159763db91fe2ce32d934590cac86892cf1ff5699b1',
-       i686: '8b845f438dc26b4d4c2e717f306535df198c3211579cdbf42dafdd720454ef78',
-     x86_64: '8f2f78ffcff91fe7b00b8a6a97d41fa3d994f2529ef18f8cc3f652b37bb04a55'
+    aarch64: '0399157f09775ae6426eb1d3d6af9f80028cafb31afbfa73708864e6a8014704',
+     armv7l: '0399157f09775ae6426eb1d3d6af9f80028cafb31afbfa73708864e6a8014704',
+       i686: '12cfe45c0532d8e1fa352e12e3568d6277aeb55ec14f72684f7dd4b80c6d23b6',
+     x86_64: '57da1a305558f655d1d6d2c322a0000cd8cec298ba431b0113c54c301a9eecbe'
   })
 
   depends_on 'glibc' # R
-
-  def self.prebuild
-    system 'perl', 'Makefile.PL'
-    system "sed -i 's,/usr/local,#{CREW_PREFIX},g' Makefile"
-  end
-
-  def self.build
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
--- packages/perl_xml_parser.rb
+++ packages/perl_xml_parser.rb
@@ -1,42 +1,29 @@
-require 'package'
+require 'buildsystems/perl'
 
-class Perl_xml_parser < Package
+class Perl_xml_parser < PERL
   description 'Perl XML::Parser - A perl module for parsing XML documents'
   homepage 'https://metacpan.org/pod/XML::Parser'
-  version '2.46-perl5.38'
+  version '2.46-perl5.38.1'
   license 'GPL-1+ or Artistic'
   compatibility 'all'
   source_url 'https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.46.tar.gz'
   source_sha256 'd331332491c51cccfb4cb94ffc44f9cd73378e618498d4a37df9e043661c515d'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38_armv7l/perl_xml_parser-2.46-perl5.38-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38_armv7l/perl_xml_parser-2.46-perl5.38-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38_i686/perl_xml_parser-2.46-perl5.38-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38_x86_64/perl_xml_parser-2.46-perl5.38-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38.1_armv7l/perl_xml_parser-2.46-perl5.38.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38.1_armv7l/perl_xml_parser-2.46-perl5.38.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38.1_i686/perl_xml_parser-2.46-perl5.38.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/perl_xml_parser/2.46-perl5.38.1_x86_64/perl_xml_parser-2.46-perl5.38.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '303aef4332505beafeed08597e20a3d9b503a30f536dd52cc716a77fdf08beb8',
-     armv7l: '303aef4332505beafeed08597e20a3d9b503a30f536dd52cc716a77fdf08beb8',
-       i686: '0b3a7fabfa96ac44dce380b8e2c22107832caddf0bf67c8e9ee0535ed9d7aa3a',
-     x86_64: 'c9986fa03bdeb3cccd6eb51ceefc5da14a2b78b5cf94aef3bc8ce7de7e33653b'
+    aarch64: '88ccca7d9086251d110221235b46f981d1840bbc0842c9bb58ccc16f019b6dee',
+     armv7l: '88ccca7d9086251d110221235b46f981d1840bbc0842c9bb58ccc16f019b6dee',
+       i686: 'f07408a005c1055d7e9602b15104323e039b5f91df85f0e6e017dd627998395e',
+     x86_64: '6ce38db71eab3397edb8b9d16f0c96d95d805fc8173cdabd531164b0b734f54c'
   })
 
   depends_on 'expat' # R
   depends_on 'glibc' # R
 
   conflicts_ok # conflicts with perl_date_format
-
-  def self.prebuild
-    system 'perl', 'Makefile.PL'
-    system "sed -i 's,/usr/local,#{CREW_PREFIX},g' Makefile"
-  end
-
-  def self.build
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
