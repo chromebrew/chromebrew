36d6888962ffcfb651ee73e4db1726217f49391f
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-11-28 11:04:07 -0500

    weston => 13 (#9003)
    * weston => 13
    
    * Weston => 13.0, fix crew prepare_package

--- packages/weston.rb
+++ packages/weston.rb
@@ -1,31 +1,34 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Weston < Package
+class Weston < Meson
   description 'Weston is the reference implementation of a Wayland compositor, and a useful compositor in its own right.'
   homepage 'http://wayland.freedesktop.org'
-  version '12.0.0'
+  version '13.0.0'
   license 'MIT and CC-BY-SA-3.0'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.freedesktop.org/wayland/weston.git'
   git_hashtag version
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/weston/12.0.0_armv7l/weston-12.0.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/weston/12.0.0_armv7l/weston-12.0.0-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/weston/12.0.0_x86_64/weston-12.0.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/weston/13.0.0_armv7l/weston-13.0.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/weston/13.0.0_armv7l/weston-13.0.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/weston/13.0.0_x86_64/weston-13.0.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '52bf23d0bd9eb64a05f172996dc921f049ba409ae8c6554d81b3b9cc3e5c8a88',
-     armv7l: '52bf23d0bd9eb64a05f172996dc921f049ba409ae8c6554d81b3b9cc3e5c8a88',
-     x86_64: '01e6bad830f2bdc6232872ffd71bcd1a1e6096df8347b15fc255a658e8c05f23'
+    aarch64: '6bdc016e369c410e42adfd2e67d2e757c77b6d31036081cbbcb5d2e1c189efbf',
+     armv7l: '6bdc016e369c410e42adfd2e67d2e757c77b6d31036081cbbcb5d2e1c189efbf',
+     x86_64: '1472d72e07bcf37651bee693f50c33f02cd3829d58581e9ef8e3d32480305dcb'
   })
 
+  depends_on 'cairo' # R
   depends_on 'dbus' => :build
   depends_on 'eudev' # R
   depends_on 'ffmpeg' # R
+  depends_on 'fontconfig' # R
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'gmp' # R
   depends_on 'gnutls' # R
   depends_on 'graphite' => :build
   depends_on 'gstreamer' # R
@@ -48,8 +51,8 @@ class Weston < Package
   depends_on 'libxkbcommon' # R
   depends_on 'libxxf86vm'
   depends_on 'linux_pam' # R
-  depends_on 'llvm16_lib' => :build
   depends_on 'mesa' # R
+  depends_on 'nettle' # R
   depends_on 'pango' # R
   depends_on 'pipewire' # R
   depends_on 'pixman' # R
@@ -60,40 +63,33 @@ class Weston < Package
   depends_on 'xdg_base' => :build
   depends_on 'zlibpkg' # R
 
-  def self.build
-    system "LIBRARY_PATH=#{CREW_LIB_PREFIX} meson setup #{CREW_MESON_OPTIONS} \
-        -Dbackend-default=wayland \
+  meson_options "-Dbackend-default=wayland \
         -Dbackend-drm=true \
         -Dbackend-rdp=false \
         -Dcolor-management-lcms=false \
-        -Dlauncher-libseat=true \
         -Dremoting=true \
         -Dshell-ivi=false \
         -Dsystemd=false \
-        -Dxwayland-path=#{CREW_PREFIX}/bin/Xwayland \
-        builddir"
-    system 'meson configure --no-pager builddir'
-    system "#{CREW_NINJA} -C builddir"
+        -Dxwayland-path=#{CREW_PREFIX}/bin/Xwayland"
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     File.write 'weston.ini', <<~WESTON_INI_EOF
       [core]
       xwayland=true
 
       [terminal]
       font=Cousine
-      EOF"
     WESTON_INI_EOF
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
     FileUtils.install 'weston.ini', "#{CREW_DEST_HOME}/.config/weston.ini", mode: 0o644
   end
 
   def self.postinstall
-    puts
-    puts 'To run weston with xwayland try something like this:'.lightblue
-    puts 'export WAYLAND_DISPLAY=wayland-1'.lightblue
-    puts 'WAYLAND_DISPLAY=wayland-0 weston -Swayland-1 --xwayland'.lightblue
-    puts
+    ExitMessage.add <<~EOT1.lightblue
+
+      To run weston with xwayland try something like this:
+      export WAYLAND_DISPLAY=wayland-1
+      WAYLAND_DISPLAY=wayland-0 weston -Swayland-1 --xwayland
+    EOT1
   end
 end
