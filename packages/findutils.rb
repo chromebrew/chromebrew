776f4315cb7563e76f2dd1994a082afc4dd7968d
Satadru Pramanik <satadru@gmail.com>
2023-11-28 16:31:12 -0500

    findutils => 4.9.0

--- packages/findutils.rb
+++ packages/findutils.rb
@@ -3,26 +3,27 @@ require 'buildsystems/autotools'
 class Findutils < Autotools
   description 'the basic directory searching utilities of the GNU operating system'
   homepage 'https://www.gnu.org/software/findutils/'
-  version '4.8.0'
+  version '4.9.0'
   license 'GPL-3+'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/findutils/findutils-4.8.0.tar.xz'
-  source_sha256 '57127b7e97d91282c6ace556378d5455a9509898297e46e10443016ea1387164'
+  source_url 'https://ftpmirror.gnu.org/findutils/findutils-4.9.0.tar.xz'
+  source_sha256 'a2bfb8c09d436770edc59f50fa483e785b161a3b7b9d547573cb08065fd462fe'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_armv7l/findutils-4.8.0-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_armv7l/findutils-4.8.0-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_i686/findutils-4.8.0-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.8.0_x86_64/findutils-4.8.0-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.9.0_armv7l/findutils-4.9.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.9.0_armv7l/findutils-4.9.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.9.0_i686/findutils-4.9.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/findutils/4.9.0_x86_64/findutils-4.9.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'e20da96c2bc854e84a1f0f7f6f6876d5d5a36c8add7ebf3d8df94b64299ab25c',
-     armv7l: 'e20da96c2bc854e84a1f0f7f6f6876d5d5a36c8add7ebf3d8df94b64299ab25c',
-       i686: '3246911390cb99054dc438bf67f01bb91453363fb95e2c10a5cf9170d965d1f3',
-     x86_64: '98a700b356ab5b021e51116cdbc80c86ee36168e8583bdd8e49a205938c18562'
+    aarch64: '5da7b4458d47e84fcddd9eddd27d202a3e8abce7e0d92684fbf912b155176075',
+     armv7l: '5da7b4458d47e84fcddd9eddd27d202a3e8abce7e0d92684fbf912b155176075',
+       i686: '7e02631ff1cc208365692179508851a701c695c17b71905d6d079e5148ec1b0f',
+     x86_64: '0611927c689c7732037764964ba8fb2c41f22a9c594c978bd9ae6bf2e259f96e'
   })
 
-  depends_on 'python3'
+  depends_on 'glibc' # R
+  depends_on 'python3' => :build
 
   configure_options '--without-selinux'
   run_tests
