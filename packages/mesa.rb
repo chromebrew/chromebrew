9d22a7a9609fd9e02a1577f9f0b8444e3f048fd1
Satadru Pramanik <satadru@gmail.com>
2023-11-29 22:05:35 -0500

    mesa => 23.3.0

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -3,7 +3,7 @@ require 'package'
 class Mesa < Package
   description 'Open-source implementation of the OpenGL specification'
   homepage 'https://www.mesa3d.org'
-  @_ver = '23.2.1'
+  @_ver = '23.3.0'
   version "#{@_ver}-llvm17"
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
@@ -11,14 +11,14 @@ class Mesa < Package
   git_hashtag "mesa-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.2.1-llvm17_armv7l/mesa-23.2.1-llvm17-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.2.1-llvm17_armv7l/mesa-23.2.1-llvm17-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.2.1-llvm17_x86_64/mesa-23.2.1-llvm17-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.0-llvm17_armv7l/mesa-23.3.0-llvm17-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.0-llvm17_armv7l/mesa-23.3.0-llvm17-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.0-llvm17_x86_64/mesa-23.3.0-llvm17-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '0351537b769df9e57f47d5522ff3b36efe02057d50992db4be5e65936942b0b9',
-     armv7l: '0351537b769df9e57f47d5522ff3b36efe02057d50992db4be5e65936942b0b9',
-     x86_64: '0855c4a56b8cb638aee65161cc8eff7c8d21a400bf9207ae8f80508f4d524009'
+    aarch64: '564d187761f483d75e73f404833cc776cda9b47499244660f0f9efb1e2c71b53',
+     armv7l: '564d187761f483d75e73f404833cc776cda9b47499244660f0f9efb1e2c71b53',
+     x86_64: 'f33bd2e5a89b590ed092173e3b98fe231392c85ec360c0a6f8f617803279acf0'
   })
 
   depends_on 'elfutils' # R
@@ -27,6 +27,7 @@ class Mesa < Package
   depends_on 'gcc_dev' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
+  depends_on 'glibc_lib' #
   depends_on 'glslang' => :build
   depends_on 'libdrm' # R
   depends_on 'libglvnd' # R
