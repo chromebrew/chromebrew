497a4048c93aa1fc109bbf4f605886cae4311489
Ed Reel <edreel@gmail.com>
2023-12-03 08:46:06 -0600

    Telegram 4.9.9 => 4.12.2

--- packages/telegram.rb
+++ packages/telegram.rb
@@ -3,21 +3,32 @@ require 'package'
 class Telegram < Package
   description "Telegram is a messaging app with a focus on speed and security, it's super-fast, simple and free."
   homepage 'https://telegram.org/'
-  version '4.9.9'
+  version '4.12.2'
   license 'BSD, LGPL-2+ and GPL-3-with-openssl-exception'
   compatibility 'x86_64'
-  source_url 'https://updates.tdesktop.com/tlinux/tsetup.4.9.9.tar.xz'
-  source_sha256 '4a316cbd7250c7662b7680f4df5292eab0e3935ca88b16af4fbf3d49de2e394c'
+  source_url 'https://updates.tdesktop.com/tlinux/tsetup.4.12.2.tar.xz'
+  source_sha256 '0b412e0d685d7bf85302d971c893a505e5eb05f0c39a6706566f230058f4ad40'
 
   depends_on 'mesa'
 
   no_compile_needed
   no_shrink
 
+  def self.preflight
+    if Gem::Version.new(LIBC_VERSION.to_s) < Gem::Version.new('2.29')
+      puts "\nOpera requires GLIBC 2.29 and above.".lightred
+      abort "ChromeOS is currently running GLIBC #{LIBC_VERSION}.\n".lightred
+    end
+  end
+
   def self.build
     telegram = <<~EOF
       #!/bin/bash
-      QT_QPA_PLATFORM=wayland-egl DISPLAY= WAYLAND_DISPLAY=wayland-0 Telegram "$@"
+      DISPLAY=
+      GDK_BACKEND=wayland
+      WAYLAND_DISPLAY=wayland-0
+      QT_QPA_PLATFORM=wayland-egl
+      Telegram "$@"
     EOF
     File.write('telegram', telegram)
   end
