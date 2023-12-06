a3c9f6a5b4f8105c8c12b953514f93f5c5131112
Ed Reel <edreel@gmail.com>
2023-12-06 11:55:22 -0600

    Chrome 119.0.6045.199-1 => 120.0.6099.62-1 (#9049)

--- packages/chrome.rb
+++ packages/chrome.rb
@@ -3,11 +3,11 @@ require 'package'
 class Chrome < Package
   description 'Google Chrome is a fast, easy to use, and secure web browser.'
   homepage 'https://www.google.com/chrome'
-  version '119.0.6045.199-1'
+  version '120.0.6099.62-1'
   license 'google-chrome'
   compatibility 'x86_64'
-  source_url 'https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_119.0.6045.199-1_amd64.deb'
-  source_sha256 'f67663c899d7bce3b589979ade676c8f915893296b5a7842a2567eabe5ffc5c5'
+  source_url 'https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_120.0.6099.62-1_amd64.deb'
+  source_sha256 '6e0a762bb8e6399c540ba9393b381adde18e2229f7ac4f1df92232d99342f061'
 
   depends_on 'nspr'
   depends_on 'cairo'
@@ -17,6 +17,13 @@ class Chrome < Package
 
   no_compile_needed
   no_shrink
+
+  def self.preflight
+    if Gem::Version.new(LIBC_VERSION.to_s) < Gem::Version.new('2.29')
+      puts "\nGoogle Chrome requires GLIBC 2.29 and above.".lightred
+      abort "ChromeOS is currently running GLIBC #{LIBC_VERSION}.\n".lightred
+    end
+  end
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
