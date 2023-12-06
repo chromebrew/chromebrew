c4d31b7471995eab2987f6ef489d18d50785c4f2
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-12-06 13:24:57 -0500

    curl => 8.5.0 + update deps (#9052)

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,23 +3,23 @@ require 'buildsystems/autotools'
 class Curl < Autotools
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.se/'
-  version '8.4.0'
+  version '8.5.0'
   license 'curl'
   compatibility 'all'
-  source_url 'https://curl.se/download/curl-8.4.0.tar.xz'
-  source_sha256 '16c62a9c4af0f703d28bda6d7bbf37ba47055ad3414d70dec63e2e6336f2a82d'
+  source_url 'https://curl.se/download/curl-8.5.0.tar.xz'
+  source_sha256 '42ab8db9e20d8290a3b633e7fbb3cec15db34df65fd1015ef8ac1e4723750eeb'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.4.0_armv7l/curl-8.4.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.4.0_armv7l/curl-8.4.0-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.4.0_i686/curl-8.4.0-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.4.0_x86_64/curl-8.4.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.5.0_armv7l/curl-8.5.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.5.0_armv7l/curl-8.5.0-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.5.0_i686/curl-8.5.0-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/curl/8.5.0_x86_64/curl-8.5.0-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '7971313b11a2a2b9107a0a55449c8db7094b5021d0591002e29889d0ae0aca9d',
-     armv7l: '7971313b11a2a2b9107a0a55449c8db7094b5021d0591002e29889d0ae0aca9d',
-       i686: '531b2bfff2f68471fd93e7497e3736d82cec55da150a093ac3a04f1ed2d89d66',
-     x86_64: '5758ac57c392aa7f8afef9a28b85960b75122cdfeeea4516b64b587eaf9fef51'
+    aarch64: 'edcd12652578507a20da5fffd4ba2b33b2fe0f681070f9a334efc97970f08ce8',
+     armv7l: 'edcd12652578507a20da5fffd4ba2b33b2fe0f681070f9a334efc97970f08ce8',
+       i686: 'a07f7983b8448fb12bbe0fc0a74ce0992f566d51488c2e73742ad5443c1c5510',
+     x86_64: 'e48a1d65575457eb66e1b165f285b75b3b6303ae7efef3e7b57da2ff2fcdad1d'
   })
 
   depends_on 'brotli' # R
--- packages/libnghttp2.rb
+++ packages/libnghttp2.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Libnghttp2 < CMake
   description 'library implementing HTTP/2 protocol'
   homepage 'https://nghttp2.org/'
-  @_ver = '1.56.0'
+  @_ver = '1.58.0'
   version "#{@_ver}-py3.12"
   license 'MIT'
   compatibility 'all'
@@ -11,21 +11,21 @@ class Libnghttp2 < CMake
   git_hashtag "v#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.56.0-py3.12_armv7l/libnghttp2-1.56.0-py3.12-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.56.0-py3.12_armv7l/libnghttp2-1.56.0-py3.12-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.56.0-py3.12_i686/libnghttp2-1.56.0-py3.12-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.56.0-py3.12_x86_64/libnghttp2-1.56.0-py3.12-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.58.0-py3.12_armv7l/libnghttp2-1.58.0-py3.12-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.58.0-py3.12_armv7l/libnghttp2-1.58.0-py3.12-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.58.0-py3.12_i686/libnghttp2-1.58.0-py3.12-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libnghttp2/1.58.0-py3.12_x86_64/libnghttp2-1.58.0-py3.12-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'fda02efa2fb3c4e0e9fe57bec96111a49f716718737b1be9aa7461c5ee829035',
-     armv7l: 'fda02efa2fb3c4e0e9fe57bec96111a49f716718737b1be9aa7461c5ee829035',
-       i686: '9e52b30c85ff5d06070c58c168519722f42538a896cfdf7baf8e8975a6626ca4',
-     x86_64: 'ba6bc4a6f2fb3443410076df3e21c83021537c09d1c07ffb53e91fb6f282986a'
+    aarch64: '3d71393f9447a4cba71c42766ea5f53b97f9eda232103feb1066c3cf94443b50',
+     armv7l: '3d71393f9447a4cba71c42766ea5f53b97f9eda232103feb1066c3cf94443b50',
+       i686: 'b9b4d51de06704610900f74811a2132779ae629bfccc5c3b6d89edf913dd817d',
+     x86_64: '1637c2318a5ea05f958c32be7cbc9bead3df2b167f8eaaab78bd89b16929288a'
   })
 
   depends_on 'glibc' # R
-  depends_on 'jansson'
-  depends_on 'jemalloc'
+  depends_on 'jansson' => :build
+  depends_on 'jemalloc' => :build
   depends_on 'libev' => :build
   depends_on 'openssl' # R
   depends_on 'python3' => :build
