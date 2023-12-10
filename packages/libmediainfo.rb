56d87e595c3349d94c99d4b896e4b9e2abaa445b
Ed Reel <edreel@gmail.com>
2023-12-10 07:25:49 -0600

    Update mediainfo packages
    libmediainfo 23.03 => 23.11
    
    mediainfo 23.03 => 23.11
    
    mediainfo_gui 23.03 => 23.11

--- packages/libmediainfo.rb
+++ packages/libmediainfo.rb
@@ -3,23 +3,23 @@ require 'package'
 class Libmediainfo < Package
   description 'MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files.'
   homepage 'http://mediaarea.net/en/MediaInfo'
-  version '23.03'
+  version '23.11'
   license 'BSD-2'
   compatibility 'all'
-  source_url 'https://mediaarea.net/download/binary/libmediainfo0/23.03/MediaInfo_DLL_23.03_GNU_FromSource.tar.bz2'
-  source_sha256 '18cae1c3a6b1186efe8f474fd1e55900521a9cefdebd89b0ea084808f86e44e6'
+  source_url 'https://mediaarea.net/download/binary/libmediainfo0/23.11/MediaInfo_DLL_23.11_GNU_FromSource.tar.xz'
+  source_sha256 '7ffa6728840dd022af261b665dbf0c3cd8419b8001abcdc507801944bcaaa7a6'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.03_armv7l/libmediainfo-23.03-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.03_armv7l/libmediainfo-23.03-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.03_i686/libmediainfo-23.03-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.03_x86_64/libmediainfo-23.03-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.11_armv7l/libmediainfo-23.11-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.11_armv7l/libmediainfo-23.11-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.11_i686/libmediainfo-23.11-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libmediainfo/23.11_x86_64/libmediainfo-23.11-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '151c5672fed748af22ed39063aaff41f2130e29255522d799bd90964065cfa4a',
-     armv7l: '151c5672fed748af22ed39063aaff41f2130e29255522d799bd90964065cfa4a',
-       i686: 'd7e47c544d5d8094bc6646de5c44d9cbd1b3b9ed2376d73c995fbce549bc97d4',
-     x86_64: '4fc5df9a05958c83a55f80d248d0537137537bc6b7e63fdbf66a019c2de5ca0b'
+    aarch64: 'a897d1fc07ea2de848275dadfca354e786809c5e3d0155fed556c0eb0649956c',
+     armv7l: 'a897d1fc07ea2de848275dadfca354e786809c5e3d0155fed556c0eb0649956c',
+       i686: '981a710eff8bd7c45bdb73ba46589e875f6cfbb731274e6baae9c606054bdffa',
+     x86_64: 'ce74c4ff5b7d4e0957858313aadfbe9c0aadd6bb67c13b8424d10323d408a031'
   })
 
   def self.patch
