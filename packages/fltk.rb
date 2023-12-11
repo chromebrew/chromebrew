6e4e88bc8248e1445faa74115af8d40306c21cbc
Ed Reel <edreel@gmail.com>
2023-12-11 06:08:26 -0600

    Fltk 1.3.8 => 1.3.9 (#9067)

--- packages/fltk.rb
+++ packages/fltk.rb
@@ -3,21 +3,21 @@ require 'package'
 class Fltk < Package
   description 'Fast Light Toolkit or FLTK (pronounced "fulltick") is a cross-platform C++ GUI toolkit'
   homepage 'https://www.fltk.org/'
-  version '1.3.8'
+  version '1.3.9'
   license 'FLTK and LGPL-2'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://www.fltk.org/pub/fltk/1.3.8/fltk-1.3.8-source.tar.bz2'
-  source_sha256 '1d9832409fc08c908c64ab40ad9fd8b0e8af5d203651eb9690b810f41e2f412d'
+  source_url 'https://www.fltk.org/pub/fltk/1.3.9/fltk-1.3.9-source.tar.bz2'
+  source_sha256 '103441134915402808fd45424d4061778609437e804334434e946cfd26b196c2'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.8_armv7l/fltk-1.3.8-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.8_armv7l/fltk-1.3.8-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.8_x86_64/fltk-1.3.8-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.9_armv7l/fltk-1.3.9-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.9_armv7l/fltk-1.3.9-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/fltk/1.3.9_x86_64/fltk-1.3.9-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '0eb0447c12dc146a402b31c0b829d8b9bb74c148454a2b5bda1b7546291062ef',
-     armv7l: '0eb0447c12dc146a402b31c0b829d8b9bb74c148454a2b5bda1b7546291062ef',
-     x86_64: 'b6ae65a5a86c7ea727358a0333a261d4f67bfbfc1f51b68c81e099b1f0d2b614'
+    aarch64: 'dba8dab43b88e317c70b674d84cf787ed56f5e7ff81e66b51056be549a85bcec',
+     armv7l: 'dba8dab43b88e317c70b674d84cf787ed56f5e7ff81e66b51056be549a85bcec',
+     x86_64: '3f46ea64f96026a3b8c62f4ea8951a69365c7c2340e9d3c64885978980946159'
   })
 
   depends_on 'sommelier'
