dbe0c5744614836c28ad0eb690870f5e9b946942
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2023-12-13 19:44:26 -0500

    update xwayland, xorg_server (#9083)

--- packages/xwayland.rb
+++ packages/xwayland.rb
@@ -3,21 +3,21 @@ require 'package'
 class Xwayland < Package
   description 'X server configured to work with weston or sommelier'
   homepage 'https://x.org'
-  version '23.2.0'
+  version '23.2.3'
   license 'MIT-with-advertising, ISC, BSD-3, BSD and custom'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.freedesktop.org/xorg/xserver.git'
   git_hashtag "xwayland-#{version}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xwayland/23.2.0_armv7l/xwayland-23.2.0-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xwayland/23.2.0_armv7l/xwayland-23.2.0-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xwayland/23.2.0_x86_64/xwayland-23.2.0-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xwayland/23.2.3_armv7l/xwayland-23.2.3-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xwayland/23.2.3_armv7l/xwayland-23.2.3-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/xwayland/23.2.3_x86_64/xwayland-23.2.3-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'ecfae880ef9c93fe1b773de79b88c6bca42f3ccefa40d9e8e68e5fbbe2b4fcc6',
-     armv7l: 'ecfae880ef9c93fe1b773de79b88c6bca42f3ccefa40d9e8e68e5fbbe2b4fcc6',
-     x86_64: '1a528a4f10404cb1a2be9448bd8d27a5b2be8080b6d71d1d54f5c3ac07f13c6d'
+    aarch64: 'e9d428fd69fe6d5e4fe30c22c492833de98825d74cf9af664cfe177a54750190',
+     armv7l: 'e9d428fd69fe6d5e4fe30c22c492833de98825d74cf9af664cfe177a54750190',
+     x86_64: '9e8803363d834c6a08050e3bf23d9c83e882e2bdf3dd1ce449841944bb449158'
   })
 
   no_env_options
@@ -28,11 +28,12 @@ class Xwayland < Package
   depends_on 'font_util' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
-  depends_on 'glproto'
-  depends_on 'graphite'
+  depends_on 'glproto' => :build
+  depends_on 'graphite' => :build
   depends_on 'libbsd' # R
   depends_on 'libdrm' # R
   depends_on 'libepoxy' # R
+  depends_on 'libglvnd' # R
   depends_on 'libmd' # R
   depends_on 'libtirpc' # R
   depends_on 'libunwind' # Runtime dependency for sommelier
@@ -41,7 +42,7 @@ class Xwayland < Package
   depends_on 'libxdmcp' # R
   depends_on 'libxfont2' # R
   depends_on 'libxfont' # R
-  depends_on 'libxkbcommon'
+  depends_on 'libxkbcommon' => :build
   depends_on 'libxkbfile' # R
   depends_on 'libxshmfence' # R
   depends_on 'libxtrans' => :build
@@ -50,7 +51,6 @@ class Xwayland < Package
   depends_on 'rendercheck' # R
   depends_on 'wayland' # R
   depends_on 'xkbcomp' => :build
-  depends_on 'libglvnd' # R
 
   def self.build
     system "mold -run meson setup #{CREW_MESON_OPTIONS.sub(/(-Dcpp_args='*)(.*)(')/, '')} \
