d625c23b2837224c503711823bd4083d27c1b8bf
Ed Reel <edreel@gmail.com>
2023-12-16 18:37:02 -0600

    Peazip 8.4.0 => 8.6.0

--- packages/peazip.rb
+++ packages/peazip.rb
@@ -3,31 +3,39 @@ require 'package'
 class Peazip < Package
   description 'Free file archiver utility to open and extract RAR, TAR and ZIP archives'
   homepage 'https://peazip.github.io/'
-  version '8.4.0'
   license 'LGPL-3'
-  compatibility 'all'
+  compatibility 'x86_64 aarch64 armv7l'
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://osdn.net/dl/peazip/pea-gtk2-arm.tar.gz'
+    version '5.2.0'
+    source_url 'https://downloads.sourceforge.net/project/peazip/5.2.0/pea-gtk2-arm.tar.gz'
     source_sha256 '6724a5c4e273f086680ac30440a2014b647322d25ed9bba574b7b8edba61abcc'
-  when 'i686'
-    source_url 'https://github.com/peazip/PeaZip/releases/download/8.4.0/peazip_portable-8.4.0.LINUX.GTK2.x86.tar.gz'
-    source_sha256 '9236327c76c06bc1a90c85ce5de798250059316f641658f390b93809b2bcbcb3'
   when 'x86_64'
-    source_url 'https://github.com/peazip/PeaZip/releases/download/8.4.0/peazip_portable-8.4.0.LINUX.GTK2.x86_64.tar.gz'
-    source_sha256 'c3b323cdbeb14779b7f65458b179fd9312b665cc85f195d3429665c7a4b0011e'
+    version '8.6.0'
+    source_url 'https://github.com/peazip/PeaZip/releases/download/8.6.0/peazip_portable-8.6.0.LINUX.GTK2.x86_64.tar.gz'
+    source_sha256 '8d430a24dc6aaf21931aa82e46e91e36fcf82eaeb9a53906b3cce360668a443b'
   end
 
   depends_on 'gtk2'
   depends_on 'sommelier'
 
+  no_compile_needed
+  no_shrink
+
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/peazip"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/application"
-    FileUtils.mv 'res/', "#{CREW_DEST_PREFIX}/share/peazip/"
+    case ARCH
+    when 'aarch64', 'armv7l'
+      FileUtils.mv 'FreeDesktop_integration/peazip.desktop', "#{CREW_DEST_PREFIX}/share/application/"
+    when 'x86_64'
+      FileUtils.mv 'pea', "#{CREW_DEST_PREFIX}/share/peazip/"
+      FileUtils.cp 'res/share/batch/freedesktop_integration/peazip.desktop', "#{CREW_DEST_PREFIX}/share/application/"
+      FileUtils.ln_s "#{CREW_PREFIX}/share/peazip/pea", "#{CREW_DEST_PREFIX}/bin/pea"
+    end
     FileUtils.mv 'peazip', "#{CREW_DEST_PREFIX}/share/peazip/"
-    FileUtils.mv 'FreeDesktop_integration/peazip.desktop', "#{CREW_DEST_PREFIX}/share/application/"
+    FileUtils.mv 'res/', "#{CREW_DEST_PREFIX}/share/peazip/"
     FileUtils.ln_s "#{CREW_PREFIX}/share/peazip/peazip", "#{CREW_DEST_PREFIX}/bin/peazip"
   end
 
