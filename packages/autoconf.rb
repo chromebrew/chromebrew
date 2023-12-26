6e981a0dd069e48517aca90d4f8f5011fdfc6607
Ed Reel <edreel@gmail.com>
2023-12-26 00:26:33 -0600

    Autoconf 2.71 => 2.72 (#9129)

--- packages/autoconf.rb
+++ packages/autoconf.rb
@@ -1,36 +1,27 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Autoconf < Package
+class Autoconf < Autotools
   description 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.'
   homepage 'http://www.gnu.org/software/autoconf/'
-  version '2.71'
+  version '2.72'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/autoconf/autoconf-2.71.tar.xz'
-  source_sha256 'f14c83cfebcc9427f2c3cea7258bd90df972d92eb26752da4ddad81c87a0faa4'
+  source_url 'https://ftpmirror.gnu.org/autoconf/autoconf-2.72.tar.xz'
+  source_sha256 'ba885c1319578d6c94d46e9b0dceb4014caafe2490e437a0dbca3f270a223f5a'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.71_armv7l/autoconf-2.71-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.71_armv7l/autoconf-2.71-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.71_i686/autoconf-2.71-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.71_x86_64/autoconf-2.71-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.72_armv7l/autoconf-2.72-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.72_armv7l/autoconf-2.72-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.72_i686/autoconf-2.72-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/autoconf/2.72_x86_64/autoconf-2.72-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'cf2d3888909044d6dd3109b83d856a7face1d6bded14af644dd09985b5c92272',
-     armv7l: 'cf2d3888909044d6dd3109b83d856a7face1d6bded14af644dd09985b5c92272',
-       i686: '281f27740a18dcf251a36ee1b3067624b98da669e62d4cbd41e6631acba4805a',
-     x86_64: 'bd98d4a7920f4c4e02587d56a11b893436c81d0ce10e6b136532376bf78c8d2d'
+    aarch64: '1afb15ee3b59839b1db68a3a498b226b98b55bbb4080398860c832a5ec9e1afd',
+     armv7l: '1afb15ee3b59839b1db68a3a498b226b98b55bbb4080398860c832a5ec9e1afd',
+       i686: 'feb717b740e3277a2d75430a128690cdd4a722ebc80c9d94ca9b2f6019633d1d',
+     x86_64: 'cc237426fdda757d28d9413e669e7d4003f8062b4e206fb39f5456d86b1e06e1'
   })
 
   depends_on 'perl'
   depends_on 'm4'
-
-  def self.build
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
