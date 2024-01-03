9b63d3de888eca59a20ed327e25c53b1649dedc6
Ed Reel <edreel@gmail.com>
2024-01-03 00:26:24 -0600

    Telegram 4.13.1 => 4.14.2

--- packages/telegram.rb
+++ packages/telegram.rb
@@ -3,12 +3,12 @@ require 'package'
 class Telegram < Package
   description "Telegram is a messaging app with a focus on speed and security, it's super-fast, simple and free."
   homepage 'https://telegram.org/'
-  version '4.13.1'
+  version '4.14.2'
   license 'BSD, LGPL-2+ and GPL-3-with-openssl-exception'
   compatibility 'x86_64'
   min_glibc '2.29'
-  source_url 'https://updates.tdesktop.com/tlinux/tsetup.4.13.1.tar.xz'
-  source_sha256 '237b72d36cb1e0850fb613cde10b8f67df5ea0ae2fecf27279d8d0f84c4afac2'
+  source_url 'https://updates.tdesktop.com/tlinux/tsetup.4.14.2.tar.xz'
+  source_sha256 'bace1b31cfccd1be309a83b274967d40c6863034b0e09775d6d526819dd5140f'
 
   depends_on 'mesa'
 
@@ -16,7 +16,7 @@ class Telegram < Package
   no_shrink
 
   def self.build
-    telegram = <<~EOF
+    File.write 'telegram', <<~EOF
       #!/bin/bash
       DISPLAY=
       GDK_BACKEND=wayland
@@ -24,11 +24,9 @@ class Telegram < Package
       QT_QPA_PLATFORM=wayland-egl
       Telegram "$@"
     EOF
-    File.write('telegram', telegram)
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.install 'Telegram', "#{CREW_DEST_PREFIX}/bin/Telegram", mode: 0o755
     FileUtils.install 'telegram', "#{CREW_DEST_PREFIX}/bin/telegram", mode: 0o755
   end
