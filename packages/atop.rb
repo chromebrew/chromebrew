47226d0256d579f8943f655987ca703f3d9bad76
Ed Reel <edreel@gmail.com>
2024-01-05 01:39:13 -0600

    Add atop package

---
+++ packages/atop.rb
@@ -0,0 +1,41 @@
+require 'package'
+
+class Atop < Package
+  description 'ASCII full-screen performance monitor'
+  homepage 'https://www.atoptool.nl/'
+  version '2.10.0'
+  license 'GPL-2'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://www.atoptool.nl/download/atop-2.10.0.tar.gz'
+  source_sha256 'e7a673cf2c82578e7dd82ecb0dec83fd9ecb30828b2561c28a9fa5aaf75d5f93'
+
+  binary_url({
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atop/2.10.0_armv7l/atop-2.10.0-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atop/2.10.0_armv7l/atop-2.10.0-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/atop/2.10.0_x86_64/atop-2.10.0-chromeos-x86_64.tar.zst'
+  })
+  binary_sha256({
+    aarch64: '99489c26c22d314b131765b7765d638f9ca0281dc012621714df2bdf053f49ab',
+     armv7l: '99489c26c22d314b131765b7765d638f9ca0281dc012621714df2bdf053f49ab',
+     x86_64: 'e043a73b634257e9401be802a1e583e75e1242c971bc83fc81d90dc16c0fcb0c'
+  })
+
+  def self.patch
+    system "sed -i 's,/usr/bin,#{CREW_PREFIX}/bin,g' Makefile"
+    system "sed -i 's,/usr/sbin,#{CREW_PREFIX}/sbin,g' Makefile"
+    system "sed -i 's,/usr/share,#{CREW_PREFIX}/share,g' Makefile"
+    system "sed -i 's,/lib/systemd,#{CREW_PREFIX}/lib/systemd,g' Makefile"
+    system "sed -i 's,/usr/lib/,#{CREW_PREFIX}/lib/,g' Makefile"
+    system "sed -i 's,/usr/lib64/,#{CREW_LIB_PREFIX}/,g' Makefile"
+    system "sed -i 's,/etc/,#{CREW_PREFIX}/etc/,g' Makefile"
+    system "sed -i 's,/var/,#{CREW_PREFIX}/var/,g' Makefile"
+  end
+
+  def self.build
+    system "CFLAGS='-I#{CREW_PREFIX}/include/ncursesw' make"
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'sysvinstall'
+  end
+end
