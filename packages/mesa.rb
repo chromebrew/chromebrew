521b73a7f2438177fbbc6dc2221ac4507d93121e
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-01-11 18:13:22 -0500

    mesa => 23.3.3 (#9169)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -3,7 +3,7 @@ require 'package'
 class Mesa < Package
   description 'Open-source implementation of the OpenGL specification'
   homepage 'https://www.mesa3d.org'
-  @_ver = '23.3.2'
+  @_ver = '23.3.3'
   version "#{@_ver}-llvm17"
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
@@ -11,14 +11,14 @@ class Mesa < Package
   git_hashtag "mesa-#{@_ver}"
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.2-llvm17_armv7l/mesa-23.3.2-llvm17-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.2-llvm17_armv7l/mesa-23.3.2-llvm17-chromeos-armv7l.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.2-llvm17_x86_64/mesa-23.3.2-llvm17-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.3-llvm17_armv7l/mesa-23.3.3-llvm17-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.3-llvm17_armv7l/mesa-23.3.3-llvm17-chromeos-armv7l.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/mesa/23.3.3-llvm17_x86_64/mesa-23.3.3-llvm17-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '804d515c0966940b6ba135576ddf5c599256839a87199b2e2d3e5a918335c9f6',
-     armv7l: '804d515c0966940b6ba135576ddf5c599256839a87199b2e2d3e5a918335c9f6',
-     x86_64: 'c85da5aa4858cd656881d0eac882ae94f3ce64ac7d9ccab0b8b5b57ca4219ef8'
+    aarch64: '81a3d1604ccb950a51cca7c6efa947162034db4e4d850bc00eea22963d733374',
+     armv7l: '81a3d1604ccb950a51cca7c6efa947162034db4e4d850bc00eea22963d733374',
+     x86_64: 'e3f45bcd7f74248f78586d8908b11586ef833de3f0c18c2af634cdaccc747fb7'
   })
 
   depends_on 'elfutils' # R
