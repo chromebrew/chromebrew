6f2a17937ffff9ca7ac92b4eb3e1725aa145acd0
Satadru Pramanik <satadru@gmail.com>
2024-01-11 13:35:54 -0500

    shellcheck => 0.9.0
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/shellcheck.rb
+++ packages/shellcheck.rb
@@ -3,20 +3,20 @@ require 'package'
 class Shellcheck < Package
   description 'Static analysis tool for shell scripts'
   homepage 'https://www.shellcheck.net/'
-  version '0.8.0'
+  version '0.9.0'
   license 'GPL-3.0'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.linux.armv6hf.tar.xz',
-     armv7l: 'https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.linux.armv6hf.tar.xz',
-       i686: 'https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.linux.x86_64.tar.xz',
-     x86_64: 'https://github.com/koalaman/shellcheck/releases/download/v0.8.0/shellcheck-v0.8.0.linux.x86_64.tar.xz'
+    aarch64: 'https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.linux.armv6hf.tar.xz',
+     armv7l: 'https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.linux.armv6hf.tar.xz',
+       i686: 'https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.linux.x86_64.tar.xz',
+     x86_64: 'https://github.com/koalaman/shellcheck/releases/download/v0.9.0/shellcheck-v0.9.0.linux.x86_64.tar.xz'
   })
   source_sha256({
-    aarch64: '17857c8a0a8f4001aa9638732991cbb6e85c4a410500b11e2e0a98d9858afca8',
-     armv7l: '17857c8a0a8f4001aa9638732991cbb6e85c4a410500b11e2e0a98d9858afca8',
-       i686: 'ab6ee1b178f014d1b86d1e24da20d1139656c8b0ed34d2867fbb834dad02bf0a',
-     x86_64: 'ab6ee1b178f014d1b86d1e24da20d1139656c8b0ed34d2867fbb834dad02bf0a'
+    aarch64: '03deed9ded9dd66434ccf9649815bcde7d275d6c9f6dcf665b83391673512c75',
+     armv7l: '03deed9ded9dd66434ccf9649815bcde7d275d6c9f6dcf665b83391673512c75',
+       i686: '700324c6dd0ebea0117591c6cc9d7350d9c7c5c287acbad7630fa17b1d4d9e2f',
+     x86_64: '700324c6dd0ebea0117591c6cc9d7350d9c7c5c287acbad7630fa17b1d4d9e2f'
   })
 
   no_compile_needed
