c1ec5ff71405d18271ebd2cd99dc89a0741c9a4d
Ed Reel <edreel@gmail.com>
2024-01-12 02:48:51 -0600

    Ngrok 3.4.0 => 3.5.0 (#9171)

--- packages/ngrok.rb
+++ packages/ngrok.rb
@@ -3,7 +3,7 @@ require 'package'
 class Ngrok < Package
   description 'ngrok exposes local servers behind NATs and firewalls to the public internet over secure tunnels.'
   homepage 'https://ngrok.com/'
-  version '3.4.0'
+  version '3.5.0'
   license 'Copyright (c) 2015 by Alan Shreve' # Source code not available
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Ngrok < Package
      x86_64: 'https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz'
   })
   source_sha256({
-    aarch64: 'c16b2408a967917aa4d3daa8f14f923ded71c5004fdc27411c06a26413f4906a',
-     armv7l: 'c16b2408a967917aa4d3daa8f14f923ded71c5004fdc27411c06a26413f4906a',
-       i686: 'c817db35544f0572c73cf43eb1a7dd0ebde54d838d95a541e8732f04c9d0e8f8',
-     x86_64: '8b6040b5e35dab4624e4206850d944db385386bde0c0f834077f7ce04cc10387'
+    aarch64: '735994630becf32ce7ac53e0122c2c548eacaf6d4e7a2bea7358cd30eb18fbe0',
+     armv7l: '735994630becf32ce7ac53e0122c2c548eacaf6d4e7a2bea7358cd30eb18fbe0',
+       i686: 'cf1cd238d6c1cef5ec2b7f6e4f2cf029b33e96d12f7751d14405dfc0845e5f80',
+     x86_64: '596e2c9d4573d9640c2b3778984ad9c7e5610f4a914b2ec91163996ecdd77967'
   })
 
   no_compile_needed
