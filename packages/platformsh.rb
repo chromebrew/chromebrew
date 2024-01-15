6633279f3f23adb05bda9505ff543c6b9bdf3b33
Ed Reel <edreel@gmail.com>
2024-01-15 12:03:10 -0600

    Platformsh 4.11.4 => 4.12.1

--- packages/platformsh.rb
+++ packages/platformsh.rb
@@ -3,11 +3,11 @@ require 'package'
 class Platformsh < Package
   description 'The unified tool for managing your Platform.sh services from the command line.'
   homepage 'https://docs.platform.sh/overview/cli.html'
-  version '4.11.4'
+  version '4.12.1'
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://github.com/platformsh/legacy-cli/releases/download/v4.11.4/platform.phar'
-  source_sha256 '5ba4c30a8ba438c0d143a865323cd9b5ab654136ff8d14285b2511b7c71d3880'
+  source_url 'https://github.com/platformsh/legacy-cli/releases/download/v4.12.1/platform.phar'
+  source_sha256 '22cbe0a0be3a3f91e646cbeb6a85fb5e8782b65814ffd6d1ab22bde9a6a87fb7'
 
   depends_on 'php81' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
