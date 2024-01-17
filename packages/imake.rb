c80e529f60054bf53e56a97683eba02ef30b581d
Ed Reel <edreel@gmail.com>
2024-01-17 15:59:59 -0600

    Imake 1.0.8 => 1.0.10 (#9191)

--- packages/imake.rb
+++ packages/imake.rb
@@ -1,36 +1,29 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Imake < Package
+class Imake < Autotools
   description 'A build automation system written for the X Window System.'
   homepage 'https://xorg.freedesktop.org'
-  version '1.0.8'
+  version '1.0.10'
   license 'custom'
   compatibility 'all'
-  source_url 'https://www.x.org/releases/individual/util/imake-1.0.8.tar.gz'
-  source_sha256 '8178a09bfef33ad5f61cb5cb62283df7d3a5682f014507d2e7cfd922485a5c00'
+  source_url 'https://www.x.org/releases/individual/util/imake-1.0.10.tar.xz'
+  source_sha256 '75decbcea8d7b354cf36adc9675e53c4790ee3de56a14bd87b42c8e8aad2ecf5'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.8_armv7l/imake-1.0.8-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.8_armv7l/imake-1.0.8-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.8_i686/imake-1.0.8-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.8_x86_64/imake-1.0.8-chromeos-x86_64.tar.xz'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.10_armv7l/imake-1.0.10-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.10_armv7l/imake-1.0.10-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.10_i686/imake-1.0.10-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/imake/1.0.10_x86_64/imake-1.0.10-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: 'b990112a38f374af2bb69f84201c4c3234075e3ba5c3f91d5af4fe1660edf5af',
-     armv7l: 'b990112a38f374af2bb69f84201c4c3234075e3ba5c3f91d5af4fe1660edf5af',
-       i686: '3068b234517cb02e3e246554f4c390ad0fe72bf11535878094ae791e55ef584b',
-     x86_64: '1cef5aab03fa1906200b88b87d31be2d48e1a3b71d47b779fd4e53ea2a4bf3ee'
+    aarch64: 'e8ff6d0e9a56f8bb730bce9a966b0403cd9b4418cb793f716b30b167590d0009',
+     armv7l: 'e8ff6d0e9a56f8bb730bce9a966b0403cd9b4418cb793f716b30b167590d0009',
+       i686: '5360565b8f907486945ab562c7ee77265aea78ebcb2c0e1ed0ccdc6f36968449',
+     x86_64: '783c7b1402a4602240421e92ca2a8131d691b874c45c5dbc219ec3ef5d0e967c'
   })
 
   depends_on 'xorg_cf_files'
+  depends_on 'xorg_proto'
 
-  def self.build
-    system "./configure #{CREW_OPTIONS} \
-            --sysconfdir=#{CREW_PREFIX}/etc"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options "--sysconfdir=#{CREW_PREFIX}/etc"
 end
