b0178781571ab9bcca79df600572d7ce281d18ad
Ed Reel <edreel@gmail.com>
2024-01-23 13:43:41 -0600

    Zlibpkg 1.3 => 1.3.1 (#9206)

--- packages/zlibpkg.rb
+++ packages/zlibpkg.rb
@@ -3,26 +3,26 @@ require 'buildsystems/cmake'
 class Zlibpkg < CMake
   description 'zlib is a massively spiffy yet delicately unobtrusive compression library.'
   homepage 'https://www.zlib.net/'
-  version '1.3' # Do not use @_ver here, it will break the installer.
+  version '1.3.1' # Do not use @_ver here, it will break the installer.
   # When upgrading zlibpkg, be sure to upgrade minizip in tandem.
   # The following breaks the installer script.
   # puts "#{self} version differs from Minizip version #{Minizip.version}".orange if @_ver != Minizip.version
   license 'zlib'
   compatibility 'all'
-  source_url 'https://www.zlib.net/zlib-1.3.tar.gz'
-  source_sha256 'ff0ba4c292013dbc27530b3a81e1f9a813cd39de01ca5e0f8bf355702efa593e'
+  source_url 'https://www.zlib.net/zlib-1.3.1.tar.gz'
+  source_sha256 '9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23'
 
   binary_url({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3_armv7l/zlibpkg-1.3-chromeos-armv7l.tar.zst',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3_armv7l/zlibpkg-1.3-chromeos-armv7l.tar.zst',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3_i686/zlibpkg-1.3-chromeos-i686.tar.zst',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3_x86_64/zlibpkg-1.3-chromeos-x86_64.tar.zst'
+    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3.1_armv7l/zlibpkg-1.3.1-chromeos-armv7l.tar.zst',
+     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3.1_armv7l/zlibpkg-1.3.1-chromeos-armv7l.tar.zst',
+       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3.1_i686/zlibpkg-1.3.1-chromeos-i686.tar.zst',
+     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/zlibpkg/1.3.1_x86_64/zlibpkg-1.3.1-chromeos-x86_64.tar.zst'
   })
   binary_sha256({
-    aarch64: '26f4076cb8286135fd4f6912fb07d1a12da5f0f4447d3656a9bf76f52b6cf83d',
-     armv7l: '26f4076cb8286135fd4f6912fb07d1a12da5f0f4447d3656a9bf76f52b6cf83d',
-       i686: '42aa72fec758a9ecf8a87a2a7bc5c40eb7a5c988e10dfa86feb5d75e3ca9de95',
-     x86_64: '196e293ec2ee7c06bc8bbd3746001781e9ad2e06bb57b61e442e8fe72f903d08'
+    aarch64: 'a5eb9bbd6dce00c096617c1db16a71dd1f590043c39519e1645d5eb9750ed88f',
+     armv7l: 'a5eb9bbd6dce00c096617c1db16a71dd1f590043c39519e1645d5eb9750ed88f',
+       i686: '1e4ae082c29430d3c081b3f9a8a66bebc9c6cfbf5912476a5ddcfc5c00edfcf2',
+     x86_64: '84abb78b6aabdcb976484173d6b8c53c25495db8a30cd729a584d56d2d66722f'
   })
 
   depends_on 'glibc' # R
