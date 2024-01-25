f5903c72d6ea45c5531387a4468d47f15fce62b1
supechicken <supechicken666@gmail.com>
2024-01-26 04:13:33 +0800

    Bump version

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.42.1'
+CREW_VERSION = '1.42.2'
 
 # kernel architecture
 KERN_ARCH = Etc.uname[:machine]
