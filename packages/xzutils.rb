1e6ae4f210cce79fabe0db7158de92e32f5b2084
Ed Reel <edreel@gmail.com>
2024-01-29 11:13:17 -0600

    Xzutils 5.4.5 => 5.4.6 (#9229)

--- packages/xzutils.rb
+++ packages/xzutils.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Xzutils < Autotools
   description 'XZ Utils is free general-purpose data compression software with a high compression ratio.'
   homepage 'http://tukaani.org/xz/'
-  version '5.4.5'
-  license 'public-domain, LGPL-2.1+ and GPL-2+'
+  version '5.4.6'
+  license 'public-domain70651c6d7d0301739b6ef1c88a116004e2cff05abb8e3e2e83d3dd8a6cf7222f LGPL-2.1+ and GPL-2+'
   compatibility 'all'
-  source_url 'https://tukaani.org/xz/xz-5.4.5.tar.gz'
-  source_sha256 '135c90b934aee8fbc0d467de87a05cb70d627da36abe518c357a873709e5b7d6'
+  source_url 'https://github.com/tukaani-project/xz/releases/download/v5.4.6/xz-5.4.6.tar.xz'
+  source_sha256 'b92d4e3a438affcf13362a1305cd9d94ed47ddda22e456a42791e630a5644f5c'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f8226905f3d3ebb242a704d4af6eb02258e916fc1615fdf9c8522804c62bb32e',
-     armv7l: 'f8226905f3d3ebb242a704d4af6eb02258e916fc1615fdf9c8522804c62bb32e',
-       i686: '35ae2f4c29f9244fd9157b3a6afc6c30cc3615e7ffc45a3c74bcee48de09b809',
-     x86_64: '28b3de7aee6c9996541ef99585f221cff7e3a4c0a376cf355cdf12cfb22b4b77'
+    aarch64: '70651c6d7d0301739b6ef1c88a116004e2cff05abb8e3e2e83d3dd8a6cf7222f',
+     armv7l: '70651c6d7d0301739b6ef1c88a116004e2cff05abb8e3e2e83d3dd8a6cf7222f',
+       i686: 'b87a8080bda5d334f7749fe0d13a26332f38764b41f40f82b364e345efc0b051',
+     x86_64: 'e411899c36c64fa9e7bad354ee2733c33bfcaf9d6a55133ca80e66c10fea9df0'
   })
 
   depends_on 'autoconf_archive' => :build
