a1d9194143b92235cfa62e5e2d06d765b91e1542
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-02-03 11:35:10 -0500

    mesa => 24.0.0, update py3 package versions, libdrm => 2.4.120 (#9253)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_markupsafe.rb
+++ packages/py3_markupsafe.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_markupsafe < Pip
   description 'Markupsafe allows the safe addition of untrusted strings to HTML/XML markup.'
   homepage 'https://markupsafe.palletsprojects.com/'
-  @_ver = '2.0.1'
+  @_ver = '2.1.5'
   version "#{@_ver}-py3.12"
   license 'BSD-3'
   compatibility 'all'
