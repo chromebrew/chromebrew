ff8d1c0bc9d82b0677a439a8f331f4af81f01e82
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-02-03 13:51:53 -0500

    libmicrohttpd => 1.0.0 (#9256)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libmicrohttpd.rb
+++ packages/libmicrohttpd.rb
@@ -1,32 +1,23 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libmicrohttpd < Package
+class Libmicrohttpd < Autotools
   description 'GNU libmicrohttpd is a small C library that is supposed to make it easy to run an HTTP server as part of another application.'
   homepage 'https://www.gnu.org/software/libmicrohttpd/'
-  version '0.9.75'
+  version '1.0.0'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-0.9.75.tar.gz'
-  source_sha256 '9278907a6f571b391aab9644fd646a5108ed97311ec66f6359cebbedb0a4e3bb'
+  source_url 'https://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-1.0.0.tar.gz'
+  source_sha256 'a02792d3cd1520e2ecfed9df642079d44a36ed87167442b28d7ed19e906e3e96'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '206d5948a84b2c884464c974764806441076c64db7ce6c887cc4af3f33683950',
-     armv7l: '206d5948a84b2c884464c974764806441076c64db7ce6c887cc4af3f33683950',
-       i686: 'beaaf1d4036354f8cf5b38e157816f151777c6a9fdfad6b1c88b9c0a3dd61303',
-     x86_64: 'f9d2926975d54adbf19858c8b2a2cb9b6e20368301a24aa168f7a10ec3228717'
+    aarch64: 'b2623ef36b1587c1d3e6f7821d4d9d016b7cadbaf6a0442fd0f055dd69338925',
+     armv7l: 'b2623ef36b1587c1d3e6f7821d4d9d016b7cadbaf6a0442fd0f055dd69338925',
+       i686: '97798a780803f029b9bc17f2f687da43d8f23c1ab6e27e6606f3bf8c16e06abc',
+     x86_64: 'f8dbc8399732197b0152fda7114ae969627b3ae4501f1d20b67669d892d58f8d'
   })
 
   depends_on 'diffutils' => :build
   depends_on 'glibc' # R
   depends_on 'gnutls' # R
-
-  def self.build
-    system "./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
