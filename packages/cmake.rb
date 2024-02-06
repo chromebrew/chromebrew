485c38b3f3acb096eff34ee33118d3275d7047b2
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-02-06 12:55:04 -0500

    cmake => 3.28.3 (#9267)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Cmake < CMake
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  version '3.28.2'
+  version '3.28.3'
   license 'CMake'
   compatibility 'all'
   source_url 'https://github.com/Kitware/CMake.git'
@@ -11,10 +11,10 @@ class Cmake < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ebc9b61a7f41780cea0ea0d11452922889abdfd566e54e76d65a25c21e4af116',
-     armv7l: 'ebc9b61a7f41780cea0ea0d11452922889abdfd566e54e76d65a25c21e4af116',
-       i686: '1f8ecc98f73fd1fb59d6117cc7a65c0f8d4e139d8125c61825ab02b77dcc63b8',
-     x86_64: '43a4ea28bd271b830871297ad7232a8bc295c61eccfe543d4f795d0b5d956271'
+    aarch64: 'f85711e69b752e38f7f45a32ebdf84127bf728b9b6a1ca36713a6604d2ea9738',
+     armv7l: 'f85711e69b752e38f7f45a32ebdf84127bf728b9b6a1ca36713a6604d2ea9738',
+       i686: 'dc4770ea4e8ac55b9ccefcb1ec1e983f0a7659634aad05f5093d4ae66217f579',
+     x86_64: '9e6c251d8ec90b8d526d805b1b2b2c03bac2b34a7ec66ee9c529d5dc30d9465d'
   })
 
   depends_on 'bzip2' => :build
