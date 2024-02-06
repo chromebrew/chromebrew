8c0119f24d6f80bde8b1bb3c00231f6d10bd56c0
Ed Reel <edreel@gmail.com>
2024-02-06 07:19:30 +0000

    Fzf 0.46.0 => 0.46.1

--- packages/fzf.rb
+++ packages/fzf.rb
@@ -3,18 +3,18 @@ require 'package'
 class Fzf < Package
   description 'A command-line fuzzy finder'
   homepage 'https://github.com/junegunn/fzf'
-  version '0.46.0'
+  version '0.46.1'
   license 'MIT and BSD-with-disclosure'
   compatibility 'x86_64 aarch64 armv7l'
   source_url({
-    aarch64: 'https://github.com/junegunn/fzf/releases/download/0.46.0/fzf-0.46.0-linux_armv7.tar.gz',
-     armv7l: 'https://github.com/junegunn/fzf/releases/download/0.46.0/fzf-0.46.0-linux_armv7.tar.gz',
-     x86_64: 'https://github.com/junegunn/fzf/releases/download/0.46.0/fzf-0.46.0-linux_amd64.tar.gz'
+    aarch64: 'https://github.com/junegunn/fzf/releases/download/0.46.1/fzf-0.46.1-linux_armv7.tar.gz',
+     armv7l: 'https://github.com/junegunn/fzf/releases/download/0.46.1/fzf-0.46.1-linux_armv7.tar.gz',
+     x86_64: 'https://github.com/junegunn/fzf/releases/download/0.46.1/fzf-0.46.1-linux_amd64.tar.gz'
   })
   source_sha256({
-    aarch64: 'ccc4b6d8cfa66e35066754a8659f54c1a2bc3920bda063ea1f780ed1afa6d135',
-     armv7l: 'ccc4b6d8cfa66e35066754a8659f54c1a2bc3920bda063ea1f780ed1afa6d135',
-     x86_64: '122da61584a9aef38f23597b0e5f10416ab3a4ad78b6f20d632d03e35ced63c1'
+    aarch64: '594768969ef1ae63cb437eb29f867ed69d0c9d25f083e6b27b1e5b10cb0332a7',
+     armv7l: '594768969ef1ae63cb437eb29f867ed69d0c9d25f083e6b27b1e5b10cb0332a7',
+     x86_64: '6878dc810c0e464d392985e4e9d3525f7c4af346985f4b094ca117ce1bb0838c'
   })
 
   no_compile_needed
