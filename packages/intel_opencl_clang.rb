e18f941e6b0de3cbb38ca0ff8f96d61c0c793ff2
Satadru Pramanik <satadru@gmail.com>
2024-02-07 23:24:09 -0500

    add more packages
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/intel_opencl_clang.rb
@@ -0,0 +1,29 @@
+# Adapted from Arch Linux intel-opencl-clang PKGBUILD at:
+# https://github.com/archlinux/svntogit-community/raw/packages/intel-opencl-clang/trunk/PKGBUILD
+
+require 'buildsystems/cmake'
+
+class Intel_opencl_clang < CMake
+  description 'Wrapper library around clang that can compile OpenCL C kernels to SPIR-V modules'
+  homepage 'https://github.com/intel/opencl-clang'
+  version '17.0.0'
+  license 'llvm'
+  compatibility 'all'
+  source_url 'https://github.com/intel/opencl-clang.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '4a3849c0054664f1a55b424470a44f0b752426c4d97d3b2fbbd87cd598a25fc2',
+     armv7l: '4a3849c0054664f1a55b424470a44f0b752426c4d97d3b2fbbd87cd598a25fc2',
+     x86_64: '3e9bf8dc7e7da207b99f157bfc2da6895fd9b7114336feb1a8b65b3e065fbcf4'
+  })
+
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'llvm17_dev' # R
+  depends_on 'llvm17_lib' # R
+  depends_on 'spirv_llvm_translator' => :build
+
+  cmake_options "-DPREFERRED_LLVM_VERSION='17' -DLLVM_DIR=#{CREW_LIB_PREFIX}/cmake/llvm"
+end
---
+++ packages/vc_intrinsics.rb
@@ -0,0 +1,25 @@
+# Adapted from Arch Linux vc-intrinsics PKGBUILD at:
+# https://github.com/archlinux/svntogit-community/raw/packages/vc-intrinsics/trunk/PKGBUILD
+
+require 'buildsystems/cmake'
+
+class Vc_intrinsics < CMake
+  description 'Set of new intrinsics on top of core LLVM IR instructions that represent SIMD semantics of a program targeting GPU'
+  homepage 'https://github.com/intel/vc-intrinsics'
+  version '0.17.0'
+  license 'MIT'
+  compatibility 'all'
+  source_url 'https://github.com/intel/vc-intrinsics.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '774dec7bc0305d184895ca67978b6b5e2032e4bcf680b46a2bc096f3d6e857f9',
+     armv7l: '774dec7bc0305d184895ca67978b6b5e2032e4bcf680b46a2bc096f3d6e857f9',
+     x86_64: 'f5609153af13fe159c6aa7bf1fa2bcdaebbe9e7fa63de0ee93252a8433db0c21'
+  })
+
+  depends_on 'llvm17_dev' => :build
+
+  cmake_options "-DLLVM_DIR=#{CREW_LIB_PREFIX}/cmake/llvm"
+end
