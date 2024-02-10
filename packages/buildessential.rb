6cee0d0053c056e64677f08a767690f634b332d0
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-02-10 03:58:27 -0500

    Getrealdeps Process Refactor (#9295)
    * wip getrealdeps update
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * refactor to allow for using crew_dest_dir in getrealdeps
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * move install logic for getrealdeps in crew and force option and expand documentation
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Replace mawk with gawk in core.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * move gawk to buildessential
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * fixup crew install
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * bump version
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * cleanup
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * suggested changes
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * wrap everything in a function
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * unnest function
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * adjust require path
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * fix loading const.rb when invoked from crew
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * adjust version text
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,7 +3,7 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage 'SKIP'
-  version '1.27'
+  version '1.28'
   license 'GPL-3+'
   compatibility 'all'
 
@@ -69,6 +69,9 @@ class Buildessential < Package
 
   # COBOL
   # depends_on 'gnucobol'
+
+  # getrealdeps needs gawk
+  depends_on 'gawk'
 
   # Google
   # depends_on 'dart'
