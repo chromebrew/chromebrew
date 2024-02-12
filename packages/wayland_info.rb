59370bd08b3dd65a0d7a2e62c0a41e904fd58960
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-02-13 00:09:30 +1100

    Update mimalloc, wayland-info, libgnome-games-support, gnome-nibbles (#9313)
    * Update mimalloc to 1.8.2
    
    * Update wayland-info to 1.2.0
    
    * Update libgnome-games-support to 1.8.2
    
    * Update gnome-nibbles to 3.8.3

--- packages/wayland_info.rb
+++ packages/wayland_info.rb
@@ -1,33 +1,23 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Wayland_info < Package
+class Wayland_info < Meson
   description 'wayland-info is a utility for displaying information about the Wayland protocols supported by a Wayland compositor.'
   homepage 'https://gitlab.freedesktop.org/wayland/wayland-utils'
-  @_ver = '3fec240801442ae229112d35e070ea71eac06d0b'
-  version "1.1-#{@_ver[0...7]}"
+  version '1.2.0'
   license 'MIT/Expat'
   compatibility 'all'
   source_url 'https://gitlab.freedesktop.org/wayland/wayland-utils.git'
-  git_hashtag @_ver
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '47fffcb289200ca6fadc3e03a6dc6f38aa411c03baa4c1e38186084a5c9fc0ff',
-     armv7l: '47fffcb289200ca6fadc3e03a6dc6f38aa411c03baa4c1e38186084a5c9fc0ff',
-       i686: '48af9f71adf12c8916a8c3fec92a17746a55fe126b1c8f9ea06b0b85449773e9',
-     x86_64: '460ac914559ad023d4e6f26214bff0773b1e04626312917982d8bba31ecfafff'
+    aarch64: 'fe9c7444babd95c443f4807a08a5f5441d7c0827f53e5c2a4a38fd083e6edec1',
+     armv7l: 'fe9c7444babd95c443f4807a08a5f5441d7c0827f53e5c2a4a38fd083e6edec1',
+       i686: '93adf54ced18ea647a10be36a96c1e2b369149d5d6a584b55d30818ab88ab479',
+     x86_64: '74ab29066adc68ed529bee0f1e57314a9244b5d9bb2109c193c52ebf5a709851'
   })
 
   depends_on 'glibc' # R
   depends_on 'wayland' # R
   depends_on 'wayland_protocols' => :build
-
-  def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} builddir"
-    system "#{CREW_NINJA} -C builddir"
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
-  end
 end
