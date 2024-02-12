829aa72d8dadcc7d3249e51f58f23b06828d5275
Satadru Pramanik <satadru@gmail.com>
2024-02-12 13:43:15 -0500

    add descriptive header
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- tools/hello_world_chromebrew.c
+++ tools/hello_world_chromebrew.c
@@ -1,3 +1,6 @@
+/* This file is used by the hello_world_chromebrew build test package.
+   It should be kept in tools/hello_world_chromebrew.c
+*/
 #include <stdio.h>
 int main() {
    printf("Hello Chromebrew!");
