7b02503b049514721e86e59149d1ee7ff6473430
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-02-13 14:22:16 -0500

    py3_libxml2 gets pulled in on builds, so add to buildessential (#9324)
    * py3_libxml2 gets pulled in on builds, so add to buildessential
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * bump version properly
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * fix
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * alphabetize
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/buildessential.rb
+++ packages/buildessential.rb
@@ -3,7 +3,7 @@ require 'package'
 class Buildessential < Package
   description 'A collection of tools essential to compile and build software.'
   homepage 'SKIP'
-  version '1.28'
+  version '1.29'
   license 'GPL-3+'
   compatibility 'all'
 
@@ -141,6 +141,7 @@ class Buildessential < Package
   # depends_on 'py3_build'
   # depends_on 'py3_installer'
   depends_on 'py3_flit_core'
+  depends_on 'py3_libxml2'
   # Pax_utils needs this.
   depends_on 'py3_pyelftools'
 
