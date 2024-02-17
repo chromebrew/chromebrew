2e3b4ef735cfcb21e62b2b44d4425d03e3cbf417
Ed Reel <edreel@gmail.com>
2024-02-17 13:02:30 -0600

    Update php packages (#9351)
    php82 8.2.14 => 8.2.16
    
    php83 8.3.1 => 8.3.3

--- packages/php82.rb
+++ packages/php82.rb
@@ -3,17 +3,17 @@ require 'package'
 class Php82 < Package
   description 'PHP is a popular general-purpose scripting language that is especially suited to web development.'
   homepage 'http://www.php.net/'
-  version '8.2.14'
+  version '8.2.16'
   license 'PHP-3.01'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://www.php.net/distributions/php-8.2.14.tar.xz'
-  source_sha256 '763ecd39fcf51c3815af6ef6e43fa9aa0d0bd8e5a615009e5f4780c92705f583'
+  source_url 'https://www.php.net/distributions/php-8.2.16.tar.xz'
+  source_sha256 '28cdc995b7d5421711c7044294885fcde4390c9f67504a994b4cf9bc1b5cc593'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5238263a9f7579dda27a22dd688ff45d96d5076168910eb3ff8d83936efe7e99',
-     armv7l: '5238263a9f7579dda27a22dd688ff45d96d5076168910eb3ff8d83936efe7e99',
-     x86_64: '03db149e61d36af1d44b1fe8fc83799145784289e043f08c0de7bb58e22bbe68'
+    aarch64: '32fd5420fc14680127a3e8b114e4f6d689b9acb4e6958968fc7d8af9511d15d5',
+     armv7l: '32fd5420fc14680127a3e8b114e4f6d689b9acb4e6958968fc7d8af9511d15d5',
+     x86_64: 'fbf5ffe6daa9e8f40961a8b43727835ff8dae82341a8c5ec106b9f560c87192d'
   })
 
   depends_on 'aspell_en' => :build
