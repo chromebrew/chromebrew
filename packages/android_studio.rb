3aafbd91ddc88d6cd59a06385bb9ca19a8e97a96
995 <67975160+995qa@users.noreply.github.com>
2024-02-18 17:41:20 -0500

    update sha256

--- packages/android_studio.rb
+++ packages/android_studio.rb
@@ -7,7 +7,7 @@ class Android_studio < Package
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url 'https://r2---sn-5ualdnss.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.26/android-studio-2023.1.1.26-linux.tar.gz'
-  source_sha256 '0018e0dfc0dd2921700516f7a2c443377c557788da7fb0a45243ecb4300745be'
+  source_sha256 '977e8a9855414f7d41157f0be0e10fb740c42bd337f763d5d96b3e059780663d'
 
   depends_on 'jdk11'
   depends_on 'xdg_base'
