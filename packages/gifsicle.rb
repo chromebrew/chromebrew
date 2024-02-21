a04b5653e47d7a55ec9ed695b5c8e192cac6f3de
Ed Reel <edreel@gmail.com>
2024-02-21 09:11:51 +0000

    Gifsicle 1.89 => 1.95

--- packages/gifsicle.rb
+++ packages/gifsicle.rb
@@ -1,29 +1,31 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Gifsicle < Package
+class Gifsicle < Autotools
   description 'Gifsicle is a command-line tool for creating, editing, and getting information about GIF images and animations.'
   homepage 'http://www.lcdf.org/gifsicle/'
-  version '1.89'
+  version '1.95'
   license 'GPL-2 and MIT'
   compatibility 'all'
-  source_url 'https://github.com/kohler/gifsicle/archive/v1.89.tar.gz'
-  source_sha256 '9b19ff8d50d88af5a5151eaf9e62beb1dd5b72002e7b7cc3aec9b385780e6b83'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/kohler/gifsicle.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6cbb8a84e03b8446d26d94341d166917ae50919a6148d2ce2da54e103ebf6310',
-     armv7l: '6cbb8a84e03b8446d26d94341d166917ae50919a6148d2ce2da54e103ebf6310',
-       i686: '5e6e15e3ff4093e5b287ceecd60f170cf32275761aa60f706977a746eb58f3a3',
-     x86_64: 'fd897442df51c04763921fbcba22143079eab0e6127ee2c031103f93768c755a'
+    aarch64: 'b408cd75c80c180c68265617e77e795fb74232c379b2fe70576edb0186b4166f',
+     armv7l: 'b408cd75c80c180c68265617e77e795fb74232c379b2fe70576edb0186b4166f',
+       i686: '5a81d9d1719b248848c189045f70dda0664ce01aa04604daadb20f0c9aab76b3',
+     x86_64: '6fc9976eb9f4680a3a3b57adefc027f0b53b27e5186aea1d3e824509e4244c92'
   })
 
-  def self.build
-    system './bootstrap.sh'
-    system './configure --without-x --disable-gifview'
-    system 'make'
+  configure_options '--without-x --disable-gifview'
+
+  def self.patch
+    system "sed -i 's,/usr/bin/perl,#{CREW_PREFIX}/bin/perl,' test/testie"
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  def self.prebuild
+    system './bootstrap.sh'
   end
+
+  run_tests
 end
