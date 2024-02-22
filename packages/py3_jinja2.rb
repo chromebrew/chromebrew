8af5e88edb909bed8843abd7af39396639f00d63
Satadru Pramanik <satadru@gmail.com>
2024-02-21 21:39:54 -0500

    update py3_jinja
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_jinja2.rb
+++ packages/py3_jinja2.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_jinja2 < Pip
   description 'Jinja2 is a very fast and expressive template engine.'
   homepage 'https://jinja.palletsprojects.com/'
-  @_ver = '3.0.2'
+  @_ver = '3.1.3'
   version "#{@_ver}-py3.12"
   license 'BSD-3'
   compatibility 'all'
