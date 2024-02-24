6c8b7fbd7f624ccea50658afdd8c7bed0eb15b23
Satadru Pramanik <satadru@gmail.com>
2024-02-23 19:26:56 -0500

    add binaries for glib, libslirp
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libslirp.rb
+++ packages/libslirp.rb
@@ -13,6 +13,13 @@ class Libslirp < Meson
   git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
+  binary_sha256({
+    aarch64: '60e4b6b8f317dc288d85a36ed194d2211417446457fbab174ef84b76423d7047',
+     armv7l: '60e4b6b8f317dc288d85a36ed194d2211417446457fbab174ef84b76423d7047',
+       i686: '427a983deb897340d2ec30570efedb6d9c2072da5125f25ead6e63b11da67a1f',
+     x86_64: 'bcfe4319c8f6902437cddc15a1dd96c0003dea68f754acad0a0160f499956ec7'
+  })
+
   depends_on 'glib'
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
