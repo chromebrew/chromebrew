12b79afce8fb95ea19b37c0bd3ebb93a739977e5
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-02-24 23:33:09 -0500

    xz => 5.6.0 (#9389)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/xzutils.rb
+++ packages/xzutils.rb
@@ -1,25 +1,24 @@
-require 'buildsystems/autotools'
+require 'buildsystems/cmake'
 
-class Xzutils < Autotools
+class Xzutils < CMake
   description 'XZ Utils is free general-purpose data compression software with a high compression ratio.'
   homepage 'http://tukaani.org/xz/'
-  version '5.4.6'
-  license 'public-domain70651c6d7d0301739b6ef1c88a116004e2cff05abb8e3e2e83d3dd8a6cf7222f LGPL-2.1+ and GPL-2+'
+  version '5.6.0'
+  license 'GPL-3'
   compatibility 'all'
-  source_url 'https://github.com/tukaani-project/xz/releases/download/v5.4.6/xz-5.4.6.tar.xz'
-  source_sha256 'b92d4e3a438affcf13362a1305cd9d94ed47ddda22e456a42791e630a5644f5c'
+  source_url 'https://github.com/tukaani-project/xz/releases/download/v5.6.0/xz-5.6.0.tar.xz'
+  source_sha256 'cdafe1632f139c82937cc1ed824f7a60b7b0a0619dfbbd681dcac02b1ac28f5b'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '70651c6d7d0301739b6ef1c88a116004e2cff05abb8e3e2e83d3dd8a6cf7222f',
-     armv7l: '70651c6d7d0301739b6ef1c88a116004e2cff05abb8e3e2e83d3dd8a6cf7222f',
-       i686: 'b87a8080bda5d334f7749fe0d13a26332f38764b41f40f82b364e345efc0b051',
-     x86_64: 'e411899c36c64fa9e7bad354ee2733c33bfcaf9d6a55133ca80e66c10fea9df0'
+    aarch64: '91369bd0f0fca21a32f25e3105bac709ab7078392c58901e7fce79589b5bff53',
+     armv7l: '91369bd0f0fca21a32f25e3105bac709ab7078392c58901e7fce79589b5bff53',
+       i686: 'b995844c097b8e9b829717a9eb83ec1a4297cee253e3ac7555ff11626ad7abee',
+     x86_64: 'b0a0a69fd76ff9c1f9369e156b2fcb41118e6b820f5bf2a0381c7db7d9db0a13'
   })
 
-  depends_on 'autoconf_archive' => :build
   depends_on 'glibc' # R
 
-  configure_options '--enable-shared'
-  run_tests
+  cmake_options '-DBUILD_SHARED_LIBS=ON \
+                 -DENABLE_SANDBOX=OFF'
 end
