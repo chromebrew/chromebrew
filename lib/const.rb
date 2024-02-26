15b0f0eb59bb839ee450b522c9637c016b926758
Ed Reel <edreel@gmail.com>
2024-02-26 02:03:30 -0600

    Prompt to copy local repo package to crew prior to the build

--- lib/const.rb
+++ lib/const.rb
@@ -1,7 +1,7 @@
 # lib/const.rb
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.43.9'
+CREW_VERSION = '1.43.10'
 
 # kernel architecture
 KERN_ARCH = `uname -m`.chomp
