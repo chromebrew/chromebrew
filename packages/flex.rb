47d18f7268303c24d2a5ebdf8f1703cdcc4d06ab
Satadru Pramanik <satadru@gmail.com>
2024-03-01 17:30:57 -0500

    update flex
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/flex.rb
+++ packages/flex.rb
@@ -1,36 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Flex < Package
+class Flex < Autotools
   description 'Flex (The Fast Lexical Analyzer) is a fast lexical analyser generator tool for generating programs that perform pattern-matching on text.'
   homepage 'https://www.gnu.org/software/flex/'
-  version '2.6.4'
-  license 'FLEX'
+  version '2.6.4-cf9c659'
+  license 'BSD'
   compatibility 'all'
-  source_url 'https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz'
-  source_sha256 'e87aae032bf07c26f85ac0ed3250998c37621d95f8bd748b31f15b33c45ee995'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/westes/flex.git'
+  git_hashtag 'cf9c659c5438961d2dfd9f5326cb2e39b31ab27c'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'bbd90f1a0355f5b938713ce21e0b490ae9519f5d4cb272afb025b84884f443b9',
-     armv7l: 'bbd90f1a0355f5b938713ce21e0b490ae9519f5d4cb272afb025b84884f443b9',
-       i686: '246f92f5cb79802d51596504a7cc8f72367264018409833d389eac07084afd24',
-     x86_64: '805be1fdbf4d9d2ce81a652a064c6972eaf91824bb866867eb57c407dd9c2313'
+    aarch64: 'f403031451ec780b44b760139024954245778f5a49379da19611ba830b3bc5c1',
+     armv7l: 'f403031451ec780b44b760139024954245778f5a49379da19611ba830b3bc5c1',
+       i686: 'e6ffa41d1911e3f7b27855c81ec6f0e44930e8be056492fd9d5c7ebd25b7f4ef',
+     x86_64: '0da49f0a50a07ba056dabaf2ef1b0019a55b042815e88e30af3aea91e025289b'
   })
 
   depends_on 'glibc' # R
-  depends_on 'm4'
+  depends_on 'm4' => :build
   depends_on 'bison' => :build
 
-  def self.build
-    system './configure', "--libdir=#{CREW_LIB_PREFIX}", '--with-pic', '--disable-static', '--enable-shared'
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  configure_options '--with-pic --disable-static --enable-shared'
 end
--- packages/groff.rb
+++ packages/groff.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Groff < Package
+class Groff < Autotools
   description 'Groff (GNU troff) is a typesetting system that reads plain text mixed with formatting commands and produces formatted output.'
   homepage 'https://www.gnu.org/software/groff/'
-  version '1.22.4'
+  version '1.23.0'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/groff/groff-1.22.4.tar.gz'
-  source_sha256 'e78e7b4cb7dec310849004fa88847c44701e8d133b5d4c13057d876c1bad0293'
-  binary_compression 'tar.xz'
+  source_url 'https://git.savannah.gnu.org/git/groff.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '7e64b76b272bc182c6525c7eef2c46515a34a8a5f4c5baa98232e5a89fcf01b1',
@@ -19,26 +19,14 @@ class Groff < Package
 
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
+  depends_on 'netpbm' => :build
   # depends_on 'libice' # R (Not needed for groff binary)
   # depends_on 'libsm' # R (Not needed for groff binary)
   # depends_on 'libx11' # R (Not needed for groff binary)
   # depends_on 'libxaw' # R (Not needed for groff binary)
   # depends_on 'libxmu' # R (Not needed for groff binary)
   # depends_on 'libxt' # R (Not needed for groff binary)
-  depends_on 'uchardet'
+  depends_on 'uchardet' => :build
 
-  def self.build
-    system 'INSTALL_PROGRAM=\'${INSTALL} -s\' ./configure'
-
-    # force to compile in sequential since groff Makefile doesn't work in parallel
-    system 'make', '-j1'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    # system "make", "check"
-  end
+  configure_options '--without-x'
 end
