acfb5f04dabfe49911c637cf4c6df1c5e6075f65
Ed Reel <edreel@gmail.com>
2024-03-01 03:17:47 -0600

    Tcl 8.6.13 => 8.6.14, Tk 8.6.13-1 => 8.6.14

--- packages/tcl.rb
+++ packages/tcl.rb
@@ -3,18 +3,18 @@ require 'package'
 class Tcl < Package
   description 'Tcl (Tool Command Language) is a very powerful but easy to learn dynamic programming language, suitable for a very wide range of uses, including web and desktop applications, networking, administration, testing and many more.'
   homepage 'http://www.tcl.tk/'
-  version '8.6.13'
+  version '8.6.14'
   license 'tcltk'
   compatibility 'all'
   source_url "https://downloads.sourceforge.net/project/tcl/Tcl/#{version}/tcl#{version}-src.tar.gz"
-  source_sha256 '43a1fae7412f61ff11de2cfd05d28cfc3a73762f354a417c62370a54e2caf066'
+  source_sha256 '5880225babf7954c58d4fb0f5cf6279104ce1cd6aa9b71e9a6322540e1c4de66'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2f64a7d1350b05414f52a3e12b5cf2d7d0396619c83810bea71eed443064b83a',
-     armv7l: '2f64a7d1350b05414f52a3e12b5cf2d7d0396619c83810bea71eed443064b83a',
+    aarch64: 'a2f6571a792ae2827c9991d3dac762036659b61b03179deeb02cffa01788d0f2',
+     armv7l: 'a2f6571a792ae2827c9991d3dac762036659b61b03179deeb02cffa01788d0f2',
        i686: 'd36ca1f8dd507f12e5c2d43080bb8b5bd0e4b161efb1f3949fa4e46fd5bf6a46',
-     x86_64: '02b7cc2f6a94226a4400348d62933eb4b297aa52380a4d5303ce865d34a8d639'
+     x86_64: '7c381e1f0fb53435c0d7ffe504e1c217ffc40e702f28779c1ec668f7548e2225'
   })
 
   depends_on 'glibc' # R
