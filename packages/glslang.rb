759ade79feff25d870847ac01091e8cd80062531
Satadru Pramanik <satadru@gmail.com>
2024-03-06 22:48:44 -0500

    test dep additions
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/glslang.rb
+++ packages/glslang.rb
@@ -6,16 +6,16 @@ require 'buildsystems/cmake'
 class Glslang < CMake
   description 'OpenGL and OpenGL ES shader front end and validator'
   homepage 'https://github.com/KhronosGroup/glslang'
-  version '14.0.0'
+  version '14.0.0-bada5c8'
   license 'BSD-3, MIT, BSD-2, Apache-2.0 and NVIDIA'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/KhronosGroup/glslang.git'
-  git_hashtag version
+  git_hashtag 'bada5c87ec6db4441db129d8506742c4a72bd610'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3e751f7dd531c74756289df26b246b60ccbc366acdfe858fed75f0110fed70df',
-     armv7l: '3e751f7dd531c74756289df26b246b60ccbc366acdfe858fed75f0110fed70df',
+    aarch64: '7462ef838ed52f456a820fc0a1475148506a1a230f5f9ab54fc7bcdfe30a35aa',
+     armv7l: '7462ef838ed52f456a820fc0a1475148506a1a230f5f9ab54fc7bcdfe30a35aa',
      x86_64: 'ef0a720e22567f8d14b2ed5dc14b5c489c4b5f5d9327847519b8299bf063fb19'
   })
 
