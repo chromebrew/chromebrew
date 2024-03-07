66ee11db24b74f93a63b9a5e230a9ccd8a4e07ea
Ed Reel <edreel@gmail.com>
2024-03-07 16:31:37 -0600

    Brave 1.63.165 => 1.63.169

--- packages/brave.rb
+++ packages/brave.rb
@@ -3,12 +3,12 @@ require 'package'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '1.63.165'
+  version '1.63.169'
   license 'MPL-2'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-#{version}-linux-amd64.zip"
-  source_sha256 '46d6fa2eab2de1c1eb142798111a1ebb0565dd0557ec561072644fb63b99200b'
+  source_sha256 'bfc038aea36f3684c6d8cc7bf58f0bcf7e8efcd5d7530f4ea7de473ae7625bc5'
 
   no_compile_needed
   no_shrink
