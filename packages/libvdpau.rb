908b94726312eb90c752d6b4c1c1f105dcea7eb7
Satadru Pramanik <satadru@gmail.com>
2024-03-06 23:59:28 -0500

    libvdpau => 1.5
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libvdpau.rb
+++ packages/libvdpau.rb
@@ -3,17 +3,17 @@ require 'buildsystems/meson'
 class Libvdpau < Meson
   description 'VDPAU is the Video Decode and Presentation API for UNIX. It provides an interface to video decode acceleration and presentation hardware present in modern GPUs.'
   homepage 'https://www.freedesktop.org/wiki/Software/VDPAU/'
-  version '1.4'
+  version '1.5'
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://gitlab.freedesktop.org/vdpau/libvdpau/-/archive/1.4/libvdpau-1.4.tar.bz2'
-  source_sha256 '4258824c5a4555ef31de0a7d82b3caf19e75a16a13949f1edafc5f6fb2d33f30'
+  source_url 'https://gitlab.freedesktop.org/vdpau/libvdpau.git'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '73992847475a7d71f6b5a021d0f4e7aeee3477eb0fd75951019e78a01489f5e0',
-     armv7l: '73992847475a7d71f6b5a021d0f4e7aeee3477eb0fd75951019e78a01489f5e0',
-     x86_64: '5bec2dc65194041707e6c39e131d85639314b1cb40925d015309c7061fe046bc'
+    aarch64: '54be014d64a3d0151dd9c77fba9bee49a1e8e6d46b39d25e3bba20abccc70fb3',
+     armv7l: '54be014d64a3d0151dd9c77fba9bee49a1e8e6d46b39d25e3bba20abccc70fb3',
+     x86_64: '7ea9f5cb75ca6a3511ff5da6ead6bd15e1cafb9d991d21000a76f5d06367a02d'
   })
 
   depends_on 'gcc_lib' # R
