a42f923445262ca6b37daeb4391d927db561e98e
Satadru Pramanik <satadru@gmail.com>
2024-03-08 12:39:33 -0500

    Mandb: background cache rebuild from postinstall
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/mandb.rb
+++ packages/mandb.rb
@@ -62,10 +62,10 @@ class Mandb < Package
   end
 
   def self.postinstall
-    puts 'Started mandb cache rebuild. (Errors from this can either be ignored or reported upstream to the relevant package maintainers.)'.yellow
+    puts 'Starting backgrounded mandb cache rebuild. This will be logged to /tmp/man-db-rebuild.log.'.yellow
+    puts '(Errors from this can either be ignored or reported upstream to the relevant package maintainers.)'.yellow
     # See https://gitlab.com/man-db/man-db/-/issues/4
     # Also https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1003089
-    system "MANPATH='' MAN_DISABLE_SECCOMP=1 mandb -C #{CREW_PREFIX}/etc/man_db.conf -psc"
-    puts 'Finished mandb cache rebuild.'.lightgreen
+    system "MANPATH='' MAN_DISABLE_SECCOMP=1 nice -n 20 mandb -C #{CREW_PREFIX}/etc/man_db.conf -psc &>/tmp/man-db-rebuild.log &"
   end
 end
