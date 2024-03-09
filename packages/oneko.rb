aff2e139f7b721d05e7fbd37813d3b5cdef996bd
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-03-09 19:16:28 +1100

    Fix 'gcc_libmakedep' dependency in oneko (#9457)

--- packages/oneko.rb
+++ packages/oneko.rb
@@ -22,7 +22,7 @@ class Oneko < Package
   depends_on 'sommelier'
   depends_on 'imake' => :build
   depends_on 'xorg_cf_files' => :build
-  depends_on 'gcc_libmakedep' => :build
+  depends_on 'gccmakedep' => :build
 
   def self.patch
     # Download extra sources
