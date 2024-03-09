0019ed68b4d12560874740068f1fa782535b53e7
Satadru Pramanik <satadru@gmail.com>
2024-03-09 14:43:47 -0500

    crew: allow override of ARCH const in crew with env variable
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -1,7 +1,7 @@
 # lib/const.rb
 # Defines common constants used in different parts of crew
 
-CREW_VERSION = '1.44.8'
+CREW_VERSION = '1.44.9'
 
 # kernel architecture
 KERN_ARCH = `uname -m`.chomp
@@ -40,7 +40,7 @@ QEMU_EMULATED = !CPU_SUPPORTED_ARCH.include?(KERN_ARCH)
 # This helps with virtualized builds on aarch64 machines which report armv8l when linux32 is run.
 # We also report aarch64 machines as armv7l for now, as we treat them as if they were armv7l.
 # When we have proper aarch64 support, remove this.
-ARCH = %w[aarch64 armv8l].include?(KERN_ARCH) ? 'armv7l' : KERN_ARCH
+ARCH = ENV.fetch('ARCH', %w[aarch64 armv8l].include?(KERN_ARCH) ? 'armv7l' : KERN_ARCH)
 
 # Allow for edge case of i686 install on a x86_64 host before linux32 is
 # downloaded, e.g. in a docker container.
