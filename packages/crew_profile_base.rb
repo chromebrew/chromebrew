f31c05b137293602eeac6683416a0b407b1ac40b
Satadru Pramanik <satadru@gmail.com>
2024-03-11 00:15:14 -0400

    crew_profile_base => 0.0.13
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/crew_profile_base.rb
+++ packages/crew_profile_base.rb
@@ -3,11 +3,11 @@ require 'package'
 class Crew_profile_base < Package
   description 'Crew-profile-base sets up Chromebrew\'s environment capabilities.'
   homepage 'https://github.com/chromebrew/crew-profile-base'
-  version '0.0.12'
+  version '0.0.13'
   license 'GPL-3+'
   compatibility 'all'
   source_url "https://github.com/chromebrew/crew-profile-base/archive/refs/tags/#{version}.tar.gz"
-  source_sha256 '7236cfa4fba6557a6215c1d23ac3d8057490a021a45fdc3e40217d1fe30fe68e'
+  source_sha256 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
 
   no_compile_needed
   print_source_bashrc
