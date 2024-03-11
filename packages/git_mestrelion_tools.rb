efe2bb523f8c5ebe4b8b3f28eeac2d00da705063
Satadru Pramanik <satadru@gmail.com>
2024-03-11 17:25:24 -0400

    rename to git_mestrelion_tools
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/git_mestrelion_tools.rb
+++ packages/git_mestrelion_tools.rb
@@ -3,7 +3,7 @@
 
 require 'package'
 
-class Git_restore_mtime < Package
+class Git_mestrelion_tools < Package
   description 'Assorted git tools, including git-restore-mtime'
   homepage 'https://github.com/MestreLion/git-tools'
   version '2022.12'
@@ -14,10 +14,10 @@ class Git_restore_mtime < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '58e115db241d5b14dcd0e667d23249d077a0f9d507dacfcdbf9d10b0b8714804',
-     armv7l: '58e115db241d5b14dcd0e667d23249d077a0f9d507dacfcdbf9d10b0b8714804',
-       i686: 'cd632b25c4cbcc5799a13d07e47b662d804a7cb9dcd62479025991476cab044d',
-     x86_64: '1a590202433b722a18ed599334356bd25fdfeaf2578dad600221166f3129ea75'
+    aarch64: 'e008b4240a6ff1580dcb0786cc687454608d2a3dd3351da9bb7aa0a17a44cfa7',
+     armv7l: 'e008b4240a6ff1580dcb0786cc687454608d2a3dd3351da9bb7aa0a17a44cfa7',
+       i686: 'ccb3144001c111b47c4851e6b670090e8cfccee02d1a19624865dfdbdeafc47a',
+     x86_64: '9cc57f97e060f2604f8a0e1710ac118869bc27f7487017d8dd58652398fe1346'
   })
 
   depends_on 'git' # L
