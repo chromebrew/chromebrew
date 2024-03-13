8305f0623ae6c98c09e6344c7b5198e212d20766
Satadru Pramanik <satadru@gmail.com>
2024-03-13 18:28:17 -0400

    rebuild gspell, gedit
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gedit.rb
+++ packages/gedit.rb
@@ -11,13 +11,14 @@ class Gedit < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6ea2d20c4559494331e6236f717ce1eb95887942e63cf814d5b214dfd48fbaf8',
-     armv7l: '6ea2d20c4559494331e6236f717ce1eb95887942e63cf814d5b214dfd48fbaf8',
-     x86_64: '5994fc28d453006ddafc6605ef52828c44911c41ad2c1c9f9c045646f19833c0'
+    aarch64: 'a1f180e43690a54b65de8606bf4098c190f8e51a0ffccd357af25308adf4173c',
+     armv7l: 'a1f180e43690a54b65de8606bf4098c190f8e51a0ffccd357af25308adf4173c',
+     x86_64: '8258cfcc36d014370730b4c5c9285ddbbfbe276dc6b8d7e958ee001dfd4519b7'
   })
 
   depends_on 'cairo' # R
   depends_on 'desktop_file_utils' => :build
+  depends_on 'gcc_lib' # R
   depends_on 'gdk_pixbuf' # R
   depends_on 'glibc' # R
   depends_on 'glib' # R
@@ -39,4 +40,8 @@ class Gedit < Meson
   meson_options '-Drequire_all_tests=false \
     -Dgtk_doc=false \
     -Duser_documentation=true'
+
+  def self.postinstall
+    system "glib-compile-schemas #{CREW_PREFIX}/share/glib-2.0/schemas"
+  end
 end
