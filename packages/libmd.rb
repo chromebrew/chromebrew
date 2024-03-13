5f80ee15e5fb822783f2b4d55ec9446e6ac9378d
Satadru Pramanik <satadru@gmail.com>
2024-03-13 10:05:23 -0400

    libmd => 1.1.0, OpenSSH => 9.7p1
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libmd.rb
+++ packages/libmd.rb
@@ -1,34 +1,21 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libmd < Package
+class Libmd < Autotools
   description 'libmd provides message digest functions found on BSD systems.'
   homepage 'https://www.hadrons.org/software/libmd/'
-  version '1.0.4'
+  version '1.1.0'
   license 'BSD-3, BSD-2, ISC, Beerware, public-domain'
   compatibility 'all'
   source_url 'https://git.hadrons.org/git/libmd.git'
   git_hashtag version
-  binary_compression 'tpxz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd7f7fb355eeffac5bed6e547812810e56b722663262e8059048e3e66da1610b4',
-     armv7l: 'd7f7fb355eeffac5bed6e547812810e56b722663262e8059048e3e66da1610b4',
-       i686: 'd07ff3785775f604b95d0bfec9e3fefe054dc8b9131da983c93033ae4b5042c1',
-     x86_64: '672b5ccf0a9b2188476b6c00940f93c2f8615c55694871e399076a6b7527cdba'
+    aarch64: 'ce3644f76c0f6aea9b942e491f24705f7894546a0fa910a7558b95c0fe3f3680',
+     armv7l: 'ce3644f76c0f6aea9b942e491f24705f7894546a0fa910a7558b95c0fe3f3680',
+       i686: '86b5f02127f17691f83745eb8ca16014342e170fdfc6010a06fe69f0f701a847',
+     x86_64: 'c3686286a0de8dae5b6059ef30a262f4586fae65e3196a1b379c840e915b87cd'
   })
 
-  def self.build
-    system 'autoupdate'
-    system 'autoreconf -fiv'
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  depends_on 'glibc' # R
 end
