cb19b5163668fd10481f565323b07cc9e4b47adb
Satadru Pramanik <satadru@gmail.com>
2024-03-14 19:40:11 -0400

    add binaries for adwaita_icon_theme
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/adwaita_icon_theme.rb
+++ packages/adwaita_icon_theme.rb
@@ -11,9 +11,9 @@ class Adwaita_icon_theme < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '565d1fd47c6f327d21fb4e589ac8037484c72424ab71c8e8dfc3da2eb932b437',
-     armv7l: '565d1fd47c6f327d21fb4e589ac8037484c72424ab71c8e8dfc3da2eb932b437',
-     x86_64: '4c49fd45d5be47a4c50dd0c69ff192593b602dfe8042a847dfbe2f468483c2b5'
+    aarch64: 'b9758f5261c1e37870387bd852928a74bb57c1d49482405730b3e9ce58c06a50',
+     armv7l: 'b9758f5261c1e37870387bd852928a74bb57c1d49482405730b3e9ce58c06a50',
+     x86_64: '1ab8a6010d91bb3dd6e4a7f844ed505f8b3f4cd3dba5f607818ac486ad65019b'
   })
 
   depends_on 'cantarell_fonts' # L
