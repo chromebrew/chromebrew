8f73a0b4334fa4625b489735b16362bacf5d60ed
Satadru Pramanik <satadru@gmail.com>
2024-03-14 17:52:01 -0400

    move gnome function to lib/gnome.rb
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ lib/gnome.rb
@@ -0,0 +1,24 @@
+class GnomePostinstall
+  @gnome_postinstall_count = 0
+
+  def self.add
+    # Use the print_last option to allow important messages (like sommelier) to print at the bottom
+    # Usage:
+    # ExitMessage.add 'Last Message', print_last: true
+    @gnome_postinstall_count += 1
+  end
+
+  def self.run
+    if @gnome_postinstall_count >= 1
+      puts 'Running Gnome post_installs...'.orange
+      system "glib-compile-schemas #{CREW_PREFIX}/share/glib-2.0/schemas" if File.file?("#{CREW_PREFIX}/bin/glib-compile-schemas")
+      system "gio-querymodules #{CREW_LIB_PREFIX}/gio/modules" if File.file?("#{CREW_PREFIX}/bin/gio-querymodules")
+      system "gtk-update-icon-cache -ft #{CREW_PREFIX}/share/icons/*", exception: false if File.file?("#{CREW_PREFIX}/bin/gtk-update-icon-cache")
+      system "update-mime-database #{CREW_PREFIX}/share/mime" if File.file?("#{CREW_PREFIX}/bin/update-mime-database")
+      return unless File.file?("#{CREW_PREFIX}/bin/gdk-pixbuf-query-loaders")
+
+      system 'gdk-pixbuf-query-loaders',
+             '--update-cache'
+    end
+  end
+end
