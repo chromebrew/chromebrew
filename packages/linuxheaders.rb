d81696f3710dfc56c068a900c5f452ac85d3cee5
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-03-15 00:35:35 +1100

    Add 6.1 builds to linuxheaders (#9484)
    * Add rsync build dependency to linuxheaders
    
    * Add 6.1 builds to linuxheaders

--- packages/linuxheaders.rb
+++ packages/linuxheaders.rb
@@ -39,6 +39,12 @@ class Linuxheaders < Package
        armv7l: 'f777a2fa6f124a808f32dc3dea4d71d939bd6b1237d6eff6bd61e53a21d60707',
        x86_64: '5b0fa3daf6ac8f2cd1028bd326a6c64f0cac052ba225845ba314e06d12a3de76'
     })
+  when '6.1'
+    binary_sha256({
+      aarch64: '424399cfc8e7f1372003e9766fde2006c4597a70253069a4a3c4a4254a94c7d8',
+       armv7l: '424399cfc8e7f1372003e9766fde2006c4597a70253069a4a3c4a4254a94c7d8',
+       x86_64: '9b3cd996703add46eaf40b8e18b0cfa8a11cf9387f106434d6bfd4e1958b85c0'
+    })
   else
     binary_sha256({
       aarch64: '23b6f70dad19663d42a1bb8707e66eb1b35c4934ef051577701397b4708e8cff',
@@ -46,6 +52,8 @@ class Linuxheaders < Package
        x86_64: '797e7a8369f613b50b049b2352f82d5842a2c9ae766d6d5b1165b90a528ac139'
     })
   end
+
+  depends_on 'rsync' => :build
 
   no_env_options
   no_fhs
@@ -71,7 +79,7 @@ class Linuxheaders < Package
   end
 
   def self.postinstall
-    return unless %w[3.8 4.14 5.4 5.10 5.15].include? CREW_KERNEL_VERSION
+    return unless %w[3.8 4.14 5.4 5.10 5.15 6.1].include? CREW_KERNEL_VERSION
 
     puts 'The installed kernel headers do NOT match the current kernel version.'.orange
     puts 'Please build and install the appropriate kernel headers with:'.orange
