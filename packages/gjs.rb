95e78ad468c27b5cacd657a2b1efc063fb4e5fd4
Satadru Pramanik <satadru@gmail.com>
2024-03-13 20:14:50 -0400

    more updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gjs.rb
+++ packages/gjs.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Gjs < Package
+class Gjs < Meson
   description 'Javascript Bindings for GNOME'
   homepage 'https://gitlab.gnome.org/GNOME/gjs/'
-  version '1.74.0'
+  version '1.79.90'
   license 'MIT and MPL-1.1, LGPL-2+ or GPL-2+'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/gjs.git'
@@ -19,28 +19,18 @@ class Gjs < Package
   depends_on 'cairo' # R
   depends_on 'dbus' => :build
   depends_on 'dconf' => :build
-  depends_on 'glib' # R
-  depends_on 'gobject_introspection' # R
-  depends_on 'js102'
-  depends_on 'libx11' # R
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
+  depends_on 'glib' # R
+  depends_on 'gobject_introspection' # R
   depends_on 'harfbuzz' # R
+  depends_on 'js115' => :build
   depends_on 'libffi' # R
+  depends_on 'libx11' # R
 
-  def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} \
-    -Dinstalled_tests=false \
+  meson_options '-Dinstalled_tests=false \
     -Dskip_dbus_tests=true \
     -Dskip_gtk_tests=true \
     -Dprofiler=disabled \
-    -Dreadline=disabled \
-    builddir"
-    system 'meson configure --no-pager builddir'
-    system 'ninja -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
+    -Dreadline=disabled'
 end
