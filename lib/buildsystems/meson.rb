6542b77484dddcbeaa4a502c9dbe92852b85f716
Satadru Pramanik <satadru@gmail.com>
2024-03-13 21:54:29 -0400

    Also update mime db in gnome postinstall
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/buildsystems/meson.rb
+++ lib/buildsystems/meson.rb
@@ -28,6 +28,7 @@ class Meson < Package
       system "glib-compile-schemas #{CREW_PREFIX}/share/glib-2.0/schemas" if File.file?("#{CREW_PREFIX}/bin/glib-compile-schemas")
       system "gio-querymodules #{CREW_LIB_PREFIX}/gio/modules" if File.file?("#{CREW_PREFIX}/bin/gio-querymodules")
       system "gtk-update-icon-cache -ft #{CREW_PREFIX}/share/icons/*", exception: false if File.file?("#{CREW_PREFIX}/bin/gtk-update-icon-cache")
+      system "update-mime-database #{CREW_PREFIX}/share/mime" if File.file?("#{CREW_PREFIX}/bin/update-mime-database")
       return unless File.file?("#{CREW_PREFIX}/bin/gdk-pixbuf-query-loaders")
 
       system 'gdk-pixbuf-query-loaders',
