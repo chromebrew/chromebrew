b50e97bb56ccd61d7b4272033356a7d1f631c6e5
Satadru Pramanik <satadru@gmail.com>
2024-03-15 11:03:34 -0400

    fixup
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_activesupport.rb
+++ packages/ruby_activesupport.rb
@@ -1,6 +1,6 @@
 require 'buildsystems/ruby'
 
-class Ruby_concurrent_ruby < RUBY
+class Ruby_activesupport < RUBY
   description 'A toolkit of support libraries and Ruby core extensions extracted from the Rails framework.'
   homepage 'https://rubyonrails.org/'
   version '7.1.3.2'
