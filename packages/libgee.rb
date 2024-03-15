6ab127a792713648ebadb0a251ef3fdca573efa3
Satadru Pramanik <satadru@gmail.com>
2024-03-15 15:40:24 -0400

    update libgee
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libgee.rb
+++ packages/libgee.rb
@@ -6,24 +6,25 @@ class Libgee < Autotools
   version '0.20.6'
   license 'LGPL-2.1+'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url "https://gitlab.gnome.org/GNOME/libgee.git"
+  source_url 'https://gitlab.gnome.org/GNOME/libgee.git'
   git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f15038fd3c894a96872bedeac5fc922bbf204900139fbf0bdad9edc42166910d',
-     armv7l: 'f15038fd3c894a96872bedeac5fc922bbf204900139fbf0bdad9edc42166910d',
-     x86_64: '6fbb91042ade5d1fe01e47e87599a883e75f7b7d26304b0e4788684de47f8993'
+    aarch64: '0640bc60d3abf147de56f30c728ef8f6bf85f10be79f42107c6c30c34672d597',
+     armv7l: '0640bc60d3abf147de56f30c728ef8f6bf85f10be79f42107c6c30c34672d597',
+     x86_64: 'afd8b8d1dd1286486ede6bc1e70a1aee9fb48e03ba156e6040d2a2bed93df4c3'
   })
 
-  depends_on 'gobject_introspection' => :build
-
+  depends_on 'vala' => :build
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
   git_fetchtags
   gnome
 
   def self.patch
-    system 'aclocal --install --force'
+    system "sed -i '/GOBJECT_INTROSPECTION/d' configure.ac"
+    system "sed -i '/^if\ HAVE_INTROSPECTION/,/^endif/d' gee/Makefile.am"
     system 'NOCONFIGURE=1 ./autogen.sh'
   end
-
 end
