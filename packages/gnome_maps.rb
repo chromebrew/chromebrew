a9dff3d2c07965a804de88f8f75b42cebd1cc817
Satadru Pramanik <satadru@gmail.com>
2024-03-15 23:19:28 -0400

    gnome_maps
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gnome_maps.rb
+++ packages/gnome_maps.rb
@@ -11,36 +11,35 @@ class Gnome_maps < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-     x86_64: 'f2f638b2c045495f20509ef548617f273f79116a5fd0705e7842e33f4e9ad06c'
+    aarch64: 'd46b5f25779c2f3d261e77bbd6d26d4d8ab3c69879821b95ec7f2d637fa32147',
+     armv7l: 'd46b5f25779c2f3d261e77bbd6d26d4d8ab3c69879821b95ec7f2d637fa32147',
+     x86_64: '204839763a97c30d6c14be1dfad9ea578f9df6e2c6534e14f0b95c7f5bd93fed'
   })
 
-  depends_on 'geocode_glib' => :build
-  depends_on 'geocode_glib2' => :build
+  depends_on 'cairo' # R
+  depends_on 'folks' => :build
   depends_on 'geoclue' => :build
+  depends_on 'geocode_glib2' => :build
+  depends_on 'geocode_glib' => :build
+  depends_on 'gfbgraph' => :build
   depends_on 'gjs' => :build
-  depends_on 'gtk3' => :build
-  depends_on 'gtk4' => :build
+  depends_on 'glibc' # R
+  depends_on 'glib' # R
+  depends_on 'gobject_introspection' => :build
+  depends_on 'gtk4' # R
+  depends_on 'harfbuzz' # R
+  depends_on 'json_glib' # R
   depends_on 'libchamplain' => :build
   depends_on 'libgee' => :build
   depends_on 'libhandy' => :build
-  depends_on 'libshumate' => :build
-  depends_on 'folks' => :build
-  depends_on 'gfbgraph' => :build
-  depends_on 'gobject_introspection' => :build
+  depends_on 'librsvg' # R
+  depends_on 'libshumate' # R
+  depends_on 'libxml2' # R
+  depends_on 'pango' # R
+  depends_on 'rest' # R
   depends_on 'vulkan_headers' => :build
   depends_on 'vulkan_icd_loader'
   depends_on 'yelp_tools' => :build
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
-  depends_on 'libxml2' # R
-  depends_on 'rest' # R
-  depends_on 'cairo' # R
-  depends_on 'harfbuzz' # R
-  depends_on 'gtk4' # R
-  depends_on 'json_glib' # R
-  depends_on 'librsvg' # R
-  depends_on 'libshumate' # R
-  depends_on 'pango' # R
 
   gnome
 
