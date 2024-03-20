d2daabfd39a5f304e2f440984c9803b6064ae5b0
Satadru Pramanik <satadru@gmail.com>
2024-03-20 17:15:24 -0400

    update avahi
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/avahi.rb
+++ packages/avahi.rb
@@ -1,45 +1,44 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Avahi < Package
+class Avahi < Autotools
   description 'Avahi is a system which facilitates service discovery on a local network via the mDNS/DNS-SD protocol suite.'
   homepage 'http://www.avahi.org/'
-  version '0.8-4'
+  version '0.9-rc1'
   license 'LGPL-2.1'
-  compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://github.com/lathiat/avahi/releases/download/v0.8/avahi-0.8.tar.gz'
-  source_sha256 '060309d7a333d38d951bc27598c677af1796934dbd98e1024e7ad8de798fedda'
+  compatibility 'all'
+  source_url 'https://github.com/avahi/avahi.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '44967482fa6955e784d164cbfa534d54c3498705c71a12748fdfc4dc865ea8b6',
-     armv7l: '44967482fa6955e784d164cbfa534d54c3498705c71a12748fdfc4dc865ea8b6',
-     x86_64: '7eebce85f0689787cbc42121b9f0052a7b9153f92a8cbbba233badf6dba9a839'
+       i686: '9d676e30ef2a4086479ef269c3430e2619114a8ddadd1809941edcaf815afead',
+    aarch64: '73dc564fe8de40bfdb67d44fe02152896cd7fdb3251bac125db96552dc0fac58',
+     armv7l: '73dc564fe8de40bfdb67d44fe02152896cd7fdb3251bac125db96552dc0fac58',
+     x86_64: '2b4c8f3324e2130b5e28417c54e8b4a082abbeac55287ecc4ec24dfb1c3dc7ed'
   })
 
-  depends_on 'dbus' # R
+  depends_on 'dbus' unless ARCH == 'i686' # R
   depends_on 'glib' # R
   depends_on 'libcap' => :build
   depends_on 'libdaemon' # R
   depends_on 'libevent' # R
+  depends_on 'xmltoman' => :build
+  depends_on 'expat' # R
+  depends_on 'glibc' # R
+  depends_on 'libcap' # R
+  depends_on 'libssp' # R
+  depends_on 'gdbm' # R
+  depends_on 'libcap' # R
 
-  def self.build
-    system "./configure \
-      #{CREW_OPTIONS} \
-      --with-dbus-sys=#{CREW_PREFIX}/share/dbus-1 \
-      --with-distro=none \
-      --disable-mono \
-      --disable-monodoc \
+  configure_options "#{ARCH == 'i686' ? '--disable-dbus' : "--with-dbus-sys=#{CREW_PREFIX}/share/dbus-1"} \
       --disable-gtk \
       --disable-gtk3 \
+      --disable-libsystemd \
+      --disable-mono \
+      --disable-monodoc \
+      --disable-python \
       --disable-qt3 \
       --disable-qt4 \
       --disable-qt5 \
-      --disable-python \
-      --disable-xmltoman"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+      --with-distro=none"
 end
