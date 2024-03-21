b8c8b44f4cb4ab699512bee9ee0334b8472e939f
Satadru Pramanik <satadru@gmail.com>
2024-03-21 18:38:58 -0400

    wayland_protocols => 1.34
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/wayland_protocols.rb
+++ packages/wayland_protocols.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Wayland_protocols < Meson
   description 'Wayland is a protocol for a compositor to talk to its clients.'
   homepage 'https://wayland.freedesktop.org/'
-  version '1.32'
+  version '1.34'
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.freedesktop.org/wayland/wayland-protocols.git'
@@ -11,9 +11,9 @@ class Wayland_protocols < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ff6f55bfbbb6f5db5beb2e63d4b65b6ce04028bb08edb3e84102ee3e19c9439d',
-     armv7l: 'ff6f55bfbbb6f5db5beb2e63d4b65b6ce04028bb08edb3e84102ee3e19c9439d',
-     x86_64: 'ae4daf867ad5c1f35ede366d9d8fa6a687b2a97517a1e469044e57a306f4274b'
+    aarch64: 'd5a84b3bd77216e1d2a0e90b6258076acdfd641e3eb23b6158755d9ac55c741a',
+     armv7l: 'd5a84b3bd77216e1d2a0e90b6258076acdfd641e3eb23b6158755d9ac55c741a',
+     x86_64: 'c1a4929efb9e12e73d957a391607ef38451a16770671eec0539a479ba6f3f511'
   })
 
   depends_on 'wayland' # L
