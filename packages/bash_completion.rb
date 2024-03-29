f0d0111c9439ec2d28fc14ea948e8764c8481716
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-03-29 19:19:32 -0400

    rebuild xzutils from git tag, update bash_completion, fix i686 build of util_linux 2.40 (#9568)
    * rebuild xzutils from git tag, update bash_completion, fix i686 build of util_linux 2.40
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * fixup
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * update util_linux builds
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * fixup
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/bash_completion.rb
+++ packages/bash_completion.rb
@@ -1,29 +1,19 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Bash_completion < Package
+class Bash_completion < Autotools
   description 'Programmable completion functions for bash'
   homepage 'https://github.com/scop/bash-completion'
-  version '2.11-1'
+  version '2.12.0'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://github.com/scop/bash-completion/archive/refs/tags/2.11.tar.gz'
-  source_sha256 '16adefabf43ec8ffb473704f5724d775c2f47e9f750d7d608f0251ec21fe8813'
+  source_url 'https://github.com/scop/bash-completion.git'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f08fa79964a1f0077f8c523ab3f72afcf287bab069aae3c475ef49a515689508',
-     armv7l: 'f08fa79964a1f0077f8c523ab3f72afcf287bab069aae3c475ef49a515689508',
-       i686: '3a8cd80d863f72a2f88fa27e4cdce054c9133361cfcea693c8fa23ddd3b692d1',
-     x86_64: 'ff8dce6b4da22853b8e558cbf814ed86fd1793b39a0123f64569cd09f80af27f'
+    aarch64: 'aae12d4f233a342bbc17695221f8398bc7ccc23945533782816a6ad562acc4c3',
+     armv7l: 'aae12d4f233a342bbc17695221f8398bc7ccc23945533782816a6ad562acc4c3',
+       i686: '8134858d86d86ed6ff327c8f36368533a66c29665c4266821b882c9eb8e974c4',
+     x86_64: 'ead9142a41cc77b373a313c64a3a7e4e4f5da17cdda2cf8767efdbdc30a9c8b9'
   })
-
-  def self.build
-    system 'autoreconf -i'
-    system "./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
--- packages/util_linux.rb
+++ packages/util_linux.rb
@@ -3,31 +3,26 @@ require 'buildsystems/meson'
 class Util_linux < Meson
   description 'essential linux tools'
   homepage 'https://www.kernel.org/pub/linux/utils/util-linux/'
-  case ARCH
-  when 'i686'
-    # 2.40 needs bpf, which isn't available on i686.
-    # See https://github.com/util-linux/util-linux/issues/2874
-    version '2.39.3-py3.12'
-  else
-    version '2.40-py3.12'
-  end
+  version '2.40-py3.12'
   license 'GPL-2, LGPL-2.1, BSD-4, MIT and public-domain'
   compatibility 'all'
   source_url 'https://github.com/util-linux/util-linux.git'
-  git_hashtag "v#{version[/^.*?(?=-)/]}"
+  git_hashtag '6ff6f5b6ffd9323cc568def2169225dd84bc3a0c'
+  # git_hashtag "v#{version[/^.*?(?=-)/]}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'df71ebcfffc64a80e2fb2956c8a834f7da22393ddafd1f12425952d5a66e0675',
-     armv7l: 'df71ebcfffc64a80e2fb2956c8a834f7da22393ddafd1f12425952d5a66e0675',
-       i686: '082eca25c5a7c714bdfbb906952ecf948458ce1a93edc47a140b6ca9024e4304',
-     x86_64: '23ee3e2a1d1d702a1b3b0e65a22fd0e7589825dc77585e4c3a6006193df9976e'
+    aarch64: 'dce99bef03d7d431d5fa6535f6266c6ed00e1d7fde578116d369e559cab8b3cb',
+     armv7l: 'dce99bef03d7d431d5fa6535f6266c6ed00e1d7fde578116d369e559cab8b3cb',
+       i686: 'a117bc46b9ead409f5de3070a4a4634b14127b63bc909c3e34362113c02c4140',
+     x86_64: '97fa74dc9cb10aaf6818eba7342f69dc34fa144d1add60c433fb20a9eb4962fd'
   })
 
   depends_on 'bash_completion' # R
   depends_on 'bzip2' # R
   depends_on 'eudev' if ARCH == 'x86_64' # (for libudev.h)
   depends_on 'filecmd' # R
+  depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'libcap_ng' # R
   depends_on 'libeconf' # R
@@ -43,7 +38,16 @@ class Util_linux < Meson
   depends_on 'zstd' # R
 
   year2038 = ARCH == 'x86_64' ? '' : '-Dallow-32bit-time=true'
-
+  lsfd = ARCH == 'i686' ? '-Dbuild-lsfd=disabled' : ''
   # Kill conflicts with coreutils.
-  meson_options "-Dbuild-kill=disabled #{year2038}"
+  meson_options "-Dbuild-kill=disabled #{year2038} #{lsfd}"
+
+  def self.patch
+    return unless ARCH == 'i686'
+
+    # 2.40 needs bpf, which isn't available on i686.
+    # See https://github.com/util-linux/util-linux/issues/2874
+    downloader 'https://github.com/util-linux/util-linux/pull/2879.patch', '531a8c0904d6b86c0c9adc2a41d657ce1d75cb63e2a408a51cb14fe3d9ffa4dc'
+    system 'patch -Np1 -i 2879.patch'
+  end
 end
