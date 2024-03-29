a76c146105dcff832869516bde61e9eca6615647
Satadru Pramanik <satadru@gmail.com>
2024-03-29 09:38:50 -0400

    Add util_linux update for non-i686 architectures
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/util_linux.rb
+++ packages/util_linux.rb
@@ -3,7 +3,14 @@ require 'buildsystems/meson'
 class Util_linux < Meson
   description 'essential linux tools'
   homepage 'https://www.kernel.org/pub/linux/utils/util-linux/'
-  version '2.40-py3.12'
+  case ARCH
+  when 'i686'
+    # 2.40 needs bpf, which isn't available on i686.
+    # See https://github.com/util-linux/util-linux/issues/2874
+    version '2.39.3-py3.12'
+  else
+    version '2.40-py3.12'
+  end
   license 'GPL-2, LGPL-2.1, BSD-4, MIT and public-domain'
   compatibility 'all'
   source_url 'https://github.com/util-linux/util-linux.git'
@@ -36,46 +43,7 @@ class Util_linux < Meson
   depends_on 'zstd' # R
 
   year2038 = ARCH == 'x86_64' ? '' : '-Dallow-32bit-time=true'
-  # Needs bpf, which isn't available on i686.
-  lsfd = ARCH == 'i686' ? '-Dbuild-lsfd=disabled' : ''
+
   # Kill conflicts with coreutils.
-  meson_options "-Dbuild-kill=disabled #{year2038} #{lsfd}"
-
-  def self.patch
-    File.write 'lsfd.patch', <<~LSFD_PATCHEOF
-      diff -Npaur a/meson.build b/meson.build
-      --- a/meson.build	2024-03-28 23:37:49.623330230 -0400
-      +++ b/meson.build	2024-03-28 23:39:08.982615866 -0400
-      @@ -2703,6 +2703,7 @@ endif
-       mq_libs = []
-       mq_libs += cc.find_library('rt', required : true)
-
-      +opt = not get_option('build-lsfd').disabled()
-       exe = executable(
-         'lsfd',
-         lsfd_sources,
-      @@ -2712,7 +2713,7 @@ exe = executable(
-         dependencies : mq_libs,
-         install_dir : usrbin_exec_dir,
-         install : true)
-      -if not is_disabler(exe)
-      +if opt and not is_disabler(exe)
-         exes += exe
-         manadocs += ['misc-utils/lsfd.1.adoc']
-       endif
-      diff -Npaur a/meson_options.txt b/meson_options.txt
-      --- a/meson_options.txt	2024-03-28 23:39:28.782438124 -0400
-      +++ b/meson_options.txt	2024-03-28 23:40:38.789810977 -0400
-      @@ -53,6 +53,8 @@ option('build-fdisks', type : 'feature',
-              description : 'build fdisk(8), sfdisk(8) and cfdisk(8)')
-       option('build-mount', type : 'feature',
-              description : 'build mount(8) and umount(8)')
-      +option('build-lsfd', type : 'feature',
-      +       description : 'build lsfd')
-       option('build-losetup', type : 'feature',
-              description : 'build losetup')
-       option('build-zramctl', type : 'feature',
-    LSFD_PATCHEOF
-    system 'patch -Np1 -i lsfd.patch'
-  end
+  meson_options "-Dbuild-kill=disabled #{year2038}"
 end
