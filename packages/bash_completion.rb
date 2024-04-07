2f7c50d704f23bcb48482651cde43551e8a4167b
Ed Reel <edreel@gmail.com>
2024-04-07 17:06:43 -0500

    Bash_completion 2.12.0 => 2.13.0

--- packages/bash_completion.rb
+++ packages/bash_completion.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Bash_completion < Autotools
   description 'Programmable completion functions for bash'
   homepage 'https://github.com/scop/bash-completion'
-  version '2.12.0'
+  version '2.13.0'
   license 'GPL-2+'
   compatibility 'all'
   source_url 'https://github.com/scop/bash-completion.git'
@@ -11,9 +11,9 @@ class Bash_completion < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aae12d4f233a342bbc17695221f8398bc7ccc23945533782816a6ad562acc4c3',
-     armv7l: 'aae12d4f233a342bbc17695221f8398bc7ccc23945533782816a6ad562acc4c3',
-       i686: '8134858d86d86ed6ff327c8f36368533a66c29665c4266821b882c9eb8e974c4',
-     x86_64: 'ead9142a41cc77b373a313c64a3a7e4e4f5da17cdda2cf8767efdbdc30a9c8b9'
+    aarch64: '89eada9fa2c7cefa756f84092b7263ad8bc0d847793cad334443e9929cfdb76d',
+     armv7l: '89eada9fa2c7cefa756f84092b7263ad8bc0d847793cad334443e9929cfdb76d',
+       i686: 'b687949c680d08cddd59c7e01d882db0f8006800dd01dcf38948d78fb9e30f25',
+     x86_64: 'c8794d54d7e2d25c987cc1b5a50957eedac78a65965d0270e6d8aabadd61a710'
   })
 end
