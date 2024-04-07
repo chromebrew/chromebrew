f0beb446724eaa9e9ceb1ab92b4a4d53b1f05a18
Ed Reel <edreel@gmail.com>
2024-04-07 17:01:54 -0500

    Libxext 1.3.4 => 1.3.6 (#9611)

--- packages/libxext.rb
+++ packages/libxext.rb
@@ -1,32 +1,29 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libxext < Package
+class Libxext < Autotools
   description 'library for the X window system'
   homepage 'https://x.org'
-  version '1.3.4'
+  version '1.3.6'
   license 'MIT'
-  compatibility 'all'
-  source_url 'https://www.x.org/releases/individual/lib/libXext-1.3.4.tar.gz'
-  source_sha256 '8ef0789f282826661ff40a8eef22430378516ac580167da35cc948be9041aac1'
-  binary_compression 'tar.xz'
+  compatibility 'x86_64 aarch64 armv7l'
+  source_url 'https://www.x.org/releases/individual/lib/libXext-1.3.6.tar.xz'
+  source_sha256 'edb59fa23994e405fdc5b400afdf5820ae6160b94f35e3dc3da4457a16e89753'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '71402bff06db86bd9b08030c7615868a8bfcf3ed52ef9369d066267c1827c118',
-     armv7l: '71402bff06db86bd9b08030c7615868a8bfcf3ed52ef9369d066267c1827c118',
-       i686: '1aee8625004cb1872da572c6ca962717d421c8245e2c79c418425c86babd1cab',
-     x86_64: '9024bf186472eeb3ed2fe0ea7c77716f76c9860248418d8f804f600d5b5c2704'
+    aarch64: '5ed26f102a7fefc71259c3849ba8549d6cc11b6af74a73bafe333c8ee880120b',
+     armv7l: '5ed26f102a7fefc71259c3849ba8549d6cc11b6af74a73bafe333c8ee880120b',
+     x86_64: '0d9a949182965e1eb29f6c27c20b74dd30adef38cad9f169d09e662730777ae0'
   })
 
-  depends_on 'llvm16_lib' => :build
+  depends_on 'glibc' # R
+  depends_on 'libx11' # R
+  depends_on 'libxau' # R
+  depends_on 'libxcb' # R
+  depends_on 'libxdmcp' # R
+  depends_on 'llvm18_lib' => :build
+  depends_on 'xmlto' => :build
+  depends_on 'xorg_proto' # R
 
-  def self.build
-    ENV['CFLAGS'] = '-fuse-ld=lld'
-    ENV['CXXFLAGS'] = '-fuse-ld=lld'
-    system "./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  run_tests
 end
