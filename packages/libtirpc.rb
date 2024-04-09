f69fe227f716bfcb040e2a1255d3a73c9252d39c
Satadru Pramanik <satadru@gmail.com>
2024-04-09 17:38:30 -0400

    Update libtirpc, add back libnsl
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libtirpc.rb
+++ packages/libtirpc.rb
@@ -1,32 +1,23 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libtirpc < Package
+class Libtirpc < Autotools
   description 'Libtirpc is a port of Suns Transport-Independent RPC library to Linux.'
   homepage 'https://sourceforge.net/projects/libtirpc'
-  version '1.3.3'
+  version '1.3.4'
   license 'GPL-2'
   compatibility 'all'
   source_url "http://downloads.sourceforge.net/project/libtirpc/libtirpc/#{version}/libtirpc-#{version}.tar.bz2"
-  source_sha256 '6474e98851d9f6f33871957ddee9714fdcd9d8a5ee9abb5a98d63ea2e60e12f3'
+  source_sha256 '1e0b0c7231c5fa122e06c0609a76723664d068b0dba3b8219b63e6340b347860'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '35c1c157bcddb9807781d1a6ccd8d1b3499f06fda5b3d6789c60f2b54000fbd2',
-     armv7l: '35c1c157bcddb9807781d1a6ccd8d1b3499f06fda5b3d6789c60f2b54000fbd2',
-       i686: '7d8e3e54442fa216539c32e024d5d4aefa203c042ff9081f34690b98cb714849',
-     x86_64: '05420165d33ce9802715424596c55fa0f32e71c4662fa03253b889ebcf7deade'
+    aarch64: 'e50a429b7df4cacb0895f5a0c387934e0fb12ec38733019db46236bcd5c19dc8',
+     armv7l: 'e50a429b7df4cacb0895f5a0c387934e0fb12ec38733019db46236bcd5c19dc8',
+       i686: 'ab43a0bd0f003df12fce5a0fc64ba445ad040cf63832fc587e602cf40d96cfbf',
+     x86_64: '010df2b2eab43e5f646b768042d98e846553fcbfe4a5356c886d9c031921222a'
   })
 
   depends_on 'e2fsprogs' # R
   depends_on 'glibc' # R
   depends_on 'krb5' # R
-
-  def self.build
-    system "./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
