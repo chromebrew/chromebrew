a303b961c225719a7371d54a8fb0ae6063ad4d86
Ed Reel <edreel@gmail.com>
2024-04-09 10:07:26 -0500

    Telegram 4.15.2 => 4.16.4

--- packages/telegram.rb
+++ packages/telegram.rb
@@ -3,12 +3,12 @@ require 'package'
 class Telegram < Package
   description "Telegram is a messaging app with a focus on speed and security, it's super-fast, simple and free."
   homepage 'https://telegram.org/'
-  version '4.15.2'
+  version '4.16.4'
   license 'BSD, LGPL-2+ and GPL-3-with-openssl-exception'
   compatibility 'x86_64'
   min_glibc '2.29'
-  source_url 'https://updates.tdesktop.com/tlinux/tsetup.4.15.2.tar.xz'
-  source_sha256 '33943e8bb36738807aed761d318536f436bb6bed4e577f35f85a0e288629fa09'
+  source_url 'https://updates.tdesktop.com/tlinux/tsetup.4.16.4.tar.xz'
+  source_sha256 'b80138f728ed14a953138aa79dab3679d1e6bf9ce5f05b5e2da859cc2dc7ba8a'
 
   depends_on 'mesa'
 
