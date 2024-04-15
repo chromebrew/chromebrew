930e34c05a4b96b51ad7584f195ec29fc6becdf2
Satadru Pramanik <satadru@gmail.com>
2024-04-14 21:02:47 -0400

    revert to using env variable as flag for bash trap enablement
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -116,6 +116,7 @@ CREW_DEST_HOME          = File.join(CREW_DEST_DIR, HOME)
 CREW_CACHE_DIR          = ENV.fetch('CREW_CACHE_DIR', "#{HOME}/.cache/crewcache")
 CREW_CACHE_BUILD        = ENV.fetch('CREW_CACHE_BUILD', '0').eql?('1')
 CREW_CACHE_FAILED_BUILD = ENV.fetch('CREW_CACHE_FAILED_BUILD', '0').eql?('1')
+CREW_SOURCE_BASHRC_ACTIVATED = ENV.fetch('CREW_SOURCE_BASHRC_ACTIVATED', '0').eql?('1')
 
 # Set CREW_NPROC from environment variable, `distcc -j`, or `nproc`.
 CREW_NPROC = if File.file?("#{CREW_PREFIX}/bin/distcc")
