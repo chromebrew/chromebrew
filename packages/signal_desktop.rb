f84d482d0a27fd470429377b9f069b0590b59efe
Ed Reel <edreel@gmail.com>
2024-04-17 15:46:33 -0500

    Signal_desktop 7.4.0 => 7.5.0 (#9667)

--- packages/signal_desktop.rb
+++ packages/signal_desktop.rb
@@ -3,12 +3,12 @@ require 'package'
 class Signal_desktop < Package
   description 'Private Messenger for Windows, Mac, and Linux'
   homepage 'https://signal.org/'
-  version '7.4.0'
+  version '7.5.0'
   license 'AGPL-3.0'
   compatibility 'x86_64'
   min_glibc '2.29'
-  source_url 'https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_7.4.0_amd64.deb'
-  source_sha256 'f5af18f2771ab729cab290bfab4631516263fa710daf502bc02640f4d83c3319'
+  source_url 'https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_7.5.0_amd64.deb'
+  source_sha256 'c0b66cdea93d4af07df939cdbdb8b393696a34e0cf84098a69c14ba59621eee1'
 
   no_compile_needed
   no_shrink
