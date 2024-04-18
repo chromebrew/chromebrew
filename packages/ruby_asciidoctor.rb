5186619db323849391bcd1799c7edf84785d7bf8
Satadru Pramanik <satadru@gmail.com>
2024-04-18 18:17:37 -0400

    update some ruby gems
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_asciidoctor.rb
+++ packages/ruby_asciidoctor.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_asciidoctor < RUBY
   description 'A fast text processor & publishing toolchain for converting AsciiDoc to HTML5, DocBook & more.'
   homepage 'https://asciidoctor.org/'
-  version '2.0.20-ruby-3.3'
+  version '2.0.22-ruby-3.3'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
--- packages/ruby_concurrent_ruby.rb
+++ packages/ruby_concurrent_ruby.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_concurrent_ruby < RUBY
   description 'Modern concurrency tools for Ruby. Inspired by Erlang, Clojure, Scala, Haskell, F#, C#, Java, and classic concurrency patterns.'
   homepage 'https://github.com/ruby-concurrency/concurrent-ruby'
-  version '1.2.2-ruby-3.3'
+  version '1.2.3-ruby-3.3'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
