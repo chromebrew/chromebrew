f2e8281b2659439dfefb67f4cfd79ca629ea8fdb
Zopolis4 <creatorsmithmdt@gmail.com>
2024-04-21 11:25:07 +1000

    Actually remove rubocop code from crew upload

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.47.4'
+CREW_VERSION = '1.47.5'
 
 # kernel architecture
 KERN_ARCH = Etc.uname[:machine]
