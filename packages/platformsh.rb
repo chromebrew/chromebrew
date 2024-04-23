fcb9d017d3012a4cb54fb33873ec8a27b51bbd28
Ed Reel <edreel@gmail.com>
2024-04-23 18:23:24 -0500

    Platformsh 4.18.0 => 4.18.1

--- packages/platformsh.rb
+++ packages/platformsh.rb
@@ -3,13 +3,15 @@ require 'package'
 class Platformsh < Package
   description 'The unified tool for managing your Platform.sh services from the command line.'
   homepage 'https://docs.platform.sh/overview/cli.html'
-  version '4.18.0'
+  version '4.18.1'
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://github.com/platformsh/legacy-cli/releases/download/v4.18.0/platform.phar'
-  source_sha256 '7564e35386b53ee2223b7d553e39946a3d9b1f59ea36d965b1f003d7bbff91cb'
+  source_url 'https://github.com/platformsh/legacy-cli/releases/download/v4.18.1/platform.phar'
+  source_sha256 '5933787472548eb6d298deb226c0b05db132825e68386d3f41fe14efd28ea8a2'
 
   depends_on 'php83' unless File.exist? "#{CREW_PREFIX}/bin/php"
+
+  no_compile_needed
 
   def self.install
     FileUtils.install 'platform.phar', "#{CREW_DEST_PREFIX}/bin/platform", mode: 0o755
