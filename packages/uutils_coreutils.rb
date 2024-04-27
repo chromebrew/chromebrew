31d27c27efc22f3bca3c5ff2c55d21be4f827b4a
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-04-27 16:45:40 -0400

    uutils_coreutils => 0.0.26 (#9731)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/uutils_coreutils.rb
+++ packages/uutils_coreutils.rb
@@ -6,7 +6,7 @@ require 'package'
 class Uutils_coreutils < Package
   description 'Cross-platform Rust rewrite of the GNU coreutils'
   homepage 'https://github.com/uutils/coreutils'
-  version '0.0.23'
+  version '0.0.26'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/uutils/coreutils.git'
@@ -14,10 +14,10 @@ class Uutils_coreutils < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '06be1802c7cf4725b824ae6872a875c84b605a03101ab6fa68df93a8eba3e5c2',
-     armv7l: '06be1802c7cf4725b824ae6872a875c84b605a03101ab6fa68df93a8eba3e5c2',
-       i686: '01ba93e28719d0587ea73b7d9e7e926b0c9850a31c9634d3dce6cc5ab85d7954',
-     x86_64: '75f1b5c7f4a66e689df88e29a4703c69fdca3dca7191bc23e79aa461c827341d'
+    aarch64: '2c8ea8a36890ded8d53bd89f43bdd1271eb05ee063af4f735974fea50e758e6a',
+     armv7l: '2c8ea8a36890ded8d53bd89f43bdd1271eb05ee063af4f735974fea50e758e6a',
+       i686: 'c0d5006e8e6943673fecb7a2afa7d98839ab00019e51e6590728a18f7714ce18',
+     x86_64: '7338dea58b174490a88287dc306b929fd17f91c314f758006fcdb48875d8390f'
   })
 
   depends_on 'rust' => :build
