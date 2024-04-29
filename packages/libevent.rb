19a15d9be3e4818d0b3e6096c546028a1ecb6309
Satadru Pramanik <satadru@gmail.com>
2024-04-29 16:55:06 -0400

    Update tmux, libevent, rebuild tmate
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libevent.rb
+++ packages/libevent.rb
@@ -3,18 +3,18 @@ require 'buildsystems/cmake'
 class Libevent < CMake
   description 'The libevent API provides a mechanism to execute a callback function when a specific event occurs on a file descriptor or after a timeout has been reached.'
   homepage 'http://libevent.org/'
-  version '2.2-f2b3ce6'
+  version '2.2-64decd4'
   license 'BSD'
   compatibility 'all'
   source_url 'https://github.com/libevent/libevent.git'
-  git_hashtag 'f2b3ce6b5587efb206bc74298d7a835372ce2caf'
+  git_hashtag '64decd48e20f6d20d6f510aa75ab05861fd3d51c'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6a8f0cb7221c99bcafac9e859b29f8f2e2598fc27a4298d846996100dafc2556',
-      armv7l: '6a8f0cb7221c99bcafac9e859b29f8f2e2598fc27a4298d846996100dafc2556',
-        i686: '163311e020dc2c2bf9825518e990efbd3373a2a7f8251d625b63bca1e4547baa',
-      x86_64: '6885dced405129dd97bf398517d35dbdab2797b9c9f117dcbdc2a78764e67b05'
+    aarch64: 'd8e80ad3a816756ed0a10d2b036f756dbee87cefc8eada17a2e81184ec79343b',
+      armv7l: 'd8e80ad3a816756ed0a10d2b036f756dbee87cefc8eada17a2e81184ec79343b',
+        i686: '79aeefd04d639e1a57bd3e0acdb91196ccb5bd326f25d10d8b2b4da5b22cf8cc',
+      x86_64: '5fee31e3b068d46c7cddb56c830893ac2bedcac948f0ef2c3f655f6c5fc2b448'
   })
 
   depends_on 'glibc' # R
