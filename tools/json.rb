9e6e0885e2e4a081a33283e679a997e84a2f425a
Zopolis4 <creatorsmithmdt@gmail.com>
2024-05-01 09:09:43 +1000

    Fix Repology.json

--- tools/json.rb
+++ tools/json.rb
@@ -9,6 +9,9 @@ require_relative '../lib/package'
 output = []
 
 Dir.glob('../packages/*.rb').each do |filename|
+  # docbook_xml runs code on loading that does not work when chromebrew is not installed.
+  # It shouldn't be doing that, but work around it for now.
+  next if filename.include?('docbook')
   pkg = Package.load_package(filename)
   next if pkg.is_fake?
   output << { name: File.basename(filename, '.rb').gsub('_', '-'), description: pkg.description, homepage: pkg.homepage, version: pkg.version, license: pkg.license, compatibility: pkg.compatibility }
