3febdc10914f589070ac3cf17cb26cbc42109e17
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-05-06 10:35:40 -0400

    Update wayland_protocols, fix wayland_info compat, have crew identify incompatible packages when throwing incompatibility error (#9754)
    * update wayland_protocols, rename wayland_info and fix compat, have crew identify incompatible packages when throwing incompatibility error
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Revert naming changes
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * rebuild binaries
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * fixup packages.yaml
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Add fake package for wayland_utils
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.47.6'
+CREW_VERSION = '1.47.7'
 
 # kernel architecture
 KERN_ARCH = Etc.uname[:machine]
--- packages/wayland_info.rb
+++ packages/wayland_info.rb
@@ -1,21 +1,20 @@
 require 'buildsystems/meson'
 
 class Wayland_info < Meson
-  description 'wayland-info is a utility for displaying information about the Wayland protocols supported by a Wayland compositor.'
+  description 'Provides wayland-info, a standalone version of the weston-info utility for displaying information about the Wayland protocols supported by a Wayland compositor.'
   homepage 'https://gitlab.freedesktop.org/wayland/wayland-utils'
   version '1.2.0'
   license 'MIT/Expat'
   compatibility 'all'
-  min_glibc '2.34'
   source_url 'https://gitlab.freedesktop.org/wayland/wayland-utils.git'
   git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'fe9c7444babd95c443f4807a08a5f5441d7c0827f53e5c2a4a38fd083e6edec1',
-     armv7l: 'fe9c7444babd95c443f4807a08a5f5441d7c0827f53e5c2a4a38fd083e6edec1',
-       i686: '93adf54ced18ea647a10be36a96c1e2b369149d5d6a584b55d30818ab88ab479',
-     x86_64: '74ab29066adc68ed529bee0f1e57314a9244b5d9bb2109c193c52ebf5a709851'
+    aarch64: '2e9a0b23b60ad89d46a579661f88e92de74d23f0400b7b89c0873088255c64ba',
+     armv7l: '2e9a0b23b60ad89d46a579661f88e92de74d23f0400b7b89c0873088255c64ba',
+       i686: '8700677d65661f0c0264a628edf3b7d549973fea47a09ef079c6ccc9f1baacf7',
+     x86_64: '3dd8ce56664f9affa0d56bf7c3a36f6d99b6f7a723edbc4694540b4df5624281'
   })
 
   depends_on 'glibc' # R
