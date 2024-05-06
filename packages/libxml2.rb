b882f4afbdd6a6a58714b87df6c635eb84669974
Satadru Pramanik <satadru@gmail.com>
2024-05-06 15:43:41 -0400

    update libxml2, libunwind
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libxml2.rb
+++ packages/libxml2.rb
@@ -1,20 +1,20 @@
-require 'buildsystems/autotools'
+require 'buildsystems/cmake'
 
-class Libxml2 < Autotools
+class Libxml2 < CMake
   description 'Libxml2 is the XML C parser and toolkit developed for the Gnome project.'
   homepage 'http://xmlsoft.org/'
-  version '2.12.5-icu74.2'
+  version '2.12.6-icu74.2'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.12.5/libxml2-v2.12.5.tar.bz2'
-  source_sha256 '6ac1511e1f659940708254c430b51a7ba6b88a8d1d46b03e5a1e0e264bd87679'
+  source_url 'https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.12.6/libxml2-v2.12.6.tar.bz2'
+  source_sha256 '69f08b81d4532d285ceea4cdb017eb2d948cb87c34c1d64248a92be90e84132d'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '77dce5287d36803131e59f1e26b999812c2fdd3cbde52e1fea6a532fca2f51f1',
-     armv7l: '77dce5287d36803131e59f1e26b999812c2fdd3cbde52e1fea6a532fca2f51f1',
-       i686: 'edc340c49ab96eac98150297b9604e545c153108346b45091acc2c02eb364c71',
-     x86_64: '3fd4e2262836228624e2de226889993be7561d133cdc7e51010ae21e78a721ab'
+    aarch64: 'a4214df62d178e10c6254541a1aebf9c5df77914d5f41cdcc8f6a6841439a76c',
+     armv7l: 'a4214df62d178e10c6254541a1aebf9c5df77914d5f41cdcc8f6a6841439a76c',
+       i686: '62f44c0ebcc1a37a4ef38f069f645795ddeaa89707962bff617c4de348884755',
+     x86_64: '7a1f6e5704b59ff0a535a2aa4c245be9dc038bd59dd4d581d6ce2eddf94bcaca'
   })
 
   depends_on 'gcc_lib' # R
@@ -29,23 +29,12 @@ class Libxml2 < Autotools
   def self.patch
     # Fix encoding.c:1961:31: error: ‘TRUE’ undeclared (first use in this function)
     system "for f in $(grep -rl 'TRUE)'); do sed -i 's,TRUE),true),g' $f; done"
-  end
-
-  configure_options '--enable-shared \
-      --enable-static \
-      --with-pic \
-      --without-python \
-      --without-lzma \
-      --with-zlib \
-      --with-icu \
-      --with-threads \
-      --with-history'
-
-  def self.check
     # Remove EBCDIC test since it fails.
     # Check https://mail.gnome.org/archives/xml/2010-April/msg00010.html for details.
-    system 'rm', 'test/ebcdic_566012.xml'
-
-    system 'make', 'check'
+    FileUtils.rm 'test/ebcdic_566012.xml'
   end
+
+  cmake_options '-DLIBXML2_WITH_PYTHON=OFF \
+    -DLIBXML2_WITH_LZMA=OFF \
+    -DLIBXML2_WITH_ICU=ON'
 end
