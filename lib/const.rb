ab636e159a8a2dfa4ce50a11fa54994a865f8d1e
Satadru Pramanik <satadru@gmail.com>
2024-05-06 10:45:20 -0400

    move bashrc reload at_exit out of default code path to only happen during [re]installs.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.47.7'
+CREW_VERSION = '1.47.8'
 
 # kernel architecture
 KERN_ARCH = Etc.uname[:machine]
