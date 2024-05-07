f323de888a0b2f4f6d377bd213e51ce991716577
Ed Reel <edreel@gmail.com>
2024-05-07 14:25:09 -0500

    Util_linux 2.40-py3.12 => 2.40.1-py3.12 (#9758)

--- packages/util_linux.rb
+++ packages/util_linux.rb
@@ -3,19 +3,18 @@ require 'buildsystems/meson'
 class Util_linux < Meson
   description 'essential linux tools'
   homepage 'https://www.kernel.org/pub/linux/utils/util-linux/'
-  version '2.40-py3.12'
+  version '2.40.1-py3.12'
   license 'GPL-2, LGPL-2.1, BSD-4, MIT and public-domain'
   compatibility 'all'
   source_url 'https://github.com/util-linux/util-linux.git'
-  git_hashtag '6ff6f5b6ffd9323cc568def2169225dd84bc3a0c'
-  # git_hashtag "v#{version[/^.*?(?=-)/]}"
+  git_hashtag "v#{version.split('-').first}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'dce99bef03d7d431d5fa6535f6266c6ed00e1d7fde578116d369e559cab8b3cb',
-     armv7l: 'dce99bef03d7d431d5fa6535f6266c6ed00e1d7fde578116d369e559cab8b3cb',
-       i686: 'a117bc46b9ead409f5de3070a4a4634b14127b63bc909c3e34362113c02c4140',
-     x86_64: '97fa74dc9cb10aaf6818eba7342f69dc34fa144d1add60c433fb20a9eb4962fd'
+    aarch64: 'b4dd547d28140af6fd9e891787e146fe073122c617b32f2cfe89c9fb47b4b8e8',
+     armv7l: 'b4dd547d28140af6fd9e891787e146fe073122c617b32f2cfe89c9fb47b4b8e8',
+       i686: '658cf6859fc4cddf46e95451be4d77ae09cea0c7aa8ed89b80106abe9132ef64',
+     x86_64: '4af0730dfa0f8b458f44a0cb1831df26bc75d49a1031c89cc521ddd0c1b7eeda'
   })
 
   depends_on 'bash_completion' # R
@@ -38,16 +37,7 @@ class Util_linux < Meson
   depends_on 'zstd' # R
 
   year2038 = ARCH == 'x86_64' ? '' : '-Dallow-32bit-time=true'
-  lsfd = ARCH == 'i686' ? '-Dbuild-lsfd=disabled' : ''
-  # Kill conflicts with coreutils.
-  meson_options "-Dbuild-kill=disabled #{year2038} #{lsfd}"
-
-  def self.patch
-    return unless ARCH == 'i686'
-
-    # 2.40 needs bpf, which isn't available on i686.
-    # See https://github.com/util-linux/util-linux/issues/2874
-    downloader 'https://github.com/util-linux/util-linux/pull/2879.patch', '531a8c0904d6b86c0c9adc2a41d657ce1d75cb63e2a408a51cb14fe3d9ffa4dc'
-    system 'patch -Np1 -i 2879.patch'
-  end
+  # Avoid incompatibilities and conflicts with coreutils.
+  disabled_builds = ARCH == 'i686' ? '-Dbuild-kill=disabled -Dbuild-blkzone=disabled -Dbuild-lsfd=disabled -Dprogram-tests=false' : '-Dbuild-kill=disabled'
+  meson_options "#{year2038} #{disabled_builds}"
 end
