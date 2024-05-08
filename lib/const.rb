008aee3e5d759a51f2a40567e56db0bb32261677
Zopolis4 <creatorsmithmdt@gmail.com>
2024-05-09 09:00:01 +1000

    Properly handle fake packages in commands/files.rb

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.47.9'
+CREW_VERSION = '1.48.0'
 
 # kernel architecture
 KERN_ARCH = Etc.uname[:machine]
