45fe6cb4c6577c3c82fe6e71b785e21dc2896c35
Ed Reel <edreel@gmail.com>
2024-05-08 04:02:27 -0500

    Fix fake package removal bug (#9766)

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.47.8'
+CREW_VERSION = '1.47.9'
 
 # kernel architecture
 KERN_ARCH = Etc.uname[:machine]
