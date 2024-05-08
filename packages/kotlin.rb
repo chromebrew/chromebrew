1fdc0c551ab56fbe1b2eb7f3dc913859d3743f78
Ed Reel <edreel@gmail.com>
2024-05-08 01:42:46 -0500

    Kotlin 1.7.21 => 1.9.24

--- packages/kotlin.rb
+++ packages/kotlin.rb
@@ -3,11 +3,11 @@ require 'package'
 class Kotlin < Package
   description 'Statically typed programming language for modern multiplatform applications'
   homepage 'https://kotlinlang.org/'
-  version '1.7.21'
+  version '1.9.24'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://github.com/JetBrains/kotlin/releases/download/v1.7.21/kotlin-compiler-1.7.21.zip'
-  source_sha256 '8412b31b808755f0c0d336dbb8c8443fa239bf32ddb3cdb81b305b25f0ad279e'
+  source_url 'https://github.com/JetBrains/kotlin/releases/download/v1.9.24/kotlin-compiler-1.9.24.zip'
+  source_sha256 'eb7b68e01029fa67bc8d060ee54c12018f2c60ddc438cf21db14517229aa693b'
 
   depends_on 'openjdk8'
 
