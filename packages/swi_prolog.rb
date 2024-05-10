db785e78c1411828ea042b4d1816d9431a386d7a
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-05-11 02:02:01 +1000

    Upgrade swi-prolog to 9.3.5 (#9778)

--- packages/swi_prolog.rb
+++ packages/swi_prolog.rb
@@ -1,49 +1,34 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Swi_prolog < Package
-  description 'SWI-Prolog offers a comprehensive free Prolog environment. Since its start in 1987, SWI-Prolog development has been driven by the needs of real world applications. SWI-Prolog is widely used in research and education as well as commercial applications. Join over a million users who have downloaded SWI-Prolog.'
-  homepage 'https://www.swi-prolog.org/'
-  version '8.2.4'
+class Swi_prolog < CMake
+  description 'SWI-Prolog offers a comprehensive free Prolog environment.'
+  homepage 'https://www.swi-prolog.org'
+  version '9.3.5'
   license 'BSD-2'
-  compatibility 'i686,x86_64'
-  source_url "https://www.swi-prolog.org/download/stable/src/swipl-#{version}.tar.gz"
-  source_sha256 'f4bcc78437f9080ab089762e9e6afa7071df7f584c14999b92b9a90a4efbd7d8'
-  binary_compression 'tar.xz'
+  compatibility 'all'
+  source_url 'https://github.com/SWI-Prolog/swipl-devel.git'
+  git_hashtag "V#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    i686: 'd8985a0a160baca9d6565d4177a43a840450ef4bb96eefae476a51ce27fec063',
-    x86_64: 'cbb431b0bd28fa1c581071aeb1eeda79b70b4ec3c28c2d07b509054e810cb62b'
+    aarch64: 'c6045d6ad6e8e4f21e30ec4e937dfbcdff93dee67de1c297ca251e58bba52c21',
+     armv7l: 'c6045d6ad6e8e4f21e30ec4e937dfbcdff93dee67de1c297ca251e58bba52c21',
+       i686: 'c87d2ad03d051055420b8c5b65f0d6b26b93e933b1095a77e16928332f6f1d76',
+     x86_64: '86cf759a7fbb1ffcc2c625b97efbf7e1c8ce0c2946b04ed324412b5bea85290f'
   })
 
-  depends_on 'libyaml'
+  depends_on 'glibc' # R
+  depends_on 'gmp' # R
   depends_on 'libarchive'
   depends_on 'libdb'
-  depends_on 'jdk8'
+  depends_on 'libedit' # R
+  depends_on 'libyaml'
+  depends_on 'openssl' # R
+  depends_on 'pcre2' # R
+  depends_on 'python3' # R
+  depends_on 'readline' # R
+  depends_on 'zlibpkg' # R
 
-  def self.build
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "cmake -G Ninja \
-      #{CREW_CMAKE_OPTIONS} \
-      -DCURSES_CURSES_LIBRARY=#{CREW_LIB_PREFIX}/libncurses.so.6 \
-      -DCURSES_INCLUDE_PATH=#{CREW_PREFIX}/include/ncurses  \
-      -DCMAKE_C_FLAGS=-flto=auto \
-      -DCMAKE_CXX_FLAGS=-flto=auto \
-      -DCMAKE_EXE_LINKER_FLAGS=-flto=auto \
-      -DINSTALL_TESTS=ON \
-      .."
-      system '../scripts/pgo-compile.sh'
-      system 'ninja'
-    end
-  end
-
-  # def self.check
-  # Dir.chdir 'builddir' do
-  # system 'ctest -j 2'
-  # end
-  # end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
+  pre_cmake_options "CFLAGS+=' -I#{CREW_PREFIX}/include/ncurses'"
+  cmake_options '-DCMAKE_BUILD_TYPE=PGO'
 end
