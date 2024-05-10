3d5785823c81981691ae82cb152cfc5748c5c1f6
Satadru Pramanik <satadru@gmail.com>
2024-05-09 22:15:51 -0400

    fixup package file and update compatibility and deps
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/wkhtmltox.rb
+++ packages/wkhtmltox.rb
@@ -3,17 +3,22 @@ require 'package'
 class Wkhtmltox < Package
   description 'Convert HTML to PDF using Webkit (QtWebKit)'
   homepage 'https://wkhtmltopdf.org/'
-  version '0.12.4-1'
+  version '0.12.4'
   license 'LGPL-3'
-  compatibility 'i686,x86_64'
+  compatibility 'x86_64'
   source_url({
-    i686: 'https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-i386.tar.xz',
     x86_64: 'https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz'
   })
   source_sha256({
-    i686: '4087b264ec860ab0b0f9b7299ef733fc51e8e39f42047a06724b7450016ab9b8',
     x86_64: '049b2cdec9a8254f0ef8ac273afaf54f7e25459a273e27189591edc7d7cf29db'
   })
+
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'libx11' # R
+  depends_on 'libxext' # R
+  depends_on 'libxrender' # R
+  depends_on 'zlibpkg' # R
 
   def self.install
     FileUtils.mkdir_p CREW_DEST_PREFIX
@@ -22,5 +27,13 @@ class Wkhtmltox < Package
     FileUtils.cp_r Dir.glob('lib/*'), CREW_DEST_LIB_PREFIX
     FileUtils.cp_r 'share/', CREW_DEST_PREFIX
     FileUtils.cp_r 'include/', CREW_DEST_PREFIX
+  end
+
+  def self.postinstall
+    ExitMessage.add <<~EOT1.lightpurple
+      Note that the wkkhtmlto{image,pdf} programs are vulnerable as per
+      CVE-2020-21365 and CVE-2022-35583. As such, please be careful with
+      html files that you convert with this package.
+    EOT1
   end
 end
