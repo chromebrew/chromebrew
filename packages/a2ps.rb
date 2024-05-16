2e4f300d7abac2ec0051788becce554dec20de26
Zopolis4 <creatorsmithmdt@gmail.com>
2024-05-16 11:50:54 +1000

    a2ps 4.15 => 4.15.6

--- packages/a2ps.rb
+++ packages/a2ps.rb
@@ -3,22 +3,24 @@ require 'buildsystems/autotools'
 class A2ps < Autotools
   description 'GNU a2ps is an Any to PostScript filter.'
   homepage 'https://www.gnu.org/software/a2ps/'
-  version '4.15'
+  version '4.15.6'
   license 'GPL-3'
-  compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/a2ps/a2ps-4.15.tar.gz'
-  source_sha256 'a5adc5a9222f98448a57c6b5eb6948b72743eaf9a30c67a134df082e99c76652'
+  # Builds are currently broken on i686: https://savannah.gnu.org/bugs/?65903
+  compatibility 'aarch64 armv7l x86_64'
+  # Bootstrapping from git fails: https://savannah.gnu.org/bugs/index.php?65830
+  source_url 'https://ftpmirror.gnu.org/a2ps/a2ps-4.15.6.tar.gz'
+  source_sha256 '87ff9d801cb11969181d5b8cf8b65e65e5b24bb0c76a1b825e8098f2906fbdf4'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3611cd68968a4c8402ff4adb4a7c89579956543d085186199d64baf7d305605a',
-     armv7l: '3611cd68968a4c8402ff4adb4a7c89579956543d085186199d64baf7d305605a',
-       i686: '30d1558dbd5121b970c2dbfdee87d7d31de9a1de6faea090290fffecb1582171',
-     x86_64: '8002a71b0d233f1243a9443ab0a3a5d2d7c894aab357514d244dbf6575b9cb0d'
+    aarch64: 'ebecfeff108d16f671271dadcb423d43f5342c2a19fd705ff18e38673de86d32',
+     armv7l: 'ebecfeff108d16f671271dadcb423d43f5342c2a19fd705ff18e38673de86d32',
+     x86_64: '1e7a0c86ad72bbddcbcb85e01d3d848109ddc9e5680004206a2fc5428d0e261c'
   })
 
-  depends_on 'gperf'
-  depends_on 'filecmd'
+  depends_on 'bdwgc'
+  depends_on 'libpaper'
+  depends_on 'py3_pspdfutils'
 
-  configure_options "--localstatedir=#{CREW_PREFIX}/tmp"
+  run_tests
 end
