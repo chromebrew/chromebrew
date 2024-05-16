bb295ba61a552f1aef7caf505e24efa4e3b38a05
Ed Reel <edreel@gmail.com>
2024-05-16 18:48:21 +0000

    Add openjdk22 package

--- packages/openjdk22.rb
+++ packages/openjdk22.rb
@@ -1,14 +1,14 @@
 require 'package'
 
-class Openjdk21 < Package
+class Openjdk22 < Package
   description 'The JDK is a development environment for building applications, applets, and components using the Java programming language.'
   homepage 'https://openjdk.org/'
-  version '21.0.2'
+  version '22.0.1'
   license 'GPL-2'
   compatibility 'x86_64'
-  # Visit https://www.azul.com/downloads/?version=java-21-lts&package=jdk#zulu to download the binary.
-  source_url 'https://cdn.azul.com/zulu/bin/zulu21.32.17-ca-jdk21.0.2-linux_x64.tar.gz'
-  source_sha256 '5ad730fbee6bb49bfff10bf39e84392e728d89103d3474a7e5def0fd134b300a'
+  # Visit https://www.azul.com/downloads/?package=jdk#zulu to download the binary.
+  source_url 'https://cdn.azul.com/zulu/bin/zulu22.30.13-ca-jdk22.0.1-linux_x64.tar.gz'
+  source_sha256 '993d91062c631d10508475f7b112724fa8136704ec1412d5cc1f93ddda1eddb0'
 
   no_compile_needed
   no_shrink
@@ -30,20 +30,20 @@ class Openjdk21 < Package
 
   def self.install
     FileUtils.mkdir_p CREW_DEST_MAN_PREFIX
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/openjdk21"
-    FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/openjdk21/"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/openjdk22"
+    FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/openjdk22/"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    Dir["#{CREW_DEST_PREFIX}/share/openjdk21/bin/*"].each do |binfile|
+    Dir["#{CREW_DEST_PREFIX}/share/openjdk22/bin/*"].each do |binfile|
       @basename = File.basename(binfile)
-      FileUtils.ln_s "#{CREW_PREFIX}/share/openjdk21/bin/#{@basename}", "#{CREW_DEST_PREFIX}/bin/#{@basename}"
+      FileUtils.ln_s "#{CREW_PREFIX}/share/openjdk22/bin/#{@basename}", "#{CREW_DEST_PREFIX}/bin/#{@basename}"
     end
-    FileUtils.mv Dir["#{CREW_DEST_PREFIX}/share/openjdk21/man/*"], CREW_DEST_MAN_PREFIX
+    FileUtils.mv Dir["#{CREW_DEST_PREFIX}/share/openjdk22/man/*"], CREW_DEST_MAN_PREFIX
     # Add environment variable.
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
     javaenv = <<~EOF
       # Java configuration
       JAVA_HOME=#{CREW_PREFIX}
     EOF
-    File.write("#{CREW_DEST_PREFIX}/etc/env.d/10-openjdk21", javaenv)
+    File.write("#{CREW_DEST_PREFIX}/etc/env.d/10-openjdk22", javaenv)
   end
 end
