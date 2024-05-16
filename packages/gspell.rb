72fb62f2ab86c5513343da97e96cb4cf4321c247
Zopolis4 <creatorsmithmdt@gmail.com>
2024-05-16 17:45:16 +1000

    Switch gspell to autotools buildsystem

--- packages/gspell.rb
+++ packages/gspell.rb
@@ -1,23 +1,24 @@
-require 'buildsystems/cmake'
+require 'buildsystems/autotools'
 
-class Gspell < CMake
+class Gspell < Autotools
   description 'a flexible API to implement the spell checking in a GTK+ application'
   homepage 'https://wiki.gnome.org/Projects/gspell'
-  version '1.12.2-f3c1eed'
+  version '1.12.2-87b8146'
   license 'LGPL-2.1+'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/gspell.git'
-  git_hashtag 'f3c1eed397751b3af88b841fcdb52199a13dccd5'
+  git_hashtag '87b8146864a130bc1c85c57baa211df160ae564c'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '77a4e9f9e28dd49e56f1b00ab7957619683091b8edd515f530312a57def70d0d',
-     armv7l: '77a4e9f9e28dd49e56f1b00ab7957619683091b8edd515f530312a57def70d0d',
-     x86_64: 'd3b25c41bc7d13a792af74d1849a110354d131c3c1d94a936b754c11aa0318e9'
+    aarch64: 'eb9f5a0b0d0dd27050ec4d953c648cc87c47610ca19968eebfedc699e417e998',
+     armv7l: 'eb9f5a0b0d0dd27050ec4d953c648cc87c47610ca19968eebfedc699e417e998',
+     x86_64: '3350a5e32bfe4783f680aa54091072f2955824f1271de4336678d519a02c947c'
   })
 
   depends_on 'aspell' => :build
   depends_on 'at_spi2_core' # R
+  depends_on 'autoconf_archive' => :build
   depends_on 'cairo' # R
   depends_on 'enchant' # R
   depends_on 'gcc_lib' => :build
