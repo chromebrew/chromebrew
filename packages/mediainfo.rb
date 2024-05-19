e272fc6e3dcaaac39e4c7ef45efe421d890c0971
Ed Reel <edreel@gmail.com>
2024-05-19 17:37:19 +0000

    Update mediainfo packages
    Libmediainfo 23.11 => 24.04
    Mediainfo 23.11 => 24.04
    Mediainfo_gui 23.11 => 24.04

--- packages/mediainfo.rb
+++ packages/mediainfo.rb
@@ -3,18 +3,18 @@ require 'package'
 class Mediainfo < Package
   description 'MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files.'
   homepage 'http://mediaarea.net/en/MediaInfo'
-  version '23.11'
+  version '24.04'
   license 'BSD-2'
   compatibility 'all'
-  source_url 'https://mediaarea.net/download/binary/mediainfo/23.11/MediaInfo_CLI_23.11_GNU_FromSource.tar.xz'
-  source_sha256 '2f126c3cb7d5d8c1eb45088a2ae327c5e60acc6891c7d842e05f733b4c189bf8'
+  source_url 'https://mediaarea.net/download/binary/mediainfo/24.04/MediaInfo_CLI_24.04_GNU_FromSource.tar.xz'
+  source_sha256 'bb5bae69a9067f76250976cd1a6fadde110cf9bfb4dea7e18070f0f08cf23eb8'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1106583c2748362f6724dc7c913e44aa7f9dd64fcb5499efdd54c16717b00d38',
-     armv7l: '1106583c2748362f6724dc7c913e44aa7f9dd64fcb5499efdd54c16717b00d38',
-       i686: '164f4aa1220f90aabb73f3d2aeb38d1888c0fe441f11bbbfa66d97e7c3a2f706',
-     x86_64: 'd4d2f92642aac630c91d16ef5d5b65da1d4b3044ce788000b8c9133a0e333dd5'
+    aarch64: 'a3fa699cb3ef87f24fcd94401b4811d52a176c8abd721be5ac4432e40efa32f4',
+     armv7l: 'a3fa699cb3ef87f24fcd94401b4811d52a176c8abd721be5ac4432e40efa32f4',
+       i686: '48a637c6095d6fb5843078e4c7da412e855c517965753e3f77a68763ed9c8f9d',
+     x86_64: '9a761f66c3e2f8f7f8728cf2adba442b6ee62182c3037b49abbd4af489855bec'
   })
 
   def self.patch
