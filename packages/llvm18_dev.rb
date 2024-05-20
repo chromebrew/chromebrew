362801dc1eb402192d5bfe8bab3044d926f63cda
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-05-20 11:33:03 -0400

    Llvm => 18.1.6 (#9825)
    * llvm18_build => 18.1.6
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * llvm18_{lib,dev}, openmp => 18.1.6
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * update py3_pygments
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/llvm18_dev.rb
+++ packages/llvm18_dev.rb
@@ -4,8 +4,8 @@ require_relative 'llvm18_build'
 class Llvm18_dev < Package
   description 'LLVM: Everything except libLLVM & llvm-strip'
   homepage Llvm18_build.homepage
-  version '18.1.5'
-  # When upgrading llvm_build*, be sure to upgrade llvm_lib* and llvm_dev* in tandem.
+  version '18.1.6'
+  # When upgrading llvm_build*, be sure to upgrade llvm_lib*, llvm_dev*, and openmp in tandem.
   puts "#{self} version differs from llvm version #{Llvm18_build.version}".orange if version != Llvm18_build.version
   license Llvm18_build.license
   compatibility 'all'
@@ -13,10 +13,10 @@ class Llvm18_dev < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f19d8b61707dc0718c34c7fc04ba94a17bbb7f6d9c3057deed04b9779242bd7e',
-     armv7l: 'f19d8b61707dc0718c34c7fc04ba94a17bbb7f6d9c3057deed04b9779242bd7e',
-       i686: '7f457c75fbe3075bd35db8d9e646ad76f6bb75e736cc55e1091a8d4369f5ffbc',
-     x86_64: '2a54fc6920364a6e6965437289b8f50b87345567955f955f84ecf3e6978f005b'
+    aarch64: '189df30c062bb5cb5cdf33e1a5424c687bbaee830445366c9f4310d764e65a8a',
+     armv7l: '189df30c062bb5cb5cdf33e1a5424c687bbaee830445366c9f4310d764e65a8a',
+       i686: '2d3222ae8d22d3c44ec318f9b70442ece5cba0af6307fc903f189692c0b8eaab',
+     x86_64: 'f537b3c187f7ef02243d750b22b388fb5cb568e68cd655af0d5ffed8099514bc'
   })
 
   depends_on 'gcc_dev' # R
--- packages/llvm18_lib.rb
+++ packages/llvm18_lib.rb
@@ -4,8 +4,8 @@ require_relative 'llvm18_build'
 class Llvm18_lib < Package
   description 'LibLLVM and llvm-strip'
   homepage Llvm18_build.homepage
-  version '18.1.5'
-  # When upgrading llvm_build*, be sure to upgrade llvm_lib* and llvm_dev* in tandem.
+  version '18.1.6'
+  # When upgrading llvm_build*, be sure to upgrade llvm_lib*, llvm_dev*, and openmp in tandem.
   puts "#{self} version differs from llvm version #{Llvm18_build.version}".orange if version != Llvm18_build.version
   license Llvm18_build.license
   compatibility 'all'
@@ -13,10 +13,10 @@ class Llvm18_lib < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'dc6ba05336c844955436fe50cfe6de2b9869276aa449b191e231abd11bfe925b',
-     armv7l: 'dc6ba05336c844955436fe50cfe6de2b9869276aa449b191e231abd11bfe925b',
-       i686: 'a71e7812258b0f7334647fa405c68e5cfa13c228a2cddb25eb98762946b9e80b',
-     x86_64: '9afeb19d5c05417a468481d38edb1b4292028c8a74a63050a5275114aec537dc'
+    aarch64: '755b80a919606ad053d9331cf5ca84b70df4b3a6abc07641c50cd49f80b80e18',
+     armv7l: '755b80a919606ad053d9331cf5ca84b70df4b3a6abc07641c50cd49f80b80e18',
+       i686: '1305a29b46ddd903386d2b3b9348bcd997ecd154dc76177ef7bd3a0d395c670c',
+     x86_64: '8e6c4909abd94a54a5b82b3a4a0d4e3acdc806f52ba7dbcd9923632edc7bb310'
   })
 
   depends_on 'gcc_lib' # R
