b396a832e1af7afcf145ff25ffa081d35180d646
Satadru Pramanik <satadru@gmail.com>
2024-05-20 10:06:49 -0400

    update py3_pygments
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pygments.rb
+++ packages/py3_pygments.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_pygments < Pip
   description 'Python Syntax Highlighter'
   homepage 'https://pygments.org/'
-  @_ver = '2.17.2'
+  @_ver = '2.18.0'
   version "#{@_ver}-py3.12"
   license 'BSD-2'
   compatibility 'all'
