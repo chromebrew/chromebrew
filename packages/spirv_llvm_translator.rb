932f5cade1209063b1efce22de20bb890e70f126
Satadru Pramanik <satadru@gmail.com>
2024-05-23 08:31:07 -0400

    Add versioning to spirv_llvm_translator
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/spirv_llvm_translator.rb
+++ packages/spirv_llvm_translator.rb
@@ -6,17 +6,18 @@ require 'buildsystems/cmake'
 class Spirv_llvm_translator < CMake
   description 'Tool and a library for bi-directional translation between SPIR-V and LLVM IR'
   homepage 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator'
-  version '18.1.0'
+  @_ver = '18.1.0'
+  version "#{@_ver}-gcc14"
   license 'custom'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git'
-  git_hashtag "v#{version}"
+  git_hashtag "v#{@_ver}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f98669e96f7168605997bc17bf3403931a585a38d5159d9ece7c01f9938e8bcb',
-     armv7l: 'f98669e96f7168605997bc17bf3403931a585a38d5159d9ece7c01f9938e8bcb',
-     x86_64: '85be281a6bd28b33f6987112137e32da1e846fef48103f5c2df0496c90d63a5f'
+    aarch64: '314bbf245fb76336bb506e23a5f7dfa7a605d2f346e1e578d24c0946a0571538',
+     armv7l: '314bbf245fb76336bb506e23a5f7dfa7a605d2f346e1e578d24c0946a0571538',
+     x86_64: '595cbecb0276f45f4f45e9a8cf33d48f5dc272ecf05b80a636feccdfaea4d444'
   })
 
   depends_on 'gcc_lib' # R
