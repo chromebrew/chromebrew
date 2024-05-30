aa65a850406e70450b5a56ad327b48c59cf49efa
Ed Reel <edreel@gmail.com>
2024-05-30 02:53:00 +0000

    Netbeans 21 => 22

--- packages/netbeans.rb
+++ packages/netbeans.rb
@@ -3,13 +3,13 @@ require 'package'
 class Netbeans < Package
   description 'Apache NetBeans IDE is free open source software to quickly and easily develop desktop, mobile, and web applications with Java, JavaScript, HTML5, PHP, C/C++ and more.'
   homepage 'https://netbeans.org/'
-  version '21'
+  version '22'
   license 'Apache-2.0, custom, EPL-1.0, W3C, MIT, BSD, MPL-1.0, CC-BY-3.0, CDDL and CDDL-1.1'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://dlcdn.apache.org/netbeans/netbeans/21/netbeans-21-bin.zip'
-  source_sha256 '7a71a9683c526a847c1ff43bc9403d5c01c50b739e3c5170feffb2aa7fbaf7fd'
+  source_url 'https://dlcdn.apache.org/netbeans/netbeans/22/netbeans-22-bin.zip'
+  source_sha256 'baecc2da28934a7ddccd6a1ddda05b361f2654ce5b0af9a3294974a6d35d9b73'
 
-  depends_on 'openjdk11'
+  depends_on 'openjdk17'
   depends_on 'libxtst'
   depends_on 'sommelier'
 
