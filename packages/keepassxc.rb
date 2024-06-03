b020a6f9e5026e6420cf0bba048ab3b2fce4fc51
Ed Reel <edreel@gmail.com>
2024-06-03 23:14:31 +0000

    Keepassxc 2.7.6 => 2.7.8

--- packages/keepassxc.rb
+++ packages/keepassxc.rb
@@ -3,33 +3,32 @@ require 'package'
 class Keepassxc < Package
   description 'KeePass Cross-Platform Community Edition, the free, open source, light-weight and easy-to-use password manager.'
   homepage 'https://keepassxc.org/'
-  version '2.7.6'
+  version '2.7.8'
   license 'GPL-2 or GPL-3'
   compatibility 'x86_64'
-  source_url 'https://github.com/keepassxreboot/keepassxc/releases/download/2.7.6/KeePassXC-2.7.6-x86_64.AppImage'
-  source_sha256 'f32f7e7ab4bca789b24bd6a420c1d87dff40982646abef58fca481a7c56ace48'
+  source_url 'https://github.com/keepassxreboot/keepassxc/releases/download/2.7.8/KeePassXC-2.7.8-x86_64.AppImage'
+  source_sha256 '101b4d28a4960f01cc4dd6ca6a937dc4be60f47ccadd84b729d51401b4d346dd'
 
   no_compile_needed
 
   depends_on 'sommelier'
 
   def self.build
-    keepass = <<~EOF
+    File.write 'keepassxc.sh', <<~EOF
       #!/bin/bash
       cd #{CREW_PREFIX}/share/keepassxc
       ./AppRun "$@"
     EOF
-    File.write('keepassxc.sh', keepass)
   end
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/keepassxc"
     FileUtils.install 'keepassxc.sh', "#{CREW_DEST_PREFIX}/bin/keepassxc", mode: 0o755
-    FileUtils.mv Dir.glob('*'), "#{CREW_DEST_PREFIX}/share/keepassxc"
+    FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/keepassxc"
   end
 
   def self.postinstall
-    puts "\nType 'keepassxc' to get started.\n".lightblue
+    ExitMessage.add "\nType 'keepassxc' to get started.\n".lightblue
   end
 end
