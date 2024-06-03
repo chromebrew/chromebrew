ab6fffd4863449fb1c20f8b627f095d44d15f9f4
Ed Reel <edreel@gmail.com>
2024-06-03 18:47:48 -0500

    Thorium 117.0.5938.157 => 124.0.6367.218 (#9893)

--- packages/thorium.rb
+++ packages/thorium.rb
@@ -3,12 +3,12 @@ require 'package'
 class Thorium < Package
   description 'The fastest browser on Earth.'
   homepage 'https://thorium.rocks/'
-  version '117.0.5938.157'
+  version '124.0.6367.218'
   license 'BSD-3 Clause'
   compatibility 'x86_64'
   min_glibc '2.29'
-  source_url "https://github.com/Alex313031/thorium/releases/download/M#{version}/Thorium_Browser_#{version}_x64.AppImage"
-  source_sha256 '7657c29416f04a4420e2cb4a65d4a0360dc416c5a4b28236d5cc511b948cace3'
+  source_url 'https://github.com/Alex313031/thorium/releases/download/M124.0.6367.218/Thorium_Browser_124.0.6367.218_SSE4.AppImage'
+  source_sha256 '500d82637b91fe5e74862ba3c7714048232c09c8f7912ed26ef1a02326164cb4'
 
   depends_on 'ffmpeg'
   depends_on 'gtk3'
