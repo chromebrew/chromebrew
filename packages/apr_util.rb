2c9401d8c2aabe38f684a97d6362ffa43cadc620
Zopolis4 <creatorsmithmdt@gmail.com>
2024-06-06 08:30:24 +0000

    Update apr_util homepage

--- packages/apr_util.rb
+++ packages/apr_util.rb
@@ -2,7 +2,7 @@ require 'buildsystems/autotools'
 
 class Apr_util < Autotools
   description 'APR-util provides a number of helpful abstractions on top of APR.'
-  homepage 'http://apr.apache.org/'
+  homepage 'https://apr.apache.org/'
   version '1.6.3'
   license 'Apache-2.0'
   compatibility 'all'
