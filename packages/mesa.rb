ef5ce09e8516f604a1b04c418035be457bbfe337
Satadru Pramanik <satadru@gmail.com>
2024-06-06 08:46:52 -0400

    Add initial mesa 24.1.1 package update
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Mesa < Package
+class Mesa < Meson
   description 'Open-source implementation of the OpenGL specification'
   homepage 'https://www.mesa3d.org'
-  @_ver = '24.1.0'
+  @_ver = '24.1.1'
   version "#{@_ver}-llvm18"
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
@@ -23,7 +23,7 @@ class Mesa < Package
   depends_on 'gcc_dev' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
-  depends_on 'glibc_lib' #
+  depends_on 'glibc_lib' # R
   depends_on 'glslang' => :build
   depends_on 'libclc' => :build
   depends_on 'libdrm' # R
@@ -46,6 +46,7 @@ class Mesa < Package
   depends_on 'libxxf86vm' # R
   depends_on 'llvm18_dev' => :build
   depends_on 'llvm18_lib' # R
+  depends_on 'spirv_llvm_translator' => :build
   depends_on 'lm_sensors' # R
   depends_on 'py3_mako' => :build
   depends_on 'py3_ply' => :build
@@ -59,24 +60,19 @@ class Mesa < Package
   depends_on 'zlibpkg' # R
   depends_on 'zstd' # R
 
-  def self.build
-    system "mold -run meson setup #{CREW_MESON_OPTIONS.gsub('-mfpu=vfpv3-d16', '-mfpu=neon-fp16')} \
-      -Db_asneeded=false \
-      -Ddri3=enabled \
-      -Degl=enabled \
-      -Dgbm=enabled \
-      -Dgles1=disabled \
-      -Dgles2=enabled \
-      -Dglvnd=enabled \
-      -Dglx=dri \
-      -Dllvm=enabled \
-      -Dgallium-drivers='#{ARCH == 'x86_64' ? 'i915,r300,r600,radeonsi,nouveau,virgl,svga,swrast,iris,crocus,zink' : 'v3d,freedreno,etnaviv,nouveau,svga,tegra,virgl,lima,panfrost,swrast,iris,zink'}' \
-      -Dvulkan-drivers='#{ARCH == 'x86_64' ? 'amd, intel, intel_hasvk, swrast' : 'auto'}' \
-      -Dvideo-codecs='all' \
-       builddir"
-    system 'meson configure --no-pager builddir'
-    system "mold -run #{CREW_NINJA} -C builddir"
-  end
+  meson_options "#{CREW_MESON_OPTIONS.gsub('-mfpu=vfpv3-d16', '-mfpu=neon-fp16')} \
+    -Db_asneeded=false \
+    -Ddri3=enabled \
+    -Degl=enabled \
+    -Dgbm=enabled \
+    -Dgles1=disabled \
+    -Dgles2=enabled \
+    -Dglvnd=enabled \
+    -Dglx=dri \
+    -Dllvm=enabled \
+    -Dgallium-drivers='#{ARCH == 'x86_64' ? 'i915,r300,r600,radeonsi,nouveau,virgl,svga,swrast,iris,crocus,zink' : 'v3d,freedreno,etnaviv,nouveau,svga,tegra,virgl,lima,panfrost,swrast,iris,zink'}' \
+    -Dvulkan-drivers='#{ARCH == 'x86_64' ? 'amd, intel, intel_hasvk, swrast' : 'auto'}' \
+    -Dvideo-codecs='all'"
 
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
