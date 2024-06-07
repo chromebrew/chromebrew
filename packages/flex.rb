0eb32ecf2724865e30d3f39b7340641f4ddb65a7
Ed Reel <edreel@gmail.com>
2024-06-07 02:39:13 +0000

    Flex 2.6.4-1 => 2.6.4-2

--- packages/flex.rb
+++ packages/flex.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Flex < Autotools
   description 'Flex (The Fast Lexical Analyzer) is a fast lexical analyser generator tool for generating programs that perform pattern-matching on text.'
   homepage 'https://www.gnu.org/software/flex/'
-  version '2.6.4-1'
+  version '2.6.4-2'
   license 'BSD'
   compatibility 'all'
   source_url 'https://github.com/westes/flex/releases/download/v2.6.4/flex-2.6.4.tar.gz'
@@ -11,21 +11,27 @@ class Flex < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a51ead6fb16d80f31592cb4057d3d6a439f98579b0ead1ab72736b1502ada313',
-     armv7l: 'a51ead6fb16d80f31592cb4057d3d6a439f98579b0ead1ab72736b1502ada313',
-       i686: '12f3f97a8868b5f41b8b7f4332ca3f75bb39aa49861cb90994dff7a914b28230',
-     x86_64: '5506c83d69cbb5411eaefc355a05931f1955639dccec927caac5f8f6fe622f41'
+    aarch64: '7968e998075c49c0b49d0db6d9d2fc49f1e1e35a283a1cd450db8b149191cfcc',
+     armv7l: '7968e998075c49c0b49d0db6d9d2fc49f1e1e35a283a1cd450db8b149191cfcc',
+       i686: '9fd42095b66754a94d1b09b242966275efcd41b39f75ca0ba504b75125a38bf9',
+     x86_64: 'c2dc3fd71ce8109b2bd2d8fa233e0082a772d50d0dcf5629cc105f2c5d8be8b6'
   })
 
   depends_on 'bison' => :build
   depends_on 'glibc' # R
   depends_on 'm4' => :build
 
+  configure_options '--with-pic --disable-static --enable-shared'
+
   def self.patch
-    downloader 'https://gitlab.archlinux.org/archlinux/packaging/packages/flex/-/raw/main/flex-pie.patch?ref_type=heads&inline=false', '20f3cce6b0ea6ab67a902a46b89c292b959994dedcbe6ee5d187f9bba1408b0e', 'flex-pie.patch'
+    downloader 'https://gitlab.archlinux.org/archlinux/packaging/packages/flex/-/raw/main/flex-pie.patch?ref_type=heads&inline=false',
+               '20f3cce6b0ea6ab67a902a46b89c292b959994dedcbe6ee5d187f9bba1408b0e', 'flex-pie.patch'
     system 'patch -p1 -i flex-pie.patch'
     system 'autoreconf -fiv'
   end
 
-  configure_options '--with-pic --disable-static --enable-shared'
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    FileUtils.ln_s "#{CREW_PREFIX}/bin/flex", "#{CREW_DEST_PREFIX}/bin/lex"
+  end
 end
