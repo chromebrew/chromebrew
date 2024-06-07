6c3d91a76b3bdc2429556a536a0414dab880f025
Ed Reel <edreel@gmail.com>
2024-06-07 07:37:16 +0000

    Clojure 1.11.1 => 1.11.3

--- packages/clojure.rb
+++ packages/clojure.rb
@@ -3,19 +3,18 @@ require 'package'
 class Clojure < Package
   description 'Clojure is a robust, practical, and fast programming language with a set of useful features that together form a simple, coherent, and powerful tool.'
   homepage 'https://clojure.org/'
-  @_ver = '1.11.1'
-  version "#{@_ver}-1"
+  version '1.11.3'
   license 'EPL-1.0, Apache-2.0 and BSD'
   compatibility 'all'
-  source_url 'https://github.com/clojure/clojure/archive/clojure-1.11.1.tar.gz'
-  source_sha256 'f2fc71a53186c92a45ecb9bb4f0a3467bedcd0f9ce8eafe3bded737036e7fcfa'
+  source_url 'https://github.com/clojure/clojure.git'
+  git_hashtag "clojure-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c97961780791b487304a48a09d75a56fac3f5614b49303a75fedeb8e94d46933',
-     armv7l: 'c97961780791b487304a48a09d75a56fac3f5614b49303a75fedeb8e94d46933',
-       i686: 'a7223033ec6f8c669769e4f7adb3c7fa02c4b2748ede60fa0696299da0560528',
-     x86_64: 'd6aeaf5ddcec5243c2fdf551c735ffea6cb2952935ac4ed0df54d5f812b326d0'
+    aarch64: '944560332940086342379caa876f578609f0ae0b609852ea4d8c1ba145c4b85f',
+     armv7l: '944560332940086342379caa876f578609f0ae0b609852ea4d8c1ba145c4b85f',
+       i686: '6fd46d3898db62673f38f629f7b5fb6d846fddee9d6305f8a5750f3baa849a23',
+     x86_64: 'c0ffc01bf45e7ec7d75c8f7f3c2dff766426b9aa1b2e91d69387c139b747b954'
   })
 
   depends_on 'openjdk8'
