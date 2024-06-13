2024f6d6b8019496225c972dfbeeb65bea5500bc
Ed Reel <edreel@gmail.com>
2024-06-13 03:34:48 +0000

    Update alsa packages
    alsa_lib 1.2.11-py3.12 => 1.2.12-py3.12
    alsa_utils 1.2.11 => 1.2.12

--- packages/alsa_lib.rb
+++ packages/alsa_lib.rb
@@ -3,19 +3,18 @@ require 'buildsystems/autotools'
 class Alsa_lib < Autotools
   description 'The Advanced Linux Sound Architecture (ALSA) provides audio and MIDI functionality to the Linux operating system.'
   homepage 'https://www.alsa-project.org/main/index.php/Main_Page'
-  @_ver = '1.2.11'
-  version "#{@_ver}-py3.12"
+  version '1.2.12-py3.12'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url "https://github.com/alsa-project/alsa-lib/archive/v#{@_ver}.tar.gz"
-  source_sha256 '12216f0730d6dde3ded6a2a5388bc0009ad07f5c65972bd89aac9a76f8f085a4'
+  source_url "https://github.com/alsa-project/alsa-lib/archive/v#{version.split('-').first}.tar.gz"
+  source_sha256 'f067dbba9376e5bbbb417b77751d2a9f2f277c54fb3a2b5c023cc2c7dfb4e3c1'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'dd76e895e0314728a1600cac5e47ed5a0c15092d5927e5d6e2e280a3a6a56a42',
-     armv7l: 'dd76e895e0314728a1600cac5e47ed5a0c15092d5927e5d6e2e280a3a6a56a42',
-       i686: 'afa0c01b6cb3f50b2d8db1e08797ff8866ed1421f2e093195acacd65edbcae69',
-     x86_64: '171d4c8ee0f259871010214b1b2bb0043fe393ca8579e53f46e94cbeb2383b29'
+    aarch64: '1409d8a119ee28fb2b9e6cd789ef86e17034a9331e0f0bde0db3a06af102d156',
+     armv7l: '1409d8a119ee28fb2b9e6cd789ef86e17034a9331e0f0bde0db3a06af102d156',
+       i686: '45fc80421c2e3220a540ff0b534578506e43f54e4ca8561b9ff93888f528d70c',
+     x86_64: '241d2bee8a74d9446ed29765a00300e0b60d4b1b778ae28040902c24c50bdf3d'
   })
 
   depends_on 'glibc' # R
--- packages/alsa_utils.rb
+++ packages/alsa_utils.rb
@@ -1,19 +1,19 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Alsa_utils < Package
+class Alsa_utils < Autotools
   description 'The Advanced Linux Sound Architecture (ALSA) - utilities'
   homepage 'https://github.com/alsa-project/alsa-utils'
-  version '1.2.11'
+  version '1.2.12'
   license 'GPL-2'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url "https://github.com/alsa-project/alsa-utils/archive/v#{version}.tar.gz"
-  source_sha256 '978961153fa8ca4c783c93767e7054d0dc1fb42ef6f1008040ca71363d0f4d35'
+  source_url 'https://github.com/alsa-project/alsa-utils.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5f702ad52afed8235203c6155c5670bb525f88a51d0941e22231fd0b1d277a34',
-     armv7l: '5f702ad52afed8235203c6155c5670bb525f88a51d0941e22231fd0b1d277a34',
-     x86_64: '5ef82a21da013de424f882f7796ca215c14e46503420a51747cb784f98694dbf'
+    aarch64: 'adf3a2732a82eda1e9d4c62e5c71a6d15bf430bc0db13caf0b23213793c3355f',
+     armv7l: 'adf3a2732a82eda1e9d4c62e5c71a6d15bf430bc0db13caf0b23213793c3355f',
+     x86_64: '319eafbd0afed0942f9da2a1e28d6e4d8d2d4fe0ba2fd9d797ad9d274d8ac6b7'
   })
 
   depends_on 'alsa_lib' # R
@@ -26,18 +26,5 @@ class Alsa_utils < Package
     downloader 'https://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.guess', 'SKIP'
     downloader 'https://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.sub', 'SKIP'
     system 'autoreconf -fiv'
-  end
-
-  def self.build
-    system "mold -run ./configure #{CREW_OPTIONS}"
-  end
-
-  def self.check
-    # This takes several hours to run!
-    # system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
