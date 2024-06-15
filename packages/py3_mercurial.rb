4eed88a8ee3aca2d1f65585cd0371e4ca2fd39ee
Ed Reel <edreel@gmail.com>
2024-06-15 06:07:23 +0000

    Py3_mercurial 6.7.3 => 6.7.4

--- packages/py3_mercurial.rb
+++ packages/py3_mercurial.rb
@@ -3,16 +3,16 @@ require 'buildsystems/pip'
 class Py3_mercurial < Pip
   description 'Mercurial is a free, distributed source control management tool. It efficiently handles projects of any size and offers an easy and intuitive interface.'
   homepage 'https://www.mercurial-scm.org/'
-  version '6.7.3'
+  version '6.7.4'
   license 'GPL-2+'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '16fd75bc6e57d5053b7d050471fdca3272c3311dfac61ae3674de6268128f7f8',
-     armv7l: '16fd75bc6e57d5053b7d050471fdca3272c3311dfac61ae3674de6268128f7f8',
-     x86_64: 'ddc550c75c3f169a4f5737b4932b1c8bf573fb19b768015609718a9fee33a07d'
+    aarch64: 'c85013c1f2fea8cc5c07f6e66d73a99abe527cc7a0529b53a504d7b865cf9d29',
+     armv7l: 'c85013c1f2fea8cc5c07f6e66d73a99abe527cc7a0529b53a504d7b865cf9d29',
+     x86_64: 'c408b753a4a7b271ec69d971494bcca92f30b53425fbb12ebc7b758569f908ee'
   })
 
   no_source_build
