a730f202292bed839f3492bbb06307286bc210c3
Zopolis4 <creatorsmithmdt@gmail.com>
2024-06-15 08:39:02 +0000

    Update font_adobe_utopia_type1 homepage

--- packages/font_adobe_utopia_type1.rb
+++ packages/font_adobe_utopia_type1.rb
@@ -2,7 +2,7 @@ require 'package'
 
 class Font_adobe_utopia_type1 < Package
   description 'Adobe Utopia Type1 fonts'
-  homepage 'https://xorg.freedesktop.org/'
+  homepage 'https://xorg.freedesktop.org/wiki/'
   version '1.0.4'
   license 'custom'
   compatibility 'x86_64 aarch64 armv7l'
