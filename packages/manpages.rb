6d24a5e5529c4e0eb9bc8898b25517f3de9e7033
Ed Reel <edreel@gmail.com>
2024-06-15 02:01:01 -0500

    Manpages 6.8 => 6.9 (#9970)

--- packages/manpages.rb
+++ packages/manpages.rb
@@ -3,18 +3,18 @@ require 'package'
 class Manpages < Package
   description 'The Linux man-pages project documents the Linux kernel and C library interfaces that are employed by user-space programs.'
   homepage 'https://www.kernel.org/doc/man-pages/'
-  version '6.8'
+  version '6.9'
   license 'man-pages, GPL-2+ and BSD'
   compatibility 'all'
-  source_url 'https://mirrors.edge.kernel.org/pub/linux/docs/man-pages/man-pages-6.8.tar.xz'
-  source_sha256 'b9c6b0a420f839148be04b2fc13a85692313728d54d47c69c8a138379665d226'
+  source_url "https://mirrors.edge.kernel.org/pub/linux/docs/man-pages/man-pages-#{version}.tar.xz"
+  source_sha256 'd14caff54cc662f34b1b70bcf62d4e27e4c2fd260672a35b6e74af71b3f71f88'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f486cb05ecea867dffc7a0c5d60edf2b073d552e65a840b3b29e12e32593e38d',
-     armv7l: 'f486cb05ecea867dffc7a0c5d60edf2b073d552e65a840b3b29e12e32593e38d',
-       i686: 'fc94cfd272bd3c8a60dad9e43d01c54a5746ac80dc29158c4c7d999cba7c249f',
-     x86_64: '812d7acf982fd98946aba5132a08685a950d7e6bddfd8ba9e4b45b4ed3b38b29'
+    aarch64: '1b468aad94416658289624a9d5c013f781b69aac7e15c64d3562f738b0684b40',
+     armv7l: '1b468aad94416658289624a9d5c013f781b69aac7e15c64d3562f738b0684b40',
+       i686: '5d5e1e9530401745e471050b4d2a33040d986ddbae21ba6c6906f106593918b7',
+     x86_64: '51cbc74fd2139adcff1be2d0bc39e2a050c17378797674894831435329bf301d'
   })
 
   depends_on 'man_db'
@@ -24,6 +24,6 @@ class Manpages < Package
   end
 
   def self.postinstall
-    puts "\nTry 'man printf' to see if it works.\n".lightblue
+    ExitMessage.add "\nTry 'man printf' to see if it works.\n".lightblue
   end
 end
