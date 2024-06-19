830b85956344c9b04f797b49c8ffeab561cf9924
Ed Reel <edreel@gmail.com>
2024-06-19 22:54:11 +0000

    Fix error with autotools builds
    Use Kernel.system

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.49.1'
+CREW_VERSION = '1.49.2'
 
 # kernel architecture
 KERN_ARCH = Etc.uname[:machine]
