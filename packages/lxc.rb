7b6a20499cbe48d351d6ac18971875a7f23b9f41
supechicken <me@supechicken666.dev>
2024-06-22 16:58:20 +0800

    Use `date` from coreutils
    Signed-off-by: supechicken <me@supechicken666.dev>

--- packages/lxc.rb
+++ packages/lxc.rb
@@ -32,6 +32,7 @@ class Lxc < Meson
     # use `date` from coreutils
     # FIXME: Remove `date` binary from `zoneinfo`
     # meson.build:310:16: ERROR: Command `/usr/local/bin/date --utc --date=@1719046192 +%Y-%m-%d` failed with status 1.
-    system 'sed', '-i', "s,find_program('date'),'/bin/date',g", 'meson.build'
+    system 'sed', '-i', "s,find_program('date'),'date',g", 'meson.build'
+    system 'sed', '-i', "s,'date','/bin/date',g", 'meson.build'
   end
 end
