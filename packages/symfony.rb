b075befc7aea9c65a78f32c4a6fee3e40cb152f7
Ed Reel <edreel@gmail.com>
2024-06-24 16:23:21 -0500

    Symfony 5.8.15 => 5.9.1 (#10022)

--- packages/symfony.rb
+++ packages/symfony.rb
@@ -3,23 +3,24 @@ require 'package'
 class Symfony < Package
   description 'Symfony is a set of PHP Components, a Web Application framework'
   homepage 'https://symfony.com/'
-  version '5.8.15'
+  version '5.9.1'
   license 'AGPL-3.0'
   compatibility 'x86_64 aarch64 armv7l'
   source_url({
-    aarch64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.8.15/symfony-cli_linux_armv6.tar.gz',
-     armv7l: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.8.15/symfony-cli_linux_armv6.tar.gz',
-     x86_64: 'https://github.com/symfony-cli/symfony-cli/releases/download/v5.8.15/symfony-cli_linux_amd64.tar.gz'
+    aarch64: "https://github.com/symfony-cli/symfony-cli/releases/download/v#{version}/symfony-cli_linux_armv6.tar.gz",
+     armv7l: "https://github.com/symfony-cli/symfony-cli/releases/download/v#{version}/symfony-cli_linux_armv6.tar.gz",
+     x86_64: "https://github.com/symfony-cli/symfony-cli/releases/download/v#{version}/symfony-cli_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '7e7b58ec5f2f024d3db320f1efd1bf6ef4366a77bfcb818088895eb82805378d',
-     armv7l: '7e7b58ec5f2f024d3db320f1efd1bf6ef4366a77bfcb818088895eb82805378d',
-     x86_64: '885273d6714031c08c7c3fc5377d4f8ed57e0f3be371f64e871dfa72305242e5'
+    aarch64: '5a5aa4c11ae051af09280fcbde51c4708038d75ebc041b1ca31c3f89d97d7cf3',
+     armv7l: '5a5aa4c11ae051af09280fcbde51c4708038d75ebc041b1ca31c3f89d97d7cf3',
+     x86_64: '124013c9d3175a98bd6e577c9dd7024d4311ee79e44c201ab8b3531c2b8bd571'
   })
 
   depends_on 'php83' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
   no_compile_needed
+  no_shrink
 
   def self.install
     FileUtils.install 'symfony', "#{CREW_DEST_PREFIX}/bin/symfony", mode: 0o755
