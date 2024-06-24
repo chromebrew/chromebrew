5fe129e2a0fc136816930c99775fe4b8dd5133cf
Ed Reel <edreel@gmail.com>
2024-06-24 15:02:43 -0500

    Glab 1.41.0 => 1.43.0 (#10017)

--- packages/glab.rb
+++ packages/glab.rb
@@ -3,20 +3,20 @@ require 'package'
 class Glab < Package
   description 'A GitLab CLI tool bringing GitLab to your command line'
   homepage 'https://gitlab.com/gitlab-org/cli'
-  version '1.41.0'
+  version '1.43.0'
   license 'MIT'
   compatibility 'all'
   source_url({
-    aarch64: 'https://gitlab.com/gitlab-org/cli/-/releases/v1.41.0/downloads/glab_1.41.0_Linux_armv6.tar.gz',
-     armv7l: 'https://gitlab.com/gitlab-org/cli/-/releases/v1.41.0/downloads/glab_1.41.0_Linux_armv6.tar.gz',
-       i686: 'https://gitlab.com/gitlab-org/cli/-/releases/v1.41.0/downloads/glab_1.41.0_Linux_i386.tar.gz',
-     x86_64: 'https://gitlab.com/gitlab-org/cli/-/releases/v1.41.0/downloads/glab_1.41.0_Linux_x86_64.tar.gz'
+    aarch64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_armv6.tar.gz",
+     armv7l: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_armv6.tar.gz",
+       i686: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_i386.tar.gz",
+     x86_64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_x86_64.tar.gz"
   })
   source_sha256({
-    aarch64: '8cf620342b2f1c8b348dd79e26c27589047fe3be03868327f1bc68bc8e9c7734',
-     armv7l: '8cf620342b2f1c8b348dd79e26c27589047fe3be03868327f1bc68bc8e9c7734',
-       i686: '683745ae13394f95187469cbaffdf2ae7bb82401f555e5add3c2e03f5fef8972',
-     x86_64: 'ff7775b328ec7cad64211a1573597ba8e34b9ef6bc53e0448e1dc174ca14a698'
+    aarch64: '99a41fa0aaa70f72c9ed0c9e1dd40438058bce3a0a6ca68658d74b670d1e09e7',
+     armv7l: '99a41fa0aaa70f72c9ed0c9e1dd40438058bce3a0a6ca68658d74b670d1e09e7',
+       i686: 'c76f5fb151f7b828cb842c9bd0d33cd55d108369bfdce8732a04a0b98a1ad9fe',
+     x86_64: '0d4424819c5b9c35063ac387da5e26e00eedfe76c491bb83196dfe64cc456830'
   })
 
   no_compile_needed
