461ae727cbee01852d9f9b33a46637564b512425
Ed Reel <edreel@gmail.com>
2024-06-25 10:59:48 -0500

    Opera 111.0.5168.25 => 111.0.5168.43 (#10027)

--- packages/opera.rb
+++ packages/opera.rb
@@ -3,7 +3,7 @@ require 'package'
 class Opera < Package
   description 'Opera is a multi-platform web browser based on Chromium and developed by Opera Software.'
   homepage 'https://www.opera.com/'
-  version '111.0.5168.25'
+  version '111.0.5168.43'
   license 'OPERA-2018'
   compatibility 'x86_64'
   min_glibc '2.29'
@@ -11,7 +11,7 @@ class Opera < Package
   # faster apt mirror, but only works when downloading latest version of opera
   # source_url "https://deb.opera.com/opera/pool/non-free/o/opera-stable/opera-stable_#{version}_amd64.deb"
   source_url "https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/opera-stable_#{version}_amd64.deb"
-  source_sha256 '812564281f95523d98a60bc45f227daff400d17a17e1a6fd815bbf7d34e0bf49'
+  source_sha256 '04ab43c4a3d5bb44541be0ceadf6754e924c2bf168a5f4144537d018d584deb7'
 
   depends_on 'gtk3'
   depends_on 'gsettings_desktop_schemas'
