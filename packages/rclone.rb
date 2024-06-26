2643988ce374709f62358a7a26f414e9d45d57d5
Ed Reel <edreel@gmail.com>
2024-06-26 15:22:12 -0500

    Rclone 1.59.2 => 1.67.0 (#10038)

--- packages/rclone.rb
+++ packages/rclone.rb
@@ -3,23 +3,24 @@ require 'package'
 class Rclone < Package
   description 'Rclone is a command-line program to manage files on cloud storage.'
   homepage 'https://rclone.org/'
-  version '1.59.2'
+  version '1.67.0'
   license 'MIT'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/rclone/rclone/releases/download/v1.59.2/rclone-v1.59.2-linux-arm-v7.zip',
-     armv7l: 'https://github.com/rclone/rclone/releases/download/v1.59.2/rclone-v1.59.2-linux-arm-v7.zip',
-       i686: 'https://github.com/rclone/rclone/releases/download/v1.59.2/rclone-v1.59.2-linux-386.zip',
-     x86_64: 'https://github.com/rclone/rclone/releases/download/v1.59.2/rclone-v1.59.2-linux-amd64.zip'
+    aarch64: "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-linux-arm-v7.zip",
+     armv7l: "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-linux-arm-v7.zip",
+       i686: "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-linux-386.zip",
+     x86_64: "https://github.com/rclone/rclone/releases/download/v#{version}/rclone-v#{version}-linux-amd64.zip"
   })
   source_sha256({
-    aarch64: 'da638646b76966fe9ba2ab4a49aa9fa74324e58d5abaec2ebf9657069a905699',
-     armv7l: 'da638646b76966fe9ba2ab4a49aa9fa74324e58d5abaec2ebf9657069a905699',
-       i686: '27f59f2bcc5b8938d0c3d2d080e15ee67ce8c9a44147b52da52d1183afdd8ce7',
-     x86_64: '81e7be456369f5957713463e3624023e9159c1cae756e807937046ebc9394383'
+    aarch64: 'b6d0f1d60596d87349f81aff517a1c340b16e68a68d72fbb568307a8a8e0a7e8',
+     armv7l: 'b6d0f1d60596d87349f81aff517a1c340b16e68a68d72fbb568307a8a8e0a7e8',
+       i686: 'd4284fe74349d67fb89076845ce27d80a378d35b76622a57e32581ea1226859f',
+     x86_64: '07c23d21a94d70113d949253478e13261c54d14d72023bb14d96a8da5f3e7722'
   })
 
   no_compile_needed
+  no_shrink
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
