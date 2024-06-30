a46b83f85285bbcbddf1b94a8a8cc0ee3ddbcd44
Zopolis4 <creatorsmithmdt@gmail.com>
2024-06-30 15:04:34 +1000

    abseil_cpp 20230802.1 -> 20240116.2

--- packages/abseil_cpp.rb
+++ packages/abseil_cpp.rb
@@ -3,28 +3,23 @@ require 'buildsystems/cmake'
 class Abseil_cpp < CMake
   description 'Abseil Common Libraries C++'
   homepage 'https://abseil.io/'
-  version '20230802.1'
+  version '20240116.2'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url "https://github.com/abseil/abseil-cpp/archive/#{version}.tar.gz"
-  source_sha256 '987ce98f02eefbaf930d6e38ab16aa05737234d7afbab2d5c4ea7adbe50c28ed'
+  source_url 'https://github.com/abseil/abseil-cpp.git'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a51a050572ebc8f1243f20c2d9816d521b79782b98fce3cfd9705966b03fe7ae',
-     armv7l: 'a51a050572ebc8f1243f20c2d9816d521b79782b98fce3cfd9705966b03fe7ae',
-       i686: '6ea06e63a8d1cb44251f15742ab1c1e6cd221cb356b650f7580b9b475368da3e',
-     x86_64: '3c413c2009fdab56fb499d63104fefcbde3ebcdbb772a0fce1e43cd580ec67f6'
+    aarch64: '38591a94d29aa2d9f3aaaff1e9aae3ae3365dfa528d190260049b8d497c4d5d3',
+     armv7l: '38591a94d29aa2d9f3aaaff1e9aae3ae3365dfa528d190260049b8d497c4d5d3',
+       i686: '999225ef6c27d49fb99fbb4617989147b8773fe3e05083327f06e14e86fc755a',
+     x86_64: 'd7152997112ed202a94493f3148f588660154287b052ec1c02462840a1e8155a'
   })
 
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
+  depends_on 'glibc_lib' # R
 
-  cmake_options '-DCMAKE_CXX_STANDARD=17 \
-        -DABSL_CXX_STANDARD=17 \
-        -DABSL_USE_GOOGLETEST_HEAD=ON \
-        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
-        -DABSL_ENABLE_INSTALL=ON \
-        -DCMAKE_MODULE_LINKER_FLAGS=\"-Wl,--no-undefined -Wl,--no-undefined\" \
-        -DBUILD_SHARED_LIBS=ON'
+  cmake_options '-DBUILD_SHARED_LIBS=ON'
 end
