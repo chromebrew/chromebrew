4e4605688a667bdec2b946f26971f7f7fc08d596
Ed Reel <edreel@gmail.com>
2024-07-01 04:24:53 -0500

    Lego 4.14.0 => 4.17.4 (#10063)

--- packages/lego.rb
+++ packages/lego.rb
@@ -3,20 +3,20 @@ require 'package'
 class Lego < Package
   description "Let's Encrypt/ACME client and library written in Go"
   homepage 'https://go-acme.github.io/lego/'
-  version '4.14.0'
+  version '4.17.4'
   license 'MIT'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/go-acme/lego/releases/download/v4.14.0/lego_v4.14.0_linux_armv7.tar.gz',
-     armv7l: 'https://github.com/go-acme/lego/releases/download/v4.14.0/lego_v4.14.0_linux_armv7.tar.gz',
-       i686: 'https://github.com/go-acme/lego/releases/download/v4.14.0/lego_v4.14.0_linux_386.tar.gz',
-     x86_64: 'https://github.com/go-acme/lego/releases/download/v4.14.0/lego_v4.14.0_linux_amd64.tar.gz'
+    aarch64: "https://github.com/go-acme/lego/releases/download/v#{version}/lego_v#{version}_linux_armv7.tar.gz",
+     armv7l: "https://github.com/go-acme/lego/releases/download/v#{version}/lego_v#{version}_linux_armv7.tar.gz",
+       i686: "https://github.com/go-acme/lego/releases/download/v#{version}/lego_v#{version}_linux_386.tar.gz",
+     x86_64: "https://github.com/go-acme/lego/releases/download/v#{version}/lego_v#{version}_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '4c3494b4fb1df16d9e51d954a48a23416fb17c4aad033af7ab2f9cd1a7f31693',
-     armv7l: '4c3494b4fb1df16d9e51d954a48a23416fb17c4aad033af7ab2f9cd1a7f31693',
-       i686: 'ed1377f2e4a094ad6221349401bfe7d8bc52baff40cfcfd4a617db6830297069',
-     x86_64: '6ac2bbfc67069a62407389c1ac36590ca70d7adf1f940c4288c11b2d0b628dd7'
+    aarch64: 'bb5a8a9cd4176c222656701e61b6420bed03294fc88f48a39dc541c229f0b961',
+     armv7l: 'bb5a8a9cd4176c222656701e61b6420bed03294fc88f48a39dc541c229f0b961',
+       i686: 'b447b44a219334f659bec482e14b52a3f975cbcfe7ab65c0e0be890c6c681282',
+     x86_64: 'f362d59ff5b6f92c599e3151dcf7b6ed853de05533be179b306ca40a7b67fb47'
   })
 
   no_compile_needed
