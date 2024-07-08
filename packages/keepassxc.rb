b0f71018d184f983bba1803daaabcda196cad792
Ed Reel <edreel@gmail.com>
2024-07-08 14:46:20 -0500

    Keepassxc 2.7.8 => 2.7.9 (#10107)

--- packages/keepassxc.rb
+++ packages/keepassxc.rb
@@ -3,11 +3,12 @@ require 'package'
 class Keepassxc < Package
   description 'KeePass Cross-Platform Community Edition, the free, open source, light-weight and easy-to-use password manager.'
   homepage 'https://keepassxc.org/'
-  version '2.7.8'
+  version '2.7.9'
   license 'GPL-2 or GPL-3'
   compatibility 'x86_64'
-  source_url 'https://github.com/keepassxreboot/keepassxc/releases/download/2.7.8/KeePassXC-2.7.8-x86_64.AppImage'
-  source_sha256 '101b4d28a4960f01cc4dd6ca6a937dc4be60f47ccadd84b729d51401b4d346dd'
+  min_glibc '2.32'
+  source_url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-x86_64.AppImage"
+  source_sha256 '2a868b681a8ec4e381ac14203aec3d80ff6fa7a535fa102265a3ec9329b4b846'
 
   no_compile_needed
 
