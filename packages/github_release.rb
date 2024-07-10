af3b6cc9cd72294be30cd480c4aa5e3dac06a27b
Zopolis4 <creatorsmithmdt@gmail.com>
2024-07-11 08:58:27 +1000

    Update github_release homepage

--- packages/github_release.rb
+++ packages/github_release.rb
@@ -2,11 +2,11 @@ require 'package'
 
 class Github_release < Package
   description 'Commandline app to create and edit releases on Github (and upload artifacts)'
-  homepage 'https://github.com/aktau/github-release'
+  homepage 'https://github.com/github-release/github-release'
   version '0.7.2'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/aktau/github-release/archive/v0.7.2.tar.gz'
+  source_url 'https://github.com/github-release/github-release/archive/v0.7.2.tar.gz'
   source_sha256 '057d57b01cd45d0316e2d32b7593ff0f4bb493d4767b5701b21b54301d74ff48'
   binary_compression 'tar.xz'
 
