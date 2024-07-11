a1c161a03a0b9c52d525bc81d9337c16d36b796b
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-07-11 12:30:26 +1000

    daq 2.0.7 -> 3.0.15 (#10120)
    * libnl3 3.7.0 -> 3.9.0
    
    * libpcap 1.10.0 -> 1.10.4
    
    * daq 2.0.7 -> 3.0.15

--- packages/libnl3.rb
+++ packages/libnl3.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libnl3 < Package
+class Libnl3 < Autotools
   description 'libnl is a library for applications dealing with netlink sockets.'
-  homepage 'http://www.infradead.org/~tgr/libnl/'
-  version '3.7.0'
+  homepage 'https://github.com/thom311/libnl'
+  version '3.9.0'
   license 'LGPL-2.1'
   compatibility 'all'
   source_url 'https://github.com/thom311/libnl.git'
@@ -11,29 +11,16 @@ class Libnl3 < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6eb8a20bc0ff41d8a61116f92eb6426e6f085ce219b2af19ab99ac3d6a9d745e',
-     armv7l: '6eb8a20bc0ff41d8a61116f92eb6426e6f085ce219b2af19ab99ac3d6a9d745e',
-       i686: '123e2b875eb3a25373ba275c79fdfacf877fa0871a522e456715049c3c0f142d',
-     x86_64: 'de4409d700e3335d43d63acb1d50ff29e0827a28766075704ba54170d7858967'
+    aarch64: 'e78307d62f182317cfd64bc7f009d6ebee55338c25df46e2e86ed0e1289be93a',
+     armv7l: 'e78307d62f182317cfd64bc7f009d6ebee55338c25df46e2e86ed0e1289be93a',
+       i686: '8f05af9fc50c552a1e40b635336da213e948ee736fcc7b5ac9d21e67ca606baa',
+     x86_64: '84183099ad2f31076fe48d7fe6b31f82581d7d621f8b30fbf41c4350e3414e67'
   })
 
+  # depends_on 'check' => :build
   depends_on 'glibc' # R
 
-  def self.build
-    system 'autoupdate'
-    system './autogen.sh'
-    system "./configure #{CREW_OPTIONS} \
-              --enable-cli \
-              --enable-pthreads \
-              --disable-debug"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  configure_options '--disable-debug'
+  # https://github.com/thom311/libnl/issues/393
+  # run_tests
 end
