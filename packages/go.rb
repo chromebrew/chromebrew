73a659f29e1e70512e6bd162f97ff6dcaa3977f8
Ed Reel <edreel@gmail.com>
2024-07-11 09:31:44 -0500

    Go 1.22.4 => 1.22.5 (#10130)

--- packages/go.rb
+++ packages/go.rb
@@ -3,20 +3,20 @@ require 'package'
 class Go < Package
   description 'Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.'
   homepage 'https://go.dev'
-  version '1.22.4'
+  version '1.22.5'
   license 'BSD'
   compatibility 'all'
   source_url({
-    aarch64: 'https://go.dev/dl/go1.22.4.linux-armv6l.tar.gz',
-     armv7l: 'https://go.dev/dl/go1.22.4.linux-armv6l.tar.gz',
-       i686: 'https://go.dev/dl/go1.22.4.linux-386.tar.gz',
-     x86_64: 'https://go.dev/dl/go1.22.4.linux-amd64.tar.gz'
+    aarch64: "https://go.dev/dl/go#{version}.linux-armv6l.tar.gz",
+     armv7l: "https://go.dev/dl/go#{version}.linux-armv6l.tar.gz",
+       i686: "https://go.dev/dl/go#{version}.linux-386.tar.gz",
+     x86_64: "https://go.dev/dl/go#{version}.linux-amd64.tar.gz"
   })
   source_sha256({
-    aarch64: 'e2b143fbacbc9cbd448e9ef41ac3981f0488ce849af1cf37e2341d09670661de',
-     armv7l: 'e2b143fbacbc9cbd448e9ef41ac3981f0488ce849af1cf37e2341d09670661de',
-       i686: '47a2a8d249a91eb8605c33bceec63aedda0441a43eac47b4721e3975ff916cec',
-     x86_64: 'ba79d4526102575196273416239cca418a651e049c2b099f3159db85e7bade7d'
+    aarch64: '8c4587cf3e63c9aefbcafa92818c4d9d51683af93ea687bf6c7508d6fa36f85e',
+     armv7l: '8c4587cf3e63c9aefbcafa92818c4d9d51683af93ea687bf6c7508d6fa36f85e',
+       i686: '3ea4c78e6fa52978ae1ed2e5927ad17495da440c9fae7787b1ebc1d0572f7f43',
+     x86_64: '904b924d435eaea086515bc63235b192ea441bd8c9b198c507e85009e6e4c7f0'
   })
 
   no_compile_needed
