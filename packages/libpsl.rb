f4cfd30c1fb093df5a29c1bfd06d0e35f2f4c495
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-07-11 18:08:27 -0400

    Update meson, rebuild libpsl (#10132)
    * meson => 1.5
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * rebuild libpsl
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * suggested changes
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * suggested changes, use M90 & M91 containwrs for unit tests.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * use our bash in unit tests
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * adjust done logic
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * do not look at ALL_CHANGED_FILES unless it is not empty
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * adjust bash logic
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * fix case for non-existent variable check
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * use different bash test
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * change bash logic again
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libpsl.rb
+++ packages/libpsl.rb
@@ -3,19 +3,18 @@ require 'buildsystems/meson'
 class Libpsl < Meson
   description 'C library for the Public Suffix List'
   homepage 'https://github.com/rockdaboot/libpsl'
-  @_ver = '0.21.2'
-  version @_ver.to_s
+  version '0.21.5-1'
   license 'MIT'
   compatibility 'all'
-  source_url "https://github.com/rockdaboot/libpsl/releases/download/#{@_ver}/libpsl-#{@_ver}.tar.lz"
-  source_sha256 'aa3d706c452786d1345e094dae201cd36d81f03cf81d636d5cfc10d365907f17'
+  source_url 'https://github.com/rockdaboot/libpsl.git'
+  git_hashtag version.split('-').first
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '81c52e6dc9a2d935016d0f1d65dde2e2fa122f55e79c3a4a9465698041c5fe63',
-     armv7l: '81c52e6dc9a2d935016d0f1d65dde2e2fa122f55e79c3a4a9465698041c5fe63',
-       i686: '3c9c931dc72bf5dac871eef9dda2a866087b42457d6bd7535f2c760f33f27128',
-     x86_64: '2ee50e70cf121a7f46577e0494cbfe121eb13c23ae8bc308027e5315ec8b4ccb'
+    aarch64: '56fd7e35ce2706b2b4cf60a74b106ea0b923d14db34ad24e478f72bc070e9160',
+     armv7l: '56fd7e35ce2706b2b4cf60a74b106ea0b923d14db34ad24e478f72bc070e9160',
+       i686: '7bdb977732ba2994d809ca9eff9623f5d81c6642e03f7870887de5bc8cea1f38',
+     x86_64: '56c4274e773e48dccb6c6a0fa5f81f35b90794c63b121b252547daa1c473c797'
   })
 
   depends_on 'glibc' # R
