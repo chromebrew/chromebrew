70e76af07697b633c18af3420d78aa9dd3bac7c3
Zopolis4 <creatorsmithmdt@gmail.com>
2024-07-11 16:43:20 +1000

    i3 4.18.3 -> 4.23

--- packages/i3.rb
+++ packages/i3.rb
@@ -1,55 +1,55 @@
-require 'package'
+require 'buildsystems/meson'
 
-class I3 < Package
+class I3 < Meson
   description 'Improved tiling window manager'
   homepage 'https://i3wm.org/'
-  version '4.18.3'
+  version '4.23'
   license 'BSD'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://i3wm.org/downloads/i3-4.18.3.tar.bz2'
-  source_sha256 '53ae7903fad6eea830d58e949698e4a502c432c0d0a582659a0a59b1b995b10d'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/i3/i3.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f29889c961b37e86483c66e442ba8db653d1da7b0a3f4ab9d461f9acc7f75fb1',
-     armv7l: 'f29889c961b37e86483c66e442ba8db653d1da7b0a3f4ab9d461f9acc7f75fb1',
-     x86_64: '4af8555c05da78e9c24e81475b411c6397fc3afbca733016d422dc00dfc0402e'
+    aarch64: '09197fe2c8416f12d9aa328fe391ad7ec1b76401ead8873c5d78e65812cc6326',
+     armv7l: '09197fe2c8416f12d9aa328fe391ad7ec1b76401ead8873c5d78e65812cc6326',
+     x86_64: 'cc6c6aa08ea83ad04585638e2f2133b981615ccfd999f8489d65e87a33c4b4e9'
   })
 
+  depends_on 'cairo' # R
+  depends_on 'glib' # R
+  depends_on 'harfbuzz' # R
   depends_on 'libev'
+  depends_on 'libxcb' # R
+  depends_on 'libxkbcommon' # R
+  depends_on 'pango'
+  depends_on 'pcre2' # R
+  depends_on 'sommelier'
   depends_on 'startup_notification'
+  depends_on 'wayland' => :build
+  depends_on 'xcb_util' # R
   depends_on 'xcb_util_cursor'
   depends_on 'xcb_util_keysyms'
   depends_on 'xcb_util_wm'
   depends_on 'xcb_util_xrm'
   depends_on 'yajl'
-  depends_on 'wayland' => :build
-  depends_on 'sommelier'
 
-  def self.build
-    system "./configure #{CREW_OPTIONS} --disable-builddir --disable-maintainer-mode --enable-mans"
-    system "make -j#{CREW_NPROC}"
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    Dir.chdir "#{CREW_DEST_PREFIX}/bin" do
-      system "for f in \$(ls #{CREW_TARGET}-*); do g=\$(echo \$f | sed 's,#{CREW_TARGET}-,,'); ln -sf \$f \$g; done"
-      File.write 'starti3', <<~EOF, perm: 0o755
-        #!/bin/sh
-        stopsommelier
-        export DISPLAY=100.115.92.2:0
-        i3 "$@"
-        export DISPLAY=:0
-        startsommelier
-      EOF
-    end
+  meson_install_extras do
+    File.write "#{CREW_DEST_PREFIX}/bin/starti3", <<~EOF, perm: 0o755
+      #!/bin/sh
+      stopsommelier
+      export DISPLAY=100.115.92.2:0
+      i3 "$@"
+      export DISPLAY=:0
+      startsommelier
+    EOF
   end
 
   def self.postinstall
-    puts
-    puts 'To use this package, you need to download XServer XSDL from Google Play Store'.lightblue
-    puts "Use 'starti3' instead of 'i3' to execute this package".lightblue
-    puts "Open XServer XSDL before running 'starti3'"
+    ExitMessage.add <<~EOT.lightblue
+      To use this package, you need to download XServer XSDL from Google Play Store.
+      Use 'starti3' instead of 'i3' to execute this package.
+      Open XServer XSDL before running 'starti3'.
+    EOT
   end
 end
