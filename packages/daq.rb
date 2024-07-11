8a94eaae020e535da0b6ba4372a557bbfd267244
Zopolis4 <creatorsmithmdt@gmail.com>
2024-07-11 10:30:29 +1000

    daq 2.0.7 -> 3.0.15

--- packages/daq.rb
+++ packages/daq.rb
@@ -1,38 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Daq < Package
+class Daq < Autotools
   description 'Data Acquisition library, for packet I/O.'
   homepage 'https://www.snort.org'
-  version '2.0.7'
+  version '3.0.15'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://www.snort.org/downloads/snort/daq-2.0.7.tar.gz'
-  source_sha256 'd1f6709bc5dbddee3fdf170cdc1e49fb926e2031d4869ecf367a8c47efc87279'
+  source_url 'https://github.com/snort3/libdaq.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8350c5c96bdb011e2cffc6a5b217fd9d56fccfa7686ee95eb4c3553a927983d9',
-     armv7l: '8350c5c96bdb011e2cffc6a5b217fd9d56fccfa7686ee95eb4c3553a927983d9',
-       i686: 'b390dedcfbe3465e2ae9396246c74f12c53de573b212767c062c63c5b842a6c7',
-     x86_64: 'a91c2f6bac768394669178837bbf9491b359e79e4b0aa2843af80ad0796bdf8e'
+    aarch64: '3c809aeb3c5663f7a3b84e573679dd5c68acb6b4f188ad7961721bf45470019f',
+     armv7l: '3c809aeb3c5663f7a3b84e573679dd5c68acb6b4f188ad7961721bf45470019f',
+       i686: 'cdf573c9b712ea6e17b6ac49bdd8553343aecedeb44987439f2e2428be5d4aec',
+     x86_64: '1f5b8b93b32144d8c603ef13752a7ddbee93c16b5df5a0bcbacd03d5463b91fd'
   })
 
+  # depends_on 'cmocka' => :build
   depends_on 'libpcap'
 
-  def self.patch
-    system 'filefix' # Fix /usr/bin/file: No such file or directory
-  end
-
-  def self.build
-    system "./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  # https://github.com/snort3/libdaq/issues/30
+  # run_tests
 end
