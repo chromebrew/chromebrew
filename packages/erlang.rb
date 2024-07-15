0c00ea328a6742b8db96c33dd8a80dadfc8a1a6b
Ed Reel <edreel@gmail.com>
2024-07-15 07:41:54 +0000

    Erlang 27.0 => 27.0.1

--- packages/erlang.rb
+++ packages/erlang.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Erlang < Autotools
   description 'Erlang is a programming language used to build massively scalable soft real-time systems with requirements on high availability.'
   homepage 'https://www.erlang.org/'
-  version '27.0'
+  version '27.0.1'
   license 'Apache-2.0'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/erlang/otp.git'
@@ -11,15 +11,13 @@ class Erlang < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '0df239e644449e6611cac6b31411b70018633682f314991179db6addcb4407d5',
-     armv7l: '0df239e644449e6611cac6b31411b70018633682f314991179db6addcb4407d5',
-     x86_64: '3cf4d3e96f1acc8a067792c3f8b016a919c0636c7c52a0db873fd19a8f0e6ffb'
+    aarch64: 'e217ec9ee6e3603b264bb551543ae204e7790c7b370213d049e7ce92f59f4463',
+     armv7l: 'e217ec9ee6e3603b264bb551543ae204e7790c7b370213d049e7ce92f59f4463',
+     x86_64: 'bdcbe7622352f10703b073311deabeaf9fb2994b49e945e7667373ff56da3866'
   })
 
   depends_on 'openjdk8'
   depends_on 'wxwidgets'
 
-  year2038 = ARCH == 'x86_64' ? '' : '--disable-year2038'
-
-  configure_options "ERL_OTP=#{Dir.pwd} #{year2038}"
+  configure_options "ERL_OTP=#{Dir.pwd} #{'--disable-year2038' unless ARCH.eql?('x86_64')}"
 end
