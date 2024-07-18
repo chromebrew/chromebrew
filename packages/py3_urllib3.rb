02ca31c041e8c567912ccaac5feaa595514b5a2e
Satadru Pramanik <satadru@gmail.com>
2024-07-18 10:28:30 -0400

    update version variable for py3_urllib3
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_urllib3.rb
+++ packages/py3_urllib3.rb
@@ -3,8 +3,7 @@ require 'buildsystems/pip'
 class Py3_urllib3 < Pip
   description 'URLlib3 is an HTTP library with thread-safe connection pooling, file post, and more.'
   homepage 'https://urllib3.readthedocs.io/'
-  @_ver = '2.2.2'
-  version "#{@_ver}-py3.12"
+  version "2.2.2-py3.12"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
