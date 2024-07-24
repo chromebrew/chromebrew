49aa173749c5ddf764651fcdc8b001800e431ad7
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-07-24 17:02:11 +1000

    fpc 3.2.2 -> 3.2.3 (#10076)

--- packages/fpc.rb
+++ packages/fpc.rb
@@ -3,56 +3,48 @@ require 'package'
 class Fpc < Package
   description 'Free Pascal is a 32, 64 and 16 bit professional Pascal compiler.'
   homepage 'https://www.freepascal.org/'
-  version '3.2.2'
+  version '3.2.3'
   license 'GPL-2 and LGPL-2.1-with-linking-exception'
   compatibility 'all'
-  case ARCH
-  when 'aarch64', 'armv7l'
-    source_url 'https://downloads.sourceforge.net/project/freepascal/Linux/3.2.2/fpc-3.2.2.arm-linux.tar'
-    source_sha256 '971bbcaa1934286ccdaf3998d338afc19a33235e910f8881c75579ed5d6adefd'
-  when 'i686'
-    source_url 'https://downloads.sourceforge.net/project/freepascal/Linux/3.2.2/fpc-3.2.2.i386-linux.tar'
-    source_sha256 'f62980ac0b2861221f79fdbff67836aa6912a4256d4192cfa4dfa0ac5b419958'
-  when 'x86_64'
-    source_url 'https://downloads.sourceforge.net/project/freepascal/Linux/3.2.2/fpc-3.2.2.x86_64-linux.tar'
-    source_sha256 '5adac308a5534b6a76446d8311fc340747cbb7edeaacfe6b651493ff3fe31e83'
-  end
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.com/freepascal.org/fpc/source.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '84701dedbee28a6db2386366318a8c2e27c389a758ea8033502d40fa2f4ec588',
-     armv7l: '84701dedbee28a6db2386366318a8c2e27c389a758ea8033502d40fa2f4ec588',
-       i686: '20062e277f6432b76115a74b6b46a38888ef696f15bbf79efce8546a1aff9a46',
-     x86_64: 'b5b9152ce23f5e7f33ab8b788ef80ca440d2d26ef300321f1e944c24051f9b45'
+    aarch64: '59e1525374c44782be0bc0cdaa41620b0f6317403640e9530e4524be7ba25817',
+     armv7l: '59e1525374c44782be0bc0cdaa41620b0f6317403640e9530e4524be7ba25817',
+       i686: 'dd54485d5db294e4cd713debc7e09b3f00150b46574dfd7bf16b21e781428729',
+     x86_64: '90ba51e07916559f4cfbdc41ce3f57bd8498f5b35138b8822ea7338d7cd9d256'
   })
 
-  def self.patch
-    system "sed -i 's,PREFIX=\"\$HOME/fpc-\$VERSION\",PREFIX=#{CREW_DEST_PREFIX},' install.sh"
-    system "sed -i 's,\$PREFIX/lib,#{CREW_DEST_LIB_PREFIX},' install.sh"
+  print_source_bashrc
+
+  def self.build
+    # https://gitlab.com/freepascal.org/fpc/source/-/issues/40841
+    # fpc isn't fooled by our docker emulation, and thus disables reading from the config because it thinks we're cross-compiling.
+    # See https://gitlab.com/freepascal.org/fpc/source/-/commit/898a4debcab843b2f6f5c29b795a517485582dfb
+    # Because we don't read from the config, we don't get the correct library paths.
+    # The correct thing to do here would be to find the fault in our docker setup that reveals we're running under emulation, and fix it.
+    # In the meantime, though, just pass the correct library path manually.
+    # Chromebrew messes things up if we don't pass this in one string, adding single quotes around OPT which break things.
+    system "make OVERRIDEVERSIONCHECK=1 OPT='#{"-Fl#{CREW_LIB_PREFIX}" unless ARCH.eql?('x86_64')} -k-L#{CREW_LIB_PREFIX}' all"
   end
 
   def self.install
-    system './install.sh'
-    FileUtils.ln_sf "#{CREW_LIB_PREFIX}/fpc/#{version}/ppcarm", "#{CREW_DEST_PREFIX}/bin/ppcarm"
-    FileUtils.mv "#{CREW_DEST_PREFIX}/man", CREW_DEST_MAN_PREFIX
+    system 'make', "PREFIX=#{CREW_DEST_PREFIX}", 'install'
+    compiler = { aarch64: 'ppcarm', armv7l: 'ppcarm', i686: 'ppc386', x86_64: 'ppcx64' }
+    FileUtils.ln_sf "#{CREW_PREFIX}/lib/fpc/#{version}/#{compiler[ARCH.to_sym]}", "#{CREW_DEST_PREFIX}/bin/#{compiler[ARCH.to_sym]}"
+    # Set up the default fpc config file.
+    system "#{CREW_DEST_PREFIX}/lib/fpc/#{version}/samplecfg #{CREW_DEST_PREFIX}/lib/fpc/#{version} #{CREW_DEST_PREFIX}/etc"
+    system "sed -i '/# searchpath for libraries/a\-Fl#{CREW_LIB_PREFIX}' #{CREW_DEST_PREFIX}/etc/fpc.cfg"
+    File.write 'fpcenv', <<~FPC_ENV_EOF
+      # Set PPC_CONFIG_PATH so that fpc finds fpc.cfg
+      export PPC_CONFIG_PATH=#{CREW_PREFIX}/etc
+    FPC_ENV_EOF
+    FileUtils.install 'fpcenv', "#{CREW_DEST_PREFIX}/etc/env.d/fpc", mode: 0o644
   end
 
   def self.postinstall
-    puts "\nType 'fp' to start.\n".lightblue
-  end
-
-  def self.remove
-    print 'Would you like to remove the config directories? [y/N] '
-    response = $stdin.gets.chomp.downcase
-    config_dirs = ["#{HOME}/.fpc.cfg", "#{HOME}/.config/fppkg.cfg", "#{HOME}/.fppkg"]
-    config_dirs.each do |config_dir|
-      case response
-      when 'y', 'yes'
-        FileUtils.rm_rf config_dir
-        puts "#{config_dir} removed.".lightred
-      else
-        puts "#{config_dir} saved.".lightgreen
-      end
-    end
+    ExitMessage.add "Type 'fp' to start.".lightblue
   end
 end
