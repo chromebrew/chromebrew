d3b622294ddc864cff79217f54bfaf97c73c247d
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-07-24 13:53:50 +1000

    bashdb 5.0-1.1.2-abac -> 5.0-1.1.2-1f1118d (#10201)

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.49.6'
+CREW_VERSION = '1.49.7'
 
 # Kernel architecture.
 KERN_ARCH = Etc.uname[:machine]
