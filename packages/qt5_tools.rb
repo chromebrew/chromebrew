ce8ed2fcfa206b51c06ddb854161068db6bdbe34
Zopolis4 <creatorsmithmdt@gmail.com>
2024-07-30 00:16:10 +1000

    qt5_tools 5.15.11-38ae810 -> kde-5.15.14-f82ed36

--- packages/qt5_tools.rb
+++ packages/qt5_tools.rb
@@ -3,17 +3,17 @@ require 'buildsystems/qmake'
 class Qt5_tools < Qmake
   description 'Qt Tools'
   homepage 'https://github.com/qt/qttools'
-  version '5.15.11-38ae810'
+  version 'kde-5.15.14-f82ed36'
   license 'FDL, GPL-2, GPL-3, GPL-3-with-qt-exception and LGPL-3'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://invent.kde.org/qt/qt/qttools.git'
-  git_hashtag '38ae810be3fb4984f75b55a16d3413e35d701af1' # from kde/5.15 branch
+  git_hashtag 'f82ed367d1b80b69d738cfcde534b75854a45476'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2f8ee51fd04a90af6ae36435acd9db326464235ec752e3796450a3fe5a5cdbb6',
-     armv7l: '2f8ee51fd04a90af6ae36435acd9db326464235ec752e3796450a3fe5a5cdbb6',
-     x86_64: '6f279180b9a03233b6b7e593736fec1ac9891773c3830b8b1dc72025174518aa'
+    aarch64: '4bf3d78b1ffd262bd5cb718c3b2eee22510eb20efadd907e688e56e405eda9d9',
+     armv7l: '4bf3d78b1ffd262bd5cb718c3b2eee22510eb20efadd907e688e56e405eda9d9',
+     x86_64: '1e4621ac6e8ab9216f6ef0418381513ceaa952f2ee6524aa31837afa9d8f362f'
   })
 
   depends_on 'gcc_lib' # R
