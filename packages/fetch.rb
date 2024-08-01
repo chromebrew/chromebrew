3da8ec776846fdbedf893519dfcc8160896754f7
Zopolis4 <creatorsmithmdt@gmail.com>
2024-08-01 11:33:26 +1000

    fetch 12.0.10 -> 12.0.11

--- packages/fetch.rb
+++ packages/fetch.rb
@@ -1,39 +1,28 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Fetch < Package
+class Fetch < CMake
   description 'FreeBSD Fetch retrieves files by URL.'
-  homepage 'https://github.com/jrmarino/fetch-freebsd/'
-  version '12.0.10'
+  homepage 'https://github.com/jrmarino/fetch-freebsd'
+  version '12.0.11'
   license 'BSD-3'
   compatibility 'all'
   source_url 'https://github.com/jrmarino/fetch-freebsd.git'
   git_hashtag "v#{version}"
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'cf7304b9e2227a67de69305f7652cf47e8902ab88711115e3abc5b4a1e3dfd48',
-     armv7l: 'cf7304b9e2227a67de69305f7652cf47e8902ab88711115e3abc5b4a1e3dfd48',
-       i686: '336be94ecea92cadf1b89b00508f2c9678fbda29291c5825f5068589a2b8b39d',
-     x86_64: 'fbf71d42114f4b398f6ad0b49917f24710d191e45f9bc3fe4d69fdf5ee27a0ab'
+    aarch64: '41075f997abd150a9b45550c189366757304c1604ebbf50154ecf6b9d76474dc',
+     armv7l: '41075f997abd150a9b45550c189366757304c1604ebbf50154ecf6b9d76474dc',
+       i686: '2a88b20b8fddddab48017a39bb7087c5858e803433d1749b38bbcc77df82d34f',
+     x86_64: '62999f08847a7a9c51fcfa70abb595906553ed8eb8fa75c2b11e67baa639b57c'
   })
 
-  def self.build
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "cmake -G Ninja #{CREW_CMAKE_OPTIONS} \
-              -DFETCH_LIBRARY=ON \
-              -DFETCH_PROGRAM=ON \
-              -DUSE_SYSTEM_SSL=ON .."
-    end
-    system 'samu -C builddir'
+  depends_on 'openssl'
+
+  def self.patch
+    downloader 'https://patch-diff.githubusercontent.com/raw/jrmarino/fetch-freebsd/pull/6.patch', 'd872cae4f979c563be3659657a323ce444a469167733712a2e96f578ff9d7427'
+    system 'git apply 6.patch'
   end
 
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
-    if CREW_LIB_SUFFIX == '64'
-      FileUtils.mkdir_p CREW_DEST_LIB_PREFIX
-      FileUtils.mv Dir.glob("#{CREW_DEST_PREFIX}/lib/*"), CREW_DEST_LIB_PREFIX
-      FileUtils.rm_r "#{CREW_DEST_PREFIX}/lib/"
-    end
-  end
+  cmake_options '-DFETCH_LIBRARY=ON -DUSE_SYSTEM_SSL=ON'
 end
