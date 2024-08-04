3959f322cc9091a040102d21fca706d225dd1b1e
Satadru Pramanik <satadru@gmail.com>
2024-08-03 23:00:50 -0400

    add icu version to libarchive
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libarchive.rb
+++ packages/libarchive.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Libarchive < Autotools
   description 'Multi-format archive and compression library.'
   homepage 'https://www.libarchive.org/'
-  version '3.7.4'
+  version '3.7.4-icu75.1'
   license 'BSD, BSD-2, BSD-4 and public-domain'
   compatibility 'all'
-  source_url "https://www.libarchive.org/downloads/libarchive-#{version}.tar.xz"
+  source_url "https://www.libarchive.org/downloads/libarchive-#{version.split('-').first}.tar.xz"
   source_sha256 'f887755c434a736a609cbd28d87ddbfbe9d6a3bb5b703c22c02f6af80a802735'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c2e27421af7c3f046eda5b1699fc57483e3cefbfbcdec98fcf717255cde4b623',
-     armv7l: 'c2e27421af7c3f046eda5b1699fc57483e3cefbfbcdec98fcf717255cde4b623',
-       i686: '9264b769b88313287d575e55f5a8aff1c3ebbfa19013f459b09b9527f9d28721',
-     x86_64: '68f0e3c3e2f8e1f123368310b3e5a64f8c08a6ebc30d0ba0905efe4d960c6c38'
+    aarch64: '86a2891e468a8b0aba9f017ddb9986193dc1416c9971edd00a62e84c1132ccfe',
+     armv7l: '86a2891e468a8b0aba9f017ddb9986193dc1416c9971edd00a62e84c1132ccfe',
+       i686: '2aef89c9ed12b699ebb226a73acb7d6849825af6b0e9ef4604e685f85f15e594',
+     x86_64: 'a055feffdd4c67283a1584b198073f48633d64f7f58297b1411bdfd011053fa8'
   })
 
   depends_on 'acl' # R
