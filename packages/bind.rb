77beae1442ec99b9260de810f05ef95b10b16d40
Satadru Pramanik <satadru@gmail.com>
2024-08-05 22:31:12 -0400

    rebuild bind
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/bind.rb
+++ packages/bind.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Bind < Autotools
   description 'BIND is open source software that enables you to publish your Domain Name System (DNS) information on the Internet, and to resolve DNS queries for your users.'
   homepage 'https://www.isc.org/bind/'
-  version '9.20.0'
+  version '9.20.0-icu75.1'
   license 'Apache-2.0, BSD, BSD-2, GPL-2, HPND, ISC and MPL-2.0'
   compatibility 'all'
-  source_url "https://ftp.isc.org/isc/bind9/#{version}/bind-#{version}.tar.xz"
+  source_url "https://ftp.isc.org/isc/bind9/#{version.split('-').first}/bind-#{version.split('-').first}.tar.xz"
   source_sha256 'cc580998017b51f273964058e8cb3aa5482bc785243dea71e5556ec565a13347'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '271147a451d6a6c81231a7d1e2e74480f8e24e4cf26fc01dc9d4b40c918cf980',
-     armv7l: '271147a451d6a6c81231a7d1e2e74480f8e24e4cf26fc01dc9d4b40c918cf980',
-       i686: '8c4401d053912ccd13417d97edbaa8ba3a1c5c5bd6db0653f71d216f0d251f0b',
-     x86_64: '0e0bf08ec93ad2ca55f2e0393882eec31f70185adf0a0c1a93a641d436d95425'
+    aarch64: 'c38d7b10a9da318f591bfc9daddfa427f4559bf836ab7c4d85a85b9dff5e64f8',
+     armv7l: 'c38d7b10a9da318f591bfc9daddfa427f4559bf836ab7c4d85a85b9dff5e64f8',
+       i686: 'bf43eb116dbce313bb2842af5b34221c1d1ffbcc9da14f7b103638666d3bde9b',
+     x86_64: '15e82de63f95ca9ee4f48627e2240bac20e0991fb55b8e8006a5d5b8ac27d262'
   })
 
   depends_on 'e2fsprogs' # R
@@ -28,7 +28,7 @@ class Bind < Autotools
   depends_on 'libidn2' # R
   depends_on 'libnghttp2' # R
   depends_on 'libunistring' => :build
-  depends_on 'liburcu' => :build
+  depends_on 'liburcu' # R
   depends_on 'libuv' # R
   depends_on 'libxml2' # R
   depends_on 'ncurses' => :build
