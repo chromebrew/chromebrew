a97f223f562644c7ed789ac60c9bdb5b660076b2
Ed Reel <edreel@gmail.com>
2024-08-06 17:19:10 -0500

    Signal_desktop 7.18.0 => 7.19.0 (#10262)

--- packages/signal_desktop.rb
+++ packages/signal_desktop.rb
@@ -3,12 +3,12 @@ require 'package'
 class Signal_desktop < Package
   description 'Private Messenger for Windows, Mac, and Linux'
   homepage 'https://signal.org/'
-  version '7.18.0'
+  version '7.19.0'
   license 'AGPL-3.0'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://updates.signal.org/desktop/apt/pool/s/signal-desktop/signal-desktop_#{version}_amd64.deb"
-  source_sha256 'c48dc60acf597a410d92db927fd34dc6838e1ae62d2ab395f0d837785cb8f773'
+  source_sha256 '94ef0638273f2f858ef9a3d1eaa4a7dc7425a9e081eade6958010d45739385d2'
 
   no_compile_needed
   no_shrink
@@ -34,6 +34,6 @@ class Signal_desktop < Package
   end
 
   def self.postinstall
-    ExitMessage.add "\nType 'signal-desktop' to get started.\n".lightblue
+    ExitMessage.add "\nType 'signal-desktop' to get started.\n"
   end
 end
