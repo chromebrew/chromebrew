356a7a3b45270b6cf35914c96b6ac288343e36bc
Zopolis4 <creatorsmithmdt@gmail.com>
2024-08-06 20:53:26 +1000

    libraw1394 2.0.5-1 -> 2.1.2

--- packages/libraw1394.rb
+++ packages/libraw1394.rb
@@ -1,32 +1,21 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libraw1394 < Package
+class Libraw1394 < Autotools
   description 'libraw1394 provides direct access to the IEEE 1394 bus through the Linux 1394 subsystem\'s raw1394 user space interface.'
   homepage 'https://sourceforge.net/projects/libraw1394/'
-  version '2.0.5-1'
+  version '2.1.2'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'https://downloads.sourceforge.net/project/libraw1394/libraw1394/libraw1394-2.0.5.tar.gz'
-  source_sha256 '50e7b812f09ec8181fc060e7e25e260017c16c1b41a04c51e23446f26fa109d4'
-  binary_compression 'tpxz'
+  source_url "https://www.kernel.org/pub/linux/libs/ieee1394/libraw1394-#{version}.tar.xz"
+  source_sha256 '03ccc69761d22c7deb1127fc301010dd13e70e44bb7134b8ff0d07590259a55e'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f98d25737550b990177048ca94b6bfbc262c1029bece77c27885053070e9bcae',
-     armv7l: 'f98d25737550b990177048ca94b6bfbc262c1029bece77c27885053070e9bcae',
-       i686: '53af9bb5729d454481705f5d7f8b4f1cb69178cb1955da7392f52e334cf6cd4c',
-     x86_64: '874f0550d05403d7b90c448430c82fab2490a61d826c7b9d1c49aeb7441f1476'
+    aarch64: 'c275175e2e4e8be4fa817d3398faf415ccdd74bb37b0d4e856f6381dedae5cc4',
+     armv7l: 'c275175e2e4e8be4fa817d3398faf415ccdd74bb37b0d4e856f6381dedae5cc4',
+       i686: '9bee6317419d78c8497fce66f5f2e6c35d4dd124a206d3d32d618601a5f82c79',
+     x86_64: '11474a36fe379f1a95f3280d0dfa0ae6453c1ef030197571dd48210e76c20c43'
   })
 
-  def self.build
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  run_tests
 end
