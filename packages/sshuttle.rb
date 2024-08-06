d7b92b81c4b737f4f02ef04fe6be23a9983ef699
Zopolis4 <creatorsmithmdt@gmail.com>
2024-08-06 11:21:55 +1000

    sshuttle 1.0.5-1 -> 1.1.2

--- packages/sshuttle.rb
+++ packages/sshuttle.rb
@@ -1,31 +1,22 @@
-require 'package'
+require 'buildsystems/python'
 
-class Sshuttle < Package
+class Sshuttle < Python
   description 'Full-featured VPN over an SSH tunnel'
-  homepage 'https://github.com/sshuttle/sshuttle/'
-  @_ver = '1.0.5'
-  version "#{@_ver}-1"
+  homepage 'https://github.com/sshuttle/sshuttle'
+  version '1.1.2'
   license 'LGPL-2.1+'
   compatibility 'all'
   source_url 'https://github.com/sshuttle/sshuttle.git'
-  git_hashtag "v#{@_ver}"
-  binary_compression 'tpxz'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6828c870bd62da9edb41061bf39e99383a1a53823356d6f1a443c6b86eb19f1a',
-     armv7l: '6828c870bd62da9edb41061bf39e99383a1a53823356d6f1a443c6b86eb19f1a',
-       i686: 'da7fdd4ddd53b363e92d2f72e871dee467c20bb7a79e00a817afc0fa037bb843',
-     x86_64: '4ea4ba19a20a57fb2006b623b7c77e0e84fb9598e069ebcc67357ce50ce744b2'
+    aarch64: 'f8c81a5d421f5c393ed4d512c8ec40fdbddafc6e203fa21083997b004df81a05',
+     armv7l: 'f8c81a5d421f5c393ed4d512c8ec40fdbddafc6e203fa21083997b004df81a05',
+       i686: 'ced49c2fd3203e735589e16ab842a9187d4c9b0abdf6d27353d013d5497b7fe9',
+     x86_64: '1e168c1546b5c8f10f8e99a7c0dde8edb9b5d79a6a53a398ec5bd3e95705557e'
   })
 
   depends_on 'py3_psutil'
-  depends_on 'python3' => :build
-
-  def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
-
-  def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
-  end
+  depends_on 'python3'
 end
