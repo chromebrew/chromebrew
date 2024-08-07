6ca69ce4f143f46540609835be0b5c4ebe4a7be9
Satadru Pramanik <satadru@gmail.com>
2024-08-07 19:35:32 -0400

    glibc 2.37 patchelf workaround for ChromeOS broken glibc 2.37
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/glibc_lib237.rb
+++ packages/glibc_lib237.rb
@@ -4,20 +4,19 @@ Package.load_package("#{__dir__}/glibc_build237.rb")
 class Glibc_lib237 < Package
   description 'glibc libraries'
   homepage Glibc_build237.homepage
-  version '2.37-1' # Do not use @_ver here, it will break the installer.
+  version '2.37-patchelf1' # Do not use @_ver here, it will break the installer.
   license Glibc_build237.license
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'efb6e1f5593f97b6fe8232e7e265911bba0f632e178f86c3f0412c1265cf2962',
-     armv7l: 'efb6e1f5593f97b6fe8232e7e265911bba0f632e178f86c3f0412c1265cf2962',
-     x86_64: '2645a7f26f0d0f007c57f296ef18c853253b98da4756f7ada840a40147c8c8ed'
+    aarch64: '7327f9e37705d2b46a2a89300c154e16aa078f93bc3a6ce69932783ce6f56e50',
+     armv7l: '7327f9e37705d2b46a2a89300c154e16aa078f93bc3a6ce69932783ce6f56e50',
+     x86_64: '37c91128508324b3f865687061e46df5f650cc634489ec9d1a96b88f5917451a'
   })
 
   depends_on 'glibc_build237' => :build
-  depends_on 'glibc' # R
 
   conflicts_ok
   no_shrink
@@ -42,10 +41,18 @@ class Glibc_lib237 < Package
       FileUtils.install @filename_target, @destpath
     end
     # Symlinks to system libraries.
-    %w[libanl.so.1 libc_malloc_debug.so.0 libc.so.6 libdl.so.2 libm.so.6
-       libmvec.so.1 libnss_dns.so.2 libnss_files.so.2 libpthread.so.0
-       libresolv.so.2 librt.so.1 libthread_db.so.1 libutil.so.1].each do |lib|
-      FileUtils.ln_sf "/#{ARCH_LIB}/#{lib}", "#{CREW_DEST_LIB_PREFIX}/#{lib}"
+    @glibc_libs = %w[libanl.so.1 libc_malloc_debug.so.0 libc.so.6 libdl.so.2 libm.so.6
+                     libmvec.so.1 libnss_dns.so.2 libnss_files.so.2 libpthread.so.0
+                     libresolv.so.2 librt.so.1 libthread_db.so.1 libutil.so.1]
+    @glibc_libs -= ['libc.so.6'] if Kernel.system("patchelf --print-needed #{File.join(CREW_LIB_PREFIX, 'libc.so.6')} | grep -q libC.so.6")
+    @glibc_libs -= ['libm.so.6'] if Kernel.system("patchelf --print-needed #{File.join(CREW_LIB_PREFIX, 'libm.so.6')} | grep -q libC.so.6")
+
+    @glibc_libs.each do |lib|
+      if ARCH == 'x86_64'
+        puts "skipping symlink of system/#{ARCH_LIB}/#{lib} into #{CREW_DEST_LIB_PREFIX}/#{lib}...".orange
+      else
+        FileUtils.ln_sf "/#{ARCH_LIB}/#{lib}", "#{CREW_DEST_LIB_PREFIX}/#{lib}"
+      end
     end
   end
 end
