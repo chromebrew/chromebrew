fac254478ab197ee7b213bb5d7739f20aff30ce1
Ed Reel <edreel@gmail.com>
2024-08-08 16:54:55 -0500

    Vivaldi 6.8.3381.50-1 => 6.8.3381.53-1 (#10282)

--- packages/vivaldi.rb
+++ packages/vivaldi.rb
@@ -3,7 +3,7 @@ require 'package'
 class Vivaldi < Package
   description 'Vivaldi is a new browser that blocks unwanted ads, protects you from trackers, and puts you in control with unique built-in features.'
   homepage 'https://vivaldi.com/'
-  version '6.8.3381.50-1'
+  version '6.8.3381.53-1'
   license 'Vivaldi'
   compatibility 'x86_64 aarch64 armv7l'
   min_glibc '2.29'
@@ -23,10 +23,10 @@ class Vivaldi < Package
   case ARCH
   when 'aarch64', 'armv7l'
     arch = 'armhf'
-    source_sha256 'b5b2007c7d4db77d1f3cef39177464dc60472cc66b5e8e274e5396f0e42aabf7'
+    source_sha256 'b5d26121a80fa670a9b46619642b83360850510677567edfcc81fbfd713a6892'
   when 'x86_64'
     arch = 'amd64'
-    source_sha256 'c30e1b8cca7017899f0231e9cc77ce761b48e97395e71d9526eca3c3ec7a39d0'
+    source_sha256 'e962c6bddf4aa7a7d11b51f130e8cae0c84ae1ea188f0120606251388ee53366'
   end
 
   source_url "https://downloads.vivaldi.com/stable/vivaldi-stable_#{version}_#{arch}.deb"
