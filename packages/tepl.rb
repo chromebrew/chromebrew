3d9e3df4516f4ee05d15249df7ede0f97dc1ec20
Satadru Pramanik <satadru@gmail.com>
2024-08-09 12:37:09 -0400

    update tepl packages
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/tepl.rb
+++ packages/tepl.rb
@@ -1,16 +1,14 @@
 require 'package'
-Package.load_package("#{__dir__}/tepl_5.rb")
 Package.load_package("#{__dir__}/tepl_6.rb")
 
 class Tepl < Package
-  description Tepl_5.description
-  homepage Tepl_5.homepage
-  version "#{Tepl_5.version}+#{Tepl_6.version}"
-  license Tepl_5.license
-  compatibility Tepl_5.compatibility
+  description Tepl_6.description
+  homepage Tepl_6.homepage
+  version Tepl_6.version
+  license Tepl_6.license
+  compatibility Tepl_6.compatibility
 
   is_fake
 
-  depends_on 'tepl_5'
   depends_on 'tepl_6'
 end
