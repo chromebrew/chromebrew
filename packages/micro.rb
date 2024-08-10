a48f2c68d0883723586463e25df95dd0c0b6b76b
shogi-dojo <95250967+shogi-dojo@users.noreply.github.com>
2024-08-10 20:11:34 +0300

    fix: update sha256 for x86

--- packages/micro.rb
+++ packages/micro.rb
@@ -7,7 +7,7 @@ class Micro < Package
   license 'MIT'
   compatibility 'all'
   binary_compression 'tar.zst'
-  
+
   no_compile_needed
 
   case ARCH
@@ -16,10 +16,10 @@ class Micro < Package
     source_sha256 'adb9cf644354a5c85819db40e1a427f0f4951b172597bbcd3ef94ecc4a8c4b75'
   when 'i686'
     source_url 'https://github.com/zyedidia/micro/releases/download/v2.0.13/micro-2.0.13-linux32.tar.gz'
-    source_sha256 'd72d511b4f0fec5bb614b38607e68c5f0918f836d0f60e87c48f0b18e2eab916'
+    source_sha256 'a711d7281152b2b578a7b91bca70fe6782ca3e317e99be640dbb6bb3eb5a2bcd'
   when 'x86_64'
     source_url 'https://github.com/zyedidia/micro/releases/download/v2.0.13/micro-2.0.13-linux64.tar.gz'
-    source_sha256 'e7d4c9427f9fdfed78e69d42cf518e93ae15fc8f70b7f0f87d292ed81206e900'
+    source_sha256 'a50e405d3d09d58f6b2c182429c18537a05f317dc0c3c9cb834b3271362e4781'
   end
 
   def self.install
