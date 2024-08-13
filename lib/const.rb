b01b64bf8a58e8ebbbeaeff1a41d90b9fed0082b
Satadru Pramanik <satadru@gmail.com>
2024-08-13 18:12:09 -0400

    bump version
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -2,7 +2,7 @@
 # Defines common constants used in different parts of crew
 require 'etc'
 
-CREW_VERSION = '1.50.3'
+CREW_VERSION = '1.50.4'
 
 # Kernel architecture.
 KERN_ARCH = Etc.uname[:machine]
