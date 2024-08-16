ae8574912a5ac3d267e7f2b5901bb87aa9ba569a
Satadru Pramanik <satadru@gmail.com>
2024-08-16 01:36:54 -0400

    Make git changes local not global.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/fixup.rb
+++ lib/fixup.rb
@@ -32,15 +32,15 @@ if File.exist?("#{CREW_PREFIX}/etc/env.d/00-path") && File.exist?("#{CREW_PREFIX
   FileUtils.rm "#{CREW_PREFIX}/etc/env.d/path"
 end
 
-# Set new sparse-checkout paths for commands directory
 Dir.chdir CREW_LIB_PATH do
+  # Set new sparse-checkout paths for commands directory
   system 'git sparse-checkout add commands'
   system 'git sparse-checkout reapply'
-end
 
-# Set git timeout values for situations where GitHub is down.
-system 'git config --global http.lowSpeedLimit 1000' if `git config --local http.lowSpeedLimit`.empty?
-system 'git config --global http.lowSpeedTime 5' if `git config --local http.lowSpeedTime`.empty?
+  # Set git timeout values for situations where GitHub is down.
+  system 'git config --local http.lowSpeedLimit 1000' if `git config --local http.lowSpeedLimit`.empty?
+  system 'git config --local http.lowSpeedTime 5' if `git config --local http.lowSpeedTime`.empty?
+end
 
 # Rename the binary_sha256 variable to sha256 in the device.json file
 system(" sed -i 's/binary_sha256/sha256/g' #{File.join(CREW_CONFIG_PATH, 'device.json')}")
