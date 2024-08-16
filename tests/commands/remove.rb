91fe0e7fd1a6c845d61f178aa380e8cae7ed4b1b
Satadru Pramanik <satadru@gmail.com>
2024-08-16 11:05:45 -0400

    Fix one remove test.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- tests/commands/remove.rb
+++ tests/commands/remove.rb
@@ -11,19 +11,21 @@ $LOAD_PATH << File.join(CREW_LIB_PATH, 'lib')
 
 String.use_color = false
 
-device_json = PackageUtils.load_json
-essential_deps = device_json[:essential_deps]
-
 class RemoveCommandTest < Minitest::Test
   def test_remove_essential_package
+    device_json = PackageUtils.load_json
+    essential_deps = device_json[:essential_deps]
     puts "Essential depependency packages: #{essential_deps}."
     random_essential_package = essential_deps[rand(0...(essential_deps.length - 1))]
 
-    expected_output = "#{random_essential_package.capitalize} is considered an essential package needed for"
+    # expected_output = "(?:^|\W)#{random_essential_package.capitalize} is considered an essential package needed for\\n  Chromebrew to function and thus cannot be removed.\\n(?:$|\W)"
+    expected_output = %(  #{random_essential_package.capitalize} is considered an essential package needed for
+  Chromebrew to function and thus cannot be removed.
+)
     name = random_essential_package
     pkg = Package.load_package("#{name}.rb")
     puts "Testing removal of essential package #{name}, which SHOULD fail."
-    assert_output(/^#{Regexp.escape(expected_output.chomp)}!/, nil) do
+    assert_output(expected_output, nil) do
       Command.remove(pkg, true)
     end
   end
@@ -31,12 +33,12 @@ class RemoveCommandTest < Minitest::Test
   def test_remove_normal_package
     name = 'xxd_standalone'
     pkg = Package.load_package("#{name}.rb")
+    system "crew install #{name} &>/dev/null", out: File::NULL unless PackageUtils.installed?(name)
     expected_output = <<~EOT
       #{name} removed
     EOT
     assert_output(/^#{Regexp.escape(expected_output.chomp)}!/, nil) do
-      system "crew install #{name} &>/dev/null", out: File::NULL unless PackageUtils.installed?(name)
-      Command.remove(pkg, true)
+      ( system "crew install #{name} &>/dev/null", out: File::NULL unless PackageUtils.installed?(name) ) and Command.remove(pkg, true)
     end
   end
 end
