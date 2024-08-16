c888894392787ed3447997b37a31e65f75f40c73
Satadru Pramanik <satadru@gmail.com>
2024-08-16 13:56:12 -0400

    suggested changes
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/buildsystems/python.rb
+++ lib/buildsystems/python.rb
@@ -22,9 +22,11 @@ class Python < Package
     Dir.chdir(@python_build_relative_dir) do
       if File.file?('setup.py')
         puts "Python build options being used: #{PY3_SETUP_BUILD_OPTIONS} #{@python_build_options}".orange
+        puts "MAKEFLAGS=-j#{CREW_NPROC} python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS} #{@python_build_options}" if CREW_VERBOSE
         system "MAKEFLAGS=-j#{CREW_NPROC} python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS} #{@python_build_options}"
       else
         puts "Python build options being used: #{PY3_BUILD_OPTIONS}".orange
+        puts "MAKEFLAGS=-j#{CREW_NPROC} python3 -m build #{PY3_BUILD_OPTIONS}" if CREW_VERBOSE
         system "MAKEFLAGS=-j#{CREW_NPROC} python3 -m build #{PY3_BUILD_OPTIONS}"
       end
       @python_build_extras&.call
@@ -36,9 +38,11 @@ class Python < Package
       if File.file?('setup.py')
         @py_setup_install_options = @no_svem ? PY_SETUP_INSTALL_OPTIONS_NO_SVEM : PY_SETUP_INSTALL_OPTIONS
         puts "Python install options being used: #{@py_setup_install_options} #{@python_install_options}".orange
+        puts "MAKEFLAGS=-j#{CREW_NPROC} python3 setup.py install #{@py_setup_install_options} #{@python_install_options}" if CREW_VERBOSE
         system "MAKEFLAGS=-j#{CREW_NPROC} python3 setup.py install #{@py_setup_install_options} #{@python_install_options}"
       else
         puts "Python install options being used: #{PY3_INSTALLER_OPTIONS}".orange
+        puts "MAKEFLAGS=-j#{CREW_NPROC} python3 -m installer #{PY3_INSTALLER_OPTIONS}" if CREW_VERBOSE
         system "MAKEFLAGS=-j#{CREW_NPROC} python3 -m installer #{PY3_INSTALLER_OPTIONS}"
       end
       @python_install_extras&.call
