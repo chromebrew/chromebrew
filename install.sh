273a5c00468a19bc8dd6e7a036a6d28f5affede8
Satadru Pramanik <satadru@gmail.com>
2024-08-15 23:32:09 -0400

    Adjust git configs for network connectivity locally only.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- install.sh
+++ install.sh
@@ -359,8 +359,8 @@ else
   # Make the git default branch error messages go away.
   git config --global init.defaultBranch main
   # Help handle situations where GitHub is down.
-  git config --global http.lowSpeedLimit 1000
-  git config --global http.lowSpeedTime 5
+  git config http.lowSpeedLimit 1000
+  git config http.lowSpeedTime 5
 
   # Setup the folder with git information.
   git init --ref-format=reftable
