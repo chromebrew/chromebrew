d5520058155643bb83f531d9cce98bd244e8bc23
Satadru Pramanik <satadru@gmail.com>
2024-08-17 12:30:43 -0400

    Cleanup remove.rb tests.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- tests/commands/remove.rb
+++ tests/commands/remove.rb
@@ -12,35 +12,44 @@ $LOAD_PATH << File.join(CREW_LIB_PATH, 'lib')
 String.use_color = false
 
 class RemoveCommandTest < Minitest::Test
-  def test_remove_essential_package
-    device_json = PackageUtils.load_json
-    essential_deps = device_json[:essential_deps]
-    puts "\nEssential depependency packages: #{essential_deps}."
-    random_essential_package = essential_deps[rand(0...(essential_deps.length - 1))]
+  def setup
+    essential_deps = PackageUtils.load_json[:essential_deps]
+    @random_essential_package_name = essential_deps[rand(0...(essential_deps.length - 1))]
+    puts <<~ESSENTIAL_PACKAGE_REMOVAL_TEST_EOF
 
-    expected_output = %(  #{random_essential_package.capitalize} is considered an essential package needed for
+      Testing the removal of essential package #{@random_essential_package_name}, which was picked at random from one the essential packages: #{essential_deps.join(', ')}
+      This should fail.
+    ESSENTIAL_PACKAGE_REMOVAL_TEST_EOF
+    @random_essential_pkg = Package.load_package("#{@random_essential_package_name}.rb")
+
+    @normal_package_name = 'xxd_standalone'
+    puts <<~NORMAL_PACKAGE_REMOVAL_TEST_EOF
+
+      Testing the removal of normal package #{@normal_package_name}.
+      This should succeed.
+    NORMAL_PACKAGE_REMOVAL_TEST_EOF
+    @normal_pkg = Package.load_package("#{@normal_package_name}.rb")
+  end
+
+  def test_remove_essential_package
+    expected_output = %(  #{@random_essential_package_name.capitalize} is considered an essential package needed for
   Chromebrew to function and thus cannot be removed.
 )
-    name = random_essential_package
-    pkg = Package.load_package("#{name}.rb")
-    puts "Testing the removal of essential package #{name}, which was picked at random, which SHOULD fail."
     assert_output(expected_output, nil) do
-      Command.remove(pkg, true)
+      Command.remove(@random_essential_pkg, true)
     end
   end
 
   def test_remove_normal_package
-    name = 'xxd_standalone'
-    pkg = Package.load_package("#{name}.rb")
     expected_output = <<~EOT
-      #{name} removed
+      #{@normal_package_name} removed
     EOT
     assert_output(/^#{Regexp.escape(expected_output.chomp)}!/, nil) do
-      until PackageUtils.installed?(name)
-        system "crew install -d #{name} &>/dev/null", out: File::NULL
+      until PackageUtils.installed?(@normal_package_name)
+        system "crew install -d #{@normal_package_name} &>/dev/null", out: File::NULL
         sleep 2
       end
-      Command.remove(pkg, true)
+      Command.remove(@normal_pkg, true)
     end
   end
 end
