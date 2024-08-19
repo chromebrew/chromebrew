adc4422855fdfd832e7a6a5db1fa138106e58d81
Ed Reel <edreel@gmail.com>
2024-08-19 10:08:23 -0500

    Binutils 2.43-gcc14 => 2.43.1-gcc14 (#10322)

--- packages/binutils.rb
+++ packages/binutils.rb
@@ -5,18 +5,18 @@ require 'package'
 class Binutils < Package
   description 'The GNU Binutils are a collection of binary tools.'
   homepage 'https://www.gnu.org/software/binutils/'
-  version '2.43-gcc14'
+  version '2.43.1-gcc14'
   license 'GPL-3+'
   compatibility 'all'
   source_url "https://ftpmirror.gnu.org/binutils/binutils-#{version.split('-').first}.tar.bz2"
-  source_sha256 'fed3c3077f0df7a4a1aa47b080b8c53277593ccbb4e5e78b73ffb4e3f265e750'
+  source_sha256 'becaac5d295e037587b63a42fad57fe3d9d7b83f478eb24b67f9eec5d0f1872f'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2495535e6fb7b8d8ed7eadd4072192cd14526f0edb111b57968aa6fc43b93a00',
-     armv7l: '2495535e6fb7b8d8ed7eadd4072192cd14526f0edb111b57968aa6fc43b93a00',
-       i686: 'd31cfcbf745167c12d286284bc46d72c56a4e43c22fa0d27c39458ec0355c88e',
-     x86_64: '2c7087344bd4eee7df8402b6a69f4e2d043163298eb37ff68b1a74dc7324dbae'
+    aarch64: '2054d452f15cb8b018453e863f04421ee5cec46da7af695ab279f0060ebbc00d',
+     armv7l: '2054d452f15cb8b018453e863f04421ee5cec46da7af695ab279f0060ebbc00d',
+       i686: '62db3f76719701daf87f4e299bfc033b8adeb39dea00a991b59a25a7b48a52e0',
+     x86_64: 'd457df70b8784a7e8ffcb641f377731d87846b8d269c4d5c8c94a1b2bf2ef361'
   })
 
   depends_on 'elfutils' # R
