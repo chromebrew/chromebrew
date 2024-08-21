296c3798ec615bcec0faa27b319a23ed7d0d8f2a
Satadru Pramanik <satadru@gmail.com>
2024-08-21 16:54:13 -0400

    Update qbittorrent, modify qmake buildsystem to try to enable newer glibc compatibility.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/qbittorrent.rb
+++ packages/qbittorrent.rb
@@ -3,21 +3,22 @@ require 'buildsystems/cmake'
 class Qbittorrent < CMake
   description 'Open-source software alternative to µTorrent.'
   homepage 'https://www.qbittorrent.org/'
-  version '4.6.3'
+  version '4.6.6'
   license 'GPL-2'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url 'https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.6.3/qbittorrent-4.6.3.tar.xz'
+  source_url 'https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.6.6/qbittorrent-4.6.6.tar.xz'
   source_sha256 '5f9f3e0b89861e8bae1894656f8b8f76feddb3c92e228065c8173632af6e544e'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a3bb615c33f412adcf44b9284b5f9c28c45d2f05dab0d299220b28d4844d64ff',
-     armv7l: 'a3bb615c33f412adcf44b9284b5f9c28c45d2f05dab0d299220b28d4844d64ff',
-     x86_64: 'afbe852b182f0e0ca7abc58a16415da49f1ae294853436b2b5e14d50c3b8cc54'
+    aarch64: '4c30e5dab2abdba5be6e33bc1f42b9425438c9d68d02b95abba9ff65abc93da2',
+     armv7l: '4c30e5dab2abdba5be6e33bc1f42b9425438c9d68d02b95abba9ff65abc93da2',
+     x86_64: 'edc558f1067f4bfa2c4fb28fa495a1c7f3227144b3d989488f1b993a16512df8'
   })
 
   depends_on 'cmake' => :build
   depends_on 'gcc_lib' # R
+  depends_on 'glibc_lib' # R
   depends_on 'glibc' # R
   depends_on 'libtorrent' # R
   depends_on 'openssl' # R
