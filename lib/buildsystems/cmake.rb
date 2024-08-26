c63129586911d720e2a42de5a5b3786d4bd686ee
Satadru Pramanik <satadru@gmail.com>
2024-08-26 15:16:52 -0400

    Adjust cmake logic for cmake_build_relative_dir.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/buildsystems/cmake.rb
+++ lib/buildsystems/cmake.rb
@@ -12,20 +12,26 @@ class CMake < Package
 
   def self.build
     puts "Additional cmake_options being used: #{@pre_cmake_options.nil? ? '<no pre_cmake_options>' : @pre_cmake_options} #{@cmake_options.nil? ? '<no cmake_options>' : @cmake_options}".orange
-    @crew_cmake_options = @no_lto ? CREW_CMAKE_FNO_LTO_OPTIONS : CREW_CMAKE_OPTIONS
-    @mold_linker_prefix_cmd = CREW_LINKER == 'mold' ? 'mold -run' : ''
-    system "#{@pre_cmake_options} #{@mold_linker_prefix_cmd} cmake -C #{@cmake_build_relative_dir} -B builddir -G Ninja #{@crew_cmake_options} #{@cmake_options}"
-    system "#{CREW_NINJA} -C #{@cmake_build_relative_dir}/builddir"
+    Dir.chdir(@cmake_build_relative_dir) do
+      @crew_cmake_options = @no_lto ? CREW_CMAKE_FNO_LTO_OPTIONS : CREW_CMAKE_OPTIONS
+      @mold_linker_prefix_cmd = CREW_LINKER == 'mold' ? 'mold -run' : ''
+      system "#{@pre_cmake_options} #{@mold_linker_prefix_cmd} cmake -B builddir -G Ninja #{@crew_cmake_options} #{@cmake_options}"
+      system "#{CREW_NINJA} -C builddir"
+    end
     @cmake_build_extras&.call
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C #{@cmake_build_relative_dir}/builddir install"
+    Dir.chdir(@cmake_build_relative_dir) do
+      system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C #{@cmake_build_relative_dir}/builddir install"
+    end
     @cmake_install_extras&.call
   end
 
   def self.check
     puts "Testing with #{CREW_NINJA} test.".orange if @run_tests
-    system "#{CREW_NINJA} -C #{@cmake_build_relative_dir}/builddir test" if @run_tests
+    Dir.chdir(@cmake_build_relative_dir) do
+      system "#{CREW_NINJA} -C builddir test" if @run_tests
+    end
   end
 end
