7fa8b3941098d9a6f48b217ea8bc124b9eab1907
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-08-29 19:13:17 +1000

    docker-systemctl-replacement 9cbe1a0 -> 1.5.8066, hyphen 73dd29 -> 2.8.8-73dd296, Inxi 811a19 -> 3.3.35-1, libXdmcp 4a71fdf -> 1.1.5, py3_snowballstemmer 2.1.0-py3.12 -> 2.2.0-py3.12, libstemmer 78c149 -> 2.2.0 (#10365)
    * docker-systemctl-replacement 9cbe1a0 -> 1.5.8066
    
    * hyphen 73dd29 -> 2.8.8-73dd296
    
    * Inxi 811a19 -> 3.3.35-1
    
    * libXdmcp 4a71fdf -> 1.1.5
    
    * py3_snowballstemmer 2.1.0-py3.12 -> 2.2.0-py3.12
    
    * libstemmer 78c149 -> 2.2.0

--- packages/py3_snowballstemmer.rb
+++ packages/py3_snowballstemmer.rb
@@ -3,10 +3,11 @@ require 'buildsystems/pip'
 class Py3_snowballstemmer < Pip
   description 'Snowball stemming library collection for Python'
   homepage 'https://snowballstem.org'
-  version '2.1.0-py3.12'
+  version '2.2.0-py3.12'
   license 'BSD'
   compatibility 'all'
   source_url 'SKIP'
 
   depends_on 'py3_pystemmer'
+  depends_on 'python3'
 end
