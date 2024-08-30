7f293ba60a262c047e519bdb8c2020ca34adc79a
Zopolis4 <creatorsmithmdt@gmail.com>
2024-08-30 12:53:38 +1000

    polkit 124-eafbf7d -> 125

--- packages/polkit.rb
+++ packages/polkit.rb
@@ -3,17 +3,17 @@ require 'buildsystems/meson'
 class Polkit < Meson
   description 'Application development toolkit for controlling system-wide privileges'
   homepage 'https://github.com/polkit-org/polkit'
-  version '124-eafbf7d'
+  version '125'
   license 'LGPL-2'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://github.com/polkit-org/polkit.git'
-  git_hashtag 'eafbf7ded1b1b0424fb7da16c32629e43c71af27'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '619ae1856a98ffaf5aa0fa4e81b21f5b73b2e0db3f25d30d8f26c1b535b50980',
-     armv7l: '619ae1856a98ffaf5aa0fa4e81b21f5b73b2e0db3f25d30d8f26c1b535b50980',
-     x86_64: 'bdc6e6282b546beb5188347e7b5a0956367fbd85a5859bdfbed5cfbd2cc8c6e3'
+    aarch64: '049b8152177810491fd8a7af7ba60707c25806cc74a7ffcb7e46f6351fb8a024',
+     armv7l: '049b8152177810491fd8a7af7ba60707c25806cc74a7ffcb7e46f6351fb8a024',
+     x86_64: 'd718513f21699196b8524a8f04a089ab7eb887fc273a83cf490636b54aca98c8'
   })
 
   depends_on 'duktape'
@@ -23,12 +23,6 @@ class Polkit < Meson
   depends_on 'gobject_introspection' => :build
   depends_on 'gtk_doc' => :build
   depends_on 'linux_pam' # R
-
-  def self.patch
-    # meson: do not depend on systemd
-    downloader 'https://patch-diff.githubusercontent.com/raw/polkit-org/polkit/pull/471.patch', '50641d00cb837f1a97623dd0ad3d49d810ef977fcef6ef10cd88b61b64650ede'
-    system 'git apply 471.patch'
-  end
 
   meson_options "-Dsession_tracking=elogind -Dsystemdsystemunitdir=#{CREW_PREFIX}/etc/elogind"
   run_tests
