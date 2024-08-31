19fe0df682f14767ec9ba18a65dc468e1db9f105
Satadru Pramanik <satadru@gmail.com>
2024-08-31 17:57:45 -0400

    Add binaries
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/perl_xml_xpath.rb
+++ packages/perl_xml_xpath.rb
@@ -13,5 +13,12 @@ class Perl_xml_xpath < PERL
   source_sha256 '7bc75be36b239e5b2e700a9570d2b53b43093d467f2abe6a743f9ff9093790cd'
   binary_compression 'tar.zst'
 
+  binary_sha256({
+    aarch64: '54e2d7d5233406f25f120f17a4787390bc146a980a87bfab768160418cf41189',
+     armv7l: '54e2d7d5233406f25f120f17a4787390bc146a980a87bfab768160418cf41189',
+       i686: '49e0e8f88a201231a0fa1899fcc8a2ea6e7fda10c9d763d65f6699e7f1ffddc5',
+     x86_64: '8c5a8886d499f67b1df3ff62d6cc50605d4afe87122194e962ceeb4196e8b266'
+  })
+
   depends_on 'perl_xml_parser'
 end
