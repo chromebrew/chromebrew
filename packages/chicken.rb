8e2ce8d0c5478b00878ab8421e9d79993267615b
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-09-01 10:28:12 +1000

    aspell 0.60.8 -> 0.60.8.1, axel 2.17.6 -> 2.17.14, chafa cf15d59 -> 1.14.2, chicken 5.1.0 -> 5.4.0, colord 1.4.5 -> 1.4.7 (#10368)
    * aspell 0.60.8 -> 0.60.8.1
    
    * axel 2.17.6 -> 2.17.14
    
    * chafa cf15d59 -> 1.14.2
    
    * chicken 5.1.0 -> 5.4.0
    
    * colord 1.4.5 -> 1.4.7

--- packages/chicken.rb
+++ packages/chicken.rb
@@ -1,29 +1,29 @@
 require 'package'
 
 class Chicken < Package
-  description 'CHICKEN is practical and portable scheme system.'
+  description 'CHICKEN is a practical and portable scheme system.'
   homepage 'https://code.call-cc.org/'
-  version '5.1.0'
+  version '5.4.0'
   license 'BSD'
   compatibility 'all'
-  source_url 'https://code.call-cc.org/releases/5.1.0/chicken-5.1.0.tar.gz'
-  source_sha256 '5c1101a8d8faabfd500ad69101e0c7c8bd826c68970f89c270640470e7b84b4b'
-  binary_compression 'tar.xz'
+  source_url "https://code.call-cc.org/releases/#{version}/chicken-#{version}.tar.gz"
+  source_sha256 '3c5d4aa61c1167bf6d9bf9eaf891da7630ba9f5f3c15bf09515a7039bfcdec5f'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '508dcf75c52f3d316a9479e593654a6db5fd8cfa54e874e673750f446a0ed8c4',
-     armv7l: '508dcf75c52f3d316a9479e593654a6db5fd8cfa54e874e673750f446a0ed8c4',
-       i686: '608ec4490b87f1b125d0577a2fc91bbc8a2d4031a4340307f7a8194ff73b2aa0',
-     x86_64: '43218a8fdc1f7118dd93b3955da341c76b1689d1c29615eccb378beb450ae176'
+    aarch64: 'f9821dfba5cd2e2be8653792fe29a3524e2851a99320702b304fe946963012fe',
+     armv7l: 'f9821dfba5cd2e2be8653792fe29a3524e2851a99320702b304fe946963012fe',
+       i686: 'f5b00b3ac5b6e06fbc17ba765cc1ad44de159178117ecf361fa1741413acc5a8',
+     x86_64: '82e81bf7b319bd2b879407968928fa3adac20d0ee8ea74e3dd62058aafdc8b99'
   })
 
-  depends_on 'filecmd'
+  depends_on 'filecmd' => :build
 
   def self.build
-    system 'make', '-j1', 'PLATFORM=linux' # parallel builds don't work
+    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}"
   end
 
   def self.install
-    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install', 'PLATFORM=linux'
+    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
--- packages/colord.rb
+++ packages/colord.rb
@@ -3,35 +3,31 @@ require 'buildsystems/meson'
 class Colord < Meson
   description 'colord is a system service that makes it easy to manage, install and generate color profiles to accurately color manage input and output devices.'
   homepage 'https://www.freedesktop.org/software/colord/'
-  version '1.4.5'
+  version '1.4.7'
   license 'GPL-2'
   compatibility 'x86_64 aarch64 armv7l'
-  source_url "https://www.freedesktop.org/software/colord/releases/colord-#{version}.tar.xz"
-  source_sha256 'b774ea443d239f4a2ee1853bd678426e669ddeda413dcb71cea1638c4d6c5e17'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/hughsie/colord.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '52e9192284622a986090b7c2b88fd888e0a338e212681f21f3df62dd69670554',
-     armv7l: '52e9192284622a986090b7c2b88fd888e0a338e212681f21f3df62dd69670554',
-     x86_64: '35bb9659086e6c86785609196d8aff86b008584ef139bb22644f71a5909d4ba8'
+    aarch64: '1997467d2cd378d26eb12f5846badd952fe66362f5a56523cf6b2c731c8585e5',
+     armv7l: '1997467d2cd378d26eb12f5846badd952fe66362f5a56523cf6b2c731c8585e5',
+     x86_64: '45ee103244fd062d2f578a6f0b7167c9ea33b5b3762430b7685eb2c51d888f16'
   })
 
   depends_on 'bash_completion'
   depends_on 'dbus'
+  depends_on 'eudev' # R
   depends_on 'glib'
   depends_on 'gobject_introspection'
-  depends_on 'gtk_doc'
   depends_on 'gusb'
   depends_on 'lcms'
   depends_on 'libgudev'
   depends_on 'polkit'
-  depends_on 'vala'
+  depends_on 'vala' => :build
 
-  no_lto
-
-  def self.patch
-    system "sed -i 's,-fstack-protector-strong,-fno-stack-protector,' meson.build"
-  end
-
-  meson_options "-Dsystemd=false -Ddaemon_user=#{USER}"
+  # Building manpages fails due to the supposed lack of docbook-xsl-ns.
+  meson_options "-Dsystemd=false -Ddocs=false -Dman=false -Ddaemon_user=#{USER}"
+  run_tests
 end
