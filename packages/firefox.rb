1c3d55c218912d233a1ce8bd67f78d97758f03e3
Ed Reel <edreel@gmail.com>
2024-09-02 14:09:38 -0500

    Firefox 129.0.2 => 130.0 (#10399)

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,12 +3,12 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '129.0.2'
+  version '130.0'
   license 'MPL-2.0, GPL-2 and LGPL-2.1'
   compatibility 'x86_64'
   min_glibc '2.35'
   source_url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.bz2"
-  source_sha256 'abc39c9deb686084933371bbe0546001f7bfab46c9d7a0cf4b1a4a025886cd5e'
+  source_sha256 'd5246cea96094146cb4403df7797df3000e94d9c9b7e6101700283a347d03ef9'
 
   depends_on 'at_spi2_core'
   depends_on 'cairo'
