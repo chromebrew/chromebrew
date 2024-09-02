a976a71b8fe0844607cc93e1c559a0fc6fcf8bc8
Zopolis4 <creatorsmithmdt@gmail.com>
2024-09-02 21:13:00 +1000

    apr 1.7.4-1 -> 1.7.5

--- packages/apr.rb
+++ packages/apr.rb
@@ -3,19 +3,21 @@ require 'buildsystems/autotools'
 class Apr < Autotools
   description 'The mission of the Apache Portable Runtime (APR) project is to create and maintain software libraries that provide a predictable and consistent interface to underlying platform-specific implementations.  APR is the base portability library.'
   homepage 'https://apr.apache.org/'
-  version '1.7.4-1'
+  version '1.7.5'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://dlcdn.apache.org/apr/apr-1.7.4.tar.bz2'
-  source_sha256 'fc648de983f3a2a6c9e78dea1f180639bd2fad6c06d556d4367a701fe5c35577'
+  source_url "https://dlcdn.apache.org/apr/apr-#{version}.tar.bz2"
+  source_sha256 'cd0f5d52b9ab1704c72160c5ee3ed5d3d4ca2df4a7f8ab564e3cb352b67232f2'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '465f8cada526545e28103e254f21d2ad628833b769abe5862e6830c72145efd6',
-     armv7l: '465f8cada526545e28103e254f21d2ad628833b769abe5862e6830c72145efd6',
-       i686: 'ee88e192864d2aafb8e508b5a8ddda2bd660cc3657b77860339fae080ab81c57',
-     x86_64: '535c8ec82c9ac8b3f51e23fcb053a51d69a0c8ed22d7daa06a7e1b92b6f9016b'
+    aarch64: 'f87428e564b81cb430f250a91f10e8e161d53cfa9919802eecd4c871ab8be54a',
+     armv7l: 'f87428e564b81cb430f250a91f10e8e161d53cfa9919802eecd4c871ab8be54a',
+       i686: '0b61996688a957a0f4a9101fc218665bdff14429a921254a65d03e6d4a345a1b',
+     x86_64: 'c5f65c79ac2dc3942a024a42caafca2a8777f02bcafe23ad751de564de14c8d0'
   })
+
+  depends_on 'util_linux' # R
 
   # https://bz.apache.org/bugzilla/show_bug.cgi?id=63439
   configure_options '--with-devrandom --without-sendfile --enable-layout=GNU'
