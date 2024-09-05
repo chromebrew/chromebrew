4e28216482beceb2efa5b8ccdd9b89a14b5234f6
Zopolis4 <creatorsmithmdt@gmail.com>
2024-09-05 11:00:12 +1000

    weechat 2.1 -> 4.4.1

--- packages/weechat.rb
+++ packages/weechat.rb
@@ -1,39 +1,37 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Weechat < Package
+class Weechat < CMake
   description 'WeeChat is a fast, light and extensible chat client.'
   homepage 'https://weechat.org/'
-  version '2.1'
+  version '4.4.1'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://weechat.org/files/src/weechat-2.1.tar.xz'
-  source_sha256 'cda0f81ff8243fd25cca76c8e490e2fdb5b8fbb2554d7aa87cb3b0beda16ed38'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/weechat/weechat.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9b00d99af938abc66093f1b64287d71502a3d4967530d4d5f7e55756ba6fc00e',
-     armv7l: '9b00d99af938abc66093f1b64287d71502a3d4967530d4d5f7e55756ba6fc00e',
-       i686: '31ef7d36a76b8b41bcd2f1a352adcd20456deccc4ed2e452e882223c32b67ba3',
-     x86_64: '4ab72526832466b8d2166ab093951fe87cf2e4faba1e29361c95c6bc966210bd'
+    aarch64: '3755d6351a04f3afd99377e117608113c952a353be98aa9b1ac1c2cbfbe8a565',
+     armv7l: '3755d6351a04f3afd99377e117608113c952a353be98aa9b1ac1c2cbfbe8a565',
+       i686: '02a64442c7bf8c421c7f947e76020a35fdea9bb04ae3efbc41dae1ea500c04fb',
+     x86_64: 'f9018d6a3bd471185516d7a7d2fc70ab37f442458eedd27441d4f6a80d3923fd'
   })
 
+  depends_on 'aspell'
+  depends_on 'bdwgc' # R
+  depends_on 'cjson'
+  depends_on 'curl'
+  depends_on 'gnutls'
+  depends_on 'guile'
   depends_on 'libgcrypt'
-  depends_on 'aspell' => :build
-  depends_on 'curl' => :build
-  depends_on 'lua' => :build
-  depends_on 'python2'
-  depends_on 'tcl' => :build
+  depends_on 'lua'
+  depends_on 'ncurses'
+  depends_on 'perl'
+  depends_on 'python3'
+  depends_on 'ruby'
+  depends_on 'tcl'
+  depends_on 'zlib'
+  depends_on 'zstd'
 
-  def self.build
-    system 'cmake',
-           "-DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX}",
-           '-DCMAKE_BUILD_TYPE=Release',
-           '-DENABLE_MAN=ON',
-           "-DPYTHON_EXECUTABLE=#{CREW_PREFIX}/bin/python2"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "LIBDIR=#{CREW_LIB_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  cmake_options '-DENABLE_PHP=OFF'
 end
