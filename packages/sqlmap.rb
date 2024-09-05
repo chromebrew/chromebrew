1ee091587d67b0e07bd92cbe27659f5eb964854b
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2024-09-05 17:22:14 +1000

    Reduce more of the python2 dependency (#10422)
    * Remove q package as it is no longer suitable for packaging (see Homebrew/homebrew-core#90174)
    
    * smem 1.4 -> 1.5
    
    * sqlmap 1.4.2 -> 1.8.8
    
    * weechat 2.1 -> 4.4.1

--- packages/sqlmap.rb
+++ packages/sqlmap.rb
@@ -1,41 +1,12 @@
-require 'package'
+require 'buildsystems/pip'
 
-class Sqlmap < Package
+class Sqlmap < Pip
   description 'sqlmap is an open source penetration testing tool that automates the process of detecting and exploiting SQL injection flaws and taking over of database servers.'
   homepage 'https://sqlmap.org/'
-  version '1.4.2'
+  version '1.8.8'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/sqlmapproject/sqlmap/archive/1.4.2.tar.gz'
-  source_sha256 '77faf85164eb17dce769ec830cbd146768644315bc1024613ad13155e09c2d11'
-  binary_compression 'tar.xz'
+  source_url 'SKIP'
 
-  binary_sha256({
-    aarch64: '02a1b56739af28c5c2dbd920f69c630e7e1ffafc043387b06534c9586886f5f7',
-     armv7l: '02a1b56739af28c5c2dbd920f69c630e7e1ffafc043387b06534c9586886f5f7',
-       i686: '0fbff386670b5ef09f12d3b3e3478d203f4f1140d529b039be36d7123399497e',
-     x86_64: '886d073baae73b24d03dfdc8c7d09dff3de47eda9229794caed5a1915b25c588'
-  })
-
-  depends_on 'python27' unless File.exist? "#{CREW_PREFIX}/bin/python"
-
-  def self.build
-    system "echo '#!/bin/bash' > sqlmap"
-    system "echo 'cd #{CREW_PREFIX}/share/sqlmap' >> sqlmap"
-    system "echo 'python sqlmap.py -c sqlmap.conf \"\$@\"' >> sqlmap"
-  end
-
-  def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/sqlmap"
-    system "cp -r . #{CREW_DEST_PREFIX}/share/sqlmap"
-    system "install -Dm755 sqlmap #{CREW_DEST_PREFIX}/bin/sqlmap"
-  end
-
-  def self.postinstall
-    puts
-    puts "To get extended help, run 'sqlmap -hh | most'".lightblue
-    puts
-    puts "To configure, edit #{CREW_PREFIX}/share/sqlmap/sqlmap.conf".lightblue
-    puts
-  end
+  depends_on 'python3', '>= 3.12.0'
 end
