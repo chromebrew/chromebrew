42a10ddb6eceae0afd7f270bead4e2635a3006c4
Satadru Pramanik <satadru@gmail.com>
2024-09-08 19:22:24 -0400

    Move pytimeparse to pip.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pytimeparse.rb
+++ packages/py3_pytimeparse.rb
@@ -1,13 +1,12 @@
-require 'buildsystems/python'
+require 'buildsystems/pip'
 
-class Py3_pytimeparse < Python
+class Py3_pytimeparse < Pip
   description 'Pytimeparse is a small Python module to parse various kinds of time expressions. '
   homepage 'https://github.com/wroberts/pytimeparse/'
   version '1.1.8-py3.12'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/wroberts/pytimeparse.git'
-  version '1.1.8-py3.12'
+  source_url 'SKIP'
   binary_compression 'tar.zst'
 
   depends_on 'python3' => :build
