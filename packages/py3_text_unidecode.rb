f24866a4b6c14859c4b414a1663f7a2dbf450ca9
Satadru Pramanik <satadru@gmail.com>
2024-09-08 19:39:02 -0400

    move py3_text_unidecode to pip
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_text_unidecode.rb
+++ packages/py3_text_unidecode.rb
@@ -1,13 +1,12 @@
-require 'buildsystems/python'
+require 'buildsystems/pip'
 
-class Py3_text_unidecode < Python
+class Py3_text_unidecode < Pip
   description 'Text-unicode is the most basic Text::Unidecode port.'
   homepage 'https://github.com/kmike/text-unidecode/'
   version '1.3-py3.12'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/kmike/text-unidecode.git'
-  version '1.3-py3.12'
+  source_url 'SKIP'
   binary_compression 'tar.zst'
 
   depends_on 'python3' => :build
