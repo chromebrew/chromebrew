0fe79f05abc1c9dc16ee629b73939ee5d0972371
Satadru Pramanik <satadru@gmail.com>
2024-09-08 19:12:55 -0400

    move py3_wheel to pip
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_wheel.rb
+++ packages/py3_wheel.rb
@@ -1,13 +1,12 @@
-require 'buildsystems/python'
+require 'buildsystems/pip'
 
-class Py3_wheel < Python
+class Py3_wheel < Pip
   description 'Wheel is the binary package format for python.'
   homepage 'https://wheel.readthedocs.io/'
   version '0.44.0-py3.12'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/pypa/wheel.git'
-  version '0.44.0-py3.12'
+  source_url 'SKIP'
   binary_compression 'tar.zst'
 
   depends_on 'python3'
