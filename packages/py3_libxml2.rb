ac8dacee9befd1ef9dab8152abd3a4086a990585
Satadru Pramanik <satadru@gmail.com>
2024-09-09 11:07:13 -0400

    Add py3_patchelf and py3_libxml2 binaries
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_libxml2.rb
+++ packages/py3_libxml2.rb
@@ -11,6 +11,13 @@ class Py3_libxml2 < Package
   git_hashtag Libxml2.git_hashtag
   binary_compression 'tar.zst'
 
+  binary_sha256({
+    aarch64: '3e1e15cefeae56e1900f403e451df0aecf830ff5b5a87ab14bd7728567aae7da',
+     armv7l: '3e1e15cefeae56e1900f403e451df0aecf830ff5b5a87ab14bd7728567aae7da',
+       i686: '2c00954c38ec288fe6fb7d892f85f21af2d418584cd4b37bd1f2a7283f30d7d5',
+     x86_64: '3f19e088f7e3bc2bb4c354ec4d6c1896aa55d728b18508d5d0de0517c66acbca'
+  })
+
   depends_on 'glibc' # R
   depends_on 'libxml2' # R
   depends_on 'py3_setuptools' => :build
--- packages/py3_patchelf.rb
+++ packages/py3_patchelf.rb
@@ -9,6 +9,13 @@ class Py3_patchelf < Pip
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
+  binary_sha256({
+    aarch64: '886bf82495e4abc3b83c72a12e6ea1b313f26c2c66ffce7c0dd5a963e52b1869',
+     armv7l: '886bf82495e4abc3b83c72a12e6ea1b313f26c2c66ffce7c0dd5a963e52b1869',
+       i686: 'c4b9b418670f8d84f3ab7cd8c46b8393caf80494e1a8989cdf9b709f36e0a8d0',
+     x86_64: '6f6a40b47f0298026b7bac3344bc3a70889d5488944c9299467b5a3c2f6ada4c'
+  })
+
   depends_on 'python3'
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
