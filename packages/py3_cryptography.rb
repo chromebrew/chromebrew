5390d833f016282ee9acf5905dac2d1b5ad5856b
Satadru Pramanik <satadru@gmail.com>
2024-09-08 22:26:39 -0400

    Update python packages...
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_cryptography.rb
+++ packages/py3_cryptography.rb
@@ -9,6 +9,13 @@ class Py3_cryptography < Pip
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
+  binary_sha256({
+    aarch64: 'b1f0e4321cf8cb114a591ab14612d21f38ed3a3884e9af8d7e110378ae5dd1f0',
+     armv7l: 'b1f0e4321cf8cb114a591ab14612d21f38ed3a3884e9af8d7e110378ae5dd1f0',
+       i686: '4caa722a093ff2a40bd6b20f8d917c26464df6ec5562930548df044535066a35',
+     x86_64: '322f4fc9b4fb79ccf177e614500e51a3dbb32f4b0061ffa314ccec2ca53c5033'
+  })
+
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'openssl' # R
@@ -17,5 +24,5 @@ class Py3_cryptography < Pip
   depends_on 'py3_typing_extensions'
   depends_on 'rust' => :build
 
-  no_compile_needed
+  no_source_build
 end
--- packages/py3_pyicu.rb
+++ packages/py3_pyicu.rb
@@ -9,9 +9,18 @@ class Py3_pyicu < Pip
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
+  binary_sha256({
+    aarch64: 'f5604d4b56e4360e55ede348accc9c7f9c22e2ced2570cef9c3be1dfad59390f',
+     armv7l: 'f5604d4b56e4360e55ede348accc9c7f9c22e2ced2570cef9c3be1dfad59390f',
+       i686: '7efd3f6fb928f43567b34d888f733fe2ca1c80f6e9609a3b8bcd55379c6f9961',
+     x86_64: '33f98e0f8ba4a385afe434a168042c0c2528ea43534345613873c1cb7c546730'
+  })
+
   depends_on 'python3' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'glibc_lib' # R
   depends_on 'icu4c' # R
+
+  no_source_build
 end
