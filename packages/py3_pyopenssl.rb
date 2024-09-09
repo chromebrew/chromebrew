649d15ae33dd933dabef276d698d8e084e6791fd
Satadru Pramanik <satadru@gmail.com>
2024-09-09 18:28:32 -0400

    Add binaries for py3_pyopenssl.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pyopenssl.rb
+++ packages/py3_pyopenssl.rb
@@ -7,6 +7,14 @@ class Py3_pyopenssl < Pip
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: 'a38123ee16686fcb9029c3d3fce06fe8fb85973424addd0d559fe9d2975dee51',
+     armv7l: 'a38123ee16686fcb9029c3d3fce06fe8fb85973424addd0d559fe9d2975dee51',
+       i686: '4e32faaade1d77148402696e2dc7faded8b8c28044f36a1425d4e109ac9afc07',
+     x86_64: '0b9bfa34bf7a9e6d70287b4822055b9043520e2c655f331351555a93b3ff7348'
+  })
 
   depends_on 'openssl'
   depends_on 'py3_cryptography'
