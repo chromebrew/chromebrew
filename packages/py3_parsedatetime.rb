3320bb068b8f1d1f7b29a448ab8e2cfb65990f4d
Satadru Pramanik <satadru@gmail.com>
2024-09-09 10:15:08 -0400

    Fix missing version in py3_parsedatetime.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_parsedatetime.rb
+++ packages/py3_parsedatetime.rb
@@ -3,6 +3,7 @@ require 'buildsystems/pip'
 class Py3_parsedatetime < Pip
   description 'Parse human-readable date/time strings'
   homepage 'https://github.com/bear/parsedatetime/'
+  version '2.6-py3.12'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
