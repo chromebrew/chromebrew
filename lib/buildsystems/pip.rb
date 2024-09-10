92aec3a4537d5448e59d5a2f019e4cd297382c85
Satadru Pramanik <satadru@gmail.com>
2024-09-09 21:11:37 -0400

    cleanup buildsystems/pip
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/buildsystems/pip.rb
+++ lib/buildsystems/pip.rb
@@ -2,7 +2,6 @@ Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 require 'json'
 require 'package'
-require 'package_utils'
 require_relative '../const'
 
 class Pip < Package
