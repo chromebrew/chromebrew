98bf9831508695e4357044e48b223cc2e17ee02b
Ed Reel <edreel@gmail.com>
2024-09-13 13:39:14 -0500

    Platformsh 5.0.18 => 5.0.20 (#10473)

--- packages/platformsh.rb
+++ packages/platformsh.rb
@@ -3,11 +3,11 @@ require 'package'
 class Platformsh < Package
   description 'The unified tool for managing your Platform.sh services from the command line.'
   homepage 'https://docs.platform.sh/overview/cli.html'
-  version '5.0.18'
+  version '5.0.20'
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
   source_url "https://github.com/platformsh/cli/releases/download/#{version}/platform_#{version}_linux_amd64.tar.gz"
-  source_sha256 'cdae7b0f7856c83402534947ca7018033ea25a492aaa3aaf6f3c17e1495c4bba'
+  source_sha256 'a0f732d72bc03136a5afe141ca72b472ae4381cd6c7bf4dfd45662c02a9ac08b'
 
   depends_on 'php83' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
