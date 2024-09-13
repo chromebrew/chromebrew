4e9ad6bde49ae77311dd5b18927fc55a9ae27f9c
Satadru Pramanik <satadru@gmail.com>
2024-09-13 16:30:06 -0400

    Add more binaries
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_zipp.rb
+++ packages/py3_zipp.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_zipp < Pip
   description 'Zipp is a backport of pathlib-compatible object wrapper for zip files.'
   homepage 'https://github.com/jaraco/zipp/'
-  version '3.20.1-py3.12'
+  version '3.20.2-py3.12'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a2ec9442fafc3c4341e719b9215e1df245dbbcd0a757c77da652f65e030df0fd',
-     armv7l: 'a2ec9442fafc3c4341e719b9215e1df245dbbcd0a757c77da652f65e030df0fd',
-       i686: '8211758d6b9bb5b7f2a247ed43bb6a82f6bd23e0e47ebaeb667369080fdb0be6',
-     x86_64: '8d30fdb6bef920ecc450457e73abb1f2355c7dae38c0afa1fff4d8640bd7cff9'
+    aarch64: 'df69e9c1a655f8e24bca7357ee2e17d51e0c9fb2fdd285f786f2e65a033281bc',
+     armv7l: 'df69e9c1a655f8e24bca7357ee2e17d51e0c9fb2fdd285f786f2e65a033281bc',
+       i686: '587bc59750542770282d2b873e3bedbf8b59700e410486dd304bdb3d35b0fac0',
+     x86_64: 'ce4202d8306b56bb29f762b5a7dc427db11407617386915e25026f620a873c2c'
   })
 
   depends_on 'python3' => :build
