4e97b3060c5cd5a70602edac30f39b82bfe6dd0a
Satadru Pramanik <satadru@gmail.com>
2024-09-15 21:46:43 -0400

    Add fastfetch, yyjson, and py3 updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_idna.rb
+++ packages/py3_idna.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_idna < Pip
   description 'IDNA provides internationalized domain names for Python.'
   homepage 'https://github.com/kjd/idna/'
-  version '3.9-py3.12'
+  version '3.10-py3.12'
   license 'BSD-3'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b8664822f6157e92b8fed191e85e33216a60c32db4900be779f4311f7240a8be',
-     armv7l: 'b8664822f6157e92b8fed191e85e33216a60c32db4900be779f4311f7240a8be',
-       i686: 'c9bd9415d84fe223489796692d36f67e2bff4cff35e28099d998dd882ab8f9a9',
-     x86_64: 'a2e85e5209722ed0ecc4e3f03f7dff905a26e87f7189e35a55242896f32e9172'
+    aarch64: '933812e4fc1780ee39c2d24b7758320a27e3b8f0f1d35b6f73d3cf675f6ee8cc',
+     armv7l: '933812e4fc1780ee39c2d24b7758320a27e3b8f0f1d35b6f73d3cf675f6ee8cc',
+       i686: '7684da659cb9e024c554ca8747e536c31908f520b1470ebe68023e65b5ee4c88',
+     x86_64: '2f92d90c413ca1a58524d1795ff5bfdaf9a43f690a961e388e6dccd31e1eaeb2'
   })
 
   depends_on 'python3' => :build
---
+++ packages/yyjson.rb
@@ -0,0 +1,24 @@
+# Adapted from Arch Linux yyjson PKGBUILD at:
+# https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yyjson
+
+require 'buildsystems/cmake'
+
+class Yyjson < CMake
+  description 'A high performance JSON library written in ANSI C'
+  homepage 'https://ibireme.github.io/yyjson'
+  version '0.10.0'
+  license 'MIT'
+  compatibility 'all'
+  source_url 'https://github.com/ibireme/yyjson.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '5c5b6b4c4e70177af3d30de8e6bde00d9695ca7f02eb06e42ca5ff07ef14f678',
+     armv7l: '5c5b6b4c4e70177af3d30de8e6bde00d9695ca7f02eb06e42ca5ff07ef14f678',
+       i686: '907d2cdf87a79073011b4649b3df7f18a312f304ae5997590d8bfe9a8fefcb29',
+     x86_64: '27554b9952ece260c7b760c260e2d76a5cf7a63a0811d173fcc908dd2d0b8d36'
+  })
+
+  cmake_options '-DBUILD_SHARED_LIBS=ON'
+end
