26dc2cfbd30a0e6696b0a584811b58c236a8640e
Ed Reel <edreel@gmail.com>
2024-09-18 11:45:39 -0500

    Qbittorrent 4.6.6 => 4.6.7 (#10489)

--- packages/qbittorrent.rb
+++ packages/qbittorrent.rb
@@ -3,18 +3,18 @@ require 'buildsystems/cmake'
 class Qbittorrent < CMake
   description 'Open-source software alternative to µTorrent.'
   homepage 'https://www.qbittorrent.org/'
-  version '4.6.6'
+  version '4.6.7'
   license 'GPL-2'
   compatibility 'x86_64 aarch64 armv7l'
   min_glibc '2.37'
-  source_url 'https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.6.6/qbittorrent-4.6.6.tar.xz'
-  source_sha256 '5f9f3e0b89861e8bae1894656f8b8f76feddb3c92e228065c8173632af6e544e'
+  source_url "https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-#{version}/qbittorrent-#{version}.tar.xz"
+  source_sha256 'bcf2dcf52a0186c3f5da2f1413453888654dc1aee5e4ad80e5a026764bc640c4'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4c30e5dab2abdba5be6e33bc1f42b9425438c9d68d02b95abba9ff65abc93da2',
-     armv7l: '4c30e5dab2abdba5be6e33bc1f42b9425438c9d68d02b95abba9ff65abc93da2',
-     x86_64: 'edc558f1067f4bfa2c4fb28fa495a1c7f3227144b3d989488f1b993a16512df8'
+    aarch64: 'ab18023baf9d990fb90acabc39884c9b0f38ef008e9ecef20cd9d90dea9619b6',
+     armv7l: 'ab18023baf9d990fb90acabc39884c9b0f38ef008e9ecef20cd9d90dea9619b6',
+     x86_64: 'dbaca4cd1e372ac6707b8d9547bf571d401067c1c151ca55f8cb9bb61f0824f0'
   })
 
   depends_on 'cmake' => :build
