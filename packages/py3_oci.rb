54412f69b37756679c9cc0dd4dd59aa852a87770
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-09-25 11:37:50 -0400

    Mold => 2.34, and py3/ruby pkg updates. (#10522)
    * Mold => 2.34, and py3/ruby pkg updates.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * rebuild py3_oci for arm
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_oci.rb
+++ packages/py3_oci.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_oci < Pip
   description 'Oracle Cloud Infrastructure Python SDK'
   homepage 'https://oracle-cloud-infrastructure-python-sdk.readthedocs.io/'
-  version '2.134.0-py3.12'
+  version '2.135.0-py3.12'
   license 'UPL-1.0 or Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '721a5605893b5acf7ffe78759b6df446d4bbb0f425460f8b582c8a6a16c1eaed',
-     armv7l: '721a5605893b5acf7ffe78759b6df446d4bbb0f425460f8b582c8a6a16c1eaed',
-       i686: '2ccecf9406a78e0f75d0fa3b71aa932b1937ef7fb4a83341402312792f8052cc',
-     x86_64: '66fbd922295d49198b7ac80a0340b81b31b2ec96580e145c7975b14113f501e2'
+    aarch64: '6f2f47a1c7f719921c6b0eea243deefa451b6744019ecdf40c46b21e28c82072',
+     armv7l: '6f2f47a1c7f719921c6b0eea243deefa451b6744019ecdf40c46b21e28c82072',
+       i686: '3a50ea19822b6b96dfbdd60e67cb4183b00810b1bf1bfbe2f479432cfb403d8a',
+     x86_64: 'dcbb849bdc7814214534baaa41b309e3861d3e76614b45266e2ca5c437de5ac4'
   })
 
   depends_on 'py3_python_dateutil'
