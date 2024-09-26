28c9a809d6400efeb8995bdfe76a2bedff8c7187
Satadru Pramanik <satadru@gmail.com>
2024-09-26 12:58:15 -0400

    Move activesupport gem behind requre_gem function.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/downloader.rb
+++ lib/downloader.rb
@@ -1,10 +1,30 @@
-require 'active_support/core_ext/object/blank'
 require 'digest/sha2'
 require 'io/console'
 require 'uri'
 require_relative 'const'
 require_relative 'color'
 require_relative 'progress_bar'
+
+def require_gem(gem_name_and_require = nil, require_override = nil)
+  # Allow only loading gems when needed.
+  return if gem_name_and_require.nil?
+
+  gem_name = gem_name_and_require.split('/')[0]
+  begin
+    gem gem_name
+  rescue LoadError
+    puts " -> install #{gem_name} gem".orange
+    Gem.install(gem_name)
+    gem gem_name
+  end
+  requires = if require_override.nil?
+               gem_name_and_require.split('/')[1].nil? ? gem_name_and_require.split('/')[0] : gem_name_and_require
+             else
+               require_override
+             end
+  require requires
+end
+require_gem('activesupport', 'active_support/core_ext/object/blank')
 
 begin
   require 'securerandom'
