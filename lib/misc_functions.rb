5bd017e6ea11f0bad99e7be71cfc29a4e8b6e3d6
Satadru Pramanik <satadru@gmail.com>
2024-09-28 01:40:25 -0400

    Work around matrix removal breaking crew.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/misc_functions.rb
+++ lib/misc_functions.rb
@@ -1,6 +1,25 @@
 # lib/misc_functions.rb
 # Generic implementations of various functions/algorithms that are not crew-specific.
-require 'matrix'
+def require_gem(gem_name_and_require = nil, require_override = nil)
+  # Allow only loading gems when needed.
+  return if gem_name_and_require.nil?
+
+  gem_name = gem_name_and_require.split('/')[0]
+  begin
+    gem gem_name
+  rescue LoadError
+    puts " -> install #{gem_name} gem".orange
+    Gem.install(gem_name)
+    gem gem_name
+  end
+  requires = if require_override.nil?
+               gem_name_and_require.split('/')[1].nil? ? gem_name_and_require.split('/')[0] : gem_name_and_require
+             else
+               require_override
+             end
+  require requires
+end
+require_gem('matrix')
 
 class MiscFunctions
   def self.human_size(bytes)
