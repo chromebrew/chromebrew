0663519c55be86b1c836109cc20e69c13457483a
Satadru Pramanik <satadru@gmail.com>
2024-10-01 15:12:22 -0400

    Python => 3.12.7 + pip updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/awscli.rb
+++ packages/awscli.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Awscli < Pip
   description 'Universal Command Line Interface for Amazon Web Services'
   homepage 'https://github.com/aws/aws-cli'
-  version "1.34.29-#{CREW_PY_VER}"
+  version "1.34.30-#{CREW_PY_VER}"
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4e09a646ab51f48df4499d9a6bfcdd6a5f989240cee552597b86aede3840bddf',
-     armv7l: '4e09a646ab51f48df4499d9a6bfcdd6a5f989240cee552597b86aede3840bddf',
-       i686: 'eb5b9d40a91e002f1f819a79c4d4cdcc0b16dc062fa875c9f0074463ee39b48d',
-     x86_64: '67d0b489172a31ea500ab22ce44e5a463be2230f734331f14a42971dad6daf17'
+    aarch64: 'eb1da1b73b6feab36ff107783b2d83e984dd8b9040366e31dd49589b349684bf',
+     armv7l: 'eb1da1b73b6feab36ff107783b2d83e984dd8b9040366e31dd49589b349684bf',
+       i686: '77a62a26306f0a747f2bf1f1bf9f1cb4b4826fd654cdbf7aacf3846350790af6',
+     x86_64: 'a91a8705a835c18c6c88c1a05b60d67d901ec0813d71206f88c371236dbe78b5'
   })
 
   depends_on 'groff'
--- packages/py3_oci_cli.rb
+++ packages/py3_oci_cli.rb
@@ -3,16 +3,16 @@ require 'buildsystems/pip'
 class Py3_oci_cli < Pip
   description 'Command Line Interface for Oracle Cloud Infrastructure'
   homepage 'https://github.com/oracle/oci-cli/'
-  version "3.48.1-#{CREW_PY_VER}"
+  version "3.48.2-#{CREW_PY_VER}"
   license 'UPL-1.0'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '17963af39be8298aeb55dcc4fe56e83ec206414d027da3c8c29966c421499036',
-     armv7l: '17963af39be8298aeb55dcc4fe56e83ec206414d027da3c8c29966c421499036',
-     x86_64: '522e23619bcb05dd1eb16a7e8347e06f034856edf117ef010e4946919b588e78'
+    aarch64: '506fd51c673fd069bc6b513bfc342a60afd5aa3fd0b761cb385213aea06fb26f',
+     armv7l: '506fd51c673fd069bc6b513bfc342a60afd5aa3fd0b761cb385213aea06fb26f',
+     x86_64: '5a6784fb9c0ae77502a7000687cf1be1598bf12b1c5aca5c51766c3e0065187f'
   })
 
   depends_on 'py3_arrow'
