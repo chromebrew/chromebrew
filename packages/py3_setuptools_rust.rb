9f17207591b922099c1b18bbc36507c9e5c59527
Satadru Pramanik <satadru@gmail.com>
2024-10-02 10:27:15 -0400

    fixup
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_setuptools_rust.rb
+++ packages/py3_setuptools_rust.rb
@@ -12,7 +12,7 @@ class Py3_setuptools_rust < Pip
   binary_sha256({
     aarch64: '5f40289c4dea96b00d8a5d98e5bf76e0aeb6583bc77a20e1ee8c1e072a73b7cb',
      armv7l: '5f40289c4dea96b00d8a5d98e5bf76e0aeb6583bc77a20e1ee8c1e072a73b7cb',
-       i686: '5fc04e2e18f47c0e838ec3f7364855116d2de15482f0ac1c410eeede08d7bd9c',
+       i686: 'f82f0da32b291af8fab801a1135e3865b3edccb997770fd4086838889bb7e657',
      x86_64: '952b75c25e84015b64fb0c68bc3d0af5a863b61e9693e7f7a11908bc6cfeeb37'
   })
 
