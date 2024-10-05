29ba7cd356eb1402d15205c276c74b320aa2e02f
Jason Pratt <Jason.Pratt@asu.edu>
2024-10-04 20:25:33 -0700

    Terraform 1.9.6 => 1.9.7

--- packages/terraform.rb
+++ packages/terraform.rb
@@ -3,7 +3,7 @@ require 'package'
 class Terraform < Package
   description 'Terraform is a tool for building, changing, and combining infrastructure safely and efficiently.'
   homepage 'https://www.terraform.io/'
-  version '1.9.6'
+  version '1.9.7'
   license 'Apache-2.0, BSD-2, BSD-4, ECL-2.0, imagemagick, ISC, JSON, MIT, MIT-with-advertising, MPL-2.0 and unicode'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Terraform < Package
      x86_64: "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
   })
   source_sha256({
-    aarch64: 'bd71d88f37e5a61bb5070d452da5c1c6fad702256b972e499dc8bae8c705131f',
-     armv7l: 'bd71d88f37e5a61bb5070d452da5c1c6fad702256b972e499dc8bae8c705131f',
-       i686: '8f7f621ee87df157b50514d4183054ea3696cbaea5a7e2d8e21669112d9674ed',
-     x86_64: 'f2c90fb1efb2ad411519d1d3ccbaee7489a60e3147f2206fdb824fb35fac9c1c'
+    aarch64: '14f0a10f9fed64a22581c32b7e0ad675065f8470f32cef6d5be29c29928dcd3b',
+     armv7l: '14f0a10f9fed64a22581c32b7e0ad675065f8470f32cef6d5be29c29928dcd3b',
+       i686: 'ef13210f44ef4497cfae3d2226162ebd1078c383a8d0a22126c6862a4cf54b2f',
+     x86_64: '76a77ed7e785ef354b7f565abaa990e3e7b5232d3ec4afb87e0f5a413fcbdad1'
   })
 
   def self.install
