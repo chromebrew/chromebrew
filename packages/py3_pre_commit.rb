539e4e837a4067a706ae58454d872b1c6d745d23
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-10-06 23:03:06 -0400

    pip updates (#10577)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pre_commit.rb
+++ packages/py3_pre_commit.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_pre_commit < Pip
   description 'A framework for managing and maintaining multi-language pre-commit hooks.'
   homepage 'https://pre-commit.com/'
-  version "3.8.0-#{CREW_PY_VER}"
+  version "4.0.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd3b919ae26f05e1219e6ce4a8d4c1c07ffcf4ed26b367c7dddff993782f9dcd1',
-     armv7l: 'd3b919ae26f05e1219e6ce4a8d4c1c07ffcf4ed26b367c7dddff993782f9dcd1',
-       i686: '95b5aed355837f26c46bb9770780f7a34c693ff6ac593da89c0f4bc4e0466df4',
-     x86_64: 'de2247dec9286a780c8b87bbca1e12a3cf4950d5ff9cb1177df16b3acc0fcc17'
+    aarch64: '7e4a70c33daa7a861598d6a9d84202acb3f300cb2f153b6535177616aa249dc2',
+     armv7l: '7e4a70c33daa7a861598d6a9d84202acb3f300cb2f153b6535177616aa249dc2',
+       i686: '7bf6837f592c739457b6937124a573bda700a8a669f3459997914ef0e7fb54a8',
+     x86_64: '7c2f7e68657b97a094a7c33b49f2aeab5fb9b334c08d926fa29fbd595f03f019'
   })
 
   depends_on 'py3_cfgv'
