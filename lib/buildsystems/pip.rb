5764598ac9a8727c39eff896dc0d489e5daa506b
Satadru Pramanik <satadru@gmail.com>
2024-10-09 12:28:21 -0400

    Add PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1 to pip.rb to fix python 3.13 builds.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/buildsystems/pip.rb
+++ lib/buildsystems/pip.rb
@@ -16,7 +16,7 @@ class Pip < Package
     pip_cache_dir = `pip cache dir`.chomp
 
     puts 'Checking for pip updates'.orange if CREW_VERBOSE
-    system "python3 -s -m pip install -U pip | grep -v 'Requirement already satisfied'", exception: false
+    system "PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1 python3 -s -m pip install -U pip | grep -v 'Requirement already satisfied'", exception: false
     @py_pkg = name.gsub('py3_', '')
     @py_pkg_chromebrew_version = version.gsub(/-py3\.\d{2}/, '').gsub(/-icu\d{2}\.\d/, '')
     puts "Checking for #{@py_pkg} python dependencies...".orange if CREW_VERBOSE
@@ -34,18 +34,18 @@ class Pip < Package
       @py_pkg_deps.each do |pip_dep|
         @cleaned_py_dep = pip_dep[/[^;]+/]
         puts "——Installing: #{@cleaned_py_dep}".gray
-        system "python3 -s -m pip install --ignore-installed -U \"#{@cleaned_py_dep}\" | grep -v 'Requirement already satisfied'", exception: false
+        system "PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1 python3 -s -m pip install --ignore-installed -U \"#{@cleaned_py_dep}\" | grep -v 'Requirement already satisfied'", exception: false
       end
     end
     puts "Installing #{@py_pkg} python module. This may take a while...".lightblue
     puts "Additional pre_configure_options being used: #{@pre_configure_options.nil? ? '<no pre_configure_options>' : @pre_configure_options}".orange
     puts "#{@py_pkg.capitalize} is configured to install a pre-release version." if prerelease?
-    system "MAKEFLAGS=-j#{CREW_NPROC} #{@pre_configure_options} python -s -m pip install --ignore-installed -U \"#{@py_pkg}==#{@py_pkg_chromebrew_version}\" | grep -v 'Requirement already satisfied'", exception: false
+    system "PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1 MAKEFLAGS=-j#{CREW_NPROC} #{@pre_configure_options} python -s -m pip install --ignore-installed -U \"#{@py_pkg}==#{@py_pkg_chromebrew_version}\" | grep -v 'Requirement already satisfied'", exception: false
 
     if @source_url == 'SKIP'
-      system "pip wheel -w #{pip_cache_dir} #{@py_pkg}"
+      system "PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1 pip wheel -w #{pip_cache_dir} #{@py_pkg}"
     else
-      system "pip wheel -w #{pip_cache_dir} git+#{source_url}"
+      system "PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1 pip wheel -w #{pip_cache_dir} git+#{source_url}"
     end
 
     @pip_files = `python3 -s -m pip show -f #{@py_pkg}`.chomp
