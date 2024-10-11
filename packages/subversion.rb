982eae8ead3a48089781f12a5725428cfb6a8a10
Ed Reel <edreel@gmail.com>
2024-10-11 01:42:02 +0000

    Subversion 1.14.3 => 1.14.4

--- packages/subversion.rb
+++ packages/subversion.rb
@@ -3,22 +3,27 @@ require 'buildsystems/autotools'
 class Subversion < Autotools
   description 'Subversion is an open source version control system.'
   homepage 'https://subversion.apache.org/'
-  version '1.14.3'
+  version '1.14.4'
   license 'Apache-2.0, BSD, MIT, BSD-2, FSFAP and unicode'
   compatibility 'x86_64 aarch64 armv7l'
   source_url "https://dlcdn.apache.org/subversion/subversion-#{version}.tar.bz2"
-  source_sha256 '949efd451a09435f7e8573574c71c7b71b194d844890fa49cd61d2262ea1a440'
+  source_sha256 '44ead116e72e480f10f123c914bb6f9f8c041711c041ed7abff1b8634a199e3c'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '52d5520e27a6dae64e207ebcabf99bba1f6fe559e8b1c34473b4f9c4c302b32a',
-     armv7l: '52d5520e27a6dae64e207ebcabf99bba1f6fe559e8b1c34473b4f9c4c302b32a',
-     x86_64: '5569d6fadab22e7032d518f703c42b4fb574bcdd1f33ab1379c37aa374dc69d8'
+    aarch64: '62704e12605742de34089c350055f2859e6dd23731921c1db9eb7b60381316d3',
+     armv7l: '62704e12605742de34089c350055f2859e6dd23731921c1db9eb7b60381316d3',
+     x86_64: 'c05a9400f3fdecc25f2c1eba9a5502926eb234a5aa6302e5e2392be27fd97d6d'
   })
 
   depends_on 'apr_util'
+  depends_on 'expat'
   depends_on 'serf'
   depends_on 'sqlite'
 
   configure_options '--disable-static --with-utf8proc=internal'
+
+  def self.prebuild
+    ConvenienceFunctions.libtoolize('expat')
+  end
 end
