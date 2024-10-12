62053f66edbae175675a8340cd7c9e2c4e795ccd
Satadru Pramanik <satadru@gmail.com>
2024-10-12 13:31:15 -0400

    update trove classifiers.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_trove_classifiers.rb
+++ packages/py3_trove_classifiers.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_trove_classifiers < Pip
   description 'Canonical source for classifiers on PyPI.'
   homepage 'https://github.com/pypa/trove-classifiers'
-  version "2024.10.11-#{CREW_PY_VER}"
+  version "2024.10.12-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8948fc6aacd0dcc5ad4f4f94d5e31ed2ad38331d7aa6ca29f120bdce92138429',
-     armv7l: '8948fc6aacd0dcc5ad4f4f94d5e31ed2ad38331d7aa6ca29f120bdce92138429',
-       i686: 'f2d5d92bfe11b0a9470024fb4e0e403c891a1f5aac5a90248c77c7bea3322f8f',
-     x86_64: 'f292185f36e7502c24f14d803408932a4066ca76e3461276661db5ae0b03e98d'
+    aarch64: '72f55bf7d257224d29f30ebedf6803736de21d0025a366264831e573bd51b03f',
+     armv7l: '72f55bf7d257224d29f30ebedf6803736de21d0025a366264831e573bd51b03f',
+       i686: '4f2c173e829513672fbf85389d2c499bd602c5f949153610434d7ca1dcb0b2f7',
+     x86_64: '745b76997903e68f6aa03ad87ab8fd1bc52fa5f366db04a8c90278aa7f9939c1'
   })
 
   depends_on 'python3' => :build
