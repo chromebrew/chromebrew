4e306efc2d63556fea07229fdcb86cc2fa8e08d6
Satadru Pramanik <satadru@gmail.com>
2024-10-12 22:28:58 -0400

    lint
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_ptools.rb
+++ packages/ruby_ptools.rb
@@ -1,7 +1,7 @@
 require 'buildsystems/ruby'
 
 class Ruby_ptools < RUBY
-  description "The ptools (power tools) library is an additional set of commands for the File class based on Unix command line tools."
+  description 'The ptools (power tools) library is an additional set of commands for the File class based on Unix command line tools.'
   homepage 'https://github.com/djberg96/ptools'
   version "1.5.0-#{CREW_RUBY_VER}"
   license 'Apache-2.0'
