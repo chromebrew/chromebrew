dfd2bf3444f102c04a4f4c7baa7cf5796c4a7495
Satadru Pramanik <satadru@gmail.com>
2024-10-14 11:52:36 -0400

    Update packages, update unit tests to use M129 container, refresh tools/core_packages.txt
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pyparsing.rb
+++ packages/py3_pyparsing.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_pyparsing < Pip
   description 'The pyparsing module is an alternative approach to creating and executing simple grammars, vs. the traditional lex/yacc approach, or the use of regular expressions.'
   homepage 'https://github.com/pyparsing/pyparsing/'
-  version "3.1.4-#{CREW_PY_VER}"
+  version "3.2.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a16351552ea09fc6659dc4af89f68ac55082aa9d2ba8bce966775e60a3f9711b',
-     armv7l: 'a16351552ea09fc6659dc4af89f68ac55082aa9d2ba8bce966775e60a3f9711b',
-       i686: 'f24620e845f22533eed7f1843385aa3d1136ed9af0c6a37bc376a7830f417bd3',
-     x86_64: '54e51484646ce0ef81c7ba33f5f491f972d7705163f691b4e62eeab03b46abb4'
+    aarch64: '3990fb5fe628edd46fd128c1fe5ac0735247b2b0690af32bab401d8668b1a52f',
+     armv7l: '3990fb5fe628edd46fd128c1fe5ac0735247b2b0690af32bab401d8668b1a52f',
+       i686: '4830dd9c90f5e59f23cf2f306e678a7f3bb1e363c730e0a884418cd0ef24f1d0',
+     x86_64: '62f56649660855f8f2b8d4a400a099d570036bbc3395aad0d49ee8f166db3a0b'
   })
 
   depends_on 'python3'
--- packages/py3_trove_classifiers.rb
+++ packages/py3_trove_classifiers.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_trove_classifiers < Pip
   description 'Canonical source for classifiers on PyPI.'
   homepage 'https://github.com/pypa/trove-classifiers'
-  version "2024.10.12-#{CREW_PY_VER}"
+  version "2024.10.13-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '72f55bf7d257224d29f30ebedf6803736de21d0025a366264831e573bd51b03f',
-     armv7l: '72f55bf7d257224d29f30ebedf6803736de21d0025a366264831e573bd51b03f',
-       i686: '4f2c173e829513672fbf85389d2c499bd602c5f949153610434d7ca1dcb0b2f7',
-     x86_64: '745b76997903e68f6aa03ad87ab8fd1bc52fa5f366db04a8c90278aa7f9939c1'
+    aarch64: '9684d45e3a65953970d1e9158c38cbe4a2c90121ab833f9f02ed8cb39e6b3c70',
+     armv7l: '9684d45e3a65953970d1e9158c38cbe4a2c90121ab833f9f02ed8cb39e6b3c70',
+       i686: 'c3bdfaf0c12270481909dba07a19cbebba56476146492b9430876d0b3f2482ff',
+     x86_64: '061bf133ba6aa28e25ec19c04bfbd0844c13c0c2db3b5f6517d5dd3c66bced65'
   })
 
   depends_on 'python3' => :build
