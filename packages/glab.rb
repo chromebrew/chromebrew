11b5a3108f9cabc8be5312632274d91c9680b9d9
Ed Reel <edreel@gmail.com>
2024-10-15 13:17:58 -0500

    Glab 1.46.1 => 1.47.0 (#10613)

--- packages/glab.rb
+++ packages/glab.rb
@@ -3,20 +3,20 @@ require 'package'
 class Glab < Package
   description 'A GitLab CLI tool bringing GitLab to your command line'
   homepage 'https://gitlab.com/gitlab-org/cli'
-  version '1.46.1'
+  version '1.47.0'
   license 'MIT'
   compatibility 'all'
   source_url({
-    aarch64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_armv6.tar.gz",
-     armv7l: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_armv6.tar.gz",
-       i686: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_i386.tar.gz",
-     x86_64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_Linux_x86_64.tar.gz"
+    aarch64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_linux_armv6.tar.gz",
+     armv7l: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_linux_armv6.tar.gz",
+       i686: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_linux_386.tar.gz",
+     x86_64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '2cfa1714e8f9294769f432efcda682717587a2de0dbfcd0c735d77e80daa3c8b',
-     armv7l: '2cfa1714e8f9294769f432efcda682717587a2de0dbfcd0c735d77e80daa3c8b',
-       i686: '48f319febd9020ba681b17ba262f749b50b7f6d4af0ef736bfbea220c81e0d64',
-     x86_64: 'f7e83a1ed07ab01acfca094b64a4160c074662aea83890481002b840c9f79524'
+    aarch64: '0a85f0bcc972c8705a1e7439b0535f76921f45b1c692365e81d8ca67b5ab4003',
+     armv7l: '0a85f0bcc972c8705a1e7439b0535f76921f45b1c692365e81d8ca67b5ab4003',
+       i686: 'a9e42a824c0779e56545bdfd164d9833b1ea257f1e84ab22420aa945f4120105',
+     x86_64: '407079d03b3e3d2953f7c08c7b555f00e59916f532336a43619afef9d11ed0d8'
   })
 
   no_compile_needed
