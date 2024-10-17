6e900a48a4bd8f7b566b5a59b84988741d6ba5f7
Satadru Pramanik <satadru@gmail.com>
2024-10-17 11:14:07 -0400

    Lint now that we know the checks fail appropriately when they are tripped.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/buildsystems/ruby.rb
+++ lib/buildsystems/ruby.rb
@@ -65,7 +65,7 @@ def set_vars(passed_name = nil, passed_version = nil)
   # Any version with a letter is considered a prerelease as per
   # https://github.com/rubygems/rubygems/blob/b5798efd348935634d4e0e2b846d4f455582db48/lib/rubygems/version.rb#L305
   gem_test_versions.delete_if { |i| i.match?(/[a-zA-Z]/) }
-  gem_test_version = gem_test_versions.map {|v| Gem::Version.new(v)}.max.to_s
+  gem_test_version = gem_test_versions.map { |v| Gem::Version.new(v) }.max.to_s
   @gem_name = gem_test_name.blank? ? Gem::SpecFetcher.fetcher.suggest_gems_from_name(passed_name.gsub(/^ruby_/, '')).first : gem_test_name
   @remote_gem_ver = gem_test_name.blank? ? Gem.latest_version_for(@gem_name).to_s : gem_test_version
   @gem_ver = passed_version.split('-').first.to_s
