88de687c260b170a381879a42333f60edaf5b78d
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2024-10-23 18:18:04 -0400

    pip updates (#10657)
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pyright.rb
+++ packages/py3_pyright.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_pyright < Pip
   description 'Static Type Checker for Python'
   homepage 'https://github.com/microsoft/pyright'
-  version "1.1.385-#{CREW_PY_VER}"
+  version "1.1.386-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e86f736612714dc5d2a8ba028cd44763af9f62ee54e565a98d60f858b32918f9',
-     armv7l: 'e86f736612714dc5d2a8ba028cd44763af9f62ee54e565a98d60f858b32918f9',
-       i686: '0534e4599eea7b684eb730e91807e1ca1454f7f9b364f9c4e27b08a65ac693f7',
-     x86_64: '482d464ebd79cc8d5afa55284eadb2a412b9adef3572c9476ad7977dc1f2aab7'
+    aarch64: '294468ec8188d3ec9923a55b95b0333e137ca6073b27b3d43d82b238a658927c',
+     armv7l: '294468ec8188d3ec9923a55b95b0333e137ca6073b27b3d43d82b238a658927c',
+       i686: '188d6b4e071e819b11d6374e80603c5a3b5cb4528f32d526fa9262253b956fe0',
+     x86_64: '8ce6a5d580af253abf7e485267275568024e5fdbc7767f9e24e60ef04f1fabad'
   })
 
   depends_on 'python3'
