cef442013b91073c5285fa6abf159c7de0941206
Satadru Pramanik <satadru@gmail.com>
2024-10-24 07:18:15 -0400

    qemu => 9.1.1 + pip updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_nuitka.rb
+++ packages/py3_nuitka.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_nuitka < Pip
   description 'Nuitka is a Python to binary compiler written in Python. You feed it your Python app, it does a lot of clever things, and spits out an executable or extension module.'
   homepage 'https://nuitka.net/'
-  version "2.4.10-#{CREW_PY_VER}"
+  version "2.4.11-#{CREW_PY_VER}"
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '19e00f92db747bdb521733c2e231ec22f90b5e679e1758eaa76a735ccd2b8585',
-     armv7l: '19e00f92db747bdb521733c2e231ec22f90b5e679e1758eaa76a735ccd2b8585',
-       i686: '7808a0586cb5a9eee9cfbf1a44111ca4052017f4b7403ad53b7af8dd4d5c998a',
-     x86_64: '2f5ea981175a4678ea8132582090923005d17db0da4834f65dc4c982d1f9938c'
+    aarch64: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+     armv7l: 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+       i686: '8a4d03d506d31951c78e2d22d088883544fe346f8113b43e23a45cb1f68e77e9',
+     x86_64: 'ba825500a278ce77094b867d25ce75109dbbdd4dbb457736eaa56e2b6dd6e44d'
   })
 
   depends_on 'python3' => :build
