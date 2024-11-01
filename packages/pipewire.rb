7913a1ff1faa28d93cfa858514d7dc2b0e2e4bd4
Satadru Pramanik <satadru@gmail.com>
2024-11-01 13:29:40 -0400

    Fix pipewire
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/pipewire.rb
+++ packages/pipewire.rb
@@ -21,7 +21,7 @@ class Pipewire < Meson
     binary_sha256({
          i686: '0dbeda58c4e1db7a180ebfb2b7bc3057cc6966927f4d5ee543953b734dfc4510'
     })
-  elsif Gem::Version.new(CREW_KERNEL_VERSION.to_s) <= Gem::Version.new('5.4')
+  elsif Gem::Version.new(CREW_KERNEL_VERSION.to_s) <= Gem::Version.new('5.4') || Gem::Version.new(LIBC_VERSION) < Gem::Version.new('2.32')
     binary_sha256({
       aarch64: '237ad8299b16e6d294a6561a4959efb47fc72ee66d06f51a3863f55dbdedcf78',
        armv7l: '237ad8299b16e6d294a6561a4959efb47fc72ee66d06f51a3863f55dbdedcf78',
