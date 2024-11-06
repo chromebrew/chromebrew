518ef12d0d9a4ddbbe6f3690a71537a0173333b2
Satadru Pramanik <satadru@gmail.com>
2024-11-06 13:00:26 -0500

    pip and gem updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/sqlmap.rb
+++ packages/sqlmap.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Sqlmap < Pip
   description 'sqlmap is an open source penetration testing tool that automates the process of detecting and exploiting SQL injection flaws and taking over of database servers.'
   homepage 'https://sqlmap.org/'
-  version "1.8.10-#{CREW_PY_VER}"
+  version "1.8.11-#{CREW_PY_VER}"
   license 'GPL-2'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '414f21d5f2e26d5887ff35bd347cd1f1c4e31152b06fa94297fd45e8a9184f81',
-     armv7l: '414f21d5f2e26d5887ff35bd347cd1f1c4e31152b06fa94297fd45e8a9184f81',
-       i686: 'c04e336885d07ba5aa980219c6257fbe0e53afccd6750ec2f296df05ed780cff',
-     x86_64: '6b4d945b556086c4e02851aad2b931a7430eda1f534fdbbd70d9d27cfa52dd9e'
+    aarch64: 'f6e4151cacaa39eda43e36f2cfbb1b43bb911c73ff70262a01b11c5c4092677c',
+     armv7l: 'f6e4151cacaa39eda43e36f2cfbb1b43bb911c73ff70262a01b11c5c4092677c',
+       i686: '9bbb6e9fe05a73c61b8c2e36b4a692648ee5a501638e14251b971debb1724cf7',
+     x86_64: '7c300a321af68eee9eca8b1710fe22ba000cda130bae9e0cc37df3b524e56e3c'
   })
 
   depends_on 'python3', '>= 3.12.0'
