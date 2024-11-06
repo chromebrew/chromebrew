ca694514606ea8c73257472d4a61d9707028fd9c
Satadru Pramanik <satadru@gmail.com>
2024-11-06 13:00:26 -0500

    pip and gem updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pyright.rb
+++ packages/py3_pyright.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_pyright < Pip
   description 'Static Type Checker for Python'
   homepage 'https://github.com/microsoft/pyright'
-  version "1.1.387-#{CREW_PY_VER}"
+  version "1.1.388-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '7cbd073105c63c70e98190acb563427a596c8a4597e03eb61b8f31cc8bffd4ec',
-     armv7l: '7cbd073105c63c70e98190acb563427a596c8a4597e03eb61b8f31cc8bffd4ec',
-       i686: '0409c0727fd8c209900a386e3716d18f1de698b0de993876b064ac43431f3c5a',
-     x86_64: '6b4cc484ad22ef6d3e36da18ae13e9e32053c4e1c3e2fc2201560398d6718644'
+    aarch64: 'f2a8288f97b5e48e2186ac9518fefda6dd42f37c53ad2d960054a1c3a3434e2f',
+     armv7l: 'f2a8288f97b5e48e2186ac9518fefda6dd42f37c53ad2d960054a1c3a3434e2f',
+       i686: '1e7555c57c6cb790e72cf3d4f24cf4f1896e9519d3d72ca05944801f9be753eb',
+     x86_64: 'fd09a0a55fe60b18fb28621b151535138813516525dd6d908d4bbfa0a30f0791'
   })
 
   depends_on 'python3'
--- packages/py3_werkzeug.rb
+++ packages/py3_werkzeug.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_werkzeug < Pip
   description 'Werkzeug is a comprehensive WSGI web application library.'
   homepage 'https://palletsprojects.com/p/werkzeug/'
-  version "3.1.1-#{CREW_PY_VER}"
+  version "3.1.2-#{CREW_PY_VER}"
   license 'BSD-3'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5b8207fe0b77605340d694807d46135215b9216abccc16a7b8965396a5fc753b',
-     armv7l: '5b8207fe0b77605340d694807d46135215b9216abccc16a7b8965396a5fc753b',
-       i686: '27fa3f62438286349fe9bbfc55bf14941608065039eb6b2cbbb9b6a0708cbf01',
-     x86_64: '9de873ba4163fbfd0fd4d8836955c0de40b2ffdf1ec871028f0c35618df8f58a'
+    aarch64: 'd14d8a90118a712dd20281d32e361247a590e4c7eb22f7eab5335d81d34c3e1b',
+     armv7l: 'd14d8a90118a712dd20281d32e361247a590e4c7eb22f7eab5335d81d34c3e1b',
+       i686: '01ead243345a42b1fa65cfc19d83fbef3851cf7e25aed44bde6761d6f137517c',
+     x86_64: '74be366bb065034915b40432ca444b600a09e983876e5fa209513b1da8eba614'
   })
 
   depends_on 'python3' => :build
