bf94ed015d192ab252d37f9fc18517d7cabf9a92
Ed Reel <edreel@gmail.com>
2024-11-14 18:33:03 +0000

    Add tauri_cli package

---
+++ packages/tauri_cli.rb
@@ -0,0 +1,22 @@
+require 'package'
+
+class Tauri_cli < Package
+  description 'Build smaller, faster, and more secure desktop and mobile applications with a web frontend.'
+  homepage 'https://tauri.app/'
+  version '2.1.0'
+  license 'Apache-2.0, MIT'
+  compatibility 'x86_64'
+  min_glibc '2.29'
+  source_url "https://github.com/tauri-apps/tauri/releases/download/tauri-cli-v#{version}/cargo-tauri-x86_64-unknown-linux-gnu.tgz"
+  source_sha256 'c8cd0f2610253007f1370e11a5ba38107e91c6d6a2cc4c238b31e0a3a40195de'
+
+  no_compile_needed
+
+  def self.install
+    FileUtils.install 'cargo-tauri', "#{CREW_DEST_PREFIX}/bin/tauri", mode: 0o755
+  end
+
+  def self.postinstall
+    ExitMessage.add "\nType 'tauri' to get started.\n"
+  end
+end
