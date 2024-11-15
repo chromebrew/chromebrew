d505e24b2e6bf0d83c864750f2b0d46ee0315e0d
Satadru Pramanik <satadru@gmail.com>
2024-11-15 08:39:40 -0500

    Update ruby (built against kernel 3.10 headers on armv7l) and libffi.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libffi.rb
+++ packages/libffi.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libffi < Package
+class Libffi < Autotools
   description 'The libffi library provides a portable, high level programming interface to various calling conventions.'
   homepage 'https://sourceware.org/libffi/'
-  version '3.4.4'
+  version '3.4.6'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/libffi/libffi.git'
@@ -11,25 +11,14 @@ class Libffi < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '14026a74658574e2f7dfe5516f16bff43ed8d0c4ed3c65764216c72602cb6b54',
-     armv7l: '14026a74658574e2f7dfe5516f16bff43ed8d0c4ed3c65764216c72602cb6b54',
-       i686: '6fa26f0ad0e3d78315dbb50e7fc1f059711d75beae1d10d4a23c425cbb5da653',
-     x86_64: '4a2afb9850e7ab6b723a9df9279282479ce60a1db4fdb336b60e2244d327902a'
+    aarch64: '3019c6b5747d07c5db6a45e697df097264076d0f7f7562c031cbe6f78b6f9c0f',
+     armv7l: '3019c6b5747d07c5db6a45e697df097264076d0f7f7562c031cbe6f78b6f9c0f',
+       i686: '42d4375e7977b761d65df125e4cb22c910e6bbe9f15f1770126e92414d56f46f',
+     x86_64: '248bdaa9c63f2a2e6bde72294a7170329de410e57066f2b47c8a3c44b36c62cf'
   })
 
+  depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
 
-  def self.build
-    system 'autoreconf -fiv'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.check
-    # system "make check"         # DejaGNU required
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  # run_tests         # DejaGNU required
 end
--- packages/ruby.rb
+++ packages/ruby.rb
@@ -3,7 +3,7 @@ require 'package'
 class Ruby < Package
   description 'Ruby is a dynamic, open source programming language with a focus on simplicity and productivity.'
   homepage 'https://www.ruby-lang.org/en/'
-  version '3.3.5' # Do not use @_ver here, it will break the installer.
+  version '3.3.6' # Do not use @_ver here, it will break the installer.
   license 'Ruby-BSD and BSD-2'
   compatibility 'all'
   source_url 'https://github.com/ruby/ruby.git'
@@ -11,10 +11,10 @@ class Ruby < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9ad018a5a3f263bdeb9e548c42613a7e894aba6b8d9941e2c6e9c9f54fb84ee0',
-     armv7l: '9ad018a5a3f263bdeb9e548c42613a7e894aba6b8d9941e2c6e9c9f54fb84ee0',
-       i686: 'b62c49e3d8c8e70bcd81fec5708a848a254385c74d788c9cf8225190b660ba97',
-     x86_64: 'ea3fffee782c2e8c4f755eed36e865a2cb8ae346728f820a32df1e21be147b62'
+    aarch64: 'b9513fdcba659ef938a74ab3b0a7615bba7600cb70f75e839ea311cb6a77dc47',
+     armv7l: 'b9513fdcba659ef938a74ab3b0a7615bba7600cb70f75e839ea311cb6a77dc47',
+       i686: 'ba0cf252f0ecdbdf3895464b3304c799390ec256981c67f868c1298186877e85',
+     x86_64: 'ed10371a0e72451fe98dcb37fa51760c34928e9cf8e677e0e5e62e94e09cbf9e'
   })
 
   depends_on 'ca_certificates' # L
@@ -31,6 +31,11 @@ class Ruby < Package
   conflicts_ok # Needed for successful build.
 
   # at run-time, system's gmp, openssl, and zlib can be used
+
+  def self.prebuild
+    # See https://github.com/chromebrew/chromebrew/issues/10724#issuecomment-2466486461
+    abort 'Please build with CREW_KERNEL_VERSION=3.10' if %w[armv7l aarch64].include?(ARCH) && Gem::Version.new(CREW_KERNEL_VERSION) > Gem::Version.new('3.10')
+  end
 
   def self.build
     system '[ -x configure ] || autoreconf -fiv'
