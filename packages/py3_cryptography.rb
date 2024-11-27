b24475ed5889b1e022aac69f8684b8734ce805b3
satmandu <satmandu@users.noreply.github.com>
2024-11-27 22:03:42 +0000

    Add unbuilt updated pip packages to updates-2024-11-27-22-02

--- packages/py3_cryptography.rb
+++ packages/py3_cryptography.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_cryptography < Pip
   description 'Cryptography provides cryptographic recipes and primitives to Python developers.'
   homepage 'https://cryptography.io/'
-  version "43.0.3-#{CREW_PY_VER}"
+  version "44.0.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
