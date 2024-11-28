f2c76abd511b29518bb39e8859543b97e98f6faf
satmandu <satmandu@users.noreply.github.com>
2024-11-27 23:29:05 -0500

    Add updated packages for linux/amd64 to updates-2024-11-28-04-24

--- packages/py3_pipdeptree.rb
+++ packages/py3_pipdeptree.rb
@@ -13,7 +13,7 @@ class Py3_pipdeptree < Pip
     aarch64: 'cd30883d52a4aedcb4d6915df60cc50b864f7301b642dc28b6a93c9d6b8c7257',
      armv7l: 'cd30883d52a4aedcb4d6915df60cc50b864f7301b642dc28b6a93c9d6b8c7257',
        i686: 'd7ab47aaa1a8ba5b8751ce01d9c582686e55a00743f46a9b3cc05f464adbe6ef',
-     x86_64: '5c6e54c943c38bb39b48a34ffaba1129230d192e23faea783878767e0d71f1bd'
+     x86_64: '34212c88debc7862aef953720ddc84072b10b1250cd55354369808ab42c23910'
   })
 
   depends_on 'python3' => :build
--- packages/py3_tomli.rb
+++ packages/py3_tomli.rb
@@ -13,11 +13,12 @@ class Py3_tomli < Pip
     aarch64: '4ab9cc614472ee8991c3a237954cd77e368b177c5c35e5420f6f7924343bb7dc',
      armv7l: '4ab9cc614472ee8991c3a237954cd77e368b177c5c35e5420f6f7924343bb7dc',
        i686: 'e6bc6d30574430306d25723e58bfc342083d17da1027fc50792ec8238ddc7e6b',
-     x86_64: 'ac424c4bab16702961e38864c98279e17e3ab2eca7310cec2b47a95d57348e80'
+     x86_64: 'a2b894aaa45b5d5fd55505af5214832ab28209dffbacb02d3af17ed3abb41780'
   })
 
   depends_on 'python3'
   depends_on 'py3_flit_core'
+  depends_on 'glibc' # R
 
   no_source_build
 end
