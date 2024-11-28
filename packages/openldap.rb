e89338e3530dab88095f176ec78af4025483e422
Ed Reel <edreel@gmail.com>
2024-11-28 06:34:13 +0000

    Openldap 2.6.8 => 2.6.9

--- packages/openldap.rb
+++ packages/openldap.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Openldap < Autotools
   description 'OpenLDAP Software is an open source implementation of the Lightweight Directory Access Protocol.'
   homepage 'https://www.openldap.org/'
-  version '2.6.8'
+  version '2.6.9'
   license 'OpenLDAP and GPL-2'
   compatibility 'all'
-  source_url 'https://openldap.org/software/download/OpenLDAP/openldap-release/openldap-2.6.8.tgz'
-  source_sha256 '48969323e94e3be3b03c6a132942dcba7ef8d545f2ad35401709019f696c3c4e'
+  source_url "https://openldap.org/software/download/OpenLDAP/openldap-release/openldap-#{version}.tgz"
+  source_sha256 '2cb7dc73e9c8340dff0d99357fbaa578abf30cc6619f0521972c555681e6b2ff'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3b475001d98c3143a48c57887a26fe2366149061c6dfeb6b26aa84fc2179a6cd',
-     armv7l: '3b475001d98c3143a48c57887a26fe2366149061c6dfeb6b26aa84fc2179a6cd',
-       i686: '86d7876872980466fd29a2f0fa94b1f2f08d7d9c99e72f74df30e6df0caa7a86',
-     x86_64: 'c60b1a431470cdef4b61b2cfbd16d512541a95e83a447f90327f5da03c7b6414'
+    aarch64: 'f48ecd71d964e7c422a5f06a602c3ea49545dffa774fe4cfb89bb5d475a5490d',
+     armv7l: 'f48ecd71d964e7c422a5f06a602c3ea49545dffa774fe4cfb89bb5d475a5490d',
+       i686: 'db013c7fe3e73cd8ae0dabf876d5146dbb7c5438cdd1e9bff56f8d211f792d6c',
+     x86_64: '40578ae19ead9a8ccba67cad00b278bc3bef87a08cd4e075ba6cdcac51eba290'
   })
 
   depends_on 'e2fsprogs' => :build
