5c61a671efee62637dca3875dcd961bd8249aa66
Ed Reel <edreel@gmail.com>
2024-11-28 19:05:46 +0000

    Update gnupg and dependencies
    Gnupg 2.4.6 => 2.4.7
    
    Gnutls 2.8.7.1 => 2.8.8
    
    Npth 1.7 => 1.8

--- packages/gnupg.rb
+++ packages/gnupg.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Gnupg < Autotools
   description 'GnuPG is a complete and free implementation of the OpenPGP standard as defined by RFC4880 (also known as PGP).'
   homepage 'https://gnupg.org/'
-  version '2.4.6'
+  version '2.4.7'
   license 'GPL-2'
   compatibility 'all'
   source_url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-#{version}.tar.bz2"
-  source_sha256 '95acfafda7004924a6f5c901677f15ac1bda2754511d973bb4523e8dd840e17a'
+  source_sha256 '7b24706e4da7e0e3b06ca068231027401f238102c41c909631349dcc3b85eb46'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '11f64f2bf8d36d22c4b12de99a5dba2e9eb471a897d2a124a7082a75569f3ce7',
-     armv7l: '11f64f2bf8d36d22c4b12de99a5dba2e9eb471a897d2a124a7082a75569f3ce7',
-       i686: '772375004996c7cb5837f9f0ed54d107b201db9dd099fec3c9b7ab2ee8cba9b8',
-     x86_64: '5915ea7434e4a720737832fd6a21fea46baa996c2c34bab4feba437f044b971d'
+    aarch64: '358da660d1034cd0486210638a35bd0334531555650634351695541940e207da',
+     armv7l: '358da660d1034cd0486210638a35bd0334531555650634351695541940e207da',
+       i686: '03f99a147bd90de24241fe2319e4137729167959e6b00d227a7b1b3d85f874f2',
+     x86_64: 'e3c31805bf60a2503ab6df58ae96ebf7c22accd63d7e1fdd3bb4159ff2d4fc09'
   })
 
   depends_on 'bzip2' # R
@@ -39,9 +39,4 @@ class Gnupg < Autotools
     --with-readline'
 
   run_tests
-
-  def self.patch
-    downloader 'https://files.gnupg.net/file/data/zy2tsxkydxq4okdhxj4p/PHID-FILE-cnig3h7b4hnrmr5efbh2/file', '7beaf4c72dc91a885fb088f7c1ef043ab49f230666727bcf2609954d722aba36', 'unistd.patch'
-    system 'patch -Np1 -i unistd.patch'
-  end
 end
