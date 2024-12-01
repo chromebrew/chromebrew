150b3afc51bdd131af4406f7c8d1a41880a0e80c
Satadru Pramanik <satadru@gmail.com>
2024-11-30 23:56:29 -0500

    Switch maturin to python buildsystem.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_maturin.rb
+++ packages/py3_maturin.rb
@@ -1,28 +1,20 @@
 # Adapted from Arch Linux maturin PKGBUILD at:
 # https://gitlab.archlinux.org/archlinux/packaging/packages/maturin/-/blob/main/PKGBUILD?ref_type=heads
 
-require 'buildsystems/pip'
+require 'buildsystems/python'
 
-class Py3_maturin < Package
+class Py3_maturin < Python
   description 'Build and publish crates with pyo3, rust-cpython and cffi bindings'
   homepage 'https://github.com/PyO3/maturin'
-  version '1.7.6'
+  version "1.7.6-#{CREW_PY_VER}"
   license 'Apache-2.0 OR MIT'
   compatibility 'all'
-  source_url 'SKIP'
+  source_url 'https://github.com/PyO3/maturin.git'
+  git_hashtag "v#{version.split('-').first}"
   binary_compression 'tar.zst'
-
-  binary_sha256({
-    aarch64: '10c452d5a74b1810ec56c42bb722593ae076ff743f1f33bf5aca63cedb7b15c6',
-     armv7l: '10c452d5a74b1810ec56c42bb722593ae076ff743f1f33bf5aca63cedb7b15c6',
-       i686: 'a280c4b575a7bbda4e4b09b9fda573da64b8c774f01eb8b12e83bc331d97800b',
-     x86_64: '0b3cc62c4f2ff1e65186d619fcab0c218fc50fb5877606e8dbbfac1dffa90701'
-  })
 
   depends_on 'py3_setuptools'
   depends_on 'py3_setuptools_rust'
   depends_on 'python3'
   depends_on 'rust'
-
-  no_source_build
 end
