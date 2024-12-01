1dff8afc880e28f47fe7221c73c8f1c939067733
satmandu <satmandu@users.noreply.github.com>
2024-12-01 00:33:36 -0500

    Add built packages for linux/386 to ruff

--- packages/py3_maturin.rb
+++ packages/py3_maturin.rb
@@ -16,13 +16,14 @@ class Py3_maturin < Python
   binary_sha256({
     aarch64: '06f1a37b7864fb8af6baa6d473e945226c0086a1a96bc1cf18edb7c860d66def',
      armv7l: '06f1a37b7864fb8af6baa6d473e945226c0086a1a96bc1cf18edb7c860d66def',
+       i686: 'ee8b37da3f76cded261e51bbda254bc199859290d4fb251125be8a37d6cded74',
      x86_64: '62ba08658e8d09f0b46cff6a70610ea43bcc364d73a98b54ac0de147b18cb50a'
   })
 
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
   depends_on 'py3_setuptools'
   depends_on 'py3_setuptools_rust'
   depends_on 'python3'
   depends_on 'rust'
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
 end
