55aee412c880a644a4214c0f8de25de3f110a312
Satadru Pramanik <satadru@gmail.com>
2024-11-30 23:15:47 -0500

    Switch ruff from pip to python buildsystem and update to 0.8.1.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_ruff.rb
+++ packages/py3_ruff.rb
@@ -1,12 +1,13 @@
-require 'buildsystems/pip'
+require 'buildsystems/python'
 
-class Py3_ruff < Pip
+class Py3_ruff < Python
   description 'An extremely fast Python linter, written in Rust.'
   homepage 'https://docs.astral.sh/ruff'
-  version "0.8.0-#{CREW_PY_VER}"
+  version "0.8.1-#{CREW_PY_VER}"
   license 'GPL-2.0'
   compatibility 'all'
-  source_url 'SKIP'
+  source_url 'https://github.com/astral-sh/ruff.git'
+  git_hashtag version.split('-').first
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -20,6 +21,4 @@ class Py3_ruff < Pip
   depends_on 'glibc' # R
   depends_on 'python3'
   depends_on 'rust' => :build
-
-  no_source_build
 end
