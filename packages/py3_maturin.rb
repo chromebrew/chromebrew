3d7c2de24f547b06091090cfdb5e112c09cec24e
Satadru Pramanik <satadru@gmail.com>
2024-11-30 23:46:32 -0500

    Add py3_maturin package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/py3_maturin.rb
@@ -0,0 +1,20 @@
+# Adapted from Arch Linux maturin PKGBUILD at:
+# https://gitlab.archlinux.org/archlinux/packaging/packages/maturin/-/blob/main/PKGBUILD?ref_type=heads
+
+require 'buildsystems/python'
+
+class Py3_maturin < Python
+  description 'Build and publish crates with pyo3, rust-cpython and cffi bindings'
+  homepage 'https://github.com/PyO3/maturin'
+  version "1.7.6-#{CREW_PY_VER}"
+  license 'Apache-2.0 OR MIT'
+  compatibility 'all'
+  source_url 'https://github.com/PyO3/maturin.git'
+  git_hashtag "v#{version.split('-').first}"
+  binary_compression 'tar.zst'
+
+  depends_on 'py3_setuptools'
+  depends_on 'py3_setuptools_rust'
+  depends_on 'python3'
+  depends_on 'rust'
+end
