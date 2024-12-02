7e8803b8172ed589742c4a2264e9f81b30809fae
satmandu <satmandu@users.noreply.github.com>
2024-12-01 23:19:00 -0500

    Add built packages for linux/386 to gem_builds

--- packages/ruby_fcntl.rb
+++ packages/ruby_fcntl.rb
@@ -12,6 +12,7 @@ class Ruby_fcntl < RUBY
   binary_sha256({
     aarch64: '5c70a361da3cedbac596ce40bfe1807a0a5a263cedeb290aa7db99abe65793f8',
      armv7l: '5c70a361da3cedbac596ce40bfe1807a0a5a263cedeb290aa7db99abe65793f8',
+       i686: '1af766faa607737239f2e96a9991e56610ae9e666e9f848627061922276a1b84',
      x86_64: '5d7e8d0d5620baf40e0c091099b8aa5e1c9e37058efcfdeabb43876b545d2c05'
   })
 
