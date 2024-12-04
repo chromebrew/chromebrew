8801aff94c34372bb992dd11b3022d9fe67d9b09
github-merge-queue <github-merge-queue@users.noreply.github.com>
2024-12-03 21:10:47 -0500

    Add updated packages for linux/arm/v7 to updates-2024-12-04-00-20

--- packages/awscli.rb
+++ packages/awscli.rb
@@ -10,8 +10,8 @@ class Awscli < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '319341bda0bde67eb06f7eb6f1665fbf8bd06d2c616056a9890b6bc5317916dd',
-     armv7l: '319341bda0bde67eb06f7eb6f1665fbf8bd06d2c616056a9890b6bc5317916dd',
+    aarch64: '703ca6f6351abc7e904945e0fb8baf9dc1546e7d7dbda88c50879bc3519058b7',
+     armv7l: '703ca6f6351abc7e904945e0fb8baf9dc1546e7d7dbda88c50879bc3519058b7',
        i686: 'dd3fe05e088c8a488d02d5e31181ff62693c8ae81a76e6d6ca712d9ced388bd8',
      x86_64: '2c93f9866cf80db93805368cc53ab7b3c809921f4823ff07672bbaab2d7b8dfa'
   })
--- packages/py3_mako.rb
+++ packages/py3_mako.rb
@@ -10,8 +10,8 @@ class Py3_mako < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '220ed206ccd2915c267ac076cf4bda40e388f7011195c8cca8b5b9f2e01c8816',
-     armv7l: '220ed206ccd2915c267ac076cf4bda40e388f7011195c8cca8b5b9f2e01c8816',
+    aarch64: '56555446b78f3b62a62fa7f540c9feaf06fa4cab61ae425ec7e4dbb7d57f4af4',
+     armv7l: '56555446b78f3b62a62fa7f540c9feaf06fa4cab61ae425ec7e4dbb7d57f4af4',
        i686: '2f0d2eba18269af9bc8ba51fa3268c4d80e12a7cae3a05cb90fd067648c78813',
      x86_64: 'ebbba63584eacd7cc9c50a5fdae18ae83e70f1030835403707187b83872ec5da'
   })
