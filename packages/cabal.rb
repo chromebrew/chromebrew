b4eb5e93e96dd7c36c4696b0aa66a61de194a8e7
Ed Reel <edreel@gmail.com>
2024-12-04 20:31:08 -0600

    Cabal 3.10.3.0 => 3.12.1.0 (#10867)

--- packages/cabal.rb
+++ packages/cabal.rb
@@ -3,16 +3,16 @@ require 'package'
 class Cabal < Package
   description 'Common Architecture for Building Applications and Libraries'
   homepage 'https://www.haskell.org/cabal/'
-  version '3.10.3.0'
+  version '3.12.1.0'
   license 'BSD'
   compatibility 'x86_64 i686'
   source_url({
-    x86_64: 'https://downloads.haskell.org/~cabal/cabal-install-3.10.3.0/cabal-install-3.10.3.0-x86_64-linux-alpine3_12.tar.xz',
-      i686: 'https://downloads.haskell.org/~cabal/cabal-install-3.10.3.0/cabal-install-3.10.3.0-i386-linux-alpine3_12.tar.xz'
+    x86_64: "https://downloads.haskell.org/~cabal/cabal-install-#{version}/cabal-install-#{version}-x86_64-linux-alpine3_12.tar.xz",
+      i686: "https://downloads.haskell.org/~cabal/cabal-install-#{version}/cabal-install-#{version}-i386-linux-alpine3_12.tar.xz"
   })
   source_sha256({
-    x86_64: '284978da43d9d52af678e52cbae8fb3359ec59180130b099913f5d3e6f19fda4',
-      i686: '284978da43d9d52af678e52cbae8fb3359ec59180130b099913f5d3e6f19fda4'
+    x86_64: '6fa71b75c87cd57700348fc9c089703201aac5fd248a92e6bc7346fd2e007da7',
+      i686: '7890041cba03195bac2835eecaec349c39aad2ec0b729bd0c92f6f7f7ea240bf'
   })
 
   no_compile_needed
