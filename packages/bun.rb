4a784ce832ac10ab174555e04d1d238b88b6f9e8
Ed Reel <edreel@gmail.com>
2024-12-04 19:42:25 -0600

    Bun 1.1.34 => 1.1.38 (#10865)

--- packages/bun.rb
+++ packages/bun.rb
@@ -3,7 +3,7 @@ require 'package'
 class Bun < Package
   description 'Incredibly fast JavaScript runtime, bundler, test runner, and package manager – all in one'
   homepage 'https://bun.sh/'
-  version '1.1.34'
+  version '1.1.38'
   license 'MIT'
   compatibility 'x86_64'
   source_url 'SKIP'
