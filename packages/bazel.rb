d6df83aa289e6c4c10a4435592889e2bc4244a30
Ed Reel <edreel@gmail.com>
2024-12-06 07:48:20 +0000

    Bazel 7.4.0 => 7.4.1

--- packages/bazel.rb
+++ packages/bazel.rb
@@ -3,11 +3,11 @@ require 'package'
 class Bazel < Package
   description 'a fast, scalable, multi-language and extensible build system'
   homepage 'https://bazel.build/'
-  version '7.4.0'
+  version '7.4.1'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/bazelbuild/bazel/releases/download/#{version}/bazel-#{version}-linux-x86_64"
-  source_sha256 '48ea0ff9d397a48add6369c261c5a4431fe6d5d5348cfb81411782fb80c388d3'
+  source_sha256 'c97f02133adce63f0c28678ac1f21d65fa8255c80429b588aeeba8a1fac6202b'
 
   no_compile_needed
   no_shrink
