27530c2fd6ed1730ef4c6dfdf5c4df50675fc3ac
Satadru Pramanik <satadru@gmail.com>
2024-12-07 15:26:07 -0500

    Only have tools/version.rb report stable version updates when using 'crew check -V <package>'.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- tools/version.rb
+++ tools/version.rb
@@ -22,9 +22,9 @@ def get_version(name, homepage)
   anitya_id = get_anitya_id(name, homepage)
   # If we weren't able to get an Anitya ID, return early here to save time and headaches
   return nil if anitya_id.nil?
-  # Get the latest version of the package
+  # Get the latest stable version of the package
   json = JSON.parse(Net::HTTP.get(URI("https://release-monitoring.org/api/v2/versions/?project_id=#{anitya_id}")))
-  return json['latest_version']
+  return json['stable_versions'][0]
 end
 
 def get_anitya_id(name, homepage)
