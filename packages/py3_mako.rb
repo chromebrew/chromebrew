28760d02b07ed47046430ce95fd20cc9203242fe
github-merge-queue <github-merge-queue@users.noreply.github.com>
2024-12-07 19:24:21 -0500

    Add updated packages for linux/amd64 to updates-2024-12-08-00-22

--- packages/py3_mako.rb
+++ packages/py3_mako.rb
@@ -13,7 +13,7 @@ class Py3_mako < Pip
     aarch64: '56555446b78f3b62a62fa7f540c9feaf06fa4cab61ae425ec7e4dbb7d57f4af4',
      armv7l: '56555446b78f3b62a62fa7f540c9feaf06fa4cab61ae425ec7e4dbb7d57f4af4',
        i686: '755ccb9d1bc4bdfa3c040c63ee676a5df997a50eb09969dff4a778e3c8fd05d2',
-     x86_64: 'ebbba63584eacd7cc9c50a5fdae18ae83e70f1030835403707187b83872ec5da'
+     x86_64: '27a69a779929a48ef31dc7f59208037c0af315d9dbb136187de7f09eccd49165'
   })
 
   depends_on 'py3_markupsafe'
