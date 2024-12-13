38c223e828427d31869fd0fedfdc18ecac3e6c71
satmandu <satmandu@users.noreply.github.com>
2024-12-13 18:50:19 -0500

    Add built packages for linux/arm/v7 to automake

--- packages/automake.rb
+++ packages/automake.rb
@@ -11,8 +11,8 @@ class Automake < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b991366b2b59e1c6bdbce17789552240019d7e4d175661dacf3533a66e584037',
-     armv7l: 'b991366b2b59e1c6bdbce17789552240019d7e4d175661dacf3533a66e584037',
+    aarch64: '7a811c20ed1cccfa17f78d2fc20a742765354d37eaa5d889a26b2abaae036bbb',
+     armv7l: '7a811c20ed1cccfa17f78d2fc20a742765354d37eaa5d889a26b2abaae036bbb',
        i686: '0487cf58b02db8a932cb4c8691460d8f3034d154d9905e23be5ab96f442c0445',
      x86_64: 'e5bd0e72355c5aaaed40a118f253fe3004cb44a8f7325f79708820b4596d396a'
   })
