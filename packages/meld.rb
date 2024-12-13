44530e0eb01d506671d6f526019c6ff8ab25dca0
Ed Reel <edreel@gmail.com>
2024-12-13 17:36:53 -0600

    Meld 3.22.1-py3.13 => 3.23.0-py3.13 (#10960)

--- packages/meld.rb
+++ packages/meld.rb
@@ -1,33 +1,31 @@
-require 'buildsystems/meson'
+require 'package'
 
-class Meld < Meson
+class Meld < Package
   description 'Meld is a visual diff and merge tool targeted at developers.'
   homepage 'https://meldmerge.org/'
-  @_ver = '3.22.1'
-  version "#{@_ver}-#{CREW_PY_VER}"
+  version "3.23.0-#{CREW_PY_VER}"
   license 'GPL-2'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.gnome.org/GNOME/meld.git'
-  git_hashtag @_ver
+  git_hashtag '16224fb03d1652e2a5dc75e491719db539a106a0'
   binary_compression 'tar.zst'
-
-  binary_sha256({
-    aarch64: '8c9c00dfe88d9afb6f486cf3e2cd8788fb45a27992e856056ee98960e4528547',
-     armv7l: '8c9c00dfe88d9afb6f486cf3e2cd8788fb45a27992e856056ee98960e4528547',
-     x86_64: '5fa48ea39ba4f272052001622f828eb2b67eed4065037ab93d8a20baeccd75b1'
-  })
 
   depends_on 'desktop_file_utils' # L
   depends_on 'gtk3' # L
   depends_on 'gtksourceview_4' # L
   depends_on 'py3_libxml2' # L
   depends_on 'py3_pycairo' # L
+  depends_on 'py3_pygobject' # L
   depends_on 'python3' # L
 
   gnome
+  no_compile_needed
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
-    system "sed -i 's:#!/usr/bin/python3:#!/usr/bin/env python3:' #{CREW_DEST_PREFIX}/bin/meld"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/meld"
+    FileUtils.cp_r Dir['*'], "#{CREW_DEST_PREFIX}/share/meld"
+    FileUtils.install 'data/meld.1', "#{CREW_DEST_MAN_PREFIX}/man1/meld.1", mode: 0o644
+    FileUtils.ln_s "#{CREW_PREFIX}/share/meld/bin/meld", "#{CREW_DEST_PREFIX}/bin/meld"
   end
 end
