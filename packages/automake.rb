1f5215379f1e5fc8e00b88b50e54134807da9d67
Satadru Pramanik <satadru@gmail.com>
2024-12-13 16:22:27 -0500

    Automake => 1.17
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/automake.rb
+++ packages/automake.rb
@@ -1,12 +1,12 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Automake < Package
+class Automake < Autotools
   description 'Automake is a tool for automatically generating Makefile.in files compliant with the GNU Coding Standards.'
   homepage 'https://www.gnu.org/software/automake/'
-  version '1.16.5'
+  version '1.17'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gnu/automake/automake-1.16.5.tar.xz'
+  source_url "https://ftpmirror.gnu.org/gnu/automake/automake-#{version}.tar.xz"
   source_sha256 'f01d58cd6d9d77fbdca9eb4bbd5ead1988228fdb73d6f7a201f5f8d6b118b469'
   binary_compression 'tar.xz'
 
@@ -18,13 +18,4 @@ class Automake < Package
   })
 
   depends_on 'autoconf'
-
-  def self.build
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
