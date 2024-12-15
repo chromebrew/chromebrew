2bb3bf3755ea421cd3e4f12c22ca6ee3d3e8217a
github-merge-queue <github-merge-queue@users.noreply.github.com>
2024-12-14 19:25:59 -0500

    Add updated packages for linux/arm/v7 to updates-2024-12-15-00-22

--- packages/py3_certifi.rb
+++ packages/py3_certifi.rb
@@ -10,8 +10,8 @@ class Py3_certifi < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '61bc904434759198ddfd30a6cec6a340e87daf15f186ab9556eb083bb43dbfee',
-     armv7l: '61bc904434759198ddfd30a6cec6a340e87daf15f186ab9556eb083bb43dbfee',
+    aarch64: '5ebcc5e98b5a370084759c5d5d50546d70b89d6b881e6e64264b181d9ec73d05',
+     armv7l: '5ebcc5e98b5a370084759c5d5d50546d70b89d6b881e6e64264b181d9ec73d05',
        i686: 'eb86cc67fbecb05b079de34731d7e4cac0aed998e7eee3bfc6c747a71ac377f5',
      x86_64: '3e0c45c38eabc69dab21a80e9a1db0ff146de769e87bc8e26dfa202649ea072c'
   })
