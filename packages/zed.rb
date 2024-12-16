b8d482ea03fbedc96f2a6080d7d27734b6c81a96
Ed Reel <edreel@gmail.com>
2024-12-15 19:11:37 -0600

    Zed 0.163.3 => 0.165.4 (#10984)

--- packages/zed.rb
+++ packages/zed.rb
@@ -3,12 +3,12 @@ require 'package'
 class Zed < Package
   description 'Zed is a high-performance, multiplayer code editor'
   homepage 'https://zed.dev/'
-  version '0.163.3'
+  version '0.165.4'
   license 'GPL-3, AGPL-3, Apache-2.0'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-x86_64.tar.gz"
-  source_sha256 '7b4fa77e8aa767114a1344cccab0a42340aeaca8d7ba27d656d68fb6497668f4'
+  source_sha256 '006a3cf587da3056f49e146525af9606ca75558326961364f950f5a6f080b1eb'
 
   depends_on 'alsa_lib'
   depends_on 'libbsd'
