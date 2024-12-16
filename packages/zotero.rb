c8b9adc48797fafe5b221339a4fc2a71f7c24cd8
Ed Reel <edreel@gmail.com>
2024-12-16 00:50:09 +0000

    Zotero 7.0.10 => 7.0.11

--- packages/zotero.rb
+++ packages/zotero.rb
@@ -3,11 +3,11 @@ require 'package'
 class Zotero < Package
   description 'Zotero is a free, easy-to-use tool to help you collect, organize, annotate, cite, and share research.'
   homepage 'https://www.zotero.org/'
-  version '7.0.10'
+  version '7.0.11'
   license 'GPL-3'
   compatibility 'x86_64'
   source_url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}_linux-x86_64.tar.bz2"
-  source_sha256 'b742c0a5a535ded4ffd789ef4699d66701725a226af967d936057284d6b231cb'
+  source_sha256 '4a5760e2c47ec06d171f4d2609c9ece5cc9f633360ed140fe2ff7954d33a46ee'
 
   depends_on 'dbus_glib'
   depends_on 'gtk3'
