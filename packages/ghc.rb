e85d7837b50dd13334d242b4116382fc69b3be91
Ed Reel <edreel@gmail.com>
2024-12-16 01:55:16 +0000

    Ghc 9.0.2 => 9.8.4

--- packages/ghc.rb
+++ packages/ghc.rb
@@ -3,13 +3,14 @@ require 'package'
 class Ghc < Package
   description 'The Glasgow Haskell Compiler is a state-of-the-art, open source compiler and interactive environment for the functional language Haskell.'
   homepage 'https://www.haskell.org/ghc/'
-  version '9.0.2'
+  version '9.8.4'
   license 'BSD'
   compatibility 'x86_64'
   source_url 'SKIP'
 
   no_compile_needed
 
+  depends_on 'libtinfo'
   depends_on 'stack'
 
   def self.build
