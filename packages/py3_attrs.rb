1abf43714eb3c7b08e5494d9ef1b1a8cf4995312
github-merge-queue <github-merge-queue@users.noreply.github.com>
2024-12-16 19:26:21 -0500

    Add updated packages for linux/amd64 to updates-2024-12-17-00-20

--- packages/py3_attrs.rb
+++ packages/py3_attrs.rb
@@ -13,7 +13,7 @@ class Py3_attrs < Pip
     aarch64: 'a785e7de88f7c6260cf9406baf45f8fe148f31544e27ed8d4c6fc5d03c708c87',
      armv7l: 'a785e7de88f7c6260cf9406baf45f8fe148f31544e27ed8d4c6fc5d03c708c87',
        i686: '59faf9c8c08a400e9dca233d220b8236fda5e823b6017760596f60af3770cd96',
-     x86_64: '81b5cb4f23f0fe01afe430d6366a9c6b17f14c70dce9cc3daa28a54874b5df69'
+     x86_64: 'cf30acab3973341a84b00eb5d3ad75bb76aec27d2d8bf0e6876ec5502d6cddd2'
   })
 
   depends_on 'python3' => :build
