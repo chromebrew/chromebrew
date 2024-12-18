b903e397359806ecc916e797b5673e759ffe7edf
Satadru Pramanik <satadru@gmail.com>
2024-12-18 00:15:00 -0500

    Bump version on llvm19_dev, llvm19_lib
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/llvm19_lib.rb
+++ packages/llvm19_lib.rb
@@ -4,7 +4,7 @@ Package.load_package("#{__dir__}/llvm19_build.rb")
 class Llvm19_lib < Package
   description 'LibLLVM and llvm-strip'
   homepage Llvm19_build.homepage
-  version '19.1.5'
+  version '19.1.6'
   # When upgrading llvm*_build, be sure to upgrade llvm_lib*, llvm_dev*, libclc, and openmp in tandem.
   puts "#{self} version differs from llvm version #{Llvm19_build.version}".orange if version != Llvm19_build.version
   license Llvm19_build.license
