63ee19376a2989c27873a2d9dbc543ea6f812518
Satadru Pramanik <satadru@gmail.com>
2024-12-20 06:59:44 -0500

    mesa => 24.3.2
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Mesa < Meson
   description 'Open-source implementation of the OpenGL specification'
   homepage 'https://www.mesa3d.org'
-  version '24.3.1-llvm19'
+  version '24.3.2-llvm19'
   license 'MIT'
   compatibility 'x86_64 aarch64 armv7l'
   source_url 'https://gitlab.freedesktop.org/mesa/mesa.git'
