490f3289ff2f8455c74a8304d81db82985728da6
Ed Reel <edreel@gmail.com>
2024-12-28 22:01:44 -0600

    Doxygen 1.11.0 => 1.13.0

--- packages/doxygen.rb
+++ packages/doxygen.rb
@@ -3,18 +3,18 @@ require 'buildsystems/cmake'
 class Doxygen < CMake
   description 'Doxygen is the de facto standard tool for generating documentation from annotated C++ sources, but it also supports other popular programming languages such as C, Objective-C, C#, PHP, Java, Python, IDL (Corba, Microsoft, and UNO/OpenOffice flavors), Fortran, VHDL, Tcl, and to some extent D.'
   homepage 'https://www.doxygen.nl/'
-  version '1.11.0'
+  version '1.13.0'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/doxygen/doxygen.git'
-  git_hashtag 'Release_1_11_0'
+  git_hashtag "Release_#{version.gsub('.', '_')}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '15dedc67203dc4b24273e0efed9296c7cdd63e6a3c65e869047890c9bc24aab3',
-     armv7l: '15dedc67203dc4b24273e0efed9296c7cdd63e6a3c65e869047890c9bc24aab3',
-       i686: 'aad9ef98fd93177a5d90c4d616de28c49a58a4cf586a4a79ca927836dc02bfb3',
-     x86_64: 'f3bdbafbb4e0f1e912959fbba038b403180952a17e48704283d072225b14e806'
+    aarch64: '6310bab6da18b11eaf67580b51eff5e666fa1803bd79005b0ad125abe56d1347',
+     armv7l: '6310bab6da18b11eaf67580b51eff5e666fa1803bd79005b0ad125abe56d1347',
+       i686: 'f46ff831b6c7f25363186787ae1ef2cfaf1ca34703bd7a4791767601987c1234',
+     x86_64: '75c759b7f4df9c08b9bff7808767b7d2decdc19524b99cc92f7ed2e588660c26'
   })
 
   depends_on 'python3' => :build
