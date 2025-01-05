b7082fe30c685f7b4cb18dd4e89d2e87b32deba2
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-01-05 00:21:32 +0000

    Add unbuilt updated ruby gem packages to updates-2025-01-05-00-20

--- packages/ruby_date.rb
+++ packages/ruby_date.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_date < RUBY
   description 'A subclass of object includes comparable module for handling dates.'
   homepage 'https://github.com/ruby/date'
-  version "3.4.0-#{CREW_RUBY_VER}"
+  version "3.4.1-#{CREW_RUBY_VER}"
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
--- packages/ruby_openssl.rb
+++ packages/ruby_openssl.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_openssl < RUBY
   description 'Openssl for ruby provides access to ssl/tls and general-purpose cryptography based on the openssl library.'
   homepage 'https://github.com/ruby/openssl'
-  version "3.2.0-#{CREW_RUBY_VER}"
+  version "3.3.0-#{CREW_RUBY_VER}"
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
