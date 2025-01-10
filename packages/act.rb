98e60cb7357691f4569bfb80e72a4cf2fa5d8ef3
Ed Reel <edreel@gmail.com>
2025-01-10 16:41:13 -0600

    Act 0.2.34 => 0.2.71 (#11121)
    Co-authored-by: Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>

--- packages/act.rb
+++ packages/act.rb
@@ -3,20 +3,20 @@ require 'package'
 class Act < Package
   description 'Run your GitHub Actions locally'
   homepage 'https://github.com/nektos/act'
-  version '0.2.34'
+  version '0.2.71'
   license 'MIT'
   compatibility 'all'
   source_url({
-    aarch64: 'https://github.com/nektos/act/releases/download/v0.2.34/act_Linux_armv7.tar.gz',
-     armv7l: 'https://github.com/nektos/act/releases/download/v0.2.34/act_Linux_armv7.tar.gz',
-       i686: 'https://github.com/nektos/act/releases/download/v0.2.34/act_Linux_i386.tar.gz',
-     x86_64: 'https://github.com/nektos/act/releases/download/v0.2.34/act_Linux_x86_64.tar.gz'
+    aarch64: "https://github.com/nektos/act/releases/download/v#{version}/act_Linux_armv7.tar.gz",
+     armv7l: "https://github.com/nektos/act/releases/download/v#{version}/act_Linux_armv7.tar.gz",
+       i686: "https://github.com/nektos/act/releases/download/v#{version}/act_Linux_i386.tar.gz",
+     x86_64: "https://github.com/nektos/act/releases/download/v#{version}/act_Linux_x86_64.tar.gz"
   })
   source_sha256({
-    aarch64: '4ed0b6af2f7dd0f744cc88b9adcbede9522a515510f4169724a251f9da6f96c3',
-     armv7l: '4ed0b6af2f7dd0f744cc88b9adcbede9522a515510f4169724a251f9da6f96c3',
-       i686: 'eeaa3219a22f08f8a2d1f684aed5ad8abf5b00f264926fbd0ef9c69de507ecf9',
-     x86_64: 'b8429964db6f265ddc1834802acd3ffb891249165edd6a9228e94c3d0cbbd8c9'
+    aarch64: '5e6caeafc26777e56a2eb9230a4be92d8a1ff0ec44495946bd5eff76e5257fd3',
+     armv7l: '5e6caeafc26777e56a2eb9230a4be92d8a1ff0ec44495946bd5eff76e5257fd3',
+       i686: '53aa859c9317ad7475d3b8d01e1649c2fed04e3a12898027a24718ea733bc236',
+     x86_64: 'f00cd2ca6310b73831042d01e3c2f00cb60c28620588e981bf551a0aca659dc5'
   })
 
   no_compile_needed
