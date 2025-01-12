7aa2b7fd19e1583eab62668fd8f48207e3fa96c4
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-01-11 20:17:29 -0500

    Add byebug gem and fix regression in crew install of no_source_build packages (#11126)
    * Add Ruby Byebug gem
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Fix regression in crew install of no_source_build package binaries.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * lint
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * bump version
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * cleanup
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/ruby_byebug.rb
@@ -0,0 +1,21 @@
+require 'buildsystems/ruby'
+
+class Ruby_byebug < RUBY
+  description 'Byebug is a Ruby debugger.'
+  homepage 'https://github.com/deivid-rodriguez/byebug'
+  version "11.1.3-#{CREW_RUBY_VER}"
+  license 'BSD-2-Clause'
+  compatibility 'all'
+  source_url 'SKIP'
+  binary_compression 'gem'
+
+  binary_sha256({
+    aarch64: 'a66196f0efe8c3d71684307b069a33483e56ec1d2217e4f33101fdbfef1f1f87',
+     armv7l: 'a66196f0efe8c3d71684307b069a33483e56ec1d2217e4f33101fdbfef1f1f87',
+       i686: '59217683304ae983a704955c2082222b5eeefc369b62804fc06cbc01d567cd39',
+     x86_64: 'c668fb6e1798a9ac0091fcaae08d47d09af02762204f839754619c385621b559'
+  })
+
+  conflicts_ok
+  gem_compile_needed
+end
--- packages/ruby_pry_byebug.rb
+++ packages/ruby_pry_byebug.rb
@@ -8,6 +8,7 @@ class Ruby_pry_byebug < RUBY
   compatibility 'all'
   source_url 'SKIP'
 
+  depends_on 'ruby_byebug' # L
   depends_on 'ruby_pry' # L
   conflicts_ok
   no_compile_needed
