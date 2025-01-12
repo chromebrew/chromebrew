26cc9889ab3e01c183de1c227947f78270e74eb0
uberhacker <uberhacker@users.noreply.github.com>
2025-01-12 02:23:47 -0500

    Add built packages for linux/amd64 to update-magic

--- packages/magic.rb
+++ packages/magic.rb
@@ -13,12 +13,18 @@ class Magic < Autotools
   binary_sha256({
     aarch64: 'd79946662a86b2691f78b594ae0694ae07deb566848be63924fd00fafbe36693',
      armv7l: 'd79946662a86b2691f78b594ae0694ae07deb566848be63924fd00fafbe36693',
-     x86_64: '3b57b174f7fe788bca276885aa74e855210e2bb1560cb0270a61742bc5b55733'
+     x86_64: '063af0803d89568fef8d1d0b1749f9f73fe85ec600c937aa6a2c5e92ee4fbfa5'
   })
 
   depends_on 'cairo'
   depends_on 'tcl'
   depends_on 'tk'
+  depends_on 'harfbuzz' # R
+  depends_on 'fontconfig' # R
+  depends_on 'freetype' # R
+  depends_on 'glibc' # R
+  depends_on 'libx11' # R
+  depends_on 'zlib' # R
 
   # https://github.com/RTimothyEdwards/magic/issues/323
   configure_options "--with-tcl=#{CREW_LIB_PREFIX} --with-tk=#{CREW_LIB_PREFIX} CFLAGS='-fpermissive'"
