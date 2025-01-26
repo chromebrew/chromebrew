e4060a40cc4fec5c0c6440263a96062dc20a6ca5
Ed Reel <edreel@gmail.com>
2025-01-26 15:30:34 -0600

    Update no_compile_needed packages
    Appflowy 0.8.1 => 0.8.2
    
    Bun 1.1.45 => 1.2.0
    
    Composer 2.8.4 => 2.8.5
    
    Flutter 3.27.2 => 3.27.3
    
    Gradle 8.11.1 => 8.12.1
    
    Joplin 3.2.11 => 3.2.12
    
    Nocodb 0.258.10 => 0.260.2
    
    Package 1.11.2 => 1.12.0
    
    Rqlite 8.36.6 => 8.36.8
    
    Tauri_cli 2.2.4 => 2.2.5
    
    Zed 0.169.2 => 0.170.2

--- packages/tauri_cli.rb
+++ packages/tauri_cli.rb
@@ -3,12 +3,12 @@ require 'package'
 class Tauri_cli < Package
   description 'Build smaller, faster, and more secure desktop and mobile applications with a web frontend.'
   homepage 'https://tauri.app/'
-  version '2.2.4'
+  version '2.2.5'
   license 'Apache-2.0, MIT'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/tauri-apps/tauri/releases/download/tauri-cli-v#{version}/cargo-tauri-x86_64-unknown-linux-gnu.tgz"
-  source_sha256 'aea58bf34bc55cc4f44cc5577c65947576862414e1e6b3710cefc84d4951c360'
+  source_sha256 '0e9c1098f13ede6952bce09305f22bf300a67f7e6945a0198ce4f785ebb17c42'
 
   no_compile_needed
 
