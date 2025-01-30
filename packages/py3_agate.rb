221258bd2b8a2d087665ac05e57a16b991ab355b
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-01-30 00:19:01 +0000

    Add unbuilt updated pip packages to updates-2025-01-30-00-18

--- packages/py3_agate.rb
+++ packages/py3_agate.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_agate < Pip
   description 'Agate is a data analysis library that is optimized for humans instead of machines.'
   homepage 'https://agate.readthedocs.io/'
-  version "1.12.0-#{CREW_PY_VER}"
+  version "1.13.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
