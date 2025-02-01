e906e19a3968e1c54997d13b878c83c96d0ad38a
Ed Reel <edreel@gmail.com>
2025-02-01 01:09:21 +0000

    Httpd 2.4.62 => 2.4.63

--- packages/httpd.rb
+++ packages/httpd.rb
@@ -1,13 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
+require 'convenience_functions'
 
-class Httpd < Package
+class Httpd < Autotools
   description 'The Apache HTTP Server Project is an effort to develop and maintain an open-source HTTP server for modern operating systems including UNIX and Windows.'
   homepage 'https://httpd.apache.org/'
-  version '2.4.62'
+  version '2.4.63'
   license 'GPL-2+'
   compatibility 'all'
   source_url "https://dlcdn.apache.org/httpd/httpd-#{version}.tar.bz2"
-  source_sha256 '674188e7bf44ced82da8db522da946849e22080d73d16c93f7f4df89e25729ec'
+  source_sha256 '88fc236ab99b2864b248de7d49a008ec2afd7551e64dce8b95f58f32f94c46ab'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -23,27 +24,36 @@ class Httpd < Package
   depends_on 'libxcrypt'
   depends_on 'pcre'
   depends_on 'expat'
+  depends_on 'brotli' # R
+  depends_on 'glibc' # R
+  depends_on 'libnghttp2' # R
+  depends_on 'openssl' # R
+  depends_on 'pcre2' # R
+  depends_on 'util_linux' # R
+  depends_on 'zlib' # R
 
   print_source_bashrc
 
-  def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}/share/httpd",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           "--bindir=#{CREW_PREFIX}/bin",
-           "--sbindir=#{CREW_PREFIX}/bin",
-           '--disable-maintainer-mode',
-           '--disable-debugger-mode'
-    system 'make'
+  def self.prebuild
+    ConvenienceFunctions.libtoolize('expat')
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/bash.d"
-    File.write "#{CREW_DEST_PREFIX}/etc/bash.d/10-httpd", <<~EOF
+  configure_build_extras do
+    File.write '10-httpd', <<~EOF
       #!/bin/bash
       [ -f #{CREW_PREFIX}/bin/apachectl ] && sudo #{CREW_PREFIX}/bin/apachectl start
     EOF
+  end
+
+  configure_options "--prefix=#{CREW_PREFIX}/share/httpd \
+    --libdir=#{CREW_LIB_PREFIX} \
+    --bindir=#{CREW_PREFIX}/bin \
+    --sbindir=#{CREW_PREFIX}/bin \
+    --disable-maintainer-mode \
+    --disable-debugger-mode"
+
+  configure_install_extras do
+    FileUtils.install '10-httpd', "#{CREW_PREFIX}/etc/bash.d/10-httpd", mode: 0o755
   end
 
   def self.postinstall
@@ -60,17 +70,6 @@ class Httpd < Package
   end
 
   def self.postremove
-    config_dir = "#{CREW_PREFIX}/share/httpd"
-    if Dir.exist? config_dir
-      puts 'WARNING: This will remove all hosting files and configuration.'.orange
-      print "Would you like to remove the #{config_dir} directory? [y/N] "
-      case $stdin.gets.chomp.downcase
-      when 'y', 'yes'
-        FileUtils.rm_rf config_dir
-        puts "#{config_dir} removed.".lightgreen
-      else
-        puts "#{config_dir} saved.".lightgreen
-      end
-    end
+    Package.agree_to_remove("#{CREW_PREFIX}/share/httpd")
   end
 end
