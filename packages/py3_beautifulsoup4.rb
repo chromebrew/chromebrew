499cefb6ff31ceb3e5dd9403395a4971511cc9f5
satmandu <satmandu@users.noreply.github.com>
2025-02-04 09:30:54 -0500

    Add built packages for linux/amd64 to updates-2025-02-04-00-18

--- packages/py3_beautifulsoup4.rb
+++ packages/py3_beautifulsoup4.rb
@@ -13,7 +13,7 @@ class Py3_beautifulsoup4 < Pip
     aarch64: '47cbe7a9359c7246cd540073086e871b6be8b821bab7432d123e4ba431d64cb7',
      armv7l: '47cbe7a9359c7246cd540073086e871b6be8b821bab7432d123e4ba431d64cb7',
        i686: 'f91fa6e0a2329196c83e5481a8f857e12a3d7c9300315e56f386ea4164595f4d',
-     x86_64: 'b3a208aab090a0a63a9ce815c9af85fc965a9255ca5519d9de64a96a00412e8d'
+     x86_64: 'aca0ba6cbaef5dd23432ab3497d0d9b31e16a01cb1155a3c905be8a5d4553fad'
   })
 
   depends_on 'py3_hatchling' => :build
