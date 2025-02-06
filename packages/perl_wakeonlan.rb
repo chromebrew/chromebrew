26314168b2c487b3cee3033aaafa2ac7c10ad612
uberhacker <uberhacker@users.noreply.github.com>
2025-02-06 00:42:12 -0500

    Add built packages for linux/amd64 to add-perl_wakeonlan

--- packages/perl_wakeonlan.rb
+++ packages/perl_wakeonlan.rb
@@ -9,4 +9,8 @@ class Perl_wakeonlan < PERL
   source_url 'https://github.com/jpoliv/wakeonlan.git'
   git_hashtag "v#{version.split('-')[0]}"
   binary_compression 'tar.zst'
+
+  binary_sha256({
+     x86_64: 'bcb13535f88f4985f3424ed9fa0cad07c97e2bec23d53e48e1b8fbdd5f9367fb'
+  })
 end
