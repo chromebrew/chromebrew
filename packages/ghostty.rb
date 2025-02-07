baafebd3235840bfc2b4ed821f43624fb6e0fb83
Satadru Pramanik <satadru@gmail.com>
2025-02-06 20:57:00 -0500

    Cleanup ghostty package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ghostty.rb
+++ packages/ghostty.rb
@@ -8,17 +8,31 @@ class Ghostty < Package
   homepage 'https://ghostty.org'
   version '1.1.0'
   license 'MIT'
-  compatibility 'x86_64 aarch64 armv7l'
+  compatibility 'x86_64'
   source_url "https://release.files.ghostty.org/#{version}/ghostty-source.tar.gz"
   source_sha256 '51dd34e68d3eac61beff8e12db0068790678778b81208bb4d626fa59e9ff171a'
   binary_compression 'tar.zst'
 
-  depends_on 'bzip2' # R
+  binary_sha256({
+     x86_64: '88693f453d735af7fe1932cd5ad827af2ac9f8308b1f1e2a16903f284655bbb4'
+  })
+
+  depends_on 'fontconfig' # R
+  depends_on 'freetype' # R
+  depends_on 'glibc' # R
+  depends_on 'glib' # R
+  depends_on 'gtk4' # R
+  depends_on 'harfbuzz' # R
   depends_on 'libadwaita' # R
   depends_on 'libx11' # R
-  depwnds_on 'onigurama' => :build
+  depends_on 'oniguruma' # R
   depends_on 'pandoc' => :build
+  depends_on 'wayland' # R
   depends_on 'zig' => :build
+
+  def self.patch
+   system "sed -i 's/linkSystemLibrary2(\"bzip2\", dynamic_link_opts)/linkSystemLibrary2(\"bz2\", dynamic_link_opts)/' src/build/SharedDeps.zig"
+  end
 
   def self.build
     @zig_global_cache_dir = `pwd`.chomp + '/zig_cache'
@@ -38,7 +52,6 @@ class Ghostty < Package
   end
 
   def self.install
-    FileUtils.mkdir_p CREW_DEST_PREFIX
-    FileUtils.mv Dir.glob('build/*'), "#{CREW_DEST_PREFIX}/"
+    FileUtils.mv Dir.glob('build/*'), "#{CREW_DEST_DIR}/"
   end
 end
