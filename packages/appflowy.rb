4dccfcb5c8964195748a70dd51d20712cc6432a1
Ed Reel <edreel@gmail.com>
2025-02-08 17:24:50 -0600

    Appflowy 0.8.2 => 0.8.3 (#11269)

--- packages/appflowy.rb
+++ packages/appflowy.rb
@@ -3,12 +3,12 @@ require 'package'
 class Appflowy < Package
   description 'AI collaborative workspace where you achieve more without losing control of your data. The best open source alternative to Notion.'
   homepage 'https://www.appflowy.io/'
-  version '0.8.2'
+  version '0.8.3'
   license 'AGPL-3.0'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/AppFlowy-#{version}-linux-x86_64.tar.gz"
-  source_sha256 '433fd88bfd8db5cd8f7651302098fd5f5c56e8ff2774a5fc593e1e9e479a12c0'
+  source_sha256 '00d5688cdb273e07930dba96ed54b8c4f1fed2ee2b8f8f5525f8e09020d5b293'
 
   depends_on 'gtk3'
   depends_on 'gobject_introspection'
