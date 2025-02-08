780bc9fd688580c41523d94391fb7d6fec3c30f8
Maximilian Downey Twiss <creatorsmithmdt@gmail.com>
2025-02-08 15:05:15 +1100

    liba52 0.7.4-2 -> 0.8.0, aircrack-ng 1.6-17f9 -> 1.7-b2985bf, apr 1.7.4-1 -> 1.7.5 (#11267)
    * liba52 0.7.4-2 -> 0.8.0
    
    * aircrack-ng 1.6-17f9 -> 1.7-b2985bf
    
    * apr 1.7.4-1 -> 1.7.5

--- packages/aircrack_ng.rb
+++ packages/aircrack_ng.rb
@@ -1,72 +1,36 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Aircrack_ng < Package
+class Aircrack_ng < Autotools
   description 'Key cracker for the 802.11 WEP and WPA-PSK protocols.'
   homepage 'https://www.aircrack-ng.org'
-  version '1.6-17f9'
+  version '1.7-b2985bf'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/aircrack-ng/aircrack-ng/archive/17f971cebdaeea0c86e338bde09a52787fcd6d9c.zip'
-  source_sha256 'd6dc9078f2ab56671f8fb707aa5ddbbec36d1a732cc7ad9a3d2803a5dbbaae5b'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/aircrack-ng/aircrack-ng.git'
+  git_hashtag 'b2985bf1a3ba6cd5842ceebae806ce4ba4441460'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5e48972faf181388c35a818ecfd5057b384752e82aab190376acbf838be7ee88',
-     armv7l: '5e48972faf181388c35a818ecfd5057b384752e82aab190376acbf838be7ee88',
-       i686: 'e3a7f24e822efc47f0402993ca482c5e4eae75cb49e6912eda44dcc52bbc2e81',
-     x86_64: '5c7cfe8e71c1dc3f5634027855ebee88d458a08f44bca3ed0a01388f0524524c'
+    aarch64: '4d566a77451e65d57cbe2f402556d3308902c778186bb2da6451de7d1e27b52a',
+     armv7l: '4d566a77451e65d57cbe2f402556d3308902c778186bb2da6451de7d1e27b52a',
+       i686: '0e7ac13f7e3054d06206c1518519ed86ba367853134995c245ca8ff3d6eca02c',
+     x86_64: '87fa96b73ec4d321b4d929b08b8ccd95e1609544c474e27bc48cbc71a7445af4'
   })
 
   depends_on 'libnl3'
   depends_on 'libpcap'
+  depends_on 'openssl' # R
+  depends_on 'pcre2' # R
   depends_on 'rfkill'
+  depends_on 'sqlite' # R
+  depends_on 'zlib' # R
 
-  def self.build
-    system 'NOCONFIGURE=1 ./autogen.sh'
-    # system "./configure --help"
-    # Could also add more specific optimization options:
-    # --with-static-simd[=x86-sse2|x86-avx|x86-avx2|x86-avx512|ppc-altivec|ppc-power8|arm-neon|arm-asimd],
-    # use specific SIMD implementation at static link, [default=none]
-    # --with-avx512[=yes|no]  use AVX-512F instruction set, [default=no]
-    system "env CFLAGS='-flto=auto' CXXFLAGS='-flto=auto' \
-      LDFLAGS='-flto=auto' \
-      ./configure \
-      #{CREW_CONFIGURE_OPTIONS} \
-      --program-prefix='' --program-suffix='' \
-      --with-lto \
-      --with-experimental=yes \
-      --with-openssl=#{CREW_PREFIX} \
-      sbindir=#{CREW_PREFIX}/sbin \
-      mandir=#{CREW_PREFIX}/share/man/man1 \
-      smandir=#{CREW_PREFIX}/share/man/man8"
-    system 'make'
+  # https://github.com/aircrack-ng/aircrack-ng/issues/773
+  def self.patch
+    system "sed -i 's|/etc/aircrack-ng|#{CREW_PREFIX}/aircrack-ng|' scripts/airodump-ng-oui-update"
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}",
-           'install'
-    # Fix for [*] Run 'airodump-ng-oui-update' as root (or with sudo) to install or update Airodump-ng OUI file (Internet connection required).
-    #
-    # Before fix:
-    # $ sudo airodump-ng-oui-update
-    # mkdir: cannot create directory '/etc/aircrack-ng': Read-only file system
-    # [*] Downloading IEEE OUI file...
-    # [*] Error: Failed to download OUI list, aborting...
-    #
-    # After fix:
-    # $ sudo airodump-ng-oui-update
-    # [*] Downloading IEEE OUI file...
-    # [*] Parsing OUI file...
-    # [*] Airodump-ng OUI file successfully updated
-    system "sed -i 's,/usr/local,,' #{CREW_DEST_PREFIX}/sbin/airodump-ng-oui-update"
-    system "sed -i 's,/etc,#{CREW_PREFIX}/etc,g' #{CREW_DEST_PREFIX}/sbin/airodump-ng-oui-update"
-    system "sed -i 's,/usr/share,#{CREW_PREFIX}/share,' #{CREW_DEST_PREFIX}/sbin/airodump-ng-oui-update"
-  end
-
-  def self.check
-    system 'make',
-           'sqlite=true',
-           'experimental=true',
-           'check'
-  end
+  # https://github.com/aircrack-ng/aircrack-ng/issues/2623
+  pre_configure_options 'CFLAGS=-Wno-error=implicit-function-declaration'
+  run_tests
 end
--- packages/liba52.rb
+++ packages/liba52.rb
@@ -1,38 +1,21 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Liba52 < Package
+class Liba52 < Autotools
   description 'liba52 is a free library for decoding ATSC A/52 streams.'
   homepage 'https://sourceforge.net/projects/liba52/'
-  @_ver = '0.7.4'
-  version "#{@_ver}-2"
+  version '0.8.0'
   compatibility 'all'
   license 'GPL-2+'
-  source_url 'https://salsa.debian.org/multimedia-team/a52dec.git'
-  git_hashtag "debian/#{@_ver}-20"
+  source_url 'https://git.adelielinux.org/community/a52dec.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '844f79a23cd1dde50dee917211786abdef130f7e952ec6b1c9c8208f8ac69abc',
-     armv7l: '844f79a23cd1dde50dee917211786abdef130f7e952ec6b1c9c8208f8ac69abc',
-       i686: '8fb90edfa222ac82c6b357152f5416a8e5e5d2faa9a904e0b7c531c5e8f7d04a',
-     x86_64: '01c64a8b207833ee58dc4482d4d96f5dff1261071ef6573e5817ce43e76a0789'
+    aarch64: 'a16c0ecfbf1b48be822e3957ef8b8de277cef06b034603bf1bc122f2922c7823',
+     armv7l: 'a16c0ecfbf1b48be822e3957ef8b8de277cef06b034603bf1bc122f2922c7823',
+       i686: '51c100dddd25d3befa5cbafc291b1900f855eb4e41017e4024ca861b1d762e33',
+     x86_64: '541e8d510e4b517df2aecd46d835f92c7ca223c171f227a89b443254aab66b33'
   })
 
-  def self.patch
-    system "for i in \$(cat debian/patches/series); do patch -Np1 -i debian/patches/\${i}; done"
-  end
-
-  def self.build
-    system './bootstrap'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  run_tests
 end
