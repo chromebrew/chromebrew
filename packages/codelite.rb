fd048a94dfa32ac31275b04e642236703aa22d51
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-02-08 07:37:47 -0500

    Add built packages for linux/amd64 to codelite

--- packages/codelite.rb
+++ packages/codelite.rb
@@ -13,11 +13,21 @@ class Codelite < CMake
   binary_sha256({
     aarch64: '3ce2928e3a9d72d9a46c90260a6d5b4396913dcdb02a7c8342acebeb5db5c9e0',
      armv7l: '3ce2928e3a9d72d9a46c90260a6d5b4396913dcdb02a7c8342acebeb5db5c9e0',
-     x86_64: '8a3cb05597521854499fd7f69287ed82a68853a11bda85621c16456b1dc1c081'
+     x86_64: '447f8d99e7e5875c3bd17c38a21e184b2b976f80fd9c903d128eb5123513b54f'
   })
 
   depends_on 'libedit'
   depends_on 'libssh'
   depends_on 'sqlite'
   depends_on 'wxwidgets'
+  depends_on 'at_spi2_core' # R
+  depends_on 'cairo' # R
+  depends_on 'harfbuzz' # R
+  depends_on 'gcc_lib' # R
+  depends_on 'gdk_pixbuf' # R
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
+  depends_on 'gtk3' # R
+  depends_on 'pango' # R
+  depends_on 'uchardet' # R
 end
