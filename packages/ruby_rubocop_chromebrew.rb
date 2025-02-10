17f83bd118f5ecce7e4d86265847b6671fec0810
Ed Reel <edreel@gmail.com>
2025-02-10 09:18:33 -0600

    Ruby_rubocop_chromebrew 0.0.2-ruby3.4 => 0.0.3-ruby3.4 (#11278)

--- packages/ruby_rubocop_chromebrew.rb
+++ packages/ruby_rubocop_chromebrew.rb
@@ -1,9 +1,9 @@
 require 'buildsystems/ruby'
 
 class Ruby_rubocop_chromebrew < RUBY
-  description "A RuboCop extension to enforce Chromebrew-specific practices."
+  description 'A RuboCop extension to enforce Chromebrew-specific practices.'
   homepage 'https://github.com/chromebrew/rubocop-chromebrew'
-  version "0.0.2-#{CREW_RUBY_VER}"
+  version "0.0.3-#{CREW_RUBY_VER}"
   license 'GPL-3.0-or-later'
   compatibility 'all'
   source_url 'SKIP'
