90c8e9b5af90b9128243d8d8cdac7aa846dd0232
Ed Reel <edreel@gmail.com>
2025-02-10 19:28:25 -0600

    Nushell 0.101.0 => 0.102.0 (#11293)

--- packages/nushell.rb
+++ packages/nushell.rb
@@ -3,7 +3,7 @@ require 'package'
 class Nushell < Package
   description 'A new type of shell'
   homepage 'https://www.nushell.sh/'
-  version '0.101.0'
+  version '0.102.0'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.28'
@@ -13,9 +13,9 @@ class Nushell < Package
      x86_64: "https://github.com/nushell/nushell/releases/download/#{version}/nu-#{version}-x86_64-unknown-linux-gnu.tar.gz"
   })
   source_sha256({
-    aarch64: '016052987cbe7aea796b12390c70786ed000f75089b93819aaa1bd61f60fddae',
-     armv7l: '016052987cbe7aea796b12390c70786ed000f75089b93819aaa1bd61f60fddae',
-     x86_64: '7149728c779b5d7e7f86e34f36fd31332f7677df3e9a47b5744a1e1756d3ce76'
+    aarch64: '9acf79ef550b0a351013f5140c2790145952f75eef922c0872a59d0293b07f19',
+     armv7l: '9acf79ef550b0a351013f5140c2790145952f75eef922c0872a59d0293b07f19',
+     x86_64: '8facc8575dc5cc1406d5f00625faf40556da986f8932e90c8f891241df7275a3'
   })
 
   no_compile_needed
