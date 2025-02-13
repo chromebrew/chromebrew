1d4cb57634422ed2a2a115ddebf5c8bc872bf750
uberhacker <uberhacker@users.noreply.github.com>
2025-02-13 17:16:28 -0500

    Add built packages for linux/amd64 to update-curl

--- packages/curl.rb
+++ packages/curl.rb
@@ -14,7 +14,7 @@ class Curl < Autotools
     aarch64: 'a2efe09834e9ad3bf04b47f2817f92b1ef5fce6c92af700b77ade324f8ce448b',
      armv7l: 'a2efe09834e9ad3bf04b47f2817f92b1ef5fce6c92af700b77ade324f8ce448b',
        i686: '9e8c806535e1afa86737f3f4ff4b7547c9a7b6d88d508e4fc672e70173541bfa',
-     x86_64: '2ac3615eec384ef6981ba103e337c033a28a95d6ade77c1fd6377fbe4bbc7341'
+     x86_64: '78107de10d329a4c0ea60b81ee0902568f5ef257208aee2cf780a12a167c6551'
   })
 
   depends_on 'brotli' # R
