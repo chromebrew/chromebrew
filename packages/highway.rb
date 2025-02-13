613bfd15f8354bf5b6d936d3c0d6e68b9834a3fc
satmandu <satmandu@users.noreply.github.com>
2025-02-12 19:44:06 -0500

    Add built packages for linux/386 to jxl

--- packages/highway.rb
+++ packages/highway.rb
@@ -16,7 +16,7 @@ class Highway < CMake
   binary_sha256({
     aarch64: 'ce08cdaa2edb56d438ddadd439dad0aad7cc81b75e79507970355a00d1299d08',
      armv7l: 'ce08cdaa2edb56d438ddadd439dad0aad7cc81b75e79507970355a00d1299d08',
-       i686: 'cb0048d50137ad17bd13aa5986cf90a1d073821cadbb29dde17d15db20fd7d90',
+       i686: '45b041fb1f9f636dee1bb76bb111cc3fbea307f52c6b7080cfc1cd433ba526f3',
      x86_64: 'aa0488630c6ddfbdbecdd43870568767f4bb99075f188285d4fb9d5af2caa6dd'
   })
 
