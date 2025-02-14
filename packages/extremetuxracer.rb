116538ac4a92f1c1b9e05ef94651296c770db268
Finn Baltazar <finnbaltazar11@gmail.com>
2025-02-14 10:15:50 -0800

    Update extremetuxracer.rb

--- packages/extremetuxracer.rb
+++ packages/extremetuxracer.rb
@@ -9,12 +9,12 @@ class Extremetuxracer < Package
   source_url ({
     "armv7l": "https://github.com/FinnBaltazar1111/extremetuxracer/releases/download/0.8.4/extremetuxracer_#{version}_armhf.deb",
     "x86_64": "https://github.com/FinnBaltazar1111/extremetuxracer/releases/download/0.8.4/extremetuxracer_#{version}_amd64.deb",
-    "aarch64": "https://github.com/FinnBaltazar1111/extremetuxracer/releases/download/0.8.4/extremetuxracer_#{version}_arm64.deb"
+    "aarch64": "https://github.com/FinnBaltazar1111/extremetuxracer/releases/download/0.8.4/extremetuxracer_#{version}_armhf.deb"
   })
   source_sha256 ({
     "armv7l": "47e14334f142d6adc2094d5b9304b0c9129c2fa94e25765ad2c53523ac635202",
     "x86_64": "d081c1b4fcc8f4586f734c9c9f4c23a5f00909eda2596729d141328a637b99ea",
-    "aarch64": "05528ec0e802a57f2c965ba392c98d7a601dfce9c02be84d3da069ad8c5fdd42"
+    "aarch64": "47e14334f142d6adc2094d5b9304b0c9129c2fa94e25765ad2c53523ac635202"
   })
   
   depends_on 'libsdl2'
