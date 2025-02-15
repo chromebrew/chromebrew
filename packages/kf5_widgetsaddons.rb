43d4d6adc5723396417f1ea67bf71594b66c3e3b
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-02-15 00:51:11 -0500

    Add built packages for linux/amd64 to kf5_widgetsaddons

--- packages/kf5_widgetsaddons.rb
+++ packages/kf5_widgetsaddons.rb
@@ -13,12 +13,14 @@ class Kf5_widgetsaddons < CMake
   binary_sha256({
     aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
      armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+     x86_64: 'e3ea2dd36694fd82f5b2b3e9d6fb0321fed11db795b9b4d8f356fdf26e79ef7b'
   })
 
   depends_on 'extra_cmake_modules'
   depends_on 'qt5_base'
   depends_on 'qt5_tools'
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
 
   cmake_options "-DECM_MKSPECS_INSTALL_DIR=#{CREW_PREFIX}/share/qt5/mkspecs/modules"
 end
