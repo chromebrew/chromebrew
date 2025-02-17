44382e509f7dc0c152aa9cb4060000ecca27fdea
Ed Reel <edreel@gmail.com>
2025-02-16 22:48:28 -0600

    Bazel 8.0.1 => 8.1.0 (#11341)

--- packages/bazel.rb
+++ packages/bazel.rb
@@ -3,11 +3,11 @@ require 'package'
 class Bazel < Package
   description 'a fast, scalable, multi-language and extensible build system'
   homepage 'https://bazel.build/'
-  version '8.0.1'
+  version '8.1.0'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/bazelbuild/bazel/releases/download/#{version}/bazel-#{version}-linux-x86_64"
-  source_sha256 '40f243b118f46d1c88842315e78ec5f9f6390980d67a90f7b64098613e60d65b'
+  source_sha256 'b4bae524f58e00a69f7c6fa10e62a91f85bfee586105dd480dccb4300c7cbca5'
 
   no_compile_needed
   no_shrink
