9303305eaaab396aebb000b6d69ef19f319e046a
Ed Reel <edreel@gmail.com>
2025-02-17 03:29:52 +0000

    Act 0.2.72 => 0.2.74

--- packages/act.rb
+++ packages/act.rb
@@ -3,7 +3,7 @@ require 'package'
 class Act < Package
   description 'Run your GitHub Actions locally'
   homepage 'https://github.com/nektos/act'
-  version '0.2.72'
+  version '0.2.74'
   license 'MIT'
   compatibility 'all'
   source_url({
@@ -13,13 +13,14 @@ class Act < Package
      x86_64: "https://github.com/nektos/act/releases/download/v#{version}/act_Linux_x86_64.tar.gz"
   })
   source_sha256({
-    aarch64: 'c4958be9b6d9f9d009efd7cfaeffef5958db4276def0b47b3ffb63295cabac31',
-     armv7l: 'c4958be9b6d9f9d009efd7cfaeffef5958db4276def0b47b3ffb63295cabac31',
-       i686: '1d90876d166cd78bcd0f4f563efa2ea2d1ab561535f0e6025579f8e6e4138bc4',
-     x86_64: '8314db77371828dea9a517d5a64fecc541fd74e227c1cf399756f27e6f54c75b'
+    aarch64: 'a303564e1af2ae49d43808910db6bf8bc128a7d2e70e359ca14825fa2960454d',
+     armv7l: 'a303564e1af2ae49d43808910db6bf8bc128a7d2e70e359ca14825fa2960454d',
+       i686: 'f496c10f5f64bb46af8c07907316b22d0e0eb8de2cd5033b2abb6e23818d7513',
+     x86_64: '7512b40671e073a011470e216eb2256d1b3ae37b5970d53dc23b29375ffb90f6'
   })
 
   no_compile_needed
+  no_shrink
 
   def self.install
     FileUtils.install 'act', "#{CREW_DEST_PREFIX}/bin/act", mode: 0o755
