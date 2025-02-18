4abfcc2abab5ce0cd7284f348935c624c530a5d2
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-02-17 19:01:38 -0500

    Add built packages for linux/arm/v7 to plasma_wayland_protocols

--- packages/plasma_wayland_protocols.rb
+++ packages/plasma_wayland_protocols.rb
@@ -11,8 +11,8 @@ class Plasma_wayland_protocols < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    aarch64: '5ecac893396a61340ca5ee30709101fe7484ef8fe0355ab103ed23a78e081473',
+     armv7l: '5ecac893396a61340ca5ee30709101fe7484ef8fe0355ab103ed23a78e081473',
      x86_64: '3e252e1a34cd4685a451cbea7e1e0af24aee16cecb1e3e5bd566a5d0db4ee5f1'
   })
 
