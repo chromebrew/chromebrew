c27a4d393fef952916a16d0384e4a73fc742e64d
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-02-18 06:02:59 -0500

    Add built packages for linux/amd64 to kf5_i18n

--- packages/kf5_i18n.rb
+++ packages/kf5_i18n.rb
@@ -13,12 +13,14 @@ class Kf5_i18n < CMake
   binary_sha256({
     aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
      armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+     x86_64: 'eeefaa742dc51e480aa26cd2771a19ab153a1c36a9556653dfa6ab812a345bbe'
   })
 
   depends_on 'extra_cmake_modules'
   depends_on 'qt5_base'
   depends_on 'qt5_declarative'
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
 
   cmake_options "-DECM_MKSPECS_INSTALL_DIR=#{CREW_PREFIX}/share/qt5/mkspecs/modules"
 end
