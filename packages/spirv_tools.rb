d6aaa2bc1bad829fb0d8ee829333d95b332ca928
Satadru Pramanik <satadru@gmail.com>
2025-02-19 14:12:04 -0500

    Add i686 binaries for spirv_tools
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/spirv_tools.rb
+++ packages/spirv_tools.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Spirv_tools < CMake
   homepage 'https://github.com/KhronosGroup/SPIRV-Tools'
   description 'API and commands for processing SPIR-V modules'
-  version '2024.3-1'
+  version '2024.4.rc2'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'https://github.com/KhronosGroup/SPIRV-Tools.git'
@@ -13,7 +13,7 @@ class Spirv_tools < CMake
   binary_sha256({
     aarch64: '52a58053473626c4d8e6adbb3d58e353ea8468c63d4b481ec827dd8905a006e4',
      armv7l: '52a58053473626c4d8e6adbb3d58e353ea8468c63d4b481ec827dd8905a006e4',
-       i686: 'ccb2da56af36c69d66548acc33d5d13cb816e9698fbfa8d69ba1e853e1e4da16',
+       i686: '78e150f81274d2476b562c5f78fb6e3242919d296f56a1f6a0ba43487cc48030',
      x86_64: '21eafdec332b855639c05586fffa18f6719ff3ae63573e86a659a72799ae8887'
   })
 
