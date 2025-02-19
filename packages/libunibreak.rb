ed7efbb39f577bc90045c4d2aa8c10fe8a2a81a8
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-02-19 18:40:40 -0500

    Add built packages for linux/arm/v7 to libunibreak

--- packages/libunibreak.rb
+++ packages/libunibreak.rb
@@ -11,8 +11,8 @@ class Libunibreak < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    aarch64: '8ef735b3e2d32847bec5f5efeb4262fc13b3fea13675413b788be9eda6982368',
+     armv7l: '8ef735b3e2d32847bec5f5efeb4262fc13b3fea13675413b788be9eda6982368',
        i686: '4117eedf5a1ce99d64c232c557ea08761caff4efaf43e910e47a5efe1f3874e7',
      x86_64: 'ec19ac9a29328534d8f32bd4d88b4cb11c5bea9e72f9623e46fafb4221097f11'
   })
