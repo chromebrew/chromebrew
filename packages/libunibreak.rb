eaadb435b4020580ad4fa7ad6c93569cde819268
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-02-19 18:39:33 -0500

    Add built packages for linux/amd64 to libunibreak

--- packages/libunibreak.rb
+++ packages/libunibreak.rb
@@ -14,7 +14,7 @@ class Libunibreak < Autotools
     aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
      armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
        i686: '4117eedf5a1ce99d64c232c557ea08761caff4efaf43e910e47a5efe1f3874e7',
-     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+     x86_64: 'ec19ac9a29328534d8f32bd4d88b4cb11c5bea9e72f9623e46fafb4221097f11'
   })
 
   depends_on 'glibc' # R
