c7451dda22c0e2cd44ef5098487565baa77e4f61
satmandu <satmandu@users.noreply.github.com>
2025-02-20 13:30:19 -0500

    Add built packages for linux/arm/v7 to zstd

--- packages/zstd.rb
+++ packages/zstd.rb
@@ -12,8 +12,8 @@ class Zstd < CMake
   binary_compression 'tar.xz'
 
   binary_sha256({
-    aarch64: 'fcef09c11c9fa7ce65b4c81fe3622343311bae1ea3e79d0efbd109e6d154174e',
-     armv7l: 'fcef09c11c9fa7ce65b4c81fe3622343311bae1ea3e79d0efbd109e6d154174e',
+    aarch64: 'afacaaf12e074d05c00561b63cda1e78c57a18e727f9338b96db7661176b98cb',
+     armv7l: 'afacaaf12e074d05c00561b63cda1e78c57a18e727f9338b96db7661176b98cb',
        i686: '1877238675dacf9f79d22f1093825759ea9d5921b7648a44f26b0a05d87618bf',
      x86_64: 'ec080cde6474de45b19c68f7cde4ae651b93e2a18bf8c2d452bb8eb50aeed34f'
   })
