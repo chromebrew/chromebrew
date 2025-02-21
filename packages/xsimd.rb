48a98a88009afd7d84d5282ed363a99031bd1dd8
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-02-20 19:48:21 -0500

    Add built packages for linux/arm/v7 to xsimd

--- packages/xsimd.rb
+++ packages/xsimd.rb
@@ -11,8 +11,8 @@ class Xsimd < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    aarch64: 'fa3854ffa913efa3bb6169ab5dfc78f84371a33fd95a55c71e9c2ce4990cdc41',
+     armv7l: 'fa3854ffa913efa3bb6169ab5dfc78f84371a33fd95a55c71e9c2ce4990cdc41',
        i686: '0dac1ca2d7465f24777d0d72b98f028f1a783b35a9b51ff33ca3a1b89279d06e',
      x86_64: 'ffe5b730f73df8e5fcac970ac2c55e7e6697480197ef9505f0b0b63d58ac0270'
   })
