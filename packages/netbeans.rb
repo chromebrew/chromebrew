497b6a500a24ce1bcea0190d5f7fe90521811830
Ed Reel <edreel@gmail.com>
2025-02-20 19:30:30 -0600

    Netbeans 24 => 25

--- packages/netbeans.rb
+++ packages/netbeans.rb
@@ -3,7 +3,7 @@ require 'package'
 class Netbeans < Package
   description 'Apache NetBeans IDE is free open source software to quickly and easily develop desktop, mobile, and web applications with Java, JavaScript, HTML5, PHP, C/C++ and more.'
   homepage 'https://netbeans.apache.org/front/main/index.html'
-  version '24'
+  version '25'
   license 'Apache-2.0, custom, EPL-1.0, W3C, MIT, BSD, MPL-1.0, CC-BY-3.0, CDDL and CDDL-1.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://dlcdn.apache.org/netbeans/netbeans/#{version}/netbeans-#{version}-bin.zip"
