e00c548ec6885c97bb76a29b47732dc34d0b7a54
satmandu <satmandu@users.noreply.github.com>
2025-02-24 14:33:06 -0500

    Add built packages for linux/arm/v7 to upx

--- packages/upx.rb
+++ packages/upx.rb
@@ -14,8 +14,8 @@ class Upx < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd33d8bf41db6e536dc9302b62bbeb47233ab6428f5c759d4c055612aaf7551b1',
-     armv7l: 'd33d8bf41db6e536dc9302b62bbeb47233ab6428f5c759d4c055612aaf7551b1',
+    aarch64: '5bdebb37916fc9b21ea0d075d8467d9b9e415ed84ef5e52214f1da93d407c459',
+     armv7l: '5bdebb37916fc9b21ea0d075d8467d9b9e415ed84ef5e52214f1da93d407c459',
        i686: 'b2be0ce308cd928c01afc387384f151ecc9835e7cbd8e513baa73b0d0cad0758',
      x86_64: '015f51b52d826208baaa029f58a0c98de722311989df83a7f322f8e4da3242cf'
   })
