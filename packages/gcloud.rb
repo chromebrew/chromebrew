45fc677b2dbc48c301b079491829bbf858f6955a
Ed Reel <edreel@gmail.com>
2025-02-25 16:54:18 -0600

    Gcloud 511.0.0 => 512.0.0

--- packages/gcloud.rb
+++ packages/gcloud.rb
@@ -3,7 +3,7 @@ require 'package'
 class Gcloud < Package
   description 'Command-line interface for Google Cloud Platform products and services'
   homepage 'https://cloud.google.com/sdk/gcloud/'
-  version '511.0.0'
+  version '512.0.0'
   license 'Apache-2.0'
   compatibility 'all'
   source_url({
