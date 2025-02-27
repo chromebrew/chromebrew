0bf1e9c1221263bb8f058656b30aea033e9ecf02
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-02-27 00:19:52 +0000

    Add unbuilt updated pip packages to updates-2025-02-27-00-19

--- packages/py3_setuptools.rb
+++ packages/py3_setuptools.rb
@@ -4,7 +4,7 @@ require 'ptools'
 class Py3_setuptools < Python
   description 'Setuptools is the python build system from the Python Packaging Authority.'
   homepage 'https://setuptools.readthedocs.io/'
-  version "75.8.1-#{CREW_PY_VER}"
+  version "75.8.2-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/pypa/setuptools.git'
