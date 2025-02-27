fc716eb7c4ec70cb4680dff753194e40ad7c4f13
Ed Reel <edreel@gmail.com>
2025-02-27 09:52:11 +0000

    Gettext 0.22.5-icu75.1 => 0.24-icu75.1

--- packages/gettext.rb
+++ packages/gettext.rb
@@ -3,10 +3,10 @@ require 'buildsystems/autotools'
 class Gettext < Autotools
   description 'GNU gettext utilities are a set of tools that provides a framework to help other GNU packages produce multi-lingual messages.'
   homepage 'https://www.gnu.org/software/gettext/'
-  version "0.22.5-#{CREW_ICU_VER}"
+  version "0.24-#{CREW_ICU_VER}"
   license 'GPL-3+ and LGPL-2.1+'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gettext/gettext-0.22.5.tar.lz'
+  source_url "https://ftpmirror.gnu.org/gettext/gettext-#{version.split('-')[0]}.tar.lz"
   source_sha256 'caa44aed29c9b4900f1a401d68f6599a328a3744569484dc95f62081e80ad6cb'
   binary_compression 'tar.zst'
 
@@ -27,8 +27,6 @@ class Gettext < Autotools
   depends_on 'ncurses' # R
   depends_on 'openjdk8' => :build
   depends_on 'zlib' # R
-
-  no_env_options
 
   configure_options '--disable-static \
     --enable-shared \
