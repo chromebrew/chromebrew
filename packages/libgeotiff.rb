a037ca25065408f7fb3de5593ba7bbd5c2b8df3e
Ed Reel <edreel@gmail.com>
2025-03-03 01:51:53 -0600

    Libgeotiff 1.7.1-1 => 1.7.4

--- packages/libgeotiff.rb
+++ packages/libgeotiff.rb
@@ -1,39 +1,29 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libgeotiff < Package
+class Libgeotiff < Autotools
   description 'GeoTIFF is based on the TIFF format and is used as an interchange format for georeferenced raster imagery.'
   homepage 'https://github.com/OSGeo/libgeotiff'
-  version '1.7.1-1'
-  license 'GPL-2'
-  compatibility 'all'
-  source_url 'https://github.com/OSGeo/libgeotiff/releases/download/1.7.1/libgeotiff-1.7.1.tar.gz'
-  source_sha256 '05ab1347aaa471fc97347d8d4269ff0c00f30fa666d956baba37948ec87e55d6'
+  version '1.7.4'
+  license 'MIT'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url "https://github.com/OSGeo/libgeotiff/releases/download/#{version}/libgeotiff-#{version}.tar.gz"
+  source_sha256 'c598d04fdf2ba25c4352844dafa81dde3f7fd968daa7ad131228cd91e9d3dc47'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '37c527b38436f6313b6be20ed176d7f8be509f2dd2cb9e2fadcba16823b17331',
-     armv7l: '37c527b38436f6313b6be20ed176d7f8be509f2dd2cb9e2fadcba16823b17331',
-       i686: 'dabea0e5c4f43acabdd1de7b6c46f883a2c2734a0fefe502dd13c3bb4ee2d32e',
-     x86_64: '14c3b9e715d8fb79247f8fb17b93a843219f4e064b6b71115a4d57f2ff81ed6b'
+    aarch64: 'd2f582b4ee393dc0eca53805098b354efc68a766a32b2277fad26033a4bc1258',
+     armv7l: 'd2f582b4ee393dc0eca53805098b354efc68a766a32b2277fad26033a4bc1258',
+     x86_64: '622a92ff1bb9711422ba33d37a7635245adc0c1ba5f60cfb7a3aa7b150956682'
   })
 
-  depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
+  depends_on 'jbigkit' # R
+  depends_on 'libdeflate' # R
+  depends_on 'libjpeg_turbo' # R
   depends_on 'libtiff' # R
+  depends_on 'libwebp' # R
   depends_on 'proj' # R
-
-  def self.build
-    system "cmake -B builddir -G Ninja \
-        #{CREW_CMAKE_OPTIONS} \
-        -DGEOTIFF_LIB_SUBDIR=#{CREW_LIB_PREFIX} \
-        -DCMAKE_INSTALL_DOCDIR=#{CREW_PREFIX}/share/doc \
-        -DBUILD_SHARED_LIBS=true"
-    system 'ninja -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
-    FileUtils.mv "#{CREW_DEST_PREFIX}/doc", "#{CREW_DEST_PREFIX}/share/"
-  end
+  depends_on 'xzutils' # R
+  depends_on 'zlib' # R
+  depends_on 'zstd' # R
 end
