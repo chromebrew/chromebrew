3f1709a4281d3ce7a80df44939fe1e5d44e5b6e1
Ed Reel <edreel@gmail.com>
2025-03-05 15:43:45 -0600

    Github_cli 2.67.0 => 2.68.0 (#11476)

--- packages/github_cli.rb
+++ packages/github_cli.rb
@@ -3,7 +3,7 @@ require 'package'
 class Github_cli < Package
   description 'Official Github CLI tool'
   homepage 'https://cli.github.com/'
-  version '2.67.0'
+  version '2.68.0'
   license 'MIT'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Github_cli < Package
      x86_64: "https://github.com/cli/cli/releases/download/v#{version}/gh_#{version}_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: 'a0d6aaaf85f7cc9ce1ed4eaacee6a5ae942bcb9772a966cc88c32ac74d625198',
-     armv7l: 'a0d6aaaf85f7cc9ce1ed4eaacee6a5ae942bcb9772a966cc88c32ac74d625198',
-       i686: '86ac88c2681cbb7d72521ccbb1316c88760201d77b34209256c2b45c485f3d1b',
-     x86_64: 'd77623479bec017ef8eebadfefc785bafd4658343b3eb6d3f3e26fd5e11368d5'
+    aarch64: '6dfd3ed341b1ea08fcff55436ee50bc1c878b8c5fda9a1254e8653da9bd15c65',
+     armv7l: '6dfd3ed341b1ea08fcff55436ee50bc1c878b8c5fda9a1254e8653da9bd15c65',
+       i686: '6758f00fd2c04771ab9d1c3c1e18cad6eaafe10c09b85a7744d9e7b53c1d372e',
+     x86_64: 'dcd944ecd9905b62fbaf3fe3703af7d6f9a33bc8c36d8603af55cab0d3f67879'
   })
 
   no_compile_needed
