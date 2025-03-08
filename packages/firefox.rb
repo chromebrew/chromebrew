8ee500818da08690742a2c4b3a9fe0288e337f6c
Ed Reel <edreel@gmail.com>
2025-03-08 03:22:41 -0600

    Firefox 135.0.1 => 136.0 (#11498)

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,12 +3,12 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '135.0.1'
+  version '136.0'
   license 'MPL-2.0, GPL-2 and LGPL-2.1'
   compatibility 'x86_64'
   min_glibc '2.35'
   source_url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.xz"
-  source_sha256 '4f76c45196cd4d7cacbdc9038d80757cc9bbe387dd43e2b81bd8a7748d630ee8'
+  source_sha256 '5222f51caacfccaf0f0cf795117f0ad37422fe9d413ef18f2c171e1622b9455a'
 
   depends_on 'at_spi2_core'
   depends_on 'cairo'
