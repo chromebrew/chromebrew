ea693f5fe9f4b4c8f32c6becd5f2b71ea9a37323
Ed Reel <edreel@gmail.com>
2025-03-10 03:14:06 +0000

    Add py3_markitdown package

---
+++ packages/py3_markitdown.rb
@@ -0,0 +1,22 @@
+require 'buildsystems/pip'
+
+class Py3_markitdown < Pip
+  description 'Python tool for converting files and office documents to Markdown.'
+  homepage 'https://github.com/microsoft/markitdown'
+  version "0.0.2-#{CREW_PY_VER}"
+  license 'MIT'
+  compatibility 'all'
+  source_url 'SKIP'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '36cd69c61ae1817edeec239eab4b3c4acc2359ac252f8e74374e3ec1fc55bd25',
+     armv7l: '36cd69c61ae1817edeec239eab4b3c4acc2359ac252f8e74374e3ec1fc55bd25',
+       i686: '0c4d7a3092775e375e5578c0d54d1fc9255ee772a99806f73c7467df7e2470fa',
+     x86_64: 'e989f3043f9d7a873a42be0bdd5071c23b8297091e2a2de0c202242ba8aa9cbc'
+  })
+
+  depends_on 'python3' => :build
+
+  no_source_build
+end
