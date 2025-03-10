e8704d84ae0e1f8c5aa6249632a2983cd2462b2c
uberhacker <uberhacker@users.noreply.github.com>
2025-03-09 23:20:12 -0400

    Add built packages for linux/amd64 to add-py3_markitdown-package

--- packages/py3_markitdown.rb
+++ packages/py3_markitdown.rb
@@ -13,7 +13,7 @@ class Py3_markitdown < Pip
     aarch64: '36cd69c61ae1817edeec239eab4b3c4acc2359ac252f8e74374e3ec1fc55bd25',
      armv7l: '36cd69c61ae1817edeec239eab4b3c4acc2359ac252f8e74374e3ec1fc55bd25',
        i686: 'd85b462ed2b344bcb61b6bfa634d778d7f4937096a118bacdcd60aa2e52a0f13',
-     x86_64: 'e989f3043f9d7a873a42be0bdd5071c23b8297091e2a2de0c202242ba8aa9cbc'
+     x86_64: 'fdcfba7371bc75cddcd3e950db3543f7dc1a0e3ee628e5e360dbdbe5bdb4d985'
   })
 
   depends_on 'python3' => :build
