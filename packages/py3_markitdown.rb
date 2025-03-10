4ed7e3afab8cc6c674c48600fcf4d68a0552b2cd
uberhacker <uberhacker@users.noreply.github.com>
2025-03-09 23:23:04 -0400

    Add built packages for linux/arm/v7 to add-py3_markitdown-package

--- packages/py3_markitdown.rb
+++ packages/py3_markitdown.rb
@@ -10,8 +10,8 @@ class Py3_markitdown < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '36cd69c61ae1817edeec239eab4b3c4acc2359ac252f8e74374e3ec1fc55bd25',
-     armv7l: '36cd69c61ae1817edeec239eab4b3c4acc2359ac252f8e74374e3ec1fc55bd25',
+    aarch64: '471672cfc197ace5130edf70633b3e4fded9f0eed8900e1a9b170e146891cecf',
+     armv7l: '471672cfc197ace5130edf70633b3e4fded9f0eed8900e1a9b170e146891cecf',
        i686: 'd85b462ed2b344bcb61b6bfa634d778d7f4937096a118bacdcd60aa2e52a0f13',
      x86_64: 'fdcfba7371bc75cddcd3e950db3543f7dc1a0e3ee628e5e360dbdbe5bdb4d985'
   })
