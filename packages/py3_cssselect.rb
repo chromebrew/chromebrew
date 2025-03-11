420711024abeb220311e9c651286b2acdc18adf1
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-03-11 00:20:22 +0000

    Add unbuilt updated pip packages to updates-2025-03-11-00-19

--- packages/py3_cssselect.rb
+++ packages/py3_cssselect.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_cssselect < Pip
   description 'CSSselect parses CSS3 Selectors and translates them to XPath 1.0.'
   homepage 'https://cssselect.readthedocs.io/'
-  version "1.2.0-#{CREW_PY_VER}"
+  version "1.3.0-#{CREW_PY_VER}"
   license 'BSD'
   compatibility 'all'
   source_url 'SKIP'
