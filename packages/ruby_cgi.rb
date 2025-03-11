2f54bea158dc78b5f00eaf06177e346ba5246162
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-03-11 00:20:30 +0000

    Add unbuilt updated ruby gem packages to updates-2025-03-11-00-19

--- packages/ruby_cgi.rb
+++ packages/ruby_cgi.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_cgi < RUBY
   description 'Support for the common gateway interface protocol.'
   homepage 'https://github.com/ruby/cgi'
-  version "0.4.1-#{CREW_RUBY_VER}"
+  version "0.4.2-#{CREW_RUBY_VER}"
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
--- packages/ruby_uri.rb
+++ packages/ruby_uri.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_uri < RUBY
   description 'Uri is a module providing classes to handle uniform resource identifiers.'
   homepage 'https://github.com/ruby/uri'
-  version "1.0.2-#{CREW_RUBY_VER}"
+  version "1.0.3-#{CREW_RUBY_VER}"
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
