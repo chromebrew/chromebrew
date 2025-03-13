1fda7fc5a7d11818612be5b14afce9adaf671dcc
Ed Reel <edreel@gmail.com>
2025-03-13 13:54:54 -0500

    Cpu_x 5.0.4 => 5.1.3 (#11531)

--- packages/cpu_x.rb
+++ packages/cpu_x.rb
@@ -3,13 +3,14 @@ require 'package'
 class Cpu_x < Package
   description 'CPU-X is a Free software that gathers information on CPU, motherboard and more.'
   homepage 'https://thetumultuousunicornofdarkness.github.io/CPU-X/'
-  version '5.0.4'
+  version '5.1.3'
   license 'GPL-3'
   compatibility 'x86_64'
   min_glibc '2.29'
-  source_url 'https://github.com/TheTumultuousUnicornOfDarkness/CPU-X/releases/download/v5.0.4/CPU-X-5.0.4-x86_64.AppImage'
-  source_sha256 '40894470a38c86011e607c5db8492ca9a7906daf7e3c7e6ba7f873c7acc8f777'
+  source_url "https://github.com/TheTumultuousUnicornOfDarkness/CPU-X/releases/download/v#{version}/CPU-X-#{version}-x86_64.AppImage"
+  source_sha256 'ddcd482438f58f97a76d7d4e511bffdef34c0feb3de8b1f3756405004d067835'
 
+  gnome
   no_compile_needed
 
   depends_on 'gtk3'
@@ -18,6 +19,7 @@ class Cpu_x < Package
   def self.build
     File.write 'cpu-x.sh', <<~EOF
       #!/bin/bash
+      export LD_LIBRARY_PATH="#{CREW_PREFIX}/share/cpu-x/usr/lib:$LD_LIBRARY_PATH"
       cd #{CREW_PREFIX}/share/cpu-x
       ./AppRun "$@"
     EOF
@@ -31,6 +33,6 @@ class Cpu_x < Package
   end
 
   def self.postinstall
-    ExitMessage.add "\nType 'cpu-x' to get started.\n".lightblue
+    ExitMessage.add "\nType 'cpu-x' to get started.\n"
   end
 end
