37bf1da8141ab6f31b03a332acdf81603e9a7df1
Satadru Pramanik <satadru@gmail.com>
2025-03-14 18:36:03 -0400

    Add zlib_ng
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/zlib_ng.rb
@@ -0,0 +1,20 @@
+# Adapted from Arch Linux zlib-ng PKGBUILD at:
+# https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=zlib-ng
+
+require 'buildsystems/cmake'
+
+class Zlib_ng < CMake
+  description 'zlib replacement with optimizations for next generation systems'
+  homepage 'https://github.com/zlib-ng/zlib-ng'
+  version '2.2.4'
+  license 'Zlib'
+  compatibility 'all'
+  source_url 'https://github.com/zlib-ng/zlib-ng.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
+
+  depends_on 'glibc'
+
+  cmake_options '-DWITH_GTEST=OFF
+    -DBUILD_SHARED_LIBS=ON'
+end
