09d06d25ebd3b4c855a507e3d3ae69b46c1a1937
Ed Reel <edreel@gmail.com>
2025-03-14 12:16:13 -0500

    Edge 134.0.3124.51-1 => 134.0.3124.68-1 (#11538)

--- packages/edge.rb
+++ packages/edge.rb
@@ -4,12 +4,12 @@ require 'convenience_functions'
 class Edge < Package
   description 'Microsoft Edge is the fast and secure browser'
   homepage 'https://www.microsoft.com/en-us/edge'
-  version '134.0.3124.51-1'
+  version '134.0.3124.68-1'
   license 'MIT'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-stable/microsoft-edge-stable_#{version}_amd64.deb"
-  source_sha256 '8bb0ba438b820599c8c9dd8b03e2b0b39fd6c4765aa33fd4c6037d02d642b6ea'
+  source_sha256 'ce337cb2c55e477cb2c3e7bab55cdcaeb6242d3a28f119a6e068dc1bd8903299'
 
   depends_on 'at_spi2_core'
   depends_on 'libcom_err'
