eb6d3d03000fca28a121851150e9c40d7670e00f
satmandu <satmandu@users.noreply.github.com>
2025-03-14 18:53:38 -0400

    Add built packages for linux/arm/v7 to git

--- packages/zlib_ng.rb
+++ packages/zlib_ng.rb
@@ -14,6 +14,8 @@ class Zlib_ng < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
+    aarch64: '6f70e2c3a18e809cfc0bd92059170e9ae938416847e2c91b2549800fc6e2c387',
+     armv7l: '6f70e2c3a18e809cfc0bd92059170e9ae938416847e2c91b2549800fc6e2c387',
        i686: '67cc7fbd92642298fc6bca2a20c50f648095a42482b5ebe7a0e02657a238a317',
      x86_64: 'baf51c97c8244097c847dcd5645d21feb65631b20e1cc3fb70c24455bacaf0c3'
   })
