c1597bc82ca7a61b6aca03a1b8dc412ec3d3cc49
Finn Baltazar <finnbaltazar11@gmail.com>
2025-03-14 18:11:43 -0700

    Update fceux.rb

--- packages/fceux.rb
+++ packages/fceux.rb
@@ -7,7 +7,7 @@ class Fceux < Package
   compatibility 'x86_64'
   license 'GPLv2'
   source_url "https://github.com/FinnBaltazar1111/fceux/releases/download/v#{version}/fceux-#{version}-amd64.deb"
-  source_sha256 "7b85f7682ac89a148b8c2a43dd4b8c71e8effd719a3d8f251f119e56ba63052f"
+  source_sha256 "7a740608aad4157aec6010cea29aecc30f5e7341667375b49bc38df770c5ca49"
   
   depends_on 'ffmpeg'
   depends_on 'libarchive'
@@ -26,6 +26,6 @@ class Fceux < Package
   end
 
   def self.postinstall
-    ExitMessage.add "\nInstallation complete. FCEUX executable is located in \"#{CREW_PREFIX}/bin/fceux\".\n"
+    ExitMessage.add "\nFCEUX installation complete. Run \"fceux\" command (located in #{CREW_PREFIX}/fceux) to use.\n"
   end
 end
