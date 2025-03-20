38351b2b5654d748b4299ccecb8f9d15a2c23e5f
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-03-20 00:20:25 +0000

    Add unbuilt updated ruby gem packages to updates-2025-03-20-00-19

--- packages/ruby_ast.rb
+++ packages/ruby_ast.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_ast < RUBY
   description 'A library for working with Abstract Syntax Trees.'
   homepage 'https://whitequark.github.io/ast/'
-  version "2.4.2-#{CREW_RUBY_VER}"
+  version "2.4.3-#{CREW_RUBY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
