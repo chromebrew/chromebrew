bf161543e009814f5666c65cea3b180f03e02230
Satadru Pramanik <satadru@gmail.com>
2025-03-25 18:02:05 -0400

    mpv => 0.40.0
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/mpv.rb
+++ packages/mpv.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Mpv < Meson
   description 'Video player based on MPlayer/mplayer2'
   homepage 'https://mpv.io/'
-  version '0.39.0'
+  version '0.40.0'
   license 'LGPL-2.1+, GPL-2+, BSD, ISC and GPL-3+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/mpv-player/mpv.git'
