5da98bba009b18ef861e7eb9b9767dadd1f82084
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-03-26 03:01:37 -0400

    Add built packages for linux/arm/v7 to csvkit

--- packages/csvkit.rb
+++ packages/csvkit.rb
@@ -10,6 +10,8 @@ class Csvkit < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
+    aarch64: '237442c5f24223eeaee2c8d93d706b47880ab9506038cef0c40158fc59b5b486',
+     armv7l: '237442c5f24223eeaee2c8d93d706b47880ab9506038cef0c40158fc59b5b486',
        i686: '294dc0cc7ee96c87d137e2afd8219d3a10c6d9cc667c36adfe8ce67a7ef70ce8',
      x86_64: '1f2311ce413c14a19152d26b5a66baaa3ef3c8f3526a1b81337709f68c7b1c78'
   })
