7025ad4cbe51da823a6644577d0062c91f9f3446
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-03-26 01:23:16 -0400

    Add built packages for linux/arm/v7 to firejail

--- packages/firejail.rb
+++ packages/firejail.rb
@@ -12,8 +12,8 @@ class Firejail < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    aarch64: '4f4546c5e596cf3f7cddcf4e5457876acbd6526d951d744dfd1d4b07cf06534e',
+     armv7l: '4f4546c5e596cf3f7cddcf4e5457876acbd6526d951d744dfd1d4b07cf06534e',
        i686: 'd20f8ff80b1720301d6e66913b91b7ea9f791d655c4c815739b035b1021d7680',
      x86_64: '064c256e5346d784028223236f4beb03df707511f1e444bef418503f443b1aac'
   })
