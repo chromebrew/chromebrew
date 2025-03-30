d02fa246627b245079c06509a4de5723f81db19c
Zopolis4 <creatorsmithmdt@gmail.com>
2025-03-30 17:06:02 +1100

    qt5_location kde-5.15.15-e0a477d -> kde 5.15.16-6e89db9

--- packages/qt5_location.rb
+++ packages/qt5_location.rb
@@ -3,8 +3,8 @@ require 'buildsystems/qmake'
 class Qt5_location < Qmake
   description 'Qt Location and Positioning'
   homepage 'https://www.qt.io/'
-  kde_5_15_githash = 'e0a477d04f35495ba6eeda8578d1311dba623270'
-  version "kde-5.15.15-#{kde_5_15_githash[0, 7]}"
+  kde_5_15_githash = '6e89db9fcf76fa35c9275123c814e260610d355e'
+  version "kde-5.15.16-#{kde_5_15_githash[0, 7]}"
   license 'FDL, GPL-2, GPL-3, GPL-3-with-qt-exception, LGPL-2.1 and LGPL-3'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://invent.kde.org/qt/qt/qtlocation.git'
@@ -12,9 +12,9 @@ class Qt5_location < Qmake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd7226e3dd07f0ff22d1d3620b3070de21652d925dfb991bb29afc685d5ef8ac8',
-     armv7l: 'd7226e3dd07f0ff22d1d3620b3070de21652d925dfb991bb29afc685d5ef8ac8',
-     x86_64: '48a9b9b9a82d920f2a978ba5beed7cfb50cd182e2092bb662e07387dc7c78492'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
 
   depends_on 'gcc_lib' # R
