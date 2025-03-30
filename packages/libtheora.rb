4e7a452da551ebdacfb6e29e5e3f1d982529b7e9
Zopolis4 <creatorsmithmdt@gmail.com>
2025-03-30 17:41:21 +1100

    libtheora 1.1.1-0 -> 1.2.0

--- packages/libtheora.rb
+++ packages/libtheora.rb
@@ -1,31 +1,27 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libtheora < Package
+class Libtheora < Autotools
   description 'Theora is a free and open video compression format from the Xiph.org Foundation.'
   homepage 'https://theora.org/'
-  version '1.1.1-0'
-  license 'BSD'
+  version '1.2.0'
+  license 'BSD-3'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.bz2'
-  source_sha256 'b6ae1ee2fa3d42ac489287d3ec34c5885730b1296f0801ae577a35193d3affbc'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.xiph.org/xiph/theora.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8737530ad3a7923d32b273258123f8c18d9f7c7ef13be95880e9b4e85d435cfb',
-     armv7l: '8737530ad3a7923d32b273258123f8c18d9f7c7ef13be95880e9b4e85d435cfb',
-     x86_64: '281643b002a7da1589c01550d7a73ca0eed69d4bde2529b6c881bc8debd2bade'
+    aarch64: 'fd23420be72f47ce81a228bcaf3c3a0a2d14e9066e61b11bd866b583946000f2',
+     armv7l: 'fd23420be72f47ce81a228bcaf3c3a0a2d14e9066e61b11bd866b583946000f2',
+     x86_64: '67f6f01c91e6028ae23c58f36e8867d966c6d10d1f856f97e8801b6064c20184'
   })
 
-  depends_on 'libvorbis'
-  depends_on 'libsdl2'
+  depends_on 'glibc' # R
+  depends_on 'libogg'
 
-  def self.build
-    system "sed -i 's/png_sizeof/sizeof/g' ./examples/png2theora.c"
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
+  def self.patch
+    system "sed -i 's|/usr/bin/perl|#{CREW_PREFIX}/bin/perl|' ./lib/arm/arm2gnu.pl"
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  run_tests
 end
