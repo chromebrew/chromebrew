b5485cf18fac27697e4eb7ed957624d5f5c6e68c
Ed Reel <edreel@gmail.com>
2025-03-30 08:06:52 +0000

    Patch 2.7.6 => 2.8

--- packages/patch.rb
+++ packages/patch.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Patch < Package
+class Patch < Autotools
   description 'Patch takes a patch file containing a difference listing produced by the diff program and applies those differences to one or more original files, producing patched versions.'
   homepage 'https://www.gnu.org/software/patch/'
-  version '2.7.6'
+  version '2.8'
   license 'GPL-3+'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/patch/patch-2.7.6.tar.xz'
+  source_url "https://ftpmirror.gnu.org/patch/patch-#{version}.tar.xz"
   source_sha256 'ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'f5ed23759094417cda17c86653388d8bfdacd8073f50b63754115bd1e3470718',
@@ -20,18 +20,8 @@ class Patch < Package
   depends_on 'attr' # R
   depends_on 'glibc' # R
 
-  def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
+  year2038 = ARCH.include?('x86_64') ? '' : '--disable-year2038'
+  configure_options year2038
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  run_tests
 end
