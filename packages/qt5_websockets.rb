139828404c6b03ac70ff642cd7b34ebc9a846302
Zopolis4 <creatorsmithmdt@gmail.com>
2025-03-31 08:34:09 +1100

    qt5_websockets kde-5.15.15-0231c7c -> kde-5.15.16-a0c1c33

--- packages/qt5_websockets.rb
+++ packages/qt5_websockets.rb
@@ -3,8 +3,8 @@ require 'buildsystems/qmake'
 class Qt5_websockets < Qmake
   description 'Qt Websockets'
   homepage 'https://www.qt.io/'
-  kde_5_15_githash = '0231c7c3a17c0320601e7766e7e701db5b9eb0dc'
-  version "kde-5.15.15-#{kde_5_15_githash[0, 7]}"
+  kde_5_15_githash = 'a0c1c335b691ad5ecaddbec17a14dcb2a129a177'
+  version "kde-5.15.16-#{kde_5_15_githash[0, 7]}"
   license 'GPL-3'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://invent.kde.org/qt/qt/qtwebsockets.git'
@@ -12,9 +12,9 @@ class Qt5_websockets < Qmake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f2656344e2f7e71b9f170fe07e07eba45f5d5a9c29198af49d952127c3f3ec0d',
-     armv7l: 'f2656344e2f7e71b9f170fe07e07eba45f5d5a9c29198af49d952127c3f3ec0d',
-     x86_64: 'dfd957d60047987384111ca9ca35a8fdd87a93a5f8bc73ed3eb742280cf47efa'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
 
   depends_on 'gcc_lib' # R
