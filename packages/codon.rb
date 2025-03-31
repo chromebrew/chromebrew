666ab831f953fdf3d6abfecf2b2b1927ac537efc
Ed Reel <edreel@gmail.com>
2025-03-31 05:30:54 +0000

    Codon 0.18.1 => 0.18.2

--- packages/codon.rb
+++ packages/codon.rb
@@ -3,11 +3,11 @@ require 'package'
 class Codon < Package
   description 'Codon is a high-performance Python compiler that compiles Python code to native machine code without any runtime overhead.'
   homepage 'https://docs.exaloop.io/codon/'
-  version '0.18.1'
+  version '0.18.2'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/exaloop/codon/releases/download/v#{version}/codon-linux-x86_64.tar.gz"
-  source_sha256 'bef8ffdfc3fb36b079f298881611315e1cfa953b81bb05d7ceb85d43840102c1'
+  source_sha256 '74b501e7037b45563daa4857ec132c989eca011515904d59e8bb02f4bd8f81d2'
 
   depends_on 'llvm'
   depends_on 'python3'
