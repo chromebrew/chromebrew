39627bea014eac83e71e77b2d4c8d32217d216b5
Ed Reel <edreel@gmail.com>
2025-03-31 17:29:34 -0500

    Acquia_cli 2.39.0 => 2.41.0 (#11644)

--- packages/acquia_cli.rb
+++ packages/acquia_cli.rb
@@ -3,11 +3,11 @@ require 'package'
 class Acquia_cli < Package
   description 'Acquia CLI - The official command-line tool for interacting with the Drupal Cloud Platform and services.'
   homepage 'https://github.com/acquia/cli'
-  version '2.39.0'
+  version '2.41.0'
   license 'GPL-2.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://github.com/acquia/cli/releases/download/#{version}/acli.phar"
-  source_sha256 '33df30a3bd1eeefd3b3aa80862564c6dd46f02d5c9fb62d18b35499d4e2c5fe5'
+  source_sha256 '27b97c98f9037311b42e26df6d3f480f2985f8025852ad07b566f83cee844d29'
 
   depends_on 'php83' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
@@ -16,7 +16,7 @@ class Acquia_cli < Package
   def self.preflight
     major = `php -v 2> /dev/null | head -1 | cut -d' ' -f2 | cut -d'.' -f1`
     minor = `php -v 2> /dev/null | head -1 | cut -d' ' -f2 | cut -d'.' -f2`
-    abort "acli requires php >= 8.0. php#{major.chomp}#{minor.chomp} does not meet the minimum requirement.".lightred unless major.empty? || minor.empty? || ((major.to_i >= 8) && (minor.to_i >= 0))
+    abort "acli requires php >= 8.2. php#{major.chomp}#{minor.chomp} does not meet the minimum requirement.".lightred unless major.empty? || minor.empty? || ((major.to_i >= 8) && (minor.to_i >= 2))
   end
 
   def self.install
