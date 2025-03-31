090e57378fe7d15a44e6306423f0b6f6a95a97f0
Ed Reel <edreel@gmail.com>
2025-03-31 08:41:22 +0000

    Ldc 1.40.0 => 1.40.1

--- packages/ldc.rb
+++ packages/ldc.rb
@@ -3,12 +3,12 @@ require 'package'
 class Ldc < Package
   description 'D language compiler using LLVM.'
   homepage 'https://github.com/ldc-developers/ldc'
-  version '1.40.0'
+  version '1.40.1'
   license 'BSD-3, Boost-1.0, MIT, NCSA and Artistic-1.0 or GPL-2+'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/ldc-developers/ldc/releases/download/v#{version}/ldc2-#{version}-linux-x86_64.tar.xz"
-  source_sha256 '0da61ed2ea96583aa0ccbeb00f8d78983b23d1e87b84a6f2098eb12059475b27'
+  source_sha256 '085a593dba4b1385ec03e7521aa97356e5a7d9f6194303eccb3c1e35935c69d8'
 
   depends_on 'curl'
   depends_on 'ncurses'
