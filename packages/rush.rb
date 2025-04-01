08b324b2b409ed5322ef290404674a83a65d597e
Zopolis4 <creatorsmithmdt@gmail.com>
2025-04-01 18:49:32 +1100

    rush 2.1 -> 2.4

--- packages/rush.rb
+++ packages/rush.rb
@@ -1,32 +1,21 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Rush < Package
+class Rush < Autotools
   description 'GNU Rush is a Restricted User Shell.'
   homepage 'https://www.gnu.org/software/rush/'
-  version '2.1'
+  version '2.4'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/rush/rush-2.1.tar.xz'
-  source_sha256 'c2ff487d44ce3d14854d0269eb0aa4c0f98bcca35390fad5ea52da75d9e4abdf'
-  binary_compression 'tar.xz'
+  source_url 'http://git.gnu.org.ua/rush.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3b5629f53b3efc539ba397302fddd223fce1cbe5e5dee50be3e3d36868424984',
-     armv7l: '3b5629f53b3efc539ba397302fddd223fce1cbe5e5dee50be3e3d36868424984',
-       i686: '005bf529cd4383852c40f426e8c3f2cb44107888a14590bbb570ef90266bf18c',
-     x86_64: '69e660ca63ddfa08b4d4da0884ede5826715aaec6c189e79214ac4c8b30f3cd5'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+       i686: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  run_tests
 end
