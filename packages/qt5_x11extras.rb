ced3337ee6ecf299c0403a7037e87518410ae1fc
Zopolis4 <creatorsmithmdt@gmail.com>
2025-04-01 12:15:16 +1100

    qt5_x11extras kde-5.15.15-ff2cc00 -> kde-5.15.16-0c61151

--- packages/qt5_x11extras.rb
+++ packages/qt5_x11extras.rb
@@ -3,8 +3,8 @@ require 'package'
 class Qt5_x11extras < Package
   description 'Provides classes for developing for the X11 platform.'
   homepage 'https://www.qt.io/'
-  kde_5_15_githash = 'ff2cc0065a3504e6043b47b1b4601fcdce62eefb'
-  version "kde-5.15.15-#{kde_5_15_githash[0, 7]}"
+  kde_5_15_githash = '0c61151bf14e5b4c74187608b6b47b9d0d6ca745'
+  version "kde-5.15.16-#{kde_5_15_githash[0, 7]}"
   license 'FDL, GPL-2, GPL-3, GPL-3-with-qt-exception and LGPL-3'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://invent.kde.org/qt/qt/qtx11extras.git'
@@ -12,9 +12,9 @@ class Qt5_x11extras < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8f0ead9577a59ea420d245b360238f7523b2b40cfcdf88067835ea49ac2d3e6b',
-     armv7l: '8f0ead9577a59ea420d245b360238f7523b2b40cfcdf88067835ea49ac2d3e6b',
-     x86_64: '24cd8d5e40511986bbed75f9ed50798aa36de42e39c03df4aff1d8cbc4bf3310'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
 
   depends_on 'gcc_lib' # R
