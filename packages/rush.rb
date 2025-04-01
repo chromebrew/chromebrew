d25f8fd247a35c1e9338a1ca0f84e86ef4d323f1
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-04-01 03:56:13 -0400

    Add built packages for linux/386 to rush

--- packages/rush.rb
+++ packages/rush.rb
@@ -13,9 +13,11 @@ class Rush < Autotools
   binary_sha256({
     aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
      armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-       i686: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
+       i686: '1c891be6b48584f7579213b542c76a6df6b2ef790814273fd8c84df8309ea5b0',
      x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
+
+  depends_on 'glibc' # R
 
   run_tests
 end
