710a4e81d8d213efad7c97cf2a9ed86d7d7a00b6
Satadru Pramanik <satadru@gmail.com>
2025-04-03 13:41:36 -0400

    Update libidn2
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libidn2.rb
+++ packages/libidn2.rb
@@ -3,11 +3,10 @@ require 'buildsystems/autotools'
 class Libidn2 < Autotools
   description 'GNU Libidn is a fully documented implementation of the Stringprep, Punycode and IDNA 2003 specifications.'
   homepage 'https://www.gnu.org/software/libidn/'
-  @_ver = '2.3.4'
-  version "#{@_ver}-1"
+  version '2.3.8'
   license 'GPL-2+ and LGPL-3+'
   compatibility 'all'
-  source_url "https://ftpmirror.gnu.org/libidn/libidn2-#{@_ver}.tar.gz"
+  source_url "https://ftpmirror.gnu.org/libidn/libidn2-#{version}.tar.gz"
   source_sha256 '93caba72b4e051d1f8d4f5a076ab63c99b77faee019b72b9783b267986dbb45f'
   binary_compression 'tar.zst'
 
