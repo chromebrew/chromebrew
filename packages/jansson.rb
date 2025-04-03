b3552004d5249d1a1c65e1b02805470a572526fa
satmandu <satmandu@users.noreply.github.com>
2025-04-03 13:23:55 -0400

    Add built packages for linux/arm/v7 to libngh

--- packages/jansson.rb
+++ packages/jansson.rb
@@ -11,8 +11,8 @@ class Jansson < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '934cd7813f9a0c8ad07c967f0d65de05a545d697cc10c6a270c09e6dbbdf30df',
-     armv7l: '934cd7813f9a0c8ad07c967f0d65de05a545d697cc10c6a270c09e6dbbdf30df',
+    aarch64: 'bc0f1fb68eb545878b69c4f947f2067034bf7671da0c12e6eea2922ec26d2c7a',
+     armv7l: 'bc0f1fb68eb545878b69c4f947f2067034bf7671da0c12e6eea2922ec26d2c7a',
        i686: '17d0fce402ba1eb72c150f599e0844b51b1822f07b7dfcc013d02df1fe2a87f4',
      x86_64: 'e924bb12424fabfddc56c8515272f2af3fae693aea4361a780713079f56a813b'
   })
