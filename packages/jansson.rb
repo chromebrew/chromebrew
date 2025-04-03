48e0a2b509e1d411b87d18280e8a7e226dd45393
satmandu <satmandu@users.noreply.github.com>
2025-04-03 13:20:17 -0400

    Add built packages for linux/386 to libngh

--- packages/jansson.rb
+++ packages/jansson.rb
@@ -13,9 +13,11 @@ class Jansson < CMake
   binary_sha256({
     aarch64: '934cd7813f9a0c8ad07c967f0d65de05a545d697cc10c6a270c09e6dbbdf30df',
      armv7l: '934cd7813f9a0c8ad07c967f0d65de05a545d697cc10c6a270c09e6dbbdf30df',
-       i686: '3a4c91fbfd2b20b52174e9dfb28b16d1ed22767af66379711e1da77ac9ec9e86',
+       i686: '17d0fce402ba1eb72c150f599e0844b51b1822f07b7dfcc013d02df1fe2a87f4',
      x86_64: '224290a4b9c5e4361d7a41171745ec02211281582af9a6f7aa0b7de47a06b7de'
   })
+
+  depends_on 'glibc' # R
 
   cmake_options '-DJANSSON_BUILD_SHARED_LIBS=ON'
 end
