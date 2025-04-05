03aacc5bd2bef23ecd7236398c0cfe9f62377571
Ed Reel <edreel@gmail.com>
2025-04-05 03:58:28 +0000

    Erlang 27.3 => 27.3.2

--- packages/erlang.rb
+++ packages/erlang.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Erlang < Autotools
   description 'Erlang is a programming language used to build massively scalable soft real-time systems with requirements on high availability.'
   homepage 'https://www.erlang.org/'
-  version '27.3'
+  version '27.3.2'
   license 'Apache-2.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/erlang/otp.git'
@@ -16,16 +16,16 @@ class Erlang < Autotools
      x86_64: 'cf26969cca258ff301a0c33c0e51ee4d785ce400852e6ad62baaa8b23be4a1dd'
   })
 
-  depends_on 'openjdk8'
-  depends_on 'wxwidgets'
   depends_on 'gcc_lib' # R
   depends_on 'glib' # R
   depends_on 'glibc' # R
   depends_on 'libglu' # R
   depends_on 'libglvnd' # R
   depends_on 'ncurses' # R
+  depends_on 'openjdk8' # R
   depends_on 'openssl' # R
   depends_on 'unixodbc' # R
+  depends_on 'wxwidgets' # R
   depends_on 'zlib' # R
 
   configure_options "ERL_OTP=#{Dir.pwd} #{'--disable-year2038' unless ARCH.eql?('x86_64')}"
