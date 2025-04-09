c61b0b75467ccab21ad782925ec18e483660919b
Ed Reel <edreel@gmail.com>
2025-04-09 19:36:27 +0000

    Diffutils 3.11 => 3.12

--- packages/diffutils.rb
+++ packages/diffutils.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Diffutils < Autotools
   description 'GNU Diffutils is a package of several programs related to finding differences between files.'
   homepage 'https://www.gnu.org/software/diffutils/'
-  version '3.11'
+  version '3.12'
   license 'GPL-2'
   compatibility 'all'
-  source_url "https://ftpmirror.gnu.org/gnu/diffutils/diffutils-#{version}.tar.xz"
-  source_sha256 'a73ef05fe37dd585f7d87068e4a0639760419f810138bd75c61ddaa1f9e2131e'
+  source_url "https://ftpmirror.gnu.org/diffutils/diffutils-#{version}.tar.xz"
+  source_sha256 '7c8b7f9fc8609141fdea9cece85249d308624391ff61dedaf528fcb337727dfd'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -18,7 +18,7 @@ class Diffutils < Autotools
   })
 
   depends_on 'glibc' # R
-  depends_on 'libsigsegv'
+  depends_on 'libsigsegv' # R
 
   configure_options '--disable-year2038' unless ARCH == 'x86_64'
 end
