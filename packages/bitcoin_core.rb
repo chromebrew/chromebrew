3c3183d068fa1cd884ecda02769989f9e4dddaf7
fanquake <fanquake@gmail.com>
2025-04-16 17:16:03 +0100

    Bitcoin Core 28.1 => 29.0 (#11722)

--- packages/bitcoin_core.rb
+++ packages/bitcoin_core.rb
@@ -3,18 +3,18 @@ require 'package'
 class Bitcoin_core < Package
   description 'Bitcoin Core is a full Bitcoin client and builds the backbone of the network.'
   homepage 'https://bitcoincore.org/'
-  version '28.1'
+  version '29.0'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url({
-    aarch64: 'https://bitcoincore.org/bin/bitcoin-core-28.1/bitcoin-28.1-arm-linux-gnueabihf.tar.gz',
-     armv7l: 'https://bitcoincore.org/bin/bitcoin-core-28.1/bitcoin-28.1-arm-linux-gnueabihf.tar.gz',
-     x86_64: 'https://bitcoincore.org/bin/bitcoin-core-28.1/bitcoin-28.1-x86_64-linux-gnu.tar.gz'
+    aarch64: "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-arm-linux-gnueabihf.tar.gz",
+     armv7l: "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-arm-linux-gnueabihf.tar.gz",
+     x86_64: "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-x86_64-linux-gnu.tar.gz"
   })
   source_sha256({
-    aarch64: '6448274420ac632c528bbd4da7198692232cef7bd16d101febc5d05f7d4af1d2',
-     armv7l: '6448274420ac632c528bbd4da7198692232cef7bd16d101febc5d05f7d4af1d2',
-     x86_64: '07f77afd326639145b9ba9562912b2ad2ccec47b8a305bd075b4f4cb127b7ed7'
+    aarch64: 'ea8ca24ab56d486a55289c43cb4256f9f0e66224899cc43482c9498a3f2614d1',
+     armv7l: 'ea8ca24ab56d486a55289c43cb4256f9f0e66224899cc43482c9498a3f2614d1',
+     x86_64: 'a681e4f6ce524c338a105f214613605bac6c33d58c31dc5135bbc02bc458bb6c'
   })
 
   no_compile_needed
