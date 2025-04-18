fe02ebdb59e592d900cc46ad1f20660857577729
Satadru Pramanik <satadru@gmail.com>
2025-04-18 11:35:24 -0400

    update dejagnu
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/dejagnu.rb
+++ packages/dejagnu.rb
@@ -1,12 +1,12 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Dejagnu < Package
+class Dejagnu < Autotools
   description 'DejaGnu is a framework for running test suites on GNU tools.'
   homepage 'https://www.gnu.org/software/dejagnu/'
-  version '1.6.1'
+  version '1.6.3'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/dejagnu/dejagnu-1.6.1.tar.gz'
+  source_url "https://ftpmirror.gnu.org/dejagnu/dejagnu-#{version}.tar.gz"
   source_sha256 'bf5b28bb797e0ace4cfc0766a996339c795d8223bef54158be7887046bc01692'
   binary_compression 'tar.xz'
 
@@ -19,21 +19,14 @@ class Dejagnu < Package
 
   depends_on 'expect'
   depends_on 'texinfo'
+  run_tests
 
-  def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
+  configure_build_extras do
     system 'makeinfo --html --no-split -o doc/dejagnu.html doc/dejagnu.texi'
     system 'makeinfo --plaintext       -o doc/dejagnu.txt  doc/dejagnu.texi'
   end
 
-  def self.check
-    system 'make check'
-  end
-
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
+  configure_install_extras do
     system "install -v -dm755   #{CREW_DEST_PREFIX}/share/doc/dejagnu-1.6.1"
     system "install -v -m644 doc/dejagnu.html #{CREW_DEST_PREFIX}/share/doc/dejagnu-1.6.1"
     system "install -v -m644 doc/dejagnu.txt  #{CREW_DEST_PREFIX}/share/doc/dejagnu-1.6.1"
