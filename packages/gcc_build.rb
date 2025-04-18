d0b6758868c80026699e8fd00e48334017408a78
Satadru Pramanik <satadru@gmail.com>
2025-04-18 15:30:08 -0400

    back out rust
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gcc_build.rb
+++ packages/gcc_build.rb
@@ -48,7 +48,6 @@ class Gcc_build < Package
   depends_on 'libssp' # L
   depends_on 'mpc' # R
   depends_on 'mpfr' # R
-  depends_on 'rust' => :build
   depends_on 'zlib' # R
   depends_on 'zstd' # R
 
@@ -138,7 +137,7 @@ class Gcc_build < Package
     @cflags = @cxxflags = '-fPIC -pipe'
     # @languages = 'c,c++,jit,objc,fortran,go'
     # go build fails on 20220305 snapshot
-    @languages = 'c,c++,jit,objc,fortran,go,rust'
+    @languages = 'c,c++,jit,objc,fortran,go'
     case ARCH
     when 'armv7l', 'aarch64'
       @archflags = '--with-arch=armv7-a+fp --with-float=hard --with-tune=cortex-a15 --with-fpu=vfpv3-d16'
