d23b9254dbde3cad5f464fdc2e1e208dd5530983
Satadru Pramanik <satadru@gmail.com>
2025-04-18 14:42:50 -0400

    Add rust build dep to gcc.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gcc_build.rb
+++ packages/gcc_build.rb
@@ -48,6 +48,7 @@ class Gcc_build < Package
   depends_on 'libssp' # L
   depends_on 'mpc' # R
   depends_on 'mpfr' # R
+  depends_on 'rust' => :build
   depends_on 'zlib' # R
   depends_on 'zstd' # R
 
