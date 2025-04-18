af6832d1abc0e9fd9d889865d639532afb2e6a8b
supechicken <supechicken@users.noreply.github.com>
2025-04-18 14:57:22 -0400

    Add built packages for linux/arm/v7 to update-acpica

--- packages/acpica.rb
+++ packages/acpica.rb
@@ -11,8 +11,8 @@ class Acpica < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4fab7ff907309a9ded50cc5054d3796e6cf852c057e9eaa7f447d6d4b31dff90',
-     armv7l: '4fab7ff907309a9ded50cc5054d3796e6cf852c057e9eaa7f447d6d4b31dff90',
+    aarch64: '3fcd388f8f8fba58e2aa3a15377b782bb2aa6412ba090bdf5b9da00fdae723e3',
+     armv7l: '3fcd388f8f8fba58e2aa3a15377b782bb2aa6412ba090bdf5b9da00fdae723e3',
        i686: '0b01a7b5429b74a165642d83b8f7b06115182338c7483c47987c1ba318dd3332',
      x86_64: '80c71c2cda3045d6c94b4fb298833b647001dc519c5f60e819bbb50b41a022a9'
   })
