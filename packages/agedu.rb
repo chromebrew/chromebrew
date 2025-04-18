d34cbb8db21bbc19e8247922ff3d634a5383dd36
SupeChicken666 <supechicken666@gmail.com>
2025-04-19 02:54:56 +0800

    agedu 20211129 => 20241013

--- packages/agedu.rb
+++ packages/agedu.rb
@@ -1,13 +1,13 @@
 require 'package'
 
-class Agedu < Package
+class Agedu < Cmake
   description 'Unix utility for tracking down wasted disk space'
   homepage 'https://www.chiark.greenend.org.uk/~sgtatham/agedu/'
-  version '20211129'
+  version '20241013'
   license 'Copyright 2008 Simon Tatham. All rights reserved.'
   compatibility 'all'
-  source_url 'https://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-20211129.8cd63c5.tar.gz'
-  source_sha256 'ceaee592ef21b8cbb254aa7e9c5d22cefab24535e137618a4d0af591eba8339f'
+  source_url 'https://www.chiark.greenend.org.uk/~sgtatham/agedu/agedu-20241013.3622eda.tar.gz'
+  source_sha256 '3f77cb2e4dd64c100f7a7b0789a6c06cc16f23e7fe78c1451f5020dd823cf2f8'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -16,18 +16,4 @@ class Agedu < Package
        i686: 'd6f0c3671b9dd06835c9ff8f2d73e95c9ff61e14a915d41f9f1161076884cefa',
      x86_64: '1cb548694350a3bdd97252f51ee588b272733a2973a8db19fd797ace6a5a8042'
   })
-
-  def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system "cmake #{CREW_CMAKE_OPTIONS} .."
-      system 'make'
-    end
-  end
-
-  def self.install
-    Dir.chdir 'build' do
-      system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    end
-  end
 end
