d6bcdc7b8dd4fc1854af4838b248063b32af7e29
SupeChicken666 <supechicken666@gmail.com>
2025-04-19 16:05:50 +0800

    angle_grinder 0.19.4 => 0.19.6

--- packages/angle_grinder.rb
+++ packages/angle_grinder.rb
@@ -3,11 +3,11 @@ require 'package'
 class Angle_grinder < Package
   description 'Slice and dice logs on the command line'
   homepage 'https://github.com/rcoh/angle-grinder'
-  version '0.19.4'
+  version '0.19.6'
   license 'MIT'
   compatibility 'x86_64'
   source_url "https://github.com/rcoh/angle-grinder/releases/download/v#{version}/agrind-x86_64-unknown-linux-gnu.tar.gz"
-  source_sha256 '2c9472b93e1250545167eacadda0ad1b72f1770208cefa969d1a24ab735c32ee'
+  source_sha256 '3c3c3611c5ee86edabddcf5546715fef30e1644e474bd4a29b2eb1f8f074d626'
 
   no_compile_needed
   no_shrink
