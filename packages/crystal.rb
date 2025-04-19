a70600516aad97f9ed84a64bf3384ccec9d9b625
SupeChicken666 <supechicken666@gmail.com>
2025-04-20 02:52:11 +0800

    Suggested changes

--- packages/crystal.rb
+++ packages/crystal.rb
@@ -6,10 +6,10 @@ class Crystal < Package
   version '1.16.1'
   license 'Apache-2.0'
   compatibility 'x86_64'
-  source_url 'https://github.com/crystal-lang/crystal/releases/download/1.16.1/crystal-1.16.1-1-linux-x86_64-bundled.tar.gz'
+  source_url "https://github.com/crystal-lang/crystal/releases/download/1.16.1/crystal-#{version}-1-linux-x86_64-bundled.tar.gz"
   source_sha256 '1e53c11e831eea3637434ad0f4beef30e5ae78e823f5d23945f213c55ab22ac6'
 
-  depends_on 'llvm16_lib' # R
+  depends_on 'llvm20_lib' # R
 
   no_compile_needed
   print_source_bashrc
