9ab0c500dbe5e40b36b7d265e09b08b9cd7f572a
Ed Reel <edreel@gmail.com>
2025-04-19 17:06:25 -0500

    Peazip 10.3.0 => 10.4.0

--- packages/peazip.rb
+++ packages/peazip.rb
@@ -3,7 +3,7 @@ require 'package'
 class Peazip < Package
   description 'Free file archiver utility to open and extract RAR, TAR and ZIP archives'
   homepage 'https://peazip.github.io/'
-  version ARCH.eql?('x86_64') ? '10.3.0' : '5.2.0'
+  version ARCH.eql?('x86_64') ? '10.4.0' : '5.2.0'
   license 'LGPL-3'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.34' if ARCH.eql?('x86_64')
@@ -15,7 +15,7 @@ class Peazip < Package
   source_sha256({
     aarch64: '6724a5c4e273f086680ac30440a2014b647322d25ed9bba574b7b8edba61abcc',
      armv7l: '6724a5c4e273f086680ac30440a2014b647322d25ed9bba574b7b8edba61abcc',
-     x86_64: '460546fa208e9476289035e178c80b4ebefcc831f6b1b0049d9ab2b1d37a6699'
+     x86_64: 'bd638a1f7f7cd7127dd181562d5b0c0463994f890576e9c409edb5b3e2760fa8'
   })
 
   depends_on 'gtk2'
