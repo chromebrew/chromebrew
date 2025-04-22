7accc851a2af007eed1535d7f9d421d8aeaa8d3b
Ed Reel <edreel@gmail.com>
2025-04-22 03:21:20 -0500

    Btop 1.2.13 => 1.4.0 (#11800)

--- packages/btop.rb
+++ packages/btop.rb
@@ -3,23 +3,23 @@ require 'package'
 class Btop < Package
   description 'Resource monitor that shows usage and stats for processor, memory, disks, network and processes.'
   homepage 'https://github.com/aristocratos/btop'
-  version '1.2.13'
+  version '1.4.0'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://github.com/aristocratos/btop.git'
-  git_hashtag "v#{version}"
-  binary_compression 'tar.zst'
-
-  binary_sha256({
-    aarch64: '17be864bd2d5ce7c2c7975452d8baefc8b7fcef0adf833227b17e13d46cebb4b',
-     armv7l: '17be864bd2d5ce7c2c7975452d8baefc8b7fcef0adf833227b17e13d46cebb4b',
-       i686: '49cede8a4d3b2f1d0091dac06aa0cb8527890764e17dcccef2c3fa8e17535dca',
-     x86_64: 'c7ee94ef41ce100eb4c9b247a97dfeb008b5f176dc5641614b49c0322029aa56'
+  source_url({
+    aarch64: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-armv7l-linux-musleabihf.tbz",
+     armv7l: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-armv7l-linux-musleabihf.tbz",
+       i686: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-i686-linux-musl.tbz",
+     x86_64: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-x86_64-linux-musl.tbz"
+  })
+  source_sha256({
+    aarch64: '7f793a78c875829b14ded0c9dd3e851a284a50b9eaefdf9407e93d8f767993f5',
+     armv7l: '7f793a78c875829b14ded0c9dd3e851a284a50b9eaefdf9407e93d8f767993f5',
+       i686: '987ab934b52f6e4cfddd3d4706142963f45e4b8a4d4fdeed3b45e27331892954',
+     x86_64: '2e49cde9c0f273e5b1699d2c4f1ce5d1c08cca5fd90e5e0226dbac9fd00c82f3'
   })
 
-  def self.build
-    system 'make'
-  end
+  no_compile_needed
 
   def self.install
     system 'make', "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install'
