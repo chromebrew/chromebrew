f33c77e3b3bdb7187d1c22c9e109c6c854b9fdd1
SupeChicken666 <me@supechicken666.dev>
2025-04-29 20:08:25 +0800

    Move all patches into a separate repo
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- packages/glibc_standalone.rb
+++ packages/glibc_standalone.rb
@@ -3,12 +3,12 @@ require 'package'
 class Glibc_standalone < Package
   description 'The GNU C Library project provides the core libraries for GNU/Linux systems.'
   homepage 'https://www.gnu.org/software/libc/'
-  version '2.41-1'
+  version '2.41-2'
   license 'LGPL-2.1+, BSD, HPND, ISC, inner-net, rc, and PCRE'
   compatibility 'all'
   source_url "https://ftpmirror.gnu.org/glibc/glibc-#{version.partition('-')[0]}.tar.xz"
   source_sha256 'a5a26b22f545d6b7d7b3dd828e11e428f24f4fac43c934fb071b6a7d0828e901'
-  binary_compression 'tar.zst'
+  binary_compression 'tar.xz'
 
   binary_sha256({
     aarch64: 'e19d852da8d4ead346d62003f9e5f15076ed5fae5bcd12dc9bd8f7f2602bc1b2',
@@ -31,166 +31,27 @@ class Glibc_standalone < Package
   no_shrink
 
   def self.patch
-    # See https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/refs/heads/release-R136-16238.B/sys-libs/glibc/files/local/glibc-2.39/0002-libc-Speed-up-large-memcpy-on-Cortex-A7-A15.patch
-    File.write '0001-libc-Speed-up-large-memcpy-on-Cortex-A7-A15.patch', <<~EOF
-      From 8611480aeeefb3c52f124079fa5e946e831db27d Mon Sep 17 00:00:00 2001
-      From: Yunlian Jiang <yunlian@google.com>
-      Date: Fri, 1 Aug 2014 15:19:34 -0700
-      Subject: [PATCH 1/3] libc: Speed up large memcpy() on Cortex-A7/A15
-
-      Details please see crbug://331427
-
-      Experimentally it's been found that the "unaligned" memcpy() is
-      actually faster for sufficiently large memory copies.  It appears that
-      the changeover point is a little different for different processors,
-      though.  For A15 there's a lot more run-to-run variance for
-      medium-sized memcpy() but the changeover appears to be at ~16K.  For
-      A7 (and maybe A9) the changeover seems to be a little further out.
-      We think the variance in A15 memcpy() is is due to different physical
-      addresses for the chunks of memory given to us by the kernel.  It is
-      certain that the "aligned" code works faster at 4K and less and that
-      the "unaligned" code works faster with very large chunks of memory.
-      Since we care most about A15 performance and the A7 performance is not
-      that much worse (and actually better for SDRAM transfers), we'll pick
-      the number that's best for the A15.
-      Tests on snow (A15 only):
-      * Large (12M) aligned copies go from ~2350 MiB/s to ~2900 MiB/s.
-      * Medium (96K) aligned copies go from ~5900 MiB/s to ~6300 MiB/s.
-      * Medium (16K) aligned copies seem to be better but there's a lot of
-        noise (old=8151.8, 8736.6, 8168.7; new=9364.9, 9829.5, 9639.0)
-      * Small (4K, 8K) algined copies are unchanged.
-      For A7-only on pit:
-      * Large (12M) aligned copies go from 440 MiB/s to 930 MiB/s.
-      * Medium (96K) aligned copies regress from ~2650 MiB/s to ~2400 MiB/s.
-      * Medium (16K) aligned copies regress from ~3000 MiB/s to ~2800 MiB/s.
-      * Small (4K, 8K) aligned copies are unchanged.
-      See punybench changes at
-       <https://chromium-review.googlesource.com/#/c/182168/3> for how this
-      was tested.  For A15 changes I ran 3 times and averaged (there wasn't
-      lots of variance except for 16K).  For A7 changes I ran 2 times.
-      [Adrian: forward-ported to glibc 2.32]
-      ---
-       sysdeps/arm/armv7/multiarch/memcpy_impl.S | 4 ++++
-       1 file changed, 4 insertions(+)
-
-      diff --git a/sysdeps/arm/armv7/multiarch/memcpy_impl.S b/sysdeps/arm/armv7/multiarch/memcpy_impl.S
-      index dc701827..ac171cde 100644
-      --- a/sysdeps/arm/armv7/multiarch/memcpy_impl.S
-      +++ b/sysdeps/arm/armv7/multiarch/memcpy_impl.S
-      @@ -327,6 +327,10 @@ ENTRY(memcpy)
-       \tcmp\ttmp1, tmp2
-       \tbne\t.Lcpy_notaligned
-
-      +\t/* Use the non-aligned code for >=16K; faster on A7/A15 (A9 too?) */
-      +\tcmp\tcount, #0x4000
-      +\tbge\t.Lcpy_notaligned
-      +
-       #ifdef USE_VFP
-       \t/* Magic dust alert!  Force VFP on Cortex-A9.  Experiments show
-       \t   that the FP pipeline is much better at streaming loads and
-      --
-      2.49.0
-    EOF
-
-    # See https://chromium.googlesource.com/chromiumos/overlays/chromiumos-overlay/+/refs/heads/release-R136-16238.B/sys-libs/glibc/files/local/glibc-2.39/0009-Revert-Add-GLIBC_ABI_DT_RELR-for-DT_RELR-support.patch
-    File.write '0002-Revert-Add-GLIBC_ABI_DT_RELR-for-DT_RELR-support.patch', <<~EOF
-      From 135c5b39bb9b0c9b2d33dd2619381fb5d833ce18 Mon Sep 17 00:00:00 2001
-      From: Adrian Ratiu <adrian.ratiu@collabora.com>
-      Date: Tue, 27 Jun 2023 15:11:47 +0300
-      Subject: [PATCH 2/3] Revert "Add GLIBC_ABI_DT_RELR for DT_RELR support"
-
-      This partially reverts commit 57292f574156f817b7cbeb33ea62
-
-      Adding the GLIBC_ABI_DT_RELR dependency breaks pre-built
-      vendor binaries, so we decided to just revert this simple
-      check, even though our binutils version supports adding it
-      for the binaries we are able to rebuild.
-
-      For more details see b:284450929 and CL 4632684
-      ---
-       elf/dl-version.c | 24 ------------------------
-       1 file changed, 24 deletions(-)
-
-      diff --git a/elf/dl-version.c b/elf/dl-version.c
-      index d414bd1e..5208ec88 100644
-      --- a/elf/dl-version.c
-      +++ b/elf/dl-version.c
-      @@ -359,30 +359,6 @@ _dl_check_map_versions (struct link_map *map, int verbose, int trace_mode)
-       \t}
-           }
-
-      -  /* When there is a DT_VERNEED entry with libc.so on DT_NEEDED, issue
-      -     an error if there is a DT_RELR entry without GLIBC_ABI_DT_RELR
-      -     dependency.  */
-      -  if (dyn != NULL
-      -      && map->l_info[DT_NEEDED] != NULL
-      -      && map->l_info[DT_RELR] != NULL
-      -      && __glibc_unlikely (!map->l_dt_relr_ref))
-      -    {
-      -      const char *strtab = (const void *) D_PTR (map, l_info[DT_STRTAB]);
-      -      const ElfW(Dyn) *d;
-      -      for (d = map->l_ld; d->d_tag != DT_NULL; ++d)
-      -\tif (d->d_tag == DT_NEEDED)
-      -\t  {
-      -\t    const char *name = strtab + d->d_un.d_val;
-      -\t    if (strncmp (name, "libc.so.", 8) == 0)
-      -\t      {
-      -\t\t_dl_exception_create
-      -\t\t  (&exception, DSO_FILENAME (map->l_name),
-      -\t\t   N_("DT_RELR without GLIBC_ABI_DT_RELR dependency"));
-      -\t\tgoto call_error;
-      -\t      }
-      -\t  }
-      -    }
-      -
-         return result;
-       }
-
-      --
-      2.49.0
-    EOF
-
-    File.write '0003-Add-crew-audit.so-to-audit-list-by-default.patch', <<~EOF
-      From bb6db2edcefd9a7ebaea1e301c436b814da767a7 Mon Sep 17 00:00:00 2001
-      From: SupeChicken666 <me@supechicken666.dev>
-      Date: Tue, 29 Apr 2025 17:57:30 +0800
-      Subject: [PATCH 3/3] Add crew-audit.so to audit list by default
-
-      Signed-off-by: SupeChicken666 <me@supechicken666.dev>
-      ---
-       elf/rtld.c | 3 +++
-       1 file changed, 3 insertions(+)
-
-      diff --git a/elf/rtld.c b/elf/rtld.c
-      index 00bec153..2d41ed1f 100644
-      --- a/elf/rtld.c
-      +++ b/elf/rtld.c
-      @@ -2560,6 +2560,9 @@ process_envvars_default (struct dl_main_state *state)
-         char *envline;
-         char *debug_output = NULL;
-
-      +  /* Add crew-audit.so to audit list by default */
-      +  if (access(CREW_AUDIT, F_OK) == 0) audit_list_add_string(&state->audit_list, CREW_AUDIT);
-      +
-         while ((envline = _dl_next_ld_env_entry (&runp)) != NULL)
-           {
-             size_t len = 0;
-      --
-      2.49.0
-    EOF
-
+    system 'git clone --depth=1 https://github.com/chromebrew/crew-package-glibc'
     system 'filefix'
 
-    Dir.glob('*.patch') do |patch|
+    Dir.glob('crew-package-glibc/patches/*.patch') do |patch|
       puts "Applying #{patch}...".yellow
       system 'patch', '-p1', '-i', patch
     end
   end
 
   def self.build
+    cc_macro_list = %W[
+      -DCREW_AUDIT=\\"#{CREW_GLIBC_PREFIX}/crew-audit.so\\"
+      -DCREW_PREFIX=\\"#{CREW_PREFIX}\\"
+      -DCREW_GLIBC_PREFIX=\\"#{CREW_GLIBC_PREFIX}\\"
+      -DCREW_GLIBC_VERSION=\\"#{version.partition('-')[0]}\\"
+      -DCREW_LD_LIBRARY_PATH=\\"#{CREW_GLIBC_PREFIX}:#{ENV['LD_LIBRARY_PATH']}\\"
+    ]
+
     build_env = {
-      CFLAGS:   "-O3 -pipe -fPIC -fno-lto -fuse-ld=lld -DCREW_AUDIT=\\\"#{CREW_GLIBC_PREFIX}/crew-audit.so\\\"",
-      CXXFLAGS: "-O3 -pipe -fPIC -fno-lto -fuse-ld=lld -DCREW_AUDIT=\\\"#{CREW_GLIBC_PREFIX}/crew-audit.so\\\"",
+      CFLAGS:   "-O3 -pipe -fPIC -fno-lto -fuse-ld=lld #{cc_macro_list.join(' ')}",
+      CXXFLAGS: "-O3 -pipe -fPIC -fno-lto -fuse-ld=lld #{cc_macro_list.join(' ')}",
       LD:       'ld.lld' # use lld here as mold will segfault
     }
 
@@ -226,10 +87,19 @@ class Glibc_standalone < Package
       system build_env.transform_keys(&:to_s), '../configure', *config_opts
       system "make PARALLELMFLAGS='-j #{CREW_NPROC}'"
     end
+
+    # compile crew-audit with system's glibc version, as we want it to work on system's glibc also
+    system "cc #{CREW_COMMON_FLAGS.sub(/-L[^\s]+/, '')} #{cc_macro_list.join(' ')} -shared crew-audit.c -o crew-audit.so", chdir: 'crew-package-glibc'
   end
 
   def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install', chdir: 'builddir'
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'localedata/install-locales', chdir: 'builddir'
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d"
+
+    system "make DESTDIR=#{CREW_DEST_DIR} install", chdir: 'builddir'
+    system "make DESTDIR=#{CREW_DEST_DIR} localedata/install-locales", chdir: 'builddir'
+
+    # install crew-audit
+    FileUtils.install 'crew-package-glibc/crew-audit.so', File.join(CREW_GLIBC_PREFIX, 'crew-audit.so'), mode: 0o755
+    File.write "#{CREW_DEST_PREFIX}/etc/env.d/10-glibc", "LD_AUDIT=#{File.join(CREW_GLIBC_PREFIX, 'crew-audit.so')}\n"
   end
 end
