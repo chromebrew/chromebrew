5fefe8907ab9a6e599c7b32c7786317770ef3b28
Zopolis4 <Zopolis4@users.noreply.github.com>
2025-04-28 23:45:37 -0400

    Add built packages for linux/amd64 to cbase

--- packages/cbase.rb
+++ packages/cbase.rb
@@ -14,7 +14,7 @@ class Cbase < Autotools
     aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
      armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
        i686: 'd518a6e7e4cc41244c947a289ef2071e2fe5ecf1b51f0c7f0ff973106352d2cf',
-     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+     x86_64: '3ec3e9c82d5aa3649eecaaec20245abd16c3fbf7835660c06270e3d6a711446d'
   })
 
   depends_on 'expat'
