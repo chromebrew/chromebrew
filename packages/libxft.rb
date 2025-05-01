41539db4d33000aeee6f344f72c7717df5233a06
Satadru Pramanik <satadru@gmail.com>
2025-05-01 13:12:00 -0400

    Add x86_64 & armv7l binaries
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libxft.rb
+++ packages/libxft.rb
@@ -7,15 +7,17 @@ class Libxft < Autotools
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://www.x.org/archive/individual/lib/libXft-#{version}.tar.xz"
-  source_sha256 '79f0b37c45007381c371a790c2754644ad955166dbf2a48e3625032e9bdd4f71'
+  source_sha256 '60a25b78945ed6932635b3bb1899a517d31df7456e69867ffba27f89ff3976f5'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2114f2123bd6fc24fc4f5d9b16441f0ce70aab9bf752ceb2aff99a79ce576334',
-     armv7l: '2114f2123bd6fc24fc4f5d9b16441f0ce70aab9bf752ceb2aff99a79ce576334',
-     x86_64: 'bac8b590570dd2b635e90f0b0b383237bf9df45e7c836bad733ff4023b903a4b'
+    aarch64: 'b842c29e310467395653be8f166d13381c47fb72322ee51f3a8cd34a0c72c023',
+     armv7l: 'b842c29e310467395653be8f166d13381c47fb72322ee51f3a8cd34a0c72c023',
+     x86_64: '0de5d34d95f7577b91e220256929a60ae1839438a95f9120bcf87cb9d3c6e055'
   })
 
+  depends_on 'fontconfig' # R
+  depends_on 'freetype' # R
   depends_on 'glibc' # R
   depends_on 'harfbuzz' # R
   depends_on 'libx11' # R
--- packages/pixman.rb
+++ packages/pixman.rb
@@ -11,10 +11,10 @@ class Pixman < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'cc34d57fb02e6ac863a726b3a13c4d80b3ba975bde965b03113da95b5f72ef6a',
-     armv7l: 'cc34d57fb02e6ac863a726b3a13c4d80b3ba975bde965b03113da95b5f72ef6a',
+    aarch64: '96328061e6337e4395ea4e178cc9fe201ea7713a7ca47dcc26e425c00e878f59',
+     armv7l: '96328061e6337e4395ea4e178cc9fe201ea7713a7ca47dcc26e425c00e878f59',
        i686: '0afdfa6f061738fc4ca8594857886f7e80bcc1ccbaefcaa11ab22663aee623cd',
-     x86_64: 'fa18fdd7bd6c4f8bed2199391b36b6403bee73da220b5730371e13d2f074fed8'
+     x86_64: '649b3673ed250ed5ef978fb795d15136f9975c8b9b961bf5b9606e3931c61f63'
   })
 
   depends_on 'gcc_lib' # R
