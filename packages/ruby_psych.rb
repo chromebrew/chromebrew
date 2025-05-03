e62e76ee513e346680170ad7c2b88d7d068a7199
Satadru Pramanik <satadru@gmail.com>
2025-05-03 16:35:20 -0400

    Update ruby_psych version
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_psych.rb
+++ packages/ruby_psych.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_psych < RUBY
   description '| psych is a yaml parser and emitter.'
   homepage 'https://github.com/ruby/psych'
-  version "5.2.3-#{CREW_RUBY_VER}"
+  version "5.2.4-#{CREW_RUBY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
