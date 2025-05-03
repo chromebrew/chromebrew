307d88cd89d2419da10297c2dac0c3ced0add686
Satadru Pramanik <satadru@gmail.com>
2025-05-03 16:10:20 -0400

    Adjust glibc dev packages for limited LIBC.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/glibc_lib.rb
+++ packages/glibc_lib.rb
@@ -1,28 +1,21 @@
 require 'package'
-Package.load_package("#{__dir__}/glibc.rb")
-Package.load_package("#{__dir__}/glibc_build235.rb")
-Package.load_package("#{__dir__}/glibc_build237.rb")
+Package.load_package("#{__dir__}/glibc_standalone.rb")
 
 class Glibc_lib < Package
   description 'glibc libraries'
-  homepage Glibc.homepage
-  license Glibc.license
+  homepage Glibc_standalone.homepage
+  license Glibc_standalone.license
   source_url 'SKIP'
 
   is_fake
 
-  case LIBC_VERSION
-  when '2.35'
-    version Glibc_build235.version
-    compatibility Glibc_build235.compatibility
-    depends_on 'glibc_lib235'
-  when '2.37'
-    version Glibc_build237.version
-    compatibility Glibc_build237.compatibility
-    depends_on 'glibc_lib237'
+  if LIBC_VERSION <= '2.41'
+    version Glibc_standalone.version
+    compatibility Glibc_standalone.compatibility
+    depends_on 'glibc_standalone'
   else
-    version Glibc.version
-    compatibility Glibc.compatibility
-    depends_on 'glibc'
+    version LIBC_VERSION
+    compatibility 'aarch64 armv7l x86_64'
+    depends_on 'glibc_fallthrough'
   end
 end
