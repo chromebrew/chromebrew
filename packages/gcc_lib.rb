1810f11e1670490c84f582d9cee8fe5f4aabfca0
Satadru Pramanik <satadru@gmail.com>
2025-05-03 15:44:23 -0400

    Adjust gcc versions for limited LIBC variations, tweak LIBC_VERSION detection.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gcc_lib.rb
+++ packages/gcc_lib.rb
@@ -4,11 +4,7 @@ Package.load_package("#{__dir__}/gcc_build.rb")
 class Gcc_lib < Package
   description 'GCC shared libs except libgccjit'
   homepage Gcc_build.homepage
-  @gcc_libc_version = if %w[2.23 2.27 2.32 2.33 2.35 2.37].any? { |i| LIBC_VERSION.include? i }
-                        LIBC_VERSION
-                      else
-                        ARCH.eql?('i686') ? '2.23' : '2.27'
-                      end
+  @gcc_libc_version = ARCH.eql?('i686') ? '2.23' : '2.27'
   version "14.2.0-glibc#{@gcc_libc_version}" # Do not use @_ver here, it will break the installer.
   license Gcc_build.license
   # When upgrading gcc_build, be sure to upgrade gcc_lib, gcc_dev, and libssp in tandem.
