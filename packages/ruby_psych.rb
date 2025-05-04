b44f21300a879ef9f642e4252db1de6aacbbd3a8
Satadru Pramanik <satadru@gmail.com>
2025-05-04 00:18:45 -0400

    Add x86_64 binary for ruby_psych
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_psych.rb
+++ packages/ruby_psych.rb
@@ -12,7 +12,8 @@ class Ruby_psych < RUBY
   binary_sha256({
     aarch64: '2e2f03b3a65ef035f9b878623b33783eb8bda65327c55608d65d5f0d82ef2437',
      armv7l: '2e2f03b3a65ef035f9b878623b33783eb8bda65327c55608d65d5f0d82ef2437',
-       i686: 'bad042d6fb6384410a6e933ecbe923c165879bbdb40c5eb1e31a267666ad268f'
+       i686: 'bad042d6fb6384410a6e933ecbe923c165879bbdb40c5eb1e31a267666ad268f',
+     x86_64: 'f6f4246d8934f2d8b9c6cf2ed74a34793cdf78cb01e7d1dbe993ef33eb4082c3'
   })
 
   depends_on 'ruby_stringio' # R
