9d4e5384e44d7016d5c3c14f386fec4e516e082d
Satadru Pramanik <satadru@gmail.com>
2025-05-05 15:04:16 -0400

    Add x86_64 binaries
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_ruff.rb
+++ packages/py3_ruff.rb
@@ -14,7 +14,7 @@ class Py3_ruff < Python
     aarch64: 'a62bd705f308cfb203318ff0dc97d2083bcbb1f32c9950458c2e3d94aa7742cb',
      armv7l: 'a62bd705f308cfb203318ff0dc97d2083bcbb1f32c9950458c2e3d94aa7742cb',
        i686: '1b472dcd33eb4b2e27a086efea97cddd6378a659616fcaa689a5a449e3b65790',
-     x86_64: '619ea0dc08acdf0cbdc983970202122821b6f3713858268fff9772616dcf75fa'
+     x86_64: '10d9052912cbac6bde63fe077de3422ce7bf7baefa35e8788ec18e69537cd0aa'
   })
 
   depends_on 'gcc_lib' # R
