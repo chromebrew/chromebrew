c8ad3b1b8aaed1037d7f4e160d59ab6b8375886d
Satadru Pramanik <satadru@gmail.com>
2025-05-08 19:09:17 -0400

    Update py3_urwid x86_64
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_urwid.rb
+++ packages/py3_urwid.rb
@@ -13,7 +13,7 @@ class Py3_urwid < Pip
     aarch64: 'f3a85ed7d90f0ca1dca9808a291859221661b62f3501ec4da972f71f11a5118a',
      armv7l: 'f3a85ed7d90f0ca1dca9808a291859221661b62f3501ec4da972f71f11a5118a',
        i686: '944ce3812ee30f90a54422cbfa3eefccede3679bea06fbed0123456b4bfa0d6b',
-     x86_64: 'd53b6a491471a3970bf5322285d8d75ccd86688fb1cfb5f217c521cd9a6ad1f7'
+     x86_64: '40f114dbfa93b0f074291e731e5251e0ba77dbf4d7e70b77a7ade3e0bea9d7c7'
   })
 
   depends_on 'glibc' # R
