63137fafc24f7a3fc3dd893f71d1b1bdabe0b4fb
Satadru Pramanik <satadru@gmail.com>
2025-05-08 15:58:24 -0400

    fix binary generation conflict
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_pystemmer.rb
+++ packages/py3_pystemmer.rb
@@ -12,13 +12,12 @@ class Py3_pystemmer < Pip
   binary_sha256({
     aarch64: '72e21d7400cd773a2b60bed2f70ef09377361e546e7f1cc6374b53e1628f7507',
      armv7l: '72e21d7400cd773a2b60bed2f70ef09377361e546e7f1cc6374b53e1628f7507',
-       i686: '84cd405c13f2a8ee59506eb4774a5bad9249b7cf7700077d5988441166b2ac79',
+       i686: 'b208e6e20abd18baab0657120420c051dbcb1e80b372a72beb8e17d0c5ed7d03',
      x86_64: '8626bc38307965996af5318aef1a17f9b9ce7c9017d0c0910e1467823709ac65'
   })
 
   depends_on 'glibc' # R
   depends_on 'python3' => :build
 
-  # no_source_build
-  no_compile_needed
+  no_source_build
 end
--- packages/py3_snowballstemmer.rb
+++ packages/py3_snowballstemmer.rb
@@ -10,8 +10,11 @@ class Py3_snowballstemmer < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
+       i686: '1dded22e1ca04a3ec5f453a3d0f4dbf9197b14303cd880596190a29a16d95ad2'
   })
 
   depends_on 'py3_pystemmer'
   depends_on 'python3'
+
+  no_source_build
 end
