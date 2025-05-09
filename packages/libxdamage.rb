16e7655f85713c9a1ee945e21cb31a89741d6bc9
Satadru Pramanik <satadru@gmail.com>
2025-05-09 09:45:59 -0400

    Rebuild libxdamage.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libxdamage.rb
+++ packages/libxdamage.rb
@@ -11,17 +11,12 @@ class Libxdamage < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1acd0ec73124faf374844894ebbaa792e985272284c5ff544c93499c3b6d8e2f',
-     armv7l: '1acd0ec73124faf374844894ebbaa792e985272284c5ff544c93499c3b6d8e2f',
-     x86_64: '122712b7c3630e9d849fa134d94201437da15ff1cda107d9932cc3a39d08d6c4'
+    aarch64: 'aa014235465fda5eec08c134e335d8f20b3ecdb9e78d333f9a377c535b6078f6',
+     armv7l: 'aa014235465fda5eec08c134e335d8f20b3ecdb9e78d333f9a377c535b6078f6',
+     x86_64: '1782de63b4e5ba46ef8d5389bc5dd4bd8ae72a6cf38cd18f8b6d68af227ea5b9'
   })
 
   depends_on 'glibc' # R
-  depends_on 'libbsd' # R
-  depends_on 'libmd' # R
   depends_on 'libx11' # R
-  depends_on 'libxau' # R
-  depends_on 'libxcb' # R
-  depends_on 'libxdmcp' # R
-  depends_on 'libxfixes' # R
+  depends_on 'libxfixes' => :build
 end
