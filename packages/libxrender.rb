b8a1d2fae72ae5fea712cd80ca7c21ac117da21b
Satadru Pramanik <satadru@gmail.com>
2025-05-09 10:03:27 -0400

    Rebuild libxrender
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libxrender.rb
+++ packages/libxrender.rb
@@ -1,36 +1,21 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Libxrender < Package
+class Libxrender < Meson
   description 'X Rendering Extension client library.'
   homepage 'https://www.x.org/wiki/'
-  version '0.9.11'
-  license 'custom'
+  version '0.9.11-01c4bb5'
+  license 'HPND-sell-variant'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.freedesktop.org/xorg/lib/libxrender.git'
-  git_hashtag "libXrender-#{version}"
+  git_hashtag '01c4bb5b30bbb09921ca4042da369a60a73faefa'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '859bf33ee1c94216071d6ef524e3f7ccb977cede86ab483b8936d6ef02ad802f',
-     armv7l: '859bf33ee1c94216071d6ef524e3f7ccb977cede86ab483b8936d6ef02ad802f',
-     x86_64: '247dda42e157a2eb78da0f9408e0cd8281787df630f6bba62e428d02dd31f217'
+    aarch64: '1d5def75a204202215bb57e90f922a84dc095bcf5e305d28fdc4f36504b54bc1',
+     armv7l: '1d5def75a204202215bb57e90f922a84dc095bcf5e305d28fdc4f36504b54bc1',
+     x86_64: '9f8c1d060da336e3d70a3aca14a12c7ceba34a0e37467dd1a616a7cede3d3b46'
   })
 
-  depends_on 'libx11'
   depends_on 'glibc' # R
-  depends_on 'libbsd' # R
-  depends_on 'libmd' # R
-  depends_on 'libxau' # R
-  depends_on 'libxcb' # R
-  depends_on 'libxdmcp' # R
-
-  def self.build
-    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'libx11' # R
 end
