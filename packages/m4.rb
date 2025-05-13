7e15516cff09d45e2154e177622c3dc60aca1c11
Ed Reel <edreel@gmail.com>
2025-05-13 05:59:48 +0000

    M4 1.4.19-1 => 1.4.20

--- packages/m4.rb
+++ packages/m4.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class M4 < Autotools
   description 'GNU M4 is an implementation of the traditional Unix macro processor.'
   homepage 'https://www.gnu.org/software/m4/'
-  version '1.4.19-1'
+  version '1.4.20'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/gnu/m4/m4-1.4.19.tar.xz'
-  source_sha256 '63aede5c6d33b6d9b13511cd0be2cac046f2e70fd0a07aa9573a04a82783af96'
+  source_url "https://ftpmirror.gnu.org/gnu/m4/m4-#{version}.tar.xz"
+  source_sha256 'e236ea3a1ccf5f6c270b1c4bb60726f371fa49459a8eaaebc90b216b328daf2b'
   binary_compression 'tar.zst'
 
   binary_sha256({
