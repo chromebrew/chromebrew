b18b98c8d388368765928cf23bf8fb0a4f2cbdec
satmandu <satmandu@users.noreply.github.com>
2025-05-17 00:28:41 -0400

    Add built packages for linux/arm/v7 to updates-2025-05-17-03-37

--- packages/py3_ruff.rb
+++ packages/py3_ruff.rb
@@ -6,13 +6,12 @@ class Py3_ruff < Pip
   version "0.11.10-#{CREW_PY_VER}"
   license 'GPL-2.0'
   compatibility 'all'
-  source_url 'https://github.com/astral-sh/ruff.git'
-  git_hashtag version.split('-').first
+  source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5c07e55ccfefc1bfc81c10c5520e0dab4e898754ccffb03249d537116f92ac4c',
-     armv7l: '5c07e55ccfefc1bfc81c10c5520e0dab4e898754ccffb03249d537116f92ac4c',
+    aarch64: 'd50d2e1aa60b66e37734eb5f890995dc824a3831ac686996201011ad715749ba',
+     armv7l: 'd50d2e1aa60b66e37734eb5f890995dc824a3831ac686996201011ad715749ba',
        i686: 'e89ae0fd22f77a85a59b091804d1c67799fb682f5a59533a4e4a055b90d735b3',
      x86_64: '36df885fd7b4385220fe35ebf0f754a3e96651c0b4afb41b501cf98e8ac8a2b7'
   })
