6426f374e3b60815895bb9cd607e0fdcf523b703
Satadru Pramanik <satadru@gmail.com>
2025-05-17 00:01:13 -0400

    Add built packages for linux/x86_64 to updates-2025-05-17-03-37
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_ruff.rb
+++ packages/py3_ruff.rb
@@ -1,6 +1,6 @@
-require 'buildsystems/python'
+require 'buildsystems/pip'
 
-class Py3_ruff < Python
+class Py3_ruff < Pip
   description 'An extremely fast Python linter, written in Rust.'
   homepage 'https://docs.astral.sh/ruff'
   version "0.11.10-#{CREW_PY_VER}"
@@ -14,7 +14,7 @@ class Py3_ruff < Python
     aarch64: '5c07e55ccfefc1bfc81c10c5520e0dab4e898754ccffb03249d537116f92ac4c',
      armv7l: '5c07e55ccfefc1bfc81c10c5520e0dab4e898754ccffb03249d537116f92ac4c',
        i686: 'e89ae0fd22f77a85a59b091804d1c67799fb682f5a59533a4e4a055b90d735b3',
-     x86_64: '636a5326dad0de2883e421c02415ef6c8f777a54e699094a6537692ac8bc7687'
+     x86_64: '36df885fd7b4385220fe35ebf0f754a3e96651c0b4afb41b501cf98e8ac8a2b7'
   })
 
   depends_on 'gcc_lib' # R
@@ -22,4 +22,6 @@ class Py3_ruff < Python
   depends_on 'python3'
   depends_on 'py3_maturin' => :build
   depends_on 'rust' => :build
+
+  no_source_build
 end
