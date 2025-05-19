07e964637836fe0c9a558f92a4cb515210cc7077
Satadru Pramanik <satadru@gmail.com>
2025-05-18 22:40:09 -0400

    Update py3_cryptography
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_cryptography.rb
+++ packages/py3_cryptography.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Py3_cryptography < Pip
   description 'Cryptography provides cryptographic recipes and primitives to Python developers.'
   homepage 'https://cryptography.io/'
-  version "45.0.1-#{CREW_PY_VER}"
+  version "45.0.2-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '95bced5388c4f5e09d96c4597c79a52ec653505d4097015401d716cad5739d4b',
-     armv7l: '95bced5388c4f5e09d96c4597c79a52ec653505d4097015401d716cad5739d4b',
-       i686: '349d729a1b1b3c65e020dabd531200964343f2cf50898d7ad6ee81a871aff046',
-     x86_64: '68551f4563fc6312cfc33b0488c587357ecffc65ebe409dc380b918aa43284fc'
+    aarch64: '7728f431a303989a47a7c226535ae7df519919bd123f825bc9560b992404eda2',
+     armv7l: '7728f431a303989a47a7c226535ae7df519919bd123f825bc9560b992404eda2',
+       i686: 'c92608b8551de0f82b66ade47cc15121e46a30aaa79f7a0bd872fd09dedfa772',
+     x86_64: 'a0c75d402b75fc997b0a2be195a3fde475f6509cec5355f84e06ff7ebec440bb'
   })
 
   depends_on 'gcc_lib' # R
