cf9168b4afc4427e6fcf2c9ce2f92b6b032d122f
SupeChicken666 <supechicken666@gmail.com>
2025-05-22 01:40:18 +0800

    iperf3 3.7 => 3.19

--- packages/iperf3.rb
+++ packages/iperf3.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Iperf3 < Package
+class Iperf3 < Autotools
   description 'iPerf3 is a tool for active measurements of the maximum achievable bandwidth on IP networks.'
   homepage 'https://iperf.fr'
-  version '3.7'
+  version '3.19'
   license 'BSD-3'
   compatibility 'all'
-  source_url 'https://github.com/esnet/iperf/archive/3.7.tar.gz'
-  source_sha256 'c349924a777e8f0a70612b765e26b8b94cc4a97cc21a80ed260f65e9823c8fc5'
-  binary_compression 'tar.xz'
+  source_url "https://github.com/esnet/iperf/archive/refs/tags/#{version}.tar.gz"
+  source_sha256 'da5cff29e4945b2ee05dcf9a0c67768cc000dc1b122935bce3492c4e36f6b5e9'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'b58c66a6ac474fff36e1336194fb0e8b9fac68ccd7bcb8b083e85c48d4d7011e',
@@ -17,16 +17,5 @@ class Iperf3 < Package
      x86_64: '790a85d3e616b4e02b05c43c6e2ae31d0f8e100a6020f060279ba7dec427c8f5'
   })
 
-  def self.build
-    system './configure',
-           "--libdir=#{CREW_LIB_PREFIX}",
-           "--prefix=#{CREW_PREFIX}",
-           '--disable-dependency-tracking',
-           '--disable-maintainer-mode'
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  configure_options '--disable-maintainer-mode'
 end
