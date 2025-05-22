3183ebcdf5eca3818af063d0385fcc51025a4ed5
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-05-22 11:50:26 -0400

    Adjust package file for  iperf3.rb

--- packages/iperf3.rb
+++ packages/iperf3.rb
@@ -5,9 +5,9 @@ class Iperf3 < Autotools
   homepage 'https://iperf.fr'
   version '3.19'
   license 'BSD-3'
-  compatibility 'all'
-  source_url "https://github.com/esnet/iperf/archive/refs/tags/#{version}.tar.gz"
-  source_sha256 'da5cff29e4945b2ee05dcf9a0c67768cc000dc1b122935bce3492c4e36f6b5e9'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url 'https://github.com/esnet/iperf.git'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -16,6 +16,4 @@ class Iperf3 < Autotools
        i686: 'ddf4ec2075b93914bb9d05054d825d988d0c539cc520ba7af271e151597b6b94',
      x86_64: '790a85d3e616b4e02b05c43c6e2ae31d0f8e100a6020f060279ba7dec427c8f5'
   })
-
-  configure_options '--disable-maintainer-mode'
 end
