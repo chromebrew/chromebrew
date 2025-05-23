9101ed33512b6b34597c348dff0da32de3e42964
Satadru Pramanik <satadru@gmail.com>
2025-05-23 14:12:08 -0400

    update libavif
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libavif.rb
+++ packages/libavif.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Libavif < CMake
   description 'Library for encoding and decoding .avif files'
   homepage 'https://github.com/AOMediaCodec/libavif'
-  version '1.1.0'
+  version '1.3.0'
   license 'BSD-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/AOMediaCodec/libavif.git'
@@ -11,27 +11,28 @@ class Libavif < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a6632560338310f3909f141763e886af378a1671329d10f9bfd22310c596d50c',
-     armv7l: 'a6632560338310f3909f141763e886af378a1671329d10f9bfd22310c596d50c',
-     x86_64: '2f81bae4159dae77502f9c0c573e5b9a16156640a8c0eb23f93b5769c1c3b969'
+    aarch64: '91029d3d2d8a826af8fa188f06217710a2ed4ab49eb7880a356b0454d5ca2e7d',
+     armv7l: '91029d3d2d8a826af8fa188f06217710a2ed4ab49eb7880a356b0454d5ca2e7d',
+     x86_64: '78252aa3decc765b1488c92d447c2cc43ac629d396ec4cc83ccdeadf142d3278'
   })
 
-  depends_on 'dav1d' # R
+  depends_on 'dav1d' => :build
   depends_on 'gcc_lib' # R
   depends_on 'gdk_pixbuf' # R
   depends_on 'glibc' # R
   depends_on 'glib' # R
-  depends_on 'libaom' # R
+  depends_on 'libaom' => :build
   depends_on 'libjpeg_turbo' # R
   depends_on 'libpng' # R
   depends_on 'libtiff' # L
-  depends_on 'libwebp' # R
+  depends_on 'libwebp' => :build
+  depends_on 'libxml2' # R
   depends_on 'libyuv' # R
   depends_on 'nasm' => :build
   depends_on 'pandoc' => :build
   depends_on 'pkgconf' => :build
-  depends_on 'rav1e' # R
-  depends_on 'svt_av1' # R
+  depends_on 'rav1e' => :build
+  depends_on 'svt_av1' => :build
   depends_on 'zlib' # R
 
   gnome
@@ -42,5 +43,6 @@ class Libavif < CMake
       -DAVIF_CODEC_RAV1E=ON \
       -DAVIF_CODEC_SVT=ON \
       -DAVIF_BUILD_GDK_PIXBUF=ON \
-      -DAVIF_BUILD_MAN_PAGES=ON'
+      -DAVIF_BUILD_MAN_PAGES=OFF \
+      -DAVIF_LIBXML2=SYSTEM'
 end
