59201cf8e2067617e60d14609ddfbbde8326ca40
satmandu <satmandu@users.noreply.github.com>
2025-05-24 15:10:03 +0000

    Add unbuilt updated ruby gem packages to updates-2025-05-24-15-08

--- packages/ruby_drb.rb
+++ packages/ruby_drb.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_drb < RUBY
   description 'Distributed object system for ruby.'
   homepage 'https://github.com/ruby/drb'
-  version "2.2.1-#{CREW_RUBY_VER}"
+  version "2.2.3-#{CREW_RUBY_VER}"
   license 'BSD-2-Clause'
   compatibility 'all'
   source_url 'SKIP'
