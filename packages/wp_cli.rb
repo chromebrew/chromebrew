4e9a9ce483eeb5af21d41962db7abbc5a7f29d28
Ed Reel <edreel@gmail.com>
2025-05-24 19:32:58 -0500

    Wp_cli 2.11.0 => 2.12.0 (#11945)

--- packages/wp_cli.rb
+++ packages/wp_cli.rb
@@ -3,11 +3,11 @@ require 'package'
 class Wp_cli < Package
   description 'The command line interface for WordPress'
   homepage 'https://wp-cli.org/'
-  version '2.11.0'
+  version '2.12.0'
   license 'LGPL-3'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://github.com/wp-cli/wp-cli/releases/download/v#{version}/wp-cli-#{version}.phar"
-  source_sha256 'a39021ac809530ea607580dbf93afbc46ba02f86b6cffd03de4b126ca53079f6'
+  source_sha256 'ce34ddd838f7351d6759068d09793f26755463b4a4610a5a5c0a97b68220d85c'
 
   depends_on 'php83' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
