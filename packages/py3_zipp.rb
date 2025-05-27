dcac668faafbd5f5d66d5182e431ae65672b1191
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-05-27 00:22:06 +0000

    Add unbuilt updated pip packages to updates-2025-05-27-00-21

--- packages/py3_zipp.rb
+++ packages/py3_zipp.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_zipp < Pip
   description 'Zipp is a backport of pathlib-compatible object wrapper for zip files.'
   homepage 'https://github.com/jaraco/zipp/'
-  version "3.21.0-#{CREW_PY_VER}"
+  version "3.22.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
