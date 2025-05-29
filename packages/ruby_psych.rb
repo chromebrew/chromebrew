26dd3f7c916cf43038778b620e22393a4921ca9c
Satadru Pramanik <satadru@gmail.com>
2025-05-29 18:06:15 -0400

    Ruby Psych gem => 5.2.6
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_psych.rb
+++ packages/ruby_psych.rb
@@ -3,17 +3,17 @@ require 'buildsystems/ruby'
 class Ruby_psych < RUBY
   description '| psych is a yaml parser and emitter.'
   homepage 'https://github.com/ruby/psych'
-  version "5.2.4-#{CREW_RUBY_VER}"
+  version "5.2.6-#{CREW_RUBY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'gem'
 
   binary_sha256({
-    aarch64: '2e2f03b3a65ef035f9b878623b33783eb8bda65327c55608d65d5f0d82ef2437',
-     armv7l: '2e2f03b3a65ef035f9b878623b33783eb8bda65327c55608d65d5f0d82ef2437',
-       i686: 'bad042d6fb6384410a6e933ecbe923c165879bbdb40c5eb1e31a267666ad268f',
-     x86_64: 'f6f4246d8934f2d8b9c6cf2ed74a34793cdf78cb01e7d1dbe993ef33eb4082c3'
+    aarch64: 'bd3d40e6b922c5d12fe69c915d7200cd125650bf904524b6824e673fa23aaecc',
+     armv7l: 'bd3d40e6b922c5d12fe69c915d7200cd125650bf904524b6824e673fa23aaecc',
+       i686: 'fd9332dec71d6a9c17170a29d1b06036c00b2288b4fc64297c68fadd00b9fefd',
+     x86_64: 'f968a6f7c4faac0159e7fdcc049364e03f78a841a6a53f19d56e8d8e745ba86c'
   })
 
   depends_on 'ruby_stringio' # R
