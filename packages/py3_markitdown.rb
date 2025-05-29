5285a93295d2f49fb36df497b98e0ce763883805
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-05-29 00:22:51 +0000

    Add unbuilt updated pip packages to updates-2025-05-29-00-21

--- packages/py3_markitdown.rb
+++ packages/py3_markitdown.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_markitdown < Pip
   description 'Python tool for converting files and office documents to Markdown.'
   homepage 'https://github.com/microsoft/markitdown'
-  version "0.1.1-#{CREW_PY_VER}"
+  version "0.1.2-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
