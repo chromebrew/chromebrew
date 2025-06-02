2f01c9ceea9a7873d30f9ef0361a544ef2d2f8db
Satadru Pramanik <satadru@gmail.com>
2025-06-02 17:44:53 -0400

    ruby updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_language_server_protocol.rb
+++ packages/ruby_language_server_protocol.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_language_server_protocol < RUBY
   description 'A Language Server Protocol SDK.'
   homepage 'https://github.com/mtsmfm/language_server-protocol-ruby'
-  version "3.17.0.4-#{CREW_RUBY_VER}"
+  version "3.17.0.5-#{CREW_RUBY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
