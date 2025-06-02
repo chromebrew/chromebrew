84d5cf36d68b45d0b1aed089fe26bea0e1e7e0ac
Satadru Pramanik <satadru@gmail.com>
2025-06-02 16:37:47 -0400

    Update ncurses, readline, bash
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/readline.rb
+++ packages/readline.rb
@@ -3,18 +3,20 @@ require 'package'
 class Readline < Package
   description 'The GNU Readline library provides a set of functions for use by applications that allow users to edit command lines as they are typed in.'
   homepage 'https://tiswww.cwru.edu/php/chet/readline/rltop.html'
-  version '8.3-rc1'
+  version '8.3-rc2'
   license 'GPL-2'
   compatibility 'all'
-  source_url "https://ftpmirror.gnu.org/readline/readline-#{version}.tar.gz"
-  source_sha256 '4df532d8e46e726bb216c63304163a174542cc870109abfe23177fac9e605425'
+  # source_url "https://ftpmirror.gnu.org/readline/readline-#{version}.tar.gz"
+  # source_sha256 '4df532d8e46e726bb216c63304163a174542cc870109abfe23177fac9e605425'
+  source_url 'https://https.git.savannah.gnu.org/git/readline.git'
+  git_hashtag '1f2d3a51df1195446e126c5ebca621d9d81809ea'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f2cfc8d63b451d41775745e249fc9d5c34323f1ec28e44b3e0a03e596b72c307',
-     armv7l: 'f2cfc8d63b451d41775745e249fc9d5c34323f1ec28e44b3e0a03e596b72c307',
-       i686: '3ebb7933e2d68c84911303de7f035e0ebfa3f2a55193166ea911250b1df4fc2c',
-     x86_64: '338cb34141f881537ff58b26d2cb9d25445e08074e0de6fe0d462dc72f127d39'
+    aarch64: '85dff3d0aa6a72f845cb8136d631c10b5efbac27ff62e52b1cf3432a5f350669',
+     armv7l: '85dff3d0aa6a72f845cb8136d631c10b5efbac27ff62e52b1cf3432a5f350669',
+       i686: '1603ee7a4ac94b0d33e5aa75275391d3ec1797c1dc161ae3f332230068913cef',
+     x86_64: '3522e3a49daa2a52ff5dfe2d31162579401d888613dbd34147370270d424ed30'
   })
 
   depends_on 'glibc' # R
