f475f61372cc868d22dcce16b4dc7fbed180013f
Satadru Pramanik <satadru@gmail.com>
2025-06-03 13:32:23 -0400

    updates and rebuilds
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/m4.rb
+++ packages/m4.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class M4 < Autotools
   description 'GNU M4 is an implementation of the traditional Unix macro processor.'
   homepage 'https://www.gnu.org/software/m4/'
-  version '1.4.20'
+  version '1.4.20-1'
   license 'GPL-3'
   compatibility 'all'
-  source_url "https://ftpmirror.gnu.org/gnu/m4/m4-#{version}.tar.xz"
+  source_url "https://ftpmirror.gnu.org/gnu/m4/m4-#{version.split('-').first}.tar.xz"
   source_sha256 'e236ea3a1ccf5f6c270b1c4bb60726f371fa49459a8eaaebc90b216b328daf2b'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '0101268d39289d2b1391ac557f3b79a7f8fb984d754aa3cdcff7e16d87d55189',
-     armv7l: '0101268d39289d2b1391ac557f3b79a7f8fb984d754aa3cdcff7e16d87d55189',
-       i686: '643ba2e3e17488c548bec58c6c898bad54a9fb23dc187e5c156c10880ef0e7d3',
-     x86_64: 'c7536184e670ec649a4233dc2f5f54470472d934cfd73a0acd76ff2891d502dd'
+    aarch64: 'aca57f77f399011bccaeaccd05136fac7d66ba713b72794ed00922808f9731b5',
+     armv7l: 'aca57f77f399011bccaeaccd05136fac7d66ba713b72794ed00922808f9731b5',
+       i686: '0b3dfd39dd009232673eb3277ac8762b4f4d1d156de74ba9691d30d7ceb18f5c',
+     x86_64: '2761b53aa8ead691fc1762671000a5191af930663bd13282d9869e175acee917'
   })
 
   depends_on 'glibc' # R
