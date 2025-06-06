d9e8952a9ceedb079c34f54fb35ebf209b9d02df
Satadru Pramanik <satadru@gmail.com>
2025-06-06 19:32:59 -0400

    lint
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -105,8 +105,7 @@ CREW_WINE_PREFIX      ||= File.join(CREW_LIB_PREFIX, 'wine')
 CREW_DEST_WINE_PREFIX ||= File.join(CREW_DEST_PREFIX, CREW_WINE_PREFIX)
 
 # Local constants for contributors.
-_revparse_stdin, revparse_stdout, _revparse_stderr, _revparse_threads = Open3.popen3('git rev-parse --show-toplevel')
-CREW_LOCAL_REPO_ROOT ||= revparse_stdout.nil? ? revparse_stdout : ''
+CREW_LOCAL_REPO_ROOT ||= `git rev-parse --show-toplevel 2>/dev/null`.chomp
 CREW_LOCAL_BUILD_DIR ||= "#{CREW_LOCAL_REPO_ROOT}/release/#{ARCH}"
 CREW_GITLAB_PKG_REPO ||= 'https://gitlab.com/api/v4/projects/26210301/packages'
 
