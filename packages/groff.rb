6c374c4d6ddeb4b2e1613b15b5602ff67cf12366
Satadru Pramanik <satadru@gmail.com>
2025-06-06 21:18:10 -0400

    Handle ELOOP exception.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/groff.rb
+++ packages/groff.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Groff < Autotools
   description 'Groff (GNU troff) is a typesetting system that reads plain text mixed with formatting commands and produces formatted output.'
   homepage 'https://www.gnu.org/software/groff/'
-  version '1.23.0'
+  version '1.23.0-1'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://git.savannah.gnu.org/git/groff.git'
-  git_hashtag version
+  git_hashtag version.split('-').first
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c3b103307d0dbac5754652a1e17a6baacb3b22bd07856c60a6339b973dd073c5',
-     armv7l: 'c3b103307d0dbac5754652a1e17a6baacb3b22bd07856c60a6339b973dd073c5',
-       i686: '8de28bbe694839b9b657f321a54656d0e5a09669846c37b78b32ce876f120a8c',
-     x86_64: '188ba47477f4e3d468f4ccd91b0915f676d9dddb524991f7f70d81cc824e8213'
+    aarch64: '6c4051837c19bf45308d5f1afd113a14d03efe7479c115e6702e798efc671bb2',
+     armv7l: '6c4051837c19bf45308d5f1afd113a14d03efe7479c115e6702e798efc671bb2',
+       i686: '9d2cb7c9038714c158345a7f7979379c25ed9b78f13e517989887904c70557ee',
+     x86_64: 'c64d5b6b3ff5d80d021d81a35e1b7900e991551bf7b38660ccf3ad47f8b6984c'
   })
 
   depends_on 'gcc_lib' # R
