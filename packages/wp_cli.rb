018e4c98d51ad19ca09d2bfe947862cf2445533b
Satadru Pramanik <satadru@gmail.com>
2025-06-08 09:37:04 -0400

    updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/wp_cli.rb
+++ packages/wp_cli.rb
@@ -8,6 +8,13 @@ class Wp_cli < Package
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://github.com/wp-cli/wp-cli/releases/download/v#{version}/wp-cli-#{version}.phar"
   source_sha256 'ce34ddd838f7351d6759068d09793f26755463b4a4610a5a5c0a97b68220d85c'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '853e6d80abefe05d1a1f671f7df78fbc7ee3104c8f36fc183d5b11f8d45620d3',
+     armv7l: '853e6d80abefe05d1a1f671f7df78fbc7ee3104c8f36fc183d5b11f8d45620d3',
+     x86_64: 'a8f3a8b23c8c52488ce8aacb165cea88db99a9d9eff2680776c46c50846069a7'
+  })
 
   depends_on 'php83' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
