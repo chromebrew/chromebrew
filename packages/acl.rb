8dc25a66cc2d2f390fa2382f8b384bc0401c9fb2
Satadru Pramanik <satadru@gmail.com>
2025-06-10 11:11:57 -0400

    Rebuilds of acl and attr for i686
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/acl.rb
+++ packages/acl.rb
@@ -3,21 +3,20 @@ require 'buildsystems/autotools'
 class Acl < Autotools
   description 'Commands for Manipulating POSIX Access Control Lists.'
   homepage 'https://savannah.nongnu.org/projects/acl'
-  version '2.3.2'
+  version '2.3.2-1'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'https://git.savannah.nongnu.org/cgit/acl.git'
-  git_hashtag "v#{version}"
-  binary_compression 'tar.xz'
+  source_url 'https://git.savannah.gnu.org/git/acl.git'
+  git_hashtag "v#{version.split('-').first}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '653b625434c8575fea35c39ca5e35af609211c7de54bb2544c62d5b9d835bb2c',
      armv7l: '653b625434c8575fea35c39ca5e35af609211c7de54bb2544c62d5b9d835bb2c',
-       i686: '62d6a3760da581987301da062b5da6790eb1f92d8fb5a2b4b54b5cc54293e7d4',
+       i686: '8fdb8c78cb4eec75bab8fa77a94cab4968c018285b0b3b98ff006c702dbf3a22',
      x86_64: '8ac4dfde9ec567c5ff429b4c47a3e63293e9283d304bcf447b40b7f65be3f589'
   })
 
   depends_on 'attr'
-
-  no_zstd
+  depends_on 'glibc' # R
 end
--- packages/attr.rb
+++ packages/attr.rb
@@ -3,21 +3,21 @@ require 'buildsystems/autotools'
 class Attr < Autotools
   description 'Commands for Manipulating Filesystem Extended Attributes.'
   homepage 'https://savannah.nongnu.org/projects/attr'
-  version '2.5.2'
+  version '2.5.2-4d4a562'
   license 'LGPL-2.1'
   compatibility 'all'
   source_url 'https://git.savannah.gnu.org/git/attr.git'
-  git_hashtag "v#{version}"
-  binary_compression 'tar.xz'
+  git_hashtag '4d4a562d7b73dcea8bd4b2d40b8c030d5065111b'
+  # git_hashtag "v#{version.split('-').first}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '6730b3d290f8aaea61bbe814606910e2b24ec1fc1dc1f751a7820c4ba3915be5',
      armv7l: '6730b3d290f8aaea61bbe814606910e2b24ec1fc1dc1f751a7820c4ba3915be5',
-       i686: '15f296a5eeb14d80ffb85b6f38c10112fa5aab3d3caaceb6f250d49da51bf8f7',
+       i686: '9ef95add0044d6d1681132eeb371c3214768af5caf9b25aaa8aa1fe7fa483ec4',
      x86_64: '97ebf045a15e003b21a35401d8f7b5c40f6282840fdc10bdcba2def3bbfbb714'
   })
 
+  depends_on 'glibc' # R
   depends_on 'libcap' => :build
-
-  no_zstd
 end
