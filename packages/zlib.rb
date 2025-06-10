31111440f40953cd41deb72c3e7328cbd44b546f
Satadru Pramanik <satadru@gmail.com>
2025-06-10 09:50:13 -0400

    Rebuild zlib
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/zlib.rb
+++ packages/zlib.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Zlib < CMake
   description 'zlib is a massively spiffy yet delicately unobtrusive compression library.'
   homepage 'https://zlib.net'
-  version '1.3.1-2' # Do not use @_ver here, it will break the installer.
+  version '1.3.1-3' # Do not use @_ver here, it will break the installer.
   # When upgrading zlib, be sure to upgrade minizip in tandem.
   # The following breaks the installer script.
   # puts "#{self} version differs from Minizip version #{Minizip.version}".orange if @_ver != Minizip.version
@@ -11,17 +11,16 @@ class Zlib < CMake
   compatibility 'all'
   source_url "https://www.zlib.net/zlib-#{version.split('-').first}.tar.gz"
   source_sha256 '9a93b2b7dfdac77ceba5a558a580e74667dd6fede4585b91eefb60f03b72df23'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b78cc677fde2ccc7464e06076db20b0a8999d0dd165447e5fd9d07b68d337f45',
-     armv7l: 'b78cc677fde2ccc7464e06076db20b0a8999d0dd165447e5fd9d07b68d337f45',
-       i686: 'd9ca99faf6633b9aa675bb721b89c0dd4aabaddcda3746c4dfad1e54b8437c2d',
-     x86_64: '4018bdcde1651a846f079b8c4f9f3f4a9c9da260d5caf9329da3a6d0b1ac6fdb'
+    aarch64: '91195628001afc8a237774386e6ca494fa820f073985355544a0bb177389d7aa',
+     armv7l: '91195628001afc8a237774386e6ca494fa820f073985355544a0bb177389d7aa',
+       i686: '463a48ec367bc9920178643139a3eabe3c648ed513f35c18c5e28ea5040f664b',
+     x86_64: 'dde89b4ba95675c8e3fa91123f6c5b35712571970b5ddcae6f3d25d25162b1b2'
   })
 
   depends_on 'glibc' # R
-  no_zstd
 
   cmake_options "#{CREW_CMAKE_OPTIONS.gsub('-mfpu=vfpv3-d16', '-mfpu=neon-fp16')} -Wno-dev"
 
