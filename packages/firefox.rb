38b31c0b99495fa33b2084864c34925d13966269
Ed Reel <edreel@gmail.com>
2025-06-11 19:28:00 -0500

    Firefox 139.0.1 => 139.0.4
    Move to fhs compliant directory

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,12 +3,12 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '139.0.1'
+  version '139.0.4'
   license 'MPL-2.0, GPL-2 and LGPL-2.1'
   compatibility 'x86_64'
   min_glibc '2.35'
   source_url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.xz"
-  source_sha256 'f1a264f61d41700e8fcfa2310478d0c909655399a413072664fddaf612b53c07'
+  source_sha256 'fe22686f09a0c46390fea430b26cccc323d92af26869699f394f25b5e71527a3'
 
   depends_on 'at_spi2_core'
   depends_on 'cairo'
@@ -37,12 +37,11 @@ class Firefox < Package
 
   no_compile_needed
   no_shrink
-  no_fhs
 
   def self.build
     File.write 'firefox_sh', <<~FIREFOX_SH
       #!/bin/bash
-      GDK_BACKEND=x11 #{CREW_PREFIX}/firefox/firefox "\$@"
+      GDK_BACKEND=x11 #{CREW_PREFIX}/share/firefox/firefox "$@"
     FIREFOX_SH
     # For some reason, the binaries do not have a desktop file so add it here.
     File.write 'firefox_desktop', <<~FIREFOX_DESKTOP
@@ -63,7 +62,7 @@ class Firefox < Package
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/firefox"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/firefox"
     icon_base_path = "#{CREW_DEST_PREFIX}/share/icons/hicolor"
     FileUtils.mkdir_p "#{icon_base_path}/16x16/apps"
     FileUtils.mkdir_p "#{icon_base_path}/32x32/apps"
@@ -71,7 +70,7 @@ class Firefox < Package
     FileUtils.mkdir_p "#{icon_base_path}/64x64/apps"
     FileUtils.mkdir_p "#{icon_base_path}/128x128/apps"
     FileUtils.mkdir_p "#{icon_base_path}/256x256/apps"
-    FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/firefox"
+    FileUtils.cp_r Dir['.'], "#{CREW_DEST_PREFIX}/share/firefox"
     FileUtils.install 'firefox_sh', "#{CREW_DEST_PREFIX}/bin/firefox", mode: 0o755
     FileUtils.install 'firefox_desktop', "#{CREW_DEST_PREFIX}/share/applications/firefox.desktop", mode: 0o644
     Dir.chdir 'browser/chrome/icons/default' do
@@ -88,26 +87,11 @@ class Firefox < Package
   end
 
   def self.postinstall
-    print "\nSet Firefox as your default browser? [Y/n]: "
-    case $stdin.gets.chomp.downcase
-    when '', 'y', 'yes'
-      Dir.chdir("#{CREW_PREFIX}/bin") do
-        FileUtils.ln_sf 'firefox', 'x-www-browser'
-      end
-      puts 'Firefox is now your default browser.'.lightgreen
-    else
-      puts 'No change has been made.'.orange
-    end
-    ExitMessage.add "\nType 'firefox' to get started.\n"
+    ConvenienceFunctions.set_default_browser('Firefox', 'firefox')
   end
 
   def self.preremove
-    Dir.chdir("#{CREW_PREFIX}/bin") do
-      if File.exist?('x-www-browser') && File.symlink?('x-www-browser') &&
-         File.realpath('x-www-browser') == "#{CREW_PREFIX}/bin/firefox"
-        FileUtils.rm "#{CREW_PREFIX}/bin/x-www-browser"
-      end
-    end
+    ConvenienceFunctions.unset_default_browser('Firefox', 'firefox')
   end
 
   def self.postremove
