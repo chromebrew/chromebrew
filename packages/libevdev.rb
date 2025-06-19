4632d6c44ee360fdde88d0b03c2e4c964c47ee46
supechicken <supechicken@users.noreply.github.com>
2025-06-19 14:35:14 +0000

    Add built packages for linux/arm/v7 to update-libevdev

--- packages/libevdev.rb
+++ packages/libevdev.rb
@@ -11,8 +11,8 @@ class Libevdev < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ef21c213c561863ccb9cea8a5b88501fdec31c094af3238bac482b3db7b4b053',
-     armv7l: 'ef21c213c561863ccb9cea8a5b88501fdec31c094af3238bac482b3db7b4b053',
+    aarch64: '7cdc504498a4d6da831ebfb36a7b3112d029b96ccaad4166c92a0f556cd9d89e',
+     armv7l: '7cdc504498a4d6da831ebfb36a7b3112d029b96ccaad4166c92a0f556cd9d89e',
        i686: '69e54f648be62110a5a488c39b5bdd9c0957ea4705a25d82d8e6cde86f704458',
      x86_64: '3c285862c00dbbdfd19f88cc9a9cdb5c57580844a5db46c4b4155dae9618f08a'
   })
