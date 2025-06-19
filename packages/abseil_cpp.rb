ed42255c7f304b3c3eb7a45beb71d9a9cb8bc46d
SupeChicken666 <supechicken666@gmail.com>
2025-06-20 02:02:11 +0800

    abseil_cpp => 20250512.1

--- packages/abseil_cpp.rb
+++ packages/abseil_cpp.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Abseil_cpp < CMake
   description 'Abseil Common Libraries C++'
   homepage 'https://abseil.io/'
-  version '20250127.1'
+  version '20250512.1'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'https://github.com/abseil/abseil-cpp.git'
