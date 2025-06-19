0bc6c7faf772ccdf46ffb4c6da4d361a69073104
supechicken <supechicken@users.noreply.github.com>
2025-06-19 17:29:04 +0000

    Add built packages for linux/arm/v7 to update-libmicrohttpd

--- packages/libmicrohttpd.rb
+++ packages/libmicrohttpd.rb
@@ -11,8 +11,8 @@ class Libmicrohttpd < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b2623ef36b1587c1d3e6f7821d4d9d016b7cadbaf6a0442fd0f055dd69338925',
-     armv7l: 'b2623ef36b1587c1d3e6f7821d4d9d016b7cadbaf6a0442fd0f055dd69338925',
+    aarch64: 'c9b72083e688cb547d069a1d8796751beb085ff47eb6535fcfa6fbfd5b8e4007',
+     armv7l: 'c9b72083e688cb547d069a1d8796751beb085ff47eb6535fcfa6fbfd5b8e4007',
        i686: '0f3b8b82cf4f9707c877f2ca7758a5ad148c6ba24b892fb2a26276f3cc4d62bc',
      x86_64: '2f817d93c17190c753485e35df89b1f23da6c3b10c57e21610b13ff7a84ccdda'
   })
