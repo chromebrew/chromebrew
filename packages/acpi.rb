2069ba9e45cb11f9d0004d87a5cff4ec4742cb21
SupeChicken666 <supechicken666@gmail.com>
2025-06-20 02:08:57 +0800

    acpi => 1.8

--- packages/acpi.rb
+++ packages/acpi.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Acpi < Package
+class Acpi < Autotools
   description 'Attempts to replicate the functionality of the \'old\' apm command on ACPI systems, including battery and thermal information.'
   homepage 'https://sourceforge.net/projects/acpiclient/'
-  version '1.7'
+  version '1.8'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://downloads.sourceforge.net/project/acpiclient/acpiclient/1.7/acpi-1.7.tar.gz'
-  source_sha256 'd7a504b61c716ae5b7e81a0c67a50a51f06c7326f197b66a4b823de076a35005'
-  binary_compression 'tar.xz'
+  source_url "https://downloads.sourceforge.net/project/acpiclient/acpiclient/#{version}/acpi-#{version}.tar.gz"
+  source_sha256 'ed61d20400c4fd3965dde9b49ab3ac74da02f8eca1a55454f7a1ac3fa1fd5c31'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '86f608f45ef9c7d423f26f4b1a268e7b995061e1ecb613bf3fbca2d9c7bf5665',
@@ -16,13 +16,4 @@ class Acpi < Package
        i686: 'bbb6d7c0db9e4777aaca3b38e08affc74f16af661274b2d962ef8189a5338d2f',
      x86_64: 'db69280f951b024431221a7f28af55b9f103baa82a981619a3da01c161c2d485'
   })
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
