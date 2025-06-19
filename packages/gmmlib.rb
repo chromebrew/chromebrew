9949fc2734e838f722b36f21664e501185cef3f5
SupeChicken666 <supechicken666@gmail.com>
2025-06-19 23:09:51 +0800

    gmmlib => 22.7.2

--- packages/gmmlib.rb
+++ packages/gmmlib.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Gmmlib < Package
+class Gmmlib < CMake
   description 'The Intel(R) Graphics Memory Management Library provides device specific and buffer management for the Intel(R) Graphics Compute Runtime for OpenCL(TM) and the Intel(R) Media Driver for VAAPI.'
   homepage 'https://github.com/intel/gmmlib/'
-  version '22.3.3'
+  version '22.7.2'
   license 'MIT'
   compatibility 'x86_64'
   source_url 'https://github.com/intel/gmmlib.git'
@@ -17,20 +17,4 @@ class Gmmlib < Package
   depends_on 'libva' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
-
-  def self.build
-    FileUtils.mkdir('builddir')
-    Dir.chdir('builddir') do
-      system "cmake -G Ninja \
-        #{CREW_CMAKE_OPTIONS} \
-        -DRUN_TEST_SUITE=OFF \
-        -Wno-dev \
-        .."
-    end
-    system 'ninja -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
 end
