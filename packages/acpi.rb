e4ce1b58b4f1d5af2849d44f2d82ff516b052a14
supechicken <supechicken@users.noreply.github.com>
2025-06-19 18:13:39 +0000

    Add built packages for linux/amd64 to update-acpi

--- packages/acpi.rb
+++ packages/acpi.rb
@@ -14,7 +14,7 @@ class Acpi < Autotools
     aarch64: '86f608f45ef9c7d423f26f4b1a268e7b995061e1ecb613bf3fbca2d9c7bf5665',
      armv7l: '86f608f45ef9c7d423f26f4b1a268e7b995061e1ecb613bf3fbca2d9c7bf5665',
        i686: 'fc511f646db0b0ed8f553dae69a3176bb48ddf30f434faed31be8def9078b13b',
-     x86_64: 'db69280f951b024431221a7f28af55b9f103baa82a981619a3da01c161c2d485'
+     x86_64: '18af9268c928bf8a50711cdc8dee48c8d6271de65a5c5a963eb616acba7255c0'
   })
 
   depends_on 'glibc' # R
