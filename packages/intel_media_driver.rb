fd034e6d544a5f7739a73550da683293ef473d00
SupeChicken666 <me@supechicken666.dev>
2025-06-19 23:21:50 +0800

    Revert "intel_media_driver => 25.2.5"
    This reverts commit 353d3cc701a595ded2bf912aa498ad44b9b4f999.

--- packages/intel_media_driver.rb
+++ packages/intel_media_driver.rb
@@ -1,9 +1,9 @@
-require 'buildsystems/cmake'
+require 'package'
 
-class Intel_media_driver < CMake
+class Intel_media_driver < Package
   description 'The Intel(R) Media Driver for VAAPI is a new VA-API (Video Acceleration API) user mode driver supporting hardware accelerated decoding, encoding, and video post processing for GEN based graphics hardware.'
   homepage 'https://github.com/intel/media-driver'
-  version '25.2.5'
+  version '22.6.6'
   license 'BSD-3, and MIT'
   compatibility 'x86_64'
   source_url 'https://github.com/intel/media-driver.git'
@@ -19,14 +19,28 @@ class Intel_media_driver < CMake
   depends_on 'gmmlib' # R
   depends_on 'libva' # R
 
-  cmake_options '-DINSTALL_DRIVER_SYSCONF=OFF -DMEDIA_BUILD_FATAL_WARNINGS=OFF -Wno-dev'
+  #  def self.preflight
+  #    abort 'Not an Intel processor, aborting.'.lightred unless CREW_IS_INTEL
+  #  end
 
-  cmake_build_extras do
+  def self.build
+    FileUtils.mkdir('builddir')
+    Dir.chdir('builddir') do
+      system "cmake #{CREW_CMAKE_OPTIONS.sub('-pipe', '-pipe -Wno-error')} \
+      -DMEDIA_BUILD_FATAL_WARNINGS=OFF \
+      ../ -G Ninja"
+    end
+    system 'ninja -C builddir'
+  end
+
+  def self.install
+    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
+
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
-
-    File.write "#{CREW_DEST_PREFIX}/etc/env.d/intel_media_driver", <<~CONFIG_EOF
+    @env = <<~CONFIG_EOF
       # intel_media_driver configuration
       export LIBVA_DRIVER_NAME=iHD
     CONFIG_EOF
+    File.write("#{CREW_DEST_PREFIX}/etc/env.d/intel_media_driver", @env)
   end
 end
