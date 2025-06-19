a869932c7b605cd804dd13fe15ee58ee7250ed39
supechicken <supechicken@users.noreply.github.com>
2025-06-19 14:29:34 +0000

    Add built packages for linux/386 to update-libevdev

--- packages/libevdev.rb
+++ packages/libevdev.rb
@@ -13,11 +13,12 @@ class Libevdev < Meson
   binary_sha256({
     aarch64: 'ef21c213c561863ccb9cea8a5b88501fdec31c094af3238bac482b3db7b4b053',
      armv7l: 'ef21c213c561863ccb9cea8a5b88501fdec31c094af3238bac482b3db7b4b053',
-       i686: 'c950e9e171bb65513f22ed76a1195caa4706cde60397f4ba0e1d0c213e9edd99',
+       i686: '69e54f648be62110a5a488c39b5bdd9c0957ea4705a25d82d8e6cde86f704458',
      x86_64: '175b179c49706e118bdb1da0dfa4d216271396eecf3bd289f0db6fc11f631a64'
   })
 
   depends_on 'check' => :build
   depends_on 'doxygen' => :build
   depends_on 'python3' => :build
+  depends_on 'glibc' # R
 end
