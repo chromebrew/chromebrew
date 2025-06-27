73e01e4b420d55cb8390cf77954282d031a54873
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-06-27 00:23:30 +0000

    Add unbuilt updated pip packages to updates-2025-06-27-00-22

--- packages/py3_ruff.rb
+++ packages/py3_ruff.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_ruff < Pip
   description 'An extremely fast Python linter, written in Rust.'
   homepage 'https://docs.astral.sh/ruff'
-  version "0.12.0-#{CREW_PY_VER}"
+  version "0.12.1-#{CREW_PY_VER}"
   license 'GPL-2.0'
   compatibility 'all'
   source_url 'SKIP'
