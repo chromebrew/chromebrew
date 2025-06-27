a01ff034827861fa2d9a9dacba55db28f53604a5
Ed Reel <edreel@gmail.com>
2025-06-27 12:23:49 -0500

    Mullvad 14.5.3 => 14.5.4 (#12114)
    Co-authored-by: Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>

--- packages/mullvad.rb
+++ packages/mullvad.rb
@@ -4,11 +4,11 @@ require 'convenience_functions'
 class Mullvad < Package
   description 'Privacy-focused browser'
   homepage 'https://mullvad.net/browser'
-  version '14.5.3'
+  version '14.5.4'
   license 'Mozilla Public License V2'
   compatibility 'x86_64'
   source_url "https://github.com/mullvad/mullvad-browser/releases/download/#{version}/mullvad-browser-linux-x86_64-#{version}.tar.xz"
-  source_sha256 '5b4d392e4830f7ab18b1e07c2c113be9befe471302e6f35bd7edcaac3a7c204d'
+  source_sha256 '0c911cfb61891f11b8f5eb95a701fc87fc8ba11e83567d1ad195054b8dbd5da0'
 
   depends_on 'gtk3'
   depends_on 'gdk_base'
