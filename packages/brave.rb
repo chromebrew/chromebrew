63bdc6db21a4d095c79f1be8b54a1c247204beb9
Ed Reel <edreel@gmail.com>
2025-07-02 03:53:14 -0500

    Brave 1.80.113 => 1.80.115

--- packages/brave.rb
+++ packages/brave.rb
@@ -4,12 +4,12 @@ require 'convenience_functions'
 class Brave < Package
   description 'Next generation Brave browser for macOS, Windows, Linux, Android.'
   homepage 'https://brave.com/'
-  version '1.80.113'
+  version '1.80.115'
   license 'MPL-2'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/brave/brave-browser/releases/download/v#{version}/brave-browser-#{version}-linux-amd64.zip"
-  source_sha256 '9527f97f823f0ce67d4ef127b61158a01850b710dc41c9d0514d1c4a337e0412'
+  source_sha256 '6d66cf5d2b4eda48d12e20b3f992e71fef11a9e8b6da86874f1405b49eb0e446'
 
   no_compile_needed
   no_shrink
