30580293854c5a5e12de8376fca9b501679a2f78
Satadru Pramanik <satadru@gmail.com>
2025-07-05 12:29:19 -0400

    Update psl
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libpsl.rb
+++ packages/libpsl.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Libpsl < Meson
   description 'C library for the Public Suffix List'
   homepage 'https://github.com/rockdaboot/libpsl'
-  version '0.21.5-1'
+  version '0.21.5-2'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/rockdaboot/libpsl.git'
@@ -13,11 +13,16 @@ class Libpsl < Meson
   binary_sha256({
     aarch64: '56fd7e35ce2706b2b4cf60a74b106ea0b923d14db34ad24e478f72bc070e9160',
      armv7l: '56fd7e35ce2706b2b4cf60a74b106ea0b923d14db34ad24e478f72bc070e9160',
-       i686: '7bdb977732ba2994d809ca9eff9623f5d81c6642e03f7870887de5bc8cea1f38',
+       i686: '8caf19357b4bb6303be7fe58cb42397978ef8179644efab1e33972c3c788c501',
      x86_64: '56c4274e773e48dccb6c6a0fa5f81f35b90794c63b121b252547daa1c473c797'
   })
 
   depends_on 'glibc' # R
   depends_on 'libidn2' # R
   depends_on 'libunistring' # R
+
+  def self.patch
+    # Update the PSL during build.
+    downloader 'https://github.com/publicsuffix/list/raw/refs/heads/main/public_suffix_list.dat', 'SKIP', 'list/public_suffix_list.dat'
+  end
 end
