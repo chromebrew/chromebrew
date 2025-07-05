df2c630a81f609c326fc401ca475abde9f484d93
satmandu <satmandu@users.noreply.github.com>
2025-07-05 23:19:34 +0000

    Add built packages for linux/amd64 to expat

--- packages/expat.rb
+++ packages/expat.rb
@@ -14,7 +14,7 @@ class Expat < CMake
     aarch64: 'eecda9a8b2b6958c8f7fa9cc0b8d4c02583ad5f8f2645af86a90cb14d0c28cf3',
      armv7l: 'eecda9a8b2b6958c8f7fa9cc0b8d4c02583ad5f8f2645af86a90cb14d0c28cf3',
        i686: 'c218b06e2ce410241fca0ed6601d168fbd4c9d1b6e70fc78317763bea18472db',
-     x86_64: '0e522946e938d9b929d7936e6c49da49f81a56935514590b97807d120ab3c4e9'
+     x86_64: '5b19dbd1ae39faf8561e29b9bc5ec4e8e8016d77278a806549fbb53412d6b2d4'
   })
 
   depends_on 'docbook2x' => :build
