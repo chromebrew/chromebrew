8a159225508597ddc95e4e80178e3989277982ca
Satadru Pramanik <satadru@gmail.com>
2025-07-05 10:25:32 -0400

    pcre2 => 10.45
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/pcre2.rb
+++ packages/pcre2.rb
@@ -1,13 +1,13 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Pcre2 < Package
+class Pcre2 < CMake
   description 'The PCRE2 package contains a new generation of the Perl Compatible Regular Expression libraries.'
   homepage 'http://pcre.org/'
-  version '10.42'
+  version '10.45'
   license 'BSD-3'
   compatibility 'all'
-  source_url 'https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.42/pcre2-10.42.tar.gz'
-  source_sha256 'c33b418e3b936ee3153de2c61cc638e7e4fe3156022a5c77d0711bcbb9d64f1f'
+  source_url 'https://github.com/PCRE2Project/pcre2.git'
+  git_hashtag "pcre2-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -17,32 +17,15 @@ class Pcre2 < Package
      x86_64: 'a25406089eec4ef61249c02927b2af2f20602eee7389409739041d5c777942f4'
   })
 
+  depends_on 'bzip2' # R
   depends_on 'glibc' # R
   depends_on 'readline' # R
-  depends_on 'bzip2' # R
   depends_on 'zlib' # R
 
-  def self.build
-    system "cmake -B builddir\
-      -G Ninja \
-      #{CREW_CMAKE_OPTIONS} \
-      -DPCRE2_BUILD_TESTS=OFF \
+  cmake_options '-DPCRE2_BUILD_TESTS=OFF \
       -DBUILD_SHARED_LIBS=ON \
       -DPCRE2_SUPPORT_JIT=ON \
       -DPCRE2_STATIC_PIC=ON \
       -DPCRE2_BUILD_PCRE2_16=ON \
-      -DPCRE2_BUILD_PCRE2_32=ON \
-      -Wno-dev"
-    system "#{CREW_NINJA} -C builddir"
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
-    FileUtils.mkdir_p CREW_DEST_MAN_PREFIX
-    FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}/cmake/pcre2"
-    FileUtils.mv Dir.glob("#{CREW_DEST_PREFIX}/man/*"), "#{CREW_DEST_MAN_PREFIX}/"
-    FileUtils.rmdir "#{CREW_DEST_PREFIX}/man"
-    FileUtils.mv Dir.glob("#{CREW_DEST_PREFIX}/cmake/*"), "#{CREW_DEST_LIB_PREFIX}/cmake/pcre2/"
-    FileUtils.rmdir "#{CREW_DEST_PREFIX}/cmake"
-  end
+      -DPCRE2_BUILD_PCRE2_32=ON'
 end
