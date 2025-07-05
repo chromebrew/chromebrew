f5f75929c221aa6e8b25210911f6ba6cb6d83e48
satmandu <satmandu@users.noreply.github.com>
2025-07-05 17:05:32 +0000

    Add built packages for linux/arm/v7 to openldap

--- packages/openldap.rb
+++ packages/openldap.rb
@@ -11,8 +11,8 @@ class Openldap < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f48ecd71d964e7c422a5f06a602c3ea49545dffa774fe4cfb89bb5d475a5490d',
-     armv7l: 'f48ecd71d964e7c422a5f06a602c3ea49545dffa774fe4cfb89bb5d475a5490d',
+    aarch64: 'd8bb5325e395c903edeb8618df08d0ef66e7d3b06709e3826b918b3676dbf537',
+     armv7l: 'd8bb5325e395c903edeb8618df08d0ef66e7d3b06709e3826b918b3676dbf537',
        i686: 'c2b276a5f1f926d5132606cd38737f327525308c50e76ead03bc54d1c64d19a1',
      x86_64: '85636fa7fe52809a39b22c2bf69783fdc6a677bbb129da387d37426e354344e0'
   })
