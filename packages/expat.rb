42fdd9c328c89d59016053af7e85ae8d85da746c
satmandu <satmandu@users.noreply.github.com>
2025-07-05 15:08:04 +0000

    Add built packages for linux/386 to expat

--- packages/expat.rb
+++ packages/expat.rb
@@ -13,7 +13,7 @@ class Expat < CMake
   binary_sha256({
     aarch64: 'eecda9a8b2b6958c8f7fa9cc0b8d4c02583ad5f8f2645af86a90cb14d0c28cf3',
      armv7l: 'eecda9a8b2b6958c8f7fa9cc0b8d4c02583ad5f8f2645af86a90cb14d0c28cf3',
-       i686: 'b1147c02b294454066f7edb0c466e7e2c0f9caad4eeaf02b996059bc0720c756',
+       i686: 'b21ce4a13d49b7ec551570c5345d05c7ad4a9274e3e6ba81a4edacd14a1af70f',
      x86_64: '0e522946e938d9b929d7936e6c49da49f81a56935514590b97807d120ab3c4e9'
   })
 
