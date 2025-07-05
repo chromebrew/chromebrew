a171ba0aff56b5982f55f12745595591d74bc27e
Satadru Pramanik <satadru@gmail.com>
2025-07-05 10:20:17 -0400

    psmisc => 23.7
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/psmisc.rb
+++ packages/psmisc.rb
@@ -1,13 +1,13 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Psmisc < Package
+class Psmisc < Autotools
   description 'PSmisc is a set of some small useful utilities that use the proc filesystem.'
   homepage 'https://gitlab.com/psmisc/psmisc'
-  version '23.6'
+  version '23.7'
   license 'GPL-2'
   compatibility 'all'
-  source_url "https://gitlab.com/psmisc/psmisc/-/archive/v#{version}/psmisc-v#{version}.tar.bz2"
-  source_sha256 '91573ca0a1a50bd491b7c3cbe400126b0dadef9a2e328030d6469bb2448e0794'
+  source_url 'https://gitlab.com/psmisc/psmisc.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -20,14 +20,4 @@ class Psmisc < Package
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'ncurses' # R
-
-  def self.build
-    system './autogen.sh'
-    system "CFLAGS+=' -I#{CREW_PREFIX}/include/ncurses' ./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
