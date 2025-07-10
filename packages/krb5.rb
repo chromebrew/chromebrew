9d8504f8edef74ebb81324d78198013eebabb3b5
Satadru Pramanik <satadru@gmail.com>
2025-07-10 13:22:32 -0400

    krb5 => 1.21.3
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/krb5.rb
+++ packages/krb5.rb
@@ -3,11 +3,11 @@ require 'package'
 class Krb5 < Package
   description 'Kerberos is a network authentication protocol.'
   homepage 'https://web.mit.edu/kerberos/'
-  version '1.21.2'
+  version '1.21.3'
   license 'openafs-krb5-a, BSD, MIT, OPENLDAP, BSD-2, HPND, BSD-4, ISC, RSA, CC-BY-SA-3.0 and BSD-2 or GPL-2+ )'
   compatibility 'all'
-  source_url 'https://web.mit.edu/kerberos//dist/krb5/1.21/krb5-1.21.2.tar.gz'
-  source_sha256 '9560941a9d843c0243a71b17a7ac6fe31c7cebb5bce3983db79e52ae7e850491'
+  source_url 'https://web.mit.edu/kerberos/dist/krb5/1.21/krb5-1.21.3.tar.gz'
+  source_sha256 'b7a4cd5ead67fb08b980b21abd150ff7217e85ea320c9ed0c6dadd304840ad35'
   binary_compression 'tar.zst'
 
   binary_sha256({
