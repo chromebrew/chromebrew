72adb8debb0ae27ec1096170de10be08e2333685
Satadru Pramanik <satadru@gmail.com>
2025-07-15 13:41:35 -0400

    adjust build
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/psmisc.rb
+++ packages/psmisc.rb
@@ -23,7 +23,7 @@ class Psmisc < Autotools
   depends_on 'glibc' # R
   depends_on 'ncurses' # R
 
-  no_lto unless ARCH == 'x86_64'
+  no_lto if ARCH == 'x86_64'
 
   autotools_pre_configure_options "CFLAGS+=' -I#{CREW_PREFIX}/include/ncurses'"
   autotools_configure_options '--disable-statx'
