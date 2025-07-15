6b9759bf4e66c68b164d4cb5f5e3a2c0679973fd
satmandu <satmandu@users.noreply.github.com>
2025-07-15 19:13:10 +0000

    Add built packages for linux/arm/v7 to branch sqlite.

--- packages/sqlite.rb
+++ packages/sqlite.rb
@@ -11,8 +11,8 @@ class Sqlite < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '7607848e3e335925b7f93ec9b05b625986cca2e6cd1cea843de397ae84d67c4f',
-     armv7l: '7607848e3e335925b7f93ec9b05b625986cca2e6cd1cea843de397ae84d67c4f',
+    aarch64: 'e34ccf2a5d6458959a8a4ff59a480ad6125685d00a52480cbd41e4955e8b4c9d',
+     armv7l: 'e34ccf2a5d6458959a8a4ff59a480ad6125685d00a52480cbd41e4955e8b4c9d',
        i686: '2e66a04a245e2e25b62ddff2e4fdec7129fcaa42f6e5a8813cbb5a95ddb017ff',
      x86_64: '3c1a3bde311155b4d46c525eded402e4a93e887e23b3501a151da5f1fe1d06a5'
   })
@@ -22,6 +22,7 @@ class Sqlite < Autotools
   depends_on 'readline' => :build
   depends_on 'zlib' # R
   depends_on 'ncurses' # R
+  depends_on 'gcc_lib' # R
 
   autotools_configure_options '--enable-shared \
     --enable-editline \
