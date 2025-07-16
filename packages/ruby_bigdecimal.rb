eadd0a62e6d33274d2a8151e81bb6362e7ce68b3
satmandu <satmandu@users.noreply.github.com>
2025-07-16 17:12:16 +0000

    Add built packages for linux/amd64 to branch bigdecimal.

--- packages/ruby_bigdecimal.rb
+++ packages/ruby_bigdecimal.rb
@@ -13,7 +13,7 @@ class Ruby_bigdecimal < RUBY
     aarch64: 'b0d23dfb1d20da3bb43b0be1e48c1ef73e2017aee6dbabd3de6b72e88bec8f23',
      armv7l: 'b0d23dfb1d20da3bb43b0be1e48c1ef73e2017aee6dbabd3de6b72e88bec8f23',
        i686: '97d40beb3af93e11cfe2bd5f3d2ca7faedaf61a6f50ef88a4edc2d6762673068',
-     x86_64: '3d6a052a3a09e584d55fa12f4c9f4f71250ced05830fdccc430dbdf3cd5518dc'
+     x86_64: '194605ae2247af5799cdd70c521d207fb63a0da72a623cfeb5070309e1e9e06d'
   })
 
   conflicts_ok
