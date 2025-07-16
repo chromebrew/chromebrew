1381207534c99184f418104a49fae1e0683ed14e
satmandu <satmandu@users.noreply.github.com>
2025-07-16 16:52:18 +0000

    Add built packages for linux/386 to branch libxml2.

--- packages/libxslt.rb
+++ packages/libxslt.rb
@@ -13,16 +13,14 @@ class Libxslt < CMake
   binary_sha256({
     aarch64: '79a84b35213937c568b715b859049a1d4555f989ba1379a7328e106c385ea502',
      armv7l: '79a84b35213937c568b715b859049a1d4555f989ba1379a7328e106c385ea502',
-       i686: '086d2c221318809d333c9424ee7714843975ecedab93efd2b7fed3254cc0b731',
+       i686: '68202b3dce43263f8fb736709f784d23b61778c52dff446b31b96377a5b72309',
      x86_64: '57ea96a35e54f1c0c26645e847e1ccfdef098ab0a6fa86f825748f0817e1a010'
   })
 
   depends_on 'docbook_xsl' => :build
   depends_on 'glibc' # R
-  depends_on 'libgcrypt' # R
-  depends_on 'libgpg_error' # R
   depends_on 'libxml2' # R
-  depends_on 'icu4c' # R
+  depends_on 'libxml2_autotools' # R
 
   cmake_options '-DLIBXSLT_WITH_PYTHON=OFF'
 end
