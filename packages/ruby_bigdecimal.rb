88b1998a0582b5488d92d6f206cfb47281106221
satmandu <satmandu@users.noreply.github.com>
2025-07-16 17:08:11 +0000

    Add built packages for linux/386 to branch bigdecimal.

--- packages/ruby_bigdecimal.rb
+++ packages/ruby_bigdecimal.rb
@@ -12,7 +12,7 @@ class Ruby_bigdecimal < RUBY
   binary_sha256({
     aarch64: 'b0d23dfb1d20da3bb43b0be1e48c1ef73e2017aee6dbabd3de6b72e88bec8f23',
      armv7l: 'b0d23dfb1d20da3bb43b0be1e48c1ef73e2017aee6dbabd3de6b72e88bec8f23',
-       i686: 'a216725e25a38a0eb03446137d0c660a912cc54b099d22f7fd706e4564b20413',
+       i686: '97d40beb3af93e11cfe2bd5f3d2ca7faedaf61a6f50ef88a4edc2d6762673068',
      x86_64: '3d6a052a3a09e584d55fa12f4c9f4f71250ced05830fdccc430dbdf3cd5518dc'
   })
 
