737310b27d3c212b80c50943d0b774ccd4fc4fa7
Satadru Pramanik <satadru@gmail.com>
2025-07-17 21:09:02 -0400

    Update boost url
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/boost.rb
+++ packages/boost.rb
@@ -6,7 +6,7 @@ class Boost < Package
   version "1.85.0-#{CREW_ICU_VER}-#{CREW_PY_VER}"
   license 'Boost-1.0'
   compatibility 'all'
-  source_url "https://boostorg.jfrog.io/artifactory/main/release/#{version.split('-').first}/source/boost_#{version.split('-').first.gsub('.', '_')}.tar.bz2"
+  source_url "https://archives.boost.io/release/#{version.split('-').first}/source/boost_#{version.split('-').first.gsub('.', '_')}.tar.bz2"
   source_sha256 '07911da8fe22fb10e3918528a8f0a5676f03c0b3b135ac19c26779f6804baebe'
   binary_compression 'tar.zst'
 
