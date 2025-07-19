f93643f0bad40e2ecf7058b275e4f674ad741de7
Satadru Pramanik <satadru@gmail.com>
2025-07-19 09:48:43 -0400

    Refactor jbigkit
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/jbigkit.rb
+++ packages/jbigkit.rb
@@ -3,29 +3,34 @@ require 'package'
 class Jbigkit < Package
   description 'JBIG-KIT is a software implementation of the JBIG1 data compression standard'
   homepage 'https://www.cl.cam.ac.uk/~mgk25/jbigkit/'
-  version '2.1-709282d'
+  version '2.1-6.1ubuntu2'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/nu774/jbigkit.git'
-  git_hashtag '709282d1ddcb35ab17134ea8cdda43ec2d647d2f'
+  source_url 'https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/jbigkit/2.1-6.1ubuntu2/jbigkit_2.1.orig.tar.gz'
+  source_sha256 'de7106b6bfaf495d6865c7dd7ac6ca1381bd12e0d81405ea81e7f2167263d932'
   binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '33d7651880ff70c07345d33a971dd5a6ecd3a630ab3dd7a35066bd0c4e1e8dcb',
      armv7l: '33d7651880ff70c07345d33a971dd5a6ecd3a630ab3dd7a35066bd0c4e1e8dcb',
        i686: 'd1a83c13c9e277c20f61a3ae708299e582efade2934cf58933600049f1080fdb',
-     x86_64: '89ca5e27aa2ee40643ae9d0a4f26c44f74d2604dbdc4611102f257cd7e94bd62'
+     x86_64: 'ddbc74c69e240fd2b6d25e476b0746cca3adcad4e2a94fc662ef91d204785037'
   })
 
   depends_on 'glibc' # R
 
   def self.patch
+    downloader "https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/jbigkit/#{version}/jbigkit_#{version}.debian.tar.xz", '6e487c3eb8e6ac36db05678628b9ef889887551af053c0858e383e45b85eef19'
+    system "tar xf jbigkit_#{version}.debian.tar.xz"
+    File.foreach 'debian/patches/series' do |patch|
+      system "patch -Np1 -i debian/patches/#{patch}"
+    end
     system "sed -i 's,/usr/bin/perl,#{CREW_PREFIX}/bin/perl,' pbmtools/jbgfuzz.pl"
+    system "sed -i 's,lib/\\$(DEB_HOST_MULTIARCH),#{ARCH_LIB},g' Makefile"
+    system "sed -i 's,/usr,#{CREW_PREFIX},g' Makefile"
   end
 
   def self.build
-    system 'autoreconf -fiv'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
     system 'make all'
   end
 
