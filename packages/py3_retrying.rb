7c5b2cf5f3dcbed6ec71fff628d630aa47eed8bb
Satadru Pramanik <satadru@gmail.com>
2025-07-20 07:20:40 -0400

    add automated update
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_retrying.rb
+++ packages/py3_retrying.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_retrying < Pip
   description 'Retrying simplifies the task of adding retry behavior to just about anything.'
   homepage 'https://github.com/rholder/retrying/'
-  version "1.4.0-#{CREW_PY_VER}"
+  version "1.4.1-#{CREW_PY_VER}"
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
@@ -13,7 +13,7 @@ class Py3_retrying < Pip
     aarch64: 'b949dc58356f623a36f55e0c9c3a42613256d52da2e7f3c04e5f2a2b599ea364',
      armv7l: 'b949dc58356f623a36f55e0c9c3a42613256d52da2e7f3c04e5f2a2b599ea364',
        i686: '2bef91ee48c697898fe141cb53ba58f403e8928e3df97a6b4866c322029f4cd6',
-     x86_64: '6110ae3b198fd455d8bb9270403c657f11827dcfbc00bed1186214b246f77b42'
+     x86_64: '960c396dfd1d5fcdb2cfed0edb4c0526a60f0288c01e5675d60713351a89ebf0'
   })
 
   depends_on 'py3_six'
