bec85cecfda19c2e79a710ae929e7c5ae8b39eb7
Ed Reel <edreel@gmail.com>
2025-07-23 17:32:32 -0500

    Google_chrome 138.0.7204.157-1 => 138.0.7204.168-1 (#12244)

--- packages/google_chrome.rb
+++ packages/google_chrome.rb
@@ -5,12 +5,12 @@ class Google_chrome < Package
   description 'Google Chrome is a fast, easy to use, and secure web browser.'
   homepage 'https://www.google.com/chrome/'
   @update_channel = 'stable'
-  version '138.0.7204.157-1'
+  version '138.0.7204.168-1'
   license 'google-chrome'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-#{@update_channel}/google-chrome-#{@update_channel}_#{@version}_amd64.deb"
-  source_sha256 '42659ebd4e1c62651cea551b146e1b43568a16e532214a2b26330c175e1bcd9e'
+  source_sha256 'bdf30440e87d559b0b12595df2a7de2d8f41e77cf502ac914d0ffba767311d88'
 
   depends_on 'nss'
   depends_on 'cairo'
