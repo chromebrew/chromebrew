33d37856ae100deba8ee13a817cf1e66224e5382
Satadru Pramanik <satadru@gmail.com>
2025-07-25 19:29:33 -0400

    move zed to rust build
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/zed.rb
+++ packages/zed.rb
@@ -1,14 +1,13 @@
-require 'package'
+require 'buildsystems/rust'
 
-class Zed < Package
+class Zed < RUST
   description 'Zed is a high-performance, multiplayer code editor'
   homepage 'https://zed.dev/'
-  version '0.186.12'
+  version '0.196.6'
   license 'GPL-3, AGPL-3, Apache-2.0'
   compatibility 'x86_64'
-  min_glibc '2.28'
-  source_url "https://github.com/zed-industries/zed/releases/download/v#{version}/zed-linux-x86_64.tar.gz"
-  source_sha256 '9c0abca37319d96bf8ba03653a93413011210a338acc386ddaf88d68013c1a6d'
+  source_url 'https://github.com/zed-industries/zed.git'
+  git_hashtag "v#{version}"
 
   depends_on 'alsa_lib'
   depends_on 'libbsd'
@@ -16,19 +15,11 @@ class Zed < Package
   depends_on 'libxcb'
   depends_on 'libxdmcp'
   depends_on 'libxkbcommon'
+  depends_on 'llvm_dev' => :build
   depends_on 'openssl'
+  depends_on 'rust' => :build
   depends_on 'zlib'
   depends_on 'zstd'
-
-  no_compile_needed
-  no_shrink
-
-  def self.install
-    FileUtils.mkdir_p CREW_DEST_PREFIX.to_s
-    FileUtils.mv 'bin/', CREW_DEST_PREFIX.to_s
-    FileUtils.mv 'share/', CREW_DEST_PREFIX.to_s
-    FileUtils.mv 'libexec/', CREW_DEST_PREFIX.to_s
-  end
 
   def self.postinstall
     ExitMessage.add "\nType 'zed' to get started.\n"
