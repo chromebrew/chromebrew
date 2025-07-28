cb0bee50556660eff269a847483680ab28146606
satmandu <satmandu@users.noreply.github.com>
2025-07-28 13:24:18 +0000

    Updating package files for linux/386 to branch automake.

--- packages/automake.rb
+++ packages/automake.rb
@@ -15,7 +15,7 @@ class Automake < Autotools
   binary_sha256({
     aarch64: 'dac62cec4062da0ced7e00c53ffaf3809c67d38d3d7342b0ae9ebc5a1b55d30a',
      armv7l: 'dac62cec4062da0ced7e00c53ffaf3809c67d38d3d7342b0ae9ebc5a1b55d30a',
-       i686: '3c25bc262cc5dd27837663fbc92d846e14b111d36ad6e7d707f6d139901f9573',
+       i686: '7df52319759ec22991e5744f144093f7d724a20b6cd8aa82f2a71145fed3c274',
      x86_64: 'f0d23c064d7f7a18a3ef1e7f59b188298c3bd12a5ccf744a554f140f7bf6725c'
   })
 
