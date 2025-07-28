5dce2395f1a6c081934bd08ff58e81dd7ebebf40
satmandu <satmandu@users.noreply.github.com>
2025-07-28 13:15:00 +0000

    Updating package files for linux/arm/v7 to branch automake.

--- packages/automake.rb
+++ packages/automake.rb
@@ -13,8 +13,8 @@ class Automake < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'db04372b9e8d82526113ba8daa8fa6f61369cbe6e9b864d0434ed307425870b4',
-     armv7l: 'db04372b9e8d82526113ba8daa8fa6f61369cbe6e9b864d0434ed307425870b4',
+    aarch64: 'dac62cec4062da0ced7e00c53ffaf3809c67d38d3d7342b0ae9ebc5a1b55d30a',
+     armv7l: 'dac62cec4062da0ced7e00c53ffaf3809c67d38d3d7342b0ae9ebc5a1b55d30a',
        i686: '3c25bc262cc5dd27837663fbc92d846e14b111d36ad6e7d707f6d139901f9573',
      x86_64: 'f0d23c064d7f7a18a3ef1e7f59b188298c3bd12a5ccf744a554f140f7bf6725c'
   })
