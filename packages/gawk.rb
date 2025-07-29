b9b71d81c0a4300254d084378f36190a62b07d69
Satadru Pramanik <satadru@gmail.com>
2025-07-29 17:40:55 -0400

    Test gawk patch
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gawk.rb
+++ packages/gawk.rb
@@ -10,7 +10,7 @@ require 'buildsystems/autotools'
 class Gawk < Autotools
   description 'The gawk utility interprets a special-purpose programming language that makes it possible to handle simple data-reformatting jobs with just a few lines of code.'
   homepage 'https://www.gnu.org/software/gawk/'
-  version '5.3.2-1'
+  version '5.3.2-2'
   license 'GPL-2'
   compatibility 'all'
   source_url "https://ftpmirror.gnu.org/gawk/gawk-#{version.split('-').first}.tar.xz"
@@ -42,8 +42,111 @@ class Gawk < Autotools
   end
 
   def self.patch
+    # Suggested patch for docker incompatiblity from gawk mailing list.
+    File.write 'pipetable.patch', <<~PIPETABLE_PATCH_EOF
+      diff --git a/io.c b/io.c
+      index 098a56bf..b46007ae 100644
+      --- a/io.c
+      +++ b/io.c
+      @@ -4604,9 +4604,28 @@ avoid_flush(const char *name)
+       typedef struct write_pipe {
+       	FILE *fp;
+       	pid_t pid;
+      +	struct write_pipe *next;
+       } write_pipe;
+
+       static write_pipe *open_pipes = NULL;
+      +#define PIPE_HASH_SIZE	127
+      +
+      +static void
+      +save_pipe(int fd, FILE *fp, pid_t pid)
+      +{
+      +	write_pipe *bucket = &open_pipes[fd % PIPE_HASH_SIZE];
+      +	write_pipe *wp;
+      +
+      +	if (bucket->fp == NULL)
+      +		wp = bucket;
+      +	else {
+      +		emalloc(wp, write_pipe *, sizeof(write_pipe));
+      +		wp->next = bucket->next;
+      +		bucket->next = wp;
+      +	}
+      +	wp->fp = fp;
+      +	wp->pid = pid;
+      +}
+
+       /* gawk_popen_write --- open a pipe for writing, set up a FILE * return value. */
+
+      @@ -4623,7 +4642,7 @@ gawk_popen_write(const char *cmd)
+       		return NULL;
+
+       	if (open_pipes == NULL) {
+      -		int count = getdtablesize();
+      +		int count = PIPE_HASH_SIZE;
+
+       		emalloc(open_pipes, write_pipe *, sizeof(write_pipe) * count);
+       		memset(open_pipes, 0, sizeof(write_pipe) * count);
+      @@ -4653,9 +4672,7 @@ gawk_popen_write(const char *cmd)
+       		return NULL;
+       	}
+
+      -	int index = pipefds[1];	// use the write file desciptor.
+      -	open_pipes[index].pid = childpid;
+      -	open_pipes[index].fp = fp;
+      +	save_pipe(pipefds[1], fp, childpid);
+
+       	return fp;
+       #endif
+      @@ -4669,23 +4686,33 @@ gawk_popen_write_close(FILE *fp)
+       #if defined(VMS) || defined(__MINGW32__)
+       	return pclose(fp);
+       #else
+      -	int status, index;
+      +	write_pipe *wp;
+      +	write_pipe *parent;
+
+       	if (open_pipes == NULL || fp == NULL) {
+       		errno = EINVAL;
+       		return -1;
+       	}
+
+      -	index = fileno(fp);
+      -	if (open_pipes[index].fp == fp) {
+      -		(void) fflush(fp);
+      -		(void) fclose(fp);
+      -		status = wait_any(open_pipes[index].pid);
+      -
+      -		open_pipes[index].fp = NULL;
+      -		open_pipes[index].pid = 0;
+      -
+      -		return status;
+      +	parent = NULL;
+      +	for (wp = &open_pipes[fileno(fp) % PIPE_HASH_SIZE]; wp != NULL;
+      +	     wp = wp->next) {
+      +		if (wp->fp == fp) {
+      +			int status;
+      +
+      +			(void) fflush(fp);
+      +			(void) fclose(fp);
+      +			status = wait_any(wp->pid);
+      +
+      +			wp->fp = NULL;
+      +			wp->pid = 0;
+      +			if (parent != NULL) {
+      +				parent->next = wp->next;
+      +				efree(wp);
+      +			}
+      +			return status;
+      +		}
+      +		parent = wp;
+       	}
+
+       	errno = EBADF;
+    PIPETABLE_PATCH_EOF
+    system 'patch -Np1 -i pipetable.patch'
+
     # Fix automake 1.16 requirements.
-    system 'grep -rlZ AM_INIT_AUTOMAKE . | xargs -0 sed -i \'s,1.16,1.18,g\'' unless ARCH == 'x86_64'
-    system 'grep -rlZ aclocal . | xargs -0 sed -i \'s,1.16,1.18,g\'' unless ARCH == 'x86_64'
+    system 'grep -rlZ AM_INIT_AUTOMAKE . | xargs -0 sed -i \'s,1.16,1.18,g\''
+    system 'grep -rlZ aclocal . | xargs -0 sed -i \'s,1.16,1.18,g\''
   end
 end
