b167118fd093612c8beb8b1473e52af0136c1ae5
Satadru Pramanik <satadru@gmail.com>
2025-07-30 14:28:47 -0400

    mold => 2.40.3
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/mold.rb
+++ packages/mold.rb
@@ -6,7 +6,7 @@ require 'buildsystems/cmake'
 class Mold < CMake
   description 'A Modern Linker'
   homepage 'https://github.com/rui314/mold'
-  version '2.40.2'
+  version '2.40.3'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/rui314/mold.git'
