27270ffbf70635f954ac70d5e3f49691a68253f1
Satadru Pramanik <satadru@gmail.com>
2025-07-31 18:15:28 -0400

    glib => 2.85.2
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/glib.rb
+++ packages/glib.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Glib < Meson
   description 'GLib provides the core application building blocks for libraries and applications written in C.'
   homepage 'https://docs.gtk.org/glib/'
-  version '2.85.1-1'
+  version '2.85.2'
   license 'LGPL-2.1'
   compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/glib.git'
