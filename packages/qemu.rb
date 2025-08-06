44c43fcad570f969037b77f7da781056a6777099
satmandu <satmandu@users.noreply.github.com>
2025-08-06 02:10:00 +0000

    qemu: Build Run on linux/arm/v7.

--- packages/qemu.rb
+++ packages/qemu.rb
@@ -17,55 +17,25 @@ class Qemu < Package
      x86_64: '30183ad202272d2d568a6da30a58ad228cd1b42daebee015051703cbdd88eafe'
   })
 
-  depends_on 'alsa_lib' # R
-  depends_on 'at_spi2_core' # R
   depends_on 'bzip2' # R
-  depends_on 'cairo' # R
   depends_on 'curl' # R
-  depends_on 'elfutils' # R
   depends_on 'eudev' # R
-  depends_on 'fftw' # R
-  depends_on 'fontconfig' # R
   depends_on 'gcc_lib' # R
-  depends_on 'gdk_pixbuf' # R
-  depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'glibc' # R
   depends_on 'gnutls' # R
-  depends_on 'gtk3' # R
-  depends_on 'harfbuzz' # R
   depends_on 'hicolor_icon_theme'
-  depends_on 'jack' # R
   depends_on 'jemalloc'
   depends_on 'libaio' # R
   depends_on 'libcap_ng' # R
-  depends_on 'libcyrussasl' # R
-  depends_on 'libepoxy' # R
   depends_on 'libgcrypt'
-  depends_on 'libjpeg_turbo' # R
-  depends_on 'libpng' # R
-  depends_on 'libsamplerate' # R
-  depends_on 'libsdl2' # R
-  depends_on 'libseccomp' # R
-  depends_on 'libslirp' # R
   depends_on 'libssh' # R
-  depends_on 'libusb' # R
-  depends_on 'libx11' # R
   depends_on 'libxkbcommon' # R
   depends_on 'linux_pam' # R
   depends_on 'lzfse' # R
-  depends_on 'lzo' # R
-  depends_on 'mesa' # R
-  depends_on 'ncurses' # R
-  depends_on 'pango' # R
-  depends_on 'pipewire' # R
-  depends_on 'pixman' # R
-  depends_on 'pulseaudio' # R
   depends_on 'py3_sphinx_rtd_theme' => :build
   depends_on 'rust' => :build
-  depends_on 'sdl2_image' # R
-  depends_on 'snappy' # R
   depends_on 'sphinx' => :build
-  depends_on 'vte' # R
   depends_on 'zlib' # R
   depends_on 'zstd' # R
 
