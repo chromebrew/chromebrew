f5a79d47b1da3cd03f35132b8f2830499a1499aa
Satadru Pramanik <satadru@gmail.com>
2025-08-06 15:49:23 -0400

    Add unbuilt zile to zile-2025-08-06-19-49

--- packages/zile.rb
+++ packages/zile.rb
@@ -3,12 +3,12 @@ require 'package'
 class Zile < Package
   description 'A small, fast, and powerful Emacs clone'
   homepage 'http://www.gnu.org/software/zile/'
-  version '2.4.14'
+  version '2.6.4'
   license 'GPL-3+'
   compatibility 'all'
   source_url 'https://ftpmirror.gnu.org/zile/zile-2.4.14.tar.gz'
   source_sha256 '7a78742795ca32480f2bab697fd5e328618d9997d6f417cf1b14e9da9af26b74'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '9af8d03f90d5a5f6ffd48774ccdc053ffb8d0a878baf883a301ff709d1d8d2d4',
