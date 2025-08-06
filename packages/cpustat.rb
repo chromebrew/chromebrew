3af88641b40b745aa8ee96766b2fc149998ca459
Satadru Pramanik <satadru@gmail.com>
2025-08-06 12:44:04 -0400

    Update package file.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/cpustat.rb
+++ packages/cpustat.rb
@@ -2,13 +2,13 @@ require 'package'
 
 class Cpustat < Package
   description 'cpustat periodically dumps out the current CPU utilisation statistics of running processes.'
-  homepage 'https://kernel.ubuntu.com/~cking/cpustat/'
-  version '0.02.15'
+  homepage 'https://github.com/ColinIanKing/cpustat'
+  version '0.03.00'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://kernel.ubuntu.com/~cking/tarballs/cpustat/cpustat-0.02.10.tar.xz'
-  source_sha256 'ea9ab5a970ec657496c0127e3e5d58d49ce0fe07e750b4aafcfeb4896ccd74e9'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/ColinIanKing/cpustat.git'
+  git_hashtag "V#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'd8a55c174fee28fc12a5a153836696fdfc7320f5fbc106095e0d18faccc7c58e',
@@ -16,6 +16,8 @@ class Cpustat < Package
        i686: '8e215c1064acc4bc8329f801d79aa96b7d8537596e6b91c12037c5782f5b3454',
      x86_64: '8e96b86281a64fe06adc42c18a8507f0e52ffba4368cd3acb96b4f428ad0bb0d'
   })
+
+  depends_on 'ncurses' => :build
 
   def self.build
     system "sed -i 's,sbin,bin,' Makefile"
