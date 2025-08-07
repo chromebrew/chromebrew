3b65ecf65a63ff969ea0af6864bfb9dcd18e164f
Satadru Pramanik <satadru@gmail.com>
2025-08-07 08:36:43 -0400

    Fix dehtml build.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/dehtml.rb
+++ packages/dehtml.rb
@@ -11,13 +11,15 @@ class Dehtml < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '949bb24173f0339c6ea0ba0878862ba730d5dd20ad76f8f95c4f6961297a33c7',
-     armv7l: '949bb24173f0339c6ea0ba0878862ba730d5dd20ad76f8f95c4f6961297a33c7',
-       i686: 'eb13507fc8eb05871a4af0fc5a67399bed4aaddb963e36d4d77f13ffb2af4da6',
-     x86_64: '5788fbdec1e02e91412c869b23b813144894e4a2715faa865f1f0421dc7c6315'
+    aarch64: 'd443a2fbbc3d16fdcefce0e8910bd20a230ecce4aeea3cf4aa6a811cd76c3ed1',
+     armv7l: 'd443a2fbbc3d16fdcefce0e8910bd20a230ecce4aeea3cf4aa6a811cd76c3ed1',
+       i686: 'ead0e0cc2d7a9486ccb8d7a0a0f4b19cdb71f3081df2e7d0d8402aa93ef539de',
+     x86_64: 'ac7002d89ff6d5537cd22b350d7b86c46424b7c8f918533b5d06239490727aae'
   })
 
   depends_on 'glibc' # R
+
+  autotools_build_relative_dir '.'
 
   # The Makefile.in does not respect DESTDIR, so we override the buildsystem and append the explicit paths here.
   def self.build
