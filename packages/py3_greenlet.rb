c8cf1fff30518acb94b8d58ce4336df45580148b
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-08-08 00:24:48 +0000

    Add unbuilt py3_greenlet to py3_greenlet-2025-08-08-00-24

--- packages/py3_greenlet.rb
+++ packages/py3_greenlet.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_greenlet < Pip
   description 'Greenlet provides lightweight in-process concurrent programming.'
   homepage 'https://greenlet.readthedocs.io/'
-  version "3.2.3-#{CREW_PY_VER}"
+  version "3.2.4-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
