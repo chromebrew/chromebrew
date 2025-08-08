65402ff3ee6f479beb32647bd95c6455f2f07259
Ed Reel <edreel@gmail.com>
2025-08-08 05:58:05 -0500

    Android_studio 2023.1.1.26 => 2025.1.2.11 (#12385)

--- packages/android_studio.rb
+++ packages/android_studio.rb
@@ -4,18 +4,29 @@ require 'misc_functions'
 class Android_studio < Package
   description 'Android Studio is the official IDE for Android development.'
   homepage 'https://developer.android.com/studio'
-  version '2023.1.1.26'
+  version '2025.1.2.11'
   license 'Apache-2.0'
   compatibility 'x86_64'
-  source_url 'https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2023.1.1.26/android-studio-2023.1.1.26-linux.tar.gz'
-  source_sha256 '977e8a9855414f7d41157f0be0e10fb740c42bd337f763d5d96b3e059780663d'
+  min_glibc '2.28'
+  source_url "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/#{version}/android-studio-#{version}-linux.tar.gz"
+  source_sha256 '8f3874c6b119538cdd96c7bcb65555feea81133e251f69365d21ddd77777be78'
 
   depends_on 'openjdk17'
   depends_on 'xdg_base'
   depends_on 'sommelier'
 
+  print_source_bashrc
+
+  @short_ver = version.sub(/\.\d+$/, '')
+
   def self.preflight
-    MiscFunctions.check_free_disk_space(6442450944)
+    # Need at least 7.5 gb of free disk space to install.
+    MiscFunctions.check_free_disk_space(8053063680)
+  end
+
+  def self.patch
+    # product-info.json must exist in the installation directory.
+    system "sed -i '7i[ ! -f \$(pwd)/product-info.json ] && touch product-info.json' bin/studio.sh"
   end
 
   def self.install
@@ -24,31 +35,18 @@ class Android_studio < Package
     FileUtils.mv 'bin', "#{CREW_DEST_PREFIX}/share/android-studio"
     FileUtils.mv 'lib', "#{CREW_DEST_PREFIX}/share/android-studio"
     FileUtils.mv 'plugins', "#{CREW_DEST_PREFIX}/share/android-studio"
-    FileUtils.cd "#{CREW_DEST_PREFIX}/bin" do
-      FileUtils.ln_s "#{CREW_PREFIX}/share/android-studio/bin/studio.sh", 'studio'
-    end
+    FileUtils.ln_s "#{CREW_PREFIX}/share/android-studio/bin/studio.sh", "#{CREW_DEST_PREFIX}/bin/studio"
   end
 
   def self.postinstall
-    puts "\nTo finish the installation, execute the following:".lightblue
-    puts "source #{HOME}/.bashrc".lightblue
-    puts "\nTo start using Android Studio, type `studio`.\n".lightblue
+    FileUtils.mkdir_p "#{CREW_PREFIX}/.cache/Google/AndroidStudio#{@short_ver}/tmp"
+    ExitMessage.add "\nType `studio` to start using Android Studio.\n"
   end
 
   def self.postremove
-    print 'Would you like to remove the config directories? [y/N] '
-    response = $stdin.gets.chomp.downcase
-    config_dirs = ["#{HOME}/.android", "#{HOME}/Android"]
-    config_dirs.each do |config_dir|
-      next unless Dir.exist? config_dir
-
-      case response
-      when 'y', 'yes'
-        FileUtils.rm_rf config_dir
-        puts "#{config_dir} removed.".lightred
-      else
-        puts "#{config_dir} saved.".lightgreen
-      end
-    end
+    Package.agree_to_remove("#{CREW_PREFIX}/.cache/Google/AndroidStudio#{@short_ver}")
+    Package.agree_to_remove("#{HOME}/.android")
+    Package.agree_to_remove("#{HOME}/Android")
+    Package.agree_to_remove("#{HOME}/StudioProjects")
   end
 end
