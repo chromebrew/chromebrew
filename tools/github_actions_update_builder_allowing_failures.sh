a49d720fd009ff5dcd6144cfdfea5a322fd6c2c1
Satadru Pramanik <satadru@gmail.com>
2025-08-09 08:22:25 -0400

    Adjust for rubocop issues.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- tools/github_actions_update_builder_allowing_failures.sh
+++ tools/github_actions_update_builder_allowing_failures.sh
@@ -8,5 +8,7 @@ yes | crew upgrade || true
 source ~/.bashrc
 git config --global --add safe.directory /output
 export CREW_AGREE_TIMEOUT_SECONDS=1
+# Deal with a regexp_parser gem issue
+gem install regexp_parser
 yes | crew reinstall ruby_rubocop
 tools/build_updated_packages.rb --skip --continue-after-failed-builds || exit 1
