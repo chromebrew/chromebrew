67c011f8a88a1ecadcbf766ab6a89b4b4ef7f5bc
satmandu <satmandu@users.noreply.github.com>
2025-08-10 16:14:42 +0000

    Add unbuilt atomicparsley to atomicparsley-20240608.083822.1ed9031

--- packages/atomicparsley.rb
+++ packages/atomicparsley.rb
@@ -3,12 +3,12 @@ require 'package'
 class Atomicparsley < Package
   description 'AtomicParsley is a lightweight command line program for reading, parsing and setting metadata into MPEG-4 files, in particular, iTunes-style metadata.'
   homepage 'https://github.com/wez/atomicparsley'
-  version '0.9.6'
+  version '20240608.083822.1ed9031'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://bitbucket.org/wez/atomicparsley/get/0.9.6.tar.gz'
   source_sha256 '8ba4e3e21d7a9239932e2a6f34842194d8f9eba84ce9eb83fb35369f5f3f05ab'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '55d6f7bf30bd0e178dd9d117c08b6b5c6f6965808f2e483256ab100a0823b4ae',
