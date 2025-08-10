0fc3a54da79045cc4e9d4e382cc530e839e0d06e
satmandu <satmandu@users.noreply.github.com>
2025-08-10 16:14:46 +0000

    Add unbuilt audacious to audacious-4.5

--- packages/audacious.rb
+++ packages/audacious.rb
@@ -3,26 +3,29 @@ require 'package'
 class Audacious < Package
   description 'Audacious is an open source audio player.'
   homepage 'https://audacious-media-player.org/'
-  version '3.10.1'
+  version '4.5'
   license 'BSD-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://distfiles.audacious-media-player.org/audacious-3.10.1.tar.bz2'
   source_sha256 '8366e840bb3c9448c2cf0cf9a0800155b0bd7cc212a28ba44990c3d2289c6b93'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6597ca2c03a630bc4da5ff6f0543e2dc5a4d7621b9bcdb2828fe49fc3889df9d',
-     armv7l: '6597ca2c03a630bc4da5ff6f0543e2dc5a4d7621b9bcdb2828fe49fc3889df9d',
-     x86_64: '5660d4bbee7230c4b2698973b2a82a45e20461dba246fb22fcdbf91be74b9793'
+    aarch64: 'ba6f82f28f35f02059be703f7360eeb44660561995592db41f80c9944853e4ad',
+     armv7l: 'ba6f82f28f35f02059be703f7360eeb44660561995592db41f80c9944853e4ad',
+     x86_64: '631f3e20599b98e94ddf0acd36805ed8daed499218df202b8fc5249d3245fc0b'
   })
 
+  depends_on 'at_spi2_core' # R
   depends_on 'audacious_plugins' => :runtime
-  depends_on 'gdbm'
-  depends_on 'gtk2'
-  depends_on 'qt5_base'
+  depends_on 'cairo' # R
+  depends_on 'gdk_pixbuf' # R
+  depends_on 'harfbuzz' # R
   depends_on 'mpg123'
-  depends_on 'xdg_base'
+  depends_on 'pango' # R
+  depends_on 'qt5_base'
   depends_on 'sommelier'
+  depends_on 'xdg_base'
 
   def self.build
     system './configure',
