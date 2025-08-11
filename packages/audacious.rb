a053d11991721b4f8d00f47f6556c0ef0d419aa8
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-08-11 00:28:08 -0400

    Audacious => 4.5, Opus => 1.5.2 (#12415)
    * Add unbuilt audacious to audacious-4.5
    
    * Update sources for audacious packages.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: satmandu <satmandu@users.noreply.github.com>

--- packages/audacious.rb
+++ packages/audacious.rb
@@ -1,41 +1,35 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Audacious < Package
+class Audacious < Autotools
   description 'Audacious is an open source audio player.'
   homepage 'https://audacious-media-player.org/'
-  version '3.10.1'
+  version '4.5'
   license 'BSD-2'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://distfiles.audacious-media-player.org/audacious-3.10.1.tar.bz2'
-  source_sha256 '8366e840bb3c9448c2cf0cf9a0800155b0bd7cc212a28ba44990c3d2289c6b93'
-  binary_compression 'tar.xz'
+  source_url "https://distfiles.audacious-media-player.org/audacious-#{version}.tar.bz2"
+  source_sha256 '1ea5e0f871c6a8b2318e09a9d58fc573fe3f117ae0d8d163b60cc05b2ce7c405'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6597ca2c03a630bc4da5ff6f0543e2dc5a4d7621b9bcdb2828fe49fc3889df9d',
-     armv7l: '6597ca2c03a630bc4da5ff6f0543e2dc5a4d7621b9bcdb2828fe49fc3889df9d',
-     x86_64: '5660d4bbee7230c4b2698973b2a82a45e20461dba246fb22fcdbf91be74b9793'
+    aarch64: '2d20a44ff9efcc984fad709fedf198d226ea9816945f5c1b0e4c2ea2ab4af3b8',
+     armv7l: '2d20a44ff9efcc984fad709fedf198d226ea9816945f5c1b0e4c2ea2ab4af3b8',
+     x86_64: '7223fd048011675270767994ba0aa0fe49309c84102245b187ddd7ac36a40475'
   })
 
-  depends_on 'audacious_plugins' => :runtime
-  depends_on 'gdbm'
-  depends_on 'gtk2'
-  depends_on 'qt5_base'
-  depends_on 'mpg123'
-  depends_on 'xdg_base'
+  depends_on 'at_spi2_core' # R
+  # depends_on 'audacious_plugins' => :runtime
+  depends_on 'cairo' # R
+  depends_on 'gcc_lib' # R
+  depends_on 'gdk_pixbuf' # R
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
+  depends_on 'pango' # R
   depends_on 'sommelier'
+  depends_on 'xdg_base'
+  depends_on 'zlib' # R
 
-  def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-dbus',
-           '--enable-qt'
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  autotools_configure_options '--disable-dbus \
+                               --disable-qt'
 
   def self.postinstall
     puts
