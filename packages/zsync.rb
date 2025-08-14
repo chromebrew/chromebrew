f552e08c0503317580f0dcbb981c4bd9b4a20f99
Satadru Pramanik <satadru@gmail.com>
2025-08-14 10:08:46 -0400

    Update zsync package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/zsync.rb
+++ packages/zsync.rb
@@ -1,12 +1,12 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Zsync < Package
+class Zsync < Autotools
   description 'zsync is a client-side file transfer program similar to rsync.'
   homepage 'http://zsync.moria.org.uk/'
   version '0.6.3'
   license 'Artistic-2'
   compatibility 'all'
-  source_url 'https://zsync.moria.org.uk/download/zsync-0.6.2.tar.bz2'
+  source_url "https://zsync.moria.org.uk/download/zsync-#{version}.tar.bz2"
   source_sha256 '0b9d53433387aa4f04634a6c63a5efa8203070f2298af72a705f9be3dda65af2'
   binary_compression 'tar.zst'
 
@@ -18,16 +18,7 @@ class Zsync < Package
   })
 
   def self.patch
-    system "curl -Ls -o autotools/config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'"
-    system "curl -Ls -o autotools/config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'"
-  end
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS} --disable-maintainer-mode"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    downloader 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD', 'asasasa', 'autotools/config.guess'
+    downloader 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD', 'asascdcd', 'autotools/config.sub'
   end
 end
