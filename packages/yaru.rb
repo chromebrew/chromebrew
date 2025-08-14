62deacced809a58e2757ea679199e0c387a9d9e0
satmandu <satmandu@users.noreply.github.com>
2025-08-14 18:30:48 +0000

    Add unbuilt yaru to updater-yaru-25.10.1-0ubuntu1

--- packages/yaru.rb
+++ packages/yaru.rb
@@ -3,12 +3,12 @@ require 'package'
 class Yaru < Package
   description 'Yaru default ubuntu theme'
   homepage 'https://github.com/ubuntu/yaru'
-  version '21.04.1'
+  version '25.10.1-0ubuntu1'
   license 'GPL-3 and CC-BY-SA-4.0'
   compatibility 'all'
   source_url "https://github.com/ubuntu/yaru/archive/refs/tags/#{version}.tar.gz"
   source_sha256 '8cbbb1fcc7fa1e46e48d870cc1f941069e8213ac53200001aa9548ad79086836'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'b13e932b15e3867f2243d230cc6cdb36c475227a7167e298634cb72a48e37d0c',
