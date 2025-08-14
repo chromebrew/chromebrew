dcff3a512a1b9a1704ee41e30a70222aad65d50f
SupeChicken666 <me@supechicken666.dev>
2025-08-15 01:27:10 +0800

    Add files via upload

--- packages/brave.rb
+++ packages/brave.rb
@@ -19,18 +19,18 @@ class Brave < Package
   depends_on 'sommelier'
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/brave"
-    FileUtils.cp_r '.', "#{CREW_DEST_PREFIX}/share/brave"
-    FileUtils.ln_s "#{CREW_PREFIX}/share/brave/brave", "#{CREW_DEST_PREFIX}/bin/brave"
-    FileUtils.ln_s CREW_LIB_PREFIX, "#{CREW_DEST_PREFIX}/share/#{ARCH_LIB}"
+    FileUtils.mkdir_p %W[#{CREW_DEST_PREFIX}/bin #{CREW_DEST_PREFIX}/share/brave]
+    FileUtils.mv Dir['usr/share/*', 'opt/brave.com/brave'], "#{CREW_DEST_PREFIX}/share"
+
+    FileUtils.ln_s '../share/brave/brave-browser-stable', "#{CREW_DEST_PREFIX}/bin/brave-browser"
+    FileUtils.ln_s '../share/brave/brave', "#{CREW_DEST_PREFIX}/bin/brave-browser"
   end
 
   def self.postinstall
-    ConvenienceFunctions.set_default_browser('Brave', 'brave')
+    ConvenienceFunctions.set_default_browser('Brave', 'brave-browser-stable')
   end
 
   def self.preremove
-    ConvenienceFunctions.unset_default_browser('Brave', 'brave')
+    ConvenienceFunctions.unset_default_browser('Brave', 'brave-browser-stable')
   end
 end
