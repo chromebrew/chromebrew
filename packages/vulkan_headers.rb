5228f262d6c861a57b2d4baed76ee4a4e8d85fdf
satmandu <satmandu@users.noreply.github.com>
2025-08-15 07:06:35 +0000

    Add unbuilt vulkan_headers to updater-vulkan_headers-1.4.325

--- packages/vulkan_headers.rb
+++ packages/vulkan_headers.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Vulkan_headers < CMake
   description 'Vulkan header files'
   homepage 'https://github.com/KhronosGroup/Vulkan-Headers'
-  version '1.4.323'
+  version '1.4.325'
   license 'Apache-2.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/KhronosGroup/Vulkan-Headers.git'
