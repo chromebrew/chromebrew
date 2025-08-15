cd6387a7b9b074fa702cabdde4b026b81e629bcc
Satadru Pramanik <satadru@gmail.com>
2025-08-14 20:50:09 -0400

    Add more xcb updates
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/xcb_util_renderutil.rb
+++ packages/xcb_util_renderutil.rb
@@ -1,30 +1,23 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Xcb_util_renderutil < Package
+class Xcb_util_renderutil < Autotools
   description 'The xcb-util-renderutil package provides additional extensions to the XCB library'
   homepage 'https://xcb.freedesktop.org/'
-  version '0.3.9-1'
+  version '0.3.10'
   compatibility 'all'
   license 'MIT-with-advertising'
-  source_url 'https://xcb.freedesktop.org/dist/xcb-util-renderutil-0.3.9.tar.bz2'
-  source_sha256 'c6e97e48fb1286d6394dddb1c1732f00227c70bd1bedb7d1acabefdd340bea5b'
-  binary_compression 'tpxz'
+  source_url 'https://gitlab.freedesktop.org/xorg/lib/libxcb-render-util.git'
+  git_hashtag "xcb-util-renderutil-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '1129f723592633bc655e902f5a8715294fbb16b68de40e9113735cf1f24dc618',
      armv7l: '1129f723592633bc655e902f5a8715294fbb16b68de40e9113735cf1f24dc618',
        i686: '4100d5c0a01b165790d6993cd6bcc1e73b9e3ff6e2e7f5c09b3ac9425cf2bc59',
-     x86_64: 'ffe1a614f538c63b4665b044c3ffe335f6fcf0331d850f8e10307ffa7ed24a2e'
+     x86_64: '5d656eec964bedb059c208b15a069027d3921795c2e1cc2f219b7fd4e09536c4'
   })
 
-  depends_on 'libxcb'
+  depends_on 'glibc' # R
+  depends_on 'libxdmcp' # R
 
-  def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
--- packages/xcb_util_wm.rb
+++ packages/xcb_util_wm.rb
@@ -1,31 +1,24 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Xcb_util_wm < Package
+class Xcb_util_wm < Autotools
   description 'The xcb-util-wm package contains libraries which provide client and window-manager helpers for EWMH and ICCCM.'
   homepage 'https://xcb.freedesktop.org/'
-  version '0.4.1-1'
+  version '0.4.2'
   compatibility 'all'
   license 'MIT-with-advertising'
-  source_url 'https://xcb.freedesktop.org/dist/xcb-util-wm-0.4.1.tar.bz2'
-  source_sha256 '28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f'
-  binary_compression 'tpxz'
+  source_url 'https://gitlab.freedesktop.org/xorg/lib/libxcb-wm.git'
+  git_hashtag "xcb-util-wm-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '860d613c9e5ddb61f6cc91f1a12a3c31733e7c489de988b904708ae557e092d5',
      armv7l: '860d613c9e5ddb61f6cc91f1a12a3c31733e7c489de988b904708ae557e092d5',
        i686: 'c30f8f2da2d2493e53929556390ef1e140ac53e6e785c30a1ecc7adb4c26dfb6',
-     x86_64: '2db848479b901c4a9cdb59723ad1f96c549707c9edd7975c1620bf1e96307615'
+     x86_64: '927bb5dc470f70d8fbe5d2f690470ad15e13458ab8254c3e7e15df2c40385bf2'
   })
 
-  depends_on 'libxcb'
+  depends_on 'glibc' # R
+  depends_on 'libxdmcp' # R
   depends_on 'm4'
 
-  def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
