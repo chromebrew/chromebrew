ccb3c6a00991c04ec7ef94caf26ec93cf15a342b
Satadru Pramanik <satadru@gmail.com>
2025-08-15 12:41:46 -0400

    Adjust build to add CMAKE_POLICY_VERSION_MINIMUM env variable.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/vcpkg.rb
+++ packages/vcpkg.rb
@@ -19,6 +19,8 @@ class Vcpkg < Package
 
   def self.build
     ENV['VCPKG_FORCE_SYSTEM_BINARIES'] = 'ON'
+    # See https://github.com/microsoft/vcpkg/issues/44783
+    ENV['CMAKE_POLICY_VERSION_MINIMUM'] = '3.5'
     system './bootstrap-vcpkg.sh'
   end
 
