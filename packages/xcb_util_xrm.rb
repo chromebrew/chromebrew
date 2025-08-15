9805ece82bd6bb905dc11ef9ffb4e45eb6af4221
Satadru Pramanik <satadru@gmail.com>
2025-08-14 21:01:10 -0400

    Updte xcb_util_xrm
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/xcb_util_xrm.rb
+++ packages/xcb_util_xrm.rb
@@ -4,20 +4,21 @@ class Xcb_util_xrm < Autotools
   description 'XCB utility functions for the X resource manager'
   homepage 'https://github.com/Airblader/xcb-util-xrm'
   version '1.3-2'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   license 'MIT-with-advertising'
   source_url 'https://github.com/Airblader/xcb-util-xrm.git'
   git_hashtag "v#{version.split('-').first}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '70cdd52679ee61272c4f07bcc1bc75132512415004011e67dc844ca26c22e5e6',
-     armv7l: '70cdd52679ee61272c4f07bcc1bc75132512415004011e67dc844ca26c22e5e6',
-       i686: '85e432baec61c1092613482a57fba8b4e159458680c09e28e3239213b375d579',
+    aarch64: '95e5da6b9813e3395a3ee77aa34e47c966dc77e3f035b409fa09c862272db963',
+     armv7l: '95e5da6b9813e3395a3ee77aa34e47c966dc77e3f035b409fa09c862272db963',
      x86_64: '0fba4c6710557d350cb45cecea726084294835f1b3c7e1887487e594d83a9aec'
   })
 
   depends_on 'glibc' # R
+  depends_on 'libbsd' # R
+  depends_on 'libx11' => :build
   depends_on 'libxau' # R
-
+  depends_on 'libxcb' # R
 end
