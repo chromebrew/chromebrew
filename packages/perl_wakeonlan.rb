282965f98ba31e5af71bb028731fd87a80fb7d75
satmandu <satmandu@users.noreply.github.com>
2025-08-16 03:37:28 +0000

    Add unbuilt perl_wakeonlan to updater-perl_wakeonlan-3.1.0

--- packages/perl_wakeonlan.rb
+++ packages/perl_wakeonlan.rb
@@ -3,7 +3,7 @@ require 'buildsystems/perl'
 class Perl_wakeonlan < PERL
   description 'Perl script for waking up computers via Wake-On-LAN magic packets'
   homepage 'https://github.com/jpoliv/wakeonlan'
-  version "0.42-#{CREW_PERL_VER}"
+  version "3.1.0-#{CREW_PERL_VER}"
   license 'Artistic License 2.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/jpoliv/wakeonlan.git'
