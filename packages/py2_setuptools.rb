92d8862bc4331a8ee696a8b9236539309c5df1f6
satmandu <satmandu@users.noreply.github.com>
2025-08-16 03:37:51 +0000

    Add unbuilt py2_setuptools to updater-py2_setuptools-80.9.0

--- packages/py2_setuptools.rb
+++ packages/py2_setuptools.rb
@@ -4,12 +4,12 @@ class Py2_setuptools < Package
   description 'Setuptools is the python build system from the Python Packaging Authority.'
   homepage 'https://setuptools.readthedocs.io/'
   # Newest python2 supported version as of 2021-04-24
-  version '44.1.1'
+  version '80.9.0'
   license 'MIT'
   compatibility 'all'
   source_url "https://files.pythonhosted.org/packages/b2/40/4e00501c204b457f10fe410da0c97537214b2265247bc9a5bc6edd55b9e4/setuptools-#{version}.zip"
   source_sha256 'c67aa55db532a0dadc4d2e20ba9961cbd3ccc84d544e9029699822542b5a476b'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '668bdf06acc1e02d9a2ef79e16815d14930ecf3a0cacbc449f8b4581be6453ca',
