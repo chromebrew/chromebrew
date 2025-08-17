b8f4f936caa51a474c080b4edf03e6de1da35581
satmandu <satmandu@users.noreply.github.com>
2025-08-17 20:08:37 +0000

    Add unbuilt mimalloc to updater-mimalloc-3.1.5

--- packages/mimalloc.rb
+++ packages/mimalloc.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Mimalloc < CMake
   description 'General-purpose allocator with excellent performance characteristics'
   homepage 'https://github.com/microsoft/mimalloc'
-  version '1.8.2'
+  version '3.1.5'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/microsoft/mimalloc.git'
