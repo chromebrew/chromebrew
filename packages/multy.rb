7faadc8a62a8e40f30ccc6fba891bbffc9bd5c7b
satmandu <satmandu@users.noreply.github.com>
2025-08-17 20:08:54 +0000

    Add unbuilt multy to updater-multy-1.0.1

--- packages/multy.rb
+++ packages/multy.rb
@@ -3,7 +3,7 @@ require 'package'
 class Multy < Package
   description 'Easily deploy multi cloud infrastructure.'
   homepage 'https://multy.dev/'
-  version '0.1.57'
+  version '1.0.1'
   license 'Apache-2.0'
   compatibility 'i686 x86_64'
   source_url({
@@ -11,8 +11,8 @@ class Multy < Package
       i686: "https://github.com/multycloud/multy/releases/download/v#{version}/multy-linux-386.tar.gz"
   })
   source_sha256({
-    x86_64: '348fe997f026f3734a96f61c5198fe2ad10a9810dd71ab7c6971a63bf1c013fe',
-      i686: '2913857363d20d66c9a9c21f0a0cd498611e745f6d6121ebd665de833ad7118c'
+    x86_64: '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5',
+      i686: '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
   })
 
   no_compile_needed
