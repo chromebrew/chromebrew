6b607e7fb2f1397a5f3c90ccc0db5f7ca6d2e44d
Ed Reel <edreel@gmail.com>
2025-08-17 02:16:10 +0000

    Update source_sha256 in nocodb.rb

--- packages/nocodb.rb
+++ packages/nocodb.rb
@@ -7,7 +7,7 @@ class Nocodb < Package
   license 'AGPL-3.0'
   compatibility 'x86_64'
   source_url "https://github.com/nocodb/nocodb/releases/download/#{version}/Noco-linux-x64"
-  source_sha256 '77c20e3c9217f1da3e31937848aabc2dda372e6b3c7e1864981739f4ba2f5f68'
+  source_sha256 'd8224675bf8b07428579b9f2b5e270b34279003921604985040d98a8bf9ae356'
 
   no_compile_needed
   no_shrink
