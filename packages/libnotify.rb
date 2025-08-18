62685ae065c96e82912d3a7625eda006188cda24
satmandu <satmandu@users.noreply.github.com>
2025-08-18 00:55:20 +0000

    Add unbuilt libnotify to updater-libnotify-0.8.6

--- packages/libnotify.rb
+++ packages/libnotify.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Libnotify < Meson
   description 'A library for sending desktop notifications.'
   homepage 'https://gitlab.gnome.org/GNOME/libnotify'
-  version '0.8.3'
+  version '0.8.6'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/GNOME/libnotify.git'
