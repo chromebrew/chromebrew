802a22da6028393879f5bd7d2eb8616e5678037f
satmandu <satmandu@users.noreply.github.com>
2025-08-18 00:55:16 +0000

    Add unbuilt libnftnl to updater-libnftnl-1.3.0

--- packages/libnftnl.rb
+++ packages/libnftnl.rb
@@ -4,10 +4,10 @@ class Libnftnl < Autotools
   description 'libnftnl is a userspace library providing a low-level netlink programming interface (API) to the in-kernel nf_tables subsystem.'
   homepage 'https://netfilter.org/projects/libnftnl/'
   license 'GPL-2'
-  version '1.2.9'
+  version '1.3.0'
   compatibility 'all'
   source_url "https://netfilter.org/projects/libnftnl/files/libnftnl-#{version}.tar.xz"
-  source_sha256 'e8c216255e129f26270639fee7775265665a31b11aa920253c3e5d5d62dfc4b8'
+  source_sha256 '0f4be47a8bb8b77a350ee58cbd4b5fae6260ad486a527706ab15cfe1dd55a3c4'
   binary_compression 'tar.zst'
 
   binary_sha256({
