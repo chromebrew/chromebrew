5d5d3d1911a949a21aadb0286c9c54cee6ad5fa6
satmandu <satmandu@users.noreply.github.com>
2025-08-18 00:56:42 +0000

    Add unbuilt libxvmc to updater-libxvmc-1.0.14

--- packages/libxvmc.rb
+++ packages/libxvmc.rb
@@ -3,11 +3,11 @@ require 'package'
 class Libxvmc < Package
   description 'X.org X-Video Motion Compensation Library'
   homepage 'https://www.x.org/wiki/'
-  version '1.0.13'
+  version '1.0.14'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://gitlab.freedesktop.org/xorg/lib/libxvmc/-/archive/libXvMC-#{version}/libxvmc-libXvMC-#{version}.tar.gz"
-  source_sha256 '58a1766176947ec41cf44c917d831db5d619fec11f99637d6deca45458e9829b'
+  source_sha256 '08d9a9f8ebdd353ef0d831641a8959c01824db618ef833453f6f0707488ae300'
   binary_compression 'tar.zst'
 
   binary_sha256({
