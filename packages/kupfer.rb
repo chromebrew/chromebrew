ced4487bce6b4e674c0f71fdcb8f2b941f2be2b3
satmandu <satmandu@users.noreply.github.com>
2025-08-19 19:51:03 +0000

    Add unbuilt kupfer to updater-kupfer-v328

--- packages/kupfer.rb
+++ packages/kupfer.rb
@@ -3,7 +3,7 @@ require 'package'
 class Kupfer < Package
   description 'a smart, quick launcher'
   homepage 'https://kupferlauncher.github.io/'
-  version 'v321'
+  version 'v328'
   license 'GPL-3.0'
   compatibility 'x86_64'
   source_url 'https://github.com/kupferlauncher/kupfer.git'
