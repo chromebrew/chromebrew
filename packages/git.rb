2ba94c0ef3f3f6ea4273bbc8c03312e63fe24ac3
satmandu <satmandu@users.noreply.github.com>
2025-08-19 03:59:43 +0000

    Add unbuilt git to updater-git-2.51.0

--- packages/git.rb
+++ packages/git.rb
@@ -3,11 +3,11 @@ require 'buildsystems/meson'
 class Git < Meson
   description 'Git is a free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.'
   homepage 'https://git-scm.com/'
-  version '2.50.1'
+  version '2.51.0'
   license 'GPL-2'
   compatibility 'all'
   source_url "https://mirrors.edge.kernel.org/pub/software/scm/git/git-#{version.split('-').first}.tar.xz"
-  source_sha256 '618190cf590b7e9f6c11f91f23b1d267cd98c3ab33b850416d8758f8b5a85628'
+  source_sha256 '60a7c2251cc2e588d5cd87bae567260617c6de0c22dca9cdbfc4c7d2b8990b62'
   binary_compression 'tar.zst'
 
   binary_sha256({
