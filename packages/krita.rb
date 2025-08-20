c2c4f97a4ce3e10d889022b76da9a3b346871ab1
supechicken <supechicken@users.noreply.github.com>
2025-08-20 21:53:13 +0000

    updater-krita-5.2.11: Build Run on linux/amd64.

--- packages/krita.rb
+++ packages/krita.rb
@@ -17,10 +17,20 @@ class Krita < CMake
   })
 
   depends_on 'boost'
+  depends_on 'bzip2' # R
   depends_on 'eigen'
   depends_on 'extra_cmake_modules'
+  depends_on 'fontconfig' # R
+  depends_on 'freetype' # R
+  depends_on 'fribidi' # R
+  depends_on 'gcc_lib' # R
+  depends_on 'giflib' # R
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
   depends_on 'gsl'
+  depends_on 'harfbuzz' # R
   depends_on 'immer'
+  depends_on 'json_c' # R
   depends_on 'kf5_completion'
   depends_on 'kf5_config'
   depends_on 'kf5_coreaddons'
@@ -32,40 +42,32 @@ class Krita < CMake
   depends_on 'kf5_widgetsaddons'
   depends_on 'kf5_windowsystem'
   depends_on 'lager'
-  depends_on 'libexiv2'
-  depends_on 'libmypaint'
-  depends_on 'libunibreak'
-  depends_on 'mlt'
-  depends_on 'openexr'
-  depends_on 'qt5_base'
-  depends_on 'qt5_svg'
-  depends_on 'quazip'
-  depends_on 'zug'
-  depends_on 'bzip2' # R
-  depends_on 'fontconfig' # R
-  depends_on 'harfbuzz' # R
-  depends_on 'freetype' # R
-  depends_on 'fribidi' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'giflib' # R
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
-  depends_on 'json_c' # R
   depends_on 'lcms' # R
+  depends_on 'libexiv2'
   depends_on 'libheif' # R
   depends_on 'libice' # R
   depends_on 'libjpeg_turbo' # R
   depends_on 'libjxl' # R
+  depends_on 'libmypaint'
   depends_on 'libpng' # R
   depends_on 'libsdl2' # R
   depends_on 'libsm' # R
   depends_on 'libtiff' # R
+  depends_on 'libunibreak'
   depends_on 'libwebp' # R
   depends_on 'libx11' # R
   depends_on 'libxext' # R
   depends_on 'libxi' # R
+  depends_on 'mlt'
+  depends_on 'openexr'
   depends_on 'openjpeg' # R
   depends_on 'poppler' # R
+  depends_on 'qt5_base'
+  depends_on 'qt5_svg'
   depends_on 'qt5_x11extras' # R
+  depends_on 'quazip'
   depends_on 'zlib' # R
+  depends_on 'zug'
+
+
 end
