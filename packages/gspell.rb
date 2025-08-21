6bc2e34441123b93c2e138f3a67b07ba27f61bd2
satmandu <satmandu@users.noreply.github.com>
2025-08-21 18:11:35 +0000

    Add unbuilt gspell to updater-gspell-1.14.0

--- packages/gspell.rb
+++ packages/gspell.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Gspell < Meson
   description 'a flexible API to implement the spell checking in a GTK+ application'
   homepage 'https://wiki.gnome.org/Projects/gspell'
-  version "1.13.1-#{CREW_ICU_VER}"
+  version "1.14.0-#{CREW_ICU_VER}"
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gspell.git'
