e46b5df18166be98b46e602936163675022e4f10
satmandu <satmandu@users.noreply.github.com>
2025-08-21 18:11:40 +0000

    Add unbuilt gtk_vnc to updater-gtk_vnc-1.5.0

--- packages/gtk_vnc.rb
+++ packages/gtk_vnc.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Gtk_vnc < Meson
   description 'VNC viewer widget for GTK'
   homepage 'https://wiki.gnome.org/Projects/gtk-vnc'
-  version '1.3.1'
+  version '1.5.0'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gtk-vnc.git'
