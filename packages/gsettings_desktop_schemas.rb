3bffcd039da746d38e46c1d558f88edf9a83fd68
satmandu <satmandu@users.noreply.github.com>
2025-08-21 18:11:31 +0000

    Add unbuilt gsettings_desktop_schemas to updater-gsettings_desktop_schemas-48.0

--- packages/gsettings_desktop_schemas.rb
+++ packages/gsettings_desktop_schemas.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Gsettings_desktop_schemas < Meson
   description 'Collection of GSettings schemas for GNOME desktop.'
   homepage 'https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas'
-  version '46.rc'
+  version '48.0'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gsettings-desktop-schemas.git'
