8c36c5e3b6f951c510b5133433fc689988decc7c
satmandu <satmandu@users.noreply.github.com>
2025-08-21 19:47:32 +0000

    Add unbuilt epiphany to updater-epiphany-48.5

--- packages/epiphany.rb
+++ packages/epiphany.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Epiphany < Meson
   description 'A GNOME web browser based on the WebKit rendering engine'
   homepage 'https://wiki.gnome.org/Apps/Web'
-  version '43.1'
+  version '48.5'
   license 'GPL'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/epiphany.git'
