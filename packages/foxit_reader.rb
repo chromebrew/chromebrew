4e353e4c1b363a7627c05fcc1dce2a20e8677a21
Ed Reel <edreel@gmail.com>
2025-08-22 09:46:46 +0000

    Foxit_reader 2.4.4.0911 => 2.4.5.0727

--- packages/foxit_reader.rb
+++ packages/foxit_reader.rb
@@ -3,46 +3,55 @@ require 'package'
 class Foxit_reader < Package
   description "Industry's most powerful PDF reader. View, annotate, form fill, and sign PDFs."
   homepage 'https://www.foxit.com/pdf-reader/'
-  version '2025.2.0'
+  version '2.4.5.0727'
+  major = version.split('.')[0]
+  minor = version.split('.').slice(0..1).join('.')
   license 'Foxit'
   compatibility 'x86_64'
-  source_url 'SKIP'
+  source_url "https://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/#{major}.x/#{minor}/en_us/FoxitReader.enu.setup.#{version}.x64.run.tar.gz"
+  source_sha256 '633279b6c9fcfa3c45e055df321a87d633ecfea11f668a06e838bb91b5bf6247'
 
   no_compile_needed
 
   depends_on 'sommelier'
 
   def self.install
-    foxit_bin = "#{HOME}/Downloads/FoxitReader.enu.setup.2.4.4.0911.x64.run.tar.gz"
-    foxit_sha256 = '6b579bd4ecdf86f7e70a009886c511da0b5085b831b0d6afc42442cabc249b90'
-    platform = 'Linux-64-bit'
-    unless File.exist? foxit_bin
-      puts "\nCopy and paste into your browser address bar the url below and download to #{HOME}/Downloads.".orange
-      puts "\nhttps://www.foxit.com/downloads/latest.html?product=Foxit-Reader&platform=#{platform}&version=&package_type=&language=English&distID=".orange
-      puts "\nAfter the download is complete, restart and install to #{CREW_DEST_PREFIX}/share/foxitreader.\n".orange
-      abort
+    # Define directories.
+    bin_path = "#{CREW_DEST_PREFIX}/bin"
+    root_path = "#{CREW_PREFIX}/share/foxitreader"
+    dest_path = "#{CREW_DEST_PREFIX}/share/foxitreader"
+    app_path = "#{CREW_DEST_PREFIX}/share/applications"
+    icon_path = "#{CREW_DEST_PREFIX}/share/icons/hicolor/64x64/apps"
+
+    puts "IMPORTANT: Copy #{dest_path} to your clipboard and, when prompted, paste to replace".orange
+    print "the installation folder. Click 'Ignore' if you experience an error during the installation process. Continue? [y/N] ".orange
+    case $stdin.gets.chomp.downcase
+    when 'y', 'yes'
+      # Launch the installation script.
+      system ".'/FoxitReader.enu.setup.#{version}(rb70e8df).x64.run'"
     end
-    abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest(File.read(foxit_bin)) == foxit_sha256
-    system "tar xvf #{foxit_bin}"
-    system "./'FoxitReader.enu.setup.2.4.4.0911(r057d814).x64.run'"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
-    FileUtils.ln_s "#{CREW_PREFIX}/share/foxitreader/FoxitReader", "#{CREW_DEST_PREFIX}/bin/foxit"
-    FileUtils.ln_s "#{CREW_PREFIX}/share/foxitreader/FoxitReader", "#{CREW_DEST_PREFIX}/bin/foxitreader"
-    FileUtils.ln_s "#{CREW_PREFIX}/share/foxitreader/FoxitReader", "#{CREW_DEST_PREFIX}/bin/FoxitReader"
+    abort 'Aborted since installation folder not found.'.lightred unless Dir.exist?(dest_path.to_s)
+
+    # Create directories.
+    FileUtils.mkdir_p bin_path.to_s
+    FileUtils.mkdir_p app_path.to_s
+    FileUtils.mkdir_p icon_path.to_s
+
+    # Add symlinks.
+    FileUtils.ln_s "#{root_path}/FoxitReader", "#{bin_path}/foxit"
+    FileUtils.ln_s "#{root_path}/FoxitReader", "#{bin_path}/foxitreader"
+    FileUtils.ln_s "#{root_path}/FoxitReader", "#{bin_path}/FoxitReader"
 
     # Prepare FoxitReader.desktop and FoxitReader.ico for use with crew-launcher.
-    FileUtils.rm "#{CREW_DEST_PREFIX}/share/foxitreader/Activation.desktop"
-    FileUtils.rm "#{CREW_DEST_PREFIX}/share/foxitreader/Uninstall.desktop"
-    FileUtils.rm "#{CREW_DEST_PREFIX}/share/foxitreader/Update.desktop"
-    system "sed -i 's,#{CREW_DEST_PREFIX}/share/foxitreader/FoxitReader.sh,#{CREW_PREFIX}/share/foxitreader/FoxitReader,' #{CREW_DEST_PREFIX}/share/foxitreader/FoxitReader.desktop"
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/applications"
-    icon_path = "#{CREW_DEST_PREFIX}/share/icons/hicolor/64x64/apps"
-    FileUtils.mkdir_p icon_path.to_s
-    FileUtils.mv "#{CREW_DEST_PREFIX}/share/foxitreader/FoxitReader.desktop", "#{CREW_DEST_PREFIX}/share/applications"
-    FileUtils.mv "#{CREW_DEST_PREFIX}/share/foxitreader/images/FoxitReader-64x64.ico", "#{icon_path}/FoxitReader.ico"
+    FileUtils.rm "#{dest_path}/Activation.desktop"
+    FileUtils.rm "#{dest_path}/Uninstall.desktop"
+    FileUtils.rm "#{dest_path}/Update.desktop"
+    system "sed -i 's,#{dest_path}/FoxitReader.sh,#{root_path}/FoxitReader,' #{dest_path}/FoxitReader.desktop"
+    FileUtils.mv "#{dest_path}/FoxitReader.desktop", app_path.to_s
+    FileUtils.mv "#{dest_path}/images/FoxitReader-64x64.ico", "#{icon_path}/FoxitReader.ico"
   end
 
   def self.postinstall
-    puts "\nType 'foxit' to get started.\n".lightblue
+    ExitMessage.add "\nType 'foxit' to get started.\n"
   end
 end
