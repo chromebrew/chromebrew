895f93486b0073684790b4f2f0b8368b200935c5
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-08-27 15:22:34 -0400

    Update py3_ruff.rb

--- packages/py3_ruff.rb
+++ packages/py3_ruff.rb
@@ -19,7 +19,6 @@ class Py3_ruff < Pip
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'py3_maturin' => :build
-  depends_on 'python3'
   depends_on 'python3' # R
   depends_on 'rust' => :build
 
