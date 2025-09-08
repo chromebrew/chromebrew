c1eaadb4e736378f4a1700cd3a21c4dce8a216b6
Satadru Pramanik <satadru@gmail.com>
2025-09-08 18:42:32 -0400

    Refactor foxit_reader package.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/foxit_reader.rb
+++ packages/foxit_reader.rb
@@ -4,18 +4,30 @@ class Foxit_reader < Package
   description "Industry's most powerful PDF reader. View, annotate, form fill, and sign PDFs."
   homepage 'https://www.foxit.com/pdf-reader/'
   version '2.4.5.0727'
-  major = version.split('.')[0]
-  minor = version.split('.').slice(0..1).join('.')
   license 'Foxit'
   compatibility 'x86_64'
-  source_url "https://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/#{major}.x/#{minor}/en_us/FoxitReader.enu.setup.#{version}.x64.run.tar.gz"
-  source_sha256 '633279b6c9fcfa3c45e055df321a87d633ecfea11f668a06e838bb91b5bf6247'
+  source_url 'SKIP'
 
   no_compile_needed
 
-  depends_on 'sommelier'
+  depends_on 'pkg_7_zip' => :build
+  depends_on 'sommelier' # L
 
   def self.install
+    # Adapted from https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=foxitreader
+    major = version.split('.')[0]
+    minor = version.split('.').slice(0..1).join('.')
+    downloader "https://cdn01.foxitsoftware.com/pub/foxit/reader/desktop/linux/#{major}.x/#{minor}/en_us/FoxitReader.enu.setup.#{version}.x64.run.tar.gz", '633279b6c9fcfa3c45e055df321a87d633ecfea11f668a06e838bb91b5bf6247', 'foxit.x64.run.tar.gz'
+    system 'tar fx foxit.x64.run.tar.gz'
+    system 'mv *.run foxit.x64.run'
+    positions = `LANG=C grep --only-matching --byte-offset --binary \
+              --text $'7z\\xBC\\xAF\\x27\\x1C' foxit.x64.run | cut -f1 -d:`.chomp.split
+    FileUtils.mkdir 'build'
+    positions.each do |position|
+      system "dd if=foxit.x64.run \
+              bs=1M iflag=skip_bytes status=none skip=#{position} \
+              of=foxit-installer-bin-#{position}.7z && 7z -bd -bb0 -y x -obuild foxit-installer-bin-#{position}.7z || true"
+    end
     # Define directories.
     bin_path = "#{CREW_DEST_PREFIX}/bin"
     root_path = "#{CREW_PREFIX}/share/foxitreader"
@@ -23,19 +35,21 @@ class Foxit_reader < Package
     app_path = "#{CREW_DEST_PREFIX}/share/applications"
     icon_path = "#{CREW_DEST_PREFIX}/share/icons/hicolor/64x64/apps"
 
-    puts "IMPORTANT: Copy #{dest_path} to your clipboard and, when prompted, paste to replace".orange
-    print "the installation folder. Click 'Ignore' if you experience an error during the installation process. Continue? [y/N] ".orange
-    case $stdin.gets.chomp.downcase
-    when 'y', 'yes'
-      # Launch the installation script.
-      system ".'/FoxitReader.enu.setup.#{version}(rb70e8df).x64.run'"
-    end
-    abort 'Aborted since installation folder not found.'.lightred unless Dir.exist?(dest_path.to_s)
-
     # Create directories.
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
     FileUtils.mkdir_p bin_path.to_s
     FileUtils.mkdir_p app_path.to_s
     FileUtils.mkdir_p icon_path.to_s
+
+    unneeded_files = %w[
+      Activation Activation.desktop Activation.sh
+      countinstalltion countinstalltion.sh
+      installUpdate ldlibrarypath.sh
+      maintenancetool.sh Uninstall.desktop
+      Update.desktop updater updater.sh
+    ]
+    unneeded_files.each { |f| FileUtils.rm "build/#{f}" }
+    FileUtils.mv 'build', dest_path
 
     # Add symlinks.
     FileUtils.ln_s "#{root_path}/FoxitReader", "#{bin_path}/foxit"
@@ -43,9 +57,6 @@ class Foxit_reader < Package
     FileUtils.ln_s "#{root_path}/FoxitReader", "#{bin_path}/FoxitReader"
 
     # Prepare FoxitReader.desktop and FoxitReader.ico for use with crew-launcher.
-    FileUtils.rm "#{dest_path}/Activation.desktop"
-    FileUtils.rm "#{dest_path}/Uninstall.desktop"
-    FileUtils.rm "#{dest_path}/Update.desktop"
     system "sed -i 's,#{dest_path}/FoxitReader.sh,#{root_path}/FoxitReader,' #{dest_path}/FoxitReader.desktop"
     FileUtils.mv "#{dest_path}/FoxitReader.desktop", app_path.to_s
     FileUtils.mv "#{dest_path}/images/FoxitReader-64x64.ico", "#{icon_path}/FoxitReader.ico"
