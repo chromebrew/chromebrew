e36276d52225936f966e486f2be03016d0c8f45f
Satadru Pramanik <satadru@gmail.com>
2025-09-10 15:30:18 -0400

    Update some packages with incomplete binary hash sections.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_zcmds.rb
+++ packages/py3_zcmds.rb
@@ -10,7 +10,14 @@ class Py3_zcmds < Pip
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
-  binary_sha256({})
+  binary_sha256({
+    aarch64: 'aaaaa',
+     armv7l: 'aaaaa',
+       i686: 'bbbbb',
+     x86_64: 'ccccc'
+  })
+
+  depends_on 'python3' # R
 
   no_source_build
 end
