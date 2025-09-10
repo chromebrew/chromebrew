13e7a134c25d6112fafc3c3e2b722bce3dd299a9
Satadru Pramanik <satadru@gmail.com>
2025-09-10 13:17:31 -0400

    Add python3 anitya exception.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- lib/const.rb
+++ lib/const.rb
@@ -194,6 +194,7 @@ unless defined?(CREW_ANITYA_PACKAGE_NAME_MAPPINGS)
     { pkg_name: 'gvim', anitya_pkg: 'vim', comments: '' },
     { pkg_name: 'pcre2', anitya_pkg: 'pcre2', comments: 'Prefer to GitHub' },
     { pkg_name: 'py3_atspi', anitya_pkg: 'pyatspi', comments: '' },
+    { pkg_name: 'python3', anitya_pkg: 'python', comments: 'Prefer to GitHub' },
     { pkg_name: 'signal_desktop', anitya_pkg: 'signal', comments: '' },
     { pkg_name: 'vim_runtime', anitya_pkg: 'vim', comments: '' },
     { pkg_name: 'webkitgtk_6', anitya_pkg: 'webkitgtk~stable', comments: '' },
