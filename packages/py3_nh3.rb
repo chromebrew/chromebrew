785963492fe37d0690a6463238ae0d3b9b89518d
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-09-11 16:19:45 -0400

    Update py3_nh3.rb

--- packages/py3_nh3.rb
+++ packages/py3_nh3.rb
@@ -22,7 +22,6 @@ class Py3_nh3 < Pip
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R  depends_on 'py3_maturin' => :build
   depends_on 'python3' # R
-  depends_on 'python3' # R
   depends_on 'rust' => :build
 
   no_source_build
