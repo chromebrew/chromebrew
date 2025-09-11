7f1567362ef6b49ef400c144ebe9e9d5c578824a
Ed Reel <edreel@gmail.com>
2025-09-11 14:38:49 -0500

    Termius 9.27.2 => 9.31.0 (#12738)

--- packages/termius.rb
+++ packages/termius.rb
@@ -3,12 +3,12 @@ require 'package'
 class Termius < Package
   description 'Modern SSH Client'
   homepage 'https://termius.com/'
-  version '9.27.2'
+  version '9.31.0'
   license 'Apache-2.0, LGPL-2.1, MIT'
   compatibility 'x86_64'
   min_glibc '2.33'
   source_url 'https://www.termius.com/download/linux/Termius.deb'
-  source_sha256 '5a1086cad7e2335db505a62126fb583b2cb3f5ca8ffc74296b936a8f42812a0a'
+  source_sha256 'e2cebef4ba72f000573d572f180dfeace7611a18b711b3c63df95884c3b719ef'
 
   depends_on 'sommelier'
 
