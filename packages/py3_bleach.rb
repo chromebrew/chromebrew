4eb5bd85b35564f7d8fdf0e9cc5cc759b28f46f6
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-09-11 15:53:36 -0400

    Add pip packages py3_twine seems to need.  (#12735)
    * Update some packages with incomplete binary hash sections.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * revert py3_zcmd changes
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Print directory contents if not empty during install.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Add more py3 packages.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * sync with master
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * update packages.yml
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/py3_bleach.rb
@@ -0,0 +1,25 @@
+# Adapted from Arch Linux python-bleach PKGBUILD at:
+# https://github.com/archlinux/svntogit-community/raw/packages/python-bleach/trunk/PKGBUILD
+
+require 'buildsystems/pip'
+
+class Py3_bleach < Pip
+  description 'An easy whitelist-based HTML-sanitizing tool'
+  homepage 'https://pypi.org/project/bleach/'
+  version '6.0.0'
+  license 'Apache'
+  compatibility 'all'
+  source_url 'SKIP'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: 'df557e12ef5e2f8abdf74e38135e3b3c38ac3f24fc9bd999cbae77db40bdee94',
+     armv7l: 'df557e12ef5e2f8abdf74e38135e3b3c38ac3f24fc9bd999cbae77db40bdee94',
+       i686: '1a85d2dd86282a266ee90f9e04d196572e21a9495bee6e241c52e5dd9074094f',
+     x86_64: '8a5b0b03d9afc652571b1a1e4896d5956ceebd5c3c5d77d4166c6e916399ebc9'
+  })
+
+  depends_on 'python3' # R
+
+  no_source_build
+end
---
+++ packages/py3_nh3.rb
@@ -0,0 +1,28 @@
+# Adapted from Arch Linux python-nh3 PKGBUILD at:
+# https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=python-nh3
+
+require 'buildsystems/pip'
+
+class Py3_nh3 < Pip
+  description 'Ammonia HTML sanitizer Python binding'
+  homepage 'https://pypi.org/project/nh3/'
+  version '0.2.14'
+  license 'MIT'
+  compatibility 'all'
+  source_url 'SKIP'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: 'a40e34470880b4da07313cff19db30477390ee2366dfa1cb9be871fc020d1ad0',
+     armv7l: 'a40e34470880b4da07313cff19db30477390ee2366dfa1cb9be871fc020d1ad0',
+       i686: '48b229dd5f9ebc03fc4885788edf7de8376e4fd48f5181acb87e2dde2e4549e5',
+     x86_64: '87d00ea9fb8aa7f52e1153b84ebfb43c0d04356569287822b298656970242717'
+  })
+
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R  depends_on 'py3_maturin' => :build
+  depends_on 'python3' # R
+  depends_on 'rust' => :build
+
+  no_source_build
+end
