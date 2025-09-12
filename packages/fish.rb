84696c809c864b87b61a548f535e98336ca0eead
satmandu <satmandu@users.noreply.github.com>
2025-09-12 12:35:20 +0000

    Add unbuilt fish to updater-fish-4.0.6

--- packages/fish.rb
+++ packages/fish.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Fish < CMake
   description 'fish is a smart and user-friendly command line shell for macOS, Linux, and the rest of the family.'
   homepage 'https://fishshell.com/'
-  version '3.7.1'
+  version '4.0.6'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/fish-shell/fish-shell.git'
