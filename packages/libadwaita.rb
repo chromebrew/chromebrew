3751024ac268a3dfb4e576a666436fa3482794d6
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-09-13 14:32:41 -0400

    libadwaita => 1.7.6 (#12762)
    * Add unbuilt libadwaita to updater-libadwaita-1.7.6
    
    * Disable broken vapi bindings.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * updater-libadwaita-1.7.6: Build Run on linux/amd64.
    
    * updater-libadwaita-1.7.6: Build Run on linux/arm/v7.
    
    * updater-libadwaita-1.7.6: Package File Update Run on linux/amd64 container.
    
    * fix deps
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: satmandu <satmandu@users.noreply.github.com>
    Co-authored-by: chromebrew-actions[bot] <chromebrew-actions[bot]@users.noreply.github.com>

--- packages/libadwaita.rb
+++ packages/libadwaita.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Libadwaita < Meson
   description 'Library of GNOME-specific UI patterns, replacing libhandy for GTK4'
   homepage 'https://gitlab.gnome.org/GNOME/libadwaita/'
-  version '1.7.2'
+  version '1.7.6'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/libadwaita.git'
@@ -11,42 +11,37 @@ class Libadwaita < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ec3ccbc0da68a25293f9df687cfb20964409f2438ea8f8ae17544dd145ccfab3',
-     armv7l: 'ec3ccbc0da68a25293f9df687cfb20964409f2438ea8f8ae17544dd145ccfab3',
-     x86_64: '5da3bd5e81264b886cd5086ef5345aed9201e0344a3058216c6e228002e29dba'
+    aarch64: 'b0e0b175e7143f603c853093bb0418f211b3fdfd87ee7b2a2fd96bad1d3f527c',
+     armv7l: 'b0e0b175e7143f603c853093bb0418f211b3fdfd87ee7b2a2fd96bad1d3f527c',
+     x86_64: '9e7c5b55f927620903c2ec41557b7f183e9ad7285ad9be6df46c588842dc020a'
   })
 
   depends_on 'appstream' # R
   depends_on 'cairo' => :build
-  depends_on 'curl' # R
   depends_on 'fribidi' # R
   depends_on 'gcc_lib' # R
   depends_on 'gdk_pixbuf' => :build
-  depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'glibc' # R
   depends_on 'gobject_introspection' => :build
   depends_on 'gperf' => :build
   depends_on 'graphene' # R
-  # Keep the gtk4 dependency from being pulled in during a gtk3 build.
-  depends_on 'gtk4' unless ancestors[0].to_s.split('::')[1].downcase == 'gtk3' # R
+  depends_on 'gtk4' unless ancestors[0].to_s.split('::')[1].downcase == 'gtk3' # R Keep the gtk4 dependency from being pulled in during a gtk3 build.
   depends_on 'harfbuzz' => :build
   depends_on 'libjpeg_turbo' => :build
   depends_on 'libsass' => :build
-  depends_on 'libxml2' # R
-  depends_on 'libyaml' # R
   depends_on 'pango' # R
   depends_on 'py3_gi_docgen' => :build
   depends_on 'sassc' => :build
   depends_on 'vala' => :build
   depends_on 'vulkan_headers' => :build
   depends_on 'vulkan_icd_loader' => :build
-  depends_on 'xzutils' # R
-  depends_on 'zstd' # R
 
   gnome
 
   meson_options '-Dintrospection=enabled \
             -Dexamples=false \
             -Ddocumentation=false \
-            -Dtests=false'
+            -Dtests=false \
+            -Dvapi=false'
 end
