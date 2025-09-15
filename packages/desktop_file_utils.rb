7f6c208137b99012164672dd758c600dcc5fa954
Satadru Pramanik <satadru@gmail.com>
2025-09-15 12:48:59 -0400

    Update desktop_file_utils
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/desktop_file_utils.rb
+++ packages/desktop_file_utils.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Desktop_file_utils < Package
+class Desktop_file_utils < Autotools
   description 'contains a few command line utilities for working with desktop entries'
   homepage 'https://www.freedesktop.org/wiki/Software/desktop-file-utils/'
-  version '0.23'
+  version '0.28'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-0.23.tar.xz'
-  source_sha256 '6c094031bdec46c9f621708f919084e1cb5294e2c5b1e4c883b3e70cb8903385'
-  binary_compression 'tar.xz'
+  source_url "https://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-#{version}.tar.xz"
+  source_sha256 '4401d4e231d842c2de8242395a74a395ca468cd96f5f610d822df33594898a70'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'b13f9202b0b0f3d36a7fc0df45a7f321eebf1efc870aa553b2e8c435bfa1a063',
@@ -16,13 +16,4 @@ class Desktop_file_utils < Package
        i686: 'af80702f193a3ec436b122b9f20889b196d51eac01c7c2c5b6e432e466a2c303',
      x86_64: '3f2a161ca73edca9d51619b39bd27acd23eb6259947f41a2d2bfda3a2720695c'
   })
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
