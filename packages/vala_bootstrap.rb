5637b1d21850e9134bf81d10cea914157d1025c3
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-09-14 20:00:39 -0400

    updater-vala-0.56.18 — vala_bootstrap → 0.56.16,vala → 0.56.18 (#12776)
    * Add unbuilt vala to updater-vala-0.56.18
    
    * Refactor vala build.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * updater-vala-0.56.18: Build Run on linux/arm/v7.
    
    * updater-vala-0.56.18: Package File Update Run on linux/amd64 container.
    
    * updater-vala-0.56.18: Package File Update Run on linux/arm/v7 container.
    
    * Update git hash for vala_bootstrap.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: satmandu <satmandu@users.noreply.github.com>
    Co-authored-by: chromebrew-actions[bot] <chromebrew-actions[bot]@users.noreply.github.com>

---
+++ packages/vala_bootstrap.rb
@@ -0,0 +1,50 @@
+require 'package'
+
+class Vala_bootstrap < Package
+  description 'Vala is a programming language that aims to bring modern programming language features to GNOME developers.'
+  homepage 'https://wiki.gnome.org/Projects/Vala'
+  version '0.56.16'
+  license 'LGPL-2.1+'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url 'https://gitlab.gnome.org/Archive/vala-bootstrap.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: 'b0780c098de6f9a613c2c963e390bcebb5bea73222c345874e62fb4f761bafad',
+     armv7l: 'b0780c098de6f9a613c2c963e390bcebb5bea73222c345874e62fb4f761bafad',
+     x86_64: '58f591a559ad11aa772e3107191136c6c4895ca9372fb917a0f22536acd45401'
+  })
+
+  depends_on 'autoconf_archive' => :build
+  depends_on 'autoconf213' => :build
+  depends_on 'graphviz' => :build
+  depends_on 'libxslt' => :build
+  depends_on 'dbus' => :build
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
+
+  def self.build
+    # Ur-bootstrap vala
+    FileUtils.mkdir_p 'bootstrap_install'
+    system 'git clone https://gitlab.gnome.org/Archive/vala-bootstrap.git'
+    Dir.chdir('vala-bootstrap') do
+      system 'git checkout b2beeaccdf2307ced172646c2ada9765e1747b28'
+      system 'touch */*.stamp'
+      system 'autoreconf -fi'
+      system "VALAC=/no-valac ./configure --prefix=#{Dir.pwd}/../bootstrap_install"
+      system 'make'
+      system 'make install'
+    end
+
+    system "VALAC=#{Dir.pwd}/bootstrap_install/bin/valac ./autogen.sh \
+      #{CREW_CONFIGURE_OPTIONS} \
+      --disable-maintainer-mode \
+      --disable-valadoc"
+    system 'make'
+  end
+
+  def self.install
+    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  end
+end
