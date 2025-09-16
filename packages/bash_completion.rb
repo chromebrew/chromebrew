6019dd8e305afd1ba4b2f7c46d820a211be2d36b
Satadru Pramanik <satadru@gmail.com>
2025-09-16 19:06:30 -0400

    Rebuild
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/bash_completion.rb
+++ packages/bash_completion.rb
@@ -11,13 +11,41 @@ class Bash_completion < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '56bf4308e78c4b5ef81289118452f2cbc7fabfc872bacd6cbed42f42ee4afd86',
-     armv7l: '56bf4308e78c4b5ef81289118452f2cbc7fabfc872bacd6cbed42f42ee4afd86',
-       i686: '557efd11b3b8ef2ec4ca9e8c9fc256a7c83ada457f36eccd3aa53877f8c105e6',
-     x86_64: 'ef58935c98389edfb7819eb31f3ffed503a4e02f317609fa7bfb9958e4936757'
+    aarch64: 'b144439590fdc6dea667c35d40e816eb872dda828dc4d5129fd4244e7a2fd4bb',
+     armv7l: 'b144439590fdc6dea667c35d40e816eb872dda828dc4d5129fd4244e7a2fd4bb',
+       i686: 'cb0932c71faa7e6c5c4a7771fc61a72ce1b79990219be04db4d8a742c36f38bd',
+     x86_64: 'ff10f76bdc6d113c4bb26458d01e79270965402b3fdfe1d9f39f6d49589ac7c2'
   })
 
   depends_on 'bash' # L
 
-  conflicts_ok # Conflicts with uutils_coreutils.
+  # Handle conflicts with uutils_coreutils.
+  autotools_install_extras do
+    %w[
+      arch
+      b2sum
+      chgrp
+      chmod
+      chown
+      dd
+      env
+      hostname
+      id
+      kill
+      md5sum
+      mktemp
+      nproc
+      printenv
+      pwd
+      sha1sum
+      sha224sum
+      sha256sum
+      sha384sum
+      sha512sum
+      timeout
+      truncate
+    ].each do |completion|
+      FileUtils.mv "#{CREW_DEST_PREFIX}/share/bash-completion/completions/#{completion}", "#{CREW_DEST_PREFIX}/share/bash-completion/completions/#{completion}_bash"
+    end
+  end
 end
