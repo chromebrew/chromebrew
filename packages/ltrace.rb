be5c12bfe9bc501d1fc16b7016a832f9f1f9c9c7
Zopolis4 <creatorsmithmdt@gmail.com>
2025-09-17 10:22:27 +1000

    ltrace 0.7.91-ea8928 -> 0.8.1

--- packages/ltrace.rb
+++ packages/ltrace.rb
@@ -1,45 +1,27 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Ltrace < Package
+class Ltrace < Autotools
   description 'ltrace intercepts and records dynamic library calls which are called by an executed process and the signals received by that process.'
   homepage 'https://www.ltrace.org/'
-  version '0.7.91-ea8928'
-  license 'GPL-2'
+  version '0.8.1'
+  license 'GPL-2+'
   compatibility 'all'
   source_url 'https://gitlab.com/cespedes/ltrace.git'
-  git_hashtag 'ea8928dab8a0a1f549d0ed8ebc6ec563e9fa1159'
-  binary_compression 'tpxz'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '09bc63d26ef561e10f01628d61eb4a6c8d5caeb24a77f19deddb5fa1a5867dd2',
-     armv7l: '09bc63d26ef561e10f01628d61eb4a6c8d5caeb24a77f19deddb5fa1a5867dd2',
-       i686: 'fc5ca046f26608668f2885a0389d47baf7fea535503366c79464bb9c0014873d',
-     x86_64: '69ce6519d523f4b225e28f421f766c4f502a0e970e093a1d3beba117904898a7'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+       i686: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
 
   depends_on 'libunwind'
   depends_on 'procps'
 
-  def self.patch
-    # Use readdir instead of deprecated readdir_r.
-    # This patch can be removed after the merge request is merged.
-    system 'curl -L#O https://gitlab.com/cespedes/ltrace/-/merge_requests/1.diff'
-    abort 'Checksum mismatch. 😔 Try again.'.lightred unless Digest::SHA256.hexdigest(File.read('1.diff')) == '77442c497bd8410e0afba3a03638a6504ed9ae216bd694a771682f592a9c3759'
-    system 'patch -p1 < 1.diff'
-  end
 
-  def self.build
-    system './autogen.sh'
-    system 'filefix'
-    system "./configure #{CREW_CONFIGURE_OPTIONS} --disable-werror --without-elfutils --disable-maintainer-mode"
-    system 'make'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  # TODO: waiting on https://gitlab.com/cespedes/ltrace/-/merge_requests/27
+  # ngl im not making this a patch i can wait for this to be a clean release
+  autotools_configure_options '--disable-werror --without-elfutils'
 end
