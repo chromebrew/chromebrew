874552ca011ad2f7742fec875a891057f4b51d7d
Ed Reel <edreel@gmail.com>
2025-09-17 02:57:10 -0500

    Vivaldi 7.5.3735.66-1 => 7.5.3735.74-1

--- packages/vivaldi.rb
+++ packages/vivaldi.rb
@@ -4,7 +4,7 @@ require 'convenience_functions'
 class Vivaldi < Package
   description 'Vivaldi is a new browser that blocks unwanted ads, protects you from trackers, and puts you in control with unique built-in features.'
   homepage 'https://vivaldi.com/'
-  version '7.5.3735.66-1'
+  version '7.5.3735.74-1'
   license 'Vivaldi'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.37'
@@ -24,10 +24,10 @@ class Vivaldi < Package
   case ARCH
   when 'aarch64', 'armv7l'
     arch = 'armhf'
-    source_sha256 'd2ca9c354056ba16ca254f55f16f95ee5a78eaea5abab84c55b9cb03f0cb1aff'
+    source_sha256 '9017e6327c140ad9a9e1f0ce450681a729a15ea764337c30226f51c042ff7e62'
   when 'x86_64'
     arch = 'amd64'
-    source_sha256 '1138fdd7e013fdf17c50186ab233894384a76ca205e896ff4f85e6b7732eb663'
+    source_sha256 '6828213bae065598c2aa79b38fb187a448549363e577654a4b8e06ffe125a205'
   end
 
   source_url "https://downloads.vivaldi.com/stable/vivaldi-stable_#{version}_#{arch}.deb"
