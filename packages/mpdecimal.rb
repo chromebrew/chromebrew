e57f406380c1d8d151ef7076dcfe04c992c0f9c6
Satadru Pramanik <satadru@gmail.com>
2025-09-17 17:42:28 -0400

    mpdecimal => 4.0.1
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/mpdecimal.rb
+++ packages/mpdecimal.rb
@@ -1,17 +1,17 @@
 # Adapted from Arch Linux mpdecimal PKGBUILD at:
 # https://github.com/archlinux/svntogit-packages/raw/packages/mpdecimal/trunk/PKGBUILD
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Mpdecimal < Package
+class Mpdecimal < Autotools
   description 'Package for correctly-rounded arbitrary precision decimal floating point arithmetic'
   homepage 'https://www.bytereef.org/mpdecimal/index.html'
-  version '2.5.1'
+  version '4.0.1'
   license 'BSD'
   compatibility 'all'
-  source_url 'https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-2.5.1.tar.gz'
-  source_sha256 '9f9cd4c041f99b5c49ffb7b59d9f12d95b683d88585608aa56a6307667b2b21f'
-  binary_compression 'tpxz'
+  source_url "https://www.bytereef.org/software/mpdecimal/releases/mpdecimal-#{version}.tar.gz"
+  source_sha256 '96d33abb4bb0070c7be0fed4246cd38416188325f820468214471938545b1ac8'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'e995ff521db72f576566f4b7b9d9a5e4474be854a4e9d5177872b2185dc4ccbe',
@@ -19,13 +19,4 @@ class Mpdecimal < Package
        i686: '683d89e09f6826a1ee6c70cdafa00e5fbd5fe6cc472b8141afb8f6dd989aba9d',
      x86_64: '9eeeca2110c6be8a862f76d81910e070b60502d3542cc24842422b771b5292fd'
   })
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
-  end
 end
