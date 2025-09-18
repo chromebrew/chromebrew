37e6ff27da838f78299cb13ed37c995cf106d767
satmandu <satmandu@users.noreply.github.com>
2025-09-18 19:11:08 +0000

    Add unbuilt py3_gpt_researcher to updater-py3_gpt_researcher-0.14.4

--- packages/py3_gpt_researcher.rb
+++ packages/py3_gpt_researcher.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_gpt_researcher < Pip
   description 'LLM based autonomous agent that conducts deep local and web research on any topic and generates a long report with citations.'
   homepage 'https://gptr.dev/'
-  version "0.14.3-#{CREW_PY_VER}"
+  version "0.14.4-#{CREW_PY_VER}"
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
