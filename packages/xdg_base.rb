6d8027e3b5dd02598901190cb04d6211a88f4fce
Satadru Pramanik <satadru@gmail.com>
2025-09-19 11:11:45 -0400

    Update xdg_base package.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/xdg_base.rb
+++ packages/xdg_base.rb
@@ -3,18 +3,20 @@ require 'package'
 class Xdg_base < Package
   description 'XDG Base Directory Specification Configuration'
   homepage 'https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html'
-  version '0.7-7-1'
+  version '0.8'
   license 'GPL-3+'
   compatibility 'all'
   source_url 'SKIP'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2ec80e4af5d9791083b8004542b4fda18160a8773ef42a7390ec3018927c37ea',
-     armv7l: '2ec80e4af5d9791083b8004542b4fda18160a8773ef42a7390ec3018927c37ea',
-       i686: '34df15da8d205ac30a6a439829851502676e9f3484d1c823cdffb65eeba91327',
-     x86_64: '6e8adac27ccf434f6e4c227487cc6d0ab262093c34095170ec351537a003981b'
+    aarch64: '732748fcdc3667c4325136a0fff23eff880996e5b3a2161b51782243c10547ea',
+     armv7l: '732748fcdc3667c4325136a0fff23eff880996e5b3a2161b51782243c10547ea',
+       i686: '50372f2b3b0a72b5297e4e9e2635297850c9924e6e3cfb89ae1bcf9a6828bf83',
+     x86_64: 'aa41edb071a49e784fff9aa4b1bea1de3fa370d422c7a686bc5a10c158646b0a'
   })
+
+  no_source_build
 
   def self.preinstall
     # Save any previous configuration, if it exists.
@@ -24,13 +26,11 @@ class Xdg_base < Package
     end
     if File.directory?("#{HOME}/.local") && !File.symlink?("#{HOME}/.local") && !FileUtils.cp_r("#{HOME}/.local/.",
                                                                                                 "#{CREW_PREFIX}/.config/")
-      # FileUtils.mkdir_p("#{CREW_PREFIX}/.config") unless Dir.exist? "#{CREW_PREFIX}/.config"
       FileUtils.rm_rf("#{HOME}/.local")
     end
   end
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
     @xdgbaseenv = <<~XDGBASEEOF
       # Chromebrew's XDG configuration
       # See https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
@@ -40,13 +40,16 @@ class Xdg_base < Package
       export XDG_CONFIG_HOME=#{CREW_PREFIX}/.config
       export XDG_DATA_DIRS=#{CREW_PREFIX}/share
       export XDG_DATA_HOME=#{CREW_PREFIX}/.config/.local/share
+      export XDG_STATE_HOME=#{CREW_PREFIX}/.config/.local/state
       export XDG_RUNTIME_DIR=/var/run/chrome
     XDGBASEEOF
-    File.write("#{CREW_DEST_PREFIX}/etc/env.d/xdg_base", @xdgbaseenv)
+    File.write('xdg_base', @xdgbaseenv)
+    FileUtils.install 'xdg_base', "#{CREW_DEST_PREFIX}/etc/env.d/xdg_base", mode: 0o644
   end
 
   def self.postinstall
     FileUtils.mkdir_p "#{CREW_PREFIX}/.config/.local/share"
+    FileUtils.mkdir_p "#{CREW_PREFIX}/.config/.local/state"
     FileUtils.mkdir_p "#{CREW_PREFIX}/.cache"
     FileUtils.mkdir_p "#{CREW_PREFIX}/etc/xdg"
   end
