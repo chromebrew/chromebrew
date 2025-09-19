56cc851cc419d278c22f735b4507186c57356fe0
Satadru Pramanik <satadru@gmail.com>
2025-09-19 12:11:28 -0400

    rdfind => 1.7.0
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/rdfind.rb
+++ packages/rdfind.rb
@@ -1,12 +1,12 @@
 # Adapted from Arch Linux rdfind PKGBUILD at:
 # https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=rdfind
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Rdfind < Package
+class Rdfind < Autotools
   description 'Redundant data find - a program that finds duplicate files.'
   homepage 'https://rdfind.pauldreik.se/'
-  version '1.6.0'
+  version '1.7.0'
   license 'GPL2'
   compatibility 'all'
   source_url 'https://github.com/pauldreik/rdfind.git'
@@ -14,29 +14,20 @@ class Rdfind < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4ca9417e258b5ff07bf3a102278e9b963656c443478b2aeec1eb8cba3361d5e1',
-     armv7l: '4ca9417e258b5ff07bf3a102278e9b963656c443478b2aeec1eb8cba3361d5e1',
-       i686: '6c6af3b3f4628b9a7aa8c427d2673ef7b3122343f555646895337727437a126f',
-     x86_64: '741d44e5fe82ac4a791f09fe7fa019c8398cea277c764221d8f178d33f897eb3'
+    aarch64: 'a8c69c6491066c5ec4e4d03d93e8ad0b52f0ed860726fc30c6811a9640eb1ef8',
+     armv7l: 'a8c69c6491066c5ec4e4d03d93e8ad0b52f0ed860726fc30c6811a9640eb1ef8',
+       i686: '89b9632df969a15d3cf6004e8ba93ce79219a1b4f0e306c729cfb2e0e537b298',
+     x86_64: '5fce0767f527c9f1f0af226aad64a21d992f9ed9dcfba497a180bfb18d33a2c7'
   })
 
   depends_on 'autoconf_archive' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'nettle' # R
+  depends_on 'xxhash' # R
 
   def self.patch
     system "sed -i '/<vector>/a #include <limits>' rdfind.cc"
     system "sed -i '/<cstdio>/a #include <stdexcept>' Checksum.cc"
-  end
-
-  def self.build
-    system '[ -x configure ] || autoreconf -fvi'
-    system "./configure #{CREW_CONFIGURE_OPTIONS} #{CREW_ENV_OPTIONS}"
-    system 'make -s'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
