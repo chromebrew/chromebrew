6ea60d1826cb67f3f58e1ed16ca4ed8ea5050b79
Satadru Pramanik <satadru@gmail.com>
2025-09-19 12:21:59 -0400

    Fixup anitya mappings and package versions.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/intltool.rb
+++ packages/intltool.rb
@@ -1,12 +1,12 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Intltool < Package
+class Intltool < Autotools
   description 'intltool is a set of tools to centralize translation of many different file formats using GNU gettext-compatible PO files.'
   homepage 'https://freedesktop.org/wiki/Software/intltool/'
   version '0.51.0-1'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://launchpad.net/intltool/trunk/0.51.0/+download/intltool-0.51.0.tar.gz'
+  source_url "https://launchpad.net/intltool/trunk/#{version.split('-').first}/+download/intltool-#{version.split('-').first}.tar.gz"
   source_sha256 '67c74d94196b153b774ab9f89b2fa6c6ba79352407037c8c14d5aeb334e959cd'
   binary_compression 'tar.xz'
 
@@ -25,14 +25,5 @@ class Intltool < Package
     system 'curl -#LO https://raw.githubusercontent.com/Alexpux/MSYS2-packages/master/intltool/perl-5.22-compatibility.patch'
     abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest(File.read('perl-5.22-compatibility.patch')) == '9c6527072aada6e3cb9aceb6e07cfdf51d58839a2beb650168da0601a85ebda3'
     system 'patch intltool-update.in perl-5.22-compatibility.patch'
-  end
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
   end
 end
