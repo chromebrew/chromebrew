7cc09a5efda505fc59ee7b90e12faa08e7e1fec1
Satadru Pramanik <satadru@gmail.com>
2025-09-22 19:47:48 -0400

    Adjust compatibility
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/yaru.rb
+++ packages/yaru.rb
@@ -5,7 +5,7 @@ class Yaru < Meson
   homepage 'https://github.com/ubuntu/yaru'
   version '25.10.2-0ubuntu1'
   license 'GPL-3 and CC-BY-SA-4.0'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/ubuntu/yaru.git'
   git_hashtag version
   binary_compression 'tar.zst'
@@ -13,7 +13,6 @@ class Yaru < Meson
   binary_sha256({
     aarch64: '41ab422c7136ed3f8c6c959687a7808615624857d885a58cc366bf3aeafcacb8',
      armv7l: '41ab422c7136ed3f8c6c959687a7808615624857d885a58cc366bf3aeafcacb8',
-       i686: 'a87c2a4231a968a2d4bb30283d1a84103ddf225651444368a173b30472a3a398',
      x86_64: 'dc2b68a4a1c4a0c5a2efe8ee180e5d773629e61edeaa543bc7ef45bc1b91348c'
   })
 
