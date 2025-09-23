27fea2d515f55731abca7e07b5cc54a39c2322f5
Ed Reel <edreel@gmail.com>
2025-09-23 07:04:18 -0500

    Bitcoin_core 29.0 => 29.1 (#12894)

--- packages/bitcoin_core.rb
+++ packages/bitcoin_core.rb
@@ -3,19 +3,29 @@ require 'package'
 class Bitcoin_core < Package
   description 'Bitcoin Core is a full Bitcoin client and builds the backbone of the network.'
   homepage 'https://bitcoincore.org/'
-  version '29.0'
+  version '29.1'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
+  min_glibc '2.29'
   source_url({
     aarch64: "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-arm-linux-gnueabihf.tar.gz",
      armv7l: "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-arm-linux-gnueabihf.tar.gz",
      x86_64: "https://bitcoincore.org/bin/bitcoin-core-#{version}/bitcoin-#{version}-x86_64-linux-gnu.tar.gz"
   })
   source_sha256({
-    aarch64: 'ea8ca24ab56d486a55289c43cb4256f9f0e66224899cc43482c9498a3f2614d1',
-     armv7l: 'ea8ca24ab56d486a55289c43cb4256f9f0e66224899cc43482c9498a3f2614d1',
-     x86_64: 'a681e4f6ce524c338a105f214613605bac6c33d58c31dc5135bbc02bc458bb6c'
+    aarch64: 'aec1838105c44a97e10bd7a47af52d83728c98272f9e32650b3fe83ed78f9dec',
+     armv7l: 'aec1838105c44a97e10bd7a47af52d83728c98272f9e32650b3fe83ed78f9dec',
+     x86_64: '2dddeaa8c0626ec446b6f21b64c0f3565a1e7e67ff0b586d25043cbd686c9455'
   })
+
+  # Dependencies needed for bitcoin-qt
+  depends_on 'fontconfig'
+  depends_on 'libxkbcommon'
+  depends_on 'xcb_util'
+  depends_on 'xcb_util_image'
+  depends_on 'xcb_util_keysyms'
+  depends_on 'xcb_util_renderutil'
+  depends_on 'xcb_util_wm'
 
   no_compile_needed
 
@@ -24,5 +34,12 @@ class Bitcoin_core < Package
     FileUtils.mv 'bin/', CREW_DEST_PREFIX
     FileUtils.mv 'share/', CREW_DEST_PREFIX
     FileUtils.mv 'bitcoin.conf', "#{CREW_DEST_PREFIX}/etc"
+  end
+
+  def self.postremove
+    if Dir.exist? "#{HOME}/.bitcoin"
+      puts 'WARNING: This will remove all bitcoin data including wallets!'.orange
+      Package.agree_to_remove("#{HOME}/.bitcoin")
+    end
   end
 end
