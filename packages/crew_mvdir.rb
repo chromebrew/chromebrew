7efd263dec00f1a6844154659fbfcea1beabee14
Satadru Pramanik <satadru@gmail.com>
2025-09-24 14:34:32 -0400

    Rebuild crew-mvdir
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/crew_mvdir.rb
+++ packages/crew_mvdir.rb
@@ -11,19 +11,24 @@ class Crew_mvdir < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '49f18debfacdb14a5c9bebc8537e2cb7415175dc5fca46f49393e79a30df166a',
-     armv7l: '49f18debfacdb14a5c9bebc8537e2cb7415175dc5fca46f49393e79a30df166a',
-       i686: '446dd4251251aea7ba3461e7f5453909f73dc815e8d422f284b57b4dd1cf7958',
-     x86_64: '052256b4db73db3fafdea1d6ab681ec3e513ff648bcf430614423e56ccd31707'
+    aarch64: '4324373576d266422033fb835b4062c691398eb2b4f8ffbf12dc1931656981ae',
+     armv7l: '4324373576d266422033fb835b4062c691398eb2b4f8ffbf12dc1931656981ae',
+       i686: '5e424ec6654f40a69d57240845c809c73665033bc2a378ecf97ee4dec601d64a',
+     x86_64: 'e7bd6d453cf491be2d1170f189ddb4d7aaf8e7d6b126a6cc34b77380dbbad04e'
   })
 
   depends_on 'glibc' # R
+  depends_on 'ruby' # R
 
   def self.build
-    system "cc #{CREW_COMMON_FLAGS} crew-mvdir.c -o crew-mvdir"
+    system './build.sh'
   end
 
   def self.install
-    FileUtils.install 'crew-mvdir', "#{CREW_DEST_PREFIX}/bin/crew-mvdir", mode: 0o755
+    Dir.chdir('builddir') do
+      FileUtils.install 'crew-mvdir', "#{CREW_DEST_PREFIX}/bin/crew-mvdir", mode: 0o755
+      FileUtils.install 'crew-mvdir.so.1', "#{CREW_DEST_LIB_PREFIX}/crew-mvdir.so.1", mode: 0o755
+      FileUtils.install 'crew_mvdir.so', "#{CREW_DEST_LIB_PREFIX}/crew_mvdir.so", mode: 0o755
+    end
   end
 end
