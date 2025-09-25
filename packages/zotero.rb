d9ef575a6b092a45e3be2db8276d1d0ab970c304
Satadru Pramanik <satadru@gmail.com>
2025-09-25 17:57:56 -0400

    zotero => 7.0.24
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/zotero.rb
+++ packages/zotero.rb
@@ -3,11 +3,11 @@ require 'package'
 class Zotero < Package
   description 'Zotero is a free, easy-to-use tool to help you collect, organize, annotate, cite, and share research.'
   homepage 'https://www.zotero.org/'
-  version '7.0.25'
+  version '7.0.24'
   license 'GPL-3'
   compatibility 'x86_64'
   source_url "https://download.zotero.org/client/release/#{version}/Zotero-#{version}_linux-x86_64.tar.bz2"
-  source_sha256 '3f98aebf6ed9aa637e8ec15c1417358f60e6164943a3ce2452ddf9354b8ca891'
+  source_sha256 'a9b78cb2ba163d29036ee7ec8b7c88ebae4370a3fbf5255d74949069ddb5d6a0'
 
   depends_on 'dbus_glib'
   depends_on 'gtk3'
