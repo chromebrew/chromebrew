a7e19c4dd11910156433bd048df7b7248a6e967c
satmandu <satmandu@users.noreply.github.com>
2025-09-25 12:06:44 +0000

    updater-py3_maturin-1.9.4: Package File Update Run on linux/arm/v7 container.

--- packages/py3_maturin.rb
+++ packages/py3_maturin.rb
@@ -14,8 +14,8 @@ class Py3_maturin < Python
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6e1e8e8e3f3de1e532d0b34b138c6790ab21d0f776ca031e1bba5882a66aef15',
-     armv7l: '6e1e8e8e3f3de1e532d0b34b138c6790ab21d0f776ca031e1bba5882a66aef15',
+    aarch64: '90b40e4d0f4294a7baea59100c907af065cd81b05ffba445384c9be706e4ec26',
+     armv7l: '90b40e4d0f4294a7baea59100c907af065cd81b05ffba445384c9be706e4ec26',
        i686: '499f7b0411a6211db7a70d063fd2e0a17b272e26c2e1d399c80f7e750db93af9',
      x86_64: '1e1afeb8573c21bfa76330e952e04ee94b4a3416879d77a8060f524889049e01'
   })
