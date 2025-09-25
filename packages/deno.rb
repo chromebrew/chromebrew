81af7d931a1382c3115e5b0b8fdc43e5a177eb8b
Satadru Pramanik <satadru@gmail.com>
2025-09-25 11:47:45 -0400

    Add deno binaries.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/deno.rb
@@ -0,0 +1,30 @@
+# Adapted from Arch Linux deno PKGBUILD at:
+# https://github.com/archlinux/svntogit-community/raw/packages/deno/trunk/PKGBUILD
+
+require 'buildsystems/rust'
+
+class Deno < RUST
+  description 'A secure runtime for JavaScript and TypeScript'
+  homepage 'https://deno.land'
+  version '2.5.2'
+  license 'MIT'
+  compatibility 'aarch64 x86_64'
+  source_url 'https://github.com/denoland/deno.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+     x86_64: '0cad3fd862366c2da84a7edff54c6af5bd75b69207f6b293db2dfc1b11341124'
+  })
+
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'llvm_dev' => :build
+  depends_on 'protobuf' => :build
+
+  def self.install
+    FileUtils.install 'target/release/deno', "#{CREW_DEST_PREFIX}/bin/deno", mode: 0o755
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/bash-completion/completions"
+    system "./target/release/deno completions bash > #{CREW_DEST_PREFIX}/share/bash-completion/completions/deno"
+  end
+end
