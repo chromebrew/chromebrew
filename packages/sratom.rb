0941bb0d66a1fc2adc0d2dca0e957234bc1d94b7
satmandu <satmandu@users.noreply.github.com>
2025-09-27 01:29:30 +0000

    Add unbuilt sratom to updater-sratom-0.6.18

--- packages/sratom.rb
+++ packages/sratom.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Sratom < Meson
   description 'An LV2 Atom RDF serialisation library'
   homepage 'https://drobilla.net/software/sratom/'
-  version '0.6.14'
+  version '0.6.18'
   license 'ISC'
   compatibility 'all'
   source_url 'https://github.com/lv2/sratom.git'
