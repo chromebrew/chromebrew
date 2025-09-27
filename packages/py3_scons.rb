38d8b2a02f469a334d4df6490bc8927f482b2a35
satmandu <satmandu@users.noreply.github.com>
2025-09-27 15:16:40 +0000

    updater-scons-4.9.1: Package File Update Run on linux/arm/v7 container.

--- packages/py3_scons.rb
+++ packages/py3_scons.rb
@@ -10,8 +10,8 @@ class Py3_scons < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b9367afb7551a94f38bf1ec27316dfd438dbbe120c0e9b20b3b76eebe7e16005',
-     armv7l: 'b9367afb7551a94f38bf1ec27316dfd438dbbe120c0e9b20b3b76eebe7e16005',
+    aarch64: '5f3171ec6f6503dcfedc593f150f65e547c392ce48d84c3ec9b01eb456314a4a',
+     armv7l: '5f3171ec6f6503dcfedc593f150f65e547c392ce48d84c3ec9b01eb456314a4a',
        i686: '282fba9583ce3404692223a84e7fcf0531fb16acdff68bb5307da31cd2ad4934',
      x86_64: 'be69ff1da1d5e13461856345b9821e7923308e2deabca0f8fb7d140b7cc28350'
   })
