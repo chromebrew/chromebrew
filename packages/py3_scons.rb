9b5b42885e59dc038f2e3e86be4e2b2d33245463
Satadru Pramanik <satadru@gmail.com>
2025-09-27 11:08:38 -0400

    scons => py3_scons
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_scons.rb
+++ packages/py3_scons.rb
@@ -1,13 +1,12 @@
-require 'buildsystems/python'
+require 'buildsystems/pip'
 
-class Scons < Python
+class Py3_scons < Pip
   description 'SCons is an Open Source software construction tool that is, a next-generation build tool.'
   homepage 'https://scons.org/'
   license 'MIT'
   version '4.9.1'
   compatibility 'all'
-  source_url 'https://github.com/SCons/scons.git'
-  git_hashtag version
+  source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -17,5 +16,6 @@ class Scons < Python
      x86_64: '399b23e77cc424b794f4f699ba1775764049bc1192f036e551ac062b7d50f351'
   })
 
-  depends_on 'python3'
+  depends_on 'python3' # R
+  no_source_build
 end
