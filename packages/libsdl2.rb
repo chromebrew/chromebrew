b6d8ba7c7a8f376dea20c49d2170b41773e6dd9d
Satadru Pramanik <satadru@gmail.com>
2025-09-29 19:37:41 -0400

    lint
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libsdl2.rb
+++ packages/libsdl2.rb
@@ -2,7 +2,7 @@ require 'package'
 Package.load_package("#{__dir__}/sdl2.rb")
 
 class Libsdl2 < Package
-  description  Sdl2.description
+  description Sdl2.description
   homepage Sdl2.homepage
   version Sdl2.version
   license Sdl2.license
