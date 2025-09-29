445aba1dab6d46a3f94a0e476a7b5d019c48e742
Satadru Pramanik <satadru@gmail.com>
2025-09-29 19:33:50 -0400

    Add libsdl2 stub
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/libsdl2.rb
@@ -0,0 +1,14 @@
+require 'package'
+Package.load_package("#{__dir__}/sdl2.rb")
+
+class Libsdl2 < Package
+  description  Sdl2.description
+  homepage Sdl2.homepage
+  version Sdl2.version
+  license Sdl2.license
+  compatibility Sdl2.compatibility
+
+  is_fake
+
+  depends_on 'sdl2'
+end
