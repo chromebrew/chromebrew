f1e3b3a355a5ed27f1712c361bd821a72b7bd90b
Satadru Pramanik <satadru@gmail.com>
2025-09-30 14:09:51 -0400

    Refactor and update webkit2gtk_4_1
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/webkit2gtk_4_1.rb
+++ packages/webkit2gtk_4_1.rb
@@ -1,13 +1,13 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Webkit2gtk_4_1 < Package
+class Webkit2gtk_4_1 < CMake
   description 'Web content engine for GTK'
   homepage 'https://webkitgtk.org'
-  version "2.44.3-#{CREW_ICU_VER}"
+  version "2.50.0"
   license 'LGPL-2+ and BSD-2'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://webkitgtk.org/releases/webkitgtk-2.44.3.tar.xz'
-  source_sha256 'dc82d042ecaca981a4852357c06e5235743319cf10a94cd36ad41b97883a0b54'
+  source_url "https://webkitgtk.org/releases/webkitgtk-#{version.split('-').first}.tar.xz"
+  source_sha256 'e564b8099f9a3ae32409539b290bbd2ad084e99b6d22d4aac5e51e4554df8bc2'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -64,6 +64,7 @@ class Webkit2gtk_4_1 < Package
   depends_on 'py3_gi_docgen' => :build
   depends_on 'py3_smartypants' => :build
   depends_on 'sqlite' # R
+  depends_on 'sysprof' => :build
   depends_on 'unifdef' => :build
   depends_on 'valgrind' => :build
   depends_on 'vulkan_icd_loader'
@@ -73,57 +74,31 @@ class Webkit2gtk_4_1 < Package
   depends_on 'zlib' # R
 
   no_env_options
+  no_lto
 
   def self.patch
     # Fix incompatibility with gtk3 from current gobjects_introspection causing build failure
     # as per https://bugs.webkit.org/show_bug.cgi?id=276180 .
-    downloader 'https://github.com/WebKit/WebKit/pull/30446.diff', '6beda7960b232117f8445db4e588a45ef384d42ccb13f6926b695c472a4eea51'
-    system 'patch -Np1 -i 30446.diff'
+    # downloader 'https://github.com/WebKit/WebKit/pull/30446.diff', '67ab9642e9766d9c9d9cbe52032edc6ddee05f690851d890eeda60d9cdd9cc30'
+    # system 'patch -Np1 -i 30446.diff'
     system "sed -i 's,/usr/bin,/usr/local/bin,g' Source/JavaScriptCore/inspector/scripts/codegen/preprocess.pl"
-    @arch_flags = ''
-    @gcc_ver = ''
-    @arch_flags = '-mfloat-abi=hard -mtls-dialect=gnu -mthumb -mfpu=vfpv3-d16 -mlibarch=armv7-a+fp -march=armv7-a+fp' if ARCH == 'armv7l' || ARCH == 'aarch64'
   end
 
-  def self.build
-    # This builds webkit2gtk4_1 (which uses gtk3, but not libsoup2)
-    @workdir = Dir.pwd
-    # Bubblewrap sandbox breaks on epiphany with
-    # bwrap: Can't make symlink at /var/run: File exists
-    # LDFLAGS from debian: -Wl,--no-keep-memory
-    unless File.file?('build.ninja')
-      @arch_linker_flags = ARCH == 'x86_64' ? '' : '-Wl,--no-keep-memory'
-      system "CREW_LINKER_FLAGS='#{@arch_linker_flags}' \
-          cmake -B builddir -G Ninja \
-          #{CREW_CMAKE_OPTIONS.sub('-pipe', '-pipe -Wno-error').gsub('-flto=auto', '').sub('-DCMAKE_INTERPROCEDURAL_OPTIMIZATION=TRUE', '')} \
-          -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
+  cmake_options "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
           -DENABLE_BUBBLEWRAP_SANDBOX=OFF \
           -DENABLE_DOCUMENTATION=OFF \
-          -DENABLE_JOURNALD_LOG=OFF \
           -DENABLE_GAMEPAD=OFF \
+          -DENABLE_JOURNALD_LOG=OFF \
           -DENABLE_MINIBROWSER=ON \
-          -DUSE_SYSTEM_MALLOC=ON \
+          -DENABLE_SPEECH_SYNTHESIS=OFF \
           -DPORT=GTK \
+          -DPYTHON_EXECUTABLE=`which python` \
+          -DUSER_AGENT_BRANDING='Chromebrew' \
           -DUSE_GTK4=OFF \
           -DUSE_JPEGXL=ON \
-          -DUSE_SOUP2=OFF \
-          -DPYTHON_EXECUTABLE=`which python` \
-          -DUSER_AGENT_BRANDING='Chromebrew'"
-    end
-    @counter = 1
-    @counter_max = 20
-    loop do
-      break if Kernel.system "#{CREW_NINJA} -C builddir -j #{CREW_NPROC}"
+          -DUSE_SOUP2=OFF"
 
-      puts "Make iteration #{@counter} of #{@counter_max}...".orange
-
-      @counter += 1
-      break if @counter > @counter_max
-    end
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
-    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/WebKitWebDriver", "#{CREW_DEST_PREFIX}/bin/WebKitWebDriver_4.1"
+  cmake_install_extras do
+      FileUtils.mv "#{CREW_DEST_PREFIX}/bin/WebKitWebDriver", "#{CREW_DEST_PREFIX}/bin/WebKitWebDriver_4.1"
   end
 end
