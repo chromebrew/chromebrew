bdc3f47e322ac1ca4546d795332bb524488e744e
Satadru Pramanik <satadru@gmail.com>
2025-10-03 22:30:16 -0400

    Update procps
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/procps.rb
+++ packages/procps.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Procps < Package
+class Procps < Autotools
   description 'procps is a set of command line and full-screen utilities that provide information out of the pseudo-filesystem most commonly located at /proc.'
   homepage 'https://gitlab.com/procps-ng/procps'
-  version '3.3.16'
+  version '4.0.5'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://gitlab.com/procps-ng/procps/-/archive/v3.3.16/procps-v3.3.16.tar.bz2'
-  source_sha256 '720caf307ab5dfe6d1cf4fc3e6ce786d749c69baa088627dbe1b01828f2528b1'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.com/procps-ng/procps.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'c126d133d112220e9b055f58ac5f71970344df4e88d5475d5005252914fe3ae0',
@@ -16,14 +16,4 @@ class Procps < Package
        i686: '907304652a1d986d6864498115d980d4b9ed73939a9b8f2f8ae2772ad373ee90',
      x86_64: '4365db9d4e3397c9c1f48d6366c44a6351b19a9a1dafb142fb65e7d614b36a6e'
   })
-
-  def self.build
-    system './autogen.sh'
-    system "CFLAGS='-I#{CREW_PREFIX}/include/ncursesw' ./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
