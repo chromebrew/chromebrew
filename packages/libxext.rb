794969a1dbe82dee62bc6fc402e95a86c55f58d4
satmandu <satmandu@users.noreply.github.com>
2025-10-04 21:49:40 +0000

    updater-libxfont2-2.0.7: Package File Update Run on linux/amd64 container.

--- packages/libxext.rb
+++ packages/libxext.rb
@@ -13,17 +13,17 @@ class Libxext < Autotools
   binary_sha256({
     aarch64: '5ed26f102a7fefc71259c3849ba8549d6cc11b6af74a73bafe333c8ee880120b',
      armv7l: '5ed26f102a7fefc71259c3849ba8549d6cc11b6af74a73bafe333c8ee880120b',
-     x86_64: '0d9a949182965e1eb29f6c27c20b74dd30adef38cad9f169d09e662730777ae0'
+     x86_64: 'f1284ee9e7b256da531edad962a014302d6d627d8beb60a30af5cd6c751d1a34'
   })
 
   depends_on 'glibc' # R
+  depends_on 'libbsd' # R
   depends_on 'libx11' # R
   depends_on 'libxau' # R
   depends_on 'libxcb' # R
   depends_on 'libxdmcp' # R
   depends_on 'llvm_lib' => :build
   depends_on 'xmlto' => :build
-  depends_on 'xorg_proto' # R
 
   run_tests
 end
