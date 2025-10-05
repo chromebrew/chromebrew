b7aedf88a8dcfa494229ec63e6c3ac429cf16675
Satadru Pramanik <satadru@gmail.com>
2025-10-05 13:46:05 -0400

    modernize package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libxt.rb
+++ packages/libxt.rb
@@ -3,11 +3,11 @@ require 'package'
 class Libxt < Package
   description 'X.org X Toolkit Library'
   homepage 'https://www.x.org/wiki/'
-  version '1.2.1'
-  license 'custom'
+  version '1.3.1'
+  license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://www.x.org/archive/individual/lib/libXt-1.2.1.tar.gz'
-  source_sha256 '6da1bfa9dd0ed87430a5ce95b129485086394df308998ebe34d98e378e3dfb33'
+  source_url 'https://gitlab.freedesktop.org/xorg/lib/libxt.git'
+  git_hashtag "libXt-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -16,16 +16,6 @@ class Libxt < Package
      x86_64: 'c59be078d6b5120a57ee6a0ee21caea62e9f6a227855e014a3b661c3400e50f0'
   })
 
-  depends_on 'libsm'
-  depends_on 'libx11'
-  patchelf
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'libsm' => :build
+  depends_on 'libx11' => :build
 end
