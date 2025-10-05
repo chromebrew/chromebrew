399c943b48f0cd3bbf9eeea198759ea3ee1c7016
Satadru Pramanik <satadru@gmail.com>
2025-10-05 10:12:56 -0400

    modernize package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/xkeyboard_config.rb
+++ packages/xkeyboard_config.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Xkeyboard_config < Package
+class Xkeyboard_config < Meson
   description 'The non-arch keyboard configuration database for X Window.'
   homepage 'https://www.freedesktop.org/wiki/Software/XKeyboardConfig/'
   compatibility 'aarch64 armv7l x86_64'
-  version '2.29'
+  version '2.46'
   license 'MIT'
-  source_url 'https://www.x.org/releases/individual/data/xkeyboard-config/xkeyboard-config-2.29.tar.bz2'
-  source_sha256 '1d4175278bf06000683656763a8b1d3282c61a314b6db41260c8efe92d621802'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git'
+  git_hashtag "xkeyboard-config-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'eae7e1d93e5ad45040b4f98f641d9710c5815bc753fabb0e51d781eeb45785bc',
@@ -16,18 +16,5 @@ class Xkeyboard_config < Package
      x86_64: 'a3d3467cbc60baa18a8f41585103ad440cff1461c835b895c82e5f51f4f34382'
   })
 
-  depends_on 'libx11'
-
-  def self.patch
-    system "sed -i 's,/usr/bin/python3,#{CREW_PREFIX}/bin/python3,' ./rules/compat/map-variants.py"
-  end
-
-  def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'libx11' => :build
 end
