f11dcdd824dd8930e136340311f910002d1bd7cd
Satadru Pramanik <satadru@gmail.com>
2025-10-05 11:36:02 -0400

    modernize package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/xkbcomp.rb
+++ packages/xkbcomp.rb
@@ -1,14 +1,15 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Xkbcomp < Package
+class Xkbcomp < Meson
   description 'Compile XKB keyboard'
   homepage 'https://xorg.freedesktop.org/wiki/'
-  version '1.4.4'
+  version '1.4.7-d03a4ab'
   license 'ISC, MIT and custom'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://www.x.org/archive/individual/app/xkbcomp-1.4.4.tar.bz2'
-  source_sha256 '59cce603a607a17722a0a1cf99010f4894e7812beb5d695abbc08474d59af27e'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.freedesktop.org/xorg/app/xkbcomp.git'
+  git_hashtag 'd03a4ab1c0b24f6581411622ccf729ceb329aeb8'
+  # git_hashtag "xkbcomp-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'c654874e78a8da297b0bc5addc3a461e840366bdc333a010db05315f79497329',
@@ -16,17 +17,8 @@ class Xkbcomp < Package
      x86_64: '5dbe7bc9bae31c26a4d0cb5a87e1655de43d05ed97307778df49a0b3fbbabaaa'
   })
 
-  depends_on 'mesa'
-  depends_on 'xcb_util'
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'mesa' => :build
+  depends_on 'xcb_util' => :build
 
   def self.postinstall
     puts
