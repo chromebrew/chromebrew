8f837c6aac35235d487a5464cb3c928208f80323
Satadru Pramanik <satadru@gmail.com>
2025-10-05 14:05:52 -0400

    modernize package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/hicolor_icon_theme.rb
+++ packages/hicolor_icon_theme.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Hicolor_icon_theme < Package
+class Hicolor_icon_theme < Meson
   description 'Icon-theme contains the standard also references the default icon theme called hicolor.'
   homepage 'https://www.freedesktop.org/wiki/Software/icon-theme/'
-  version '0.17'
+  version '0.18'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://icon-theme.freedesktop.org/releases/hicolor-icon-theme-0.17.tar.xz'
-  source_sha256 '317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.freedesktop.org/xdg/default-icon-theme'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '3341e27bb5e2ea8c55b573c32bf34ba4e88da50c6cc1d865eb1c2d91232d44e6',
@@ -16,13 +16,4 @@ class Hicolor_icon_theme < Package
        i686: '574db604ef7db3b401e56ce4387e2a488952fcfa308316cc7127f872ee87557c',
      x86_64: 'f0640114582b89e78c5e884e801d512b3a0f484443fdc0157a6f4db289261bae'
   })
-
-  def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
