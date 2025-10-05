fc1045bae60d3105cf196b4eab0ee63918e650d7
Satadru Pramanik <satadru@gmail.com>
2025-10-05 14:11:16 -0400

    Modernize package
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gdk_pixbuf.rb
+++ packages/gdk_pixbuf.rb
@@ -1,14 +1,13 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Gdk_pixbuf < Package
+class Gdk_pixbuf < Meson
   description 'GdkPixbuf is a library for image loading and manipulation.'
   homepage 'https://developer.gnome.org/gdk-pixbuf'
-  @_ver = '2.42.10'
-  version "#{@_ver}-1"
+  version '2.44.3'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gdk-pixbuf.git'
-  git_hashtag @_ver
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -38,31 +37,24 @@ class Gdk_pixbuf < Package
 
   gnome
 
-  def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} \
-      -Dinstalled_tests=false \
+ meson_options '-Dinstalled_tests=false \
       -Dbuiltin_loaders=all \
       -Drelocatable=true \
       -Ddebug=false \
       -Dgtk_doc=false \
       -Dman=true \
-      -Dtests=false \
-      builddir"
-    system 'samu -C builddir'
-  end
+      -Dtests=false'
 
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
+  meson_install_extras do
     loader_dir = "#{CREW_DEST_LIB_PREFIX}/gdk-pixbuf-2.0/2.10.0/loaders"
     FileUtils.mkdir_p loader_dir
     system "touch #{loader_dir}.cache"
 
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/env.d/"
-    @gdk_pixbuf_env = <<~GDK_PIXBUF_ENV_EOF
+    File.write 'gdk_pixbuf_env.d', <<~GDK_PIXBUF_ENV_EOF
       # gdk-pixbuf configuration
       export GDK_PIXBUF_MODULEDIR=#{CREW_LIB_PREFIX}/gdk-pixbuf-2.0/2.10.0/loaders
       export GDK_PIXBUF_MODULE_FILE=#{CREW_LIB_PREFIX}/gdk-pixbuf-2.0/2.10.0/loaders.cache
     GDK_PIXBUF_ENV_EOF
-    File.write("#{CREW_DEST_PREFIX}/etc/env.d/gdk_pixbuf", @gdk_pixbuf_env)
+    FileUtils.install 'gdk_pixbuf_env.d', "#{CREW_DEST_PREFIX}/etc/env.d/gdk_pixbuf", mode: 0o644
   end
 end
