114a92474cd5561eb5093f5e9b7450349d5a894f
Satadru Pramanik <satadru@gmail.com>
2025-10-07 13:23:56 -0400

    Adjust gem options.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_openssl.rb
+++ packages/ruby_openssl.rb
@@ -7,7 +7,15 @@ class Ruby_openssl < RUBY
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
+  binary_compression 'gem'
+
+  binary_sha256({
+    aarch64: 'abf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
+     armv7l: 'abf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
+       i686: 'a34602ef5635703fef1155cf1e1ef3f3680b4bef0555c53f399ce610e541794a',
+     x86_64: 'a899294875e4bfe3e9f4bcd11b718525fca60bf03435acca63ed79279a5e6230'
+  })
 
   conflicts_ok
-  no_compile_needed
+  gem_compile_needed
 end
