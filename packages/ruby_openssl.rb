26a593033adbca1971df6eb8cbbf88f2ff5a4928
Satadru Pramanik <satadru@gmail.com>
2025-10-07 13:51:47 -0400

    Add arm binaries
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_openssl.rb
+++ packages/ruby_openssl.rb
@@ -10,8 +10,8 @@ class Ruby_openssl < RUBY
   binary_compression 'gem'
 
   binary_sha256({
-    aarch64: 'abf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
-     armv7l: 'abf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
+    aarch64: 'b74e4a2051672b3238a892b90d28b97cf543d79509e7adab07dc620819eb2a79',
+     armv7l: 'b74e4a2051672b3238a892b90d28b97cf543d79509e7adab07dc620819eb2a79',
        i686: '628704dbc75ae704c28d2cb0c19edeb06179b036f8d0cfa1a8dde580b7a461ec',
      x86_64: 'b25dc0cee0c2f28afdbe95a9976bfc63e31edbfe5a25d3be90de781bb0cddbb7'
   })
