381e578226f5d6c2f0440201ce4758ce9d3177ce
satmandu <satmandu@users.noreply.github.com>
2025-10-07 17:36:40 +0000

    updater-ruby_openssl-3.3.1: Package File Update Run on linux/386 container.

--- packages/ruby_openssl.rb
+++ packages/ruby_openssl.rb
@@ -12,8 +12,8 @@ class Ruby_openssl < RUBY
   binary_sha256({
     aarch64: 'abf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
      armv7l: 'abf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
-       i686: 'a34602ef5635703fef1155cf1e1ef3f3680b4bef0555c53f399ce610e541794a',
-     x86_64: 'a899294875e4bfe3e9f4bcd11b718525fca60bf03435acca63ed79279a5e6230'
+       i686: '628704dbc75ae704c28d2cb0c19edeb06179b036f8d0cfa1a8dde580b7a461ec',
+     x86_64: 'b25dc0cee0c2f28afdbe95a9976bfc63e31edbfe5a25d3be90de781bb0cddbb7'
   })
 
   conflicts_ok
