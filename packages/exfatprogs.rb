0025dab7538eac5940dfb0605692149b02d5b65d
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2025-10-15 17:23:20 -0400

    updater-exfatprogs_1.3.0 — exfatprogs → 1.3.0 (#13140)
    * Exfatprogs => 1.3.0, modernize package.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * updater-exfatprogs_1.3.0: Package File Update Run on linux/386 container.
    
    * updater-exfatprogs_1.3.0: Package File Update Run on linux/amd64 container.
    
    * updater-exfatprogs_1.3.0: Package File Update Run on linux/arm/v7 container.
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: chromebrew-actions[bot] <chromebrew-actions[bot]@users.noreply.github.com>

--- packages/exfatprogs.rb
+++ packages/exfatprogs.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Exfatprogs < Package
+class Exfatprogs < Autotools
   description 'exFAT filesystem userspace utilities for the Linux Kernel exfat driver.'
   homepage 'https://github.com/exfatprogs/exfatprogs'
-  version '1.2.9'
+  version '1.3.0'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/exfatprogs/exfatprogs.git'
@@ -11,21 +11,11 @@ class Exfatprogs < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f707e6f1519aa495d08f059132215e44709f3d8fd8527bae556dc618c00cf477',
-     armv7l: 'f707e6f1519aa495d08f059132215e44709f3d8fd8527bae556dc618c00cf477',
-       i686: 'f7b3a9426b7a9f5d2bf8559c9b0e4602d5c5b0696882b38a3d09451ed5f04de9',
-     x86_64: '959e41a09ec92ae430c5c0c5485eb5f56cd25d1eab51c867d7d171ee7575d3b9'
+    aarch64: '353b01f8d423aa4580e5ec6f5ea56664c91a3ede89125686866b256f4e0d3013',
+     armv7l: '353b01f8d423aa4580e5ec6f5ea56664c91a3ede89125686866b256f4e0d3013',
+       i686: 'b8386e36d404726675ca078f71cb308a3be271d391c438086c09704c5efe4c82',
+     x86_64: 'b5b0df3a389481dbec05bad847970979f45ee9275d6bbb3ba864276c3a5084f4'
   })
 
   depends_on 'glibc' # R
-
-  def self.build
-    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
