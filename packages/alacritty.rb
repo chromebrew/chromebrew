dd0f9d8f942b1c99e83232f73b84740d57883b5c
Ed Reel <edreel@gmail.com>
2025-10-19 18:26:39 -0500

    Alacritty 0.15.1 => 0.16.0 (#13174)

--- packages/alacritty.rb
+++ packages/alacritty.rb
@@ -6,7 +6,7 @@ require 'package'
 class Alacritty < Package
   description 'A cross-platform, GPU-accelerated terminal emulator'
   homepage 'https://github.com/alacritty/alacritty'
-  version '0.15.1'
+  version '0.16.0'
   license 'Apache'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/alacritty/alacritty.git'
@@ -14,21 +14,22 @@ class Alacritty < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '110e93dfcb455ada8777a38c1874f670b03a02e32d756faa3d8bd877e8131e93',
-     armv7l: '110e93dfcb455ada8777a38c1874f670b03a02e32d756faa3d8bd877e8131e93',
-     x86_64: 'd6db5d3bebc9807b2b8dc476ca83668c92f3e4d6e6d0f8734d2c5e55e84dddc2'
+    aarch64: 'e5a1d61d4ee4ceccc36fe873f4742664f1fa6a0c240a9f35e66b3817c54f5a08',
+     armv7l: 'e5a1d61d4ee4ceccc36fe873f4742664f1fa6a0c240a9f35e66b3817c54f5a08',
+     x86_64: '91a290725efad5d27126a338e445e31cd44d399889e8a130d0d01197d655b392'
   })
 
-  depends_on 'fontconfig'
-  depends_on 'ncurses'
-  depends_on 'libxcb'
-  depends_on 'rust' => :build
+  depends_on 'fontconfig' # R
   depends_on 'freetype' # R
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'harfbuzz' # R
+  depends_on 'libxcb' # R
   depends_on 'libxcursor' # R
   depends_on 'libxi' # R
+  depends_on 'libxkbcommon' # R
+  depends_on 'ncurses' # R
+  depends_on 'rust' => :build
 
   def self.build
     system 'CARGO_INCREMENTAL=0 cargo build --release'
