d72b6dbef29ee89efe2c373666e9d63faf55061f
Ed Reel <edreel@gmail.com>
2025-10-21 16:32:10 -0500

    Google_chrome 141.0.7390.107-1 => 141.0.7390.122-1

--- packages/google_chrome.rb
+++ packages/google_chrome.rb
@@ -3,12 +3,12 @@ require 'package'
 class Google_chrome < Package
   description 'Google Chrome is a fast, easy to use, and secure web browser.'
   homepage 'https://www.google.com/chrome/'
-  version '141.0.7390.107-1'
+  version '141.0.7390.122-1'
   license 'google-chrome'
   compatibility 'x86_64'
 
   source_url "https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_#{version}_amd64.deb"
-  source_sha256 '74d738a546aa82180cc626cea479f7a761fff39072aa93c3458a545975fb6735'
+  source_sha256 'b2fcd4c498b0e65747c25e35dd057ef44822c5eb3c0fbb449aa154fa4f789a50'
 
   no_compile_needed
   no_shrink
