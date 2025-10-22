20b71552c7d6670792daca0e0d58f3d25919261e
Ed Reel <edreel@gmail.com>
2025-10-22 05:05:02 +0000

    Ansible 2.17.3 => 12.1.0-py3.13

--- packages/ansible.rb
+++ packages/ansible.rb
@@ -1,20 +1,19 @@
-require 'buildsystems/python'
+require 'buildsystems/pip'
 
-class Ansible < Python
+class Ansible < Pip
   description 'Ansible is a radically simple IT automation engine that automates cloud provisioning, configuration management, application deployment, intra-service orchestration, and many other IT needs.'
   homepage 'https://www.ansible.com/'
-  version '2.17.3'
+  version "12.1.0-#{CREW_PY_VER}"
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://github.com/ansible/ansible.git'
-  git_hashtag "v#{version}"
+  source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9062621a2fa226e56059a777f2a7ea35f632c7a0866c5b1bcfa5b933e09350f0',
-     armv7l: '9062621a2fa226e56059a777f2a7ea35f632c7a0866c5b1bcfa5b933e09350f0',
-       i686: 'b38555e1996ca01a2180a88dccf22c85a46ab94b6248fe3fa2078667d293eb4c',
-     x86_64: '6741f337d27cd106fdc721f3bccb536c5f6f42cd3cd3ff650f32a7729970c353'
+    aarch64: '94d59822b4f69d9910facdc765f1f40bf822f93dfd673af632544be04dba4bd5',
+     armv7l: '94d59822b4f69d9910facdc765f1f40bf822f93dfd673af632544be04dba4bd5',
+       i686: 'eef9ea112f40c04a52ab07a9a55a8c505e03b0ab49efc284869ff7a7d7083fa1',
+     x86_64: '06a8fa801ccb30951cb5c6b4a1d78a28e909410746a36fdef79bf1051711a49e'
   })
 
   depends_on 'xdg_base'
@@ -23,4 +22,6 @@ class Ansible < Python
   depends_on 'py3_packaging'
   depends_on 'py3_jinja2'
   depends_on 'python3' => :build
+
+  no_source_build
 end
