1fd6738b541c2786fb59b2723e08fe040f29d2ce
Ed Reel <edreel@gmail.com>
2025-10-26 19:54:23 +0000

    Add binaries and filelists

--- packages/trzsz.rb
+++ packages/trzsz.rb
@@ -6,8 +6,16 @@ class Trzsz < Package
   version '1.1.8'
   license 'MIT'
   compatibility 'all'
-  source_url "https://github.com/trzsz/trzsz-go/archive/refs/tags/v#{version}.tar.gz"
-  source_sha256 '9ff477c98081ffccecdd61b1bb51d573a0b67c7d205ecfc9d50b20dd4b54f3f1'
+  source_url 'https://github.com/trzsz/trzsz-go.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: 'ee0c1d36bd136bee8fc4acff260191acd5d8bce505e3e592f050d9a718d530d5',
+     armv7l: 'ee0c1d36bd136bee8fc4acff260191acd5d8bce505e3e592f050d9a718d530d5',
+       i686: '8ad1cbd074aa7ff0dfe9447959f3320ba78c7f455334e9685f85e3847fd1097f',
+     x86_64: '35f74a16ce3ad9532f7cdd8a5ae27e7f92ce6c46b252c7b59c7eab6d83148e13'
+  })
 
   depends_on 'glibc' # R
   depends_on 'go' => :build
