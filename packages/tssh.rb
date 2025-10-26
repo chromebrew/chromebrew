eea7877639b0e8d9567e4456e50d62ba3d2b0cff
Ed Reel <edreel@gmail.com>
2025-10-26 20:05:54 +0000

    Add binaries and filelists

--- packages/tssh.rb
+++ packages/tssh.rb
@@ -6,8 +6,16 @@ class Tssh < Package
   version '0.1.23'
   license 'MIT'
   compatibility 'all'
-  source_url "https://github.com/trzsz/trzsz-ssh/archive/refs/tags/v#{version}.tar.gz"
-  source_sha256 'e4fa0f6f443faede83380ceca4a0de862c054f37c27983a7bbe6bced1d9f80da'
+  source_url 'https://github.com/trzsz/trzsz-ssh.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: 'f197490536feab3eda8219bcd31ee26c961c091ba26ee34a091f8e23eac16cbb',
+     armv7l: 'f197490536feab3eda8219bcd31ee26c961c091ba26ee34a091f8e23eac16cbb',
+       i686: '0613c398ef5a1790942c6508a45bc8bb31ac0f3c488e14201521fee22d3ec915',
+     x86_64: '1aa6ef20c85dc24b10360380f2eef5acc0b3ac8514ecb9f6de9c8a486604e288'
+  })
 
   depends_on 'glibc' # R
   depends_on 'go' => :build
