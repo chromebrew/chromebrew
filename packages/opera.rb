ba28934c49b505f9591d9fe95ad8421f2f42e1cb
Ed Reel <edreel@gmail.com>
2025-10-29 21:03:01 -0500

    Opera 122.0.5643.142 => 123.0.5669.23

--- packages/opera.rb
+++ packages/opera.rb
@@ -3,12 +3,12 @@ require 'package'
 class Opera < Package
   description 'Opera is a multi-platform web browser based on Chromium and developed by Opera Software.'
   homepage 'https://www.opera.com/'
-  version '122.0.5643.142'
+  version '123.0.5669.23'
   license 'OPERA-2018'
   compatibility 'x86_64'
 
   source_url "https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/opera-stable_#{version}_amd64.deb"
-  source_sha256 '1a227356e2ed567efdaab55c7f91f7672bf5fa09b15f487ae342e29d2d5f55de'
+  source_sha256 '8f6907760f1deb44bd8f76cbc9c863987fdc4405c71888ce8062aa98d2219c75'
 
   no_compile_needed
   no_shrink
