313898cfb995411db9d795f851257ef7e2b2b2b6
Satadru Pramanik <satadru@gmail.com>
2025-10-30 19:41:07 -0400

    Switch ruby date gem to gem_compile_needed.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_date.rb
+++ packages/ruby_date.rb
@@ -7,7 +7,16 @@ class Ruby_date < RUBY
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
+  binary_compression 'gem'
+
+  binary_sha256({
+    aarch64: '28bfef4e763889bbcac15f926a0169692ec2d971ee80b6327b6d434b8ea1b6c0',
+     armv7l: '28bfef4e763889bbcac15f926a0169692ec2d971ee80b6327b6d434b8ea1b6c0',
+       i686: '73d25f28cba1059253346e9524feed3b25c7fbca0e620791603b5ee1db240b43',
+     x86_64: 'a6afba634c7672c9fc0791c10ad84772dc93f851a878c4bd85e62743f378a33f'
+  })
 
   conflicts_ok
-  no_compile_needed
+  gem_compile_needed
+  no_source_build
 end
