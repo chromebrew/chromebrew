6abfb99fd5f53a52cca652d789df82c53a96ba0f
Satadru Pramanik <satadru@gmail.com>
2025-10-31 12:58:35 -0400

    Adjust rubocop reinstalls during builds.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- tools/github_actions_update_builder_allowing_failures.sh
+++ tools/github_actions_update_builder_allowing_failures.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Mapping of /output should be set in the docker run options for the
 # run-updater step of the generate job in .github/workflows/Updater.yml
+# Version 1.0
 cd /output/ || exit 1
 crew update && yes | crew upgrade || true
 unset LD_AUDIT
@@ -10,5 +11,5 @@ git config --global --add safe.directory /output
 export CREW_AGREE_TIMEOUT_SECONDS=1
 # Deal with a regexp_parser gem issue
 gem install regexp_parser
-yes | crew reinstall ruby_rubocop
+yes | crew install ruby_rubocop
 tools/build_updated_packages.rb --skip --continue-after-failed-builds || exit 1
