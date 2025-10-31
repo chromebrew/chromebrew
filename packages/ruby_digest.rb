344f18977d7a2852324d983ec12dc98672b23266
Satadru Pramanik <satadru@gmail.com>
2025-10-31 08:11:06 -0400

    Add gem_compile_needed to ruby_digest.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ruby_digest.rb
+++ packages/ruby_digest.rb
@@ -7,7 +7,15 @@ class Ruby_digest < RUBY
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
+  binary_compression 'gem'
+
+  binary_sha256({
+    aarch64: '2bf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
+     armv7l: '2bf0e552fbd0d9b9912a54f45df642de80536807a5e178ca3a6ca0345e82d64f',
+       i686: '934602ef5635703fef1155cf1e1ef3f3680b4bef0555c53f399ce610e541794a',
+     x86_64: '7899294875e4bfe3e9f4bcd11b718525fca60bf03435acca63ed79279a5e6230'
+  })
 
   conflicts_ok
-  no_compile_needed
+  gem_compile_needed
 end
