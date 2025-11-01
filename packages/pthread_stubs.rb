118f935e7e58d8c6bac531db871eee878dbc4eab
Satadru Pramanik <satadru@gmail.com>
2025-11-01 10:10:40 -0400

    Update mappings and versions for xwayland deps.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/pthread_stubs.rb
+++ packages/pthread_stubs.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Pthread_stubs < Package
+class Pthread_stubs < Autotools
   description 'pthread stubs to get libX11 working'
   homepage 'https://x.org/wiki/'
-  version '0.4'
+  version '0.5'
   license 'MIT-with-advertising'
   compatibility 'all'
-  source_url 'https://xcb.freedesktop.org/dist/libpthread-stubs-0.4.tar.bz2'
-  source_sha256 'e4d05911a3165d3b18321cc067fdd2f023f06436e391c6a28dff618a78d2e733'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.freedesktop.org/xorg/lib/pthread-stubs.git'
+  git_hashtag "libpthread-stubs-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '85fd777e81a2dd5e3954e82465291c2c889567b7ae8bbb7a3e51b196c8ed35dd',
@@ -16,13 +16,4 @@ class Pthread_stubs < Package
        i686: '3fa0eb953101a37c924a22fa28699d1cb7e4a85bd79b7c492f087df91e108e42',
      x86_64: '74d475f18422369cea3e5ca5591c55aa2caaac2ad23abaf17e5678daec534cf9'
   })
-
-  def self.build
-    system "./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
--- packages/rendercheck.rb
+++ packages/rendercheck.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Rendercheck < Package
+class Rendercheck < Meson
   description 'rendercheck is a program to test a Render extension implementation against separate calculations of expected output.'
   homepage 'https://www.x.org/wiki/'
-  version '1.5'
+  version '1.6'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://x.org/archive/individual/app/rendercheck-1.5.tar.bz2'
-  source_sha256 '00605679436d65ccf9a6f1f1cb206df7a2e8b28a7821e867922d2b14b009f1cc'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.freedesktop.org/xorg/test/rendercheck.git'
+  git_hashtag "rendercheck-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '90b0f792889946717870f1497e523c3229cb5840feaf1152da6a125710310697',
@@ -16,13 +16,4 @@ class Rendercheck < Package
        i686: 'b0ca933742e0d17470c8f1d3e66dac65c5a03ac4d490a477e6e59c700b08b1b7',
      x86_64: '7c29b512ecdf2472ade1bce7f7b7491cd8f828a1146061fa8e2e512de6dd1dc9'
   })
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
 end
