69f4d4ec561a234127ef26f3eae90e4984cee3b0
Ed Reel <edreel@gmail.com>
2025-11-02 01:16:35 -0600

    Remind 03.01.15 => 06.01.08 (#13308)

--- packages/remind.rb
+++ packages/remind.rb
@@ -1,28 +1,28 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Remind < Package
+class Remind < Autotools
   description 'Remind is a sophisticated calendar and alarm program.'
-  homepage 'https://www.roaringpenguin.com/products/remind'
-  version '03.01.15'
+  homepage 'https://dianne.skoll.ca/projects/remind/'
+  version '06.01.08'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://www.roaringpenguin.com/files/download/remind-03.01.15.tar.gz'
-  source_sha256 '8adab4c0b30a556c34223094c5c74779164d5f3b8be66b8039f44b577e678ec1'
-  binary_compression 'tar.xz'
+  source_url "https://dianne.skoll.ca/projects/remind/download/remind-#{version}.tar.gz"
+  source_sha256 '685d96e96721df58f1a23bf6075b8d5313a796b9c5910a056fe749b6435ce607'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '65bae63d254189573223d8771b257e8a0186ee6bfcfb8c97a7df74995e4fe042',
-     armv7l: '65bae63d254189573223d8771b257e8a0186ee6bfcfb8c97a7df74995e4fe042',
-       i686: '833cf2d86df2ed62af3056d7633a7c133bd289a459d10ad39e7160575dc2ad25',
-     x86_64: '93144719846bf23d63c0b31c688ce5815b74e6600fe034d9c783cdd6887c7b0f'
+    aarch64: 'c7775bea89c10b0becf2029693a588edb2f7b38ceec0aac4ba7713a4c9e4c1d6',
+     armv7l: 'c7775bea89c10b0becf2029693a588edb2f7b38ceec0aac4ba7713a4c9e4c1d6',
+       i686: 'a030d6c3a2e66b0188dc481bcf6eccba3e3ce664e37cee5dae4c600040bc16a2',
+     x86_64: '1c3dab44607e67bcdf9414655fa15f3d9a47d4d15804dea92d678b970a81e27f'
   })
 
-  def self.build
-    system "./configure --prefix=#{CREW_PREFIX}"
-    system 'make'
-  end
+  depends_on 'glibc' # R
+  depends_on 'readline' # R
+  depends_on 'tk' unless ARCH.eql?('i686') # Needed for tkremind.
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  autotools_install_extras do
+    FileUtils.mkdir_p CREW_DEST_HOME
+    FileUtils.touch "#{CREW_DEST_HOME}/.reminders"
   end
 end
