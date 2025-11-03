ab7d55d4c5143489290e53ee6519e3edc7c9e0de
Ed Reel <edreel@gmail.com>
2025-11-03 07:16:35 +0000

    Blender 4.1.1 => 4.5.4

--- packages/blender.rb
+++ packages/blender.rb
@@ -1,18 +1,14 @@
-require 'buildsystems/cmake'
+require 'package'
 
-class Blender < CMake
+class Blender < Package
   description 'Blender is the free and open source 3D creation suite.'
   homepage 'https://www.blender.org'
-  version '4.1.1'
+  version '4.5.4'
   license 'GPLv3+'
   compatibility 'x86_64'
-  source_url 'https://download.blender.org/source/blender-4.1.1.tar.xz'
-  source_sha256 '4fbb3af64d3f84df5c7103748454226c1885c1ac2ed5373d0cea1e80e82c0848'
-  binary_compression 'tar.zst'
-
-  binary_sha256({
-     x86_64: '1785f34e092a4c359f003b6bf70ae1a39c8b06eb9f4a89aa9b729e1893bfc4e4'
-  })
+  min_glibc '2.29'
+  source_url "https://download.blender.org/release/Blender#{version.sub(/\.\d+$/, '')}/blender-#{version}-linux-x64.tar.xz"
+  source_sha256 '2e6ef8e99fc36327270429ddc8e7bad2859dd878a5a137d2e0bf0f02f6792505'
 
   # depends_on 'audaspace'
   depends_on 'boost' # R
@@ -49,18 +45,14 @@ class Blender < CMake
   depends_on 'zlib'
   depends_on 'zstd'
 
-  def self.patch
-    # These fixes haven't made their way to a release yet, so we just patch them in.
-    # [PATCH] Use set_and_warn_dependency to disable WITH_MOD_OCEANSIM if WITH_FFTW3 is disabled.
-    downloader 'https://projects.blender.org/blender/blender/commit/0a74065a1995ef5423b3e073f53328d664b43b05.patch', '8a2f9739c716822a94c94894cf32b4ed8d87809b38a8c53d6288f5f2ce8490f1'
-    system 'patch -Np1 -i 0a74065a1995ef5423b3e073f53328d664b43b05.patch'
-    # [PATCH] Unbreak lite-build WITH_GMP enabled
-    downloader 'https://projects.blender.org/blender/blender/commit/861536b24c866d09857ea4b92717f822da4bc75e.patch', 'c7b6560da9062aa984aba3b67212d4694cbd3ec4a72dc6cae3b3d01a43ed0b5a'
-    system 'patch -Np1 -i 861536b24c866d09857ea4b92717f822da4bc75e.patch'
-  end
+  no_compile_needed
 
-  # Blender needs the single-precision build of fftw, which we do not provide.
-  # WITH_SYSTEM_AUDASPACE is broken in 4.1.1, should be resolved in 4.2
-  # WITH_INSTALL_PORTABLE needs to be disabled to get a a FHS-compatible install.
-  cmake_options '-DWITH_FFTW3=OFF -DWITH_SYSTEM_EIGEN3=ON -DPYTHON_VERSION=3.12 -DWITH_PYTHON_INSTALL=OFF -DWITH_INSTALL_PORTABLE=OFF'
+  def self.install
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/blender"
+    FileUtils.install 'blender.desktop', "#{CREW_DEST_PREFIX}/share/applications/blender.desktop", mode: 0o644
+    FileUtils.install 'blender.svg', "#{CREW_DEST_PREFIX}/share/icons/blender.svg", mode: 0o644
+    FileUtils.ln_s "#{CREW_PREFIX}/share/blender/blender-launcher", "#{CREW_DEST_PREFIX}/bin/blender"
+    FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/blender"
+  end
 end
