1d33460b6ed157f46d12ddacb861a085c6a9ab6a
Ed Reel <edreel@gmail.com>
2025-11-04 03:22:41 -0600

    Crystal 1.17.1 => 1.18.2 (#13334)

--- packages/crystal.rb
+++ packages/crystal.rb
@@ -3,11 +3,11 @@ require 'package'
 class Crystal < Package
   description 'A language for humans and computers'
   homepage 'https://crystal-lang.org/'
-  version '1.17.1'
+  version '1.18.2'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/crystal-lang/crystal/releases/download/#{version}/crystal-#{version}-1-linux-x86_64-bundled.tar.gz"
-  source_sha256 'cb24d17927a8dc712aaa2ed1c78cedf4ebf96afc5a1de72b569896e8d8cbc991'
+  source_sha256 '179c48162365e1f84426a3d352fe7a97e7fc798fd73b4232d1f491b11a0ef2dc'
 
   no_compile_needed
   print_source_bashrc
