0f8bceec6b4aa77020fff48f4fdc49052dd03026
Ed Reel <edreel@gmail.com>
2025-11-03 20:19:31 -0600

    Android_studio 2025.1.2.11 => 2025.2.1.7 (#13323)

--- packages/android_studio.rb
+++ packages/android_studio.rb
@@ -1,15 +1,14 @@
 require 'package'
-require 'misc_functions'
 
 class Android_studio < Package
   description 'Android Studio is the official IDE for Android development.'
   homepage 'https://developer.android.com/studio'
-  version '2025.1.2.11'
+  version '2025.2.1.7'
   license 'Apache-2.0'
   compatibility 'x86_64'
-  min_glibc '2.28'
+  min_glibc '2.29'
   source_url "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/#{version}/android-studio-#{version}-linux.tar.gz"
-  source_sha256 '8f3874c6b119538cdd96c7bcb65555feea81133e251f69365d21ddd77777be78'
+  source_sha256 '16af1fa02d746e4c182d8852ff3859bba5c182768efeacfa18a8b81d65e78cb1'
 
   depends_on 'openjdk17'
   depends_on 'xdg_base'
@@ -35,6 +34,7 @@ class Android_studio < Package
     FileUtils.mv 'bin', "#{CREW_DEST_PREFIX}/share/android-studio"
     FileUtils.mv 'lib', "#{CREW_DEST_PREFIX}/share/android-studio"
     FileUtils.mv 'plugins', "#{CREW_DEST_PREFIX}/share/android-studio"
+    FileUtils.touch "#{CREW_DEST_PREFIX}/share/android-studio/product-info.json"
     FileUtils.ln_s "#{CREW_PREFIX}/share/android-studio/bin/studio.sh", "#{CREW_DEST_PREFIX}/bin/studio"
   end
 
@@ -44,6 +44,8 @@ class Android_studio < Package
   end
 
   def self.postremove
+    # Needed until bug in #13322 is fixed.
+    FileUtils.rm_rf "#{CREW_PREFIX}/share/android-studio"
     Package.agree_to_remove("#{CREW_PREFIX}/.cache/Google/AndroidStudio#{@short_ver}")
     Package.agree_to_remove("#{HOME}/.android")
     Package.agree_to_remove("#{HOME}/Android")
