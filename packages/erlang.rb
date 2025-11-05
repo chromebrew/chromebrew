69fdefc8be691d312cbdb883330f8a566671d4c5
Ed Reel <edreel@gmail.com>
2025-11-05 01:44:13 +0000

    Erlang 27.3.2 => 28.1.1

--- packages/erlang.rb
+++ packages/erlang.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Erlang < Autotools
   description 'Erlang is a programming language used to build massively scalable soft real-time systems with requirements on high availability.'
   homepage 'https://www.erlang.org/'
-  version '27.3.2'
+  version '28.1.1'
   license 'Apache-2.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/erlang/otp.git'
@@ -11,18 +11,15 @@ class Erlang < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '512a920a92e5011e80196cfad84571398142398030cfd8ffd59229803b68f0bf',
-     armv7l: '512a920a92e5011e80196cfad84571398142398030cfd8ffd59229803b68f0bf',
-     x86_64: '6d2449b58f505a341b545c88b92eac5805a8cfecfdf99b77e34d0df10580bb1d'
+    aarch64: '0f2a93355ad7f7a9fdebd6e2cb7c099d89e1f8abd2dab05ddf470258d6fea914',
+     armv7l: '0f2a93355ad7f7a9fdebd6e2cb7c099d89e1f8abd2dab05ddf470258d6fea914',
+     x86_64: 'ed314f0181934c9e29c70b15f97bc9d7ede13bf29ca954afe2aaff581af9103d'
   })
 
   depends_on 'gcc_lib' # R
   depends_on 'glib' # R
   depends_on 'glibc' # R
   depends_on 'libglu' # R
-  depends_on 'libglvnd' # R
-  depends_on 'ncurses' # R
-  depends_on 'openjdk8' # R
   depends_on 'openssl' # R
   depends_on 'unixodbc' # R
   depends_on 'wxwidgets' # R
