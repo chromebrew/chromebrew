c1b744e41cc17e81b87bb5fee23ecd7b1040e146
Ed Reel <edreel@gmail.com>
2025-11-09 08:30:11 -0600

    Libkmod 29 => 34.2 (#13386)

--- packages/libkmod.rb
+++ packages/libkmod.rb
@@ -1,46 +1,26 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Libkmod < Package
+class Libkmod < Meson
   description 'Linux kernel module handling library'
   homepage 'https://kernel.org'
-  version '29'
+  version '34.2'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://git.kernel.org/pub/scm/utils/kernel/kmod/kmod.git'
+  source_url 'https://github.com/kmod-project/kmod.git'
   git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '40fe9af2580dbc280c5c8b44f9dccbd4869cdb439eeee510bc26468f2652fa32',
-     armv7l: '40fe9af2580dbc280c5c8b44f9dccbd4869cdb439eeee510bc26468f2652fa32',
-       i686: 'e3cbd134f96e7e25cba0eb1e721ecaa2372c218ff1deb5d5da9303691f6fc42b',
-     x86_64: 'a69c727d00f82f0e50dc12f2030e613a50f21095d070c80d59437f9eab02e372'
+    aarch64: '01389eaaeb72f0ab2dc125448660e938ab453eba4e426ad6cb181b2c8b099576',
+     armv7l: '01389eaaeb72f0ab2dc125448660e938ab453eba4e426ad6cb181b2c8b099576',
+       i686: 'd9cc3171ee84edfd3ede4af986561496c725d1a4263c15730068f361ee5bc4df',
+     x86_64: '9db893c96661faf9faf2a2c00ace249e731de01e17e0272028cd9184321ba848'
   })
 
-  depends_on 'py3_cython'
-  depends_on 'xzutils'
-
-  def self.build
-    system './autogen.sh'
-    system 'filefix'
-    system "./configure #{CREW_CONFIGURE_OPTIONS} \
-    --disable-maintainer-mode \
-    --enable-python \
-    --with-openssl \
-    --with-xz \
-    --with-zlib \
-    --with-zstd"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/sbin"
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/kmod", "#{CREW_DEST_PREFIX}/bin/lsmod"
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/kmod", "#{CREW_DEST_PREFIX}/sbin/depmod"
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/kmod", "#{CREW_DEST_PREFIX}/sbin/insmod"
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/kmod", "#{CREW_DEST_PREFIX}/sbin/modinfo"
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/kmod", "#{CREW_DEST_PREFIX}/sbin/modprobe"
-    FileUtils.ln_s "#{CREW_PREFIX}/bin/kmod", "#{CREW_DEST_PREFIX}/sbin/rmmod"
-  end
+  depends_on 'glibc' # R
+  depends_on 'openssl' # R
+  depends_on 'scdoc' => :build # Needed for manpages.
+  depends_on 'xzutils' # R
+  depends_on 'zlib' # R
+  depends_on 'zstd' # R
 end
