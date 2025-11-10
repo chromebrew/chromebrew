4dc6b318486d940f7653303914bf0bd1eeb69529
Ed Reel <edreel@gmail.com>
2025-11-10 05:32:20 +0000

    Onlyoffice 8.0.1 => 9.1.0

--- packages/onlyoffice.rb
+++ packages/onlyoffice.rb
@@ -3,16 +3,17 @@ require 'package'
 class Onlyoffice < Package
   description 'The most complete office suite'
   homepage 'https://www.onlyoffice.com/en/'
-  version '8.0.1'
+  version '9.1.0'
   license 'AGPL-3'
   compatibility 'x86_64'
   source_url "https://github.com/ONLYOFFICE/appimage-desktopeditors/releases/download/v#{version}/DesktopEditors-x86_64.AppImage"
-  source_sha256 '85e9e23363fe5076cebffe5569a1bc0f51eb80bd424e5085d19ec84e936c33f2'
+  source_sha256 '45f07575e18b04d6b3d4aeb86c6d651259745d7801eba14068c0017ced8f9d08'
 
-  depends_on 'gtk3'
-  depends_on 'gdk_base'
-  depends_on 'gsettings_desktop_schemas'
-  depends_on 'sommelier'
+  depends_on 'gtk3' # R
+  depends_on 'gdk_base' # R
+  depends_on 'gsettings_desktop_schemas' # R
+  depends_on 'libnotify' # R
+  depends_on 'sommelier' # R
 
   gnome
   no_compile_needed
@@ -35,6 +36,6 @@ class Onlyoffice < Package
   end
 
   def self.postinstall
-    ExitMessage.add "\nType 'onlyoffice' to get started.\n".lightblue
+    ExitMessage.add "\nType 'onlyoffice' to get started.\n"
   end
 end
