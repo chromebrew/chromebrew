bcb02e56efa6d63e2e53f24f41b1af29ad63b893
Ed Reel <edreel@gmail.com>
2025-11-10 08:16:40 +0000

    Platformsh 5.5.0 => 5.6.0

--- packages/platformsh.rb
+++ packages/platformsh.rb
@@ -3,11 +3,11 @@ require 'package'
 class Platformsh < Package
   description 'The unified tool for managing your Platform.sh services from the command line.'
   homepage 'https://docs.platform.sh/overview/cli.html'
-  version '5.5.0'
+  version '5.6.0'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://github.com/platformsh/cli/releases/download/#{version}/platform_#{version}_linux_amd64.tar.gz"
-  source_sha256 'ed6523905f2ef00bc2153d4cb4d8d8408de3966a00066984532838a5cb42ac6a'
+  source_sha256 'c5c3fac9c030a478a9fa69871b01f29be6be00de1f6cbb1e349d8a3a68b5f2b8'
 
   depends_on 'php83' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
@@ -19,6 +19,10 @@ class Platformsh < Package
     FileUtils.install 'platform', "#{CREW_DEST_PREFIX}/bin/platform", mode: 0o755
     FileUtils.install 'completion/bash/platform.bash', "#{CREW_DEST_PREFIX}/etc/bash.d/platform.bash", mode: 0o644
     FileUtils.install 'completion/bash/upsun.bash', "#{CREW_DEST_PREFIX}/etc/bash.d/upsun.bash", mode: 0o644
+  end
+
+  def self.postinstall
+    ExitMessage.add "\nType 'platform -h' to get started.\n"
   end
 
   def self.postremove
