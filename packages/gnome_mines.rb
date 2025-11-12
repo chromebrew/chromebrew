d7a3098d917d4023efda3b49d569c6828b741541
Satadru Pramanik <satadru@gmail.com>
2025-11-12 12:14:00 -0500

    Fix builds and rebuild librsvg.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gnome_mines.rb
+++ packages/gnome_mines.rb
@@ -11,21 +11,22 @@ class Gnome_mines < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'bfedcf49e0df255d097cd6db7ed3e9bba0ece1d18e3205f88297c9fa92120e0b',
-     armv7l: 'bfedcf49e0df255d097cd6db7ed3e9bba0ece1d18e3205f88297c9fa92120e0b',
-     x86_64: '2d59a0cd39fe8afe0fc6e5303a940dd4c9b0a28b5e77ff0964ce8f30b513f165'
+    aarch64: 'd73ee740fc15ad7336544d9ec41255f7838377a28927de5bd06cb3c7eb5610f5',
+     armv7l: 'd73ee740fc15ad7336544d9ec41255f7838377a28927de5bd06cb3c7eb5610f5',
+     x86_64: 'b350dd6d4692bf330f78806c7b00210e15d45430994322bf3bc83717366f2699'
   })
 
   depends_on 'clutter_gtk' => :build
   depends_on 'desktop_file_utils' => :build
-  depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'glibc' # R
   depends_on 'gsound' => :build
   depends_on 'gtk4' # R
-  depends_on 'libadwaita' => :build
-  depends_on 'libgnome_games_support' # R
+  depends_on 'libadwaita' # R
+  depends_on 'libgee' # R
   depends_on 'librsvg' => :build
   depends_on 'py3_itstool' => :build
+  depends_on 'vala' => :build
   depends_on 'wayland' => :build
 
   gnome
--- packages/libgnome_games_support.rb
+++ packages/libgnome_games_support.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Libgnome_games_support < Meson
   description 'libgnome-games-support is a small library intended for internal use by GNOME Games.'
   homepage 'https://gitlab.gnome.org/GNOME/libgnome-games-support'
-  version '1.8.2' # 2.0.0 is out, but gnome_mines and gnome_klotski don't work with it yet.
+  version '2.0.2'
   license 'LGPL-3+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/libgnome-games-support.git'
@@ -11,14 +11,16 @@ class Libgnome_games_support < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6f83e25e2fd800d309f96006eeb9c96f76ac11a6ab3370193427da0b7a8ba0b8',
-     armv7l: '6f83e25e2fd800d309f96006eeb9c96f76ac11a6ab3370193427da0b7a8ba0b8',
-     x86_64: 'a332d79955f7dd3e8804337410daea63b39cc41bd42b529acb61559cc4bed27b'
+    aarch64: 'c5154e770b824321f6981806bc604b72233e66b902a30c1e2c84171e11f4a08e',
+     armv7l: 'c5154e770b824321f6981806bc604b72233e66b902a30c1e2c84171e11f4a08e',
+     x86_64: 'd103aa843545add7198a0e9ee20949cbdf8982d4fc68c6e51864318f5557ddac'
   })
 
-  depends_on 'clutter'
-  depends_on 'gtk3'
-  depends_on 'libgee'
+  depends_on 'clutter' => :build
+  depends_on 'glib' # R
+  depends_on 'glibc' # R
+  depends_on 'gtk4' # R
+  depends_on 'libgee' # R
   depends_on 'vala' => :build
 
   gnome
