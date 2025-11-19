ea443e304a05fb52e9639cc72aff022873b3c41a
Ed Reel <edreel@gmail.com>
2025-11-19 16:50:09 -0600

    Tauri_cli 2.5.0 => 2.9.4 (#13524)

--- packages/tauri_cli.rb
+++ packages/tauri_cli.rb
@@ -3,12 +3,12 @@ require 'package'
 class Tauri_cli < Package
   description 'Build smaller, faster, and more secure desktop and mobile applications with a web frontend.'
   homepage 'https://tauri.app/'
-  version '2.5.0'
+  version '2.9.4'
   license 'Apache-2.0, MIT'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/tauri-apps/tauri/releases/download/tauri-cli-v#{version}/cargo-tauri-x86_64-unknown-linux-gnu.tgz"
-  source_sha256 '4ef86eb8bb53a9ec096a0ee3c406bfeaa8c371902877ee77313b6f9656f0b263'
+  source_sha256 '7889c6722ff7caa8dac0a0acaae505561df4a93e12e8985feea7ccaf3fc2b221'
 
   no_compile_needed
 
