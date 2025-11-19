d9c9864bf151e21703cc2008dfc31ca864ffb50a
Satadru Pramanik <satadru@gmail.com>
2025-11-19 15:01:03 -0500

    Update gexiv2 => 0.14.6 from the stable tag.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/gexiv2.rb
+++ packages/gexiv2.rb
@@ -3,17 +3,17 @@ require 'buildsystems/meson'
 class Gexiv2 < Meson
   description 'gexiv2 is a GObject wrapper around the Exiv2 photo metadata library.'
   homepage 'https://wiki.gnome.org/Projects/gexiv2/'
-  version '0.16.0'
+  version '0.14.6'
   license 'LGPL-2.1+ and GPL-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gexiv2.git'
-  git_hashtag version
+  git_hashtag "gexiv2-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6b1a205a7d8aa20fa47c8926c7dcb5575ddbc3e193ec2e41bfca1274c3774947',
-     armv7l: '6b1a205a7d8aa20fa47c8926c7dcb5575ddbc3e193ec2e41bfca1274c3774947',
-     x86_64: 'b199d4c0d0d6870144bf54af9db29f0f3a653f59f4683085521071c00f760763'
+    aarch64: '1948fbe179b802852811e91322676660acff7a345c12a04b93069863a2ed430c',
+     armv7l: '1948fbe179b802852811e91322676660acff7a345c12a04b93069863a2ed430c',
+     x86_64: '2a25ac5db8d59dd65d75d4df4a37cb35449bf06f065c1fd6764f64bd6e0b4e25'
   })
 
   depends_on 'gcc_lib' # R
