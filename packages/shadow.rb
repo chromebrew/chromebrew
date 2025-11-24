b31364f9ed6f6e7f2b2e72930867ce64b2b0fc05
Satadru Pramanik <satadru@gmail.com>
2025-11-23 23:01:12 -0500

    Add builds.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/shadow.rb
+++ packages/shadow.rb
@@ -11,29 +11,25 @@ class Shadow < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ce09c87fd90b9751c7f47be22d835490685f41f0fd4af484a7e06184115176e2',
-     armv7l: 'ce09c87fd90b9751c7f47be22d835490685f41f0fd4af484a7e06184115176e2',
-       i686: '09ea3f8487715d726dd822d921f6e1206e49e2e98ca8e5fd3d09e1933d6ce4f5',
-     x86_64: '3074d0d8cbfb2d807d5e5e9993a2429ac43391a085eadbcd95a9ed12ceeb391a'
+    aarch64: 'c2bbcfb3398aba4ca42c0f7947d4398f717f4a7ec397cb7884ca9fb993689811',
+     armv7l: 'c2bbcfb3398aba4ca42c0f7947d4398f717f4a7ec397cb7884ca9fb993689811',
+       i686: 'a5a6b286bd2bff3f63bf3beb89c4d59e45a1470e471b002b4506616f92f09c1d',
+     x86_64: 'f1f6ea17a3b13d6cb3f4cc4e1c2a60c82cb72f7706ca306d97738432780a8e36'
   })
 
   depends_on 'acl' # R
   depends_on 'attr' # R
   depends_on 'glibc' # R
-  depends_on 'libbsd' => :build
+  depends_on 'libbsd' # R
   depends_on 'libeconf' # R
-  depends_on 'libxcrypt' => :build
-  depends_on 'linux_pam' => :build
+  depends_on 'libmd' # R
+  depends_on 'libxcrypt' # R
+  depends_on 'linux_pam' # R
 
-  # ENV['CREW_LINKER_FLAGS'] = '-Wl,--undefined-version'
-  # no_env_options
-  no_mold
-
-  # autotools_pre_configure_options 'LD=ld.bfd CCLD=ld.bfd CXXLD=ld.bfd'
-  autotools_pre_configure_options 'env ;'
   autotools_configure_options "--bindir=#{CREW_PREFIX}/bin \
       --sbindir=#{CREW_PREFIX}/bin \
       --disable-logind \
+      --disable-static \
       --enable-shared \
       --with-libpam \
       --without-selinux \
@@ -44,9 +40,11 @@ class Shadow < Autotools
       --disable-account-tools-setuid"
 
   autotools_install_extras do
-    # Handle conflicts with coreutils.
-    @conflicts = %w[login su chfn chsh groups]
+    # Handle conflicts.
+    @conflicts = %w[login su chfn chsh groups newgrp]
     @conflicts.each do |conflict_file|
+      next unless File.file?("#{CREW_DEST_PREFIX}/bin/#{conflict_file}")
+
       FileUtils.mv "#{CREW_DEST_PREFIX}/bin/#{conflict_file}", "#{CREW_DEST_PREFIX}/bin/shadow_#{conflict_file}"
     end
   end
