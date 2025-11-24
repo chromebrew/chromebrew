c300906d8b5208c074d0511e5b110e4fcc9adec8
Satadru Pramanik <satadru@gmail.com>
2025-11-24 16:42:18 -0500

    Fix build, add i686 binaries.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/wget2.rb
+++ packages/wget2.rb
@@ -13,7 +13,7 @@ class Wget2 < Autotools
   binary_sha256({
     aarch64: '09e08370f029c62c46d1b5c1e308d77a9b195891e350c887252e1dc79b2380be',
      armv7l: '09e08370f029c62c46d1b5c1e308d77a9b195891e350c887252e1dc79b2380be',
-       i686: '3f1890cd75d06060a4cbc6c7abe5ab1f8ebdfade7fc9fcc899078eb27b396d32',
+       i686: 'fcb9cf5a798decde6d27134fe90e982668147a3aceeae56e917544bb3728c08d',
      x86_64: '7f1da024c4e1d6c8492906e1bcb9fcf627ec283b12c477e7c84d8e5ded28b886'
   })
 
@@ -21,13 +21,20 @@ class Wget2 < Autotools
   depends_on 'bzip2' # R
   depends_on 'ca_certificates' # L
   depends_on 'glibc' # R
+  depends_on 'gmp' # R
   depends_on 'gnutls' # R
   depends_on 'gperf' => :build
   depends_on 'libidn2' # R
   depends_on 'libmetalink' => :build
   depends_on 'libnghttp2' # R
   depends_on 'libpsl' # R
+  depends_on 'libtasn1' # R
+  depends_on 'libunbound' # R
+  depends_on 'libunistring' # R
   depends_on 'lzlib' # R
+  depends_on 'nettle' # R
+  depends_on 'openssl' # R
+  depends_on 'p11kit' # R
   depends_on 'pcre2' # R
   depends_on 'util_linux' => :build
   depends_on 'xzutils' # R
@@ -42,7 +49,22 @@ class Wget2 < Autotools
                   --with-lzma'
 
   def self.patch
-    system "sed -i '/AM_GNU_GETTEXT_VERSION/d' configure.ac"
+    File.write 'gettext.patch', <<~GETTEXT_PATCH_EOF
+      diff -Npaur a/configure.ac b/configure.ac
+      --- a/configure.ac	2025-11-24 16:32:01.716022174 -0500
+      +++ b/configure.ac	2025-11-24 16:33:02.520730059 -0500
+      @@ -417,10 +417,6 @@ m4_ifdef([AM_GNU_GETTEXT], [
+       ], [
+         have_po=no
+       ])
+      -m4_ifdef([AM_GNU_GETTEXT_VERSION], [
+      -#do not indent here
+      -AM_GNU_GETTEXT_VERSION([0.21])
+      -])
+       AM_CONDITIONAL([HAVE_PO], [ test "$have_po" = "yes" ])
+    GETTEXT_PATCH_EOF
+    system 'patch -Np1 -i gettext.patch'
+    system './bootstrap'
     system 'autoreconf -fiv'
   end
 
