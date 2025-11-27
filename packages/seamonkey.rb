73018020a137be316832ea9bfa418c6d5d4c63db
Ed Reel <edreel@gmail.com>
2025-11-27 09:35:48 +0000

    Seamonkey 2.53.21 => 2.53.22

--- packages/seamonkey.rb
+++ packages/seamonkey.rb
@@ -3,12 +3,12 @@ require 'package'
 class Seamonkey < Package
   description 'Web-browser, advanced e-mail, newsgroup and feed client, IRC chat, and HTML editing made simple'
   homepage 'https://www.seamonkey-project.org/'
-  version '2.53.21'
+  version '2.53.22'
   license 'Mozilla Public License 2.0'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://archive.seamonkey-project.org/releases/#{version}/linux-x86_64/en-US/seamonkey-#{version}.en-US.linux-x86_64.tar.bz2"
-  source_sha256 'd02d1e9409a2c3b864c6f6e96436da17aafef5cd75131e08c9e93f2e5276638c'
+  source_sha256 '56607cdb040aee55caef740441dd06a4d2a385c4c279a29d5aa95757aacfb7e0'
 
   depends_on 'dbus_glib'
   depends_on 'gtk3'
@@ -25,26 +25,14 @@ class Seamonkey < Package
   end
 
   def self.postinstall
-    print "\nSet seamonkey as your default browser? [Y/n]: "
-    case $stdin.gets.chomp.downcase
-    when '', 'y', 'yes'
-      Dir.chdir("#{CREW_PREFIX}/bin") do
-        FileUtils.ln_sf 'seamonkey', 'x-www-browser'
-      end
-      puts 'seamonkey is now your default browser.'.lightgreen
-    else
-      puts 'No change has been made.'.orange
-    end
-    ExitMessage.add "\nType 'seamonkey' to get started.\n"
+    ConvenienceFunctions.set_default_browser('seamonkey', 'seamonkey')
+  end
+
+  def self.preremove
+    ConvenienceFunctions.unset_default_browser('seamonkey', 'seamonkey')
   end
 
   def self.postremove
-    Dir.chdir("#{CREW_PREFIX}/bin") do
-      if File.exist?('x-www-browser') && File.symlink?('x-www-browser') \
-        && (File.realpath('x-www-browser') == "#{CREW_PREFIX}/bin/seamonkey")
-        FileUtils.rm 'x-www-browser'
-      end
-    end
     if Dir.exist?("#{HOME}/.mozilla/firefox")
       Package.agree_to_remove("#{HOME}/.mozilla/seamonkey")
     else
