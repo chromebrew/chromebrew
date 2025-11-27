46b52eb829fe5ff3868ae1340058182a7c9b861f
satmandu <satmandu@users.noreply.github.com>
2025-11-27 15:24:51 +0000

    Add unbuilt evolution_data_server to updater-evolution_data_server-3.58.2-icu77.1

--- packages/evolution_data_server.rb
+++ packages/evolution_data_server.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Evolution_data_server < CMake
   description 'Centralized access to appointments and contacts'
   homepage 'https://wiki.gnome.org/Apps/Evolution'
-  version "3.52.4-#{CREW_ICU_VER}"
+  version "3.58.2-#{CREW_ICU_VER}"
   license 'LGPL-2 or LGPL-3, BSD and Sleepycat'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/evolution-data-server.git'
