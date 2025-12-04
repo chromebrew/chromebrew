cda6c70f36fb061caf6b7f1d976f1f2e7dab907e
Satadru Pramanik <satadru@gmail.com>
2025-12-04 13:32:59 -0500

    Rebuild twine
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/py3_twine.rb
+++ packages/py3_twine.rb
@@ -10,10 +10,10 @@ class Py3_twine < Pip
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '88d5221118b3e99f26641c6bfd7bacfe70560581ffc334d9a80dfff6e6b831da',
-     armv7l: '88d5221118b3e99f26641c6bfd7bacfe70560581ffc334d9a80dfff6e6b831da',
-       i686: '6deda0090ed750c2a6d4e253e4c6068d78c2a1bc39376e7d3171c6125efe9dae',
-     x86_64: 'e2b9d778632deb60fd90d55c02798d1bd97c75c165e54132a0b54774c187c57d'
+    aarch64: 'd8af47fafc774e234b6bb6c2849b2fea5bb363dce9b5b9c281df2e9e6faf27b5',
+     armv7l: 'd8af47fafc774e234b6bb6c2849b2fea5bb363dce9b5b9c281df2e9e6faf27b5',
+       i686: '7f856334a6eadc87ad0f50453ee69beac1f57c746e41262a5e652ac2b8d76632',
+     x86_64: 'c96395c791a54d9b5aeae94fef798393f91e6bff6f25549e0bfdebae8fac8a98'
   })
 
   depends_on 'py3_certifi'
