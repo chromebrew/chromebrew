564b1c85e3a19dc6ddf0b40b3f16ac55f6792920
Ed Reel <edreel@gmail.com>
2025-12-05 17:06:33 -0600

    Clamav 1.4.3 => 1.5.1
    Json_c 0.18-20240915-1 => 0.18-a1249bfd

--- packages/json_c.rb
+++ packages/json_c.rb
@@ -3,18 +3,18 @@ require 'buildsystems/cmake'
 class Json_c < CMake
   description 'JSON-C implements a reference counting object model that allows you to easily construct JSON objects in C, output them as JSON formatted strings and parse JSON formatted strings back into the C representation of JSON objects.'
   homepage 'https://github.com/json-c/json-c'
-  version '0.18-20240915-1'
+  version '0.18-a1249bfd'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/json-c/json-c.git'
-  git_hashtag "json-c-#{version.split('-').tap { |a| a.pop 1 }.join('-')}"
+  git_hashtag 'a1249bfda0f6adf0363d5ab42d9ca816b3366ff2'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e19c19220772405804d3e2737af3962d9a87f6caa904dcd91a1f69804ed753eb',
-     armv7l: 'e19c19220772405804d3e2737af3962d9a87f6caa904dcd91a1f69804ed753eb',
-       i686: '50c56ec2983be8ce16abafb87e3e988aeb484e7d8b314518defb0525907df424',
-     x86_64: 'c1667d019008395fb5643bc88a3fd6860318b4ff3adf6014044a22a9c1fe7ce9'
+    aarch64: '43eef2f78f952ad0244cccbc275f4072c95039602fbe95e637cb72ff3e14d4dc',
+     armv7l: '43eef2f78f952ad0244cccbc275f4072c95039602fbe95e637cb72ff3e14d4dc',
+       i686: '588339a0482d0f81f221e1b13ac19f101f1e3487f1cf050190795994a589737b',
+     x86_64: 'bb359d8041f610e47d1e9c808c03338207d310ddae7546b5b3116da391d1302a'
   })
 
   depends_on 'glibc' # R
