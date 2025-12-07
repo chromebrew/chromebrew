d57b07eed58ae84d96bfceca42ad88386b603f79
Ed Reel <edreel@gmail.com>
2025-12-07 00:36:45 -0600

    Appstream 1.0.3 => 1.1.1 (#13765)

--- packages/appstream.rb
+++ packages/appstream.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Appstream < Meson
   description 'Provides a standard for creating app stores across distributions'
   homepage 'https://www.freedesktop.org/wiki/Distributions/AppStream/'
-  version '1.0.3'
+  version '1.1.1'
   license 'GPL'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/ximion/appstream.git'
@@ -14,26 +14,25 @@ class Appstream < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1b507f791833e4d60dd494cf1fbea2cffe203373aed8063383956361e7f0f1d1',
-     armv7l: '1b507f791833e4d60dd494cf1fbea2cffe203373aed8063383956361e7f0f1d1',
-     x86_64: '23f37368bd8055286db5ff9dd7b1d7acd538896fc24a6bc0ef90ebb469b52c7e'
+    aarch64: 'd058391cf39a5a986273a8413c29ff466b3fb7541e2895853769a004efa62a81',
+     armv7l: 'd058391cf39a5a986273a8413c29ff466b3fb7541e2895853769a004efa62a81',
+     x86_64: '459bf98ba7d915107c9f42c07fbdd5e35b1f7847d4db0b4c9d637cc877104d01'
   })
 
   depends_on 'cairo' # R
   depends_on 'curl' # R
   depends_on 'fontconfig' # R
   depends_on 'freetype' # R
-  depends_on 'gcc_lib' => :build
   depends_on 'gcc_lib' # R
   depends_on 'gdk_pixbuf' # R
-  depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'glibc' # R
   depends_on 'gobject_introspection' => :build
   depends_on 'gperf' => :build
   depends_on 'gtk_doc' => :build
   depends_on 'harfbuzz' # R
-  depends_on 'py3_itstool' => :build
   depends_on 'libadwaita' # R
+  depends_on 'libfyaml' # R
   depends_on 'librsvg' # R
   depends_on 'libstemmer' # R
   depends_on 'libxml2' # R
@@ -41,6 +40,7 @@ class Appstream < Meson
   depends_on 'libyaml' # R
   depends_on 'pango' # R
   depends_on 'py3_gi_docgen' => :build
+  depends_on 'py3_itstool' => :build
   depends_on 'py3_libxml2' => :build
   depends_on 'vala' => :build
   depends_on 'xmlto' => :build
@@ -49,4 +49,8 @@ class Appstream < Meson
   meson_options '-Dcompose=true \
     -Dsystemd=false \
     -Dvapi=true'
+
+  def self.postinstall
+    ExitMessage.add "\nType 'appstreamcli --help' to get started.\n"
+  end
 end
