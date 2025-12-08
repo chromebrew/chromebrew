e64cdb712d4994ad24b62e9442eb90bf558fcb6c
Zopolis4 <creatorsmithmdt@gmail.com>
2025-12-08 12:56:21 +1100

    Drop native extensions for ruby_ruby_libversion

--- packages/ruby_ruby_libversion.rb
+++ packages/ruby_ruby_libversion.rb
@@ -7,15 +7,8 @@ class Ruby_ruby_libversion < RUBY
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
-  binary_compression 'gem'
 
-  binary_sha256({
-    aarch64: 'ec87b38fb09bdff6bf2f5b89c4f4eb275078407234fe7e4bdcad0eef676edab6',
-     armv7l: 'ec87b38fb09bdff6bf2f5b89c4f4eb275078407234fe7e4bdcad0eef676edab6',
-       i686: 'd1ff6e76b3ab134b5a1cc3fc0fdb314930f7aa3b87eb4e0eb1e846d75cd19069',
-     x86_64: '513694e8aaa844a72bcf2c9dcbd75ea3f0b23d743d583c5b2028a6809b811362'
-  })
-
-  depends_on 'libversion' # R
-  gem_compile_needed
+  # TODO: We should be compiling the native extensions, but that is currently broken
+  # as the resulting binary does not have GEM_ARCH at the end of the name, causing
+  # an empty filelist to be generated.
 end
