ef7261a8db46e46ab7c59ab22324736ae35963d5
uberhacker <edreel@gmail.com>
2025-12-14 05:06:54 +0000

    Audacious_plugins 4.5 => 4.5.1

--- packages/audacious_plugins.rb
+++ packages/audacious_plugins.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Audacious_plugins < Autotools
   description 'Audacious is an open source audio player.'
   homepage 'https://github.com/audacious-media-player/audacious-plugins'
-  version '4.5'
+  version '4.5.1'
   license 'GPL-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://distfiles.audacious-media-player.org/audacious-plugins-#{version}.tar.bz2"
-  source_sha256 '36c19940ee7227f67df4f0c7fd98a5f60c60257a1a47ecd014c9e2a26d7846dd'
+  source_sha256 'f4feedc32776acfa9d24701d3b794fc97822f76da6991e91e627e70e561fdd3b'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -22,13 +22,23 @@ class Audacious_plugins < Autotools
   depends_on 'cairo' # R
   depends_on 'curl' # R
   depends_on 'flac' # R
-  depends_on 'flac' => :build
+  depends_on 'flac' # R
   depends_on 'gcc_lib' # R
   depends_on 'gdk_pixbuf' # R
   depends_on 'glib' # R
   depends_on 'glibc' # R
+  depends_on 'gtk3' # R
+  depends_on 'harfbuzz' # R
+  depends_on 'json_glib' # R
   depends_on 'libglvnd' # R
+  depends_on 'libogg' # R
+  depends_on 'libvorbis' # R
   depends_on 'libx11' # R
+  depends_on 'libxcomposite' # R
+  depends_on 'libxml2' # R
+  depends_on 'libxrender' # R
+  depends_on 'mpg123' # R
+  depends_on 'neon' # R
   depends_on 'pango' # R
   depends_on 'wavpack'
   depends_on 'zlib' # R
