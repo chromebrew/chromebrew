9a6991879a269610ae9a1ef30867545bc423a238
Ed Reel <edreel@gmail.com>
2025-12-17 11:19:02 +0000

    Kakoune v2018.09.04 => 2025.06.03

--- packages/kakoune.rb
+++ packages/kakoune.rb
@@ -1,15 +1,14 @@
 require 'package'
 
 class Kakoune < Package
-  # TODO: prebuild version
-  description 'mawww\'s experiment for a better code editor'
+  description "mawww's experiment for a better code editor"
   homepage 'http://kakoune.org/'
   compatibility 'all'
   license 'Unlicense'
-  version 'v2018.09.04'
-  source_url 'https://github.com/mawww/kakoune/releases/download/v2018.09.04/kakoune-2018.09.04.tar.bz2'
-  source_sha256 '7a31c9f08c261c5128d1753762721dd7b7fe4bb4e9a3c368c9d768c72a1472e1'
-  binary_compression 'tar.xz'
+  version '2025.06.03'
+  source_url 'https://github.com/mawww/kakoune.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'a4304ec155e6c375c841451df07d60b19ed288b0abc9f99b46a6de52a2b40321',
@@ -18,19 +17,19 @@ class Kakoune < Package
      x86_64: 'daed7d68e53f1f2751dc6a9a59c3efaab0a89651ff690b2cb5ac8be02e4d95f1'
   })
 
-  depends_on 'ncurses' => :build
-  depends_on 'py3_asciidoc' => :build
-  depends_on 'libxslt' => :build
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
 
   def self.build
-    Dir.chdir('src') do
-      system 'make', "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'debug=no'
-    end
+    system 'make && make man'
   end
 
   def self.install
-    Dir.chdir('src') do
-      system 'make', 'install', "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'debug=no'
-    end
+    FileUtils.install 'src/kak', "#{CREW_DEST_PREFIX}/bin/kak", mode: 0o755
+    FileUtils.install 'doc/kak.1', "#{CREW_DEST_MAN_PREFIX}/man1/kak.1", mode: 0o644
+  end
+
+  def self.postinstall
+    ExitMessage.add "\nType 'kak' to get started.\n"
   end
 end
