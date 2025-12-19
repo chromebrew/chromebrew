18470ba2e6aaf69b382bd8ce6ccb0e3fbd2c0940
github-merge-queue <github-merge-queue@users.noreply.github.com>
2025-12-19 00:24:00 +0000

    Add unbuilt ruby_forwardable to updater-ruby_forwardable-1.4.0-ruby3.4

--- packages/ruby_forwardable.rb
+++ packages/ruby_forwardable.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_forwardable < RUBY
   description 'Provides delegation of specified methods to a designated object.'
   homepage 'https://github.com/ruby/forwardable'
-  version "1.3.3-#{CREW_RUBY_VER}"
+  version "1.4.0-#{CREW_RUBY_VER}"
   license 'Ruby'
   compatibility 'all'
   source_url 'SKIP'
