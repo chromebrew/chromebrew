a8fe12f6d827aae7f1fa834de5ea5943b05a5b4a
Ed Reel <edreel@gmail.com>
2025-12-20 21:09:37 -0600

    Etcd 3.6.6 => 3.6.7 (#13916)

--- packages/etcd.rb
+++ packages/etcd.rb
@@ -3,11 +3,11 @@ require 'package'
 class Etcd < Package
   description 'Distributed reliable key-value store for the most critical data of a distributed system'
   homepage 'https://etcd.io/'
-  version '3.6.6'
+  version '3.6.7'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/etcd-io/etcd/releases/download/v#{version}/etcd-v#{version}-linux-amd64.tar.gz"
-  source_sha256 '887afaa4a99f22d802ccdfbe65730a5e79aa5c9ce2c8799c67e9d804c50ecedb'
+  source_sha256 'cf8af880c5a01ee5363cefa14a3e0cb7e5308dcf4ed17a6973099c9a7aee5a9a'
 
   no_compile_needed
   no_shrink
