8c9d7f8955f817e7dd706bb86de684773b5054a7
Ed Reel <edreel@gmail.com>
2025-12-20 21:08:56 -0600

    Go 1.25.4 => 1.25.5 (#13918)

--- packages/go.rb
+++ packages/go.rb
@@ -3,7 +3,7 @@ require 'package'
 class Go < Package
   description 'Go is an open source programming language that makes it easy to build simple, reliable, and efficient software.'
   homepage 'https://go.dev'
-  version '1.25.4'
+  version '1.25.5'
   license 'BSD'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Go < Package
      x86_64: "https://go.dev/dl/go#{version}.linux-amd64.tar.gz"
   })
   source_sha256({
-    aarch64: 'ff86a6406310d840edb170f539a51a7efac0ac2b2f84527b1b2bf5935fc4ab6d',
-     armv7l: 'ff86a6406310d840edb170f539a51a7efac0ac2b2f84527b1b2bf5935fc4ab6d',
-       i686: '0cf5f721ab7c5e7a170dedb2b51d9b1fedfe6ef1b2c626bf7a47fb9a613c5d96',
-     x86_64: '9fa5ffeda4170de60f67f3aa0f824e426421ba724c21e133c1e35d6159ca1bec'
+    aarch64: '0b27e3dec8d04899d6941586d2aa2721c3dee67c739c1fc1b528188f3f6e8ab5',
+     armv7l: '0b27e3dec8d04899d6941586d2aa2721c3dee67c739c1fc1b528188f3f6e8ab5',
+       i686: 'db908a86e888574ed3432355ba5372ad3ef2c0821ba9b91ceaa0f6634620c40c',
+     x86_64: '9e9b755d63b36acf30c12a9a3fc379243714c1c6d3dd72861da637f336ebb35b'
   })
 
   conflicts_ok # FIXME: Remove this when file conflicts between go and gcc are fixed
