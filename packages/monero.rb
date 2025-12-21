49da0391d241cea206ea8caa0e3c8a53b44c0b19
Ed Reel <edreel@gmail.com>
2025-12-20 20:51:01 -0600

    Monero 0.18.4.3 => 0.18.4.4 (#13925)

--- packages/monero.rb
+++ packages/monero.rb
@@ -3,7 +3,7 @@ require 'package'
 class Monero < Package
   description 'Private, decentralized cryptocurrency that keeps your finances confidential and secure.'
   homepage 'https://www.getmonero.org/'
-  version '0.18.4.3'
+  version '0.18.4.4'
   license 'The Cryptonote developers,The Boolberry developers,MIT'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.27'
@@ -14,9 +14,9 @@ class Monero < Package
      x86_64: "https://downloads.getmonero.org/cli/monero-linux-x64-v#{version}.tar.bz2"
   })
   source_sha256({
-    aarch64: '3ac83049bc565fb5238501f0fa629cdd473bbe94d5fb815088af8e6ff1d761cd',
-     armv7l: '3ac83049bc565fb5238501f0fa629cdd473bbe94d5fb815088af8e6ff1d761cd',
-     x86_64: '3a7b36ae4da831a4e9913e0a891728f4c43cd320f9b136cdb6686b1d0a33fafa'
+    aarch64: '2040dc22748ef39ed8a755324d2515261b65315c67b91f449fa1617c5978910b',
+     armv7l: '2040dc22748ef39ed8a755324d2515261b65315c67b91f449fa1617c5978910b',
+     x86_64: '7fe45ee9aade429ccdcfcad93b905ba45da5d3b46d2dc8c6d5afc48bd9e7f108'
   })
 
   no_compile_needed
