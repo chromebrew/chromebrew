5e44a3861b5983de46652c9579cf6cd994abcd6a
Zopolis4 <creatorsmithmdt@gmail.com>
2025-12-26 06:46:57 -0500

    fontforge 20230101 -> 20251009

--- packages/fontforge.rb
+++ packages/fontforge.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Fontforge < CMake
   description 'Free (libre) font editor for Windows, Mac OS X and GNU+Linux'
   homepage 'https://fontforge.org/'
-  version '20230101'
+  version '20251009'
   license 'BSD and GPL-3+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/fontforge/fontforge.git'
@@ -11,17 +11,10 @@ class Fontforge < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5f507e5c55da931b9d29865c522c7beea91bb906f15b4a354cee23bb89c60d16',
-     armv7l: '5f507e5c55da931b9d29865c522c7beea91bb906f15b4a354cee23bb89c60d16',
-     x86_64: '0c38cb948b509b784a32e1bf6de083f4f95982e7ca067e9d1397ddc978ea43b6'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
-
-  # https://github.com/fontforge/fontforge/issues/5251
-  def self.patch
-    # Fix errors in French and Italian translations
-    downloader 'https://patch-diff.githubusercontent.com/raw/fontforge/fontforge/pull/5257.patch', '91a2836b4ae8a572ad0cc9b867cf9313b64adbb094d416e8ba17535fc8b839b3'
-    system 'git apply 5257.patch'
-  end
 
   depends_on 'cairo' # R
   depends_on 'freetype' # R
@@ -29,6 +22,7 @@ class Fontforge < CMake
   depends_on 'giflib' # R
   depends_on 'glib' # R
   depends_on 'gtk3'
+  depends_on 'gtkmm3'
   depends_on 'harfbuzz' # R
   depends_on 'libjpeg_turbo' # R
   depends_on 'libpng' # R
