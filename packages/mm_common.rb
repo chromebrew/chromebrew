3b731752a0842325e30a18657680c4daa971f751
Zopolis4 <creatorsmithmdt@gmail.com>
2025-12-26 10:50:00 -0500

    Fix compatibility for mm_common

--- packages/mm_common.rb
+++ packages/mm_common.rb
@@ -5,7 +5,7 @@ class Mm_common < Meson
   homepage 'https://www.gtkmm.org/'
   version '1.0.7'
   license 'GPL-2'
-  compatibility 'aarch64 armv7l x86_64'
+  compatibility 'all'
   source_url 'https://gitlab.gnome.org/GNOME/mm-common.git'
   git_hashtag version
   binary_compression 'tar.zst'
@@ -13,6 +13,7 @@ class Mm_common < Meson
   binary_sha256({
     aarch64: '8ddca6353d6b5db344516daa86b1d49bc2a61f9431f32fcfee26ab08a85e7a0b',
      armv7l: '8ddca6353d6b5db344516daa86b1d49bc2a61f9431f32fcfee26ab08a85e7a0b',
+       i686: '9253e0064a20cb48957c452b1edf13e76a320a941582f4f1b6d47bd018d81628',
      x86_64: 'bea3f846e57af0a970adb5875bc9953864059e744a933ad035738d0e3b519ea7'
   })
 
