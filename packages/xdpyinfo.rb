6bd5d70865e6b141e614792abaa2630db83b8947
Zopolis4 <creatorsmithmdt@gmail.com>
2025-12-26 20:53:54 -0500

    xdpyinfo 1.3.3 -> 1.4.0

--- packages/xdpyinfo.rb
+++ packages/xdpyinfo.rb
@@ -1,12 +1,9 @@
-# Adapted from Arch Linux xorg-xdpyinfo PKGBUILD at:
-# https://github.com/archlinux/svntogit-packages/raw/packages/xorg-xdpyinfo/trunk/PKGBUILD
+require 'buildsystems/autotools'
 
-require 'package'
-
-class Xdpyinfo < Package
+class Xdpyinfo < Autotools
   description 'Display information utility for X'
   homepage 'https://xorg.freedesktop.org/wiki/'
-  version '1.3.3'
+  version '1.4.0'
   license 'MIT-with-advertising'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.freedesktop.org/xorg/app/xdpyinfo.git'
@@ -14,32 +11,23 @@ class Xdpyinfo < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6041d86f4823e1dd0918d920124ef79eebfa2f7496947d3171a8f58d09358b33',
-     armv7l: '6041d86f4823e1dd0918d920124ef79eebfa2f7496947d3171a8f58d09358b33',
-     x86_64: '50a3cd0a5276633ef0b5d825a76fb90fa5f10d5790ed9a394eb900924d44d667'
+    aarch64: '2faca8909ba01d4f05e0c3bd6eff944f8ed7251d7583b2d8a71c68b2061e804c',
+     armv7l: '2faca8909ba01d4f05e0c3bd6eff944f8ed7251d7583b2d8a71c68b2061e804c',
+     x86_64: '43db5b0f7d7d32505e4348d5c819eab63a3c65f688008e79dc017f033043a85e'
   })
 
-  depends_on 'xorg_proto' => :build
   depends_on 'glibc' # R
   depends_on 'libdmx' # R
   depends_on 'libx11' # R
   depends_on 'libxcb' # R
   depends_on 'libxcomposite' # R
   depends_on 'libxext' # R
+  depends_on 'libxfixes'
   depends_on 'libxi' # R
   depends_on 'libxinerama' # R
   depends_on 'libxrender' # R
   depends_on 'libxtst' # R
   depends_on 'libxxf86dga' # R
   depends_on 'libxxf86vm' # R
-
-  def self.build
-    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
-  end
+  depends_on 'xorg_proto' => :build
 end
