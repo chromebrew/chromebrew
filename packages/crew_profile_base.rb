eb750f61fb1daaea31e238b67da3cc59cc31a5d5
Ed Reel <edreel@gmail.com>
2025-12-28 16:49:55 +0000

    Crew_profile_base 0.0.32 => 0.0.33

--- packages/crew_profile_base.rb
+++ packages/crew_profile_base.rb
@@ -3,11 +3,11 @@ require 'package'
 class Crew_profile_base < Package
   description 'Crew-profile-base sets up Chromebrew\'s environment capabilities.'
   homepage 'https://github.com/chromebrew/crew-profile-base'
-  version '0.0.32'
+  version '0.0.33'
   license 'GPL-3+'
   compatibility 'all'
-  source_url "https://github.com/chromebrew/crew-profile-base/archive/refs/tags/#{version}.tar.gz"
-  source_sha256 'f9f0705fb68fa69372c0f730af398029704be9b15d8781230ded67cc459a0bdd'
+  source_url 'https://github.com/chromebrew/crew-profile-base.git'
+  git_hashtag version
 
   no_compile_needed
   print_source_bashrc
