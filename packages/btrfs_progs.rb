350542801722a964fe81316c4208eda62b75f01d
Ed Reel <edreel@gmail.com>
2025-12-29 19:41:17 -0600

    Btrfs_progs 6.9 => 6.17.1 (#14010)

--- packages/btrfs_progs.rb
+++ packages/btrfs_progs.rb
@@ -3,18 +3,17 @@ require 'buildsystems/autotools'
 class Btrfs_progs < Autotools
   description 'BTRFS is a modern copy on write filesystem for Linux aimed at implementing advanced features while also focusing on fault tolerance, repair and easy administration.'
   homepage 'https://btrfs.readthedocs.io/en/latest/'
-  version '6.9'
+  version '6.17.1'
   license 'GPL-2'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/kdave/btrfs-progs.git'
   git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b82e35f3745ec1d31ae24c99d9a1abd46830c576106280810cc0035ed23b181b',
-     armv7l: 'b82e35f3745ec1d31ae24c99d9a1abd46830c576106280810cc0035ed23b181b',
-       i686: 'bc6641ab32514d25078506a97456ab0556fb0b109f2874e138940cab57cc14b4',
-     x86_64: '5de03dcbd63e8741b54552cb2568766e7884eec1838cf4d52d2bf527bf5db790'
+    aarch64: '093314c5f1cf6c974bfea1c9980d7fb644850a53d14d2df05d41e499ede93b7a',
+     armv7l: '093314c5f1cf6c974bfea1c9980d7fb644850a53d14d2df05d41e499ede93b7a',
+     x86_64: 'caa3bfd5d60bae8f3fab608b5bcd5e1ee649eb4c4e5a7152599efcb0c33f2c3d'
   })
 
   depends_on 'e2fsprogs'
@@ -22,6 +21,8 @@ class Btrfs_progs < Autotools
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'libgcrypt' # R
+  depends_on 'libsodium' # R
+  depends_on 'libuuid' # R
   depends_on 'lzo' # R
   depends_on 'util_linux' # R
   depends_on 'zlib' # R
