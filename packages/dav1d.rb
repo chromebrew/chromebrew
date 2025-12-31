fcf6ed57fda9d3ea896ce8e87b6617c166ae819e
Ed Reel <edreel@gmail.com>
2025-12-31 21:56:00 +0000

    Dav1d 1.5.2 => 1.5.3

--- packages/dav1d.rb
+++ packages/dav1d.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Dav1d < Meson
   description 'dav1d is a new AV1 cross-platform decoder, open-source, and focused on speed and correctness.'
   homepage 'https://code.videolan.org/videolan/dav1d'
-  version '1.5.2'
+  version '1.5.3'
   license 'BSD-2'
   compatibility 'all'
   source_url 'https://code.videolan.org/videolan/dav1d.git'
@@ -11,10 +11,10 @@ class Dav1d < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e5fc00913117a06424326c6803e9d8ebcca07db9164f734c9b383b4f0d8e456d',
-     armv7l: 'e5fc00913117a06424326c6803e9d8ebcca07db9164f734c9b383b4f0d8e456d',
-       i686: '4c402d4771a7ebef6b38a8d55d737ae87ca2906acc807b0127ec21c7662e63b9',
-     x86_64: '691d00f59ff04c670f19c800e62d02a3a52c5bff947c43535c54ba8f8068877f'
+    aarch64: '96852f722190552c9362c8cacc0450ed6e983d31f049f413191fabea83579b3e',
+     armv7l: '96852f722190552c9362c8cacc0450ed6e983d31f049f413191fabea83579b3e',
+       i686: 'bb2b93f8b16cb3c3e4f0f13152b55b9f0215be2ee3c58cf35f068dffb2cc8f54',
+     x86_64: '422fe62bb8f9dd287e1b03a96f19d788ecd868b30e535b4d1f693aee0c1066f0'
   })
 
   depends_on 'gcc_lib' # R
