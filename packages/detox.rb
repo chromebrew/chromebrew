04a959bf15390b9b84401e4e46663f77f3004b78
Ed Reel <edreel@gmail.com>
2025-12-31 07:39:28 +0000

    Detox 2.0.0 => 3.0.1

--- packages/detox.rb
+++ packages/detox.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Detox < Autotools
   description 'detox is a program that renames files to make them easier to work with under Linux and other Unix-like operating systems.'
   homepage 'https://github.com/dharple/detox'
-  version '2.0.0'
+  version '3.0.1'
   license 'BSD-3 Clause'
   compatibility 'all'
   source_url 'https://github.com/dharple/detox.git'
@@ -11,9 +11,11 @@ class Detox < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '997436983cee04e7d368d74709b77ded41959793c0c9536aedb6d065517d6856',
-     armv7l: '997436983cee04e7d368d74709b77ded41959793c0c9536aedb6d065517d6856',
-       i686: 'e8db275366a3402c1f293a2de4ae2734438e606cbe7e807144d581c9cb1a3b30',
-     x86_64: 'c6841edbd7daf7c0fda861de3d30546f2108f5207bc1b8dab6529095c5fb3724'
+    aarch64: '63b3f1aed4cca8017ea6ff5c7188c9ea1ad30627debbe0825220a9ce1ecb0c12',
+     armv7l: '63b3f1aed4cca8017ea6ff5c7188c9ea1ad30627debbe0825220a9ce1ecb0c12',
+       i686: 'bbc10e0d040d9cab86b60cfdb7514f71f33084d44e37a7a72faf2433bac50049',
+     x86_64: 'f6075327c543f7896d7747f42c45e7c043ade6d4628b80f56daaccdae594297b'
   })
+
+  depends_on 'glibc' # R
 end
