0606e55eedb724c6f98067c749d1a421608b3754
Ed Reel <edreel@gmail.com>
2025-12-31 20:15:04 +0000

    Doctl 1.100.0 => 1.148.0

--- packages/doctl.rb
+++ packages/doctl.rb
@@ -3,20 +3,23 @@ require 'package'
 class Doctl < Package
   description 'The official command line interface for the DigitalOcean API.'
   homepage 'https://github.com/digitalocean/doctl'
-  version '1.100.0'
+  version '1.148.0'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9de72297695d143c82386eb728114e2169fc384922ce67fa4565050228f6a8ec',
-     armv7l: '9de72297695d143c82386eb728114e2169fc384922ce67fa4565050228f6a8ec',
-       i686: 'f568369579923bb53c52e628dad85904314a0d6e579c14ca004de298acf7a95d',
-     x86_64: '87919f94bf861fc6b7f5c3320b75fff075553729774d4fc0afe4e6a2878ebc78'
+    aarch64: 'f8c20f273b7a8e7bdb6b3d9907bf987325023bdeba5acc8cbd38e77edd55b07f',
+     armv7l: 'f8c20f273b7a8e7bdb6b3d9907bf987325023bdeba5acc8cbd38e77edd55b07f',
+       i686: '1f6e651d0e733bd1e56eaf474b055934920808d7aed1ba2f61ddd49c3cb579ac',
+     x86_64: 'a6d80d75f25048da7c546d656dc32eef2fcade30c6a0df46781cc92eaae9677e'
   })
 
+  depends_on 'glibc' # R
   depends_on 'go' => :build
+
+  no_source_build
 
   def self.install
     system "GOBIN=#{CREW_DEST_PREFIX}/bin go install github.com/digitalocean/doctl/cmd/doctl@v#{version}"
