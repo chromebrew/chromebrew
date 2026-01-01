9e5d408405df53f40a34d12c049b2a9cf31cc922
Ed Reel <edreel@gmail.com>
2026-01-01 22:38:55 +0000

    Enet 1.3.17 => 1.3.18

--- packages/enet.rb
+++ packages/enet.rb
@@ -3,7 +3,7 @@ require 'package'
 class Enet < Package
   description 'ENet reliable UDP networking library'
   homepage 'https://github.com/lsalzman/enet'
-  version '1.3.17'
+  version '1.3.18'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/lsalzman/enet.git'
@@ -11,11 +11,13 @@ class Enet < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8dceccdbee07800d199f700a1c3eeb2a8a9003b948052ff29ea09cbdc5e358ea',
-     armv7l: '8dceccdbee07800d199f700a1c3eeb2a8a9003b948052ff29ea09cbdc5e358ea',
-       i686: '443da89c623d5bb1d921b145643e5e83f2178f72fd85c55427f641c4b59e964c',
-     x86_64: '0f96b8e2b248e78bc8e3f70bb21c5006d0c0064382b3930441317a8856078252'
+    aarch64: 'f6eb360adef080a2b6ac1954086cce85537f9899bbf4d2f8b49e6dacd9ade1a7',
+     armv7l: 'f6eb360adef080a2b6ac1954086cce85537f9899bbf4d2f8b49e6dacd9ade1a7',
+       i686: 'd666bba3ba04f0895215641257190582fcfbcd1f898c111156fcd6ab158e930a',
+     x86_64: '3f8e7519ccfd3d65c4834bf66157e5df9e84a6451a4d73c520a08ce5fd738039'
   })
+
+  depends_on 'glibc' # R
 
   def self.build
     system 'autoreconf -vfi'
