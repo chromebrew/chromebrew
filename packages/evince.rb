0e68058e57767bb74fd0da52192453f23c9d8b3e
Ed Reel <edreel@gmail.com>
2026-01-02 09:12:44 +0000

    Evince 45.0 => 48.1

--- packages/evince.rb
+++ packages/evince.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Evince < Meson
   description 'Document viewer PDF, PostScript, XPS, djvu, dvi, tiff, cbr, cbz, cb7, cbt'
   homepage 'https://wiki.gnome.org/Apps/Evince'
-  version '45.0'
+  version '48.1'
   license 'GPL'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/evince.git'
@@ -14,9 +14,9 @@ class Evince < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '25eb43693a5ca3fc77814f63d91a5dd07382f9e73f4bd10d689751485395519d',
-     armv7l: '25eb43693a5ca3fc77814f63d91a5dd07382f9e73f4bd10d689751485395519d',
-     x86_64: 'b18d1011d5dcf0718034146d0f93ede644b15111e005167391c7c52e057d5288'
+    aarch64: '74d656129feb4be560d3f0796238c9436f610f63fd2df7b78422cae392cd88d7',
+     armv7l: '74d656129feb4be560d3f0796238c9436f610f63fd2df7b78422cae392cd88d7',
+     x86_64: '305b088604804d6e8da83e1f8936610199aab024a8de87d39ada08f59b9026e6'
   })
 
   depends_on 'at_spi2_core' # R
@@ -25,9 +25,10 @@ class Evince < Meson
   depends_on 'docbook_xsl' => :build
   depends_on 'gcc_lib' # R
   depends_on 'gdk_pixbuf' # R
-  depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'glibc' # R
   depends_on 'gnome_desktop' # R
+  depends_on 'gspell' # R
   depends_on 'gstreamer' # R
   depends_on 'gtk3' # R
   depends_on 'gtk_doc' => :build
