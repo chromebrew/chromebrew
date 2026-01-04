c71743f8d2776b740a6ff798fa029d9e25d5da2b
Satadru Pramanik <satadru@gmail.com>
2026-01-04 15:34:50 -0500

    ddrescue => 1.30
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/ddrescue.rb
+++ packages/ddrescue.rb
@@ -1,14 +1,14 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Ddrescue < Package
+class Ddrescue < Autotools
   description 'GNU ddrescue is a data recovery tool.'
   homepage 'https://www.gnu.org/software/ddrescue/'
-  version '1.25'
+  version '1.30'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://ftpmirror.gnu.org/ddrescue/ddrescue-1.25.tar.lz'
-  source_sha256 'ce538ebd26a09f45da67d3ad3f7431932428231ceec7a2d255f716fa231a1063'
-  binary_compression 'tar.xz'
+  source_url "https://ftpmirror.gnu.org/ddrescue/ddrescue-#{version}.tar.lz"
+  source_sha256 '2264622d309d6c87a1cfc19148292b8859a688e9bc02d4702f5cd4f288745542'
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'abc719d94dc994ffc18cbe065e6606282d04f78652cab536897af2773b447d0d',
@@ -17,17 +17,5 @@ class Ddrescue < Package
      x86_64: '8fca45af7835153cff43b00cebc1a990b450070f7e7de16887d9ff4b91bcdf0e'
   })
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS} \
-            --enable-non-posix"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  autotools_configure_options '--enable-non-posix'
 end
