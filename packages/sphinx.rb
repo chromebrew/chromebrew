c2ac721b9b96c163d869b28b069d6a454e24be0e
Ed Reel <edreel@gmail.com>
2026-01-05 17:47:12 -0600

    Sphinx 9.0.3-py3.14 => 9.1.0-py3.14 (#14048)
    Add py3_roman_numerals dependency

--- packages/sphinx.rb
+++ packages/sphinx.rb
@@ -3,34 +3,35 @@ require 'buildsystems/pip'
 class Sphinx < Pip
   description 'Sphinx is a tool that makes it easy to create intelligent and beautiful documentation.'
   homepage 'https://www.sphinx-doc.org/'
-  version "9.0.3-#{CREW_PY_VER}"
+  version "9.1.0-#{CREW_PY_VER}"
   license 'BSD'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8cb2c60a2cd27729ddf1c2fe38777e2b4a10b299e585e44d4c0d46b6527f2134',
-     armv7l: '8cb2c60a2cd27729ddf1c2fe38777e2b4a10b299e585e44d4c0d46b6527f2134',
-       i686: '4ca18d7635c92e2ba99b3b79cf633d6162bc67b305d7e06da6f7d66dbde495a9',
-     x86_64: '30645c21680e73e8f34d2595cfaeb68ce86b7e28e6451ec2ffe3750dbede5578'
+    aarch64: 'b713724e8a7589de9470f8051d7947fe0f2a36003eb388d1f886268ad5f08ccb',
+     armv7l: 'b713724e8a7589de9470f8051d7947fe0f2a36003eb388d1f886268ad5f08ccb',
+       i686: '85771e05e3a7f9ee81056cd62930234f5db354e68146a8fc2b48a96113501bc1',
+     x86_64: '4f71ae6cfb24281a41bfca1a5ba866e9b04a244bebf04f9c1e9ba5788f105c11'
   })
 
-  depends_on 'py3_jinja2'
-  depends_on 'py3_sphinxcontrib_devhelp'
-  depends_on 'py3_sphinxcontrib_applehelp'
-  depends_on 'py3_sphinxcontrib_jsmath'
-  depends_on 'py3_sphinxcontrib_serializinghtml'
-  depends_on 'py3_sphinxcontrib_qthelp'
-  depends_on 'py3_sphinxcontrib_htmlhelp'
-  depends_on 'py3_docutils'
-  depends_on 'py3_imagesize'
-  depends_on 'py3_requests'
-  depends_on 'py3_babel'
-  depends_on 'py3_alabaster'
-  depends_on 'py3_pygments'
-  depends_on 'py3_packaging'
-  depends_on 'py3_snowballstemmer'
+  depends_on 'py3_jinja2' # R
+  depends_on 'py3_roman_numerals' # R
+  depends_on 'py3_sphinxcontrib_devhelp' # R
+  depends_on 'py3_sphinxcontrib_applehelp' # R
+  depends_on 'py3_sphinxcontrib_jsmath' # R
+  depends_on 'py3_sphinxcontrib_serializinghtml' # R
+  depends_on 'py3_sphinxcontrib_qthelp' # R
+  depends_on 'py3_sphinxcontrib_htmlhelp' # R
+  depends_on 'py3_docutils' # R
+  depends_on 'py3_imagesize' # R
+  depends_on 'py3_requests' # R
+  depends_on 'py3_babel' # R
+  depends_on 'py3_alabaster' # R
+  depends_on 'py3_pygments' # R
+  depends_on 'py3_packaging' # R
+  depends_on 'py3_snowballstemmer' # R
   depends_on 'python3' => :build
 
   no_source_build
