f508227ad325f9925a2d32c6e82d9076727774ed
Ed Reel <edreel@gmail.com>
2026-01-06 06:51:12 +0000

    Btop 1.4.5 => 1.4.6

--- packages/btop.rb
+++ packages/btop.rb
@@ -3,20 +3,20 @@ require 'package'
 class Btop < Package
   description 'Resource monitor that shows usage and stats for processor, memory, disks, network and processes.'
   homepage 'https://github.com/aristocratos/btop'
-  version '1.4.5'
+  version '1.4.6'
   license 'Apache-2.0'
   compatibility 'all'
   source_url({
-    aarch64: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-armv7l-linux-musleabihf.tbz",
-     armv7l: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-armv7l-linux-musleabihf.tbz",
-       i686: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-i686-linux-musl.tbz",
-     x86_64: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-x86_64-linux-musl.tbz"
+    aarch64: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-armv7-unknown-linux-musleabi.tbz",
+     armv7l: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-armv7-unknown-linux-musleabi.tbz",
+       i686: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-i686-unknown-linux-musl.tbz",
+     x86_64: "https://github.com/aristocratos/btop/releases/download/v#{version}/btop-x86_64-unknown-linux-musl.tbz"
   })
   source_sha256({
-    aarch64: '2e34a699a940cd1da70e1ee50f387e4ba899483c021571cbbf54d1a52dbcfe18',
-     armv7l: '2e34a699a940cd1da70e1ee50f387e4ba899483c021571cbbf54d1a52dbcfe18',
-       i686: '0ea7f6e0f1d6fcbf9981455e2744fb0365f4b0872bbc4cf3e3596e5f32ee9ff4',
-     x86_64: '206b0f9334e93c06de9025eaf90676c374ca79815b41dadff1b36ef4e4e6d1d4'
+    aarch64: 'f37622a833ab5c51881e17cefe4f27c035ec59faa55fde468f50ad0eca367e02',
+     armv7l: 'f37622a833ab5c51881e17cefe4f27c035ec59faa55fde468f50ad0eca367e02',
+       i686: '33499176966c0c955f3452d6d316196dc5b2568b881d94592cc082a88937274b',
+     x86_64: '9bf7ed170423bb3709a8020767bd2ca529c5632af478f229dce7cdfd32b25691'
   })
 
   no_compile_needed
