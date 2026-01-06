96a59b1317e7b9c1431a79a3211eaae0e5d79940
Ed Reel <edreel@gmail.com>
2026-01-06 19:14:22 +0000

    Ansible 13.1.0-py3.14 => 13.2.0-py3.14

--- packages/ansible.rb
+++ packages/ansible.rb
@@ -3,17 +3,17 @@ require 'buildsystems/pip'
 class Ansible < Pip
   description 'Ansible is a radically simple IT automation engine that automates cloud provisioning, configuration management, application deployment, intra-service orchestration, and many other IT needs.'
   homepage 'https://www.ansible.com/'
-  version "13.1.0-#{CREW_PY_VER}"
+  version "13.2.0-#{CREW_PY_VER}"
   license 'GPL-3'
   compatibility 'all'
   source_url 'SKIP'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e26ef8be53718de60c1dca6104a2f636467c50a16ec2a59a3a99618ffa12272e',
-     armv7l: 'e26ef8be53718de60c1dca6104a2f636467c50a16ec2a59a3a99618ffa12272e',
-       i686: 'dbd72736acb3866e6f9f5e5054843e749c7475502781f9b5e446f33a216fa0d5',
-     x86_64: 'f86c047074b0824cc6481d6da2c7311ac91202707f989abdd3a85d8b69b76fc4'
+    aarch64: '98a268e8386eb79796fb9afbf614351fd32a97b194b1d836b813b6248984782b',
+     armv7l: '98a268e8386eb79796fb9afbf614351fd32a97b194b1d836b813b6248984782b',
+       i686: 'd11c9e150c72e677fcecd88617d23ab0cb6df406943ecd57408e64ba5246594f',
+     x86_64: '05b6e6f998837c38c6bfb4f3f2a7e22b36c1b033e5514b08b1fedbfb4b4b1314'
   })
 
   depends_on 'xdg_base'
