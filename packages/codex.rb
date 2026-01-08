b2f2ef433cb329df64bdbc6517c96edc9fdbe1f8
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-01-08 00:26:16 +0000

    Add unbuilt codex to updater-codex-0.79.0

--- packages/codex.rb
+++ packages/codex.rb
@@ -3,12 +3,12 @@ require 'package'
 class Codex < Package
   description 'Lightweight coding agent that runs in your terminal'
   homepage 'https://github.com/openai/codex'
-  version '0.78.0'
+  version '0.79.0'
   license 'Apache-2.0'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-x86_64-unknown-linux-gnu.zst"
-  source_sha256 'fdb96793ffc98dcb445b66196da12da8e503030ad9de0ba98e42145f5305a1fb'
+  source_sha256 '8ab37ba3020586c3ee0972bbf06412c996824f10ef4e6c57f4aedffc35ab42b9'
 
   depends_on 'zstd'
 
