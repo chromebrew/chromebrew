c45950122dff49c6a1fe7431d463c38dc5629ab5
Ed Reel <edreel@gmail.com>
2026-01-11 19:16:46 -0600

    Gedit 48.2 => 49.0, Libgedit_gtksourcevew 299.5.0 => 299.6.0, Tepl_6 6.13.0-icu77.1 => 6.14.0-icu77.1 (#14131)

--- packages/libgedit_gtksourceview.rb
+++ packages/libgedit_gtksourceview.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Libgedit_gtksourceview < Meson
   description 'A library that extends GtkTextView, the standard GTK '
   homepage 'https://gedit-technology.github.io'
-  version '299.5.0'
+  version '299.6.0'
   license 'LGPL2.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/World/gedit/libgedit-gtksourceview.git'
@@ -14,9 +14,9 @@ class Libgedit_gtksourceview < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4b6bbcebe2578342907bd5116110e4fccd46ff000b16d6883da636c75f7f2cc7',
-     armv7l: '4b6bbcebe2578342907bd5116110e4fccd46ff000b16d6883da636c75f7f2cc7',
-     x86_64: '1fd2f299c0a789895b5671daad278b76817a3b1672881d0bebd20fc581fa97c2'
+    aarch64: 'bf33b299ce17545a74dac05054979e2fece05c13cb9cf0a1bac83bc728ab3124',
+     armv7l: 'bf33b299ce17545a74dac05054979e2fece05c13cb9cf0a1bac83bc728ab3124',
+     x86_64: 'c61d8bb63dc8ced4d3b10b863c750bc5c1f411b36ee858d5eabfa1b1a7cfe47a'
   })
 
   depends_on 'cairo' # R
