1c0bc1a73842e3eebbabf731facec34f64a46888
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-01-15 00:26:23 +0000

    Add unbuilt libpng to updater-libpng-1.6.54

--- packages/libpng.rb
+++ packages/libpng.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Libpng < CMake
   description 'libpng is the official PNG reference library.'
   homepage 'https://www.libpng.org/pub/png/libpng.html'
-  version '1.6.53'
+  version '1.6.54'
   license 'libpng2'
   compatibility 'all'
   source_url 'https://github.com/pnggroup/libpng.git'
