582e7c70c128683302507f4cb55712ef3705cf70
Ed Reel <edreel@gmail.com>
2026-01-17 09:32:06 +0000

    Gnome_maps 49.2 => 49.3, Libportal 0.8.1 => 0.9.1

--- packages/gnome_maps.rb
+++ packages/gnome_maps.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Gnome_maps < Meson
   description 'A simple GNOME maps application'
   homepage 'https://wiki.gnome.org/Apps/Maps'
-  version '49.2'
+  version '49.3'
   license 'GPL-2+, LGPL-2+, MIT, CC-BY-3.0 and CC-BY-SA-3.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gnome-maps.git'
@@ -11,17 +11,17 @@ class Gnome_maps < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aa8e60521f23ca1a3263114605fcf6dcc381caf6eaf086a223a03781cef64251',
-     armv7l: 'aa8e60521f23ca1a3263114605fcf6dcc381caf6eaf086a223a03781cef64251',
-     x86_64: 'c05459c1373316f079482fbb475ee9a1587dca8456ff897805aedd5ea0dc443c'
+    aarch64: '757812f0671443c6fb4eba9aa727d5db6c8a54d5614771f644c82582877c1c04',
+     armv7l: '757812f0671443c6fb4eba9aa727d5db6c8a54d5614771f644c82582877c1c04',
+     x86_64: '5aca499e006b3831ec8acf82ed669de79f65f982ffd7fbff9ddc2c35370dbd9f'
   })
 
   depends_on 'cairo' # R
   depends_on 'desktop_file_utils' => :build
   depends_on 'folks' => :build
-  depends_on 'geoclue' => :build
-  depends_on 'geocode_glib2' => :build
-  depends_on 'gjs' => :build
+  depends_on 'geoclue' # R
+  depends_on 'geocode_glib2' # R
+  depends_on 'gjs' # R
   depends_on 'glib' # R
   depends_on 'glibc' # R
   depends_on 'gnome_weather' => :build
@@ -29,16 +29,18 @@ class Gnome_maps < Meson
   depends_on 'gtk4' # R
   depends_on 'harfbuzz' # R
   depends_on 'json_glib' # R
-  depends_on 'libadwaita' => :build
+  depends_on 'libadwaita' # R
   depends_on 'libchamplain' => :build
   depends_on 'libgee' => :build
-  depends_on 'libgweather' => :build
+  depends_on 'libgweather' # R
   depends_on 'libhandy' => :build
-  depends_on 'libportal' => :build
+  depends_on 'libportal' # R
   depends_on 'librsvg' # R
+  depends_on 'libsecret' # R
   depends_on 'libshumate' # R
   depends_on 'libxml2' # R
   depends_on 'pango' # R
+  depends_on 'rest' # R
   depends_on 'vulkan_headers' => :build
   depends_on 'vulkan_icd_loader'
   depends_on 'yelp_tools' => :build
