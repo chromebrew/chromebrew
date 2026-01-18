4b32604d902b3a45727043fa5cab84ad54402f81
Ed Reel <edreel@gmail.com>
2026-01-18 03:36:17 +0000

    Add package test, update binaries and filelists and include in automatically_updatable_packages.txt

--- packages/trzsz.rb
+++ packages/trzsz.rb
@@ -11,10 +11,10 @@ class Trzsz < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ee0c1d36bd136bee8fc4acff260191acd5d8bce505e3e592f050d9a718d530d5',
-     armv7l: 'ee0c1d36bd136bee8fc4acff260191acd5d8bce505e3e592f050d9a718d530d5',
-       i686: '8ad1cbd074aa7ff0dfe9447959f3320ba78c7f455334e9685f85e3847fd1097f',
-     x86_64: '35f74a16ce3ad9532f7cdd8a5ae27e7f92ce6c46b252c7b59c7eab6d83148e13'
+    aarch64: '7fa43763fff3f603d9551ac570bd7fdbb7fa65782af26679027eaec4f8e0b06b',
+     armv7l: '7fa43763fff3f603d9551ac570bd7fdbb7fa65782af26679027eaec4f8e0b06b',
+       i686: '60d6e27aeaffc2e1827f81d8a41c735157ef95ce371181fce0cfe1b4bb61fd08',
+     x86_64: 'f5fd48dde0c04fcdb669be6a44e47dbd86566e57b1afc34797aa3daa0da29897'
   })
 
   depends_on 'glibc' # R
