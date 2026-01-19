ecce89badd88f3cd42af218d51c3c6dfa2080c43
Ed Reel <edreel@gmail.com>
2026-01-19 03:29:52 +0000

    Protobuf 33.1 => 33.4

--- packages/protobuf.rb
+++ packages/protobuf.rb
@@ -6,7 +6,7 @@ require 'buildsystems/cmake'
 class Protobuf < CMake
   description 'Protocol Buffers - Googles data interchange format'
   homepage 'https://protobuf.dev'
-  version '33.1'
+  version '33.4'
   license 'BSD'
   compatibility 'all'
   source_url 'https://github.com/protocolbuffers/protobuf.git'
