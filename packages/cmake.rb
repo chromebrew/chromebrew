599e3a7356f6d0760d95d65143dc35bf08ea120a
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-01-21 00:27:53 +0000

    Add unbuilt cmake to updater-cmake-4.2.2

--- packages/cmake.rb
+++ packages/cmake.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Cmake < CMake
   description 'CMake is an open-source, cross-platform family of tools designed to build, test and package software.'
   homepage 'https://cmake.org/'
-  version '4.2.1'
+  version '4.2.2'
   license 'CMake'
   compatibility 'all'
   source_url 'https://gitlab.kitware.com/cmake/cmake.git'
