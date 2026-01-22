212126eaefc0425662db98b1cc939eb780f63e03
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-01-22 00:27:43 +0000

    Add unbuilt py3_sqlalchemy to updater-py3_sqlalchemy-2.0.46-py3.14

--- packages/py3_sqlalchemy.rb
+++ packages/py3_sqlalchemy.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_sqlalchemy < Pip
   description 'SQLalchemy is a database toolkit for Python.'
   homepage 'https://sqlalchemy.org'
-  version "2.0.45-#{CREW_PY_VER}"
+  version "2.0.46-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
