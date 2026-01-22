44a6751506f54310d7793775d9ed0813d4ee5271
Ed Reel <edreel@gmail.com>
2026-01-22 05:40:49 -0600

    Fakeroot 1.37.1.2 => 1.37.2 (#14268)

--- packages/fakeroot.rb
+++ packages/fakeroot.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Fakeroot < Autotools
   description 'Run a command in an environment faking root privileges for file manipulation.'
   homepage 'https://wiki.debian.org/FakeRoot'
-  version '1.37.1.2'
+  version '1.37.2'
   license 'GPL-3'
   compatibility 'all'
   source_url "https://deb.debian.org/debian/pool/main/f/fakeroot/fakeroot_#{version}.orig.tar.gz"
-  source_sha256 '959496928c8a676ec8377f665ff6a19a707bfad693325f9cc4a4126642f53224'
+  source_sha256 '0eea60fbe89771b88fcf415c8f2f0a6ccfe9edebbcf3ba5dc0212718d98884db'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '51a0e401806e8102e06eef577a7edf927c13df93f0823f45c2c15fa28610d7e2',
-     armv7l: '51a0e401806e8102e06eef577a7edf927c13df93f0823f45c2c15fa28610d7e2',
-       i686: '37cd31b4ddebe9bf6a4928e9d1dd60fef33a6f29407d331d9257b265b31d7f14',
-     x86_64: 'bf8e53c22d7df9c5c08b971220b6d627b3255b69b73226e88363edca8041581a'
+    aarch64: '29312d3f89552a9876c215b7c0dfb2d9b4e34639c5ce3a0d0bd6c687bf0f9868',
+     armv7l: '29312d3f89552a9876c215b7c0dfb2d9b4e34639c5ce3a0d0bd6c687bf0f9868',
+       i686: '5f31abf62547f183d5db5f5c138cf24ba8ab907dcf6145b2e0a6fd406d288e80',
+     x86_64: 'fa89066a514460c8bf00e77b02f7bf1a07e7152be920ede97fce767916c86c65'
   })
 
   depends_on 'glibc' # R
