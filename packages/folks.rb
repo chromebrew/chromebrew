cf343ae0e793afad987ea44cf58b140e99e9dfea
Ed Reel <edreel@gmail.com>
2026-01-22 14:26:14 -0600

    Folks 0.15.9 => 0.15.12 (#14271)

--- packages/folks.rb
+++ packages/folks.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Folks < Meson
   description 'Library to aggregates people into metacontacts'
   homepage 'https://wiki.gnome.org/Projects/Folks'
-  version '0.15.9'
+  version '0.15.12'
   license 'LGPL-2.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/folks.git'
@@ -11,13 +11,13 @@ class Folks < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '491428b6cb2de8c119ab455aded923b3edc697ec7f894be547eba5d9e7101384',
-     armv7l: '491428b6cb2de8c119ab455aded923b3edc697ec7f894be547eba5d9e7101384',
-     x86_64: '5acbdbbf7fe754124a526bcb1ae856af49b7ba907346e7fcc2cbec06c85e6464'
+    aarch64: 'b31d41f4b77abafa041aae54486f730c3ab25e104c5982c480bf024926d3831a',
+     armv7l: 'b31d41f4b77abafa041aae54486f730c3ab25e104c5982c480bf024926d3831a',
+     x86_64: 'f4bf40195fafb9f12bde0ace0d04f4abdda18798319872b078f1c1745777fef9'
   })
 
-  depends_on 'glibc' # R
   depends_on 'glib' # R
+  depends_on 'glibc' # R
   depends_on 'gobject_introspection' => :build
   depends_on 'gtk_doc' => :build
   depends_on 'libgee' # R
