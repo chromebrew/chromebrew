7d273a0f16f997a68fb7ac1e6547e5b5ff508640
Ed Reel <edreel@gmail.com>
2026-01-23 19:29:56 -0600

    Github_cli 2.83.2 => 2.86.0, Glab 1.80.4 => 1.81.0, Go 1.25.5 => 1.25.6, Gradle 9.2.1 => 9.3.0 (#14292)

--- packages/glab.rb
+++ packages/glab.rb
@@ -3,7 +3,7 @@ require 'package'
 class Glab < Package
   description 'A GitLab CLI tool bringing GitLab to your command line'
   homepage 'https://gitlab.com/gitlab-org/cli'
-  version '1.80.4'
+  version '1.81.0'
   license 'MIT'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Glab < Package
      x86_64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '1e9477b10295ea375e9cc460d7caa81991782125f06769798cb63ddaa130fdaa',
-     armv7l: '1e9477b10295ea375e9cc460d7caa81991782125f06769798cb63ddaa130fdaa',
-       i686: '7b24ebf2cc25981b1dba18d7349de8f7aa892ea66fa2be1e35dde4c9ce4d8e28',
-     x86_64: '1675ca17511d67042f3ec20a383b9395da8e1b68d18d4f7eb038043c19ce0a91'
+    aarch64: '8973608965994fa4c3a6907cb8c2e9917443c9d8c4fc6ee373b8b3bf43e60b6d',
+     armv7l: '8973608965994fa4c3a6907cb8c2e9917443c9d8c4fc6ee373b8b3bf43e60b6d',
+       i686: '75809e4602d5c474787fd5d56d23546c1d3ea5fe752d886b10b621fe6828fb0e',
+     x86_64: 'df01aa3b8e10be2c8daa6e7d514c29596115b32bfc26a981f1ad894b5ff6f340'
   })
 
   no_compile_needed
