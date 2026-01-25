60c4e11d012c01484d484eabc29e990856693484
Ed Reel <edreel@gmail.com>
2026-01-25 17:58:30 -0600

    I3 4.24 => 4.25 (#14302)

--- packages/i3.rb
+++ packages/i3.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class I3 < Meson
   description 'Improved tiling window manager'
   homepage 'https://i3wm.org/'
-  version '4.24'
+  version '4.25'
   license 'BSD'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/i3/i3.git'
@@ -11,29 +11,29 @@ class I3 < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd0c0849c2b4b1402fe72e781c963af5aec1d84ac413f8a5616fc38becd1db3d9',
-     armv7l: 'd0c0849c2b4b1402fe72e781c963af5aec1d84ac413f8a5616fc38becd1db3d9',
-     x86_64: 'a07c0b7f085c716c60f6044a5e2904bb3d77eb392051a808348d408dcd535555'
+    aarch64: 'c80e9228de17400f0867ffdc06320a575961036da921b73c02d55792a3255a2a',
+     armv7l: 'c80e9228de17400f0867ffdc06320a575961036da921b73c02d55792a3255a2a',
+     x86_64: '1bd6abf4f8444c6be265251ca350e20fbc172300e1a36dc3f06d7c1727c8d751'
   })
 
   depends_on 'cairo' # R
   depends_on 'glib' # R
   depends_on 'glibc' # R
   depends_on 'harfbuzz' # R
-  depends_on 'libev'
+  depends_on 'libev' # R
   depends_on 'libxcb' # R
   depends_on 'libxkbcommon' # R
-  depends_on 'pango'
+  depends_on 'pango' # R
   depends_on 'pcre2' # R
-  depends_on 'sommelier'
-  depends_on 'startup_notification'
+  depends_on 'sommelier' # R
+  depends_on 'startup_notification' # R
   depends_on 'wayland' => :build
   depends_on 'xcb_util' # R
-  depends_on 'xcb_util_cursor'
-  depends_on 'xcb_util_keysyms'
-  depends_on 'xcb_util_wm'
-  depends_on 'xcb_util_xrm'
-  depends_on 'yajl'
+  depends_on 'xcb_util_cursor' # R
+  depends_on 'xcb_util_keysyms' # R
+  depends_on 'xcb_util_wm' # R
+  depends_on 'xcb_util_xrm' # R
+  depends_on 'yajl' # R
 
   meson_install_extras do
     File.write "#{CREW_DEST_PREFIX}/bin/starti3", <<~EOF, perm: 0o755
