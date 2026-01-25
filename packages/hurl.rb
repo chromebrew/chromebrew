b02e9ed48d4ec2dc5fd9fb47b2382d65ce894c6c
Ed Reel <edreel@gmail.com>
2026-01-25 16:42:19 -0600

    Hurl 7.0.0 => 7.1.0 (#14300)

--- packages/hurl.rb
+++ packages/hurl.rb
@@ -3,7 +3,7 @@ require 'buildsystems/rust'
 class Hurl < RUST
   description 'Hurl is a command line tool that runs HTTP requests defined in a simple plain text format.'
   homepage 'https://hurl.dev/'
-  version '7.0.0'
+  version '7.1.0'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url 'https://github.com/Orange-OpenSource/hurl.git'
@@ -11,11 +11,10 @@ class Hurl < RUST
   binary_compression 'tar.zst'
 
   binary_sha256({
-     x86_64: 'bae3d7291c2046ee47a49ba34061188fbba9b397e9f6f731bcb973ed8c30d5c2'
+     x86_64: '5ff7edcca23cd4acc96681e2f6943b9e82326a5905cdac6ff40a9c1b6a3c17cb'
   })
 
   depends_on 'curl' # R
-  depends_on 'curl' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
   depends_on 'libxml2' # R
