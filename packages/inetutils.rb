5c98b7f3d2e826d80f8ae736494b7b2cb58f82bc
Ed Reel <edreel@gmail.com>
2026-01-26 15:58:20 -0600

    Inetutils 2.6 => 2.7 (#14323)

--- packages/inetutils.rb
+++ packages/inetutils.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Inetutils < Autotools
   description 'The Inetutils package contains programs for basic networking. Such as dnsdomainname, ftp, hostname, ifconfig, ping, ping6, talk, telnet, tftp, traceroute'
   homepage 'https://www.gnu.org/software/inetutils/'
-  version '2.6'
+  version '2.7'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://git.savannah.gnu.org/git/inetutils.git'
@@ -11,10 +11,10 @@ class Inetutils < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c326018755ce8ecc004d4edc2a9fc7045b68b32934208e8ab6fbfa6298fe0672',
-     armv7l: 'c326018755ce8ecc004d4edc2a9fc7045b68b32934208e8ab6fbfa6298fe0672',
-       i686: '383ee756feeebac0c6d0cbb58b2d1023c09708da76e6e7e9045f0aef8bbbd1d7',
-     x86_64: '73cb5f01b5ff1cb5d7035f47a74acef04bec7a56c6ea899ae87ac016d7ce35a5'
+    aarch64: 'f777bafbe05d22eb95a209022240480d8e0035a1c679a3ae7f69b8c3fca11c51',
+     armv7l: 'f777bafbe05d22eb95a209022240480d8e0035a1c679a3ae7f69b8c3fca11c51',
+       i686: '8132b26c8d8bf555fe5b101760c5e9e5574dc1a397cd0d9398b205c9d9dda629',
+     x86_64: '515af1f9004aaf426c1f5d280144c75537174aa221c3e4e9cddd81bdc2e42f18'
   })
 
   depends_on 'e2fsprogs' # R
@@ -23,6 +23,8 @@ class Inetutils < Autotools
   depends_on 'libxcrypt' # R
   depends_on 'ncurses' # R
   depends_on 'readline' # R
+
+  conflicts_ok # Some executables such as hostname are also found in uutils_coreutils.
 
   autotools_pre_configure_options "CFLAGS='-Wno-incompatible-pointer-types -Wno-implicit-function-declaration -Wno-unused-result -fno-strict-aliasing' CC='gcc -lncursesw -lpthread -ltinfow' CXX='g++ -lncursesw -lpthread -ltinfow'"
   autotools_configure_options "--with-ncurses-include-dir=#{CREW_PREFIX}/include/ncursesw \
@@ -41,35 +43,39 @@ class Inetutils < Autotools
     system './bootstrap.sh'
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-    # Handle conflict with the uutils_coreutils hostname.
-    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/hostname", "#{CREW_DEST_PREFIX}/bin/inetutils_hostname"
-    FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man1/hostname.1", "#{CREW_DEST_MAN_PREFIX}/man1/inetutils_hostname.1"
+  # def self.install
+  # system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  # Handle conflict with the uutils_coreutils hostname.
+  # FileUtils.mv "#{CREW_DEST_PREFIX}/bin/hostname", "#{CREW_DEST_PREFIX}/bin/inetutils_hostname"
+  # FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man1/hostname.1", "#{CREW_DEST_MAN_PREFIX}/man1/inetutils_hostname.1"
 
-    FileUtils.install "#{CREW_DEST_PREFIX}/bin/ping", "#{CREW_DEST_PREFIX}/bin/ping.elf", mode: 0o755
-    FileUtils.install "#{CREW_DEST_PREFIX}/bin/ping6", "#{CREW_DEST_PREFIX}/bin/ping6.elf", mode: 0o755
-    FileUtils.install "#{CREW_DEST_PREFIX}/bin/traceroute", "#{CREW_DEST_PREFIX}/bin/traceroute.elf", mode: 0o755
-    @PING_SH = <<~PING_HEREDOC
-      #!/bin/bash
-      sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
-          --keep=1 --user=nobody --addamb=cap_net_raw -- \\
-          -c "#{CREW_PREFIX}/bin/ping.elf $@"
-    PING_HEREDOC
-    File.write("#{CREW_DEST_PREFIX}/bin/ping", @PING_SH, perm: 0o755)
-    @PING6_SH = <<~PING6_HEREDOC
-      #!/bin/bash
-      sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
-          --keep=1 --user=nobody --addamb=cap_net_raw -- \\
-          -c "#{CREW_PREFIX}/bin/ping6.elf $@"
-    PING6_HEREDOC
-    File.write("#{CREW_DEST_PREFIX}/bin/ping6", @PING6_SH, perm: 0o755)
-    @TRACEROUTE_SH = <<~TRACEROUTE_HEREDOC
-      #!/bin/bash
-      sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
-          --keep=1 --user=nobody --addamb=cap_net_raw -- \\
-          -c "#{CREW_PREFIX}/bin/traceroute.elf $@"
-    TRACEROUTE_HEREDOC
-    File.write("#{CREW_DEST_PREFIX}/bin/traceroute", @TRACEROUTE_SH, perm: 0o755)
+  # FileUtils.install "#{CREW_DEST_PREFIX}/bin/ping", "#{CREW_DEST_PREFIX}/bin/ping.elf", mode: 0o755
+  # FileUtils.install "#{CREW_DEST_PREFIX}/bin/ping6", "#{CREW_DEST_PREFIX}/bin/ping6.elf", mode: 0o755
+  # FileUtils.install "#{CREW_DEST_PREFIX}/bin/traceroute", "#{CREW_DEST_PREFIX}/bin/traceroute.elf", mode: 0o755
+  # @PING_SH = <<~PING_HEREDOC
+  #  #!/bin/bash
+  #  sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
+  #      --keep=1 --user=nobody --addamb=cap_net_raw -- \\
+  #      -c "#{CREW_PREFIX}/bin/ping.elf $@"
+  # PING_HEREDOC
+  # File.write("#{CREW_DEST_PREFIX}/bin/ping", @PING_SH, perm: 0o755)
+  # @PING6_SH = <<~PING6_HEREDOC
+  # #!/bin/bash
+  #  sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
+  #       --keep=1 --user=nobody --addamb=cap_net_raw -- \\
+  #       -c "#{CREW_PREFIX}/bin/ping6.elf $@"
+  # PING6_HEREDOC
+  # File.write("#{CREW_DEST_PREFIX}/bin/ping6", @PING6_SH, perm: 0o755)
+  # @TRACEROUTE_SH = <<~TRACEROUTE_HEREDOC
+  #  #!/bin/bash
+  #  sudo -E #{CREW_PREFIX}/sbin/capsh --caps='cap_net_raw+eip cap_setpcap,cap_setuid,cap_setgid+ep' \\
+  #       --keep=1 --user=nobody --addamb=cap_net_raw -- \\
+  #       -c "#{CREW_PREFIX}/bin/traceroute.elf $@"
+  # TRACEROUTE_HEREDOC
+  # File.write("#{CREW_DEST_PREFIX}/bin/traceroute", @TRACEROUTE_SH, perm: 0o755)
+  # end
+
+  def self.postinstall
+    ExitMessage.add "\nMake sure to precede commands with 'sudo' to execute.\nFor example, 'sudo ping <domain>'\n"
   end
 end
