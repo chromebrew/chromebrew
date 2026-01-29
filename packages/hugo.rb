ae5e0a066d9bd2c8877bd6c53e4d298559d3def9
satmandu <satmandu@users.noreply.github.com>
2026-01-29 13:12:50 +0000

    Add unbuilt hugo to updater-hugo-0.155.0

--- packages/hugo.rb
+++ packages/hugo.rb
@@ -3,7 +3,7 @@ require 'package'
 class Hugo < Package
   description 'Hugo is one of the most popular open-source static site generators.'
   homepage 'https://gohugo.io'
-  version %w[aarch64 armv7l x86_64].include?(ARCH) ? '0.154.5' : '0.101.0'
+  version %w[aarch64 armv7l x86_64].include?(ARCH) ? '0.155.0' : '0.101.0'
   license 'Apache-2.0, Unlicense, BSD, BSD-2 and MPL-2.0'
   compatibility 'all'
   min_glibc '2.29' if ARCH.eql?('x86_64')
@@ -14,10 +14,10 @@ class Hugo < Package
      x86_64: "https://github.com/gohugoio/hugo/releases/download/v#{version}/hugo_extended_#{version}_linux-amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '9ef096e3ae139421b9d9f4ebec4b6023d796f35bf5360770fa88822933a7fc5d',
-     armv7l: '9ef096e3ae139421b9d9f4ebec4b6023d796f35bf5360770fa88822933a7fc5d',
+    aarch64: '5268d8c89c558201eec825f7ac852a507ef06507499de956f717812f10eb5ace',
+     armv7l: '5268d8c89c558201eec825f7ac852a507ef06507499de956f717812f10eb5ace',
        i686: '9ae794edd86415a611cae15fc72382ee6f2b729754e15319c144057a5457eaed',
-     x86_64: '372d2f0538b24d2bb9285f0583c1e3b02d023ec2c2b8eb90e5c3bbfb3139fb13'
+     x86_64: '6c8b34a98afe207982137d69abb98c6efb2829e0cfc099e23ab4c51f260e8c6a'
   })
 
   no_compile_needed
