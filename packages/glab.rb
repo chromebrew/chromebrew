a5bd615bad69e7d65c25fa3c201e38fdaa8d6ef7
satmandu <satmandu@users.noreply.github.com>
2026-01-29 13:12:31 +0000

    Add unbuilt glab to updater-glab-1.82.0

--- packages/glab.rb
+++ packages/glab.rb
@@ -3,7 +3,7 @@ require 'package'
 class Glab < Package
   description 'A GitLab CLI tool bringing GitLab to your command line'
   homepage 'https://gitlab.com/gitlab-org/cli'
-  version '1.81.0'
+  version '1.82.0'
   license 'MIT'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Glab < Package
      x86_64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '8973608965994fa4c3a6907cb8c2e9917443c9d8c4fc6ee373b8b3bf43e60b6d',
-     armv7l: '8973608965994fa4c3a6907cb8c2e9917443c9d8c4fc6ee373b8b3bf43e60b6d',
-       i686: '75809e4602d5c474787fd5d56d23546c1d3ea5fe752d886b10b621fe6828fb0e',
-     x86_64: 'df01aa3b8e10be2c8daa6e7d514c29596115b32bfc26a981f1ad894b5ff6f340'
+    aarch64: '77910ec0534c2240ce5da9c3e93e2d922cc0a1e77a58af764079233e0c8da8c1',
+     armv7l: '77910ec0534c2240ce5da9c3e93e2d922cc0a1e77a58af764079233e0c8da8c1',
+       i686: '5d9152a18598343e9d69f67acddfb1a06637c74a8c377d0be4e4a0cc130fbe93',
+     x86_64: '6589bd77e3042b728f0918090eb95ae63f8e9993ddd5db64bdb2a04e2c38ade6'
   })
 
   no_compile_needed
