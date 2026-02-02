e375381dcdaa080d688736323fd2a72e28319c73
Ed Reel <edreel@gmail.com>
2026-02-02 00:31:02 -0600

    Krb5 1.22.1 => 1.22.2 (#14444)

--- packages/krb5.rb
+++ packages/krb5.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Krb5 < Autotools
   description 'Kerberos is a network authentication protocol.'
   homepage 'https://web.mit.edu/kerberos/'
-  version '1.22.1'
+  version '1.22.2'
   license 'openafs-krb5-a, BSD, MIT, OPENLDAP, BSD-2, HPND, BSD-4, ISC, RSA, CC-BY-SA-3.0 and BSD-2 or GPL-2+ )'
   compatibility 'all'
-  source_url "https://web.mit.edu/kerberos/dist/krb5/#{version.split('.')[0..1].join('.')}/krb5-#{version}.tar.gz"
-  source_sha256 '9560941a9d843c0243a71b17a7ac6fe31c7cebb5bce3983db79e52ae7e850491'
+  source_url "https://web.mit.edu/kerberos/dist/krb5/#{version.sub(/\.\d+$/, '')}/krb5-#{version}.tar.gz"
+  source_sha256 '074de0ecf72199f091c88812d46090c27724fd36a1284b5f44bc35b8dd31a59e'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '62318d53977497f7bf6c6038f5d17603685af369b8ba35ae1fa705e9f084bc59',
-     armv7l: '62318d53977497f7bf6c6038f5d17603685af369b8ba35ae1fa705e9f084bc59',
-       i686: 'd08304b052307c1ad9d484f405ab7452e8bf9250fd9a62473e6e4fec1fe4db05',
-     x86_64: '6f0019b8f7c1d58dcdd4ddaf6dc74f3d59c52f43fa7ffabe0da05dccfdb36afd'
+    aarch64: 'd07b691662223916e3c030af5db17e7931e15a06ed5c6275bf0ccfafb9865261',
+     armv7l: 'd07b691662223916e3c030af5db17e7931e15a06ed5c6275bf0ccfafb9865261',
+       i686: '9ebda0a19b9270d9faedea58287c69c83e720b1ab28f25659259bf12cdf9c770',
+     x86_64: '2e15c20afb9dbe75a27ac94a4c8a3b01ec093ae22145a8a5b9604e535b4737be'
   })
 
   depends_on 'ccache' => :build
