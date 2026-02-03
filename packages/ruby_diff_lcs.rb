13f626c9aa6ef7459b2c932fe6365fe0a7e1f967
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-03 00:31:19 +0000

    ruby_diff_lcs -> 2.0.0-ruby4.0 in updater-ruby_diff_lcs-2.0.0-ruby4.0

--- packages/ruby_diff_lcs.rb
+++ packages/ruby_diff_lcs.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_diff_lcs < RUBY
   description 'Diff::LCS computes the difference between two Enumerable sequences using the McIlroy-Hunt longest common subsequence (LCS) algorithm.'
   homepage 'https://github.com/halostatue/diff-lcs'
-  version "1.6.2-#{CREW_RUBY_VER}"
+  version "2.0.0-#{CREW_RUBY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
