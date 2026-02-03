3af566b946e0af7aa41278421141ce7495bcf1eb
Ed Reel <edreel@gmail.com>
2026-02-03 15:59:28 -0600

    Mediainfo 25.07 => 25.10, Libmediainfo 25.07 => 25.10 (#14482)

--- packages/mediainfo.rb
+++ packages/mediainfo.rb
@@ -3,18 +3,18 @@ require 'package'
 class Mediainfo < Package
   description 'MediaInfo is a convenient unified display of the most relevant technical and tag data for video and audio files.'
   homepage 'https://mediaarea.net/en/MediaInfo'
-  version '25.07'
+  version '25.10'
   license 'BSD-2'
   compatibility 'all'
   source_url "https://mediaarea.net/download/binary/mediainfo/#{version}/MediaInfo_CLI_#{version}_GNU_FromSource.tar.xz"
-  source_sha256 'd810a56b84a8f6c009958312459e1d7353b3722db8f18c2bc87da512ebd51482'
+  source_sha256 '9a7743562053d619dcfd0cdafe71eadd6c36b134649d378a4c5c8d074ace42b5'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'daaea81c433718d4760a8ea6b0dcd6745b380addd374ef2ddd4ec4257ea1df98',
-     armv7l: 'daaea81c433718d4760a8ea6b0dcd6745b380addd374ef2ddd4ec4257ea1df98',
-       i686: '99647e32da8e872ba14b00fda6caf1cc1db7aa918f83d3cd4bf56b3d66925349',
-     x86_64: '9e07a9c7cc8ca950cb01198c6c049c1db1c5d9c66634bc07270618f09daef4f0'
+    aarch64: 'aa34ae3825326db3e645c9ac3ce99c0a98e7f3d5ecf89ba12c9a100d24aaccc7',
+     armv7l: 'aa34ae3825326db3e645c9ac3ce99c0a98e7f3d5ecf89ba12c9a100d24aaccc7',
+       i686: '6755edc5c6ed4790c3a11dfa73e03dc62c970f5972a06547c5aaf34879392226',
+     x86_64: '532495e2b5d746eea8cd59ecd9125f58c5eea13776546fda1ced704708d5b9b0'
   })
 
   depends_on 'gcc_lib' # R
