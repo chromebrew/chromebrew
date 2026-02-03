8b1f6b8040acbe06e7fbe3656c3327ed129c10c0
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-03 00:36:12 +0000

    hugo -> 0.155.2 in updater-hugo-0.155.2

--- packages/hugo.rb
+++ packages/hugo.rb
@@ -3,7 +3,7 @@ require 'package'
 class Hugo < Package
   description 'Hugo is one of the most popular open-source static site generators.'
   homepage 'https://gohugo.io'
-  version %w[aarch64 armv7l x86_64].include?(ARCH) ? '0.155.0' : '0.101.0'
+  version %w[aarch64 armv7l x86_64].include?(ARCH) ? '0.155.2' : '0.101.0'
   license 'Apache-2.0, Unlicense, BSD, BSD-2 and MPL-2.0'
   compatibility 'all'
   min_glibc '2.29' if ARCH.eql?('x86_64')
@@ -14,10 +14,10 @@ class Hugo < Package
      x86_64: "https://github.com/gohugoio/hugo/releases/download/v#{version}/hugo_extended_#{version}_linux-amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '5268d8c89c558201eec825f7ac852a507ef06507499de956f717812f10eb5ace',
-     armv7l: '5268d8c89c558201eec825f7ac852a507ef06507499de956f717812f10eb5ace',
+    aarch64: 'f65d8571895539247b006ab065dcbea62a582824a8e932de3b9b58da5b718584',
+     armv7l: 'f65d8571895539247b006ab065dcbea62a582824a8e932de3b9b58da5b718584',
        i686: '9ae794edd86415a611cae15fc72382ee6f2b729754e15319c144057a5457eaed',
-     x86_64: '6c8b34a98afe207982137d69abb98c6efb2829e0cfc099e23ab4c51f260e8c6a'
+     x86_64: '2bf76c75610e75fa5d15fd3dd0e1377c16ce4fa7202713283c744a19323ff419'
   })
 
   no_compile_needed
