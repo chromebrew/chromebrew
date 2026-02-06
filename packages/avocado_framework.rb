1722bfb8c88ace057f99fd6625d92510ee8d19a0
Ed Reel <edreel@gmail.com>
2026-02-06 05:30:27 -0600

    Avocado_framework 112.0 => 113.0 (#14538)

--- packages/avocado_framework.rb
+++ packages/avocado_framework.rb
@@ -3,7 +3,7 @@ require 'buildsystems/python'
 class Avocado_framework < Python
   description 'Avocado is a next generation testing framework inspired by Autotest and modern development tools such as git.'
   homepage 'https://avocado-framework.github.io/'
-  version '112.0'
+  version '113.0'
   license 'GPL-2 and GPL-2+'
   compatibility 'all'
   source_url 'https://github.com/avocado-framework/avocado.git'
@@ -11,10 +11,10 @@ class Avocado_framework < Python
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6a378adcadb53c3be8c7bf82cb61ca67157e82b63e7d34e809ad41a3189d14cf',
-     armv7l: '6a378adcadb53c3be8c7bf82cb61ca67157e82b63e7d34e809ad41a3189d14cf',
-       i686: 'b15ef7164ac47b3fbdcc821a116409ddaabd09410cfce7db38a11b50f12df054',
-     x86_64: '3f299d803640057d49850454097ce822616da78ae6e8a0e6b36c5a729d7fbfd9'
+    aarch64: 'fce2e9d4f9ca41e6dbd21c386f23a77045ea38a7097c60fda0d2e2245e21649a',
+     armv7l: 'fce2e9d4f9ca41e6dbd21c386f23a77045ea38a7097c60fda0d2e2245e21649a',
+       i686: '2fd30547a3d685775e31217876ed785daa7d74a225d5deb71d789a672e38032f',
+     x86_64: '988c044f3e6c99dce5a6c3216080f12df7bd76a68ffa50a684f7c8499f5200f5'
   })
 
   depends_on 'xdg_base'
