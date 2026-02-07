6f1f704883e3b7e0cf9d848eb727fdbb0300541c
satmandu <satmandu@users.noreply.github.com>
2026-02-07 03:53:47 +0000

    updater-mesa-25.3.5-llvm21: Package File Update Run on linux/amd64 container.

--- packages/mesa.rb
+++ packages/mesa.rb
@@ -11,9 +11,9 @@ class Mesa < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ed0c56169053d4e648f0ea96b3c7cebb1253056c708d911500544e88330ae08a',
-     armv7l: 'ed0c56169053d4e648f0ea96b3c7cebb1253056c708d911500544e88330ae08a',
-     x86_64: 'f020eb3bc882419d27c57dc752feca4760a505899a251ba6e5e39de7d169334d'
+    aarch64: '587ade575a36fd8facc3216d5aec4e62044f5f89cbaad58d80ffe36588adfb4d',
+     armv7l: '587ade575a36fd8facc3216d5aec4e62044f5f89cbaad58d80ffe36588adfb4d',
+     x86_64: '8396457f8ed62f5e151815841ae7c8dc7da3fc4ecd2b7f83a06623ec5bddad06'
   })
 
   depends_on 'elfutils' # R
