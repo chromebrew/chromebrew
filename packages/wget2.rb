715804def02d50d310d68e0c111b7478156a1af1
Satadru Pramanik <satadru@gmail.com>
2026-02-09 13:34:49 -0500

    remove patch
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/wget2.rb
+++ packages/wget2.rb
@@ -6,8 +6,10 @@ class Wget2 < Autotools
   version '2.2.1'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://gitlab.com/gnuwget/wget2.git'
-  git_hashtag "v#{version}"
+  source_url "https://ftp.gnu.org/gnu/wget/wget2-#{version}.tar.gz"
+  source_sha256 'agagag'
+  # source_url 'https://gitlab.com/gnuwget/wget2.git'
+  # git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -20,6 +22,7 @@ class Wget2 < Autotools
   depends_on 'brotli' # R
   depends_on 'bzip2' # R
   depends_on 'ca_certificates' # L
+  depends_on 'curl' => :build
   depends_on 'glibc' # R
   depends_on 'gmp' # R
   depends_on 'gnutls' # R
@@ -41,34 +44,17 @@ class Wget2 < Autotools
   depends_on 'zlib' # R
   depends_on 'zstd' # R
 
+  def self.patch
+    # system './bootstrap'
+    # system 'autoreconf -fiv'
+  end
+
   autotools_configure_options '--enable-year2038 \
                   --with-openssl=no \
                   --with-ssl=gnutls \
                   --without-libidn \
                   --with-bzip2 \
                   --with-lzma'
-
-  def self.patch
-    # Patch needed due to failure of gettext detection
-    # when 'autoreconf -fvi' invokes autopoint.
-    File.write 'gettext.patch', <<~GETTEXT_PATCH_EOF
-      diff -Npaur a/configure.ac b/configure.ac
-      --- a/configure.ac	2025-11-24 16:32:01.716022174 -0500
-      +++ b/configure.ac	2025-11-24 16:33:02.520730059 -0500
-      @@ -417,10 +417,6 @@ m4_ifdef([AM_GNU_GETTEXT], [
-       ], [
-         have_po=no
-       ])
-      -m4_ifdef([AM_GNU_GETTEXT_VERSION], [
-      -#do not indent here
-      -AM_GNU_GETTEXT_VERSION([0.21])
-      -])
-       AM_CONDITIONAL([HAVE_PO], [ test "$have_po" = "yes" ])
-    GETTEXT_PATCH_EOF
-    system 'patch -Np1 -i gettext.patch'
-    system './bootstrap'
-    system 'autoreconf -fiv'
-  end
 
   autotools_install_extras do
     FileUtils.ln_sf "#{CREW_PREFIX}/bin/wget2", "#{CREW_DEST_PREFIX}/bin/wget"
