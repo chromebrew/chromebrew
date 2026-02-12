3adb6aed6278c467b4f56f56511854e7c8b66936
satmandu <satmandu@users.noreply.github.com>
2026-02-12 14:47:23 +0000

    pnpm -> 10.29.3 in updater-pnpm-10.29.3

--- packages/pnpm.rb
+++ packages/pnpm.rb
@@ -3,11 +3,11 @@ require 'package'
 class Pnpm < Package
   description 'Fast, disk space efficient package manager'
   homepage 'https://pnpm.io/'
-  version '10.29.2'
+  version '10.29.3'
   license 'MIT'
   compatibility 'x86_64'
   source_url "https://github.com/pnpm/pnpm/releases/download/v#{version}/pnpm-linux-x64"
-  source_sha256 'f9a8e1652b83884da6a3c174f504220015001144cee8733c07df68c598d4c43a'
+  source_sha256 '2fc98db127c611be0c110af11b5b72759f7d736893dddc81df73b7b59b30f15a'
 
   no_compile_needed
 
