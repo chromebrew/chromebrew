0a5e652457b82a82019fd1b1f975579ea2ac505d
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-12 00:31:41 +0000

    py3_botocore -> 1.42.47-py3.14 in updater-py3_botocore-1.42.47-py3.14

--- packages/py3_botocore.rb
+++ packages/py3_botocore.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_botocore < Pip
   description 'Low-level, data-driven core of boto 3.'
   homepage 'https://github.com/boto/botocore'
-  version "1.42.46-#{CREW_PY_VER}"
+  version "1.42.47-#{CREW_PY_VER}"
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
