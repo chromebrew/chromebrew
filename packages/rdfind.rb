40110d2f27dac439f7df1b3da36c48fc46ce6d74
Satadru Pramanik <satadru@gmail.com>
2026-02-12 18:37:45 -0500

    Rdfind fixed for Nettle 4.0
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/rdfind.rb
+++ packages/rdfind.rb
@@ -6,25 +6,26 @@ require 'buildsystems/autotools'
 class Rdfind < Autotools
   description 'Redundant data find - a program that finds duplicate files.'
   homepage 'https://rdfind.pauldreik.se/'
-  version '1.7.0'
+  version '1.7.0-1ed3036'
   license 'GPL2'
   compatibility 'all'
   source_url 'https://github.com/pauldreik/rdfind.git'
-  git_hashtag "releases/#{version}"
+  git_hashtag '1ed3036a133037c4a32b99f960177f9774495f74'
+  # git_hashtag "releases/#{version.split('-').first}"
   binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: 'a8c69c6491066c5ec4e4d03d93e8ad0b52f0ed860726fc30c6811a9640eb1ef8',
      armv7l: 'a8c69c6491066c5ec4e4d03d93e8ad0b52f0ed860726fc30c6811a9640eb1ef8',
-       i686: '89b9632df969a15d3cf6004e8ba93ce79219a1b4f0e306c729cfb2e0e537b298',
+       i686: '8906db22f0dcca36ca4938cedcb4374c0e9f3402bad72f46a93082e28fc6ac4d',
      x86_64: '5fce0767f527c9f1f0af226aad64a21d992f9ed9dcfba497a180bfb18d33a2c7'
   })
 
   depends_on 'autoconf_archive' => :build
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'nettle' # R
-  depends_on 'xxhash' # R
+  depends_on 'gcc_lib' => :executable_only
+  depends_on 'glibc' => :executable_only
+  depends_on 'nettle' => :executable_only
+  depends_on 'xxhash' => :executable_only
 
   def self.patch
     system "sed -i '/<vector>/a #include <limits>' rdfind.cc"
