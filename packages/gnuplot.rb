a99268c7a6c730951d14dc914ec5ae09e8c8dca9
Ed Reel <edreel@gmail.com>
2026-02-13 08:49:29 +0000

    Gnuplot 6.0.2 => 6.0.4

--- packages/gnuplot.rb
+++ packages/gnuplot.rb
@@ -3,31 +3,43 @@ require 'buildsystems/autotools'
 class Gnuplot < Autotools
   description 'Gnuplot is a portable command-line driven graphing utility'
   homepage 'https://gnuplot.sourceforge.net/'
-  version '6.0.2'
+  version '6.0.4'
   license 'gnuplot'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://downloads.sourceforge.net/project/gnuplot/gnuplot/#{version}/gnuplot-#{version}.tar.gz"
-  source_sha256 'f68a3b0bbb7bbbb437649674106d94522c00bf2f285cce0c19c3180b1ee7e738'
+  source_sha256 '458d94769625e73d5f6232500f49cbadcb2b183380d43d2266a0f9701aeb9c5b'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c942209f80d406f73ed343cabcaf11e05017c9dfeef7655bde3c47574b882c29',
-     armv7l: 'c942209f80d406f73ed343cabcaf11e05017c9dfeef7655bde3c47574b882c29',
-     x86_64: 'a3a87f4195008a31f6d11bde6c19a22594673b7c7a25a6a2fa628a1ba284c7f7'
+    aarch64: 'a9d193a5f66e87d418772a22386826a4c7c695c58193fc799a846af90ae41795',
+     armv7l: 'a9d193a5f66e87d418772a22386826a4c7c695c58193fc799a846af90ae41795',
+     x86_64: '71bb3d83d27298efa8bfe43b333f3b0146632034dbfa037db7cf33b2f9f59509'
   })
 
-  depends_on 'cairo'
-  depends_on 'libcerf'
-  depends_on 'libgd'
-  depends_on 'libwebp'
+  depends_on 'at_spi2_core' => :executable_only
+  depends_on 'cairo' => :executable_only
+  depends_on 'gcc_lib' => :executable_only
+  depends_on 'gdk_pixbuf' => :executable_only
+  depends_on 'glib' => :executable_only
+  depends_on 'glibc' => :executable_only
+  depends_on 'gtk3' => :executable_only
+  depends_on 'harfbuzz' => :executable_only
+  depends_on 'libcerf' => :executable_only
+  depends_on 'libgd' => :executable_only
+  depends_on 'libwebp' => :executable_only
+  depends_on 'libx11' => :executable_only
   depends_on 'libxrender'
   depends_on 'libxxf86vm'
-  depends_on 'lua'
-  depends_on 'pango'
-  depends_on 'qt5_base'
-  depends_on 'qt5_svg'
+  depends_on 'lua' => :executable_only
+  depends_on 'ncurses' => :executable_only
+  depends_on 'pango' => :executable_only
+  depends_on 'qt5_base' => :executable_only
+  depends_on 'qt5_svg' => :executable_only
   depends_on 'qt5_tools'
-  depends_on 'wxwidgets'
+  depends_on 'readline' => :executable_only
+  depends_on 'wxwidgets' => :executable_only
+  depends_on 'zlib' => :executable_only
 
-  autotools_configure_options "CFLAGS='-I#{CREW_PREFIX}/share/Qt-5/include/QtCore'"
+  autotools_configure_options "CPPFLAGS='-I#{CREW_PREFIX}/share/Qt-5/include/QtCore \
+    -I#{CREW_LIB_PREFIX}/wx/include/gtk3-unicode-3.3'"
 end
