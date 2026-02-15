8e83048520365a211dff0607659a53df00b309f0
satmandu <satmandu@users.noreply.github.com>
2026-02-15 15:18:24 +0000

    updater-vim_runtime-9.2.0: Package File Update Run on linux/386 container.

--- packages/vim.rb
+++ packages/vim.rb
@@ -12,10 +12,10 @@ class Vim < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a9c1c43506dfdf06964de1bb3c40e3de1acb45a68b5d106044e0ddc7e590920d',
-     armv7l: 'a9c1c43506dfdf06964de1bb3c40e3de1acb45a68b5d106044e0ddc7e590920d',
+    aarch64: '768e096b0566627b71ff5f3f91b4e6b24dda1f533f5cede6ad8bf62bc2d360a8',
+     armv7l: '768e096b0566627b71ff5f3f91b4e6b24dda1f533f5cede6ad8bf62bc2d360a8',
        i686: '4f4909b64c3f3be106d7567203ddbe3737bfb041c956fd00f02baff31c2de210',
-     x86_64: 'b629880d8f1f8f0f67c22baa94fdb98842002e27274ba6ca2e294dd2698a1f97'
+     x86_64: '51623ed1b9f4963fb0611c997e0b35b671a3449149a7a8b2b89578382b8b97e0'
   })
 
   depends_on 'acl' => :executable_only
