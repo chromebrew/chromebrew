109a48bf5110ee77bb1d9958e9ef5f75c80a1485
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-16 00:32:32 +0000

    py3_xmltodict -> 1.0.3-py3.14 in updater-py3_xmltodict-1.0.3-py3.14

--- packages/py3_xmltodict.rb
+++ packages/py3_xmltodict.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_xmltodict < Pip
   description 'XMLtoDict makes working with XML feel like you are working with JSON.'
   homepage 'https://github.com/martinblech/xmltodict/'
-  version "1.0.2-#{CREW_PY_VER}"
+  version "1.0.3-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
