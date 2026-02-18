14fae3e05467c5d74323358a401535dbf2e28cea
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-18 00:35:08 +0000

    fish -> 4.5.0 in updater-fish-4.5.0

--- packages/fish.rb
+++ packages/fish.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Fish < CMake
   description 'fish is a smart and user-friendly command line shell for macOS, Linux, and the rest of the family.'
   homepage 'https://fishshell.com/'
-  version '4.4.0'
+  version '4.5.0'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/fish-shell/fish-shell.git'
