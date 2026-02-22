226375ecf4f5e0a8882e2d4cdfebe339792ef12a
Ed Reel <edreel@gmail.com>
2026-02-22 10:03:57 +0000

    Mc 4.8.32 => 4.8.33

--- packages/mc.rb
+++ packages/mc.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Mc < Autotools
   description 'GNU Midnight Commander is a visual file manager'
   homepage 'http://midnight-commander.org/'
-  version '4.8.32'
+  version '4.8.33'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/MidnightCommander/mc.git'
@@ -11,15 +11,20 @@ class Mc < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e2a24066767225d5b6eb92e83fea3e17fb30877403dbe1fb17f43646164547b2',
-     armv7l: 'e2a24066767225d5b6eb92e83fea3e17fb30877403dbe1fb17f43646164547b2',
-       i686: '2e8cd28f3e608407f5a9eaae4fc664142db54302e56e4f8f4b5d1b0fc600608f',
-     x86_64: '96b9254b193ec6af096fc2a8d535c825356a5ee8faa271708df10b85520e220c'
+    aarch64: 'e172ee211ea018e8b25000857b8ded88adcebaf0d1f965b977efb3a62b7fb8bb',
+     armv7l: 'e172ee211ea018e8b25000857b8ded88adcebaf0d1f965b977efb3a62b7fb8bb',
+       i686: '2076782eb634f221f3ce2e5cb8adcf8e95959920178a1b2d575679942014fb51',
+     x86_64: 'bd6a5409fdec7c666d211581808ac91e3f27d32ba80168d2532134696057f680'
   })
 
-  depends_on 'glib' => :build
   depends_on 'aspell' => :build
-  depends_on 'gpm'
+  depends_on 'check' => :build
+  depends_on 'e2fsprogs' => :executable_only
+  depends_on 'glib' => :executable_only
+  depends_on 'glibc' => :executable_only
+  depends_on 'gpm' => :executable_only
+  depends_on 'graphviz' => :build unless ARCH.eql?('i686')
+  depends_on 'slang' => :executable_only
 
   run_tests
 end
