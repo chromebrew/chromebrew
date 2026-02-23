9e159066466b46bcc65a0a433f7637dfc37cb351
Satadru Pramanik <satadru@gmail.com>
2026-02-23 15:20:02 -0500

    use autoreconf
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/guile.rb
+++ packages/guile.rb
@@ -28,6 +28,7 @@ class Guile < Autotools
   depends_on 'readline' # R
 
   def self.patch
+    system 'autoreconf -fiv'
     # Fix automake 1.17 requirements.
     system 'grep -rlZ AM_INIT_AUTOMAKE . | xargs -0 sed -i \'s,1.17,1.18,g\''
     system 'grep -rlZ aclocal . | xargs -0 sed -i \'s,1.17,1.18,g\''
