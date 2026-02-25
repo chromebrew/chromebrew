305ba5164378df8a9a9e7ba4df15b901209dcf62
Satadru Pramanik <satadru@gmail.com>
2026-02-24 22:35:50 -0500

    Let Build workflow check downstream depends for needed package builds.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

---
+++ packages/llvm22_dev.rb
@@ -0,0 +1,58 @@
+require 'package'
+Package.load_package("#{__dir__}/llvm22_build.rb")
+
+class Llvm22_dev < Package
+  description 'LLVM: Everything except libLLVM & llvm-strip'
+  homepage Llvm22_build.homepage
+  version '22.1.0'
+  # When upgrading llvm*_build, be sure to upgrade llvm_lib*, llvm_dev*, libclc, and openmp in tandem.
+  puts "#{self} version differs from llvm version #{Llvm22_build.version}".orange if version != Llvm22_build.version && !ENV['NESTED_CI']
+  license Llvm22_build.license
+  compatibility 'all'
+  source_url 'SKIP'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '48e63c1ac0463d33962e3ea3be7988ea10d92f79aed91604346e81092ea5fdce',
+     armv7l: '48e63c1ac0463d33962e3ea3be7988ea10d92f79aed91604346e81092ea5fdce',
+       i686: 'fb993ceb76a87174561f66854f4283d1ba883b83294cd324a0e5805be3172209',
+     x86_64: 'abe59f97a1ca6a4b6e081fa8a566976192f444fb5f15d55e632b070cbe55f9c3'
+  })
+
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'libedit' # R
+  depends_on 'libxml2' # R
+  depends_on 'llvm22_build' => :build
+  depends_on 'llvm_lib' # R
+  depends_on 'xzutils' # R
+  depends_on 'zlib' # R
+  depends_on 'zstd' # R
+
+  conflicts_ok
+  no_shrink
+  no_source_build
+  no_strip
+
+  def self.preflight
+    abort "Update #{CREW_LLVM_VER}_build first.".lightred if Gem::Version.new(version.split('-').first) < Gem::Version.new(Llvm22_build.version.split('-').first)
+    llvm_lib_obj = Package.load_package("#{__dir__}/#{CREW_LLVM_VER}_dev.rb")
+    abort "Update  #{CREW_LLVM_VER}_lib first.".lightred if Gem::Version.new(version.split('-').first) > Gem::Version.new(llvm_lib_obj.version.split('-').first)
+  end
+
+  def self.install
+    puts 'Installing llvm22_build to pull files for build...'.lightblue
+    @filelist_path = File.join(CREW_META_PATH, 'llvm22_build.filelist')
+    abort 'File list for llvm22_build does not exist!'.lightred unless File.file?(@filelist_path)
+    @filelist = File.readlines(@filelist_path, chomp: true).grep(/^(?!#)/)
+
+    @filelist.each do |filename|
+      next if filename.include?('.so') && filename.include?('libLLVM')
+      next if filename.include?('llvm-strip')
+
+      @destpath = File.join(CREW_DEST_DIR, filename)
+      @filename_target = File.realpath(filename)
+      FileUtils.install @filename_target, @destpath
+    end
+  end
+end
---
+++ packages/llvm22_lib.rb
@@ -0,0 +1,54 @@
+require 'package'
+Package.load_package("#{__dir__}/llvm22_build.rb")
+
+class Llvm22_lib < Package
+  description 'LibLLVM and llvm-strip'
+  homepage Llvm22_build.homepage
+  version '22.1.0'
+  # When upgrading llvm*_build, be sure to upgrade llvm_lib*, llvm_dev*, libclc, and openmp in tandem.
+  puts "#{self} version differs from llvm version #{Llvm22_build.version}".orange if version != Llvm22_build.version && !ENV['NESTED_CI']
+  license Llvm22_build.license
+  compatibility 'all'
+  source_url 'SKIP'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '04165005ee1648c72038510ce5ef28d32d33cf371e6fd19ee2399b1ab5e3a37f',
+     armv7l: '04165005ee1648c72038510ce5ef28d32d33cf371e6fd19ee2399b1ab5e3a37f',
+       i686: '07c6fe53416c04cbf5f7dea7de1c589eb478ec310f66c7467c60a7c6d0622dc5',
+     x86_64: '95ff75665bf1b0f53280a063054b9cf902b411395c5d33c93c2c2f7a170b5ada'
+  })
+
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'libedit' # R
+  depends_on 'libffi' # R
+  depends_on 'libxml2' # R
+  depends_on 'llvm22_build' => :build
+  depends_on 'zlib' # R
+  depends_on 'zstd' # R
+
+  conflicts_ok
+  no_shrink
+  no_source_build
+  no_strip
+
+  def self.preflight
+    abort "Update #{CREW_LLVM_VER} first.".lightred if Gem::Version.new(version.split('-').first) < Gem::Version.new(Llvm22_build.version.split('-').first)
+  end
+
+  def self.install
+    puts 'Installing llvm22_build to pull files for build...'.lightblue
+    @filelist_path = File.join(CREW_META_PATH, 'llvm22_build.filelist')
+    abort 'File list for llvm22_build does not exist!'.lightred unless File.file?(@filelist_path)
+    @filelist = File.readlines(@filelist_path, chomp: true).grep(/^(?!#)/)
+
+    @filelist.each do |filename|
+      next unless (filename.include?('.so') && filename.include?('libLLVM')) || filename.include?('llvm-strip')
+
+      @destpath = File.join(CREW_DEST_DIR, filename)
+      @filename_target = File.realpath(filename)
+      FileUtils.install @filename_target, @destpath
+    end
+  end
+end
