d03375cec20fe80e3cff52620af86cbc54b028c4
Ed Reel <edreel@gmail.com>
2026-02-25 00:25:40 +0000

    Mujs 1.1.0 => 1.3.7

--- packages/mujs.rb
+++ packages/mujs.rb
@@ -1,17 +1,17 @@
 # Adapted from Arch Linux mujs PKGBUILD at:
 # https://github.com/archlinux/svntogit-community/raw/packages/mujs/trunk/PKGBUILD
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Mujs < Package
+class Mujs < Autotools
   description 'An embeddable Javascript interpreter in C'
   homepage 'https://mujs.com/'
-  version '1.1.0'
+  version '1.3.7'
   license 'ISC'
   compatibility 'all'
-  source_url "https://github.com/ccxvii/mujs/archive/refs/tags/#{version}.tar.gz"
-  source_sha256 '8e43a38fdea75f036a9f3213e346a6c304206b4e3d00886564fb6bf70c1c2807'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/ccxvii/mujs.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
     aarch64: '6e35d13ccbbf048b85406b5edf5a9ff1965359bcace8a7c53ca9a293691bc09c',
@@ -20,11 +20,13 @@ class Mujs < Package
      x86_64: '016d3bcc9608929db4bcb55b7449151200557c4407187a1f6cd806cfdd8a42ad'
   })
 
-  def self.build
-    system "env XCFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      XCPPFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
-      make prefix=release shared"
-  end
+  depends_on 'glibc' # R
+  depends_on 'readline' => :executable_only
+
+  autotools_skip_configure
+
+  autotools_pre_make_options "XCFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto' \
+    XCPPFLAGS='-pipe -fno-stack-protector -U_FORTIFY_SOURCE -flto=auto'"
 
   def self.install
     system "make \
