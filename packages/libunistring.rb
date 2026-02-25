a24efaeac59489e53f2a9f09aad2839e94257d16
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-25 00:36:51 +0000

    libunistring -> 1.4.2 in updater-libunistring-1.4.2

--- packages/libunistring.rb
+++ packages/libunistring.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Libunistring < Autotools
   description 'A library that provides functions for manipulating Unicode strings and for manipulating C strings according to the Unicode standard.'
   homepage 'https://www.gnu.org/software/libunistring/'
-  version '1.4.1'
+  version '1.4.2'
   license 'LGPL-3+ or GPL-2+ and FDL-1.2 or GPL-3+'
   compatibility 'all'
   source_url "https://ftp.gnu.org/gnu/libunistring/libunistring-#{version}.tar.xz"
-  source_sha256 '67d88430892527861903788868c77802a217b0959990f7449f2976126a307763'
+  source_sha256 '5b46e74377ed7409c5b75e7a96f95377b095623b689d8522620927964a41499c'
   binary_compression 'tar.zst'
 
   binary_sha256({
