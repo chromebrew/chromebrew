86d4c59c57c142e7ee2b9becdcfc7b1dcba75505
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-25 00:37:45 +0000

    dart -> 3.11.1 in updater-dart-3.11.1

--- packages/dart.rb
+++ packages/dart.rb
@@ -3,7 +3,7 @@ require 'package'
 class Dart < Package
   description 'The Dart SDK is a set of tools and libraries for the Dart programming language.  You can find information about Dart online at dartlang.org.'
   homepage 'https://dart.dev'
-  version '3.11.0'
+  version '3.11.1'
   license 'BSD-3'
   compatibility 'aarch64 armv7l x86_64'
 
@@ -13,9 +13,9 @@ class Dart < Package
      x86_64: "https://storage.googleapis.com/dart-archive/channels/stable/release/#{version}/sdk/dartsdk-linux-x64-release.zip"
   })
   source_sha256({
-    aarch64: '544e6137a072d73451148db128bfeb7fb73992615537b81d641b3396983c6884',
-     armv7l: '544e6137a072d73451148db128bfeb7fb73992615537b81d641b3396983c6884',
-     x86_64: 'f31729b567be318c7cc23bdafe6b9a997fa7ddbf829df5016f066227b6aa0c99'
+    aarch64: '69afd778175725c9ef25a7d6ca52c1a84f0e803cd8e8c39b76ff7667e7933d0a',
+     armv7l: '69afd778175725c9ef25a7d6ca52c1a84f0e803cd8e8c39b76ff7667e7933d0a',
+     x86_64: 'cffb8fa4afb777c2630c66311bf59eb034cd3ea0c7f94ad326e1a62c6aa9c272'
   })
 
   conflicts_with 'flutter'
