85beb5fd782da39f1ef6281aa7acf7946711de78
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-26 00:32:44 +0000

    vivaldi -> 7.8.3925.74 in updater-vivaldi-7.8.3925.74

--- packages/vivaldi.rb
+++ packages/vivaldi.rb
@@ -5,7 +5,7 @@ class Vivaldi < Package
   description 'Vivaldi is a new browser that blocks unwanted ads, protects you from trackers, and puts you in control with unique built-in features.'
   homepage 'https://vivaldi.com/'
   # The project stopped supporting armv7l after the 7.5 release.
-  version ARCH.eql?('x86_64') ? '7.8.3925.73' : '7.5.3735.74-1'
+  version ARCH.eql?('x86_64') ? '7.8.3925.74' : '7.5.3735.74-1'
   license 'Vivaldi'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.37'
@@ -46,7 +46,7 @@ class Vivaldi < Package
     source_sha256 '9017e6327c140ad9a9e1f0ce450681a729a15ea764337c30226f51c042ff7e62'
   when 'x86_64'
     arch = 'amd64'
-    source_sha256 '03901bf61af8d367bfcbb10da16cb67d60fe6b0d2b05ded9c449c05e223c4e4b'
+    source_sha256 'bfb7c013c8945582421e75110ff5cba4ccfcdd7d110cad0862b2a648c52d9b10'
   end
 
   case ARCH
