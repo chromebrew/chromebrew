73c1d2bd5e4ebdad4e5dd73848e1c14057fe9a0d
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-27 00:37:33 +0000

    bearly -> 5.0.3 in updater-bearly-5.0.3

--- packages/bearly.rb
+++ packages/bearly.rb
@@ -3,11 +3,11 @@ require 'package'
 class Bearly < Package
   description 'Bearly makes you 10x faster by adding state of the art AI to your workflow.'
   homepage 'https://bearly.ai/'
-  version '3.0.0'
+  version '5.0.3'
   license 'Unknown'
   compatibility 'x86_64'
   source_url "https://github.com/bearlyai/releases/releases/download/v#{version}/Bearly-#{version}.AppImage"
-  source_sha256 'bc418e3a37707fa054abce20ed83b60543e8817b892e13a5432bcf3862598b15'
+  source_sha256 '2260ec1880d3dfdffd0b0f79d7dab972cea1286ec26b2ca6ceedf46dc6d50d0b'
 
   no_compile_needed
   no_shrink
