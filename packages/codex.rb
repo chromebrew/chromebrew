6742b589dacf54eb998ff3ab00cfb03b9e219c87
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-02-27 00:37:51 +0000

    codex -> 0.106.0 in updater-codex-0.106.0

--- packages/codex.rb
+++ packages/codex.rb
@@ -3,12 +3,12 @@ require 'package'
 class Codex < Package
   description 'Lightweight coding agent that runs in your terminal'
   homepage 'https://github.com/openai/codex'
-  version '0.105.0'
+  version '0.106.0'
   license 'Apache-2.0'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://github.com/openai/codex/releases/download/rust-v#{version}/codex-x86_64-unknown-linux-gnu.zst"
-  source_sha256 'cb659cea2c37902dfc8d9afde3f86923b87ba21e1aff63d507acf8fc07f018c2'
+  source_sha256 '86d93835bd00398301ba6af0ea9e68dc455737467cc63789e5f3fcd18d1dcbc3'
 
   depends_on 'zstd'
 
