49807071998dca94d8a0be5be448143b4b81b30e
Ed Reel <edreel@gmail.com>
2026-02-28 07:42:04 +0000

    Multitail 7.1.3 => 7.1.5

--- packages/multitail.rb
+++ packages/multitail.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Multitail < Package
+class Multitail < CMake
   description 'MultiTail allows you to monitor logfiles and command output in multiple windows in a terminal, colorize, filter and merge.'
   homepage 'https://www.vanheusden.com/multitail/'
-  version '7.1.3'
+  version '7.1.5'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/folkertvanheusden/multitail.git'
@@ -11,19 +11,14 @@ class Multitail < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9b1283d41d4db63ae5f8128f518efdcf220eec40bf1e45156c1bf6919c83436e',
-     armv7l: '9b1283d41d4db63ae5f8128f518efdcf220eec40bf1e45156c1bf6919c83436e',
-       i686: '830acfcc1baecbaac8a8a3b2518b8713d8aa174357fd1222a5d6999b5ffad168',
-     x86_64: '8e5532a2c47fa8f690433568cb3e9d8b9d4548ff643955d85c5da69b2d1a8696'
+    aarch64: '0e9d7d2bdb9f328d92e366c5d908e8fdb94c12cd8aeb0331fa7921595ce6321e',
+     armv7l: '0e9d7d2bdb9f328d92e366c5d908e8fdb94c12cd8aeb0331fa7921595ce6321e',
+       i686: '72d384d94f5dc87c27e9afa704e7343b02618573413522a39f04527c4fd359bb',
+     x86_64: 'eb31ec5a22057605bfc74da263eb0bff38c84f20b73e9d1092fc3c2b420bf781'
   })
 
-  depends_on 'ncurses'
+  depends_on 'glibc' => :executable_only
+  depends_on 'ncurses' => :executable_only
 
-  def self.build
-    system 'make', "PREFIX=#{CREW_PREFIX}", "SYSCONFDIR=#{CREW_PREFIX}/etc"
-  end
-
-  def self.install
-    system 'make', "PREFIX=#{CREW_PREFIX}", "SYSCONFDIR=#{CREW_PREFIX}/etc", "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  cmake_options "-DCMAKE_C_FLAGS='-I#{CREW_PREFIX}/include/ncursesw'"
 end
