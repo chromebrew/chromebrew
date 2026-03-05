5fcd1c54423c0a083911c49488dab70e37de9d13
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-05 00:35:37 +0000

    delve -> 1.26.1 in updater-delve-1.26.1

--- packages/delve.rb
+++ packages/delve.rb
@@ -3,7 +3,7 @@ require 'package'
 class Delve < Package
   description 'Debugger for the Go programming language'
   homepage 'https://github.com/go-delve/delve'
-  version '1.26.0'
+  version '1.26.1'
   license 'MIT'
   compatibility 'i686 x86_64'
   source_url 'SKIP'
