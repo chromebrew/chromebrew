29de8aa643ba57cafb5063ed334e879f9fd4a8c4
uberhacker <edreel@gmail.com>
2026-03-08 08:04:57 +0000

    Nlopt 2.10.0 => 2.10.1

--- packages/nlopt.rb
+++ packages/nlopt.rb
@@ -6,7 +6,7 @@ require 'buildsystems/cmake'
 class Nlopt < CMake
   description 'nonlinear optimization library'
   homepage 'http://ab-initio.mit.edu/wiki/index.php/NLopt'
-  version '2.10.0'
+  version '2.10.1'
   license 'LGPL'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/stevengj/nlopt.git'
@@ -14,16 +14,17 @@ class Nlopt < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a5d63cb98c4bdc8d602d12afa67ce13361bdcf139cb1dcc07df3e7078981aa48',
-     armv7l: 'a5d63cb98c4bdc8d602d12afa67ce13361bdcf139cb1dcc07df3e7078981aa48',
-     x86_64: 'c7d368c94742dc911fe359c2a960b89f81e12b77982bab19bb2b3486bd619a85'
+    aarch64: 'dfc04694a16c0d3f268e2535cb751985aeea7b67d5fa3c12d0bbef052f1f4eec',
+     armv7l: 'dfc04694a16c0d3f268e2535cb751985aeea7b67d5fa3c12d0bbef052f1f4eec',
+     x86_64: '403aad9d25a98ee35f9bd9f9e0d28d43f1f879bb62d81ea743ff0155a9574d3c'
   })
 
   depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
   depends_on 'guile' => :build
   depends_on 'octave' # R
+  depends_on 'openjdk11' => :build
   depends_on 'py3_numpy' => :build
+  depends_on 'python3' => :build
   depends_on 'swig' => :build
-
-  cmake_options '-DNLOPT_MATLAB=OFF -DNLOPT_CXX=ON -DNLOPT_SWIG=OFF -DNLOPT_LINK_PYTHON=OFF'
 end
