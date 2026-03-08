97e2f0cf4fb3c980416c5c5ee9562b64161db9a3
Ed Reel <edreel@gmail.com>
2026-03-07 21:21:29 -0600

    Nuspell 5.1.3 => 5.1.7 (#14909)

--- packages/nuspell.rb
+++ packages/nuspell.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Nuspell < CMake
   description 'Nuspell is a fast and safe spelling checker software program.'
   homepage 'https://nuspell.github.io/'
-  version '5.1.3'
+  version '5.1.7'
   license 'LGPL-3.0, GPL-3.0'
   compatibility 'all'
   source_url 'https://github.com/nuspell/nuspell.git'
@@ -11,11 +11,17 @@ class Nuspell < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '18ab9d2cb145ca77a7d1de062bb5670ecdc828bc1b23386c35de4f9fd0189de4',
-     armv7l: '18ab9d2cb145ca77a7d1de062bb5670ecdc828bc1b23386c35de4f9fd0189de4',
-       i686: 'bc3cf76a3daee07e6775293f763a0b45f18f9e4f9f1a87a0a1cdae992d45d857',
-     x86_64: '346124fe9a711f4eb3a660532e79b7dc28bd863e2de22df079bcd00e1c095742'
+    aarch64: '1783c2cb9635732c5c1199490f9453ecf2263e85c0fa273a057d19a095c34316',
+     armv7l: '1783c2cb9635732c5c1199490f9453ecf2263e85c0fa273a057d19a095c34316',
+       i686: 'fab4f186e62d5a90da541778c12541d60413328b60f877a460279b51064b26db',
+     x86_64: 'ac05081c6ef527225c697e1d1606be8838b8b59bde1ed7c73541d6a9d91b8ce0'
   })
+
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'graphviz' => :build
+  depends_on 'icu4c' # R
+  depends_on 'pandoc' => :build
 
   run_tests
 end
