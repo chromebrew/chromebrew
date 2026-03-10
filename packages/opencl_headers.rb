12d49603af2a2e8e1a245e72af0c7f7e658ca717
Ed Reel <edreel@gmail.com>
2026-03-10 08:33:24 +0000

    Opencl_headers 2022.09.30 => 2025.07.22, Opencl_icd_loader 2022.09.30 => 2025.07.22

--- packages/opencl_headers.rb
+++ packages/opencl_headers.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Opencl_headers < Package
+class Opencl_headers < CMake
   description 'OpenCL header files'
   homepage 'https://github.com/KhronosGroup/OpenCL-Headers'
-  version '2022.09.30'
+  version '2025.07.22'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'https://github.com/KhronosGroup/OpenCL-Headers.git'
@@ -11,22 +11,9 @@ class Opencl_headers < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '68ea6d940a1d947e20f199406ba7ba9ff3bcbfe339a909d05eb1e2ec3b1a7f52',
-     armv7l: '68ea6d940a1d947e20f199406ba7ba9ff3bcbfe339a909d05eb1e2ec3b1a7f52',
-       i686: '78c61aef623259ba152518900461ee23cdad15dfa18b8f18ba7602477786f88d',
-     x86_64: '5a930f083f71b83e79eb14fd90ea5e0df880e5d2227a8cfc781e66c4b5862edc'
+    aarch64: '51b119085b6e69a18adb3fe7161abe8260266b1a11d305c9c684902f92eb7fa4',
+     armv7l: '51b119085b6e69a18adb3fe7161abe8260266b1a11d305c9c684902f92eb7fa4',
+       i686: '41ea1de6261d31385d48caf55e94819986ab201bbad1f446299ea574082792b8',
+     x86_64: '7d0b762685e75ce2617145b5c6d58d4b871dfdd6f0edafeadb672e8cc98e7f6c'
   })
-
-  def self.build
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "cmake -G Ninja \
-        #{CREW_CMAKE_OPTIONS} .."
-    end
-    system 'samu -C builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} samu -C builddir install"
-  end
 end
