95913aebe52d09b009c478443e4e3a720e0aa4f4
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-11 00:32:43 +0000

    keepassxc -> 2.7.12 in updater-keepassxc-2.7.12

--- packages/keepassxc.rb
+++ packages/keepassxc.rb
@@ -3,12 +3,12 @@ require 'package'
 class Keepassxc < Package
   description 'KeePass Cross-Platform Community Edition, the free, open source, light-weight and easy-to-use password manager.'
   homepage 'https://keepassxc.org/'
-  version '2.7.11'
+  version '2.7.12'
   license 'GPL-2 or GPL-3'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-x86_64.AppImage"
-  source_sha256 '4629e533d3377d02df883790a3466849a9a1ce9898b7d716d3298acff306766b'
+  source_sha256 '564fe8b751b9ef7aa057e4d3d0b2878db24eaa0f6b1c855c82e699ab0913ae49'
 
   no_compile_needed
 
