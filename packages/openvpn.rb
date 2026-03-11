5cfaf37780815cc2c5d0147728ef4389f475d4f4
Ed Reel <edreel@gmail.com>
2026-03-10 22:31:13 -0500

    Openvpn 2.6.17 => 2.7.0 (#14967)

--- packages/openvpn.rb
+++ packages/openvpn.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Openvpn < Autotools
   description 'OpenVPN is an open source VPN daemon'
   homepage 'https://openvpn.net/'
-  version '2.6.17'
+  version '2.7.0'
   license 'GPL-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/OpenVPN/openvpn.git'
@@ -11,24 +11,24 @@ class Openvpn < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1d97aaadde2a82b0d7528da9138c47932422554a172bce51c82cb21281d3a953',
-     armv7l: '1d97aaadde2a82b0d7528da9138c47932422554a172bce51c82cb21281d3a953',
-     x86_64: '0fb418b73729a2a31a29e8ee42ffa88f13d5aa46d788d1b542389cf78cf87bfd'
+    aarch64: '40b53f3efc0fa76468858ebd6c028b2d5da9448b82806310809ac45b031241fe',
+     armv7l: '40b53f3efc0fa76468858ebd6c028b2d5da9448b82806310809ac45b031241fe',
+     x86_64: '936a43fe1d03e8f3b448f1104da2e4dcc5e75324a9d3701dd83de759576ad111'
   })
 
-  depends_on 'e2fsprogs' # R
+  depends_on 'e2fsprogs' => :executable_only
   depends_on 'glibc' # R
-  depends_on 'krb5' # R
-  depends_on 'libcap_ng' # R
+  depends_on 'krb5' => :executable_only
+  depends_on 'libcap_ng' => :executable_only
   depends_on 'libeconf' # R
   depends_on 'libmbedtls' => :build
-  depends_on 'libnl3' # R
-  depends_on 'libtirpc' # R
+  depends_on 'libnl3' => :executable_only
+  depends_on 'libtirpc' => :executable_only
   depends_on 'linux_pam' # R
-  depends_on 'lz4' # R
-  depends_on 'lzo' # R
+  depends_on 'lz4' => :executable_only
+  depends_on 'lzo' => :executable_only
   depends_on 'mold' => :build
-  depends_on 'openssl' # R
+  depends_on 'openssl' => :executable_only
   depends_on 'py3_docutils' => :build
 
   run_tests
