53ff339cbf7297e25a77ec98ced23dc3d9aa080f
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-11 00:32:22 +0000

    monero -> 0.18.4.6 in updater-monero-0.18.4.6

--- packages/monero.rb
+++ packages/monero.rb
@@ -3,7 +3,7 @@ require 'package'
 class Monero < Package
   description 'Private, decentralized cryptocurrency that keeps your finances confidential and secure.'
   homepage 'https://www.getmonero.org/'
-  version '0.18.4.5'
+  version '0.18.4.6'
   license 'The Cryptonote developers,The Boolberry developers,MIT'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.27'
@@ -14,9 +14,9 @@ class Monero < Package
      x86_64: "https://downloads.getmonero.org/cli/monero-linux-x64-v#{version}.tar.bz2"
   })
   source_sha256({
-    aarch64: '42fbcbcf678794d6b104134bb7218093d6aa2764cc9cfa6fad404a4648a7c38a',
-     armv7l: '42fbcbcf678794d6b104134bb7218093d6aa2764cc9cfa6fad404a4648a7c38a',
-     x86_64: '423b49f3658e29f70a1d971667dec924c7ee7a107cfc93440456e28500b471a6'
+    aarch64: 'c4dbec17d1f25ef52f9a4832b8ba80b8818c8ed9adb786fa193ed38a019b1817',
+     armv7l: 'c4dbec17d1f25ef52f9a4832b8ba80b8818c8ed9adb786fa193ed38a019b1817',
+     x86_64: '60cfc32d39c6fe6c19b23513d02017fe9055d2e412ee4cbc30fa40ba811fe052'
   })
 
   no_compile_needed
