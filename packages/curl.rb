7a39eedc4b5932a17215a4a7bc3abf5631cb73eb
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-12 00:29:00 +0000

    curl -> 8.19.0 in updater-curl-8.19.0

--- packages/curl.rb
+++ packages/curl.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Curl < Autotools
   description 'Command line tool and library for transferring data with URLs.'
   homepage 'https://curl.se/'
-  version '8.18.0'
+  version '8.19.0'
   license 'curl'
   compatibility 'all'
   source_url "https://curl.se/download/curl-#{version.split('-').first}.tar.xz"
-  source_sha256 '40df79166e74aa20149365e11ee4c798a46ad57c34e4f68fd13100e2c9a91946'
+  source_sha256 '4eb41489790d19e190d7ac7e18e82857cdd68af8f4e66b292ced562d333f11df'
   binary_compression 'tar.zst'
 
   binary_sha256({
