539bb4abf6782ee915bbce4013c2e59b5bd67d25
Ed Reel <edreel@gmail.com>
2026-03-13 13:33:53 -0500

    Peco 0.5.11 => 0.6.0 (#14994)

--- packages/peco.rb
+++ packages/peco.rb
@@ -3,7 +3,7 @@ require 'package'
 class Peco < Package
   description 'Simplistic interactive filtering tool'
   homepage 'https://github.com/peco/peco'
-  version '0.5.11'
+  version '0.6.0'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/peco/peco.git'
@@ -11,16 +11,15 @@ class Peco < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2da48435de834d1365e1d6080babe514c6adf7ae1d70f04ee2bad71009730c6b',
-     armv7l: '2da48435de834d1365e1d6080babe514c6adf7ae1d70f04ee2bad71009730c6b',
-       i686: 'cf7949ba4b021bfeeaa3b3c06a0d8b0ded4c3bafeb7fa6ef73b9c32494fd47f4',
-     x86_64: '7bfddfa8261c705a5bc5ffa1802845eb251e208025864df34271e2e4ca9b7f6c'
+    aarch64: '824b8568e54f044f07176ed2378cbf7d9567d7bd7e35bf62d1c917236a3b5306',
+     armv7l: '824b8568e54f044f07176ed2378cbf7d9567d7bd7e35bf62d1c917236a3b5306',
+       i686: 'a965a51598801382c319ed078cc05e0dc374526591079303947c007bb16684cc',
+     x86_64: '795abcddad3f4713fce15baa93cffc3ece5f223ff19abd588589409eebe12010'
   })
 
   depends_on 'go' => :build
 
   def self.install
-    system 'make build'
     system "go build -o #{CREW_DEST_PREFIX}/bin/peco cmd/peco/peco.go"
   end
 end
