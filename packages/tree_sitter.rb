7e3159cbdcf20e474ac66522e729f0b8b6c2e01f
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-15 00:43:32 +0000

    tree_sitter -> 0.26.7 in updater-tree_sitter-0.26.7

--- packages/tree_sitter.rb
+++ packages/tree_sitter.rb
@@ -6,7 +6,7 @@ require 'package'
 class Tree_sitter < Package
   description 'An incremental parsing system for programming tools'
   homepage 'https://github.com/tree-sitter/tree-sitter'
-  version '0.26.6'
+  version '0.26.7'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/tree-sitter/tree-sitter.git'
