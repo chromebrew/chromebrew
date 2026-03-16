b90b28acf3a4c46ada9543867802f540d7999b4a
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-16 00:41:22 +0000

    armadillo -> 15.2.4 in updater-armadillo-15.2.4

--- packages/armadillo.rb
+++ packages/armadillo.rb
@@ -3,11 +3,11 @@ require 'buildsystems/cmake'
 class Armadillo < CMake
   description 'Armadillo is a high quality linear algebra library (matrix maths) for the C++ language, aiming towards a good balance between speed and ease of use'
   homepage 'https://arma.sourceforge.net/'
-  version '15.2.3'
+  version '15.2.4'
   license 'Apache-2.0'
   compatibility 'all'
   source_url "https://downloads.sourceforge.net/project/arma/armadillo-#{version}.tar.xz"
-  source_sha256 '0182d67d6949e4347a0bc62fc8c2793be7eb203c71f19edff93f8c45fd4a8190'
+  source_sha256 'bb03a16da6b2cca43962b65a2890faf4e6b16607220cf60270436a11e09e6f46'
   binary_compression 'tar.zst'
 
   binary_sha256({
