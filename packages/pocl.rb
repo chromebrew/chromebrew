81c1fbae0bc9569887b14e39329649c91f0bc32c
Ed Reel <edreel@gmail.com>
2026-03-18 03:57:49 -0500

    Pocl 6.0 => 7.1 (#15060)

--- packages/pocl.rb
+++ packages/pocl.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Pocl < CMake
   description 'PoCL is a portable open source (MIT-licensed) implementation of the OpenCL standard.'
   homepage 'https://portablecl.org/'
-  version '6.0'
+  version '7.1'
   license 'MIT'
   compatibility 'x86_64'
   source_url 'https://github.com/pocl/pocl.git'
@@ -11,15 +11,17 @@ class Pocl < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    x86_64: '233188364ce93b7cd95c273c728fee947b2a6ef4d376825a65b08484d7eb74ba'
+    x86_64: 'ca5034ff4857842753dd3002bee4bf5476e19788c207b74e2cf2e0fea8782cb4'
   })
 
   depends_on 'cmake' => :build
-  # llvm version between 14.0 and 18.0 required to build.
-  # llvm19+ must be removed to avoid detection.
-  depends_on 'llvm18_dev' => :build
-  depends_on 'llvm18_lib' => :build
-  depends_on 'hwloc'
+  depends_on 'gcc_lib' # R
+  depends_on 'glibc' # R
+  depends_on 'hwloc' # R
+  depends_on 'llvm_dev' # R
+  depends_on 'llvm_lib' # R
+  depends_on 'spirv_llvm_translator' # R
+  depends_on 'spirv_tools' # R
 
   cmake_options '-DLLC_HOST_CPU=x86-64'
 
