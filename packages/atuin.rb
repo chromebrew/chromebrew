13ca444c336836c60c1be322dc6972638c902b8b
Ed Reel <edreel@gmail.com>
2026-03-17 22:52:22 -0500

    Add atuin package (#15047)

---
+++ packages/atuin.rb
@@ -0,0 +1,40 @@
+require 'package'
+
+class Atuin < Package
+  description 'Sync, search and backup shell history.'
+  homepage 'https://atuin.sh/'
+  version '18.13.3'
+  license 'MIT'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url 'https://github.com/atuinsh/atuin.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: '55a7fe9d48b915e1ba79ea4651014b5e5d93686860e5376416d3002658b50962',
+     armv7l: '55a7fe9d48b915e1ba79ea4651014b5e5d93686860e5376416d3002658b50962',
+     x86_64: 'd9b3f7941ea4441f06cc229acf07510ea157e7bb1703837fce3519a036abbb33'
+  })
+
+  depends_on 'gcc_lib' => :executable_only
+  depends_on 'glibc' => :executable_only
+  depends_on 'rust' => :build
+
+  print_source_bashrc
+
+  def self.build
+    File.write 'atuin.bash', <<~EOF
+      eval "$(atuin init bash)"
+    EOF
+  end
+
+  def self.install
+    Dir.chdir 'crates/atuin' do
+      system "cargo install --path . --root #{CREW_DEST_PREFIX}"
+    end
+    Dir.chdir CREW_DEST_PREFIX do
+      FileUtils.rm %w[.crates.toml .crates2.json]
+    end
+    FileUtils.install 'atuin.bash', "#{CREW_DEST_PREFIX}/etc/env.d/10-atuin", mode: 0o644
+  end
+end
