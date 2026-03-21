74e81c940b897621247f0223cd44f9506e948b61
Ed Reel <edreel@gmail.com>
2026-03-20 20:29:54 -0500

    Q 3.1.6 => 3.1.7 (#15090)

--- packages/q.rb
+++ packages/q.rb
@@ -1,12 +1,9 @@
-# Adapted from Arch Linux q PKGBUILD at:
-# https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=q
-
 require 'buildsystems/python'
 
 class Q < Python
   description 'SQL-like queries on tabular text data, including joins and subqueries'
   homepage 'https://github.com/harelba/q'
-  version '3.1.6'
+  version '3.1.7'
   license 'GPL3'
   compatibility 'x86_64'
   source_url 'https://github.com/harelba/q.git'
@@ -14,8 +11,8 @@ class Q < Python
   binary_compression 'tar.zst'
 
   binary_sha256({
-     x86_64: '126b2ae1d134132be5e94963e286dc14c33c141bc68892614283f853851aa064'
+    x86_64: '4a9a576af685b838f26a8f90b84f2648ff660b097c451a1f28a42b4f0e890121'
   })
 
-  depends_on 'python3'
+  depends_on 'python3' => :logical
 end
