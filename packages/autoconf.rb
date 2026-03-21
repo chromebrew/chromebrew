26a0a9ce311c34a7bc382ae0d943123d27e426e9
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-21 00:34:04 +0000

    autoconf -> 2.73 in updater-autoconf-2.73

--- packages/autoconf.rb
+++ packages/autoconf.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Autoconf < Autotools
   description 'Autoconf is an extensible package of M4 macros that produce shell scripts to automatically configure software source code packages.'
   homepage 'http://www.gnu.org/software/autoconf/'
-  version '2.72'
+  version '2.73'
   license 'GPL-3'
   compatibility 'all'
   source_url "https://ftp.gnu.org/gnu/autoconf/autoconf-#{version}.tar.xz"
-  source_sha256 'ba885c1319578d6c94d46e9b0dceb4014caafe2490e437a0dbca3f270a223f5a'
+  source_sha256 '9fd672b1c8425fac2fa67fa0477b990987268b90ff36d5f016dae57be0d6b52e'
   binary_compression 'tar.zst'
 
   binary_sha256({
