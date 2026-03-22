b42d416f434ebf4d22d9d048e1a0d1e483988e12
Ed Reel <edreel@gmail.com>
2026-03-22 18:36:45 +0000

    R 4.5.1-icu77.1 => 4.5.3-icu77.1

--- packages/r.rb
+++ packages/r.rb
@@ -3,44 +3,44 @@ require 'buildsystems/autotools'
 class R < Autotools
   description 'R is a free software environment for statistical computing and graphics.'
   homepage 'https://www.r-project.org/'
-  version "4.5.1-#{CREW_ICU_VER}"
+  version "4.5.3-#{CREW_ICU_VER}"
   license 'GPL-2 or GPL-3 and LGPL-2.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://cran.r-project.org/src/base/R-4/R-#{version.split('-')[0]}.tar.xz"
-  source_sha256 'b4cb675deaaeb7299d3b265d218cde43f192951ce5b89b7bb1a5148a36b2d94d'
+  source_sha256 'c9a6555bb94c104b9c02905360c57de0dcac5dd31923dc5bb6ef3af7744615f1'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'be5b9c300e23c4477b0d992bfbd2760086547dcac525b0c708c349650a54ae54',
-     armv7l: 'be5b9c300e23c4477b0d992bfbd2760086547dcac525b0c708c349650a54ae54',
-     x86_64: 'efd9537804839b891a5c4b93f0e1214f26542bbf46b522725cefa46702bf904c'
+    aarch64: '932ab1a7e6c328c49e1d6976f52820062a5f197ad26db6403f7353f234e39e5d',
+     armv7l: '932ab1a7e6c328c49e1d6976f52820062a5f197ad26db6403f7353f234e39e5d',
+     x86_64: '21fe2797731ed3ce5804e15020a7b5d6a89d6045f516b94b27ee4c49dc9a37d0'
   })
 
-  depends_on 'bzip2' # R
+  depends_on 'bzip2' => :library
   depends_on 'curl' => :build
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'glib' # R
-  depends_on 'icu4c' # R
-  depends_on 'lapack' # R
-  depends_on 'libdeflate' # R
-  depends_on 'libice' # R
-  depends_on 'libpng' # R
-  depends_on 'libsm' # R
-  depends_on 'libtiff' # R
-  depends_on 'libx11' # R
-  depends_on 'libxext' # R
-  depends_on 'libxmu' # R
-  depends_on 'libxss' # R
-  depends_on 'libxt' # R
-  depends_on 'pango' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'icu4c' => :library
+  depends_on 'lapack' => :library
+  depends_on 'libdeflate' => :library
+  depends_on 'libice' => :library
+  depends_on 'libpng' => :library
+  depends_on 'libsm' => :library
+  depends_on 'libtiff' => :library
+  depends_on 'libx11' => :library
+  depends_on 'libxext' => :library
+  depends_on 'libxmu' => :library
+  depends_on 'libxss' => :library
+  depends_on 'libxt' => :library
+  depends_on 'pango' => :library
   depends_on 'pcre2' => :build
-  depends_on 'tcl' # R
-  depends_on 'tk' # R
+  depends_on 'tcl' => :library
+  depends_on 'tk' => :library
   depends_on 'xdg_utils' => :build
-  depends_on 'xzutils' # R
-  depends_on 'zlib' # R
-  depends_on 'zstd' # R
+  depends_on 'xzutils' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
 
   autotools_configure_options '--enable-R-shlib \
            --with-x'
