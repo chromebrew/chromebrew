5a6220d4682fe1e24af23a43c6810b89f76ea932
Zopolis4 <creatorsmithmdt@gmail.com>
2026-03-24 10:43:33 +1100

    unicode-character-database 16.0.0 -> 17.0.0

--- packages/unicode_character_database.rb
+++ packages/unicode_character_database.rb
@@ -3,11 +3,11 @@ require 'package'
 class Unicode_character_database < Package
   description 'Unicode Character Database'
   homepage 'https://www.unicode.org/'
-  version '16.0.0'
+  version '17.0.0'
   license 'Unicode-DFS-2015'
   compatibility 'all'
-  source_url "https://www.unicode.org/Public/zipped/#{version}/UCD.zip"
-  source_sha256 'cb1c663d053926500cd501229736045752713a066bd75802098598b7a7056177'
+  source_url "https://www.unicode.org/Public/#{version}/ucd/UCD.zip"
+  source_sha256 '2066d1909b2ea93916ce092da1c0ee4808ea3ef8407c94b4f14f5b7eb263d28e'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -20,7 +20,7 @@ class Unicode_character_database < Package
   depends_on 'libarchive' => :build
 
   def self.build
-    system "curl -Ls https://www.unicode.org/Public/zipped/#{version}/Unihan.zip | bsdtar --no-same-owner --no-same-permissions -xf -"
+    system "curl -Ls https://www.unicode.org/Public/#{version}/ucd/Unihan.zip| bsdtar --no-same-owner --no-same-permissions -xf -"
   end
 
   def self.install
