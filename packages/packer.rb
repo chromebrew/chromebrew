0d987e4d9b16b48cc28172355aa1095dea79033d
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-27 00:42:25 +0000

    packer -> 1.15.1 in updater-packer-1.15.1

--- packages/packer.rb
+++ packages/packer.rb
@@ -3,7 +3,7 @@ require 'package'
 class Packer < Package
   description 'Packer is an open source tool for creating identical machine images for multiple platforms from a single source configuration.'
   homepage 'https://www.packer.io/'
-  version '1.15.0'
+  version '1.15.1'
   license 'Apache-2.0, BSD-2, BSD-4, MIT, MPL-2.0 and unicode'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Packer < Package
      x86_64: "https://releases.hashicorp.com/packer/#{version}/packer_#{version}_linux_amd64.zip"
   })
   source_sha256({
-    aarch64: '7d5ee19adc7659720f03363c27e914945204eadb633ad5a3c37a3719f20e6eee',
-     armv7l: '7d5ee19adc7659720f03363c27e914945204eadb633ad5a3c37a3719f20e6eee',
-       i686: '13c1a0cc1112807a6900d14c83f6d3ce79f9474eeab7a09031aba01cb68665f5',
-     x86_64: '2fd1149c5c6c7604ced64d7b56638af05f6b7ed3f6835182bc913ddaba1f16b8'
+    aarch64: 'b2f3b0d138f78c4ced18f63e92e78944f71466b2c23ee2c082d2dacf1f76391f',
+     armv7l: 'b2f3b0d138f78c4ced18f63e92e78944f71466b2c23ee2c082d2dacf1f76391f',
+       i686: 'c6a89998d0f87d60a43c110ba22c57d270b3a083ac480f9e0579b042ff15d2bc',
+     x86_64: '648d704bec73805a5508ed612185a9e5858ca7d57b730b88b1a54c3af3a91f5f'
   })
 
   no_compile_needed
