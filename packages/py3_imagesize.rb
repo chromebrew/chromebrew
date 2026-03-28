36f632b6d76b234333f095115ad4cad60c28969f
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-28 00:33:53 +0000

    py3_imagesize -> 2.0.0-py3.14 in updater-py3_imagesize-2.0.0-py3.14

--- packages/py3_imagesize.rb
+++ packages/py3_imagesize.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_imagesize < Pip
   description 'Imagesize gets image size from png/jpeg/jpeg2000/gif file.'
   homepage 'https://github.com/shibukawa/imagesize_py/'
-  version "1.4.1-#{CREW_PY_VER}"
+  version "2.0.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
