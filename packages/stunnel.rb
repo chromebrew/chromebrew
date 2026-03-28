7f46c4fef3dbbf7a63cf21345fa6b1ee0381af8d
Ed Reel <edreel@gmail.com>
2026-03-27 19:05:11 -0500

    Stunnel 5.75 => 5.77 (#15200)

--- packages/stunnel.rb
+++ packages/stunnel.rb
@@ -3,20 +3,26 @@ require 'buildsystems/autotools'
 class Stunnel < Autotools
   description "Stunnel is a proxy designed to add TLS encryption functionality to existing clients and servers without any changes in the programs' code."
   homepage 'https://www.stunnel.org/index.html'
-  version '5.75'
+  version '5.77'
   license 'GPL-2+'
   compatibility 'all'
   source_url "https://www.stunnel.org/downloads/stunnel-#{version}.tar.gz"
-  source_sha256 '0c1ef0ed85240974dccb94fe74fb92d6383474c7c0d10e8796d1f781a3ba5683'
+  source_sha256 'ec026f4fae4e0d25b940cc7a9451d925e359e7fd59e9edad20baea66ce45f263'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '75268660f310062a51be36c114a8bf658cae38fdf85d4a53f65430767283f6cd',
-     armv7l: '75268660f310062a51be36c114a8bf658cae38fdf85d4a53f65430767283f6cd',
-       i686: 'a910279777b46c6b2c85debfac69dc5e57e1a541e27b41653dba03bb63325bdb',
-     x86_64: 'b337876d9a7ac28168a07d7b5b3969dc2a08110ac3eca5363132942931afa538'
+    aarch64: '1ae4abb77054bd08932698e9b21644cb47465d210d69a0ddc96c4a510cc22915',
+     armv7l: '1ae4abb77054bd08932698e9b21644cb47465d210d69a0ddc96c4a510cc22915',
+       i686: 'de106a125e6086200e35deccb4760614484a54f30441c3b749163d72bac3a703',
+     x86_64: 'ca239963a16af6d9c4af777b6d5cc8a3e722c15e0324e8f6d68b056f7d2678ac'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'openssl'
+  depends_on 'glibc' => :library
+  depends_on 'openssl' => :executable
+  depends_on 'tcpwrappers' => :library
+
+  def self.postbuild
+    # Fix bash: /usr/local/bin/stunnel3: /usr/bin/perl: bad interpreter: No such file or directory
+    system "sed -i 's,/usr/bin/perl,#{CREW_PREFIX}/bin/perl,' #{CREW_DEST_PREFIX}/bin/stunnel3"
+  end
 end
