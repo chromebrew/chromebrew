6e0ffb21137c1da8a0936889ee5c19960c3adb71
Ed Reel <edreel@gmail.com>
2026-03-29 16:42:39 -0500

    Taglib 2.1.1 => 2.2.1 (#15282)

--- packages/taglib.rb
+++ packages/taglib.rb
@@ -3,18 +3,18 @@ require 'buildsystems/cmake'
 class Taglib < CMake
   description 'TagLib is a library for reading and editing the meta-data of several popular audio formats.'
   homepage 'https://taglib.org'
-  version '2.1.1'
+  version '2.2.1'
   license 'LGPL-2.1 and MPL-1.1'
   compatibility 'all'
   source_url "https://taglib.org/releases/taglib-#{version}.tar.gz"
-  source_sha256 '0de288d7fe34ba133199fd8512f19cc1100196826eafcb67a33b224ec3a59737'
+  source_sha256 '7e76b5299dcef427c486bffe455098470c8da91cf3ccb9ea804893df57389b5e'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '523fad65ddd68a04cbda6b4b93184981d498d92fff777d3659ed733904e92f82',
-     armv7l: '523fad65ddd68a04cbda6b4b93184981d498d92fff777d3659ed733904e92f82',
-       i686: 'ad18cefa364f4ec60badde0ce4b57723822ce592eddd8b47699f96e085144225',
-     x86_64: '88502332e2346e539fa019488622b44f3f3757e60a9c6bec9bcb64f2965f450b'
+    aarch64: '5e7c62cd76eb51e7e72e9ca2a51222f25d1ba0162279819c1bb217b8e3c1b682',
+     armv7l: '5e7c62cd76eb51e7e72e9ca2a51222f25d1ba0162279819c1bb217b8e3c1b682',
+       i686: '899332f0dd2d8c962fe8aa97af7cab8dda83ce131ae5f141fe9acf73e209e007',
+     x86_64: 'edda8b6900ca4bc2f56ebe0ab784596afbc8b77edc1a8540e8810c276df349c3'
   })
 
   depends_on 'boost' => :build
