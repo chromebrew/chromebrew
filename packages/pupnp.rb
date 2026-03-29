ca206dd2c8f4f59a1298c531556dd56c08105cdb
Zopolis4 <creatorsmithmdt@gmail.com>
2026-03-29 11:10:38 +1100

    Build pupnp for i686

--- packages/pupnp.rb
+++ packages/pupnp.rb
@@ -4,7 +4,7 @@ class Pupnp < CMake
   description 'PUPnP is the Portable SDK for UPnP devices.'
   homepage 'https://pupnp.github.io/pupnp/'
   version '1.18.4'
-  compatibility 'aarch64 armv7l x86_64'
+  compatibility 'all'
   license 'BSD-3'
   source_url 'https://github.com/pupnp/pupnp.git'
   git_hashtag "release-#{version}"
@@ -13,6 +13,7 @@ class Pupnp < CMake
   binary_sha256({
     aarch64: 'd0e35db6e0afa9d965dec8d063324dfd86b0a1f3d763718688c54480270da309',
      armv7l: 'd0e35db6e0afa9d965dec8d063324dfd86b0a1f3d763718688c54480270da309',
+       i686: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
      x86_64: 'e99762e6f47b368d9c27dc2bedf0fbec44826d260e66e9c1c0298227453594aa'
   })
 
@@ -20,6 +21,5 @@ class Pupnp < CMake
   depends_on 'gtest' => :build
 
   cmake_options "-DGTest_DIR=#{CREW_LIB_PREFIX}/cmake/GTest"
-
   run_tests
 end
