c5ff1509a716b6bda64f2ee41068a603e088116f
Ed Reel <edreel@gmail.com>
2026-03-30 04:51:58 +0000

    Tealdeer 1.8.0 => 1.8.1

--- packages/tealdeer.rb
+++ packages/tealdeer.rb
@@ -3,7 +3,7 @@ require 'buildsystems/rust'
 class Tealdeer < RUST
   description 'A very fast implementation of tldr in Rust.'
   homepage 'https://tealdeer-rs.github.io/tealdeer/'
-  version '1.8.0'
+  version '1.8.1'
   license 'Apache-2.0, MIT'
   compatibility 'all'
   source_url 'https://github.com/tealdeer-rs/tealdeer.git'
@@ -11,14 +11,14 @@ class Tealdeer < RUST
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '814f8deaec35a3c0947f320d9db6835cba9a189da48fc55db0201c0186d31ca8',
-     armv7l: '814f8deaec35a3c0947f320d9db6835cba9a189da48fc55db0201c0186d31ca8',
-       i686: '8f0b10662ebd2fcebb3d1ca3650ef5dc3299eb35b18e9d8c04a599139a6d332e',
-     x86_64: '8f116680132091b4a84646214cc639b28038c52dd9c2f79341f9b742abb5e812'
+    aarch64: 'e639ffb547ccbf4ee97270393458ba4b55ca688a729526d07991c241c8b00da8',
+     armv7l: 'e639ffb547ccbf4ee97270393458ba4b55ca688a729526d07991c241c8b00da8',
+       i686: '2e61fbdb1ed8a41706d8a59a669ed66fb50c68f54c88732226638c95d1bdbd24',
+     x86_64: '91d9929ed05d09459a77ac0ca6d459248d8acac04b0afb388d8c99ca06e0f32d'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :executable
+  depends_on 'glibc' => :executable
   depends_on 'rust' => :build
 
   conflicts_with 'tldr'
