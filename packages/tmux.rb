2946b93eb198e952ead686654f040efa5c4c2a51
Ed Reel <edreel@gmail.com>
2026-03-30 18:20:21 -0500

    Tmux 3.6 => 3.6a (#15312)

--- packages/tmux.rb
+++ packages/tmux.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Tmux < Autotools
   description 'tmux is a terminal multiplexer'
   homepage 'https://tmux.github.io/'
-  version '3.6'
+  version '3.6a'
   license 'ISC'
   compatibility 'all'
   source_url 'https://github.com/tmux/tmux.git'
@@ -11,13 +11,13 @@ class Tmux < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e8ef8141b84d05658b85c9a23e0c81f63ee90f9e755b7f68c15aebb5636b9be3',
-     armv7l: 'e8ef8141b84d05658b85c9a23e0c81f63ee90f9e755b7f68c15aebb5636b9be3',
-       i686: '0ac5848183706b175b40b33c9f3ffe51414f59b1dfc1a13b798752d710583510',
-     x86_64: '3f668409f0eab1ede9528edea34dbf5f4df62bd3247342d1e451f523b33790fa'
+    aarch64: '90b0ed451743c1bfb6a891042d56a85ddfe6f430a3c6510a771081dafaf2012d',
+     armv7l: '90b0ed451743c1bfb6a891042d56a85ddfe6f430a3c6510a771081dafaf2012d',
+       i686: '9ccb995cf5437c44cd6c46c24aef618e3e473d95d593e3c6659f44a56f4e7c97',
+     x86_64: 'e220309f4a78a9cd028861d3e7341087cbb4c09f401177adfccc643201d45594'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'libevent' # R
-  depends_on 'ncurses' # R
+  depends_on 'glibc' => :executable
+  depends_on 'libevent' => :executable
+  depends_on 'ncurses' => :executable
 end
