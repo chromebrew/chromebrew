ac172088658786e862887fe2136aecf595032c8a
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-03-30 00:35:56 +0000

    ruby_nokogiri -> 1.19.2-ruby4.0 in updater-ruby_nokogiri-1.19.2-ruby4.0

--- packages/ruby_nokogiri.rb
+++ packages/ruby_nokogiri.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_nokogiri < RUBY
   description 'Nokogiri (鋸) makes it easy and painless to work with XML and HTML from Ruby.'
   homepage 'https://nokogiri.org'
-  version "1.19.1-#{CREW_RUBY_VER}"
+  version "1.19.2-#{CREW_RUBY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
