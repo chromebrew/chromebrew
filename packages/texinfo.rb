b95195a6ac97f3b7843d08c784dd6c835c71b75e
Ed Reel <edreel@gmail.com>
2026-03-30 05:11:02 +0000

    Texinfo 7.2-perl5.42 => 7.3-perl5.42

--- packages/texinfo.rb
+++ packages/texinfo.rb
@@ -3,30 +3,30 @@ require 'buildsystems/autotools'
 class Texinfo < Autotools
   description 'Texinfo is the official documentation format of the GNU project.'
   homepage 'https://www.gnu.org/software/texinfo/'
-  version "7.2-#{CREW_PERL_VER}"
+  version "7.3-#{CREW_PERL_VER}"
   license 'GPL-3'
   compatibility 'all'
-  source_url "https://git.savannah.gnu.org/cgit/texinfo.git/snapshot/texinfo-#{version.split('-').first}.tar.gz"
-  source_sha256 'c4bc246447a9f81c5f578829713a48387b007ab01dbe2705fe24d105e2303693'
+  source_url "https://ftp.gnu.org/gnu/texinfo/texinfo-#{version.split('-')[0]}.tar.xz"
+  source_sha256 'e085acf253e094f159d7afab18cf740cf7a0c99d18d14c2d45ee221749bddf16'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'eeddad66dce11b52a37c3bde8f2b5ac74f406eba1f44f911b9ad9c8f4973fbcc',
-     armv7l: 'eeddad66dce11b52a37c3bde8f2b5ac74f406eba1f44f911b9ad9c8f4973fbcc',
-       i686: '20a9e39352127c908f03cc40c4eced6ce29b8e77e1bd8ac4ec74f26e0a667f47',
-     x86_64: '6304cda178394a4466c657be65ca517292606f482796510d51003fec6f212347'
+    aarch64: '6a00c0a5514556d1fa56e1e82bb85f2cff9923f7ccebb29e948ec8626f7de2f3',
+     armv7l: '6a00c0a5514556d1fa56e1e82bb85f2cff9923f7ccebb29e948ec8626f7de2f3',
+       i686: 'b0e4bd189c9c8a23306cc6005788563bc6f3bea5f49166441836988e31ace5b3',
+     x86_64: 'a9c4421a7cc4e6a4735d81a7bd74a0a6881ea68ebee4b1013dd2ea165e6eaa96'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'libunistring' # R
-  depends_on 'ncurses' # R
-  depends_on 'perl' # L
-  depends_on 'perl_libintl_perl' # L
-  depends_on 'perl_text_unidecode' # L
-  depends_on 'perl_unicode_eastasianwidth' # L
+  depends_on 'glibc' => :library
+  depends_on 'libunistring' => :library
+  depends_on 'ncurses' => :executable
+  depends_on 'perl' => :logical
+  depends_on 'perl_libintl_perl' => :logical
+  depends_on 'perl_text_unidecode' => :logical
+  depends_on 'perl_unicode_eastasianwidth' => :logical
 
   autotools_configure_options '--with-external-Text-Unidecode \
     --with-external-Unicode-EastAsianWidth'
 
-  # run_tests
+  run_tests
 end
