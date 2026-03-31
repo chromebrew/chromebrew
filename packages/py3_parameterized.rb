38a952f9bda0d2287e5cb0a7700f4c98685d3dc7
Zopolis4 <creatorsmithmdt@gmail.com>
2026-03-31 13:34:34 +1100

    Add py3-parameterized package

---
+++ packages/py3_parameterized.rb
@@ -0,0 +1,22 @@
+require 'buildsystems/pip'
+
+class Py3_parameterized < Pip
+  description 'Parameterized testing with any Python test framework'
+  homepage 'https://github.com/wolever/parameterized'
+  version "0.9.0-#{CREW_PY_VER}"
+  license 'BSD-2'
+  compatibility 'all'
+  source_url 'SKIP'
+  binary_compression 'tar.zst'
+
+  binary_sha256({
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+       i686: 'iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+  })
+
+  depends_on 'python3' => :logical
+
+  no_source_build
+end
