3aa76212acbc59d643ae69bfa882379931cd761a
Ed Reel <edreel@gmail.com>
2026-03-31 06:56:49 +0000

    Ugrep 7.4.0 => 7.6.0

--- packages/ugrep.rb
+++ packages/ugrep.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Ugrep < Autotools
   description 'A more powerful, ultra fast, user-friendly, compatible grep'
   homepage 'https://ugrep.com/'
-  version '7.4.0'
+  version '7.6.0'
   license 'BSD-3 Clause'
   compatibility 'x86_64'
   source_url 'https://github.com/Genivia/ugrep.git'
@@ -11,18 +11,18 @@ class Ugrep < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-     x86_64: '74ab6380ba186b270b8c3fd75f1c421ffd14cec7ef46932cb5c7c263a4349a14'
+    x86_64: 'db52769d9b3f6c63584b06b498aa9bfe41e3b875b1d4032cd2e1ad902e3cac95'
   })
 
-  depends_on 'brotli'
-  depends_on 'bzip2'
-  depends_on 'lz4'
-  depends_on 'pcre2'
-  depends_on 'xzutils'
-  depends_on 'zlib'
-  depends_on 'zstd'
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'brotli' => :executable
+  depends_on 'bzip2' => :executable
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'lz4' => :executable
+  depends_on 'pcre2' => :executable
+  depends_on 'xzutils' => :executable
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :executable
 
   def self.patch
     system 'autoreconf -f'
