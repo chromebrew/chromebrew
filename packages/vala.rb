ac78d29fe104cc38e91aa44c415892b069f8d287
Ed Reel <edreel@gmail.com>
2026-04-01 03:45:16 +0000

    Vala 0.56.18 => 0.56.19

--- packages/vala.rb
+++ packages/vala.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Vala < Autotools
   description 'Vala is a programming language that aims to bring modern programming language features to GNOME developers.'
   homepage 'https://wiki.gnome.org/Projects/Vala'
-  version '0.56.18'
+  version '0.56.19'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/vala.git'
@@ -11,16 +11,16 @@ class Vala < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6d503dc7421e47424458112704ce8e7597720fd5e9ca4fe7e017f27081e3c094',
-     armv7l: '6d503dc7421e47424458112704ce8e7597720fd5e9ca4fe7e017f27081e3c094',
-     x86_64: '98bf8c5cf34f6f564c90ed01b3a64218e48856aa6a853a3d9f161e1fd14ff3a9'
+    aarch64: '6c8eddb4171dd65fc5e32078bece8f3fe92722e57f3d039eb8b5365b1cd39722',
+     armv7l: '6c8eddb4171dd65fc5e32078bece8f3fe92722e57f3d039eb8b5365b1cd39722',
+     x86_64: 'fdf00195f4762f76bc242e0680f06e0fc56f07148089c98fff27919260961b15'
   })
 
   depends_on 'autoconf213' => :build
   depends_on 'autoconf_archive' => :build
   depends_on 'dbus' => :build
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
   depends_on 'gobject_introspection' => :build
   depends_on 'graphviz' => :build
   depends_on 'libxslt' => :build
