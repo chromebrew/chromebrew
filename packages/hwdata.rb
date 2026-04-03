c9fc99e4410f3e8e20715dde68726f426e4e3da6
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-03 00:39:49 +0000

    hwdata -> 0.406 in updater-hwdata-0.406

--- packages/hwdata.rb
+++ packages/hwdata.rb
@@ -6,7 +6,7 @@ require 'buildsystems/autotools'
 class Hwdata < Autotools
   description 'hardware identification databases'
   homepage 'https://github.com/vcrhonek/hwdata'
-  version '0.405'
+  version '0.406'
   license 'GPL2'
   compatibility 'all'
   source_url 'https://github.com/vcrhonek/hwdata.git'
