928f025b15769076a4a53428caf7929212c69949
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2026-04-03 15:36:13 -0400

    Harfbuzz => 14.0, freetype => 2.14.3 — freetype: 2.14.1 → 2.14.3,harfbuzz: 12.2.0-icu77.1 → 14.0.0-icu77.1 (#15386)
    * Harfbuzz => 14.0, freetype => 2.14.3
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Add builds
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/freetype.rb
+++ packages/freetype.rb
@@ -4,7 +4,7 @@ require 'buildsystems/meson'
 class Freetype < Meson
   description 'FreeType is a freely available software library to render fonts.'
   homepage 'https://freetype.org/'
-  version '2.14.1' # Update freetype in harfbuzz when updating freetype
+  version '2.14.3' # Update freetype in harfbuzz when updating freetype
   license 'FTL or GPL-2+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.freedesktop.org/freetype/freetype.git'
@@ -12,25 +12,25 @@ class Freetype < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2a4a2c88f5841abccd759c1a5868a82f58b9131c5d6db8f6d76cc371d423f4fb',
-     armv7l: '2a4a2c88f5841abccd759c1a5868a82f58b9131c5d6db8f6d76cc371d423f4fb',
-     x86_64: '8f4bc29413c19bf54e506f26b818856974d47051fc580de6c247ac0ed999b0d6'
+    aarch64: '377dbd0b0817a7fc2bf4c15274f25568f14458bf53fe28c5e2b47a21eebee0df',
+     armv7l: '377dbd0b0817a7fc2bf4c15274f25568f14458bf53fe28c5e2b47a21eebee0df',
+     x86_64: 'ba3713b19ac36215e5c251796b80b971163d11806d01ec382937d2445d8faae2'
   })
 
-  depends_on 'brotli' # R
-  depends_on 'bzip2' # R
+  depends_on 'brotli' => :library
+  depends_on 'bzip2' => :library
   depends_on 'expat' => :build
   depends_on 'gcc_lib' # R
   depends_on 'glib' => :build
-  depends_on 'glibc' # R
+  depends_on 'glibc' => :library
   depends_on 'graphite' => :build
-  depends_on 'harfbuzz' # R
-  depends_on 'libpng' # R
+  depends_on 'harfbuzz' => :logical
+  depends_on 'libpng' => :library
   depends_on 'pcre' => :build
   depends_on 'py3_docwriter' => :build
-  depends_on 'zlib' # R
+  depends_on 'zlib' => :library
 
   conflicts_ok # Allow overwriting of harfbuzz freetype files.
 
-  meson_options '-Dharfbuzz=enabled'
+  meson_options '-Dharfbuzz=dynamic'
 end
