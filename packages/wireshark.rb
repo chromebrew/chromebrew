346f2241d144aba941e112ba405992228f746629
Ed Reel <edreel@gmail.com>
2026-04-03 13:06:00 -0500

    Wireshark 4.4.9 => 4.6.4 (#15387)

--- packages/wireshark.rb
+++ packages/wireshark.rb
@@ -6,51 +6,53 @@ require 'buildsystems/cmake'
 class Wireshark < CMake
   description 'Network traffic and protocol analyzer/sniffer'
   homepage 'https://www.wireshark.org/'
-  version '4.4.9'
+  version '4.6.4'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/wireshark/wireshark.git'
-  git_hashtag "wireshark-#{version}"
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2d7c7e952bddde62f79e51841559421113504bd7141489f433ea7e46b664346d',
-     armv7l: '2d7c7e952bddde62f79e51841559421113504bd7141489f433ea7e46b664346d',
-       i686: 'b666f63531be99e1d7a4cbf305c4eb75859ae09bb7ffb08a3d0178a39986e31e',
-     x86_64: '28a157411c42b12d2681cb0d5b04efd40b5e4a2c0fe174a0c888caa77b62e8be'
+    aarch64: 'ffba04ac791f0a7b4996dab77c4e2eb3d2a2684c909f4d3bda4d20b678468f12',
+     armv7l: 'ffba04ac791f0a7b4996dab77c4e2eb3d2a2684c909f4d3bda4d20b678468f12',
+       i686: 'b93a72137a0eca33f03772d93c4536a06eb630692e56b55fcf2d90ab7f563ae8',
+     x86_64: 'b7903b1831d30ba4a5a8e0dc6fe4b0582b8b9e0055b8ca35d054331a21bee5eb'
   })
 
-  depends_on 'brotli' # R
-  depends_on 'c_ares' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
-  depends_on 'gnutls' # R
-  depends_on 'krb5' # R
-  depends_on 'libcap' # R
-  depends_on 'libgcrypt' # R
-  depends_on 'libmaxminddb' # R
-  depends_on 'libnghttp2' # R
-  depends_on 'libnl3' # R
-  depends_on 'libopencoreamr' # R
-  depends_on 'libpcap' # R
-  depends_on 'libsmi' # R
-  depends_on 'libssh' # R
-  depends_on 'libxml2' # R
-  depends_on 'lz4' # R
-  depends_on 'opus' # R
-  depends_on 'pcre2' # R
+  depends_on 'brotli' => :library
+  depends_on 'c_ares' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'gnutls' => :library
+  depends_on 'krb5' => :library
+  depends_on 'libcap' => :executable
+  depends_on 'libgcrypt' => :library
+  depends_on 'libmaxminddb' => :executable
+  depends_on 'libnghttp2' => :library
+  depends_on 'libnghttp3' => :library
+  depends_on 'libnl3' => :library
+  depends_on 'libopencoreamr' => :library
+  depends_on 'libpcap' => :executable
+  depends_on 'libsmi' => :library
+  depends_on 'libssh' => :library
+  depends_on 'libxml2' => :library
+  depends_on 'lz4' => :library
+  depends_on 'opus' => :library
+  depends_on 'pcre2' => :library
   depends_on 'qt5_base' unless ARCH == 'i686'
   depends_on 'qt5_multimedia' unless ARCH == 'i686'
   depends_on 'qt5_svg' unless ARCH == 'i686'
   depends_on 'qt5_tools' unless ARCH == 'i686'
   depends_on 'ruby_asciidoctor' => :build
-  depends_on 'sbc' # R
-  depends_on 'snappy' # R
-  depends_on 'speexdsp' # R
-  depends_on 'zlib' # R
-  depends_on 'zlib_ng' # R
-  depends_on 'zstd' # R
+  depends_on 'sbc' => :library
+  depends_on 'snappy' => :library
+  depends_on 'speexdsp' => :executable
+  depends_on 'xxhash' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zlib_ng' => :library
+  depends_on 'zstd' => :library
 
   git_fetchtags
 
