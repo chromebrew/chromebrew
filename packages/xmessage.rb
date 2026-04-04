18d1d1b14aa1fb36e8fd0dcb4ba7a45c98bfbfda
Ed Reel <edreel@gmail.com>
2026-04-04 17:04:48 -0500

    Xmessage 1.0.6 => 1.0.7 (#15399)

--- packages/xmessage.rb
+++ packages/xmessage.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Xmessage < Package
+class Xmessage < Autotools
   description 'Xmessage displays a message or query in a window.'
   homepage 'https://www.x.org/wiki/'
-  version '1.0.6'
+  version '1.0.7'
   license 'MIT-with-advertising'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.freedesktop.org/xorg/app/xmessage.git'
@@ -11,23 +11,13 @@ class Xmessage < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ea8d6e673d1f82e0aa006ee2bc504c6d8704490133e11740fe817cbc2bbe5ecc',
-     armv7l: 'ea8d6e673d1f82e0aa006ee2bc504c6d8704490133e11740fe817cbc2bbe5ecc',
-     x86_64: '97a8472c63527ae07a93370511899898d7b727669ad27b52a1123f5dc8fadc73'
+    aarch64: 'd94321b3643048f45bb7f6081a6b3062ff4022cf1c8a2f2105ce46899731c139',
+     armv7l: 'd94321b3643048f45bb7f6081a6b3062ff4022cf1c8a2f2105ce46899731c139',
+     x86_64: '325090fbac9447bc8665d39bf741a286a527d81ec02919d1bcc2c22cf6a37594'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'libx11' # R
-  depends_on 'libxaw' # R
-  depends_on 'libxt' # R
-
-  def self.build
-    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'glibc' => :library
+  depends_on 'libx11' => :library
+  depends_on 'libxaw' => :library
+  depends_on 'libxt' => :library
 end
