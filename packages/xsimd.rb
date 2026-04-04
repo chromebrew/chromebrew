bb0a74f89afb6a5ca974f2e921dbe1fee1854db4
Ed Reel <edreel@gmail.com>
2026-04-04 20:08:01 +0000

    Xsimd 13.2.0 => 14.1.0

--- packages/xsimd.rb
+++ packages/xsimd.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Xsimd < CMake
   description 'C++ wrappers for SIMD intrinsics'
   homepage 'https://github.com/xtensor-stack/xsimd'
-  version '13.2.0'
+  version '14.1.0'
   license 'BSD-3'
   compatibility 'all'
   source_url 'https://github.com/xtensor-stack/xsimd.git'
@@ -11,9 +11,9 @@ class Xsimd < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f9e142937891f08def9765129f4c44c5ebc6cea0fd21581b519556ad06e44378',
-     armv7l: 'f9e142937891f08def9765129f4c44c5ebc6cea0fd21581b519556ad06e44378',
-       i686: 'f43c484cefd93ed909690113dbe2aa162326aa2d0abfc36d9a8f978a3fafd991',
-     x86_64: 'c12d6d3ca99426f7f9a7dcd792260dad81e16b310a74f788868258f6c87db2b1'
+    aarch64: 'd497f81cdba24b0d1a98bb275751b70370a8982b064f05c37b618647b2fb0703',
+     armv7l: 'd497f81cdba24b0d1a98bb275751b70370a8982b064f05c37b618647b2fb0703',
+       i686: 'c2cd6ecf8e70943d6e6a906bd795ee5db3c2795728b8f5a82648243b41ff4628',
+     x86_64: 'c693e579fdd54118b4d118269a494cd96b70950ed19548ff84ff530dad11f109'
   })
 end
