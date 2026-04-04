7b4b2ad5b698159b3a1c02a49f5f9a172570a7c3
Ed Reel <edreel@gmail.com>
2026-04-04 18:43:11 +0000

    Xfsprogs 6.15.0-icu77.1 => 6.19.0-icu77.1

--- packages/xfsprogs.rb
+++ packages/xfsprogs.rb
@@ -3,7 +3,7 @@ require 'package'
 class Xfsprogs < Package
   description 'XFS filesystem utilities'
   homepage 'https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git/'
-  version "6.15.0-#{CREW_ICU_VER}"
+  version "6.19.0-#{CREW_ICU_VER}"
   license 'LGPL-2.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://git.kernel.org/pub/scm/fs/xfs/xfsprogs-dev.git'
@@ -11,20 +11,20 @@ class Xfsprogs < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '83a58ffab41c114b605ece09f1c3348a2f853b963053ece82f11e7893646b50c',
-     armv7l: '83a58ffab41c114b605ece09f1c3348a2f853b963053ece82f11e7893646b50c',
-     x86_64: '9fae8e4f8ed6aa2e03e5408bbe966b14eeb48198ad3e036975df9d1b66ce0900'
+    aarch64: '426217685ae0316b24e6611dfd3aaeef1a43acf89d7a589da226f827cc04925a',
+     armv7l: '426217685ae0316b24e6611dfd3aaeef1a43acf89d7a589da226f827cc04925a',
+     x86_64: '950bbf87240e82b35d880e047199cc9622d28c8eb4690cd254fe088b59c57e99'
   })
 
-  depends_on 'gcc_lib' # R
+  depends_on 'gcc_lib' => :library
   depends_on 'gettext' => :build
-  depends_on 'glibc' # R
-  depends_on 'icu4c' # R
-  depends_on 'libeconf' # R
-  depends_on 'libinih' # R
-  depends_on 'liburcu' # R
-  depends_on 'lvm2' # R
-  depends_on 'util_linux' # R
+  depends_on 'glibc' => :library
+  depends_on 'icu4c' => :library
+  depends_on 'libeconf' => :library
+  depends_on 'libinih' => :library
+  depends_on 'liburcu' => :library
+  depends_on 'lvm2' => :library
+  depends_on 'util_linux' => :executable
 
   def self.build
     system 'make configure'
