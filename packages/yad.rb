ee0d2057c583499e902960efa1570425a49a85d8
Ed Reel <edreel@gmail.com>
2026-04-04 20:50:40 +0000

    Yad 14.1 => 14.2

--- packages/yad.rb
+++ packages/yad.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Yad < Autotools
   description 'display graphical dialogs from shell scripts or command line.'
   homepage 'https://github.com/v1cont/yad'
-  version '14.1'
+  version '14.2'
   license 'GPL-3+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/v1cont/yad.git'
@@ -11,22 +11,22 @@ class Yad < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aed62ef4ab23878851f8cba414244b5ffbfb7afe1af0f85260a810bc8feafc0d',
-     armv7l: 'aed62ef4ab23878851f8cba414244b5ffbfb7afe1af0f85260a810bc8feafc0d',
-     x86_64: 'aac4bf54b984da9242c9570a2d8bc667b2a60aac93224022cee76aad2693de31'
+    aarch64: '1acf3efba04d8be0c28d3ae2b315267686968433e36a8979391101c61e615675',
+     armv7l: '1acf3efba04d8be0c28d3ae2b315267686968433e36a8979391101c61e615675',
+     x86_64: '0f5d4fb32a373699d3bd54242d37d63fee5bcb5233aefc04a616558835b6bbcf'
   })
 
-  depends_on 'at_spi2_core' # R
-  depends_on 'cairo' # R
-  depends_on 'gdk_pixbuf' # R
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
-  depends_on 'gtk3'
-  depends_on 'harfbuzz' # R
-  depends_on 'pango' # R
+  depends_on 'at_spi2_core' => :library
+  depends_on 'cairo' => :library
+  depends_on 'gdk_pixbuf' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'gtk3' => :library
+  depends_on 'harfbuzz' => :library
+  depends_on 'pango' => :library
   depends_on 'pkgconf' => :build
-  depends_on 'rgb'
-  depends_on 'zlib' # R
+  depends_on 'rgb' => :library
+  depends_on 'zlib' => :library
 
   autotools_pre_configure_options "PKG_CONFIG=#{CREW_PREFIX}/bin/pkgconf"
   autotools_configure_options "--enable-gio \
