73b7be2d84d785a277723039bc7b232f9d400a1b
Ed Reel <edreel@gmail.com>
2026-04-04 19:43:14 +0000

    Xprop 1.2.6 => 1.2.8

--- packages/xprop.rb
+++ packages/xprop.rb
@@ -1,9 +1,9 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Xprop < Package
+class Xprop < Autotools
   description 'Xprop is a property displayer for X.'
   homepage 'https://www.x.org/wiki/'
-  version '1.2.6'
+  version '1.2.8'
   license 'MIT-with-advertising and ISC'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.freedesktop.org/xorg/app/xprop.git'
@@ -11,21 +11,11 @@ class Xprop < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '29b6c1397a06c4b4691ffb044d6ad1e9ab6686b1c3b6c8dffbbb195a09499d38',
-     armv7l: '29b6c1397a06c4b4691ffb044d6ad1e9ab6686b1c3b6c8dffbbb195a09499d38',
-     x86_64: '0121b423cdce35f7dafd41a9aaf8199cd2d69eb3fda164c69058354f545e08c6'
+    aarch64: 'fa7f965ada21f80c281ef53ba828734bad11f86193e66a4afa3fd7614405d583',
+     armv7l: 'fa7f965ada21f80c281ef53ba828734bad11f86193e66a4afa3fd7614405d583',
+     x86_64: '9204c99c67b14ff58b687ed8e18be279a359b0433b3d37d5db1573adb1e5560b'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'libx11' # R
-
-  def self.build
-    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'glibc' => :library
+  depends_on 'libx11' => :library
 end
