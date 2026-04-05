36f544c178482a358e126b48db689c250ebd21d6
Ed Reel <edreel@gmail.com>
2026-04-05 06:37:29 -0500

    Zoneinfo 2025c => 2026a (#15424)

--- packages/zoneinfo.rb
+++ packages/zoneinfo.rb
@@ -3,23 +3,21 @@ require 'package'
 class Zoneinfo < Package
   description 'Code and data that represent the history of local time for many representative locations around the globe.'
   homepage 'https://www.iana.org/time-zones'
-  version '2025c'
+  version '2026a'
   license 'public-domain'
   compatibility 'all'
   source_url "https://data.iana.org/time-zones/releases/tzdb-#{version}.tar.lz"
-  source_sha256 'fbe5b52a151c992c1aeb49bc6ca41e170ca9f8d3fb810ec459eeb79c82d6972b'
+  source_sha256 '0913509a37f26b81bb6396018ad5cdf32065374ed36e82cceb61b2ee57a94b7c'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1e6e4630f0cbd342ccd5131ecc9a52cb6384e6a00098ef55f5c539f2800d5a73',
-     armv7l: '1e6e4630f0cbd342ccd5131ecc9a52cb6384e6a00098ef55f5c539f2800d5a73',
-       i686: 'ab199299d95a1d8d8b37121debf3ecd35aef8045d2c6b5186318331ce5864863',
-     x86_64: 'e273aef17205ded1ffdf1ba596c3856b7804231528e34fcce45ef0450df2b506'
+    aarch64: '3bd3779f7cf7ccc75005ae17c08e09b12c7d72bb4287ecc7e89b0c0723625fdb',
+     armv7l: '3bd3779f7cf7ccc75005ae17c08e09b12c7d72bb4287ecc7e89b0c0723625fdb',
+       i686: 'bdc41a48fee778d6395daf9bd8fd92070f39262705e96b22a7c22adf5b241c2a',
+     x86_64: '92c0953051b5ba5c17154b7e02aae6cd08e824a3799191db18ba8f94e7adc6af'
   })
 
-  depends_on 'glibc' => :executable
-
-  conflicts_ok # Conflicts with coreutils.
+  depends_on 'glibc' => :library
 
   def self.patch
     system "sed -i 's:TZDEFAULT = $(TOPDIR)/etc/localtime:TZDEFAULT = #{CREW_PREFIX}/etc/localtime:g' Makefile"
@@ -33,9 +31,11 @@ class Zoneinfo < Package
   end
 
   def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'INSTALL'
+    system 'make', "LIBDIR=#{CREW_LIB_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'INSTALL'
+    FileUtils.mv "#{CREW_DEST_PREFIX}/bin/date", "#{CREW_DEST_PREFIX}/bin/zoneinfo_date"
     FileUtils.mv "#{CREW_DEST_PREFIX}/bin/tzselect", "#{CREW_DEST_PREFIX}/bin/zoneinfo_tzselect"
     FileUtils.mv "#{CREW_DEST_PREFIX}/sbin/zic", "#{CREW_DEST_PREFIX}/sbin/zoneinfo_zic"
+    FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man1/date.1", "#{CREW_DEST_MAN_PREFIX}/man1/zoneinfo_date.1"
     FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man5/tzfile.5", "#{CREW_DEST_MAN_PREFIX}/man5/zoneinfo_tzfile.5"
     FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man8/zdump.8", "#{CREW_DEST_MAN_PREFIX}/man8/zoneinfo_zdump.8"
     FileUtils.mv "#{CREW_DEST_MAN_PREFIX}/man8/zic.8", "#{CREW_DEST_MAN_PREFIX}/man8/zoneinfo_zic.8"
