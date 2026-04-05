f9fca2010f55f706a8abf064fdbba2ae2a6c6295
Ed Reel <edreel@gmail.com>
2026-04-05 05:22:34 +0000

    Zenity 4.0.1 => 4.2.2

--- packages/zenity.rb
+++ packages/zenity.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Zenity < Meson
   description 'A tool that allows you to display GTK dialog boxes in commandline and shell scripts.'
   homepage 'https://wiki.gnome.org/Projects/Zenity'
-  version '4.0.1'
+  version '4.2.2'
   license 'GPL-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/zenity.git'
@@ -11,18 +11,19 @@ class Zenity < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '223b0b0ebfe52714948e28749002250c84bf73ec1db0906e4572583d2f9e0287',
-     armv7l: '223b0b0ebfe52714948e28749002250c84bf73ec1db0906e4572583d2f9e0287',
-     x86_64: 'c461828c5bfa673184b08ace17c1a0fc525ee85bcabe772c5e8c880213778f30'
+    aarch64: 'ad538ff18a60e55cae608b42bd3ce92b1697a003220a9baeb2534c78c6575a97',
+     armv7l: 'ad538ff18a60e55cae608b42bd3ce92b1697a003220a9baeb2534c78c6575a97',
+     x86_64: 'cef68f9e259ac5a704ae28496ba4c2eed4ab42e08fb695731ef01a044cea5d43'
   })
 
   depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'glib' # R
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
   depends_on 'gtk3' => :build
-  depends_on 'gtk4' # R
-  depends_on 'libadwaita' # R
-  depends_on 'pango' # R
+  depends_on 'gtk4' => :library
+  depends_on 'libadwaita' => :library
+  depends_on 'pango' => :library
+  depends_on 'py3_itstool' => :build
   depends_on 'webkit2gtk' => :build
 
   gnome
