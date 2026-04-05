558ce44b395d08fee33a47535dc0267203ac06d3
Ed Reel <edreel@gmail.com>
2026-04-05 06:33:57 -0500

    Zola 0.21.0 => 0.22.1 (#15423)

--- packages/zola.rb
+++ packages/zola.rb
@@ -3,12 +3,12 @@ require 'package'
 class Zola < Package
   description 'A fast static site generator'
   homepage 'https://www.getzola.org/'
-  version '0.21.0'
+  version '0.22.1'
   license 'MIT'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/getzola/zola/releases/download/v#{version}/zola-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-  source_sha256 '5c37a8f706567d6cad3f0dbc0eaebe3b9591cc301bd67089e5ddc0d0401732d6'
+  source_sha256 '0ca09aa40376aaa9ddfb512ff9ad963262ef95edb0d0f2d5ec6961b6f5cf22ef'
 
   no_compile_needed
 
