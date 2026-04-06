8905b32a1897214cb22b02626a6fdbae554d10cd
Ed Reel <edreel@gmail.com>
2026-04-06 04:33:41 +0000

    Abcl 1.9.3 => 1.10.0

--- packages/abcl.rb
+++ packages/abcl.rb
@@ -3,22 +3,21 @@ require 'package'
 class Abcl < Package
   description 'Armed Bear Common Lisp (ABCL) is a full implementation of the Common Lisp language featuring both an interpreter and a compiler, running in the JVM.'
   homepage 'https://abcl.org/'
-  version '1.9.3'
+  version '1.10.0'
   license 'GPL-2'
   compatibility 'all'
   source_url "https://abcl.org/releases/#{version}/abcl-bin-#{version}.tar.gz"
-  source_sha256 'dbf7fc10cf80657cf9bca7d349020fd021b92d6294bbd74b111873ccb7b2134e'
+  source_sha256 'db6ed25fe4f3a35d44b4df4f4eee622c9b0e0e0fea45314550c0ae3b6f6d72f3'
 
   depends_on 'openjdk17'
 
   no_compile_needed
 
   def self.build
-    abcl = <<~EOF
+    File.write 'abcl.sh', <<~EOF
       #!/bin/bash
       java -jar #{CREW_PREFIX}/share/abcl/abcl.jar
     EOF
-    File.write('abcl.sh', abcl)
   end
 
   def self.install
