1af23dbfedeed693397b6b6553d64e2b9218658b
Ed Reel <edreel@gmail.com>
2026-04-06 14:50:14 -0500

    Aribb24 1.0.3 => 1.0.4

--- packages/aribb24.rb
+++ packages/aribb24.rb
@@ -2,23 +2,23 @@ require 'buildsystems/autotools'
 
 class Aribb24 < Autotools
   description 'aribb24 is a basic implementation of the ARIB STD-B24 public standard.'
-  homepage 'https://github.com/nkoriyama/aribb24/'
-  @_ver = '1.0.3'
-  version "#{@_ver}-1"
+  homepage 'https://code.videolan.org/jeeb/aribb24'
+  version '1.0.4'
   compatibility 'all'
   license 'LGPL-3'
-  source_url 'https://github.com/nkoriyama/aribb24.git'
-  git_hashtag "v#{@_ver}"
+  source_url 'https://code.videolan.org/jeeb/aribb24.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '80476e675586eeb7e06b18f29d716ebdb92cad48ba9f7cc1dbdec384f6eec99c',
-     armv7l: '80476e675586eeb7e06b18f29d716ebdb92cad48ba9f7cc1dbdec384f6eec99c',
-       i686: '1065aa544418fbd6ea3bf3306fa9195cf98cfd904df01a8b438f58d719074df0',
-     x86_64: '6fb662a2d4d31161b0c555f183256f557af7cbd44e31b3de0ab8fe975cbea48f'
+    aarch64: 'f8fd427ec1d1dab49ce236e73c81c07f5f63fa983ba35ed23bc822343864bf5d',
+     armv7l: 'f8fd427ec1d1dab49ce236e73c81c07f5f63fa983ba35ed23bc822343864bf5d',
+       i686: '4e43b212af800a2d6ade6e1a6f98529b9539a982535a511dfb13e53e4d8349f9',
+     x86_64: 'c992f12f2b2e99105ed698d23d7d5eb45d1d825e592a3a26e5994a8c96e86ff3'
   })
 
-  depends_on 'libpng'
+  depends_on 'glibc' => :library
+  depends_on 'libpng' => :library
 
   run_tests
 end
