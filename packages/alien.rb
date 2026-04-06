f3af6ea81171563ffb5ed8a0afe3a602f145dc07
Ed Reel <edreel@gmail.com>
2026-04-06 05:59:28 -0500

    Alien 8.95.8 => 8.95.9 (#15436)

--- packages/alien.rb
+++ packages/alien.rb
@@ -1,28 +1,21 @@
-require 'package'
+require 'buildsystems/perl'
 
-class Alien < Package
+class Alien < PERL
   description 'This program converts Linux packages between the rpm, deb, tgz and slp packages.'
   homepage 'https://sourceforge.net/projects/alien-pkg-convert/'
-  version '8.95.8'
+  version '8.95.9'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'https://deb.debian.org/debian/pool/main/a/alien/alien_8.95.8.tar.xz'
-  source_sha256 '586a649bc9366acc15047d4c9f34e253208907142e12174eafb4f3704fea47a5'
+  source_url 'https://github.com/Project-OSS-Revival/alien.git'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a795208f4fc7e2de0548a043945d42196359b56dc4f4e891ec901a98e773d4d6',
-     armv7l: 'a795208f4fc7e2de0548a043945d42196359b56dc4f4e891ec901a98e773d4d6',
-       i686: 'a910d11ebeb7b2dee53fdc567be042b41734b09b7bc237274c90745e6e6cfc3d',
-     x86_64: 'bef7236785ff5a65dcae3644e90ef1a63a36388ee0759890e307452ea5c54b78'
+    aarch64: 'ea65282e62f9b5817c40ee89ed7acc9bc81153a05e3a8f3fd21f490a0a06bb99',
+     armv7l: 'ea65282e62f9b5817c40ee89ed7acc9bc81153a05e3a8f3fd21f490a0a06bb99',
+       i686: 'c9b8bb2bf420c7b32306c28f0b3a51c457794f41f1b462b923634e7068853a4a',
+     x86_64: 'a5d99661630a80f99e3dbd6cce40dc6b683ef7e178afa36bf09b8a0207066835'
   })
 
-  def self.build
-    system 'perl Makefile.PL'
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'perl' => :logical
 end
