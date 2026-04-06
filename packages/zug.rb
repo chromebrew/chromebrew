265ae6a01a00bf4cfc765cde443d2bb674891366
Ed Reel <edreel@gmail.com>
2026-04-05 23:24:07 -0500

    Zug 0.1.1 => 0.1.2 (#15431)

--- packages/zug.rb
+++ packages/zug.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Zug < CMake
   description 'Transducers for C++'
   homepage 'https://github.com/arximboldi/zug'
-  version '0.1.1'
+  version '0.1.2'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://github.com/arximboldi/zug.git'
@@ -11,10 +11,10 @@ class Zug < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e8fa6cc5881b5e7d7a396cbfa25d67620b21801f2a1832b29a3b8c37ad70d86c',
-     armv7l: 'e8fa6cc5881b5e7d7a396cbfa25d67620b21801f2a1832b29a3b8c37ad70d86c',
-       i686: '47504fd456582c53a1e08b9dd4f74dcb8a828cd1926733d50b426f214074a0d8',
-     x86_64: 'dfaf133c57e60c8218cd3738fd3a5faa3896883ec6c56c38bd466f3cc22e6455'
+    aarch64: '1079c96d027728edd61760520d7163af033828132779114969f0ddadb7c6f6f6',
+     armv7l: '1079c96d027728edd61760520d7163af033828132779114969f0ddadb7c6f6f6',
+       i686: 'eb5c43b689efb1632813e3abd8b4bb89729b120d98a8a1852098999c173b52c1',
+     x86_64: '0c81c1bacee34b675ce8384a3eeefe067c0294d046cb7cd9e389813adb1e0faf'
   })
 
   cmake_options '-Dzug_BUILD_EXAMPLES=OFF -Dzug_BUILD_TESTS=OFF'
