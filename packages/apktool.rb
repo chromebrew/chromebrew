df4f60a7529c2fa37b6630ba2a9837300238e561
Ed Reel <edreel@gmail.com>
2026-04-06 06:40:10 +0000

    Apktool 2.12.1 => 3.0.1

--- packages/apktool.rb
+++ packages/apktool.rb
@@ -3,19 +3,19 @@ require 'package'
 class Apktool < Package
   description 'A tool for reverse engineering Android apk files'
   homepage 'https://apktool.org/'
-  version '2.12.1'
+  version '3.0.1'
   license 'Apache-2.0'
   compatibility 'all'
   source_url "https://raw.githubusercontent.com/iBotPeaches/Apktool/v#{version}/scripts/linux/apktool"
   source_sha256 '493a9c98df968a25c373ca9df871ca77730c82954be4959b74e90adaf91a0e77'
 
-  depends_on 'openjdk8'
+  depends_on 'openjdk17'
 
   no_compile_needed
 
   def self.install
     downloader "https://github.com/iBotPeaches/Apktool/releases/download/v#{version}/apktool_#{version}.jar",
-               '66cf4524a4a45a7f56567d08b2c9b6ec237bcdd78cee69fd4a59c8a0243aeafa', 'apktool.jar'
+               'b947b945b4bc455609ba768d071b64d9e63834079898dbaae15b67bf03bcd362', 'apktool.jar'
     FileUtils.install 'apktool.jar', "#{CREW_DEST_PREFIX}/bin/apktool.jar", mode: 0o644
     FileUtils.install 'apktool', "#{CREW_DEST_PREFIX}/bin/apktool", mode: 0o755
   end
