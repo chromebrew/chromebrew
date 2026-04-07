12fe2110d73ef8e76fe2382fc86bc856a4b40da0
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-07 00:43:10 +0000

    units -> 2.27 in updater-units-2.27

--- packages/units.rb
+++ packages/units.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Units < Autotools
   description 'GNU Units converts quantities expressed in various systems of measurement to their equivalents in other systems of measurement.'
   homepage 'https://www.gnu.org/software/units/units.html'
-  version '2.26'
+  version '2.27'
   license 'FDL-1.3 and GPL-3'
   compatibility 'all'
   source_url "https://ftp.gnu.org/gnu/units/units-#{version}.tar.gz"
-  source_sha256 '4c43f7a49fe2212ee433d3c0755a0a1935db35497c4a56bf9f68c5f718873c54'
+  source_sha256 'e1bbdb09672e7c08eee986749e7a1629eb84a6bdf41f5a2a79d6804444abbe10'
   binary_compression 'tar.zst'
 
   no_fhs
