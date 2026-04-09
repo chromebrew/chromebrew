eb8673fa1a1acd241d70e44a9dfe5eddd3af6e9d
Ed Reel <edreel@gmail.com>
2026-04-09 18:37:58 +0000

    Boost 1.85.0-icu77.1-py3.14 => 1.90.0-icu77.1-py3.14

--- packages/boost.rb
+++ packages/boost.rb
@@ -3,11 +3,11 @@ require 'package'
 class Boost < Package
   description 'Boost provides free peer-reviewed portable C++ source libraries.'
   homepage 'https://www.boost.org/'
-  version "1.85.0-#{CREW_ICU_VER}-#{CREW_PY_VER}"
+  version "1.90.0-#{CREW_ICU_VER}-#{CREW_PY_VER}"
   license 'Boost-1.0'
   compatibility 'all'
-  source_url "https://archives.boost.io/release/#{version.split('-').first}/source/boost_#{version.split('-').first.gsub('.', '_')}.tar.bz2"
-  source_sha256 '7009fe1faa1697476bdc7027703a2badb84e849b7b0baad5086b087b971f8617'
+  source_url "https://archives.boost.io/release/#{version.split('-')[0]}/source/boost_#{version.split('-')[0].gsub('.', '_')}.tar.bz2"
+  source_sha256 '49551aff3b22cbc5c5a9ed3dbc92f0e23ea50a0f7325b0d198b705e8ee3fc305'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -17,14 +17,14 @@ class Boost < Package
      x86_64: 'eec7fe6a9c7bed95592c758df739230383d2c5913085f709f42aa82d19d2e2cf'
   })
 
-  depends_on 'bzip2' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'icu4c' # R
+  depends_on 'bzip2' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'icu4c' => :library
   depends_on 'python3' => :build
-  depends_on 'xzutils' # R
-  depends_on 'zlib' # R
-  depends_on 'zstd' # R
+  depends_on 'xzutils' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
 
   def self.build
     system './bootstrap.sh'
