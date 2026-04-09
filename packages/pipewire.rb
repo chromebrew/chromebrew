b173eb41f542fd0499a1677b9f519f43c11f0e4d
Satadru Pramanik <satadru@gmail.com>
2026-04-09 12:08:40 -0400

    Add c11threads package, update pipewire to work with older glibc.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/pipewire.rb
+++ packages/pipewire.rb
@@ -3,64 +3,50 @@ require 'buildsystems/meson'
 class Pipewire < Meson
   description 'PipeWire is a project that aims to greatly improve handling of audio and video under Linux.'
   homepage 'https://pipewire.org'
-  if Gem::Version.new(CREW_KERNEL_VERSION.to_s) < Gem::Version.new('3.9')
-    version '0.3.29'
-  elsif Gem::Version.new(CREW_KERNEL_VERSION.to_s) <= Gem::Version.new('5.4') || Gem::Version.new(LIBC_VERSION) < Gem::Version.new('2.32')
-    version '0.3.60'
-  else
-    version '1.2.6'
-    min_glibc '2.32'
-  end
+  version '1.6.3'
   compatibility 'aarch64 armv7l x86_64'
   license 'LGPL-2.1+'
   source_url 'https://gitlab.freedesktop.org/pipewire/pipewire.git'
   git_hashtag version
   binary_compression 'tar.zst'
 
-  if Gem::Version.new(CREW_KERNEL_VERSION.to_s) < Gem::Version.new('3.9')
+  binary_sha256({
+    aarch64: '7d5370bb611e801e75e7e3364589fe9d645e4d2a8455dac1e54da006612ce868',
+     armv7l: '7d5370bb611e801e75e7e3364589fe9d645e4d2a8455dac1e54da006612ce868',
+     x86_64: '1e6691d5a66b5004e597ca2f28f2b47ce292aa08da8e44a789b224c8f474c906'
+  })
 
-    binary_sha256({
-         i686: '0dbeda58c4e1db7a180ebfb2b7bc3057cc6966927f4d5ee543953b734dfc4510'
-    })
-  elsif Gem::Version.new(CREW_KERNEL_VERSION.to_s) <= Gem::Version.new('5.4') || Gem::Version.new(LIBC_VERSION) < Gem::Version.new('2.32')
-    binary_sha256({
-      aarch64: '237ad8299b16e6d294a6561a4959efb47fc72ee66d06f51a3863f55dbdedcf78',
-       armv7l: '237ad8299b16e6d294a6561a4959efb47fc72ee66d06f51a3863f55dbdedcf78',
-       x86_64: '1534c6a7d71870ac60ec77aab0f795e148e63cf2eac61ff6ec58a5d3af23d994'
-    })
-  else
-
-    binary_sha256({
-      aarch64: 'fdacbfe0ebfcf7af8d3d3acc6b895fed672a53d0245739edbd64671a0e95c8c5',
-       armv7l: 'fdacbfe0ebfcf7af8d3d3acc6b895fed672a53d0245739edbd64671a0e95c8c5',
-       x86_64: '6c5363156c2d4251c9a1b604e8961267ce0e0a38554f87faa6766060e9203224'
-    })
-  end
-
-  # depends_on 'webrtc_audio_processing' # R
-  depends_on 'alsa_lib' # R
+  depends_on 'alsa_lib' => :library
   depends_on 'alsa_plugins' => :build
-  depends_on 'avahi' # R
+  depends_on 'avahi' => :library
   depends_on 'ca_certificates' => :build
-  depends_on 'dbus' # R
-  depends_on 'elogind' # R
-  depends_on 'eudev' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'glib' # R
+  depends_on 'dbus' => :library
+  depends_on 'elogind' => :build
+  depends_on 'eudev' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'glib' => :library
+  depends_on 'glib_stub' => :library
+  depends_on 'glibc' => :library
   depends_on 'gsettings_desktop_schemas' => :build
+  depends_on 'gstreamer' => :library
   depends_on 'gstreamer' => :logical
-  depends_on 'jack' # R
-  depends_on 'libdrm' # R
-  depends_on 'libsndfile' # R
-  depends_on 'lilv' # R
-  depends_on 'ncurses' # R
-  depends_on 'openssl' # R
-  depends_on 'opus' # R
-  depends_on 'pulseaudio' # R
+  depends_on 'jack' => :library
+  depends_on 'jack1' => :library
+  depends_on 'libdrm' => :library
+  depends_on 'libmysofa' => :library
+  depends_on 'libsndfile' => :library
+  depends_on 'libudev_stub' => :library
+  depends_on 'lilv' => :library
+  depends_on 'ncurses' => :executable
+  depends_on 'openssl' => :library
+  depends_on 'opus' => :library
+  depends_on 'pulseaudio' => :library
   depends_on 'py3_lxml' => :build
-  depends_on 'readline' # R
+  depends_on 'readline' => :executable
+  depends_on 'vdev' => :library
   depends_on 'vulkan_headers' => :build
-  depends_on 'vulkan_icd_loader' # R
+  depends_on 'vulkan_icd_loader' => :library
+  depends_on 'webrtc_audio_processing' => :library
 
   def self.prebuild
     # Without running the ca_certificates postinstall armv7l build breaks
@@ -68,11 +54,13 @@ class Pipewire < Meson
     system "#{CREW_PREFIX}/bin/update-ca-certificates --fresh --certsconf #{CREW_PREFIX}/etc/ca-certificates.conf"
   end
 
-  pre_meson_options "LDFLAGS='#{CREW_LINKER_FLAGS} -pthread'"
+  # -lpthread is needed for pre-Glibc 2.30 with the c11threads package.
+  pre_meson_options "CC='gcc -lpthread' CXX='g++ -lpthread' LDFLAGS='#{CREW_LINKER_FLAGS} -pthread'"
   meson_options "-Dbluez5-backend-hsphfpd=disabled \
       -Dbluez5-backend-ofono=disabled \
       -Dbluez5=disabled \
       -Dexamples=disabled \
+      -Dsession-managers=[] \
       -Dtest=disabled \
       -Dudevrulesdir=#{CREW_PREFIX}/etc/udev/rules.d \
       -Dv4l2=disabled \
