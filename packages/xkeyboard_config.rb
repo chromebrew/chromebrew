30cf18edb52293bf33d99616151153a125ef5e4a
Zopolis4 <creatorsmithmdt@gmail.com>
2026-04-09 12:41:33 +1000

    Rebuild xkeyboard-config

--- packages/xkeyboard_config.rb
+++ packages/xkeyboard_config.rb
@@ -3,17 +3,17 @@ require 'buildsystems/meson'
 class Xkeyboard_config < Meson
   description 'The non-arch keyboard configuration database for X Window.'
   homepage 'https://www.freedesktop.org/wiki/Software/XKeyboardConfig/'
-  version '2.47-1'
+  version '2.47'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.freedesktop.org/xkeyboard-config/xkeyboard-config.git'
-  git_hashtag "xkeyboard-config-#{version.split('-').first}"
+  git_hashtag "xkeyboard-config-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f96d53d29a55c9f0b46cb1e1e2891dc6d7ce46f6c0be8fe91f7179a6eaf13a39',
-     armv7l: 'f96d53d29a55c9f0b46cb1e1e2891dc6d7ce46f6c0be8fe91f7179a6eaf13a39',
-     x86_64: 'a8ec9dad6dc2a7611981b01b4beee2b7c03e021f0cb774d14a89a846bbe2f056'
+    aarch64: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     armv7l: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+     x86_64: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
   })
 
   depends_on 'libx11' => :build
