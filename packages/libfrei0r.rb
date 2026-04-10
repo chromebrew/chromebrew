98400a8571ba5c7ac48170718880a86a515e85c2
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-10 00:41:27 +0000

    libfrei0r -> 3.0.0 in updater-libfrei0r-3.0.0

--- packages/libfrei0r.rb
+++ packages/libfrei0r.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Libfrei0r < CMake
   description 'Minimalistic API for a collection of free video effect plugins.'
   homepage 'https://frei0r.dyne.org/'
-  version '2.5.6'
+  version '3.0.0'
   license 'GPL-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/dyne/frei0r.git'
