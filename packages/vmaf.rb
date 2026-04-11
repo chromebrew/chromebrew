e63f4cad7f196b5fdd62b2030a84b0b928212ec2
Zopolis4 <creatorsmithmdt@gmail.com>
2026-04-11 21:45:56 +1000

    vmaf 3.0.0 -> 3.1.0

--- packages/vmaf.rb
+++ packages/vmaf.rb
@@ -1,13 +1,13 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Vmaf < Package
+class Vmaf < Meson
   description 'Perceptual video quality assessment algorithm based on multi-method fusion'
-  homepage 'https://github.com/Netflix/vmaf/'
-  version '3.0.0'
+  homepage 'https://github.com/Netflix/vmaf'
+  version '3.1.0'
   license 'BSD-2'
   compatibility 'all'
-  source_url "https://github.com/Netflix/vmaf/archive/v#{version}.tar.gz"
-  source_sha256 '7178c4833639e6b989ecae73131d02f70735fdb3fc2c7d84bc36c9c3461d93b1'
+  source_url 'https://github.com/Netflix/vmaf.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -19,24 +19,9 @@ class Vmaf < Package
 
   depends_on 'gcc_lib' # R
   depends_on 'glibc' # R
-  depends_on 'nasm' => :build
+  depends_on 'nasm' => :build if %w[x86_64 i686].include?(ARCH)
 
-  def self.build
-    @avx512 = ARCH == 'x86_64' ? 'true' : 'false'
-
-    Dir.chdir 'libvmaf' do
-      system "meson setup #{CREW_MESON_OPTIONS} \
-      -Denable_docs=false \
-      -Denable_tests=false \
-      -Denable_avx512=#{@avx512} \
-      -Denable_float=true \
-      builddir"
-    end
-    system 'meson configure libvmaf/builddir'
-    system 'ninja -C libvmaf/builddir'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C libvmaf/builddir install"
-  end
+  meson_build_relative_dir 'libvmaf'
+  # vmaf currently fails to build with default configuration in i686: https://github.com/Netflix/vmaf/issues/1481
+  meson_options "-Denable_docs=false -Denable_tests=false -Denable_float=true #{'-Denable_asm=false' if ARCH == 'i686'}"
 end
