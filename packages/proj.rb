3fdb82fc6443e7b0cf04bd86998b8e686eba0cf0
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-11 00:43:54 +0000

    proj -> 9.8.1 in updater-proj-9.8.1

--- packages/proj.rb
+++ packages/proj.rb
@@ -3,11 +3,11 @@ require 'buildsystems/cmake'
 class Proj < CMake
   description 'PROJ is a generic coordinate transformation software that transforms geospatial coordinates from one coordinate reference system (CRS) to another.'
   homepage 'https://proj.org'
-  version '9.8.0'
+  version '9.8.1'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://download.osgeo.org/proj/proj-#{version}.tar.gz"
-  source_sha256 'a8395f9696338ffd46b0feb603edbb730fad6746fba77753c77f7f997345e3d3'
+  source_sha256 'af5b731c145c1d13c4e3b4eeb7d167e94e845e440f71e3496b4ed8dae0291960'
   binary_compression 'tar.zst'
 
   binary_sha256({
