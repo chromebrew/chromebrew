48083888a994565162ee1be39a2965da6c57c9ca
Ed Reel <edreel@gmail.com>
2026-04-12 10:30:31 +0000

    Bullet 2.89 => 3.25

--- packages/bullet.rb
+++ packages/bullet.rb
@@ -1,36 +1,34 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Bullet < Package
+class Bullet < CMake
   description '3D Collision Detection and Rigid Body Dynamics Library'
   homepage 'https://pybullet.org/Bullet/phpBB3/'
-  version '2.89'
+  version '3.25'
   license 'ZLIB'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://github.com/bulletphysics/bullet3/archive/2.89.tar.gz'
-  source_sha256 '621b36e91c0371933f3c2156db22c083383164881d2a6b84636759dc4cbb0bb8'
-  binary_compression 'tar.xz'
+  source_url "https://github.com/bulletphysics/bullet3/archive/#{version}.tar.gz"
+  source_sha256 'c45afb6399e3f68036ddb641c6bf6f552bf332d5ab6be62f7e6c54eda05ceb77'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'de8d4c188438087d438173aa33cea512deadc88e1c3f94cd6b9932c389b46d47',
-     armv7l: 'de8d4c188438087d438173aa33cea512deadc88e1c3f94cd6b9932c389b46d47',
-     x86_64: 'a864c0d9f5429c0a5bff7e4a38b5367820ca3bf110f9baacff5b3b5306114b09'
+    aarch64: 'bb49e18b6f578154267f8d1a6979793225fecf2b6f9bd02afe1af2f75cf53cd8',
+     armv7l: 'bb49e18b6f578154267f8d1a6979793225fecf2b6f9bd02afe1af2f75cf53cd8',
+     x86_64: '94d80568223ebc79a74d8485d8e7e88155d3332854dc996f77b710e9febac61e'
   })
 
-  depends_on 'bdftopcf'
-  depends_on 'libglu'
-  depends_on 'openvr'
+  depends_on 'bdftopcf' => :executable
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'libglu' => :library
+  depends_on 'openvr' => :library
 
-  def self.build
-    Dir.mkdir 'build'
-    Dir.chdir 'build' do
-      system "cmake .. -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} -DLIB_DESTINATION=#{ARCH_LIB} -DCMAKE_BUILD_TYPE=Release"
-      system 'make'
-    end
-  end
+  cmake_options '-DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DBUILD_SHARED_LIBS=ON -DBUILD_PYBULLET=ON \
+    -DCLSOCKET_SHARED=ON -DINSTALL_LIBS=ON -DUSE_DOUBLE_PRECISION=ON -DUSE_OPENVR=ON'
 
-  def self.install
-    Dir.chdir 'build' do
-      system "DESTDIR=#{CREW_DEST_DIR} make install"
+  cmake_install_extras do
+    if ARCH.eql?('x86_64')
+      FileUtils.mkdir_p CREW_DEST_LIB_PREFIX.to_s
+      FileUtils.mv Dir["#{CREW_DEST_PREFIX}/lib/*"], CREW_DEST_LIB_PREFIX.to_s
     end
   end
 end
