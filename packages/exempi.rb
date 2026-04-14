46a674bc5274cde83d261134b252f7567b304c13
Ed Reel <edreel@gmail.com>
2026-04-14 21:27:11 +0000

    Rebuild exempi 2.6.6 with boost 1.90.0-icu77.1-py3.14

--- packages/exempi.rb
+++ packages/exempi.rb
@@ -8,22 +8,21 @@ class Exempi < Autotools
   homepage 'https://libopenraw.freedesktop.org/wiki/Exempi'
   version '2.6.6'
   license 'BSD'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url "https://gitlab.freedesktop.org/libopenraw/exempi/-/archive/#{version}/exempi-#{version}.tar.bz2"
   source_sha256 'c553fe596d4e5b44ce645a3d9a8b1d8609f2572d50953b10fe7103380462e084'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '91378cfb659b7e2274d67819ece09559741d0c54c7759bc041575f132dbf3cfd',
-     armv7l: '91378cfb659b7e2274d67819ece09559741d0c54c7759bc041575f132dbf3cfd',
-       i686: '98df788ee6bbe75d1440c1537d7bf3155982d5060f31619dbae40dac0085d433',
-     x86_64: '9ec94e0eadb49908bed87211caa897d565d7967946c225a3f38214187fc042da'
+    aarch64: '34e33ad229aaa70bb70644700d147b71c604f21a0672f7747088d3eb3aca5214',
+     armv7l: '34e33ad229aaa70bb70644700d147b71c604f21a0672f7747088d3eb3aca5214',
+     x86_64: 'edd15699260158174906ff7e792d3c2dd68a00082eb0ca15a0945bc4f059420b'
   })
 
   depends_on 'autoconf_archive' => :build
   depends_on 'boost' => :build
-  depends_on 'expat' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'zlib' # R
+  depends_on 'expat' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'zlib' => :library
 end
