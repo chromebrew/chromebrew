20f2a1b223dff42700a7a0f01cdea704ac8510c5
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-14 00:51:44 +0000

    terminus -> 4.2.0 in updater-terminus-4.2.0

--- packages/terminus.rb
+++ packages/terminus.rb
@@ -3,11 +3,11 @@ require 'package'
 class Terminus < Package
   description 'The Pantheon CLI — a standalone utility for performing operations on the Pantheon Platform'
   homepage 'https://github.com/pantheon-systems/terminus'
-  version '4.1.9'
+  version '4.2.0'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://github.com/pantheon-systems/terminus/releases/download/#{version}/terminus.phar"
-  source_sha256 '8573fbe281e4a6ad50bedbdbd1a77c7b2683217b06abc24d00ac7e25cec0ba57'
+  source_sha256 '05feb98d0c78abbd33857e53f4e33a7551b2f55bcbe8cc5613f8bfdb71e3a0ff'
 
   depends_on 'php84' unless File.exist? "#{CREW_PREFIX}/bin/php"
 
