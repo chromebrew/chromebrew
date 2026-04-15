ba9d4967906d3fa87e24639fd39cb9ca5518de23
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-15 00:42:00 +0000

    py3_virtualenv -> 21.2.4-py3.14 in updater-py3_virtualenv-21.2.4-py3.14

--- packages/py3_virtualenv.rb
+++ packages/py3_virtualenv.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_virtualenv < Pip
   description 'Virtualenv is a Virtual Environment builder for Python.'
   homepage 'https://virtualenv.pypa.io/'
-  version "21.2.2-#{CREW_PY_VER}"
+  version "21.2.4-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
