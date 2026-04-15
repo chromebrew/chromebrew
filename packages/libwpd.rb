4001fad6c97f46ec9a2c74d81b4bbda4e2cddc73
Ed Reel <edreel@gmail.com>
2026-04-15 08:31:35 +0000

    Rebuild libwpd 0.10.3 and libwpg 0.3.4 with boost 1.90.0-icu77.1-py3.14

--- packages/libwpd.rb
+++ packages/libwpd.rb
@@ -1,43 +1,41 @@
 # Adapted from Arch Linux libwpd PKGBUILD at:
 # https://github.com/archlinux/svntogit-packages/raw/packages/libwpd/trunk/PKGBUILD
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Libwpd < Package
+class Libwpd < Autotools
   description 'Library for importing WordPerfect tm documents'
   homepage 'https://libwpd.sourceforge.net/'
   version '0.10.3'
   license 'LGPL'
-  compatibility 'all'
-  source_url 'https://downloads.sourceforge.net/sourceforge/libwpd/libwpd-0.10.3.tar.xz'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url "https://downloads.sourceforge.net/sourceforge/libwpd/libwpd-#{version}.tar.xz"
   source_sha256 '2465b0b662fdc5d4e3bebcdc9a79027713fb629ca2bff04a3c9251fdec42dd09'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '7a4d5834941571f13ad15e7564b2ec780514f086e96620924516cde800fe422c',
-     armv7l: '7a4d5834941571f13ad15e7564b2ec780514f086e96620924516cde800fe422c',
-       i686: '1d0aacfcb3db543c644fef06b3d04c4431686826b1578f619c0f1d4059a7c7a1',
-     x86_64: '94f5db8b8e15651c00a01020db0412d6a837c203952f083dbb21ce4138cb698a'
+    aarch64: 'ad5fca99f96d69903922da446465d3c97fa4a0fe58260d531a5e68e187849c94',
+     armv7l: 'ad5fca99f96d69903922da446465d3c97fa4a0fe58260d531a5e68e187849c94',
+     x86_64: 'b56b4ad2c61d08f369a778dbcb7714e18fd0641b293e2dbd76e5cec6479d3a48'
   })
 
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
   depends_on 'libgsf' => :build
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'librevenge' # R
-  depends_on 'zlib' # R
+  depends_on 'librevenge' => :library
+  depends_on 'zlib' => :executable
 
   def self.patch
-    downloader 'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/libwpd/trunk/libwpd-gcc11.patch',
-               '7612c36e5e16df2b786fc4c905f096a6e7d600aade292e91950607bfbfba6c32'
-    system 'patch -Np1 -i libwpd-gcc11.patch'
+    patch = [
+      [
+        'https://raw.githubusercontent.com/archlinux/svntogit-packages/packages/libwpd/trunk/libwpd-gcc11.patch',
+        '7612c36e5e16df2b786fc4c905f096a6e7d600aade292e91950607bfbfba6c32'
+      ]
+    ]
+    ConvenienceFunctions.patch(patch)
   end
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS} --disable-static"
-    system 'make'
-  end
+  autotools_configure_options '--disable-static'
 
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
-  end
+  run_tests
 end
--- packages/libwpg.rb
+++ packages/libwpg.rb
@@ -1,39 +1,31 @@
 # Adapted from Arch Linux libwpg PKGBUILD at:
 # https://github.com/archlinux/svntogit-packages/raw/packages/libwpg/trunk/PKGBUILD
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Libwpg < Package
+class Libwpg < Autotools
   description 'Library for importing and converting Corel WordPerfecttm Graphics images.'
   homepage 'https://libwpg.sourceforge.net/'
-  version '0.3.3'
+  version '0.3.4'
   license 'LGPL'
-  compatibility 'all'
-  source_url 'https://downloads.sourceforge.net/libwpg/libwpg-0.3.3.tar.xz'
-  source_sha256 '99b3f7f8832385748582ab8130fbb9e5607bd5179bebf9751ac1d51a53099d1c'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url "https://downloads.sourceforge.net/libwpg/libwpg-#{version}.tar.xz"
+  source_sha256 'b55fda9440d1e070630eb2487d8b8697cf412c214a27caee9df69cec7c004de3'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '645138bbef5bc5d36a2f5f91c59f9163078ef9044d127a27fe257449eec45f5e',
-     armv7l: '645138bbef5bc5d36a2f5f91c59f9163078ef9044d127a27fe257449eec45f5e',
-       i686: 'b187f519dc386f57aad39c01947be79e64c6ff8b3d7d4aaea7956b7f4ec125a0',
-     x86_64: '9b7f4b5b3e31bd451a7c80d895490c784517f29d2b5f3126e7abf332e4ab4a21'
+    aarch64: '1553b1f3f4ce4f49bb0b6be720ba7673e69801a2053d9bd0504cdef6051d2217',
+     armv7l: '1553b1f3f4ce4f49bb0b6be720ba7673e69801a2053d9bd0504cdef6051d2217',
+     x86_64: 'b89675f729f3cd3221445b9cc5a089045ffa71e60f1cf8090ceb888bb47ee0ae'
   })
 
   depends_on 'doxygen' => :build
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'librevenge' # R
-  depends_on 'libwpd' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'librevenge' => :library
+  depends_on 'libwpd' => :library
   depends_on 'perl' => :build
-  depends_on 'zlib' # R
+  depends_on 'zlib' => :executable
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
-  end
+  run_tests
 end
