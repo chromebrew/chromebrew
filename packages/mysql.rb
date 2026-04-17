1d928083df5b06b251721f2a61437d9797722287
Ed Reel <edreel@gmail.com>
2026-04-17 01:54:14 +0000

    Build mysql 9.6.0-icu77.1 with boost 1.90.0-icu77.1-py3.14

--- packages/mysql.rb
+++ packages/mysql.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Mysql < CMake
   description "MySQL Community Edition is a freely downloadable version of the world's most popular open source database"
   homepage 'https://www.mysql.com/'
-  version "9.4.0-#{CREW_ICU_VER}"
+  version "9.6.0-#{CREW_ICU_VER}"
   license 'GPL-2'
   compatibility 'x86_64' # Only 64-bit platforms are supported, so this will work on aarch64 userspaces once those are supported.
   source_url 'https://github.com/mysql/mysql-server.git'
@@ -11,62 +11,59 @@ class Mysql < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-     x86_64: '466d38093023fe22a957aec83ad8ecdd335b7207bd1c8766ea9cdc3d3861bf78'
+     x86_64: '0dc285c0ad79fc1350fd18d1ec097b016134f8086366652a6ff6f97f85aa1795'
   })
 
+  depends_on 'abseil_cpp' => :library
   depends_on 'boost' => :build
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'icu4c' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'icu4c' => :executable
   depends_on 'libcyrussasl' => :build
-  depends_on 'libedit' # R
-  depends_on 'libtirpc' # R
-  depends_on 'lz4' # R
+  depends_on 'libedit' => :executable
+  depends_on 'libtirpc' => :library
+  depends_on 'lz4' => :library
   depends_on 'openldap' => :build
-  depends_on 'openssl' # R
-  depends_on 'protobuf' # R
+  depends_on 'openssl' => :library
+  depends_on 'protobuf' => :library
   depends_on 'rapidjson' => :build
   depends_on 'rpcsvc_proto' => :build
-  depends_on 'zlib' # R
-  depends_on 'zstd' # R
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
+
+  print_source_bashrc
 
   def self.postinstall
     return if CREW_IN_CONTAINER
 
     FileUtils.mkdir_p "#{CREW_PREFIX}/var/mysql/data"
     system "mysqld --initialize-insecure --user=#{USER}" unless Dir.empty? "#{CREW_PREFIX}/var/mysql/data"
-    puts
-    puts 'WARNING: This is an insecure install without a root password!'.orange
-    puts
-    puts 'To lockdown permissions, execute the following:'.orange
-    puts 'mysql_secure_installation'.orange
-    puts
-    puts 'To reset the root password, see https://dev.mysql.com/doc/refman/8.4/en/resetting-permissions.html#resetting-permissions-unix.'.lightblue
-    puts
-    puts 'To start/stop mysqld, execute the following:'.lightblue
-    puts 'mysql.server start'.lightblue
-    puts 'mysql.server stop'.lightblue
-    puts
-    puts 'mysql.server start will execute on login.'.lightblue
-    puts "To prevent this, edit or remove #{CREW_PREFIX}/etc/bash.d/01-mysql.".lightblue
-    puts
-    puts "Databases are stored in #{CREW_PREFIX}/var/mysql/data.".lightblue
-    puts
-    puts 'MySQL Server documentation: https://dev.mysql.com/doc/refman/9.1/en/'.lightblue
-    puts
+    ExitMessage.add <<~EOM
+
+      WARNING: This is an insecure install without a root password!
+
+      To lockdown permissions, execute the following:
+      mysql_secure_installation
+
+      To reset the root password, see https://dev.mysql.com/doc/refman/8.4/en/resetting-permissions.html#resetting-permissions-unix.
+
+      To start/stop mysqld, execute the following:
+      mysql.server start
+      mysql.server stop
+
+      mysql.server start will execute on login.
+      To prevent this, edit or remove #{CREW_PREFIX}/etc/bash.d/01-mysql.
+
+      Databases are stored in #{CREW_PREFIX}/var/mysql/data.
+
+      MySQL Server documentation: https://dev.mysql.com/doc/refman/9.1/en/
+    EOM
   end
 
   def self.postremove
     if Dir.exist?("#{CREW_PREFIX}/var/mysql")
       puts "\nWARNING: This will delete all your databases!".orange
-      print "Would you like to remove #{CREW_PREFIX}/var/mysql? [y/N] "
-      case $stdin.gets.chomp.downcase
-      when 'y', 'yes'
-        FileUtils.rm_rf("#{CREW_PREFIX}/var/mysql")
-        puts "#{CREW_PREFIX}/var/mysql removed.".lightgreen
-      else
-        puts "#{CREW_PREFIX}/var/mysql saved.".lightgreen
-      end
+      Package.agree_to_remove("#{CREW_PREFIX}/var/mysql")
     end
   end
 
@@ -87,6 +84,7 @@ class Mysql < CMake
   # 52/136 Test  #52: merge_small_tests ..................................***Failed   62.42 sec
   # run_tests
   cmake_install_extras do
+    FileUtils.mv "#{CREW_DEST_PREFIX}/lib", CREW_DEST_LIB_PREFIX.to_s
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/etc/bash.d"
     File.write "#{CREW_DEST_PREFIX}/etc/bash.d/01-mysql", <<~EOF
       if [[ ! -f /.dockerenv ]]; then
