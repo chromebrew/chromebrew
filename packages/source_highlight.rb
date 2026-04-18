42fd8addc1cb31a473b9d93a52d6e4d358b1fe16
Ed Reel <edreel@gmail.com>
2026-04-18 07:47:03 +0000

    Build source_highlight 3.1.9-894cacd-boost1.90 with boost 1.90.0-icu77.1-py3.14

--- packages/source_highlight.rb
+++ packages/source_highlight.rb
@@ -6,31 +6,29 @@ require 'buildsystems/autotools'
 class Source_highlight < Autotools
   description 'Convert source code to syntax highlighted document'
   homepage 'https://www.gnu.org/software/src-highlite/'
-  version '3.1.9-894cacd-boost1.85'
+  version '3.1.9-894cacd-boost1.90'
   license 'GPL'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url 'https://git.savannah.gnu.org/git/src-highlite.git'
   git_hashtag '894cacd0799ca60afa359a63782729dec76cbb79'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '244749f21be5f01c11309e77959f4da2b47936a7cdd047321a06a066c8203a06',
-     armv7l: '244749f21be5f01c11309e77959f4da2b47936a7cdd047321a06a066c8203a06',
-       i686: '1f1ccc4f04308bb23c465b6f59e6d1f3f01baed828d3eb5847922754a56bb2b8',
-     x86_64: '7fbb5ac63a8225cf47824a54965b2faffd7059384fbd1a349fc68135f173b17e'
+    aarch64: 'd5b38d39474082bee61f1ceec3c363e682d4d3ef5083e243ce286bc3693e0575',
+     armv7l: 'd5b38d39474082bee61f1ceec3c363e682d4d3ef5083e243ce286bc3693e0575',
+     x86_64: 'b1e536d72b7f4fca36732ffd404b8a5b8aafd68eb2c6789d6e3bb1a744aa2411'
   })
 
-  depends_on 'boost' # R
+  depends_on 'boost' => :library
   depends_on 'ctags' => :logical
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
   depends_on 'texinfo' => :build
 
   autotools_configure_options "--sysconfdir=#{CREW_PREFIX}/etc \
-      --with-bash-completion=#{CREW_PREFIX}/share/bash-completion/completions"
+    --with-bash-completion=#{CREW_PREFIX}/share/bash-completion/completions"
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  autotools_install_extras do
     system "make prefix=#{CREW_DEST_PREFIX} bash_completiondir=#{CREW_DEST_PREFIX}/share/bash-completion/completions install"
   end
 end
