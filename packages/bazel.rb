8dd8f81a82a6d007f025e3b6d9873362ca58929a
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-21 00:49:49 +0000

    bazel -> 9.1.0 in updater-bazel-9.1.0

--- packages/bazel.rb
+++ packages/bazel.rb
@@ -3,11 +3,11 @@ require 'package'
 class Bazel < Package
   description 'a fast, scalable, multi-language and extensible build system'
   homepage 'https://bazel.build/'
-  version '9.0.2'
+  version '9.1.0'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/bazelbuild/bazel/releases/download/#{version}/bazel-#{version}-linux-x86_64"
-  source_sha256 '422e7a1690b76d7e615c29091d3aca28d0bd3a93fe3c93cbefb8f72d774926d5'
+  source_sha256 'a667454f3f4f8878df8199136b82c199f6ada8477b337fae3b1ef854f01e4e2f'
 
   no_compile_needed
   no_shrink
