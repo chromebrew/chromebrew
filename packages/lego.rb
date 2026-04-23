40b58c1173e9fbc7d7958b638e18d6b4187a63b6
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-23 00:48:58 +0000

    lego -> 4.35.1 in updater-lego-4.35.1

--- packages/lego.rb
+++ packages/lego.rb
@@ -3,7 +3,7 @@ require 'package'
 class Lego < Package
   description "Let's Encrypt/ACME client and library written in Go"
   homepage 'https://go-acme.github.io/lego/'
-  version '4.35.0'
+  version '4.35.1'
   license 'MIT'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Lego < Package
      x86_64: "https://github.com/go-acme/lego/releases/download/v#{version}/lego_v#{version}_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '6662f4201003d99e2c13e2070c0a42c21b94fa7c67d6ee65e42f35825cbfa3df',
-     armv7l: '6662f4201003d99e2c13e2070c0a42c21b94fa7c67d6ee65e42f35825cbfa3df',
-       i686: '96aece9543ef5f06aca9a798325c23b9826294658346a71fac55921e1e5e21ba',
-     x86_64: 'f40ff1f8bdd16c98970f44f748b976b7e30f5ffef2f6faddf9dd65a4c0670c7a'
+    aarch64: '65d64bb0f2208b63a9c91edc6a0defb088bcc1da36e0bcd277e72fc27d063811',
+     armv7l: '65d64bb0f2208b63a9c91edc6a0defb088bcc1da36e0bcd277e72fc27d063811',
+       i686: '495d02879cf7974b9b1ca75734813267156b71162f976335659eb605d33a6717',
+     x86_64: 'ab7f885fab2d28f70c068cab5ad662a353d5cf31e485f9d040d039a285e1dd6d'
   })
 
   no_compile_needed
