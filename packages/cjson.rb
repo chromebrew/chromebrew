2cc9e001dc0ba6ca55d37365f65acac7a2c9aecf
Ed Reel <edreel@gmail.com>
2026-04-24 06:51:32 +0000

    Cjson 1.7.15 => 1.7.19

--- packages/cjson.rb
+++ packages/cjson.rb
@@ -1,36 +1,23 @@
-# Adapted from Arch Linux cjson PKGBUILD at:
-# https://github.com/archlinux/svntogit-community/raw/packages/cjson/trunk/PKGBUILD
+require 'buildsystems/cmake'
 
-require 'package'
-
-class Cjson < Package
+class Cjson < CMake
   description 'Ultralightweight JSON parser in ANSI C'
   homepage 'https://github.com/DaveGamble/cJSON'
-  version '1.7.15'
+  version '1.7.19'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/DaveGamble/cJSON/archive/v1.7.15.tar.gz'
-  source_sha256 '5308fd4bd90cef7aa060558514de6a1a4a0819974a26e6ed13973c5f624c24b2'
+  source_url 'https://github.com/DaveGamble/cJSON.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '00cd4e939d680a9979c44f8007d0ff232fc1b4bda1b6f8325adbf9d3e8f34cf6',
-     armv7l: '00cd4e939d680a9979c44f8007d0ff232fc1b4bda1b6f8325adbf9d3e8f34cf6',
-       i686: 'ab346f52cef0555503e07ac58d97964da3986b5b7c2a1c1c9e7980efcc8ad861',
-     x86_64: 'a74121d3420cec8975f7154d42e65e780de5ee07ad6b31d3380427035ce8148c'
+    aarch64: 'b7bf01bc69417a6e04653c85cd2e499459e1b3278b75bf0bcc3a420816a1f1a5',
+     armv7l: 'b7bf01bc69417a6e04653c85cd2e499459e1b3278b75bf0bcc3a420816a1f1a5',
+       i686: '966bf049dfa73e39047efc27da6ff06092b2a3b3452cf12aaa06b90eaed965b4',
+     x86_64: '018f1cff5176ac884fe5a359a9168e1e13bd6c444b3d44e47ce22e990478c243'
   })
 
-  depends_on 'glibc' # R
+  depends_on 'glibc' => :library
 
-  def self.build
-    system "cmake -B builddir #{CREW_CMAKE_OPTIONS} \
-        -DBUILD_SHARED_LIBS=ON \
-        -Wno-dev \
-        -G Ninja"
-    system "#{CREW_NINJA} -C builddir"
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} #{CREW_NINJA} -C builddir install"
-  end
+  cmake_options '-DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DBUILD_SHARED_LIBS=ON'
 end
