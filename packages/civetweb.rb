909824b0c1937c7bc506a347da6057ac6514b62b
Ed Reel <edreel@gmail.com>
2026-04-24 06:25:30 +0000

    Civetweb 1.13 => 1.16

--- packages/civetweb.rb
+++ packages/civetweb.rb
@@ -1,43 +1,32 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Civetweb < Package
+class Civetweb < CMake
   description 'Embedded C/C++ web server'
   homepage 'https://github.com/civetweb/civetweb/'
-  version '1.13'
+  version '1.16'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/civetweb/civetweb/archive/v1.13.tar.gz'
-  source_sha256 'a7ccc76c2f1b5f4e8d855eb328ed542f8fe3b882a6da868781799a98f4acdedc'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/civetweb/civetweb.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a9c69122c495afd89473928171d8b6528d8d1980d136cc16160733a317e21704',
-     armv7l: 'a9c69122c495afd89473928171d8b6528d8d1980d136cc16160733a317e21704',
-       i686: '856eeb6d5543fe03baec996f21324ad0bb14bc0a2e9c080d61cf33614697a36d',
-     x86_64: 'a314284c9ea9b1f3190174645c5c13872387520dc1877a3fb7eb5397916b8bed'
+    aarch64: '5fb14b489952ec14441dfb8d46963c7a649212e771f38348d4c543ea0a884914',
+     armv7l: '5fb14b489952ec14441dfb8d46963c7a649212e771f38348d4c543ea0a884914',
+       i686: '7510b42696d93f2e6758a108d468de165e87d786da4e4593e648f374175bfc44',
+     x86_64: '0882f5f23adf43bb16669240e5996cbe97ffe737e711838c57109d0a69803403'
   })
 
-  def self.build
-    Dir.mkdir 'builddir'
-    Dir.chdir 'builddir' do
-      system "cmake -G 'Ninja' #{CREW_CMAKE_OPTIONS} \
-		          -DBUILD_TESTING=OFF \
-		          -DBUILD_SHARED_LIBS=ON \
-		          -DCIVETWEB_BUILD_TESTING=OFF \
-		          -DCIVETWEB_ENABLE_LUA=OFF \
-		          -DCIVETWEB_ENABLE_DUKTAPE=OFF \
-		          -DCIVETWEB_ENABLE_CXX=ON \
-		          -DCIVETWEB_ENABLE_SERVER_EXECUTABLE=ON \
-		          -DCIVETWEB_ENABLE_SSL=ON .."
-      system 'ninja'
-    end
-  end
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
 
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C builddir install"
-  end
-
-  def self.check
-    system 'ninja -C builddir test'
-  end
+  cmake_options '-DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
+    -DBUILD_TESTING=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCIVETWEB_BUILD_TESTING=OFF \
+    -DCIVETWEB_ENABLE_LUA=OFF \
+    -DCIVETWEB_ENABLE_DUKTAPE=OFF \
+    -DCIVETWEB_ENABLE_CXX=ON \
+    -DCIVETWEB_ENABLE_SERVER_EXECUTABLE=ON \
+    -DCIVETWEB_ENABLE_SSL=ON'
 end
