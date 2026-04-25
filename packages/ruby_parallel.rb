e249880043a25789b8520224a84d39ed18e48b71
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-25 00:37:47 +0000

    ruby_parallel -> 2.1.0-ruby4.0 in updater-ruby_parallel-2.1.0-ruby4.0

--- packages/ruby_parallel.rb
+++ packages/ruby_parallel.rb
@@ -3,7 +3,7 @@ require 'buildsystems/ruby'
 class Ruby_parallel < RUBY
   description 'Run any code in parallel Processes(> use all CPUs), Threads(> speedup blocking operations), or Ractors(> use all CPUs).'
   homepage 'https://github.com/grosser/parallel'
-  version "2.0.1-#{CREW_RUBY_VER}"
+  version "2.1.0-#{CREW_RUBY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
