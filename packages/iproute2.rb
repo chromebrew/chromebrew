2fbbd18ab8279d0e7b988ac108175b8e9a655825
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-25 00:44:39 +0000

    iproute2 -> 7.0.0 in updater-iproute2-7.0.0

--- packages/iproute2.rb
+++ packages/iproute2.rb
@@ -6,11 +6,11 @@ require 'package'
 class Iproute2 < Package
   description 'IP Routing Utilities'
   homepage 'https://git.kernel.org/pub/scm/network/iproute2/iproute2.git'
-  version '6.18.0'
+  version '7.0.0'
   license 'GPL2'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://www.kernel.org/pub/linux/utils/net/iproute2/iproute2-#{version}.tar.xz"
-  source_sha256 '6ba520e1975e4c50dc931eeae91ea37c198b8a173744885f8895b84325f9d456'
+  source_sha256 'e62890f7b5de63c05a3bf331dc8deb4c015c336013f341a4edf46969797f2f4e'
   binary_compression 'tar.zst'
 
   binary_sha256({
