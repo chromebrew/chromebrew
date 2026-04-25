c1970a000638267609ddcde783e04fc709a89cdd
Ed Reel <edreel@gmail.com>
2026-04-25 06:27:53 +0000

    Cmocka 1.1.8 => 2.0.2

--- packages/cmocka.rb
+++ packages/cmocka.rb
@@ -6,26 +6,22 @@ require 'buildsystems/cmake'
 class Cmocka < CMake
   description 'Elegant unit testing framework for C with support for mock objects'
   homepage 'https://cmocka.org/'
-  version '1.1.8'
+  version '2.0.2'
   license 'Apache-2.0'
-  compatibility 'all'
-  source_url 'https://cmocka.org/files/1.1/cmocka-1.1.5.tar.xz'
-  source_sha256 'f0ccd8242d55e2fd74b16ba518359151f6f8383ff8aef4976e48393f77bba8b6'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url "https://cmocka.org/files/#{version.sub(/\.\d+$/, '')}/cmocka-#{version}.tar.xz"
+  source_sha256 '39f92f366bdf3f1a02af4da75b4a5c52df6c9f7e736c7d65de13283f9f0ef416'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '377206b2a3535eb15f45878d96d9b100a175de623616b3fcf71e17ab679198c5',
-     armv7l: '377206b2a3535eb15f45878d96d9b100a175de623616b3fcf71e17ab679198c5',
-       i686: 'f1fa76c44de13364903b2e3cd8a3fd49dde9c20600ff4c810c389118fd4e596e',
-     x86_64: 'cf3cc34b349ac21ae5aaaa800b139cb5f22b7a016039ac7af8c78c3224aa0354'
+    aarch64: '8ac9b9143aa97b13b574bcf2f83f45108cc7ea2e70df5e6bea59b021989a62c1',
+     armv7l: '8ac9b9143aa97b13b574bcf2f83f45108cc7ea2e70df5e6bea59b021989a62c1',
+     x86_64: 'a987d7182ece6dace474408dcf2ed8e363d342d525668223ed77fce34c01bc20'
   })
 
-  depends_on 'glibc' # R
+  depends_on 'glibc' => :library
+  depends_on 'graphviz' => :build
   depends_on 'llvm_dev' => :build
 
-  cmake_options '-DUNIT_TESTING=ON'
-
-  def self.patch
-    system "sed -i 's/-fstack-protector-strong/-fno-stack-protector/g' CompilerChecks.cmake"
-  end
+  cmake_options '-DBUILD_TESTING=OFF'
 end
