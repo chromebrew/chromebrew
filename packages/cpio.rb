6a6485b9ae0d0c55b4df0dc0dd5d1b8f54ace57b
Ed Reel <edreel@gmail.com>
2026-04-27 20:22:30 +0000

    Cpio 2.14 => 2.15-ef694f7

--- packages/cpio.rb
+++ packages/cpio.rb
@@ -3,22 +3,33 @@ require 'buildsystems/autotools'
 class Cpio < Autotools
   description 'GNU cpio copies files into or out of a cpio or tar archive. The archive can be another file on the disk, a magnetic tape, or a pipe.'
   homepage 'https://www.gnu.org/software/cpio/'
-  version '2.14'
+  version '2.15-ef694f7'
   license 'GPL-3'
-  compatibility 'all'
-  source_url 'https://ftp.gnu.org/gnu/cpio/cpio-2.14.tar.bz2'
-  source_sha256 'fcdc15d60f7267a6fc7efcd6b9db7b6c8966c4f2fbbb964c24d41336fd3f2c12'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url 'https://https.git.savannah.gnu.org/git/cpio.git'
+  git_hashtag 'ef694f7bcc60d2b92e5737e31790791ff4de20f3'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b3bca5b31c4e1b45e2f843658464c9fcbdb8eefeec7c5f4284f2be2011b3d969',
-     armv7l: 'b3bca5b31c4e1b45e2f843658464c9fcbdb8eefeec7c5f4284f2be2011b3d969',
-       i686: 'ca491cfb7ce43d1121d3682da98335d26b73a64eb3fc54af638b54137d814ea7',
-     x86_64: '7801234185c3afa64b2b27e1a2ebb5da842d065a9d5f57e4b5c803f529805978'
+    aarch64: '170bd5a0e64bc2a21f5fffa5d6cac9bfb5886b8712719d90a6ad0dcc7fc56767',
+     armv7l: '170bd5a0e64bc2a21f5fffa5d6cac9bfb5886b8712719d90a6ad0dcc7fc56767',
+     x86_64: 'e3db1999d5ce0744f02eb7f48273ddfc473562961a7a5010b5e87ae8121dc5ec'
   })
 
-  depends_on 'glibc' # R
   depends_on 'glibc' => :executable
 
-  autotools_configure_options '--enable-mt' # magnetic tape, not manifest tool
+  autotools_configure_options "--enable-mt #{'--disable-year2038' unless ARCH.eql?('x86_64')}"
+
+  def self.patch
+    system './bootstrap'
+    # The aclocal version is hardcoded.
+    aclocal_version = `aclocal --version|head -1|cut -d' ' -f4`.chomp.gsub(/\.\d+$/, '')
+    system "sed -i 's,1.16,#{aclocal_version},g' configure"
+  end
+
+  autotools_install_extras do
+    # Remove conflicts with tar package.
+    FileUtils.rm_f "#{CREW_DEST_PREFIX}/libexec/rmt"
+    FileUtils.rm_f "#{CREW_DEST_MAN_PREFIX}/man8/rmt.8"
+  end
 end
