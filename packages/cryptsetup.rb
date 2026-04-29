47d0b4918b805885fca967bff15adb15b11a72c3
Ed Reel <edreel@gmail.com>
2026-04-29 02:00:07 +0000

    Cryptsetup 2.7.3 => 2.8.6

--- packages/cryptsetup.rb
+++ packages/cryptsetup.rb
@@ -3,24 +3,28 @@ require 'buildsystems/autotools'
 class Cryptsetup < Autotools
   description 'The cryptsetup utility is used to conveniently setup disk en-/decryption based on DMCrypt kernel module.'
   homepage 'https://gitlab.com/cryptsetup/cryptsetup'
-  version '2.7.3'
+  version '2.8.6'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://mirrors.edge.kernel.org/pub/linux/utils/cryptsetup/v2.7/cryptsetup-2.7.3.tar.xz'
-  source_sha256 'b772ae4f6df0cee7200b28cea960e4daaff2a203d2fd502beab3c1317b07a456'
+  source_url "https://mirrors.edge.kernel.org/pub/linux/utils/cryptsetup/v#{version.sub(/\.\d+$/, '')}/cryptsetup-#{version}.tar.xz"
+  source_sha256 '8004265fd993885d08f7b633dbe056851de1a210307613a4ebddc743fccefe5a'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '0a48fd6d1484d347870e919a53a0de9700ccb8f01994acca02bc8b00448e585e',
-     armv7l: '0a48fd6d1484d347870e919a53a0de9700ccb8f01994acca02bc8b00448e585e',
-       i686: '8b14383a9cadf16697e12eafea51ef990e8a7e03018becb2ee814cb79d626ab5',
-     x86_64: '47cd09d893cfd85342ecac90a564396743e209f53ebed70aee9fd94d6bdb19ed'
+    aarch64: 'f41f0851a056c8ec2e48a15da9d388163e41f40326934a32a3597fdb5bbc946a',
+     armv7l: 'f41f0851a056c8ec2e48a15da9d388163e41f40326934a32a3597fdb5bbc946a',
+       i686: '7ed753be31eedd86c2527136305dbc0e651010573d37cb64f3789c9d602c2355',
+     x86_64: 'c54367b164056afc9cc065d4a0affeb62c7774cb68058a86309ce6f0176e690f'
   })
 
-  depends_on 'lvm2'
-  depends_on 'json_c'
-  depends_on 'libgcrypt'
-  depends_on 'popt'
+  depends_on 'glibc' => :library
+  depends_on 'json_c' => :library
+  depends_on 'libgcrypt' => :library
+  depends_on 'libssh' => :library
+  depends_on 'lvm2' => :library
+  depends_on 'openssl' => :library
+  depends_on 'popt' => :executable
+  depends_on 'util_linux' => :library
 
   autotools_configure_options '--disable-asciidoc'
 end
