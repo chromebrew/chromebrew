4b8baf1ebe56c00266ff9771b07de3b908cf18a6
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-30 00:56:38 +0000

    crystal -> 1.20.1 in updater-crystal-1.20.1

--- packages/crystal.rb
+++ packages/crystal.rb
@@ -3,11 +3,11 @@ require 'package'
 class Crystal < Package
   description 'A language for humans and computers'
   homepage 'https://crystal-lang.org/'
-  version '1.20.0'
+  version '1.20.1'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/crystal-lang/crystal/releases/download/#{version}/crystal-#{version}-1-linux-x86_64-bundled.tar.gz"
-  source_sha256 '9f6fb0dd6b38daa22b6fba07342ff72390efe533639d181360793a218ca164c9'
+  source_sha256 '90c041c2da54a4571ee8169cb79f891f051d1a8284b7732af1d4e3ff98d9be80'
 
   no_compile_needed
   print_source_bashrc
