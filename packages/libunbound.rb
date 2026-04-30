587f79420e5919aaf8867a0761b7afbf8bc399e4
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-04-30 00:48:23 +0000

    libunbound -> 1.25.0 in updater-libunbound-1.25.0

--- packages/libunbound.rb
+++ packages/libunbound.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Libunbound < Autotools
   description 'Unbound is a validating, recursive, and caching DNS resolver.'
   homepage 'https://nlnetlabs.nl/projects/unbound/about/'
-  version '1.24.2'
+  version '1.25.0'
   license 'BSD and GPL-2'
   compatibility 'all'
   source_url "https://nlnetlabs.nl/downloads/unbound/unbound-#{version}.tar.gz"
-  source_sha256 '44e7b53e008a6dcaec03032769a212b46ab5c23c105284aa05a4f3af78e59cdb'
+  source_sha256 '062a6eda723fe2f041bee4079b76981569f1d12e066bbd74800242fc1ebddec7'
   binary_compression 'tar.zst'
 
   binary_sha256({
