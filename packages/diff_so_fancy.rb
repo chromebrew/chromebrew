4b2a5d6256c47ab6f324adbd9366344a37a89229
Ed Reel <edreel@gmail.com>
2026-05-02 07:52:12 +0000

    Diff_so_fancy 1.4.4 => 1.4.10

--- packages/diff_so_fancy.rb
+++ packages/diff_so_fancy.rb
@@ -3,11 +3,11 @@ require 'package'
 class Diff_so_fancy < Package
   description 'diff-so-fancy strives to make your diffs human readable instead of machine readable.'
   homepage 'https://github.com/so-fancy/diff-so-fancy'
-  version '1.4.4'
+  version '1.4.10'
   license 'MIT'
   compatibility 'all'
-  source_url 'https://github.com/so-fancy/diff-so-fancy/releases/download/v1.4.4/diff-so-fancy'
-  source_sha256 '14ded2376a20ce089952c64a2894afeb70ff0b77e5d7b661eb1c973820208e59'
+  source_url 'https://github.com/so-fancy/diff-so-fancy.git'
+  git_hashtag version
 
   depends_on 'perl' # R
 
