52227805da6e43bc3a5a4b05c7e87174406b0127
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-04 00:52:59 +0000

    ollama -> 0.23.0 in updater-ollama-0.23.0

--- packages/ollama.rb
+++ packages/ollama.rb
@@ -3,11 +3,11 @@ require 'package'
 class Ollama < Package
   description 'Get up and running with large language models.'
   homepage 'https://ollama.com/'
-  version '0.22.1'
+  version '0.23.0'
   license 'MIT'
   compatibility 'x86_64'
   source_url "https://github.com/ollama/ollama/releases/download/v#{version}/ollama-linux-amd64.tar.zst"
-  source_sha256 'e27c0fe8f60a824162f81ce07b4bfda767dce4f357d762e149b3d0de0abad9fb'
+  source_sha256 '581ba6cc0c6da3e80f4f454c274a0260ba07095089a21692b165caba9bf49ad4'
 
   no_compile_needed
   no_shrink
