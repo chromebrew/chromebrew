ca703da8dd825af88a61ecee6b006cac87f3225e
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-06 00:43:37 +0000

    py3_python_discovery -> 1.3.0-py3.14 in updater-py3_python_discovery-1.3.0-py3.14

--- packages/py3_python_discovery.rb
+++ packages/py3_python_discovery.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_python_discovery < Pip
   description 'Python interpreter discovery'
   homepage 'https://github.com/tox-dev/python-discovery'
-  version "1.2.2-#{CREW_PY_VER}"
+  version "1.3.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
