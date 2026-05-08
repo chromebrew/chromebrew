b83fa6b1f7beb1b80553005623bc5ef1c2832ce2
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-08 00:54:23 +0000

    doctl -> 1.158.0 in updater-doctl-1.158.0

--- packages/doctl.rb
+++ packages/doctl.rb
@@ -3,7 +3,7 @@ require 'package'
 class Doctl < Package
   description 'The official command line interface for the DigitalOcean API.'
   homepage 'https://github.com/digitalocean/doctl'
-  version '1.157.0'
+  version '1.158.0'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'SKIP'
