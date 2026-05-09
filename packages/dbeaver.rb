3fca5e00711e9f9f3d9250d1df01bc2534c68b1d
Ed Reel <edreel@gmail.com>
2026-05-09 05:06:22 -0500

    Dbeaver 25.3.4 => 26.0.4 (#16093)

--- packages/dbeaver.rb
+++ packages/dbeaver.rb
@@ -3,12 +3,12 @@ require 'package'
 class Dbeaver < Package
   description 'Free Universal Database Tool'
   homepage 'https://dbeaver.io'
-  version '25.3.4'
+  version '26.0.4'
   license 'Apache-2.0'
   compatibility 'x86_64'
   min_glibc '2.30'
-  source_url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-linux.gtk.x86_64.tar.gz"
-  source_sha256 '8aa6e9bd74a7d23b0735db27ebf0b5455dbc5dce78a365e7bed1bc5ae8e92451'
+  source_url "https://github.com/dbeaver/dbeaver/releases/download/#{version}/dbeaver-ce-#{version}-linux-x86_64.tar.gz"
+  source_sha256 '05786fe33b8fcf6bb171052555df09bb9dd028117aae746fc3d71b2400e26e97'
 
   depends_on 'gtk3'
   depends_on 'xdg_base'
