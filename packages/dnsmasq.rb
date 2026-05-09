74ab6d1f0645d34ac80df6a9e8dfab71c640abf1
Ed Reel <edreel@gmail.com>
2026-05-09 05:03:31 -0500

    Dnsmasq 2.89 => 2.92 (#16089)

--- packages/dnsmasq.rb
+++ packages/dnsmasq.rb
@@ -3,19 +3,21 @@ require 'package'
 class Dnsmasq < Package
   description 'Lightweight, easy to configure DNS forward ,TFTP server and DHCP server'
   homepage 'https://thekelleys.org.uk/dnsmasq/doc.html'
-  version '2.89'
+  version '2.92'
   license 'GPL-2 or GPL-3'
   compatibility 'all'
-  source_url 'http://thekelleys.org.uk/dnsmasq/dnsmasq-2.89.tar.xz'
-  source_sha256 '02bd230346cf0b9d5909f5e151df168b2707103785eb616b56685855adebb609'
+  source_url "https://thekelleys.org.uk/dnsmasq/dnsmasq-#{version}.tar.xz"
+  source_sha256 '4bf50c2c1018f9fbc26037df51b90ecea0cb73d46162846763b92df0d6c3a458'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e798ec6969f5ec63d9975efbeef5f82d537fc8349f95b4151d07dd19de9792e2',
-     armv7l: 'e798ec6969f5ec63d9975efbeef5f82d537fc8349f95b4151d07dd19de9792e2',
-       i686: 'a73b32027305d5d9d28331624f95272c47675b25fe0be8b65ff51a3e59b0a442',
-     x86_64: 'aa5c65411792ffc97c989e8b3f5e7447e12a68f7c6c992e4e4e747236e626845'
+    aarch64: '909462504985225785fe23c88806120a7f1a544b2ce47ff221e4d2cd3fc28f57',
+     armv7l: '909462504985225785fe23c88806120a7f1a544b2ce47ff221e4d2cd3fc28f57',
+       i686: 'c76c7b8bad418e3f64b8fca2eab47d64a1ac5b21cf06b948f0ea19a8c0985607',
+     x86_64: '9670b1ee33df42da9078e77b568f3edacaf994c9d326b48bea53114a708bb758'
   })
+
+  depends_on 'glibc' => :executable
 
   def self.build
     system 'make'
