b6a3551fa275aa8cf607bc54b60737b53d7cbe91
Ed Reel <edreel@gmail.com>
2026-05-10 11:25:37 -0500

    Daq 3.0.15 => 3.0.27 (#16122)

--- packages/daq.rb
+++ packages/daq.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Daq < Autotools
   description 'Data Acquisition library, for packet I/O.'
   homepage 'https://www.snort.org'
-  version '3.0.15'
+  version '3.0.27'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/snort3/libdaq.git'
@@ -11,14 +11,16 @@ class Daq < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3c809aeb3c5663f7a3b84e573679dd5c68acb6b4f188ad7961721bf45470019f',
-     armv7l: '3c809aeb3c5663f7a3b84e573679dd5c68acb6b4f188ad7961721bf45470019f',
-       i686: 'cdf573c9b712ea6e17b6ac49bdd8553343aecedeb44987439f2e2428be5d4aec',
-     x86_64: '1f5b8b93b32144d8c603ef13752a7ddbee93c16b5df5a0bcbacd03d5463b91fd'
+    aarch64: '665a396c382f92e9f4f72c404dd02cc572cc555d19940f74870f89d219365e51',
+     armv7l: '665a396c382f92e9f4f72c404dd02cc572cc555d19940f74870f89d219365e51',
+       i686: 'd1899623019f3b89ea36662ce91e2247670d29ab93da80c1f63a3e03f0482a70',
+     x86_64: 'f71eb7d7e0c8ed0db4004cb64473e7a7b2ed0a9f3de60ea898cffef66364ab60'
   })
 
   # depends_on 'cmocka' => :build
-  depends_on 'libpcap'
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'libpcap' => :library
 
   # https://github.com/snort3/libdaq/issues/30
   # run_tests
