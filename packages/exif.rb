16eb28206327ef7c198331388751369eae41d13a
Ed Reel <edreel@gmail.com>
2026-05-13 05:23:07 -0500

    Exif 0.6.21 => 0.6.22 (#16198)

--- packages/exif.rb
+++ packages/exif.rb
@@ -1,36 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Exif < Package
+class Exif < Autotools
   description 'A small command-line utility to show EXIF information hidden in JPEG files'
   homepage 'https://libexif.github.io/'
-  version '0.6.21'
+  version '0.6.22'
   license 'LGPL-2+'
   compatibility 'all'
-  source_url 'https://github.com/libexif/exif/archive/exif-0_6_21-release.tar.gz'
-  source_sha256 'f55e125eee6c2a75d367d3b388bcd7bea75dc944fabe8671bb32e889192f4b77'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/libexif/exif.git'
+  git_hashtag "exif-#{version.gsub('.', '_')}-release"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8f8de193c0571c268e8004876a0365178237107d2e21d44766ef128f204adcb3',
-     armv7l: '8f8de193c0571c268e8004876a0365178237107d2e21d44766ef128f204adcb3',
-       i686: '66264ec4145cb0ea008eeb7a584dbda2339e1b3f2209e712e5bb8b6e794f3db8',
-     x86_64: '5d99ef721aafe110f565698a8ee42d258d89748cbb37073175781af7ec7cc617'
+    aarch64: '160f7c51344f87189820a99ef8da0db87a5fce3bc33c796e51d435adce7eeb57',
+     armv7l: '160f7c51344f87189820a99ef8da0db87a5fce3bc33c796e51d435adce7eeb57',
+       i686: 'd4cf687aa7365857f08c914bab891223366cb2b321876e2fe0665ec9cbeef12e',
+     x86_64: '90e05f7b4a54269966f7fd3f954082f10005fa49169c55e2a35de79e61dca650'
   })
 
-  depends_on 'libexif'
-  depends_on 'popt'
+  depends_on 'glibc' => :executable
+  depends_on 'libexif' => :executable
+  depends_on 'popt' => :executable
 
-  def self.build
-    system 'autoreconf -i -f'
-    system "sed -i '69,70d' po/Makefile.in.in"
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-maintainer-mode'
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  run_tests
 end
