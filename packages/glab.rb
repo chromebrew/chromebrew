0af8725d1c5c31e9c5a496be29073195d50eeff2
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-14 01:04:13 +0000

    glab -> 1.97.0 in updater-glab-1.97.0

--- packages/glab.rb
+++ packages/glab.rb
@@ -3,7 +3,7 @@ require 'package'
 class Glab < Package
   description 'A GitLab CLI tool bringing GitLab to your command line'
   homepage 'https://gitlab.com/gitlab-org/cli'
-  version '1.96.0'
+  version '1.97.0'
   license 'MIT'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Glab < Package
      x86_64: "https://gitlab.com/gitlab-org/cli/-/releases/v#{version}/downloads/glab_#{version}_linux_amd64.tar.gz"
   })
   source_sha256({
-    aarch64: '7ab70f7ce665a524d1692fbabd9feb188f2d13a53f46ce1313e8e074e03c5aca',
-     armv7l: '7ab70f7ce665a524d1692fbabd9feb188f2d13a53f46ce1313e8e074e03c5aca',
-       i686: '8f27e136fc067cb126c68aa09e6b086e94dfda297bf6d20d10c74d0d53f56988',
-     x86_64: '23420e9599a872d966a7518ad1833850b86a036fc29aa0332143ba5ee1fb7cca'
+    aarch64: 'd029bf7e98d286fe86f0292548b0b30e0d61d2df44ff6701227e650ec36670c7',
+     armv7l: 'd029bf7e98d286fe86f0292548b0b30e0d61d2df44ff6701227e650ec36670c7',
+       i686: '581147a1595128081b73c04bad81f9b5136cdc0676aa73c825b83199b89b042c',
+     x86_64: 'c9c02e18fe51a5ceef6d9f8e245ab9beca80811bb6aebd5747c5e624e9f40918'
   })
 
   no_compile_needed
