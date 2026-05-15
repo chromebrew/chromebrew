dbbbe7db79fe3af24905e754d5dd9ec3ed6e027d
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-15 01:03:24 +0000

    vapoursynth -> 76 in updater-vapoursynth-76

--- packages/vapoursynth.rb
+++ packages/vapoursynth.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Vapoursynth < Meson
   description 'A video processing framework with the future in mind'
   homepage 'http://www.vapoursynth.com/'
-  version '75'
+  version '76'
   license 'LGPL-2.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/vapoursynth/vapoursynth.git'
