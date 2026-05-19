7d6eedf2cf1770da4de71c459aa373aa35d19462
Ed Reel <edreel@gmail.com>
2026-05-19 07:18:49 +0000

    Font_util 1.4.1 => 1.4.2

--- packages/font_util.rb
+++ packages/font_util.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Font_util < Autotools
   description 'Tools for truncating and subseting of ISO10646-1 BDF fonts'
   homepage 'https://gitlab.freedesktop.org/xorg/font/util'
-  version '1.4.1'
+  version '1.4.2'
   license 'MIT'
   compatibility 'all'
   source_url 'https://gitlab.freedesktop.org/xorg/font/util.git'
@@ -11,12 +11,14 @@ class Font_util < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1ef384a719dd423ee9529ad503f52ffa13af4afc4802a20e9e5cccac455e90c3',
-     armv7l: '1ef384a719dd423ee9529ad503f52ffa13af4afc4802a20e9e5cccac455e90c3',
-       i686: '50f78fd1b51145eced75ea02b93955be8c11e6fa100d47686a87481010229b4f',
-     x86_64: '9e65f04191f8f2d120bff3e71368fc09b2ba41063de84eea202041a3785d72d8'
+    aarch64: '3b83ecd6672fb870c4cad80b6492b5536fec3df82960e5ed66635c3061151600',
+     armv7l: '3b83ecd6672fb870c4cad80b6492b5536fec3df82960e5ed66635c3061151600',
+       i686: '75eae3617d4add8b1a060ecbd3d1a1da5e9c5154df45fdfd0fe14e25482b0f07',
+     x86_64: '63d511ff979e9e8af5dba620d259780e04e4003f988cffaf4f9f5c14f049048d'
   })
 
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
   depends_on 'xorg_macros' => :build
 
   autotools_configure_options "--with-fontrootdir=#{CREW_PREFIX}/share/fonts"
