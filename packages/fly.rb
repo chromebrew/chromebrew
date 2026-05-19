3b712940058dd45f47197aaab8071895ab68411e
Ed Reel <edreel@gmail.com>
2026-05-19 07:04:01 +0000

    Fly 1.6.5 => 2.0.1

--- packages/fly.rb
+++ packages/fly.rb
@@ -3,28 +3,38 @@ require 'package'
 class Fly < Package
   description 'fly is a C program that creates PNG, JPEG or GIF images on the fly from CGI and other programs.'
   homepage 'http://www.w3perl.com/fly/'
-  version '1.6.5'
+  version '2.0.1'
   license 'custom'
-  compatibility 'all'
-  source_url 'http://www.w3perl.com/fly/dist/fly-1.6.5.tar.gz'
-  source_sha256 '8411915a9acd9a3d86896db5a53ac6bd4e35b44106c0f180cc0f96a6f02a62c9'
-  binary_compression 'tar.xz'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url "http://www.w3perl.com/fly/dist/fly-#{version}.tar.gz"
+  source_sha256 '492351681ee243fd90d2d386d813b9e2b5f8515c345e286d682cc2ea539ddcc6'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '18e5b2d2e945f0192bc56de5c24a822421027c20477a1306ea9f61a8fc55eea9',
-     armv7l: '18e5b2d2e945f0192bc56de5c24a822421027c20477a1306ea9f61a8fc55eea9',
-       i686: '9a59d20d16c94b64b5efda732669b862c12544b0e1bec6570ad9d6dd6db71f47',
-     x86_64: '6232f0497b3d6cd9956dcce7b372a23a26a2874210deca99d57556ffe9c61644'
+    aarch64: '6ce67b8da73c06dbdbe767793fa7490ffb355d2390b46a3bdaeaeac89720f9d1',
+     armv7l: '6ce67b8da73c06dbdbe767793fa7490ffb355d2390b46a3bdaeaeac89720f9d1',
+     x86_64: '16263c36098f902401f56e54811b8ecce27ec16c3674bbc638b94924f185d97c'
   })
 
-  depends_on 'libgd'
+  depends_on 'freetype' => :executable
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'libgd' => :executable
+  depends_on 'libjpeg_turbo' => :executable
+  depends_on 'libpng' => :executable
+  depends_on 'zlib' => :executable
+
+  def self.patch
+    # Fix mold: error: fly.o:(.text): R_X86_64_32 relocation at offset 0x39e9
+    # against symbol `<fragment>' can not be used; recompile with -fPIC
+    system "sed -i 's,fly fly.o,fly fly.c,' Makefile"
+  end
 
   def self.build
     system 'make'
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
-    system "cp fly #{CREW_DEST_PREFIX}/bin"
+    FileUtils.install 'fly', "#{CREW_DEST_PREFIX}/bin/fly", mode: 0o755
   end
 end
