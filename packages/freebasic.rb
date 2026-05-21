36b1843755e2fce96a74cdc7df2699c0792f3af5
Ed Reel <edreel@gmail.com>
2026-05-21 14:00:05 -0500

    Freebasic 1.09.0 => 1.10.1 (#16373)
    Co-authored-by: Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>

--- packages/freebasic.rb
+++ packages/freebasic.rb
@@ -3,22 +3,26 @@ require 'package'
 class Freebasic < Package
   description 'FreeBASIC is a free and open source, BASIC compiler for Microsoft Windows, DOS and Linux.'
   homepage 'https://www.freebasic.net/'
-  version '1.09.0'
+  version %w[i686 x86_64].include?(ARCH) ? '1.10.1' : '1.09.0'
   license 'LGPL-2 and GPL-2'
   compatibility 'all'
   case ARCH
   when 'aarch64', 'armv7l'
-    source_url 'https://downloads.sourceforge.net/project/fbc/FreeBASIC-1.09.0/Binaries-Linux/FreeBASIC-1.09.0-linux-arm.tar.xz'
+    source_url "https://downloads.sourceforge.net/project/fbc/FreeBASIC-#{version}/Binaries-Linux/FreeBASIC-#{version}-linux-arm.tar.xz"
     source_sha256 '7e75720f3f1b706a8d0d99d0fefbfffcf08305b7801083443c2aa58f4bc8a41e'
   when 'i686'
-    source_url 'https://downloads.sourceforge.net/project/fbc/FreeBASIC-1.09.0/Binaries-Linux/FreeBASIC-1.09.0-linux-x86.tar.xz'
-    source_sha256 'fcbfa40d8b7fbab866fd2ece9813e50c1177ba3f7e8cf746bfef1412a6f0dac1'
+    source_url "https://downloads.sourceforge.net/project/fbc/FreeBASIC-#{version}/Binaries-Linux/FreeBASIC-#{version}-linux-x86.tar.xz"
+    source_sha256 'ff811caa5a25f5e6eba02dac42f0d01a8ea6d07d2c678fb71c56c1d2f5c2a4f3'
   when 'x86_64'
-    source_url 'https://downloads.sourceforge.net/project/fbc/FreeBASIC-1.09.0/Binaries-Linux/FreeBASIC-1.09.0-linux-x86_64.tar.xz'
-    source_sha256 'c8143b1207541ddcdf8d5d87f36a85a8bf6fdf85b3e6aee56323a0ecdacbc1e5'
+    source_url "https://downloads.sourceforge.net/project/fbc/FreeBASIC-#{version}/Binaries-Linux/FreeBASIC-#{version}-linux-x86_64.tar.xz"
+    source_sha256 '8a3e82f5e07664ef47932733358257a2c70e4c16279c844a6080b48e2da256b1'
   end
 
   depends_on 'libtinfo'
+
+  def self.patch
+    system "sed -i 's,\$prefix/lib/freebasic,\$prefix/lib64/freebasic,g' install.sh" if ARCH.eql?('x86_64')
+  end
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share"
@@ -27,6 +31,6 @@ class Freebasic < Package
   end
 
   def self.postinstall
-    puts "\nType 'fbc' to get started.\n".lightblue
+    ExitMessage.add "\nType 'fbc' to get started.\n"
   end
 end
