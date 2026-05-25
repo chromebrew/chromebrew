8f1f70ec93c26752f615792d0441a5c16ecd5e74
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2026-05-25 00:18:42 -0400

    libnfs: 4.0.0-1 → 6.0.2 (#16434)
    * libnfs => 6.0.2
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Mark packages from successful builds as automatically buildable.
    
    * update-libnfs-6.0.2: Package File Update Run on linux/386 container.
    
    * update-libnfs-6.0.2: Package File Update Run on linux/amd64 container.
    
    * update-libnfs-6.0.2: Package File Update Run on linux/arm/v7 container.
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: satmandu <satmandu@users.noreply.github.com>
    Co-authored-by: chromebrew-actions[bot] <chromebrew-actions[bot]@users.noreply.github.com>

--- packages/libnfs.rb
+++ packages/libnfs.rb
@@ -1,35 +1,36 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Libnfs < Package
+class Libnfs < Autotools
   description 'client library for accessing NFS shares'
   homepage 'https://github.com/sahlberg/libnfs'
-  @_ver = '4.0.0'
-  version "#{@_ver}-1"
+  version '6.0.2'
   compatibility 'all'
   license 'GPL-3, LGPL-2.1 and BSD'
   source_url 'https://github.com/sahlberg/libnfs.git'
-  git_hashtag "libnfs-#{@_ver}"
-  binary_compression 'tpxz'
+  git_hashtag "libnfs-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '93c8332ed7a11e62196a451206a1bb01f5c8d1bba0c860fdf08a6ee52638748f',
-     armv7l: '93c8332ed7a11e62196a451206a1bb01f5c8d1bba0c860fdf08a6ee52638748f',
-       i686: 'd838e8f83938d1621884496bcc226b33c61b5d20a1c45496d98733cde4c2505c',
-     x86_64: '8ff8bdbe19ee2c2bafe7ca410729eb30fc1195de1c3cab48867375752ad59299'
+    aarch64: '6767c33c704f4240a9ad5a72c656f491696f7ff54a40ffc22adef5302ef1ddde',
+     armv7l: '6767c33c704f4240a9ad5a72c656f491696f7ff54a40ffc22adef5302ef1ddde',
+       i686: '6883e2ab1e6b20dc688eb5b4e000407d19e7f9738c9aba8d344d9af48a41a620',
+     x86_64: '28e4008977ca30b2bc74c8cdec9ca5cdc9933aa998da8f62a2b343ab9a0517a3'
   })
 
-  def self.build
-    system 'autoreconf -fiv'
-    system "#{CREW_ENV_OPTIONS} ./configure #{CREW_CONFIGURE_OPTIONS} \
-            --enable-utils"
-    system 'make'
-  end
+  depends_on 'brotli' => :executable
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gmp' => :executable
+  depends_on 'gnutls' => :executable
+  depends_on 'krb5' => :library
+  depends_on 'libidn2' => :executable
+  depends_on 'libtasn1' => :executable
+  depends_on 'libunistring' => :executable
+  depends_on 'nettle' => :executable
+  depends_on 'p11kit' => :executable
+  depends_on 'zlib' => :executable
+  depends_on 'zstd' => :executable
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  autotools_configure_options '--enable-utils'
+  autotools_pre_configure_options ('CFLAGS="$CFLAGS -Wno-cast-align"' if ARCH.include?('armv7l')).to_s
 end
