4578d6effa5154573d0a77445e7ad783e63dfec4
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-26 00:58:58 +0000

    zed -> 1.3.7 in updater-zed-1.3.7

--- packages/zed.rb
+++ packages/zed.rb
@@ -3,7 +3,7 @@ require 'buildsystems/rust'
 class Zed < RUST
   description 'Zed is a high-performance, multiplayer code editor'
   homepage 'https://zed.dev/'
-  version '1.3.6'
+  version '1.3.7'
   license 'GPL-3, AGPL-3, Apache-2.0'
   compatibility 'x86_64'
   source_url 'https://github.com/zed-industries/zed.git'
