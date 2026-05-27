71cd0a3d04a4722cd6b9bbd02ad0092629d9f501
Ed Reel <edreel@gmail.com>
2026-05-27 05:46:32 +0000

    Muparser 2.2.6.1 => 2.3.5

--- packages/muparser.rb
+++ packages/muparser.rb
@@ -1,33 +1,26 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Muparser < Package
+class Muparser < CMake
   description 'muParser is an extensible high performance math expression parser library written in C++.'
   homepage 'https://beltoforion.de/article.php?a=muparser'
-  version '2.2.6.1'
+  version '2.3.5'
   license 'BSD-2'
   compatibility 'all'
-  source_url 'https://github.com/beltoforion/muparser/archive/v2.2.6.1.tar.gz'
-  source_sha256 'd2562853d972b6ddb07af47ce8a1cdeeb8bb3fa9e8da308746de391db67897b3'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/beltoforion/muparser.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4fce29777174db06a8b8ced631ed78b16f2ccbee130cc5a72ff80035ee9b788e',
-     armv7l: '4fce29777174db06a8b8ced631ed78b16f2ccbee130cc5a72ff80035ee9b788e',
-       i686: '7491c925d7a81eb17bbb1746aa3b6bfee18c6e62e4a43a5bbde4e7b620fee76c',
-     x86_64: 'f00187a350bb837e95c946ed8a85fcfa47ee62e34891155dd4a9f255aa529b39'
+    aarch64: 'f97418183d4797ba6ea12f805eae482a096985a7deecfb6f78fa3c25a176f656',
+     armv7l: 'f97418183d4797ba6ea12f805eae482a096985a7deecfb6f78fa3c25a176f656',
+       i686: '0904571afb0b115b965d64f076b8e24b1bf22e031641fa5d75c6ee7d41f84fdd',
+     x86_64: 'e0070df94f04d25c19b20eb00f1e83f3be8a551b3e7f77ca162aceedfcce171a'
   })
 
-  def self.build
-    system "./configure \
-           --prefix=#{CREW_PREFIX} \
-           --libdir=#{CREW_LIB_PREFIX} \
-           --enable-shared=yes \
-           --enable-samples=no \
-           --enable-debug=no"
-    system 'make'
-  end
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'openmp' => :library
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  cmake_options '-DENABLE_WIDE_CHAR=ON -DENABLE_SAMPLES=OFF'
 end
