ed0f8fbc078c65d8c48ac82a0955c1924b3a418f
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-27 01:01:47 +0000

    libcdr -> 0.1.9-icu78.3 in updater-libcdr-0.1.9-icu78.3

--- packages/libcdr.rb
+++ packages/libcdr.rb
@@ -6,11 +6,11 @@ require 'buildsystems/autotools'
 class Libcdr < Autotools
   description 'CorelDraw file format importer library for LibreOffice'
   homepage 'https://wiki.documentfoundation.org/DLP/Libraries/libcdr'
-  version "0.1.8-#{CREW_ICU_VER}"
+  version "0.1.9-#{CREW_ICU_VER}"
   license 'GPL2 LGPL2.1 MPL'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://dev-www.libreoffice.org/src/libcdr/libcdr-#{version.split('-')[0]}.tar.xz"
-  source_sha256 'ced677c8300b29c91d3004bb1dddf0b99761bf5544991c26c2ee8f427e87193c'
+  source_sha256 'f7bb6abdd7f226820f288a93dd8d07759833c0250d9e202af90f9b312c4665a3'
   binary_compression 'tar.zst'
 
   binary_sha256({
