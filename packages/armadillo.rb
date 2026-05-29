a2d9fdc8e13c5fa72dc5184a97ab4ed7ce1b97f8
satmandu <satmandu@users.noreply.github.com>
2026-05-29 11:54:15 +0000

    armadillo -> 15.2.7 in updater-armadillo-15.2.7

--- packages/armadillo.rb
+++ packages/armadillo.rb
@@ -3,11 +3,11 @@ require 'buildsystems/cmake'
 class Armadillo < CMake
   description 'Armadillo is a high quality linear algebra library (matrix maths) for the C++ language, aiming towards a good balance between speed and ease of use'
   homepage 'https://arma.sourceforge.net/'
-  version '15.2.6'
+  version '15.2.7'
   license 'Apache-2.0'
   compatibility 'all'
   source_url "https://downloads.sourceforge.net/project/arma/armadillo-#{version}.tar.xz"
-  source_sha256 '97cb8ef708541f632e861d005a462dd0367240f81ff96f8e63ebbdd75c8ce55f'
+  source_sha256 '029230b29c3745924ea850c7046efd1db74b863fdfead8b1aaba3bdd5b0cf895'
   binary_compression 'tar.zst'
 
   binary_sha256({
