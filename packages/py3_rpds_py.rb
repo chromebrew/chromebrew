1fbde30b33a92a2f8e544a46ab0f73f939687376
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-29 00:56:42 +0000

    py3_rpds_py -> 2026.5.1 in updater-py3_rpds_py-2026.5.1

--- packages/py3_rpds_py.rb
+++ packages/py3_rpds_py.rb
@@ -6,7 +6,7 @@ require 'buildsystems/pip'
 class Py3_rpds_py < Pip
   description 'Python bindings to the Rust rpds crate for persistent data structures'
   homepage 'https://github.com/crate-py/rpds'
-  version '0.30.0'
+  version '2026.5.1'
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
