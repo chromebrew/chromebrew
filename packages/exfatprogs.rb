98e2c0f3bb5dd3be82ce33cd5f4655796cc76d61
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-05-29 01:05:48 +0000

    exfatprogs -> 1.4.1 in updater-exfatprogs-1.4.1

--- packages/exfatprogs.rb
+++ packages/exfatprogs.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Exfatprogs < Autotools
   description 'exFAT filesystem userspace utilities for the Linux Kernel exfat driver.'
   homepage 'https://github.com/exfatprogs/exfatprogs'
-  version '1.4.0'
+  version '1.4.1'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/exfatprogs/exfatprogs.git'
