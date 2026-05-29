346d42946c3c950180a68b72a069ac3cbda7709e
Satadru Pramanik <satadru@gmail.com>
2026-05-29 18:01:18 -0400

    Remove i686 from wireshark compatibility.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/wireshark.rb
+++ packages/wireshark.rb
@@ -8,7 +8,7 @@ class Wireshark < CMake
   homepage 'https://www.wireshark.org/'
   version '4.6.6'
   license 'GPL-2'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/wireshark/wireshark.git'
   git_hashtag "v#{version}"
   binary_compression 'tar.zst'
@@ -16,7 +16,6 @@ class Wireshark < CMake
   binary_sha256({
     aarch64: 'ffba04ac791f0a7b4996dab77c4e2eb3d2a2684c909f4d3bda4d20b678468f12',
      armv7l: 'ffba04ac791f0a7b4996dab77c4e2eb3d2a2684c909f4d3bda4d20b678468f12',
-       i686: 'b93a72137a0eca33f03772d93c4536a06eb630692e56b55fcf2d90ab7f563ae8',
      x86_64: 'b7903b1831d30ba4a5a8e0dc6fe4b0582b8b9e0055b8ca35d054331a21bee5eb'
   })
 
@@ -41,10 +40,10 @@ class Wireshark < CMake
   depends_on 'lz4' => :library
   depends_on 'opus' => :library
   depends_on 'pcre2' => :library
-  depends_on 'qt5_base' unless ARCH == 'i686'
-  depends_on 'qt5_multimedia' unless ARCH == 'i686'
-  depends_on 'qt5_svg' unless ARCH == 'i686'
-  depends_on 'qt5_tools' unless ARCH == 'i686'
+  depends_on 'qt5_base' => :build
+  depends_on 'qt5_multimedia' => :build
+  depends_on 'qt5_svg' => :build
+  depends_on 'qt5_tools' => :build
   depends_on 'ruby_asciidoctor' => :build
   depends_on 'sbc' => :library
   depends_on 'snappy' => :library
@@ -58,6 +57,6 @@ class Wireshark < CMake
 
   pre_cmake_options 'WIRESHARK_BUILD_WITH_QT5=1'
   cmake_options "-DENABLE_LTO=true \
-    -DBUILD_wireshark=#{ARCH == 'i686' ? 'false' : 'true'} \
+    -DBUILD_wireshark=true \
     -DUSE_qt6=OFF"
 end
