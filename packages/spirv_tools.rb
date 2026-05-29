2f69b168d1a47af061275d021d33bb2701090b69
satmandu <satmandu@users.noreply.github.com>
2026-05-29 14:02:52 +0000

    spirv_tools -> 2026.2 in updater-spirv_tools-2026.2

--- packages/spirv_tools.rb
+++ packages/spirv_tools.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Spirv_tools < CMake
   homepage 'https://github.com/KhronosGroup/SPIRV-Tools'
   description 'API and commands for processing SPIR-V modules'
-  version '2026.1'
+  version '2026.2'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'https://github.com/KhronosGroup/SPIRV-Tools.git'
