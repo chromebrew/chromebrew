ba02c328d12c956fbd8176db4ded0ba0c95d1745
Ed Reel <edreel@gmail.com>
2026-05-30 13:49:56 -0500

    Ghostscript 10.05.0 => 10.07.1 (#16561)

--- packages/ghostscript.rb
+++ packages/ghostscript.rb
@@ -1,77 +1,77 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Ghostscript < Package
+class Ghostscript < Autotools
   description 'Interpreter for the PostScript language'
   homepage 'https://www.ghostscript.com/'
-  version '10.05.0'
+  version '10.07.1'
   license 'AGPL-3+'
   compatibility 'aarch64 armv7l x86_64'
-  source_url "https://github.com/ArtifexSoftware/ghostpdl-downloads/releases/download/gs#{version.tr('.', '')}/ghostpdl-#{version}.tar.xz"
-  source_sha256 '0603f5629bc6f567b454911d104cd96702489c9e70e577787843f480b23d4a77'
+  source_url 'git://git.ghostscript.com/ghostpdl.git'
+  git_hashtag "ghostpdl-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e5c46ac878acbc612a605c767e17887bb5eb49d04b1c35645856d0359ac58d09',
-     armv7l: 'e5c46ac878acbc612a605c767e17887bb5eb49d04b1c35645856d0359ac58d09',
-     x86_64: 'e97d4b792adf25187c89103b3f9b85df5cb98dbe56135742033922bb41177a13'
+    aarch64: '70532561af7b9397afedadd4e004c198d8c8cfd8d269c27e20e485f3e57cffc5',
+     armv7l: '70532561af7b9397afedadd4e004c198d8c8cfd8d269c27e20e485f3e57cffc5',
+     x86_64: 'c3eceae2c5f813c4cae0d64d699afff72d338b6043b976f8df4fb5b1bf770df1'
   })
 
-  depends_on 'at_spi2_core' # R
-  depends_on 'cairo' # R
-  depends_on 'cups' # R
-  depends_on 'expat' # R
-  depends_on 'fontconfig' # R
-  depends_on 'freetype' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'gdk_pixbuf' # R
-  depends_on 'glibc' # R
-  depends_on 'glib' # R
-  depends_on 'gtk3' # R
-  depends_on 'harfbuzz' # R
+  depends_on 'at_spi2_core' => :executable
+  depends_on 'brotli' => :library
+  depends_on 'cairo' => :executable
+  depends_on 'cups' => :library
+  depends_on 'expat' => :library
+  depends_on 'fontconfig' => :library
+  depends_on 'freetype' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'gdk_pixbuf' => :executable
+  depends_on 'glib' => :executable
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gtk3' => :executable
+  depends_on 'harfbuzz' => :executable
   depends_on 'jbigkit' => :build
-  depends_on 'lcms' # R
-  depends_on 'libice' # R
-  depends_on 'libjpeg_turbo' # R
-  depends_on 'libpaper' # R
-  depends_on 'libpng' # R
-  depends_on 'libsm' # R
-  depends_on 'libtiff' # R
-  depends_on 'libx11' # R
-  depends_on 'libxext' # R
-  depends_on 'libxt' # R
-  depends_on 'openjpeg' # R
-  depends_on 'pango' # R
-  depends_on 'zlib' # R
+  depends_on 'lcms' => :library
+  depends_on 'libice' => :library
+  depends_on 'libjpeg_turbo' => :library
+  depends_on 'libpaper' => :library
+  depends_on 'libpng' => :library
+  depends_on 'libsm' => :library
+  depends_on 'libtiff' => :library
+  depends_on 'libx11' => :library
+  depends_on 'libxext' => :library
+  depends_on 'libxt' => :library
+  depends_on 'openjpeg' => :library
+  depends_on 'pango' => :executable
+  depends_on 'zlib' => :library
 
   no_lto
 
   def self.patch
-    FileUtils.rm_rf %w[cups/libs expat ijs jpeg lcms2mt libpng openjpeg tiff zlib]
+    FileUtils.rm_rf %w[brotli cups/libs expat ijs jpeg lcms2mt libpng openjpeg tiff zlib]
   end
 
-  def self.build
-    system '[ -x configure ] || NOCONFIGURE=1 ./autogen.sh'
-    system 'filefix'
-    system "./configure #{CREW_CONFIGURE_OPTIONS} \
-      --disable-hidden-visibility \
-      --disable-compile-inits \
-      --disable-neon \
-      --enable-dynamic \
-      --enable-fontconfig \
-      --enable-freetype \
-      --enable-openjpeg \
-      --with-drivers=ALL \
-      --with-fontpath=#{CREW_PREFIX}/share/fonts/gsfonts \
-      --with-ijs \
-      --with-jbig2dec \
-      --with-libpaper \
-      --with-system-libtiff"
-    system 'make'
-    system 'make so' # Make libgs
+  autotools_configure_options " \
+    --disable-hidden-visibility \
+    --disable-compile-inits \
+    --disable-neon \
+    --enable-dynamic \
+    --enable-fontconfig \
+    --enable-freetype \
+    --enable-openjpeg \
+    --with-drivers=ALL \
+    --with-fontpath=#{CREW_PREFIX}/share/fonts/gsfonts \
+    --with-ijs \
+    --with-jbig2dec \
+    --with-libpaper \
+    --with-system-brotli \
+    --with-system-libtiff"
+
+  autotools_build_extras do
+    system 'make so' # Make libg
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  autotools_install_extras do
     system "make soinstall DESTDIR=#{CREW_DEST_DIR}" # Install libgs
     FileUtils.cp_r Dir['./base/*.h'], "#{CREW_DEST_PREFIX}/include/ghostscript"
     FileUtils.ln_sf 'ghostscript', "#{CREW_DEST_PREFIX}/include/ps"
