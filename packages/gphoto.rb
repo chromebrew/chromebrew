cc4d84f4dd790d10aa5cf6403f33b9bb2db7d8d2
Ed Reel <edreel@gmail.com>
2026-06-02 06:05:27 -0500

    Gphoto 2.5.23 => 2.5.32 (#16634)

--- packages/gphoto.rb
+++ packages/gphoto.rb
@@ -1,38 +1,35 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Gphoto < Package
+class Gphoto < Autotools
   description 'The gphoto2 commandline tool for accessing and controlling digital cameras.'
   homepage 'http://www.gphoto.org/'
-  version '2.5.23'
+  version '2.5.32'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/gphoto/gphoto2/archive/gphoto2-2_5_23-release.tar.gz'
-  source_sha256 'dc78b7f8a88803937301d157b5b32cd45f6defcc771564438a477a7fb05f4489'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/gphoto/gphoto2.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '055e881c56a7e40a0208176d08555a8992f15844aedc204f53d99db8e86e1f31',
-     armv7l: '055e881c56a7e40a0208176d08555a8992f15844aedc204f53d99db8e86e1f31',
-       i686: '72969ad37c17319ce08ca6a80d26d8cfd1bbeac71cbfbcf4ae89f582d6db0f52',
-     x86_64: '92fcfd96e8a3a8d40d5a2661d5a9b37dad019ef086b925c97a738c9879c82663'
+    aarch64: '05e47afcfc1416eaae1f94cabe5f34ef95321a3f559ab09162798376ed397f11',
+     armv7l: '05e47afcfc1416eaae1f94cabe5f34ef95321a3f559ab09162798376ed397f11',
+       i686: '4b2e14c9fc24acc7963ef86f1ac71d1a5fe84837589e800f1630ea5014be8ba7',
+     x86_64: '15c44dd84d053db86ba8198727afc9f34f902a5fee5d00967144aaa57c69a5ff'
   })
 
-  depends_on 'libgphoto'
-  depends_on 'popt'
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'libexif' => :executable
+  depends_on 'libgphoto' => :executable
+  depends_on 'libjpeg_turbo' => :executable
+  depends_on 'libtool' => :executable
+  depends_on 'popt' => :executable
+  depends_on 'readline' => :executable
 
-  def self.build
-    system 'autoreconf -is'
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
+  autotools_install_extras do
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/bin"
+    FileUtils.ln_s "#{CREW_PREFIX}/bin/gphoto2", "#{CREW_DEST_PREFIX}/bin/gphoto"
   end
 
-  def self.check
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  run_tests
 end
