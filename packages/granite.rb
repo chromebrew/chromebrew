0b369be610edeb818759c47e995aa1a4752a6bfa
Ed Reel <edreel@gmail.com>
2026-06-04 05:51:59 -0500

    Granite 6.2.0 => 7.8.1 (#16678)

--- packages/granite.rb
+++ packages/granite.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Granite < Meson
   description 'Library that extends GTK with common widgets and utilities'
   homepage 'https://github.com/elementary/granite'
-  version '6.2.0'
+  version '7.8.1'
   license 'LGPL-3.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/elementary/granite.git'
@@ -11,20 +11,24 @@ class Granite < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6d9ff541ccd52c404af93fa0126073e4eefcecd7a05cef32d15661c699dde3e5',
-     armv7l: '6d9ff541ccd52c404af93fa0126073e4eefcecd7a05cef32d15661c699dde3e5',
-     x86_64: '30a4237114c1f8d37d1c86b31a5b62ae3abdaa72890b740bb4dea7049b60e42b'
+    aarch64: '4df2afcd696818545b8eb47d705a12a2834a4462444ad4bfaff0a92873f2327a',
+     armv7l: '4df2afcd696818545b8eb47d705a12a2834a4462444ad4bfaff0a92873f2327a',
+     x86_64: '48b2de2162231337492a3f917ecdb7656fbbf130a77ecdd8c32a1367b3ae7780'
   })
 
   gnome
 
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gobject_introspection' => :library
+  depends_on 'gsettings_desktop_schemas' => :executable
+  depends_on 'gtk3' => :library
+  depends_on 'gtk4' => :library
+  depends_on 'libgee' => :library
+  depends_on 'libshumate' => :executable
+  depends_on 'pango' => :library
   depends_on 'py3_meson' => :build
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
-  depends_on 'gobject_introspection'
-  depends_on 'gsettings_desktop_schemas' # R
-  depends_on 'gtk3' # R
-  depends_on 'libgee' # R
-  depends_on 'sassc'
-  depends_on 'vala'
+  depends_on 'sassc' => :library
+  depends_on 'vala' => :library
 end
