c8fba426fd58b095955095eb5672f390b7da97e8
Ed Reel <edreel@gmail.com>
2026-06-05 05:15:33 -0500

    Gtk_layer_shell 0.8.2 => 0.10.1 (#16724)

--- packages/gtk_layer_shell.rb
+++ packages/gtk_layer_shell.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Gtk_layer_shell < Meson
   description 'Library to create Wayland desktop components using the Layer Shell protocol'
   homepage 'https://github.com/wmww/gtk-layer-shell'
-  version '0.8.2'
+  version '0.10.1'
   license 'LGPL3'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/wmww/gtk-layer-shell.git'
@@ -14,13 +14,16 @@ class Gtk_layer_shell < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '689e9f3992779c13ec280cee60394577fd419fbaa79f3962b5551e8a096736b7',
-     armv7l: '689e9f3992779c13ec280cee60394577fd419fbaa79f3962b5551e8a096736b7',
-     x86_64: 'fca549d22f350feb268baf2ba75f280fa3152b52268377233f8b6b5174deaa85'
+    aarch64: 'f2de4a716525777afd7832a25c2b2dbfc08d173c40143a03c2c071e80efb046b',
+     armv7l: 'f2de4a716525777afd7832a25c2b2dbfc08d173c40143a03c2c071e80efb046b',
+     x86_64: 'bad153e8372f4cc180c57d023d9f0fa6de334f4593d486cd662279fac5a17cce'
   })
 
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
-  depends_on 'gtk3' # R
-  depends_on 'wayland' # R
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gobject_introspection' => :library
+  depends_on 'gtk3' => :library
+  depends_on 'vala' => :executable
+  depends_on 'wayland' => :library
 end
