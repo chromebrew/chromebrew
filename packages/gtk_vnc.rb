a67b2008e54a561f81d0d6ff6494d51b73e9e77f
Ed Reel <edreel@gmail.com>
2026-06-06 03:44:12 +0000

    Gtk_vnc 1.3.1 => 1.5.0

--- packages/gtk_vnc.rb
+++ packages/gtk_vnc.rb
@@ -3,33 +3,36 @@ require 'buildsystems/meson'
 class Gtk_vnc < Meson
   description 'VNC viewer widget for GTK'
   homepage 'https://wiki.gnome.org/Projects/gtk-vnc'
-  version '1.3.1'
+  version '1.5.0'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gtk-vnc.git'
-  git_hashtag "v#{version}"
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '40f49371a8483626e1c080435e4bf7afaf3f9908e6722558d394f65dff9e7874',
-     armv7l: '40f49371a8483626e1c080435e4bf7afaf3f9908e6722558d394f65dff9e7874',
-     x86_64: 'd135ac0a8ab47f12e4104eabbd36036c3519c1e38722a6e4aae25c0b35a5aac7'
+    aarch64: 'be916c19ee88d003a5285ee328751c71b9113e9966a14003409c450e096d8ee8',
+     armv7l: 'be916c19ee88d003a5285ee328751c71b9113e9966a14003409c450e096d8ee8',
+     x86_64: '17c2567f91c5a8625f5856d6bbfaa28f1864df8da41c21678ceb31cfac4c995e'
   })
 
-  depends_on 'cairo'
-  depends_on 'glib'
-  depends_on 'gobject_introspection'
-  depends_on 'gtk3'
-  depends_on 'libgcrypt'
-  depends_on 'pulseaudio'
-  depends_on 'gdk_pixbuf' # R
-  depends_on 'glibc' # R
-  depends_on 'gnutls' # R
-  depends_on 'harfbuzz' # R
-  depends_on 'libcyrussasl' # R
-  depends_on 'libx11' # R
-  depends_on 'zlib' # R
-  depends_on 'gcc_lib' # R
+  depends_on 'cairo' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'gdk_pixbuf' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gmp' => :library
+  depends_on 'gnutls' => :library
+  depends_on 'gobject_introspection' => :library
+  depends_on 'gtk3' => :library
+  depends_on 'harfbuzz' => :library
+  depends_on 'libcyrussasl' => :library
+  depends_on 'libgcrypt' => :library
+  depends_on 'libx11' => :library
+  depends_on 'pulseaudio' => :library
+  depends_on 'py3_gi_docgen' => :build
+  depends_on 'zlib' => :library
 
   gnome
 end
