2a915e73f739b65ea49799230e5f30be5493e41b
Ed Reel <edreel@gmail.com>
2026-06-07 05:24:02 +0000

    Gwt 2.8.2 => 2.13.0

--- packages/gwt.rb
+++ packages/gwt.rb
@@ -3,48 +3,52 @@ require 'package'
 class Gwt < Package
   description 'Google Web Toolkit'
   homepage 'https://www.gwtproject.org/'
-  version '2.8.2'
+  version '2.13.0'
   license 'Apache-2.0'
   compatibility 'all'
-  source_url 'https://storage.googleapis.com/gwt-releases/gwt-2.8.2.zip'
-  source_sha256 '970701dacc55170088f5eb327137cb4a7581ebb4734188dfcc2fad9941745d1b'
-  binary_compression 'tar.xz'
+  source_url "https://github.com/gwtproject/gwt/releases/download/#{version}/gwt-#{version}.zip"
+  source_sha256 '43ca936765a432d5e6fb7ce746caf9c9a7d6d47abb955da3f03742d7a61b31d3'
 
-  binary_sha256({
-    aarch64: 'abb5a99c68ece5bac1c17b336fc6178ddee1b870a4e3491af31c1695a9b9d70d',
-     armv7l: 'abb5a99c68ece5bac1c17b336fc6178ddee1b870a4e3491af31c1695a9b9d70d',
-       i686: '533e96845f196c95510cec486bf4cd4f173a88b298a104eb6f8f9713a24f665b',
-     x86_64: 'ec69121900cf3370ad358465158e76967e5998a006604e522b102836c754f92b'
-  })
-
-  depends_on 'ant'
+  depends_on 'ant' => :logical
   depends_on 'unzip' => :build
 
-  def self.install
-    system 'rm -f *.cmd'
-    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
-    system "mkdir -p #{CREW_DEST_PREFIX}/share/gwt"
-    system "cp -r . #{CREW_DEST_PREFIX}/share/gwt"
-    Dir.chdir "#{CREW_DEST_PREFIX}/bin" do
-      File.write 'i18nCreator', <<~EOF, perm: 0o755
-        #!/bin/bash
-        cd #{CREW_PREFIX}/share/gwt
-        ./i18nCreator "$@"
-      EOF
+  no_compile_needed
 
-      File.write 'webAppCreator', <<~EOF, perm: 0o755
-        #!/bin/bash
-        cd #{CREW_PREFIX}/share/gwt
-        ./webAppCreator "$@"
-      EOF
-    end
+  def self.patch
+    # Remove Windows commands.
+    FileUtils.rm_f Dir['*.cmd']
+    # Adjust HOMEDIR.
+    system "sed -i '2d' i18nCreator"
+    system "sed -i '2iHOMEDIR=#{CREW_PREFIX}/share/gwt' i18nCreator"
+    system "sed -i '2d' webAppCreator"
+    system "sed -i '2iHOMEDIR=#{CREW_PREFIX}/share/gwt' webAppCreator"
   end
 
-  def self.postinstall
-    puts
-    puts 'To uninstall completely including projects, execute the following:'.lightblue
-    puts 'crew remove gwt'.lightblue
-    puts "rm -rf #{CREW_PREFIX}/share/gwt".lightblue
-    puts
+  def self.build
+    File.write 'i18nCreator.sh', <<~EOF
+      #!/bin/bash
+      cd #{CREW_PREFIX}/share/gwt
+      ./i18nCreator "$@"
+    EOF
+    File.write 'webAppCreator.sh', <<~EOF
+      #!/bin/bash
+      cd #{CREW_PREFIX}/share/gwt
+      ./webAppCreator "$@"
+    EOF
+  end
+
+  def self.install
+    FileUtils.install 'i18nCreator.sh', "#{CREW_DEST_PREFIX}/bin/i18nCreator", mode: 0o755
+    FileUtils.install 'webAppCreator.sh', "#{CREW_DEST_PREFIX}/bin/webAppCreator", mode: 0o755
+    FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/gwt"
+    FileUtils.mv Dir['*'], "#{CREW_DEST_PREFIX}/share/gwt"
+  end
+
+  def self.postremove
+    config_dir = "#{CREW_PREFIX}/share/gwt"
+    if Dir.exist?(config_dir)
+      puts "WARNING: This will remove all interfaces in #{config_dir}!".orange
+      Package.agree_to_remove(config_dir)
+    end
   end
 end
