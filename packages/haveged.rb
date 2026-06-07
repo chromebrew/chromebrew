b7e946c7e5b2c29b5c61b54b0ddbadfc4408c21c
Ed Reel <edreel@gmail.com>
2026-06-07 21:51:10 +0000

    Haveged 1.9.14 => 1.9.22

--- packages/haveged.rb
+++ packages/haveged.rb
@@ -1,37 +1,35 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Haveged < Package
+class Haveged < Autotools
   description 'A simple entropy daemon based on the HAVEGE algorithm, significantly faster than /dev/urandom.'
   homepage 'https://issihosts.com/haveged/'
-  version '1.9.14'
+  version '1.9.22'
   license 'GPL-3+'
   compatibility 'all'
-  source_url 'https://github.com/jirka-h/haveged/archive/v1.9.14.tar.gz'
-  source_sha256 '938cb494bcad7e4f24e61eb50fab4aa0acbc3240c80f3ad5c6cf7e6e922618c3'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/jirka-h/haveged.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9089376d8df95644f812c8f9226e729e12fb68c4ebe4c365a52862f0d7ba1fc9',
-     armv7l: '9089376d8df95644f812c8f9226e729e12fb68c4ebe4c365a52862f0d7ba1fc9',
-       i686: 'c3f4e3381887a68cc8a0b1194d8b16012d268dde46b32b74c1009a1b89fcd2dd',
-     x86_64: 'ed0492d5350f653b8ea93ef67ea1da5d611d3988879aa59c8c735810789da0f5'
+    aarch64: '75259359f5c0e602394376f25b06db15b2ed3c605ea7ea256522f398debdae4d',
+     armv7l: '75259359f5c0e602394376f25b06db15b2ed3c605ea7ea256522f398debdae4d',
+       i686: '9d58e2f93d8e127a05de0ce8ce423b997171095f7522a707f3aa0d704576290d',
+     x86_64: '6b6928656b656366240ab29b5a730af40f6391d48e9c5116933f3af0d9fbeb03'
   })
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS} \
-            --disable-daemon \
-            --enable-nistest \
-            --enable-enttest \
-            --enable-olt \
-            --enable-threads"
-    system 'make'
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+
+  autotools_configure_options ' \
+    --disable-daemon \
+    --enable-nistest \
+    --enable-enttest \
+    --enable-olt \
+    --enable-threads'
+
+  def self.prebuild
+    system 'autoreconf -fiv'
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
+  run_tests
 end
