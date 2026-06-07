c8157f8ada0eac1041ff9dc67e59d3624e3da30f
Ed Reel <edreel@gmail.com>
2026-06-07 16:37:23 -0500

    Haproxy 3.2.4 => 3.4.0 (#16780)

--- packages/haproxy.rb
+++ packages/haproxy.rb
@@ -3,27 +3,31 @@ require 'package'
 class Haproxy < Package
   description 'The Reliable, High Performance TCP/HTTP Load Balancer.'
   homepage 'https://www.haproxy.org/'
-  version '3.2.4'
+  version '3.4.0'
   license 'GPL-2 and LGPL-2.1'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url "https://www.haproxy.org/download/#{version.split('.')[0..1].join('.')}/src/haproxy-#{version}.tar.gz"
-  source_sha256 '624f911f80a6aaf047d3bedc51bd2d282fd31bf664fa205b1f447a537a055c1d'
+  source_sha256 '9298f565c2a9ba8a4e7f89c54be2c5d3fd960b5b304eb5515e15d29d2c15d4f7'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '622d3a99622c5c10960ea2e670556f428a1e5290b1c984ce03329f4206143245',
-     armv7l: '622d3a99622c5c10960ea2e670556f428a1e5290b1c984ce03329f4206143245',
-       i686: '2e21699bc8884afc6991d02bd43caa094b821ce0d57c338dd63af8508831293c',
-     x86_64: 'f0057eab7bd92c0bd51277f42e92a32e4447ce0d1288e6d73c8d8f3b6fc05c52'
+    aarch64: 'f809110ff8d2514437ae68053d87719dc03ac93c68352856fb7a5229c44f0190',
+     armv7l: 'f809110ff8d2514437ae68053d87719dc03ac93c68352856fb7a5229c44f0190',
+     x86_64: '830613af4191eb3f1d32bb080dc1745480f23ecb32b64fa29c701de9576f3cf5'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'zlib' # R
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'libxcrypt' => :executable
+  depends_on 'openssl' => :executable
+  depends_on 'pcre2' => :executable
+  depends_on 'zlib' => :executable
 
   no_fhs
 
   def self.build
-    system 'make', 'TARGET=linux-glibc', 'USE_OPENSSL=1', 'USE_PCRE2_JIT=1', 'USE_ZLIB=1', 'DEBUG=-s'
+    system 'make', "PREFIX=#{CREW_PREFIX}", 'TARGET=linux-glibc',
+           'USE_OPENSSL=1', 'USE_PCRE2_JIT=1', 'USE_ZLIB=1', 'DEBUG=-s'
   end
 
   def self.install
