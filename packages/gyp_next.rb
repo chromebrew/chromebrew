f79b0c3add2d706d16e43731018ee61b9b618eb4
Ed Reel <edreel@gmail.com>
2026-06-07 07:19:20 -0500

    Gyp_next 0.21.0 => 0.22.2 (#16773)

--- packages/gyp_next.rb
+++ packages/gyp_next.rb
@@ -3,7 +3,7 @@ require 'buildsystems/python'
 class Gyp_next < Python
   description 'GYP is a fork of the GYP build system for use in the Node.js projects.'
   homepage 'https://github.com/nodejs/gyp-next/'
-  version '0.21.0'
+  version '0.22.2'
   license 'BSD-3'
   compatibility 'all'
   source_url 'https://github.com/nodejs/gyp-next.git'
@@ -11,11 +11,15 @@ class Gyp_next < Python
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'aaa4754161030d45808f5d6d9e8084cfd2bd44f2b07922fa156b83888e23150e',
-     armv7l: 'aaa4754161030d45808f5d6d9e8084cfd2bd44f2b07922fa156b83888e23150e',
-       i686: '86c9e2f8c7c7b74789b7348720a1afd5db0a157f40782aab0477137dd99bccaa',
-     x86_64: 'd81b4a63056e5e274c5ed6132a4a93c3a3b3425c89df52db465755edb16f1a5e'
+    aarch64: '656648167d6aef5207d10fb6d1034f14ad03366f4548eb709fe67a4e335ae721',
+     armv7l: '656648167d6aef5207d10fb6d1034f14ad03366f4548eb709fe67a4e335ae721',
+       i686: 'c2dd2182895244cac1b2aa0e0a04be47274c8821a01dee50435f8a6c742af7c8',
+     x86_64: 'ae7feba700a457fed6769d588576619fa1f1b787f13c57c54b28a2a12a5e5f72'
   })
 
-  depends_on 'python3' => :build
+  depends_on 'python3' => :logical
+
+  def self.postinstall
+    ExitMessage.add "\nType 'gyp -h' to get started.\n"
+  end
 end
