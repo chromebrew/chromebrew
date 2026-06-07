16e32f7baa1883320ee2062a2ffd85279c7d514a
Ed Reel <edreel@gmail.com>
2026-06-07 02:30:37 +0000

    Gutenprint 5.3.3 => 5.3.5

--- packages/gutenprint.rb
+++ packages/gutenprint.rb
@@ -1,28 +1,35 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Gutenprint < Package
+class Gutenprint < Autotools
   description 'High quality drivers for Canon, Epson, Lexmark, Sony, Olympus, and PCL printers for use with CUPS and GIMP.'
   homepage 'https://gimp-print.sourceforge.io/'
-  version '5.3.3'
+  version '5.3.5'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://downloads.sourceforge.net/project/gimp-print/gutenprint-5.3/5.3.3/gutenprint-5.3.3.tar.xz'
-  source_sha256 '7279ecbc8e67d1858c53cb24e423ea1be98e214872006b79e6e03650dd54a072'
-  binary_compression 'tar.xz'
+  source_url "https://downloads.sourceforge.net/project/gimp-print/gutenprint-#{version.sub(/\.\d+$/, '')}/#{version}/gutenprint-#{version}.tar.xz"
+  source_sha256 'f5a9f47de28530b1ae2069cfbc647a9a641baeeabe809bb0ef2b3ec5b9668d70'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '881b8199d6167bcf0a8008cff249e6bf2a792d1711d10a302e7e853e2fecfb2f',
-     armv7l: '881b8199d6167bcf0a8008cff249e6bf2a792d1711d10a302e7e853e2fecfb2f',
-       i686: 'af03ca9c9904c2c5970aa89e4074c338ea525fd165c1872f3f473e4e43531379',
-     x86_64: '08e10a199a709b9078abaee3a3ccbfb380adfe68c411b7d824c7e143d8a67b5d'
+    aarch64: '9d81ece7322bc66dd87817414da1c1f6707b6706d53b58c65b6d47d983394bae',
+     armv7l: '9d81ece7322bc66dd87817414da1c1f6707b6706d53b58c65b6d47d983394bae',
+       i686: 'b9375679e96a366a33135c0dc47c272c76252788cfa77ae87b11fc1ac31a1430',
+     x86_64: 'ff9831aaf8160e2d2e1e208a9b25dd83e9ecdad195269a553e5d61d87e24b6dd'
   })
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
+  depends_on 'cups' => :library
+  depends_on 'eudev' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'libusb' => :library
+  depends_on 'ncurses' => :library
+  depends_on 'readline' => :library
+  depends_on 'zlib' => :library
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  autotools_install_extras do
+    if ARCH.eql?('x86_64')
+      FileUtils.mkdir_p CREW_DEST_LIB_PREFIX
+      FileUtils.mv Dir["#{CREW_DEST_PREFIX}/lib/*"], CREW_DEST_LIB_PREFIX
+    end
   end
 end
