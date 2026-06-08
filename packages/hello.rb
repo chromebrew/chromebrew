daba4d79276447453d71539e6f1f277f049354bd
Ed Reel <edreel@gmail.com>
2026-06-08 23:51:51 +0000

    Hello 2.12 => 2.12.3

--- packages/hello.rb
+++ packages/hello.rb
@@ -1,34 +1,22 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Hello < Package
+class Hello < Autotools
   description 'GNU Hello is another implementation of the classic program that prints “Hello, world!”'
   homepage 'https://www.gnu.org/software/hello/'
-  version '2.12'
+  version '2.12.3'
   license 'FDL-1.3 GPL-3'
   compatibility 'all'
-  source_url 'https://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz'
-  source_sha256 'cf04af86dc085268c5f4470fbae49b18afbc221b78096aab842d934a76bad0ab'
+  source_url "https://ftp.gnu.org/gnu/hello/hello-#{version}.tar.gz"
+  source_sha256 '0d5f60154382fee10b114a1c34e785d8b1f492073ae2d3a6f7b147687b366aa0'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1ffdc1749b56194ef9913c92d98aa29b2ed7592eb9c52828f0e458da9a84f977',
-     armv7l: '1ffdc1749b56194ef9913c92d98aa29b2ed7592eb9c52828f0e458da9a84f977',
-       i686: 'c75ebf55540a37f20fdcb80c4c0157d88ce8f269e16bf158fe35b41c37427a41',
-     x86_64: '432d72ce76e2224d5af08ddaa323b577dd625cfac7feefe18866c9afdf3b1190'
+    aarch64: 'c63124375c8ab30bbea41bee61adc596b8f48438ea3f805825e68922c61de952',
+     armv7l: 'c63124375c8ab30bbea41bee61adc596b8f48438ea3f805825e68922c61de952',
+       i686: 'fc0abcba774d6763f2dc7f0b5a459523132800e0baed90188def92968ebf2573',
+     x86_64: '6f098bd789a71eb5c5a64d6aaf735a798f421f46ee8ade36d1fa22a3ead4f58c'
   })
 
   depends_on 'glibc' => :executable
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.check
-    system 'make', 'check'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'glibc_lib' => :executable
 end
