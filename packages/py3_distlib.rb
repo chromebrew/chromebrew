aaae9cd445919ffe2f8e7dba740235f00baeede1
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-06-09 00:53:30 +0000

    py3_distlib -> 0.4.2-py3.14 in updater-py3_distlib-0.4.2-py3.14

--- packages/py3_distlib.rb
+++ packages/py3_distlib.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_distlib < Pip
   description 'Distlib provides distribution utilities for Python packages.'
   homepage 'https://bitbucket.org/pypa/distlib/'
-  version "0.4.1-#{CREW_PY_VER}"
+  version "0.4.2-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
