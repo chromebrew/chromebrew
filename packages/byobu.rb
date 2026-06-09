5fcf042d4e0f4de0a95358d8a1f16920389603a3
Ed Reel <edreel@gmail.com>
2026-06-09 05:20:22 +0000

    Byobu 7.8 => 7.9

--- packages/byobu.rb
+++ packages/byobu.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Byobu < Autotools
   description 'Byobu is a GPLv3 open source text-based window manager and terminal multiplexer.'
   homepage 'https://www.byobu.org'
-  version '7.8'
+  version '7.9'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://github.com/dustinkirkland/byobu.git'
@@ -11,16 +11,37 @@ class Byobu < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'fae97c2055e22f1c7426b2ee58592c2cd1649e904c552d1c4254479a06a2608d',
-     armv7l: 'fae97c2055e22f1c7426b2ee58592c2cd1649e904c552d1c4254479a06a2608d',
-       i686: 'a5ca3b37b3e230b845cd15754e91d189b5e59039dfe74ecbc5e734d75351b125',
-     x86_64: 'bafe968bd5150b1c2c66f06e9bcceeaaafdd89b14e92cdc7d5740d3542e9812b'
+    aarch64: 'fc9c058ed3974e0bdba53e0fb4f3c468fc5f85418cbc6137e637b97a0717dad9',
+     armv7l: 'fc9c058ed3974e0bdba53e0fb4f3c468fc5f85418cbc6137e637b97a0717dad9',
+       i686: '1d0bec29a063308e448561cc929d53388ec2f1c47297ac49fb84500239ee7ffe',
+     x86_64: '42f968f9fd48c2baf42db7eae57a23b8047317f08b31c95372bbb19231f89266'
   })
 
   depends_on 'tmux' unless File.exist? "#{CREW_PREFIX}/bin/screen"
 
   def self.patch
-    system "sed -i '318,328d' ./usr/lib/byobu/include/shutil"
-    system "sed -i '34,36d' ./usr/lib/byobu/release"
+    # Fix /usr/local/lib/byobu/include/common: line 52: get_distro: command not found
+    system "sed -i 's,get_distro,echo ChromeOS,' ./usr/lib/byobu/include/common"
+    # Fix for fhs compliance.
+    if ARCH.eql?('x86_64')
+      Dir['./usr/lib/byobu/*/**'].each do |file|
+        system "sed -i 's,/lib/,/lib#{CREW_LIB_SUFFIX}/,g' #{file}" unless Dir.exist?(file)
+      end
+    end
+  end
+
+  autotools_install_extras do
+    # Fix for fhs compliance.
+    if ARCH.eql?('x86_64')
+      Dir["#{CREW_DEST_PREFIX}/lib/byobu/*"].each do |lib|
+        FileUtils.mv lib, "#{CREW_DEST_LIB_PREFIX}/byobu"
+      end
+      Dir["#{CREW_DEST_PREFIX}/lib/trustmux/*"].each do |lib|
+        FileUtils.mv lib, "#{CREW_DEST_LIB_PREFIX}/trustmux"
+      end
+      Dir["#{CREW_DEST_PREFIX}/bin/*"].each do |bin|
+        system "sed -i 's,/lib/,/lib#{CREW_LIB_SUFFIX}/,g' #{bin}"
+      end
+    end
   end
 end
