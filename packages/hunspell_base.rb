8818109c2f67b5687533da1de4ddbb57ef350f16
Ed Reel <edreel@gmail.com>
2026-06-11 08:13:20 +0000

    Hunspell_base 1.7.2 => 1.7.3

--- packages/hunspell_base.rb
+++ packages/hunspell_base.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Hunspell_base < Autotools
   description 'Hunspell is a spell checker and morphological analyzer library'
   homepage 'http://hunspell.github.io/'
-  version '1.7.2'
+  version '1.7.3'
   license 'MPL-1.1, GPL-2 and LGPL-2.1'
   compatibility 'all'
   source_url 'https://github.com/hunspell/hunspell.git'
@@ -11,16 +11,17 @@ class Hunspell_base < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c8c4db6f90ce30d23c176dc7eb8453ac91253fc181d763cb0f614fbb7bf59544',
-     armv7l: 'c8c4db6f90ce30d23c176dc7eb8453ac91253fc181d763cb0f614fbb7bf59544',
-       i686: 'e18ba0d501eec99d78a57667be100f9c7406edbcff9297534769ff10dea5dda1',
-     x86_64: '58fe35f3257cfba2233b0c3259b583d81cb59a4003ab1d0b6283300f198034bf'
+    aarch64: '4c225dcf41eca361066d3db5462061493e3593b17ac9780794cffa8985709060',
+     armv7l: '4c225dcf41eca361066d3db5462061493e3593b17ac9780794cffa8985709060',
+       i686: '6556856dec7e5426f8433e92d4883360a33e62e0e49d3d784131c5e30e914e31',
+     x86_64: '5332efd061c450b1143447ac21899c878765a9da7e0a01c9f4f17d2fd15d2ce9'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'ncurses' # R
-  depends_on 'readline'
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'ncurses' => :library
+  depends_on 'readline' => :executable
 
   def self.patch
     system "sed -i 's,/usr/share,#{CREW_PREFIX}/share,g' man/hunspell.1"
@@ -30,4 +31,8 @@ class Hunspell_base < Autotools
 
   autotools_pre_configure_options "CPPFLAGS+=' -I#{CREW_PREFIX}/include -I#{CREW_PREFIX}/include/ncursesw -I#{CREW_PREFIX}/include/ncurses '"
   autotools_configure_options '--with-ui --with-readline'
+
+  autotools_install_extras do
+    system "sed -i 's,/usr/bin/perl,#{CREW_PREFIX}/bin/perl,' #{CREW_DEST_PREFIX}/bin/ispellaff2myspell"
+  end
 end
