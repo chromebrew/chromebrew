f83c2e6ef5e88286fa6870e88d22fad6822dddf3
Ed Reel <edreel@gmail.com>
2026-06-12 05:05:32 -0500

    Imake 1.0.10 => 1.0.11 (#16862)

--- packages/imake.rb
+++ packages/imake.rb
@@ -3,22 +3,28 @@ require 'buildsystems/autotools'
 class Imake < Autotools
   description 'A build automation system written for the X Window System.'
   homepage 'https://xorg.freedesktop.org/wiki/'
-  version '1.0.10'
+  version '1.0.11'
   license 'custom'
-  compatibility 'all'
-  source_url 'https://www.x.org/releases/individual/util/imake-1.0.10.tar.xz'
-  source_sha256 '75decbcea8d7b354cf36adc9675e53c4790ee3de56a14bd87b42c8e8aad2ecf5'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url "https://www.x.org/releases/individual/util/imake-#{version}.tar.xz"
+  source_sha256 '55955527eaebe94633e4083d4fe5f2160a65fe4c6dafdee48b89fea5f1ca8a78'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e8ff6d0e9a56f8bb730bce9a966b0403cd9b4418cb793f716b30b167590d0009',
-     armv7l: 'e8ff6d0e9a56f8bb730bce9a966b0403cd9b4418cb793f716b30b167590d0009',
-       i686: '5360565b8f907486945ab562c7ee77265aea78ebcb2c0e1ed0ccdc6f36968449',
-     x86_64: '783c7b1402a4602240421e92ca2a8131d691b874c45c5dbc219ec3ef5d0e967c'
+    aarch64: '97bc1972c81637af20e1e0fbbadc914618cca7d7035dc15031a90a938e15d6d3',
+     armv7l: '97bc1972c81637af20e1e0fbbadc914618cca7d7035dc15031a90a938e15d6d3',
+     x86_64: '3874d07eaefc983608a2591042167235703aa5503c6b5b5f6345e2e02852aaf2'
   })
 
-  depends_on 'xorg_cf_files'
-  depends_on 'xorg_proto'
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'xorg_cf_files' => :executable
+  depends_on 'xorg_proto' => :executable
 
   autotools_configure_options "--sysconfdir=#{CREW_PREFIX}/etc"
+
+  def self.patch
+    # Fix bash: mkhtmlindex: /usr/bin/perl: bad interpreter: No such file or directory
+    system "sed -i 's,/usr/bin/perl,#{CREW_PREFIX}/bin/perl,' mkhtmlindex.pl"
+  end
 end
