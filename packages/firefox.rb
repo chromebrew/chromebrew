cf33bdcc032d62e6b9decccd274f373607fdde30
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-06-14 01:07:17 +0000

    firefox -> 151.0.4 in updater-firefox-151.0.4

--- packages/firefox.rb
+++ packages/firefox.rb
@@ -3,12 +3,12 @@ require 'package'
 class Firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '151.0.3'
+  version '151.0.4'
   license 'MPL-2.0, GPL-2 and LGPL-2.1'
   compatibility 'x86_64'
   min_glibc '2.35'
   source_url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/linux-x86_64/en-US/firefox-#{version}.tar.xz"
-  source_sha256 'b2405272cc3a8dde0fadeb5cea2b1fb03fc0a181b3015bd60985a63b04e70ed7'
+  source_sha256 '5fa872cb4845a52829fa58568c2901edcbda4b79ce466b4b9b09afe41cffcd9b'
 
   depends_on 'alsa_lib' # R
   depends_on 'at_spi2_core' # R
