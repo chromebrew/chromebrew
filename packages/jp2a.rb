f7560be49c93157ace2b26141e5bfd30f0e420ff
Ed Reel <edreel@gmail.com>
2026-06-15 07:41:45 -0500

    Jp2a 1.3.2 => 1.3.3 (#16910)

--- packages/jp2a.rb
+++ packages/jp2a.rb
@@ -3,25 +3,27 @@ require 'buildsystems/autotools'
 class Jp2a < Autotools
   description 'jp2a is a simple JPEG/PNG to ASCII converter.'
   homepage 'https://github.com/Talinx/jp2a'
-  version '1.3.2'
+  version '1.3.3'
   license 'GPL-2'
-  compatibility 'all'
-  source_url 'https://github.com/Talinx/jp2a/releases/download/v1.1.1/jp2a-1.1.1.tar.bz2'
-  source_sha256 '3b91f26f79eca4e963b1b1ae2473722a706bf642218f20bfe4ade5333aebb106'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url 'https://github.com/Talinx/jp2a.git'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6f27bfadc6dd38133c88500d05b3f5cedb26c79a5bf632e65d0bdf1045c96407',
-     armv7l: '6f27bfadc6dd38133c88500d05b3f5cedb26c79a5bf632e65d0bdf1045c96407',
-       i686: '1ff7e8633fb8b8d9a58beed5b2bfd2ddc844a51991f456cbf634eea213ffe6ef',
-     x86_64: 'a557c2f043bc5694010481cad6e41db5daf6487a78d761d935633cc1f1eed391'
+    aarch64: '9ee3d3bd47e418ec3631fd92f3b350b26704b10fe8853a3ff29f1e019932b0c1',
+     armv7l: '9ee3d3bd47e418ec3631fd92f3b350b26704b10fe8853a3ff29f1e019932b0c1',
+     x86_64: 'b7e1e269dbfd6a9c25e29790981f61c67f07ac7fd451275882f703b4e58b8a1c'
   })
 
-  depends_on 'curl' # R
-  depends_on 'glibc' # R
-  depends_on 'libjpeg_turbo' # R
-  depends_on 'libpng' # R
-  depends_on 'ncurses' # R
+  depends_on 'curl' => :executable
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'libexif' => :executable
+  depends_on 'libjpeg_turbo' => :executable
+  depends_on 'libpng' => :executable
+  depends_on 'libwebp' => :executable
+  depends_on 'ncurses' => :executable
 
   autotools_pre_configure_options "CFLAGS='-lncurses -ltinfo -I#{CREW_PREFIX}/include/ncurses #{CREW_ENV_OPTIONS_HASH['CFLAGS']}' LDFLAGS='-L#{CREW_LIB_PREFIX} -lncurses -ltinfo #{CREW_ENV_OPTIONS_HASH['LDFLAGS']}'"
   autotools_configure_options '--enable-curl'
