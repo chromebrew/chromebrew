7cd7276f4fbbce0bce2d944e570072e876c9a071
Ed Reel <edreel@gmail.com>
2026-06-15 23:12:02 +0000

    Lesspipe 2.18 => 2.27

--- packages/lesspipe.rb
+++ packages/lesspipe.rb
@@ -3,7 +3,7 @@ require 'package'
 class Lesspipe < Package
   description 'Input filter for the pager less'
   homepage 'https://www-zeuthen.desy.de/~friebel/unix/lesspipe.html'
-  version '2.18'
+  version '2.27'
   license 'GPL-2.0'
   compatibility 'all'
   source_url 'https://github.com/wofr06/lesspipe.git'
@@ -11,10 +11,10 @@ class Lesspipe < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4bedbaedd6667ef7985c5a3a4fcae368cc6c842eed340ec9783914ed0b8df352',
-     armv7l: '4bedbaedd6667ef7985c5a3a4fcae368cc6c842eed340ec9783914ed0b8df352',
-       i686: '1e6a90cd20229e66589e08cd41b113378eddecda1ce56e22028956515d4ce163',
-     x86_64: 'f019a7f7088347b8c2467e2b7aaa1aca70fa87d835549781b58bdaa65fbf98ed'
+    aarch64: 'bd6525d466b9331209db7757fa5a91e52a10ad09350c3e05618f47ff9519be17',
+     armv7l: 'bd6525d466b9331209db7757fa5a91e52a10ad09350c3e05618f47ff9519be17',
+       i686: '545318a50139f171be312a4eadf560deea7e73d24e3cf9860f50e9457e85f6d4',
+     x86_64: 'fbf5435348f91bb6689e5d61977bd77134a16e0b9d81d067d4b702985f5ad864'
   })
 
   depends_on 'less'
@@ -28,9 +28,9 @@ class Lesspipe < Package
   end
 
   def self.install
-    FileUtils.install %w[lesspipe.sh code2color archive_color vimcolor sxw2txt lesscomplete],
+    FileUtils.install %w[archive_color lesscomplete lesspipe.sh vimcolor],
                       "#{CREW_DEST_PREFIX}/bin", mode: 0o755
     FileUtils.install '10-lesspipe', "#{CREW_DEST_PREFIX}/etc/env.d/10-lesspipe", mode: 0o644
-    FileUtils.install 'less_completion', "#{CREW_DEST_PREFIX}/etc/bash.d/less_completion", mode: 0o644
+    FileUtils.install 'bash_completion', "#{CREW_DEST_PREFIX}/etc/bash.d/less_completion", mode: 0o644
   end
 end
