eeba261002d00a0190be12cca85ce4d90c57cbc9
Ed Reel <edreel@gmail.com>
2026-06-15 22:23:12 +0000

    Leptonica 1.87.0-pre1 => 1.87.0

--- packages/leptonica.rb
+++ packages/leptonica.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Leptonica < CMake
   description 'Software that is broadly useful for image processing and image analysis applications'
   homepage 'http://www.leptonica.org'
-  version '1.87.0-pre1'
+  version '1.87.0'
   license 'Apache-2.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/DanBloomberg/leptonica.git'
@@ -11,22 +11,23 @@ class Leptonica < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ce467a8ce915951e6a579ef134bb92c9ed82e5b7000db9c65a81cfa2b3ec27cd',
-     armv7l: 'ce467a8ce915951e6a579ef134bb92c9ed82e5b7000db9c65a81cfa2b3ec27cd',
-     x86_64: '77166ed2e0b882ccabe95844007027a4907dce27bbcc67fb17b4107d6cae62c4'
+    aarch64: '8524c29dded67c6587f8166a3e53bd59fafb6d27e40c94a0776ad9bcc932c1a7',
+     armv7l: '8524c29dded67c6587f8166a3e53bd59fafb6d27e40c94a0776ad9bcc932c1a7',
+     x86_64: '6209b8824d1900443e10ffa24113560cf56c9b3bdcdce6f913a15595a4ca5255'
   })
 
-  depends_on 'giflib' # R
-  depends_on 'glibc' # R
+  depends_on 'giflib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
   depends_on 'harfbuzz' => :build
   depends_on 'libdeflate' => :build
-  depends_on 'libjpeg_turbo' # R
-  depends_on 'libpng' # R
-  depends_on 'libtiff' # R
-  depends_on 'libwebp' # R
-  depends_on 'openjpeg' # R
+  depends_on 'libjpeg_turbo' => :library
+  depends_on 'libpng' => :library
+  depends_on 'libtiff' => :library
+  depends_on 'libwebp' => :library
+  depends_on 'openjpeg' => :library
   depends_on 'xzutils' => :build
-  depends_on 'zlib' # R
+  depends_on 'zlib' => :library
   depends_on 'zstd' => :build
 
   cmake_options '-DBUILD_SHARED_LIBS=ON'
