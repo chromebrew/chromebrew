889a9bfe00f5df94e0fd1a4a2ecb41d93cf61b5b
Ed Reel <edreel@gmail.com>
2026-06-15 01:04:47 +0000

    Julia 1.12.4 => 1.12.6

--- packages/julia.rb
+++ packages/julia.rb
@@ -3,7 +3,7 @@ require 'package'
 class Julia < Package
   description 'Julia is a flexible dynamic language, appropriate for scientific and numerical computing'
   homepage 'https://julialang.org/'
-  version ARCH.eql?('x86_64') ? '1.12.4' : '1.7.3'
+  version ARCH.eql?('x86_64') ? '1.12.6' : '1.7.3'
   license 'MIT'
   compatibility 'all'
 
@@ -17,27 +17,28 @@ class Julia < Package
     aarch64: 'e9de15c56b9b62727c69d10da4b8e90fa6609d2e94e9cfb9f99128dfb59a8677',
      armv7l: 'e9de15c56b9b62727c69d10da4b8e90fa6609d2e94e9cfb9f99128dfb59a8677',
        i686: 'c1e1a4f9a53affee269c7e740cb8bd46740f9021414459c3ab3bb2c540d9d499',
-     x86_64: 'c57baf178fe140926acb1a25396d482f325af9d7908d9b066d2fbc0d6639985d'
+     x86_64: 'bbabf3bef19421a9dbd24a767d807606ab85e444323b5a1c73ffe293fa3d079a'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'gmp' # R
-  depends_on 'libnghttp2' # R
-  depends_on 'zlib' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gmp' => :library
+  depends_on 'libnghttp2' => :library
+  depends_on 'libssh2' => :library
+  depends_on 'libunwind' => :library
+  depends_on 'openssl' => :library
+  depends_on 'suitesparse' => :library
+  depends_on 'zlib' => :library
 
+  no_compile_needed
   no_shrink
 
   def self.install
-    FileUtils.mkdir_p "#{CREW_DEST_LIB_PREFIX}/julia"
+    FileUtils.mkdir_p CREW_DEST_PREFIX
     FileUtils.mv Dir['*'], CREW_DEST_PREFIX
     FileUtils.mv "#{CREW_DEST_PREFIX}/LICENSE.md", "#{CREW_DEST_PREFIX}/share/julia"
-    if ARCH == 'x86_64'
-      FileUtils.cd "#{CREW_DEST_PREFIX}/lib/julia" do
-        system "find . -type f -exec ln -s #{CREW_PREFIX}/lib/julia/{} #{CREW_DEST_LIB_PREFIX}/julia/{} \\;"
-        system "find . -type l -exec ln -s #{CREW_PREFIX}/lib/julia/{} #{CREW_DEST_LIB_PREFIX}/julia/{} \\;"
-      end
-    end
+    FileUtils.mv "#{CREW_DEST_PREFIX}/lib", CREW_DEST_LIB_PREFIX if ARCH.eql?('x86_64')
     # Remove conflict with gcc_lib.
     FileUtils.rm_f "#{CREW_DEST_LIB_PREFIX}/bfd-plugins/liblto_plugin.so"
   end
