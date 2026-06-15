6449bf55f91b9fc141d7d5e7e072eb6b08f4d5a6
Ed Reel <edreel@gmail.com>
2026-06-15 18:21:15 -0500

    Lager 0.1.1 => 0.1.3 (#16919)

--- packages/lager.rb
+++ packages/lager.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Lager < CMake
   description 'C++ library for value-oriented design using the unidirectional data-flow architecture'
   homepage 'https://github.com/arximboldi/lager'
-  version '0.1.1'
+  version '0.1.3'
   license 'GPL-3'
   compatibility 'all'
   source_url 'https://github.com/arximboldi/lager.git'
@@ -11,10 +11,10 @@ class Lager < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'e623c6f059f430a10eb9ad4edc148d1b2b0b3fadcac4e532836c8903edc6af3d',
-     armv7l: 'e623c6f059f430a10eb9ad4edc148d1b2b0b3fadcac4e532836c8903edc6af3d',
-       i686: '90a36c7ed4ad6accdd045d214ebb88e9962725f814858d3068605ba412014afc',
-     x86_64: '2ed346288a1e1e9481f5b02a300ab8dc950137987a28c808eae11b376580f6dd'
+    aarch64: '8231ec4d87e6cd3aad2278925e60364fd6d024a048cad785cb1f49dda18b43e5',
+     armv7l: '8231ec4d87e6cd3aad2278925e60364fd6d024a048cad785cb1f49dda18b43e5',
+       i686: '612ccf9403f3088fe9193a98daf86a75cae8f76312465558aad95bb318e19de8',
+     x86_64: '3bef496ed97b601fb03fffa8832b6b6be52bfeec5b09045fed9df2f50a4a6c88'
   })
 
   cmake_options '-Dlager_BUILD_EXAMPLES=OFF -Dlager_BUILD_TESTS=OFF'
