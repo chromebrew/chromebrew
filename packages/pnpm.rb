6ba20be964d8d7e4a3a5632a8ff186b544dd112c
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-06-16 01:19:31 +0000

    pnpm -> 11.7.0 in updater-pnpm-11.7.0

--- packages/pnpm.rb
+++ packages/pnpm.rb
@@ -3,11 +3,11 @@ require 'package'
 class Pnpm < Package
   description 'Fast, disk space efficient package manager'
   homepage 'https://pnpm.io/'
-  version '11.6.0'
+  version '11.7.0'
   license 'MIT'
   compatibility 'x86_64'
   source_url "https://github.com/pnpm/pnpm/releases/download/v#{version}/pnpm-linux-x64.tar.gz"
-  source_sha256 '74d64c1646385fb21691f32f0ab6aca1a9f5c829ba54d3cda3a24838a228e68c'
+  source_sha256 '752e31654c6f24bc945db784d12831b80b29f533b43105575ce30dd3c8658609'
 
   no_compile_needed
 
