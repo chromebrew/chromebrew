4565076c1ce4be82256eef15a3bf6f36c99defe1
Ed Reel <edreel@gmail.com>
2026-06-19 06:33:36 -0500

    Snappy 1.1.7 => 1.2.2 (#16982)

--- packages/snappy.rb
+++ packages/snappy.rb
@@ -1,34 +1,27 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Snappy < Package
+class Snappy < CMake
   description 'Snappy, a fast compressor/decompressor.'
   homepage 'https://github.com/google/snappy'
-  version '1.1.7'
+  version '1.2.2'
   license 'GPL-2 and LGPL-2'
   compatibility 'all'
-  source_url 'https://github.com/google/snappy/archive/1.1.7.tar.gz'
-  source_sha256 '3dfa02e873ff51a11ee02b9ca391807f0c8ea0529a4924afa645fbf97163f9d4'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/google/snappy.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3ea4b27d211d42908d27ab51d5c35ad3ed4fc19300a6714040e3b2f97e9d106a',
-     armv7l: '3ea4b27d211d42908d27ab51d5c35ad3ed4fc19300a6714040e3b2f97e9d106a',
-       i686: '98d7365e20a8f9f20a93697ea99955dd8a2e1f2411e314dd0d86f51a0e07a391',
-     x86_64: '45695bc3b76470fb98ca385eb7cf0fcb9e08f79492474ea65ee690c4b4ed5474'
+    aarch64: '697cff1e0c956c29d65d690a5440aee2f38fe3d9d02bab13396cf3744cb04a66',
+     armv7l: '697cff1e0c956c29d65d690a5440aee2f38fe3d9d02bab13396cf3744cb04a66',
+       i686: 'b9cb1f83e790e0307cbe2e8d51474a76bfb5e77068f2bcc8ca3affbe4b0460c5',
+     x86_64: 'da62465a10ede8e2ce13fc5e4228af137377ef7ebb6e4e219b3031ffe1578872'
   })
 
-  def self.build
-    FileUtils.mkdir 'build'
-    FileUtils.cd('build') do
-      system "cmake .. -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} \
-                       -DCMAKE_INSTALL_LIBDIR=#{CREW_LIB_PREFIX} \
-                       -DBUILD_SHARED_LIBS=ON \
-                       -DSNAPPY_BUILD_TESTS=OFF" # Tests don't work on ARM
-      system 'make'
-    end
-  end
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'llvm_dev' => :build
+  depends_on 'lzo' => :library
 
-  def self.install
-    system "make -C build DESTDIR=#{CREW_DEST_DIR} install"
-  end
+  cmake_options '-DBUILD_SHARED_LIBS=ON -DSNAPPY_BUILD_TESTS=OFF' # Tests don't work on ARM
 end
