e786157603ca2cac1e5c187de607d3e50817c4b8
Ed Reel <edreel@gmail.com>
2026-06-21 19:52:22 +0000

    Lttng_ust 2.13.7 => 2.15.1
    Liburcu 0.14.0-2 => 0.15.6

--- packages/lttng_ust.rb
+++ packages/lttng_ust.rb
@@ -3,20 +3,22 @@ require 'buildsystems/autotools'
 class Lttng_ust < Autotools
   description 'Linux Trace Toolkit: next generation is a modern toolkit for tracing Linux systems and applications.'
   homepage 'https://lttng.org/'
-  version '2.13.7'
+  version '2.15.1'
   license 'LGPL-2.1-only, MIT, GPL-2.0-only, GPL-3.0-or-later, BSD-2-Clause, BSD-3-Clause'
   compatibility 'all'
-  source_url 'https://lttng.org/files/lttng-ust/lttng-ust-2.13.7.tar.bz2'
-  source_sha256 '5fb4f17c307c8c1b79c68561e89be9562d07e7425bf40e728c4d66755342a5eb'
+  source_url "https://lttng.org/files/lttng-ust/lttng-ust-#{version}.tar.bz2"
+  source_sha256 '37c9b58ea7aa7bc47d6630b52ba1a48ebce095b9a196eab4ddd273d78301792d'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1ac688d427738fbb306c05b2296ba7f3046450ebed75ec237b46cf0b7990aabd',
-     armv7l: '1ac688d427738fbb306c05b2296ba7f3046450ebed75ec237b46cf0b7990aabd',
-       i686: 'd20a9cb55fad688ec847e440a159fcef9654be7004f833dc2b4d0bd80cb54016',
-     x86_64: '569a554f0b8a891430547346f75563d0ba77aacd7a48958d543aad7245f17b1b'
+    aarch64: '8abc564f183bab9aea7b3ac964158a910b618728cdcfea6c02ebc87950d81ab6',
+     armv7l: '8abc564f183bab9aea7b3ac964158a910b618728cdcfea6c02ebc87950d81ab6',
+       i686: 'b5591119e085684594d45fd2ff40c41a876e3f45558565ceea147a29144cf049',
+     x86_64: '38fd7ace2ba041d476337c403f90678ff773c54e359c455a5063d1c396000d31'
   })
 
-  depends_on 'liburcu'
-  depends_on 'numactl'
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'liburcu' => :library
+  depends_on 'numactl' => :library
 end
