48fab06f0aba87973aae2e16b21df37b12573d05
Ed Reel <edreel@gmail.com>
2026-06-21 19:56:34 -0500

    Lttng_ust 2.13.7 => 2.15.1 (#16999)
    Liburcu 0.14.0-2 => 0.15.6

--- packages/liburcu.rb
+++ packages/liburcu.rb
@@ -3,21 +3,22 @@ require 'buildsystems/autotools'
 class Liburcu < Autotools
   description 'Userspace RCU (read-copy-update) library.'
   homepage 'https://liburcu.org/'
-  version '0.14.0-2'
+  version '0.15.6'
   license 'LGPLv2.1'
   compatibility 'all'
-  source_url "https://lttng.org/files/urcu/userspace-rcu-#{version.split('-').first}.tar.bz2"
-  source_sha256 'ca43bf261d4d392cff20dfae440836603bf009fce24fdc9b2697d837a2239d4f'
+  source_url "https://lttng.org/files/urcu/userspace-rcu-#{version}.tar.bz2"
+  source_sha256 '850b192096eb11ebf2c70e8f97bc7da7479ee41da1bebeb44e3986908bac414f'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2a28e5a8fb742bde0e3177aaee4b59a39cbbc36a4b462897a3e63ca7fa31d5f9',
-     armv7l: '2a28e5a8fb742bde0e3177aaee4b59a39cbbc36a4b462897a3e63ca7fa31d5f9',
-       i686: '7806f53b85628320cf866aafc7c2d3add0b0b42a6afbea1fd5dd15d465ee152f',
-     x86_64: 'c3cfb3686d8d76b611380ac84d2f6c15cffda194a804560413b49ce7998165e5'
+    aarch64: '4fa45616cf52697faa7fc3513ab3d31ecabd61b93ff73ea651e8befda58db4d5',
+     armv7l: '4fa45616cf52697faa7fc3513ab3d31ecabd61b93ff73ea651e8befda58db4d5',
+       i686: 'f61e50171dc9d59d223049468cadc6e14d663c00e1c2501198ecf6e029566f2f',
+     x86_64: '33441ab4a008123e6712d9b8a3b99b4cf1863ff520f853e99afa5b1f3d1ff50e'
   })
 
-  depends_on 'glibc' # R
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 
   autotools_configure_options '--disable-static'
 
