b7ea2dd4feda0296cd26d7ea62a93a34dc2e0446
satmandu <satmandu@users.noreply.github.com>
2026-06-23 02:46:35 +0000

    updater-sccache-0.16.0: Package File Update Run on linux/amd64 container.

--- packages/sccache.rb
+++ packages/sccache.rb
@@ -19,6 +19,7 @@ class Sccache < Package
 
   depends_on 'gcc_lib' => :executable
   depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
   depends_on 'openssl' => :executable
   depends_on 'rust' => :build
 
