791f2e4eca7cc5af739cfa70bd3c97127d6ce280
Zopolis4 <creatorsmithmdt@gmail.com>
2026-06-24 21:00:59 +1000

    Rebuild ragel

--- packages/ragel.rb
+++ packages/ragel.rb
@@ -3,10 +3,10 @@ require 'buildsystems/autotools'
 class Ragel < Autotools
   description 'Compiles finite state machines from regular languages into executable C, C++, Objective-C, or D code.'
   homepage 'https://www.colm.net/open-source/ragel/'
-  version '6.10-1'
+  version '6.10'
   license 'GPL-2.0-only'
   compatibility 'all'
-  source_url 'https://www.colm.net/files/ragel/ragel-6.10.tar.gz'
+  source_url "https://www.colm.net/files/ragel/ragel-#{version}.tar.gz"
   source_sha256 '5f156edb65d20b856d638dd9ee2dfb43285914d9aa2b6ec779dac0270cd56c3f'
   binary_compression 'tar.zst'
 
