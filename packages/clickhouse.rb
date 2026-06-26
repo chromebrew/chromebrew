272c0f42749194eba113a0f8018c9c86c4bfef0c
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-06-26 01:02:14 +0000

    clickhouse -> 26.6.1.1193-stable in updater-clickhouse-26.6.1.1193-stable

--- packages/clickhouse.rb
+++ packages/clickhouse.rb
@@ -3,11 +3,11 @@ require 'package'
 class Clickhouse < Package
   description 'Real-time analytics DBMS'
   homepage 'https://clickhouse.com/'
-  version '26.5.1.882-stable'
+  version '26.6.1.1193-stable'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/ClickHouse/ClickHouse/releases/download/v#{version}/clickhouse-common-static-#{version.split('-')[0]}-amd64.tgz"
-  source_sha256 'd93e0e9f6f684f0b233b82cbdf96e8cc9428b57447c5f4ed2b7d72368c974e66'
+  source_sha256 '8bb014df471a7dc65808ad3e13d792de089a093abadc9760760de95c15fe9241'
 
   no_compile_needed
   no_shrink
