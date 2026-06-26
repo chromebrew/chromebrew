b3d5d4489ffc045fec3f2ba0a33135a456862208
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-06-26 01:03:18 +0000

    gitlab_runner -> 19.1.1 in updater-gitlab_runner-19.1.1

--- packages/gitlab_runner.rb
+++ packages/gitlab_runner.rb
@@ -3,7 +3,7 @@ require 'package'
 class Gitlab_runner < Package
   description 'Run your CI/CD jobs and send the results back to GitLab'
   homepage 'https://gitlab.com/gitlab-org/gitlab-runner'
-  version '19.1.0'
+  version '19.1.1'
   license 'MIT'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Gitlab_runner < Package
      x86_64: "https://s3.dualstack.us-east-1.amazonaws.com/gitlab-runner-downloads/v#{version}/binaries/gitlab-runner-linux-amd64"
   })
   source_sha256({
-    aarch64: 'b1cff0e2c47ffd3c7f1e64e9a86561769ba201f75dcadf3e9da248d38c762d06',
-     armv7l: 'b1cff0e2c47ffd3c7f1e64e9a86561769ba201f75dcadf3e9da248d38c762d06',
-       i686: '23360335711960146ac683bef6e8989dcb421da04667ec1e8dd0f14cf85bde84',
-     x86_64: '895e4dfb4bae77b6c0f0611cf9af75b95312ab40c5d8d2c607b5be1c1fae9511'
+    aarch64: '429942b3e71b8972172f76bfc74e37496b3d2cf02aba2fda8e2358ad43cdb11e',
+     armv7l: '429942b3e71b8972172f76bfc74e37496b3d2cf02aba2fda8e2358ad43cdb11e',
+       i686: '0832f1099d32d135bb523461dc4aab577f53d48cb0bf3411753e4c6ad03c2388',
+     x86_64: '8c8ea572b9f0d40e93b876b90c7d093eb403a41bbadbbf9a0fcf27b6b75c2800'
   })
 
   no_compile_needed
