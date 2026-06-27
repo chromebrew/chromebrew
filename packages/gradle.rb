bcb2abff65ceb338e1a059e042184c42d5d48982
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-06-27 01:00:09 +0000

    gradle -> 9.6.1 in updater-gradle-9.6.1

--- packages/gradle.rb
+++ packages/gradle.rb
@@ -3,11 +3,11 @@ require 'package'
 class Gradle < Package
   description 'From mobile apps to microservices, from small startups to big enterprises, Gradle helps teams build, automate and deliver better software, faster.'
   homepage 'https://gradle.org/'
-  version '9.6.0'
+  version '9.6.1'
   license 'Apache-2.0'
   compatibility 'all'
   source_url "https://github.com/gradle/gradle-distributions/releases/download/v#{version}/gradle-#{version}-bin.zip"
-  source_sha256 'bbaeb2fef8710818cf0e261201dab964c572f92b942812df0c3620d62a529a01'
+  source_sha256 '9c0f7faeeb306cb14e4279a3e084ca6b596894089a0638e68a07c945a32c9e14'
 
   depends_on 'openjdk8' unless File.exist? "#{CREW_PREFIX}/bin/java"
 
