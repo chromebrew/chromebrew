e2524f332f53881ec36f9270f571c7698047c7ae
Ed Reel <edreel@gmail.com>
2026-06-28 20:52:02 -0500

    Mobile_broadband_provider_info 20240316 => 20240407 (#17174)

--- packages/mobile_broadband_provider_info.rb
+++ packages/mobile_broadband_provider_info.rb
@@ -3,17 +3,17 @@ require 'buildsystems/meson'
 class Mobile_broadband_provider_info < Meson
   description 'Network Management daemon'
   homepage 'https://gitlab.gnome.org/GNOME/mobile-broadband-provider-info'
-  version '20240316'
+  version '20240407'
   license 'CC-PD'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/mobile-broadband-provider-info.git'
-  git_hashtag 'df3c25cf6f8257805e77effdcbdaea10adc9419f'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '44259ceaf722de3e14fe73303d5518309d3252e9dac2df2a8dd005c2de5ec845',
-     armv7l: '44259ceaf722de3e14fe73303d5518309d3252e9dac2df2a8dd005c2de5ec845',
-     x86_64: 'ba3e807317fa0f2bf2cf593fa927dff33eb383d9fe4152ee5ee3fcdd35f5f25c'
+    aarch64: 'dded72b0145848af57f0572e89570734058b44cca038e1d096ea734dbd6b8812',
+     armv7l: 'dded72b0145848af57f0572e89570734058b44cca038e1d096ea734dbd6b8812',
+     x86_64: '47a969f93e38f2d30de24d3da415aba252f46d0b7e2ae443275bcbb852177515'
   })
 
   depends_on 'libxslt' => :build
