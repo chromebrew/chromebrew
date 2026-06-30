51099c5c9c3aa92be3643af4c0c2907bb82de663
Ed Reel <edreel@gmail.com>
2026-06-29 20:47:00 -0500

    Modemmanager 1.16.2 => 1.24.2 (#17178)
    Libmbim 1.24.6 => 1.34.0
    
    Libqim 1.28.2 => 1.38.0
    
    Add libqrtr_glib package

--- packages/libqmi.rb
+++ packages/libqmi.rb
@@ -1,33 +1,25 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Libqmi < Package
+class Libqmi < Meson
   description 'libqmi is a glib-based library for talking to WWAN modems and devices which speak the Qualcomm MSM Interface (QMI) protocol.'
   homepage 'https://www.freedesktop.org/wiki/Software/libqmi/'
-  version '1.28.2'
+  version '1.38.0'
   license 'LGPL-2'
   compatibility 'aarch64 armv7l x86_64'
-  source_url "https://www.freedesktop.org/software/libqmi/libqmi-#{version}.tar.xz"
-  source_sha256 '8c8c3ee719874d2529bce9b35b028fe435b36f003979a360d3ad0938449db783'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.freedesktop.org/mobile-broadband/libqmi.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '03ccdba7bf32be2b5e02951d25a664af9654acceb1eb5c71fcbf5179a46190d7',
-     armv7l: '03ccdba7bf32be2b5e02951d25a664af9654acceb1eb5c71fcbf5179a46190d7',
-     x86_64: '49c2629596059768498962291481e1d0212bdb2e56b5bdf37f834aeb84fe8680'
+    aarch64: '9db4f8109ce7732da2d34d19c68e573ec583b17610e62265119138aeaa49dca2',
+     armv7l: '9db4f8109ce7732da2d34d19c68e573ec583b17610e62265119138aeaa49dca2',
+     x86_64: '4451de048b938be627e113be45464926fa3bcc181b0d01311e2a90192305ba46'
   })
 
-  depends_on 'libgudev'
-
-  def self.build
-    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
-      LDFLAGS='-flto=auto' \
-      ./configure \
-      #{CREW_CONFIGURE_OPTIONS} \
-      --disable-maintainer-mode"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'libgudev' => :executable
+  depends_on 'libmbim' => :library
+  depends_on 'libqrtr_glib' => :library
 end
--- packages/modemmanager.rb
+++ packages/modemmanager.rb
@@ -1,35 +1,30 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Modemmanager < Package
+class Modemmanager < Meson
   description 'ModemManager is a DBus-activated daemon which controls mobile broadband (2G/3G/4G) devices and connections.'
   homepage 'https://www.freedesktop.org/wiki/Software/ModemManager/'
-  version '1.16.2'
+  version '1.24.2'
   license 'GPL-2+'
   compatibility 'aarch64 armv7l x86_64'
-  source_url "https://www.freedesktop.org/software/ModemManager/ModemManager-#{version}.tar.xz"
-  source_sha256 'efa9a963499e0885f3f163096d433334143c4937545134ecd682e0157fa591e3'
-  binary_compression 'tar.xz'
+  source_url 'https://gitlab.freedesktop.org/mobile-broadband/ModemManager.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '63659ccd41a81696e3ac2036d21bcb2279bd3b9aff60de09ef2fbd443712c711',
-     armv7l: '63659ccd41a81696e3ac2036d21bcb2279bd3b9aff60de09ef2fbd443712c711',
-     x86_64: '290c162bd314ba5d52cdc5f3a9d8abe1efc78349214392c237885653cfdf76a2'
+    aarch64: '94f3c8bbd650ef793c33b776ddf0595d5802dc47a351145d4f9c05b1fa26d215',
+     armv7l: '94f3c8bbd650ef793c33b776ddf0595d5802dc47a351145d4f9c05b1fa26d215',
+     x86_64: 'e7b396102ea6fdeda439dcae3f55cb60598e512f2d276d1699e17187c7dd2e10'
   })
 
-  depends_on 'libmbim'
-  depends_on 'libqmi'
+  depends_on 'dbus' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'libgudev' => :executable
+  depends_on 'libmbim' => :library
+  depends_on 'libqmi' => :library
+  depends_on 'libqrtr_glib' => :executable
+  depends_on 'polkit' => :executable
 
-  def self.build
-    system "env CFLAGS='-pipe -flto=auto' CXXFLAGS='-pipe -flto=auto' \
-      LDFLAGS='-flto=auto' \
-      ./configure \
-      #{CREW_CONFIGURE_OPTIONS} \
-      --with-dbus-sys-dir=#{CREW_PREFIX}/share/dbus-1 \
-      --disable-maintainer-mode"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  meson_options '-Dsystemdsystemunitdir=no -Dsystemd_suspend_resume=false -Dsystemd_journal=false'
 end
