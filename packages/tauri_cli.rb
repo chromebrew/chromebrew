a8f1835f291395b8d5741d023647d2ae749a2604
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-06-30 01:07:54 +0000

    tauri_cli -> 2.11.4 in updater-tauri_cli-2.11.4

--- packages/tauri_cli.rb
+++ packages/tauri_cli.rb
@@ -3,12 +3,12 @@ require 'package'
 class Tauri_cli < Package
   description 'Build smaller, faster, and more secure desktop and mobile applications with a web frontend.'
   homepage 'https://tauri.app/'
-  version '2.11.3'
+  version '2.11.4'
   license 'Apache-2.0, MIT'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/tauri-apps/tauri/releases/download/tauri-cli-v#{version}/cargo-tauri-x86_64-unknown-linux-gnu.tgz"
-  source_sha256 '56ebe242d3cab6747dea41b47cdabace47fbea86d65c796f34a95d620d3bad1d'
+  source_sha256 '6864602a34292aa6f2ad40ae019eebe5c1064d6c623fe20696a8a8974067e60b'
 
   no_compile_needed
 
