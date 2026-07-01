ac0769087387c529dce1aabf42e2e5067c2350c0
Ed Reel <edreel@gmail.com>
2026-07-01 08:31:56 +0000

    Moreutils 0.69 => 0.70
    Add perl_io_pty and perl_ipc_run dependencies

--- packages/moreutils.rb
+++ packages/moreutils.rb
@@ -1,24 +1,29 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Moreutils < Package
+class Moreutils < Autotools
   description 'Growing collection of the unix tools that nobody thought to write long ago when unix was young.'
   homepage 'https://joeyh.name/code/moreutils/'
-  version '0.69'
+  version '0.70'
   license 'GPL-2'
   compatibility 'all'
   source_url "http://http.debian.net/debian/pool/main/m/moreutils/moreutils_#{version}.orig.tar.xz"
-  source_sha256 '2170c46219ce8d6f17702321534769dfbfece52148a78cd12ea73b5d3a72ff7c'
+  source_sha256 'a844c5e3360a73d12c0a5624750ecc1969d64afea2e84925328f137576e2eb55'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c83203b78454cb54457723aa593e062b3bae81eb53ac29e5cf5eccb987b0fccf',
-     armv7l: 'c83203b78454cb54457723aa593e062b3bae81eb53ac29e5cf5eccb987b0fccf',
-       i686: 'f160028bbb5067a324e03d348c239c739b4bc890b8f721d767a51365ad537ad5',
-     x86_64: 'c0347330127aed4625b1108ca4144a5569dee1add6932c63d804589b9be177cc'
+    aarch64: '9849c38dc05d08c82fc4898e268d47909451b1d6455ff83cb50e8e75099aa789',
+     armv7l: '9849c38dc05d08c82fc4898e268d47909451b1d6455ff83cb50e8e75099aa789',
+       i686: '0875f08ef243ff2dd953fa4c9883b8f743d8122800ef5e9dac6d5b324a1e4986',
+     x86_64: '919c1c1aee64008e5f97a8c413b8618f4f64d7958d634dd62403ee1e2a158718'
   })
 
   depends_on 'docbook_xml' => :build
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
   depends_on 'libxslt' => :build
+  depends_on 'perl_ipc_run' => :executable
+
+  autotools_skip_configure
 
   def self.patch
     system "sed -i 's,PREFIX?=/usr,PREFIX?=#{CREW_PREFIX},' Makefile"
@@ -26,11 +31,9 @@ class Moreutils < Package
     system "sed -i 's,share/man,man,g' Makefile"
   end
 
-  def self.build
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  autotools_install_extras do
+    Dir["#{CREW_DEST_PREFIX}/bin/*"].each do |bin|
+      system "sed -i 's,/usr/bin/perl,#{CREW_PREFIX}/bin/perl,' #{bin}"
+    end
   end
 end
