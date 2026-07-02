734f6cde22d27a123b55333061fa0277b9d2d7b5
Satadru Pramanik <satadru@gmail.com>
2026-07-01 23:33:08 -0400

    Remove i686 compatibility from strace.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/strace.rb
+++ packages/strace.rb
@@ -5,7 +5,7 @@ class Strace < Autotools
   homepage 'https://strace.io/'
   version '7.1'
   license 'BSD'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url "https://strace.io/files/#{version}/strace-#{version}.tar.xz"
   source_sha256 '81743ecf2a5b44186b2f5038afdc8beda7e5c70aed15b4fbfbcc6e9ece24490f'
   binary_compression 'tar.zst'
@@ -13,16 +13,11 @@ class Strace < Autotools
   binary_sha256({
     aarch64: '0b564f88270e87fdb1e4ef55d985f7ac5cefa77ab4db9ecef476037cf66e23c3',
      armv7l: '0b564f88270e87fdb1e4ef55d985f7ac5cefa77ab4db9ecef476037cf66e23c3',
-       i686: '970f7b5a47fe5a2bd254e8457f3acc8cd70f27f85e9823ca29e48d59e563b6b4',
      x86_64: '03f78a27e21c3535bdbcf79f17fd4322cdd0816e58db84397bb23eed5b147c32'
   })
 
-  depends_on 'elfutils' # Needed for stack trace support
   depends_on 'elfutils' => :executable
   depends_on 'glibc' => :executable
-
-  # This needs to be built with linux headers 5.15 on x86_64 via
-  # CREW_KERNEL_VERSION=5.15 crew upgrade linuxheaders ; CREW_KERNEL_VERSION=5.15 crew build strace
 
   autotools_configure_options '--disable-mpers --with-libdw --disable-gcc-Werror'
 end
