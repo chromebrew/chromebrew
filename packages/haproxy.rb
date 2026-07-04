b52bb87598818abfb0f7139fb58a2fac04c64dac
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-07-04 00:54:53 +0000

    haproxy -> 3.4.2 in updater-haproxy-3.4.2

--- packages/haproxy.rb
+++ packages/haproxy.rb
@@ -3,11 +3,11 @@ require 'package'
 class Haproxy < Package
   description 'The Reliable, High Performance TCP/HTTP Load Balancer.'
   homepage 'https://www.haproxy.org/'
-  version '3.4.1'
+  version '3.4.2'
   license 'GPL-2 and LGPL-2.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://www.haproxy.org/download/#{version.split('.')[0..1].join('.')}/src/haproxy-#{version}.tar.gz"
-  source_sha256 '2e62c4ce4fd77d3bc7cf17e586431663454456a078b7c8465b8f0125b5bc22f8'
+  source_sha256 'b1330dbb0d6e6bc4a72c4708a6a9e585579cd1156dfe5763c26305105bc12907'
   binary_compression 'tar.zst'
 
   binary_sha256({
