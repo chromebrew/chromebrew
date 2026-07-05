473bdebf17a60ef9b6962fe7c48de71f4a965d75
Ed Reel <edreel@gmail.com>
2026-07-05 01:16:17 +0000

    Ndisc6 1.0.4 => 1.0.8

--- packages/ndisc6.rb
+++ packages/ndisc6.rb
@@ -1,28 +1,24 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Ndisc6 < Package
+class Ndisc6 < Autotools
   description 'Small collection of useful tools for IPv6 networking (ndisc6, rdisc6, tcptraceroute6, traceroute6, rdnssd).'
   homepage 'https://www.remlab.net/ndisc6/'
-  version '1.0.4'
+  version '1.0.8'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://www.remlab.net/files/ndisc6/ndisc6-1.0.4.tar.bz2'
-  source_sha256 'abb1da4a98d94e5abe1dd7b1c975de540306b0581cbbd36aff035118b2f25c1f'
-  binary_compression 'tar.xz'
+  source_url "https://www.remlab.net/files/ndisc6/ndisc6-#{version}.tar.bz2"
+  source_sha256 '1f2fb2dc1172770aa5a09d39738a44d8b753cc5e2e25e306ca78682f9fea0b4f'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5a9f7557dd481117483f81035f168a8c0de9a2210c57b049219727f6546fc2b1',
-     armv7l: '5a9f7557dd481117483f81035f168a8c0de9a2210c57b049219727f6546fc2b1',
-       i686: 'b2d727341b6a5302dda0da0529635b11cc4e04a1ca1e8eb129ab53bafae17a0f',
-     x86_64: '2fe334ddc96bd956663ceb593383800e9d8216fb867beadd5ecb099d41504148'
+    aarch64: '18e22de4da573e25a38c5dd108a4121e45278129b90bc6faee91a41c666d538d',
+     armv7l: '18e22de4da573e25a38c5dd108a4121e45278129b90bc6faee91a41c666d538d',
+       i686: '9bcdc5cee50c6cc0023226ae9c5a0628861650ef7b99788362c8de15e68fe610',
+     x86_64: '4903331acbaefa2f66db1514c08ee5b614508432d2b1fbe21a0392191020c099'
   })
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS} --disable-suid-install"
-    system 'make'
-  end
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  autotools_configure_options '--disable-suid-install'
 end
