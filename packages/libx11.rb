c6844ab451972c732743411a9b1fb22e6ba94d4f
satmandu <satmandu@users.noreply.github.com>
2026-07-08 02:40:48 +0000

    updater-libtiff-4.7.2: Package File Update Run on linux/arm/v7 container.

--- packages/libx11.rb
+++ packages/libx11.rb
@@ -5,7 +5,7 @@ class Libx11 < Autotools
   homepage 'https://x.org/wiki/'
   version '1.8.12'
   license 'X11'
-  compatibility 'aarch64 armv7l x86_64'
+  compatibility 'all'
   source_url 'https://gitlab.freedesktop.org/xorg/lib/libx11.git'
   git_hashtag "libX11-#{version}"
   binary_compression 'tar.zst'
@@ -13,13 +13,15 @@ class Libx11 < Autotools
   binary_sha256({
     aarch64: '9d9aacc2ecc3e54fa78057498456e235304c4f11f2e834d4120961b69070b0ea',
      armv7l: '9d9aacc2ecc3e54fa78057498456e235304c4f11f2e834d4120961b69070b0ea',
+       i686: '906d6c0eb0d44d46ce4c9a80d21a09b63adb4bf43a9ea04aeb6b1a1f576b0f03',
      x86_64: '472ca926ca8cf52d5ee45b609c329e955630920a8116663d25d1dc38cf649c33'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'libxau' # R
-  depends_on 'libxcb' # R
-  depends_on 'libxdmcp' # R
+  depends_on 'glibc' => :library
+  depends_on 'libbsd' => :library
+  depends_on 'libxau' => :library
+  depends_on 'libxcb' => :library
+  depends_on 'libxdmcp' => :library
   depends_on 'libxtrans' => :build
   depends_on 'llvm_lib' => :build
   depends_on 'xorg_proto' => :build
