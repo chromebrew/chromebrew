5e49d188c0c8a7a71a00c5a2128a2fd0da5d5ed3
Satadru Pramanik <satadru@gmail.com>
2026-07-07 22:32:03 -0400

    Fix libtiff build.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/libtiff.rb
+++ packages/libtiff.rb
@@ -7,35 +7,35 @@ class Libtiff < CMake
   license 'libtiff'
   compatibility 'all'
   source_url 'https://github.com/libsdl-org/libtiff.git'
-  git_hashtag "v#{version.split('-').first}"
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '95226e90392eed108b5863a692f2c4bd39a1c524e1f330868e2f471655008310',
-     armv7l: '95226e90392eed108b5863a692f2c4bd39a1c524e1f330868e2f471655008310',
-       i686: 'ae9cbd6400afb1f4423fefe190336be4c9d90dec3b9aa2364e32f8fb837cb555',
-     x86_64: '1199576773668006bc8af5553deffda57a45e18c6b2d3654c0f87595ae721d74'
+    aarch64: 'e65411777c8eda46b9daad9868d19f7948c1ec16e222b8e40736f95be0eac4ad',
+     armv7l: 'e65411777c8eda46b9daad9868d19f7948c1ec16e222b8e40736f95be0eac4ad',
+       i686: '47538b362b03d6c713ada6c303451bd24f5e68e0c62bd182006c2bbae4a6c4b0',
+     x86_64: '1a6ed0b5a95a367b263ccb68ef47daaaeafc1bf5f820b51d4502dc7282eb67a7'
   })
 
   depends_on 'freeglut' unless ARCH == 'i686' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
   depends_on 'imake' => :build
-  depends_on 'jbigkit' # R
-  depends_on 'libdeflate' # R
+  depends_on 'jbigkit' => :library
+  depends_on 'libdeflate' => :library
   depends_on 'libglu' unless ARCH == 'i686' # R
   depends_on 'libglvnd' unless ARCH == 'i686' # R
   depends_on 'libice' unless ARCH == 'i686' # R
-  depends_on 'libjpeg_turbo' # R
+  depends_on 'libjpeg_turbo' => :library
   depends_on 'libsm' unless ARCH == 'i686' # R
   depends_on 'libx11' unless ARCH == 'i686' # R
   depends_on 'libxi' unless ARCH == 'i686' # R
   depends_on 'mesa' => :build unless ARCH == 'i686'
   depends_on 'sphinx' => :build
   depends_on 'wget2' => :build
-  depends_on 'xzutils' # R
-  depends_on 'zlib' # R
-  depends_on 'zstd' # R
+  depends_on 'xzutils' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
 
   gnome
 
