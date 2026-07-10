478a38e68bb19a621d5f614deb66cb314e8c8a26
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-07-10 00:55:14 +0000

    opera -> 133.0.5932.34 in updater-opera-133.0.5932.34

--- packages/opera.rb
+++ packages/opera.rb
@@ -3,12 +3,12 @@ require 'package'
 class Opera < Package
   description 'Opera is a multi-platform web browser based on Chromium and developed by Opera Software.'
   homepage 'https://www.opera.com/'
-  version '133.0.5932.24'
+  version '133.0.5932.34'
   license 'OPERA-2018'
   compatibility 'x86_64'
 
   source_url "https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/opera-stable_#{version}_amd64.deb"
-  source_sha256 'bc8a5b3cb19da2b880b593b6b469c60b42bacfc7c885a693260e52e988fdeae3'
+  source_sha256 '2867eda628e9d9ea5d7c0a5553b4504bd93ab198fd08703dd3292033ba4c020f'
 
   no_compile_needed
   no_shrink
