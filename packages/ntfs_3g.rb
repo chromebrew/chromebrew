488d1f63481a10f30f95d7a91e049df8281acab2
Ed Reel <edreel@gmail.com>
2026-07-11 05:39:49 -0500

    Ntfs_3g 2022.10.3 => 2026.2.25 (#17460)

--- packages/ntfs_3g.rb
+++ packages/ntfs_3g.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Ntfs_3g < Autotools
   description 'NTFS-3G Safe Read/Write NTFS Driver'
   homepage 'https://github.com/tuxera/ntfs-3g'
-  version '2022.10.3'
+  version '2026.2.25'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/tuxera/ntfs-3g.git'
@@ -11,14 +11,15 @@ class Ntfs_3g < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '20a7ab1f3292de6dd70298bfd1939d09ee1326a2bd6b83da0134bd9142297e53',
-     armv7l: '20a7ab1f3292de6dd70298bfd1939d09ee1326a2bd6b83da0134bd9142297e53',
-       i686: '03b6d129a013d7f219d3ddd29ece882c6b6821fd4e163d9cef6373970c3a5aa7',
-     x86_64: '24498aee82923066f076c889d1df019477797fc6f682d0d49426cd0c31875db2'
+    aarch64: '6a0945e41b06452ba9b4e7e2b46c45d87a47b0287dd201e94c234d16243bdd92',
+     armv7l: '6a0945e41b06452ba9b4e7e2b46c45d87a47b0287dd201e94c234d16243bdd92',
+       i686: '075120ba32bbf1b1b4b0cacccb88981f3d0a7981385afbccce56e23f2721ab31',
+     x86_64: '7b7ef5e12c8f9d0856575b3dcf0c25ca184a04eb5b65f8416c76b87b1844532b'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'util_linux' # R
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'util_linux' => :executable
 
   autotools_configure_options "--exec-prefix=#{CREW_PREFIX} --disable-ntfs-3g"
 end
