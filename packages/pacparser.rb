7ada70198f7a052d9d86811774ff748cd8044c86
Ed Reel <edreel@gmail.com>
2026-07-19 23:27:02 +0000

    Pacparser 1.3.7-1 => 1.5.1

--- packages/pacparser.rb
+++ packages/pacparser.rb
@@ -3,25 +3,29 @@ require 'package'
 class Pacparser < Package
   description 'pacparser is a library to parse proxy auto-config (PAC) files.'
   homepage 'https://pacparser.manugarg.com/'
-  version '1.3.7-1'
+  version '1.5.1'
   license 'LGPL-3'
   compatibility 'all'
-  source_url 'https://github.com/pacparser/pacparser/archive/1.3.7.tar.gz'
-  source_sha256 '575c5d8096b4c842b2af852bbb8bcfde96170b28b49f33249dbe2057a8beea13'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/pacparser/pacparser.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'b5552a187ea7fdc2bdf7038e9af576b5dab3beb31e10be7f9fcf87345cad3269',
-     armv7l: 'b5552a187ea7fdc2bdf7038e9af576b5dab3beb31e10be7f9fcf87345cad3269',
-       i686: 'e19682f29ee5ee1671ede739a231a273e24f1d4e589c97db1661b38b02674145',
-     x86_64: '19fba2248fd1de821160e42a66c01d5e9543e20996e4d6c4795a35d7325e3a65'
+    aarch64: 'dba823a01d88db2af7e68015fbaadae2eb601cd1346a0a6aa387a7461f45c1b2',
+     armv7l: 'dba823a01d88db2af7e68015fbaadae2eb601cd1346a0a6aa387a7461f45c1b2',
+       i686: 'dd7339aa274690f26ebf100df056a2c04ddda1fc478c4c4b63f74f7745296944',
+     x86_64: '86853d02df16871221d2c0f5637115fbf6dd7add392f8e289e412a7b8849d812'
   })
 
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+
   def self.build
-    system 'make -j1 -C src'
+    system "CFLAGS='-lpthread' make -j1 -C src"
   end
 
   def self.install
     system "DESTDIR=#{CREW_DEST_DIR} PREFIX=#{CREW_PREFIX} make -C src install"
+    FileUtils.mv "#{CREW_DEST_PREFIX}/lib", CREW_DEST_LIB_PREFIX if ARCH.eql?('x86_64')
   end
 end
