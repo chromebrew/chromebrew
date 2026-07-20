9aa4fb36ed1a25f14b07f55a3247c96a38c8dc7e
Ed Reel <edreel@gmail.com>
2026-07-20 16:42:31 -0500

    Papirus_icon_theme 20200801 => 20250501 (#17604)

--- packages/papirus_icon_theme.rb
+++ packages/papirus_icon_theme.rb
@@ -3,14 +3,16 @@ require 'package'
 class Papirus_icon_theme < Package
   description 'SVG icon theme for Linux, based on Paper Icon Set'
   homepage 'https://github.com/PapirusDevelopmentTeam/papirus-icon-theme'
-  version '20200801'
+  version '20250501'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/20200801.tar.gz'
-  source_sha256 '30c1f537b8fd5c4e5991fcd96d62cf2119a3a844b6e8fc55a4a08841b49137fd'
+  source_url 'https://github.com/PapirusDevelopmentTeam/papirus-icon-theme.git'
+  git_hashtag version
+
+  no_compile_needed
 
   def self.install
     FileUtils.mkdir_p "#{CREW_DEST_PREFIX}/share/icons/"
-    FileUtils.cp_r Dir.glob('*Papirus*'), "#{CREW_DEST_PREFIX}/share/icons/"
+    FileUtils.cp_r Dir['*Papirus*'], "#{CREW_DEST_PREFIX}/share/icons/"
   end
 end
