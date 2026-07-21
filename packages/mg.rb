a9ee54472702add920b01e27a71d34d94658cd2c
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-07-21 00:48:33 +0000

    mg -> 20260719 in updater-mg-20260719

--- packages/mg.rb
+++ packages/mg.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Mg < Autotools
   description 'mg is the portable version of the mg editor from OpenBSD'
   homepage 'https://github.com/hboetes/mg/'
-  version '20260227'
+  version '20260719'
   license 'public-domain'
   compatibility 'all'
   source_url 'https://github.com/hboetes/mg.git'
