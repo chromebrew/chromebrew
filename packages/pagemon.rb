7cfed2c7034dbfb5949be287898e8bd81a5d4c08
Ed Reel <edreel@gmail.com>
2026-07-21 18:43:47 +0000

    Pagemon 0.02.05 => 0.02.06

--- packages/pagemon.rb
+++ packages/pagemon.rb
@@ -3,7 +3,7 @@ require 'package'
 class Pagemon < Package
   description 'Pagemon is an interactive memory/page monitoring tool allowing one to browse the memory map of an active running process.'
   homepage 'https://github.com/ColinIanKing/pagemon'
-  version '0.02.05'
+  version '0.02.06'
   license 'GPL-2'
   compatibility 'all'
   source_url 'https://github.com/ColinIanKing/pagemon.git'
@@ -11,10 +11,10 @@ class Pagemon < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '03bc0311994ece280d5511d97847503c2e2793c037cc9225367fe9d5eca5c00e',
-     armv7l: '03bc0311994ece280d5511d97847503c2e2793c037cc9225367fe9d5eca5c00e',
-       i686: '80cef6fd289eaf68c841491cd76dae1a4b7e043484fdef66860ca1e3f5433670',
-     x86_64: '9efa248be2e4d1be3d420051ea17c17aaafa3232b63efa4ee6e98cb89499acab'
+    aarch64: '7090fa3616ffe16dec3ea81277531d2062a60374f2cd34467f69fe81ef86a924',
+     armv7l: '7090fa3616ffe16dec3ea81277531d2062a60374f2cd34467f69fe81ef86a924',
+       i686: 'c0ef52b6e2604f3e13de266c0cb1e6f1c8970eb61093080cd8040d24ea226d32',
+     x86_64: 'c42726dffc0d91e416fd3de87a696912b6e375873108db02dc36f9aeb1b341c9'
   })
 
   depends_on 'glibc' => :executable
