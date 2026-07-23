29800074c66cd94fe02dfcf93b4ce95326f9a2be
Ed Reel <edreel@gmail.com>
2026-07-23 23:59:19 +0000

    Asmc 2.38 => 2.39

--- packages/asmc.rb
+++ packages/asmc.rb
@@ -4,17 +4,17 @@ class Asmc < Package
   description 'Asmc Macro Assembler'
   homepage 'https://github.com/nidud/asmc'
   license 'GPL-2.0'
-  version '2.38'
+  version '2.39'
   compatibility 'all'
   source_url 'https://github.com/nidud/asmc.git'
   git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'a2916448cb69f6e19dccd24e50b4418db76aab39f657b0b3d62be042a6371476',
-     armv7l: 'a2916448cb69f6e19dccd24e50b4418db76aab39f657b0b3d62be042a6371476',
-       i686: 'b9ed1b02d45dad14a9e013a0177f436ffce15844a2d8588afd895bb002126cbd',
-     x86_64: '54473d9215b5e13215f7221e2692dd8d071b51d40926e301cc3cb2effb81c505'
+    aarch64: 'e4e66ec418d389c19ce28c740d083e41d16ee3f7bea90d31b89b964758f75614',
+     armv7l: 'e4e66ec418d389c19ce28c740d083e41d16ee3f7bea90d31b89b964758f75614',
+       i686: 'f9ab49087ea87467a9b30b8c49dedf049d44798dd1480bbb7b980f5028bb6ee8',
+     x86_64: '344903c084b24ea8df7a58428bd5ba580d5cde9fa216a40a8a598371fbb0dcfc'
   })
 
   def self.build
