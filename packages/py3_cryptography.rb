7068890e028abbf854466de0890e00e4373fe24c
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-07-24 00:37:55 +0000

    py3_cryptography -> 49.0.0-py3.14 in updater-py3_cryptography-49.0.0-py3.14

--- packages/py3_cryptography.rb
+++ packages/py3_cryptography.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_cryptography < Pip
   description 'Cryptography provides cryptographic recipes and primitives to Python developers.'
   homepage 'https://cryptography.io/'
-  version "48.0.0-#{CREW_PY_VER}"
+  version "49.0.0-#{CREW_PY_VER}"
   license 'MIT'
   compatibility 'all'
   source_url 'SKIP'
