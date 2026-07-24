918ee225dde1c58a5aef15a0f99f1b08a16104ba
Ed Reel <edreel@gmail.com>
2026-07-24 06:26:07 +0000

    Powerstat 0.02.24 => 0.04.06

--- packages/powerstat.rb
+++ packages/powerstat.rb
@@ -2,20 +2,23 @@ require 'package'
 
 class Powerstat < Package
   description 'Powerstat measures the power consumption of a laptop using the ACPI battery information.'
-  homepage 'https://kernel.ubuntu.com/~cking/powerstat/'
-  version '0.02.24'
+  homepage 'https://github.com/ColinIanKing/powerstat'
+  version '0.04.06'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://kernel.ubuntu.com/~cking/tarballs/powerstat/powerstat-0.02.24.tar.gz'
-  source_sha256 '12781cb108be1fc3be5ec893e6d025bfb40ada060bdc5f7715b65397620f2c7b'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/ColinIanKing/powerstat.git'
+  git_hashtag "V#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '76653569b562862ac42fd13a04a0f89b450da94450b6a25eccb7e1e399267dcb',
-     armv7l: '76653569b562862ac42fd13a04a0f89b450da94450b6a25eccb7e1e399267dcb',
-       i686: '69f76808c40ed957f30738fbb0e76d50a592ec610f8d58ca117e6daceee8e51f',
-     x86_64: '1be1187a936d8e2c0bbe20ad45a350c8b63ce75ef77ee63fd7bbe0b33690c541'
+    aarch64: '1d35badff9788712149a2593a8e5262cc5f60bbebf60ebe754a4dec7ee9c2b54',
+     armv7l: '1d35badff9788712149a2593a8e5262cc5f60bbebf60ebe754a4dec7ee9c2b54',
+       i686: 'd0a2d380c846673a72f0bbe11e1139437a9f857995869567be5a4de299d714cc',
+     x86_64: '1116cc08bc8f783b806fb3b130c1641b790a56fdb0486da23c57557ab19f47f4'
   })
+
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
 
   def self.build
     system "sed -i 's,/usr,#{CREW_PREFIX},g' Makefile"
