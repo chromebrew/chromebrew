e5ec2d1f386425afb64b6088c04e8b29c8e82dec
Ed Reel <edreel@gmail.com>
2026-07-24 03:35:53 +0000

    Plutosvg 0.0.7 => 0.0.8

--- packages/plutosvg.rb
+++ packages/plutosvg.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Plutosvg < Meson
   description 'Tiny SVG rendering library in C'
   homepage 'https://github.com/sammycage/plutosvg'
-  version '0.0.7'
+  version '0.0.8'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/sammycage/plutosvg.git'
@@ -14,14 +14,15 @@ class Plutosvg < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6e634c8138998104ed3b153f225bb34f06baa71c145751e808f4843087250feb',
-     armv7l: '6e634c8138998104ed3b153f225bb34f06baa71c145751e808f4843087250feb',
-     x86_64: '03db96961450d5e3f4240fa37fd3d625a983a66a70561b7bbde159d0ed99e8de'
+    aarch64: '189cbe539c74e4afead95eb09174c74d31a2d959ee4a34774c9097637ad476fc',
+     armv7l: '189cbe539c74e4afead95eb09174c74d31a2d959ee4a34774c9097637ad476fc',
+     x86_64: '64dee5fc75b71355d4bdb0db48090fe6035ad6f2578abdc97f9296299955c2c3'
   })
 
-  depends_on 'freetype' # R
-  depends_on 'glibc' # R
-  depends_on 'plutovg' # R
+  depends_on 'freetype' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'plutovg' => :library
 
   meson_options '-Dfreetype=enabled \
                  -Dexamples=disabled \
