24b41dca1fb415bc756b60db8b1a6b91c3209f19
Ed Reel <edreel@gmail.com>
2026-07-28 18:50:48 -0500

    Rav1e 0.5.1-p20220927 => 0.8.1 (#17782)

--- packages/rav1e.rb
+++ packages/rav1e.rb
@@ -3,26 +3,28 @@ require 'package'
 class Rav1e < Package
   description 'An AV1 encoder focused on speed and safety'
   homepage 'https://github.com/xiph/rav1e/'
-  version '0.5.1-p20220927'
+  version '0.8.1'
   license 'BSD-2, Apache-2.0, MIT and Unlicense'
   compatibility 'all'
   source_url 'https://github.com/xiph/rav1e.git'
-  git_hashtag 'p20220927'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1a25d78f87f3f361cdfd4e1c98c0bea454a96bd73744b2a004d7a58b1ecfb6f0',
-     armv7l: '1a25d78f87f3f361cdfd4e1c98c0bea454a96bd73744b2a004d7a58b1ecfb6f0',
-       i686: 'fdf45f72a79a60b668eb49a80b6d63cb8ea7b3dd0eaa272390b105d20e3aa6a8',
-     x86_64: '8caaa04e32e710632b8d5e465dd323745a2e5d6cd55957b76ddef735487643f2'
+    aarch64: '315ec6326337d5a7f3576c97fd7ecad8197ce9f3c4177692e3f667cb1d00bcef',
+     armv7l: '315ec6326337d5a7f3576c97fd7ecad8197ce9f3c4177692e3f667cb1d00bcef',
+       i686: '1f4a0a3f23fef826301085f9bc615059b56041a2aa9267876978d38fb6f084a1',
+     x86_64: 'c853af1d920d63f20e2a87a2e3f6e4da4762f5b936017a2e9dbc9bf47dd1236c'
   })
 
-  depends_on 'libaom'
-  depends_on 'libgit2'
-  depends_on 'rust' => :build
   depends_on 'cargo_c' => :build
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'libaom' => :library
+  depends_on 'libgit2' => :library
+  depends_on 'nasm' => :build
+  depends_on 'rust' => :build
 
   def self.build
     @rust_flags = ''
