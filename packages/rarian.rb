98a6807083109a4005e9649f59dbeef43278c676
Ed Reel <edreel@gmail.com>
2026-07-28 18:51:20 -0500

    Rarian 0.8.1 => 0.8.6 (#17780)

--- packages/rarian.rb
+++ packages/rarian.rb
@@ -3,17 +3,22 @@ require 'buildsystems/autotools'
 class Rarian < Autotools
   description 'Documentation metadata library based on the proposed Freedesktop.org spec.'
   homepage 'https://rarian.freedesktop.org/'
-  version '0.8.1'
+  version '0.8.6'
   license 'LGPL-2.1'
   compatibility 'all'
-  source_url 'https://ftp.gnome.org/pub/gnome/sources/rarian/0.8/rarian-0.8.1.tar.bz2'
-  source_sha256 'aafe886d46e467eb3414e91fa9e42955bd4b618c3e19c42c773026b205a84577'
-  binary_compression 'tar.xz'
+  source_url "https://gitlab.freedesktop.org/rarian/rarian/-/archive/#{version}/rarian-#{version}.tar.bz2"
+  source_sha256 '2dcb9a421e084511d654aa8803807ddd56560747ac37ab2fe7d84270b94084ed'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '0432d2e342be1d21b788bd906d12312faae65f9badef502c9cbcfe74c56aabf3',
-     armv7l: '0432d2e342be1d21b788bd906d12312faae65f9badef502c9cbcfe74c56aabf3',
-       i686: 'f8feef1bf91eb6fc6c503f8b088dcb23da53de683809d941e793fbeaaf87bb00',
-     x86_64: '3ececd8ac3dce371c40d1b32c10b538821a2b74edff336d9c041f8fcab4ac09c'
+    aarch64: '4e61c8edc87cdfbdbead29dfc360bcdbf46b804c4bc630068ae7b1ebf0c24ed4',
+     armv7l: '4e61c8edc87cdfbdbead29dfc360bcdbf46b804c4bc630068ae7b1ebf0c24ed4',
+       i686: '779fb792378b1c694c3f358cca9489d403b110837e65811feb2381ce5d267f0d',
+     x86_64: '9e1533e82cffb9fc5eb937de733125b6e3c17aa1d99642edf972b7b555636622'
   })
+
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'tinyxml2' => :library
 end
