3a859c0b7580df2bd2b5add16a27fa7e195e0e12
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-07-29 00:47:38 +0000

    libmicrohttpd -> 1.0.8 in updater-libmicrohttpd-1.0.8

--- packages/libmicrohttpd.rb
+++ packages/libmicrohttpd.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Libmicrohttpd < Autotools
   description 'GNU libmicrohttpd is a small C library that is supposed to make it easy to run an HTTP server as part of another application.'
   homepage 'https://www.gnu.org/software/libmicrohttpd/'
-  version '1.0.7'
+  version '1.0.8'
   license 'LGPL-2.1'
   compatibility 'all'
   source_url "https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-#{version}.tar.gz"
-  source_sha256 '827250db649546cdb04bea01f5f0560f0edffde9130e256387d1f977242eaf98'
+  source_sha256 '0763970a0e39f8f382123366e3cf5d03f70aa1e2208d3101e84da3e2cd674703'
   binary_compression 'tar.zst'
 
   binary_sha256({
