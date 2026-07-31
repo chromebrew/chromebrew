394972b494daff8ca52a7708a23b07cd8a1fa270
Ed Reel <edreel@gmail.com>
2026-07-31 02:21:13 -0500

    Ripgrep 12.1.0 => 15.2.0 (#17841)
    * Ripgrep 12.1.0 => 15.2.0
    
    * Convert ripgrep to rust buildsystem

--- packages/ripgrep.rb
+++ packages/ripgrep.rb
@@ -1,46 +1,28 @@
-require 'package'
+require 'buildsystems/rust'
 
-class Ripgrep < Package
+class Ripgrep < RUST
   description 'ripgrep recursively searches directories for a regex pattern'
   homepage 'https://github.com/BurntSushi/ripgrep'
-  version '12.1.0'
+  version '15.2.0'
   license 'Apache-2.0, BSD-2, Boost-1.0 and MIT or Unlicense'
   compatibility 'all'
-  source_url 'https://github.com/BurntSushi/ripgrep/archive/12.1.0.tar.gz'
-  source_sha256 'ca2d11dd7b7346734d47ad8073468e9c409fbe85842a608d372b8d4fb36be291'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/BurntSushi/ripgrep.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3a258a81559b077d04cf3d9912731da26d2f8398538e9a9faa3fb3e0a15aa6cd',
-     armv7l: '3a258a81559b077d04cf3d9912731da26d2f8398538e9a9faa3fb3e0a15aa6cd',
-     x86_64: '1e1b614726be5ad7de8e35921d8ea73c0fd72f4cd3d245b5dae78e356055e17b'
+    aarch64: '5b7a8062fd9cb4bedd1b52394b4f7423d4f0a82915d77cba7029c805ae8452ab',
+     armv7l: '5b7a8062fd9cb4bedd1b52394b4f7423d4f0a82915d77cba7029c805ae8452ab',
+       i686: 'b1ff66a6f8b683e8e63640efb6b6a48af0791677c2fa6dea61e8ac2a45950bb8',
+     x86_64: '5039e808a0542c47dca18dad9000e095616e2943031f4ad495c4a897d7d3b4b7'
   })
 
+  depends_on 'gcc_lib' => :executable
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
   depends_on 'rust' => :build
 
-  def self.build
-    case ARCH
-    when 'aarch64', 'armv7l'
-      system 'rustup toolchain install 1.43.1-armv7-unknown-linux-gnueabihf'
-      system 'rustup default 1.43.1-armv7-unknown-linux-gnueabihf'
-    else
-      system 'rustup toolchain install stable'
-      system 'rustup default stable'
-    end
-    system 'cargo build --release'
-  end
-
-  def self.check
-    system 'cargo test --all'
-  end
-
-  def self.install
-    system "install -Dm755 target/release/rg #{CREW_DEST_PREFIX}/bin/rg"
-  end
-
   def self.postinstall
-    puts
-    puts "Type 'rg' to get started.".lightblue
-    puts
+    ExitMessage.add "\nType 'rg' to get started.\n"
   end
 end
