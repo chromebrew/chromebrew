4d8a29bbe31467a9aadc3723fe8a099994b55877
Ed Reel <edreel@gmail.com>
2026-07-31 07:08:17 +0000

    Convert ripgrep to rust buildsystem

--- packages/ripgrep.rb
+++ packages/ripgrep.rb
@@ -1,6 +1,6 @@
-require 'package'
+require 'buildsystems/rust'
 
-class Ripgrep < Package
+class Ripgrep < RUST
   description 'ripgrep recursively searches directories for a regex pattern'
   homepage 'https://github.com/BurntSushi/ripgrep'
   version '15.2.0'
@@ -11,36 +11,16 @@ class Ripgrep < Package
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'cdff508de644154a18a1636ab681c6d9255d7551d19f2f35a92c42db04006ea0',
-     armv7l: 'cdff508de644154a18a1636ab681c6d9255d7551d19f2f35a92c42db04006ea0',
-       i686: 'bad92325e65b9950933b7e3ddfce7e3a6bbc2b91a10b2333251dd3b213d08c39',
-     x86_64: 'd22b1ddd15dafa60399cabba67f30d29d526488411aa741dcdae48e2b63693cf'
+    aarch64: '5b7a8062fd9cb4bedd1b52394b4f7423d4f0a82915d77cba7029c805ae8452ab',
+     armv7l: '5b7a8062fd9cb4bedd1b52394b4f7423d4f0a82915d77cba7029c805ae8452ab',
+       i686: 'b1ff66a6f8b683e8e63640efb6b6a48af0791677c2fa6dea61e8ac2a45950bb8',
+     x86_64: '5039e808a0542c47dca18dad9000e095616e2943031f4ad495c4a897d7d3b4b7'
   })
 
   depends_on 'gcc_lib' => :executable
   depends_on 'glibc' => :executable
   depends_on 'glibc_lib' => :executable
   depends_on 'rust' => :build
-
-  def self.build
-    case ARCH
-    when 'aarch64', 'armv7l'
-      system 'rustup toolchain install 1.43.1-armv7-unknown-linux-gnueabihf'
-      system 'rustup default 1.43.1-armv7-unknown-linux-gnueabihf'
-    else
-      system 'rustup toolchain install stable'
-      system 'rustup default stable'
-    end
-    system 'cargo build --release'
-  end
-
-  def self.check
-    system 'cargo test --all'
-  end
-
-  def self.install
-    FileUtils.install 'target/release/rg', "#{CREW_DEST_PREFIX}/bin/rg", mode: 0o755
-  end
 
   def self.postinstall
     ExitMessage.add "\nType 'rg' to get started.\n"
