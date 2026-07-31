5691019aaa5962df6c1963fd5ed9f616b590d134
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-07-31 00:52:54 +0000

    pnpm -> 11.18.0 in updater-pnpm-11.18.0

--- packages/pnpm.rb
+++ packages/pnpm.rb
@@ -3,11 +3,11 @@ require 'package'
 class Pnpm < Package
   description 'Fast, disk space efficient package manager'
   homepage 'https://pnpm.io/'
-  version '11.17.0'
+  version '11.18.0'
   license 'MIT'
   compatibility 'x86_64'
   source_url "https://github.com/pnpm/pnpm/releases/download/v#{version}/pnpm-linux-x64.tar.gz"
-  source_sha256 'bdb1db01bf0f757495405a59a09c5c287f315889dc98d3b14bc374b9fe43a0bf'
+  source_sha256 'f4e62794c56f7a1ad01097f94b90ca95680080aa2e83ebc3d0a751fb8aa8e4ad'
 
   no_compile_needed
 
