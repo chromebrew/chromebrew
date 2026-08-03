5448c8c21fd8281ae70d2c3bafa64eca400751a2
Ed Reel <edreel@gmail.com>
2026-08-03 00:02:57 +0000

    Scrot 1.8.1 => 2.0.0

--- packages/scrot.rb
+++ packages/scrot.rb
@@ -1,33 +1,30 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Scrot < Package
+class Scrot < Autotools
   description 'scrot, an acronym for (SCReen shOT) is a simple, freely distributed and open source command-line screen capture utility'
   homepage 'https://github.com/resurrecting-open-source-projects/scrot'
-  version '1.8.1'
+  version '2.0.0'
   license 'feh and LGPL-2+'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://github.com/resurrecting-open-source-projects/scrot/releases/download/1.8.1/scrot-1.8.1.tar.bz2'
-  source_sha256 '05f91be9a32eb912f8f2b9abdb3dca83166f77ed0a5a430b6766067ab13c3b18'
+  source_url 'https://github.com/resurrecting-open-source-projects/scrot.git'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '369e6e791ae3be26802eb410e76e87ef94aacf9a22d8770f8553d08393359c19',
-     armv7l: '369e6e791ae3be26802eb410e76e87ef94aacf9a22d8770f8553d08393359c19',
-     x86_64: '985f1c550bb81074318088309b31e5e89fca94a98c63ea4a8e946fe49172724d'
+    aarch64: '4e7db7044752a4f8a7d9450377ab1f71984e285d2af91815d46341643a184c4d',
+     armv7l: '4e7db7044752a4f8a7d9450377ab1f71984e285d2af91815d46341643a184c4d',
+     x86_64: 'b72f4e64f666f330a8938c35215139081754b0443b053ce8b25100cfa4f78845'
   })
 
   depends_on 'autoconf_archive' => :build
-  depends_on 'giblib'
-  depends_on 'libxcursor'
-  depends_on 'libxfixes'
-  depends_on 'optipng'
-
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'giblib' => :executable
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'imlib2' => :executable
+  depends_on 'libx11' => :executable
+  depends_on 'libxcomposite' => :executable
+  depends_on 'libxcursor' => :executable
+  depends_on 'libxfixes' => :executable
+  depends_on 'libxrandr' => :executable
+  depends_on 'optipng' => :executable
 end
