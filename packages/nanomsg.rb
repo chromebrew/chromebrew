bea35ec0efc3e930e94207b3ed618668543299b3
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-03 00:51:43 +0000

    nanomsg -> 1.2.3 in updater-nanomsg-1.2.3

--- packages/nanomsg.rb
+++ packages/nanomsg.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Nanomsg < CMake
   description 'nanomsg is a socket library that provides several common communication patterns.'
   homepage 'https://nanomsg.org/'
-  version '1.2.2'
+  version '1.2.3'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/nanomsg/nanomsg.git'
