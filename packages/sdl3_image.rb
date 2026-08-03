b932c43c91bb3b1ae1f53abb6ce6704ffac6e63e
Ed Reel <edreel@gmail.com>
2026-08-03 16:58:48 -0500

    Sdl3_image 3.2.4 => 3.4.4 (#17913)

--- packages/sdl3_image.rb
+++ packages/sdl3_image.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Sdl3_image < CMake
   description 'SDL3_image is an image loading library that is used with the SDL2 library.'
   homepage 'https://github.com/libsdl-org/SDL_image'
-  version '3.2.4'
+  version '3.4.4'
   license 'zlib'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/libsdl-org/SDL_image.git'
@@ -11,18 +11,19 @@ class Sdl3_image < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '32763b2ef38dbe572dc727f840167283c3e264e649f3774ae7202c47fdf42834',
-     armv7l: '32763b2ef38dbe572dc727f840167283c3e264e649f3774ae7202c47fdf42834',
-     x86_64: '45c73a636295549a8f46de09438355ae2be606c15080ca830c26e30aa5dc281c'
+    aarch64: '05a4129c6cceee96765486c45f97ac25dd406331d2a212cabecea4430b8fecf7',
+     armv7l: '05a4129c6cceee96765486c45f97ac25dd406331d2a212cabecea4430b8fecf7',
+     x86_64: '4dd5bc6aaa28ae8e7aa41b71488d4328879a98d25732f9ef7588a7ed7ea783bb'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
   depends_on 'libavif' => :build
   depends_on 'libjpeg_turbo' => :build
   depends_on 'libjxl' => :build
   depends_on 'libpng' => :build
   depends_on 'libtiff' => :build
   depends_on 'libwebp' => :build
-  depends_on 'sdl3' # R
+  depends_on 'sdl3' => :library
 end
