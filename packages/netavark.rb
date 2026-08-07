60aefbda0135c6a5cf8ec0e5a164618ea2fad5f6
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-07 01:30:48 +0000

    netavark -> 2.1.0 in updater-netavark-2.1.0

--- packages/netavark.rb
+++ packages/netavark.rb
@@ -6,7 +6,7 @@ require 'package'
 class Netavark < Package
   description 'Container network stack'
   homepage 'https://github.com/containers/netavark'
-  version '2.0.0'
+  version '2.1.0'
   license 'Apache'
   compatibility 'all'
   source_url 'https://github.com/containers/netavark.git'
