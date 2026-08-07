f10ea622b89bbace4c4ab50386697606378853e2
Ed Reel <edreel@gmail.com>
2026-08-07 08:16:46 +0000

    Sleuthkit 4.6.0 => 4.15.0

--- packages/sleuthkit.rb
+++ packages/sleuthkit.rb
@@ -1,31 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Sleuthkit < Package
+class Sleuthkit < Autotools
   description 'The Sleuth Kit® (TSK) is a library and collection of command line tools that allow you to investigate disk images.'
   homepage 'https://www.sleuthkit.org/sleuthkit/'
-  version '4.6.0'
+  version '4.15.0'
   license 'BSD, CPL-1.0, GPL-2+, IBM and Apache-2.0'
   compatibility 'all'
-  source_url 'https://github.com/sleuthkit/sleuthkit/releases/download/sleuthkit-4.6.0/sleuthkit-4.6.0.tar.gz'
-  source_sha256 'f52a08ab0de078182c0f2d19d3e1b341424a9e0c1633a61c3b892fb38f9acb97'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/sleuthkit/sleuthkit.git'
+  git_hashtag "sleuthkit-#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '5ec2af7f82c975c22a8801bfa28a25ce6544fc079c38987fa9e6276f6198ec3a',
-     armv7l: '5ec2af7f82c975c22a8801bfa28a25ce6544fc079c38987fa9e6276f6198ec3a',
-       i686: '77311cc6236c21017fe7378309d262ee743edb5065ee00dc4b2e363751097d8d',
-     x86_64: 'ec404bf9be6b70fd1638e45a997f26b74ad5522fa878a20a311d962d51562685'
+    aarch64: 'f6ec4ed0a74c16af52580474c07b23a1298bb7b231899a96b02951a95fa343da',
+     armv7l: 'f6ec4ed0a74c16af52580474c07b23a1298bb7b231899a96b02951a95fa343da',
+       i686: '5805cd0aa0a2ad9c736b3477981d675320b9de68d628a99432700bacb8063b32',
+     x86_64: '3bacdd0de80cb020511932587b46a2def582f273739825edc815bddb048d9f39'
   })
 
-  def self.build
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-maintainer-mode'
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'sqlite' => :library
+  depends_on 'zlib' => :library
 end
