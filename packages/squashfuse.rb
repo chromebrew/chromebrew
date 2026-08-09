0b913b33c2fd590434042b222db1a04b79063605
Ed Reel <edreel@gmail.com>
2026-08-09 15:53:00 -0500

    Squashfuse 0.1.104 => 0.6.2 (#18055)

--- packages/squashfuse.rb
+++ packages/squashfuse.rb
@@ -1,37 +1,31 @@
 # Adapted from Arch Linux squashfuse PKGBUILD at:
 # https://github.com/archlinux/svntogit-packages/raw/packages/squashfuse/trunk/PKGBUILD
 
-require 'package'
+require 'buildsystems/autotools'
 
-class Squashfuse < Package
+class Squashfuse < Autotools
   description 'FUSE filesystem to mount squashfs archives'
   homepage 'https://github.com/vasi/squashfuse'
-  version '0.1.104'
+  version '0.6.2'
   license 'custom'
   compatibility 'all'
-  source_url 'https://github.com/vasi/squashfuse/releases/download/0.1.104/squashfuse-0.1.104.tar.gz'
-  source_sha256 'aa52460559e0d0b1753f6b1af5c68cfb777ca5a13913285e93f4f9b7aa894b3a'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/vasi/squashfuse.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '83bad067dece48c952a5fd510fdc2f62d4a790d6134f0015eb1c958bb66a4507',
-     armv7l: '83bad067dece48c952a5fd510fdc2f62d4a790d6134f0015eb1c958bb66a4507',
-       i686: 'afb0d28b2ee43ebc540e6b1d3c4cc56324a3356bd33cea66e47d2ba3797bf5ca',
-     x86_64: 'df130e28a509798c98213a139936e6711b7d6d97b2510f3194820cb47e22631d'
+    aarch64: '9af06b0894554939781f2f02da73eff7e96ebdfe69c2591b8515a60b8762547b',
+     armv7l: '9af06b0894554939781f2f02da73eff7e96ebdfe69c2591b8515a60b8762547b',
+       i686: '66dca481d52e25ebd6ae5be10aa188649ff426b6aa51b961e4ec335dea13ca08',
+     x86_64: 'b624917fef01972cc4d2ac72f15d673535eca1c04ea60994e941e3f822f479f6'
   })
 
-  depends_on 'fuse3'
-  depends_on 'lzo'
-
-  def self.build
-    system "env CFLAGS='-pipe -flto=auto' \
-      CXXFLAGS='-pipe -flto=auto' \
-      LDFLAGS='-flto=auto' \
-      ./configure #{CREW_CONFIGURE_OPTIONS}"
-    system 'make'
-  end
-
-  def self.install
-    system "make DESTDIR=#{CREW_DEST_DIR} install"
-  end
+  depends_on 'fuse3' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'lz4' => :library
+  depends_on 'lzo' => :library
+  depends_on 'xzutils' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
 end
