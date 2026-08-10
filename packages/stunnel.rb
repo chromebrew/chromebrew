0b2092e37af1fd3d68ce3a087f0223dc7a59aeeb
Ed Reel <edreel@gmail.com>
2026-08-10 06:44:28 +0000

    Stunnel 5.78 => 5.80

--- packages/stunnel.rb
+++ packages/stunnel.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Stunnel < Autotools
   description "Stunnel is a proxy designed to add TLS encryption functionality to existing clients and servers without any changes in the programs' code."
   homepage 'https://www.stunnel.org/index.html'
-  version '5.78'
+  version '5.80'
   license 'GPL-2+'
   compatibility 'all'
   source_url 'https://github.com/mtrojnar/stunnel.git'
@@ -11,16 +11,19 @@ class Stunnel < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9a80a9dd3a7f31a740ed926adaf41867f01175218417e5c5e37d989dfcfde0dd',
-     armv7l: '9a80a9dd3a7f31a740ed926adaf41867f01175218417e5c5e37d989dfcfde0dd',
-       i686: '45e5a3b88201859af59e8fb9f2334e890ad0d76e3e3944f3a73f7c7776144aa1',
-     x86_64: '2a1faf8c8a4e10a4fa483b9116a8462747d6983cc2ae097aca4d2b63400a4d57'
+    aarch64: '877e2fe163c0215bc262eb8cd80f6f27db5952c7388cb8ed9abee762ce568dcf',
+     armv7l: '877e2fe163c0215bc262eb8cd80f6f27db5952c7388cb8ed9abee762ce568dcf',
+       i686: '7a00118302234de0a24f1706b9d342c76031ca3f8304719985b4210fafe03568',
+     x86_64: '4ada48f543baef427c34560031391b83ecad9d0048d28593c68cf2e8d4502072'
   })
 
   depends_on 'glibc' => :library
   depends_on 'glibc_lib' => :library
   depends_on 'openssl' => :executable
+  depends_on 'pandoc' => :build
   depends_on 'tcpwrappers' => :library
+
+  autotools_skip_autoreconf
 
   def self.patch
     # The aclocal & automake versions are hardcoded.
