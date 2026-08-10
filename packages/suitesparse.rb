375eb9dfbbb67b116c2f06f7ee76556e90b15639
Ed Reel <edreel@gmail.com>
2026-08-10 23:40:22 +0000

    Suitesparse 7.12.1 => 7.12.3

--- packages/suitesparse.rb
+++ packages/suitesparse.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Suitesparse < CMake
   description 'A suite of sparse matrix software'
   homepage 'https://people.engr.tamu.edu/davis/suitesparse.html'
-  version '7.12.1'
+  version '7.12.3'
   license 'metapackage'
   compatibility 'all'
   source_url 'https://github.com/DrTimothyAldenDavis/SuiteSparse.git'
@@ -11,16 +11,17 @@ class Suitesparse < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '74d600ca187a132cf02c1e289ead0cbe3640eecaad64ad9bee1235f11b4006d8',
-     armv7l: '74d600ca187a132cf02c1e289ead0cbe3640eecaad64ad9bee1235f11b4006d8',
-       i686: '7bf702d27beb0d0bccc0827740150ead66e4a2ecfc06ef9e20a544bb3597610b',
-     x86_64: 'fdfdf549665de7b7fc194a939730154d1325052e7ebbcb0623b7c6a036db3d92'
+    aarch64: '0ead52eaaa446ba93455588133ddda2eac111cc6fb72a105dc8b972ea873de8b',
+     armv7l: '0ead52eaaa446ba93455588133ddda2eac111cc6fb72a105dc8b972ea873de8b',
+       i686: 'fe9b59efaf6a6413963304800dace25ab731bb265d7b070ed79e846c91485080',
+     x86_64: 'a00c65e3763537b970a324daaa38da9c4d3c221c84879c3fc889a83e99e6a1ea'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'gmp' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gmp' => :library
   depends_on 'lapack' => :build
-  depends_on 'mpfr' # R
-  depends_on 'openblas' # R
+  depends_on 'mpfr' => :library
+  depends_on 'openblas' => :library
 end
