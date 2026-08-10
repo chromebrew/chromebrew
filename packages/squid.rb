52f34dcd1ecac44c495c4daf4e8b98e3bfc8cea7
Ed Reel <edreel@gmail.com>
2026-08-09 23:22:46 -0500

    Squid 6.8 => 7.6 (#18061)

--- packages/squid.rb
+++ packages/squid.rb
@@ -3,34 +3,46 @@ require 'buildsystems/autotools'
 class Squid < Autotools
   description 'Squid Web Proxy Cache'
   homepage 'https://www.squid-cache.org/'
-  version '6.8'
+  version '7.6'
   license 'GPL-2.0'
   compatibility 'all'
   source_url 'https://github.com/squid-cache/squid.git'
-  git_hashtag 'SQUID_6_8'
+  git_hashtag "SQUID_#{version.gsub('.', '_')}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3f24eec890e52e5f3cb3bad3e0496d8947158427ba770a97144acf4965277699',
-     armv7l: '3f24eec890e52e5f3cb3bad3e0496d8947158427ba770a97144acf4965277699',
-       i686: '09b29eb0a306d5e1b99b320b2994228fa06b83e4889744d579d07ca63027c85e',
-     x86_64: '99550be07e2e0b426fa29a2ee3cf84127241022509c54b449151ae9755ca0e8a'
+    aarch64: '11d80c1fadffaebd5566e0c06016e4d8339eda1a8ec892faa9e98f5c2d2f58e0',
+     armv7l: '11d80c1fadffaebd5566e0c06016e4d8339eda1a8ec892faa9e98f5c2d2f58e0',
+       i686: 'c5d5b0c40bfb7aa5c2fae55763591fb8049aa598853fd388603e325307bb17dc',
+     x86_64: '7746a33a08bb292dc9971460ad5da1ef3568c527558bd806d17d27d6d1c268e5'
   })
 
-  depends_on 'e2fsprogs' # R
-  depends_on 'expat' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'gnutls' # R
-  depends_on 'krb5' # R
-  depends_on 'libcap' # R
-  depends_on 'libcyrussasl' # R
-  depends_on 'libdb' # R
-  depends_on 'libtool' # R
-  depends_on 'libxml2' # R
-  depends_on 'linux_pam' # R
-  depends_on 'nettle' # R
-  depends_on 'openldap' # R
-  depends_on 'glibc' # R
+  depends_on 'brotli' => :executable
+  depends_on 'e2fsprogs' => :library
+  depends_on 'expat' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gmp' => :executable
+  depends_on 'gnutls' => :executable
+  depends_on 'krb5' => :library
+  depends_on 'libcap' => :executable
+  depends_on 'libcyrussasl' => :library
+  depends_on 'libdb' => :library
+  depends_on 'libidn2' => :executable
+  depends_on 'libtasn1' => :executable
+  depends_on 'libtirpc' => :library
+  depends_on 'libtool' => :executable
+  depends_on 'libunistring' => :executable
+  depends_on 'libxcrypt' => :library
+  depends_on 'libxml2' => :library
+  depends_on 'linux_pam' => :library
+  depends_on 'nettle' => :library
+  depends_on 'openldap' => :library
+  depends_on 'openssl' => :library
+  depends_on 'p11kit' => :executable
+  depends_on 'zlib' => :executable
+  depends_on 'zstd' => :executable
 
   # https://github.com/squid-cache/squid/pull/1118
   autotools_pre_configure_options "CXXFLAGS='-Wno-alloc-size-larger-than'"
