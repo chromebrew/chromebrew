decbf0d2d81ea2e879cfb726dd973d3af505c2d1
Ed Reel <edreel@gmail.com>
2026-08-12 06:30:59 -0500

    Sysfsutils 2.1.0 => 2.1.1 (#18124)

--- packages/sysfsutils.rb
+++ packages/sysfsutils.rb
@@ -1,28 +1,22 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Sysfsutils < Package
+class Sysfsutils < Autotools
   description 'These are a set of utilities built upon sysfs, a new virtual filesystem in Linux kernel versions 2.5+ that exposes a system\'s device tree. The current version of sysfsutils includes libsysfs and systool.'
   homepage 'https://linux-diag.sourceforge.net/Sysfsutils.html'
-  version '2.1.0'
+  version '2.1.1'
   license 'GPL-2 and LGPL-2.1'
   compatibility 'all'
-  source_url 'https://sourceforge.net/projects/linux-diag/files/sysfsutils/2.1.0/sysfsutils-2.1.0.tar.gz'
-  source_sha256 'e865de2c1f559fff0d3fc936e660c0efaf7afe662064f2fb97ccad1ec28d208a'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/linux-ras/sysfsutils.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f5cce4169a5647744a583823b0ea095f531574a0fcb11a584207dd871bfef43e',
-     armv7l: 'f5cce4169a5647744a583823b0ea095f531574a0fcb11a584207dd871bfef43e',
-       i686: '6f7d32bd3a199f8febb2101b368c36da43c5c6363570e020dd66774386ab0288',
-     x86_64: '81876c5c17b99ad08b662f0b560021a419c3a441be485963fab688e7fbde6a0b'
+    aarch64: '08ea5ac3b4c6cfa2cefa71fc16fe7011f7bcfc223fdf0013f5fec8a558164739',
+     armv7l: '08ea5ac3b4c6cfa2cefa71fc16fe7011f7bcfc223fdf0013f5fec8a558164739',
+       i686: 'a9ab189f5843e8cca18017ec7e0fe48acdc68bf77ecc18bff7ed08ab88bf3b85',
+     x86_64: '9ca3180951bfc976230861af19532ea87e7b62dcbcbc4c5061f5c07439b5cced'
   })
 
-  def self.build
-    system './configure', "--prefix=#{CREW_PREFIX}", "--libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
-
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 end
