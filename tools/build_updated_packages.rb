7be88e346fe73dd80fce6d1a4c9eb7f062f89228
Satadru Pramanik <satadru@gmail.com>
2026-08-13 17:37:08 -0400

    Simplify logic.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- tools/build_updated_packages.rb
+++ tools/build_updated_packages.rb
@@ -324,11 +324,7 @@ updated_packages.delete_if { !File.file?(File.join(crew_local_repo_root, it.down
 unless updated_packages.empty?
   updated_packages.delete_if do
     p = Package.load_package(File.join(crew_local_repo_root, it.downcase))
-    !PackageUtils.compatible?(p)
-  end
-  updated_packages.delete_if do
-    p = Package.load_package(File.join(crew_local_repo_root, it.downcase))
-    p.ignore_updater?
+    !PackageUtils.compatible?(p) || p.ignore_updater?
   end
 end
 
