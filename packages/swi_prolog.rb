f3c6d8c784d44e0eadc568891b842dab69a51987
Ed Reel <edreel@gmail.com>
2026-08-13 00:01:59 +0000

    Swi_prolog 9.3.5 => 10.1.5 (#18127)
    * Swi_prolog 9.3.5 => 10.1.5
    
    * Swi_prolog 9.3.5 => 10.1.5

--- packages/swi_prolog.rb
+++ packages/swi_prolog.rb
@@ -3,32 +3,62 @@ require 'buildsystems/cmake'
 class Swi_prolog < CMake
   description 'SWI-Prolog offers a comprehensive free Prolog environment.'
   homepage 'https://www.swi-prolog.org'
-  version '9.3.5'
+  version '10.1.5'
   license 'BSD-2'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/SWI-Prolog/swipl-devel.git'
   git_hashtag "V#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'c6045d6ad6e8e4f21e30ec4e937dfbcdff93dee67de1c297ca251e58bba52c21',
-     armv7l: 'c6045d6ad6e8e4f21e30ec4e937dfbcdff93dee67de1c297ca251e58bba52c21',
-       i686: 'c87d2ad03d051055420b8c5b65f0d6b26b93e933b1095a77e16928332f6f1d76',
-     x86_64: '86cf759a7fbb1ffcc2c625b97efbf7e1c8ce0c2946b04ed324412b5bea85290f'
+    aarch64: 'e3ee8a715b5dc861fa82e1735d9b35b1101c883725bb6966aad0866a97ccd796',
+     armv7l: 'e3ee8a715b5dc861fa82e1735d9b35b1101c883725bb6966aad0866a97ccd796',
+     x86_64: '6c3b1b3c153360fc3355f8a053bfdf27cc2dec13e3a54a27762eecd362e33c5e'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'gmp' # R
-  depends_on 'libarchive'
-  depends_on 'libdb'
-  depends_on 'libedit' # R
-  depends_on 'libyaml'
-  depends_on 'openssl' # R
-  depends_on 'pcre2' # R
-  depends_on 'python3' # R
-  depends_on 'readline' # R
-  depends_on 'zlib' # R
+  depends_on 'cairo' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gmp' => :library
+  depends_on 'harfbuzz' => :library
+  depends_on 'libarchive' => :library
+  depends_on 'libdb' => :library
+  depends_on 'libedit' => :library
+  depends_on 'libxcrypt' => :library
+  depends_on 'libyaml' => :library
+  depends_on 'ncurses' => :library
+  depends_on 'openjdk17' => :executable
+  depends_on 'openssl' => :library
+  depends_on 'pango' => :library
+  depends_on 'pcre2' => :library
+  depends_on 'python3' => :library
+  depends_on 'readline' => :library
+  depends_on 'sdl3' => :library
+  depends_on 'sdl3_image' => :library
+  depends_on 'termcap' => :library
+  depends_on 'zlib' => :library
 
   pre_cmake_options "CFLAGS+=' -I#{CREW_PREFIX}/include/ncurses'"
   cmake_options '-DCMAKE_BUILD_TYPE=PGO'
+
+  def self.patch
+    # Fix error: implicit declaration of function ‘va_start’.
+    system "sed -i '35i#include <stdarg.h>' packages/libedit/libedit/src/terminal.c"
+  end
+
+  cmake_install_extras do
+    if ARCH.eql?('x86_64')
+      FileUtils.mv "#{CREW_DEST_PREFIX}/lib", CREW_DEST_LIB_PREFIX
+      Dir["#{CREW_DEST_LIB_PREFIX}/swipl/bin/x86_64-linux/swipl*"].each do |binary|
+        bin = File.basename(binary)
+        FileUtils.ln_sf "#{CREW_LIB_PREFIX}/swipl/bin/x86_64-linux/#{bin}", "#{CREW_DEST_PREFIX}/bin/#{bin}"
+      end
+      Dir["#{CREW_DEST_LIB_PREFIX}/swipl/lib/x86_64-linux/*"].each do |library|
+        lib = File.basename(library)
+        FileUtils.ln_sf "#{CREW_LIB_PREFIX}/swipl/lib/x86_64-linux/#{lib}", "#{CREW_DEST_LIB_PREFIX}/#{lib}"
+      end
+    end
+  end
 end
