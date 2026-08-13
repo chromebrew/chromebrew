0c9cd7daef463d3c69978b87be86c6879f42346d
Ed Reel <edreel@gmail.com>
2026-08-13 02:35:20 +0000

    Tbb 2022.3.0 => 2023.1.0 (#18136)

--- packages/tbb.rb
+++ packages/tbb.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Tbb < CMake
   description 'High level abstract threading library'
   homepage 'https://github.com/oneapi-src/oneTBB'
-  version '2022.3.0'
+  version '2023.1.0'
   license 'Apache-2.0'
   compatibility 'all'
   source_url 'https://github.com/oneapi-src/oneTBB.git'
@@ -11,15 +11,16 @@ class Tbb < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9a2453136b00c543298f959a58393e981f12f0bf26857a1244b8e74a0325b438',
-     armv7l: '9a2453136b00c543298f959a58393e981f12f0bf26857a1244b8e74a0325b438',
-       i686: '7334111f25246a231f2529bdfef298fd405eac6f90847e626ae11116816ade3e',
-     x86_64: 'afcd9c317b60065c885d90c2ec2558a5314dad85ca1574e3934d7aba3bff6cd0'
+    aarch64: '0e59232a6e4f62a126a8c1e57a690203253bde2eda35cccf9698045d6aefba0d',
+     armv7l: '0e59232a6e4f62a126a8c1e57a690203253bde2eda35cccf9698045d6aefba0d',
+       i686: '701b6034b542a7698c026f5a97b64fab0faca851191bf483460b2c3059bd6e02',
+     x86_64: '561f9d71af8d30c680c93fb162436d650d1a4659d2458e923d6edabc54cb3dd5'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'hwloc'
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'hwloc' => :library
   depends_on 'swig' => :build
 
   cmake_options '-DTBB4PY_BUILD=ON -DTBB_TEST=OFF'
