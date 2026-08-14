a5554e6ae5a5f5a614c2e7722c4ff5d06e967ea6
Ed Reel <edreel@gmail.com>
2026-08-14 18:38:42 +0000

    Openjdk21 21.0.10 => 21.0.12 (#18199)

--- packages/openjdk21.rb
+++ packages/openjdk21.rb
@@ -3,12 +3,12 @@ require 'package'
 class Openjdk21 < Package
   description 'The JDK is a development environment for building applications, applets, and components using the Java programming language.'
   homepage 'https://openjdk.org/'
-  version '21.0.10'
+  version '21.0.12'
   license 'GPL-2'
   compatibility 'x86_64'
   # Visit https://www.azul.com/downloads/?version=java-21-lts&package=jdk#zulu to download the binary.
-  source_url 'https://cdn.azul.com/zulu/bin/zulu21.48.15-ca-jdk21.0.10-linux_x64.tar.gz'
-  source_sha256 '7f15f667580a8977962dc0a709cf2a097cc71244614fad3f236debce9d1c2670'
+  source_url 'https://cdn.azul.com/zulu/bin/zulu21.52.15-ca-jdk21.0.12-linux_x64.tar.gz'
+  source_sha256 'b1a9df12e798770d1b2db43b402a80f1e6080cff6d5d1d1fbe5c768fb4225f6a'
 
   no_compile_needed
   no_shrink
