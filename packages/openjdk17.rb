6aac1a843ae39a5b2f593bd747919c27d8ab3d3f
Ed Reel <edreel@gmail.com>
2026-08-14 18:08:40 +0000

    Openjdk17 17.0.18 => 17.0.20

--- packages/openjdk17.rb
+++ packages/openjdk17.rb
@@ -3,21 +3,22 @@ require 'package'
 class Openjdk17 < Package
   description 'The JDK is a development environment for building applications, applets, and components using the Java programming language.'
   homepage 'https://openjdk.org/'
-  version '17.0.18'
+  version %w[i686 x86_64].include?(ARCH) ? '17.0.20' : '17.0.19'
   license 'GPL-2'
   compatibility 'all'
   # Visit https://www.azul.com/downloads/?version=java-17-lts&package=jdk#zulu to download the binaries.
+  # The project stopped supporting arm 32-bit with version 17.0.19.
   source_url({
-    aarch64: 'https://cdn.azul.com/zulu/bin/zulu17.64.17-ca-jdk17.0.18-c2-linux_aarch32hf.tar.gz',
-     armv7l: 'https://cdn.azul.com/zulu/bin/zulu17.64.17-ca-jdk17.0.18-c2-linux_aarch32hf.tar.gz',
-       i686: 'https://cdn.azul.com/zulu/bin/zulu17.64.15-ca-jdk17.0.18-linux_i686.tar.gz',
-     x86_64: 'https://cdn.azul.com/zulu/bin/zulu17.64.15-ca-jdk17.0.18-linux_x64.tar.gz'
+    aarch64: 'https://cdn.azul.com/zulu/bin/zulu17.66.19-ca-jdk17.0.19-c2-linux_aarch32hf.tar.gz',
+     armv7l: 'https://cdn.azul.com/zulu/bin/zulu17.66.19-ca-jdk17.0.19-c2-linux_aarch32hf.tar.gz',
+       i686: 'https://cdn.azul.com/zulu/bin/zulu17.68.17-ca-jdk17.0.20-linux_i686.tar.gz',
+     x86_64: 'https://cdn.azul.com/zulu/bin/zulu17.68.17-ca-jdk17.0.20-linux_x64.tar.gz'
   })
   source_sha256({
-    aarch64: 'a8ef931ae3c5c6ee563b385b9f92a445bff382753437a4014bce3b3e311f3709',
-     armv7l: 'a8ef931ae3c5c6ee563b385b9f92a445bff382753437a4014bce3b3e311f3709',
-       i686: '473f89250a27f55d3490140565d881a1a3e5911d624bd4f791cf79e3c95d2f8c',
-     x86_64: '48dcbcb09f9802b144347b164080df762ddbd0077966e273116c973edb3020ef'
+    aarch64: 'bda6b8fbeb63d4be15f4d98604502fc9f08671e2ffd0c1b838f1ad1a053535dc',
+     armv7l: 'bda6b8fbeb63d4be15f4d98604502fc9f08671e2ffd0c1b838f1ad1a053535dc',
+       i686: 'fc51a5a4551c697a56324bf63052e76302d9491407a6bbf48a43f57a1e4f2e88',
+     x86_64: '32c5efedf69f4a95635ea2923f6a6ee90ce6ca83df0bd43ba55dd662d5af429a'
   })
 
   no_compile_needed
