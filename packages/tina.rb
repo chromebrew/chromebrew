73be094b9f5710c5e6e66ca27c1263fb8e2b1854
Ed Reel <edreel@gmail.com>
2026-08-16 03:07:20 +0000

    Tina 0.1.12 => 0.1.14 (#18224)

--- packages/tina.rb
+++ packages/tina.rb
@@ -1,34 +1,25 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Tina < Package
+class Tina < Autotools
   description 'Tina is a personal information manager with a curses interface.'
   homepage 'https://devel.ringlet.net/misc/tina/'
-  version '0.1.12'
+  version '0.1.14'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://devel.ringlet.net/files/misc/tina/tina-0.1.12.tar.xz'
-  source_sha256 '108cec9d1dbd938c67f3fae2e4238e12d064f1d12bd0d9debd0ee3ac79553edf'
-  binary_compression 'tar.xz'
+  source_url "https://devel.ringlet.net/files/misc/tina/tina-#{version}.tar.xz"
+  source_sha256 'eae5b5dfe71005c8ec24cee0b69c4b2da8fb45061a543e48a6599e4b54a8fcec'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '07d4a2cdbffe0286a98b28ee0041d6d8cdb40ca18721c8dc4aeebcfd95416559',
-     armv7l: '07d4a2cdbffe0286a98b28ee0041d6d8cdb40ca18721c8dc4aeebcfd95416559',
-       i686: 'decefb2b9b8b30d1e34e8b82ddb89a4a416f5613352b75342922e068f26a3344',
-     x86_64: '5112dcef239f9f6e8b68eb420529d01e889fe84ae9f009e86bc21030096e2273'
+    aarch64: 'd19a814b70e60892e1123d1728efe97514d0c81f26235d897de36e8735541e2f',
+     armv7l: 'd19a814b70e60892e1123d1728efe97514d0c81f26235d897de36e8735541e2f',
+       i686: 'c19a7a470ff27131fd94a1221933d11a1125c4aa6b622063bd9f31e5446dd14a',
+     x86_64: '6216d2d92fc5197c84da22909fa3d534f7f00e787aaa580b941bf0b605da4244'
   })
 
-  def self.build
-    system "sed -i 's,<curses.h>,<#{CREW_PREFIX}/include/ncurses/curses.h>,' curslib.c"
-    system "sed -i 's,<curses.h>,<#{CREW_PREFIX}/include/ncurses/curses.h>,' error.c"
-    system "sed -i 's,<curses.h>,<#{CREW_PREFIX}/include/ncurses/curses.h>,' main.c"
-    system "sed -i 's,<curses.h>,<#{CREW_PREFIX}/include/ncurses/curses.h>,' view.c"
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}"
-    system 'make'
-  end
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'ncurses' => :executable
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  autotools_pre_configure_options "CFLAGS='-I#{CREW_PREFIX}/include/ncursesw'"
 end
