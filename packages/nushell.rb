a7def822e7ad4aa702fe7ce1d70ae73309c10f14
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-16 00:30:11 +0000

    nushell -> 0.115.0 in updater-nushell-0.115.0

--- packages/nushell.rb
+++ packages/nushell.rb
@@ -3,7 +3,7 @@ require 'package'
 class Nushell < Package
   description 'A new type of shell'
   homepage 'https://www.nushell.sh/'
-  version '0.114.1'
+  version '0.115.0'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.28'
@@ -13,9 +13,9 @@ class Nushell < Package
      x86_64: "https://github.com/nushell/nushell/releases/download/#{version}/nu-#{version}-x86_64-unknown-linux-gnu.tar.gz"
   })
   source_sha256({
-    aarch64: '43e510c8a54e478577393d2754e1480e768de8df5f6d813c5db7de151d63bdfb',
-     armv7l: '43e510c8a54e478577393d2754e1480e768de8df5f6d813c5db7de151d63bdfb',
-     x86_64: '8802b26edcdf1a64477567b5ce909fbae3d72d731c8f0847892ea16c6fa73c53'
+    aarch64: 'db498add9f3cec41d79ba474ee4b8f247242381fcbac9bf258d673da54b890a9',
+     armv7l: 'db498add9f3cec41d79ba474ee4b8f247242381fcbac9bf258d673da54b890a9',
+     x86_64: 'da83cfe482060d2c34b6b9af829975a313bce6b92e0398c3b2a59cb38630c7b2'
   })
 
   no_compile_needed
