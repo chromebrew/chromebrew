73a528a0a3729f447a794e26a764ef6ef73a8ae5
Ed Reel <edreel@gmail.com>
2026-08-19 01:30:08 +0000

    Totem_pl_parser 3.26.6 => 3.26.7

--- packages/totem_pl_parser.rb
+++ packages/totem_pl_parser.rb
@@ -3,23 +3,28 @@ require 'buildsystems/meson'
 class Totem_pl_parser < Meson
   description 'Totem playlist parsing library'
   homepage 'https://gitlab.gnome.org/GNOME/totem-pl-parser'
-  version '3.26.6'
+  version '3.26.7'
   license 'LGPL GPL-2'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://download.gnome.org/sources/totem-pl-parser/3.26/totem-pl-parser-3.26.6.tar.xz'
-  source_sha256 'c0df0f68d5cf9d7da43c81c7f13f11158358368f98c22d47722f3bd04bd3ac1c'
+  source_url "https://download.gnome.org/sources/totem-pl-parser/#{version.split('.')[0..1].join('.')}/totem-pl-parser-#{version}.tar.xz"
+  source_sha256 '60d517c1acabe54ae337f64451264fc76730696eaae26b5480fb37166689b5f3'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '0b5ddaa9e3078bf36f0f6c86047ac03c23df75cc31bf098191a269e9c978c1c9',
-     armv7l: '0b5ddaa9e3078bf36f0f6c86047ac03c23df75cc31bf098191a269e9c978c1c9',
-     x86_64: 'd940b25706611415e4fe0d8e7d090d55b3b7b44d02a6884f9b5386a0a3555175'
+    aarch64: '985a8f2eda4f5bb4f75c6c1757deb88bf0c40d67088157bd3f0c738da0b2cb3a',
+     armv7l: '985a8f2eda4f5bb4f75c6c1757deb88bf0c40d67088157bd3f0c738da0b2cb3a',
+     x86_64: '21d088f2c3d3bc5fa1f960f1ea0b9f5d4b725d4e781707f5316bdb6a0c10c4c7'
   })
 
-  depends_on 'gobject_introspection'
-  depends_on 'libarchive'
-  depends_on 'libgcrypt'
-  depends_on 'libsoup'
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gobject_introspection' => :library
+  depends_on 'libarchive' => :library
+  depends_on 'libgcrypt' => :library
+  depends_on 'libsoup' => :library
+  depends_on 'libxml2' => :library
+  depends_on 'uchardet' => :library
 
   gnome
 end
