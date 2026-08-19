e8d1e131d5058d66dc331e30eea303ef69c6f7d2
Ed Reel <edreel@gmail.com>
2026-08-19 23:45:47 +0000

    Uwsgi 2.0.17 => 2.0.31 (#18265)

--- packages/uwsgi.rb
+++ packages/uwsgi.rb
@@ -3,13 +3,31 @@ require 'package'
 class Uwsgi < Package
   description 'uWSGI application server container'
   homepage 'https://uwsgi-docs.readthedocs.io/'
-  version '2.0.17'
+  version '2.0.31'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://github.com/unbit/uwsgi/archive/2.0.17.tar.gz'
-  source_sha256 '3dc2e9b48db92b67bfec1badec0d3fdcc0771316486c5efa3217569da3528bf2'
+  source_url 'https://github.com/unbit/uwsgi.git'
+  git_hashtag version
+  binary_compression 'tar.zst'
 
-  depends_on 'pcre'
+  binary_sha256({
+    aarch64: 'a8b6047057a3ffff0726325ae3cf0ebe830655ef92c78a4f8b282668b8e28f65',
+     armv7l: 'a8b6047057a3ffff0726325ae3cf0ebe830655ef92c78a4f8b282668b8e28f65',
+       i686: 'bb7c677a15901b9252021548938ec7f7eadf654128fcc1c13256afdff2babda6',
+     x86_64: 'be2d84db95ed5ff9ad4a7c74bcd05dc6909b7a24c60429e729245c64aca67776'
+  })
+
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'libcap' => :executable
+  depends_on 'libxcrypt' => :executable
+  depends_on 'libxml2' => :executable
+  depends_on 'openssl' => :executable
+  depends_on 'pcre' => :executable
+  depends_on 'pcre2' => :executable
+  depends_on 'python3' => :executable
+  depends_on 'util_linux' => :executable
+  depends_on 'zlib' => :executable
 
   def self.build
     puts
@@ -22,6 +40,6 @@ class Uwsgi < Package
   end
 
   def self.install
-    system "install -Dm755 uwsgi #{CREW_DEST_PREFIX}/bin/uwsgi"
+    FileUtils.install 'uwsgi', "#{CREW_DEST_PREFIX}/bin/uwsgi", mode: 0o755
   end
 end
