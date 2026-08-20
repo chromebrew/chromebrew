0d13e9dec36ae3cb57f5c9f2767d0a2475bc5e8c
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-20 01:33:10 +0000

    faad2 -> 2.11.3 in updater-faad2-2.11.3

--- packages/faad2.rb
+++ packages/faad2.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Faad2 < CMake
   description 'Freeware Advanced Audio (AAC) Decoder'
   homepage 'https://sourceforge.net/projects/faac/'
-  version '2.11.2'
+  version '2.11.3'
   license 'GPL2'
   compatibility 'all'
   source_url 'https://github.com/knik0/faad2.git'
