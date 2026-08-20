70cfc81dfae0c58ffcb73ade877dfed722ceea0b
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-20 01:34:47 +0000

    gradle -> 9.7.1 in updater-gradle-9.7.1

--- packages/gradle.rb
+++ packages/gradle.rb
@@ -3,11 +3,11 @@ require 'package'
 class Gradle < Package
   description 'From mobile apps to microservices, from small startups to big enterprises, Gradle helps teams build, automate and deliver better software, faster.'
   homepage 'https://gradle.org/'
-  version '9.7.0'
+  version '9.7.1'
   license 'Apache-2.0'
   compatibility 'all'
   source_url "https://github.com/gradle/gradle-distributions/releases/download/v#{version}/gradle-#{version}-bin.zip"
-  source_sha256 '84fbba45c7f4c64abc77460e1c00f541e9f960e3c7ed2538f1ede19eacd873ae'
+  source_sha256 'acd53f1edaf02f1a8ff99879f8a34b302661a057d9b063ae9e35b552f804d20a'
 
   depends_on 'openjdk8' unless File.exist? "#{CREW_PREFIX}/bin/java"
 
