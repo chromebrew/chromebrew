e96aa56fd6bca1b2ea6b29cc2e65529c5ba1f190
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-21 01:26:49 +0000

    protobuf -> 36.0 in updater-protobuf-36.0

--- packages/protobuf.rb
+++ packages/protobuf.rb
@@ -6,7 +6,7 @@ require 'buildsystems/cmake'
 class Protobuf < CMake
   description 'Protocol Buffers - Googles data interchange format'
   homepage 'https://protobuf.dev'
-  version '35.1'
+  version '36.0'
   license 'BSD'
   compatibility 'all'
   source_url 'https://github.com/protocolbuffers/protobuf.git'
