b9831057a479cd75c8ba4bacd63ca6ee6e3b99a1
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-21 01:27:42 +0000

    zola -> 0.23.4 in updater-zola-0.23.4

--- packages/zola.rb
+++ packages/zola.rb
@@ -3,12 +3,12 @@ require 'package'
 class Zola < Package
   description 'A fast static site generator'
   homepage 'https://www.getzola.org/'
-  version '0.23.3'
+  version '0.23.4'
   license 'MIT'
   compatibility 'x86_64'
   min_glibc '2.29'
   source_url "https://github.com/getzola/zola/releases/download/v#{version}/zola-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-  source_sha256 'f07c92607e5745268b576bd325ceef3a582aada253bb64db8d92a8a85303d958'
+  source_sha256 '54d1a347781b2f32330914fcc02def81c7e3ddb6111b36d1cc89c06557aed1de'
 
   no_compile_needed
 
