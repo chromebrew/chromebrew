370e75f43c93c19c2a5c3ee26bc8d4f07b671fb5
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-21 01:27:30 +0000

    vapoursynth -> 80A1 in updater-vapoursynth-80A1

--- packages/vapoursynth.rb
+++ packages/vapoursynth.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Vapoursynth < Meson
   description 'A video processing framework with the future in mind'
   homepage 'http://www.vapoursynth.com/'
-  version '77'
+  version '80A1'
   license 'LGPL-2.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/vapoursynth/vapoursynth.git'
