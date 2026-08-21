3428021dab4823510a7547e06d428abc9e3c7895
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-21 01:26:40 +0000

    perl_uri -> 5.36-perl5.44 in updater-perl_uri-5.36-perl5.44

--- packages/perl_uri.rb
+++ packages/perl_uri.rb
@@ -3,11 +3,11 @@ require 'buildsystems/perl'
 class Perl_uri < PERL
   description 'Uniform Resource Identifiers (absolute and relative)'
   homepage 'https://metacpan.org/pod/URI'
-  version "5.35-#{CREW_PERL_VER}"
+  version "5.36-#{CREW_PERL_VER}"
   license 'GPL PerlArtistic'
   compatibility 'all'
   source_url "https://cpan.metacpan.org/authors/id/O/OA/OALDERS/URI-#{version.split('-')[0]}.tar.gz"
-  source_sha256 '89648964ce5ae006726951f42f718576fbefe9e98a41bd2212d57386163092d2'
+  source_sha256 '32719e57413db6e18492e104707b95c2210df637614c512e7368c9ec3c2f783b'
   binary_compression 'tar.zst'
 
   binary_sha256({
