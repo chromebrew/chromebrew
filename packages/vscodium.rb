87125e00729a7ec72e6c0e872bbe1548391a2e8b
Ed Reel <edreel@gmail.com>
2026-08-22 23:44:20 +0000

    Vscodium 1.121.03429 => 1.126.04524 (#18349)

--- packages/vscodium.rb
+++ packages/vscodium.rb
@@ -3,7 +3,7 @@ require 'package'
 class Vscodium < Package
   description 'VSCodium is Open Source Software Binaries of VSCode with a community-driven default configuration.'
   homepage 'https://vscodium.com/'
-  version '1.121.03429'
+  version ARCH.eql?('x86_64') ? '1.126.04524' : '1.121.03429'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.28'
