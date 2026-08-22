0a57ca05f6667e843d173ed15d210698b125eb03
Ed Reel <edreel@gmail.com>
2026-08-22 03:15:04 +0000

    Textract 1.6.4 => 2.0.0

--- packages/textract.rb
+++ packages/textract.rb
@@ -1,39 +1,32 @@
-require 'package'
+require 'buildsystems/pip'
 
-class Textract < Package
+class Textract < Pip
   description 'Textract provides text extracting tools for many formats.'
   homepage 'http://textract.readthedocs.io/'
-  version '1.6.4'
+  version '2.0.0'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://github.com/deanmalmgren/textract.git'
-  git_hashtag "v#{version}"
-  binary_compression 'tpxz'
+  source_url 'SKIP'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd621a56b5d12557eb7d96c234a70f46fe009bb9f9cf4cb3ccf1d3d2193f95ce2',
-     armv7l: 'd621a56b5d12557eb7d96c234a70f46fe009bb9f9cf4cb3ccf1d3d2193f95ce2',
-     x86_64: '0b3fedcbfd45de8555ac00ae5bf343bc979a0d8b843315335959996332e8fb90'
+    aarch64: 'ff36c30fa82026d3c7753cdbd7ab2230a64c77fe6e40e185ed7e1ffb43aa0411',
+     armv7l: 'ff36c30fa82026d3c7753cdbd7ab2230a64c77fe6e40e185ed7e1ffb43aa0411',
+     x86_64: 'b0a12650fd86780c09af6908c3c1fb780fe67e9e4c73e6e38522af1dda7b18fa'
   })
 
-  depends_on 'py3_pdfminer_six'
-  depends_on 'py3_ebooklib'
-  depends_on 'py3_python_pptx'
-  depends_on 'py3_extract_msg'
-  depends_on 'py3_xlrd'
-  depends_on 'py3_docx2txt'
-  depends_on 'py3_argcomplete'
-  depends_on 'py3_speechrecognition'
-  depends_on 'py3_chardet'
-  depends_on 'py3_beautifulsoup4'
-  depends_on 'py3_six'
-  depends_on 'python3' => :build
+  depends_on 'py3_argcomplete' => :library
+  depends_on 'py3_beautifulsoup4' => :library
+  depends_on 'py3_chardet' => :library
+  depends_on 'py3_docx2txt' => :library
+  depends_on 'py3_ebooklib' => :library
+  depends_on 'py3_extract_msg' => :library
+  depends_on 'py3_pdfminer_six' => :library
+  depends_on 'py3_python_pptx' => :library
+  depends_on 'py3_six' => :library
+  depends_on 'py3_speechrecognition' => :library
+  depends_on 'py3_xlrd' => :library
+  depends_on 'python3' => :logical
 
-  def self.build
-    system "python3 setup.py build #{PY3_SETUP_BUILD_OPTIONS}"
-  end
-
-  def self.install
-    system "python3 setup.py install #{PY_SETUP_INSTALL_OPTIONS}"
-  end
+  no_source_build
 end
