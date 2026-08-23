39147c904dfa7dd712c35165751cb1a4191d7e84
Ed Reel <edreel@gmail.com>
2026-08-23 08:34:29 +0000

    Vidstab 1.1.1 => 1.1.2 (#18353)

--- packages/vidstab.rb
+++ packages/vidstab.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Vidstab < CMake
   description 'Transcode video stabilization plugin.'
   homepage 'http://public.hronopik.de/vid.stab/'
-  version '1.1.1'
+  version '1.1.2'
   license 'GPL-2+'
   compatibility 'all'
   source_url 'https://github.com/georgmartius/vid.stab.git'
@@ -11,9 +11,13 @@ class Vidstab < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'fabc6a0fb8cc0570cd9c9ab031c5bdc331007ea5f3b2edaa1efb56818915ae7e',
-     armv7l: 'fabc6a0fb8cc0570cd9c9ab031c5bdc331007ea5f3b2edaa1efb56818915ae7e',
-       i686: '8feab77e6b415747500ec4d398cfefdc8f94a13244b61beea7eceb1c4e859835',
-     x86_64: 'e55882a58cb795e010a3fcdbd5a43d636f8446389874c77cd231cbea71d6d1cc'
+    aarch64: 'db0cfcdea3641fc8883ba6de616379297ac76c81c0ddddd5a630c3a4aa2b0f78',
+     armv7l: 'db0cfcdea3641fc8883ba6de616379297ac76c81c0ddddd5a630c3a4aa2b0f78',
+       i686: '5961d2fdde5156103d08c9fb51a3330eaf4cf8b22503a9559979049cbfdd37bb',
+     x86_64: 'b2ff3858190a14761a8d2cdcd3146a2c099111f315900cb5989d277827f17d5f'
   })
+
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 end
