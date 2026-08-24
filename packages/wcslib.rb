23cfc05b84b27830b0a29a4adc4e63a3ae0540e7
Ed Reel <edreel@gmail.com>
2026-08-24 23:51:08 +0000

    Wcslib 8.2.1 => 8.9 (#18373)

--- packages/wcslib.rb
+++ packages/wcslib.rb
@@ -3,21 +3,23 @@ require 'buildsystems/autotools'
 class Wcslib < Autotools
   description 'C library that implements the "World Coordinate System" (WCS) standard in FITS (Flexible Image Transport System).'
   homepage 'https://www.atnf.csiro.au/people/mcalabre/WCS/wcslib/'
-  version '8.2.1'
+  version '8.9'
   license 'Copyright (C) 1995-2023, Mark Calabretta'
   compatibility 'all'
-  source_url 'https://www.atnf.csiro.au/people/mcalabre/WCS/wcslib-8.2.1.tar.bz2'
-  source_sha256 'b666c79568fc0cf16b6d585ff3d560ae5e472c3b1125a90ccc8038f8e84aca19'
+  source_url "https://www.atnf.csiro.au/computing/software/wcs/wcslib-releases/wcslib-#{version}.tar.bz2"
+  source_sha256 '82ac09ce5091b0bf06cec8f5cdeec1dabe1d06ba5dfb7ff2bdb0c1680488807b'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'db36d11b51c6b9b41178a9a783162a1d1f846a70b9d8d13403f66fdb757b4189',
-     armv7l: 'db36d11b51c6b9b41178a9a783162a1d1f846a70b9d8d13403f66fdb757b4189',
-       i686: '618d931f5e1115e487d13f6d7fab99f8976b995dbfd44cfd61e92e5303becd90',
-     x86_64: 'c2bba8126d82b1b8a7fe559477fbb9bbf1ebdcccc2277a70924d1aaa1d6004a9'
+    aarch64: '389ea06fc147fd22fefc6bfc09ea8124e8a10d049e581a08de48e461ce9e3e25',
+     armv7l: '389ea06fc147fd22fefc6bfc09ea8124e8a10d049e581a08de48e461ce9e3e25',
+       i686: '4243fd3fed5a0a1732c90153f6eb1b677c687ce086bc035edefd6f04a466e56b',
+     x86_64: '82714b7589fd80823f39f7a308f53ab35981ac4a45af6c797158e662d4d9fcff'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+
   autotools_pre_configure_options "LDFLAGS='-L#{CREW_LIB_PREFIX} #{CREW_ENV_OPTIONS_HASH['LDFLAGS']}'"
 end
