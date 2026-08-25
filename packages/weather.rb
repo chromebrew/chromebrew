8fba2a1080bffbe54a479b028c5b971d3ef0e1e4
Ed Reel <edreel@gmail.com>
2026-08-25 19:33:20 +0000

    Weather 2.3 => 2.5.0

--- packages/weather.rb
+++ packages/weather.rb
@@ -3,46 +3,27 @@ require 'package'
 class Weather < Package
   description 'This command-line utility is intended to provide quick access to current weather conditions and forecasts.'
   homepage 'https://fungi.yuggoth.org/weather/'
-  version '2.3'
+  version '2.5.0'
   license 'ISC'
   compatibility 'all'
-  source_url 'http://fungi.yuggoth.org/weather/src/weather-2.3.tar.xz'
-  source_sha256 '86148d2f1d59867f637f52558cc2a6b3280fac94df55c6e5af0ce37cc190d146'
-  binary_compression 'tar.xz'
+  source_url "https://fungi.yuggoth.org/weather/src/weather-#{version}.tar.xz"
+  source_sha256 'c27ddca607eb96a9ed308895161e37d7b0eb6c6810e18467173dca1d769c4f0e'
 
-  binary_sha256({
-    aarch64: '1b25450f1ee6d3f6ec3b8e0bf356b47741cd7e942b093f6203558dad1a97d5e2',
-     armv7l: '1b25450f1ee6d3f6ec3b8e0bf356b47741cd7e942b093f6203558dad1a97d5e2',
-       i686: '8d6458b01ac471cde9b77bc5de55ba647896317d98b4d75de8fe6bd0751130cc',
-     x86_64: '1c4e0246d00a2fd2511153582862d4b9dccc5a1afb6e7ffd0763a485d60b4490'
-  })
+  no_compile_needed
+  print_source_bashrc
 
   def self.build
-    system "echo 'setpath = $HOME/.weather:#{CREW_PREFIX}/data/weather' >> weatherrc"
+    File.write 'weatherrc', <<~EOF
+      [default]
+      setpath = .:#{HOME}/.weather:#{CREW_PREFIX}/share/weather/data
+    EOF
   end
 
   def self.install
-    system "mkdir -p #{CREW_DEST_PREFIX}/bin"
-    system "mkdir -p #{CREW_DEST_PREFIX}/data/weather"
-    system "mkdir -p #{CREW_DEST_PREFIX}/etc"
-    system "mkdir -p #{CREW_DEST_PREFIX}/man/man1"
-    system "mkdir -p #{CREW_DEST_PREFIX}/man/man5"
-    system "cp weather #{CREW_DEST_PREFIX}/bin"
-    system "cp weather.py #{CREW_DEST_PREFIX}/bin"
-    system "cp airports #{CREW_DEST_PREFIX}/data/weather"
-    system "cp places #{CREW_DEST_PREFIX}/data/weather"
-    system "cp slist #{CREW_DEST_PREFIX}/data/weather"
-    system "cp stations #{CREW_DEST_PREFIX}/data/weather"
-    system "cp zctas #{CREW_DEST_PREFIX}/data/weather"
-    system "cp zlist #{CREW_DEST_PREFIX}/data/weather"
-    system "cp zones #{CREW_DEST_PREFIX}/data/weather"
-    system "cp weatherrc #{CREW_DEST_PREFIX}/etc"
-    system "cp weather.1 #{CREW_DEST_PREFIX}/man/man1"
-    system "cp weatherrc.5 #{CREW_DEST_PREFIX}/man/man5"
-  end
-
-  def self.postinstall
-    system 'if [ ! -d $HOME/.weather ]; then mkdir $HOME/.weather; fi'
-    system "if [ ! -f $HOME/.weather/weatherrc ]; then cp #{CREW_PREFIX}/etc/weatherrc $HOME/.weather; fi"
+    FileUtils.install %w[weather weather.py], "#{CREW_DEST_PREFIX}/bin", mode: 0o755
+    FileUtils.install %w[airports places slist stations zctas zlist zones], "#{CREW_DEST_PREFIX}/share/weather/data", mode: 0o644
+    FileUtils.install 'weatherrc', "#{CREW_DEST_HOME}/.weather/weatherrc", mode: 0o600
+    FileUtils.install 'weather.1', "#{CREW_DEST_MAN_PREFIX}/man1/weather.1", mode: 0o644
+    FileUtils.install 'weatherrc.5', "#{CREW_DEST_MAN_PREFIX}/man5/weatherrc.5", mode: 0o644
   end
 end
