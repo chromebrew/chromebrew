ccf1a5b24df1d1e3ff00a72823f7183e5881f05e
Satadru Pramanik <satadru@gmail.com>
2026-08-26 11:30:25 -0400

    Rebuild vkd3d w/o min_glibc.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/vkd3d.rb
+++ packages/vkd3d.rb
@@ -6,13 +6,12 @@ class Vkd3d < Autotools
   version '2.1'
   license 'LGPL-2.1'
   compatibility 'x86_64'
-  min_glibc '2.31'
   source_url 'https://gitlab.winehq.org/wine/vkd3d.git'
   git_hashtag "vkd3d-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-     x86_64: '9d451383c82c1c46b7306b7a5c5075cc0ff52c987f4fb74901fffe804cfeaa11'
+     x86_64: '47acaa8587ad918bdd1d09b853f8d85642142e256b9ec605019cbbce6e838256'
   })
 
   depends_on 'glibc' => :library
