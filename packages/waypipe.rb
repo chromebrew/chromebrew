f05e95f05291c23445e4a768c3a0daaaeeeb9a5c
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-27 01:58:36 +0000

    waypipe -> 0.11.2 in updater-waypipe-0.11.2

--- packages/waypipe.rb
+++ packages/waypipe.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Waypipe < Meson
   description 'A proxy for Wayland protocol applications.'
   homepage 'https://gitlab.freedesktop.org/mstoeckl/waypipe'
-  version '0.11.1'
+  version '0.11.2'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.freedesktop.org/mstoeckl/waypipe.git'
