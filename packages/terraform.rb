a7da57d6ea4f520047bdddb3809e9a94e315f93d
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-27 01:58:09 +0000

    terraform -> 1.16.0 in updater-terraform-1.16.0

--- packages/terraform.rb
+++ packages/terraform.rb
@@ -3,7 +3,7 @@ require 'package'
 class Terraform < Package
   description 'Terraform is a tool for building, changing, and combining infrastructure safely and efficiently.'
   homepage 'https://www.terraform.io/'
-  version '1.15.9'
+  version '1.16.0'
   license 'Apache-2.0, BSD-2, BSD-4, ECL-2.0, imagemagick, ISC, JSON, MIT, MIT-with-advertising, MPL-2.0 and unicode'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Terraform < Package
      x86_64: "https://releases.hashicorp.com/terraform/#{version}/terraform_#{version}_linux_amd64.zip"
   })
   source_sha256({
-    aarch64: '9b7a67a2ff2db4768a697db96fa5859ae4c9490f7c4f365fa8b9b40ec7b870ae',
-     armv7l: '9b7a67a2ff2db4768a697db96fa5859ae4c9490f7c4f365fa8b9b40ec7b870ae',
-       i686: '5caaa32a7099dd40efb43679e3921095b665edd277b3c4be82e65a83f2e4fc84',
-     x86_64: '76edd0b22d2f27d3d2e097cd793209646f719cf60f02ff3af626b07361137da1'
+    aarch64: 'ae6f7a891fa7d65b30d7c1cebcd02b2ea2998632ec3def200b4fb9d3d3f135c9',
+     armv7l: 'ae6f7a891fa7d65b30d7c1cebcd02b2ea2998632ec3def200b4fb9d3d3f135c9',
+       i686: '1a61829dea3bb5194433f8a44619cd696817706fb4e2d67caaa56aa0bdc6bde3',
+     x86_64: '41d05b927aa174f15d1228c4eba832a323b716c68b415ef5a00179f46cc602d3'
   })
 
   no_compile_needed
