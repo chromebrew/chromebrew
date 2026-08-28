80f6644f856cb3925af8be2b91349139b3d1b04b
Satadru Pramanik <satadru@gmail.com>
2026-08-28 09:38:14 -0400

    Update intel_opencl_clang package.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/intel_opencl_clang.rb
+++ packages/intel_opencl_clang.rb
@@ -2,12 +2,12 @@
 # https://github.com/archlinux/svntogit-community/raw/packages/intel-opencl-clang/trunk/PKGBUILD
 
 require 'buildsystems/cmake'
-Package.load_package("#{__dir__}/llvm19_build.rb")
+Package.load_package("#{__dir__}/#{CREW_LLVM_VER}_build.rb")
 
 class Intel_opencl_clang < CMake
   description 'Wrapper library around clang that can compile OpenCL C kernels to SPIR-V modules'
   homepage 'https://github.com/intel/opencl-clang'
-  version '19.1.0'
+  version '23.1.0'
   license 'llvm'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/intel/opencl-clang.git'
@@ -17,14 +17,15 @@ class Intel_opencl_clang < CMake
   binary_sha256({
     aarch64: '2f2aed9c42fe4f7eb03018ac5a547ad53327fdfa00bca3eec2552fe9b5c47af1',
      armv7l: '2f2aed9c42fe4f7eb03018ac5a547ad53327fdfa00bca3eec2552fe9b5c47af1',
-     x86_64: '4f42c34a737a16ab24350da369989cc1e07f8fc51b9e6487e19f60df228dbf21'
+     x86_64: 'fe2fe6331b7b0c9dddad299a633f9e7907f7d0e847f5899a22af436beced9a76'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'llvm19_dev' # R
-  depends_on 'llvm19_lib' # R
-  depends_on 'spirv_llvm_translator' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'llvm_dev' => :library
+  depends_on 'llvm_lib' => :library
+  depends_on 'spirv_llvm_translator' => :library
 
-  cmake_options "-DPREFERRED_LLVM_VERSION=#{Llvm19_build.version} -DLLVM_DIR=#{CREW_LIB_PREFIX}/cmake/llvm"
+  cmake_options "-DPREFERRED_LLVM_VERSION=#{`clang -dumpversion`.chomp.sub(/\.\d+$/, '')} -DLLVM_DIR=#{CREW_LIB_PREFIX}/cmake/llvm"
 end
