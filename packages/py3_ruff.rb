15267b28b0b661b76d6adf3fc7c07085ee65657b
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-08-29 00:32:04 +0000

    py3_ruff -> 0.16.5-py3.14 in updater-py3_ruff-0.16.5-py3.14

--- packages/py3_ruff.rb
+++ packages/py3_ruff.rb
@@ -3,7 +3,7 @@ require 'buildsystems/pip'
 class Py3_ruff < Pip
   description 'An extremely fast Python linter, written in Rust.'
   homepage 'https://docs.astral.sh/ruff'
-  version "0.16.4-#{CREW_PY_VER}"
+  version "0.16.5-#{CREW_PY_VER}"
   license 'GPL-2.0'
   compatibility 'all'
   source_url 'SKIP'
