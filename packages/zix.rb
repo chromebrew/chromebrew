d6c51370c985386189faee1fba1944d33205ed3b
Ed Reel <edreel@gmail.com>
2026-09-02 14:37:20 +0000

    Zix 0.8.0 => 0.8.2 (#18547)

--- packages/zix.rb
+++ packages/zix.rb
@@ -6,7 +6,7 @@ require 'buildsystems/meson'
 class Zix < Meson
   description 'A lightweight C99 portability and data structure library'
   homepage 'https://gitlab.com/drobilla/zix'
-  version '0.8.0'
+  version '0.8.2'
   license '0BSD OR ISC'
   compatibility 'all'
   source_url 'https://gitlab.com/drobilla/zix.git'
@@ -14,11 +14,12 @@ class Zix < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '53fcab8ea102cde913a61df8fde58198f3d69095a8f70689c5f91d9a1f9b3d14',
-     armv7l: '53fcab8ea102cde913a61df8fde58198f3d69095a8f70689c5f91d9a1f9b3d14',
-       i686: '4cece6cdbf6d5b79a1892fbc379c6110aa0a9ab252b5358d31a5d1e8740507d5',
-     x86_64: '8ab8cedd9c499bc5cbe9a1845050857d8485ef3bb44dade7b3b106fccbaabb4a'
+    aarch64: 'd510c18cda3426e390228875b16e204a219dd33c477b58a39cb9db05650c5596',
+     armv7l: 'd510c18cda3426e390228875b16e204a219dd33c477b58a39cb9db05650c5596',
+       i686: '8fb137540fdf409b3ba5c6489a29b28eece34b981ad817317b7796b059488e51',
+     x86_64: '667dbd03ec466f96d9e33000f613e5a1cab6bee4a454954ae36d403113d3290a'
   })
 
-  depends_on 'glibc' # R
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 end
