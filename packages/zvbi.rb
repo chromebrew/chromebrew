d4e9c88e78d36ca1d903d8ce46588105a912f2b2
Ed Reel <edreel@gmail.com>
2026-09-02 10:02:13 +0000

    Zvbi 0.2.42-be5efeb => 0.2.45 (#18577)

--- packages/zvbi.rb
+++ packages/zvbi.rb
@@ -3,26 +3,27 @@ require 'buildsystems/autotools'
 class Zvbi < Autotools
   description 'VBI capture and decoding library.'
   homepage 'https://github.com/zapping-vbi/zvbi'
-  version '0.2.42-be5efeb'
+  version '0.2.45'
   license 'GPL-2+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/zapping-vbi/zvbi.git'
-  git_hashtag 'be5efebaadeb95be57d5435468bd3ec0f3007683'
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd2f582b4ee393dc0eca53805098b354efc68a766a32b2277fad26033a4bc1258',
-     armv7l: 'd2f582b4ee393dc0eca53805098b354efc68a766a32b2277fad26033a4bc1258',
-     x86_64: '622a92ff1bb9711422ba33d37a7635245adc0c1ba5f60cfb7a3aa7b150956682'
+    aarch64: 'fbf3013bca7f19416d6bd897bb2160ff7800aa55942f3dcda716dd9b3130359a',
+     armv7l: 'fbf3013bca7f19416d6bd897bb2160ff7800aa55942f3dcda716dd9b3130359a',
+     x86_64: '89c7a3244515259b3ccc016bd568054e43b1d35b6a7c7a8cfa644772faae0f7d'
   })
 
-  depends_on 'glibc' # R
-  depends_on 'libbsd' # R
-  depends_on 'libmd' # R
-  depends_on 'libpng' # R
-  depends_on 'libx11' # R
-  depends_on 'libxau' # R
-  depends_on 'libxcb' # R
-  depends_on 'libxdmcp' # R
-  depends_on 'zlib' # R
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'libbsd' => :executable
+  depends_on 'libmd' => :library
+  depends_on 'libpng' => :library
+  depends_on 'libx11' => :executable
+  depends_on 'libxau' => :executable
+  depends_on 'libxcb' => :executable
+  depends_on 'libxdmcp' => :executable
+  depends_on 'zlib' => :library
 end
