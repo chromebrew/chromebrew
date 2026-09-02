40a72fa12b96fbb5a73a8b019e2d1c52926f04a9
Ed Reel <edreel@gmail.com>
2026-09-02 20:32:27 +0000

    Ansifilter 2.22 => 2.23 (#18580)

--- packages/ansifilter.rb
+++ packages/ansifilter.rb
@@ -1,30 +1,28 @@
-require 'package'
+require 'buildsystems/cmake'
 
-class Ansifilter < Package
+class Ansifilter < CMake
   description 'Ansifilter parses common ANSI codes to remove them or to convert them to another colored text file format (HTML, TeX, LaTeX, RTF, Pango or BBCode).'
   homepage 'http://andre-simon.de/doku/ansifilter/en/ansifilter.php'
-  version '2.22'
+  version '2.23'
   license 'GPL-3+'
-  compatibility 'all'
+  compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.com/saalen/ansifilter.git'
   git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '7742ae36f472fd8082e663c59d3fadb3b47e96c431aca4f6e1b1d483501d7662',
-     armv7l: '7742ae36f472fd8082e663c59d3fadb3b47e96c431aca4f6e1b1d483501d7662',
-       i686: '1058b2e8cd7650305f44863e2e6abb2f657235cc15f95fe01fd1860b7534348c',
-     x86_64: 'd2d7ef991a3ef271a18dfd9be65babe988cb64d4c63521795d6e80d77a4a4ae1'
+    aarch64: 'b0da205b3aec5dc6c8df09607248dfea49d1a52dd0eea6d142d1a4464a8586e9',
+     armv7l: 'b0da205b3aec5dc6c8df09607248dfea49d1a52dd0eea6d142d1a4464a8586e9',
+     x86_64: 'ab5dbfcf568c4d668e9070b4d2acb47f7fbb562ae004276e7e338f8fac9e819f'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :executable
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'qt5_base' => :executable
 
-  def self.build
-    system 'make', "PREFIX=#{CREW_PREFIX}"
-  end
-
-  def self.install
-    system 'make', "PREFIX=#{CREW_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  # For some reason, we are not able to set PREFIX.
+  def self.patch
+    system "sed -i 's,set(PREFIX /usr),set(PREFIX #{CREW_PREFIX}),' CMakeLists.txt"
   end
 end
