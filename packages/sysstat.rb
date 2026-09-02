a9396a76e67dae6ec8e4ee1a0fe29e017c22e22a
Satadru Pramanik <satadru@gmail.com>
2026-09-02 16:31:19 -0400

    Refactor sysstat build.
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>

--- packages/sysstat.rb
+++ packages/sysstat.rb
@@ -11,19 +11,15 @@ class Sysstat < Autotools
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '2bbc4b040c8830a9e1c29c5245aeef718ffeaa0a7a4eba240b3d357e8a4e6743',
-     armv7l: '2bbc4b040c8830a9e1c29c5245aeef718ffeaa0a7a4eba240b3d357e8a4e6743',
-       i686: '8ce65526577737faa583fa9e837bf755ed8ffa4ae07f7c2ff258f34f175d1945',
-     x86_64: '31fda4f33ffb1e7a237e70bb14685e9203b5dbc91652ab9c3afaad5fa373ba0d'
+    aarch64: 'fba6cb3a5d87032eeef03825ce89e4ec870cd5e88eb638b495004a7e79ffe282',
+     armv7l: 'fba6cb3a5d87032eeef03825ce89e4ec870cd5e88eb638b495004a7e79ffe282',
+       i686: '1c6b70023336e57001b3cfc35ac70e5b898f6528ccebb67adc230693041814aa',
+     x86_64: 'deec9065516e164d598cac67ef893d728528c8dd6e46a88bedcb096c47ca03b2'
   })
 
   depends_on 'glibc' => :library
   depends_on 'glibc_lib' => :library
 
-  def self.patch
-    system "sed -i 's/GRP=root/GRP=$(whoami)/' configure"
-    system "sed -i 's/\"root\"/\"$(whoami)\"/g' configure"
-    system "sed -i 's/root/$(whoami)/g' configure.ac"
-    system "sed -i 's/root/$(whoami)/g' sysstat-#{version}.spec"
-  end
+  autotools_pre_configure_options "man_group=#{`whoami`.chomp}"
+  autotools_configure_options '--disable-automated-sar-reporting'
 end
