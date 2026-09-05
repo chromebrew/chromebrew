a78bdc95700cde7dc3b93653b74c05c9f9c1b73e
Ed Reel <edreel@gmail.com>
2026-09-05 00:33:20 +0000

    Attr 2.5.2-4d4a562 => 2.6.0

--- packages/attr.rb
+++ packages/attr.rb
@@ -3,22 +3,22 @@ require 'buildsystems/autotools'
 class Attr < Autotools
   description 'Commands for Manipulating Filesystem Extended Attributes.'
   homepage 'https://savannah.nongnu.org/projects/attr'
-  version '2.5.2-4d4a562'
+  version '2.6.0'
   license 'LGPL-2.1'
   compatibility 'all'
   source_url 'https://git.savannah.gnu.org/git/attr.git'
-  git_hashtag '4d4a562d7b73dcea8bd4b2d40b8c030d5065111b'
-  # git_hashtag "v#{version.split('-').first}"
+  git_hashtag "v#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'ad03b735e0a60ab60b0c68cb5590b0f93206f31d50c27516b89c39814dcec6f1',
-     armv7l: 'ad03b735e0a60ab60b0c68cb5590b0f93206f31d50c27516b89c39814dcec6f1',
-       i686: '9ef95add0044d6d1681132eeb371c3214768af5caf9b25aaa8aa1fe7fa483ec4',
-     x86_64: 'eab11fd7c7b070b0bdaaa130ef733168481a25c05ec7b4c285a3dd893f33e947'
+    aarch64: '1446a5efca5a65e2dda6db0bbb3b64f820dc762d6d9abba3244c3afd6b92a7d5',
+     armv7l: '1446a5efca5a65e2dda6db0bbb3b64f820dc762d6d9abba3244c3afd6b92a7d5',
+       i686: 'd8abeca24be3aa0c70d3ef2e35ea254e7db773d0c64dc32a9fca99f023f8c1da',
+     x86_64: 'ac19a0cb4e2175a81f63fdb32bd4d70da57c9968601ffb40db1509e3b71d7593'
   })
 
   depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
   depends_on 'libcap' => :build
 
   no_filefix
