17c7dd6ceb23fbeb5faa686a7b14c761cd653d16
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-06 00:59:18 +0000

    taglib -> 2.3.2 in updater-taglib-2.3.2

--- packages/taglib.rb
+++ packages/taglib.rb
@@ -3,11 +3,11 @@ require 'buildsystems/cmake'
 class Taglib < CMake
   description 'TagLib is a library for reading and editing the meta-data of several popular audio formats.'
   homepage 'https://taglib.org'
-  version '2.3.1'
+  version '2.3.2'
   license 'LGPL-2.1 and MPL-1.1'
   compatibility 'aarch64 armv7l x86_64'
   source_url "https://taglib.org/releases/taglib-#{version}.tar.gz"
-  source_sha256 'a19d90e6fd41d09a0281ec0fe762d51491d7a6ccffc923c4f7868c5e647ca230'
+  source_sha256 '3ca2d8afaa7f1cf7f6ed10e511ebc368bfacd6dcaa3dbfa690b89e502e8963dc'
   binary_compression 'tar.zst'
 
   binary_sha256({
