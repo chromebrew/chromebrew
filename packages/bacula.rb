0774585cc815e31f344c2f9cc8e955a0aaf0517a
Ed Reel <edreel@gmail.com>
2026-09-06 07:44:05 +0000

    Bacula 15.0.3 => 17.0.0

--- packages/bacula.rb
+++ packages/bacula.rb
@@ -3,27 +3,32 @@ require 'buildsystems/autotools'
 class Bacula < Autotools
   description 'Bacula is a set of computer programs that permits the system administrator to manage backup, recovery, and verification of computer data across a network of computers of different kinds.'
   homepage 'https://www.bacula.org/'
-  version '15.0.3'
+  version '17.0.0'
   license 'AGPL-3'
   compatibility 'x86_64'
-  source_url "https://downloads.sourceforge.net/project/bacula/bacula/#{version}/bacula-#{version}.tar.gz"
-  source_sha256 '294afd3d2eb9d5b71c3d0e88fdf19eb513bfdb843b28d35c0552e4ae062827a1'
+  source_url 'https://gitlab.bacula.org/bacula-community-edition/bacula-community.git'
+  git_hashtag "Release-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-     x86_64: '883d7955b35e0e1608a923b51e863c32fab10a5628567576bb7155c0f22b4276'
+     x86_64: 'c1b245c79892c1d10843c6c615d9e0e9f2ee5008a4ccbddf6264efbe3470d622'
   })
 
-  depends_on 'acl' # R
-  depends_on 'gcc_lib' # R
+  depends_on 'acl' => :executable
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'libcap' => :library
   depends_on 'lzo'
-  depends_on 'mysql'
-  depends_on 'openssl' # R
+  depends_on 'lzo' => :executable
+  depends_on 'mysql' => :library
+  depends_on 'openssl' => :library
   depends_on 'sommelier' => :logical
-  depends_on 'tcpwrappers'
-  depends_on 'zlib' # R
-  depends_on 'zstd' # R
+  depends_on 'tcpwrappers' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
 
+  autotools_build_relative_dir 'bacula'
   autotools_configure_options "--sbindir=#{CREW_PREFIX}/bin \
     --enable-smartalloc \
     --with-mysql \
