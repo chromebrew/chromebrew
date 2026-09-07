383b9175887c5303367dbd513c2a1ba0dd60d856
Ed Reel <edreel@gmail.com>
2026-09-07 23:30:08 +0000

    Atkmm 2.36.3 => 2.36.4 (#18629)

--- packages/atkmm.rb
+++ packages/atkmm.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Atkmm < Meson
   description 'Atkmm is the official C++ interface for the ATK accessibility toolkit library.'
   homepage 'https://www.gtkmm.org/'
-  version '2.36.3'
+  version '2.36.4'
   license 'LGPL-2.1+'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/atkmm.git'
@@ -11,17 +11,20 @@ class Atkmm < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '9224b82b6db207ebcb652edeb5403eec1be918f7b395d449c2b4a6bd04298a37',
-     armv7l: '9224b82b6db207ebcb652edeb5403eec1be918f7b395d449c2b4a6bd04298a37',
-     x86_64: 'a31362204e6e24f6f808389b62868f690b9377185c979dcbdadf737293d4c306'
+    aarch64: 'c60479d42cfab2a0397b6fc4de6d9d9a31921191b953a6b732035a5c7c700c44',
+     armv7l: 'c60479d42cfab2a0397b6fc4de6d9d9a31921191b953a6b732035a5c7c700c44',
+     x86_64: '89ab2e70d0a18d4ba0b59780727b06e2e40b23d09bfb7bb38ec6619593fab75b'
   })
 
-  depends_on 'at_spi2_core' # R
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
-  depends_on 'glibmm_2_68' # R
-  depends_on 'glib' # R
-  depends_on 'libsigcplusplus3' # R
+  depends_on 'at_spi2_core' => :library
+  depends_on 'gcc_lib' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'glibmm_2_68' => :library
+  depends_on 'libsigcplusplus' => :library
+  depends_on 'libsigcplusplus3' => :library
+  depends_on 'mm_common' => :library
 
   gnome
 
