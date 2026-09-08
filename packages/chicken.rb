01ca3864858e9b9237983c1b65ab35e0185cc614
Ed Reel <edreel@gmail.com>
2026-09-08 16:31:15 +0000

    Chicken 5.4.0 => 6.0.0

--- packages/chicken.rb
+++ packages/chicken.rb
@@ -3,27 +3,28 @@ require 'package'
 class Chicken < Package
   description 'CHICKEN is a practical and portable scheme system.'
   homepage 'https://code.call-cc.org/'
-  version '5.4.0'
+  version '6.0.0'
   license 'BSD'
   compatibility 'all'
   source_url "https://code.call-cc.org/releases/#{version}/chicken-#{version}.tar.gz"
-  source_sha256 '3c5d4aa61c1167bf6d9bf9eaf891da7630ba9f5f3c15bf09515a7039bfcdec5f'
+  source_sha256 '92835552b1b687ad26737e429b5aba36510bf429f8816ec0f6d336c8cb41f443'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f9821dfba5cd2e2be8653792fe29a3524e2851a99320702b304fe946963012fe',
-     armv7l: 'f9821dfba5cd2e2be8653792fe29a3524e2851a99320702b304fe946963012fe',
-       i686: 'f5b00b3ac5b6e06fbc17ba765cc1ad44de159178117ecf361fa1741413acc5a8',
-     x86_64: '82e81bf7b319bd2b879407968928fa3adac20d0ee8ea74e3dd62058aafdc8b99'
+    aarch64: '497ad8e35946e23e39e4fea42dd14bad382e7a902ae59987c5cdf60520c4336d',
+     armv7l: '497ad8e35946e23e39e4fea42dd14bad382e7a902ae59987c5cdf60520c4336d',
+       i686: '469dc02022c66f9b3b328c2a7a1627d48099af3b5aa352f5bb9e7d3e9f2ca3cf',
+     x86_64: 'f48a8b54e92b10c600961573c29aa05a4ac4d309c958cfaa37cfe5c5af9a25b3'
   })
 
-  depends_on 'filecmd' => :build
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 
   def self.build
-    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}"
+    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}", 'PLATFORM=linux'
   end
 
   def self.install
-    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'install'
+    system 'make', "PREFIX=#{CREW_PREFIX}", "LIBDIR=#{CREW_LIB_PREFIX}", "DESTDIR=#{CREW_DEST_DIR}", 'PLATFORM=linux', 'install'
   end
 end
