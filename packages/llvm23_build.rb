a7ff773a5726203cc424b7d7d293722afc2b0646
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-09 00:54:32 +0000

    llvm23_build -> 23.1.1 in updater-llvm23_build-23.1.1

--- packages/llvm23_build.rb
+++ packages/llvm23_build.rb
@@ -6,12 +6,12 @@ class Llvm23_build < Package
   @llvm_projects_to_build = ARCH == 'x86_64' ? 'bolt;clang;clang-tools-extra;lld;lldb;compiler-rt;polly' : 'clang;clang-tools-extra;lld;lldb;compiler-rt;polly'
   description "The LLVM Project is a collection of modular and reusable compiler and toolchain technologies. The packages included are: #{@llvm_projects_to_build.gsub(';', ' ')}"
   homepage 'https://llvm.org/'
-  version '23.1.0'
+  version '23.1.1'
   # When upgrading llvm*_build, be sure to upgrade llvm_lib*, llvm_dev*, libclc, and openmp in tandem.
   license 'Apache-2.0-with-LLVM-exceptions, UoI-NCSA, BSD, public-domain, rc, Apache-2.0 and MIT'
   compatibility 'all'
   source_url "https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-#{version}.tar.gz"
-  source_sha256 'd8657b2a7291e518407bf13c4b41c85ef2cded2d4354097a2f451644dfc817b0'
+  source_sha256 '851b3d701a4fbdd9f69536d4acda578469e810ca7056687d6556443f5fd39557'
   binary_compression 'tar.zst'
 
   binary_sha256({
