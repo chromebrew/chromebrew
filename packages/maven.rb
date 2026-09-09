95955a7b89c0bf938c587c65dfdc0e2d62c76ce7
Ed Reel <edreel@gmail.com>
2026-09-09 08:12:38 +0000

    Maven 3.9.12 => 3.9.16

--- packages/maven.rb
+++ packages/maven.rb
@@ -3,11 +3,11 @@ require 'package'
 class Maven < Package
   description 'Apache Maven is a software project management and comprehension tool.'
   homepage 'https://maven.apache.org/'
-  version '3.9.12'
+  version '3.9.16'
   license 'Apache-2.0'
   compatibility 'all'
   source_url "https://dlcdn.apache.org/maven/maven-3/#{version}/binaries/apache-maven-#{version}-bin.tar.gz"
-  source_sha256 'fa2c9948729296c23afd18fd01a90f62cdda09a46191b54a8bc3764c2eee812e'
+  source_sha256 '80ffca22aed9e8b9713a232f3394fd81d7f20322df75efdb2b047dbd3e3a23bb'
 
   depends_on 'openjdk17' unless File.exist? "#{CREW_PREFIX}/bin/java"
 
