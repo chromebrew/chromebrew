d231e9507f06f72b0a923fe54b1ed6dbbe9e4975
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-09 00:51:55 +0000

    codon -> 0.20.0 in updater-codon-0.20.0

--- packages/codon.rb
+++ packages/codon.rb
@@ -3,11 +3,11 @@ require 'package'
 class Codon < Package
   description 'Codon is a high-performance Python compiler that compiles Python code to native machine code without any runtime overhead.'
   homepage 'https://docs.exaloop.io/codon/'
-  version '0.19.6'
+  version '0.20.0'
   license 'Apache-2.0'
   compatibility 'x86_64'
   source_url "https://github.com/exaloop/codon/releases/download/v#{version}/codon-linux-x86_64.tar.gz"
-  source_sha256 '38befce9eb87244698014b1fbe56a4102660120f1b82b3c7777c2a98c109770a'
+  source_sha256 '3699b803e65e3faf3321f114ae111a8981e11ac603a0d2b8447877063496c23b'
 
   depends_on 'llvm'
   depends_on 'python3'
