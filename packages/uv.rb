645890bf50a081d4f21d26cdd88b205dc5388a51
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-10 00:54:51 +0000

    uv -> 0.12.12 in updater-uv-0.12.12

--- packages/uv.rb
+++ packages/uv.rb
@@ -3,7 +3,7 @@ require 'package'
 class Uv < Package
   description 'An extremely fast Python package and project manager, written in Rust.'
   homepage 'https://docs.astral.sh/uv/'
-  version '0.12.10'
+  version '0.12.12'
   license 'Apache-2.0, MIT'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Uv < Package
      x86_64: "https://releases.astral.sh/github/uv/releases/download/#{version}/uv-x86_64-unknown-linux-gnu.tar.gz"
   })
   source_sha256({
-    aarch64: '682a52fea5deb07238420d7197e2d7cc10d558872f008a9b6799fb5326c8e420',
-     armv7l: '682a52fea5deb07238420d7197e2d7cc10d558872f008a9b6799fb5326c8e420',
-       i686: 'd943d0c0af37ad884bef24972753f7c93e5765b458e6492a3eb0c2b855c83a58',
-     x86_64: '173d95a0c32d18c896c46ba6fafbf3cf9c14ab74b033f81b76c883ef492a976b'
+    aarch64: 'ed00794f3750bcf405556ac28e8aff0480a4e309e4ab1505df9f96b83088b230',
+     armv7l: 'ed00794f3750bcf405556ac28e8aff0480a4e309e4ab1505df9f96b83088b230',
+       i686: 'ce4411f863d3bd0cf97699eecebce9331ae72d69cf57735806321bf60b637b03',
+     x86_64: 'ab9b309d4586403f024e100abaceb396616e178a553e2500c36087d180f09509'
   })
 
   no_compile_needed
