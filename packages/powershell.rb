d7c8bce64017eaebb9e3d42240439dc7c7ece9d3
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-10 00:54:06 +0000

    powershell -> 7.6.6 in updater-powershell-7.6.6

--- packages/powershell.rb
+++ packages/powershell.rb
@@ -3,7 +3,7 @@ require 'package'
 class Powershell < Package
   description 'Powershell is a cross-platform, task-based command-line shell and scripting language that helps rapidly automate tasks that manage operating systems and processes'
   homepage 'https://learn.microsoft.com/en-us/powershell/'
-  version '7.6.5'
+  version '7.6.6'
   license 'MIT'
   compatibility 'aarch64 armv7l x86_64'
   min_glibc '2.34'
@@ -14,9 +14,9 @@ class Powershell < Package
      x86_64: "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-linux-x64.tar.gz"
   })
   source_sha256({
-    aarch64: 'c5c1902984541d72e8220b2328a36de9d4c10ebedfd9506f73b8a550a8af8df5',
-     armv7l: 'c5c1902984541d72e8220b2328a36de9d4c10ebedfd9506f73b8a550a8af8df5',
-     x86_64: 'b34ab3b19acac1d3d4d0d3cfdb02acf62f457b0b6a962ff008132033f7566844'
+    aarch64: '7652b53ea39ca3ea2db4ff3d63769fcccb858bde0eb0ec6be4e927a47544ee55',
+     armv7l: '7652b53ea39ca3ea2db4ff3d63769fcccb858bde0eb0ec6be4e927a47544ee55',
+     x86_64: 'ddbc4a2d113bbd46d283cfedcbcd117a70caefd7673f41f2b4e0000badf103bc'
   })
 
   depends_on 'xdg_base'
