c1bdf642c67bc012feb8cb37ccf59e453c333825
Ed Reel <edreel@gmail.com>
2026-09-10 23:08:32 +0000

    Curtail 1.3.0 => 1.16.2

--- packages/curtail.rb
+++ packages/curtail.rb
@@ -1,34 +1,29 @@
-require 'package'
+require 'buildsystems/meson'
 
-class Curtail < Package
+class Curtail < Meson
   description 'Curtail (previously ImCompressor) is an useful image compressor, supporting PNG, JPEG and WEBP file types.'
   homepage 'https://github.com/Huluti/Curtail'
-  version '1.3.0'
+  version '1.16.2'
   license 'GPL-3'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://github.com/Huluti/Curtail/archive/1.3.0.tar.gz'
-  source_sha256 '56859211b1b147aec677a00295e524b6beabcdf415938db1bd8ca4dae79409cf'
+  source_url 'https://github.com/Huluti/Curtail.git'
+  git_hashtag version
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '6d5947e652598d3e7aac80193bbb5a51e3b4bd2678ed8ed1f459e63b68c74da0',
-     armv7l: '6d5947e652598d3e7aac80193bbb5a51e3b4bd2678ed8ed1f459e63b68c74da0',
-       i686: '2643affd0cc34358f1b3540f7701313e699401975d242f5128b856b742cd7699',
-     x86_64: '8fab707529ff0e2d8db29045a9e05a91a30d8b66d738dde84a5ca12145a0d6fa'
+    aarch64: 'd3352d0a5a64e6d837e03b9c910b33adf4ec7eeacba9c3996ae0018683bc4d81',
+     armv7l: 'd3352d0a5a64e6d837e03b9c910b33adf4ec7eeacba9c3996ae0018683bc4d81',
+     x86_64: 'd2e0fd2a8c490e8210f6940e88a83f03df922d96bf48786d9f7eea8b9ea6b708'
   })
 
-  depends_on 'gtk3'
-  depends_on 'optipng'
-  depends_on 'pngquant'
-  depends_on 'jpegoptim'
-  depends_on 'libwebp'
-
-  def self.build
-    system "meson setup #{CREW_MESON_OPTIONS} build"
-    system 'ninja -C build'
-  end
-
-  def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} ninja -C build install"
-  end
+  depends_on 'appstream' => :library
+  depends_on 'blueprint_compiler' => :library
+  depends_on 'desktop_file_utils' => :library
+  depends_on 'gobject_introspection' => :library
+  depends_on 'gtk4' => :library
+  depends_on 'jpegoptim' => :library
+  depends_on 'libadwaita' => :library
+  depends_on 'libwebp' => :library
+  depends_on 'optipng' => :library
+  depends_on 'pngquant' => :library
 end
