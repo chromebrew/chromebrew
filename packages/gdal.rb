51ae8704c51cc93361cc017bf9d2435240e52fd1
Ed Reel <edreel@gmail.com>
2026-09-14 21:33:27 +0000

    Gdal 3.7.3 => 3.13.3

--- packages/gdal.rb
+++ packages/gdal.rb
@@ -3,11 +3,11 @@ require 'buildsystems/cmake'
 class Gdal < CMake
   description 'The Geospatial Data Abstraction Library is a translator for raster and vector geospatial data formats.'
   homepage 'https://gdal.org/'
-  version '3.7.3'
+  version '3.13.3'
   license 'BSD, Info-ZIP and MIT'
   compatibility 'aarch64 armv7l x86_64'
-  source_url 'https://download.osgeo.org/gdal/3.7.3/gdal-3.7.3.tar.xz'
-  source_sha256 'e0a6f0c453ea7eb7c09967f50ac49426808fcd8f259dbc9888140eb69d7ffee6'
+  source_url "https://download.osgeo.org/gdal/#{version}/gdal-#{version}.tar.xz"
+  source_sha256 'a3348c2102dd91962290007d7c293b467bb1b0cd89f086f24bb97b9b653a9804'
   binary_compression 'tar.zst'
 
   binary_sha256({
@@ -16,39 +16,43 @@ class Gdal < CMake
      x86_64: '0f7ba51a7a8fa276c28992219004b7c56b3fba03fc01bfe777ca33a684c5a9fe'
   })
 
-  depends_on 'curl' # R
-  depends_on 'expat' # R
+  depends_on 'curl' => :library
+  depends_on 'expat' => :library
   depends_on 'freeimage' => :build
-  depends_on 'gcc_lib' # R
-  depends_on 'geos' # R
-  depends_on 'giflib' # R
-  depends_on 'glibc' # R
-  depends_on 'hdf5' # R
-  depends_on 'json_c' # R
-  depends_on 'libarchive' # R
-  depends_on 'libdeflate' # R
-  depends_on 'libheif' # R
-  depends_on 'libjpeg_turbo' # R
-  depends_on 'libjxl' # R
-  depends_on 'libpng' # R
-  depends_on 'libwebp' # R
-  depends_on 'libxml2' # R
-  depends_on 'lz4' # R
-  depends_on 'openexr' # R
-  depends_on 'openjpeg' # R
-  depends_on 'openssl' # R
-  depends_on 'pcre2' # R
-  depends_on 'poppler' # R
-  depends_on 'proj' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'geos' => :library
+  depends_on 'giflib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'hdf5' => :library
+  depends_on 'json_c' => :library
+  depends_on 'libaec' => :library
+  depends_on 'libarchive' => :library
+  depends_on 'libbsd' => :library
+  depends_on 'libdeflate' => :library
+  depends_on 'libheif' => :library
+  depends_on 'libjpeg_turbo' => :library
+  depends_on 'libjxl' => :library
+  depends_on 'libpng' => :library
+  depends_on 'libwebp' => :library
+  depends_on 'libxml2' => :library
+  depends_on 'lz4' => :library
+  depends_on 'mysql' => :library
+  depends_on 'openexr' => :library
+  depends_on 'openjpeg' => :library
+  depends_on 'openssl' => :library
+  depends_on 'pcre2' => :library
+  depends_on 'poppler' => :library
+  depends_on 'proj' => :library
   depends_on 'py3_numpy' => :build
-  depends_on 'qhull' # R
-  depends_on 'sqlite' # R
+  depends_on 'qhull' => :library
+  depends_on 'sqlite' => :library
   depends_on 'swig' => :build
-  depends_on 'unixodbc' # R
-  depends_on 'xercesc' # R
-  depends_on 'xzutils' # R
-  depends_on 'zlib' # R
-  depends_on 'zstd' # R
+  depends_on 'unixodbc' => :library
+  depends_on 'xercesc' => :library
+  depends_on 'xzutils' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
 
   cmake_options '-DGDAL_USE_TIFF_INTERNAL=ON \
     -DGDAL_USE_GEOTIFF_INTERNAL=ON'
