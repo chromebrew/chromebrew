ef2c6922e63ca680b41ae10a1536feda1786566b
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-14 00:38:32 +0000

    libtirpc -> 1.3.8 in updater-libtirpc-1.3.8

--- packages/libtirpc.rb
+++ packages/libtirpc.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Libtirpc < Autotools
   description 'Libtirpc is a port of Suns Transport-Independent RPC library to Linux.'
   homepage 'https://sourceforge.net/projects/libtirpc/'
-  version '1.3.7'
+  version '1.3.8'
   license 'GPL-2'
   compatibility 'all'
   source_url "http://downloads.sourceforge.net/project/libtirpc/libtirpc/#{version}/libtirpc-#{version}.tar.bz2"
-  source_sha256 'b47d3ac19d3549e54a05d0019a6c400674da716123858cfdb6d3bdd70a66c702'
+  source_sha256 '8839959bfcc7a0f4c609d8e4f53f1c67ae33de23775ec35beb39ff15adf11920'
   binary_compression 'tar.zst'
 
   binary_sha256({
