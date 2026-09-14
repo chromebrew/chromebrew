543f4a73eae4397a53c353faeb6d58a63870f032
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2026-09-14 23:44:22 +0000

    coreutils: 9.11 → 9.12 (#18830)
    * coreutils -> 9.12 in updater-coreutils-9.12
    
    * Add autotools_skip_bootstrap boolean.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Add autotools_skip_autoreconf.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Ensure ./bootstrap is a file.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Handle hard-coded aclocal version.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * updater-coreutils-9.12: Package File Update Run on linux/386 container.
    
    * updater-coreutils-9.12: Package File Update Run on linux/amd64 container.
    
    * updater-coreutils-9.12: Package File Update Run on linux/arm/v7 container.
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: satmandu <satmandu@users.noreply.github.com>
    Co-authored-by: chromebrew-actions[bot] <220035932+chromebrew-actions[bot]@users.noreply.github.com>
    Co-authored-by: chromebrew-actions[bot] <chromebrew-actions[bot]@users.noreply.github.com>

--- packages/coreutils.rb
+++ packages/coreutils.rb
@@ -3,29 +3,40 @@ require 'buildsystems/autotools'
 class Coreutils < Autotools
   description 'The GNU Core Utilities are the basic file, shell and text manipulation utilities of the GNU operating system.'
   homepage 'https://www.gnu.org/software/coreutils/coreutils.html'
-  version '9.11'
+  version '9.12'
   license 'GPL-3'
   compatibility 'all'
   source_url "https://ftp.gnu.org/gnu/coreutils/coreutils-#{version}.tar.xz"
-  source_sha256 '394024eda0a5955217ceda9cd1201e65dc8fa3aa29c2951135a49521d57c3cc3'
+  source_sha256 'a480198559733e9b3da999e90543ac6f888a2caa544d8d664c5a1f17e528e210'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'fa39da43df4459a4a591211c50b465b449c9ecc0b6e1c0ca5ce0281a34e76ed3',
-     armv7l: 'fa39da43df4459a4a591211c50b465b449c9ecc0b6e1c0ca5ce0281a34e76ed3',
-       i686: '5ebefc26d69481ed2541fc5da8b272f891d24ecfffcf2ad2cd1d56ede2ac6a38',
-     x86_64: 'b448f293e47b1fce2a9d7ab1e10d4c05f580168bd1da31e50d4dcd2112bd4adb'
+    aarch64: 'ac79ea5c5fa7e0901ffaad17d5ad5d9b45c78123f3d263a2531cf8ad954726a4',
+     armv7l: 'ac79ea5c5fa7e0901ffaad17d5ad5d9b45c78123f3d263a2531cf8ad954726a4',
+       i686: '91fdc86356fe80aa5798271fe78f0fbc80bf81f887fa35602843c10b0037cfd1',
+     x86_64: '4c96f5744cd154bf19d961b0d5c04299ca5b5915fbe6cf8e8fcd56dc240239cf'
   })
 
   depends_on 'acl' => :executable
   depends_on 'attr' => :executable
   depends_on 'glibc' => :executable
   depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
   depends_on 'gmp' => :executable
   depends_on 'libcap' => :executable
   depends_on 'openssl' => :executable
 
+  autotools_skip_autoreconf
+  autotools_skip_bootstrap
+
   CREW_IN_CONTAINER ? conflicts_ok : (conflicts_with 'uutils_coreutils')
+
+  def self.patch
+    # The aclocal version is hardcoded.
+    aclocal_version = `aclocal --version|head -1|cut -d' ' -f4`.chomp
+    system "sed -i 's,1.18,#{aclocal_version},g' aclocal.m4"
+    system "sed -i 's,1.18,#{aclocal_version},g' configure"
+  end
 
   def self.prebuild
     File.write 'arch', <<~EOF
