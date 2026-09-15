df6c356c4fd8fb05b7941865fb77933bad435dec
Ed Reel <edreel@gmail.com>
2026-09-15 03:54:07 +0000

    Update gexiv2 (#18853)
    * Geos 3.14.1 => 3.15.0
    
    * Gexiv2 0.14.6 => 0.14.7

--- packages/gexiv2.rb
+++ packages/gexiv2.rb
@@ -3,7 +3,7 @@ require 'buildsystems/meson'
 class Gexiv2 < Meson
   description 'gexiv2 is a GObject wrapper around the Exiv2 photo metadata library.'
   homepage 'https://wiki.gnome.org/Projects/gexiv2/'
-  version '0.14.6'
+  version '0.14.7'
   license 'LGPL-2.1+ and GPL-2'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://gitlab.gnome.org/GNOME/gexiv2.git'
@@ -11,16 +11,17 @@ class Gexiv2 < Meson
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '1948fbe179b802852811e91322676660acff7a345c12a04b93069863a2ed430c',
-     armv7l: '1948fbe179b802852811e91322676660acff7a345c12a04b93069863a2ed430c',
-     x86_64: '2a25ac5db8d59dd65d75d4df4a37cb35449bf06f065c1fd6764f64bd6e0b4e25'
+    aarch64: '13d4fcf67d4093eb6b958f1d5b36c4a971a1c311dac245408570583978acaa4e',
+     armv7l: '13d4fcf67d4093eb6b958f1d5b36c4a971a1c311dac245408570583978acaa4e',
+     x86_64: 'd80f8423ee50874498dd501a618b3084bcac632668b4d543607fd522f2da3e5d'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
   depends_on 'gobject_introspection' => :build
-  depends_on 'libexiv2' # R
+  depends_on 'libexiv2' => :library
 
   gnome
 end
