b2b57f0a4729c1a082bcdb644514a4335df00da9
Ed Reel <edreel@gmail.com>
2026-09-15 10:40:44 +0000

    Gperf 3.1-97c1fbc => 3.3 (#18858)

--- packages/gperf.rb
+++ packages/gperf.rb
@@ -3,30 +3,27 @@ require 'buildsystems/autotools'
 class Gperf < Autotools
   description 'GNU gperf is a perfect hash function generator.'
   homepage 'https://www.gnu.org/software/gperf/'
-  version '3.1-97c1fbc'
+  version '3.3'
   license 'GPL-2'
   compatibility 'all'
-  source_url 'https://git.savannah.gnu.org/git/gperf.git'
-  git_hashtag '97c1fbcc8cc00c3a56af8be8543dedc588aace0f'
+  source_url "https://ftp.gnu.org/pub/gnu/gperf/gperf-#{version}.tar.gz"
+  source_sha256 'fd87e0aba7e43ae054837afd6cd4db03a3f2693deb3619085e6ed9d8d9604ad8'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '49b1152ce33b46afac2e25a656d28e1304283a6e3c847662dbdd80ad58cecf90',
-     armv7l: '49b1152ce33b46afac2e25a656d28e1304283a6e3c847662dbdd80ad58cecf90',
-       i686: '04ae0d6f0337cbc88935e2909074699461bf48534811b9a4b6cb22c2caa13614',
-     x86_64: 'e3876dc56fa7d9a23f2a91a15078f242f282da6f6178082e9d393a2002355d38'
+    aarch64: '9f913442cd16d2c1612ac95ecdfc7bae329b40e9bb753d2bf973a2b4b64bb27f',
+     armv7l: '9f913442cd16d2c1612ac95ecdfc7bae329b40e9bb753d2bf973a2b4b64bb27f',
+       i686: '0591d0d275bc2430867996f859bb0fbca9118021ae51fd7fb7e668abaf43d040',
+     x86_64: '42b377dbb7b1e42dd6762b0f50d098f7a1d0298f5d557300d6e16fd03aa070cf'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :executable
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
   depends_on 'wget2' => :build
 
-  def self.patch
-    # Documentation requires texlive, our package for which is currently not working on certain architectures or glibc versions.
-    system "sed -i '/gperf.texi/d' configure.ac"
-    system "sed -i 's,tests doc],tests],' configure.ac"
-    system "cat configure.ac | tac | sed '/doc/,+2d' | tac > configure.ac.new && mv configure.ac.new configure.ac"
-    # For whatever reason, gperf doesn't actually support the usual `./autogen.sh && ./configure && make` build setup.
-    system './autopull.sh'
+  def self.build
+    system "./configure --prefix=#{CREW_PREFIX}"
+    system 'make'
   end
 end
