0ad7835683a01c7f2bbea1c99ee011ddf73bbf61
Ed Reel <edreel@gmail.com>
2026-09-15 03:05:04 +0000

    Geos 3.14.1 => 3.15.0

--- packages/geos.rb
+++ packages/geos.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Geos < CMake
   description 'GEOS (Geometry Engine - Open Source) is a C++ port of the Java Topology Suite (JTS).'
   homepage 'https://libgeos.org/'
-  version '3.14.1'
+  version '3.15.0'
   license 'LGPL-2.1'
   compatibility 'all'
   source_url 'https://github.com/libgeos/geos.git'
@@ -11,12 +11,13 @@ class Geos < CMake
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'd8218f82c8e8005a4d52b2a0c539800b0d5945f98627daf3e7e53c32703ced5c',
-     armv7l: 'd8218f82c8e8005a4d52b2a0c539800b0d5945f98627daf3e7e53c32703ced5c',
-       i686: '6a50e6407976132d15a4e015b37ea88acfa9eea37ad643b7ce1ece3a7b7afe30',
-     x86_64: 'f9987bd69f636cb17867c7a9b3a298b67ca1c2ab6032f98061e6b16d1338826b'
+    aarch64: '0a1ef15f80382b1fc219070c113befedf489adc754fc0961d3a2faf959d5e14a',
+     armv7l: '0a1ef15f80382b1fc219070c113befedf489adc754fc0961d3a2faf959d5e14a',
+       i686: '65c5e063057d6d16fe4598710a8479d7d2c880d431ee75aecbebd6d6aae1a223',
+     x86_64: 'f64cae983257535e7303d6de18c18e1a826063a125b1c22d5c693179b93050d6'
   })
 
-  depends_on 'gcc_lib' # R
-  depends_on 'glibc' # R
+  depends_on 'gcc_lib' => :library
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 end
