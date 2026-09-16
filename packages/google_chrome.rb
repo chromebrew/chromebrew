faa09be43fb208166c72e331bd3b3c7ddf6810fe
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-16 00:53:14 +0000

    google_chrome -> 153.0.8010.47 in updater-google_chrome-153.0.8010.47

--- packages/google_chrome.rb
+++ packages/google_chrome.rb
@@ -3,11 +3,11 @@ require 'package'
 class Google_chrome < Package
   description 'Google Chrome is a fast, easy to use, and secure web browser.'
   homepage 'https://www.google.com/chrome/'
-  version '153.0.8010.36'
+  version '153.0.8010.47'
   license 'google-chrome'
   compatibility 'x86_64'
   source_url "https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_#{version}-1_amd64.deb"
-  source_sha256 '9bb44e33031c2f2857cf36b4343051a12f93058e4b781e3c76313df87f6c8d32'
+  source_sha256 'a326defde54de2f1c0d46d8bf4b6b5f3f76f0c897bd30b20b025097139a05bde'
 
   no_compile_needed
   no_shrink
