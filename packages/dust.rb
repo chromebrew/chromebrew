21d79f1ead1c4fcb87eae4c17388433d496977f7
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-17 00:49:55 +0000

    dust -> 1.2.6 in updater-dust-1.2.6

--- packages/dust.rb
+++ packages/dust.rb
@@ -3,7 +3,7 @@ require 'package'
 class Dust < Package
   description 'A more intuitive version of du in rust'
   homepage 'https://github.com/bootandy/dust'
-  version '1.2.5'
+  version '1.2.6'
   license 'Apache-2.0'
   compatibility 'all'
   source_url({
@@ -13,10 +13,10 @@ class Dust < Package
      x86_64: "https://github.com/bootandy/dust/releases/download/v#{version}/dust-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
   })
   source_sha256({
-    aarch64: 'e714a1cbd5a375489d06653256cd39b5144351fee0a21b00a694fc70d2f1c892',
-     armv7l: 'e714a1cbd5a375489d06653256cd39b5144351fee0a21b00a694fc70d2f1c892',
-       i686: 'a9b4a80e5e12b3879f7556b4cf96c41ec54594668d5c48dcb56a70fbb68bc32d',
-     x86_64: '64b16f5c10cc4c25d2eaa144e9d2d44b3ed8f72ee63b3bc0a92c85e21e9e0932'
+    aarch64: 'd78cc3aade9bde5d7eaf724a2f0d3f09d484e92f87c29c64c17d5c9e021281b8',
+     armv7l: 'd78cc3aade9bde5d7eaf724a2f0d3f09d484e92f87c29c64c17d5c9e021281b8',
+       i686: '0657c64d00d8e9b05a46c003a9e95e4af4c0038d125ea6b5879f187b487ad204',
+     x86_64: '14f788707aad217667f6812cf1a9639bf5c861a2ee815d486cffc36051f5e759'
   })
 
   no_compile_needed
