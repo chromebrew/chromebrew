fe6e5a3cf6a492489b5bfb1811fd2e7b0f6146f9
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-18 00:47:31 +0000

    jemalloc -> 5.4.0 in updater-jemalloc-5.4.0

--- packages/jemalloc.rb
+++ packages/jemalloc.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Jemalloc < Autotools
   description 'jemalloc is a general purpose malloc(3) implementation that emphasizes fragmentation avoidance and scalable concurrency support.'
   homepage 'http://jemalloc.net/'
-  version '5.3.1'
+  version '5.4.0'
   license 'BSD'
   compatibility 'all'
   source_url "https://github.com/jemalloc/jemalloc/releases/download/#{version}/jemalloc-#{version}.tar.bz2"
-  source_sha256 '3826bc80232f22ed5c4662f3034f799ca316e819103bdc7bb99018a421706f92'
+  source_sha256 '200776fac271093e7c2f21edd6d62657ecd2be578d9328633f2a86bfa6ef4f1d'
   binary_compression 'tar.zst'
 
   binary_sha256({
