98e8af41870af28f6585f7f350b66d708c1eec3b
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-18 00:50:22 +0000

    usql -> 0.21.5 in updater-usql-0.21.5

--- packages/usql.rb
+++ packages/usql.rb
@@ -3,7 +3,7 @@ require 'package'
 class Usql < Package
   description 'Universal command-line interface for SQL databases'
   homepage 'https://github.com/xo/usql'
-  version '0.21.4'
+  version '0.21.5'
   license 'MIT'
   compatibility 'x86_64'
   source_url 'SKIP'
