bd8b9ee51af6858e3f4ba9ccdbf3063d3057e744
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-20 00:53:33 +0000

    vulkan_headers -> 1.4.363 in updater-vulkan_headers-1.4.363

--- packages/vulkan_headers.rb
+++ packages/vulkan_headers.rb
@@ -3,7 +3,7 @@ require 'buildsystems/cmake'
 class Vulkan_headers < CMake
   description 'Vulkan header files'
   homepage 'https://www.khronos.org/vulkan'
-  version '1.4.362'
+  version '1.4.363'
   license 'Apache-2.0'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/KhronosGroup/Vulkan-Headers.git'
