4a7938488ac6c4323547d8b101fd0c0cf30dc705
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-20 00:48:23 +0000

    appflowy -> 0.14.4 in updater-appflowy-0.14.4

--- packages/appflowy.rb
+++ packages/appflowy.rb
@@ -3,12 +3,12 @@ require 'package'
 class Appflowy < Package
   description 'AI collaborative workspace where you achieve more without losing control of your data. The best open source alternative to Notion.'
   homepage 'https://www.appflowy.io/'
-  version '0.14.3'
+  version '0.14.4'
   license 'AGPL-3.0'
   compatibility 'x86_64'
   min_glibc '2.28'
   source_url "https://github.com/AppFlowy-IO/AppFlowy/releases/download/#{version}/AppFlowy-#{version}-linux-x86_64.tar.gz"
-  source_sha256 '92c77841e65aa823eef6b4d9525006a88236fc0ca9be680782ecabe37f3baf40'
+  source_sha256 'e465cbddcc7ccd930fee4ce82a81842447972371a5cfdc2e20bb4a66057f6499'
 
   depends_on 'gtk3' # R
   depends_on 'gobject_introspection' # R
