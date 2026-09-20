f2c1712177da43da31aa503c78110090910b358b
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-20 00:52:25 +0000

    remind -> 06.03.04 in updater-remind-06.03.04

--- packages/remind.rb
+++ packages/remind.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Remind < Autotools
   description 'Remind is a sophisticated calendar and alarm program.'
   homepage 'https://dianne.skoll.ca/projects/remind/'
-  version '06.03.03'
+  version '06.03.04'
   license 'GPL-2'
   compatibility 'all'
   source_url "https://dianne.skoll.ca/projects/remind/download/remind-#{version}.tar.gz"
-  source_sha256 '6045283b98e004a683b13a636409ba691401d2ae9e5122d244c151972e923341'
+  source_sha256 'c56976b4bb3f3c838b4861f35b1a0ed80695b6a5e27c7736763c6518a884218b'
   binary_compression 'tar.zst'
 
   binary_sha256({
