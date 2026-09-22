af203326037a5841416519e96407c4828632cca7
satmandu <satmandu@users.noreply.github.com>
2026-09-22 15:08:37 +0000

    lttng_ust -> 2.16.0 in updater-lttng_ust-2.16.0

--- packages/lttng_ust.rb
+++ packages/lttng_ust.rb
@@ -3,11 +3,11 @@ require 'buildsystems/autotools'
 class Lttng_ust < Autotools
   description 'Linux Trace Toolkit: next generation is a modern toolkit for tracing Linux systems and applications.'
   homepage 'https://lttng.org/'
-  version '2.15.1'
+  version '2.16.0'
   license 'LGPL-2.1-only, MIT, GPL-2.0-only, GPL-3.0-or-later, BSD-2-Clause, BSD-3-Clause'
   compatibility 'all'
   source_url "https://lttng.org/files/lttng-ust/lttng-ust-#{version}.tar.bz2"
-  source_sha256 '37c9b58ea7aa7bc47d6630b52ba1a48ebce095b9a196eab4ddd273d78301792d'
+  source_sha256 '1e84e02fa1fc1261eb6cf3d14d64006506a29f7c605ac85de02180318c6aa38a'
   binary_compression 'tar.zst'
 
   binary_sha256({
