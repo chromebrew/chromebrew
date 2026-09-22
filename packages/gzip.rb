4ab484e0bc0c553893ed092cf7224b1cde5944ad
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2026-09-22 22:44:43 +0000

    gzip -> 1.15 in updater-gzip-1.15 — gzip: 1.14 → 1.15 (#19014)
    * gzip -> 1.15 in updater-gzip-1.15
    
    * Adjust armv7l build to add --disable-year2038.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Disable tests for gzip.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * updater-gzip-1.15: Package File Update Run on linux/386 container.
    
    * updater-gzip-1.15: Package File Update Run on linux/amd64 container.
    
    * updater-gzip-1.15: Package File Update Run on linux/arm/v7 container.
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: github-merge-queue <github-merge-queue@users.noreply.github.com>
    Co-authored-by: chromebrew-actions[bot] <chromebrew-actions[bot]@users.noreply.github.com>

--- packages/gzip.rb
+++ packages/gzip.rb
@@ -3,21 +3,25 @@ require 'buildsystems/autotools'
 class Gzip < Autotools
   description 'GNU Gzip is a popular data compression program originally written by Jean-loup Gailly for the GNU project.'
   homepage 'https://www.gnu.org/software/gzip/'
-  version '1.14'
+  version '1.15'
   compatibility 'all'
   license 'GPL-3'
   source_url "https://ftp.gnu.org/gnu/gzip/gzip-#{version}.tar.xz"
-  source_sha256 '01a7b881bd220bfdf615f97b8718f80bdfd3f6add385b993dcf6efd14e8c0ac6'
+  source_sha256 '9aa0cc780dec156b8282844833b342ab7cb08c25d2cd9a1869cdd0df31deff48'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '4cc05d25a34eb5a7d26b3aded51d97ef80edc37fad21fad41888ddb774190b4a',
-     armv7l: '4cc05d25a34eb5a7d26b3aded51d97ef80edc37fad21fad41888ddb774190b4a',
-       i686: 'f3b6c28a23474b435d3b9479af4f003e50a873bd599b8ec5cba2b1e582b90936',
-     x86_64: '5b2d9a48456d3aca4c07f778d4562ce86afdb8cf37eb333e1a1340c180b399b2'
+    aarch64: '896303cba73c10a3decf76698703107442501aeb107f490dc3776a3d772c2faa',
+     armv7l: '896303cba73c10a3decf76698703107442501aeb107f490dc3776a3d772c2faa',
+       i686: '177e708ddf22ddf87af76841a30b2a8d59385b24e7bed8cdc55c0a3bf42b17d4',
+     x86_64: '7853b68cd349c842d0c3937d72035c05377085612ece18eaa2be6e74dca03b9c'
   })
 
   depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
 
-  run_tests
+  # Tests fail due to tty not being available in the automated build containers.
+  # more: Failed to initialize input reader
+
+  autotools_configure_options ('--disable-year2038' if ARCH == 'armv7l').to_s
 end
