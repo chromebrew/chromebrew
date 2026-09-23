06ce2dc74f21242830e26031ee304d52971ae621
Ed Reel <edreel@gmail.com>
2026-09-23 06:19:20 +0000

    Json_c 0.18-d1018cf => 0.19-20260627

--- packages/json_c.rb
+++ packages/json_c.rb
@@ -1,20 +1,20 @@
-require 'buildsystems/cmake'
+require 'buildsystems/meson'
 
-class Json_c < CMake
+class Json_c < Meson
   description 'JSON-C implements a reference counting object model that allows you to easily construct JSON objects in C, output them as JSON formatted strings and parse JSON formatted strings back into the C representation of JSON objects.'
   homepage 'https://github.com/json-c/json-c'
-  version '0.18-d1018cf'
+  version '0.19-20260627'
   license 'MIT'
   compatibility 'all'
   source_url 'https://github.com/json-c/json-c.git'
-  git_hashtag 'd1018cf6ba662a4003a0ce526b53def94da7f04b'
+  git_hashtag "json-c-#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'f14c0115baee999fd056107d474db37ef639b025d5554c7258710769d2b67aa4',
-     armv7l: 'f14c0115baee999fd056107d474db37ef639b025d5554c7258710769d2b67aa4',
-       i686: '9f8804415a3dfc2bcb0943e97d2eb71f60a9502526ee42579269a6fcee6be83f',
-     x86_64: '4984e1d5d1f7436165cd7cb7f2c9ceea2b7263609512896e9b2643521632ced4'
+    aarch64: '03b86eeb687c4bf8bc06dfd483aaf30a56dc5dcfc6fa62803092e22e73a4329c',
+     armv7l: '03b86eeb687c4bf8bc06dfd483aaf30a56dc5dcfc6fa62803092e22e73a4329c',
+       i686: 'de0cc3e7b525e4bbf3dcbc06561b3d76788cbcab6d4977e3905bc569ec6ac932',
+     x86_64: '5469ff095557137325ca317b416c28fed2ef009129c8d23551877cc4af81eb37'
   })
 
   depends_on 'glibc' => :library
@@ -22,13 +22,9 @@ class Json_c < CMake
   depends_on 'graphviz' => :build
   depends_on 'libbsd' => :library
 
-  def self.patch
-    system "sed -i 's,*child,*child = NULL,' json_object.c"
-  end
+  meson_options '-Denable_threading=true'
 
-  cmake_options '-DCMAKE_POLICY_VERSION_MINIMUM=3.5'
-
-  cmake_install_extras do
+  meson_install_extras do
     FileUtils.ln_s "#{CREW_LIB_PREFIX}/libjson-c.so", "#{CREW_DEST_LIB_PREFIX}/libjson-c.so.3"
     FileUtils.ln_s "#{CREW_LIB_PREFIX}/libjson-c.so", "#{CREW_DEST_LIB_PREFIX}/libjson-c.so.4"
   end
