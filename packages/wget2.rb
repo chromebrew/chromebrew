85d4f2c4cc812b6ce0849a1d1fb004ce8b2919d5
Satadru Pramanik, DO, MPH, MEng <satadru@gmail.com>
2026-09-23 20:10:34 +0000

    wget2 -> 2.3.0 in updater-wget2-2.3.0 — wget2: 2.2.1 → 2.3.0 (#19026)
    * wget2 -> 2.3.0 in updater-wget2-2.3.0
    
    * Add threads fix.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Add patch to fix broken i686 compile.
    
    Fixes #19029
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * Add permissions section to workflow.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    * updater-wget2-2.3.0: Package File Update Run on linux/386 container.
    
    * updater-wget2-2.3.0: Package File Update Run on linux/amd64 container.
    
    * updater-wget2-2.3.0: Package File Update Run on linux/arm/v7 container.
    
    * Lint Linter-Handoff workflow.
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    
    ---------
    
    Signed-off-by: Satadru Pramanik <satadru@gmail.com>
    Co-authored-by: github-merge-queue <github-merge-queue@users.noreply.github.com>
    Co-authored-by: chromebrew-actions[bot] <chromebrew-actions[bot]@users.noreply.github.com>

--- packages/wget2.rb
+++ packages/wget2.rb
@@ -3,43 +3,44 @@ require 'buildsystems/autotools'
 class Wget2 < Autotools
   description 'GNU Wget is a free software package for retrieving files using HTTP, HTTPS, FTP and FTPS.'
   homepage 'https://www.gnu.org/software/wget/'
-  version '2.2.1'
+  version '2.3.0'
   license 'GPL-3'
   compatibility 'all'
   source_url "https://ftp.gnu.org/gnu/wget/wget2-#{version}.tar.gz"
-  source_sha256 'd7544b13e37f18e601244fce5f5f40688ac1d6ab9541e0fbb01a32ee1fb447b4'
+  source_sha256 '4f1915b2a55a789a15f2f9ada7cc44bca81418e648f76fd88a7f4dd028b2149f'
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: 'bcc35aa52125321e99fa293f99bff03c764ac36da3b48b921ed04dca1203fefb',
-     armv7l: 'bcc35aa52125321e99fa293f99bff03c764ac36da3b48b921ed04dca1203fefb',
-       i686: '94fd7df2890c9feb82f44e3d50363c45aa067c7a550e7335654e70db911c3f33',
-     x86_64: '7fcbd974cec93c4161057f3b6743dc709fe624eb0de97dc1736f623d2369333b'
+    aarch64: '68874477d0d746481ac31a2af585ab8e35b8d788ce41914f1268bc92ed86e09a',
+     armv7l: '68874477d0d746481ac31a2af585ab8e35b8d788ce41914f1268bc92ed86e09a',
+       i686: '927f2ab0a6affa680a3bc2915f1a0034e53b90caee0963ef9e74e88dcb3ad319',
+     x86_64: 'ea7e4870d0fe46eaf2f16a5bd611ffd489e69be7c7a020f2b343632aeeba4ec6'
   })
 
-  depends_on 'brotli' # R
-  depends_on 'bzip2' # R
+  depends_on 'brotli' => :library
+  depends_on 'bzip2' => :library
   depends_on 'ca_certificates' => :logical
-  depends_on 'glibc' # R
-  depends_on 'gmp' # R
-  depends_on 'gnutls' # R
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
+  depends_on 'gmp' => :executable
+  depends_on 'gnutls' => :library
   depends_on 'gperf' => :build
-  depends_on 'libidn2' # R
+  depends_on 'libidn2' => :library
   depends_on 'libmetalink' => :build
-  depends_on 'libnghttp2' # R
-  depends_on 'libpsl' # R
-  depends_on 'libtasn1' # R
-  depends_on 'libunbound' # R
-  depends_on 'libunistring' # R
-  depends_on 'lzlib' # R
-  depends_on 'nettle' # R
-  depends_on 'openssl' # R
-  depends_on 'p11kit' # R
-  depends_on 'pcre2' # R
+  depends_on 'libnghttp2' => :library
+  depends_on 'libpsl' => :library
+  depends_on 'libtasn1' => :executable
+  depends_on 'libunbound' => :executable
+  depends_on 'libunistring' => :executable
+  depends_on 'lzlib' => :library
+  depends_on 'nettle' => :executable
+  depends_on 'openssl' => :executable
+  depends_on 'p11kit' => :executable
+  depends_on 'pcre2' => :library
   depends_on 'util_linux' => :build
-  depends_on 'xzutils' # R
-  depends_on 'zlib' # R
-  depends_on 'zstd' # R
+  depends_on 'xzutils' => :library
+  depends_on 'zlib' => :library
+  depends_on 'zstd' => :library
 
   conflicts_with 'wget_static'
 
@@ -53,5 +54,22 @@ class Wget2 < Autotools
   autotools_install_extras do
     FileUtils.ln_sf "#{CREW_PREFIX}/bin/wget2", "#{CREW_DEST_PREFIX}/bin/wget"
     FileUtils.rm_f "#{CREW_DEST_PREFIX}/bin/wget2_noinstall"
+  end
+
+  def self.patch
+    # The c11threads threads.h breaks builds on software that uses gnulib.
+    # See: https://github.com/jtsiomb/c11threads/issues/19
+    # Note that c11threads is a workaround for C11 Threads only being
+    # introduced in Glibc 2.28 as per:
+    # https://sourceware.org/bugzilla/show_bug.cgi?id=14092#c10
+    if LIBC_VERSION.to_f < 2.28 && ENV['NESTED_CI']
+      puts 'Removing the c11threads include/threads.h from the c11threads package to prevent build failures.'.orange
+      FileUtils.rm_f "#{CREW_PREFIX}/include/threads.h"
+    end
+    patches = [
+      # Fix for broken legacy TCP Fast Open code branch
+      ['https://github.com/rockdaboot/wget2/commit/db600f40c51f0e612085f19351df3f9ad963bb5f.patch', '24b6928e87f388f633b51b98025c15ca6cd40c4cd388278cbb98bd06c64be719']
+    ]
+    ConvenienceFunctions.patch(patches) if ARCH == 'i686' && version == '2.3.0'
   end
 end
