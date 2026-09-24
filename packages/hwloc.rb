5030535c2f3b336158da5eb7482ff5e89cc155bd
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-24 00:50:53 +0000

    hwloc -> 2.15.0 in updater-hwloc-2.15.0

--- packages/hwloc.rb
+++ packages/hwloc.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Hwloc < Autotools
   description 'Portable Hardware Locality is a portable abstraction of hierarchical architectures'
   homepage 'https://www.open-mpi.org/projects/hwloc/'
-  version '2.14.0'
+  version '2.15.0'
   license 'BSD-3 Clause'
   compatibility 'all'
   source_url 'https://github.com/open-mpi/hwloc.git'
