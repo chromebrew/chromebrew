23545ea3fc36a2aa8cb379640fce0e40cf93521b
Ed Reel <edreel@gmail.com>
2026-09-24 21:33:50 +0000

    Keybase 5.5.1 => 6.6.3

--- packages/keybase.rb
+++ packages/keybase.rb
@@ -3,24 +3,25 @@ require 'package'
 class Keybase < Package
   description 'Keybase is encryption for everyone. Installs keybase cli'
   homepage 'https://keybase.io'
-  version '5.5.1'
+  version '6.6.3'
   license 'Apache-2.0, BSD, BSD-2, LGPL-3, MIT, MPL-2.0'
   compatibility 'all'
-  source_url 'https://github.com/keybase/client/archive/v5.5.1.tar.gz'
-  source_sha256 'a65dc4b62fc1299dd17da52ddd2484fa1dc1e7d2a4776c3a6e112ee020980b12'
-  binary_compression 'tar.xz'
+  source_url 'https://github.com/keybase/client.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '248fe6da5cd0a7c8e4663901a6acbcb94ca1347291309545e3e068aad6a0dd2d',
-     armv7l: '248fe6da5cd0a7c8e4663901a6acbcb94ca1347291309545e3e068aad6a0dd2d',
-       i686: '15662482b0381d8a3053897f21059bd425f87e06cd907c7ceb3e8f6e453f2afb',
-     x86_64: '554a661f752277920cb572de6aad58d0888e188defc0a4fb79ed8a0a3d8eadb2'
+    aarch64: '651fe8afc45cb926d225fadf1d7973b266da4ed6044f025847930643c4692aec',
+     armv7l: '651fe8afc45cb926d225fadf1d7973b266da4ed6044f025847930643c4692aec',
+       i686: 'a84d66c4375aa92049c9718d077909e961cfd183f07d5e407b672455aead4482',
+     x86_64: 'd91c91285cd5093706353a56fe8488a5b88eea39b70ee200859abbd0dda6feeb'
   })
 
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
   depends_on 'go' => :build
 
   def self.install
-    system 'go get github.com/keybase/client/go/keybase'
-    system "go build -o #{CREW_DEST_PREFIX}/bin/keybase -tags production github.com/keybase/client/go/keybase"
+    system "go build -C go/keybase -o #{CREW_DEST_PREFIX}/bin/keybase"
   end
 end
