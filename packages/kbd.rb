55e6f5fa4d69eb9f14872d3c1ac4eab1abc0723c
Ed Reel <edreel@gmail.com>
2026-09-24 15:44:25 +0000

    Kbd 2.0.4 => 2.10.0

--- packages/kbd.rb
+++ packages/kbd.rb
@@ -1,33 +1,44 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Kbd < Package
+class Kbd < Autotools
   description 'The kbd project contains tools for managing Linux console'
   homepage 'http://kbd-project.org/'
-  version '2.0.4'
+  version '2.10.0'
   license 'GPL-2'
-  compatibility 'all'
-  source_url 'https://github.com/legionus/kbd/archive/2.0.4.tar.gz'
-  source_sha256 '18f45ee8790ddb18363fcb98d60e715528b1c04aa4501bd69111609d43240d3e'
-  binary_compression 'tar.xz'
+  compatibility 'aarch64 armv7l x86_64'
+  source_url 'https://github.com/legionus/kbd.git'
+  git_hashtag "v#{version}"
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '0c53c9ac9dfae25f7ddbc308db92cff85b5343e594cfb5e9f73058340eda1401',
-     armv7l: '0c53c9ac9dfae25f7ddbc308db92cff85b5343e594cfb5e9f73058340eda1401',
-       i686: '448eb6055759318a5e87bbcf37e57bf636aaaa941b69c245f7db09d5b20c5720',
-     x86_64: '7e16b148b66c8466ec6dc2685da62c83feb09a915e21ed0b5b7fb5d9be0e14a1'
+    aarch64: 'ffd2abaa65fc09a2ec8442804d11fe6b3b60f3f83750a4a043783a9e952cc1c3',
+     armv7l: 'ffd2abaa65fc09a2ec8442804d11fe6b3b60f3f83750a4a043783a9e952cc1c3',
+     x86_64: '3697dfda4d60aea89714feda753f53551ff743c8c96329a876c88df382a4fe17'
   })
 
-  depends_on 'linux_pam'
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'libxkbcommon' => :executable
+  depends_on 'libxkbfile' => :build
+  depends_on 'linux_pam' => :executable
 
-  def self.build
-    system './autogen.sh'
-    system './configure',
-           "--prefix=#{CREW_PREFIX}",
-           '--disable-static'
-    system 'make'
+  # Fix mold: error: undefined symbol: dlopen
+  autotools_configure_options "LIBS='-ldl' --disable-tests"
+
+  def self.patch
+    Dir['src/libkeymap/*'].each do |file|
+      # Fix error: implicit declaration of function 'strlcat'
+      system "sed -i 's,strlcat,strncat,g' #{file}"
+      # Fix error: implicit declaration of function 'strlcpy'
+      system "sed -i 's,strlcpy,strncpy,g' #{file}"
+    end
+    Dir['tests/libkeymap/*'].each do |file|
+      # Fix error: implicit declaration of function 'strlcpy'
+      system "sed -i 's,strlcpy,strncpy,g' #{file}"
+    end
   end
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  # Tests fail spectacularly.
+  # ERROR: 67 tests were run, 66 failed unexpectedly.
+  # run_tests
 end
