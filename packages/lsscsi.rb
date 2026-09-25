e7c2de626eea92a9490c3e6cfa9d8d0f0e429001
Ed Reel <edreel@gmail.com>
2026-09-25 21:28:26 +0000

    Lsscsi 0.32 => 0.33 (#19121)

--- packages/lsscsi.rb
+++ packages/lsscsi.rb
@@ -3,18 +3,18 @@ require 'buildsystems/autotools'
 class Lsscsi < Autotools
   description 'The lsscsi command lists information about SCSI devices in Linux.'
   homepage 'http://sg.danny.cz/scsi/lsscsi.html'
-  version '0.32'
+  version '0.33'
   license 'GPL-2'
   compatibility 'all'
-  source_url "https://sg.danny.cz/scsi/lsscsi-#{version}.tgz"
-  source_sha256 '0a800e9e94dca2ab702d65d72777ae8cae078e3d74d0bcbed64ba0849e8029a1'
+  source_url 'https://github.com/doug-gilbert/lsscsi.git'
+  git_hashtag "r#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '3c18783f57d5e4d774c089d47ce19a4084b28180e267c5a2b3f6bdbf702998a6',
-     armv7l: '3c18783f57d5e4d774c089d47ce19a4084b28180e267c5a2b3f6bdbf702998a6',
-       i686: 'ed007f117c7401e0fec65b3624117eda1b5a04cac0382853dbd81dba6c62f59c',
-     x86_64: '3b3e228b6f3cc737ff8dc4d951ca5aea843d42ee0043cd0c90b6ba56838c3655'
+    aarch64: 'a58f691a1a8ab2b5e221037ff8c88a99e1e2a75fa61c6c3d0c67fa3aeae6c61b',
+     armv7l: 'a58f691a1a8ab2b5e221037ff8c88a99e1e2a75fa61c6c3d0c67fa3aeae6c61b',
+       i686: '9a23053d5583154830ce2121161cc8f3ca51ac763ee626d359ae6f4451de504d',
+     x86_64: '73a11678236942ac0dbb3009bb74aaf14a2ae9cf89d7efad23351b42e3ed6572'
   })
 
   depends_on 'glibc' => :executable
