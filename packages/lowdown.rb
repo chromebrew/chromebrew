88afbead7e5f28f5ff3a57e7cb87a8069940c248
Ed Reel <edreel@gmail.com>
2026-09-25 21:03:58 +0000

    Lowdown 1.4.0 => 3_2_1

--- packages/lowdown.rb
+++ packages/lowdown.rb
@@ -3,31 +3,38 @@ require 'package'
 class Lowdown < Package
   description 'Simple markdown translator'
   homepage 'https://kristaps.bsd.lv/lowdown'
-  version '1.4.0'
+  version '3_2_1'
   license 'ISC'
   compatibility 'aarch64 armv7l x86_64'
   source_url 'https://github.com/kristapsdz/lowdown.git'
-  git_hashtag "VERSION_#{version.gsub('.', '_')}"
+  git_hashtag "VERSION_#{version}"
   binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '69af2c86e0e24dfaa47073714445c037944caa11fea828bb34fd5b0184da2007',
-     armv7l: '69af2c86e0e24dfaa47073714445c037944caa11fea828bb34fd5b0184da2007',
-     x86_64: '62a800d9f0895e4245a8cbe010f2e250f861610828cc0f60d959d16e950d61b2'
+    aarch64: 'd80f0e9173b29d5537177d632da362f98d042e013c8c24f39897623c7fff86b9',
+     armv7l: 'd80f0e9173b29d5537177d632da362f98d042e013c8c24f39897623c7fff86b9',
+     x86_64: '84177cd0013162e8be87ee1d19e1a8d9b7360c93877103b8d8a3351773ce34bc'
   })
 
-  depends_on 'glibc' # R
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 
   def self.build
     system './configure'
     # These constants need set inside Makefile
-    system "sed -i '17iPREFIX = #{CREW_PREFIX}' Makefile"
-    system "sed -i '18iLIBDIR = #{CREW_LIB_PREFIX}' Makefile"
+    system "sed -i '18iPREFIX = #{CREW_PREFIX}' Makefile"
+    system "sed -i '19iLIBDIR = #{CREW_LIB_PREFIX}' Makefile"
+    system "sed -i '20iDESTDIR = #{CREW_DEST_DIR}' Makefile"
+    # The following lines fix Makefile:###: *** missing separator.
+    system "sed -i '192d' Makefile"
+    system "sed -i '188,190d' Makefile"
+    system "sed -i '182,183d' Makefile"
+    system "sed -i '178,179d' Makefile"
+    system "sed -i '156,162d' Makefile"
     system 'make'
-    system 'make regress'
   end
 
   def self.install
-    system "DESTDIR=#{CREW_DEST_DIR} make install install_libs"
+    system 'make install install_libs'
   end
 end
