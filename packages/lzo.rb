187919e7d92d5a400cf3137977d3170951156e12
Ed Reel <edreel@gmail.com>
2026-09-25 09:50:50 +0000

    Rebuild lzo to fix error (#19119)

--- packages/lzo.rb
+++ packages/lzo.rb
@@ -1,34 +1,26 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Lzo < Package
+class Lzo < Autotools
   description 'LZO is a portable lossless data compression library written in ANSI C.'
   homepage 'http://www.oberhumer.com/opensource/lzo/'
-  version '2.10-1'
+  version '2.10'
   license 'GPL-2+'
   compatibility 'all'
-  source_url 'https://www.oberhumer.com/opensource/lzo/download/lzo-2.10.tar.gz'
+  source_url "https://www.oberhumer.com/opensource/lzo/download/lzo-#{version}.tar.gz"
   source_sha256 'c0f892943208266f9b6543b3ae308fab6284c5c90e627931446fb49b4221a072'
-  binary_compression 'tar.xz'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '8909723551598aea2da661c6e327d42ede4eedef60351113b2ee7923e45523d4',
-     armv7l: '8909723551598aea2da661c6e327d42ede4eedef60351113b2ee7923e45523d4',
-       i686: 'cec4de5d184f57d18660b4ea8a8ca5f69a8d80cc620e79dedc617f9fab04d63f',
-     x86_64: '04c5e6c4405569793a8681869376ee5144258ced7c39eb06742f891fed741ff7'
+    aarch64: 'bdbc8f70380449f6e6c88b2757b0e74d0fc85dcdc18f11b25ae6e121351fed0f',
+     armv7l: 'bdbc8f70380449f6e6c88b2757b0e74d0fc85dcdc18f11b25ae6e121351fed0f',
+       i686: '8b4d18a37e159d1577651c39b34f0117e9c376524e0858349daf5d9c0d582ab0',
+     x86_64: '7273e03053685652fe8af5fe5abced0b584e694ffc5e511a1516acfe5cfb97c7'
   })
 
-  def self.build
-    system './configure',
-           '--enable-shared',
-           '--disable-static',
-           "--prefix=#{CREW_PREFIX}",
-           '--disable-maintainer-mode',
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--disable-dependency-tracking'
-    system 'make'
-  end
+  depends_on 'glibc' => :library
+  depends_on 'glibc_lib' => :library
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
-  end
+  autotools_configure_options ' \
+    --enable-shared \
+    --disable-static'
 end
