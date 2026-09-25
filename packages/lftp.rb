fbde3d2bde2d50a6a0636b56384df2cac06e6cca
Ed Reel <edreel@gmail.com>
2026-09-25 09:51:21 +0000

    Lftp 4.9.2 => 4.9.3 (#19115)

--- packages/lftp.rb
+++ packages/lftp.rb
@@ -1,28 +1,43 @@
-require 'package'
+require 'buildsystems/autotools'
 
-class Lftp < Package
+class Lftp < Autotools
   description 'LFTP is a sophisticated file transfer program for ftp/http/sftp/fish/torrent'
   homepage 'https://lftp.yar.ru'
-  version '4.9.2'
+  version '4.9.3'
   license 'GPL-3'
   compatibility 'all'
-  source_url 'https://lftp.yar.ru/ftp/lftp-4.9.2.tar.xz'
-  source_sha256 'c517c4f4f9c39bd415d7313088a2b1e313b2d386867fe40b7692b83a20f0670d'
-  binary_compression 'tar.xz'
+  source_url "https://lftp.yar.ru/ftp/lftp-#{version}.tar.xz"
+  source_sha256 '96e7199d7935be33cf6b1161e955b2aab40ab77ecdf2a19cea4fc1193f457edc'
+  binary_compression 'tar.zst'
 
   binary_sha256({
-    aarch64: '370398386c9df785c8f13dce8f1d25313bfb5a5f0a472e4abd14664abcae51aa',
-     armv7l: '370398386c9df785c8f13dce8f1d25313bfb5a5f0a472e4abd14664abcae51aa',
-       i686: '07e9ced116f2c5cec2f1a05b866493ba8df967cc7af2d7703a50224e3242e85a',
-     x86_64: 'd8df0155b54627beb280cc208d40c6f702a1aa970e9863ac17c154d9f478d9db'
+    aarch64: 'de3ce92cfea7a78528a1838621d71783458eab0ed12fe5a614d562062fa1f9a6',
+     armv7l: 'de3ce92cfea7a78528a1838621d71783458eab0ed12fe5a614d562062fa1f9a6',
+       i686: 'a87599a505f1ff68712074d375db00e3eef299c7968c89f5b1e67929c57a84ca',
+     x86_64: '428ea5e80bbe8d51d53b991532275f6bb1c2b3a4e2475b18bf1eb103fee0a67a'
   })
 
-  def self.build
-    system "./configure #{CREW_CONFIGURE_OPTIONS} --disable-nls --disable-dependency-tracking --with-linux-crypto"
-    system 'make'
-  end
+  depends_on 'brotli' => :executable
+  depends_on 'expat' => :executable
+  depends_on 'gcc_lib' => :executable
+  depends_on 'glibc' => :executable
+  depends_on 'glibc_lib' => :executable
+  depends_on 'gmp' => :executable
+  depends_on 'gnutls' => :executable
+  depends_on 'libidn2' => :executable
+  depends_on 'libtasn1' => :executable
+  depends_on 'libunistring' => :executable
+  depends_on 'ncurses' => :executable
+  depends_on 'nettle' => :executable
+  depends_on 'p11kit' => :executable
+  depends_on 'readline' => :executable
+  depends_on 'zlib' => :executable
+  depends_on 'zstd' => :executable
 
-  def self.install
-    system 'make', "DESTDIR=#{CREW_DEST_DIR}", 'install'
+  autotools_configure_options '--disable-nls --with-linux-crypto'
+
+  def self.patch
+    # Fix error: weak declaration of 'thrd_exit' must be public.
+    system "sed -i 's,static C11THREADS_INLINE void thrd_exit(,public C11THREADS_INLINE void thrd_exit(,' #{CREW_PREFIX}/include/threads.h"
   end
 end
