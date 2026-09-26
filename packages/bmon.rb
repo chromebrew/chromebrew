2a6e1e4705db071d03d1170809a3039ef03b32be
github-merge-queue <github-merge-queue@users.noreply.github.com>
2026-09-26 00:45:28 +0000

    bmon -> 5.0 in updater-bmon-5.0

--- packages/bmon.rb
+++ packages/bmon.rb
@@ -3,7 +3,7 @@ require 'buildsystems/autotools'
 class Bmon < Autotools
   description 'bmon is a commandline bandwidth monitor and rate estimator.'
   homepage 'https://github.com/tgraf/bmon'
-  version '4.0'
+  version '5.0'
   license 'BSD-2 and MIT'
   compatibility 'all'
   source_url 'https://github.com/tgraf/bmon.git'
